c602d90fca5e3aed7abbcb8d7f0b22f12a369256
Unknown <targetjr>
2000-04-06 10:27:01 -0400

    change to #include <vcl/vcl_c*>

---
+++ vcl/vcl_cfloat.h
@@ -0,0 +1,10 @@
+#ifndef vcl_cfloat_h_
+#define vcl_cfloat_h_
+/*
+  Peter.Vanroose@esat.kuleuven.ac.be
+*/
+
+#include <float.h>
+/* This should define C-style numeric floating point macros */
+
+#endif
