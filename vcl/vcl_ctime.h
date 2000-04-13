b7fed27760ea7891605735c8bd73fdcafa741646
Unknown <targetjr>
2000-04-13 11:40:46 -0400

    #include <sys/times.h> added; this won't hurt

--- vcl/vcl_ctime.h
+++ vcl/vcl_ctime.h
@@ -1,8 +1,5 @@
 #ifndef vcl_ctime_h_
 #define vcl_ctime_h_
-#ifdef __GNUC__
-#pragma interface
-#endif
 /*
   fsm@robots.ox.ac.uk
 */
@@ -10,5 +7,9 @@
 // Purpose: provide access to the <time.h> standard C header.
 
 #include <time.h>
+
+#ifndef WIN32
+#include <sys/times.h>
+#endif
 
 #endif
