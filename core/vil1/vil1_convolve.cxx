628e2f699b919e2d1825a4f30f9785f21f892ad2
Unknown <targetjr>
2000-04-17 05:02:43 -0400

    vil_convolve.cxx does not generate any code

--- core/vil1/vil1_convolve.cxx
+++
@@ -1,9 +0,0 @@
-/*
-  fsm@robots.ox.ac.uk
-*/
-#ifdef __GNUC__
-#pragma implementation
-#endif
-#include "vil_convolve.h"
-
-//--------------------------------------------------------------------------------
