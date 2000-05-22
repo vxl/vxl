86ecbc37588806fdff8e453305a54ecdf32885af
Unknown <targetjr>
2000-05-22 11:54:25 -0400

    somewhat more diagnostic output and comments

--- core/vnl/tests/test_sparse_matrix.cxx
+++ core/vnl/tests/test_sparse_matrix.cxx
@@ -108,17 +108,13 @@ int doTest3()
   const unsigned int n = 20;
 
   vnl_sparse_matrix<double> ms(n,n);
-  vnl_matrix<double> md(n,n);
-  md = 0.0;
+  vnl_matrix<double> md(n,n); md = 0.0; // Initialise to all zeros
+  // The matrix must be symmetric
   for (unsigned i=0; i<n; i++) {
-    md(i,i) = i+1.0;
-    md(i,(i+3)%n) = 1.0;
-    md(i,(i+n-3)%n) = 1.0;
-    ms(i,i) = i+1.0;
-    ms(i,(i+3)%n) = 1.0;
-    ms(i,(i+n-3)%n) = 1.0;
-    //       md(i,i) = 1.0*(i+1)*(i+1);
-    //       ms(i,i) = 1.0*(i+1)*(i+1);
+    ms(i,i)         = md(i,i)         = i+1.0;
+    ms(i,(i+3)%n)   = md(i,(i+3)%n)   = 1.0;
+    ms(i,(i+n-3)%n) = md(i,(i+n-3)%n) = 1.0;
+    // ms(i,i) = md(i,i) = 1.0*(i+1)*(i+1);
   }
 
   cout << "ms:" << endl;
@@ -132,13 +128,16 @@ int doTest3()
   const unsigned int nvals = 2;
   vnl_symmetric_eigensystem<double> ed(md);
   vnl_sparse_symmetric_eigensystem es;
-  es.CalculateNPairs(ms,nvals,true,20);
+  if (0 == es.CalculateNPairs(ms,nvals,true,20))
+    cout << "vnl_sparse_symmetric_eigensystem::CalculateNPairs() succeeded\n";
+  else
+    cout << "vnl_sparse_symmetric_eigensystem::CalculateNPairs() failed\n";
   
   // Report 'em.
   for (unsigned i=0; i<nvals; i++) {
-    cout << "Dense : " << ed.D(i,i) << " -> " 
+    cout << "Dense[" << i << "] : " << ed.D(i,i) << " -> " 
 	 << ed.get_eigenvector(i) << endl;
-    cout << "Sparse: " << es.get_eigenvalue(i) << " -> " 
+    cout << "Sparse[" << i << "]: " << es.get_eigenvalue(i) << " -> " 
 	 << es.get_eigenvector(i) << endl;
   }
 
@@ -150,30 +149,29 @@ int doTest4()
   const unsigned int n = 20;
 
   vnl_sparse_matrix<double> ms(n,n);
-  vnl_matrix<double> md(n,n);
-  md = 0.0;
+  vnl_matrix<double> md(n,n); md = 0.0; // Initialise to all zeros
+  // The matrix must be symmetric
   for (unsigned i=0; i<n; i++) {
-    md(i,i) = i+1.0;
-    md(i,(i+3)%n) = 1.0;
-    md(i,(i+n-3)%n) = 1.0;
-    ms(i,i) = i+1.0;
-    ms(i,(i+3)%n) = 1.0;
-    ms(i,(i+n-3)%n) = 1.0;
-    //       md(i,i) = 1.0*(i+1)*(i+1);
-    //       ms(i,i) = 1.0*(i+1)*(i+1);
+    ms(i,i)         = md(i,i)         = i+1.0;
+    ms(i,(i+3)%n)   = md(i,(i+3)%n)   = 1.0;
+    ms(i,(i+n-3)%n) = md(i,(i+n-3)%n) = 1.0;
+    // ms(i,i) = md(i,i) = 1.0*(i+1)*(i+1);
   }
   
   const unsigned int nvals = 3;
   vnl_symmetric_eigensystem<double> ed(md);
   vnl_sparse_symmetric_eigensystem es;
-  es.CalculateNPairs(ms,nvals);
+  if (0 == es.CalculateNPairs(ms,nvals))
+    cout << "vnl_sparse_symmetric_eigensystem::CalculateNPairs() succeeded\n";
+  else
+    cout << "vnl_sparse_symmetric_eigensystem::CalculateNPairs() failed\n";
 
   // Report 'em.
   for (unsigned i=0; i<nvals; i++) {
     double dense = ed.D(i,i);
     double sparse = es.get_eigenvalue(i);
-    cout << "Dense : " << dense << endl;
-    cout << "Sparse: " << sparse << endl;
+    cout << "Dense[" << i << "] : " << dense << endl;
+    cout << "Sparse[" << i << "]: " << sparse << endl;
     double err = fabs(dense - sparse);
     cout << "Error: " << err << endl;
     Assert("vnl_sparse_symmetric_eigensystem eigenvalue error", err < 1e-10);
@@ -186,9 +184,13 @@ int doTest4()
 extern "C"
 int test_sparse_matrix()
 {
+  cout << "Starting test 1\n";
   int r = doTest1();
+  cout << "Starting test 2\n";
   r = r + doTest2();
+  cout << "Starting test 3\n";
   r = r + doTest3();
+  cout << "Starting test 4\n";
   r = r + doTest4();
   return r;
 }
