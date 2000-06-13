80fbd0509c33d9b3aa60985d2aeb91e8de60d58a
Unknown <remcvs_targetjr_grumpie>
2000-06-13 15:40:25 -0400

    temporary fix

--- vcl/Templates/vcl_list+ulong-.cxx
+++ vcl/Templates/vcl_list+ulong-.cxx
@@ -3,4 +3,6 @@
 
 VCL_LIST_INSTANTIATE(unsigned long);
 VCL_FIND_INSTANTIATE(vcl_list<unsigned long>::iterator, unsigned long);
+#ifdef VCL_REMOVE_INSTANTIATE
 VCL_REMOVE_INSTANTIATE(vcl_list<unsigned long>::iterator, unsigned long);
+#endif
