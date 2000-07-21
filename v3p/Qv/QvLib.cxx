8db521a011a84859a0cc5d2abf973d7ac5f90248
Unknown <remcvs_tgtjr_oxford>
2000-07-21 13:24:48 -0400

    .cpp -> .cxx

---
+++ v3p/Qv/QvLib.cxx
@@ -0,0 +1,115 @@
+// mpichler, 19950801
+#ifdef PMAX
+enum Part { goofyPart };        // confused about QvCone::Part and QvCylinder::Part
+enum Binding { goofyBinding };  // confused about QvMaterialBinding/QvNormalBinding::Binding
+#endif
+
+// RWMC: Had to define _strdup for linux since __PC__ set.  Hope this
+// doesn't break other systems.
+#include <vcl/vcl_cmath.h>
+#include "QvBasic.h"
+
+// mpichler, 19950802
+#ifndef WIN32
+#if defined(PMAX) || defined(PMAX_GNU) || defined(__PC__)
+#  include <vcl/vcl_cstring.h>
+#  include <malloc.h>
+char* strdup (const char* s)  // strdup not contained in system libraries
+{
+  char* p = (char*) malloc (strlen (s) + 1);
+  if (p)
+    strcpy (p, s);
+  return p;
+}
+#endif
+#endif
+
+// changed by Gerbert Orasche 260695
+#ifdef __PC__
+#pragma warning(disable:4244)
+#endif
+
+// mpichler, 19951109
+// definition of a suffix would be sufficient for compilers,
+// but UNIX' makedepend does not check this
+// e.g. #define CPPSUFFIX ".C"
+// #include "QvXXX" CPPSUFFIX
+// gorasche, 19960229
+// VC++ now rejects them too, so we're back to the old, large ifdef
+
+#define VRMLEXTENSIONS
+
+# include "QvAsciiText.cxx"
+# include "QvChildList.cxx"
+# include "QvCone.cxx"
+# include "QvCoordinate3.cxx"
+# include "QvCube.cxx"
+# include "QvCylinder.cxx"
+# include "QvDB.cxx"
+# include "QvDebugError.cxx"
+# include "QvDict.cxx"
+# include "QvDirectionalLight.cxx"
+# include "QvElement.cxx"
+# include "QvField.cxx"
+# include "QvFieldData.cxx"
+# include "QvFontStyle.cxx"
+# include "QvGroup.cxx"
+# include "QvIndexedFaceSet.cxx"
+# include "QvIndexedLineSet.cxx"
+# include "QvInfo.cxx"
+# include "QvInput.cxx"
+# include "QvLists.cxx"
+# include "QvLOD.cxx"
+# include "QvMFColor.cxx"
+# include "QvMFFloat.cxx"
+# include "QvMFLong.cxx"
+# include "QvMFString.cxx"
+# include "QvMFVec2f.cxx"
+# include "QvMFVec3f.cxx"
+# include "QvMaterial.cxx"
+# include "QvMaterialBinding.cxx"
+# include "QvMatrixTransform.cxx"
+# include "QvName.cxx"
+# include "QvNode.cxx"
+# include "QvNormal.cxx"
+# include "QvNormalBinding.cxx"
+# include "QvOrthographicCamera.cxx"
+# include "QvPList.cxx"
+# include "QvPerspectiveCamera.cxx"
+# include "QvPointLight.cxx"
+# include "QvPointSet.cxx"
+# include "QvReadError.cxx"
+# include "QvRotation.cxx"
+# include "QvSFBitMask.cxx"
+# include "QvSFBool.cxx"
+# include "QvSFColor.cxx"
+# include "QvSFEnum.cxx"
+# include "QvSFFloat.cxx"
+# include "QvSFImage.cxx"
+# include "QvSFLong.cxx"
+# include "QvSFMatrix.cxx"
+# include "QvSFRotation.cxx"
+# include "QvSFString.cxx"
+# include "QvSFVec2f.cxx"
+# include "QvSFVec3f.cxx"
+# include "QvScale.cxx"
+# include "QvSeparator.cxx"
+# include "QvShapeHints.cxx"
+# include "QvSphere.cxx"
+# include "QvSpotLight.cxx"
+# include "QvState.cxx"
+# include "QvString.cxx"
+# include "QvSwitch.cxx"
+# include "QvTexture2.cxx"
+# include "QvTexture2Transform.cxx"
+# include "QvTextureCoordinate2.cxx"
+# include "QvTransform.cxx"
+# include "QvTransformSeparator.cxx"
+# include "QvTranslation.cxx"
+# include "QvUnknownNode.cxx"
+# include "QvWWWAnchor.cxx"
+# include "QvWWWInline.cxx"
+
+# ifdef VRMLEXTENSIONS
+#  include "QvExtensions.cxx"
+# endif
