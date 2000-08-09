1b5618730131b9e35541066df443214d4d8d221b
Unknown <remcvs_tgtjr_oxford>
2000-08-09 04:54:15 -0400

    update header

--- contrib/oxl/mvl/HomgOperator2D.cxx
+++ contrib/oxl/mvl/HomgOperator2D.cxx
@@ -12,10 +12,10 @@
 #include <vcl/vcl_cassert.h>
 #include <vcl/vcl_iostream.h>
 #include <vcl/vcl_vector.h>
-#include <vnl/vnl_matrix.h>
 
 #include <vnl/vnl_math.h>
-#include <vnl/vnl_scatter_3x3.h>
+#include <vnl/vnl_matrix.h>
+#include <vnl/algo/vnl_scatter_3x3.h>
 
 #include <mvl/HomgLine2D.h>
 #include <mvl/HomgPoint2D.h>
