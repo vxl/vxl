cfbd31e2b4b5566c469c5ab8b4cc95a7d55d284a
Unknown <targetjr>
2000-08-10 06:26:02 -0400

    library path for tiff, jpeg and netlib is now v3p

--- core/iulibs.mk
+++ core/iulibs.mk
@@ -1,26 +1,14 @@
 IUE_PACKAGE_LIBRARIES += -lvsl
 
-IUE_PACKAGE_LIBRARIES += -lvgl-algo
-IUE_PACKAGE_LIBRARIES += -lvgl
+IUE_PACKAGE_LIBRARIES += -lvgl-algo -lvgl
 
 #IUE_PACKAGE_LIBRARIES += -lvil-algo
 IUE_PACKAGE_LIBRARIES += -lvil
 
-# For libTIFF.so, and libJPEG.so we may need these in the future.
-# At present they serve no purpose and confuse the SGI linker.
-# fsm
-# LIBDIRS += $(wildcard $(IUELOCALROOT_libbase)/v3p/tiff/$(tj_libdir))
-# LIBDIRS += $(wildcard $(IUELOCALROOT_libbase)/v3p/jpeg/$(tj_libdir))
-# LIBDIRS += $(wildcard $(IUELOCALROOT_libbase)/v3p/netlib/$(tj_libdir))
+# For libTIFF.so libJPEG.so and libnetlib.so
+LIBDIRS += $(wildcard $(IUELOCALROOT_libbase)/v3p/$(tj_libdir))
 
-IUE_PACKAGE_LIBRARIES += -lvnl-algo
-
-# A library path is needed here.
-# LIBDIRS += $(IUELOCALROOT_libbase)/v3p/netlib/$(tj_libdir)
-LIBDIRS += $(IUELOCALROOT_libbase)/Numerics/$(tj_libdir)
-IUE_PACKAGE_LIBRARIES += -lnetlib
-
-IUE_PACKAGE_LIBRARIES += -lvnl
+IUE_PACKAGE_LIBRARIES += -lvnl-algo -lvnl -lnetlib
 
 IUE_PACKAGE_LIBRARIES += -lvbl
 
