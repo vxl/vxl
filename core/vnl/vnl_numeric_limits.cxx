b547a3095a2678af47d07176050b3fe215e45895
Unknown <remcvs_tgtjr_oxford>
2000-08-13 09:33:16 -0400

    collapse many headers into one

--- core/vnl/vnl_numeric_limits.cxx
+++ core/vnl/vnl_numeric_limits.cxx
@@ -1,8 +1,5 @@
 #ifdef __GNUC__
-#pragma implementation "vnl_numeric_limits.h"
-#pragma implementation "vnl_numeric_limits_float.h"
-#pragma implementation "vnl_numeric_limits_int.h"
-#pragma implementation "vnl_numeric_limits_double.h"
+#pragma implementation "vnl_numeric_limits"
 #endif
 //
 // Class: numeric_limits
@@ -13,14 +10,6 @@
 //-----------------------------------------------------------------------------
 
 #include "vnl_numeric_limits.h"
-
-// // FIXME: sunpro5.0 warns about anachronistic syntax without the
-// // template <> f, but 2.95 and egcs emit error messages. - fsm
-// #include <vcl/vcl_compiler.h>
-// #if defined(VCL_GCC_295) || defined(VCL_EGCS)
-// # undef VCL_DEFINE_SPECIALIZATION
-// # define VCL_DEFINE_SPECIALIZATION(f) /* template <> f; */
-// #endif
 
 union vnl_numeric_limits_double_nan {
   double nan;
@@ -119,50 +108,19 @@ float vnl_numeric_limits<float>::signaling_NaN()
   return quiet_NaN();
 }
 
-#if defined(VCL_GCC)
-# if !defined(VCL_GCC_EGCS)
-template class vnl_numeric_limits<float>;
-template class vnl_numeric_limits<double>;
-# endif
-#else
-// #pragma instantiate vnl_numeric_limits<float>
-// #pragma instantiate vnl_numeric_limits<double>
-
-// Double inits
-const bool vnl_numeric_limits<double>::is_specialized = true;
-const int vnl_numeric_limits<double>::digits   = 53;
-const int vnl_numeric_limits<double>::digits10 =  15;
-const bool vnl_numeric_limits<double>::is_signed  = true;
-const bool vnl_numeric_limits<double>::is_integer = false;
-const bool vnl_numeric_limits<double>::is_exact   = false;
-const int vnl_numeric_limits<double>::radix = 2;
-const int vnl_numeric_limits<double>::min_exponent   = -1021;
-const int vnl_numeric_limits<double>::min_exponent10 = -307;
-const int vnl_numeric_limits<double>::max_exponent   = 1024;
-const int vnl_numeric_limits<double>::max_exponent10 = 308;
-const bool vnl_numeric_limits<double>::has_infinity      = true;
-const bool vnl_numeric_limits<double>::has_quiet_NaN     = true;
-const bool vnl_numeric_limits<double>::has_signaling_NaN = true;
-const bool vnl_numeric_limits<double>::has_denorm        = true;
-const bool vnl_numeric_limits<double>::is_iec559  = true;
-const bool vnl_numeric_limits<double>::is_bounded = true;
-const bool vnl_numeric_limits<double>::is_modulo  = false;
-const bool vnl_numeric_limits<double>::traps      = true;
-const bool vnl_numeric_limits<double>::tinyness_before = true;
-const vnl_float_round_style vnl_numeric_limits<double>::round_style = vnl_round_to_nearest;
-
-// Float inits
+#if VCL_IMPLEMENT_STATIC_CONSTS
+// float
 const bool vnl_numeric_limits<float>::is_specialized = true;
-const int vnl_numeric_limits<float>::digits   = 24;
-const int vnl_numeric_limits<float>::digits10 =  6;
+const int  vnl_numeric_limits<float>::digits   = 24;
+const int  vnl_numeric_limits<float>::digits10 =  6;
 const bool vnl_numeric_limits<float>::is_signed  = true;
 const bool vnl_numeric_limits<float>::is_integer = false;
 const bool vnl_numeric_limits<float>::is_exact   = false;
-const int vnl_numeric_limits<float>::radix = 2;
-const int vnl_numeric_limits<float>::min_exponent   = -125;
-const int vnl_numeric_limits<float>::min_exponent10 = -37;
-const int vnl_numeric_limits<float>::max_exponent   = 128;
-const int vnl_numeric_limits<float>::max_exponent10 = 38;
+const int  vnl_numeric_limits<float>::radix = 2;
+const int  vnl_numeric_limits<float>::min_exponent   = -125;
+const int  vnl_numeric_limits<float>::min_exponent10 = -37;
+const int  vnl_numeric_limits<float>::max_exponent   = 128;
+const int  vnl_numeric_limits<float>::max_exponent10 = 38;
 const bool vnl_numeric_limits<float>::has_infinity      = true;
 const bool vnl_numeric_limits<float>::has_quiet_NaN     = true;
 const bool vnl_numeric_limits<float>::has_signaling_NaN = true;
@@ -173,4 +131,26 @@ const bool vnl_numeric_limits<float>::is_modulo  = false;
 const bool vnl_numeric_limits<float>::traps      = true;
 const bool vnl_numeric_limits<float>::tinyness_before = true;
 const vnl_float_round_style vnl_numeric_limits<float>::round_style = vnl_round_to_nearest;
+// double
+const bool vnl_numeric_limits<double>::is_specialized = true;
+const int  vnl_numeric_limits<double>::digits   = 53;
+const int  vnl_numeric_limits<double>::digits10 =  15;
+const bool vnl_numeric_limits<double>::is_signed  = true;
+const bool vnl_numeric_limits<double>::is_integer = false;
+const bool vnl_numeric_limits<double>::is_exact   = false;
+const int  vnl_numeric_limits<double>::radix = 2;
+const int  vnl_numeric_limits<double>::min_exponent   = -1021;
+const int  vnl_numeric_limits<double>::min_exponent10 = -307;
+const int  vnl_numeric_limits<double>::max_exponent   = 1024;
+const int  vnl_numeric_limits<double>::max_exponent10 = 308;
+const bool vnl_numeric_limits<double>::has_infinity      = true;
+const bool vnl_numeric_limits<double>::has_quiet_NaN     = true;
+const bool vnl_numeric_limits<double>::has_signaling_NaN = true;
+const bool vnl_numeric_limits<double>::has_denorm        = true;
+const bool vnl_numeric_limits<double>::is_iec559  = true;
+const bool vnl_numeric_limits<double>::is_bounded = true;
+const bool vnl_numeric_limits<double>::is_modulo  = false;
+const bool vnl_numeric_limits<double>::traps      = true;
+const bool vnl_numeric_limits<double>::tinyness_before = true;
+const vnl_float_round_style vnl_numeric_limits<double>::round_style = vnl_round_to_nearest;
 #endif
--- core/vnl/vnl_numeric_limits.h
+++ core/vnl/vnl_numeric_limits.h
@@ -2,7 +2,7 @@
 #ifndef vnl_numeric_limits_h_
 #define vnl_numeric_limits_h_
 #ifdef __GNUC__
-#pragma interface
+#pragma interface "vnl_numeric_limits"
 #endif
 //
 // Class : numeric_limits
@@ -189,9 +189,124 @@ public:
   static const vnl_float_round_style round_style;
 };
 
-// Include specializations
-#include <vnl/vnl_numeric_limits_int.h>
-#include <vnl/vnl_numeric_limits_float.h>
-#include <vnl/vnl_numeric_limits_double.h>
+// SPECIALIZATIONS :
+
+// -------------------- #include <vnl/vnl_numeric_limits_int.h>
+
+#ifdef IUE_64_BIT
+# define vnl_tmp_signed32 ????
+#else
+# define vnl_tmp_signed32 int
+#endif
+
+VCL_DEFINE_SPECIALIZATION
+class vnl_numeric_limits<vnl_tmp_signed32> {
+public:
+  static const bool is_specialized VCL_STATIC_CONST_INIT_INT(true);
+  inline static int min() { return -0x7fffffff; }
+  inline static int max() { return  0x7fffffff; }
+  static const int digits   VCL_STATIC_CONST_INIT_INT(31);
+  static const int digits10 VCL_STATIC_CONST_INIT_INT(9);
+  static const bool is_signed  VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_integer VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_exact   VCL_STATIC_CONST_INIT_INT(true);
+  static const int radix VCL_STATIC_CONST_INIT_INT(2);
+  inline static int epsilon()     { return 0; }
+  inline static int round_error() { return 0; }
+  static const int min_exponent   VCL_STATIC_CONST_INIT_INT(-31);
+  static const int min_exponent10 VCL_STATIC_CONST_INIT_INT(-9);
+  static const int max_exponent   VCL_STATIC_CONST_INIT_INT(31);
+  static const int max_exponent10 VCL_STATIC_CONST_INIT_INT(9);
+  static const bool has_infinity      VCL_STATIC_CONST_INIT_INT(false);
+  static const bool has_quiet_NaN     VCL_STATIC_CONST_INIT_INT(false);
+  static const bool has_signaling_NaN VCL_STATIC_CONST_INIT_INT(false);
+  static const bool has_denorm        VCL_STATIC_CONST_INIT_INT(false);
+  static int infinity() { return max(); }
+  static int quiet_NaN();
+  static int signaling_NaN();
+  inline static int denorm_min()    { return min(); }
+  static const bool is_iec559  VCL_STATIC_CONST_INIT_INT(false);
+  static const bool is_bounded VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_modulo  VCL_STATIC_CONST_INIT_INT(true);
+  static const bool traps      VCL_STATIC_CONST_INIT_INT(false);
+  static const bool tinyness_before VCL_STATIC_CONST_INIT_INT(false);
+  static const vnl_float_round_style round_style VCL_STATIC_CONST_INIT_INT(vnl_round_toward_zero);
+};
+
+#undef vnl_tmp_signed32
+
+// -------------------- #include <vnl/vnl_numeric_limits_float.h>
+
+// IEEE 754 single precision
+VCL_DEFINE_SPECIALIZATION
+class vnl_numeric_limits<float> {
+public:
+  static const bool is_specialized VCL_STATIC_CONST_INIT_INT(true);
+  inline static float min() { return 1.17549435E-38F; }
+  inline static float max() { return 3.40282347E+38F; }
+  static const int digits   VCL_STATIC_CONST_INIT_INT(24);
+  static const int digits10 VCL_STATIC_CONST_INIT_INT(6);
+  static const bool is_signed  VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_integer VCL_STATIC_CONST_INIT_INT(false);
+  static const bool is_exact   VCL_STATIC_CONST_INIT_INT(false);
+  static const int radix VCL_STATIC_CONST_INIT_INT(2);
+  inline static float epsilon()     { return 1.19209290E-07F; }
+  inline static float round_error() { return 0.5F; }
+  static const int min_exponent   VCL_STATIC_CONST_INIT_INT(-125);
+  static const int min_exponent10 VCL_STATIC_CONST_INIT_INT(-37);
+  static const int max_exponent   VCL_STATIC_CONST_INIT_INT(128);
+  static const int max_exponent10 VCL_STATIC_CONST_INIT_INT(38);
+  static const bool has_infinity      VCL_STATIC_CONST_INIT_INT(true);
+  static const bool has_quiet_NaN     VCL_STATIC_CONST_INIT_INT(true);
+  static const bool has_signaling_NaN VCL_STATIC_CONST_INIT_INT(true);
+  static const bool has_denorm        VCL_STATIC_CONST_INIT_INT(false);
+  static float infinity();
+  static float quiet_NaN();
+  static float signaling_NaN();
+  inline static float denorm_min()    { return min(); }
+  static const bool is_iec559  VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_bounded VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_modulo  VCL_STATIC_CONST_INIT_INT(false);
+  static const bool traps      VCL_STATIC_CONST_INIT_INT(true);
+  static const bool tinyness_before VCL_STATIC_CONST_INIT_INT(true);
+  static const vnl_float_round_style round_style VCL_STATIC_CONST_INIT_INT(vnl_round_to_nearest);
+};
+
+// -------------------- #include <vnl/vnl_numeric_limits_double.h>
+
+// IEEE 754 double precision with denorm
+VCL_DEFINE_SPECIALIZATION
+class vnl_numeric_limits<double> {
+public:
+  static const bool is_specialized VCL_STATIC_CONST_INIT_INT(true);
+  inline static double min() { return 2.2250738585072014e-308; }
+  inline static double max() { return 1.7976931348623157e+308; }
+  static const int digits   VCL_STATIC_CONST_INIT_INT(53);
+  static const int digits10 VCL_STATIC_CONST_INIT_INT(15);
+  static const bool is_signed  VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_integer VCL_STATIC_CONST_INIT_INT(false);
+  static const bool is_exact   VCL_STATIC_CONST_INIT_INT(false);
+  static const int radix VCL_STATIC_CONST_INIT_INT(2);
+  inline static double epsilon()     { return 2.220446049250313e-16; }
+  inline static double round_error() { return 0.5; }
+  static const int min_exponent   VCL_STATIC_CONST_INIT_INT(-1021);
+  static const int min_exponent10 VCL_STATIC_CONST_INIT_INT(-307);
+  static const int max_exponent   VCL_STATIC_CONST_INIT_INT(1024);
+  static const int max_exponent10 VCL_STATIC_CONST_INIT_INT(308);
+  static const bool has_infinity      VCL_STATIC_CONST_INIT_INT(true);
+  static const bool has_quiet_NaN     VCL_STATIC_CONST_INIT_INT(true);
+  static const bool has_signaling_NaN VCL_STATIC_CONST_INIT_INT(true);
+  static const bool has_denorm        VCL_STATIC_CONST_INIT_INT(false);
+  static double infinity();
+  static double quiet_NaN();
+  static double signaling_NaN();
+  inline static double denorm_min() { return /* 5e-324 */ min(); }
+  static const bool is_iec559  VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_bounded VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_modulo  VCL_STATIC_CONST_INIT_INT(false);
+  static const bool traps      VCL_STATIC_CONST_INIT_INT(true);
+  static const bool tinyness_before VCL_STATIC_CONST_INIT_INT(true);
+  static const vnl_float_round_style round_style VCL_STATIC_CONST_INIT_INT(vnl_round_to_nearest);
+};
 
 #endif // vnl_numeric_limits_h_
