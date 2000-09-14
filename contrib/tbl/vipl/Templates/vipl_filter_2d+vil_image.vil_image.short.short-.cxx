4c2faa0144302146b081bf7fd0e2dba3a10af4ea
Unknown <targetjr>
2000-09-14 04:54:55 -0400

    *** empty log message ***

---
+++ contrib/tbl/vipl/Templates/vipl_filter_2d+vil_image.vil_image.short.short-.cxx
@@ -0,0 +1,22 @@
+#include <vil/vil_image.h>
+
+#define FILTER_IMPTR_DEC_REFCOUNT(v) ((v)=0)
+#define FILTER_IMPTR_INC_REFCOUNT(v) 
+#define FILTER_KERNPTR_DEC_REFCOUNT(v) 
+
+// this must be here for filter-2d to work
+#include <vipl/section/vipl_section_container.h>
+#include <vipl/section/vipl_section_iterator.h>
+vipl_section_container<short>*
+vipl_filterable_section_container_generator(const vil_image& im, short*);
+
+#include <vcl/vcl_compiler.h> // for the definition of VCL_DFL_TMPL_ARG
+
+#include <vipl/filter/vipl_filter.h>
+#include <vipl/filter/vipl_filter.txx>
+template class vipl_filter<vil_image, vil_image, short, short, 2 VCL_DFL_TMPL_ARG(vipl_trivial_pixeliter)>;
+
+#include <vipl/filter/vipl_filter_2d.h>
+#include <vipl/filter/vipl_filter_2d.txx>
+template class vipl_filter_2d<vil_image, vil_image, short, short VCL_DFL_TMPL_ARG(vipl_trivial_pixeliter)>;
+
