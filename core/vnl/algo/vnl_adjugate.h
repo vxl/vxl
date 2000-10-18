7d46186c3b4ffd8707dc6eefa435f231ee113082
Unknown <remcvs_tgtjr_oxford>
2000-10-18 06:39:55 -0400

    adj

---
+++ core/vnl/algo/vnl_adjugate.h
@@ -0,0 +1,21 @@
+#ifndef vnl_adjugate_h_
+#define vnl_adjugate_h_
+#ifdef __GNUC__
+#pragma interface "vnl_adjugate"
+#endif
+/*
+  fsm@robots.ox.ac.uk
+*/
+
+template <class T> class vnl_matrix;
+
+// By definition, the product of A and its adjugate
+// is det(A) [times an identity matrix].
+
+template <class T>
+void vnl_adjugate(vnl_matrix<T> const &A, vnl_matrix<T> *out);
+
+template <class T>
+vnl_matrix<T> vnl_adjugate(vnl_matrix<T> const &A);
+
+#endif
