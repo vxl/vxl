da9190220e9a14146ee35d81ee7def2feb79c63a
Unknown <remcvs_tgtjr_oxford>
2000-10-23 10:08:09 -0400

    uniformize

--- core/vil1/vil1_file_format.cxx
+++ core/vil1/vil1_file_format.cxx
@@ -8,10 +8,28 @@ vil_file_format::~vil_file_format()
 {
 }
 
+#define HAS_PNM  1
+#define HAS_VIFF 1
+#define HAS_IRIS 1
+#define HAS_MIT  1
+#define HAS_BMP  1
+#define HAS_GIF  0
+
+#if HAS_PNM
 #include <vil/file_formats/vil_pnm.h>
+#endif
+
+#if HAS_IRIS
 #include <vil/file_formats/vil_iris.h>
+#endif
+
+#if HAS_MIT
 #include <vil/file_formats/vil_mit.h>
+#endif
+
+#if HAS_VIFF
 #include <vil/file_formats/vil_viff.h>
+#endif
 
 #if HAS_PNG
 #include <vil/file_formats/vil_png.h>
@@ -25,9 +43,13 @@ vil_file_format::~vil_file_format()
 #include <vil/file_formats/vil_tiff.h>
 #endif
 
+#if HAS_BMP
 #include <vil/file_formats/vil_bmp.h>
-//#include <vil/file_formats/vil_gif.h>
+#endif
 
+#if HAS_GIF
+#include <vil/file_formats/vil_gif.h>
+#endif
 
 
 static vil_file_format** storage = 0;
@@ -46,12 +68,24 @@ vil_file_format** vil_file_format::all()
 #if HAS_TIFF
     storage[c++] = new vil_tiff_file_format;
 #endif
+#if HAS_PNM
     storage[c++] = new vil_pnm_file_format;
+#endif
+#if HAS_IRIS
     storage[c++] = new vil_iris_file_format;
+#endif
+#if HAS_MIT
     storage[c++] = new vil_mit_file_format;
+#endif
+#if HAS_VIFF
     storage[c++] = new vil_viff_file_format;
+#endif
+#if HAS_BMP
     storage[c++] = new vil_bmp_file_format;
-    //storage[c++] = new vil_gif_file_format;
+#endif
+#if HAS_GIF
+    storage[c++] = new vil_gif_file_format;
+#endif
 
     storage[c++] = 0;
   }
