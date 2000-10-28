80210e976b5dfc534c6549cfcf0d67f5b4cdc162
Unknown <remcvs_tgtjr_oxford>
2000-10-27 23:02:30 -0400

    remove redundant file. add new file.

---
+++ contrib/oxl/fmk_libdeps.mk
@@ -0,0 +1,10 @@
+#
+# fsm@robots.ox.ac.uk
+#
+
+fmk_libdeps += lib_mvox:lib_mvl
+fmk_libdeps += lib_oxp:lib_vbl,lib_vil,lib_vnl-algo
+fmk_libdeps += lib_mvl:lib_vnl-algo,lib_vgl,lib_vbl,lib_vil
+fmk_libdeps += lib_vrml:lib_vgl,lib_vnl,lib_vbl
+
+fmk_libdeps += lib_mvox,lib_oxp,lib_mvl,lib_vrml:pkg_oxl
