61e084ecbe26df80459d9ebb8f30e28fd1685eb0
Unknown <remcvs_tgtjr_oxford>
2000-10-27 23:03:38 -0400

    widen interface

--- core/vgl/vgl_window_scan_iterator.h
+++ core/vgl/vgl_window_scan_iterator.h
@@ -17,22 +17,37 @@ struct vgl_window_scan_iterator : vgl_region_scan_iterator
 {
   int x1, y1, x2, y2;
 
-  // Assumes x1<=x2, y1<=y2, so region is [x1, x2] x [y1, y2].
+  // [x1, x2] x [y1, y2]
+  void set_window_corners(float x1_, float y1_, float x2_, float y2_) {
+    x1 = (int) ceil (x1_);
+    y1 = (int) ceil (y1_);
+    x2 = (int) floor(x2_);
+    y2 = (int) floor(y2_);
+  }
+
+  // [x-w, x+w] x [y-h, y+h]
+  void set_window(float x, float y, float w, float h) 
+    { set_window_corners(x - w, y - w, x + w, y + w); }
+
+  // [x-r, x+r] x [y-r, y+r]
+  void set_window(float x, float y, float r) 
+    { set_window_corners(x - r, y - r, x + r, y + r); }
+
+
+  // makes uninitialized iterator.
+  vgl_window_scan_iterator() { }
+
+  // assumes x1<=x2, y1<=y2, so region is [x1, x2] x [y1, y2].
   vgl_window_scan_iterator(float x1_, float y1_, float x2_, float y2_)
-    : x1((int) ceil (x1_))
-    , y1((int) ceil (y1_))
-    , x2((int) floor(x2_))
-    , y2((int) floor(y2_)) { }
-
-  ~vgl_window_scan_iterator() { }
-
+    { set_window_corners(x1_, y1_, x2_, y2_); }
+  
   int current_y;
 
   void reset() { current_y = y1-1; }
-  bool next() { return (++current_y) <= y2; }
-  int  scany() const { return current_y; }
+  bool next () { return (++current_y) <= y2; }
+  int  scany () const { return current_y; }
   int  startx() const { return x1; }
-  int  endx() const { return x2; }
+  int  endx  () const { return x2; }
 };
 
 #endif // vgl_window_scan_iterator_h_
