f400329611d27e359e33bbe5e372e5605615c319
Unknown <remcvs_tgtjr_oxford>
2000-11-15 05:18:20 -0500

    missing SunPro instance

--- vcl/Templates/stream-instances.cxx
+++ vcl/Templates/stream-instances.cxx
@@ -42,4 +42,23 @@ template ostream & operator<<(ostream &, smanip<int> const &);
 #if defined(VCL_SUNPRO_CC_50)
 ostream &operator<<(ostream &s, short x)
 { return s << int(x); }
+
+
+
+
+# include <string>
+//void blah(std::istream &s1, std::string &s2) { s1 >> s2; }
+//
+//__type_0&__rwstd::rw_extract_string<std::basic_istream<char,std::char_traits<char> >,std::basic_string<char,std::char_traits<char>,std::allocator<char> >,std::char_traits<char> >(__type_0&,__type_1&,__type_2)
+
+#define type0 std::basic_istream<char,std::char_traits<char> >
+#define type1 std::basic_string<char,std::char_traits<char>,std::allocator<char> >
+#define type2 std::char_traits<char>
+
+template type0 & __rwstd::rw_extract_string<type0, type1, type2>(type0 &, type1 &, type2);
+
+#undef type0
+#undef type1
+#undef type2
+
 #endif
