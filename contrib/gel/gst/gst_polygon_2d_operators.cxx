44b0d70ebc3956785593029c522cadc2f881f010
Geoffrey Cross <geoffreycross@users.sourceforge.net>
2000-11-21 12:33:28 -0500

    *** empty log message ***

---
+++ contrib/gel/gst/gst_polygon_2d_operators.cxx
@@ -0,0 +1,82 @@
+/*
+  crossge@crd.ge.com
+*/
+#ifdef __GNUC__
+#pragma implementation "gst_polygon_2d_operators"
+#endif
+#include "gst_polygon_2d_operators.h"
+
+
+
+
+vcl_vector<gst_polygon_2d_ref> gst_make_polygons_2d( const vcl_vector<gst_edge_2d_ref> edges)
+{
+  // flags showing edges already used
+  vcl_vector<int> used( edges.size(), 0);
+  
+  // repository of polygons as they are created
+  vcl_vector<gst_polygon_2d_ref> polygons;
+
+  // start a polygon with each edge, and look for a closed cycle
+  //  hopefully using a NEW edge
+  for( int i=0; i< edges.size(); i++)
+    {
+      bool newface= false;
+      bool closed= false;
+      gst_polygon_2d_ref thispoly= new gst_polygon_2d;
+
+      // flags showing edges already used in this polygon
+      vcl_vector<int> pused( edges.size(), 0);
+
+      thispoly->add( edges[i]);
+
+      if( !used[i]) 
+	{
+	  newface= true;
+	}
+
+      used[i]= 1;
+      pused[i]= 1;
+
+      gst_vertex_2d_ref start= edges[i]->get_start();
+      gst_vertex_2d_ref end  = edges[i]->get_end();
+
+      // repeatedly look for the next edge in the cycle
+      //  until we do a complete pass without finding any further
+      //  edges
+      bool added= true;
+
+      while(( added) && ( !closed))
+	{
+	  added= false;
+
+	  for( int j=0; ((j< edges.size()) && (!closed)); j++)
+	    {
+	      if(( edges[j]->get_start().ptr()== end.ptr()) && ( !pused[j]))
+		{
+		  thispoly->add( edges[j]);
+		  added= true;
+
+		  end= edges[j]->get_end();
+		  
+		  if( !used[j]) newface= true;
+
+		  used[j]= 1;
+		  pused[j]= 1;
+		  
+		  if( end.ptr()== start.ptr())
+		    {
+		      closed= true;
+		    }
+		}
+	    }
+	}
+
+      if(( newface) && ( closed))
+	{
+	  polygons.push_back( thispoly);
+	}
+    }
+
+  return polygons;
+}
---
+++ contrib/gel/gst/gst_polygon_2d_operators.h
@@ -0,0 +1,23 @@
+#ifndef gst_polygon_2d_operators_h_
+#define gst_polygon_2d_operators_h_
+#ifdef __GNUC__
+#pragma interface "gst_polygon_2d_operators"
+#endif
+/*
+  crossge@crd.ge.com
+*/
+
+#include <vcl/vcl_vector.h>
+
+#include <gst/gst_edge_2d_ref.h>
+#include <gst/gst_polygon_2d_ref.h>
+
+// Turn a list of edges into a list of polygons.  This is not necessarily
+//  efficient, and not unique if the topology is complicated.  If there are
+//  shared edges, beware... it might be ok, though.  
+//
+//  The edges must be corrected oriented (which makes possible shared edges
+//  difficult).
+vcl_vector<gst_polygon_2d_ref> gst_make_polygons_2d( const vcl_vector<gst_edge_2d_ref> edges);
+
+#endif
