11a28a060322f93b032ba955a392cfd6759dd754
Unknown <targetjr>
2000-11-27 05:33:23 -0500

    include f2c.h after math.h to avoid compiler problems related to f2c's complex

--- v3p/netlib/cdabs.c
+++ v3p/netlib/cdabs.c
@@ -1,8 +1,8 @@
 /*
   fsm@robots.ox.ac.uk
 */
-#include "f2c.h"
 #include <math.h>
+#include "f2c.h"
 
 doublereal cdabs_(doublecomplex const *z) {
   return hypot(z->r, z->i); /* is this what cdabs_() is supposed to do? */
--- v3p/netlib/cdsqrt.c
+++ v3p/netlib/cdsqrt.c
@@ -1,8 +1,8 @@
 /*
   fsm@robots.ox.ac.uk
 */
-#include "f2c.h"
 #include <math.h>
+#include "f2c.h"
 
 doublereal cdsqrt_(doublecomplex const *z) {
   return hypot(z->r, z->i);
