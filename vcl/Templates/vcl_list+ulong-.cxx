7c0fec8c2062d9a4bc89751f1954e2acaa48312f
Unknown <targetjr>
2000-12-05 11:05:10 -0500

    simplified VCL_FIND_INSTANTIATE

--- vcl/Templates/vcl_list+ulong-.cxx
+++ vcl/Templates/vcl_list+ulong-.cxx
@@ -2,7 +2,7 @@
 #include <vcl/vcl_algorithm.txx>
 
 VCL_LIST_INSTANTIATE(unsigned long);
-VCL_FIND_INSTANTIATE_ITER(vcl_list<unsigned long>::iterator, unsigned long);
+VCL_FIND_INSTANTIATE(vcl_list<unsigned long>::iterator, unsigned long);
 #ifdef VCL_REMOVE_INSTANTIATE
 VCL_REMOVE_INSTANTIATE(vcl_list<unsigned long>::iterator, unsigned long);
 #endif
