252d0cfbfd08f904402ea7bcfd99bdee29921ddb
Unknown <targetjr>
2000-12-05 11:04:51 -0500

    moved find(...bidirectional_iterator_tag) to gcc-295/vcl_algorithm.txx

--- vcl/Templates/vcl_list+int-.cxx
+++ vcl/Templates/vcl_list+int-.cxx
@@ -9,24 +9,7 @@ VCL_LIST_INSTANTIATE(int);
 VCL_COPY_INSTANTIATE(vcl_list<int>::const_iterator, vcl_ostream_iterator<int>);
 VCL_COPY_INSTANTIATE(vcl_list<int>::iterator, vcl_ostream_iterator<int>);
 
-#if defined(VCL_GCC_295) && !defined(GNU_LIBSTDCXX_V3)
-//fsm: the instantiation macro for find() needs to instantiate find(I, I, T, tag)
-//for noshared builds. however, there seems to be no way to get the iterator 
-//category of I other than using iterator_traits<I>::iterator_category. the 
-//problem then is that find() is only defined for input_iterators and random_access_iterators.
-//since a bidirectional_iterator_tag is an input_iterator, the following should
-//be harmless.
-//it could be moved into vcl_algorithm.txx
-template <class _BdIter, class _Tp>
-inline _BdIter find(_BdIter __first, _BdIter __last,
-                    _Tp const & __val,
-		    bidirectional_iterator_tag)
-{
-  return ::find(__first, __last, __val, input_iterator_tag());
-}
-#endif
-VCL_FIND_INSTANTIATE_ITER(vcl_list<int>::iterator, int);
-
+VCL_FIND_INSTANTIATE(vcl_list<int>::iterator, int);
 
 #if defined(VCL_GCC_295) && !defined(GNU_LIBSTDCXX_V3)
 //vxl_filter:skip
