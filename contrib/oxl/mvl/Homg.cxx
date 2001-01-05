456b9a8bc75a7fa2cd4a96d3bc908dfc7f65d157
Unknown <remcvs_tgtjr_oxford>
2001-01-05 06:32:16 -0500

    fix for sunpro

--- contrib/oxl/mvl/Homg.cxx
+++ contrib/oxl/mvl/Homg.cxx
@@ -2,8 +2,11 @@
 #pragma implementation
 #endif
 
+#include "Homg.h"
 #include <vcl/vcl_cmath.h>
-#include <mvl/Homg.h>
+#ifdef VCL_SUNPRO_CC_50
+# include <math.h> // no HUGE_VAL in <cmath>
+#endif
 
 double Homg::infinity = HUGE_VAL;
 double Homg::infinitesimal_tol = 1e-12;
