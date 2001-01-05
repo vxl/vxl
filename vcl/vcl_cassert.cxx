109b03230af75f469e0bb4015e80a05acbf210d9
Unknown <remcvs_tgtjr_oxford>
2001-01-05 07:12:26 -0500

    fix for sunpro and CVS gcc

--- vcl/vcl_cassert.cxx
+++ vcl/vcl_cassert.cxx
@@ -1,21 +1,32 @@
 /*
   fsm@robots.ox.ac.uk
 */
-#include "vcl_compiler.h"
+#include <vcl/vcl_compiler.h>
 
 #if defined(VCL_SUNPRO_CC) || defined(VCL_KAI)
 // fsm@robots: This is a silly hack to enable us to link code compiled with SunPro
 // against a library (such as libMesaGL) compiled with gcc. The gcc assert macro
 // uses a function call __eprintf() which is defined in gcc/libgcc2.c in the gcc
 // sources.
-#include <vcl_cstdio.h>
-#include <vcl_cstdlib.h>
+#include <vcl/vcl_cstdio.h>
+#include <vcl/vcl_cstdlib.h>
 extern "C" void
 __eprintf (char const *string, char const *expression,
 	   unsigned int line, char const *filename)
 {
-  fprintf (stderr, string, expression, line, filename);
-  fflush (stderr);
-  vcl_abort ();
+  std::fprintf (stderr, string, expression, line, filename);
+  std::fflush (stderr);
+  std::abort ();
+}
+#endif
+
+#ifdef GNU_LIBSTDCXX_V3
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+void vcl_cassert_failure(char const *FILE, int LINE, char const *expr)
+{
+  fprintf(stderr, "%s:%d assertion failure \'%s\'\n", FILE, LINE, expr);
+  abort();
 }
 #endif
--- vcl/vcl_cmath.cxx
+++ vcl/vcl_cmath.cxx
@@ -21,17 +21,17 @@ double tickle_cmath_inlines()
 // they are not provided in the run-time library.
 // fsm
 namespace std {
-  float atan2(float y, float x) { return float(::atan2(double(y), double(x))); }
-  float sqrt(float x) { return float(::sqrt(double(x))); }
-  double abs(double x) { return ::fabs(x); }
-  float ceil(float x) { return float(::ceil(double(x))); }
-  float floor(float x) { return float(::floor(double(x))); }
-  float sin(float x) { return float(::sin(double(x))); }
-  float fabs(float x) { return float(::fabs(double(x))); }
-  float cos(float x) { return float(::cos(double(x))); }
-  float pow(float x, float a) { return float(::pow(double(x), double(a))); }
-  float exp(float x) { return float(::exp(double(x))); }
-  float log(float x) { return float(::log(double(x))); }
-  float acos(float x) { return float(::acos(double(x))); }
+  double abs(double x) { return x >= 0 ? x : -x; }
+  float atan2(float y, float x) { return float(std::atan2(double(y), double(x))); }
+  float sqrt(float x) { return float(std::sqrt(double(x))); }
+  float ceil(float x) { return float(std::ceil(double(x))); }
+  float floor(float x) { return float(std::floor(double(x))); }
+  float sin(float x) { return float(std::sin(double(x))); }
+  float fabs(float x) { return float(std::fabs(double(x))); }
+  float cos(float x) { return float(std::cos(double(x))); }
+  float pow(float x, float a) { return float(std::pow(double(x), double(a))); }
+  float exp(float x) { return float(std::exp(double(x))); }
+  float log(float x) { return float(std::log(double(x))); }
+  float acos(float x) { return float(std::acos(double(x))); }
 }
 #endif
