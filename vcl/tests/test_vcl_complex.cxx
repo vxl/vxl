f04009653e3c7547d64f91a57e4063c8811d4882
Unknown <targetjr>
2001-01-19 08:44:50 -0500

    use vcl_real(x) instead of x.real()

--- vcl/tests/test_vcl_complex.cxx
+++ vcl/tests/test_vcl_complex.cxx
@@ -4,7 +4,7 @@ vcl_complex<double> doublify(vcl_complex<float> const &);
 
 #include <vcl_complex.h>
 vcl_complex<double> doublify(vcl_complex<float> const &z) {
-  return vcl_complex<double>(z.real(), z.imag());
+  return vcl_complex<double>(vcl_real(z), vcl_imag(z));
 }
 
 #include <vcl_iostream.h>
@@ -17,8 +17,8 @@ int main()
   vcl_cout << dc1 << " + " << dc2 << " = " << (dc1+dc2) << vcl_endl;
   vcl_cout << fc1 << " + " << fc2 << " = " << (fc1+fc2) << vcl_endl;
 
-  vcl_complex<double> dc3(dc1.real(),dc2.imag());
-  vcl_complex<float> fc3(fc1.real(),fc2.imag());
+  vcl_complex<double> dc3(vcl_real(dc1),vcl_imag(dc2));
+  vcl_complex<float> fc3(vcl_real(fc1),vcl_imag(fc2));
   
   vcl_cout << dc3 << " / " << dc1 << " = " << dc3/dc1 << vcl_endl;
   vcl_cout << fc3 << " / " << fc1 << " = " << fc3/fc1 << vcl_endl;
