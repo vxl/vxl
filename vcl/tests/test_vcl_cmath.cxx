97e3a20d968102dd39f2981ba6c9f44520e73a3a
Unknown <remcvs_tgtjr_oxford>
2001-01-19 13:30:14 -0500

    dsync.pl: pedantry

--- vcl/tests/test_vcl_cmath.cxx
+++ vcl/tests/test_vcl_cmath.cxx
@@ -19,16 +19,32 @@ do { \
 
 int main()
 {
-  int    xi = 314159265;
-  long   xl = 314159265L;
-  float  xf = 13.14159265358979323846;
-  double xd = 23.14159265358979323846;
-  // + long double
+  {
+    int    xi = 314159265;
+    long   xl = 314159265L;
+    float  xf = 13.14159265358979323846;
+    double xd = 23.14159265358979323846;
+    // + long double
+    
+    macro(vcl_abs(- xi) == xi, int);
+    macro(vcl_abs(- xl) == xl, long);
+    macro(vcl_abs(- xf) == xf, float);
+    macro(vcl_abs(- xd) == xd, double);
+  }
   
-  macro(vcl_abs(- xi) == xi, int);
-  macro(vcl_abs(- xl) == xl, long);
-  macro(vcl_abs(- xf) == xf, float);
-  macro(vcl_abs(- xd) == xd, double);
+  {
+    // This shows why 
+    //   #define vcl_cos cos
+    // isn't good enough. It has to be
+    //   #define vcl_cos ::cos
+    // or
+    //   #define vcl_cos std::cos
+    double theta = 0.1234;
+    double cos = vcl_cos(theta);
+    double sin = vcl_sin(theta);
+    double tan = vcl_tan(theta);
+    theta = cos + sin + tan; // quell 'unused variable' warning.
+  }
   
   return 0;
 }
