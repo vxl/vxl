daa0dc473d9dcca1bff2149fb4c2468a223fb09a
Unknown <remcvs_tgtjr_oxford>
2001-02-01 11:56:20 -0500

    dsync.pl (oxford): misc changes

--- contrib/oxl/mvl/Templates/vcl_map+uint.FMatrix~-.cxx
+++ contrib/oxl/mvl/Templates/vcl_map+uint.FMatrix~-.cxx
@@ -1,6 +1,6 @@
 // Instantiation of vcl_map<uint.FMatrix*>
-#include <vcl/vcl_functional.h>
-#include <vcl/vcl_map.txx>
+#include <vcl_functional.h>
+#include <vcl_map.txx>
 #include <mvl/FMatrix.h>
 
 VCL_MAP_INSTANTIATE(unsigned int, FMatrix*, vcl_less<unsigned int>);
