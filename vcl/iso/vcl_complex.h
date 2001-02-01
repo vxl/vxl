389054acd6a2a4192816c57b657b138c289da3a2
Unknown <remcvs_tgtjr_oxford>
2001-02-01 11:36:56 -0500

    dsync.pl (oxford): *** empty log message ***

--- vcl/iso/vcl_complex.h
+++ vcl/iso/vcl_complex.h
@@ -1,83 +1,16 @@
 #ifndef vcl_iso_complex_h_
 #define vcl_iso_complex_h_
 
+// This is a generated file. DO NOT EDIT! Not even a little bit.
+
 #include <complex>
 
-// complex
-#ifndef vcl_complex
-#define vcl_complex std::complex
+#ifdef vcl_generic_complex_STD
+  ** error **
+#else
+# define vcl_generic_complex_STD std
 #endif
-// real
-#ifndef vcl_real
-#define vcl_real std::real
-#endif
-// imag
-#ifndef vcl_imag
-#define vcl_imag std::imag
-#endif
-// abs
-#ifndef vcl_abs
-#define vcl_abs std::abs
-#endif
-// arg
-#ifndef vcl_arg
-#define vcl_arg std::arg
-#endif
-// norm
-#ifndef vcl_norm
-#define vcl_norm std::norm
-#endif
-// conj
-#ifndef vcl_conj
-#define vcl_conj std::conj
-#endif
-// polar
-#ifndef vcl_polar
-#define vcl_polar std::polar
-#endif
-// cos
-#ifndef vcl_cos
-#define vcl_cos std::cos
-#endif
-// cosh
-#ifndef vcl_cosh
-#define vcl_cosh std::cosh
-#endif
-// exp
-#ifndef vcl_exp
-#define vcl_exp std::exp
-#endif
-// log
-#ifndef vcl_log
-#define vcl_log std::log
-#endif
-// log10
-#ifndef vcl_log10
-#define vcl_log10 std::log10
-#endif
-// pow
-#ifndef vcl_pow
-#define vcl_pow std::pow
-#endif
-// sin
-#ifndef vcl_sin
-#define vcl_sin std::sin
-#endif
-// sinh
-#ifndef vcl_sinh
-#define vcl_sinh std::sinh
-#endif
-// sqrt
-#ifndef vcl_sqrt
-#define vcl_sqrt std::sqrt
-#endif
-// tan
-#ifndef vcl_tan
-#define vcl_tan std::tan
-#endif
-// tanh
-#ifndef vcl_tanh
-#define vcl_tanh std::tanh
-#endif
+
+#include "../generic/vcl_complex.h"
 
 #endif // vcl_iso_complex_h_
--- vcl/iso/vcl_csetjmp.h
+++ vcl/iso/vcl_csetjmp.h
@@ -1,15 +1,16 @@
 #ifndef vcl_iso_csetjmp_h_
 #define vcl_iso_csetjmp_h_
 
+// This is a generated file. DO NOT EDIT! Not even a little bit.
+
 #include <csetjmp>
 
-// jmp_buf
-#ifndef vcl_jmp_buf
-#define vcl_jmp_buf std::jmp_buf
+#ifdef vcl_generic_csetjmp_STD
+  ** error **
+#else
+# define vcl_generic_csetjmp_STD std
 #endif
-// longjmp
-#ifndef vcl_longjmp
-#define vcl_longjmp std::longjmp
-#endif
+
+#include "../generic/vcl_csetjmp.h"
 
 #endif // vcl_iso_csetjmp_h_
