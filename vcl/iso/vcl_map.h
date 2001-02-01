b42647da660d0c0380b1e810f55d776b83db4d3b
Unknown <remcvs_tgtjr_oxford>
2001-02-01 11:27:12 -0500

    dsync.pl (oxford): *** empty log message ***

--- vcl/iso/vcl_map.h
+++ vcl/iso/vcl_map.h
@@ -1,19 +1,16 @@
 #ifndef vcl_iso_map_h_
 #define vcl_iso_map_h_
 
+// This is a generated file. DO NOT EDIT! Not even a little bit.
+
 #include <map>
 
-// map
-#ifndef vcl_map
-#define vcl_map std::map
+#ifdef vcl_generic_map_STD
+  ** error **
+#else
+# define vcl_generic_map_STD std
 #endif
-// multimap
-#ifndef vcl_multimap
-#define vcl_multimap std::multimap
-#endif
-// swap
-#ifndef vcl_swap
-#define vcl_swap std::swap
-#endif
+
+#include "../generic/vcl_map.h"
 
 #endif // vcl_iso_map_h_
