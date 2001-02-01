d9dd7f6d0cf769cad6e54454a092fe04d37afa62
Unknown <remcvs_tgtjr_oxford>
2001-02-01 11:36:16 -0500

    dsync.pl (oxford): misc changes

--- contrib/oxl/mvl/Templates/vcl_map+uint.ImageMetric~-.cxx
+++ contrib/oxl/mvl/Templates/vcl_map+uint.ImageMetric~-.cxx
@@ -1,6 +1,6 @@
 // Instantiation of vcl_map<uint.ImageMetric*>
-#include <vcl/vcl_functional.h>
-#include <vcl/vcl_map.txx>
+#include <vcl_functional.h>
+#include <vcl_map.txx>
 class ImageMetric;
 
 VCL_MAP_INSTANTIATE(unsigned int, ImageMetric*, vcl_less<unsigned int>);
