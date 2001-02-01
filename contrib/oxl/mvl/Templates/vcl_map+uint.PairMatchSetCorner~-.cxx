68f1178565a2fd1b9bf45f67e4180e0ce4dc3032
Unknown <remcvs_tgtjr_oxford>
2001-02-01 11:31:55 -0500

    dsync.pl (oxford): misc changes

--- contrib/oxl/mvl/Templates/vcl_map+uint.PairMatchSetCorner~-.cxx
+++ contrib/oxl/mvl/Templates/vcl_map+uint.PairMatchSetCorner~-.cxx
@@ -1,6 +1,6 @@
 // Instantiation of vcl_map<uint.PairMatchSetCorner*>
-#include <vcl/vcl_functional.h>
-#include <vcl/vcl_map.txx>
+#include <vcl_functional.h>
+#include <vcl_map.txx>
 #include <mvl/PairMatchSetCorner.h>
 
 VCL_MAP_INSTANTIATE(unsigned int, PairMatchSetCorner*, vcl_less<unsigned int>);
