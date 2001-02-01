f42a5a141d236b7ea2e73d2703c8c49e1f68af82
Unknown <remcvs_tgtjr_oxford>
2001-02-01 11:17:43 -0500

    dsync.pl (oxford): *** empty log message ***

--- vcl/iso/vcl_iomanip.h
+++ vcl/iso/vcl_iomanip.h
@@ -1,33 +1,16 @@
 #ifndef vcl_iso_iomanip_h_
 #define vcl_iso_iomanip_h_
 
+// This is a generated file. DO NOT EDIT! Not even a little bit.
+
 #include <iomanip>
 
-// resetiosflags
-#ifndef vcl_resetiosflags
-#define vcl_resetiosflags std::resetiosflags
-#endif
-// setiosflags
-#ifndef vcl_setiosflags
-#define vcl_setiosflags std::setiosflags
-#endif
-// setbase
-#ifndef vcl_setbase
-#define vcl_setbase std::setbase
-#endif
-// setfill
-#ifndef vcl_setfill
-#define vcl_setfill std::setfill
-#endif
-// setprecision
-#ifndef vcl_setprecision
-#define vcl_setprecision std::setprecision
-#endif
-// setw
-#ifndef vcl_setw
-#define vcl_setw std::setw
+#ifdef vcl_generic_iomanip_STD
+  ** error **
+#else
+# define vcl_generic_iomanip_STD std
 #endif
 
-using std::setw;
+#include "../generic/vcl_iomanip.h"
 
 #endif // vcl_iso_iomanip_h_
