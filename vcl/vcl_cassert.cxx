5bfd1266707e46224d3705546fac3507944cde7f
Unknown <remcvs_tgtjr_oxford>
2001-02-02 04:06:20 -0500

    messing about with namespaces

--- vcl/vcl_cassert.cxx
+++ vcl/vcl_cassert.cxx
@@ -8,25 +8,25 @@
 // against a library (such as libMesaGL) compiled with gcc. The gcc assert macro
 // uses a function call __eprintf() which is defined in gcc/libgcc2.c in the gcc
 // sources.
-#include "vcl_cstdio.h"
-#include "vcl_cstdlib.h"
+#include <cstdio>
+#include <cstdlib>
 extern "C" void
 __eprintf (char const *string, char const *expression,
 	   unsigned int line, char const *filename)
 {
-  std::fprintf (stderr, string, expression, line, filename);
-  std::fflush (stderr);
-  std::abort ();
+  std::fprintf(stderr, string, expression, line, filename);
+  std::fflush(stderr);
+  std::abort();
 }
 #endif
 
 #ifdef GNU_LIBSTDCXX_V3
 #include <cstdio>
 #include <cstdlib>
-using namespace std;
 void vcl_cassert_failure(char const *FILE, int LINE, char const *expr)
 {
-  fprintf(stderr, "%s:%d assertion failure \'%s\'\n", FILE, LINE, expr);
-  abort();
+  std::fprintf(stderr, "%s:%d assertion failure \'%s\'\n", FILE, LINE, expr);
+  std::fflush(stderr);
+  std::abort();
 }
 #endif
