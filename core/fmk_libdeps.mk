a65adc31c0cc9847724e4c776a19d19d80abfdb4
Unknown <remcvs_tgtjr_oxford>
2001-02-05 08:43:39 -0500

    mission accomplished

--- core/fmk_libdeps.mk
+++ core/fmk_libdeps.mk
@@ -27,18 +27,4 @@ fmk_libdeps += lib_netlib:lib_fmkc,lib_fmkm
 
 fmk_libdeps += lib_vsl:lib_vil,lib_vbl,lib_vnl-algo
 
-fmk_libdeps += lib_vgui:lib_vpl,lib_vbl,lib_vnl-algo,lib_vil,lib_vgl
-fmk_libdeps += lib_vgui:lib_glut
-ifeq (1,$(tpp_HASPKG_HERMES))
-fmk_libdeps += lib_vgui:lib_Hermes
-endif
-fmk_libdeps += lib_vgui-SDL:lib_vgui,lib_SDL
-fmk_libdeps += lib_vgui-Fl:lib_vgui,lib_fltk
-fmk_libdeps += lib_vgui-X:lib_vgui,lib_X11
-fmk_libdeps += lib_vgui-Xm:lib_vgui,lib_vgui-X
-fmk_libdeps += lib_vgui-glX:lib_vgui,lib_vgui-X
-fmk_libdeps += lib_vgui-glut:lib_vgui,lib_glut
-fmk_libdeps += lib_vgui-gtk:lib_vgui,lib_gtkgl
-fmk_libdeps += lib_vgui-qt:lib_vgui,lib_qt
-
 fmk_libdeps += lib_vpl,lib_vbl,lib_vtl,lib_vgl,lib_vil,lib_vnl:pkg_vxl
