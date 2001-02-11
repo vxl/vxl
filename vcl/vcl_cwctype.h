b5325b673e91c7ce5eace6fad068c15970d6563c
Unknown <remcvs_tgtjr_oxford>
2001-02-11 06:54:17 -0500

    dsync.pl (oxford): boring

--- vcl/vcl_cwctype.h
+++ vcl/vcl_cwctype.h
@@ -9,10 +9,14 @@
 
 #include "vcl_compiler.h"
 
-#ifndef hpux
-#include <wctype.h>
-#else // HP does not have /usr/include/wctype.h
-#include <wchar.h>
+#ifdef VCL_SGI_CC_720
+# include <wctype.h>
+#elif defined(hpux)
+// someone: HP does not have /usr/include/wctype.h
+// fsm: but the compiler might supply <cwctype>?
+# include <wchar.h>
+#else
+# include "iso/vcl_cwctype.h"
 #endif
 
 #endif // vcl_cwctype_h_
