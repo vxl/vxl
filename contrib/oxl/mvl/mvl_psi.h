647f96e409e05a8a165263495f5f6230074311e0
Unknown <remcvs_tgtjr_oxford>
2001-02-11 06:55:52 -0500

    dsync.pl (oxford): projective structure from 3 views of 6 points.

---
+++ contrib/oxl/mvl/mvl_psi.h
@@ -0,0 +1,35 @@
+#ifndef mvl_psi_h_
+#define mvl_psi_h_
+#ifdef __GNUC__
+#pragma interface "mvl_psi"
+#endif
+/*
+  fsm@robots.ox.ac.uk
+*/
+
+// You don't need to know what these functions are for.
+
+#define mvl_typename class
+
+template <mvl_typename U>
+U mvl_psi_constraint(U const abcde[5]);
+
+template <mvl_typename U>
+void mvl_psi_constraint_restrict(U const p[5], U const q[5], U coeffs[4]);
+
+template <mvl_typename U>
+void mvl_psi_apply(U const XYZT[4], U abcde[5]);
+
+template <mvl_typename U>
+void psi_invert_direct(U const abcde[5], U XYZT[4], int which);
+
+template <mvl_typename U>
+void psi_invert_direct(U const abcde[5], U XYZT[4]);
+
+template <mvl_typename U>
+void psi_invert_design(U const abcde[5], U XYZT[4]);
+
+template <mvl_typename U>
+void mvl_psi_invert(U const abcde[5], U XYZT[4]);
+
+#endif
---
+++ contrib/oxl/mvl/mvl_three_view_six_point_structure.h
@@ -0,0 +1,47 @@
+#ifndef mvl_three_view_six_point_structure_h_
+#define mvl_three_view_six_point_structure_h_
+#ifdef __GNUC__
+#pragma interface "mvl_three_view_six_point_structure"
+#endif
+/*
+  fsm@robots.ox.ac.uk
+*/
+
+// Code for computing projective structure from 3 views of 6 points.
+//
+// The six points are assumed to be:
+//
+//  [ 1 ] [ 0 ] [ 0 ] [ 0 ] [ 1 ]     [ X ]
+//  [ 0 ] [ 1 ] [ 0 ] [ 0 ] [ 1 ] and [ Y ] = Q
+//  [ 0 ] [ 0 ] [ 1 ] [ 0 ] [ 1 ]     [ Z ]
+//  [ 0 ] [ 0 ] [ 0 ] [ 1 ] [ 1 ]     [ T ]
+//
+// There are up to three solutions.
+
+#include <vnl/vnl_vector.h>
+#include <vnl/vnl_matrix.h>
+
+struct mvl_three_view_six_point_structure
+{
+  mvl_three_view_six_point_structure();
+  
+  // this flag is currently ignored.
+  bool verbose;
+  
+  // These matrices have one row per view and 6 columns.
+  // The ith view of the jth point is (u(i, j), v(i, j)).
+  vnl_matrix<double> u;
+  vnl_matrix<double> v;
+  
+  // Call this once you have filled in 'u' and 'v'.
+  bool compute();
+  
+  // output data.
+  struct solution_t {
+    bool valid;
+    vnl_matrix<double> P[3]; // camera matrices.
+    vnl_vector<double> Q;    // last world point.
+  } solution[3];
+};
+
+#endif
