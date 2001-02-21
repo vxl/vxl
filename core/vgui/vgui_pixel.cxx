d3d0b648a38bde582428c4ee44711ee3b423cd1f
Unknown <remcvs_tgtjr_oxford>
2001-02-21 04:16:30 -0500

    dsync.pl (oxford): add rgba5551 for win32 caching

--- core/vgui/vgui_pixel.cxx
+++ core/vgui/vgui_pixel.cxx
@@ -14,8 +14,9 @@ extern "C" int printf(const char *format, ...);
 static bool tunit_init() {
 #define as(T,sz) { if (sizeof(T) != sz) printf("%s : %s has size %d, not %d\n", __FILE__, #T, sizeof(T), sz); }
   as(vgui_pixel_rgb888, 3);
-  as(vgui_pixel_rgb565, 2);
   as(vgui_pixel_bgr888, 3);
+  as(vgui_pixel_rgb565, 2);
+  as(vgui_pixel_bgra5551, 2);
   as(vgui_pixel_rgba8888, 4);
   as(vgui_pixel_abgr8888, 4);
   as(vgui_pixel_bgra8888, 4);
@@ -26,103 +27,76 @@ static bool dummy = tunit_init();
 
 //--------------------------------------------------------------------------------
 
-// macro tragic...  I got confused, and it didn't work on windows, so I did this.  --awf
+// 200201 AWF changed from template magic to an explicit writing out of
+//            the 40 conversions.  It's not a template, because the
+//            bodies are different in each one.
 
-//template <class S, class D> inline void copy_r(S const* s, D* d) { d->R = s->R; }
-//template <class S, class D> inline void copy_g(S const* s, D* d) { d->G = s->G; }
-//template <class S, class D> inline void copy_b(S const* s, D* d) { d->B = s->B; }
-//template <class S, class D> inline void copy_a(S const* s, D* d) { d->A = s->A; }
+//
+//template <class S, class D>
+//void vgui_pixel_convert_span(S const *src, D *dst, unsigned size) {
+//  for (unsigned i=0; i<size; ++i) {
+//    copy_r(src+i, dst+i);
+//    copy_g(src+i, dst+i);
+//    copy_b(src+i, dst+i);
+//    copy_a(src+i, dst+i);
+//  }
+//}
+//#define VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(S, D) \
+//template void vgui_pixel_convert_span(S const *, D *, unsigned);
 
-// Specialize for uchar...
-template <class S> inline void copy_r(S const* s, unsigned char* d) { *d = s->R; }
-template <class S> inline void copy_g(S const* s, unsigned char* d) { *d = s->G; }
-template <class S> inline void copy_b(S const* s, unsigned char* d) { *d = s->B; }
-template <class S> inline void copy_a(S const* s, unsigned char* d) { }
+//: Set an int of unknown size to all-bits-one
+#define SET_FF(x) x = 0; x = ~x
 
-// Source as uchar
-template <class D> inline void copy_r(unsigned char const* s, D* d) { d->R = *s; }
-template <class D> inline void copy_g(unsigned char const* s, D* d) { d->G = *s; }
-template <class D> inline void copy_b(unsigned char const* s, D* d) { d->B = *s; }
-template <class D> inline void copy_a(unsigned char const* s, D* d) { d->A = ~0u; }
-VCL_DEFINE_SPECIALIZATION inline void copy_a(unsigned char const* s, vgui_pixel_rgb888* d) {  }
-VCL_DEFINE_SPECIALIZATION inline void copy_r(unsigned char const* s, vgui_pixel_rgb565* d) { d->R = (*s) >> 3; }
-VCL_DEFINE_SPECIALIZATION inline void copy_g(unsigned char const* s, vgui_pixel_rgb565* d) { d->G = (*s) >> 2; }
-VCL_DEFINE_SPECIALIZATION inline void copy_b(unsigned char const* s, vgui_pixel_rgb565* d) { d->B = (*s) >> 3; }
-VCL_DEFINE_SPECIALIZATION inline void copy_a(unsigned char const* s, vgui_pixel_rgb565* d) {  }
-
-// Source as float
-inline unsigned int clamp(float f) {
-  if (f > 255) return 255;
-  if (f < 0) return 0;
-  return (int)f;
+//: Clamp float to 0..255
+static inline 
+unsigned int clamp(float f) {
+  if (f > 255) return 255u;
+  if (f < 0) return 0u;
+  return (unsigned int)f;
 }
 
-template <class D> inline void copy_r(float const* s, D* d) { d->R = clamp(*s); }
-template <class D> inline void copy_g(float const* s, D* d) { d->G = clamp(*s); }
-template <class D> inline void copy_b(float const* s, D* d) { d->B = clamp(*s); }
-template <class D> inline void copy_a(float const* s, D* d) { d->A = ~0u; }
-VCL_DEFINE_SPECIALIZATION inline void copy_a(float const* s, vgui_pixel_rgb888* d) {  }
-VCL_DEFINE_SPECIALIZATION inline void copy_r(float const* s, vgui_pixel_rgb565* d) { d->R = clamp(*s) >> 3; }
-VCL_DEFINE_SPECIALIZATION inline void copy_g(float const* s, vgui_pixel_rgb565* d) { d->G = clamp(*s) >> 2; }
-VCL_DEFINE_SPECIALIZATION inline void copy_b(float const* s, vgui_pixel_rgb565* d) { d->B = clamp(*s) >> 3; }
-VCL_DEFINE_SPECIALIZATION inline void copy_a(float const* s, vgui_pixel_rgb565* d) {  }
-
-// Source is 888
-template <class D> inline void copy_r(vgui_pixel_rgb888	const* s, D* d) { d->R = s->R; }
-template <class D> inline void copy_g(vgui_pixel_rgb888	const* s, D* d) { d->G = s->G; }
-template <class D> inline void copy_b(vgui_pixel_rgb888	const* s, D* d) { d->B = s->B; }
-template <class D> inline void copy_a(vgui_pixel_rgb888	const* s, D* d) { d->A = ~0u; }
-VCL_DEFINE_SPECIALIZATION inline void copy_a(vgui_pixel_rgb888 const* s, vgui_pixel_rgb888* d) {}
-VCL_DEFINE_SPECIALIZATION inline void copy_r(vgui_pixel_rgb888 const* s, vgui_pixel_rgb565* d) { d->R = (s->R) >> 3; }
-VCL_DEFINE_SPECIALIZATION inline void copy_g(vgui_pixel_rgb888 const* s, vgui_pixel_rgb565* d) { d->G = (s->G) >> 2; }
-VCL_DEFINE_SPECIALIZATION inline void copy_b(vgui_pixel_rgb888 const* s, vgui_pixel_rgb565* d) { d->B = (s->B) >> 3; }
-VCL_DEFINE_SPECIALIZATION inline void copy_a(vgui_pixel_rgb888 const* s, vgui_pixel_rgb565* d) { }
-
-template <class D> inline void copy_r(vgui_pixel_rgba8888 const* s, D* d) { d->R = s->R; }
-template <class D> inline void copy_g(vgui_pixel_rgba8888 const* s, D* d) { d->G = s->G; }
-template <class D> inline void copy_b(vgui_pixel_rgba8888 const* s, D* d) { d->B = s->B; }
-template <class D> inline void copy_a(vgui_pixel_rgba8888 const* s, D* d) { d->A = s->A; }
-VCL_DEFINE_SPECIALIZATION inline void copy_a(vgui_pixel_rgba8888 const* s, vgui_pixel_rgb888* d) {  }
-VCL_DEFINE_SPECIALIZATION inline void copy_r(vgui_pixel_rgba8888 const* s, vgui_pixel_rgb565* d) { d->R = (s->R) >> 3; }
-VCL_DEFINE_SPECIALIZATION inline void copy_g(vgui_pixel_rgba8888 const* s, vgui_pixel_rgb565* d) { d->G = (s->G) >> 2; }
-VCL_DEFINE_SPECIALIZATION inline void copy_b(vgui_pixel_rgba8888 const* s, vgui_pixel_rgb565* d) { d->B = (s->B) >> 3; }
-VCL_DEFINE_SPECIALIZATION inline void copy_a(vgui_pixel_rgba8888 const* s, vgui_pixel_rgb565* d) {  }
-
-//--------------------------------------------------------------------------------
-
-template <class S, class D>
-void vgui_pixel_convert_span(S const *src, D *dst, unsigned size) {
-  for (unsigned i=0; i<size; ++i) {
-    copy_r(src+i, dst+i);
-    copy_g(src+i, dst+i);
-    copy_b(src+i, dst+i);
-    copy_a(src+i, dst+i);
+//: This macro is used to generate a "vgui_pixel_convert_span(S*, D*)" function with body "Code"
+// Within Code, s is a reference to the source pixel, d to the destination.
+// Remember, vgui_pixel_convert_span is not a function template -- each overload has
+// rather different code.
+#define VGUI_PIXEL_CONVERT_SPAN_DO(S, D, Code) \
+  void vgui_pixel_convert_span(S const *src, D *dst, unsigned size) { \
+    for (unsigned i=0; i<size; ++i) { \
+      S const& s = src[i]; \
+      D      & d = dst[i]; \
+      Code; \
+    }\
   }
-}
 
-#define VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(S, D) \
-template void vgui_pixel_convert_span(S const *, D *, unsigned);
+// SrcType, DstType, Code
+VGUI_PIXEL_CONVERT_SPAN_DO(GLubyte, vgui_pixel_rgb888,   { d.R = d.G = d.B = s; } );
+VGUI_PIXEL_CONVERT_SPAN_DO(GLubyte, vgui_pixel_bgr888,   { d.R = d.G = d.B = s; } );
+VGUI_PIXEL_CONVERT_SPAN_DO(GLubyte, vgui_pixel_rgb565,   { d.R = d.B = (s >> 3); d.G = (s >> 2); } );
+VGUI_PIXEL_CONVERT_SPAN_DO(GLubyte, vgui_pixel_bgra5551, { d.R = d.G = d.B = (s>>3); SET_FF(d.A); } );
+VGUI_PIXEL_CONVERT_SPAN_DO(GLubyte, vgui_pixel_rgba8888, { d.R = d.G = d.B = s; SET_FF(d.A); } );
+VGUI_PIXEL_CONVERT_SPAN_DO(GLubyte, vgui_pixel_abgr8888, { d.R = d.G = d.B = s; SET_FF(d.A); } );
+VGUI_PIXEL_CONVERT_SPAN_DO(GLubyte, vgui_pixel_bgra8888, { d.R = d.G = d.B = s; SET_FF(d.A); } );
 
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(GLubyte, vgui_pixel_rgb888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(GLubyte, vgui_pixel_rgb565);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(GLubyte, vgui_pixel_rgba8888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(GLubyte, vgui_pixel_abgr8888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(GLubyte, vgui_pixel_bgra8888);
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgb888, vgui_pixel_rgb888,   { d = s; });
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgb888, vgui_pixel_bgr888,   { d.R = s.R; d.G = s.G; d.B = s.B; })
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgb888, vgui_pixel_rgb565,   { d.R = s.R>>3; d.G = s.G>>2; d.B = s.B>>3; })
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgb888, vgui_pixel_bgra5551, { d.R = s.R>>3; d.G = s.G>>3; d.B = s.B>>3; SET_FF(d.A); })
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgb888, vgui_pixel_rgba8888, { d.R = s.R; d.G = s.G; d.B = s.B; SET_FF(d.A); })
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgb888, vgui_pixel_abgr8888, { d.R = s.R; d.G = s.G; d.B = s.B; SET_FF(d.A); })
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgb888, vgui_pixel_bgra8888, { d.R = s.R; d.G = s.G; d.B = s.B; SET_FF(d.A); })
 
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(vgui_pixel_rgb888, vgui_pixel_rgb888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(vgui_pixel_rgb888, vgui_pixel_rgb565);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(vgui_pixel_rgb888, vgui_pixel_rgba8888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(vgui_pixel_rgb888, vgui_pixel_abgr8888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(vgui_pixel_rgb888, vgui_pixel_bgra8888);
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgba8888, vgui_pixel_rgb888,   { d.R = s.R;    d.G = s.G;    d.B = s.B; })
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgba8888, vgui_pixel_bgr888,   { d.R = s.R;    d.G = s.G;    d.B = s.B; })
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgba8888, vgui_pixel_rgb565,   { d.R = s.R>>3; d.G = s.G>>2; d.B = s.B>>3; })
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgba8888, vgui_pixel_bgra5551, { d.R = s.R>>3; d.G = s.G>>3; d.B = s.B>>3; })
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgba8888, vgui_pixel_rgba8888, { d = s; })
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgba8888, vgui_pixel_abgr8888, { d.R = s.R;    d.G = s.G;    d.B = s.B; d.A = s.A; })
+VGUI_PIXEL_CONVERT_SPAN_DO(vgui_pixel_rgba8888, vgui_pixel_bgra8888, { d.R = s.R;    d.G = s.G;    d.B = s.B; d.A = s.A; })
 
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(vgui_pixel_rgba8888, vgui_pixel_rgb888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(vgui_pixel_rgba8888, vgui_pixel_rgb565);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(vgui_pixel_rgba8888, vgui_pixel_rgba8888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(vgui_pixel_rgba8888, vgui_pixel_abgr8888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(vgui_pixel_rgba8888, vgui_pixel_bgra8888);
-
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(float, vgui_pixel_rgb888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(float, vgui_pixel_rgb565);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(float, vgui_pixel_rgba8888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(float, vgui_pixel_abgr8888);
-VGUI_PIXEL_CONVERT_SPAN_INSTANTIATE(float, vgui_pixel_bgra8888);
+VGUI_PIXEL_CONVERT_SPAN_DO(float, vgui_pixel_rgb888,   { d.R = d.G = d.B = clamp(s); })
+VGUI_PIXEL_CONVERT_SPAN_DO(float, vgui_pixel_bgr888,   { d.R = d.G = d.B = clamp(s); })
+VGUI_PIXEL_CONVERT_SPAN_DO(float, vgui_pixel_rgb565,   { int v = clamp(s); d.R = d.B = v>>3; d.G = v>>2; })
+VGUI_PIXEL_CONVERT_SPAN_DO(float, vgui_pixel_bgra5551, { d.R = d.G = d.B = clamp(s)>>3; SET_FF(d.A); })
+VGUI_PIXEL_CONVERT_SPAN_DO(float, vgui_pixel_rgba8888, { d.R = d.G = d.B = clamp(s); SET_FF(d.A); })
+VGUI_PIXEL_CONVERT_SPAN_DO(float, vgui_pixel_abgr8888, { d.R = d.G = d.B = clamp(s); SET_FF(d.A); })
+VGUI_PIXEL_CONVERT_SPAN_DO(float, vgui_pixel_bgra8888, { d.R = d.G = d.B = clamp(s); SET_FF(d.A); })
