01fa6465ebe12214abd7fe291333ce8c4fcdc153
Unknown <remcvs_tgtjr_oxford>
2001-02-21 04:18:28 -0500

    dsync.pl (oxford): use operator norm instead of frobenius norm

--- core/vnl/vnl_matrix_exp.h
+++ core/vnl/vnl_matrix_exp.h
@@ -13,6 +13,7 @@
 // .INCLUDE	vnl/vnl_matrix_exp.h
 // .FILE	vnl_matrix_exp.txx
 // .SECTION Description
+//
 // Compute the exponential of a square matrix, by summing its
 // exponential series $exp(X) = \sum_{n \ge 0} X^n/n!$ till a
 // convergence requirement is met.
