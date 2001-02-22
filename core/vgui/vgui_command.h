253be221d8ec6744cda9ef367484133ca1cbbed4
Unknown <remcvs_tgtjr_oxford>
2001-02-22 03:52:02 -0500

    dsync.pl (oxford): fsm - more command templates

--- core/vgui/vgui_command.h
+++ core/vgui/vgui_command.h
@@ -15,25 +15,34 @@
 #include <vbl/vbl_ref_count.h>
 
 // this defines the abstract interface to commands.
-class vgui_command : public vbl_ref_count {
-public:
+struct vgui_command : vbl_ref_count
+{
   vgui_command();
   virtual ~vgui_command();
   virtual void execute() =0;
 };
 
 // an implementation using a C callback function.
-class vgui_command_cfunc : public vgui_command {
-public:
-  typedef void (*function_pv)(const void*);
+struct vgui_command_cfunc : vgui_command
+{
+  typedef void (*function_pv)(void const*);
   typedef void (*function)();
   function_pv fn_pv;
   function fn;
-  const void *data;
+  void const *data;
 
-  vgui_command_cfunc(function_pv, const void *);
+  vgui_command_cfunc(function_pv, void const *);
   vgui_command_cfunc(function);
  ~vgui_command_cfunc();
+  void execute();
+};
+
+// command for toggle buttons
+struct vgui_command_toggle : vgui_command
+{
+  bool state;
+  vgui_command_toggle(bool v) : state(v) { }
+  ~vgui_command_toggle();
   void execute();
 };
 
@@ -47,26 +56,35 @@ template <class receiver>
 class vgui_command_simple : public vgui_command {
 public:
   typedef void (receiver::* action)();
-
-  // 2.7 barfs unless this is inline.
-  vgui_command_simple(receiver* o, action m) : vgui_command(), obj(o), mem(m) { } 
-  ~vgui_command_simple() { }
+  
+  vgui_command_simple(receiver* o, action m) : obj(o), mem(m) { }
   void execute() { (obj->*mem)(); }
   
   receiver* obj;
   action mem;
 };
-// instantiation macro :
-#define INSTANTIATE_VGUI_simple_command(receiver) \
+#define VGUI_COMMAND_SIMPLE_INSTANTIATE(receiver) \
 template class vgui_command_simple<receiver >
 
-// command for toggle buttons
-class vgui_command_toggle : public vgui_command {
-public:
-  bool state;
-  vgui_command_toggle(bool v) : state(v) { }
-  ~vgui_command_toggle();
-  void execute();
+// similar, but for methods that take a single argument (fsm).
+template <class object_t, class data_t>
+struct vgui_command_bound_method : vgui_command
+{
+  typedef void (object_t::*action_t)(data_t);
+  
+  vgui_command_bound_method(object_t *o, action_t m, data_t d) : obj(o), mem(m), dat(d) { }
+  void execute() { (obj->*mem)(dat); }
+  
+  object_t *obj;
+  action_t mem;
+  data_t dat;
 };
+#define VGUI_COMMAND_BOUND_METHOD_INSTANTIATE(O, D) \
+template struct vgui_command_bound_method<O, D >
+
+//----------------------------------------------------------------------
+
+#define INSTANTIATE_VGUI_simple_command(receiver) \
+VGUI_COMMAND_SIMPLE_INSTANTIATE(receiver) /* backwards compat */
 
 #endif // vgui_command_h_
---
+++ core/vgui/vgui_command.txx
@@ -0,0 +1,10 @@
+#ifndef vgui_command_txx_
+#define vgui_command_txx_
+/*
+  fsm@robots.ox.ac.uk
+*/
+#include "vgui_command.h"
+
+// the macros are in the .h file.
+
+#endif
