9acf871e3bdd7fc6ae732ad149509189811c45dc
Unknown <remcvs_tgtjr_oxford>
2001-03-02 21:25:28 -0500

    dsync.pl (oxford): KYM modifiers added to key press/release events & added some Doxygen comments

--- core/vgui/impl/mfc/vgui_mfc_adaptor.h
+++ core/vgui/impl/mfc/vgui_mfc_adaptor.h
@@ -25,41 +25,59 @@ public:
 	CDC* m_pDC;
 	
 // Operations
-public:
 	typedef vgui_adaptor_mixin mixin;
+
 	// 0. vgui_adaptor implementations
+  //--------------------------------
+  //: MFC implementation of vgui_adaptor function - returns width of rendering area.
 	unsigned int get_width() const { return m_width; }
+  //: MFC implementation of vgui_adaptor function - returns height of rendering area.
 	unsigned int get_height() const { return m_height; }
+  //: MFC implementation of vgui_adaptor function - redraws rendering area.
 	void post_redraw();
+  //: MFC implementation of vgui_adaptor function - redraws overlay buffer.
 	void post_overlay_redraw();
+  //: MFC implementation of vgui_adaptor function - swap buffers if using double buffering.
 	void swap_buffers();
+  //: MFC implementation of vgui_adaptor function - 
 	void make_current();
+  //: MFC implementation of vgui_adaptor function - redraw everything now.
 	void draw();
+
 	// 0.5 fsm stuff
+  //--------------
+  //: Redraws the OpenGL area.
 	void service_redraws();
-	bool come_out_now;
+	//bool come_out_now;
+  //: True while a redraw event has been requested but not implemented.
 	bool redraw_posted;
+  //: Change the default popup menu to the given one (not yet implemented).
 	void set_default_popup(vgui_menu);
-        void post_timer(float,int);
+  //: Sets the timer to dispatch WM_TIME event to a mainframe every time miliseconds
+  void post_timer(float,int);
+  //: Return the default popup menu (not yet implemented).
 	vgui_menu get_popup();
+  //: MFC implementation of vgui_adaptor function - set button & modifier to display popup.
   void bind_popups(vgui_modifier m, vgui_button b) {
     mixin::popup_modifier = m;
     mixin::popup_button = b;
   }
+  //: MFC implementation of vgui_adaptor function - get button & modifier which display popup.
   void get_popup_bindings(vgui_modifier &m,vgui_button &b) const {
     m = mixin::popup_modifier;
     b = mixin::popup_button;
   }
-	// 1. MFC stuff
-	// Overrides
-	// ClassWizard generated virtual function overrides
 
+	// 1. MFC stuff
+  //-------------
+	// ClassWizard generated virtual function overrides
 protected:
-	virtual void OnDraw(CDC* pDC);      // overridden to draw this view
+  //: Called by MFC when a draw event is required - overridden to draw this view.
+	virtual void OnDraw(CDC* pDC);
+  //: Called by MFC before the creation of the window attached to this object.
 	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
 	
 // Implementation
-protected:
 	virtual ~vgui_mfc_adaptor();
 #ifdef _DEBUG
 	virtual void AssertValid() const;
@@ -67,30 +85,51 @@ protected:
 #endif
 
 	// Generated message map functions
-protected:
+  //--------------------------------
+  //: Handles mouse press/release events.
 	void domouse(vgui_event_type e, UINT nFlags, CPoint point, vgui_button b);
 	HGLRC m_hRC;
 	BOOL SetupPixelFormat();
+  //: Width of rendering area.
 	int m_width;
-        int m_height;
-        HBITMAP m_oldbitmap;
-        bool double_buffered;
+  //: Height of rendering area.
+  int m_height;
+  HBITMAP m_oldbitmap;
+  //: Whether or not double buffering is being used.
+  bool double_buffered;
 	vgui_overlay_helper *ovl_helper;
 	static vgui_menu last_popup;
+  //: Called by MFC when the application requests the creation of a window.
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+  //: Called by MFC when the window has been destroyed.
 	afx_msg void OnDestroy();
+  //: Called by MFC when the application requests part of the window is redrawn.
 	afx_msg void OnPaint();
+  //: Called by MFC when the background needs erasing.
 	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+  //: Called by MFC when the application is resized.
 	afx_msg void OnSize(UINT nType, int cx, int cy);
+  //: Create the corresponding vgui_event from an MFC event.
+  vgui_event generate_vgui_event(UINT nChar, UINT nRepCnt, UINT nFlags, vgui_event_type evttype);
+  //: Called by MFC when a key is pressed inside the application.
 	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+  //: Called by MFC when a key is released inside the application.
 	afx_msg void OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags);
+  //: Called by MFC when the left mouse button is pressed inside the application.
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+  //: Called by MFC when the left mouse button is released inside the application.
 	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+  //: Called by MFC when the mouse is moved inside the application.
 	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+  //: Called by MFC when the right mouse button is pressed inside the application.
 	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+  //: Called by MFC when the right mouse button is released inside the application.
 	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+  //: Called by MFC when a user rotates a mouse wheel.
 	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
+  //: Called by MFC when the middle mouse button is pressed inside the application.
 	afx_msg void OnMButtonDown(UINT nFlags, CPoint point);
+  //: Called by MFC when the middle mouse button is released inside the application.
 	afx_msg void OnMButtonUp(UINT nFlags, CPoint point);
 	DECLARE_MESSAGE_MAP()
 };
