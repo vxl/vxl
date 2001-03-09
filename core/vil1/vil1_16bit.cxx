2f9d6a3a1e5ac636579dc1ab7ce172c731962740
Unknown <remcvs_targetjr_grumpie>
2001-03-09 03:18:53 -0500

    typedef causes syntax error

--- core/vil1/vil1_16bit.cxx
+++ core/vil1/vil1_16bit.cxx
@@ -8,35 +8,32 @@
 #include <vil/vil_stream.h>
 #include <vxl_config.h>
 
-typedef vxl_uint_8  word8;
-typedef vxl_uint_16 word16;
-
 unsigned vil_16bit_read_big_endian(vil_stream *s)
 {
-  word8 bytes[2];
+  vxl_uint_8 bytes[2];
   s->read(bytes, sizeof bytes);
-  return word16(bytes[1]) + (word16(bytes[0])<<8);
+  return vxl_uint_16(bytes[1]) + (vxl_uint_16(bytes[0])<<8);
 }
 
 unsigned vil_16bit_read_little_endian(vil_stream *s)
 {
-  word8 bytes[2];
+  vxl_uint_8 bytes[2];
   s->read(bytes, sizeof bytes);
-  return word16(bytes[0]) + (word16(bytes[1])<<8);
+  return vxl_uint_16(bytes[0]) + (vxl_uint_16(bytes[1])<<8);
 }
 
 void vil_16bit_write_big_endian(vil_stream *s, unsigned w)
 {
-  word8 bytes[2];
-  bytes[0] = word8(w >> 8);
-  bytes[1] = word8(w & 0xff);
+  vxl_uint_8 bytes[2];
+  bytes[0] = vxl_uint_8(w >> 8);
+  bytes[1] = vxl_uint_8(w & 0xff);
   s->write(bytes, sizeof bytes);
 }
 
 void vil_16bit_write_little_endian(vil_stream *s, unsigned w)
 {
-  word8 bytes[2];
-  bytes[0] = word8(w & 0xff);
-  bytes[1] = word8(w >> 8);
+  vxl_uint_8 bytes[2];
+  bytes[0] = vxl_uint_8(w & 0xff);
+  bytes[1] = vxl_uint_8(w >> 8);
   s->write(bytes, sizeof bytes);
 }
--- core/vil1/vil1_32bit.cxx
+++ core/vil1/vil1_32bit.cxx
@@ -9,26 +9,23 @@
 #include <vil/vil_stream.h>
 #include <vxl_config.h>
 
-typedef vxl_uint_8  word8;
-typedef vxl_uint_32 word32;
-
 unsigned vil_32bit_read_big_endian(vil_stream *s)
 {
-  word8 bytes[4];
+  vxl_uint_8 bytes[4];
   s->read(bytes, sizeof bytes);
-  return (word32(bytes[0])<<24) + (word32(bytes[1])<<16) + (word32(bytes[2])<<8) + (word32(bytes[3]));
+  return (vxl_uint_32(bytes[0])<<24) + (vxl_uint_32(bytes[1])<<16) + (vxl_uint_32(bytes[2])<<8) + (vxl_uint_32(bytes[3]));
 }
 
 unsigned vil_32bit_read_little_endian(vil_stream *s)
 {
-  word8 bytes[4];
+  vxl_uint_8 bytes[4];
   s->read(bytes, sizeof bytes);
-  return (word32(bytes[3])<<24) + (word32(bytes[2])<<16) + (word32(bytes[1])<<8) + (word32(bytes[0]));
+  return (vxl_uint_32(bytes[3])<<24) + (vxl_uint_32(bytes[2])<<16) + (vxl_uint_32(bytes[1])<<8) + (vxl_uint_32(bytes[0]));
 }
 
 void vil_32bit_write_big_endian(vil_stream *s, unsigned w)
 {
-  word8 bytes[4];
+  vxl_uint_8 bytes[4];
   bytes[0] = w >> 24;
   bytes[1] = w >> 16;
   bytes[2] = w >> 8;
@@ -38,7 +35,7 @@ void vil_32bit_write_big_endian(vil_stream *s, unsigned w)
 
 void vil_32bit_write_little_endian(vil_stream *s, unsigned w)
 {
-  word8 bytes[4];
+  vxl_uint_8 bytes[4];
   bytes[0] = w >> 0;
   bytes[1] = w >> 8;
   bytes[2] = w >> 16;
