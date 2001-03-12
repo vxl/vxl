f415fe5676613b238cdb16a8ea93e9d19c4910ec
Unknown <remcvs_tgtjr_oxford>
2001-03-11 21:24:52 -0500

    dsync.pl (oxford): use configure macros dsync.pl: fix for sunpro 5.0

--- vcl/vcl_csetjmp.h
+++ vcl/vcl_csetjmp.h
@@ -4,8 +4,23 @@
   Peter.Vanroose@esat.kuleuven.ac.be
 */
 
-#include "vcl_compiler.h"
-#include <setjmp.h>
 /* This should define C-style stack unwinding */
+
+#include "vcl_compiler.h"
+
+// SunPro 5.0's <csetjmp> is broken.
+#if !VCL_CXX_HAS_HEADER_CSETJMP || defined(VCL_SUNPRO_CC_50)
+# include <setjmp.h>
+# define vcl_generic_csetjmp_STD /* */
+# include "generic/vcl_csetjmp.h"
+#else
+# include "iso/vcl_csetjmp.h"
+#endif
+
+// In ISO C, setjmp() is a macro. However, many people
+// don't know this so we provide another macro here in
+// order to avoid confusing people who think they want
+// to use std::setjmp() or ::setjmp().
+#define vcl_setjmp setjmp
 
 #endif // vcl_csetjmp_h_
