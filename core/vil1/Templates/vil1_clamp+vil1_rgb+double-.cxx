b3f2cd8d38a6bb4951ad320a3f7f04ae5e92c49e
Unknown <remcvs_tgtjr_oxford>
2001-03-11 21:30:03 -0500

    dsync.pl (oxford): purge 2.7 code

--- core/vil1/Templates/vil1_clamp+vil1_rgb+double-.cxx
+++ core/vil1/Templates/vil1_clamp+vil1_rgb+double-.cxx
@@ -6,14 +6,6 @@
 #if VCL_CAN_DO_PARTIAL_SPECIALIZATION
 // instantiate the inline partially specializated template from vil_rgb.h :
 VCL_INSTANTIATE_INLINE(vil_rgb<unsigned char> vil_clamp(const vil_rgb<double> &, vil_rgb<unsigned char> *));
-#elif defined(VCL_GCC_27)
-// define a plain (non-templated) function. 2.7 can't tell the difference.
-vil_rgb<unsigned char> vil_clamp(vil_rgb<double> const &d, vil_rgb<unsigned char>*) {
-  unsigned char ttt;
-  return vil_rgb<unsigned char>(vil_clamp(d.r, &ttt),
-			    vil_clamp(d.g, &ttt),
-			    vil_clamp(d.b, &ttt));
-}
 #else
 // instantiate the inline non-partial specialization from vil_rgb.h :
 VCL_INSTANTIATE_INLINE(vil_rgb<unsigned char> vil_clamp(const vil_rgb<double> &, vil_rgb<unsigned char> *));
