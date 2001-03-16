fa53f82db0bb5e9cb5cb317638cfb23594554de3
Unknown <remcvs_tgtjr_oxford>
2001-03-15 22:26:17 -0500

    dsync.pl (oxford): KYM: statusbar message is no longer deleted immediately

--- core/vgui/impl/mfc/vgui_mfc_statusbar.cxx
+++ core/vgui/impl/mfc/vgui_mfc_statusbar.cxx
@@ -3,56 +3,67 @@
 #pragma implementation
 #endif
 //
-// .NAME vgui_mfc_statusbar
+// Class: vgui_mfc_statusbar
 // Author: Marko Bacic, RRG, University of Oxford
 // Created: 10 Aug 2000
 //
+// Modifications:
+//   13-MAR-2001 K.Y.McGaul   Messages are now cleared when a new message 
+//                            appears, not immediately.
 //-----------------------------------------------------------------------------
 
 #include "vgui_mfc_statusbar.h"
 #include "vgui_mfc_mainfrm.h" 
+
+//: Constructor, creates an MFC status bar object and displays it.
 vgui_mfc_statusbar::vgui_mfc_statusbar()
   : statusbuf(new vgui_statusbuf(this))
   , out(statusbuf)
 {
-	// -- Create status bar object
 	statusbar = new CStatusBar();
 	statusbar->Create(AfxGetApp()->GetMainWnd());
-        unsigned x = ID_SEPARATOR;
-        statusbar->SetIndicators(&x,1);
-        CFrameWnd *fwnd = (CFrameWnd *)AfxGetApp()->GetMainWnd();
-        fwnd->ShowControlBar(statusbar,TRUE,FALSE);
-        statusbar->UpdateWindow();
+  unsigned x = ID_SEPARATOR;
+  statusbar->SetIndicators(&x,1);
+  CFrameWnd *fwnd = (CFrameWnd *)AfxGetApp()->GetMainWnd();
+  fwnd->ShowControlBar(statusbar,TRUE,FALSE);
+  statusbar->UpdateWindow();
 	statusbar->ShowWindow(SW_SHOW);
 }
-
 
 vgui_mfc_statusbar::~vgui_mfc_statusbar()
 {
 }
 
 static int context_id = 1;
-// -- Updates the status bar by calling OnUpdateStatusBar of vgui_mfc_mainfrm
+
+//: Updates the status bar by calling OnUpdateStatusBar of vgui_mfc_mainfrm
 void vgui_mfc_statusbar::update()
 {
-   
-         CCmdUI cui;
-         cui.m_nID = ID_SEPARATOR;
-         cui.m_nIndex = 4;
-         cui.m_pMenu = NULL;
-         cui.m_pOther = statusbar;
+  CCmdUI cui;
+  cui.m_nID = ID_SEPARATOR;
+  cui.m_nIndex = 4;
+  cui.m_pMenu = NULL;
+  cui.m_pOther = statusbar;
 
-         ((vgui_mfc_mainfrm *)AfxGetApp()->GetMainWnd())->UpdateStatusBar(&cui);
-      
+  ((vgui_mfc_mainfrm *)AfxGetApp()->GetMainWnd())->UpdateStatusBar(&cui);      
 }
-int vgui_mfc_statusbar::write(const char* text, int n) {
 
+
+//: Append given text (of given length) to the status bar
+int vgui_mfc_statusbar::write(const char* text, int n) {
+  static bool start_new = false;
   if(n == 1) {
     if (text[0] == '\n') {
-
-	  statusbar->SetWindowText(linebuffer.c_str());
-          statusbar->SetPaneText(0,linebuffer.c_str());
+      // We are at the end of the message. Set a bool so we know next time 
+      // to clear the buffer before we start a new message.
+      start_new = true;
+    }
+    else if (start_new == true){
+      statusbar->SetWindowText(linebuffer.c_str());
+      statusbar->SetPaneText(0,linebuffer.c_str());
       linebuffer = "";
+      linebuffer += text[0];
+      start_new = false;
     }
     else
       linebuffer += text[0];
@@ -60,30 +71,27 @@ int vgui_mfc_statusbar::write(const char* text, int n) {
   else {
     linebuffer.append(text, n);
     if (linebuffer.find('\n')) {
-
-	  statusbar->SetWindowText(linebuffer.c_str());
-          statusbar->SetPaneText(0,linebuffer.c_str());
-          linebuffer = "";
-      
+	    statusbar->SetWindowText(linebuffer.c_str());
+      statusbar->SetPaneText(0,linebuffer.c_str());
+      linebuffer = "";
     }
   }
-  	statusbar->UpdateWindow();
+  statusbar->UpdateWindow();
 	statusbar->ShowWindow(SW_SHOW);
 	AfxGetApp()->GetMainWnd()->UpdateWindow();
 	AfxGetApp()->GetMainWnd()->ShowWindow(SW_SHOW);
-    update();
+  update();
   return n;  
 }
 
-
+//: Write given text to the status bar.
 int vgui_mfc_statusbar::write(const char* text) {
-  
 	statusbar->SetWindowText(text);
-        linebuffer = text;
-        statusbar->SetPaneText(0,linebuffer.c_str());
+  linebuffer = text;
+  statusbar->SetPaneText(0,linebuffer.c_str());
 	statusbar->UpdateWindow();
 	statusbar->ShowWindow(SW_SHOW);
-          update();
+  update();
 
 	return 1;
 }
