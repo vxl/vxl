4f8ad170c62f41c2746e774742a699231c144e3e
Unknown <targetjr>
2001-03-16 05:53:41 -0500

    annoying warning

--- vcl/vcl_strstream.h
+++ vcl/vcl_strstream.h
@@ -7,7 +7,8 @@
 // <strstream> is deprecated, i.e. it is normative for the
 // current (1998) version of the standard, but may not be
 // in future standards.
-#include "vcl_deprecated.h"
+// But as it is used a lot in TargetJr, don't issue this warning - PVr
+//#include "vcl_deprecated.h"
 
 // this is to get the vcl_ios_* macros.
 #include "vcl_iostream.h"
