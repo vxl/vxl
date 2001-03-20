a4c2e772da89bc74ff3e2ba1311b70fc49627f69
Unknown <remcvs_tgtjr_oxford>
2001-03-20 06:40:40 -0500

    dsync.pl (oxford): KYM: added PreTranslateMessage to give menu accelerators

--- core/vgui/impl/mfc/vgui_mfc_mainfrm.cxx
+++ core/vgui/impl/mfc/vgui_mfc_mainfrm.cxx
@@ -21,13 +21,13 @@ static char THIS_FILE[] = __FILE__;
 IMPLEMENT_DYNCREATE(vgui_mfc_mainfrm, CFrameWnd)
 
 BEGIN_MESSAGE_MAP(vgui_mfc_mainfrm, CFrameWnd)
-	ON_WM_CREATE()
-	ON_WM_HSCROLL() // -- We are processing both horizontal scroll bar
-	ON_WM_VSCROLL() // and vertical scroll bar events
-        ON_WM_TIMER() //
-	ON_WM_CLOSE() // added manually by awf
-	ON_COMMAND_RANGE(ID_MENU_ITEMS,ID_MENU_ITEMS+MAX_ITEM_COUNT,process_menus)
-        ON_UPDATE_COMMAND_UI(ID_SEPARATOR,OnUpdateStatusBar)
+  ON_WM_CREATE()
+  ON_WM_HSCROLL() // - We are processing both horizontal scroll bar
+  ON_WM_VSCROLL() // and vertical scroll bar events
+  ON_WM_TIMER()   //
+  ON_WM_CLOSE()   // added manually by awf
+  ON_COMMAND_RANGE(ID_MENU_ITEMS,ID_MENU_ITEMS+MAX_ITEM_COUNT,process_menus)
+  ON_UPDATE_COMMAND_UI(ID_SEPARATOR,OnUpdateStatusBar)
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -43,7 +43,9 @@ vgui_mfc_mainfrm::~vgui_mfc_mainfrm()
 {
 }
 
-//: Called before window creation.  Store any desired info in cs.
+//: Called by MFC before window creation.  
+//  Store any desired info in cs.
+//  Returns non-zero value to indicate creation should continue.
 BOOL vgui_mfc_mainfrm::PreCreateWindow(CREATESTRUCT& cs)
 {
 	if( !CFrameWnd::PreCreateWindow(cs) )
@@ -54,7 +56,7 @@ BOOL vgui_mfc_mainfrm::PreCreateWindow(CREATESTRUCT& cs)
 	return TRUE;
 }
 
-//: Called on window creation
+//: Called by MFC on window creation
 int vgui_mfc_mainfrm::OnCreate(LPCREATESTRUCT lpCreateStruct)
 {
   if (CFrameWnd::OnCreate(lpCreateStruct) == -1)
@@ -69,16 +71,31 @@ void vgui_mfc_mainfrm::OnClose()
   //CFrameWnd::OnClose();
 
   DestroyWindow();
+  DestroyAcceleratorTable(vgui_mfc_utils::instance()->AccelTable);
   PostQuitMessage(0);
 }
 
-// -- Called whenever a menu item has been selected
+//: Called by MFC before a message is sent to the menus.
+//  We are overriding this function so we can use our own menu
+//  accelerators defined AccelTable.
+//  FIXME - kym, this doesn't seem to work!!!
+BOOL vgui_mfc_mainfrm::PreTranslateMessage(MSG* pmsg)
+{
+  if (pmsg->message >= WM_KEYFIRST && pmsg->message <= WM_KEYLAST)
+  {
+    // m_hWnd is the handle of this Windows window attached to this CWnd:
+    TranslateAccelerator(m_hWnd, vgui_mfc_utils::instance()->AccelTable, (LPMSG)pmsg);
+  }
+  return CFrameWnd::PreTranslateMessage(pmsg);
+}
+
+//: Called whenever a menu item has been selected
 void vgui_mfc_mainfrm::process_menus(UINT nID)
 {
   vgui_mfc_utils::instance()->menu_dispatcher(nID);
 }
 
-// -- Called when ON_WM_HSCROLL message is received
+//: Called when ON_WM_HSCROLL message is received
 void vgui_mfc_mainfrm::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar )
 {
   // --- For the time being this routine only deals with this windows' scroll 
@@ -118,10 +135,10 @@ void vgui_mfc_mainfrm::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar
       break;
     }
 }
-// -- Called when ON_WM_VSCROLL message is received
+//: Called when ON_WM_VSCROLL message is received
 void vgui_mfc_mainfrm::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar )
 {
-  // --- For the time being this routine only deals with this windows' scroll 
+  // For the time being this routine only deals with this windows' scroll 
   // bar and not to any controls within the window
   CWinApp *app = AfxGetApp();
   POSITION pos = app->GetFirstDocTemplatePosition();
