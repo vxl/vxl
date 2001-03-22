ecedd41dab28dfb6a966131c16733f3230dde23f
Geoffrey Cross <geoffreycross@users.sourceforge.net>
2001-03-22 14:32:41 -0500

    vcl_ fixes and things

--- contrib/gel/gmvl/gmvl_database.cxx
+++ contrib/gel/gmvl/gmvl_database.cxx
@@ -23,11 +23,12 @@ void gmvl_database::remove_node( const gmvl_node_ref node)
   connectioncache_.rebuild();
 }
 
+// returns the first occurance if there are multiple tags of the same type
 gmvl_node_ref gmvl_database::find_tag( const vcl_string &string) const
 {
   vcl_vector<gmvl_node_ref> tags= nodecache_.get( "gmvl_tag_node");
 
-  for( unsigned int i=0; i< tags.size(); ++i)
+  for( unsigned int i=0; i< tags.size(); i++)
     {
       gmvl_node *ptr= tags[i].ptr();
 
@@ -53,7 +54,7 @@ void gmvl_database::add_connection( const gmvl_node_ref node1, const gmvl_node_r
 
 void gmvl_database::add_connections( const gmvl_node_ref node1, vcl_vector<gmvl_node_ref> nodes)
 {
-  for( unsigned int i=0; i< nodes.size(); ++i)
+  for( unsigned int i=0; i< nodes.size(); i++)
     add_connection( node1, nodes[i]);
 }
 
@@ -70,12 +71,26 @@ vcl_vector<gmvl_node_ref> gmvl_database::get_connected_nodes( const gmvl_node_re
   vcl_vector<int> c= connectioncache_.get_connected_nodes( node);
   vcl_vector<gmvl_node_ref> l;
 
-  for( unsigned int i=0; i< c.size(); ++i)
+  for( unsigned int i=0; i< c.size(); i++)
     {
       l.push_back( nodecache_.get( c[i]));
     }
   
   return l;
+}
+
+vcl_vector<gmvl_node_ref> gmvl_database::get_connected_nodes( const gmvl_node_ref node, const vcl_string type) const
+{
+  vcl_vector<gmvl_node_ref> l= get_connected_nodes( node);
+  vcl_vector<gmvl_node_ref> m;
+
+  for( unsigned int i=0; i< l.size(); i++)
+    {
+      if( l[i]->type()== type)
+	m.push_back( l[i]);
+    }
+
+  return m;
 }
 
 // two way connection
@@ -85,7 +100,7 @@ vcl_vector<gmvl_node_ref> gmvl_database::get_connected_nodes( const gmvl_node_re
   vcl_vector<int> c= connectioncache_.get_connected_nodes( node1, node2);
   vcl_vector<gmvl_node_ref> l;
 
-  for( unsigned int i=0; i< c.size(); ++i)
+  for( unsigned int i=0; i< c.size(); i++)
     {
       l.push_back( nodecache_.get( c[i]));
     }
@@ -101,7 +116,7 @@ vcl_vector<gmvl_node_ref> gmvl_database::get_connected_nodes( const gmvl_node_re
   vcl_vector<int> c= connectioncache_.get_connected_nodes( node1, node2, node3);
   vcl_vector<gmvl_node_ref> l;
 
-  for( unsigned int i=0; i< c.size(); ++i)
+  for( unsigned int i=0; i< c.size(); i++)
     {
       l.push_back( nodecache_.get( c[i]));
     }
@@ -115,12 +130,33 @@ vcl_vector<gmvl_node_ref> gmvl_database::get_connected_nodes( const vcl_vector<g
   vcl_vector<int> c= connectioncache_.get_connected_nodes( nodes);
   vcl_vector<gmvl_node_ref> l;
 
-  for( unsigned int i=0; i< c.size(); ++i)
+  for( unsigned int i=0; i< c.size(); i++)
     {
       l.push_back( nodecache_.get( c[i]));
     }
   
   return l;
+}
+
+vcl_vector<gmvl_node_ref> gmvl_database::get_connected_nodes( const vcl_vector<gmvl_node_ref> nodes, const vcl_string type) const
+{
+  vcl_vector<int> c= connectioncache_.get_connected_nodes( nodes);
+  vcl_vector<gmvl_node_ref> l;
+
+  for( unsigned int i=0; i< c.size(); i++)
+    {
+      l.push_back( nodecache_.get( c[i]));
+    }
+  
+  vcl_vector<gmvl_node_ref> m;
+
+  for( unsigned int i=0; i< l.size(); i++)
+    {
+      if( l[i]->type()== type)
+	m.push_back( l[i]);
+    }
+
+  return m;
 }
 
 
@@ -131,13 +167,9 @@ vcl_vector<gmvl_node_ref> gmvl_database::get_connected_nodes( const vcl_vector<g
 vcl_ostream &operator<<( vcl_ostream &os, const gmvl_database &db)
 {
   os << "gmvl_database:" << vcl_endl;
-
   os << "  nodes:" << vcl_endl;
-
   os << "    " << db.nodecache_ << vcl_endl;
-
   os << "  connections:" << vcl_endl;
-
   os << "    " << db.connectioncache_ << vcl_endl;
 
   return os;
