bae6e8497e270024c33ff8409dbf9a47797176c6
Unknown <remcvs_tgtjr_oxford>
2001-03-28 07:15:02 -0500

    psync.pl: abs() for int and long moved here

--- vcl/sunpro/vcl_cstdlib.h
+++ vcl/sunpro/vcl_cstdlib.h
@@ -6,4 +6,9 @@
 
 #include "../iso/vcl_cstdlib.h"
 
+#undef  vcl_abs
+#define vcl_abs vcl_abs
+inline int  vcl_abs(int  x) { return x >= 0    ? x : -x; }
+inline long vcl_abs(long x) { return x >= 0L   ? x : -x; }
+
 #endif // vcl_cstdlib_h_
