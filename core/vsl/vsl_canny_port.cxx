9d867c423bb9fdd2411a95d1ee768edbf2a9143a
Unknown <remcvs_tgtjr_oxford>
2001-04-03 08:34:15 -0400

    moved to oxl

--- core/vsl/vsl_canny_port.cxx
+++
@@ -1,8 +0,0 @@
-/*
-  fsm@robots.ox.ac.uk
-*/
-#ifdef __GNUC__
-#pragma implementation
-#endif
-#include "vsl_canny_port.h"
-
--- core/vsl/vsl_roi_window.cxx
+++
@@ -1,7 +0,0 @@
-/*
-  fsm@robots.ox.ac.uk
-*/
-#ifdef __GNUC__
-#pragma implementation
-#endif
-#include "vsl_roi_window.h"
