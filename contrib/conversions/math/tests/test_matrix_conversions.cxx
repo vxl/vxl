fdbab83560d40e1b86242a1a49f2e798c121d805
Unknown <targetjr>
2001-04-04 04:43:58 -0400

    preliminary math tests added

--- contrib/conversions/math/tests/test_matrix_conversions.cxx
+++ contrib/conversions/math/tests/test_matrix_conversions.cxx
@@ -1,8 +1,10 @@
 #include <math/CoolMatrix_to_vnl_matrix.h>
 #include <math/CoolVector_to_vnl_vector.h>
-#include <math/iue_matrix_to_vnl_matrix.h>
-#include <math/iue_vector_to_vnl_vector.h>
+#include <math/IUE_matrix_to_vnl_matrix.h>
+#include <math/IUE_vector_to_vnl_vector.h>
 #include <math/vnl_matrix_to_CoolMatrix.h>
-#include <math/vnl_matrix_to_iue_matrix.h>
+#include <math/vnl_matrix_to_IUE_matrix.h>
 #include <math/vnl_vector_to_CoolVector.h>
-#include <math/vnl_vector_to_iue_vector.h>
+#include <math/vnl_vector_to_IUE_vector.h>
+
+main() {}
