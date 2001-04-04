a9623a6e7cc9a0762c06542251bc6dab794d3203
Unknown <remcvs_tgtjr_oxford>
2001-04-04 05:21:38 -0400

    psync.pl: purge crud

--- core/vbl/vbl_fwd.h
+++ core/vbl/vbl_fwd.h
@@ -1,17 +1,10 @@
 #ifndef vbl_fwd_h_
 #define vbl_fwd_h_
 
-#ifdef __GNUG__
-#pragma interface
-#endif
-
-// point[23]d.h
-template <class type> class tmpl_point2D;
-typedef tmpl_point2D<float>  Point2D;
-typedef tmpl_point2D<int>    intPoint2D;
-typedef tmpl_point2D<float>  floatPoint2D;
-typedef tmpl_point2D<double> doublePoint2D;
-
-template <class T> struct fast_array2d;
+template <class T> class vbl_array_1d;
+export
+template <class T> class vbl_array_2d;
+export
+template <class T> class vbl_array_3d;
 
 #endif // vbl_fwd_h_
