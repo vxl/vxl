69aa9f731b1c340657ca3a5018e37afa57243826
Unknown <remcvs_tgtjr_oxford>
2001-04-04 15:03:01 -0400

    stlport for windows

---
+++ vcl/config.stlport.win32-vc60/vcl_config_manual.h
@@ -0,0 +1,13 @@
+#ifndef vcl_config_manual_h_
+#define vcl_config_manual_h_
+
+// This file is *not* generated.
+
+#define VCL_USE_NATIVE_STL 1
+
+#define VCL_USE_NATIVE_COMPLEX 1
+
+// Use STL port stl libraries
+#define VCL_USE_STLPORT 1
+
+#endif
