7c1b95b96c943b9c33536fef19d453fffd624ceb
Unknown <remcvs_tgtjr_oxford>
2001-04-10 15:30:22 -0400

    psync.pl (oxford): vsl -> osl

--- contrib/oxl/xcv/xcv_processing.h
+++ contrib/oxl/xcv/xcv_processing.h
@@ -15,10 +15,10 @@
 //--------------------------------------------------------------------------------
 
 class vgui_menu;
-class vsl_harris_params;
-class vsl_canny_ox_params;
+class osl_harris_params;
+class osl_canny_ox_params;
 class vgui_easy2D;
-class vsl_edge;
+class osl_edge;
 
 class xcv_processing
 {
