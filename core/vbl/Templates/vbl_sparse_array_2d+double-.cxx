9f2dc0551776db2871d5358cc3f0d4166a849aeb
Unknown <uid1820>
2001-04-11 06:13:12 -0400

    manual instantiation stuff

--- core/vbl/Templates/vbl_sparse_array_2d+double-.cxx
+++ core/vbl/Templates/vbl_sparse_array_2d+double-.cxx
@@ -1,3 +1,2 @@
 #include <vbl/vbl_sparse_array_2d.txx>
-#include <vcl_map.txx>
 VBL_SPARSE_ARRAY_2D_INSTANTIATE(double);
--- core/vbl/Templates/vbl_sparse_array_3d+double-.cxx
+++ core/vbl/Templates/vbl_sparse_array_3d+double-.cxx
@@ -1,20 +1,2 @@
 #include <vbl/vbl_sparse_array_3d.txx>
 VBL_SPARSE_ARRAY_3D_INSTANTIATE(double);
-
-#if 0
-int main()
-{
-  vbl_sparse_array_3d<double> x;
-
-  x(1,2,3) = 1.23;
-  x(100,200,3) = 100.2003;
-
-  vcl_cout << "123 = " << x(1,2,3) << vcl_endl;
-  vcl_cout << "222 = " << x(2,2,2) << vcl_endl;
-
-  vcl_cout << "333 is full? " << x.fullp(3,3,3) << vcl_endl;
-
-  vcl_cout << x;
-  return 1;
-}
-#endif
