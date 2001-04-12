4338af75de1e81aa98ef8fc177aab9af639333c5
Tim Cootes <timcootes@users.sourceforge.net>
2001-04-12 13:09:06 -0400

    Added data wrapper object

---
+++ contrib/mul/mbl/mbl_data_wrapper.h
@@ -0,0 +1,72 @@
+#ifndef mbl_data_wrapper_h
+#define mbl_data_wrapper_h
+#ifdef __GNUC__
+#pragma interface
+#endif
+
+
+//:
+// \file
+// \author Tim Cootes
+// \brief A wrapper to provide access to sets of objects
+
+//: Base class for objects which can return a set of objects, one at a time
+//  This is an iterator-like object.  However, unlike STL, generality is
+//  obtained by deriving from the class rather than providing a similar
+//  interface.
+//
+//  In particular one can query the object to find out how many examples
+//  to expect.
+//
+//  A typical use is to allow clients to build
+//  models from sets of data without having to hold all the data in memory
+//  at once.
+//
+//  Example:
+//  \verbatim
+//  vnl_vector<double> my_sum(mbl_data_wrapper<vnl_vector<double> >& data)
+//  {
+//    data.reset();
+//    vnl_vector<double> sum = data.current();
+//    while (data.next())
+//      sum += data.current();
+//  }
+//  \endverbatim
+//
+//  Note: It should be fairly simple to provide an iterator type object
+//  which steps through the examples in a standard STL way by using this class.
+template<class T>
+class mbl_data_wrapper {
+public:
+	//: Default constructor
+	mbl_data_wrapper();
+
+	//: Default destructor
+	virtual ~mbl_data_wrapper();
+
+	//: Number of objects available
+	virtual int size() const = 0;
+
+	//: Reset so that current() returns first object
+	virtual void reset() = 0;
+
+	//: Return current object
+	virtual const T& current() = 0;
+
+	//: Move to next object, returning true if is valid
+	virtual bool next() = 0;
+
+		//: Return current index
+		//  First example has index 0
+	virtual int index() const =0;
+
+		//: Move to element n
+		//  First example has index 0
+	virtual void set_index(int n);
+
+		//: Create copy on heap and return base pointer
+	virtual mbl_data_wrapper< T >* clone() const = 0;
+
+};
+
+#endif // mbl_data_wrapper_h
---
+++ contrib/mul/mbl/mbl_data_wrapper.txx
@@ -0,0 +1,35 @@
+#include <mbl/mbl_data_wrapper.h>
+#include <vcl_iostream.h>
+#include <vcl_cstdlib.h>
+
+//: Default constructor
+template<class T>
+mbl_data_wrapper<T>::mbl_data_wrapper()
+{
+}
+
+//: Default destructor
+template<class T>
+mbl_data_wrapper<T>::~mbl_data_wrapper()
+{
+}
+
+//: Move to element n
+//  First example has index 0
+template<class T>
+void mbl_data_wrapper<T>::set_index(int n)
+{
+  if ((n<0) || (n>=size()))
+  {
+    vcl_cerr<<"TC_VectorDataBase::set_index(n) ";
+    vcl_cerr<<"n = "<<n<<" not in range 0 .. "<<size()-1<<vcl_endl;
+    vcl_abort();
+  }
+
+  if (index()==n) return;
+  if (index()>n) reset();
+  while (index()!=n) next();
+}
+
+#define MBL_DATA_WRAPPER_INSTANTIATE(T) \
+template class mbl_data_wrapper< T >;
