434f65b0b305a7b83a78b578c7f6a047235a646a
Geoffrey Cross <geoffreycross@users.sourceforge.net>
2001-04-17 10:09:57 -0400

    gesync:

---
+++ contrib/gel/mrc/vpgl_tests/vpgl_tests.h
@@ -0,0 +1,15 @@
+#include <vcl/vcl_string.h>
+#include <vcl_cassert.h>
+#include <vbl/vbl_arg.h>
+#include <vcl_iosfwd.h>
+#include <vcl_vector.h>
+#include <vcl_utility.h>
+#include <vbl/vbl_ref_count.h>
+#include <vbl/vbl_ref_count.h>
+#include <vnl/vnl_c_vector.h>
+#include <vnl/vnl_vector.h>
+
+#include <vpgl/vpgl_basic_camera.h>
+#include <vpgl/vpgl_matrix_camera.h>
+#include <vpgl/vpgl_perspective_camera.h>
+
