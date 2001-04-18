3d02264d9f229fc88ac1dda47a54fcc13f00968f
Unknown <remcvs_tgtjr_oxford>
2001-04-18 08:34:03 -0400

    psync.pl (oxford): unify

--- core/vnl/tests/test_levenberg_marquardt.cxx
+++ core/vnl/tests/test_levenberg_marquardt.cxx
@@ -3,41 +3,35 @@
 */
 #include <vcl_cmath.h>
 #include <vcl_iostream.h>
+
+#include <vnl/vnl_test.h>
 #include <vnl/vnl_least_squares_function.h>
 #include <vnl/algo/vnl_levenberg_marquardt.h>
 
-struct vnl_rosenbrock_nograd : vnl_least_squares_function
+struct vnl_rosenbrock : vnl_least_squares_function
 {
-  vnl_rosenbrock_nograd(): vnl_least_squares_function(2, 2, no_gradient) {}
+  vnl_rosenbrock(bool with_grad): vnl_least_squares_function(2, 2, with_grad ? use_gradient : no_gradient) {}
   
   void f(vnl_vector<double> const &x, vnl_vector<double> &y) {
-    y[0] = 10*(x[1] - x[0]*x[0]);
-    y[1] = 1 - x[0];
-  }
-};
-
-struct vnl_rosenbrock_grad : vnl_least_squares_function
-{
-  vnl_rosenbrock_grad(): vnl_least_squares_function(2, 2, use_gradient) {}
-  
-  void f(vnl_vector<double> const &x, vnl_vector<double> &y) {
+    //vnl_test_assert("size of x", x.size() == 2);
+    //vnl_test_assert("size of y", y.size() == 2);
     y[0] = 10*(x[1] - x[0]*x[0]);
     y[1] = 1 - x[0];
   }
   
   void gradf(vnl_vector<double> const &x, vnl_matrix<double> &J) {
+    //vnl_test_assert("size of x", x.size() == 2);
+    //vnl_test_assert("size of J", J.rows() == 2 && J.cols() == 2);
     J[0][0] = -20 * x[0]; J[0][1] = 10;
     J[1][0] = -1;         J[1][1] = 0;
   }
 };
 
-//----------------------------------------------------------------------
-
-#include <vnl/vnl_test.h>
-
 static
-void do_test(vnl_least_squares_function &f)
+void do_test(bool with_grad)
 {
+  vnl_rosenbrock f(with_grad);
+  
   vnl_vector<double> x0(2);
   x0[0] =  2.7;
   x0[1] = -1.3;
@@ -50,23 +44,19 @@ void do_test(vnl_least_squares_function &f)
     lm.minimize_using_gradient(x1);
   else
     lm.minimize_without_gradient(x1);
+  lm.diagnose_outcome(vcl_cout);
   vcl_cout << "x1 = " << x1 << vcl_endl;
   
   double err = vcl_abs(x1[0] - 1) + vcl_abs(x1[1] - 1);
+  vcl_cout << "err = " << err << vcl_endl;
   vnl_test_assert("converged to (1, 1)", err <= 1e-10);
 }
 
 static
 void do_test()
 {
-  {
-    vnl_rosenbrock_nograd f;
-    do_test(f);
-  }
-  {
-    vnl_rosenbrock_grad f;
-    do_test(f);
-  }
+  do_test(true);
+  do_test(false);
 }
 
 TESTMAIN(do_test);
