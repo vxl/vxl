f20f9f1168efd2a2c2b334879ed4e55c90cb4533
Unknown <remcvs_tgtjr_oxford>
2001-04-25 05:22:05 -0400

    *** empty log message ***

---
+++ core/vnl/vnl_complexify.h
@@ -0,0 +1,13 @@
+#ifndef vnl_complexify_h_
+#define vnl_complexify_h_
+#ifdef __GNUC__
+#pragma interface
+#endif
+/*
+  fsm@robots.ox.ac.uk
+*/
+
+// See this file for further information:
+#include <vnl/vnl_complex_ops.h>
+
+#endif
