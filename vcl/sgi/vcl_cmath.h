7795a9a76d42726de2874bc23c5d300cba2b3f1d
Unknown <targetjr>
2001-04-25 17:26:58 -0400

    add long double versions

--- vcl/sgi/vcl_cmath.h
+++ vcl/sgi/vcl_cmath.h
@@ -12,40 +12,47 @@
 
 # undef  vcl_acos
 # define vcl_acos vcl_acos
-inline float  vcl_acos(float  x) { return ::acos(x); }
-inline double vcl_acos(double x) { return ::acos(x); }
+inline float       vcl_acos(float       x) { return ::acos(x); }
+inline double      vcl_acos(double      x) { return ::acos(x); }
+inline long double vcl_acos(long double x) { return ::acos(x); }
 
 # undef  vcl_asin
 # define vcl_asin vcl_asin
-inline float  vcl_asin(float  x) { return ::asin(x); }
-inline double vcl_asin(double x) { return ::asin(x); }
+inline float       vcl_asin(float       x) { return ::asin(x); }
+inline double      vcl_asin(double      x) { return ::asin(x); }
+inline long double vcl_asin(long double x) { return ::asin(x); }
 
 # undef  vcl_atan
 # define vcl_atan vcl_atan
-inline float  vcl_atan(float  x) { return ::atan(x); }
-inline double vcl_atan(double x) { return ::atan(x); }
+inline float       vcl_atan(float       x) { return ::atan(x); }
+inline double      vcl_atan(double      x) { return ::atan(x); }
+inline long double vcl_atan(long double x) { return ::atan(x); }
 
 # undef  vcl_atan2
 # define vcl_atan2 vcl_atan2
-inline float  vcl_atan2(float  y, float  x) { return ::atan2(y, x); }
-inline double vcl_atan2(double y, double x) { return ::atan2(y, x); }
+inline float       vcl_atan2(float       y,float       x){return ::atan2(y, x);}
+inline double      vcl_atan2(double      y,double      x){return ::atan2(y, x);}
+inline long double vcl_atan2(long double y,long double x){return ::atan2(y, x);}
 
 # define vcl_ceil  ::ceil
 
 # undef  vcl_cos
 # define vcl_cos vcl_cos
-inline float  vcl_cos(float  x) { return ::cos(x); }
-inline double vcl_cos(double x) { return ::cos(x); }
+inline float       vcl_cos(float       x) { return ::cos(x); }
+inline double      vcl_cos(double      x) { return ::cos(x); }
+inline long double vcl_cos(long double x) { return ::cos(x); }
 
 # undef  vcl_cosh
 # define vcl_cosh vcl_cosh
-inline float  vcl_cosh(float  x) { return ::cosh(x); }
-inline double vcl_cosh(double x) { return ::cosh(x); }
+inline float       vcl_cosh(float       x) { return ::cosh(x); }
+inline double      vcl_cosh(double      x) { return ::cosh(x); }
+inline long double vcl_cosh(long double x) { return ::cosh(x); }
 
 # undef  vcl_exp
 # define vcl_exp vcl_exp
-inline float  vcl_exp(float  x) { return ::exp(x); }
-inline double vcl_exp(double x) { return ::exp(x); }
+inline float       vcl_exp(float       x) { return ::exp(x); }
+inline double      vcl_exp(double      x) { return ::exp(x); }
+inline long double vcl_exp(long double x) { return ::exp(x); }
 
 # define vcl_fabs  ::fabs
 # define vcl_floor ::floor
@@ -55,29 +62,35 @@ inline double vcl_exp(double x) { return ::exp(x); }
 
 # undef  vcl_log
 # define vcl_log vcl_log
-inline float  vcl_log(float  x) { return ::log(x); }
-inline double vcl_log(double x) { return ::log(x); }
+inline float       vcl_log(float       x) { return ::log(x); }
+inline double      vcl_log(double      x) { return ::log(x); }
+inline long double vcl_log(long double x) { return ::log(x); }
 
 # undef vcl_log10
 # define vcl_log10 vcl_log10
-inline float  vcl_log10(float  x) { return ::log10(x); }
-inline double vcl_log10(double x) { return ::log10(x); }
+inline float       vcl_log10(float       x) { return ::log10(x); }
+inline double      vcl_log10(double      x) { return ::log10(x); }
+inline long double vcl_log10(long double x) { return ::log10(x); }
+
 # define vcl_modf  ::modf
 
 # undef  vcl_pow
 # define vcl_pow vcl_pow
-inline float  vcl_pow(float  x, float  a) { return ::pow(x, a); }
-inline double vcl_pow(double x, double a) { return ::pow(x, a); }
+inline float       vcl_pow(float       x, float       a) { return ::pow(x, a); }
+inline double      vcl_pow(double      x, double      a) { return ::pow(x, a); }
+inline long double vcl_pow(long double x, long double a) { return ::pow(x, a); }
 
 # undef  vcl_sin
 # define vcl_sin vcl_sin
-inline float  vcl_sin(float  x) { return ::sin(x); }
-inline double vcl_sin(double x) { return ::sin(x); }
+inline float       vcl_sin(float       x) { return ::sin(x); }
+inline double      vcl_sin(double      x) { return ::sin(x); }
+inline long double vcl_sin(long double x) { return ::sin(x); }
 
 # undef  vcl_sinh
 # define vcl_sinh vcl_sinh
-inline float  vcl_sinh(float  x) { return ::sinh(x); }
-inline double vcl_sinh(double x) { return ::sinh(x); }
+inline float       vcl_sinh(float       x) { return ::sinh(x); }
+inline double      vcl_sinh(double      x) { return ::sinh(x); }
+inline long double vcl_sinh(long double x) { return ::sinh(x); }
 
 # undef  vcl_sqrt
 # define vcl_sqrt vcl_sqrt
@@ -87,13 +100,15 @@ inline long double vcl_sqrt(long double x) { return ::sqrt(x); } // ?
 
 # undef  vcl_tan
 # define vcl_tan vcl_tan
-inline float  vcl_tan(float  x) { return ::tan(x); }
-inline double vcl_tan(double x) { return ::tan(x); }
+inline float       vcl_tan(float       x) { return ::tan(x); }
+inline double      vcl_tan(double      x) { return ::tan(x); }
+inline long double vcl_tan(long double x) { return ::tan(x); }
 
 # undef  vcl_tanh
 # define vcl_tanh vcl_tanh
-inline float  vcl_tanh(float  x) { return ::tanh(x); }
-inline double vcl_tanh(double x) { return ::tanh(x); }
+inline float       vcl_tanh(float       x) { return ::tanh(x); }
+inline double      vcl_tanh(double      x) { return ::tanh(x); }
+inline long double vcl_tanh(long double x) { return ::tanh(x); }
 
 # undef  vcl_abs
 # define vcl_abs vcl_abs
