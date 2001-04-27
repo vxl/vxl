af50f128fe7fe9a07d505b8d098830043490bd1d
Unknown <targetjr>
2001-04-26 23:35:19 -0400

    two remaining files moved from MViewBasics

---
+++ contrib/oxl/mvl/PMatrixAffine.cxx
@@ -0,0 +1,14 @@
+#ifdef __GNUC__
+#pragma implementation
+#endif
+//
+// Class: PMatrixAffine
+// Author: Andrew W. Fitzgibbon, Oxford RRG
+// Created: 24 Feb 97
+// Modifications:
+//   970224 AWF Initial version.
+//   000621 Peter Vanroose removed implementation for compiler-supplied methods
+//-----------------------------------------------------------------------------
+
+#include "PMatrixAffine.h"
+
---
+++ contrib/oxl/mvl/PMatrixEuclidean.cxx
@@ -0,0 +1,21 @@
+#ifdef __GNUC__
+#pragma implementation "PMatrixEuclidean.h"
+#endif
+//
+// Class: PMatrixEuclidean
+// Author: Andrew W. Fitzgibbon, Oxford RRG
+// Created: 24 Feb 97
+// Modifications:
+//   970224 AWF Initial version.
+//   000621 Peter Vanroose removed implementation for compiler-supplied methods
+//
+//-----------------------------------------------------------------------------
+
+#include "PMatrixEuclidean.h"
+
+bool PMatrixEuclidean::decompose_to_intrinsic_extrinsic_params (
+	vnl_matrix<double> *camera_matrix,
+	vnl_matrix<double> *R,
+	vnl_vector<double> *t) {
+  return false; // FIXME: not yet implemented
+};
