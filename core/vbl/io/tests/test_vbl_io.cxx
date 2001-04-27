011b912ff64c487e8fe4f5ce94d134381e7a4516
Unknown <targetjr>
2001-04-27 00:32:10 -0400

    test_user_info_io() added

--- core/vbl/io/tests/test_vbl_io.cxx
+++ core/vbl/io/tests/test_vbl_io.cxx
@@ -9,10 +9,9 @@
 #include <vbl/io/tests/test_array_1d_io.cxx>
 #include <vbl/io/tests/test_array_2d_io.cxx>
 #include <vbl/io/tests/test_array_3d_io.cxx>
-//#include <vbl/io/tests/test_user_info_io.cxx>
+#include <vbl/io/tests/test_user_info_io.cxx>
 #include <vbl/io/tests/test_sparse_array_io.cxx>
 #include <vbl/io/tests/golden_test_vbl_io.cxx>
-
 
 
 #undef TESTMAIN
@@ -26,7 +25,7 @@ void run_test_vbl_io()
   test_array_1d_io();
   test_array_2d_io();
   test_array_3d_io();
-//  test_user_info_io();
+  test_user_info_io();
   test_sparse_array_io();
   golden_test_vbl_io();
 }
