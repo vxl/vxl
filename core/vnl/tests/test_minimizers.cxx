0dc73ea8ee946763103d7ebc24ee5da1c2e36ae1
Unknown <remcvs_tgtjr_oxford>
2001-04-28 06:29:24 -0400

    psync.pl: dont blather on stderr

--- core/vnl/tests/test_minimizers.cxx
+++ core/vnl/tests/test_minimizers.cxx
@@ -14,7 +14,7 @@ struct cubic : public vnl_cost_function {
   cubic() : vnl_cost_function(1) {}
 
   double f(const vnl_vector<double>& x) {
-    vcl_cerr << " " << x[0];
+    vcl_cout << " " << x[0];
     return (2 - x[0]) * (2 - x[0]) + 10;
   }
 };
@@ -24,14 +24,14 @@ void test_minimizers()
   cubic c;
   vnl_brent b(&c);
   double x = 77;
-  vcl_cerr << "brent1: ";
+  vcl_cout << "brent1: ";
   b.minimize_given_bounds(-100, x, 100, 1e-6, &x);
-  vcl_cerr << vcl_endl;
+  vcl_cout << vcl_endl;
   TEST("brent1", vcl_fabs(x - 2) < 1e-5, true);
-  vcl_cerr << "brent2: ";
+  vcl_cout << "brent2: ";
   x = 77;
   x = b.minimize(x);
-  vcl_cerr << vcl_endl;
+  vcl_cout << vcl_endl;
   TEST("brent2", vcl_fabs(x - 2) < 1e-5, true);
 }
 
