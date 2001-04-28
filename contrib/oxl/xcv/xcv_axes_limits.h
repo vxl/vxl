2ff49ff72ec61586c01b010e8c01675abdaa5202
Unknown <remcvs_tgtjr_oxford>
2001-04-28 06:28:19 -0400

    psync.pl: missing comment after #endif

---
+++ contrib/oxl/xcv/xcv_axes_limits.h
@@ -0,0 +1,46 @@
+#ifndef xcv_axes_limits_h_
+#define xcv_axes_limits_h_
+//
+// This is xcv/xcv_axes_limits.h
+//
+//: Given a range (from the number low to high) this class computes
+//  a suitable spacing, start value and end value for tick marks
+//  the axes of a graph.
+//
+// \file 
+// \author   Andrew Fitzgibbon <andrewfg@ed.ac.uk>
+// \date     23 Apr 96
+// \verbatim
+//   Modifications:
+//     Andrew Fitzgibbon 23-APR-1996   Initial version.
+//     K.Y.McGaul        26-APR-2001   Converted to vxl.
+// \endverbatim
+
+class xcv_axes_limits {
+public:
+  //: Lowest value to be plotted.
+  double  low;
+  //: Highest value to be plotted.
+  double  high;
+  //: Interval between tick marks on the axes.
+  double  tick_spacing;
+  //: Start of axes numbering.
+  double  tick_start;
+  //: End of axes numbering.
+  double  tick_end;
+  //: Recommended number of tick marks.
+  int     tick_n;
+  double  exp;
+  int     exp_n;
+
+  //: Constructor.
+  xcv_axes_limits();
+  //: Set the lowest and highest plotted values. 
+  void set_range(double low, double high);
+  //: Compute the start, end and spacing for the tick marks.
+  void calc_ticks();
+  //: Compute the spacing between tick marks.
+  double calc_tick_spacing();
+};
+
+#endif // xcv_axes_limits_h_
