d1bd2ca98b0201afde3d13228d417aea9984f697
Unknown <remcvs_tgtjr_oxford>
2001-04-28 06:28:12 -0400

    psync.pl (oxford): KYM: remove waffle accidentally added

--- contrib/oxl/xcv/xcv_display.cxx
+++ contrib/oxl/xcv/xcv_display.cxx
@@ -1,30 +1,51 @@
-//-----------------------------------------------------------------------------
-// .DESCRIPTION:
-//   See xcv_display.h.
-//-----------------------------------------------------------------------------
+//
+// This is xcv/xcv_display.cxx
+// See xcv_display.h for a description of this file.
+//
+// \author K.Y.McGaul
+// \verbatim
+//  Modifications:
+//    K.Y.McGaul  27-JAN-2000    Initial version.
+// \endverbatim
+//
 
 #include <vcl_cstdio.h> // sprintf()
 #include <vcl_string.h>
 #include <vcl_iostream.h>
 #include <vcl_vector.h>
+#include <vcl_cmath.h>
+
+#include <vil/vil_memory_image_of.h>
+#include <vil/vil_image_as.h>
+#include <vnl/vnl_math.h>
+
 #include <vgui/vgui.h>
 #include <vgui/vgui_menu.h>
+#include <vgui/vgui_window.h>
+#include <vgui/vgui_tableau.h>
+#include <vgui/vgui_adaptor.h>
 #include <vgui/vgui_dialog.h>
 #include <vgui/vgui_enhance.h>
 #include <vgui/vgui_easy2D.h>
 #include <vgui/vgui_viewer2D.h>
 #include <vgui/vgui_composite.h>
 #include <vgui/vgui_rubberbander.h>
+#include <vgui/vgui_shell_tableau.h>
+#include <vgui/vgui_text_tableau.h>
 #include <vgui/vgui_roi_tableau.h>
 #include <vgui/vgui_event_server.h>
+
 #include <xcv/xcv_image_tableau.h>
-#include "xcv_display.h"
+#include <xcv/xcv_picker_tableau.h>
+#include <xcv/xcv_axes_tableau.h>
+#include <xcv/xcv_display.h>
 
 //vgui_roi_tableau_make_roi *xcv_display::roi_tableau_client_ = 0;
 extern void post_to_status_bar(const char*);
 extern void get_current(unsigned*, unsigned*);
 extern vcl_vector<xcv_image_tableau_sptr> get_image_list();
 extern vgui_easy2D_sptr get_easy2D_at(unsigned, unsigned);
+extern xcv_picker_tableau_sptr get_picker_tableau_at(unsigned, unsigned);
 extern vgui_composite_sptr get_composite_at(unsigned, unsigned);
 extern vgui_viewer2D_sptr get_viewer2D_at(unsigned, unsigned);
 extern xcv_image_tableau_sptr get_image_tableau_at(unsigned, unsigned);
@@ -42,7 +63,7 @@ static xcv_image_tableau_sptr img;
 static vgui_easy2D_sptr easy;
 
 //-----------------------------------------------------------------------------
-//: Centre the current image.
+//: Centre the current image inside the frame.
 //-----------------------------------------------------------------------------
 void xcv_display::centre_image()
 {
@@ -57,8 +78,8 @@ void xcv_display::centre_image()
 }
 
 //-----------------------------------------------------------------------------
-//: Toggle display of the enhance lens.
-//   This is done by slotting in an enhance tableau above the image tableau.
+//: Turn the enhance lens on/off.
+//  This is done by slotting in an enhance tableau above the image tableau.
 //-----------------------------------------------------------------------------
 void xcv_display::toggle_enhance()
 {
@@ -105,8 +126,8 @@ void xcv_display::toggle_enhance()
 }
 
 //-----------------------------------------------------------------------------
-//: Toggle display of the magnify glass.
-//   This is done by slotting in an enhance tableau above the image tableau.
+//: Turn the magnify glass on/off.
+//  This is done by slotting in an enhance tableau above the image tableau.
 //-----------------------------------------------------------------------------
 void xcv_display::toggle_magnify()
 {
@@ -139,7 +160,7 @@ void xcv_display::toggle_magnify()
   }
 }
 //-----------------------------------------------------------------------------
-//: Define region of interest
+//: Define a region of interest
 //-----------------------------------------------------------------------------
 void xcv_display::make_roi()
 {
@@ -165,7 +186,7 @@ void xcv_display::make_roi()
   //roi_tableau_client_ = 0;
 }
 //-----------------------------------------------------------------------------
-//: Remove region of interest
+//: Remove the region of interest
 //-----------------------------------------------------------------------------
 void xcv_display::remove_roi()
 {
@@ -184,6 +205,98 @@ void xcv_display::remove_roi()
 }
 
 //-----------------------------------------------------------------------------
+//: Generates intensity values along a line.
+//  Given an image, a start point and an end point (both
+//  in image coordinates), this method generates two arrays representing
+//  the coordinates of the image pixels in the slice, and one array
+//  containing the intensity values along the line.
+//-----------------------------------------------------------------------------
+void xcv_display::line_profile(const vil_image& src, float x0, float y0, float x1,float y1, 
+  int num_points, float* xvals, float* yvals, float* ivals)
+{
+  int sdepth = src.bits_per_component();
+
+  if ((sdepth != 8) /*&& (sdepth != 16)*/)
+  {
+    vcl_cerr << sdepth << " bit profile not yet implemented." << vcl_endl;
+    return;
+  }
+
+  float x_step = (x1 - x0)/(num_points-1);
+  float y_step = (y1 - y0)/(num_points-1);
+
+  for(int i = num_points-1; i>=0; i--)
+  {
+    xvals[i] = x0 + x_step*i;
+    yvals[i] = y0 + y_step*i;
+ 
+    // copy input image to byte buffer
+    //vil_memory_image_of<vil_byte> memimg(src);
+    vil_memory_image_of<vil_byte> memimg;
+    memimg.resize(src.width(), src.height());
+    vil_image_as_byte(src).get_section(memimg.get_buffer(), 0, 0, src.width(), src.height()); 
+
+    if (sdepth == 8)
+    {
+      float imgval = (float)memimg(vnl_math_rnd(xvals[i]), vnl_math_rnd(yvals[i]));
+      ivals[i] = imgval;
+    }
+    /*else if (sdepth == 16)
+      ivals[i] 
+        = (int)(*(unsigned short*)(memimg(vnl_math_rnd(xvals[i]), vnl_math_rnd(yvals[i]))));*/
+  }
+}
+
+//-----------------------------------------------------------------------------
+//: Show a profile of the image intensity on a user-selected line.
+//-----------------------------------------------------------------------------
+void xcv_display::show_line_slice()
+{
+  unsigned col, row;
+  get_current(&col, &row);
+  xcv_picker_tableau_sptr picker = get_picker_tableau_at(col, row);
+  if (!picker) return;
+
+  float fx0,fy0,fx1,fy1; 
+  picker->pick_line(&fx0, &fy0, &fx1, &fy1);
+
+  vil_image img;
+  if (!get_image_at(&img, col, row))
+    return;
+
+  int num_points 
+    = (int)(vnl_math_rnd(sqrt((double)((fx1-fx0)*(fx1-fx0)+ (fy1-fy0)*(fy1-fy0)))));
+
+  // Avoid divide by zero
+  if(!num_points) num_points = 10;
+
+  float* x = new float[num_points+1];
+  float* y = new float[num_points+1];
+  float* val = new float[num_points+1];
+
+  line_profile(img, fx0, fy0, fx1, fy1, num_points+1, x, y, val);
+
+  char tmp_heading[200];
+  sprintf(tmp_heading, "Image Intensity Profile across (%.0f, %.0f) to (%.0f, %.0f)",
+    fx0, fy0, fx1, fy1);
+
+  xcv_axes_tableau_new axes(tmp_heading, "X", "Intensity");
+  for (int i=0; i<num_points+1; i++)
+    axes->add_point(x[i], val[i]);
+  axes->compute_axes(); 
+  vgui_viewer2D_new viewer(axes);
+
+  vgui_window *popup = vgui::produce_window(700, 500, "Image Line Profile");
+  if (popup)
+  {
+    popup->set_statusbar(false);
+    popup->get_adaptor()->set_tableau(vgui_shell_tableau_new(viewer));
+    popup->show();
+  }
+
+}
+
+//-----------------------------------------------------------------------------
 //: Creates a menu containing all the functions in this file.
 //-----------------------------------------------------------------------------
 vgui_menu xcv_display::create_display_menu()
@@ -196,6 +309,7 @@ vgui_menu xcv_display::create_display_menu()
   display_menu.separator();
   display_menu.add("Make ROI",make_roi);
   display_menu.add("Remove ROI",remove_roi);
+  display_menu.add("Show line slice", show_line_slice);
   return display_menu;
 }
 
