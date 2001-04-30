7c6fe5a1880f473e4804ddf83221ebf4263b7e19
Unknown <targetjr>
2001-04-30 01:52:40 -0400

    added output after reading back

--- core/vnl/io/tests/test_vector_io.cxx
+++ core/vnl/io/tests/test_vector_io.cxx
@@ -12,45 +12,43 @@
 
 void test_vector_double_io()
 {
-    vcl_cout << "***********************" << vcl_endl;
-    vcl_cout << "Testing vnl_vector<double> io" << vcl_endl;
-    vcl_cout << "***********************" << vcl_endl;  
-    //// test constructors, accessors
-    const int n = 50;
-    vnl_vector<double> v_out(n), v_in;
+  vcl_cout << "*****************************" << vcl_endl;
+  vcl_cout << "Testing vnl_vector<double> io" << vcl_endl;
+  vcl_cout << "*****************************" << vcl_endl;
+  //// test constructors, accessors
+  const int n = 50;
+  vnl_vector<double> v_out(n), v_in;
 
-    for (int i=0; i<n; i++)
-    {
-        v_out(i) = (double)(i*i);
-    }
+  for (int i=0; i<n; i++)
+  {
+    v_out(i) = (double)(i*i);
+  }
 
-    
-    vsl_b_ofstream bfs_out("vnl_vector_test_double_io.bvl.tmp");
-    TEST ("Created vnl_vector_test_double_io.bvl.tmp for writing",
-             (!bfs_out), false);
-    vsl_b_write(bfs_out, v_out);
-    bfs_out.close();        
-        
-    vsl_b_ifstream bfs_in("vnl_vector_test_double_io.bvl.tmp");
-    TEST ("Opened vnl_vector_test_double_io.bvl.tmp for reading",
-             (!bfs_in), false);
-    vsl_b_read(bfs_in, v_in);
-    bfs_in.close();     
-        
-    
-    
-    TEST ("v_out == v_in", v_out == v_in, true);
+  vcl_cout << "before saving:\t"; vsl_print_summary(vcl_cout, v_out);
 
+  vsl_b_ofstream bfs_out("vnl_vector_test_double_io.bvl.tmp");
+  TEST ("Created vnl_vector_test_double_io.bvl.tmp for writing",
+        (!bfs_out), false);
+  vsl_b_write(bfs_out, v_out);
+  bfs_out.close();
+
+  vcl_cout << "after saving:\t"; vsl_print_summary(vcl_cout, v_out);
+
+  vsl_b_ifstream bfs_in("vnl_vector_test_double_io.bvl.tmp");
+  TEST ("Opened vnl_vector_test_double_io.bvl.tmp for reading",
+        (!bfs_in), false);
+  vsl_b_read(bfs_in, v_in);
+  bfs_in.close();
+
+  vcl_cout << "after reading in:\t"; vsl_print_summary(vcl_cout, v_in);
+
+  TEST ("v_out == v_in", v_out == v_in, true);
 
   vsl_print_summary(vcl_cout, v_out);
   vcl_cout << vcl_endl;
-        
 }
 
 
-
-
-   
 void test_vector_prime()
 {
   test_vector_double_io();
