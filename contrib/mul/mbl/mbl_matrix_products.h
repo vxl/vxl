599b9f6747bc431992fbd8610caabe10d29e7cd2
Unknown <targetjr>
2001-05-02 03:19:38 -0400

    <TAB>s replaced by spaces

--- contrib/mul/mbl/mbl_matrix_products.h
+++ contrib/mul/mbl/mbl_matrix_products.h
@@ -4,7 +4,6 @@
 #pragma interface
 #endif
 
-
 //:
 // \file
 // \author Tim Cootes
@@ -19,30 +18,30 @@ void mbl_matrix_product(vnl_matrix<double>& AB, const vnl_matrix<double>& A,
 
 //: Compute product ABt = A * B.transpose()
 void mbl_matrix_product_a_bt(vnl_matrix<double>& ABt, const vnl_matrix<double>& A,
-                   const vnl_matrix<double>& B);
+                             const vnl_matrix<double>& B);
 
 //: Compute ABt = A * B.transpose() using only first n_cols columns of A and B
 void mbl_matrix_product_a_bt(vnl_matrix<double>& ABt,
-                   const vnl_matrix<double>& A,
-                   const vnl_matrix<double>& B,
-				   int n_cols);
+                             const vnl_matrix<double>& A,
+                             const vnl_matrix<double>& B,
+                             int n_cols);
 
 //: Compute product AtB = A.transpose() * B
 //  Result is of size A.cols() x B.cols()
 void mbl_matrix_product_at_b(vnl_matrix<double>& AtB, const vnl_matrix<double>& A,
-                   const vnl_matrix<double>& B);
+                             const vnl_matrix<double>& B);
 
 //: Compute AtB = A.transpose() * B , using first ncols_a cols of A
 //  Result is of size ncols_a x B.cols()
 void mbl_matrix_product_at_b(vnl_matrix<double>& AtB, const vnl_matrix<double>& A,
-                   const vnl_matrix<double>& B, int ncols_a);
+                             const vnl_matrix<double>& B, int ncols_a);
 
 //: ADB = A * D * B where D is diagonal with elements d
 // A, d, and B should be compatible sizes, ADB will
 // be resized to fit.
 void mbl_matrix_product_adb(vnl_matrix<double>& ADB,
-                             const vnl_matrix<double>& A,
-                             const vnl_vector<double>& d,
-                             const vnl_matrix<double>& B);
+                            const vnl_matrix<double>& A,
+                            const vnl_vector<double>& d,
+                            const vnl_matrix<double>& B);
 
 #endif // mbl_matrix_product_h
