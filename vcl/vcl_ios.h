b87862c017ae575130129be912caf22d090d701e
Ian Scott <scottim@users.sourceforge.net>
2001-05-03 13:20:03 -0400

    previous vcl_ios.h , did not actully include ios.h It needs to in order to pick up manipulators fixed, scientific, etc.

--- vcl/vcl_ios.h
+++ vcl/vcl_ios.h
@@ -12,4 +12,13 @@
 // FIXME
 #include "vcl_iostream.h"
 
+#if defined(VCL_SGI_CC_720)
+# include "sgi/vcl_ios.h"
+
+#else // -------------------- ISO
+# include "iso/vcl_ios.h"
+#endif
+
+
+
 #endif // vcl_ios_h_
