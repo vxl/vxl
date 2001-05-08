be0d5beb0856099a9866dfee117e63bb3ed5b5af
Geoffrey Cross <geoffreycross@users.sourceforge.net>
2001-05-08 14:33:22 -0400

    gesync:

---
+++ README.cmake
@@ -0,0 +1,4 @@
+The config files and such for CMake lie in $VXLROOT/config.cmake. The
+CMake source is currently not in the vxl repository, since it is under
+active development. The source can be cvs'd from the Insight project at
+Kitware.
