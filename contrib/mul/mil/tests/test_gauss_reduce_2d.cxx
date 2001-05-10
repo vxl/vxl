d72e727a2d8c41761ec7b188ce9556e23c1e572c
Unknown <targetjr>
2001-05-10 17:12:19 -0400

    fabs --> vcl_fabs

--- contrib/mul/mil/tests/test_gauss_reduce_2d.cxx
+++ contrib/mul/mil/tests/test_gauss_reduce_2d.cxx
@@ -24,13 +24,13 @@ void test_gauss_reduce_2d_byte(int nx)
 
   for (int y=0;y<image0.ny();++y)
      for (int x=0;x<image0.nx();++x)
-	 {
-	   image0(x,y) = x+y*10;
-	 }
+     {
+       image0(x,y) = x+y*10;
+     }
 
   mil_gauss_reduce_2d(reduced_x.plane(0),reduced_x.xstep(),reduced_x.ystep(),
                     image0.plane(0),image0.nx(),image0.ny(),
-					  image0.xstep(),image0.ystep());
+                      image0.xstep(),image0.ystep());
 
   vcl_cout<<"Original: "; image0.print_all(vcl_cout);
   vcl_cout<<"reduced_x : "; reduced_x.print_all(vcl_cout);
@@ -46,7 +46,7 @@ void test_gauss_reduce_2d_byte(int nx)
   test2.fill(222);
   mil_gauss_reduce_2d(test2.plane(0),test2.xstep(),test2.ystep(),
                     image0.plane(0),image0.nx(),image0.ny(),
-					  image0.xstep(),image0.ystep());
+                      image0.xstep(),image0.ystep());
   TEST("No overrun",test2(L+1,1)==222,true);
 
 
@@ -58,20 +58,20 @@ void test_gauss_reduce_2d_byte(int nx)
 
   for (int y=0;y<image1.ny();++y)
      for (int x=0;x<image1.nx();++x)
-	 {
-	   image1(x,y) = x+y*10;
-	 }
+     {
+       image1(x,y) = x+y*10;
+     }
 
   mil_gauss_reduce_2d(reduced_y.plane(0),reduced_y.ystep(),reduced_y.xstep(),
                     image1.plane(0),image1.ny(),image1.nx(),
-					  image1.ystep(),image1.xstep());
+                      image1.ystep(),image1.xstep());
 
   vcl_cout<<"Original: "; image1.print_all(vcl_cout);
   vcl_cout<<"reduced_y : "; reduced_y.print_all(vcl_cout);
 
   TEST("First element",reduced_y(1,0)==image1(1,0),true);
   TEST("Next element",reduced_y(1,1)==image1(1,2),true);
-  TEST("Last element",reduced_y(1,L)==image1(1,2*L),true); 
+  TEST("Last element",reduced_y(1,L)==image1(1,2*L),true);
 }
 
 void test_gauss_reduce_2d_float(int nx)
@@ -87,21 +87,21 @@ void test_gauss_reduce_2d_float(int nx)
 
   for (int y=0;y<image0.ny();++y)
      for (int x=0;x<image0.nx();++x)
-	 {
-	   image0(x,y) = x+y*10;
-	 }
+     {
+       image0(x,y) = x+y*10;
+     }
 
   mil_gauss_reduce_2d(reduced_x.plane(0),reduced_x.xstep(),reduced_x.ystep(),
                     image0.plane(0),image0.nx(),image0.ny(),
-					  image0.xstep(),image0.ystep());
+                      image0.xstep(),image0.ystep());
 
   vcl_cout<<"Original: "; image0.print_all(vcl_cout);
   vcl_cout<<"reduced_x : "; reduced_x.print_all(vcl_cout);
 
-  TEST("First element",fabs(reduced_x(0,1)-image0(0,1))<1e-6,true);
-  TEST("Next element",fabs(reduced_x(1,1)-image0(2,1))<1e-6,true);
+  TEST("First element",vcl_fabs(reduced_x(0,1)-image0(0,1))<1e-6,true);
+  TEST("Next element",vcl_fabs(reduced_x(1,1)-image0(2,1))<1e-6,true);
   int L = (nx-1)/2;
-  TEST("Last element",fabs(reduced_x(L,1)-image0(2*L,1))<1e-6,true);
+  TEST("Last element",vcl_fabs(reduced_x(L,1)-image0(2*L,1))<1e-6,true);
 
 
   mil_image_2d_of<float> test2;
@@ -109,9 +109,8 @@ void test_gauss_reduce_2d_float(int nx)
   test2.fill(222);
   mil_gauss_reduce_2d(test2.plane(0),test2.xstep(),test2.ystep(),
                     image0.plane(0),image0.nx(),image0.ny(),
-					  image0.xstep(),image0.ystep());
-  TEST("No overrun",fabs(test2(L+1,1)-222)<1e-6,true);
-
+                      image0.xstep(),image0.ystep());
+  TEST("No overrun",vcl_fabs(test2(L+1,1)-222)<1e-6,true);
 }
 
 void test_gauss_reduce_2d()
