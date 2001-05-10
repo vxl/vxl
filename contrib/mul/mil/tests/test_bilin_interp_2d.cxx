6c63b3148d6930d5d306da6e189c53edf0c3911b
Unknown <targetjr>
2001-05-10 17:13:30 -0400

    fabs --> vcl_fabs

--- contrib/mul/mil/tests/test_bilin_interp_2d.cxx
+++ contrib/mul/mil/tests/test_bilin_interp_2d.cxx
@@ -11,7 +11,6 @@
 #include <vil/vil_byte.h>
 
 
-
 void test_bilin_interp_2d_byte()
 {
   vcl_cout << "***********************" << vcl_endl;
@@ -23,43 +22,42 @@ void test_bilin_interp_2d_byte()
 
   for (int y=0;y<image0.ny();++y)
      for (int x=0;x<image0.nx();++x)
-	 {
-	   image0(x,y) = x+y*10;
-	 }
+     {
+       image0(x,y) = x+y*10;
+     }
 
    {
      double v1 = mil_bilin_interp_2d(3,3,image0.plane(0),image0.ystep());
-     TEST("mil_bilin_interp_2d at grid point",fabs(v1-33)<1e-8,true);
+     TEST("mil_bilin_interp_2d at grid point",vcl_fabs(v1-33)<1e-8,true);
      v1 = mil_bilin_interp_2d(3,3,image0.plane(0),image0.xstep(),image0.ystep());
-     TEST("mil_bilin_interp_2d at grid point",fabs(v1-33)<1e-8,true);
+     TEST("mil_bilin_interp_2d at grid point",vcl_fabs(v1-33)<1e-8,true);
 
      double v2 = mil_bilin_interp_2d(3.4,3,image0.plane(0),image0.ystep());
-     TEST("mil_bilin_interp_2d at off-grid point",fabs(v2-33.4)<1e-8,true);
+     TEST("mil_bilin_interp_2d at off-grid point",vcl_fabs(v2-33.4)<1e-8,true);
      double v3 = mil_bilin_interp_2d(3.4,3.5,image0.plane(0),image0.ystep());
-     TEST("mil_bilin_interp_2d at off-grid point",fabs(v3-38.4)<1e-8,true);
+     TEST("mil_bilin_interp_2d at off-grid point",vcl_fabs(v3-38.4)<1e-8,true);
    }
 
    {
      int nx = image0.nx();
      int ny = image0.ny();
-	 int xstep = image0.xstep();
-	 int ystep = image0.ystep();
-	 const vil_byte* data = image0.plane(0);
+     int xstep = image0.xstep();
+     int ystep = image0.ystep();
+     const vil_byte* data = image0.plane(0);
 
      double v1 = mil_safe_bilin_interp_2d(3,3,data,nx,ny,ystep);
-     TEST("mil_safe_bilin_interp_2d at grid point",fabs(v1-33)<1e-8,true);
+     TEST("mil_safe_bilin_interp_2d at grid point",vcl_fabs(v1-33)<1e-8,true);
      v1 = mil_safe_bilin_interp_2d(3,3,data,nx,ny,xstep,ystep);
-     TEST("mil_safe_bilin_interp_2d at grid point",fabs(v1-33)<1e-8,true);
+     TEST("mil_safe_bilin_interp_2d at grid point",vcl_fabs(v1-33)<1e-8,true);
 
      double v2 = mil_safe_bilin_interp_2d(3.4,3,data,nx,ny,ystep);
-     TEST("mil_safe_bilin_interp_2d at off-grid point",fabs(v2-33.4)<1e-8,true);
+     TEST("mil_safe_bilin_interp_2d at off-grid point",vcl_fabs(v2-33.4)<1e-8,true);
      double v3 = mil_safe_bilin_interp_2d(3.4,3.5,data,nx,ny,ystep);
-     TEST("mil_safe_bilin_interp_2d at off-grid point",fabs(v3-38.4)<1e-8,true);
+     TEST("mil_safe_bilin_interp_2d at off-grid point",vcl_fabs(v3-38.4)<1e-8,true);
 
-	 double v_outside = mil_safe_bilin_interp_2d(-1,-1,data,nx,ny,ystep);
-     TEST("mil_safe_bilin_interp_2d outside image",fabs(v_outside)<1e-8,true);
+     double v_outside = mil_safe_bilin_interp_2d(-1,-1,data,nx,ny,ystep);
+     TEST("mil_safe_bilin_interp_2d outside image",vcl_fabs(v_outside)<1e-8,true);
    }
-
 }
 
 void test_bilin_interp_2d()
