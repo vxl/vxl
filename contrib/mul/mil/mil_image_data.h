90fe04fc28a63a4e464589ee72e3eb0eededb0a7
Unknown <targetjr>
2001-05-10 03:40:50 -0400

    ostream must be vcl_ostream

--- contrib/mul/mil/mil_image_data.h
+++ contrib/mul/mil/mil_image_data.h
@@ -18,63 +18,63 @@ class vsl_b_istream;
 template<class T>
 class mil_image_data {
 private:
-	//: Data
-	T *data_;
+    //: Data
+    T *data_;
 
-	//: Number of elements
-	unsigned int size_;
+    //: Number of elements
+    unsigned int size_;
 
-	//: Reference count
-	int ref_count_;
+    //: Reference count
+    int ref_count_;
 
 public:
-		//: Dflt ctor
-	mil_image_data();
+    //: Dflt ctor
+    mil_image_data();
 
-		//: Copy ctor
-	mil_image_data(const mil_image_data&);
+    //: Copy ctor
+    mil_image_data(const mil_image_data&);
 
-		//: Copy operator
-	mil_image_data<T>& operator=(const mil_image_data&);
+    //: Copy operator
+    mil_image_data<T>& operator=(const mil_image_data&);
 
-		//: Destructor
-	~mil_image_data();
+    //: Destructor
+    ~mil_image_data();
 
-	//: Increment reference count
-	void ref();
+    //: Increment reference count
+    void ref();
 
-	//: Decrement reference count
-	void unref();
+    //: Decrement reference count
+    void unref();
 
-	//: Number of objects refering to this data
-	int ref_count() const { return ref_count_; }
+    //: Number of objects refering to this data
+    int ref_count() const { return ref_count_; }
 
-	//: Pointer to first element of data
-	const T* data() const { return data_;}
+    //: Pointer to first element of data
+    const T* data() const { return data_;}
 
-	//: Pointer to first element of data
-	T* data() { return data_;}
+    //: Pointer to first element of data
+    T* data() { return data_;}
 
-	//: Number of elements allocated
-	unsigned int size() const { return size_; }
+    //: Number of elements allocated
+    unsigned int size() const { return size_; }
 
-	//: Create space for n elements
-	void resize(int n);
+    //: Create space for n elements
+    void resize(int n);
 
-	//: Version number for I/O 
-	short version_no() const;
+    //: Version number for I/O
+    short version_no() const;
 
-		//: Name of the class
-	vcl_string is_a() const;
+    //: Name of the class
+    vcl_string is_a() const;
 
-		//: Print class to os
-	void print(ostream& os) const;
-		
-		//: Save class to binary file stream
-	void b_write(vsl_b_ostream& bfs) const;
+    //: Print class to os
+    void print(vcl_ostream& os) const;
 
-		//: Load class from binary stream
-	void b_read(vsl_b_istream& bfs);
+    //: Save class to binary file stream
+    void b_write(vsl_b_ostream& bfs) const;
+
+    //: Load class from binary stream
+    void b_read(vsl_b_istream& bfs);
 };
 
 
@@ -95,5 +95,3 @@ template<class T>
 void vsl_b_read(vsl_b_istream& s, mil_image_data<T>*& v);
 
 #endif // mil_image_data_h_
-
-
