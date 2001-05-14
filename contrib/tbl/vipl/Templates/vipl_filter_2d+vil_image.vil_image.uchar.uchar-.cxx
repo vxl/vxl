54af891ac821acaf9444148549252775bb1d2634
Unknown <targetjr>
2001-05-14 01:28:20 -0400

    no need for VCL_DFL_TMPL_ARG in template instantiation

--- contrib/tbl/vipl/Templates/vipl_filter_2d+vil_image.vil_image.uchar.uchar-.cxx
+++ contrib/tbl/vipl/Templates/vipl_filter_2d+vil_image.vil_image.uchar.uchar-.cxx
@@ -21,13 +21,11 @@ vipl_filterable_section_container_generator(const vil_image& im, unsigned char*)
   return rtn;
 }
 
-#include <vcl_compiler.h> // for the definition of VCL_DFL_TMPL_ARG
-
 #include <vipl/filter/vipl_filter.h>
 #include <vipl/filter/vipl_filter.txx>
-template class vipl_filter<vil_image, vil_image, unsigned char, unsigned char, 2 VCL_DFL_TMPL_ARG(vipl_trivial_pixeliter)>;
+template class vipl_filter<vil_image, vil_image, unsigned char, unsigned char, 2, vipl_trivial_pixeliter>;
 
 #include <vipl/filter/vipl_filter_2d.h>
 #include <vipl/filter/vipl_filter_2d.txx>
-template class vipl_filter_2d<vil_image, vil_image, unsigned char, unsigned char VCL_DFL_TMPL_ARG(vipl_trivial_pixeliter)>;
+template class vipl_filter_2d<vil_image, vil_image, unsigned char, unsigned char, vipl_trivial_pixeliter>;
 
