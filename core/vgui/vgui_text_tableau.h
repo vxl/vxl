7da347b0f94d9272fd358835d7cb3b8a32e780fb
Unknown <remcvs_tgtjr_oxford>
2001-05-16 03:23:08 -0400

    psync.pl (oxford): KYM: added change_colour

--- core/vgui/vgui_text_tableau.h
+++ core/vgui/vgui_text_tableau.h
@@ -5,23 +5,21 @@
 #pragma interface
 #endif
 //
-// .NAME vgui_text
-// .INCLUDE vgui/vgui_text_tableau.h
-// .FILE vgui_text_tableau.cxx
+// This is vgui/vgui_text_tableau.h
 //
-// .SECTION Description
-// Tableau for rendering text.
+//:
+// \file
+// \author Philip C. Pritchett, RRG, University of Oxford
+// \date   19 Oct 99
+// \brief  Tableau for rendering text.
 // Each piece of text is associated with an integer handle through
 // which it can be retrieved, moved about, changed or removed.
-//
-// .SECTION Author
-//    Philip C. Pritchett, RRG, University of Oxford
-//    Created: 19 Oct 99
-//
-// .SECTION Modifications:
-// 30 Dec 1999 fsm@robots. modified to render arbitrary text.
-// 23-FEB-2000 K.Y.McGaul - Handle now remains constant when text is removed.
-//                          Note that this means that the vector will never get smaller.
+// \verbatim
+//  Modifications:
+//   30 Dec 1999 fsm@robots. modified to render arbitrary text.
+//   23-FEB-2000 K.Y.McGaul - Handle now remains constant when text is removed.
+//                            Note that this means that the vector will never get smaller.
+// \endverbatim
 //-----------------------------------------------------------------------------
 
 #include <vcl_string.h>
@@ -40,6 +38,8 @@ public:
   int add(float x, float y, char const *text);
   int add(float x, float y, vcl_string const &text) { return add(x,y,text.c_str()); }
 
+  //: Set the colour of the text
+  void set_colour(float r, float g, float b);
   float get_posx(int handle) const;
   float get_posy(int handle) const;
   vcl_string const &get_text(int handle) const;
@@ -59,6 +59,8 @@ private:
   vcl_vector<float> xs;
   vcl_vector<float> ys;
   vcl_vector<vcl_string> ts;
+
+  float r_, g_, b_;
 
   unsigned first_empty;    // Position of the first empty space in the vectors
 };
