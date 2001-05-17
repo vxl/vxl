d62d239d74d48ba853422c33ea7dbcb941bea453
Tim Cootes <timcootes@users.sourceforge.net>
2001-05-17 12:40:53 -0400

    Added binary_loader template

--- contrib/mul/mbl/Templates/mbl_data_collector+vnl_vector+double--.cxx
+++ contrib/mul/mbl/Templates/mbl_data_collector+vnl_vector+double--.cxx
@@ -1,3 +1,5 @@
 #include<mbl/mbl_data_collector.txx>
 #include<vnl/vnl_vector.h>
+#include<vsl/vsl_binary_loader.txx>
 MBL_DATA_COLLECTOR_INSTANTIATE(vnl_vector<double>);
+VSL_BINARY_LOADER_INSTANTIATE(mbl_data_collector<vnl_vector<double> >);
