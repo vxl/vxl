697db969c2a02f5c5222fc68637b4f1ce10915b7
Unknown <remcvs_tgtjr_oxford>
2001-05-21 04:23:08 -0400

    psync.pl: blah

--- contrib/oxl/mvl/Templates/vbl_sparse_array_3d+TripleMatchSetCorner~-.cxx
+++ contrib/oxl/mvl/Templates/vbl_sparse_array_3d+TripleMatchSetCorner~-.cxx
@@ -1,5 +1,5 @@
-// Instantiation of vbl_sparse_array_3d<TripleMatchSetCorner*>
-#include <vbl/vbl_sparse_array_3d.txx>
-#include <mvl/TripleMatchSetCorner.h>
-
-VBL_SPARSE_ARRAY_3D_INSTANTIATE(TripleMatchSetCorner*);
+//// Instantiation of vbl_sparse_array_3d<TripleMatchSetCorner*>
+//#include <vbl/vbl_sparse_array_3d.txx>
+//#include <mvl/TripleMatchSetCorner.h>
+//
+//VBL_SPARSE_ARRAY_3D_INSTANTIATE(TripleMatchSetCorner*);
--- contrib/oxl/mvl/Templates/vbl_sparse_array_3d+TripleMatchSetLineSeg~-.cxx
+++ contrib/oxl/mvl/Templates/vbl_sparse_array_3d+TripleMatchSetLineSeg~-.cxx
@@ -1,5 +1,5 @@
-// Instantiation of vbl_sparse_array_3d<TripleMatchSetLineSeg*>
-#include <vbl/vbl_sparse_array_3d.txx>
-#include <mvl/TripleMatchSetLineSeg.h>
-
-VBL_SPARSE_ARRAY_3D_INSTANTIATE(TripleMatchSetLineSeg*);
+//// Instantiation of vbl_sparse_array_3d<TripleMatchSetLineSeg*>
+//#include <vbl/vbl_sparse_array_3d.txx>
+//#include <mvl/TripleMatchSetLineSeg.h>
+//
+//VBL_SPARSE_ARRAY_3D_INSTANTIATE(TripleMatchSetLineSeg*);
