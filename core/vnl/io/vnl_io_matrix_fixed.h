17d8e10ba1ed89a057d6328fd167eecd553b0492
Tim Cootes <timcootes@users.sourceforge.net>
2001-05-22 04:48:25 -0400

    Removed matrix and vector fixed IO, so as to get clean(er) compilation under Solaris.

--- core/vnl/io/vnl_io_matrix_fixed.h
+++ core/vnl/io/vnl_io_matrix_fixed.h
@@ -1,30 +1,7 @@
 #ifndef vnl_io_matrix_fixed_h
 #define vnl_io_matrix_fixed_h
-#ifdef __GNUC__
-#pragma interface
-#endif
-
-// This is vxl/vnl/io/vnl_io_matrix_fixed.h
-
-//:
-// \file 
-// \author Louise Butcher
-// \date 21-Mar-2001
-
-#include <vsl/vsl_binary_io.h>
+// No need for separate functions
+// This file just for backward compatability
 #include <vnl/vnl_matrix_fixed.h>
-
-//: Binary save vnl_matrix_fixed to stream.
-template <class T, int m, int n>
-void vsl_b_write(vsl_b_ostream & os, const vnl_matrix_fixed<T,m,n> & v);
-
-//: Binary load vnl_matrix_fixed from stream.
-template <class T, int m,int n>
-void vsl_b_read(vsl_b_istream & is, vnl_matrix_fixed<T,m,n> & v);
-
-//: Print human readable summary of object to a stream
-template <class T, int m, int n>
-void vsl_print_summary(vcl_ostream & os,const vnl_matrix_fixed<T,m,n> & b);
-
-
+#include <vnl/io/vnl_io_matrix.h>
 #endif // vnl_io_matrix_fixed_h
