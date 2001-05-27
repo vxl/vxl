f900ee93606ba09ce19272f4bbdbdee37cc8618e
Unknown <targetjr>
2001-05-27 13:43:59 -0400

    a few additional declarations

--- core/vbl/vbl_fwd.h
+++ core/vbl/vbl_fwd.h
@@ -1,11 +1,17 @@
 #ifndef vbl_fwd_h_
 #define vbl_fwd_h_
 
-export
+#include <vcl_compiler.h> // for export keyword
+
+template <class T> class vbl_arg;
 template <class T> struct vbl_array_1d;
 export
 template <class T> class vbl_array_2d;
 export
 template <class T> class vbl_array_3d;
+template <class T> class vbl_smart_ptr;
+template <class T> class vbl_sparse_array_1d;
+template <class T> class vbl_sparse_array_2d;
+template <class T> class vbl_sparse_array_3d;
 
 #endif // vbl_fwd_h_
