3e994c4ddfdb28c9eb883360ce91da066e5945b3
Unknown <targetjr>
2001-05-27 13:46:10 -0400

    a few additional includes

--- core/vbl/tests/vbl_test_all.cxx
+++ core/vbl/tests/vbl_test_all.cxx
@@ -1,27 +1,19 @@
 /*
   fsm@robots.ox.ac.uk
 */
+#include <vbl/vbl_fwd.h>
 #include <vbl/vbl_arg.h>
 #include <vbl/vbl_array_1d.h>
 #include <vbl/vbl_array_2d.h>
 #include <vbl/vbl_array_3d.h>
 #include <vbl/vbl_awk.h>
-//#include <vbl/vbl_basic_optional.h>
-//#include <vbl/vbl_basic_relation.h>
-//#include <vbl/vbl_basic_relation_type.h>
-//#include <vbl/vbl_basic_relation_where.h>
-//#include <vbl/vbl_basic_tuple.h>
 #include <vbl/vbl_binary.h>
 #include <vbl/vbl_bool_ostream.h>
-//#include <vbl/vbl_bounding_box.h>
-//#include <vbl/vbl_br_default.h>
-//#include <vbl/vbl_br_impl.h>
-#include <vbl/vbl_canonical_path.h>
+#include <vbl/vbl_bounding_box.h>
+#include <vbl/vbl_expand_path.h>
 #include <vbl/vbl_file.h>
 #include <vbl/vbl_file_iterator.h>
-#include <vbl/vbl_fwd.h>
 #include <vbl/vbl_get_timestamp.h>
-//#include <vbl/vbl_hash_map.h>
 #include <vbl/vbl_printf.h>
 #include <vbl/vbl_protection_traits.h>
 #include <vbl/vbl_psfile.h>
@@ -42,5 +34,16 @@
 #include <vbl/vbl_trace.h>
 #include <vbl/vbl_user_info.h>
 #include <vbl/vbl_whereami.h>
+
+#if 0 // These files use the nonexistent vcl_hash_map.h and/or vcl_hash_string.h
+#include <vbl/vbl_hash_map.h>
+#include <vbl/vbl_basic_optional.h>
+#include <vbl/vbl_basic_relation.h>
+#include <vbl/vbl_basic_tuple.h>
+#include <vbl/vbl_basic_relation_where.h>
+#include <vbl/vbl_br_default.h>
+#include <vbl/vbl_br_impl.h>
+#endif
+#include <vbl/vbl_basic_relation_type.h>
 
 int main() { return 0; }
