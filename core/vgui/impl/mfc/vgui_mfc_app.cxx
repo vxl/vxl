4e8abded5a2dea2a9512bc7f20a190a9ceb6787b
Unknown <targetjr>
2001-06-06 17:04:35 -0400

    not using _AFXDLL

--- core/vgui/impl/mfc/vgui_mfc_app.cxx
+++ core/vgui/impl/mfc/vgui_mfc_app.cxx
@@ -54,9 +54,8 @@ BOOL vgui_mfc_app::InitInstance()
         // If you are not using these features and wish to reduce the size
         //  of your final executable, you should remove from the following
         //  the specific initialization routines you do not need.
-
 #if 0
-  #ifdef _AFXDLL
+  #ifdef _DEBUG
         Enable3dControls();       // Call this when using MFC in a shared DLL
   #else
         Enable3dControlsStatic(); // Call this when linking to MFC statically
