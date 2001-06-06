24ae7383c51dad42b948614ea7a3e52e0769cf86
Unknown <targetjr>
2001-06-06 19:01:13 -0400

    only one //: per entry

--- contrib/gel/vmal/vmal_homog2d.h
+++ contrib/gel/vmal/vmal_homog2d.h
@@ -1,12 +1,12 @@
 #ifndef vmal_homog2d_h_
 #define vmal_homog2d_h_
- 
+
 //--------------------------------------------------------------------------------
-// .NAME	vmal_homog2d
-// .INCLUDE	vmal/vmal_homog2d.h
-// .FILE	vmal_homog2d.cxx
+// .NAME        vmal_homog2d
+// .INCLUDE     vmal/vmal_homog2d.h
+// .FILE        vmal_homog2d.cxx
 // .SECTION Description:
-//   
+//
 // .SECTION Author
 //   L. Guichard
 // .SECTION Modifications:
@@ -17,7 +17,7 @@
 #include <vcl_vector.h>
 class vmal_homog2d
 {
-  
+
 //***************************************************************************
 // Initialization
 //***************************************************************************
@@ -25,21 +25,21 @@ public:
 //---------------------------------------------------------------------------
 //: Constructors
 //---------------------------------------------------------------------------
-	vmal_homog2d();
+  vmal_homog2d();
 
 //---------------------------------------------------------------------------
 //: Destructor
 //---------------------------------------------------------------------------
-	virtual ~vmal_homog2d();
+  virtual ~vmal_homog2d();
 
 //---------------------------------------------------------------------------
-//: Compute the 2d homography from to set of matched points. A point pima1[i] 
-//: in the first image match with the point pima2[i] in the second image.
+//: Compute the 2d homography from to set of matched points.
+// A point pima1[i] in the first image match with the point pima2[i]
+// in the second image.
 //---------------------------------------------------------------------------
-	void compute_homo(const vcl_vector<vnl_double_3 > & pima1,
-					  const vcl_vector<vnl_double_3 > & pima2,
-					  vnl_double_3x3 &hmatrix);
-
+  void compute_homo(const vcl_vector<vnl_double_3 > & pima1,
+            const vcl_vector<vnl_double_3 > & pima2,
+            vnl_double_3x3 &hmatrix);
 };
 
 #endif // vmal_homog2d_h_
