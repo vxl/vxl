bebfd18bfee7fbbfa00143487b48b28b7ef76c8f
Unknown <targetjr>
2001-06-06 19:00:49 -0400

    spurious <CR>'s removed

--- contrib/gel/vmal/vmal_dense_matching.h
+++ contrib/gel/vmal/vmal_dense_matching.h
@@ -1,16 +1,16 @@
 #ifndef vmal_dense_matching_h_
 #define vmal_dense_matching_h_
- 
-//--------------------------------------------------------------------------------
-// .NAME	vmal_homography
-// .INCLUDE	vmal/vmal_homography.h
-// .FILE	vmal_homography.cxx
+
+//------------------------------------------------------------------------------
+// .NAME        vmal_dense_matching
+// .INCLUDE     vmal/vmal_dense_matching.h
+// .FILE        vmal_dense_matching.cxx
 // .SECTION Description:
-//   
+//
 // .SECTION Author
 //   L. Guichard
 // .SECTION Modifications:
-//--------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 
 #include <vmal/vmal_multi_view_data_edge_sptr.h>
@@ -23,32 +23,32 @@ class vmal_dense_matching
 {
 public:
 
-	vmal_dense_matching(const vnl_double_3x3 & H0,
-		  				const vnl_double_3x3 & H1);
+  vmal_dense_matching(const vnl_double_3x3 & H0,
+            const vnl_double_3x3 & H1);
 
-	~vmal_dense_matching();
+  ~vmal_dense_matching();
 
-	void set_fmatrix(const vnl_double_3x3 & F);
+  void set_fmatrix(const vnl_double_3x3 & F);
 
-	void set_hmatrix(const vnl_double_3x3 & H);
+  void set_hmatrix(const vnl_double_3x3 & H);
 
 
-	// Between two set of lines in 2 images that are matched, it compute 
-	//the best lines using the fundamental constraint.
-	void refine_lines_using_F(vmal_multi_view_data_edge_sptr mvd_edge,
-							  vmal_multi_view_data_edge_sptr res);
-	// Between two set of lines in 2 images that are matched, it compute 
-	//the best lines using the homography.
-	void refine_lines_using_H(vmal_multi_view_data_edge_sptr mvd_edge,
-						      vmal_multi_view_data_edge_sptr res);
+  // Between two set of lines in 2 images that are matched, it compute
+  //the best lines using the fundamental constraint.
+  void refine_lines_using_F(vmal_multi_view_data_edge_sptr mvd_edge,
+                vmal_multi_view_data_edge_sptr res);
+  // Between two set of lines in 2 images that are matched, it compute
+  //the best lines using the homography.
+  void refine_lines_using_H(vmal_multi_view_data_edge_sptr mvd_edge,
+                  vmal_multi_view_data_edge_sptr res);
 
-	void disparity_map(vmal_multi_view_data_edge_sptr mvd_edge,
-		int h, int w);
+  void disparity_map(vmal_multi_view_data_edge_sptr mvd_edge,
+    int h, int w);
 
 private:
-	vnl_double_3x3 _H0,_H1,_F;
-	vnl_double_3x3 _H;
-	int _type; //1 for Fundamental matrix, 2 for Homography matrix
+  vnl_double_3x3 _H0,_H1,_F;
+  vnl_double_3x3 _H;
+  int _type; //1 for Fundamental matrix, 2 for Homography matrix
 };
 
 #endif
--- contrib/gel/vmal/vmal_kl.cxx
+++ contrib/gel/vmal/vmal_kl.cxx
@@ -22,34 +22,34 @@ vmal_kl::~vmal_kl()
 void vmal_kl::match_sequence(vcl_vector<vil_image> &image_list,vmal_multi_view_data_vertex_sptr matches)
 {
 
-	// Uses the KL tracker to track points through an image
-	int nFeatures = _params.numpoints;
-	int nFrames = image_list.size();
+  // Uses the KL tracker to track points through an image
+  int nFeatures = _params.numpoints;
+  int nFrames = image_list.size();
    // If there are no frames in this movie, then skip
-	if (nFrames < 1) return;
+  if (nFrames < 1) return;
 
-	// Set up the context
-	KLT_TrackingContext tc = KLTCreateTrackingContext();
-	KLT_FeatureList     fl = KLTCreateFeatureList(nFeatures);
-	KLT_FeatureTable    ft = KLTCreateFeatureTable(nFrames, nFeatures);
+  // Set up the context
+  KLT_TrackingContext tc = KLTCreateTrackingContext();
+  KLT_FeatureList     fl = KLTCreateFeatureList(nFeatures);
+  KLT_FeatureTable    ft = KLTCreateFeatureTable(nFrames, nFeatures);
 
-	// Apply the defaults
-	set_tracking_context (tc);
-	tc->sequentialMode = TRUE;
+  // Apply the defaults
+  set_tracking_context (tc);
+  tc->sequentialMode = TRUE;
 
-	int width=image_list[0].width();
-	int height=image_list[0].height();
+  int width=image_list[0].width();
+  int height=image_list[0].height();
 
-	// Now, get the imagery into a linear buffer
-	KLT_PixelType* img1=convert_to_gs_image(image_list[0]);
+  // Now, get the imagery into a linear buffer
+  KLT_PixelType* img1=convert_to_gs_image(image_list[0]);
 
-	// Get some features from the first image
-	KLTSelectGoodFeatures(tc, img1, width, height, fl);
-	KLTStoreFeatureList(fl, ft, 0);
+  // Get some features from the first image
+  KLTSelectGoodFeatures(tc, img1, width, height, fl);
+  KLTStoreFeatureList(fl, ft, 0);
 
-	for (int i=1; i<nFrames; i++)  
+  for (int i=1; i<nFrames; i++)
       {
-	   KLT_PixelType* img2=convert_to_gs_image(image_list[i]);
+     KLT_PixelType* img2=convert_to_gs_image(image_list[i]);
 
       // Track the points
       KLTTrackFeatures(tc, img1, img2, width, height, fl);
@@ -65,43 +65,42 @@ void vmal_kl::match_sequence(vcl_vector<vil_image> &image_list,vmal_multi_view_d
    int matchnum = -1;
    int pointnum, viewnum;
    //matches.set_params(ft->nFrames,ft->nFeatures);
-   
+
    for (pointnum=0; pointnum<ft->nFeatures; pointnum++)
       for (viewnum=0; viewnum<ft->nFrames; viewnum++)
-	 {
-	 // Get the current feature
-	 KLT_Feature feat = ft->feature[pointnum][viewnum];
-	
-	 // Get the components of this feature
- 	 float x = feat->x;
-	 float y = feat->y;
-	 int val = feat->val;
+   {
+   // Get the current feature
+   KLT_Feature feat = ft->feature[pointnum][viewnum];
 
-	 // Test to see if this is the continuation of a sequence 
-	 // -- then put in the table
-	 if (feat->val == 0)
-	 {
-		vtol_vertex_2d_sptr vertex=new vtol_vertex_2d(x,y);
-	    matches->set(viewnum, matchnum, vertex);
-	 }	
+   // Get the components of this feature
+   float x = feat->x;
+   float y = feat->y;
 
-	 // Otherwise, this is the start of a sequence
-	 if (feat->val > 0)
-	    {
-	    // Must test to see if the next value is zero 
-	    // -- otherwise this is a 1-frame sequence
-	    if (viewnum < ft->nFrames-1 && 
-		ft->feature[pointnum][viewnum+1]->val == 0)
-	       {
-	       // This is a new match
-	       matchnum++;
+   // Test to see if this is the continuation of a sequence
+   // - then put in the table
+   if (feat->val == 0)
+   {
+    vtol_vertex_2d_sptr vertex=new vtol_vertex_2d(x,y);
+      matches->set(viewnum, matchnum, vertex);
+   }
 
-	       // Store it
-		   vtol_vertex_2d_sptr vertex=new vtol_vertex_2d(x,y);
-	       matches->set (viewnum, matchnum, vertex);
-	       }
-	    }
-	 }
+   // Otherwise, this is the start of a sequence
+   if (feat->val > 0)
+      {
+      // Must test to see if the next value is zero
+      // - otherwise this is a 1-frame sequence
+      if (viewnum < ft->nFrames-1 &&
+    ft->feature[pointnum][viewnum+1]->val == 0)
+         {
+         // This is a new match
+         matchnum++;
+
+         // Store it
+       vtol_vertex_2d_sptr vertex=new vtol_vertex_2d(x,y);
+         matches->set (viewnum, matchnum, vertex);
+         }
+      }
+   }
    // Finally, renumber the matches
 //   matches.renumber();
 }
@@ -109,63 +108,63 @@ void vmal_kl::match_sequence(vcl_vector<vil_image> &image_list,vmal_multi_view_d
 
 void vmal_kl::match_sequence(vidl_movie_sptr movie,vmal_multi_view_data_vertex_sptr matches)
 {
-	
-	vcl_vector<vil_image> image_list;
-	for (vidl_movie::frame_iterator pframe = movie->first();
+
+  vcl_vector<vil_image> image_list;
+  for (vidl_movie::frame_iterator pframe = movie->first();
         pframe <= movie->last();
         ++pframe)
     {
-		vil_image im = vil_image(pframe->get_image());
-		image_list.push_back(im);
+    vil_image im = vil_image(pframe->get_image());
+    image_list.push_back(im);
     }
-	match_sequence(image_list,matches);
+  match_sequence(image_list,matches);
 }
 
 vcl_vector<vtol_vertex_2d_sptr>* vmal_kl::extract_points(vil_image & image)
 {
-	int width=image.width();
-	int height=image.height();
-	vcl_cerr << "Beginning points extraction" << vcl_endl;
-	
-	KLT_PixelType* img1=convert_to_gs_image(image);
+  int width=image.width();
+  int height=image.height();
+  vcl_cerr << "Beginning points extraction" << vcl_endl;
 
-	// Now, run the extractor
-	int nFeatures = _params.numpoints;
+  KLT_PixelType* img1=convert_to_gs_image(image);
 
-	vcl_cerr << "Setting up the context..." << vcl_endl;
-	// Set up the context
-	KLT_TrackingContext tc = KLTCreateTrackingContext();
+  // Now, run the extractor
+  int nFeatures = _params.numpoints;
 
-	// Set the default values
-	set_tracking_context (tc);
+  vcl_cerr << "Setting up the context..." << vcl_endl;
+  // Set up the context
+  KLT_TrackingContext tc = KLTCreateTrackingContext();
 
-	// KLTPrintTrackingContext(tc);
+  // Set the default values
+  set_tracking_context (tc);
 
-	// Set up structure to hold the features.
-	vcl_cerr << "Setting up structure to hold the features..." << vcl_endl;
-	KLT_FeatureList fl = KLTCreateFeatureList(nFeatures);
+  // KLTPrintTrackingContext(tc);
 
-	// Extract the features
-	vcl_cerr << "Extracting the features..." << vcl_endl;
-	KLTSelectGoodFeatures(tc, img1, width, height, fl);
+  // Set up structure to hold the features.
+  vcl_cerr << "Setting up structure to hold the features..." << vcl_endl;
+  KLT_FeatureList fl = KLTCreateFeatureList(nFeatures);
+
+  // Extract the features
+  vcl_cerr << "Extracting the features..." << vcl_endl;
+  KLTSelectGoodFeatures(tc, img1, width, height, fl);
 
   // Make an IUPointGroup to hold the values
-	 vcl_vector<vtol_vertex_2d_sptr> *grp = new vcl_vector<vtol_vertex_2d_sptr>();
+   vcl_vector<vtol_vertex_2d_sptr> *grp = new vcl_vector<vtol_vertex_2d_sptr>();
 
-  for (int i=0 ; i< fl->nFeatures ; i++)  
+  for (int i=0 ; i< fl->nFeatures ; i++)
      {
-     // Change the point into an IUPoint -- offset by ilow, jlow
+     // Change the point into an IUPoint - offset by ilow, jlow
      float x = fl->feature[i]->x;
      float y = fl->feature[i]->y;
      //HomgPoint2D *point = new HomgPoint2D (x, y,1.0)
-	  vtol_vertex_2d_sptr point=new vtol_vertex_2d(x,y);
+    vtol_vertex_2d_sptr point=new vtol_vertex_2d(x,y);
      // Put the point in the backup list
      grp->push_back(point);
      }
 
   // !!
   // We probably need to delete the feature list
-  
+
   // Return the group
   return grp;
 }
@@ -173,52 +172,49 @@ vcl_vector<vtol_vertex_2d_sptr>* vmal_kl::extract_points(vil_image & image)
 //Convert a vil_image to an array of grey scale
 KLT_PixelType* vmal_kl::convert_to_gs_image(vil_image &image)
 {
-	vcl_cerr << "Converting image to grey scale..." << vcl_endl;
-	if (vil_pixel_format(image)==VIL_RGB_BYTE) 
-	{
-		int w=image.width();
-		int h=image.height();
-		KLT_PixelType* tab_mono=new KLT_PixelType[w*h];
-		vcl_cerr << "width: " <<w<< "  height"<<h<<  vcl_endl;
-		
-		vil_memory_image_of<vil_byte> ima_mono;
-		ima_mono.resize(w,h);
-	
-		vil_image_as_byte(image).get_section(ima_mono.get_buffer(), 0, 0, w, h);
-		vil_byte* p=ima_mono.get_buffer();
-	
-		for(int i=0;i<w;i++) 
-			for(int j=0;j<h;j++)
-				tab_mono[i*h+j]=(KLT_PixelType)p[i*h+j];
-			
-		return tab_mono;
-	} else return NULL;
+  vcl_cerr << "Converting image to grey scale..." << vcl_endl;
+  if (vil_pixel_format(image)==VIL_RGB_BYTE)
+  {
+    int w=image.width();
+    int h=image.height();
+    KLT_PixelType* tab_mono=new KLT_PixelType[w*h];
+    vcl_cerr << "width: " <<w<< "  height"<<h<<  vcl_endl;
+
+    vil_memory_image_of<vil_byte> ima_mono;
+    ima_mono.resize(w,h);
+
+    vil_image_as_byte(image).get_section(ima_mono.get_buffer(), 0, 0, w, h);
+    vil_byte* p=ima_mono.get_buffer();
+
+    for(int i=0;i<w;i++)
+      for(int j=0;j<h;j++)
+        tab_mono[i*h+j]=(KLT_PixelType)p[i*h+j];
+
+    return tab_mono;
+  } else return NULL;
 }
 
 void vmal_kl::set_tracking_context( KLT_TrackingContext tc)
 {
   /* Set values to values derived from the parameters */
-  tc->mindist 			= _params.mindist;
-  tc->window_width 		= _params.window_width;
-  tc->window_height 		= _params.window_height;
-  tc->sequentialMode 		= _params.sequentialMode;
-  tc->smoothBeforeSelecting 	= _params.smoothBeforeSelecting;
-  tc->writeInternalImages 	= _params.writeInternalImages;
-  tc->min_eigenvalue 		= _params.min_eigenvalue;
-  tc->min_determinant 		= _params.min_determinant;
-  tc->max_iterations 		= _params.max_iterations;
-  tc->min_displacement 		= _params.min_displacement;
-  tc->max_residue 		= _params.max_residue;
-  tc->grad_sigma 		= _params.grad_sigma;
-  tc->smooth_sigma_fact 	= _params.smooth_sigma_fact;
-  tc->pyramid_sigma_fact 	= _params.pyramid_sigma_fact;
-  tc->nSkippedPixels 		= _params.nSkippedPixels;
+  tc->mindist               = _params.mindist;
+  tc->window_width          = _params.window_width;
+  tc->window_height         = _params.window_height;
+  tc->sequentialMode        = _params.sequentialMode;
+  tc->smoothBeforeSelecting = _params.smoothBeforeSelecting;
+  tc->writeInternalImages   = _params.writeInternalImages;
+  tc->min_eigenvalue        = _params.min_eigenvalue;
+  tc->min_determinant       = _params.min_determinant;
+  tc->max_iterations        = _params.max_iterations;
+  tc->min_displacement      = _params.min_displacement;
+  tc->max_residue           = _params.max_residue;
+  tc->grad_sigma            = _params.grad_sigma;
+  tc->smooth_sigma_fact     = _params.smooth_sigma_fact;
+  tc->pyramid_sigma_fact    = _params.pyramid_sigma_fact;
+  tc->nSkippedPixels        = _params.nSkippedPixels;
 
   // klt functions to complete the setup
   KLTChangeTCPyramid (tc, _params.search_range); //set nPyramidLevels and subsampling
   KLTUpdateTCBorder  (tc); //set borderx and bordery
- 
 }
 
-
- 
