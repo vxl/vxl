ecd7f2d6839543d17e6294f6fe9599b73c31ec8c
Unknown <targetjr>
2001-06-08 07:19:39 -0400

    file names corrected

---
+++ core/vnl/Templates/vcl_vector+vnl_vector+vcl_complex+double---.cxx
@@ -0,0 +1,7 @@
+#include <vcl_complex.h>
+#include <vnl/vnl_vector.h>
+#include <vcl_vector.txx>
+#include <vcl_algorithm.txx>
+
+VCL_CONTAINABLE_INSTANTIATE(vnl_vector<vcl_complex<double> >);
+VCL_VECTOR_INSTANTIATE(vnl_vector<vcl_complex<double> >);
