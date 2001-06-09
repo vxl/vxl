41837d42771238a4f2bbb416b58161a926134830
Unknown <targetjr>
2001-06-08 20:26:05 -0400

    no need for typedef

--- core/vgui/impl/glut/Templates/vcl_vector+vgui_glut_adaptor~-.cxx
+++ core/vgui/impl/glut/Templates/vcl_vector+vgui_glut_adaptor~-.cxx
@@ -1,7 +1,7 @@
+struct vgui_glut_adaptor;
 #include <vcl_vector.txx>
+
+VCL_VECTOR_INSTANTIATE(vgui_glut_adaptor*);
+
 #include <vcl_algorithm.txx>
-
-typedef struct vgui_glut_adaptor *elt_t;
-
-VCL_VECTOR_INSTANTIATE(elt_t);
-VCL_FIND_INSTANTIATE(vcl_vector<elt_t>::iterator, elt_t);
+VCL_FIND_INSTANTIATE(vcl_vector<vgui_glut_adaptor*>::iterator, vgui_glut_adaptor*);
