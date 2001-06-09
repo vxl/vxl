8a9924e87457f882f7c14fff0dd6a6764f8fcd48
Unknown <targetjr>
2001-06-08 20:12:57 -0400

    "unsigned int" instead of "unsigned"

--- core/vbl/Templates/vbl_array_3d+uint-.cxx
+++ core/vbl/Templates/vbl_array_3d+uint-.cxx
@@ -1,3 +1,3 @@
 #include <vbl/vbl_array_3d.txx>
-VBL_ARRAY_3D_INSTANTIATE(unsigned);
-VBL_ARRAY_3D_IO_INSTANTIATE(unsigned);
+VBL_ARRAY_3D_INSTANTIATE(unsigned int);
+VBL_ARRAY_3D_IO_INSTANTIATE(unsigned int);
--- vcl/Templates/vcl_vector+vcl_pair+uint.uint--.cxx
+++ vcl/Templates/vcl_vector+vcl_pair+uint.uint--.cxx
@@ -1,5 +1,5 @@
 #include <vcl_utility.h>
 #include <vcl_vector.txx>
 
-typedef vcl_pair<unsigned, unsigned> pair_uu;
+typedef vcl_pair<unsigned int, unsigned int> pair_uu;
 VCL_VECTOR_INSTANTIATE(pair_uu);
