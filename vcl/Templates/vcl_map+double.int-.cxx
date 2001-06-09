86de3d5e6cdb5b0b8cb6d464c0f074afe6221b66
Unknown <targetjr>
2001-06-08 20:12:24 -0400

    first VCL_MAP_INSTANTIATE, then VCL_MULTIMAP_INSTANTIATE

--- vcl/Templates/vcl_map+double.int-.cxx
+++ vcl/Templates/vcl_map+double.int-.cxx
@@ -1,4 +1,4 @@
 #include <vcl_map.txx>
 
-VCL_MULTIMAP_INSTANTIATE(double, int, vcl_less<double>);
 VCL_MAP_INSTANTIATE(double, int, vcl_less<double>);
+VCL_MULTIMAP_INSTANTIATE(double, int, vcl_less<double>);
--- vcl/Templates/vcl_map+unsigned_int.unsigned_int-.cxx
+++ vcl/Templates/vcl_map+unsigned_int.unsigned_int-.cxx
@@ -1,4 +1,4 @@
 #include <vcl_map.txx>
 
-VCL_MULTIMAP_INSTANTIATE(unsigned, unsigned, vcl_less<unsigned>);
-VCL_MAP_INSTANTIATE(unsigned, unsigned, vcl_less<unsigned>);
+VCL_MAP_INSTANTIATE(unsigned int, unsigned int, vcl_less<unsigned int>);
+VCL_MULTIMAP_INSTANTIATE(unsigned int, unsigned int, vcl_less<unsigned int>);
