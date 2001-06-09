bc69bdac091d30b665f0ba713288834aad8d6967
Unknown <targetjr>
2001-06-08 20:16:20 -0400

    no need for typedef

--- core/vnl/Templates/vcl_vector+vcl_vector+vnl_double_3x4--.cxx
+++ core/vnl/Templates/vcl_vector+vcl_vector+vnl_double_3x4--.cxx
@@ -1,6 +1,4 @@
 #include <vnl/vnl_double_3x4.h>
 #include <vcl_vector.txx>
 
-typedef vcl_vector<vnl_double_3x4> vec_d_3_4;
-
-VCL_VECTOR_INSTANTIATE(vec_d_3_4);
+VCL_VECTOR_INSTANTIATE(vcl_vector<vnl_double_3x4>);
--- core/vnl/Templates/vcl_vector+vcl_vector+vnl_float_2--.cxx
+++ core/vnl/Templates/vcl_vector+vcl_vector+vnl_float_2--.cxx
@@ -1,6 +1,4 @@
 #include <vnl/vnl_float_2.h>
 #include <vcl_vector.txx>
 
-typedef vcl_vector<vnl_float_2> vec_f_2;
-
-VCL_VECTOR_INSTANTIATE(vec_f_2);
+VCL_VECTOR_INSTANTIATE(vcl_vector<vnl_float_2>);
