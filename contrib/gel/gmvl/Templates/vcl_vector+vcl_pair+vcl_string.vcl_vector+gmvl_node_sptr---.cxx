befe28c50e227a4eb080d2ed16e10ba234bcd1d2
Unknown <targetjr>
2001-06-08 20:18:26 -0400

    first instantiate vcl_vector<>

--- contrib/gel/gmvl/Templates/vcl_vector+vcl_pair+vcl_string.vcl_vector+gmvl_node_sptr---.cxx
+++ contrib/gel/gmvl/Templates/vcl_vector+vcl_pair+vcl_string.vcl_vector+gmvl_node_sptr---.cxx
@@ -5,7 +5,6 @@
 
 typedef vcl_pair<vcl_string,vcl_vector<gmvl_node_sptr> > pair_si;
 
-VCL_INSTANTIATE_INLINE( pair_si make_pair(vcl_string const &, vcl_vector<gmvl_node_sptr> const &));
-
 VCL_VECTOR_INSTANTIATE( pair_si);
 
+VCL_INSTANTIATE_INLINE( pair_si make_pair(vcl_string const &, vcl_vector<gmvl_node_sptr> const &));
