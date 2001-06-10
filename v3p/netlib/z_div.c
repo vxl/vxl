6de11c2090276a45b1caed86d4153a30c7b22804
Unknown <targetjr>
2001-06-10 05:22:59 -0400

    allow return variable to be identical to one of the 2 other arguments

--- v3p/netlib/z_div.c
+++ v3p/netlib/z_div.c
@@ -1,5 +1,7 @@
 #include "f2c.h"
 
+/* Modified by Peter Vanroose, June 2001, to allow c being equal to a or b */
+
 #ifdef KR_headers
 extern VOID sig_die();
 VOID z_div(c, a, b) doublecomplex *a, *b, *c;
@@ -10,35 +12,26 @@ void z_div(doublecomplex *c, doublecomplex *a, doublecomplex *b)
 {
   double ratio, den;
   double abr, abi;
-  
+  double ar = a->r, ai = a->i;
+
   if( (abr = b->r) < 0.)
     abr = - abr;
   if( (abi = b->i) < 0.)
     abi = - abi;
   if( abr <= abi ) {
     if(abi == 0) {
-
-
-
-
-
-
-
-
       sig_die("complex division by zero", 1);
-
     }
     ratio = (double)b->r / b->i ;
     den = b->i * (1 + ratio*ratio);
-    c->r = (a->r*ratio + a->i) / den;
-    c->i = (a->i*ratio - a->r) / den;
+    c->r = (ar*ratio + ai) / den;
+    c->i = (ai*ratio - ar) / den;
   }
-  
+
   else {
     ratio = (double)b->i / b->r ;
     den = b->r * (1 + ratio*ratio);
-    c->r = (a->r + a->i*ratio) / den;
-    c->i = (a->i - a->r*ratio) / den;
+    c->r = (ar + ai*ratio) / den;
+    c->i = (ai - ar*ratio) / den;
   }
-  
 }
