5b57d2c09a060c4839a104a461f82cb22a824d02
Unknown <remcvs_tgtjr_oxford>
2001-06-11 05:15:11 -0400

    psync.pl (oxford): macros for determinants

--- core/vnl/algo/vnl_determinant.h
+++ core/vnl/algo/vnl_determinant.h
@@ -14,20 +14,19 @@
 //  dac (Manchester) 26/03/2001: tidied up documentation
 // \endverbatim
 
-#include <vnl/vnl_determinant.h> // <-- not this file: different dir!
-
 //: calculates the determinant of a matrix
 //  evaluation of determinants of any size. for small
 //  matrices, will use the direct routines (no netlib)
 //  but for larger matrices, a matrix decomposition
 //  such as SVD or QR will be used.
 
-template <class T>
-T vnl_determinant(T const * const *rows, unsigned n);
-
-template <class T> class vnl_matrix;
+#include <vnl/vnl_matrix.h>
+#include <vnl/vnl_determinant.h> // <-- not this file; different directory.
 
 template <class T>
-T vnl_determinant(vnl_matrix<T> const &);
+T vnl_determinant(T const *const *rows, int size, bool balance = false);
+
+template <class T>
+T vnl_determinant(vnl_matrix<T> const &M, bool balance = false);
 
 #endif // vnl_algo_determinant_h_
