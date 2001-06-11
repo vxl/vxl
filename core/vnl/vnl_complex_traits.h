9961ab56f832da10ffc81e3535754f438682c385
Unknown <remcvs_tgtjr_oxford>
2001-06-11 05:15:19 -0400

    psync.pl (oxford): primary template should be empty

--- core/vnl/vnl_complex_traits.h
+++ core/vnl/vnl_complex_traits.h
@@ -6,10 +6,8 @@
 #endif
 
 //: \file
-//  \brief To allow templated real or complex algorithms to determine appropriate actions of conjugation, complexification etc.
-//  \author F. Schaffalitzky, Oxford RRG
-//  \date   26 Mar 1999
-//
+//  \brief To allow templated algorithms to determine appropriate actions of conjugation, complexification etc.
+//  \author F. Schaffalitzky, Oxford RRG, 26 Mar 1999
 // \verbatim
 // Modifications:
 // LSB (Manchester) 26/3/01 Documentation tidied
@@ -17,44 +15,76 @@
 //-----------------------------------------------------------------------------
 
 #include <vcl_complex.h>
-//: To allow templated real or complex algorithms to determine appropriate
-//    actions of conjugation, complexification etc.
-// Default is real.
-template <class T>
-class vnl_complex_traits {
-public:
-  //: Whether complex or not
+
+//  //: Whether complex or not
+//  enum { isreal = true };
+//
+//  //: Complex conjugation.
+//  static T conjugate(T x);
+//
+//  //: Complexification.
+//  static vcl_complex<T> complexify(T x);
+
+template <class T> // the primary template is empty, by design.
+struct vnl_complex_traits;
+
+#define macro(T) \
+VCL_DEFINE_SPECIALIZATION struct vnl_complex_traits<T > \
+{ \
+  enum { isreal = true }; \
+  static T conjugate(T x) { return x; } \
+  static vcl_complex<T> complexify(T x) { return vcl_complex<T >(x, (T)0); } \
+}
+#define makro(T) \
+macro(signed T); \
+macro(unsigned T)
+makro(char);
+makro(int);
+makro(long);
+#undef makro
+#undef macro
+
+
+VCL_DEFINE_SPECIALIZATION struct vnl_complex_traits<float>
+{
   enum { isreal = true };
-
-  //: Complex conjugation
-  static T conjugate(T x) { return x; }
-
-  //: Complexification
-  static vcl_complex<T> complexify(T x) { return vcl_complex<T>(x,T(0)); }
+  static float conjugate(float x) { return x; }
+  static vcl_complex<float> complexify(float x) { return vcl_complex<float>(x, 0.0f); }
 };
 
-//: override for vcl_complex<REAL> :
-// vcl_complex<float>
-VCL_DEFINE_SPECIALIZATION
-class vnl_complex_traits< vcl_complex<float> > {
-public:
-  enum { isreal = false };
-  static vcl_complex<float> conjugate(vcl_complex<float> z)
-    { return vcl_complex<float>(vcl_real(z), -vcl_imag(z)); }
-  static vcl_complex<float> complexify(vcl_complex<float> z)
-    { return z; }
+VCL_DEFINE_SPECIALIZATION struct vnl_complex_traits<double>
+{
+  enum { isreal = true };
+  static double conjugate(double x) { return x; }
+  static vcl_complex<double> complexify(double x) { return vcl_complex<double>(x, 0.0); }
 };
 
-//: override for vcl_complex<REAL> :
-// vcl_complex<double>
-VCL_DEFINE_SPECIALIZATION
-class vnl_complex_traits< vcl_complex<double> > {
-public:
+VCL_DEFINE_SPECIALIZATION struct vnl_complex_traits<long double>
+{
+  enum { isreal = true };
+  static long double conjugate(long double x) { return x; }
+  static vcl_complex<long double> complexify(long double x) { return vcl_complex<long double>(x, 0.0); }
+};
+
+VCL_DEFINE_SPECIALIZATION struct vnl_complex_traits<vcl_complex<float> >
+{
   enum { isreal = false };
-  static vcl_complex<double> conjugate(vcl_complex<double> z)
-    { return vcl_complex<double>(vcl_real(z), -vcl_imag(z)); }
-  static vcl_complex<double> complexify(vcl_complex<double> z)
-    { return z; }
+  static vcl_complex<float> conjugate(vcl_complex<float> x) { return vcl_conj(x); }
+  static vcl_complex<float> complexify(float x) { return x; }
+};
+
+VCL_DEFINE_SPECIALIZATION struct vnl_complex_traits<vcl_complex<double> >
+{
+  enum { isreal = false };
+  static vcl_complex<double> conjugate(vcl_complex<double> x) { return vcl_conj(x); }
+  static vcl_complex<double> complexify(double x) { return x; }
+};
+
+VCL_DEFINE_SPECIALIZATION struct vnl_complex_traits<vcl_complex<long double> >
+{
+  enum { isreal = false };
+  static vcl_complex<long double> conjugate(vcl_complex<long double> x) { return vcl_conj(x); }
+  static vcl_complex<long double> complexify(long double x) { return x; }
 };
 
 #endif // vnl_complex_traits_h_
