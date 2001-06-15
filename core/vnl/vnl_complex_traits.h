d4b5e1de0fc59865321eafddcbfa9476dddc2a73
Unknown <targetjr>
2001-06-15 11:52:47 -0400

    old implementation commented out with #if 0

--- core/vnl/vnl_complex_traits.h
+++ core/vnl/vnl_complex_traits.h
@@ -16,14 +16,16 @@
 
 #include <vcl_complex.h>
 
-//  //: Whether complex or not
-//  enum { isreal = true };
-//
-//  //: Complex conjugation.
-//  static T conjugate(T x);
-//
-//  //: Complexification.
-//  static vcl_complex<T> complexify(T x);
+#if 0 // The old implementation
+  //: Whether complex or not
+  enum { isreal = true };
+
+  //: Complex conjugation.
+  static T conjugate(T x);
+
+  //: Complexification.
+  static vcl_complex<T> complexify(T x);
+#endif
 
 template <class T> // the primary template is empty, by design.
 struct vnl_complex_traits;
