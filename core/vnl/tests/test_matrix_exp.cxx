cc7d2df7810f3a54213112e49759d78b6a2d53f5
Unknown <targetjr>
2001-06-19 08:08:44 -0400

    use <vnl/vnl_test.h>

--- core/vnl/tests/test_matrix_exp.cxx
+++ core/vnl/tests/test_matrix_exp.cxx
@@ -2,6 +2,7 @@
   fsm@robots.ox.ac.uk
 */
 #include <vcl_iostream.h>
+#include <vcl_cmath.h> // for vcl_abs()
 
 #include <vnl/vnl_vector.h>
 #include <vnl/vnl_matrix.h>
@@ -9,8 +10,9 @@
 #include <vnl/vnl_matlab_print.h>
 #include <vnl/vnl_rotation_matrix.h>
 #include <vnl/vnl_cross_product_matrix.h>
+#include <vnl/vnl_test.h>
 
-int main(int, char **)
+void test_matrix_exp()
 {
   vnl_vector<double> v(3);
   v[0] =  1.233;
@@ -22,9 +24,14 @@ int main(int, char **)
   
   vnl_matrix<double> expX = vnl_matrix_exp(X);
   vnl_matlab_print(vcl_cout, expX, "matrix exp([v])");
+  vnl_test_assert("expX(0,0)", vcl_abs(expX(0,0)-0.6221833130) < 1e-10);
+  vnl_test_assert("expX(0,1)", vcl_abs(expX(0,1)+0.7825192869) < 1e-10);
+  vnl_test_assert("expX(1,1)", vcl_abs(expX(1,1)-0.1379544126) < 1e-10);
+  vnl_test_assert("expX(2,2)", vcl_abs(expX(2,2)-0.2501918781) < 1e-10);
 
   vnl_matrix<double> rotv = vnl_rotation_matrix(v);
   vnl_matlab_print(vcl_cout, rotv, "rotate exp([v])");
-
-  return 0;
+  vnl_test_assert("rotv == expX", (rotv-expX).fro_norm() < 1e-10);
 }
+
+TESTMAIN(test_matrix_exp);
