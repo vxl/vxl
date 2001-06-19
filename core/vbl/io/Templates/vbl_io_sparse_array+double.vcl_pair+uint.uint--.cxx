4c36404c909fa03291ce3685fd306239d906d248
Unknown <targetjr>
2001-06-19 08:04:37 -0400

    it seems that SUNPRO has problems with these...

--- core/vbl/io/Templates/vbl_io_sparse_array+double.vcl_pair+uint.uint--.cxx
+++ core/vbl/io/Templates/vbl_io_sparse_array+double.vcl_pair+uint.uint--.cxx
@@ -1,4 +1,6 @@
 #include <vbl/io/vbl_io_sparse_array_base.txx>
 
+#ifndef VCL_SUNPRO_CC_50
 typedef vcl_pair<unsigned int, unsigned int> pair_uu;
 VBL_IO_SPARSE_ARRAY_BASE_INSTANTIATE(double, pair_uu);
+#endif
