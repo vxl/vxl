fc3be6eb046fd3fe84ef081038687ea4fbb85c68
Unknown <targetjr>
2001-06-27 05:12:40 -0400

    Fixed vtl/vbl rationalisation problem

--- core/vgui/client.mk
+++ core/vgui/client.mk
@@ -9,11 +9,12 @@ TRY_GLUT := 1
 TRY_HERMES := 1
 
 include $(IUELOCALROOT)/vxl/vbl/client.mk
+include $(IUELOCALROOT)/vxl/vul/client.mk
 include $(IUELOCALROOT)/vxl/vnl/client.mk
 include $(IUELOCALROOT)/vxl/vil/client.mk
 include $(IUELOCALROOT)/vxl/vgl/client.mk
 
-ald_libdeps += vgui:vpl,vbl,vnl-algo,vil,vgl
+ald_libdeps += vgui:vpl,vbl,vul,vnl-algo,vil,vgl
 ald_libdeps += vgui-SDL:vgui
 ald_libdeps += vgui-glut:vgui
 ald_libdeps += vgui-Fl:vgui
---
+++ core/vul/client.mk
@@ -0,0 +1,11 @@
+ifndef vul_client_mk
+vul_client_mk := 1
+#
+# fsm@robots.ox.ac.uk
+#
+
+include $(IUELOCALROOT)/vcl/client.mk
+
+ald_libdeps += vul
+
+endif
