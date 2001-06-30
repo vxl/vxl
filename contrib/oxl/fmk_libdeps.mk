300738aceaeea76d3e1c3a17a48f89e72ec5b5a5
Unknown <remcvs_tgtjr_oxford>
2001-06-30 04:31:41 -0400

    psync.pl (oxford): sun fix

--- contrib/oxl/fmk_libdeps.mk
+++ contrib/oxl/fmk_libdeps.mk
@@ -14,6 +14,8 @@ fmk_libdeps += lib_mvox,lib_oxp,lib_mvl,lib_vrml:pkg_oxl
 fmk_libdeps += lib_vgui:pkg_oxl
 fmk_libdeps += lib_vgui:lib_vpl,lib_vbl,lib_vnl-algo,lib_vil,lib_vgl
 fmk_libdeps += lib_vgui:lib_glut
+# needed for vgui_texture_hacks.cxx:
+fmk_libdeps += lib_vgui:lib_dl
 ifeq (1,$(tpp_HASPKG_HERMES))
 fmk_libdeps += lib_vgui:lib_Hermes
 endif
