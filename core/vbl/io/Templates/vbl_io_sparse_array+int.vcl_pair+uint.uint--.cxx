3472f16963c283eae0287409c4093810bf02491e
Unknown <remcvs_tgtjr_oxford>
2001-06-30 04:31:30 -0400

    psync.pl: should work now

--- core/vbl/io/Templates/vbl_io_sparse_array+int.vcl_pair+uint.uint--.cxx
+++ core/vbl/io/Templates/vbl_io_sparse_array+int.vcl_pair+uint.uint--.cxx
@@ -1,6 +1,4 @@
 #include <vbl/io/vbl_io_sparse_array_base.txx>
 
-#ifndef VCL_SUNPRO_CC_50
 typedef vcl_pair<unsigned int, unsigned int> pair_uu;
 VBL_IO_SPARSE_ARRAY_BASE_INSTANTIATE(int, pair_uu);
-#endif
