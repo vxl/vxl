b7b7cf83fe140ecbc9158935c2d93cbff6eb8abe
Unknown <targetjr>
2001-07-03 07:27:36 -0400

    new file

---
+++ core/vgl/vgl_fwd.h
@@ -0,0 +1,27 @@
+#ifndef vgl_fwd_h_
+#define vgl_fwd_h_
+// This is vxl/vgl/vgl_fwd.h
+
+template <class Type> class vgl_box_2d;
+template <class Type> class vgl_box_3d;
+template <class Type> class vgl_homg_line_2d;
+template <class Type> class vgl_homg_line_3d_2_points;
+template <class Type> class vgl_homg_plane_3d;
+template <class Type> class vgl_homg_point_2d;
+template <class Type> class vgl_homg_point_3d;
+template <class Type> class vgl_line_2d;
+template <class Type> class vgl_line_segment_2d;
+template <class Type> class vgl_line_segment_3d;
+template <class Type> class vgl_plane_3d;
+template <class Type> class vgl_point_2d;
+template <class Type> class vgl_point_3d;
+template <class Type> class vgl_plane_3d;
+template <class T> class vgl_vector_2d;
+template <class T> class vgl_vector_3d;
+class vgl_polygon;
+class vgl_polygon_scan_iterator;
+struct vgl_region_scan_iterator;
+struct vgl_triangle_scan_iterator;
+struct vgl_window_scan_iterator;
+
+#endif // vgl_fwd_h_
