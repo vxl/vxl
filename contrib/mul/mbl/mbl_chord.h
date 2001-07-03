383201f7d5911a07a86b97217ec35817ec55768e
Unknown <targetjr>
2001-07-03 07:31:06 -0400

    <TAB>s replaced by spaces

--- contrib/mul/mbl/mbl_chord.h
+++ contrib/mul/mbl/mbl_chord.h
@@ -13,40 +13,40 @@
 //: Horizontal line used in images, with integer co-ordinates
 class mbl_chord {
 private:
-	int start_x_;
-	int end_x_;
-	int y_;
+  int start_x_;
+  int end_x_;
+  int y_;
 public:
-      //: Constructor
-	mbl_chord() : start_x_(0),end_x_(-1),y_(0) {}
+    //: Constructor
+  mbl_chord() : start_x_(0),end_x_(-1),y_(0) {}
 
-	  //: Constructor
-	mbl_chord(int start_x, int end_x, int y)
-	  : start_x_(start_x), end_x_(end_x), y_(y) {}
+    //: Constructor
+  mbl_chord(int start_x, int end_x, int y)
+    : start_x_(start_x), end_x_(end_x), y_(y) {}
 
-      //: X-ordinate of start
-    int start_x() const { return start_x_; }
+    //: X-ordinate of start
+  int start_x() const { return start_x_; }
 
-      //: X-ordinate of end
-    int end_x() const { return end_x_; }
+    //: X-ordinate of end
+  int end_x() const { return end_x_; }
 
-	  //: y-ordinate
-    int y() const { return y_; }
+    //: y-ordinate
+  int y() const { return y_; }
 
-	  //: Length
-    int length() const { return 1+end_x_-start_x_; }
+    //: Length
+  int length() const { return 1+end_x_-start_x_; }
 
-	  //: Write to binary stream
-    inline void b_write(vsl_b_ostream& bfs) const;
+    //: Write to binary stream
+  inline void b_write(vsl_b_ostream& bfs) const;
 
-	  //: Read from binary stream
-    inline void b_read(vsl_b_istream& bfs);
+    //: Read from binary stream
+  inline void b_read(vsl_b_istream& bfs);
 
-	  //: Comparison
-	bool operator==(const mbl_chord& c) const;
+    //: Comparison
+  bool operator==(const mbl_chord& c) const;
 };
 
-	  //: Write to binary stream
+    //: Write to binary stream
 inline void mbl_chord::b_write(vsl_b_ostream& bfs) const
 {
   vsl_b_write(bfs,start_x_);
@@ -70,7 +70,7 @@ inline bool mbl_chord::operator==(const mbl_chord& c) const
 //: Print
 inline vcl_ostream& operator<<(vcl_ostream& os, const mbl_chord& c)
 {
-	return os<<"(["<<c.start_x()<<","<<c.end_x()<<"],"<<c.y()<<")";
+  return os<<"(["<<c.start_x()<<","<<c.end_x()<<"],"<<c.y()<<")";
 }
 
 //: Save
@@ -92,5 +92,3 @@ inline void vsl_print_summary(vcl_ostream& os, const mbl_chord& t)
 }
 
 #endif // mbl_chord_h_
-
-
