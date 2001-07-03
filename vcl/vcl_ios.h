4b83fa5f500289c61221828bd05d2f0183c1afd5
Unknown <targetjr>
2001-07-03 16:04:14 -0400

    include definitions in generic/vcl_ios.h

--- vcl/vcl_ios.h
+++ vcl/vcl_ios.h
@@ -14,9 +14,13 @@
 
 #if defined(VCL_SGI_CC_720)
 # include "sgi/vcl_ios.h"
+# define vcl_generic_ios_STD vcl_ios
+# include "generic/vcl_ios.h"
 
 #elif !VCL_CXX_HAS_HEADER_IOS
 # include "vcl_iostream.h" // should should do it
+# define vcl_generic_ios_STD vcl_ios
+# include "generic/vcl_ios.h"
 
 #else // -------------------- ISO
 # include "iso/vcl_ios.h"
