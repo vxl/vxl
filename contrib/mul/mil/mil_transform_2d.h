b151e95a55da8818c593245607b9a95e89e126ce
Unknown <targetjr>
2001-07-03 11:45:32 -0400

    vgl_vector_2.hd was not (yet) included from vgl_point_2d.h

--- contrib/mul/mil/mil_transform_2d.h
+++ contrib/mul/mil/mil_transform_2d.h
@@ -9,6 +9,7 @@
 // \author Tim Cootes
 // \brief Base class for sets of points in arbitrary dimension
 
+#include <vgl/vgl_vector_2d.h>
 #include <vgl/vgl_point_2d.h>
 #include <vnl/vnl_fwd.h>
 #include <vsl/vsl_binary_io.h>
