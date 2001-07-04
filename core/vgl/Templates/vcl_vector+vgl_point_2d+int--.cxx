a414dd31906d7041d3a6d90a9c02c215d90b661c
Unknown <targetjr>
2001-07-04 18:22:59 -0400

    *** empty log message ***

---
+++ core/vgl/Templates/vcl_vector+vgl_point_2d+int--.cxx
@@ -0,0 +1,7 @@
+#include <vgl/vgl_point_2d.h>
+
+#include <vcl_vector.txx>
+VCL_VECTOR_INSTANTIATE(vgl_point_2d<int>);
+
+#include <vcl_algorithm.txx>
+VCL_CONTAINABLE_INSTANTIATE(vgl_point_2d<int>);
