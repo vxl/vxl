88310d900d759c5976dc45422f225c13867b8ba0
Unknown <targetjr>
2001-07-08 04:45:32 -0400

    filename

---
+++ core/vil1/Templates/vil1_memory_image_of+vil1_rgb+vxl_uint_16--.cxx
@@ -0,0 +1,7 @@
+#include <vil/vil_rgb.h>
+#include <vil/vil_memory_image_of.txx>
+#include <vxl_config.h>
+
+typedef vxl_uint_16 word;
+
+VIL_MEMORY_IMAGE_OF_INSTANTIATE(vil_rgb<word>);
---
+++ core/vil1/Templates/vil1_rgb+vxl_uint_16-.cxx
@@ -0,0 +1,6 @@
+#include <vil/vil_rgb.txx>
+
+#include <vxl_config.h>
+
+VIL_RGB_INSTANTIATE(vxl_uint_16);
+VIL_RGB_INSTANTIATE_LS(vxl_uint_16);
