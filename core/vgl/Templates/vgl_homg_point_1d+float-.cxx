a5a44e1605f08ed27504b579da8f619bddde3048
Unknown <targetjr>
2001-07-09 03:59:46 -0400

    *** empty log message ***

---
+++ core/vgl/Templates/vgl_homg_point_1d+float-.cxx
@@ -0,0 +1,3 @@
+// Instantiation of vgl_homg_point_1d<float>
+#include <vgl/vgl_homg_point_1d.txx>
+VGL_HOMG_POINT_1D_INSTANTIATE(float);
---
+++ core/vgl/Templates/vgl_homg_point_1d+int-.cxx
@@ -0,0 +1,3 @@
+// Instantiation of vgl_homg_point_1d<int>
+#include <vgl/vgl_homg_point_1d.txx>
+VGL_HOMG_POINT_1D_INSTANTIATE(int);
