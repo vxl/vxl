7297dfd75b88a3f841c7f2322d41f4ceb4c1333f
Unknown <targetjr>
2001-07-09 04:00:38 -0400

    vgl_1d_basis and vgl_homg_point_1d added

--- core/vgl/vgl_fwd.h
+++ core/vgl/vgl_fwd.h
@@ -2,20 +2,20 @@
 #define vgl_fwd_h_
 // This is vxl/vgl/vgl_fwd.h
 
-template <class Type> class vgl_box_2d;
-template <class Type> class vgl_box_3d;
-template <class Type> class vgl_homg_line_2d;
-template <class Type> class vgl_homg_line_3d_2_points;
-template <class Type> class vgl_homg_plane_3d;
-template <class Type> class vgl_homg_point_2d;
-template <class Type> class vgl_homg_point_3d;
-template <class Type> class vgl_line_2d;
-template <class Type> class vgl_line_segment_2d;
-template <class Type> class vgl_line_segment_3d;
-template <class Type> class vgl_plane_3d;
-template <class Type> class vgl_point_2d;
-template <class Type> class vgl_point_3d;
-template <class Type> class vgl_plane_3d;
+template <class T> class vgl_box_2d;
+template <class T> class vgl_box_3d;
+template <class T> class vgl_homg_line_2d;
+template <class T> class vgl_homg_line_3d_2_points;
+template <class T> class vgl_homg_plane_3d;
+template <class T> class vgl_homg_point_1d;
+template <class T> class vgl_homg_point_2d;
+template <class T> class vgl_homg_point_3d;
+template <class T> class vgl_line_2d;
+template <class T> class vgl_line_segment_2d;
+template <class T> class vgl_line_segment_3d;
+template <class T> class vgl_plane_3d;
+template <class T> class vgl_point_2d;
+template <class T> class vgl_point_3d;
 template <class T> class vgl_vector_2d;
 template <class T> class vgl_vector_3d;
 class vgl_polygon;
