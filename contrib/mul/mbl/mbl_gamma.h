a9f8e0846cf2a18dd7267bf3487cac49ae5f0d73
Ian Scott <scottim@users.sourceforge.net>
2001-07-19 05:20:52 -0400

    added an erf function

--- contrib/mul/mbl/mbl_gamma.h
+++ contrib/mul/mbl/mbl_gamma.h
@@ -20,4 +20,9 @@ double mbl_gamma_q(double a, double x);
 //: log of Incomplete gamma function, Q(a,x)
 double mbl_log_gamma_q(double a, double x);
 
+//: The Error function
+// erf(x) = (2/sqrt(pi)) Integral from 0 to x (exp(-t^2) dt)
+inline double mbl_erf(double x) 
+{ return (x<0)?-mbl_gamma_p(0.5,x*x):mbl_gamma_p(0.5,x*x); };
+
 #endif // mbl_gamma_h
