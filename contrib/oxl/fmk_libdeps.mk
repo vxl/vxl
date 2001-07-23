89eceeac36c106d532a2f84553ca5cc9e420fae1
Unknown <remcvs_tgtjr_oxford>
2001-07-23 05:01:51 -0400

    psync.pl (oxford): add vul

--- contrib/oxl/fmk_libdeps.mk
+++ contrib/oxl/fmk_libdeps.mk
@@ -6,7 +6,7 @@ fmk_libdeps += lib_osl:lib_vil,lib_vul,lib_vnl-algo,lib_vbl,pkg_oxl
 
 fmk_libdeps += lib_mvox:lib_mvl
 fmk_libdeps += lib_oxp:lib_vbl,lib_vil,lib_vnl-algo
-fmk_libdeps += lib_mvl:lib_vnl-algo,lib_vgl,lib_vbl,lib_vil
+fmk_libdeps += lib_mvl:lib_vnl-algo,lib_vgl,lib_vbl,lib_vil,lib_vul
 fmk_libdeps += lib_vrml:lib_vgl,lib_vnl,lib_vbl
 
 fmk_libdeps += lib_mvox,lib_oxp,lib_mvl,lib_vrml:pkg_oxl
