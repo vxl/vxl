0ff80b8650f5a6972346763ce4bd77d48d997627
Unknown <remcvs_tgtjr_oxford>
2001-08-03 05:57:38 -0400

    psync.pl (oxford): fmk vul/vbl fix

--- contrib/oxl/fmk_libdeps.mk
+++ contrib/oxl/fmk_libdeps.mk
@@ -2,12 +2,13 @@
 # fsm@robots.ox.ac.uk
 #
 
-fmk_libdeps += lib_osl:lib_vil,lib_vul,lib_vnl-algo,lib_vbl,pkg_oxl
+fmk_libdeps += lib_osl:lib_vil,lib_vnl-algo,lib_vul,lib_vbl,pkg_oxl
 
 fmk_libdeps += lib_mvox:lib_mvl
-fmk_libdeps += lib_oxp:lib_vbl,lib_vil,lib_vnl-algo
-fmk_libdeps += lib_mvl:lib_vnl-algo,lib_vgl,lib_vbl,lib_vil,lib_vul
-fmk_libdeps += lib_vrml:lib_vgl,lib_vnl,lib_vbl
+
+fmk_libdeps += lib_oxp:lib_vul,lib_vbl,lib_vil,lib_vnl-algo
+fmk_libdeps += lib_mvl:lib_vnl-algo,lib_vgl,lib_vul,lib_vbl,lib_vil
+fmk_libdeps += lib_vrml:lib_vgl,lib_vnl,lib_vul,lib_vbl
 
 fmk_libdeps += lib_mvox,lib_oxp,lib_mvl,lib_vrml:pkg_oxl
 
