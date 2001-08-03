0409644432cf1f97fd69f59babe857306a2d0722
Unknown <targetjr>
2001-08-03 06:51:43 -0400

    removed space at end of line

--- core/doc/vxl_doc_rules.cxx
+++ core/doc/vxl_doc_rules.cxx
@@ -6,7 +6,7 @@
 
 #include "vxl_doc_rules.h"
 
-//: By all means replicate the documentation from the .h file here 
+//: By all means replicate the documentation from the .h file here
 // But it seems the doxygen does not reliably pick up documentation in
 // the .cxx file whereas we haven't had a problem with it picking it up
 // from the .h file.
