59ab65e1af287384328711f9e98a433ed91333d0
Ian Scott <scottim@users.sourceforge.net>
2001-08-14 15:19:03 -0400

    created a text properties reader for general use

---
+++ contrib/mul/mbl/mbl_read_props.cxx
@@ -0,0 +1,77 @@
+#include "mbl/mbl_read_props.h"
+
+//: Read properties from a text stream.
+// The function will terminate on an eof. If one of
+// the opening lines contains an opening brace '{', then the function
+// will also stop reading the stream after finding a line containing
+// a closing brace '}'
+//
+// There should be one property per line, with a colon ':' after
+// each property label. Each property label should not contain
+// any whitespace.
+mbl_read_props_type mbl_read_props(vcl_istream &afs)
+{
+  if (!afs) return mbl_read_props_type();
+
+  vcl_string label, str1;
+
+  while (afs>>vcl_ws, !afs.eof())
+  {
+
+    afs>>label;
+    if (label.substr(0,2) =="//")
+    {
+      // Comment line, so read to end
+      vcl_getline(afs, str1);
+    }
+    else break;
+  }
+  bool need_closing_brace = false;
+
+  if (label[0] == '{')
+    need_closing_brace = true;
+
+  mbl_read_props_type props;
+
+  while (afs>>vcl_ws, !afs.eof())
+  {
+    afs >> label;
+    if (label.substr(0,2) =="//")
+    {
+      // Comment line, so read to end
+      vcl_getline(afs, str1);
+    }
+    else if (need_closing_brace && label[0] == '}')
+    {
+      // Strip rest of line
+      return props;
+    }
+    else
+    {
+      if (label.size() > 1 && label[label.size() -1] == ':')
+        label.erase(label.size() -1, 1);
+      else
+      {
+        char c;
+        afs >> vcl_ws;
+        afs >> c;
+        if (c != ':')
+        {
+          vcl_getline(afs, str1);
+          vcl_cerr << "ERROR: mbl_read_props. Could not find colon ':'"
+            <<"separator while reading line" <<
+            label << " " << str1 << vcl_endl;
+          return props;
+        }
+      }
+      afs >> vcl_ws;
+      vcl_getline(afs, str1);
+      props[label] = str1;
+    }
+  }
+  if (need_closing_brace)
+    vcl_cerr << "ERROR: mbl_read_props. Unexpected end of file while " <<
+    "looking for '}'" <<vcl_endl;
+  return props;
+}
+
---
+++ contrib/mul/mbl/mbl_read_props.h
@@ -0,0 +1,28 @@
+#ifndef mbl_read_props_h
+#define mbl_read_props_h
+
+//:
+// \file
+// \author Ian Scott
+// \date 14-Aug-2001
+// \brief Load properties from text files
+
+#include <vcl_map.h>
+#include <vcl_iostream.h>
+#include <vcl_string.h>
+
+//: The type of the property dictionary
+typedef vcl_map<vcl_string, vcl_string > mbl_read_props_type;
+
+//: Read properties from a text stream.
+// The function will terminate on an eof. If one of
+// the opening lines contains an opening brace '{', then the function
+// will also stop reading the stream after finding a line containing
+// a closing brace '}'
+//
+// There should be one property per line, with a colon ':' after
+// each property label. Each property label should not contain
+// any whitespace.
+mbl_read_props_type mbl_read_props(vcl_istream &afs);
+
+#endif // mbl_read_props_h
