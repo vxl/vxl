7f8c14858d7f86ce601cc09565677fcfbae8ce4f
Ian Scott <scottim@users.sourceforge.net>
2001-08-17 11:13:26 -0400

    added missing binary loader stuff for mil

---
+++ contrib/mul/mil/Templates/vsl_binary_loader+mil_image-.cxx
@@ -0,0 +1,3 @@
+#include <vsl/vsl_binary_loader.txx>
+#include <mil/mil_image.h>
+VSL_BINARY_LOADER_INSTANTIATE(mil_image);
---
+++ contrib/mul/mil/mil_all.h
@@ -0,0 +1,19 @@
+#ifndef mil_all_h_
+#define mil_all_h_
+
+#include <mil/mil_byte_image_2d_io.h>
+#include <mil/mil_gaussian_pyramid_builder_2d.h>
+#include <mil/mil_gaussian_pyramid_builder_2d_general.h>
+#include <mil/mil_image_2d_of.h>
+#include <mil/mil_image_data.h>
+
+static void mil_add_all_loaders()
+{
+  vsl_add_to_binary_loader(mil_byte_image_2d_io());
+  vsl_add_to_binary_loader(mil_gaussian_pyramid_builder_2d<vil_byte>());
+  vsl_add_to_binary_loader(mil_gaussian_pyramid_builder_2d_general<vil_byte>());
+
+  vsl_add_to_binary_loader(mil_image_2d_of<vil_byte>());
+}
+
+#endif mil_all_h_
