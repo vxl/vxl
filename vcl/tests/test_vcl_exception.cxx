fbbb9561ec8ec25b1817612b495cbe7d77fe7c73
Amitha Perera <amithaperera@users.sourceforge.net>
2001-08-20 13:36:14 -0400

    abort macro magic won't work with libstdc++-v3. Made test return 0 on success, 1 on failure.

--- vcl/tests/test_vcl_exception.cxx
+++ vcl/tests/test_vcl_exception.cxx
@@ -1,25 +1,20 @@
-
-#define abort my_abort
-extern "C" void my_abort();
-
 #include <vcl_exception.h>
 
 #include <vcl_iostream.h>
 
-extern "C" void my_abort()
-{
-  vcl_cout << "abort()\n";
-}
-
 int main()
 {
+  int result;
   vcl_try {
-    vcl_throw "Bad something....\n";
+    vcl_throw "\"const char* exception\"";
   }
-  vcl_catch (char* e) {
-    vcl_cout << "caught " << e << vcl_endl;
+  vcl_catch (const char* e) {
+    vcl_cout << "caught " << e << ". Good." << vcl_endl;
+    result = 1;
   }
   vcl_catch_all {
-    vcl_cout << "caught nuffink " << vcl_endl;
+    vcl_cout << "caught nuffink. Bad. " << vcl_endl;
+    result = 0;
   }
+  return result;
 }
