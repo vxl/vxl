8ab159c00cfac531c198d0f73b874db3a46ade0b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-08-22 08:45:39 -0400

    These template instances belong to Qv, not vgui/vrml

---
+++ v3p/Qv/Templates/vcl_vector+point3D~-.cxx
@@ -0,0 +1,4 @@
+#include <vcl_vector.txx>
+
+class point3D;
+VCL_VECTOR_INSTANTIATE(point3D*);
---
+++ v3p/Qv/Templates/vcl_vector+vector3D_const~-.cxx
@@ -0,0 +1,4 @@
+#include <vcl_vector.txx>
+
+class vector3D;
+VCL_VECTOR_INSTANTIATE(vector3D const*);
