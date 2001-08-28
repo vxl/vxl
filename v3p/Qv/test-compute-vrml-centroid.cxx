e364d2c6e49c81f208fb5933c348450def302127
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-08-28 12:14:09 -0400

    bug fixed in case this program is run without a command line argument

--- v3p/Qv/test-compute-vrml-centroid.cxx
+++ v3p/Qv/test-compute-vrml-centroid.cxx
@@ -4,12 +4,6 @@
 // Purpose: None, totally pointless
 // Author: Andrew W. Fitzgibbon, Oxford RRG
 // Created: 10 Jul 00
-// Compile with:
-//
-// $CXX -I$IUEROOT/vxl -I$IUEROOT/vxl/config.$TJ_BUILD_TAG -I$IUEROOT/v3p test-compute-vrml-centroid.cxx \
-//    -L$IUEROOT/v3p/lib/$TJ_BUILD_TAG -L$IUEROOT/vxl/lib/$TJ_BUILD_TAG \
-//    -Wl,-rpath,$IUEROOT/v3p/lib/$TJ_BUILD_TAG:$IUEROOT/vxl/lib/$TJ_BUILD_TAG \
-//    -lQv -lvcl
 //
 // Modifications:
 //
@@ -19,7 +13,7 @@
 
 int main(int argc, char ** argv)
 {
-  QvVrmlFile f(argv[1]);
+  QvVrmlFile f((argc>1) ? argv[1] : "test.wrl");
   f.compute_centroid_radius();
   return -1;
 }
