1efbe44bb9fda1f960a34c92dfb5ca4ab381d2b9
Tim Cootes <timcootes@users.sourceforge.net>
2001-09-01 05:28:18 -0400

    Added class to convert between image types

---
+++ contrib/mul/mil/mil_convert_image_2d.cxx
@@ -0,0 +1,86 @@
+//: \file
+//  \brief Functions to convert images from one type to another
+//  \author Tim Cootes
+
+#include <mil/mil_convert_image_2d.h>
+
+//: Copys src_im (of bytes) into dest_im (of float)
+void mil_convert_image_2d(mil_image_2d_of<float>& dest,
+                       const mil_image_2d_of<vil_byte>& src)
+{
+  int n = src.n_planes();
+	int nx = src.nx();
+	int ny = src.ny();
+	dest.resize(nx,ny,n);
+	int sxstep=src.xstep();
+	int systep=src.ystep();
+	int dxstep=dest.xstep();
+	int dystep=dest.ystep();
+	for (int i=0;i<n;++i)
+	{
+	  const vil_byte* s_row = src.plane(i);
+		float* d_row = dest.plane(i);
+
+	  for (int y=0;y<ny;++y)
+		{
+      const vil_byte* s = s_row;
+			float * d = d_row;
+
+			for (int x=0;x<nx;++x)
+			{
+			  *d = float(*s);
+				s+=sxstep;
+				d+=dxstep;
+      }
+
+			s_row += systep;
+			d_row += dystep;
+		}
+	}
+}
+
+//: Copys src_im (of float) into dest_im (of byte)
+void mil_convert_image_2d(mil_image_2d_of<vil_byte>& dest,
+                       const mil_image_2d_of<float>& src)
+{
+  mil_convert_image_2d(dest,src,1.0,0);
+}
+
+//: Copys src_im (of float) into dest_im (of byte) after linear transform
+//  Applies scale and offset to pixels of src_im and puts results in dest_im
+void mil_convert_image_2d(mil_image_2d_of<vil_byte>& dest,
+                       const mil_image_2d_of<float>& src,
+											 double scale, double offset)
+{
+  int n = src.n_planes();
+	int nx = src.nx();
+	int ny = src.ny();
+	dest.resize(nx,ny,n);
+	int sxstep=src.xstep();
+	int systep=src.ystep();
+	int dxstep=dest.xstep();
+	int dystep=dest.ystep();
+	for (int i=0;i<n;++i)
+	{
+	  const float* s_row = src.plane(i);
+		vil_byte* d_row = dest.plane(i);
+
+	  for (int y=0;y<ny;++y)
+		{
+      const float* s = s_row;
+			vil_byte * d = d_row;
+
+			for (int x=0;x<nx;++x)
+			{
+			  *d = vil_byte(*s * scale + offset);
+				s+=sxstep;
+				d+=dxstep;
+      }
+
+			s_row += systep;
+			d_row += dystep;
+		}
+	}
+}
+
+
---
+++ contrib/mul/mil/mil_convert_image_2d.h
@@ -0,0 +1,29 @@
+#ifndef mil_convert_image_2d_h_
+#define mil_convert_image_2d_h_
+
+//: \file
+//  \brief Functions to convert images from one type to another
+//  \author Tim Cootes
+
+#include <mil/mil_image_2d_of.h>
+#include <vil/vil_byte.h>
+
+//: Copys src_im (of bytes) into dest_im (of float)
+void mil_convert_image_2d(mil_image_2d_of<float>& dest_im,
+                       const mil_image_2d_of<vil_byte>& src_im);
+
+//: Copys src_im (of float) into dest_im (of byte)
+void mil_convert_image_2d(mil_image_2d_of<vil_byte>& dest_im,
+                       const mil_image_2d_of<float>& src_im);
+
+//: Copys src_im (of float) into dest_im (of byte) after linear transform
+//  Applies scale and offset to pixels of src_im and puts results in dest_im
+//  Useful to compress range of data for display
+void mil_convert_image_2d(mil_image_2d_of<vil_byte>& dest_im,
+                       const mil_image_2d_of<float>& src_im,
+											 double scale, double offset);
+
+
+#endif // mil_convert_image_2d_h_
+
+
