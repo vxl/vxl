e4675717b4bb6bda37a42d02680f75e05cd899d4
Amitha Perera <amithaperera@users.sourceforge.net>
2001-09-06 14:48:46 -0400

    Added comments

--- core/vgl/vgl_region_scan_iterator.h
+++ core/vgl/vgl_region_scan_iterator.h
@@ -15,10 +15,22 @@ struct vgl_region_scan_iterator
   inline vgl_region_scan_iterator() { }
   virtual ~vgl_region_scan_iterator();
 
+  //: Resets the scan iterator to before the first scan line
+  //  After calling this function, next() needs to be called before
+  //  startx() and endx() form a valid scan line.
   virtual void reset() =0;
+
+  //: Tries to moves to the next scan line.
+  //  Returns false if there are no more scan lines.
   virtual bool next() =0;
+
+  //: y-coordinate of the current scan line
   virtual int  scany() const =0;
+
+  //: Returns starting x-value of the current scan line.
   virtual int  startx() const =0;
+
+  //: Returns starting x-value of the current scan line.
   virtual int  endx() const =0;
 };
 
