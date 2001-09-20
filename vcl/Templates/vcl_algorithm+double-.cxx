965588796cf01d18cab3bd01e3031d2ef968f402
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-09-20 06:20:23 -0400

    missing instantiation added

--- vcl/Templates/vcl_algorithm+double-.cxx
+++ vcl/Templates/vcl_algorithm+double-.cxx
@@ -4,6 +4,9 @@
 # include <vcl_vector.h>
 VCL_SORT_INSTANTIATE(vcl_vector<double>::iterator, double);
 VCL_SORT_INSTANTIATE(vcl_vector<double*>::iterator, double*);
+namespace std {
+  template double* std::fill_n<double*, unsigned long, double>(double*, unsigned long, double const&);
+}
 #endif
 
 VCL_SORT_INSTANTIATE(double*, double);
