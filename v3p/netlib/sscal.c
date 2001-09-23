181405429fee530277cf566e503800cedb232433
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-09-23 03:54:54 -0400

    Parameter adjustments removed

--- v3p/netlib/sscal.c
+++ v3p/netlib/sscal.c
@@ -10,24 +10,15 @@ integer *n;
 real *sa, *sx;
 integer *incx;
 {
-    /* System generated locals */
-    integer i__1, i__2;
-
     /* Local variables */
-    static integer i, m, nincx, mp1;
+    static integer i, m, nincx;
 
+/*     scales a vector by a constant.					*/
+/*     uses unrolled loops for increment equal to 1.			*/
+/*     jack dongarra, linpack, 3/11/78.					*/
+/*     modified 3/93 to return if incx .le. 0.				*/
+/*     modified 12/3/93, array(1) declarations changed to array(*)	*/
 
-/*     scales a vector by a constant. */
-/*     uses unrolled loops for increment equal to 1. */
-/*     jack dongarra, linpack, 3/11/78. */
-/*     modified 3/93 to return if incx .le. 0. */
-/*     modified 12/3/93, array(1) declarations changed to array(*) */
-
-
-    /* Parameter adjustments */
-    --sx;
-
-    /* Function Body */
     if (*n <= 0 || *incx <= 0) {
         return;
     }
@@ -38,16 +29,12 @@ integer *incx;
 /*        code for increment not equal to 1 */
 
     nincx = *n * *incx;
-    i__1 = nincx;
-    i__2 = *incx;
-    for (i = 1; i__2 < 0 ? i >= i__1 : i <= i__1; i += i__2) {
+    for (i = 0; i < nincx; i += *incx) {
         sx[i] = *sa * sx[i];
-/* L10: */
     }
     return;
 
 /*        code for increment equal to 1 */
-
 
 /*        clean-up loop */
 
@@ -56,24 +43,19 @@ L20:
     if (m == 0) {
         goto L40;
     }
-    i__2 = m;
-    for (i = 1; i <= i__2; ++i) {
+    for (i = 0; i < m; ++i) {
         sx[i] = *sa * sx[i];
-/* L30: */
     }
     if (*n < 5) {
         return;
     }
 L40:
-    mp1 = m + 1;
-    i__2 = *n;
-    for (i = mp1; i <= i__2; i += 5) {
+    for (i = m; i < *n; i += 5) {
         sx[i] = *sa * sx[i];
-        sx[i + 1] = *sa * sx[i + 1];
-        sx[i + 2] = *sa * sx[i + 2];
-        sx[i + 3] = *sa * sx[i + 3];
-        sx[i + 4] = *sa * sx[i + 4];
-/* L50: */
+        sx[i+1] = *sa * sx[i+1];
+        sx[i+2] = *sa * sx[i+2];
+        sx[i+3] = *sa * sx[i+3];
+        sx[i+4] = *sa * sx[i+4];
     }
     return;
 } /* sscal_ */
