a5af734e63fcdc3b3cf4c9939f67296f4ef90c9b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-09-26 07:25:09 -0400

    computation simplified, and fixed a problem when D slightly negative

--- core/vgl/vgl_ellipse_scan_iterator.cxx
+++ core/vgl/vgl_ellipse_scan_iterator.cxx
@@ -5,11 +5,9 @@
 #include <vgl/vgl_ellipse_scan_iterator.h>
 
 #include <vcl_cmath.h>
-#include <vcl_cassert.h>
 
 // Helper functions
 namespace {
-  inline double my_min( double x, double y ) { return x<y ? x : y; }
   inline double my_max( double x, double y ) { return x<y ? y : x; }
 }
 
@@ -37,14 +35,13 @@ vgl_ellipse_scan_iterator::reset()
   if( vcl_sin( theta_ ) == 0.0 ) {
     y0 = vcl_sqrt(ry_);
   } else {
-    double t = vcl_atan( vcl_sqrt(ry_) / (vcl_sqrt(rx_) * vcl_tan( theta_ )) );
+    double t = vcl_atan2( vcl_sqrt(ry_) , vcl_sqrt(rx_) * vcl_tan( theta_ ) );
     y0 = vcl_sqrt(rx_) * vcl_cos( t ) * vcl_sin( theta_ ) + vcl_sqrt(ry_) * vcl_sin( t ) * vcl_cos( theta_ );
   }
   if( y0 < 0 ) y0 = -y0;
 
   y_ = int( vcl_floor( yc_ + y0 ) ) + 1;
   min_y_ = int( vcl_ceil( yc_ - y0 ) );
-  
 }
 
 bool
@@ -56,27 +53,28 @@ vgl_ellipse_scan_iterator::next()
   double st = vcl_sin( -theta_ );
   double ct = vcl_cos( -theta_ );
   double A = rx_ * st * st + ry_ * ct * ct;
-  double B = 2.0 * (rx_ - ry_) * (y_-yc_) * ct * st;
-  double C = - rx_ * ry_ + (rx_ * ct * ct + ry_ * st * st) * (y_-yc_) * (y_-yc_);
 
-  double x0, x1; // the intersection points of the scan line
+  double x0, x1; // the intersection points of the scan line; x0 >= x1
 
   if( A > 0 ) {
     // not a denegerate horizontal line
     //
-    assert( B*B - 4*A*C >= 0 );
-    
-    x0 = (-B + vcl_sqrt( B*B - 4*A*C )) / (2.0 * A);
-    x1 = (-B - vcl_sqrt( B*B - 4*A*C )) / (2.0 * A);
+    double B = (rx_ - ry_) * (y_-yc_) * ct*st;
+//  double C = - rx_*ry_ + (rx_*ct*ct + ry_*st*st)*(y_-yc_)*(y_-yc_);
+    double D = rx_*ry_*(rx_*st*st + ry_*ct*ct - (y_-yc_)*(y_-yc_)); // = B*B-A*C
+    if (D < 0) D=0; // could be slightly < 0 due to rounding errors
+
+    x0 = (-B + vcl_sqrt( D )) / A;
+    x1 = (-B - vcl_sqrt( D )) / A;
   } else {
     // "ellipse" is a horizontal line or a point
     //
-    x1 = vcl_sqrt( my_max(rx_,ry_) );
-    x0 = -x1;
+    x0 = vcl_sqrt( my_max(rx_,ry_) );
+    x1 = -x0;
   }
 
-  start_x_ = int( vcl_ceil( xc_ + my_min(x0,x1) ) );
-  end_x_ = int( vcl_floor( xc_ + my_max(x0,x1) ) );
+  start_x_= int( vcl_ceil( xc_ + x1 - 1e-9 ) ); // avoid problems with rounding
+  end_x_ = int( vcl_floor( xc_ + x0 + 1e-9 ) ); // by slightly shifting.
 
   if( start_x_ > end_x_ ) {
     // Could happen with very thin ellipses, near the end points
