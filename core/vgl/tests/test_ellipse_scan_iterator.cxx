5867f61fafff29d33085a389721437ada2b921d8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-09-26 07:27:27 -0400

    local function count() now moved to class vgl_region_scan_iterator

--- core/vgl/tests/test_ellipse_scan_iterator.cxx
+++ core/vgl/tests/test_ellipse_scan_iterator.cxx
@@ -2,137 +2,80 @@
 
 #include <vgl/vgl_ellipse_scan_iterator.h>
 
-int count( vgl_ellipse_scan_iterator& iter )
-{
-  int cnt = 0;
-  iter.reset();
-  while( iter.next() ) {
-    if( iter.startx() > iter.endx() )
-      return -1;
-    cnt += iter.endx() - iter.startx() + 1;
-  }
-  return cnt;
-}
-
 void
 test_circle()
 {
   // Scan convert the unit circle
-  vgl_test_begin("unit circle");
   vgl_ellipse_scan_iterator circle1( 0, 0, 1, 1, 0 );
-  vgl_test_perform( count( circle1 ) == 5 );
+  TEST("unit circle", circle1.count(), 5 );
 
-  vgl_test_begin("offset unit circle");
   vgl_ellipse_scan_iterator circle2( 10, 10, 1, 1, 0 );
-  vgl_test_perform( count( circle2 ) == 5 );
+  TEST("offset unit circle", circle2.count(), 5 );
 
-  vgl_test_begin("rotated unit circle");
   vgl_ellipse_scan_iterator circle3( 0, 0, 1, 1, 0.2 );
-  vgl_test_perform( count( circle3 ) == 5 );
+  TEST("rotated unit circle", circle3.count(), 5 );
 
-  vgl_test_begin("radius 3 circle");
   vgl_ellipse_scan_iterator circle4( 0, 0, 3, 3, 0 );
-  vgl_test_perform( count( circle4 ) == 29 );
+  TEST("radius 3 circle", circle4.count(), 29 );
 
-  vgl_test_begin("offset radius 3 circle");
   vgl_ellipse_scan_iterator circle5( -5, -5, 3, 3, 0 );
-  vgl_test_perform( count( circle5 ) == 29 );
+  TEST("offset radius 3 circle", circle5.count(), 29 );
 
-  vgl_test_begin("offset, rotated radius 3 circle");
   vgl_ellipse_scan_iterator circle6( 20, 5, 3, 3, 3.6 );
-  vgl_test_perform( count( circle6 ) == 29 );
+  TEST("offset, rotated radius 3 circle", circle6.count(), 29 );
 }
 
 void
 test_ellipse()
 {
   // Scan convert simple ellipses
-  vgl_test_begin("centred ellipse radius 1,2");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0, 0, 1, 2, 0 );
-    vgl_test_perform( count( ellipse ) == 7 );
-  }
+  vgl_ellipse_scan_iterator ellipse1( 0, 0, 1, 2, 0 );
+  TEST("centred ellipse radius 1,2", ellipse1.count(), 7 );
 
-  vgl_test_begin("offset ellipse radius 2,1");
-  {
-    vgl_ellipse_scan_iterator ellipse( 10, -10, 2, 1, 0 );
-    vgl_test_perform( count( ellipse ) == 7 );
-  }
+  vgl_ellipse_scan_iterator ellipse2( 10, -10, 2, 1, 0 );
+  TEST("offset ellipse radius 2,1", ellipse2.count(), 7 );
 
-  vgl_test_begin("centred, rotated ellipse radius 2,1");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0, 0, 2, 1, 0.1 );
-    vgl_test_perform( count( ellipse ) == 5 );
-  }
+  vgl_ellipse_scan_iterator ellipse3( 0, 0, 2, 1, 0.1 );
+  TEST("centred, rotated ellipse radius 2,1", ellipse3.count(), 5 );
 
-  vgl_test_begin("centred, rotated ellipse radius 2,3");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0, 0, 2, 3, -3.1415/4.0 );
-    vgl_test_perform( count( ellipse ) == 19 );
-  }
+  vgl_ellipse_scan_iterator ellipse4( 0, 0, 2, 3, -3.1415/4.0 );
+  TEST("centred, rotated ellipse radius 2,3", ellipse4.count(), 19 );
 }
 
 void
 test_sliver()
 {
-  vgl_test_begin("vertical sliver: 0.1,3");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0, 0, 0.1, 3, 0 );
-    vgl_test_perform( count( ellipse ) == 7 );
-  }
+  vgl_ellipse_scan_iterator ellipse1( 0, 0, 0.1, 3, 0 );
+  TEST("vertical sliver: 0.1,3", ellipse1.count(), 7 );
 
-  vgl_test_begin("horizontal sliver: 2,0.1");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0, 0, 2, 0.1, 0 );
-    vgl_test_perform( count( ellipse ) == 5 );
-  }
+  vgl_ellipse_scan_iterator ellipse2( 0, 0, 2, 0.1, 0 );
+  TEST("horizontal sliver: 2,0.1", ellipse2.count(), 5 );
 
-  vgl_test_begin("horizontal sliver: 2,0.1");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0, 0, 2, 0.1, 0 );
-    vgl_test_perform( count( ellipse ) == 5 );
-  }
+  vgl_ellipse_scan_iterator ellipse3( 0, 0, 2, 0.1, 0 );
+  TEST("horizontal sliver: 2,0.1", ellipse3.count(), 5 );
 
-  vgl_test_begin("rotated sliver");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0, 0, 0.1, 3, 1.4 );
-    vgl_test_perform( count( ellipse ) == 1 );
-  }
+  vgl_ellipse_scan_iterator ellipse4( 0, 0, 0.1, 3, 1.4 );
+  TEST("rotated sliver", ellipse4.count(), 1 );
 }
 
 
 void
 test_degenerate()
 {
-  vgl_test_begin("empty ellipse");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0.5, 0.5, 0.3, 0.1, 0 );
-    vgl_test_perform( count( ellipse ) == 0 );
-  }
+  vgl_ellipse_scan_iterator ellipse1( 0.5, 0.5, 0.3, 0.1, 0 );
+  TEST("empty ellipse", ellipse1.count(), 0 );
 
-  vgl_test_begin("horizontal line");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0, 0, 2, 0, 0 );
-    vgl_test_perform( count( ellipse ) == 5 );
-  }
+  vgl_ellipse_scan_iterator ellipse2( 0, 0, 2, 0, 0 );
+  TEST("horizontal line", ellipse2.count(), 5 );
 
-  vgl_test_begin("vertical line");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0, 0, 0, 2, 0 );
-    vgl_test_perform( count( ellipse ) == 5 );
-  }
+  vgl_ellipse_scan_iterator ellipse3( 0, 0, 0, 2, 0 );
+  TEST("vertical line", ellipse3.count(), 5 );
 
-  vgl_test_begin("point at integer coordinates");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0, 0, 0, 0, 0 );
-    vgl_test_perform( count( ellipse ) == 1 );
-  }
+  vgl_ellipse_scan_iterator ellipse4( 0, 0, 0, 0, 0 );
+  TEST("point at integer coordinates", ellipse4.count(), 1 );
 
-  vgl_test_begin("point at non-integer coordinates");
-  {
-    vgl_ellipse_scan_iterator ellipse( 0.1, 0.1, 0, 0, 0 );
-    vgl_test_perform( count( ellipse ) == 0 );
-  }
+  vgl_ellipse_scan_iterator ellipse5( 0.1, 0.1, 0, 0, 0 );
+  TEST("point at non-integer coordinates", ellipse5.count(), 0 );
 }
 
 int main()
