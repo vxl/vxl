40469e2b235005a9fd396c1a2064f702b809cd37
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-09-26 02:58:39 -0400

    <TAB>s replaced by spaces

--- vcl/tests/test_vcl_root_dir.cxx
+++ vcl/tests/test_vcl_root_dir.cxx
@@ -4,7 +4,6 @@
 
 int main()
 {
-
   // Check that a file exists
   vcl_string path = vcl_root_dir() + "/vcl/vcl_root_dir.h";
 
@@ -12,14 +11,13 @@ int main()
 
   if (!is)
   {
-    vcl_cerr<<"Unable to open "<<path<<" vcl_root_dir() is probably wrong."<<vcl_endl;
-	vcl_cerr<<"Try setting $VXLSRC to the source root directory."<<vcl_endl;
-	return 1;
+    vcl_cerr<<"Unable to open "<<path<<"\nvcl_root_dir() is probably wrong.\n"
+              "Try setting $VXLSRC to the source root directory."<<vcl_endl;
+    return 1;
   }
   is.close();
 
   vcl_cout<<"Root Dir: "<<vcl_root_dir()<<" appears to be correct."<<vcl_endl;
-
 
   return 0;
 }
