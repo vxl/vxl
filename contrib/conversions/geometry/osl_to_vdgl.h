8755da7e1aa04bf9b0e6cdb07389807150b2cc46
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-09-27 11:37:58 -0400

    more conversions added: osl_edgel_chain to vdgl_digital_curve

---
+++ contrib/conversions/geometry/osl_to_vdgl.h
@@ -0,0 +1,20 @@
+#ifndef osl_to_vdgl_h_
+#define osl_to_vdgl_h_
+
+#include <osl/osl_edgel_chain.h>
+#include <vdgl/vdgl_digital_curve.h>
+#include <vdgl/vdgl_interpolator_linear.h>
+#include <vcl_cassert.h>
+
+inline vdgl_digital_curve osl_to_vdgl(osl_edgel_chain const& dc)
+{
+  vcl_vector<vdgl_edgel> edgels;
+  for (int i=0; i<dc.size(); ++i)
+  {
+    // Create a vdgl_edgel and push it on the edgel list:
+    edgels.push_back(vdgl_edgel(dc.x[i],dc.y[i],dc.grad[i],dc.theta[i]));
+  }
+  return vdgl_digital_curve(new vdgl_interpolator_linear(new vdgl_edgel_chain(edgels)));
+}
+
+#endif // osl_to_vdgl_h_
---
+++ contrib/conversions/geometry/tests/test_osl_to_vdgl.cxx
@@ -0,0 +1,23 @@
+#include <geometry/osl_to_vdgl.h>
+#include <osl/osl_edgel_chain.h>
+#include <vdgl/vdgl_digital_curve.h>
+#include <vsol/vsol_point_2d.h>
+#include <vgl/vgl_test.h>
+
+void test_osl_to_vdgl() {
+  osl_edgel_chain dc(7);
+  float x[] = { 1, 2, 3, 2, 1, 0, -1 };
+  float y[] = { 0, 1, 3, 2, 1, 0, -1 };
+  for (int i=0; i<7; ++i) {
+    dc.SetX(x[i], i);
+    dc.SetY(y[i], i);
+  }
+  vdgl_digital_curve vdc = osl_to_vdgl(dc);
+  vsol_point_2d_sptr p0 = vdc.p0();
+  TEST("p0()", p0->x()==1 && p0->y()==0, true);
+  vsol_point_2d_sptr p1 = vdc.p1();
+  TEST("p1()", p1->x()==-1 && p1->y()==-1, true);
+  TEST("midpt", vdc.get_x(0.5)==2 && vdc.get_y(0.5)==2, true);
+}
+
+TESTMAIN(test_osl_to_vdgl);
