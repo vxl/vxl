877c0096826b1c5a9921967e9490699f06fc25bb
Ian Scott <scottim@users.sourceforge.net>
2001-10-01 13:30:01 -0400

    Template instantiations should always go in the lowest level library that they reference

--- core/vbl/io/Templates/vbl_io_sparse_array+double.uint-.cxx
+++ core/vbl/io/Templates/vbl_io_sparse_array+double.uint-.cxx
@@ -1,5 +1,4 @@
 #include <vbl/io/vbl_io_sparse_array_base.txx>
-#include <vsl/vsl_pair_io.txx>
 
 VBL_IO_SPARSE_ARRAY_BASE_INSTANTIATE(double, unsigned int);
-VSL_PAIR_INSTANTIATE(double, unsigned int);
+
--- core/vbl/io/Templates/vbl_io_sparse_array+int.uint-.cxx
+++ core/vbl/io/Templates/vbl_io_sparse_array+int.uint-.cxx
@@ -1,5 +1,3 @@
 #include <vbl/io/vbl_io_sparse_array_base.txx>
-#include <vsl/vsl_pair_io.txx>
 
 VBL_IO_SPARSE_ARRAY_BASE_INSTANTIATE(int, unsigned int);
-VSL_PAIR_INSTANTIATE(int, unsigned int);
