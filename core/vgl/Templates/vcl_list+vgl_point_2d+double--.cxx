ddf06d66b58a661c5bde77c6021f485fc8c0c29d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-10-09 03:42:25 -0400

    template instantiation

---
+++ core/vgl/Templates/vcl_list+vgl_point_2d+double--.cxx
@@ -0,0 +1,4 @@
+#include <vcl_list.txx>
+#include <vgl/vgl_point_2d.h>
+VCL_DO_NOT_INSTANTIATE(void vcl_list<vgl_point_2d<double> >::merge(vcl_list<vgl_point_2d<double> > &), VCL_VOID_RETURN);
+VCL_LIST_INSTANTIATE(vgl_point_2d<double>);
