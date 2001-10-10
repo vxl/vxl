1a048c676f12f1c7d3a840baf5546e1383dfded9
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-10-10 06:03:46 -0400

    avoid macro redefinition problems

--- contrib/gel/vgel/kl/base.h
+++ contrib/gel/vgel/kl/base.h
@@ -25,10 +25,11 @@
 #define ulong unsigned long
 #endif
 
+#undef max
 #define max(a,b)    ((a) > (b) ? (a) : (b))
+#undef min
 #define min(a,b)    ((a) < (b) ? (a) : (b))
 #define max3(a,b,c) ((a) > (b) ? max((a),(c)) : max((b),(c)))
 #define min3(a,b,c) ((a) < (b) ? min((a),(c)) : min((b),(c)))
 
 #endif
-
