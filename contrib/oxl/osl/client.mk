9c5ea04a6eef5a3599f363862c4728764248694b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-10-11 16:50:55 -0400

    osl_fit_circle depends on vgl

--- contrib/oxl/osl/client.mk
+++ contrib/oxl/osl/client.mk
@@ -7,7 +7,8 @@ osl_client_mk := 1
 include $(IUELOCALROOT)/vxl/vbl/client.mk
 include $(IUELOCALROOT)/vxl/vnl/client.mk
 include $(IUELOCALROOT)/vxl/vil/client.mk
+include $(IUELOCALROOT)/vxl/vgl/client.mk
 
-ald_libdeps += osl:vil,vbl,vnl-algo
+ald_libdeps += osl:vil,vbl,vgl,vnl-algo
 
 endif
