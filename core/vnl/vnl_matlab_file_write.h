849f05e03037e76cc8f865921bb1df447567a61d
F. Schaffalitzky <fsm@users.sourceforge.net>
2001-10-17 14:51:25 -0400

    now i dont have to remember how its spellec

---
+++ core/vnl/vnl_matlab_file_write.h
@@ -0,0 +1,10 @@
+#ifndef vnl_matlab_file_write_h_
+#define vnl_matlab_file_write_h_
+/*
+  fsm@robots.ox.ac.uk
+*/
+
+#include <vnl/vnl_matlab_filewrite.h>
+#define vnl_matlab_file_write vnl_matlab_filewrite
+
+#endif
