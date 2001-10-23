c7c817bd7e2c2acbf31a474102a8b306b9558a05
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-10-23 12:56:41 -0400

    solve_quadratic and solve_cubic rewritten and documented

--- contrib/oxl/mvl/FMatrixCompute7Point.cxx
+++ contrib/oxl/mvl/FMatrixCompute7Point.cxx
@@ -4,7 +4,6 @@
 
 //-----------------------------------------------------------------------------
 //
-// Class: FMatrixCompute7Point
 // Author: David N. McKinnon, UQ I.R.I.S
 // Created: 25 Nov 00
 //
@@ -21,15 +20,14 @@
 #include <vnl/vnl_matrix.h>
 #include <vnl/vnl_math.h>
 #include <vnl/algo/vnl_svd.h>
-#include <vnl/algo/vnl_rpoly_roots.h>
 
 #include <mvl/HomgMetric.h>
 #include <mvl/PairMatchSetCorner.h>
 #include <mvl/FDesignMatrix.h>
 #include <mvl/HomgNorm2D.h>
 
-FMatrixCompute7Point::FMatrixCompute7Point(bool precondition, bool rank2_truncate):
-  precondition_(precondition),
+FMatrixCompute7Point::FMatrixCompute7Point(bool precondition, bool rank2_truncate)
+: precondition_(precondition),
   rank2_truncate_(rank2_truncate)
 {
 }
@@ -40,7 +38,6 @@ FMatrixCompute7Point::FMatrixCompute7Point(bool precondition, bool rank2_truncat
 //
 // Return false if the calculation fails or there are fewer than eight point
 // matches in the list.
-//
 
 bool FMatrixCompute7Point::compute(PairMatchSetCorner& matches, vcl_vector<FMatrix*>& F)
 {
@@ -69,16 +66,17 @@ bool FMatrixCompute7Point::compute (vcl_vector<HomgPoint2D>& points1,
     HomgNorm2D conditioned2(points2);
 
     // Compute F with preconditioned points
-    if(!compute_preconditioned(conditioned1.get_normalized_points(),
-                               conditioned2.get_normalized_points(), F))
+    if (!compute_preconditioned(conditioned1.get_normalized_points(),
+                                conditioned2.get_normalized_points(), F))
       return false;
 
     // De-condition F
-    for(int i = 0; i < F.size(); i++) {
-      F[i] = new FMatrix(HomgMetric::homg_to_image_F(*F[i], &conditioned1, &conditioned2));
+    for (int i = 0; i < F.size(); i++) {
+      F[i] = new FMatrix(HomgMetric::homg_to_image_F(*F[i], &conditioned1,
+                                                            &conditioned2));
     }
   } else
-    if(!compute_preconditioned(points1, points2, F))
+    if (!compute_preconditioned(points1, points2, F))
       return false;
 
   return true;
@@ -91,9 +89,6 @@ bool FMatrixCompute7Point::compute_preconditioned(vcl_vector<HomgPoint2D>& point
 {
   // Create design matrix from conditioned points
   FDesignMatrix design(points1, points2);
-  vnl_matrix<double> F_temp;
-  vnl_vector<double> *a = new vnl_vector<double>(4);
-  FMatrix *F1 = new FMatrix(), *F2 = new FMatrix();
 
   // Normalize rows for better conditioning
   design.normalize_rows();
@@ -106,154 +101,132 @@ bool FMatrixCompute7Point::compute_preconditioned(vcl_vector<HomgPoint2D>& point
   // Take the first and second nullvectors from the nullspace
   // Since rank 2 these should be the only associated with non-zero
   // root (Probably need conditioning first to be actually rank 2)
-  F1->set(vnl_double_3x3(W.get_column(0).data_block()));
-  F2->set(vnl_double_3x3(W.get_column(1).data_block()));
-  vnl_vector<double> roots(3);
+  FMatrix F1(vnl_double_3x3(W.get_column(0).data_block()));
+  FMatrix F2(vnl_double_3x3(W.get_column(1).data_block()));
 
   // Using the fact that Det(alpha*F1 +(1 - alpha)*F2) == 0
   // find the real roots of the cubic equation that satisfy
-  // There will either be 1 real and 2 complex conjugate or
-  // 3 real - note only real components taken
-  // Or there will be to real roots forming a quadratic
-  // (Special form)
-  int no_solns = 0;
-  double one, two, three;
-  if(FMatrixCompute7Point::GetCoef(F1, F2, a))
-    no_solns = solve_cubic(a->get(0), a->get(1), a->get(2), a->get(3), &one, &two, &three);
-  else
-    return false;
-  if(one == 0.0 && two == 0.0 && three == 0.0)
-    no_solns = 0;
+  vcl_vector<double> a = FMatrixCompute7Point::GetCoef(F1, F2);
+  vcl_vector<double> roots = FMatrixCompute7Point::solve_cubic(a);
 
-  roots.put(0, one);
-  roots.put(1, two);
-  roots.put(2, three);
-  for(int i = 0; i < no_solns; i++) {
-    // F = alpha*F1 + (1 - alpha)*F2
-    FMatrix *temp = new FMatrix();
-    F_temp = F1->get_matrix()*roots.get(0) + F2->get_matrix()*(1 - roots.get(i));
-    temp->set(F_temp);
-    F.push_back(temp);
+  for (int i = 0; i < roots.size(); i++) {
+    vnl_matrix<double> F_temp =
+      F1.get_matrix()*roots[0] + F2.get_matrix()*(1 - roots[i]);
+    F.push_back(new FMatrix(F_temp));
   }
   // Rank-truncate F
   if (rank2_truncate_) {
-    for(int h = 0; h < F.size(); h++) {
+    for (int h = 0; h < F.size(); ++h) {
       F[h]->set_rank2_using_svd();
     }
   }
-
-  delete F1, F2;
   return true;
 }
 
 //
 // Det(alpha*F1 +(1 - alpha)*F2) == 0
-// (I obtained these co-efficients from Maple (fingers crossed!!!))
+// (I obtained these coefficients from Maple (fingers crossed!!!))
 //
-bool FMatrixCompute7Point::GetCoef(FMatrix* F1, FMatrix* F2, vnl_vector<double> *vect)
+vcl_vector<double> FMatrixCompute7Point::GetCoef(FMatrix const& F1, FMatrix const& F2)
 {
-  double a=F1->get(0,0), j=F2->get(0,0), aa=a-j,
-         b=F1->get(0,1), k=F2->get(0,1), bb=b-k,
-         c=F1->get(0,2), l=F2->get(0,2), cc=c-l,
-         d=F1->get(1,0), m=F2->get(1,0), dd=d-m,
-         e=F1->get(1,1), n=F2->get(1,1), ee=e-n,
-         f=F1->get(1,2), o=F2->get(1,2), ff=f-o,
-         g=F1->get(2,0), p=F2->get(2,0), gg=g-p,
-         h=F1->get(2,1), q=F2->get(2,1), hh=h-q,
-         i=F1->get(2,2), r=F2->get(2,2), ii=i-r;
+  double a=F1.get(0,0), j=F2.get(0,0), aa=a-j,
+         b=F1.get(0,1), k=F2.get(0,1), bb=b-k,
+         c=F1.get(0,2), l=F2.get(0,2), cc=c-l,
+         d=F1.get(1,0), m=F2.get(1,0), dd=d-m,
+         e=F1.get(1,1), n=F2.get(1,1), ee=e-n,
+         f=F1.get(1,2), o=F2.get(1,2), ff=f-o,
+         g=F1.get(2,0), p=F2.get(2,0), gg=g-p,
+         h=F1.get(2,1), q=F2.get(2,1), hh=h-q,
+         i=F1.get(2,2), r=F2.get(2,2), ii=i-r;
 
   double a1=ee*ii-ff*hh, b1=ee*r+ii*n-ff*q-hh*o, c1=r*n-o*q;
   double d1=bb*ii-cc*hh, e1=bb*r+ii*k-cc*q-hh*l, f1=r*k-l*q;
   double g1=bb*ff-cc*ee, h1=bb*o+ff*k-cc*n-ee*l, i1=o*k-l*n;
 
-  double aaa = aa*a1-dd*d1+gg*g1;
-  double bbb = aa*b1+a1*j-dd*e1-d1*m+gg*h1+g1*p;
-  double ccc = aa*c1+b1*j-dd*f1-e1*m+gg*i1+h1*p;
-  double ddd = c1*j-f1*m+i1*p;
+  vcl_vector<double> v(aa*a1-dd*d1+gg*g1);
+  v.push_back(aa*b1+a1*j-dd*e1-d1*m+gg*h1+g1*p);
+  v.push_back(aa*c1+b1*j-dd*f1-e1*m+gg*i1+h1*p);
+  v.push_back(c1*j-f1*m+i1*p);
 
-  vect->put(0, aaa);
-  vect->put(1, bbb);
-  vect->put(2, ccc);
-  vect->put(3, ddd);
-
-  return true;
+  return v;
 }
 
-/--------------------
+//--------------------
 //:
-//  Gives solns x1 and x2 as given by Numerical Recipes to ax^2 + bx + c = 0.
-//  Returns the number of real solutions, so if both are imaginary it returns
-//  zero, otherwise two.
+//  Gives solutions to 0x^3 + ax^2 + bx + c = 0.
+//  Returns the set of real solutions, so if both are imaginary it returns an
+//  empty list, otherwise a list of length two.
+//  v is a 4-vector of which the first element is ignored.
 //-------------------
-int FMatrixCompute7Point::solve_quadratic (double a, double b, double c, double *x1, double *x2)
+vcl_vector<double> FMatrixCompute7Point::solve_quadratic (vcl_vector<double> v)
 {
-   double q, s, d;
-
-   if (b > 0.0) s = 1.0;
-   else         s = -1.0;
-
-   d = b * b - 4 * a * c;
+   double a = v[1], b = v[2], c = v[3]; 
+   double s = (b > 0.0) ? 1.0 : -1.0;
+   double d = b * b - 4 * a * c;
 
    // round off error
-   if ( (d > -0.001) && (d < 0))
+   if ( d > -1e-5 && d < 0)
      d = 0.0;
 
-   if (d < 0.0) /* doesn't work for complex roots */
-   {
-      return 0;
-   }
-   else q = -0.5 * ( b + s * vcl_sqrt(d));
+   if (d < 0.0) // doesn't work for complex roots
+      return vcl_vector<double>(); // empty list
 
-   *x1 = q/a;
-   *x2 = c/q;
-   return 2;
+   double q = -0.5 * ( b + s * vcl_sqrt(d));
+
+   vcl_vector<double> l(q/a); l.push_back(c/q);
+   return l;
 }
 
-double FMatrixCompute7Point::cbrt (double x)
+// Compute cube root of a positive or a negative number
+inline double my_cbrt(double x)
 {
-   return vcl_exp(vcl_log(x)/3.0);
+   return (x>=0) ? vcl_exp(vcl_log(x)/3.0) : -vcl_exp(vcl_log(-x)/3.0);
 }
 
 //------------------
 //:
-//  Solves a cubic by formula given in Numerical Recipes and
-//  returns the number of real solutions.
-//  Thus it returns 1 if there are 2 complex roots and 1 real solution
-//  2 if it is in fact a fact quadratic with 2 solns
-//  3 if there are 3 real solns
+//  Solves a cubic and returns the real solutions.
+//  Thus it returns a list of length 1 if there are 2 complex roots and 1 real,
+//  of length 2 if it is in fact a quadratic with 2 solutions,
+//  of length 3 if there are 3 real solutions
 //     a x^3 + b x^2 + c x + d = 0
-//  puts the solns into x1, x2, x3 respectively
-//  (the last of these are unset if there aren't enough solns).
 //-------------------
-int FMatrixCompute7Point::solve_cubic (double a, double b, double c, double d,
-                                       double *x1, double *x2, double *x3)
+// Rewritten and documented by Peter Vanroose, 23 October 2001.
+
+vcl_vector<double> FMatrixCompute7Point::solve_cubic(vcl_vector<double> v)
 {
-   double q, r, theta, e, f, len;
+   double a = v[0], b = v[1], c = v[2], d = v[3];
 
    /* firstly check to see if we have approximately a quadratic */
-   len = a*a + b*b + c*c + d*d;
-   if (vcl_abs(a*a/len) < 0.000001 )
-      return ( solve_quadratic ( b, c, d, x1, x2 ) );
+   double len = a*a + b*b + c*c + d*d;
+   if (vcl_abs(a*a/len) < 1e-6 )
+      return FMatrixCompute7Point::solve_quadratic(v);
 
-   b /= a; c /= a; d /= a; a = 1.0;
-   q = (b*b - 3.0*c)/9.0;
-   r = (2.0*b*b*b - 9.0*b*c + 27.0*d)/54.0;
+   b /= a; c /= a; d /= a; b /= 3;
+   // With the substitution x = y-b, the equation becomes y^3-3qy+2r = 0:
+   double q = b*b - c/3;
+   double r = b*(b*b-c/2) + d/2;
+   // At this point, a, c and d are no longer needed (c and d will be reused).
 
-   if ( r*r >= q*q*q )
+   if (q == 0)
+      return vcl_vector<double> (my_cbrt(-2*r) - b);
+
+   // With the Vieta substitution y = z+q/z this becomes z^6+2rz^3+q^3 = 0
+   // which is essentially a quadratic equation:
+
+   d = r*r - q*q*q;
+   if ( d >= 0.0 )
    {
-      e  = cbrt(vcl_abs(r) + vcl_sqrt(r*r - q*q*q));
-      if ( e*r > 0.0) e *= -1.0; /* e has to be opposite sign of r? */
-
-      if ( e == 0.0) f = 0.0;
-      else           f = q/e;
-
-      *x1 = e + f - b/3.0;
-      return 1;
+      double z  = my_cbrt(-r + vcl_sqrt(d));
+      // The case z=0 is excluded since this is q==0 which is handled above
+      return vcl_vector<double> (z + q/z - b);
    }
 
-   theta = vcl_acos( r/vcl_sqrt(q*q*q) );
-   *x1 = -2.0*vcl_sqrt(q)*vcl_cos(theta/3.0)                      - b/3.0;
-   *x2 = -2.0*vcl_sqrt(q)*vcl_cos((theta + 2.0*vnl_math::pi)/3.0) - b/3.0;
-   *x3 = -2.0*vcl_sqrt(q)*vcl_cos((theta - 2.0*vnl_math::pi)/3.0) - b/3.0;
-   return 3;
+   // And finally the "irreducible case" (with 3 solutions):
+   c = vcl_sqrt(q);
+   double theta = vcl_acos( r/q/c ) / 3;
+   vcl_vector<double> l(-2.0*c*vcl_cos(theta)           - b);
+   l.push_back(-2.0*c*vcl_cos(theta + 2*vnl_math::pi/3) - b);
+   l.push_back(-2.0*c*vcl_cos(theta - 2*vnl_math::pi/3) - b);
+   return l;
 }
