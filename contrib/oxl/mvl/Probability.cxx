db04eaaca7283939aa06e64cf643098f59efa525
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-10-25 03:56:05 -0400

    function commented out with #if 0 instead of with /* ... */

--- contrib/oxl/mvl/Probability.cxx
+++ contrib/oxl/mvl/Probability.cxx
@@ -26,7 +26,7 @@ vcl_vector<int> Monte_Carlo(vcl_vector<HomgPoint2D> points, vcl_vector<int> inde
   double col_size = 2.0;
   vcl_vector<int> out_points(samples);
   double row_div = row_size/buckets;
-  double col_div = col_size/buckets;  
+  double col_div = col_size/buckets;
   int no_buckets = buckets*buckets;
   if(buckets < 1) {
     vcl_cout << "Warning Monte Carlo sampling will not work." << vcl_endl
@@ -35,7 +35,7 @@ vcl_vector<int> Monte_Carlo(vcl_vector<HomgPoint2D> points, vcl_vector<int> inde
   if(index.size() < samples) {
     vcl_cout << "Warning Monte Carlo sampling will not work." << vcl_endl
              << "Not enough points to choose from: need " << samples
-             << ", have " << index.size() << "." << vcl_endl; 
+             << ", have " << index.size() << "." << vcl_endl;
   }
   int i = 0;
   while(i < samples) {
@@ -59,11 +59,8 @@ vcl_vector<int> Monte_Carlo(vcl_vector<HomgPoint2D> points, vcl_vector<int> inde
       col_num = 0;
     }
 
-
-
-
     double row_check_lower = row_num * row_div;
-    double col_check_lower = col_num * col_div; 
+    double col_check_lower = col_num * col_div;
     double row_check_upper = row_num * row_div + row_div;
     double col_check_upper = col_num * col_div + col_div;
     row_check_lower -= 1.0;
@@ -78,44 +75,43 @@ vcl_vector<int> Monte_Carlo(vcl_vector<HomgPoint2D> points, vcl_vector<int> inde
       if(points[j].get_y() >= row_check_lower && points[j].get_y() < row_check_upper && points[j].get_x() >= col_check_lower &&
 points[j].get_x() < col_check_upper) {
         list.push_back(index[j]);
-        
       }
     }
-    
-    int list_size = list.size();  
+
+    int list_size = list.size();
     bool not_picked = true;
     if(list_size != 0) {
       int counter = 0;
       bool fail;
-      while(not_picked && counter < list_size*4) {  
+      while(not_picked && counter < list_size*4) {
         int pick = (int)((float)(list_size - 1)*rand()/(RAND_MAX+1.0));
         int picked = list[pick];
-        fail = false; 
+        fail = false;
         for(int k = 0; k < i; k++) {
           if(picked == out_points[k])
             fail = true;
         }
         if(!fail) {
-          out_points[i] = picked; 
+          out_points[i] = picked;
           not_picked = false;
           i++;
         } else {
           counter++;
           //vcl_cout << "Failed" << vcl_endl;
         }
-      }       
-    }  
+      }
+    }
     list.clear();
-  }   
- 
+  }
+
   return out_points;
-} 
-/*
-// Note : this hasn't been implemented properly yet
+}
+
+#if 0 // Note : this hasn't been implemented properly yet
 vcl_vector<HomgPoint2D> Taubins_MLE(HomgPoint2D x1, HomgPoint2D x2, FMatrix *F)
 {
   vcl_vector<HomgPoint2D> actual_points;
-  
+
   // Generate a Jacobian matrix
   vnl_matrix<double> J;
   J.put(1, 1, (F->get(1, 1) - x2*F->get(3, 1)));
@@ -128,28 +124,28 @@ vcl_vector<HomgPoint2D> Taubins_MLE(HomgPoint2D x1, HomgPoint2D x2, FMatrix *F)
   J.put(2, 4, (-x*F->get(3, 1) - y_dash*F->get(3, 2) - F->get(3, 3)));
 
   // Find the Moore-Penrose Psuedo Inverse for the Jacobian matrix
-  vnl_svd<double> svd(J, 1e-8);  
+  vnl_svd<double> svd(J, 1e-8);
   vnl_diag_matrix<double> diag = svd.W;
   for(int i = 0; i < diag.size(); i++) {
     if(diag(i, i) != 0.0)
       diag(i, i) = 1 / diag(i, i);
   }
   vnl_matrix<double> psuedo = svd.U * diag * svd.V.transpose();
-  
+
   // Find the residuals
-   
+
   return actual_points;
 }
-*/
+#endif
 
 double Sampsons_MLE(HomgPoint2D x1, HomgPoint2D x2, FMatrix *F)
 {
-  double rX, rY, rX_dash, rY_dash, GRADr, r, dist; 
+  double rX, rY, rX_dash, rY_dash, GRADr, r, dist;
   vnl_matrix<double> temp(3, 3);
   temp = (vnl_matrix<double>)F->get_matrix();
   vcl_cout << x2.get_x() << vcl_endl;
   rX = temp.get(0, 0)*x2.get_x() + temp.get(1, 0)*x2.get_y() + temp.get(2, 0);
-  rY = F->get(0, 1)*x2.get_x() + F->get(1, 1)*x2.get_y() + F->get(2, 1);  
+  rY = F->get(0, 1)*x2.get_x() + F->get(1, 1)*x2.get_y() + F->get(2, 1);
   rX_dash = F->get(0, 0)*x1.get_x() + F->get(0, 1)*x1.get_y() + F->get(0, 2);
   rY_dash = F->get(1, 0)*x1.get_x() + F->get(1, 1)*x1.get_y() + F->get(1, 2);
   vcl_cout << "Points : " << rX << " " << rY << " " << rX_dash << " " << rY_dash << vcl_endl;
