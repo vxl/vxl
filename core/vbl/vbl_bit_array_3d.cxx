a346f6c2a13521fc41480fb810508603cfb53371
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-10-25 03:43:09 -0400

    vbl_bit_array_3d added

---
+++ core/vbl/vbl_bit_array_3d.cxx
@@ -0,0 +1,119 @@
+//-*- c++ -*-------------------------------------------------------------------
+#ifdef __GNUC__
+#pragma implementation
+#endif
+//:
+// \file
+// \author
+// Author: Geoffrey Cross, Oxford RRG
+// Created: 17 Jul 99
+//
+//-----------------------------------------------------------------------------
+
+#include <vcl_cstring.h> // for memset()
+#include <vcl_cassert.h>
+#include <vcl_iostream.h>
+#include <vcl_climits.h>
+
+#include "vbl_bit_array_3d.h"
+
+// Default ctor
+vbl_bit_array_3d::vbl_bit_array_3d(unsigned int sizex,
+                                   unsigned int sizey,
+                                   unsigned int sizez)
+  : vbl_bit_array_3d_base(sizex, sizey, sizez),
+    data_(new unsigned char[(sizex*sizey*sizez+CHAR_BIT-1)/CHAR_BIT])
+{
+}
+
+vbl_bit_array_3d::vbl_bit_array_3d(unsigned int sizex,
+                                   unsigned int sizey,
+                                   unsigned int sizez,
+                                   bool v)
+  : vbl_bit_array_3d_base(sizex, sizey, sizez),
+    data_(new unsigned char[(sizex*sizey*sizez+CHAR_BIT-1)/CHAR_BIT])
+{
+  fill(v);
+}
+
+vbl_bit_array_3d::~vbl_bit_array_3d()
+{
+  delete[] data_;
+}
+
+void vbl_bit_array_3d::set(unsigned int i1, unsigned int i2, unsigned int i3, bool v)
+{
+  unsigned long byteindex;
+  unsigned char bitindex;
+
+  index(i1, i2, i3, byteindex, bitindex);
+
+  unsigned char mask = v ? (1<<bitindex) : 0;
+  unsigned char nmask = ~(1<<bitindex);
+
+  data_[byteindex] = mask|(nmask & data_[byteindex]);
+}
+
+void vbl_bit_array_3d::flip(unsigned int i1, unsigned int i2, unsigned int i3)
+{
+  unsigned long byteindex;
+  unsigned char bitindex;
+
+  index(i1, i2, i3, byteindex, bitindex);
+
+  unsigned char mask = (data_[byteindex] & (1<<bitindex)) ? 0 : (1<<bitindex);
+  unsigned char nmask = ~(1<<bitindex);
+
+  data_[byteindex] = mask|(nmask & data_[byteindex]);
+}
+
+void vbl_bit_array_3d::fill(bool v)
+{
+  unsigned char temp=0;
+  if (v)
+    for (int t=0; t< CHAR_BIT; t++)
+      temp = (temp<<1)+1;
+
+  unsigned long memsize=(row1_count()*row2_count()*row3_count()+CHAR_BIT-1)/CHAR_BIT;
+  vcl_memset(data_, temp, memsize);
+}
+
+bool vbl_bit_array_3d::operator() (unsigned int i1, unsigned int i2, unsigned int i3) const
+{
+  unsigned long byteindex;
+  unsigned char bitindex;
+
+  index(i1, i2, i3, byteindex, bitindex);
+  unsigned char mask = (1<<bitindex);
+
+  return bool(data_[byteindex] & mask);
+}
+
+void vbl_bit_array_3d_base::index(unsigned x, unsigned y, unsigned z,
+                                  unsigned long & byteindex,
+                                  unsigned char & bitindex) const
+{
+  assert(x<row1_count() && y<row2_count() && z<row3_count());
+  unsigned long i = (z*row2_count()+y)*row1_count()+x;
+
+  byteindex = i/CHAR_BIT;
+  bitindex  = i%CHAR_BIT;
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+vcl_ostream &operator<<(vcl_ostream& os, vbl_bit_array_3d const& bitarray)
+{
+  for (int i=0; i< bitarray.row1_count(); i++)
+  {
+    for (int j=0; j< bitarray.row2_count(); j++)
+    {
+      for (int k=0; k< bitarray.row3_count(); k++)
+        os << (bitarray(k,j,i) ? 'x' : '.');
+      os << vcl_endl;
+    }
+    os << vcl_endl;
+  }
+  return os;
+}
---
+++ core/vbl/vbl_bit_array_3d.h
@@ -0,0 +1,79 @@
+//-*- c++ -*-------------------------------------------------------------------
+#ifndef vbl_bit_array_3d_h_
+#define vbl_bit_array_3d_h_
+#ifdef __GNUC__
+#pragma interface
+#endif
+//:
+// \file
+// \author
+//     Geoffrey Cross, Oxford RRG, 17 Jul 99
+// \verbatim
+// Modifications:
+//   990717 Geoff Initial version.
+//   011023 Peter Vanroose - renamed and moved to vbl
+//\endverbatim
+//
+//-----------------------------------------------------------------------------
+
+#include <vcl_iosfwd.h>
+
+class vbl_bit_array_3d_base
+{
+public:
+  vbl_bit_array_3d_base(int a, int b, int c)
+    : row1_count_(a),
+      row2_count_(b),
+      row3_count_(c)
+    {}
+
+  int row1_count() const { return row1_count_; }
+  int row2_count() const { return row2_count_; }
+  int row3_count() const { return row3_count_; }
+
+protected:
+  int row1_count_;
+  int row2_count_;
+  int row3_count_;
+
+  // Helpers-------------------------------------------------------------------
+
+  void index(unsigned int x, unsigned int y, unsigned int z,
+             unsigned long &byteindex, unsigned char &bitindex) const;
+};
+
+
+class vbl_bit_array_3d : public vbl_bit_array_3d_base
+{
+public:
+  // Constructors/Destructor---------------------------------------------------
+
+  //: Create a bitarray of the specified size, without initialising elements
+  vbl_bit_array_3d(unsigned int sizex, unsigned int sizey, unsigned int sizez);
+  //: Create a bitarray of the specified size, with initialisation of elements
+  vbl_bit_array_3d(unsigned int sizex, unsigned int sizey, unsigned int sizez, bool v);
+  ~vbl_bit_array_3d();
+
+  // Data Access---------------------------------------------------------------
+
+  //: Set all cell values to v
+  void fill(bool v);
+
+  //: Set the value of a cell; default is to set the value on
+  void set(unsigned int i1, unsigned int i2, unsigned int i3, bool v=true);
+
+  //: Change the value of a cell
+  void flip(unsigned int i1, unsigned int i2, unsigned int i3);
+
+  //: Return the value of a cell
+  bool operator() (unsigned int i1, unsigned int i2, unsigned int i3) const;
+
+protected:
+  // Data Members--------------------------------------------------------------
+
+  unsigned char *data_;
+};
+
+vcl_ostream &operator<<(vcl_ostream &os, vbl_bit_array_3d const&);
+
+#endif // vbl_bit_array_3d_h_
