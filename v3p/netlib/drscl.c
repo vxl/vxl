649987718ef26a83c037d2d59584acee0aad11b2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-10-27 17:43:18 -0400

    return type of subroutines changed from int to void

--- v3p/netlib/drscl.c
+++ v3p/netlib/drscl.c
@@ -1,11 +1,11 @@
 /*  -- translated by f2c (version 19991025).
    You must link the resulting object file with the libraries:
-	-lf2c -lm   (in that order)
+        -lf2c -lm   (in that order)
 */
 
 #include "f2c.h"
 
-/* Subroutine */ int drscl_(n, sa, sx, incx)
+/* Subroutine */ void drscl_(n, sa, sx, incx)
 integer *n;
 doublereal *sa, *sx;
 integer *incx;
@@ -13,68 +13,46 @@ integer *incx;
     static doublereal cden;
     static logical done;
     static doublereal cnum, cden1, cnum1;
-    extern /* Subroutine */ int dscal_(), dlabad_();
+    extern /* Subroutine */ void dscal_(), dlabad_();
     extern doublereal dlamch_();
     static doublereal bignum, smlnum, mul;
-
 
 /*  -- LAPACK auxiliary routine (version 3.0) -- */
 /*     Univ. of Tennessee, Univ. of California Berkeley, NAG Ltd., */
 /*     Courant Institute, Argonne National Lab, and Rice University */
 /*     September 30, 1994 */
 
-/*     .. Scalar Arguments .. */
-/*     .. */
-/*     .. Array Arguments .. */
-/*     .. */
-
-/*  Purpose */
-/*  ======= */
-
-/*  DRSCL multiplies an n-element real vector x by the real scalar 1/a. */
-/*  This is done without overflow or underflow as long as */
-/*  the final result x/a does not overflow or underflow. */
-
-/*  Arguments */
-/*  ========= */
-
-/*  N       (input) INTEGER */
-/*          The number of components of the vector x. */
-
-/*  SA      (input) DOUBLE PRECISION */
-/*          The scalar a which is used to divide each component of x. */
-/*          SA must be >= 0, or the subroutine will divide by zero. */
-
-/*  SX      (input/output) DOUBLE PRECISION array, dimension */
-/*                         (1+(N-1)*abs(INCX)) */
-/*          The n-element vector x. */
-
-/*  INCX    (input) INTEGER */
-/*          The increment between successive values of the vector SX. */
+/*  Purpose                                                              */
+/*  =======                                                              */
+/*                                                                       */
+/*  DRSCL multiplies an n-element real vector x by the real scalar 1/a.  */
+/*  This is done without overflow or underflow as long as                */
+/*  the final result x/a does not overflow or underflow.                 */
+/*                                                                       */
+/*  Arguments                                                            */
+/*  =========                                                            */
+/*                                                                       */
+/*  N       (input) INTEGER                                              */
+/*          The number of components of the vector x.                    */
+/*                                                                       */
+/*  SA      (input) DOUBLE PRECISION                                     */
+/*          The scalar a which is used to divide each component of x.    */
+/*          SA must be >= 0, or the subroutine will divide by zero.      */
+/*                                                                       */
+/*  SX      (input/output) DOUBLE PRECISION array, dimension             */
+/*                         (1+(N-1)*abs(INCX))                           */
+/*          The n-element vector x.                                      */
+/*                                                                       */
+/*  INCX    (input) INTEGER                                              */
+/*          The increment between successive values of the vector SX.    */
 /*          > 0:  SX(1) = X(1) and SX(1+(i-1)*INCX) = x(i),     1< i<= n */
-
+/*                                                                       */
 /* ===================================================================== */
-
-/*     .. Parameters .. */
-/*     .. */
-/*     .. Local Scalars .. */
-/*     .. */
-/*     .. External Functions .. */
-/*     .. */
-/*     .. External Subroutines .. */
-/*     .. */
-/*     .. Intrinsic Functions .. */
-/*     .. */
-/*     .. Executable Statements .. */
 
 /*     Quick return if possible */
 
-    /* Parameter adjustments */
-    --sx;
-
-    /* Function Body */
     if (*n <= 0) {
-	return 0;
+        return;
     }
 
 /*     Get machine parameters */
@@ -95,35 +73,29 @@ L10:
 
 /*        Pre-multiply X by SMLNUM if CDEN is large compared to CNUM. */
 
-	mul = smlnum;
-	done = FALSE_;
-	cden = cden1;
+        mul = smlnum;
+        done = FALSE_;
+        cden = cden1;
     } else if (abs(cnum1) > abs(cden)) {
 
 /*        Pre-multiply X by BIGNUM if CDEN is small compared to CNUM. */
 
-	mul = bignum;
-	done = FALSE_;
-	cnum = cnum1;
+        mul = bignum;
+        done = FALSE_;
+        cnum = cnum1;
     } else {
 
 /*        Multiply X by CNUM / CDEN and return. */
 
-	mul = cnum / cden;
-	done = TRUE_;
+        mul = cnum / cden;
+        done = TRUE_;
     }
 
 /*     Scale the vector X by MUL */
 
-    dscal_(n, &mul, &sx[1], incx);
+    dscal_(n, &mul, sx, incx);
 
     if (! done) {
-	goto L10;
+        goto L10;
     }
-
-    return 0;
-
-/*     End of DRSCL */
-
 } /* drscl_ */
-
