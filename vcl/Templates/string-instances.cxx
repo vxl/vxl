a16e3d46b2e0bd9f713e19c85dfb5a21ce8475a7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-11-06 04:51:25 -0500

    vcl_string_instance_tickler made static (local) to avoid multiple definition linker errors

--- vcl/Templates/string-instances.cxx
+++ vcl/Templates/string-instances.cxx
@@ -10,7 +10,7 @@ template class vcl_basic_string<char, vcl_char_traits<char> >;
 // this should work for all compilers. by putting it in the
 // library we (a) get the implicit template instances it
 // needs and (b) make sure that it does work.
-void vcl_string_instance_tickler(vcl_ostream &os, vcl_string::iterator i, char *a, char const *b)
+static void vcl_string_instance_tickler(vcl_ostream &os, vcl_string::iterator i, char *a, char const *b)
 {
   char ch;
   vcl_char_traits<char>::eq(ch, ch);
@@ -24,6 +24,7 @@ void vcl_string_instance_tickler(vcl_ostream &os, vcl_string::iterator i, char *
 
   vcl_string(s.begin()+3, s.end()) == b;
   s + s;
+  vcl_string_instance_tickler(os, i, a, b); // avoid "unused function" warning
 }
 
 
