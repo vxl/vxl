519fc385b7de10b09f6eee3b1659371b6f496de2
Amitha Perera <amithaperera@users.sourceforge.net>
2001-11-09 14:38:51 -0500

    Create links to the SourceForge CVS browser (once my Dart patches are accepted).

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -1,8 +1,13 @@
 #
-# Dart server to submit results (used by client)
+# Client configuration
 #
 SET (DROP_SITE "ftp.cs.rpi.edu")
 SET (DROP_LOCATION "/private/vision/incoming")
 SET (DROP_SITE_USER "ftp")
 SET (DROP_SITE_PASSWORD "vxl-tester@somewhere.com")
 SET (TRIGGER_SITE "http://cgi.cs.rpi.edu/~perera/vxl/submit-testing-results.cgi")
+
+#
+# Server configuration
+#
+SET (CVS_WEB_URL "http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/vxl/vxl/")
