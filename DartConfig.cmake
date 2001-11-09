e7c7fb7f6d349d946ee36c8306c29ed0d1b6474d
Amitha Perera <amithaperera@users.sourceforge.net>
2001-11-08 19:09:01 -0500

    Dart configuration for the dashboard at RPI. I hope to announce it tomorrow...

---
+++ DartConfig.cmake
@@ -0,0 +1,8 @@
+#
+# Dart server to submit results (used by client)
+#
+SET (DROP_SITE "ftp.cs.rpi.edu")
+SET (DROP_LOCATION "/private/vision/incoming")
+SET (DROP_SITE_USER "ftp")
+SET (DROP_SITE_PASSWORD "vxl-tester@somewhere.com")
+SET (TRIGGER_SITE "http://cgi.cs.rpi.edu/~perera/vxl/submit-testing-results.cgi")
