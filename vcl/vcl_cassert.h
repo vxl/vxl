dc7c4de7011ec09fb6cf723bb87c3549d164ebee
Andrew Fitzgibbon <awf@users.sourceforge.net>
2001-11-16 12:28:38 -0500

    win32: must call vcl_cassert_failure in order to "work" -- you can't set a breakpoint on abort.

--- vcl/vcl_cassert.h
+++ vcl/vcl_cassert.h
@@ -20,10 +20,22 @@
 //   #endif
 // If the system/compiler version works, use that instead.
 
+// Win32 doesn't work as you can't set a bp on abort
+#ifdef _WIN32
+#undef assert
+#ifdef NDEBUG
+# define assert(x) ((void) 0)
+#else
+extern void vcl_cassert_failure(char const *, int, char const *);
+# define assert(x) do { if (!(x)) vcl_cassert_failure(__FILE__, __LINE__, #x); } while (false)
+#endif
+
+#else
 #if !VCL_CXX_HAS_HEADER_CASSERT
 # include <assert.h>
 #else
 # include "iso/vcl_cassert.h"
+#endif
 #endif
 
 // fsm: There should not be a vcl_assert macro as there is no
