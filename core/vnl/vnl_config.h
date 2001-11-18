f9f5bdc89101f784711993d86432a318654f6006
F. Schaffalitzky <fsm@users.sourceforge.net>
2001-11-18 11:26:19 -0500

    obviously, i want to edit this file and not commit the changes

---
+++ core/vnl/vnl_config.h
@@ -0,0 +1,10 @@
+#ifndef vnl_config_h_
+#define vnl_config_h_
+
+// Edit this file to suit your taste, but
+// don't commit the changes.
+
+#define VNL_CONFIG_CHECK_BOUNDS   1
+#define VNL_CONFIG_LEGACY_METHODS 0
+
+#endif
