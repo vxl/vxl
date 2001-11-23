94053133a373d01f030a6c3926b1dc5bd84cf329
Ian Scott <scottim@users.sourceforge.net>
2001-11-23 09:01:13 -0500

    test vpl_putenv

--- core/vpl/tests/test_unistd.cxx
+++ core/vpl/tests/test_unistd.cxx
@@ -2,35 +2,45 @@
 
 #include <vpl/vpl.h>
 #include <sys/types.h>
+#include <vcl_cstdlib.h>
+#include <vcl_cstring.h>
 
 #define Assert(x) do { if (x) vcl_cout << "test PASSED\n"; else vcl_cout << "test FAILED [" #x "]\n"; } while (0)
 
+#ifdef VCL_WIN32
+#define ROOT_PATH "C:/TEMP"
+#else
+#define ROOT_PATH "/tmp"
+#endif
 
 int main(int, char **)
 {
-  vpl_mkdir("/tmp/vpltest", 0777);
-  vpl_chdir("/tmp/vpltest");
+  vpl_mkdir(ROOT_PATH "/vpltest", 0777);
+  vpl_chdir(ROOT_PATH "/vpltest");
 
   {
     vcl_ofstream f("file");
     f << 1234;
   }
   {
-    vcl_ifstream f("/tmp/vpltest/file");
+    vcl_ifstream f(ROOT_PATH "/vpltest/file");
     int s;
     f >> s;
     Assert(s == 1234);
   }
   vpl_unlink("file");
   {
-    vcl_ifstream f("/tmp/vpltest/file");
+    vcl_ifstream f(ROOT_PATH "/vpltest/file");
     Assert(!f.good());
   }
 
-  vpl_chdir("/tmp");
-  vpl_rmdir("/tmp/vpltest");
+  vpl_chdir(ROOT_PATH);
+  vpl_rmdir(ROOT_PATH "/vpltest");
 
   vpl_usleep(5000000); // 5 seconds
+
+  vpl_putenv("VPL_PUTENV_TEST=GOOD");
+  Assert(vcl_strcmp(vcl_getenv("VPL_PUTENV_TEST"), "GOOD") == 0);
 
   return 0;
 }
