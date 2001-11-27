9f47904c01c7d39479ea0b6dd9eab25d50aeff96
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-11-27 03:45:30 -0500

    #ifndef --> #ifdef  -- please check this with VC60

--- core/vgui/vgui_glut.h
+++ core/vgui/vgui_glut.h
@@ -11,7 +11,14 @@
 #else
 # define VGUI_GLUT_PRESENT 1
 # if 1 // fsm@robots
-# ifndef VCL_WIN32
+/*
+   It looks like the original #ifdef below had its condition reversed.
+   Also, it looks like Windows needs some serious hacking here, which
+   is the reason for the #ifdef being here.  For more info, check the
+   explanation in a comment around the top of glut.h
+    - Atanas Georgiev <atanas@cs.columbia.edu>
+*/
+# ifdef VCL_WIN32
    extern "C" void vgui_glut_exit(int);
 #  define exit vgui_glut_exit
 # endif
