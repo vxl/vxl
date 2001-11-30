c94f11ebffe0551f68bfe50018b525df9a9aa9f7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-11-30 11:23:59 -0500

    exit() must be vcl_exit()

--- vcl/tests/test_vcl_exception.cxx
+++ vcl/tests/test_vcl_exception.cxx
@@ -1,7 +1,7 @@
 #include <vcl_exception.h>
 #include <vcl_iostream.h>
 
-extern "C" void abort() { vcl_cout << "abort()\n"; exit(1); }
+extern "C" void abort() { vcl_cout << "abort()\n"; vcl_exit(1); }
 
 int main()
 {
