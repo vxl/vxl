6a7ed71846f7a319c74e858efdec7a087f15ed6b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-11-30 07:24:31 -0500

    model is not a pointer

--- contrib/mul/clsfy/clsfy_random_builder.cxx
+++ contrib/mul/clsfy/clsfy_random_builder.cxx
@@ -96,7 +96,7 @@ double clsfy_random_builder::build(clsfy_classifier_base& model,
                                    const vcl_vector<unsigned> &outputs) const
 {
   const unsigned n = outputs.size();
-  assert(model->is_a() == "clsfy_random_classifier");
+  assert(model.is_a() == "clsfy_random_classifier");
   clsfy_random_classifier &randclass = (clsfy_random_classifier&) model;
 
   if (nClasses==1) nClasses=2;
