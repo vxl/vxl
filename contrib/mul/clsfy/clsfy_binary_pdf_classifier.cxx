6f4c21c41df1a64b0b37d5b3a3b7712131edfc25
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-11-30 07:01:23 -0500

    comment corrected: P(class|data) instead of P(data|class)

--- contrib/mul/clsfy/clsfy_binary_pdf_classifier.cxx
+++ contrib/mul/clsfy/clsfy_binary_pdf_classifier.cxx
@@ -1,7 +1,4 @@
-
-//	Copyright: (C) 2000 British Telecommunications PLC
-
-
+//  Copyright: (C) 2000 British Telecommunications PLC
 
 #include <clsfy/clsfy_binary_pdf_classifier.h>
 #include <vcl_string.h>
@@ -29,8 +26,8 @@ void clsfy_binary_pdf_classifier::deleteStuff()
 // Returns either class1 (Inside PDF mode) or class 0 (Outside PDF mode).
 unsigned clsfy_binary_pdf_classifier::classify(const vnl_vector<double> &input) const
 {
-  assert(pdf_);
-  
+  assert(pdf_!=0);
+
   if (pdf_->log_p(input) >= log_prob_limit_)
     return 1;
   else
@@ -39,15 +36,14 @@ unsigned clsfy_binary_pdf_classifier::classify(const vnl_vector<double> &input) 
 
 //=======================================================================
 
-//: Return the probability the input being in each class p(class|data).
-// output(i) i<n_classes, contains the probability that the input
-// is in class i;
-// 
+//: Return the probability the input being in each class P(class|data).
+// output(i) i<n_classes, contains the probability that the input is in class i
+//
 void clsfy_binary_pdf_classifier::class_probabilities(
-  vcl_vector<double> &outputs,
-  const vnl_vector<double> &input)  const
+          vcl_vector<double> &outputs,
+          const vnl_vector<double> &input)  const
 {
-		//: likelyhood = p(input|InClass) / prob_limit_
+  // likelyhood = P(input|InClass) / prob_limit_
   double likelyhood= vcl_exp(log_l(input));
   outputs.resize(1);
   outputs[0] = likelyhood / (1 + likelyhood);
@@ -55,17 +51,17 @@ void clsfy_binary_pdf_classifier::class_probabilities(
 
 //=======================================================================
 
-//: Log likelyhood  of being in class 0.vcl_log(p(data|class=0))
+//: Log likelyhood of being in class 0.  log(P(class=0|data))
 // This function is intended for use in binary classifiers only. It is
-// related to the class 0 probability as follows,  
-// P(class=0|data) = vcl_exp(logL) / (1+vcl_exp(logL).
-// Don't forget that p(data|X) is a density and so the result can be
+// related to the class 0 probability as follows,
+// P(class=0|data) = exp(logL) / (1+exp(logL)).
+// Don't forget that P(X|data) is a density and so the result can be
 // greater than 1.0 or less than 0.0, (or indeed between 0.0 and 1.0).
 double clsfy_binary_pdf_classifier::log_l(const vnl_vector<double> &input) const
 {
-  assert(pdf_);
-  
-		//: likelyhood = p(input|InClass) / prob_limit_
+  assert(pdf_!=0);
+
+  // likelyhood = P(input|InClass) / prob_limit_
   return pdf_->log_p(input) - log_prob_limit_;
 }
 
@@ -78,9 +74,9 @@ vcl_string clsfy_binary_pdf_classifier::is_a() const
 
 //=======================================================================
 
-short clsfy_binary_pdf_classifier::version_no() const 
-{ 
-  return 1; 
+short clsfy_binary_pdf_classifier::version_no() const
+{
+  return 1;
 }
 
 //=======================================================================
@@ -103,16 +99,16 @@ clsfy_classifier_base* clsfy_binary_pdf_classifier::clone() const
 clsfy_binary_pdf_classifier& clsfy_binary_pdf_classifier::operator=(const clsfy_binary_pdf_classifier& classifier)
 {
   if (&classifier==this) return *this;
-  
+
   clsfy_classifier_base::operator=(classifier);
-  
+
   deleteStuff();
-  
+
   if (classifier.pdf_)
     pdf_ = classifier.pdf_->clone();
-  
+
   log_prob_limit_ = classifier.log_prob_limit_;
-  
+
   return *this;
 }
 
@@ -131,20 +127,19 @@ void clsfy_binary_pdf_classifier::b_write(vsl_b_ostream& bfs) const
 void clsfy_binary_pdf_classifier::b_read(vsl_b_istream& bfs)
 {
   if (!bfs) return;
-  
+
   short version;
   vsl_b_read(bfs,version);
   switch (version)
   {
-		case (1):
+    case (1):
       vsl_b_read(bfs,log_prob_limit_);
       vsl_b_read(bfs,pdf_);
       break;
     default:
-      vcl_cerr << "I/O ERROR: clsfy_binary_pdf_classifier::b_read(vsl_b_istream&) \n";
-      vcl_cerr << "           Unknown version number "<< version << "\n";
+      vcl_cerr << "I/O ERROR: clsfy_binary_pdf_classifier::b_read(vsl_b_istream&)\n"
+               << "           Unknown version number "<< version << "\n";
       bfs.is().clear(vcl_ios::badbit); // Set an unrecoverable IO error on stream
-      return;
   }
 }
 
--- contrib/mul/clsfy/clsfy_binary_pdf_classifier.h
+++ contrib/mul/clsfy/clsfy_binary_pdf_classifier.h
@@ -1,6 +1,6 @@
 #ifndef clsfy_binary_pdf_classifier_h_
 #define clsfy_binary_pdf_classifier_h_
-//	Copyright: (C) 2000 British Telecommunications PLC
+// Copyright: (C) 2000 British Telecommunications PLC
 
 //:
 // \file
@@ -14,118 +14,110 @@
 //:  Decisions are based on an explicit multivariate probability distribution
 class clsfy_binary_pdf_classifier : public clsfy_classifier_base {
 protected:
-//: The current distribution model.
-  vpdfl_pdf_base *pdf_;
+  vpdfl_pdf_base *pdf_; //!< The current distribution model
 
-
-//: The value of log probability density above which the answer is 1, below which is 0;
-  double	log_prob_limit_;
+  double log_prob_limit_; //!< The value of log probability density above which the answer is 1, and below which it is 0
 
 public:
 
-//: default constructor;
+  // default constructor
   clsfy_binary_pdf_classifier(): pdf_(0), log_prob_limit_(0.0) {}
 
-//: A useful constructor
-// Specify the log probability density limit;
+  //: A useful constructor
+  // Specify the log probability density limit
   clsfy_binary_pdf_classifier(const vpdfl_pdf_base &pdf,
-    double log_prob_limit):
-		pdf_(pdf.clone()),
-    log_prob_limit_(log_prob_limit) {};
+                              double log_prob_limit):
+      pdf_(pdf.clone()), log_prob_limit_(log_prob_limit) {}
 
-// Destructor;
-  ~clsfy_binary_pdf_classifier()
-  {	deleteStuff(); }
+  // Destructor
+  ~clsfy_binary_pdf_classifier() { deleteStuff(); }
 
-//: Classify the input vector
-// Returns either class1 (Inside PDF mode) or class 0 (Outside PDF mode).
+  //: Classify the input vector
+  // Returns either class1 (Inside PDF mode) or class 0 (Outside PDF mode).
   unsigned virtual classify(const vnl_vector<double> &input) const;
 
-//: Return the probability the input being in class 0.
-// output(0) , contains the probability that the input
-// is in class 1;
+  //: Return the probability the input being in class 0.
+  // output(0) contains the probability that the input is in class 1
   void virtual class_probabilities(vcl_vector<double> &outputs, const vnl_vector<double> &input) const;
 
-//: Log likelyhood of being in class0, i.e. const + vcl_log(p(data|class=0)).
-// The constant is chosen such that the decision boundary is at logL ==0;
-// This function is intended for binary classifiers only.
-// logL is related to class probability as p(class=0|data) = vcl_exp(logL) / (1+vcl_exp(logL))
+  //: Log likelyhood of being in class 0, i.e. const + log(P(class=0|data)).
+  // The constant is chosen such that the decision boundary is at logL ==0;
+  // This function is intended for binary classifiers only.
+  // logL is related to class probability as P(class=0|data) = exp(logL) / (1+exp(logL))
   double log_l(const vnl_vector<double> &input) const;
 
-//: Set the log probability density limit,
-// above which the inputs are in class 1.
+  //: Set the log probability density limit,
+  // above which the inputs are in class 1.
   void set_log_prob_limit(double limit)
   {
     log_prob_limit_ = limit;
   }
 
-//: The log probability density limit,
-// above which the inputs are in class 1.
+  //: The log probability density limit,
+  // above which the inputs are in class 1.
   double log_prob_limit() const
   {
     return log_prob_limit_;
   }
 
-//: Set the PDF.
-// The class takes its own deep copy of the PDF.
+  //: Set the PDF.
+  // The class takes its own deep copy of the PDF.
   void set_pdf(const vpdfl_pdf_base &pdf)
   {
     deleteStuff();
     pdf_ = pdf.clone();
-  };
+  }
 
   bool has_pdf() const
   {
     return (pdf_ != 0);
   }
-//: Get the internal PDFs;
-// The object will return a reference to the internal data.
+  //: Get the internal PDFs
+  // The object will return a reference to the internal data.
   const vpdfl_pdf_base & pdf() const
   {
-    assert (pdf_);
+    assert (pdf_!=0);
     return *pdf_;
   }
 
-//: The dimensionality of input vectors.
-  unsigned virtual n_dims() const { assert(pdf_); return pdf_->n_dims();} 
+  //: The dimensionality of input vectors.
+  unsigned virtual n_dims() const { assert(pdf_!=0); return pdf_->n_dims();}
 
-//: The number of possible output classes.
-  unsigned virtual n_classes() const {return 1;} 
+  //: The number of possible output classes.
+  unsigned virtual n_classes() const {return 1;}
 
-//: Produce a deep copy.
-// client has responsibility for deletion.
+  //: Produce a deep copy.
+  // client has responsibility for deletion.
   clsfy_classifier_base* clone() const;
 
-//: Version number for I/O 
+  //: Version number for I/O
   short version_no() const;
 
-//: Name of the class
-  virtual vcl_string is_a() const ;
+  //: Name of the class
+  virtual vcl_string is_a() const;
 
-//: Print class to os
+  //: Print class to os
   virtual void print_summary(vcl_ostream& os) const;
 
-//: Save class to a binary File Stream
+  //: Save class to a binary File Stream
   virtual void b_write(vsl_b_ostream& bfs) const;
 
-//: Load the class from a Binary File Stream
+  //: Load the class from a Binary File Stream
   virtual void b_read(vsl_b_istream& bfs);
 
 public:
 
-// Copy constructor;
+  // Copy constructor
   clsfy_binary_pdf_classifier( const clsfy_binary_pdf_classifier& b ):
-    pdf_(0), log_prob_limit_(0.0)
-  { *this = b; }
+      pdf_(0), log_prob_limit_(0.0) { *this = b; }
 
-//:Assignment operator
+  //:Assignment operator
   clsfy_binary_pdf_classifier& operator=(const clsfy_binary_pdf_classifier& classifier);
-
 
 protected:
 
-//: Delete members
+  // Delete members
   void deleteStuff();
 };
 
-#endif
+#endif // clsfy_binary_pdf_classifier_h_
