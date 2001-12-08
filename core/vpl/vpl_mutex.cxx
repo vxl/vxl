75ede5577435c8cd3b0ea4c23ac127895be457bd
F. Schaffalitzky <fsm@users.sourceforge.net>
2001-12-08 08:50:05 -0500

    simple wrapper for pthread_mutex_t

---
+++ core/vpl/vpl_mutex.cxx
@@ -0,0 +1,8 @@
+/*
+  fsm@robots.ox.ac.uk
+*/
+#ifdef __GNUC__
+#pragma implementation
+#endif
+#include "vpl_mutex.h"
+
---
+++ core/vpl/vpl_mutex.h
@@ -0,0 +1,47 @@
+#ifndef vpl_mutex_h_
+#define vpl_mutex_h_
+#ifdef __GNUC__
+#pragma interface
+#endif
+
+//:
+// \file
+// \author F. Schaffalitzky, Oxford RRG
+// \date   08 Dec 2001
+//
+// \verbatim
+// History
+//   08 Dec 2001 first version.
+// \endverbatim
+
+#include <vxl_config.h>
+
+#if VXL_HAS_PTHREAD_H
+# include <pthread.h>
+# include <vcl_cerrno.h> // for EBUSY, I think
+struct vpl_mutex
+{
+  vpl_mutex() { pthread_mutex_init(&mutex_, 0); }
+
+  void lock() { pthread_mutex_lock(&mutex_); }
+
+  //: returns `true' if lock was acquired.
+  bool trylock() { return pthread_mutex_trylock(&mutex_) != EBUSY; }
+
+  void unlock() { pthread_mutex_unlock(&mutex_); }
+
+  ~vpl_mutex() { pthread_mutex_destroy(&mutex_); }
+
+private:
+  pthread_mutex_t mutex_;
+
+  // disallow assignment.
+  vpl_mutex(vpl_mutex const &) { }
+  void operator=(vpl_mutex const &) { }
+};
+
+#else
+# error "only works with pthreads for now"
+#endif
+
+#endif
