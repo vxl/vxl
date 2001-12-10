e1ab92bc74c9793768cfc8ff6cead63cd3b49dcf
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-12-10 16:19:39 -0500

    using // as comment delimiter

--- core/vgui/vgui_glut.h
+++ core/vgui/vgui_glut.h
@@ -11,13 +11,12 @@
 #else
 # define VGUI_GLUT_PRESENT 1
 # if 1 // fsm@robots
-/*
-   It looks like the original #ifdef below had its condition reversed.
-   Also, it looks like Windows needs some serious hacking here, which
-   is the reason for the #ifdef being here.  For more info, check the
-   explanation in a comment around the top of glut.h
-    - Atanas Georgiev <atanas@cs.columbia.edu>
-*/
+
+// It looks like the original #ifdef below had its condition reversed.
+// Also, it looks like Windows needs some serious hacking here, which
+// is the reason for the #ifdef being here.  For more info, check the
+// explanation in a comment around the top of glut.h
+//  - Atanas Georgiev <atanas@cs.columbia.edu>
 # ifdef VCL_WIN32
    extern "C" void vgui_glut_exit(int);
 #  define exit vgui_glut_exit
