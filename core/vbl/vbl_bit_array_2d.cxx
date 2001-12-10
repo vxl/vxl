f60cfd49b6e71909bf82e54bdca2c65bf1ee74f8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-12-10 15:38:57 -0500

    ported vbl_bit_array_2d from gel/gbl

---
+++ core/vbl/vbl_bit_array_2d.cxx
@@ -0,0 +1,179 @@
+#ifdef __GNUC__
+#pragma implementation
+#endif
+//:
+// \file
+
+#include <vcl_iostream.h>
+#include <vcl_limits.h>  // for CHAR_BIT
+#include <vcl_cassert.h>
+#include <vcl_cstring.h> // for memcmp()
+
+#include "vbl_bit_array_2d.h"
+
+//: Copy constructor
+vbl_bit_array_2d::vbl_bit_array_2d(vbl_bit_array_2d const& that)
+  : data_(0), num_rows_(0), num_cols_(0)
+{
+  if( that.data_)
+  {
+    construct(that.num_rows_, that.num_cols_);
+
+    unsigned long byteindex;
+    unsigned int bitindex;
+    index( num_rows_-1, num_cols_-1, byteindex, bitindex);
+
+    for( int i=0; i<= byteindex; i++)
+      data_[i] = that.data_[i];
+  }
+}
+
+//: Assignment operator
+vbl_bit_array_2d& vbl_bit_array_2d::operator=(vbl_bit_array_2d const& that)
+{
+  if (num_rows_ != that.num_rows_ ||
+      num_cols_ != that.num_cols_)
+    resize(that.num_rows_, that.num_cols_);
+
+  unsigned long byteindex;
+  unsigned int bitindex;
+  index(num_rows_-1, num_cols_-1, byteindex, bitindex);
+
+  for (int i=0;i< byteindex;i++)
+    data_[i]= that.data_[i];
+
+  return *this;
+}
+
+//: Resizes and pads with zeros; keeps existing data
+void vbl_bit_array_2d::enlarge( int num_rows, int num_cols)
+{
+  assert(( num_rows> num_rows_) || ( num_cols> num_cols_));
+
+  unsigned char *tempdata= data_;
+  int tempm= num_rows_;
+  int tempn= num_cols_;
+
+  construct( num_rows, num_cols);
+  fill( false);
+
+  if( tempdata)
+  {
+    for( int i=0; i< tempm; i++)
+    {
+      // find start of new column
+      unsigned long byteindex;
+      unsigned int bitindex;
+      index( i, 0, byteindex, bitindex);
+
+      // find start of old column
+      unsigned long index= i* tempn;
+      unsigned long oldbyteindex= (unsigned long)(double(index)/CHAR_BIT);
+
+      for( int j=0; j< tempn/CHAR_BIT; j++)
+        data_[byteindex+j] = tempdata[oldbyteindex+j];
+    }
+    delete[] tempdata;
+  }
+}
+
+//: Fill with value
+void vbl_bit_array_2d::fill(bool value)
+{
+  unsigned long byteindex;
+  unsigned int bitindex;
+  index( num_rows_-1, num_cols_-1, byteindex, bitindex);
+
+  register unsigned char v = 0;
+
+  for( int i=0; i< CHAR_BIT; i++)
+  {
+    v <<= 1;
+    if (value) v |= 1;
+  }
+
+  for (int i=0;i<= byteindex;i++)
+    data_[i]= v;
+}
+
+int vbl_bit_array_2d::size() const { return rows()*cols()/CHAR_BIT; }
+
+void vbl_bit_array_2d::construct(int num_rows, int num_cols)
+{
+  // ensure that we have a factor of CHAR_BIT rows and cols
+  num_rows= (int((num_rows-1)/CHAR_BIT)+1)*CHAR_BIT;
+  num_cols= (int((num_cols-1)/CHAR_BIT)+1)*CHAR_BIT;
+
+  num_rows_ = num_rows;
+  num_cols_ = num_cols;
+  data_ = new unsigned char [num_rows*num_cols/CHAR_BIT + 1];
+}
+
+void vbl_bit_array_2d::index( unsigned int x, unsigned int y, unsigned long &byteindex, unsigned int &bitindex) const
+{
+  unsigned long index= x* num_cols_ + y;
+
+  byteindex= (unsigned long)(double(index)/CHAR_BIT);
+  bitindex = index%CHAR_BIT;
+}
+
+bool vbl_bit_array_2d::operator==(vbl_bit_array_2d const &a) const
+{
+  if (rows() != a.rows() || cols() != a.cols())
+    return false;
+  return 0 == vcl_memcmp(data_, a.data_, size());
+}
+
+bool vbl_bit_array_2d::operator() (int i, int j) const
+{
+  unsigned long byteindex;
+  unsigned int bitindex;
+  index( i, j, byteindex, bitindex);
+
+  unsigned char mask= (1<<bitindex);
+
+  return bool(data_[byteindex] & mask);
+}
+
+bool vbl_bit_array_2d::operator() (int i, int j)
+{
+  unsigned long byteindex;
+  unsigned int bitindex;
+  index( i, j, byteindex, bitindex);
+
+  unsigned char mask= (1<<bitindex);
+
+  return bool(data_[byteindex] & mask);
+}
+
+void vbl_bit_array_2d::put(int i, int j, bool const &x)
+{
+  unsigned long byteindex;
+  unsigned int bitindex;
+
+  index( i, j, byteindex, bitindex);
+
+  unsigned char mask= (((unsigned char )(x))<<bitindex);
+  unsigned char nmask= ~(((unsigned char )(1))<<bitindex);
+
+  data_[byteindex]= mask+(nmask & data_[byteindex]);
+}
+
+bool vbl_bit_array_2d::get(int i, int j) const
+{
+  return operator()(i,j);
+}
+
+//
+vcl_ostream& operator<< (vcl_ostream &os, const vbl_bit_array_2d &array)
+{
+  for( int i=0; i< array.rows(); i++)
+  {
+    for( int j=0; j< array.columns(); j++)
+      os << array(i,j) << " ";
+
+    os << vcl_endl;
+  }
+  return os;
+}
+
---
+++ core/vbl/vbl_bit_array_2d.h
@@ -0,0 +1,82 @@
+#ifndef vbl_bit_array_2d_h_
+#define vbl_bit_array_2d_h_
+#ifdef __GNUC__
+#pragma interface
+#endif
+// This is vxl/vbl/vbl_bit_array_2d.h
+
+//:
+// \file
+// \brief Contains class for a 2d bit array; interface as vbl_array_2d<T>
+// \author Geoffrey Cross
+//
+// \verbatim
+// Modifications
+// Peter Vanroose -4dec01- adapted interface to that of vbl_array_2d<T>
+// \endverbatim
+//-----------------------------------------------------------------------------
+
+#include <vcl_iosfwd.h>
+
+//: simple 2D bit array
+// essentially identical to vbl_array_2d<bool> but more efficiently stored
+class vbl_bit_array_2d {
+public:
+  // Default constructor
+  vbl_bit_array_2d() : data_(0), num_rows_(0), num_cols_(0) {}
+  //: Construct num_rows x num_cols array and leave data uninitialised
+  vbl_bit_array_2d(int m, int n) { construct(m,n); }
+  //: Construct num_rows x num_cols array and fill all cells with v
+  vbl_bit_array_2d(int m, int n, bool v) { construct(m,n); fill(v); }
+  //: Copy constructor
+  vbl_bit_array_2d(vbl_bit_array_2d const &);
+  // Destructor
+ ~vbl_bit_array_2d() { destruct(); }
+
+  //: Assignment operator
+  vbl_bit_array_2d& operator=(vbl_bit_array_2d const&);
+
+  //: Comparison
+  bool operator==(vbl_bit_array_2d const &a) const;
+  //: 
+  bool operator!=(vbl_bit_array_2d const &a) const { return ! operator==(a); }
+
+  // Operations----------------------------------------------------------------
+
+  //: Fill with value
+  void fill(bool value);
+  //: Delete contents and resize to m rows x n cols
+  void resize(int m, int n) { destruct(); construct(m,n); }
+  //: Resizes and pads with zeros; keeps existing data
+  void enlarge(int m, int n);
+  //: make as if default-constructed.
+  void clear() { if (data_) { destruct(); construct(0,0); } }
+
+  // Data Access---------------------------------------------------------------
+  bool operator() (int i, int j) const;
+  bool operator() (int i, int j);
+
+  void put(int i, int j, bool const &x);
+  bool get(int i, int j) const;
+
+  inline int rows() const { return num_rows_; }
+  inline int cols() const { return num_cols_; }
+  inline int columns() const { return num_cols_; }
+  //: Number of bytes allocated by the data
+  int size() const;
+
+private:
+  unsigned char *data_;
+  int num_rows_;
+  int num_cols_;
+
+  void destruct() { if (data_) delete[] data_; }
+  void construct(int m, int n);
+
+  //helper
+  void index( unsigned int x, unsigned int y, unsigned long &byteindex, unsigned int &bitindex) const;
+};
+
+vcl_ostream& operator<< (vcl_ostream& os, const vbl_bit_array_2d &v);
+
+#endif // vbl_bit_array_2d_h_
