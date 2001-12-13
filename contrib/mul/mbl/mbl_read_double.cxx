fa343efab66182cc2d6af2799642b4ded1d34841
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-12-13 09:17:28 -0500

    <TAB>s replaced by spaces

--- contrib/mul/mbl/mbl_read_double.cxx
+++ contrib/mul/mbl/mbl_read_double.cxx
@@ -4,97 +4,72 @@
 
 //:
 // \file
-// \brief Description!
+// \brief Asks question and waits for an answer
 // \author tim
 // hand crafted into vxl by gvw
-// 
-//  Function Name:  mbl_read_double
-//  Synopsis: double mbl_read_double(char* q_str, double default_d)
-//  Inputs:   q_str: A question 
-//      default_d: Default answer
-//        min_d: Min allowed value (optional)
-//        max_d: Max allowed reply (optional)
-//  Outputs:  -
-//  Returns:    The answer or a default
-//  Description:  Asks question and waits for an answer.
-//      If the answer is a double, returns it.
-//      If the answer is an empty vcl_string (return)
-//      then returns default.
-//      Otherwise waits for another input.
-//  References: -
-//  Example: 
-//    new_scale = mbl_read_double("Scale?",1.00);
-//    double new_scale = mbl_read_double("Scale?",1.00,min_scale,max_scale);
-
-//#include <cstdio>
 //
+//  Function Name:  mbl_read_double
+//  Synopsis:       double mbl_read_double(char* q_str, double default_d)
+//  Inputs:         q_str: A question
+//                  default_d: Default answer
+//                  min_d: Min allowed value (optional)
+//                  max_d: Max allowed value (optional)
+//  Outputs:        -
+//  Returns:        The answer or a default
+//  Description:    Asks question and waits for an answer.
+//                  If the answer is a double, returns it.
+//                  If the answer is an empty vcl_string (return)
+//                  then returns default.
+//                  Otherwise waits for another input.
+//  References:     -
+//  Example:
+//    double new_scale = mbl_read_double("Scale?",1.00);
+//    double new_scale = mbl_read_double("Scale?",1.00,min_scale,max_scale);
 
 #include <mbl/mbl_read_double.h>
 
 const int MAX_LEN = 40;
 
+// If min_d != 0 or max_d != 0 then prints range but doesn't check that reply is in range
 double RD_ReadDouble1(char *q_str, double default_d,
-					double min_d, double max_d)
-// If min_d != 0 or max_d != 0 then
-// Prints range but doesn't check that reply is in range
+                      double min_d, double max_d)
 {
-     double r;
-     int got_double = 0;   /* Set to 1 when find result */
+  char reply[MAX_LEN];
 
-     char reply[MAX_LEN];
+  while (true)
+  {
+    if (min_d==0 && max_d==0)
+      vcl_cout<<q_str<<" ("<<default_d<<") :";
+    else
+      vcl_cout<<q_str<<" ["<<min_d<<".."<<max_d<<"] ("<<default_d<<") :";
+    vcl_cout.flush();
 
-     do 
-	 {
-		if (min_d==0 && max_d==0)
-		{
-			vcl_cout<<q_str;
-			vcl_cout<<" ("<<default_d<<") :";
-		}
-		else
-		{
-			vcl_cout<<q_str;
-			vcl_cout<<" ["<<min_d<<".."<<max_d<<"] ("<<default_d<<") :";
-		}
-		vcl_cout.flush();
-
-		if (fgets(reply,MAX_LEN,stdin)!=NULL)
-		{
-	    	if (reply[0]=='\n') 
-			{
-	        	r = default_d;
-				got_double = 1;
-			}
-	    	else if (sscanf(reply,"%lf",&r)>0) got_double = 1;
-	    }
-	 }
-     while (got_double==0);
-     return r;
+    if (fgets(reply,MAX_LEN,stdin)!=NULL)
+    {
+      double r = default_d;
+      if (reply[0]=='\n' || sscanf(reply,"%lf",&r)>0)
+        return r;
+    }
+  }
 }
 
 double mbl_read_double(char *q_str, double default_d)
 {
-	return RD_ReadDouble1(q_str,default_d,0,0);
+  return RD_ReadDouble1(q_str,default_d,0,0);
 }
 
-double mbl_read_double(	char *q_str, 
-					double default_d,
-					double min_d,
-					double max_d)
+double mbl_read_double( char *q_str, double default_d,
+                        double min_d, double max_d)
 {
-	int acceptable = 0;
-	double R;
-	do
-	{
-		R = RD_ReadDouble1(q_str,default_d,min_d,max_d);
-		if (R<min_d)
-			vcl_cout<<"Must be at least "<<min_d<<"\n";
-		else
-		if (R>max_d)
-			vcl_cout<<"Must be no more than "<<max_d<<"\n";
-		else acceptable = 1;
-	}
-	while (!acceptable);
-	return R;
+  while (true)
+  {
+    double R = RD_ReadDouble1(q_str,default_d,min_d,max_d);
+    if (R<min_d)
+      vcl_cout<<R<<": must be at least "<<min_d<<"\n";
+    else if (R>max_d)
+      vcl_cout<<R<<": must be no more than "<<max_d<<"\n";
+    else
+      return R; // acceptable
+  }
 }
-	
 
--- contrib/mul/mbl/mbl_read_int.cxx
+++ contrib/mul/mbl/mbl_read_int.cxx
@@ -7,94 +7,68 @@
 // \brief Asks question and waits for an answer
 // \author tim, updated by me apparently!!!
 //  updated to vxl caringly in the way one can only do on a Friday afternoon by gvw
-//	Function Name:	mbl_read_int
-//	Synopsis:	int mbl_read_int(char* q_str, int default_int)
-//	Inputs:		q_str: A question 
-//			default_int: Default answer
-//			min_int: Min allowed value (optional)
-//			max_int: Max allowed value (optional)
-//	Outputs:	-
-//	Returns:	The answer or a default
-//	Description:	Asks question and waits for an answer.
-//			If the answer is an integer, returns it.
-//			If the answer is an empty vcl_string (return)
-//			then returns default.
-//			Otherwise waits for another input.
-//	References: -
-//	Example: 
-//  int new_scale = mbl_read_int("Scale?",5);
-//  int new_scale = mbl_read_int("Scale?",5,min_scale,max_scale);
-
-
-//#include <cstdio>
 //
-
+//  Function Name:  mbl_read_int
+//  Synopsis:       int mbl_read_int(char* q_str, int default_int)
+//  Inputs:         q_str: A question
+//                  default_int: Default answer
+//                  min_int: Min allowed value (optional)
+//                  max_int: Max allowed value (optional)
+//  Outputs:        -
+//  Returns:        The answer or a default
+//  Description:    Asks question and waits for an answer.
+//                  If the answer is an integer, returns it.
+//                  If the answer is an empty vcl_string (return)
+//                  then returns default.
+//                  Otherwise waits for another input.
+//  References:     -
+//  Example:
+//    int new_scale = mbl_read_int("Scale?",5);
+//    int new_scale = mbl_read_int("Scale?",5,min_scale,max_scale);
 
 #include <mbl/mbl_read_int.h>
 
 const int MAX_LEN = 20;
 
-int RD_ReadInt1(char* q_str, int default_int, 
-				int min_int, int max_int)
-// If min_int != 0 or max_int != 0 then
-// Displays range but doesn't check it.
+// If min_int != 0 or max_int != 0 then prints range but doesn't check that reply is in range
+int RD_ReadInt1(char* q_str, int default_int,
+                int min_int, int max_int)
 {
-	int r;
-	int got_int = 0;   /* Set to 1 when find result */
+  char reply[MAX_LEN];
 
-	char reply[MAX_LEN];
+  while (true)
+  {
+    if (min_int==0 && max_int==0)
+      vcl_cout<<q_str<<" ("<<default_int<<") :";
+    else
+      vcl_cout<<q_str<<" ["<<min_int<<".."<<max_int<<"] ("<<default_int<<") :";
+    vcl_cout.flush();
 
-	do 
-	{
-		if (min_int==0 && max_int==0)
-		{
-			vcl_cout<<q_str<<" ("<<default_int<<") :";
-		}
-		else
-		{
-			vcl_cout<<q_str<<" ["<<min_int<<".."<<max_int;
-			vcl_cout<<"] ("<<default_int<<") :";
-		}
-		vcl_cout.flush();
-
-		if (fgets(reply,MAX_LEN,stdin)!=NULL)
-		{
-			if (reply[0]=='\n') 
-			{
-				r = default_int;
-				got_int = 1;
-			}
-	    	else if (sscanf(reply,"%d",&r)>0) got_int = 1;
-		}
-	}
-	while (got_int==0);
-	return r;
+    if (fgets(reply,MAX_LEN,stdin)!=NULL)
+    {
+      int r = default_int;
+      if (reply[0]=='\n' || sscanf(reply,"%d",&r)>0)
+        return r;
+    }
+  }
 }
-
 
 int mbl_read_int(char* q_str, int default_int)
 {
-	return RD_ReadInt1(q_str,default_int,0,0);
+  return RD_ReadInt1(q_str,default_int,0,0);
 }
-    
-int mbl_read_int(char* q_str, int default_int, 
-				int min_int, int max_int)
-{
-	int R;
-	int acceptable = 0;
-	
-	do
-	{
-		R = RD_ReadInt1(q_str,default_int,min_int,max_int);
-		if (R<min_int)
-			vcl_cout<<"Must be at least "<<min_int<<"\n";
-		else
-		if (R>max_int)
-			vcl_cout<<"Must be no more than "<<max_int<<"\n";
-		else
-			acceptable = 1;
-	}
-	while (!acceptable);
 
-	return R;
+int mbl_read_int(char* q_str, int default_int,
+                 int min_int, int max_int)
+{
+  while (true)
+  {
+    int R = RD_ReadInt1(q_str,default_int,min_int,max_int);
+    if (R<min_int)
+      vcl_cout<<R<<": must be at least "<<min_int<<"\n";
+    else if (R>max_int)
+      vcl_cout<<R<<": must be no more than "<<max_int<<"\n";
+    else
+      return R; // acceptable
+  }
 }
