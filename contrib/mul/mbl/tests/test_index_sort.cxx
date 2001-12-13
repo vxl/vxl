7b6b0635304eb6bb13ed03dc56f8585467d93277
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2001-12-13 09:18:17 -0500

    2 tests added

--- contrib/mul/mbl/tests/test_index_sort.cxx
+++ contrib/mul/mbl/tests/test_index_sort.cxx
@@ -7,42 +7,43 @@
 
 void test_index_sort()
 {
-  vcl_cout << "**********************" << vcl_endl;
-  vcl_cout << " Testing mbl_index_sort " << vcl_endl;
-  vcl_cout << "**********************" << vcl_endl;
+  vcl_cout << "**********************\n"
+           << "Testing mbl_index_sort\n"
+           << "**********************\n";
 
-	int n = 20;
+  const int n = 20;
   vcl_vector<double> x(n);
-	vcl_vector<int> index;
+  vcl_vector<int> index;
 
-	for (int i=0;i<n;++i) x[i]=n-i;
+  for (int i=0;i<n;++i) x[i]=n-i;
 
-	mbl_index_sort(x,index);
+  mbl_index_sort(x,index);
 
-	TEST("Correct number of elements",index.size()==n,true);
+  TEST("Correct number of elements",index.size()==n,true);
 
-	bool order_ok = true;
-	for (int i=1;i<n;++i)
-	  if (x[index[i]]<x[index[i-1]]) order_ok=false;
+  bool order_ok = true;
+  for (int i=1;i<n;++i)
+    if (x[index[i]]<x[index[i-1]]) order_ok=false;
 
-  TEST("First element",index[0]==(n-1),true);
-  TEST("Last element",index[n-1]==0,true);
-
-  TEST("Order correct",order_ok,true);
-
-	for (int i=0;i<n;++i) x[i]=(i+3)%n;
-
-	mbl_index_sort(x,index);
-
-	TEST("Correct number of elements",index.size()==n,true);
-
-	order_ok = true;
-	for (int i=1;i<n;++i)
-	  if (x[index[i]]<x[index[i-1]]) order_ok=false;
+  TEST("First element",index[0],  n-1);
+  TEST("Last element", index[n-1],0);
 
   TEST("Order correct",order_ok,true);
 
+  for (int i=0;i<n;++i) x[i]=(i+3)%n;
 
+  mbl_index_sort(x,index);
+
+  TEST("Correct number of elements",index.size()==n,true);
+
+  order_ok = true;
+  for (int i=1;i<n;++i)
+    if (x[index[i]]<x[index[i-1]]) order_ok=false;
+
+  TEST("First element",index[0],  n-3);
+  TEST("Last element", index[n-1],n-4);
+
+  TEST("Order correct",order_ok,true);
 }
 
 TESTMAIN(test_index_sort);
