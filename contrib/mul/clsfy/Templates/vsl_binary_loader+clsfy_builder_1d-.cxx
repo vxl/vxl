acd2f430d632678c3226fa2828235cc59bb8a4f3
Tim Cootes <timcootes@users.sourceforge.net>
2001-12-18 09:36:34 -0500

    Added templates for scalar classifiers

---
+++ contrib/mul/clsfy/Templates/vsl_binary_loader+clsfy_builder_1d-.cxx
@@ -0,0 +1,3 @@
+#include <vsl/vsl_binary_loader.txx>
+#include <clsfy/clsfy_builder_1d.h>
+VSL_BINARY_LOADER_INSTANTIATE(clsfy_builder_1d);
---
+++ contrib/mul/clsfy/Templates/vsl_binary_loader+clsfy_classifier_1d-.cxx
@@ -0,0 +1,3 @@
+#include <vsl/vsl_binary_loader.txx>
+#include <clsfy/clsfy_classifier_1d.h>
+VSL_BINARY_LOADER_INSTANTIATE(clsfy_classifier_1d);
