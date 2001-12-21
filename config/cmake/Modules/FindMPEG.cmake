eec129b6237e8f0fb4b7ca919f4e75dcb2f8bd5c
Andrew Fitzgibbon <awf@users.sourceforge.net>
2001-12-21 13:19:05 -0500

    mpeg lib

---
+++ config/cmake/Modules/FindMPEG.cmake
@@ -0,0 +1,36 @@
+#
+# Find a MPEG library
+#
+
+IF(NOT HAS_MPEG)
+  INCLUDE( ${allvxl_SOURCE_DIR}/config.cmake/Modules/FindNativeMPEG.cmake )
+  
+  IF(NOT HAS_NATIVE_MPEG)
+  
+    #
+    # At some point, in a "release" version, it is possible that someone
+    # will not have the v3p png library, so make sure the headers
+    # exist.
+    #
+  
+    FIND_PATH( MPEG_INCLUDE_PATH png.h
+      ${allvxl_SOURCE_DIR}/v3p/png
+    )
+  
+    IF(MPEG_INCLUDE_PATH)
+  
+      SET( HAS_MPEG "YES" )
+      ADD_DEFINITIONS( -DHAS_MPEG )
+  
+      INCLUDE( ${allvxl_SOURCE_DIR}/v3p/png/CMakeListsLink.txt )
+  
+    ENDIF(MPEG_INCLUDE_PATH)
+  
+  ELSE(NOT HAS_NATIVE_MPEG)
+  
+    SET( HAS_MPEG "YES" )
+    ADD_DEFINITIONS( -DHAS_MPEG )
+  
+  ENDIF(NOT HAS_NATIVE_MPEG)
+
+ENDIF(NOT HAS_MPEG)
---
+++ config/cmake/Modules/FindNativeMPEG.cmake
@@ -0,0 +1,32 @@
+#
+# Find the native MPEG includes and library
+#
+
+IF(NOT HAS_NATIVE_MPEG)  
+  FIND_PATH( NATIVE_MPEG_INCLUDE_PATH mpeg2dec/video_out.h
+    /usr/local/include
+    /usr/include
+  )
+  
+  FIND_LIBRARY( NATIVE_MPEG_LIBRARY mpeg2lib
+    /usr/lib
+    /usr/local/lib
+  )
+  
+  FIND_LIBRARY( NATIVE_MPEG_VO_LIBRARY libvo
+    /usr/lib
+    /usr/local/lib
+  )
+  
+  IF(NATIVE_MPEG_INCLUDE_PATH)
+   IF(NATIVE_MPEG_LIBRARY)
+  
+    SET( HAS_NATIVE_MPEG "YES" )
+  
+    INCLUDE_DIRECTORIES( ${NATIVE_MPEG_INCLUDE_PATH} )
+    LINK_LIBRARIES( ${NATIVE_MPEG_LIBRARY} )
+    LINK_LIBRARIES( ${NATIVE_MPEG_VO_LIBRARY} )
+  
+   ENDIF(NATIVE_MPEG_LIBRARY)
+  ENDIF(NATIVE_MPEG_INCLUDE_PATH)
+ENDIF(NOT HAS_NATIVE_MPEG)
