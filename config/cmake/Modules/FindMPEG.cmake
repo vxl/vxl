705d84e110420cb8a14deb6ae239867b8f0befba
Amitha Perera <amithaperera@users.sourceforge.net>
2002-01-07 22:57:50 -0500

    There is no mpeg library in v3p. Therefore, no native mpeg => no mpeg.

--- config/cmake/Modules/FindMPEG.cmake
+++ config/cmake/Modules/FindMPEG.cmake
@@ -6,25 +6,28 @@ IF(NOT HAS_MPEG)
   INCLUDE( ${allvxl_SOURCE_DIR}/config.cmake/Modules/FindNativeMPEG.cmake )
   
   IF(NOT HAS_NATIVE_MPEG)
-  
+
+    # There is no mpeg library in v3p. If you don't have a native library installed,
+    # then you don't have mpeg support - Amitha Perera.
+
+    # #
+    # # At some point, in a "release" version, it is possible that someone
+    # # will not have the v3p png library, so make sure the headers
+    # # exist.
+    # #
+    # 
+    # FIND_PATH( MPEG_INCLUDE_PATH png.h
+    #   ${allvxl_SOURCE_DIR}/v3p/png
+    # )
     #
-    # At some point, in a "release" version, it is possible that someone
-    # will not have the v3p png library, so make sure the headers
-    # exist.
-    #
-  
-    FIND_PATH( MPEG_INCLUDE_PATH png.h
-      ${allvxl_SOURCE_DIR}/v3p/png
-    )
-  
-    IF(MPEG_INCLUDE_PATH)
-  
-      SET( HAS_MPEG "YES" )
-      ADD_DEFINITIONS( -DHAS_MPEG )
-  
-      INCLUDE( ${allvxl_SOURCE_DIR}/v3p/png/CMakeListsLink.txt )
-  
-    ENDIF(MPEG_INCLUDE_PATH)
+    # IF(MPEG_INCLUDE_PATH)
+    #  
+    #   SET( HAS_MPEG "YES" )
+    #   ADD_DEFINITIONS( -DHAS_MPEG )
+    # 
+    #  INCLUDE( ${allvxl_SOURCE_DIR}/v3p/png/CMakeListsLink.txt )
+    # 
+    # ENDIF(MPEG_INCLUDE_PATH)
   
   ELSE(NOT HAS_NATIVE_MPEG)
   
