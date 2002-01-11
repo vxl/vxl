3dd0b37125c48ea3485c6f8ccb117342d140a22f
Amitha Perera <amithaperera@users.sourceforge.net>
2002-01-11 07:51:02 -0500

    A few more HAS_MPEG type changes to get a green dashboard. (At least under FreeBSD.)

--- contrib/oxl/apps/glmovie/glmovie.cxx
+++ contrib/oxl/apps/glmovie/glmovie.cxx
@@ -38,6 +38,8 @@
 
 #ifdef VCL_WIN32
 #include <vidl/vidl_avicodec.h>
+#endif
+#ifdef HAS_MPEG
 #include <oxp/oxp_vidl_mpeg_codec.h>
 #endif
 
@@ -741,6 +743,8 @@ int main(int argc, char ** argv)
   // Register video codec
 #ifdef VCL_WIN32
   vidl_io::register_codec(new vidl_avicodec);
+#endif
+#if HAS_MPEG
   vidl_io::register_codec(new oxp_vidl_mpeg_codec);
 #endif
 
