829d29a9529f2db59e5c41b5e741810ca057c559
Amitha Perera <amithaperera@users.sourceforge.net>
2002-01-11 16:37:43 -0500

    Dart config for sending email on broken continuous builds.

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -15,3 +15,13 @@ SET (TRIGGER_SITE "http://cgi.cs.rpi.edu/~perera/vxl/submit-testing-results.cgi"
 # Server configuration
 #
 SET (CVS_WEB_URL "http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/vxl/vxl/")
+
+SET (DELIVER_CONTINUOUS_EMAIL "ON")  #  (not TRUE, not YES, just ON)
+SET (CONTINUOUS_FROM "vxl-maintainers@lists.sourceforge.net")
+# This is probably best set on the server build's cache
+# SET (SMTP_MAILHOST "a smtp server")
+SET (DEFAULT_CONTINUOUS_DOMAIN "users.sourceforge.net")
+SET (CONTINUOUS_MONITOR_LIST "vxl-maintainers@lists.sourceforge.net")
+SET (CONTINUOUS_BASE_URL "http://www.cs.rpi.edu/research/vision/vxl/testing")
+SET (PROJECT_NAME "VXL")
+
