ea9dfb6cd7b2385c8ae7edd59c5b0edbd702e0ef
Amitha Perera <amithaperera@users.sourceforge.net>
2002-01-14 10:49:50 -0500

    Do not send email to a list when continuous builds break.

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -23,7 +23,7 @@ SET (CONTINUOUS_FROM "vxl-maintainers@lists.sourceforge.net")
 # This is probably best set on the server build's cache
 # SET (SMTP_MAILHOST "a smtp server")
 SET (DEFAULT_CONTINUOUS_DOMAIN "users.sourceforge.net")
-SET (CONTINUOUS_MONITOR_LIST "vxl-maintainers@lists.sourceforge.net")
+SET (CONTINUOUS_MONITOR_LIST "")
 SET (CONTINUOUS_BASE_URL "http://www.cs.rpi.edu/research/vision/vxl")
 SET (PROJECT_NAME "VXL")
 
