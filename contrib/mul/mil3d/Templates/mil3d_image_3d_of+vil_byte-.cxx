750bf1681bb1a65115c0f3bba0338eae37966ced
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-01-16 08:33:52 -0500

    no <EOLN> at <EOF>

--- contrib/mul/mil3d/Templates/mil3d_image_3d_of+vil_byte-.cxx
+++ contrib/mul/mil3d/Templates/mil3d_image_3d_of+vil_byte-.cxx
@@ -1,6 +1,4 @@
 #include <mil3d/mil3d_image_3d_of.txx>
 #include <vil/vil_byte.h>
-template class mil3d_image_3d_of<vil_byte>;
 
-// MIL_IMAGE_3D_OF_INSTANTIATE(vil_byte);
- 
+template class mil3d_image_3d_of<vil_byte>;
