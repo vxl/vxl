3ee6cb02fa8101817b6acd9bd62bf989266fb1c9
Graham Vincent <grv@users.sourceforge.net>
2002-01-16 06:43:28 -0500

    Template instantiations for mil3d

---
+++ contrib/mul/mil3d/Templates/mil3d_image_3d_of+long-.cxx
@@ -0,0 +1,4 @@
+#include <mil3d/mil3d_image_3d_of.txx>
+template class mil3d_image_3d_of<long>;
+
+// MIL_IMAGE_3D_OF_INSTANTIATE(short);
---
+++ contrib/mul/mil3d/Templates/mil3d_image_3d_of+vil_byte-.cxx
@@ -0,0 +1,6 @@
+#include <mil3d/mil3d_image_3d_of.txx>
+#include <vil/vil_byte.h>
+template class mil3d_image_3d_of<vil_byte>;
+
+// MIL_IMAGE_3D_OF_INSTANTIATE(vil_byte);
+ 
