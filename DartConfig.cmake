869aadc43e8c20ca779d516adff00d6dc85e2dc6
Amitha Perera <amithaperera@users.sourceforge.net>
2002-01-17 16:54:36 -0500

    Remove server config variables from the repository. The clients don't use it, and it just creates update headaches for the server.

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -11,21 +11,3 @@ SET (DROP_SITE_USER "ftp")
 SET (DROP_SITE_PASSWORD "vxl-tester@somewhere.com")
 SET (TRIGGER_SITE "http://cgi.cs.rpi.edu/~perera/vxl/submit-testing-results.cgi")
 
-#
-# Server configuration
-#
-SET (CVS_WEB_URL "http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/vxl/vxl/")
-SET (USE_GNATS "ON")
-SET (GNATS_WEB_URL "http://sourceforge.net/tracker/?group_id=24293")
-
-#  (not TRUE, not YES, just ON)
-SET (DELIVER_CONTINUOUS_EMAIL "ON")
-
-SET (CONTINUOUS_FROM "vxl-maintainers@lists.sourceforge.net")
-# This is probably best set on the server build's cache
-# SET (SMTP_MAILHOST "a smtp server")
-SET (DEFAULT_CONTINUOUS_DOMAIN "users.sourceforge.net")
-SET (CONTINUOUS_MONITOR_LIST "")
-SET (CONTINUOUS_BASE_URL "http://www.cs.rpi.edu/research/vision/vxl/Testing")
-SET (PROJECT_NAME "VXL")
-
