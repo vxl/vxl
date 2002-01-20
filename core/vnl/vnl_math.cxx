8da2cd6545a166dece6785a7592657eadec7af4d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-01-20 06:33:29 -0500

    ieeefp.h does not define finitef()

--- core/vnl/vnl_math.cxx
+++ core/vnl/vnl_math.cxx
@@ -16,6 +16,7 @@
 # define isnan _isnan
 #elif VXL_IEEEFP_HAS_FINITE
 # include <ieeefp.h>
+# define finitef finite
 
 #elif VXL_MATH_HAS_FINITE
 # include <math.h>  // dont_vxl_filter: this is *not* supposed to be <cmath>
