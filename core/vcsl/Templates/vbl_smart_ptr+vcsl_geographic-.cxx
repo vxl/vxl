e7fed3daf9140e3957fbe15557530a622d18c985
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-01-28 09:48:59 -0500

    extra #includes needed

--- core/vcsl/Templates/vbl_smart_ptr+vcsl_geographic-.cxx
+++ core/vcsl/Templates/vbl_smart_ptr+vcsl_geographic-.cxx
@@ -1,4 +1,6 @@
 #include <vcsl/vcsl_axis.h> // see vbl_smart_ptr+vcsl_cartesian-.cxx
+#include <vcsl/vcsl_graph.h>
+#include <vcsl/vcsl_spatial_transformation.h>
 
 #include <vcsl/vcsl_geographic.h>
 #include <vbl/vbl_smart_ptr.txx>
--- core/vcsl/Templates/vbl_smart_ptr+vcsl_spherical-.cxx
+++ core/vcsl/Templates/vbl_smart_ptr+vcsl_spherical-.cxx
@@ -1,4 +1,6 @@
 #include <vcsl/vcsl_axis.h> // see vbl_smart_ptr+vcsl_cartesian-.cxx
+#include <vcsl/vcsl_graph.h>
+#include <vcsl/vcsl_spatial_transformation.h>
 
 #include <vcsl/vcsl_spherical.h>
 #include <vbl/vbl_smart_ptr.txx>
