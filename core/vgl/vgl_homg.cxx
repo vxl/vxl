6f00760809c7c96d850645e8f2f8a2b89a087293
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-01-31 03:35:18 -0500

    specialisation for float and for long double added

--- core/vgl/vgl_homg.cxx
+++ core/vgl/vgl_homg.cxx
@@ -8,5 +8,11 @@
 # include <math.h> // dont_vxl_filter: no HUGE_VAL in <cmath>
 #endif
 
+float vgl_homg<float>::infinity = HUGE_VAL;
+float vgl_homg<float>::infinitesimal_tol = 1e-12;
+
 double vgl_homg<double>::infinity = HUGE_VAL;
 double vgl_homg<double>::infinitesimal_tol = 1e-12;
+
+long double vgl_homg<long double>::infinity = HUGE_VAL;
+long double vgl_homg<long double>::infinitesimal_tol = 1e-12;
