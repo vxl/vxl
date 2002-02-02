15869d54c3422f3f9d7a94e39bd4d54d0d74eb94
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-02-01 19:47:06 -0500

    test consistency of all header files by just including them in the test source file

---
+++ core/vpl/tests/vpl_test_all.cxx
@@ -0,0 +1,8 @@
+#include <vpl/vpl.h>
+ 
+#include <vxl_config.h>
+#if VXL_HAS_PTHREAD_H
+#include <vpl/vpl_mutex.h>
+#endif
+
+int main() { return 0; }
---
+++ core/vsl/tests/vsl_test_all.cxx
@@ -0,0 +1,20 @@
+#include <vsl/vsl_fwd.h>
+#include <vsl/vsl_test.h>
+#include <vsl/vsl_binary_io.h>
+#include <vsl/vsl_binary_explicit_io.h>
+#include <vsl/vsl_binary_loader_base.h>
+#include <vsl/vsl_binary_loader.h>
+#include <vsl/vsl_clipon_binary_loader.h>
+#include <vsl/vsl_complex_io.h>
+#include <vsl/vsl_deque_io.h>
+#include <vsl/vsl_indent.h>
+#include <vsl/vsl_list_io.h>
+#include <vsl/vsl_map_io.h>
+#include <vsl/vsl_pair_io.h>
+#include <vsl/vsl_quick_file.h>
+#include <vsl/vsl_set_io.h>
+#include <vsl/vsl_stack_io.h>
+#include <vsl/vsl_string_io.h>
+#include <vsl/vsl_vector_io.h>
+
+int main() { return 0; }
