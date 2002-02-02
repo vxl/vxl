a28112dc72b7bb35b0ccb85bbc051ede5967f38f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-02-02 17:02:14 -0500

    deprecated file now removed

--- core/vpl/vpl_unistd.h
+++
@@ -1,9 +0,0 @@
-#ifndef vpl_unistd_h_
-#define vpl_unistd_h_
-
-// This header is deprecated. Include vpl/vpl.h instead.
-
-#include <vcl_deprecated_header.h>
-#include <vpl/vpl.h>
-
-#endif // vpl_unistd_h_
