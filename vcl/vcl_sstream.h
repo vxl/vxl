2f5d2d09fe5056f44c07f8d83ed939e9f1a13490
Ian Scott <scottim@users.sourceforge.net>
2002-02-15 10:16:23 -0500

    libstdg++ no longer defines GNU_LIBSTDCXX_V3, or any other identifier that I can find. So instead rely on configure script to decide whether to use real <sstream>

--- vcl/vcl_sstream.h
+++ vcl/vcl_sstream.h
@@ -7,7 +7,7 @@
 // this is to get the vcl_ios_* macros.
 #include "vcl_iostream.h"
 
-#if (defined(VCL_GCC) && !defined(GNU_LIBSTDCXX_V3)) || defined(VCL_SGI_CC_720)
+#if (defined(VCL_GCC) && !VCL_CXX_HAS_HEADER_SSTREAM) || defined(VCL_SGI_CC_720)
 # include <strstream.h>
 # include <vcl_string.h>
 
