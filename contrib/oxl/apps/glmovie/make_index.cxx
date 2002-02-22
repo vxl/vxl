723e48fb388ba9210db653335920e9a9e1a02c46
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-02-22 05:44:38 -0500

    stdio replaced with iostream

--- contrib/oxl/apps/glmovie/make_index.cxx
+++ contrib/oxl/apps/glmovie/make_index.cxx
@@ -1,10 +1,11 @@
-/*
-  fsm@robots.ox.ac.uk
-*/
+// This is ./oxl/apps/glmovie/make_index.cxx
 
-// g++ -O3 -o ./mpeg_chunks ./mpeg_chunks.cxx -Wl,-Bstatic -lstdc++
+//:
+// \file
+// \author fsm@robots.ox.ac.uk
 
-#include <vcl_cstdio.h>
+#include <vcl_fstream.h>
+#include <vcl_iostream.h>
 #include <vcl_cassert.h>
 #include <vcl_cstring.h>
 #include <vcl_memory.h>
@@ -12,83 +13,81 @@
 struct scanner_t
 {
   scanner_t(char const *filename)
-    : m_fp(vcl_fopen(filename, "rb"))
+    : m_fp(filename, vcl_ios_binary)
     , m_p(m_buf)
     , m_q(m_buf)
-    , m_where(0)
+    , m_where(0L)
   {
     if (! m_fp) {
-      vcl_fprintf(stderr, "%s:%d failed to open \'%s\' for reading\n", __FILE__, __LINE__, filename);
+      vcl_cerr << __FILE__ << ':' << __LINE__
+               << " failed to open '" << filename << "' for reading:"
+               << " error code " << m_fp.fail() << '\n';
       assert(false);
     }
   }
-  
+
   ~scanner_t()
   {
-    if (m_fp) {
-      vcl_fclose(m_fp);
-      m_fp = 0;
-    }
+    // if (m_fp.rdbuf()->is_open()) m_fp.close();
+    // Not needed, since it is implicitly done by m_fp's destructor
   }
-  
+
   // seek to position.
-  void seek(long pos)
+  void seek(vcl_streampos pos)
   {
     m_p = m_q = m_buf;
-    if (! m_fp)
+    if (! m_fp || ! m_fp.rdbuf()->is_open())
       return;
-    if (vcl_fseek(m_fp, pos, SEEK_SET) == 0) {
+    m_fp.seekg(pos);
+    if (m_fp.tellg() == pos)
       m_where = pos;
-    }
-    else {
-      // error.
-      vcl_fclose(m_fp);
-      m_fp = 0;
-    }
+    else // error.
+      m_fp.close();
   }
-  
+
   // return position of next byte to be read.
   long tell() const
   {
     return m_where;
   }
-  
+
   // get one byte, or -1 on EOF.
   int get()
   {
     if (m_p == m_q) {
       // this branch is taken when we need more data, i.e. rarely.
-      if (! m_fp)
+      if (! m_fp || ! m_fp.rdbuf()->is_open())
         return -1;
-      int n = vcl_fread(m_buf, 1, sizeof m_buf, m_fp);
+      m_fp.read(m_buf, sizeof m_buf);
+      int n = m_fp.gcount();
       if (n > 0) {
         m_p = m_buf;
         m_q = m_p + n;
       }
       else {
-	printf("fread=%d\n", n);
+        vcl_cout << "read=" << n << '\n';
         // probably EOF.
-        fclose(m_fp);
-        m_fp = 0;
+        m_fp.close();
         return -1;
       }
     }
-    
+
 #if 1
-    ++ m_where;
+    m_where += 1;
     return *m_p++;
 #else
     unsigned char byte = *m_p++;
-    vcl_fprintf(stdout, "read: 0x%08lX 0x%02X\n", m_where++, unsigned(byte));
+    vcl_cout << "read: " << vcl_hex << m_where << ' ' << vcl_hex << unsigned(byte) << '\n';
+    m_where += 1;
     return byte;
 #endif
   }
-  
-private: 
-  vcl_FILE *m_fp;
-  unsigned char *m_p, *m_q;
-  unsigned char m_buf[65536];
-  long m_where;
+
+private:
+  vcl_ifstream m_fp;
+  char *m_p, *m_q;
+  char m_buf[65536];
+  vcl_streampos m_where;
 };
 
 void make_mpeg_index(char const* in, char const* out)
@@ -104,54 +103,52 @@ void make_mpeg_index(char const* in, char const* out)
       p = buf + vcl_strlen(buf)-1;
     }
     vcl_strcpy(p, ".idx");
-    vcl_fprintf(stderr, "Making index file [%s]\n", buf);
+    vcl_cerr << "Making index file [" << buf << "]\n";
     out = buf;
   }
-      
-  vcl_FILE* fp = fopen(out, "w");
-  vcl_fprintf(fp, "MPEG_IDX %s\n", in);
-  
+
+  vcl_ofstream fp(out);
+  fp << "MPEG_IDX " << in << '\n';
+
   unsigned shift = 0;
   long last_mb = 0;
 
-  int n = 0;  
+  int n = 0;
   while (true) {
     int c = scanner.get();
     ++n;
     if (c < 0) {
-      // std :: printf("Brk on char %d, scnr = %d\n", n, c);
+      // vcl_cout << "Brk on char " << n << ", scnr = " << c << '\n';
       break; // EOF
     }
-    
+
     shift = (shift | c) << 8; // NB. last 8 bits will be zero.
     if (shift == 0x00000100) {
       long pos = scanner.tell() - 3;
       c = scanner.get(); shift = (shift | c) << 8;
-      
+
       int code = c;
-      //vcl_printf("code = 0x%02X\n", code);
+      // vcl_printf("code = 0x%02X\n", code);
       if (code == 0x00) {
         c = scanner.get(); shift = (shift | c) << 8;
         c = scanner.get(); shift = (shift | c) << 8;
         int type = (c >> 3) & 0x07;
-        vcl_fprintf(fp, "0x%08lX 0x%02X %c\n", pos, code, "01234567"[type]);
+        fp << vcl_hex << pos << ' ' << vcl_hex << code << ' ' << type << '\n';
       }
       else if (code < 0xB0)
         ; // ignore
       else {
-        vcl_fprintf(fp, "0x%08lX 0x%02X\n", pos, code);
+        fp << vcl_hex << pos << ' ' << vcl_hex << code << '\n';
       }
 
       // show progress.
       if ((pos>>20) > last_mb) {
         last_mb = (pos>>20);
-        vcl_fprintf(stderr, "[%ld Mb]", (pos>>20));
-        vcl_fflush(stderr);
+        vcl_cerr << "[" << last_mb << " Mb]" << vcl_flush;
       }
     }
   }
-  vcl_fclose(fp);
-  vcl_printf("Index done\n");
-  // long pos = scanner.tell();
-  // vcl_printf("0x%08lX\n", pos);
+  fp.close();
+  vcl_cout << "Index done\n";
+  // vcl_printf("0x%08lX\n", scanner.tell());
 }
