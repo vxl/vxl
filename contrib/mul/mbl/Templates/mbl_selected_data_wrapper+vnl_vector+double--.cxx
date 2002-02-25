dcf0830c4cdf3bc3529f8d9c3c76dbeb43330003
Ian Scott <scottim@users.sourceforge.net>
2002-02-25 09:36:36 -0500

    fixed erroneous macro - don't know why the compiler didn't pick it up.

--- contrib/mul/mbl/Templates/mbl_selected_data_wrapper+vnl_vector+double--.cxx
+++ contrib/mul/mbl/Templates/mbl_selected_data_wrapper+vnl_vector+double--.cxx
@@ -1,3 +1,3 @@
 #include<mbl/mbl_selected_data_wrapper.txx>
 #include<vnl/vnl_vector.h>
-MBL_DATA_SELECTION_WRAPPER_INSTANTIATE(vnl_vector<double>);
+MBL_SELECTED_DATA_WRAPPER_INSTANTIATE(vnl_vector<double>);
