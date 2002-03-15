ce43bef1faf0607b89b8294944288975abf05cd1
Andrew Fitzgibbon <awf@users.sourceforge.net>
2002-03-15 07:25:22 -0500

    just a documentation pointer...

---
+++ core/vnl/vnl_random.h
@@ -0,0 +1,16 @@
+//-*- c++ -*-------------------------------------------------------------------
+#ifndef vnl_random_h_
+#define vnl_random_h_
+#ifdef __GNUC__
+#pragma interface
+#endif
+// Author: awf@robots.ox.ac.uk
+// Created: 15 Sep 01
+
+//: This file is here only to point you toward vnl_sample.h
+//  if you're looking for random number generation.
+//  The function is a dummy
+void vnl_random_sample();
+
+#endif   // DO NOT ADD CODE AFTER THIS LINE! END OF DEFINITION FOR CLASS vnl_random.
+
