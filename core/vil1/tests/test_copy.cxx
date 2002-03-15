e0acd3629b7517963ac2aa6ad365d25e4b39b4b9
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-03-15 17:27:19 -0500

    simplified by using vil_test.h

--- core/vil1/tests/test_copy.cxx
+++ core/vil1/tests/test_copy.cxx
@@ -5,21 +5,25 @@
 #include <vil/vil_copy.h>
 
 #include <vxl_config.h>
-
 #include <vcl_iostream.h>
+#include <vil/vil_test.h>
+
+static char default_filename[] = "square.pgm";
 
 int
 main( int argc, char* argv[] )
 {
-  vcl_cout << "Testing vil_copy\n";
-  if( argc==1 ) {
-    vcl_cerr << "Need an input image" << vcl_endl;
-    return 1;
+  vil_test_start("test_vil_copy");
+
+  char* filename = argv[1];
+  if( argc<2 ) {
+    filename = default_filename;
+    vcl_cerr << "Using default input image " << filename << vcl_endl;
   }
 
-  vil_memory_image_of<vxl_uint_8> a( vil_load( argv[1] ) );
+  vil_memory_image_of<vxl_uint_8> a( vil_load( filename ) );
   if( !a ) {
-    vcl_cerr << "Could not load image " << argv[1] << vcl_endl;
+    vcl_cerr << "Could not load image " << filename << vcl_endl;
     return 1;
   }
   vil_memory_image_of<vxl_uint_8> b( a.width(), a.height() );
@@ -28,11 +32,6 @@ main( int argc, char* argv[] )
   a(0,0) = 0;
   b(0,0) = 1;
   c(0,0) = 2;
-  if( a(0,0) != b(0,0) && a(0,0) != c(0,0) && b(0,0) != c(0,0) ) {
-    vcl_cout << "  PASSED  \n";
-    return 0;
-  } else {
-    vcl_cout << "**FAILED**\n";
-    return 1;
-  }
+  TEST("", a(0,0) != b(0,0) && a(0,0) != c(0,0) && b(0,0) != c(0,0) , true);
+  return vil_test_summary();
 }
