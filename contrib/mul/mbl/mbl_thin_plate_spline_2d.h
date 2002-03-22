c993ed46a66bd5783aa6568d4a4def4cf431980d
Tim Cootes <timcootes@users.sourceforge.net>
2002-03-22 04:43:58 -0500

    Added thin plane spine class for 2D data. Not added to CMake until I test on a second system.

---
+++ contrib/mul/mbl/mbl_thin_plate_spline_2d.h
@@ -0,0 +1,119 @@
+#ifndef mbl_thin_plate_spline_2d_h_
+#define mbl_thin_plate_spline_2d_h_
+
+//:
+// \file
+// \brief Construct thin plate spline to map 2D to 2D
+// \author Tim Cootes
+
+#include <vcl_string.h>
+#include <vgl/vgl_point_2d.h>
+#include <vcl_vector.h>
+#include <vnl/vnl_vector.h>
+#include <vnl/vnl_matrix.h>
+#include <vsl/vsl_binary_io.h>
+
+//=======================================================================
+//: Construct thin plate spline to map 2D to 2D
+// ie does some mapping (x',y') = f(x,y). (See Booksteins work, eg IPMI 1993)
+// The warp is `guided' by a set of
+// landmarks p(0) .. p(n-1) in the source plane which are to be
+// mapped to a (possibly deformed) set q(0)..q(n-1) in the destination.
+// Thus the mapping is constrained so that f(p(i)) = q(i) for i = 0..n-1.
+// The points are given to the build() function to set up the object.
+//
+// If one wishes to map a set of source points to multiple target points,
+// use set_source_pts(src_pts);  then build(target_pts); for each target set.
+class mbl_thin_plate_spline_2d {
+private:
+	vnl_vector<double> Wx_,Wy_;
+	double Ax0_, AxX_, AxY_;
+	double Ay0_, AyX_, AyY_;
+	double energy_x_,energy_y_;
+
+	vcl_vector<vgl_point_2d<double> > src_pts_;
+
+	  //: Used to estimate weights in set_source_points()
+	vnl_matrix<double> L_inv_;
+
+	  //: Build from small number of points
+	void build_pure_affine(const vcl_vector<vgl_point_2d<double> >& source_pts,
+				const vcl_vector<vgl_point_2d<double> >& dest_pts);
+
+   //: Set parameters from vectors
+  void set_params(const vnl_vector<double>& W1,
+	                const vnl_vector<double>& W2);
+
+  void set_up_rhs(vnl_vector<double>& Bx,
+	                vnl_vector<double>& By,
+									const vcl_vector<vgl_point_2d<double> >& dest_pts);
+
+   //: Compute spline bending energy
+  void compute_energy(vnl_vector<double>& W1,
+	                vnl_vector<double>& W2,
+									const vnl_matrix<double>& L);
+
+public:
+
+		//: Dflt ctor
+	mbl_thin_plate_spline_2d();
+
+		//: Destructor
+	virtual ~mbl_thin_plate_spline_2d();
+
+		//: Sets up internal transformation to map source_pts onto dest_pts
+	void build(const vcl_vector<vgl_point_2d<double> >& source_pts,
+				const vcl_vector<vgl_point_2d<double> >& dest_pts,
+									 bool compute_the_energy=false);
+
+		//: Define source point positions
+		//  Performs precomputations so that build(dest_points) can be
+		//  called multiple times efficiently
+	void set_source_pts(const vcl_vector<vgl_point_2d<double> >& source_pts);
+
+		//: Sets up internal transformation to map source_pts onto dest_pts
+	void build(const vcl_vector<vgl_point_2d<double> >& dest_pts);
+
+       //: Return transformed version of (x,y)
+	vgl_point_2d<double>  operator()(double x, double y) const;
+
+       //: Return transformed version of (x,y)
+	vgl_point_2d<double>  operator()(const vgl_point_2d<double>&  p) const
+	{ return operator()(p.x(),p.y()); }
+
+		//: Bending energy of X component (zero for pure affine)
+		//  A measure of total amount of non-linear deformation
+	double bendingEnergyX() const { return energy_x_; }
+
+		//: Bending energy of X component (zero for pure affine)
+		//  A measure of total amount of non-linear deformation
+	double bendingEnergyY() const { return energy_y_; }
+
+		//: Version number for I/O
+	short version_no() const;
+
+		//: Print class to os
+  void print_summary(vcl_ostream& os) const;
+
+		//: Save class to binary file stream
+  void b_write(vsl_b_ostream& bfs) const;
+
+		//: Load class from binary file stream
+  void b_read(vsl_b_istream& bfs);
+
+	  //: Comparison operator
+  bool operator==(const mbl_thin_plate_spline_2d& tps) const;
+};
+
+	//: Binary file stream output operator for class reference
+void vsl_b_write(vsl_b_ostream& bfs, const mbl_thin_plate_spline_2d& b);
+
+	//: Binary file stream input operator for class reference
+void vsl_b_read(vsl_b_istream& bfs, mbl_thin_plate_spline_2d& b);
+
+	//: Stream output operator for class reference
+vcl_ostream& operator<<(vcl_ostream& os,const mbl_thin_plate_spline_2d& b);
+
+#endif
+
+
---
+++ contrib/mul/mbl/tests/test_thin_plate_spline_2d.cxx
@@ -0,0 +1,65 @@
+#include <vcl_iostream.h>
+#include <vcl_fstream.h>
+#include <vcl_utility.h>
+#include <vcl_cmath.h>
+#include <vnl/vnl_test.h>
+#include <mbl/mbl_thin_plate_spline_2d.h>
+#include <mbl/mbl_mz_random.h>
+#include <vsl/vsl_vector_io.h>
+#include <vgl/vgl_distance.h>
+
+void test_tps_at_fixed_points(int n_points)
+{
+  vcl_cout<<"Testing with "<<n_points<<vcl_endl;
+  vcl_vector<vgl_point_2d<double> > pts1(n_points),pts2(n_points);
+
+  mbl_mz_random mz_random;
+	mz_random.reseed(1647+2*n_points);
+
+	for (int i=0;i<n_points;++i)
+	{
+	  pts1[i]=vgl_point_2d<double>(mz_random.drand64(),mz_random.drand64());
+	  pts2[i]=vgl_point_2d<double>(mz_random.drand64(),mz_random.drand64());
+	}
+	vsl_print_summary(vcl_cout,pts1);
+	vsl_print_summary(vcl_cout,pts2);
+
+  mbl_thin_plate_spline_2d tps;
+  tps.build(pts1,pts2);
+	vcl_cout<<tps<<vcl_endl;
+
+	for (int i=0;i<n_points;++i)
+	{
+	  vcl_cout<<"Test point "<<i<<" : ";
+	  TEST("Warped point = target point",vgl_distance(tps(pts1[i]),pts2[i])<1e-6,true);
+	}
+
+  mbl_thin_plate_spline_2d tps2 = tps;
+	TEST("Comparison operator",tps==tps2,true);
+
+	vcl_cout<<"Testing set_source_pts(),build() pair"<<vcl_endl;
+  mbl_thin_plate_spline_2d tps3;
+	tps3.set_source_pts(pts1);
+	tps3.build(pts2);
+	for (int i=0;i<n_points;++i)
+	{
+	  vcl_cout<<"Test point "<<i<<" : ";
+	  TEST("Warped point = target point",vgl_distance(tps(pts1[i]),pts2[i])<1e-6,true);
+	}
+
+
+}
+
+void test_thin_plate_spline_2d()
+{
+  vcl_cout << "******************************" << vcl_endl;
+  vcl_cout << " Testing mbl_thin_plate_spline_2d " << vcl_endl;
+  vcl_cout << "*****************************" << vcl_endl;
+
+  mbl_thin_plate_spline_2d tps;
+
+	for (int i=1;i<=5;++i)
+    test_tps_at_fixed_points(i);
+ }
+
+TESTMAIN(test_thin_plate_spline_2d);
