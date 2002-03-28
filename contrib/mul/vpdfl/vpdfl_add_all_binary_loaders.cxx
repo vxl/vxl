8b560530404c2ce3eaebc7729bc90d70b315b0a9
Tim Cootes <timcootes@users.sourceforge.net>
2002-03-28 09:29:16 -0500

    Added utility function

---
+++ contrib/mul/vpdfl/vpdfl_add_all_binary_loaders.cxx
@@ -0,0 +1,20 @@
+#include <vpdfl/vpdfl_axis_gaussian.h>
+#include <vpdfl/vpdfl_axis_gaussian_builder.h>
+#include <vpdfl/vpdfl_gaussian.h>
+#include <vpdfl/vpdfl_gaussian_builder.h>
+#include <vpdfl/vpdfl_mixture.h>
+#include <vpdfl/vpdfl_mixture_builder.h>
+#include <vpdfl/vpdfl_pc_gaussian.h>
+#include <vpdfl/vpdfl_pc_gaussian_builder.h>
+
+void vpdfl_add_all_binary_loaders()
+{
+  vsl_add_to_binary_loader(vpdfl_axis_gaussian());
+  vsl_add_to_binary_loader(vpdfl_axis_gaussian_builder());
+  vsl_add_to_binary_loader(vpdfl_gaussian());
+  vsl_add_to_binary_loader(vpdfl_gaussian_builder());
+  vsl_add_to_binary_loader(vpdfl_mixture());
+  vsl_add_to_binary_loader(vpdfl_mixture_builder());
+  vsl_add_to_binary_loader(vpdfl_pc_gaussian());
+  vsl_add_to_binary_loader(vpdfl_pc_gaussian_builder());
+}
---
+++ contrib/mul/vpdfl/vpdfl_add_all_binary_loaders.h
@@ -0,0 +1,8 @@
+#ifndef vpdfl_add_all_binary_loaders_h_
+#define vpdfl_add_all_binary_loaders_h_
+
+//: Add all available derived classes to binary loaders
+//  Allows loading objects by base class pointer
+void vpdfl_add_all_binary_loaders();
+
+#endif
