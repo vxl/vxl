3e2a4482d151d3a44525d81b5ae6f79810071249
Tim Cootes <timcootes@users.sourceforge.net>
2002-03-28 09:30:19 -0500

    Added utility function

--- contrib/mul/vpdfl/vpdfl_calc_mean_var.cxx
+++ contrib/mul/vpdfl/vpdfl_calc_mean_var.cxx
@@ -4,6 +4,7 @@
 // \brief Calculate mean and variance of multivariate data.
 
 #include <vpdfl/vpdfl_calc_mean_var.h>
+#include <mbl/mbl_data_array_wrapper.h>
 
 //: Compute mean and variance of data
 void vpdfl_calc_mean_var(vnl_vector<double>& mean,
@@ -30,6 +31,55 @@ void vpdfl_calc_mean_var(vnl_vector<double>& mean,
       mean_data[j] += v[j];
       sum_sq_data[j] += v[j]*v[j];
     }
+  }
+
+  mean/=n;
+
+  var.resize(n_dims);
+  for (int j=0;j<n_dims;j++)
+  {
+    var_data[j] = sum_sq_data[j]/n - mean_data[j]*mean_data[j];
+  }
+}
+
+//: Compute mean and variance of data
+void vpdfl_calc_mean_var(vnl_vector<double>& mean,
+                         vnl_vector<double>& var,
+                         mbl_data_wrapper<vnl_vector<double> >& data)
+{
+  if (data.is_a()=="mbl_data_array_wrapper<T>")
+	{
+	  // Use more efficient algorithm
+    mbl_data_array_wrapper<vnl_vector<double> > array_data
+		             = (mbl_data_array_wrapper<vnl_vector<double> >&) data;
+    vpdfl_calc_mean_var(mean,var,array_data.data(),array_data.size());
+		return;
+  }
+
+	int n = data.size();
+  if (n==0) return;
+	data.reset();
+  int n_dims = data.current().size();
+  vnl_vector<double> sum_sq(n_dims);
+  mean.resize(n_dims);
+  var.resize(n_dims);
+
+  double* var_data = var.data_block();
+  double* mean_data = mean.data_block();
+  double* sum_sq_data = sum_sq.data_block();
+  mean.fill(0.0);
+  sum_sq.fill(0.0);
+
+  for (int i=0;i<n;i++)
+  {
+    const double *v = data.current().data_block();
+    for (int j=0;j<n_dims;j++)
+    {
+      mean_data[j] += v[j];
+      sum_sq_data[j] += v[j]*v[j];
+    }
+
+		data.next();
   }
 
   mean/=n;
--- contrib/mul/vpdfl/vpdfl_calc_mean_var.h
+++ contrib/mul/vpdfl/vpdfl_calc_mean_var.h
@@ -6,12 +6,17 @@
 // \brief Calculate mean and variance of multivariate data.
 
 #include <vnl/vnl_vector.h>
+#include <mbl/mbl_data_wrapper.h>
 
 //: Compute mean and variance of data
 void vpdfl_calc_mean_var(vnl_vector<double>& mean,
                          vnl_vector<double>& var,
                          const vnl_vector<double>* data, int n);
 
+//: Compute mean and variance of data
+void vpdfl_calc_mean_var(vnl_vector<double>& mean,
+                         vnl_vector<double>& var,
+                         mbl_data_wrapper<vnl_vector<double> >& data);
 
 #endif // vpdfl_calc_mean_var_h
 
