e894206c7bf7f8b09f64d18a2263f7210e451cc5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-04-03 07:11:41 -0500

    \file added (for Doxygen)

--- contrib/mul/vpdfl/vpdfl_add_all_binary_loaders.h
+++ contrib/mul/vpdfl/vpdfl_add_all_binary_loaders.h
@@ -1,5 +1,7 @@
 #ifndef vpdfl_add_all_binary_loaders_h_
 #define vpdfl_add_all_binary_loaders_h_
+//:
+// \file
 
 //: Add all available derived classes to binary loaders
 //  Allows loading objects by base class pointer
