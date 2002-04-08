c8769c606e24a2e669d38ea9935578fabf83ca29
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-04-08 06:09:19 -0400

    explicit cast from double to float, to avoid compiler warning

--- core/vgl/vgl_homg.cxx
+++ core/vgl/vgl_homg.cxx
@@ -8,8 +8,8 @@
 # include <math.h> // dont_vxl_filter: no HUGE_VAL in <cmath>
 #endif
 
-float vgl_homg<float>::infinity = HUGE_VAL;
-float vgl_homg<float>::infinitesimal_tol = 1e-12;
+float vgl_homg<float>::infinity = (float)HUGE_VAL;
+float vgl_homg<float>::infinitesimal_tol = 1e-12f;
 
 double vgl_homg<double>::infinity = HUGE_VAL;
 double vgl_homg<double>::infinitesimal_tol = 1e-12;
