ac4fb4af6b2dd7fa9dc1cb80b39830324593db61
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-04-10 12:10:24 -0400

    distance_curve_to_point() implementation moved to .cxx file

--- contrib/gel/vdgl/vdgl_interpolator.cxx
+++ contrib/gel/vdgl/vdgl_interpolator.cxx
@@ -4,3 +4,11 @@
 
 #include "vdgl_interpolator.h"
 
+double vdgl_interpolator::distance_curve_to_point ( vsol_point_2d_sptr p )
+{
+  vsol_point_2d_sptr curve_point = this->closest_point_on_curve ( p );
+  if ( curve_point.ptr() == NULL )
+    return 1e11;
+
+  return vgl_distance ( p->get_p(), curve_point->get_p() );
+}
