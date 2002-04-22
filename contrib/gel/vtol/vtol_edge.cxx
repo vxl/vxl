6d37086b06fffd464006dc7bda5734f68c06f371
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-04-22 05:01:49 -0400

    using cast_to_zero_chain() instead of C-style casting

--- contrib/gel/vtol/vtol_edge.cxx
+++ contrib/gel/vtol/vtol_edge.cxx
@@ -19,8 +19,8 @@
 //---------------------------------------------------------------------------
 vtol_edge::vtol_edge(void)
 {
-  _v1=0;
-  _v2=0;
+  v1_=0;
+  v2_=0;
   vtol_zero_chain_sptr c = new vtol_zero_chain();
   // vtol_zero_chain c;
   // link_inferior ( ( (vtol_topology_object*) new vtol_zero_chain) );
@@ -29,11 +29,15 @@ vtol_edge::vtol_edge(void)
 
 
 //---------------------------------------------------------------------------
-//: Return the first zero-chain of `this'
+//: Return the first non-empty zero-chain of `this'
 //---------------------------------------------------------------------------
 vtol_zero_chain_sptr vtol_edge::zero_chain(void) const
 {
-  return (vtol_zero_chain *)(_inferiors[0].ptr());
+  vcl_vector<vtol_topology_object_sptr> const& inf = *(inferiors());
+  for (unsigned int i=0; i<inf.size(); ++i)
+    if (inf[i]->cast_to_zero_chain()->v0()) // PVr- added this condition so no empty chain is returned
+      return inf[i]->cast_to_zero_chain();
+  return 0;
 }
 
 
@@ -48,37 +52,12 @@ vtol_edge::~vtol_edge()
 //---------------------------------------------------------------------------
 //: Set the first endpoint.
 //---------------------------------------------------------------------------
-void vtol_edge::set_v1(vtol_vertex *new_v1)
-{
-  if (_v1&&_v1!=_v2)
-    zero_chain()->unlink_inferior(*_v1);
-  _v1=new_v1;
-  zero_chain()->link_inferior(*_v1);
-  touch();
-}
-
-
-//---------------------------------------------------------------------------
-//: Set the first endpoint.
-//---------------------------------------------------------------------------
 void vtol_edge::set_v1(vtol_vertex_sptr new_v1)
 {
-  if (_v1&&_v1!=_v2)
-    zero_chain()->unlink_inferior(*_v1);
-  _v1=new_v1;
-  zero_chain()->link_inferior(*_v1);
-  touch();
-}
-
-//---------------------------------------------------------------------------
-//: Set the last endpoint.
-//---------------------------------------------------------------------------
-void vtol_edge::set_v2(vtol_vertex *new_v2)
-{
-  if (_v2&&_v2!=_v1)
-    zero_chain()->unlink_inferior(*_v2);
-  _v2=new_v2;
-  zero_chain()->link_inferior(*_v2);
+  if (v1_&&v1_!=v2_)
+    zero_chain()->unlink_inferior(*v1_);
+  v1_=new_v1;
+  zero_chain()->link_inferior(*v1_);
   touch();
 }
 
@@ -87,10 +66,10 @@ void vtol_edge::set_v2(vtol_vertex *new_v2)
 //---------------------------------------------------------------------------
 void vtol_edge::set_v2(vtol_vertex_sptr new_v2)
 {
-  if (_v2&&_v2!=_v1)
-    zero_chain()->unlink_inferior(*_v2);
-  _v2=new_v2;
-  zero_chain()->link_inferior(*_v2);
+  if (v2_&&v2_!=v1_)
+    zero_chain()->unlink_inferior(*v2_);
+  v2_=new_v2;
+  zero_chain()->link_inferior(*v2_);
   touch();
 }
 
@@ -103,25 +82,25 @@ void vtol_edge::set_v2(vtol_vertex_sptr new_v2)
 //:
 //     This function removes curendpt from the edge and replaces it with
 //     newendpt.  Both curendpt and newendpt must be non-NULL pointers,
-//     curendpt must point to an endpoint of the edge (either _v1 or
-//     _v2), and newendpt must be colinear with the edge.  Neither vertex
+//     curendpt must point to an endpoint of the edge (either v1_ or
+//     v2_), and newendpt must be colinear with the edge.  Neither vertex
 //     is destroyed in the process; only links are manipulated.
 //     (RYF 7-14-98)
 //
 void vtol_edge::replace_end_point(vtol_vertex &curendpt,
-                                     vtol_vertex &newendpt)
+                                  vtol_vertex &newendpt)
 {
   // require
   //  assert(curendpt);
   //  assert(newendpt);
-  assert(curendpt==*_v1||curendpt==*_v2);
+  assert(curendpt==*v1_||curendpt==*v2_);
 
   zero_chain()->unlink_inferior(curendpt);
   zero_chain()->link_inferior(newendpt);
-  if(curendpt==*_v1)  // update the appropriate endpoint
-    _v1=&newendpt;
+  if (curendpt==*v1_)  // update the appropriate endpoint
+    v1_=&newendpt;
   else // curendpt == this->get_v2()
-    _v2=&newendpt;
+    v2_=&newendpt;
 }
 
 //---------------------------------------------------------------------------
@@ -129,38 +108,34 @@ void vtol_edge::replace_end_point(vtol_vertex &curendpt,
 //---------------------------------------------------------------------------
 void vtol_edge::set_vertices_from_zero_chains(void)
 {
-  vtol_topology_object_sptr zc0;
-  int numverts;
-
-  if(numinf()==1)
-    // This is the normal case.
+  if (numinf()==1) // This is the normal case.
     {
-      // Set _v1 to be the first on the
-      // vtol_zero_chain. Set _v2 to be the last on
+      // Set v1_ to be the first on the
+      // vtol_zero_chain. Set v2_ to be the last on
       // the vtol_zero_chain.
-      zc0=*(inferiors()->begin());
-      numverts=zc0->numinf();
+      vtol_topology_object_sptr zc0=inferiors()->front();
+      int numverts=zc0->numinf();
       //topology_list *verts=zc0->inferiors();
       switch(numverts)
         {
         case 0:
-          _v1=0;
-          _v2=0;
+          v1_=0;
+          v2_=0;
           break;
         case 1:
-          _v1=(vtol_vertex *)((*(zc0->inferiors()))[0].ptr());
-          _v2=_v1;
+          v1_=zc0->inferiors()->front()->cast_to_vertex();
+          v2_=v1_;
           break;
         default:
-          _v1=(vtol_vertex *)((*(zc0->inferiors()))[0].ptr());
-          _v2=(vtol_vertex *)((*(zc0->inferiors()))[(numverts-1)].ptr());
+          vcl_vector<vtol_topology_object_sptr> const * v = zc0->inferiors();
+          v1_=v->front()->cast_to_vertex();
+          v2_=v->back()->cast_to_vertex();
           break;
         }
     }
-  else
-    // This is a Strange case but we`ll take care of it.
+  else // This is a Strange case but we'll take care of it.
     // Assuming zero_chains have an ordering,
-    // Set _v1 to the first vertex and Set _v2
+    // Set v1_ to the first vertex and Set v2_
     // to be the last vertex.  They will not
     // be on the same vtol_zero_chain.
     {
@@ -172,12 +147,12 @@ void vtol_edge::set_vertices_from_zero_chains(void)
           switch(numverts)
             {
             case 0:
-              _v1=0;
-              _v2=0;
+              v1_=0;
+              v2_=0;
               break;
             default:
-              _v1=(*verts)[0];
-              _v2=(*verts)[numverts-1];
+              v1_=(*verts)[0];
+              v2_=(*verts)[numverts-1];
             }
         }
       delete verts;
@@ -203,26 +178,6 @@ void vtol_edge::add_edge_loop(vtol_one_chain &new_edge_loop)
 void vtol_edge::remove_edge_loop(vtol_one_chain &doomed_edge_loop)
 {
   doomed_edge_loop.unlink_inferior(*this);
-}
-
-//***************************************************************************
-// Replaces dynamic_cast<T>
-//***************************************************************************
-
-//---------------------------------------------------------------------------
-//: Return `this' if `this' is an edge, 0 otherwise
-//---------------------------------------------------------------------------
-const vtol_edge * vtol_edge::cast_to_edge(void) const
-{
-  return this;
-}
-
-//---------------------------------------------------------------------------
-//: Return `this' if `this' is an edge, 0 otherwise
-//---------------------------------------------------------------------------
-vtol_edge * vtol_edge::cast_to_edge(void)
-{
-  return this;
 }
 
 //***************************************************************************
@@ -259,19 +214,10 @@ bool vtol_edge::operator==(const vtol_edge &other) const
   if (this==&other) return true;
 
 
-  if(!compare_geometry(other))
+  if (!compare_geometry(other))
     return false;
 
-#if 0
-  if ( (_curve && !other._curve) ||
-  (!_curve && other._curve) )
-    return false;
-
-  if (_curve && (*_curve)!=(*other._curve))
-    return false;
-#endif
-
-  if (!(*_v1==*(other._v1)) || !(*_v2==*(other._v2))) // ((*_v1!=*(other._v1)) || (*_v2!=*(other._v2)))
+  if (!(*v1_==*(other.v1_)) || !(*v2_==*(other.v2_))) // ((*v1_!=*(other.v1_)) || (*v2_!=*(other.v2_)))
     return false;
 
   vtol_zero_chain_sptr zc1=zero_chain();
@@ -341,14 +287,14 @@ vcl_vector<vtol_block *> *vtol_edge::compute_blocks(void)
 }
 
 //: Returns a list of vertices containing the endpoints of the edge.
-// These vertices are _v1 and _v2 in that order.
+// These vertices are v1_ and v2_ in that order.
 vertex_list *vtol_edge::endpoints(void)
 {
   vertex_list *newl=new vcl_vector<vtol_vertex_sptr>();
-  if(_v1)
-    newl->push_back(_v1);
-  if(_v2)
-    newl->push_back(_v2);
+  if (v1_)
+    newl->push_back(v1_);
+  if (v2_)
+    newl->push_back(v2_);
   return newl;
 }
 
@@ -365,9 +311,9 @@ bool vtol_edge::share_vertex_with(vtol_edge &other)
 
   vcl_vector<vtol_vertex_sptr>::iterator i1;
   vcl_vector<vtol_vertex_sptr>::iterator i2;
-  for(i1=thisedges->begin();i1!=thisedges->end();++i1 )
-    for(i2=eedges->begin();i2!=eedges->end(); ++i2 )
-      if((*i1)==(*i2))
+  for (i1=thisedges->begin();i1!=thisedges->end();++i1 )
+    for (i2=eedges->begin();i2!=eedges->end(); ++i2 )
+      if ((*i1)==(*i2))
         return true;
   return false;
 }
@@ -382,10 +328,10 @@ bool vtol_edge::add_vertex(vtol_vertex &newvert)
 
   zc=zero_chain();
   if (!zc)
-    {
-      zc=new vtol_zero_chain;
-      link_inferior(*zc);
-    }
+  {
+    zc=new vtol_zero_chain;
+    link_inferior(*zc);
+  }
 
   zc->link_inferior(newvert);
   return true;
@@ -397,9 +343,9 @@ bool vtol_edge::add_vertex(vtol_vertex &newvert)
 
 bool vtol_edge::remove_vertex(vtol_vertex &uglyvert)
 {
-  if(uglyvert==*_v1)
+  if (uglyvert==*v1_)
     set_v1(0);
-  else if(uglyvert==*_v2)
+  else if (uglyvert==*v2_)
     set_v2(0);
   else
     return false;
@@ -413,58 +359,53 @@ bool vtol_edge::is_endpoint(const vtol_vertex &v) const
 }
 
 
-//: Returns True if v is equal to the first vtol_edge endpoint,_v1.
+//: Returns True if v is equal to the first vtol_edge endpoint,v1_.
 bool vtol_edge::is_endpoint1(const vtol_vertex &v) const
 {
-  return *_v1==v;
+  return *v1_==v;
 }
 
-//: Returns True if v is equal to the second vtol_edge endpoint, _v2.
+//: Returns True if v is equal to the second vtol_edge endpoint, v2_.
 bool vtol_edge::is_endpoint2(const vtol_vertex &v) const
 {
-  return *_v2 ==v;
+  return *v2_ ==v;
 }
 
 
 //: This method works only for ImplicitLine edges.
 vtol_vertex_sptr vtol_edge::other_endpoint(const vtol_vertex &overt) const
 {
-  if (overt==*_v1)
-    return _v2;
-  else if (overt==*_v2)
-    return _v1;
+  if (overt==*v1_)
+    return v2_;
+  else if (overt==*v2_)
+    return v1_;
   else
     return 0;
 }
 
 //-----------------------------------------------------------------
-//: Compute bounds from the geometry of _curve.
+//: Compute bounds from the geometry of curve().
 // If the curve is not fully bounded, then use the vertices.
 //
 void vtol_edge::compute_bounding_box(void)
 {
-  // TODO
-#if 0
-  if(_curve && _curve->GetGeometryType() != GeometryObject::IMPLICITLINE)
-     {
-      // Get bounds from curve.
-      // But are curve endpoints consistent with vertices? -JLM
-      // Anyway, this is what was done in get_min_max
-       if(_curve->GetExtent() == vsol_curve::FULLY_BOUNDED)
-         {
-           this->set_min_x(_curve->get_min_x());
-           this->set_min_y(_curve->get_min_y());
-           // this->set_min_z(_curve->get_min_z());
-
-           this->set_max_x(_curve->get_max_x());
-           this->set_max_y(_curve->get_max_y());
-           //  this->set_max_z(_curve->get_max_z());
-         }
-     }
-
+#if 0 // TODO
+  if (curve() && curve()->GetGeometryType() != GeometryObject::IMPLICITLINE)
+  {
+    // Get bounds from curve.
+    // But are curve endpoints consistent with vertices? -JLM
+    // Anyway, this is what was done in get_min_max
+    if (curve()->GetExtent() == vsol_curve::FULLY_BOUNDED)
+    {
+      this->set_min_x(curve()->get_min_x());
+      this->set_min_y(curve()->get_min_y());
+      this->set_max_x(curve()->get_max_x());
+      this->set_max_y(curve()->get_max_y());
+    }
+  }
   else  // Just use the generic method computing bounds from vertices
-    this->vtol_topology_object::compute_bounding_box();
 #endif
+    this->vtol_topology_object::compute_bounding_box();
 }
 
 
@@ -482,17 +423,12 @@ void vtol_edge::describe(vcl_ostream &strm,
   for (int i1=0; i1<blanking; ++i1) strm << ' ';
   print(strm);
   for (int i2=0; i2<blanking; ++i2) strm << ' ';
-  if(_v1) {
-    _v1->print(strm);
-  } else {
-    strm << "Null vertex 1" << vcl_endl;
-  }
+  if (v1_) v1_->print(strm);
+  else     strm << "Null vertex 1" << vcl_endl;
+
   for (int i3=0; i3<blanking; ++i3) strm << ' ';
-  if(_v2) {
-    _v2->print(strm);
-  } else {
-    strm << "Null vertex 2" << vcl_endl;
-  }
+  if (v2_) v2_->print(strm);
+  else     strm << "Null vertex 2" << vcl_endl;
 }
 
 //:
