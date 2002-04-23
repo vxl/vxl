0afd8741db6d5b503c19de0eddc19e3ed9987a27
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-04-23 16:49:51 -0400

    "const" added in cast

--- core/vil1/file_formats/vil1_ras.cxx
+++ core/vil1/file_formats/vil1_ras.cxx
@@ -41,7 +41,7 @@ namespace {
   // Equivalent of ntoh
   bool read_uint_32( vil_stream* vs, vxl_uint_32& word )
   {
-    if( vs->read( &word, 4 ) < 4 )  return false;
+    if ( vs->read( &word, 4 ) < 4 )  return false;
     swap_endian( word );
     return true;
   }
@@ -80,8 +80,8 @@ vil_image_impl* vil_ras_file_format::make_input_image(vil_stream* vs)
   // Check the magic number
   vxl_uint_8 buf[4] = { 0, 0, 0, 0 };
   vs->read(buf, 4);
-  if( ! ( buf[0] == RAS_MAGIC[0] && buf[1] == RAS_MAGIC[1] &&
-          buf[2] == RAS_MAGIC[2] && buf[3] == RAS_MAGIC[3]  ) )
+  if ( ! ( buf[0] == RAS_MAGIC[0] && buf[1] == RAS_MAGIC[1] &&
+           buf[2] == RAS_MAGIC[2] && buf[3] == RAS_MAGIC[3]  ) )
     return 0;
 
   return new vil_ras_generic_image(vs);
@@ -122,7 +122,7 @@ bool vil_ras_generic_image::get_property(char const *tag, void *prop) const
   // The default raw colour format is BGR. The default indexed colour
   // format is RGB. Go figure.
   if (0==vcl_strcmp(tag, vil_property_component_order_is_BGR)) {
-    if( prop ) (*(bool*)prop) = ( map_type_ == RMT_NONE && type_ != RT_FORMAT_RGB );
+    if ( prop ) (*(bool*)prop) = ( map_type_ == RMT_NONE && type_ != RT_FORMAT_RGB );
     return true;
   }
 
@@ -146,15 +146,15 @@ vil_ras_generic_image::vil_ras_generic_image(vil_stream* vs, int planes,
   width_ = width;
   height_ = height;
 
-  if( planes != 1 ) {
+  if ( planes != 1 ) {
     vcl_cerr << __FILE__ << ": can only handle 1 plane" << vcl_endl;
     return;
   }
-  if( components != 3 && components != 1 ) {
+  if ( components != 3 && components != 1 ) {
     vcl_cerr << __FILE__ << ": can't handle " << components << " components" << vcl_endl;
     return;
   }
-  if( bits_per_component != 8 ) {
+  if ( bits_per_component != 8 ) {
     vcl_cerr << __FILE__ << ": can't handle " << bits_per_component << " bits per component" << vcl_endl;
     return;
   }
@@ -183,7 +183,6 @@ vil_ras_generic_image::~vil_ras_generic_image()
 }
 
 
-
 //: Read the header of a Sun raster file.
 bool vil_ras_generic_image::read_header()
 {
@@ -192,11 +191,11 @@ bool vil_ras_generic_image::read_header()
 
   vxl_uint_8 buf[4];
   if (vs_->read(buf, 4) < 4) return false; // at end-of-file?
-  if( ! ( buf[0] == RAS_MAGIC[0] && buf[1] == RAS_MAGIC[1] &&
+  if (! ( buf[0] == RAS_MAGIC[0] && buf[1] == RAS_MAGIC[1] &&
           buf[2] == RAS_MAGIC[2] && buf[3] == RAS_MAGIC[3]  ) )
     return false; // magic number isn't correct
 
-  if( !( read_uint_32( vs_, width_ ) &&
+  if (!( read_uint_32( vs_, width_ ) &&
          read_uint_32( vs_, height_ ) &&
          read_uint_32( vs_, depth_ ) &&
          read_uint_32( vs_, length_ ) &&
@@ -207,40 +206,40 @@ bool vil_ras_generic_image::read_header()
 
   // Do consistency checks of the header
 
-  if( type_ != RT_OLD && type_ != RT_STANDARD &&
+  if (type_ != RT_OLD && type_ != RT_STANDARD &&
       type_ != RT_BYTE_ENCODED && type_ != RT_FORMAT_RGB ) {
     vcl_cerr << __FILE__ << ": unknown type " << type_ << vcl_endl;
     return false;
   }
-  if( map_type_ != RMT_NONE && map_type_ != RMT_EQUAL_RGB ) {
+  if ( map_type_ != RMT_NONE && map_type_ != RMT_EQUAL_RGB ) {
     vcl_cerr << __FILE__ << ": unknown map type " << map_type_ << vcl_endl;
     return false;
   }
-  if( map_type_ == RMT_NONE && map_length_ != 0 ) {
+  if ( map_type_ == RMT_NONE && map_length_ != 0 ) {
     vcl_cerr << __FILE__ << ": No colour map according to header, but there is a map!" << vcl_endl;
     return false;
   }
 
-  if( depth_ != 8 && depth_ != 24 ) {
+  if ( depth_ != 8 && depth_ != 24 ) {
     vcl_cerr << __FILE__ << ": depth " << depth_ << " not implemented" << vcl_endl;
     return false;
   }
 
   // The "old" format always has length set to zero, so we should compute it ourselves.
-  if( type_ == RT_OLD ) {
+  if ( type_ == RT_OLD ) {
     length_ = compute_length( width_, height_, depth_ );
   }
-  if( length_ == 0 ) {
+  if ( length_ == 0 ) {
     vcl_cerr << __FILE__ << ": header says image has length zero" << vcl_endl;
     return false;
   }
-  if( type_ != RT_BYTE_ENCODED && length_ != compute_length( width_, height_, depth_ ) ) {
+  if ( type_ != RT_BYTE_ENCODED && length_ != compute_length( width_, height_, depth_ ) ) {
     vcl_cerr << __FILE__ << ": length " << length_ << " does not match wxhxd = "
              << compute_length( width_, height_, depth_ ) << vcl_endl;
     return false;
   }
 
-  if( map_length_ ) {
+  if ( map_length_ ) {
     assert( map_length_ % 3 == 0 );
     col_map_ = new vxl_uint_8[ map_length_ ];
     vs_->read( col_map_, map_length_ );
@@ -252,7 +251,7 @@ bool vil_ras_generic_image::read_header()
 
   assert( depth_ == 8 || depth_ == 24 );
 
-  if( map_type_ != RMT_NONE || depth_ == 24 ) {
+  if ( map_type_ != RMT_NONE || depth_ == 24 ) {
     components_ = 3;
   } else {
     components_ = 1;
@@ -285,7 +284,7 @@ bool vil_ras_generic_image::write_header()
 
 bool vil_ras_generic_image::get_section(void* buf, int x0, int y0, int xs, int ys) const
 {
-  if( type_ == RT_BYTE_ENCODED )
+  if ( type_ == RT_BYTE_ENCODED )
     return false; // not yet implemented
 
   int file_bytes_per_pixel = (depth_+7)/8;
@@ -297,10 +296,10 @@ bool vil_ras_generic_image::get_section(void* buf, int x0, int y0, int xs, int y
 
   vxl_uint_8* ib = (vxl_uint_8*) buf;
 
-  if( !col_map_ ) {
+  if ( !col_map_ ) {
     // No colourmap, so just read in the bytes. This could be RGB or
     // BGR, depending on type_.
-    for(int y = 0; y < ys; ++y) {
+    for (int y = 0; y < ys; ++y) {
       vs_->seek(file_byte_start + y * file_byte_width);
       vs_->read(ib + y * buff_byte_width, buff_byte_width);
     }
@@ -310,12 +309,12 @@ bool vil_ras_generic_image::get_section(void* buf, int x0, int y0, int xs, int y
     int col_len = map_length_ / 3;
     // Read a line, and map every index into an RGB triple
     vxl_uint_8* line = new vxl_uint_8[ xs ];
-    for(int y = 0; y < ys; ++y) {
+    for (int y = 0; y < ys; ++y) {
       vs_->seek( file_byte_start + y * file_byte_width );
       vs_->read( line, xs );
       vxl_uint_8* in_p = line;
       vxl_uint_8* out_p = ib + y * buff_byte_width;
-      for( int i=0; i < xs; ++i ) {
+      for ( int i=0; i < xs; ++i ) {
         assert( *in_p < col_len );
         *(out_p++) = col_map_[ *in_p ];
         *(out_p++) = col_map_[ *in_p + col_len ];
@@ -324,22 +323,22 @@ bool vil_ras_generic_image::get_section(void* buf, int x0, int y0, int xs, int y
       }
     }
     delete[] line;
-  }    
+  }
 
   return true;
 }
 
 bool vil_ras_generic_image::put_section(void const* buf, int x0, int y0, int xs, int ys)
 {
-  if( col_map_ ) {
+  if ( col_map_ ) {
     vcl_cerr << __FILE__ << ": writing to file with a colour map is not implemented" << vcl_endl;
     return false;
   }
-  if( type_ == RT_BYTE_ENCODED ) {
+  if ( type_ == RT_BYTE_ENCODED ) {
     vcl_cerr << __FILE__ << ": writing to a run-length encoded file is not implemented" << vcl_endl;
     return false;
   }
-  if( components_ == 3 && type_ != RT_FORMAT_RGB ) {
+  if ( components_ == 3 && type_ != RT_FORMAT_RGB ) {
     vcl_cerr << __FILE__ << ": writing BGR format is not implemented" << vcl_endl;
     return false;
   }
@@ -358,12 +357,12 @@ bool vil_ras_generic_image::put_section(void const* buf, int x0, int y0, int xs,
   bool need_pad = ( file_byte_width != buff_byte_width );
   vxl_uint_8 zero = 0;
 
-  vxl_uint_8* ob = (vxl_uint_8*) buf;
+  vxl_uint_8 const* ob = (vxl_uint_8 const*) buf;
 
-  for(int y = 0; y < ys; ++y) {
+  for (int y = 0; y < ys; ++y) {
     vs_->seek( file_byte_start + y * file_byte_width );
     vs_->write( ob + y * buff_byte_width, buff_byte_width );
-    if( need_pad ) vs_->write( &zero, 1 );
+    if ( need_pad ) vs_->write( &zero, 1 );
   }
 
   return true;
