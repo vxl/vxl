26070e6430ebddae6da51343ae7135f6b4317036
Amitha Perera <amithaperera@users.sourceforge.net>
2002-04-25 05:55:14 -0400

    I forgot this one too. Sigh.

---
+++ core/vil1/io/tests/test_include.cxx
@@ -0,0 +1,13 @@
+#include <vil/io/vil_io_image.h>
+#include <vil/io/vil_io_image_impl.h>
+#include <vil/io/vil_io_memory_image.h>
+#include <vil/io/vil_io_memory_image_format.h>
+#include <vil/io/vil_io_memory_image_impl.h>
+#include <vil/io/vil_io_memory_image_of.h>
+#include <vil/io/vil_io_rgb.h>
+#include <vil/io/vil_io_rgba.h>
+
+int test_include_main( int, char*[] )
+{
+  return 0;
+}
