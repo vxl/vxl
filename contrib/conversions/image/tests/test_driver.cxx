0c5ec00af758ab0023c36c5392d259efc0985c54
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-04-26 04:28:53 -0400

    also register test_image_conversions

--- contrib/conversions/image/tests/test_driver.cxx
+++ contrib/conversions/image/tests/test_driver.cxx
@@ -1,9 +1,11 @@
 #include <testlib/testlib_register.h>
 
 DECLARE( test_vil_vbl_conversions );
+DECLARE( test_image_conversions );
 
 void
 register_tests()
 {
   REGISTER( test_vil_vbl_conversions );
+  REGISTER( test_image_conversions );
 }
