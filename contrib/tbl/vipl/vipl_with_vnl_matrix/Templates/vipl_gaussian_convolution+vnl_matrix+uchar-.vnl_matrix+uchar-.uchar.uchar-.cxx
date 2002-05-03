f91697f78c6c82f1cef55207de38ef8ba0a6b79e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-05-03 15:34:23 -0400

    *** empty log message ***

---
+++ contrib/tbl/vipl/vipl_with_vnl_matrix/Templates/vipl_gaussian_convolution+vnl_matrix+uchar-.vnl_matrix+uchar-.uchar.uchar-.cxx
@@ -0,0 +1,8 @@
+// for vipl_gaussian_convolution<...> instantiations:
+#include "../vipl_filterable_section_container_generator_vnl_matrix.txx"
+#include "../accessors/vipl_accessors_vnl_matrix.h"
+#include <vipl/vipl_gaussian_convolution.txx>
+typedef unsigned char ubyte;
+typedef vnl_matrix<ubyte> img_type;
+
+template class vipl_gaussian_convolution<img_type,img_type,ubyte,ubyte, vipl_trivial_pixeliter>;
