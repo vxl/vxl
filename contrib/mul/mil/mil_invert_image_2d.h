a741e5c2b7e136005b0c19ba5fbc9bf40b87b10e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-05-14 10:39:35 -0400

    include guard should be different from that of  mil_normalise_image_2d.h

--- contrib/mul/mil/mil_invert_image_2d.h
+++ contrib/mul/mil/mil_invert_image_2d.h
@@ -1,18 +1,15 @@
-#ifndef mil_normalise_image_2d_h_
-#define mil_normalise_image_2d_h_
+#ifndef mil_invert_image_2d_h_
+#define mil_invert_image_2d_h_
 
 //: \file
 //  \brief Functions to convert images from one type to another
 //  \author Tim Cootes
 
 #include <mil/mil_image_2d_of.h>
-#include <vnl/vnl_vector.h>
 
 
-//: creates a variance normalised version of an input image  
+//: creates an inverted version of an input image  
 void mil_invert_image_2d(mil_image_2d_of<float>& dest,
-                          const mil_image_2d_of<float>& src);
+                         const mil_image_2d_of<float>& src);
 
-
-
-#endif // mil_normalise_image_2d_h_
+#endif // mil_invert_image_2d_h_
