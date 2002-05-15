b849eb05b5976950684911bb7c4221b53b647d66
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-05-15 10:32:24 -0400

    need #include <vcl_cmath.h> for sqrt(), exp(), fabs()

--- contrib/rpl/rrel/rrel_misc.cxx
+++ contrib/rpl/rrel/rrel_misc.cxx
@@ -1,7 +1,6 @@
-#include <rrel/rrel_misc.h>
-
+#include "rrel_misc.h"
+#include <vcl_cmath.h>
 #include <vnl/vnl_math.h>
-
 
 // Chebychev approximation to erfc --- the complement of the error
 // function.  Taken from Numerical Recipes in C.
@@ -53,14 +52,14 @@ rrel_misc_gaussian_cdf_inv( double p )
    double dp , dx , dt , ddq , dq ;
    int    newt ;
 
-   dp = (p <= 0.5) ? (p) : (1.0-p) ;   /* make between 0 and 0.5 */
+   dp = (p <= 0.5) ? p : 1.0-p;   // make between 0 and 0.5
 
    // if original value is invalid, return +infinity or -infinity
    // changed from original code to reflect the fact that the
    // the inverse of P(x) not Q(x) is desired.
    //
    // Original line: used for inverse of Q(x)
-   // if( dp <= 0.0 ){ dx = 13.0 ;  return ( (p <= 0.5) ? (dx) : (-dx) ) ; }
+   // if ( dp <= 0.0 ){ dx = 13.0 ;  return (p <= 0.5) ? (dx) : (-dx); }
 
    // replaced with this if construct for the inverse of P(x)
    if (p <= 0.0)
@@ -69,26 +68,26 @@ rrel_misc_gaussian_cdf_inv( double p )
      return vnl_huge_val(double(0));
 
 
-   /**  Step 1:  use 26.2.23 from Abramowitz and Stegun **/
+   //  Step 1:  use 26.2.23 from Abramowitz and Stegun
 
    dt = vcl_sqrt( -2.0 * vcl_log(dp) ) ;
    dx = dt
      - ((.010328e+0*dt + .802853e+0)*dt + 2.515517e+0)
      /(((.001308e+0*dt + .189269e+0)*dt + 1.432788e+0)*dt + 1.e+0) ;
 
-   /**  Step 2:  do 3 Newton steps to improve this **/
+   //  Step 2:  do 3 Newton steps to improve this
 
-   for( newt=0 ; newt < 3 ; newt++ ){
+   for ( newt=0 ; newt < 3 ; newt++ ){
      dq  = 0.5e+0 * rrel_misc_erfcc( dx / 1.414213562373095e+0 ) - dp ;
      ddq = vcl_exp( -0.5e+0 * dx * dx ) / 2.506628274631000e+0 ;
      dx  = dx + dq / ddq ;
    }
 
    // original line when computing the inverse of Q(x)
-   // return ( (p <= 0.5) ? (dx) : (-dx) ) ;  /* return with correct sign */
+   // return (p <= 0.5) ? dx : (-dx)   // return with correct sign
    //
    // Note that P(-x) = Q(x), so whatever x was calculated for Q(x) = p,
    // we simply need to return the negative of x to get P(xp) = p.
    //
-   return ( (p <= 0.5) ? (-dx) : (dx) ); // return with correct sign
+   return (p <= 0.5) ? (-dx) : dx; // return with correct sign
 }
