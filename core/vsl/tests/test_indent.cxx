ea58217f9c2a7e604602cd307bcd2207b0470534
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-05-17 07:26:36 -0400

    adding TEST()

--- core/vsl/tests/test_indent.cxx
+++ core/vsl/tests/test_indent.cxx
@@ -6,10 +6,11 @@
 #include <vsl/vsl_test.h>
 #include <vsl/vsl_indent.h>
 
-void test_indent1()
+static void test_indent1(int t)
 {
   vcl_cout<<"Tab Size is "<<vsl_indent_tab(vcl_cout)<<vcl_endl
           <<vsl_indent()<<"No Indent\n";
+  TEST("Tab size", vsl_indent_tab(vcl_cout), t);
   vsl_indent_inc(vcl_cout);
   vcl_cout<<vsl_indent()<<"1 Indent\n";
   vsl_indent_inc(vcl_cout);
@@ -28,12 +29,12 @@ void test_indent()
 
   vcl_cout<<"Using Tab size 2:\n\n";
   vsl_indent_set_tab(vcl_cout,2);
-  test_indent1();
+  test_indent1(2);
 
 
   vcl_cout<<"Using Tab size 4:\n\n";
   vsl_indent_set_tab(vcl_cout,4);
-  test_indent1();
+  test_indent1(4);
 
   vcl_cout<<vsl_indent()<<"Indent 0\n";
   vsl_indent_inc(vcl_cout);
