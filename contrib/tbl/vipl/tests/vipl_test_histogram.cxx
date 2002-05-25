bfde197d22e946db9c9953291241c0a2f432c4a4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-05-25 07:17:11 -0400

    avoid signed/unsigned comparison

--- contrib/tbl/vipl/tests/vipl_test_histogram.cxx
+++ contrib/tbl/vipl/tests/vipl_test_histogram.cxx
@@ -8,15 +8,13 @@
 // \date   12 Sept. 2000
 //
 
-
-//#include <vcl_cstdio.h>
 #include <vil/vil_memory_image_of.h>
 #include <vipl/accessors/vipl_accessors_vil_image.h>
 #include <vipl/accessors/vipl_accessors_vcl_vector.h>
 #include <vipl/vipl_with_vnl_matrix/accessors/vipl_accessors_vnl_vector.h>
-#include <vil/vil_pixel.h>
 #include <vipl/vipl_histogram.h>
 #include <vnl/vnl_vector.h>
+#include <vcl_iostream.h>
 #include "test_driver.h"
 
 
@@ -38,7 +36,6 @@ vil_image vipl_test_histogram_CreateTest8bitImage(int wd, int ht)
 }
 
 
-
 int vipl_test_histogram() {
   vil_image byte_img = vipl_test_histogram_CreateTest8bitImage(32, 32);
 
@@ -53,10 +50,11 @@ int vipl_test_histogram() {
 
     // check that final bin has correct number.
     bool test_pass = byte_out(byte_out.size()-1) + ((expected_n-2)*(expected_n-1))/2
-      == byte_img.width() * byte_img.height();
+                     == byte_img.width() * byte_img.height();
 
     // check that rest of bins are correct
-    for (int i=0; i<byte_out.size()-2; ++i) if (byte_out(i)  != i*2 + i*2+1) test_pass = false;
+    for (unsigned int i=0; i+2<byte_out.size(); ++i)
+      if (byte_out(i)  != i*2 + i*2+1) test_pass = false;
     TEST("vnl_vector histogram counted correctly", test_pass, true);
     vcl_cout << byte_out << vcl_endl;
   }
@@ -67,16 +65,13 @@ int vipl_test_histogram() {
 
     // check that final bin has correct number.
     bool test_pass = byte_out.back() + ((byte_out.size()-2)*(byte_out.size()-1))/2
-      == byte_img.width() * byte_img.height();
+                     == byte_img.width() * byte_img.height();
 
     // check that rest of bins are correct
-    for (int i=0; i<byte_out.size()-1; ++i) if (byte_out[i] != i) test_pass = false;
+    for (unsigned int i=0; i+1<byte_out.size(); ++i)
+      if (byte_out[i] != i) test_pass = false;
     TEST("vcl_vector histogram counted correctly", test_pass, true);
   }
-  
-
-  
-
 
   return 0;
 }
