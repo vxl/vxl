54f18756030d8d7c59fe82baa652ebf8d861fc71
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-05-25 16:33:35 -0400

    now using uchar instead of vil_byte

--- contrib/tbl/vipl/vipl_with_vnl_matrix/Templates/vipl_filter_2d+vil_image.vnl_vector+uint-.uchar.uint.cxx
+++ contrib/tbl/vipl/vipl_with_vnl_matrix/Templates/vipl_filter_2d+vil_image.vnl_vector+uint-.uchar.uint.cxx
@@ -1,5 +1,4 @@
 #include <vil/vil_image.h>
-#include <vil/vil_byte.h>
 #include <vnl/vnl_vector.h>
 
 // this must be here for filter-2d to work
@@ -7,10 +6,10 @@
 #include "../vipl_filterable_section_container_generator_vnl_vector.txx"
 
 #include <vipl/filter/vipl_filter.txx>
-template class vipl_filter<vil_image, vnl_vector<unsigned>, vil_byte, unsigned, 2, vipl_trivial_pixeliter>;
+template class vipl_filter<vil_image, vnl_vector<unsigned>, unsigned char, unsigned, 2, vipl_trivial_pixeliter>;
 
 #include <vipl/filter/vipl_filter_2d.txx>
-template class vipl_filter_2d<vil_image, vnl_vector<unsigned>, vil_byte, unsigned, vipl_trivial_pixeliter>;
+template class vipl_filter_2d<vil_image, vnl_vector<unsigned>, unsigned char, unsigned, vipl_trivial_pixeliter>;
 
 #include <vbl/vbl_smart_ptr.h>
 template class vbl_smart_ptr<vnl_vector<unsigned> >;
