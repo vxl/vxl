050811272800053f59ecaf348955ebec3e23a831
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-05-25 21:45:13 -0400

    2 tests added

--- contrib/mul/pdf1d/tests/test_exponential.cxx
+++ contrib/mul/pdf1d/tests/test_exponential.cxx
@@ -8,9 +8,9 @@
 
 void test_exponential()
 {
-  vcl_cout << "***********************" << vcl_endl;
-  vcl_cout << " Testing pdf1d_exponential " << vcl_endl;
-  vcl_cout << "***********************" << vcl_endl;
+  vcl_cout << "***************************\n";
+  vcl_cout << " Testing pdf1d_exponential\n";
+  vcl_cout << "***************************\n";
 
   vnl_vector<double> x(1000);
   pdf1d_exponential exp_pdf(5);
@@ -18,8 +18,11 @@ void test_exponential()
   pdf1d_sampler* sampler = exp_pdf.new_sampler();
   sampler->get_samples(x);
 
-  vcl_cout<<"Mean of samples: "<<x.mean()<<vcl_endl;
   vcl_cout<<"PDF mean : "<<exp_pdf.mean()<<vcl_endl;
+  vcl_cout<<"Mean of samples: "<<x.mean()<<vcl_endl;
+  TEST("PDF mean", exp_pdf.mean(), 0.2);
+  double e = x.mean()-exp_pdf.mean(); if (e<0) e = -e;
+  TEST("deviation from mean", e<0.01, true);
 
   delete sampler;
 }
