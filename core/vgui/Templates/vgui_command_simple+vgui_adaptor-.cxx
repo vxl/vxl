3a9293e86c0fd25d5f636627ca6524b30cbea213
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-05-27 02:18:27 -0400

    file renamed, since class was renamed

---
+++ core/vgui/Templates/vgui_command_simple+vgui_adaptor-.cxx
@@ -0,0 +1,4 @@
+#include <vgui/vgui_adaptor.h>
+#include <vgui/vgui_command.h>
+
+VGUI_COMMAND_SIMPLE_INSTANTIATE(vgui_adaptor);
---
+++ core/vgui/Templates/vgui_command_simple+vgui_tableau-.cxx
@@ -0,0 +1,4 @@
+#include <vgui/vgui_tableau.h>
+#include <vgui/vgui_command.h>
+
+VGUI_COMMAND_SIMPLE_INSTANTIATE(vgui_tableau);
