578d98e4d47b5a05aa96769a525a50465434ab16
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-05-27 06:27:57 -0400

    absolute include path needed

--- contrib/tbl/vipl/vipl_with_section/Templates/vipl_histogram+section+uchar.2-.vcl_vector+uint-.uchar.uint-.cxx
+++ contrib/tbl/vipl/vipl_with_section/Templates/vipl_histogram+section+uchar.2-.vcl_vector+uint-.uchar.uint-.cxx
@@ -1,8 +1,9 @@
 // for vipl_histogram<...> instantiations:
 #include "../vipl_filterable_section_container_generator_section.txx"
-#include "vipl/section/vipl_filterable_section_container_generator_vcl_vector.txx"
 #include "../accessors/vipl_accessors_section.h"
-#include "vipl/accessors/vipl_accessors_vcl_vector.h"
+#include <vcl_vector.h>
+#include <vipl/section/vipl_filterable_section_container_generator_vcl_vector.txx>
+#include <vipl/accessors/vipl_accessors_vcl_vector.h>
 #include <vipl/vipl_histogram.txx>
 
-template class vipl_histogram<section<unsigned char,2>,vcl_vector<unsigned>,unsigned char,unsigned, vipl_trivial_pixeliter>;
+template class vipl_histogram<section<unsigned char,2>,vcl_vector<unsigned int>,unsigned char,unsigned int,vipl_trivial_pixeliter>;
