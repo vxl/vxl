c515c2f21d2c8d1c835f39c4fe15a60c027d8e69
Geoffrey Cross <geoffreycross@users.sourceforge.net>
2002-06-02 12:20:53 -0400

    useful for conics and things...

---
+++ core/vnl/vnl_double_5.cxx
@@ -0,0 +1,13 @@
+#ifdef __GNUC__
+#pragma implementation
+#endif
+// This is vxl/vnl/vnl_double_5.cxx
+
+//
+// vnl_double_5
+// Author: Geoffrey Cross
+// Created: 2 Jun 2002
+//
+//-----------------------------------------------------------------------------
+
+#include "vnl_double_5.h"
