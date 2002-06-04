51c2be9a01f69dd9a087c3618534ecef398ab0bf
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-06-04 04:10:28 -0400

    deprecated header files finally removed

--- core/vnl/vnl_imag.h
+++
@@ -1,6 +0,0 @@
-#ifndef vnl_imag_h_
-#define vnl_imag_h_
-
-#include <vnl/vnl_complex_ops.h>
-
-#endif
--- core/vnl/vnl_real.h
+++
@@ -1,6 +0,0 @@
-#ifndef vnl_real_h_
-#define vnl_real_h_
-
-#include <vnl/vnl_complex_ops.h>
-
-#endif
