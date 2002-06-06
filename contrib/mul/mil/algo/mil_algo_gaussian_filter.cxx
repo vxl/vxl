3fcc20c6e5e2df1e31e307fd103f43e1f87fe346
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-06-06 02:48:45 -0400

    should be "template class C<T>;", not "template C<T>;"

--- contrib/mul/mil/algo/mil_algo_gaussian_filter.cxx
+++ contrib/mul/mil/algo/mil_algo_gaussian_filter.cxx
@@ -1,5 +1,5 @@
 #include <mil/algo/mil_algo_gaussian_filter.txx>
 #include <vil/vil_byte.h>
 
-template mil_algo_gaussian_filter<float,float>;
-template mil_algo_gaussian_filter<vil_byte,float>;
+template class mil_algo_gaussian_filter<float,float>;
+template class mil_algo_gaussian_filter<vil_byte,float>;
