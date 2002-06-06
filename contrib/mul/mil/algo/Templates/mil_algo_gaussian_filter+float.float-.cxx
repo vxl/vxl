678ae3da2a91b521556100d2140264cb9ebf5d90
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-06-06 03:02:51 -0400

    template instantiations moved to Templates directory

--- contrib/mul/mil/algo/Templates/mil_algo_gaussian_filter+float.float-.cxx
+++ contrib/mul/mil/algo/Templates/mil_algo_gaussian_filter+float.float-.cxx
@@ -1,5 +1,3 @@
 #include <mil/algo/mil_algo_gaussian_filter.txx>
-#include <vil/vil_byte.h>
 
 template class mil_algo_gaussian_filter<float,float>;
-template class mil_algo_gaussian_filter<vil_byte,float>;
---
+++ contrib/mul/mil/algo/Templates/mil_algo_gaussian_filter+vil_byte.float-.cxx
@@ -0,0 +1,4 @@
+#include <mil/algo/mil_algo_gaussian_filter.txx>
+#include <vil/vil_byte.h>
+
+template class mil_algo_gaussian_filter<vil_byte,float>;
