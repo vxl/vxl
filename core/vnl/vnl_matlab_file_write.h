497068e67d5b293d7a7667fe13059b5faa9452cf
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-06-08 05:27:12 -0400

    This header file is deprecated

--- core/vnl/vnl_matlab_file_write.h
+++ core/vnl/vnl_matlab_file_write.h
@@ -1,9 +1,13 @@
 #ifndef vnl_matlab_file_write_h_
 #define vnl_matlab_file_write_h_
+//:
+// \file
+// \deprecated Use <vnl/vnl_matlab_filewrite.h> instead
 /*
   fsm@robots.ox.ac.uk
 */
 
+#include <vcl_deprecated_header.h>
 #include <vnl/vnl_matlab_filewrite.h>
 #define vnl_matlab_file_write vnl_matlab_filewrite
 
--- core/vnl/vnl_random.h
+++ core/vnl/vnl_random.h
@@ -3,10 +3,13 @@
 #define vnl_random_h_
 //:
 // \file
+// \deprecated Use <vnl/vnl_sample.h> instead
 // \author awf@robots.ox.ac.uk
 // \date   15 Sep 01
 //  This file is here only to point you toward vnl_sample.h
 //  if you're looking for random number generation.
+
+#include <vcl_deprecated_header.h>
 
 //: The function is a dummy
 void vnl_random_sample();
