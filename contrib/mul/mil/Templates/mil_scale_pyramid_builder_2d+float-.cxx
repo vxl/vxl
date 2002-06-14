a101f00c526a47fe52c3bc4bd72447ac6ce87fc6
David Cristinacce <dcristinacce@users.sourceforge.net>
2002-06-14 06:16:49 -0400

    scale pyramid templates

---
+++ contrib/mul/mil/Templates/mil_scale_pyramid_builder_2d+float-.cxx
@@ -0,0 +1,4 @@
+#include <mil/mil_scale_pyramid_builder_2d.txx>
+template class mil_scale_pyramid_builder_2d<float>;
+
+// MIL_GAUSSIAN_PYRAMID_BUILDER_2D_GENERAL_INSTANTIATE(float);
---
+++ contrib/mul/mil/Templates/mil_scale_pyramid_builder_2d+vil_byte-.cxx
@@ -0,0 +1,5 @@
+#include <mil/mil_scale_pyramid_builder_2d.txx>
+#include <vil/vil_byte.h>
+template class mil_scale_pyramid_builder_2d<vil_byte>;
+
+// MIL_GAUSSIAN_PYRAMID_BUILDER_2D_GENERAL_INSTANTIATE(vil_byte);
