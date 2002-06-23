d8fa67a5508179c0786213800395c9cc555a8234
Fred Wheeler <fred_wheeler@users.sourceforge.net>
2002-06-22 23:14:22 -0400

    add defines which must be in place when png.h is included in Cygwin

--- config/cmake/Modules/FindPNG.cmake
+++ config/cmake/Modules/FindPNG.cmake
@@ -23,7 +23,32 @@ IF(NOT HAS_PNG)
       ADD_DEFINITIONS( -DHAS_PNG )
   
       INCLUDE( ${allvxl_SOURCE_DIR}/v3p/png/CMakeListsLink.txt )
-  
+
+      # With cygwin, the there are multiple user configurations
+      # possible. From pngconf.h:
+      #     'Cygwin' defines/defaults:
+      #       PNG_BUILD_DLL -- building the dll
+      #       (no define)   -- building an application, linking to the dll
+      #       PNG_STATIC    -- building the static lib, or building an application
+      #                        which links to the static lib.
+      #
+      # Since we are here, there is no external png library. Therefore, we
+      # build a static or shared based on the vxl build property, and rely
+      # on FindPNG.cmake to propagate the appropriate build flags to user
+      # code.
+      #
+      # NB: make sure this logic is consistent with v3p/png/CMakeLists.txt
+
+      IF (CYGWIN)
+        # in Cygwin a define is needed by any file including
+        # vxl/v3p/png/png.h (which in turn includes pngconf.h)
+        IF(BUILD_SHARED_LIBS)
+          ADD_DEFINITIONS(-DPNG_BUILD_DLL)
+        ELSE(BUILD_SHARED_LIBS)
+          ADD_DEFINITIONS(-DPNG_STATIC)
+        ENDIF(BUILD_SHARED_LIBS)
+      ENDIF (CYGWIN)
+
     ENDIF(PNG_INCLUDE_PATH)
   
   ELSE(NOT HAS_NATIVE_PNG)
