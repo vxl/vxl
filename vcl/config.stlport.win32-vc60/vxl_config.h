37fbb2a3ca6fa2fb530b7c08b7a0f44224b745b6
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-06-25 18:14:13 -0400

    brought in correspondence with vxl_config.h.in

--- vcl/config.stlport.win32-vc60/vxl_config.h
+++ vcl/config.stlport.win32-vc60/vxl_config.h
@@ -1,8 +1,8 @@
 #ifndef vxl_config_h_config_stlport_win32_vc60_
 #define vxl_config_h_config_stlport_win32_vc60_
 
-// This file was *not* generated from vxl_config.h.in
-// It is maintained manually.
+/* This file was *not* generated from vxl_config.h.in */
+/* It is maintained manually. */
 
 #define VXL_LITTLE_ENDIAN 1
 #define VXL_BIG_ENDIAN    0
@@ -10,18 +10,23 @@
 typedef char      vxl_int8;
 typedef short     vxl_int16;
 typedef int       vxl_int32;
-typedef void      vxl_int64;           // !
+typedef void      vxl_int64;           /* ! */
 
 typedef unsigned char      vxl_uint8;
 typedef unsigned short     vxl_uint16;
 typedef unsigned int       vxl_uint32;
-typedef void               vxl_uint64; // !
+typedef void               vxl_uint64; /* ! */
 
 typedef signed char      vxl_sint8;
 typedef signed short     vxl_sint16;
 typedef signed int       vxl_sint32;
-typedef void             vxl_sint64;   // !
+typedef void             vxl_sint64;   /* ! */
 
+#define vxl_ieee_32  float
+#define vxl_ieee_64  double
+
+#define VXL_HAS_PTHREAD_H         0
+#define VXL_HAS_SEMAPHORE_H       0
 
 #define VXL_UNISTD_HAS_USECONDS_T 0
 #define VXL_UNISTD_HAS_INTPTR_T   0
@@ -35,22 +40,11 @@ typedef void             vxl_sint64;   // !
 #define VXL_UNISTD_USLEEP_IS_VOID 0
 #define VXL_TWO_ARG_GETTIME       0
 #define VXL_STDLIB_HAS_QSORT      1
-#define VXL_STDLIB_HAS_LRAND48    1 // ?
+#define VXL_STDLIB_HAS_LRAND48    1 /* ? */
 #define VXL_STDLIB_HAS_DRAND48    0
-#define VXL_MATH_HAS_FINITE       0 // ?
+#define VXL_IEEEFP_HAS_FINITE     0 /* ? */
+#define VXL_MATH_HAS_FINITE       0 /* ? */
+#define VXL_MATH_HAS_SQRTF        1 /* ? */
 #define VXL_HAS_IEEEFP_H          1
-
-
-#if 0
-#define VXL_HAS_JPEG
-#define VXL_HAS_TIFF
-#define VXL_HAS_PNG
-#define VXL_HAS_HERMES
-#define VXL_HAS_OPENGL 1
-#define VXL_HAS_GLUT   1
-#define VXL_HAS_FLTK
-#define VXL_HAS_GTK    1
-#define VXL_HAS_SDL
-#endif
 
 #endif
