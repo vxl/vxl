59f0ad4d0d137309e673ba0357a6bdf2faf81ceb
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-07-10 10:36:12 -0400

    inconsistent naming of parameters src0 and src1 in documentation corrected

--- contrib/mul/mbl/mbl_data_collector.h
+++ contrib/mul/mbl/mbl_data_collector.h
@@ -1,9 +1,9 @@
+// This is mul/mbl/mbl_data_collector.h
 #ifndef mbl_data_collector_h_
 #define mbl_data_collector_h_
 #ifdef __GNUC__
 #pragma interface
 #endif
-
 //:
 // \file
 // \brief Templated base class for objects which collect sets of data.
@@ -52,17 +52,20 @@ unsigned long mbl_data_collector_copy_all(mbl_data_collector<T> &dest, mbl_data_
 
 
 //: Merge all the data from two mbl_data_wrapper-s into one mbl_data_collector
-// This function will change the position of the iterator in src1 and src2,
-// but will not modify any of the data therein. Existing vectors in dest are
-// preserved, as is the vector ordering within src1 and src2.
-// The function returns the number of vectors copied.
-// \param order If specified, this vector will contains 0 in every position
-// where a value from src0 has been copied into dest, and a 1 where a
-// valud from src1 has been copied into dest.
+// This function will change the position of the iterator in src0 and
+// src1, but will not modify any of the data therein. Existing vectors
+// in dest are preserved, as is the vector ordering within src0
+// and src1.  The function returns the number of vectors copied.
+// \param src0 The first input mbl_data_wrapper
+// \param src1 The second input mbl_data_wrapper
+// \param dest The destination mbl_data_collector
+// \param order If specified, this vector will contain 0 in every position
+// where a value from src0 has been copied into dest, and a 1
+// where a value from src1 has been copied into dest.
 template<class T>
 unsigned long mbl_data_collector_merge_all(mbl_data_collector<T > &dest,
-                           mbl_data_wrapper<T > &src0,
-                           mbl_data_wrapper<T > &src1,
-                           vcl_vector<unsigned> *order = 0);
+                                           mbl_data_wrapper<T > &src0,
+                                           mbl_data_wrapper<T > &src1,
+                                           vcl_vector<unsigned> *order = 0);
 
 #endif // mbl_data_collector_h_
