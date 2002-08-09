ebcde13a98dd05a554de541eeee21d6be88070ad
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-08-09 07:16:40 -0400

    define NO_MESA when GL/xmesa.h is not found

--- config/cmake/Modules/FindOpenGL.cmake
+++ config/cmake/Modules/FindOpenGL.cmake
@@ -1,5 +1,5 @@
-#
-# try to find OpenGL on UNIX systems once done this will define
+# Try to find OpenGL on UNIX systems.
+# Once done this will define
 #
 # HAS_OPENGL          = system has OpenGL and it should be used
 #
@@ -15,33 +15,30 @@
 #
 
 IF(NOT HAS_OPENGL)
-
-
   IF (WIN32)
-
     IF (CYGWIN)
-  
+
       SET( HAS_OPENGL "NO" )
-  
-      FIND_PATH(OPENGL_INCLUDE_PATH GL/gl.h 
-        /usr/include 
-        /usr/X11R6/include 
+
+      FIND_PATH(OPENGL_INCLUDE_PATH GL/gl.h
+        /usr/include
+        /usr/X11R6/include
       )
-  
+
       FIND_LIBRARY(OPENGL_gl_LIBRARY opengl32
-        /usr/lib 
+        /usr/lib
         /usr/lib/w32api
       )
-  
+
       FIND_LIBRARY(OPENGL_glu_LIBRARY glu32
-        /usr/lib 
+        /usr/lib
         /usr/lib/w32api
       )
-  
+
       IF(OPENGL_INCLUDE_PATH)
       IF(OPENGL_gl_LIBRARY)
       IF(OPENGL_glu_LIBRARY)
-  
+
         SET( HAS_OPENGL "YES" )
         ADD_DEFINITIONS( -DHAS_OPENGL )
 
@@ -70,9 +67,9 @@ IF(NOT HAS_OPENGL)
     ENDIF (CYGWIN)
 
   ELSE (WIN32)
-  
+
     SET( HAS_OPENGL "NO" )
-  
+
     # The first line below is to make sure that the proper headers
     # are used on a Linux machine with the NVidia drivers installed.
     # They replace Mesa with NVidia's own library but normally do not
@@ -81,45 +78,62 @@ IF(NOT HAS_OPENGL)
     # Make sure the NVIDIA directory comes BEFORE the others.
     #  - Atanas Georgiev <atanas@cs.columbia.edu>
 
-    FIND_PATH(OPENGL_INCLUDE_PATH GL/gl.h 
+    FIND_PATH(OPENGL_INCLUDE_PATH GL/gl.h
       /usr/share/doc/NVIDIA_GLX-1.0/include
-      /usr/include 
-      /usr/local/include 
-      /usr/openwin/share/include 
-      /opt/graphics/OpenGL/include 
-      /usr/X11R6/include 
+      /usr/include
+      /usr/local/include
+      /usr/openwin/share/include
+      /opt/graphics/OpenGL/include
+      /usr/X11R6/include
     )
-  
+
+    FIND_PATH(OPENGL_XMESA_INCLUDE_PATH GL/xmesa.h
+      /usr/share/doc/NVIDIA_GLX-1.0/include
+      /usr/include
+      /usr/local/include
+      /usr/openwin/share/include
+      /opt/graphics/OpenGL/include
+      /usr/X11R6/include
+    )
+
     FIND_LIBRARY(OPENGL_gl_LIBRARY
       NAMES MesaGL GL
-      PATHS /usr/lib 
-            /usr/local/lib 
-            /opt/graphics/OpenGL/lib 
-            /usr/openwin/lib 
+      PATHS /usr/lib
+            /usr/local/lib
+            /opt/graphics/OpenGL/lib
+            /usr/openwin/lib
             /usr/X11R6/lib
     )
-  
+
     FIND_LIBRARY(OPENGL_glu_LIBRARY
       NAMES MesaGLU GLU
-      PATHS /usr/lib 
-            /usr/local/lib 
-            /opt/graphics/OpenGL/lib 
-            /usr/openwin/lib 
+      PATHS /usr/lib
+            /usr/local/lib
+            /opt/graphics/OpenGL/lib
+            /usr/openwin/lib
             /usr/X11R6/lib
     )
-  
+
     IF(OPENGL_INCLUDE_PATH)
     IF(OPENGL_gl_LIBRARY)
     IF(OPENGL_glu_LIBRARY)
-  
+
       SET( HAS_OPENGL "YES" )
       ADD_DEFINITIONS( -DHAS_OPENGL )
 
       SET( OPENGL_LIBRARIES ${OPENGL_gl_LIBRARY} ${OPENGL_glu_LIBRARY} ${CMAKE_THREAD_LIBS} )
 
+      IF(OPENGL_XMESA_INCLUDE_PATH)
+        SET( HAS_MESAGL "YES" )
+        ADD_DEFINITIONS( -DMESA )
+      ELSE(OPENGL_XMESA_INCLUDE_PATH)
+        SET( HAS_MESAGL "NO" )
+        ADD_DEFINITIONS( -DNO_MESA )
+      ENDIF(OPENGL_XMESA_INCLUDE_PATH)
+
     ENDIF(OPENGL_glu_LIBRARY)
     ENDIF(OPENGL_gl_LIBRARY)
     ENDIF(OPENGL_INCLUDE_PATH)
-  
+
   ENDIF (WIN32)
 ENDIF(NOT HAS_OPENGL)
