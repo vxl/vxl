7d40c9f1d0eaba8341cd6c84db58a9575d8aea42
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-08-12 11:57:55 -0400

    FMatrixCompute8Point is actually FMatrixComputeLinear

--- contrib/oxl/mvl/FMatrixCompute8Point.h
+++ contrib/oxl/mvl/FMatrixCompute8Point.h
@@ -1,31 +1,18 @@
 // This is oxl/mvl/FMatrixCompute8Point.h
 #ifndef FMatrixCompute8Point_h_
 #define FMatrixCompute8Point_h_
-#ifdef VCL_NEEDS_PRAGMA_INTERFACE
-#pragma interface
-#endif
 //-----------------------------------------------------------------------------
 //:
 // \file
 // \brief FMatrix by linear least squares
 //
-//    FMatrixCompute8Point is a subclass of FMatrixCompute, and implements
-//    the ``8-point'' or linear least squares algorithm for estimation of
+//    FMatrixCompute8Point is actually Hartley's FMatrixComputeLinear
+//    ``8-point'' or linear least squares algorithm for estimation of
 //    the fundamental matrix.
 //
-// \example examples/exampleFMatrixCompute.cxx
-//
-// \author
-//     Andrew W. Fitzgibbon, Oxford IERG, 23 Jul 96
 
-#include <mvl/FMatrix.h>
-#include <mvl/FMatrixCompute.h>
+#include <mvl/FMatrixComputeLinear.h>
 
-class FMatrixCompute8Point : public FMatrixCompute
-{
- public:
-  // Implement FMatrixCompute interface
-  bool compute();
-};
+typedef FMatrixCompute8Point FMatrixComputeLinear;
 
 #endif // FMatrixCompute8Point_h_
