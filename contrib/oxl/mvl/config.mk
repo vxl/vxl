f93186d7efda7b37e1678012c87b7621ebaa4585
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-08-27 13:04:47 -0400

    use of ald-libs now replaced with using config.mk

--- contrib/oxl/mvl/config.mk
+++ contrib/oxl/mvl/config.mk
@@ -4,3 +4,4 @@ include ${IUEROOT}/vxl/vnl/algo/config.mk
 include ${IUEROOT}/vxl/vbl/config.mk
 include ${IUEROOT}/vxl/vgl/config.mk
 include ${IUEROOT}/vxl/vul/config.mk
+include ${IUEROOT}/vxl/vil/config.mk # although not really used yet ...
