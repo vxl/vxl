0cc04e873753208b3ecf4ac40612b94cb35eaa22
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-08-31 09:44:48 -0400

    tests added

--- core/vil1/tests/test_resample.cxx
+++ core/vil1/tests/test_resample.cxx
@@ -1,14 +1,17 @@
 // This is vxl/vil/tests/test_resample.cxx
 // \author Peter Vanroose
 
-#include <vil/vil_load.h>
 #include <vil/vil_resample.h>
 
 #include <vcl_iostream.h>
 
+#include <vil/vil_load.h>
 #include <testlib/testlib_test.h>
 
 static char default_filename[] = "square.pgm";
+
+template <class T> inline
+T get_pixel(vil_image const& i, int x, int y, T* /*dummy*/) { T t; i.get_section(&t,x,y,1,1); return t; }
 
 MAIN(test_resample)
 {
@@ -26,40 +29,41 @@ MAIN(test_resample)
     return 1;
   }
 
-  char aa[3];
-  a.get_section(aa,  0, 0, 1,1);
-  a.get_section(aa+1,24,16,1,1);
-  a.get_section(aa+2,10,30,1,1);
-
   int wd=a.width(), ht=a.height();
+
+  if ( wd < 25 || ht < 31 ) {
+    vcl_cerr << "Could not use this image " << filename << " since it is too small\n";
+    return 1;
+  }
+
+  unsigned char a1 = get_pixel(a,  0,  0, (unsigned char*)0);
+  unsigned char a2 = get_pixel(a, 24, 16, (unsigned char*)0);
+  unsigned char a3 = get_pixel(a, 10, 30, (unsigned char*)0);
+
+  vcl_cout << "Simplest resampling: reduce the image by a factor 2 in both directions.\n";
 
   vil_image b = vil_resample(a,wd/2,ht/2);
   TEST("width", b.width(), wd/2);
   TEST("height", b.height(), ht/2);
 
-  char bb[3];
-  b.get_section(bb,  0, 0, 1,1);
-  b.get_section(bb+1,12,8, 1,1);
-  b.get_section(bb+2,5, 15,1,1);
+  TEST("subsampled pixel value", get_pixel(b,  0,  0, (unsigned char*)0), a1);
+  TEST("subsampled pixel value", get_pixel(b, 12,  8, (unsigned char*)0), a2);
+  TEST("subsampled pixel value", get_pixel(b,  5, 15, (unsigned char*)0), a3);
 
-  TEST("subsampled pixel values", bb[0]==aa[0] && bb[1]==aa[1] && bb[2]==aa[2], true);
+  vcl_cout << "Now enlarge the image by a factor 2 in both directions.\n";
 
   vil_image c = vil_resample(a,wd*2,ht*2);
   TEST("width", c.width(), wd*2);
   TEST("height", c.height(), ht*2);
 
-  c.get_section(bb,  1, 1, 1,1);
-  c.get_section(bb+1,49,33,1,1);
-  c.get_section(bb+2,21,61,1,1);
+  TEST("upsampled pixel value", get_pixel(c,  1,  1, (unsigned char*)0), a1);
+  TEST("upsampled pixel value", get_pixel(c, 49, 33, (unsigned char*)0), a2);
+  TEST("upsampled pixel value", get_pixel(c, 21, 61, (unsigned char*)0), a3);
 
-  TEST("upsampled pixel values", bb[0]==aa[0] && bb[1]==aa[1] && bb[2]==aa[2], true);
-
-  // Intermediate pixels are black (0):
-  c.get_section(bb,  0, 0, 1,1);
-  c.get_section(bb+1,48,32,1,1);
-  c.get_section(bb+2,20,60,1,1);
-
-  TEST("intermediate pixel values", bb[0]==0 && bb[1]==0 && bb[2]==0, true);
+  // Intermediate pixels were black (0), but are now equal to neighbour:
+  TEST("intermediate pixel value", get_pixel(c,  0,  0, (unsigned char*)0), a1);
+  TEST("intermediate pixel value", get_pixel(c, 48, 32, (unsigned char*)0), a2);
+  TEST("intermediate pixel value", get_pixel(c, 20, 60, (unsigned char*)0), a3);
 
   SUMMARY();
 }
