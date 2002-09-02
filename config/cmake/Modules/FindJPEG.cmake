a586c80aaaabe3f563395e99dc9a9343da7cd5d0
Ian Scott <scottim@users.sourceforge.net>
2002-09-02 11:00:00 -0400

    Move FindJPEG over to new scheme

--- config/cmake/Modules/FindJPEG.cmake
+++ config/cmake/Modules/FindJPEG.cmake
@@ -1,37 +1,47 @@
 #
 # Find a JPEG library
 #
+#
+# This file is used to manage using either a natively provided JPEG library or the one in v3p if provided.
+#
+#
+# As per the standard scheme the following definitions are used
+# JPEG_INCLUDE_DIR - where to find jpeglib.h
+# JPEG_LIBRARIES   - the set of libraries to include to use JPEG.
+# JPEG_FOUND       - TRUE, if available someone one the system.
 
-IF(NOT HAS_JPEG)
+# Additionally
+# VXL_USING_NATIVE_JPEG  - True if we are using a JPEG library provided outside vxl (or v3p)
 
-  INCLUDE( ${MODULE_PATH}/FindNativeJPEG.cmake )
-  
-  IF(NOT HAS_NATIVE_JPEG)
-  
-    #
-    # At some point, in a "release" version, it is possible that someone
-    # will not have the v3p jpeg library
-    #
-  
-    FIND_PATH( JPEG_INCLUDE_PATH jpeglib.h
-      ${allvxl_SOURCE_DIR}/v3p/jpeg
-    )
-  
-    IF(JPEG_INCLUDE_PATH)
-  
-      SET( HAS_JPEG "YES" )
-      ADD_DEFINITIONS( -DHAS_JPEG )
 
-      SET( JPEG_LIBRARIES jpeg )  
-  
-    ENDIF(JPEG_INCLUDE_PATH)
-  
-  ELSE(NOT HAS_NATIVE_JPEG)
-  
-    SET( JPEG_INCLUDE_PATH ${NATIVE_JPEG_INCLUDE_PATH} )
-    SET( JPEG_LIBRARIES ${NATIVE_JPEG_LIBRARIES} )
-    SET( HAS_JPEG "YES" )
-    ADD_DEFINITIONS( -DHAS_JPEG )
-  
-  ENDIF(NOT HAS_NATIVE_JPEG)
-ENDIF(NOT HAS_JPEG)
+INCLUDE( ${MODULE_PATH}/NewCMake/FindJPEG.cmake )
+
+IF(NOT FOUND_JPEG)
+
+  #
+  # At some point, in a "release" version, it is possible that someone
+  # will not have the v3p jpeg library
+  #
+
+  FIND_PATH( JPEG_INCLUDE_PATH jpeglib.h
+    ${allvxl_SOURCE_DIR}/v3p/jpeg
+  )
+
+  IF(EXISTS ${allvxl_SOURCE_DIR}/v3p/jpeg/jpeglib.h)
+
+    SET( JPEG_FOUND "YES" )
+    SET( JPEG_LIBRARIES jpeg )  
+    SET( JPEG_INCLUDE_DIR ${allvxl_SOURCE_DIR}/v3p/jpeg)  
+	
+  ENDIF(EXISTS ${allvxl_SOURCE_DIR}/v3p/jpeg/jpeglib.h)
+
+ENDIF(NOT FOUND_JPEG)
+
+IF(JPEG_LIBRARY)
+  SET(VXL_USING_NATIVE_JPEG)
+ENDIF(JPEG_LIBRARY)
+
+IF(JPEG_FOUND)
+  SET (JPEG_DEFINITIONS ${JPEG_DEFINITIONS} -DHAS_JPEG)
+ENDIF(JPEG_FOUND)
+
