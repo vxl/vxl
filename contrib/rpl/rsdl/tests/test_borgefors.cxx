d31e095e91056cce618cdc712b837539f1ff1318
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-02 08:56:30 -0400

    no longer using rsdl_test.h

--- contrib/rpl/rsdl/tests/test_borgefors.cxx
+++ contrib/rpl/rsdl/tests/test_borgefors.cxx
@@ -2,12 +2,16 @@
 #include <vcl_iostream.h>
 #include <vcl_vector.h>
 #include <vnl/vnl_vector_fixed.h>
-#include <rsdl/rsdl_test.h>
+#include <testlib/testlib_test.h>
 
-bool location_close(const vnl_vector_fixed<double,2>& v1,
-                    const vnl_vector_fixed<double,2>& v2)
+static inline
+void TEST_NEAR_vec(const char* msg,
+                   const vnl_vector_fixed<double,2>& v1,
+                   const vnl_vector_fixed<double,2>& v2,
+                   double tol)
 {
-  return rsdl_close(v1.x(), v2.x()) && rsdl_close(v1.y(), v2.y());
+  TEST_NEAR(msg, v1.x(), v2.x(), tol);
+  TEST_NEAR(msg, v1.y(), v2.y(), tol);
 }
 
 int
@@ -21,22 +25,22 @@ main()
   for (int j = -range; j< range; j++)
     data.push_back(vnl_vector_fixed<double,2>(0.0,j));
 
-  rsdl_tests_start( vcl_string("Borgefors distance map") );
+  testlib_test_start( "Borgefors distance map" );
 
   vcl_cout << "\nTests a borgefors map without a mask.\n";
   rsdl_borgefors<vnl_vector_fixed<double,2> >
     bg_map1(-range+50, -range+50, 2*range-100, 2*range-100, data.begin(), data.end());
-  TEST("in_map (out of boundary) ", (bg_map1.in_map(-range+49,0) == false));
-  TEST("in_map (in boundary) ", (bg_map1.in_map(-range+51,-range+51) == true));
+  TEST("in_map (out of boundary) ", bg_map1.in_map(-range+49,0), false);
+  TEST("in_map (in boundary) ", bg_map1.in_map(-range+51,-range+51), true);
   vnl_vector_fixed<double,2> closest_vec(100,0);
-  TEST("get_nearest ",(location_close(*(bg_map1.nearest(100,99)),closest_vec)));
-  TEST("get_distnace ", rsdl_close(bg_map1.distance(100,99),99));
+  TEST_NEAR_vec("get_nearest ", *(bg_map1.nearest(100,99)) ,closest_vec, 1e-6);
+  TEST_NEAR("get_distance ", bg_map1.distance(100,99), 99, 1e-6);
 
   rsdl_borgefors<vnl_vector_fixed<double,2> > bg_map2;
   bg_map2.set(-range+50, -range+50, 2*range-100, 2*range-100, data.begin(), data.end());
 
-  TEST("test operator== ", (bg_map2 == bg_map1) );
+  TEST("test operator== ", bg_map2, bg_map1);
   bg_map2.reset();
   bg_map1.reset();
-  return rsdl_tests_summary();
+  return testlib_test_summary();
 }
