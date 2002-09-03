f6abf3f82c65cc255466bd383473e051f445c0e3
Ian Scott <scottim@users.sourceforge.net>
2002-09-03 08:22:15 -0400

    Oops - I checked in the wrong file previously

--- config/cmake/Modules/NewCMake/FindMPEG.cmake
+++ config/cmake/Modules/NewCMake/FindMPEG.cmake
@@ -1,28 +1,38 @@
 #
-# Find the native TIFF includes and library
+# Find the native MPEG includes and library
 #
 # This module defines
-# TIFF_INCLUDE_DIR, where to find tiff.h, etc.
-# TIFF_LIBRARIES, the libraries to link against to use TIFF.
-# TIFF_FOUND, If false, do not try to use TIFF.
+# MPEG_INCLUDE_DIR, where to find MPEG.h, etc.
+# MPEG_LIBRARIES, the libraries to link against to use MPEG.
+# MPEG_FOUND, If false, do not try to use MPEG.
 
 # also defined, but not for general use are
-# TIFF_LIBRARY, where to find the TIFF library.
+# MPEG_mpeg2_LIBRARY, where to find the MPEG library.
+# MPEG_vo_LIBRARY, where to find the vo library.
 
-FIND_PATH(TIFF_INCLUDE_DIR tiff.h
+FIND_PATH(MPEG_INCLUDE_DIR mpeg2dec/include/video_out.h
   /usr/local/include
   /usr/include
+  /usr/local/livid
 )
 
-FIND_LIBRARY(TIFF_LIBRARY tiff
-  /usr/lib
+FIND_LIBRARY(MPEG_mpeg2_LIBRARY mpeg2
   /usr/local/lib
+  /usr/lib
+  /usr/local/livid/mpeg2dec/libmpeg2/.libs
 )
 
-IF(TIFF_INCLUDE_DIR)
-  IF(TIFF_LIBRARY)
-    SET( TIFF_FOUND "YES" )
-    SET( TIFF_LIBRARIES ${TIFF_LIBRARY} )
-  ENDIF(TIFF_LIBRARY)
-ENDIF(TIFF_INCLUDE_DIR)
+FIND_LIBRARY( MPEG_vo_LIBRARY vo
+  /usr/local/lib
+  /usr/lib
+  /usr/local/livid/mpeg2dec/libvo/.libs
+)
+
+SET( MPEG_FOUND "NO" )
+IF(MPEG_INCLUDE_DIR)
+  IF(MPEG_mpeg2_LIBRARY)
+    SET( MPEG_FOUND "YES" )
+    SET( MPEG_LIBRARIES ${MPEG_mpeg2_LIBRARY} ${MPEG_vo_LIBRARY} )
+  ENDIF(MPEG_mpeg2_LIBRARY)
+ENDIF(MPEG_INCLUDE_DIR)
 
