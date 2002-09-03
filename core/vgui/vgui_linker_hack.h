933ea6391df12e390e2e6ecaa44c91e112433980
Amitha Perera <amithaperera@users.sourceforge.net>
2002-09-03 12:16:25 -0400

    Fix comment

--- core/vgui/vgui_linker_hack.h
+++ core/vgui/vgui_linker_hack.h
@@ -3,7 +3,7 @@
 //:
 // \file
 // This file is no longer necessary. The functionality has been moved
-// into vgui_link_all.cxx. However, there are probably many
+// into vgui_register_all.cxx. However, there are probably many
 // executables that include this file, so it's best to keep it around.
 
 #include <vcl_deprecated_header.h>
