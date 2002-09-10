54ba5a830067730435d6b7b8fa90068a90e2952c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-10 06:26:35 -0400

    should check that argument is valid test; e.g., cstdio was misspelled

--- vcl/tests/test_driver.cxx
+++ vcl/tests/test_driver.cxx
@@ -2,119 +2,124 @@
 // \file
 // \brief vcl_tests.cxx : Run all vcl tests from one app.
 //  I think this is preferable to having many vcl_test_* projects.
+//  Note that all tests' main function should have signature (int,char**).
 // \author awf, mar 2000
 
 #include <vcl_iostream.h>
 #include <vcl_string.h>
 
-int test_algorithm_main();
-int test_cctype_main();
-int test_cmath_main();
-int test_compiler_main();
-int test_complex_main();
-int test_deque_main();
-int test_exception_main();
-int test_fstream_main();
-int test_headers_main(int, char**);
-int test_iostream_main();
-int test_iterator_main();
-int test_list_main();
-int test_map_main();
-int test_multimap_main();
-int test_new_main();
-int test_rel_ops_main();
-int test_set_main();
-int test_stlfwd_main();
-int test_string_main();
-int test_vector_main();
-int test_cstdio_main( const char* );
+int test_algorithm_main(int, char*[]);
+int test_cctype_main(int, char*[]);
+int test_cmath_main(int, char*[]);
+int test_compiler_main(int, char*[]);
+int test_complex_main(int, char*[]);
+int test_deque_main(int, char*[]);
+int test_exception_main(int, char*[]);
+int test_fstream_main(int, char*[]);
+int test_headers_main(int, char**); // need not be called: just a compiler test
+int test_iostream_main(int, char*[]);
+int test_iterator_main(int, char*[]);
+int test_list_main(int, char*[]);
+int test_map_main(int, char*[]);
+int test_multimap_main(int, char*[]);
+int test_new_main(int, char*[]);
+int test_set_main(int, char*[]);
+int test_stlfwd_main(int, char*[]); // need not be called: just a compiler test
+int test_string_main(int, char*[]);
+int test_vector_main(int, char*[]);
+int test_cstdio_main(int, char*[]);
 
-int result;
+int passed;
+int failed;
 
 void testname( char* testname )
 {
-  vcl_cout << "   Testing " << testname << "...";
+  vcl_cout << "   Testing vcl_" << testname << " ... ";
   vcl_cout.flush();
 }
 
 void testresult( int testresult )
 {
-  result |= testresult;
   if ( testresult==0 ) {
+    ++passed;
     vcl_cout << "    PASSED" << vcl_endl;
   } else {
+    ++failed;
     vcl_cout << "  **FAILED**" << vcl_endl;
   }
 }
 
 // The else is for a trailing ; after the macro
-#define DO_TEST( Name, Func ) \
-  if( name == "" || name == Name ) { \
-    testname( Name ); \
-    testresult( Func ); \
+#define DO_TEST( Name ) \
+  if ( name == "" || name == "test_" #Name ) { \
+    testname( #Name ); \
+    testresult( test_##Name##_main(argc,argv) ); \
+    test_run = 1; \
   } else
 
 int main( int argc, char* argv[] )
 {
-  result = 0;
-  vcl_string name;
- 
-  if( argc <= 1 )
-    name = "";
-  else {
+  int test_run = 0;
+  passed = failed = 0;
+  vcl_string name = "";
+
+  if ( argc > 1 ) {
     name = argv[1];
     ++argv;
     --argc;
   }
 
-  
-  DO_TEST( "test_algorithm", test_algorithm_main() );
 
-  DO_TEST( "test_cctype",  test_cctype_main() );
+  DO_TEST( algorithm );
 
-  DO_TEST( "test_cmath", test_cmath_main() );
+  DO_TEST( cctype );
 
-  DO_TEST( "test_compiler", test_compiler_main() );
+  DO_TEST( cmath );
 
-  DO_TEST( "test_complex", test_complex_main() );
+  DO_TEST( compiler );
 
-  DO_TEST( "test_deque", test_deque_main() );
+  DO_TEST( complex );
 
-  DO_TEST( "test_exception", test_exception_main() );
+  DO_TEST( cstdio );
 
-  DO_TEST( "test_fstream", test_fstream_main() );
+  DO_TEST( deque );
 
-  DO_TEST( "test_headers", test_headers_main(argc, argv) );
+  DO_TEST( exception );
 
-  DO_TEST( "test_iostream", test_iostream_main() );
+  DO_TEST( fstream );
 
-  DO_TEST( "test_iterator", test_iterator_main() );
+  DO_TEST( iostream );
 
-  DO_TEST( "test_list", test_list_main() );
+  DO_TEST( iterator );
 
-  DO_TEST( "test_map", test_map_main() );
+  DO_TEST( list );
 
-  DO_TEST( "test_multimap", test_multimap_main() );
+  DO_TEST( map );
 
-  DO_TEST( "test_new", test_new_main() );
+  DO_TEST( multimap );
 
-  DO_TEST( "test_set", test_set_main() );
+  DO_TEST( new );
 
-  DO_TEST( "test_stlfwd", test_stlfwd_main() );
+  DO_TEST( set );
 
-  DO_TEST( "test_string", test_string_main() );
+  DO_TEST( string );
 
-  DO_TEST( "test_vector", test_vector_main() );
+  DO_TEST( vector );
 
-  if( name == "" || name == "test_cstdio" ) {
-    testname( "test_cstdio" );
-    if ( argc < 2 ) {
-      vcl_cout << "No input file given for test_cstdio" << vcl_endl;
-      testresult( 1 );
-    } else {
-      testresult( test_cstdio_main( argv[1] ) );
-    }
+  if (test_run == 0)
+  {
+    vcl_cout << "Unsupported test " << name
+             << "; should first be added to test_driver.cxx\n";
   }
 
-  return result;
+  vcl_cout << "Test Summary: ";
+  if (failed > 0)
+    vcl_cout<<passed<<" tests succeeded, "<<failed<<" tests failed\t\t\t*****";
+  else if (passed > 1)
+    vcl_cout<<"All "<<passed<<" tests succeeded";
+  else
+    vcl_cout<<"All tests succeeded";
+  vcl_cout << "\n-----------------------------------------------------------------------------\n";
+
+  return failed;
 }
