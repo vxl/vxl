f8589e346af77e1157e38588ad762238e957c90f
Graham Vincent <grv@users.sourceforge.net>
2002-09-12 13:01:25 -0400

    added int versions of the gauss reduce methods

--- contrib/mul/mil/mil_gauss_reduce_2d.cxx
+++ contrib/mul/mil/mil_gauss_reduce_2d.cxx
@@ -79,6 +79,45 @@ void mil_gauss_reduce_2d(float* dest_im,
     }
 }
 
+
+//: Smooth and subsample single plane src_im in x to produce dest_im
+//  Applies 1-5-8-5-1 filter in x, then samples
+//  every other pixel.  Fills [0,(nx+1)/2-1][0,ny-1] elements of dest
+void mil_gauss_reduce_2d(int* dest_im,
+                         int d_x_step, int d_y_step,
+                         const int* src_im,
+                         int src_nx, int src_ny,
+                         int s_x_step, int s_y_step)
+{
+    int* d_row = dest_im;
+    const int* s_row = src_im;
+    int sxs2 = s_x_step*2;
+    int nx2 = (src_nx-3)/2;
+    for (int y=0;y<src_ny;++y)
+    {
+        // Set first element of row
+        *d_row = *s_row;
+        int * d = d_row + d_x_step;
+        const int* s = s_row + sxs2;
+        for (int x=0;x<nx2;++x)
+        {
+            // The 0.5 offset in the following ensures rounding
+            *d = int(0.5+ 0.05*s[-sxs2] +0.05 *s[sxs2]
+                          +0.25*s[-s_x_step]+0.25*s[s_x_step]
+                          +0.4*s[0]);
+
+            d += d_x_step;
+            s += sxs2;
+        }
+        // Set last elements of row
+        *d = *s;
+
+        d_row += d_y_step;
+        s_row += s_y_step;
+    }
+}
+
+
 //: Smooth and subsample single plane src_im in x to produce dest_im using 121 filter in x and y
 //  Smooths with a 3x3 filter and subsamples
 void mil_gauss_reduce_121_2d(vil_byte* dest_im,
@@ -212,6 +251,80 @@ void mil_gauss_reduce_121_2d(float* dest_im,
     int yhi = (src_ny-1)/2;
     float* dest_last_row = dest_im + yhi*d_y_step;
     const float* s_last = src_im + yhi*sys2;
+    for (int i=0;i<nx;++i)
+    {
+      dest_last_row[i]= *s_last;
+      s_last+=sxs2;
+    }
+  }
+}
+
+
+
+
+//: Smooth and subsample single plane src_im in x to produce dest_im using 121 filter in x and y
+//  Smooths with a 3x3 filter and subsamples
+void mil_gauss_reduce_121_2d(int* dest_im,
+                             int d_x_step, int d_y_step,
+                             const int* src_im,
+                             int src_nx, int src_ny,
+                             int s_x_step, int s_y_step)
+{
+  int sxs2 = s_x_step*2;
+  int sys2 = s_y_step*2;
+  int* d_row = dest_im+d_y_step;
+  const int* s_row1 = src_im + s_y_step;
+  const int* s_row2 = s_row1 + s_y_step;
+  const int* s_row3 = s_row2 + s_y_step;
+  int nx2 = (src_nx-2)/2;
+  int ny2 = (src_ny-2)/2;
+  for (int y=0;y<ny2;++y)
+  {
+      // Set first element of row
+      *d_row = *s_row2;
+      int * d = d_row + d_x_step;
+      const int* s1 = s_row1 + sxs2;
+      const int* s2 = s_row2 + sxs2;
+      const int* s3 = s_row3 + sxs2;
+      for (int x=0;x<nx2;++x)
+      {
+          // The following is a little inefficient - could group terms to reduce arithmetic
+          // Add 0.5 so that truncating effetively rounds
+          *d = int( 0.0625f * s1[-s_x_step] + 0.125f * s1[0] + 0.0625f * s1[s_x_step]
+                       + 0.1250f * s2[-s_x_step] + 0.250f * s2[0] + 0.1250f * s2[s_x_step]
+                       + 0.0625f * s3[-s_x_step] + 0.125f * s3[0] + 0.0625f * s3[s_x_step] +0.5);
+
+          d += d_x_step;
+          s1 += sxs2;
+          s2 += sxs2;
+          s3 += sxs2;
+      }
+      // Set last elements of row
+      if (src_nx%2==1)
+        *d = *s2;
+
+      d_row += d_y_step;
+      s_row1 += sys2;
+      s_row2 += sys2;
+      s_row3 += sys2;
+  }
+
+  // Need to set first and last rows as well
+
+  // Dest image should be (src_nx+1)/2 x (src_ny+1)/2
+  const int* s0 = src_im;
+  int nx=(src_nx+1)/2;
+  for (int i=0;i<nx;++i)
+  {
+    dest_im[i]= *s0;
+    s0+=sxs2;
+  }
+ 
+  if (src_ny%2==1)
+  {
+    int yhi = (src_ny-1)/2;
+    int* dest_last_row = dest_im + yhi*d_y_step;
+    const int* s_last = src_im + yhi*sys2;
     for (int i=0;i<nx;++i)
     {
       dest_last_row[i]= *s_last;
--- contrib/mul/mil/mil_gauss_reduce_2d.h
+++ contrib/mul/mil/mil_gauss_reduce_2d.h
@@ -40,6 +40,23 @@ void mil_gauss_reduce_2d(float* dest_im,
                          int src_nx, int src_ny,
                          int s_x_step, int s_y_step);
 
+
+
+//: Smooth and subsample single plane src_im in x to produce dest_im
+//  Applies 1-5-8-5-1 filter in x, then samples
+//  every other pixel.  Fills [0,(nx+1)/2-1][0,ny-1] elements of dest
+//  Assumes dest_im has suffient data allocated.
+//
+//  This is essentially a utility function, used by mil_gauss_pyramid_builder
+//
+//  By applying twice we can obtain a full gaussian smoothed and
+//  sub-sampled 2D image (see mil_gauss_pyramid_builder)
+void mil_gauss_reduce_2d(int* dest_im,
+                         int d_x_step, int d_y_step,
+                         const int* src_im,
+                         int src_nx, int src_ny,
+                         int s_x_step, int s_y_step);
+
 //: Smooth and subsample single plane src_im in x to produce dest_im using 121 filter in x and y
 //  Smooths with a 3x3 filter and subsamples
 void mil_gauss_reduce_121_2d(vil_byte* dest_im,
@@ -55,5 +72,15 @@ void mil_gauss_reduce_121_2d(float* dest_im,
                          const float* src_im,
                          int src_nx, int src_ny,
                          int s_x_step, int s_y_step);
+
+
+//: Smooth and subsample single plane src_im in x to produce dest_im using 121 filter in x and y
+//  Smooths with a 3x3 filter and subsamples
+void mil_gauss_reduce_121_2d(int* dest_im,
+                         int d_x_step, int d_y_step,
+                         const int* src_im,
+                         int src_nx, int src_ny,
+                         int s_x_step, int s_y_step);
+
 
 #endif // mil_gauss_reduce_2d_h_
