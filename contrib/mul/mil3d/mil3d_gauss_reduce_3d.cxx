719be5cd5353cd84ee081813297a2ba31dcb5b73
Graham Vincent <grv@users.sourceforge.net>
2002-09-12 13:04:18 -0400

    added int versions of the gauss reduce methods

--- contrib/mul/mil3d/mil3d_gauss_reduce_3d.cxx
+++ contrib/mul/mil3d/mil3d_gauss_reduce_3d.cxx
@@ -43,3 +43,22 @@ void mil3d_gauss_reduce_3d(float* dest_im,
   }
 }
 
+
+//: Smooth and subsample single plane src_im in x to produce dest_im
+//  Applies 1-5-8-5-1 filter in x, then samples every other pixel.
+//  Fills [0,(nx+1)/2-1][0,ny-1][0,nz-1] elements of dest.
+void mil3d_gauss_reduce_3d(int* dest_im,
+                           int d_x_step, int d_y_step, int d_z_step,
+                           const int* src_im,
+                           int src_nx, int src_ny, int src_nz,
+                           int s_x_step, int s_y_step, int s_z_step)
+{
+  for (int z=0;z<src_nz;++z)
+  {
+    mil_gauss_reduce_2d(dest_im,d_x_step, d_y_step,
+                        src_im, src_nx,src_ny, s_x_step,s_y_step);
+    dest_im += d_z_step;
+    src_im  += s_z_step;
+  }
+}
+
--- contrib/mul/mil3d/mil3d_gauss_reduce_3d.h
+++ contrib/mul/mil3d/mil3d_gauss_reduce_3d.h
@@ -40,4 +40,21 @@ void mil3d_gauss_reduce_3d(float* dest_im,
                          int src_nx, int src_ny, int src_nz,
                          int s_x_step, int s_y_step, int s_z_step);
 
+
+//: Smooth and subsample single plane src_im in x to produce dest_im
+//  Applies 1-5-8-5-1 filter in x, then samples
+//  every other pixel.  Fills [0,(nx+1)/2-1][0,ny-1][0,nz-1] elements of dest
+//  Assumes dest_im has suffient data allocated.
+//
+//  This is essentially a utility function, used by mil3d_gauss_pyramid_builder_3d
+//
+//  By applying three times we can obtain a full gaussian smoothed and
+//  sub-sampled 3D image (see mil3d_gauss_pyramid_builder_3d)
+void mil3d_gauss_reduce_3d(int* dest_im,
+                         int d_x_step, int d_y_step, int d_z_step,
+                         const int* src_im,
+                         int src_nx, int src_ny, int src_nz,
+                         int s_x_step, int s_y_step, int s_z_step);
+
+
 #endif // mil3d_gauss_reduce_3d_h_
