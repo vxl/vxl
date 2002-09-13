d4db4a97bfb5ead2aa935dac77ffb5eb66cd7bf7
Graham Vincent <grv@users.sourceforge.net>
2002-09-13 04:47:34 -0400

    template instantiation of binary io of vcl_vector of vbl_triple of ints

---
+++ core/vbl/io/Templates/vsl_vector_io+vcl_vector+vbl_triple+int.int.int---.cxx
@@ -0,0 +1,5 @@
+#include <vsl/vsl_vector_io.txx>
+#include <vbl/vbl_triple.txx>
+#include <vbl/io/vbl_io_triple.txx>
+typedef vcl_vector<vbl_triple<int,int,int> > vector_triple_iii;
+VSL_VECTOR_IO_INSTANTIATE(vector_triple_iii);
