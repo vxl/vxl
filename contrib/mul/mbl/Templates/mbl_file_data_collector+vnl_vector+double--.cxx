735009f1d4af53aed7f38624746b4286e0d057ac
David Cristinacce <dcristinacce@users.sourceforge.net>
2002-09-16 07:35:04 -0400

    template instansiations for mbl_file_data_collector test program

---
+++ contrib/mul/mbl/Templates/mbl_file_data_collector+vnl_vector+double--.cxx
@@ -0,0 +1,3 @@
+#include <mbl/mbl_file_data_collector.txx>
+#include <vnl/io/vnl_io_vector.h>
+MBL_FILE_DATA_COLLECTOR_INSTANTIATE(vnl_vector<double> );
---
+++ contrib/mul/mbl/Templates/mbl_file_data_wrapper+double-.cxx
@@ -0,0 +1,2 @@
+#include<mbl/mbl_file_data_wrapper.txx>
+MBL_FILE_DATA_WRAPPER_INSTANTIATE(double);
