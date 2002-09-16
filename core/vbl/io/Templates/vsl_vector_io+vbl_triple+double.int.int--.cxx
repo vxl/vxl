1206bdbf859fe5de24c5f50eb593d31bb86a2107
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-16 12:45:47 -0400

    missing template instantiation

---
+++ core/vbl/io/Templates/vsl_vector_io+vbl_triple+double.int.int--.cxx
@@ -0,0 +1,5 @@
+#include <vsl/vsl_vector_io.txx>
+#include <vbl/vbl_triple.txx>
+#include <vbl/io/vbl_io_triple.txx>
+typedef vbl_triple<double,int,int> vbl_triple_dii; 
+VSL_VECTOR_IO_INSTANTIATE(vbl_triple_dii);
