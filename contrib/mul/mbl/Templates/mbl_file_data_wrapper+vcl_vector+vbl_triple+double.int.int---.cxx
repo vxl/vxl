9c6a24a8581ea462180a3cbdf679bc529e92e0d0
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-17 03:08:13 -0400

    splitting up and renaming instantiation file

---
+++ contrib/mul/mbl/Templates/mbl_file_data_wrapper+vcl_vector+vbl_triple+double.int.int---.cxx
@@ -0,0 +1,6 @@
+#include <vbl/vbl_triple.h>
+#include <vsl/vsl_vector_io.h>
+#include <mbl/mbl_file_data_wrapper.txx>
+
+typedef vcl_vector<vbl_triple<double,int,int> > vec_triple_dii;
+MBL_FILE_DATA_WRAPPER_INSTANTIATE( vec_triple_dii );
