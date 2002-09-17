8890ae264b7e3cd54063fe8ce9a04d42b540054b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-17 11:54:58 -0400

    make sure test_exception_main returns 0 on success

--- vcl/tests/test_vcl_exception.cxx
+++ vcl/tests/test_vcl_exception.cxx
@@ -1,19 +1,21 @@
 #include <vcl_exception.h>
 #include <vcl_iostream.h>
-#include <vcl_cstdlib.h>
 
 int test_vcl_exception_main()
 {
+  const char *ex = "\"const char* exception\"";
   int result = 0;
 #if VCL_HAS_EXCEPTIONS
   vcl_try {
-    vcl_throw "\"const char* exception\"";
+    vcl_cout << "throw " << ex << vcl_endl;
+    vcl_throw ex;
   }
   vcl_catch (const char* e) {
-    vcl_cout << "caught " << e << ". Good.\n";
+    vcl_cout << "caught " << e << ".  Good.\n";
+    result = 0;
   }
   vcl_catch_all {
-    vcl_cout << "caught nuffink. Bad.\n";
+    vcl_cout << "caught nothing.  Bad.\n";
     result = 1;
   }
 #else
