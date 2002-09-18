8166069c908020cad02f9663717732ae57430e29
Fred Wheeler <fred_wheeler@users.sourceforge.net>
2002-09-18 13:13:13 -0400

    copy constructor now actually copies values instead of setting defaults

--- contrib/gel/vgel/vgel_kl_params.cxx
+++ contrib/gel/vgel/vgel_kl_params.cxx
@@ -16,27 +16,25 @@ vgel_kl_params::~vgel_kl_params()
 
 vgel_kl_params::vgel_kl_params(const vgel_kl_params & params)
 {
-  // A faire
-  numpoints =params.numpoints;
-  search_range = params.search_range ;
-
-  mindist = 10;
-  window_width = 7;
-  window_height = 7;
-  min_eigenvalue = 1;
-  min_determinant = (float)0.01;
-  min_displacement = (float)0.1;
-  max_iterations = 10;
-  max_residue = 10.0;
-  grad_sigma = 1.0;
-  smooth_sigma_fact = (float)0.1;
-  pyramid_sigma_fact = (float)0.9;
-  replaceLostPoints = true;
-  sequentialMode = false;
-  smoothBeforeSelecting = true;
-  writeInternalImages = false;
-  nSkippedPixels = 0;
-  verbose = true;
+  numpoints = params.numpoints;
+  search_range = params.search_range;
+  mindist = params.mindist;
+  window_width = params.window_width;
+  window_height = params.window_height;
+  min_eigenvalue = params.min_eigenvalue;
+  min_determinant = params.min_determinant;
+  min_displacement = params.min_displacement;
+  max_iterations = params.max_iterations;
+  max_residue = params.max_residue;
+  grad_sigma = params.grad_sigma;
+  smooth_sigma_fact = params.smooth_sigma_fact;
+  pyramid_sigma_fact = params.pyramid_sigma_fact;
+  replaceLostPoints = params.replaceLostPoints;
+  sequentialMode = params.sequentialMode;
+  smoothBeforeSelecting = params.smoothBeforeSelecting;
+  writeInternalImages = params.writeInternalImages;
+  nSkippedPixels = params.nSkippedPixels;
+  verbose = params.verbose;
 }
 
 
