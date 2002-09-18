244a8c1d64a41e4188446a00ba2e1fb2ac1040c0
Ian Scott <scottim@users.sourceforge.net>
2002-09-18 06:30:05 -0400

    fix template instantiation naming

---
+++ contrib/mul/clsfy/Templates/vbl_array_2d+clsfy_binary_threshold_1d-.cxx
@@ -0,0 +1,3 @@
+#include <vbl/vbl_array_2d.txx>
+#include <clsfy/clsfy_binary_threshold_1d.h>
+VBL_ARRAY_2D_INSTANTIATE(clsfy_binary_threshold_1d);
---
+++ contrib/mul/clsfy/Templates/vbl_io_array_2d+clsfy_binary_threshold_1d-.cxx
@@ -0,0 +1,3 @@
+#include <vbl/io/vbl_io_array_2d.txx>
+#include <clsfy/clsfy_binary_threshold_1d.h>
+VBL_IO_ARRAY_2D_INSTANTIATE(clsfy_binary_threshold_1d);
