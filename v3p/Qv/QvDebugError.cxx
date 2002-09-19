0735c2ac5036bd439345b9d96bee03d4d3455ea6
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-19 03:55:56 -0400

    No need to conditionally #include <stdarg>

--- v3p/Qv/QvDebugError.cxx
+++ v3p/Qv/QvDebugError.cxx
@@ -1,16 +1,7 @@
-#ifdef SUN4
-# include <varargs.h>
-#else  /* makedepend does not understand || */
-# ifdef SUN4_GNU
-#  include <varargs.h>
-# else
-#  include <vcl_cstdarg.h>
-# endif
-#endif
-
 #include "QvString.h"
 #include "QvDebugError.h"
 #include <vcl_iostream.h>
+#include <vcl_cstdarg.h>
 
 QvDebugErrorCallback QvDebugError::callback_ = 0;  // mpichler, 19950713
 
--- v3p/Qv/QvReadError.cxx
+++ v3p/Qv/QvReadError.cxx
@@ -1,17 +1,8 @@
-#ifdef SUN4
-# include <varargs.h>
-#else  /* makedepend does not understand || */
-# ifdef SUN4_GNU
-#  include <varargs.h>
-# else
-#  include <vcl_cstdarg.h>
-# endif
-#endif
-
+#include "QvReadError.h"
 #include "QvString.h"
 #include "QvInput.h"
-#include "QvReadError.h"
 #include <vcl_iostream.h>
+#include <vcl_cstdarg.h>
 
 QvReadErrorCallback QvReadError::callback_ = 0;  // mpichler, 19950713
 
