6eb6ec2d4b731e0510e50971700a9397bdef2c5e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-19 04:18:04 -0400

    interface moved to new .h file

---
+++ contrib/oxl/oxp/oxp_vob_frame_index.h
@@ -0,0 +1,19 @@
+#ifndef oxp_vob_frame_index_h_
+#define oxp_vob_frame_index_h_
+
+#include <vcl_vector.h>
+
+struct oxp_vob_frame_index_entry {
+  int lba;
+  int frame;
+};
+ 
+struct oxp_vob_frame_index
+{
+  vcl_vector<oxp_vob_frame_index_entry> l;
+
+  bool load(char const* filename);
+  int frame_to_lba_of_prev_I_frame(int frame_number, int* f_actual = 0);
+};
+
+#endif // oxp_vob_frame_index_h_
