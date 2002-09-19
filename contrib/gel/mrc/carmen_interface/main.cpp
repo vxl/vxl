87da24ffd1f6d41a16b4ce38713f209c951d1e85
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-19 03:21:36 -0400

    Use command line arg if present

--- contrib/gel/mrc/carmen_interface/main.cpp
+++ contrib/gel/mrc/carmen_interface/main.cpp
@@ -1,56 +1,20 @@
-// <begin copyright notice>
-// ---------------------------------------------------------------------------
-//
-//                   Copyright (c) 1997 TargetJr Consortium
-//               GE Corporate Research and Development (GE CRD)
-//                             1 Research Circle
-//                            Niskayuna, NY 12309
-//                            All Rights Reserved
-//              Reproduction rights limited as described below.
-//
-//      Permission to use, copy, modify, distribute, and sell this software
-//      and its documentation for any purpose is hereby granted without fee,
-//      provided that (i) the above copyright notice and this permission
-//      notice appear in all copies of the software and related documentation,
-//      (ii) the name TargetJr Consortium (represented by GE CRD), may not be
-//      used in any advertising or publicity relating to the software without
-//      the specific, prior written permission of GE CRD, and (iii) any
-//      modifications are clearly marked and summarized in a change history
-//      log.
-//
-//      THE SOFTWARE IS PROVIDED "AS IS" AND WITHOUT WARRANTY OF ANY KIND,
-//      EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
-//      WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
-//      IN NO EVENT SHALL THE TARGETJR CONSORTIUM BE LIABLE FOR ANY SPECIAL,
-//      INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY KIND OR ANY
-//      DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
-//      WHETHER OR NOT ADVISED OF THE POSSIBILITY OF SUCH DAMAGES, OR ON
-//      ANY THEORY OF LIABILITY ARISING OUT OF OR IN CONNECTION WITH THE
-//      USE OR PERFORMANCE OF THIS SOFTWARE.
-//
-// ---------------------------------------------------------------------------
-// <end copyright notice>
 #include "carmen_interface.h"
 #include <vcl_string.h>
-#include <vcl_iosfwd.h>
-#include <vcl_vector.h>
-#include <vcl_utility.h>
-
-//using namespace std;
+#include <vcl_iostream.h>
 
 int main(int argc, char ** argv)
 {
-  carmen_interface ci;
   vcl_string path = "./egemen1.cm";
-  vcl_vector<double> vvect();
-//  vcl_string s = "try out this";
-  vcl_cout << path << vcl_endl;
+  if (argc > 1) path = vcl_string(argv[1]);
+  vcl_cout << "loading correspondence file " << path << vcl_endl;
 
+  carmen_interface ci;
   ci.set_carmen_camera(1);
   ci.load_correspondence_file(path);
   ci.solve();
   ci.print_results();
-  ci.save_camera("egemen_out1");
+  path += ".out1";
+  ci.save_camera(path.c_str());
 
   return 0;
 }
