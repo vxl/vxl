1b25a0ff91e895ae69046efb9d2f44c49ba3f897
Tim Cootes <timcootes@users.sourceforge.net>
2002-09-24 04:31:59 -0400

    Added class

---
+++ core/vil/vil_memory_chunk.cxx
@@ -0,0 +1,59 @@
+//: \file
+//  \brief Ref. counted block of data on the heap
+//  \author Tim Cootes
+
+#include <vil2/vil2_memory_chunk.h>
+#include <vcl_cstring.h>
+
+//: Dflt ctor
+vil2_memory_chunk::vil2_memory_chunk()
+: data_(0), size_(0), ref_count_(0)
+{
+}
+
+//: Destructor
+vil2_memory_chunk::~vil2_memory_chunk()
+{
+  delete [] (char*) data_;
+}
+
+//: Copy ctor
+vil2_memory_chunk::vil2_memory_chunk(const vil2_memory_chunk& d)
+: data_(0), size_(0), ref_count_(0)
+{
+  *this=d;
+}
+
+//: Copy operator
+vil2_memory_chunk& vil2_memory_chunk::operator=(const vil2_memory_chunk& d)
+{
+  if (this==&d) return *this;
+  
+  resize(d.size());
+  vcl_memcpy(data_,d.data_,size_);
+  return *this;
+}
+
+//: Decrement reference count
+void vil2_memory_chunk::unref()
+{
+  ref_count_--;
+  if (ref_count_==0)
+  {
+    delete [] (char*) data_; data_=0;
+    delete this;
+  }
+}
+
+//: Create space for n elements
+void vil2_memory_chunk::resize(int n)
+{
+  if (size_==(unsigned int)n) return;
+  delete [] (char*) data_;
+  data_ = 0;
+  if (n>0)
+    data_ = new char[n];
+  size_ = n;
+}
+
+
---
+++ core/vil/vil_memory_chunk.h
@@ -0,0 +1,55 @@
+// This is mul/vil2/vil2_memory_chunk.h
+#ifndef vil2_memory_chunk_h_
+#define vil2_memory_chunk_h_
+//:
+//  \file
+//  \brief Ref. counted block of data on the heap
+//  \author Tim Cootes
+
+//: Ref. counted block of data on the heap
+//  Image data block used by vil2_image_view<T>.
+class vil2_memory_chunk
+{
+    //: Data
+    void *data_;
+
+    //: Number of elements
+    unsigned int size_;
+
+    //: Reference count
+    int ref_count_;
+
+ public:
+    //: Dflt ctor
+    vil2_memory_chunk();
+
+    //: Copy ctor
+    vil2_memory_chunk(const vil2_memory_chunk&);
+
+    //: Copy operator
+    vil2_memory_chunk& operator=(const vil2_memory_chunk&);
+
+    //: Destructor
+    virtual ~vil2_memory_chunk();
+
+    //: Increment reference count
+    void ref() { ref_count_++; }
+
+    //: Decrement reference count
+    void unref();
+
+    //: Number of objects refering to this data
+    int ref_count() const { return ref_count_; }
+
+    //: Pointer to first element of data
+    void* data() { return data_;}
+
+    //: Number of bytes allocated
+    unsigned int size() const { return size_; }
+
+    //: Create space for n elements
+    void resize(int n);
+};
+
+#endif
+
