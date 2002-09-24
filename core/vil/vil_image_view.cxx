5f41f6c6fdfcb17765f8c3014f66c01b6b2c33b6
Ian Scott <scottim@users.sourceforge.net>
2002-09-24 18:05:35 -0400

    Added lots of new pixel types for image views

--- core/vil/vil_image_view.cxx
+++ core/vil/vil_image_view.cxx
@@ -14,6 +14,14 @@ template<> vcl_string vil2_image_view<vil_byte>::is_a() const
 template<> vcl_string vil2_image_view<int>::is_a() const
 {  return vcl_string("vil2_image_view<int>"); }
 
+// Specialise the is_a() for bool
+template<> vcl_string vil2_image_view<bool>::is_a() const
+{  return vcl_string("vil2_image_view<bool>"); }
+
+// Specialise the is_a() for uint
+template<> vcl_string vil2_image_view<unsigned int>::is_a() const
+{  return vcl_string("vil2_image_view<uint>"); }
+
 // Specialise the is_a() for float
 template<> vcl_string vil2_image_view<float>::is_a() const
 {  return vcl_string("vil2_image_view<float>"); }
@@ -25,6 +33,10 @@ template<> vcl_string vil2_image_view<signed char>::is_a() const
 // Specialise the is_a() for short
 template<> vcl_string vil2_image_view<short>::is_a() const
 {  return vcl_string("vil2_image_view<short>"); }
+
+// Specialise the is_a() for ushort
+template<> vcl_string vil2_image_view<unsigned short>::is_a() const
+{  return vcl_string("vil2_image_view<ushort>"); }
 
 // Specialise the is_a() for vil_rgb<vil_byte>
 template<> vcl_string vil2_image_view<vil_rgb<vil_byte> >::is_a() const
