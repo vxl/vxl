4e362e6478ead98479c05488ddc32298967a8b97
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-25 03:57:35 -0400

    more missing template instantiations

---
+++ core/vil/Templates/vil_smart_ptr+vil_memory_chunk-.cxx
@@ -0,0 +1,3 @@
+#include <vil2/vil2_smart_ptr.txx>
+#include <vil2/vil2_memory_chunk.h>
+VIL2_SMART_PTR_INSTANTIATE(vil2_memory_chunk);
--- core/vil/vil_image_view.cxx
+++ core/vil/vil_image_view.cxx
@@ -5,43 +5,25 @@
 #include <vil2/vil2_image_view.h>
 #include <vil/vil_byte.h>
 #include <vil/vil_rgb.h>
+#include <vcl_config_compiler.h>
 
-// Specialise the is_a() for vil_byte
-template<> vcl_string vil2_image_view<vil_byte>::is_a() const
-{  return vcl_string("vil2_image_view<uchar>"); }
+// Specialise the is_a() method for all possible types
+#define macro(T) \
+VCL_DEFINE_SPECIALIZATION vcl_string vil2_image_view<T >::is_a() const \
+{  return vcl_string("vil2_image_view<T>"); }
 
-// Specialise the is_a() for int
-template<> vcl_string vil2_image_view<int>::is_a() const
-{  return vcl_string("vil2_image_view<int>"); }
+macro(bool)
+macro(unsigned char)
+macro(signed char)
+macro(short)
+macro(unsigned short)
+macro(int)
+macro(unsigned int)
+macro(float)
+macro(vil_rgb<vil_byte> )
+macro(vil_rgb<int> )
+macro(vil_rgb<unsigned int> )
+macro(vil_rgb<short> )
+macro(vil_rgb<unsigned short> )
 
-// Specialise the is_a() for bool
-template<> vcl_string vil2_image_view<bool>::is_a() const
-{  return vcl_string("vil2_image_view<bool>"); }
-
-// Specialise the is_a() for uint
-template<> vcl_string vil2_image_view<unsigned int>::is_a() const
-{  return vcl_string("vil2_image_view<uint>"); }
-
-// Specialise the is_a() for float
-template<> vcl_string vil2_image_view<float>::is_a() const
-{  return vcl_string("vil2_image_view<float>"); }
-
-// Specialise the is_a() for char
-template<> vcl_string vil2_image_view<signed char>::is_a() const
-{  return vcl_string("vil2_image_view<schar>"); }
-
-// Specialise the is_a() for short
-template<> vcl_string vil2_image_view<short>::is_a() const
-{  return vcl_string("vil2_image_view<short>"); }
-
-// Specialise the is_a() for ushort
-template<> vcl_string vil2_image_view<unsigned short>::is_a() const
-{  return vcl_string("vil2_image_view<ushort>"); }
-
-// Specialise the is_a() for vil_rgb<vil_byte>
-template<> vcl_string vil2_image_view<vil_rgb<vil_byte> >::is_a() const
-{  return vcl_string("vil2_image_view<vil_rgb<vil_byte>>"); }
-
-
-
-
+#undef macro
