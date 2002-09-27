3c388c0c3187ce33f36f0594fe8cec1a0d5e5081
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-27 08:06:24 -0400

    avoid variable names starting with underscore

--- contrib/gel/vsrl/vsrl_token.cxx
+++ contrib/gel/vsrl/vsrl_token.cxx
@@ -1,65 +1,59 @@
-// In a dynamic progamming problem we are given the task of 
+// This is gel/vsrl/vsrl_token.cxx
+#include "vsrl_token.h"
+//:
+// \file
+// In a dynamic progamming problem we are given the task of
 // assigning one set of tokens to another set of tokens.
 // This class represents the token.
 
-#include <vsrl/vsrl_token.h>
-
-
-// constructor
-
 vsrl_token::vsrl_token()
 {
-  _index = 0-1;
-  _assigned_token = 0;
-  _x=0;
-  _y=0;
+  index_ = 0-1;
+  assigned_token_ = 0;
+  x_=0;
+  y_=0;
 }
 
-// destructor
-  
 vsrl_token::~vsrl_token()
 {
-  
 }
 
-// get the index of this token
+//: get the index of this token
 int vsrl_token::get_index()
 {
-  return _index;
+  return index_;
 }
 
-// set the index of this token
+//: set the index of this token
 void vsrl_token::set_index(int index)
 {
-  _index = index;
+  index_ = index;
 }
 
-// get the assigned token 
-
+//: get the assigned token
 vsrl_token* vsrl_token::get_assigned_token()
 {
-  return _assigned_token;
+  return assigned_token_;
 }
 
-// set the assigned token 
-
+//: set the assigned token
 void vsrl_token::set_assigned_token(vsrl_token *tok)
 {
-  _assigned_token = tok;
+  assigned_token_ = tok;
 }
 
-// set the position
-
+//: set the position
 void vsrl_token::set_position(double x, double y)
 {
-  _x=x;
-  _y=y;
+  x_=x;
+  y_=y;
 }
 
+//: get the position
 void vsrl_token::get_position(double &x, double &y)
 {
-  x= _x;
-  y= _y;
+  x= x_;
+  y= y_;
 }
 
 //: what is the direct cost of assigning this token to tok
@@ -68,14 +62,9 @@ double vsrl_token::cost(vsrl_token *tok)
   return 0;
 }
 
-//: what is the incremental cost of going from from this -> tok2 given 
-//: the assignment going from tok_p -> tok_2p
-
+//: what is the incremental cost of going from from this to tok2 given the assignment going from tok_p to tok_2p
 double vsrl_token::incremental_cost(vsrl_token *tok2, vsrl_token *tok_p, vsrl_token *tok_2p)
 {
   return 0;
 }
-
-
-
 
--- contrib/gel/vsrl/vsrl_token.h
+++ contrib/gel/vsrl/vsrl_token.h
@@ -1,3 +1,4 @@
+// This is gel/vsrl/vsrl_token.h
 #ifndef vsrl_token_h
 #define vsrl_token_h
 //:
@@ -6,13 +7,14 @@
 // assigning one set of tokens to another set of tokens.
 // This class represents the abstract token.
 
-#include <vcl_cmath.h>
-#include <vcl_cstddef.h>
-#include <vcl_cstdio.h>
-#include <vcl_cstdlib.h>
-
 class vsrl_token
 {
+  int index_;
+  vsrl_token* assigned_token_;
+
+  double x_; // the x position
+  double y_; // the y position
+
  public:
 
   // constructor
@@ -21,50 +23,39 @@ class vsrl_token
   // destructor
   virtual ~vsrl_token();
 
-  // does this token represent a null assignment?
+  //: does this token represent a null assignment?
   virtual bool null_token() {return false;}
 
-  // is this token an intensity token ?
-
+  //: is this token an intensity token ?
   virtual bool intensity_token() {return false;}
 
-  // what is the direct cost of assigning this token to tok
+  //: what is the direct cost of assigning this token to tok
   virtual double cost(vsrl_token *tok);
 
-  //: what is the incremental cost of going from from this -> tok2 given 
-  //: the assignment going from tok_p -> tok_2p
-  
+  //: what is the incremental cost of going from from this to tok2 given the assignment going from tok_p to tok_2p
   virtual double incremental_cost(vsrl_token *tok2, vsrl_token *tok_p, vsrl_token *tok_2p);
 
-  // get the index of this token
+  //: get the index of this token
   int get_index();
 
-  // set the id of this token
+  //: set the id of this token
   void set_index(int index);
 
-  // get the assigned token
+  //: get the assigned token
   vsrl_token* get_assigned_token();
 
-  // set the assigned token
+  //: set the assigned token
   void set_assigned_token(vsrl_token *tok);
 
-  // set the x and y position
+  //: set the x and y position
 
   void set_position(double x, double y);
 
-  // get the x and y position
-
+  //: get the x and y position
   void get_position(double &x, double &y);
 
-  double get_x() {return _x;}
-  double get_y() {return _y;}
-
- private:
-  int _index;
-  vsrl_token* _assigned_token;
-
-  double _x; // the x position
-  double _y; // the y position
+  double get_x() {return x_;}
+  double get_y() {return y_;}
 };
 
 #endif // vsrl_token_h
