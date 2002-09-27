cbc23e514aa95b42ff383ce85fd92fdefd611800
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-27 03:26:02 -0400

    switching from MPEG to MPEG2

--- core/vidl/config.mk
+++ core/vidl/config.mk
@@ -1,4 +1,8 @@
 IULIBS += -lvidl
 include ${IUEROOT}/vxl/vul/config.mk
 include ${IUEROOT}/vxl/vil/config.mk
-TRY_MPEG := 1
+TRY_MPEG2 := 1
+include ${IUEROOT}/config/try.mk
+ifeq ($(HAS_MPEG2),1)
+IULIBS += $(MPEG2_LIB_DIR)
+endif
