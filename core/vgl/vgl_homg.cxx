15fddcbca6df663f8b0257c430f8bbfa85eb3fc3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-27 04:10:09 -0400

    avoid using nonstandard HUGE_VAL

--- core/vgl/vgl_homg.cxx
+++ core/vgl/vgl_homg.cxx
@@ -4,16 +4,12 @@
 #endif
 
 #include "vgl_homg.h"
-#include <vcl_cmath.h>
-#ifdef VCL_SUNPRO_CC_50
-# include <math.h> // dont_vxl_filter: no HUGE_VAL in <cmath>
-#endif
 
-float vgl_homg<float>::infinity = (float)HUGE_VAL;
+float vgl_homg<float>::infinity = 3.4028234663852886e+38f;
 float vgl_homg<float>::infinitesimal_tol = 1e-12f;
 
-double vgl_homg<double>::infinity = HUGE_VAL;
+double vgl_homg<double>::infinity = 1.7976931348623157e+308;
 double vgl_homg<double>::infinitesimal_tol = 1e-12;
 
-long double vgl_homg<long double>::infinity = HUGE_VAL;
+long double vgl_homg<long double>::infinity = 1.7976931348623157e+308;
 long double vgl_homg<long double>::infinitesimal_tol = 1e-12;
