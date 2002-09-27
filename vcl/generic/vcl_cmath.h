deca1c9585540aed0cb1294132f40f0ad9df6f07
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-27 15:15:41 -0400

    added hypot()

--- vcl/generic/vcl_cmath.h
+++ vcl/generic/vcl_cmath.h
@@ -95,5 +95,9 @@
 #ifndef vcl_tanh
 #define vcl_tanh vcl_generic_cmath_STD :: tanh
 #endif
+// hypot
+#ifndef vcl_hypot
+#define vcl_hypot vcl_generic_cmath_STD :: hypot
+#endif
 
 #endif // vcl_generic_cmath_h_
