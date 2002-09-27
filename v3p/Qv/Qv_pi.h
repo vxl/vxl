6d7a76ee7fedea6375a0aa8227f88c48cfbe977f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-27 08:06:10 -0400

    added QV_ONE_OVER_PI

--- v3p/Qv/Qv_pi.h
+++ v3p/Qv/Qv_pi.h
@@ -3,5 +3,6 @@
 
 #define QV_PI   3.14159265358979323846  // M_PI is not iso standard
 #define QV_PI_4 0.78539816339744830962  // M_PI_4 is not iso standard
+#define QV_ONE_OVER_PI 0.31830988618379067154
 
 #endif
