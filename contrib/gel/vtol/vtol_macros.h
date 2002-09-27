5cc71de3de8d056ebef1e9bc7ae50553abdea9f6
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-27 08:09:49 -0400

    old genman header replaced by Doxygen style header

--- contrib/gel/vtol/vtol_macros.h
+++ contrib/gel/vtol/vtol_macros.h
@@ -1,23 +1,27 @@
-// .SECTION Modifications
-// 05/13/98  RIH replaced append by insert_after to avoid n^2 behavior
-
+// This is gel/vtol/vtol_macros.h
 #ifndef MACROS_H
 #define MACROS_H
+//:
+// \file
+// \verbatim
+// Modifications
+// 05/13/98  RIH replaced append by insert_after to avoid n^2 behavior
+// \endverbatim
+
 #include <vcl_vector.h>
 #include <vcl_list.h>
-#include <vcl_algorithm.h>
 
 #define SEL_SUP(suptype,target)\
   vcl_vector<suptype *> *new_list=new vcl_vector<suptype *>();\
   vcl_vector<suptype *> *sublist;\
   vcl_list<vtol_topology_object_sptr>::iterator i;\
   vcl_vector<suptype*>::iterator m_i;\
-  for(i=_superiors.begin();i!=_superiors.end();i++)\
+  for (i=_superiors.begin();i!=_superiors.end();i++)\
       {\
        sublist=(*i)->target();\
-        if(sublist->size())\
+        if (sublist->size())\
         { \
-          for(m_i=sublist->begin();m_i!=sublist->end();m_i++){\
+          for (m_i=sublist->begin();m_i!=sublist->end();m_i++){\
                new_list->push_back(*m_i);\
           }\
         }\
@@ -29,13 +33,13 @@
 #define SEL_INF(inftype,target)\
   vcl_vector<inftype *> *new_list=new vcl_vector<inftype *>();\
   vcl_vector<inftype *> *sublist;\
-  for(topology_list::iterator i=_inferiors.begin();i!=_inferiors.end();i++)\
+  for (topology_list::iterator i=_inferiors.begin();i!=_inferiors.end();i++)\
      {\
      sublist=(*i)->target();\
-     if(sublist->size())\
+     if (sublist->size())\
        {\
         vcl_vector<inftype *>::iterator m_i;\
-          for(m_i=sublist->begin();m_i!=sublist->end();m_i++){\
+          for (m_i=sublist->begin();m_i!=sublist->end();m_i++){\
             new_list->push_back(*m_i);\
           }\
       }\
@@ -51,7 +55,7 @@
        { \
          templist = ((suptype *) ((*hi).ptr()))->target(); \
           vcl_vector<inftype*>::iterator m_i; \
-          for(m_i=templist->begin();m_i!=templist->end();m_i++){ \
+          for (m_i=templist->begin();m_i!=templist->end();m_i++){ \
             listnm->push_back(*m_i); \
           } \
          delete templist;\
@@ -63,11 +67,11 @@
 #define OUTSIDE_BOUNDARY(targettype, inftype, target) \
    vcl_vector<targettype*> *newlist = new vcl_vector<targettype*>(); \
    vcl_vector<targettype*> *templist;  \
-      for(topology_list::iterator i = _inferiors.begin(); i != _inferiors.end(); i++) \
+      for (topology_list::iterator i = _inferiors.begin(); i != _inferiors.end(); i++) \
          { \
         templist = ((inftype*)((*i).ptr()))->outside_boundary_##target(); \
         vcl_vector<targettype*>::iterator m_i; \
-        for(m_i=templist->begin();m_i!=templist->end();m_i++){ \
+        for (m_i=templist->begin();m_i!=templist->end();m_i++){ \
             newlist->push_back(*m_i); \
           } \
         delete templist;  \
@@ -84,7 +88,7 @@
 
 #define COPY_SUP(suptype)  \
    vcl_vector<suptype*> *new_list = new vcl_vector<suptype*>(); \
-   for(topology_list::iterator i = _superiors.begin(); i != _superiors.end(); i++) \
+   for (topology_list::iterator i = _superiors.begin(); i != _superiors.end(); i++) \
    {\
    new_list->push_back((suptype*)(*i)); \
    }\
@@ -92,7 +96,7 @@
 
 #define COPY_INF(inftype) \
    vcl_vector<inftype*> *new_list = new vcl_vector<inftype*>(); \
-      for(topology_list::iterator i = _inferiors.begin(); i != _inferiors.end(); i++) \
+      for (topology_list::iterator i = _inferiors.begin(); i != _inferiors.end(); i++) \
      {\
    new_list->push_back((inftype*)((*i).ptr())); \
    }\
