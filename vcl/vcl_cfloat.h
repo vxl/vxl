bfa5fc706f8ff30adf274f8a14103145e31481e0
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-09-29 17:47:33 -0400

    MAXFLOAT is nonstandard; FLT_MAX is standard

--- vcl/vcl_cfloat.h
+++ vcl/vcl_cfloat.h
@@ -13,58 +13,58 @@
 # include "iso/vcl_cfloat.h"
 #endif
 
-#ifndef MAXFLOAT
-# ifdef FLT_MAX
-#  define MAXFLOAT FLT_MAX
-# endif
-#endif
-#ifndef MAXDOUBLE
-# ifdef DBL_MAX
-#  define MAXDOUBLE DBL_MAX
-# endif
-#endif
-#ifndef MINFLOAT
-# ifdef FLT_MIN
-#  define MINFLOAT FLT_MIN
-# endif
-#endif
-#ifndef MINDOUBLE
-# ifdef DBL_MIN
-#  define MINDOUBLE DBL_MIN
-# endif
-#endif
-#ifndef MAXFLOAT
-# include <vcl_cmath.h>
-#endif
-#ifndef MAXFLOAT
-# include <values.h>
-#endif
-#ifndef MAXFLOAT
-# include <limits.h>
-#endif
-#ifndef MAXFLOAT
-# define MAXFLOAT 3.40282346638528860e38F
-#endif
-#ifndef MAXDOUBLE
-# define MAXDOUBLE 1.7976931348623157e308
-#endif
-#ifndef MINFLOAT
-# define MINFLOAT 1.1754943508222875e-38F
-#endif
-#ifndef MINDOUBLE
-# define MINDOUBLE 2.2250738585072014e-308
-#endif
 #ifndef FLT_MAX
-# define FLT_MAX MAXFLOAT
+# ifdef MAXFLOAT
+#  define FLT_MAX MAXFLOAT
+# endif
 #endif
 #ifndef DBL_MAX
-# define DBL_MAX MAXDOUBLE
+# ifdef MAXDOUBLE
+#  define DBL_MAX MAXDOUBLE
+# endif
 #endif
 #ifndef FLT_MIN
-# define FLT_MIN MINFLOAT
+# ifdef MINFLOAT
+#  define FLT_MIN MINFLOAT
+# endif
 #endif
 #ifndef DBL_MIN
-# define DBL_MIN MINDOUBLE
+# ifdef MINDOUBLE
+#  define DBL_MIN MINDOUBLE
+# endif
+#endif
+#ifndef FLT_MAX
+# include <vcl_cmath.h>
+#endif
+#ifndef FLT_MAX
+# include <values.h>
+#endif
+#ifndef FLT_MAX
+# include <limits.h>
+#endif
+#ifndef FLT_MAX
+# define FLT_MAX 3.40282346638528860e38F
+#endif
+#ifndef DBL_MAX
+# define DBL_MAX 1.7976931348623157e308
+#endif
+#ifndef FLT_MIN
+# define FLT_MIN 1.1754943508222875e-38F
+#endif
+#ifndef DBL_MIN
+# define DBL_MIN 2.2250738585072014e-308
+#endif
+#ifndef MAXFLOAT
+# define MAXFLOAT FLT_MAX
+#endif
+#ifndef MAXDOUBLE
+# define MAXDOUBLE DBL_MAX
+#endif
+#ifndef MINFLOAT
+# define MINFLOAT FLT_MIN
+#endif
+#ifndef MINDOUBLE
+# define MINDOUBLE DBL_MIN
 #endif
 
 #endif // vcl_cfloat_h_
