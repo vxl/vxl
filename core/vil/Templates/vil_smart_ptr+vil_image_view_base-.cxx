1ae390db5bcea4982bff8e7acd63e7295ba7b93e
Ian Scott <scottim@users.sourceforge.net>
2002-09-30 10:18:54 -0400

    missing files

---
+++ core/vil/Templates/vil_smart_ptr+vil_image_view_base-.cxx
@@ -0,0 +1,3 @@
+#include <vil2/vil2_smart_ptr.txx>
+#include <vil2/vil2_image_view_base.h>
+VIL2_SMART_PTR_INSTANTIATE(vil2_image_view_base);
---
+++ core/vil/examples/file_example3.cxx
@@ -0,0 +1,40 @@
+//:
+// \file
+// \brief Example of creating, processing and using an image data object
+// \author Ian Scott
+
+#include <vcl_iostream.h>
+#include <testlib/testlib_root_dir.h>
+#include <vil2/vil2_load.h>
+#include <vil2/vil2_save.h>
+#include <vil2/vil2_image_view.h>
+#include <vil2/vil2_image_view_functions.h>
+
+
+int main(int argc, char** argv)
+{
+  vcl_string filename = testlib_root_dir() + "/vxl/vil/tests/file_read_data/ff_grey8bit_raw.pgm";
+  vcl_cout<<"Load " << filename << " into an image data object." << vcl_endl;
+
+
+  // This is how we quickly load an image view.
+
+  vil2_image_view<vil_byte> im = vil2_load(filename.c_str());
+
+
+  vil2_print_all(vcl_cout, im);
+
+  // We can apply some operation to it.
+
+
+
+  // And the we save it.
+
+
+  vil2_save(im, "test.pbm");
+
+
+  //don't forget to delete created view,
+
+  return 0;
+}
