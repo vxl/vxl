eaa60f77774b27168aa4a3c34946ba8b859f95c2
Karen McGaul <mcgaul@users.sourceforge.net>
2002-10-02 12:40:57 -0400

    KYM moved vgui_slot_data to vgui_parent_child_link_data

---
+++ core/vgui/internals/vgui_parent_child_link_data.cxx
@@ -0,0 +1,6 @@
+// This is oxl/vgui/internals/vgui_parent_child_link_data.cxx
+#ifdef VCL_NEEDS_PRAGMA_INTERFACE
+#pragma implementation
+#endif
+
+#include "vgui_parent_child_link_data.h"
---
+++ core/vgui/internals/vgui_parent_child_link_data.h
@@ -0,0 +1,25 @@
+// This is oxl/vgui/internals/vgui_parent_child_link_data.h
+#ifndef vgui_parent_child_link_data_h_
+#define vgui_parent_child_link_data_h_
+#ifdef VCL_NEEDS_PRAGMA_INTERFACE
+#pragma interface
+#endif
+//:
+// \file
+// \author fsm@robots.ox.ac.uk
+
+#include <vcl_vector.h>
+
+struct vgui_parent_child_link_impl;
+struct vgui_parent_child_link;
+class vgui_tableau;
+
+class vgui_parent_child_link_data
+{
+ private:
+  vcl_vector<vgui_tableau*> parents;
+  friend struct vgui_parent_child_link_impl;
+  friend struct vgui_parent_child_link;
+};
+
+#endif // vgui_parent_child_link_data_h_
