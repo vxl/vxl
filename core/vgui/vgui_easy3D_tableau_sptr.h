8eb83fff00884524838cdb153633ec5baf2f11eb
Karen McGaul <mcgaul@users.sourceforge.net>
2002-10-02 13:35:39 -0400

    KYM moved vgui_easy3D to vgui_easy3D_tableau

---
+++ core/vgui/vgui_easy3D_tableau_sptr.h
@@ -0,0 +1,15 @@
+// This is oxl/vgui/vgui_easy3D_tableau_sptr.h
+#ifndef vgui_easy3D_tableau_sptr_h_
+#define vgui_easy3D_tableau_sptr_h_
+//:
+// \file
+// \brief  Smart-pointer to a vgui_easy3D_tableau tableau.
+//
+// This is a generated file.
+
+#include <vgui/vgui_tableau_sptr.h>
+
+class vgui_easy3D_tableau;
+typedef vgui_tableau_sptr_t<vgui_easy3D_tableau> vgui_easy3D_tableau_sptr;
+
+#endif // vgui_easy3D_tableau_sptr_h_
