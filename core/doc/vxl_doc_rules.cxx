9d721e9af9c047fe031776093b0ee7c4bc828f18
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-02 02:04:36 -0400

    typo corrected

--- core/doc/vxl_doc_rules.cxx
+++ core/doc/vxl_doc_rules.cxx
@@ -7,17 +7,18 @@
 
 #include "vxl_doc_rules.h"
 
-//: By all means replicate the documentation from the .h file here
-// But it seems the doxygen does not reliably pick up documentation in
+//: By all means replicate the documentation from the .h file here.
+// But it seems that doxygen does not reliably pick up documentation in
 // the .cxx file whereas we haven't had a problem with it picking it up
 // from the .h file.
+// The .h file documentation, if present, has precedence in any case.
 my_class::my_class()
 {
-  // This class does absolutely nothing
+  // This constructor does absolutely nothing
 }
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
-class some_weird_class_I_do_not_want_docuemented
+class some_weird_class_I_do_not_want_documented
 {
 };
 #endif // DOXYGEN_SHOULD_SKIP_THIS
