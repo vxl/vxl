7128c4b25ac7768194db6ba4dd5c9d1e8b7f83a4
Karen McGaul <mcgaul@users.sourceforge.net>
2002-10-02 13:03:29 -0400

    KYM: moved vgui_displaybase to vgui_displaybase_tableau

---
+++ core/vgui/vgui_displaybase_tableau_sptr.h
@@ -0,0 +1,15 @@
+// This is oxl/vgui/vgui_displaybase_tableau_sptr.h
+#ifndef vgui_displaybase_tableau_sptr_h_
+#define vgui_displaybase_tableau_sptr_h_
+//:
+// \file
+// \brief  Smart-pointer to a vgui_displaybase_tableau tableau.
+//
+// this is a generated file.
+
+#include <vgui/vgui_tableau_sptr.h>
+
+class vgui_displaybase_tableau;
+typedef vgui_tableau_sptr_t<vgui_displaybase_tableau> vgui_displaybase_tableau_sptr;
+
+#endif // vgui_displaybase_tableau_sptr_h_
