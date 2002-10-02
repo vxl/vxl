94b37633eb0c5b2006e6d99105e5e919db2a2007
Tim Cootes <timcootes@users.sourceforge.net>
2002-10-02 08:11:34 -0400

    Added /relates documentation (experimental)

--- core/vil/vil_image_view_functions.h
+++ core/vil/vil_image_view_functions.h
@@ -10,54 +10,66 @@
 #include <vil/vil_rgb.h>
 
 //: Return a 3-plane view of an RGB image
+// \relates vil2_image_view
 template<class T>
 vil2_image_view<T> vil2_view_as_planes(const vil2_image_view<vil_rgb<T> >& rgb_view);
 
-//: Return an RGB component view of a 3-plane image.
+//: \relates vil2_image_view
+//  Return an RGB component view of a 3-plane image.
 //  Aborts if plane image not in correct format (ie planestep()!=1)
 template<class T>
 vil2_image_view<vil_rgb<T> > vil2_view_as_rgb(const vil2_image_view<T>& plane_view);
 
 //: Create a view which appears as the transpose of this view.
 //  i.e vil2_transpose(view)(i,j,p) = view(j,i,p)
+// \relates vil2_image_view
 template<class T>
 vil2_image_view<T> vil2_transpose(const vil2_image_view<T>& view);
 
 //: Create a reflected view in which i -> ni-1-i.
 //  i.e vil2_flip_lr(view)(i,j,p) = view(ni-1-i,j,p)
+// \relates vil2_image_view
 template<class T>
 vil2_image_view<T> vil2_flip_lr(const vil2_image_view<T>& view);
 
 //: Create a reflected view in which y -> nj-1-j.
 //  i.e vil2_flip_ud(view)(i,j,p) = view(i,nj-1-j,p)
+// \relates vil2_image_view
 template<class T>
 vil2_image_view<T> vil2_flip_ud(const vil2_image_view<T>& view);
 
 //: Fill view with given value
+// \relates vil2_image_view
 template<class T>
 void vil2_fill(vil2_image_view<T>& view, T value);
 
 //: Fill data[i*step] (i=0..n-1) with given value
+// \relates vil2_image_view
 template<class T>
 void vil2_fill_line(T* data, unsigned n, int step, T value);
 
 //: Fill row j in view with given value
+// \relates vil2_image_view
 template<class T>
 void vil2_fill_row(vil2_image_view<T>& view, unsigned j, T value);
 
 //: Fill column i in view with given value
+// \relates vil2_image_view
 template<class T>
 void vil2_fill_col(vil2_image_view<T>& view, unsigned i, T value);
 
 //: Compute minimum and maximum values over view
+// \relates vil2_image_view
 template<class T>
 void vil2_value_range(T& min_value, T& max_value,const vil2_image_view<T>& view);
 
 //: How to print value in vil2_print_all(image_view)
+// \relates vil2_image_view
 template<class T>
 void vil2_print_value(vcl_ostream& s, const T& value);
 
 //: print all image data to os in a grid
+// \relates vil2_image_view
 template<class T>
 void vil2_print_all(vcl_ostream& os,const vil2_image_view<T>& view);
 
