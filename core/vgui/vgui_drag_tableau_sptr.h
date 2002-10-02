eba222094518c10cece6ef2b3c8a259fc58ff308
Karen McGaul <mcgaul@users.sourceforge.net>
2002-10-02 13:14:57 -0400

    KYM smart-pointer implementation for vgui_drag_tableau

---
+++ core/vgui/vgui_drag_tableau_sptr.h
@@ -0,0 +1,20 @@
+// This is oxl/vgui/vgui_drag_tableau_sptr.h
+#ifndef vgui_drag_tableau_sptr_h
+#define vgui_drag_tableau_sptr_h
+//:
+// \file
+// \author K.Y.McGaul
+// \date   02-OCT-2002
+// \brief  Smart-pointer to a vgui_drag_tableau.
+//
+// \verbatim
+//  Modifications:
+//   02-OCT-2002 K.Y.McGaul - Initial version.
+// \endverbatim
+
+#include <vgui/vgui_tableau_sptr.h>
+
+class vgui_drag_tableau;
+typedef vgui_tableau_sptr_t<vgui_drag_tableau> vgui_drag_tableau_sptr;
+
+#endif // vgui_drag_tableau_sptr_h
