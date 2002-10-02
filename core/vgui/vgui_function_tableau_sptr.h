d01a23b07d7bfadce1c245cbba45b90da2a9a618
Karen McGaul <mcgaul@users.sourceforge.net>
2002-10-02 13:15:40 -0400

    KYM smart-pointer implemenation for vgui_function_tableau

---
+++ core/vgui/vgui_function_tableau_sptr.h
@@ -0,0 +1,20 @@
+// This is oxl/vgui/vgui_function_tableau_sptr.h
+#ifndef vgui_function_tableau_sptr_h
+#define vgui_function_tableau_sptr_h
+//:
+// \file
+// \author K.Y.McGaul
+// \date   02-OCT-2002
+// \brief  Smart-pointer to a vgui_function_tableau.
+// 
+// \verbatim
+//  Modifications:
+//   02-OCT-2002 K.Y.McGaul - Initial version.
+// \endverbatim
+
+#include <vgui/vgui_tableau_sptr.h>
+
+class vgui_function_tableau;
+typedef vgui_tableau_sptr_t<vgui_function_tableau> vgui_function_tableau_sptr;
+
+#endif // vgui_function_tableau_sptr_h
