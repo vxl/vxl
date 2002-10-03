17f8f709a2363924c1fc316bae4cefef07fee7ae
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-03 04:33:59 -0400

    EOLN added at EOF

--- core/vil/tests/test_pixel_format.cxx
+++ core/vil/tests/test_pixel_format.cxx
@@ -6,18 +6,17 @@
 
 int test_pixel_format_main( int argc, char* argv[] )
 {
-  vcl_cout << "*******************************\n";
-  vcl_cout << " Testing vil2_pixel_format \n";
-  vcl_cout << "*******************************\n";
-
+  vcl_cout << "***************************\n"
+           << " Testing vil2_pixel_format\n"
+           << "***************************\n";
 
   TEST("vil2_pixel_format_of(byte)", vil2_pixel_format_of(bool(0)), VIL2_PIXEL_FORMAT_BOOL);
 
   TEST("vil2_pixel_format_of(vil_rgb<vil_byte>) has 3 components", 
-    vil2_pixel_format_num_components(vil2_pixel_format_of(vil_rgb<float> (0))), 3);
+    vil2_pixel_format_num_components(vil2_pixel_format_of(vil_rgb<float>(0))), 3);
  
   TEST("vil2_pixel_format_component_format(vil_rgb<vil2_byte>)",
     vil2_pixel_format_component_format(vil2_pixel_format_of(vil_rgba<vil2_byte>(0))), VIL2_PIXEL_FORMAT_BYTE);
-  return testlib_test_summary();
 
+  return testlib_test_summary();
 }
