3f6d44f5e7f78e5b512de97f8bb2bbaf2898f86a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-03 03:45:14 -0400

    added missing #include <vcl_cassert.h>

--- core/vil/examples/file_example4.cxx
+++ core/vil/examples/file_example4.cxx
@@ -9,13 +9,12 @@
 #include <vil2/vil2_save.h>
 #include <vil2/vil2_image_view.h>
 #include <vil2/vil2_image_view_functions.h>
-
+#include <vcl_cassert.h>
 
 int main(int argc, char** argv)
 {
   vcl_string filename = testlib_root_dir() + "/vxl/vil/tests/file_read_data/ff_rgb8bit_ascii.ppm";
-  vcl_cout<<"Load " << filename << " into an rgb image object." << vcl_endl;
-
+  vcl_cout<<"Load " << filename << " into an rgb image object.\n";
 
   // This is how we quickly load an rgb image view.
 
@@ -27,16 +26,13 @@ int main(int argc, char** argv)
   // then the view will be set to empty.
 
   vcl_string filename2 = testlib_root_dir() + "/vxl/vil/tests/file_read_data/ff_grey8bit_raw.pgm";
-  vcl_cout<<"\n\n\nTry to load " << filename2 << " into an rgb image object." << vcl_endl;
+  vcl_cout<<"\n\n\nTry to load " << filename2 << " into an rgb image object.\n";
 
   b_im = vil2_load(filename2.c_str());
 
   assert (!b_im);
 
   vil2_print_all(vcl_cout, b_im);
-
-
-
 
   return 0;
 }
