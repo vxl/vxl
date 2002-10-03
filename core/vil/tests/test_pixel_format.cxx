3d72e581bb474828b4228758f9ecdf8df04fb3fb
Ian Scott <scottim@users.sourceforge.net>
2002-10-03 10:45:15 -0400

    confused float and double - now fixed

--- core/vil/tests/test_pixel_format.cxx
+++ core/vil/tests/test_pixel_format.cxx
@@ -10,13 +10,15 @@ int test_pixel_format_main( int argc, char* argv[] )
            << " Testing vil2_pixel_format\n"
            << "***************************\n";
 
-  TEST("vil2_pixel_format_of(byte)", vil2_pixel_format_of(bool(0)), VIL2_PIXEL_FORMAT_BOOL);
+  TEST("vil2_pixel_format_of(byte)", vil2_pixel_format_of(bool()), VIL2_PIXEL_FORMAT_BOOL);
+
+  TEST("vil2_pixel_format_of(float)", vil2_pixel_format_of(float()), VIL2_PIXEL_FORMAT_FLOAT);
 
   TEST("vil2_pixel_format_of(vil_rgb<vil_byte>) has 3 components", 
-    vil2_pixel_format_num_components(vil2_pixel_format_of(vil_rgb<float>(0))), 3);
+    vil2_pixel_format_num_components(vil2_pixel_format_of(vil_rgb<float>())), 3);
  
   TEST("vil2_pixel_format_component_format(vil_rgb<vil2_byte>)",
-    vil2_pixel_format_component_format(vil2_pixel_format_of(vil_rgba<vil2_byte>(0))), VIL2_PIXEL_FORMAT_BYTE);
+    vil2_pixel_format_component_format(vil2_pixel_format_of(vil_rgba<vil2_byte>())), VIL2_PIXEL_FORMAT_BYTE);
 
   return testlib_test_summary();
 }
