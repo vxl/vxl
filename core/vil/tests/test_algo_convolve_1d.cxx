7a1dfa101afe3a0031b0f78fcb647d82a0c84a42
Ian Scott <scottim@users.sourceforge.net>
2002-10-03 06:29:57 -0400

    quash unused variable ac warning

--- core/vil/tests/test_algo_convolve_1d.cxx
+++ core/vil/tests/test_algo_convolve_1d.cxx
@@ -16,9 +16,9 @@ void test_algo_convolve_1d_byte()
   for (int i=0;i<n;++i) src[i]=i+1;
   for (int i=0;i<3;++i) kernel[i]=(i+1);
 
-  double ac;  // Indicates accumulator type
   vil2_algo_convolve_1d(&src[0],n,1, &dest[0],1,
-                        &kernel[1],-1,1,ac,
+                        &kernel[1],-1,1,
+                        double(), // indicates accumulator type
                         vil2_convolve_no_extend,vil2_convolve_no_extend);
   TEST_NEAR("First full value",dest[1],14.0,1e-6);
   TEST_NEAR("Last full value",dest[n-2],6*n-4,1e-6);
@@ -27,7 +27,8 @@ void test_algo_convolve_1d_byte()
   for (int i=0;i<n;++i) dest[i]=999;
 
   vil2_algo_convolve_1d(&src[0],n,1, &dest[0],1,
-                        &kernel[1],-1,1,ac,
+                        &kernel[1],-1,1,
+                        double(), // indicates accumulator type
                         vil2_convolve_ignore_edge,vil2_convolve_ignore_edge);
 
   TEST_NEAR("Start",dest[0],999,1e-6);
@@ -39,7 +40,8 @@ void test_algo_convolve_1d_byte()
   for (int i=0;i<n;++i) dest[i]=999;
 
   vil2_algo_convolve_1d(&src[0],n,1, &dest[0],1,
-                        &kernel[1],-1,1,ac,
+                        &kernel[1],-1,1,
+                        double(), // indicates accumulator type
                         vil2_convolve_no_extend,vil2_convolve_no_extend);
 
   TEST_NEAR("Start",dest[0],0,1e-6);
@@ -51,7 +53,8 @@ void test_algo_convolve_1d_byte()
   vcl_cout<<"Testing vil2_convolve_zero_extend end type"<<vcl_endl;
   for (int i=0;i<n;++i) dest[i]=999;
   vil2_algo_convolve_1d(&src[0],n,1, &dest[0],1,
-                        &kernel[1],-1,1,ac,
+                        &kernel[1],-1,1,
+                        double(), // indicates accumulator type
                         vil2_convolve_zero_extend,vil2_convolve_zero_extend);
 
   TEST_NEAR("Start",dest[0],8,1e-6);
@@ -62,7 +65,8 @@ void test_algo_convolve_1d_byte()
   vcl_cout<<"Testing vil2_convolve_constant_extend end type"<<vcl_endl;
   for (int i=0;i<n;++i) dest[i]=999;
   vil2_algo_convolve_1d(&src[0],n,1, &dest[0],1,
-                        &kernel[1],-1,1,ac,
+                        &kernel[1],-1,1,
+                        double(), // indicates accumulator type
                         vil2_convolve_constant_extend,vil2_convolve_constant_extend);
 
   TEST_NEAR("Start",dest[0],9,1e-6);
@@ -73,7 +77,8 @@ void test_algo_convolve_1d_byte()
   vcl_cout<<"Testing vil2_convolve_reflect_extend end type"<<vcl_endl;
   for (int i=0;i<n;++i) dest[i]=999;
   vil2_algo_convolve_1d(&src[0],n,1, &dest[0],1,
-                        &kernel[1],-1,1,ac,
+                        &kernel[1],-1,1,
+                        double(), // indicates accumulator type
                         vil2_convolve_reflect_extend,vil2_convolve_reflect_extend);
 
   TEST_NEAR("Start",dest[0],n+8,1e-6);
@@ -84,7 +89,8 @@ void test_algo_convolve_1d_byte()
   vcl_cout<<"Testing vil2_convolve_trim end type"<<vcl_endl;
   for (int i=0;i<n;++i) dest[i]=999;
   vil2_algo_convolve_1d(&src[0],n,1, &dest[0],1,
-                        &kernel[1],-1,1,ac,
+                        &kernel[1],-1,1,
+                        double(), // indicates accumulator type
                         vil2_convolve_trim,vil2_convolve_trim);
 
   TEST_NEAR("Start",dest[0],48.0/5.0,1e-6);
