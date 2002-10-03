008bbf2e9b0975099b3690df97d50ca315ab615c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-03 02:34:53 -0400

    some more changed names

--- core/vgui/vgui_fwd.h
+++ core/vgui/vgui_fwd.h
@@ -16,26 +16,25 @@ class vgui_composite_tableau;
 class vgui_deck_tableau;
 class vgui_displaylist2D_tableau;
 class vgui_easy2D_tableau;
-class vgui_soview2D_circle;
-class vgui_soview2D_infinite_line;
-class vgui_soview2D_lineseg;
-class vgui_soview2D_point;
 class vgui_soview;
-class vgui_soview_namer;
+class vgui_soview3D;
+class vgui_soview2D;
+class vgui_soview3D;
+class vgui_point3D;
+class vgui_lineseg3D;
 
-class vgui_rubbercircle;
-class vgui_rubberline;
+class vgui_rubberband_tableau;
 
 class vgui_wrapper_tableau;
 class vgui_image_tableau;
-class vgui_image_renderer_tableau;
-class vgui_load_tableau;
+class vgui_image_renderer;
+class vgui_loader_tableau;
 
 class vgui_viewer2D_tableau;
 class vgui_viewer3D_tableau;
 class vgui_vrml_tableau;
 
-class vgui_command_tableau;
-class vgui_menu_tableau;
+struct vgui_command;
+class vgui_menu;
 
 #endif // vgui_fwd_h_
