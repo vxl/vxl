efa8ee38f21af0c59bf6bde91d13aa58b43b876b
Ian Scott <scottim@users.sourceforge.net>
2002-10-04 12:53:35 -0400

    collapse repetitive cases win a switch statment using macro This should also reduce the number of uncovered lines in the test profiler

--- core/vil/vil_memory_image.cxx
+++ core/vil/vil_memory_image.cxx
@@ -27,33 +27,18 @@ vil2_memory_image::vil2_memory_image(unsigned ni,
 
   switch (format)
   {
-  case VIL2_PIXEL_FORMAT_BYTE:
-    view_ = new vil2_image_view<vxl_byte>(ni, nj, nplanes);
-    break;
-  case VIL2_PIXEL_FORMAT_SBYTE:
-    view_ = new vil2_image_view<vxl_sbyte>(ni, nj, nplanes);
-    break;
-  case VIL2_PIXEL_FORMAT_UINT_32:
-    view_ = new vil2_image_view<vxl_uint_32>(ni, nj, nplanes);
-    break;
-  case VIL2_PIXEL_FORMAT_UINT_16:
-    view_ = new vil2_image_view<vxl_uint_16>(ni, nj, nplanes);
-    break;
-  case VIL2_PIXEL_FORMAT_INT_32:
-    view_ = new vil2_image_view<vxl_int_32>(ni, nj, nplanes);
-    break;
-  case VIL2_PIXEL_FORMAT_INT_16:
-    view_ = new vil2_image_view<vxl_int_16>(ni, nj, nplanes);
-    break;
-  case VIL2_PIXEL_FORMAT_BOOL:
-    view_ = new vil2_image_view<bool>(ni, nj, nplanes);
-    break;
-  case VIL2_PIXEL_FORMAT_FLOAT:
-    view_ = new vil2_image_view<float>(ni, nj, nplanes);
-    break;
-  case VIL2_PIXEL_FORMAT_DOUBLE:
-    view_ = new vil2_image_view<double>(ni, nj, nplanes);
-    break;
+#define macro( F , T ) \
+  case F :     view_ = new vil2_image_view<T >(ni, nj, nplanes); break;
+macro(VIL2_PIXEL_FORMAT_BYTE, vxl_byte )
+macro(VIL2_PIXEL_FORMAT_SBYTE , vxl_sbyte )
+macro(VIL2_PIXEL_FORMAT_UINT_32 , vxl_uint_32 )
+macro(VIL2_PIXEL_FORMAT_UINT_16 , vxl_uint_16 )
+macro(VIL2_PIXEL_FORMAT_INT_32 , vxl_int_32 )
+macro(VIL2_PIXEL_FORMAT_INT_16 , vxl_int_16 )
+macro(VIL2_PIXEL_FORMAT_BOOL , bool )
+macro(VIL2_PIXEL_FORMAT_FLOAT , float )
+macro(VIL2_PIXEL_FORMAT_DOUBLE , double )
+#undef macro
   default:
     vcl_cerr << "ERROR: vil2_memory_image::vil2_memory_image\n"
                 "\t unknown format " << format << vcl_endl;
@@ -70,87 +55,24 @@ vil2_image_view_base_sptr vil2_memory_image::get_copy_view(unsigned i0, unsigned
 {
   switch(view_->pixel_format())
   {
-  case  VIL2_PIXEL_FORMAT_BYTE:
-    {
-      const vil2_image_view<vxl_byte> &v =
-        static_cast<const vil2_image_view<vxl_byte> &>(*view_);
-      vil2_image_view<vxl_byte> w(v.memory_chunk(), &v(i0,j0),
-                                   ni, nj, v.nplanes(),
-                                   v.istep(), v.jstep(), v.planestep());
-      return new vil2_image_view<vxl_byte>(vil2_copy_deep(w));
-    }
-  case  VIL2_PIXEL_FORMAT_SBYTE:
-    {
-      const vil2_image_view<vxl_sbyte> &v =
-        static_cast<const vil2_image_view<vxl_sbyte> &>(*view_);
-      vil2_image_view<vxl_sbyte> w(v.memory_chunk(), &v(i0,j0),
-                                   ni, nj, v.nplanes(),
-                                   v.istep(), v.jstep(), v.planestep());
-      return new vil2_image_view<vxl_sbyte>(vil2_copy_deep(w));
-    }
-  case  VIL2_PIXEL_FORMAT_UINT_16:
-    {
-      const vil2_image_view<vxl_uint_16> &v =
-        static_cast<const vil2_image_view<vxl_uint_16> &>(*view_);
-      vil2_image_view<vxl_uint_16> w(v.memory_chunk(), &v(i0,j0),
-                                     ni, nj, v.nplanes(),
-                                     v.istep(), v.jstep(), v.planestep());
-      return new vil2_image_view<vxl_uint_16>(vil2_copy_deep(w));
-    }
-  case  VIL2_PIXEL_FORMAT_INT_16:
-    {
-      const vil2_image_view<vxl_int_16> &v =
-        static_cast<const vil2_image_view<vxl_int_16> &>(*view_);
-      vil2_image_view<vxl_int_16> w(v.memory_chunk(), &v(i0,j0),
-                                    ni, nj, v.nplanes(),
-                                    v.istep(), v.jstep(), v.planestep());
-      return new vil2_image_view<vxl_int_16>(vil2_copy_deep(w));
-    }
-  case  VIL2_PIXEL_FORMAT_UINT_32:
-    {
-      const vil2_image_view<vxl_uint_32> &v =
-        static_cast<const vil2_image_view<vxl_uint_32> &>(*view_);
-      vil2_image_view<vxl_uint_32> w(v.memory_chunk(), &v(i0,j0),
-                                     ni, nj, v.nplanes(),
-                                     v.istep(), v.jstep(), v.planestep());
-      return new vil2_image_view<vxl_uint_32>(vil2_copy_deep(w));
-    }
-  case  VIL2_PIXEL_FORMAT_INT_32:
-    {
-      const vil2_image_view<vxl_int_32> &v =
-        static_cast<const vil2_image_view<vxl_int_32> &>(*view_);
-      vil2_image_view<vxl_int_32> w(v.memory_chunk(), &v(i0,j0),
-                                    ni, nj, v.nplanes(),
-                                    v.istep(), v.jstep(), v.planestep());
-      return new vil2_image_view<vxl_int_32>(vil2_copy_deep(w));
-    }
-  case  VIL2_PIXEL_FORMAT_FLOAT:
-    {
-      const vil2_image_view<float> &v =
-        static_cast<const vil2_image_view<float> &>(*view_);
-      vil2_image_view<float> w(v.memory_chunk(), &v(i0,j0),
-                               ni, nj, v.nplanes(),
-                               v.istep(), v.jstep(), v.planestep());
-      return new vil2_image_view<float>(vil2_copy_deep(w));
-    }
-  case  VIL2_PIXEL_FORMAT_DOUBLE:
-    {
-      const vil2_image_view<double> &v =
-        static_cast<const vil2_image_view<double> &>(*view_);
-      vil2_image_view<double> w(v.memory_chunk(), &v(i0,j0),
-                                ni, nj, v.nplanes(),
-                                v.istep(), v.jstep(), v.planestep());
-      return new vil2_image_view<double>(vil2_copy_deep(w));
-    }
-  case  VIL2_PIXEL_FORMAT_BOOL:
-    {
-      const vil2_image_view<bool> &v =
-        static_cast<const vil2_image_view<bool> &>(*view_);
-      vil2_image_view<bool> w(v.memory_chunk(), &v(i0,j0),
-                              ni, nj, v.nplanes(),
-                              v.istep(), v.jstep(), v.planestep());
-      return new vil2_image_view<bool>(vil2_copy_deep(w));
-    }
+#define macro( F , T ) \
+  case  F : { \
+      const vil2_image_view< T > &v = \
+        static_cast<const vil2_image_view< T > &>(*view_); \
+      vil2_image_view< T > w(v.memory_chunk(), &v(i0,j0), \
+                                   ni, nj, v.nplanes(), \
+                                   v.istep(), v.jstep(), v.planestep()); \
+      return new vil2_image_view< T >(vil2_copy_deep(w)); }
+macro(VIL2_PIXEL_FORMAT_BYTE, vxl_byte )
+macro(VIL2_PIXEL_FORMAT_SBYTE , vxl_sbyte )
+macro(VIL2_PIXEL_FORMAT_UINT_32 , vxl_uint_32 )
+macro(VIL2_PIXEL_FORMAT_UINT_16 , vxl_uint_16 )
+macro(VIL2_PIXEL_FORMAT_INT_32 , vxl_int_32 )
+macro(VIL2_PIXEL_FORMAT_INT_16 , vxl_int_16 )
+macro(VIL2_PIXEL_FORMAT_BOOL , bool )
+macro(VIL2_PIXEL_FORMAT_FLOAT , float )
+macro(VIL2_PIXEL_FORMAT_DOUBLE , double )
+#undef macro
   default:
     return 0;
   }
@@ -163,78 +85,23 @@ vil2_image_view_base_sptr vil2_memory_image::get_view(unsigned i0, unsigned ni,
 {
   switch(view_->pixel_format())
   {
-  case  VIL2_PIXEL_FORMAT_BYTE:
-    {
-      const vil2_image_view<vxl_byte> &v =
-        static_cast<const vil2_image_view<vxl_byte> &>(*view_);
-      return new vil2_image_view<vxl_byte>(v.memory_chunk(), &v(i0,j0),
-                                            ni, nj, v.nplanes(),
-                                            v.istep(), v.jstep(), v.planestep());
-    }
-  case  VIL2_PIXEL_FORMAT_SBYTE:
-    {
-      const vil2_image_view<vxl_sbyte> &v =
-        static_cast<const vil2_image_view<vxl_sbyte> &>(*view_);
-      return new vil2_image_view<vxl_sbyte>(v.memory_chunk(), &v(i0,j0),
-                                            ni, nj, v.nplanes(),
-                                            v.istep(), v.jstep(), v.planestep());
-    }
-  case  VIL2_PIXEL_FORMAT_BOOL:
-    {
-      const vil2_image_view<bool> &v =
-        static_cast<const vil2_image_view<bool> &>(*view_);
-      return new vil2_image_view<bool>(v.memory_chunk(), &v(i0,j0),
-                                       ni, nj, v.nplanes(),
-                                       v.istep(), v.jstep(), v.planestep());
-    }
-  case  VIL2_PIXEL_FORMAT_UINT_32:
-    {
-      const vil2_image_view<vxl_uint_32> &v =
-        static_cast<const vil2_image_view<vxl_uint_32> &>(*view_);
-      return new vil2_image_view<vxl_uint_32>(v.memory_chunk(), &v(i0,j0),
-                                             ni, nj, v.nplanes(),
-                                             v.istep(), v.jstep(), v.planestep());
-    }
-  case  VIL2_PIXEL_FORMAT_UINT_16:
-    {
-      const vil2_image_view<vxl_uint_16> &v =
-        static_cast<const vil2_image_view<vxl_uint_16> &>(*view_);
-      return new vil2_image_view<vxl_uint_16>(v.memory_chunk(), &v(i0,j0),
-                                              ni, nj, v.nplanes(),
-                                              v.istep(), v.jstep(), v.planestep());
-    }
-  case  VIL2_PIXEL_FORMAT_INT_32:
-    {
-      const vil2_image_view<vxl_int_32> &v =
-        static_cast<const vil2_image_view<vxl_int_32> &>(*view_);
-      return new vil2_image_view<vxl_int_32>(v.memory_chunk(), &v(i0,j0),
-                                             ni, nj, v.nplanes(),
-                                             v.istep(), v.jstep(), v.planestep());
-    }
-  case  VIL2_PIXEL_FORMAT_INT_16:
-    {
-      const vil2_image_view<vxl_int_16> &v =
-        static_cast<const vil2_image_view<vxl_int_16> &>(*view_);
-      return new vil2_image_view<vxl_int_16>(v.memory_chunk(), &v(i0,j0),
-                                             ni, nj, v.nplanes(),
-                                             v.istep(), v.jstep(), v.planestep());
-    }
-  case  VIL2_PIXEL_FORMAT_FLOAT:
-    {
-      const vil2_image_view<float> &v =
-        static_cast<const vil2_image_view<float> &>(*view_);
-      return new vil2_image_view<float>(v.memory_chunk(), &v(i0,j0),
-                                        ni, nj, v.nplanes(),
-                                        v.istep(), v.jstep(), v.planestep());
-    }
-  case  VIL2_PIXEL_FORMAT_DOUBLE:
-    {
-      const vil2_image_view<double> &v =
-        static_cast<const vil2_image_view<double> &>(*view_);
-      return new vil2_image_view<double>(v.memory_chunk(), &v(i0,j0),
-                                         ni, nj, v.nplanes(),
-                                         v.istep(), v.jstep(), v.planestep());
-    }
+#define macro( F , T ) \
+  case  F : { \
+      const vil2_image_view< T > &v = \
+        static_cast<const vil2_image_view< T > &>(*view_); \
+      return new vil2_image_view< T >(v.memory_chunk(), &v(i0,j0), \
+                                            ni, nj, v.nplanes(), \
+                                            v.istep(), v.jstep(), v.planestep()); }
+macro(VIL2_PIXEL_FORMAT_BYTE , vxl_byte )
+macro(VIL2_PIXEL_FORMAT_SBYTE , vxl_sbyte )
+macro(VIL2_PIXEL_FORMAT_UINT_32 , vxl_uint_32 )
+macro(VIL2_PIXEL_FORMAT_UINT_16 , vxl_uint_16 )
+macro(VIL2_PIXEL_FORMAT_INT_32 , vxl_int_32 )
+macro(VIL2_PIXEL_FORMAT_INT_16 , vxl_int_16 )
+macro(VIL2_PIXEL_FORMAT_BOOL , bool )
+macro(VIL2_PIXEL_FORMAT_FLOAT , float )
+macro(VIL2_PIXEL_FORMAT_DOUBLE , double )
+#undef macro
   default:
     return 0;
   }
@@ -251,150 +118,34 @@ bool vil2_memory_image::put_view(const vil2_image_view_base& im,unsigned i0, uns
 
   switch(view_->pixel_format())
   {
-  case  VIL2_PIXEL_FORMAT_BYTE:
-    {
-      vil2_image_view<vxl_byte> &v =
-        static_cast<vil2_image_view<vxl_byte> &>(*view_);
-      const vil2_image_view<vxl_byte> &w =
-        static_cast<const vil2_image_view<vxl_byte> &>(im);
-      if (v.memory_chunk() == w.memory_chunk())
-      {
-        if (&v(i0,j0) != w.top_left_ptr())
-          assert("ERROR: vil2_memory_image::put_view()\n"
-                 "different window from that used in get_view()");
-        else return true; // The user has already modified the data in place.
-      }
-      vil2_copy_to_window(w, v, i0, j0);
-      return true;
-    }
-  case  VIL2_PIXEL_FORMAT_SBYTE:
-    {
-      vil2_image_view<vxl_byte> &v =
-        static_cast<vil2_image_view<vxl_byte> &>(*view_);
-      const vil2_image_view<vxl_byte> &w =
-        static_cast<const vil2_image_view<vxl_byte> &>(im);
-      if (v.memory_chunk() == w.memory_chunk())
-      {
-        if (&v(i0,j0) != w.top_left_ptr())
-          assert("ERROR: vil2_memory_image::put_view()\n"
-                 "different window from that used in get_view()");
-        else return true; // The user has already modified the data in place.
-      }
-      vil2_copy_to_window(w, v, i0, j0);
-      return true;
-    }
-  case  VIL2_PIXEL_FORMAT_BOOL:
-    {
-      vil2_image_view<bool> &v =
-        static_cast<vil2_image_view<bool> &>(*view_);
-      const vil2_image_view<bool> &w =
-        static_cast<const vil2_image_view<bool> &>(im);
-      if (v.memory_chunk() == w.memory_chunk())
-      {
-        if (&v(i0,j0) != w.top_left_ptr())
-          assert("ERROR: vil2_memory_image::put_view()\n"
-                 "different window from that used in get_view()");
-        else return true; // The user has already modified the data in place.
-      }
-      vil2_copy_to_window(w, v, i0, j0);
-      return true;
-    }
-  case  VIL2_PIXEL_FORMAT_UINT_32:
-    {
-      vil2_image_view<vxl_uint_32> &v =
-        static_cast<vil2_image_view<vxl_uint_32> &>(*view_);
-      const vil2_image_view<vxl_uint_32> &w =
-        static_cast<const vil2_image_view<vxl_uint_32> &>(im);
-      if (v.memory_chunk() == w.memory_chunk())
-      {
-        if (&v(i0,j0) != w.top_left_ptr())
-          assert("ERROR: vil2_memory_image::put_view()\n"
-                 "different window from that used in get_view()");
-        else return true; // The user has already modified the data in place.
-      }
-      vil2_copy_to_window(w, v, i0, j0);
-      return true;
-    }
-  case  VIL2_PIXEL_FORMAT_UINT_16:
-    {
-      vil2_image_view<vxl_uint_16> &v =
-        static_cast<vil2_image_view<vxl_uint_16> &>(*view_);
-      const vil2_image_view<vxl_uint_16> &w =
-        static_cast<const vil2_image_view<vxl_uint_16> &>(im);
-      if (v.memory_chunk() == w.memory_chunk())
-      {
-        if (&v(i0,j0) != w.top_left_ptr())
-          assert("ERROR: vil2_memory_image::put_view()\n"
-                 "different window from that used in get_view()");
-        else return true; // The user has already modified the data in place.
-      }
-      vil2_copy_to_window(w, v, i0, j0);
-      return true;
-    }
-  case  VIL2_PIXEL_FORMAT_INT_32:
-    {
-      vil2_image_view<vxl_int_32> &v =
-        static_cast<vil2_image_view<vxl_int_32> &>(*view_);
-      const vil2_image_view<vxl_int_32> &w =
-        static_cast<const vil2_image_view<vxl_int_32> &>(im);
-      if (v.memory_chunk() == w.memory_chunk())
-      {
-        if (&v(i0,j0) != w.top_left_ptr())
-          assert("ERROR: vil2_memory_image::put_view()\n"
-                 "different window from that used in get_view()");
-        else return true; // The user has already modified the data in place.
-      }
-      vil2_copy_to_window(w, v, i0, j0);
-      return true;
-    }
-  case  VIL2_PIXEL_FORMAT_INT_16:
-    {
-      vil2_image_view<vxl_int_16> &v =
-        static_cast<vil2_image_view<vxl_int_16> &>(*view_);
-      const vil2_image_view<vxl_int_16> &w =
-        static_cast<const vil2_image_view<vxl_int_16> &>(im);
-      if (v.memory_chunk() == w.memory_chunk())
-      {
-        if (&v(i0,j0) != w.top_left_ptr())
-          assert("ERROR: vil2_memory_image::put_view()\n"
-                 "different window from that used in get_view()");
-        else return true; // The user has already modified the data in place.
-      }
-      vil2_copy_to_window(w, v, i0, j0);
-      return true;
-    }
-  case  VIL2_PIXEL_FORMAT_FLOAT:
-    {
-      vil2_image_view<float> &v =
-        static_cast<vil2_image_view<float> &>(*view_);
-      const vil2_image_view<float> &w =
-        static_cast<const vil2_image_view<float> &>(im);
-      if (v.memory_chunk() == w.memory_chunk())
-      {
-        if (&v(i0,j0) != w.top_left_ptr())
-          assert("ERROR: vil2_memory_image::put_view()\n"
-                 "different window from that used in get_view()");
-        else return true; // The user has already modified the data in place.
-      }
-      vil2_copy_to_window(w, v, i0, j0);
-      return true;
-    }
-  case  VIL2_PIXEL_FORMAT_DOUBLE:
-    {
-      vil2_image_view<double> &v =
-        static_cast<vil2_image_view<double> &>(*view_);
-      const vil2_image_view<double> &w =
-        static_cast<const vil2_image_view<double> &>(im);
-      if (v.memory_chunk() == w.memory_chunk())
-      {
-        if (&v(i0,j0) != w.top_left_ptr())
-          assert("ERROR: vil2_memory_image::put_view()\n"
-                 "different window from that used in get_view()");
-        else return true; // The user has already modified the data in place.
-      }
-      vil2_copy_to_window(w, v, i0, j0);
-      return true;
-    }
+
+#define macro( F , T ) \
+  case  F : { \
+      vil2_image_view< T > &v = \
+        static_cast<vil2_image_view< T > &>(*view_); \
+      const vil2_image_view< T > &w = \
+        static_cast<const vil2_image_view< T > &>(im); \
+      if (v.memory_chunk() == w.memory_chunk()) \
+      { \
+        if (&v(i0,j0) != w.top_left_ptr()) \
+          assert("ERROR: vil2_memory_image::put_view()\n" \
+                 "different window from that used in get_view()"); \
+        else return true; /* The user has already modified the data in place.*/ \
+      } \
+      vil2_copy_to_window(w, v, i0, j0); \
+      return true; }
+
+macro(VIL2_PIXEL_FORMAT_BYTE , vxl_byte )
+macro(VIL2_PIXEL_FORMAT_SBYTE , vxl_sbyte )
+macro(VIL2_PIXEL_FORMAT_UINT_32 , vxl_uint_32 )
+macro(VIL2_PIXEL_FORMAT_UINT_16 , vxl_uint_16 )
+macro(VIL2_PIXEL_FORMAT_INT_32 , vxl_int_32 )
+macro(VIL2_PIXEL_FORMAT_INT_16 , vxl_int_16 )
+macro(VIL2_PIXEL_FORMAT_BOOL , bool )
+macro(VIL2_PIXEL_FORMAT_FLOAT , float )
+macro(VIL2_PIXEL_FORMAT_DOUBLE , double )
+
+#undef macro
   default:
     vcl_cerr << "WARNING: vil2_memory_image::put_view()\n"
                 "\t Unexpected pixel type" << view_->pixel_format() << vcl_endl;
