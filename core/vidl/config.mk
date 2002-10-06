6f0aae9965fa4d18d492f53d33245c9919d14b12
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-06 06:34:15 -0400

    try.mk is loaded automatically

--- core/vidl/config.mk
+++ core/vidl/config.mk
@@ -2,7 +2,3 @@ IULIBS += -lvidl
 include ${IUEROOT}/vxl/vul/config.mk
 include ${IUEROOT}/vxl/vil/config.mk
 TRY_MPEG2 := 1
-include ${IUEROOT}/config/try.mk
-ifeq ($(HAS_MPEG2),1)
-IULIBS += $(MPEG2_LIB_DIR)
-endif
