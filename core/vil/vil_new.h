5ab87163bd800258a59b47a953efbb7135d68af9
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-09 03:02:50 -0400

    vertical alignment for better readability

--- core/vil/vil_new.h
+++ core/vil/vil_new.h
@@ -28,35 +28,35 @@
 //: Make a new image of given format.
 // \relates vil2_image_resource
 vil2_image_resource_sptr vil2_new_image_resource(unsigned ni, unsigned nj, unsigned nplanes,
-                                         vil2_pixel_format format);
+                                                 vil2_pixel_format format);
 
 //: Make a new image, similar format to the prototype.
 // \relates vil2_image_resource
 vil2_image_resource_sptr vil2_new_image_resource(unsigned ni, unsigned nj,
-                                         vil2_image_resource_sptr const& prototype);
+                                                 vil2_image_resource_sptr const& prototype);
 
 //: Make a new image.
 // \relates vil2_image_resource
 vil2_image_resource_sptr vil2_new_image_resource(vil_stream* os,
-                                         unsigned ni, unsigned nj,
-                                         unsigned nplanes,
-                                         vil2_pixel_format format,
-                                         char const* file_format = 0);
+                                                 unsigned ni, unsigned nj,
+                                                 unsigned nplanes,
+                                                 vil2_pixel_format format,
+                                                 char const* file_format = 0);
 
 //: Make a new image on disk
 // \relates vil2_image_resource
 vil2_image_resource_sptr vil2_new_image_resource(char const* filename,
-                                         unsigned ni, unsigned nj,
-                                         unsigned nplanes,
-                                         vil2_image_resource_sptr const &prototype,
-                                         char const* format = 0);
+                                                 unsigned ni, unsigned nj,
+                                                 unsigned nplanes,
+                                                 vil2_image_resource_sptr const &prototype,
+                                                 char const* format = 0);
 
 //: Make a new image.
 // \relates vil2_image_resource
 vil2_image_resource_sptr vil2_new_image_resource(vil_stream* os,
-                                         unsigned ni, unsigned nj,
-                                         unsigned nplanes,
-                                         vil2_image_resource_sptr const& prototype,
-                                         char const* file_format = 0);
+                                                 unsigned ni, unsigned nj,
+                                                 unsigned nplanes,
+                                                 vil2_image_resource_sptr const& prototype,
+                                                 char const* file_format = 0);
 
 #endif // vil2_new_h_
