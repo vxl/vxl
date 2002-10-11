d124988f1554e3db8f5742d476db21a53f6e060e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-11 08:27:14 -0400

    extended the applicable region a little bit (to include the end points)

--- core/vil/tests/test_bilin_interp.cxx
+++ core/vil/tests/test_bilin_interp.cxx
@@ -1,46 +1,47 @@
 // This is mul/vil2/tests/test_bilin_interp.cxx
-#include <vcl_iostream.h>
-#include <vxl_config.h>
 #include <testlib/testlib_test.h>
+#include <vcl_iostream.h>
+#include <vxl_config.h> // for vxl_byte
 #include <vil2/vil2_image_view.h>
 #include <vil2/vil2_bilin_interp.h>
 
 void test_bilin_interp_byte()
 {
-  vcl_cout << "***************************\n";
-  vcl_cout << " Testing vil2_bilin_interp\n";
-  vcl_cout << "***************************\n";
+  vcl_cout << "***************************\n"
+           << " Testing vil2_bilin_interp\n"
+           << "***************************\n";
 
   vil2_image_view<vxl_byte> image0;
   image0.resize(5,5);
 
-  for (unsigned int y=0;y<image0.nj();++y)
-     for (unsigned int x=0;x<image0.ni();++x)
-       image0(x,y) = x+y*10;
+  for (unsigned int j=0;j<image0.nj();++j)
+     for (unsigned int i=0;i<image0.ni();++i)
+       image0(i,j) = i+j*10;
 
-   {
-     double v1 = vil2_bilin_interp(image0,3,3);
-     TEST_NEAR("vil2_bilin_interp at grid point",v1,33,1e-8);
+  {
+    double v1 = vil2_bilin_interp(image0,3,3);
+    TEST_NEAR("vil2_bilin_interp at grid point",v1,33,1e-8);
+    double v2 = vil2_bilin_interp(image0,3.4,3);
+    TEST_NEAR("vil2_bilin_interp at off-grid point",v2,33.4,1e-8);
+    double v3 = vil2_bilin_interp(image0,3.4,3.5);
+    TEST_NEAR("vil2_bilin_interp at off-grid point",v3,38.4,1e-8);
+  }
 
-     double v2 = vil2_bilin_interp(image0,3.4,3);
-     TEST_NEAR("vil2_bilin_interp at off-grid point",v2,33.4,1e-8);
-     double v3 = vil2_bilin_interp(image0,3.4,3.5);
-     TEST_NEAR("vil2_bilin_interp at off-grid point",v3,38.4,1e-8);
-   }
+  {
+    double v1 = vil2_bilin_interp_safe(image0,4,4);
+    TEST_NEAR("vil2_bilin_interp_safe at grid point",v1,44,1e-8);
+    double v2 = vil2_bilin_interp_safe(image0,3.4,3);
+    TEST_NEAR("vil2_bilin_interp_safe at off-grid point",v2,33.4,1e-8);
+    double v3 = vil2_bilin_interp_safe(image0,3.4,3.5);
+    TEST_NEAR("vil2_bilin_interp_safe at off-grid point",v3,38.4,1e-8);
 
-   {
-
-     double v1 = vil2_bilin_interp_safe(image0,3,3);
-     TEST_NEAR("vil2_bilin_interp_safe at grid point",v1,33,1e-8);
-
-     double v2 = vil2_bilin_interp_safe(image0,3.4,3);
-     TEST_NEAR("vil2_bilin_interp_safe at off-grid point",v2,33.4,1e-8);
-     double v3 = vil2_bilin_interp_safe(image0,3.4,3.5);
-     TEST_NEAR("vil2_bilin_interp_safe at off-grid point",v3,38.4,1e-8);
-
-     double v_outside = vil2_bilin_interp_safe(image0,-1,-1);
-     TEST_NEAR("vil2_bilin_interp_safe outside image",v_outside,0,1e-8);
-   }
+    double v4 = vil2_bilin_interp_safe(image0,5,2);
+    TEST_NEAR("vil2_bilin_interp_safe outside image",v4,0,1e-8);
+    double v5 = vil2_bilin_interp_safe(image0,4.001,1);
+    TEST_NEAR("vil2_bilin_interp_safe outside image",v5,0,1e-8);
+    double v6 = vil2_bilin_interp_safe(image0,1,4.001);
+    TEST_NEAR("vil2_bilin_interp_safe outside image",v6,0,1e-8);
+  }
 }
 
 MAIN( test_bilin_interp )
@@ -51,4 +52,3 @@ MAIN( test_bilin_interp )
 
   SUMMARY();
 }
-
--- core/vil/vil_bilin_interp.h
+++ core/vil/vil_bilin_interp.h
@@ -8,113 +8,115 @@
 #include <vcl_cassert.h>
 
 //: Compute bilinear interpolation at (x,y), no bound checks
-//  Image is nx * ny array of Ts. x,y element is data[ystep*y+xstep*x]
+//  Image is nx * ny array of Ts. x,y element is data[xstep*x+ystep*y]
 //  No bound checks are done.
 template<class T>
 inline double vil2_bilin_interp_raw(double x, double y, const T* data, int xstep, int ystep)
 {
-    int p1x,p1y;
-    double normx,normy;
-    p1x=int(x);
-    normx = x-p1x;
-    p1y=int(y);
-    normy = y-p1y;
+    int p1x=int(x);
+    double normx = x-p1x;
+    int p1y=int(y);
+    double normy = y-p1y;
 
-    const T* row1 = data + p1y*ystep + p1x * xstep;
-    const T* row2 = row1 + ystep;
+    const T* pix1 = data + p1y*ystep + p1x*xstep;
 
-    double i1,i2;
-    i1 = (double)row1[0]+
-        (double)(row2[0]-row1[0])*normy;
-    i2 = (double)row1[xstep]+
-        (double)(row2[xstep]-row1[xstep])*normy;
+    // special boundary cases can be handeled more quickly first;
+    // also avoids accessing an invalid pix1[t] which is going to have weight 0.
+    if (normx == 0 && normy == 0) return pix1[0];
+    if (normx == 0) return pix1[0]+(pix1[ystep]-pix1[0])*normy;
+    if (normy == 0) return pix1[0]+(pix1[xstep]-pix1[0])*normx;
+
+    double i1 = pix1[0    ]+(pix1[      ystep]-pix1[0    ])*normy;
+    double i2 = pix1[xstep]+(pix1[xstep+ystep]-pix1[xstep])*normy;
 
     return i1+(i2-i1)*normx;
 }
 
 //: Compute bilinear interpolation at (x,y), with bound checks
-//  Image is nx * ny array of Ts. x,y element is data[ystep*y+xstep*x]
+//  Image is nx * ny array of Ts. x,y element is data[xstep*x+ystep*y]
 //  If (x,y) is outside interpolatable image region, zero is returned.
-//  The safe interpolatable region is [0,nx)*[0,ny).
+//  The safe interpolatable region is [0,nx-1]*[0,ny-1].
 template<class T>
 inline double vil2_bilin_interp_safe(double x, double y, const T* data,
                                      int nx, int ny, int xstep, int ystep)
 {
     if (x<0) return 0.0;
     if (y<0) return 0.0;
-    if (((int)x)>=nx-1) return 0.0;
-    if (((int)y)>=ny-1) return 0.0;
+    if (x>nx-1) return 0.0;
+    if (y>ny-1) return 0.0;
     return vil2_bilin_interp_raw(x,y,data,xstep,ystep);
 }
 
 //: Compute bilinear interpolation at (x,y), with bound checks
 //  If (x,y) is outside interpolatable image region, zero is returned.
-//  The safe interpolatable region is [0,view.ni)*[0,view.nj).
+//  The safe interpolatable region is [0,view.ni()-1]*[0,view.nj()-1].
 // \relates vil2_image_view
 template<class T>
 inline double vil2_bilin_interp_safe(const vil2_image_view<T> &view, 
                                      double x, double y, unsigned p=0)
 {
     return vil2_bilin_interp_safe(x, y, &view(0,0,p),
-      view.ni(), view.nj(), view.istep(), view.jstep());
+                                  view.ni(), view.nj(),
+                                  view.istep(), view.jstep());
 }
-
 
 
 //: Compute bilinear interpolation at (x,y), with minimal bound checks
 //  Image is nx * ny array of Ts. x,y element is data[ystep*y+xstep*x]
 //  If (x,y) is outside interpolatable image region and NDEBUG is not defined
 //  the code will fail an ASSERT.
-//  The safe interpolatable region is [0,nx)*[0,ny).
+//  The safe interpolatable region is [0,nx-1]*[0,ny-1].
 template<class T>
 inline double vil2_bilin_interp(double x, double y, const T* data,
-                                       int nx, int ny, int xstep, int ystep)
+                                int nx, int ny, int xstep, int ystep)
 {
     assert (x>=0);
     assert (y>=0);
-    assert (((int)x)<nx-1);
-    assert (((int)y)<ny-1);
+    assert (x<=nx-1);
+    assert (y<=ny-1);
     return vil2_bilin_interp_raw(x,y,data,xstep,ystep);
 }
 
 //: Compute bilinear interpolation at (x,y), with minimal bound checks
 //  If (x,y) is outside interpolatable image region and NDEBUG is not defined
 //  the code will fail an ASSERT.
-//  The safe interpolatable region is [0,view.ni)*[0,view.nj).
+//  The safe interpolatable region is [0,view.ni()-1]*[0,view.nj()-1].
 // \relates vil2_image_view
 template<class T>
 inline double vil2_bilin_interp(const vil2_image_view<T> &view, 
                                 double x, double y, unsigned p=0)
 {
     return vil2_bilin_interp(x, y, &view(0,0,p),
-      view.ni(), view.nj(), view.istep(), view.jstep());
+                             view.ni(), view.nj(),
+                             view.istep(), view.jstep());
 }
 
 
 //: Compute bilinear interpolation at (x,y), with bound checks
 //  Image is nx * ny array of Ts. x,y element is data[nx*y+x]
 //  If (x,y) is outside safe interpolatable image region, nearest pixel value is returned.
-//  The safe interpolatable region is [0,nx)*[0,ny).
+//  The safe interpolatable region is [0,nx-1]*[0,ny-1].
 template<class T>
 inline double vil2_bilin_interp_safe_extend(double x, double y, const T* data, int nx, int ny, int xstep, int ystep)
 {
     if (x<0) x= 0.0;
     if (y<0) y= 0.0;
-    if (((int)x)>=nx-1) x=(double)nx-1.00000001;
-    if (((int)y)>=ny-1) y=(double)ny-1.00000001;
+    if (x>nx-1) x=nx-1.0;
+    if (y>ny-1) y=ny-1.0;
     return vil2_bilin_interp_raw(x,y,data,xstep,ystep);
 }
 
 //: Compute bilinear interpolation at (x,y), with bound checks
 //  If (x,y) is outside safe interpolatable image region, nearest pixel value is returned.
-//  The safe interpolatable region is [0,view.ni)*[0,view.nj).
+//  The safe interpolatable region is [0,view.ni()-1]*[0,view.nj()-1].
 // \relates vil2_image_view
 template<class T>
 inline double vil2_bilin_interp_safe_extend(const vil2_image_view<T> &view,
                                             double x, double y, unsigned p=0)
 {
     return vil2_bilin_interp_safe_extend(x, y, &view(0,0,p),
-      view.ni(), view.nj(), view.istep(), view.jstep());
+                                         view.ni(), view.nj(),
+                                         view.istep(), view.jstep());
 }
 
 
