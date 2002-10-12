ae1cd7bce10d06d2c917ce2f2561099b89a02e76
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-12 16:09:12 -0400

    Don't compile this file if HAS_CMU1394CAMERA is not set

--- contrib/brl/vvid/Templates/vbl_smart_ptr+vvid_live_video_frame-.cxx
+++ contrib/brl/vvid/Templates/vbl_smart_ptr+vvid_live_video_frame-.cxx
@@ -1,5 +1,7 @@
+#ifdef HAS_CMU1394CAMERA
 #include <vvid/vvid_live_video_frame.h>
 #include <vgui/vgui_tableau.h>
 #include <vbl/vbl_smart_ptr.txx>
  
 VBL_SMART_PTR_INSTANTIATE(vvid_live_video_frame);
+#endif
--- contrib/brl/vvid/Templates/vcl_vector+vvid_live_video_frame_sptr-.cxx
+++ contrib/brl/vvid/Templates/vcl_vector+vvid_live_video_frame_sptr-.cxx
@@ -1,5 +1,6 @@
+#ifdef HAS_CMU1394CAMERA
 #include <vvid/vvid_live_video_frame_sptr.h>
 #include <vcl_vector.txx>
 
 VCL_VECTOR_INSTANTIATE(vvid_live_video_frame_sptr);
-
+#endif
