e635f13b058d6d74fb095fbc77b81f11e9d29eee
Tim Cootes <timcootes@users.sourceforge.net>
2002-10-16 08:08:58 -0400

    Fixed bugs and added algorithms

---
+++ contrib/mul/vimt/algo/vimt_find_peaks.h
@@ -0,0 +1,62 @@
+// This is mul/vimt/algo/vimt_find_peaks.h
+#ifndef vimt_find_peaks_h_
+#define vimt_find_peaks_h_
+//:
+//  \file
+//  \brief Find peaks in image
+//  \author Tim Cootes
+
+#include <vimt/vimt_image_2d_of.h>
+
+//: True if pixel at *im is strictly above 8 neighbours
+template <class T>
+inline bool vimt_is_peak_3x3(const float* im, int i_step, int j_step)
+{
+  T v = *im;
+  if (v<=im[i_step]) return false;
+  if (v<=im[-i_step]) return false;
+  if (v<=im[j_step]) return false;
+  if (v<=im[-j_step]) return false;
+  if (v<=im[i_step+j_step]) return false;
+  if (v<=im[i_step-j_step]) return false;
+  if (v<=im[j_step-i_step]) return false;
+  if (v<=im[-i_step-j_step]) return false;
+  return true;
+}
+
+//: Return image co-ordinates of all points in image strictly above their 8 neighbours
+template <class T>
+inline void vimt_find_image_peaks_3x3(vcl_vector<vgl_point_2d<int> >& peaks,
+                                      const vil2_image_2d_of<T>& image, unsigned plane=0)
+{
+  unsigned ni=image.ni(),nj=image.nj();
+  int istep = image.istep(),jstep=image.jstep();
+  const T* row = image.top_left_ptr()+plane*im.planestep()+istep+jstep;
+  for (int j=1;j<nj;++j,row+=jstep)
+  {
+    const T* pixel = row;
+	for (int i=1;i<ni;++i,pixel+=istep)
+	  if (vimt_is_peak_3x3(pixel,istep,jstep)) peaks.push_back(vgl_point_2d<int>(i,j));
+  }
+}
+
+//: Return world co-ordinates of all points in image strictly above their 8 neighbours
+template <class T>
+inline void vimt_find_world_peaks_3x3(vcl_vector<vgl_point_2d<double> >& peaks,
+                                      const vimt_image_2d_of<T>& image, unsigned plane=0)
+{
+  const vil2_image_view<T>& im = image.image();
+  vimt_transform_2d im2w = image.world2im().inverse();
+  unsigned ni=im.ni(),nj=im.nj();
+  int istep = im.istep(),jstep=im.jstep();
+  const T* row = im.top_left_ptr()+plane*im.planestep()+istep+jstep;
+  for (int j=1;j<nj;++j,row+=jstep)
+  {
+    const T* pixel = row;
+	for (int i=1;i<ni;++i,pixel+=istep)
+	  if (vimt_is_peak_3x3(pixel,istep,jstep)) peaks.push_back(im2w(i,j));
+  }
+}
+
+
+#endif // vimt_find_peaks_h_
--- contrib/mul/vimt/vimt_convolve_2d.h
+++ contrib/mul/vimt/vimt_convolve_2d.h
@@ -15,9 +15,9 @@
 //
 // On exit dest(x,y) = sum_ij src_im(x+i,y+j)*kernel(i,j)
 //
-// dest_im.world2im() is set up so that dest_im.image(x,y) is the convolution
+// dest_im.world2im() is set up so that vimt_bilin_interp(dest_im,p) is the convolution
 // of the kernel with the source image when the kernel's reference point is
-// placed at dest_im.world2im(x,y) in src_im
+// placed at p in src_im
 // \relates vil2_image_view
 template <class srcT, class destT, class kernelT, class accumT>
 inline void vil2_convolve_2d(const vimt_image_2d_of<srcT>& src_im,
@@ -28,10 +28,8 @@ inline void vil2_convolve_2d(const vimt_image_2d_of<srcT>& src_im,
 {
   vil2_convolve_2d(src_im.image(),dest_im.image(),kernel,ac);
 	vimt_transform_2d offset;
-	offset.set_translation(kernel_ref_pt.x(),kernel_ref_pt.y());
+	offset.set_translation(-kernel_ref_pt.x(),-kernel_ref_pt.y());
   dest_im.set_world2im(offset * src_im.world2im());
-	// Point (i,j) in dest corresponds to kernel placed with (0,0) at
-	// (i,j) in src, so the ref point is at (i,j)+kernel_ref_pt
 }
 
 #endif // vimt_convolve_2d_h_
