ebcb3229b171ba6a5554329145053d91ae7f7c52
Tim Cootes <timcootes@users.sourceforge.net>
2002-10-16 09:32:10 -0400

    Added templates

---
+++ core/vil/Templates/vil_resample_bilin+byte.float-.cxx
@@ -0,0 +1,3 @@
+#include <vil2/vil2_resample_bilin.txx>
+#include <vxl_config.h>
+VIL2_RESAMPLE_BILIN_INSTANTIATE( vxl_byte , float );
---
+++ core/vil/Templates/vil_sample_grid_bilin+float.float-.cxx
@@ -0,0 +1,2 @@
+#include <vil2/vil2_sample_grid_bilin.txx>
+VIL2_SAMPLE_GRID_BILIN_INSTANTIATE( float , float );
