d1f658947107038540c2d03a11427d310f9979b2
Ian Scott <scottim@users.sourceforge.net>
2002-10-17 05:33:59 -0400

    Push up the coverage on vil2_gauss_reduce.cxx

--- core/vil/tests/test_algo_gauss_reduce.cxx
+++ core/vil/tests/test_algo_gauss_reduce.cxx
@@ -6,7 +6,7 @@
 #include <vil2/vil2_image_view.h>
 #include <vil2/algo/vil2_gauss_reduce.h>
 
-void test_algo_gauss_reduce_byte(int nx)
+void test_algo_gauss_reduce_byte(unsigned nx)
 {
   vcl_cout << "*********************************************\n"
            << " Testing vil2_algo_gauss_reduce (byte)(nx="<<nx<<")\n"
@@ -45,6 +45,63 @@ void test_algo_gauss_reduce_byte(int nx)
   vil2_image_view<vxl_byte> image1;
   image1.resize(3,nx);
   vil2_image_view<vxl_byte> reduced_y;
+  reduced_y.resize(3,(nx+1)/2);
+
+  for (unsigned int j=0;j<image1.nj();++j)
+    for (unsigned int i=0;i<image1.ni();++i)
+      image1(i,j) = i+j*10;
+
+  vil2_gauss_reduce(image1.top_left_ptr(),image1.nj(),image1.ni(),
+                     image1.jstep(),image1.istep(),
+                     reduced_y.top_left_ptr(),reduced_y.jstep(),reduced_y.istep());
+
+  vcl_cout<<"Original: "; vil2_print_all(vcl_cout,image1); vcl_cout<<vcl_endl;
+  vcl_cout<<"reduced_y : "; vil2_print_all(vcl_cout,reduced_y); vcl_cout<<vcl_endl;
+
+  TEST("First element",reduced_y(1,0),image1(1,0));
+  TEST("Next element", reduced_y(1,1),image1(1,2));
+  TEST("Last element", reduced_y(1,L),image1(1,2*L));
+}
+
+void test_algo_gauss_reduce_int_32(unsigned nx)
+{
+  vcl_cout << "*********************************************\n"
+           << " Testing vil2_algo_gauss_reduce (int_32)(nx="<<nx<<")\n"
+           << "*********************************************\n";
+
+  vil2_image_view<vxl_int_32> image0;
+  image0.resize(nx,3);
+  vil2_image_view<vxl_int_32> reduced_x;
+  reduced_x.resize((nx+1)/2,3);
+
+  for (unsigned int j=0;j<image0.nj();++j)
+    for (unsigned int i=0;i<image0.ni();++i)
+      image0(i,j) = i+j*10;
+
+  vil2_gauss_reduce(image0.top_left_ptr(),image0.ni(),image0.nj(),
+                     image0.istep(),image0.jstep(),
+                     reduced_x.top_left_ptr(),reduced_x.istep(),reduced_x.jstep());
+
+  vcl_cout<<"Original: "; vil2_print_all(vcl_cout,image0); vcl_cout<<vcl_endl;
+  vcl_cout<<"reduced_x : "; vil2_print_all(vcl_cout,reduced_x); vcl_cout<<vcl_endl;
+
+  TEST("First element",reduced_x(0,1),image0(0,1));
+  TEST("Next element", reduced_x(1,1),image0(2,1));
+  unsigned L = (nx-1)/2;
+  TEST("Last element", reduced_x(L,1),image0(2*L,1));
+
+  vil2_image_view<vxl_int_32> test2;
+  test2.resize(nx,3);
+  test2.fill(222);
+  vil2_gauss_reduce(image0.top_left_ptr(),image0.ni(),image0.nj(),
+                     image0.istep(),image0.jstep(),
+                     test2.top_left_ptr(),test2.istep(),test2.jstep());
+  TEST("No overrun",test2(L+1,1),222);
+
+  // Test it can be used to smooth in y by swapping ordinates
+  vil2_image_view<vxl_int_32> image1;
+  image1.resize(3,nx);
+  vil2_image_view<vxl_int_32> reduced_y;
   reduced_y.resize(3,(nx+1)/2);
 
   for (unsigned int j=0;j<image1.nj();++j)
@@ -146,6 +203,54 @@ void test_algo_gauss_reduce_121_byte(unsigned nx, unsigned ny)
   vcl_cout<<"Value at (1,1):"<<int(test2(1,1))<<vcl_endl;
 }
 
+
+void test_algo_gauss_reduce_121_int_32(unsigned nx, unsigned ny)
+{
+  vcl_cout << "*******************************************************\n"
+           << " Testing vil2_algo_gauss_reduce_121 (int32)(nx="<<nx<<", ny="<<ny<<")\n"
+           << "*******************************************************\n";
+
+  vil2_image_view<vxl_int_32> image0;
+  image0.resize(nx,ny);
+  vil2_image_view<vxl_int_32> reduced_x;
+  reduced_x.resize((nx+1)/2,(ny+1)/2);
+
+  for (unsigned int j=0;j<image0.nj();++j)
+    for (unsigned int i=0;i<image0.ni();++i)
+      image0(i,j) = i+j*10;
+
+  vil2_gauss_reduce_121(image0.top_left_ptr(),image0.ni(),image0.nj(),
+                         image0.istep(),image0.jstep(),
+                         reduced_x.top_left_ptr(),reduced_x.istep(),reduced_x.jstep());
+
+  vcl_cout<<"Original: "; vil2_print_all(vcl_cout,image0); vcl_cout<<vcl_endl;
+  vcl_cout<<"reduced_x : "; vil2_print_all(vcl_cout,reduced_x); vcl_cout<<vcl_endl;
+
+  TEST("First element",reduced_x(0,1),image0(0,2));
+  TEST("Next element",reduced_x(1,1),image0(2,2));
+  unsigned Lx = (nx+1)/2;
+  unsigned Ly = (ny+1)/2;
+  TEST("Last element in x",reduced_x(Lx-1,1),image0(2*(Lx-1),2));
+  TEST("Last element in y",reduced_x(1,Ly-1),image0(2,2*(Ly-1)));
+
+  vil2_image_view<vxl_int_32> test2;
+  test2.resize(nx,ny);
+  test2.fill(222);
+  vil2_gauss_reduce_121(image0.top_left_ptr(),image0.ni(),image0.nj(),
+                         image0.istep(),image0.jstep(),
+                         test2.top_left_ptr(),test2.istep(),test2.jstep());
+  TEST("No overrun in x",test2(Lx,1), 222);
+  TEST("No overrun in y",test2(1,Ly), 222);
+
+  image0.fill(17);
+  vil2_gauss_reduce_121(image0.top_left_ptr(),image0.ni(),image0.nj(),
+                        image0.istep(),image0.jstep(),
+                        test2.top_left_ptr(),test2.istep(),test2.jstep());
+  TEST("Smoothing correct",test2(1,1), 17);
+  vcl_cout<<"Value at (1,1):"<<int(test2(1,1))<<vcl_endl;
+}
+
+
 void test_algo_gauss_reduce_121_float(unsigned nx, unsigned ny)
 {
   vcl_cout << "********************************************************\n"
@@ -220,11 +325,15 @@ MAIN( test_algo_gauss_reduce )
 
   test_algo_gauss_reduce_byte(7);
   test_algo_gauss_reduce_byte(6);
+  test_algo_gauss_reduce_int_32(7);
+  test_algo_gauss_reduce_int_32(6);
   test_algo_gauss_reduce_float(7);
   test_algo_gauss_reduce_float(6);
 
   test_algo_gauss_reduce_121_byte(6,6);
   test_algo_gauss_reduce_121_byte(7,7);
+  test_algo_gauss_reduce_121_int_32(6,6);
+  test_algo_gauss_reduce_121_int_32(7,7);
   test_algo_gauss_reduce_121_float(6,6);
   test_algo_gauss_reduce_121_float(7,7);
 
