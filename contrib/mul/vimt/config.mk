1fb8f059520e72bc2b30c62da8378c011408168a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-18 07:47:02 -0400

    updated w.r.t. CMakeLists.txt

--- contrib/mul/vimt/config.mk
+++ contrib/mul/vimt/config.mk
@@ -1,5 +1,6 @@
 IULIBS += -lvimt
 include ${IUEROOT}/mul/vil2/algo/config.mk
+include ${IUEROOT}/mul/vil2/io/config.mk
 include ${IUEROOT}/vxl/vnl/algo/config.mk
 include ${IUEROOT}/vxl/vgl/config.mk
 include ${IUEROOT}/vxl/vsl/config.mk
