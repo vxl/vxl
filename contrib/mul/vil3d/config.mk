9944f07e7a67d373a8f162459ba1d1199cb97bca
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-18 08:12:44 -0400

    updated w.r.t. CMakeLists.txt

---
+++ contrib/mul/vil3d/config.mk
@@ -0,0 +1,2 @@
+IULIBS += -lvil3d
+include ${IUEROOT}/mul/vil2/algo/config.mk
---
+++ contrib/mul/vil3d/io/config.mk
@@ -0,0 +1,4 @@
+IULIBS += -lvil3d-io
+include ${IUEROOT}/mul/vil3d/config.mk
+include ${IUEROOT}/mul/vil2/io/config.mk
+include ${IUEROOT}/vxl/vsl/config.mk
