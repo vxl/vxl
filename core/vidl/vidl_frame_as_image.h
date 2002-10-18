3806b21721e0627b06efb27d5acd36b9730183b8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-18 13:16:15 -0400

    put_section() removed

--- core/vidl/vidl_frame_as_image.h
+++ core/vidl/vidl_frame_as_image.h
@@ -1,6 +1,5 @@
 #ifndef vidl_frame_as_image_h
 #define vidl_frame_as_image_h
-
 //:
 // \file
 // \author Nicolas Dano, september 1999
@@ -12,7 +11,6 @@
 // 10/4/2001 Ian Scott (Manchester) Converted perceps header to doxygen
 // \endverbatim
 
-
 #include <vil/vil_image_impl.h>
 #include <vidl/vidl_frame_as_image_sptr.h>
 #include <vidl/vidl_frame_sptr.h>
@@ -22,24 +20,24 @@
 //   an image from a frame.
 class vidl_frame_as_image : public vil_image_impl
 {
-   public:
+ public:
 
-     vidl_frame_as_image(vidl_frame* frame);
-     ~vidl_frame_as_image();
+  vidl_frame_as_image(vidl_frame* frame);
+  ~vidl_frame_as_image();
 
-     vil_component_format component_format() const { return VIL_COMPONENT_FORMAT_UNSIGNED_INT; }
-     int bits_per_component() const { return 8; }
-     int components() const;
-     int planes() const { return 1; }
-     int width() const { return frame_->width(); }
-     int height() const { return frame_->height(); }
-     bool get_section(void* ib, int x0, int y0, int width, int height) const;
-     bool put_section(void const* ib, int x0, int y0, int width, int height) { return false; }
+  vil_component_format component_format() const { return VIL_COMPONENT_FORMAT_UNSIGNED_INT; }
+  int bits_per_component() const { return 8; }
+  int components() const;
+  int planes() const { return 1; }
+  int width() const { return frame_->width(); }
+  int height() const { return frame_->height(); }
+  bool get_section(void* ib, int x0, int y0, int width, int height) const;
+  bool put_section(void const* /*ib*/, int /*x0*/, int /*y0*/, int /*width*/, int /*height*/) { return false; }
 
-   protected:
+ protected:
 
   // Dumb ptr to avoid cycle
-     vidl_frame* frame_;
+  vidl_frame* frame_;
 };
 
 #endif // vidl_frame_as_image_h
