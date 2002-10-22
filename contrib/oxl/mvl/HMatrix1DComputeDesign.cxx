9abef2e3eb651bf8f5ca00963f8410de3fc82198
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-22 07:55:32 -0400

    simplified

--- contrib/oxl/mvl/HMatrix1DComputeDesign.cxx
+++ contrib/oxl/mvl/HMatrix1DComputeDesign.cxx
@@ -22,14 +22,11 @@ HMatrix1DComputeDesign::compute_cool_homg(const vcl_vector<HomgPoint1D>&P,
 {
   unsigned N=P.size(); assert(N==Q.size());
   vnl_matrix<double> D(N,4);
-  vnl_double_2 p,q;
   for (unsigned i=0;i<N;i++) {
-    p=P[i].get_vector();
-    q=Q[i].get_vector();
-    D(i,0)=p(0)*q(1);
-    D(i,1)=p(1)*q(1);
-    D(i,2)=-p(0)*q(0);
-    D(i,3)=-p(1)*q(0);
+    D(i,0)=P[i].x()*Q[i].w();
+    D(i,1)=P[i].w()*Q[i].w();
+    D(i,2)=-P[i].x()*Q[i].x();
+    D(i,3)=-P[i].w()*Q[i].x();
   }
   vnl_svd<double> svd(D);
   M->set(svd.nullvector().data_block());
