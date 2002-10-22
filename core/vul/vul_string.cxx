fa412476150291f115892a6bde8788fe9ad5629c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-22 04:42:38 -0400

    toupper() must be vcl_toupper()

--- core/vul/vul_string.cxx
+++ core/vul/vul_string.cxx
@@ -5,18 +5,18 @@
 #include <vcl_cstdlib.h>
 #include <vcl_cstring.h>
 #include <vcl_cctype.h>
-
 #include <vcl_algorithm.h> // for vcl_transform
 
-#define TO_LOWER tolower                        // use ANSI functions
-#define TO_UPPER toupper
+#define TO_LOWER vcl_tolower                    // use ANSI functions
+#define TO_UPPER vcl_toupper
 
 #ifndef END_OF_STRING                           // If END_OF_STRING not defined
 #define END_OF_STRING (0)
 #endif
 
 // Converts all alphabetical characters to uppercase.
-char* vul_string_c_upcase (char* s) {      // Convert entire string to upper case
+char* vul_string_c_upcase (char* s)      // Convert entire string to upper case
+{
   char* p = s;                  // Point to beginning of string
   while (*p) {                  // While there are still valid characters
     if (vcl_islower (*p))       // if this is lower case
@@ -27,7 +27,8 @@ char* vul_string_c_upcase (char* s) {      // Convert entire string to upper cas
 }
 
 // Converts all alphabetical characters to lowercase.
-char* vul_string_c_downcase (char* s) {    // Convert entire string to lower case
+char* vul_string_c_downcase (char* s)    // Convert entire string to lower case
+{
   char* p = s;                  // Point to beginning of string
   while (*p) {                  // While there are still valid characters
     if (vcl_isupper (*p))       // if this is upper case
@@ -39,20 +40,22 @@ char* vul_string_c_downcase (char* s) {    // Convert entire string to lower cas
 
 // Capitalizes all words in a string. A word is defined as
 // a sequence of characters separated by non-alphanumerics.
-char* vul_string_c_capitalize (char* s) {          // Capitalize each word in string
+char* vul_string_c_capitalize (char* s)       // Capitalize each word in string
+{
   char* p = s;                          // Point to beginning of string
   while (true) {                        // Infinite loop
     for (; *p && !vcl_isalnum(*p); p++);// Skip to first alphanumeric
     if (*p == END_OF_STRING)            // If end of string
       return s;                         // Return string
     *p = TO_UPPER(*p);                  // Convert character
-    while(*++p && vcl_isalnum (*p));    // Search for next word
+    while (*++p && vcl_isalnum (*p));   // Search for next word
   }
 }
 
 // Removes any occurrence of the string rem from string str,
 // and returns the modified string str.
-char* vul_string_c_trim (char* str, const char* rem) {     // Trim characters from string
+char* vul_string_c_trim (char* str, const char* rem) // Trim characters from string
+{
   char* s = str;
   char* result = str;
   register char c;
@@ -69,7 +72,8 @@ char* vul_string_c_trim (char* str, const char* rem) {     // Trim characters fr
 
 // Removes any prefix occurrence of the string rem from
 // the first string str, and returns the modified string str.
-char* vul_string_c_left_trim (char* str, const char* rem) { // Trim prefix from string
+char* vul_string_c_left_trim (char* str, const char* rem) // Trim prefix from string
+{
   char* result = str;
   char* s;
   register char c;
@@ -80,15 +84,16 @@ char* vul_string_c_left_trim (char* str, const char* rem) { // Trim prefix from 
     if (t == END_OF_STRING)                      // If no match found
       break;
   }
-  if (s != result)                                // when characters trimed
+  if (s != result)                               // when characters trimed
     while ((*result++ = *s++) != END_OF_STRING); // shift string down
-  return str;                                     // Return pointer to string
+  return str;                                    // Return pointer to string
 }
 
 // Removes any suffix occurrence of the string rem
 // from the first string str, and returns the modified string str.
-char* vul_string_c_right_trim (char* str, const char* rem) { // Trim suffix from string
-  char* s = str + vcl_strlen(str) - 1;                // last character of str
+char* vul_string_c_right_trim (char* str, const char* rem) // Trim suffix from string
+{
+  char* s = str + vcl_strlen(str) - 1;           // last character of str
   for (; s >= str; s--) {
     register const char* r = rem;
     register char t;
@@ -102,12 +107,14 @@ char* vul_string_c_right_trim (char* str, const char* rem) { // Trim suffix from
 }
 
 // Reverses the order of the characters in char*.
-void vul_string_c_reverse (char* c) {                // Reverse the order of characters
-  int length = vcl_strlen (c);              // Number of characters in string
+void vul_string_c_reverse (char* c)          // Reverse the order of characters
+{
+  int length = vcl_strlen (c);          // Number of characters in string
   char temp;
  
   for (int i = 0, j = length-1;         // Counting from front and rear
-       i < length / 2; i++, j--) {      // until we reach the middle
+       2*i < length; ++i, --j)          // until we reach the middle
+  {
     temp = c[i];                        // Save front character
     c[i] = c[j];                        // Switch with rear character
     c[j] = temp;                        // Copy new rear character
@@ -117,14 +124,14 @@ void vul_string_c_reverse (char* c) {                // Reverse the order of cha
 // Converts all alphabetical characters in string s to uppercase.
 vcl_string& vul_string_upcase(vcl_string& s)
 {
-  vcl_transform( s.begin(), s.end(), s.begin(), toupper );
+  vcl_transform( s.begin(), s.end(), s.begin(), vcl_toupper );
   return s;
 }
 
 // Converts all alphabetical characters in string s to lowercase.
 vcl_string& vul_string_downcase(vcl_string& s)
 {
-  vcl_transform( s.begin(), s.end(), s.begin(), tolower );
+  vcl_transform( s.begin(), s.end(), s.begin(), vcl_tolower );
   return s;
 }
 
@@ -136,11 +143,11 @@ vcl_string& vul_string_capitalize(vcl_string& s)
   // transition.
   vcl_string::iterator si;
   bool in_word = false;
-  for( si = s.begin(); si != s.end(); ++si ) {
-    if( !in_word && vcl_isalnum( *si ) ) {
-      *si = toupper( *si );
+  for ( si = s.begin(); si != s.end(); ++si ) {
+    if ( !in_word && vcl_isalnum( *si ) ) {
+      *si = vcl_toupper( *si );
       in_word = true;
-    } else if( in_word && !vcl_isalnum( *si ) ) {
+    } else if ( in_word && !vcl_isalnum( *si ) ) {
       in_word = false;
     }
   }
