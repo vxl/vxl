dcac4fb91b6acc4437d4d602a68ae465bfd68307
Amitha Perera <amithaperera@users.sourceforge.net>
2002-10-23 19:34:44 -0400

    Convert tabs to spaces. Fix code layout. Eliminate unused variables

--- core/vil/tests/vil_test_random_access_timings.cxx
+++ core/vil/tests/vil_test_random_access_timings.cxx
@@ -21,10 +21,10 @@ double method1(vil2_image_view<imT>& image,
                const int* x, const int* y, int n_pts, int n_loops)
 {
   vcl_time_t t0=vcl_clock();
-	double sum=0.0;
+  double sum=0.0;
   for (int n=0;n<n_loops;++n)
   {
-	  for (int i=0;i<n_pts;++i)
+    for (int i=0;i<n_pts;++i)
       sum+=(double) image(x[i],y[i]);
   }
   vcl_time_t t1=vcl_clock();
@@ -36,10 +36,10 @@ double method2(vil2_image_view<imT>& image,
                const int* x, const int* y, int n_pts, int n_loops)
 {
   vcl_time_t t0=vcl_clock();
-	double sum=0.0;
+  double sum=0.0;
   for (int n=0;n<n_loops;++n)
   {
-	  for (int i=0;i<n_pts;++i)
+    for (int i=0;i<n_pts;++i)
       sum+=(double) image.top_left_ptr()[x[i]*image.istep()+y[i]*image.jstep()];
   }
   vcl_time_t t1=vcl_clock();
@@ -57,16 +57,16 @@ double method3(vil2_image_view<imT>& image,
   imT* raster_ptrs[NJ];
 
   {
-    unsigned ni=image.ni(),nj=image.nj(),np=image.nplanes();
+    unsigned nj=image.nj();
     for (unsigned j=0;j<nj;++j)
         (raster_ptrs)[j] = & image(0,j,0);
   }
 
   vcl_time_t t0=vcl_clock();
-	double sum=0.0;
+  double sum=0.0;
   for (int n=0;n<n_loops;++n)
   {
-	  for (int i=0;i<n_pts;++i)
+    for (int i=0;i<n_pts;++i)
       sum+=(double) raster_ptrs[y[i]][x[i]];
   }
   vcl_time_t t1=vcl_clock();
@@ -76,7 +76,7 @@ double method3(vil2_image_view<imT>& image,
 
 template <class imT>
 double method(int i, vil2_image_view<imT>& image,
-               const int* x, const int* y, int n_pts, int n_loops)
+              const int* x, const int* y, int n_pts, int n_loops)
 {
   double t;
   switch (i)
@@ -107,48 +107,48 @@ void compute_stats(int i, vil2_image_view<imT>& image,
 template<class T>
 inline double bilin_interp1(double x, double y, const T* data, int xstep, int ystep)
 {
-    int p1x=int(x);
-    double normx = x-p1x;
-    int p1y=int(y);
-    double normy = y-p1y;
+  int p1x=int(x);
+  double normx = x-p1x;
+  int p1y=int(y);
+  double normy = y-p1y;
 
-    const T* pix1 = data + p1y*ystep + p1x*xstep;
+  const T* pix1 = data + p1y*ystep + p1x*xstep;
 
-    double i1 = pix1[0    ]+(pix1[      ystep]-pix1[0    ])*normy;
-    double i2 = pix1[xstep]+(pix1[xstep+ystep]-pix1[xstep])*normy;
+  double i1 = pix1[0    ]+(pix1[      ystep]-pix1[0    ])*normy;
+  double i2 = pix1[xstep]+(pix1[xstep+ystep]-pix1[xstep])*normy;
 
-    return i1+(i2-i1)*normx;
+  return i1+(i2-i1)*normx;
 }
 
 template<class T>
 inline double bilin_interp2(double x, double y, const T** data)
 {
-    int p1x=int(x);
-    double normx = x-p1x;
-    int p1y=int(y);
-    double normy = y-p1y;
+  int p1x=int(x);
+  double normx = x-p1x;
+  int p1y=int(y);
+  double normy = y-p1y;
 
-    const T* row1=data[p1y];
-		const T* row2=data[p1y+1];
+  const T* row1=data[p1y];
+  const T* row2=data[p1y+1];
 
-    double i1 = row1[p1x]+(row1[p1x+1]-row1[p1x])*normx;
-    double i2 = row2[p1x]+(row2[p1x+1]-row2[p1x])*normx;
+  double i1 = row1[p1x]+(row1[p1x+1]-row1[p1x])*normx;
+  double i2 = row2[p1x]+(row2[p1x+1]-row2[p1x])*normx;
 
-    return i1+(i2-i1)*normy;
+  return i1+(i2-i1)*normy;
 }
 
 template <class imT>
 double bilin_method1(vil2_image_view<imT>& image, int n_pts, int n_loops)
 {
   vcl_time_t t0=vcl_clock();
-	double sum=0.0;
-	double x = 1.3,y=1.7;
+  double sum=0.0;
+  double x = 1.3,y=1.7;
   for (int n=0;n<n_loops;++n)
   {
-	  int istep=image.istep(),jstep=image.jstep();
-		imT* plane = image.top_left_ptr();
-	  for (int i=0;i<n_pts;++i,x+=0.01,y+=0.02)
-      sum+=(double) bilin_interp1(x,y,plane,istep,jstep);
+      int istep=image.istep(),jstep=image.jstep();
+      imT* plane = image.top_left_ptr();
+      for (int i=0;i<n_pts;++i,x+=0.01,y+=0.02)
+        sum+=(double) bilin_interp1(x,y,plane,istep,jstep);
   }
   vcl_time_t t1=vcl_clock();
   return 1e9*(double(t1)-double(t0))/(double(n_pts)*n_loops*CLOCKS_PER_SEC);
@@ -158,11 +158,11 @@ template <class imT>
 double bilin_method2(vil2_image_view<imT>& image, int n_pts, int n_loops)
 {
   vcl_time_t t0=vcl_clock();
-	double sum=0.0;
-	double x = 1.3,y=1.7;
+  double sum=0.0;
+  double x = 1.3,y=1.7;
   for (int n=0;n<n_loops;++n)
   {
-	  for (int i=0;i<n_pts;++i,x+=0.01,y+=0.02)
+    for (int i=0;i<n_pts;++i,x+=0.01,y+=0.02)
       sum+=(double) bilin_interp1(x,y,image.top_left_ptr(),image.istep(),image.jstep());
   }
   vcl_time_t t1=vcl_clock();
@@ -178,17 +178,17 @@ double bilin_method3(vil2_image_view<imT>& image, int n_pts, int n_loops)
   const imT* raster_ptrs[NJ];
 
   {
-    unsigned ni=image.ni(),nj=image.nj(),np=image.nplanes();
+    unsigned nj=image.nj();
     for (unsigned j=0;j<nj;++j)
-        (raster_ptrs)[j] = & image(0,j,0);
+      (raster_ptrs)[j] = & image(0,j,0);
   }
 
   vcl_time_t t0=vcl_clock();
-	double sum=0.0;
-	double x = 1.3,y=1.7;
+  double sum=0.0;
+  double x = 1.3,y=1.7;
   for (int n=0;n<n_loops;++n)
   {
-	  for (int i=0;i<n_pts;++i,x+=0.01,y+=0.02)
+    for (int i=0;i<n_pts;++i,x+=0.01,y+=0.02)
       sum+=(double) bilin_interp2(x,y,raster_ptrs);
   }
   vcl_time_t t1=vcl_clock();
@@ -223,12 +223,12 @@ int main(int argc, char** argv)
   vil2_image_view<vxl_byte> byte_image(NI,NJ,1);  byte_image.fill(17);
   vil2_image_view<float>    float_image(NI,NJ,1); float_image.fill(17);
   int n_loops = 10000;
-	int n_pts = 1000;
-	vcl_vector<int> x(n_pts),y(n_pts);
-	for (int i=0;i<n_pts;++i)
-	{
-	  x[i]=(i*7)%NI;
-		y[i]=(i*13)%NJ;
+  int n_pts = 1000;
+  vcl_vector<int> x(n_pts),y(n_pts);
+  for (int i=0;i<n_pts;++i)
+  {
+    x[i]=(i*7)%NI;
+    y[i]=(i*13)%NJ;
   }
 
   vcl_cout<<"Times to randomly access image (in nanosecs) [Range= 0.5(max-min)]"<<vcl_endl;
@@ -243,7 +243,7 @@ int main(int argc, char** argv)
     compute_stats(i,float_image,&x[0],&y[0],n_pts,n_loops);
   }
 
-	vcl_cout<<"Using Bilinear interpolation."<<vcl_endl;
+  vcl_cout<<"Using Bilinear interpolation."<<vcl_endl;
   vcl_cout<<"Images of BYTE"<<vcl_endl;
   for (int i=1;i<=3;++i)
   {
