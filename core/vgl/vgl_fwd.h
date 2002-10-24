aebb00e58687578f23238f19b688b4170242d4ad
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-24 07:16:17 -0400

    new class

--- core/vgl/vgl_fwd.h
+++ core/vgl/vgl_fwd.h
@@ -2,22 +2,24 @@
 #ifndef vgl_fwd_h_
 #define vgl_fwd_h_
 
-template <class T> class vgl_box_2d;
-template <class T> class vgl_box_3d;
-template <class T> class vgl_homg_line_2d;
-template <class T> class vgl_homg_line_3d_2_points;
-template <class T> class vgl_homg_plane_3d;
+template <class T> class vgl_1d_basis;
 template <class T> class vgl_homg_point_1d;
 template <class T> class vgl_homg_point_2d;
 template <class T> class vgl_homg_point_3d;
-template <class T> class vgl_line_2d;
-template <class T> class vgl_line_segment_2d;
-template <class T> class vgl_line_segment_3d;
-template <class T> class vgl_plane_3d;
 template <class T> class vgl_point_2d;
 template <class T> class vgl_point_3d;
 template <class T> class vgl_vector_2d;
 template <class T> class vgl_vector_3d;
+template <class T> class vgl_homg_line_2d;
+template <class T> class vgl_homg_line_3d_2_points;
+template <class T> class vgl_line_2d;
+template <class T> class vgl_line_3d_2_points;
+template <class T> class vgl_line_segment_2d;
+template <class T> class vgl_line_segment_3d;
+template <class T> class vgl_homg_plane_3d;
+template <class T> class vgl_plane_3d;
+template <class T> class vgl_box_2d;
+template <class T> class vgl_box_3d;
 template <class T> class vgl_conic;
 class vgl_polygon;
 class vgl_polygon_scan_iterator;
