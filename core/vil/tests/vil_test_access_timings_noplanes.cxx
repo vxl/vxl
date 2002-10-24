09885d78ca911d9e33a291e2145dd842d31b858e
Ian Scott <scottim@users.sourceforge.net>
2002-10-24 09:40:20 -0400

    more tests

---
+++ core/vil/tests/vil_test_access_timings_noplanes.cxx
@@ -0,0 +1,229 @@
+//:
+// \file
+// \brief Tool to test performance of different methods of accessing image data
+//        When run, tries a variety of different approaches and reports their timings.
+//        Useful to try it on different platforms to how different optimisers perform.
+// \author Tim Cootes
+
+#include <vcl_iostream.h>
+#include <vxl_config.h> // for imT
+#include <vil2/vil2_image_view.h>
+#include <vcl_ctime.h>
+#include <mbl/mbl_stats_1d.h>
+#include <vcl_vector.h>
+#include <vil/vil_rgb.h>
+
+const unsigned NI=256;
+const unsigned NJ=256;
+
+template <class imT>
+double method1(vil2_image_view<imT>& image, int n_loops)
+{
+  vcl_time_t t0=vcl_clock();
+  for (int n=0;n<n_loops;++n)
+  {
+    for (unsigned j=0;j<image.nj();++j)
+      for (unsigned i=0;i<image.ni();++i)
+        image(i,j) = imT(i+j);
+  }
+  vcl_time_t t1=vcl_clock();
+  return 1000000*(double(t1)-double(t0))/(n_loops*CLOCKS_PER_SEC);
+}
+
+template <class imT>
+double method2(vil2_image_view<imT>& image, int n_loops)
+{
+  vcl_time_t t0=vcl_clock();
+  for (int n=0;n<n_loops;++n)
+  {
+    unsigned ni=image.ni(),nj=image.nj();
+    for (unsigned j=0;j<nj;++j)
+      for (unsigned i=0;i<ni;++i)
+        image(i,j) = imT(i+j);
+  }
+  vcl_time_t t1=vcl_clock();
+  return 1000000*(double(t1)-double(t0))/(n_loops*CLOCKS_PER_SEC);
+}
+
+template <class imT>
+double method3(vil2_image_view<imT>& image, int n_loops)
+{
+  vcl_time_t t0=vcl_clock();
+  for (int n=0;n<n_loops;++n)
+  {
+    imT* row = image.top_left_ptr();
+    for (unsigned j=0;j<image.nj();++j,row += image.jstep())
+    {
+      imT* pixel = row;
+      for (unsigned i=0;i<image.ni();++i,pixel+=image.istep())
+        *pixel = imT(i+j);
+    }
+  }
+  vcl_time_t t1=vcl_clock();
+  return 1000000*(double(t1)-double(t0))/(n_loops*CLOCKS_PER_SEC);
+}
+
+template <class imT>
+double method4(vil2_image_view<imT>& image, int n_loops)
+{
+  vcl_time_t t0=vcl_clock();
+  for (int n=0;n<n_loops;++n)
+  {
+   unsigned ni=image.ni(),nj=image.nj();
+   int istep=image.istep(),jstep=image.jstep();
+    imT* row = image.top_left_ptr();
+    for (unsigned j=0;j<nj;++j,row += jstep)
+    {
+      imT* pixel = row;
+      for (unsigned i=0;i<ni;++i,pixel+=istep)
+        *pixel = imT(i+j);
+    }
+  }
+  vcl_time_t t1=vcl_clock();
+  return 1000000*(double(t1)-double(t0))/(n_loops*CLOCKS_PER_SEC);
+}
+
+template <class imT>
+double method5(vil2_image_view<imT>& image, int n_loops)
+{
+  vcl_time_t t0=vcl_clock();
+  for (int n=0;n<n_loops;++n)
+  {
+    unsigned ni=image.ni(),nj=image.nj();
+    int istep=image.istep(),jstep=image.jstep();
+    imT* row = image.top_left_ptr();
+    for (unsigned j=0;j<nj;++j,row += jstep)
+    {
+      imT* pixel = row+(ni-1)*istep;
+      for (unsigned i=ni;i;--i,pixel-=istep)
+        *pixel = imT(i-1+j);
+    }
+  }
+  vcl_time_t t1=vcl_clock();
+  return 1000000*(double(t1)-double(t0))/(n_loops*CLOCKS_PER_SEC);
+}
+
+template <class imT>
+double method6(vil2_image_view<imT>& image, int n_loops)
+{
+  assert (image.istep() == 1);
+  // Uses row[i] to simulate lookup type access used in original vil images
+  vcl_time_t t0=vcl_clock();
+  for (int n=0;n<n_loops;++n)
+  {
+   unsigned ni=image.ni(),nj=image.nj();
+   int istep=image.istep(),jstep=image.jstep();
+    imT* row = image.top_left_ptr();
+    for (unsigned j=0;j<nj;++j,row += jstep)
+    {
+      for (unsigned i=0;i<ni;++i)
+        row[i] = imT(i+j);
+    }
+  }
+  vcl_time_t t1=vcl_clock();
+  return 1000000*(double(t1)-double(t0))/(n_loops*CLOCKS_PER_SEC);
+}
+
+template <class imT>
+double method7(vil2_image_view<imT>& image, int n_loops)
+{
+  assert (image.istep() == 1);
+  // Uses row[i] to simulate lookup type access used in original vil images
+  assert(image.ni() == NI);
+  imT* raster_ptrs[NJ];
+
+  {
+    unsigned ni=image.ni(),nj=image.nj();
+    for (unsigned j=0;j<nj;++j)
+      (raster_ptrs)[j] = & image(0,j);
+  }
+
+  vcl_time_t t0=vcl_clock();
+  for (int n=0;n<n_loops;++n)
+  {
+    unsigned ni=image.ni(),nj=image.nj();
+    for (unsigned j=0;j<nj;++j)
+    {
+      for (unsigned i=0;i<ni;++i)
+        raster_ptrs[j][i] = imT(i+j);
+    }
+  }
+  vcl_time_t t1=vcl_clock();
+  return 1000000*(double(t1)-double(t0))/(n_loops*CLOCKS_PER_SEC);
+}
+
+template <class imT>
+double method8(vil2_image_view<imT>& image, int n_loops)
+{
+  assert (image.istep() == 1);
+
+  vcl_time_t t0=vcl_clock();
+  for (int n=0;n<n_loops;++n)
+  {
+    unsigned ni=image.ni(),nj=image.nj();
+    int istep=image.istep(),jstep=image.jstep();
+    imT* row = image.top_left_ptr();
+    for (unsigned j=0;j<nj;++j,row += jstep)
+    {
+      for (unsigned i=0;i<ni;++i)
+        row[i*istep] = imT(i+j);
+    }
+  }
+  vcl_time_t t1=vcl_clock();
+  return 1000000*(double(t1)-double(t0))/(n_loops*CLOCKS_PER_SEC);
+}
+
+template <class imT>
+double method(int i, vil2_image_view<imT>& image, int n_loops)
+{
+  double t;
+  switch (i)
+  {
+    case 1 : t=method1(image,n_loops); break;
+    case 2 : t=method2(image,n_loops); break;
+    case 3 : t=method3(image,n_loops); break;
+    case 4 : t=method4(image,n_loops); break;
+    case 5 : t=method5(image,n_loops); break;
+    case 6 : t=method6(image,n_loops); break;
+    case 7 : t=method7(image,n_loops); break;
+    case 8 : t=method8(image,n_loops); break;
+    default: t=-1;
+  }
+  return t;
+}
+
+template <class imT>
+void compute_stats(int i, vil2_image_view<imT>& image, int n_loops)
+{
+  mbl_stats_1d stats;
+  for (int j=0;j<10;++j) stats.obs(method(i,image,n_loops));
+  vcl_cout<<"Method "<<i<<") Mean: "<<int(stats.mean()+0.5)
+          <<"us  +/-"<<int(0.5*(stats.max()-stats.min())+0.5)<<"us"<<vcl_endl;
+}
+
+int main(int argc, char** argv)
+{
+  vil2_image_view<vxl_byte> byte_image(NI,NJ);
+  vil2_image_view<float>    float_image(NI,NJ);
+  vil2_image_view<vil_rgb<vxl_byte> > rgb_image(NI,NJ);
+  int n_loops = 100;
+
+  vcl_cout<<"Times to fill a "<<NI<<" x "<<NJ
+          <<" image of 1 plane (in microsecs) [Range= 0.5(max-min)]"<<vcl_endl;
+  vcl_cout<<"Images of BYTE"<<vcl_endl;
+  for (int i=1;i<=8;++i)
+  {
+    compute_stats(i,byte_image,n_loops);
+  }
+  vcl_cout<<"Images of FLOAT"<<vcl_endl;
+  for (int i=1;i<=8;++i)
+  {
+    compute_stats(i,float_image,n_loops);
+  }
+  vcl_cout<<"Images of RGB<BYTE>"<<vcl_endl;
+  for (int i=1;i<=8;++i)
+  {
+    compute_stats(i,rgb_image,n_loops);
+  }
+  return 0;
+}
---
+++ core/vil/tests/vil_vil1_compare_access_timings.cxx
@@ -0,0 +1,113 @@
+//:
+// \file
+// \brief Tool to test performance of different methods of accessing image data
+//        When run, tries a variety of different approaches and reports their timings.
+//        Useful to try it on different platforms to how different optimisers perform.
+// \author Tim Cootes
+
+#include <vcl_iostream.h>
+#include <vxl_config.h> // for imT
+#include <vil2/vil2_image_view.h>
+#include <vcl_ctime.h>
+#include <mbl/mbl_stats_1d.h>
+#include <vcl_vector.h>
+#include <vil/vil_memory_image_of.h>
+#include <vil/vil_rgb.h>
+
+const unsigned NI=256;
+const unsigned NJ=256;
+
+template <class T>
+unsigned width(const vil2_image_view<T> & im){return im.ni();}
+template <class T>
+unsigned width(const vil_memory_image_of<T> & im){return im.width();}
+
+template <class T>
+unsigned height(const vil2_image_view<T> & im){return im.nj();}
+template <class T>
+unsigned height(const vil_memory_image_of<T> & im){return im.height();}
+
+template <class imT>
+double method1(imT& image, int n_loops)
+{
+  typedef imT::pixel_type PT;
+  vcl_time_t t0=vcl_clock();
+  for (int n=0;n<n_loops;++n)
+  {
+    for (unsigned j=0;j<height(image);++j)
+      for (unsigned i=0;i<width(image);++i)
+        image(i,j) = PT(i+j);
+  }
+  vcl_time_t t1=vcl_clock();
+  return 1000000*(double(t1)-double(t0))/(n_loops*CLOCKS_PER_SEC);
+}
+
+template <class imT>
+double method2(imT& image, int n_loops)
+{
+  typedef imT::pixel_type PT;
+  vcl_time_t t0=vcl_clock();
+  for (int n=0;n<n_loops;++n)
+  {
+    unsigned ni=width(image),nj=height(image);
+    for (unsigned j=0;j<nj;++j)
+      for (unsigned i=0;i<ni;++i)
+        image(i,j) = PT(i+j);
+  }
+  vcl_time_t t1=vcl_clock();
+  return 1000000*(double(t1)-double(t0))/(n_loops*CLOCKS_PER_SEC);
+}
+
+template <class imT>
+double method(int i, imT& image, int n_loops)
+{
+  double t;
+  switch (i)
+  {
+    case 1 : t=method1(image,n_loops); break;
+    case 2 : t=method2(image,n_loops); break;
+    default: t=-1;
+  }
+  return t;
+}
+
+template <class imT>
+void compute_stats(int i, imT& image, int n_loops)
+{
+  mbl_stats_1d stats;
+  for (int j=0;j<10;++j) stats.obs(method(i,image,n_loops));
+  vcl_cout<<"Method "<<i<<") Mean: "<<int(stats.mean()+0.5)
+          <<"us  +/-"<<int(0.5*(stats.max()-stats.min())+0.5)<<"us"<<vcl_endl;
+}
+
+int main(int argc, char** argv)
+{
+  vil_memory_image_of<vxl_byte> byte_1image(NI,NJ);
+  vil_memory_image_of<float>    float_1image(NI,NJ);
+  vil_memory_image_of<vil_rgb<vxl_byte> >    rgb_1image(NI,NJ);
+  vil2_image_view<vxl_byte> byte_2image(NI,NJ);
+  vil2_image_view<float>    float_2image(NI,NJ);
+  vil2_image_view<vil_rgb<vxl_byte> >    rgb_2image(NI,NJ);
+  int n_loops = 100;
+  vcl_cout<<"Times to fill a "<<NI<<" x "<<NJ
+          <<" image of 1 plane (in microsecs) [Range= 0.5(max-min)]"<<vcl_endl;
+  vcl_cout<<"vil_memory_image_of Images of BYTE"<<vcl_endl;
+  for (int i=1; i<=2; ++i)
+    compute_stats(i,byte_1image,n_loops);
+  vcl_cout<<"vil2_image_view Images of BYTE"<<vcl_endl;
+  for (int i=1; i<=2; ++i)
+    compute_stats(i,byte_2image,n_loops);
+  vcl_cout<<"vil_memory_image_of Images of FLOAT"<<vcl_endl;
+  for (int i=1; i<=2; ++i)
+    compute_stats(i,float_1image,n_loops);
+  vcl_cout<<"vil2_image_view Images of FLOAT"<<vcl_endl;
+  for (int i=1; i<=2; ++i)
+    compute_stats(i,float_2image,n_loops);
+  vcl_cout<<"vil_memory_image_of Images of RGB<BYTE>"<<vcl_endl;
+  for (int i=1; i<=2; ++i)
+    compute_stats(i,rgb_1image,n_loops);
+  vcl_cout<<"vil2_image_view Images of RGB<BYTE>"<<vcl_endl;
+  for (int i=1; i<=2; ++i)
+    compute_stats(i,rgb_2image,n_loops);
+  return 0;
+}
