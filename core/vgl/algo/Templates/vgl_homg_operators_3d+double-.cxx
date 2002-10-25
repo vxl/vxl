924577c13ec764a6f0ce700f0cb8e55d9434e6df
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-25 19:05:41 -0400

    simplified

--- core/vgl/algo/Templates/vgl_homg_operators_3d+double-.cxx
+++ core/vgl/algo/Templates/vgl_homg_operators_3d+double-.cxx
@@ -1,5 +1,3 @@
 // Instantiation of vgl_homg_operators_3d<double>
 #include <vgl/algo/vgl_homg_operators_3d.txx>
-template class vgl_homg_operators_3d<double>;
-template vgl_homg_point_2d<double> operator*(vnl_matrix_fixed<double,3,4> const& m, vgl_homg_point_3d<double> const& p);
-template vgl_homg_line_2d<double> operator*(vnl_matrix_fixed<double,3,4> const& m, vgl_homg_plane_3d<double> const& l);
+VGL_HOMG_OPERATORS_3D_INSTANTIATE(double);
--- core/vgl/algo/Templates/vgl_homg_operators_3d+float-.cxx
+++ core/vgl/algo/Templates/vgl_homg_operators_3d+float-.cxx
@@ -1,5 +1,3 @@
 // Instantiation of vgl_homg_operators_3d<float>
 #include <vgl/algo/vgl_homg_operators_3d.txx>
-template class vgl_homg_operators_3d<float>;
-template vgl_homg_point_2d<float> operator*(vnl_matrix_fixed<float,3,4> const& m, vgl_homg_point_3d<float> const& p);
-template vgl_homg_line_2d<float> operator*(vnl_matrix_fixed<float,3,4> const& m, vgl_homg_plane_3d<float> const& l);
+VGL_HOMG_OPERATORS_3D_INSTANTIATE(float);
