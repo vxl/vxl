9ad3e91bd097fce5822ae3174fe8c61f4409fd61
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-25 19:09:55 -0400

    - For speed-up, changed all fixed size matrices to vnl_matrix_fixed - As a consequence, now using vnl_inverse in several places instead of svd - Added vgl_homg_point interface next to HomgPoint, which will be fased out

--- contrib/oxl/mvl/config.mk
+++ contrib/oxl/mvl/config.mk
@@ -1,7 +1,6 @@
-#USES += oxl
 IULIBS += -lmvl
 include ${IUEROOT}/vxl/vnl/algo/config.mk
 include ${IUEROOT}/vxl/vbl/config.mk
-include ${IUEROOT}/vxl/vgl/config.mk
+include ${IUEROOT}/vxl/vgl/algo/config.mk
 include ${IUEROOT}/vxl/vul/config.mk
-include ${IUEROOT}/vxl/vil/config.mk # although not really used yet ...
+#include ${IUEROOT}/vxl/vil/config.mk # although not really used yet ...
