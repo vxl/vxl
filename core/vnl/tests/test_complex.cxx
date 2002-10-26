428e4eacc3495160806f4225259435847db0b395
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-26 06:09:47 -0400

    "cos" --> "vcl_cos"

--- core/vnl/tests/test_complex.cxx
+++ core/vnl/tests/test_complex.cxx
@@ -12,26 +12,9 @@
 
 #include <testlib/testlib_test.h>
 
-//: inverse cosine for complex numbers, implemented by Peter.Vanroose@esat.kuleuven.ac.be
+//: inverse cosine for complex numbers, by Peter.Vanroose@esat.kuleuven.ac.be
+// The implementation is at the bottom of this file.
 vcl_complex<double> tc_acos(vcl_complex<double> x);
-#if 0
-{
-  double a = vcl_real(x), b = vcl_imag(x);
-  // special cases:
-  if (b==0 && a > 1)
-    return vcl_complex<double>(0.0, vcl_log(a+vcl_sqrt(a*a-1))); // == acosh(a)
-  else if (b==0 && a >= -1)
-    return vcl_acos(a);
-
-  // the general case:
-  // the result c + d*i satisfies a = cos(c)*cosh(d), b = -sin(c)*sinh(d)
-  // hence $\frac{a^2}{\cos^2(c)} - \frac{b^2}{\sin^2(c)} = 1$.
-  double t = 0.5*(1+a*a+b*b-vcl_sqrt((a*a-1)*(a*a-1)+b*b*(b*b+2*a*a+2)));
-  // this $t = \cos^2(c)$ solves the latter biquadratic equation and lies in [0,1].
-  double aa = a/vcl_sqrt(t), bb = b/vcl_sqrt(1-t);
-  return vcl_complex<double>(vcl_acos(vcl_sqrt(t)), vcl_log(vcl_abs(aa-bb)));
-}
-#endif
 
 // make a vector with random, complex entries :
 static void fill_rand(vcl_complex<double> *b, vcl_complex<double> *e) {
@@ -93,7 +76,7 @@ void test_complex() {
     seed = (seed*16807)%2147483647L;
     double v = double(seed)/1000000000L;
     vcl_complex<double> c(u,v);
-    vcl_complex<double> d = cos(c);
+    vcl_complex<double> d = vcl_cos(c);
     vcl_complex<double> e = tc_acos(d);
     vcl_cout << c << ' ' << d << ' ' << e << '\n';
     testlib_test_assert_near("acos", c, e, 1e-12);
@@ -102,6 +85,8 @@ void test_complex() {
 
 TESTMAIN(test_complex);
 
+//: inverse cosine for complex numbers.
+// implementation by Peter.Vanroose@esat.kuleuven.ac.be
 vcl_complex<double> tc_acos(vcl_complex<double> x)
 {
   double a = vcl_real(x), b = vcl_imag(x);
@@ -117,6 +102,5 @@ vcl_complex<double> tc_acos(vcl_complex<double> x)
   double t = 0.5*(1+a*a+b*b-vcl_sqrt((a*a-1)*(a*a-1)+b*b*(b*b+2*a*a+2)));
   // this $t = \cos^2(c)$ solves the latter biquadratic equation and lies in [0,1].
   double aa = a/vcl_sqrt(t), bb = b/vcl_sqrt(1-t);
-  double tmp = vcl_abs(aa-bb);
-  return vcl_complex<double>(vcl_acos(vcl_sqrt(t)), vcl_log(tmp));
+  return vcl_complex<double>(vcl_acos(vcl_sqrt(t)), vcl_log(vcl_fabs(aa-bb)));
 }
