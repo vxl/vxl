17793bd356f5ebab65bd6f34e83c11feb9b27e8d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-10-30 11:53:24 -0500

    Name of file corected

--- core/vnl/tests/test_cholesky.cxx
+++ core/vnl/tests/test_cholesky.cxx
@@ -1,6 +1,4 @@
-// This is vxl/vnl/tests/test_qr.cxx
-#include <vcl_iostream.h>
-#include <vcl_complex.h>
+// This is vxl/vnl/tests/test_cholesky.cxx
 #include <testlib/testlib_test.h>
 #include <vnl/vnl_matrix.h>
 #include <vnl/vnl_matlab_print.h>
@@ -9,25 +7,13 @@
 
 #include "test_util.h"
 
-// void test_matrix(char const* name, const vnl_matrix<double>& A, double det = 0)
-// {
-//   vnl_qr<double> qr(A);
-// 
-//   vcl_string n(name); n+= ": ";
-//   testlib_test_assert_near(n+"Q * R residual", (qr.Q() * qr.R() - A).fro_norm());
-//   testlib_test_assert(n+"Q * Q = I", (qr.Q().transpose() * qr.Q()).is_identity(1e-12));
-// 
-//   if (det)
-//     testlib_test_assert_near(n+ "Determinant", qr.determinant(), det, 1e-10);
-// }
-
 void test_cholesky()
 {
   vnl_matrix<double> A(3,3);
   test_util_fill_random(A.begin(), A.end());
   A = A * A.transpose();
-  
-  vnl_matrix<double> I(3,3); 
+
+  vnl_matrix<double> I(3,3);
   I.set_identity();
 
   {
@@ -38,7 +24,6 @@ void test_cholesky()
     testlib_test_assert_near("svd.inverse() ~= cholesky.inverse()",
                              (chol.inverse() - svd.inverse()).fro_norm());
   }
-
   {
     vnl_cholesky chol(A);
     testlib_test_assert_near("Ai * A - I", (chol.inverse() * A - I).fro_norm());
