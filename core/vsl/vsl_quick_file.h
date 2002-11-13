378e6dbfd07a894a4d3310d0b9e9489b1e520d10
Ian Scott <scottim@users.sourceforge.net>
2002-11-13 13:34:26 -0500

    exted quick saving and loading to 5 and 6 objects

--- core/vsl/vsl_quick_file.h
+++ core/vsl/vsl_quick_file.h
@@ -112,7 +112,7 @@ inline bool vsl_quick_file_load(T &data1,
 // Save two objects to a file 
 template <class T, class S>
 inline bool vsl_quick_file_save(const T &data1,
-                                S &data2,
+                                const S &data2,
                                 const vcl_string& path,
                                 vcl_ostream* errorStream = &vcl_cerr)
 {
@@ -171,7 +171,7 @@ inline bool vsl_quick_file_load(T &data1,
 // Save three objects to a file 
 template <class T, class S, class U>
 inline bool vsl_quick_file_save(const T &data1,
-                                S &data2, U &data3,
+                                const S &data2, const U &data3,
                                 const vcl_string& path,
                                 vcl_ostream* errorStream = &vcl_cerr)
 {
@@ -195,7 +195,7 @@ inline bool vsl_quick_file_save(const T &data1,
   return false;
 } 
 
-// Load two objects from a file 
+// Load four objects from a file 
 template <class T, class S, class U, class V>
 inline bool vsl_quick_file_load(T &data1,
                                 S &data2, U &data3, V &data4,
@@ -231,8 +231,8 @@ inline bool vsl_quick_file_load(T &data1,
 
 // Save four objects to a file 
 template <class T, class S, class U, class V>
-inline bool vsl_quick_file_save(const T &data1,
-                                S &data2, U &data3, V &data4,
+inline bool vsl_quick_file_save(const T &data1, const S &data2,
+                                const U &data3, const V &data4,
                                 const vcl_string& path,
                                 vcl_ostream* errorStream = &vcl_cerr)
 {
@@ -257,4 +257,134 @@ inline bool vsl_quick_file_save(const T &data1,
   return false;
 } 
 
+// Load five objects from a file 
+template <class T, class S, class U, class V, class W>
+inline bool vsl_quick_file_load(T &data1,
+                                S &data2, U &data3, V &data4, W &data5,
+                                const vcl_string& path,
+                                vcl_ostream* errorStream = &vcl_cerr)
+{
+  vsl_b_ifstream bfs(path);
+  if ( !(!bfs))
+  {
+    vsl_b_read(bfs,data1);
+    vsl_b_read(bfs,data2);
+    vsl_b_read(bfs,data3);
+    vsl_b_read(bfs,data4);
+    vsl_b_read(bfs,data5);
+    if (!(!bfs))
+    {
+      // Check that we have reached the end of the file.
+      char dummy;
+      vsl_b_read(bfs,dummy);
+      if (bfs.is().eof())
+      {
+        bfs.close();
+        if (errorStream)
+          *errorStream << "Successfully loaded: " << path << vcl_endl;
+        return true;
+      }
+    }
+  }
+  bfs.close();
+  if (errorStream)
+    *errorStream << "Unable to load: "<< path <<vcl_endl;
+  return false;
+}
+
+// Save five objects to a file 
+template <class T, class S, class U, class V, class W>
+inline bool vsl_quick_file_save(const T &data1, const S &data2, const U &data3,
+                                const V &data4, const W &data5,
+                                const vcl_string& path,
+                                vcl_ostream* errorStream = &vcl_cerr)
+{
+  vsl_b_ofstream bfs(path);
+  if (!(!bfs))
+  {
+    vsl_b_write(bfs,data1);
+    vsl_b_write(bfs,data2);
+    vsl_b_write(bfs,data3);
+    vsl_b_write(bfs,data4);
+    vsl_b_write(bfs,data5);
+    if (!(!bfs))
+    {
+      bfs.close();
+      if (errorStream)
+        *errorStream << "Successfully saved: "<< path <<vcl_endl;
+      return true;
+    }
+  }
+  bfs.close();
+  if (errorStream)
+    *errorStream << "Unable to save: " << path << vcl_endl;
+  return false;
+} 
+
+
+// Load six objects from a file 
+template <class T, class S, class U, class V, class W, class X>
+inline bool vsl_quick_file_load(T &data1, S &data2, U &data3,
+                                V &data4, W &data5, X &data6,
+                                const vcl_string& path,
+                                vcl_ostream* errorStream = &vcl_cerr)
+{
+  vsl_b_ifstream bfs(path);
+  if ( !(!bfs))
+  {
+    vsl_b_read(bfs,data1);
+    vsl_b_read(bfs,data2);
+    vsl_b_read(bfs,data3);
+    vsl_b_read(bfs,data4);
+    vsl_b_read(bfs,data5);
+    vsl_b_read(bfs,data6);
+    if (!(!bfs))
+    {
+      // Check that we have reached the end of the file.
+      char dummy;
+      vsl_b_read(bfs,dummy);
+      if (bfs.is().eof())
+      {
+        bfs.close();
+        if (errorStream)
+          *errorStream << "Successfully loaded: " << path << vcl_endl;
+        return true;
+      }
+    }
+  }
+  bfs.close();
+  if (errorStream)
+    *errorStream << "Unable to load: "<< path <<vcl_endl;
+  return false;
+}
+
+// Save six objects to a file 
+template <class T, class S, class U, class V, class W, class X>
+inline bool vsl_quick_file_save(const T &data1, const S &data2, const U &data3,
+                                const V &data4, const W &data5, const X &data6,
+                                const vcl_string& path,
+                                vcl_ostream* errorStream = &vcl_cerr)
+{
+  vsl_b_ofstream bfs(path);
+  if (!(!bfs))
+  {
+    vsl_b_write(bfs,data1);
+    vsl_b_write(bfs,data2);
+    vsl_b_write(bfs,data3);
+    vsl_b_write(bfs,data4);
+    vsl_b_write(bfs,data5);
+    vsl_b_write(bfs,data6);
+    if (!(!bfs))
+    {
+      bfs.close();
+      if (errorStream)
+        *errorStream << "Successfully saved: "<< path <<vcl_endl;
+      return true;
+    }
+  }
+  bfs.close();
+  if (errorStream)
+    *errorStream << "Unable to save: " << path << vcl_endl;
+  return false;
+} 
 #endif // vsl_quick_file_h_
