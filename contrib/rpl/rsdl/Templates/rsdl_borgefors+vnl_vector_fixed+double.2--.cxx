f24c5b5759aab31e47179be0176c331680f5c49e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-11-20 17:10:47 -0500

    #include<blah.h> --> #include <blah.h>

--- contrib/rpl/rsdl/Templates/rsdl_borgefors+vnl_vector_fixed+double.2--.cxx
+++ contrib/rpl/rsdl/Templates/rsdl_borgefors+vnl_vector_fixed+double.2--.cxx
@@ -1,5 +1,5 @@
-#include<rsdl/rsdl_borgefors.txx>
-#include<vnl/vnl_vector_fixed.h>
+#include <rsdl/rsdl_borgefors.txx>
+#include <vnl/vnl_vector_fixed.h>
 
 typedef vnl_vector_fixed<double,2> vec_2d;
 RSDL_BORGEFORS_INSTANTIATE(vec_2d);
