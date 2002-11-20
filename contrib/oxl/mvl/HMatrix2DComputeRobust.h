67d8a06dbb6ea3536b8a90b0e6b1f40195594fa3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-11-20 17:43:57 -0500

    implementations were moved to HMatrix2DComputeRobust.cxx

--- contrib/oxl/mvl/HMatrix2DComputeRobust.h
+++ contrib/oxl/mvl/HMatrix2DComputeRobust.h
@@ -1,6 +1,5 @@
 #ifndef HMatrix2DComputeRobust_h
 #define HMatrix2DComputeRobust_h
-
 //:
 // \file
 // \brief Robust HMatrix computation
@@ -9,27 +8,26 @@
 // Modifications
 //    22 Oct 2002 - Peter Vanroose - added vgl_homg_point_2d interface
 // \endverbatim
-//
+
 #include <mvl/HMatrix2DCompute4Point.h>
 #include <mvl/HMatrix2D.h>
 #include <mvl/AffineMetric.h>
 #include <mvl/HomgPoint2D.h>
 #include <vcl_vector.h>
 
-class HMatrix2DComputeRobust {
-public:
+class HMatrix2DComputeRobust
+{
+ public:
   HMatrix2DComputeRobust();
   virtual ~HMatrix2DComputeRobust();
 
   bool compute(PairMatchSetCorner& matches, HMatrix2D* H);
 
   // Return a HMatrix2D computed as above.
-  HMatrix2D compute(PairMatchSetCorner& matches) { return HMatrix2DComputeRobust::compute(matches); }
-  HMatrix2D compute(vcl_vector<HomgPoint2D>& points1, vcl_vector<HomgPoint2D>& points2) {
-    return HMatrix2DComputeRobust::compute(points1, points2); }
+  HMatrix2D compute(PairMatchSetCorner& matches);
+  HMatrix2D compute(vcl_vector<HomgPoint2D>& points1, vcl_vector<HomgPoint2D>& points2);
   HMatrix2D compute(vcl_vector<vgl_homg_point_2d<double> >& points1,
-                    vcl_vector<vgl_homg_point_2d<double> >& points2) {
-    return HMatrix2DComputeRobust::compute(points1, points2); }
+                    vcl_vector<vgl_homg_point_2d<double> >& points2);
 
   // Data Access
   vcl_vector<int> get_basis() const { return basis_; }
@@ -52,7 +50,7 @@ public:
   int data_size_;
   vcl_vector<double> residuals_;
   vcl_vector<bool> inliers_;
-//  AffineMetric metric_;
+  //  AffineMetric metric_;
 };
 
 #endif // HMatrix2DComputeRobust_h
