6ea83afdd014de60bc92be2f617f721e97bfd889
Tim Cootes <timcootes@users.sourceforge.net>
2002-11-25 10:49:22 -0500

    Added function

--- contrib/mul/vimt3d/vimt3d_trilin_interp.h
+++ contrib/mul/vimt3d/vimt3d_trilin_interp.h
@@ -9,7 +9,7 @@
 #include <vil3d/vil3d_trilin_interp.h>
 #include <vimt/vimt_image_2d_of.h>
 
-//: Compute trilinear interpolation at (x,y,z) in world coordinates, no bound checks
+//: Compute trilinear interpolation at (x,y,z) in world coordinates
 //  Interpolates given plane of image.image() at image.world2im(p)
 template<class T>
 inline double vimt3d_trilin_interp_safe(const vimt3d_image_3d_of<T>& image,
@@ -21,7 +21,21 @@ inline double vimt3d_trilin_interp_safe(const vimt3d_image_3d_of<T>& image,
   return vil3d_trilin_interp_safe(im_p.x(),im_p.y(),im_p.z(),
                                 im.top_left_ptr()+plane*im.planestep(),
                                 im.ni(),im.nj(),im.nk(),
-																im.istep(),im.jstep(),im.kstep());
+                                im.istep(),im.jstep(),im.kstep());
+}
+
+//: Compute trilinear interpolation at (x,y,z) in world coordinates, no bound checks
+//  Interpolates given plane of image.image() at image.world2im(p)
+template<class T>
+inline double vimt3d_trilin_interp_raw(const vimt3d_image_3d_of<T>& image,
+                                     const vgl_point_3d<double>& p,
+                                     int plane=0)
+{
+  vgl_point_3d<double> im_p = image.world2im()(p);
+  const vil3d_image_view<T>& im = image.image();
+  return vil3d_trilin_interp_raw(im_p.x(),im_p.y(),im_p.z(),
+                                im.top_left_ptr()+plane*im.planestep(),
+                                im.istep(),im.jstep(),im.kstep());
 }
 
 #endif // vimt3d_trilin_interp_h_
