84e89afa020530bbbc143f58c674f100141166f8
Amitha Perera <amithaperera@users.sourceforge.net>
2002-11-26 14:07:50 -0500

    Avoid violation of ODR for the common case of vector::iterator==pointer.

--- contrib/rpl/rrel/Templates/rrel_util_vector+double-.cxx
+++ contrib/rpl/rrel/Templates/rrel_util_vector+double-.cxx
@@ -1,4 +1,24 @@
 #include <vcl_vector.h>
 #include <rrel/rrel_util.txx>
 
-RREL_UTIL_INSTANTIATE_RAN_ITER( double, vcl_vector<double>::iterator );
+// Trigger this implicitly, because on many compilers, the
+// vector::iterator conflicts with a pointer. With GCC 3.x (only?)
+// vector::iterator _is not_ a pointer*. The implicit instantiation
+// should make the symbols weak. Therefore, if
+// vector::iterator==pointer, the symbols here will be discarded in
+// favour of the symbols in the pointer instantiation.
+
+// not static to avoid compiler warnings about unused functions.
+void
+rrel_util_vector_double_iterator_instantiation_tickler()
+{
+  typedef vcl_vector<double>::iterator Iter;
+  Iter itr;
+  double val;
+
+  rrel_util_weighted_scale( itr, itr, itr, 1, &val );
+  rrel_util_weighted_scale( itr, itr, itr, 1 );
+  rrel_util_median_and_scale_copy( itr, itr, val, val, 1 );
+  rrel_util_intercept_adjustment_copy( itr, itr, val, val, 1 );
+  rrel_util_intercept_adjust_stats_copy( itr, itr, val, val, val, 1 );
+}
