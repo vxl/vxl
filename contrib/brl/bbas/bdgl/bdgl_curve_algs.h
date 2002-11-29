ef001816e62224370a7d03424b773b521781e4da
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-11-29 02:52:15 -0500

    files should not contain ASCII 13 (carriage return); SGI compiler does not like this

--- contrib/brl/bbas/bdgl/bdgl_curve_algs.h
+++ contrib/brl/bbas/bdgl/bdgl_curve_algs.h
@@ -1,13 +1,14 @@
-#ifndef _bdgl_curve_algs_h
-#define _bdgl_curve_algs_h
+#ifndef bdgl_curve_algs_h_
+#define bdgl_curve_algs_h_
 
 //-----------------------------------------------------------------------------
-//
+//:
 // \file
 // \author J.L. Mundy
 // \brief topology algorithms involving edges or edge-centric routines
 //
 // \verbatim
+// Modifications
 // Inital version November 25, 2002
 // \endverbatim
 //
@@ -15,22 +16,22 @@
 #include <btol/dll.h>
 #include <vdgl/vdgl_edgel_chain_sptr.h>
 
-class bdgl_curve_algs 
+class bdgl_curve_algs
 {
-public:
+  // use static methods
+ public:
   static BTOL_DLL_DATA const double tol;
   ~bdgl_curve_algs();
-  // use static methods
   //:
-  // Finds the edgel index on a digital curve closest to the given 
-  // point (x, y). Later this routine can become a method on 
+  // Finds the edgel index on a digital curve closest to the given
+  // point (x, y). Later this routine can become a method on
   // vdgl_edgel_chain.
   //
   static int closest_point(vdgl_edgel_chain_sptr& dc,
-                            const double x, const double y);
-                       
+                           const double x, const double y);
+
  private:
   bdgl_curve_algs();
 };
 
-#endif
+#endif // bdgl_curve_algs_h_
