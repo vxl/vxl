cf5c2ef4bbda9eb96eb78300c3ac3fb8da65b8f3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-06 04:04:26 -0500

    no longer need dummy source file, since there is now a "real" source file in this library

--- contrib/mul/vil3d/algo/vil3d_algo_dummy.cxx
+++
@@ -1,9 +0,0 @@
-// This is mul/vil3d/algo/vil3d_algo_dummy.cxx
-//:
-// \file
-// \author Ian Scott
-// This file merely creates some code so that MSVC and other compilers will actually produce a library.
-
-
-static char * dummy = "dummy";
-
