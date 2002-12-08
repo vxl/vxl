c1e01f4b368a1e11a2dc5cd91e639307a9ff4cb5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-08 05:24:44 -0500

    TABs replaced with spaces

--- contrib/mul/vil3d/algo/vil3d_exp_filter.h
+++ contrib/mul/vil3d/algo/vil3d_exp_filter.h
@@ -1,9 +1,9 @@
 #ifndef vil3d_exp_filter_h_
 #define vil3d_exp_filter_h_
 //:
-//  \file
-//  \brief Apply exponential filter to 3D images
-//  \author Tim Cootes
+// \file
+// \brief Apply exponential filter to 3D images
+// \author Tim Cootes
 
 #include <vil2/algo/vil2_exp_filter_1d.h>
 #include <vil3d/vil3d_image_view.h>
@@ -23,19 +23,19 @@ inline void vil3d_exp_filter_i(const vil3d_image_view<srcT>& src_im,
   unsigned nk = src_im.nk();
   dest_im.set_size(ni,nj,nk,src_im.nplanes());
   vcl_ptrdiff_t s_istep = src_im.istep(), s_jstep = src_im.jstep();
-	vcl_ptrdiff_t s_kstep = src_im.kstep();
+  vcl_ptrdiff_t s_kstep = src_im.kstep();
   vcl_ptrdiff_t d_istep = dest_im.istep(),d_jstep = dest_im.jstep();
-	vcl_ptrdiff_t d_kstep = dest_im.kstep();
+  vcl_ptrdiff_t d_kstep = dest_im.kstep();
 
   for (unsigned p=0;p<src_im.nplanes();++p)
   {
     const srcT*  src_slice  = src_im.origin_ptr()+p*src_im.planestep();
     destT* dest_slice = dest_im.origin_ptr()+p*dest_im.planestep();
-		// Filter slice
+    // Filter slice
     for (unsigned k=0;k<nk;++k,src_slice+=s_kstep,dest_slice+=d_kstep)
     {
-		  const srcT* src_row = src_slice;
-			destT* dest_row = dest_slice;
+      const srcT* src_row = src_slice;
+      destT* dest_row = dest_slice;
       // Filter each row
       for (unsigned j=0;j<nj;++j,src_row+=s_jstep,dest_row+=d_jstep)
         vil2_exp_filter_1d(src_row,s_istep, dest_row,d_istep,   ni, kf);
@@ -49,27 +49,25 @@ inline void vil3d_exp_filter_i(const vil3d_image_view<srcT>& src_im,
 // \relates vil2_image_view
 template <class srcT, class destT, class accumT>
 inline void vil3d_exp_filter(const vil3d_image_view<srcT>& src_im,
-                               vil3d_image_view<destT>& dest_im,
-                               accumT kf)
+                             vil3d_image_view<destT>& dest_im,
+                             accumT kf)
 {
   // Smooth along i axis
   vil3d_image_view<destT> smooth_i;
   vil3d_exp_filter_i(src_im,smooth_i,kf);
 
-	// Smooth along j axis
+  // Smooth along j axis
   vil3d_image_view<destT> smooth_j;
   vil3d_exp_filter_i(vil3d_switch_axes_jik(smooth_i),
-	                   smooth_j,kf);
+                     smooth_j,kf);
     // Note that smooth_j is nj * ni * nk
 
   // Smooth along k axis
-	dest_im.set_size(src_im.ni(),src_im.nj(),src_im.nk(),src_im.nplanes());
+  dest_im.set_size(src_im.ni(),src_im.nj(),src_im.nk(),src_im.nplanes());
   vil3d_image_view<destT> dest_kij = vil3d_switch_axes_kij(dest_im);
 
-	// Use of vil3d_switch_axes_kji ensures axes are nk*ni*nj to match dest_kij
-  vil3d_exp_filter_i(vil3d_switch_axes_kji(smooth_j),
-	                   dest_kij,kf);
+  // Use of vil3d_switch_axes_kji ensures axes are nk*ni*nj to match dest_kij
+  vil3d_exp_filter_i(vil3d_switch_axes_kji(smooth_j), dest_kij,kf);
 }
 
-#endif
-
+#endif // vil3d_exp_filter_h_
--- contrib/mul/vil3d/tests/test_algo_exp_filter.cxx
+++ contrib/mul/vil3d/tests/test_algo_exp_filter.cxx
@@ -1,36 +1,35 @@
 // This is mul/vil3d/tests/test_algo_exp_filter.cxx
 #include <testlib/testlib_test.h>
 #include <vcl_iostream.h>
-#include <vxl_config.h> // for vxl_byte
 #include <vil3d/algo/vil3d_exp_filter.h>
 #include <vil3d/vil3d_crop.h>
 
 void test_exp_filter_float(vil3d_image_view<float>& image)
 {
-  vcl_cout << "*************************\n"
+  vcl_cout << "**************************\n"
            << " Testing vil3d_exp_filter\n"
-           << "*************************\n";
+           << "**************************\n";
 
   vil3d_image_view<float> dest;
 
-	image.fill(0.0f);
-	for (int j=0;j<image.nj();++j) image(5,j,5)=90;
+  image.fill(0.0f);
+  for (unsigned int j=0;j<image.nj();++j) image(5,j,5)=90;
 
-	vil3d_exp_filter_i(image,dest,0.5f);
-	TEST_NEAR("exp_filter_i(0)",dest(5,5,5),30.0f,0.1f);
-	TEST_NEAR("exp_filter_i(-1)",dest(4,5,5),15.0f,0.1f);
-	TEST_NEAR("exp_filter_i(1)",dest(6,5,5),15.0f,0.1f);
+  vil3d_exp_filter_i(image,dest,0.5f);
+  TEST_NEAR("exp_filter_i(0)",dest(5,5,5),30.0f,0.1f);
+  TEST_NEAR("exp_filter_i(-1)",dest(4,5,5),15.0f,0.1f);
+  TEST_NEAR("exp_filter_i(1)",dest(6,5,5),15.0f,0.1f);
 
-	image.fill(0.0f);
-	image(5,5,5)=81.0f;
-	vil3d_exp_filter(image,dest,0.5f);
-	TEST_NEAR("exp_filter(5,5,5)",dest(5,5,5),3.0f,0.01f);
-	TEST_NEAR("exp_filter(4,5,5)",dest(4,5,5),1.5f,0.01f);
-	TEST_NEAR("exp_filter(6,5,5)",dest(6,5,5),1.5f,0.01f);
-	TEST_NEAR("exp_filter(5,6,5)",dest(5,6,5),1.5f,0.01f);
-	TEST_NEAR("exp_filter(5,4,5)",dest(5,4,5),1.5f,0.01f);
-	TEST_NEAR("exp_filter(5,5,4)",dest(5,5,4),1.5f,0.01f);
-	TEST_NEAR("exp_filter(5,5,6)",dest(5,5,6),1.5f,0.01f);
+  image.fill(0.0f);
+  image(5,5,5)=81.0f;
+  vil3d_exp_filter(image,dest,0.5f);
+  TEST_NEAR("exp_filter(5,5,5)",dest(5,5,5),3.0f,0.01f);
+  TEST_NEAR("exp_filter(4,5,5)",dest(4,5,5),1.5f,0.01f);
+  TEST_NEAR("exp_filter(6,5,5)",dest(6,5,5),1.5f,0.01f);
+  TEST_NEAR("exp_filter(5,6,5)",dest(5,6,5),1.5f,0.01f);
+  TEST_NEAR("exp_filter(5,4,5)",dest(5,4,5),1.5f,0.01f);
+  TEST_NEAR("exp_filter(5,5,4)",dest(5,5,4),1.5f,0.01f);
+  TEST_NEAR("exp_filter(5,5,6)",dest(5,5,6),1.5f,0.01f);
 }
 
 void test_exp_filter_float()
