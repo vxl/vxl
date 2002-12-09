37a2a102abd7786c9ab017a4c1e0310fdffa54b8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-09 12:12:22 -0500

    "double" tests added

--- core/vgl/io/tests/test_line_3d_2_points_io.cxx
+++ core/vgl/io/tests/test_line_3d_2_points_io.cxx
@@ -1,4 +1,4 @@
-// This is vxl/vgl/io/tests/test_line_3d_2_points_io.cxx
+// This is core/vgl/io/tests/test_line_3d_2_points_io.cxx
 #include <vcl_iostream.h>
 #include <vgl/vgl_point_3d.h>
 #include <vgl/io/vgl_io_line_3d_2_points.h>
@@ -16,24 +16,59 @@ void test_line_3d_2_points_float_io()
   vgl_line_3d_2_points<float> p_out(p1, p2), p_in;
 
   vsl_b_ofstream bfs_out("vgl_line_3d_2_points_test_float_io.bvl.tmp");
-  TEST ("Writing to vgl_line_3d_2_points_test_float_io.bvl.tmp",
-        (!bfs_out), false);
+  TEST("Writing to vgl_line_3d_2_points_test_float_io.bvl.tmp",
+       (!bfs_out), false);
   vsl_b_write(bfs_out, p_out);
-  TEST ("Finished writing file successfully", (!bfs_out), false);
+  TEST("Finished writing file successfully", (!bfs_out), false);
   bfs_out.close();
 
   vsl_b_ifstream bfs_in("vgl_line_3d_2_points_test_float_io.bvl.tmp");
-  TEST ("Opened vgl_line_3d_2_points_test_float_io.bvl.tmp for reading",
-        (!bfs_in), false);
+  TEST("Opened vgl_line_3d_2_points_test_float_io.bvl.tmp for reading",
+       (!bfs_in), false);
+  vsl_b_read(bfs_in, p_in);
+  TEST("Finished reading file successfully", (!bfs_in), false);
+  bfs_in.close();
+
+  vpl_unlink("vgl_line_3d_2_points_test_float_io.bvl.tmp");
+
+  TEST("p1 == p_in.point1()", p1, p_in.point1());
+  TEST("p2 == p_in.point2()", p2, p_in.point2());
+  TEST("p_out == p_in", p_out, p_in);
+
+  vsl_print_summary(vcl_cout, p_out);
+  vcl_cout << '\n';
+}
+
+
+void test_line_3d_2_points_double_io()
+{
+  vcl_cout << "***************************************\n"
+           << "Testing vgl_line_3d_2_points<double> io\n"
+           << "***************************************\n";
+
+  //// test constructors, accessors
+  vgl_point_3d<double> p1(1.25, 3.5, -99.), p2(5.625, 7.875, 0.0);
+  vgl_line_3d_2_points<double> p_out(p1, p2), p_in;
+
+  vsl_b_ofstream bfs_out("vgl_line_3d_2_points_test_double_io.bvl.tmp");
+  TEST("Writing to vgl_line_3d_2_points_test_double_io.bvl.tmp",
+       (!bfs_out), false);
+  vsl_b_write(bfs_out, p_out);
+  TEST("Finished writing file successfully", (!bfs_out), false);
+  bfs_out.close();
+
+  vsl_b_ifstream bfs_in("vgl_line_3d_2_points_test_double_io.bvl.tmp");
+  TEST("Opened vgl_line_3d_2_points_test_double_io.bvl.tmp for reading",
+       (!bfs_in), false);
   vsl_b_read(bfs_in, p_in);
   TEST ("Finished reading file successfully", (!bfs_in), false);
   bfs_in.close();
 
-  vpl_unlink ("vgl_line_3d_2_points_test_float_io.bvl.tmp");
+  vpl_unlink("vgl_line_3d_2_points_test_double_io.bvl.tmp");
 
-  TEST ("p1 == p_in.point1()", p1, p_in.point1());
-  TEST ("p2 == p_in.point2()", p2, p_in.point2());
-  TEST ("p_out == p_in", p_out, p_in);
+  TEST("p1 == p_in.point1()", p1, p_in.point1());
+  TEST("p2 == p_in.point2()", p2, p_in.point2());
+  TEST("p_out == p_in", p_out, p_in);
 
   vsl_print_summary(vcl_cout, p_out);
   vcl_cout << '\n';
@@ -43,6 +78,7 @@ void test_line_3d_2_points_float_io()
 void test_line_3d_2_points_io()
 {
   test_line_3d_2_points_float_io();
+  test_line_3d_2_points_double_io();
 }
 
 TESTMAIN(test_line_3d_2_points_io);
