c42bc6e3e0f3cfb196e9bf1859962e1ff772807b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-09 02:58:57 -0500

    vimt_is_trough_3x3() should not be called with border pixel

--- contrib/mul/vimt/algo/vimt_find_troughs.h
+++ contrib/mul/vimt/algo/vimt_find_troughs.h
@@ -2,13 +2,14 @@
 #ifndef vimt_find_troughs_h_
 #define vimt_find_troughs_h_
 //:
-//  \file
-//  \brief Find troughs in image
-//  \author Tim Cootes
+// \file
+// \brief Find troughs in image
+// \author Tim Cootes
 
 #include <vimt/vimt_image_2d_of.h>
 
 //: True if pixel at *im is strictly below its 8 neighbours
+//  The image pixel (first argument) should *not* be a border pixel!
 template <class T>
 inline bool vimt_is_trough_3x3(const T* im, vcl_ptrdiff_t i_step, vcl_ptrdiff_t j_step)
 {
@@ -35,11 +36,12 @@ inline void vimt_find_image_troughs_3x3(vcl_vector<vgl_point_2d<unsigned> >& tro
   unsigned ni=image.ni(),nj=image.nj();
   vcl_ptrdiff_t istep = image.istep(),jstep=image.jstep();
   const T* row = image.top_left_ptr()+plane*image.planestep()+istep+jstep;
-  for (unsigned j=1;j<nj;++j,row+=jstep)
+  for (unsigned j=1;j<nj-1;++j,row+=jstep) // do not run over border
   {
     const T* pixel = row;
-    for (unsigned i=1;i<ni;++i,pixel+=istep)
-      if (vimt_is_trough_3x3(pixel,istep,jstep)) troughs.push_back(vgl_point_2d<unsigned>(i,j));
+    for (unsigned i=1;i<ni-1;++i,pixel+=istep) // do not run over border
+      if (vimt_is_trough_3x3(pixel,istep,jstep))
+        troughs.push_back(vgl_point_2d<unsigned>(i,j));
   }
 }
 
@@ -47,8 +49,8 @@ inline void vimt_find_image_troughs_3x3(vcl_vector<vgl_point_2d<unsigned> >& tro
 // \param clear_list: If true (the default) then empty list before adding new examples
 template <class T>
 inline void vimt_find_world_troughs_3x3(vcl_vector<vgl_point_2d<double> >& troughs,
-                                      const vimt_image_2d_of<T>& image,
-                                      unsigned plane=0, bool clear_list=true)
+                                        const vimt_image_2d_of<T>& image,
+                                        unsigned plane=0, bool clear_list=true)
 {
   if (clear_list) troughs.resize(0);
   const vil2_image_view<T>& im = image.image();
@@ -56,10 +58,10 @@ inline void vimt_find_world_troughs_3x3(vcl_vector<vgl_point_2d<double> >& troug
   unsigned ni=im.ni(),nj=im.nj();
   vcl_ptrdiff_t istep = im.istep(),jstep=im.jstep();
   const T* row = im.top_left_ptr()+plane*im.planestep()+istep+jstep;
-  for (unsigned j=1;j<nj;++j,row+=jstep)
+  for (unsigned j=1;j<nj-1;++j,row+=jstep) // do not run over border
   {
     const T* pixel = row;
-    for (unsigned i=1;i<ni;++i,pixel+=istep)
+    for (unsigned i=1;i<ni-1;++i,pixel+=istep) // do not run over border
       if (vimt_is_trough_3x3(pixel,istep,jstep)) troughs.push_back(im2w(i,j));
   }
 }
