722c4f17a8138c2da37cb6a62675d2d69bf0f1ab
Joseph L. Mundy <mundyj@users.sourceforge.net>
2002-12-09 18:59:00 -0500

    added a reverse method to digital_curve

--- contrib/brl/bbas/bdgl/bdgl_curve_algs.h
+++ contrib/brl/bbas/bdgl/bdgl_curve_algs.h
@@ -1,37 +1,35 @@
-#ifndef bdgl_curve_algs_h_
-#define bdgl_curve_algs_h_
+#ifndef _bdgl_curve_algs_h
+#define _bdgl_curve_algs_h
 
 //-----------------------------------------------------------------------------
-//:
+//
 // \file
 // \author J.L. Mundy
 // \brief topology algorithms involving edges or edge-centric routines
 //
 // \verbatim
-// Modifications
 // Inital version November 25, 2002
 // \endverbatim
 //
 //-----------------------------------------------------------------------------
 #include <btol/dll.h>
 #include <vdgl/vdgl_edgel_chain_sptr.h>
-
-class bdgl_curve_algs
+#include <vdgl/vdgl_digital_curve_sptr.h>
+class bdgl_curve_algs 
 {
-  // use static methods
- public:
+public:
   static BTOL_DLL_DATA const double tol;
   ~bdgl_curve_algs();
-  //:
-  // Finds the edgel index on a digital curve closest to the given
-  // point (x, y). Later this routine can become a method on
-  // vdgl_edgel_chain.
-  //
+
+  //:Finds the edgel index on a digital curve closest to the given point 
   static int closest_point(vdgl_edgel_chain_sptr& dc,
-                           const double x, const double y);
+                            const double x, const double y);
+                       
+  //:Reverses the edgel chain
+  static vdgl_digital_curve_sptr reverse(vdgl_digital_curve_sptr& dc);
 
  private:
   bdgl_curve_algs();
 };
 
-#endif // bdgl_curve_algs_h_
+#endif
