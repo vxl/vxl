8837bdbed11766356d1eeb6680b77e76a81d6278
Joseph L. Mundy <mundyj@users.sourceforge.net>
2002-12-09 20:21:10 -0500

    Templates for xml conversion classes

---
+++ contrib/brl/bbas/bxml/Templates/vcl_map+vcl_string.bxml_generic_ptr-.cxx
@@ -0,0 +1,6 @@
+// Instantiation of vcl_map<vcl_string.bxml_generic_ptr>
+#include <vcl_functional.h>
+#include <vcl_map.txx>
+#include <bxml/bxml_generic_ptr.h>
+
+VCL_MAP_INSTANTIATE(vcl_string, bxml_generic_ptr, vcl_less<vcl_string>);
---
+++ contrib/brl/bbas/bxml/Templates/vcl_vector+bxml_generic_ptr-.cxx
@@ -0,0 +1,6 @@
+#ifdef HAS_XERCES
+#include <bxml/bxml_generic_ptr.h>
+#include <vcl_vector.txx>
+
+VCL_VECTOR_INSTANTIATE(bxml_generic_ptr);
+#endif
