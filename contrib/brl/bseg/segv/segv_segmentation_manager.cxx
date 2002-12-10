1ec321f73dab6b71e33671b897adae61a22a1318
Joseph L. Mundy <mundyj@users.sourceforge.net>
2002-12-09 20:36:27 -0500

    Added region and xml methods

--- contrib/brl/bseg/segv/segv_segmentation_manager.cxx
+++ contrib/brl/bseg/segv/segv_segmentation_manager.cxx
@@ -11,6 +11,7 @@
 #include <vdgl/vdgl_digital_curve.h>
 #include <vdgl/vdgl_interpolator.h>
 #include <vdgl/vdgl_edgel_chain.h>
+#include <bxml/bxml_vtol_io.h>
 #include <sdet/sdet_detector_params.h>
 #include <sdet/sdet_detector.h>
 #include <gevd/gevd_clean_edgels.h>
@@ -30,6 +31,8 @@
 #include <vgui/vgui_shell_tableau.h>
 #include <vgui/vgui_image_tableau.h>
 #include <gevd/gevd_float_operators.h>
+#include <sdet/sdet_region_proc_params.h>
+#include <sdet/sdet_region_proc.h>
 #include <segv/segv_vtol2D_tableau.h>
 
 //static live_video_manager instance
@@ -101,7 +104,7 @@ void segv_segmentation_manager::clear_display()
 }
 
 //-----------------------------------------------------------------------------
-//: Draw the given edges onto the given location.
+//: Draw edges onto the tableau
 //-----------------------------------------------------------------------------
 void
 segv_segmentation_manager::draw_edges(vcl_vector<vtol_edge_2d_sptr>& edges,
@@ -145,6 +148,30 @@ segv_segmentation_manager::draw_edges(vcl_vector<vtol_edge_2d_sptr>& edges,
   t2D_->post_redraw();
 }
 
+void segv_segmentation_manager::draw_regions(vcl_vector<vdgl_intensity_face_sptr>& regions,
+											 bool verts)
+{
+   for(vcl_vector<vdgl_intensity_face_sptr>::iterator rit = regions.begin();
+       rit != regions.end(); rit++)
+     {
+       vtol_face_2d_sptr f = (*rit)->cast_to_face_2d();
+       _t2D->set_foreground(0.0, 1.0, 0.0);
+       _t2D->add_face(f);
+       if(verts)
+         {
+           vcl_vector<vtol_vertex_sptr>* vts = f->vertices();
+           for(vcl_vector<vtol_vertex_sptr>::iterator vit = vts->begin();
+               vit != vts->end(); vit++)
+             {
+               vtol_vertex_2d_sptr v = (*vit)->cast_to_vertex_2d();
+               _t2D->set_foreground(1.0, 0, 0.0);
+               _t2D->add_vertex(v);
+             }
+		delete vts;
+         }
+     }
+}
+
 void segv_segmentation_manager::vd_edges()
 {
   this->clear_display();
@@ -175,16 +202,55 @@ void segv_segmentation_manager::vd_edges()
       t2D_->add_vertex(*vit);
 }
 
-void segv_segmentation_manager::clean_vd_edges()
+void segv_segmentation_manager::regions()
 {
-  sdet_detector det(img_);
-  det.DoContour();
-  vcl_vector<vtol_edge_2d_sptr>* edges = det.GetEdges();
-  vcl_vector<vtol_edge_2d_sptr> broken_edges;
-  det.corner_angle=70.0;
-  // det.DoBreakCorners(*edges, broken_edges);
-  gevd_clean_edgels clean;
-  vcl_vector<vtol_edge_2d_sptr> clean_edges;
-  clean.DoCleanEdgelChains(broken_edges, clean_edges);
-  this->draw_edges(clean_edges, true);
+  this->clear_display();
+  static bool agr = true;
+  static sdet_detector_params dp;
+  vgui_dialog* vd_dialog = new vgui_dialog("VD Edges");
+  vd_dialog->field("Gaussian sigma", dp.smooth);
+  vd_dialog->field("Noise Threshold", dp.noise_multiplier);
+  vd_dialog->checkbox("Automatic Threshold", dp.automatic_threshold);
+  vd_dialog->checkbox("Agressive Closure", agr);
+  if(!vd_dialog->ask())
+    return;
+  if(agr)
+    dp.aggressive_junction_closure=1;
+  else
+    dp.aggressive_junction_closure=0;
+  bool debug = false;
+  sdet_region_proc_params rpp(debug, true, dp);
+  sdet_region_proc rp(rpp);
+  rp.set_image(_img);
+  rp.extract_regions();
+  if(debug)
+    {
+      vil_image ed_img = rp.get_edge_image();
+      vgui_image_tableau_sptr itab =  _t2D->get_image_tableau();
+	  if(!itab)
+	  {
+		  vcl_cout << "In segv_segmentation_manager::regions() - null image tableau"
+			  << vcl_endl;
+		  return;
+	  }
+      itab->set_image(ed_img);
+    }
+  if(!debug)
+    {
+   vcl_vector<vdgl_intensity_face_sptr>& regions = rp.get_regions();
+   this->draw_regions(regions, true);
+    }
+}
+
+void segv_segmentation_manager::read_xml_edges()
+{
+  vgui_dialog load_image_dlg("Load XML edges");
+  static vcl_string xml_filename = "";
+  static vcl_string ext = "*.*";
+  load_image_dlg.file("XML filename:", ext, xml_filename);
+  if (!load_image_dlg.ask())
+    return;
+  vcl_vector<vtol_edge_2d_sptr> edges;
+  if(bxml_vtol_io::read_edges(xml_filename, edges))
+    this->draw_edges(edges, true);
 }
--- contrib/brl/bseg/segv/segv_segmentation_manager.h
+++ contrib/brl/bseg/segv/segv_segmentation_manager.h
@@ -16,6 +16,7 @@
 #include <vcl_vector.h>
 #include <vil/vil_image.h>
 #include <vtol/vtol_edge_2d_sptr.h>
+#include <vdgl/vdgl_intensity_face_sptr.h>
 #include <vgui/vgui_grid_tableau.h>
 #include <vgui/vgui_shell_tableau.h>
 #include <vgui/vgui_image_tableau_sptr.h>
@@ -23,6 +24,7 @@
 #include <vgui/vgui_viewer2D_tableau_sptr.h>
 #include <vgui/vgui_dialog.h>
 #include <vgui/vgui_window.h>
+
 #include <segv/segv_vtol2D_tableau_sptr.h>
 
 //: A manager for displaying segmentation results.
@@ -36,7 +38,8 @@ class segv_segmentation_manager : public vgui_grid_tableau
   void load_image();
   void clear_display();
   void vd_edges();
-  void clean_vd_edges();
+  void regions();
+  void read_xml_edges();
   //: access to the window
   vgui_window* get_window(){return win_;}
   void set_window(vgui_window* win){win_=win;}
@@ -45,7 +48,8 @@ class segv_segmentation_manager : public vgui_grid_tableau
   virtual bool handle(const vgui_event&);
  protected:
   void draw_edges(vcl_vector<vtol_edge_2d_sptr>& edges, bool verts=false);
-
+  void draw_regions(vcl_vector<vdgl_intensity_face_sptr>& regions,
+                    bool verts=false);
  private:
   //flags
 
