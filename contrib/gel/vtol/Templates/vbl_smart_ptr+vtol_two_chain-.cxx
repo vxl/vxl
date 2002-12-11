58a479970dba0344256be9472e7d8edfb5096180
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-11 08:47:18 -0500

    #include added

--- contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_two_chain-.cxx
+++ contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_two_chain-.cxx
@@ -1,4 +1,7 @@
 #include <vtol/vtol_two_chain.h>
+#if defined(VCL_KAI) || defined(VCL_SGI_CC_720)
+# include <vtol/vtol_face.h>
+#endif
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vtol_two_chain);
