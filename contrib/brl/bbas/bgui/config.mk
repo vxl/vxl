864be29827cf49c40a6f48f56c0d08ba5944875c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-14 14:07:32 -0500

    updated w.r.t. CMakeLists.txt

---
+++ contrib/brl/bbas/bgui/config.mk
@@ -0,0 +1,4 @@
+IULIBS += -lbgui
+include ${IUEROOT}/contrib/gel/vdgl/config.mk
+include ${IUEROOT}/contrib/gel/vtol/config.mk
+include ${IUEROOT}/core/vgui/config.mk
---
+++ contrib/brl/bmvl/bmvv/config.mk
@@ -0,0 +1,11 @@
+IULIBS += -lbmvv
+include ${IUEROOT}/contrib/brl/bbas/bgui/config.mk
+include ${IUEROOT}/contrib/brl/bbas/bxml/config.mk
+include ${IUEROOT}/contrib/brl/bseg/sdet/config.mk
+include ${IUEROOT}/contrib/gel/gevd/config.mk
+include ${IUEROOT}/contrib/oxl/mvl/config.mk
+include ${IUEROOT}/contrib/gel/vdgl/config.mk
+include ${IUEROOT}/contrib/gel/vtol/config.mk
+include ${IUEROOT}/core/vgl/config.mk
+include ${IUEROOT}/core/vgui/config.mk
+include ${IUEROOT}/core/vil/config.mk
