ef0cbe02864b0af692c1c07dd0c37146a649242e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-16 14:27:36 -0500

    XERCES support added

--- contrib/brl/bbas/bxml/config.mk
+++ contrib/brl/bbas/bxml/config.mk
@@ -1,6 +1,12 @@
+ifneq (,$(wildcard ${IUEROOT}/v3p/xerces-c-src1_4_0/include/dom/DOM.hpp))
+ HAS_XERCES:=1
+ INCDIRS += ${IUEROOT}/v3p/xerces-c-src1_4_0/include
+ LIBDIRS += ${IUEROOT}/v3p/$(tj_libdir)
+endif
+
 ifdef HAS_XERCES
-IULIBS += -lbxml
-include ../bdgl/config.mk
+IULIBS += -lbxml -lxerces-c
+include ${IUEROOT}/contrib/brl/bbas/bdgl/config.mk
 include ${IUEROOT}/contrib/gel/vtol/config.mk
 include ${IUEROOT}/contrib/gel/vdgl/config.mk
 include ${IUEROOT}/contrib/gel/vsol/config.mk
