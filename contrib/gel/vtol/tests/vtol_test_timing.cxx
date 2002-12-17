e47de76413033ed3f1821304175e795e0a68df85
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-17 11:47:43 -0500

    added faces, and converted to testlib style

--- contrib/gel/vtol/tests/vtol_test_timing.cxx
+++ contrib/gel/vtol/tests/vtol_test_timing.cxx
@@ -1,73 +1,103 @@
-// This is gel/vtol/tests/test_edge_2d.cxx
+// This is gel/vtol/tests/vtol_test_timing.cxx
+#include <testlib/testlib_test.h>
 #include <vul/vul_timer.h>
 #include <vtol/vtol_vertex_2d_sptr.h>
 #include <vtol/vtol_vertex_2d.h>
+#include <vtol/vtol_edge_sptr.h>
 #include <vtol/vtol_edge_2d.h>
-#include <vtol/vtol_edge_2d_sptr.h>
-#include <vtol/vtol_zero_chain_sptr.h>
-#include <vtol/vtol_zero_chain.h>
+#include <vtol/vtol_face_sptr.h>
+#include <vtol/vtol_face_2d.h>
 
-#define Assert(x) { vcl_cout << #x "\t\t\t test "; \
-  if (x) { ++success; vcl_cout << "PASSED\n"; } else { ++failures; vcl_cout << "FAILED\n"; } }
-
-
-int main(int, char **)
+static void vtol_test_timing()
 {
-  int success=0, failures=0;
-
-  vcl_cout << "testing topology timing \n";
+  vcl_cout << "testing topology timing\n";
 
   vul_timer t;
-  //: make a network of s^2 vertices and edges on a grid
-  int s = 100;
-  //: make the vertices
+  // make a network of s^2 vertices and edges on an s by s grid
+  const int s = 100;
+  // make the vertices
   vcl_vector<vtol_vertex_2d_sptr> verts(s*s);
-  int yoff = 0;
   t.mark();
-  for(int y = 0; y<s; y++)
+  for (int y = 0, off = 0; y<s; ++y)
     {
-      yoff = s*y;
-      for(int x = 0; x<s; x++)
+      for (int x = 0; x<s; ++x, ++off)
         {
-          int off = x+yoff;
-          vtol_vertex_2d_sptr v = new vtol_vertex_2d(x,y);        
-          verts[off]=v;
+          verts[off] = new vtol_vertex_2d(x,y);
         }
     }
-  vcl_cout << "Time to construct " << s*s << " vertices " << t.real() 
-           << " msecs\n";
-  vcl_vector<vtol_edge_2d_sptr> edges(2*s*s);
+  vcl_cout << "Time to construct " << s*s << " vertices " << t.real()
+           << " msecs\n" << vcl_flush;
+
+  // make the edges
+  vcl_vector<vtol_edge_sptr> edges(2*s*s);
   int nedges = 0;
   t.mark();
-	for(int y = 1; y<s; y++)
-	  {
-		 yoff = s*y;
-		 for(int x = 1; x<s; x++)
+  for (int y = 0, yoff = 1; y<s; ++y, yoff+=s)
+    {
+     for (int x = 1, off = yoff; x<s; ++x, ++off)
         {
-          int off = x+yoff;
-          vtol_edge_2d_sptr eh = new vtol_edge_2d(*verts[off-1], *verts[off]);
-		  edges[nedges]=eh;
-          vtol_edge_2d_sptr ev = new vtol_edge_2d(*verts[off-s], *verts[off]);
-		  edges[nedges+1]=ev;
-          nedges += 2;
+          edges[nedges++] = new vtol_edge_2d(*verts[off-1], *verts[off]);
         }
     }
-  vcl_cout << "Time to construct " << nedges << " edges " << t.real() 
-           << " msecs\n";
+  for (int x = 0; x<s; ++x)
+    {
+     for (int y = 1, off = x+s; y<s; ++y, off+=s)
+        {
+          edges[nedges++] = new vtol_edge_2d(*verts[off-s], *verts[off]);
+        }
+    }
+  vcl_cout << "Time to construct " << nedges << " edges " << t.real()
+           << " msecs\n" << vcl_flush;
+  TEST("number of edges", nedges, 2*(s-1)*s);
+
+  // count the outgoing edges of all vertices
   t.mark();
   int n_vedges = 0;
-  for(vcl_vector<vtol_vertex_2d_sptr>::iterator vit = verts.begin();
-      vit != verts.end(); vit++)
+  for (vcl_vector<vtol_vertex_2d_sptr>::iterator vit = verts.begin();
+       vit != verts.end(); vit++)
     {
       vcl_vector<vtol_edge_sptr>* vedges = (*vit)->edges();
       n_vedges += vedges->size();
       delete vedges;
     }
-  vcl_cout << "Time to access " << n_vedges << " vertex edges " << t.real() 
-           << " msecs\n";
-  vcl_cout << "finished testing topology timing \n\n";
-  vcl_cout << "Test Summary: " << success << " tests succeeded, "
-           << failures << " tests failed" << (failures?"\t***\n":"\n");
-  return failures;
+  vcl_cout << "Time to count edges per vertex " << t.real()
+           << " msecs\n" << vcl_flush;
+  TEST("number of edges per vertex", n_vedges, 4*(s-1)*s);
+
+  // make the faces
+  vcl_vector<vtol_face_sptr> faces(s*s);
+  int nfaces = 0;
+  t.mark();
+  for (int y = 1; y<s; ++y)
+    {
+     int yoff = s*y;
+     for (int x = 1; x<s; ++x)
+       {
+         int off = x+yoff;
+         vertex_list l(4);
+         l[0] = verts[off]->cast_to_vertex();
+         l[1] = verts[off-s]->cast_to_vertex();
+         l[2] = verts[off-s-1]->cast_to_vertex();
+         l[3] = verts[off-1]->cast_to_vertex();
+         faces[nfaces++]= new vtol_face_2d(l);
+      }
+    }
+  vcl_cout << "Time to construct " << nfaces << " faces " << t.real()
+           << " msecs\n" << vcl_flush;
+  TEST("number of faces", nfaces, (s-1)*(s-1));
+
+  // count the faces adjacent to all vertices
+  t.mark();
+  int n_vfaces = 0;
+  for (vcl_vector<vtol_vertex_2d_sptr>::iterator vit = verts.begin();
+       vit != verts.end(); vit++)
+    {
+      vcl_vector<vtol_face_sptr>* vfaces = (*vit)->faces();
+      n_vfaces += vfaces->size();
+      delete vfaces;
+    }
+  vcl_cout << "Time to count faces per vertex " << t.real() << " msecs\n";
+  TEST("number of faces per vertex", n_vfaces, 4*(s-1)*(s-1));
 }
 
+TESTLIB_DEFINE_MAIN(vtol_test_timing);
