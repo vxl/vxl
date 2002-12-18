39115f4cbd50d8067e9a1d103a5e4fadddda4f4b
F. Schaffalitzky <fsm@users.sourceforge.net>
2002-12-17 22:52:56 -0500

    uncomment if you need it

--- vcl/Templates/vcl_ostream_iterator+int-.cxx
+++ vcl/Templates/vcl_ostream_iterator+int-.cxx
@@ -1,5 +1,6 @@
 #include <vcl_iterator.txx>
 
-#if !defined(VCL_SGI_CC) && !defined(VCL_WIN32) && !defined(GNU_LIBSTDCXX_V3) && !defined(__ICC)
-template class vcl_ostream_iterator<int>;
-#endif
+// which platform *does* need this?
+//#if !defined(VCL_SGI_CC) && !defined(VCL_WIN32) && !defined(GNU_LIBSTDCXX_V3) && !defined(__ICC)
+//template class vcl_ostream_iterator<int>;
+//#endif
