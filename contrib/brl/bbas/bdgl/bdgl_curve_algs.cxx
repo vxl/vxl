62b6c6d2580cf78798fd31b9f751f1fd585d3f96
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-20 11:12:11 -0500

    cross_3d is working again

--- contrib/brl/bbas/bdgl/bdgl_curve_algs.cxx
+++ contrib/brl/bbas/bdgl/bdgl_curve_algs.cxx
@@ -20,7 +20,7 @@ bdgl_curve_algs::~bdgl_curve_algs()
 //:
 //-----------------------------------------------------------------------------
 // Finds the index on an edgel_chain closest to the given
-// point (x, y). Later this routine can become a method on 
+// point (x, y). Later this routine can become a method on
 // vdgl_edgel_chain.
 //-----------------------------------------------------------------------------
 int bdgl_curve_algs::closest_point(vdgl_edgel_chain_sptr& ec,
@@ -66,59 +66,46 @@ vdgl_digital_curve_sptr bdgl_curve_algs::reverse(vdgl_digital_curve_sptr& dc)
   return rev_dc;
 }
 
-//: Need this temporary cross product for some reason
-//: The one in vnl seems to go into recursive self-destruction
-//JLM - change back after we see what is going on.
-static vnl_double_3 temp_cross_3d(vnl_double_3& v0, vnl_double_3& v1)
-{
-  vnl_double_3 v1xv2;
-  v1xv2.x() = v0.y()*v1.z()-v1.y()*v0.z();
-  v1xv2.y() = v0.z()*v1.x()-v0.x()*v1.z();
-  v1xv2.z() = v0.x()*v1.y()-v0.y()*v1.x();
-  return v1xv2;
-}
-//:Intersect an infinite line with a line formed by the two 
-//input points , p0 and p1.  It is assumed that the two lines
-//do intersect.
-static bool intersect_crossing(vnl_double_3& line_coefs, 
+//: Intersect an infinite line with a line formed by the two input points , p0 and p1.
+// It is assumed that the two lines do intersect.
+// If they are parallel, "false" is returned.
+static bool intersect_crossing(vnl_double_3& line_coefs,
                                vnl_double_3& p0,
                                vnl_double_3& p1,
                                vnl_double_3& inter)
 {
-  //uform the line from p0 and p1
-  vnl_double_3 lv01 = temp_cross_3d(p0, p1);
+  //Form the line from p0 and p1
+  vnl_double_3 lv01 = cross_3d(p0, p1);
 
   //Find the intersection point
-  inter = temp_cross_3d(lv01, line_coefs);
+  inter = cross_3d(lv01, line_coefs);
   //Check sanity of the intersection
-  if(fabs(inter.z())<bdgl_curve_algs::tol)
-    return false;
-  return true;
+  return vcl_fabs(inter.z()) >= bdgl_curve_algs::tol;
 }
 
 //-------------------------------------------------------------
-//: intersect an infinite line with the digital curve.  If there
-//    is no intersection return false. Note that the line can intersect 
-//    multiple times. All the intersections are returned.
+//: intersect an infinite line with the digital curve.
+//  If there is no intersection return false. Note that the line can intersect
+//  multiple times. All the intersections are returned.
 bool bdgl_curve_algs::intersect_line(vdgl_digital_curve_sptr& dc,
                                      vgl_line_2d<double>& line,
                                      vcl_vector<vgl_point_2d<double> >& pts)
 {
-  if(!dc)
+  if (!dc)
     {
       vcl_cout << "In bdgl_curve_algs::intersect_line - null curve\n";
       return false;
     }
-  //compute the resolution of the intersection. The digital curve is 
+  //compute the resolution of the intersection. The digital curve is
   //typically embedded in image coordinates so we would want to compute the
   //intersection to 0.1 pixels.  The parametrization of the curve is [0,1]
   //so the search interval on the parmeter should be dt = 1/(10*dc->length())
   //That is, this change of dt corresponds to 0.1 pixel on the curve.
 
-  bool intersection = false;   
+  bool intersection = false;
   vnl_double_3 lv(line.a(), line.b(), line.c());
 
-  //We take advantage of the fact that the algebraic distance to 
+  //We take advantage of the fact that the algebraic distance to
   //a line changes sign if we cross it.
   double t=0, dt = 1/(10*dc->length());
   vnl_double_3 p0(dc->get_x(t), dc->get_y(t), 1.0), p1;
@@ -127,12 +114,11 @@ bool bdgl_curve_algs::intersect_line(vdgl_digital_curve_sptr& dc,
     p1.x()=dc->get_x(t); p1.y()=dc->get_y(t); p1.z()= 1.0;
     double sign0 = dot_product(p0, lv);
     double sign1 = dot_product(p1, lv);
-    if(vcl_fabs(sign0)<bdgl_curve_algs::tol||
-       fabs(sign1)<bdgl_curve_algs::tol||sign0*sign1<=0) //we have crossed or
-                                                         // are on the line
+    if (vcl_fabs(sign0)<bdgl_curve_algs::tol||              //we have crossed or
+        vcl_fabs(sign1)<bdgl_curve_algs::tol||sign0*sign1<=0) // are on the line
       {
         vnl_double_3 inter;
-        if(intersect_crossing(lv, p0, p1, inter))
+        if (intersect_crossing(lv, p0, p1, inter))
           {
             vgl_point_2d<double> p(inter.x()/inter.z(), inter.y()/inter.z());
             pts.push_back(p);
