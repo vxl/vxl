b1786b1b364f0c7ab57c2c93f2b084c893a8b37b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-21 08:00:40 -0500

    no longer need this include

--- contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_two_chain-.cxx
+++ contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_two_chain-.cxx
@@ -1,7 +1,4 @@
 #include <vtol/vtol_two_chain.h>
-#if defined(VCL_KAI) || defined(VCL_SGI_CC_720)
-# include <vtol/vtol_face.h>
-#endif
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vtol_two_chain);
