855e1fef8c08bd88ef106a1246ab59191a16ac5a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-23 15:09:18 -0500

    No, it did not help

--- contrib/gel/vtol/Templates/vcl_vector+vtol_edge_sptr-.cxx
+++ contrib/gel/vtol/Templates/vcl_vector+vtol_edge_sptr-.cxx
@@ -1,4 +1,4 @@
-#include <vtol/vtol_edge.h>
+#include <vtol/vtol_edge_sptr.h>
 #include <vcl_vector.txx>
 
 VCL_VECTOR_INSTANTIATE(vtol_edge_sptr);
--- contrib/gel/vtol/Templates/vcl_vector+vtol_face_2d_sptr-.cxx
+++ contrib/gel/vtol/Templates/vcl_vector+vtol_face_2d_sptr-.cxx
@@ -1,4 +1,4 @@
-#include <vtol/vtol_face_2d.h>
+#include <vtol/vtol_face_2d_sptr.h>
 #include <vcl_vector.txx>
 
 VCL_VECTOR_INSTANTIATE(vtol_face_2d_sptr);
