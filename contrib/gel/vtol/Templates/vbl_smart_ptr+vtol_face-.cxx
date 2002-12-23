4f249f6605641c2fb72b684e4350560dd1fe80b4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-23 16:55:11 -0500

    trying to solve compile error on FreeBSD (undefined type when trying to instantiate smart pointer)

--- contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_face-.cxx
+++ contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_face-.cxx
@@ -2,6 +2,10 @@
 #ifdef VCL_KAI
 # include <vsol/vsol_region.h>
 #endif
+#if 1 // needed by some "eager" compilers, because vtol_face.h refers to
+      // vtol_one_chain_sptr and the compiler tries to instantiate vbl_smart_ptr<vtol_one_chain>
+# include <vtol/vtol_one_chain.h>
+#endif
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vtol_face);
--- contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_face_2d-.cxx
+++ contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_face_2d-.cxx
@@ -2,6 +2,10 @@
 #if defined(VCL_KAI) || defined(VCL_SGI_CC)
 # include <vsol/vsol_region_2d.h>
 #endif
+#if 1 // needed by some "eager" compilers, because vtol_face_2d.h refers to
+      // vtol_one_chain_sptr and the compiler tries to instantiate vbl_smart_ptr<vtol_one_chain>
+# include <vtol/vtol_one_chain.h>
+#endif
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vtol_face_2d);
