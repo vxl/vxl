f1e210306f63222fa80dfd85ef18771017f54d2f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-23 16:52:35 -0500

    trying to solve compile error on FreeBSD (undefined type when trying to instantiate smart pointer)

--- contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_edge_2d-.cxx
+++ contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_edge_2d-.cxx
@@ -3,6 +3,10 @@
 # include <vsol/vsol_point_2d.h>
 # include <vsol/vsol_curve_2d.h>
 #endif
+#if 1 // needed by some "eager" compilers, because vtol_edge_2d.h refers to
+      // vtol_one_chain_sptr and the compiler tries to instantiate vbl_smart_ptr<vtol_one_chain>
+# include <vtol/vtol_one_chain.h>
+#endif
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vtol_edge_2d);
