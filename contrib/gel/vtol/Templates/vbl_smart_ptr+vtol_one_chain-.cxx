76f4256d1972557cef36ef9c44061dfc73c999dd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2002-12-23 15:34:16 -0500

    trying to solve compile error on FreeBSD (undefined type when trying to instantiate smart pointer)

--- contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_one_chain-.cxx
+++ contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_one_chain-.cxx
@@ -1,4 +1,11 @@
 #include <vtol/vtol_one_chain.h>
+#if 1 // needed by some "eager" compilers, because vtol_one_chain.h refers to
+      // vtol_edge_sptr and the compiler tries to instantiate vbl_smart_ptr<vtol_edge>
+# include <vtol/vtol_edge.h>
+# include <vtol/vtol_edge_2d.h>
+# include <vtol/vtol_face.h>
+# include <vtol/vtol_face_2d.h>
+#endif
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vtol_one_chain);
--- contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_zero_chain-.cxx
+++ contrib/gel/vtol/Templates/vbl_smart_ptr+vtol_zero_chain-.cxx
@@ -1,4 +1,9 @@
 #include <vtol/vtol_zero_chain.h>
+#if 1 // needed by some "eager" compilers, because vtol_zero_chain.h refers to
+      // vtol_edge_sptr and the compiler tries to instantiate vbl_smart_ptr<vtol_edge>
+# include <vtol/vtol_edge.h>
+# include <vtol/vtol_edge_2d.h>
+#endif
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vtol_zero_chain);
