d8dd402918d867c14008f109cd6b64e26e20ce12
Amitha Perera <amithaperera@users.sourceforge.net>
2002-12-23 21:57:23 -0500

    Move instatiation to the correct library.

--- contrib/gel/vtol/Templates/vbl_smart_ptr+vdgl_intensity_face-.cxx
+++
@@ -1,7 +0,0 @@
-#include <vdgl/vdgl_intensity_face.h>
-#if 1 // needed by some "eager" compilers, because vdgl_intensity_face.h refers to
-      // vtol_one_chain_sptr and the compiler tries to instantiate vbl_smart_ptr<vtol_one_chain>
-# include <vtol/vtol_one_chain.h>
-#endif
-#include <vbl/vbl_smart_ptr.txx>
-VBL_SMART_PTR_INSTANTIATE(vdgl_intensity_face);
