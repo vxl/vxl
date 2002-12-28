0db59c1b73f8a8eed40e8b895c94709047d8af03
Joseph L. Mundy <mundyj@users.sourceforge.net>
2002-12-28 09:04:29 -0500

    Moved menus to brl/bseg/segv

--- contrib/brl/bseg/sbin/seg.cxx
+++ contrib/brl/bseg/sbin/seg.cxx
@@ -2,9 +2,9 @@
 #include <vgui/vgui_adaptor.h>
 #include <vgui/vgui_window.h>
 #include <vgui/vgui_command.h>
-#include <vgui/vgui_image_tableau.h>
+#include <vgui/vgui_shell_tableau.h>
 #include <vgui/internals/vgui_accelerate.h>
-#include "segv_menus.h"
+#include <bseg/segv/segv_menus.h>
 #include <bseg/segv/segv_segmentation_manager.h>
 
 #ifdef HAS_X11
@@ -17,16 +17,14 @@ int main(int argc, char** argv)
   vgui::init(argc, argv);
   vgui_menu menubar = segv_menus::get_menu();
   unsigned w = 400, h = 340;
-
-  vcl_string title = "SEG";
-  vgui_window* win = vgui::produce_window(w, h, menubar, title);
-  win->get_adaptor()->set_tableau(segv_segmentation_manager::instance());
-  win->set_statusbar(true);
-  win->enable_vscrollbar(true);
-  win->enable_hscrollbar(true);
-  win->show();
-  segv_segmentation_manager::instance()->set_window(win);
-  segv_segmentation_manager::instance()->post_redraw();
-
+   vcl_string title = "SEG";
+   vgui_window* win = vgui::produce_window(w, h, menubar, title);
+   segv_segmentation_manager* segm = segv_segmentation_manager::instance();
+   segm->set_window(win);
+   win->get_adaptor()->set_tableau(segm);
+   win->set_statusbar(true);
+   win->enable_vscrollbar(true);
+   win->enable_hscrollbar(true);
+   win->show();
   return  vgui::run();
 }
