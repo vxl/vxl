31e6f867b42d97f362e5adf3c6c1e568602590ed
Gavin Wheeler <gvw@users.sourceforge.net>
2003-01-06 10:31:39 -0500

    Added a test to the vector io for the binary io of vectors of bools

--- core/vsl/tests/test_vector_io.cxx
+++ core/vsl/tests/test_vector_io.cxx
@@ -12,6 +12,8 @@ void test_vector_io()
            << "****************************\n";
 
   int n = 10;
+  vcl_vector<bool> v_bool_out(n);
+  for (int i=0;i<n;++i) v_bool_out[i]=(i%2==0);
   vcl_vector<int> v_int_out(n);
   for (int i=0;i<n;++i) v_int_out[i]=i;
   vcl_vector<float> v_float_out(n);
@@ -19,15 +21,18 @@ void test_vector_io()
 
   vsl_b_ofstream bfs_out("vsl_vector_io_test.bvl.tmp");
   TEST("Created vsl_vector_io_test.bvl.tmp for writing", (!bfs_out), false);
+  vsl_b_write(bfs_out, v_bool_out);
   vsl_b_write(bfs_out, v_int_out);
   vsl_b_write(bfs_out, v_float_out);
   bfs_out.close();
 
+  vcl_vector<bool> v_bool_in;
   vcl_vector<int> v_int_in;
   vcl_vector<float> v_float_in;
 
   vsl_b_ifstream bfs_in("vsl_vector_io_test.bvl.tmp");
   TEST("Opened vsl_vector_io_test.bvl.tmp for reading", (!bfs_in), false);
+  vsl_b_read(bfs_in, v_bool_in);
   vsl_b_read(bfs_in, v_int_in);
   vsl_b_read(bfs_in, v_float_in);
   TEST("Finished reading file successfully", (!bfs_in), false);
@@ -35,11 +40,13 @@ void test_vector_io()
 
   vpl_unlink ("vsl_vector_io_test.bvl.tmp");
 
+  TEST("vcl_vector<bool> out == vcl_vector<bool> in", v_bool_out, v_bool_in);
   TEST("vcl_vector<int> out == vcl_vector<int> in", v_int_out, v_int_in);
   TEST("vcl_vector<float> out == vcl_vector<float> in", v_float_out,v_float_in);
 
+  vsl_print_summary(vcl_cout, v_bool_in);
   vsl_print_summary(vcl_cout, v_int_in);
-  vsl_print_summary(vcl_cout, v_float_in);
+  vsl_print_summary(vcl_cout, v_float_in); 
   vcl_cout << vcl_endl;
 }
 
