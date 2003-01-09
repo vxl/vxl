079327176230bb5c39f0cb4616b7cab7a8890183
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-01-09 08:31:24 -0500

    undoing previous commit

---
+++ contrib/gel/vtol/Templates/vbl_smart_ptr+vdgl_intensity_face-.cxx
@@ -0,0 +1,6 @@
+// PLEASE DO NOT MOVE THIS FILE TO vdgl/Templates !
+// It should be compiled into vtol since vdgl_intensity_face is compiled in vtol
+// (See the comment on this is vdgl/CMakeLists.txt)
+#include <vdgl/vdgl_intensity_face.h>
+#include <vbl/vbl_smart_ptr.txx>
+VBL_SMART_PTR_INSTANTIATE(vdgl_intensity_face);
