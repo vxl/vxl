8e3781db916223486c23c1d0b444ff445b602e77
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-01-09 05:27:18 -0500

    converted to use testlib

---
+++ contrib/gel/vsol/tests/test_driver.cxx
@@ -0,0 +1,37 @@
+#include <testlib/testlib_register.h>
+
+DECLARE(test_vsol_conic_2d);
+DECLARE(test_vsol_group_2d);
+DECLARE(test_vsol_group_3d);
+DECLARE(test_vsol_line_2d);
+DECLARE(test_vsol_line_3d);
+DECLARE(test_vsol_point_2d);
+DECLARE(test_vsol_point_3d);
+DECLARE(test_vsol_polygon_2d);
+DECLARE(test_vsol_polygon_3d);
+DECLARE(test_vsol_rectangle_2d);
+DECLARE(test_vsol_rectangle_3d);
+DECLARE(test_vsol_tetrahedron);
+DECLARE(test_vsol_triangle_2d);
+DECLARE(test_vsol_triangle_3d);
+
+void
+register_tests()
+{
+  REGISTER(test_vsol_conic_2d);
+  REGISTER(test_vsol_group_2d);
+  REGISTER(test_vsol_group_3d);
+  REGISTER(test_vsol_line_2d);
+  REGISTER(test_vsol_line_3d);
+  REGISTER(test_vsol_point_2d);
+  REGISTER(test_vsol_point_3d);
+  REGISTER(test_vsol_polygon_2d);
+  REGISTER(test_vsol_polygon_3d);
+  REGISTER(test_vsol_rectangle_2d);
+  REGISTER(test_vsol_rectangle_3d);
+  REGISTER(test_vsol_tetrahedron);
+  REGISTER(test_vsol_triangle_2d);
+  REGISTER(test_vsol_triangle_3d);
+}
+
+DEFINE_MAIN;
