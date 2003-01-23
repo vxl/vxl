3594897aa9dd39a5c6accb14ea5648e750fdee88
Amitha Perera <amithaperera@users.sourceforge.net>
2003-01-23 14:10:50 -0500

    Fix instantation for vector<T>::iterator.

--- contrib/rpl/rrel/Templates/rrel_util_vector+double-.cxx
+++ contrib/rpl/rrel/Templates/rrel_util_vector+double-.cxx
@@ -3,18 +3,22 @@
 
 // Trigger this implicitly, because on many compilers, the
 // vector::iterator conflicts with a pointer. With GCC 3.x (only?)
-// vector::iterator _is not_ a pointer*. The implicit instantiation
+// vector::iterator _is not_ a pointer. The implicit instantiation
 // should make the symbols weak. Therefore, if
 // vector::iterator==pointer, the symbols here will be discarded in
 // favour of the symbols in the pointer instantiation.
+//
+// This function must have external linkage (i.e. should not be static);
+// otherwise, an aggressive optimizer (VC7) will throw it away and never
+// emit the instantiations.
 
-static void
+void
 rrel_util_vector_double_iterator_instantiation_tickler()
 {
   vcl_vector<double> v;
   typedef vcl_vector<double>::iterator Iter;
   Iter itr = v.begin(); // to avoid compiler warning on uninitialised variable
-  double val;
+  double val = 0.0;
 
   rrel_util_median_abs_dev_scale( itr, itr, 1, &val );
   rrel_util_median_abs_dev_scale( itr, itr, 1 );
@@ -23,7 +27,4 @@ rrel_util_vector_double_iterator_instantiation_tickler()
   rrel_util_median_and_scale_copy( itr, itr, val, val, 1 );
   rrel_util_intercept_adjustment_copy( itr, itr, val, val, 1 );
   rrel_util_intercept_adjust_stats_copy( itr, itr, val, val, val, 1 );
-
-  // avoid compiler warning about unused function:
-  rrel_util_vector_double_iterator_instantiation_tickler();
 }
