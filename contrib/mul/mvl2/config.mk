0e70e21e6a9bbf56bcca1089a90bc38701dfd2f0
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-01-27 12:11:14 -0500

    updated w.r.t. CMakeLists.txt

---
+++ contrib/mul/mvl2/config.mk
@@ -0,0 +1,6 @@
+IULIBS += -lmvl2
+include ${IUEROOT}/contrib/mul/vimt/config.mk
+include ${IUEROOT}/contrib/mul/mbl/config.mk
+include ${IUEROOT}/core/vul/config.mk
+include ${IUEROOT}/core/vil2/config.mk
+TRY_AVI := 1
