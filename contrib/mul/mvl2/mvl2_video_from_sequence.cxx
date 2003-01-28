39e313cce11acc6b0186cfb721eb8b85ef5788f0
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-01-28 03:42:48 -0500

    assert that current_frame_ is always nonnegative

--- contrib/mul/mvl2/mvl2_video_from_sequence.cxx
+++ contrib/mul/mvl2/mvl2_video_from_sequence.cxx
@@ -9,7 +9,8 @@
 #include <vul/vul_file.h>
 #include <vul/vul_sprintf.h>
 #include <vcl_fstream.h>
-#include <stdio.h>
+#include <vcl_cstdio.h>
+#include <vcl_cassert.h>
 
 mvl2_video_from_sequence::mvl2_video_from_sequence()
 {
@@ -37,7 +38,7 @@ mvl2_video_reader* mvl2_video_from_sequence::clone() const
 
 // possible options : Grey
 bool mvl2_video_from_sequence::initialize( int width, int height, 
-                   vcl_string format, vcl_string file_name)
+                                           vcl_string format, vcl_string file_name)
 {
   use_colour_=true;
   if (!format.find(vcl_string("Grey"))) use_colour_=false;
@@ -49,20 +50,20 @@ bool mvl2_video_from_sequence::initialize( int width, int height,
   }
 
   if (vul_file::extension(file_name.c_str())==vcl_string(".seq"))
-    {
+  {
     char buffer[201];
     vcl_ifstream ifile(file_name.c_str());
     use_seq_file_=true;
    
     while(!ifile.eof())
-      {
+    {
       ifile.getline(buffer,200);
       vcl_string filename(buffer);
       if (filename.length()>0) list_files_.push_back(filename);
-      } 
+    } 
     current_frame_=0;
     is_initialized_=true;
-    }
+  }
 
   //knock off the extension
   int name_length=file_name.length();
@@ -114,37 +115,37 @@ bool mvl2_video_from_sequence::get_frame(vimt_image_2d_of<vxl_byte>& image)
   vcl_string curr_file;
 
   if (use_seq_file_)
+  {
+    if (current_frame_<list_files_.size())
     {
-    if (current_frame_>=0 && current_frame_<list_files_.size())
-      {
       curr_file=list_files_[current_frame_];
-      }
-    else
-      {
-      return false;
-      }
     }
-  else
+    else
     {
+      return false;
+    }
+  }
+  else
+  {
     int currno=current_frame_+offset_;
     curr_file=vul_sprintf("%s%d.jpg",file_stem_.c_str(),currno);
-    }
+  }
 
   vil2_image_view_base_sptr image_view_sptr;
   if (!(image_view_sptr=vil2_load(curr_file.c_str())))
-    {
+  {
     vcl_cout<<"Unable to load : " << curr_file<<vcl_endl;
     return false;
-    }
+  }
   if (use_colour_)
-    {
+  {
     image.image().deep_copy(image_view_sptr); 
-    }
+  }
   else
-    {
+  {
     image.image().deep_copy(vil2_convert_to_grey_using_rgb_weighting(
         image_view_sptr,(vxl_byte)0)); 
-    }
+  }
   height_=image.image().ni();
   width_=image.image().nj();
   return true;
@@ -161,17 +162,17 @@ void mvl2_video_from_sequence::set_frame_rate(double frame_rate)
  
 double mvl2_video_from_sequence::get_frame_rate()
 {
-	return frame_rate_;
+  return frame_rate_;
 }
  
 int mvl2_video_from_sequence::get_width()
 {
-	 return width_;
+  return width_;
 }
  
 int mvl2_video_from_sequence::get_height()
 {
-	 return height_;
+  return height_;
 }
 
 void mvl2_video_from_sequence::set_capture_size(int width,int height)
@@ -186,7 +187,7 @@ int mvl2_video_from_sequence::length()
 
 int mvl2_video_from_sequence::seek(int frame_number)
 {
+  assert (frame_number >= 0);
   current_frame_=frame_number;
   return current_frame_;
 } 
-
