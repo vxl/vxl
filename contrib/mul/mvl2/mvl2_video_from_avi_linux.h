d8b12e7bb4e30eb68155406045f795636f5905e2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-01-28 05:01:18 -0500

    brief Doxygen comment should be on single line

--- contrib/mul/mvl2/mvl2_video_from_avi_linux.h
+++ contrib/mul/mvl2/mvl2_video_from_avi_linux.h
@@ -1,6 +1,5 @@
 #ifndef mvl2_video_from_avi_linux_h_
 #define mvl2_video_from_avi_linux_h_
-
 //:
 // \file
 // \brief A class to get video input from file
@@ -22,66 +21,63 @@
 //: A class to get video input from avi file
 class mvl2_video_from_avi: public mvl2_video_reader
 {
+ public:
 
-public:
-
-    //: Dflt ctor
+  //: Dflt ctor
   mvl2_video_from_avi();
  
-    //: Destructor
+  //: Destructor
   virtual ~mvl2_video_from_avi();
  
-    //: Initialize the file (format can by Grey, RGB, or more complex)
+  //: Initialize the file (format can by Grey, RGB, or more complex)
   virtual bool initialize( int width, int height, 
-      vcl_string format, vcl_string file_name);
+                           vcl_string format, vcl_string file_name);
  
-    //: Tidy up
+  //: Tidy up
   virtual void uninitialize();
  
-    //: Set the size of image captured
+  //: Set the size of image captured
   virtual void  set_capture_size(int width,int height);
  
-    //: Return width of image (in pixels)
+  //: Return width of image (in pixels)
   virtual int get_width();
 
-    //: Return height of image (in pixels)
+  //: Return height of image (in pixels)
   virtual int get_height();
  
-    //: Return the frame rate in frames per second
+  //: Return the frame rate in frames per second
   virtual double get_frame_rate ();
  
-    //: Set the frame rate in frames per second
+  //: Set the frame rate in frames per second
   virtual void set_frame_rate(double frame_rate);
  
-    //: Move frame counter on to next frame
+  //: Move frame counter on to next frame
   virtual int next_frame();
  
-    //: Reset frame counter to zero
+  //: Reset frame counter to zero
   virtual void reset_frame();
  
-    //: Put frame data into the given image
+  //: Put frame data into the given image
   virtual bool get_frame(vimt_image_2d_of<vxl_byte>& image);
  
-    //: Name of the class
+  //: Name of the class
   virtual vcl_string is_a() const;
  
-    //: Create a copy on the heap and return base class pointer
+  //: Create a copy on the heap and return base class pointer
   virtual mvl2_video_reader* clone() const;
 
-    //: Returns the number of frames available from the beginning
-    //  of the stream or -1 if unknown
+  //: Returns the number of frames available from the beginning of the stream or -1 if unknown
   virtual int length();
 
-    //: Move frame counter on to given frame if relevant
-    //  Use only when necessary, because it's slow.
+  //: Move frame counter on to given frame if relevant
+  //  Use only when necessary, because it's slow.
   virtual int seek(int frame_number);
 
-private:
+ private:
 
   vxl_byte* buffer_;
   IAviReadFile* moviefile_;
   IAviReadStream* moviestream_;
-
 };
 
 #endif // mvl2_video_from_avi_linux_h_
--- contrib/mul/mvl2/mvl2_video_from_sequence.h
+++ contrib/mul/mvl2/mvl2_video_from_sequence.h
@@ -16,52 +16,51 @@ class mvl2_video_from_sequence: public mvl2_video_reader
 
   //: Dflt ctor
   mvl2_video_from_sequence();
- 
+
   //: Destructor
   virtual ~mvl2_video_from_sequence();
- 
+
   //: Initialize the file (format can by Grey, RGB, or more complex)
-  virtual bool initialize( int width, int height, 
-      vcl_string format, vcl_string file_name);
- 
+  virtual bool initialize( int width, int height,
+                           vcl_string format, vcl_string file_name);
+
   //: Tidy up
   virtual void uninitialize();
- 
+
   //: Set the size of image captured
   virtual void  set_capture_size(int width,int height);
- 
+
   //: Return width of image (in pixels)
   virtual int get_width();
 
   //: Return height of image (in pixels)
   virtual int get_height();
- 
+
   //: Return the frame rate in frames per second
   virtual double get_frame_rate ();
- 
+
   //: Set the frame rate in frames per second
   virtual void set_frame_rate(double frame_rate);
- 
+
   //: Move frame counter on to next frame
   virtual int next_frame();
- 
+
   //: Reset frame counter to zero
   virtual void reset_frame();
- 
+
   //: Put frame data into the given image
   virtual bool get_frame(vimt_image_2d_of<vxl_byte>& image);
- 
+
   //: Name of the class
   virtual vcl_string is_a() const;
- 
+
   //: Create a copy on the heap and return base class pointer
   virtual mvl2_video_reader* clone() const;
 
   //: Move frame counter on to given frame if relevant
   virtual int seek(int frame_number);
 
-  //: Returns the number of frames available from the beginning
-  //  of the stream or -1 if unknown
+  //: Returns the number of frames available from the beginning of the stream or -1 if unknown
   virtual int length();
 
  private:
@@ -70,7 +69,7 @@ class mvl2_video_from_sequence: public mvl2_video_reader
   vcl_string file_stem_;
   int offset_;
   bool use_seq_file_;
-  vcl_vector<vcl_string> list_files_;  
+  vcl_vector<vcl_string> list_files_;
 };
 
 #endif // mvl2_video_from_sequence_h_
