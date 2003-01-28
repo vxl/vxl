74531b9bca845b1a8e35935a1ff2c96a684f67ee
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-01-28 03:21:35 -0500

    avoid comparing signed with unsigned

--- contrib/mul/mvl2/mvl2_video_reader.cxx
+++ contrib/mul/mvl2/mvl2_video_reader.cxx
@@ -97,15 +97,15 @@ void mvl2_video_reader::display_configs()
 
 bool mvl2_video_reader::use_config(vcl_string configname)
 {
-  int position;
-  position=vcl_find(config_names_.begin(), config_names_.end(), configname) - config_names_.begin();
+  unsigned int position=vcl_find(config_names_.begin(), config_names_.end(), configname) - config_names_.begin();
   if (position<config_names_.size())
   {
     return initialize(config_sizes_[position].first,
                       config_sizes_[position].second,
                       config_strings_[position],config_filenames_[position]);
   }
-  return false;
+  else
+    return false;
 }
 
 int mvl2_video_reader::length()
