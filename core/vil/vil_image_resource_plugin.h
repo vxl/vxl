3aa4a1827e93fbea50c4a472ed578cf933dc936e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-01-28 03:05:20 -0500

    class derives publicly from vil2_image_resource

--- core/vil/vil_image_resource_plugin.h
+++ core/vil/vil_image_resource_plugin.h
@@ -19,76 +19,75 @@
 
 //: A base class for a plugin for vil2 images loading
 // This class provides an interface for loading images in new formats
-class vil2_image_resource_plugin : vil2_image_resource
+
+class vil2_image_resource_plugin : public vil2_image_resource
 {
  public:
 
-    //: Default constructor
+  //: Default constructor
   vil2_image_resource_plugin();
 
-    //: Destructor
+  //: Destructor
   virtual ~vil2_image_resource_plugin();
 
-    //: Name of the class
+  //: Name of the class
   virtual vcl_string is_a() const;
 
-  virtual vil2_pixel_format pixel_format() const 
-      { return VIL2_PIXEL_FORMAT_UNKNOWN; };
- 
-  virtual unsigned ni() const { return 0; };
-  virtual unsigned nj() const { return 0; };
-  virtual unsigned nplanes() const { return 0; };
- 
-  virtual bool get_property (char const *tag, void *property_value=0) const { return false; };
-  virtual vil2_image_view_base_sptr get_copy_view (unsigned i0, unsigned ni, unsigned j0, unsigned nj) const { return vil2_image_view_base_sptr(0); };
- 
-  virtual bool put_view (const vil2_image_view_base &im, unsigned i0, unsigned j0) { return false; }; 
+  virtual vil2_pixel_format pixel_format() const { return VIL2_PIXEL_FORMAT_UNKNOWN; }
 
-    //: Attempt to load image from named file. 
-    // \param filetype  String hinting at what image format is
-    // \param colour    define whether to load images as colour or grey-scale.
-    //        Options are '' (i.e. rely on image), 'Grey' or 'RGB'
-    // \ret   true if successful
-  virtual bool load_the_image (vil2_image_view_base_sptr& image, 
-                             const vcl_string & path);
+  virtual unsigned ni() const { return 0; }
+  virtual unsigned nj() const { return 0; }
+  virtual unsigned nplanes() const { return 0; }
 
-    //: Attempt to load image from named file. 
-    // \param filetype  String hinting at what image format is
-    // \param colour    define whether to load images as colour or grey-scale.
-    //        Options are '' (i.e. rely on image), 'Grey' or 'RGB'
-    // \ret   true if successful
-  virtual bool load_the_image (vil2_image_view_base_sptr& image, 
-                             const vcl_string & path,
-                             const vcl_string & filetype, 
-                             const vcl_string & colour);
+  virtual bool get_property (char const *tag, void *property_value=0) const { return false; }
+  virtual vil2_image_view_base_sptr get_copy_view (unsigned i0, unsigned ni, unsigned j0, unsigned nj) const
+  { return vil2_image_view_base_sptr(0); }
 
-    //: Register a vil2_image_resource_plugin to the list of plugins
+  virtual bool put_view (const vil2_image_view_base &im, unsigned i0, unsigned j0) { return false; }
+
+  //: Attempt to load image from named file.
+  // \param filetype  String hinting at what image format is
+  // \param colour    define whether to load images as colour or grey-scale.
+  //        Options are '' (i.e. rely on image), 'Grey' or 'RGB'
+  // \ret   true if successful
+  virtual bool load_the_image (vil2_image_view_base_sptr& image, const vcl_string & path);
+
+  //: Attempt to load image from named file.
+  // \param filetype  String hinting at what image format is
+  // \param colour    define whether to load images as colour or grey-scale.
+  //        Options are '' (i.e. rely on image), 'Grey' or 'RGB'
+  // \ret   true if successful
+  virtual bool load_the_image (vil2_image_view_base_sptr& image,
+                               const vcl_string & path,
+                               const vcl_string & filetype,
+                               const vcl_string & colour);
+
+  //: Register a vil2_image_resource_plugin to the list of plugins
   static void register_plugin(vil2_image_resource_plugin* plugin);
 
-    //: Delete all registered plugins
+  //: Delete all registered plugins
   static void delete_all_plugins();
 
-    //: Set the desired image size 
+  //: Set the desired image size
   virtual void set_size(int width, int height);
 
-    //: Check whether a filename is a potential candidate i
-    // for loading and if it is available.
+  //: Check whether a filename is a potential candidate for loading and if it is available.
   virtual bool can_be_loaded(const vcl_string& filename);
 
-    //: Set the colour options
+  //: Set the colour options
   void set_colour(const vcl_string& colour);
 
-    //: Set the filetype options
+  //: Set the filetype options
   void set_filetype(const vcl_string& filetype);
 
 protected:
 
-    //: file type
+  //: file type
   vcl_string filetype_;
 
-    //: colour
+  //: colour
   vcl_string colour_;
-  
+
   vil2_pixel_format pixel_format_;
   unsigned int ni_;
   unsigned int nj_;
