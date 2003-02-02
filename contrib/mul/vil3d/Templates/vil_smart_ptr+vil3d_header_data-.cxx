a494ee245f7cae282af507dd01244106179f8a57
Tim Cootes <timcootes@users.sourceforge.net>
2003-02-02 15:43:18 -0500

    Added shiny new template things

---
+++ contrib/mul/vil3d/Templates/vil_smart_ptr+vil3d_header_data-.cxx
@@ -0,0 +1,4 @@
+#include <vil2/vil2_smart_ptr.txx>
+#include <vil3d/vil3d_header_data.h>
+VIL2_SMART_PTR_INSTANTIATE(vil3d_header_data);
+
---
+++ contrib/mul/vil3d/Templates/vil_smart_ptr+vil3d_image_view_base-.cxx
@@ -0,0 +1,4 @@
+#include <vil2/vil2_smart_ptr.txx>
+#include <vil3d/vil3d_image_view_base.h>
+VIL2_SMART_PTR_INSTANTIATE(vil3d_image_view_base);
+
