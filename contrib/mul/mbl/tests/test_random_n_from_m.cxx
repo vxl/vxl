f856ecab4dd861aa5d1cfb72f685e7e8de42a777
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-02-07 05:38:54 -0500

    added tests, mainly for boundary cases

--- contrib/mul/mbl/tests/test_random_n_from_m.cxx
+++ contrib/mul/mbl/tests/test_random_n_from_m.cxx
@@ -1,23 +1,36 @@
-// This is mul/mbl/tests/test_gamma.cxx
+// This is mul/mbl/tests/test_random_n_from_m.cxx
 #include <vcl_iostream.h>
 #include <mbl/mbl_random_n_from_m.h>
 
 void test_random_n_from_m()
 {
-  vcl_cout << "*******************\n"
+  vcl_cout << "*****************************\n"
            << " Testing mbl_random_n_from_m\n"
-           << "*******************\n";
+           << "*****************************\n";
 
   mbl_random_n_from_m random;
 
   vcl_vector<int> chosen;
   random.choose_n_from_m(chosen,4,7);
   TEST("Four chosen",chosen.size(),4);
-  for (int i=0;i<chosen.size();++i)
+  for (unsigned int i=0;i<chosen.size();++i)
   {
-//    vcl_cout<<i<<") "<<chosen[i]<<vcl_endl;
-    TEST("Element in [0,6]",chosen[i]>=0 && chosen[i]<7,true);
+    TEST("Elements in [0,6]",chosen[i]>=0 && chosen[i]<7,true);
   }
+  vcl_vector<int> not_chosen;
+  random.choose_n_from_m(chosen,not_chosen,4,7);
+  TEST("Three not chosen",not_chosen.size(),3);
+  for (unsigned int i=0;i<not_chosen.size();++i)
+  {
+    TEST("Elements in [0,6]",not_chosen[i]>=0 && not_chosen[i]<7,true);
+  }
+  random.choose_n_from_m(chosen,not_chosen,7,7);
+  TEST("All chosen",not_chosen.size(),0);
+  random.choose_n_from_m(chosen,0,7);
+  TEST("None chosen",chosen.size(),0);
+  random.choose_n_from_m(chosen,not_chosen,0,0);
+  TEST("None chosen from none",chosen.size(),0);
+  TEST("None not chosen from none",not_chosen.size(),0);
 }
 
 TESTLIB_DEFINE_MAIN(test_random_n_from_m);
--- contrib/mul/mbl/tests/test_select_n_from_m.cxx
+++ contrib/mul/mbl/tests/test_select_n_from_m.cxx
@@ -4,9 +4,9 @@
 
 void test_select_n_from_m()
 {
-  vcl_cout << "*******************\n"
+  vcl_cout << "*****************************\n"
            << " Testing mbl_select_n_from_m\n"
-           << "*******************\n";
+           << "*****************************\n";
 
   mbl_select_n_from_m selector(2,9);
   
@@ -15,8 +15,19 @@ void test_select_n_from_m()
   TEST("First element 0",selector.subset()[0],0);
   TEST("Second element 1",selector.subset()[1],1);
   TEST("7 not selected",selector.complement().size(),7);
+  TEST("First element 2",selector.complement()[0],2);
+  TEST("Last element 8",selector.complement()[6],8);
+
+  selector.next();
+  TEST("Two selected",selector.subset().size(),2);
+  TEST("First element 0",selector.subset()[0],0);
+  TEST("Second element 2",selector.subset()[1],2);
+  TEST("7 not selected",selector.complement().size(),7);
+  TEST("First element 1",selector.complement()[0],1);
+  TEST("Second element 3",selector.complement()[1],3);
 
   int count=0;
+  if (selector.reset())
   do
   {
     count++;
@@ -29,19 +40,33 @@ void test_select_n_from_m()
 
   selector.set_n_m(3,7);
   count=0;
-  do { count++; } while (selector.next());
+  if (selector.reset()) do { count++; } while (selector.next());
   TEST("3 from 7 = 7*6*5/(2*3) = 35 combinations",count,35);
 
   selector.set_n_m(1,6);
   count=0;
-  do { count++; } while (selector.next());
+  if (selector.reset()) do { count++; } while (selector.next());
   TEST("1 from 6 = 6 combinations",count,6);
 
   selector.set_n_m(6,6);
   count=0;
-  do { count++; } while (selector.next());
+  if (selector.reset()) do { count++; } while (selector.next());
   TEST("6 from 6 = one combination",count,1);
 
+  selector.set_n_m(0,6);
+  count=0;
+  if (selector.reset()) do { count++; } while (selector.next());
+  TEST("0 from 6 = one combination",count,1);
+
+  selector.set_n_m(0,0);
+  count=0;
+  if (selector.reset()) do { count++; } while (selector.next());
+  TEST("0 from 0 = one combination",count,1);
+
+  selector.set_n_m(7,6);
+  count=0;
+  if (selector.reset()) do { count++; } while (selector.next());
+  TEST("7 from 6 = no combinations",count,0);
 }
 
 TESTLIB_DEFINE_MAIN(test_select_n_from_m);
