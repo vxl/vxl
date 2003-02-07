4bd91c7b15004cac2350ea9c85bd074bf8a993f7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-02-07 05:38:42 -0500

    added documentation on behaviour in boundary cases, and changed arguments from int to unsigned int (since these are counts)

--- contrib/mul/mbl/mbl_random_n_from_m.cxx
+++ contrib/mul/mbl/mbl_random_n_from_m.cxx
@@ -3,7 +3,7 @@
 // \brief Randomly select n from m integers without replacement
 // \author Tim Cootes
 
-#include <mbl/mbl_random_n_from_m.h>
+#include "mbl_random_n_from_m.h"
 #include <vcl_iostream.h>
 #include <vcl_cstdlib.h>
 
@@ -17,70 +17,53 @@ void mbl_random_n_from_m::reseed(long new_seed)
 }
 
 //: Select n integers from range [0,m-1], without replacement
-void mbl_random_n_from_m::choose_n_from_m(vcl_vector<int>& choice, int n, int m)
+void mbl_random_n_from_m::choose_n_from_m(vcl_vector<int>& choice,
+                                          unsigned int n, unsigned int m)
 {
-  if ((n>m) | (n<0) | (m<0))
+  if (n>m)
   {
-    vcl_cerr<<"mbl_random_n_from_m::chooseNfromM : Can't choose "<<n;
-    vcl_cerr<<" different integers from "<<m<<vcl_endl;
+    vcl_cerr<<"mbl_random_n_from_m::choose_n_from_m() : Can't choose "<<n
+            <<" different integers from "<<m<<vcl_endl;
     vcl_abort();
   }
 
   if (choice.size()!=n) choice.resize(n);
 
-  int i,j,k1,k2;
+  if (used_.size()<m) used_.resize(m);
 
-  if (used_.size()!=m) used_.resize(m);
+  for (unsigned int i=0;i<m;i++) used_[i] = false;
 
-  int* used_data = &used_[0];
-  int* choice_data = &choice[0];
-
-  for (i=0;i<m;i++) used_[i] = 0;
-
-  for (i=0;i<n;i++)
+  for (unsigned int i=0;i<n;i++)
   {
-    j = mz_random_.lrand32(m-i-1);  // Select a random integer in
-                    // a reducing range
+    // Select a random integer in a reducing range
+    int j = mz_random_.lrand32(m-i-1);
     // Find the j'th un-used integer
-    k1=0;
-    k2=-1;
-    while (k1<=j)
+    int k2=-1;
+    for (int k1=0; k1<=j; ++k1)
     {
       // Move k2 to next unused
-      k2++;
-      while (used_data[k2])
-      {
-        k2++;
-      }
-
-      k1++;
+      ++k2;
+      while (used_[k2])
+        ++k2;
     }
 
-    choice_data[i] = k2;
-    used_data[k2] = 1;
+    choice[i] = k2;
+    used_[k2] = true;
   }
 }
-  
+
 //: Select n integers from range [0,m-1], without replacement
 void mbl_random_n_from_m::choose_n_from_m(vcl_vector<int>& chosen,
-            vcl_vector<int>& not_chosen, int n, int m)
+                                          vcl_vector<int>& not_chosen,
+                                          unsigned int n, unsigned int m)
 {
   choose_n_from_m(chosen,n,m);
 
   // The used array contains details of choice.
-  int n1 = m-n;
+  unsigned int n1 = m-n; // n is guaranteed <= m
   if (not_chosen.size()!=n1) not_chosen.resize(n1);
 
-  int i;
-  int j=0;
-
-  for (i=0;i<m;i++)
-  {
+  for (unsigned int i=0,j=0;i<m;i++)
     if (!used_[i])
-    {
-      not_chosen[j]=i;
-      j++;
-    }
-  }
+      not_chosen[j++]=i;
 }
-
--- contrib/mul/mbl/mbl_random_n_from_m.h
+++ contrib/mul/mbl/mbl_random_n_from_m.h
@@ -1,6 +1,5 @@
 #ifndef mbl_random_n_from_m_h_
 #define mbl_random_n_from_m_h_
-
 //:
 // \file
 // \brief Randomly select n from m integers without replacement
@@ -10,32 +9,36 @@
 #include <mbl/mbl_mz_random.h>
 
 //: Class to generate arrays of n integers chosen without replacement from [0,m-1]
-class mbl_random_n_from_m {
-private:
+class mbl_random_n_from_m
+{
+ private:
   mbl_mz_random mz_random_;
-  vcl_vector<int> used_;
-public:
+  vcl_vector<bool> used_;
+ public:
 
-    //: Dflt ctor
+  //: Dflt ctor
   mbl_random_n_from_m();
-  
-    //: Set seed of random number generator
-  void reseed(long new_seed);
-  
-    //: Select n integers from range [0,m-1], without replacement
-    //  ie all different
-  void choose_n_from_m(vcl_vector<int>& chosen, int n, int m);
-  
-    //: Select n integers from range [0,m-1], without replacement
-    //  ie all differen
-    // \retval chosen  Array [0,n-1] of chosen
-    // \retval not_chosen  Array[0,m-n-1] of not chosen elements
-  void choose_n_from_m(vcl_vector<int>& chosen,
-                       vcl_vector<int>& not_chosen, int n, int m);
 
+  //: Set seed of random number generator
+  void reseed(long new_seed);
+
+  //: Select n integers from range [0,m-1], without replacement.
+  //  ie all different
+  //  n is required to be <= m; otherwise, the function abort()s.
+  //  But n might be 0, in which case an empty list is returned.
+  // \retval chosen  Array [0,n-1] of chosen elements
+  void choose_n_from_m(vcl_vector<int>& chosen,
+                       unsigned int n, unsigned int m);
+
+  //: Select n integers from range [0,m-1], without replacement.
+  //  ie all different
+  //  n is required to be <= m; otherwise, the function abort()s.
+  //  But n might be 0, in which case an empty list is returned.
+  // \retval chosen  Array [0,n-1] of chosen elements
+  // \retval not_chosen  Array[0,m-n-1] of not chosen elements
+  void choose_n_from_m(vcl_vector<int>& chosen,
+                       vcl_vector<int>& not_chosen,
+                       unsigned int n, unsigned int m);
 };
 
-#endif
-
-
-
+#endif // mbl_random_n_from_m_h_
