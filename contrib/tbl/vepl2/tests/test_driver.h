5c81645a442af15f2f6aead3e870813b902a6597
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-02-08 12:21:26 -0500

    added 32-bit image tests, and added fuzzy test

--- contrib/tbl/vepl2/tests/test_driver.h
+++ contrib/tbl/vepl2/tests/test_driver.h
@@ -15,6 +15,9 @@ vil2_image_view_base_sptr CreateTest8bitImage(int wd, int ht);
 // create a 16 bit test image
 vil2_image_view_base_sptr CreateTest16bitImage(int wd, int ht);
 
+// create a 32 bit test image
+vil2_image_view_base_sptr CreateTest32bitImage(int wd, int ht);
+
 // create a 24 bit color test image
 vil2_image_view_base_sptr CreateTest24bitImage(int wd, int ht);
 
@@ -28,8 +31,11 @@ vil2_image_view_base_sptr CreateTestfloatImage(int wd, int ht);
 vil2_image_view_base_sptr CreateTestdoubleImage(int wd, int ht);
 
 // Compare two images and return true if their difference is not v
-bool difference(vil2_image_view_base_sptr const& a, vil2_image_view_base_sptr const& b, long v, vcl_string const& m);
+bool difference(vil2_image_view_base_sptr const& a,
+                vil2_image_view_base_sptr const& b,
+                vxl_int_32 v, vcl_string const& m, bool exact=true);
 
 #define ONE_TEST(x,i,r,T,v,m,a) { vcl_cout << "Starting "<<m<<" test\n"; i = x(*r a); difference(i,r,v,m); }
+#define FUZ_TEST(x,i,r,T,v,m,a) { vcl_cout << "Starting "<<m<<" test\n"; i = x(*r a); difference(i,r,v,m,false); }
 
 #endif // vepl2_test_driver_h_
