add49164615771d0a573218fb61914b3547d8093
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-02-17 23:55:53 -0500

    updated w.r.t. CMakeLists.txt

--- contrib/brl/vvid/config.mk
+++ contrib/brl/vvid/config.mk
@@ -1,7 +1,9 @@
 IULIBS += -lvvid
 include ${IUEROOT}/core/vgui/config.mk
 include ${IUEROOT}/contrib/brl/bseg/sdet/config.mk
+include ${IUEROOT}/contrib/brl/bseg/brip/config.mk
 include ${IUEROOT}/contrib/gel/gevd/config.mk
+include ${IUEROOT}/contrib/gel/vtol/config.mk
 include ${IUEROOT}/core/vidl/config.mk
 include ${IUEROOT}/core/vul/config.mk
 include ${IUEROOT}/core/vil/config.mk
