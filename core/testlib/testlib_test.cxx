49ebb36611abfa25df4f59cea07c6735039fcb91
Fred Wheeler <fred_wheeler@users.sourceforge.net>
2003-02-20 13:04:31 -0500

    use parenthesis because ! has higher precedence than <; oops

--- core/testlib/testlib_test.cxx
+++ core/testlib/testlib_test.cxx
@@ -80,7 +80,7 @@ void testlib_test_assert_near(const vcl_string& msg, double expr, double target,
 {
   vcl_cout << msg << " should be " << target << ", is " << expr << ", ";
   double diff = vcl_abs(expr - target);
-  if ( ! diff < tol)
+  if ( ! (diff < tol))
     vcl_cout << "difference " << diff << ", ";
   testlib_test_perform(diff < tol);
 }
@@ -89,7 +89,7 @@ void testlib_test_assert_near(const vcl_string& msg, vcl_complex<double> expr, v
 {
   vcl_cout << msg << " should be " << target << ", is " << expr << ", ";
   double diff = vcl_abs(expr - target);
-  if ( ! diff < tol)
+  if ( ! (diff < tol))
     vcl_cout << "difference " << diff << ", ";
   testlib_test_perform(diff < tol);
 }
