ea45bc2ea8e1449b8f16bcd63822bec0b650eb5b
Amitha Perera <amithaperera@users.sourceforge.net>
2003-02-21 14:21:44 -0500

    Fix instantiation problems with VC7. (Thanks to Michal Sofka and Gehua Yang for help in diagnosing and fixing.)

--- core/vgui/Templates/vgui_vil_section_buffer_apply+float-.cxx
+++ core/vgui/Templates/vgui_vil_section_buffer_apply+float-.cxx
@@ -1,3 +1,3 @@
 #include <vgui/vgui_vil2_section_buffer_apply.txx>
 
-template void vgui_vil2_section_buffer_apply( vgui_vil2_section_buffer&, vil2_image_view<float> const& );
+INSTANTIATE_VGUI_VIL2_SECTION_BUFFER( float )
--- core/vgui/Templates/vgui_vil_section_buffer_apply+uint_32-.cxx
+++ core/vgui/Templates/vgui_vil_section_buffer_apply+uint_32-.cxx
@@ -1,3 +1,3 @@
 #include <vxl_config.h>
 #include <vgui/vgui_vil2_section_buffer_apply.txx>
-template void vgui_vil2_section_buffer_apply(vgui_vil2_section_buffer& sec_buf, vil2_image_view<vxl_uint_32> const& image_in);
+INSTANTIATE_VGUI_VIL2_SECTION_BUFFER( vxl_uint_32 )
