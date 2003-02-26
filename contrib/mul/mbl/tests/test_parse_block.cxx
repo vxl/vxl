98cece9cd4c4a4456b86f96711a5dffababfdfdb
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-02-26 03:46:52 -0500

    there is no method stringstream::str(char const*)

--- contrib/mul/mbl/tests/test_parse_block.cxx
+++ contrib/mul/mbl/tests/test_parse_block.cxx
@@ -8,37 +8,44 @@
 void test_parse_block()
 {
   vcl_cout << "\n*************************\n"
-           <<   " Testing mbl_parse_block \n"
+           <<   " Testing mbl_parse_block\n"
            <<   "*************************\n";
 
+  {
+    vcl_stringstream ss("{}");
+    TEST("Trivial case 1", mbl_parse_block(ss) == "{}" && !ss.fail(), true);
+  }
 
-  vcl_stringstream  ss("{}");
-  TEST("Trivial case 1", mbl_parse_block(ss) == "{}" && !ss.fail(), true);
+  {
+    vcl_stringstream ss("}");
+    TEST("Trivial case 2", mbl_parse_block(ss,true) == "{}" && !ss.fail(), true);
+  }
 
-  ss.str("}");
-  ss.clear();
-  TEST("Trivial case 2", mbl_parse_block(ss,true) == "{}" && !ss.fail(), true);
+  {
+    vcl_stringstream ss(" {}");
+    TEST("Trivial case 3", mbl_parse_block(ss) == "{}" && !ss.fail(), true);
+  }
 
-  ss.str(" {}");
-  ss.clear();
-  TEST("Trivial case 3", mbl_parse_block(ss) == "{}" && !ss.fail(), true);
+  {
+    vcl_stringstream ss("{ abc }");
+    TEST("Simple case 1", mbl_parse_block(ss) == "{ abc }" && !ss.fail(), true);
+  }
 
-  ss.str("{ abc }");
-  ss.clear();
-  TEST("Simple case 1", mbl_parse_block(ss) == "{ abc }" && !ss.fail(), true);
-  
-  ss.str("{ a\n bc }");
-  ss.clear();
-  TEST("Simple case 2", mbl_parse_block(ss) == "{ a\n bc }" && !ss.fail(), true);
-  
-  ss.str("{ a\n {b}c }");
-  ss.clear();
-  TEST("Multi-level case 1", mbl_parse_block(ss) == "{ a\n {b}c }" && !ss.fail(), true);
-  
-  ss.str("{ a\n {b\n  // wibble } \n}c }");
-  ss.clear();
-  TEST("Comment case 1", mbl_parse_block(ss) == "{ a\n {b\n}c }" && !ss.fail(), true);
-  
+  {
+    vcl_stringstream ss("{ a\n bc }");
+    TEST("Simple case 2", mbl_parse_block(ss) == "{ a\n bc }" && !ss.fail(), true);
+  }
+
+  {
+    vcl_stringstream ss("{ a\n {b}c }");
+    TEST("Multi-level case 1", mbl_parse_block(ss) == "{ a\n {b}c }" && !ss.fail(), true);
+  }
+
+  {
+    vcl_stringstream ss("{ a\n {b\n  // wibble } \n}c }");
+    TEST("Comment case 1", mbl_parse_block(ss) == "{ a\n {b\n}c }" && !ss.fail(), true);
+  }
+
   vcl_cout << "\n\n";
 }
 
