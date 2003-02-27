5426b335aee4140b3f1376d5e4330b6b9bc0c2c3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-02-27 04:04:54 -0500

    made the test suite more readable by using testlib

--- core/vgui/tests/test_displaybase.cxx
+++ core/vgui/tests/test_displaybase.cxx
@@ -2,56 +2,33 @@
 #include <vgui/vgui_soview2D.h>
 #include <vgui/vgui_style.h>
 #include <vcl_iostream.h>
+#include <testlib/testlib_test.h>
 
-int
-main()
+void test_displaybase()
 {
   vgui_easy2D_tableau_new tab1;
   tab1->set_foreground( 1.0f, 1.0f, 1.0f );
+  vgui_soview* line1 = tab1->add_line( 0.0f, 0.0f, 1.0f, 1.0f );
+
+  TEST_NEAR("Try to retrieve style for line 1", line1->get_style()->rgba[0], 1.0f, 1e-7f);
 
   vgui_easy2D_tableau_new tab2;
-  tab2->set_foreground( 1.0f, 1.0f, 1.0f );
-
-  vgui_soview* line1 = tab1->add_line( 0.0f, 0.0f, 1.0f, 1.0f );
+  tab2->set_foreground( 0.5f, 0.1f, 0.2f );
   vgui_soview* line2 = tab2->add_line( 0.0f, 0.0f, 1.0f, 1.0f );
 
-  vcl_cout << "Try to retrieve style 1" << vcl_endl;
-  if( line1->get_style()->rgba[0] != 1.0f ) {
-    vcl_cout << "ERROR: Red != 1.0" << vcl_endl;
-    return 1;
-  } else {
-    vcl_cout << "Style appears to be correct for line 1" << vcl_endl;
-  }
-  vcl_cout << "Try to retrieve style 2" << vcl_endl;
-  if( line2->get_style()->rgba[0] != 1.0f ) {
-    vcl_cout << "ERROR: Red != 1.0" << vcl_endl;
-    return 1;
-  } else {
-    vcl_cout << "Style appears to be correct for line 2" << vcl_endl;
-  }
+  TEST_NEAR("Try to retrieve style for line 2", line2->get_style()->rgba[0], 0.5f, 1e-7f);
 
-  vcl_cout << "Clear tableau 1" << vcl_endl;
+  vcl_cout << "Clear tableau 1\n";
   tab1->clear();
 
   // this should create a new style object
   tab1->set_foreground( 0.0f, 0.0f, 0.0f );
-  vgui_soview* line3 = tab1->add_line( 0.0f, 0.0f, 1.0f, 1.0f );
 
-  vcl_cout << "Try to retrieve style 2 again" << vcl_endl;
-  if( line2->get_style()->rgba[0] != 1.0f ) {
-    vcl_cout << "ERROR: Red != 1.0 (is "<<line2->get_style()->rgba[0]<<")" << vcl_endl;
-    return 1;
-  } else {
-    vcl_cout << "Style appears to be correct for line 2" << vcl_endl;
-  }
+  TEST_NEAR("Try to retrieve style for line 2 again", line2->get_style()->rgba[0], 0.5f, 1e-7f);
 
-  vcl_cout << "Try to retrieve style 3" << vcl_endl;
-  if( line3->get_style()->rgba[0] != 0.0f ) {
-    vcl_cout << "ERROR: Red != 0.0 (is "<<line3->get_style()->rgba[0]<<")" << vcl_endl;
-    return 1;
-  } else {
-    vcl_cout << "Style appears to be correct for line 3" << vcl_endl;
-  }
+  vgui_soview* line3 = tab1->add_line( 0.0f, 0.0f, 3.0f, 3.0f );
 
-  return 0;
+  TEST_NEAR("Try to retrieve style for line 3", line3->get_style()->rgba[0], 0.0f, 1e-7f);
 }
+
+TESTLIB_DEFINE_MAIN(test_displaybase);
