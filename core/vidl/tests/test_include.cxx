cddd9eca8ee7d6acb23a6a3a8f6f922b053ec311
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-03-11 07:05:23 -0500

    moved vidl_test.cxx to tests subdirectory

---
+++ core/vidl/tests/test_include.cxx
@@ -0,0 +1,28 @@
+#include <vidl/dll.h>
+
+#include <vcl_compiler.h>
+#ifdef VCL_VC
+#include <vidl/vidl_avicodec.h>
+#endif
+#include <vidl/vidl_avicodec_sptr.h>
+#include <vidl/vidl_clip.h>
+#include <vidl/vidl_clip_sptr.h>
+#include <vidl/vidl_codec.h>
+#include <vidl/vidl_codec_sptr.h>
+#include <vidl/vidl_file_sequence.h>
+#include <vidl/vidl_frame.h>
+#include <vidl/vidl_frame_as_image.h>
+#include <vidl/vidl_frame_as_image_sptr.h>
+#include <vidl/vidl_frame_sptr.h>
+#include <vidl/vidl_image_list_codec.h>
+#include <vidl/vidl_image_list_codec_sptr.h>
+#include <vidl/vidl_io.h>
+#include <vidl/vidl_movie.h>
+#include <vidl/vidl_movie_sptr.h>
+#include <vidl/vidl_mpegcodec.h>
+#include <vidl/vidl_mpegcodec_helper.h>
+#include <vidl/vidl_mpegcodec_sptr.h>
+#include <vidl/vidl_vob_frame_index.h>
+#include <vidl/vidl_yuv_2_rgb.h>
+
+int main() { return 0; }
--- core/vidl/tests/vidl_test.cxx
+++ core/vidl/tests/vidl_test.cxx
@@ -2,70 +2,72 @@
 #include <vidl/vidl_frame.h>
 #include <vcl_iostream.h>
 
-void tryit ()
-  {
+int main ()
+{
   vidl_movie movie;
   for (vidl_movie::frame_iterator pframe = movie.begin();
        pframe != movie.end();
        ++pframe)
-    {
+  {
     vidl_frame_sptr frame = pframe;
-    if (!frame) return;
-    }
+    if (!frame) return 1;
+  }
 
   // A better way to do it
   for (vidl_movie::frame_iterator pframe = movie.first();
-        pframe <= movie.last();
-        ++pframe)
-    {
+       pframe <= movie.last();
+       ++pframe)
+  {
     vil_image im = pframe->get_image();
     vcl_cout << "Got frame: " << im.width() << 'x' << im.height() << '\n';
-    }
+  }
 
   // Running through the frames 2 images at a time
   for (vidl_movie::frame_iterator pframe = movie.first();
-        pframe <= movie.last();
-        pframe = pframe + 2)
-    {
+       pframe <= movie.last();
+       pframe = pframe + 2)
+  {
     vil_image im = pframe->get_image();
     vcl_cout << "Got frame: " << im.width() << 'x' << im.height() << '\n';
-    }
+  }
 
   // Running backwards throught the image
   for (vidl_movie::frame_iterator pframe = movie.last();
-        pframe >= movie.first();
-        --pframe)
-    {
+       pframe >= movie.first();
+       --pframe)
+  {
     vil_image im = pframe->get_image();
     vcl_cout << "Got frame: " << im.width() << 'x' << im.height() << '\n';
-    }
+  }
 
   // Backwards two at a time
   for (vidl_movie::frame_iterator pframe = movie.last();
-        pframe >= movie.first();
-        pframe = pframe - 2)
-    {
+       pframe >= movie.first();
+       pframe = pframe - 2)
+  {
     vil_image im = pframe->get_image();
     vcl_cout << "Got frame: " << im.width() << 'x' << im.height() << '\n';
-    }
+  }
 
   // Run over all pairs of images
   for (vidl_movie::frame_iterator pframe1 = movie.first();
-      pframe1 <= movie.last() - 1;
-      ++pframe1)
-     {
-     for (vidl_movie::frame_iterator pframe2 = pframe1+1;
-        pframe2 <= movie.last();
-        ++pframe2)
-        {
-        // Run some test on the two images
-        }
-     }
+       pframe1 <= movie.last() - 1;
+       ++pframe1)
+  {
+    for (vidl_movie::frame_iterator pframe2 = pframe1+1;
+         pframe2 <= movie.last();
+         ++pframe2)
+    {
+      // Run some test on the two images
+    }
+  }
 
   // Running over frames 10 to 20
   vidl_movie::frame_iterator pframe = movie.begin();
   for (pframe = 10; pframe <=20; ++pframe)
-     {
-     pframe->get_image();
-     }
+  {
+    pframe->get_image();
   }
+
+  return 0;
+}
