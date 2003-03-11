2f7e3fe94642a35f607654efa4b48f631c706107
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-03-11 09:34:24 -0500

    sqrt -> vcl_sqrt; and explicitly cast float to int to avoid compiler warnings

--- contrib/brl/bmvl/btom/btom_slice_simulator.cxx
+++ contrib/brl/bmvl/btom/btom_slice_simulator.cxx
@@ -1,5 +1,4 @@
-//----*-c++-*----tells emacs to use C++ mode----------
-// This is brl/bmvv/btom/btom_slice_simulator.cxx
+// This is brl/bmvl/btom/btom_slice_simulator.cxx
 #include <vcl_cmath.h>
 #include <btom/btom_gauss_cylinder.h>
 #include <btom/btom_gauss_cylinder_sptr.h>
@@ -24,49 +23,45 @@ btom_slice_simulator::~btom_slice_simulator()
 }
 
 void btom_slice_simulator::gaussian_sinogram(vil_memory_image_of<float> & sinogram,
-										 vil_memory_image_of<float> & reconst)
+                                             vil_memory_image_of<float> & reconst)
 {
-
   int xmax = 512, ymax = 512, off = 64;
   float xt = xmax/2, yt = ymax/2;
   int theta_max = 360;
-  int tmax = (int)sqrt(float((xmax)*(xmax)+(ymax)*(ymax)));
+  int tmax = (int)vcl_sqrt(double((xmax)*(xmax)+(ymax)*(ymax)));
   int tt = (tmax-1)/2;
   sinogram.resize(theta_max,tmax);
   reconst.resize(xmax, ymax);
   reconst.fill(0.0);
   //construct the cylinders
   vcl_vector<btom_gauss_cylinder_sptr> cyls;
-  float linear_cyl = sqrt(float(ncyl_));
-  int delta_x = (int)(xmax-2*off)/linear_cyl,
-    delta_y = (int)(ymax-2*off)/linear_cyl;
+  double linear_cyl = vcl_sqrt(double(ncyl_));
+  int delta_x = (int)((xmax-2*off)/linear_cyl),
+      delta_y = (int)((ymax-2*off)/linear_cyl);
   float delta_sigma = (max_xy_sigma_-min_xy_sigma_)/ncyl_;
   float sigma = min_xy_sigma_;
-  for(int y = off; y<ymax; y+=delta_y)
-    for(int x=off; x<xmax; x+= delta_x, sigma+=delta_sigma)
+  for (int y = off; y<ymax; y+=delta_y)
+    for (int x=off; x<xmax; x+= delta_x, sigma+=delta_sigma)
       cyls.push_back(new btom_gauss_cylinder(sigma, 5.0, 10.0, 1.0, x-xt, y-yt));
-        
-  for(float t = -tt; t<tt; t++)
-    for(float th = 0; th<theta_max; th++)
+
+  for (float t = -tt; t<tt; t++)
+    for (float th = 0; th<theta_max; th++)
       {
         float total_density = 0;
-        for(vcl_vector<btom_gauss_cylinder_sptr>::iterator cit = cyls.begin();
-            cit != cyls.end(); cit++)
-			{
-				
-				total_density += (*cit)->radon_transform(th,t);}
-			
+        for (vcl_vector<btom_gauss_cylinder_sptr>::iterator cit = cyls.begin();
+             cit != cyls.end(); cit++)
+          total_density += (*cit)->radon_transform(th,t);
         sinogram(int(th),int(t+tt)) = total_density;
       }
 
-  for(int y = -yt; y<yt; y++)
-    for(int x=-xt; x<xt; x++)
+  for (int y = -(int)yt; y<yt; y++)
+    for (int x=-(int)xt; x<xt; x++)
       {
         float intensity = 0.0;
-        for(vcl_vector<btom_gauss_cylinder_sptr>::iterator cit = cyls.begin();
-            cit != cyls.end(); cit++)
+        for (vcl_vector<btom_gauss_cylinder_sptr>::iterator cit = cyls.begin();
+             cit != cyls.end(); cit++)
           intensity += (*cit)->cylinder_intensity(x,y);
-      
-        reconst(x+xt,y+yt) = intensity;
+
+        reconst(int(x+xt),int(y+yt)) = intensity;
       }
 }
