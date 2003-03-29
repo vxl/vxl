29b9689cc3ea45b4fc3ea34db525fb71fddaa047
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-03-29 12:31:05 -0500

    removing #include of nonexistent bvgl_pair_match_set_corner.h

--- contrib/brl/bbas/bvgl/bvgl_h_matrix_2d_compute.cxx
+++ contrib/brl/bbas/bvgl/bvgl_h_matrix_2d_compute.cxx
@@ -1,11 +1,9 @@
-//--*-c++-*---
 #include "bvgl_h_matrix_2d_compute.h"
 
 #include <vcl_cstdlib.h>
 #include <vcl_iostream.h>
 
 #include <bvgl/bvgl_h_matrix_2d.h>
-#include <bvgl/bvgl_pair_match_set_corner.h>
 #include <vcl_vector.h>
 
 bool bvgl_h_matrix_2d_compute::
@@ -31,13 +29,12 @@ compute_pl(vcl_vector<vgl_homg_point_2d<double> > const& points1,
            vcl_vector<vgl_homg_line_2d<double> > const& lines2,
            bvgl_h_matrix_2d<double>& H)
 {
-  vcl_cerr << vcl_endl;
-  vcl_cerr << "bvgl_h_matrix_2d_compute::compute_pl() :" << vcl_endl;
-  vcl_cerr << "This is a virtual method which should have been" << vcl_endl;
-  vcl_cerr << "overridden by a class derived from bvgl_h_matrix_2d_compute." << vcl_endl;
-  vcl_cerr << "The derived class may have omitted to implement" << vcl_endl;
-  vcl_cerr << "enough of the methods compute_p(),compute_l() and" << vcl_endl;
-  vcl_cerr << "compute_pl()." << vcl_endl;
+  vcl_cerr << "\nbvgl_h_matrix_2d_compute::compute_pl() :\n"
+           << "This is a virtual method which should have been\n"
+           << "overridden by a class derived from bvgl_h_matrix_2d_compute.\n"
+           << "The derived class may have omitted to implement\n"
+           << "enough of the methods compute_p(),compute_l() and\n"
+           << "compute_pl().\n";
   vcl_abort();
   return false;
 }
