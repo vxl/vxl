af7258f6d08ca91f4e8b5e84c6b25f40a3853114
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-03-30 06:57:10 -0500

    added vsrl_manager and vsrl_point_picker to the compilation (hence added vtol, vdgl, vgui, vepl, vgl to dependencies)

--- contrib/gel/vsrl/config.mk
+++ contrib/gel/vsrl/config.mk
@@ -1,3 +1,9 @@
 IULIBS += -lvsrl
+include ${IUEROOT}/core/vgui/config.mk
+include ${IUEROOT}/contrib/gel/vtol/config.mk
+include ${IUEROOT}/contrib/gel/vdgl/config.mk
+include ${IUEROOT}/contrib/tbl/vepl/config.mk
+include ${IUEROOT}/contrib/brl/bseg/sdet/config.mk
+include ${IUEROOT}/core/vgl/config.mk
 include ${IUEROOT}/core/vnl/config.mk
 include ${IUEROOT}/core/vil/config.mk
