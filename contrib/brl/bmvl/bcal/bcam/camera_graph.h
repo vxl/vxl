8c8e65a753695cb921f592b643b24b9a9f43ab0f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-04-01 02:50:57 -0500

    exit() --> vcl_exit()

--- contrib/brl/bmvl/bcal/bcam/camera_graph.h
+++ contrib/brl/bmvl/bcal/bcam/camera_graph.h
@@ -17,6 +17,7 @@
 #endif // _MSC_VER > 1000
 
 #include <vcl_cassert.h>
+#include <vcl_iostream.h>
 #include <vcsl/vcsl_graph.h>
 #include <vcsl/vcsl_spatial.h>
 #include <vcsl/vcsl_spatial_sptr.h>
@@ -25,15 +26,15 @@
 template<class ObjectNode, class CameraNode, class Trans>
 class camera_graph
 {
-private:
+ private:
   vcsl_graph_sptr pCamG_;
   vcl_vector<Trans*> edges_;
   vcl_vector<int> vertice_; // store camera IDs
   ObjectNode* source_;
   int nCams_; // number of cameras
 
-public: // constructor and de-constructor
-  
+ public: // constructor and de-constructor
+
   camera_graph()
   {
     pCamG_ = new vcsl_graph;
@@ -44,43 +45,43 @@ public: // constructor and de-constructor
 
   virtual ~camera_graph()
   {
-    if(source_)
+    if (source_)
       delete source_;
-    
+
     source_ = 0;
 
     vertice_.clear();
   }
 
-public: // operations
+ public: // operations
   ObjectNode* getSource() {  return source_;}
 
   // add a new camera and edge into the graph
   // it return a camera ID.
-  int addVertex() 
+  int addVertex()
   {
     CameraNode *newCamera = new CameraNode(nCams_++);
     vcsl_spatial_sptr v = newCamera;
     if (!source_){ // if no source
-      exit(1);
       vcl_cerr<<"no source in the camera graph\n";
+      vcl_exit(1);
     }
 
     Trans* e = new Trans;
     v->set_graph(pCamG_);
     v->set_unique(source_, e);
     edges_.push_back(e);
-    
+
     int id = newCamera->get_id();
     vertice_.push_back(id);
     return id;
   }
-   
+
   // return a camera 0-based
-  CameraNode* get_vertex(int i) 
+  CameraNode* get_vertex(int i)
   {
     // the first one are source
-     vcsl_spatial_sptr v = pCamG_->item(i+1); 
+     vcsl_spatial_sptr v = pCamG_->item(i+1);
      CameraNode* pV = (CameraNode*)v.ptr();
      //assert(pV->get_id()==i);
      return pV;
@@ -99,8 +100,6 @@ public: // operations
   }
 
   int num_vertice() { return vertice_.size();}
-
 };
 
 #endif // AFX_CAMERAGRAPH_H__8810B4C3_E6C7_42CE_8C7F_D9F8F201F6F6__INCLUDED_
-
