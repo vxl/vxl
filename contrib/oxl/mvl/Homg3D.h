e0ca233c7721aa77b254655fac692f4af9df065e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-04-08 16:14:23 -0400

    avoid variable names starting with underscore; and made deprecated interface private

--- contrib/oxl/mvl/Homg3D.h
+++ contrib/oxl/mvl/Homg3D.h
@@ -1,11 +1,11 @@
 // This is oxl/mvl/Homg3D.h
-#ifndef _Homg3D_h
-#define _Homg3D_h
+#ifndef Homg3D_h_
+#define Homg3D_h_
 #ifdef VCL_NEEDS_PRAGMA_INTERFACE
 #pragma interface
 #endif
 //:
-//  \file
+// \file
 // \brief Base class for 3D homogeneous features
 //
 // Base class for 3D homogeneous features.
@@ -16,8 +16,8 @@
 //
 // \verbatim
 //  Modifications
-//    210297 AWF Switched to fixed-length vectors for speed.
-//    110397 Peter Vanroose - added operator==
+//   210297 AWF Switched to fixed-length vectors for speed.
+//   110397 Peter Vanroose - added operator==
 // \endverbatim
 //-----------------------------------------------------------------------------
 
@@ -33,19 +33,19 @@ class Homg3D : public Homg
   // Constructors/Initializers/Destructors-------------------------------------
 
   Homg3D() {}
-  Homg3D(const Homg3D& that):_homg_vector(that._homg_vector) {}
-  Homg3D(double px, double py, double pz, double pw = 1): _homg_vector(px,py,pz,pw) {}
-  Homg3D(const vnl_vector<double>& v): _homg_vector(v) { }
-  Homg3D(const vnl_vector_fixed<double,4>& v): _homg_vector(v) { }
+  Homg3D(const Homg3D& that):homg_vector_(that.homg_vector_) {}
+  Homg3D(double px, double py, double pz, double pw = 1): homg_vector_(px,py,pz,pw) {}
+  Homg3D(const vnl_vector<double>& v): homg_vector_(v) { }
+  Homg3D(const vnl_vector_fixed<double,4>& v): homg_vector_(v) { }
  ~Homg3D() {}
 
-  Homg3D& operator=(const Homg3D& that) { _homg_vector = that._homg_vector; return *this; }
+  Homg3D& operator=(const Homg3D& that) { homg_vector_ = that.homg_vector_; return *this; }
   // default ok. nope, egcs chokes
 
   // Data Access---------------------------------------------------------------
 
   vnl_double_4 get_vector() const { return vnl_double_4(x(),y(),z(),w()); }
-  vnl_double_4& asVector() { return _homg_vector; }
+  vnl_double_4& asVector() { return homg_vector_; }
 
   // get x,y,z,w.  Do not try to fill null pointers.
   void get(double *x_ptr, double *y_ptr, double *z_ptr, double *w_ptr) const
@@ -57,31 +57,34 @@ class Homg3D : public Homg
   }
 
   //: Get x.
-  inline double x() const { return _homg_vector[0]; }
-  //: deprecated
-  double get_x() const { return _homg_vector[0]; }
+  inline double x() const { return homg_vector_[0]; }
 
   //: Get y.
-  inline double y() const { return _homg_vector[1]; }
-  //: deprecated
-  double get_y() const { return _homg_vector[1]; }
+  inline double y() const { return homg_vector_[1]; }
 
   //: Get z.
-  inline double z() const { return _homg_vector[2]; }
-  //: deprecated
-  double get_z() const { return _homg_vector[2]; }
+  inline double z() const { return homg_vector_[2]; }
 
   //: Get w.
-  inline double w() const { return _homg_vector[3]; }
+  inline double w() const { return homg_vector_[3]; }
+
+ private:
   //: deprecated
-  double get_w() const { return _homg_vector[3]; }
+  double get_x() const { return homg_vector_[0]; }
+  //: deprecated
+  double get_y() const { return homg_vector_[1]; }
+  //: deprecated
+  double get_z() const { return homg_vector_[2]; }
+  //: deprecated
+  double get_w() const { return homg_vector_[3]; }
+ public:
 
   //: Set x,y,z,w.
   void set(double px, double py, double pz, double pw = 1) {
-    _homg_vector[0] = px;
-    _homg_vector[1] = py;
-    _homg_vector[2] = pz;
-    _homg_vector[3] = pw;
+    homg_vector_[0] = px;
+    homg_vector_[1] = py;
+    homg_vector_[2] = pz;
+    homg_vector_[3] = pw;
   }
 
   //: Set from vector
@@ -91,15 +94,15 @@ class Homg3D : public Homg
   void set(const vnl_vector<double>& v) { set(v[0],v[1],v[2],v[3]); }
 
   //: Set element.
-  void set(unsigned int idx, double v) { assert(idx<=3); _homg_vector[idx]=v; }
+  void set(unsigned int idx, double v) { assert(idx<=3); homg_vector_[idx]=v; }
 
   // Utility Methods-----------------------------------------------------------
-  bool operator==(Homg3D const& p) const { return _homg_vector == p.get_vector(); }
+  bool operator==(Homg3D const& p) const { return homg_vector_ == p.get_vector(); }
 
   // INTERNALS-----------------------------------------------------------------
  protected:
   // Data Members--------------------------------------------------------------
-  vnl_double_4 _homg_vector;
+  vnl_double_4 homg_vector_;
 };
 
-#endif // _Homg3D_h
+#endif // Homg3D_h_
