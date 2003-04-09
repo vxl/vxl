2809e0d50b2f76549b7180e4e987083b6c301146
David Cristinacce <dcristinacce@users.sourceforge.net>
2003-04-09 12:22:57 -0400

    added new template instansiation

---
+++ core/vsl/Templates/vsl_vector_io+vcl_vector+float--.cxx
@@ -0,0 +1,4 @@
+#include <vcl_utility.h>
+#include <vsl/vsl_vector_io.txx>
+//typedef vcl_vector<vcl_pair<unsigned int,unsigned int> > vector_pair_uu;
+VSL_VECTOR_IO_INSTANTIATE( vcl_vector<float> );
