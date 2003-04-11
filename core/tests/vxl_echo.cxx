c54ecc1a8ba0fad9d89e9e00aa1a59479f32f468
Ian Scott <scottim@users.sourceforge.net>
2003-04-11 10:56:02 -0400

    Persuade the Dashboard to report important settings on each build.

---
+++ core/tests/vxl_echo.cxx
@@ -0,0 +1,14 @@
+#include <vcl_iostream.h>
+
+int main (int argc, char * argv[])
+{
+  for (int i = 1; i < argc; ++i)
+  {
+    if (argv[i][0] == 'n' && argv[i][1] == 'l' && argv[i][2] == 0)
+      vcl_cout << vcl_endl;
+    else
+      vcl_cout << argv[i] << " ";
+  }
+  vcl_cout << vcl_endl;
+  return 0;
+}
