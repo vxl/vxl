c9f3f0f9cd0fb707b1eff66d156e37c3e016b508
Fred Wheeler <fred_wheeler@users.sourceforge.net>
2003-04-14 15:02:51 -0400

    new test that generated a Notes.xml file for Dart, based on similar test in ITK

--- core/tests/test_driver.cxx
+++ core/tests/test_driver.cxx
@@ -1,11 +1,13 @@
 #include <testlib/testlib_register.h>
 
 DECLARE( test_config );
+DECLARE( test_build_info );
 
 void
 register_tests()
 {
   REGISTER( test_config );
+  REGISTER( test_build_info );
 }
 
 DEFINE_MAIN;
