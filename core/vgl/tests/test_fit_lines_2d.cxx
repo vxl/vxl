50405146a26e84c390d29dee2eba35031b373982
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-04-16 16:53:57 -0400

    tangent_angle was renamed to slope_degrees

--- core/vgl/tests/test_fit_lines_2d.cxx
+++ core/vgl/tests/test_fit_lines_2d.cxx
@@ -18,15 +18,15 @@ static void test_line_segment_methods()
 {
   vgl_point_2d<double> p0(0.0,0.0), p1(1.0,2.0);
   vgl_line_2d<double> l(1.0,-1.0,0.0);
-  vcl_cout << "line angle " << l.tangent_angle() << "\n";
-  TEST_NEAR("line angle", l.tangent_angle(), 45, 1e-04);
+  vcl_cout << "line angle " << l.slope_degrees() << '\n';
+  TEST_NEAR("line angle", l.slope_degrees(), 45, 1e-04);
   vgl_line_segment_2d<double> seg(p0,p1);
-  double angle = seg.tangent_angle();
-  TEST_NEAR("line angle", seg.tangent_angle(),63.43495 , 1e-04);
+  double angle = seg.slope_degrees();
+  TEST_NEAR("line angle", seg.slope_degrees(),63.43495 , 1e-04);
   vgl_vector_2d<double> dir = seg.direction(), norm = seg.normal();
-  vcl_cout << "segment angle = " << angle << "\n";
-  vcl_cout << "direction " << dir << "\n";
-  vcl_cout << "normal " << norm << "\n";
+  vcl_cout << "segment angle = " << angle << '\n'
+           << "direction " << dir << '\n'
+           << "normal " << norm << '\n';
 }
 static void test_line_2d_regression()
 {
@@ -37,8 +37,8 @@ static void test_line_2d_regression()
   reg.increment_partial_sums(p1.x(), p1.y());
   reg.increment_partial_sums(p2.x(), p2.y());
   reg.fit();
-  vcl_cout << "fitting error " << reg.get_rms_error() << "\n";
-  vcl_cout << "fitted line " << reg.get_line() << "\n";
+  vcl_cout << "fitting error " << reg.get_rms_error() << '\n'
+           << "fitted line " << reg.get_line() << '\n';
   double dist = vcl_fabs(0.1-reg.get_rms_error());
   TEST_NEAR("identity", dist, 0.0, 1e-04);
 }
@@ -48,13 +48,13 @@ static void test_fit_simple_chain()
   int npts= 15;
   vgl_fit_lines_2d<double> fitter;
   //A segment with a slope of 2
-  for(int i = 0; i<npts; i++)
+  for (int i = 0; i<npts; i++)
     {
       vgl_point_2d<double> p(i,2*i);
       fitter.add_point(p);
     }
   //A segment with a slope of -1
-  for(int i = 0; i<npts; i++)
+  for (int i = 0; i<npts; i++)
     {
       vgl_point_2d<double> p(i+npts,(2*(npts-1)-i));
       fitter.add_point(p);
@@ -64,17 +64,18 @@ static void test_fit_simple_chain()
   vgl_point_2d<double> p1(2*npts+1, npts+1);
   fitter.add_point(p0);fitter.add_point(p1);
   vcl_vector<vgl_point_2d<double> >& c = fitter.get_points();
-	for(vcl_vector<vgl_point_2d<double> >::iterator pit = c.begin();
+  for (vcl_vector<vgl_point_2d<double> >::iterator pit = c.begin();
        pit != c.end(); pit++)
-     vcl_cout << *pit << "\n";
+     vcl_cout << *pit << '\n';
   fitter.fit();
-  vcl_vector<vgl_line_segment_2d<double> >& segs = fitter.get_line_segs(); 
-  for(vcl_vector<vgl_line_segment_2d<double> >::iterator sit = segs.begin();
-      sit != segs.end(); sit++)
-    vcl_cout << *sit << "\n";
+  vcl_vector<vgl_line_segment_2d<double> >& segs = fitter.get_line_segs();
+  for (vcl_vector<vgl_line_segment_2d<double> >::iterator sit = segs.begin();
+       sit != segs.end(); sit++)
+    vcl_cout << *sit << '\n';
   TEST("no of line segments ", segs.size(), 2);
   TEST("last endpoint x", segs[1].point2().x(), 29);
 }
+
 MAIN( test_fit_lines_2d )
 {
   START( "test_vgl_fit_lines_2d" );
