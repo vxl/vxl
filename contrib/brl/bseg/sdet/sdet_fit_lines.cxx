22c50b3f470f216b81d623a926f9ffe054bf4bb7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-04-17 06:21:25 -0400

    there is no vcl_abs(int) nor vcl_qsort() in this file

--- contrib/brl/bseg/sdet/sdet_fit_lines.cxx
+++ contrib/brl/bseg/sdet/sdet_fit_lines.cxx
@@ -2,7 +2,6 @@
 #include "sdet_fit_lines.h"
 //:
 // \file
-#include <vcl_cstdlib.h>   // for vcl_abs(int) and vcl_qsort()
 #include <vsol/vsol_line_2d.h>
 #include <vtol/vtol_edge_2d.h>
 #include <vdgl/vdgl_digital_curve_sptr.h>
@@ -44,30 +43,30 @@ bool sdet_fit_lines::fit_lines()
 {
   if (segs_valid_)
     return false;
-  if(!edges_.size())
+  if (!edges_.size())
     return false;
   fitter_.set_min_fit_length(min_fit_length_);
   fitter_.set_rms_error_tol(rms_distance_);
-  for(vcl_vector<vtol_edge_2d_sptr>::iterator eit = edges_.begin();
+  for (vcl_vector<vtol_edge_2d_sptr>::iterator eit = edges_.begin();
       eit != edges_.end(); eit++)
     {
       vsol_curve_2d_sptr c = (*eit)->curve();
       vdgl_digital_curve_sptr dc = c->cast_to_digital_curve();
-      if(!dc)
+      if (!dc)
         continue;
       vdgl_interpolator_sptr intp = dc->get_interpolator();
       vdgl_edgel_chain_sptr ec = intp->get_edgel_chain();
       fitter_.clear();
      int nedgl = ec->size();
-      for(int i=0; i<nedgl; i++)
+      for (int i=0; i<nedgl; i++)
         {
           vgl_point_2d<double> p((*ec)[i].x(), (*ec)[i].y());
           fitter_.add_point(p);
         }
-      
+
       fitter_.fit();
       vcl_vector<vgl_line_segment_2d<double> >& segs = fitter_.get_line_segs();
-      for(vcl_vector<vgl_line_segment_2d<double> >::iterator sit=segs.begin();
+      for (vcl_vector<vgl_line_segment_2d<double> >::iterator sit=segs.begin();
           sit != segs.end(); sit++)
         {
           vsol_line_2d_sptr line = new vsol_line_2d(*sit);
@@ -76,13 +75,14 @@ bool sdet_fit_lines::fit_lines()
     }
   segs_valid_ = true;
   return true;
-}    
+}
+
 //-------------------------------------------------------------------------
 //: Get the line segments
 //
 vcl_vector<vsol_line_2d_sptr>& sdet_fit_lines::get_line_segs()
 {
-  if(segs_valid_)
+  if (segs_valid_)
     return line_segs_;
   this->fit_lines();
   return line_segs_;
