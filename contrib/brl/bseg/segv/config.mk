750ea845f7ae42ce48e5f46a5b7006391242b61a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-04-18 02:10:43 -0400

    segv now depends on bsol

--- contrib/brl/bseg/segv/config.mk
+++ contrib/brl/bseg/segv/config.mk
@@ -1,6 +1,7 @@
 IULIBS += -lsegv
 include ${IUEROOT}/contrib/brl/bseg/sdet/config.mk
 include ${IUEROOT}/contrib/brl/bbas/bgui/config.mk
+include ${IUEROOT}/contrib/brl/bbas/bsol/config.mk
 include ${IUEROOT}/contrib/gel/gevd/config.mk
 include ${IUEROOT}/contrib/gel/vdgl/config.mk
 include ${IUEROOT}/contrib/gel/vtol/config.mk
