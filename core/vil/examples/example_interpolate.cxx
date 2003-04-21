d6108279c24bfc4548ba2c0d9111ed644db6fc0b
Fred Wheeler <fred_wheeler@users.sourceforge.net>
2003-04-21 10:17:55 -0400

    scan down original image diagonal and use vil2_bilin_interp_safe and vil2_bicub_interp_safe

--- core/vil/examples/example_interpolate.cxx
+++ core/vil/examples/example_interpolate.cxx
@@ -4,10 +4,14 @@
 // \author Fred Wheeler
 
 #include <vcl_iostream.h>
+#include <vcl_iomanip.h>
+#include <vcl_ios.h>
 #include <vxl_config.h> // for vxl_byte
 #include <vil2/vil2_image_view.h>
 #include <vil2/vil2_save.h>
 #include <vil2/vil2_print.h>
+#include <vil2/vil2_bilin_interp.h>
+#include <vil2/vil2_bicub_interp.h>
 #include <vil2/vil2_resample_bilin.h>
 #include <vil2/vil2_resample_bicub.h>
 
@@ -22,15 +26,20 @@ main (int argc, char** argv)
   // size of original inage to be created
   unsigned ni = 16;
   unsigned nj = 16;
-  unsigned np = 3;
+  unsigned np = 3;              // 3 plane/color image
 
+  // the original image
   vil2_image_view<vxl_byte> image_orig (ni, nj, np);
 
+  // orig image values will stay this far from 0 and 255
   vxl_byte pad = 16;
 
+  // fill in the original image with random values
   for (unsigned p=0; p<np; p++)
   for (unsigned j=0; j<nj; j++)
   for (unsigned i=0; i<ni; i++)
+      // there is nothing magic about these numbers
+      // this just makes an image that looks random
       image_orig(i,j,p) =
           vxl_byte((87+145*i+167*j+197*p*i*j) % (256 - 2*pad) + pad);
 
@@ -43,7 +52,7 @@ main (int argc, char** argv)
            << fn_orig << vcl_endl;
 
   // over-sampling factor for interpolated images
-  double fact = 20.0;
+  int fact = 20;
 
   // create and save a bilinearly interpolated over-sampled image
   const char * fn_bilin = "example_interpolate_image_bilin.png";
@@ -51,7 +60,7 @@ main (int argc, char** argv)
   vil2_resample_bilin(image_orig, image_bilin,
                       -1.0, -1.0,
                       1.0/fact, 0.0, 0.0, 1.0/fact,
-                      int(fact*(ni+1)+1), int(fact*(nj+1)+1));
+                      fact*(ni+1)+1, fact*(nj+1)+1);
   vil2_save(image_bilin, fn_bilin);
   vcl_cout << "Wrote bilinearly interpolated over-sampled image to "
            << fn_bilin << vcl_endl;
@@ -62,10 +71,46 @@ main (int argc, char** argv)
   vil2_resample_bicub(image_orig, image_bicub,
                       -1.0, -1.0,
                       1.0/fact, 0.0, 0.0, 1.0/fact,
-                      int(fact*(ni+1)+1), int(fact*(nj+1)+1));
+                      fact*(ni+1)+1, fact*(nj+1)+1);
   vil2_save(image_bicub, fn_bicub);
   vcl_cout << "Wrote bicubicly interpolated over-sampled image to "
            << fn_bilin << vcl_endl;
+
+  // change over-sampling factor for diagonal scan
+  fact = 4;
+
+  vcl_cout << vcl_right;
+  vcl_cout << vcl_fixed;
+  vcl_cout << vcl_setprecision (2);
+
+  // scan down the diagonal of the original image
+  // at increments, show the bilinear and bicubic interpolation
+
+  for (int k = - fact; k < fact * int(ni) + 1; k++) {
+      vcl_cout << "k: " << vcl_setw(3) << k << "  ";
+
+      double x = double(k) / fact;
+      vcl_cout << "x: " << vcl_setw(5) << x << "  ";
+      double y = double(k) / fact;
+      vcl_cout << "y: " << vcl_setw(5) << y << "  ";
+
+      double v_bilin = vil2_bilin_interp_safe (image_orig, x, y);
+      vcl_cout << "bilin: " << vcl_setw(6) << v_bilin << "  ";
+
+      double v_bicub = vil2_bicub_interp_safe (image_orig, x, y);
+      vcl_cout << "bicub: " << vcl_setw(6) << v_bicub << "  ";
+
+      // if we are lined up on an original pixel value, print that too
+      if (0 == (k % fact)) {
+          int ki = k / fact;
+          if (ki >= 0 && ki < ni && ki < nj) {
+              double v_orig = image_orig (ki, ki);
+              vcl_cout << "orig: " << vcl_setw(3) << v_orig << "  ";
+          }
+      }
+
+      vcl_cout << vcl_endl;
+  }
 
   return 0;
 }
