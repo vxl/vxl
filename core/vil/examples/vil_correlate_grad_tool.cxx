08b187ff06b8c717af88c8e191783ad8f3397407
Tim Cootes <timcootes@users.sourceforge.net>
2003-05-09 05:28:54 -0400

    Arranged to use vul_arg for more elegant interface

--- core/vil/examples/vil_correlate_grad_tool.cxx
+++ core/vil/examples/vil_correlate_grad_tool.cxx
@@ -5,27 +5,40 @@
 
 #include <vcl_iostream.h>
 #include <vxl_config.h> // for vxl_byte
+#include <vul/vul_arg.h>
 #include <vil2/vil2_load.h>
 #include <vil2/vil2_save.h>
 #include <vil2/vil2_image_view.h>
 #include <vil2/algo/vil2_normalised_correlation_2d.h>
 #include <vil2/algo/vil2_sobel_3x3.h>
 #include <vil2/vil2_convert.h>
+#include <vil2/algo/vil2_suppress_non_max.h>
+
+void print_usage()
+{
+  vcl_cout<<"vil2_correlate_grad_tool  -i src_image -k kernel_image -o dest_image [-s]\n";
+  vcl_cout<<"Correlates gradient of kernel_image with gradient of"<<vcl_endl;
+  vcl_cout<<"src_image, saves to dest_image\n";
+}
 
 int main(int argc, char** argv)
 {
-  if (argc!=4)
+  vul_arg<vcl_string> in_path("-i","Input image");
+  vul_arg<vcl_string> kernel_path("-k","Kernel image");
+  vul_arg<vcl_string> out_path("-o","Output image","output.jpg");
+  vul_arg<bool> suppress_non_max("-s","Suppress non-peaks",false);
+  vul_arg_parse(argc, argv);
+
+  if(in_path() == "" || kernel_path()=="")
   {
-    vcl_cout<<"vil2_correlate_grad_tool  src_image kernel_image dest_image\n";
-    vcl_cout<<"Correlates gradient of kernel_image with gradient of src_image,"
-            <<" saves to dest_image\n";
-    return 0;
+    print_usage();
+    vul_arg_display_usage_and_exit();
   }
 
-  vil2_image_view<vxl_byte> src_im = vil2_load(argv[1]);
+  vil2_image_view<vxl_byte> src_im = vil2_load(in_path().c_str());
   if (src_im.size()==0)
   {
-    vcl_cout<<"Unable to load source image from "<<argv[1]<<'\n';
+    vcl_cout<<"Unable to load source image from "<<in_path()<<'\n';
     return 1;
   }
 
@@ -34,10 +47,10 @@ int main(int argc, char** argv)
   vil2_convert_cast(src_im,src_im_f);
   vil2_sobel_3x3(src_im_f,s_grad_x,s_grad_y);
 
-  vil2_image_view<vxl_byte> kernel_im_byte = vil2_load(argv[2]);
+  vil2_image_view<vxl_byte> kernel_im_byte = vil2_load(kernel_path().c_str());
   if (kernel_im_byte.size()==0)
   {
-    vcl_cout<<"Unable to load kernel image from "<<argv[1]<<'\n';
+    vcl_cout<<"Unable to load kernel image from "<<kernel_path()<<'\n';
     return 1;
   }
 
@@ -58,6 +71,8 @@ int main(int argc, char** argv)
 
   vil2_math_image_sum(dest_x,dest_y,dest_image);
 
+  if (suppress_non_max()) vil2_suppress_non_max_3x3(dest_image);
+
   // Stretch range to [0,255]
   float min_v,max_v;
   vil2_math_value_range(dest_image,min_v,max_v);
@@ -70,13 +85,13 @@ int main(int argc, char** argv)
 
   vcl_cout<<"Resulting image is "<<result_image.ni()<<" x "<<result_image.nj()<<'\n';
 
-  if (!vil2_save(result_image, argv[3]))
+  if (!vil2_save(result_image, out_path().c_str()))
   {
-    vcl_cerr<<"Unable to save result image to "<<argv[3]<<'\n';
+    vcl_cerr<<"Unable to save result image to "<<out_path()<<'\n';
     return 1;
   }
 
-  vcl_cout<<"Saved result of convolution to "<<argv[3]<<'\n';
+  vcl_cout<<"Saved result of convolution to "<<out_path()<<'\n';
 
   return 0;
 }
