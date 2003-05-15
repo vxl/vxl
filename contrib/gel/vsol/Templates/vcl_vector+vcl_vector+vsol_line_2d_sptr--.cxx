172f30248f845ee34dfe72188c4a79539c64db72
Joseph L. Mundy <mundyj@users.sourceforge.net>
2003-05-15 10:42:19 -0400

    Added templates for vsol_line_2d collections

---
+++ contrib/gel/vsol/Templates/vcl_vector+vcl_vector+vsol_line_2d_sptr--.cxx
@@ -0,0 +1,4 @@
+#include <vsol/vsol_line_2d_sptr.h>
+#include <vcl_vector.txx>
+
+VCL_VECTOR_INSTANTIATE(vcl_vector<vsol_line_2d_sptr>);
---
+++ contrib/gel/vsol/Templates/vcl_vector+vsol_line_2d_sptr-.cxx
@@ -0,0 +1,4 @@
+#include <vsol/vsol_line_2d_sptr.h>
+#include <vcl_vector.txx>
+
+VCL_VECTOR_INSTANTIATE(vsol_line_2d_sptr);
