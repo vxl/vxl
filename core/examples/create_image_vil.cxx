8a894162e5ab2655be31a386ce446675187720af
Ian Scott <scottim@users.sourceforge.net>
2003-05-21 08:45:27 -0400

    renaming vil to vil1 and vil2 to vil

--- core/examples/create_image_vil.cxx
+++ core/examples/create_image_vil.cxx
@@ -1,12 +1,12 @@
 #include <vcl_string.h>
 #include <vul/vul_arg.h>
-#include <vil2/vil2_save.h>
-#include <vil2/vil2_image_view_base.h>
-#include <vil2/vil2_image_view.h>
+#include <vil/vil_save.h>
+#include <vil/vil_image_view_base.h>
+#include <vil/vil_image_view.h>
 
-vil2_image_view_base_sptr make_image(int wd, int ht)
+vil_image_view_base_sptr make_image(int wd, int ht)
 {
-  vil2_image_view<unsigned char> image(wd, ht);
+  vil_image_view<unsigned char> image(wd, ht);
   for (int x = 0; x < wd; x++)
     for (int y = 0; y < ht; y++)
       image(x,y) = ((x-wd/2)*(y-ht/2)/16) % 256;
@@ -22,9 +22,9 @@ int main(int argc, char** argv)
   int sizex = 253;
   int sizey = 155;
 
-  vil2_image_view_base_sptr image_p = make_image(sizex, sizey);
+  vil_image_view_base_sptr image_p = make_image(sizex, sizey);
 
-  vil2_save( *image_p, output_filename().c_str());
+  vil_save( *image_p, output_filename().c_str());
 
   return 0;
 }
--- core/examples/create_image_vil1.cxx
+++ core/examples/create_image_vil1.cxx
@@ -1,14 +1,14 @@
-// There is a movement to replace vil with vil2. Consider looking at
+// There is a movement to replace vil1 with vil. Consider looking at
 // the other example file, create_image_vil2.cxx
 
 #include <vcl_string.h>
 #include <vul/vul_arg.h>
-#include <vil/vil_save.h>
-#include <vil/vil_memory_image_of.h>
+#include <vil1/vil1_save.h>
+#include <vil1/vil1_memory_image_of.h>
 
-vil_image make_image(int wd, int ht)
+vil1_image make_image(int wd, int ht)
 {
-  vil_memory_image_of<unsigned char> image(wd, ht);
+  vil1_memory_image_of<unsigned char> image(wd, ht);
   for (int x = 0; x < wd; x++)
     for (int y = 0; y < ht; y++)
       image(x,y) = ((x-wd/2)*(y-ht/2)/16) % 256;
@@ -24,9 +24,9 @@ int main(int argc, char** argv)
   int sizex = 253;
   int sizey = 155;
 
-  vil_image image = make_image(sizex, sizey);
+  vil1_image image = make_image(sizex, sizey);
 
-  vil_save(image, output_filename().c_str());
+  vil1_save(image, output_filename().c_str());
 
   return 0;
 }
