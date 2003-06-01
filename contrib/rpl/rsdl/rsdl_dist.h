0aa07e39527966918534905bbf42a177909132dd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-06-01 18:40:27 -0400

    typo corrected (minmum-->minimum)

--- contrib/rpl/rsdl/rsdl_dist.h
+++ contrib/rpl/rsdl/rsdl_dist.h
@@ -1,7 +1,5 @@
-
 #ifndef rsdl_dist_h_
 #define rsdl_dist_h_
-
 //:
 // \file
 // \brief Point to point and point to bounding box distance functions.
@@ -28,32 +26,32 @@
 #include <rsdl/rsdl_bounding_box.h>
 
 //: Return the square distance between two rsdl_point's.
-double  
+double
 rsdl_dist_sq( const rsdl_point & p, const rsdl_point& q );
 
 //: Return the distance between two rsdl_point's.
-double 
+double
 rsdl_dist( const rsdl_point & p, const rsdl_point& q );
 
-//: Return the minmum square distance between \a p and any point in \a b.
-double  
+//: Return the minimum square distance between \a p and any point in \a b.
+double
 rsdl_dist_sq( const rsdl_point & p, const rsdl_bounding_box& b );
 
-//: Return the minmum distance between \a p and any point in \a b.
-double  
+//: Return the minimum distance between \a p and any point in \a b.
+double
 rsdl_dist( const rsdl_point & p, const rsdl_bounding_box& b );
 
 //: Determine if a point is inside a bounding box.
 bool
-rsdl_dist_point_in_box( const rsdl_point & pt, 
+rsdl_dist_point_in_box( const rsdl_point & pt,
                         const rsdl_bounding_box & box );
 
 
 //: Determine the relation between a proported "inner" box and a proported "outer" box.
-//  \a inside will be true iff all points in \a inner are in \a outer 
+//  \a inside will be true iff all points in \a inner are in \a outer
 //  \a intersect will be true iff at least one point from \a inner is in \a outer.
 void
-rsdl_dist_box_relation( const rsdl_bounding_box & inner, 
+rsdl_dist_box_relation( const rsdl_bounding_box & inner,
                         const rsdl_bounding_box & outer,
                         bool& inside,
                         bool& intersects );
