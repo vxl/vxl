1fc951cd74f10bdd7d2e7fca93c074f46bb9bee7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-06-01 18:40:46 -0400

    typo corrected (sobel-->Sobel)

--- contrib/tbl/vepl/vepl_sobel.h
+++ contrib/tbl/vepl/vepl_sobel.h
@@ -1,16 +1,15 @@
 #ifndef vepl_sobel_h_
 #define vepl_sobel_h_
-
 //:
 // \file
-// \brief sobel convolution filter
+// \brief Sobel convolution filter
 //
 // \author Peter Vanroose, K.U.Leuven (ESAT/PSI)
 // \date   28 April 2001
 
 #include <vil1/vil1_image.h>
 
-//: sobel convolution filter
+//: Sobel convolution filter
 vil1_image vepl_sobel(vil1_image const& );
 
 #endif // vepl_sobel_h_
--- contrib/tbl/vepl2/vepl2_sobel.h
+++ contrib/tbl/vepl2/vepl2_sobel.h
@@ -1,16 +1,15 @@
 #ifndef vepl2_sobel_h_
 #define vepl2_sobel_h_
-
 //:
 // \file
-// \brief sobel convolution filter
+// \brief Sobel convolution filter
 //
 // \author Peter Vanroose, K.U.Leuven (ESAT/PSI)
 // \date   7 October 2002
 
 #include <vil/vil_image_view_base.h>
 
-//: sobel convolution filter
+//: Sobel convolution filter
 vil_image_view_base_sptr vepl2_sobel(vil_image_view_base const& );
 
 #endif // vepl2_sobel_h_
