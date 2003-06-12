698aaa54c9c10c3e61f4ed6447f3e163fe990438
Amir Tamrakar <yakkeeper@users.sourceforge.net>
2003-06-12 17:31:03 -0400

    Amir

---
+++ contrib/brl/bbas/bgui/bgui_vsol2D_tableau_sptr.h
@@ -0,0 +1,9 @@
+#ifndef bgui_vsol2D_tableau_sptr_h_
+#define bgui_vsol2D_tableau_sptr_h_
+
+#include <vgui/vgui_tableau_sptr.h>
+
+class bgui_vsol2D_tableau;
+typedef vgui_tableau_sptr_t<bgui_vsol2D_tableau> bgui_vsol2D_tableau_sptr;
+
+#endif
