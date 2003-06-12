29d9afc80b111d65d1335f0eff8151f966a3a6aa
Amir Tamrakar <yakkeeper@users.sourceforge.net>
2003-06-12 17:51:44 -0400

    Amir: Added POLYLINE type

--- contrib/gel/vsol/vsol_curve_2d.h
+++ contrib/gel/vsol/vsol_curve_2d.h
@@ -12,6 +12,7 @@
 // Modifications
 //  2000/06/17 Peter Vanroose  Implemented all operator==()s and type info
 //  2000/04/27 François BERTEL Creation
+//  2002/04/22 Amir Tamrakar   Added POLYLINE type 
 // \endverbatim
 //*****************************************************************************
 
@@ -20,6 +21,9 @@
 //*****************************************************************************
 #include <vsol/vsol_spatial_object_2d.h>
 #include <vsol/vsol_point_2d_sptr.h>
+#include <vsol/vsol_line_2d_sptr.h>
+#include <vsol/vsol_polyline_2d_sptr.h>
+
 class vdgl_digital_curve;
 class vsol_line_2d;
 class vsol_curve_2d
@@ -30,6 +34,7 @@ public:
   { CURVE_NO_TYPE=0,
     LINE,
     CONIC,
+	 POLYLINE,
     DIGITAL_CURVE,
     NUM_CURVE_TYPES
   };
@@ -66,18 +71,24 @@ public:
   //---------------------------------------------------------------------------
   virtual vsol_curve_2d *cast_to_curve(void) {return this;}
   virtual const vsol_curve_2d *cast_to_curve(void) const {return this;}
+  
+  //---------------------------------------------------------------------------
+  //: Return `this' if `this' is an line, 0 otherwise
+  //---------------------------------------------------------------------------
+  virtual vsol_line_2d const*cast_to_line_2d(void)const{return 0;}
+  virtual vsol_line_2d *cast_to_line_2d(void) {return 0;}
+
+  //---------------------------------------------------------------------------
+  //: Return `this' if `this' is an polyline, 0 otherwise
+  //---------------------------------------------------------------------------
+  virtual vsol_polyline_2d const*cast_to_polyline_2d(void)const{return 0;}
+  virtual vsol_polyline_2d *cast_to_polyline_2d(void) {return 0;}
 
   //---------------------------------------------------------------------------
   //: Return `this' if `this' is an digital_curve, 0 otherwise
   //---------------------------------------------------------------------------
   virtual vdgl_digital_curve const*cast_to_digital_curve(void)const{return 0;}
   virtual vdgl_digital_curve *cast_to_digital_curve(void) {return 0;}
-
-  //---------------------------------------------------------------------------
-  //: Return `this' if `this' is a line_2d, 0 otherwise
-  //---------------------------------------------------------------------------
-  virtual vsol_line_2d const*cast_to_line_2d(void)const{return 0;}
-  virtual vsol_line_2d *cast_to_line_2d(void) {return 0;}
 
   //---------------------------------------------------------------------------
   //: Return the first point of `this'
