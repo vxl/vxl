aa4366a318d5ed687029fffb34f1848745c08e0a
Amir Tamrakar <yakkeeper@users.sourceforge.net>
2003-06-12 17:35:24 -0400

    Amir: Removed the vsol hacks from this tableau..finally!

--- contrib/brl/bbas/bgui/bgui_vtol2D_tableau.cxx
+++ contrib/brl/bbas/bgui/bgui_vtol2D_tableau.cxx
@@ -6,21 +6,20 @@
 #include <vgui/vgui.h>
 #include <vgui/vgui_style.h>
 #include <bgui/bgui_style.h>
-#include <vsol/vsol_point_2d.h>
 #include <vtol/vtol_face_2d.h>
 #include <vdgl/vdgl_edgel_chain.h>
 #include <vdgl/vdgl_interpolator.h>
-#include <vdgl/vdgl_digital_curve.h>
+
 bgui_vtol2D_tableau::bgui_vtol2D_tableau(const char* n) :
-  vgui_easy2D_tableau(n) { this->init(); }
+  bgui_vsol2D_tableau(n) { this->init(); }
 
 bgui_vtol2D_tableau::bgui_vtol2D_tableau(vgui_image_tableau_sptr const& it,
                                          const char* n) :
-  vgui_easy2D_tableau(it, n) { this->init(); }
+  bgui_vsol2D_tableau(it, n) { this->init(); }
 
 bgui_vtol2D_tableau::bgui_vtol2D_tableau(vgui_tableau_sptr const& t,
                                          const char* n) :
-  vgui_easy2D_tableau(t, n) { this->init(); }
+  bgui_vsol2D_tableau(t, n) { this->init(); }
 
 bgui_vtol2D_tableau::~bgui_vtol2D_tableau()
 {
@@ -46,128 +45,19 @@ static void print_edgels(vtol_edge_2d_sptr const & e)
 
 void bgui_vtol2D_tableau::init()
 {
-  old_id_ = 0;
-  highlight_ = true;
-  //set highlight display style parameters
-  highlight_style_ = new bgui_style(0.0f, 0.0f, 1.0f, 5.0f, 5.0f);
-
   //define default soview styles
   //these can be overridden by later set_*_syle commands prior to drawing.
   //
-   this->set_vsol_point_2d_style(0.0f, 1.0f, 0.0f, 5.0f);
-   this->set_vsol_line_2d_style(0.8f, 0.2f, 0.9f, 3.0f);
-   this->set_digital_curve_style(0.8f, 0.0f, 0.8f, 3.0f);
-   this->set_dotted_digital_curve_style(0.8f, 0.0f, 0.8f, 3.0f, 3.0f);
    this->set_vertex_style(1.0f, 0.0f, 0.0f, 3.0f);
    this->set_edge_style(0.0f, 1.0f, 0.0f, 3.0f);
    this->set_edge_group_style(0.0f, 1.0f, 0.0f, 3.0f);
    this->set_face_style(0.0f, 1.0f, 0.0f, 3.0f);
-}
-//:
-// Provide roaming highlighting for soviews in the tableau.
-// As the mouse moves the soview closest to the mouse is
-// changed to the highlighted style.
-// vgui_displaylist2D_tableau::motion(..) has a mechanism for
-// highlighting the nearest object but it doesn't work.
 
-bool bgui_vtol2D_tableau::handle(vgui_event const &e)
-{
-  if (e.type == vgui_MOTION&&highlight_)
-    {
-      //retrive the previously highlighted soview and
-      //restore it to its default style
-      vgui_soview* old_so = vgui_soview::id_to_object(old_id_);
-      if (old_so)
-        {
-          bgui_style_sptr default_sty = style_map_[old_so->type_name()];
-          bgui_style* bs = (bgui_style*)default_sty.ptr();
-          vgui_style* s = (vgui_style*)bs;
-          old_so->set_style(s);
-        }
-      //get the soview that is closest to the mouse
-      vgui_soview2D* high_so = (vgui_soview2D*)get_highlighted_soview();
-      if (high_so&&high_so->get_style())
-        {
-          //replace the old soview with the currently closest view
-          int id = high_so->get_id();
-          old_id_ = id;
-          //set soview style to the highlight color and weight
-          bgui_style* bsh = (bgui_style*)highlight_style_.ptr();
-          vgui_style* sh = (vgui_style*)bsh;
-          high_so->set_style(sh);
-          this->post_redraw();
-        }
-    }
-  // We aren't interested in other events so pass them to the base class.
-  return vgui_easy2D_tableau::handle(e);
+	//call the init() function of bgui_vsol2D_tableau
+	bgui_vsol2D_tableau::init();
 }
 
-bgui_vtol_soview2D_point*
-bgui_vtol2D_tableau::add_vsol_point_2d(vsol_point_2d_sptr const& p)
-{
-  bgui_vtol_soview2D_point* obj =
-    new bgui_vtol_soview2D_point();
-  obj->x = p->x();
-  obj->y = p->y();
-  add(obj);
-   bgui_style_sptr s = style_map_[obj->type_name()];
-   obj->set_style(s.ptr());
-  return obj;
-}
-
-bgui_vtol_soview2D_line_seg*
-bgui_vtol2D_tableau::add_vsol_line_2d(vsol_line_2d_sptr const& line)
-{
-  bgui_vtol_soview2D_line_seg* obj =
-    new bgui_vtol_soview2D_line_seg(line);
-  add(obj);
-  //set the default style
-  bgui_style_sptr sty = style_map_[obj->type_name()];
-  obj->set_style(sty.ptr());
-  return obj;
-}
-
-bgui_vtol_soview2D_digital_curve*
-bgui_vtol2D_tableau::add_digital_curve(vdgl_digital_curve_sptr const& dc)
-{
-  bgui_vtol_soview2D_digital_curve* obj =
-    new bgui_vtol_soview2D_digital_curve(dc);
-  //set the default style
-  bgui_style_sptr sty = style_map_[obj->type_name()];
-  add(obj);
-  if (obj)
-    {
-      obj->set_style(sty.ptr());
-      if (highlight_)
-        {
-          int id = obj->get_id();
-          obj_map_[id]=dc->cast_to_topology_object();
-        }
-    }
-  return obj;
-}
-
-bgui_vtol_soview2D_dotted_digital_curve*
-bgui_vtol2D_tableau::add_dotted_digital_curve(vdgl_digital_curve_sptr const& dc)
-{
-  bgui_vtol_soview2D_dotted_digital_curve* obj =
-    new bgui_vtol_soview2D_dotted_digital_curve(dc);
-  //set the default style
-  bgui_style_sptr sty = style_map_[obj->type_name()];
-  add(obj);
-  if (obj)
-    {
-      obj->set_style(sty.ptr());
-      if (highlight_)
-        {
-          int id = obj->get_id();
-          obj_map_[id]=dc->cast_to_topology_object();
-        }
-    }
-  return obj;
-}
-
-
+//display topological objects
 bgui_vtol_soview2D_vertex*
 bgui_vtol2D_tableau::add_vertex(vtol_vertex_2d_sptr const& v)
 {
@@ -241,53 +131,6 @@ bgui_vtol2D_tableau::add_face(vtol_face_2d_sptr const& f)
         }
     }
   return obj;
-}
-
-//--------------------------------------------------------------
-// only add vsol objects, treat topology separately
-//
-void bgui_vtol2D_tableau::
-add_spatial_objects(vcl_vector<vsol_spatial_object_2d_sptr> const& sos)
-{
-  for (vcl_vector<vsol_spatial_object_2d_sptr>::const_iterator sit = sos.begin();
-       sit != sos.end(); sit++)
-    {
-      add_spatial_object( (*sit) );
-    }
-}
-
-void bgui_vtol2D_tableau::
-add_spatial_object(vsol_spatial_object_2d_sptr const& sos)
-{
-  if (sos->cast_to_point()) {
-    {
-      vsol_point_2d_sptr p = sos->cast_to_point();
-      this->add_vsol_point_2d(p);
-    }
-  }
-  if (sos->cast_to_curve()) {
-    if (sos->cast_to_curve()->cast_to_digital_curve())
-      {
-        vdgl_digital_curve_sptr dc =
-          sos->cast_to_curve()->cast_to_digital_curve();
-        this->add_digital_curve(dc);
-      }
-    if (sos->cast_to_curve()->cast_to_line_2d())
-      {
-        vsol_line_2d_sptr line =
-          sos->cast_to_curve()->cast_to_line_2d();
-        this->add_vsol_line_2d(line);
-      }
-  }
-  return;
-}
-
-void bgui_vtol2D_tableau::
-add_lines(vcl_vector<vsol_line_2d_sptr> const& lines)
-{
-  for (vcl_vector<vsol_line_2d_sptr>::const_iterator lit = lines.begin();
-       lit != lines.end(); lit++)
-    add_vsol_line_2d(*lit);
 }
 
 //--------------------------------------------------------------
@@ -388,63 +231,11 @@ vtol_edge_2d_sptr bgui_vtol2D_tableau::get_mapped_edge(const int id)
 
 void bgui_vtol2D_tableau::clear_all()
 {
-  bool temp = highlight_;
-  highlight_ = false;//in case of event interrupts during the clear
   obj_map_.clear();
-  vgui_easy2D_tableau::clear();
-  old_id_ = 0;
-  highlight_ = temp;
-  this->post_redraw();
-}
+  //now call the clear_all from bgui_vsol2D_tableau
 
-void bgui_vtol2D_tableau::set_vsol_spatial_object_2d_style(vsol_spatial_object_2d_sptr sos,
-                                                           const float r, const float g, const float b,
-                                                           const float line_width,
-                                                           const float point_radius)
-{
-  if (sos->cast_to_point()) {
-    set_vsol_point_2d_style(r,g,b, point_radius);
-  } else if (sos->cast_to_curve()) {
-    set_digital_curve_style(r,g,b, line_width);
-  }
+  bgui_vsol2D_tableau::clear_all();
 }
-
-void bgui_vtol2D_tableau::set_vsol_point_2d_style(const float r, const float g,
-                                                  const float b,
-                                                  const float point_radius)
-{
-  bgui_style_sptr sty = new bgui_style(r, g, b, point_radius, 0.0f);
-  bgui_vtol_soview2D_point p;
-  style_map_[p.type_name()]=sty;
-}
-
-void bgui_vtol2D_tableau::set_vsol_line_2d_style(const float r, const float g,
-                                                 const float b,
-                                                 const float line_width)
-{
-  bgui_style_sptr sty = new bgui_style(r, g, b, 0.0f, line_width);
-  bgui_vtol_soview2D_line_seg seg;
-  style_map_[seg.type_name()]=sty;
-}
-
-void bgui_vtol2D_tableau::set_digital_curve_style(const float r, const float g,
-                                                  const float b,
-                                                  const float line_width)
-{
-  bgui_style_sptr sty = new bgui_style(r, g, b, 0.0f, line_width);
-  bgui_vtol_soview2D_digital_curve dc;
-  style_map_[dc.type_name()]=sty;
-}
-
-void bgui_vtol2D_tableau::set_dotted_digital_curve_style(const float r, const float g,
-                                                         const float b,
-                                                         const float line_width, const float point_radius)
-{
-  bgui_style_sptr sty = new bgui_style(r, g, b, point_radius, line_width);
-  bgui_vtol_soview2D_dotted_digital_curve dc;
-  style_map_[dc.type_name()]=sty;
-}
-
 
 void bgui_vtol2D_tableau::set_vtol_topology_object_style(vtol_topology_object_sptr tos,
                                                          const float r, const float g, const float b,
--- contrib/brl/bbas/bgui/bgui_vtol2D_tableau.h
+++ contrib/brl/bbas/bgui/bgui_vtol2D_tableau.h
@@ -4,7 +4,8 @@
 //-----------------------------------------------------------------------------
 //:
 // \file
-// \brief A child tableau of vgui_easy2D_tableau that knows how to display vtol objects.
+// \brief A child tableau of bgui_vsol2D_tableau that knows how to display 
+//  vtol objects.
 // \author
 //   J.L. Mundy
 //
@@ -18,16 +19,15 @@
 //  Modifications:
 //   J.L. Mundy November 28, 2002    Initial version.
 //   J.L. Mundy December 16, 2002    Added map between soviews and vtol objects
-//   J.L. Mundy March 22, 2003  Added set style commands
+//   J.L. Mundy March 22, 2003       Added set style commands
+//   Amir Tamrakar April 22, 2002    Removed the functions to display vsol objects
+//                                   to bgui_vsol2D_tableau and subclasses this
+//                                   from it instead
 // \endverbatim
 //-----------------------------------------------------------------------------
 #include <vcl_vector.h>
 #include <vcl_map.h>
 #include <vgui/vgui_style.h>
-#include <vsol/vsol_spatial_object_2d_sptr.h>
-#include <vsol/vsol_point_2d_sptr.h>
-#include <vsol/vsol_line_2d_sptr.h>
-#include <vdgl/vdgl_digital_curve_sptr.h>
 #include <vdgl/vdgl_edgel_chain_sptr.h>
 #include <vtol/vtol_vertex_2d.h>
 #include <vtol/vtol_edge_2d.h>
@@ -39,18 +39,16 @@
 #include <bgui/bgui_style_sptr.h>
 #include <vgui/vgui_image_tableau_sptr.h>
 #include <vgui/vgui_easy2D_tableau.h>
+#include <bgui/bgui_vsol2D_tableau.h>
+#include <bgui/bgui_vsol2D_tableau_sptr.h>
 #include <bgui/bgui_vtol2D_tableau_sptr.h>
 
-class bgui_vtol_soview2D_point;
-class bgui_vtol_soview2D_digital_curve;
-class bgui_vtol_soview2D_dotted_digital_curve;
-class bgui_vtol_soview2D_line_seg;
 class bgui_vtol_soview2D_vertex;
 class bgui_vtol_soview2D_edge;
 class bgui_vtol_soview2D_edge_group;
 class bgui_vtol_soview2D_face;
 
-class bgui_vtol2D_tableau : public vgui_easy2D_tableau
+class bgui_vtol2D_tableau : public bgui_vsol2D_tableau
 {
  public:
   bgui_vtol2D_tableau(const char* n="unnamed");
@@ -63,25 +61,6 @@ class bgui_vtol2D_tableau : public vgui_easy2D_tableau
 
   ~bgui_vtol2D_tableau();
 
-  //:virtual handle method for events
-  virtual bool handle(vgui_event const &);
-
-  //: display for vsol_point_2d  (not vtol but often useful)
-  bgui_vtol_soview2D_point*
-    add_vsol_point_2d(vsol_point_2d_sptr const& p);
-
-  //: display for vsol_line_2d (not vtol but useful)
-  bgui_vtol_soview2D_line_seg* 
-    add_vsol_line_2d(vsol_line_2d_sptr const& line);
-
-  //: display for digital_curve (not vtol but often useful)
-  bgui_vtol_soview2D_digital_curve*
-    add_digital_curve(vdgl_digital_curve_sptr const& dc);
-
-  //: display for digital_curve (not vtol but often useful)
-  bgui_vtol_soview2D_dotted_digital_curve*
-    add_dotted_digital_curve(vdgl_digital_curve_sptr const& dc);
-
   //: the vtol display methods for individual topology classes
   bgui_vtol_soview2D_vertex* add_vertex(vtol_vertex_2d_sptr const& v);
   bgui_vtol_soview2D_edge* add_edge(vtol_edge_2d_sptr const& e);
@@ -91,14 +70,7 @@ class bgui_vtol2D_tableau : public vgui_easy2D_tableau
   bgui_vtol_soview2D_face* add_face(vtol_face_2d_sptr const& f);
 
   //: display methods for vectors of topology classes (not grouped)
-
-  void add_spatial_object(vsol_spatial_object_2d_sptr const& sos);
-
   void add_topology_object(vtol_topology_object_sptr const& tos);
-
-  void add_spatial_objects(vcl_vector<vsol_spatial_object_2d_sptr> const& sos);
-
-  void add_lines(vcl_vector<vsol_line_2d_sptr> const & lines);
 
   void add_topology_objects(vcl_vector<vtol_topology_object_sptr> const& tos);
 
@@ -116,23 +88,6 @@ class bgui_vtol2D_tableau : public vgui_easy2D_tableau
   vtol_edge_2d_sptr get_mapped_edge(const int id);
 
   //: Methods for changing the default style of displayable objects
-  void set_vsol_spatial_object_2d_style(vsol_spatial_object_2d_sptr sos,
-                                        const float r, const float g, const float b,
-                                        const float line_width, const float point_radius);
-  void set_vsol_point_2d_style(const float r, const float g, const float b,
-                               const float point_radius);
-
-  void set_vsol_line_2d_style(const float r, const float g, const float b,
-                              const float line_width);
-
-  void set_digital_curve_style(const float r, const float g, const float b,
-                               const float line_width);
-
-  void set_dotted_digital_curve_style(const float r, const float g, 
-                                      const float b,
-                                      const float line_width,
-                                      const float point_radius);
-
   void set_vtol_topology_object_style(vtol_topology_object_sptr tos,
                                       const float r, const float g, const float b,
                                       const float line_width, const float point_radius);
@@ -145,13 +100,8 @@ class bgui_vtol2D_tableau : public vgui_easy2D_tableau
   void set_face_style(const float r, const float g, const float b,
                       const float line_width);
  protected:
-  bool highlight_;
   void init();
   vcl_map<int, vtol_topology_object_sptr> obj_map_;
-  vcl_map<vcl_string, bgui_style_sptr> style_map_;
-  int old_id_;
-  bgui_style_sptr highlight_style_;
-  bgui_style_sptr old_style_;
 };
 
 //this stuff is needed to establish inheritance between tableau  smart pointers
@@ -169,7 +119,7 @@ struct bgui_vtol2D_tableau_new : public bgui_vtol2D_tableau_sptr
   bgui_vtol2D_tableau_new(vgui_tableau_sptr const& t, const char* n="unnamed")
     :  base(new bgui_vtol2D_tableau(t, n)) { }
 
-  operator vgui_easy2D_tableau_sptr () const { vgui_easy2D_tableau_sptr tt; tt.vertical_cast(*this); return tt; }
+  operator bgui_vsol2D_tableau_sptr () const { bgui_vsol2D_tableau_sptr tt; tt.vertical_cast(*this); return tt; }
 };
 
 #endif // bgui_vtol2D_tableau_h_
