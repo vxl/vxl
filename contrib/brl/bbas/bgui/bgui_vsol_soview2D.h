4e6e452516a330ca59894d9a65bec620ae1f1346
Amir Tamrakar <yakkeeper@users.sourceforge.net>
2003-06-12 17:28:46 -0400

    Amir: added a soview for vsol_polyline2d

--- contrib/brl/bbas/bgui/bgui_vsol_soview2D.h
+++ contrib/brl/bbas/bgui/bgui_vsol_soview2D.h
@@ -10,17 +10,26 @@
 //
 // \verbatim
 //  Modifications:
-//   J.L. Mundy June 2, 2003    Initial version.
+//   J.L. Mundy June 2, 2003      Initial version.
+//	  Amir Tamrakar June 06, 2003  Added soview for vsol_polyline_2d.
 // \endverbatim
 //--------------------------------------------------------------------------------
 #include <vcl_vector.h>
 #include <vcl_iosfwd.h>
+
 #include <vsol/vsol_point_2d_sptr.h>
 #include <vsol/vsol_line_2d_sptr.h>
+#include <vsol/vsol_polyline_2d_sptr.h>
+#include <vsol/vsol_conic_2d_sptr.h>
 #include <vsol/vsol_polygon_2d_sptr.h>
+#include <vsol/vsol_rectangle_2d_sptr.h>
+#include <vsol/vsol_triangle_2d_sptr.h>
+#include <vsol/vsol_group_2d_sptr.h>
+#include <vdgl/vdgl_digital_curve_sptr.h>
 
 #include <vgui/vgui_soview2D.h>
 #include <vgui/vgui_style.h>
+
 
 //: vsol_point_2d
 // This view is essentially the same as a point the only difference
@@ -51,6 +60,23 @@ class bgui_vsol_soview2D_line_seg : public vgui_soview2D_lineseg
 
   //: Returns the type of this class ('bgui_vsol_soview2D_edge').
   vcl_string type_name() const { return "bgui_vsol_soview2D_line_seg"; }
+};
+
+//: vsol_polyline_2d
+class bgui_vsol_soview2D_polyline : public vgui_soview2D_linestrip
+{
+ public:
+  //: Constructor - creates a default edge_2d view
+  bgui_vsol_soview2D_polyline() {}
+
+  //: Constructor - creates a view of a given vsol_polyline_2d
+  bgui_vsol_soview2D_polyline(vsol_polyline_2d_sptr const& pline);
+
+  //: Print details about this vtol_edge_2d to the given stream.
+  virtual vcl_ostream& print(vcl_ostream&) const;
+
+  //: Returns the type of this class ('bgui_vsol_soview2D_polyline').
+  vcl_string type_name() const { return "bgui_vsol_soview2D_polyline"; }
 };
 
 //: vsol_polygon_2d 
@@ -87,6 +113,41 @@ class bgui_vsol_soview2D_line_group : public vgui_soview2D_group
   vcl_string type_name() const { return "bgui_vsol_soview2D_line_group"; }
 };
 
+//: vdgl_dotted_digital_curve
+// This one displays points
+class bgui_vsol_soview2D_dotted_digital_curve : public vgui_soview2D_group 
+{
+ public:
+  //: Constructor - creates a default vdgl_digital_curve view
+  bgui_vsol_soview2D_dotted_digital_curve() {;}
+
+  //: Constructor - creates a view of a vdgl_digital_curve
+  bgui_vsol_soview2D_dotted_digital_curve(vdgl_digital_curve_sptr const& e);
+
+  //: Print details about this vdgl_digital_curve to the given stream.
+  virtual vcl_ostream& print(vcl_ostream&) const;
+
+  //: Returns the type of this class ('bgui_vsol_soview2D_dotted_digital_curve').
+  vcl_string type_name() const { return "bgui_vsol_soview2D_dotted_digital_curve"; }
+};
+
+//: vdgl_digital_curve
+class bgui_vsol_soview2D_digital_curve : public vgui_soview2D_linestrip
+{
+ public:
+  //: Constructor - creates a default vdgl_digital_curve view
+  bgui_vsol_soview2D_digital_curve() {;}
+
+  //: Constructor - creates a view of a vdgl_digital_curve
+  bgui_vsol_soview2D_digital_curve(vdgl_digital_curve_sptr const& e);
+
+  //: Print details about this vdgl_digital_curve to the given stream.
+  virtual vcl_ostream& print(vcl_ostream&) const;
+
+  //: Returns the type of this class ('bgui_vsol_soview2D_digital_curve').
+  vcl_string type_name() const { return "bgui_vsol_soview2D_digital_curve"; }
+};
+
 //: a group of vsol_polygon_2d - used for projecting 3-d polyhedra
 class bgui_vsol_soview2D_polygon_group : public vgui_soview2D_group
 {
@@ -103,6 +164,7 @@ class bgui_vsol_soview2D_polygon_group : public vgui_soview2D_group
   //: Returns the type of this class ('bgui_vsol_soview2D_polygon_group').
   vcl_string type_name() const { return "bgui_vsol_soview2D_polygon_group"; }
 };
+
 
 
 #endif // bgui_vsol_soview2D_h_
