00bfd3fe670e977e7d9d35ca2ddc3b116e24717a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-06-16 02:55:07 -0400

    updated w.r.t. CMakeLists.txt

--- contrib/mul/vimt3d/tests/test_include.cxx
+++ contrib/mul/vimt3d/tests/test_include.cxx
@@ -1,6 +1,7 @@
 #include <vimt3d/vimt3d_gaussian_pyramid_builder_3d.h>
 #include <vimt3d/vimt3d_image_3d.h>
 #include <vimt3d/vimt3d_image_3d_of.h>
+#include <vimt3d/vimt3d_load.h>
 #include <vimt3d/vimt3d_sample_grid_trilin.h>
 #include <vimt3d/vimt3d_sample_profile_trilin.h>
 #include <vimt3d/vimt3d_slice.h>
