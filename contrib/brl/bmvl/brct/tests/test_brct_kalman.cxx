620075940cd09947004fefd1ff0bbf224b839bac
Kongbin Kang <kkang@users.sourceforge.net>
2003-06-17 17:09:43 -0400

    fixed a bug on initialization menu

--- contrib/brl/bmvl/brct/tests/test_brct_kalman.cxx
+++ contrib/brl/bmvl/brct/tests/test_brct_kalman.cxx
@@ -16,17 +16,14 @@ int main(int argc, char **argv)
  
 	// set up components of window: menu, scroll bar etc.
   vcl_string title = "Brown Reconstruction Demo";
-
-  vgui_window* win = vgui::produce_window(800, 600, brct_menus::get_menu(), title);
+  vgui_menu bar = brct_menus::get_menu();
+  vgui_window* win = vgui::produce_window(800, 600, bar, title);
   win->get_adaptor()->set_tableau(brct_windows_frame::instance());
-/*  win->get_adaptor()->bind_popups(vgui_CTRL);  
+  win->get_adaptor()->bind_popups(vgui_CTRL);  
   win->set_statusbar(true);
   win->enable_vscrollbar(true);
   win->enable_hscrollbar(true);
   win->show();
-*/
- //brct_windows_frame::instance()->set_window(win);
- //brct_windows_frame::instance()->post_redraw();
 
   return vgui::run();
  
