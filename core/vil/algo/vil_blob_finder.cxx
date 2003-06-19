a70e24cfdc648e7ba72c6c4cb49f6b42137d0000
Tim Cootes <timcootes@users.sourceforge.net>
2003-06-19 18:01:04 -0400

    Added blob finder (no tests yet)

---
+++ core/vil/algo/vil_blob_finder.cxx
@@ -0,0 +1,108 @@
+//:
+// \file
+// \brief Finds connected regions in a boolean image.
+// \author Tim Cootes
+
+#include "vil_blob_finder.h"
+#include <vil/algo/vil_trace_4con_boundary.h>
+
+//: Default constructor
+vil_blob_finder::vil_blob_finder()
+  : i_(-1),j_(-1) {}
+
+//: Construct to work on image (takes deep copy)
+vil_blob_finder::vil_blob_finder(const vil_image_view<bool>& image)
+  : i_(0),j_(0)
+{
+  image_.deep_copy(image);
+}
+
+//: Define image to work on (deep copy taken)
+void vil_blob_finder::set_image(const vil_image_view<bool>& image)
+{
+  i_=0; j_=0;
+  image_.deep_copy(image);
+}
+
+//: Define image to work on and use as workspace
+//  Image will be erased during process. Avoids the copy in set_image()
+void vil_blob_finder::set_work_image(vil_image_view<bool>& image)
+{
+  i_=0; j_=0;
+  image_ = image;
+}
+
+//: Delete region in image with boundary (bi,bj)
+void delete_blob(vil_image_view<bool>& image,
+                 const vcl_vector<int>& bi, const vcl_vector<int>& bj)
+{
+  const unsigned n = bi.size();
+	const unsigned ni = image.ni();
+  for (unsigned k=0;k<n;++k)
+	{
+	  int j = bj[k];
+
+	  // Delete all neighbours to the right
+	  int i = bi[k];
+    while (i<ni && image(i,j)) { image(i,j)=false; ++i; }
+
+		// Delete all neighbours to the left
+    i=bi[k]-1;
+		while (i>=0 && image(i,j)) { image(i,j)=false; --i; }
+	}
+}
+
+//: Get boundary pixels of next blob in current image.
+// Uses four connected boundary representation.
+// Return false if no more regions
+bool vil_blob_finder::next_4con_region(vcl_vector<int>& bi, vcl_vector<int>& bj)
+{
+  while (j_<image_.nj())
+	{
+	  while (i_<image_.ni())
+		{
+      if (image_(i_,j_))
+			{
+			  vil_trace_4con_boundary(bi,bj,image_,i_,j_);
+				delete_blob(image_,bi,bj);
+				return true;
+			}
+			i_++;
+
+		}
+		// Move to first element of next row
+		i_ = 0;
+	  j_++;
+	}
+
+	return false;  // Reached end of image without finding another blob
+}
+
+//: Get largest blob in current image
+//  Assumes image has been initialised, and that next_4con_region not
+//  yet called.  Erases internal image during this call, so any
+//  subsequent calls will not work.
+void vil_blob_finder::largest_4con_region(vcl_vector<int>& bi, vcl_vector<int>& bj)
+{
+  bi.resize(0); bj.resize(0);
+  vcl_vector<int> tmp_bi,tmp_bj;
+	while (next_4con_region(tmp_bi,tmp_bj))
+	{
+	  if (tmp_bi.size()>bi.size())
+		{
+		  vcl_swap(bi,tmp_bi);
+		  vcl_swap(bj,tmp_bj);
+		}
+  }
+}
+
+//: Get number of blobs in given image
+unsigned vil_blob_finder::n_4con_regions(const vil_image_view<bool>& image)
+{
+  set_image(image);
+  unsigned n=0;
+  vcl_vector<int> tmp_bi,tmp_bj;
+	while (next_4con_region(tmp_bi,tmp_bj)) n++;
+	return n;
+}
+
---
+++ core/vil/algo/vil_blob_finder.h
@@ -0,0 +1,75 @@
+#ifndef vil_blob_finder_h_
+#define vil_blob_finder_h_
+
+//:
+// \file
+// \brief Finds connected regions in a boolean image.
+// \author Tim Cootes
+
+#include <vil/vil_image_view.h>
+#include <vcl_vector.h>
+
+//: Finds connected regions in a boolean image
+//  Class to iterate through image, finding boundaries of all regions
+//  (connected true pixels).
+//
+//  Currently only 4-connected regions supported. Sometime 8-connected will
+//  be added.
+//
+//  Note that the usual use of the class copies the boolean image input,
+//  then erases the true pixels as it goes through finding the blobs (in
+//  order to avoid finding the same blob twice).  If the image is expendable,
+//  then the deep copy can be avoided using set_work_image(im).  In this
+//  case the supplied image (im) will be erased as the blob finder goes
+//  about it's humble task.
+//
+//  \verbatim
+//  vcl_vector<int> bi,bj;
+//  vil_blob_finder finder(bool_image);
+//  while (finder.next_4con_region(bi,bj))
+//  {
+//    vcl_cout<<"Blob boundary length: "<<bi.size()<<vcl_endl;
+//  }
+//  \endverbatim
+class vil_blob_finder {
+private:
+    //: Workspace (may be view of external image)
+  vil_image_view<bool> image_;
+
+	  //: Current point of interest
+	int i_,j_;
+
+public:
+    //: Default constructor
+  vil_blob_finder();
+
+	  //: Construct to work on image (takes deep copy)
+  vil_blob_finder(const vil_image_view<bool>& image);
+
+	  //: Define image to work on (deep copy taken)
+  void set_image(const vil_image_view<bool>& image);
+
+	  //: Define image to work on and use as workspace
+		//  Image will be erased during process.
+		//  Avoids the deep copy in set_image()
+  void set_work_image(vil_image_view<bool>& image);
+
+	  //: Get boundary pixels of next blob in current image.
+		// Uses four connected boundary representation.
+		// Return false if no more regions
+  bool next_4con_region(vcl_vector<int>& bi, vcl_vector<int>& bj);
+
+	  //: Get largest blob in current image
+		//  Assumes image has been initialised, and that next_4con_region not
+		//  yet called.  Erases internal image during this call, so any
+		//  subsequent calls will not work.
+		//
+		//  bi,bj empty if no blobs found.
+  void largest_4con_region(vcl_vector<int>& bi, vcl_vector<int>& bj);
+
+	  //: Get number of blobs in given image
+		//  Overrides any internal state
+  unsigned n_4con_regions(const vil_image_view<bool>& image);
+};
+
+#endif
