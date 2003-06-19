aadefe4891368f264a2d442332a84adfdc06960b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-06-19 11:18:09 -0400

    use random image pixel values for testing FFT

--- core/vil/tests/test_algo_fft.cxx
+++ core/vil/tests/test_algo_fft.cxx
@@ -4,40 +4,46 @@
 #include <vil/vil_math.h>
 #include <vil/vil_image_view.h>
 #include <vil/algo/vil_fft.h>
+#include <vcl_ctime.h> // seed for random number generator
 
 MAIN( test_algo_fft )
 {
   START( "vil_algo_fft" );
 
-  vil_image_view<vcl_complex<double> > img0 (4, 8, 2);
+  vil_image_view<vcl_complex<double> > img0(4, 8, 2);
+  unsigned int seed = (unsigned int)vcl_time(0);
 
   // fill in image
   for (unsigned i=0; i<img0.ni(); i++)
   for (unsigned j=0; j<img0.nj(); j++)
-  for (unsigned p=0; p<img0.nplanes(); p++)
-      img0 (i, j, p) =
-          vcl_complex<double> (111+i+10*j+100*p+0.1, 111+i+10*j+100*p+0.2);
+  for (unsigned p=0; p<img0.nplanes(); ++p, seed*=16807)
+      img0(i,j,p) = vcl_complex<double>(-1e-5*seed+111.1, -1e-5*seed+222.2);
 
   // copy image
   vil_image_view<vcl_complex<double> > img1;
-  img1.deep_copy (img0);
+  img1.deep_copy(img0);
 
   // FFT and inverse FFT
-  vil_fft_2d_fwd (img1);
-  vil_fft_2d_bwd (img1);
+  vil_fft_2d_fwd(img1);
+  vil_fft_2d_bwd(img1);
 
   // make sure original image is recovered
-  double d = vil_math_ssd_complex (img0, img1, double());
-  d = vcl_sqrt (d / (double) img0.size());
-  TEST_NEAR ("FFT and inverse FFT recovers image", d, 0.0, 1e-5);
+  double d = vil_math_ssd_complex(img0, img1, double());
+  d /= (double) img0.size();
+  TEST_NEAR("FFT and inverse FFT recovers image", d, 0.0, 1e-9);
 
   // FFT of constant image
   vcl_complex<double> v = vcl_complex<double>(1.1, 2.2);
-  img0.fill (v);
-  vil_fft_2d_fwd (img0);
-  TEST_NEAR("FFT coeff. 0,0 is mean", vcl_abs (v - img0(0,0,0)), 0.0, 1e-5);
-  TEST_NEAR("FFT coeff. 0,1 is 0", vcl_abs (img0(0,1,0)), 0.0, 1e-5);
-  TEST_NEAR("FFT coeff. 1,0 is 0", vcl_abs (img0(1,0,0)), 0.0, 1e-5);
+  img0.fill(v);
+  vil_fft_2d_fwd(img0);
+  TEST_NEAR("FFT coeff. 0,0 is mean", img0(0,0,0), v, 1e-9);
+  TEST_NEAR("FFT coeff. 0,0 is mean", img0(0,0,1), v, 1e-9);
+  // select just any pixel component (i,j,p) different from (0,0,*):
+  unsigned int i = seed%img0.ni(); seed *= 16807;
+  unsigned int j = seed%img0.nj(); seed *= 16807;
+  unsigned int p = seed%img0.nplanes();
+  if (i==0 && j==0) i=1;
+  TEST_NEAR("any other FFT coeff. is 0", img0(i,j,p), 0.0, 1e-9);
 
   SUMMARY();
 }
