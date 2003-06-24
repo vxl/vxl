89e8887ed50809f39aaa47280e69e451865d050c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-06-24 12:33:17 -0400

    implemented the second compute() method

--- core/vgl/algo/vgl_h_matrix_1d_compute.h
+++ core/vgl/algo/vgl_h_matrix_1d_compute.h
@@ -2,19 +2,19 @@
 #define vgl_h_matrix_1d_compute_h_
 //:
 // \file
-//
-// Base class of classes to generate a line-to-line projectivity matrix from
-// a set of matched points.
+// \brief Virtual base class of classes to generate a line-to-line projectivity from a set of matched points
 //
 // \author
 //  Frederik Schaffalitzky , Robotic Research Group
 //
 // \verbatim
-// Modifications
-//    23 Mar 2003 - J.L. Mundy - preparing for upgrade to vgl 
-//                  computations restricted to vgl_homg_point_1d<double>
-//                  Seems somewhat overdoing it to template the transform
-//                  solvers since double is needed for robust computation 
+//  Modifications
+//   23 Mar 2003 - J.L. Mundy - preparing for upgrade to vgl 
+//                 computations restricted to vgl_homg_point_1d<double>
+//                 Seems somewhat overdoing it to template the transform
+//                 solvers since double is needed for robust computation 
+//   23 Jun 2003 - Peter Vanroose - made compute_cool_homg pure virtual
+//   24 Jun 2003 - Peter Vanroose - implemented the second compute() method
 // \endverbatim
 
 #include <vgl/vgl_homg_point_1d.h>
@@ -28,31 +28,35 @@ class vgl_h_matrix_1d_compute
   vgl_h_matrix_1d_compute() : verbose_(false) { }
   virtual ~vgl_h_matrix_1d_compute() { }
 
-  //
-  void verbose(bool); // set this to true for verbose run-time information
+  //: set this to true for verbose run-time information; default is false
+  void verbose(bool v) { verbose_=v; }
 
   //
   // Compute methods :
   //
-  bool compute(const vcl_vector<vgl_homg_point_1d<double> >&,
-               const vcl_vector<vgl_homg_point_1d<double> >&,
-               vgl_h_matrix_1d<double>& H );
 
-  bool compute(const double [],
-               const double [],
-               int,
-               vgl_h_matrix_1d<double> *);
+  //: principal interface: given point correspondences in p1,p2, returns H
+  bool compute(const vcl_vector<vgl_homg_point_1d<double> >& p1,
+               const vcl_vector<vgl_homg_point_1d<double> >& p2,
+               vgl_h_matrix_1d<double>& H)
+  { return compute_cool_homg(p1,p2,H); } // calls pure virtual function
+
+  //: nonhomogeneous interface: given point correspondences in p1,p2, returns H
+  bool compute(const double p1[],
+               const double p2[],
+               unsigned int length, // length of both p1 and p2
+               vgl_h_matrix_1d<double>& H)
+  { return compute_array_dbl(p1,p2,length,H); }
+
  protected:
   bool verbose_;
 
   virtual bool compute_cool_homg(const vcl_vector<vgl_homg_point_1d<double> > &,
                                  const vcl_vector<vgl_homg_point_1d<double> > &,
-                                 vgl_h_matrix_1d<double>& H);
+                                 vgl_h_matrix_1d<double>& H) = 0;
 
-  virtual bool compute_array_dbl(const double [],
-                                 const double [],
-                                 int N,
-                                 vgl_h_matrix_1d<double>& H);
+  bool compute_array_dbl(const double p1[], const double p2[], unsigned int length,
+                         vgl_h_matrix_1d<double>& H);
 };
 
 #endif // vgl_h_matrix_1d_compute_h_
