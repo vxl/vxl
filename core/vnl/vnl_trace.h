22368f5f1a1d448c5178a955e6ab691514a31821
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-06-30 12:00:08 -0400

    trace of vnl_matrix_fixed added

--- core/vnl/vnl_trace.h
+++ core/vnl/vnl_trace.h
@@ -10,15 +10,33 @@
 //  \author fsm
 //
 // \verbatim
-// Modifications
-// LSB (Manchester) 19/3/01 Documentation tidied
+//  Modifications
+//   LSB (Manchester) 19/3/01 Documentation tidied
+//   Peter Vanroose   27-Jun-2003  made inline and added trace(matrix_fixed)
 // \endverbatim
 
-template <class T> class vnl_matrix;
+#include <vnl/vnl_matrix.h>
+#include <vnl/vnl_matrix_fixed.h>
 
 //: Calculate trace of a matrix
 // \relates vnl_matrix
 template <class T>
-T vnl_trace(vnl_matrix<T> const &);
+T vnl_trace(vnl_matrix<T> const& M) {
+  T sum(0);
+  const unsigned int N = M.rows()<M.cols() ? M.rows() : M.cols();
+  for (unsigned int i=0; i<N; ++i)
+    sum += M(i, i);
+  return sum;
+}
+
+//: Calculate trace of a matrix
+// \relates vnl_matrix_fixed
+template <class T, unsigned int N1, unsigned int N2>
+T vnl_trace(vnl_matrix_fixed<T,N1,N2> const& M) {
+  T sum(0);
+  for (unsigned int i=0; i<N1 && i<N2; ++i)
+    sum += M(i, i);
+  return sum;
+}
 
 #endif // vnl_trace_h_
