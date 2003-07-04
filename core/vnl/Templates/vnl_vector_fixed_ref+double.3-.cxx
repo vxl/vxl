cb0253f4af32fcce35f72b658a90b16161ef6bce
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-07-04 05:19:50 -0400

    end macro call with a ";"

--- core/vnl/Templates/vnl_vector_fixed_ref+double.3-.cxx
+++ core/vnl/Templates/vnl_vector_fixed_ref+double.3-.cxx
@@ -1,2 +1,2 @@
 #include <vnl/vnl_vector_fixed_ref.txx>
-VNL_VECTOR_FIXED_REF_INSTANTIATE(double,3)
+VNL_VECTOR_FIXED_REF_INSTANTIATE(double,3);
--- core/vnl/Templates/vnl_vector_fixed_ref+float.3-.cxx
+++ core/vnl/Templates/vnl_vector_fixed_ref+float.3-.cxx
@@ -1,2 +1,2 @@
 #include <vnl/vnl_vector_fixed_ref.txx>
-VNL_VECTOR_FIXED_REF_INSTANTIATE(float,3)
+VNL_VECTOR_FIXED_REF_INSTANTIATE(float,3);
