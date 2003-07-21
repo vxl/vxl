f540ea06e188d2de21a1024701f37795460e284f
F. Schaffalitzky <fsm@users.sourceforge.net>
2003-07-21 00:57:23 -0400

    resolved documentation/implementation conflict

--- core/vgl/vgl_lineseg_test.cxx
+++ core/vgl/vgl_lineseg_test.cxx
@@ -10,16 +10,27 @@
 #include <vgl/vgl_triangle_test.h>
 
 template <class T>
-bool vgl_lineseg_test(T x1, T y1, T x2, T y2, T x3, T y3, T x4, T y4)
+bool vgl_lineseg_test_line(T x1, T y1, T x2, T y2, T x3, T y3, T x4, T y4)
 {
   T a = vgl_triangle_test_discriminant(x1, y1, x2, y2, x3, y3);
   T b = vgl_triangle_test_discriminant(x1, y1, x2, y2, x4, y4);
+  // this condition just says that [3] and [4] lie on opposite
+  // sides of the line joining [1], [2].
   return (a<=0 && b>=0) || (a>=0 && b<=0);
+}
+
+template <class T>
+bool vgl_lineseg_test_lineseg(T x1, T y1, T x2, T y2, T x3, T y3, T x4, T y4)
+{
+  return
+    vgl_lineseg_test_line(x1, y1, x2, y2, x3, y3, x4, y4) &&
+    vgl_lineseg_test_line(x3, y3, x4, y4, x1, y1, x2, y2);
 }
 
 //--------------------------------------------------------------------------------
 
 #define inst(T) \
-template bool vgl_lineseg_test(T, T, T, T, T, T, T, T);
+template bool vgl_lineseg_test_line(T, T, T, T, T, T, T, T); \
+template bool vgl_lineseg_test_lineseg(T, T, T, T, T, T, T, T)
 inst(float);
 inst(double);
--- core/vgl/vgl_lineseg_test.h
+++ core/vgl/vgl_lineseg_test.h
@@ -10,20 +10,28 @@
 
 #include <vgl/vgl_line_segment_2d.h>
 
-//: return true if the two linesegments meet
-template <class T>
-bool vgl_lineseg_test(T x1, T y1, T x2, T y2, T x3, T y3, T x4, T y4);
+// The old signature vgl_lineseg_test() was incorrectly documented. Its
+// meaning was the same as the new vgl_lineseg_test_line(). Only you can
+// decide which one you want.
+
+//: true if the line joining [1], [2] meets the linesegment joining [3], [4].
+export template <class T>
+bool vgl_lineseg_test_line(T x1, T y1, T x2, T y2, T x3, T y3, T x4, T y4);
+
+//: true if the linesegment joining [1], [2] meets the linesegment joining [3], [4].
+export template <class T>
+bool vgl_lineseg_test_lineseg(T x1, T y1, T x2, T y2, T x3, T y3, T x4, T y4);
 
 //: return true if the two linesegments meet
 // \relates vgl_line_segment_2d
 template <class T>
-inline bool vgl_lineseg_test(vgl_line_segment_2d<T> const& l1,
-                             vgl_line_segment_2d<T> const& l2)
+inline bool vgl_lineseg_test_lineseg(vgl_line_segment_2d<T> const& l1,
+                                     vgl_line_segment_2d<T> const& l2)
 {
-  return vgl_lineseg_test(l1.point1().x(),l1.point1().y(),
-                          l1.point2().x(),l1.point2().y(),
-                          l2.point1().x(),l2.point1().y(),
-                          l2.point2().x(),l2.point2().y());
+  return vgl_lineseg_test_lineseg(l1.point1().x(),l1.point1().y(),
+                                  l1.point2().x(),l1.point2().y(),
+                                  l2.point1().x(),l2.point1().y(),
+                                  l2.point2().x(),l2.point2().y());
 }
 
 #endif // vgl_lineseg_test_h_
