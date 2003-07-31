3f0e9257fa20fbeb57637bc5562ffa6c14f61ec7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-07-31 07:08:46 -0400

    replaced #include <vtol/vtol_edge.h> with #include <vtol/vtol_topology_object.h>

--- contrib/gel/vifa/vifa_typedefs.h
+++ contrib/gel/vifa/vifa_typedefs.h
@@ -7,8 +7,7 @@
 #include <sdet/sdet_fit_lines_params.h>
 #include <vdgl/vdgl_intensity_face_sptr.h>
 #include <vtol/vtol_edge_2d_sptr.h>
-#include <vtol/vtol_edge.h>
-#include <vtol/vtol_face.h>
+#include <vtol/vtol_topology_object.h>
 #include <vifa/vifa_imp_line.h>
 
 
@@ -17,23 +16,23 @@ typedef vcl_vector<vdgl_intensity_face_sptr>  iface_list;
 typedef iface_list::iterator          iface_iterator;
 
 // Edges
-typedef vcl_vector<vtol_edge_2d_sptr>      edge_2d_list;
-typedef edge_2d_list::iterator          edge_2d_iterator;
+typedef vcl_vector<vtol_edge_2d_sptr> edge_2d_list;
+typedef edge_2d_list::iterator        edge_2d_iterator;
 
 // Edge list iterator
-typedef edge_list::iterator            edge_iterator;
+typedef edge_list::iterator           edge_iterator;
 
 // Face list iterator
-typedef face_list::iterator            face_iterator;
+typedef face_list::iterator           face_iterator;
 
 // Implicit lines
-typedef vifa_imp_line<double>          imp_line;
-typedef vbl_smart_ptr<imp_line>          imp_line_sptr;
-typedef vcl_vector<imp_line_sptr>        imp_line_list;
-typedef imp_line_list::iterator          imp_line_iterator;
+typedef vifa_imp_line<double>         imp_line;
+typedef vbl_smart_ptr<imp_line>       imp_line_sptr;
+typedef vcl_vector<imp_line_sptr>     imp_line_list;
+typedef imp_line_list::iterator       imp_line_iterator;
 
-typedef vcl_vector<imp_line_list*>        imp_line_table;
-typedef imp_line_table::iterator        imp_line_table_iterator;
+typedef vcl_vector<imp_line_list*>    imp_line_table;
+typedef imp_line_table::iterator      imp_line_table_iterator;
 
 // Line fitting parameters
 typedef vbl_smart_ptr<sdet_fit_lines_params>  sdet_fit_lines_params_sptr;
