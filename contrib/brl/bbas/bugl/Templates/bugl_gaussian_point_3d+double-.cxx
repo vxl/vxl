062a7fadecab7ed6996610aea6c04488c5e874bb
Kongbin Kang <kkang@users.sourceforge.net>
2003-08-02 02:41:36 -0400

    adding the missed files

---
+++ contrib/brl/bbas/bugl/Templates/bugl_gaussian_point_3d+double-.cxx
@@ -0,0 +1,3 @@
+#include <bugl/bugl_gaussian_point_3d.txx>
+BUGL_GAUSSIAN_POINT_3D_INSTANTIATE(double);
+
---
+++ contrib/brl/bbas/bugl/Templates/bugl_uncertainty_point_3d+double-.cxx
@@ -0,0 +1,2 @@
+#include <bugl/bugl_uncertainty_point_3d.txx>
+BUGL_UNCERTAINTY_POINT_3D_INSTANTIATE(double);
