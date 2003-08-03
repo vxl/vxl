c5e008602d4225931e2f8f63b42695add958db0b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-08-03 03:18:15 -0400

    doxygen comments will not be shown without \file

--- contrib/brl/bbas/bugl/bugl_gaussian_point_2d.h
+++ contrib/brl/bbas/bugl/bugl_gaussian_point_2d.h
@@ -1,21 +1,19 @@
 #ifndef gaussian_point_2d_h_
 #define gaussian_point_2d_h_
 //:
+// \file
 // \author Kongbin Kang
 // A class for 2d point with Gaussian distribution
-// 
-// \verbatim
-// \endverbatim
 
 #include "bugl_uncertainty_point_2d.h"
 #include <vnl/vnl_matrix_fixed.h>
 template<class T>
 class bugl_gaussian_point_2d:public bugl_uncertainty_point_2d<T>
 {
-private:
+ private:
   vnl_matrix_fixed<T, 2, 2> sigma_;
   vnl_matrix_fixed<T, 2, 2> sigma_inv_;
-public:  
+ public:
   bugl_gaussian_point_2d(T x, T y, vnl_matrix_fixed<T, 2, 2> & s);
   ~bugl_gaussian_point_2d() {}
 };
