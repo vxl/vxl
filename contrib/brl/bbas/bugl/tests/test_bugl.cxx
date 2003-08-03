e6ec991d6ae8a2adfaae3e44190536d53cc758a3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-08-03 03:21:31 -0400

    bug fix: there is no [2][2] in a double_2x2

--- contrib/brl/bbas/bugl/tests/test_bugl.cxx
+++ contrib/brl/bbas/bugl/tests/test_bugl.cxx
@@ -1,4 +1,3 @@
-#include <vcl_iostream.h>
 #include <bugl/bugl_gaussian_point_2d.h>
 #include <bugl/bugl_gaussian_point_3d.h>
 #include <vnl/vnl_double_2x2.h>
@@ -6,15 +5,12 @@
 
 int main()
 {
-
-  vnl_double_2x2 s2;
-  s2 = 0;
-  s2[0][0] = 1; s2[1][1] = 3; s2[2][2] ;
+  vnl_double_2x2 s2(0.0);
+  s2[0][0] = 1; s2[1][1] = 3;
   bugl_gaussian_point_2d<double> p2d(1, 1, s2);
 
-  vnl_double_3x3 s3;
-  s3 = 0; 
+  vnl_double_3x3 s3(0.0);
   s3[0][0] = 1; s3[1][1] = 3; s3[2][2] = 5;
-  bugl_gaussian_point_3d<double> p3d(100, 100, 100, s3);  
+  bugl_gaussian_point_3d<double> p3d(100, 100, 100, s3);
   return 0;
 }
