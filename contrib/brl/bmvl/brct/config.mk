04e7439dfdeef8b02e653c3e99e3636aca101dbc
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-08-05 04:26:33 -0400

    updated w.r.t. CMakeLists.txt

--- contrib/brl/bmvl/brct/config.mk
+++ contrib/brl/bmvl/brct/config.mk
@@ -1,5 +1,6 @@
 IULIBS += -lbrct
 include ${IUEROOT}/contrib/brl/bbas/bdgl/config.mk
+include ${IUEROOT}/contrib/brl/bbas/bugl/config.mk
 include ${IUEROOT}/contrib/gel/vdgl/config.mk
 include ${IUEROOT}/contrib/gel/vsol/config.mk
 include ${IUEROOT}/contrib/oxl/mvl/config.mk
