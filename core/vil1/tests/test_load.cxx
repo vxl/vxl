60ec5fc1cb7888815dcc6beed7697f251e000cac
Amitha Perera <amithaperera@users.sourceforge.net>
2003-08-06 16:01:14 -0400

    Increase buffer sizes to match what the tests require. How were these tests passing before?!

--- core/vil1/tests/test_load.cxx
+++ core/vil1/tests/test_load.cxx
@@ -4,6 +4,7 @@
 // \author Peter Vanroose
 
 #include <vcl_fstream.h>
+#include <vcl_ios.h>
 
 #include <vpl/vpl.h>
 #include <vul/vul_temp_filename.h>
@@ -15,19 +16,31 @@
 
 static void test(char const* magic, int comps, int bits, int maxval)
 {
-  vcl_string tmp_nam = vul_temp_filename() + ".pgm";
-  char const *file = tmp_nam!=".pgm" ? tmp_nam.c_str() : "t.pgm";
+  char const* ext = ".pgm";
+  if( comps == 3 )
+    ext = ".ppm";
+  vcl_string tmp_nam = vul_temp_filename();
+  if( tmp_nam == "" )
+    tmp_nam = "t";
+  tmp_nam += ext;
+  char const *file = tmp_nam.c_str();
   {
-    vcl_ofstream f(file);
+    vcl_ofstream f(file, vcl_ios_binary);
 #ifdef LEAVE_IMAGES_BEHIND
       vpl_chmod(file, 0666); // -rw-rw-rw-
 #endif
     f << magic << "\n2\n3\n";
     if (maxval > 1) f << maxval << "\n";
     if (magic[1] > '3')
-      f << "ABCDEFGHIJKLMNOPQRSTUVWX"; // 24 bytes: sufficient for 32bit ppm
+      // 72 bytes: sufficient for 32bit/component ppm (although the
+      // ppm standard, at http://netpbm.sourceforge.net/doc//ppm.html,
+      // does not allow more than 16bits/component).
+      f << "ABCDEFGHIJKLMNOPQRSTUVWX"
+           "ABCDEFGHIJKLMNOPQRSTUVWX"
+           "ABCDEFGHIJKLMNOPQRSTUVWX"
+           "ABCDEFGHIJKLMNOPQRSTUVWX";
     else
-      for (int i=0; i<24; ++i) f << 5+7*i+9*i*i << '\n';
+      for (int i=0; i<72; ++i) f << 5+7*i+9*i*i << '\n';
   }
 
   vil1_image i = vil1_load(file);
@@ -35,7 +48,8 @@ static void test(char const* magic, int comps, int bits, int maxval)
   if (i) {
     vcl_cout << "test vil1_load: size " << i.width() << "x" << i.height() << ", "
              << i.components() << " component(s)" << ", "
-             << i.bits_per_component() << " bits\n";
+             << i.bits_per_component() << " bits (magic "
+             << magic << ")" << vcl_endl;
 
     TEST("width", i.width(), 2);
     TEST("height", i.height(), 3);
@@ -46,7 +60,7 @@ static void test(char const* magic, int comps, int bits, int maxval)
     TEST("get_property(\"top row first\")", i.get_property("top row first"), true); 
     TEST("get_property(\"left first\")", i.get_property("left first"), true); 
     TEST("get_property(\"component order is B,G,R\")", i.get_property("component order is B,G,R"), false); 
-    char buf[24];
+    char buf[72];
     TEST("get_plane(0)", i.get_plane(0), i); 
     TEST("get_section()", i.get_section(buf, 0, 0, 2, 3) != 0, true); 
     if (magic[1] > '3')
@@ -75,10 +89,14 @@ MAIN( test_load )
   test("P1", 1,  1, 1);
   test("P2", 1,  8, 255);
   test("P2", 1, 16, 65535);
+// PNM does not support components > 16 bits
+// (http://netpbm.sourceforge.net/doc//ppm.html)
+#if 0 
   test("P2", 1, 32, 16777216);
+#endif
   test("P3", 3,  8, 255);
-#if 0 // These give a bus error on some platforms when vil1_image i is destructed
   test("P3", 3, 16, 65535);
+#if 0
   test("P3", 3, 32, 16777216);
 #endif
   test("P4", 1,  1, 1);
