71eb034caddc1d0a109ed45746fe06c1328e8707
Amitha Perera <amithaperera@users.sourceforge.net>
2003-08-06 17:45:09 -0400

    Rename from vdgl/vdgl_intensity_face.h
    Previous log contents:
    
    ----------------------------
    revision 1.13
    date: 2003/07/29 20:59:11;  author: peter_vanroose;  state: Exp;  lines: +1 -1
    TABs replaced with spaces
    ----------------------------
    revision 1.12
    date: 2003/07/25 18:37:00;  author: petersmi;  state: Exp;  lines: +23 -3
    Added support for intensity face attributes (contrib/gel/vifa)
    ----------------------------
    revision 1.11
    date: 2003/06/01 22:33:17;  author: peter_vanroose;  state: Exp;  lines: +3 -3
    typo corrected (samping-->sampling)
    ----------------------------
    revision 1.10
    date: 2003/04/16 15:48:05;  author: brooksby;  state: Exp;  lines: +2 -0
    Added a couple of methods due to changes in vdgl_digital_region.
    ----------------------------
    revision 1.9
    date: 2003/02/05 09:31:33;  author: peter_vanroose;  state: Exp;  lines: +1 -1
    moved IsHoleP() from vdgl_intensity_face up to vtol_face
    ----------------------------
    revision 1.8
    date: 2003/01/10 22:04:11;  author: peter_vanroose;  state: Exp;  lines: +21 -24
    safer and simpler interface to bounding box, and it now also works when (0,0) is not part of the bounding box; meanwhile deprecating the old interface
    ----------------------------
    revision 1.7
    date: 2003/01/08 22:30:44;  author: mundyj;  state: Exp;  lines: +4 -0
    added a few checks for Npts = 0 in digital_region and added more elements of the digital region interface to intensity_face
    ----------------------------
    revision 1.6
    date: 2002/05/15 10:29:26;  author: peter_vanroose;  state: Exp;  lines: +31 -39
    replacing call to invalid Xi() and Yi() by Ix() and Iy(); making methods from vdgl_digital_region const
    ----------------------------
    revision 1.5
    date: 2002/05/08 18:54:10;  author: amithaperera;  state: Exp;  lines: +2 -0
    Reorder SUBDIRS to match dependencies. Put in hack for vdgl/vtol dependency
    cycle. Comment out undefined function from header. Things should gel should
    now compile and link.
    ----------------------------
    revision 1.4
    date: 2002/05/08 09:27:53;  author: peter_vanroose;  state: Exp;  lines: +56 -7
    copy constructor added; diamond-inheritance avoided by adding member region_ and redirecting interface
    ----------------------------
    revision 1.3
    date: 2002/05/07 17:34:30;  author: kelliher;  state: Exp;  lines: +1 -1
    ERR: reorder inheritance, get rid of warning
    ----------------------------
    revision 1.2
    date: 2002/05/07 14:27:25;  author: kelliher;  state: Exp;  lines: +1 -8
    move intensity face to vdgl
    ----------------------------
    revision 1.1
    date: 2002/05/06 14:49:33;  author: kelliher;  state: Exp;
    ADM: move from gevd
    =============================================================================

---
+++ contrib/gel/vtol/vtol_intensity_face.h
@@ -0,0 +1,147 @@
+// This is gel/vtol/vtol_intensity_face.h
+#ifndef vtol_intensity_face_h_
+#define vtol_intensity_face_h_
+//:
+// \file
+// \brief A face with intensity attributes
+//
+//  A sub-class of Face which contains intensity attributes that are the
+//  result of sampling the interior of the face in an image. The boundaries
+//  of the face are vtol_edge(s) with DigitalCurve geometry. The
+//  IntensityFace is nominally 2d and embedded in an image coordinate
+//  frame. The IntensityFace can be constructed from a 3-d linear
+//  Face (Face with ImplicitPlane and ImplicitLine geometry) under
+//  a WorldToImage camera projection, or directly from an image region
+//  analysis.  In the latter case the IntensityFace is constructed
+//  from a set of vtol_edge(s) and then the interior is derived from the
+//  associated image region.  If the IntensityFace is derived from
+//  a projected linear Face, the projected boundaries of a set of Faces
+//  are used to tessellate the image for region analysis.
+//
+// \author J.L. Mundy
+// \date   November 21, 1999
+//
+// \verbatim
+//  Modifications
+//   8-May-2002 - Peter Vanroose - no longer inherits from vdgl_digital_region:
+//                    dependency changed to "has_a", but with cast_to semantics
+//   8-Jan-2003 - Peter Vanroose - moved compute_bounding_box() to vtol_face
+//   5-Feb-2003 - Peter Vanroose - moved IsHoleP() to vtol_face_2d
+//   9-May-2003 - Mike Petersen - intensity face attributes support:
+//                    - added get_min() & get_max() pass-throughs
+//                    - restored perimeter() API
+//                    - added topology_type() override (INTENSITYFACE)
+// \endverbatim
+//
+//-------------------------------------------------------------------------
+
+#include <vnl/vnl_matrix.h>
+#include <vdgl/vdgl_digital_region.h>
+#include <vtol/vtol_topology_object.h>
+#include <vtol/vtol_face_2d.h>
+#include <vtol/vtol_intensity_face_sptr.h>
+
+class vtol_intensity_face : public vtol_face_2d
+{
+ protected:
+  // Data Members--------------------------------------------------------------
+  vdgl_digital_region* region_;
+ public:
+  //Constructors---------------------------------------------------------------
+  vtol_intensity_face(vcl_vector<vtol_edge_sptr>* edges);
+  vtol_intensity_face(one_chain_list& one_chains);
+  //  vtol_intensity_face(vcl_vector<vtol_edge_sptr>* edges, vdgl_digital_region& dr);
+  vtol_intensity_face(vcl_vector<vtol_one_chain_sptr>* chains, vdgl_digital_region const& dr);
+  vtol_intensity_face(vtol_intensity_face const& iface);
+  vtol_intensity_face(vtol_face_2d& face, int npts, float const* xp, float const* yp,
+                      unsigned short const* pix);
+  vtol_intensity_face(vtol_face_2d& face, int npts, float const* xp, float const* yp,
+                      float const* zp, unsigned short const* pix);
+  ~vtol_intensity_face();
+
+  //---------------------------------------------------------------------------
+  //: Clone `this': creation of a new object and initialization
+  //  See Prototype pattern
+  //---------------------------------------------------------------------------
+  virtual vsol_spatial_object_2d_sptr clone(void) const;
+
+  inline vtol_topology_object::vtol_topology_object_type
+    GetTopologyType() const { return vtol_topology_object::INTENSITYFACE; }
+
+  // MPP 5/9/2003
+  // Added API consistent w/ overloaded vtol method
+  virtual vtol_topology_object::vtol_topology_object_type
+  topology_type(void) const { return GetTopologyType(); }
+
+  virtual vtol_intensity_face* CastToIntensityFace() {return this;}
+  virtual vdgl_digital_region* cast_to_digital_region() const {return region_;}
+  virtual vdgl_digital_region* cast_to_digital_region() {return region_;}
+  double area() const { return region_->area(); }
+  void ResetPixelData() { region_->ResetPixelData(); }
+  void IncrementMeans(float x, float y, unsigned short pix){region_->IncrementMeans(x,y,pix);}
+  void ComputeIntensityStdev(){region_->ComputeIntensityStdev();}
+  void InitPixelArrays() { region_->InitPixelArrays(); }
+  void InsertInPixelArrays(float x, float y, unsigned short pix){region_->InsertInPixelArrays(x,y,pix);}
+  float const* Xj() const { return region_->Xj(); }
+  float const* Yj() const { return region_->Yj(); }
+  float const* Zj() const { return region_->Zj(); }
+  unsigned short const* Ij() const { return region_->Ij(); }
+
+  int Npix()const {return region_->Npix(); }
+  float X() const { return region_->X(); }
+  float Y() const { return region_->Y(); }
+  float Z() const { return region_->Z(); }
+  unsigned short I() const {return region_->I();}
+  void reset() const {region_->reset();}
+  bool next() const {return region_->next();}
+  float Xo()const { return region_->Xo(); }
+  float Yo()const { return region_->Yo(); }
+  float Zo()const { return region_->Zo(); }
+  float Io()const { return region_->Io(); }
+  float Io_sd()const { return region_->Io_sd(); }
+  double X2()const { return region_->X2(); }
+  double Y2()const { return region_->Y2(); }
+  double XY()const { return region_->XY(); }
+  double I2()const { return region_->I2(); }
+  double XI()const { return region_->XI(); }
+  double YI()const { return region_->YI(); }
+  double Ix() const { return region_->Ix(); }
+  double Iy() const { return region_->Iy(); }
+  float Ir() const { return region_->Ir(); }
+  float Diameter() const { return region_->Diameter(); }
+  float AspectRatio() const { return region_->AspectRatio(); }
+#if 0
+  void PrincipalOrientation(vcl_vector<float>& axis) { region_->PrincipalOrientation(axis); }
+#endif
+  double Var() const { return region_->Var(); }
+
+  // MPP 5/9/2003
+  // Additional digital region pass-throughs
+  float get_min() const { return region_->get_min(); }
+  float get_max() const { return region_->get_max(); }
+
+  //Accessors
+  // The Face moment matrix
+  virtual vnl_matrix<double> MomentMatrix();
+
+  //Utility Methods
+
+  // MPP 5/9/2003
+  // Resurrected from #ifdef'd block below for intensity face attributes
+  double perimeter();
+
+// Nobody appears to call these methods
+#if 0
+  // The projection of the face onto a given orientation
+  virtual void extrema(vcl_vector<float>& orientation, float& min, float& max);
+
+  // Only TaggedTransform can handle the shared geometry.
+  virtual bool TaggedTransform(CoolTransform const& t);
+
+  // Computations on the adjacent Face(s)
+  Histogram_ref GetAdjacentRegionHistogram();
+  float GetAdjacentRegionMean();
+#endif
+};
+
+#endif // vtol_intensity_face_h_
