76bdf0b942e7f22a689a59a53ad47f81d6cb857f
Amitha Perera <amithaperera@users.sourceforge.net>
2003-08-07 13:39:49 -0400

    including the X.h traditionally defines X_sptr too.

--- contrib/gel/vdgl/vdgl_intensity_face.h
+++ contrib/gel/vdgl/vdgl_intensity_face.h
@@ -2,6 +2,7 @@
 #define vdgl_intensity_face_h_
 
 #include <vtol/vtol_intensity_face.h>
+#include <vtol/vtol_intensity_face_sptr.h>
 
 // vdgl_intensity_face has been renamed to vtol_intensity_face,
 // because that's where it properly belongs. Please include
@@ -11,5 +12,6 @@
 #include <vcl_deprecated_header.h>
 
 typedef vtol_intensity_face vdgl_intensity_face;
+typedef vtol_intensity_face_sptr vdgl_intensity_face_sptr;
 
 #endif // vdgl_intensity_face_h_
