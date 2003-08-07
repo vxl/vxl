aa71f56a561b1f803943f1efce4029468b87a0ff
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-08-07 13:05:21 -0400

    this is vdgl_intensity_face, not vtol_intensity_face

--- contrib/gel/vdgl/vdgl_intensity_face.h
+++ contrib/gel/vdgl/vdgl_intensity_face.h
@@ -1,15 +1,15 @@
-#ifndef vtol_intensity_face_h_
-#define vtol_intensity_face_h_
+#ifndef vdgl_intensity_face_h_
+#define vdgl_intensity_face_h_
 
 #include <vtol/vtol_intensity_face.h>
 
-// vtol_intensity_face has been renamed to vtol_intensity_face,
+// vdgl_intensity_face has been renamed to vtol_intensity_face,
 // because that's where it properly belongs. Please include
 // <vtol/vtol_intensity_face.h> and replace all instances of
-// vtol_intensity_face with vtol_intensity_face
+// vdgl_intensity_face with vtol_intensity_face
 
 #include <vcl_deprecated_header.h>
 
-typedef vtol_intensity_face vtol_intensity_face;
+typedef vtol_intensity_face vdgl_intensity_face;
 
-#endif // vtol_intensity_face_h_
+#endif // vdgl_intensity_face_h_
--- contrib/gel/vdgl/vdgl_intensity_face_sptr.h
+++ contrib/gel/vdgl/vdgl_intensity_face_sptr.h
@@ -1,15 +1,15 @@
-#ifndef vtol_intensity_face_sptr_h_
-#define vtol_intensity_face_sptr_h_
+#ifndef vdgl_intensity_face_sptr_h_
+#define vdgl_intensity_face_sptr_h_
 
 #include <vtol/vtol_intensity_face_sptr.h>
 
-// vtol_intensity_face has been renamed to vtol_intensity_face,
+// vdgl_intensity_face has been renamed to vtol_intensity_face,
 // because that's where it properly belongs. Please include
 // <vtol/vtol_intensity_face.h> and replace all instances of
-// vtol_intensity_face with vtol_intensity_face
+// vdgl_intensity_face with vtol_intensity_face
 
 #include <vcl_deprecated_header.h>
 
-typedef vtol_intensity_face_sptr vtol_intensity_face_sptr;
+typedef vtol_intensity_face_sptr vdgl_intensity_face_sptr;
 
-#endif // vtol_intensity_face_sptr_h_
+#endif // vdgl_intensity_face_sptr_h_
