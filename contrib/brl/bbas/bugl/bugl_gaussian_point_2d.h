065dfbb4bc35bb7a81851d7dca2f58f2585b786a
Kongbin Kang <kkang@users.sourceforge.net>
2003-08-06 23:10:09 -0400

    set_covariant_matrix() and set_point() are not inlined ==> removed 'inline' keyword

--- contrib/brl/bbas/bugl/bugl_gaussian_point_2d.h
+++ contrib/brl/bbas/bugl/bugl_gaussian_point_2d.h
@@ -13,16 +13,24 @@ class bugl_gaussian_point_2d:public bugl_uncertainty_point_2d<T>
  private:
   vnl_matrix_fixed<T, 2, 2> sigma_;
   vnl_matrix_fixed<T, 2, 2> sigma_inv_;
+  T det_;
  public:
   bugl_gaussian_point_2d(vgl_point_2d<T> &p, vnl_matrix_fixed<T, 2, 2> &s);
   bugl_gaussian_point_2d(T x, T y, vnl_matrix_fixed<T, 2, 2> & s);
   bugl_gaussian_point_2d();
   ~bugl_gaussian_point_2d() {}
 
+  //: get covariant matrix
   vnl_matrix_fixed<T, 2, 2> get_covariant_matrix() { return sigma_;}
-  inline void set_covariant_matrix(vnl_matrix_fixed<T, 2, 2>& s); 
-  inline void set_point(vgl_point_2d<T> & p); 
- 
+
+  //: set covariant matrix
+  void set_covariant_matrix(vnl_matrix_fixed<T, 2, 2>& s); 
+
+  //: set point value
+  void set_point(vgl_point_2d<T> & p); 
+  
+  //: probability density at point p
+  T prob_at(vgl_point_2d<T> &p);
 };
 
 #define BUGL_GAUSSIAN_POINT_2D_INSTANTIATE(T) extern "please include bugl/gaussian_point_2d.txx first"
