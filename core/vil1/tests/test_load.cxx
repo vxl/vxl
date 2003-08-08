afdd791d83dae6f9219ed6840e68a90a22a2f019
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-08-08 04:36:11 -0400

    vcl_ios_binary lives in <iostream>, not <ios>

--- core/vil1/tests/test_load.cxx
+++ core/vil1/tests/test_load.cxx
@@ -4,7 +4,7 @@
 // \author Peter Vanroose
 
 #include <vcl_fstream.h>
-#include <vcl_ios.h>
+#include <vcl_iostream.h>
 
 #include <vpl/vpl.h>
 #include <vul/vul_temp_filename.h>
@@ -17,10 +17,10 @@
 static void test(char const* magic, int comps, int bits, int maxval)
 {
   char const* ext = ".pgm";
-  if( comps == 3 )
+  if ( comps == 3 )
     ext = ".ppm";
   vcl_string tmp_nam = vul_temp_filename();
-  if( tmp_nam == "" )
+  if ( tmp_nam == "" )
     tmp_nam = "t";
   tmp_nam += ext;
   char const *file = tmp_nam.c_str();
@@ -30,10 +30,10 @@ static void test(char const* magic, int comps, int bits, int maxval)
       vpl_chmod(file, 0666); // -rw-rw-rw-
 #endif
     f << magic << "\n2\n3\n";
-    if (maxval > 1) f << maxval << "\n";
+    if (maxval > 1) f << maxval << '\n';
     if (magic[1] > '3')
       // 72 bytes: sufficient for 32bit/component ppm (although the
-      // ppm standard, at http://netpbm.sourceforge.net/doc//ppm.html,
+      // ppm standard, at http://netpbm.sourceforge.net/doc/ppm.html,
       // does not allow more than 16bits/component).
       f << "ABCDEFGHIJKLMNOPQRSTUVWX"
            "ABCDEFGHIJKLMNOPQRSTUVWX"
@@ -46,23 +46,22 @@ static void test(char const* magic, int comps, int bits, int maxval)
   vil1_image i = vil1_load(file);
 
   if (i) {
-    vcl_cout << "test vil1_load: size " << i.width() << "x" << i.height() << ", "
-             << i.components() << " component(s)" << ", "
-             << i.bits_per_component() << " bits (magic "
-             << magic << ")" << vcl_endl;
+    vcl_cout<< "test vil1_load: size " << i.width() << 'x' << i.height() << ", "
+            << i.components() << " component(s), "
+            << i.bits_per_component() << " bits (magic " << magic << ")\n";
 
     TEST("width", i.width(), 2);
     TEST("height", i.height(), 3);
     TEST("planes", i.planes(), 1);
     TEST("components", i.components(), comps);
     TEST("bits per component", i.bits_per_component(), bits);
-    TEST("get_property(\"memory\")", i.get_property("memory"), false); 
-    TEST("get_property(\"top row first\")", i.get_property("top row first"), true); 
-    TEST("get_property(\"left first\")", i.get_property("left first"), true); 
-    TEST("get_property(\"component order is B,G,R\")", i.get_property("component order is B,G,R"), false); 
+    TEST("get_property(\"memory\")", i.get_property("memory"), false);
+    TEST("get_property(\"top row first\")", i.get_property("top row first"), true);
+    TEST("get_property(\"left first\")", i.get_property("left first"), true);
+    TEST("get_property(\"component order is B,G,R\")", i.get_property("component order is B,G,R"), false);
     char buf[72];
-    TEST("get_plane(0)", i.get_plane(0), i); 
-    TEST("get_section()", i.get_section(buf, 0, 0, 2, 3) != 0, true); 
+    TEST("get_plane(0)", i.get_plane(0), i);
+    TEST("get_section()", i.get_section(buf, 0, 0, 2, 3) != 0, true);
     if (magic[1] > '3')
     {
       int j=0; for (; 8*j < 6*i.bits_per_component(); ++j) if (buf[j] != j+'A') break;
@@ -90,15 +89,13 @@ MAIN( test_load )
   test("P2", 1,  8, 255);
   test("P2", 1, 16, 65535);
 // PNM does not support components > 16 bits
-// (http://netpbm.sourceforge.net/doc//ppm.html)
-#if 0 
+// (http://netpbm.sourceforge.net/doc/ppm.html)
+#if 0
   test("P2", 1, 32, 16777216);
+  test("P3", 3, 32, 16777216);
 #endif
   test("P3", 3,  8, 255);
   test("P3", 3, 16, 65535);
-#if 0
-  test("P3", 3, 32, 16777216);
-#endif
   test("P4", 1,  1, 1);
   test("P5", 1,  8, 255);
   test("P5", 1, 16, 65535);
