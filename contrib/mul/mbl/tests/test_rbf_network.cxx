26f235e1f747ac9628c9ad9d50e8574bf6885c9b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-08-16 07:02:39 -0400

    unused variable nCentres commented out

--- contrib/mul/mbl/tests/test_rbf_network.cxx
+++ contrib/mul/mbl/tests/test_rbf_network.cxx
@@ -1,26 +1,25 @@
 // This is mul/mbl/tests/test_rbf_network.cxx
 #include <vcl_iostream.h>
-#include <vcl_cmath.h> // for vcl_sqrt()
+#include <vcl_cmath.h> // for fabs()
 #include <vcl_cassert.h>
 #include <vcl_vector.h>
 
 #include <mbl/mbl_mz_random.h>
 #include <mbl/mbl_rbf_network.h>
-#include <mbl/mbl_data_array_wrapper.h>
-#include <vnl/vnl_math.h>
 #include <vnl/vnl_vector.h>
+
 #include <testlib/testlib_test.h>
 
 void test_rbf_network()
 {
   vcl_cout << "\n*************************\n"
-           <<   " Testing mbl_rbf_network \n"
+           <<   " Testing mbl_rbf_network\n"
            <<   "*************************\n";
 
   mbl_mz_random rng;
   rng.reseed(123456);
 
-  const unsigned nCentres = 64;
+//const unsigned nCentres = 64;
   const unsigned nSamples = 100;
   const unsigned nDims = 3;
   const unsigned nTests = 3;
@@ -30,32 +29,29 @@ void test_rbf_network()
     data(nSamples, vnl_vector<double>(nDims, 0.0));
 
   // sample data uniformly from unit cube
-  unsigned i, j;
-  for (i=0; i<nSamples; ++i)
-    for (j=0; j<nDims; ++j) {
+  for (unsigned int i=0; i<nSamples; ++i)
+    for (unsigned int j=0; j<nDims; ++j) {
       data[i](j) = rng.drand32(0.0, 1.0);
       assert ( data[i](j) >= 0 && data[i](j) < 1);
     }
 
-mbl_rbf_network rbf;
-rbf.build(data);
-vnl_vector<double > w;
+  mbl_rbf_network rbf;
+  rbf.build(data);
+  vnl_vector<double > w;
 
-for (i=0; i<nTests ; ++i)
-{
-  unsigned k=0;
-  vcl_cout<<"We pick the "<<i<<" sample from training."<<vcl_endl;
-  vcl_cout<<"The input data "<<data[i]<<vcl_endl;
-  rbf.calcWts(w,data[i]);//"It fails here"
-  vcl_cout<<w[i]<<vcl_endl;
-  vcl_cout<<"Sum of weights = "<<w.sum()<<vcl_endl;
-  TEST_NEAR("Sum of weights near unity",w.sum(),1.0,1e-4);
-  TEST_NEAR("Target weight near unity",w[i],1.0,1e-4);
-  TEST_NEAR("Next weight near zero",w[i+1],0.0,1e-4);
-  vcl_cout<<"First 10 weights: ";
-  for (unsigned j=0;j<10;++j) vcl_cout<<w[j]<<" ";
-  vcl_cout<<vcl_endl;
+  for (unsigned int i=0; i<nTests ; ++i)
+  {
+    vcl_cout<<"\nWe pick sample "<<i+1<<" from training.\n"
+            <<"The input data: "<<data[i]<<vcl_endl;
+    rbf.calcWts(w,data[i]);//"It fails here"
+    TEST_NEAR("Target weight near unity",w[i],1.0,1e-4);
+    TEST_NEAR("Sum of weights near unity",w.sum(),1.0,1e-4);
+    double wts=-w[i]; for (unsigned int j=0; j<nSamples; ++j) wts += vcl_fabs(w[j]);
+    TEST_NEAR("All other weights near zero",wts,0.0,1e-4);
+    vcl_cout<<"First 10 weights:";
+    for (unsigned j=0;j<10;++j) vcl_cout<<' '<<w[j];
+    vcl_cout<<vcl_endl;
+  }
 }
 
-}
 TESTLIB_DEFINE_MAIN(test_rbf_network);
