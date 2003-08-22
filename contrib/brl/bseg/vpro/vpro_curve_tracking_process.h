f43f98c183d0a8aca54168f4f136eed050bb526e
Vishal Jain <jainv@users.sourceforge.net>
2003-08-22 16:37:06 -0400

    added new tracker

--- contrib/brl/bseg/vpro/vpro_curve_tracking_process.h
+++ contrib/brl/bseg/vpro/vpro_curve_tracking_process.h
@@ -11,26 +11,30 @@
 // \verbatim
 //  Modifications:
 //   P.L. Bazin  March 2003  Initial version.
+//   Vishal Jain August 2003 added write_to_file function
 // \endverbatim
 //--------------------------------------------------------------------------------
-#include <bdgl/bdgl_curve_tracker.h>
+#include <bdgl/bdgl_curve_tracking.h>
 #include <vpro/vpro_video_process.h>
 #include <sdet/sdet_detector_params.h>
 
-class vpro_curve_tracking_process : public vpro_video_process, public bdgl_curve_tracker
+class vpro_curve_tracking_process : public vpro_video_process ,public bdgl_curve_tracking 
 {
  public:
-  vpro_curve_tracking_process(const bdgl_curve_tracker_params & tp, const sdet_detector_params & dp);
+  vpro_curve_tracking_process(bdgl_curve_tracking_params & tp, const sdet_detector_params & dp);
   ~vpro_curve_tracking_process();
-
+ 
   virtual process_data_type get_output_type(){return SPATIAL_OBJECT;}
+  // for geting the tracks
+  bool write_to_file();
   //: compute edges on the input image
   virtual bool execute();
   virtual bool finish(){return true;}
-
+  
  private:
   //members
   sdet_detector_params detect_params_;
+  bdgl_curve_tracking_params params_;
 };
 
 #endif // vpro_curve_tracking_process_h_
