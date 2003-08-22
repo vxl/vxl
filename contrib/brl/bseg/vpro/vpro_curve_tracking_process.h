782edf0053d3d23b75677d85f9a23a3ba1cf6b7c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-08-22 19:07:50 -0400

    *** empty log message ***

--- contrib/brl/bseg/vpro/vpro_curve_tracking_process.h
+++ contrib/brl/bseg/vpro/vpro_curve_tracking_process.h
@@ -1,4 +1,4 @@
-// This is brl/vpro/vpro_curve_tracking_process.h
+// This is brl/bseg/vpro/vpro_curve_tracking_process.h
 #ifndef vpro_curve_tracking_process_h_
 #define vpro_curve_tracking_process_h_
 //--------------------------------------------------------------------------------
@@ -18,19 +18,19 @@
 #include <vpro/vpro_video_process.h>
 #include <sdet/sdet_detector_params.h>
 
-class vpro_curve_tracking_process : public vpro_video_process ,public bdgl_curve_tracking 
+class vpro_curve_tracking_process : public vpro_video_process ,public bdgl_curve_tracking
 {
  public:
   vpro_curve_tracking_process(bdgl_curve_tracking_params & tp, const sdet_detector_params & dp);
   ~vpro_curve_tracking_process();
- 
+
   virtual process_data_type get_output_type(){return SPATIAL_OBJECT;}
   // for geting the tracks
   bool write_to_file();
   //: compute edges on the input image
   virtual bool execute();
   virtual bool finish(){return true;}
-  
+
  private:
   //members
   sdet_detector_params detect_params_;
