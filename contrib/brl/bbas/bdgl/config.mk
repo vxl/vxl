2102492424cd3a15b169d71e794288aae2584ad4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-08-22 17:43:23 -0400

    updated w.r.t. CMakeLists.txt

--- contrib/brl/bbas/bdgl/config.mk
+++ contrib/brl/bbas/bdgl/config.mk
@@ -1,4 +1,6 @@
 IULIBS += -lbdgl
 include ${IUEROOT}/contrib/gel/vdgl/config.mk
-include ${IUEROOT}/core/vnl/config.mk
+include ${IUEROOT}/contrib/gel/vtol/config.mk
+include ${IUEROOT}/core/vnl/algo/config.mk
+include ${IUEROOT}/core/vgl/config.mk
 include ${IUEROOT}/core/vbl/config.mk
