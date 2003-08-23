d115f2e6a4f8b9b1b1cfc134fcc6dcb157ab3fc1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-08-23 15:26:07 -0400

    new method vil_new_image_view_i_j_plane() added, and method docs brought in correspondence with that in vil_new.cxx

--- core/vil/vil_new.h
+++ core/vil/vil_new.h
@@ -16,21 +16,23 @@
 // \author awf@robots.ox.ac.uk
 // \date 26 Feb 00
 //
-//\verbatim
+// \verbatim
 //  Modifications
-//     000216 AWF Initial version.
-//     25 Sep 2002 Ian Scott - convert to vil.
-//\endverbatim
+//   000216 AWF Initial version.
+//   25 Sep 2002 Ian Scott - convert to vil.
+// \endverbatim
 
 #include <vil/vil_fwd.h>
 #include <vil/vil_image_resource.h>
 #include <vil/vil_image_view.h>
+#include <vcl_deprecated.h>
 
 //: Make a new image of given format.
 // If the format is not scalar, the number of planes must be 1. When you create
 // a multi-component image in this way, the vil_image_resource API will treat
 // it as a scalar pixel image with multiple planes. (This doesn't affect the
 // underlying data storage.)
+// \relates vil_image_resource
 vil_image_resource_sptr vil_new_image_resource(unsigned ni, unsigned nj, unsigned nplanes,
                                                vil_pixel_format format);
 
@@ -58,7 +60,7 @@ vil_image_resource_sptr vil_new_image_resource(vil_stream* os,
                                                vil_pixel_format format,
                                                char const* file_format = 0);
 
-//: Make a new image on disk
+//: Make a new vil_image_resource, writing to file "filename", size ni x nj, copying pixel format etc from "prototype".
 // \relates vil_image_resource
 vil_image_resource_sptr vil_new_image_resource(char const* filename,
                                                unsigned ni, unsigned nj,
@@ -66,7 +68,7 @@ vil_image_resource_sptr vil_new_image_resource(char const* filename,
                                                vil_image_resource_sptr const &prototype,
                                                char const* format = 0);
 
-//: Make a new image.
+//: Make a new vil_image_resource, writing to stream "os", size ni x nj, copying pixel format etc from "prototype".
 // \relates vil_image_resource
 vil_image_resource_sptr vil_new_image_resource(vil_stream* os,
                                                unsigned ni, unsigned nj,
@@ -74,25 +76,39 @@ vil_image_resource_sptr vil_new_image_resource(vil_stream* os,
                                                vil_image_resource_sptr const& prototype,
                                                char const* file_format = 0);
 
-//: Create an image view whose plane step is 1.
-// i_step will by nplanes.
+//: Create a new image view whose plane step is 1.
+//  Pixel data type is the type of the last (dummy) argument.
+//  i_step will be nplanes, j_step will be nplanes x ni.
 // \deprecated in favour of vil_image_view constructor
 template <class T>
 vil_image_view<T> vil_new_image_view_j_i_plane(unsigned ni, unsigned nj, unsigned nplanes, T /*dummy*/)
 {
+  VXL_DEPRECATED("vil_new_image_view_j_i_plane");
   vil_memory_chunk_sptr chunk = new vil_memory_chunk(ni*nj*nplanes*sizeof(T),
                                                      vil_pixel_format_component_format(vil_pixel_format_of(T())));
-  return vil_image_view<T>(chunk, reinterpret_cast<T *>(chunk->data()), ni, nj, nplanes, nplanes*nj, nplanes, 1);
+  return vil_image_view<T>(chunk, reinterpret_cast<T*>(chunk->data()), ni, nj, nplanes, nplanes, nplanes*ni, 1);
 }
 
-//: Create an image view whose j step is 1.
-// i_step will be nj.
+//: Create a new image view whose j_step is 1.
+//  Pixel data type is the type of the last (dummy) argument.
+//  i_step will be nj, planestep will be ni x nj.
 template <class T>
 vil_image_view<T> vil_new_image_view_plane_i_j(unsigned ni, unsigned nj, unsigned nplanes, T /*dummy*/)
 {
   vil_memory_chunk_sptr chunk = new vil_memory_chunk(ni*nj*nplanes*sizeof(T),
                                                      vil_pixel_format_component_format(vil_pixel_format_of(T())));
-  return vil_image_view<T>(chunk, reinterpret_cast<T *>(chunk->data()), ni, nj, nplanes, nj, 1, nj*ni);
+  return vil_image_view<T>(chunk, reinterpret_cast<T*>(chunk->data()), ni, nj, nplanes, nj, 1, nj*ni);
+}
+
+//: Create a new image view whose plane step is 1 and whose j_step is nplanes.
+//  Pixel data type is the type of the last (dummy) argument.
+//  i_step will be nplanes x nj.
+template <class T>
+vil_image_view<T> vil_new_image_view_i_j_plane(unsigned ni, unsigned nj, unsigned nplanes, T /*dummy*/)
+{
+  vil_memory_chunk_sptr chunk = new vil_memory_chunk(ni*nj*nplanes*sizeof(T),
+                                                     vil_pixel_format_component_format(vil_pixel_format_of(T())));
+  return vil_image_view<T>(chunk, reinterpret_cast<T*>(chunk->data()), ni, nj, nplanes, nplanes*nj, nplanes, 1);
 }
 
 #endif // vil_new_h_
