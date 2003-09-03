c8e50156e1369f2a220aabdc46fd22a519b49e1a
Daniel Crispell <dcrispell@users.sourceforge.net>
2003-09-03 11:36:47 -0400

    added call to sdet_grid_finder::check_grid_match()

--- contrib/brl/bseg/vpro/vpro_grid_finder_process.cxx
+++ contrib/brl/bseg/vpro/vpro_grid_finder_process.cxx
@@ -2,6 +2,7 @@
 #include <vpro/vpro_grid_finder_process.h>
 //:
 // \file
+#include <vcl_fstream.h>
 #include <vcl_iostream.h>
 #include <vil1/vil1_image.h>
 #include <sdet/sdet_fit_lines.h>
@@ -9,6 +10,7 @@
 #include <vpro/vpro_edge_process.h>
 #include <vpro/vpro_line_fit_process.h>
 #include <vsol/vsol_curve_2d.h>
+
 
 vpro_grid_finder_process::vpro_grid_finder_process(sdet_detector_params& dp,
                                                    sdet_fit_lines_params& flp,
@@ -61,6 +63,7 @@ bool vpro_grid_finder_process::execute()
     }
   sdet_grid_finder gf(*((sdet_grid_finder_params*)this));
   gf.unset_verbose();
+
   if (!gf.set_lines(img.width(), img.height(), lines))
     {
       this->clear_input();
@@ -68,14 +71,15 @@ bool vpro_grid_finder_process::execute()
     }
   //Get the backprojected grid
   vcl_vector<vsol_line_2d_sptr> mapped_lines;
-  if (!gf.compute_homography())
+  gf.compute_homography();
+  if (!gf.check_grid_match(img))
     {
-      vcl_cout << "Homograpy Failed\n";
+      vcl_cout << "check_grid_match() failed - disregarding homography\n";
       this->clear_input();
       return false;
     }
   if (!gf.get_backprojected_grid(mapped_lines))
-//if (!gf.get_mapped_lines(mapped_lines))
+    //if (!gf.get_mapped_lines(mapped_lines))
     {
       this->clear_input();
       return false;
@@ -86,6 +90,27 @@ bool vpro_grid_finder_process::execute()
           vsol_line_2d* l2d = (*lit).ptr();
           output_spat_objs_.push_back((vsol_spatial_object_2d*)l2d);
         }
+  // TEMP - Write grid points to file
+  if (true)
+    {
+      char filename[] = "./grid_points.txt";
+      static int view_count = 0;
+      static vcl_ofstream outstream(filename);
+ 
+      if (view_count == 0)
+	gf.init_output_file(outstream);
+      
+      vcl_string answer("y");
+      //vcl_cout << "write image points to file? (y or n) ";
+      //vcl_cin >> answer;
+      if (answer == "y")
+	{
+	  gf.write_image_points(outstream);
+	  ++view_count;
+	}
+      vcl_cout << "total of "<<view_count<<" views written.\n";
+      
+    }
   this->clear_input();
   return true;
 }
