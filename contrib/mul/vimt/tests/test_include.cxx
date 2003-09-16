7ed82a7eacfd1266a187ba5ed4d85d04f39b198e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-09-16 09:20:50 -0400

    newly added header file

--- contrib/mul/vimt/tests/test_include.cxx
+++ contrib/mul/vimt/tests/test_include.cxx
@@ -5,6 +5,7 @@
 #include <vimt/vimt_add_all_binary_loaders.h>
 #include <vimt/vimt_bilin_interp.h>
 #include <vimt/vimt_crop.h>
+#include <vimt/vimt_dog_pyramid_builder_2d.h>
 #include <vimt/vimt_gaussian_pyramid_builder_2d_general.h>
 #include <vimt/vimt_gaussian_pyramid_builder_2d.h>
 #include <vimt/vimt_image_2d.h>
