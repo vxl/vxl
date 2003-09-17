0897d25dc3bd045884822bd59c05a6a8690ff606
Fred Wheeler <fred_wheeler@users.sourceforge.net>
2003-09-17 11:13:04 -0400

    find FindMPEG2.cmake from VXL_CMAKE_DIR, not MODULE_PATH, which is not known by other projects that use VXL

--- config/cmake/Modules/FindMPEG2.cmake
+++ config/cmake/Modules/FindMPEG2.cmake
@@ -16,7 +16,7 @@
 # If this FORCE variable is unset or is FALSE, try to find a native library.
 IF( VXL_FORCE_V3P_MPEG2 )
 ELSE( VXL_FORCE_V3P_MPEG2 )
-  INCLUDE( ${MODULE_PATH}/NewCMake/FindMPEG2.cmake )
+  INCLUDE( ${VXL_CMAKE_DIR}/Modules/NewCMake/FindMPEG2.cmake )
 ENDIF( VXL_FORCE_V3P_MPEG2 )
 
 IF( MPEG2_FOUND )
