9b5dc1cc5709c89feca7b7ab1d2d1b8dc2c7c8be
Tim Cootes <timcootes@users.sourceforge.net>
2003-09-18 05:04:26 -0400

    Extended tests

--- contrib/mul/vimt/tests/test_dog_pyramid_builder_2d.cxx
+++ contrib/mul/vimt/tests/test_dog_pyramid_builder_2d.cxx
@@ -6,6 +6,7 @@
 #include <vimt/vimt_dog_pyramid_builder_2d.h>
 #include <vimt/vimt_image_pyramid.h>
 #include <vsl/vsl_binary_loader.h>
+#include <vimt/algo/vimt_find_peaks.h>
 
 #ifndef LEAVE_FILES_BEHIND
 #define LEAVE_FILES_BEHIND 0
@@ -13,30 +14,45 @@
 
 void test_dog_pyramid_builder_2d_build(vimt_dog_pyramid_builder_2d<float>& builder)
 {
-  unsigned ni = 20, nj = 20;
+  unsigned ni = 60, nj = 60;
   vcl_cout<<"Image Size: "<<ni<<" x "<<nj<<'\n';
 
   vimt_image_2d_of<float> image0;
   image0.image().set_size(ni,nj);
-
-  for (unsigned y=0;y<image0.image().nj();++y)
-    for (unsigned x=0;x<image0.image().ni();++x)
-      image0.image()(x,y) = x+y*10;
+  image0.image().fill(0.0f);
+  image0.image()(ni/2,nj/2) = 100.0f;
 
   int default_n_levels = builder.max_levels();
   TEST("Default max. number of levels is 99", default_n_levels, 99);
-  builder.set_max_levels(3);
-  vimt_image_pyramid image_pyr;
+  builder.set_max_levels(6);
+  vimt_image_pyramid dog_pyr,smooth_pyr;
 
-  builder.build(image_pyr,image0);
+  builder.build_dog(dog_pyr,smooth_pyr,image0,true);
 
-  vcl_cout<<"Result:\n";
-  image_pyr.print_all(vcl_cout);
-
-  TEST("Found correct number of levels", image_pyr.n_levels(), 3);
-  const vimt_image_2d_of<float>& v_image0 = static_cast<const vimt_image_2d_of<float>&>(image_pyr(0));
+  TEST("Found correct number of levels", dog_pyr.n_levels(), 6);
+  const vimt_image_2d_of<float>& v_image0 =
+                 static_cast<const vimt_image_2d_of<float>&>(dog_pyr(0));
   TEST("Base width",v_image0.image().ni(),ni);
   TEST("Base height",v_image0.image().nj(),nj);
+
+  for (unsigned L=0;L<smooth_pyr.n_levels();++L)
+  {
+    const vimt_image_2d_of<float>& imageL =
+             static_cast<const vimt_image_2d_of<float>&>(smooth_pyr(L));
+
+    vgl_point_2d<double> p = vimt_find_max(imageL);
+
+    vcl_cout<<"Level: "<<L<<" Peak point is at: "<<p<<vcl_endl;
+
+    const vimt_image_2d_of<float>& dogL =
+             static_cast<const vimt_image_2d_of<float>&>(dog_pyr(L));
+
+    p = vimt_find_max(imageL);
+
+    vcl_cout<<"Level: "<<L<<" Peak DoG response is at: "<<p<<vcl_endl;
+
+    TEST_NEAR("Peak DoG Response",(p-vgl_point_2d<double>(ni/2,nj/2)).length(),0,1.5);
+  }
 }
 
 void test_dog_pyramid_builder_2d_a()
