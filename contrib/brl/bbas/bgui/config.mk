6e66c3d7433d00f6b79db4a66ab2330412a66528
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-09-21 15:20:37 -0400

    updated w.r.t. CMakeLists.txt

--- contrib/brl/bbas/bgui/config.mk
+++ contrib/brl/bbas/bgui/config.mk
@@ -6,3 +6,4 @@ include ${IUEROOT}/core/vgui/config.mk
 include ${IUEROOT}/core/vgl/algo/config.mk
 include ${IUEROOT}/core/vbl/config.mk
 include ${IUEROOT}/core/vul/config.mk
+include ${IUEROOT}/core/vil/algo/config.mk
