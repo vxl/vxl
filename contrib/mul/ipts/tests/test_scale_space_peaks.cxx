372b22050185497ea3ea0ff00e53999714cfa66a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-09-21 08:32:04 -0400

    test should not be performed on an uninitialised image

--- contrib/mul/ipts/tests/test_scale_space_peaks.cxx
+++ contrib/mul/ipts/tests/test_scale_space_peaks.cxx
@@ -1,13 +1,15 @@
-// This is mul/ipts/tests/test_scale_space_peaks.h
+// This is mul/ipts/tests/test_scale_space_peaks.cxx
 
 #include <testlib/testlib_test.h>
-
 #include <ipts/ipts_scale_space_peaks.h>
 
 void test_scale_space_peaks_float()
 {
   // Just check it compiles cleanly first
   vimt_image_2d_of<float> image;
+  image.image().set_size(10,10);
+  image.image().fill(10);
+  image.image()(3,7)=18;  // One peak
   vcl_vector<vgl_point_3d<double> > peak_pts;
   ipts_scale_space_peaks_2d(peak_pts,image,image,image,float(0),false);
 }
