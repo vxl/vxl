c91839053e36f599ee772c5f4065aa67f5617b33
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-09-24 04:03:48 -0400

    added new header file

--- contrib/mul/ipts/tests/test_include.cxx
+++ contrib/mul/ipts/tests/test_include.cxx
@@ -2,6 +2,7 @@
 #include <ipts/ipts_draw.h>
 #include <ipts/ipts_entropy_pyramid.h>
 #include <ipts/ipts_local_entropy.h>
+#include <ipts/ipts_orientation_pyramid.h>
 #include <ipts/ipts_scale_space_peaks.h>
 
 int main() { return 0; }
