4440480a67563ab6d113b41b694b7d51935f5461
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-09-26 04:54:48 -0400

    default constructor call should not have brackets

--- contrib/brl/bbas/bugl/tests/test_bugl.cxx
+++ contrib/brl/bbas/bugl/tests/test_bugl.cxx
@@ -10,8 +10,8 @@ int main()
   s2[0][0] = 1; s2[1][1] = 3;
   bugl_gaussian_point_2d<double> p2d(1, 1, s2);
 
-  
-  bugl_gaussian_point_3d<double> p1();
+
+  bugl_gaussian_point_3d<double> p1;
   vnl_double_3x3 s3(0.0);
   s3[0][0] = 1; s3[1][1] = 3; s3[2][2] = 5;
   bugl_gaussian_point_3d<double> p3d(100, 100, 100, s3);
