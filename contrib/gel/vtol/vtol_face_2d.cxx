8c83cacc5a46e0659ca022e0ee858587ff741e56
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-01 14:31:02 -0400

    removed commented out function

--- contrib/gel/vtol/vtol_face_2d.cxx
+++ contrib/gel/vtol/vtol_face_2d.cxx
@@ -70,63 +70,6 @@ vtol_face_2d::vtol_face_2d(vtol_face_2d_sptr const& other)
     set_surface(other->surface_->clone()->cast_to_region());
 }
 
-#if 0 // deprecated
-//---------------------------------------------------------------------------
-//: Copy constructor.  Deep copy.  Deprecated.
-//---------------------------------------------------------------------------
-// This is the Copy Constructor for vtol_face_2d. It performs a deep copy of
-// all vtol_face_2d inferior one_chains.
-vtol_face_2d::vtol_face_2d(const vtol_face_2d &other)
-  : surface_(0)
-{
-  vcl_cerr << "vtol_face_2d copy constructor is deprecated; use vtol_face_2d_sptr constructor instead\n";
-  vtol_face_2d *oldf=const_cast<vtol_face_2d*>(&other); // const violation
-  edge_list *edgs=oldf->edges();
-  vertex_list *verts=oldf->vertices();
-
-  topology_list newedges(edgs->size());
-  topology_list newverts(verts->size());
-
-  int i=0;
-  for (vertex_list::iterator vi=verts->begin();vi!=verts->end();++vi,++i)
-    {
-      vtol_vertex_sptr v=(*vi);
-      newverts[i]=v->clone()->cast_to_topology_object();
-      v->set_id(i);
-    }
-  int j=0;
-  for (edge_list::iterator ei=edgs->begin();ei!= edgs->end();++ei,++j)
-    {
-      vtol_edge_sptr e=(*ei);
-
-      vtol_topology_object_sptr V1 = newverts[e->v1()->get_id()];
-      vtol_topology_object_sptr V2 = newverts[e->v2()->get_id()];
-      if (!V1 || !V2)
-        {
-          vcl_cerr << "Inconsistent topology in vtol_face_2d copy constructor\n";
-          link_inferior(new vtol_one_chain);
-          return;
-         }
-      // make the topology and geometry match
-      vtol_edge_sptr newedge=new vtol_edge_2d(V1->cast_to_vertex(),
-                                              V2->cast_to_vertex());
-
-      newedges[j]=newedge->cast_to_topology_object();
-      e->set_id(j);
-    }
-  // This is a deep copy of the vtol_face_2d.
-
-  topology_list::const_iterator ii;
-  for (ii=oldf->inferiors()->begin();ii!= oldf->inferiors()->end();++ii)
-    link_inferior((*ii)->cast_to_one_chain()->copy_with_arrays(newverts,newedges));
-  delete edgs;
-  delete verts;
-  set_surface(0);
-  if (oldf->surface_)
-    set_surface(oldf->surface_->clone()->cast_to_region());
-}
-#endif
-
 //---------------------------------------------------------------------------
 // Destructor
 //---------------------------------------------------------------------------
@@ -160,11 +103,11 @@ vtol_face_2d::copy_with_arrays(topology_list &verts,
     newface->unlink_inferior((*i)->cast_to_one_chain());
   newface->inferiors()->clear();
   for (i=inferiors()->begin();i!=inferiors()->end();++i)
-    {
-      vtol_one_chain *onech=(*i)->cast_to_one_chain()->copy_with_arrays(verts,edges);
-      assert(*onech == *(*i));
-      newface->link_inferior(onech);
-    }
+  {
+    vtol_one_chain *onech=(*i)->cast_to_one_chain()->copy_with_arrays(verts,edges);
+    assert(*onech == *(*i));
+    newface->link_inferior(onech);
+  }
   if (surface_)
     newface->set_surface(surface_->clone()->cast_to_region());
   return newface;
@@ -211,28 +154,28 @@ vtol_face_2d::vtol_face_2d(vertex_list const& verts)
   vcl_vector<signed char> directions;
 
   while (!done)
+  {
+    // if no next vertex, then use the first vertex by calling
+    // verts->end() again to wrap around  This will close the loop
+    ++vi;
+
+    if (vi==verts.end())
     {
-      // if no next vertex, then use the first vertex by calling
-      // verts->end() again to wrap around  This will close the loop
-      ++vi;
-
-      if (vi==verts.end())
-        {
-          vi=verts.begin();
-          done=true;
-        }
-
-      vtol_vertex_sptr v02=(*vi); // get the next vertex (may be first)
-
-      vtol_edge_sptr newedge=v01->new_edge(v02);
-      elist.push_back(newedge);
-
-      if (*v02 == *(newedge->v2()))
-        directions.push_back((signed char)1);
-      else
-        directions.push_back((signed char)(-1));
-      v01=v02; // in the next go around v1 is v2 of the last
+      vi=verts.begin();
+      done=true;
     }
+
+    vtol_vertex_sptr v02=(*vi); // get the next vertex (may be first)
+
+    vtol_edge_sptr newedge=v01->new_edge(v02);
+    elist.push_back(newedge);
+
+    if (*v02 == *(newedge->v2()))
+      directions.push_back((signed char)1);
+    else
+      directions.push_back((signed char)(-1));
+    v01=v02; // in the next go around v1 is v2 of the last
+  }
 
   link_inferior(new vtol_one_chain(elist,directions,true));
 }
@@ -323,8 +266,8 @@ bool vtol_face_2d::operator==(const vtol_face_2d &other) const
   if (numinf()!=other.numinf())
     return false;
 
-  if ( (surface_ && ! other.surface_)
-     ||(other.surface_ && ! surface_))
+  if ( (surface_ && ! other.surface_) ||
+       (other.surface_ && ! surface_))
     return false;
 
   if (surface_ && *surface_!=*(other.surface_))
@@ -345,7 +288,8 @@ bool vtol_face_2d::operator==(const vtol_face &other) const
 {
   if (! other.cast_to_face_2d() )
     return false;
-  return *this == (vtol_face_2d const&) other;
+  else
+    return *this == (vtol_face_2d const&) other;
 }
 
 //: spatial object equality
@@ -370,12 +314,12 @@ void vtol_face_2d::describe(vcl_ostream &strm,
   for (int j=0; j<blanking; ++j) strm << ' ';
   print(strm);
   for (unsigned int i=0;i<inferiors()->size();++i)
-    {
-      if ((inferiors_[i])->cast_to_one_chain()!=0)
-        (inferiors_[i])->cast_to_one_chain()->describe(strm,blanking);
-      else
-        strm << "*** Odd inferior for a face\n";
-    }
+  {
+    if ((inferiors_[i])->cast_to_one_chain()!=0)
+      inferiors_[i]->cast_to_one_chain()->describe(strm,blanking);
+    else
+      strm << "*** Odd inferior for a face\n";
+  }
 }
 
 //:
