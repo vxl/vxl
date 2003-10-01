7590804321d4b4e09e45c8ee2fc8aeab3cf36fba
Ian Scott <scottim@users.sourceforge.net>
2003-10-01 09:50:07 -0400

    better doc and references

--- core/vil/algo/vil_cartesian_differential_invariants.h
+++ core/vil/algo/vil_cartesian_differential_invariants.h
@@ -11,8 +11,11 @@
 // Finds the first 8 Cartesian differential invariants of an image. That is
 // 1x1st order, 3x2nd order and 4x3rd order.
 // The results are returned in 8 adjacent planes (for each input plane)
+// The results are unscaled. If the range of your input is $r$,
+// you can normalise the results by dividing the planes by
+// $r, r^{-1}, r^{-1}, r^{-1}, r^{-2}, r^{-2}, r^{-2}, r^{-2}$
 //
-// See Florak et al. Image and Vision Computing, 10(6) pp376-388, 1992. and
+// See Romeny et al. Proc.IPMI1993, pp77-93. and
 // Walker et al. Proc.BMVC1997 pp541-549.
 template <class S, class T>
 void vil_cartesian_differential_invariants_3(const vil_image_view<S>& src,
