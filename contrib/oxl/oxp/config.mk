f0ba4eb2d71287ba93db9ff79785131cc6ae9cb2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-05 04:21:31 -0400

    updated w.r.t. CMakeLists.txt

--- contrib/oxl/oxp/config.mk
+++ contrib/oxl/oxp/config.mk
@@ -1,6 +1,6 @@
 TRY_MPEG2 := 1
 IULIBS += -loxp
-include ${IUEROOT}/core/vidl/config.mk
+include ${IUEROOT}/core/vidl_vil1/config.mk
 include ${IUEROOT}/core/vnl/config.mk
 include ${IUEROOT}/core/vil1/config.mk
 include ${IUEROOT}/core/vul/config.mk
