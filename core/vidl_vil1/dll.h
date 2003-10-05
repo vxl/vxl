3c64f369d102c258b30da7bf54fb9025eb70375e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-05 04:38:56 -0400

    this file should not be identical to core/vidl/dll.h

--- core/vidl_vil1/dll.h
+++ core/vidl_vil1/dll.h
@@ -1,19 +1,19 @@
-// This is core/vidl/dll.h
-#ifndef vidl_dll_h_
-#define vidl_dll_h_
+// This is core/vidl_vil1/dll.h
+#ifndef vidl_vil1_dll_h_
+#define vidl_vil1_dll_h_
 
 #include <vcl_compiler.h>
 
-#define VIDL_DLL_DATA
+#define VIDL_VIL1_DLL_DATA
 
 #if defined(VCL_WIN32) && !defined(BUILDING_VIDL_DLL)
 
 // if win32 and not buiding the DLL then you need a dllimport
 // Only if you are building a DLL linked application.
 # ifdef BUILD_DLL
-#  undef VIDL_DLL_DATA
-#  define VIDL_DLL_DATA _declspec(dllimport)
+#  undef VIDL_VIL1_DLL_DATA
+#  define VIDL_VIL1_DLL_DATA _declspec(dllimport)
 # endif // BUILD_DLL
 #endif // VCL_WIN32 and !Building_*_dll
 
-#endif // vidl_dll_h_
+#endif // vidl_vil1_dll_h_
