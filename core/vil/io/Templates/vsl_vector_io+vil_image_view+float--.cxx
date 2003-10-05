808a5e4b6fd03953a460b7b8a33d007344ee7188
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-05 12:36:22 -0400

    removed unused #include

--- core/vil/io/Templates/vsl_vector_io+vil_image_view+float--.cxx
+++ core/vil/io/Templates/vsl_vector_io+vil_image_view+float--.cxx
@@ -1,4 +1,4 @@
-#include <vsl/vsl_binary_loader.h>
+//#include <vsl/vsl_binary_loader.h>
 #include <vsl/vsl_vector_io.txx>
 #include <vil/io/vil_io_image_view.h> 
 VSL_VECTOR_IO_INSTANTIATE( vil_image_view<float> );
