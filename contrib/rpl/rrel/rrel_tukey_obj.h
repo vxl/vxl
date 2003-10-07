79fb2a49129d6f4f90c5ec71c96411c334f09863
Jim Miller <millerjv@users.sourceforge.net>
2003-10-07 13:58:50 -0400

    FIX: added missing overrides of overloaded functions

--- contrib/rpl/rrel/rrel_tukey_obj.h
+++ contrib/rpl/rrel/rrel_tukey_obj.h
@@ -43,9 +43,49 @@ class rrel_tukey_obj : public rrel_m_est_obj
   //: The robust loss function for the M-estimator.
   virtual double rho( double u ) const;
 
+  //: The robust loss function for the M-estimator. Overriding the
+  //  overloaded version rho(u) hides the superclass' implementation of
+  //  this version of rho(). This implementation simply calls the
+  //  superclass' version of the same routine.
+  //  \a r is the residual and
+  //  \a s is the scale for that residual.
+  inline  double rho( double r, double s ) const
+    { return rrel_m_est_obj::rho(r, s); }
+
   //: The weight of the residual.
   virtual double wgt( double u ) const;
 
+  //: Evaluate the objective function on heteroscedastic
+  //  residuals.  Overriding the overloaded version wgt(u) hides the
+  //  superclass' implementation of this version of wgt(). This
+  //  implementation simply calls the superclass' version of the same
+  //  routine. 
+  //  \sa rrel_wls_obj::wgt()
+  virtual void wgt( vect_const_iter res_begin, vect_const_iter res_end,
+                    vect_const_iter scale_begin,
+                    vect_iter wgt_begin ) const
+    { rrel_m_est_obj::wgt(res_begin, res_end, scale_begin, wgt_begin); }
+
+  //: Computes the weights for homoscedastic residuals.  Overriding
+  //  the overloaded version wgt(u) hides the superclass' implementation
+  //  of this version of wgt(). This implementation simply calls the
+  //  superclass' version of the same routine.
+  //  \sa rrel_wls_obj::wgt()
+  virtual void wgt( vect_const_iter begin, vect_const_iter end,
+                    double scale,
+                    vect_iter wgt_begin ) const
+    { rrel_m_est_obj::wgt(begin, end, scale, wgt_begin); }
+
+  //: The weight of the residual. Overriding the overloaded version
+  //  wgt(u) hides the superclass' implementation of this version of
+  //  wgt(). This implementation simply calls the superclass' version of
+  //  the same routine.
+  //  \a r is the residual and
+  //  \a s is the scale for that residual.
+  inline  double wgt( double r, double s ) const
+    { return rrel_m_est_obj::wgt(r, s); }
+    
+  
  private:
   double B_;
 };
