8318c6270bb9a989fffd8823a1a67cedf94f9dfa
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-08 03:10:34 -0400

    brief Doxygen comment should be on single line

--- contrib/rpl/rrel/rrel_trunc_quad_obj.h
+++ contrib/rpl/rrel/rrel_trunc_quad_obj.h
@@ -26,8 +26,9 @@
 //  discontinuous. This is the same cost function used in the MSAC
 //  generalisation of RANSAC.
 
-class rrel_trunc_quad_obj : public rrel_m_est_obj {
-public:
+class rrel_trunc_quad_obj : public rrel_m_est_obj
+{
+ public:
   //: Constructor.
   //  \a scale_mult * scale is the truncation threshold.
   rrel_trunc_quad_obj( double scale_mult = 2.0 );
@@ -38,51 +39,50 @@ public:
   //: The robust loss function for the M-estimator.
   virtual double rho( double u ) const;
 
-  //: The robust loss function for the M-estimator. Overriding the
-  //  overloaded version rho(u) hides the superclass' implementation of
-  //  this version of rho(). This implementation simply calls the
-  //  superclass' version of the same routine.
+  //: The robust loss function for the M-estimator.
+  //  Overriding the overloaded version rho(u) hides the superclass'
+  //  implementation of this version of rho(). This implementation simply
+  //  calls the superclass' version of the same routine.
   //  \a r is the residual and
   //  \a s is the scale for that residual.
-  inline  double rho( double r, double s ) const
+  virtual double rho( double r, double s ) const
     { return rrel_m_est_obj::rho(r, s); }
 
   //: The weight of the residual.
   virtual double wgt( double u ) const;
 
-  //: Evaluate the objective function on heteroscedastic
-  //  residuals.  Overriding the overloaded version wgt(u) hides the
-  //  superclass' implementation of this version of wgt(). This
-  //  implementation simply calls the superclass' version of the same
-  //  routine. 
+  //: Evaluate the objective function on heteroscedastic residuals.
+  //  Overriding the overloaded version wgt(u) hides the superclass'
+  //  implementation of this version of wgt(). This implementation simply
+  //  calls the superclass' version of the same routine.
   //  \sa rrel_wls_obj::wgt()
   virtual void wgt( vect_const_iter res_begin, vect_const_iter res_end,
                     vect_const_iter scale_begin,
                     vect_iter wgt_begin ) const
     { rrel_m_est_obj::wgt(res_begin, res_end, scale_begin, wgt_begin); }
 
-  //: Computes the weights for homoscedastic residuals.  Overriding
-  //  the overloaded version wgt(u) hides the superclass' implementation
-  //  of this version of wgt(). This implementation simply calls the
-  //  superclass' version of the same routine.
+  //: Computes the weights for homoscedastic residuals.
+  //  Overriding the overloaded version wgt(u) hides the superclass'
+  //  implementation of this version of wgt(). This implementation simply
+  //  calls the superclass' version of the same routine.
   //  \sa rrel_wls_obj::wgt()
   virtual void wgt( vect_const_iter begin, vect_const_iter end,
                     double scale,
                     vect_iter wgt_begin ) const
     { rrel_m_est_obj::wgt(begin, end, scale, wgt_begin); }
 
-  //: The weight of the residual. Overriding the overloaded version
-  //  wgt(u) hides the superclass' implementation of this version of
-  //  wgt(). This implementation simply calls the superclass' version of
-  //  the same routine.
+  //: The weight of the residual.
+  //  Overriding the overloaded version wgt(u) hides the superclass'
+  //  implementation of this version of wgt(). This implementation simply
+  //  calls the superclass' version of the same routine.
   //  \a r is the residual and
   //  \a s is the scale for that residual.
-  inline  double wgt( double r, double s ) const
+  virtual double wgt( double r, double s ) const
     { return rrel_m_est_obj::wgt(r, s); }
 
-private:
+ private:
   //: Squared threshold.
   double T_sqr_;
 };
 
-#endif
+#endif // rrel_trunc_quad_obj_h_
