942c29d418b97638de9aee2165dca472128c2b8c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-08 09:10:15 -0400

    should not access uninitialised elements

--- core/vnl/tests/test_resize.cxx
+++ core/vnl/tests/test_resize.cxx
@@ -18,9 +18,10 @@ void test_resize()
 
     vnl_resize_v(X, 5);
     TEST("size", X.size(), 5);
+#if 0
     // After resize, old data is lost, so the following test must fail:
-//  TEST("resize", X(0)+X(1)+X(2)+X(3)+X(4), 6.0);
-    vcl_cout << "X = " << X << vcl_endl;
+    TEST("resize", X(0)+X(1)+X(2)+X(3)+X(4), 6.0);
+#endif
   }
 
   {
@@ -33,9 +34,10 @@ void test_resize()
     vnl_resize_m(M, 5, 7);
     TEST("size", M.rows(), 5);
     TEST("size", M.cols(), 7);
+#if 0
     // After resize, old data is lost, so the following test must fail:
-//  TEST("resize", M(0,0)+M(1,1)+M(2,2)+M(3,3)+M(4,4), 6.0);
-    vcl_cout << "M =\n" << M << vcl_endl;
+    TEST("resize", M(0,0)+M(1,1)+M(2,2)+M(3,3)+M(4,4), 6.0);
+#endif
   }
 }
 
