904172a987030dd4026026c02afb078b1383e13f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-08 08:34:16 -0400

    removed duplicate template instantiation

--- contrib/mul/mil/Templates/mil_gaussian_pyramid_builder+vil1_byte-.cxx
+++
@@ -1,5 +0,0 @@
-#include <mil/mil_gaussian_pyramid_builder_2d.txx>
-#include <vil1/vil1_byte.h>
-template class mil_gaussian_pyramid_builder_2d<vil1_byte>;
-
-// MIL_GAUSSIAN_PYRAMID_BUILDER_2D_INSTANTIATE(vil1_byte);
