f2a46f09ac069e4f0e57f0847c2b05d30b0570a7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-11 05:27:16 -0400

    replaced left() by left_event(), to avoid potential confusion with std::left()

--- core/vgui/examples/example_sprite.cxx
+++ core/vgui/examples/example_sprite.cxx
@@ -10,42 +10,36 @@
 #include <vgui/vgui_viewer2D_tableau.h>
 #include <vgui/vgui_soview2D.h>
 
-vgui_event_condition left( vgui_key('h') );
-vgui_event_condition right( vgui_key('l') );
-vgui_event_condition up( vgui_key('k') );
-vgui_event_condition down( vgui_key('j') );
+vgui_event_condition left_event( vgui_key('h') );
+vgui_event_condition right_event( vgui_key('l') );
+vgui_event_condition up_event( vgui_key('k') );
+vgui_event_condition down_event( vgui_key('j') );
 
 struct move_sprite_tableau : public vgui_wrapper_tableau
 {
   move_sprite_tableau( vgui_tableau_sptr tab, vgui_soview2D* so )
     : vgui_wrapper_tableau( tab ),
       so_view_( so )
-    { }
+  { }
 
   bool handle(vgui_event const &e)
-    {
-      bool moved = false;
-      if( left(e) ) {
-        so_view_->translate( -5.0f, 0.0f );
-        moved = true;
-      } else if( right( e ) ) {
-        so_view_->translate( +5.0f, 0.0f );
-        moved = true;
-      } else if( up( e ) ) {
-        so_view_->translate( 0.0f, -5.0f );
-        moved = true;
-      } else if( down( e ) ) {
-        so_view_->translate( 0.0f, +5.0f );
-        moved = true;
-      }
-
-      if( moved ) {
-        post_redraw();
-        return true;
-      } else {
-        return vgui_wrapper_tableau::handle( e );
-      }
+  {
+    if ( left_event(e) ) {
+      so_view_->translate( -5.0f, 0.0f );
+      post_redraw(); return true;
+    } else if ( right_event( e ) ) {
+      so_view_->translate( +5.0f, 0.0f );
+      post_redraw(); return true;
+    } else if ( up_event( e ) ) {
+      so_view_->translate( 0.0f, -5.0f );
+      post_redraw(); return true;
+    } else if ( down_event( e ) ) {
+      so_view_->translate( 0.0f, +5.0f );
+      post_redraw(); return true;
     }
+    else
+      return vgui_wrapper_tableau::handle( e );
+  }
 
   vgui_soview2D* so_view_;
 };
@@ -57,7 +51,7 @@ struct move_sprite_tableau_new
 {
   move_sprite_tableau_new( vgui_tableau_sptr tab, vgui_soview2D* so )
     : move_sprite_tableau_sptr( new move_sprite_tableau( tab, so ) )
-    { }
+  { }
 };
 
 int
@@ -73,47 +67,46 @@ main( int argc, char ** argv )
   vil_image_view_base_sptr sprite_img;
 
 
-  if( background.set() ) {
+  if ( background.set() )
+  {
     back_img = vil_load( background() );
-    if( !back_img ) {
-      vcl_cerr << "Couldn't load background image " << background() << "\n";
-    }
+    if ( !back_img )
+      vcl_cerr << "Couldn't load background image " << background() << '\n';
   }
-  if( !back_img ) {
+  if ( !back_img )
+  {
     vil_image_view<vxl_byte>* img = new vil_image_view<vxl_byte>( 256, 256, 1 );
-    for( unsigned j = 0; j < img->nj(); ++j ) {
-      for( unsigned i = 0; i < img->ni(); ++i ) {
+    for ( unsigned j = 0; j < img->nj(); ++j )
+      for ( unsigned i = 0; i < img->ni(); ++i )
         (*img)(i,j) = (j+i) % 256;
-      }
-    }
     back_img = img;
   }
 
-  if( sprite.set() ) {
+  if ( sprite.set() )
+  {
     sprite_img = vil_load( sprite() );
-    if( !sprite_img ) {
-      vcl_cerr << "Couldn't load foreground sprite image " << sprite() << "\n";
-    }
+    if ( !sprite_img )
+      vcl_cerr << "Couldn't load foreground sprite image " << sprite() << '\n';
   }
-  if( !sprite_img ) {
+  if ( !sprite_img )
+  {
     vil_image_view< vxl_byte>* img = new vil_image_view<vxl_byte>( 32, 48, 4 );
-    for( unsigned j = 0; j < img->nj(); ++j ) {
-      for( unsigned i = 0; i < img->ni(); ++i ) {
-        // r,g,b,a
+    for ( unsigned j = 0; j < img->nj(); ++j )
+      for ( unsigned i = 0; i < img->ni(); ++i )
+      { // r,g,b,a
         (*img)(i,j,0) = 255;
         (*img)(i,j,1) = 0;
         (*img)(i,j,2) = 0;
         (*img)(i,j,3) = i*255 / img->ni();
       }
-    }
     sprite_img = img;
   }
 
   vcl_cout << "Move sprite around with\n"
-           << " left:  " << left.as_string() << "\n"
-           << " right: " << right.as_string() << "\n"
-           << " up:    " << up.as_string() << "\n"
-           << " down:  " << down.as_string() << vcl_endl;
+           << " left:  " << left_event.as_string() << '\n'
+           << " right: " << right_event.as_string() << '\n'
+           << " up:    " << up_event.as_string() << '\n'
+           << " down:  " << down_event.as_string() << vcl_endl;
 
   vgui_image_tableau_new    back_img_tab( *back_img );
   vgui_easy2D_tableau_new   easy2d_tab( back_img_tab );
