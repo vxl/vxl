b88c5d3e4a0c331f926f695fad0e75fc31443451
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-12 03:17:36 -0400

    replaced #include <vtol/vtol_intensity_face.h> by #include <vtol/vtol_intensity_face_sptr.h>

--- contrib/brl/bseg/sdet/sdet_tracker.h
+++ contrib/brl/bseg/sdet/sdet_tracker.h
@@ -5,13 +5,13 @@
 //:
 // \file
 // \brief a processor for tracking a face_2d based on intensity matching
-// 
-//  The tracker operates by randomly generating a set of hypotheses in the 
+//
+//  The tracker operates by randomly generating a set of hypotheses in the
 //  vicinity of the previous best n matches. These new hypotheses are tested,
 //  (for now by normalized cross-correlation) and ranked to select the best
-//  matches for the next iteration.  The current algorithm only adjusts the 
+//  matches for the next iteration.  The current algorithm only adjusts the
 //  translation each tracked face.
-//  
+//
 // \author
 //  J.L. Mundy - August 20, 2003
 //
@@ -26,8 +26,9 @@
 #include <vil1/vil1_image.h>
 #include <vil1/vil1_memory_image_of.h>
 #include <vtol/vtol_face_2d_sptr.h>
-#include <vtol/vtol_intensity_face.h>
+#include <vtol/vtol_intensity_face_sptr.h>
 #include <sdet/sdet_tracker_params.h>
+
 //: A container to support sorting of samples
 //  Will result in descending order according to correlation
 struct sdet_correlated_face
@@ -55,10 +56,11 @@ struct sdet_correlated_face
   float intensity_mi_;
   float gradient_dir_mi_;
   float total_info_;
-  float* Ix_; 
-  float* Iy_; 
+  float* Ix_;
+  float* Iy_;
   vtol_intensity_face_sptr f_;
 };
+
 struct sdet_byte_hist
 {
   sdet_byte_hist (int nbins = 16);
@@ -67,7 +69,7 @@ struct sdet_byte_hist
   void upcount(const double x);
   double p(const int bin);
   void print();
-  private:
+ private:
   void compute_area();
   bool area_valid_;
   double area_;
@@ -84,7 +86,7 @@ struct sdet_byte_joint_hist
   void upcount(const double a, const double b);
   double p(const int a, const int b);
   void print();
-  private:
+ private:
   void compute_volume();
   bool volume_valid_;
   int nbins_;
@@ -101,7 +103,7 @@ struct sdet_gradient_dir_hist
   void upcount(const double dir, const double mag);
   double p(const int bin);
   void print();
-  private:
+ private:
   void compute_area();
   bool area_valid_;
   double area_;
@@ -109,6 +111,7 @@ struct sdet_gradient_dir_hist
   double delta_;
   vcl_vector<double> counts_;
 };
+
 struct sdet_gradient_dir_joint_hist
 {
   sdet_gradient_dir_joint_hist (int nbins = 8);
@@ -118,7 +121,7 @@ struct sdet_gradient_dir_joint_hist
                const double b, const double mag_b);
   double p(const int a, const int b);
   void print();
-  private:
+ private:
   void compute_volume();
   bool volume_valid_;
   int nbins_;
@@ -144,22 +147,22 @@ class sdet_tracker : public sdet_tracker_params
   void init();
   void generate_samples();
   void cull_samples();
-  void track(); 
-  void clear(); 
+  void track();
+  void clear();
 
  protected:
   //protected methods
   //: translate the input face by tx and ty
-  vtol_intensity_face_sptr 
+  vtol_intensity_face_sptr
     transform_face(vtol_intensity_face_sptr const& face,
                    double tx, double ty, double theta, double scale);
-  
+
   //: fill the pixels in the input face from the input image
   void fill_face(vtol_intensity_face_sptr const& face,
                  vil1_memory_image_of<float> const& image);
 
   //: set the gradient values in the model face
-  void set_gradient(sdet_correlated_face* cf, 
+  void set_gradient(sdet_correlated_face* cf,
                     vil1_memory_image_of<float> const& Ix,
                     vil1_memory_image_of<float> const& Iy);
 
@@ -167,15 +170,15 @@ class sdet_tracker : public sdet_tracker_params
   void correlate_face(sdet_correlated_face* cf);
 
   //: Generate a random sample for face translation
-  vtol_intensity_face_sptr 
+  vtol_intensity_face_sptr
     generate_sample(vtol_intensity_face_sptr const& seed);
 
   //: Generate a more informative face using the previous method
-  sdet_correlated_face* 
+  sdet_correlated_face*
   generate_cf_sample(sdet_correlated_face* seed);
-  
+
   //: Form a new sample from the current image
-  sdet_correlated_face* 
+  sdet_correlated_face*
     regenerate_cf_sample(sdet_correlated_face* sample);
 
   //: Form a new sample from the current image
@@ -193,7 +196,7 @@ class sdet_tracker : public sdet_tracker_params
 
   double compute_gradient_angle(sdet_correlated_face* cf);
 
-  void compute_gradient_angle_hist(sdet_correlated_face* cf, 
+  void compute_gradient_angle_hist(sdet_correlated_face* cf,
                                    vcl_vector<double>& ang_hist);
 
   double compute_angle_motion(sdet_correlated_face* cf);
@@ -214,7 +217,6 @@ class sdet_tracker : public sdet_tracker_params
   double model_intensity_entropy_;
   sdet_gradient_dir_hist* model_gradient_dir_hist_;
   double model_gradient_dir_entropy_;
-
 };
 
 #endif // sdet_tracker_h_
