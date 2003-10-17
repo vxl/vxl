f4deeddb3128cd029ae5b53b5778fc19707dc807
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-17 04:30:40 -0400

    replaced #include <vtol/vtol_intensity_face.h> by <vtol/vtol_intensity_face_sptr.h> in this header file

--- contrib/brl/bseg/sdet/sdet_info_tracker.h
+++ contrib/brl/bseg/sdet/sdet_info_tracker.h
@@ -1,4 +1,3 @@
-//-*-c++-*-
 // This is brl/bseg/sdet/sdet_info_tracker.h
 #ifndef sdet_info_tracker_h_
 #define sdet_info_tracker_h_
@@ -6,13 +5,13 @@
 //:
 // \file
 // \brief a processor for tracking a face_2d based on intensity matching
-// 
-//  The info_tracker operates by randomly generating a set of hypotheses in the 
+//
+//  The info_tracker operates by randomly generating a set of hypotheses in the
 //  vicinity of the previous best n matches. These new hypotheses are tested,
 //  (for now by normalized cross-correlation) and ranked to select the best
-//  matches for the next iteration.  The current algorithm only adjusts the 
+//  matches for the next iteration.  The current algorithm only adjusts the
 //  translation each tracked face.
-//  
+//
 // \author
 //  J.L. Mundy - August 20, 2003
 //
@@ -27,15 +26,16 @@
 #include <vil1/vil1_image.h>
 #include <vil1/vil1_memory_image_of.h>
 #include <vtol/vtol_face_2d_sptr.h>
-#include <vtol/vtol_intensity_face.h>
+#include <vtol/vtol_intensity_face_sptr.h>
 #include <sdet/sdet_info_tracker_params.h>
+
 //: A container to support sorting of samples
 //  Will result in descending order according to correlation
 struct sdet_augmented_face
 {
   sdet_augmented_face ();
   sdet_augmented_face (sdet_augmented_face* af);
-  ~sdet_augmented_face();
+ ~sdet_augmented_face();
   void set_face(vtol_intensity_face_sptr const& f);
   void set_int_mutual_info(const float mi);
   void set_grad_mutual_info(const float mi);
@@ -51,19 +51,20 @@ struct sdet_augmented_face
   float intensity_mi_;
   float gradient_dir_mi_;
   float total_info_;
-  float* Ix_; 
-  float* Iy_; 
+  float* Ix_;
+  float* Iy_;
   vtol_intensity_face_sptr f_;
 };
+
 struct sdet_hist
 {
   sdet_hist (double range = 255, int nbins = 16);
-  ~sdet_hist (){};
+ ~sdet_hist (){};
   int nbins(){return nbins_;}
   void upcount(const double x);
   double p(const int bin);
   void print();
-  private:
+ private:
   void compute_area();
   bool area_valid_;
   double area_;
@@ -76,12 +77,12 @@ struct sdet_hist
 struct sdet_joint_hist
 {
   sdet_joint_hist (int nbins = 16);
-  ~sdet_joint_hist (){};
+ ~sdet_joint_hist (){};
   int nbins(){return nbins_;}
   void upcount(const double a, const double b);
   double p(const int a, const int b);
   void print();
-  private:
+ private:
   void compute_volume();
   bool volume_valid_;
   int nbins_;
@@ -93,12 +94,12 @@ struct sdet_joint_hist
 struct sdet_double_hist
 {
   sdet_double_hist (int nbins = 8);
-  ~sdet_double_hist (){};
+ ~sdet_double_hist (){};
   int nbins(){return nbins_;}
   void upcount(const double dir, const double mag);
   double p(const int bin);
   void print();
-  private:
+ private:
   void compute_area();
   bool area_valid_;
   double area_;
@@ -109,13 +110,13 @@ struct sdet_double_hist
 struct sdet_double_joint_hist
 {
   sdet_double_joint_hist (int nbins = 8);
-  ~sdet_double_joint_hist (){};
+ ~sdet_double_joint_hist (){};
   int nbins(){return nbins_;}
   void upcount(const double a, const double mag_a,
                const double b, const double mag_b);
   double p(const int a, const int b);
   void print();
-  private:
+ private:
   void compute_volume();
   bool volume_valid_;
   int nbins_;
@@ -129,8 +130,8 @@ class sdet_info_tracker : public sdet_info_tracker_params
  public:
   //Constructors/destructor
   sdet_info_tracker(sdet_info_tracker_params& tp);
+ ~sdet_info_tracker();
 
-  ~sdet_info_tracker();
   //Accessors
   void set_image_0(vil1_image& image);
   void set_image_i(vil1_image& image);
@@ -142,21 +143,21 @@ class sdet_info_tracker : public sdet_info_tracker_params
   void init();
   void generate_samples();
   void cull_samples();
-  void track(); 
-  void clear(); 
+  void track();
+  void clear();
 
  protected:
   //protected methods
 
   //: transform a face, returning a new face
-  vtol_intensity_face_sptr 
+  vtol_intensity_face_sptr
     transform_face(vtol_intensity_face_sptr const& face,
-                   const double tx, const double ty, 
+                   const double tx, const double ty,
                    const double theta, const double scale);
-  
+
   //: transform an existing augmented face
   void transform_sample_in_place(sdet_augmented_face* sample,
-                                 const double tx, const double ty, 
+                                 const double tx, const double ty,
                                  const double theta, const double scale);
 
   //: fill the pixels in the input face from the input image
@@ -164,18 +165,15 @@ class sdet_info_tracker : public sdet_info_tracker_params
                  vil1_memory_image_of<float> const& image);
 
   //: set the gradient values in the model face
-  void set_gradient(sdet_augmented_face* cf, 
+  void set_gradient(sdet_augmented_face* cf,
                     vil1_memory_image_of<float> const& Ix,
                     vil1_memory_image_of<float> const& Iy);
 
   //: Generate a random sample for face translation
-  vtol_intensity_face_sptr 
-    generate_face(vtol_intensity_face_sptr const& seed);
+  vtol_intensity_face_sptr generate_face(vtol_intensity_face_sptr const& seed);
 
   //: Generate a more informative face using the previous method
-  sdet_augmented_face* 
-  generate_augmented_face(sdet_augmented_face* seed);
-  
+  sdet_augmented_face* generate_augmented_face(sdet_augmented_face* seed);
 
   double compute_intensity_mutual_information(sdet_augmented_face* cf);
   double compute_gradient_mutual_information(sdet_augmented_face* cf);
@@ -195,7 +193,6 @@ class sdet_info_tracker : public sdet_info_tracker_params
   double model_intensity_entropy_;
   sdet_double_hist* model_gradient_dir_hist_;
   double model_gradient_dir_entropy_;
-
 };
 
 #endif // sdet_info_tracker_h_
