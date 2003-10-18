a1f886df7cf670d8404ee329975fa19b027d2ae4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-18 16:42:41 -0400

    gcc 3.1 no longer needs the 2.95 & 3.0 hack

--- vcl/Templates/vcl_vector+bool-.cxx
+++ vcl/Templates/vcl_vector+bool-.cxx
@@ -1,6 +1,7 @@
 #include <vcl_vector.txx>
 
-#if defined(NO_STD_BOOL) || defined(VCL_SUNPRO_CC_50) || defined(VCL_GCC_295) || defined(GNU_LIBSTDCXX_V3) || defined(VCL_EGCS)
+#if defined(NO_STD_BOOL) || defined(VCL_SUNPRO_CC_50) || defined(VCL_GCC_295) || \
+  ( defined(GNU_LIBSTDCXX_V3) && !defined(VCL_GCC_31) ) || defined(VCL_EGCS)
 // SunPro 5.0 provides vector<bool> as a specialization of vector<T>.
 // Ditto for GNU lib v3.
 namespace {
