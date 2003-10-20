9b226c5d18c56388dc94fe82671f796f381bae7c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-20 03:00:31 -0400

    added check for exception handling support in vxl; Xerces assumes it is on

--- contrib/brl/bbas/bxml/bxml_io.cxx
+++ contrib/brl/bbas/bxml/bxml_io.cxx
@@ -1,24 +1,29 @@
-#include <util/PlatformUtils.hpp>
-#include <parsers/DOMParser.hpp>
-#include <dom/DOM_Document.hpp>
-#include <dom/DOM_Element.hpp>
+#include "bxml_io.h"
 #include <bxml/bxml_input_converter.h>
 #include <bxml/bxml_vsol_point_2d_input_converter.h>
 #include <bxml/bxml_vdgl_digital_curve_input_converter.h>
 #include <bxml/bxml_vtol_vertex_2d_input_converter.h>
 #include <bxml/bxml_vtol_zero_chain_2d_input_converter.h>
 #include <bxml/bxml_vtol_edge_2d_input_converter.h>
-#include <bxml/bxml_io.h>
 #include <vcl_iostream.h>
+
+// From XERCES:
+#if !VCL_HAS_EXCEPTIONS
+#include <util/XMLException.hpp>
+#undef ThrowXML
+#define ThrowXML(type,code) /* empty */
+#endif
+#include <util/PlatformUtils.hpp>
+#include <parsers/DOMParser.hpp>
+#include <dom/DOM_Document.hpp>
+#include <dom/DOM_Element.hpp>
 
 int bxml_io::current_object_index_ = 1;
 vcl_vector<bxml_input_converter_sptr> bxml_io::input_converters_;
 
-
 bxml_io::bxml_io()
 {
 }
-
 
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
@@ -59,7 +64,7 @@ void bxml_io::register_input_converter(bxml_input_converter_sptr conv)
    return;
 
   //if not found then add it
-#if 0
+#ifdef DEBUG
   vcl_cout << "registering " << conv->get_class_name() << " converter\n";
 #endif
   input_converters_.push_back(conv);
@@ -71,20 +76,19 @@ bool bxml_io::get_xml_root(vcl_string fname,DOM_Element& root)
   //  ErrorHandler *errReporter = new xml_error_handler();
   //  parser->setErrorHandler(errReporter);
   parser->setToCreateXMLDeclTypeNode(true);
-#if 0
+#ifdef DEBUG
   vcl_cout << "parsing file " << fname << vcl_endl;
 #endif
   parser->parse(fname.c_str());
-#if 0
+#ifdef DEBUG
   vcl_cout << "done parsing file " << fname << vcl_endl;
 #endif
   DOM_Node doc = parser->getDocument();
   delete parser;
   //  delete errReporter;
 
-#if 0
-  DOMString node_name = doc.getNodeName();
-  vcl_cout << "node_name=" << node_name.transcode() << vcl_endl;
+#ifdef DEBUG
+  vcl_cout << "node_name=" << doc.getNodeName().transcode() << vcl_endl;
 #endif
   int node_type = doc.getNodeType();
   if (node_type != DOM_Node::DOCUMENT_NODE) {
@@ -98,9 +102,8 @@ bool bxml_io::get_xml_root(vcl_string fname,DOM_Element& root)
     vcl_cout << "Error: document has null root\n";
     return false;
   }
-#if 0
-  char* root_name = root.getTagName().transcode();
-  vcl_cout << "root_name=" << vcl_string(root_name) << vcl_endl;
+#ifdef DEBUG
+  vcl_cout << "root_name=" << root.getTagName().transcode() << vcl_endl;
 #endif
   return true;
 }
@@ -120,44 +123,40 @@ bool bxml_io::parse_xml(vcl_string fname,vcl_vector<bxml_generic_ptr>& objs)
   bxml_input_converter_sptr conv;
 
   DOM_Node child = root.getFirstChild();
-  while ( (child != 0) ) {
+  while ( child != 0 )
+  {
     int cnode_type = child.getNodeType();
 
-    if (cnode_type == DOM_Node::ELEMENT_NODE) {
-      char* ctag_name = ((DOM_Element*)&child)->getTagName().transcode();
-      vcl_string ctname(ctag_name);
+    if (cnode_type == DOM_Node::ELEMENT_NODE)
+    {
+      vcl_string ctname = ((DOM_Element*)&child)->getTagName().transcode();
       conv = find_converter_from_tag(ctname);
-      if (conv) {
-#if 0
+      if (conv)
+      {
+#ifdef DEBUG
         vcl_cout << "calling " << conv->get_class_name() << vcl_endl;
 #endif
         if (conv->extract_from_dom(child)) {
           bxml_generic_ptr gp = conv->construct_object();
           objs.push_back(gp);
         }
-        else {
+        else
           vcl_cout << "Error: unable to parse tag =" << ctname << vcl_endl;
-        }
       }
-      else {
+      else
         vcl_cout << "Unrecognized tag =" << ctname << vcl_endl;
-      }
     }
     child = child.getNextSibling();
   }
   XMLPlatformUtils::Terminate();
   return true;
 }
+
 void bxml_io::register_input_converters()
 {
-  bxml_io::
-    register_input_converter(new bxml_vsol_point_2d_input_converter());
-  bxml_io::
-    register_input_converter(new bxml_vdgl_digital_curve_input_converter());
-  bxml_io::
-    register_input_converter(new bxml_vtol_vertex_2d_input_converter());
-  bxml_io::
-    register_input_converter(new bxml_vtol_zero_chain_2d_input_converter());
-  bxml_io::
-    register_input_converter(new bxml_vtol_edge_2d_input_converter());
+  bxml_io::register_input_converter(new bxml_vsol_point_2d_input_converter());
+  bxml_io::register_input_converter(new bxml_vdgl_digital_curve_input_converter());
+  bxml_io::register_input_converter(new bxml_vtol_vertex_2d_input_converter());
+  bxml_io::register_input_converter(new bxml_vtol_zero_chain_2d_input_converter());
+  bxml_io::register_input_converter(new bxml_vtol_edge_2d_input_converter());
 }
