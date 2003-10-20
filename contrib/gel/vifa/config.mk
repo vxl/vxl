16f2ef14e9b69b3ad088e9cf29b08bb45763fe49
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-20 07:23:03 -0400

    vifa_image_histogram uses vil

--- contrib/gel/vifa/config.mk
+++ contrib/gel/vifa/config.mk
@@ -7,3 +7,4 @@ include ${IUEROOT}/core/vnl/config.mk
 include ${IUEROOT}/core/vgl/config.mk
 include ${IUEROOT}/core/vbl/config.mk
 include ${IUEROOT}/core/vul/config.mk
+include ${IUEROOT}/core/vil/config.mk
