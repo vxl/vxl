9127075b4ae7f7b8620c640e561c30d668878e53
Brad King <bradking@users.sourceforge.net>
2003-10-20 13:33:49 -0400

    Fixes for borland compiler.

--- core/vpl/vpl_fdopen.cxx
+++ core/vpl/vpl_fdopen.cxx
@@ -7,8 +7,10 @@ extern "C" FILE *fdopen(int, char const *);
 
 vcl_FILE *vpl_fdopen(int fd, char const *mode)
 {
-#if defined(VCL_BORLAND)
-  return _fdopen(fd, const_cast<char*>(mode));
+#if defined(VCL_BORLAND_55)
+  return fdopen(fd, const_cast<char*>(mode));
+#elif defined(VCL_BORLAND_56)
+  return std::fdopen(fd, const_cast<char*>(mode));
 #else
   return fdopen(fd, mode);
 #endif
