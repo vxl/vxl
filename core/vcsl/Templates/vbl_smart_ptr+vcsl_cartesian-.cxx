674b796db9843aff81882d9db8c17b12728e7337
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-10-21 07:00:15 -0400

    removed stuff that was commented out

--- core/vcsl/Templates/vbl_smart_ptr+vcsl_cartesian-.cxx
+++ core/vcsl/Templates/vbl_smart_ptr+vcsl_cartesian-.cxx
@@ -11,37 +11,3 @@
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vcsl_cartesian);
-
-#if 0
-template <typename T>
-struct wrap
-{
-  wrap () : ptr_(0) { }
-  wrap (wrap<T> const &p) : ptr_(p.ptr_) { if (ptr_) f(); }
-  wrap (T *p) : ptr_(p) { }
-  wrap<T> &operator = (wrap<T> const &r) { ptr_ = r.ptr_; return *this; }
-
-  void f();
-
-private:
-  T *ptr_;
-};
-
-#include <vcl_vector.h>
-
-class X;
-
-void g(X *x)
-{
-  vcl_vector<wrap<X> > v;
-  v.push_back(x);
-};
-
-#if 1
-template <typename T>
-void wrap<T>::f()
-{
-  ptr_->f();
-}
-#endif // 1
-#endif // 0
