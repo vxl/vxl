bffcbc3a4406310cf4411de7ff7cc705da3a4b84
David Cristinacce <dcristinacce@users.sourceforge.net>
2003-10-22 13:05:06 -0400

    added instantiation to save vectors of images

---
+++ core/vil/io/Templates/vsl_vector_io+vcl_vector+vil_image_view+float---.cxx
@@ -0,0 +1,4 @@
+//#include <vsl/vsl_binary_loader.h>
+#include <vsl/vsl_vector_io.txx>
+#include "vsl_vector_io+vil_image_view+float--.cxx"
+VSL_VECTOR_IO_INSTANTIATE( vcl_vector< vil_image_view<float> > );
