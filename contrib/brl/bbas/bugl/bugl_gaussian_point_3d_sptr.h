df56a9bad0c728ddd6a74d7b24ddb65b20f9833e
Kongbin Kang <kkang@users.sourceforge.net>
2003-10-25 00:10:34 -0400

    add smart pointer

---
+++ contrib/brl/bbas/bugl/bugl_gaussian_point_3d_sptr.h
@@ -0,0 +1,8 @@
+#if !defined bugl_guassian_point_3d_sptr_h_
+#define bugl_guassian_point_3d_sptr_h_
+
+#include <vbl/vbl_smart_ptr.h>
+class bugl_gaussian_point_3d;
+typedef vbl_smart_ptr<bugl_gaussian_point_3d> bugl_gaussian_point_3d_sptr;
+
+#endif
