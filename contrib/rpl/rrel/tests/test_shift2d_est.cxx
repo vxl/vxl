3d8ff539f533d0bfc1977ca13f23e32ddaf2d25d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-11-02 10:14:22 -0500

    TEST replaced with TEST_NEAR

--- contrib/rpl/rrel/tests/test_shift2d_est.cxx
+++ contrib/rpl/rrel/tests/test_shift2d_est.cxx
@@ -8,93 +8,88 @@
 
 static void test_shift2d_est()
 {
-  vnl_double_3x3 H (0.0);
-  vnl_matrix<double> cofact;
-  vcl_vector <int> indices;
-  vcl_vector <vnl_vector<double> > p,q;
-  vnl_vector<double> param (2,0.0);
-  vnl_vector<double> true_param (2,0.0);
-  int n;
-  int i;
-  bool ret;
-  const double tol = 1e-8;
-  vnl_double_3 t (0,0,1);
+  vcl_vector <vnl_vector<double> > p;
+  vnl_double_3 t(0,0,1);
 
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = 2; t(1) = 5;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = 4; t(1) = 9;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = -1; t(1) = -1;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = -3; t(1) = -5;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = -9; t(1) = .5;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = 5; t(1) = -5.678;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = 5; t(1) = -5.678; t(2) = 3;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = 0.4; t(1) = 0.894; t(2) = 0.1;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = 500; t(1) = -100; t(2) = 100;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = -20; t(1) = -20; t(2) = 1;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = 4; t(1) = 0.02; t(2) = 1.5;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = 2.345; t(1) = -10; t(2) = 1;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = 8.9e-4; t(1) = -3.1e-4; t(2) = -1e-4;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   t(0) = -10; t(1) = 40; t(2) = 1;
-  p.push_back (t.as_ref());
+  p.push_back(t.as_ref());
 
   // ----------------------------------------------------------------
-  n = p.size ();
-  q.resize (n);
-  indices.resize (1);
+  int n = p.size();
+  vcl_vector<vnl_vector<double> > q(n);
 
   // Test points to instantiate
   {
-    rrel_shift2d_est shift_est (p,p);
-    TEST ("Points to instantiate", shift_est.num_samples_to_instantiate(), 1);
+    rrel_shift2d_est shift_est(p,p);
+    TEST("Points to instantiate", shift_est.num_samples_to_instantiate(), 1);
   }
   // translation only
+  vnl_double_3x3 H(0.0);
   H(0,0) = H(1,1) = 1;
   H(0,2) = -4;
   H(1,2) = 2;
   H(2,2) = 1;
   {
+    vnl_vector<double> true_param(2,0.0);
     true_param[0] = H(0,2);
     true_param[1] = H(1,2);
     true_param /= true_param.two_norm();
-    for (i=0;i<n;i++)
+    for (int i=0; i<n; i++)
       q[i] = H *p[i];
-    rrel_shift2d_est shift_est (p,q);
+    rrel_shift2d_est shift_est(p,q);
 
+    vcl_vector<int> indices(1);
     indices[0] = 1;
-    ret = shift_est.fit_from_minimal_set (indices, param);
+    vnl_vector<double> param(2,0.0);
+    TEST("fit_from_minimal_set()", shift_est.fit_from_minimal_set(indices, param), true);
     param /= param.two_norm();
-    TEST ("(Translation) minimal-set estimation", (param-true_param).two_norm() < tol, true);
+    TEST_NEAR("(Translation) minimal-set estimation", (param-true_param).two_norm(), 0.0, 1e-8);
 
-    ret = shift_est.weighted_least_squares_fit (param, cofact, NULL);
+    vnl_matrix<double> cofact;
+    TEST("weighted_least_squares_fit()", shift_est.weighted_least_squares_fit(param, cofact, NULL), true);
     param /= param.two_norm();
-    TEST ("(Translation) Weighted Least Squares", (param-true_param).two_norm() < tol, true);
+    TEST_NEAR("(Translation) Weighted Least Squares", (param-true_param).two_norm(), 0.0, 1e-8);
   }
 }
 
