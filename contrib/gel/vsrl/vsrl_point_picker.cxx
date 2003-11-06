3b99922dc1b82118e6bab53184bf8482e63c24bd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-11-06 02:07:06 -0500

    made point picker more robust

--- contrib/gel/vsrl/vsrl_point_picker.cxx
+++ contrib/gel/vsrl/vsrl_point_picker.cxx
@@ -8,8 +8,8 @@
 #include <vgui/vgui_find.h>
 #include <vgui/vgui_projection_inspector.h>
 #include <vil1/vil1_image.h>
-#include <vil/vil_image_view.h>
-#include <vil/vil_image_view_base.h>
+#include <vil1/vil1_pixel.h>
+#include <vxl_config.h>
 
 // Default ctor
 vsrl_point_picker::vsrl_point_picker( vgui_tableau_sptr child)
@@ -25,39 +25,39 @@ bool vsrl_point_picker::handle( vgui_event const &e)
   vgui_modifier m = e.modifier;
 
   if (e.type== vgui_BUTTON_DOWN)
-    {
-      // undo the window transformation
-      vgui_projection_inspector pi;
-      float ix, iy;
-      pi.window_to_image_coordinates( e.wx, e.wy, ix, iy);
-      point_.set(ix,iy); // save the point that was picked.
+  {
+    // undo the window transformation
+    vgui_projection_inspector pi;
+    float ix, iy;
+    pi.window_to_image_coordinates( e.wx, e.wy, ix, iy);
+    point_.set(ix,iy); // save the point that was picked.
 
-      if ( (e.button== vgui_LEFT) && !m)
-        {
-          put_point(ix,iy);
-          vgui::out << '(' << ix << ' ' << iy << ")\n";
-        }
-      else if (e.button== vgui_LEFT && (m & vgui_SHIFT))
-        {
-          put_H_line(ix,iy);
-          vgui::out << '(' << ix << ' ' << iy << ")\n";
-        }
-      else if (e.button== vgui_LEFT && (m & vgui_ALT))
-        {
-          put_H_line(ix,iy);
-          vgui::out << '(' << ix << ' ' << iy << ") ALT!\n";
-        }
-      else if (e.button== vgui_MIDDLE && !e.modifier)
-        {
-          int value = this->get_value(ix,iy);
-          vgui::out << '(' << ix << ' ' << iy << ") I= " << value << "\n";
-        }
-      else
-        {
-          vgui::out << '(' << ix << ' ' << iy << ")\n";
-        }
-      this->post_redraw();
+    if ( (e.button== vgui_LEFT) && !m)
+    {
+      put_point(ix,iy);
+      vgui::out << '(' << ix << ' ' << iy << ")\n";
     }
+    else if (e.button== vgui_LEFT && (m & vgui_SHIFT))
+    {
+      put_H_line(ix,iy);
+      vgui::out << '(' << ix << ' ' << iy << ")\n";
+    }
+    else if (e.button== vgui_LEFT && (m & vgui_ALT))
+    {
+      put_H_line(ix,iy);
+      vgui::out << '(' << ix << ' ' << iy << ") ALT!\n";
+    }
+    else if (e.button== vgui_MIDDLE && !e.modifier)
+    {
+      int value = this->get_value(ix,iy);
+      vgui::out << '(' << ix << ' ' << iy << ") I= " << value << "\n";
+    }
+    else
+    {
+      vgui::out << '(' << ix << ' ' << iy << ")\n";
+    }
+    this->post_redraw();
+  }
 
 
   return child && child->handle( e);
@@ -69,7 +69,7 @@ vsrl_point_picker::get_easy2D_pointer(vgui_tableau_sptr const& tab)
   vgui_easy2D_tableau_sptr e2d_ptr;
   if (tab)
     e2d_ptr.vertical_cast(vgui_find_below_by_type_name(tab,
-                                                       vcl_string("vgui_easy2D_tableau")));
+                          vcl_string("vgui_easy2D_tableau")));
   return e2d_ptr;
 }
 
@@ -79,7 +79,7 @@ vsrl_point_picker::get_image_tab_pointer(vgui_tableau_sptr const& tab)
   vgui_image_tableau_sptr img_tab_ptr;
   if (tab)
     img_tab_ptr.vertical_cast(vgui_find_below_by_type_name(tab,
-                                                       vcl_string("vgui_image_tableau")));
+                              vcl_string("vgui_image_tableau")));
   return img_tab_ptr;
 }
 
@@ -122,7 +122,16 @@ int vsrl_point_picker::get_value(float x, float y)
 {
   vgui_image_tableau_sptr img_tab = get_image_tab_pointer(this);
   vil1_image img = img_tab->get_image();
-  unsigned int value=0;
-  img.get_section(&value,x,y,1,1);
-  return value;
+  switch ( vil1_pixel_format(img) )
+  {
+   case VIL1_BYTE: { vxl_byte value=0;
+    img.get_section(&value,int(x),int(y),1,1); return int(value); }
+   case VIL1_UINT16: { vxl_uint_16 value=0;
+    img.get_section(&value,int(x),int(y),1,1); return int(value); }
+   case VIL1_UINT32: { vxl_uint_32 value=0;
+    img.get_section(&value,int(x),int(y),1,1); return int(value); }
+   default:
+    vcl_cerr << "vsrl_point_picker::get_value() not implemented for this pixel type\n";
+    return -1;
+  }
 }
