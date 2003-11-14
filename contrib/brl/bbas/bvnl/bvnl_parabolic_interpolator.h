a871e7fd9579ffc591d2d17d447181efea974891
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-11-14 18:56:11 -0500

    removing spurious "}"

--- contrib/brl/bbas/bvnl/bvnl_parabolic_interpolator.h
+++ contrib/brl/bbas/bvnl/bvnl_parabolic_interpolator.h
@@ -1,18 +1,16 @@
 #ifndef bvnl_parabolic_interpolator_h_
 #define bvnl_parabolic_interpolator_h_
-
 //-----------------------------------------------------------------------------
 //:
 // \file
 // \brief Parabolic interpolation of a 1-d point set
 //
 // \author
-//   J.L. Mundy - November 12, 2003
+//  J.L. Mundy - November 12, 2003
 //
 //-----------------------------------------------------------------------------
 #include <vcl_vector.h>
 #include <vnl/vnl_matrix.h>
-
 
 //-----------------------------------------------------------------------------
 class bvnl_parabolic_interpolator
@@ -32,7 +30,7 @@ class bvnl_parabolic_interpolator
   double extremum(){return p_ext_;}
   //: Print the data
   void print();
-private:
+ private:
   //private methods
   void fill_scatter_matrix();
   //members
@@ -40,9 +38,6 @@ private:
   vcl_vector<double> v_; //data values
   vnl_matrix<double> s_; //scatter matrix
   double p_ext_;//extremum values
-};
-
-
 };
 
 #endif // bvnl_parabolic_interpolator_h_
