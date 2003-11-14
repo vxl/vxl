1013b46697b97cb8a86ddc0d2747df7609243c61
Joseph L. Mundy <mundyj@users.sourceforge.net>
2003-11-14 18:12:14 -0500

    added a new face_algs class for misc face methods

---
+++ contrib/brl/bbas/btol/btol_face_algs.h
@@ -0,0 +1,41 @@
+#ifndef btol_face_algs_h_
+#define btol_face_algs_h_
+//-----------------------------------------------------------------------------
+//:
+// \file
+// \author J.L. Mundy
+// \brief topology algorithms involving faces or face-centric routines
+// \verbatim
+//  Initial version October 30, 2003
+// \endverbatim
+//
+//-----------------------------------------------------------------------------
+#include <vgl/vgl_polygon.h>
+#include <vsol/vsol_point_2d_sptr.h>
+#include <vtol/vtol_edge_2d_sptr.h>
+#include <vtol/vtol_face_2d_sptr.h>
+#include <vcl_vector.h>
+
+class btol_face_algs
+{
+ public:
+  ~btol_face_algs();
+  static  bool vtol_to_vgl(vtol_face_2d_sptr const & face, vgl_polygon& poly);
+  static  bool edge_intersects(vtol_face_2d_sptr const & face,
+                               vtol_edge_2d_sptr const & edge);
+
+  static  bool intersecting_edges(vtol_face_2d_sptr const & face,
+                                  vcl_vector<vtol_edge_2d_sptr> const & edges,
+                                  vcl_vector<vtol_edge_2d_sptr>& inter_edges);
+
+  //:only valid for face with straight line edges
+  static vsol_point_2d_sptr centroid(vtol_face_2d_sptr const & face);
+
+  //:create an axis-aligned box face
+  static vtol_face_2d_sptr box(const double x0, const double y0, 
+                               const double width, const double height); 
+ private:
+  btol_face_algs();
+};
+
+#endif
