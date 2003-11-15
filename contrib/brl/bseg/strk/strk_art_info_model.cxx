61903ed3dc1edfb339434c16d4d965e5fcd9cb86
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-11-15 16:09:39 -0500

    own header file should be first one to #include

--- contrib/brl/bseg/strk/strk_art_info_model.cxx
+++ contrib/brl/bseg/strk/strk_art_info_model.cxx
@@ -1,16 +1,14 @@
-//----*-c++-*----tells emacs to use C++ mode----------
 // This is brl/bseg/strk/strk_art_info_model.cxx
-#include <vcl_cmath.h>   // for vcl_fabs(double)
-#include <strk/strk_art_info_model.h>
-#include <strk/strk_tracking_face_2d.h>
-#include <btol/btol_face_algs.h>
-#include <vsol/vsol_point_2d.h>
-
+#include "strk_art_info_model.h"
 //:
 // \file
 // See strk_art_info_model.h
 //
 //-----------------------------------------------------------------------------
+#include <strk/strk_tracking_face_2d.h>
+#include <btol/btol_face_algs.h>
+#include <vsol/vsol_point_2d.h>
+#include <vcl_cmath.h> // for sqrt()
 
 //------------------------------------------------------------------------
 // Constructors
@@ -21,7 +19,7 @@ strk_art_info_model::strk_art_info_model()
 
 double strk_art_info_model::arm_radius(strk_tracking_face_2d_sptr const& face)
 {
-  if(!face)
+  if (!face)
     return 0;
   double fx0 = 0, fy0 = 0;
   face->centroid(fx0, fy0);
@@ -31,11 +29,11 @@ double strk_art_info_model::arm_radius(strk_tracking_face_2d_sptr const& face)
 
 strk_art_info_model::
 strk_art_info_model(vcl_vector<vtol_face_2d_sptr> const& faces,
-                      vsol_point_2d_sptr const stem_pivot,
+                    vsol_point_2d_sptr const stem_pivot,
                     vil1_memory_image_of<float> & image)
 {
-	for(vcl_vector<vtol_face_2d_sptr>::const_iterator fit = faces.begin();
-      fit != faces.end(); fit++)
+  for (vcl_vector<vtol_face_2d_sptr>::const_iterator fit = faces.begin();
+       fit != faces.end(); fit++)
     faces_.push_back(new strk_tracking_face_2d(*fit, image));
   stem_pivot_ = stem_pivot;
   //compute the arm radii
@@ -50,8 +48,8 @@ strk_art_info_model(vcl_vector<vtol_face_2d_sptr> const& faces,
                     vil1_memory_image_of<float>& Ix,
                     vil1_memory_image_of<float>& Iy)
 {
-	for(vcl_vector<vtol_face_2d_sptr>::const_iterator fit = faces.begin();
-      fit != faces.end(); fit++)
+  for (vcl_vector<vtol_face_2d_sptr>::const_iterator fit = faces.begin();
+       fit != faces.end(); fit++)
     faces_.push_back(new strk_tracking_face_2d(*fit, image, Ix, Iy));
   stem_pivot_ = stem_pivot;
   //compute the arm radii
@@ -61,9 +59,9 @@ strk_art_info_model(vcl_vector<vtol_face_2d_sptr> const& faces,
 
 strk_art_info_model::strk_art_info_model(strk_art_info_model_sptr const& im)
 {
-  if(!im)
+  if (!im)
     return;
-  for(int i = 0; i<im->n_faces(); i++)
+  for (int i = 0; i<im->n_faces(); i++)
     faces_.push_back(new strk_tracking_face_2d(im->face(i)));
   stem_pivot_ = im->stem_pivot_;
   long_arm_radius_ = im->long_arm_radius_;
@@ -77,11 +75,11 @@ strk_art_info_model::~strk_art_info_model()
 
 bool strk_art_info_model::
 pivot_tip_face(strk_tracking_face_2d_sptr const& face,
-			   const double angle, const double radius)
+               const double angle, const double radius)
 {
-  if(!face||!radius)
+  if (!face||!radius)
     return false;
-  if(!angle)
+  if (!angle)
     return true;
   double fx0 = 0, fy0 = 0;
   face->centroid(fx0, fy0);
@@ -96,20 +94,21 @@ pivot_tip_face(strk_tracking_face_2d_sptr const& face,
   face->transform(tx, ty, 0, 1.0);
   return true;
 }
+
 //:transform the articulated model
 bool strk_art_info_model::transform(const double stem_tx,
-                                    const double stem_ty, 
+                                    const double stem_ty,
                                     const double stem_angle,
                                     const double long_arm_pivot_angle,
-                                    const double short_arm_pivot_angle, 
+                                    const double short_arm_pivot_angle,
                                     const double long_tip_angle,
                                     const double short_tip_angle)
 {
-//   vcl_cout << " long_radius btrans "<< arm_radius(faces_[LONG_TIP]) 
+//   vcl_cout << " long_radius btrans "<< arm_radius(faces_[LONG_TIP])
 //            << " short_radius btrans"<< arm_radius(faces_[SHORT_TIP]) << "\n";
 
   // move the pivot
-  if(!stem_pivot_)
+  if (!stem_pivot_)
     return false;
 //   stem_pivot_->set_x(stem_pivot_->x()+stem_tx);
 //   stem_pivot_->set_y(stem_pivot_->y()+stem_ty);
@@ -118,70 +117,75 @@ bool strk_art_info_model::transform(const double stem_tx,
   //for now we assume that the stem_pivot is the same as the centroid
   //of the stem face (It is being constructed that way at present, although
   //the option remains to have the pivot independently defined).
- for(vcl_vector<strk_tracking_face_2d_sptr>::iterator fit = faces_.begin();
+ for (vcl_vector<strk_tracking_face_2d_sptr>::iterator fit = faces_.begin();
       fit != faces_.end(); fit++)
-    if(!(*fit))
+    if (!(*fit))
       return false;
     else
       (*fit)->transform(stem_tx, stem_ty, stem_angle, 1.0);
 
-  stem_pivot_ = btol_face_algs::centroid(faces_[STEM]->face()->cast_to_face_2d()); 
-//   vcl_cout << "Stem Pivot (" << stem_pivot_->x() << " " << stem_pivot_->y()
-//           << ") Stem COG(" << cog->x() << " " << cog->y() << ")\n";
+  stem_pivot_ = btol_face_algs::centroid(faces_[STEM]->face()->cast_to_face_2d());
+#ifdef DEBUG
+  vcl_cout << "Stem Pivot (" << stem_pivot_->x() << " " << stem_pivot_->y()
+           << ") Stem COG(" << cog->x() << " " << cog->y() << ")\n";
+#endif
 
  //pivot the tips by the same stem angle
- if(!this->pivot_tip_face(faces_[LONG_TIP],stem_angle,
-                          long_arm_radius_))
-   return false;
+  if (!this->pivot_tip_face(faces_[LONG_TIP],stem_angle,
+                            long_arm_radius_))
+    return false;
 
- if(!this->pivot_tip_face(faces_[SHORT_TIP], stem_angle,
-                          short_arm_radius_))
-	return false;
-//   vcl_cout << " long_radius_atrans "<< arm_radius(faces_[LONG_TIP]) 
-//            << " short_radius_atrans "<< arm_radius(faces_[SHORT_TIP]) << "\n";
+  if (!this->pivot_tip_face(faces_[SHORT_TIP], stem_angle,
+                            short_arm_radius_))
+    return false;
+#ifdef DEBUG
+  vcl_cout << " long_radius_atrans "<< arm_radius(faces_[LONG_TIP])
+           << " short_radius_atrans "<< arm_radius(faces_[SHORT_TIP]) << "\n";
+#endif
 
   //transform the arms around the pivot
   //this just translates the tip faces since we assume their orientation
   //only changes by pivoting around their centroid
-  if(!this->pivot_tip_face(faces_[LONG_TIP],long_arm_pivot_angle, long_arm_radius_))
-	return false;
+  if (!this->pivot_tip_face(faces_[LONG_TIP],long_arm_pivot_angle, long_arm_radius_))
+    return false;
 
-  if(!this->pivot_tip_face(faces_[SHORT_TIP], short_arm_pivot_angle,
-	  short_arm_radius_))
-	return false;
-  
+  if (!this->pivot_tip_face(faces_[SHORT_TIP], short_arm_pivot_angle,
+      short_arm_radius_))
+    return false;
+
   //rotate the tip faces around their centroids
   faces_[LONG_TIP]->transform(0.0, 0.0, long_tip_angle, 1.0);
   faces_[SHORT_TIP]->transform(0.0, 0.0, short_tip_angle, 1.0);
 
   return true;
 }
+
 vcl_vector<vtol_face_2d_sptr> strk_art_info_model::vtol_faces()
 {
   vcl_vector<vtol_face_2d_sptr> vtol_faces;
-  for(vcl_vector<strk_tracking_face_2d_sptr>::iterator fit = faces_.begin();
-      fit != faces_.end(); fit++)
-    {
-      vtol_face_2d_sptr f = (*fit)->face()->cast_to_face_2d();
-      vtol_faces.push_back(f);
-    }
+  for (vcl_vector<strk_tracking_face_2d_sptr>::iterator fit = faces_.begin();
+       fit != faces_.end(); fit++)
+  {
+    vtol_face_2d_sptr f = (*fit)->face()->cast_to_face_2d();
+    vtol_faces.push_back(f);
+  }
   return vtol_faces;
 }
 
 bool strk_art_info_model::
 compute_mutual_information(vil1_memory_image_of<float> const& image)
 {
-  if(!image)
+  if (!image)
     return false;
   vil1_memory_image_of<float> null_x, null_y;//dummy args (replace with deflts)
   double model_info = 0.0;
-  for(vcl_vector<strk_tracking_face_2d_sptr>::iterator fit =  faces_.begin();
-      fit != faces_.end(); fit++)
-    {
-      if(!(*fit)->compute_mutual_information(image, null_x, null_y));
-      continue;
-      model_info += (*fit)->total_info();
-    }
+  for (vcl_vector<strk_tracking_face_2d_sptr>::iterator fit =  faces_.begin();
+       fit != faces_.end(); fit++)
+  {
+    if (!(*fit)->compute_mutual_information(image, null_x, null_y));
+    continue;
+    model_info += (*fit)->total_info();
+  }
   total_model_info_ = model_info;
   return true;
 }
@@ -192,16 +196,16 @@ compute_mutual_information(vil1_memory_image_of<float> const& image,
                            vil1_memory_image_of<float> const& Ix,
                            vil1_memory_image_of<float> const& Iy)
 {
-  if(!image||!Ix||!Iy)
+  if (!image||!Ix||!Iy)
     return false;
   double model_info = 0.0;
-  for(vcl_vector<strk_tracking_face_2d_sptr>::iterator fit =  faces_.begin();
-      fit != faces_.end(); fit++)
-    {
-      if(!(*fit)->compute_mutual_information(image, Ix, Iy))
-        continue;
-      model_info += (*fit)->total_info();
-    }
+  for (vcl_vector<strk_tracking_face_2d_sptr>::iterator fit =  faces_.begin();
+       fit != faces_.end(); fit++)
+  {
+    if (!(*fit)->compute_mutual_information(image, Ix, Iy))
+      continue;
+    model_info += (*fit)->total_info();
+  }
   total_model_info_ = model_info;
   return true;
 }
--- contrib/brl/bseg/strk/strk_track_display_process.cxx
+++ contrib/brl/bseg/strk/strk_track_display_process.cxx
@@ -1,5 +1,5 @@
-//----*-c++-*----tells emacs to use C++ mode----------
-// This is brl/strk/strk_track_display_process.cxx
+// This is brl/bseg/strk/strk_track_display_process.cxx
+#include "strk_track_display_process.h"
 #include <vcl_iostream.h>
 #include <vcl_vector.h>
 #include <vtol/vtol_topology_object.h>
@@ -7,7 +7,6 @@
 #include <vtol/vtol_edge.h>
 #include <vtol/vtol_vertex_2d.h>
 #include <vtol/vtol_face_2d.h>
-#include <strk/strk_track_display_process.h>
 
 strk_track_display_process::strk_track_display_process()
 {
@@ -25,15 +24,15 @@ bool strk_track_display_process::input_tracked_faces(vcl_ifstream & str)
   int n_verts =0;
   vcl_string sf, nf, nv;
   str >> sf;
-  if(sf!="START_FRAME:")
+  if (sf!="START_FRAME:")
     return false;
   str >> start_frame_;
   str >> nf;
-  if(nf!="N_FRAMES:")
+  if (nf!="N_FRAMES:")
     return false;
   str >> n_frames;
   str >> nv;
-  if(nv!="N_VERTS:")
+  if (nv!="N_VERTS:")
     return false;
   str >> n_verts;
   vnl_matrix<double> cog(n_frames, 2);
@@ -41,71 +40,72 @@ bool strk_track_display_process::input_tracked_faces(vcl_ifstream & str)
   vnl_matrix<double> Y(n_frames, n_verts);
   vcl_string cogs, x, y;
   str >> cogs;
-  if(cogs!="COG:")
+  if (cogs!="COG:")
     return false;
   str >> cog;
   str >> x;
-  if(x!="X:")
+  if (x!="X:")
     return false;
   str >> X;
   str >> y;
-  if(y!="Y:")
+  if (y!="Y:")
     return false;
   str >> Y;
   vcl_vector<vtol_vertex_sptr> verts;
-  for(int i = 0; i<n_frames; i++)
+  for (int i = 0; i<n_frames; i++)
+  {
+    verts.clear();
+    for (int j = 0; j<n_verts; j++)
     {
-      verts.clear();
-      for(int j = 0; j<n_verts; j++)
-        {
-          vtol_vertex_2d* v2d  = new vtol_vertex_2d(X[i][j], Y[i][j]);
-          verts.push_back((vtol_vertex*)v2d);
-        }
-      vgl_point_2d<double> p(cog[i][0], cog[i][1]);
-      tracked_cogs_.push_back(p);
-      tracked_faces_.push_back(new vtol_face_2d(verts));
+      vtol_vertex_2d* v2d  = new vtol_vertex_2d(X[i][j], Y[i][j]);
+      verts.push_back((vtol_vertex*)v2d);
     }
+    vgl_point_2d<double> p(cog[i][0], cog[i][1]);
+    tracked_cogs_.push_back(p);
+    tracked_faces_.push_back(new vtol_face_2d(verts));
+  }
   return true;
 }
+
 bool strk_track_display_process::execute()
 {
-  if(failure_)
+  if (failure_)
     return false;
   if (this->get_N_input_images()!=1)
-    {
-      vcl_cout << "In strk_track_display_process::execute() - not exactly one"
-               << " input image \n";
-      failure_ = true;
-      return false;
-    }
+  {
+    vcl_cout << "In strk_track_display_process::execute() -"
+             << " not exactly one input image\n";
+    failure_ = true;
+    return false;
+  }
   output_topo_objs_.clear();
   input_images_.clear();
   static int n_faces = 0;
-  if(first_frame_)
-    {
-      first_frame_ = false;
+  if (first_frame_)
+  {
+    first_frame_ = false;
 
-      vcl_ifstream str = vcl_ifstream(track_file_.c_str());
-      if(!str.is_open())
-        {
-          vcl_cout << "In strk_track_display_process::execute() -"
-                   << " could not open file " << track_file_ << "\n";
-          failure_ = true;
-          return false;
-        }
-        
-      this->input_tracked_faces(str);
-      n_faces = tracked_faces_.size();
-      if(!n_faces)
-        {
-          vcl_cout << "In strk_track_display_process::execute() -"
-                   << " no faces found in track file\n";
-          failure_ = true;
-        }
+    vcl_ifstream str(track_file_.c_str());
+    if (!str)
+    {
+      vcl_cout << "In strk_track_display_process::execute() -"
+               << " could not open file " << track_file_ << "\n";
+      failure_ = true;
+      return false;
     }
+
+    this->input_tracked_faces(str);
+    n_faces = tracked_faces_.size();
+    if (!n_faces)
+    {
+      vcl_cout << "In strk_track_display_process::execute() -"
+               << " no faces found in track file\n";
+      failure_ = true;
+    }
+  }
   int frame_index = this->frame_index();
   int offset = frame_index-start_frame_;
-  if(offset<0||offset>=n_faces)
+  if (offset<0||offset>=n_faces)
     return false;
   vtol_face_2d_sptr f = tracked_faces_[offset];
   vcl_vector<vtol_edge_sptr> edges;
@@ -115,27 +115,27 @@ bool strk_track_display_process::execute()
   vtol_topology_object_sptr to = new vtol_vertex_2d(p.x(), p.y());
   output_topo_objs_.push_back(to);
   //edges
-  for(vcl_vector<vtol_edge_sptr>::iterator eit = edges.begin();
-      eit != edges.end(); eit++)
+  for (vcl_vector<vtol_edge_sptr>::iterator eit = edges.begin();
+       eit != edges.end(); eit++)
     {
       to = (*eit)->cast_to_edge();
       output_topo_objs_.push_back(to);
     }
   return true;
 }
-          
+
 bool strk_track_display_process::set_input_file(vcl_string const& file_name)
 {
   track_file_ = file_name;
-  if(track_file_=="")
-	return false;
-  vcl_ifstream str = vcl_ifstream(track_file_.c_str());
-  if(!str.is_open())
-    {
-      vcl_cout << "In strk_track_display_process::set_input_file() -"
-               << " could not open file " << track_file_ << "\n";
-      return false;
-    }
+  if (track_file_=="")
+    return false;
+  vcl_ifstream str(track_file_.c_str());
+  if (!str)
+  {
+    vcl_cout << "In strk_track_display_process::set_input_file() -"
+             << " could not open file " << track_file_ << "\n";
+    return false;
+  }
   str.close();
   return true;
 }
