e04c491b6d29b84fca03c760291333f505ca534a
Joseph L. Mundy <mundyj@users.sourceforge.net>
2003-11-15 11:09:24 -0500

    removed several tests

--- contrib/brl/bseg/tests/test_driver.cxx
+++ contrib/brl/bseg/tests/test_driver.cxx
@@ -3,7 +3,6 @@
 DECLARE(sdet_test_detector);
 DECLARE(sdet_test_region_proc);
 DECLARE(brip_test_lucas_kanade);
-DECLARE(sdet_test_tracker);
 
 void
 register_tests()
@@ -11,7 +10,6 @@ register_tests()
   REGISTER(sdet_test_detector);
   REGISTER(sdet_test_region_proc);
   REGISTER(brip_test_lucas_kanade);
-  REGISTER(sdet_test_tracker);
 }
 
 DEFINE_MAIN;
