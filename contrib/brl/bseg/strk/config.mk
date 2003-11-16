be6cec680e55a64041acc7b984e05016da4f19be
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-11-16 13:33:16 -0500

    updated w.r.t. CMakeLists.txt

---
+++ contrib/brl/bseg/strk/config.mk
@@ -0,0 +1,14 @@
+IULIBS += -lstrk
+include ${IUEROOT}/contrib/brl/bseg/sdet/config.mk
+include ${IUEROOT}/contrib/brl/bseg/brip/config.mk
+include ${IUEROOT}/contrib/brl/bbas/bdgl/config.mk
+include ${IUEROOT}/contrib/brl/bbas/bsol/config.mk
+include ${IUEROOT}/contrib/brl/bbas/btol/config.mk
+include ${IUEROOT}/contrib/gel/gevd/config.mk
+include ${IUEROOT}/contrib/gel/vdgl/config.mk
+include ${IUEROOT}/contrib/gel/vtol/config.mk
+include ${IUEROOT}/contrib/gel/vsol/config.mk
+include ${IUEROOT}/core/vgl/algo/config.mk
+include ${IUEROOT}/core/vil1/config.mk
+include ${IUEROOT}/core/vnl/config.mk
+include ${IUEROOT}/core/vbl/config.mk
