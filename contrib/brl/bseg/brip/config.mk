be5336ecb0164f5f6b3b1fcf13ce706d6518914b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-11-17 11:59:21 -0500

    updated w.r.t. CMakeList.txt

--- contrib/brl/bseg/brip/config.mk
+++ contrib/brl/bseg/brip/config.mk
@@ -1,5 +1,7 @@
 IULIBS += -lbrip
+include ${IUEROOT}/core/vil/config.mk
 include ${IUEROOT}/core/vil1/config.mk
+include ${IUEROOT}/core/vnl/algo/config.mk
 include ${IUEROOT}/core/vnl/config.mk
 include ${IUEROOT}/core/vbl/config.mk
 include ${IUEROOT}/core/vul/config.mk
