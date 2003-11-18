044723db650e7a3b3909a562e75abdb194f988bd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-11-18 02:12:30 -0500

    added missing #includes

--- contrib/brl/bseg/brip/tests/test_include.cxx
+++ contrib/brl/bseg/brip/tests/test_include.cxx
@@ -1,4 +1,6 @@
-#include <brip/brip_histogram.h>
-#include <brip/brip_mutual_info.h>
+#include <bseg/brip/brip_histogram.h>
+#include <bseg/brip/brip_mutual_info.h>
+#include <bseg/brip/brip_float_ops.h>
+#include <bseg/brip/brip_vil_ops.h>
 
 int main() { return 0; }
