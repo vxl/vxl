6b86b01742d1dd93c23a85fddea265938b36826a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-11-23 00:56:22 -0500

    made more robust against erroneous input

--- contrib/brl/bbas/bugl/bugl_curve_3d.cxx
+++ contrib/brl/bbas/bugl/bugl_curve_3d.cxx
@@ -3,80 +3,59 @@
 #include <bbas/bugl/bugl_normal_point_2d_sptr.h>
 #include <bugl/bugl_normal_point_2d.h>
 #include <bugl/bugl_normal_point_3d.h>
-#include <vcl_iostream.h>
+#include <vcl_cassert.h>
 
-bugl_curve_3d::~bugl_curve_3d()
+bugl_normal_point_3d_sptr bugl_curve_3d::get_point(unsigned int index) const
 {
-
+  assert(index < data_.size());
+  return data_[index][num_neighbors_];
 }
 
-bugl_curve_3d::bugl_curve_3d(const int n)
+vcl_vector<bugl_normal_point_3d_sptr> bugl_curve_3d::get_neighbors(unsigned int index) const
 {
-	num_neighbors_=n;
-}
-
-bugl_normal_point_3d_sptr bugl_curve_3d::get_point(const int index) const
-{
-	return data_[index][num_neighbors_];
-}
-
-vcl_vector<bugl_normal_point_3d_sptr> bugl_curve_3d::get_neighbors(const int index) const
-{
+  assert(index < data_.size());
   return data_[index];
 }
 
-bugl_normal_point_3d_sptr bugl_curve_3d::get_neighbor(const int self, const int offset ) const
+bugl_normal_point_3d_sptr bugl_curve_3d::get_neighbor(unsigned int self, int offset) const
 {
-  assert(offset > -num_neighbors_ && offset < num_neighbors_);
+  assert(offset > -int(num_neighbors_) && offset < int(num_neighbors_));
   return data_[self][num_neighbors_ + offset];
 }
 
-int bugl_curve_3d::add_curve(vcl_vector<bugl_normal_point_3d_sptr > & pts)
+void bugl_curve_3d::add_curve(vcl_vector<bugl_normal_point_3d_sptr > & pts)
 {
-  int size = pts.size();
+  unsigned int size = pts.size();
   assert(size > 2*num_neighbors_ + 1);
 
   int prev_total = data_.size();
 
   vcl_vector<bugl_normal_point_3d_sptr> seg(2*num_neighbors_+1);
-  for(int i=0; i<size; i++){
+  for (unsigned int i=0; i<size; i++){
     seg[num_neighbors_] = pts[i]; // assign the middle point
-    for(int j=1; j<=num_neighbors_; j++){
+    for (unsigned int j=1; j<=num_neighbors_; j++){
       // assign the left neighbors
-      if(j > i)
+      if (j > i)
         seg[num_neighbors_-j] = 0;
       else
         seg[num_neighbors_-j] = pts[i-j];
 
       // assign the right neighbors
-      if(j < size - i)
+      if (i+j < size)
         seg[num_neighbors_ + j] = pts[i+j];
       else
         seg[num_neighbors_ + j] = 0;
     }//end neighbors
 
     data_.push_back(seg);
-      
   }//end of all the points
 
   index_.push_back(prev_total);
-
-  return 0;
 }
 
-int bugl_curve_3d::get_num_points() const
+unsigned int bugl_curve_3d::get_fragment_size(unsigned int i) const
 {
-  return data_.size();
-}
-
-int bugl_curve_3d::get_num_fragments() const
-{
-  return index_.size();
-}
-
-int bugl_curve_3d::get_fragment_size(int i) const
-{
-  if(i<index_.size()-1)
+  if (i+1<index_.size())
     return index_[i+1] - index_[i];
   else 
     return data_.size() - index_[i];
--- contrib/brl/bbas/bugl/bugl_curve_3d.h
+++ contrib/brl/bbas/bugl/bugl_curve_3d.h
@@ -1,50 +1,54 @@
 #ifndef bugl_curve_3d_h_
 #define bugl_curve_3d_h_
-#include "bugl_normal_point_3d_sptr.h"
-#include "bugl_normal_point_3d.h"
-#include "bugl_normal_point_3d_sptr.h"
+//:
+// \file
+#include <bugl/bugl_normal_point_3d_sptr.h>
+#include <bugl/bugl_normal_point_3d.h>
+#include <vcl_vector.h>
 
 class bugl_curve_3d
 {
   //: number of neighbors
-	int num_neighbors_;
+  unsigned int num_neighbors_;
 
   //: data
-	vcl_vector<vcl_vector<bugl_normal_point_3d_sptr > > data_;
+  vcl_vector<vcl_vector<bugl_normal_point_3d_sptr > > data_;
 
   //: starting position index for each fragment
   vcl_vector<int> index_;
 
-public:
+ public:
 
   //: total number of points in the curve
-	int get_num_points() const;
+  int get_num_points() const { return data_.size(); }
 
   //: how many fragments in the curve
-  int get_num_fragments() const;
+  int get_num_fragments() const { return index_.size(); }
 
   //: add a vector of point as a curve
-	int add_curve(vcl_vector<bugl_normal_point_3d_sptr > &pts);
+  void add_curve(vcl_vector<bugl_normal_point_3d_sptr > &pts);
 
-  bugl_curve_3d(int neighbors = 2);
+  bugl_curve_3d(unsigned int neighbors = 2) : num_neighbors_(neighbors) {}
 
-  ~bugl_curve_3d();
+  ~bugl_curve_3d() {}
 
   //: get the point
-	bugl_normal_point_3d_sptr  get_point(const int index) const;
+  bugl_normal_point_3d_sptr get_point(unsigned int index) const;
 
-  //: get a point with neighbors 
-  vcl_vector<bugl_normal_point_3d_sptr> get_neighbors(const int index) const;
+  //: get a point with neighbors
+  vcl_vector<bugl_normal_point_3d_sptr> get_neighbors(unsigned int index) const;
 
   //: get a neighbor point
-  bugl_normal_point_3d_sptr get_neighbor(const int self, const int offset ) const;
+  bugl_normal_point_3d_sptr get_neighbor(unsigned int self, int offset) const;
 
-  //: get the global position of a point in a fragment. 0-based indice are used 
-  inline int get_global_pos(int frag_index, int loc_pos ) 
-  {return index_[frag_index]+loc_pos;}
+  //: get the global position of a point in a fragment. 0-based indices are used.
+  //  Returns -1 if the arguments are invalid.
+  inline int get_global_pos(unsigned int frag_index, unsigned int loc_pos)
+  { return frag_index<index_.size() && loc_pos<get_fragment_size(frag_index) ?
+           int(index_[frag_index]+loc_pos) : -1; }
 
-  //: get the i-th fragemnet size
-  int get_fragment_size(int i) const;
+  //: get the i-th fragment size
+  unsigned int get_fragment_size(unsigned int i) const;
 };
 
 #endif
