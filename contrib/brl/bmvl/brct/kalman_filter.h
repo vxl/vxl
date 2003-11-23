1d8887465252f0621327d8a89d205efe8a1f2a44
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-11-23 00:52:05 -0500

    qualified name is not allowed in method declaration

--- contrib/brl/bmvl/brct/kalman_filter.h
+++ contrib/brl/bmvl/brct/kalman_filter.h
@@ -41,7 +41,7 @@ class kalman_filter
   vcl_vector<vgl_point_2d<double> > get_cur_observes();
   vcl_vector<vgl_point_2d<double> > get_next_observes();
   vcl_vector<vgl_point_3d<double> > get_local_pts(); // will be superseded
-  bugl_curve_3d kalman_filter::get_curve_3d();
+  bugl_curve_3d get_curve_3d();
 
   //: read all the data including time stampes, trackers.
   void read_data(const char* fname);
@@ -64,7 +64,7 @@ class kalman_filter
  protected:
   //: read time stamp
   vcl_vector<double> read_timestamp_file(char* fname);
-  
+
   //: if the zero probability returned, the matched point is a outlier
   double matched_point_prob(vnl_double_2& z, vnl_double_2& z_pred);
 
@@ -81,7 +81,7 @@ class kalman_filter
   //: set linearized observation matrix
   vnl_matrix_fixed<double, 2, 6> get_H_matrix(vnl_double_3x4 &P, vnl_double_3 &Y);
 
-  //: computer projective matrix from predicted position
+  //: compute projective matrix from predicted position
   vnl_double_3x4 get_projective_matrix(const vnl_double_3 &v) const;
 
   void init_covariant_matrix();
@@ -98,10 +98,10 @@ class kalman_filter
   //: used for matching point
   vcl_vector<vcl_vector<bugl_gaussian_point_2d<double> > > observes_;
 
-  //: each element represents shoting times for this frame.
+  //: each element represents shooting times for this frame.
   vcl_vector<double> time_tick_;
   //: each element of the vector represents a projection of the same 3D curves.
-  vcl_vector<vcl_vector<vdgl_digital_curve_sptr> >	trackers_;
+  vcl_vector<vcl_vector<vdgl_digital_curve_sptr> > trackers_;
 
   vcl_vector<vnl_double_3> motions_;
   //: current frame position in history pool
@@ -118,7 +118,7 @@ class kalman_filter
   //: covariant matrix of state vector
   vnl_matrix_fixed<double, 6, 6> Q_;
 
-  //: constrain kalman gain matrix
+  //: constraint kalman gain matrix
   vnl_matrix_fixed<double, 6, 2> G_;
 
   //: initial covariant matrix of state vector
@@ -133,8 +133,8 @@ class kalman_filter
   //: camera intrinsic parameters
   vnl_double_3x3 K_;
 
-  //: used for denote outlier point in image
-  static const double large_num_ ;
+  //: used to denote outlier point in image
+  static const double large_num_;
 };
 
 #endif // brct_kalman_filter_h_
