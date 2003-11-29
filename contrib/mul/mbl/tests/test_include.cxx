481cf8b56bc84b948d3ddfc93c101d0a1bdd1e13
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-11-29 04:43:01 -0500

    added missing #include <mbl_test.h>

--- contrib/mul/mbl/tests/test_include.cxx
+++ contrib/mul/mbl/tests/test_include.cxx
@@ -40,6 +40,7 @@
 #include <mbl/mbl_stl.h>
 #include <mbl/mbl_stochastic_data_collector.h>
 #include <mbl/mbl_sum_1d.h>
+#include <mbl/mbl_test.h>
 #include <mbl/mbl_thin_plate_spline_2d.h>
 #include <mbl/mbl_thin_plate_spline_3d.h>
 #include <mbl/mbl_thin_plate_spline_weights_3d.h>
