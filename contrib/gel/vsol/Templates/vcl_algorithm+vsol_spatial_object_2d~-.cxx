460648721923f91a0eafc60ebd1afdacac507188
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-12-01 01:53:06 -0500

    moved instantiation of find() to separate file

---
+++ contrib/gel/vsol/Templates/vcl_algorithm+vsol_spatial_object_2d~-.cxx
@@ -0,0 +1,4 @@
+class vsol_spatial_object_2d;
+#include <vcl_vector.h>
+#include <vcl_algorithm.txx>
+VCL_FIND_INSTANTIATE(vcl_vector<vsol_spatial_object_2d*>::iterator, vsol_spatial_object_2d*);
--- contrib/gel/vsol/Templates/vcl_vector+vsol_spatial_object_2d~-.cxx
+++ contrib/gel/vsol/Templates/vcl_vector+vsol_spatial_object_2d~-.cxx
@@ -1,7 +1,3 @@
-#include <vsol/vsol_spatial_object_2d.h>
+class vsol_spatial_object_2d;
 #include <vcl_vector.txx>
-
 VCL_VECTOR_INSTANTIATE(vsol_spatial_object_2d*);
-
-#include <vcl_algorithm.txx>
-VCL_FIND_INSTANTIATE(vcl_vector<vsol_spatial_object_2d*>::iterator, vsol_spatial_object_2d*);
