1fd84e568b82b37ee3411351609a577f4f1e8d8e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-12-04 17:37:17 -0500

    declaration and instantiation of vil1_convolve_separable() is now in vil1_convolve.*

--- core/vil1/vil1_smooth.cxx
+++ core/vil1/vil1_smooth.cxx
@@ -11,23 +11,9 @@
 #include <vcl_cmath.h>
 #include <vcl_vector.h>
 
-#include <vil1/vil1_byte.h>
-#include <vil1/vil1_memory_image_of.h>
+#include <vxl_config.h>
 #include <vil1/vil1_convolve.h>
 #include <vil1/vil1_convolve.txx>
-
-#define inst(pixel_type, float_type) \
-template \
-void vil1_convolve_separable(float const kernel[], unsigned N, \
-                             vil1_memory_image_of<pixel_type> & buf, \
-                             vil1_memory_image_of<float_type>& tmp, \
-                             vil1_memory_image_of<float_type>& out); \
-template \
-vil1_image vil1_convolve_separable(vil1_image const &, float const*, int, pixel_type*, float_type* )
-
-inst(vil1_byte, float);
-inst(int, float);
-
 
 vil1_image vil1_smooth_gaussian(vil1_image const & in, double sigma)
 {
@@ -36,27 +22,29 @@ vil1_image vil1_smooth_gaussian(vil1_image const & in, double sigma)
   double lc = -2 * vcl_log(cutoff); // cutoff guaranteed > 0
   int radius = (lc<=0) ? 0 : 1 + int(vcl_sqrt(lc)*sigma); // sigma guaranteed >= 0
   int size = 2*radius + 1;
-  vcl_vector<float> mask(size);
+  vcl_vector<double> mask(size);
   double halfnorm = 0.0;
-  mask[radius] = 1.0f;
+  mask[radius] = 1.0;
   for (int x=1; x<=radius; ++x) {
     double v = vcl_exp(-0.5*x*x/(sigma*sigma));
-    mask[radius - x] = mask[radius + x] = float(v);
+    mask[radius - x] = mask[radius + x] = v;
     halfnorm += v;
   }
 
   // normalise mask
   double mass_scale = 1.0/(1 + 2*halfnorm);
   for (int x=0; x< size; ++x)
-    mask[x] *= float(mass_scale);
+    mask[x] *= mass_scale;
 
   // Call convolver
-  if (vil1_pixel_format(in) == VIL1_BYTE)
-    return vil1_convolve_separable(in, /* xxx */&mask[0], size-1, (vil1_byte*)0, (float*)0);
-
-  if (vil1_pixel_format(in) == VIL1_FLOAT)
-    return vil1_convolve_separable(in, /* xxx */&mask[0], size-1, (float*)0, (float*)0);
-
-  return 0;
+  switch(vil1_pixel_format(in))
+  {
+    case VIL1_BYTE:  return vil1_convolve_separable(in, &mask[0], size-1, (vxl_byte*)0, (double*)0, (float*)0);
+    case VIL1_UINT16:return vil1_convolve_separable(in, &mask[0], size-1, (vxl_uint_16*)0, (double*)0, (float*)0);
+    case VIL1_UINT32:return vil1_convolve_separable(in, &mask[0], size-1, (vxl_uint_32*)0, (double*)0, (float*)0);
+    case VIL1_FLOAT: return vil1_convolve_separable(in, &mask[0], size-1, (float*)0, (double*)0, (float*)0);
+    case VIL1_DOUBLE:return vil1_convolve_separable(in, &mask[0], size-1, (double*)0, (double*)0, (double*)0);
+    default: return 0;
+  }
 }
 
