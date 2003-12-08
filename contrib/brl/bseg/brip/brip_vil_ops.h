d91c03854dc0e317068653bad10bbff956a2771c
Matthew J. Leotta <mleotta@users.sourceforge.net>
2003-12-08 17:05:17 -0500

    Added a function for gaussian smoothing with any size kernel

--- contrib/brl/bseg/brip/brip_vil_ops.h
+++ contrib/brl/bseg/brip/brip_vil_ops.h
@@ -15,10 +15,13 @@
 //  Modifications
 // \endverbatim
 
+#include <vcl_cmath.h>
 #include <vil/vil_image_view.h>
 #include <vil/vil_math.h>
 #include <vil/vil_fill.h>
+#include <vil/vil_transpose.h>
 #include <vil/algo/vil_sobel_3x3.h>
+#include <vil/algo/vil_convolve_1d.h>
 
 //: computes the conditioning of the 2n+1 x 2n+1 gradient neigborhood
 template<class T>
@@ -64,6 +67,51 @@ brip_sqrt_grad_singular_values(const vil_image_view<T>& input, vil_image_view<T>
     vil_fill_col(output, c, (T)0);
     vil_fill_col(output, ni-c-1, (T)0);
   }
+}
+
+
+
+//: Filter an image with a gaussian kernel 
+// This is an ntap alternative to vil_gauss_filter_5tap 
+// The kernel is generated using vcl_exp instead of vnl_erf
+// \param sigma The width of the gaussian
+// \param k_size The number of elements in the 1D filter
+// \param option The boundary option applied at all boundaries (see vil_convolve_1d)
+template <class srcT, class destT>
+void brip_gauss_filter( const vil_image_view<srcT>& src_im,
+                        vil_image_view<destT>& dest_im,
+                        double sigma,
+                        unsigned k_size,
+                        vil_convolve_boundary_option option )
+{
+  unsigned ni = src_im.ni();
+  unsigned nj = src_im.nj();
+  unsigned n_planes = src_im.nplanes();
+  dest_im.set_size(ni, nj, n_planes);
+  assert (k_size>1 && k_size<ni && k_size<nj);
+
+  // compute the kernel
+  vcl_vector<double> kernel(k_size);
+  for(int i=0; i<k_size; ++i){
+    double val = ((double(i)+0.5)-double(k_size)/2.0);
+    kernel[i] = vcl_exp(-(val*val)/(2.0*sigma*sigma));
+  }
+  double sum = 0.0;
+  for(int i=0; i<k_size; ++i) sum += kernel[i];
+  for(int i=0; i<k_size; ++i) kernel[i] /= sum;
+
+  vil_image_view<destT> work(ni, nj, n_planes);
+
+  // filter horizontal
+  vil_convolve_1d(src_im, work, kernel.begin()+int(k_size)/2, 
+                  -int(k_size)/2, int(k_size-1)/2, 
+                  destT(0), option, option);
+
+  // filter vertical
+  vil_convolve_1d(vil_transpose(work), vil_transpose(dest_im), kernel.begin()+int(k_size)/2, 
+                  -int(k_size)/2, int(k_size-1)/2, 
+                  destT(0), option, option);    
+
 }
 
 #endif // brip_vil_ops_h_
