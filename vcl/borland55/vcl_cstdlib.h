71603cea05b3be43db3627e7c878ec16aabfcf98
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-12-09 14:19:46 -0500

    more additions for Borland compiler

--- vcl/borland55/vcl_cstdlib.h
+++ vcl/borland55/vcl_cstdlib.h
@@ -3,6 +3,15 @@
 // This is a first attempt at a <cstdlib> for the Borland compiler - PVr,Dec.2003.
 #include <cstdlib>
 
+extern "C" {
+ void abort(); // where do these functions live with the Borland compiler?
+ void qsort(void*, size_t, size_t, int(*)(const void*, const void*));
+ void* malloc(size_t);
+ void* calloc(size_t, size_t);
+ void* realloc(void*, size_t);
+ void free(void*);
+}
+
 // If we define vcl_abs, for example, to ::abs, we have conflicts
 // with std::abs(std::complex<T>) which *is* declared in the
 // std namespace. To avoid these issues, we inject the math
@@ -25,6 +34,24 @@ namespace std {
 #endif
 #ifndef vcl_ldiv
 #define vcl_ldiv std::ldiv
+#endif
+#ifndef vcl_abort
+#define vcl_abort ::abort
+#endif
+#ifndef vcl_qsort
+#define vcl_qsort ::qsort
+#endif
+#ifndef vcl_malloc
+#define vcl_malloc ::malloc
+#endif
+#ifndef vcl_calloc
+#define vcl_calloc ::calloc
+#endif
+#ifndef vcl_realloc
+#define vcl_realloc ::realloc
+#endif
+#ifndef vcl_free
+#define vcl_free ::free
 #endif
 
 #define vcl_generic_cstdlib_STD
