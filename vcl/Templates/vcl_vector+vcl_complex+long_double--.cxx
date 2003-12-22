93fe213cf587eececa68023d9ea58f91641a38a5
Ian Scott <scottim@users.sourceforge.net>
2003-12-22 09:25:28 -0500

    disable warnings found in MSVC STL implementation

--- vcl/Templates/vcl_vector+vcl_complex+long_double--.cxx
+++ vcl/Templates/vcl_vector+vcl_complex+long_double--.cxx
@@ -1,3 +1,8 @@
+#if (defined _MSC_VER) && _MSC_VER == 1200
+// disable warning buried deep in the standard library
+// warning C4018: '<' : signed/unsigned mismatch: vector(159)
+# pragma warning(disable: 4018)
+#endif
 #include <vcl_complex.h>
 #include <vcl_vector.txx>
 
