d043ea0f8840afbb6e1e317f9e6c0831128621f2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2003-12-26 18:42:52 -0500

    added missing explicit template instantiations

---
+++ core/vil/Templates/vil_smart_ptr+vil_nitf_image_subheader-.cxx
@@ -0,0 +1,3 @@
+#include <vil/vil_smart_ptr.txx>
+#include <vil/file_formats/vil_nitf_image_subheader.h>
+VIL_SMART_PTR_INSTANTIATE(vil_nitf_image_subheader);
---
+++ core/vil/Templates/vil_smart_ptr+vil_nitf_message_header-.cxx
@@ -0,0 +1,3 @@
+#include <vil/vil_smart_ptr.txx>
+#include <vil/file_formats/vil_nitf_message_header.h>
+VIL_SMART_PTR_INSTANTIATE(vil_nitf_message_header);
