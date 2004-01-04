14e9e690e7af9e622ab35413c724d2fd4a821b71
Fred Wheeler <fred_wheeler@users.sourceforge.net>
2004-01-04 13:04:40 -0500

    explicit instantiation of vcl_vector<> classes is not needed and Borland cannot handle a + character in an object filename when linking an executable (OK in a lib), so get rid is Templates dir here

--- contrib/oxl/xcv/Templates/vcl_vector+xcv_threeview_manager~-.cxx
+++
@@ -1,7 +0,0 @@
-#include <vcl_vector.txx>
-#include <vcl_algorithm.txx>
-
-class xcv_threeview_manager;
-VCL_VECTOR_INSTANTIATE(xcv_threeview_manager *);
-VCL_FIND_INSTANTIATE(vcl_vector<xcv_threeview_manager *>::iterator, xcv_threeview_manager *);
-VCL_FIND_INSTANTIATE(vcl_vector<xcv_threeview_manager *>::iterator, xcv_threeview_manager const *);
