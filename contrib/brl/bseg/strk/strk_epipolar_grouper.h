25d4ca40fec9ffcade06aedb0132c543c55e4e40
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-01-12 07:33:26 -0500

    removed unused #include <vgl/vgl_line_segment_2d.h>

--- contrib/brl/bseg/strk/strk_epipolar_grouper.h
+++ contrib/brl/bseg/strk/strk_epipolar_grouper.h
@@ -1,4 +1,3 @@
-//--*-c++-*--
 // This is brl/bseg/strk/strk_epipolar_grouper.h
 #ifndef strk_epipolar_grouper_h_
 #define strk_epipolar_grouper_h_
@@ -7,9 +6,9 @@
 // \file
 // \brief a processor for grouping edgel chains for translational motion
 //
-//  The epipolar_grouper finds consistent translational motion groups 
-//  corresponding to intersections of a given epipolar line with 
-//  moving curves.  
+//  The epipolar_grouper finds consistent translational motion groups
+//  corresponding to intersections of a given epipolar line with
+//  moving curves.
 //
 // \author
 //  J.L. Mundy - December 23, 2003
@@ -24,13 +23,13 @@
 #include <vcl_vector.h>
 #include <vnl/vnl_double_3.h>
 #include <vgl/vgl_point_2d.h>
-#include <vgl/vgl_line_segment_2d.h>
 #include <vsol/vsol_polyline_2d_sptr.h>
 #include <vdgl/vdgl_digital_curve_sptr.h>
 #include <vtol/vtol_edge_2d_sptr.h>
 #include <strk/strk_epipolar_grouper_params.h>
 #include <strk/strk_epi_point_sptr.h>
 #include <strk/strk_epi_seg_sptr.h>
+
 class strk_epipolar_grouper : public strk_epipolar_grouper_params
 {
  public:
@@ -60,21 +59,21 @@ class strk_epipolar_grouper : public strk_epipolar_grouper_params
                            strk_epi_point_sptr const &pp1);
   bool inside_epipolar_wedge(vdgl_digital_curve_sptr const& dc);
   bool extract_alpha_segments(vdgl_digital_curve_sptr const & dc,
-                                   vcl_vector<strk_epi_seg_sptr>& epi_segs);
+                              vcl_vector<strk_epi_seg_sptr>& epi_segs);
   bool compute_segments();
   bool intensity_candidates(const strk_epi_seg_sptr& seg,
                             vcl_vector<strk_epi_seg_sptr>& left_cand,
                             vcl_vector<strk_epi_seg_sptr>& right_cand);
   double radius(const double s);
-  double find_left_s(const double a, const double s, 
+  double find_left_s(const double a, const double s,
                      vcl_vector<strk_epi_seg_sptr> const& cand);
-  double find_right_s(const double a, const double s, 
+  double find_right_s(const double a, const double s,
                       vcl_vector<strk_epi_seg_sptr> const& cand);
   double ds(const double s);
   bool image_coords(const double a, const double s,
                     double& u, double& v);
   double scan_interval(const double a, const double sl, const double s);
-  double scan_left(double a, double s, 
+  double scan_left(double a, double s,
                    vcl_vector<strk_epi_seg_sptr> const& left_cand,
                    double& ds);
   double scan_right(double a,double s,
@@ -84,15 +83,15 @@ class strk_epipolar_grouper : public strk_epipolar_grouper_params
   bool fill_intensity_values(strk_epi_seg_sptr& seg);
   bool set_intensity_info();
   //members
-  double du_; 
+  double du_;
   double da_;
   double alpha_min_;
   double alpha_inv_;
   double smax_;
   int frame_; //the current frame index
   vgl_point_2d<double> epi_;//the epipole
-  vnl_double_3 upper_wedge_line_; 
-  vnl_double_3 lower_wedge_line_; 
+  vnl_double_3 upper_wedge_line_;
+  vnl_double_3 lower_wedge_line_;
   vil1_memory_image_of<float> image_; //the current frame
   vcl_vector<vtol_edge_2d_sptr> edges_;//edges for current frame
   // frames    segments
