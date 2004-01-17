109e3da73728539230c390cc92605d66f6d6b677
Matthew J. Leotta <mleotta@users.sourceforge.net>
2004-01-17 15:19:40 -0500

    added bmrf_network files

--- contrib/brl/bseg/bmrf/tests/test_include.cxx
+++ contrib/brl/bseg/bmrf/tests/test_include.cxx
@@ -1,6 +1,12 @@
 //if this file compiles, then the test passes
+#include <bmrf/bmrf_epi_point_sptr.h>
+#include <bmrf/bmrf_epi_point.h>
+#include <bmrf/bmrf_epi_seg_sptr.h>
+#include <bmrf/bmrf_epi_seg.h>
 #include <bmrf/bmrf_node_sptr.h>
 #include <bmrf/bmrf_node.h>
+#include <bmrf/bmrf_network_sptr.h>
+#include <bmrf/bmrf_network.h>
 
 
 int main() { return 0; }
