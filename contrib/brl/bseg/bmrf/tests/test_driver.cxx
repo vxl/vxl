0f67c7f5085e4ed5b65b0544e98174b13933e553
Matthew J. Leotta <mleotta@users.sourceforge.net>
2004-01-19 16:45:11 -0500

    added network tests

--- contrib/brl/bseg/bmrf/tests/test_driver.cxx
+++ contrib/brl/bseg/bmrf/tests/test_driver.cxx
@@ -1,15 +1,17 @@
 #include <testlib/testlib_register.h>
 
-DECLARE( test_node );
 DECLARE( test_epi_point );
 DECLARE( test_epi_seg );
+DECLARE( test_node );
+DECLARE( test_network );
 
 void
 register_tests()
 {
-  REGISTER ( test_node );
   REGISTER ( test_epi_point );
   REGISTER ( test_epi_seg );
+  REGISTER ( test_node );
+  REGISTER ( test_network );
 }
 
 DEFINE_MAIN;
