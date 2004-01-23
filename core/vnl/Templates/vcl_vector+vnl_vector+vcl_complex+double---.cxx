a0c80125478d341a0cb733cdf23ae8d47856d267
Ian Scott <scottim@users.sourceforge.net>
2004-01-23 05:27:57 -0500

    Suppresss compiler warnings deep in MSVC6.0 stndard library

--- core/vnl/Templates/vcl_vector+vnl_vector+vcl_complex+double---.cxx
+++ core/vnl/Templates/vcl_vector+vnl_vector+vcl_complex+double---.cxx
@@ -1,3 +1,9 @@
+#if (defined _MSC_VER) && _MSC_VER == 1200
+// disable warning buried deep in the standard library
+// warning C4018: '<' : signed/unsigned mismatch: vector(159,163,174)
+# pragma warning(disable: 4018)
+#endif
+
 #include <vcl_complex.h>
 #include <vnl/vnl_vector.h>
 #include <vcl_vector.txx>
