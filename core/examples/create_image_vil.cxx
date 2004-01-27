78df28725aedf23ec619836db98f767bdd959f3a
Ian Scott <scottim@users.sourceforge.net>
2004-01-27 08:25:18 -0500

    make example more robust

--- core/examples/create_image_vil.cxx
+++ core/examples/create_image_vil.cxx
@@ -4,27 +4,27 @@
 #include <vil/vil_image_view_base.h>
 #include <vil/vil_image_view.h>
 
-vil_image_view_base_sptr make_image(int wd, int ht)
+vil_image_view<unsigned char> make_image(int wd, int ht)
 {
   vil_image_view<unsigned char> image(wd, ht);
   for (int x = 0; x < wd; x++)
     for (int y = 0; y < ht; y++)
       image(x,y) = ((x-wd/2)*(y-ht/2)/16) % 256;
  
-  return &image;
+  return image;
 }
 
 int main(int argc, char** argv)
 {
-  vul_arg<vcl_string> output_filename(0, "output");
+  vul_arg<vcl_string> output_filename(0, "output filename");
   vul_arg_parse(argc, argv);
 
   int sizex = 253;
   int sizey = 155;
 
-  vil_image_view_base_sptr image_p = make_image(sizex, sizey);
+  vil_image_view<unsigned char> image = make_image(sizex, sizey);
 
-  vil_save( *image_p, output_filename().c_str());
+  vil_save( image, output_filename().c_str());
 
   return 0;
 }
