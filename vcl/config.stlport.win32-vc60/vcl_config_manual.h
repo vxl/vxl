a29327c6e43390d09b619469f45535c6f79699fc
Paul Smyth <paul_smyth@users.sourceforge.net>
2004-02-04 01:57:34 -0500

    #define of VCL_USE_STLPORT should have been updated to VCL_STLPORT

--- vcl/config.stlport.win32-vc60/vcl_config_manual.h
+++ vcl/config.stlport.win32-vc60/vcl_config_manual.h
@@ -10,6 +10,6 @@
 #define VCL_USE_IMPLICIT_TEMPLATES 1
 
 // Use STL port stl libraries.
-#define VCL_USE_STLPORT 1
+#define VCL_STLPORT 1
 
 #endif // vcl_config_manual_h_config_stlport_win32_vc60_
