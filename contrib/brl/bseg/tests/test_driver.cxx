382865b867516658dcda3d91eef33268e75df5b1
Joseph L. Mundy <mundyj@users.sourceforge.net>
2004-02-06 09:56:18 -0500

    added test for homography

--- contrib/brl/bseg/tests/test_driver.cxx
+++ contrib/brl/bseg/tests/test_driver.cxx
@@ -4,6 +4,7 @@ DECLARE(sdet_test_detector);
 DECLARE(sdet_test_region_proc);
 DECLARE(brip_test_lucas_kanade);
 DECLARE(brip_test_fourier);
+DECLARE(brip_test_homography);
 
 void
 register_tests()
@@ -12,6 +13,7 @@ register_tests()
   REGISTER(sdet_test_region_proc);
   REGISTER(brip_test_lucas_kanade);
   REGISTER(brip_test_fourier);
+  REGISTER(brip_test_homography);
 }
 
 DEFINE_MAIN;
