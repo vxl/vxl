4b1872a94eb8013d59e54df8dab43920709621c3
Joseph L. Mundy <mundyj@users.sourceforge.net>
2004-02-06 09:55:04 -0500

    added bounding box and homography methods

--- contrib/brl/bbas/bsol/bsol_algs.cxx
+++ contrib/brl/bbas/bsol/bsol_algs.cxx
@@ -2,8 +2,13 @@
 // \file
 #include <vsol/vsol_point_2d_sptr.h>
 #include <vsol/vsol_point_2d.h>
+#include <vsol/vsol_polygon_2d.h>
 #include <vsol/vsol_line_2d.h>
 #include <bsol/bsol_algs.h>
+#include <vsol/vsol_box_2d.h>
+#include <vsol/vsol_polygon_2d.h>
+#include <vgl/vgl_box_2d.h>
+
 
 
 //: Destructor
@@ -41,4 +46,62 @@ bool bsol_algs::meet(vsol_box_2d_sptr const & a, vsol_box_2d_sptr const & b)
     return false;
   return true;
 }
-  
+
+//:find the intersection of two boxes. Return false if no intersection
+bool bsol_algs::intersection(vsol_box_2d_sptr const & a,
+                             vsol_box_2d_sptr const & b,
+                             vsol_box_2d_sptr& a_int_b)
+{
+  vgl_point_2d<double> a_min(a->get_min_x(), a->get_min_y());
+  vgl_point_2d<double> a_max(a->get_max_x(), a->get_max_y());
+  vgl_box_2d<double> vga(a_min, a_max);
+
+  vgl_point_2d<double> b_min(b->get_min_x(), b->get_min_y());
+  vgl_point_2d<double> b_max(b->get_max_x(), b->get_max_y());
+  vgl_box_2d<double> vgb(b_min, b_max);
+  vgl_box_2d<double> temp = intersect(vga, vgb);
+  if(temp.is_empty())
+    return false;
+  a_int_b = new vsol_box_2d();
+  a_int_b->add_point(temp.min_x(), temp.min_y());
+  a_int_b->add_point(temp.max_x(), temp.max_y());
+  return true;
+}
+
+vsol_polygon_2d_sptr bsol_algs::poly_from_box(vsol_box_2d_sptr const& box)
+{
+  vcl_vector<vsol_point_2d_sptr> pts;
+  vsol_point_2d_sptr pa = new vsol_point_2d(box->get_min_x(), box->get_min_y());
+  vsol_point_2d_sptr pb = new vsol_point_2d(box->get_max_x(), box->get_min_y());
+  vsol_point_2d_sptr pc = new vsol_point_2d(box->get_max_x(), box->get_max_y());
+  vsol_point_2d_sptr pd = new vsol_point_2d(box->get_min_x(), box->get_max_y());
+  pts.push_back(pa);   pts.push_back(pb);
+  pts.push_back(pc);   pts.push_back(pd);
+  return new vsol_polygon_2d(pts); 
+}
+
+//: Transform a vsol_polygon_2d with a general homography.  Return false
+// if any of the points are turned into ideal points since vsol geometry
+// is assumed finite.
+bool bsol_algs::homography(vsol_polygon_2d_sptr const& p,
+                           vgl_h_matrix_2d<double> const& H,
+                           vsol_polygon_2d_sptr& Hp)
+{
+  int n = p->size();
+  vcl_vector<vsol_point_2d_sptr> pts;
+  double tol = 1e-06;
+  for(int i = 0; i<n; i++)
+    {
+      vsol_point_2d_sptr v = p->vertex(i);
+      vgl_homg_point_2d<double> hp(v->x(), v->y());
+      vgl_homg_point_2d<double> Hhp = H(hp);
+      if(Hhp.ideal(tol))
+        return false;
+      vgl_point_2d<double> q(Hhp);
+      vsol_point_2d_sptr qs = new vsol_point_2d(q.x(), q.y());
+      pts.push_back(qs);
+    }
+  Hp = new vsol_polygon_2d(pts);
+  return true;
+}
+      
--- contrib/brl/bbas/bsol/bsol_algs.h
+++ contrib/brl/bbas/bsol/bsol_algs.h
@@ -16,8 +16,9 @@
 //-----------------------------------------------------------------------------
 #include <vbl/vbl_bounding_box.h>
 #include <vsol/vsol_line_2d_sptr.h>
+#include <vsol/vsol_polygon_2d_sptr.h>
 #include <bsol/dll.h>
-
+#include <vgl/algo/vgl_h_matrix_2d.h>
 class bsol_algs
 {
  public:
@@ -26,8 +27,25 @@ class bsol_algs
   //: Finds a bounding box for a set of vsol_line_2ds
   static vbl_bounding_box<double,2> 
     bounding_box(vcl_vector<vsol_line_2d_sptr> const& lines);
+
   //:do two boxes intersect?
   static bool meet(vsol_box_2d_sptr const & a, vsol_box_2d_sptr const & b);
+
+
+  //:find the intersection of two boxes. Return false if no intersection
+  static bool intersection(vsol_box_2d_sptr const & a,
+                           vsol_box_2d_sptr const & b,
+                           vsol_box_2d_sptr& a_int_b);
+
+  //:construct a vsol_polygon from a vsol_box
+  static vsol_polygon_2d_sptr poly_from_box(vsol_box_2d_sptr const& box);
+
+  //: Transform a vsol_polygon_2d with a general homography
+  static bool homography(vsol_polygon_2d_sptr const& p,
+                  vgl_h_matrix_2d<double> const& H,
+                  vsol_polygon_2d_sptr& Hp);
+  
+
  private:
   bsol_algs();
 };
