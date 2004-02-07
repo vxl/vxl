9e15ceed0bba76fd420780ac51562b499677d3b8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-02-07 05:25:32 -0500

    brip now depends on bsol

--- contrib/brl/bseg/brip/config.mk
+++ contrib/brl/bseg/brip/config.mk
@@ -1,7 +1,10 @@
 IULIBS += -lbrip
+USES += contrib/gel
 include ${IUEROOT}/core/vil/config.mk
 include ${IUEROOT}/core/vil1/config.mk
 include ${IUEROOT}/core/vnl/algo/config.mk
 include ${IUEROOT}/core/vnl/config.mk
 include ${IUEROOT}/core/vbl/config.mk
 include ${IUEROOT}/core/vul/config.mk
+include ${IUEROOT}/contrib/gel/vsol/config.mk
+include ${IUEROOT}/contrib/brl/bbas/bsol/config.mk
