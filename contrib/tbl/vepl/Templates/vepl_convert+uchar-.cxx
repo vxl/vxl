dc8822db96182e1f4d3f0487ab447bd456bb99e3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-02-12 07:21:45 -0500

    simplification

--- contrib/tbl/vepl/Templates/vepl_convert+uchar-.cxx
+++ contrib/tbl/vepl/Templates/vepl_convert+uchar-.cxx
@@ -1,5 +1,4 @@
 #ifndef INSTANTIATE_TEMPLATES
 #include <vepl/vepl_convert.txx>
-typedef unsigned char ubyte;
-template vil1_image vepl_convert(vil1_image const&, ubyte);
+template vil1_image vepl_convert(vil1_image const&, unsigned char);
 #endif
