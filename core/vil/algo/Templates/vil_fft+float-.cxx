f24fba7e24312062cb28933d7208d71f52837c9c
Ian Scott <scottim@users.sourceforge.net>
2004-02-16 09:29:17 -0500

    Quash warnings

--- core/vil/algo/Templates/vil_fft+float-.cxx
+++ core/vil/algo/Templates/vil_fft+float-.cxx
@@ -1,2 +1,8 @@
+#if (defined _MSC_VER) && _MSC_VER == 1200
+// disable warning buried deep in the standard library
+// warning C4018: '<' : signed/unsigned mismatch: vector(159)
+# pragma warning(disable: 4018)
+#endif
+
 #include <vil/algo/vil_fft.txx>
 VIL_FFT_INSTANTIATE(float);
