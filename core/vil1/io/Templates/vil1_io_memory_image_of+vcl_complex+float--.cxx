a57171ed2613393e64b07b49d6f6803f38971dd4
Ian Scott <scottim@users.sourceforge.net>
2004-02-19 06:18:53 -0500

    Disable warnings deep in MSVC's STL implementation

--- core/vil1/io/Templates/vil1_io_memory_image_of+vcl_complex+float--.cxx
+++ core/vil1/io/Templates/vil1_io_memory_image_of+vcl_complex+float--.cxx
@@ -1,3 +1,9 @@
+#if (defined _MSC_VER) && _MSC_VER == 1200
+// disable warning buried deep in the standard library
+// warning C4018: '<' : signed/unsigned mismatch: vector(159)
+# pragma warning(disable: 4018)
+#endif
+
 #include <vcl_complex.h>
 #include <vil1/io/vil1_io_memory_image_of.txx>
 
