f5c7ff7633f04c12a761a943481a5de88cf6bd27
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-03-08 06:01:41 -0500

    qualified name in member declaration is nonstandard

--- contrib/brl/bbas/bsol/bsol_point_index_2d.h
+++ contrib/brl/bbas/bsol/bsol_point_index_2d.h
@@ -7,7 +7,6 @@
 // \brief A distance index for 2-d vsol points
 //
 //  Indexes points in a 2-d array to support proximity and matching queries
-//  
 //
 // \verbatim
 //  Modifications
@@ -16,25 +15,23 @@
 //
 //-----------------------------------------------------------------------------
 
-#include <vcl_iostream.h>
 #include <vcl_vector.h>
-#include <vbl/vbl_array_2d.h>
 #include <vsol/vsol_box_2d_sptr.h>
 #include <vsol/vsol_point_2d_sptr.h>
 
 class bsol_point_index_2d
 {
  public:
-  bsol_point_index_2d(){}//not meaningful
+  bsol_point_index_2d() {} //not meaningful
   bsol_point_index_2d(int nrows, int ncols, vsol_box_2d_sptr const& bb);
-  bsol_point_index_2d(int nrows, int ncols, 
-                   vcl_vector<vsol_point_2d_sptr> const& points);
+  bsol_point_index_2d(int nrows, int ncols,
+                      vcl_vector<vsol_point_2d_sptr> const& points);
   ~bsol_point_index_2d();
   //:accessors
-  int ncols(){return ncols_;}
-  int nrows(){return nrows_;}
-  double row_spacing(){return row_spacing_;}
-  double col_spacing(){return col_spacing_;}
+  int ncols() { return ncols_; }
+  int nrows() { return nrows_; }
+  double row_spacing() { return row_spacing_; }
+  double col_spacing() { return col_spacing_; }
 
   //: origin of the index space
   void origin(double& x0, double& y0);
@@ -46,14 +43,12 @@ class bsol_point_index_2d
   //: total number of points in the index
   int n_points();
 
-
   //:the points in an index cell
   vcl_vector<vsol_point_2d_sptr> points(const int row, const int col);
 
-
   //:all points in the index
   vcl_vector<vsol_point_2d_sptr> points();
-  
+
   //: the box corresponding to an index cell, r, c
   vsol_box_2d_sptr index_cell(const int row, const int col);
 
@@ -61,7 +56,7 @@ class bsol_point_index_2d
   vsol_box_2d_sptr index_cell(const double x, const double y);
 
   //: the box corresponding to the bounds of all points in the index
-  vsol_box_2d_sptr bsol_point_index_2d::point_bounds();
+  vsol_box_2d_sptr point_bounds();
 
   //:mutators
   bool add_point(vsol_point_2d_sptr const& p);
@@ -69,27 +64,31 @@ class bsol_point_index_2d
 
   //:mark as not in index, but point remains. Useful for matching
   bool mark_point(vsol_point_2d_sptr&  p);
-  //:clear mark 
+  //:clear mark
   bool unmark_point(vsol_point_2d_sptr& p);
   //:Is a point marked
   bool marked(vsol_point_2d_sptr const& p);
 
-  //:spatial queries
+  //spatial queries
+
   //:find a point in the index
   bool find_point(vsol_point_2d_sptr const& p);
+
   //:find the points within a radius of a point, possibly none
   bool in_radius(const double radius, vsol_point_2d_sptr const& p,
                  vcl_vector<vsol_point_2d_sptr>& points);
+
   //:find the closest point to p within a radius, possibly none
   bool closest_in_radius(const double radius, vsol_point_2d_sptr const& p,
                          vsol_point_2d_sptr& point);
 
   //:remove all points from the index, bounds remain the same
   void clear();
+
   //:clear marks
   void clear_marks();
- private:
 
+ private:
   bool trans(const double x, const double y,  //transform to index coords
              int& row, int& col);
 
