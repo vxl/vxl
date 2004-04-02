eeaaa2800b97450949b66f5e986dc5b781a99765
M. Laymon <mlaymon@users.sourceforge.net>
2004-04-02 09:46:59 -0500

    Fix delete of block_buffer in read_single_band_data. Was "delete", should be delete []".

--- core/vil/file_formats/vil_nitf.cxx
+++ core/vil/file_formats/vil_nitf.cxx
@@ -993,7 +993,7 @@ vil_memory_chunk_sptr vil_nitf_image::read_single_band_data(
              << " != bytes read = " << total_read_count << vcl_endl;
   }
 
-  delete block_buffer ;
+  delete [] block_buffer ;
 
 #if (defined DEBUG) && DEBUG
   const unsigned int DEFAULT_PIXELS_TO_DISPLAY = 8;
