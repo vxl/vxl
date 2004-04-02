5dd0d13feacdb1125af9fe65421ac674baeef1f8
Matthew J. Leotta <mleotta@users.sourceforge.net>
2004-04-02 14:29:24 -0500

    added a lot of code for curve reconstructing

--- contrib/brl/bseg/bmrf/bmrf_curve_3d_builder.cxx
+++ contrib/brl/bseg/bmrf/bmrf_curve_3d_builder.cxx
@@ -4,21 +4,72 @@
 
 #include "bmrf_curve_3d_builder.h"
 #include "bmrf_curvel_3d.h"
+#include "bmrf_network.h"
 #include "bmrf_node.h"
+#include "bmrf_epipole.h"
+#include "bmrf_gamma_func.h"
 
+#include <vgl/vgl_point_2d.h>
 #include <vnl/vnl_identity_3x3.h>
+#include <vnl/vnl_double_3.h>
+#include <vnl/vnl_double_4.h>
+#include <vnl/algo/vnl_svd.h>
 
+#undef MAX
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#undef MIN
+#define MIN(a,b) ((a)<(b)?(a):(b))
 
 //: Constructor
 bmrf_curve_3d_builder::bmrf_curve_3d_builder() 
- : network_(NULL)
+ : network_(NULL), min_alpha_(0.0), max_alpha_(0.0)
 {
+  init_intrinsic();
 }
 
 //: Constructor
 bmrf_curve_3d_builder::bmrf_curve_3d_builder(bmrf_network_sptr network)
- : network_(network)
+ : network_(network), min_alpha_(0.0), max_alpha_(0.0)
 {
+  init_intrinsic();
+  init_cameras();
+}
+
+
+//: Initialize the intrinsic camera parameters
+void 
+bmrf_curve_3d_builder::init_intrinsic()
+{
+  // For our camera images are 1024 x 768
+  // The focal length is 12.5mm / (6.25 um/pixel) = 2000 pixels
+  K_[0][0] = 2000;  K_[0][1] = 0;     K_[0][2] = 512;
+  K_[1][0] = 0;     K_[1][1] = 2000;  K_[1][2] = 384;
+  K_[2][0] = 0;     K_[2][1] = 0;     K_[2][2] = 1;
+}
+
+
+//: Initialize the camera matrices
+void 
+bmrf_curve_3d_builder::init_cameras()
+{
+  C_.clear();
+  if(!network_)
+    return;
+  int num_frames = network_->num_frames();
+  const vgl_point_2d<double>& ep = network_->epipole(1).location();
+
+  vnl_double_3x4 E;
+  E[0][0] = 1;  E[0][1] = 0;  E[0][2] = 0;  E[0][3] = ep.x();
+  E[1][0] = 0;  E[1][1] = 1;  E[1][2] = 0;  E[1][3] = ep.y();
+  E[2][0] = 0;  E[2][1] = 0;  E[2][2] = 1;  E[2][3] = 1;
+
+  for (int f=0; f<num_frames; ++f){
+    vnl_double_3x4 Ef = K_*E;
+    Ef[0][3] = -f*ep.x();
+    Ef[1][3] = -f*ep.y();
+    Ef[2][3] = -f;
+    C_.push_back(Ef);
+  }
 }
 
 
@@ -27,28 +78,280 @@ void
 bmrf_curve_3d_builder::set_network(const bmrf_network_sptr& network)
 {
   network_ = network;
+  init_cameras();
+}
+
+
+//: Return the constructed curves
+vcl_set<vcl_list<bmrf_curvel_3d_sptr> > 
+bmrf_curve_3d_builder::curves() const
+{
+  return curves_;
 }
 
 
 //: Build The curves
-vcl_set<vcl_list<bmrf_curvel_3d_sptr> > 
+bool
 bmrf_curve_3d_builder::build()
 {
-  vcl_set<vcl_list<bmrf_curvel_3d_sptr> > curves;
-  vcl_cout << "Building curves" << vcl_endl;
-  vcl_list<bmrf_curvel_3d_sptr> c1, c2;
+  if (!network_)
+    return false;
 
-  vnl_double_3x3 I = vnl_identity_3x3();
-  c1.push_back( new bmrf_curvel_3d(10.0, 10.0, 10.0, I) );
-  c1.push_back( new bmrf_curvel_3d(5.0, 15.0, 10.0, I) );
-  c1.push_back( new bmrf_curvel_3d(0.0, 15.0, 20.0, I) );
-  curves.insert(c1);
+  int num_frames = network_->num_frames();
+  if (num_frames < 2)
+    return false;
 
-  c2.push_back( new bmrf_curvel_3d(5.0, 15.0, 30.0, I) );
-  c2.push_back( new bmrf_curvel_3d(10.0, 15.0, 40.0, I) );
-  c2.push_back( new bmrf_curvel_3d(15.0, 10.0, 40.0, I) );
-  c2.push_back( new bmrf_curvel_3d(20.0, 10.0, 40.0, I) );
-  curves.insert(c2);
+  find_alpha_bounds();
 
-  return curves;
+  double a_init = (min_alpha_ + max_alpha_)*0.5;
+  vcl_list<vcl_list<bmrf_curvel_3d_sptr>*> growing_curves;
+
+  vcl_list<bmrf_curvel_3d_sptr> init_curvels = build_curvels(min_alpha_);
+  for ( vcl_list<bmrf_curvel_3d_sptr>::iterator itr = init_curvels.begin();
+        itr != init_curvels.end(); ++itr) 
+  {
+    vcl_list<bmrf_curvel_3d_sptr> new_curve;
+    new_curve.push_back(*itr);
+    //curves_.insert(new_curve);
+    growing_curves.push_back(&(*(curves_.insert(new_curve).first)));
+  }
+
+  for( double alpha = min_alpha_+0.001; alpha < max_alpha_; alpha += 0.001 ) {
+    // find all curvels
+    vcl_list<bmrf_curvel_3d_sptr> curvels = build_curvels(alpha);
+    this->append_curvels(curvels, growing_curves);
+  }
+
+  for ( vcl_set<vcl_list<bmrf_curvel_3d_sptr> >::iterator itr = curves_.begin();
+        itr != curves_.end();)
+  {
+    vcl_set<vcl_list<bmrf_curvel_3d_sptr> >::iterator next_itr = itr;
+    ++next_itr;
+    if( itr->size() < 10 )
+      curves_.erase(itr);
+    itr = next_itr;
+  }
+
+#if 0
+  int i = 0;
+  for ( vcl_list<bmrf_curvel_3d_sptr>::iterator a_itr = init_curvels.begin();
+        a_itr != init_curvels.end(); ++a_itr, ++i )
+  {
+    vcl_cout << "curve #" << i << vcl_endl;
+    vcl_cout << *(*a_itr) << vcl_endl;
+    vnl_double_4 hpt3 = vnl_double_4((*a_itr)->x(),(*a_itr)->y(),(*a_itr)->z(),1);
+    for ( int f = 0; f < num_frames; ++f){
+      vnl_double_2 pos;
+      if( (*a_itr)->pos_in_frame(f, pos) ){
+        vnl_double_3 hpt2 = C_[f]*hpt3;
+        vcl_cout << "at frame " << f << " pos=" << pos 
+                 << " <--> "<< hpt2[0]/hpt2[2] << ' '<< hpt2[1]/hpt2[2] << vcl_endl;
+      }
+    }
+  }
+#endif 
+
+  return true;
+}
+
+
+//: Determine the alpha bounds from the network
+void 
+bmrf_curve_3d_builder::find_alpha_bounds()
+{
+  min_alpha_ = 4.0;  // greater than pi
+  max_alpha_ = -4.0; // less than -pi
+  for ( bmrf_network::seg_node_map::const_iterator itr = network_->begin();
+        itr != network_->end();  ++itr )
+  {
+    min_alpha_ = MIN(min_alpha_, itr->first->min_alpha());
+    max_alpha_ = MAX(max_alpha_, itr->first->max_alpha());
+  }
+}
+
+
+//: Build curvels by matching curves in all frames at \p alpha
+vcl_list<bmrf_curvel_3d_sptr> 
+bmrf_curve_3d_builder::build_curvels(double alpha, int min)
+{
+  int num_frames = network_->num_frames();
+
+  vcl_list<bmrf_curvel_3d_sptr> all_curvels;
+  vcl_list<bmrf_curvel_3d_sptr> prev_curvels;
+
+  // find all intersecting curves
+  vcl_map<double, bmrf_node_sptr> matches = find_curves_at(alpha, 0);
+  for ( vcl_map<double, bmrf_node_sptr>::iterator itr = matches.begin();
+        itr != matches.end(); ++itr )
+  {
+    bmrf_curvel_3d_sptr curvel = new bmrf_curvel_3d();
+    curvel->set_proj_in_frame(0, alpha, itr->second);
+    prev_curvels.push_back(curvel);
+    all_curvels.push_back(curvel);
+  }
+
+  for ( int frame = 1; frame < num_frames; ++frame ) {
+    vcl_list<bmrf_curvel_3d_sptr> curr_curvels;
+    // find all intersecting curves
+    vcl_map<double, bmrf_node_sptr> matches = find_curves_at(alpha, frame);
+    for ( vcl_map<double, bmrf_node_sptr>::iterator itr = matches.begin();
+          itr != matches.end(); ++itr )
+    {
+      bmrf_curvel_3d_sptr curvel = best_match(itr->second, prev_curvels, alpha);
+      if( !curvel ){
+        curvel = new bmrf_curvel_3d();
+        all_curvels.push_back(curvel);
+      }
+      curvel->set_proj_in_frame(frame, alpha, itr->second);
+      curr_curvels.push_back(curvel);
+    }
+    prev_curvels = curr_curvels;
+  }
+
+  // remove those with less than 'min' projections
+  // triangulate 3D points for the rest
+  for ( vcl_list<bmrf_curvel_3d_sptr>::iterator itr = all_curvels.begin();
+        itr != all_curvels.end(); ) 
+  {
+    vcl_list<bmrf_curvel_3d_sptr>::iterator next_itr = itr;
+    ++next_itr;
+    
+    if((*itr)->num_projections() < 3)
+      all_curvels.erase(itr);
+    else{
+      this->reconstruct_3d(*itr);
+    }
+
+    itr = next_itr;
+  }
+
+  return all_curvels;
+}
+
+
+//: Find all curves that intersect \p alpha in \p frame
+vcl_map<double, bmrf_node_sptr> 
+bmrf_curve_3d_builder::find_curves_at(double alpha, int frame)
+{
+  vcl_map<double, bmrf_node_sptr> matches;
+  for ( bmrf_network::seg_node_map::const_iterator itr = network_->begin(frame);
+        itr != network_->end(frame);  ++itr )
+  {
+    if ((itr->first->min_alpha() <= alpha) &&
+        (itr->first->max_alpha() >= alpha))
+    {
+      matches[itr->first->s(alpha)] = (itr->second);
+    }
+  }
+  return matches;
+}
+
+
+
+//: return the curvel in \p list that best matches \p node at \p alpha
+bmrf_curvel_3d_sptr 
+bmrf_curve_3d_builder::best_match( const bmrf_node_sptr& node, 
+                                   const vcl_list<bmrf_curvel_3d_sptr>& list,
+                                   double alpha ) const
+{
+  for ( bmrf_node::arc_iterator itr = node->begin(bmrf_node::TIME);
+        itr != node->end(bmrf_node::TIME); ++itr )
+  {
+    bmrf_node_sptr neighbor = (*itr)->to();
+    if ( (neighbor->epi_seg()->min_alpha() > alpha) ||
+         (neighbor->epi_seg()->max_alpha() < alpha) )
+      continue;
+
+    for ( vcl_list<bmrf_curvel_3d_sptr>::const_iterator l_itr = list.begin();
+          l_itr != list.end();  ++l_itr )
+    {
+      if ((*l_itr)->is_projection(neighbor))
+        return *l_itr;
+    }
+  }
+  return NULL;
+}
+
+
+//: Reconstruct the 3d location of a curvel from its projections
+void
+bmrf_curve_3d_builder::reconstruct_3d(bmrf_curvel_3d_sptr curvel) const
+{
+  unsigned int num_frames = network_->num_frames();
+  unsigned int nviews = curvel->num_projections();
+
+  vnl_matrix<double> A(2*nviews, 4, 0.0);
+
+  unsigned int v = 0;
+  for (unsigned int f = 0; f<num_frames; ++f){
+    vnl_double_2 pos;
+    if ( curvel->pos_in_frame(f,pos) ){
+      for (unsigned int i=0; i<4; i++) {
+        A[2*v  ][i] = pos[0]*C_[f][2][i] - C_[f][0][i];
+        A[2*v+1][i] = pos[1]*C_[f][2][i] - C_[f][1][i];
+      }
+      ++v;
+    }
+  }
+  vnl_svd<double> svd_solver(A);
+  vnl_double_4 p = svd_solver.nullvector();
+
+  curvel->set(p[0]/p[3], p[1]/p[3], p[2]/p[3]);
+}
+
+
+//: Match the \p curvels to the ends of the \p growing_curves
+void 
+bmrf_curve_3d_builder::append_curvels(vcl_list<bmrf_curvel_3d_sptr> curvels, 
+                                      vcl_list<vcl_list<bmrf_curvel_3d_sptr>*>& growing_curves)
+{
+  vcl_list<vcl_list<bmrf_curvel_3d_sptr>*> grown_curves;
+  for( vcl_list<vcl_list<bmrf_curvel_3d_sptr>*>::iterator g_itr = growing_curves.begin();
+       g_itr != growing_curves.end();  ++g_itr )
+  {
+    bmrf_curvel_3d_sptr end_curvel = (*g_itr)->back();
+    bool found_match = false;
+    for ( vcl_list<bmrf_curvel_3d_sptr>::iterator c_itr = curvels.begin();
+          c_itr != curvels.end();  ++c_itr )
+    {
+      if ( this->append_correct((*c_itr), end_curvel) ){
+        (*g_itr)->push_back((*c_itr));
+        grown_curves.push_back(*g_itr);
+        curvels.erase(c_itr);
+        found_match = true;
+        break;
+      }
+    }
+  }
+  // make new curves for the unmatched curvels
+  for ( vcl_list<bmrf_curvel_3d_sptr>::iterator c_itr = curvels.begin();
+        c_itr != curvels.end();  ++c_itr )
+  {
+    vcl_list<bmrf_curvel_3d_sptr> new_curve;
+    new_curve.push_back(*c_itr);
+    grown_curves.push_back(&(*(curves_.insert(new_curve).first)));
+  }
+
+  growing_curves = grown_curves;
+}
+
+
+//: Return true if \p new_c is a reasonable match to \p prev_c 
+bool 
+bmrf_curve_3d_builder::append_correct( const bmrf_curvel_3d_sptr& new_c, 
+                                       const bmrf_curvel_3d_sptr& prev_c ) const
+{
+  unsigned int num_frames = network_->num_frames();
+  unsigned int total_overlap = 0;
+  unsigned int total_equal = 0;
+  for (unsigned int f = 0; f<num_frames; ++f){
+    bmrf_node_sptr p_node = prev_c->node_at_frame(f);
+    bmrf_node_sptr n_node = new_c->node_at_frame(f);
+    if( p_node && n_node ){
+      ++total_overlap;
+      if( p_node == n_node )
+        ++total_equal;
+    }
+  }
+  return float(total_equal)/float(total_equal) > 0.5;
 }
--- contrib/brl/bseg/bmrf/bmrf_curve_3d_builder.h
+++ contrib/brl/bseg/bmrf/bmrf_curve_3d_builder.h
@@ -13,8 +13,13 @@
 
 #include <bmrf/bmrf_curvel_3d_sptr.h>
 #include <bmrf/bmrf_network_sptr.h>
+#include <bmrf/bmrf_node_sptr.h>
+#include <vnl/vnl_double_3x3.h>
+#include <vnl/vnl_double_3x4.h>
 #include <vcl_set.h>
 #include <vcl_list.h>
+#include <vcl_vector.h>
+#include <vcl_map.h>
 
 //: A 3D curve builder
 class bmrf_curve_3d_builder 
@@ -30,12 +35,58 @@ class bmrf_curve_3d_builder
   void set_network(const bmrf_network_sptr& network);
 
   //: Build The curves
-  vcl_set<vcl_list<bmrf_curvel_3d_sptr> > build();
+  bool build();
+
+  //: Return the constructed curves
+  vcl_set<vcl_list<bmrf_curvel_3d_sptr> > curves() const;
+
+ protected:
+  //: Initialize the intrinsic camera parameters
+  void init_intrinsic();
+
+  //: Initialize the camera matrices
+  void init_cameras();
+
+  //: Determine the alpha bounds from the network
+  void find_alpha_bounds();
+
+  //: Build curvels by matching curves in all frames at \p alpha
+  //: curves with less than \p min projections are removed
+  vcl_list<bmrf_curvel_3d_sptr> build_curvels(double alpha, int min = 3);
+
+  //: Find all curves that intersect \p alpha in \p frame
+  vcl_map<double, bmrf_node_sptr> find_curves_at(double alpha, int frame = -1);
+
+  //: return the curvel in \p list that best matches \p node at \p alpha
+  bmrf_curvel_3d_sptr best_match(const bmrf_node_sptr& node, 
+                                 const vcl_list<bmrf_curvel_3d_sptr>& list,
+                                 double alpha) const;
+
+  //: Reconstruct the 3d location of a curvel from its projections
+  void reconstruct_3d(bmrf_curvel_3d_sptr curvel) const;
+
+  //: Match the \p curvels to the ends of the \p growing_curves
+  void append_curvels(vcl_list<bmrf_curvel_3d_sptr> curvels, 
+                      vcl_list<vcl_list<bmrf_curvel_3d_sptr>*>& growing_curves);\
+
+  //: Return true if \p new_c is a reasonable match to \p prev_c 
+  bool append_correct(const bmrf_curvel_3d_sptr& new_c, const bmrf_curvel_3d_sptr& prev_c) const;
 
  protected:
   //: The network
   bmrf_network_sptr network_;
 
+  //: Bounds on the alpha values in the network;
+  double min_alpha_;
+  double max_alpha_;
+
+  vcl_set<vcl_list<bmrf_curvel_3d_sptr> > curves_;
+
+  //: Camera intrinsic parameters
+  vnl_double_3x3 K_;
+
+  //: Vector of cameras
+  vcl_vector<vnl_double_3x4> C_;
 };
 
 #endif // bmrf_curve_3d_builder_h_
