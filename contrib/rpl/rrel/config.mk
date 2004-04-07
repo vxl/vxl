6ff0064e8513f3b205873052b549a091a94255ba
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-04-07 06:53:22 -0400

    mbl_mz_random was moved to vnl_random

--- contrib/rpl/rrel/config.mk
+++ contrib/rpl/rrel/config.mk
@@ -1,5 +1,4 @@
 IULIBS += -lrrel
-include ${IUEROOT}/contrib/mul/mbl/config.mk
 include ${IUEROOT}/core/vnl/algo/config.mk
 include ${IUEROOT}/core/vgl/config.mk
 include ${IUEROOT}/core/vbl/config.mk
