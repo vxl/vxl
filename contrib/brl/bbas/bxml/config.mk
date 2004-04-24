0d9be2fcd726cef2096c2b5eabfb3f7f63e0ebcc
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-04-24 05:21:08 -0400

    use $(XERCES_LIBS) instead of -lxerces-c

--- contrib/brl/bbas/bxml/config.mk
+++ contrib/brl/bbas/bxml/config.mk
@@ -1,16 +1,12 @@
 ifneq (,$(wildcard ${IUEROOT}/v3p/xerces-c-src1_4_0/include/dom/DOM.hpp))
  HAS_XERCES:=1
- INCDIRS += ${IUEROOT}/v3p/xerces-c-src1_4_0/include
- LIBDIRS += ${IUEROOT}/v3p/$(tj_libdir)
-endif
-
-ifdef HAS_XERCES
-IULIBS += -lbxml -lxerces-c
-USES += contrib/gel
-include ${IUEROOT}/contrib/brl/bbas/bdgl/config.mk
-include ${IUEROOT}/contrib/gel/vtol/config.mk
-include ${IUEROOT}/contrib/gel/vdgl/config.mk
-include ${IUEROOT}/contrib/gel/vsol/config.mk
-include ${IUEROOT}/core/vul/config.mk
-include ${IUEROOT}/core/vbl/config.mk
+ include ${IUEROOT}/config/top-params.mk
+ IULIBS += -lbxml $(XERCES_LIBS)
+ USES += contrib/gel
+ include ${IUEROOT}/contrib/brl/bbas/bdgl/config.mk
+ include ${IUEROOT}/contrib/gel/vtol/config.mk
+ include ${IUEROOT}/contrib/gel/vdgl/config.mk
+ include ${IUEROOT}/contrib/gel/vsol/config.mk
+ include ${IUEROOT}/core/vul/config.mk
+ include ${IUEROOT}/core/vbl/config.mk
 endif
