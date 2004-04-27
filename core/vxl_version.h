0a781a9adee06f9fddd52de082e226fb99e34937
VXL Robots <vxl_robots@users.sourceforge.net>
2004-04-26 21:50:32 -0400

    daily version number

--- core/vxl_version.h
+++ core/vxl_version.h
@@ -28,7 +28,7 @@
 // Formats are year=CCYY, month=MM, day=DD
 #define VXL_VERSION_DATE_YEAR 2004
 #define VXL_VERSION_DATE_MONTH 04
-#define VXL_VERSION_DATE_DAY 24
+#define VXL_VERSION_DATE_DAY 26
 
 //: This can either be "RELEASE" or "CVS"
 #define VXL_SOURCE "CVS"
