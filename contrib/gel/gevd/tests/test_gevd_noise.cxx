0ece27b314fd46e2a7af1957e6d77ac479bb26d8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-04-28 07:31:33 -0400

    try to solve test failure on Intel: print out sensor_noise and texture_noise

--- contrib/gel/gevd/tests/test_gevd_noise.cxx
+++ contrib/gel/gevd/tests/test_gevd_noise.cxx
@@ -11,6 +11,7 @@
 #include <gevd/gevd_noise.h>
 #include <testlib/testlib_test.h>
 #include <vnl/vnl_sample.h>
+#include <vcl_iostream.h>
 
 void
 test_gevd_noise()
@@ -22,6 +23,8 @@ test_gevd_noise()
   gevd_noise noise_estim(data,size);
   float sensor_noise, texture_noise;
   noise_estim.EstimateSensorTexture(sensor_noise, texture_noise);
+  vcl_cout << "sensor_noise = " << sensor_noise
+           << ", texture_noise = " << texture_noise << vcl_endl;
   TEST("gevd_noise", sensor_noise <= 2 && sensor_noise >= 0 &&
                      texture_noise <= 2 && texture_noise >= 0, true);
 }
