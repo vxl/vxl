94d904043292960c929c641a958e0d8188ae02f1
Ian Scott <scottim@users.sourceforge.net>
2004-04-28 06:09:27 -0400

    Nobody should be using mbl_mz_random anymore. I'll leave the file in with a compiler error for a few months. Not going through the deprecation process since it isn't in core

--- contrib/mul/mbl/mbl_mz_random.h
+++ contrib/mul/mbl/mbl_mz_random.h
@@ -3,10 +3,8 @@
 #define mbl_mz_random_h
 //:
 // \file
+// \brief Use vnl_random instead.
 
-#include <vnl/vnl_random.h>
-
-typedef vnl_random mbl_mz_random;
-
+extern "ERROR: use vnl_random instead"
 
 #endif // mbl_mz_random_h
