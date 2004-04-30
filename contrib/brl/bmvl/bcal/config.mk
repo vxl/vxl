33ed715cd2bbc250656814a298614daafd785ab7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-04-30 05:16:38 -0400

    bcal does not directly depend on vbl

--- contrib/brl/bmvl/bcal/config.mk
+++ contrib/brl/bmvl/bcal/config.mk
@@ -3,4 +3,3 @@ USES += contrib/gel
 include ${IUEROOT}/contrib/gel/vsol/config.mk
 include ${IUEROOT}/core/vcsl/config.mk
 include ${IUEROOT}/core/vnl/config.mk
-include ${IUEROOT}/core/vbl/config.mk
