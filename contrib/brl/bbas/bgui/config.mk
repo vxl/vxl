dab3ba5a03920e5712129f0b1e9a60b0089e4def
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-04-30 05:16:09 -0400

    bgui does not directly depend on vul nor vbl

--- contrib/brl/bbas/bgui/config.mk
+++ contrib/brl/bbas/bgui/config.mk
@@ -6,6 +6,4 @@ include ${IUEROOT}/contrib/gel/vsol/config.mk
 include ${IUEROOT}/contrib/gel/vtol/config.mk
 include ${IUEROOT}/core/vgui/config.mk
 include ${IUEROOT}/core/vgl/algo/config.mk
-include ${IUEROOT}/core/vbl/config.mk
-include ${IUEROOT}/core/vul/config.mk
 include ${IUEROOT}/core/vil/algo/config.mk
