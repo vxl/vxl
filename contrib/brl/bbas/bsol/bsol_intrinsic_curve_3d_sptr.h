0e4ad21121025d81f27b1a047f6b181c5cb44d57
MingChing Chang <mcchang@users.sourceforge.net>
2004-04-30 17:36:06 -0400

    Creation of bsol_intrinsic_curve_3d_sptr.h

---
+++ contrib/brl/bbas/bsol/bsol_intrinsic_curve_3d_sptr.h
@@ -0,0 +1,23 @@
+//*****************************************************************************
+// File name: bsol_intrinsic_curve_3d_sptr.h
+// Description: A smart pointer on a bsol_intrinsic_curve_3d
+//-----------------------------------------------------------------------------
+// Language: C++
+//
+// Version |Date      | Author                   |Comment
+// --------+----------+--------------------------+-----------------------------
+// 1.0     |2004/03/17| MingChing Chang          |Creation
+//*****************************************************************************
+#ifndef BSOL_INTRINSIC_CURVE_3D_REF_H
+#define BSOL_INTRINSIC_CURVE_3D_REF_H
+
+class bsol_intrinsic_curve_3d;
+
+//*****************************************************************************
+// External declarations for values
+//*****************************************************************************
+#include <vbl/vbl_smart_ptr.h>
+
+typedef vbl_smart_ptr<bsol_intrinsic_curve_3d> bsol_intrinsic_curve_3d_sptr;
+
+#endif // #ifndef BSOL_INTRINSIC_CURVE_3D_REF_H
