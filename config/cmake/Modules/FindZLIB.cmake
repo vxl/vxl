f7378ab138cb641356f30e4b0d4335d42a1a4898
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-05-02 17:41:22 -0400

    fixed an inconsistency: even if VXL_FORCE_V3P_ZLIB was set, still a native z library was being used in e.g. v3p/png

--- config/cmake/Modules/FindZLIB.cmake
+++ config/cmake/Modules/FindZLIB.cmake
@@ -10,35 +10,36 @@
 # ZLIB_FOUND       - TRUE, if available somewhere on the system.
 
 # Additionally
-# VXL_USING_NATIVE_ZLIB  - True if we are using a ZLIB library provided outside vxl (or v3p)
+# VXL_USING_NATIVE_ZLIB  - True if we are using a ZLIB library provided outside v3p
 
 
 # If this FORCE variable is unset or is FALSE, try to find a native library.
-IF( VXL_FORCE_V3P_ZLIB )
-ELSE( VXL_FORCE_V3P_ZLIB )
+IF (NOT VXL_FORCE_V3P_ZLIB)
   INCLUDE( ${CMAKE_ROOT}/Modules/FindZLIB.cmake )
-ENDIF( VXL_FORCE_V3P_ZLIB )
-
+ENDIF (NOT VXL_FORCE_V3P_ZLIB)
   
-IF(ZLIB_FOUND)
+IF (ZLIB_FOUND)
 
   SET(VXL_USING_NATIVE_ZLIB "YES")
   # All the other variables are set by CMake's FindZLIB. Don't
   # set them here.
 
-ELSE(ZLIB_FOUND)
+ELSE (ZLIB_FOUND)
 
   #
   # At some point, in a "release" version, it is possible that someone
-  # will not have the v3p ZLIB library, so make sure the headers
-  # exist.
+  # will not have the v3p ZLIB library, so make sure the headers exist.
   #
   
-  IF(EXISTS ${vxl_SOURCE_DIR}/v3p/zlib/zlib.h)
+  IF (EXISTS ${vxl_SOURCE_DIR}/v3p/zlib/zlib.h)
 
-    SET( ZLIB_FOUND "YES" )
-    SET( ZLIB_INCLUDE_DIR ${vxl_SOURCE_DIR}/v3p/zlib)  
-    SET( ZLIB_LIBRARIES z )
+    SET(ZLIB_INCLUDE_DIR ${vxl_SOURCE_DIR}/v3p/zlib CACHE PATH "What is the path where the file zlib.h can be found" FORCE)
+    FIND_LIBRARY( ZLIB_LIBRARY z ${LIBRARY_OUTPUT_PATH} ${vxl_BINARY_DIR}/v3p/zlib )
+    IF (ZLIB_LIBRARY)
+      SET(ZLIB_LIBRARY ${ZLIB_LIBRARY} CACHE FILEPATH "Where can the z library be found" FORCE)
+      SET(ZLIB_LIBRARIES ${ZLIB_LIBRARY})
+      SET(ZLIB_FOUND "YES")
+    ENDIF (ZLIB_LIBRARY)
   
-  ENDIF(EXISTS ${vxl_SOURCE_DIR}/v3p/zlib/zlib.h)
-ENDIF(ZLIB_FOUND)
+  ENDIF (EXISTS ${vxl_SOURCE_DIR}/v3p/zlib/zlib.h)
+ENDIF (ZLIB_FOUND)
