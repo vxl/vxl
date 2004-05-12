50a0215ede49f3accdfd396acfac6f1f763f9ef9
Martin G. Roberts <mgroberts@users.sourceforge.net>
2004-05-12 06:52:46 -0400

    dded some template binder type functors for map iterators along the lines of select1st select2nd

--- contrib/mul/mbl/mbl_stl.h
+++ contrib/mul/mbl/mbl_stl.h
@@ -9,7 +9,15 @@
 // Actually, this is mostly an opportunity to mess around in STL to produce code
 // which would be much simpler in ordinary C++. Stroustrup assures us that
 // this approach is faster in general - which I don't really believe.
-
+//
+// Added quite a few little functors mainly to do with iterating through maps
+// for example a version of the non-standard select1st and select2nd
+// 30 April 2004
+// Martin Roberts
+//
+#include <vcl_functional.h>
+#include <vcl_vector.h>
+// 
 //: Fill an output sequence with incrementing values.
 // A bit like vcl_fill, but after each assignment, the value is incremented.
 // \return the next value in the sequence.
@@ -55,5 +63,92 @@ inline T mbl_stl_sequence_n(Out first, Size n, UnOp op, T init)
   for (; 0 < n; ++first, --n, init = op(init)) *first = init;
   return init;
 }
+//----------------------------------------------------------------------------------------------
+//Now some map related functors
+//
+//: select 1st element of a pair (e.g. for map iterators)
+//NB something like this is in the SGI extension to the STL but is not included in the standard VCL
+//However this is very useful with map iterators so include it here
+template <class Pair>
+struct mbl_stl_select1st : public vcl_unary_function<Pair, typename Pair::first_type>
+{
+    inline typename Pair::first_type const & operator()(Pair const & pair) const
+    {
+        return pair.first;
+    }
+};
+//: select 2nd element of a pair (e.g. for map iterators)
+//NB something like this is in the SGI extension to the STL but is not included in the standard VCL
+//However this is very useful with map iterators so include it here
+template <class Pair>
+struct mbl_stl_select2nd : public vcl_unary_function<Pair, typename Pair::second_type>
+{
+    inline typename Pair::second_type const & operator()(Pair const & pair) const
+    {
+        return pair.second;
+    }
+};
+
+//Accumulate the second elements of a pair (e.g. for accumulating values through a map)
+template <class Pair>
+struct mbl_stl_add2nd : public vcl_binary_function<typename Pair::second_type, Pair, typename Pair::second_type>
+{
+    inline typename Pair::second_type  operator()(typename Pair::second_type partSum, Pair const & x2 ) const
+    {
+        return partSum + x2.second;
+    }
+};
+
+//Order a collection of pair iterators according to their dereferenced keys
+//NB assumes the key type supports operator<
+template <class PairIter>
+struct mbl_stl_pair_iter_key_order : public vcl_binary_function<PairIter,PairIter, bool>
+{
+    inline bool  operator()(const PairIter& iter1, const PairIter& iter2 ) const
+    {
+        return (iter1->first < iter2->first) ? true : false;
+    }
+};
+
+//Order a collection of pair iterators according to their dereferenced values
+//NB assumes the key type supports operator<
+template <class PairIter>
+struct mbl_stl_pair_iter_value_order : public vcl_binary_function<PairIter,PairIter, bool>
+{
+    inline bool  operator()(const PairIter& iter1, const PairIter& iter2 ) const
+    {
+        return (iter1->second < iter2->second) ? true : false ;
+    }
+};
+
+// End of map/pair related functors
+//------------------------------------------------------------------------------------
+//: Given a vector of things, select an indexed element
+//For use in eg STL transform algorithm to extract out required subset of (indexed) objects into a working vector
+//e.g. given vector of indices and vector of values, copy out the required subset thus
+// \code
+// vcl_vector<T> subset
+// subset.reserve(indices.size());
+// vcl_transform(indices.begin(),indices.end(),
+//               vcl_back_inserter(subset),
+//               mbl_stl_index_functor(values));
+// \endcode
+template <class T>
+class mbl_stl_index_functor  
+{
+    //This functor copies out  element vec[index]
+    //For use in eg STL transform algorithm to extract out required subset of (indexed) points into a working vector
+    //No bounds checking is done
+  private:
+    //:const reference to vector used to store the objects indexed
+    const vcl_vector<T >& vec_;
+
+  public:
+    mbl_stl_index_functor(const vcl_vector<T >& vec): vec_(vec) {}
+    inline const T& operator()(unsigned index) const
+        {return vec_[index];}
+	       
+};
+
 
 #endif // mbl_stl_h_
