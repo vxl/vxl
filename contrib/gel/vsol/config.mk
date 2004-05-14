90db7a82e9a67552e0f7ac023621d42afd4c050c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-05-14 08:48:50 -0400

    updated w.r.t. CMakeLists.txt

--- contrib/gel/vsol/config.mk
+++ contrib/gel/vsol/config.mk
@@ -1,6 +1,7 @@
 IULIBS += -lvsol
 include ${IUEROOT}/core/vgl/algo/config.mk
 include ${IUEROOT}/core/vnl/config.mk
+include ${IUEROOT}/core/vbl/io/config.mk
 include ${IUEROOT}/core/vbl/config.mk
 include ${IUEROOT}/core/vsl/config.mk
 include ${IUEROOT}/core/vul/config.mk
