ed4419fd5137bbbe2aae5d49c970943d1f9aa9ea
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-05-15 16:10:25 -0400

    reverting to the old situation; the problem was solved by moving #include <vsl/vsl_binary_loader.h> from vsol_spatial_object_2d.h to vsol_spatial_object_2d.cxx

--- contrib/gel/vsol/Templates/vbl_io_smart_ptr+vsol_line_2d-.cxx
+++ contrib/gel/vsol/Templates/vbl_io_smart_ptr+vsol_line_2d-.cxx
@@ -1,7 +1,3 @@
 #include <vsol/vsol_line_2d.h>
 #include <vbl/io/vbl_io_smart_ptr.txx>
- 
-vsl_binary_loader<vsol_line_2d>* vsl_binary_loader<vsol_line_2d>::instance_ = 0;
-vsl_binary_loader<vsol_line_2d>& vsl_binary_loader<vsol_line_2d>::instance() { return *instance_; }
-void vsl_binary_loader<vsol_line_2d>::load_object( vsl_b_istream&, vsol_line_2d*&) {}
 VBL_IO_SMART_PTR_INSTANTIATE(vsol_line_2d);
--- contrib/gel/vsol/Templates/vbl_io_smart_ptr+vsol_spatial_object_2d-.cxx
+++ contrib/gel/vsol/Templates/vbl_io_smart_ptr+vsol_spatial_object_2d-.cxx
@@ -1,4 +1,5 @@
 #include <vsol/vsol_spatial_object_2d.h>
+#include <vsl/vsl_binary_loader.h>
 #include <vbl/io/vbl_io_smart_ptr.txx>
  
 VBL_IO_SMART_PTR_INSTANTIATE(vsol_spatial_object_2d);
