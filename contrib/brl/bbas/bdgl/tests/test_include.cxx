3feb42ca49859445596a9ef7588719a51c7cfeb0
Vishal Jain <jainv@users.sourceforge.net>
2004-05-24 10:16:57 -0400

    removed bdgl_curve_tracking files

--- contrib/brl/bbas/bdgl/tests/test_include.cxx
+++ contrib/brl/bbas/bdgl/tests/test_include.cxx
@@ -1,18 +1,5 @@
 #include <bbas/bdgl/bdgl_curve_algs.h>
-#include <bbas/bdgl/bdgl_curve_description.h>
-#include <bbas/bdgl/bdgl_curve_matcher.h>
 #include <bbas/bdgl/bdgl_curve_region.h>
-#include <bbas/bdgl/bdgl_curve_tracker.h>
-#include <bbas/bdgl/bdgl_curve_tracker_primitive.h>
-#include <bbas/bdgl/bdgl_curve_clustering.h>
-#include <bbas/bdgl/bdgl_curve_matching.h>
-#include <bbas/bdgl/bdgl_curve_tracking.h>
-#include <bbas/bdgl/bdgl_tracker_curve.h>
-#include <bbas/bdgl/bdgl_tracker_curve_sptr.h>
-#include <bbas/bdgl/DPMatch.h>
-#include <bbas/bdgl/curve.h>
-#include <bbas/bdgl/curveMatch.h>
-#include <bbas/bdgl/utils.h>
 #include <bbas/bdgl/dll.h>
 
 int main() { return 0; }
