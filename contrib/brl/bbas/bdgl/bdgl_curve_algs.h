bfa247614111328e0a69538b507427910c4da46c
Vishal Jain <jainv@users.sourceforge.net>
2004-05-24 10:03:04 -0400

    removed transformed_euclidean_distance()

--- contrib/brl/bbas/bdgl/bdgl_curve_algs.h
+++ contrib/brl/bbas/bdgl/bdgl_curve_algs.h
@@ -19,7 +19,6 @@
 #include <vgl/vgl_point_2d.h>
 #include <vdgl/vdgl_edgel_chain_sptr.h>
 #include <vdgl/vdgl_digital_curve_sptr.h>
-#include <bdgl/bdgl_tracker_curve_sptr.h>
 class bdgl_curve_algs
 {
  public:
@@ -102,13 +101,7 @@ class bdgl_curve_algs
 
   static vdgl_digital_curve_sptr
     create_digital_curves(vcl_vector<vgl_point_2d<double> > & curve);
-  static double
-    compute_transformed_euclidean_distance(bdgl_tracker_curve_sptr c1,
-                                           bdgl_tracker_curve_sptr c2,
-                                           vnl_matrix<double> R,
-                                           vnl_matrix<double> T,
-                                           double s,
-                                           vcl_map<int,int> alignment);
+  
  private:
   bdgl_curve_algs();
 };
