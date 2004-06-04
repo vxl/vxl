9b0d5087fa01dc0a16b3b92af00701a3efb7dbf8
Brendan McCane <mccane@users.sourceforge.net>
2004-06-03 22:33:49 -0400

    Added a video-for-linux framegrabber, so should work with all those devices.

---
+++ contrib/oul/oufgl/examples/example1.cxx
@@ -0,0 +1,59 @@
+#include <vil1/vil1_memory_image_of.h>
+#include <vil1/vil1_save.h>
+#include <oufgl/frame_grabber_v4l.h>
+#include <vcl_iostream.h>
+#include <getopt.h>
+#include <vul/vul_timer.h>
+
+int main(int argc, char *argv[])
+{
+	extern char *optarg;
+	extern int optind;
+	int opt;
+    bool debug=false;
+    int width=320, height=240;
+    int num_to_grab=1;
+    char *camera_name = "/dev/video0";
+	while ((opt=getopt(argc, argv, "x:y:n:c:d"))!=EOF)
+    {
+        switch(opt)
+        {
+        case 'x':
+            width = atoi(optarg);
+            break;
+        case 'y':
+            height = atoi(optarg);
+            break;
+        case 'n':
+            num_to_grab = atoi(optarg);
+            break;
+        case 'c':
+            camera_name = strdup(optarg);
+            break;
+        case 'd':
+            debug = !debug;
+            break;
+        default:
+            vcl_cout << "Usage: example1 [-x width] [-y height] "
+                     << "[-n num_to_grab] [-c device_name] [-d]" << vcl_endl;
+            vcl_cout << "\t-d turn debug info on" << vcl_endl;
+            break;
+        }
+    }
+    FrameGrabberV4lGrey fg(width, height, debug, camera_name);
+
+    vul_timer t;
+
+    for (int i=0; i<num_to_grab; i++)
+        fg.acquire_frame_synch();
+
+    long time_taken = t.real();
+
+    vcl_cout << "That took " << time_taken << " ms\n";
+    vcl_cout << "which means "<< 1000.0*(double)num_to_grab/(double)time_taken 
+             << "frames per second\n";
+
+    // only save the last one
+    vil1_memory_image *img = fg.get_current_frame();
+    vil1_save(*img, "image.pgm");
+} 
---
+++ contrib/oul/oufgl/frame_grabber_v4l.h
@@ -0,0 +1,83 @@
+//-*-c++-*--------------------------------------------------------------
+//: \file frame_grabber_v4l.h: 
+// 
+// \brief Classes for video input under linux making use of the video
+// for linux stuff. This has been tested using a webcam running under
+// the Phillips Web Cam driver.
+//
+// Copyright (c) 2002 Brendan McCane
+// University of Otago, Dunedin, New Zealand
+// Reproduction rights limited as described in the COPYRIGHT file.
+//----------------------------------------------------------------------
+
+#ifndef OTAGO_frame_grabber_v4l__h_INCLUDED
+#define OTAGO_frame_grabber_v4l__h_INCLUDED
+
+#include <vil1/vil1_memory_image.h>
+#include <vil1/vil1_memory_image_of.h>
+#include <vil1/vil1_rgb.h>
+#include <vxl_config.h>
+#include <vcl_cstdio.h>
+#include <vcl_cstdlib.h>
+#include "asyncio.h"
+#include "frame_grabber.h"
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <linux/videodev.h>
+#include <vcl_iostream.h>
+
+//----------------------------------------------------------------------
+//: The FrameGrabberV4lGrey class 
+//
+// This class provides an interface with a v4l camera on a
+// linux system. I have not attempted at all to provide
+// cross-compatibility with linux and windows and indeed some
+// difficult questions are raised when hardware issues become
+// involved. 
+//
+// \status under development
+// \author Brendan McCane
+//----------------------------------------------------------------------
+
+
+class FrameGrabberV4lGrey : public FrameGrabber
+{
+ public:
+	typedef vxl_byte ImageContents; 
+	typedef vil1_memory_image_of< ImageContents > ImageGrey;
+	
+	FrameGrabberV4lGrey(int width_=384, int height_=288, bool _debug=false,
+						char *devname="/dev/video0");
+	virtual ~FrameGrabberV4lGrey();
+	// returns the current acquired frame
+	inline vil1_memory_image *get_current_frame(){
+        if (debug)
+        {
+            vcl_cout << "get_current_frame called\n";
+            vcl_cout << "current = " << current;
+            vcl_cout << " im = " << im[current] << vcl_endl;
+        }
+		return(im[current]);
+	};
+	// acquire a new frame synchronously (ie don't return until completed)
+	void acquire_frame_synch();
+	// acquire a new frame asynchronously (ie start acquiring and
+	// return immediately )
+	void acquire_frame_asynch();
+	// return the current frame and start acquiring the next one. This
+	// function is only useful if asynchronous acquires are going to
+	// be used.
+	vil1_memory_image *get_current_and_acquire();
+private:
+	ImageContents *contents[2];
+	ImageGrey *im[2];
+	int current;
+	AsyncIO *aio;
+	int width, height;
+	int fd;
+	void flip_current();
+    bool debug;
+};
+
+#endif
