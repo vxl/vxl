72f93d64c2674041c5cb9cec053a7930de607b81
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-07-08 04:42:22 -0400

    FMatrix is not used here

--- contrib/brl/bmvl/bmvv/bmvv_recon_manager.h
+++ contrib/brl/bmvl/bmvv/bmvv_recon_manager.h
@@ -22,7 +22,6 @@
 #include <vgui/vgui_grid_tableau.h>
 #include <vgui/vgui_grid_tableau_sptr.h>
 #include <vgui/vgui_style_sptr.h>
-#include <mvl/FMatrix.h>
 #include <bgui/bgui_vtol2D_tableau_sptr.h>
 #include <bgui/bgui_picker_tableau_sptr.h>
 #include <brct/brct_corr_sptr.h>
