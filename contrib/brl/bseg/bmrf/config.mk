fffef4b10525d67f02aee8b66bb457694a1c1e71
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-07-08 05:05:24 -0400

    updated w.r.t. CMakeLists.txt

--- contrib/brl/bseg/bmrf/config.mk
+++ contrib/brl/bseg/bmrf/config.mk
@@ -2,12 +2,10 @@ IULIBS += -lbmrf
 USES += contrib/gel
 include ${IUEROOT}/core/vil/config.mk
 include ${IUEROOT}/core/vgl/config.mk
-include ${IUEROOT}/core/vnl/config.mk
-include ${IUEROOT}/core/vbl/config.mk
+include ${IUEROOT}/core/vnl/algo/config.mk
+include ${IUEROOT}/core/vbl/io/config.mk
 include ${IUEROOT}/core/vsl/config.mk
 include ${IUEROOT}/contrib/gel/gevd/config.mk
 include ${IUEROOT}/contrib/gel/vdgl/config.mk
-include ${IUEROOT}/contrib/gel/vsol/config.mk
 include ${IUEROOT}/contrib/gel/vtol/config.mk
 include ${IUEROOT}/contrib/brl/bbas/bugl/config.mk
-include ${IUEROOT}/contrib/brl/bseg/brip/config.mk
