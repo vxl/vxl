6d63b9eb774195e37518c18ba82d517fddc13db1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-07-09 11:38:20 -0400

    dependency on bxml is commented out

--- contrib/brl/bseg/segv/config.mk
+++ contrib/brl/bseg/segv/config.mk
@@ -5,9 +5,6 @@ include ${IUEROOT}/contrib/brl/bseg/brip/config.mk
 include ${IUEROOT}/contrib/brl/bseg/strk/config.mk
 include ${IUEROOT}/contrib/brl/bbas/bgui/config.mk
 include ${IUEROOT}/contrib/brl/bbas/bsol/config.mk
-ifeq "$(HAS_XERCES)" "1"
- include ${IUEROOT}/contrib/brl/bbas/bxml/config.mk
-endif
 include ${IUEROOT}/contrib/gel/vtol/config.mk
 include ${IUEROOT}/contrib/gel/vdgl/config.mk
 include ${IUEROOT}/core/vil1/config.mk
