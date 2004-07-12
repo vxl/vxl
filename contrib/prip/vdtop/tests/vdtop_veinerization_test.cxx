0c0aa783a38146137c50ffb09a2c2953521664f5
Jocelyn Marchadier <jocelynmar@users.sourceforge.net>
2004-07-12 08:06:02 -0400

    first release

---
+++ contrib/prip/vdtop/tests/vdtop_veinerization_test.cxx
@@ -0,0 +1,53 @@
+// This is contrib/prip/vdtop/tests/vdtop_veinerization_test.cxx
+
+#include <vxl_config.h>
+#include <vcl_iostream.h>
+
+#include "vmap/vmap_2_tmap.h"
+#include "vdtop/vil_canny_deriche_grad_filter.h"
+#include "vdtop/vdtop_well_composed_lower_leveling_kernel.h"
+#include "vdtop/vdtop_set_4_veinerization_structure.h"
+
+#include <vil/vil_convert.h>
+#include <vil/vil_load.h>
+#include <vil/vil_save.h>
+#include "vdtop/vil_clamp.h"
+
+#include <vcl_vector.h>
+
+typedef vmap_2_tmap<> my_map_type ;
+
+int main()
+{
+	my_map_type map ;
+	vil_image_view<vxl_byte> img,res,res2;
+	img= vil_load("lena.org.pgm") ;
+	
+	vil_image_view<float> gradi ;
+	
+	float k=1.0;
+	vil_canny_deriche_grad_filter(img,gradi,k) ; 
+	vil_clamp_below(gradi,float(5.0),float(0.0)) ;
+	vil_convert_cast(gradi, res) ;
+	vil_save(res,"lena.grad.pgm");
+
+	vdtop_well_composed_lower_leveling_kernel(res) ;
+	vil_save(res,"lena.kernel.pgm");
+
+	vdtop_set_4_veinerization_structure(map,res) ;
+	
+	// removes pendant darts	
+	my_map_type::contraction_kernel kc(map) ;
+	kc.initialise();
+	kc.add_1_cycles() ;
+	kc.finalise() ;
+	map.contraction(kc) ;
+	
+	res2.set_size(img.ni(),img.nj()) ;
+	res2.fill(0) ;
+	for (my_map_type::vertex_iterator v=map.begin_vertex(); v!=map.end_vertex(); ++v)
+	{
+		res2.top_left_ptr()[map.position(*v)]=255 ;
+	}
+	vil_save(res2,"res.pgm") ;
+}
