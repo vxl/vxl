9da61c4b5b3e8ac7e892e358d35a218aab134107
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-07-16 09:26:00 -0400

    make sure that the tests can be run from other places than from the source directory

--- contrib/prip/vdtop/tests/vdtop_well_composed_kernel_test.cxx
+++ contrib/prip/vdtop/tests/vdtop_well_composed_kernel_test.cxx
@@ -10,19 +10,19 @@
 #include <vil/vil_load.h>
 #include <vil/vil_save.h>
 
-int main()
+int main(int argc, char* argv[])
 {
   vil_image_view<vxl_byte> img,res,res2;
-  img= vil_load("lena.org.pgm") ;
+  img= vil_load(argc>1 ? argv[1] : "lena.org.pgm") ;
   vil_image_view<float> gradi ;
 
   double k=1.5;
   vil_canny_deriche_grad_filter(img,gradi,k) ;
   vil_convert_cast(gradi, res) ;
-  vil_save(res,"lena.grad.pgm");
+  vil_save(res,argc>2 ? argv[2] : "lena.grad.pgm");
 
   vdtop_well_composed_lower_leveling_kernel(res) ;
   vdtop_replace_quasi_8_minima(res,res2,(vxl_byte)0) ;
   vil_convert_cast(res2, res) ;
-  vil_save(res,"lena.kernel.pgm");
+  vil_save(res,argc>3 ? argv[3] : "lena.kernel.pgm");
 }
