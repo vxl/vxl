8f75ce283ee313a3b36673d138cdf30a65f6346e
Martin G. Roberts <mgroberts@users.sourceforge.net>
2004-07-29 12:36:19 -0400

    added some new stl style predicates to eg adapt a predicate from a vector to an index into that vector selecting strings including a substring moved in some ordering predicates from mbl_stl

---
+++ contrib/mul/mbl/mbl_stl_pred.h
@@ -0,0 +1,82 @@
+// This is mul/mbl/mbl_stl.h
+#ifndef mbl_stl_pred_h_
+#define mbl_stl_pred_h_
+//:
+// \file
+// \brief Useful predicate functors for use in stl find_if,partition etc.
+// \author martin roberts
+// \date  July 2004
+
+
+#include <vcl_functional.h>
+#include <vcl_vector.h>
+#include <vcl_string.h>
+
+//: Return true if a string contains a substring
+// Note without this you'd need bind2nd mem_fun which can cause reference to reference compile errors
+class mbl_stl_pred_str_contains : public vcl_unary_function<vcl_string, bool>
+{
+    //: The sought substring
+    const vcl_string& substr_;    
+  public:
+    mbl_stl_pred_str_contains(const vcl_string& substr): substr_(substr){}
+        
+    inline bool operator()(const vcl_string& str) const
+    {
+        return ( (str.find(substr_) != str.npos) ? true : false);
+    }
+};
+
+//: Adapt a predicate over a vector to the operation specified on an index into that vector
+// T is type of the vector, and Pred the boolean predicate to really be applied
+template <class T, class Pred>
+class mbl_stl_pred_index_adapter : public vcl_unary_function<unsigned, bool>
+{
+  //:const reference to vector used to store the objects indexed
+  const vcl_vector<T >& vec_;
+  //: The predicate to really be applied
+  Pred Op_;
+  public:
+    mbl_stl_pred_index_adapter(const vcl_vector<T >& v, Pred Op):
+        vec_(v),Op_(Op){}
+        
+    inline bool operator()(const unsigned& i) const
+    {
+        return Op_(vec_[i]);
+    }
+};
+
+//: Helper function to create an index adapter of the appropriate type
+// As this is a function not a class, it saves some template gobbledegook in the class name
+// Vec is assumed vector<T> where T is the type associated with the constructed adapter
+//However note that using this means an extra copy of the predicate functor will occur
+template <class T, class Pred>
+inline mbl_stl_pred_index_adapter<T,Pred> mbl_stl_pred_create_index_adapter(const vcl_vector<T>& v, Pred Op)
+{
+    return  mbl_stl_pred_index_adapter<T,Pred>(v,Op);
+};
+
+//Order a collection of pair iterators according to their dereferenced keys
+//NB assumes the key type supports operator<
+template <class PairIter>
+struct mbl_stl_pred_pair_iter_key_order : public vcl_binary_function<PairIter,PairIter, bool>
+{
+  inline bool  operator()(const PairIter& iter1, const PairIter& iter2 ) const
+  {
+    return (iter1->first < iter2->first) ? true : false;
+  }
+};
+
+//Order a collection of pair iterators according to their dereferenced values
+//NB assumes the key type supports operator<
+template <class PairIter>
+struct mbl_stl_pred_pair_iter_value_order : public vcl_binary_function<PairIter,PairIter, bool>
+{
+  inline bool  operator()(const PairIter& iter1, const PairIter& iter2 ) const
+  {
+    return (iter1->second < iter2->second) ? true : false ;
+  }
+};
+
+
+#endif
