71e3b68425428e8cd83fe2ea9dcfc9fdc6b4169a
Matthew J. Leotta <mleotta@users.sourceforge.net>
2004-07-29 15:57:25 -0400

    smart pointer headers should not include the class definition headers

--- contrib/brl/bseg/bmrf/bmrf_epi_point_sptr.h
+++ contrib/brl/bseg/bmrf/bmrf_epi_point_sptr.h
@@ -5,5 +5,5 @@
 
 class bmrf_epi_point;
 typedef vbl_smart_ptr<bmrf_epi_point> bmrf_epi_point_sptr;
-#include <bmrf/bmrf_epi_point.h>
+
 #endif
--- contrib/brl/bseg/bmrf/bmrf_epi_seg_sptr.h
+++ contrib/brl/bseg/bmrf/bmrf_epi_seg_sptr.h
@@ -5,5 +5,5 @@
 
 class bmrf_epi_seg;
 typedef vbl_smart_ptr<bmrf_epi_seg> bmrf_epi_seg_sptr;
-#include <bmrf/bmrf_epi_seg.h>
+
 #endif
