c21fde1dbaf7bc88f637d2bfa8b53bc0ac7a9f90
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-11 05:34:50 -0400

    added test on point values of merge

--- contrib/brl/bbas/bdgl/tests/test_region_algs.cxx
+++ contrib/brl/bbas/bdgl/tests/test_region_algs.cxx
@@ -12,30 +12,45 @@ static void test_region_algs()
   float* X1 = new float[n1];
   float* Y1 = new float[n1];
   unsigned short * I1 = new unsigned short[n1];
-  for(int i = 0; i<n1; i++)
+  for (int i = 0; i<n1; i++)
   {
-	X1[i] = i; Y1[i] = 2*i; I1[i]= 10*i;
+    X1[i] = i; Y1[i] = 2*i; I1[i]= 10*i;
   }
-  
+
   vdgl_digital_region_sptr r1 = new vdgl_digital_region(n1, X1, Y1, I1);
 
   float* X2 = new float[n2];
   float* Y2 = new float[n2];
   unsigned short * I2 = new unsigned short[n2];
-  for(int i = 0; i<n2; i++)
+  for (int i = 0; i<n2; i++)
   {
-	X2[i] = 3*i; Y2[i] = -6*i; I2[i]= 100*i;
+    X2[i] = 3*i; Y2[i] = -6*i; I2[i]= 100*i;
   }
   vdgl_digital_region_sptr r2 = new vdgl_digital_region(n2, X2, Y2, I2);
 
   vdgl_digital_region_sptr rm;
-
   bdgl_region_algs::merge(r1, r2, rm);
+
   int n  = n1 + n2;
-  for(rm->reset(); rm->next();)
-    vcl_cout << "P(" << rm->X() << " " << rm->Y() << " "
-             << rm->I() << ")";
-//  TEST("Num Intersection points = 1", pts.size(), 1);
+  TEST("bdgl_region_algs::merge(): num points", rm->Npix(), n);
+
+  for (rm->reset(); rm->next();)
+    vcl_cout << "P(" << rm->X() << ' ' << rm->Y() << ' ' << rm->I() << ")\n";
+
+  rm->reset(); rm->next();
+  for (r1->reset(); r1->next(); rm->next())
+  {
+    TEST("bdgl_region_algs::merge(): X value", rm->X(), r1->X());
+    TEST("bdgl_region_algs::merge(): Y value", rm->Y(), r1->Y());
+    TEST("bdgl_region_algs::merge(): I value", rm->I(), r1->I());
+  }
+  for (r2->reset(); r2->next(); rm->next())
+  {
+    TEST("bdgl_region_algs::merge(): X value", rm->X(), r2->X());
+    TEST("bdgl_region_algs::merge(): Y value", rm->Y(), r2->Y());
+    TEST("bdgl_region_algs::merge(): I value", rm->I(), r2->I());
+  }
+  //TEST("Num Intersection points = 1", pts.size(), 1);
 }
 
-TESTLIB_DEFINE_MAIN(test_region_algs);
+TESTMAIN(test_region_algs);
