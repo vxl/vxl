ed59bb1535163ce113e6b5126b90a11acdafbe6f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-11 06:48:20 -0400

    added new header files

--- contrib/brl/bseg/brip/tests/test_include.cxx
+++ contrib/brl/bseg/brip/tests/test_include.cxx
@@ -1,7 +1,15 @@
 #include <bseg/brip/brip_histogram.h>
 #include <bseg/brip/brip_mutual_info.h>
+#include <bseg/brip/brip_para_cvrg.h>
+#include <bseg/brip/brip_para_cvrg_params.h>
+#include <bseg/brip/brip_region_pixel.h>
+#include <bseg/brip/brip_region_pixel_sptr.h>
+#include <bseg/brip/brip_roi.h>
+#include <bseg/brip/brip_roi_sptr.h>
 #include <bseg/brip/brip_vil_float_ops.h>
 #include <bseg/brip/brip_vil1_float_ops.h>
 #include <bseg/brip/brip_vil_ops.h>
+#include <bseg/brip/brip_watershed.h>
+#include <bseg/brip/brip_watershed_params.h>
 
 int main() { return 0; }
