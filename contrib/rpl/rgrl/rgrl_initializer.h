c1143664a14a9f1c9c3e4acdef85ca6a6f194e28
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-13 17:38:37 -0400

    avoid compiler warning (only one of the methods next_initial() is pure virtual; the other ones are implemented in the base class and need not be virtual)

--- contrib/rpl/rgrl/rgrl_initializer.h
+++ contrib/rpl/rgrl/rgrl_initializer.h
@@ -1,16 +1,14 @@
-
 #ifndef rgrl_initializer_h_
 #define rgrl_initializer_h_
-
+//:
 // \file
 // \brief Base class for generating initial estimates.
 // \author Chuck Stewart
 // \date 12 Nov 2002
-// 
+//
 // \verbatim
-// Modifications:
-// Jan 2004 Charlene: Added other non-virtual functions to hide view.
-// 
+//  Modifications:
+//   Jan 2004 Charlene: Added other non-virtual functions to hide view.
 // \endverbatim
 
 #include "rgrl_transformation_sptr.h"
@@ -24,7 +22,7 @@
 class rgrl_initializer
   : public rgrl_object
 {
-public:
+ public:
   virtual ~rgrl_initializer();
 
   //: Get next initial view.
@@ -33,40 +31,38 @@ public:
   // transformation estimate must have a covariance matrix filled in
   // because it is used in matching
   //
-  // This is the only virtual function implemented in the derived
-  // classes.
+  // This is the only pure virtual function implemented in the derived classes.
   virtual bool next_initial( rgrl_view_sptr           & view,
                              rgrl_scale_sptr          & prior_scale) = 0;
 
   //: Get next set of initial iniformation without the notion of a view.
   //
-  virtual bool next_initial( rgrl_mask_box            & from_image_roi,
-                             rgrl_mask_box            & to_image_roi,
-                             rgrl_mask_box            & current_region,
-                             rgrl_mask_box            & global_region,
-                             rgrl_estimator_sptr      & xform_estimator,
-                             rgrl_transformation_sptr & xform_estimate,
-                             unsigned                 & current_resolution,
-                             rgrl_scale_sptr          & prior_scale);
+  bool next_initial( rgrl_mask_box            & from_image_roi,
+                     rgrl_mask_box            & to_image_roi,
+                     rgrl_mask_box            & current_region,
+                     rgrl_mask_box            & global_region,
+                     rgrl_estimator_sptr      & xform_estimator,
+                     rgrl_transformation_sptr & xform_estimate,
+                     unsigned                 & current_resolution,
+                     rgrl_scale_sptr          & prior_scale);
 
   //: Get next set of initial information, assuming \a current_region is the region of interest for registration.
   //
-  virtual bool next_initial( rgrl_mask_box            & current_region,
-                             rgrl_estimator_sptr      & xform_estimator,
-                             rgrl_transformation_sptr & xform_estimate,
-                             unsigned                 & current_resolution,
-                             rgrl_scale_sptr          & prior_scale);
+  bool next_initial( rgrl_mask_box            & current_region,
+                     rgrl_estimator_sptr      & xform_estimator,
+                     rgrl_transformation_sptr & xform_estimate,
+                     unsigned                 & current_resolution,
+                     rgrl_scale_sptr          & prior_scale);
 
   //: Get next set of initial information, assuming \a current_region is the region of interest for registration, and single-resolution.
   //
-  virtual bool next_initial( rgrl_mask_box            & current_region,
-                             rgrl_estimator_sptr      & xform_estimator,
-                             rgrl_transformation_sptr & xform_estimate,
-                             rgrl_scale_sptr          & prior_scale);
+  bool next_initial( rgrl_mask_box            & current_region,
+                     rgrl_estimator_sptr      & xform_estimator,
+                     rgrl_transformation_sptr & xform_estimate,
+                     rgrl_scale_sptr          & prior_scale);
 
   // Defines type-related functions
   rgrl_type_macro( rgrl_initializer, rgrl_object );
-
 };
 
 #endif // rgrl_initializer_h_
