1db7cf1442fdef91270e40fdc56414bb5c87cd21
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-13 11:03:32 -0400

    updated w.r.t. CMakeLists.txt

---
+++ contrib/rpl/rgrl/config.mk
@@ -0,0 +1,10 @@
+IULIBS += -lrgrl
+USES += contrib/mul
+include ${IUEROOT}/contrib/rpl/rrel/config.mk
+include ${IUEROOT}/contrib/rpl/rsdl/config.mk
+include ${IUEROOT}/contrib/mul/mbl/config.mk
+include ${IUEROOT}/contrib/mul/vil3d/config.mk
+include ${IUEROOT}/core/vil/config.mk
+include ${IUEROOT}/core/vnl/algo/config.mk
+include ${IUEROOT}/core/vbl/config.mk
+include ${IUEROOT}/core/vul/config.mk
