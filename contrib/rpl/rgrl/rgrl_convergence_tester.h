b8bf10a99aa80bc150c2ee59d3fa7fc40c40d6b4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-13 17:38:31 -0400

    avoid compiler warning (only one of the methods compute_status() is pure virtual; the other ones are implemented in the base class and need not be virtual)

--- contrib/rpl/rgrl/rgrl_convergence_tester.h
+++ contrib/rpl/rgrl/rgrl_convergence_tester.h
@@ -1,6 +1,7 @@
 #ifndef rgrl_convergence_tester_h_
 #define rgrl_convergence_tester_h_
-
+//:
+// \file
 #include "rgrl_set_of.h"
 #include "rgrl_match_set_sptr.h"
 #include "rgrl_scale_sptr.h"
@@ -16,24 +17,23 @@ class rgrl_transformation;
 class rgrl_convergence_tester
   : public rgrl_object
 {
-public:
+ public:
   //:  Compute the converge_status of the current view using multiple match sets
-  //   This is the virtual function implemented in the derived classes
+  //   This is the pure virtual function implemented in the derived classes.
   //
-  virtual rgrl_converge_status_sptr 
+  virtual rgrl_converge_status_sptr
   compute_status( rgrl_converge_status_sptr               prev_status,
-		  rgrl_view                        const& prev_view,
-		  rgrl_view                        const& current_view,
-		  rgrl_set_of<rgrl_match_set_sptr> const& current_match_sets,
-		  rgrl_set_of<rgrl_scale_sptr>     const& current_scales,
+                  rgrl_view                        const& prev_view,
+                  rgrl_view                        const& current_view,
+                  rgrl_set_of<rgrl_match_set_sptr> const& current_match_sets,
+                  rgrl_set_of<rgrl_scale_sptr>     const& current_scales,
                   bool                                    penalize_scaling = false )const  = 0;
 
   //:  Compute the converge_status of the current view using single match set
   //
   //   Matches are assumed to have from_feature mapped and stored and
-  //   associated weights computeded already when passed to this
-  //   function.
-  virtual rgrl_converge_status_sptr 
+  //   associated weights computed already when passed to this function.
+  rgrl_converge_status_sptr
   compute_status( rgrl_converge_status_sptr               prev_status,
                   rgrl_view                        const& prev_view,
                   rgrl_view                        const& current_view,
@@ -42,7 +42,7 @@ public:
                   bool                                    penalize_scaling = false)const;
 
   //:  Compute the converge_status of the current xform estimate using multiple match sets
-  virtual rgrl_converge_status_sptr 
+  rgrl_converge_status_sptr
   compute_status( rgrl_converge_status_sptr               prev_status,
                   rgrl_transformation_sptr                xform_estimate,
                   rgrl_estimator_sptr                     xform_estimator,
@@ -50,17 +50,16 @@ public:
                   rgrl_set_of<rgrl_scale_sptr>     const& current_scales,
                   bool                                    penalize_scaling = false)const;
   //:  Compute the converge_status of the current xform estimate using single match set
-  virtual rgrl_converge_status_sptr 
+  rgrl_converge_status_sptr
   compute_status( rgrl_converge_status_sptr               prev_status,
                   rgrl_transformation_sptr                xform_estimate,
                   rgrl_estimator_sptr                     xform_estimator,
                   rgrl_match_set_sptr                     current_match_set,
                   rgrl_scale_sptr                         current_scale,
-                  bool                                    penalize_scaling = false)const; 
+                  bool                                    penalize_scaling = false)const;
 
   // Defines type-related functions
   rgrl_type_macro( rgrl_convergence_tester, rgrl_object );
-
 };
 
 #endif
