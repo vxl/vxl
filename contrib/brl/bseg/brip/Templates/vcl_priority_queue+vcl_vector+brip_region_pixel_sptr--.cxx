a396124f000ea9402d1470089d86f55d5dd76d00
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-14 13:38:19 -0400

    put two template instantiations in two separate files

--- contrib/brl/bseg/brip/Templates/vcl_priority_queue+vcl_vector+brip_region_pixel_sptr--.cxx
+++ contrib/brl/bseg/brip/Templates/vcl_priority_queue+vcl_vector+brip_region_pixel_sptr--.cxx
@@ -1,8 +1,5 @@
-#include <brip/brip_region_pixel.h>
+#include <brip/brip_region_pixel_sptr.h>
+#include <vcl_vector.h>
 #include <vcl_queue.txx>
 
-VCL_QUEUE_INSTANTIATE(brip_region_pixel_sptr);
-
 VCL_PRIORITY_QUEUE_INSTANTIATE(vcl_vector<brip_region_pixel_sptr>, brip_region_pixel_sptr, brip_region_pixel::compare);
-
-
---
+++ contrib/brl/bseg/brip/Templates/vcl_queue+brip_region_pixel_sptr-.cxx
@@ -0,0 +1,4 @@
+#include <brip/brip_region_pixel_sptr.h>
+#include <vcl_queue.txx>
+
+VCL_QUEUE_INSTANTIATE(brip_region_pixel_sptr);
