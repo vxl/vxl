54ea46368bb6c7158569bfce686047cb3dd652ce
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-14 07:46:25 -0400

    cos() --> vcl_cos()

--- contrib/rpl/rgrl/rgrl_trans_rigid.cxx
+++ contrib/rpl/rgrl/rgrl_trans_rigid.cxx
@@ -1,11 +1,11 @@
+#include "rgrl_trans_rigid.h"
 //:
 // \file
 // \author Tomasz Malisiewicz
 // \date   March 2004
 
-#include "rgrl_trans_rigid.h"
-
 #include <vcl_cassert.h>
+#include <vcl_cstdlib.h>
 #include <vnl/algo/vnl_svd.h>
 #include <vnl/vnl_math.h>
 #include <rgrl/rgrl_util.h>
@@ -17,41 +17,41 @@ rgrl_trans_rigid( unsigned int dimension )
     covar_( vnl_matrix<double>( 3*(dimension - 1), 3*(dimension - 1), 0.0 ) )
 {
   // only accept 2d or 3d rigid transformation
-  assert(dimension==2 || dimension==3);
+  assert (dimension==2 || dimension==3);
 }
-  
+
 rgrl_trans_rigid::
-rgrl_trans_rigid( vnl_matrix<double> const& rot, 
+rgrl_trans_rigid( vnl_matrix<double> const& rot,
                   vnl_vector<double> const& in_trans )
   : R_( rot ),
     trans_( in_trans ),
     covar_( vnl_matrix<double>( 3*(in_trans.size()-1), 3*(in_trans.size()-1), 0.0 ) )
 {
-  assert( R_.rows() == R_.cols() );  //make sure rotation is square matrix
-  assert( R_.rows() == trans_.size() ); 
-  assert( covar_.rows() == covar_.cols() );
-  assert( ( R_.rows() != 2 || covar_.rows() == 3 ) ); // 2d has 3 params (1 angle + 2 displacements)
-  assert( ( R_.rows() != 3 || covar_.rows() == 6 ) ); // 3d has 6 params (3 angles + 3 displacements)
+  assert ( R_.rows() == R_.cols() );  //make sure rotation is square matrix
+  assert ( R_.rows() == trans_.size() );
+  assert ( covar_.rows() == covar_.cols() );
+  assert ( ( R_.rows() != 2 || covar_.rows() == 3 ) ); // 2d has 3 params (1 angle + 2 displacements)
+  assert ( ( R_.rows() != 3 || covar_.rows() == 6 ) ); // 3d has 6 params (3 angles + 3 displacements)
 }
 
 rgrl_trans_rigid::
-rgrl_trans_rigid( vnl_matrix<double> const& rot, 
+rgrl_trans_rigid( vnl_matrix<double> const& rot,
                   vnl_vector<double> const& in_trans,
                   vnl_matrix<double> const& in_covar )
   : R_( rot ),
     trans_( in_trans ),
     covar_( in_covar )
 {
-  assert( R_.rows() == R_.cols() );
-  assert( R_.rows() == trans_.size() );
-  assert( covar_.rows() == covar_.cols() );
-  assert( ( R_.rows() != 2 || covar_.rows() == 3 ) ); // 2d has 3 params
-  assert( ( R_.rows() != 3 || covar_.rows() == 6 ) ); // 3d has 6 params
+  assert ( R_.rows() == R_.cols() );
+  assert ( R_.rows() == trans_.size() );
+  assert ( covar_.rows() == covar_.cols() );
+  assert ( ( R_.rows() != 2 || covar_.rows() == 3 ) ); // 2d has 3 params
+  assert ( ( R_.rows() != 3 || covar_.rows() == 6 ) ); // 3d has 6 params
 }
 
 void rgrl_trans_rigid::set_translation(double tx, double ty, double tz)
 {
-  assert( trans_.size() == 3);
+  assert ( trans_.size() == 3);
   trans_[0]=tx;
   trans_[1]=ty;
   trans_[2]=tz;
@@ -59,31 +59,34 @@ void rgrl_trans_rigid::set_translation(double tx, double ty, double tz)
 
 void rgrl_trans_rigid::set_translation(double tx, double ty)
 {
-  assert( trans_.size() == 2);
+  assert ( trans_.size() == 2);
   trans_[0]=tx;
   trans_[1]=ty;
 }
 
 void rgrl_trans_rigid::set_rotation(double theta, double alpha, double phi)
 {
-  assert( trans_.size() == 3);
-  
-  R_(0,0) =cos(alpha)*cos(theta) ;                              R_(0,1) = -cos(alpha)*sin(theta);                              R_(0,2)=sin(alpha);
-  R_(1,0) =cos(theta)*sin(alpha)*sin(phi)+cos(phi)*sin(theta);  R_(1,1) = -sin(alpha)*sin(phi)*sin(theta)+cos(phi)*cos(theta); R_(1,2)=-cos(alpha)*sin(phi);
-  R_(2,0)=-cos(phi)*cos(theta)*sin(alpha)+sin(phi)*sin(theta);  R_(2,1) = cos(phi)*sin(alpha)*sin(theta)+cos(theta)*sin(phi);  R_(2,2)=cos(alpha)*cos(phi);
+  assert ( trans_.size() == 3);
+
+  double cos_a = vcl_cos(alpha), sin_a = vcl_sin(alpha),
+         cos_t = vcl_cos(theta), sin_t = vcl_sin(theta),
+         cos_p = vcl_cos(phi),   sin_p = vcl_sin(phi);
+  R_(0,0) =cos_a*cos_t ;                   R_(0,1) = -cos_a*sin_t;                   R_(0,2)=sin_a;
+  R_(1,0) =cos_t*sin_a*sin_p+cos_p*sin_t;  R_(1,1) = -sin_a*sin_p*sin_t+cos_p*cos_t; R_(1,2)=-cos_a*sin_p;
+  R_(2,0)=-cos_p*cos_t*sin_a+sin_p*sin_t;  R_(2,1) = cos_p*sin_a*sin_t+cos_t*sin_p;  R_(2,2)=cos_a*cos_p;
 }
 
 void rgrl_trans_rigid::set_rotation(double theta)
 {
-  assert( trans_.size() == 2);
+  assert ( trans_.size() == 2);
 
-  R_(0,0) = cos(theta);    R_(0,1) = sin(theta);
-  R_(1,0) = -sin(theta);   R_(1,1) = cos(theta);
+  R_(0,0) = vcl_cos(theta);    R_(0,1) = vcl_sin(theta);
+  R_(1,0) = -vcl_sin(theta);   R_(1,1) = vcl_cos(theta);
 }
 
 void rgrl_trans_rigid::determine_angles(double& phi, double& alpha, double& theta) const
 {
-  assert(trans_.size() == 3);
+  assert (trans_.size() == 3);
 
   alpha = vcl_asin( R_(0,2) );
 
@@ -93,7 +96,7 @@ void rgrl_trans_rigid::determine_angles(double& phi, double& alpha, double& thet
   }
   else
   {
-    theta = vcl_atan( -1 * R_(0,1)/R_(0,0) ) + vnl_math::pi; 
+    theta = vcl_atan( -1 * R_(0,1)/R_(0,0) ) + vnl_math::pi;
   }
 
 
@@ -105,12 +108,11 @@ void rgrl_trans_rigid::determine_angles(double& phi, double& alpha, double& thet
   {
     phi = vcl_atan( -1 * R_(1,2) / R_(2,2) ) + vnl_math::pi;
   }
-
 }
 
 void rgrl_trans_rigid::determine_angles( double& theta ) const
 {
-  assert(trans_.size() == 2);
+  assert (trans_.size() == 2);
   theta = vcl_asin( R_(0,1) );
 }
 
@@ -119,7 +121,7 @@ rgrl_trans_rigid::
 map_loc( vnl_vector<double> const& from,
          vnl_vector<double>      & to   ) const
 {
-  assert( from.size() == R_.rows() );
+  assert ( from.size() == R_.rows() );
   to = R_ * from + trans_;
 }
 
@@ -129,8 +131,8 @@ map_dir( vnl_vector<double> const& from_loc,
          vnl_vector<double> const& from_dir,
          vnl_vector<double>      & to_dir    ) const
 {
-  assert( from_loc.size() == R_.cols() );
-  assert( from_dir.size() == R_.cols() );
+  assert ( from_loc.size() == R_.cols() );
+  assert ( from_dir.size() == R_.cols() );
   to_dir = R_ * from_dir;
   to_dir.normalize();
 }
@@ -140,8 +142,7 @@ vnl_matrix<double>
 rgrl_trans_rigid::
 transfer_error_covar( vnl_vector<double> const& p  ) const
 {
-
-  assert( p.size() == trans_.size() );
+  assert ( p.size() == trans_.size() );
   vnl_matrix<double> jacobian(3,6,0.0);
 
   double phi,alpha,theta;
@@ -153,20 +154,20 @@ transfer_error_covar( vnl_vector<double> const& p  ) const
   vnl_matrix<double> Rthetad(3,3,0.0);
 
   // derivative matrices now
-  Rthetad(0,0) = -sin(theta);
-  Rthetad(0,1) = -cos(theta);
-  Rthetad(1,0) = cos(theta);
-  Rthetad(1,1) = -sin(theta);
+  Rthetad(0,0) = -vcl_sin(theta);
+  Rthetad(0,1) = -vcl_cos(theta);
+  Rthetad(1,0) = vcl_cos(theta);
+  Rthetad(1,1) = -vcl_sin(theta);
 
-  Ralphad(0,0) = -sin(alpha);
-  Ralphad(0,2) = cos(alpha);
-  Ralphad(2,0) = -cos(alpha);  
-  Ralphad(2,2) = -sin(alpha);
+  Ralphad(0,0) = -vcl_sin(alpha);
+  Ralphad(0,2) = vcl_cos(alpha);
+  Ralphad(2,0) = -vcl_cos(alpha);
+  Ralphad(2,2) = -vcl_sin(alpha);
 
-  Rphid(1,1) = -sin(phi);
-  Rphid(1,2) = -cos(phi);
-  Rphid(2,1) = cos(phi);
-  Rphid(2,2) = -sin(phi);
+  Rphid(1,1) = -vcl_sin(phi);
+  Rphid(1,2) = -vcl_cos(phi);
+  Rphid(2,1) = vcl_cos(phi);
+  Rphid(2,2) = -vcl_sin(phi);
 
   jacobian.set_column(0,Rthetad*p);
   jacobian.set_column(1,Ralphad*p);
@@ -202,7 +203,7 @@ t() const
 
 void
 rgrl_trans_rigid::
-inv_map( const vnl_vector<double>& to, 
+inv_map( const vnl_vector<double>& to,
          bool initialize_next,
          const vnl_vector<double>& to_delta,
          vnl_vector<double>& from,
@@ -210,7 +211,7 @@ inv_map( const vnl_vector<double>& to,
 {
   const double epsilon = 0.01;
   vnl_vector<double> to_est = this->map_location(from);
-  
+
   // compute the inverse of the Jacobian, which is the R_^-1
   vnl_svd<double> svd( R_ );
   vnl_matrix<double> J_inv = svd.inverse();
@@ -233,54 +234,54 @@ jacobian( vnl_vector<double> const& from_loc ) const
 
 void
 rgrl_trans_rigid::
-inv_map( const vnl_vector<double>& to, 
+inv_map( const vnl_vector<double>& to,
          vnl_vector<double>& from ) const
 {
   vnl_svd<double> svd( R_ );
   from = svd.inverse()*to - svd.inverse()*trans_;
 }
 
-rgrl_transformation_sptr 
+rgrl_transformation_sptr
 rgrl_trans_rigid::
 scale_by( double scale ) const
 {
-  return new rgrl_trans_rigid( R_, trans_ * scale, 
-                                    covar_); 
+  return new rgrl_trans_rigid( R_, trans_ * scale,
+                                    covar_);
 }
 
 
-void 
+void
 rgrl_trans_rigid::
 write( vcl_ostream& os ) const
 {
   // tag
-  os << "RIGID" << vcl_endl;
+  os << "RIGID\n"
   // parameters
-  os << t().size() << vcl_endl;
-  os << R() << t() << vcl_endl;
+     << t().size() << vcl_endl
+     << R() << t() << vcl_endl;
 }
 
-void 
+void
 rgrl_trans_rigid::
 read( vcl_istream& is )
 {
   int dim;
-  
+
   // skip empty lines
   rgrl_util_skip_empty_lines( is );
-  
+
   vcl_string str;
   vcl_getline( is, str );
-  
-  if( str != "RIGID" ) {
-    WarningMacro( "The tag is not RIGID. reading is aborted." << vcl_endl );
-    exit(10);
+
+  if ( str != "RIGID" ) {
+    WarningMacro( "The tag is not RIGID. reading is aborted.\n" );
+    vcl_exit(10);
   }
-  
+
   // input global xform
   dim=-1;
   is >> dim;
-  if( dim > 0 ) {
+  if ( dim > 0 ) {
     R_.set_size( dim, dim );
     trans_.set_size( dim );
     is >> R_ >> trans_;
