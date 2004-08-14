25454913e584a79e701d335747ba688edb91a164
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-14 07:47:22 -0400

    removed duplicate #include <vcl_algorithm.h>

--- contrib/rpl/rgrl/rgrl_initializer_inv_indexing.cxx
+++ contrib/rpl/rgrl/rgrl_initializer_inv_indexing.cxx
@@ -2,11 +2,9 @@
 
 #include <vcl_vector.h>
 #include <vcl_algorithm.h>
-#include <vcl_algorithm.h>
 #include <vcl_cassert.h>
 
 #include <rsdl/rsdl_kd_tree.h>
-#include <vnl/vnl_vector.h>
 
 #include "rgrl_view.h"
 #include "rgrl_util.h"
@@ -45,7 +43,7 @@ rgrl_initializer_inv_indexing( rgrl_mask_box  const&     from_image_roi,
   }
 
   view_ = new rgrl_view( from_image_roi, to_image_roi, from_image_roi,
-                         from_image_roi, xform_estimator, 0, 
+                         from_image_roi, xform_estimator, 0,
                          initial_resolution );
 }
 
@@ -53,7 +51,7 @@ void
 rgrl_initializer_inv_indexing::
 add_data( vcl_vector<rgrl_invariant_sptr> fixed_set,
           vcl_vector<rgrl_invariant_sptr> moving_set,
-          double nn_radius, 
+          double nn_radius,
           unsigned int k_nn )
 {
   vcl_vector<vcl_vector<rgrl_invariant_sptr> > moving_sets;
@@ -67,13 +65,13 @@ add_data( vcl_vector<rgrl_invariant_sptr> fixed_set,
           vcl_vector<vcl_vector<rgrl_invariant_sptr> > moving_sets,
           double nn_radius,
           unsigned int k_nn )
-{  
+{
   unsigned int num_moving_sets = moving_sets.size();
-  assert(fixed_set.size() > 0);
+  assert (fixed_set.size() > 0);
 
   // allocate space for matches
   if ( matches_.empty() ) matches_.resize( num_moving_sets );
-  else assert( matches_.size() == num_moving_sets );
+  else assert ( matches_.size() == num_moving_sets );
 
   // Obtain the number of cartesian, nc, and number of angular, na,
   // from the first invariant feature
@@ -82,33 +80,33 @@ add_data( vcl_vector<rgrl_invariant_sptr> fixed_set,
 
   // Construct a kd-tree for the set of search points from the fixed_set
   vcl_vector<rsdl_point> search_pts(fixed_set.size());
-  for(unsigned int pt=0; pt<fixed_set.size(); ++pt){
+  for (unsigned int pt=0; pt<fixed_set.size(); ++pt){
     // Set number of expected cartesian and angular values
     search_pts[pt].resize( nc, na );
-    search_pts[pt].set_cartesian(fixed_set[pt]->cartesian_invariants());  
+    search_pts[pt].set_cartesian(fixed_set[pt]->cartesian_invariants());
     search_pts[pt].set_angular(fixed_set[pt]->angular_invariants());
   }
   rsdl_kd_tree kd_tree( search_pts );
 
   // Iterate through the features from each moving set
-  for(unsigned int m_ind = 0; m_ind<num_moving_sets; ++m_ind){
+  for (unsigned int m_ind = 0; m_ind<num_moving_sets; ++m_ind){
     // Iterate through all constellations/points in the current "from" image
-    for(unsigned int pt = 0; pt<moving_sets[m_ind].size(); pt++){
+    for (unsigned int pt = 0; pt<moving_sets[m_ind].size(); pt++){
       // Create a query point from the invariants of the current constellation
       rsdl_point query_pt( nc, na );
       query_pt.set_cartesian(moving_sets[m_ind][pt]->cartesian_invariants());
       query_pt.set_angular(moving_sets[m_ind][pt]->angular_invariants());
-      
+
       // Find all points (and their indices) within nn_radius of the query_pt
       vcl_vector<rsdl_point> near_neighbor_pts;
       vcl_vector<int> near_neighbor_indices;
       kd_tree.points_in_radius( query_pt, nn_radius, near_neighbor_pts, near_neighbor_indices );
-      
+
       // If no points were found in nn_radius, find the nearest point
       if (near_neighbor_indices.size() == 0) {
         kd_tree.n_nearest( query_pt, k_nn, near_neighbor_pts, near_neighbor_indices );
       }
-      
+
       // Create matches from the nearest neighbors and push them onto
       // the current vector
       for (unsigned int nn_ind = 0; nn_ind<near_neighbor_indices.size(); ++nn_ind){
@@ -116,10 +114,10 @@ add_data( vcl_vector<rgrl_invariant_sptr> fixed_set,
       }
     }
     vcl_sort(matches_[m_ind].begin(), matches_[m_ind].end(), dist_greater);
-  }    
+  }
 }
 
-void 
+void
 rgrl_initializer_inv_indexing::
 set_current_moving_image( unsigned int moving_image_index)
 {
@@ -134,18 +132,18 @@ matches_for_moving_image( unsigned int moving_image_index)
   return matches_[moving_image_index];
 }
 
-bool 
+bool
 rgrl_initializer_inv_indexing::
 next_initial( rgrl_view_sptr           & view,
               rgrl_scale_sptr          & prior_scale)
 {
-  if ( max_num_matches_tried_ > 0 && 
+  if ( max_num_matches_tried_ > 0 &&
        num_matches_tried_ == max_num_matches_tried_ )
     return false;
 
-  rgrl_invariant_match_sptr best_match; 
+  rgrl_invariant_match_sptr best_match;
   bool found_best_match = false;
-  if(!matches_[current_moving_image_ind_].empty()){
+  if (!matches_[current_moving_image_ind_].empty()){
     // Remove the best (last) match from the vector
     do {
       best_match = matches_[current_moving_image_ind_].back();
@@ -160,7 +158,7 @@ next_initial( rgrl_view_sptr           & view,
     //
     rgrl_mask_box global_region = view_->from_image_roi();
     if ( should_estimate_global_region_ )
-      global_region = 
+      global_region =
         rgrl_util_estimate_global_region(view_->from_image_roi(),
                                          view_->to_image_roi(),
                                          view_->from_image_roi(),
@@ -168,7 +166,7 @@ next_initial( rgrl_view_sptr           & view,
     // Determine the initial region
     //
     rgrl_mask_box initial_region = view_->from_image_roi();
-    if ( best_match->has_initial_region() ) 
+    if ( best_match->has_initial_region() )
       initial_region = best_match->initial_region();
 
     view = new rgrl_view( view_->from_image_roi(),
@@ -177,7 +175,7 @@ next_initial( rgrl_view_sptr           & view,
                           global_region,
                           view_->xform_estimator(),
                           best_match->transform(),
-                          view_->resolution() );  
+                          view_->resolution() );
     prior_scale = best_match->scale();
 
     return true;
