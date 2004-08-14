25dda7511eb65b23c298ce4492c1c5b046229682
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-14 07:47:41 -0400

    removed unused #include <vcl_cstdlib.h>

--- contrib/rpl/rgrl/tests/test_match_set.cxx
+++ contrib/rpl/rgrl/tests/test_match_set.cxx
@@ -4,13 +4,11 @@
 
 #include <vcl_vector.h>
 #include <vcl_algorithm.h>
-#include <vcl_cstdlib.h>
 #include <vcl_iostream.h>
 
 #include <vnl/vnl_vector.h>
 
 #include <rgrl/rgrl_feature_point.h>
-
 
 
 namespace {
@@ -57,43 +55,43 @@ test_one_to_one_contents( match_set_type* ms,
 
   bool okay = true;
   unsigned count = 0; // number of correspondences
-  for( from_iter_type fi = ms->from_begin(); fi != ms->from_end(); ++fi ) {
-    if( ! is_in( from_pts, fi.from_feature() ) ) {
+  for ( from_iter_type fi = ms->from_begin(); fi != ms->from_end(); ++fi ) {
+    if ( ! is_in( from_pts, fi.from_feature() ) ) {
       vcl_cout << "From feature isn't in original list of features\n";
       okay = false;
     } else {
       unsigned idx = index_in( from_pts, fi.from_feature() );
 
-      if( fi.mapped_from_feature() ) {
+      if ( fi.mapped_from_feature() ) {
         vcl_cout << "From feature "<<idx<<": mapped from feature is wrong\n";
         okay = false;
       }
 
       // number of correspondences for this "from" feature
       unsigned num_matches = 0;
-      for( to_iter_type ti = fi.begin(); ti != fi.end(); ++ti ) {
+      for ( to_iter_type ti = fi.begin(); ti != fi.end(); ++ti ) {
         ++num_matches;
         ++count;
       }
-      if( num_matches != 1 ) {
+      if ( num_matches != 1 ) {
         vcl_cout << "From feature "<<idx<<": has " << num_matches << " matches, not 1\n";
         okay = false;
-      } else if( fi.begin().to_feature() != to_pts[idx] ) {
+      } else if ( fi.begin().to_feature() != to_pts[idx] ) {
         vcl_cout << "From feature "<<idx<<": has incorrect to feature\n";
         okay = false;
-      } else if( fi.begin().geometric_weight() != wgts[idx] ) {
+      } else if ( fi.begin().geometric_weight() != wgts[idx] ) {
         vcl_cout << "From feature "<<idx<<": has incorrect geometric weight\n";
         okay = false;
-      } else if( fi.begin().signature_weight() != wgts[idx] ) {
+      } else if ( fi.begin().signature_weight() != wgts[idx] ) {
         vcl_cout << "From feature "<<idx<<": has incorrect signature weight\n";
         okay = false;
-      } else if( fi.begin().cumulative_weight() != wgts[idx] ) {
+      } else if ( fi.begin().cumulative_weight() != wgts[idx] ) {
         vcl_cout << "From feature "<<idx<<": has incorrect cumulative weight\n";
         okay = false;
-      } 
+      }
     }
   }
-  if( count != from_pts.size() ) {
+  if ( count != from_pts.size() ) {
     vcl_cout << "Total number of matches in set ("<<count<<") != actual number of matches ("<<from_pts.size()<<")\n";
     okay = false;
   }
@@ -112,7 +110,7 @@ test_one_to_one_correspondence()
 
   unsigned num_corres = random_count( 100 );
   vcl_cout << "Creating " << num_corres << " correspondences\n";
-  for( unsigned i=0; i < num_corres; ++i ) {
+  for ( unsigned i=0; i < num_corres; ++i ) {
     from_pts.        push_back( new rgrl_feature_point( random_2d_vector() ) );
     to_pts.          push_back( new rgrl_feature_point( random_2d_vector() ) );
     wgts.            push_back( random_count( 10 ) );
@@ -123,7 +121,7 @@ test_one_to_one_correspondence()
   testlib_test_perform( ms && ms->from_size() == 0 );
 
   testlib_test_begin( "Adding the features" );
-  for( unsigned i=0; i < from_pts.size(); ++i ) {
+  for ( unsigned i=0; i < from_pts.size(); ++i ) {
     ms->add_feature_and_match( from_pts[i], 0, to_pts[i], wgts[i] );
   }
   testlib_test_perform( true );
