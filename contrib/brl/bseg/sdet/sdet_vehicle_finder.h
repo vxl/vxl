2432f5f97330a66e31190b6a43a1f2351cfd92a5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-14 11:02:31 -0400

    removed unused #include <vbl/vbl_array_2d.h>

--- contrib/brl/bseg/sdet/sdet_vehicle_finder.h
+++ contrib/brl/bseg/sdet/sdet_vehicle_finder.h
@@ -16,7 +16,6 @@
 //-------------------------------------------------------------------------
 #include <vcl_vector.h>
 #include <vgl/vgl_point_2d.h>
-#include <vbl/vbl_array_2d.h>
 #include <vil1/vil1_image.h>
 #include <vsol/vsol_box_2d_sptr.h>
 #include <vsol/vsol_polygon_2d_sptr.h>
@@ -34,7 +33,7 @@ class sdet_vehicle_finder : public sdet_vehicle_finder_params
   //Accessors
 
   void set_image(vil1_image& image){image_ = image;}
-                 
+
   void set_pick(const int x, const int y);
 
   vsol_box_2d_sptr search_box(){return search_box_;}
@@ -55,7 +54,7 @@ class sdet_vehicle_finder : public sdet_vehicle_finder_params
   //protected methods
   void construct_search_box();
   bool n_regions_closest_to_pick(vcl_vector<sdet_region_sptr> const& regions,
-                                 const int n, 
+                                 const int n,
                                  vcl_vector<sdet_region_sptr> & n_regions);
 
   bool region_box_union(vcl_vector<sdet_region_sptr> const& regions,
