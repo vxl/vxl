f4f21d18f3b15ecc17e7257502596db1c7f42cfd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-14 11:45:12 -0400

    avoid compiler warning ("prev_resol" might be used uninitialised)

--- contrib/rpl/rgrl/rgrl_feature_based_registration.cxx
+++ contrib/rpl/rgrl/rgrl_feature_based_registration.cxx
@@ -273,7 +273,7 @@ register_single_feature( rgrl_mask_box            image_region,
   vcl_vector<rgrl_estimator_sptr>   xform_estimators;
   rgrl_estimator_sptr               xform_estimator;
   bool                              failed, scale_in_range;
-  unsigned                          prev_resol;
+  unsigned                          prev_resol = 0;
 
   failed = false;
   scale_in_range = true;
