d901d18daa37881a62570c1502834944e9acaee2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-14 07:47:15 -0400

    removed duplicate #include <rgrl/rgrl_trans_similarity.h>

--- contrib/rpl/rgrl/tests/test_convergence.cxx
+++ contrib/rpl/rgrl/tests/test_convergence.cxx
@@ -1,21 +1,9 @@
 #include <testlib/testlib_test.h>
 
-#include <vcl_vector.h>
-#include <vcl_iostream.h>
-#include <vcl_cmath.h>
-#include <vnl/vnl_matrix.h>
-#include <vnl/vnl_vector.h>
-#include <vnl/vnl_double_3.h>
-#include <vnl/algo/vnl_svd.h>
-#include <vnl/algo/vnl_determinant.h>
-#include <vnl/vnl_math.h>
-#include <vnl/vnl_random.h>
-
 #include <rgrl/rgrl_feature_point.h>
 #include <rgrl/rgrl_feature_trace_pt.h>
 #include <rgrl/rgrl_match_set.h>
 #include <rgrl/rgrl_trans_affine.h>
-#include <rgrl/rgrl_trans_similarity.h>
 #include <rgrl/rgrl_estimator.h>
 #include <rgrl/rgrl_est_affine.h>
 #include <rgrl/rgrl_spline.h>
@@ -37,27 +25,27 @@ MAIN( test_convergence )
 {
   START( "various convergence objects" );
 
-  // there should be a test to make sure that all convergence objects converge when we are registering 
+  // there should be a test to make sure that all convergence objects converge when we are registering
   // identical data sets
 
   // currently, there is a chunk of code in each of rgrl_convergence_on_median_error and rgrl_convergence_on_weighted_error {.cxx}
-  // that looks like: 
+  // that looks like:
   // bool converged = false;
-  //  if (prev_status && current_view.regions_converged(prev_view) ) { 
-  //  ... 
-  // converged = vcl_abs( (new_error-old_error) / new_error ) < 1e-3; 
+  //  if (prev_status && current_view.regions_converged(prev_view) ) {
+  //  ...
+  // converged = vcl_abs( (new_error-old_error) / new_error ) < 1e-3;
   // ... }
 
   // one problem that I see is: converged will always be false after the first iteration
   // is this really a warranted assumption?
-  // although the error would most likely never be == to 0.0f on a real data set, 
-  // i could imagine a threshold incorporated such as: 
+  // although the error would most likely never be == to 0.0f on a real data set,
+  // i could imagine a threshold incorporated such as:
   // bool converged = false;
   //  if (new_error < tolerance_/200) converged = true;
-  //  else if (prev_status && current_view.regions_converged(prev_view) ) { 
-  //  ... 
-  // converged = vcl_abs( (new_error-old_error) / new_error ) < 1e-3; 
-  // ... }  
+  //  else if (prev_status && current_view.regions_converged(prev_view) ) {
+  //  ...
+  // converged = vcl_abs( (new_error-old_error) / new_error ) < 1e-3;
+  // ... }
   // however; i'm not sur ethat tolerance_/200 is a good measure of convergence
 
   SUMMARY();
