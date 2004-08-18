202d5536d34fcad5e7eb54d2b4b6106ed068e13e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-18 06:54:44 -0400

    with the SGI native compiler, the functionality of ios::binary is given by ios::openmode(0) [as defined by the macro vcl_ios_binary]

--- contrib/rpl/rgrl/tests/test_trans_reader.cxx
+++ contrib/rpl/rgrl/tests/test_trans_reader.cxx
@@ -1,6 +1,5 @@
 #include <testlib/testlib_test.h>
 
-#include <rgrl/rgrl_transformation.h>
 #include <rgrl/rgrl_transformation_sptr.h>
 #include <rgrl/rgrl_trans_reader.h>
 #include <rgrl/rgrl_trans_affine.h>
@@ -9,30 +8,31 @@
 
 MAIN_ARGS( test_trans_reader )
 {
-  if( argc < 2 ) {
-    vcl_cerr << "Please supply one transformation file." << vcl_endl;
+  if ( argc < 2 ) {
+    vcl_cerr << "Please supply one transformation file.\n";
     return 0;
   }
-  
+
   START( "smart reader of transformation" );
 
-  vcl_ifstream is( argv[1], vcl_ios::in|vcl_ios::binary );
-  if( !is ) {
+  vcl_ifstream is( argv[1], vcl_ios_in|vcl_ios_binary );
+  if ( !is ) {
     vcl_cerr << "Cannot open transformation file: " << argv[2] << vcl_endl;
     return 0;
   }
-  
-  // read transformation 
+
+  // read transformation
   rgrl_transformation_sptr trans_sptr = rgrl_trans_reader( is );
 
   TEST("Check validity of returned transformation", (!trans_sptr), false );
-  
-  if( trans_sptr ) {
-    TEST("Affine tye", trans_sptr->is_type( rgrl_trans_affine::type_id() ), true );
-    
+
+  if ( trans_sptr )
+  {
+    TEST("Affine type", trans_sptr->is_type( rgrl_trans_affine::type_id() ), true );
+
     rgrl_trans_affine* affine = rgrl_cast<rgrl_trans_affine*>(trans_sptr);
     TEST("Convert to affine type", affine!=0, true );
-    
+
     vnl_vector<double> t = affine->t();
     vnl_vector<double> true_t(2);
     true_t[0] = 1.0;
