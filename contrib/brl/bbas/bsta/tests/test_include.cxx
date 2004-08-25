53dd93bcbeb79755bd62ae7be59fe6009556dfbe
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-25 04:36:28 -0400

    corrected the #includes: bsta instead of bgrl

---
+++ contrib/brl/bbas/bsta/tests/test_include.cxx
@@ -0,0 +1,6 @@
+//if this file compiles, then the test passes
+#include <bbas/bsta/bsta_gauss.h>
+#include <bbas/bsta/bsta_histogram.h>
+#include <bbas/bsta/bsta_joint_histogram.h>
+
+int main() { return 0; }
