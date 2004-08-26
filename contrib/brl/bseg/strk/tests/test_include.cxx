ed917f88ce1eeb714b52b00136b797159cda1b42
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-26 05:00:41 -0400

    added test_include.cxx

---
+++ contrib/brl/bseg/strk/tests/test_include.cxx
@@ -0,0 +1,29 @@
+#include <bseg/strk/strk_art_info_model_sptr.h>
+#include <bseg/strk/strk_epi_point_sptr.h>
+#include <bseg/strk/strk_epi_seg_sptr.h>
+#include <bseg/strk/strk_tracking_face_2d_sptr.h>
+
+#include <bseg/strk/strk_epipolar_grouper_params.h>
+#include <bseg/strk/strk_info_model_tracker_params.h>
+#include <bseg/strk/strk_info_tracker_params.h>
+#include <bseg/strk/strk_region_info_params.h>
+#include <bseg/strk/strk_tracker_params.h>
+
+#include <bseg/strk/strk_art_info_model.h>
+#include <bseg/strk/strk_epi_point.h>
+#include <bseg/strk/strk_epi_seg.h>
+#include <bseg/strk/strk_epipolar_grouper.h>
+#include <bseg/strk/strk_info_model_tracker.h>
+#include <bseg/strk/strk_info_tracker.h>
+#include <bseg/strk/strk_region_info.h>
+#include <bseg/strk/strk_tracker.h>
+#include <bseg/strk/strk_tracking_face_2d.h>
+
+#include <bseg/strk/strk_art_model_display_process.h>
+#include <bseg/strk/strk_corr_tracker_process.h>
+#include <bseg/strk/strk_epipolar_grouper_process.h>
+#include <bseg/strk/strk_info_model_tracker_process.h>
+#include <bseg/strk/strk_info_tracker_process.h>
+#include <bseg/strk/strk_track_display_process.h>
+
+int main() { return 0; }
