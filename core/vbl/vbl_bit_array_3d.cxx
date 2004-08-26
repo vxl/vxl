574116a1d987f47b1627efa71788739eb9fc1ce8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-26 10:10:33 -0400

    adapted and extended the interface to that of vbl_array_3d<bool>

--- core/vbl/vbl_bit_array_3d.cxx
+++ core/vbl/vbl_bit_array_3d.cxx
@@ -17,31 +17,7 @@
 #include <vcl_iostream.h>
 #include <vcl_climits.h>
 
-// Default ctor
-vbl_bit_array_3d::vbl_bit_array_3d(unsigned int sizex,
-                                   unsigned int sizey,
-                                   unsigned int sizez)
-  : vbl_bit_array_3d_base(sizex, sizey, sizez),
-    data_(new unsigned char[(sizex*sizey*sizez+CHAR_BIT-1)/CHAR_BIT])
-{
-}
-
-vbl_bit_array_3d::vbl_bit_array_3d(unsigned int sizex,
-                                   unsigned int sizey,
-                                   unsigned int sizez,
-                                   bool v)
-  : vbl_bit_array_3d_base(sizex, sizey, sizez),
-    data_(new unsigned char[(sizex*sizey*sizez+CHAR_BIT-1)/CHAR_BIT])
-{
-  fill(v);
-}
-
-vbl_bit_array_3d::~vbl_bit_array_3d()
-{
-  delete[] data_;
-}
-
-void vbl_bit_array_3d::set(unsigned int i1, unsigned int i2, unsigned int i3, bool v)
+void vbl_bit_array_3d::put(unsigned int i1, unsigned int i2, unsigned int i3, bool v)
 {
   unsigned long byteindex;
   unsigned char bitindex;
@@ -74,8 +50,7 @@ void vbl_bit_array_3d::fill(bool v)
     for (int t=0; t< CHAR_BIT; t++)
       temp = (temp<<1)+1;
 
-  unsigned long memsize=(row1_count()*row2_count()*row3_count()+CHAR_BIT-1)/CHAR_BIT;
-  vcl_memset(data_, temp, memsize);
+  vcl_memset(data_, temp, this->size());
 }
 
 bool vbl_bit_array_3d::operator() (unsigned int i1, unsigned int i2, unsigned int i3) const
@@ -89,11 +64,11 @@ bool vbl_bit_array_3d::operator() (unsigned int i1, unsigned int i2, unsigned in
   return (data_[byteindex] & mask) != 0;
 }
 
-void vbl_bit_array_3d_base::index(unsigned x, unsigned y, unsigned z,
-                                  unsigned long & byteindex,
-                                  unsigned char & bitindex) const
+void vbl_bit_array_3d::index(unsigned x, unsigned y, unsigned z,
+                             unsigned long & byteindex,
+                             unsigned char & bitindex) const
 {
-  assert(int(x)<row1_count() && int(y)<row2_count() && int(z)<row3_count());
+  assert(x<row1_count_ && y<row2_count_ && z<row3_count_);
   unsigned long i = (z*row2_count()+y)*row1_count()+x;
 
   byteindex = i/CHAR_BIT;
@@ -105,11 +80,11 @@ void vbl_bit_array_3d_base::index(unsigned x, unsigned y, unsigned z,
 
 vcl_ostream &operator<<(vcl_ostream& os, vbl_bit_array_3d const& bitarray)
 {
-  for (int i=0; i< bitarray.row1_count(); i++)
+  for (unsigned int i=0; i< bitarray.row3_count(); ++i)
   {
-    for (int j=0; j< bitarray.row2_count(); j++)
+    for (unsigned int j=0; j< bitarray.row2_count(); ++j)
     {
-      for (int k=0; k< bitarray.row3_count(); k++)
+      for (unsigned int k=0; k< bitarray.row1_count(); ++k)
         os << (bitarray(k,j,i) ? 'x' : '.');
       os << vcl_endl;
     }
@@ -117,3 +92,58 @@ vcl_ostream &operator<<(vcl_ostream& os, vbl_bit_array_3d const& bitarray)
   }
   return os;
 }
+
+void vbl_bit_array_3d::construct(unsigned int m, unsigned int n, unsigned int p)
+{
+  // quick return if possible
+  if (m==0 || n==0 || p==0) { row1_count_=row2_count_=row3_count_=0; data_ = 0; return; }
+  row1_count_ = m; row2_count_ = n; row3_count_ = p;
+  data_ = new unsigned char [size() + 1];
+}
+
+//: Copy constructor
+vbl_bit_array_3d::vbl_bit_array_3d(vbl_bit_array_3d const& that)
+  : row1_count_(0), row2_count_(0), row3_count_(0), data_(0)
+{
+  if ( that.data_)
+  {
+    construct(that.row1_count_, that.row2_count_, that.row3_count_);
+    vcl_memcpy(data_, that.data_, size());
+  }
+}
+
+vbl_bit_array_3d::vbl_bit_array_3d(unsigned int m, unsigned int n, unsigned int p, bool v[])
+{
+  construct(m,n,p);
+  for (unsigned int x=0; x<m; ++x)
+    for (unsigned int y=0; y<n; ++y)
+      for (unsigned int z=0; z<p; ++z)
+        set(x,y,z, v[(n*z+y)*m+x]);
+}
+
+//: Assignment operator
+vbl_bit_array_3d& vbl_bit_array_3d::operator=(vbl_bit_array_3d const& that)
+{
+  if (row1_count_ != that.row1_count() ||
+      row2_count_ != that.row2_count() ||
+      row3_count_ != that.row3_count())
+    resize(that.row1_count_, that.row2_count_, that.row3_count_);
+
+  vcl_memcpy(data_, that.data_, size());
+  return *this;
+}
+
+bool vbl_bit_array_3d::operator==(vbl_bit_array_3d const &a) const
+{
+  if (row1_count_ != a.row1_count() ||
+      row2_count_ != a.row2_count() ||
+      row3_count_ != a.row3_count())
+    return false;
+  return 0 == vcl_memcmp(data_, a.data_, size());
+}
+
+unsigned long vbl_bit_array_3d::size() const
+{
+  return (row1_count_*row2_count_*row3_count_+CHAR_BIT-1)/CHAR_BIT;
+}
+
--- core/vbl/vbl_bit_array_3d.h
+++ core/vbl/vbl_bit_array_3d.h
@@ -14,55 +14,60 @@
 //  Modifications
 //   990717 Geoff Initial version.
 //   011023 Peter Vanroose - renamed and moved to vbl
+//   040826 Peter Vanroose - adapted interface to that of vbl_array_3d<T>
 //\endverbatim
 //
 //-----------------------------------------------------------------------------
 
 #include <vcl_iosfwd.h>
 
-class vbl_bit_array_3d_base
-{
- public:
-  vbl_bit_array_3d_base(int a, int b, int c)
-    : row1_count_(a),
-      row2_count_(b),
-      row3_count_(c)
-    {}
-
-  int row1_count() const { return row1_count_; }
-  int row2_count() const { return row2_count_; }
-  int row3_count() const { return row3_count_; }
-
- protected:
-  int row1_count_;
-  int row2_count_;
-  int row3_count_;
-
-  // Helpers-------------------------------------------------------------------
-
-  void index(unsigned int x, unsigned int y, unsigned int z,
-             unsigned long &byteindex, unsigned char &bitindex) const;
-};
-
-
-class vbl_bit_array_3d : public vbl_bit_array_3d_base
+class vbl_bit_array_3d
 {
  public:
   // Constructors/Destructor---------------------------------------------------
 
   //: Create a bitarray of the specified size, without initialising elements
-  vbl_bit_array_3d(unsigned int sizex, unsigned int sizey, unsigned int sizez);
+  vbl_bit_array_3d(unsigned int sizex, unsigned int sizey, unsigned int sizez)
+  { construct(sizex, sizey, sizez); }
+
   //: Create a bitarray of the specified size, with initialisation of elements
-  vbl_bit_array_3d(unsigned int sizex, unsigned int sizey, unsigned int sizez, bool v);
-  ~vbl_bit_array_3d();
+  vbl_bit_array_3d(unsigned int sizex, unsigned int sizey, unsigned int sizez, bool v)
+  { construct(sizex, sizey, sizez); fill(v); }
+  //: Create a bitarray of the specified size, with initialisation of elements
+  vbl_bit_array_3d(unsigned int sizex, unsigned int sizey, unsigned int sizez, bool v[]);
+  //: Copy constructor
+  vbl_bit_array_3d(vbl_bit_array_3d const &);
+  // Destructor
+  ~vbl_bit_array_3d() { delete[] data_; }
+
+  //: Assignment operator
+  vbl_bit_array_3d& operator=(vbl_bit_array_3d const&);
+
+  //: Comparison
+  bool operator==(vbl_bit_array_3d const &a) const;
+  //:
+  bool operator!=(vbl_bit_array_3d const &a) const { return ! operator==(a); }
 
   // Data Access---------------------------------------------------------------
 
   //: Set all cell values to v
   void fill(bool v);
 
+  //: Delete contents and resize to m rows x n cols x p layers
+  void resize(unsigned int m, unsigned int n, unsigned int p) { destruct(); construct(m,n,p); }
+
+  //: make as if default-constructed.
+  void clear() { if (data_) { destruct(); row1_count_=row2_count_=row3_count_=0; } }
+
+  //: Set the value of a cell
+  void put(unsigned int i1, unsigned int i2, unsigned int i3, bool v);
+
   //: Set the value of a cell; default is to set the value on
-  void set(unsigned int i1, unsigned int i2, unsigned int i3, bool v=true);
+  void set(unsigned int i1, unsigned int i2, unsigned int i3, bool v=true)
+  { put(i1, i2, i3, v); }
+
+  //: Return the value of a cell
+  bool get(unsigned int i1, unsigned int i2, unsigned int i3) const;
 
   //: Change the value of a cell
   void flip(unsigned int i1, unsigned int i2, unsigned int i3);
@@ -70,10 +75,27 @@ class vbl_bit_array_3d : public vbl_bit_array_3d_base
   //: Return the value of a cell
   bool operator() (unsigned int i1, unsigned int i2, unsigned int i3) const;
 
- protected:
+  unsigned int row1_count() const { return row1_count_; }
+  unsigned int row2_count() const { return row2_count_; }
+  unsigned int row3_count() const { return row3_count_; }
+  //: Number of bytes allocated by the data
+  unsigned long size() const;
+
+ private:
   // Data Members--------------------------------------------------------------
 
+  unsigned int row1_count_;
+  unsigned int row2_count_;
+  unsigned int row3_count_;
   unsigned char *data_;
+
+  // Helpers-------------------------------------------------------------------
+
+  void destruct() { delete[] data_; data_=0; }
+  void construct(unsigned int m, unsigned int n, unsigned int p);
+
+  void index(unsigned int x, unsigned int y, unsigned int z,
+             unsigned long &byteindex, unsigned char &bitindex) const;
 };
 
 vcl_ostream &operator<<(vcl_ostream &os, vbl_bit_array_3d const&);
