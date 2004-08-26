460013c22a71a5346333162e09d238183f665b3b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-26 05:48:37 -0400

    removed specific assignment

--- config/site.mk
+++ config/site.mk
@@ -20,7 +20,6 @@ endif
 # Leuven
 ifeq (1,$(USE_LEUVENVERSION))
  DEFINES += -DLEUVENVERSION
- TJWD := /freeware/bin/perl $(IUEROOT)/Scripts/Perl/tjwd
 
  ifeq ($(OS),SunOS5)
   VTK_EXTRA_LIBS += -lxgl
