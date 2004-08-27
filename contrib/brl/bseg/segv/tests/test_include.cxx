c8fc91cafaa4d72a6fda71e5d49c5eb472423f5c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-27 12:09:29 -0400

    added test_include.cxx

---
+++ contrib/brl/bseg/segv/tests/test_include.cxx
@@ -0,0 +1,8 @@
+#include <bseg/segv/dll.h>
+#include <bseg/segv/segv_menus.h>
+#include <bseg/segv/segv_segmentation_manager.h>
+#include <bseg/segv/segv_vtol2D_tableau.h>
+#include <bseg/segv/segv_vtol2D_tableau_sptr.h>
+#include <bseg/segv/segv_vtol_soview2D.h>
+
+int main() { return 0; }
