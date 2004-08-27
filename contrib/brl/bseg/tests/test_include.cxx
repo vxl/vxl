f0461f7e88516a94f9e342ff7d58885642f8062e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-27 11:58:52 -0400

    removed bseg/segv/*.h includes since those require vgui

--- contrib/brl/bseg/tests/test_include.cxx
+++ contrib/brl/bseg/tests/test_include.cxx
@@ -55,12 +55,6 @@
 #include <bseg/sdet/sdet_watershed_region_proc.h>
 #include <bseg/sdet/sdet_watershed_region_proc_params.h>
 
-#include <bseg/segv/segv_menus.h>
-#include <bseg/segv/segv_segmentation_manager.h>
-#include <bseg/segv/segv_vtol2D_tableau.h>
-#include <bseg/segv/segv_vtol2D_tableau_sptr.h>
-#include <bseg/segv/segv_vtol_soview2D.h>
-
 #include <bseg/strk/strk_art_info_model.h>
 #include <bseg/strk/strk_art_info_model_sptr.h>
 #include <bseg/strk/strk_art_model_display_process.h>
