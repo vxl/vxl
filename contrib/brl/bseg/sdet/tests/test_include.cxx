d285c5a963d2857afdaba59ed6936d8d504aeb6c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-27 13:57:30 -0400

    added test_include.cxx

---
+++ contrib/brl/bseg/sdet/tests/test_include.cxx
@@ -0,0 +1,23 @@
+#include <bseg/sdet/sdet_contour.h>
+#include <bseg/sdet/sdet_detector.h>
+#include <bseg/sdet/sdet_detector_params.h>
+#include <bseg/sdet/sdet_edge_champher.h>
+#include <bseg/sdet/sdet_edgel_regions.h>
+#include <bseg/sdet/sdet_fit_lines.h>
+#include <bseg/sdet/sdet_fit_lines_params.h>
+#include <bseg/sdet/sdet_grid_finder.h>
+#include <bseg/sdet/sdet_grid_finder_params.h>
+#include <bseg/sdet/sdet_harris_detector.h>
+#include <bseg/sdet/sdet_harris_detector_params.h>
+#include <bseg/sdet/sdet_region.h>
+#include <bseg/sdet/sdet_region_edge.h>
+#include <bseg/sdet/sdet_region_edge_sptr.h>
+#include <bseg/sdet/sdet_region_proc.h>
+#include <bseg/sdet/sdet_region_proc_params.h>
+#include <bseg/sdet/sdet_region_sptr.h>
+#include <bseg/sdet/sdet_vehicle_finder.h>
+#include <bseg/sdet/sdet_vehicle_finder_params.h>
+#include <bseg/sdet/sdet_watershed_region_proc.h>
+#include <bseg/sdet/sdet_watershed_region_proc_params.h>
+
+int main() { return 0; }
---
+++ contrib/brl/bseg/vpro/tests/test_include.cxx
@@ -0,0 +1,23 @@
+#include <bseg/vpro/vpro_basis_generator_process.h>
+#include <bseg/vpro/vpro_capture_process.h>
+#include <bseg/vpro/vpro_edge_line_process.h>
+#include <bseg/vpro/vpro_edge_process.h>
+#include <bseg/vpro/vpro_epipolar_space_process.h>
+#include <bseg/vpro/vpro_fourier_params.h>
+#include <bseg/vpro/vpro_fourier_process.h>
+#include <bseg/vpro/vpro_frame_diff_params.h>
+#include <bseg/vpro/vpro_frame_diff_process.h>
+#include <bseg/vpro/vpro_grid_finder_process.h>
+#include <bseg/vpro/vpro_harris_corner_process.h>
+#include <bseg/vpro/vpro_ihs_process.h>
+#include <bseg/vpro/vpro_line_fit_process.h>
+#include <bseg/vpro/vpro_lucas_kanade_process.h>
+#include <bseg/vpro/vpro_motion_params.h>
+#include <bseg/vpro/vpro_motion_process.h>
+#include <bseg/vpro/vpro_region_process.h>
+#include <bseg/vpro/vpro_spatial_filter_params.h>
+#include <bseg/vpro/vpro_spatial_filter_process.h>
+#include <bseg/vpro/vpro_video_process.h>
+#include <bseg/vpro/vpro_video_process_sptr.h>
+
+int main() { return 0; }
