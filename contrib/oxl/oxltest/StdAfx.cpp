83298de98e8b842abd70a49aafe3ca71a7d027c5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-08-28 07:47:19 -0400

    removed oxltest; see oxl/VPLAYER for an example of a VC++ console application

--- contrib/oxl/oxltest/StdAfx.cpp
+++
@@ -1,9 +0,0 @@
-// \file
-// source file that includes just the standard includes
-// oxltest.pch will be the pre-compiled header
-// StdAfx.obj will contain the pre-compiled type information
-
-#include "StdAfx.h"
-
-// TODO: reference any additional headers you need in StdAfx.h
-// and not in this file
