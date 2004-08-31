40e64daf56ece1e3bb0fe0c47b9ba26c9ab232af
Graham Vincent <grv@users.sourceforge.net>
2004-08-31 07:47:48 -0400

    added templates for vimt3d_reconstruct_from_grid

---
+++ contrib/mul/vimt3d/Templates/vimt3d_reconstruct_from_grid+int.double-.cxx
@@ -0,0 +1,2 @@
+#include <vimt3d/vimt3d_reconstruct_from_grid.txx>
+VIMT3D_RECONSTRUCT_FROM_GRID_INSTANTIATE(int,double);
---
+++ contrib/mul/vimt3d/Templates/vimt3d_reconstruct_from_grid+uchar.double-.cxx
@@ -0,0 +1,2 @@
+#include <vimt3d/vimt3d_reconstruct_from_grid.txx>
+VIMT3D_RECONSTRUCT_GRID_TRILIN_INSTANTIATE(unsigned char,double);
