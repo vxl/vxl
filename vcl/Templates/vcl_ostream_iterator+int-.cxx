fd2821de2a279842e46ce642062a15ea871a5f42
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-01 12:04:27 -0400

    this was a no-op

--- vcl/Templates/vcl_ostream_iterator+int-.cxx
+++
@@ -1,6 +0,0 @@
-#include <vcl_iterator.txx>
-
-// which platform *does* need this?
-//#if !defined(VCL_SGI_CC) && !defined(VCL_WIN32) && !defined(GNU_LIBSTDCXX_V3) && !defined(__ICC)
-//template class vcl_ostream_iterator<int>;
-//#endif
