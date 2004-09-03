eecf54563079c0b3527341095ba63850ef457a96
Graham Vincent <grv@users.sourceforge.net>
2004-09-03 11:46:46 -0400

    Added the flexibility to set up the vimt transform in millimeters and to choose (0,0,0) as the origin i.e. override the defaults.

--- contrib/mul/vimt3d/vimt3d_load.cxx
+++ contrib/mul/vimt3d/vimt3d_load.cxx
@@ -8,13 +8,29 @@
 #include <vil3d/vil3d_property.h>
 
 //: Create a transform from the properties of image resource.
-// The transform will be from world co-ordinates in metres to image co-ordinates.
-vimt3d_transform_3d vimt3d_load_transform(const vil3d_image_resource_sptr &im)
+// The transform will be from world co-ordinates in metres to image co-ordinates (or mm if requested)
+vimt3d_transform_3d vimt3d_load_transform(const vil3d_image_resource_sptr &im, bool use_millimetres, bool use_000_as_origin)
 {
   float width[3] = { 1.0f, 1.0f, 1.0f };
   bool valid_width = im->get_property(vil3d_property_voxel_size, width);
+
+  if (use_millimetres)
+  {
+      width[0] *= 1000;
+      width[1] *= 1000;
+      width[2] *= 1000;
+  }
+
   float offset[3] = { 0.0f, 0.0f, 0.0f };
   bool valid_offset = im->get_property(vil3d_property_origin_offset, offset);
+
+  // force to use 000 as the origin
+  if (use_000_as_origin)
+  {
+      offset[0] = 0.0f;
+      offset[1] = 0.0f;
+      offset[2] = 0.0f;
+  }
 
   vimt3d_transform_3d t;
 
--- contrib/mul/vimt3d/vimt3d_load.h
+++ contrib/mul/vimt3d/vimt3d_load.h
@@ -10,6 +10,9 @@
 #include <vimt3d/vimt3d_transform_3d.h>
 
 //: Create a transform from the properties of image resource.
-vimt3d_transform_3d vimt3d_load_transform(const vil3d_image_resource_sptr &im);
+// The following parameters allow teh use of this method in legacy code
+// \param use_millimetres if true load transform in millmetres, else uses metres
+// \param use_000_as_origin if true, uses image()(0,0,0) as origin 
+vimt3d_transform_3d vimt3d_load_transform(const vil3d_image_resource_sptr &im, bool use_millimetres=false, bool use_000_as_origin=false);
 
 #endif // vimt3d_load_h_
