f161a60c5cc3765c4fb121edd7c8bd16e9c6b2f6
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-09 06:46:05 -0400

    moved template instantiations into source file using them

--- core/vbl/examples/vbl_smart_ptr_example.cxx
+++ core/vbl/examples/vbl_smart_ptr_example.cxx
@@ -121,4 +121,14 @@ void main2()
 //--------------------------------------------------------------------------------
 
 int main() { main1(); main2(); return 0; }
-// fsm: sunpro needs template instantiations to go into Templates
+
+#include <vbl/vbl_smart_ptr.txx>
+#include <vcl_list.txx>
+#include <vcl_algorithm.txx>
+
+VBL_SMART_PTR_INSTANTIATE(example_sp);
+VBL_SMART_PTR_INSTANTIATE(bigmatrix_impl);
+
+VCL_OPERATOR_NE_INSTANTIATE(vbl_smart_ptr<example_sp>);
+VCL_CONTAINABLE_INSTANTIATE(vbl_smart_ptr<example_sp>);
+VCL_LIST_INSTANTIATE(vbl_smart_ptr<example_sp>);
