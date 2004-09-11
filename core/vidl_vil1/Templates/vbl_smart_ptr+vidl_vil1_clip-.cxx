682a84f5730246472a2a4da3e6b177743b0efc9a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-11 07:49:04 -0400

    avoid automatic instantiation of other smart pointers

--- core/vidl_vil1/Templates/vbl_smart_ptr+vidl_vil1_clip-.cxx
+++ core/vidl_vil1/Templates/vbl_smart_ptr+vidl_vil1_clip-.cxx
@@ -1,6 +1,8 @@
 // This avoids automatic instantiation of other smart pointers:
+#define vidl_vil1_codec_sptr_h
+class vidl_vil1_codec_sptr {};
 #define vidl_vil1_frame_sptr_h
-class vidl_vil1_frame_sptr;
+class vidl_vil1_frame_sptr {};
 
 #include <vidl_vil1/vidl_vil1_clip.h>
 #include <vbl/vbl_smart_ptr.txx>
