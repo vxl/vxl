394bc1978b05e2acecd60ec9aeb79e8d570e198c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-17 15:06:56 -0400

    made beat(), parent() and motion() non-virtual - they just return a member and should not be overloaded

--- core/vcsl/vcsl_spatial.h
+++ core/vcsl/vcsl_spatial.h
@@ -1,28 +1,32 @@
 // This is core/vcsl/vcsl_spatial.h
-#ifndef vcsl_spatial_h
-#define vcsl_spatial_h
-#ifdef VCL_NEEDS_PRAGMA_INTERFACE
-#pragma interface
-#endif
+#ifndef vcsl_spatial_h_
+#define vcsl_spatial_h_
 //:
 // \file
+// \brief A spatial coordinate system
 // \author François BERTEL
 //
 // \verbatim
-// Modifications
-// 2000/07/10 François BERTEL Creation
-// 2001/04/10 Ian Scott (Manchester) Converted perceps header to doxygen
-// 2002/01/22 Peter Vanroose - return type of from_local_to_cs() changed from ptr to non-ptr
-// 2002/01/28 Peter Vanroose - vcl_vector members changed from ptr to non-ptr
+//  Modifications
+//   2000/07/10 François BERTEL Creation
+//   2001/04/10 Ian Scott (Manchester) Converted perceps header to doxygen
+//   2002/01/22 Peter Vanroose - return type of from_local_to_cs() changed from ptr to non-ptr
+//   2002/01/28 Peter Vanroose - vcl_vector members changed from ptr to non-ptr
+//   2004/09/17 Peter Vanroose - made beat(), parent(), motion() non-virtual: they just return a member and should not be overloaded
 // \endverbatim
 
-#include <vcsl/vcsl_spatial_sptr.h>
-
 #include <vcsl/vcsl_coordinate_system.h>
-#include <vnl/vnl_vector.h>
-#include <vcl_vector.h>
+#include <vcsl/vcsl_spatial_sptr.h>
 #include <vcsl/vcsl_spatial_transformation_sptr.h>
 #include <vcsl/vcsl_graph_sptr.h>
+#include <vnl/vnl_vector.h>
+#include <vcl_vector.h>
+class vcsl_cartesian_2d;
+class vcsl_polar;
+class vcsl_cartesian_3d;
+class vcsl_cylindrical;
+class vcsl_spherical;
+
 
 // This is needed for icc-7.0 to solve a strange link problem.
 #include <vcl_compiler.h>
@@ -33,14 +37,6 @@
 #define VCSL_SPATIAL_VECTOR_BOOL vcl_vector<bool>
 #endif
 
-// Because VXL does not use dynamic_cast<> :-(
-
-class vcsl_cartesian_2d;
-class vcsl_polar;
-
-class vcsl_cartesian_3d;
-class vcsl_cylindrical;
-class vcsl_spherical;
 
 //: A spatial coordinate system
 // class invariants:
@@ -54,53 +50,56 @@ class vcsl_spatial
   // Constructors/Destructor
   //***************************************************************************
 
-  //: Default constructor
-  explicit vcsl_spatial(void) {}
+  // Default constructor
+  vcsl_spatial() {}
 
-  //: Destructor
+  // Destructor
   virtual ~vcsl_spatial();
 
   //***************************************************************************
-  // Because VXL does not use dynamic_cast<> :-(
+  // Because VXL does not necessarily use dynamic_cast<>
   //***************************************************************************
 
-  virtual const vcsl_spatial *cast_to_spatial(void) const { return this; }
-  virtual const vcsl_cartesian_2d *cast_to_cartesian_2d(void) const {return 0; }
-  virtual const vcsl_polar *cast_to_polar(void) const {return 0; }
-  virtual const vcsl_cartesian_3d *cast_to_cartesian_3d(void) const {return 0; }
-  virtual const vcsl_cylindrical *cast_to_cylindrical(void) const {return 0; }
-  virtual const vcsl_spherical *cast_to_spherical(void) const {return 0; }
+  virtual const vcsl_spatial *cast_to_spatial() const { return this; }
+  virtual const vcsl_cartesian_2d *cast_to_cartesian_2d() const {return 0; }
+  virtual const vcsl_polar *cast_to_polar() const {return 0; }
+  virtual const vcsl_cartesian_3d *cast_to_cartesian_3d() const {return 0; }
+  virtual const vcsl_cylindrical *cast_to_cylindrical() const {return 0; }
+  virtual const vcsl_spherical *cast_to_spherical() const {return 0; }
 
   //***************************************************************************
   // Status report
   //***************************************************************************
 
   //: Return the list of time clocks
-  virtual vcl_vector<double> beat(void) const { return beat_; }
+  vcl_vector<double> beat() const { return beat_; }
+
+  //: Return the time duration
+  unsigned int duration() const { return beat_.size(); }
 
   //: Return the list of parent coordinate system along the time
-  virtual vcl_vector<vcsl_spatial_sptr> parent(void) const { return parent_; }
+  vcl_vector<vcsl_spatial_sptr> parent() const { return parent_; }
 
   //: Return the list of transformations along the time
-  virtual vcl_vector<vcsl_spatial_transformation_sptr> motion(void) const
-  { return motion_; }
+  vcl_vector<vcsl_spatial_transformation_sptr> motion() const {return motion_;}
 
   //: Is `time' between the two time bounds ?
-  virtual bool valid_time(double time) const;
+  bool valid_time(double time) const;
 
   //***************************************************************************
   // Status setting
   //***************************************************************************
 
   //: Set the list of time clocks
-  virtual void set_beat(vcl_vector<double> const& new_beat);
+  void set_beat(vcl_vector<double> const& new_beat) { beat_=new_beat; }
 
   //: Set the list of parent coordinate system along the time
-  virtual void set_parent(vcl_vector<vcsl_spatial_sptr> const& new_parent);
+  void set_parent(vcl_vector<vcsl_spatial_sptr> const& new_parent);
 
   //: Set the list of transformations along the time
-  virtual void set_motion(vcl_vector<vcsl_spatial_transformation_sptr> const& new_motion);
+  void set_motion(vcl_vector<vcsl_spatial_transformation_sptr> const& m) { motion_=m; }
 
+  //: Set the unique parent and the unique motion
   //
   // Can be called to initialize a CS for the first time.  Dynamically
   // allocates space for parent_ and motion_.  Makes new_parent and new_motion
@@ -112,8 +111,8 @@ class vcsl_spatial
   //
   // \param new_parent CS that 'this' is derived from.
   // \param new_motion The transformation from new_parent to 'this'.
-  virtual void set_unique(const vcsl_spatial_sptr &new_parent,
-                          const vcsl_spatial_transformation_sptr &new_motion);
+  void set_unique(const vcsl_spatial_sptr &new_parent,
+                  const vcsl_spatial_transformation_sptr &new_motion);
 
   //***************************************************************************
   // Basic operations
@@ -141,11 +140,11 @@ class vcsl_spatial
 
   virtual void set_graph(const vcsl_graph_sptr &new_graph);
 
-  //: WARNING: Only used by vcsl_spatial and vcsl_graph
-  //  DO NOT USE IT DIRECTLY
-  virtual void set_reached(const bool &new_reached);
-
-  virtual bool reached(void) const;
+ private:
+  // Only used by vcsl_spatial and vcsl_graph
+  void set_reached(const bool &new_reached) { reached_=new_reached; }
+  bool reached() const { return reached_; }
+  friend class vcsl_graph;
 
   //***************************************************************************
   // Implementation
@@ -192,4 +191,4 @@ class vcsl_spatial
   bool reached_;
 };
 
-#endif // vcsl_spatial_h
+#endif // vcsl_spatial_h_
