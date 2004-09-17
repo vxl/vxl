7bba7028bba52d795147412614a8a20ef0f2e820
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-17 15:05:51 -0400

    made composition() non-virtual - it just returns a member and should not be overloaded

--- core/vcsl/vcsl_composition.cxx
+++ core/vcsl/vcsl_composition.cxx
@@ -1,13 +1,6 @@
 // This is core/vcsl/vcsl_composition.cxx
-#ifdef VCL_NEEDS_PRAGMA_INTERFACE
-#pragma implementation
-#endif
 #include "vcsl_composition.h"
 #include <vcl_cassert.h>
-
-//***************************************************************************
-// Status report
-//***************************************************************************
 
 //---------------------------------------------------------------------------
 // Is `this' invertible at time `time'?
@@ -30,34 +23,14 @@ bool vcsl_composition::is_invertible(double time) const
 //---------------------------------------------------------------------------
 // Is `this' correctly set ?
 //---------------------------------------------------------------------------
-bool vcsl_composition::is_valid(void) const
+bool vcsl_composition::is_valid() const
 {
   vcl_vector<vcsl_spatial_transformation_sptr>::const_iterator i;
+  for (i=transformations_.begin(); i!=transformations_.end(); ++i)
+    if (!(*i)->is_valid()) return false;
 
-  bool result=true;
-  for (i=transformations_.begin();result&&i!=transformations_.end();++i)
-    result=(*i)->is_valid();
-
-  return result;
+  return true;
 }
-
-//***************************************************************************
-// Status setting
-//***************************************************************************
-
-//---------------------------------------------------------------------------
-// Set the list of transformations of the composition
-// The transformations are performed in the order of the list
-//---------------------------------------------------------------------------
-void
-vcsl_composition::set_composition(vcl_vector<vcsl_spatial_transformation_sptr> const& new_transformations)
-{
-  transformations_=new_transformations;
-}
-
-//***************************************************************************
-// Basic operations
-//***************************************************************************
 
 //---------------------------------------------------------------------------
 // Image of `v' by `this'
--- core/vcsl/vcsl_composition.h
+++ core/vcsl/vcsl_composition.h
@@ -1,28 +1,21 @@
 // This is core/vcsl/vcsl_composition.h
-#ifndef vcsl_composition_h
-#define vcsl_composition_h
-#ifdef VCL_NEEDS_PRAGMA_INTERFACE
-#pragma interface
-#endif
+#ifndef vcsl_composition_h_
+#define vcsl_composition_h_
 //:
 // \file
 // \brief Composition of transformations
 // \author François BERTEL
 //
 // \verbatim
-// Modifications
-// 2000/06/28 François BERTEL Creation. Adapted from IUE
-// 2002/01/22 Peter Vanroose - return type of execute() and inverse() changed to non-ptr
-// 2002/01/28 Peter Vanroose - vcl_vector member transformations_ changed to non-ptr
+//  Modifications
+//   2000/06/28 François BERTEL Creation. Adapted from IUE
+//   2002/01/22 Peter Vanroose - return type of execute() and inverse() changed to non-ptr
+//   2002/01/28 Peter Vanroose - vcl_vector member transformations_ changed to non-ptr
+//   2004/09/17 Peter Vanroose - made composition() non-virtual - it just returns a member and should not be overloaded
 // \endverbatim
 
-
-#include <vcsl/vcsl_composition_sptr.h>
-
-//*****************************************************************************
-// External declarations
-//*****************************************************************************
 #include <vcsl/vcsl_spatial_transformation.h>
+#include <vcsl/vcsl_composition_sptr.h>
 #include <vcl_vector.h>
 
 //: Composition of transformations
@@ -36,10 +29,10 @@ class vcsl_composition
   // Constructors/Destructor
   //***************************************************************************
 
-  //: Default constructor
-  explicit vcsl_composition(void) {}
+  // Default constructor
+  vcsl_composition() {}
 
-  //: Destructor
+  // Destructor
   virtual ~vcsl_composition() {}
 
   //***************************************************************************
@@ -48,14 +41,15 @@ class vcsl_composition
 
   //: Is `this' invertible at time `time'?
   //  REQUIRE: valid_time(time)
+  // Pure virtual function of vcsl_spatial_transformation
   virtual bool is_invertible(double time) const;
 
   //: Is `this' correctly set ?
-  virtual bool is_valid(void) const;
+  // Virtual function of vcsl_spatial_transformation
+  virtual bool is_valid() const;
 
   //: Return the list of transformations
-  virtual vcl_vector<vcsl_spatial_transformation_sptr> composition(void) const
-  { return transformations_; }
+  vcl_vector<vcsl_spatial_transformation_sptr> composition() const { return transformations_; }
 
   //***************************************************************************
   // Status setting
@@ -63,7 +57,7 @@ class vcsl_composition
 
   //: Set the list of transformations of the composition
   //  The transformations are performed in the order of the list
-  virtual void set_composition(vcl_vector<vcsl_spatial_transformation_sptr> const& new_transformations);
+  void set_composition(vcl_vector<vcsl_spatial_transformation_sptr> const& t) { transformations_=t; }
 
   //***************************************************************************
   // Basic operations
@@ -71,16 +65,18 @@ class vcsl_composition
 
   //: Image of `v' by `this'
   //  REQUIRE: is_valid()
+  // Pure virtual function of vcsl_spatial_transformation
   virtual vnl_vector<double> execute(const vnl_vector<double> &v,
                                      double time) const;
 
   //: Image of `v' by the inverse of `this'
   //  REQUIRE: is_valid()
   //  REQUIRE: is_invertible(time)
+  // Pure virtual function of vcsl_spatial_transformation
   virtual vnl_vector<double> inverse(const vnl_vector<double> &v,
                                      double time) const;
  protected:
   vcl_vector<vcsl_spatial_transformation_sptr> transformations_;
 };
 
-#endif // vcsl_composition_h
+#endif // vcsl_composition_h_
