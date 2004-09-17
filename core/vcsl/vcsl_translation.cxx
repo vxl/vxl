bd4bc5a474019498fe25ad0dda1621617d06221a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-17 15:07:20 -0400

    made vector() non-virtual - it just returns a member and should not be overloaded

--- core/vcsl/vcsl_translation.cxx
+++ core/vcsl/vcsl_translation.cxx
@@ -1,16 +1,6 @@
 // This is core/vcsl/vcsl_translation.cxx
-#ifdef VCL_NEEDS_PRAGMA_INTERFACE
-#pragma implementation
-#endif
-//:
-// \file
-
 #include "vcsl_translation.h"
 #include <vcl_cassert.h>
-
-//***************************************************************************
-// Status report
-//***************************************************************************
 
 //---------------------------------------------------------------------------
 // Is `this' invertible at time `time'?
@@ -25,19 +15,6 @@ bool vcsl_translation::is_invertible(double time) const
 }
 
 //---------------------------------------------------------------------------
-// Is `this' correctly set ?
-//---------------------------------------------------------------------------
-bool vcsl_translation::is_valid(void) const
-{
-  return (beat_.size()==0&&vector_.size()==1) ||
-         (beat_.size()==interpolator_.size()+1&&beat_.size()==vector_.size());
-}
-
-//***************************************************************************
-// Transformation parameters
-//***************************************************************************
-
-//---------------------------------------------------------------------------
 // Set the parameters of a static translation
 //---------------------------------------------------------------------------
 void vcsl_translation::set_static(vnl_vector<double> const& new_vector)
@@ -45,18 +22,6 @@ void vcsl_translation::set_static(vnl_vector<double> const& new_vector)
   vector_.clear(); vector_.push_back(new_vector);
   vcsl_spatial_transformation::set_static();
 }
-
-//---------------------------------------------------------------------------
-//: Set the direction vector variation along the time
-//---------------------------------------------------------------------------
-void vcsl_translation::set_vector(list_of_vectors const& new_vector)
-{
-  vector_=new_vector;
-}
-
-//***************************************************************************
-// Basic operations
-//***************************************************************************
 
 //---------------------------------------------------------------------------
 // Image of `v' by `this'
@@ -101,7 +66,7 @@ vnl_vector<double> vcsl_translation::inverse(const vnl_vector<double> &v,
 //---------------------------------------------------------------------------
 vnl_vector<double> vcsl_translation::vector_value(double time) const
 {
-  if (beat_.size()==0) // static
+  if (this->duration()==0) // static
     return vector_[0];
   else
   {
--- core/vcsl/vcsl_translation.h
+++ core/vcsl/vcsl_translation.h
@@ -1,23 +1,22 @@
 // This is core/vcsl/vcsl_translation.h
-#ifndef vcsl_translation_h
-#define vcsl_translation_h
-#ifdef VCL_NEEDS_PRAGMA_INTERFACE
-#pragma interface
-#endif
+#ifndef vcsl_translation_h_
+#define vcsl_translation_h_
 //:
 // \file
+// \brief Translation transformation
 // \author François BERTEL
 //
 // \verbatim
-// Modifications
-// 2000/06/28 François BERTEL Creation. Adapted from IUE
-// 2001/04/10 Ian Scott (Manchester) Converted perceps header to doxygen
-// 2002/01/22 Peter Vanroose - return type of execute() and inverse() changed to non-ptr
-// 2002/01/28 Peter Vanroose - vcl_vector member vector_ changed to non-ptr
+//  Modifications
+//   2000/06/28 François BERTEL Creation. Adapted from IUE
+//   2001/04/10 Ian Scott (Manchester) Converted perceps header to doxygen
+//   2002/01/22 Peter Vanroose - return type of execute() and inverse() changed to non-ptr
+//   2002/01/28 Peter Vanroose - vcl_vector member vector_ changed to non-ptr
+//   2004/09/17 Peter Vanroose - made vector() non-virtual - it just returns a member and should not be overloaded
 // \endverbatim
 
-#include <vcsl/vcsl_translation_sptr.h>
 #include <vcsl/vcsl_spatial_transformation.h>
+#include <vcsl/vcsl_translation_sptr.h>
 #include <vnl/vnl_vector.h>
 
 //: Translation transformation
@@ -25,15 +24,14 @@ class vcsl_translation
   :public vcsl_spatial_transformation
 {
  public:
-
   //***************************************************************************
   // Constructors/Destructor
   //***************************************************************************
 
-  //: Default constructor
-  explicit vcsl_translation(void) {}
+  // Default constructor
+  vcsl_translation() {}
 
-  //: Destructor
+  // Destructor
   virtual ~vcsl_translation() {}
 
   //***************************************************************************
@@ -42,10 +40,15 @@ class vcsl_translation
 
   //: Is `this' invertible at time `time'?
   //  REQUIRE: valid_time(time)
+  // Pure virtual function of vcsl_spatial_transformation
   virtual bool is_invertible(double time) const;
 
   //: Is `this' correctly set ?
-  virtual bool is_valid(void) const;
+  // Virtual function of vcsl_spatial_transformation
+  virtual bool is_valid() const
+  { return vcsl_spatial_transformation::is_valid() &&
+           ((this->duration()==0&&vector_.size()==1) ||
+            this->duration()==vector_.size()); }
 
   //***************************************************************************
   // Transformation parameters
@@ -55,10 +58,10 @@ class vcsl_translation
   void set_static(vnl_vector<double> const& new_vector);
 
   //: Set the direction vector variation along the time
-  virtual void set_vector(list_of_vectors const& new_vector);
+  void set_vector(list_of_vectors const&new_vector){vector_=new_vector;}
 
   //: Return the angle variation along the time
-  virtual list_of_vectors vector(void) const { return vector_; }
+  list_of_vectors vector() const { return vector_; }
 
   //***************************************************************************
   // Basic operations
@@ -66,21 +69,23 @@ class vcsl_translation
 
   //: Image of `v' by `this'
   //  REQUIRE: is_valid()
+  // Pure virtual function of vcsl_spatial_transformation
   virtual vnl_vector<double> execute(const vnl_vector<double> &v,
                                      double time) const;
 
   //: Image of `v' by the inverse of `this'
   //  REQUIRE: is_valid()
   //  REQUIRE: is_invertible(time)
+  // Pure virtual function of vcsl_spatial_transformation
   virtual vnl_vector<double> inverse(const vnl_vector<double> &v,
                                      double time) const;
 
  protected:
   //: Compute the value of the parameter at time `time'
-  virtual vnl_vector<double> vector_value(double time) const;
+  vnl_vector<double> vector_value(double time) const;
 
   //: Direction vector variation along the time
   list_of_vectors vector_;
 };
 
-#endif // vcsl_translation_h
+#endif // vcsl_translation_h_
