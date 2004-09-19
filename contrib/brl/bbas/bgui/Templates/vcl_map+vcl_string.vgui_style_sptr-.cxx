ef097ba99cf159b21f325ae8c6a27777a8c457de
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-19 04:47:23 -0400

    no need for explicit instantiation of STL

--- contrib/brl/bbas/bgui/Templates/vcl_map+vcl_string.vgui_style_sptr-.cxx
+++
@@ -1,5 +0,0 @@
-#include <vgui/vgui_style_sptr.h>
-#include <vcl_string.h>
-#include <vcl_map.txx>
-
-VCL_MAP_INSTANTIATE(vcl_string, vgui_style_sptr, vcl_less<vcl_string> );
--- contrib/brl/bbas/bgui/Templates/vcl_vector+bgui_vtol2D_tableau_sptr-.cxx
+++
@@ -1,5 +0,0 @@
-#include <bgui/bgui_vtol2D_tableau_sptr.h>
-#include <vcl_vector.txx>
-
-VCL_VECTOR_INSTANTIATE(bgui_vtol2D_tableau_sptr);
-
