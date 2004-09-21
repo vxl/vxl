557540b272491680c8e4ddbb75db2acf27f93a9f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-21 08:27:18 -0400

    more includes ...

--- contrib/gel/mrc/vpgl/Templates/vbl_smart_ptr+vpgl_perspective_camera-.cxx
+++ contrib/gel/mrc/vpgl/Templates/vbl_smart_ptr+vpgl_perspective_camera-.cxx
@@ -1,8 +1,10 @@
+#include <vpgl/vpgl_perspective_camera.h>
 #include <vcsl/vcsl_axis.h>
 #include <vcsl/vcsl_graph.h>
 #include <vcsl/vcsl_spatial.h>
 #include <vcsl/vcsl_spatial_transformation.h>
-#include <vpgl/vpgl_perspective_camera.h>
+#include <vcsl/vcsl_dimension.h>
+#include <vcsl/vcsl_unit.h>
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vpgl_perspective_camera);
