cfc4474b15aa25594290883b539126f11a4519d8
Amitha Perera <amithaperera@users.sourceforge.net>
2004-09-20 20:43:34 -0400

    Looks like making some functions non-virtual puts demands on smart ptr instantiation.

--- core/vcsl/Templates/vbl_smart_ptr+vcsl_axis-.cxx
+++ core/vcsl/Templates/vbl_smart_ptr+vcsl_axis-.cxx
@@ -1,4 +1,6 @@
 #include <vcsl/vcsl_axis.h>
+#include <vcsl/vcsl_dimension.h>
+#include <vcsl/vcsl_unit.h>
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vcsl_axis);
--- core/vcsl/Templates/vbl_smart_ptr+vcsl_geographic-.cxx
+++ core/vcsl/Templates/vbl_smart_ptr+vcsl_geographic-.cxx
@@ -1,8 +1,8 @@
+#include <vcsl/vcsl_geographic.h>
 #include <vcsl/vcsl_axis.h> // see vbl_smart_ptr+vcsl_cartesian-.cxx
 #include <vcsl/vcsl_graph.h>
 #include <vcsl/vcsl_spatial_transformation.h>
-
-#include <vcsl/vcsl_geographic.h>
+#include <vcsl/vcsl_spheroid.h>
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vcsl_geographic);
