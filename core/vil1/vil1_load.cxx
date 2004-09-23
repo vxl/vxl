d3f27fca32fc2d08591f2fa6a50e7fbcff4a6023
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-23 04:54:01 -0400

    added a bit more explanation to the previous modification

--- core/vil1/vil1_load.cxx
+++ core/vil1/vil1_load.cxx
@@ -48,7 +48,9 @@ vil1_image vil1_load_raw(char const* filename)
   vil1_stream *is = vil1_open(filename, "r");
   if (is) {
     vil1_image im = vil1_load_raw(is);
-    // this will delete the stream object.
+    // this will delete the stream object in case the loader has failed;
+    // otherwise the specific image file format will have ref()ed "is"
+    // in which case this does not harm.
     is->ref(); is->unref();
     return im;
   }
