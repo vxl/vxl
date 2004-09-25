482b5587969a71efdf45b41b5135313ffa555d23
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-25 19:03:35 -0400

    too early

--- contrib/gel/vsol/Templates/vbl_smart_ptr+vsol_digital_curve_3d-.cxx
+++
@@ -1,6 +0,0 @@
-#include <vsol/vsol_digital_curve_3d.h>
-#include <vsol/vsol_box_3d.h>
-#include <vsol/vsol_point_3d.h>
-#include <vbl/vbl_smart_ptr.txx>
-
-VBL_SMART_PTR_INSTANTIATE(vsol_digital_curve_3d);
