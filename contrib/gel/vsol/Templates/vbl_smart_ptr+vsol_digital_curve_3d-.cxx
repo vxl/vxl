38949538e91156d64734a7f95a1d7fa8c07cbdea
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-25 19:01:08 -0400

    added an include

--- contrib/gel/vsol/Templates/vbl_smart_ptr+vsol_digital_curve_3d-.cxx
+++ contrib/gel/vsol/Templates/vbl_smart_ptr+vsol_digital_curve_3d-.cxx
@@ -1,6 +1,6 @@
-#include <vsol/vsol_line_3d.h>
+#include <vsol/vsol_digital_curve_3d.h>
 #include <vsol/vsol_box_3d.h>
 #include <vsol/vsol_point_3d.h>
 #include <vbl/vbl_smart_ptr.txx>
 
-VBL_SMART_PTR_INSTANTIATE(vsol_line_3d);
+VBL_SMART_PTR_INSTANTIATE(vsol_digital_curve_3d);
--- contrib/gel/vsol/Templates/vbl_smart_ptr+vsol_polyline_2d-.cxx
+++ contrib/gel/vsol/Templates/vbl_smart_ptr+vsol_polyline_2d-.cxx
@@ -1,5 +1,6 @@
 #include <vsol/vsol_polyline_2d.h>
 #include <vsol/vsol_box_2d.h>
+#include <vsol/vsol_point_2d.h>
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vsol_polyline_2d);
