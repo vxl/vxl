9733daafa35bc54448e77babf79795e5b40b6a04
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-25 18:37:19 -0400

    added an include

--- contrib/gel/vsol/Templates/vbl_smart_ptr+vsol_conic_2d-.cxx
+++ contrib/gel/vsol/Templates/vbl_smart_ptr+vsol_conic_2d-.cxx
@@ -1,5 +1,6 @@
 #include <vsol/vsol_conic_2d.h>
 #include <vsol/vsol_box_2d.h>
+#include <vsol/vsol_point_2d.h>
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vsol_conic_2d);
--- contrib/gel/vsol/Templates/vbl_smart_ptr+vsol_line_2d-.cxx
+++ contrib/gel/vsol/Templates/vbl_smart_ptr+vsol_line_2d-.cxx
@@ -1,5 +1,6 @@
 #include <vsol/vsol_line_2d.h>
 #include <vsol/vsol_box_2d.h>
+#include <vsol/vsol_point_2d.h>
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(vsol_line_2d);
