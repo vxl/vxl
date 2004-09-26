3d838048c707ec0d5478a75a4c7b8b111fabb4fe
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-26 15:56:38 -0400

    added binary I/O

--- contrib/gel/vsol/vsol_box_3d.cxx
+++ contrib/gel/vsol/vsol_box_3d.cxx
@@ -1,45 +1,41 @@
 // This is gel/vsol/vsol_box_3d.cxx
+#include <vbl/io/vbl_io_bounding_box.h>
 #include "vsol_box_3d.h"
 //:
 // \file
 #include <vcl_cassert.h>
 
-vsol_box_3d::vsol_box_3d(vsol_box_3d const &b) : vbl_ref_count(), vul_timestamp()
-{
-  box_ = b.box_;
-}
-
-double vsol_box_3d::get_min_x()
+double vsol_box_3d::get_min_x() const
 {
   assert(!box_.empty());
   return (box_.min())[0];
 }
 
-double vsol_box_3d::get_max_x()
+double vsol_box_3d::get_max_x() const
 {
   assert(!box_.empty());
   return (box_.max())[0];
 }
 
-double vsol_box_3d::get_min_y()
+double vsol_box_3d::get_min_y() const
 {
   assert(!box_.empty());
   return (box_.min())[1];
 }
 
-double vsol_box_3d::get_max_y()
+double vsol_box_3d::get_max_y() const
 {
   assert(!box_.empty());
   return (box_.max())[1];
 }
 
-double vsol_box_3d::get_min_z()
+double vsol_box_3d::get_min_z() const
 {
   assert(!box_.empty());
   return (box_.min())[2];
 }
 
-double vsol_box_3d::get_max_z()
+double vsol_box_3d::get_max_z() const
 {
   assert(!box_.empty());
   return (box_.max())[2];
@@ -62,7 +58,7 @@ void vsol_box_3d::grow_minmax_bounds(vsol_box_3d & comp_box)
 //-------------------------------------------------------------------
 //:   Determines if this box is inside the right hand side box.
 //    That is, all boundaries of *this must be on or inside the boundaries of b.
-bool vsol_box_3d::operator< (vsol_box_3d& b)
+bool vsol_box_3d::operator< (vsol_box_3d& b) const
 {
   if (box_.empty()) return true;
   if (b.box_.empty()) return false;
@@ -80,7 +76,7 @@ inline static bool near_same(double f1, double f2, float tolerance)
   return f1-f2<tolerance && f2-f1<tolerance;
 }
 
-bool vsol_box_3d::near_equal(vsol_box_3d& b, float tolerance)
+bool vsol_box_3d::near_equal(vsol_box_3d const& b, float tolerance) const
 {
   if (box_.empty() && b.box_.empty()) return true;
   if (b.box_.empty() || b.box_.empty()) return false;
@@ -96,4 +92,89 @@ bool vsol_box_3d::near_equal(vsol_box_3d& b, float tolerance)
 void vsol_box_3d::reset_bounds()
 {
   box_.reset();
+}
+
+//----------------------------------------------------------------
+// ================   Binary I/O Methods ========================
+//----------------------------------------------------------------
+
+//: Binary save self to stream.
+void vsol_box_3d::b_write(vsl_b_ostream &os) const
+{
+  vsl_b_write(os, version());
+  vsl_b_write(os, box_);
+}
+
+//: Binary load self from stream (not typically used)
+void vsol_box_3d::b_read(vsl_b_istream &is)
+{
+  if (!is)
+    return;
+  short ver;
+  vsl_b_read(is, ver);
+  switch (ver)
+  {
+   case 1:
+    vsl_b_read(is, box_);
+    break;
+   default:
+    vcl_cerr << "vsol_box_3d: unknown I/O version " << ver << '\n';
+  }
+}
+
+//: Return IO version number;
+short vsol_box_3d::version() const
+{
+  return 1;
+}
+
+//: Print an ascii summary to the stream
+void vsol_box_3d::print_summary(vcl_ostream &os) const
+{
+  os << *this;
+}
+
+//external functions
+vcl_ostream& operator<<(vcl_ostream& s, vsol_box_3d const& b)
+{
+  s << "[(" << b.get_min_x() << ' ' << b.get_min_y() << ' ' << b.get_min_z() << ")("
+    << b.get_max_x() << ' ' << b.get_max_y() << ' ' << b.get_max_z() << ")]";
+  return s;
+}
+
+//: Binary save vsol_box_3d_sptr to stream.
+void
+vsl_b_write(vsl_b_ostream &os, vsol_box_3d_sptr const& b)
+{
+  if (!b){
+    vsl_b_write(os, false); // Indicate null boxer stored
+  }
+  else{
+    vsl_b_write(os,true); // Indicate non-null boxer stored
+    b->b_write(os);
+  }
+}
+
+//: Binary load vsol_box_3d_sptr from stream.
+void
+vsl_b_read(vsl_b_istream &is, vsol_box_3d_sptr &b)
+{
+  bool not_null_ptr;
+  vsl_b_read(is, not_null_ptr);
+  if (not_null_ptr)
+  {
+    short ver;
+    vsl_b_read(is, ver);
+    switch (ver)
+    {
+     case 1: {
+      vbl_bounding_box<double,3> box;
+      vsl_b_read(is, box);
+      b = new vsol_box_3d(box);
+      break;
+     }
+     default:
+      b = 0;
+    }
+  }
 }
--- contrib/gel/vsol/vsol_box_3d.h
+++ contrib/gel/vsol/vsol_box_3d.h
@@ -29,8 +29,10 @@
 //  Modifications
 //   2003/01/09 Peter Vanroose deprecated set_min_x() etc. and replaced with
 //                             more safe add_point()
+//   2004/09/25 Peter Vanroose added binary I/O
 // \endverbatim
 
+#include <vsl/vsl_binary_io.h>
 #include <vul/vul_timestamp.h>
 #include <vbl/vbl_ref_count.h>
 #include <vbl/vbl_bounding_box.h>
@@ -46,39 +48,82 @@ class vsol_box_3d : public vbl_ref_count , public vul_timestamp
   //: create an empty box
   vsol_box_3d() {}
 
-  vsol_box_3d(vsol_box_3d const& b);
+  vsol_box_3d(vsol_box_3d const& b)
+    : vbl_ref_count(), vul_timestamp(), box_(b.box_) {}
 
-  vsol_box_3d(vbl_bounding_box<double,3> const &b){box_=b;}
+  vsol_box_3d(vbl_bounding_box<double,3> const &b) : box_(b) {}
 
   ~vsol_box_3d() {}
 
   // accessors
 
-  double get_min_x();
-  double get_max_x();
+  double get_min_x() const;
+  double get_max_x() const;
 
-  double get_min_y();
-  double get_max_y();
+  double get_min_y() const;
+  double get_max_y() const;
 
-  double get_min_z();
-  double get_max_z();
+  double get_min_z() const;
+  double get_max_z() const;
+
+  double width() const { return get_max_x() - get_min_x(); }
+  double height() const { return get_max_y() - get_min_y(); }
+  double depth() const { return get_max_z() - get_min_z(); }
+  double volume() const { return width() * height() * depth(); }
 
   //: enlarge the bounding box by adding the point (x,y,z) & taking convex hull
   void add_point(double x, double y, double z);
 
-  //: Compare this' bounds to comp_box and grow to the maximum bounding box
+  //: Compare this' bounds to comp_box and grow to the maximum bounding box.
+  //  I.e., take the convex union of this and comp_box
   void grow_minmax_bounds(vsol_box_3d & comp_box);
 
-  double width() { return get_max_x() - get_min_x(); }
-  double height() { return get_max_y() - get_min_y(); }
-  double depth() { return get_max_z() - get_min_z(); }
-  double volume() { return width() * height() * depth(); }
+  //: a<b means a is inside b
+  bool operator< (vsol_box_3d& box) const;
 
-  bool operator< (vsol_box_3d& box);  // a<b = a is inside b
+  //: is box about the same as this?
+  bool near_equal(vsol_box_3d const& box, float tolerance) const;
 
-  bool near_equal(vsol_box_3d& box, float tolerance); // is box about the same as this?
-  //: reset the bounds of the box
+  //: reset the bounds of the box, i.e., make the box empty
   void reset_bounds();
+
+  // ==== Binary IO methods ======
+
+  //: Binary save self to stream.
+  void b_write(vsl_b_ostream &os) const;
+
+  //: Binary load self from stream.
+  void b_read(vsl_b_istream &is);
+
+  //: Return IO version number;
+  short version() const;
+
+  //: Print an ascii summary to the stream
+  void print_summary(vcl_ostream &os) const;
+
+  //: Return a platform independent string identifying the class
+  virtual vcl_string is_a() const { return vcl_string("vsol_box_3d"); }
+
+  //: Return true if the argument matches the string identifying the class or any parent class
+  virtual bool is_class(const vcl_string& cls) const { return cls==is_a(); }
 };
+
+#include "vsol_box_3d_sptr.h"
+
+//: Stream operator
+vcl_ostream&  operator<<(vcl_ostream& s, vsol_box_3d const& p);
+
+//: Binary save vsol_box_3d* to stream.
+void vsl_b_write(vsl_b_ostream &os, vsol_box_3d_sptr const& p);
+
+//: Binary load vsol_box_3d* from stream.
+void vsl_b_read(vsl_b_istream &is, vsol_box_3d_sptr &p);
+
+//: Print human readable summary of box to a stream
+//  (This is needed for the instantiation of vsl_vector_io<vsol_box_3d_sptr>)
+inline void vsl_print_summary(vcl_ostream& os, vsol_box_3d_sptr const& b)
+{
+  os << *b;
+}
 
 #endif // vsol_box_3d_h_
