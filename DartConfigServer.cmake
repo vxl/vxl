fcae3b9e5282a28e7030623aff72fb1ba6c47f27
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-09-29 02:22:18 -0400

    added full list of "author:" names in CVS logs which are not sf.net user names

--- DartConfigServer.cmake
+++ DartConfigServer.cmake
@@ -17,7 +17,26 @@ SET (EMAIL_FROM "perera@cs.rpi.edu")
 SET (SMTP_MAILHOST "mail.cs.rpi.edu")
 SET (CVS_IDENT_TO_EMAIL "\\
   {vxl_robots DO_NOT_EMAIL} \\
+  {vanroose DO_NOT_EMAIL} \\
+  {uid1820 DO_NOT_EMAIL} \\
+  {targetjr DO_NOT_EMAIL} \\
+  {remcvs_tu_crd DO_NOT_EMAIL} \\
+  {remcvs_tgtjr_oxford DO_NOT_EMAIL} \\
+  {remcvs_targetjr_leuven DO_NOT_EMAIL} \\
+  {remcvs_targetjr_grumpie DO_NOT_EMAIL} \\
+  {remcvs_perera_rpi DO_NOT_EMAIL} \\
+  {remcvs_manchester_scott DO_NOT_EMAIL} \\
+  {remcvs_manchester_cootes DO_NOT_EMAIL} \\
+  {remcvs_hoffman_kitware DO_NOT_EMAIL} \\
+  {remcvs_hamilton_crd DO_NOT_EMAIL} \\
+  {remcvs_geoff_oxford DO_NOT_EMAIL} \\
+  {remcvs_geoff_ge DO_NOT_EMAIL} \\
+  {remcvs_fsm_oxford DO_NOT_EMAIL} \\
+  {remcvs_awf_oxford DO_NOT_EMAIL} \\
+  {remcvs_depot_balltown DO_NOT_EMAIL} \\
+  {perera DO_NOT_EMAIL} \\
   {iscott DO_NOT_EMAIL} \\
+  {jhoulihan DO_NOT_EMAIL} \\
   {.* {\\0@users.sourceforge.net}} \\
 ")
 SET (DARTBOARD_BASE_URL "http://www.cs.rpi.edu/research/vision/vxl/Testing")
@@ -30,26 +49,26 @@ SET (BUILD_MONITORS "\\
 
 SET (PROJECT_URL http://vxl.sourceforge.net/)
 SET (EXPECTED_BUILDS "\\
+  {crd.ge.com Cygwin-1.5.11_gcc-3.3.3_-Wall_-O2_static} \\
   {cs.rpi.edu FreeBSD-4.9-gcc-2.95.4} \\
   {cs.rpi.edu FreeBSD-4.9-gcc-2.95.4-static} \\
   {cs.rpi.edu FreeBSD-4.9-gcc-3.3} \\
-  {esat.kuleuven.ac.be IRIX-6.5-CC-n32} \\
-  {esat.kuleuven.ac.be Linux-2.6-gcc-3.4-O3} \\
-  {esat.kuleuven.ac.be Linux64-2.6-gcc-3.4-O3} \\
-  {esat.kuleuven.ac.be OSF1-V5.1-gcc-3.4} \\
-  {crd.ge.com Solaris-2.8_gcc-3.0.4_static} \\
-  {crd.ge.com Win2k_bcc-5.5.1_Release} \\
-  {esat.kuleuven.ac.be SunOS-5.8-gcc-3.4-O2} \\
-  {esat.kuleuven.ac.be HP-UX-B.11.00-gcc-3.4-O1} \\
-  {crd.ge.com Playstation2-Linux-2.2.1_gcc-2.95.2_-Wall_static} \\
-  {crd.ge.com Cygwin-1.5.11_gcc-3.3.3_-Wall_-O2_static} \\
-  {crd.ge.com Win2k_msvc-6.0sp5_Release} \\
   {crd.ge.com FreeBSD-5.0_gcc-3.2.1_-Wall_-O_shared} \\
+  {esat.kuleuven.ac.be HP-UX-B.11.00-gcc-3.4-O1} \\
+  {esat.kuleuven.ac.be IRIX-6.5-CC-n32} \\
   {crd.ge.com Linux-2.4.18_gcc-3.2_-Wall_profile_static} \\
   {crd.ge.com Linux-2.4.9_icc-8.0_-w1_-O2_shared} \\
+  {esat.kuleuven.ac.be Linux-2.6-gcc-3.4-O3} \\
+  {esat.kuleuven.ac.be Linux64-2.6-gcc-3.4-O3} \\
+  {crd.ge.com MinGW-3.5_gcc-3.3.3_-Wall_-O2_static} \\
+  {esat.kuleuven.ac.be OSF1-V5.1-gcc-3.4} \\
+  {crd.ge.com Playstation2-Linux-2.2.1_gcc-2.95.2_-Wall_static} \\
+  {crd.ge.com Solaris-2.8_gcc-3.0.4_static} \\
+  {esat.kuleuven.ac.be SunOS-5.8-gcc-3.4-O2} \\
+  {crd.ge.com Win2k_bcc-5.5.1_Release} \\
+  {crd.ge.com Win2k_msvc-6.0sp5_Release} \\
   {crd.ge.com Win2k_msvc-7.0_Release} \\
   {nk.crd.ge.com Win2k_msvc-7.1_Release} \\
-  {imorphics Win2k-msvc-6sp5-iMorphicsRelDeb} \\
   {imorphics Win2k-msvc-6sp5-Debug} \\
-  {crd.ge.com MinGW-3.5_gcc-3.3.3_-Wall_-O2_static} \\
+  {imorphics Win2k-msvc-6sp5-iMorphicsRelDeb} \\
 ")
