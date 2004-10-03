a6253d702c8015807b7a1f9a3a31181627600dee
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-10-03 16:37:46 -0400

    added #ifdef HAS_OSL

--- contrib/conversions/geometry/tests/test_include.cxx
+++ contrib/conversions/geometry/tests/test_include.cxx
@@ -1,6 +1,10 @@
+#ifdef HAS_VDGL
+#ifdef HAS_OSL
 #include <geometry/osl_to_vdgl.h>
+#endif
 #ifdef HAS_SPATIALOBJECTS
 #include <geometry/DigitalCurve_to_vdgl.h>
+#endif
 #endif
 
 int main() { return 0; }
