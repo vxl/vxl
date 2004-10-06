4a062273b7d67879cdf0df15c015e5a988e98946
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-10-06 16:51:48 -0400

    added missing "class"

--- contrib/gel/vtol/algo/Templates/vbl_smart_ptr+vtol_extract_topology_region_type-.cxx
+++ contrib/gel/vtol/algo/Templates/vbl_smart_ptr+vtol_extract_topology_region_type-.cxx
@@ -2,4 +2,4 @@
 #include <vsol/vsol_box_2d.h>
 #include <vtol/algo/vtol_extract_topology.h>
 
-template vbl_smart_ptr< vtol_extract_topology_region_type >;
+template class vbl_smart_ptr< vtol_extract_topology_region_type >;
