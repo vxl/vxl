69a2f5eb657e197ad876bded2df0a0e34ca89f43
Matthew J. Leotta <mleotta@users.sourceforge.net>
2004-10-08 13:07:21 -0400

    added missing instantiation

---
+++ contrib/brl/bseg/bmrf/Templates/vsl_list_io+bmrf_curvel_3d_sptr-.cxx
@@ -0,0 +1,6 @@
+// Instantiation of IO for vcl_list<bmrf_curvel_3d_sptr>
+#include <vsl/vsl_list_io.txx>
+#include <bmrf/bmrf_curvel_3d_sptr.h>
+#include <vbl/io/vbl_io_smart_ptr.h>
+
+VSL_LIST_IO_INSTANTIATE(bmrf_curvel_3d_sptr);
