a110bc50cf2bfd61fbc77a3533e3f8e6c123745e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-10-16 11:24:07 -0400

    updated w.r.t. CMakeLists.txt

--- contrib/brl/bmvl/brct/config.mk
+++ contrib/brl/bmvl/brct/config.mk
@@ -9,6 +9,7 @@ include ${IUEROOT}/contrib/oxl/mvl/config.mk
 include ${IUEROOT}/contrib/gel/gevd/config.mk
 include ${IUEROOT}/contrib/gel/vdgl/config.mk
 include ${IUEROOT}/contrib/gel/vsol/config.mk
+include ${IUEROOT}/contrib/gel/vsrl/config.mk
 include ${IUEROOT}/core/vgl/algo/config.mk
 include ${IUEROOT}/core/vnl/algo/config.mk
 include ${IUEROOT}/core/vbl/config.mk
