e982ea11e760f0c9ddd9567f1ea0bc64cc13f249
Joseph L. Mundy <mundyj@users.sourceforge.net>
2004-10-16 08:52:52 -0400

    removed base corr sptr class since it doesn't do anything directly

--- contrib/brl/bmvl/brct/brct_corr_sptr.h
+++
@@ -1,9 +0,0 @@
-#ifndef brct_corr_sptr_h_ 
-#define brct_corr_sptr_h_
-
-#include <vbl/vbl_smart_ptr.h>
-
-class brct_corr;
-typedef vbl_smart_ptr<brct_corr> brct_corr_sptr;
-#include <brct/brct_corr.h>
-#endif
