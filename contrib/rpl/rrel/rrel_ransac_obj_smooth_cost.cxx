6d921445bfeb8c69df24c3d507f7a7c6d102704a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-10-24 11:05:35 -0400

    TABs replaced by spaces

--- contrib/rpl/rrel/rrel_ransac_obj_smooth_cost.cxx
+++ contrib/rpl/rrel/rrel_ransac_obj_smooth_cost.cxx
@@ -1,21 +1,12 @@
 // This is rpl/rrel/rrel_ransac_obj_smooth_cost.cxx
 #include "rrel_ransac_obj_smooth_cost.h"
-#include "vnl/vnl_math.h"
-
-rrel_ransac_obj_smooth_cost::rrel_ransac_obj_smooth_cost( double scale_mult )
-  : rrel_ransac_obj(scale_mult),scale_mult_( scale_mult )
-{
-}
-
-rrel_ransac_obj_smooth_cost::~rrel_ransac_obj_smooth_cost( )
-{
-}
-
+#include <vnl/vnl_math.h>
 
 double
-rrel_ransac_obj_smooth_cost::fcn( vect_const_iter res_begin, vect_const_iter res_end,
-                      vect_const_iter scale_begin,
-                      vnl_vector<double>* /*param_vector*/ ) const
+rrel_ransac_obj_smooth_cost::fcn( vect_const_iter res_begin,
+                                  vect_const_iter res_end,
+                                  vect_const_iter scale_begin,
+                                  vnl_vector<double>* /*param_vector*/ ) const
 {
   double sum = 0.0;
 
@@ -23,27 +14,24 @@ rrel_ransac_obj_smooth_cost::fcn( vect_const_iter res_begin, vect_const_iter res
     double thres = *scale_begin * scale_mult_;
     if ( *res_begin < -thres || *res_begin > thres )
       sum += 1.0;
-	else sum+= vnl_math_sqr(vnl_math_abs(*res_begin/thres));
-
+    else sum+= vnl_math_sqr(*res_begin/thres);
   }
-
   return sum;
 }
 
 double
-rrel_ransac_obj_smooth_cost::fcn( vect_const_iter begin, vect_const_iter end,
-                      double scale,
-                      vnl_vector<double>* /*param_vector*/ ) const
+rrel_ransac_obj_smooth_cost::fcn( vect_const_iter res_begin,
+                                  vect_const_iter res_end,
+                                  double scale,
+                                  vnl_vector<double>* /*param_vector*/ ) const
 {
   double sum = 0.0;
   double thres = scale * scale_mult_;
 
-  for ( ; begin != end; ++begin ) {
-    if ( *begin < -thres || *begin > thres )
+  for ( ; res_begin != res_end; ++res_begin ) {
+    if ( *res_begin < -thres || *res_begin > thres )
       sum += 1.0;
-	else sum+= vnl_math_sqr(vnl_math_abs(*begin/thres));
+    else sum+= vnl_math_sqr(*res_begin/thres);
   }
-
   return sum;
 }
-
--- contrib/rpl/rrel/rrel_ransac_obj_smooth_cost.h
+++ contrib/rpl/rrel/rrel_ransac_obj_smooth_cost.h
@@ -7,37 +7,31 @@
 // \author Sancar Adali (sancar_adali@brown.edu)
 // RANSAC objective function with smooth cost function.
 
-
 class rrel_ransac_obj_smooth_cost : public rrel_ransac_obj
 {
-public:
-	rrel_ransac_obj_smooth_cost(double mult_scale = 2.0);
+ protected:
+  double scale_mult_;
+ public:
+  rrel_ransac_obj_smooth_cost(double scale_mult = 2.0)
+  : rrel_ransac_obj(scale_mult), scale_mult_( scale_mult ) {}
 
+  virtual ~rrel_ransac_obj_smooth_cost() {}
 
-	~rrel_ransac_obj_smooth_cost();
-
-	virtual double fcn( vect_const_iter res_begin, vect_const_iter res_end,
+  virtual double fcn( vect_const_iter res_begin, vect_const_iter res_end,
                       vect_const_iter scale_begin,
-                      vnl_vector<double>* = 0 /* param vector is unused */ ) const;
+                      vnl_vector<double>* param_vector = 0 ) const;
 
-  //: Evaluate the objective function on homoscedastic residuals.  
+  //: Evaluate the objective function on homoscedastic residuals.
   //  \sa rrel_objective::fcn.
   virtual double fcn( vect_const_iter begin, vect_const_iter end,
                       double scale,
-                      vnl_vector<double>* = 0 /* param vector is unused */ ) const;
+                      vnl_vector<double>* param_vector = 0 ) const;
 
   //: True.
   //  Using a RANSAC objective with an estimated scale doesn't make
   //  sense, because the any scale estimate tends to be inaccurate and
   //  RANSAC is sensitive to the threshold (and hence the scale).
-  virtual bool requires_prior_scale() const
-    { return true; }
-protected:
-
-   double scale_mult_;
-
-}
-;
-
+  virtual bool requires_prior_scale() const { return true; }
+};
 
 #endif
