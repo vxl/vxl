ea60418fbab51a0e27178edde3266924518f1657
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-10-28 15:46:35 -0400

    Doxygen wants to see a "\file"

--- contrib/rpl/rgrl/rgrl_feature_point.cxx
+++ contrib/rpl/rgrl/rgrl_feature_point.cxx
@@ -1,3 +1,4 @@
+//:
 // \file
 // \author Amitha Perera
 // \date   Feb 2003
@@ -78,41 +79,41 @@ transform( rgrl_transformation const& xform ) const
   // transform scale
   vnl_vector<double> const& scaling = xform.scaling_factors();
   const unsigned dim = this->location_.size();
-  if( this->scale_ && scaling.size() == dim ) {
+  if ( this->scale_ && scaling.size() == dim ) {
     // "average" them
-    if( dim == 2 )
+    if ( dim == 2 )
       result->scale_ = vcl_sqrt( scaling[0]*scaling[1] ) * this->scale_;
     else {
       double prod_scale=1;
-      for( unsigned i=0; i < dim; ++i )
+      for ( unsigned i=0; i < dim; ++i )
         prod_scale *= scaling[i];
       result->scale_ = vcl_exp( vcl_log(prod_scale) / double(dim) ) * this->scale_;
     }
-  } else if( this-> scale_ ) {
-    WarningMacro( "This feature has non-zero scale value, but transformation has no scaling factors." 
-                  << "The scale of transformed features is NOT set. " );
+  } else if ( this-> scale_ ) {
+    WarningMacro( "This feature has non-zero scale value, but transformation has no scaling factors."
+                  << "The scale of transformed features is NOT set." );
   }
 
   return result_sptr;
 };
 
 //:  Compute the signature weight between two features.
-double 
+double
 rgrl_feature_point ::
 absolute_signature_weight( rgrl_feature_sptr other ) const
 {
   rgrl_feature_point* pt_ptr = rgrl_cast<rgrl_feature_point*>(other);
   assert( pt_ptr );
- 
+
   double scale_wgt = 1;
-  if( this->scale_ && pt_ptr->scale_ ) {
-    if( this->scale_ >= pt_ptr->scale_ )
+  if ( this->scale_ && pt_ptr->scale_ ) {
+    if ( this->scale_ >= pt_ptr->scale_ )
       scale_wgt = pt_ptr->scale_ / this->scale_;
     else
       scale_wgt = this->scale_ / pt_ptr->scale_;
     // the weight change is too gradual, make it more steep
     scale_wgt = scale_wgt * scale_wgt;
   }
-  
+
   return  scale_wgt;
 }
