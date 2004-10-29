525053d33064a40fe520a6d4e0841fe74deb847b
Kevin de Souza <kdesouza@users.sourceforge.net>
2004-10-29 05:47:25 -0400

    Added io templates for vector<vector<vgl_point_3d<double> > > and vector<vgl_vector_3d<double> >

---
+++ core/vgl/io/Templates/vsl_vector_io+vcl_vector+vgl_point_3d+double---.cxx
@@ -0,0 +1,3 @@
+#include <vsl/vsl_vector_io.txx>
+#include <vgl/io/vgl_io_point_3d.h>
+VSL_VECTOR_IO_INSTANTIATE(vcl_vector<vgl_point_3d<double> >);
---
+++ core/vgl/io/Templates/vsl_vector_io+vgl_vector_3d+double--.cxx
@@ -0,0 +1,3 @@
+#include <vsl/vsl_vector_io.txx>
+#include <vgl/io/vgl_io_vector_3d.h>
+VSL_VECTOR_IO_INSTANTIATE(vgl_vector_3d<double>);
