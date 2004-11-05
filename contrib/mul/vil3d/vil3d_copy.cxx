28b5bb2a58f71f29f5d41809ab0b2ace73c7e0a1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-11-05 01:57:57 -0500

    new code fragment (dest->set_voxel_size()) was not executed because of a previous "return"

--- contrib/mul/vil3d/vil3d_copy.cxx
+++ contrib/mul/vil3d/vil3d_copy.cxx
@@ -30,24 +30,26 @@ bool vil3d_copy_deep(const vil3d_image_resource_sptr &src, vil3d_image_resource_
 
   if (src->ni() == 0 || src->nj() == 0 || src->nk() == 0 || src->nplanes() == 0) return true;
 
+  float sizes[3];
+  if (dest->get_property(vil3d_property_voxel_size, sizes))
+  {
+    dest->set_voxel_size(sizes[0], sizes[1], sizes[2]);
+  }
+
   vil3d_image_view_base_sptr view_ref = src->get_view();
   if (!view_ref) return false;
   return dest->put_view(*view_ref);
 
 #if 0
   const unsigned long large_image_limit = 1024ul * 1024ul * 16ul; //16M Pixels
-  if (src->ni() * src->nj() * src->nk() * src->nplanes() < large_image_limit)
-  {
-  }
-  else
+  if (src->ni() * src->nj() * src->nk() * src->nplanes() >= large_image_limit)
   {
     unsigned got_to_slice =0;
     unsigned block_size = vcl_max(static_cast<unsigned>(large_image_limit / src->ni()),1u);
 
     while (got_to_line < src->nj())
     {
-      vil3d_image_view_base_sptr view_ref = src->get_view(0, src->ni(), got_to_slice,
-                                                        vcl_min(got_to_line+block_size, src->nj()));
+      view_ref = src->get_view(0, src->ni(), got_to_slice, vcl_min(got_to_line+block_size, src->nj()));
       if (!view_ref) return false;
       if (!dest->put_view(*view_ref,0,got_to_line)) return false;
       got_to_line += block_size;
@@ -55,11 +57,5 @@ bool vil3d_copy_deep(const vil3d_image_resource_sptr &src, vil3d_image_resource_
     return true;
   }
 #endif
-
-  float sizes[3];
-  if (dest->get_property(vil3d_property_voxel_size, sizes))
-  {
-    dest->set_voxel_size(sizes[0], sizes[1], sizes[2]);
-  }
 }
 
