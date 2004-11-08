b1e457be9422f4ba4d722ac47f7ebbae2b60b12b
Panachit Kittipanya-ngam <panachit@users.sourceforge.net>
2004-11-08 10:21:52 -0500

    Added missing loader

--- contrib/mul/vpdfl/vpdfl_add_all_binary_loaders.cxx
+++ contrib/mul/vpdfl/vpdfl_add_all_binary_loaders.cxx
@@ -11,6 +11,8 @@
 #include <vpdfl/vpdfl_mixture_builder.h>
 #include <vpdfl/vpdfl_pc_gaussian.h>
 #include <vpdfl/vpdfl_pc_gaussian_builder.h>
+#include <vpdfl/vpdfl_gaussian_kernel_pdf.h>
+#include <vpdfl/vpdfl_gaussian_kernel_pdf_builder.h>
 
 void vpdfl_add_all_binary_loaders()
 {
@@ -22,4 +24,6 @@ void vpdfl_add_all_binary_loaders()
   vsl_add_to_binary_loader(vpdfl_mixture_builder());
   vsl_add_to_binary_loader(vpdfl_pc_gaussian());
   vsl_add_to_binary_loader(vpdfl_pc_gaussian_builder());
+  vsl_add_to_binary_loader(vpdfl_gaussian_kernel_pdf());
+  vsl_add_to_binary_loader(vpdfl_gaussian_kernel_pdf_builder());
 }
