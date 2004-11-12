5a77849e7a32c86177cdff3a565797e20f2f23a0
Kevin de Souza <kdesouza@users.sourceforge.net>
2004-11-12 07:30:56 -0500

    Added function to calculate the range of image values between a pair of specified percentiles.

--- contrib/mul/vil3d/vil3d_math.h
+++ contrib/mul/vil3d/vil3d_math.h
@@ -10,6 +10,7 @@
 #include <vil3d/vil3d_image_view.h>
 #include <vil3d/vil3d_plane.h>
 
+
 //: Compute minimum and maximum values over im
 // \relates vil3d_image_view
 template <class T>
@@ -52,6 +53,70 @@ inline void vil3d_math_value_range(const vil3d_image_view<T>& im,
   }
 }
 
+
+//: Compute values corresponding to a pair of percentiles of the range of im.
+// Lower and higher percentiles expressed as fractions, e.g. 0.05 and 0.95.
+// \relates vil3d_image_view
+// \note This function requires the sorting of large parts of the image data
+// and can be very expensive in terms of both processing and memory.
+template <class T>
+inline void vil3d_math_value_range_percentile(
+  const vil3d_image_view<T>& im,
+  const double frac_lo, const double frac_hi ,
+  T& value_lo, T& value_hi)
+{
+  // Test for invalid inputs
+  if (im.size()==0 || 
+      frac_lo<0.0 || frac_hi<=0.0 || frac_lo>=1.0 || frac_hi>1.0 ||
+      frac_lo>=frac_hi)
+  {
+    value_lo = 0;
+    value_hi = 0;
+    return;
+  }
+  
+  // Accumulate the pixel values into a list.
+  unsigned ni = im.ni();
+  unsigned nj = im.nj();
+  unsigned nk = im.nk();
+  unsigned np = im.nplanes();
+  vcl_ptrdiff_t istep = im.istep(); 
+  vcl_ptrdiff_t jstep=im.jstep();
+  vcl_ptrdiff_t kstep=im.kstep();
+  vcl_ptrdiff_t pstep = im.planestep();
+  vcl_vector<T> data(ni*nj*nk*np);
+  
+  vcl_vector<T>::iterator it = data.begin();
+  const T* plane = im.origin_ptr();
+  for (unsigned int p=0;p<np;++p, plane += pstep)
+  {
+    const T* slice = plane;
+    for (unsigned int k=0;k<nk;++k, slice += kstep)
+    {
+      const T* row = slice;
+      for (unsigned int j=0;j<nj;++j, row += jstep)
+      {
+        const T* pixel = row;
+        for (unsigned int i=0;i<ni;++i, pixel+=istep)
+        {
+          *it = *pixel;
+          it++;
+        }
+      }
+    }
+  }
+  unsigned npix = data.size();
+
+  // Sort the list up to the higher fraction
+  int index_hi = int (frac_hi*npix - 0.5);
+  vcl_vector<T>::iterator index_hi_it = data.begin() + index_hi;
+  vcl_nth_element(data.begin(), index_hi_it, data.end(), vcl_less<T>());
+  int index_lo = int (frac_lo*npix - 0.5);
+
+  // Get the values corresponding to the lower and higher fraction
+  value_hi = *index_hi_it;
+  value_lo = *(data.begin() + index_lo);
+}
 
 
 //: Calc the mean of each pixel over all the planes.
