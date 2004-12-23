3c8c244c96dd7ad648b39a310c26e0a249a93d84
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-12-23 15:31:33 -0500

    added missing #include <vnl/vnl_real_polynomial.h> and corrected include guard

--- contrib/brl/bbas/bnl/algo/bnl_legendre_polynomial.h
+++ contrib/brl/bbas/bnl/algo/bnl_legendre_polynomial.h
@@ -1,12 +1,12 @@
 #ifndef BNL_ALGO_LEGENDRE_POLYNOMIAL_H_
-#define BNL_ALGO_LEGENDRE_POLYNOMIAL_H
-
-//: 
+#define BNL_ALGO_LEGENDRE_POLYNOMIAL_H_
+//:
 //\file
 //\brief Evalue a real Legendre polynomail by recursive equation
 //\author Kongbin Kang, Brown University
 //\date Dec. 21th, 2004.
-//
+
+#include <vnl/vnl_real_polynomial.h>
 
 //: generate a n-th degree legendre polynomail
 vnl_real_polynomial bnl_legendre_polynomial(int n);
