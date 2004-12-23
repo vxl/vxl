6420cee729b094ee63359ea72570fd770d865724
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2004-12-23 15:31:49 -0500

    avoid lines of more than 132 characters

--- contrib/brl/bbas/bnl/algo/bnl_legendre_polynomial.cxx
+++ contrib/brl/bbas/bnl/algo/bnl_legendre_polynomial.cxx
@@ -1,24 +1,24 @@
-#include <vnl/vnl_real_polynomial.h>
 #include "bnl_legendre_polynomial.h"
+#include <vnl/vnl_real_polynomial.h>
 
 vnl_real_polynomial bnl_legendre_polynomial(int n)
 {
-  if(n == 0 )
+  if (n == 0 )
     return vnl_real_polynomial(1.0); // return 1
-  else{ 
-    if(n == 1){
+  else {
+    if (n == 1) {
       // return p = x
       vnl_real_polynomial p(1);
-      p[0] = 1; 
-      p[1] = 0; 
+      p[0] = 1;
+      p[1] = 0;
       return p;
     }
-    else{
+    else {
       // p = x
       vnl_real_polynomial p(1);
       p[0] = 1; p[1] = 0;
-      vnl_real_polynomial res = (2 - 1.0/n) * p * bnl_legendre_polynomial(n -1) - vnl_real_polynomial((1 - 1.0/n)) * bnl_legendre_polynomial(n-2); 
-      return res;
+      return (2 - 1.0/n) * p * bnl_legendre_polynomial(n-1)
+           - vnl_real_polynomial((1 - 1.0/n)) * bnl_legendre_polynomial(n-2);
     }
   }
 }
