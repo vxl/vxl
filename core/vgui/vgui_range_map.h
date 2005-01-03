62c1ffbdc20431778adc5d5de69313da45071f70
Joseph L. Mundy <mundyj@users.sourceforge.net>
2005-01-03 07:13:37 -0500

    Added RGB support

--- core/vgui/vgui_range_map.h
+++ core/vgui/vgui_range_map.h
@@ -9,24 +9,36 @@
 // \author  J.L. Mundy
 // \brief A mapping from pixels to 8 bit display range
 //
-// For byte and short pixel types, the mapping is implemented using
-// a look-up table. If table_mappable is true then the ::map() method can be 
-// used to retrieve the map table. The output 8 bit value is obtained by,
-//               out_8_bit_pixel = map[in_pixel];
+//  Supports pixel look-up table mapping for contrast adjustment
+//  and other interactive controls of the image display range
+//  A key application is producing useable display of 16 bit pixels
+//  Can be used to interactively window the dynamic range of 8 bit pixels
+//  or to display signed char or signed short.
+//
+//  For byte and short pixel types, the mapping is implemented using
+//  a look-up table. 
+//
+//  When mapping can be done by a look_up table, there are two 
+//  table types available (* = [L,R,G,B,A]):
+//     ::*map()  - rmap[in_pixel]->vxl_byte
+//     ::f*map() - frmap[in_pixe]->float
+//      
+//     *map is used to map the image pixels during conversion to
+//     display pixels carried out by vgui_section_buffer. The range of 
+//     this map is [0 256], which is the unclamped range in vgui_pixel.
+//
+//     *fmap is used to directly map image pixels using the OpenGL
+//     glPixelMapf() mapping function. The best form for a glPixelMap
+//     is an array of floats on the range [0,1] and is provided by frmap.
+//
 // For pixel types with a larger dynamic range than can
 // be stored in a table, the pixel conversion is done on the fly, using
-// the ::map_pixel(in_pixel) method.
+// the ::map_pixel(in_pixel) method. This function is applied in 
+// vgui_section_buffer during pixel conversion.
 //
-// The map is defined on a domain interval, [min, max]. Within this interval
-// the in_pixel values are mapped continuously to the output 8 bit values. The 
-// continuous mapping function is defined as,
-//                  y = 255*(x/255)^(1/gamma)
+// The gamma  mapping function is defined as,
+//                  y = (x)^(1/gamma)
 // which accounts for the standard photometric gamma correction. 
-// The map can also be inverted, i.e. swapping dark and light intensity values,
-//                  in = max(in)-in;
-//
-// The map is recomputed whenever the domain limits, gamma or invert parameters
-// are changed.
 // Note on Gamma (by Joel Savaric 1998):
 // 1/gamma is used to be consistent with other software like Photoshop,
 // Paintshop pro, GIMP or xv, that raise to the power of 1/gamma when
@@ -35,16 +47,22 @@
 // power of gamma (generally gamma=1.7 or 2.5) When you want to correct the 
 // gamma of the monitor your raise to the power of 1/gamma. 
 // (for more detail see http://www.cgsd.com/papers/gamma.html)
-
+//
+// The map can also be inverted, i.e. swapping dark and light intensity values,
+//                  in = max(in)-in;
+//
+// The map is recomputed whenever the domain limits, gamma or invert parameters
+// are changed.
+//
 // Notes:
-//   Maybe a more general output pixel type is needed but the great majority
-//   of displays and acceleration hardware use byte pixels.
-//   This map should be done in vgui_section_render, but introducing it 
-//   at that level would be a major change, i.e., mapping at the opengl level.
-//   For now, the map will be used to convert the image held by 
-//   vgui_image_tableau during the ::apply of vil_section_buffer.  
-//   Note also, that the dynamic mouse motion pixel display should 
-//   show the source pixel values not the displayed pixel values.
+//
+//   In GUI applications, the dynamic mouse motion pixel display should show
+//   the source pixel values before mapping, not the frame buffer values.
+//
+//   The current scheme can easily be extended to full color support if the
+//   need arises. The number of maps has to be extended to three
+//   (four with an alpha channel) as well as increasing the number of 
+//   mapping parameters.
 //
 // \verbatim
 //  Modifications <none>
@@ -54,7 +72,7 @@
 template <class Type>
 class vgui_range_map : public vgui_range_map_params
 {
-public:
+ public:
   vgui_range_map(vgui_range_map_params const& rmp);
   ~vgui_range_map();
 
@@ -62,40 +80,88 @@ public:
   //: set the parameters for the mapping function
   void set_map_params(vgui_range_map_params_sptr const& rmp);
 
-   //: Is the pixel type mapable at all
-   bool mapable(){return mapable_;}
+  //: Is the pixel type mapable at all
+  bool mapable(){return mapable_;}
 
-   //: Is the pixel type mapable by a lookup table
-   bool table_mapable(){return table_mapable_;}
+  //: Is the pixel type mapable by a lookup table
+  bool table_mapable(){return table_mapable_;}
 
-   //: Get the number of elements in the mapping table
-   unsigned map_size(){return size_;}
+  //: Get the number of elements in the mapping tables
+  unsigned map_size(){return size_;}
   
-   //: get the range map
-   vxl_byte* rmap(){return map_;}
+  //: get the software range maps (byte range)
+  //: Luminance map
+  vxl_byte* Lmap(){return this->compute_byte_table(min_L_, max_L_, gamma_L_, ratio_L_);}
+  //: Red Channel map
+  vxl_byte* Rmap(){return this->compute_byte_table(min_R_, max_R_, gamma_R_, ratio_R_);}
+  //: Green Channel map
+  vxl_byte* Gmap(){return this->compute_byte_table(min_G_, max_G_, gamma_G_, ratio_G_);}
+  //: Blue Channel map
+  vxl_byte* Bmap(){return this->compute_byte_table(min_B_, max_B_, gamma_B_, ratio_B_);}
+  //: Alpha Channel map
+  vxl_byte* Amap(){return this->compute_byte_table(min_A_, max_A_, gamma_A_, ratio_A_);}
+   
+  //: get the OpenGL hardware range maps (float [0, 1] range)
+  //: Luminance hardware map
+  float* fLmap(){return this->compute_float_table(min_L_, max_L_, gamma_L_, ratio_L_);}
+  //: Red channel hardware map
+  float* fRmap(){return this->compute_float_table(min_R_, max_R_, gamma_R_, ratio_R_);}
+  //: Green channel hardware map
+  float* fGmap(){return this->compute_float_table(min_G_, max_G_, gamma_G_, ratio_G_);}
+  //: Blue channel hardware map
+  float* fBmap(){return this->compute_float_table(min_B_, max_B_, gamma_B_, ratio_B_);}
+  //: Alpha channel hardware map
+  float* fAmap(){return this->compute_float_table(min_A_, max_A_, gamma_A_, ratio_A_);}
 
-  //: map a single pixel since the type is too large for a map
-  vxl_byte map_pixel(const Type pix);
+  //: map a single pixel to byte range since the type is too large for a map
+  //: Luminance computed map
+  inline vxl_byte map_L_pixel(const Type pix){return this->map_pixel_byte(pix, min_L_, max_L_, gamma_L_, ratio_L_);}
+  //: Red channel computed map
+  inline vxl_byte map_R_pixel(const Type pix){return this->map_pixel_byte(pix, min_R_, max_R_, gamma_R_, ratio_R_);}
+  //: Green channel computed map
+  inline vxl_byte map_G_pixel(const Type pix){return this->map_pixel_byte(pix, min_G_, max_G_, gamma_G_, ratio_G_);}
+  //: Blue channel computed map
+  inline vxl_byte map_B_pixel(const Type pix){return this->map_pixel_byte(pix, min_B_, max_B_, gamma_B_, ratio_B_);}
+  //: Alpha channel computed map
+  inline vxl_byte map_A_pixel(const Type pix){return this->map_pixel_byte(pix, min_A_, max_A_, gamma_A_, ratio_A_);}
 
   //: offset for potentially signed table index. Zero for unsigned, -maxval for signed.
   int offset();
 
 
-private:
-  //: Recompute the map if necessary
+ private:
   vgui_range_map();//never use this
+  //:
+  void init();
 
   //: Initialize the domain - sanity check and compute ratio_
-  void init_map_domain();
+  void init_map_domain(const Type min, const Type max,
+                       long double& ratio);
 
-  //: Initialize the mapping table
-  void compute_table();
+  //: Map a pixel from Type to vxl_byte
+  vxl_byte map_pixel_byte(const Type pix, const Type min, const Type max,
+                          const Type gamma, const long double ratio);
+  //: Map the pixel to the range [0f 1f];
+  float map_pixel_float(const Type pix, const Type min, const Type max,
+                        const Type gamma, const long double ratio);
 
-  bool mapable_;    //!< Input pixel type can be mapped 
-  bool table_mapable_;    //!< Input pixel type can be mapped by a table. 
+  //: Initialize the byte mapping table
+  vxl_byte* compute_byte_table(const Type min, const Type max,
+                               const Type gamma,  const long double ratio);
+
+  //: Initialize the float mapping table
+  float* compute_float_table(const Type min, const Type max,
+                             const Type gamma, const long double ratio);
+
+
+  bool mapable_;    //!< input pixel type can be mapped 
+  bool table_mapable_;    //!< input pixel type can be mapped by a table. 
   unsigned size_;//!< number of elements in the map
-  vxl_byte* map_;   //!< Map array 
-  long double ratio_;    //!< scale for computed mapping 
+  long double ratio_L_;    //!< scale for computed Luminance mapping 
+  long double ratio_R_;    //!< scale for computed red channel mapping 
+  long double ratio_G_;    //!< scale for computed green channel mapping 
+  long double ratio_B_;    //!< scale for computed blue channel mapping 
+  long double ratio_A_;    //!< scale for computed alpha channel mapping 
 };
 
 #define VGUI_RANGE_MAP_INSTANTIATE(T) extern "please include vgui/vgui_range_map.txx first"
