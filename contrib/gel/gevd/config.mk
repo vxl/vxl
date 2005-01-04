a1743395eb3e7d2010fef7870fc0cbbb7d22b3dc
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-01-04 15:46:48 -0500

    updated w.r.t. CMakeLists.txt

--- contrib/gel/gevd/config.mk
+++ contrib/gel/gevd/config.mk
@@ -3,5 +3,6 @@ include ${IUEROOT}/contrib/gel/vsol/config.mk
 include ${IUEROOT}/contrib/gel/vtol/config.mk
 include ${IUEROOT}/contrib/gel/vdgl/config.mk
 include ${IUEROOT}/core/vil1/config.mk
+include ${IUEROOT}/core/vil/config.mk
 include ${IUEROOT}/core/vnl/config.mk
 include ${IUEROOT}/core/vbl/config.mk
