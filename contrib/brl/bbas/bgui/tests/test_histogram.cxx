54cc46f3dd2946be87b1f82ae94a83e129ee1956
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-01-12 15:58:53 -0500

    changed hard-coded image file name to runtime parameter, and added in-memory image create, in case image load fails

--- contrib/brl/bbas/bgui/tests/test_histogram.cxx
+++ contrib/brl/bbas/bgui/tests/test_histogram.cxx
@@ -1,4 +1,4 @@
-// This is brl/bbas/vgui/tests/vgui_test_histogram.cxx
+// This is brl/bbas/bgui/tests/test_histogram.cxx
 #include <testlib/testlib_test.h>
 #include <vxl_config.h>
 #include <vcl_climits.h> // for CHAR_BIT
@@ -6,47 +6,65 @@
 #include <vil/vil_load.h>
 #include <vil/vil_image_view.h>
 #include <vil/algo/vil_histogram.h>
-static void test_histogram()
+
+// create a 16 bit test image
+static vil_image_view<unsigned short> CreateTest16bitImage(int wd, int ht)
+{
+  vil_image_view<unsigned short> image(wd, ht);
+  for (int j = 0; j < ht; j++)
+    for (int i = 0; i < wd; i++)
+      image(i,j) = ((i-wd/2)*(j-ht/2)/16) & 0xffff;
+  return image;
+}
+
+static void test_histogram(int argc, char* argv[])
 {
   vil_image_view<unsigned short> img;
-  img = vil_load("c:/images/CAD/Mouse/18_al05_1770_0365.tif");
-  vcl_cout << img(500,500) << '\n';
+  if (argc > 1) {
+    vcl_cout << "Loading image " << argv[1] << " ... ";
+    img = vil_load(argv[1]);
+    if (!img) vcl_cout << "failed!!\n";
+    else      vcl_cout << "done.\n";
+  }
+  if (!img)
+    img = CreateTest16bitImage(1000,1000);
+
+  vcl_cout << img.ni() << 'x' << img.nj() << '\n'
+           << img(455,544) << '\n';
   vcl_vector<double> histogram;
   int nbins = 1<< vil_pixel_traits<unsigned short>::num_bits();
   vil_histogram(img,histogram, vil_pixel_traits<unsigned short>::minval(),
                 vil_pixel_traits<unsigned short>::maxval(), nbins);
-               
-  for(int i =0; i<nbins; i+=200)
-	vcl_cout << "H[" << i << "]=" << histogram[i] << '\n';
 
+  for (int i =0; i<nbins; i+=200)
+    vcl_cout << "H[" << i << "]=" << histogram[i] << '\n';
 
+    vcl_cout << "short [" << vil_pixel_traits<short>::minval()
+             << ' ' << vil_pixel_traits<short>::maxval()  << "]\n"
 
+             << "unsigned short[" << vil_pixel_traits<unsigned short>::minval()
+             << ' ' << vil_pixel_traits<unsigned short>::maxval()  << "]\n"
 
-	vcl_cout << "short [" << vil_pixel_traits<short>::minval() 
-           << ' ' << vil_pixel_traits<short>::maxval()  << "]\n";
+             << "int [" << vil_pixel_traits<int>::minval()
+             << ' ' << vil_pixel_traits<int>::maxval()  << "]\n"
 
-	vcl_cout << "unsigned short[" << vil_pixel_traits<unsigned short>::minval() 
-           << ' ' << vil_pixel_traits<unsigned short>::maxval()  << "]\n";
+             << "unsigned int [" << vil_pixel_traits<unsigned int>::minval()
+             << ' ' << vil_pixel_traits<unsigned int>::maxval()  << "]\n"
 
-	vcl_cout << "int [" << vil_pixel_traits<int>::minval() 
-           << ' ' << vil_pixel_traits<int>::maxval()  << "]\n";
+             << "long [" << vil_pixel_traits< long>::minval()
+             << ' ' << vil_pixel_traits< long>::maxval()  << "]\n"
 
-	vcl_cout << "unsigned int [" << vil_pixel_traits<unsigned int>::minval() 
-           << ' ' << vil_pixel_traits<unsigned int>::maxval()  << "]\n";
+             << "unsigned  long [" << vil_pixel_traits<unsigned  long>::minval()
+             << ' ' << vil_pixel_traits<unsigned  long>::maxval()  << "]\n"
 
-	vcl_cout << "long [" << vil_pixel_traits< long>::minval() 
-           << ' ' << vil_pixel_traits< long>::maxval()  << "]\n";
+             << "float [" << vil_pixel_traits< float>::minval()
+             << ' ' << vil_pixel_traits< float>::maxval()  << "]\n"
 
-	vcl_cout << "unsigned  long [" << vil_pixel_traits<unsigned  long>::minval() 
-           << ' ' << vil_pixel_traits<unsigned  long>::maxval()  << "]\n";
+             << "double [" << vil_pixel_traits< double>::minval()
+             << ' ' << vil_pixel_traits< double>::maxval()  << "]\n"
 
-	vcl_cout << "float [" << vil_pixel_traits< float>::minval() 
-           << ' ' << vil_pixel_traits< float>::maxval()  << "]\n";
-
-	vcl_cout << "double [" << vil_pixel_traits< double>::minval() 
-           << ' ' << vil_pixel_traits< double>::maxval()  << "]\n";
-
-	vcl_cout << "long double [" << vil_pixel_traits< long double>::minval() 
-           << ' ' << vil_pixel_traits< long double>::maxval()  << "]\n";
+             << "long double [" << vil_pixel_traits< long double>::minval()
+             << ' ' << vil_pixel_traits< long double>::maxval()  << "]\n";
 }
-TESTMAIN(test_histogram);
+
+TESTMAIN_ARGS(test_histogram);
