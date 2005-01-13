9f0d0aab6f9cc5e8da44f4e91d4b744766647864
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-01-13 02:11:05 -0500

    new header files added

--- contrib/brl/bseg/segv/tests/test_include.cxx
+++ contrib/brl/bseg/segv/tests/test_include.cxx
@@ -1,7 +1,11 @@
 #include <bseg/segv/segv_menus.h>
 #include <bseg/segv/segv_segmentation_manager.h>
+#include <bseg/segv/segv_vil_menus.h>
+#include <bseg/segv/segv_vil_segmentation_manager.h>
 #include <bseg/segv/segv_vtol2D_tableau.h>
 #include <bseg/segv/segv_vtol2D_tableau_sptr.h>
 #include <bseg/segv/segv_vtol_soview2D.h>
+#include <bseg/segv/vil_dll.h>
+#include <bseg/segv/dll.h>
 
 int main() { return 0; }
