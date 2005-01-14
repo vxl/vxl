74c9ec19428b045b54da787fc1dc0450db80373d
Gehua Yang <norwegian_wood@users.sourceforge.net>
2005-01-14 14:55:03 -0500

    xcv_image_tableau_sptr should be inherited from vgui_image_tableau_sptr

--- contrib/oxl/xcv/xcv_image_tableau_sptr.h
+++ contrib/oxl/xcv/xcv_image_tableau_sptr.h
@@ -8,8 +8,9 @@
 // .FILE    xcv_image_tableau_sptr.cxx
 
 #include <vgui/vgui_tableau_sptr.h>
+#include <vgui/vgui_image_tableau_sptr.h>
 
 class xcv_image_tableau;
-typedef vgui_tableau_sptr_t<xcv_image_tableau, vgui_tableau_sptr> xcv_image_tableau_sptr;
+typedef vgui_tableau_sptr_t<xcv_image_tableau, vgui_image_tableau_sptr> xcv_image_tableau_sptr;
 
 #endif // xcv_image_tableau_sptr_h_
