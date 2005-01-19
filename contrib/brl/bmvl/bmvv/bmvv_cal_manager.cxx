cec05342149fd7017d307dd20ba11cc4e3d3fe49
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-01-19 17:28:56 -0500

    TABs replaced by spaces, and avoid compiler warnings

--- contrib/brl/bmvl/bmvv/bmvv_cal_manager.cxx
+++ contrib/brl/bmvl/bmvv/bmvv_cal_manager.cxx
@@ -52,7 +52,7 @@ bmvv_cal_manager *bmvv_cal_manager::instance()
 bmvv_cal_manager::
 bmvv_cal_manager() : vgui_wrapper_tableau(), cam_(vgl_p_matrix<double>()),
                      prev_cam_(vgl_p_matrix<double>())
-{                       
+{
   corrs_.resize(0);
   corrs_valid_.resize(0);
   proj_image_pts_.resize(0);
@@ -71,7 +71,7 @@ void bmvv_cal_manager::init()
 {
   itab_ = bgui_image_tableau_new();
   btab_ = bgui_vtol2D_tableau_new(itab_);
-  ptab_ = bgui_picker_tableau_new(btab_); 
+  ptab_ = bgui_picker_tableau_new(btab_);
   vgui_viewer2D_tableau_sptr vtab =  vgui_viewer2D_tableau_new(ptab_);
   vgui_shell_tableau_sptr shell = vgui_shell_tableau_new(vtab);
   this->add_child(shell);
@@ -85,17 +85,16 @@ void bmvv_cal_manager::init()
 //=========================================================================
 bool bmvv_cal_manager::handle(const vgui_event &e)
 {
-  float ix=0, iy=0;
   vgui_projection_inspector proj;
   if (e.type == vgui_KEY_PRESS)
-    {
-      if(btab_)
-        if(e.key == 'u')
-          {
-            btab_->deselect_all();
-            return true;
-          }
-    }
+  {
+    if (btab_)
+      if (e.key == 'u')
+      {
+        btab_->deselect_all();
+        return true;
+      }
+  }
   return this->child.handle(e);
 }
 
@@ -113,12 +112,12 @@ void bmvv_cal_manager::quit()
 void bmvv_cal_manager::load_image_file(vcl_string image_filename, bool /* greyscale */, unsigned col, unsigned row)
 {
   img_ = vil_load_image_resource(image_filename.c_str());
-  if(!img_)
-    {
-      vcl_cout << "Null image resource - couldn't load from " 
-               << image_filename << '\n';
-      return;
-    }
+  if (!img_)
+  {
+    vcl_cout << "Null image resource - couldn't load from "
+             << image_filename << '\n';
+    return;
+  }
   if (itab_)
   {
     itab_->set_image_resource(img_);
@@ -130,14 +129,13 @@ void bmvv_cal_manager::load_image_file(vcl_string image_filename, bool /* greysc
 void bmvv_cal_manager::add_image(vil_image_resource_sptr const& image,
                                  bool greyscale)
 {
-
   if (greyscale)
   {
 #if 0// do nothing right now
     vil1_memory_image_of<unsigned char> temp1 =
     brip_vil1_float_ops::convert_to_grey(image);
     img_ = temp1;
-#endif 
+#endif
   }
   else
     img_ = image;
@@ -150,6 +148,7 @@ void bmvv_cal_manager::add_image(vil_image_resource_sptr const& image,
   }
   vcl_cout << "In bmvv_cal_manager::load_image() - null tableau\n";
 }
+
 //=========================================================================
 //: load an image an put it in the currently selected grid cell
 //=========================================================================
@@ -182,6 +181,7 @@ void bmvv_cal_manager::adjust_contrast()
   if (!contrast.ask())
     return;
 }
+
 //===================================================================
 //: Clear the display
 //===================================================================
@@ -198,18 +198,18 @@ void bmvv_cal_manager::clear_display()
 //===================================================================
 void bmvv_cal_manager::clear_selected()
 {
-  if(btab_)
+  if (btab_)
     btab_->clear_all();
 }
 
 void bmvv_cal_manager::draw_corr_point(const float x, const float y)
 {
- if (!btab_)
+  if (!btab_)
     return;
- btab_->set_point_radius(5.0f);
- btab_->set_foreground(0.0f, 1.0f, 0.0f);
- btab_->add_point(x, y);
- btab_->post_redraw();
+  btab_->set_point_radius(5.0f);
+  btab_->set_foreground(0.0f, 1.0f, 0.0f);
+  btab_->add_point(x, y);
+  btab_->post_redraw();
 }
 
 
@@ -231,7 +231,7 @@ void bmvv_cal_manager::load_image_and_camera()
   vcl_ifstream cam_istr(cam_file.c_str());
   cam_istr >> cam_;
   vcl_cout << "Read Camera " << cam_ << '\n';
-   this->project_world();
+  this->project_world();
 }
 
 void bmvv_cal_manager::load_corrs()
@@ -254,13 +254,13 @@ void bmvv_cal_manager::load_corrs()
   if (!btab_)
     return;
   btab_->clear_all();
- this->project_world();
- this->draw_correspondences();
-}  
+  this->project_world();
+  this->draw_correspondences();
+}
 
 void bmvv_cal_manager::save_corrs()
 {
- vgui_dialog corr_dlg("Save Correspondences");
+  vgui_dialog corr_dlg("Save Correspondences");
   static vcl_string corr_file = "";
   static vcl_string corr_ext = "*.cm";
   corr_dlg.file("Correspondence File", corr_ext, corr_file);
@@ -283,11 +283,11 @@ void bmvv_cal_manager::save_camera()
   if (!cam_dlg.ask())
     return;
   vcl_ofstream cam_ostr(cam_file.c_str());
-  if(!cam_ostr)
-    {
-      vcl_cout << "Bad camera file\n";
-      return;
-    }
+  if (!cam_ostr)
+  {
+    vcl_cout << "Bad camera file\n";
+    return;
+  }
   cam_ostr << cam_;
   cam_ostr.close();
 }
@@ -329,13 +329,14 @@ bool bmvv_cal_manager::draw_correspondences()
   unsigned i = 0;
   for (vcl_vector<vgl_point_2d<double> >::iterator pit = corrs_.begin();
        pit != corrs_.end(); ++pit, ++i)
-    if(corrs_valid_[i])
+    if (corrs_valid_[i])
       corr_sovs_.push_back(btab_->add_point((*pit).x(), (*pit).y()));
     else
       corr_sovs_.push_back((vgui_soview2D_point*)0);
   btab_->post_redraw();
   return true;
-}  
+}
+
 void bmvv_cal_manager::clear_correspondences()
 {
   if (!btab_)
@@ -357,20 +358,19 @@ bool bmvv_cal_manager::draw_projected_world_points()
   point_3d_map_.clear();
   btab_->set_point_radius(5.0f);
   btab_->set_foreground(1.0f, 1.0f, 0.2f);
-  for (vcl_vector<vgl_point_2d<double> >::iterator pit = proj_image_pts_.begin();      pit != proj_image_pts_.end(); pit++, i++)
-    {
-      vgui_soview2D_point* sov = 
-        btab_->add_point((*pit).x(), (*pit).y());
-      int id = sov->get_id();
-      point_3d_map_[id]=i;
-    }
+  for (vcl_vector<vgl_point_2d<double> >::iterator pit = proj_image_pts_.begin();
+       pit != proj_image_pts_.end(); pit++, ++i)
+  {
+    vgui_soview2D_point* sov = btab_->add_point((*pit).x(), (*pit).y());
+    int id = sov->get_id();
+    point_3d_map_[id]=i;
+  }
   btab_->post_redraw();
   return true;
 }
 
 int  bmvv_cal_manager::get_selected_proj_world_pt(vgl_point_2d<double>&  pt)
 {
-   
   if (!btab_)
     return  -1;
   vcl_vector<unsigned> ids = btab_->get_selected();
@@ -381,33 +381,33 @@ int  bmvv_cal_manager::get_selected_proj_world_pt(vgl_point_2d<double>&  pt)
     vcl_cout << "Nothing selected\n";
     return -1;
   }
-  int i = point_3d_map_[ids[n-1]];
-  if(i<proj_image_pts_.size())
-    {
-      pt =  proj_image_pts_[i];
-	  return i;
-    }
+  unsigned int i = point_3d_map_[ids[n-1]];
+  if (i<proj_image_pts_.size())
+  {
+    pt =  proj_image_pts_[i];
+    return i;
+  }
   vcl_cout << "Bogus correspondence\n";
   return -1;
 }
 
 void bmvv_cal_manager::pick_correspondence()
 {
-  if(!world_.size())
-    {
-      vcl_cout << "Can't pick correspondence - null 3D world\n";
-      return;
-    }
+  if (!world_.size())
+  {
+    vcl_cout << "Can't pick correspondence - null 3D world\n";
+    return;
+  }
   vgl_point_2d<double> pt;
   int id = this->get_selected_proj_world_pt(pt);
-	if(id<0)
+  if (id<0)
     return;
   vcl_cout << "Projected world point " << pt << '\n';
   if (!ptab_)
-    {
-      vcl_cout << "Can't pick correspondence - null picker tableau\n";
-      return;
-    }
+  {
+    vcl_cout << "Can't pick correspondence - null picker tableau\n";
+    return;
+  }
   ptab_->set_color(0.0f, 1.0f, 1.0f);
   ptab_->set_line_width(3.0f);
   float x, y;
@@ -421,7 +421,7 @@ void bmvv_cal_manager::pick_correspondence()
     return;
   btab_->set_foreground(0.0f, 1.0f, 1.0f);
   btab_->set_point_radius(5.0);
-  if(!corr_sovs_[id])
+  if (!corr_sovs_[id])
     corr_sovs_[id]=btab_->add_point(x, y);
   else {
     corr_sovs_[id]->x = x;
@@ -432,23 +432,23 @@ void bmvv_cal_manager::pick_correspondence()
 
 void bmvv_cal_manager::remove_correspondence()
 {
-  if(!world_.size())
-    {
-      vcl_cout << "Can't remove correspondence - null 3D world\n";
-      return;
-    }
+  if (!world_.size())
+  {
+    vcl_cout << "Can't remove correspondence - null 3D world\n";
+    return;
+  }
   vgl_point_2d<double> pt;
   int id = this->get_selected_proj_world_pt(pt);
-	if(id<0)
+  if (id<0)
     return;
   corrs_[id].set(0, 0);
   corrs_valid_[id]=false;
   corr_sovs_[id]=(vgui_soview2D_point*)0;
   if (!btab_)
-    {
-      vcl_cout << "Correspondence removal aborted in progress- null btab\n";
-      return;
-    }
+  {
+    vcl_cout << "Correspondence removal aborted in progress- null btab\n";
+    return;
+  }
   this->project_world();
   this->draw_correspondences();
 }
@@ -456,11 +456,11 @@ void bmvv_cal_manager::remove_correspondence()
 void bmvv_cal_manager::project_world()
 {
   if (!btab_)
-    {
-      vcl_cout << "Can't project world - null btab\n";
-      return;
-    }
-	btab_->clear_all();
+  {
+    vcl_cout << "Can't project world - null btab\n";
+    return;
+  }
+  btab_->clear_all();
   brct_algos::project(world_, cam_, proj_image_pts_);
   this->draw_projected_world_points();
   this->draw_correspondences();
@@ -477,40 +477,40 @@ void bmvv_cal_manager::solve_camera()
   read_homg_dlg.checkbox("Restore Previous Camera? ", restore);
   if (!read_homg_dlg.ask())
     return;
-  if(restore)
-    {
-      cam_=prev_cam_;
-      return;
-    }
+  if (restore)
+  {
+    cam_=prev_cam_;
+    return;
+  }
   vcl_vector<double> image_y;
   vcl_vector< vgl_point_2d<double> > zero_Z_image_corr;
   vcl_vector< vgl_point_3d<double> > zero_Z_pts;
   vcl_vector< vgl_point_3d<double> > non_zero_Z_pts;
-  int n = world_.size();
-  for(unsigned i = 0; i<n; i++)
+  unsigned int n = world_.size();
+  for (unsigned i = 0; i<n; i++)
+  {
+    if (!corrs_valid_[i])
+      continue;
+    if (vcl_fabs(world_[i].z()) < 0.01) //planar points
     {
-      if(!corrs_valid_[i])
-        continue;
-      if(vcl_fabs(world_[i].z())<0.01)//planar points
-        {
-          zero_Z_pts.push_back(world_[i]);
-          zero_Z_image_corr.push_back(corrs_[i]);
-        }
-      else // non-planar point for camera elevation constraint
-        {
-          image_y.push_back(corrs_[i].y());
-          non_zero_Z_pts.push_back(world_[i]);
-        }
+      zero_Z_pts.push_back(world_[i]);
+      zero_Z_image_corr.push_back(corrs_[i]);
     }
+    else // non-planar point for camera elevation constraint
+    {
+      image_y.push_back(corrs_[i].y());
+      non_zero_Z_pts.push_back(world_[i]);
+    }
+  }
   vgl_h_matrix_2d<double> H;
-  if(!brct_algos::homography(zero_Z_pts, zero_Z_image_corr, H, optimize))
-    {
-      vcl_cout << "Linear SVD solve for homography failed \n";
-      return;
-    }
+  if (!brct_algos::homography(zero_Z_pts, zero_Z_image_corr, H, optimize))
+  {
+    vcl_cout << "Linear SVD solve for homography failed\n";
+    return;
+  }
   //protect from bad solution by saving old camera
   prev_cam_ = cam_;
-  if(!planar)
+  if (!planar)
     cam_= brct_algos::p_from_h(H,image_y, non_zero_Z_pts);
   else
     cam_= brct_algos::p_from_h(H);
@@ -518,12 +518,9 @@ void bmvv_cal_manager::solve_camera()
   if (!btab_)
     return;
   btab_->clear_all();
- this->project_world();
- this->draw_correspondences();
+  this->project_world();
+  this->draw_correspondences();
 }
-  
-
-
 
 
 void bmvv_cal_manager::
@@ -561,11 +558,6 @@ draw_vsol_point(const int cam, vsol_point_2d_sptr const & point,
 }
 
 
-
-
-
-
-
 void bmvv_cal_manager::compute_ransac_homography()
 {
   vgui_dialog read_homg_dlg("Read Correspondence File");
@@ -592,13 +584,13 @@ void bmvv_cal_manager::compute_ransac_homography()
     vcl_cout << "Need a file name\n";
     return;
   }
- 
+
   this->clear_display();
   vcl_ifstream istr(corr_filename.c_str());
   vcl_ofstream ostr(cam_filename.c_str());
-   vcl_string target_file = cam_filename + "-target";
+  vcl_string target_file = cam_filename + "-target";
   vcl_ofstream ostr_tar(target_file.c_str());
-vcl_vector<bool> valid;
+  vcl_vector<bool> valid;
   vcl_vector<vgl_point_2d<double> > image_points;
   vcl_vector<vgl_point_3d<double> > world_points;
   if (!brct_algos::read_target_corrs(istr, valid, image_points, world_points))
@@ -607,29 +599,29 @@ vcl_vector<bool> valid;
     return;
   }
   bool show = show_projected_points;
-  // compute the homography using ransac  
+  // compute the homography using ransac
   vgl_h_matrix_2d<double> H;
   switch (choice)
-    {
-    case 0:
-      if(!brct_algos::homography(world_points, image_points, H, optimize))
-        show = false;
-      break;
-    case 1:
-      if(!brct_algos::homography_ransac(world_points, image_points, H, optimize))
-        show = false;
-      break;
-    case 2:
-      if(!brct_algos::homography_muse(world_points, image_points, H, optimize))
-        show = false;
-      break;
-    }
+  {
+   case 0:
+    if (!brct_algos::homography(world_points, image_points, H, optimize))
+      show = false;
+    break;
+   case 1:
+    if (!brct_algos::homography_ransac(world_points, image_points, H, optimize))
+      show = false;
+    break;
+   case 2:
+    if (!brct_algos::homography_muse(world_points, image_points, H, optimize))
+      show = false;
+    break;
+  }
   vcl_vector<vgl_point_2d<double> > proj_image_points;
   vgl_p_matrix<double> P =  brct_algos::p_from_h(H);
   brct_algos::project(world_points, P, proj_image_points);
 
-if(!btab_)
-	return;
+  if (!btab_)
+    return;
   vgui_style_sptr r_style = vgui_style::new_style(1.0, 0.0, 0.0, 5, 0);
   vgui_style_sptr g_style = vgui_style::new_style(0.0, 1.0, 0.0, 5, 0);
   for (unsigned int i = 0; i<world_points.size(); i++)
@@ -640,7 +632,7 @@ if(!btab_)
     vsol_point_2d_sptr vpip = new vsol_point_2d(pip.x(),pip.y());
     vcl_cout << ip << " == " << pip << vcl_endl;
     btab_->add_vsol_point_2d(vip,r_style);
-    if(show)btab_->add_vsol_point_2d(vpip,g_style);
+    if (show) btab_->add_vsol_point_2d(vpip,g_style);
   }
   btab_->post_redraw();
 
