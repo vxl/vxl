3376154f796213b811743fa7a0ddf9ec78243760
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-01-20 15:32:24 -0500

    *** empty log message ***

--- contrib/brl/b3p/netlib/b3p_netlib.h
+++ contrib/brl/b3p/netlib/b3p_netlib.h
@@ -1,37 +1,37 @@
 #ifndef B3P_NETLIB_H_
 #define B3P_NETLIB_H_
-
 /*
-//:  \file
-//  \author Kongbin Kang
-//  \date Jan 5, 2005
-//  \brief declearations for netlib functions
+//:
+// \file
+// \author Kongbin Kang
+// \date   Jan 5, 2005
+// \brief  declarations for netlib functions
 */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int trapru_(double f(float*), float *a, float *b, int *m, float *	trule);
+int trapru_(double f(float*), float *a, float *b, int *m, float *trule);
 
-int simpru_(double f(float* ), float *a, float *b, int *m, float *srule);
+int simpru_(double f(float*), float *a, float *b, int *m, float *srule);
 
 /*
-//: input parameters
-//  f: integrant
+//:
+// input parameters
+//  f: integrand
 //  a: left endpoint
 //  b: right endpoint
-//  tol: 
-//  
+//  tol: tolerance
+//
 //  output parameters
 //  errbdd: error estimation
 //  m: substates
 */
-int adaptquad_(double f(float*), float *a, float *b, float *tol, float * srmat, float *integral, float *errbdd, int *m, int *state);
+int adaptquad_(double f(float*), float *a, float *b, float *tol, float *srmat, float *integral, float *errbdd, int *m, int *state);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif 
-
+#endif /* B3P_NETLIB_H_ */
