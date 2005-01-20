08985a38bb240d89c40675001f9e92762e38949a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-01-20 15:39:49 -0500

    *** empty log message ***

--- contrib/brl/bbas/bnl/algo/bnl_adaptsimpson_integral.h
+++ contrib/brl/bbas/bnl/algo/bnl_adaptsimpson_integral.h
@@ -1,27 +1,24 @@
 #ifndef BNL_ADAPTSIMPSON_INTEGRAL_H_
 #define BNL_ADAPTSIMPSON_INTEGRAL_H_
-
-// : 
+//:
 // \file
 // \author Kongbin Kang at Brown
-// \date Jan. 17th, 2005
-// 
+// \date   Jan. 17th, 2005
+//
 #include <bnl/bnl_definite_integral.h>
 
-class bnl_adaptsimpson_integral : public bnl_definite_integral {
+class bnl_adaptsimpson_integral : public bnl_definite_integral
+{
+ private:
+  //: used to wrap the function class to an ordinary function.
+  static double int_fnct_(float* x);
 
-  private:
-    //: used to wrap the function class to a ordinary function.
-    static double int_fnct_(float* x);
+ public:
+  bnl_adaptsimpson_integral() {}
 
-
-  public:
-
-    bnl_adaptsimpson_integral() {}
-
-    //: a nd b are integral limits respectively. 
-    // n is the number of intervals used in intergral. 
-   double integral(bnl_integrant_fnct *f, float a, float b, float accuracy);
+  //: a and b are integral limits respectively.
+  // n is the number of intervals used in integral.
+  double integral(bnl_integrant_fnct *f, float a, float b, float accuracy);
 };
 
 #endif
--- contrib/brl/bbas/bnl/algo/bnl_simpson_integral.h
+++ contrib/brl/bbas/bnl/algo/bnl_simpson_integral.h
@@ -1,28 +1,26 @@
-
 #ifndef BNL_SIMPSON_INTEGRAL_H_
 #define BNL_SIMPSON_INTEGRAL_H_
-
-// : 
+//:
 // \file
 // \author Kongbin Kang at Brown
-// \date Jan. 17th, 2005
-// 
+// \date   Jan. 17th, 2005
+//
 #include <bnl/bnl_definite_integral.h>
 
-class bnl_simpson_integral : public bnl_definite_integral {
+class bnl_simpson_integral : public bnl_definite_integral
+{
+ private:
+  //: used to wrap the function class to an ordinary function.
+  static double int_fnct_(float* x);
 
-  private:
-    //: used to wrap the function class to a ordinary function.
-    static double int_fnct_(float* x);
+ public:
 
-  public:
+  bnl_simpson_integral() {}
 
-    bnl_simpson_integral() {}
-
-    //: a nd b are integral limits respectively. 
-    // n is the number of intervals used in intergral. 
-    //The actual subinterval used is 2* num_intervals_    
-   double integral(bnl_integrant_fnct *f, float a, float b, int n);
+  //: a and b are integral limits respectively.
+  // n is the number of intervals used in integral.
+  // The actual subinterval used is 2* num_intervals_
+  double integral(bnl_integrant_fnct *f, float a, float b, int n);
 };
 
 #endif
