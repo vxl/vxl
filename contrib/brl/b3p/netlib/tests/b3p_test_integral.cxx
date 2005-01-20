d66b4943113de63498e411c4a98c35815599835a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-01-20 15:34:44 -0500

    vcl prefixes added

--- contrib/brl/b3p/netlib/tests/b3p_test_integral.cxx
+++ contrib/brl/b3p/netlib/tests/b3p_test_integral.cxx
@@ -1,5 +1,5 @@
 #include <b3p_netlib.h>
-#include <stdio.h>
+#include <vcl_cstdio.h>
 
 double f(float *x)
 {
@@ -10,10 +10,9 @@ double f(float *x)
 #if 0
 int b3p_test_integral(int argc, char * argv[])
 #endif
-  
+
 int main()
 {
-  
   float a = 0;
   float b = 1;
   float res;
@@ -21,11 +20,10 @@ int main()
   int n = 100;
 
   simpru_(&f, &a, &b, &n, &res);
-  printf("simpson integral of x/(1+x^2) from 0 to 1 (%d grids) is %lf \n", n, res);
+  vcl_printf("simpson integral of x/(1+x^2) from 0 to 1 (%d grids) is %lf\n", n, res);
 
   trapru_(&f, &a, &b, &n, &res);
-  printf("trapezod integral of x/(1+x^2) from 0 to 1 (%d grids) is %lf \n", n, res);
-
+  vcl_printf("trapezod integral of x/(1+x^2) from 0 to 1 (%d grids) is %lf\n", n, res);
 
   double rvec[11];
   float rmat[1111];
@@ -33,6 +31,6 @@ int main()
   float errbound;
   int stat;
   adaptquad_(&f, &a, &b, &tol, rmat, &res, &errbound, &n, &stat );
-  printf("adapted simpson integral of x/(1+x^2) from 0 to 1 (%d grids)  is %lf \n", n,  res);
+  vcl_printf("adapted simpson integral of x/(1+x^2) from 0 to 1 (%d grids)  is %lf\n", n,  res);
   return 0;
 }
