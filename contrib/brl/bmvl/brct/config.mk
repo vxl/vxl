7fa5c6432e4f44fc8e2a0a96efd8594053e0819d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-01-22 06:33:33 -0500

    brct_dense_reconstructor uses rrel

--- contrib/brl/bmvl/brct/config.mk
+++ contrib/brl/bmvl/brct/config.mk
@@ -6,6 +6,7 @@ include ${IUEROOT}/contrib/brl/bbas/bsol/config.mk
 include ${IUEROOT}/contrib/brl/bseg/sdet/config.mk
 include ${IUEROOT}/contrib/brl/bseg/brip/config.mk
 include ${IUEROOT}/contrib/oxl/mvl/config.mk
+include ${IUEROOT}/contrib/rpl/rrel/config.mk
 include ${IUEROOT}/contrib/gel/gevd/config.mk
 include ${IUEROOT}/contrib/gel/vdgl/config.mk
 include ${IUEROOT}/contrib/gel/vsol/config.mk
