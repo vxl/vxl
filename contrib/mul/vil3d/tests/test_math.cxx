830e2fa5a3400d01d5bd5f8f98ad8db033d22b0b
Kevin de Souza <kdesouza@users.sourceforge.net>
2005-02-10 11:39:45 -0500

    Added test program for vil3d_math_value_range() and vil3d_math_value_range_percentile()

--- contrib/mul/vil3d/tests/test_math.cxx
+++ contrib/mul/vil3d/tests/test_math.cxx
@@ -3,6 +3,7 @@
 #include <vcl_iostream.h>
 #include <vil3d/vil3d_math.h>
 
+
 static void test_image_view_maths_float()
 {
   vcl_cout << "********************************\n"
@@ -35,9 +36,61 @@ static void test_image_view_maths_float()
   TEST_NEAR("Sum of squares",sum_sq,sum_sq0,1e-8);
 }
 
+
+static void test_math_value_range()
+{
+  vcl_cout << "********************************\n"
+           << " Testing vil3d_math_value_range \n"
+           << "********************************\n";
+
+  // Create a test image with values 1...1000
+  unsigned ni=10, nj=10, nk=10;
+  vil3d_image_view<int> img(ni, nj, nk);
+  for (unsigned i=0; i<ni; ++i)
+  {
+    for (unsigned j=0; j<nj; ++j)
+    {
+      for (unsigned k=0; k<nk; ++k)
+      {
+        int val = k*nj*ni + j*ni + i +1; // NB Data values not already sorted!
+        img(i,j,k) = val;
+      }
+    }
+  }
+
+  // Check the min/max values
+  int min=1e6; 
+  int max=-1e6;
+  vil3d_math_value_range(img, min, max);
+  TEST("vil3d_math_value_range(): min", min==1, true);
+  TEST("vil3d_math_value_range(): max", max==1000, true);
+
+  int val=1e6;
+
+  // Test a likely pair of percentiles
+  vil3d_math_value_range_percentile(img, 0.05, val);
+  TEST("vil3d_math_value_range_percentile():  5  %", val==50, true);
+  vil3d_math_value_range_percentile(img, 0.95, val);
+  TEST("vil3d_math_value_range_percentile(): 95  %", val==950, true);  
+
+  // Test a likely pair of percentiles
+  vil3d_math_value_range_percentile(img, 0.10, val);
+  TEST("vil3d_math_value_range_percentile(): 10  %", val==100, true);
+  vil3d_math_value_range_percentile(img, 0.90, val);
+  TEST("vil3d_math_value_range_percentile(): 90  %", val==900, true);  
+  
+  // Test an unlikely pair of percentiles
+  vil3d_math_value_range_percentile(img, 0.311, val);
+  TEST("vil3d_math_value_range_percentile(): 31.1 %", val==311, true);
+  vil3d_math_value_range_percentile(img, 0.737, val);
+  TEST("vil3d_math_value_range_percentile(): 73.7 %", val==737, true);  
+}
+
+
 static void test_math()
 {
   test_image_view_maths_float();
+  test_math_value_range();
 }
 
 TESTMAIN(test_math);
