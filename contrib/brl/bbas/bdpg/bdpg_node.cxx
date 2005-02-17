507057f11d314661b4d117e35da1fba5cc34d5b0
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-02-17 14:16:00 -0500

    typo

--- contrib/brl/bbas/bdpg/bdpg_node.cxx
+++ contrib/brl/bbas/bdpg/bdpg_node.cxx
@@ -1,13 +1,18 @@
+//:
+// \file
 #include <vcl_cmath.h> //for exp
 #include <bdpg/bdpg_node.h>
 #include <vnl/vnl_numeric_traits.h>
+
 bdpg_node::bdpg_node() :
   previous_row_(-1),   previous_col_(-1), prior_prob_(0),
   log_prob_(-vnl_numeric_traits<double>::maxval)
 {
 }
 
-bdpg_node::~bdpg_node(){}
+bdpg_node::~bdpg_node()
+{
+}
 
 void bdpg_node::set_prior_prob(const double p)
 {
@@ -20,7 +25,7 @@ double bdpg_node::p()
   return vcl_exp(log_prob_);
 }
 
-//: append the path probabiity to the current probability
+//: append the path probability to the current probability
 void bdpg_node::update_prob(const double p)
 {
   log_prob_ += vcl_log(p);
