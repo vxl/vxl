797c78241858de597aba4a21a09334c22e112b4f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-02-17 12:41:56 -0500

    avoid lines of more than 132 characters

--- core/vnl/algo/tests/test_integral.cxx
+++ core/vnl/algo/tests/test_integral.cxx
@@ -7,25 +7,25 @@
 
 class my_test_integrant : public vnl_analytic_integrant
 {
-  public:
-    double f_(double x) { return x/(1+x*x); }
+ public:
+  double f_(double x) { return x/(1+x*x); }
 };
 
 void test_integral()
 {
-
   my_test_integrant f;
   vnl_simpson_integral simpson_integral;
 
   double a = 0;
   double b = 1;
 
-  TEST_NEAR( "simpson integral of x/(1+x^2) from  0 to 1 is: ", simpson_integral.integral(&f, a, b, 100), 0.5*vcl_log(2.0), 1e-6);
+  TEST_NEAR("simpson integral of x/(1+x^2) from  0 to 1 is: ",
+            simpson_integral.integral(&f, a, b, 100), 0.5*vcl_log(2.0), 1e-6);
 
   vnl_adaptsimpson_integral adaptsimpson_integral;
 
-  TEST_NEAR( "adaptive simpson integral of x/(1+x^2) from 0 to 1 is: ", adaptsimpson_integral.integral(&f, a, b, 1e-13), 0.5*vcl_log(2.0), 1e-6);
-   
+  TEST_NEAR("adaptive simpson integral of x/(1+x^2) from 0 to 1 is: ",
+            adaptsimpson_integral.integral(&f, a, b, 1e-13), 0.5*vcl_log(2.0), 1e-6);
 }
 
 TESTMAIN( test_integral );
