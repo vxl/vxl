358461b9206fb7aeba59226f6d87d8b3c116fd11
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-02-19 09:28:17 -0500

    *** empty log message ***

--- contrib/mul/vil3d/tests/test_resample.cxx
+++ contrib/mul/vil3d/tests/test_resample.cxx
@@ -1,4 +1,4 @@
-// This is mul/vil3d/tests/test_math.cxx
+// This is mul/vil3d/tests/test_resample.cxx
 #include <testlib/testlib_test.h>
 #include <vcl_iostream.h>
 #include <vil3d/vil3d_resample_simple.h>
@@ -9,29 +9,29 @@
 //==================================================================================
 static void test_resample_simple()
 {
-  vcl_cout << "********************************\n"
-           << " Testing vil3d_resample_simple  \n"
-           << "********************************\n";
+  vcl_cout << "*******************************\n"
+           << " Testing vil3d_resample_simple\n"
+           << "*******************************\n";
 
   int sni=3;
   int snj=3;
   int snk=3;
   vil3d_image_view<int> src(sni, snj, snk);
   for (int i=0; i<sni; ++i)
-    for (int j=0; j<snj; ++j)  
+    for (int j=0; j<snj; ++j)
       for (int k=0; k<snk; ++k)
         src(i,j,k) = k*snj + j*sni + i;
 
   int dni=6;
   int dnj=6;
-  int dnk=6;  
+  int dnk=6;
   vil3d_image_view<int> dst(dni, dnj, dnj);
 
   vil3d_resample_simple(src, dst, 2, 2, 2);
 
   bool all_voxs_correct = true;
   for (int i=0; i<dni; ++i)
-  {  
+  {
     for (int j=0; j<dnj; ++j)
     {
       for (int k=0; k<dnk; ++k)
@@ -48,22 +48,22 @@ static void test_resample_simple()
 //==================================================================================
 static void test_resample_trilinear()
 {
-  vcl_cout << "*********************************\n"
+  vcl_cout << "**********************************\n"
            << " Testing vil3d_resample_trilinear\n"
-           << "*********************************\n";
+           << "**********************************\n";
 
   int sni=3;
   int snj=3;
   int snk=3;
   vil3d_image_view<int> src(sni, snj, snk);
   for (int i=0; i<sni; ++i)
-    for (int j=0; j<snj; ++j)  
+    for (int j=0; j<snj; ++j)
       for (int k=0; k<snk; ++k)
         src(i,j,k) = k;
 
   int dni=6;
   int dnj=6;
-  int dnk=6;  
+  int dnk=6;
   vil3d_image_view<int> dst(dni, dnj, dnj);
 
   vil3d_resample_trilinear(src, dst, 2, 2, 2);
@@ -71,7 +71,7 @@ static void test_resample_trilinear()
   bool all_voxs_correct = true;
   for (int i=0; i<dni; ++i)
   {
-    for (int j=0; j<dnj; ++j)  
+    for (int j=0; j<dnj; ++j)
     {
       for (int k=0; k<dnk; ++k)
       {
