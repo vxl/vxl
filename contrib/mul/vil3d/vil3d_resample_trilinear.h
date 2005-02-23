a0eace85b7f6ea6c25a2d3a534ebf697f86cf53e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-02-23 17:59:39 -0500

    *** empty log message ***

--- contrib/mul/vil3d/vil3d_resample_trilinear.h
+++ contrib/mul/vil3d/vil3d_resample_trilinear.h
@@ -1,8 +1,6 @@
-// This is contrib/mul/vil3d/vil3d_resample_trilinear.h
-
+// This is mul/vil3d/vil3d_resample_trilinear.h
 #ifndef vil3d_resample_trilinear_h_
 #define vil3d_resample_trilinear_h_
-
 //:
 // \file
 // \brief Resample a 3D image by an integer factor in each dimension
@@ -11,15 +9,15 @@
 #include <vil3d/vil3d_image_view.h>
 
 //: Resample a 3D image by an integer factor in each dimension.
-//  dst_image resized by factors dx, dy, dz.
+//  \p dst_image resized by factors \p dx, \p dy, \p dz.
 //  dst_image(i, j, k, p) is sampled from src_image(i/dx, j/dy, k/dz, p).
 //  Trilinear interpolation is performed on all voxels except ones near
 //  the upper boundaries.
-template <class T >
-void vil3d_resample_trilinear(const vil3d_image_view< T >& src_image,
-                              vil3d_image_view< T >& dst_image,
-                              const unsigned dx, 
-                              const unsigned dy, 
+template <class T>
+void vil3d_resample_trilinear(const vil3d_image_view<T>& src_image,
+                              vil3d_image_view<T>& dst_image,
+                              const unsigned dx,
+                              const unsigned dy,
                               const unsigned dz);
 
 #endif // vil3d_resample_trilinear_h_
