6d41db66bd1e187a52efa6955cb4faec63326b81
Kevin de Souza <kdesouza@users.sourceforge.net>
2005-02-25 10:40:56 -0500

    Added composite progress class

---
+++ contrib/mul/mbl/mbl_progress_composite.cxx
@@ -0,0 +1,73 @@
+#ifdef __GNUC__
+#pragma implementation
+#endif
+
+
+#include <mbl/mbl_progress_composite.h>
+#include <vcl_iostream.h>
+#include <vsl/vsl_indent.h>
+
+
+//=======================================================================
+mbl_progress_composite::mbl_progress_composite()
+{
+  
+}
+
+
+//=======================================================================
+mbl_progress_composite::~mbl_progress_composite()
+{
+}
+
+
+//=======================================================================
+void mbl_progress_composite::add_progress(mbl_progress *progress_object)
+{
+  progress_objects_.push_back(progress_object);
+}
+
+
+//=======================================================================
+short mbl_progress_composite::version_no() const
+{
+  return 1;
+}
+
+
+//=======================================================================
+vcl_string mbl_progress_composite::is_a() const
+{
+  return vcl_string("mbl_progress_composite");
+}
+
+
+// =================================================
+void mbl_progress_composite::on_set_estimated_iterations(const vcl_string &identifier,int total_iterations)
+{
+  for (int i=0;i<progress_objects_.size();++i)
+  {
+    progress_objects_[i]->set_estimated_iterations(identifier,total_iterations,display_text(identifier));
+  }
+}
+
+
+// =================================================
+void mbl_progress_composite::on_set_progress(const vcl_string &identifier, int progress)
+{
+  for (int i=0;i<progress_objects_.size();++i)
+  {
+    progress_objects_[i]->set_progress(identifier,progress);
+  }
+}
+
+
+// =================================================
+void mbl_progress_composite::on_end_progress(const vcl_string &identifier)
+{
+  
+  for (int i=0;i<progress_objects_.size();++i)
+  {
+    progress_objects_[i]->end_progress(identifier);
+  }
+}
---
+++ contrib/mul/mbl/mbl_progress_composite.h
@@ -0,0 +1,88 @@
+#ifndef mbl_progress_composite_h_
+#define mbl_progress_composite_h_
+#ifdef __GNUC__
+#pragma interface
+#endif
+
+
+//:
+// \file
+// \brief  A composite  of progress objects.
+// \author Graham Vincent	
+// \date 	 25 Feb 2005
+
+
+#include <vcl_string.h>
+#include <vcl_vector.h>
+#include <vsl/vsl_binary_io.h>
+#include <mbl/mbl_progress.h>
+
+
+//=======================================================================
+//:  A composite  of progress objects.
+//
+// Individual progress objects are attached to an instance of 
+// mbl_progress_composite which is passed to an algorithm. 
+// The mbl_progress_composite will inform all attached progress objects 
+// if and when it is informed of any progress.
+class mbl_progress_composite : public mbl_progress
+{  
+public:
+  
+  //: Constructor
+  mbl_progress_composite();
+  
+
+  //: Destructor
+  virtual ~mbl_progress_composite();
+  
+
+  //: Adds a progress object to the composite. 
+  //  These are assumed to exist for the lifetime of this object.
+  void add_progress(mbl_progress *progress);
+  
+
+  //: Version number for I/O 
+  short version_no() const;
+  
+
+  //: Name of the class
+  virtual vcl_string is_a() const;
+  
+
+  //: Print class to os
+  virtual void print_summary(vcl_ostream& os) const {};
+  
+  
+protected:
+  
+  //: Called when set_estimate_iterations() is called for a given identifier.
+  //  \param identifier The operation being monitored. 
+  virtual void on_set_estimated_iterations(const vcl_string &identifier,
+                                           const int total_iterations);
+  
+
+  //: Called when set_progress() is called for a given identifier.
+  //  \param identifier The operation being monitored. 
+  //  \param progress The new progress status.
+  virtual void on_set_progress(const vcl_string &identifier, 
+                               const int progress);
+  
+
+  //: Called when end_progress() is called for a given identifier.
+  //  \param identifier The operation being monitored. 
+  virtual void on_end_progress(const vcl_string &identifier);
+  
+  
+private:
+  
+  //: Stores estimated iterations for each identifier
+  vcl_vector<mbl_progress*> progress_objects_;
+  
+};
+
+
+
+//=======================================================================
+
+#endif
