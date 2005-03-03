f9a38c6e3bcaead7511d58773b7a59cd669c5f94
Gehua Yang <norwegian_wood@users.sourceforge.net>
2005-03-02 19:36:41 -0500

    compute_matches() has no particular reason to be const. Hence, remove all the const modifier. In fact, I shall implement one matcher with cache, which needs the compute_matches() to be non-const

--- contrib/rpl/rgrl/rgrl_matcher.cxx
+++ contrib/rpl/rgrl/rgrl_matcher.cxx
@@ -12,7 +12,7 @@ rgrl_matcher::
 compute_matches( rgrl_feature_set const&    from_features,
                  rgrl_feature_set const&    to_features,
                  rgrl_view const&           current_view,
-                 rgrl_scale const&          current_scale ) const
+                 rgrl_scale const&          current_scale )
 {
   return compute_matches( from_features, to_features, current_view,
                           *current_view.xform_estimate(), current_scale );
@@ -24,7 +24,7 @@ compute_matches( rgrl_feature_set const&    from_features,
                  rgrl_feature_set const&    to_features,
                  rgrl_transformation const& current_xform,
                  rgrl_mask_box const&       region,
-                 rgrl_scale const&          current_scale ) const
+                 rgrl_scale const&          current_scale )
 {
   rgrl_view view( region, region, region, region, 0, 0, 0);
 
--- contrib/rpl/rgrl/rgrl_matcher.h
+++ contrib/rpl/rgrl/rgrl_matcher.h
@@ -31,7 +31,7 @@ class rgrl_matcher
                    rgrl_feature_set const&    to_features,
                    rgrl_view const&           current_view,
                    rgrl_transformation const& current_xform,
-                   rgrl_scale const&          current_scale ) const = 0;
+                   rgrl_scale const&          current_scale ) = 0;
 
   //: Build the matches with a view (new interface)
   //
@@ -40,7 +40,7 @@ class rgrl_matcher
   compute_matches( rgrl_feature_set const&    from_features,
                    rgrl_feature_set const&    to_features,
                    rgrl_view const&           current_view,
-                   rgrl_scale const&          current_scale ) const;
+                   rgrl_scale const&          current_scale );
 
   //: Build the matches without a view
   //
@@ -49,7 +49,7 @@ class rgrl_matcher
                    rgrl_feature_set const&    to_features,
                    rgrl_transformation const& current_xform,
                    rgrl_mask_box const&       region,
-                   rgrl_scale const&          current_scale ) const;
+                   rgrl_scale const&          current_scale );
 
   // Defines type-related functions
   rgrl_type_macro( rgrl_matcher, rgrl_object );
