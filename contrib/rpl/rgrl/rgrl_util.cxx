6ffb7b4963efa50c7d47bb030c7632de8cd5b81f
Gehua Yang <norwegian_wood@users.sourceforge.net>
2005-03-06 12:51:07 -0500

    istringstream has different behavior on Windows or GCC when reaching the end of input string, is.eof() in GCC 3.4 will still return false. The fix is to use if( is ) instead of if( !is.eof() ). Windows doesn't have this problem. Don't know which is the C++ standard.

--- contrib/rpl/rgrl/rgrl_util.cxx
+++ contrib/rpl/rgrl/rgrl_util.cxx
@@ -705,7 +705,7 @@ rgrl_util_skip_empty_lines( vcl_istream& is )
     vcl_getline( is, str );
 
     non_empty_pos = str.find_first_not_of( white_chars );
-  } while ( !is.eof() && (str.empty() || non_empty_pos < 0 ||
+  } while ( is && !is.eof() && (str.empty() || non_empty_pos < 0 ||
             (str[non_empty_pos]=='#') ) );
 
   // back to the beginning of non-empty line
