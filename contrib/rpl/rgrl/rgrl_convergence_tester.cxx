3350c7c1a09249b4c39f1181f4f84d44b7fce98b
Gehua Yang <norwegian_wood@users.sourceforge.net>
2005-03-10 21:59:43 -0500

    1. add a function for access relative threshold 2. add a virtual function for verification step

--- contrib/rpl/rgrl/rgrl_convergence_tester.cxx
+++ contrib/rpl/rgrl/rgrl_convergence_tester.cxx
@@ -62,20 +62,20 @@ compute_status( rgrl_converge_status_sptr               prev_status,
 
 rgrl_converge_status_sptr 
 rgrl_convergence_tester::
-verify( rgrl_view           const& current_view,
-        rgrl_match_set_sptr const& current_match_set,
-        rgrl_scale_sptr     const& current_scale )const
+verify( rgrl_transformation_sptr    const& xform_estimate,
+        rgrl_match_set_sptr         const& current_match_set,
+        rgrl_scale_sptr             const& current_scale )const
 {
   rgrl_set_of<rgrl_match_set_sptr> match_sets;
   match_sets.push_back( current_match_set );
   rgrl_set_of<rgrl_scale_sptr> scales;
   scales.push_back( current_scale );
-  return this->verify( current_view, match_sets, scales );
+  return this->verify( xform_estimate, match_sets, scales );
 }
 
 rgrl_converge_status_sptr 
 rgrl_convergence_tester::
-verify( rgrl_view                        const& current_view,
+verify( rgrl_transformation_sptr         const& xform_estimate,
         rgrl_set_of<rgrl_match_set_sptr> const& current_match_sets,
         rgrl_set_of<rgrl_scale_sptr>     const& current_scales )const
 {
--- contrib/rpl/rgrl/rgrl_convergence_tester.h
+++ contrib/rpl/rgrl/rgrl_convergence_tester.h
@@ -81,18 +81,21 @@ class rgrl_convergence_tester
   //: verify the final alignment
   virtual
   rgrl_converge_status_sptr 
-  verify( rgrl_view                        const& current_view,
+  verify( rgrl_transformation_sptr         const& xform_estimate,
           rgrl_set_of<rgrl_match_set_sptr> const& current_match_sets,
           rgrl_set_of<rgrl_scale_sptr>     const& current_scales )const;
 
   rgrl_converge_status_sptr 
-  verify( rgrl_view           const& current_view,
-          rgrl_match_set_sptr const& current_match_sets,
-          rgrl_scale_sptr     const& current_scales )const;
+  verify( rgrl_transformation_sptr    const& xform_estimate,
+          rgrl_match_set_sptr         const& current_match_sets,
+          rgrl_scale_sptr             const& current_scales )const;
 
   //: set relative tolerance on convergence
   void set_rel_tol( double rel_tol ) 
   { rel_tol_thres_ = rel_tol; }
+  
+  double rel_tol() const 
+  { return rel_tol_thres_; }
   
   // Defines type-related functions
   rgrl_type_macro( rgrl_convergence_tester, rgrl_object );
