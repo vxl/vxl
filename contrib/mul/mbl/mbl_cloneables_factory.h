35b83ad0e96eb70fcf0932c376f9ce1bf84d4064
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-03-17 17:27:40 -0500

    added "typename" - this avoids compiler errors with gcc 3.4

--- contrib/mul/mbl/mbl_cloneables_factory.h
+++ contrib/mul/mbl/mbl_cloneables_factory.h
@@ -18,13 +18,13 @@
 // After templating this on a base class, and loading in
 // a bunch of concrete instantiations of the derived classes,
 // you can create any of the derived classes simply from
-// the class's name. 
-// 
+// the class's name.
+//
 // Example:
 // \code
 // mbl_cloneables_factory<vimt_image>::add(vimt_image_2d());
 // mbl_cloneables_factory<vimt_image>::add(vimt_image_3d());
-// 
+//
 // vimt_image *p = mbl_cloneables_factory<vimt_image>::get("vimt_image_2d()");
 // assert(dynamic_cast<vimt_image_2d>(p));
 // \endcode
@@ -33,16 +33,13 @@
 template <class BASE>
 class mbl_cloneables_factory
 {
-
-private:
+ private:
   typedef vcl_map<vcl_string, mbl_cloneable_ptr<BASE> > MAP;
 
   //: Singleton array of names, and association concrete instantiations of BASE.
   static vcl_auto_ptr<MAP> objects_;
 
-
-
-private:
+ private:
 
   //: Get singleton instance.
   static MAP &objects()
@@ -56,7 +53,7 @@ private:
     return *objects_;
   }
 
-public:
+ public:
 
   //: Add an object for later cloning by the factory.
   // Use the object's class name via the is_a() member.
@@ -72,21 +69,21 @@ public:
 
   //: Get a pointer to a new copy of the object identified by name.
   // The caller is responsible for the object's deletion.
-
+  //
   // There MUST be an object labelled name in the factory,
   // otherwise behaviour is undefined.
   static BASE* get(const vcl_string & name)
   {
-    MAP::iterator found = objects().lower_bound(name);
+    typename MAP::iterator found = objects().lower_bound(name);
 
     if (found == objects().end())
     {
       vcl_ostringstream ss;
-      MAP::const_iterator it = objects().begin();
+      typename MAP::const_iterator it = objects().begin();
       if (!objects().empty())
       {
         ss << it->first;
-        const MAP::const_iterator end = objects().end();
+        const typename MAP::const_iterator end = objects().end();
         while ( ++it != end)
           ss << it->first << ", ";
       }
@@ -95,15 +92,14 @@ public:
     }
     return found->second->clone();
   }
-
 };
 
 
 #define MBL_CLONEABLES_FACTORY_INSTANTIATE(T) \
 template class mbl_cloneables_factory< T >; \
-template <class BASE> \
-vcl_auto_ptr<mbl_cloneables_factory<BASE>::MAP> \
-  mbl_cloneables_factory<BASE>::objects_ =  \
-    vcl_auto_ptr<mbl_cloneables_factory<BASE>::MAP>(0);
+template <class BASE > \
+vcl_auto_ptr<mbl_cloneables_factory<BASE >::MAP > \
+  mbl_cloneables_factory<BASE >::objects_ =  \
+    vcl_auto_ptr<mbl_cloneables_factory<BASE >::MAP >(0)
 
 #endif  // mbl_cloneables_factory_h
