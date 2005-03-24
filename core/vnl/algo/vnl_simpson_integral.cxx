5e2a1fc7c6eb4bb02f95b32ba1321586c5e0c174
Kongbin Kang <kkang@users.sourceforge.net>
2005-03-24 14:14:41 -0500

    roll back to version 1.2

--- core/vnl/algo/vnl_simpson_integral.cxx
+++ core/vnl/algo/vnl_simpson_integral.cxx
@@ -1,15 +1,15 @@
 #include "vnl_simpson_integral.h"
 #include <vnl/algo/vnl_netlib.h>
 
-double vnl_simpson_integral::int_fnct_(float* x)
+double vnl_simpson_integral::int_fnct_(double* x)
 {
   return  pfnct_->f_(*x);
 }
 
-double vnl_simpson_integral::integral(vnl_integrant_fnct* f, float a, float b, int n)
+double vnl_simpson_integral::integral(vnl_integrant_fnct* f, double a, double b, int n)
 {
 
-  float res = 0;
+  double res = 0;
  
   //set the function
   pfnct_ = f;
