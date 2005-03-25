e48780ce0b7a752efa63df0467eab84a87a8c306
Kongbin Kang <kkang@users.sourceforge.net>
2005-03-25 16:58:00 -0500

    add new tests and rewrite adaptive simpson integral

--- core/vnl/algo/vnl_adaptsimpson_integral.h
+++ core/vnl/algo/vnl_adaptsimpson_integral.h
@@ -13,8 +13,16 @@ class vnl_adaptsimpson_integral : public vnl_definite_integral
   //: used to wrap the function class to an ordinary function.
   static double int_fnct_(double* x);
 
+ protected:
+
+  //: maximum recursion deepth
+  int deepth_;
+
+  //: real computation
+  double  adaptivesimpson(double(*f)(double*), double a, double b, double eps, int level, int level_max);
+  
  public:
-  vnl_adaptsimpson_integral() {}
+  vnl_adaptsimpson_integral(int deepth = 32) : deepth_(deepth) {}
 
   //: a and b are integral limits respectively.
   // n is the number of intervals used in integral.
