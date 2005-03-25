d67168cbe3db6071b38e4139d784bf6a8e7ee611
Kongbin Kang <kkang@users.sourceforge.net>
2005-03-25 16:56:05 -0500

    *** empty log message ***

--- core/vnl/algo/vnl_adaptsimpson_integral.cxx
+++ core/vnl/algo/vnl_adaptsimpson_integral.cxx
@@ -1,5 +1,5 @@
 #include "vnl_adaptsimpson_integral.h"
-#include <vnl/algo/vnl_netlib.h>
+#include <vcl_iostream.h>
 
 double vnl_adaptsimpson_integral::int_fnct_(double* x)
 {
@@ -11,17 +11,47 @@ double vnl_adaptsimpson_integral::integral(vnl_integrant_fnct* f, double a,
 {
 
   double res = 0;
-  double srmat[1111], errbnd;
 
-  int m;
-  int stat;
- 
   //set the function
   pfnct_ = f;
 
   double tol = acury;
   
-  adaptquad_(&vnl_adaptsimpson_integral::int_fnct_, &a, &b, &tol, srmat, &res, &errbnd, &m, &stat); 
+  res = adaptivesimpson(&vnl_adaptsimpson_integral::int_fnct_, a, b, tol, 0, deepth_); 
 
   return res;
 }
+
+double vnl_adaptsimpson_integral::adaptivesimpson(double(*f)(double*), 
+          double a, double b, double eps, int level, int level_max)
+{
+  int i, j, k, kmax=1;
+  double c, d, e, h, result;
+  double one_simpson, two_simpson;
+  double left_simpson, right_simpson;
+ 
+  h = b-a;
+  c = 0.5*(a+b);
+  one_simpson = h*(f(&a)+4.0*f(&c)+f(&b))/6.0;
+  d = 0.5*(a+c);
+  e = 0.5*(c+b);
+  two_simpson = h*(f(&a)+4.0*f(&d)+2.0*f(&c)+4.0*f(&e)+f(&b))/12.0;
+  /* Check for level */
+  if(level+1 >= level_max) {  
+    result = two_simpson;
+    vcl_cerr<< "Maximum level reached\n"; 
+  }
+  else{
+    /* Check for desired accuracy */
+    if(fabs(two_simpson-one_simpson) < 15.0*eps)
+      result = two_simpson + (two_simpson-one_simpson)/15.0;
+    /* Divide further */
+    else {
+      left_simpson = adaptivesimpson(f,a,c,eps/2.0,level+1,level_max);   
+      right_simpson = adaptivesimpson(f,c,b,eps/2.0,level+1,level_max);   
+      result =  left_simpson + right_simpson;
+    }
+  }
+  return(result);
+}
+
