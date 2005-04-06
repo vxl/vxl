9218a9283d91fa905703c1414d15c4c608688732
Joseph L. Mundy <mundyj@users.sourceforge.net>
2005-04-06 07:24:51 -0400

    Added unsigned template instantiations

---
+++ core/vgl/Templates/vgl_homg_point_2d+unsigned-.cxx
@@ -0,0 +1,3 @@
+// Instantiation of vgl_homg_point_2d<unsigned>
+#include <vgl/vgl_homg_point_2d.txx>
+VGL_HOMG_POINT_2D_INSTANTIATE(unsigned);
---
+++ core/vgl/Templates/vgl_point_2d+unsigned-.cxx
@@ -0,0 +1,3 @@
+// Instantiation of vgl_point_2d<unsigned>
+#include <vgl/vgl_point_2d.txx>
+VGL_POINT_2D_INSTANTIATE(unsigned);
