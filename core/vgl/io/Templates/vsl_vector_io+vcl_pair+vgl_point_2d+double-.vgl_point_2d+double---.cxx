9235cfbe78a2d862fcf98fbce4b8c0558250ace4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-04-09 15:36:29 -0400

    renamed from vsl_vector_io+vcl_pair+vgl_point_2d+double---.cxx to vsl_vector_io+vcl_pair+vgl_point_2d+double-.vgl_point_2d+double---.cxx

--- core/vgl/io/Templates/vsl_vector_io+vcl_pair+vgl_point_2d+double-.vgl_point_2d+double---.cxx
+++ core/vgl/io/Templates/vsl_vector_io+vcl_pair+vgl_point_2d+double-.vgl_point_2d+double---.cxx
@@ -1,5 +1,5 @@
-#include <vsl/vsl_vector_io.txx>
-#include <vsl/vsl_pair_io.txx>
 #include <vgl/io/vgl_io_point_2d.h>
+#include <vsl/vsl_pair_io.h>
+#include <vsl/vsl_vector_io.txx>
 typedef vcl_pair<vgl_point_2d<double>,vgl_point_2d<double> > pair_pp;
 VSL_VECTOR_IO_INSTANTIATE(pair_pp);
