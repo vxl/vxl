0660b90f9032b8f2c85923671fa431dea1c5e9fe
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-04-30 11:11:38 -0400

    avoid compiler warning (comparison signed/unsigned)

--- contrib/brl/bseg/brip/brip_roi.cxx
+++ contrib/brl/bseg/brip/brip_roi.cxx
@@ -1,3 +1,5 @@
+//:
+// \file
 #include <vsol/vsol_box_2d.h>
 #include <brip/brip_roi.h>
 
@@ -20,30 +22,26 @@ brip_roi::brip_roi(brip_roi const& roi, float delta)
   : vbl_ref_count(), n_image_cols_(roi.n_image_cols_),
     n_image_rows_(roi.n_image_rows_), regions_(roi.regions_)
 {
+  if (delta < 0) delta *= -1.0f; // to guarantee dxmin <= dxmax.
   for (vcl_vector<vsol_box_2d_sptr>::iterator rit = regions_.begin();
        rit != regions_.end(); rit++)
-    {
-      int xmin = (int)(*rit)->get_min_x();
-      int ymin = (int)(*rit)->get_min_y();
-      int xmax = (int)(*rit)->get_max_x();
-      int ymax = (int)(*rit)->get_max_y();
-      int dxmin = (int)(xmin-delta), dymin = (int)(ymin - delta);
-      int dxmax = (int)(xmax+delta), dymax = (int)(ymax + delta);
-      if(dxmin <0 || dxmin >= n_image_cols_)
-        continue;
-      if(dymin <0 || dymin >= n_image_rows_)
-        continue;
-      if(dxmax <0 || dxmax >= n_image_cols_)
-        continue;
-      if(dymax <0 || dymax >= n_image_rows_)
-        continue;
-      vsol_box_2d_sptr dbox = new vsol_box_2d();
-      dbox->add_point(dxmin, dymin);
-      dbox->add_point(dxmax, dymin);
-      dbox->add_point(dxmax, dymax);
-      dbox->add_point(dxmin, dymax);
-      (*rit)=dbox;
-    }
+  {
+    int xmin = (int)(*rit)->get_min_x();
+    int ymin = (int)(*rit)->get_min_y();
+    int xmax = (int)(*rit)->get_max_x();
+    int ymax = (int)(*rit)->get_max_y();
+    unsigned int dxmin = (unsigned int)(xmin-delta), // overflows when negative,
+                 dymin = (unsigned int)(ymin-delta), // but this is no problem
+                 dxmax = (unsigned int)(xmax+delta), // since the test below
+                 dymax = (unsigned int)(ymax+delta); // will skip these cases:
+    if (dxmin >= n_image_cols_ || dymin >= n_image_rows_ ||
+        dxmax >= n_image_cols_ || dymax >= n_image_rows_)
+      continue;
+    vsol_box_2d_sptr dbox = new vsol_box_2d();
+    dbox->add_point(dxmin, dymin);
+    dbox->add_point(dxmax, dymax);
+    (*rit)=dbox;
+  }
 }
 
 vsol_box_2d_sptr brip_roi::clip_to_image_bounds(vsol_box_2d_sptr box)
@@ -53,29 +51,27 @@ vsol_box_2d_sptr brip_roi::clip_to_image_bounds(vsol_box_2d_sptr box)
   int x0 = (int)box->get_min_x();
   int y0 = (int)box->get_min_y();
   int xm = (int)box->get_max_x();
-  int ym= (int)box->get_max_y();
+  int ym = (int)box->get_max_y();
   //clip to image bounds
   if (x0 < 0)
     x0 = 0;
+  if ((unsigned int)x0 >= n_image_cols_)
+    x0 = n_image_cols_-1;
   if (y0 < 0)
     y0 = 0;
-  if (x0 > n_image_cols_-1)
-    x0 = n_image_cols_-1;
-  if (y0 > n_image_rows_-1)
+  if ((unsigned int)y0 >= n_image_rows_)
     y0 = n_image_rows_-1;
   if (xm < 0)
     xm = 0;
-  if (xm > n_image_cols_-1)
+  if ((unsigned int)xm >= n_image_cols_)
     xm = n_image_cols_-1;
   if (ym < 0)
     ym = 0;
-  if (ym > n_image_rows_-1)
+  if ((unsigned int)ym >= n_image_rows_)
     ym = n_image_rows_-1;
   vsol_box_2d_sptr cbox = new vsol_box_2d();
   cbox->add_point(x0, y0);
-  cbox->add_point(xm, y0);
   cbox->add_point(xm, ym);
-  cbox->add_point(x0, ym);
   return cbox;
 }
 
@@ -129,17 +125,15 @@ void brip_roi::add_region(vsol_box_2d_sptr const & box)
 }
 
 //return true if there are no regions or if they are all empty
-bool brip_roi::empty()
+bool brip_roi::empty() const
 {
-  if (!regions_.size())
-    return true;
+  return n_regions() == 0;
 #if 0//later
   for (vcl_vector<vgl_box_2d<int> >::iterator rit = regions_.begin();
        rit != regions_.end(); rit++)
     if (!(*rit).is_empty())
       return false;
 #endif
-  return false;
 }
 
 void brip_roi::clip_to_image_bounds()
@@ -151,84 +145,60 @@ void brip_roi::clip_to_image_bounds()
   regions_ = temp;
 }
 
-unsigned brip_roi::cmin(const unsigned i) const
+int brip_roi::cmin(const unsigned i) const
 {
-  if (i>=int(regions_.size()))
-    return 0;
-  else
-    return (int)regions_[i]->get_min_x();
+  return i >= regions_.size() ? 0 : (int)regions_[i]->get_min_x();
 }
 
-unsigned brip_roi::cmax(const unsigned i) const
+int brip_roi::cmax(const unsigned i) const
 {
-  if (i>=int(regions_.size()))
-    return 0;
-  else
-    return (int)regions_[i]->get_max_x();
+  return i >= regions_.size() ? 0 : (int)regions_[i]->get_max_x();
 }
 
-unsigned brip_roi::rmin(const unsigned i) const
+int brip_roi::rmin(const unsigned i) const
 {
-  if (i>=int(regions_.size()))
-    return 0;
-  else
-    return (unsigned)regions_[i]->get_min_y();
+  return i >= regions_.size() ? 0 : (int)regions_[i]->get_min_y();
 }
 
-unsigned brip_roi::rmax(const unsigned i) const
+int brip_roi::rmax(const unsigned i) const
 {
-  if (i<0||i>=int(regions_.size()))
-    return 0;
-  else
-    return (int)regions_[i]->get_max_y();
+  return i >= regions_.size() ? 0 : (int)regions_[i]->get_max_y();
 }
 
 unsigned brip_roi::csize(const unsigned i) const
 {
   int temp = cmax(i)-cmin(i) + 1;
-  if(temp<0)
-    return 0;
-  return (unsigned)temp;
+  return temp<0 ? 0 : (unsigned)temp;
 }
 
-unsigned brip_roi::rsize(const unsigned i) const 
+unsigned brip_roi::rsize(const unsigned i) const
 {
   int temp = rmax(i)-rmin(i) + 1;
-  if(temp<0)
-    return 0;
-  return (unsigned)temp;
+  return temp<0 ? 0 : (unsigned)temp;
 }
 
 unsigned brip_roi::ic(int local_col, unsigned i) const
 {
   int temp = cmin(i) + local_col;
-  if(temp<0)
-    temp = 0;
-  return (unsigned)temp;
+  return temp<0 ? 0 : (unsigned)temp;
 }
 
 unsigned brip_roi::ir(int local_row, unsigned i) const
 {
   int temp = rmin(i) + local_row;
-  if(temp<0)
-    temp = 0;
-  return (unsigned)temp;
+  return temp<0 ? 0 : (unsigned)temp;
 }
 
 unsigned brip_roi::lc(unsigned global_col, unsigned i) const
 {
- int temp = global_col - cmin(i); 
-  if(temp<0)
-    temp = 0;
-  return (unsigned)temp;
+  int temp = global_col - cmin(i);
+  return temp<0 ? 0 : (unsigned)temp;
 }
 
 unsigned brip_roi::lr(unsigned global_row, unsigned i) const
 {
- int temp = global_row - rmin(i); 
-  if(temp<0)
-    temp = 0;
-  return (unsigned)temp;
+ int temp = global_row - rmin(i);
+  return temp<0 ? 0 : (unsigned)temp;
 }
 
 bool brip_roi::remove_region(unsigned i)
