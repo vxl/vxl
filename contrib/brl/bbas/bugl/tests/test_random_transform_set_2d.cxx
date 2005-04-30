7f5f75612ac1219b82b1a3c7fa8763d69bab3f20
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-04-30 07:47:51 -0400

    TABs replaced by spaces

--- contrib/brl/bbas/bugl/tests/test_random_transform_set_2d.cxx
+++ contrib/brl/bbas/bugl/tests/test_random_transform_set_2d.cxx
@@ -3,9 +3,9 @@
 #include <vnl/vnl_math.h>
 #include <bugl/bugl_random_transform_set_2d.h>
 #include <vgl/vgl_homg_point_2d.h>
+
 void test_random_transform_set_2d()
 {
-  
   vcl_cout << "=====Testing random transform set =======\n";
 
   float dx = 1.0, dy = 2.0;
@@ -15,28 +15,28 @@ void test_random_transform_set_2d()
   float sumx = 0, sumy = 0;
   float min_x = 0, max_x = 0;
   float min_y = 0, max_y = 0;
-  for(unsigned i = 0; i<n; ++i)
-    {
-     // vcl_cout << "\n T[" << i << "]\n";
-      //vcl_cout << trans_set[i];
-	  vgl_homg_point_2d<float> p(0.0f, 0.0f), pt;
-	  pt = trans_set[i](p);
-	  sumx += pt.x();  sumy += pt.y();
-	  if(pt.x()<min_x) min_x = pt.x();
-	   if(pt.y()<min_y) min_y = pt.y();
-	    if(pt.x()>max_x) max_x = pt.x();
-	   if(pt.y()>max_y) max_y = pt.y();
-    }
-	sumx /= n; sumy /= n;
-	vcl_cout << "tavg (" << sumx << ' ' << sumy << ")\n";
-	vcl_cout << "X bounds (" << min_x << ' ' << max_x << ")\n";
-	vcl_cout << "Y bounds (" << min_y << ' ' << max_y << ")\n";
+  for (unsigned i = 0; i<n; ++i)
+  {
+    // vcl_cout << "\n T[" << i << "]\n"
+    //          << trans_set[i];
+    vgl_homg_point_2d<float> p(0.0f, 0.0f), pt;
+    pt = trans_set[i](p);
+    sumx += pt.x();  sumy += pt.y();
+    if (pt.x()<min_x) min_x = pt.x();
+    if (pt.y()<min_y) min_y = pt.y();
+    if (pt.x()>max_x) max_x = pt.x();
+    if (pt.y()>max_y) max_y = pt.y();
+  }
+  sumx /= n; sumy /= n;
+  vcl_cout << "tavg (" << sumx << ' ' << sumy << ")\n"
+           << "X bounds (" << min_x << ' ' << max_x << ")\n"
+           << "Y bounds (" << min_y << ' ' << max_y << ")\n";
   TEST_NEAR("test average transform ", vcl_fabs(sumx)+vcl_fabs(sumy), 0, 0.2);
   TEST_NEAR("test X bounds " , max_x - min_x , 2.0, 0.2);
   TEST_NEAR("test Y bounds " , max_y - min_y , 4.0, 0.2);
   //==== test rotation
   vcl_cout << "\n\n====================Testing random set of rotations ============================\n";
-  float dtheta = 3.14159f/2.0f;
+  float dtheta = float(vnl_math::pi/2);
   n = 1000;
   vcl_vector<vgl_h_matrix_2d<float> > rtrans_set;
   bugl_random_transform_set_2d<float>::equiform_uniform(n, rtrans_set, 0, 0,
@@ -44,20 +44,20 @@ void test_random_transform_set_2d()
   sumx = 0; sumy = 0;
   min_x = 0; max_x = 0;
   min_y = 0; max_y = 0;
-  for(unsigned i = 0; i<n; ++i)
-    {
-      vgl_homg_point_2d<float> p(1.0, 0.0f), pt;
-      pt = rtrans_set[i](p);
-      sumx += pt.x();  sumy += pt.y();
-      if(pt.x()<min_x) min_x = pt.x();
-      if(pt.y()<min_y) min_y = pt.y();
-	    if(pt.x()>max_x) max_x = pt.x();
-      if(pt.y()>max_y) max_y = pt.y();
-    }
-	sumx /= n; sumy /= n;
-	vcl_cout << "tavg (" << sumx << ' ' << sumy << ")\n";
-	vcl_cout << "X bounds (" << min_x << ' ' << max_x << ")\n";
-	vcl_cout << "Y bounds (" << min_y << ' ' << max_y << ")\n";
+  for (unsigned i = 0; i<n; ++i)
+  {
+    vgl_homg_point_2d<float> p(1.0, 0.0f), pt;
+    pt = rtrans_set[i](p);
+    sumx += pt.x();  sumy += pt.y();
+    if (pt.x()<min_x) min_x = pt.x();
+    if (pt.y()<min_y) min_y = pt.y();
+    if (pt.x()>max_x) max_x = pt.x();
+    if (pt.y()>max_y) max_y = pt.y();
+  }
+  sumx /= n; sumy /= n;
+  vcl_cout << "tavg (" << sumx << ' ' << sumy << ")\n"
+           << "X bounds (" << min_x << ' ' << max_x << ")\n"
+           << "Y bounds (" << min_y << ' ' << max_y << ")\n";
   TEST_NEAR("test average transform ", vcl_fabs(sumx)+vcl_fabs(sumy), 0.5, 0.2);
   TEST_NEAR("test X bounds " , max_x - min_x , 1.0, 0.2);
   TEST_NEAR("test Y bounds " , max_y - min_y , 2.0, 0.2);
@@ -70,20 +70,20 @@ void test_random_transform_set_2d()
   sumx = 0; sumy = 0;
   min_x = 1.0; max_x = 0;
   min_y = 2.0; max_y = 0;
-  for(unsigned i = 0; i<n; ++i)
-    {
-	 vgl_homg_point_2d<float> p(1.0, 2.0f), pt;
-      pt = strans_set[i](p);
-      sumx += pt.x();  sumy += pt.y();
-      if(pt.x()<min_x) min_x = pt.x();
-      if(pt.y()<min_y) min_y = pt.y();
-	    if(pt.x()>max_x) max_x = pt.x();
-      if(pt.y()>max_y) max_y = pt.y();
-    }
-	sumx /= n; sumy /= n;
-	vcl_cout << "tavg (" << sumx << ' ' << sumy << ")\n";
-	vcl_cout << "X bounds (" << min_x << ' ' << max_x << ")\n";
-	vcl_cout << "Y bounds (" << min_y << ' ' << max_y << ")\n";
+  for (unsigned i = 0; i<n; ++i)
+  {
+    vgl_homg_point_2d<float> p(1.0, 2.0f), pt;
+    pt = strans_set[i](p);
+    sumx += pt.x();  sumy += pt.y();
+    if (pt.x()<min_x) min_x = pt.x();
+    if (pt.y()<min_y) min_y = pt.y();
+    if (pt.x()>max_x) max_x = pt.x();
+    if (pt.y()>max_y) max_y = pt.y();
+  }
+  sumx /= n; sumy /= n;
+  vcl_cout << "tavg (" << sumx << ' ' << sumy << ")\n"
+           << "X bounds (" << min_x << ' ' << max_x << ")\n"
+           << "Y bounds (" << min_y << ' ' << max_y << ")\n";
   TEST_NEAR("test average transform ", vcl_fabs(sumx)+vcl_fabs(sumy), 3.0, 0.2);
   TEST_NEAR("test X bounds " , max_x/min_x , 3.0, 0.2);
   TEST_NEAR("test Y bounds " , max_y/min_y , 3.0, 0.2);
