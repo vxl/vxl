f7210491c104d76f89fae1773d6167ae0d987ada
Tim Cootes <timcootes@users.sourceforge.net>
2005-05-03 09:44:33 -0400

    Added missing templates

---
+++ contrib/mul/vil3d/algo/Templates/vil3d_gauss_reduce+byte-.cxx
@@ -0,0 +1,3 @@
+#include <vil3d/algo/vil3d_gauss_reduce.txx>
+#include <vxl_config.h>
+VIL3D_GAUSS_REDUCE_INSTANTIATE(vxl_byte);
---
+++ contrib/mul/vil3d/algo/Templates/vil3d_gauss_reduce+float-.cxx
@@ -0,0 +1,2 @@
+#include <vil3d/algo/vil3d_gauss_reduce.txx>
+VIL3D_GAUSS_REDUCE_INSTANTIATE(float);
