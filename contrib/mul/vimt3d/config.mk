3ae1a89dcaace7991864e03f88ad8f41a194d4cf
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-05-14 15:55:49 -0400

    updated w.r.t. CMakeLists.txt

--- contrib/mul/vimt3d/config.mk
+++ contrib/mul/vimt3d/config.mk
@@ -1,4 +1,5 @@
 IULIBS += -lvimt3d
+include ${IUEROOT}/contrib/mul/vil3d/algo/config.mk
 include ${IUEROOT}/contrib/mul/vil3d/config.mk
 include ${IUEROOT}/contrib/mul/vimt/config.mk
 include ${IUEROOT}/core/vnl/config.mk
