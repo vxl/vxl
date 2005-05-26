d448998015972c3df9844d2653515ba9d67f354b
Gehua Yang <norwegian_wood@users.sourceforge.net>
2005-05-26 12:34:28 -0400

    explicitly disable the warning about applying unary minus operator to unsigned types

--- core/vgl/Templates/vgl_vector_2d+uint-.cxx
+++ core/vgl/Templates/vgl_vector_2d+uint-.cxx
@@ -1,3 +1,10 @@
+// Disable warning
+#include <vcl_compiler.h>
+#ifdef VCL_VC_DOTNET
+// 4146: unary minus operator applied to unsigned type, result still unsigned
+# pragma warning(disable:4146)
+#endif //VCL_VC_DOTNET
+
 // Instantiation of vgl_vector_2d<unsigned int>
 #include <vgl/vgl_vector_2d.txx>
 VGL_VECTOR_2D_INSTANTIATE(unsigned int);
