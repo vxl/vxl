fe68ef35834ac30dd378d3a1357b90f57a372cf0
Gehua Yang <norwegian_wood@users.sourceforge.net>
2005-05-26 12:53:35 -0400

    explicitly disable the warning about applying unary minus operator to unsigned types

--- core/vgl/Templates/vgl_homg_point_2d+unsigned-.cxx
+++ core/vgl/Templates/vgl_homg_point_2d+unsigned-.cxx
@@ -1,3 +1,10 @@
+// Disable warning
+#include <vcl_compiler.h>
+#ifdef VCL_VC_DOTNET
+// 4146: unary minus operator applied to unsigned type, result still unsigned
+# pragma warning(disable:4146)
+#endif //VCL_VC_DOTNET
+
 // Instantiation of vgl_homg_point_2d<unsigned>
 #include <vgl/vgl_homg_point_2d.txx>
 VGL_HOMG_POINT_2D_INSTANTIATE(unsigned);
