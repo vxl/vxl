81282fe46800bc48f7a5a9901c4674bb24becc04
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-06-19 07:36:55 -0400

    added precompiler switch for ffmpeg

--- core/vidl/tests/test_include.cxx
+++ core/vidl/tests/test_include.cxx
@@ -5,7 +5,9 @@
 #include <vidl/vidl_clip_sptr.h>
 #include <vidl/vidl_codec.h>
 #include <vidl/vidl_codec_sptr.h>
+#ifdef HAS_FFMPEG
 #include <vidl/vidl_ffmpeg_codec.h>
+#endif
 #include <vidl/vidl_file_sequence.h>
 #include <vidl/vidl_frame.h>
 #include <vidl/vidl_frame_sptr.h>
@@ -15,9 +17,11 @@
 #include <vidl/vidl_io.h>
 #include <vidl/vidl_movie.h>
 #include <vidl/vidl_movie_sptr.h>
+#ifdef HAS_MPEG2
 #include <vidl/vidl_mpegcodec.h>
 #include <vidl/vidl_mpegcodec_helper.h>
 #include <vidl/vidl_mpegcodec_sptr.h>
+#endif
 #include <vidl/vidl_vob_frame_index.h>
 #include <vidl/vidl_yuv_2_rgb.h>
 
