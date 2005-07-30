22096bf676ae59f8eed8271b931aa3ec6e12e6d7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-07-30 17:29:50 -0400

    replaced "test_algo_threshold" by "test_algo_checker_board"

--- core/vil/algo/tests/test_algo_checker_board.cxx
+++ core/vil/algo/tests/test_algo_checker_board.cxx
@@ -1,4 +1,4 @@
-// This is core/vil/algo/tests/test_algo_threshold.cxx
+// This is core/vil/algo/tests/test_algo_checker_board.cxx
 #include <testlib/testlib_test.h>
 #include <vcl_iostream.h>
 #include <vxl_config.h> // for vxl_byte
@@ -7,27 +7,27 @@
 
 static void test_algo_checker_board()
 {
-  // create two image views 
+  // create two image views
   // One has rectangle  (40, 40) - (140, 80)
   // One has rectangle  (40, 40) - (80, 140)
-  
+
   vil_image_view<vxl_byte> a(200, 200), b(200,200);
   a.fill( vxl_byte(0) );
   b.fill( vxl_byte(0) );
-  
-  for( unsigned i=40; i<140; ++i )
-    for( unsigned j=40; j<80; ++j ) {
+
+  for ( unsigned i=40; i<140; ++i )
+    for ( unsigned j=40; j<80; ++j ) {
       a(i, j) = 100;
       b(j, i) = 200;
     }
-    
+
   // setup
   vil_image_view<vxl_byte> des;
   vil_checker_board_params params( 10, 10 );
   params.not_use_color( 0 );
-  
+
   vil_checker_board( des, a, b, params );
-  
+
   TEST("Black area", des(35,35), vxl_byte(0) );
   TEST("Black area", des(105,105), vxl_byte(0) );
   TEST("First Block", des(45,45), vxl_byte(100) );
