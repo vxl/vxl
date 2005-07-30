4126efc5caafdd0a2f1a183d3756d0d3efaa7770
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-07-30 17:31:09 -0400

    avoid identifier names starting with underscore

--- contrib/brl/bbas/bsta/bsta_otsu_threshold.h
+++ contrib/brl/bbas/bsta/bsta_otsu_threshold.h
@@ -1,5 +1,5 @@
-#ifndef _bsta_otsu_threshold_h_
-#define _bsta_otsu_threshold_h_
+#ifndef bsta_otsu_threshold_h_
+#define bsta_otsu_threshold_h_
 //:
 // \file
 // \brief Implements Otsu's threshold method for 1D distribution and 2 classes
@@ -10,35 +10,35 @@
 //  an exhaustive linear scan over the histogram bin indices
 //
 // \verbatim
-//  Modifications  Templated and moved up to bsta - J.L. Mundy June 29, 2005
+//  Modifications
+//   Templated and moved up to bsta - J.L. Mundy June 29, 2005
 // \endverbatim
+
 #include<vcl_vector.h>
 #include<bsta/bsta_histogram.h>
 #include<vil/vil_image_view.h>
 
-template <class T> class bsta_otsu_threshold{
+template <class T> class bsta_otsu_threshold
+{
+ public:
+  // constructor
+  bsta_otsu_threshold(bsta_histogram<T> const& hist);
+  //: takes vector of data as input
+  bsta_otsu_threshold(vcl_vector<T> data, T low , T high);
+  // destructor
+  ~bsta_otsu_threshold();
 
-public:
-    //: constructor
-    bsta_otsu_threshold(bsta_histogram<T> const& hist);
-    //: takes vector of data as input
-    bsta_otsu_threshold(vcl_vector<T> data, T low , T high);
-    //: destructor
-    ~bsta_otsu_threshold();
+  unsigned bin_threshold();
 
-    unsigned bin_threshold();
-    
-    T threshold()
-      {return distribution_1d_.avg_bin_value(this->bin_threshold());}
+  T threshold() {return distribution_1d_.avg_bin_value(this->bin_threshold());}
 
-protected:
-    //: default constructor doesn't make sense
-    bsta_otsu_threshold() : distribution_1d_(bsta_histogram<T>(0,0)){}
+ protected:
+  //: default constructor doesn't make sense
+  bsta_otsu_threshold() : distribution_1d_(bsta_histogram<T>(0,0)){}
 
-    bsta_histogram<T> distribution_1d_;
+  bsta_histogram<T> distribution_1d_;
 };
 
 #define BSTA_OTSU_THRESHOLD_INSTANTIATE(T) extern "Please #include <bsta/bsta_otsu_threshold.txx>"
-
 
 #endif // bsta_otsu_threshold_h_
