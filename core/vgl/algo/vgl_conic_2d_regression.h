8af764f34717b4fd2265dc972404a191a3028af2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-07-30 17:31:49 -0400

    typo corrected

--- core/vgl/algo/vgl_conic_2d_regression.h
+++ core/vgl/algo/vgl_conic_2d_regression.h
@@ -7,56 +7,57 @@
 //:
 // \file
 // \brief Fits a conic to a set of points using linear regression
-// \author J.L. Mundy 
+// \author J.L. Mundy
 // \date June 17, 2005
 //
-//  Since conic fitting is rather ill-conditioned it is necessary to 
+//  Since conic fitting is rather ill-conditioned it is necessary to
 //  normalize the point coordinates so that they have equal standard
 //  deviations with respect to the center (mean) of the pointset.
 //  The points are transformed to have zero mean. The resulting conic
-//  is transformed back to the original frame for computing 
+//  is transformed back to the original frame for computing
 //  the Sampson approximation to fitting error.
 //
-//  The reqression uses the Bookstein algorithm which constrains the 
-//  conic norm, |ax^2 + bxy + cy^2 + dx + ey +f|^2, so that 
-//   a^2 + 0.5*b^2 + c^2 = 2
+//  The regression uses the Bookstein algorithm which constrains the
+//  conic norm, $ |ax^2 + bxy + cy^2 + dx + ey +f|^2 $, so that
+//   $ a^2 + 0.5*b^2 + c^2 = 2 $
 //  With this normalization, the resulting fit is invariant up to a similarity
-//  transform of the pointset.  The solution is formulated as an eigenvalue 
-//  problem as follows:  
-//       The scatter matrix S is decomposed as 
+//  transform of the pointset.  The solution is formulated as an eigenvalue
+//  problem as follows:
 //
-//                S11   S12
-//        S   =                   note S21 = S12^t
-//                S21   S22
+//      The scatter matrix S is decomposed as
+//              
+//      $ S   =  \begin{array}{cc}     S_{11} & S_{12}     \\
+//                                     S_{21} & S_{22}     \end{array}$
+//      note $S_{21} = S_{12}^t$
 //
-//      The conic coefficents are v1 = {a, b, c}^t and v2 = {d, e, f}^t
-//      The Bookstein constraint is expressed by the diagonal matrix 
+//      The conic coefficients are $v_1 = \{a,b,c\}^t$ and $v_2 = \{d,e,f\}^t$
+//      The Bookstein constraint is expressed by the diagonal matrix
 //               D = Diag{1, 0.5, 1, 0, 0, 0}
 //      The Lagrangian to be minimized is
+//      $ L =  v_1^t S_{11} v_1  + 2 v_2^t S_{21} v_1  + v_2^t S_{22} v_2
+//             - \lambda(v_1^t D v_1 -2) $.
 //
-//  L =  v1^t S11 v1  + 2 v2^t S21 v1  + v2^t S22 v2 - lambda(v1^t D v1 -2)
+//      Minimizing with respect to v2 gives
+//      $ dL/dv_2 = 2 S_{21} v_1 + 2 S_{22} v_2 = 0 $
 //
-//     Minimizing with respect to v2 gives,
+//      So,  $ v_2 = -S_{22}^-1 S_{21} v_1 $.
 //
-//      dL/dv2 = 2 S21 v1 + 2 S22 v2 = 0
+//      Substituting for v2 in L,
 //
-//    So,   v2 = -S22^-1 S21 v1
+// $ L = v_1^t ( S_{11} - S_{12} * S_{22}^-1 * S_{21}) v_1 - \lambda(v_1^t D v_1 -2)
+//     = v_1^t ( (S_{11} - S_{12} * S_{22}^-1 * S_{21}) - \lambda D ) v_1 - 2 \lambda $,
 //
-//    Substituting for v2 in L,
+// $ dL/dv_1 =  ( (S_{11} - S_{12} * S_{22}^-1 * S_{21}) - \lambda D ) v_1 = 0 $.
 //
-//   L = v1^t ( S11 - S12 * S22^-1 * S21) v1 - lambda(v1^t D v1 -2)
-//     = v1^t ( (S11 - S12 * S22^-1 * S21) - lambda D ) v1 - 2 lambda
-
-//   dL/dv1 =  ( (S11 - S12 * S22^-1 * S21) - lambda D ) v1 = 0
+//   So, $ \lambda v_1 = D^-1 (S_{11} - S_{12} * S_{22}^-1 * S_{21}) v_1 $.
 //
-//   So,  lambda v1 = D^-1 (S11 - S12 * S22^-1 * S21) v1
-//  
 //   This eigenvalue problem is solved using singular value decomposition.
 //
 // \verbatim
 //  Modifications
 //   none
 // \endverbatim
+
 #include <vcl_vector.h>
 #include <vcl_iostream.h>
 #include <vnl/vnl_matrix_fixed.h>
@@ -90,12 +91,11 @@ class vgl_conic_2d_regression
 
   void remove_point(vgl_point_2d<T> const& p);
 
-  //: get the estimated Euclidean error with respect to the 
-  //  fitted conic segment in the original frame
+  //: get the estimated Euclidean error with respect to the fitted conic segment in the original frame
   T get_rms_error_est(vgl_point_2d<T> const& p) const;
 
   //: get the current Euclidean fitting error in the original frame
-  T get_rms_sampson_error() const {return sampson_error_;} //temporarily
+  T get_rms_sampson_error() const { return sampson_error_; } //temporarily
 
   //: get the current algebraic fitting error in the normalized frame
   T get_rms_algebraic_error() const;
@@ -105,7 +105,7 @@ class vgl_conic_2d_regression
 
   // Data Access---------------------------------------------------------------
 
-  vgl_conic<T> conic() {return conic_;}
+  vgl_conic<T> conic() const { return conic_; }
 
   // Debug support
   void print_pointset(vcl_ostream& str = vcl_cout );
@@ -116,7 +116,7 @@ class vgl_conic_2d_regression
   void fill_scatter_matrix();
   void set_sampson_error(const double a, const double b,
                          const double c, const double d,
-                         const double e, const double f); 
+                         const double e, const double f);
 
   // Data Members--------------------------------------------------------------
 
@@ -133,14 +133,13 @@ class vgl_conic_2d_regression
   vcl_vector<double> partial_sums_;
 
   //: The fitting matrices
-    vnl_double_3x3 S11_, S12_, S22_;
+  vnl_double_3x3 S11_, S12_, S22_;
   vnl_double_3x3 Dinv_;
-
 
   //: The fitted conic in the un-normalized frame
   vgl_conic<T> conic_;
 
-  //: The algebraic fitting cost in the normalized framex
+  //: The algebraic fitting cost in the normalized frame
   T cost_;
 
   //: The Sampson approximation to Euclidean distance in the normalized frame
