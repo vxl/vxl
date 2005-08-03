fef80bed3fd86d29f0a7b639378028855aabbbe4
Kevin de Souza <kdesouza@users.sourceforge.net>
2005-08-03 05:54:09 -0400

    Corrected transform. Added overload which does not require external workspace images.

--- contrib/mul/vimt3d/vimt3d_gauss_reduce.h
+++ contrib/mul/vimt3d/vimt3d_gauss_reduce.h
@@ -13,8 +13,10 @@
 //: Smooth and subsample src_im to produce dest_im
 //  Applies filter in i,j and k directions, then samples every other pixel.
 //  Resulting image is (ni+1)/2 x (nj+1)/2 x (nk+1)/2. 
+//  Transform is modified by a scaling factor of 0.5.
 //  An image can be reduced in-place, by having src_im and dest_im pointing to the same image.
-//  Transform is modified by a scaling factor of 2.0
+//  Requires client to provide 2 workspace images.
+// \sa vimt3d_gauss_reduce()
 template<class T>
 void vimt3d_gauss_reduce(const vimt3d_image_3d_of<T>& src,
                          vimt3d_image_3d_of<T>&       dst,
@@ -24,8 +26,22 @@ void vimt3d_gauss_reduce(const vimt3d_image_3d_of<T>& src,
   vil3d_gauss_reduce(src.image(), dst.image(), work1.image(), work2.image());
 
   vimt3d_transform_3d scaling;
-  scaling.set_zoom_only(2.0, 2.0, 2.0, 0.0, 0.0, 0.0);
+  scaling.set_zoom_only(0.5, 0.5, 0.5, 0.0, 0.0, 0.0);
   dst.set_world2im(scaling * src.world2im());
+}
+
+
+//: Smooth and subsample src_im to produce dest_im
+//  This simple overload does not require client to provide 2 workspace images.
+// \sa vimt3d_gauss_reduce()
+template<class T>
+void vimt3d_gauss_reduce(const vimt3d_image_3d_of<T>& src,
+                         vimt3d_image_3d_of<T>&       dst)
+{
+  // Workspace images are resized as required by vil3d_gauss_reduce()
+  vimt3d_image_3d_of<T> work1;  
+  vimt3d_image_3d_of<T> work2;
+  vimt3d_gauss_reduce(src, dst, work1, work2);
 }
 
 
