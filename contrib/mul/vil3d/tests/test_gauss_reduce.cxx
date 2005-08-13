ffedfaea61d8acb725797ca46f0cacbbcd3c0a19
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-08-13 12:43:27 -0400

    corrected "ni2" --> "nk2", and added tests for the other corner points

--- contrib/mul/vil3d/tests/test_gauss_reduce.cxx
+++ contrib/mul/vil3d/tests/test_gauss_reduce.cxx
@@ -8,11 +8,11 @@
 
 static void test_gauss_reduce_float()
 {
-  vcl_cout << "**********************************\n"
+  vcl_cout << "***********************************\n"
            << " Testing vil3d_gauss_reduce<float>\n"
-           << "**********************************\n";
+           << "***********************************\n";
 
-  unsigned ni = 20, nj = 20, nk = 20;
+  unsigned ni = 10, nj = 20, nk = 30;
   vcl_cout<<"Image Size: "<<ni<<" x "<<nj<<" k "<<nk<<vcl_endl;
 
   vil3d_image_view<float> image0;
@@ -21,7 +21,7 @@ static void test_gauss_reduce_float()
   for (unsigned k=0;k<image0.nk();++k)
     for (unsigned j=0;j<image0.nj();++j)
       for (unsigned i=0;i<image0.ni();++i)
-        image0(i,j,k) = i*0.1f+j+k*10;
+        image0(i,j,k) = i*0.1f-j+k*10;
 
 
   unsigned ni2 = (ni+1)/2;
@@ -29,20 +29,30 @@ static void test_gauss_reduce_float()
   unsigned nk2 = (nk+1)/2;
   vil3d_image_view<float> image1,work_im1,work_im2;
   vil3d_gauss_reduce(image0,image1,work_im1,work_im2);
-  TEST("size i",image1.ni(),(ni+1)/2);
-  TEST("size j",image1.nj(),(nj+1)/2);
-  TEST("size k",image1.nk(),(nk+1)/2);
+  TEST("size i",image1.ni(), ni2);
+  TEST("size j",image1.nj(), nj2);
+  TEST("size k",image1.nk(), nk2);
   TEST_NEAR("Pixel (0,0,0)",image0(0,0,0),image1(0,0,0),1e-5);
   TEST_NEAR("Pixel (1,1,1)",image0(2,2,2),image1(1,1,1),1e-5);
-  TEST_NEAR("Pixel (2,3,3)",image1(2,3,3),66.4f,1e-4);
-  TEST_NEAR("Corner pixel",image0(ni2*2-2,nj2*2-2,nk2*2-2),image1(ni2-1,nj2-1,nk2-1),1e-5);
+  TEST_NEAR("Pixel (2,3,3)",image0(4,6,6),image1(2,3,3),1e-5);
+  TEST_NEAR("RFT corner pixel",image0(ni2*2-2,nj2*2-2,nk2*2-2),image1(ni2-1,nj2-1,nk2-1),1e-5);
+  TEST_NEAR("RFB corner pixel",image0(ni2*2-2,nj2*2-2,0),      image1(ni2-1,nj2-1,0)    ,1e-5);
+  TEST_NEAR("RNT corner pixel",image0(ni2*2-2,0,nk2*2-2),      image1(ni2-1,0,nk2-1)    ,1e-5);
+  TEST_NEAR("RNB corner pixel",image0(ni2*2-2,0,0),            image1(ni2-1,0,0)        ,1e-5);
+  TEST_NEAR("LFT corner pixel",image0(0,nj2*2-2,nk2*2-2),      image1(0,nj2-1,nk2-1)    ,1e-5);
+  TEST_NEAR("LFB corner pixel",image0(0,nj2*2-2,0),            image1(0,nj2-1,0)        ,1e-5);
+  TEST_NEAR("LNT corner pixel",image0(0,0,nk2*2-2),            image1(0,0,nk2-1)        ,1e-5);
 }
 
 // Check in-homogeneous smoothing option (ie onlj smooth in i,j but not k on some levels)
 static void test_gauss_reduce_ij()
 {
-  vcl_cout<<"test_gauss_reduce_ij()\n";
-  unsigned ni = 20, nj = 20, nk = 10;
+  vcl_cout << "**************************************\n"
+           << " Testing vil3d_gauss_reduce_ij<float>\n"
+           << "**************************************\n";
+
+  unsigned ni = 10, nj = 20, nk = 30;
+  vcl_cout<<"Image Size: "<<ni<<" x "<<nj<<" k "<<nk<<vcl_endl;
 
   vil3d_image_view<float> image0;
   image0.set_size(ni,nj,nk);
@@ -50,7 +60,7 @@ static void test_gauss_reduce_ij()
   for (unsigned k=0;k<image0.nk();++k)
     for (unsigned j=0;j<image0.nj();++j)
       for (unsigned i=0;i<image0.ni();++i)
-        image0(i,j,k) = i*0.1f+j+k*10;
+        image0(i,j,k) = i*0.1f-j+k*10;
 
   unsigned ni2 = (ni+1)/2;
   unsigned nj2 = (nj+1)/2;
@@ -61,18 +71,28 @@ static void test_gauss_reduce_ij()
 
   TEST("Level 1 size i",image1.ni(),ni2);
   TEST("Level 1 size j",image1.nj(),nj2);
-  TEST("Level 1 size k",image1.nk(),ni2);
+  TEST("Level 1 size k",image1.nk(),nk2);
   TEST_NEAR("Pixel (0,0,0)",image0(0,0,0),image1(0,0,0),1e-5);
   TEST_NEAR("Pixel (1,1,1)",image0(2,2,2),image1(1,1,2),1e-5);
-  TEST_NEAR("Pixel (2,3,3)",image1(2,3,3),36.4f,1e-4);
-  TEST_NEAR("Corner pixel",image0(ni2*2-2,nj2*2-2,nk2-1),image1(ni2-1,nj2-1,nk2-1),1e-5);
+  TEST_NEAR("Pixel (2,3,3)",image0(4,6,3),image1(2,3,3),1e-5);
+  TEST_NEAR("RFT corner pixel",image0(ni2*2-2,nj2*2-2,nk2-1),image1(ni2-1,nj2-1,nk2-1),1e-5);
+  TEST_NEAR("RFB corner pixel",image0(ni2*2-2,nj2*2-2,0),    image1(ni2-1,nj2-1,0)    ,1e-5);
+  TEST_NEAR("RNT corner pixel",image0(ni2*2-2,0,nk2-1),      image1(ni2-1,0,nk2-1)    ,1e-5);
+  TEST_NEAR("RNB corner pixel",image0(ni2*2-2,0,0),          image1(ni2-1,0,0)        ,1e-5);
+  TEST_NEAR("LFT corner pixel",image0(0,nj2*2-2,nk2-1),      image1(0,nj2-1,nk2-1)    ,1e-5);
+  TEST_NEAR("LFB corner pixel",image0(0,nj2*2-2,0),          image1(0,nj2-1,0)        ,1e-5);
+  TEST_NEAR("LNT corner pixel",image0(0,0,nk2-1),            image1(0,0,nk2-1)        ,1e-5);
 }
 
 // Check in-homogeneous smoothing option (ie onlj smooth in i,k but not j on some levels)
 static void test_gauss_reduce_ik()
 {
-  vcl_cout<<"test_gauss_reduce_ik()\n";
-  int ni = 20, nj = 10, nk = 20;
+  vcl_cout << "**************************************\n"
+           << " Testing vil3d_gauss_reduce_ik<float>\n"
+           << "**************************************\n";
+
+  unsigned ni = 10, nj = 20, nk = 30;
+  vcl_cout<<"Image Size: "<<ni<<" x "<<nj<<" k "<<nk<<vcl_endl;
 
   vil3d_image_view<float> image0;
   image0.set_size(ni,nj,nk);
@@ -80,7 +100,7 @@ static void test_gauss_reduce_ik()
   for (unsigned k=0;k<image0.nk();++k)
     for (unsigned j=0;j<image0.nj();++j)
       for (unsigned i=0;i<image0.ni();++i)
-        image0(i,j,k) = i*0.1f+j+k*10;
+        image0(i,j,k) = i*0.1f-j+k*10;
 
   vil3d_image_view<float> image1, work_im;
   vil3d_gauss_reduce_ik(image0,image1,work_im);
@@ -90,18 +110,28 @@ static void test_gauss_reduce_ik()
   unsigned nk2 = (nk+1)/2;
   TEST("Level 1 size i",image1.ni(),ni2);
   TEST("Level 1 size j",image1.nj(),nj2);
-  TEST("Level 1 size k",image1.nk(),ni2);
+  TEST("Level 1 size k",image1.nk(),nk2);
   TEST_NEAR("Pixel (0,0,0)",image0(0,0,0),image1(0,0,0),1e-5);
   TEST_NEAR("Pixel (1,1,1)",image0(2,2,2),image1(1,2,1),1e-5);
-  TEST_NEAR("Pixel (2,3,3)",image1(2,3,3),63.4f,1e-4);
-  TEST_NEAR("Corner pixel",image0(ni2*2-2,nj2-1,nk2*2-2),image1(ni2-1,nj2-1,nk2-1),1e-5);
+  TEST_NEAR("Pixel (2,3,3)",image0(4,3,6),image1(2,3,3),1e-5);
+  TEST_NEAR("RFT corner pixel",image0(ni2*2-2,nj2-1,nk2*2-2),image1(ni2-1,nj2-1,nk2-1),1e-5);
+  TEST_NEAR("RFB corner pixel",image0(ni2*2-2,nj2-1,0),      image1(ni2-1,nj2-1,0)    ,1e-5);
+  TEST_NEAR("RNT corner pixel",image0(ni2*2-2,0,nk2*2-2),    image1(ni2-1,0,nk2-1)    ,1e-5);
+  TEST_NEAR("RNB corner pixel",image0(ni2*2-2,0,0),          image1(ni2-1,0,0)        ,1e-5);
+  TEST_NEAR("LFT corner pixel",image0(0,nj2-1,nk2*2-2),      image1(0,nj2-1,nk2-1)    ,1e-5);
+  TEST_NEAR("LFB corner pixel",image0(0,nj2-1,0),            image1(0,nj2-1,0)        ,1e-5);
+  TEST_NEAR("LNT corner pixel",image0(0,0,nk2*2-2),          image1(0,0,nk2-1)        ,1e-5);
 }
 
 // Check in-homogeneous smoothing option (ie onlj smooth in j,k but not i on some levels)
 static void test_gauss_reduce_jk()
 {
-  vcl_cout<<"test_gauss_reduce_jk()\n";
-  unsigned ni = 10, nj = 20, nk = 20;
+  vcl_cout << "**************************************\n"
+           << " Testing vil3d_gauss_reduce_jk<float>\n"
+           << "**************************************\n";
+
+  unsigned ni = 10, nj = 20, nk = 30;
+  vcl_cout<<"Image Size: "<<ni<<" x "<<nj<<" k "<<nk<<vcl_endl;
 
   vil3d_image_view<float> image0;
   image0.set_size(ni,nj,nk);
@@ -109,7 +139,7 @@ static void test_gauss_reduce_jk()
   for (unsigned k=0;k<image0.nk();++k)
     for (unsigned j=0;j<image0.nj();++j)
       for (unsigned i=0;i<image0.ni();++i)
-        image0(i,j,k) = i*0.1f+j+k*10;
+        image0(i,j,k) = i*0.1f-j+k*10;
 
   vil3d_image_view<float> image1, work_im;
   vil3d_gauss_reduce_jk(image0,image1,work_im);
@@ -119,45 +149,58 @@ static void test_gauss_reduce_jk()
   unsigned nk2 = (nk+1)/2;
   TEST("Level 1 size i",image1.ni(),ni2);
   TEST("Level 1 size j",image1.nj(),nj2);
-  TEST("Level 1 size k",image1.nk(),ni2);
+  TEST("Level 1 size k",image1.nk(),nk2);
   TEST_NEAR("Pixel (0,0,0)",image0(0,0,0),image1(0,0,0),1e-5);
   TEST_NEAR("Pixel (1,1,1)",image0(2,2,2),image1(2,1,1),1e-5);
-  TEST_NEAR("Pixel (2,3,3)",image1(2,3,3),66.2f,1e-4);
-  TEST_NEAR("Corner pixel",image0(ni2-1,nj2*2-2,nk2*2-2),image1(ni2-1,nj2-1,nk2-1),1e-5);
+  TEST_NEAR("Pixel (2,3,3)",image0(2,6,6),image1(2,3,3),1e-5);
+  TEST_NEAR("RFT corner pixel",image0(ni2-1,nj2*2-2,nk2*2-2),image1(ni2-1,nj2-1,nk2-1),1e-5);
+  TEST_NEAR("RFB corner pixel",image0(ni2-1,nj2*2-2,0),      image1(ni2-1,nj2-1,0)    ,1e-5);
+  TEST_NEAR("RNT corner pixel",image0(ni2-1,0,nk2*2-2),      image1(ni2-1,0,nk2-1)    ,1e-5);
+  TEST_NEAR("RNB corner pixel",image0(ni2-1,0,0),            image1(ni2-1,0,0)        ,1e-5);
+  TEST_NEAR("LFT corner pixel",image0(0,nj2*2-2,nk2*2-2),    image1(0,nj2-1,nk2-1)    ,1e-5);
+  TEST_NEAR("LFB corner pixel",image0(0,nj2*2-2,0),          image1(0,nj2-1,0)        ,1e-5);
+  TEST_NEAR("LNT corner pixel",image0(0,0,nk2*2-2),          image1(0,0,nk2-1)        ,1e-5);
 }
 
 
 static void test_gauss_reduce_int()
 {
-  vcl_cout << "********************************\n"
+  vcl_cout << "*********************************\n"
            << " Testing vil3d_gauss_reduce<int>\n"
-           << "********************************\n";
-  
-  unsigned ni=10, nj=10, nk=10;
+           << "*********************************\n";
+
+  unsigned ni = 10, nj = 20, nk = 30;
   vcl_cout<<"Image Size: "<<ni<<" x "<<nj<<" k "<<nk<<vcl_endl;
-  
+
   vil3d_image_view<int> image0;
   image0.set_size(ni,nj,nk);
-  
+
   for (unsigned k=0;k<image0.nk();++k)
     for (unsigned j=0;j<image0.nj();++j)
       for (unsigned i=0;i<image0.ni();++i)
         image0(i,j,k) = -100*static_cast<int>(i) + j + 10*k;
-            
+
   unsigned ni2 = (ni+1)/2;
   unsigned nj2 = (nj+1)/2;
   unsigned nk2 = (nk+1)/2;
   vil3d_image_view<int> image1,work_im1,work_im2;
   vil3d_gauss_reduce(image0,image1,work_im1,work_im2);
 
-  TEST("size i",image1.ni(),(ni+1)/2);
-  TEST("size j",image1.nj(),(nj+1)/2);
-  TEST("size k",image1.nk(),(nk+1)/2);
-  
-  TEST("Pixel (0,0,0)", image0(0,0,0)==image1(0,0,0), true);
-  TEST("Pixel (1,1,1)", image0(2,2,2)==image1(1,1,1), true);
-  TEST("Pixel (2,2,2)", image0(4,4,4)==image1(2,2,2), true);
-  TEST("Pixel (4,4,4)", image0(8,8,8)==image1(4,4,4), true);
+  TEST("size i",image1.ni(),ni2);
+  TEST("size j",image1.nj(),nj2);
+  TEST("size k",image1.nk(),nk2);
+
+  TEST("Pixel (0,0,0)", image0(0,0,0), image1(0,0,0));
+  TEST("Pixel (1,1,1)", image0(2,5,2), image1(1,1,1));
+  TEST("Pixel (2,2,2)", image0(4,7,4), image1(2,2,2));
+  TEST("Pixel (3,3,3)", image0(6,9,6), image1(3,3,3));
+  TEST("RFT corner pixel",image0(ni2*2-2,nj2*2-2,nk2*2-2),image1(ni2-1,nj2-1,nk2-1));
+  TEST("RFB corner pixel",image0(ni2*2-2,nj2*2-2,0),      image1(ni2-1,nj2-1,0));
+  TEST("RNT corner pixel",image0(ni2*2-2,0,nk2*2-2),      image1(ni2-1,0,nk2-1));
+  TEST("RNB corner pixel",image0(ni2*2-2,0,0),            image1(ni2-1,0,0));
+  TEST("LFT corner pixel",image0(0,nj2*2-2,nk2*2-2),      image1(0,nj2-1,nk2-1));
+  TEST("LFB corner pixel",image0(0,nj2*2-2,0),            image1(0,nj2-1,0));
+  TEST("LNT corner pixel",image0(0,0,nk2*2-2),            image1(0,0,nk2-1));
 }
 
 
