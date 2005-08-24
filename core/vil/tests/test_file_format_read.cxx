097249868aafe6364e80bb95c82cb2f87bebef09
Rob Radtke <radtker@users.sourceforge.net>
2005-08-23 20:36:43 -0400

    Added new test which covers
    - reading NSIF 1.0 file
    - parsing a image header that contains a LUT
    - reads 1-bit bool data

--- core/vil/tests/test_file_format_read.cxx
+++ core/vil/tests/test_file_format_read.cxx
@@ -484,13 +484,13 @@ test_file_format_read_main( int argc, char* argv[] )
   testlib_test_begin( "  32-bit float grey" );
   testlib_test_perform(CheckFile(CompareGreyFloat<float>(), "ff_grey_float_true.txt", "ff_grey_float.tif" ) );
 
-  vcl_cout << "NITF 2.1 [nitf] (uncompressed)\n";
+  //The following tests are targeted to the vil_nitf2_image class which can read NITF 2.1, NITF 2.0 and 
+  //NSIF 1.0 files.  All three of these formats are covered here as well as all four different
+  //types of data layouts for uncompressed data (IMODE).  Also we test a fair number of different
+  //data types (eg. 8-bit unsigned, 16-bit unsigned, 32-bit signed, double, float, bool)
   //TODO: Create NITF tests for the following cases:
-  //-create bit-image test
-  //-read second image test (multiple images in one file)
-  //-parse complex header (LUT, multiple images etc) test
-  //-NSIF 1.0 file
-  //-clean up test files (get rid of irrelevant header text)  testlib_test_begin( "  8-bit unsigned int (IMODE=P)" );
+  //1) read second image test (ie. multiple images in one file)
+  vcl_cout << "NITF 2.1 [nitf] (uncompressed)\n";
   testlib_test_begin( "  8-bit unsigned int (IMODE=P)" );
   testlib_test_perform(CheckFile(ComparePlanes<vxl_byte,3>(), "ff_nitf_8bit_true.txt", "ff_nitf_8bit_p.nitf" ) );
   testlib_test_begin( "  8-bit unsigned int (IMODE=B)" );
@@ -508,7 +508,9 @@ test_file_format_read_main( int argc, char* argv[] )
   testlib_test_perform(CheckFile(CompareGreyFloat<float>(), "ff_nitf_float_true.txt", "ff_nitf_float.nitf" ) );
   testlib_test_begin( "  64-bit float (double)" );
   testlib_test_perform(CheckFile(CompareGreyFloat<double>(), "ff_nitf_float_true.txt", "ff_nitf_double.nitf" ) );
-
+  vcl_cout << "NSIF 1.0 [nitf] (uncompressed)\n";
+  testlib_test_begin( "  1-bit bool (NSIF w/ LUT to parse)" );
+  testlib_test_perform(CheckFile(CompareGrey<bool>(), "ff_nitf_1bit_lut_true.txt", "ff_nitf_1bit_lut.nsif" ) );
 
   vcl_cout << "Sun raster [ras]\n";
   testlib_test_begin( "  8-bit grey, no colourmap" );
