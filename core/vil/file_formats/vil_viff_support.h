fdd920174dbdac60d4f4890cc28f69f787e2c0bd
Ian Scott <scottim@users.sourceforge.net>
2005-08-31 11:08:33 -0400

    Fix linker error

--- core/vil/file_formats/vil_viff_support.h
+++ core/vil/file_formats/vil_viff_support.h
@@ -4,6 +4,8 @@
 #include <vxl_config.h> /* for vxl_uint_32 */
 struct vil_viff_xvimage;
 
+
+extern "C" {
 struct vil_viff_xvimage *
 vil_viff_createimage(vxl_uint_32 col_size, vxl_uint_32 row_size,
                      vxl_uint_32 data_storage_type, vxl_uint_32 num_of_images,
@@ -12,5 +14,6 @@ vil_viff_createimage(vxl_uint_32 col_size, vxl_uint_32 row_size,
                      vxl_uint_32 map_scheme, vxl_uint_32 map_storage_type,
                      vxl_uint_32 location_type, vxl_uint_32 location_dim);
 void vil_viff_freeimage (struct vil_viff_xvimage *);
+}
 
 #endif /* vil_viff_support_h_ */
