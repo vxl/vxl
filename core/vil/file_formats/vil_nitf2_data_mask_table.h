a151be808449a6a53e03f08147e2408bdf02cc55
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-09-01 16:46:21 -0400

    added Doxygen-style comment headers

--- core/vil/file_formats/vil_nitf2_data_mask_table.h
+++ core/vil/file_formats/vil_nitf2_data_mask_table.h
@@ -1,9 +1,11 @@
 // vil_nitf2: Written by Rob Radtke (rob@) and Harry Voorhees (hlv@) of
-// Stellar Science Ltd. Co. (stellarscience.com) for 
+// Stellar Science Ltd. Co. (stellarscience.com) for
 // Air Force Research Laboratory, 2005.
 
 #ifndef VIL_NITF2_DATA_MASK_TABLE_H
 #define VIL_NITF2_DATA_MASK_TABLE_H
+//:
+// \file
 
 #include <vcl_vector.h>
 #include <vxl_config.h>
@@ -12,54 +14,53 @@
 
 class vil_stream;
 
-/**
-  * This class is responsible for parsing a NITF 2.1 data mask table.
-  * when present, a vil_nitf2_image_subheader() will use this class
-  * to extract the table from the file.  It reads a vil_stream, via
-  * the parse() method and will return false if it failed.
-  */
+//: This class is responsible for parsing a NITF 2.1 data mask table.
+//  When present, a vil_nitf2_image_subheader() will use this class
+//  to extract the table from the file.  It reads a vil_stream, via
+//  the parse() method and will return false if it failed.
 class vil_nitf2_data_mask_table
 {
-public:
-  vil_nitf2_data_mask_table( unsigned int numBlocksX, unsigned int numBlocksY, 
-                              unsigned int numBands, const vcl_string imode );
+ public:
+  vil_nitf2_data_mask_table( unsigned int numBlocksX, unsigned int numBlocksY,
+                             unsigned int numBands, const vcl_string imode );
   bool parse( vil_stream* stream );
 
-  /**
-    * If these functions return true, then you may call \sa block_band_offset()
-    * and \sa pad_pixel()
-    */
+  //: If this function returns true, then you may call \sa block_band_offset()
   bool has_offset_table() const { return BMR_n_BND_m.size() > 0; }
+
+  //: If this function returns true, then you may call \sa pad_pixel()
   bool has_pad_pixel_table() const { return TMR_n_BND_m.size() > 0; }
 
   vxl_uint_32 blocked_image_data_offset() const;
-  /**
-    * Returns true iff this block is present in the data.  False otherwise.
-    * If this returns false, then there is not sense calling block_band_offset
-    * for this band/block combination. It will return 0xFFFFFFFF.
-    * If this returns false, then this entire block/band is considered to be blank
-    */
+
+  //: Returns true iff this block is present in the data.  False otherwise.
+  //  If this returns false, then there is not sense calling block_band_offset
+  //  for this band/block combination. It will return 0xFFFFFFFF.
+  //  If this returns false, then this entire block/band is considered to be blank.
   vxl_uint_32 block_band_present( unsigned int block_x, unsigned int block_y, int band = -1) const;
-  /**
-    * if imode == "S", then the band argument is used and I will return the offset to 'band'
-    * if imode != "S", then the band argument is ignored, and I will return the offset to the 
-    * beginning of the requested block
-    */
+
+  //:
+  //  if imode == "S", then the band argument is used and I will return the offset to 'band'
+  //  if imode != "S", then the band argument is ignored, and I will return the offset to the
+  //  beginning of the requested block
   vxl_uint_32 block_band_offset( unsigned int block_x, unsigned int block_y, int band = -1) const;
-  ///band argument is ignored if imode != "S"... ie. all bands have the same pad pixel in that case
+
+  //:
+  // band argument is ignored if imode != "S"... i.e. all bands have the same pad pixel in that case
   vxl_uint_32 pad_pixel( unsigned int block_x, unsigned int block_y, int band ) const;
+
   vxl_uint_32 block_band_has_pad( unsigned int block_x, unsigned int block_y, int band = -1) const;
 
   static void maybe_endian_swap( char* a, unsigned sizeOfAInBytes, vil_pixel_format pixFormat );
   static void maybe_endian_swap( char* a, unsigned sizeOfAInBytes, unsigned int bytesPerSample );
-protected:
+ protected:
   //Blocked Image Data Offset (in bytes)
   vxl_uint_32 IMDATOFF;
   //Block Mask Record Length (in bytes)
   vxl_uint_16 BMRLNTH;
   //Pad Pixel Mask Record Length (in bytes)
   vxl_uint_16 TMRLNTH;
-  //Pad Output Pixel Code Lenth (in bits)
+  //Pad Output Pixel Code Length (in bits)
   vxl_uint_16 TPXCDLNTH;
   //Pad Output Pixel Code (it's an integer,
   //but it's length is TPXCDLNTH rounded up
--- core/vil/file_formats/vil_nitf2_field_functor.h
+++ core/vil/file_formats/vil_nitf2_field_functor.h
@@ -1,5 +1,5 @@
 // vil_nitf2: Written by Harry Voorhees (hlv@) and Rob Radtke (rob@) of
-// Stellar Science Ltd. Co. (stellarscience.com) for 
+// Stellar Science Ltd. Co. (stellarscience.com) for
 // Air Force Research Laboratory, 2005.
 
 #ifndef VIL_NITF2_FIELD_FUNCTOR_H
@@ -14,25 +14,27 @@ class vil_nitf2_index_vector;
 #include "vil_nitf2_field_sequence.h"
 
 //-----------------------------------------------------------------------------
-// Functors used by NITF classes
-
-// Base class for functors that define a function that takes a 
-// vil_nitf2_field_sequence and attempts to compute an "out" parameter of 
-// type T. The function also returns a bool that specifies whether the 
+//:
+// \file
+// \brief Functors used by NITF classes
+//
+// Base class for functors that define a function that takes a
+// vil_nitf2_field_sequence and attempts to compute an "out" parameter of
+// type T. The function also returns a bool that specifies whether the
 // value could be computed.
 //
 // These functors are used to evaluate NITF field tags and invariably
 // call vil_nitf2_field_sequence::get_value(). When calling this method,
-// please be sure to set the argument ignore_extra_indexes to true. 
+// please be sure to set the argument ignore_extra_indexes to true.
 // This will allow the functor to be used within a repeat loop
-// and reference any preceding tag, inside or outside the repeat loop. 
+// and reference any preceding tag, inside or outside the repeat loop.
 // For exampe, to define a field sequence like this:
 //   FIELD A;
 //   REPEAT i=1..N
 //     FIELD B(i)
 //     FIELD C(i) exists if B(i) > 0
-//     FIELD D(i) exists if A > 0 
-// the same conditional functor can be used in the definitions of fields 
+//     FIELD D(i) exists if A > 0
+// the same conditional functor can be used in the definitions of fields
 // C and D by simply providing the name of the appropriate tag.  When
 // the functor is evaluated, the current index (i) is used to fetch the
 // tag. By calling get_value() with ignore_extra_indexes set to true,
@@ -41,92 +43,95 @@ class vil_nitf2_index_vector;
 template<typename T>
 class vil_nitf2_field_functor
 {
-public:
-  virtual bool operator() (vil_nitf2_field_sequence* record, 
+ public:
+  virtual bool operator() (vil_nitf2_field_sequence* record,
                            const vil_nitf2_index_vector& indexes, T& out_value) = 0;
   virtual ~vil_nitf2_field_functor() {}
 };
 
-// Functor vil_nitf2_field_value defines a function that sets its out parameter 
+//:
+// Functor vil_nitf2_field_value defines a function that sets its out parameter
 // to a value of a field from a field sequence. The function returns whether
-// the field was found. The functor is instantiated with the field's tag. 
+// the field was found. The functor is instantiated with the field's tag.
 // The field sequence is passed to the function.
 //
 template<typename T>
 class vil_nitf2_field_value : public vil_nitf2_field_functor<T>
 {
-public:
+ public:
   vil_nitf2_field_value(vcl_string tag) : tag(tag) {}
 
-  bool operator() (vil_nitf2_field_sequence* record, 
-                   const vil_nitf2_index_vector& indexes, T& value) 
+  bool operator() (vil_nitf2_field_sequence* record,
+                   const vil_nitf2_index_vector& indexes, T& value)
   {
-    return record->get_value(tag, indexes, value, true); 
+    return record->get_value(tag, indexes, value, true);
   }
 
-private:
+ private:
   vcl_string tag;
 };
 
-// Functor vil_nitf2_multiply_field_values defines a function that sets its out 
+//:
+// Functor vil_nitf2_multiply_field_values defines a function that sets its out
 // parameter to the product of the values of two fields. The predicate
 // returns true iff both fields are found or the arg 'use_zero_if_tag_not_found';
 // in the latter case, the out parameter is set to 0.
 //
 class vil_nitf2_multiply_field_values : public vil_nitf2_field_functor<int>
 {
-public:
-  vil_nitf2_multiply_field_values(const vcl_string& tag_1, 
-                                  const vcl_string& tag_2, 
-                                  bool use_zero_if_tag_not_found = false) 
-    : tag_1(tag_1), 
-      tag_2(tag_2), 
+ public:
+  vil_nitf2_multiply_field_values(const vcl_string& tag_1,
+                                  const vcl_string& tag_2,
+                                  bool use_zero_if_tag_not_found = false)
+    : tag_1(tag_1),
+      tag_2(tag_2),
       use_zero_if_tag_not_found(use_zero_if_tag_not_found) {}
 
-  bool operator() (vil_nitf2_field_sequence* record, 
+  bool operator() (vil_nitf2_field_sequence* record,
                    const vil_nitf2_index_vector& indexes, int& value);
 
-private:
+ private:
   vcl_string tag_1;
   vcl_string tag_2;
   bool use_zero_if_tag_not_found;
 };
 
-
-// Functor vil_nitf2_max_field_value_plus_offset_and_threshold defines a 
-// function that sets its out parameter to either the value of a specified 
-// field plus an offset, or a specified minimum value, whichever is greater. 
+//:
+// Functor vil_nitf2_max_field_value_plus_offset_and_threshold defines a
+// function that sets its out parameter to either the value of a specified
+// field plus an offset, or a specified minimum value, whichever is greater.
 // The function returns whether the field was found.
 //
 class vil_nitf2_max_field_value_plus_offset_and_threshold : public vil_nitf2_field_functor<int>
 {
-public:
+ public:
   vil_nitf2_max_field_value_plus_offset_and_threshold(
-    vcl_string tag, int offset, int min_threshold = 0) 
+    vcl_string tag, int offset, int min_threshold = 0)
     : tag(tag), offset(offset), min_threshold(min_threshold) {}
 
-  bool operator() (vil_nitf2_field_sequence* record, 
+  bool operator() (vil_nitf2_field_sequence* record,
                    const vil_nitf2_index_vector& indexes, int& value);
 
-private:
+ private:
   vcl_string tag;
   int offset;
   int min_threshold;
 };
 
-// Functor vil_nitf2_field_value_greater_than defines a comparison predicate that 
-// sets its out parameter to true if a specified field from a field 
-// sequence is found and its value is greater than a specified threshold. 
+//:
+// Functor vil_nitf2_field_value_greater_than defines a comparison predicate that
+// sets its out parameter to true if a specified field from a field
+// sequence is found and its value is greater than a specified threshold.
 // The predicate returns whether the field was found.
 //
 template<typename T>
 class vil_nitf2_field_value_greater_than: public vil_nitf2_field_functor<bool>
 {
-public:
-  vil_nitf2_field_value_greater_than(vcl_string tag, T threshold) 
+ public:
+  vil_nitf2_field_value_greater_than(vcl_string tag, T threshold)
     : tag(tag), threshold(threshold) {}
 
-  bool operator() (vil_nitf2_field_sequence* record, 
+  bool operator() (vil_nitf2_field_sequence* record,
                    const vil_nitf2_index_vector& indexes, bool& result) {
     T value;
     if (record->get_value(tag, indexes, value, true)) {
@@ -136,34 +141,36 @@ public:
       return false;
     }
   }
-private:
+ private:
   vcl_string tag;
   T threshold;
 };
 
-// Functor vil_nitf2_field_specified defines a comparision predicate that sets
+//:
+// Functor vil_nitf2_field_specified defines a comparison predicate that sets
 // its out parameter to true iff the specified field is not blank.
 //
 class vil_nitf2_field_specified: public vil_nitf2_field_functor<bool>
 {
-public:
+ public:
   vil_nitf2_field_specified(vcl_string tag) : tag(tag) {}
 
-  bool operator() (vil_nitf2_field_sequence* record, 
+  bool operator() (vil_nitf2_field_sequence* record,
                    const vil_nitf2_index_vector& indexes, bool& result);
 
-private:
+ private:
   vcl_string tag;
 };
 
-// Functor vil_nitf2_field_value_one_of defines a predicate that sets its out 
-// parameter to true iff the value of the specified tag equals one of 
+//:
+// Functor vil_nitf2_field_value_one_of defines a predicate that sets its out
+// parameter to true iff the value of the specified tag equals one of
 // the elements of a vcl_vector of acceptable values.
 //
 template<typename T>
 class vil_nitf2_field_value_one_of: public vil_nitf2_field_functor<bool>
 {
-public:
+ public:
   /// Constructor to specify a vcl_vector of acceptable values
   vil_nitf2_field_value_one_of(vcl_string tag, vcl_vector<T> acceptable_values)
     : tag(tag), acceptable_values(acceptable_values) {};
@@ -172,7 +179,7 @@ public:
   vil_nitf2_field_value_one_of(vcl_string tag, T acceptable_value)
     : tag(tag), acceptable_values(1, acceptable_value) {}
 
-  bool operator() (vil_nitf2_field_sequence* record, 
+  bool operator() (vil_nitf2_field_sequence* record,
                    const vil_nitf2_index_vector& indexes, bool& result)
   {
     result = false;
@@ -183,31 +190,32 @@ public:
         if ((*it) == val) {
           result = true;
           break;
-        } 
+        }
       }
       return true;
-    } 
+    }
     // Field not defined
     return false;
   }
 
-protected:
+ protected:
   vcl_string tag;
   vcl_vector<T> acceptable_values;
 };
 
-// Functor vil_nitf2_choose_field_value defines a function that sets its out 
+//:
+// Functor vil_nitf2_choose_field_value defines a function that sets its out
 // parameter to a value of one of two fields of a field sequence. The
 // field chosen is determined by the evaluating the functor passed as
-// argument 'choose_tag_1_predicate': tag_1 is chosen if it evaluates to true; 
+// argument 'choose_tag_1_predicate': tag_1 is chosen if it evaluates to true;
 // tag_2, otherwise.
 //
 template<typename T>
 class vil_nitf2_choose_field_value : public vil_nitf2_field_functor<T>
 {
-public:
+ public:
   /// Constructor. I take ownership of inDecider.
-  vil_nitf2_choose_field_value(const vcl_string& tag_1, const vcl_string& tag_2, 
+  vil_nitf2_choose_field_value(const vcl_string& tag_1, const vcl_string& tag_2,
                                vil_nitf2_field_functor<bool>* choose_tag_1_predicate)
     : tag_1(tag_1), tag_2(tag_2), choose_tag_1_predicate(choose_tag_1_predicate) {}
 
@@ -215,7 +223,7 @@ public:
     if (choose_tag_1_predicate) delete choose_tag_1_predicate;
   }
 
-  bool operator() (vil_nitf2_field_sequence* record, 
+  bool operator() (vil_nitf2_field_sequence* record,
                    const vil_nitf2_index_vector& indexes, T& value) {
     bool choose_tag_1;
     if ((*choose_tag_1_predicate)(record, indexes, choose_tag_1)) {
@@ -223,7 +231,7 @@ public:
       else return record->get_value(tag_2, indexes, value, true);
     } else return false;
   }
-private:
+ private:
   vcl_string tag_1;
   vcl_string tag_2;
   vil_nitf2_field_functor<bool>* choose_tag_1_predicate;
