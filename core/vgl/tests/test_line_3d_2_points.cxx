f377011bbd6286e55ea853eefabe32b3413b3bae
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-09-03 06:32:21 -0400

    avoid compiler warning (unused variable u)

--- core/vgl/tests/test_line_3d_2_points.cxx
+++ core/vgl/tests/test_line_3d_2_points.cxx
@@ -9,7 +9,6 @@
 
 static void test_plane_intersection()
 {
-
   vgl_plane_3d<double> pl1(vgl_vector_3d<double>(10,10,10), vgl_point_3d<double>(10,0,-10));
 
   TEST("O is a point on pl1 ",pl1.d(), 0.0);
@@ -49,7 +48,7 @@ static void test_direction_vector()
   vgl_point_3d<double> p2(1,2,3);
   vgl_line_3d_2_points<double> l1(p1, p2);
   vgl_vector_3d<double> u = p2 - p1;
-  TEST("Direction vector 1", u==l1.direction(), true);
+  TEST("Direction vector 1", u, l1.direction());
 }
 
 
@@ -59,12 +58,11 @@ static void test_parametric_point()
   vgl_point_3d<double> p2(1, 2, 4);
   vgl_point_3d<double> p3(0.5, 1.0, 2.0);
   vgl_line_3d_2_points<double> l1(p1, p2);
-  vgl_vector_3d<double> u = p2 - p1;
-  TEST("Parametric point: t=0.0", l1.point_t(0.0)==p1, true);
-  TEST("Parametric point: t=1.0", l1.point_t(1.0)==p2, true);
-  TEST("Parametric point: t=0.5", l1.point_t(0.5)==p3, true);
-  TEST("Parametric point: t=-1.0", l1.point_t(-1.0)==vgl_point_3d<double>(-1,-2,-4), true);
-  TEST("Parametric point: t=2.0", l1.point_t(2.0)==vgl_point_3d<double>(2,4,8), true);
+  TEST("Parametric point: t=0.0", l1.point_t(0.0), p1);
+  TEST("Parametric point: t=1.0", l1.point_t(1.0), p2);
+  TEST("Parametric point: t=0.5", l1.point_t(0.5), p3);
+  TEST("Parametric point: t=-1.0",l1.point_t(-1.0), vgl_point_3d<double>(-1,-2,-4));
+  TEST("Parametric point: t=2.0", l1.point_t(2.0), vgl_point_3d<double>(2,4,8));
 }
 
 
