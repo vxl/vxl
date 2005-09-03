d1d038192f92830d44c4eae449065466de3339aa
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-09-03 10:33:08 -0400

    added an inline "alias" for vgl_lineseg_test_line()

--- core/vgl/vgl_lineseg_test.h
+++ core/vgl/vgl_lineseg_test.h
@@ -7,23 +7,42 @@
 // \verbatim
 //  Modifications
 //   Nov.2003 - Peter Vanroose - made all functions templated
+//   Sep.2005 - Peter Vanroose - bug fix: collinear line segments always "true"
 // \endverbatim
 
 #include <vgl/vgl_line_segment_2d.h>
+#include <vgl/vgl_line_2d.h>
 
 // The old signature vgl_lineseg_test() was incorrectly documented. Its
 // meaning was the same as the new vgl_lineseg_test_line(). Only you can
 // decide which one you want.
 
 //: true if the line joining [1], [2] meets the linesegment joining [3], [4].
+// End points are considered to belong to a line segment.
 export template <class T>
 bool vgl_lineseg_test_line(T x1, T y1, T x2, T y2, T x3, T y3, T x4, T y4);
 
 //: true if the linesegment joining [1], [2] meets the linesegment joining [3], [4].
+// End points are considered to belong to a line segment.
 export template <class T>
 bool vgl_lineseg_test_lineseg(T x1, T y1, T x2, T y2, T x3, T y3, T x4, T y4);
 
-//: return true if the two linesegments meet
+//: true if the line meets the linesegment.
+// End points are considered to belong to a line segment.
+// \relates vgl_line_2d
+// \relates vgl_line_segment_2d
+template <class T>
+inline bool vgl_lineseg_test_line(vgl_line_2d<T> const& l1,
+                                  vgl_line_segment_2d<T> const& l2)
+{
+  return vgl_lineseg_test_line(l1.point1().x(),l1.point1().y(),
+                               l1.point2().x(),l1.point2().y(),
+                               l2.point1().x(),l2.point1().y(),
+                               l2.point2().x(),l2.point2().y());
+}
+
+//: return true if the two linesegments meet.
+// End points are considered to belong to a line segment.
 // \relates vgl_line_segment_2d
 template <class T>
 inline bool vgl_lineseg_test_lineseg(vgl_line_segment_2d<T> const& l1,
