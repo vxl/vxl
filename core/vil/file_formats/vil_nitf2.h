5472db756bbb0cf24656f6a827d24287eb0c8245
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-09-25 10:40:42 -0400

    indentation

--- core/vil/file_formats/vil_nitf2.h
+++ core/vil/file_formats/vil_nitf2.h
@@ -1,5 +1,5 @@
 // vil_nitf2: Written by Rob Radtke (rob@) and Harry Voorhees (hlv@) of
-// Stellar Science Ltd. Co. (stellarscience.com) for 
+// Stellar Science Ltd. Co. (stellarscience.com) for
 // Air Force Research Laboratory, 2005.
 
 #ifndef VIL_NITF2_STREAM_H
@@ -7,38 +7,38 @@
 
 #include <vil/vil_stream.h>
 
-// These typedefs serve to maintain the method signatures' distinction between 
-// input and output streams, at least until we decide that such distinction is 
+// These typedefs serve to maintain the method signatures' distinction between
+// input and output streams, at least until we decide that such distinction is
 // no longer useful.
 typedef vil_stream vil_nitf2_istream;
 typedef vil_stream vil_nitf2_ostream;
 
 //some of the integer values stored in nitf 2.x headers can be
 //larger than 2^32.  That's why we have vil_nitf2_long_long_formatter.
-//We use this typedef so systems that don't have 64 bit integers 
+//We use this typedef so systems that don't have 64 bit integers
 //can still use the class.  Of course they will break if they try to
 //read a header that contains a value greater than 2^32.  Fortunately,
 //that is somewhat rare
 #include <vxl_config.h>
 #if VXL_HAS_INT_64
 typedef vxl_int_64 vil_nitf2_long;
-#else 
+#else
 typedef vxl_int_32 vil_nitf2_long;
-#endif 
+#endif
 
-// Wrapper class for shared enums and static variables, to avoid circular 
+// Wrapper class for shared enums and static variables, to avoid circular
 // dependencies among classes
 //
-class vil_nitf2 
+class vil_nitf2
 {
  public:
   // NITF field data types supported
-  enum enum_field_type { type_undefined=0, 
-                         type_int, type_long_long, type_double, 
+  enum enum_field_type { type_undefined=0,
+                         type_int, type_long_long, type_double,
                          type_char, type_string, type_binary,
                          type_location, type_date_time, type_tre };
 
-  // Controls the level of detail of logging to vcl_cout. 
+  // Controls the level of detail of logging to vcl_cout.
   // All errors are logged to vcl_cerr, irrespective of log level.
   enum enum_log_level { log_none=0, log_info, log_debug };
 
