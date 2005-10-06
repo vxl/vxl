b671f6d4788802228d3fd833e01fe8cc4b5aba74
Ian Scott <scottim@users.sourceforge.net>
2005-10-06 04:35:35 -0400

    It would be a lot easier if compilers would just implemnt the export keyword

---
+++ core/vbl/Templates/vbl_array_3d+string-.cxx
@@ -0,0 +1,4 @@
+#include <vcl_string.h>
+#include <vbl/vbl_array_3d.txx>
+VBL_ARRAY_3D_INSTANTIATE(vcl_string);
+VBL_ARRAY_3D_IO_INSTANTIATE(vcl_string);
---
+++ core/vbl/io/Templates/vsl_vector_io+vbl_triple+uint.uint.uint--.cxx
@@ -0,0 +1,5 @@
+#include <vsl/vsl_vector_io.txx>
+#include <vbl/vbl_triple.txx>
+#include <vbl/io/vbl_io_triple.txx>
+typedef vbl_triple<unsigned,unsigned,unsigned> vbl_triple_iii; 
+VSL_VECTOR_IO_INSTANTIATE(vbl_triple_iii);
