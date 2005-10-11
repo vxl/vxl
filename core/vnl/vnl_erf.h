a823383f0a5aed3280ad5e24f26232e34388ccf0
Nicolas Burrus <nburrus@users.sourceforge.net>
2005-10-11 08:06:41 -0400

    vnl_scaled_erfc(): New function.

--- core/vnl/vnl_erf.h
+++ core/vnl/vnl_erf.h
@@ -7,6 +7,7 @@
 // \author Tim Cootes, Ian Scott
 
 #include <vnl/vnl_gamma.h>
+#include <vnl/vnl_math.h>
 
 //: The Error function
 // erf(x) = (2/sqrt(pi)) Integral from 0 to x (exp(-t^2) dt)
@@ -19,5 +20,13 @@ inline double vnl_erf(double x)
 // This value is useful for large x, when erf(x) ~= 1 and erfc(x) < eps.
 // \note the output ranges from 0 to 2, and vnl_erfc(0) = 1.
 double vnl_erfc(double x);
+
+//: The Scaled Complementary Error function
+// erfc_scaled(x) = exp(x^2) * erfc(x)
+// This value is useful for very large x, where erf and erfc returns
+// respectively ~1 and ~0.
+// It can be approximated by (1/sqrt(pi)) * (1/x)
+inline double vnl_scaled_erfc(double x)
+{ return (vnl_math::two_over_sqrtpi/2.)*(1./x); }
 
 #endif // vnl_erf_h_
