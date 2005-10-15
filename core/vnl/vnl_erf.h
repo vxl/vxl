d3a185e28264d210abdbf4d7f0e3f9eac4ba944b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-10-15 12:19:13 -0400

    Doxygen brief comment should be on a single line

--- core/vnl/vnl_erf.h
+++ core/vnl/vnl_erf.h
@@ -9,19 +9,19 @@
 #include <vnl/vnl_gamma.h>
 #include <vnl/vnl_math.h>
 
-//: The Error function
+//: The Error function.
 // erf(x) = (2/sqrt(pi)) Integral from 0 to x (exp(-t^2) dt)
 // \note the output ranges from -1 to 1, and vnl_erf(0) = 0.
 inline double vnl_erf(double x)
 { return (x<0)?-vnl_gamma_p(0.5,x*x):vnl_gamma_p(0.5,x*x); };
 
-//: The Complementary Error function
+//: The Complementary Error function.
 // erfc(x) = 1 - erf(x) = 1 - (2/sqrt(pi)) Integral from 0 to x (exp(-t^2) dt)
 // This value is useful for large x, when erf(x) ~= 1 and erfc(x) < eps.
 // \note the output ranges from 0 to 2, and vnl_erfc(0) = 1.
 double vnl_erfc(double x);
 
-//: The Scaled Complementary Error function
+//: The Scaled Complementary Error function.
 // erfc_scaled(x) = exp(x^2) * erfc(x)
 // This value is useful for very large x, where erf and erfc returns
 // respectively ~1 and ~0.
