1d6e39f556b7710802f4f14959fbe7377fe4281a
Michal Sofka <sofka@users.sourceforge.net>
2005-10-27 18:14:38 -0400

    adding smart pointer instantiation for feature face point

---
+++ contrib/rpl/rgrl/Templates/vbl_smart_ptr+rgrl_feature_face_pt-.cxx
@@ -0,0 +1,4 @@
+#include <vbl/vbl_smart_ptr.txx>
+#include <rgrl/rgrl_feature_face_pt.h>
+
+VBL_SMART_PTR_INSTANTIATE( rgrl_feature_face_pt );
---
+++ contrib/rpl/rgrl/rgrl_feature_face_pt_sptr.h
@@ -0,0 +1,15 @@
+
+#ifndef rgrl_feature_face_pt_sptr_h_
+#define rgrl_feature_face_pt_sptr_h_
+
+//:
+// \file
+
+#include <vbl/vbl_smart_ptr.h>
+
+class rgrl_feature_face_pt;
+
+//: Reference-counted smart pointer to rgrl_feature.
+typedef vbl_smart_ptr< rgrl_feature_face_pt > rgrl_feature_face_pt_sptr;
+
+#endif // rgrl_feature_face_pt_sptr_h_
