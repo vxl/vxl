ae784b5ed7c319d35a68542b071b0f4bc0160b52
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-11-27 08:19:51 -0500

    definition of vsl_print_summary(ostream,vector_pair_id) must be visible when instantiating this

--- core/vbl/io/Templates/vbl_io_array_2d+vcl_vector+vcl_pair+int.double---.cxx
+++ core/vbl/io/Templates/vbl_io_array_2d+vcl_vector+vcl_pair+int.double---.cxx
@@ -1,5 +1,6 @@
 #include <vcl_utility.h>
 #include <vcl_vector.h>
+#include <vsl/vsl_vector_io.txx>
 #include <vbl/io/vbl_io_array_2d.txx>
 typedef vcl_vector<vcl_pair<int,double> > vector_pair_id;
 VBL_IO_ARRAY_2D_INSTANTIATE(vector_pair_id);
