4665c0940c4a8b066891eaade98a2b359c2fa8d7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2005-11-27 04:38:37 -0500

    renamed from vsl_vector_io+vcl_vector+vcl_pair+int,double---.cxx to vbl_io_array_2d+vcl_vector+vcl_pair+int.double---.cxx

--- core/vbl/io/Templates/vbl_io_array_2d+vcl_vector+vcl_pair+int.double---.cxx
+++ core/vbl/io/Templates/vbl_io_array_2d+vcl_vector+vcl_pair+int.double---.cxx
@@ -1,6 +1,5 @@
-#include <vcl_utility.txx>
-#include <vsl/vsl_vector_io.txx>
-#include <vsl/vsl_pair_io.txx>
+#include <vcl_utility.h>
+#include <vcl_vector.h>
 #include <vbl/io/vbl_io_array_2d.txx>
 typedef vcl_vector<vcl_pair<int,double> > vector_pair_id;
 VBL_IO_ARRAY_2D_INSTANTIATE(vector_pair_id);
