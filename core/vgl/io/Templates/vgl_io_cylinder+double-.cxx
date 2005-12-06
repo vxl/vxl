c599f7534243163f288089bcfb340047444d3576
Gamze Tunali <gamze@users.sourceforge.net>
2005-12-05 23:13:04 -0500

    vgl_io_cylinder templates added

---
+++ core/vgl/io/Templates/vgl_io_cylinder+double-.cxx
@@ -0,0 +1,3 @@
+// Instantiation of IO for vgl_io_cylinder<double>
+#include <vgl/io/vgl_io_cylinder.txx>
+VGL_IO_CYLINDER_INSTANTIATE(double);
---
+++ core/vgl/io/Templates/vsl_vector_io+vcl_vector+vgl_cylinder_3d+double---.cxx
@@ -0,0 +1,3 @@
+#include <vsl/vsl_vector_io.txx>
+#include <vgl/io/vgl_io_cylinder.h>
+VSL_VECTOR_IO_INSTANTIATE(vcl_vector<vgl_cylinder<double> >);
