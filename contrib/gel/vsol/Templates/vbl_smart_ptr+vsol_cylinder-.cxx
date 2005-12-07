dfdd81f6d6ad6208a49ba9d8183d1604eeb3639c
Gamze Tunali <gamze@users.sourceforge.net>
2005-12-06 22:39:45 -0500

    vsol_cylinder class templates added

---
+++ contrib/gel/vsol/Templates/vbl_smart_ptr+vsol_cylinder-.cxx
@@ -0,0 +1,5 @@
+#include <vsol/vsol_box_3d.h>
+#include <vsol/vsol_cylinder.h>
+#include <vbl/vbl_smart_ptr.txx>
+
+VBL_SMART_PTR_INSTANTIATE(vsol_cylinder);
---
+++ contrib/gel/vsol/Templates/vsl_vector_io+vsol_cylinder_sptr-.cxx
@@ -0,0 +1,5 @@
+#include <vsol/vsol_cylinder.h>
+#include <vsol/vsol_cylinder_sptr.h>
+#include <vsl/vsl_vector_io.txx>
+
+VSL_VECTOR_IO_INSTANTIATE(vsol_cylinder_sptr);
