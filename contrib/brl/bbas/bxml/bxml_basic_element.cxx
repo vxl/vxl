163cdd7b1732ef75bf533a9dfe33847f83207ca0
Gamze Tunali <gamze@users.sourceforge.net>
2006-01-05 09:53:26 -0500

    initial version

---
+++ contrib/brl/bbas/bxml/bxml_basic_element.cxx
@@ -0,0 +1,93 @@
+// This is basic/bgui3d/bgui3d_file_io.cxx
+//:
+// \file
+
+#include "bxml_basic_element.h"
+//#include <vcl_iostream.h>
+
+/*void bxml_basic_element::add_attribute(vcl_pair<vcl_string, vcl_string> attr)
+{
+  attrs_.push_back(attr);
+}*/
+
+void bxml_basic_element::add_attribute_list(vcl_vector<vcl_pair<vcl_string, vcl_string> > attrs)
+{
+  for (unsigned int i=0; i<attrs.size(); i++) {
+    attrs_.push_back(attrs[i]);
+  }
+}
+
+void bxml_basic_element::add_attribute(vcl_string attr_name, double value)
+{
+  vcl_string value_str = toString(value);
+  vcl_pair<vcl_string, vcl_string> attr(attr_name, value_str.data());
+  attrs_.push_back(attr);
+}
+
+void bxml_basic_element::add_attribute(vcl_string attr_name, int value)
+{
+  vcl_string value_str = toString(value); //"" + value;
+  vcl_pair<vcl_string, vcl_string> attr(attr_name, value_str);
+  attrs_.push_back(attr);
+}
+
+void bxml_basic_element::add_attribute(vcl_string attr_name, vcl_string value)
+{
+  vcl_pair<vcl_string, vcl_string> attr(attr_name, value);
+  attrs_.push_back(attr);
+}
+
+void bxml_basic_element::append_cdata(vcl_string cdata) 
+{
+  if (cdata_.size() > 0)
+    cdata_.append(" ");
+  cdata_.append(cdata);
+}
+
+void bxml_basic_element::append_cdata(double cdata)
+{
+  if (cdata_.size() > 0)
+    cdata_.append(" ");
+  cdata_.append(toString(cdata));
+}
+
+void bxml_basic_element::append_cdata(int cdata)
+{
+  if (cdata_.size() > 0)
+    cdata_.append(" ");
+  cdata_.append(toString(cdata));
+}
+bool bxml_basic_element::delete_attribute(vcl_string attr_name)
+{
+  return false;
+}
+
+void bxml_basic_element::x_write(vcl_ostream& ostream)
+{
+  // put the initial bracket with element name and the attribute-value list
+  x_write_open(ostream);
+
+  // put the character data between the tags
+  if (cdata_.size() > 0)
+    ostream << cdata_ << "\n";
+  
+  // close the element
+  //ostream << "</" << tag_ << ">" << "\n";
+  x_write_close(ostream);
+}
+
+void bxml_basic_element::x_write_open(vcl_ostream& ostream) 
+{
+  //ostream << "<" << tag_ << ">" << "\n"; 
+  ostream << "<" << tag_;
+  for (unsigned int i=0; i<attrs_.size(); i++) {
+    ostream << " " << attrs_[i].first << "=" << "\"" << attrs_[i].second << "\"";
+  }
+  ostream << ">" << "\n";
+} 
+
+//: writes the closing tag to the stream
+void bxml_basic_element::x_write_close(vcl_ostream& ostream) 
+{
+  ostream << "</" << tag_ << ">" << "\n";
+}
---
+++ contrib/brl/bbas/bxml/bxml_basic_element.h
@@ -0,0 +1,73 @@
+// This is basic/bgui3d/bgui3d.h
+#ifndef bxml_basic_element_h_
+#define bxml_basic_element_h_
+
+//:
+// \file
+// \brief  creates basic xml nodes and writes them out to the stream
+// \author Gamze Tunali (gamze@lems.brown.edu)
+// \date   12/21/2005
+//
+// \verbatim
+//  Modifications
+// \endverbatim
+
+#include <vcl_vector.h>
+#include <vcl_utility.h>
+#include <vcl_string.h>
+#include <vcl_sstream.h>
+#include <vcl_iostream.h>
+
+//template<typename T> std::wstring toString(const T& t)
+template<typename T> vcl_string toString(const T& t)
+{
+  vcl_stringstream strm;
+
+  strm << t;
+  vcl_string str(strm.str());
+  return str;
+
+}
+
+class bxml_basic_element{
+public:
+  //: constructs with a name
+  bxml_basic_element(vcl_string tag)
+    : tag_(tag){};
+
+  //: constructs with a name and a list of (attibute,value) pair
+  bxml_basic_element(vcl_string tag, vcl_vector<vcl_pair<vcl_string, vcl_string> > attrs)
+    : tag_(tag), attrs_(attrs){};
+
+  //:destructor
+  ~bxml_basic_element(){};
+
+  //void add_attribute(vcl_pair<vcl_string, vcl_string> attr);
+  //: overloaded add_attribute methods to add different types of values as string
+  void add_attribute(vcl_string attr_name, double value);
+  void add_attribute(vcl_string attr_name, int value);
+  void add_attribute(vcl_string attr_name, vcl_string value);
+
+  void add_attribute_list(vcl_vector<vcl_pair<vcl_string, vcl_string> > attrs);
+
+  bool delete_attribute(vcl_string attr_name);
+
+  void append_cdata(vcl_string cdata);
+  void append_cdata(double cdata);
+  void append_cdata(int cdata);
+
+  void x_write(vcl_ostream& ostream);
+  //: writes the opening tag to the stream
+  void x_write_open(vcl_ostream& ostream);
+
+  //: writes the closing tag to the stream
+  void x_write_close(vcl_ostream& ostream); 
+
+protected:
+  vcl_string tag_;
+  vcl_vector<vcl_pair<vcl_string, vcl_string> > attrs_;
+  vcl_string cdata_;
+
+};
+
+#endif // bxml_basic_element_h_
