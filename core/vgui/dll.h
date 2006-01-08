d8e264004946c30353197b0f9b86d42edad957be
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-01-08 15:13:49 -0500

    consistent DLL defines

--- core/vgui/dll.h
+++ core/vgui/dll.h
@@ -4,15 +4,14 @@
 
 #include <vcl_compiler.h>
 
-#ifdef VCL_WIN32
-#ifndef BUILDING_vgui_DLL
-// if win32 and not BUILDING_COOL_DLL then you need a dllimport 
+#if defined(VCL_WIN32) && !defined(BUILDING_VGUI_DLL)
+
+// if win32 and not BUILDING_VGUI_DLL then you need a dllimport 
 // Only if you are building a DLL linked application.
 #ifdef BUILD_DLL
 #undef vgui_DLLDATA
 #define vgui_DLLDATA _declspec(dllimport)
 #endif // BUILD_DLL
-#endif // BUILDING_SPATIAL_OBJECT_DLL
-#endif // VCL_WIN32
+#endif // VCL_WIN32 && !BUILDING_VGUI_DLL
 
 #endif // vgui_DLLDATA_H
