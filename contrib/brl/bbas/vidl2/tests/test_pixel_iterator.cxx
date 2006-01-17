c914732c137365cc8909152289fe4a01aa125c55
Matthew J. Leotta <mleotta@users.sourceforge.net>
2006-01-17 17:33:01 -0500

    added many more iterators and test cases

--- contrib/brl/bbas/vidl2/tests/test_pixel_iterator.cxx
+++ contrib/brl/bbas/vidl2/tests/test_pixel_iterator.cxx
@@ -1,6 +1,7 @@
 // This is brl/bbas/vidl2/tests/test_pixel_iterator.cxx
 #include <testlib/testlib_test.h>
 #include <vcl_iostream.h>
+#include <vidl2/vidl2_frame.h>
 #include <vidl2/vidl2_pixel_iterator.h>
 
 
@@ -11,10 +12,98 @@ static void test_pixel_iterator()
            << "******************************\n";
 
   {
+    // The test buffer below contains 3 pixels encoded in RGB 24
+    //                  | R  G  B| R  G  B| R  G  B |
+    vxl_byte buffer[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    vidl2_frame_sptr frame = new vidl2_shared_frame(buffer,3,1,VIDL2_PIXEL_FORMAT_RGB_24);
+    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_RGB_24> itr(frame);
+
+    bool success = true;
+    for(unsigned int i=0; i<3; ++i, ++itr){
+      vcl_cout << "RGB = ("<< (int)itr(0) << "," << (int)itr(1) << "," << (int)itr(2) << ")\n";
+      success = success && itr(0) == buffer[3*i]
+                        && itr(1) == buffer[3*i+1]
+                        && itr(2) == buffer[3*i+2];
+    }
+    TEST("vidl2_pixel_iterator (RGB 24)",success,true);
+  }
+
+  {
+    // The test buffer below contains 3 pixels encoded in BGR 24
+    //                  | B  G  R| B  G  R| B  G  R |
+    vxl_byte buffer[] = { 3, 2, 1, 6, 5, 4, 9, 8, 7 };
+    vidl2_frame_sptr frame = new vidl2_shared_frame(buffer,3,1,VIDL2_PIXEL_FORMAT_BGR_24);
+    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_BGR_24> itr(frame);
+
+    bool success = true;
+    for(unsigned int i=0; i<3; ++i, ++itr){
+      vcl_cout << "RGB = ("<< (int)itr(0) << "," << (int)itr(1) << "," << (int)itr(2) << ")\n";
+      success = success && itr(0) == buffer[3*i+2]
+          && itr(1) == buffer[3*i+1]
+          && itr(2) == buffer[3*i];
+    }
+    TEST("vidl2_pixel_iterator (BGR 24)",success,true);
+  }
+
+  {
+    // The test buffer below contains 3 pixels encoded in RGBA 32
+    //                  | R  G  B  A| R  G  B  A| R  G  B  A|
+    vxl_byte buffer[] = { 1, 2, 3, 0, 4, 5, 6, 0, 7, 8, 9, 0 };
+    vidl2_frame_sptr frame = new vidl2_shared_frame(buffer,3,1,VIDL2_PIXEL_FORMAT_RGBA_32);
+    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_RGBA_32> itr(frame);
+
+    bool success = true;
+    for(unsigned int i=0; i<3; ++i, ++itr){
+      vcl_cout << "RGBA = ("<< (int)itr(0) << "," << (int)itr(1)
+          << "," << (int)itr(2) << ","<< (int)itr(3) << ")\n";
+      success = success && itr(0) == buffer[4*i]
+          && itr(1) == buffer[4*i+1]
+          && itr(2) == buffer[4*i+2]
+          && itr(3) == buffer[4*i+3];
+    }
+    TEST("vidl2_pixel_iterator (RGBA 32)",success,true);
+  }
+
+  {
+    // The test buffer below contains 2 pixels encoded in RGB 555
+    //                      |RGB_555|RGB_555|
+    vxl_uint_16 buffer[] = { 0x7FAF, 0x0103 };
+    vidl2_frame_sptr frame = new vidl2_shared_frame(buffer,2,1,VIDL2_PIXEL_FORMAT_RGB_555);
+    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_RGB_555> itr(frame);
+
+    bool success = true;
+    for(unsigned int i=0; i<2; ++i, ++itr){
+      vcl_cout << "RGB = ("<< (int)itr(0) << "," << (int)itr(1) << "," << (int)itr(2) << ")\n";
+      success = success && itr(0) == vxl_byte((buffer[i] >> 10)&31)<<3
+          && itr(1) == vxl_byte((buffer[i] >> 5)&31)<<3
+          && itr(2) == vxl_byte(buffer[i]&31)<<3;
+    }
+    TEST("vidl2_pixel_iterator (RGB 555)",success,true);
+  }
+
+  {
+    // The test buffer below contains 2 pixels encoded in RGB 565
+    //                      |RGB_565|RGB_565|
+    vxl_uint_16 buffer[] = { 0x7FAF, 0x0103 };
+    vidl2_frame_sptr frame = new vidl2_shared_frame(buffer,2,1,VIDL2_PIXEL_FORMAT_RGB_565);
+    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_RGB_565> itr(frame);
+
+    bool success = true;
+    for(unsigned int i=0; i<2; ++i, ++itr){
+      vcl_cout << "RGB = ("<< (int)itr(0) << "," << (int)itr(1) << "," << (int)itr(2) << ")\n";
+      success = success && itr(0) == vxl_byte((buffer[i] >> 11)&31)<<3
+          && itr(1) == vxl_byte((buffer[i] >> 5)&63)<<2
+          && itr(2) == vxl_byte(buffer[i]&31)<<3;
+    }
+    TEST("vidl2_pixel_iterator (RGB 565)",success,true);
+  }
+
+  {
     // The test buffer below contains 4 pixels encoded in YUV 4:2:2
     //                  | U  Y0 V Y1| U  Y0 V  Y1 |
     vxl_byte buffer[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_YUV_422> itr(buffer);
+    vidl2_frame_sptr frame = new vidl2_shared_frame(buffer,2,2,VIDL2_PIXEL_FORMAT_YUV_422);
+    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_YUV_422> itr(frame);
 
     bool success = true;
     for(unsigned int i=0; i<4; ++i, ++itr){
@@ -23,13 +112,59 @@ static void test_pixel_iterator()
           && itr(1) == buffer[i*2-(i%2)*2]
           && itr(2) == buffer[i*2+2-(i%2)*2];
     }
-    TEST("vidl2_pixel_iterator (YUV_422)",success,true);
+    TEST("vidl2_pixel_iterator (YUV 422)",success,true);
   }
 
   {
-    // The test buffer below contains 4 pixels encoded in YUV 4:2:2
+    // The test buffer below contains 8 pixels encoded in YUV 4:1:1
+    //                  | U  Y0 Y1 V  Y2 Y3|U  Y0 Y1  V  Y2  Y3|
+    vxl_byte buffer[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+    vidl2_frame_sptr frame = new vidl2_shared_frame(buffer,2,2,VIDL2_PIXEL_FORMAT_YUV_411);
+    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_YUV_411> itr(frame);
+
+    bool success = true;
+    for(unsigned int i=0; i<8; ++i, ++itr){
+      vcl_cout << "YUV = ("<< (int)itr(0) << "," << (int)itr(1) << "," << (int)itr(2) << ")\n";
+      success = success && itr(0) == buffer[6*(i/4)+(i&3)+1+((i>>1)&1)]
+          && itr(1) == buffer[6*(i/4)]
+          && itr(2) == buffer[6*(i/4)+3];
+    }
+    TEST("vidl2_pixel_iterator (YUV 411)",success,true);
+  }
+
+  {
+    // The test buffer below contains 4 pixels encoded in Mono 16
+    vxl_uint_16 buffer[] = { 58791, 23010, 5, 100 };
+    vidl2_frame_sptr frame = new vidl2_shared_frame(buffer,2,2,VIDL2_PIXEL_FORMAT_MONO_16);
+    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_MONO_16> itr(frame);
+    bool success = true;
+    for(unsigned int i=0; i<4; ++i, ++itr){
+      vcl_cout << (int)itr(0) << " ";
+      success = success && (itr(0) == buffer[i]);
+    }
+    vcl_cout << '\n';
+    TEST("vidl2_pixel_iterator (Mono 16)",success,true);
+  }
+
+  {
+    // The test buffer below contains 4 pixels encoded in Mono 8
+    vxl_byte buffer[] = { 255, 133, 122, 10 };
+    vidl2_frame_sptr frame = new vidl2_shared_frame(buffer,2,2,VIDL2_PIXEL_FORMAT_MONO_8);
+    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_MONO_8> itr(frame);
+    bool success = true;
+    for(unsigned int i=0; i<4; ++i, ++itr){
+      vcl_cout << (int)itr(0) << " ";
+      success = success && (itr(0) == buffer[i]);
+    }
+    vcl_cout << '\n';
+    TEST("vidl2_pixel_iterator (Mono 8)",success,true);
+  }
+
+  {
+    // The test buffer below contains 16 pixels encoded in Mono 1 (boolean image)
     vxl_byte buffer[] = { 0xA3, 0x7B }; // 1010 0011 0111 1011
-    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_MONO_1> itr(buffer);
+    vidl2_frame_sptr frame = new vidl2_shared_frame(buffer,4,4,VIDL2_PIXEL_FORMAT_MONO_1);
+    vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_MONO_1> itr(frame);
     bool success = true;
     for(unsigned int i=0; i<16; ++i, ++itr){
       vcl_cout << itr(0) << " ";
--- contrib/brl/bbas/vidl2/vidl2_pixel_iterator.h
+++ contrib/brl/bbas/vidl2/vidl2_pixel_iterator.h
@@ -21,11 +21,101 @@
 
 
 #include "vidl2_pixel_format.h"
+#include "vidl2_frame.h"
 #include <vxl_config.h>
 #include <vcl_cassert.h>
 
+//-----------------------------------------------------------------------------
+
+//: Component color encoding
+// These functions indicate how to extract a byte for each
+// color channel given a pointer to the pixel memory
+// This is only meant for non-planar non-packed formats
 template <vidl2_pixel_format FMT> 
-class vidl2_pixel_iterator;
+struct vidl2_component_enc
+{
+  static vxl_byte channel(const vxl_byte * ptr, unsigned int i)
+  {
+    return ptr[i];
+  }
+};
+
+
+VCL_DEFINE_SPECIALIZATION 
+struct vidl2_component_enc<VIDL2_PIXEL_FORMAT_BGR_24>
+{
+  static vxl_byte channel(const vxl_byte * ptr, unsigned int i)
+  {
+    return ptr[2-i];
+  }
+};
+
+
+VCL_DEFINE_SPECIALIZATION 
+struct vidl2_component_enc<VIDL2_PIXEL_FORMAT_RGB_555>
+{
+  static vxl_byte channel(const vxl_byte * ptr, unsigned int i)
+  {
+    const vxl_uint_16* p = reinterpret_cast<const vxl_uint_16*>(ptr);
+    return static_cast<vxl_byte>((*p >> (2-i)*5) & 31)<<3;
+  }
+};
+
+
+VCL_DEFINE_SPECIALIZATION 
+struct vidl2_component_enc<VIDL2_PIXEL_FORMAT_RGB_565>
+{
+  static vxl_byte channel(const vxl_byte * ptr, unsigned int i)
+  {
+    const vxl_uint_16* p = reinterpret_cast<const vxl_uint_16*>(ptr);
+    switch(i){
+      case 0: return vxl_byte((*p & 0xF800) >> 8); // R
+      case 1: return vxl_byte((*p & 0x07E0) >> 3); // G
+      case 2: return vxl_byte((*p & 0x001F) << 3); // B
+    }
+    return 0;
+  }
+};
+
+//-----------------------------------------------------------------------------
+
+
+//: The default pixel iterator
+// used for non-planar non-packed formats 
+template <vidl2_pixel_format FMT> 
+class vidl2_pixel_iterator
+{
+  public:
+    //: Constructor
+    vidl2_pixel_iterator(const vidl2_frame_sptr& frame):
+      frame_(frame), ptr_((vxl_byte*)frame->data())
+    {
+      assert(frame->pixel_format() == FMT);
+      // The following should be a static asserts
+      assert(vidl2_pixel_traits_of<FMT>::bits_per_pixel()%8 == 0);
+      assert(!vidl2_pixel_traits_of<FMT>::planar());
+      assert(!vidl2_pixel_traits_of<FMT>::packed());
+    }
+
+    //: Pre-increment
+    vidl2_pixel_iterator<FMT> & operator++ ()
+    {
+      ptr_ += vidl2_pixel_traits_of<FMT>::bits_per_pixel()/8;
+      return *this;
+    }
+
+    //: Access the data
+    vxl_byte operator () (unsigned int i) const
+    {
+      assert(i<vidl2_pixel_traits_of<FMT>::num_channels());
+      return vidl2_component_enc<FMT>::channel(ptr_,i);
+    }
+
+  private:
+    vidl2_frame_sptr frame_;
+    const vxl_byte * ptr_;
+};
+
 
 //: Iterator for YUV 4:2:2 packed images
 VCL_DEFINE_SPECIALIZATION 
@@ -33,28 +123,19 @@ class vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_YUV_422>
 {
   public:
     //: Constructor
-    vidl2_pixel_iterator(const vxl_byte * buffer):
-      half_step_(false), ptr_(buffer)
+    vidl2_pixel_iterator(const vidl2_frame_sptr& frame):
+      frame_(frame), mode_(0), ptr_((vxl_byte*)frame->data())
     {
-      p_[1] = ptr_[0]; // U
-      p_[0] = ptr_[1]; // Y0
-      p_[2] = ptr_[2]; // V
+      assert(frame->pixel_format() == VIDL2_PIXEL_FORMAT_YUV_422);
+      offset_[0][0] = 1; offset_[0][1] = 0; offset_[0][2] = 2; 
+      offset_[1][0] = 1; offset_[1][1] = -2; offset_[1][2] = 0;
     }
 
     //: Pre-increment
     vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_YUV_422> & operator++ ()
     {
-      if(!half_step_){
-        p_[0] = ptr_[3]; // Y1
-        half_step_ = true;
-      }
-      else{
-        ptr_ += 4;
-        p_[1] = ptr_[0]; // U
-        p_[0] = ptr_[1]; // Y0
-        p_[2] = ptr_[2]; // V
-        half_step_ = false;
-      }
+      mode_ = (mode_+1)%2;
+      ptr_ += 2;
       return *this;
     }
 
@@ -62,13 +143,86 @@ class vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_YUV_422>
     vxl_byte operator () (unsigned int i) const
     {
       assert(i<3);
-      return p_[i];
+      return ptr_[offset_[mode_][i]];
     }
 
   private:
-    bool half_step_;
+    vidl2_frame_sptr frame_;
+    vxl_byte mode_;
     const vxl_byte * ptr_;
-    vxl_byte p_[3];
+    int offset_[2][3];
+};
+
+
+//: Iterator for YUV 4:1:1 packed images
+VCL_DEFINE_SPECIALIZATION 
+    class vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_YUV_411>
+{
+  public:
+    //: Constructor
+    vidl2_pixel_iterator(const vidl2_frame_sptr& frame):
+      frame_(frame), mode_(0), ptr_((vxl_byte*)frame->data())
+      {
+        assert(frame->pixel_format() == VIDL2_PIXEL_FORMAT_YUV_411);
+        offset_[0][0] = 1; offset_[0][1] = 0; offset_[0][2] = 3;
+        offset_[1][0] = 2; offset_[1][1] = 0; offset_[1][2] = 3;
+        offset_[2][0] = 4; offset_[2][1] = 0; offset_[2][2] = 3;
+        offset_[3][0] = 5; offset_[3][1] = 0; offset_[3][2] = 3;
+      }
+
+    //: Pre-increment
+      vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_YUV_411> & operator++ ()
+      {
+        mode_ = (mode_+1)%4;
+        if(mode_==0)
+          ptr_ += 6;
+        return *this;
+      }
+
+    //: Access the data
+      vxl_byte operator () (unsigned int i) const
+      {
+        assert(i<3);
+        return ptr_[offset_[mode_][i]];
+      }
+
+  private:
+    vidl2_frame_sptr frame_;
+    vxl_byte mode_;
+    const vxl_byte * ptr_;
+    int offset_[4][3];
+};
+
+
+//: Iterator for monochrome 16-bit images
+VCL_DEFINE_SPECIALIZATION 
+class vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_MONO_16>
+{
+  public:
+    //: Constructor
+    vidl2_pixel_iterator(const vidl2_frame_sptr& frame):
+      frame_(frame), ptr_((vxl_uint_16*)frame->data())
+    {
+      assert(frame->pixel_format() == VIDL2_PIXEL_FORMAT_MONO_16);
+    }
+
+    //: Pre-increment
+      vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_MONO_16> & operator++ ()
+    {
+      ++ptr_;
+      return *this;
+    }
+
+    //: Access the data
+    vxl_uint_16 operator () (unsigned int i) const
+    {
+      assert(i==0);
+      return *ptr_;
+    }
+
+  private:
+    vidl2_frame_sptr frame_;
+    const vxl_uint_16 * ptr_;
 };
 
 
@@ -78,9 +232,11 @@ class vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_MONO_1>
 {
   public:
     //: Constructor
-    vidl2_pixel_iterator(const vxl_byte * buffer):
-      bit_mask_(128), ptr_(buffer)
-    {}
+    vidl2_pixel_iterator(const vidl2_frame_sptr& frame):
+      frame_(frame), bit_mask_(128), ptr_((vxl_byte*)frame->data())
+    {
+      assert(frame->pixel_format() == VIDL2_PIXEL_FORMAT_MONO_1);
+    }
 
     //: Pre-increment
     vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_MONO_1> & operator++ ()
@@ -102,6 +258,7 @@ class vidl2_pixel_iterator<VIDL2_PIXEL_FORMAT_MONO_1>
     }
 
   private:
+    vidl2_frame_sptr frame_;
     vxl_byte bit_mask_;
     const vxl_byte * ptr_;
 };
