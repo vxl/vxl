e1aaaff454199f23d6858ccfe6d3658c1f9af8bd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-01-17 17:56:50 -0500

    added new header files

--- contrib/brl/bbas/vidl2/tests/test_include.cxx
+++ contrib/brl/bbas/vidl2/tests/test_include.cxx
@@ -1,7 +1,11 @@
+#include <vidl2/vidl2_convert.h>
+#include <vidl2/vidl2_frame.h>
+#include <vidl2/vidl2_frame_sptr.h>
 #include <vidl2/vidl2_pixel_format.h>
+#include <vidl2/vidl2_pixel_iterator.h>
 #include <vidl2/vidl2_istream.h>
-#include <vidl2/vidl2_ostream.h>
 #include <vidl2/vidl2_image_list_istream.h>
+#include <vidl2/vidl2_ostream.h>
 #include <vidl2/vidl2_image_list_ostream.h>
 #include <vidl2/vidl2_iidc1394_params.h>
 #ifdef HAS_FFMPEG
