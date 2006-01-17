dbe1fa6de605d331cf64c33256ebdc99b064a0b3
Matthew J. Leotta <mleotta@users.sourceforge.net>
2006-01-17 17:36:12 -0500

    use convert_to_view_rgb

--- contrib/brl/bbas/vidl2/vidl2_dc1394_istream.cxx
+++ contrib/brl/bbas/vidl2/vidl2_dc1394_istream.cxx
@@ -20,6 +20,8 @@
 
 #include <libdc1394/dc1394_control.h>
 
+#include <vil/vil_convert.h>
+
 
 //--------------------------------------------------------------------------------
 
@@ -53,63 +55,20 @@ struct vidl2_dc1394_istream::pimpl
 
 
 //: extract the image properties from the camera mode.
-// Sets ni_, nj_, np_, istep_, jstep_, pstep_
 void
 vidl2_dc1394_istream::pimpl::init_image()
 {
   unsigned int ni, nj, np;
-  switch (camera_info_->mode)
-  {
-    case DC1394_MODE_160x120_YUV444:
-      ni=160; nj=120; np=3;
-      break;
-    case DC1394_MODE_320x240_YUV422:
-      ni=320; nj=240; np=3;
-      break;
-    case DC1394_MODE_640x480_YUV411:
-    case DC1394_MODE_640x480_YUV422:
-    case DC1394_MODE_640x480_RGB8:
-      ni=640; nj=480; np=3;
-      break;
-    case DC1394_MODE_640x480_MONO8:
-      ni=640; nj=480; np=1;
-      break;
-    case DC1394_MODE_800x600_YUV422:
-    case DC1394_MODE_800x600_RGB8:
-      ni=800; nj=600; np=3;
-      break;
-    case DC1394_MODE_800x600_MONO8:
-      ni=800; nj=600; np=1;
-      break;
-    case DC1394_MODE_1024x768_YUV422:
-    case DC1394_MODE_1024x768_RGB8:
-      ni=1024; nj=768; np=3;
-      break;
-    case DC1394_MODE_1024x768_MONO8:
-      ni=1024; nj=768; np=1;
-      break;
-    case DC1394_MODE_1280x960_YUV422:
-    case DC1394_MODE_1280x960_RGB8:
-      ni=1280; nj=960; np=3;
-      break;
-    case DC1394_MODE_1280x960_MONO8:
-      ni=1280; nj=960; np=1;
-      break;
-    case DC1394_MODE_1600x1200_YUV422:
-    case DC1394_MODE_1600x1200_RGB8:
-      ni=1600; nj=1200; np=3;
-      break;
-    case DC1394_MODE_1600x1200_MONO8:
-      ni=1600; nj=1200; np=1;
-      break;
-    default:
-      vcl_cerr << "camera mode not currently supported by vidl2\n";
+
+  vidl2_iidc1394_params::video_mode_t vm = (vidl2_iidc1394_params::video_mode_t)camera_info_->mode;
+
+  pixel_format_ = vidl2_iidc1394_params::pixel_format(vm);
+  np = vidl2_pixel_format_num_channels(pixel_format_);
+
+  if(vidl2_iidc1394_params::resolution(vm,ni,nj)){
+    cur_img_.set_size(ni, nj, np);
+    vcl_cout << "image size: "<<ni<<'x'<<nj<<'x'<<np<<vcl_endl;
   }
-
-  pixel_format_ = vidl2_iidc1394_params::pixel_format((vidl2_iidc1394_params::video_mode_t)camera_info_->mode);
-
-  cur_img_ = vil_image_view<vxl_byte>(ni, nj, np);
-  vcl_cout << "image size: "<<ni<<'x'<<nj<<'x'<<np<<vcl_endl;
 }
 
 //--------------------------------------------------------------------------------
@@ -363,23 +322,22 @@ vidl2_dc1394_istream::current_frame()
     return NULL;
   }
 
-#if 0
-  is_->cur_img_.set_to_memory((vxl_byte*)is_->camera_info_->capture.capture_buffer,
-                              is_->cur_img_.ni(), is_->cur_img_.nj(), is_->cur_img_.nplanes(),
-                              is_->cur_img_.istep(), is_->cur_img_.jstep(), is_->cur_img_.planestep());
-#endif
 
   if (!is_->cur_img_valid_) {
     vidl2_frame_sptr frame = new vidl2_shared_frame((vxl_byte*)is_->camera_info_->capture.capture_buffer,
                                                     is_->cur_img_.ni(), is_->cur_img_.nj(),
                                                     is_->pixel_format_);
-    vidl2_convert_to_view(frame, is_->cur_img_);
 
-    is_->cur_img_valid_ = true;
+    if(vidl2_convert_to_view_rgb(frame, is_->cur_img_))
+      is_->cur_img_valid_ = true;
+    else
+      vcl_cerr << "unable to convert image" << vcl_endl;
+
   }
 
-  if (is_->cur_img_)
+  if (is_->cur_img_){
     return vil_new_image_resource_of_view(is_->cur_img_);
+  }
   return NULL;
 }
 
