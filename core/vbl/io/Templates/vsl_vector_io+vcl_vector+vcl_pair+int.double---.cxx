522b6dddc178a488360867bcebe0071764e9f126
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-01-17 16:26:06 -0500

    duplicate template instantiation

--- core/vbl/io/Templates/vsl_vector_io+vcl_vector+vcl_pair+int.double---.cxx
+++
@@ -1,6 +0,0 @@
-#include <vcl_utility.txx>
-#include <vsl/vsl_vector_io.txx>
-#include <vsl/vsl_pair_io.txx>
-#include <vbl/io/vbl_io_array_2d.txx>
-typedef vcl_vector<vcl_pair<int,double> > vector_pair_id;
-VBL_IO_ARRAY_2D_INSTANTIATE(vector_pair_id);
