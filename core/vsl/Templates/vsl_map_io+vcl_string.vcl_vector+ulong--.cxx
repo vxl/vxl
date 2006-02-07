5ba42b1e7c24d745c8aab1c021d5516961776aef
Ian Scott <scottim@users.sourceforge.net>
2006-02-07 04:29:51 -0500

    Fix dodgy vsl template instantiations.

--- core/vsl/Templates/vsl_map_io+vcl_string.vcl_vector+ulong--.cxx
+++ core/vsl/Templates/vsl_map_io+vcl_string.vcl_vector+ulong--.cxx
@@ -1,6 +1,8 @@
 // Instantiation of IO for vcl_map<vcl_string,vcl_vector<unsigned long> >
 #include <vsl/vsl_map_io.txx>
-#include <vsl/vsl_vector_io.txx>
+#include <vsl/vsl_vector_io.h>
 #include <vcl_vector.h>
 #include <vcl_string.h>
-VSL_MAP_IO_INSTANTIATE(vcl_string, vcl_vector<unsigned long>, vcl_less<vcl_string>);
+typedef vcl_vector<unsigned long> vec_ulong;
+typedef vcl_less<vcl_string> comp;
+VSL_MAP_IO_INSTANTIATE(vcl_string, vec_ulong, comp);
