989689f44fb84f0f6b64730bbea03980d9bf6db2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-02-24 19:22:45 -0500

    short Doxygen comment should be first line

--- contrib/brl/bmvl/bmvv/bmvv_cal_manager.h
+++ contrib/brl/bmvl/bmvv/bmvv_cal_manager.h
@@ -8,10 +8,10 @@
 // \author
 //   J.L. Mundy
 //
-// Currently works on a single view. Provides interactive tools for 
+// Currently works on a single view. Provides interactive tools for
 // corresponding 3-d points to 2-d image locations. Also provides for
 // the solution of planar camera models (homographies).  The homography can be
-// extended to a vgl_p_matrix by selecting a number of points off the 
+// extended to a vgl_p_matrix by selecting a number of points off the
 // ground plane. In addition there are several simple 3-d modeling tools:
 //      o Backproject a polygon onto a X-Y world plane (specified Z)
 //      o Create a 3-d box from three points on the roof face (specified Z)
@@ -50,12 +50,12 @@ class bmvv_cal_manager : public vgui_wrapper_tableau
  ~bmvv_cal_manager();
   static bmvv_cal_manager *instance();
 
-  //:methods for menu callbacks
+  //: methods for menu callbacks
   void quit();
   void load_image();
   void save_image();
 
-  //:3-d points and polygons representing a world model
+  //: 3-d points and polygons representing a world model
   void load_world();
 
   //: viewing functions
@@ -65,29 +65,30 @@ class bmvv_cal_manager : public vgui_wrapper_tableau
   void clear_selected();
   void load_image_and_camera();
 
-  //:pairs of corresponding 2-d image and 3-d world points
+  //: pairs of corresponding 2-d image and 3-d world points
   void load_corrs();
   void save_corrs();
   void pick_correspondence();
   void remove_correspondence();
 
-  //:world-to-image correspondences and vertical lines for constrained cameras
+  //: world-to-image correspondences and vertical lines for constrained cameras
   void pick_vertical();
   void save_constraints();
 
-  //:camera modeling 
+  //: camera modeling
   void set_identity_camera();
   void solve_camera();
   void compute_ransac_homography();
   void save_camera();
 
-  //:backproject an image onto the world plane
+  //: backproject an image onto the world plane
   void map_image_to_world();
 
   // == modeling functions ==
-  //:set the create XY plane height (Z)
+
+  //: set the create XY plane height (Z)
   void set_height();
-  //:define a polygon on the create plane
+  //: define a polygon on the create plane
   void pick_polygon();
   //: clear all current world information
   void clear_world();
@@ -99,7 +100,7 @@ class bmvv_cal_manager : public vgui_wrapper_tableau
   void save_world_ply2();
   //: read the world information in ply2 format (a single indexed face set)
   void read_world_ply2();
-  //:Convert boxes in a merged indexed face set to individual boxes in VRML
+  //: Convert boxes in a merged indexed face set to individual boxes in VRML
   // (A temporary tool to unmerge files consisting of all boxes)
   void ply2_to_vrml();
   //: Create a box in the world from three top face corner points on the create plane.  The bottom plane is at Z=0.
@@ -114,7 +115,7 @@ class bmvv_cal_manager : public vgui_wrapper_tableau
 
   void load_image_file(vcl_string image_filename, bool greyscale);
  protected:
-  //:internal utility methods
+  //: internal utility methods
   void draw_corr_point(const float x, const float y);
   void draw_vsol_points(const int cam,
                         vcl_vector<vsol_point_2d_sptr> const & points,
@@ -148,7 +149,7 @@ void draw_vsol_polygon_3d(vsol_polygon_3d_sptr const & poly,
   void clear_correspondences();
   void project_world();
   int  get_selected_proj_world_pt(vgl_point_2d<double>&  pt);
-  vgui_range_map_params_sptr 
+  vgui_range_map_params_sptr
     range_params(vil_image_resource_sptr const& image);
  private:
   vil_image_resource_sptr img_;
@@ -163,21 +164,21 @@ void draw_vsol_polygon_3d(vsol_polygon_3d_sptr const & poly,
   vcl_vector<vgl_point_2d<double> >  proj_image_pts_;
   vcl_vector<vgui_soview2D_point* >  corr_sovs_;
   vcl_vector<vgl_point_3d<double> > world_;
-  //:the current camera
+  //: the current camera
   vgl_p_matrix<double> cam_;
-  //:the previous camera before the current solution
+  //: the previous camera before the current solution
   vgl_p_matrix<double> prev_cam_;
-  //:lines in the image corresponding to the vertical direction
+  //: lines in the image corresponding to the vertical direction
   vcl_vector<vgl_line_segment_2d<double> > verticals_;
-  //:the Z height of the create plane
+  //: the Z height of the create plane
   double height_;
-  //:the polygon just created
+  //: the polygon just created
   vsol_polygon_3d_sptr new_poly_;
-  //:all the polygons in the world model
+  //: all the polygons in the world model
   vcl_vector<vsol_polygon_3d_sptr> polys_;
-  //:the 2-d projected form of the polygons
+  //: the 2-d projected form of the polygons
   vcl_vector<vsol_polygon_2d_sptr> proj_polys_;
-  //:an indexed face set for all the polygons in the world
+  //: an indexed face set for all the polygons in the world
   vcl_vector<vcl_vector<unsigned> > indexed_face_set_;
 };
 
