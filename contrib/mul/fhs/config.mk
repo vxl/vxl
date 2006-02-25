a618ee80c84380bd4e4db76a47598269b23bb0fb
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-02-25 11:58:52 -0500

    updated w.r.t. CMakeLists.txt

---
+++ contrib/mul/fhs/config.mk
@@ -0,0 +1,6 @@
+IULIBS += -lmbl
+include ${VXLROOT}/contrib/mul/vimt/config.mk
+include ${VXLROOT}/core/vnl/config.mk
+include ${VXLROOT}/core/vil/algo/config.mk
+include ${VXLROOT}/core/vil/config.mk
+include ${VXLROOT}/core/vsl/config.mk
