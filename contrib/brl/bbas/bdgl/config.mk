50f5e16f7a105528f0edc586ee01c7593f5768dd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-02-26 17:34:02 -0500

    updated w.r.t. CMakeLists.txt

--- contrib/brl/bbas/bdgl/config.mk
+++ contrib/brl/bbas/bdgl/config.mk
@@ -1,6 +1,5 @@
 IULIBS += -lbdgl
 USES += contrib/gel
-include ${VXLROOT}/contrib/brl/bbas/btol/config.mk
 include ${VXLROOT}/contrib/gel/vdgl/config.mk
 include ${VXLROOT}/contrib/gel/vsol/config.mk
 include ${VXLROOT}/core/vgl/algo/config.mk
