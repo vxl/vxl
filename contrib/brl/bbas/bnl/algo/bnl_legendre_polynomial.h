39db389c223a56d90f61f5b9b07eb2ecef99c58c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-02-26 17:40:38 -0500

    typo corrected

--- contrib/brl/bbas/bnl/algo/bnl_legendre_polynomial.h
+++ contrib/brl/bbas/bnl/algo/bnl_legendre_polynomial.h
@@ -2,13 +2,13 @@
 #define BNL_ALGO_LEGENDRE_POLYNOMIAL_H_
 //:
 //\file
-//\brief Evalue a real Legendre polynomail by recursive equation
+//\brief Evalue a real Legendre polynomial by recursive equation
 //\author Kongbin Kang, Brown University
 //\date Dec. 21th, 2004.
 
 #include <vnl/vnl_real_polynomial.h>
 
-//: generate a n-th degree legendre polynomail
+//: generate a n-th degree legendre polynomial
 vnl_real_polynomial bnl_legendre_polynomial(int n);
 
 #endif
