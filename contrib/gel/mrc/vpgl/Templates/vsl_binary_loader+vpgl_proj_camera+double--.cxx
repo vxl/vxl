4f3e670f59aa86c31c06bb53d5c2bf22eccfeb40
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-03-12 16:14:27 -0500

    split off from vpgl_proj_comera+double-

---
+++ contrib/gel/mrc/vpgl/Templates/vsl_binary_loader+vpgl_proj_camera+double--.cxx
@@ -0,0 +1,4 @@
+// Instantiation of vsl_binary_loader<vpgl_proj_camera<double> >
+#include <vpgl/vpgl_proj_camera.h>
+#include <vsl/vsl_binary_loader.txx>
+VSL_BINARY_LOADER_INSTANTIATE(vpgl_proj_camera<double>);
