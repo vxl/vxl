c7c1730453daef423c4026b4ed5973ccca03dc4b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-03-12 16:05:07 -0500

    moved instantiation of binary_loader to separate file

--- contrib/gel/mrc/vpgl/Templates/vpgl_proj_camera+double-.cxx
+++ contrib/gel/mrc/vpgl/Templates/vpgl_proj_camera+double-.cxx
@@ -1,6 +1,3 @@
 // Instantiation of vpgl_proj_camera<double>
 #include <vpgl/vpgl_proj_camera.txx>
 vpgl_PROJ_CAMERA_INSTANTIATE(double);
-
-#include <vsl/vsl_binary_loader.txx>
-VSL_BINARY_LOADER_INSTANTIATE(vpgl_proj_camera<double>);
