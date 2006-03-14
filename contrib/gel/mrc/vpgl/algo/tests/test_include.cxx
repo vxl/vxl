db36ce83d07760f6d6362bd162385b6e15cc5ca4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-03-14 02:06:49 -0500

    added test_include

---
+++ contrib/gel/mrc/vpgl/algo/tests/test_include.cxx
@@ -0,0 +1,13 @@
+#include <vpgl/algo/vpgl_camera_compute.h>
+#include <vpgl/algo/vpgl_construct_cameras.h>
+#include <vpgl/algo/vpgl_fm_compute_7_point.h>
+#include <vpgl/algo/vpgl_fm_compute_8_point.h>
+#include <vpgl/algo/vpgl_fm_compute_affine_ransac.h>
+#include <vpgl/algo/vpgl_fm_compute_ransac.h>
+#include <vpgl/algo/vpgl_fm_compute_reg_ransac.h>
+#include <vpgl/algo/vpgl_lens_warp_mapper.h>
+#include <vpgl/algo/vpgl_list.h>
+#include <vpgl/algo/vpgl_optimize_camera.h>
+#include <vpgl/algo/vpgl_project.h>
+#include <vpgl/algo/vpgl_vsol_lens_warp.h>
+int main() { return 0; }
---
+++ contrib/gel/mrc/vpgl/tests/test_include.cxx
@@ -0,0 +1,13 @@
+#include <vpgl/vgl_rotation_3d.h>
+#include <vpgl/vpgl_affine_camera.h>
+#include <vpgl/vpgl_affine_fundamental_matrix.h>
+#include <vpgl/vpgl_calibration_matrix.h>
+#include <vpgl/vpgl_camera.h>
+#include <vpgl/vpgl_fundamental_matrix.h>
+#include <vpgl/vpgl_lens_distortion.h>
+#include <vpgl/vpgl_perspective_camera.h>
+#include <vpgl/vpgl_poly_radial_distortion.h>
+#include <vpgl/vpgl_proj_camera.h>
+#include <vpgl/vpgl_radial_distortion.h>
+#include <vpgl/vpgl_reg_fundamental_matrix.h>
+int main() { return 0; }
