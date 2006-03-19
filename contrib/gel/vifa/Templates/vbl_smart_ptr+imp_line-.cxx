55251b03015f4d6aa98fc59e0e4210de583ebe42
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-03-19 08:22:18 -0500

    renamed from vbl_smart_ptr+imp_line.cxx to vbl_smart_ptr+vifa_imp_line_double--.cxx

--- contrib/gel/vifa/Templates/vbl_smart_ptr+imp_line-.cxx
+++
@@ -1,5 +0,0 @@
-#include <vifa/vifa_typedefs.h>
-#include <vsol/vsol_box_2d.h>
-#include <vbl/vbl_smart_ptr.txx>
-
-VBL_SMART_PTR_INSTANTIATE(imp_line);
---
+++ contrib/gel/vifa/Templates/vbl_smart_ptr+vifa_imp_line+double--.cxx
@@ -0,0 +1,4 @@
+#include <vifa/vifa_imp_line.h>
+#include <vbl/vbl_smart_ptr.txx>
+
+VBL_SMART_PTR_INSTANTIATE(vifa_imp_line<double>);
