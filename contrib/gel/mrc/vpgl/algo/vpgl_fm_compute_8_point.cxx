13e63ee5177f8c2b97a72426582c0263ae5a8f1c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-03-18 20:09:31 -0500

    avoid compiler warning (comparison signed/unsigned)

--- contrib/gel/mrc/vpgl/algo/vpgl_fm_compute_8_point.cxx
+++ contrib/gel/mrc/vpgl/algo/vpgl_fm_compute_8_point.cxx
@@ -1,36 +1,37 @@
 // This is gel/mrc/vpgl/algo/vpgl_fm_compute_8_point.cxx
-#ifndef _vpgl_fm_compute_8_point_cxx_
-#define _vpgl_fm_compute_8_point_cxx_
+#ifndef vpgl_fm_compute_8_point_cxx_
+#define vpgl_fm_compute_8_point_cxx_
 //:
 // \file
 
-#include <vnl/vnl_fwd.h>
-#include <vnl/vnl_math.h>
+#include "vpgl_fm_compute_8_point.h"
+
+#include <vnl/vnl_vector.h>
+#include <vnl/vnl_matrix.h>
+#include <vnl/vnl_matrix_fixed.h>
 #include <vnl/algo/vnl_svd.h>
 #include <vgl/vgl_fwd.h>
 #include <vgl/algo/vgl_norm_trans_2d.h>
 #include <vcl_iostream.h>
 #include <vcl_cassert.h>
 
-#include "vpgl_fm_compute_8_point.h"
-
 
 //-------------------------------------------
-bool 
-vpgl_fm_compute_8_point::compute( 
+bool
+vpgl_fm_compute_8_point::compute(
   const vcl_vector< vgl_homg_point_2d<double> >& pr,
   const vcl_vector< vgl_homg_point_2d<double> >& pl,
   vpgl_fundamental_matrix<double>& fm )
 {
   // Check that there are at least 8 points.
-  if( pr.size() < 8 || pl.size() < 8 ){
+  if ( pr.size() < 8 || pl.size() < 8 ){
     vcl_cerr << "vpgl_fm_compute_8_point: Need at least 8 point pairs.\n"
-    << "Number in each set: " << pr.size() << ", " << pl.size() << vcl_endl;
+             << "Number in each set: " << pr.size() << ", " << pl.size() << vcl_endl;
     return false;
   }
 
   // Check that the correspondence lists are the same size.
-  if( pr.size() != pl.size() ){
+  if ( pr.size() != pl.size() ){
     vcl_cerr << "vpgl_fm_compute_7_point: Need correspondence lists of same size.\n";
     return false;
   }
@@ -38,16 +39,16 @@ vpgl_fm_compute_8_point::compute(
   // Condition if necessary.
   vcl_vector< vgl_homg_point_2d<double> > pr_norm, pl_norm;
   vgl_norm_trans_2d<double> prnt, plnt;
-  if( precondition_ ){
+  if ( precondition_ ){
     prnt.compute_from_points(pr);
     plnt.compute_from_points(pl);
-    for( unsigned i = 0; i < pl.size(); i++ ){
+    for ( unsigned int i = 0; i < pl.size(); i++ ){
       pr_norm.push_back( prnt*pr[i] );
       pl_norm.push_back( plnt*pl[i] );
     }
   }
   else{
-    for( unsigned i = 0; i < pl.size(); i++ ){
+    for ( unsigned int i = 0; i < pl.size(); i++ ){
       pr_norm.push_back( pr[i] );
       pl_norm.push_back( pl[i] );
     }
@@ -55,7 +56,7 @@ vpgl_fm_compute_8_point::compute(
 
   // Solve!
   vnl_matrix<double> S(8,9);
-  for( int i = 0; i < pr_norm.size(); i++ ){
+  for ( unsigned int i = 0; i < pr_norm.size(); i++ ){
     S(i,0) = pl_norm[i].x()*pr_norm[i].x();
     S(i,1) = pl_norm[i].x()*pr_norm[i].y();
     S(i,2) = pl_norm[i].x()*pr_norm[i].w();
@@ -72,7 +73,7 @@ vpgl_fm_compute_8_point::compute(
   F_vnl(0,0) = solution(0); F_vnl(0,1) = solution(1); F_vnl(0,2) = solution(2);
   F_vnl(1,0) = solution(3); F_vnl(1,1) = solution(4); F_vnl(1,2) = solution(5);
   F_vnl(2,0) = solution(6); F_vnl(2,1) = solution(7); F_vnl(2,2) = solution(8);
-  if( precondition_ )
+  if ( precondition_ )
     fm.set_matrix( plnt.get_matrix().transpose()*F_vnl*prnt.get_matrix() );
   else
     fm.set_matrix( F_vnl );
@@ -80,4 +81,4 @@ vpgl_fm_compute_8_point::compute(
 };
 
 
-#endif //_vpgl_fm_compute_8_point_cxx_
+#endif //vpgl_fm_compute_8_point_cxx_
