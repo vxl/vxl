2fa077e7be7dd8e8d3f1f77811fe92c9721edaed
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-03-19 10:59:20 -0500

    comment corrected

--- core/vbl/io/Templates/vbl_io_bounding_box+double.2-.cxx
+++ core/vbl/io/Templates/vbl_io_bounding_box+double.2-.cxx
@@ -1,4 +1,4 @@
-// Instantiation of IO for vbl_bounding_box<double>
+// Instantiation of IO for vbl_bounding_box<double,2>
 #include <vbl/io/vbl_io_bounding_box.txx>
 
 VBL_IO_BOUNDING_BOX_INSTANTIATE(double, 2);
