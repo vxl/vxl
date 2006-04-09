81f579af27bb8d01f809a0b62e6890b80e0e2347
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-04-09 04:19:30 -0400

    added #include <cmath>, necessary for std::ceil()

--- core/vil/file_formats/vil_j2k_image.cxx
+++ core/vil/file_formats/vil_j2k_image.cxx
@@ -1,5 +1,5 @@
 // vil_j2k: Written by Rob Radtke (rob@) and Harry Voorhees (hlv@) of
-// Stellar Science Ltd. Co. (stellarscience.com) for 
+// Stellar Science Ltd. Co. (stellarscience.com) for
 // Air Force Research Laboratory, 2005.
 
 #include "vil_j2k_image.h"
@@ -8,6 +8,7 @@
 
 #include <vcl_algorithm.h>
 #include <vcl_cassert.h>
+#include <vcl_cmath.h> // for ceil()
 #include <vcl_limits.h>
 #include <vil/vil_memory_chunk.h>
 #include <vil/vil_image_view.h>
@@ -63,7 +64,7 @@ NCSEcwCellType convertType( const vil_pixel_format& vilType )
   case VIL_PIXEL_FORMAT_COMPLEX_FLOAT:
   case VIL_PIXEL_FORMAT_COMPLEX_DOUBLE:
   case VIL_PIXEL_FORMAT_UNKNOWN:
-  default: 
+  default:
     assert( 0 );
     return NCSCT_UINT8;
   }
@@ -82,7 +83,7 @@ vil_pixel_format convertType( const NCSEcwCellType& ecwType )
   case NCSCT_INT8: return VIL_PIXEL_FORMAT_SBYTE;
   case NCSCT_IEEE4: return VIL_PIXEL_FORMAT_FLOAT;
   case NCSCT_IEEE8: return VIL_PIXEL_FORMAT_DOUBLE;
-  default: 
+  default:
     assert( 0 );
     return VIL_PIXEL_FORMAT_UNKNOWN;
   }
@@ -100,7 +101,7 @@ vil_j2k_image::vil_j2k_image( const vcl_string& fileOrUrl )
     mRemoteFile( false )
 {
   mFileResource = new CNCSFile();
-  if ( mFileResource->Open( (char*)fileOrUrl.c_str(), false, false ) != NCS_SUCCESS ){ 
+  if ( mFileResource->Open( (char*)fileOrUrl.c_str(), false, false ) != NCS_SUCCESS ) {
     mFileResource = 0;
     return;
   }
@@ -121,8 +122,8 @@ vil_j2k_image::vil_j2k_image( vil_stream* is )
   mFileResource = new CNCSFile();
   CNCSJPCVilIOStream* cstr = new CNCSJPCVilIOStream();
   cstr->Open( is );
-  
-  if ( (static_cast<CNCSJP2FileView*>(mFileResource))->Open( cstr ) != NCS_SUCCESS ){ 
+
+  if ( (static_cast<CNCSJP2FileView*>(mFileResource))->Open( cstr ) != NCS_SUCCESS ) {
     mFileResource = 0;
     return;
   }
@@ -159,11 +160,11 @@ enum vil_pixel_format vil_j2k_image::pixel_format() const
   return convertType( mFileResource->GetFileInfo()->eCellType );
 }
 
-vil_image_view_base_sptr vil_j2k_image::get_copy_view_decimated(unsigned sample0, 
+vil_image_view_base_sptr vil_j2k_image::get_copy_view_decimated(unsigned sample0,
                                                                 unsigned num_samples,
-                                                                unsigned line0, 
+                                                                unsigned line0,
                                                                 unsigned numLines,
-                                                                double i_factor, 
+                                                                double i_factor,
                                                                 double j_factor) const
 {
   if ( !( mFileResource ) ||
@@ -177,11 +178,9 @@ vil_image_view_base_sptr vil_j2k_image::get_copy_view_decimated(unsigned sample0
   //eg. bandMap = {0,1,2,3...nBands}
   INT32 nBands = nplanes();
   INT32* bandMap = (INT32*) malloc(sizeof(UINT32) * nBands );
-  for( int i = 0 ; i < nBands ; i++ ){
-    bandMap[i] = i;
-  }
+  for ( int i = 0 ; i < nBands ; i++ ) { bandMap[i] = i; }
 
-  //this guards us from returning an image that is too big for the computer's memory 
+  //this guards us from returning an image that is too big for the computer's memory
   //(or would take too long to download in the remote case).
   //We don't want infinite hangs or application crashes.
   unsigned int maxDim = mRemoteFile ? mMaxRemoteDimension : mMaxLocalDimension;
@@ -198,7 +197,7 @@ vil_image_view_base_sptr vil_j2k_image::get_copy_view_decimated(unsigned sample0
   //note that we don't want ECW to do any scaling for us.  That's why
   //the box created by (sample0,line0) and (sample0+num_samples-1,line0+numLines-1) is made to be exactly num_samplesXnumLines.
 
-  NCSError setViewError = mFileResource->SetView( nBands, bandMap, output_width, output_height, 
+  NCSError setViewError = mFileResource->SetView( nBands, bandMap, output_width, output_height,
                                                   (INT32)sample0, (INT32)line0, (INT32)(sample0+num_samples-1), (INT32)(line0+numLines-1) );
   if ( setViewError != NCS_SUCCESS ){
     free( bandMap );
@@ -207,7 +206,7 @@ vil_image_view_base_sptr vil_j2k_image::get_copy_view_decimated(unsigned sample0
 
   //number of samples times the bytes per sample in each band
   double bitsPerSample                 = mFileResource->GetFileInfo()->pBands[0].nBits;
-  unsigned int bytesPerSample          = (unsigned int) ceil( bitsPerSample / 8.0 );
+  unsigned int bytesPerSample          = (unsigned int) vcl_ceil( bitsPerSample / 8.0 );
   unsigned int singleBandLineSizeBytes = output_width * bytesPerSample;
   unsigned int allBandLineSizeBytes    = singleBandLineSizeBytes * nBands;
   unsigned int dataPtrSizeBytes        = allBandLineSizeBytes * output_height;
@@ -215,8 +214,8 @@ vil_image_view_base_sptr vil_j2k_image::get_copy_view_decimated(unsigned sample0
   vil_memory_chunk_sptr data_ptr = new vil_memory_chunk( dataPtrSizeBytes, convertType( mFileResource->GetFileInfo()->eCellType ) );
   void** linePtrPtr = (void**)malloc( nBands * sizeof( int* /*all pointers have same size, so eg char* would work too*/ ) );
   //now read all the lines that we want
-  for( unsigned int currLine = 0 ; currLine < output_height ; currLine++ ){
-    for(int currBand = 0 ; currBand < nBands ; currBand++ ){
+  for ( unsigned int currLine = 0 ; currLine < output_height ; currLine++ ){
+    for (int currBand = 0 ; currBand < nBands ; currBand++ ){
       linePtrPtr[currBand] = (void*) ( ((char*)data_ptr->data()) + currLine * allBandLineSizeBytes + currBand * singleBandLineSizeBytes );
     }
     NCSEcwReadStatus readStatus = mFileResource->ReadLineBIL( mFileResource->GetFileInfo()->eCellType, nBands, linePtrPtr, 0);
@@ -233,25 +232,26 @@ vil_image_view_base_sptr vil_j2k_image::get_copy_view_decimated(unsigned sample0
 
   vil_image_view_base_sptr view = 0;
   //now create our vil_image_view
-  switch( vil_pixel_format_component_format( data_ptr->pixel_format() ) ){
+  switch ( vil_pixel_format_component_format( data_ptr->pixel_format() ) )
+  {
 #define macro( F, T ) \
   case F: \
     view = new vil_image_view< T > ( data_ptr, reinterpret_cast<T*>(data_ptr->data()), \
                                      output_width, output_height, nBands, 1, output_width*nBands, output_width); \
-    break;
-macro(VIL_PIXEL_FORMAT_BYTE , vxl_byte )
-macro(VIL_PIXEL_FORMAT_SBYTE , vxl_sbyte )
-macro(VIL_PIXEL_FORMAT_UINT_64 , vxl_uint_64 )
-macro(VIL_PIXEL_FORMAT_INT_64 , vxl_int_64 )
-macro(VIL_PIXEL_FORMAT_UINT_32 , vxl_uint_32 )
-macro(VIL_PIXEL_FORMAT_INT_32 , vxl_int_32 )
-macro(VIL_PIXEL_FORMAT_UINT_16 , vxl_uint_16 )
-macro(VIL_PIXEL_FORMAT_INT_16 , vxl_int_16 )
-macro(VIL_PIXEL_FORMAT_BOOL , bool )
-macro(VIL_PIXEL_FORMAT_FLOAT , float )
-macro(VIL_PIXEL_FORMAT_DOUBLE , double )
+    break
+  macro(VIL_PIXEL_FORMAT_BYTE , vxl_byte );
+  macro(VIL_PIXEL_FORMAT_SBYTE , vxl_sbyte );
+  macro(VIL_PIXEL_FORMAT_UINT_64 , vxl_uint_64 );
+  macro(VIL_PIXEL_FORMAT_INT_64 , vxl_int_64 );
+  macro(VIL_PIXEL_FORMAT_UINT_32 , vxl_uint_32 );
+  macro(VIL_PIXEL_FORMAT_INT_32 , vxl_int_32 );
+  macro(VIL_PIXEL_FORMAT_UINT_16 , vxl_uint_16 );
+  macro(VIL_PIXEL_FORMAT_INT_16 , vxl_int_16 );
+  macro(VIL_PIXEL_FORMAT_BOOL , bool );
+  macro(VIL_PIXEL_FORMAT_FLOAT , float );
+  macro(VIL_PIXEL_FORMAT_DOUBLE , double );
 #undef macro
-  default: 
+  default:
     assert( 0 );
     //"Unknown vil data type." );
     break;
@@ -261,9 +261,9 @@ macro(VIL_PIXEL_FORMAT_DOUBLE , double )
 }
 
 
-vil_image_view_base_sptr vil_j2k_image::get_copy_view(unsigned sample0, 
+vil_image_view_base_sptr vil_j2k_image::get_copy_view(unsigned sample0,
                                                       unsigned num_samples,
-                                                      unsigned line0,   
+                                                      unsigned line0,
                                                       unsigned numLines ) const
 {
   return get_copy_view_decimated( sample0, num_samples, line0, numLines, 1.0, 1.0 );
@@ -275,16 +275,16 @@ void vil_j2k_image::unsetMaxImageDimension( bool remote )
   setMaxImageDimension( vcl_numeric_limits< unsigned int >::max(), remote );
 }
 
-void vil_j2k_image::setMaxImageDimension( unsigned int widthOrHeight, bool remote ) 
-{ 
+void vil_j2k_image::setMaxImageDimension( unsigned int widthOrHeight, bool remote )
+{
   if ( remote ) {
-    mMaxRemoteDimension = widthOrHeight; 
+    mMaxRemoteDimension = widthOrHeight;
   } else {
-    mMaxLocalDimension = widthOrHeight; 
+    mMaxLocalDimension = widthOrHeight;
   }
 }
 
-vil_image_view_base_sptr vil_j2k_image::s_decode_jpeg_2000( vil_stream* vs, 
+vil_image_view_base_sptr vil_j2k_image::s_decode_jpeg_2000( vil_stream* vs,
                                                             unsigned i0, unsigned ni,
                                                             unsigned j0, unsigned nj,
                                                             double i_factor, double j_factor )
