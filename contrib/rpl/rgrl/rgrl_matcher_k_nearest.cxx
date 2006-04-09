2ddac5ef5b51866ec006b8ef5b66d2dad8245e74
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-04-09 05:57:24 -0400

    avoid compiler warning (unused variable "scale")

--- contrib/rpl/rgrl/rgrl_matcher_k_nearest.cxx
+++ contrib/rpl/rgrl/rgrl_matcher_k_nearest.cxx
@@ -13,9 +13,10 @@
 #include <vcl_algorithm.h>
 
 
-bool 
+bool
 rgrl_matcher_k_nearest::internal_dist_node::
-operator<( internal_dist_node const& rhs ) const {
+operator<( internal_dist_node const& rhs ) const
+{
   return this->geo_err_ < rhs.geo_err_;
 }
 
@@ -32,7 +33,7 @@ rgrl_matcher_k_nearest( unsigned int k, double dist_thres )
   : k_( k ),
     thres_( dist_thres )
 {
-  if( thres_ > 0.0 )  thres_ = thres_*thres_;
+  if ( thres_ > 0.0 )  thres_ = thres_*thres_;
 }
 
 
@@ -47,7 +48,7 @@ compute_matches( rgrl_feature_set const&       from_set,
   typedef vcl_vector<rgrl_feature_sptr> feat_vector;
   typedef feat_vector::iterator feat_iter;
 
-  rgrl_match_set_sptr matches_sptr 
+  rgrl_match_set_sptr matches_sptr
     = new rgrl_match_set( from_set.type(), to_set.type(), from_set.label(), to_set.label() );
 
   //  get the features in the current view
@@ -58,12 +59,12 @@ compute_matches( rgrl_feature_set const&       from_set,
   matches_sptr->reserve( from.size() );
 
   //  generate the matches for each feature of this feature type in the current region
-  for ( feat_iter fitr = from.begin(); fitr != from.end(); ++fitr ) {
-    
+  for ( feat_iter fitr = from.begin(); fitr != from.end(); ++fitr )
+  {
     rgrl_feature_sptr mapped = (*fitr)->transform( current_xform );
-    if( !validate( mapped, current_view.to_image_roi() ) )
+    if ( !validate( mapped, current_view.to_image_roi() ) )
       continue;   // feature is invalid
-      
+
     feat_vector matching_features = to_set.k_nearest_features( mapped, k_ );
 
     // prune the matches to satisfy the threshold
@@ -89,7 +90,6 @@ compute_matches( rgrl_feature_set const&       from_set,
 }
 
 
-
 //  It is to restrict the number of nearest neighbors during the inversion.
 void
 rgrl_matcher_k_nearest::
@@ -105,7 +105,7 @@ add_one_flipped_match( rgrl_match_set_sptr&      inv_set,
   rgrl_feature_sptr from = begin_iter->to_;
   rgrl_feature_sptr mapped = from->transform( *inverse_xform );
 
-  if( !validate( mapped, current_view.from_image_roi() ) )
+  if ( !validate( mapped, current_view.from_image_roi() ) )
     return;
 
   // compute the distance
@@ -113,73 +113,74 @@ add_one_flipped_match( rgrl_match_set_sptr&      inv_set,
   //
   vcl_vector< internal_dist_node > dist_nodes;
   dist_nodes.reserve( size );
-  for ( nodes_vec_iterator itr = begin_iter; itr!=end_iter; ++itr ) {
-    
+  for ( nodes_vec_iterator itr = begin_iter; itr!=end_iter; ++itr )
+  {
     internal_dist_node tmp_node;
-    
+
     // NOTE: should not use rgrl_feature::geometric_error() function,
     //       because it may compute normal distance, which is not desired
     //
     tmp_node.geo_err_ = vnl_vector_ssd( itr->from_->location(), mapped->location() );
     tmp_node.itr_ = itr;
-    
+
     // push back
     dist_nodes.push_back( tmp_node );
   }
 
   // Kth element based on distance
-  if( size > k_ )
+  if ( size > k_ )
     vcl_nth_element( dist_nodes.begin(), dist_nodes.begin()+k_, dist_nodes.end() );
-  
+
   // setup structure
   vcl_vector< rgrl_feature_sptr > matching_tos;
   vcl_vector< double >            sig_wgts;
   matching_tos.reserve( k_ );
   sig_wgts.reserve( k_ );
 
-  for( unsigned i=0; i<k_ && i<size; ++i ) {
-    
-    if( thres_ > 0 && dist_nodes[i].geo_err_ > thres_ )
+  for ( unsigned i=0; i<k_ && i<size; ++i )
+  {
+    if ( thres_ > 0 && dist_nodes[i].geo_err_ > thres_ )
       continue;
-      
+
     nodes_vec_iterator const& current_node = dist_nodes[i].itr_;
     matching_tos.push_back( current_node->from_ );
-    
+
     // recompute the signature weight
-    // 
+    //
     // double wgt = current_node->sig_wgt_;
     double wgt = current_node->from_->absolute_signature_weight( mapped );
     sig_wgts.push_back( wgt );
     //sig_wgts.push_back( current_node.itr_->sig_wgt_ );
   }
-      
+
   // add matches
-  if( ! matching_tos.empty() )
+  if ( ! matching_tos.empty() )
     inv_set->add_feature_matches_and_weights( from, mapped, matching_tos, sig_wgts );
 }
 
 
-bool 
+bool
 rgrl_matcher_k_nearest::
 validate( rgrl_feature_sptr const& mapped, rgrl_mask_sptr const& roi_sptr ) const
 {
   // if the mapped point is not in the image
-  if( !roi_sptr->inside( mapped->location() ) )
+  if ( !roi_sptr->inside( mapped->location() ) )
     return false;
-    
+
   // Suppose scale=1 is the lowest scale, or the finest resolution
   // Any mapped scale below 1 cannot find any correspondence
-  // due to the pixel discrtetization. 
-  // In practice, the threshold is a number less than one, 
+  // due to the pixel discretization.
+  // In practice, the threshold is a number less than one,
   // as feature of real scale=0.9 is still likely to be detected.
-  
-  
+
+#if 0
   // if the scale is too small to be detected on the other image
   const double scale = mapped->scale();
-  //if( scale && scale<0.4 ) {
-  //  return false;
-  //}
-  
-  // by default, 
+  if ( scale!=0 && scale<0.4 ) {
+    return false;
+  }
+#endif // 0
+
+  // by default,
   return true;
 }
