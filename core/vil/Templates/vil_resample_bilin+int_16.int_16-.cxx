ae9f470b0b2ece69bba98fccd6230dff29400283
Gehua Yang <norwegian_wood@users.sourceforge.net>
2006-05-03 17:20:17 -0400

    added instantiation

---
+++ core/vil/Templates/vil_resample_bilin+int_16.int_16-.cxx
@@ -0,0 +1,3 @@
+#include <vil/vil_resample_bilin.txx>
+#include <vxl_config.h>
+VIL_RESAMPLE_BILIN_INSTANTIATE( vxl_int_16 , vxl_int_16 );
---
+++ core/vil/Templates/vil_resample_bilin+uint_16.uint_16-.cxx
@@ -0,0 +1,3 @@
+#include <vil/vil_resample_bilin.txx>
+#include <vxl_config.h>
+VIL_RESAMPLE_BILIN_INSTANTIATE( vxl_uint_16 , vxl_uint_16 );
