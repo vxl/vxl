6034c191ee0253c9909025e4426be0b5115f6d35
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-05-18 07:26:39 -0400

    added new header file

--- core/vgl/tests/test_include.cxx
+++ core/vgl/tests/test_include.cxx
@@ -60,6 +60,7 @@
 #include <vgl/algo/vgl_line_2d_regression.h>
 #include <vgl/algo/vgl_norm_trans_2d.h>
 #include <vgl/algo/vgl_norm_trans_3d.h>
+#include <vgl/algo/vgl_orient_box_3d.h>
 #include <vgl/algo/vgl_p_matrix.h>
 #include <vgl/algo/vgl_rtree.h>
 
