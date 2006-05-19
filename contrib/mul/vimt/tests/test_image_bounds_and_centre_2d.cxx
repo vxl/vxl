be6b37e1035c67c8287c20a33af4098443033dfb
Kevin de Souza <kdesouza@users.sourceforge.net>
2006-05-19 04:20:29 -0400

    Enhanced test to check images with non-identity transform. Made tests more consistent.

--- contrib/mul/vimt/tests/test_image_bounds_and_centre_2d.cxx
+++ contrib/mul/vimt/tests/test_image_bounds_and_centre_2d.cxx
@@ -8,25 +8,91 @@
 
 static void test_world_bounding_box()
 {
-  vimt_image_2d_of<int> image(10, 10, 1);
-  vgl_box_2d<double> bbox = world_bounding_box(image);
-  vgl_point_2d<double> min_pt = bbox.min_point();
-  vgl_point_2d<double> max_pt = bbox.max_point();
-  bool ok = (min_pt==vgl_point_2d<double>(0,0) &&
-             max_pt==vgl_point_2d<double>(9,9));
-  TEST("world_bounding_box()", ok, true);
+  vcl_cout << "--------------------------------------------------\n";
+  vcl_cout << "test_world_bounding_box(): \n";
+  vcl_cout << "--------------------------------------------------\n";
+
+  // Image with identity transform
+  {
+    const unsigned ni = 8;  // image width (pixels)
+    const unsigned nj = 9;  // image height (pixels)
+    const unsigned np = 1;
+    const vgl_point_2d<double> pix(1.0, 1.0); // pixel dimensions (mm)
+    vimt_transform_2d w2i;
+    w2i.set_zoom_only(1.0/pix.x(), 1.0/pix.y(), 0, 0);
+    const vimt_image_2d_of<int> image(ni, nj, np, w2i);
+
+    const vgl_box_2d<double> bbox = world_bounding_box(image);
+    const vgl_point_2d<double> min_pt(0.0, 0.0);
+    const vgl_point_2d<double> max_pt((ni-1)*pix.x(), (nj-1)*pix.y());
+    bool ok = (bbox.min_point()==min_pt && bbox.max_point()==max_pt);
+    TEST("With identity transform", ok, true);
+  }
+  
+  // Image with zoom transform (i.e. non-unity pixel size)
+  {
+    const unsigned ni = 8;  // image width (pixels)
+    const unsigned nj = 9;  // image height (pixels)
+    const unsigned np = 1;
+    const vgl_point_2d<double> pix(0.7, 1.2); // pixel dimensions (mm)
+    vimt_transform_2d w2i;
+    w2i.set_zoom_only(1.0/pix.x(), 1.0/pix.y(), 0, 0);
+    const vimt_image_2d_of<int> image(ni, nj, np, w2i);
+
+    const vgl_box_2d<double> bbox = world_bounding_box(image);
+    const vgl_point_2d<double> min_pt(0.0, 0.0);
+    const vgl_point_2d<double> max_pt((ni-1)*pix.x(), (nj-1)*pix.y());
+    bool ok = (bbox.min_point()==min_pt && bbox.max_point()==max_pt);
+    TEST("With zoom transform (i.e. non-unity pixels)", ok, true);
+  }
 }
 
 
 static void test_centre_image_at_origin()
 {
-  vimt_image_2d_of<int> image(10, 10, 1);
-  vgl_point_2d<double> orig1 = image.world2im().origin();
-  vimt_centre_image_at_origin(image);
-  vgl_point_2d<double> orig2 = image.world2im().origin();
-  bool ok = (orig1 != orig2 &&
-             orig2 == vgl_point_2d<double>(4.5, 4.5));
-  TEST("centre_image_at_origin", ok, true);
+  vcl_cout << "--------------------------------------------------\n";
+  vcl_cout << "test_centre_image_at_origin(): \n";
+  vcl_cout << "--------------------------------------------------\n";
+
+  // Image with identity transform
+  {
+    const unsigned ni = 8;  // image width (pixels)
+    const unsigned nj = 9;  // image height (pixels)
+    const unsigned np = 1;
+    const vgl_point_2d<double> pix(1.0, 1.0); // pixel dimensions (mm)
+    vimt_transform_2d w2i;
+    w2i.set_zoom_only(1.0/pix.x(), 1.0/pix.y(), 0, 0);
+    vimt_image_2d_of<int> image(ni, nj, np, w2i);
+
+    vgl_point_2d<double> orig1 = image.world2im().origin();
+    vimt_centre_image_at_origin(image);
+    vgl_point_2d<double> orig2 = image.world2im().origin();
+    
+    const double cx = (ni-1)*pix.x()/2.0; // centre of image in world coords
+    const double cy = (nj-1)*pix.y()/2.0; // centre of image in world coords
+    bool ok = (orig1!=orig2 && orig2==vgl_point_2d<double>(cx, cy));
+    TEST("With identity transform", ok, true);
+  }
+
+  // Image with zoom transform (i.e. non-unity pixel size)
+  {
+    const unsigned ni = 8;  // image width (pixels)
+    const unsigned nj = 9;  // image height (pixels)
+    const unsigned np = 1;
+    const vgl_point_2d<double> pix(0.7, 1.2); // pixel dimensions (mm)
+    vimt_transform_2d w2i;
+    w2i.set_zoom_only(1.0/pix.x(), 1.0/pix.y(), 0, 0);
+    vimt_image_2d_of<int> image(ni, nj, np, w2i);
+
+    vgl_point_2d<double> orig1 = image.world2im().origin();
+    vimt_centre_image_at_origin(image);
+    vgl_point_2d<double> orig2 = image.world2im().origin();
+
+    const double cx = (ni-1)*pix.x()/2.0; // centre of image in world coords
+    const double cy = (nj-1)*pix.y()/2.0; // centre of image in world coords
+    bool ok = (orig1!=orig2 && orig2==vgl_point_2d<double>(cx, cy));
+    TEST("With zoom transform (i.e. non-unity pixels)", ok, true);
+  }
 }
 
 
