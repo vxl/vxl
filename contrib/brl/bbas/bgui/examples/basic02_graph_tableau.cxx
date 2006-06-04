928431615e9c195f83fae9be87ebe85ed0288b3c
Joseph L. Mundy <mundyj@users.sourceforge.net>
2006-06-04 07:36:32 -0400

    Brought graph example up to date

--- contrib/brl/bbas/bgui/examples/basic02_graph_tableau.cxx
+++ contrib/brl/bbas/bgui/examples/basic02_graph_tableau.cxx
@@ -7,26 +7,33 @@
 
 #include <vcl_iostream.h>
 #include <vgui/vgui.h>
+#include <vgui/vgui_dialog.h>
 #include <vgui/vgui_menu.h>
-#include <vgui/vgui_image_tableau.h>
-#include <vgui/vgui_easy2D_tableau.h>
 #include <vgui/vgui_viewer2D_tableau.h>
 #include <bgui/bgui_graph_tableau.h>
 #include <vgui/vgui_shell_tableau.h>
-//global pointer to the rubberband tableau
+//global pointers to the tableaux
 static bgui_graph_tableau_sptr graph = 0;
-static vgui_easy2D_tableau_sptr easy = 0;
 
 //the meunu callback functions
 static void create_graph()
 {
-  vcl_vector<double> data(256);
+  vcl_vector<double> pos(256);
+  vcl_vector<double> vals(256);
 
   for(int i = 0; i<256; i++)
-    data[i] = i*(255-i);
-
-  graph->update(data);
-
+    {
+      pos[i]=i;
+      vals[i] = i*(255-i);
+    }
+  graph->update(pos, vals);
+  vcl_string s = "Sample Graph";
+  vgui_dialog* ip_dialog = graph->popup_graph(s);
+  if (!ip_dialog->ask())
+  {
+    delete ip_dialog;
+    return;
+  }
 }
 
 
@@ -34,7 +41,8 @@ static void create_graph()
 vgui_menu create_menus()
 {
   vgui_menu edit;
-  edit.add("Draw histgram",create_graph,(vgui_key)'H',vgui_CTRL);
+  edit.add("Draw graph",create_graph,(vgui_key)'H',vgui_CTRL);
+  edit.add("Quit", vgui::quit);
   vgui_menu bar;
   bar.add("Edit",edit);
   return bar;
@@ -43,13 +51,9 @@ vgui_menu create_menus()
 int main(int argc, char ** argv)
 {
   vgui::init(argc,argv);
-
-  // Make the tableau hierarchy.
-  //easy = vgui_easy2D_tableau_new(image);
-  graph = bgui_graph_tableau_new();
-  vgui_viewer2D_tableau_new viewer(graph);
-  vgui_shell_tableau_new shell(viewer);
-
+  graph = bgui_graph_tableau_new(512,512);
   // Create and run the window
-  return vgui::run(shell, 512, 512, create_menus());
+  vgui_tableau_sptr v = vgui_viewer2D_tableau_new(graph);
+  vgui_tableau_sptr s = vgui_shell_tableau_new(v);
+  return vgui::run(s, 512, 512, create_menus());
 }
