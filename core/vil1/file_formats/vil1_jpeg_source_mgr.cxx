9a97e90fb0524d927c3aa6eea839c3daacece912
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-06-24 15:34:08 -0400

    opposite test ...

--- core/vil1/file_formats/vil1_jpeg_source_mgr.cxx
+++ core/vil1/file_formats/vil1_jpeg_source_mgr.cxx
@@ -156,7 +156,7 @@ vil1_jpeg_stream_src_set (j_decompress_ptr cinfo, vil1_stream *vs)
   // one image, we'd likely lose the start of the next one.)
   // This makes it unsafe to use this manager and a different source
   // manager serially with the same JPEG object.  Caveat programmer.
-  if (! ( vil1_jpeg_srcptr )( cinfo->src ))
+  if (( vil1_jpeg_srcptr )( cinfo->src ))
   { assert(!"this function must be called only once on each cinfo"); }
 
 #ifdef DEBUG
