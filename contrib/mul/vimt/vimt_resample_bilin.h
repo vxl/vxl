00209f0ee2f850ac0912cbb6cb5888e8a5db7afe
Ian Scott <scottim@users.sourceforge.net>
2006-06-26 11:14:14 -0400

    Add edge_extend version. Make smoothing use edge_edtend by default

--- contrib/mul/vimt/vimt_resample_bilin.h
+++ contrib/mul/vimt/vimt_resample_bilin.h
@@ -49,6 +49,45 @@ inline void vimt_resample_bilin(
 }
 
 
+//: Sample grid of points in one image and place in another, using bilinear interpolation.
+//  dest_image(i,j,p) is sampled from the src_image at
+//  p+i.u+j.v, where i=[0..n1-1], j=[0..n2-1] in world co-ordinates.
+//
+//  dest_image resized to (n1,n2,src_image.nplanes())
+//
+//  dest_image.world2im() set up so that the world co-ordinates in src and dest match
+//
+//  Points outside image return the value of the nearest valid pixel.
+// \relates vimt_image_view
+template <class sType, class dType>
+inline void vimt_resample_bilin_edge_extend(
+  const vimt_image_2d_of<sType>& src_image,
+  vimt_image_2d_of<dType>& dest_image,
+  const vgl_point_2d<double>& p,
+  const vgl_vector_2d<double>& u,
+  const vgl_vector_2d<double>& v,
+  int n1, int n2)
+{
+  // Not implemented for projective yet
+  assert(src_image.world2im().form()!=vimt_transform_2d::Projective);
+
+  const vimt_transform_2d& s_w2i = src_image.world2im();
+  vgl_point_2d<double> im_p = s_w2i(p);
+  vgl_vector_2d<double> im_u = s_w2i.delta(p, u);
+  vgl_vector_2d<double> im_v = s_w2i.delta(p, v);
+
+  vil_resample_bilin_edge_extend(src_image.image(),dest_image.image(),
+                      im_p.x(),im_p.y(),  im_u.x(),im_u.y(),
+                      im_v.x(),im_v.y(), n1,n2);
+
+  // Point (i,j) in dest corresponds to p+i.u+j.v,
+  // an affine transformation for image to world
+  vimt_transform_2d d_i2w;
+  d_i2w.set_affine(p,u,v);
+  dest_image.set_world2im(d_i2w.inverse());
+}
+
+
 //: Resample an image using appropriate smoothing if the resolution changes significantly.
 //  dest_image(i,j,p) is sampled from the src_image at
 //  p+i.u+j.v, where i=[0..n1-1], j=[0..n2-1] in world co-ordinates.
@@ -57,7 +96,7 @@ inline void vimt_resample_bilin(
 //
 //  dest_image.world2im() set up so that the world co-ordinates in src and dest match
 //
-//  Points outside image return zero.
+//  Points outside image return the value of the nearest valid pixel.
 // \relates vimt_image_view
 template <class sType, class dType>
 inline void vimt_resample_bilin_smoothing(
@@ -94,7 +133,7 @@ inline void vimt_resample_bilin_smoothing(
     im_d = im.world2im().delta(p, u) + im.world2im().delta(p, v);
   }
 
-  vimt_resample_bilin(im, dest_image, p, u, v, n1, n2);
+  vimt_resample_bilin_edge_extend(im, dest_image, p, u, v, n1, n2);
 }
 
 #endif // vimt_resample_bilin_h_
