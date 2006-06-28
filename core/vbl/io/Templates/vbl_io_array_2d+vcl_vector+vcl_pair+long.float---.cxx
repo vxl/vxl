8924bb88fdf2504dfcb3e7e343566c076147e654
Ian Scott <scottim@users.sourceforge.net>
2006-06-28 11:06:07 -0400

    more instantiations

---
+++ core/vbl/io/Templates/vbl_io_array_2d+vcl_vector+vcl_pair+long.float---.cxx
@@ -0,0 +1,7 @@
+#include <vcl_utility.h>
+#include <vsl/vsl_pair_io.txx>
+#include <vcl_vector.h>
+#include <vsl/vsl_vector_io.txx>
+#include <vbl/io/vbl_io_array_2d.txx>
+typedef vcl_vector<vcl_pair<long,float> > vector_pair_id;
+VBL_IO_ARRAY_2D_INSTANTIATE(vector_pair_id);
