3d9b5672646aaa0c9252cdeb197f91b85373a835
Brad King <bradking@users.sourceforge.net>
2006-07-07 11:52:41 -0400

    Converted .P file prototypes generated by f2c to v3p_netlib_-mangled protytypes in header files.

---
+++ v3p/netlib/minpack/lmder1.h
@@ -0,0 +1,14 @@
+extern int v3p_netlib_lmder1_(
+  U_fp fcn,
+  v3p_netlib_integer *m,
+  v3p_netlib_integer *n,
+  v3p_netlib_doublereal *x,
+  v3p_netlib_doublereal *fvec,
+  v3p_netlib_doublereal *fjac,
+  v3p_netlib_integer *ldfjac,
+  v3p_netlib_doublereal *tol,
+  v3p_netlib_integer *info,
+  v3p_netlib_integer *ipvt,
+  v3p_netlib_doublereal *wa,
+  v3p_netlib_integer *lwa
+  );
