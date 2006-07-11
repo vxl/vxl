3905965a0297d0ea6ab3145da03857076a445ff0
Brad King <bradking@users.sourceforge.net>
2006-07-11 15:52:04 -0400

    Converted .P file prototypes generated by f2c to v3p_netlib_-mangled protytypes in header files.

---
+++ v3p/netlib/toms/rpoly.h
@@ -0,0 +1,50 @@
+extern int v3p_netlib_rpoly_(
+  v3p_netlib_doublereal *op,
+  v3p_netlib_integer *degree,
+  v3p_netlib_doublereal *zeror,
+  v3p_netlib_doublereal *zeroi,
+  v3p_netlib_logical *fail
+  );
+extern int v3p_netlib_fxshfr_(
+  v3p_netlib_integer *l2,
+  v3p_netlib_integer *nz
+  );
+extern int v3p_netlib_quadit_(
+  v3p_netlib_doublereal *uu,
+  v3p_netlib_doublereal *vv,
+  v3p_netlib_integer *nz
+  );
+extern int v3p_netlib_realit_(
+  v3p_netlib_doublereal *sss,
+  v3p_netlib_integer *nz,
+  v3p_netlib_integer *iflag
+  );
+extern int v3p_netlib_calcsc_(
+  v3p_netlib_integer *type__
+  );
+extern int v3p_netlib_nextk_(
+  v3p_netlib_integer *type__
+  );
+extern int v3p_netlib_newest_(
+  v3p_netlib_integer *type__,
+  v3p_netlib_doublereal *uu,
+  v3p_netlib_doublereal *vv
+  );
+extern int v3p_netlib_quadsd_(
+  v3p_netlib_integer *nn,
+  v3p_netlib_doublereal *u,
+  v3p_netlib_doublereal *v,
+  v3p_netlib_doublereal *p,
+  v3p_netlib_doublereal *q,
+  v3p_netlib_doublereal *a,
+  v3p_netlib_doublereal *b
+  );
+extern int v3p_netlib_quad_(
+  v3p_netlib_doublereal *a,
+  v3p_netlib_doublereal *b1,
+  v3p_netlib_doublereal *c__,
+  v3p_netlib_doublereal *sr,
+  v3p_netlib_doublereal *si,
+  v3p_netlib_doublereal *lr,
+  v3p_netlib_doublereal *li
+  );
