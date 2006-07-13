bdc3364e3105d061ce545c264fa6ae8c2ab429f5
Brad King <bradking@users.sourceforge.net>
2006-07-13 11:59:52 -0400

    Converted .P file prototypes generated by f2c to v3p_netlib_-mangled protytypes in header files.

---
+++ v3p/netlib/lapack/double/dlartg.h
@@ -0,0 +1,7 @@
+extern int v3p_netlib_dlartg_(
+  v3p_netlib_doublereal *f,
+  v3p_netlib_doublereal *g,
+  v3p_netlib_doublereal *cs,
+  v3p_netlib_doublereal *sn,
+  v3p_netlib_doublereal *r__
+  );
