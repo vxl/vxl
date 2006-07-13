9fbd5853fa22832a28dedccfa0406630cb416400
Brad King <bradking@users.sourceforge.net>
2006-07-13 13:23:57 -0400

    Converted .P file prototypes generated by f2c to v3p_netlib_-mangled protytypes in header files.

---
+++ v3p/netlib/mathews/simpson.h
@@ -0,0 +1,14 @@
+extern int v3p_netlib_simpru_(
+  v3p_netlib_E_fp f,
+  v3p_netlib_real *a,
+  v3p_netlib_real *b,
+  v3p_netlib_integer *m,
+  v3p_netlib_real *srule
+  );
+extern int v3p_netlib_xsimpru_(
+  v3p_netlib_E_fp f,
+  v3p_netlib_real *a,
+  v3p_netlib_real *b,
+  v3p_netlib_integer *m,
+  v3p_netlib_real *srule
+  );
