0a3992f10909a8b834689e41352ecd8508f693de
Glen Brooksby <brooksby@users.sourceforge.net>
2006-07-27 17:30:57 -0400

    added support for unsigned 16-bit images.

---
+++ core/vil/Templates/vil_bicub_interp+uint_16-.cxx
@@ -0,0 +1,3 @@
+#include <vil/vil_bicub_interp.txx>
+#include <vxl_config.h>
+VIL_BICUB_INTERP_INSTANTIATE(vxl_uint_16);
---
+++ core/vil/Templates/vil_resample_bicub+uint_16.uint_16-.cxx
@@ -0,0 +1,3 @@
+#include <vil/vil_resample_bicub.txx>
+#include <vxl_config.h>
+VIL_RESAMPLE_BICUB_INSTANTIATE( vxl_uint_16 , vxl_uint_16 );
