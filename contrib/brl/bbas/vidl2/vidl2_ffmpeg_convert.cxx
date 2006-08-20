e672ad9b3764bf072d57ce1cdb42f92e53f302ee
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-08-20 13:32:23 -0400

    some libffmpeg distribs don't define PIX_FMT_UYVY411 & 422

--- contrib/brl/bbas/vidl2/vidl2_ffmpeg_convert.cxx
+++ contrib/brl/bbas/vidl2/vidl2_ffmpeg_convert.cxx
@@ -31,7 +31,7 @@
 bool vidl2_ffmpeg_convert(const vidl2_frame_sptr& in_frame,
                                 vidl2_frame_sptr& out_frame)
 {
-  if(!in_frame || !out_frame)
+  if (!in_frame || !out_frame)
     return false;
 
   PixelFormat in_fmt =
@@ -40,21 +40,19 @@ bool vidl2_ffmpeg_convert(const vidl2_frame_sptr& in_frame,
   PixelFormat out_fmt =
       vidl2_pixel_format_to_ffmpeg(out_frame->pixel_format());
 
-  if(in_fmt == PIX_FMT_NONE || out_fmt == PIX_FMT_NONE)
+  if (in_fmt == PIX_FMT_NONE || out_fmt == PIX_FMT_NONE)
     return false;
 
   unsigned ni = in_frame->ni();
   unsigned nj = in_frame->nj();
   unsigned out_size = (unsigned) avpicture_get_size( out_fmt, ni, nj );
 
-  if(out_frame->size() != out_size ||
-     out_frame->ni() != ni ||
-     out_frame->nj() != nj ||
-     !out_frame->data() ){
+  if (out_frame->size() != out_size ||
+      out_frame->ni() != ni ||
+      out_frame->nj() != nj ||
+      !out_frame->data() )
     out_frame = new vidl2_memory_chunk_frame(ni, nj, out_frame->pixel_format(),
                                              new vil_memory_chunk(out_size, VIL_PIXEL_FORMAT_BYTE));
-  }
-
 
   AVPicture in_pic;
   vcl_memset( &in_pic, 0, sizeof(in_pic) );
@@ -65,7 +63,7 @@ bool vidl2_ffmpeg_convert(const vidl2_frame_sptr& in_frame,
   avpicture_fill(&out_pic, (uint8_t*) out_frame->data(), out_fmt, ni, nj);
 
 
-  if( img_convert( &out_pic, out_fmt, &in_pic, in_fmt, ni, nj ) < 0 )
+  if ( img_convert( &out_pic, out_fmt, &in_pic, in_fmt, ni, nj ) < 0 )
     return false;
 
   return true;
@@ -76,7 +74,8 @@ bool vidl2_ffmpeg_convert(const vidl2_frame_sptr& in_frame,
 vidl2_pixel_format
 vidl2_pixel_format_from_ffmpeg(PixelFormat ffmpeg_pix_fmt)
 {
-  switch(ffmpeg_pix_fmt){
+  switch (ffmpeg_pix_fmt)
+  {
     case PIX_FMT_YUV420P:   return VIDL2_PIXEL_FORMAT_YUV_420P;
     case PIX_FMT_YUV422:    return VIDL2_PIXEL_FORMAT_YUYV_422;
     case PIX_FMT_RGB24:     return VIDL2_PIXEL_FORMAT_RGB_24;
@@ -91,8 +90,13 @@ vidl2_pixel_format_from_ffmpeg(PixelFormat ffmpeg_pix_fmt)
     case PIX_FMT_GRAY8:     return VIDL2_PIXEL_FORMAT_MONO_8;
     case PIX_FMT_MONOWHITE: return VIDL2_PIXEL_FORMAT_MONO_1;
     case PIX_FMT_MONOBLACK: return VIDL2_PIXEL_FORMAT_MONO_1;
+// Some libffmpeg distribs (notably Mandriva's) don't include these two:
+#ifdef PIX_FMT_UYVY422
     case PIX_FMT_UYVY422:   return VIDL2_PIXEL_FORMAT_UYVY_422;
+#endif
+#ifdef PIX_FMT_UYVY411
     case PIX_FMT_UYVY411:   return VIDL2_PIXEL_FORMAT_UYVY_411;
+#endif
     default: break;
   }
   return VIDL2_PIXEL_FORMAT_UNKNOWN;
@@ -103,7 +107,8 @@ vidl2_pixel_format_from_ffmpeg(PixelFormat ffmpeg_pix_fmt)
 PixelFormat
 vidl2_pixel_format_to_ffmpeg(vidl2_pixel_format vidl2_pix_fmt)
 {
-  switch(vidl2_pix_fmt){
+  switch (vidl2_pix_fmt)
+  {
     case VIDL2_PIXEL_FORMAT_RGB_24:   return PIX_FMT_RGB24;
     case VIDL2_PIXEL_FORMAT_BGR_24:   return PIX_FMT_BGR24;
     case VIDL2_PIXEL_FORMAT_RGBA_32:  return PIX_FMT_RGBA32;
@@ -115,8 +120,13 @@ vidl2_pixel_format_to_ffmpeg(vidl2_pixel_format vidl2_pix_fmt)
     case VIDL2_PIXEL_FORMAT_YUV_420P: return PIX_FMT_YUV420P;
     case VIDL2_PIXEL_FORMAT_YUV_411P: return PIX_FMT_YUV411P;
     case VIDL2_PIXEL_FORMAT_YUV_410P: return PIX_FMT_YUV410P;
+// Some libffmpeg distribs (notably Mandriva's) don't include these two:
+#ifdef PIX_FMT_UYVY422
     case VIDL2_PIXEL_FORMAT_UYVY_422: return PIX_FMT_UYVY422;
+#endif
+#ifdef PIX_FMT_UYVY411
     case VIDL2_PIXEL_FORMAT_UYVY_411: return PIX_FMT_UYVY411;
+#endif
     case VIDL2_PIXEL_FORMAT_MONO_1:   return PIX_FMT_MONOBLACK;
     case VIDL2_PIXEL_FORMAT_MONO_8:   return PIX_FMT_GRAY8;
     default: break;
