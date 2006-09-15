ccec33b8cda55799a0fbf1270dbf82c1aa711e4f
Kola Babalola <kbab@users.sourceforge.net>
2006-09-15 06:48:00 -0400

    Modified description of vil3d_fill_boundary as previous description was wrong

--- contrib/mul/vil3d/algo/vil3d_fill_boundary.h
+++ contrib/mul/vil3d/algo/vil3d_fill_boundary.h
@@ -8,11 +8,7 @@
 //: Compute a mask where the regions in each slice of a 3D image bounded by contours are set to "on"
 #include <vil3d/vil3d_image_view.h>
 
-//: Compute 3d distance transform from zeros in original image.
-//  Image is assumed to be filled with max_dist where there is
-//  background, and zero at the places of interest. On exit,
-//  the values are the 8-connected distance to the nearest
-//  original zero voxel.
+//: Compute a mask where the regions in each slice of a 3D image bounded by contours are set to "on".
 void vil3d_fill_boundary(vil3d_image_view<bool>& image);
 
 //: Follow the current boundary in the current slice.
