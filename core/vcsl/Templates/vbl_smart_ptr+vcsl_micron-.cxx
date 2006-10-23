b0d674478d084f53517b99192b63341d386d7ab8
Jesse Funaro <jdfunaro@users.sourceforge.net>
2006-10-23 15:44:43 -0400

    sptr template definitions for vcsl_millimeter and vcsl_micron

---
+++ core/vcsl/Templates/vbl_smart_ptr+vcsl_micron-.cxx
@@ -0,0 +1,4 @@
+#include <vcsl/vcsl_micron.h>
+#include <vbl/vbl_smart_ptr.txx>
+
+VBL_SMART_PTR_INSTANTIATE(vcsl_micron);
---
+++ core/vcsl/Templates/vbl_smart_ptr+vcsl_millimeter-.cxx
@@ -0,0 +1,4 @@
+#include <vcsl/vcsl_millimeter.h>
+#include <vbl/vbl_smart_ptr.txx>
+
+VBL_SMART_PTR_INSTANTIATE(vcsl_millimeter);
