77b6077c326eb71d652c7c64d0b39078e663d794
Gehua Yang <norwegian_wood@users.sourceforge.net>
2006-10-24 11:49:04 -0400

    split derived mask classes into two files. The reason is because rgrl_mask_3d_image requires vil3d, which is resided in contrib. There is no reason to always include it.

--- contrib/rpl/rgrl/rgrl_mask.h
+++ contrib/rpl/rgrl/rgrl_mask.h
@@ -10,10 +10,13 @@
 // 
 // \author Charlene Tsai
 // \date   Oct 2003
+// \verbatim
+// Modifications
+// Oct. 2006  Gehua Yang (RPI) - move rgrl_mask_3d_image into separate file
+// \endverbatim
 
 #include <vcl_iosfwd.h>
 #include <vnl/vnl_vector.h>
-#include <vil3d/vil3d_image_view.h>
 #include <vil/vil_image_view.h>
 
 #include <rgrl/rgrl_object.h>
@@ -74,28 +77,6 @@ class rgrl_mask_2d_image
  private:
   vil_image_view<vxl_byte> mask_image_;
   int org_x_, org_y_;
-};
-
-
-class rgrl_mask_3d_image
-  : public rgrl_mask
-{
- public:
-  rgrl_mask_3d_image( const vil3d_image_view< vxl_byte > & in_mask,
-                      int org_x = 0, int org_y = 0, int org_z = 0 );
-
-  //: True if a point is inside the region
-  bool inside( vnl_vector< double > const& pt ) const;
-
-  // Defines type-related functions
-  rgrl_type_macro( rgrl_mask_3d_image, rgrl_mask );
-
- private:
-  void update_bounding_box();
-
- private:
-  vil3d_image_view< vxl_byte > mask_image_;
-  int org_x_, org_y_, org_z_;
 };
 
 //: This is a wrapper on the circular region or sphere
