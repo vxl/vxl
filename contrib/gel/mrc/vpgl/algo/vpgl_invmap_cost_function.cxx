461b481f9b998a86b7907c67206cd9f3ede2b959
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-11-07 02:22:08 -0500

    Doxygen comments will not be shown without "\file"

--- contrib/gel/mrc/vpgl/algo/vpgl_invmap_cost_function.cxx
+++ contrib/gel/mrc/vpgl/algo/vpgl_invmap_cost_function.cxx
@@ -1,8 +1,11 @@
 #include <vpgl/algo/vpgl_invmap_cost_function.h>
+//:
+// \file
 #include <vcl_cmath.h>
 #include <vcl_iostream.h>
+
 vpgl_invmap_cost_function::
-vpgl_invmap_cost_function(vnl_vector_fixed<double, 2> const& image_point, 
+vpgl_invmap_cost_function(vnl_vector_fixed<double, 2> const& image_point,
                           vnl_vector_fixed<double, 4> const& plane,
                           const vpgl_rational_camera<double>* const rcam)
  : vnl_cost_function(2), image_point_(image_point),
@@ -12,15 +15,15 @@ vpgl_invmap_cost_function(vnl_vector_fixed<double, 2> const& image_point,
   // order the plane normals
   double anx = vcl_fabs(plane_[0]), any = vcl_fabs(plane_[1]),
     anz = vcl_fabs(plane_[2]);
-  if(anx<any && anz<any)
+  if (anx<any && anz<any)
     { pp_ = X_Z; return;}
-  if(any<anx && anz<anx)
+  if (any<anx && anz<anx)
      pp_ = Y_Z;
 }
 //: The main function.
 double vpgl_invmap_cost_function::f(vnl_vector<double> const& x)
 {
-  if(!rcam_ptr_)
+  if (!rcam_ptr_)
     return 0;
   // fill out the 3-d point from the parameters
   vnl_vector_fixed<double, 3> p_3d;
@@ -38,67 +41,66 @@ double vpgl_invmap_cost_function::f(vnl_vector<double> const& x)
 void vpgl_invmap_cost_function::
 set_params(vnl_vector_fixed<double, 3> const& xyz, vnl_vector<double> &x)
 {
- switch(pp_)
-    {
+  switch (pp_)
+  {
     case X_Y:
-      {
-        x[0] = xyz[0];
-        x[1] = xyz[1];
-        break;
-      }
-    case X_Z:
-      {
-        x[0] = xyz[0];
-        x[1] = xyz[2];
-        break;
-      }
-    case Y_Z:
-      {
-        x[0] = xyz[1];
-        x[1] = xyz[2];
-        break;
-      }
-    default:
-      {
-        x[0] = 0; x[1] = 0;
-        vcl_cerr << "Improper prameterization in vpgl_invmap_cost_function\n";
-      }
+    {
+      x[0] = xyz[0];
+      x[1] = xyz[1];
+      break;
     }
+    case X_Z:
+    {
+      x[0] = xyz[0];
+      x[1] = xyz[2];
+      break;
+    }
+    case Y_Z:
+    {
+      x[0] = xyz[1];
+      x[1] = xyz[2];
+      break;
+    }
+    default:
+    {
+      x[0] = 0; x[1] = 0;
+      vcl_cerr << "Improper prameterization in vpgl_invmap_cost_function\n";
+    }
+  }
 }
 
 void vpgl_invmap_cost_function::
 point_3d(vnl_vector<double> const& x, vnl_vector_fixed<double, 3>& xyz)
 {
   //Switch on plane parameterization
-  switch(pp_)
-    {
+  switch (pp_)
+  {
     case X_Y:
-      {
-        xyz[0] = x[0];
-        xyz[1] = x[1];
-        xyz[2] = -(plane_[0]*x[0] + plane_[1]*x[1] + plane_[3])/plane_[2];
-        break;
-      }
-    case X_Z:
-      {
-        xyz[0] = x[0];
-        xyz[2] = x[1];
-        xyz[1] = -(plane_[0]*x[0] + plane_[2]*x[1] + plane_[3])/plane_[1];
-        break;
-      }
-    case Y_Z:
-      {
-        xyz[1] = x[0];
-        xyz[2] = x[1];
-        xyz[0] = -(plane_[1]*x[0] + plane_[2]*x[1] + plane_[3])/plane_[0];
-        break;
-      }
-    default:
-      {
-        xyz[0] = 0; xyz[1] = 0; xyz[2] = 0; 
-        vcl_cerr << "Improper prameterization in vpgl_invmap_cost_function\n";
-      }
+    {
+      xyz[0] = x[0];
+      xyz[1] = x[1];
+      xyz[2] = -(plane_[0]*x[0] + plane_[1]*x[1] + plane_[3])/plane_[2];
+      break;
     }
+    case X_Z:
+    {
+      xyz[0] = x[0];
+      xyz[2] = x[1];
+      xyz[1] = -(plane_[0]*x[0] + plane_[2]*x[1] + plane_[3])/plane_[1];
+      break;
+    }
+    case Y_Z:
+    {
+      xyz[1] = x[0];
+      xyz[2] = x[1];
+      xyz[0] = -(plane_[1]*x[0] + plane_[2]*x[1] + plane_[3])/plane_[0];
+      break;
+    }
+    default:
+    {
+      xyz[0] = 0; xyz[1] = 0; xyz[2] = 0;
+      vcl_cerr << "Improper prameterization in vpgl_invmap_cost_function\n";
+    }
+  }
 }
-
 
--- contrib/gel/mrc/vpgl/algo/vpgl_invmap_cost_function.h
+++ contrib/gel/mrc/vpgl/algo/vpgl_invmap_cost_function.h
@@ -1,7 +1,8 @@
-// This is vpgl/algo/vpgl_invmap_cost_function.h
+// This is gel/mrc/vpgl/algo/vpgl_invmap_cost_function.h
 #ifndef vpgl_invmap_cost_function_h_
 #define vpgl_invmap_cost_function_h_
-
+//:
+// \file
 #include <vnl/vnl_cost_function.h>
 #include <vnl/vnl_vector_fixed.h>
 #include <vpgl/vpgl_rational_camera.h>
@@ -10,7 +11,7 @@ class vpgl_invmap_cost_function: public vnl_cost_function
 {
   //: Which parameterization to use for the plane
   enum plane_param{X_Y=0, X_Z, Y_Z};
-public:
+ public:
   //: Constructor - rcam pointer is not deleted by this class
   vpgl_invmap_cost_function(vnl_vector_fixed<double, 2> const& image_point, 
                             vnl_vector_fixed<double, 4> const& plane,
@@ -23,7 +24,7 @@ public:
   //: get the 3-d point defined by the parameters (and the plane).
   void point_3d(vnl_vector<double> const& x, vnl_vector_fixed<double, 3>& xyz);
 
-protected:
+ protected:
   //: image point
   vnl_vector_fixed<double, 2> image_point_; 
   //: plane coefficients
