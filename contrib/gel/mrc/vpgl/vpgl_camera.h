28c77f3f61d7cc5f0ba92891e219c116a56645ed
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2006-11-07 02:22:19 -0500

    typo corrected

--- contrib/gel/mrc/vpgl/vpgl_camera.h
+++ contrib/gel/mrc/vpgl/vpgl_camera.h
@@ -11,25 +11,23 @@
 //
 //   A basic abstract camera class on which all specific cameras are based.
 //   As such there is only one operation it performs:
-//   project a 3d world point into a 2d image point.  The class is templated 
-//   over T which had better be an algebraic field. 
-// 
-// Modifications:  October 26, 2006 - Moved homogenous methods to 
+//   project a 3d world point into a 2d image point.  The class is templated
+//   over T which had better be an algebraic field.
+//
+// Modifications:  October 26, 2006 - Moved homogeneous methods to
 // projective camera, since projective geometry may not apply in the
 // most general case, e.g. rational cameras. - JLM
 
 template<class T>
 class vpgl_camera
 {
-public:
+ public:
 
   vpgl_camera(){};
   virtual ~vpgl_camera(){};
 
   //: The generic camera interface. u represents image column, v image row.
   virtual void project(const T x, const T y, const T z, T& u, T& v) const = 0;
-
 };
-
 
 #endif // vpgl_camera_h_
