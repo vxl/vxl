f24eef89b80a44f5a8f1ae83da5f6658490c6f45
Martin G. Roberts <mgroberts@users.sourceforge.net>
2006-11-15 11:46:48 -0500

    added new builders for binary hyperplance This builder reduces the influence of points that are well inside the classificatin region using a Geman-McClure robust rho function rather than least squares. This makes the plane place more weight on missclassified points or points near the boundary WARNING not yet tested first commit

---
+++ contrib/mul/clsfy/clsfy_binary_hyperplane_gmrho_builder.cxx
@@ -0,0 +1,390 @@
+// This is mul/clsfy/clsfy_binary_hyperplane_ls_builder.cxx
+#include "clsfy_binary_hyperplane_gmrho_builder.h"
+//:
+// \file
+// \brief Implement a two-class output linear classifier builder
+// \author Martin Roberts
+// \date 4 Nov 2006
+
+//=======================================================================
+
+#include <vcl_string.h>
+#include <vcl_iostream.h>
+#include <vcl_vector.h>
+#include <vcl_cassert.h>
+#include <vcl_cmath.h>
+#include <vcl_algorithm.h>
+#include <vcl_numeric.h>
+#include <vnl/algo/vnl_svd.h>
+#include <vnl/vnl_math.h>
+#include <vnl/vnl_vector_ref.h>
+#include <vnl/vnl_cost_function.h>
+#include <vnl/algo/vnl_lbfgs.h>
+
+//: Some helper stuff, like the error function to be minimised
+namespace clsfy_binary_hyperplane_gmrho_builder_helpers
+{
+    //: The cost function, sum Geman-McClure error functions over all training examples
+    class gmrho_sum : public vnl_cost_function
+    {
+        //: Reference to data matrix, one row per training example
+        const vnl_matrix<double>& x_;
+        const vnl_vector<double>& y_;
+        double sigma_;
+        double var_;
+        unsigned num_examples_;
+        unsigned num_vars_;
+        double alpha_;
+        double beta_;
+     public:
+        //: construct passing in reference to data matrix
+        gmrho_sum(const vnl_matrix<double>& x,
+                  const vnl_vector<double>& y,double sigma=1);
+        
+        //: reset the scaling factor
+        void set_sigma(double sigma) {sigma_ = sigma; var_ = sigma*sigma;}
+
+        //:  The main function.  Given the vector of weights parameters vector , compute the value of f(x).
+        virtual double f(vnl_vector<double> const& w);
+
+        //:  Calculate the gradient of f at parameter vector x.
+        virtual void gradf(vnl_vector<double> const& x, vnl_vector<double>& gradient);
+        
+    };
+
+    //: functor to accumulate gradient contributions for given training example
+    class gm_grad_accum
+    {
+        const double* px_;
+        const double wt_;
+      public:
+        gm_grad_accum(const double* px,double wt) : px_(px),wt_(wt) {}
+        void operator()(double& grad)
+        {
+            grad += (*px_++) * wt_;
+        }
+    };
+
+    //: Given the class category variable, return the associated regression value (e.g. 1 for class 1, -1 for class 0)
+    class category_value
+    {
+        const double y0;
+        const double y1;
+    public:
+        category_value(unsigned num_category1,unsigned num_total):
+                y0(-1.0*double(num_total-num_category1)/double(num_total)),
+                y1(double(num_category1)/double(num_total)) {}
+            
+        double operator()(const unsigned& classNum)
+        {
+            //return classNum ? y1 : y0;
+            return classNum ? 1.0 : -1.0;
+        }
+    };
+};
+
+
+//: Build a linear hyperplane classifier with the given data.
+// Reduce the influence of well classified points far into their correct region by
+// applying a Geman-McClure robust error function, rather than a least squares fit
+double clsfy_binary_hyperplane_gmrho_builder::build(clsfy_classifier_base& classifier,
+                                                    mbl_data_wrapper<vnl_vector<double> >& inputs,
+                                                    const vcl_vector<unsigned>& outputs) const
+{
+    using clsfy_binary_hyperplane_gmrho_builder_helpers::category_value;
+    
+    //First let the base class get us a starting solution
+    clsfy_binary_hyperplane_ls_builder::build( classifier,inputs,outputs);
+    //Extract the data into a matrix
+    num_examples_ = inputs.size();
+    if(num_examples_ == 0)
+    {
+        vcl_cout<<"WARNING - clsfy_binary_hyperplane_gmrho_builder::build called with no data"<<vcl_endl;
+        return 0.0;
+    }
+
+    //Now copy from the urggghh data wrapper into a sensible data structure (matrix!)
+    inputs.reset();
+    num_vars_ = inputs.current().size();
+    vnl_matrix<double> data(num_examples_,num_vars_,0.0);
+    unsigned i=0;
+    do
+    {
+        double* row=data[i++];
+        vcl_copy(inputs.current().begin(),inputs.current().end(),row);
+     }while(inputs.next());
+
+    //Set up category regression values determined by output class
+    vnl_vector<double> y(num_examples_,0.0);
+    vcl_transform(outputs.begin(),outputs.end(),
+                  y.begin(),
+                  category_value(vcl_count(outputs.begin(),outputs.end(),1u),outputs.size()));
+    weights_.set_size(num_vars_+1);
+
+    //Initialise the weights using the standard least squares fit of my base class
+    clsfy_binary_hyperplane& hyperplane = dynamic_cast<clsfy_binary_hyperplane &>(classifier);
+    
+    weights_.update(hyperplane.weights(),0);
+    weights_[num_vars_] = hyperplane.bias();
+
+    //Estimate the scaling factor used in the Geman-McClure function
+    double sigma_scale_target=estimate_sigma(data,y);
+
+    //To avoid local minima perform deterministic annealing starting from a large initial sigma
+    //Set initial kappa so that everything is an inlier
+    double kappa = 5.0;
+    const double alpha_anneal=0.75;
+    //Num of iterations to reduce back to 10% on top of required sigma
+    int N = 1+int(log(1.10/kappa)/log(alpha_anneal));
+    if(N<1) N=1;
+    double sigma_scale = kappa * sigma_scale_target;
+
+    epsilon_ = 1.0E-4; //slacken off convergence tolerance during annealing
+    for(int ianneal=0;ianneal<N;++ianneal)
+    {
+        //Then do it at this sigma
+        determine_weights(data,y,sigma_scale);
+        //and then reduce sigma
+        sigma_scale *= alpha_anneal;
+    }
+    
+    epsilon_ = 1.0E-8; //re-impose a more precise convergence criterion
+    //Then re-estimate sigma scale and do a final pair of iterations
+    //as sigma depends on the mis-classification overlap depth
+
+    for(unsigned iter=0;iter<2;++iter)
+    {    
+        sigma_scale_target=estimate_sigma(data,y);
+        //Finally do it at exactly the original target sigma
+        determine_weights(data,y,sigma_scale_target);
+    }
+    //And finally copy the parameters into the hyperplane
+    vnl_vector_ref<double > weights(num_vars_,weights_.data_block());
+    hyperplane.set(weights, weights_[num_vars_]);
+
+    return clsfy_test_error(classifier, inputs, outputs);
+}
+
+void clsfy_binary_hyperplane_gmrho_builder::determine_weights(const vnl_matrix<double>& data,
+                                                              const vnl_vector<double >& y,
+                                                              double sigma) const
+{
+    //Optimise the weights to fit the data to y
+    
+    clsfy_binary_hyperplane_gmrho_builder_helpers::gmrho_sum costFn(data,y,sigma);
+
+    //minimise using the quasi-Newton lbfgs method
+    vnl_lbfgs cgMinimiser(costFn);
+    
+    cgMinimiser.set_f_tolerance(epsilon_);
+    cgMinimiser.set_x_tolerance(epsilon_);
+    
+    cgMinimiser.minimize(weights_);
+}
+
+double clsfy_binary_hyperplane_gmrho_builder::estimate_sigma(const vnl_matrix<double>& data,
+                                                             const vnl_vector<double >& y) const
+{
+    //Sigma is set to root(3) * (1+d), where d is the median distance past zero
+    //of the misclassified values
+    //The root(3) is because GM function reduces influence after sigma/sqrt(3)
+    
+    vcl_vector<double > falsePosScores;
+    vcl_vector<double > falseNegScores;
+
+    double b=weights_[num_vars_]; //constant stored as final variable
+    for(unsigned i=0; i<num_examples_;++i) //Loop over examples (matrix rows)
+    {
+        const double* px=data[i];
+        double yval = y[i];
+        double ypred = vcl_inner_product(px,px+num_vars_,weights_.begin(),0.0) - b ;
+        double e = yval - ypred;
+        if(yval>0.0)
+        {
+            if(ypred<0.0)//mis-classified false negative
+            {
+                falseNegScores.push_back(vcl_fabs(ypred));
+            }
+        }
+        else
+        {
+            if(ypred>0.0)//mis-classified false negative
+            {
+                falsePosScores.push_back(vcl_fabs(ypred));
+            }
+        }
+    }
+    double sigma=1.0;
+    double delta0=0.0;
+    if(!falsePosScores.empty())
+    {
+        vcl_vector<double >::iterator medianIter=falsePosScores.begin() + falsePosScores.size()/2;
+        vcl_nth_element(falsePosScores.begin(),medianIter,falsePosScores.end());
+        delta0 = (*medianIter);
+    }
+    double delta1=0.0;
+    if(!falseNegScores.empty())
+    {
+        vcl_vector<double >::iterator medianIter=falseNegScores.begin() + falseNegScores.size()/2;
+        vcl_nth_element(falseNegScores.begin(),medianIter,falseNegScores.end());
+        delta1 = (*medianIter);
+    }
+    sigma += vcl_max(delta0,delta1);
+
+    sigma *= vcl_sqrt(3.0);
+    return sigma;
+}
+
+//=======================================================================
+
+void clsfy_binary_hyperplane_gmrho_builder::b_write(vsl_b_ostream &bfs) const
+{
+    const int version_no=1;
+    vsl_b_write(bfs, version_no);
+    clsfy_binary_hyperplane_ls_builder::b_write(bfs);
+}
+
+//=======================================================================
+
+void clsfy_binary_hyperplane_gmrho_builder::b_read(vsl_b_istream &bfs)
+{
+    if (!bfs) return;
+
+    short version;
+    vsl_b_read(bfs,version);
+    switch (version)
+    {
+        case (1):
+            clsfy_binary_hyperplane_ls_builder::b_read(bfs);
+            break;
+        default:
+            vcl_cerr << "I/O ERROR: clsfy_binary_hyperplane_gmrho_builder::b_read(vsl_b_istream&)\n"
+                     << "           Unknown version number "<< version << '\n';
+            bfs.is().clear(vcl_ios::badbit); // Set an unrecoverable IO error on stream
+    }
+}
+
+//=======================================================================
+
+vcl_string clsfy_binary_hyperplane_gmrho_builder::is_a() const
+{
+    return vcl_string("clsfy_binary_hyperplane_gmrho_builder");
+}
+
+//=======================================================================
+
+bool clsfy_binary_hyperplane_gmrho_builder::is_class(vcl_string const& s) const
+{
+    return s == clsfy_binary_hyperplane_gmrho_builder::is_a() || clsfy_binary_hyperplane_ls_builder::is_class(s);
+}
+
+//=======================================================================
+
+short clsfy_binary_hyperplane_gmrho_builder::version_no() const
+{
+    return 1;
+}
+
+//=======================================================================
+
+void clsfy_binary_hyperplane_gmrho_builder::print_summary(vcl_ostream& os) const
+{
+    os << is_a();
+}
+
+//=======================================================================
+clsfy_builder_base* clsfy_binary_hyperplane_gmrho_builder::clone() const
+{
+    return new clsfy_binary_hyperplane_gmrho_builder(*this);
+}
+
+//---------------------------------------------------------------------------------------------
+//: The error function class
+//  This returns a geman-mcclure robust function if the point is correctly classified
+// Otherwise the squared error is returned, with coefficient and offset to ensure continuity
+// and smoothness at the join
+//---------------------------------------------------------------------------------------------
+clsfy_binary_hyperplane_gmrho_builder_helpers::gmrho_sum::gmrho_sum(const vnl_matrix<double>& x,
+                                                                    const vnl_vector<double>& y,
+                                                                    double sigma):
+        vnl_cost_function(x.cols()+1),
+        x_(x),y_(y),sigma_(sigma),var_(sigma*sigma),num_examples_(x.rows()),num_vars_(x.cols())
+{
+    double s=1.0+var_;
+    s = s*s;
+    alpha_ = var_/s;
+    beta_ = 1.0/s;
+}
+
+double clsfy_binary_hyperplane_gmrho_builder_helpers::gmrho_sum::f(vnl_vector<double> const& w)
+{
+    //Sum the error contributions from each example
+    double sum=0.0;
+    double b=w[num_vars_]; //constant stored as final variable
+    for(unsigned i=0; i<num_examples_;++i) //Loop over examples (matrix rows)
+    {
+        const double* px=x_[i];
+        double pred = vcl_inner_product(px,px+num_vars_,w.begin(),0.0) - b;
+        double e =  y_[i] - pred;
+        double e2 = e*e;
+        if( ((y_[i] > 0.0) && (e <= 1.0)) ||
+            ((y_[i] < 0.0) && (e >= -1.0)) )
+        {
+            //In the correctly classified region
+            //So use Geman-McClure function
+            sum += e2/(e2+var_);
+        }
+        else
+        {
+            //Misclassified, so keep as quadratic (influence increases with error)
+            //NB alpha and beta are chosen for continuity of function and gradient at boundary
+            sum += alpha_*e2 + beta_;
+        }
+    }
+    return sum;
+}
+void clsfy_binary_hyperplane_gmrho_builder_helpers::gmrho_sum::gradf(vnl_vector<double> const& w,
+                                                                     vnl_vector<double>& gradient)
+                                                                     
+{
+    //Return gradient vector
+    using clsfy_binary_hyperplane_gmrho_builder_helpers::gm_grad_accum;
+    double sum=0.0;
+    double b=w[num_vars_]; //constant stored as final variable
+    gradient.fill(0.0);
+    
+    for(unsigned i=0; i<num_examples_;++i) //Loop over examples (matrix rows)
+    {
+        const double* px=x_[i];
+        double pred = vcl_inner_product(px,px+num_vars_,w.begin(),0.0) - b;
+        double e =  y_[i] - pred;
+        double e2 = e*e;
+        double wt=1.0;
+
+        if( ((y_[i] > 0.0) && (e <= 1.0)) ||
+            ((y_[i] < 0.0) && (e >= -1.0)) )
+        {
+            wt = e2 + var_;
+        }
+        else
+        {
+            //Freeze weight decay once in misclassification region
+            wt = 1.0 + var_;
+        }
+            
+        double wtInv = -e/wt*wt;
+
+        //Add in to each gradient component the contribution from this example (loop over columns)
+        vcl_for_each(gradient.begin(),gradient.begin()+num_vars_,
+                     gm_grad_accum(px,wtInv));
+      
+        gradient[num_vars_] += wtInv; //dg/db, last term is for constant
+
+    }
+    //And multiply everything by 2sigma^2
+    vcl_for_each(gradient.begin(),gradient.end(),
+                 vcl_bind2nd(vcl_multiplies<double>(),2.0*var_));
+}
+
+
+
---
+++ contrib/mul/clsfy/clsfy_binary_hyperplane_gmrho_builder.h
@@ -0,0 +1,87 @@
+#ifndef clsfy_binary_hyperplane_gmrho_builder_h
+#define clsfy_binary_hyperplane_gmrho_builder_h
+
+//:
+// \file
+// \author Martin Roberts
+// \brief Builder for linear 2-state classifier, using a sigmoidal Geman-McClure rho function 
+
+#include <vcl_string.h>
+#include <vcl_functional.h>
+#include <vnl/io/vnl_io_vector.h>
+#include <vnl/io/vnl_io_matrix.h>
+#include <vnl/vnl_matrix.h>
+#include <vnl/vnl_vector.h>
+#include <clsfy/clsfy_binary_hyperplane_ls_builder.h>
+
+//=======================================================================
+
+
+//: Builder for linear 2-state classifier
+//  Uses a Geman-McClure robust function, rather than a least squares fit, on points that
+//  are not mis-classified
+//  This increases the weighting given to points near the boundary in determining its fit
+//  A conventional least squares fit is perform first to determine a starting solution,
+//  and also the sigma scaling factor used in the GM function.
+//  Several iterations are performed during which sigma is reduced
+// (i.e. deterministic annealing), to try and avoid local minima
+
+class clsfy_binary_hyperplane_gmrho_builder  : public clsfy_binary_hyperplane_ls_builder {
+private:
+    //: The classifier weights (weight N is the constant)
+    mutable vnl_vector<double> weights_;
+
+    //: Number of training examples (data.rows())
+    mutable unsigned num_examples_;
+    //: Number of variables (data.cols())
+    mutable unsigned num_vars_;
+    //: Tolerance for non-linear optimiser convergence
+    mutable double epsilon_;
+    //: Estimate the scale (sigma) used in the Geman-McClure function
+    //This is increased by the mis-classification overlap region if any
+    double estimate_sigma(const vnl_matrix<double>& data,
+                          const vnl_vector<double >& y) const;
+    //: Determine the weights for the hyperplane
+    void determine_weights(const vnl_matrix<double>& data,
+                           const vnl_vector<double >& y,
+                           double sigma) const;
+  public:
+
+
+  // Dflt ctor
+  clsfy_binary_hyperplane_gmrho_builder():
+      clsfy_binary_hyperplane_ls_builder(),
+      num_examples_(0),num_vars_(0),epsilon_(1.0E-8) {}
+
+  //: Build a linear classifier, with the given data.
+  // Return the mean error over the training set.
+  double build(clsfy_classifier_base &classifier,
+               mbl_data_wrapper<vnl_vector<double> > &inputs,
+               const vcl_vector<unsigned> &outputs) const;
+
+  //: Version number for I/O
+  short version_no() const;
+
+  //: Name of the class
+  vcl_string is_a() const;
+
+  //: Name of the class
+  virtual bool is_class(vcl_string const& s) const;
+
+  //: Print class to os
+  void print_summary(vcl_ostream& os) const;
+
+  //: Create a deep copy.
+  // client is responsible for deleting returned object.
+  virtual clsfy_builder_base* clone() const;
+
+  virtual void b_write(vsl_b_ostream &) const;
+  virtual void b_read(vsl_b_istream &);
+};
+  
+#endif // clsfy_binary_hyperplane_gmrho_builder.h_h
+
+
+
+
+
