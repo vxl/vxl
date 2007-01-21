2b39d2c5c6d8f43ef9f7284128b15d35d432d61d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-01-21 12:09:05 -0500

    avoid compiler warning (unused variables)

--- contrib/rpl/rgrl/tests/test_matcher.cxx
+++ contrib/rpl/rgrl/tests/test_matcher.cxx
@@ -24,10 +24,10 @@
 
 #include "test_util.h"
 
-namespace {
-
-  void test_matcher_k_nearest_boundary() {
-
+namespace
+{
+  void test_matcher_k_nearest_boundary()
+  {
     // create the transformation
     vnl_matrix<double> A( 2, 2 );
     vnl_vector<double> t( 2 );
@@ -52,19 +52,19 @@ namespace {
     // Create a vector of rgrl_features of the appropriate type.
     //
     vcl_vector<rgrl_feature_sptr> from_pts, to_pts;
-    
-    from_pts.push_back(new rgrl_feature_trace_pt( vnl_double_2(10.0, 20.0), 
+
+    from_pts.push_back(new rgrl_feature_trace_pt( vnl_double_2(10.0, 20.0),
                                                   vnl_double_2(0.0, 1.0),
                                                   5, 5));
-    from_pts.push_back(new rgrl_feature_trace_pt( vnl_double_2(35.0, -20.0), 
-                                                  vnl_double_2(1.0, 0.0), 
-                                                  5, 5)); 
-    to_pts.push_back(new rgrl_feature_trace_pt( vnl_double_2( 25.0, 16.0), 
+    from_pts.push_back(new rgrl_feature_trace_pt( vnl_double_2(35.0, -20.0),
+                                                  vnl_double_2(1.0, 0.0),
+                                                  5, 5));
+    to_pts.push_back(new rgrl_feature_trace_pt( vnl_double_2( 25.0, 16.0),
                                                 vnl_double_2(0.0, 1.0),
                                                 5, 5));
-    to_pts.push_back(new rgrl_feature_trace_pt( vnl_double_2(38.0, -12.0), 
-                                                vnl_double_2(-1.0, 0.0), 
-                                                5, 5)); 
+    to_pts.push_back(new rgrl_feature_trace_pt( vnl_double_2(38.0, -12.0),
+                                                vnl_double_2(-1.0, 0.0),
+                                                5, 5));
 
     // Create feature sets from the vector of features.
     //
@@ -77,7 +77,7 @@ namespace {
                                                                *view, *trans, *scale);
     testlib_test_begin( "Correct number of matches" );
     testlib_test_perform( match_set->from_size() == 4 );
-    
+
     vcl_vector<vnl_vector<double> > from_bd_locs;
     vcl_vector<vnl_vector<double> > to_bd_locs;
     vcl_vector<vnl_vector<double> > xformed_bd_locs;
@@ -85,8 +85,8 @@ namespace {
     typedef rgrl_match_set::from_iterator FIter;
     typedef FIter::to_iterator TIter;
 
-    for( FIter fi = match_set->from_begin(); fi != match_set->from_end(); ++fi ) {
-      for( TIter ti = fi.begin(); ti != fi.end(); ++ti ) {
+    for ( FIter fi = match_set->from_begin(); fi != match_set->from_end(); ++fi ) {
+      for ( TIter ti = fi.begin(); ti != fi.end(); ++ti ) {
         from_bd_locs.push_back( fi.from_feature()->location() );
         to_bd_locs.push_back( ti.to_feature()->location() );
         xformed_bd_locs.push_back(A*fi.from_feature()->location() +t);
@@ -98,36 +98,34 @@ namespace {
                           close( from_bd_locs[1], vnl_double_2(15, 20)) );
 
     testlib_test_begin( "Correct boundary matches, same side" );
-    testlib_test_perform( close( to_bd_locs[0], xformed_bd_locs[0]+diff1 ) && 
+    testlib_test_perform( close( to_bd_locs[0], xformed_bd_locs[0]+diff1 ) &&
                           close( to_bd_locs[1], xformed_bd_locs[1]+diff1 ));
-   
+
     testlib_test_begin( "Correct boundary matches, opposite side" );
-    testlib_test_perform( close( to_bd_locs[2], xformed_bd_locs[2]+diff2 ) && 
+    testlib_test_perform( close( to_bd_locs[2], xformed_bd_locs[2]+diff2 ) &&
                           close( to_bd_locs[3], xformed_bd_locs[3]+diff2 ));
-   
   }
-  
-  struct dist_id {
+
+  struct dist_id
+  {
     double dist_;
     unsigned id_;
-    
+
     dist_id()
-    : dist_(-1), id_(0)
-    {}
-    
+    : dist_(-1), id_(0) {}
+
     dist_id( double d, unsigned id )
-    : dist_(d), id_(id)
-    { }
-    
-    bool operator< (dist_id const& rhs ) const 
+    : dist_(d), id_(id) {}
+
+    bool operator< (dist_id const& rhs ) const
     { return this->dist_ < rhs.dist_; }
 
-    bool operator== (dist_id const& rhs ) const 
+    bool operator== (dist_id const& rhs ) const
     { return this->dist_ == rhs.dist_; }
   };
 
-  void test_matcher_k_nearest() {
-
+  void test_matcher_k_nearest()
+  {
     // the points layout
     //
     //    t1(0,0)                                           t2(15,0)
@@ -137,11 +135,10 @@ namespace {
     //    t3(0, 10)                                         t4(15, 10)
     //                 m3(4, 16)                                                   m4(30, 30)
     //
-    
+
     // create the transformation
     vnl_matrix<double> A( 2, 2 );
     vnl_vector<double> t( 2 );
-    vnl_double_2 diff1(12.0, 0.0), diff2(0.0, 12.0);
     vnl_matrix<double> covar( 6, 6 );
 
     A(0,0) = 1.0;  A(0,1) = 0.0;
@@ -181,28 +178,27 @@ namespace {
     to_set = new rgrl_feature_set_bins_2d( to_pts );
 
     // iterate through different matcher and different k value
-    for( unsigned id=0; id<2; ++id )
-      for( unsigned k=1; k<4; ++k ) {
-        
+    for ( unsigned id=0; id<2; ++id )
+      for ( unsigned k=1; k<4; ++k )
+      {
         rgrl_matcher_sptr matcher;
         vcl_string str;
-        switch( id ) {
-          case 0:     matcher = new rgrl_matcher_k_nearest(k);
-                      str = "rgrl_matcher_k_nearest";
-                      break;
-          case 1:     matcher = new rgrl_matcher_k_nearest_adv(k);
-                      str = "rgrl_matcher_k_nearest_adv";
-                      break;
+        switch (id)
+        {
+          case 0: matcher = new rgrl_matcher_k_nearest(k);
+                  str = "rgrl_matcher_k_nearest";
+                  break;
+          case 1: matcher = new rgrl_matcher_k_nearest_adv(k);
+                  str = "rgrl_matcher_k_nearest_adv";
+                  break;
         }
         str += vul_sprintf( "(%1d)", k );
-        
+
         // get the match_set
         rgrl_match_set_sptr match_set =  matcher->compute_matches( *from_set, *to_set,
-                                                               *view, *trans, *scale);
-    
+                                                                   *view, *trans, *scale);
         TEST( (str + ": size of match set").c_str(), match_set->from_size(), 4 );
-    
-    
+
         // check nearest neighbors
         //
         typedef rgrl_match_set::from_iterator FIter;
@@ -210,22 +206,21 @@ namespace {
         bool neighbor_size_is_k = true;
         bool nearest_correct = true;
         vcl_vector<dist_id> dist(4);
-        
-        for( FIter fi = match_set->from_begin(); fi != match_set->from_end(); ++fi ) {
-          
+
+        for ( FIter fi = match_set->from_begin(); fi != match_set->from_end(); ++fi )
+        {
           // check neighbor size first
           neighbor_size_is_k = neighbor_size_is_k && (fi.size() == k);
 
           // build neighbor distance vector
-          for( unsigned i=0; i<to_pts.size(); ++i )
+          for ( unsigned i=0; i<to_pts.size(); ++i )
             dist[i] = dist_id( (to_pts[i]->location() - fi.mapped_from_feature()->location()).squared_magnitude(), i );
           vcl_sort( dist.begin(), dist.end() );
-          
+
           unsigned j=0;
-          for( TIter ti = fi.begin(); ti != fi.end(); ++ti,++j ) {
-            
+          for ( TIter ti = fi.begin(); ti != fi.end(); ++ti,++j )
+          {
             nearest_correct =  nearest_correct && ( ti.to_feature() == to_pts[ dist[j].id_ ] );
-            
           }
         }
         TEST( (str+": all matches have k neighbors").c_str(), neighbor_size_is_k, true );
@@ -233,21 +228,20 @@ namespace {
     }
   }
 
-  void test_matcher_k_nearest_pick_one() {
-
+  void test_matcher_k_nearest_pick_one()
+  {
     // the points layout, the 1st and 2nd are coordinate and 3rd is the scale
     //
     //    t1(0,0,100)                                           t3(15,0,20)
-    //                 m1(4, 4, 1.0) 
+    //                 m1(4, 4, 1.0)
     //
     //
     //    t2(0, 10, 0.02)                                         t4(15, 10,5)
     //
-    
+
     // create the transformation
     vnl_matrix<double> A( 2, 2 );
     vnl_vector<double> t( 2 );
-    vnl_double_2 diff1(12.0, 0.0), diff2(0.0, 12.0);
     vnl_matrix<double> covar( 6, 6 );
 
     A(0,0) = 1.0;  A(0,1) = 0.0;
@@ -289,19 +283,17 @@ namespace {
     to_set = new rgrl_feature_set_bins_2d( to_pts );
 
     // iterate through different matcher and different k value
-    for( unsigned k=1; k<4; ++k ) {
-      
+    for ( unsigned k=1; k<4; ++k )
+    {
       rgrl_matcher_sptr matcher = new rgrl_matcher_k_nearest_pick_one(k);
       vcl_string str = "rgrl_matcher_k_nearest_pick_one";
       str += vul_sprintf( "(%1d)", k );
-      
+
       // get the match_set
       rgrl_match_set_sptr match_set =  matcher->compute_matches( *from_set, *to_set,
-                                                             *view, *trans, *scale);
-  
+                                                                 *view, *trans, *scale);
       TEST( (str + ": size of match set").c_str(), match_set->from_size(), 1 );
-  
-  
+
       // check nearest neighbors
       //
       typedef rgrl_match_set::from_iterator FIter;
@@ -309,17 +301,17 @@ namespace {
       bool neighbor_size_is_k = true;
       bool nearest_correct = true;
       vcl_vector<dist_id> dist(4);
-      
-      for( FIter fi = match_set->from_begin(); fi != match_set->from_end(); ++fi ) {
-        
+
+      for ( FIter fi = match_set->from_begin(); fi != match_set->from_end(); ++fi )
+      {
         // check neighbor size first
         neighbor_size_is_k = neighbor_size_is_k && (fi.size() == 1);
 
         // build neighbor distance vector
-        for( unsigned i=0; i<to_pts.size(); ++i )
+        for ( unsigned i=0; i<to_pts.size(); ++i )
           dist[i] = dist_id( (to_pts[i]->location() - fi.mapped_from_feature()->location()).squared_magnitude(), i );
         vcl_sort( dist.begin(), dist.end() );
-        
+
         nearest_correct =  nearest_correct && ( fi.begin().to_feature() == to_pts[ k-1 ] );
       }
       TEST( (str+": all matches have k neighbors").c_str(), neighbor_size_is_k, true );
@@ -331,12 +323,8 @@ namespace {
 MAIN( test_matcher )
 {
   START( "various matcher" );
-
   test_matcher_k_nearest_boundary();
-
   test_matcher_k_nearest();
-  
   test_matcher_k_nearest_pick_one();
-  
   SUMMARY();
 }
