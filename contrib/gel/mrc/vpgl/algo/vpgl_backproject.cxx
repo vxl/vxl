bd573acd677d1e0a9ed2c6cbf649cddd3c0ecd79
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-01-22 02:01:31 -0500

    now using vnl_double_2 instead of vnl_vector<double>

--- contrib/gel/mrc/vpgl/algo/vpgl_backproject.cxx
+++ contrib/gel/mrc/vpgl/algo/vpgl_backproject.cxx
@@ -8,7 +8,7 @@
 #include <vgl/vgl_plane_3d.h>
 #include <vnl/algo/vnl_amoeba.h>
 
-//: backproject an image point onto a world plane 
+//: backproject an image point onto a world plane
 
 bool vpgl_backproject::bproj_plane(vpgl_rational_camera<double> const& rcam,
                                    vnl_double_2 const& image_point,
@@ -16,24 +16,25 @@ bool vpgl_backproject::bproj_plane(vpgl_rational_camera<double> const& rcam,
                                    vnl_double_3 const& initial_guess,
                                    vnl_double_3& world_point)
 {
-  vpgl_invmap_cost_function  cf(image_point, plane, &rcam);
-  vnl_vector<double> x(2,0.0);
-  
-  cf.set_params(initial_guess, x);
+  vpgl_invmap_cost_function cf(image_point, plane, &rcam);
+  vnl_double_2 x1(0.0, 0.0);
+
+  cf.set_params(initial_guess, x1);
   vnl_amoeba amoeba(cf);
   amoeba.set_max_iterations(1000);
+  vnl_vector<double> x(&x1[0], 2);
   amoeba.minimize(x);
   cf.point_3d(x, world_point);
   vnl_double_2 final_proj = rcam.project(world_point);
   double err = vcl_sqrt(cf.f(x));
-  if(err>.05)//greater than a 20th of a pixel
+  if (err>.05) // greater than a 20th of a pixel
     return false;
   return true;
 }
-            
+
 bool vpgl_backproject::bproj_plane(vpgl_rational_camera<double> const& rcam,
                                    vgl_point_2d<double> const& image_point,
-                                   vgl_plane_3d<double> const& plane, 
+                                   vgl_plane_3d<double> const& plane,
                                    vgl_point_3d<double> const& initial_guess,
                                    vgl_point_3d<double>& world_point)
 {
@@ -43,7 +44,7 @@ bool vpgl_backproject::bproj_plane(vpgl_rational_camera<double> const& rcam,
   vnl_double_4 pl;
   ipt[0]=image_point.x(); ipt[1]=image_point.y();
   pl[0]=plane.a(); pl[1]=plane.b(); pl[2]=plane.c(); pl[3]=plane.d();
-  ig[0]=initial_guess.x();   ig[1]=initial_guess.y();  ig[2]=initial_guess.z();
+  ig[0]=initial_guess.x();  ig[1]=initial_guess.y();  ig[2]=initial_guess.z();
   bool success = vpgl_backproject::bproj_plane(rcam, ipt, pl, ig, wp);
   world_point.set(wp[0], wp[1], wp[2]);
   return success;
