580cf332f681936334cf13d533efd290936d4237
Kevin de Souza <kdesouza@users.sourceforge.net>
2007-01-22 09:40:19 -0500

    Fixed a number of long-standing errors, particularly with Similarity and Affine transforms. Added some tests, including one which detects the error I've just fixed.

--- contrib/mul/vimt3d/tests/test_transform_3d.cxx
+++ contrib/mul/vimt3d/tests/test_transform_3d.cxx
@@ -1,6 +1,7 @@
 // This is mul/vimt3d/tests/test_transform_3d.cxx
 #include <vcl_iostream.h>
 #include <vpl/vpl.h> // vpl_unlink()
+#include <vnl/vnl_math.h>
 #include <vnl/vnl_vector.h>
 #include <vnl/vnl_matrix.h>
 #include <vgl/vgl_distance.h>
@@ -48,6 +49,82 @@ static void test_products(const vimt3d_transform_3d& trans1)
   test_product(trans0,trans1);
 }
 
+
+//=========================================================================
+// Create separate scaling, rotation and translation transforms and compose. 
+// Check that this is consistent with creating the transform directly. 
+// NB This test fails for versions of Similarity and Affine transforms for
+// versions of vimt3d_transform_3d.cxx prior to 1.13
+//=========================================================================
+static void test_composition_and_construction(
+  vimt3d_transform_3d::Form form)
+{
+  if (form==vimt3d_transform_3d::Identity || 
+      form==vimt3d_transform_3d::Translation)
+    return;
+
+  // Define some parameters for scaling, rotation and translations components.
+  double sx=0.7, sy=1.3, sz=2.1;
+  double rx=vnl_math::pi/18, ry=-vnl_math::pi/36, rz=vnl_math::pi/21.34;
+  double tx=-3.7, ty=2.2, tz=5.1;
+
+  // Create separate scaling, rotation and translation transforms
+  vimt3d_transform_3d T, Sa, Si, R;
+  Si.set_zoom_only(sx, 0, 0, 0);         // (isotropic) scaling only
+  Sa.set_zoom_only(sx, sy, sz, 0, 0, 0); // (anisotropic) scaling only
+  R.set_rigid_body(rx, ry, rz, 0, 0, 0); // rotation only
+  T.set_translation(tx, ty, tz);
+
+  // Compose appropriate components
+  // Create the equivalent affine transform directly.
+  vimt3d_transform_3d A;
+  vimt3d_transform_3d B;
+  switch (form)
+  {
+  case vimt3d_transform_3d::ZoomOnly:
+    A = T * Sa;
+    B.set_zoom_only(sx, sy, sz, tx, ty, tz);
+    break;
+  case vimt3d_transform_3d::RigidBody:
+    A = T * R;
+    B.set_rigid_body(rx, ry, rz, tx, ty, tz);
+    break;
+  case vimt3d_transform_3d::Similarity:
+    A = T * R * Si;
+    B.set_similarity(sx, rx, ry, rz, tx, ty, tz);
+    break;
+  case vimt3d_transform_3d::Affine:
+    A = T * R * Sa;
+    B.set_affine(sx, sy, sz, rx, ry, rz, tx, ty, tz);
+    break;
+  default:
+    vcl_cerr << "test_composition_and_construction() is only intended for "
+      << "forms ZoomOnly, RigidBody, Similarity, Affine" << vcl_endl;
+    break;
+  }
+
+  // Test equivalence
+  bool transf_ok = A==B;
+  TEST("Construction and composition", transf_ok, true);
+#ifndef NDEBUG
+  if (!transf_ok)
+  {
+    vcl_cout << "Composition: \n";
+    A.print_all(vcl_cout);
+    vcl_cout << vcl_endl;
+
+    vcl_cout << "Construction: \n";
+    B.print_all(vcl_cout);
+    vcl_cout << vcl_endl;
+  }    
+#endif
+}
+
+
+
+//=========================================================================
+// Series of tests to apply to a transform
+//=========================================================================
 static void test_the_transform(vimt3d_transform_3d& t)
 {
    vimt3d_transform_3d t_inv = t.inverse();
@@ -63,8 +140,60 @@ static void test_the_transform(vimt3d_transform_3d& t)
    TEST_NEAR("matrix() correct",(I-I0).absolute_value_max(),0.0,1e-6);
 
    test_products(t);
+
+   test_composition_and_construction(t.form());
 }
 
+
+//=========================================================================
+// Special test for the set_affine(p,u,v,w) method
+//=========================================================================
+static void test_affine_puvw()
+{
+  // Define some parameters for scaling, rotation and translations components.
+  double sx=0.7, sy=1.3, sz=2.1;
+  double rx=vnl_math::pi/18, ry=-vnl_math::pi/36, rz=vnl_math::pi/21.34;
+  double tx=-3.7, ty=2.2, tz=5.1;
+
+  // Create an affine transform specifying scale, rotation, translation components
+  vimt3d_transform_3d A;
+  A.set_affine(sx, sy, sz, rx, ry, rz, tx, ty, tz);
+
+  // Apply this transform to the default origin and axis vectors
+  vgl_point_3d<double> p = A(0,0,0);
+  vgl_vector_3d<double> u = A(1,0,0) - p;
+  vgl_vector_3d<double> v = A(0,1,0) - p;
+  vgl_vector_3d<double> w = A(0,0,1) - p;
+  
+  // Create an affine transform using p,u,v,w
+  vimt3d_transform_3d B;
+  B.set_affine(p, u, v, w);
+  
+  // Test equivalence - NB operator== is not good enough for this test.
+  bool transf_ok = (A.matrix()-B.matrix()).absolute_value_max()<1e-8;
+  TEST("test_affine_puvw()", transf_ok, true);
+#ifndef NDEBUG
+  if (!transf_ok)
+  {
+    vcl_cout << "set_affine(sx, sy, sz, rx, ry, rz, tx, ty, tz): \n";
+    A.print_all(vcl_cout);
+    vcl_cout << vcl_endl;
+
+    vcl_cout << "set_affine(p, u, v, w): \n";
+    B.print_all(vcl_cout);
+    vcl_cout << vcl_endl;
+
+    vnl_matrix<double> M = A.matrix();
+    vnl_matrix<double> N = B.matrix();
+    vcl_cout << "Max diff= " << (M-N).absolute_value_max() << vcl_endl;
+  }    
+#endif
+}
+
+
+//=========================================================================
+// Main testing function
+//=========================================================================
 static void test_transform_3d()
 {
   vcl_cout << "*****************************\n"
@@ -75,33 +204,34 @@ static void test_transform_3d()
   vgl_point_3d<double> p0(1,2,0),p1;
 
   trans0.set_identity();
-  vcl_cout<<"== Testing Identity ==\n";
+  vcl_cout<<"\n== Testing Identity ==\n";
   TEST_NEAR("Identity transform",vgl_distance(trans0(p0),p0),0.0,1e-8);
   test_the_transform(trans0);
 
-  vcl_cout<<"== Testing Translation ==\n";
+  vcl_cout<<"\n== Testing Translation ==\n";
   trans0.set_translation(1.2, 3.4,0);
   p1 = vgl_point_3d<double>(2.2,5.4,0);
   TEST_NEAR("Translation transform",vgl_distance(trans0(p0),p1),0.0,1e-8);
   test_the_transform(trans0);
 
-  vcl_cout<<"== Testing ZoomOnly ==\n";
+  vcl_cout<<"\n== Testing ZoomOnly ==\n";
   trans0.set_zoom_only(2, 3, 4,0);
   p1 = vgl_point_3d<double>(5,8,0);
   TEST_NEAR("Zoom transform",vgl_distance(trans0(p0),p1),0.0,1e-8);
   test_the_transform(trans0);
 
-  vcl_cout<<"== Testing RigidBody ==\n";
+  vcl_cout<<"\n== Testing RigidBody ==\n";
   trans0.set_rigid_body(1.2,1,5,1,2,3);
   test_the_transform(trans0);
 
-  vcl_cout<<"== Testing Similarity ==\n";
+  vcl_cout<<"\n== Testing Similarity ==\n";
   trans0.set_similarity(0.51,2,3,4,0.2,1,-4);
   test_the_transform(trans0);
 
-   vcl_cout<<"== Testing Affine ==\n";
-   trans0.set_affine(0.2,-0.3,4,2,1,4,5,0.1,-0.21);
-   test_the_transform(trans0);
+  vcl_cout<<"\n== Testing Affine ==\n";
+  trans0.set_affine(0.2,-0.3,4,2,1,4,5,0.1,-0.21);
+  test_the_transform(trans0);
+  test_affine_puvw();
 
   // -------- Test the binary I/O --------
 
