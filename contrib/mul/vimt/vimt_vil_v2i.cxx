07a3c483428b212130e9bc8052b929babe4f557a
Ian Scott <scottim@users.sourceforge.net>
2007-02-15 17:45:55 -0500

    formalise handling of v2i images

---
+++ contrib/mul/vimt/vimt_vil_v2i.cxx
@@ -0,0 +1,414 @@
+//:
+// \file
+
+#include "vimt_vil_v2i.h"
+
+#include <vcl_cassert.h>
+#include <vcl_cstring.h>
+#include <vcl_fstream.h>
+
+#include <vxl_config.h> // for VXL_BIG_ENDIAN and vxl_byte
+
+#include <vil/vil_property.h>
+#include <vil/vil_stream.h>
+#include <vil/vil_stream_fstream.h>
+#include <vil/vil_image_resource.h>
+#include <vil/vil_image_view.h>
+#include <vsl/vsl_binary_io.h>
+#include <vimt/vimt_image_2d_of.h>
+
+// v2i magic number
+const unsigned V2I_MAGIC = 987123872U;
+
+
+class vimt_vil_fstream: vil_stream_fstream
+{
+protected:
+  vcl_fstream& underlying_stream() { return vil_stream_fstream::underlying_stream(); }
+  friend class vimt_vil_v2i_format;
+  friend class vimt_vil_v2i_image;
+private:
+  vimt_vil_fstream(): vil_stream_fstream("", "") {} // Will never be constructed
+};
+
+
+
+vil_image_resource_sptr vimt_vil_v2i_format::make_input_image(vil_stream* vs) const
+{
+  // First of all hack an vil_stream to get at the underlying
+  // vcl_stream, in order to create a vsl_b_stream
+  if (typeid(*vs) != typeid(vil_stream_fstream))
+  {
+    vcl_cerr << "vimt_vil_v2i_format::make_input_image() WARNING\n"
+             << "  Unable to deal with stream type" << vcl_endl;
+
+    return 0;
+  }
+  vsl_b_istream vslstream( static_cast<vimt_vil_fstream *>(vs)->underlying_stream());
+  if (!vslstream) return 0; // Not even a vsl file.
+  unsigned magic;
+  vil_streampos start = vs->tell();
+  vsl_b_read(vslstream, magic);
+  if (magic == V2I_MAGIC)
+    return new vimt_vil_v2i_image(vs);
+
+  // This vsl stream may hav been created with vsl_b_wrte(vimt_image) - with no
+  //  v2i magic number. Check if that is true.
+  vs->seek(start);
+  int dummy;
+  vsl_b_read(vslstream, dummy);  // vimt_image_2d_of.version
+  vsl_b_read(vslstream, dummy);  // vil_image_view.version
+  vsl_b_read(vslstream, dummy);  // vil_image_view.ni
+  vsl_b_read(vslstream, dummy);  // vil_image_view.nj
+  vsl_b_read(vslstream, dummy);  // vil_image_view.nplanes
+  vsl_b_read(vslstream, dummy);  // vil_image_view.istep
+  vsl_b_read(vslstream, dummy);  // vil_image_view.jstep
+  vsl_b_read(vslstream, dummy);  // vil_image_view.planestep
+  vsl_b_read(vslstream, dummy);  // vil_memory_chunk.version
+  vsl_b_read(vslstream, dummy);  //  chunk.pixel_format
+  vil_pixel_format f = static_cast<vil_pixel_format>(dummy);
+  switch(f)
+  {
+#define macro( F , T ) \
+    case  F : \
+    { vimt_image_2d_of< T > im; vsl_b_read(is, im); if (!is) return 0; } \
+    break;
+macro(VIL_PIXEL_FORMAT_BYTE , vxl_byte )
+//macro(VIL_PIXEL_FORMAT_SBYTE , vxl_sbyte )
+//macro(VIL_PIXEL_FORMAT_UINT_32 , vxl_uint_32 )
+//macro(VIL_PIXEL_FORMAT_UINT_16 , vxl_uint_16 )
+macro(VIL_PIXEL_FORMAT_INT_32 , vxl_int_32 )
+macro(VIL_PIXEL_FORMAT_INT_16 , vxl_int_16 )
+//macro(VIL_PIXEL_FORMAT_BOOL , bool )
+macro(VIL_PIXEL_FORMAT_FLOAT , float )
+//macro(VIL_PIXEL_FORMAT_DOUBLE , double )
+#undef macro
+    default: return 0; // Unknown pixel type - or more likely not a v2i image.
+  }
+  vs->seek(start);
+  return new vimt_vil_v2i_image(vslstream, f);
+}
+
+vil_image_resource_sptr vimt_vil_v2i_format::make_output_image(vil_stream* vs,
+                                                               unsigned ni,
+                                                               unsigned nj,
+                                                               unsigned nplanes,
+                                                               vil_pixel_format format) const
+{
+  // First of all hack an vil_stream to get at the underlying
+  // ccl_stream, in order to create a vsl_b_stream
+  if (typeid(*vs) != typeid(vil_stream_fstream))
+  {
+    vcl_cerr << "vimt_vil_v2i_format::make_output_image() WARNING\n"
+             << "  Unable to deal with stream type" << vcl_endl;
+
+    return 0;
+  }
+  if ( format != VIL_PIXEL_FORMAT_BYTE && format != VIL_PIXEL_FORMAT_SBYTE &&
+       format != VIL_PIXEL_FORMAT_UINT_32 && format != VIL_PIXEL_FORMAT_INT_32 &&
+       format != VIL_PIXEL_FORMAT_UINT_16 && format != VIL_PIXEL_FORMAT_INT_16 &&
+       format != VIL_PIXEL_FORMAT_FLOAT && format != VIL_PIXEL_FORMAT_DOUBLE &&
+       format != VIL_PIXEL_FORMAT_BOOL)
+  {
+    vcl_cerr << "vimt_vil_v2i_format::make_output_image() WARNING\n"
+             << "  Unable to deal with file format : " << format << vcl_endl;
+    return 0;
+  }
+  return new vimt_vil_v2i_image(vs, ni, nj, nplanes, format);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+vimt_vil_v2i_image::vimt_vil_v2i_image(vil_stream* vs):
+  vs_(vs), im_(0), dirty_(false)
+{
+  vs_->ref();
+  vs_->seek(0L);
+  vsl_b_istream vslstream(& static_cast<vimt_vil_fstream &>(vs_).underlying_stream());
+
+  unsigned magic;
+  vsl_b_read(is, magic);
+  assert(magic == V2I_MAGIC);
+
+  short version;
+  vsl_b_read(is, version);
+
+  switch (version)
+  {
+  case 1:
+    {
+      vimt_image *p_im=0;
+      vsl_b_read(is, p_im);
+      im_ = dynamic_cast<vimt2d_image_2d *>(p_im);
+      break;
+    }
+  default:
+    vcl_cerr << "I/O ERROR: vimt_vil_v2i_image::vimt_vil_v2i_image()\n"
+      << "           Unknown version number "<< version << '\n';
+    return;
+  }
+}
+
+
+//: Constructor to deal with directly created v2i files.
+vimt_vil_v2i_image::vimt_vil_v2i_image(vil_stream* vs, vil_pixel_format f):
+  vs_(vs), im_(0), dirty_(false)
+{
+  vs_->ref();
+  vs_->seek(0L);
+  vsl_b_istream vslstream(& static_cast<vimt_vil_fstream &>(vs_).underlying_stream());
+
+  
+  switch(f)
+  {
+#define macro( F , T ) \
+    case  F : \
+    im_ = new vimt_image_2d_of< T >(); \
+    vsl_b_read(vslstream, *static_cast<vimt_image_2d_of< T >*>(im_)); \
+    break;
+macro(VIL_PIXEL_FORMAT_BYTE , vxl_byte )
+//macro(VIL_PIXEL_FORMAT_SBYTE , vxl_sbyte )
+//macro(VIL_PIXEL_FORMAT_UINT_32 , vxl_uint_32 )
+//macro(VIL_PIXEL_FORMAT_UINT_16 , vxl_uint_16 )
+macro(VIL_PIXEL_FORMAT_INT_32 , vxl_int_32 )
+macro(VIL_PIXEL_FORMAT_INT_16 , vxl_int_16 )
+//macro(VIL_PIXEL_FORMAT_BOOL , bool )
+macro(VIL_PIXEL_FORMAT_FLOAT , float )
+//macro(VIL_PIXEL_FORMAT_DOUBLE , double )
+#undef macro
+    default: return 0; // Unknown pixel type - or more likely not a v2i image.
+  }
+
+
+  assert(! !vslstream);
+}
+
+bool vimt_vil_v2i_image::get_property(char const * key, void * value) const
+{
+  const vimt_transform_2d &tr = im_->world2im();
+
+  if (vcl_strcmp(vil_property_voxel_size, key)==0)
+  {
+    vgl_vector_2d<double> p11 = tr.inverse()(1.0, 1.0) - tr.inverse().origin();
+    //Assume no rotation or shearing.
+
+    float* array =  static_cast<float*>(value);
+    array[0] = (float) p11.x();
+    array[1] = (float) p11.y();
+    return true;
+  }
+
+  if (vcl_strcmp(vil3d_property_origin_offset, key)==0)
+  {
+    vgl_point_2d<double> origin = tr.origin();
+    float* array =  static_cast<float*>(value);
+    array[0] = (float)(origin.x());
+    array[1] = (float)(origin.y());
+    return true;
+  }
+
+  return false;
+}
+
+vimt_vil_v2i_image::vimt_vil_v2i_image(vil_stream* vs, unsigned ni, unsigned nj,
+                                       unsigned nplanes, vil_pixel_format format):
+  vs_(vs), im_(0), dirty_(true)
+{
+  switch (format)
+  {
+#define macro( F , T ) \
+   case  F : \
+    im_ = new vimt_image_2d_of< T > (ni, nj, nplanes); \
+    break;
+macro(VIL_PIXEL_FORMAT_BYTE , vxl_byte )
+//macro(VIL_PIXEL_FORMAT_SBYTE , vxl_sbyte )
+//macro(VIL_PIXEL_FORMAT_UINT_32 , vxl_uint_32 )
+//macro(VIL_PIXEL_FORMAT_UINT_16 , vxl_uint_16 )
+macro(VIL_PIXEL_FORMAT_INT_32 , vxl_int_32 )
+macro(VIL_PIXEL_FORMAT_INT_16 , vxl_int_16 )
+//macro(VIL_PIXEL_FORMAT_BOOL , bool )
+macro(VIL_PIXEL_FORMAT_FLOAT , float )
+//macro(VIL_PIXEL_FORMAT_DOUBLE , double )
+#undef macro
+   default:
+    vcl_cerr << "I/O ERROR: vimt_vil_v2i_image::vimt_vil_v2i_image()\n"
+             << "           Unknown vil_pixel_format "<< format << '\n';
+    vcl_abort();
+  }
+}
+
+
+vimt_vil_v2i_image::~vimt_vil_v2i_image()
+{
+  if (dirty_)
+  {
+    vs_->seekp(0);
+    vsl_b_ostream vslstream(& static_cast<vimt_vil_fstream &>(vs_).underlying_stream());
+    vsl_b_ostream os(file_);
+
+    vsl_b_write(os, V2I_MAGIC);
+
+    const short version = 1;
+    vsl_b_write(os, version);
+
+
+    vimt_image *p_im=im_;
+    vsl_b_write(os, p_im);
+  }
+  //delete vs_;
+  vs_->unref();
+  delete im_;
+}
+
+//: Dimensions:  nplanes x ni x nj.
+// This concept is treated as a synonym to components.
+unsigned vimt_vil_v2i_image::nplanes() const
+{
+  return im_->image_base().nplanes();
+}
+
+//: Dimensions:  nplanes x ni x nj.
+// The number of pixels in each row.
+unsigned vimt_vil_v2i_image::ni() const
+{
+  return im_->image_base().ni();
+}
+
+//: Dimensions:  nplanes x ni x nj.
+// The number of pixels in each column.
+unsigned vimt_vil_v2i_image::nj() const
+{
+  return im_->image_base().nj();
+}
+
+//: Pixel Format.
+enum vil_pixel_format vimt_vil_v2i_image::pixel_format() const
+{
+  return im_->image_base().pixel_format();
+}
+
+
+const vimt_transform_2d & vimt_vil_v2i_image::world2im() const
+{
+  return im_->world2im();
+}
+
+void vimt_vil_v2i_image::set_world2im(const vimt_transform_2d & tr)
+{
+  im_->set_world2im(tr);
+  dirty_ = true;
+}
+
+
+//: Create a read/write view of a copy of this data.
+// Currently not yet implemented.
+// \return 0 if unable to get view of correct size.
+vil_image_view_base_sptr vimt_vil_v2i_image::get_copy_view(unsigned i0, unsigned ni,
+                                                           unsigned j0, unsigned nj) const
+{
+  const vil_image_view_base &view = im_->image_base();
+
+  if (i0 + ni > view.ni() || j0 + nj > view.nj() ) return 0;
+
+  switch (view.pixel_format())
+  {
+#define macro( F , T ) \
+   case  F : { \
+    const vil_image_view< T > &v = \
+      static_cast<const vil_image_view< T > &>(view); \
+    vil_image_view< T > w(v.memory_chunk(), &v(i0,j0), \
+                          ni, nj, v.nplanes(), \
+                          v.istep(), v.jstep(), v.planestep()); \
+    return new vil_image_view< T >(vil_copy_deep(w)); }
+macro(VIL_PIXEL_FORMAT_BYTE, vxl_byte )
+//macro(VIL_PIXEL_FORMAT_SBYTE , vxl_sbyte )
+//macro(VIL_PIXEL_FORMAT_UINT_32 , vxl_uint_32 )
+//macro(VIL_PIXEL_FORMAT_UINT_16 , vxl_uint_16 )
+macro(VIL_PIXEL_FORMAT_INT_32 , vxl_int_32 )
+macro(VIL_PIXEL_FORMAT_INT_16 , vxl_int_16 )
+//macro(VIL_PIXEL_FORMAT_BOOL , bool )
+macro(VIL_PIXEL_FORMAT_FLOAT , float )
+//macro(VIL_PIXEL_FORMAT_DOUBLE , double )
+#undef macro
+   default:
+    return 0;
+  }
+}
+
+//: Create a read/write view of a copy of this data.
+// \return 0 if unable to get view of correct size.
+vil_image_view_base_sptr vimt_vil_v2i_image::get_view(unsigned i0, unsigned ni,
+                                                      unsigned j0, unsigned nj) const
+{
+  const vil_image_view_base &view = im_->image_base();
+
+
+  if (i0 + ni > view.ni() || j0 + nj > view.nj()) return 0;
+
+  switch (view.pixel_format())
+  {
+#define macro( F , T ) \
+   case  F : { \
+    const vil_image_view< T > &v = \
+      static_cast<const vil_image_view< T > &>(view); \
+    return new vil_image_view< T >(v.memory_chunk(), &v(i0,j0), \
+                                   ni, nj, v.nplanes(), \
+                                   v.istep(), v.jstep(), v.planestep()); }
+macro(VIL_PIXEL_FORMAT_BYTE , vxl_byte )
+//macro(VIL_PIXEL_FORMAT_SBYTE , vxl_sbyte )
+//macro(VIL_PIXEL_FORMAT_UINT_32 , vxl_uint_32 )
+//macro(VIL_PIXEL_FORMAT_UINT_16 , vxl_uint_16 )
+macro(VIL_PIXEL_FORMAT_INT_32 , vxl_int_32 )
+macro(VIL_PIXEL_FORMAT_INT_16 , vxl_int_16 )
+//macro(VIL_PIXEL_FORMAT_BOOL , bool )
+macro(VIL_PIXEL_FORMAT_FLOAT , float )
+//macro(VIL_PIXEL_FORMAT_DOUBLE , double )
+#undef macro
+   default:
+    return 0;
+  }
+}
+
+
+//: Set the contents of the volume.
+bool vimt_vil_v2i_image::put_view(const vil_image_view_base& vv,
+                                  unsigned i0, unsigned j0)
+{
+  if (!view_fits(vv, i0, j0))
+  {
+    vcl_cerr << "ERROR: " << __FILE__ << ":\n view does not fit\n";
+    return false;
+  }
+
+  if (vv.pixel_format() != im_->image_base().pixel_format())
+  {
+    vcl_cerr << "ERROR: vimt_vil_v2i_image::put_view(). Pixel formats do not match\n";
+    return false;
+  }
+
+
+  dirty_ = true;
+
+  switch (vv.pixel_format())
+  {
+#define macro( F , T ) \
+   case  F : \
+    vil_copy_to_window(static_cast<vil_image_view<T >const&>(vv), \
+                       static_cast<vimt_image_3d_of<T >&>(*im_).image(), i0, j0); \
+    return true;
+
+    macro(VIL_PIXEL_FORMAT_BYTE , vxl_byte )
+//  macro(VIL_PIXEL_FORMAT_SBYTE , vxl_sbyte )
+//  macro(VIL_PIXEL_FORMAT_UINT_32 , vxl_uint_32 )
+//  macro(VIL_PIXEL_FORMAT_UINT_16 , vxl_uint_16 )
+    macro(VIL_PIXEL_FORMAT_INT_32 , vxl_int_32 )
+    macro(VIL_PIXEL_FORMAT_INT_16 , vxl_int_16 )
+//  macro(VIL_PIXEL_FORMAT_BOOL , bool )
+    macro(VIL_PIXEL_FORMAT_FLOAT , float )
+//  macro(VIL_PIXEL_FORMAT_DOUBLE , double )
+#undef macro
+   default:
+    return false;
+  }
+}
---
+++ contrib/mul/vimt/vimt_vil_v2i.h
@@ -0,0 +1,134 @@
+// This is mul/vimt/vimt_vil_v2i.h
+#ifndef vimt_vil_v2i_h_
+#define vimt_vil_v2i_h_
+#ifdef VCL_NEEDS_PRAGMA_INTERFACE
+#pragma interface
+#endif
+//:
+// \file
+// \brief Reader/Writer for v2i format images.
+// \author Ian Scott - Manchester
+// v2i is a VXL specific format designed to store all the information that
+// can be stored in a vimt image. We don't recommed its use in general,
+// having no wish to add to the plethora of badly designed image formats
+// out there. However, a means of being able to save, and reload a full
+// vil/vimt image using the standard vil_load_* API is very useful.
+// The file format currently is that provided by the default vsl
+// serialisation scheme. There is an extra v2i-specific magic number to
+// reduce confusion with other vsl data files, and a version number to allow
+// for a change in format.
+
+#include <vil/vil_file_format.h>
+#include <vimt/vimt_image_2d.h>
+#include <vcl_iosfwd.h>
+
+
+//: Reader/Writer for v2i format images.
+class vimt_vil_v2i_format: public vil_file_format
+{
+ public:
+  vimt_vil_v2i_format() {}
+  //: The destructor must be virtual so that the memory chunk is destroyed.
+  virtual ~vimt_vil_v2i_format() {}
+
+  virtual vil_image_resource_sptr make_input_image(vil_stream* vs) const;
+
+  //: Make a "generic_image" on which put_section may be applied.
+  // The file may be opened immediately for writing so that a header can be written.
+  virtual vil_image_resource_sptr make_output_image(vil_stream* vs,
+                                                      unsigned ni,
+                                                      unsigned nj,
+                                                      unsigned nplanes,
+                                                      enum vil_pixel_format) const;
+
+  //: default filename tag for this image.
+  virtual const char * tag() const {return "v2i";}
+};
+
+
+// You can't create one of these yourself.
+// Use vimt_vil_v2i_format instead.
+class vimt_vil_v2i_image: public vil_image_resource
+{
+  friend class vimt_vil_v2i_format;
+  //: Pointer to open image file.
+  vil_stream *file_;
+  //: Image cache.
+  // Currently the whole image is cached im memory. This should be fixed.
+  vimt_image_2d *im_;
+  //: If true, write image file on exit.
+  bool dirty_;
+
+  //: Expected pixel type.
+  enum vil_pixel_format pixel_format_;
+  //: Private constructor, use vil_load instead.
+  // This object takes ownership of the file.
+  vimt_vil_v2i_image(vil_stream * vs);
+  //: Private constructor, use vil_load instead.
+  // This object takes ownership of the file.
+  vimt_vil_v2i_image(vil_stream * vs, vil_pixel_format f);
+  //: Private constructor, use vil_save instead.
+  // This object takes ownership of the file, for writing.
+  vimt_vil_v2i_image(vil_stream* vs, unsigned ni, unsigned nj,
+                     unsigned nplanes, vil_pixel_format format);
+
+ public:
+  virtual ~vimt_vil_v2i_image();
+
+  //: Dimensions:  nplanes x ni x nj.
+  // This concept is treated as a synonym to components.
+  virtual unsigned nplanes() const;
+  //: Dimensions:  nplanes x ni x nj.
+  // The number of pixels in each row.
+  virtual unsigned ni() const;
+  //: Dimensions:  nplanes x ni x nj.
+  // The number of pixels in each column.
+  virtual unsigned nj() const;
+
+  //: Pixel Format.
+  virtual enum vil_pixel_format pixel_format() const;
+
+  //: Set the size of the each pixel in the i,j directions.
+  // Return false if underlying image doesn't store pixel sizes.
+  virtual bool set_pixel_size(float i, float j);
+  
+  //: Get full world to image transform
+  const vimt_transform_2d & world2im() const;
+
+  //: Set full world to image transform
+  // Call this before using put_view();
+  void set_world2im(const vimt_transform_2d & tr);
+
+  //: Create a read/write view of a copy of this data.
+  // This function will always return a
+  // multi-plane scalar-pixel view of the data.
+  // \return 0 if unable to get view of correct size, or if resource is write-only.
+  virtual vil_image_view_base_sptr get_copy_view(unsigned i0, unsigned ni,
+                                                   unsigned j0, unsigned nj) const;
+
+  //: Create a read/write view of a copy of this data.
+  // This function will always return a
+  // multi-plane scalar-pixel view of the data.
+  // \return 0 if unable to get view of correct size, or if resource is write-only.
+  virtual vil_image_view_base_sptr get_view(unsigned i0, unsigned ni,
+                                              unsigned j0, unsigned nj) const;
+
+  //: Put the data in this view back into the image source.
+  // The view must be of scalar components. Assign your
+  // view to a scalar-component view if this is not the case.
+  // \return false if failed, because e.g. resource is read-only,
+  // format of view is not correct (if it is a compound pixel type, try
+  // assigning it to a multi-plane scalar pixel view.)
+  virtual bool put_view(const vil_image_view_base& im,
+                        unsigned i0, unsigned j0);
+
+  //: Return a string describing the file format.
+  // Only file images have a format, others return 0
+  virtual char const* file_format() const { return "v2i"; }
+
+  //: Extra property information
+  // This will just return the property of the first slice in the list.
+  virtual bool get_property(char const* label, void* property_value = 0) const;
+};
+
+#endif
