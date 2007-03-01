7bb19a06da5482faf6f38f15ad5de8ac5473abef
Gamze Tunali <gamze@users.sourceforge.net>
2007-03-01 08:56:23 -0500

    basic vgl_intersection(...) methods  moved up to vgl/vgl_intersection

--- core/vgl/algo/vgl_intersection.h
+++ core/vgl/algo/vgl_intersection.h
@@ -18,38 +18,11 @@
 #include <vgl/vgl_fwd.h> // forward declare various vgl classes
 #include <vcl_vector.h>
 
-//: Return box defining intersection between boxes a and b.
-//  Empty box returned if no intersection.
-// \relates vgl_box_3d
-template <class T>
-vgl_box_3d<T> intersect(vgl_box_3d<T> const& a,
-                        vgl_box_3d<T> const& b);
-
-//: Return the intersection point of two concurrent lines
-// \relates vgl_line_3d_2_points
-template <class T>
-vgl_point_3d<T> intersection(vgl_line_3d_2_points<T> const& l1,
-                             vgl_line_3d_2_points<T> const& l2);
-
-//: Return the intersection point of a line and a plane.
-// \relates vgl_line_3d_2_points
-// \relates vgl_plane_3d
-template <class T>
-vgl_point_3d<T> intersection(vgl_line_3d_2_points<T> const& line,
-                             vgl_plane_3d<T> const& plane);
-
-//: Return the intersection point of three planes.
-// \relates vgl_plane_3d
-template <class T>
-vgl_point_3d<T> intersection(const vgl_plane_3d<T>& p1,
-                             const vgl_plane_3d<T>& p2,
-                             const vgl_plane_3d<T>& p3);
-
 //: Return the intersection point of vector of planes.
 // \relates vgl_plane_3d
 template <class T>
-vgl_point_3d<T> intersection(const vcl_vector<vgl_plane_3d<T> >& p);
+vgl_point_3d<T> vgl_intersection(const vcl_vector<vgl_plane_3d<T> >& p);
 
-#define VGL_INTERSECTION_INSTANTIATE(T) extern "please include vgl/vgl_intersection.txx first"
+#define VGL_INTERSECTION_INSTANTIATE(T) extern "please include vgl/algo/vgl_intersection.txx first"
 
 #endif // vgl_intersection_h_
