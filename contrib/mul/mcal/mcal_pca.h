2692f2eb633eda05d55fd107f51b1198ef99f6a9
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-03-06 17:36:47 -0500

    added missing #include <iosfwd>

--- contrib/mul/mcal/mcal_pca.h
+++ contrib/mul/mcal/mcal_pca.h
@@ -1,6 +1,5 @@
 #ifndef mcal_pca_h
 #define mcal_pca_h
-
 //:
 // \file
 // \author Tim Cootes
@@ -10,12 +9,14 @@
 #include <vnl/io/vnl_io_vector.h>
 #include <vnl/io/vnl_io_matrix.h>
 #include <mbl/mbl_data_wrapper.h>
+#include <vcl_iosfwd.h>
 
 //: Class to perform Principle Component Analysis
 //  Applies a PCA to compute mean, variance and eigenvectors/values
 //  of covariance matrix of supplied data.
-class mcal_pca : public mcal_component_analyzer {
-private:
+class mcal_pca : public mcal_component_analyzer
+{
+ private:
     //: Define how many modes to use
   double var_prop_;
   int min_modes_,max_modes_;
@@ -24,29 +25,28 @@ private:
   unsigned choose_n_modes(const vnl_vector<double>& evals);
 
   //: Utility function
-  void fillDDt(vnl_matrix<double>& DDt, const vnl_matrix<double>& A, 
+  void fillDDt(vnl_matrix<double>& DDt, const vnl_matrix<double>& A,
                int rlo, int rhi, int clo, int chi);
 
   //: Compute eigenvectors assuming fewer dimensions than samples
   void build_evecs_nd_smaller(mbl_data_wrapper<vnl_vector<double> >& data,
-                                const vnl_vector<double>& mean,
-                                vnl_matrix<double>& evecs,
-                                vnl_vector<double>& evals);
+                              const vnl_vector<double>& mean,
+                              vnl_matrix<double>& evecs,
+                              vnl_vector<double>& evals);
 
   //: Compute eigenvectors assuming fewer samples than dimensions
   void build_evecs_ns_smaller(mbl_data_wrapper<vnl_vector<double> >& data,
-                                const vnl_vector<double>& mean,
-                                vnl_matrix<double>& evecs,
-                                vnl_vector<double>& evals);
+                              const vnl_vector<double>& mean,
+                              vnl_matrix<double>& evecs,
+                              vnl_vector<double>& evals);
 
   //: Max number of doubles allowed in memory
   double max_d_in_memory_;
 
   //: Whether to use chunks if required
   bool use_chunks_;
-    
-protected:
-public:
+
+ public:
 
     //: Dflt ctor
   mcal_pca();
@@ -57,7 +57,7 @@ public:
     //: Define limits on number of parameters to use in model
     // \param var_proportion  Proportion of variance in data to explain
   void set_mode_choice(unsigned min, unsigned max, double var_proportion);
-  
+
 
   //: Set the choice for the minimum number of model
   void set_min_modes( unsigned min );
@@ -68,10 +68,10 @@ public:
   void set_max_modes(unsigned max);
     //: Current upper limit on number of parameters
   unsigned max_modes() const;
-  
+
     //: Define proportion of data variance to explain
   virtual void set_var_prop(double v);
-  
+
     //: Current proportion of data variance to explain
   virtual double var_prop() const;
 
@@ -95,7 +95,7 @@ public:
                                 vnl_matrix<double>& modes,
                                 vnl_vector<double>& mode_var);
 
-    //: Version number for I/O 
+    //: Version number for I/O
   short version_no() const;
 
     //: Name of the class
@@ -106,7 +106,7 @@ public:
 
     //: Print class to os
   virtual void print_summary(vcl_ostream& os) const;
-    
+
     //: Save class to binary file stream
   virtual void b_write(vsl_b_ostream& bfs) const;
 
@@ -126,10 +126,6 @@ public:
   // \endverbatim
   // \throw mbl_exception_parse_error if the parse fails.
   virtual void config_from_stream(vcl_istream & is);
-
 };
 
 #endif // mcal_pca_h
-
-
-
