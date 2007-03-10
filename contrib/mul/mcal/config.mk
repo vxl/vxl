d01ccc503d8a087a25a63bf0d73f0659aeb08238
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-03-10 04:43:51 -0500

    udated w.r.t. CMakeLists.txt

---
+++ contrib/mul/mcal/config.mk
@@ -0,0 +1,5 @@
+IULIBS += -lmcal
+include ${VXLROOT}/contrib/mul/vimt/config.mk
+include ${VXLROOT}/contrib/mul/mbl/config.mk
+include ${VXLROOT}/core/vnl/algo/config.mk
+include ${VXLROOT}/core/vnl/config.mk
