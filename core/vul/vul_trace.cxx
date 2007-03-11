3eb99a14f9195922b226b4df475a5182bf1d554e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-03-11 15:40:24 -0400

    replaced fprintf by std::cerr

--- core/vul/vul_trace.cxx
+++ core/vul/vul_trace.cxx
@@ -1,17 +1,11 @@
 // This is core/vul/vul_trace.cxx
-
-//
-//  fsm
-//
-
 #ifdef VCL_NEEDS_PRAGMA_INTERFACE
 #pragma implementation
 #endif
 #include "vul_trace.h"
+#include <vcl_iostream.h>
 
-#include <vcl_cstdio.h>
-
-void vul_trace_function(char const *file, int line) {
-  vcl_fprintf(stderr, "[vul_trace %s:%d]", file, line);
-  vcl_fflush(stderr);
+void vul_trace_function(char const *file, int line)
+{
+  vcl_cerr << "[vul_trace " << file << ':' << line << ']' << vcl_flush;
 }
