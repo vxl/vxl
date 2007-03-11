c86c26a1bce7d15efc47df23494ceef90311ac8e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-03-11 13:15:46 -0400

    avoid compiler warning (order of member initialisation)

--- core/vil/vil_exception.h
+++ core/vil/vil_exception.h
@@ -40,38 +40,27 @@ void vil_exception_warning(T exception)
 #endif
 }
 
-
-
-#if !VCL_HAS_EXCEPTIONS
-
   //: Indicates that an image assignment failed because the pixel types were incompatible.
   class vil_exception_assignment_pixel_formats_incompatible
-  {
-   public:
-    enum vil_pixel_format src_type, dest_type;
-    vil_exception_assignment_pixel_formats_incompatible(
-      enum vil_pixel_format src, enum vil_pixel_format dest)
-      : src_type(src), dest_type(dest) {}
-    const char * what() const {return "Pixel formats incompatible during assignment operation.";}
-  };
-
-#else
-
-  //: Indicates that an image assignment failed because the pixel types were incompatible.
-  class vil_exception_assignment_pixel_formats_incompatible : public vcl_logic_error
-  {
-   public:
-    enum vil_pixel_format src_type, dest_type;
-    vil_exception_assignment_pixel_formats_incompatible(
-      enum vil_pixel_format src, enum vil_pixel_format dest)
-      : vcl_logic_error(vcl_string("Pixel formats incompatible during assignment operation.")),
-      src_type(src), dest_type(dest) {}
-    virtual ~vil_exception_assignment_pixel_formats_incompatible() throw() {}
-  };
-
+#if VCL_HAS_EXCEPTIONS
+  : public vcl_logic_error
 #endif
+  {
+   public:
+    enum vil_pixel_format src_type, dest_type;
+    vil_exception_assignment_pixel_formats_incompatible(
+      enum vil_pixel_format src, enum vil_pixel_format dest) :
+#if VCL_HAS_EXCEPTIONS
+      vcl_logic_error(vcl_string("Pixel formats incompatible during assignment operation.")),
+#endif
+      src_type(src), dest_type(dest) {}
+#if VCL_HAS_EXCEPTIONS
+    virtual ~vil_exception_assignment_pixel_formats_incompatible() throw() {}
+#else
+    const char * what() const {return "Pixel formats incompatible during assignment operation.";}
+#endif
+  };
 
-#if !VCL_HAS_EXCEPTIONS
 
   //: Indicates that an image load or save operation failed.
   // Generally this should be thrown, only after checks on the image type
@@ -79,37 +68,32 @@ void vil_exception_warning(T exception)
   // unrecoverable error is detected inside the image_resource constructor,
   // or similar.
   class vil_exception_image_io
+#if VCL_HAS_EXCEPTIONS
+  : public vcl_runtime_error
+#endif
   {
    public:
-    vcl_string function_name, file_type, filename, full_what;
-    vil_exception_image_io(const vcl_string& function,
-      const vcl_string& type, const vcl_string& name):
-      function_name(function), file_type(type), filename(name),
-      full_what("Unrecoverable failure in " + function + " while loading "
-        + name + " using " + type + " loader.") {}
-    const char * what() const {return full_what.c_str();}
-  };
-
-#else
-  //: Indicates that an image load or save operation failed.
-  // Generally this should be thrown, only after checks on the image type
-  // have been passed by the file format object, and while an
-  // unrecoverable error is detected inside the image_resource constructor,
-  // or similar.
-  class vil_exception_image_io : public vcl_runtime_error
-  {
-   public:
+#if !VCL_HAS_EXCEPTIONS
+    vcl_string full_what;
+#endif
     vcl_string function_name, file_type, filename;
     vil_exception_image_io(const vcl_string& function,
-      const vcl_string& type, const vcl_string& name):
-      function_name(function), file_type(type), filename(name),
-      vcl_runtime_error("Unrecoverable failure in " + function + 
-        " while loading " + name + " using " + type + " loader.") {}
-    virtual ~vil_exception_image_io() throw() {}
-  };
-
+                           const vcl_string& type,
+                           const vcl_string& name) :
+#if VCL_HAS_EXCEPTIONS
+      vcl_runtime_error
+#else
+      full_what
 #endif
-
+      ("Unrecoverable failure in " + function + " while loading "
+       + name + " using " + type + " loader."),
+      function_name(function), file_type(type), filename(name) {}
+#if VCL_HAS_EXCEPTIONS
+    virtual ~vil_exception_image_io() throw() {}
+#else
+    const char * what() const { return full_what.c_str(); }
+#endif
+  };
 
 #endif // vil_exception_h_
 
