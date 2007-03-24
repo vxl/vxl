f0f1cfedff125c0171996fb209739f585a51f081
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-03-24 11:22:06 -0400

    TABs replaced by spaces

--- contrib/brl/bbas/bvgl/tests/test_misc.cxx
+++ contrib/brl/bbas/bvgl/tests/test_misc.cxx
@@ -1,27 +1,25 @@
-
 //:
 // \file
 #include <vcl_vector.h>
 #include <testlib/testlib_test.h>
 #include <vcl_iostream.h>
+
 class misc
 {
-public:
-	misc():cx_(1),bx_(4){}
- const unsigned cx_;
- unsigned bx_;
-  const unsigned & cx(unsigned const& a) const
-  {if(a>4)
-  return cx_;
-  else
-  return bx_;}
+  const unsigned cx_;
+  unsigned bx_;
+ public:
+  misc() : cx_(1), bx_(4) {}
+  const unsigned& cx(unsigned const& a) const
+  { if (a>4) return cx_; else return bx_; }
 };
+
 static void test_misc()
 {
   misc m;
   const unsigned & x = m.cx(0);
   unsigned y = m.cx(7);
-  vcl_cout << " x " << x  << " y " << y <<"\n";
+  vcl_cout << " x " << x  << " y " << y << vcl_endl;
 }
 
 TESTMAIN( test_misc );
