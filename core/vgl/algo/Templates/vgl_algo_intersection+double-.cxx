a7d8de5b4078510182f458690c7e8c833890e385
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-03-31 16:50:10 -0400

    avoid template instantiation files with identical names

--- core/vgl/algo/Templates/vgl_algo_intersection+double-.cxx
+++ core/vgl/algo/Templates/vgl_algo_intersection+double-.cxx
@@ -1,3 +1,3 @@
-// Instantiation of vgl_intersection<double>
+// Instantiation of vgl_intersection(std::vector<vgl_plane_3d<double> const&)
 #include <vgl/algo/vgl_intersection.txx>
 VGL_ALGO_INTERSECTION_INSTANTIATE(double);
--- core/vgl/algo/Templates/vgl_algo_intersection+float-.cxx
+++ core/vgl/algo/Templates/vgl_algo_intersection+float-.cxx
@@ -1,3 +1,3 @@
-// Instantiation of vgl_intersection<float>
+// Instantiation of vgl_intersection(std::vector<vgl_plane_3d<float> const&)
 #include <vgl/algo/vgl_intersection.txx>
 VGL_ALGO_INTERSECTION_INSTANTIATE(float);
