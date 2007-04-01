de70930037b5782086756d7a5899fb41886496b2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-04-01 08:59:25 -0400

    *** empty log message ***

--- core/vil/vil_fwd.h
+++ core/vil/vil_fwd.h
@@ -5,9 +5,11 @@
 class vil_file_format;
 class vil_stream;
 class vil_image_resource;
-class vil_image_view_base;
+class vil_image_list;
 class vil_memory_image;
 class vil_memory_chunk;
+class vil_block_cache;
+class vil_image_view_base;
 template <class T> class vil_image_view;
 template <class T> struct vil_rgb;
 template <class T> struct vil_rgba;
@@ -16,5 +18,8 @@ template <class T> class vil_smart_ptr;
 typedef vil_smart_ptr<vil_image_resource> vil_image_resource_sptr;
 typedef vil_smart_ptr<vil_image_view_base> vil_image_view_base_sptr;
 typedef vil_smart_ptr<vil_memory_chunk> vil_memory_chunk_sptr;
+
+template <class imT> class vil_border;
+template <class imT> class vil_border_accessor;
 
 #endif // vil_fwd_h_
