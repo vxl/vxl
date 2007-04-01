a95cc7f316facf209ece117c128af14d3db65969
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-04-01 09:04:43 -0400

    removed deprecated header files

--- core/vbl/tests/test_include.cxx
+++ core/vbl/tests/test_include.cxx
@@ -12,6 +12,7 @@
 #include <vbl/vbl_array_3d.h>
 #include <vbl/vbl_bit_array_2d.h>
 #include <vbl/vbl_bit_array_3d.h>
+#include <vbl/vbl_sparse_array_base.h>
 #include <vbl/vbl_sparse_array_1d.h>
 #include <vbl/vbl_sparse_array_2d.h>
 #include <vbl/vbl_sparse_array_3d.h>
@@ -22,22 +23,5 @@
 #include <vbl/vbl_qsort.h>
 #include <vbl/vbl_checked_delete.h>
 #include <vbl/vbl_scoped_ptr.h>
-
-#if 0 // These files use the nonexistent vcl_hash_map.h and/or vcl_hash_string.h
-#include <vbl/vbl_hash_map.h>
-#include <vbl/vbl_basic_optional.h>
-#include <vbl/vbl_basic_relation.h>
-#include <vbl/vbl_basic_tuple.h>
-#include <vbl/vbl_basic_relation_where.h>
-#include <vbl/vbl_br_default.h>
-#include <vbl/vbl_br_impl.h>
-#endif
-
-#include <vbl/vbl_sparse_array_base.h>
-
-#if 0 // These files are deprecated
-#include <vbl/vbl_basic_relation_type.h>
-#include <vbl/vbl_protection_traits.h>
-#endif
 
 int main() { return 0; }
