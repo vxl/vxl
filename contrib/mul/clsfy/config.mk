45f8fe77bb0c4db4a6bb3694bb6057ffb51175b3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-04-08 10:54:08 -0400

    removed unused libraries

--- contrib/mul/clsfy/config.mk
+++ contrib/mul/clsfy/config.mk
@@ -3,4 +3,3 @@ include ${VXLROOT}/contrib/mul/vpdfl/config.mk
 include ${VXLROOT}/contrib/mul/mbl/config.mk
 include ${VXLROOT}/core/vnl/algo/config.mk
 include ${VXLROOT}/core/vnl/io/config.mk
-include ${VXLROOT}/core/vbl/io/config.mk
