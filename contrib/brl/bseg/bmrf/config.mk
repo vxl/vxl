68ada8ab3a5830eaa1796b5aef5bc166964d2e0a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-04-08 14:23:06 -0400

    added missing dependency vnl_io

--- contrib/brl/bseg/bmrf/config.mk
+++ contrib/brl/bseg/bmrf/config.mk
@@ -1,11 +1,12 @@
 IULIBS += -lbmrf
 USES += contrib/gel
+include ${VXLROOT}/core/vsl/config.mk
 include ${VXLROOT}/core/vil/config.mk
 include ${VXLROOT}/core/vgl/config.mk
 include ${VXLROOT}/core/vnl/config.mk
 include ${VXLROOT}/core/vnl/algo/config.mk
+include ${VXLROOT}/core/vnl/io/config.mk
 include ${VXLROOT}/core/vbl/io/config.mk
-include ${VXLROOT}/core/vsl/config.mk
 include ${VXLROOT}/contrib/gel/gevd/config.mk
 include ${VXLROOT}/contrib/gel/vdgl/config.mk
 include ${VXLROOT}/contrib/gel/vtol/config.mk
