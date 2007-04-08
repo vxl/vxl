c57e44f60811588732b585f208800c2afc3311e0
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-04-08 11:16:03 -0400

    removed unused libraries

--- contrib/brl/bseg/bmrf/config.mk
+++ contrib/brl/bseg/bmrf/config.mk
@@ -2,8 +2,8 @@ IULIBS += -lbmrf
 USES += contrib/gel
 include ${VXLROOT}/core/vil/config.mk
 include ${VXLROOT}/core/vgl/config.mk
+include ${VXLROOT}/core/vnl/config.mk
 include ${VXLROOT}/core/vnl/algo/config.mk
-include ${VXLROOT}/core/vnl/io/config.mk
 include ${VXLROOT}/core/vbl/io/config.mk
 include ${VXLROOT}/core/vsl/config.mk
 include ${VXLROOT}/contrib/gel/gevd/config.mk
