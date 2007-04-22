e3465a4951a06f29ce008d4d163581c959272c3c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-04-22 16:18:42 -0400

    no need for Doxygen structured comment here

--- contrib/brl/bbas/bnl/tests/test_sparse_lst_sqr_function.cxx
+++ contrib/brl/bbas/bnl/tests/test_sparse_lst_sqr_function.cxx
@@ -3,7 +3,7 @@
 #include <vcl_iostream.h>
 
 
-//: all ai.size() == 2, all bj.size() == 3, all eij.size() == 2
+// all ai.size() == 2, all bj.size() == 3, all eij.size() == 2
 class test_func1 : public bnl_sparse_lst_sqr_function
 {
  public:
@@ -11,14 +11,14 @@ class test_func1 : public bnl_sparse_lst_sqr_function
              const vcl_vector<vcl_vector<bool> >& xmask,
              UseGradient g = use_gradient)
    : bnl_sparse_lst_sqr_function(num_a,2,num_b,3,xmask,2,g) {}
-  
+
   void fij(int i, int j, vnl_vector<double> const& ai,
            vnl_vector<double> const& bj, vnl_vector<double>& eij)
   {
     eij[0] = (ai[0]*ai[0]-bj[0]*ai[1])*bj[2]*bj[2]*bj[2];
     eij[1] = (ai[1]*ai[1]-bj[1]*ai[0])*bj[2]*bj[2]*bj[2];
   }
-    
+
   void jac_Aij(int i, int j, vnl_vector<double> const& ai,
                vnl_vector<double> const& bj, vnl_matrix<double>& Aij)
   {
@@ -27,7 +27,7 @@ class test_func1 : public bnl_sparse_lst_sqr_function
     Aij[1][0] = -bj[1]*bj[2]*bj[2]*bj[2];
     Aij[1][1] = 2.0*ai[1]*bj[2]*bj[2]*bj[2];
   }
-    
+
   void jac_Bij(int i, int j, vnl_vector<double> const& ai,
                vnl_vector<double> const& bj, vnl_matrix<double>& Bij)
   {
@@ -60,45 +60,45 @@ MAIN( test_sparse_lst_sqr_function )
    mask[2][1] = true;
    mask[2][2] = true;
    mask[2][3] = true;
-  
+
    test_func1 my_func(3,4,mask,bnl_sparse_lst_sqr_function::use_gradient);
-   
+
    TEST("number_of_a",my_func.number_of_a(),3);
    TEST("number_of_b",my_func.number_of_b(),4);
    TEST("number_of_e",my_func.number_of_e(),9);
-   
+
    bool num_valid = true;
    bool index_valid = true;
-   for(unsigned int i=0; i<my_func.number_of_a(); ++i){
+   for (unsigned int i=0; i<my_func.number_of_a(); ++i) {
      num_valid = (my_func.number_of_params_a(i) == 2) && num_valid;
      index_valid = (my_func.index_a(i) == i*2) && index_valid;
    }
    TEST("number_of_params_a",num_valid,true);
    TEST("index_a",index_valid,true);
-   
+
    num_valid = true;
    index_valid = true;
-   for(unsigned int j=0; j<my_func.number_of_b(); ++j){
+   for (unsigned int j=0; j<my_func.number_of_b(); ++j) {
      num_valid = (my_func.number_of_params_b(j) == 3) && num_valid;
      index_valid = (my_func.index_b(j) == j*3) && index_valid;
    }
    TEST("number_of_params_b",num_valid,true);
    TEST("index_b",index_valid,true);
-      
+
    num_valid = true;
    index_valid = true;
-   for(unsigned int k=0; k<my_func.number_of_e(); ++k){
+   for (unsigned int k=0; k<my_func.number_of_e(); ++k) {
      num_valid = (my_func.number_of_residuals(k) == 2) && num_valid;
      index_valid = (my_func.index_e(k) == k*2) && index_valid;
    }
    TEST("number_of_residuals",num_valid,true);
    TEST("index_e",index_valid,true);
-   
+
    vnl_vector<double> ai(2),bj(3),e(2);
    ai[0] = 5.0;  ai[1] = -1.0;
    bj[0] = 1.2;  bj[1] = 1.5;  bj[2] = 2.2;
    const double step = 0.001;
-   
+
    my_func.fij(0,0,ai,bj,e);
    vcl_cout << "e  = " << e << vcl_endl;
    vnl_matrix<double> Aij(2,2), Bij(2,3), fd_Aij(2,2), fd_Bij(2,3);
@@ -106,16 +106,13 @@ MAIN( test_sparse_lst_sqr_function )
    my_func.fd_jac_Aij(0,0,ai,bj,fd_Aij,step);
    my_func.jac_Bij(0,0,ai,bj,Bij);
    my_func.fd_jac_Bij(0,0,ai,bj,fd_Bij,step);
-   
-   vcl_cout << "Aij =\n" << Aij << vcl_endl;
-   vcl_cout << "fd Aij =\n" << fd_Aij << vcl_endl;
+
+   vcl_cout << "Aij =\n" << Aij << vcl_endl
+            << "fd Aij =\n" << fd_Aij << vcl_endl;
    TEST("finite difference Aij", (Aij-fd_Aij).absolute_value_max()<0.001,true);
-   vcl_cout << "Bij =\n" << Bij << vcl_endl;
-   vcl_cout << "fd Bij =\n" << fd_Bij << vcl_endl;
+   vcl_cout << "Bij =\n" << Bij << vcl_endl
+            << "fd Bij =\n" << fd_Bij << vcl_endl;
    TEST("finite difference Bij", (Bij-fd_Bij).absolute_value_max()<0.001,true);
-   
- 
+
    SUMMARY();
 }
-
-
