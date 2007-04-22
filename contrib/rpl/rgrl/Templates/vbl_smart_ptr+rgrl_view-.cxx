27d121ba6dcc7b165325e5f08d704a57588d845e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-04-22 16:19:14 -0400

    added a comment

--- contrib/rpl/rgrl/Templates/vbl_smart_ptr+rgrl_view-.cxx
+++ contrib/rpl/rgrl/Templates/vbl_smart_ptr+rgrl_view-.cxx
@@ -1,6 +1,6 @@
+#include <rgrl/rgrl_estimator.h> // DO NOT REMOVE -- needed for smart pointer instantiation closure
+#include <rgrl/rgrl_transformation.h> // DO NOT REMOVE -- needed for smart pointer instantiation closure
 #include <rgrl/rgrl_view.h>
-#include <rgrl/rgrl_transformation.h>
-#include <rgrl/rgrl_estimator.h>
 #include <vbl/vbl_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE( rgrl_view );
