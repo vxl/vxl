416e970c87c70389a16a27d2f5708bd9d7400f3f
David Cristinacce <dcristinacce@users.sourceforge.net>
2007-05-15 14:12:08 -0400

    adding extra templates for array io

---
+++ core/vbl/io/Templates/vbl_io_array_2d+vcl_vector+double--.cxx
@@ -0,0 +1,6 @@
+#include <vsl/vsl_binary_loader.h>
+#include <vsl/vsl_vector_io.txx> 
+#include <vbl/io/vbl_io_array_2d.txx>
+#include <vcl_vector.h>
+#include <vcl_utility.h>
+VBL_IO_ARRAY_2D_INSTANTIATE(vcl_vector<double>);
---
+++ core/vbl/io/Templates/vbl_io_array_2d+vcl_vector+float--.cxx
@@ -0,0 +1,7 @@
+#include <vsl/vsl_binary_loader.h>
+#include <vsl/vsl_vector_io.txx> 
+#include <vbl/io/vbl_io_array_2d.txx>
+#include <vcl_vector.h>
+#include <vcl_utility.h>
+typedef vcl_vector<float> vectorfloat;
+VBL_IO_ARRAY_2D_INSTANTIATE(vectorfloat);
