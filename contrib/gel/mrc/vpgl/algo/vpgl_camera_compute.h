2743ce76bf53facab8e9471752c9d8f0d775fbfd
Joseph L. Mundy <mundyj@users.sourceforge.net>
2007-08-20 12:05:07 -0400

    Moved compute methods to static

--- contrib/gel/mrc/vpgl/algo/vpgl_camera_compute.h
+++ contrib/gel/mrc/vpgl/algo/vpgl_camera_compute.h
@@ -22,32 +22,31 @@
 class vpgl_proj_camera_compute
 {
  public:
-  //:default constructor
-  vpgl_proj_camera_compute(){};
-
-  //:use this constructor for approximating a rational camera
-  vpgl_proj_camera_compute(vpgl_rational_camera<double> const& rat_cam)
-    {rat_cam_ = rat_cam;}
 
   //: Compute from two sets of corresponding points.
   // Put the resulting camera into camera, return true if successful.
-  bool compute( const vcl_vector< vgl_homg_point_2d<double> >& image_pts,
-                const vcl_vector< vgl_homg_point_3d<double> >& world_pts,
-                vpgl_proj_camera<double>& camera );
-  bool compute( const vcl_vector< vgl_point_2d<double> >& image_pts,
-                const vcl_vector< vgl_point_3d<double> >& world_pts,
-                vpgl_proj_camera<double>& camera );
+  static 
+    bool compute( const vcl_vector< vgl_homg_point_2d<double> >& image_pts,
+                  const vcl_vector< vgl_homg_point_3d<double> >& world_pts,
+                  vpgl_proj_camera<double>& camera );
+  static bool compute( const vcl_vector< vgl_point_2d<double> >& image_pts,
+                       const vcl_vector< vgl_point_3d<double> >& world_pts,
+                       vpgl_proj_camera<double>& camera );
 
   //:Find a projective camera that best approximates the rational camera at the world center (lon (deg), lat (deg), elev (meters) )
-  bool compute(vgl_point_3d<double> const& world_center,
-               vpgl_proj_camera<double>& camera);
+  static  bool compute(vpgl_rational_camera<double> const& rat_cam,
+                       vgl_point_3d<double> const& world_center,
+                       vpgl_proj_camera<double>& camera);
 
-  //:An auxiliary matrix that transforms (normalizes) world points prior to projection by a projective camera.  (lon, lat, elevation)->[-1, 1]. 
-  vgl_h_matrix_3d<double> norm_trans();
 
- protected:
-  //:Defined when computing a proj_camera approximation
-  vpgl_rational_camera<double> rat_cam_;
+  //:An auxillary matrix that transforms (normalizes) world points prior to projection by a projective camera.  (lon, lat, elevation)->[-1, 1]. 
+ static vgl_h_matrix_3d<double> 
+   norm_trans(vpgl_rational_camera<double> const& rat_cam);
+
+ private:
+  //:default constructor
+  vpgl_proj_camera_compute();
+
 };
 
 
@@ -55,13 +54,15 @@ class vpgl_proj_camera_compute
 class vpgl_affine_camera_compute
 {
  public:
-  vpgl_affine_camera_compute(){};
+
 
   //: Compute from two sets of corresponding points.
   // Put the resulting camera into camera, return true if successful.
-  bool compute( const vcl_vector< vgl_point_2d<double> >& image_pts,
-                const vcl_vector< vgl_point_3d<double> >& world_pts,
-                vpgl_affine_camera<double>& camera );
+ static bool compute( const vcl_vector< vgl_point_2d<double> >& image_pts,
+                      const vcl_vector< vgl_point_3d<double> >& world_pts,
+                      vpgl_affine_camera<double>& camera );
+ private:
+  vpgl_affine_camera_compute();
 };
 
 
@@ -69,11 +70,9 @@ class vpgl_affine_camera_compute
 class vpgl_perspective_camera_compute
 {
  public:
-  vpgl_perspective_camera_compute(){};
-
   //: Compute from two sets of corresponding points.
   // Put the resulting camera into camera, return true if successful.
-  bool compute( const vcl_vector< vgl_point_2d<double> >& image_pts,
+  static bool compute( const vcl_vector< vgl_point_2d<double> >& image_pts,
                 const vcl_vector< vgl_point_3d<double> >& world_pts,
                 const vpgl_calibration_matrix<double>& K,
                 vpgl_perspective_camera<double>& camera );
@@ -84,10 +83,12 @@ class vpgl_perspective_camera_compute
   //  where the perspective approximation is valid. Norm trans is a pre-multiplication 
   // of the perspective camera to account for scaling the lon, lat and elevation
   // to the range [-1, 1]
-  bool compute( vpgl_rational_camera<double> const& rat_cam,
+  static bool compute( vpgl_rational_camera<double> const& rat_cam,
                 vgl_box_3d<double> const& approximation_volume,
                 vpgl_perspective_camera<double>& camera,
                 vgl_h_matrix_3d<double>& norm_trans);
+ private:
+  vpgl_perspective_camera_compute();
 };
 
 #endif // vpgl_camera_compute_h_
