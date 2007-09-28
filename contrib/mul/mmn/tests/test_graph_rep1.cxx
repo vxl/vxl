cfa22011c0b9fa83043406593b57e129a03b86bf
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-09-28 16:03:38 -0400

    corrected "This is ..."

--- contrib/mul/mmn/tests/test_graph_rep1.cxx
+++ contrib/mul/mmn/tests/test_graph_rep1.cxx
@@ -1,4 +1,4 @@
-// This is mul/mmn/tests/test_arc.cxx
+// This is mul/mmn/tests/test_graph_rep1.cxx
 #include <testlib/testlib_test.h>
 #include <vcl_vector.h>
 #include <mmn/mmn_graph_rep1.h>
@@ -17,7 +17,7 @@ void test_graph_rep1_a()
   {
     mmn_graph_rep1 graph;
     graph.build(n+1,arc);
-    
+
     TEST("N.arcs",graph.n_arcs(),n);
     vcl_vector<mmn_dependancy> deps;
     TEST("N.removed",graph.remove_all_leaves(deps),n);
@@ -27,9 +27,9 @@ void test_graph_rep1_a()
   {
     mmn_graph_rep1 graph;
     graph.build(n+1,arc);
-    
+
     vcl_vector<mmn_dependancy> deps;
-    TEST("Compute dependancies",graph.compute_dependancies(deps),true);
+    TEST("Compute dependencies",graph.compute_dependancies(deps),true);
     TEST("No arcs left",graph.n_arcs(),0);
     TEST("Number of deps.",deps.size(),n);
   }
@@ -37,8 +37,8 @@ void test_graph_rep1_a()
 
 void test_graph_rep1_b(unsigned n)
 {
-  vcl_cout<<"==== test mmn_graph_rep1 (loop) ====="<<vcl_endl;
-  vcl_cout<<"N.nodes in loop = "<<n<<vcl_endl;
+  vcl_cout<<"==== test mmn_graph_rep1 (loop) =====\n"
+          <<"N.nodes in loop = "<<n<<vcl_endl;
 
   // Generate list of arcs
   vcl_vector<mmn_arc> arc(n);
@@ -48,7 +48,7 @@ void test_graph_rep1_b(unsigned n)
   {
     mmn_graph_rep1 graph;
     graph.build(n,arc);
-    
+
     TEST("N.arcs",graph.n_arcs(),n);
     vcl_vector<mmn_dependancy> deps;
     TEST("N.removed",graph.remove_all_leaves(deps),0);
@@ -57,14 +57,14 @@ void test_graph_rep1_b(unsigned n)
   {
     mmn_graph_rep1 graph;
     graph.build(n,arc);
-    
+
     vcl_vector<mmn_dependancy> deps;
-    TEST("Compute dependancies",graph.compute_dependancies(deps),true);
+    TEST("Compute dependencies",graph.compute_dependancies(deps),true);
     TEST("No arcs left",graph.n_arcs(),0);
     TEST("Number of deps.",deps.size(),n-1);
-    
-    vcl_cout<<"Dependancy list: "<<vcl_endl;
-    for (unsigned i=0;i<deps.size();++i) vcl_cout<<i<<")"<<deps[i]<<vcl_endl;
+
+    vcl_cout<<"Dependency list: "<<vcl_endl;
+    for (unsigned i=0;i<deps.size();++i) vcl_cout<<i<<')'<<deps[i]<<vcl_endl;
   }
 }
 
