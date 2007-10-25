e6f5aae3c173b56d9a05d35f0b050c88709b07fa
Gamze Tunali <gamze@users.sourceforge.net>
2007-10-25 12:14:09 -0400

    smart pointer for command sptr

---
+++ contrib/brl/bbas/bwm/bwm_command_sptr.h
@@ -0,0 +1,13 @@
+#ifndef bwm_command_sptr_h_ 
+#define bwm_command_sptr_h_
+
+#include <vbl/vbl_smart_ptr.h>
+#include <vsol/vsol_polygon_2d.h>
+#include <vgui/vgui_style.h>
+#include <vsol/vsol_box_2d.h>
+#include <vgui/vgui_range_map_params.h>
+
+class bwm_command;
+typedef vbl_smart_ptr<bwm_command> bwm_command_sptr;
+
+#endif
