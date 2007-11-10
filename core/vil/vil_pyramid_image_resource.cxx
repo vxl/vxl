7a98b3861d1cb5b02006f92e6c654aee74bbdae2
Joseph L. Mundy <mundyj@users.sourceforge.net>
2007-11-10 17:10:31 -0500

    Extended pyramid generation to multi-band images

--- core/vil/vil_pyramid_image_resource.cxx
+++ core/vil/vil_pyramid_image_resource.cxx
@@ -39,28 +39,86 @@ decimate_block(vcl_vector<vcl_vector<vil_image_view<float> > > const& blocks)
   vil_image_view<float> dec_block;
   dec_block.set_size(sbi, sbj);
   for (unsigned dj = 0; dj<sbj; ++dj)
-  {
-    unsigned r = 0, j0 = 2*dj;
-    if (2*dj>=sbj)
     {
-      r = 1;
-      j0 = 2*dj-sbj;
+      unsigned r = 0, j0 = 2*dj;
+      if (2*dj>=sbj)
+        {
+          r = 1;
+          j0 = 2*dj-sbj;
+        }
+      for (unsigned di = 0; di<sbi; ++di)
+        {
+          unsigned c = 0, i0 = 2*di;
+          if (2*di>=sbi)
+            {
+              c = 1;
+              i0 = 2*di-sbi;
+            }
+          vil_image_view<float> blk = blocks[r][c];
+          float v =
+            0.25f*(blk(i0, j0)+blk(i0+1,j0)+blk(i0,j0+1)+blk(i0+1,j0+1));
+          dec_block(di, dj) = v;
+        }
     }
-    for (unsigned di = 0; di<sbi; ++di)
-    {
-      unsigned c = 0, i0 = 2*di;
-      if (2*di>=sbi)
-      {
-        c = 1;
-        i0 = 2*di-sbi;
-      }
-      vil_image_view<float> blk = blocks[r][c];
-      float v =
-        0.25f*(blk(i0, j0)+blk(i0+1,j0)+blk(i0,j0+1)+blk(i0+1,j0+1));
-      dec_block(di, dj) = v;
-    }
-  }
   return dec_block;
+}
+static 
+bool convert_multi_plane_to_float(vil_image_view_base_sptr& blk,
+                                  vcl_vector<vil_image_view<float> >& fblk)
+{
+  if(!blk) return false;
+  fblk.clear();
+  vil_pixel_format fmt = blk->pixel_format();
+  unsigned ni = blk->ni(), nj = blk->nj();
+  unsigned np = blk->nplanes();
+  if(fmt == VIL_PIXEL_FORMAT_BYTE)
+    {
+      vil_image_view<unsigned char> bv = blk;
+      for(unsigned p = 0; p<np; ++p){
+        vil_image_view<float> bvf(ni, nj);
+        for(unsigned j = 0; j<nj; ++j)
+          for(unsigned i= 0; i<ni; ++i)
+            bvf(i,j) = bv(i,j,p);
+        fblk.push_back(bvf);
+      }
+      return true;
+    }
+  else if(fmt == VIL_PIXEL_FORMAT_UINT_16)
+    {
+      vil_image_view<unsigned short> bv = blk;
+      for(unsigned p = 0; p<np; ++p){
+        vil_image_view<float> bvf(ni, nj);
+        for(unsigned j = 0; j<nj; ++j)
+          for(unsigned i= 0; i<ni; ++i)
+            bvf(i,j) = bv(i,j,p);
+        fblk.push_back(bvf);
+      }
+      return true;
+    }
+  return false;
+}
+static 
+void convert_multi_plane_from_float(vcl_vector<vil_image_view<float> >& fblk,
+                                    vil_image_view<unsigned char>& blk)
+{
+  unsigned ni = fblk[0].ni(), nj = fblk[0].nj();
+  unsigned np = fblk.size();
+  for(unsigned p = 0; p<np; ++p)
+    for(unsigned j = 0; j<nj; ++j)
+      for(unsigned i= 0; i<ni; ++i)
+        blk(i,j,p) = static_cast<unsigned char>(fblk[p](i,j));
+}
+
+static 
+void convert_multi_plane_from_float(vcl_vector<vil_image_view<float> >& fblk,
+                                    vil_image_view<unsigned short>& blk)
+{
+  unsigned ni = fblk[0].ni(), nj = fblk[0].nj();
+  unsigned np = fblk.size();
+  for(unsigned p = 0; p<np; ++p)
+    for(unsigned j = 0; j<nj; ++j)
+      for(unsigned i= 0; i<ni; ++i)
+        blk(i,j,p) = static_cast<unsigned short>(fblk[p](i,j));
 }
 
 bool vil_pyramid_image_resource::
@@ -72,6 +130,8 @@ blocked_decimate(vil_blocked_image_resource_sptr const& brsc,
   unsigned nbi = brsc->n_block_i(), nbj = brsc->n_block_j();
   if (nbi==0||nbj==0)
     return false;
+  unsigned np = brsc->nplanes();
+
   //check for consistent block structure
   unsigned sbi_src = brsc->size_block_i(), sbj_src = brsc->size_block_j();
   unsigned sbi_dec = dec_resc->size_block_i(),
@@ -79,6 +139,7 @@ blocked_decimate(vil_blocked_image_resource_sptr const& brsc,
   if (sbi_src!=sbi_dec||sbj_src!=sbj_dec)
     return false;
   vil_pixel_format fmt = brsc->pixel_format();
+
   //Set up the block buffer, a 2xnbi set of blocks needed to support 2x2
   //pixel decimation.  The buffer is updated as the next two rows of blocks is
   //scanned in from the resource.
@@ -86,12 +147,6 @@ blocked_decimate(vil_blocked_image_resource_sptr const& brsc,
   //|b00 |b01|b02|b03|...|b0,nbi-1|
   //|b10 |b11|b12|b13|...|b1,nbi-1|
   //
-  vcl_vector<vcl_vector<vil_image_view<float> > > buf(2), nbrhd(2);
-  for (unsigned k =0; k<2; ++k)
-  {
-    buf[k] = vcl_vector<vil_image_view<float> >(nbi);
-    nbrhd[k] = vcl_vector<vil_image_view<float> >(2);
-  }
   //=======================================================
   //
   //Decimate the image using a 2x2 block neigborhood
@@ -102,34 +157,43 @@ blocked_decimate(vil_blocked_image_resource_sptr const& brsc,
   // This neigborhood "slides" along the 2xnbi block buffer,
   //  stepping two blocks each time.
   //=======================================================
-  vil_image_view<float> dec_blk;
-  for (unsigned bj=0; bj<nbj; bj+=2)
-  {
-    //update the block buffer by stepping down two block rows
-    for (unsigned bi = 0; bi<nbi; ++bi)
+  switch (np){
+  case 1: //grey scale images
     {
-      buf[0][bi] = vil_convert_cast(float(),brsc->get_block(bi,bj));
-      if (bj+2<=nbj)//make sure there are enough block rows
-      buf[1][bi] = vil_convert_cast(float(),brsc->get_block(bi,bj+1));
-      else
-        buf[1][bi]=buf[0][bi];//otherwise just copy the upper block
-    }
-    for (unsigned bi=0; bi<nbi; bi+=2)
-    {
-      //construct the 2x2 block neighborhood
-      for (unsigned r = 0; r<2; ++r)
-        for (unsigned c = 0; c<2; ++c)
+      vcl_vector<vcl_vector<vil_image_view<float> > > buf(2), nbrhd(2);
+      for (unsigned k =0; k<2; ++k)
         {
-          unsigned ki = bi+c;
-          if (ki>=nbi)//make sure there are enough blocks in the row
-            ki = nbi-1;
-          nbrhd[r][c] = buf[r][ki];//otherwise just copy
+          buf[k] = vcl_vector<vil_image_view<float> >(nbi);
+          nbrhd[k] = vcl_vector<vil_image_view<float> >(2);
         }
-      //construct the block
-      dec_blk = decimate_block(nbrhd);
-      //convert back to the orignal pixel format
-      switch (fmt)
-      {
+      vil_image_view<float> dec_blk;
+      for (unsigned bj=0; bj<nbj; bj+=2)
+        {
+          //update the block buffer by stepping down two block rows
+          for (unsigned bi = 0; bi<nbi; ++bi)
+            {
+              buf[0][bi] = vil_convert_cast(float(),brsc->get_block(bi,bj));
+              if (bj+2<=nbj)//make sure there are enough block rows
+                buf[1][bi] = vil_convert_cast(float(),brsc->get_block(bi,bj+1));
+              else
+                buf[1][bi]=buf[0][bi];//otherwise just copy the upper block
+            }
+          for (unsigned bi=0; bi<nbi; bi+=2)
+            {
+              //construct the 2x2 block neighborhood
+              for (unsigned r = 0; r<2; ++r)
+                for (unsigned c = 0; c<2; ++c)
+                  {
+                    unsigned ki = bi+c;
+                    if (ki>=nbi)//make sure there are enough blocks in the row
+                      ki = nbi-1;
+                    nbrhd[r][c] = buf[r][ki];//otherwise just copy
+                  }
+              //construct the block
+              dec_blk = decimate_block(nbrhd);
+              //convert back to the orignal pixel format
+              switch (fmt)
+                {
 #define CONVERT_BLOCK_CASE(FORMAT, T) \
    case FORMAT: { \
     vil_image_view<T> out_blk; \
@@ -138,23 +202,95 @@ blocked_decimate(vil_blocked_image_resource_sptr const& brsc,
       return false;\
    break;\
    }
-        CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_BYTE, vxl_byte);
+                  CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_BYTE, vxl_byte);
 #if VXL_HAS_INT_64
-        CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_UINT_64, vxl_uint_64);
+                  CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_UINT_64, vxl_uint_64);
 #endif
-        CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_UINT_32, vxl_uint_32);
-        CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_UINT_16, vxl_uint_16);
-        CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_FLOAT, float);
-        CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_DOUBLE, double);
+                  CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_UINT_32, vxl_uint_32);
+                  CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_UINT_16, vxl_uint_16);
+                  CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_FLOAT, float);
+                  CONVERT_BLOCK_CASE(VIL_PIXEL_FORMAT_DOUBLE, double);
 #undef CONVERT_BLOCK_CASE
 
-       default:
-        assert(!"Unknown vil data type in pyramid image construction");
-        return false;
-      };
-    }
+                default:
+                  assert(!"Unknown vil data type in pyramid image construction");
+                  return false;
+                };
+            }
+        }
+      return true;
+    }// end of nplanes = 1 (grey scale)
+  case 3:
+  case 4:
+    {
+      vcl_vector<vcl_vector<vcl_vector<vil_image_view<float> > > > buf(2);
+      vcl_vector<vcl_vector<vil_image_view<float> > > nbrhd(2);
+      for (unsigned k =0; k<2; ++k)
+        {
+          buf[k] = vcl_vector<vcl_vector<vil_image_view<float> > >(nbi);
+          nbrhd[k] = vcl_vector<vil_image_view<float> >(2);
+        }
+
+      // The color planes of a block are separated into 
+      // individual float views
+      for (unsigned bj=0; bj<nbj; bj+=2)
+        {
+          //update the block buffer by stepping down two block rows
+          for (unsigned bi = 0; bi<nbi; ++bi)
+            {
+
+              vil_image_view_base_sptr bij = brsc->get_block(bi,bj);
+              vcl_vector<vil_image_view<float> > fbij;
+              //convert to float
+              if(!convert_multi_plane_to_float(bij, fbij)) return false;
+              buf[0][bi]=fbij;
+              if (bj+2<=nbj)//make sure there are enough block rows
+                {
+                  bij = brsc->get_block(bi,bj+1);
+                  //convert to float
+                  if(!convert_multi_plane_to_float(bij, fbij)) return false;
+                  buf[1][bi] = fbij;
+                }
+              else
+                buf[1][bi]=buf[0][bi];//otherwise just copy the upper block
+            }
+          for (unsigned bi=0; bi<nbi; bi+=2)
+            {
+              vcl_vector<vil_image_view<float> >dec_fblk(np);
+              //create decimated blocks for each plane
+              for(unsigned p = 0; p<np; ++p){
+                for (unsigned r = 0; r<2; ++r)
+                  for (unsigned c = 0; c<2; ++c)
+                    {
+                      unsigned ki = bi+c;
+                      if (ki>=nbi)
+                        ki = nbi-1;
+                      nbrhd[r][c] = buf[r][ki][p];
+                    }
+                    //construct the block
+                    vil_image_view<float> fplane = decimate_block(nbrhd);
+                    dec_fblk[p]=fplane;
+                  }
+              //convert back to original format only allow 8 bit and 16 bit
+              //color image formats
+              if(fmt == VIL_PIXEL_FORMAT_BYTE){
+                vil_image_view<unsigned char> dblk(sbi_src, sbj_src, np);
+                convert_multi_plane_from_float(dec_fblk, dblk);                
+                dec_resc->put_block(bi/2, bj/2, dblk);
+              }                
+              else if(fmt == VIL_PIXEL_FORMAT_UINT_16){
+                vil_image_view<unsigned short> dblk(sbi_src, sbj_src, np);
+                convert_multi_plane_from_float(dec_fblk, dblk);                
+                dec_resc->put_block(bi/2, bj/2, dblk);
+              }
+              else return false;
+            }
+        }
+      return true;
+    }// end of nplanes = 3, 4
+  default:
+    return false;
   }
-  return true;
 }
 
 vil_image_resource_sptr vil_pyramid_image_resource::
@@ -163,47 +299,48 @@ decimate(vil_image_resource_sptr const& resc, char const* filename,
 {
   if (!resc)
     return 0;
-  //only handle grey scale images
   vil_pixel_format fmt = resc->pixel_format();
   switch (fmt)
-  {
-  case VIL_PIXEL_FORMAT_BYTE:
-    break;
-  case VIL_PIXEL_FORMAT_UINT_16:
-    break;
-  case VIL_PIXEL_FORMAT_UINT_32:
-    break;
+    {
+    case VIL_PIXEL_FORMAT_BYTE:
+      break;
+    case VIL_PIXEL_FORMAT_UINT_16:
+      break;
+    case VIL_PIXEL_FORMAT_UINT_32:
+      break;
 #if VXL_HAS_INT_64
-  case VIL_PIXEL_FORMAT_UINT_64:
-    break;
+    case VIL_PIXEL_FORMAT_UINT_64:
+      break;
 #endif
-  case VIL_PIXEL_FORMAT_FLOAT:
-    break;
-  case VIL_PIXEL_FORMAT_DOUBLE:
-    break;
-  default:
-    vcl_cout << "Pyramids can only be constructed from grey scale base images\n";
-    return 0;
-  }
+    case VIL_PIXEL_FORMAT_FLOAT:
+      break;
+    case VIL_PIXEL_FORMAT_DOUBLE:
+      break;
+    default:
+      vcl_cout << "unrecognized pixel format in"
+               << " vil_pyramid_image_resource::decimate()\n";
+      return 0;
+    }
   //first determine if the resource is blocked, if not create a facade
   vil_blocked_image_resource_sptr brsc = blocked_image_resource(resc);
   if (brsc&&(brsc->size_block_i()%2!=0||brsc->size_block_j()%2!=0))
-  {
-    vcl_cout << "Blocked pyramid images must have even block sizes\n";
-    return 0;
-  }
+    {
+      vcl_cout << "Blocked pyramid images must have even block sizes\n";
+      return 0;
+    }
   if (!brsc)
     brsc = new vil_blocked_image_facade(resc);
 
   // create the output decimated resource
   { //file scope to close resource
     unsigned rni = resc->ni(), rnj = resc->nj();
+    unsigned np = resc->nplanes();
     //if source image has even dimensions then just divide by 2
     unsigned dni = rni/2, dnj = rnj/2;
     //else if the dimension is odd, increase the output size by 1.
     dni += rni%2; dnj += rnj%2;
     vil_blocked_image_resource_sptr dec_resc =
-      vil_new_blocked_image_resource(filename, dni, dnj, 1,
+      vil_new_blocked_image_resource(filename, dni, dnj, np,
                                      fmt, brsc->size_block_i(),
                                      brsc->size_block_j(),
                                      format);
