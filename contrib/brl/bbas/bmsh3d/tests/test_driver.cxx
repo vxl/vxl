b3893f83a85ffa12b5735f54b6aaef74222b70a0
Gamze Tunali <gamze@users.sourceforge.net>
2007-11-13 20:27:40 -0500

    removed the test name which was not valid

--- contrib/brl/bbas/bmsh3d/tests/test_driver.cxx
+++ contrib/brl/bbas/bmsh3d/tests/test_driver.cxx
@@ -3,13 +3,11 @@
 
 DECLARE( bmsh3d_test_face_geom );
 DECLARE( test_mesh_break_face );
-DECLARE( bmsh3d_mesh_tri_test );
 
 void register_tests()
 {
   REGISTER( bmsh3d_test_face_geom );
   REGISTER( test_mesh_break_face );
-  // REGISTER( dbmsh3d_mesh_tri_test );
 }
 
 DEFINE_MAIN;
