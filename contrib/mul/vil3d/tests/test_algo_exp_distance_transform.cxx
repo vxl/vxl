ac6f12b4dae6406e9c4f6775123fbdadc5343ccb
Amitha Perera <amithaperera@users.sourceforge.net>
2007-11-15 09:17:00 -0500

    Try fix VC6 compile error

--- contrib/mul/vil3d/tests/test_algo_exp_distance_transform.cxx
+++ contrib/mul/vil3d/tests/test_algo_exp_distance_transform.cxx
@@ -1,6 +1,7 @@
 // This is mul/vil3d/tests/test_algo_exp_distance_transform.cxx
 #include <testlib/testlib_test.h>
 #include <vcl_iostream.h>
+#include <vcl_cmath.h>
 #include <vil3d/algo/vil3d_exp_distance_transform.h>
 #include <vil3d/vil3d_crop.h>
 
@@ -26,13 +27,13 @@ static void test_algo_exp_distance_transform_float()
   TEST_NEAR("image(5,5,9)",image(5,5,9), 50.0f,0.01f);
 
   TEST_NEAR("image(4,3,5)",image(4,3,5),
-            100.0f*vcl_pow(0.5,vcl_sqrt(2)),0.01f);
+            100.0f*vcl_pow(0.5,vcl_sqrt(2.0)),0.01f);
   TEST_NEAR("image(4,3,3)",image(4,3,3),
             100.0f*vcl_pow(0.5,vcl_sqrt(2.25)),0.01f);
   TEST_NEAR("image(0,5,5)",image(0,5,5),
-            100.0f*vcl_pow(0.5,5),0.01f);
+            100.0f*vcl_pow(0.5,5.0),0.01f);
   TEST_NEAR("image(10,5,5)",image(10,5,5),
-            100.0f*vcl_pow(0.5,5),0.01f);
+            100.0f*vcl_pow(0.5,5.0),0.01f);
   TEST_NEAR("image(5,0,5)",image(5,0,5),
             100.0f*vcl_pow(0.5,2.5),0.01f);
   TEST_NEAR("image(5,10,5)",image(5,10,5),
