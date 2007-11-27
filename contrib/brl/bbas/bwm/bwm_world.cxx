1ef18ecd7453e09dfe62d5d730c23aa46baaca8a
Joseph L. Mundy <mundyj@users.sourceforge.net>
2007-11-27 06:15:15 -0500

    Made a few changes to bwm_world

--- contrib/brl/bbas/bwm/bwm_world.cxx
+++ contrib/brl/bbas/bwm/bwm_world.cxx
@@ -7,3 +7,11 @@ bwm_world* bwm_world::instance() {
     instance_ = new bwm_world();
   return bwm_world::instance_;
 }
+
+
+void  bwm_world::set_world_pt(vgl_point_3d<double> const& pt)
+{
+  world_pt_ = pt; 
+  vgl_vector_3d<double> normal(0, 0, 1);//z axis
+  world_plane_ = vgl_plane_3d<double>(normal, pt);
+}
--- contrib/brl/bbas/bwm/bwm_world.h
+++ contrib/brl/bbas/bwm/bwm_world.h
@@ -4,7 +4,7 @@
 #include "bwm_observable_sptr.h"
 
 #include <vgl/vgl_point_3d.h>
-#include <vgl/vgl_homg_plane_3d.h>
+#include <vgl/vgl_plane_3d.h>
 
 class bwm_world {
 public:
@@ -13,19 +13,25 @@ public:
   virtual ~bwm_world() {}
 
   vgl_point_3d<double> world_pt() { return world_pt_; }
-  vgl_homg_plane_3d<double> world_plane() {return world_plane_; }
+  vgl_plane_3d<double> world_plane() {return world_plane_; }
   vcl_vector<bwm_observable_sptr> objects() { return objects_; }
 
   void add(bwm_observable_sptr obj) { objects_.push_back(obj); }
-  void set_world_pt(vgl_point_3d<double> pt) { world_pt_ = pt; }
-  void set_world_plane(vgl_homg_plane_3d<double> plane) { world_plane_ = plane; }
+  //: Typically the world point for 1 pt correspondences
+  void set_world_pt(vgl_point_3d<double> const& pt);
+  //: Usually set to be a horizontal plane through the world point
+  //maybe this method shouldn't exist - i.e. insist world pt and world plane
+  //are consistent - leave for now
+    void set_world_plane(vgl_plane_3d<double> const& plane) { world_plane_ = plane; }
 
 protected:
   bwm_world(){} 
   static bwm_world* instance_;
-
+  //: The point common to correspondences used construct the 3-d intersection
+  // could also be a point defined by LIDAR or GPS
   vgl_point_3d<double> world_pt_;
-  vgl_homg_plane_3d<double> world_plane_;
+  //: The plane through the world point. Use Euclidean plane not homg plane
+  vgl_plane_3d<double> world_plane_;
   vcl_vector<bwm_observable_sptr > objects_;
 };
 
