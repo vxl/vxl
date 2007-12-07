b32d444841334c68819e4c4aefdd39bc8936a550
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-12-07 01:57:27 -0500

    TABs replaced by spaces

--- contrib/brl/bbas/bwm/reg/bwm_reg_edge_champher.h
+++ contrib/brl/bbas/bwm/reg/bwm_reg_edge_champher.h
@@ -1,6 +1,5 @@
 #ifndef bwm_reg_edge_champher_h_
 #define bwm_reg_edge_champher_h_
-
 //-----------------------------------------------------------------------------
 //:
 // \file
@@ -12,18 +11,18 @@
 // edge pointer at a given location can be accessed.
 //
 // \verbatim
-// Modifications
-// Adapted from the original by Charlie Rothwell - 4/5/95
-// J.L. Mundy Nov.24, 2002
-// Adapted again 
-// J.L. Mundy Nov. 26, 2007
+//  Modifications
+//   J.L. Mundy Nov.24, 2002 - Adapted from original by Charlie Rothwell, 4/5/95
+//   J.L. Mundy Nov.26, 2007 - Adapted again
 // \endverbatim
 //
 //-----------------------------------------------------------------------------
+#include <vcl_iostream.h>
 #include <vcl_vector.h>
 #include <vbl/vbl_array_2d.h>
 #include <vnl/vnl_numeric_traits.h>
 #include <vsol/vsol_digital_curve_2d.h>
+
 class bwm_reg_edge_champher
 {
   // PUBLIC INTERFACE----------------------------------------------------------
@@ -31,6 +30,7 @@ class bwm_reg_edge_champher
  public:
 
   // Constructors/Initializers/Destructors-------------------------------------
+
   bwm_reg_edge_champher();
   bwm_reg_edge_champher(unsigned col_off, unsigned row_off,
                         unsigned ncols, unsigned nrows,
@@ -39,49 +39,45 @@ class bwm_reg_edge_champher
 
   // Data Access---------------------------------------------------------------
 
-  //
   //: the champher distance at the specified image col and row
   // (col, row) are in image coordinates, not champher array coordinates
-  inline float distance(unsigned col, unsigned row) {
+  inline float distance(unsigned col, unsigned row)
+  {
     // ncols_ = number of columns; nrows_ = number of rows
     col-=col_off_; row-=row_off_;
     if ( (col<ncols_-1) && (row<nrows_-1) )
-        return (float) distance_[row+1][col+1];
-	else{
-		vcl_cout << "failed at(" << col+col_off_ << ' ' << row+row_off_ << '\n';
-		return vnl_numeric_traits<float>::maxval;}
+      return (float) distance_[row+1][col+1];
+    else {
+      vcl_cout << "failed at(" << col+col_off_ << ' ' << row+row_off_ << '\n';
+      return vnl_numeric_traits<float>::maxval;
     }
+  }
 
-  //
   //: the digital curve that contributed a point at (col, row)
-  //
-  inline vsol_digital_curve_2d_sptr image_edge(unsigned col, unsigned row) {
+  inline vsol_digital_curve_2d_sptr image_edge(unsigned col, unsigned row)
+  {
     // ncols_ = number of columns; nrows_ = number of rows
     col-=col_off_; row-=row_off_;
     if ( (col<ncols_-1) && (row<nrows_-1) )
-        return edges_[row+1][col+1];
+      return edges_[row+1][col+1];
     else
-        return 0;
-    }
+      return 0;
+  }
 
-  //
   //: the index of the vertex of the digital curve inserted at (col, row)
-  //
-  inline unsigned sample_index(unsigned col, unsigned row) {
+  inline unsigned sample_index(unsigned col, unsigned row)
+  {
     // ncols_ = number of columns; nrows_ = number of rows
     col-=col_off_; row-=row_off_;
     if (  (col<ncols_-1) && (row<nrows_-1) )
-        return sample_index_[row+1][col+1];
+      return sample_index_[row+1][col+1];
     else
-        return 0;
-    }
-
-  // Data Control--------------------------------------------------------------
-
-  // Utility Methods-----------------------------------------------------------
+      return 0;
+  }
 
   // Debug Methods------------------------------------------------------------
   void print_distance();
+
  protected:
   // INTERNALS-----------------------------------------------------------------
   void initialize_arrays(vcl_vector<vsol_digital_curve_2d_sptr> const& edges);
@@ -91,13 +87,11 @@ class bwm_reg_edge_champher
   void backward_chamfer();
   void compute_real_distances();
 
-
   // Data Members--------------------------------------------------------------
 
  private:
-
   //:the dimensions of the champher array, 2 greater than the image region
-  unsigned ncols_,nrows_; 
+  unsigned ncols_,nrows_;
 
   //:the origin of the champher region in the original image coordinate system
   unsigned col_off_, row_off_;
@@ -110,7 +104,6 @@ class bwm_reg_edge_champher
 
   //: The sample index of the edgel curve at a given cell
   vbl_array_2d<unsigned> sample_index_;
-
 };
 
 #endif
--- contrib/brl/bbas/bwm/reg/tests/test_processor.cxx
+++ contrib/brl/bbas/bwm/reg/tests/test_processor.cxx
@@ -1,41 +1,44 @@
 // This is brl/bbas/bwm/reg/tests/test_processor.cxx
+#include <testlib/testlib_test.h>
+
 #include <vcl_iostream.h>
 #include <vcl_string.h>
 #include <vnl/vnl_numeric_traits.h>
-#include <testlib/testlib_test.h>
 #include <bwm/reg/bwm_reg_processor.h>
 #include <vsol/vsol_point_2d.h>
 #include <vsol/vsol_digital_curve_2d_sptr.h>
 #include <vil/vil_load.h>
 #include <vpgl/vpgl_rational_camera.h>
+
 static void print_edges(vcl_vector<vsol_digital_curve_2d_sptr> const& edges)
 {
-  for(unsigned cv = 0; cv<edges.size(); ++cv)
-    {
-      vsol_digital_curve_2d_sptr dc = edges[cv];
-      vcl_cout << "dc[" << cv << "](" << dc->p0()->x()<< ' '
-               << dc->p0()->y() << ")(" << dc->p1()->x()<< ' '
-               << dc->p0()->y() << ")\n";
-    }
+  for (unsigned cv = 0; cv<edges.size(); ++cv)
+  {
+    vsol_digital_curve_2d_sptr dc = edges[cv];
+    vcl_cout << "dc[" << cv << "](" << dc->p0()->x()<< ' '
+             << dc->p0()->y() << ")(" << dc->p1()->x()<< ' '
+             << dc->p0()->y() << ")\n";
+  }
 }
+
 static
 void display_edges(vcl_vector<vsol_digital_curve_2d_sptr> const& edges)
 {
   //get the bounds on the model edges
   double dcmin = vnl_numeric_traits<double>::maxval, dcmax = 0;
   double drmin = dcmin, drmax = 0;
-  vcl_vector<vsol_digital_curve_2d_sptr>::const_iterator cit = 
+  vcl_vector<vsol_digital_curve_2d_sptr>::const_iterator cit =
     edges.begin();
-  for(; cit != edges.end(); ++cit)
-    for(unsigned i = 0; i<(*cit)->size(); ++i)
-      {
-        vsol_point_2d_sptr p = (*cit)->point(i);
-        double c = p->x(), r = p->y();
-        if(c<dcmin) dcmin = c;
-        if(c>dcmax) dcmax = c;
-        if(r<drmin) drmin = r;
-        if(r>drmax) drmax = r;
-      }
+  for (; cit != edges.end(); ++cit)
+    for (unsigned i = 0; i<(*cit)->size(); ++i)
+    {
+      vsol_point_2d_sptr p = (*cit)->point(i);
+      double c = p->x(), r = p->y();
+      if (c<dcmin) dcmin = c;
+      if (c>dcmax) dcmax = c;
+      if (r<drmin) drmin = r;
+      if (r>drmax) drmax = r;
+    }
   unsigned cmin = static_cast<unsigned>(dcmin);
   unsigned cmax = static_cast<unsigned>(dcmax);
   unsigned rmin = static_cast<unsigned>(drmin);
@@ -43,40 +46,41 @@ void display_edges(vcl_vector<vsol_digital_curve_2d_sptr> const& edges)
   unsigned ncols = cmax - cmin +1;
   unsigned nrows = rmax - rmin +1;
   vcl_vector<vcl_vector<bool> > edge_map(nrows);
-  for(unsigned r = 0; r<nrows; ++r)
+  for (unsigned r = 0; r<nrows; ++r)
     edge_map[r]=vcl_vector<bool>(ncols, false);
   cit = edges.begin();
-  for(; cit != edges.end(); ++cit)
-    for(unsigned i = 0; i<(*cit)->size(); ++i)
-      {
-        vsol_point_2d_sptr p = (*cit)->point(i);
-        double c = p->x(), r = p->y();
-        c-= cmin; r-=rmin;
-        unsigned ic=static_cast<unsigned>(c), ir=static_cast<unsigned>(r);
-        edge_map[ir][ic] = true;
-      }
-  for(unsigned r = 0; r<nrows; ++r)
+  for (; cit != edges.end(); ++cit)
+    for (unsigned i = 0; i<(*cit)->size(); ++i)
     {
-      for(unsigned c = 0; c<ncols; ++c)
-        if(edge_map[r][c])
-          vcl_cout << 1 ;
-        else
-          vcl_cout << ' ' ;
-      vcl_cout << '\n';
+      vsol_point_2d_sptr p = (*cit)->point(i);
+      double c = p->x(), r = p->y();
+      c-= cmin; r-=rmin;
+      unsigned ic=static_cast<unsigned>(c), ir=static_cast<unsigned>(r);
+      edge_map[ir][ic] = true;
     }
+  for (unsigned r = 0; r<nrows; ++r)
+  {
+    for (unsigned c = 0; c<ncols; ++c)
+      if (edge_map[r][c])
+        vcl_cout << '1' ;
+      else
+        vcl_cout << ' ' ;
+    vcl_cout << '\n';
+  }
 }
+
 void test_processor()
 {
-#if 0
+#ifdef SanDiegoExists
   vcl_string model_image_file =
-    "c:/images/SanDiego/SanDiegoA/po_39942_pan_0000010000/po_39942_pan_0000010000_0.NTF";
-  vil_image_resource_sptr model_image = 
-    vil_load_image_resource(model_image_file.c_str()); 
+    "C:/images/SanDiego/SanDiegoA/po_39942_pan_0000010000/po_39942_pan_0000010000_0.NTF";
+  vil_image_resource_sptr model_image =
+    vil_load_image_resource(model_image_file.c_str());
 
-  vcl_string model_cam_file = "c:/images/SanDiego/SanDiegoA/po_39942_pan_0000010000/camera/po_39942_pan_0000010000.RPB";
+  vcl_string model_cam_file = "C:/images/SanDiego/SanDiegoA/po_39942_pan_0000010000/camera/po_39942_pan_0000010000.RPB";
   vpgl_rational_camera<double>* model_cam =
     read_rational_camera<double>(model_cam_file);
- 
+
   vpgl_rational_camera<double> search_cam = *model_cam;
 
   double uoff, voff;
@@ -84,9 +88,9 @@ void test_processor()
   uoff+= 21; voff+=14;
   search_cam.set_image_offset(uoff, voff);
 
-  vgl_point_3d<double> wpt(-117.157366999,32.70183,	-29.642173);
-  vgl_plane_3d<double> wpl(0,0,1,29.642173);
- 
+  vgl_point_3d<double> wpt(-117.157366999, 32.70183, -29.642173);
+  vgl_plane_3d<double> wpl(0, 0, 1, 29.642173);
+
   bwm_reg_processor brp(model_image, *model_cam, wpt, wpl, model_image,
                         search_cam);
 
