45452a978a7cda4fdf71ee473a39de2da6438092
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-12-11 11:47:44 -0500

    commented out unused "#include <map>" and "#include <utility>"

--- contrib/brl/bbas/bmsh3d/bmsh3d_he_mesh.h
+++ contrib/brl/bbas/bmsh3d/bmsh3d_he_mesh.h
@@ -1,11 +1,10 @@
+// This is brl/bbas/bmsh3d/bmsh3d_he_mesh.h
+//---------------------------------------------------------------------
 #ifndef _bmsh3d_he_mesh_h_
 #define _bmsh3d_he_mesh_h_
-//---------------------------------------------------------------------
-// This is brl/bbas/bmsh3d/bmsh3d_he_mesh.h
 //:
 // \file
 // \brief 2-manifold half-edge mesh
-//
 //
 // \author
 //  MingChing Chang  Aug 22, 2005
@@ -16,14 +15,12 @@
 // \endverbatim
 //
 //-------------------------------------------------------------------------
-#include <vcl_cassert.h>
 
-#include <vcl_map.h>
-#include <vcl_utility.h>
+//#include <vcl_map.h>
+//#include <vcl_utility.h>
 
 #include "bmsh3d_halfedge.h"
 #include "bmsh3d_face.h"
-
 #include "bmsh3d_pt_set.h"
 
 //#######################################################
