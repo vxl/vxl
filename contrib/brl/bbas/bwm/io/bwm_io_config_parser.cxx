e10cfcf903178565657ee2b25c659d55f42d8727
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-12-11 11:47:34 -0500

    added "vcl_" prefix to strcmp(), and removed two unused #includes

--- contrib/brl/bbas/bwm/io/bwm_io_config_parser.cxx
+++ contrib/brl/bbas/bwm/io/bwm_io_config_parser.cxx
@@ -1,32 +1,28 @@
-/* \file contrib/brl/bwm/io/bwm_io_config_parser.cxx
-  Parses the configuration file for bwm tool.
-*/
+//:
+// \file
+// \brief Parses the configuration file for bwm tool.
+//
 #include "bwm_io_config_parser.h"
 #include "bwm/bwm_site.h"
 
-#include <vcl_cstdio.h>
-#include <vcl_cassert.h>
 #include <vcl_sstream.h>
 #include <vcl_iostream.h>
 
-//-----
-//--- PARSER --
-//--
+// --------------
+// --- PARSER ---
+// --------------
 template <typename T>
 void convert(const char* t, T& d)
 {
   vcl_stringstream strm(t);
-
   strm >> d;
-
 }
 
-bwm_io_config_parser::bwm_io_config_parser() 
+bwm_io_config_parser::bwm_io_config_parser()
 {
   init_params();
-  site_ = new bwm_site(); 
+  site_ = new bwm_site();
 }
-
 
 void bwm_io_config_parser::init_params()
 {
@@ -36,7 +32,7 @@ void bwm_io_config_parser::init_params()
   camera_type_ = "";
 }
 
-void 
+void
 bwm_io_config_parser ::cdataHandler(vcl_string name, vcl_string data)
 {
   // clean up the empty chars before and after the file paths
@@ -54,37 +50,35 @@ bwm_io_config_parser ::cdataHandler(vcl_string name, vcl_string data)
   } else if (name.compare(PYRAMID_EXE_TAG) == 0) {
     site_->pyr_exe_path_.assign(data);
   }
-   cdata = ""; 
+   cdata = "";
 }
 
-void 
+void
 bwm_io_config_parser::handleAtts(const XML_Char** atts)
 {
-
 }
 
-void 
+void
 bwm_io_config_parser::startElement(const char* name, const char** atts)
 {
- 
-  vcl_cout<< "element=" << name << vcl_endl; 
-  
-  if (strcmp(name, SITE_TAG) == 0) {
+  vcl_cout<< "element=" << name << vcl_endl;
+
+  if (vcl_strcmp(name, SITE_TAG) == 0) {
     for (int i=0; atts[i]; i+=2) {
       vcl_cout << "  Attr=" << atts[i] << "->" << atts[i+1] << vcl_endl;
-      if (strcmp(atts[i], "name") == 0)
+      if (vcl_strcmp(atts[i], "name") == 0)
         convert(atts[i+1], site_->name_);
     }
   }
-  else if ((strcmp(name,IMAGE_TABLEAU_TAG)== 0) ||
-    (strcmp(name, CAMERA_TABLEAU_TAG) == 0) ||
-    (strcmp(name,COIN3D_TABLEAU_TAG) == 0) || 
-    (strcmp(name,PROJ2D_TABLEAU_TAG) == 0)) {
+  else if ((vcl_strcmp(name,IMAGE_TABLEAU_TAG)== 0) ||
+    (vcl_strcmp(name, CAMERA_TABLEAU_TAG) == 0) ||
+    (vcl_strcmp(name,COIN3D_TABLEAU_TAG) == 0) ||
+    (vcl_strcmp(name,PROJ2D_TABLEAU_TAG) == 0)) {
     for (int i=0; atts[i]; i+=2) {
       vcl_cout << "  Attr=" << atts[i] << "->" << atts[i+1] << vcl_endl;
-      if (strcmp(atts[i], "name") == 0)
+      if (vcl_strcmp(atts[i], "name") == 0)
         convert(atts[i+1], name_);
-      else if (strcmp(atts[i], "status") == 0) {
+      else if (vcl_strcmp(atts[i], "status") == 0) {
         vcl_string status;
         convert(atts[i+1], status);
         if (status.compare("active") == 0)
@@ -93,58 +87,64 @@ bwm_io_config_parser::startElement(const char* name, const char** atts)
           status_ = false;
       }
     }
-  } else if (strcmp(name,CAMERA_PATH_TAG)== 0) {
+  }
+  else if (vcl_strcmp(name,CAMERA_PATH_TAG)== 0) {
     for (int i=0; atts[i]; i+=2) {
       vcl_cout << "  Attr=" << atts[i] << "->" << atts[i+1] << vcl_endl;
-      if (strcmp(atts[i], "type") == 0)
+      if (vcl_strcmp(atts[i], "type") == 0)
         convert(atts[i+1], camera_type_);
     }
-  } else if (strcmp(name, CORRESPONDENCES_TAG) == 0) {
+  }
+  else if (vcl_strcmp(name, CORRESPONDENCES_TAG) == 0) {
     for (int i=0; atts[i]; i+=2) {
       vcl_cout << "  Attr=" << atts[i] << "->" << atts[i+1] << vcl_endl;
-      if (strcmp(atts[i], "mode") == 0)
+      if (vcl_strcmp(atts[i], "mode") == 0)
         convert(atts[i+1], site_->corr_mode_);
-      else if (strcmp(atts[i], "type") == 0)
+      else if (vcl_strcmp(atts[i], "type") == 0)
         convert(atts[i+1], site_->corr_type_);
     }
-  } else if (strcmp(name, CORRESP_PT_TAG) == 0) {
+  }
+  else if (vcl_strcmp(name, CORRESP_PT_TAG) == 0) {
     for (int i=0; atts[i]; i+=2) {
       vcl_cout << "  Attr=" << atts[i] << "->" << atts[i+1] << vcl_endl;
-      if (strcmp(atts[i], "X") == 0)
+      if (vcl_strcmp(atts[i], "X") == 0)
         convert(atts[i+1], X_);
-      else if (strcmp(atts[i], "Y") == 0)
+      else if (vcl_strcmp(atts[i], "Y") == 0)
         convert(atts[i+1], Y_);
     }
-  } else if (strcmp(name, CORRESP_WORLD_PT_TAG) == 0) {
+  }
+  else if (vcl_strcmp(name, CORRESP_WORLD_PT_TAG) == 0) {
     for (int i=0; atts[i]; i+=2) {
       vcl_cout << "  Attr=" << atts[i] << "->" << atts[i+1] << vcl_endl;
-      if (strcmp(atts[i], "X") == 0)
+      if (vcl_strcmp(atts[i], "X") == 0)
         convert(atts[i+1], X_);
-      else if (strcmp(atts[i], "Y") == 0)
+      else if (vcl_strcmp(atts[i], "Y") == 0)
         convert(atts[i+1], Y_);
-      else if (strcmp(atts[i], "Z") == 0)
+      else if (vcl_strcmp(atts[i], "Z") == 0)
         convert(atts[i+1], Z_);
     }
-  } else if (strcmp(name, OBJECT_TAG) == 0) {
+  }
+  else if (vcl_strcmp(name, OBJECT_TAG) == 0) {
     for (int i=0; atts[i]; i+=2) {
       vcl_cout << "  Attr=" << atts[i] << "->" << atts[i+1] << vcl_endl;
-      if (strcmp(atts[i], "type") == 0)
+      if (vcl_strcmp(atts[i], "type") == 0)
         convert(atts[i+1], object_type_);
     }
-  } else if (strcmp(name, LVCS_TAG) == 0) {
+  }
+  else if (vcl_strcmp(name, LVCS_TAG) == 0) {
     for (int i=0; atts[i]; i+=2) {
       vcl_cout << "  Attr=" << atts[i] << "->" << atts[i+1] << vcl_endl;
-      if (strcmp(atts[i], "lat") == 0)
+      if (vcl_strcmp(atts[i], "lat") == 0)
         convert(atts[i+1], lat_);
-      else if (strcmp(atts[i], "lon") == 0)
+      else if (vcl_strcmp(atts[i], "lon") == 0)
         convert(atts[i+1], lon_);
-      else if (strcmp(atts[i], "elev") == 0)
+      else if (vcl_strcmp(atts[i], "elev") == 0)
         convert(atts[i+1], elev_);
     }
   }
 }
 
-void 
+void
 bwm_io_config_parser::endElement(const char* name)
 {
    // first check if the last element has some cdata
@@ -153,34 +153,34 @@ bwm_io_config_parser::endElement(const char* name)
      cdata= "";
    }
 
-   if (strcmp(name, IMAGE_TABLEAU_TAG) == 0) {
+   if (vcl_strcmp(name, IMAGE_TABLEAU_TAG) == 0) {
      bwm_io_tab_config_img* img = new bwm_io_tab_config_img(name, name_, status_, image_path_);
      site_->tableaus_.push_back(img);
      init_params();
-   } else if (strcmp(name, CAMERA_TABLEAU_TAG) == 0) {
+   } else if (vcl_strcmp(name, CAMERA_TABLEAU_TAG) == 0) {
      bwm_io_tab_config_cam* cam = new bwm_io_tab_config_cam(name, name_, status_, image_path_, camera_path_, camera_type_);
      site_->tableaus_.push_back(cam);
      init_params();
-   } else if (strcmp(name, COIN3D_TABLEAU_TAG) == 0) {
+   } else if (vcl_strcmp(name, COIN3D_TABLEAU_TAG) == 0) {
      bwm_io_tab_config_coin3d* coin3d = new bwm_io_tab_config_coin3d(name, name_, status_, camera_path_, camera_type_);
      site_->tableaus_.push_back(coin3d);
      init_params();
-   } else if (strcmp(name, PROJ2D_TABLEAU_TAG) == 0) {
+   } else if (vcl_strcmp(name, PROJ2D_TABLEAU_TAG) == 0) {
      bwm_io_tab_config_proj2d* proj2d = new bwm_io_tab_config_proj2d(name, name_, status_, proj2d_type_, camera_path_, camera_type_, coin3d_name_);
-   } else if (strcmp(name, CORRESP_ELM_TAG) == 0) {
+   } else if (vcl_strcmp(name, CORRESP_ELM_TAG) == 0) {
      corresp_elm_.push_back(vcl_pair<vcl_string, vsol_point_2d> (corr_cam_tab_, vsol_point_2d(X_,Y_)));
      corr_cam_tab_ = "";
-   } else if (strcmp(name, CORRESP_WORLD_PT_TAG) == 0) {
+   } else if (vcl_strcmp(name, CORRESP_WORLD_PT_TAG) == 0) {
      site_->corresp_world_pts_.push_back(vsol_point_3d(X_, Y_, Z_));
-   } else if (strcmp(name, CORRESP_TAG) == 0) {
+   } else if (vcl_strcmp(name, CORRESP_TAG) == 0) {
      site_->corresp_.push_back(corresp_elm_);
      corresp_elm_.clear();
-   } else if (strcmp(name, OBJECT_TAG) == 0) {
+   } else if (vcl_strcmp(name, OBJECT_TAG) == 0) {
      vcl_pair<vcl_string, vcl_string> object(object_path_, object_type_);
      site_->objects_.push_back(object);
-   } else if (strcmp(name, LVCS_TAG) == 0) {
+   } else if (vcl_strcmp(name, LVCS_TAG) == 0) {
      site_->lvcs_ = new vsol_point_3d(lat_, lon_, elev_);
-   }  
+   }
 }
 
 void bwm_io_config_parser::charData(const XML_Char* s, int len)
@@ -189,8 +189,8 @@ void bwm_io_config_parser::charData(const XML_Char* s, int len)
   if (len==0 || len<=leadingSpace)
      return;  // called with whitespace between elements
 
-  putchar('(');
-  fwrite(s, len, 1, stdout);
+  vcl_putchar('(');
+  vcl_fwrite(s, len, 1, stdout);
   puts(")");
   cdata.append(s, len);
 }
@@ -202,6 +202,3 @@ void bwm_io_config_parser::trim_string(vcl_string& s)
   vcl_string t = s.substr(i,j-i+1);
   s = t;
 }
-
-
-
