39fb19a21cb1ff88df925476cd6f00077b85cc7b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-12-11 11:38:35 -0500

    removed unused "#include <vnl/vnl_file_matrix.h>

--- contrib/brl/bbas/bmsh3d/examples/bmsh3d_mesh2ply.cxx
+++ contrib/brl/bbas/bmsh3d/examples/bmsh3d_mesh2ply.cxx
@@ -1,25 +1,22 @@
-// This is contrib/brl/bbas/bmsh3d/examples/bmsh3d_mesh2ply.cxx
+// This is brl/bbas/bmsh3d/examples/bmsh3d_mesh2ply.cxx
 
 //:
 // \file
-// \brief 
+// \brief
 // \date September 11, 2006
-
 
 #include <vcl_iostream.h>
 #include <vul/vul_timer.h>
-#include <vnl/vnl_file_matrix.h>
 
 #include <bmsh3d/algo/bmsh3d_fileio.h>
 #include <bmsh3d/algo/bmsh3d_mesh_triangulate.h>
 
-//: 
 int main( int argc, char **argv )
 {
   if (argc < 3)
   {
-    vcl_cerr << "Invalid parameters. Program syntax:\n";
-    vcl_cerr << argv[0] << "input_meshfile output_mesh.ply\n";
+    vcl_cerr << "Invalid parameters. Program syntax:\n"
+             << argv[0] << "input_meshfile output_mesh.ply\n";
     return 1;
   }
 
@@ -28,11 +25,11 @@ int main( int argc, char **argv )
   // mesh file to be transformed
   vcl_string input_mesh_file(argv[1]);
   vcl_string output_mesh_file(argv[2]);
-  
+
   // i. Load the meshes
   bmsh3d_mesh mesh;
 
-  vcl_cout << "i. Load input mesh file: \n";
+  vcl_cout << "i. Load input mesh file:\n";
   vul_timer timer;
   timer.mark();
 
@@ -42,21 +39,17 @@ int main( int argc, char **argv )
 
   vcl_cout << "done.\n";
 
-  vcl_cout << "Total time taken = \n";
+  vcl_cout << "Total time taken =\n";
   timer.print(vcl_cout);
-
-  
 
   vcl_cout << "Triangulating the mesh before saving...";
   bmsh3d_mesh* tri_mesh = generate_tri_mesh(&mesh);
 
   //Save the mesh to ply format
-  vcl_cout << "Saving the mesh in PLY format ... \n";
+  vcl_cout << "Saving the mesh in PLY format ...\n";
   bmsh3d_save_ply(tri_mesh, output_mesh_file.c_str(), false);
 
   delete tri_mesh;
   vcl_cout << "Done.\n";
   return 0;
-};
-
-
+}
