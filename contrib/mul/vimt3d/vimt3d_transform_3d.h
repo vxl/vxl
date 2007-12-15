3e5ff8241298fa8575b91ffc67f75f65317e3e20
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2007-12-15 08:42:35 -0500

    removed unused "#include <string>"

--- contrib/mul/vimt3d/vimt3d_transform_3d.h
+++ contrib/mul/vimt3d/vimt3d_transform_3d.h
@@ -9,7 +9,6 @@
 #include <vgl/vgl_point_3d.h>
 #include <vgl/vgl_vector_3d.h>
 #include <vnl/io/vnl_io_matrix.h>
-#include <vcl_string.h>
 
 //=======================================================================
 
