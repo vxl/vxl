be953274e32726b104b4085674bb3923c5b6f581
Kevin de Souza <kdesouza@users.sourceforge.net>
2007-12-19 10:01:55 -0500

    Revised names of expected builds for imorphics

--- DartConfigServer.cmake
+++ DartConfigServer.cmake
@@ -63,8 +63,8 @@ SET (EXPECTED_BUILDS "\\
   {GE MinGW-3.8_gcc-3.4.4_-O2_static} \\
   {GE Win2k_bcc-5.5.1_Release} \\
   {GE Win2k_msvc-7.1_Debug} \\
-  {imorphics Win_msvc} \\
-  {imorphics Linux_gcc} \\
+  {imorphics.com WinXP_msvc-7.1_RelWithDebInfo} \\
+  {imorphics.com Linux-2.6.22_gcc-4.1.2_RelWithDebInfo} \\
   {lems.brown.edu Linux-2.6_gcc-4.1.3_-Wall} \\
   {lems.brown.edu Linux-2.6.18_gcc-4.1.2} \\
   {lems.brown.edu Win32-vs71 } \\
