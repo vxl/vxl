c646ae96e0fe009aafe56b9791adf2ddf7d4af1a
Joseph L. Mundy <mundyj@users.sourceforge.net>
2008-01-08 06:57:59 -0500

    Improved clarity of implementation for camera homographies

--- contrib/gel/mrc/vpgl/algo/vpgl_camera_homographies.cxx
+++ contrib/gel/mrc/vpgl/algo/vpgl_camera_homographies.cxx
@@ -11,23 +11,27 @@ homography_to_camera(vpgl_proj_camera<double> const& cam,
   // get the translation that moves the plane to intersect with the origin
   vgl_vector_3d<double> n = plane.normal();
   double mag = n.length();
-  double trans = -plane.d()/(mag*mag);//translate the plane to the origin
+  double trans = plane.d()/(mag*mag);//translate the plane to the origin
   // find the rotation needed to align the normal with the positive z axis
   vgl_vector_3d<double> z(0, 0, 1.0);
   vgl_rotation_3d<double> R(n, z);
+  vgl_vector_3d<double> t = R*(trans*n);
+  vgl_h_matrix_3d<double> Tr = R.as_h_matrix_3d();
+  Tr.set_translation(t.x(), t.y(), t.z());
+  //note the composition is [R][t], i.e. first translate then rotate
   // invert to transform the camera
-  vgl_rotation_3d<double> Rinv = R.inverse();
-  vgl_h_matrix_3d<double> Tr = Rinv.as_h_matrix_3d();
-  Tr.set_translation(trans*n.x(), trans*n.y(), trans*n.z());
+  vgl_h_matrix_3d<double> Trinv = Tr.get_inverse();
+
   // postmultipy the camera by the inverse
-  vpgl_proj_camera<double> tcam = postmultiply(cam, Tr);
+  vpgl_proj_camera<double> tcam = postmultiply(cam, Trinv);
   // extract the homography (columns 0, 1, and 3)
   vnl_matrix_fixed<double, 3, 4> p = tcam.get_matrix();
   vnl_matrix_fixed<double, 3, 3> h;
   vnl_vector_fixed<double, 3> h0, h1, h2;//colums of h
   h0 = p.get_column(0);  h1 = p.get_column(1); h2 = p.get_column(3);
   h.set_column(0, h0);   h.set_column(1, h1); h.set_column(2, h2);
-  return vgl_h_matrix_2d<double>(h);
+  vgl_h_matrix_2d<double> H(h);
+  return H;
 }
 
   //: create a plane projective transformation from the camera image plane to
