6697a65fd928cd6cda632bd5f2ed9146246bb04f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-01-09 17:33:57 -0500

    avoid compiler warning (assigning "..." expression to char*) and meanwhile fixing potential memory allocation problem

--- contrib/brl/bseg/sbin/seg_vil.cxx
+++ contrib/brl/bseg/sbin/seg_vil.cxx
@@ -3,33 +3,35 @@
 #include <vgui/vgui_window.h>
 #include <segv/segv_vil_menus.h>
 #include <segv/segv_vil_segmentation_manager.h>
+#include <vcl_cstring.h>
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
 #if 1
-  int my_argc = argc+1;
   char** my_argv = new char*[argc+1];
-  for (int i=0; i<argc; i++)
-    my_argv[i] = argv[i];
-  my_argv[argc] = "--mfc-use-gl";
+  my_argv[0] = new char[13];
+  vcl_strcpy(my_argv[0], "--mfc-use-gl");
+  for (int i=0; i<argc; ++i)
+    my_argv[i+1] = argv[i];
 
    // Initialize the toolkit.
-  vgui::init(my_argc, my_argv);
+  vgui::init(++argc, my_argv);
+  delete [] my_argv[0];
   delete [] my_argv;
+#else
+  vgui::init(argc, argv);
 #endif
-//  vgui::init(argc, argv);
   vgui_menu menubar = segv_vil_menus::get_menu();
   unsigned w = 400, h = 340;
-   vcl_string title = "SEG_VIL";
-   vgui_window* win = vgui::produce_window(w, h, menubar, title);
-   segv_vil_segmentation_manager* segm = 
-     segv_vil_segmentation_manager::instance();
-   segm->set_window(win);
-   win->get_adaptor()->set_tableau(segm);
-   win->set_statusbar(true);
-   win->enable_vscrollbar(true);
-   win->enable_hscrollbar(true);
-   win->show();
-   vgui::run();
-   return 0;
+  vcl_string title = "SEG_VIL";
+  vgui_window* win = vgui::produce_window(w, h, menubar, title);
+  segv_vil_segmentation_manager* segm=segv_vil_segmentation_manager::instance();
+  segm->set_window(win);
+  win->get_adaptor()->set_tableau(segm);
+  win->set_statusbar(true);
+  win->enable_vscrollbar(true);
+  win->enable_hscrollbar(true);
+  win->show();
+  vgui::run();
+  return 0;
 }
