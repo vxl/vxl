f431a1b756158cd441130931fcbebd78187c191d
Joseph L. Mundy <mundyj@users.sourceforge.net>
2008-01-14 17:42:58 -0500

    removed wrong comments

--- contrib/brl/bbas/bsta/tests/test_driver.cxx
+++ contrib/brl/bbas/bsta/tests/test_driver.cxx
@@ -6,7 +6,7 @@ DECLARE( test_gaussian_indep );
 DECLARE( test_gaussian_sphere );
 DECLARE( test_mixture );
 DECLARE( test_bsta_histogram );
-//DECLARE( test_dbsta2_io );
+
 
 
 void
@@ -17,7 +17,7 @@ register_tests()
   REGISTER( test_gaussian_sphere );
   REGISTER( test_mixture );
   REGISTER( test_bsta_histogram );
-//  REGISTER( test_dbsta2_io );
+
 }
 
 DEFINE_MAIN;
