a23e1b585176035572e3f5873d520b5292988f58
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-01-19 10:31:26 -0500

    make template instantiation file independent from the fact that it resides in a subdirectory of the source files

--- contrib/gel/gst/Templates/vbl_smart_ptr+gst_vertex_2d-.cxx
+++ contrib/gel/gst/Templates/vbl_smart_ptr+gst_vertex_2d-.cxx
@@ -1,8 +1,4 @@
-/*
-  crossge@crd.ge.com
-*/
-#include "../gst_vertex_2d.h"
+#include <gst/gst_vertex_2d.h>
 #include <vbl/vbl_smart_ptr.txx>
- 
-VBL_SMART_PTR_INSTANTIATE(gst_vertex_2d);
 
+VBL_SMART_PTR_INSTANTIATE(gst_vertex_2d);
