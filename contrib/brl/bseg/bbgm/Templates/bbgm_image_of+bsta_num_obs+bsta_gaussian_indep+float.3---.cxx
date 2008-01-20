fae33b04bb16873c258bc5076b32351166acfdab
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-01-20 06:00:13 -0500

    added missing #includes

--- contrib/brl/bseg/bbgm/Templates/bbgm_image_of+bsta_num_obs+bsta_gaussian_indep+float.3---.cxx
+++ contrib/brl/bseg/bbgm/Templates/bbgm_image_of+bsta_num_obs+bsta_gaussian_indep+float.3---.cxx
@@ -1,6 +1,8 @@
 #include <bbgm/bbgm_image_of.txx>
 #include <bsta/bsta_attributes.h>
 #include <bsta/bsta_gaussian_indep.h>
+#include <bsta/io/bsta_io_attributes.h>
+#include <bsta/io/bsta_io_gaussian_indep.h>
 
 typedef bsta_num_obs<bsta_gaussian_indep<float,3> > gauss_type;
 BBGM_IMAGE_INSTANTIATE(gauss_type);
--- contrib/brl/bseg/bbgm/Templates/bbgm_image_of+bsta_num_obs+bsta_gaussian_sphere+float.1---.cxx
+++ contrib/brl/bseg/bbgm/Templates/bbgm_image_of+bsta_num_obs+bsta_gaussian_sphere+float.1---.cxx
@@ -1,6 +1,8 @@
 #include <bbgm/bbgm_image_of.txx>
 #include <bsta/bsta_attributes.h>
 #include <bsta/bsta_gaussian_sphere.h>
+#include <bsta/io/bsta_io_attributes.h>
+#include <bsta/io/bsta_io_gaussian_sphere.h>
 
 typedef bsta_num_obs<bsta_gaussian_sphere<float,1> > gauss_type;
 BBGM_IMAGE_INSTANTIATE(gauss_type);
