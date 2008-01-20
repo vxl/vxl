7b2c8bde03cd79f2b041ce68477252ada0bbc304
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-01-20 09:44:54 -0500

    replaced #include by .txx, to allow for auto-instantiation

--- contrib/brl/bseg/bbgm/Templates/bbgm_image_of+bsta_num_obs+bsta_gaussian_indep+float.3---.cxx
+++ contrib/brl/bseg/bbgm/Templates/bbgm_image_of+bsta_num_obs+bsta_gaussian_indep+float.3---.cxx
@@ -1,6 +1,6 @@
 #include <bbgm/bbgm_image_of.txx>
 #include <bsta/bsta_attributes.h>
-#include <bsta/bsta_gaussian_indep.h>
+#include <bsta/bsta_gaussian_indep.txx>
 #include <bsta/io/bsta_io_attributes.h>
 #include <bsta/io/bsta_io_gaussian_indep.h>
 
