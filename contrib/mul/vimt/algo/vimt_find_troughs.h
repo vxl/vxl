e06ca877d3dac6d59a4adf5a4c96582a8609554d
Tim Cootes <timcootes@users.sourceforge.net>
2008-02-01 05:33:27 -0500

    Added function.

--- contrib/mul/vimt/algo/vimt_find_troughs.h
+++ contrib/mul/vimt/algo/vimt_find_troughs.h
@@ -45,6 +45,31 @@ inline void vimt_find_image_troughs_3x3(vcl_vector<vgl_point_2d<unsigned> >& tro
   }
 }
 
+//: Return image co-ordinates of all points in image strictly below their 8 neighbours
+// \param clear_list: If true (the default) then empty list before adding new examples
+template <class T>
+inline void vimt_find_image_troughs_3x3(vcl_vector<vgl_point_2d<unsigned> >& troughs,
+                                      vcl_vector<T>& trough_value,
+                                        const vil_image_view<T>& image,
+                                        unsigned plane=0, bool clear_list=true)
+{
+  if (clear_list) troughs.resize(0);
+  unsigned ni=image.ni(),nj=image.nj();
+  vcl_ptrdiff_t istep = image.istep(),jstep=image.jstep();
+  const T* row = image.top_left_ptr()+plane*image.planestep()+istep+jstep;
+  for (unsigned j=1;j<nj-1;++j,row+=jstep) // do not run over border
+  {
+    const T* pixel = row;
+    for (unsigned i=1;i<ni-1;++i,pixel+=istep) // do not run over border
+      if (vimt_is_trough_3x3(pixel,istep,jstep))
+      {
+        troughs.push_back(vgl_point_2d<unsigned>(i,j));
+        trough_value.push_back(*pixel);
+      }
+  }
+}
+
+
 //: Return world co-ordinates of all points in image strictly below their 8 neighbours
 // \param clear_list: If true (the default) then empty list before adding new examples
 template <class T>
