35b770db12b87d787c15d496e9f6ac79c6391982
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-02-04 03:38:01 -0500

    added new tests test_k_medoid and test_otsu_threshold

--- contrib/brl/bbas/bsta/tests/test_driver.cxx
+++ contrib/brl/bbas/bsta/tests/test_driver.cxx
@@ -1,13 +1,12 @@
 #include <testlib/testlib_register.h>
 
-
 DECLARE( test_gaussian_full );
 DECLARE( test_gaussian_indep );
 DECLARE( test_gaussian_sphere );
 DECLARE( test_mixture );
 DECLARE( test_bsta_histogram );
-
-
+DECLARE( test_k_medoid );
+DECLARE( test_otsu_threshold );
 
 void
 register_tests()
@@ -17,10 +16,8 @@ register_tests()
   REGISTER( test_gaussian_sphere );
   REGISTER( test_mixture );
   REGISTER( test_bsta_histogram );
-
+  REGISTER( test_k_medoid );
+  REGISTER( test_otsu_threshold );
 }
 
 DEFINE_MAIN;
-
-
-
