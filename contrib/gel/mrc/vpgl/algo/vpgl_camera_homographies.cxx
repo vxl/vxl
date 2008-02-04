ec8c3fd450d896f3e0d5b3973f602847308a6785
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-02-04 09:50:12 -0500

    typo corrected: "colums"

--- contrib/gel/mrc/vpgl/algo/vpgl_camera_homographies.cxx
+++ contrib/gel/mrc/vpgl/algo/vpgl_camera_homographies.cxx
@@ -1,9 +1,12 @@
-#include <vpgl/algo/vpgl_camera_homographies.h>
+#include "vpgl_camera_homographies.h"
+//:
+// \file
 #include <vgl/vgl_vector_3d.h>
 #include <vgl/algo/vgl_rotation_3d.h>
 #include <vgl/algo/vgl_h_matrix_3d.h>
 #include <vnl/vnl_vector_fixed.h>
 #include <vnl/vnl_matrix_fixed.h>
+
 vgl_h_matrix_2d<double> vpgl_camera_homographies::
 homography_to_camera(vpgl_proj_camera<double> const& cam,
                      vgl_plane_3d<double> const& plane)
@@ -27,44 +30,39 @@ homography_to_camera(vpgl_proj_camera<double> const& cam,
   // extract the homography (columns 0, 1, and 3)
   vnl_matrix_fixed<double, 3, 4> p = tcam.get_matrix();
   vnl_matrix_fixed<double, 3, 3> h;
-  vnl_vector_fixed<double, 3> h0, h1, h2;//colums of h
+  vnl_vector_fixed<double, 3> h0, h1, h2; //columns of h
   h0 = p.get_column(0);  h1 = p.get_column(1); h2 = p.get_column(3);
   h.set_column(0, h0);   h.set_column(1, h1); h.set_column(2, h2);
   vgl_h_matrix_2d<double> H(h);
   return H;
 }
 
-  //: create a plane projective transformation from the camera image plane to
-  //  the specified plane
+//: create a plane projective transformation from the camera image plane to the specified plane
 vgl_h_matrix_2d<double> vpgl_camera_homographies::
 homography_to_camera(vpgl_perspective_camera<double> const& cam,
                      vgl_plane_3d<double> const& plane)
 {
-  vpgl_proj_camera<double> const& pcam = 
+  vpgl_proj_camera<double> const& pcam =
     static_cast<vpgl_proj_camera<double> const&>(cam);
   return vpgl_camera_homographies::homography_to_camera(pcam, plane);
 }
 
-  //: create a plane projective transformation from the camera image plane to
-  //  the specified plane
+//: create a plane projective transformation from the camera image plane to the specified plane
 vgl_h_matrix_2d<double> vpgl_camera_homographies::
 homography_from_camera(vpgl_proj_camera<double> const& cam,
                        vgl_plane_3d<double> const& plane)
 {
-  vgl_h_matrix_2d<double> H = 
+  vgl_h_matrix_2d<double> H =
     vpgl_camera_homographies::homography_to_camera(cam, plane);
   return H.get_inverse();
 }
 
-  //: create a plane projective transformation from the camera image plane to
-  //  the specified plane
+//: create a plane projective transformation from the camera image plane to the specified plane
 vgl_h_matrix_2d<double> vpgl_camera_homographies::
 homography_from_camera(vpgl_perspective_camera<double> const& cam,
-                     vgl_plane_3d<double> const& plane)
+                       vgl_plane_3d<double> const& plane)
 {
-  vgl_h_matrix_2d<double> H = 
+  vgl_h_matrix_2d<double> H =
     vpgl_camera_homographies::homography_to_camera(cam, plane);
   return H.get_inverse();
 }
-
-
