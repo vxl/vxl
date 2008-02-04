fbc431bc6c852aaf643971e8ada6eca70bcd970d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-02-04 03:42:40 -0500

    removed no longer existing bbgm_image_of.txx

--- contrib/brl/bseg/bbgm/tests/test_include.cxx
+++ contrib/brl/bseg/bbgm/tests/test_include.cxx
@@ -1,13 +1,10 @@
-
-#include<bbgm/bbgm_apply.h>
-#include<bbgm/bbgm_detect.h>
-#include<bbgm/bbgm_update.h>
-#include<bbgm/bbgm_planes_to_sample.h>
-#include<bbgm/bbgm_image_of.h>
-#include<bbgm/bbgm_image_sptr.h>
-#include<bbgm/bbgm_image_of.txx>
-#include<bbgm/bbgm_viewer_sptr.h>
-#include<bbgm/bbgm_viewer.h>
-
+#include <bbgm/bbgm_apply.h>
+#include <bbgm/bbgm_detect.h>
+#include <bbgm/bbgm_update.h>
+#include <bbgm/bbgm_planes_to_sample.h>
+#include <bbgm/bbgm_image_of.h>
+#include <bbgm/bbgm_image_sptr.h>
+#include <bbgm/bbgm_viewer_sptr.h>
+#include <bbgm/bbgm_viewer.h>
 
 int main() { return 0; }
