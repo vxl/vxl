7ff7c522688931b6d4312e81f16f62b72d4558cd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-02-04 11:39:49 -0500

    avoid identifiers starting with underscore

--- contrib/brl/bbas/bsta/algo/bsta_adaptive_updater.h
+++ contrib/brl/bbas/bsta/algo/bsta_adaptive_updater.h
@@ -1,19 +1,18 @@
-// This is brcv/seg/bsta/algo/bsta_adaptive_updater.h
+// This is brl/bbas/bsta/algo/bsta_adaptive_updater.h
 #ifndef bsta_adaptive_updater_h_
 #define bsta_adaptive_updater_h_
-
 //:
 // \file
-// \brief Adaptive updaters for mixtures of Gaussians (i.e. Grimson and similar) 
+// \brief Adaptive updaters for mixtures of Gaussians (i.e. Grimson and similar)
 // \author Matt Leotta (mleotta@lems.brown.edu)
 // \date 07/13/05
 //
-// This file contains updaters based on 
+// This file contains updaters based on
 // "Adaptive background mixture models for real-time tracking"
 // Grimson et. al.
 //
 // \verbatim
-//   Modifications
+//  Modifications
 // \endverbatim
 
 #include <bsta/bsta_distribution.h>
@@ -21,51 +20,45 @@
 #include <bsta/bsta_mixture_fixed.h>
 #include <bsta/bsta_attributes.h>
 #include <bsta/bsta_gauss_ff3.h>
-#include <vnl/vnl_matrix.h>
-#include "bsta_adaptive_updater.h"
 #include "bsta_gaussian_updater.h"
 #include "bsta_gaussian_stats.h"
 
-#include <vcl_iostream.h>
-#include <vcl_limits.h>
-//#include "bsta_gaussian_stats.h"
-
 
 //: A mixture of Gaussians adaptive updater
-// base class for common functionality in adaptive updating schemes
-template <class _mix_dist>
+//  Base class for common functionality in adaptive updating schemes
+template <class mix_dist_>
 class bsta_mg_adaptive_updater
 {
-  private:  
-    typedef typename _mix_dist::dist_type _obs_gaussian;
-    typedef typename _obs_gaussian::contained_type _gaussian;
-    typedef typename _gaussian::math_type T;
-    typedef typename _gaussian::vector_type _vector;
+  private:
+    typedef typename mix_dist_::dist_type obs_gaussian_;
+    typedef typename obs_gaussian_::contained_type gaussian_;
+    typedef typename gaussian_::math_type T;
+    typedef typename gaussian_::vector_type vector_;
 
   protected:
     //: Constructor
-    bsta_mg_adaptive_updater(const _gaussian& model,
-                               unsigned int max_cmp = 5)
+    bsta_mg_adaptive_updater(const gaussian_& model,
+                             unsigned int max_cmp = 5)
      : init_gaussian_(model,T(1)),
        max_components_(max_cmp) {}
 
-    void insert(_mix_dist& mixture, const _vector& sample, T init_weight) const
+    void insert(mix_dist_& mixture, const vector_& sample, T init_weight) const
     {
       bool removed = false;
-      if(mixture.num_components() >= max_components_){
+      if (mixture.num_components() >= max_components_){
         removed = true;
-        do{
-          mixture.remove_last(); 
-        }while(mixture.num_components() >= max_components_);
+        do {
+          mixture.remove_last();
+        } while (mixture.num_components() >= max_components_);
       }
 
       // if a mixture is removed renormalize the rest
-      if(removed){
+      if (removed){
         T adjust = T(0);
-        for(unsigned int i=0; i<mixture.num_components(); ++i)
+        for (unsigned int i=0; i<mixture.num_components(); ++i)
           adjust += mixture.weight(i);
         adjust = (T(1)-init_weight) / adjust;
-        for(unsigned int i=0; i<mixture.num_components(); ++i)
+        for (unsigned int i=0; i<mixture.num_components(); ++i)
           mixture.set_weight(i, mixture.weight(i)*adjust);
       }
       init_gaussian_.set_mean(sample);
@@ -73,45 +66,45 @@ class bsta_mg_adaptive_updater
     }
 
     //: A model for new Gaussians inserted
-    mutable _obs_gaussian init_gaussian_;
+    mutable obs_gaussian_ init_gaussian_;
     //: The maximum number of components in the mixture
     unsigned int max_components_;
-
 };
 
 
 //: A mixture of Gaussians statistical updater
-// This updater treats all data equally
-template <class _mix_dist>
-class bsta_mg_statistical_updater : public bsta_mg_adaptive_updater<_mix_dist>
+//  This updater treats all data equally
+template <class mix_dist_>
+class bsta_mg_statistical_updater : public bsta_mg_adaptive_updater<mix_dist_>
 {
   public:
-    typedef typename _mix_dist::dist_type _obs_gaussian;
-    typedef typename _obs_gaussian::contained_type _gaussian;
-    typedef typename _gaussian::math_type T;
-    typedef typename _gaussian::vector_type _vector;
-    typedef bsta_num_obs<_mix_dist> _obs_mix_dist;
+    typedef typename mix_dist_::dist_type obs_gaussian_;
+    typedef typename obs_gaussian_::contained_type gaussian_;
+    typedef typename gaussian_::math_type T;
+    typedef typename gaussian_::vector_type vector_;
+    typedef bsta_num_obs<mix_dist_> obs_mix_dist_;
 
-    enum { data_dimension = _gaussian::dimension };
+    enum { data_dimension = gaussian_::dimension };
 
     //: Constructor
-    bsta_mg_statistical_updater(const _gaussian& model,
-                                  unsigned int max_cmp = 5,
-                                  T g_thresh = T(3),
-                                  T min_stdev = T(0))
-      : bsta_mg_adaptive_updater<_mix_dist>(model, max_cmp),
+    bsta_mg_statistical_updater(const gaussian_& model,
+                                unsigned int max_cmp = 5,
+                                T g_thresh = T(3),
+                                T min_stdev = T(0))
+      : bsta_mg_adaptive_updater<mix_dist_>(model, max_cmp),
         gt2_(g_thresh*g_thresh), min_var_(min_stdev*min_stdev) {}
 
     //: The main function
-    void operator() ( _obs_mix_dist& mix, const _vector& sample ) const
+    void operator() ( obs_mix_dist_& mix, const vector_& sample ) const
     {
       mix.num_observations += T(1);
       this->update(mix, sample, T(1)/mix.num_observations);
     }
 
-    void update( _mix_dist& mix, const _vector& sample, T alpha ) const;
-
-   // void update( _mix_dist& mix, const T & sample, T alpha ) const;
+    void update( mix_dist_& mix, const vector_& sample, T alpha ) const;
+#if 0
+    void update( mix_dist_& mix, const T & sample, T alpha ) const;
+#endif
     //: Squared Gaussian Mahalanobis distance threshold
     T gt2_;
     //: Minimum variance allowed in each Gaussian component
@@ -119,33 +112,33 @@ class bsta_mg_statistical_updater : public bsta_mg_adaptive_updater<_mix_dist>
 };
 
 
-//: A mixture of Gaussians statistical updater 
-// This updater treats all data equally
-template <class _mix_dist>
-class bsta_mg_window_updater : public bsta_mg_statistical_updater<_mix_dist>
+//: A mixture of Gaussians statistical updater
+//  This updater treats all data equally
+template <class mix_dist_>
+class bsta_mg_window_updater : public bsta_mg_statistical_updater<mix_dist_>
 {
   public:
-    typedef typename _mix_dist::dist_type _obs_gaussian;
-    typedef typename _obs_gaussian::contained_type _gaussian;
-    typedef typename _gaussian::math_type T;
-    typedef typename _gaussian::vector_type _vector;
-    typedef bsta_num_obs<_mix_dist> _obs_mix_dist;
+    typedef typename mix_dist_::dist_type obs_gaussian_;
+    typedef typename obs_gaussian_::contained_type gaussian_;
+    typedef typename gaussian_::math_type T;
+    typedef typename gaussian_::vector_type vector_;
+    typedef bsta_num_obs<mix_dist_> obs_mix_dist_;
 
-    enum { data_dimension = _gaussian::dimension };
+    enum { data_dimension = gaussian_::dimension };
 
     //: Constructor
-    bsta_mg_window_updater(const _gaussian& model,
-                             unsigned int max_cmp = 5,
-                             T g_thresh = T(3),
-                             T min_stdev = T(0),
-                             unsigned int window_size = 40)
-  : bsta_mg_statistical_updater<_mix_dist>(model, max_cmp, g_thresh, min_stdev),
+    bsta_mg_window_updater(const gaussian_& model,
+                           unsigned int max_cmp = 5,
+                           T g_thresh = T(3),
+                           T min_stdev = T(0),
+                           unsigned int window_size = 40)
+  : bsta_mg_statistical_updater<mix_dist_>(model, max_cmp, g_thresh, min_stdev),
         window_size_(window_size) {}
 
     //: The main function
-    void operator() ( _obs_mix_dist& mix, const _vector& sample ) const
+    void operator() ( obs_mix_dist_& mix, const vector_& sample ) const
     {
-      if(mix.num_observations < window_size_)
+      if (mix.num_observations < window_size_)
         mix.num_observations += T(1);
       this->update(mix, sample, T(1)/mix.num_observations);
     }
@@ -155,66 +148,65 @@ class bsta_mg_window_updater : public bsta_mg_statistical_updater<_mix_dist>
 };
 
 //: A mixture of Gaussians weighted statistical updater
-// This updater treats data according to a specified weigth
-template <class _mix_dist>
-class bsta_mg_weighted_updater : bsta_mg_statistical_updater<_mix_dist>
+//  This updater treats data according to a specified weigth
+template <class mix_dist_>
+class bsta_mg_weighted_updater : bsta_mg_statistical_updater<mix_dist_>
 {
   public:
-    typedef typename _mix_dist::dist_type _obs_gaussian;
-    typedef typename _obs_gaussian::contained_type _gaussian;
-    typedef typename _gaussian::math_type T;
-    typedef typename _gaussian::vector_type _vector;
-    typedef bsta_num_obs<_mix_dist> _obs_mix_dist;
+    typedef typename mix_dist_::dist_type obs_gaussian_;
+    typedef typename obs_gaussian_::contained_type gaussian_;
+    typedef typename gaussian_::math_type T;
+    typedef typename gaussian_::vector_type vector_;
+    typedef bsta_num_obs<mix_dist_> obs_mix_dist_;
 
-    enum { data_dimension = _gaussian::dimension };
+    enum { data_dimension = gaussian_::dimension };
 
     //: Constructor
-    bsta_mg_weighted_updater(const _gaussian& model,
-                                  unsigned int max_cmp = 5,
-                                  T g_thresh = T(3),
-                                  T min_stdev = T(0))
-      : bsta_mg_statistical_updater<_mix_dist>(model, max_cmp, g_thresh, min_stdev){}
+    bsta_mg_weighted_updater(const gaussian_& model,
+                             unsigned int max_cmp = 5,
+                             T g_thresh = T(3),
+                             T min_stdev = T(0))
+      : bsta_mg_statistical_updater<mix_dist_>(model, max_cmp, g_thresh, min_stdev){}
 
     //: The main function
-    void operator() ( _obs_mix_dist& mix, const _vector& sample, const T weight ) const
+    void operator() ( obs_mix_dist_& mix, const vector_& sample, const T weight ) const
     {
       mix.num_observations += weight;
       this->update(mix, sample, weight/mix.num_observations);
     }
-
 };
 
 
 //: A mixture of Gaussians statistical updater
-// using the grimson approximation to prior probablilities
-template <class _mix_dist>
-class bsta_mg_grimson_statistical_updater : public bsta_mg_adaptive_updater<_mix_dist>
+//  Using the grimson approximation to prior probablilities
+template <class mix_dist_>
+class bsta_mg_grimson_statistical_updater : public bsta_mg_adaptive_updater<mix_dist_>
 {
   public:
-    typedef typename _mix_dist::dist_type _obs_gaussian;
-    typedef typename _obs_gaussian::contained_type _gaussian;
-    typedef typename _gaussian::math_type T;
-    typedef typename _gaussian::vector_type _vector;
-    typedef bsta_num_obs<_mix_dist> _obs_mix_dist;
+    typedef typename mix_dist_::dist_type obs_gaussian_;
+    typedef typename obs_gaussian_::contained_type gaussian_;
+    typedef typename gaussian_::math_type T;
+    typedef typename gaussian_::vector_type vector_;
+    typedef bsta_num_obs<mix_dist_> obs_mix_dist_;
 
-    enum { data_dimension = _gaussian::dimension };
+    enum { data_dimension = gaussian_::dimension };
 
     //: Constructor
-    bsta_mg_grimson_statistical_updater(const _gaussian& model,
-                                          unsigned int max_cmp = 5,
-                                          T g_thresh = T(3),
-                                          T min_stdev = T(0) )
-      : bsta_mg_adaptive_updater<_mix_dist>(model, max_cmp),
+    bsta_mg_grimson_statistical_updater(const gaussian_& model,
+                                        unsigned int max_cmp = 5,
+                                        T g_thresh = T(3),
+                                        T min_stdev = T(0) )
+      : bsta_mg_adaptive_updater<mix_dist_>(model, max_cmp),
         gt2_(g_thresh*g_thresh), min_var_(min_stdev*min_stdev) {}
 
     //: The main function
-    void operator() ( _obs_mix_dist& mix, const _vector& sample ) const
+    void operator() ( obs_mix_dist_& mix, const vector_& sample ) const
     {
       mix.num_observations += T(1);
       this->update(mix, sample, T(1)/mix.num_observations);
     }
 
-    void update( _mix_dist& mix, const _vector& sample, T alpha ) const;
+    void update( mix_dist_& mix, const vector_& sample, T alpha ) const;
 
     //: Squared Gaussian Mahalanobis distance threshold
     T gt2_;
@@ -222,33 +214,33 @@ class bsta_mg_grimson_statistical_updater : public bsta_mg_adaptive_updater<_mix
     T min_var_;
 };
 
-//: A mixture of Gaussians window updater 
-// using the grimson approximation to prior probablilities
-template <class _mix_dist>
-class bsta_mg_grimson_window_updater : public bsta_mg_grimson_statistical_updater<_mix_dist>
+//: A mixture of Gaussians window updater
+//  Using the grimson approximation to prior probablilities
+template <class mix_dist_>
+class bsta_mg_grimson_window_updater : public bsta_mg_grimson_statistical_updater<mix_dist_>
 {
   public:
-    typedef typename _mix_dist::dist_type _obs_gaussian;
-    typedef typename _obs_gaussian::contained_type _gaussian;
-    typedef typename _gaussian::math_type T;
-    typedef typename _gaussian::vector_type _vector;
-    typedef bsta_num_obs<_mix_dist> _obs_mix_dist;
+    typedef typename mix_dist_::dist_type obs_gaussian_;
+    typedef typename obs_gaussian_::contained_type gaussian_;
+    typedef typename gaussian_::math_type T;
+    typedef typename gaussian_::vector_type vector_;
+    typedef bsta_num_obs<mix_dist_> obs_mix_dist_;
 
-    enum { data_dimension = _gaussian::dimension };
+    enum { data_dimension = gaussian_::dimension };
 
     //: Constructor
-    bsta_mg_grimson_window_updater(const _gaussian& model,
-                                     unsigned int max_cmp = 5,
-                                     T g_thresh = T(3),
-                                     T min_stdev = T(0),
-                                     unsigned int window_size = 40) 
-      : bsta_mg_grimson_statistical_updater<_mix_dist>(model, max_cmp, g_thresh, min_stdev),
+    bsta_mg_grimson_window_updater(const gaussian_& model,
+                                   unsigned int max_cmp = 5,
+                                   T g_thresh = T(3),
+                                   T min_stdev = T(0),
+                                   unsigned int window_size = 40)
+      : bsta_mg_grimson_statistical_updater<mix_dist_>(model, max_cmp, g_thresh, min_stdev),
         window_size_(window_size) {}
 
     //: The main function
-    void operator() ( _obs_mix_dist& mix, const _vector& sample ) const
+    void operator() ( obs_mix_dist_& mix, const vector_& sample ) const
     {
-      if(mix.num_observations < window_size_)
+      if (mix.num_observations < window_size_)
         mix.num_observations += T(1);
       this->update(mix, sample, T(1)/mix.num_observations);
     }
@@ -258,28 +250,28 @@ class bsta_mg_grimson_window_updater : public bsta_mg_grimson_statistical_update
 };
 
 //: A mixture of Gaussians statistical weighted updater
-// using the grimson approximation to prior probablilities
-template <class _mix_dist>
-class bsta_mg_grimson_weighted_updater : bsta_mg_grimson_statistical_updater<_mix_dist>
+//  Using the grimson approximation to prior probablilities
+template <class mix_dist_>
+class bsta_mg_grimson_weighted_updater : bsta_mg_grimson_statistical_updater<mix_dist_>
 {
   public:
-    typedef typename _mix_dist::dist_type _obs_gaussian;
-    typedef typename _obs_gaussian::contained_type _gaussian;
-    typedef typename _gaussian::math_type T;
-    typedef typename _gaussian::vector_type _vector;
-    typedef bsta_num_obs<_mix_dist> _obs_mix_dist;
+    typedef typename mix_dist_::dist_type obs_gaussian_;
+    typedef typename obs_gaussian_::contained_type gaussian_;
+    typedef typename gaussian_::math_type T;
+    typedef typename gaussian_::vector_type vector_;
+    typedef bsta_num_obs<mix_dist_> obs_mix_dist_;
 
-    enum { data_dimension = _gaussian::dimension };
+    enum { data_dimension = gaussian_::dimension };
 
     //: Constructor
-    bsta_mg_grimson_weighted_updater(const _gaussian& model,
+    bsta_mg_grimson_weighted_updater(const gaussian_& model,
                                           unsigned int max_cmp = 5,
                                           T g_thresh = T(3),
                                           T min_stdev = T(0) )
-      : bsta_mg_grimson_statistical_updater<_mix_dist>(model, max_cmp, g_thresh, min_stdev){}
+      : bsta_mg_grimson_statistical_updater<mix_dist_>(model, max_cmp, g_thresh, min_stdev){}
 
     //: The main function
-    void operator() ( _obs_mix_dist& mix, const _vector& sample, const T weight ) const
+    void operator() ( obs_mix_dist_& mix, const vector_& sample, const T weight ) const
     {
       mix.num_observations += weight;
       this->update(mix, sample, weight/mix.num_observations);
--- contrib/brl/bbas/bsta/algo/bsta_adaptive_updater.txx
+++ contrib/brl/bbas/bsta/algo/bsta_adaptive_updater.txx
@@ -1,7 +1,6 @@
-// This is brcv/seg/bsta/algo/bsta_adaptive_updater.txx
+// This is brl/bbas/bsta/algo/bsta_adaptive_updater.txx
 #ifndef bsta_adaptive_updater_txx_
 #define bsta_adaptive_updater_txx_
-
 //:
 // \file
 #include "bsta_adaptive_updater.h"
@@ -20,9 +19,9 @@
 
 
 //: The update function
-template <class _mix_dist>
+template <class mix_dist_>
 void
-bsta_mg_statistical_updater<_mix_dist>::update( _mix_dist& mix, const _vector& sample, T alpha ) const
+bsta_mg_statistical_updater<mix_dist_>::update( mix_dist_& mix, const vector_& sample, T alpha ) const
 {
   const unsigned num_components = mix.num_components();
 
@@ -32,53 +31,54 @@ bsta_mg_statistical_updater<_mix_dist>::update( _mix_dist& mix, const _vector& s
   static vcl_vector<unsigned int> matched;
   matched.clear();
 
-  for(unsigned int i=0; i<num_components; ++i){
-    _obs_gaussian& g = mix.distribution(i);
+  for (unsigned int i=0; i<num_components; ++i) {
+    obs_gaussian_& g = mix.distribution(i);
     T sqr_dist = g.sqr_mahalanobis_dist(sample);
-    if(sqr_dist < gt2_){
+    if (sqr_dist < gt2_) {
       matched.push_back(i);
       probs[i] = sqr_dist; // initially these are distances, not probabilities
     }
-  } 
+  }
 
   // if no matches add a new component
-  if(matched.empty()){
+  if (matched.empty()) {
     insert(mix,sample,alpha);
     mix.normalize_weights();
   }
-  else{
+  else
+  {
     // update the weights
-    for(unsigned int i=0; i<num_components; ++i){
+    for (unsigned int i=0; i<num_components; ++i) {
       T weight = (T(1)-alpha) * mix.weight(i);
       mix.set_weight(i,weight);
     }
     // special case of 1 match - don't need to compute probabilites
-    if (matched.size() == 1){
+    if (matched.size() == 1) {
       unsigned int m_idx = matched.front();
       mix.set_weight(m_idx,mix.weight(m_idx)+alpha);
-      _obs_gaussian& g = mix.distribution(m_idx);
+      obs_gaussian_& g = mix.distribution(m_idx);
       g.num_observations += T(1);
-      T rho =(T(1)-alpha)/g.num_observations + alpha; 
+      T rho =(T(1)-alpha)/g.num_observations + alpha;
       bsta_update_gaussian(g, rho, sample, min_var_);
     }
-    else{
+    else {
       // compute probabilites for each match
       typedef typename vcl_vector<unsigned int>::iterator m_itr;
       T sum_probs = T(0);
-      for(m_itr itr = matched.begin(); itr != matched.end(); ++itr){
+      for (m_itr itr = matched.begin(); itr != matched.end(); ++itr) {
         const unsigned int i = *itr;
-        _obs_gaussian& g = mix.distribution(i);
+        obs_gaussian_& g = mix.distribution(i);
         probs[i] = g.dist_prob_density(probs[i]) * mix.weight(i);
         sum_probs += probs[i];
       }
       // update each match
-      for(m_itr itr = matched.begin(); itr != matched.end(); ++itr){
+      for (m_itr itr = matched.begin(); itr != matched.end(); ++itr) {
         const unsigned int i = *itr;
         if (sum_probs != 0) {
-	  probs[i] /= sum_probs;
-	}
+          probs[i] /= sum_probs;
+        }
         mix.set_weight(i,mix.weight(i)+alpha*probs[i]);
-        _obs_gaussian& g = mix.distribution(i);
+        obs_gaussian_& g = mix.distribution(i);
         g.num_observations += probs[i];
         T rho = probs[i] * ((1-alpha)/g.num_observations + alpha);
         bsta_update_gaussian(g, rho, sample, min_var_);
@@ -86,35 +86,34 @@ bsta_mg_statistical_updater<_mix_dist>::update( _mix_dist& mix, const _vector& s
     }
   }
 
-  mix.sort(bsta_gaussian_fitness<_gaussian>::order);
+  mix.sort(bsta_gaussian_fitness<gaussian_>::order);
 
   // try to clean up gaussian components with weights that have converged to zero
-  if(mix.weight(mix.num_components()-1) < vcl_numeric_limits<T>::epsilon()){
+  if (mix.weight(mix.num_components()-1) < vcl_numeric_limits<T>::epsilon()) {
     mix.remove_last();
     T sum = 0;
-    for(unsigned int i=0; i<mix.num_components(); ++i){
+    for (unsigned int i=0; i<mix.num_components(); ++i) {
       sum += mix.weight(i);
     }
     vcl_cout << "removed, total weight = " << sum << vcl_endl;
     mix.normalize_weights();
   }
-
 }
 
 
 //: The main function
-template <class _mix_dist>
-void 
-bsta_mg_grimson_statistical_updater<_mix_dist>::update( _mix_dist& mix,
-                                                          const _vector& sample,
-                                                          T alpha ) const
+template <class mix_dist_>
+void
+bsta_mg_grimson_statistical_updater<mix_dist_>::update( mix_dist_& mix,
+                                                        const vector_& sample,
+                                                        T alpha ) const
 {
   int match = -1;
   unsigned int mix_nc = mix.num_components();
-  for(unsigned int i=0; i<mix_nc; ++i){
-    _obs_gaussian& g = mix.distribution(i);
+  for (unsigned int i=0; i<mix_nc; ++i) {
+    obs_gaussian_& g = mix.distribution(i);
     T weight = (T(1)-alpha) * mix.weight(i);
-    if(match<0 && g.sqr_mahalanobis_dist(sample) < gt2_){
+    if (match<0 && g.sqr_mahalanobis_dist(sample) < gt2_) {
       weight += alpha;
       g.num_observations += T(1);
       T rho = (T(1)-alpha)/g.num_observations + alpha;
@@ -123,15 +122,14 @@ bsta_mg_grimson_statistical_updater<_mix_dist>::update( _mix_dist& mix,
     }
     mix.set_weight(i, weight);
   }
-  if(match<0){
+  if (match<0) {
     insert(mix,sample,alpha);
     match = mix.num_components()-1;
   }
 
-  if(match>0)
-    mix.sort(bsta_gaussian_fitness<_gaussian>::order, match);
+  if (match>0)
+    mix.sort(bsta_gaussian_fitness<gaussian_>::order, match);
 }
-
 
 
 #define DBSTA_ADAPTIVE_UPDATER_INSTANTIATE(T) \
