a0b28b10e04f6116e5487a30b314889962b0cd59
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-02-04 04:45:19 -0500

    added missing #include <algorithm> for std::max()

--- contrib/mul/mfpf/mfpf_norm_vec.h
+++ contrib/mul/mfpf/mfpf_norm_vec.h
@@ -1,12 +1,12 @@
 #ifndef mfpf_norm_vec_h_
 #define mfpf_norm_vec_h_
-
 //:
 // \file
 // \brief Sets vec to have zero mean and unit length
 // \author Tim Cootes
 
 #include <vnl/vnl_vector.h>
+#include <vcl_algorithm.h> // std::max()
 
 //: Sets vec to have zero mean and unit length
 inline void mfpf_norm_vec(vnl_vector<double>& vec)
@@ -26,5 +26,3 @@ inline void mfpf_norm_vec(vnl_vector<double>& vec)
 }
 
 #endif // mfpf_norm_vec_h_
-
-
