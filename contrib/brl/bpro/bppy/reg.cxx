14c74bd4922c1ffd7407840db8142ecd74a49c02
Joseph L. Mundy <mundyj@users.sourceforge.net>
2008-02-11 08:52:40 -0500

    added more processes

--- contrib/brl/bpro/bppy/reg.cxx
+++ contrib/brl/bpro/bppy/reg.cxx
@@ -14,6 +14,7 @@
 #include <bbgm/pro/bbgm_update_dist_image_process.h>
 #include <bbgm/pro/bbgm_update_dist_image_stream_process.h>
 #include <bbgm/pro/bbgm_display_dist_image_process.h>
+#include <bbgm/pro/bbgm_measure_process.h>
 PyObject *
 register_processes(PyObject *self, PyObject *args)
 {
@@ -27,6 +28,7 @@ register_processes(PyObject *self, PyObject *args)
   REG_PROCESS(bbgm_update_dist_image_process, bprb_batch_process_manager);
   REG_PROCESS(bbgm_update_dist_image_stream_process, bprb_batch_process_manager);
   REG_PROCESS(bbgm_display_dist_image_process, bprb_batch_process_manager);
+  REG_PROCESS(bbgm_measure_process, bprb_batch_process_manager);
 	Py_INCREF(Py_None);
 	return Py_None;
 }
