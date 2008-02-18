3fbad4659e178d9b609813605335799a69d85d26
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-02-18 02:52:54 -0500

    removed duplicate #include <string>

--- contrib/brl/bpro/bprb/bprb_process.h
+++ contrib/brl/bpro/bprb/bprb_process.h
@@ -1,7 +1,7 @@
 // This is brl/bpro/bprb/bprb_process.h
 #ifndef bprb_process_h_
 #define bprb_process_h_
-//--------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 //:
 // \file
 // \brief The bprb process base class
@@ -20,21 +20,20 @@
 //   Edu Almeida   April    21, 2007  Added global methods and variables
 //   J.L. Mundy    January  30, 2008  Migrated back to brl/bpro,adapted to brdb
 // \endverbatim
-//--------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 #include <vcl_vector.h>
-#include <vcl_map.h>
 #include <vcl_string.h>
 #include <vbl/vbl_ref_count.h>
 #include <brdb/brdb_value.h>
 #include <bprb/bprb_parameters_sptr.h>
-
+#include <brdb/brdb_value_sptr.h>
 
 //: This abstract class is the base class for process objects
 //
 // The process manager uses polymorphism to manage and execute
 // classes derived from this one.  The process manager will call
-// your execute function once for every frame of the video.  The 
+// your execute function once for every frame of the video.  The
 // process manager will call your finish function once after all
 // calls to execute are finished.
 //
@@ -46,7 +45,7 @@
 // - finish
 //
 // The clone and name function are trivial.  The clone function
-// creates a copy of your process.  The following should be 
+// creates a copy of your process.  The following should be
 // sufficient in almost all cases (in some cases you may need
 // to write an explicit copy constructor).
 // \verbatim
@@ -55,22 +54,21 @@
 //   return new bprb_my_process(*this);
 // }
 // \endverbatim
-// The name function should simply return a string containing 
+// The name function should simply return a string containing
 // a unique name.  Think of this as a title for your process.
-// It must be unique for identification, but it may also 
+// It must be unique for identification, but it may also
 // appear in menus to allow a user to select your process.
 //
 // Typically, the constructor is used to define the parameters
-// for a process. See bprb_parameters. 
-#include <brdb/brdb_value_sptr.h>
-#include <vcl_string.h>
+// for a process. See bprb_parameters.
+
 class bprb_process : public vbl_ref_count
 {
  public:
   bprb_process();
   //: Clone the process
   virtual bprb_process* clone() const = 0;
-  
+
   //: Return a smart pointer to the parameters
   bprb_parameters_sptr parameters();
 
@@ -111,23 +109,22 @@ class bprb_process : public vbl_ref_count
 
   //: get a particular output
   brdb_value_sptr output(unsigned i)
-    {if(i<n_inputs()) return output_data_[i]; return 0;}
-  
+    { if (i<n_inputs()) return output_data_[i]; return 0; }
+
   //: Insure that inputs are valid
   bool verify_inputs();
 
   //: Perform any initialization required by the process
   virtual bool init() = 0;
 
-  //: Execute the process 
+  //: Execute the process
   virtual bool execute() = 0;
 
-  //: Perform any clean up or final computation 
+  //: Perform any clean up or final computation
   virtual bool finish() = 0;
-  
 
  protected:
-  
+
   //: Copy Constructor
   bprb_process(const bprb_process& other);
   virtual ~bprb_process();
