1192eb010ad6efa05bb739fbf4eea48be0d1a934
Ian Scott <scottim@users.sourceforge.net>
2008-02-22 18:07:24 -0500

    A handy little conversion from vsl_print_summary(os,x); to os << vsl_stream_summary(x);

---
+++ core/vsl/vsl_stream.h
@@ -0,0 +1,44 @@
+// This is core/vsl/vsl_stream.h
+#ifndef vsl_stream_h_
+#define vsl_stream_h_
+//:
+// \file
+// \brief Allows you to use the stream output operator instead of vsl_print_summary.
+// \author Ian Scott
+// So instead of
+// \code
+// os << "Blah: ";
+// vsl_print_summary(os, blah);
+// \endcode
+// you can use
+// \code os << "Blah: " << vsl_stream_summary(blah); \endcode
+#include <vcl_ostream.h>
+
+
+
+
+//: Convert a vsl_print_summary function call to a streamable object.
+template <class T>
+struct vsl_stream_summary_t
+{
+  const T& x;
+  vsl_stream_summary_t(const T& x): x(x) {}
+};
+
+template <class T>
+inline struct vsl_stream_summary_t<T> vsl_stream_summary(const T& x)
+{
+  return vsl_stream_summary_t<T>(x);
+}
+
+
+//: Insert conversion object into stream
+template <class T>
+inline vcl_ostream& operator <<(vcl_ostream &os, const vsl_stream_summary_t<T>& sss)
+{
+  vsl_print_summary(os, sss.x);
+  return os;
+}
+
+
+#endif // vsl_stream_h_
