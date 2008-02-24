8ee575744d6c10748069242ab1f2c092467865a2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-02-24 12:10:00 -0500

    added the "standard" header files test

---
+++ contrib/brl/bseg/bbgm/pro/tests/test_include.cxx
@@ -0,0 +1,8 @@
+#include <bbgm/pro/bbgm_display_dist_image_process.h>
+#include <bbgm/pro/bbgm_load_image_of_process.h>
+#include <bbgm/pro/bbgm_measure_process.h>
+#include <bbgm/pro/bbgm_save_image_of_process.h>
+#include <bbgm/pro/bbgm_update_dist_image_process.h>
+#include <bbgm/pro/bbgm_update_dist_image_stream_process.h>
+
+int main() { return 0; }
