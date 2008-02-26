b9acee05dc31b089c7753aa11c901dff5467f19b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-02-26 02:26:52 -0500

    avoid compiler error by replacing inlined function "swap" by a macro

--- contrib/brl/bbas/bgui/bgui_roi_tableau_make_roi.h
+++ contrib/brl/bbas/bgui/bgui_roi_tableau_make_roi.h
@@ -26,7 +26,7 @@ class bgui_roi_tableau_make_roi : public vgui_rubberband_client
 
   void add_box(float x0,float y0,float x1,float y1)
   {
-    inline void swap(float& a, float& b) { float t=a; a=b; b=t; }
+#define swap(a, b) { float t=a; a=b; b=t; }
     if (x0<x1) swap(x0,x1);
     if (y0<y1) swap(y0,y1);
     image_tableau_->set_roi(x1,y1,x0-x1,y0-y1);
