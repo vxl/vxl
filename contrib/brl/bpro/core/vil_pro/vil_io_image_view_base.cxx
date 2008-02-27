fde5ebafe4f211c1aa658a4a80fe9dba0d697bd5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-02-27 17:31:12 -0500

    must have "\file" for doxygen manpages to be generated

--- contrib/brl/bpro/core/vil_pro/vil_io_image_view_base.cxx
+++ contrib/brl/bpro/core/vil_pro/vil_io_image_view_base.cxx
@@ -1,5 +1,6 @@
 #include "vil_io_image_view_base.h"
-
+//:
+// \file
 #include <vcl_iostream.h>
 #include <vsl/vsl_binary_io.h>
 #include <vil/vil_image_view_base.h>
@@ -8,25 +9,23 @@
 //: Binary write image view base to stream
 void vsl_b_write(vsl_b_ostream & os, vil_image_view_base_sptr const& view_base)
 {
-  vcl_cerr << "warning: vsl_b_write not implemented for vil_image_view_base_sptr" << vcl_endl;
+  vcl_cerr << "warning: vsl_b_write not implemented for vil_image_view_base_sptr\n";
 }
-
 
 //: Binary load image view base from stream.
 void vsl_b_read(vsl_b_istream & is, vil_image_view_base_sptr &view_base)
 {
-  vcl_cerr << "warning: vsl_b_read not implemented for vil_image_view_base_sptr" << vcl_endl;
-
+  vcl_cerr << "warning: vsl_b_read not implemented for vil_image_view_base_sptr\n";
 }
+
 //: Binary write voxel world to stream
 void vsl_b_write(vsl_b_ostream & os, vil_image_view_base const& view_base)
 {
-  vcl_cerr << "warning: vsl_b_write not implemented for vil_image_view_base" << vcl_endl;
+  vcl_cerr << "warning: vsl_b_write not implemented for vil_image_view_base\n";
 }
-
 
 //: Binary load voxel world from stream.
 void vsl_b_read(vsl_b_istream & is, vil_image_view_base &view_base)
 {
-  vcl_cerr << "warning: vsl_b_read not implemented for vil_image_view_base" << vcl_endl;
+  vcl_cerr << "warning: vsl_b_read not implemented for vil_image_view_base\n";
 }
