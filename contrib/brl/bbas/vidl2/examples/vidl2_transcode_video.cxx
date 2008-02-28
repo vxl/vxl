37f3f57ffb415063d65c07d7b153920f752eaada
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-02-28 17:03:24 -0500

    removing unused #include <vcl_cstdlib.h>

--- contrib/brl/bbas/vidl2/examples/vidl2_transcode_video.cxx
+++ contrib/brl/bbas/vidl2/examples/vidl2_transcode_video.cxx
@@ -6,28 +6,26 @@
 #include <vidl2/vidl2_config.h>
 
 #include <vcl_iostream.h>
-#include <vcl_cstdlib.h>
 #include <vul/vul_arg.h>
 #include <vidl2/vidl2_ffmpeg_istream.h>
 #include <vidl2/vidl2_ffmpeg_ostream.h>
 
-int
-main( int argc, char** argv )
+int main( int argc, char** argv )
 {
   vul_arg<vcl_string> input_filename( "-i", "Input filename" );
   vul_arg<vcl_string> output_filename( "-o", "Output filename" );
   vul_arg_parse( argc, argv );
 
   vidl2_ffmpeg_istream istr;
-  if( ! istr.open( input_filename() ) )
+  if ( ! istr.open( input_filename() ) )
   {
-    vcl_cerr << "Couldn't open " << input_filename() << "\n";
+    vcl_cerr << "Couldn't open " << input_filename() << '\n';
     return EXIT_FAILURE;
   }
 
   vidl2_ffmpeg_ostream ostr;
   ostr.set_filename( output_filename() );
-  if( ! ostr.open() )
+  if ( ! ostr.open() )
   {
     vcl_cerr << "Couldn't open " << input_filename() << " for writing\n";
     return EXIT_FAILURE;
@@ -36,15 +34,15 @@ main( int argc, char** argv )
   unsigned count = 0;
   do
   {
-    if( ! ostr.write_frame( istr.current_frame() ) )
+    if ( ! ostr.write_frame( istr.current_frame() ) )
     {
       break;
     }
     ++count;
     vcl_cout << "Processed frame " << istr.frame_number()
-             << " (count=" << count << "\n";
+             << " (count=" << count << vcl_endl;
   }
-  while( istr.advance() );
+  while ( istr.advance() ) ;
 
   return EXIT_SUCCESS;
 }
