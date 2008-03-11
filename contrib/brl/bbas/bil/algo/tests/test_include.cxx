0b3d8020e5a592d2bc144e144d2e41b46a571657
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-03-11 03:30:50 -0400

    added missing header files

--- contrib/brl/bbas/bil/algo/tests/test_include.cxx
+++ contrib/brl/bbas/bil/algo/tests/test_include.cxx
@@ -1,7 +1,15 @@
-#include <bil/algo/bil_scale_image.h>
-#include <bil/algo/bil_wshed2d.h>
+#include <bil/algo/bil_cedt.h>
+#include <bil/algo/bil_color_conversions.h>
+#include <bil/algo/bil_detect_ridges.h>
+#include <bil/algo/bil_edge_indicator.h>
 #include <bil/algo/bil_edt.h>
+#include <bil/algo/bil_equalize.h>
 #include <bil/algo/bil_finite_differences.h>
 #include <bil/algo/bil_finite_second_differences.h>
+#include <bil/algo/bil_harr_wavelet_transform.h>
+#include <bil/algo/bil_perform_tensor_decomposition.h>
+#include <bil/algo/bil_roi_mask.h>
+#include <bil/algo/bil_scale_image.h>
+#include <bil/algo/bil_wshed2d.h>
 
 int main() { return 0; }
