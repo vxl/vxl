e7f4aa037adf86a36bc82d358fcf5745f39a441f
Ian Scott <scottim@users.sourceforge.net>
2008-03-11 20:36:09 -0400

    Add additional tests and example code

--- contrib/mul/mbl/tests/test_log.cxx
+++ contrib/mul/mbl/tests/test_log.cxx
@@ -1,8 +1,69 @@
 // This is mul/mbl/tests/test_log.cxx
 #include <vcl_sstream.h>
 #include <vcl_fstream.h>
+#include <vcl_vector.h>
+#include <vcl_string.h>
+#include <vcl_cstddef.h>
+#include <vsl/vsl_vector_io.h>
+#include <vsl/vsl_string_io.h>
 #include <mbl/mbl_log.h>
 #include <testlib/testlib_test.h>
+
+
+namespace
+{
+  // This class stores log messages in a vector of strings - one log entry per element.
+  // The last element should be empty and unused.
+  class test_streambuf: public vcl_streambuf
+  {
+  public:
+    vcl_vector<vcl_string> messages;
+    test_streambuf(): messages(1) {}
+    virtual int sync ()
+    {
+      vcl_ptrdiff_t n = pptr() - pbase();
+
+      if (n)
+        messages.back().append(pbase(), n);
+      if (!messages.back().empty()) // Ignore flushes on empty messages
+        messages.push_back(vcl_string());
+
+      pbump(-n);  // Reset pptr().
+      return 0;
+    }
+
+    virtual int overflow (int ch)
+    {
+      vcl_ptrdiff_t n = pptr() - pbase();
+
+      if (n)
+        messages.back().append(pbase(), n);
+      pbump(-n);  // Reset pptr().
+
+      if (ch == EOF)
+      return 0;
+
+      char cbuf = ch;
+      messages.back().append(&cbuf, 1);
+      return ch;
+    }
+
+    virtual vcl_streamsize xsputn(const char *ptr, vcl_streamsize nchar)
+    {
+      // Output anything already in buffer
+      long n = pptr() - pbase();
+      if (n)
+        messages.back().append(pbase(), n);
+      pbump(-n);  // Reset pptr().
+
+      messages.back().append(ptr, nchar);
+      return nchar;
+    }
+  };
+}
+
+
+
 
 void test_log()
 {
@@ -15,17 +76,21 @@ void test_log()
   }
 
 
-  vcl_ostringstream output;
+  vcl_ostringstream output;  // Standard test output
 
+  test_streambuf my_test_streambuf; // Special test output
+  vcl_ostream special_output(&my_test_streambuf);
 
   {
     vcl_ofstream cfg_file("mbl_log.properties");
     cfg_file << 
-      "root: { level: INFO stream_output: test }\n" <<
-      "obj3: { level: INFO stream_output: cout }\n";
+      "root: { level: INFO stream_output: test }\n"
+      "obj3: { level: INFO stream_output: cout }\n"
+      "obj4: { level: INFO stream_output: test_streambuf }\n";
   }
   vcl_map<vcl_string, vcl_ostream*> registered_streams;
   registered_streams["test"] = &output;
+  registered_streams["test_streambuf"] = &special_output;
 
   mbl_logger::root().load_log_config_file(registered_streams);
   mbl_logger::root().categories().print(vcl_cout);
@@ -65,6 +130,21 @@ void test_log()
     MBL_LOG(WARN, obj3, "Test");
   else
     MBL_LOG(WARN, obj3, "Test");
+
+
+  // Check that is is possible to use a custom streambuf as the output destination, and
+  // that flushes indicate a terminating log message.
+  mbl_logger obj4("obj4");
+  MBL_LOG(WARN, obj4, "A split line" << vcl_endl << "message");
+  obj4.log(mbl_logger::NOTICE) << "A second message" << vcl_endl;
+
+  vcl_cout << "Special output messages: ";
+  vsl_print_summary(vcl_cout, my_test_streambuf.messages);
+
+  // Test for one extra due to class invariant.
+  TEST ("Special output got 2 messages", my_test_streambuf.messages.size(), 3);
+  TEST ("Message 1 correct", my_test_streambuf.messages[0], "WARN: obj4 A split line\nmessage\n");
+  TEST ("Message 2 correct", my_test_streambuf.messages[1], "NOTICE: obj4 A second message\n");
 
 
   vcl_cout << "\n\n";
