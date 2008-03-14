4da36b6b78a7359c71ee4fe2f25cd6577ffcedbf
Gamze Tunali <gamze@users.sourceforge.net>
2008-03-13 23:44:13 -0400

    template inst. for bvxm/pro

---
+++ contrib/brl/bseg/bvxm/pro/tests/Templates/brdb_value_t+bvxm_voxel_world_sptr.cxx
@@ -0,0 +1,8 @@
+#include <brdb/brdb_value.txx>
+
+#include <vbl/io/vbl_io_smart_ptr.h>
+#include <bvxm/bvxm_voxel_world.h>
+#include <bvxm/io/bvxm_io_voxel_world.h>
+
+
+BRDB_VALUE_INSTANTIATE(bvxm_voxel_world_sptr, "bvxm_voxel_world_sptr");
---
+++ contrib/brl/bseg/bvxm/pro/tests/Templates/brdb_value_t+unsigned-.cxx
@@ -0,0 +1,6 @@
+#include <brdb/brdb_value.txx>
+
+#include <vbl/io/vbl_io_smart_ptr.h>
+
+
+BRDB_VALUE_INSTANTIATE(unsigned, "unsigned");
