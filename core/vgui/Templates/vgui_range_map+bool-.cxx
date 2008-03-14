686b567fb2286d4222cdaff76815e897a9611ad3
Ian Scott <scottim@users.sourceforge.net>
2008-03-14 07:14:21 -0400

    Suppress useless performance warnings

--- core/vgui/Templates/vgui_range_map+bool-.cxx
+++ core/vgui/Templates/vgui_range_map+bool-.cxx
@@ -1,2 +1,11 @@
+#include <vcl_compiler.h>
+
+#ifdef VCL_VC
+// They are performance warnings, but you can't really do anything faster.
+# pragma warning(disable : 4800)
+#endif
+
 #include <vgui/vgui_range_map.txx>
+
+
 VGUI_RANGE_MAP_INSTANTIATE(bool);
