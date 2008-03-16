6888985698ea0f85b4e2e4f89b4a00b2d9bf86fb
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-03-16 04:44:07 -0400

    added new header files

--- contrib/mul/mfpf/tests/test_include.cxx
+++ contrib/mul/mfpf/tests/test_include.cxx
@@ -1,4 +1,5 @@
 #include <mfpf/mfpf_add_all_loaders.h>
+#include <mfpf/mfpf_draw_matches.h>
 #include <mfpf/mfpf_draw_pose_cross.h>
 #include <mfpf/mfpf_draw_pose_lines.h>
 #include <mfpf/mfpf_edge_finder_builder.h>
@@ -6,6 +7,7 @@
 #include <mfpf/mfpf_point_finder_builder.h>
 #include <mfpf/mfpf_point_finder.h>
 #include <mfpf/mfpf_pose.h>
+#include <mfpf/mfpf_pose_set.h>
 #include <mfpf/mfpf_norm_corr1d.h>
 #include <mfpf/mfpf_norm_corr1d_builder.h>
 #include <mfpf/mfpf_norm_corr2d.h>
@@ -13,6 +15,7 @@
 #include <mfpf/mfpf_norm_vec.h>
 #include <mfpf/mfpf_profile_pdf.h>
 #include <mfpf/mfpf_profile_pdf_builder.h>
+#include <mfpf/mfpf_region_form.h>
 #include <mfpf/mfpf_region_pdf.h>
 #include <mfpf/mfpf_region_pdf_builder.h>
 #include <mfpf/mfpf_sample_region.h>
--- contrib/mul/vimt/tests/test_include.cxx
+++ contrib/mul/vimt/tests/test_include.cxx
@@ -7,6 +7,7 @@
 #include <vimt/algo/vimt_pyramid_values_at_point.h>
 #include <vimt/vimt_add_all_binary_loaders.h>
 #include <vimt/vimt_bilin_interp.h>
+#include <vimt/vimt_convert.h>
 #include <vimt/vimt_crop.h>
 #include <vimt/vimt_dog_pyramid_builder_2d.h>
 #include <vimt/vimt_gaussian_pyramid_builder_2d_general.h>
