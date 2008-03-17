961445cb82658a23622c3f0ab4ddbf0455f630f2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-03-17 18:21:20 -0400

    avoid compiler warning (unused class "bvgl_articulated_poly")

--- contrib/brl/bbas/bvgl/tests/test_articulated_poly.cxx
+++ contrib/brl/bbas/bvgl/tests/test_articulated_poly.cxx
@@ -1,6 +1,5 @@
 //:
 // \file
-// not used? #include <vcl_vector.h>
 #include <testlib/testlib_test.h>
 #include <bvgl/bvgl_articulated_poly.h>
 #include <vcl_iostream.h>
@@ -8,8 +7,9 @@
 //: Test the articulated poly
 static void test_articulated_poly()
 {
+  bvgl_articulated_poly a;
   vcl_cout << "Not implemented yet\n";
-  TEST("Test articulated poly ", 1==1, true);
+  TEST("Test articulated poly ", 1, 1);
 }
 
 TESTMAIN( test_articulated_poly );
