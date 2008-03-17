2e627b490d793252da9049b5a7a32fc27b879b42
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-03-17 18:22:25 -0400

    Doxygen structured comments are only useful for functions, methods, and members

--- contrib/brl/bseg/bvxm/pro/bvxm_detect_changes_process.cxx
+++ contrib/brl/bseg/bvxm/pro/bvxm_detect_changes_process.cxx
@@ -31,45 +31,40 @@ bvxm_detect_changes_process::bvxm_detect_changes_process()
   input_types_[3] = "vcl_string";
   input_types_[4] = "unsigned";
 
-
   //process has 1 output
   //output[0]: The change detection image
   output_data_.resize(1,brdb_value_sptr(0));
   output_types_.resize(1);
   output_types_[0]= "vil_image_view_base_sptr";
-
-
 }
 
 
 bool bvxm_detect_changes_process::execute()
 {
-
   // Sanity check
-  if(!this->verify_inputs())
+  if (!this->verify_inputs())
     return false;
 
   //get the inputs
-  brdb_value_t<vil_image_view_base_sptr>* input0 = 
+  brdb_value_t<vil_image_view_base_sptr>* input0 =
     static_cast<brdb_value_t<vil_image_view_base_sptr>* >(input_data_[0].ptr());
   vil_image_view_base_sptr img = input0->value();
 
-  brdb_value_t<vpgl_camera_double_sptr>* input1 = 
+  brdb_value_t<vpgl_camera_double_sptr>* input1 =
     static_cast<brdb_value_t<vpgl_camera_double_sptr>* >(input_data_[1].ptr());
   vpgl_camera_double_sptr camera = input1->value();
 
-  brdb_value_t<bvxm_voxel_world_sptr>* input2 = 
+  brdb_value_t<bvxm_voxel_world_sptr>* input2 =
     static_cast<brdb_value_t<bvxm_voxel_world_sptr>* >(input_data_[2].ptr());
   bvxm_voxel_world_sptr world = input2->value();
 
-  brdb_value_t<vcl_string>* input3 = 
+  brdb_value_t<vcl_string>* input3 =
     static_cast<brdb_value_t<vcl_string>* >(input_data_[3].ptr());
   vcl_string voxel_type = input3->value();
 
-  brdb_value_t<unsigned>* input4 = 
+  brdb_value_t<unsigned>* input4 =
     static_cast<brdb_value_t<unsigned>* >(input_data_[4].ptr());
   unsigned bin_index = input4->value();
-
 
   //create metadata:
   bvxm_image_metadata observation(img,camera);
@@ -82,10 +77,10 @@ bool bvxm_detect_changes_process::execute()
   else
     world->pixel_probability_density<APM_MOG_GREY>(observation,prob_map,bin_index);
 
-
   // TODO: filtering / thresholding if necessary (Thom?)
-  //: the following thresholding is added temporarily in order to produce an image that can be saved
-  // threshold the image to make sure that it is binary
+
+  // the following thresholding is added temporarily in order to produce an
+  // image that can be saved threshold the image to make sure that it is binary
   float threshold_value = 0.5;
   float min_value = 0, max_value = 0;
   vil_math_value_range(prob_map, min_value, max_value);
@@ -94,13 +89,14 @@ bool bvxm_detect_changes_process::execute()
   vil_threshold_above<float>(prob_map, binary_img, threshold_value);
 
   //store output
-  brdb_value_sptr output0 = 
-    //new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<float>(prob_map));
+  brdb_value_sptr output0 =
+#if 0
+    new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<float>(prob_map));
+#else
     new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<bool>(binary_img));
+#endif
   output_data_[0] = output0;
 
   return true;
 }
-
-
 
--- contrib/brl/bseg/bvxm/pro/bvxm_normalize_image_process.cxx
+++ contrib/brl/bseg/bvxm/pro/bvxm_normalize_image_process.cxx
@@ -5,7 +5,6 @@
 
 #include <vil/vil_image_view_base.h>
 #include <vil/vil_convert.h>
-#include <vil/vil_new.h>
 #include <brip/brip_vil_float_ops.h>
 #include <vpgl/vpgl_camera.h>
 
@@ -31,116 +30,112 @@ bvxm_normalize_image_process::bvxm_normalize_image_process()
   output_data_.resize(3,brdb_value_sptr(0));
   output_types_.resize(3);
   output_types_[0]= "vil_image_view_base_sptr";
-  output_types_[1]= "float";  // output a 
+  output_types_[1]= "float";  // output a
   output_types_[2]= "float";  // output b
 
   parameters()->add("start value for a", "a_start", 0.6f);
   parameters()->add("increment for a", "a_inc", 0.05f);
   parameters()->add("end value for a", "a_end", 1.8f);
-  //: the following defaults are carried from Thom's code
-  //  however here we always work with images scaled to [0,1] interval
-  //  Thom had assumed byte images, so the defaults are divided by 255 for b
+  // The following defaults are carried from Thom's code.
+  // However here we always work with images scaled to [0,1] interval.
+  // Thom had assumed byte images, so the defaults are divided by 255 for b.
   parameters()->add("start value for intervals of b", "b_start", (100.0f/255.0f));  // start search in [-200, 200] with 100 increments for a byte image
   parameters()->add("end value for intervals of b", "b_end", (5.0f/255.0f));        // end search in [-10, 10] with 5 increments
   parameters()->add("ratio that determines increment value for intervals of b", "b_ratio", 0.5f);           // half the intervals, e.g. if start with 100, then 50, 25, 12, etc.
   parameters()->add("verbose", "verbose", false);
-
 }
 
 
 bool bvxm_normalize_image_process::execute()
 {
-
   // Sanity check
-  if(!this->verify_inputs())
+  if (!this->verify_inputs())
     return false;
 
   //get the inputs
-  brdb_value_t<vil_image_view_base_sptr>* input0 = 
+  brdb_value_t<vil_image_view_base_sptr>* input0 =
     static_cast<brdb_value_t<vil_image_view_base_sptr>* >(input_data_[0].ptr());
 
   vil_image_view_base_sptr input_img = input0->value();
 
-  brdb_value_t<vpgl_camera_double_sptr>* input1 = 
+  brdb_value_t<vpgl_camera_double_sptr>* input1 =
     static_cast<brdb_value_t<vpgl_camera_double_sptr>* >(input_data_[1].ptr());
 
   vpgl_camera_double_sptr camera = input1->value();
 
-  brdb_value_t<bvxm_voxel_world_sptr>* input2 = 
+  brdb_value_t<bvxm_voxel_world_sptr>* input2 =
     static_cast<brdb_value_t<bvxm_voxel_world_sptr>* >(input_data_[2].ptr());
 
   bvxm_voxel_world_sptr world = input2->value();
 
-   brdb_value_t<vcl_string>* input3 = 
+   brdb_value_t<vcl_string>* input3 =
     static_cast<brdb_value_t<vcl_string>* >(input_data_[3].ptr());
 
   vcl_string voxel_type = input3->value();
 
-  brdb_value_t<unsigned>* input4 = 
+  brdb_value_t<unsigned>* input4 =
     static_cast<brdb_value_t<unsigned>* >(input_data_[4].ptr());
   unsigned bin_index = input4->value();
 
-  //: if the world is not updated yet, we just return the input image
+  // if the world is not updated yet, we just return the input image
   if ((voxel_type == "apm_mog_rgb") ){
-      if(!world->num_observations<APM_MOG_RGB>(bin_index) ) {
-    
-          
-    // return the input img
-    brdb_value_sptr output0 = new brdb_value_t<vil_image_view_base_sptr>(input_img);
-    output_data_[0] = output0;
+    if (!world->num_observations<APM_MOG_RGB>(bin_index) ) {
+      // return the input img
+      brdb_value_sptr output0 = new brdb_value_t<vil_image_view_base_sptr>(input_img);
+      output_data_[0] = output0;
 
-    brdb_value_sptr output1 = new brdb_value_t<float>(1.0f); // a
-    brdb_value_sptr output2 = new brdb_value_t<float>(0.0f); // b
-    output_data_[1] = output1;
-    output_data_[2] = output2;
+      brdb_value_sptr output1 = new brdb_value_t<float>(1.0f); // a
+      brdb_value_sptr output2 = new brdb_value_t<float>(0.0f); // b
+      output_data_[1] = output1;
+      output_data_[2] = output2;
 
-    return true;
-      }
+      return true;
+    }
   }
-  if((voxel_type == "apm_mog_grey") )
+  if ((voxel_type == "apm_mog_grey") )
   {
-      if(!world->num_observations<APM_MOG_GREY>(bin_index))
-      {
-          // return the input img
-          brdb_value_sptr output0 = new brdb_value_t<vil_image_view_base_sptr>(input_img);
-          output_data_[0] = output0;
+    if (!world->num_observations<APM_MOG_GREY>(bin_index))
+    {
+      // return the input img
+      brdb_value_sptr output0 = new brdb_value_t<vil_image_view_base_sptr>(input_img);
+      output_data_[0] = output0;
 
-          brdb_value_sptr output1 = new brdb_value_t<float>(1.0f); // a
-          brdb_value_sptr output2 = new brdb_value_t<float>(0.0f); // b
-          output_data_[1] = output1;
-          output_data_[2] = output2;
+      brdb_value_sptr output1 = new brdb_value_t<float>(1.0f); // a
+      brdb_value_sptr output2 = new brdb_value_t<float>(0.0f); // b
+      output_data_[1] = output1;
+      output_data_[2] = output2;
 
-          return true;
-      }
+      return true;
+    }
   }
-  //: create metadata:
+  // create metadata:
   bvxm_image_metadata observation(input_img,camera);
 
-  //: get parameters
+  // get parameters
   bool verbose = false;
   parameters()->get_value("verbose", verbose);
   float a_start, a_end, a_inc;
   parameters()->get_value("a_start", a_start);
   parameters()->get_value("a_inc", a_inc);
   parameters()->get_value("a_end", a_end);
-  
+
   float b_start, b_end, b_ratio;
   parameters()->get_value("b_start", b_start);
   parameters()->get_value("b_ratio", b_ratio);
   parameters()->get_value("b_end", b_end);
-  
+
   if (verbose) {
-    vcl_cout << "normalization parameters to be used in this run:\n";
-    vcl_cout << "a_start: " << a_start << " a_end: " << a_end << " a_inc: " << a_inc << vcl_endl;
-    vcl_cout << "b_start: " << b_start << " b_end: " << b_end << " b_ratio: " << b_ratio << vcl_endl;
+    vcl_cout << "normalization parameters to be used in this run:\n"
+             << "a_start: " << a_start << " a_end: " << a_end << " a_inc: " << a_inc << vcl_endl
+             << "b_start: " << b_start << " b_end: " << b_end << " b_ratio: " << b_ratio << vcl_endl;
   }
-  
-  //: CAUTION: Assumption: Input image is of type vxl_byte
+
+  // CAUTION: Assumption: Input image is of type vxl_byte
   if (input_img->pixel_format() != VIL_PIXEL_FORMAT_BYTE) {
     vcl_cout << "Input image pixel format is not VIL_PIXEL_FORMAT_BYTE!\n";
     return false;
   }
-  
+
   //vil_image_view_base_sptr input_image_sptr = new vil_image_view<vxl_byte>(input_img);
   //vil_image_view<float> input_img_float = vil_convert_cast( float(), input_image_sptr );
   unsigned ni = input_img->ni();
@@ -150,12 +145,11 @@ bool bvxm_normalize_image_process::execute()
   vil_image_view<float>* input_img_float_stretched = new vil_image_view<float>( ni, nj, nplanes );
   vil_image_view<vxl_byte>* input_img_ptr = new vil_image_view<vxl_byte>(input_img);
   vil_convert_stretch_range_limited<vxl_byte>(*input_img_ptr, *input_img_float_stretched, 0, 255, 0.0f, 1.0f);
-          
 
-  //: use the weight slab below to calculate total probability
+  // use the weight slab below to calculate total probability
   bvxm_voxel_slab<float> weights(ni, nj, 1);
   weights.fill(1.0f/(ni * nj));
-  
+
   //1) get probability mixtures of all pixels in image
   bvxm_voxel_slab_base_sptr mog_image;
 
@@ -165,11 +159,11 @@ bool bvxm_normalize_image_process::execute()
   if (voxel_type == "apm_mog_rgb") {
     typedef bvxm_voxel_traits<APM_MOG_RGB>::voxel_datatype mog_type;
     typedef bvxm_voxel_traits<APM_MOG_RGB>::obs_datatype obs_datatype;
-    
+
     world->mixture_of_gaussians_image<APM_MOG_RGB>(observation, mog_image, bin_index);
     bvxm_voxel_slab<mog_type>* mog_image_ptr = dynamic_cast<bvxm_voxel_slab<mog_type>*>(mog_image.ptr());
-    
-    bvxm_voxel_traits<APM_MOG_RGB>::appearance_processor apm_processor;  
+
+    bvxm_voxel_traits<APM_MOG_RGB>::appearance_processor apm_processor;
     if (verbose) {
       bvxm_voxel_slab<obs_datatype> exp_img = apm_processor.expected_color(*mog_image_ptr);
       vil_image_view_base_sptr temp_img = new vil_image_view<vxl_byte>(ni, nj, 3);
@@ -178,12 +172,12 @@ bool bvxm_normalize_image_process::execute()
     }
 
     //2) optimize two parameters for the input image so that it is the maximally probable image seen wrt mog_image
-    for( float sa = a_start; sa <= a_end; sa+=a_inc ){
+    for ( float sa = a_start; sa <= a_end; sa+=a_inc ){
       float sb_best = 0.0f;
       bool tried_zero = false;
-      for( float sb_inc = b_start; sb_inc > b_end; sb_inc *= b_ratio ) {
+      for ( float sb_inc = b_start; sb_inc > b_end; sb_inc *= b_ratio ) {
         float sb_best_prob = 0.0;
-        for( float sb = sb_best-2*sb_inc; sb <= sb_best+2.01*sb_inc; sb+= sb_inc ) {
+        for ( float sb = sb_best-2*sb_inc; sb <= sb_best+2.01*sb_inc; sb+= sb_inc ) {
           if (sb == 0.0f) {
             if (tried_zero)
               continue;
@@ -199,107 +193,106 @@ bool bvxm_normalize_image_process::execute()
           //vil_image_view<float>* nimg_stretched_ptr = new vil_image_view<float>( ni, nj, nplanes );
           //vil_image_view_base_sptr nimg_stretched_sptr = nimg_stretched_ptr;
           //vil_convert_stretch_range_limited(nimg, *nimg_stretched_ptr, 0.0f, 255.0f, 0.0f, 1.0f);
-          
+
           // convert image to a voxel_slab
           bvxm_voxel_slab<obs_datatype> image_slab(ni, nj, 1);
           //bvxm_util::img_to_slab(nimg_stretched_ptr,image_slab);
           //nimg_stretched_sptr = 0;  // to clear up the space taken by new
           bvxm_util::img_to_slab(nimg_sptr,image_slab);
           nimg_sptr = 0;  // to clear up space
-          
-          bvxm_voxel_slab<float> prob = apm_processor.prob_density(*mog_image_ptr,image_slab); //prob( nimg );  
+
+          bvxm_voxel_slab<float> prob = apm_processor.prob_density(*mog_image_ptr,image_slab); //prob( nimg );
           // find the total prob
           bvxm_voxel_slab<float> product(ni, nj, 1);
           bvxm_util::multiply_slabs(prob, weights, product);
           float this_prob = bvxm_util::sum_slab(product);
 
           //vcl_cerr << this_prob << ' ';
-          if( this_prob < 0 ) {
+          if ( this_prob < 0 ) {
             vcl_cout << "In bvxm_normalize_image_process::execute() -- prob is negative, Exiting!\n";
             return false;
           }
 
-          if( this_prob > sb_best_prob ){ sb_best_prob = this_prob; sb_best = sb; }
-          if( this_prob > best_prob ){ best_prob = this_prob; a = sa; b = sb; }
+          if ( this_prob > sb_best_prob ){ sb_best_prob = this_prob; sb_best = sb; }
+          if ( this_prob > best_prob ){ best_prob = this_prob; a = sa; b = sb; }
         }
         //vcl_cerr << '\n';
       }
       //vcl_cerr << '\n';
     }
-    
-  } else if (voxel_type == "apm_mog_grey") {  
+  }
+  else if (voxel_type == "apm_mog_grey") {
     typedef bvxm_voxel_traits<APM_MOG_GREY>::voxel_datatype mog_type;
     typedef bvxm_voxel_traits<APM_MOG_GREY>::obs_datatype obs_datatype;
-    
+
     world->mixture_of_gaussians_image<APM_MOG_GREY>(observation, mog_image, bin_index);
     bvxm_voxel_slab<mog_type>* mog_image_ptr = dynamic_cast<bvxm_voxel_slab<mog_type>*>(mog_image.ptr());
 
-    bvxm_voxel_traits<APM_MOG_GREY>::appearance_processor apm_processor;  
+    bvxm_voxel_traits<APM_MOG_GREY>::appearance_processor apm_processor;
     if (verbose) {
       bvxm_voxel_slab<float> exp_img = apm_processor.expected_color(*mog_image_ptr);
       vil_image_view_base_sptr temp_img = new vil_image_view<vxl_byte>(ni, nj, 1);
       bvxm_util::slab_to_img(exp_img, temp_img);
       vil_save(*temp_img, "./mixture_expected_img.png");
     }
-    
+
     //2) optimize two parameters for the input image so that it is the maximally probable image seen wrt mog_image
-    for( float sa = a_start; sa <= a_end; sa+=a_inc ){
+    for ( float sa = a_start; sa <= a_end; sa+=a_inc ){
       float sb_best = 0.0f;
       bool tried_zero = false;
-      for( float sb_inc = b_start; sb_inc > b_end; sb_inc *= b_ratio ) {
+      for ( float sb_inc = b_start; sb_inc > b_end; sb_inc *= b_ratio ) {
         float sb_best_prob = 0.0;
-        for( float sb = sb_best-2*sb_inc; sb <= sb_best+2.01*sb_inc; sb+= sb_inc ) {
+        for ( float sb = sb_best-2*sb_inc; sb <= sb_best+2.01*sb_inc; sb+= sb_inc ) {
           if (sb == 0.0f) {
             if (tried_zero)
               continue;
-            else 
+            else
               tried_zero = true;
           }
           //vil_image_view<float> nimg( ni, nj, nplanes );
           //normalize_image<float>(input_img_float, nimg, sa, sb, 255.0);
-          vil_image_view<float>* nimg = new vil_image_view<float>(ni, nj, nplanes); 
+          vil_image_view<float>* nimg = new vil_image_view<float>(ni, nj, nplanes);
           normalize_image<float>(*input_img_float_stretched, *nimg, sa, sb, 1.0f);
           vil_image_view_base_sptr nimg_sptr = nimg;
 
           //vil_image_view<float>* nimg_stretched_ptr = new vil_image_view<float>( ni, nj, nplanes );
           //vil_image_view_base_sptr nimg_stretched_sptr = nimg_stretched_ptr;
           //vil_convert_stretch_range_limited(nimg, *nimg_stretched_ptr, 0.0f, 255.0f, 0.0f, 1.0f);
-          
+
           // convert image to a voxel_slab
           bvxm_voxel_slab<obs_datatype> image_slab(ni, nj, 1);
           //bvxm_util::img_to_slab(nimg_stretched_ptr,image_slab);
           //nimg_stretched_sptr = 0;  // to clear up the space taken by new
           bvxm_util::img_to_slab(nimg_sptr,image_slab);
           nimg_sptr = 0;  // to clear up space
-          
-          bvxm_voxel_slab<float> prob = apm_processor.prob_density(*mog_image_ptr,image_slab); //prob( nimg );  
+
+          bvxm_voxel_slab<float> prob = apm_processor.prob_density(*mog_image_ptr,image_slab); //prob( nimg );
           // find the total prob
           bvxm_voxel_slab<float> product(ni, nj, 1);
           bvxm_util::multiply_slabs(prob, weights, product);
           float this_prob = bvxm_util::sum_slab(product);
 
           //vcl_cerr << this_prob << ' ';
-          if( this_prob < 0 ) {
+          if ( this_prob < 0 ) {
             vcl_cout << "In bvxm_normalize_image_process::execute() -- prob is negative, Exiting!\n";
             return false;
           }
 
-          if( this_prob > sb_best_prob ){ sb_best_prob = this_prob; sb_best = sb; }
-          if( this_prob > best_prob ){ best_prob = this_prob; a = sa; b = sb; }
+          if ( this_prob > sb_best_prob ){ sb_best_prob = this_prob; sb_best = sb; }
+          if ( this_prob > best_prob ){ best_prob = this_prob; a = sa; b = sb; }
         }
         //vcl_cerr << '\n';
       }
       //vcl_cerr << '\n';
     }
-
-  } else {
+  }
+  else {
     vcl_cout << "In bvxm_normalize_image_process::execute() -- input appearance model: " << voxel_type << " is not supported\n";
     return false;
   }
 
-  
   if (verbose) {
-    vcl_cout << "\noptimum a: " << a << "  b: " << b*255.0f << " best prob: " << best_prob << "\n";
+    vcl_cout << "\noptimum a: " << a << "  b: " << b*255.0f << " best prob: " << best_prob << '\n';
   }
 
   // Normalize the image with the best a and b.
@@ -313,18 +306,17 @@ bool bvxm_normalize_image_process::execute()
   vil_convert_stretch_range_limited<float>(output_img_float, output_img_stretched, 0.0f, 1.0f, 0.0f, 255.0f);
   vil_image_view<vxl_byte> output_img;
   vil_convert_cast(output_img_stretched, output_img);
-  
+
   // return the output img
-  brdb_value_sptr output0 = 
+  brdb_value_sptr output0 =
     new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<vxl_byte>(output_img));
   output_data_[0] = output0;
 
   brdb_value_sptr output1 = new brdb_value_t<float>(a);
-  brdb_value_sptr output2 = new brdb_value_t<float>(b*255.0f); // switch back to byte 
+  brdb_value_sptr output2 = new brdb_value_t<float>(b*255.0f); // switch back to byte
   output_data_[1] = output1;
   output_data_[2] = output2;
 
   return true;
 }
-
 
