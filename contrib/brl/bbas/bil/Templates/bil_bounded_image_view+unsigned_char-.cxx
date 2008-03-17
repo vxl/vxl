4ca3316fbaea8ddb3435e9baca1e82e328d54ca2
Matthew J. Leotta <mleotta@users.sourceforge.net>
2008-03-17 09:27:54 -0400

    added missing ;

--- contrib/brl/bbas/bil/Templates/bil_bounded_image_view+unsigned_char-.cxx
+++ contrib/brl/bbas/bil/Templates/bil_bounded_image_view+unsigned_char-.cxx
@@ -1,3 +1,3 @@
 // Instantiation of bil_bounded_image_view<unsigned char>
 #include <bil/bil_bounded_image_view.txx>
-BIL_BOUNDED_IMAGE_VIEW_INSTANTIATE(unsigned char)
+BIL_BOUNDED_IMAGE_VIEW_INSTANTIATE(unsigned char);
--- contrib/brl/bbas/bil/Templates/bil_bounded_image_view+unsigned_short-.cxx
+++ contrib/brl/bbas/bil/Templates/bil_bounded_image_view+unsigned_short-.cxx
@@ -1,3 +1,3 @@
 // Instantiation of bil_bounded_image_view<unsigned short>
 #include <bil/bil_bounded_image_view.txx>
-BIL_BOUNDED_IMAGE_VIEW_INSTANTIATE(unsigned short)
+BIL_BOUNDED_IMAGE_VIEW_INSTANTIATE(unsigned short);
