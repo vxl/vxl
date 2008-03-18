5e6c03408e304a08336941a36eae7e938b7d39d3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-03-18 18:29:26 -0400

    added an example contributed by Gaston Araguas; uses vgui

---
+++ core/vgl/examples/calculate_homography.cxx
@@ -0,0 +1,104 @@
+// \file
+//
+// \version 1.0
+// \author  Gastón Araguás UTN Cordoba, Argentina
+// \date    03/03/2008
+
+#include <vgui/vgui.h>
+#include <vgui/vgui_easy2D_tableau.h>
+#include <vgui/vgui_viewer2D_tableau.h>
+#include <vgui/vgui_shell_tableau.h>
+#include <vgl/vgl_homg_point_2d.h>
+#include <vgl/algo/vgl_h_matrix_2d.h>
+#include <vgl/algo/vgl_h_matrix_2d_compute_linear.h>
+#include <vnl/vnl_math.h>
+
+int main(int argc, char **argv)
+{
+  vgui::init(argc, argv);
+  vcl_vector<vgl_homg_point_2d<double> > pointsA;
+  vcl_vector<vgl_homg_point_2d<double> > pointsB;
+
+  // Use an easy2D tableau which allows us to draw 2D objects:
+  vgui_easy2D_tableau_new easy2D("Homography between two point sets");
+  easy2D->set_foreground(0,1,0);
+  easy2D->set_point_radius(5);
+
+  // set A
+  vgl_homg_point_2d<double> const p1(100,200,1);
+  vgl_homg_point_2d<double> const p2(200,200,1);
+  vgl_homg_point_2d<double> const p3(300,300,1);
+  vgl_homg_point_2d<double> const p4(300,100,1);
+  pointsA.push_back(p1);
+  pointsA.push_back(p2);
+  pointsA.push_back(p3);
+  pointsA.push_back(p4);
+  easy2D->add_point(p1.x(),p1.y());
+  easy2D->add_point(p2.x(),p2.y());
+  easy2D->add_point(p3.x(),p3.y());
+  easy2D->add_point(p4.x(),p4.y());
+  
+  // connect all points with lines
+  easy2D->set_foreground(1,0,0); easy2D->set_line_width(1);
+  easy2D->add_line(p1.x(),p1.y(),p2.x(),p2.y());
+  easy2D->add_line(p2.x(),p2.y(),p3.x(),p3.y());
+  easy2D->add_line(p3.x(),p3.y(),p4.x(),p4.y());
+  easy2D->add_line(p4.x(),p4.y(),p1.x(),p1.y());
+
+  // draw a line from (0,0) to the center of gravity of set A
+  vgl_homg_point_2d<double> centre_pointA( centre(pointsA).x()/centre(pointsA).w(),
+                                           centre(pointsA).y()/centre(pointsA).w(),1.0 );
+  easy2D->set_foreground(1,1,0); easy2D->set_point_radius(5);
+  easy2D->add_point(centre_pointA.x(),centre_pointA.y());
+  easy2D->set_foreground(0,0,1); easy2D->set_line_width(3);
+  easy2D->add_line(0,0,centre_pointA.x(),centre_pointA.y());
+
+  // rotation and translation
+  vgl_h_matrix_2d<double> H;
+  H.set_identity();
+  double tx = 100; double ty = 20;
+  H.set_translation(tx,ty);
+  double angle = -vnl_math::pi*20/180;
+  H.set_rotation(angle);
+
+  // apply the rotation and translation to set A, obtain set B
+  easy2D->set_foreground(0,1,0); easy2D->set_point_radius(5);
+  vgl_homg_point_2d<double> tr_p1 = H*p1;
+  vgl_homg_point_2d<double> tr_p2 = H*p2;
+  vgl_homg_point_2d<double> tr_p3 = H*p3;
+  vgl_homg_point_2d<double> tr_p4 = H*p4;
+  pointsB.push_back(tr_p1);
+  pointsB.push_back(tr_p2);
+  pointsB.push_back(tr_p3);
+  pointsB.push_back(tr_p4);
+  easy2D->add_point(tr_p1.x(),tr_p1.y());
+  easy2D->add_point(tr_p2.x(),tr_p2.y());
+  easy2D->add_point(tr_p3.x(),tr_p3.y());
+  easy2D->add_point(tr_p4.x(),tr_p4.y());
+
+  // connect all points with lines
+  easy2D->set_foreground(1,0,0); easy2D->set_line_width(1);
+  easy2D->add_line(tr_p1.x(),tr_p1.y(),tr_p2.x(),tr_p2.y());
+  easy2D->add_line(tr_p2.x(),tr_p2.y(),tr_p3.x(),tr_p3.y());
+  easy2D->add_line(tr_p3.x(),tr_p3.y(),tr_p4.x(),tr_p4.y());
+  easy2D->add_line(tr_p4.x(),tr_p4.y(),tr_p1.x(),tr_p1.y());
+
+  // compute the homography between set A and set B
+  vgl_h_matrix_2d_compute_linear hcl;
+  vgl_h_matrix_2d<double> H_comp = hcl.compute(pointsA,pointsB);
+
+  // translate and rotate the centre of gravity of set A using the computed homgraphy H_comp
+  vgl_homg_point_2d<double> centre_pointB = H_comp*centre_pointA;
+  centre_pointB.set(centre_pointB.x()/centre_pointB.w(),centre_pointB.y()/centre_pointB.w(),1.0);
+
+  // draw a line from (0,0) to transformed centre of gravity of set A, i.e. centre of gravity of set B
+  easy2D->set_foreground(1,1,0); easy2D->set_point_radius(5);
+  easy2D->add_point(centre_pointB.x()/centre_pointB.w(),centre_pointB.y()/centre_pointB.w());
+  easy2D->set_foreground(0,1,0); easy2D->set_line_width(3);
+  easy2D->add_line(0,0,centre_pointB.x(),centre_pointB.y());
+
+  vgui_viewer2D_tableau_new viewer(easy2D);
+  vgui_shell_tableau_new shell(viewer);
+  return vgui::run(shell, 512, 512);
+}
+
