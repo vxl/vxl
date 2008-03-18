689bcb1898aec4f7f9193f2972c103f3d75bfc7d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-03-18 03:27:18 -0400

    added new header files

--- contrib/mul/mfpf/tests/test_include.cxx
+++ contrib/mul/mfpf/tests/test_include.cxx
@@ -4,6 +4,9 @@
 #include <mfpf/mfpf_draw_pose_lines.h>
 #include <mfpf/mfpf_edge_finder_builder.h>
 #include <mfpf/mfpf_edge_finder.h>
+#include <mfpf/mfpf_mr_point_finder.h>
+#include <mfpf/mfpf_mr_point_finder_builder.h>
+#include <mfpf/mfpf_prune_overlaps.h>
 #include <mfpf/mfpf_point_finder_builder.h>
 #include <mfpf/mfpf_point_finder.h>
 #include <mfpf/mfpf_pose.h>
@@ -20,5 +23,6 @@
 #include <mfpf/mfpf_region_pdf_builder.h>
 #include <mfpf/mfpf_sample_region.h>
 #include <mfpf/mfpf_searcher.h>
+#include <mfpf/mfpf_sort_matches.h>
 
 int main() { return 0; }
