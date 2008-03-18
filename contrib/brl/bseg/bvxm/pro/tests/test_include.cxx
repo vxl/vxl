42fa839990a0da843d9268cabe625115bf076a38
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-03-18 03:22:34 -0400

    *** empty log message ***

---
+++ contrib/brl/bseg/bvxm/pro/tests/test_include.cxx
@@ -0,0 +1,17 @@
+#include <bvxm/pro/bvxm_change_detection_display_process.h>
+#include <bvxm/pro/bvxm_clean_world_process.h>
+#include <bvxm/pro/bvxm_create_local_rpc_process.h>
+#include <bvxm/pro/bvxm_create_normalized_image_process.h>
+#include <bvxm/pro/bvxm_create_voxel_world_process.h>
+#include <bvxm/pro/bvxm_detect_changes_process.h>
+#include <bvxm/pro/bvxm_generate_edge_map_process.h>
+#include <bvxm/pro/bvxm_illum_index_process.h>
+#include <bvxm/pro/bvxm_normalize_image_process.h>
+#include <bvxm/pro/bvxm_render_expected_image_process.h>
+#include <bvxm/pro/bvxm_render_virtual_view_process.h>
+#include <bvxm/pro/bvxm_roi_init_process.h>
+#include <bvxm/pro/bvxm_rpc_registration_process.h>
+#include <bvxm/pro/bvxm_save_occupancy_raw_process.h>
+#include <bvxm/pro/bvxm_update_process.h>
+
+int main() { return 0; }
---
+++ contrib/brl/bseg/bvxm/tests/test_include.cxx
@@ -0,0 +1,18 @@
+#include <bvxm/bvxm_image_metadata.h>
+#include <bvxm/bvxm_memory_chunk.h>
+#include <bvxm/bvxm_mog_grey_processor.h>
+#include <bvxm/bvxm_mog_rgb_processor.h>
+#include <bvxm/bvxm_util.h>
+#include <bvxm/bvxm_voxel_grid.h>
+#include <bvxm/bvxm_voxel_grid_base.h>
+#include <bvxm/bvxm_voxel_slab.h>
+#include <bvxm/bvxm_voxel_slab_base.h>
+#include <bvxm/bvxm_voxel_slab_iterator.h>
+#include <bvxm/bvxm_voxel_storage.h>
+#include <bvxm/bvxm_voxel_storage_disk.h>
+#include <bvxm/bvxm_voxel_storage_mem.h>
+#include <bvxm/bvxm_voxel_traits.h>
+#include <bvxm/bvxm_voxel_world.h>
+#include <bvxm/bvxm_world_params.h>
+
+int main() { return 0; }
