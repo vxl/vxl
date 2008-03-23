2e125e2ef5dcb6dac6dbc74e8727f3cf3d71983a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-03-23 19:07:53 -0400

    no need for ";" after "}"

--- contrib/brl/bseg/bvxm/pro/bvxm_create_local_rpc_process.h
+++ contrib/brl/bseg/bvxm/pro/bvxm_create_local_rpc_process.h
@@ -1,16 +1,15 @@
-// This is contrib/brl/bseg/bvxm/pro/bvxm_create_local_rpc_process.h
+// This is brl/bseg/bvxm/pro/bvxm_create_local_rpc_process.h
 #ifndef bvxm_create_local_rpc_process_h_
 #define bvxm_create_local_rpc_process_h_
-
 //:
 // \file
 // \brief // A process that takes a world model and rational camera and returns a local rational camera
-//           
+//
 // \author Ibrahim Eden
 // \date 03/14/08
 // \verbatim
 //
-// \Modifications 
+// \Modifications
 
 #include <vcl_string.h>
 #include <bprb/bprb_process.h>
@@ -22,22 +21,19 @@ class bvxm_create_local_rpc_process : public bprb_process
 
   //: Copy Constructor (no local data)
   bvxm_create_local_rpc_process(const bvxm_create_local_rpc_process& other)
-    : bprb_process(*static_cast<const bprb_process*>(&other)){};
+  : bprb_process(*static_cast<const bprb_process*>(&other)) {}
 
-  ~bvxm_create_local_rpc_process(){};
+  ~bvxm_create_local_rpc_process() {}
 
   //: Clone the process
-  virtual bvxm_create_local_rpc_process* clone() const 
+  virtual bvxm_create_local_rpc_process* clone() const
   { return new bvxm_create_local_rpc_process(*this); }
 
-  vcl_string name(){return "bvxmCreateLocalRpcProcess";}
+  vcl_string name() { return "bvxmCreateLocalRpcProcess"; }
 
   bool init() { return true; }
   bool execute();
-  bool finish(){return true;}
- 
- private:
-
+  bool finish() { return true; }
 };
 
 #endif // bvxm_create_local_rpc_process_h_
--- contrib/brl/bseg/bvxm/pro/bvxm_create_normalized_image_process.h
+++ contrib/brl/bseg/bvxm/pro/bvxm_create_normalized_image_process.h
@@ -1,46 +1,40 @@
-// This is contrib/brl/bseg/bvxm/pro/bvxm_create_normalized_image_process.h
+// This is brl/bseg/bvxm/pro/bvxm_create_normalized_image_process.h
 #ifndef bvxm_create_normalized_image_process_h_
 #define bvxm_create_normalized_image_process_h_
-
 //:
 // \file
 // \brief // A class to create a contrast normalized image using the input gain and offset values
-//           
+//
 // \author Ozge Can Ozcanli
 // \date 02/17/08
 // \verbatim
 //
-// \Modifications 
+// \Modifications
 
 #include <vcl_string.h>
 #include <bprb/bprb_process.h>
 
-#include <vil/vil_image_view.h>
-#include <vcl_cmath.h>
-
 class bvxm_create_normalized_image_process : public bprb_process
 {
  public:
-  
+
   bvxm_create_normalized_image_process();
 
   //: Copy Constructor (no local data)
-  bvxm_create_normalized_image_process(const bvxm_create_normalized_image_process& other): bprb_process(*static_cast<const bprb_process*>(&other)){};
+  bvxm_create_normalized_image_process(const bvxm_create_normalized_image_process& other)
+  : bprb_process(*static_cast<const bprb_process*>(&other)) {}
 
-  ~bvxm_create_normalized_image_process(){};
+  ~bvxm_create_normalized_image_process() {}
 
   //: Clone the process
-  virtual bvxm_create_normalized_image_process* clone() const {return new bvxm_create_normalized_image_process(*this);}
+  virtual bvxm_create_normalized_image_process* clone() const
+  { return new bvxm_create_normalized_image_process(*this); }
 
-  vcl_string name(){return "bvxmCreateNormalizedImageProcess";}
+  vcl_string name() { return "bvxmCreateNormalizedImageProcess"; }
 
   bool init() { return true; }
   bool execute();
-  bool finish(){return true;}
- 
- private:
-
+  bool finish() { return true; }
 };
 
 #endif // bvxm_create_normalized_image_process_h_
-
