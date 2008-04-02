fac47af499e1255d6323900a6937d5a77b9b54ec
Vishal Jain <jainv@users.sourceforge.net>
2008-04-02 11:04:08 -0400

    renamed bvam to bvxm

--- contrib/brl/bseg/bvxm_batch/change_detection_params.py
+++ contrib/brl/bseg/bvxm_batch/change_detection_params.py
@@ -1,14 +1,14 @@
 import bvxm_batch
-bvam_batch.register_processes();
-bvam_batch.register_datatypes();
+bvxm_batch.register_processes();
+bvxm_batch.register_datatypes();
 # print the defaults for the process given by name
 
-bvam_batch.process_print_default_params("CreateVoxelWorldProcess", "./world_model_params.xml"); 
-bvam_batch.process_print_default_params("BvamRoiInitProcess", "./roi_params.xml"); 
-bvam_batch.process_print_default_params("BvamGenerateEdgeMapProcess", "./edge_map_params.xml"); 
-bvam_batch.process_print_default_params("BvamRpcRegistrationProcess", "./rpc_registration_parameters.xml");
-bvam_batch.process_print_default_params("BvamNormalizeImageProcess", "./normalize.xml"); 
-bvam_batch.process_print_default_params("BvamChangeDetectionDisplayProcess", "./change_display_params.xml"); 
+bvxm_batch.process_print_default_params("CreateVoxelWorldProcess", "./world_model_params.xml"); 
+bvxm_batch.process_print_default_params("BvamRoiInitProcess", "./roi_params.xml"); 
+bvxm_batch.process_print_default_params("BvamGenerateEdgeMapProcess", "./edge_map_params.xml"); 
+bvxm_batch.process_print_default_params("BvamRpcRegistrationProcess", "./rpc_registration_parameters.xml");
+bvxm_batch.process_print_default_params("BvamNormalizeImageProcess", "./normalize.xml"); 
+bvxm_batch.process_print_default_params("BvamChangeDetectionDisplayProcess", "./change_display_params.xml"); 
 
 
 print("Done");
