6eee0768f0e2f9b435104fcd9709d2ffc2a1bd61
Joseph L. Mundy <mundyj@users.sourceforge.net>
2008-04-03 08:57:12 -0400

    remove neighborhood tests temporarily

--- contrib/brl/bseg/bbgm/pro/tests/test_driver.cxx
+++ contrib/brl/bseg/bbgm/pro/tests/test_driver.cxx
@@ -3,14 +3,17 @@
 
 DECLARE( test_io );
 DECLARE( test_update_dist_image );
+#if 0
 DECLARE( test_neighborhood_ops );
-
+#endif
 void
 register_tests()
 {
   REGISTER( test_io );
   REGISTER( test_update_dist_image );
+#if 0
   REGISTER( test_neighborhood_ops );
+#endif
 }
 
 DEFINE_MAIN;
