335fe73e7013a8d3ac48d4143efcb4a7bf5154e7
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2008-04-07 10:16:51 -0400

    *** empty log message ***

---
+++ contrib/brl/bseg/bvxm/bvxm_lidar_processor.cxx
@@ -0,0 +1,35 @@
+#include "bvxm_lidar_processor.h"
+//:
+// \file
+#include "bvxm_voxel_slab.h"
+#include "bvxm_voxel_slab_iterator.h"
+#include <vcl_cassert.h>
+
+//: Return probability density of observing pixel values
+bvxm_voxel_slab<float>
+bvxm_lidar_processor::prob_density(unsigned z_dim,  bvxm_voxel_slab<float> const& obs)
+{
+	   //the output
+		  bvxm_voxel_slab<float> probabilities(obs.nx(), obs.ny(), obs.nz());
+		
+		  //the slab iterators
+		  bvxm_voxel_slab<float>::const_iterator obs_it;
+		  bvxm_voxel_slab<float>::iterator prob_it = probabilities.begin();
+		
+		  for (obs_it = obs.begin(); obs_it!= obs.end(); ++obs_it, ++prob_it)
+		  {
+		    float dummy = *obs_it;
+		    float d = *obs_it - z_dim;
+		    
+        // Calculate threshold such that the given number of pixels correspond to 2.5 standar deviation away 
+        //in a standard normal gaussian
+        float thresh = 2.5 / float(vox_thresh_);
+		    d = d * thresh;  
+		    
+		    (*prob_it) = gauss_.prob_density(d);
+		    
+		 }
+		
+		  return probabilities;
+}
+ 
---
+++ contrib/brl/bseg/bvxm/bvxm_lidar_processor.h
@@ -0,0 +1,40 @@
+// This is brl/bseg/bvxm/bvxm_lidar.h
+#ifndef bvxm_lidar_h_
+#define bvxm_lidar_h_
+//:
+// \file
+// \brief A class for a lidar processor
+//
+// \author Isabel Restrepo
+// \date 04/03/2008
+// \verbatim
+//  Modifications
+//
+// \endverbatim
+
+#include "bvxm_voxel_slab.h"
+
+#include <bsta/algo/bsta_adaptive_updater.h>
+#include <bsta/bsta_distribution.h>
+#include <bsta/bsta_gauss_f1.h>
+#include <bsta/bsta_attributes.h>
+#include <bsta/bsta_mixture_fixed.h>
+#include <bsta/bsta_gaussian_indep.h>
+
+class  bvxm_lidar_processor
+{
+
+public:
+
+  bvxm_lidar_processor(unsigned vox_thresh): gauss_(bsta_gauss_f1(0,1)), vox_thresh_(vox_thresh){};
+
+  //: A function that assigns a gaussian weight to the lidar observation 
+  bvxm_voxel_slab<float>  prob_density(unsigned z_dim,
+    bvxm_voxel_slab<float> const& obs);
+
+private:
+  bsta_gauss_f1 gauss_;	
+  unsigned vox_thresh_;
+};
+
+#endif // bvxm_lidar_h_
