e9a74a4c6403cf2f6c9423a4068647ed188ffb95
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-04-08 03:01:17 -0400

    removed nonexisting header file

--- contrib/brl/bseg/bvxm/pro/tests/test_include.cxx
+++ contrib/brl/bseg/bvxm/pro/tests/test_include.cxx
@@ -17,8 +17,6 @@
 #include <bvxm/pro/bvxm_create_synth_lidar_data_process.h>
 #include <bvxm/pro/bvxm_gen_synthetic_world_process.h>
 #include <bvxm/pro/bvxm_lidar_init_process.h>
-#include <bvxm/pro/bvxm_process_utils.h>
 #include <bvxm/pro/bvxm_save_edges_raw_process.h>
-
 
 int main() { return 0; }
