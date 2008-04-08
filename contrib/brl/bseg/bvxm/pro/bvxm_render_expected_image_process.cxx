79a6a6f35eb004f018ce2073f53b238b021b1fbb
Daniel Crispell <dcrispell@users.sourceforge.net>
2008-04-08 17:18:42 -0400

    added number of pixels as inputs to expected image process

--- contrib/brl/bseg/bvxm/pro/bvxm_render_expected_image_process.cxx
+++ contrib/brl/bseg/bvxm/pro/bvxm_render_expected_image_process.cxx
@@ -15,16 +15,20 @@ bvxm_render_expected_image_process::bvxm_render_expected_image_process()
 {
   // process takes 3 inputs:
   //input[0]: The camera to render the expected image from
-  //input[1]: The voxel world
-  //input[2]: The apperance model type :this input must be either apm_mog_grey or apm_mog_rgb
+  //input[1]: number of pixels (x)
+  //input[2]: number of pixels (y)
+  //input[3]: The voxel world
+  //input[4]: The apperance model type :this input must be either apm_mog_grey or apm_mog_rgb
   //          any other string will initialize the value for apm_mog_grey
-  //input[3]: The bin index to be updatet
-  input_data_.resize(4,brdb_value_sptr(0));
-  input_types_.resize(4);
+  //input[5]: The bin index to be updated
+  input_data_.resize(6,brdb_value_sptr(0));
+  input_types_.resize(6);
   input_types_[0] = "vpgl_camera_double_sptr";
-  input_types_[1] = "bvxm_voxel_world_sptr";
-  input_types_[2] = "vcl_string";
-  input_types_[3] = "unsigned";
+  input_types_[1] = "unsigned";
+  input_types_[2] = "unsigned";
+  input_types_[3] = "bvxm_voxel_world_sptr";
+  input_types_[4] = "vcl_string";
+  input_types_[5] = "unsigned";
 
   // process has 2 outputs:
   // output[0]: The rendered frame
@@ -46,37 +50,42 @@ bool bvxm_render_expected_image_process::execute()
     static_cast<brdb_value_t<vpgl_camera_double_sptr>* >(input_data_[0].ptr());
   vpgl_camera_double_sptr camera = input0->value();
 
-  brdb_value_t<bvxm_voxel_world_sptr>* input1 =
-    static_cast<brdb_value_t<bvxm_voxel_world_sptr>* >(input_data_[1].ptr());
-  bvxm_voxel_world_sptr world = input1->value();
+  brdb_value_t<unsigned>* input1 = 
+    static_cast<brdb_value_t<unsigned>* >(input_data_[1].ptr());
+  unsigned npixels_x = input1->value();
 
-  brdb_value_t<vcl_string>* input2 =
-    static_cast<brdb_value_t<vcl_string>* >(input_data_[2].ptr());
-  vcl_string voxel_type = input2->value();
+  brdb_value_t<unsigned>* input2 = 
+    static_cast<brdb_value_t<unsigned>* >(input_data_[2].ptr());
+  unsigned npixels_y = input2->value();
 
-  brdb_value_t<unsigned>* input3 =
-    static_cast<brdb_value_t<unsigned>* >(input_data_[3].ptr());
-  unsigned bin_index = input3->value();
+  brdb_value_t<bvxm_voxel_world_sptr>* input3 = 
+    static_cast<brdb_value_t<bvxm_voxel_world_sptr>* >(input_data_[3].ptr());
+  bvxm_voxel_world_sptr world = input3->value();
+  
+  brdb_value_t<vcl_string>* input4 = 
+    static_cast<brdb_value_t<vcl_string>* >(input_data_[4].ptr());
+  vcl_string voxel_type = input4->value();
+
+  brdb_value_t<unsigned>* input5 = 
+    static_cast<brdb_value_t<unsigned>* >(input_data_[5].ptr());
+  unsigned bin_index = input5->value();
+
 
   //create image metadata object (no image with camera, so just use dummy):
-  vil_image_view_base_sptr dummy_img;
+  vil_image_view_base_sptr dummy_img = 0;
   bvxm_image_metadata camera_metadata(dummy_img,camera);
   //render image
   bool result;
   vil_image_view_base_sptr expected_img;
-  vil_image_view<float> *mask;
+  vil_image_view<float> *mask_img = new vil_image_view<float>(npixels_x,npixels_y,1);
 
-  if (voxel_type == "apm_mog_rgb"){
-    expected_img = new vil_image_view<vxl_byte>(1280,720,3);
-    vil_image_view<float> mask_img(expected_img->ni(),expected_img->nj(),1);
-    result = world->expected_image<APM_MOG_RGB>(camera_metadata, expected_img, mask_img, bin_index);
-    mask = new vil_image_view<float>(mask_img);
+  if (voxel_type == "apm_mog_rgb"){  
+    expected_img = new vil_image_view<vxl_byte>(npixels_x,npixels_y,3);
+    result = world->expected_image<APM_MOG_RGB>(camera_metadata, expected_img, *mask_img, bin_index);
   }
   else {
-    expected_img = new vil_image_view<vxl_byte>(200,200,1);
-    vil_image_view<float> mask_img(expected_img->ni(),expected_img->nj(),1);
-    result = world->expected_image<APM_MOG_GREY>(camera_metadata, expected_img, mask_img, bin_index);
-    mask = new vil_image_view<float>(mask_img);
+    expected_img = new vil_image_view<vxl_byte>(npixels_x,npixels_y,1);
+    result = world->expected_image<APM_MOG_GREY>(camera_metadata, expected_img, *mask_img, bin_index);
   }
 
    
@@ -87,7 +96,8 @@ bool bvxm_render_expected_image_process::execute()
     new brdb_value_t<vil_image_view_base_sptr>(expected_img);
   output_data_[0] = output0;
 
-  brdb_value_sptr output1 =
+  vil_image_view_base_sptr mask_sptr = mask_img;
+  brdb_value_sptr output1 = 
     new brdb_value_t<vil_image_view_base_sptr>(mask_sptr);
   output_data_[1] = output1;
 
