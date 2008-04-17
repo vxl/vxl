2fd2607e3f268c30db157f3cd265cacdefb726b9
Daniel Crispell <dcrispell@users.sourceforge.net>
2008-04-17 16:59:43 -0400

    fixed bug -heightmap image was not being preallocated. also added image dimensions as parameters.

--- contrib/brl/bseg/bvxm/pro/bvxm_heightmap_process.cxx
+++ contrib/brl/bseg/bvxm/pro/bvxm_heightmap_process.cxx
@@ -10,14 +10,19 @@
 
 bvxm_heightmap_process::bvxm_heightmap_process()
 {
-  //this process takes 3 input:
-  //the filename of the image, the camera and the voxel world
-  input_data_.resize(2, brdb_value_sptr(0));
-  input_types_.resize(2);
+  // This process has 4 inputs:
+  //input[0]: The camera to render the heightmap from
+  //input[1]: number of pixels (x)
+  //input[2]: number of pixels (y)
+  //input[3]: The voxel world
+  input_data_.resize(4, brdb_value_sptr(0));
+  input_types_.resize(4);
 
   int i=0;
-  input_types_[i++] = "bvxm_voxel_world_sptr";    // voxel_world
   input_types_[i++] = "vpgl_camera_double_sptr";  // camera            
+  input_types_[i++] = "unsigned";
+  input_types_[i++] = "unsigned";
+  input_types_[i++] = "bvxm_voxel_world_sptr";    // voxel_world
   
   //output
   output_data_.resize(1,brdb_value_sptr(0));
@@ -34,22 +39,34 @@ bool bvxm_heightmap_process::execute()
     return false;
 
   // get the inputs:
-  //voxel_world
-  brdb_value_t<bvxm_voxel_world_sptr >* input0 =
-    static_cast<brdb_value_t<bvxm_voxel_world_sptr >* >(input_data_[0].ptr());
-  bvxm_voxel_world_sptr voxel_world = input0->value();
 
   // camera
-  brdb_value_t<vpgl_camera_double_sptr >* input1 =
-    static_cast<brdb_value_t<vpgl_camera_double_sptr >* >(input_data_[1].ptr());
-  vpgl_camera_double_sptr camera = input1->value();
+  brdb_value_t<vpgl_camera_double_sptr >* input0 =
+    static_cast<brdb_value_t<vpgl_camera_double_sptr >* >(input_data_[0].ptr());
+  vpgl_camera_double_sptr camera = input0->value();
 
-  vil_image_view<unsigned> img;
-  voxel_world->heightmap(camera, img);
+  // npixels x 
+  brdb_value_t<unsigned>* input1 = 
+    static_cast<brdb_value_t<unsigned>* >(input_data_[1].ptr());
+  unsigned npixels_x = input1->value();
 
-  // store image output
+  // npixels y
+  brdb_value_t<unsigned>* input2 = 
+    static_cast<brdb_value_t<unsigned>* >(input_data_[2].ptr());
+  unsigned npixels_y = input2->value();
+
+  //voxel_world
+  brdb_value_t<bvxm_voxel_world_sptr >* input3 =
+    static_cast<brdb_value_t<bvxm_voxel_world_sptr >* >(input_data_[3].ptr());
+  bvxm_voxel_world_sptr voxel_world = input3->value();
+
+  vil_image_view<unsigned> *hmap = new vil_image_view<unsigned>(npixels_x, npixels_y, 1);
+  voxel_world->heightmap(camera,*hmap);
+
+  //store output
   brdb_value_sptr output0 =
-    new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<unsigned>(img));
+    new brdb_value_t<vil_image_view_base_sptr>(hmap);
   output_data_[0] = output0;
 
+  return true;
 }
