1bd1ebb049819034b81836e90d1a9a689723c2c5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-04-20 09:45:08 -0400

    renamed from bvxm_voxel_storage_mem+mix_gauss_f3-.cxx to bvxm_voxel_storage_mem+bsta_num_obs+bsta_mixture_fixed+bsta_num_obs+bsta_gauss_if3-.3---.cxx

--- contrib/brl/bseg/bvxm/Templates/bvxm_voxel_storage_mem+mix_gauss_f3-.cxx
+++
@@ -1,6 +0,0 @@
-#include "bvxm_voxel_storage_mem.txx"
-#include "bvxm_mog_rgb_processor.h"
-
-typedef bvxm_mog_rgb_processor::apm_datatype mix_gauss_type;
-
-BVXM_VOXEL_STORAGE_MEM_INSTANTIATE(mix_gauss_type);
