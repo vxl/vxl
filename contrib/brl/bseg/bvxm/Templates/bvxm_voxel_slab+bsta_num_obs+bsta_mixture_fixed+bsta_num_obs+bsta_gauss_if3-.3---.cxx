59022105c0b038690c8c48b78e29bb2b761eccdc
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-04-20 09:29:56 -0400

    renamed from bvxm_voxel_slab+mix_gauss_f3-.cxx to bvxm_voxel_slab+bsta_num_obs+bsta_mixture_fixed+bsta_num_obs+bsta_gauss_if3-.3---.cxx

---
+++ contrib/brl/bseg/bvxm/Templates/bvxm_voxel_slab+bsta_num_obs+bsta_mixture_fixed+bsta_num_obs+bsta_gauss_if3-.3---.cxx
@@ -0,0 +1,10 @@
+#include <bvxm/bvxm_voxel_slab.txx>
+#include <bsta/bsta_gauss_if3.h>
+#include <bsta/bsta_attributes.h>
+#include <bsta/bsta_mixture_fixed.h>
+
+typedef bsta_num_obs<bsta_gauss_if3> gauss_type;
+typedef bsta_mixture_fixed<gauss_type, 3> mix_gauss;
+typedef bsta_num_obs<mix_gauss> mix_gauss_type;
+
+BVXM_VOXEL_SLAB_INSTANTIATE(mix_gauss_type);
