e15a2cefe87484d794bf2d6b8558ee9e7459e715
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-04-20 10:44:41 -0400

    brief Docygen comment should be on a single line

--- contrib/brl/bseg/bvxm/pro/bvxm_heightmap_process.h
+++ contrib/brl/bseg/bvxm/pro/bvxm_heightmap_process.h
@@ -1,18 +1,15 @@
 // This is brl/bseg/bvxm/pro/bvxm_heightmap_process.h
 #ifndef bvxm_heightmap_process_h_
 #define bvxm_heightmap_process_h_
-
 //:
 // \file
-// \brief A class for generating a height map from a given camera 
-//        viewpoint
+// \brief A class for generating a height map from a given camera viewpoint
 //        -  Input:
 //             - bvxm_voxel_world_sptr
-//             - vpgl_camera_double_sptr  
+//             - vpgl_camera_double_sptr
 //
 //        -  Output:
 //             - vil_image_view_base_sptr   generated image
-//
 //
 // \author  Gamze D. Tunali
 // \date    04/17/2008
@@ -22,14 +19,10 @@
 // \endverbatim
 
 #include <vcl_string.h>
-#include <vcl_vector.h>
 
-#include <vgl/vgl_point_3d.h>
 #include <bgeo/bgeo_lvcs_sptr.h>
 #include <bprb/bprb_process.h>
 #include <vpgl/vpgl_local_rational_camera.h>
-#include <vil/vil_image_view.h>
-#include <vil/vil_image_resource_sptr.h>
 
 #include <bvxm/bvxm_world_params.h>
 
@@ -41,23 +34,19 @@ class bvxm_heightmap_process : public bprb_process
 
   //: Copy Constructor (no local data)
   bvxm_heightmap_process(const bvxm_heightmap_process& other)
-    : bprb_process(*static_cast<const bprb_process*>(&other)){}
+  : bprb_process(*static_cast<const bprb_process*>(&other)) { }
 
   ~bvxm_heightmap_process(){}
 
   //: Clone the process
   virtual bvxm_heightmap_process* clone() const
-    { return new bvxm_heightmap_process(*this); }
+  { return new bvxm_heightmap_process(*this); }
 
-  vcl_string name(){return "bvxmHeightmapProcess";}
+  vcl_string name() { return "bvxmHeightmapProcess"; }
 
   bool init() { return true; }
   bool execute();
-  bool finish(){return true;}
-
- private:
-
+  bool finish() { return true; }
 };
-
 
 #endif // bvxm_heightmap_process_h_
