b1b052fcf5b6701aeb94ad75390832b0bc27639d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-04-20 10:25:38 -0400

    TABs replaced by spaces

--- contrib/brl/bbas/bsta/bsta_basic_functors.h
+++ contrib/brl/bbas/bsta/bsta_basic_functors.h
@@ -25,10 +25,10 @@ class bsta_prob_density_functor
 
   //: The main function
   bool operator() ( const dist_& d, const vector_& sample, return_T& retval ) const
-    {
-      retval = d.prob_density(sample);
-      return true;
-    }
+  {
+    retval = d.prob_density(sample);
+    return true;
+  }
 };
 
 //: A functor to return the probability density with additional covariance
@@ -44,40 +44,40 @@ class bsta_prob_density_addcovar_functor
   //: The main function
   bool operator() ( const mix_dist_& d, const vector_& sample,
                     const covar_t_& add_covar, return_T& retval ) const
-    {
-      unsigned nc = d.num_components();
-      vcl_vector<covar_t_> initial_covars(nc);
-	  mix_dist_& non_const_d = const_cast<mix_dist_&>(d);
-      for(unsigned i = 0; i<nc; ++i){
-        initial_covars[i]=(d.distribution(i)).covar();
-        (non_const_d.distribution(i)).set_covar(initial_covars[i]+add_covar);
-      }
-      retval = non_const_d.prob_density(sample);
-      for(unsigned i = 0; i<nc; ++i)
-        (non_const_d.distribution(i)).set_covar(initial_covars[i]);
-      return true;
+  {
+    unsigned nc = d.num_components();
+    vcl_vector<covar_t_> initial_covars(nc);
+    mix_dist_& non_const_d = const_cast<mix_dist_&>(d);
+    for (unsigned i = 0; i<nc; ++i) {
+      initial_covars[i]=(d.distribution(i)).covar();
+      non_const_d.distribution(i).set_covar(initial_covars[i]+add_covar);
     }
+    retval = non_const_d.prob_density(sample);
+    for (unsigned i = 0; i<nc; ++i)
+      non_const_d.distribution(i).set_covar(initial_covars[i]);
+    return true;
+  }
 };
 
 
-  //: A functor to return the probability integrated over a box
-  template <class dist_>
-    class bsta_probability_functor
-    {
-    public:
-      typedef typename dist_::math_type T;
-      typedef typename dist_::vector_type vector_;
-      typedef T return_T;
-      enum { return_dim = 1 };
+//: A functor to return the probability integrated over a box
+template <class dist_>
+class bsta_probability_functor
+{
+ public:
+  typedef typename dist_::math_type T;
+  typedef typename dist_::vector_type vector_;
+  typedef T return_T;
+  enum { return_dim = 1 };
 
-      //: The main function
-      bool operator() ( const dist_& d, const vector_& min_pt,
-                        const vector_& max_pt, return_T& retval ) const
-        {
-          retval = d.probability(min_pt,max_pt);
-          return true;
-        }
-    };
+  //: The main function
+  bool operator() ( const dist_& d, const vector_& min_pt,
+                    const vector_& max_pt, return_T& retval ) const
+  {
+    retval = d.probability(min_pt,max_pt);
+    return true;
+  }
+};
 
 //: A functor to return the probability with added covariance
 template <class mix_dist_>
@@ -95,287 +95,286 @@ class bsta_probability_addcovar_functor
                     const vector_& max_pt,
                     const covar_t_& add_covar,
                     return_T& retval ) const
-    {
-      unsigned nc = d.num_components();
-	  mix_dist_& non_const_d = const_cast<mix_dist_&>(d);
-      vcl_vector<covar_t_> initial_covars(nc);
-      for(unsigned i = 0; i<nc; ++i){
-        initial_covars[i]=(d.distribution(i)).covar();
-        (non_const_d.distribution(i)).set_covar(initial_covars[i]+add_covar);
-      }
-      retval = d.probability(min_pt, max_pt);
-      for(unsigned i = 0; i<nc; ++i)
-        (non_const_d.distribution(i)).set_covar(initial_covars[i]);
-      return true;
+  {
+    unsigned nc = d.num_components();
+    mix_dist_& non_const_d = const_cast<mix_dist_&>(d);
+    vcl_vector<covar_t_> initial_covars(nc);
+    for (unsigned i = 0; i<nc; ++i){
+      initial_covars[i]=(d.distribution(i)).covar();
+      non_const_d.distribution(i).set_covar(initial_covars[i]+add_covar);
     }
+    retval = d.probability(min_pt, max_pt);
+    for (unsigned i = 0; i<nc; ++i)
+      non_const_d.distribution(i).set_covar(initial_covars[i]);
+    return true;
+  }
 };
 
-  //: A functor to return the mean of the Gaussian
-  // \note the distribution must be Gaussian
-  template <class dist_>
-    class bsta_mean_functor
-    {
-    public:
-      typedef typename dist_::math_type T;
-      typedef typename dist_::vector_type vector_;
-      typedef vector_ return_T;
-      enum { return_dim = dist_::dimension };
+//: A functor to return the mean of the Gaussian
+// \note the distribution must be Gaussian
+template <class dist_>
+class bsta_mean_functor
+{
+ public:
+  typedef typename dist_::math_type T;
+  typedef typename dist_::vector_type vector_;
+  typedef vector_ return_T;
+  enum { return_dim = dist_::dimension };
 
-      //: The main function
-      bool operator() ( const dist_& d, return_T& retval ) const
-        {
-          retval = d.mean();
-          return true;
-        }
-    };
-
-
-
-  //: A functor to return the variance of the Gaussian
-  // \note the distribution must be spherical Gaussian
-  template <class dist_>
-    class bsta_var_functor
-    {
-    public:
-      typedef typename dist_::math_type T;
-      typedef T return_T;
-      enum { return_dim = 1 };
-
-      //: The main function
-      bool operator() ( const dist_& d, return_T& retval ) const
-        {
-          retval = d.var();
-          return true;
-        }
-    };
+  //: The main function
+  bool operator() ( const dist_& d, return_T& retval ) const
+  {
+    retval = d.mean();
+    return true;
+  }
+};
 
 
-  //: A functor to return the variance of the Gaussian
-  // \note the distribution must be spherical Gaussian
-  template <class dist_>
-    class bsta_diag_covar_functor
-    {
-    public:
-      typedef typename dist_::math_type T;
-      typedef typename dist_::vector_type vector_;
-      typedef vector_ return_T;
-      enum { return_dim = dist_::dimension };
+//: A functor to return the variance of the Gaussian
+// \note the distribution must be spherical Gaussian
+template <class dist_>
+class bsta_var_functor
+{
+ public:
+  typedef typename dist_::math_type T;
+  typedef T return_T;
+  enum { return_dim = 1 };
 
-      //: The main function
-      bool operator() ( const dist_& d, return_T& retval ) const
-        {
-          retval = d.diag_covar();
-          return true;
-        }
-    };
-
-
-  //: A functor to return the determinant of the covariance of the Gaussian
-  // \note the distribution must be Gaussian
-  template <class dist_>
-    class bsta_det_covar_functor
-    {
-    public:
-      typedef typename dist_::math_type T;
-      typedef T return_T;
-      enum { return_dim = 1 };
-
-      //: The main function
-      bool operator() ( const dist_& d, return_T& retval ) const
-        {
-          retval = d.det_covar();
-          return true;
-        }
-    };
+  //: The main function
+  bool operator() ( const dist_& d, return_T& retval ) const
+  {
+    retval = d.var();
+    return true;
+  }
+};
 
 
-  //: A functor to return the weight of the component with given index
-  // \note the distribution must be a mixture
-  template <class mixture_>
-    class bsta_weight_functor
-    {
-    public:
-      typedef typename mixture_::math_type T;
-      typedef T return_T;
-      enum { return_dim = 1 };
+//: A functor to return the variance of the Gaussian
+// \note the distribution must be spherical Gaussian
+template <class dist_>
+class bsta_diag_covar_functor
+{
+ public:
+  typedef typename dist_::math_type T;
+  typedef typename dist_::vector_type vector_;
+  typedef vector_ return_T;
+  enum { return_dim = dist_::dimension };
 
-      //: Constructor
-      bsta_weight_functor(unsigned int index = 0) : idx(index) {}
+  //: The main function
+  bool operator() ( const dist_& d, return_T& retval ) const
+  {
+    retval = d.diag_covar();
+    return true;
+  }
+};
 
-      //: The main function
-      bool operator() ( const mixture_& mix, return_T& retval ) const
-        {
-          if (idx < mix.num_components()){
-            retval = mix.weight(idx);
-            return true;
-          }
+
+//: A functor to return the determinant of the covariance of the Gaussian
+// \note the distribution must be Gaussian
+template <class dist_>
+class bsta_det_covar_functor
+{
+ public:
+  typedef typename dist_::math_type T;
+  typedef T return_T;
+  enum { return_dim = 1 };
+
+  //: The main function
+  bool operator() ( const dist_& d, return_T& retval ) const
+  {
+    retval = d.det_covar();
+    return true;
+  }
+};
+
+
+//: A functor to return the weight of the component with given index
+// \note the distribution must be a mixture
+template <class mixture_>
+class bsta_weight_functor
+{
+ public:
+  typedef typename mixture_::math_type T;
+  typedef T return_T;
+  enum { return_dim = 1 };
+
+  //: Constructor
+  bsta_weight_functor(unsigned int index = 0) : idx(index) {}
+
+  //: The main function
+  bool operator() ( const mixture_& mix, return_T& retval ) const
+  {
+    if (idx < mix.num_components()){
+      retval = mix.weight(idx);
+      return true;
+    }
+    return false;
+  }
+
+  unsigned int idx;
+};
+
+
+//: A functor to apply another functor to one distribution in the mixture
+// \note the distribution must be a mixture
+template <class mixture_, class functor_>
+class bsta_mixture_functor
+{
+ public:
+  typedef typename mixture_::math_type T;
+  typedef typename functor_::return_T return_T;
+  enum { return_dim = functor_::return_dim };
+
+  //: Constructor
+  bsta_mixture_functor(const functor_& f, unsigned int index = 0 )
+  : functor(f), idx(index) {}
+
+  //: The main function
+  bool operator() ( const mixture_& mix, return_T& retval ) const
+  {
+    if (idx < mix.num_components() && mix.weight(idx) > T(0)){
+      return functor(mix.distribution(idx),retval);
+    }
+    return false;
+  }
+
+  //: The functor to apply
+  functor_ functor;
+  //: The index to apply to
+  unsigned int idx;
+};
+
+
+//: A functor to apply another functor with data to one distribution in the mixture
+// \note the distribution must be a mixture
+template <class mixture_, class functor_>
+class bsta_mixture_data_functor
+{
+ public:
+  typedef typename mixture_::math_type T;
+  typedef typename functor_::return_T return_T;
+  typedef typename mixture_::vector_type vector_;
+  enum { return_dim = functor_::return_dim };
+
+  //: Constructor
+  bsta_mixture_data_functor(const functor_& f, unsigned int index = 0 )
+  : functor(f), idx(index) {}
+
+  //: The main function
+  bool operator() ( const mixture_& mix, const vector_& sample, return_T& retval ) const
+  {
+    if (idx < mix.num_components() && mix.weight(idx) > T(0)){
+      return functor(mix.distribution(idx),sample,retval);
+    }
+    return false;
+  }
+
+  //: The functor to apply
+  functor_ functor;
+  //: The index to apply to
+  unsigned int idx;
+};
+
+
+//: A functor to apply another functor to each distribution and produce a weighted sum
+// \note the distribution must be a mixture
+template <class mixture_, class functor_>
+class bsta_weighted_sum_functor
+{
+ public:
+  typedef typename mixture_::math_type T;
+  typedef typename functor_::return_T return_T;
+  enum { return_dim = functor_::return_dim };
+
+  //: Constructor
+  bsta_weighted_sum_functor() : functor() {}
+  //: Constructor
+  bsta_weighted_sum_functor(const functor_& f)
+  : functor(f) {}
+
+  //: The main function
+  bool operator() ( const mixture_& mix, return_T& retval ) const
+  {
+    const unsigned int nc = mix.num_components();
+    if (nc > 0) {
+      return_T temp;
+      if ( !functor(mix.distribution(0),temp) )
+        return false;
+      retval = mix.weight(0) * temp;
+      for (unsigned int idx=1; idx<nc; ++idx){
+        if ( !functor(mix.distribution(idx),temp) )
           return false;
-        }
+        retval += mix.weight(idx) * temp;
+      }
+      return true;
+    }
+    return false;
+  }
 
-      unsigned int idx;
-    };
+  //: The functor to apply
+  functor_ functor;
+};
 
 
-  //: A functor to apply another functor to one distribution in the mixture
-  // \note the distribution must be a mixture
-  template <class mixture_, class functor_>
-    class bsta_mixture_functor
+//: A functor to apply another functor with data to each distribution and produce a weighted sum
+// \note the distribution must be a mixture
+template <class mixture_, class functor_>
+class bsta_weighted_sum_data_functor
+{
+ public:
+  typedef typename mixture_::math_type T;
+  typedef typename functor_::return_T return_T;
+  typedef typename mixture_::vector_type vector_;
+  enum { return_dim = functor_::return_dim };
+
+  //: Constructor
+  bsta_weighted_sum_data_functor() : functor() {}
+  //: Constructor
+  bsta_weighted_sum_data_functor(const functor_& f)
+  : functor(f) {}
+
+  //: The main function
+  bool operator() ( const mixture_& mix, const vector_& sample, return_T& retval ) const
+  {
+    const unsigned int nc = mix.num_components();
+    if (nc > 0)
     {
-    public:
-      typedef typename mixture_::math_type T;
-      typedef typename functor_::return_T return_T;
-      enum { return_dim = functor_::return_dim };
-
-      //: Constructor
-      bsta_mixture_functor(const functor_& f, unsigned int index = 0 )
-        : functor(f), idx(index) {}
-
-      //: The main function
-      bool operator() ( const mixture_& mix, return_T& retval ) const
-        {
-          if (idx < mix.num_components() && mix.weight(idx) > T(0)){
-            return functor(mix.distribution(idx),retval);
-          }
+      return_T temp;
+      if ( !functor(mix.distribution(0),sample,temp) )
+        return false;
+      retval = mix.weight(0) * temp;
+      for (unsigned int idx=1; idx<nc; ++idx){
+        if ( !functor(mix.distribution(idx),sample,temp) )
           return false;
-        }
+        retval += mix.weight(idx) * temp;
+      }
+      return true;
+    }
+    return false;
+  }
 
-      //: The functor to apply
-      functor_ functor;
-      //: The index to apply to
-      unsigned int idx;
-    };
-
-
-  //: A functor to apply another functor with data to one distribution in the mixture
-  // \note the distribution must be a mixture
-  template <class mixture_, class functor_>
-    class bsta_mixture_data_functor
-    {
-    public:
-      typedef typename mixture_::math_type T;
-      typedef typename functor_::return_T return_T;
-      typedef typename mixture_::vector_type vector_;
-      enum { return_dim = functor_::return_dim };
-
-      //: Constructor
-      bsta_mixture_data_functor(const functor_& f, unsigned int index = 0 )
-        : functor(f), idx(index) {}
-
-      //: The main function
-      bool operator() ( const mixture_& mix, const vector_& sample, return_T& retval ) const
-        {
-          if (idx < mix.num_components() && mix.weight(idx) > T(0)){
-            return functor(mix.distribution(idx),sample,retval);
-          }
-          return false;
-        }
-
-      //: The functor to apply
-      functor_ functor;
-      //: The index to apply to
-      unsigned int idx;
-    };
+  //: The functor to apply
+  functor_ functor;
+};
 
 
-  //: A functor to apply another functor to each distribution and produce a weighted sum
-  // \note the distribution must be a mixture
-  template <class mixture_, class functor_>
-    class bsta_weighted_sum_functor
-    {
-    public:
-      typedef typename mixture_::math_type T;
-      typedef typename functor_::return_T return_T;
-      enum { return_dim = functor_::return_dim };
+//: A functor to count the number of components in the mixture
+// \note the distribution must be a mixture
+template <class mixture_>
+class bsta_mixture_size_functor
+{
+ public:
+  typedef typename mixture_::math_type T;
+  typedef unsigned int return_T;
+  enum { return_dim = 1 };
 
-      //: Constructor
-      bsta_weighted_sum_functor() : functor() {}
-      //: Constructor
-      bsta_weighted_sum_functor(const functor_& f):
-        functor(f) {}
-
-      //: The main function
-      bool operator() ( const mixture_& mix, return_T& retval ) const
-        {
-          const unsigned int nc = mix.num_components();
-          if (nc > 0){
-            return_T temp;
-            if ( !functor(mix.distribution(0),temp) )
-              return false;
-            retval = mix.weight(0) * temp;
-            for (unsigned int idx=1; idx<nc; ++idx){
-              if ( !functor(mix.distribution(idx),temp) )
-                return false;
-              retval += mix.weight(idx) * temp;
-            }
-            return true;
-          }
-          return false;
-        }
-
-      //: The functor to apply
-      functor_ functor;
-    };
-
-
-  //: A functor to apply another functor with data to each distribution and produce a weighted sum
-  // \note the distribution must be a mixture
-  template <class mixture_, class functor_>
-    class bsta_weighted_sum_data_functor
-    {
-    public:
-      typedef typename mixture_::math_type T;
-      typedef typename functor_::return_T return_T;
-      typedef typename mixture_::vector_type vector_;
-      enum { return_dim = functor_::return_dim };
-
-      //: Constructor
-      bsta_weighted_sum_data_functor() : functor() {}
-      //: Constructor
-      bsta_weighted_sum_data_functor(const functor_& f):
-        functor(f) {}
-
-      //: The main function
-      bool operator() ( const mixture_& mix, const vector_& sample, return_T& retval ) const
-        {
-          const unsigned int nc = mix.num_components();
-          if (nc > 0)
-            {
-              return_T temp;
-              if ( !functor(mix.distribution(0),sample,temp) )
-                return false;
-              retval = mix.weight(0) * temp;
-              for (unsigned int idx=1; idx<nc; ++idx){
-                if ( !functor(mix.distribution(idx),sample,temp) )
-                  return false;
-                retval += mix.weight(idx) * temp;
-              }
-              return true;
-            }
-          return false;
-        }
-
-      //: The functor to apply
-      functor_ functor;
-    };
-
-
-  //: A functor to count the number of components in the mixture
-  // \note the distribution must be a mixture
-  template <class mixture_>
-    class bsta_mixture_size_functor
-    {
-    public:
-      typedef typename mixture_::math_type T;
-      typedef unsigned int return_T;
-      enum { return_dim = 1 };
-
-      //: The main function
-      bool operator() ( const mixture_& mix, return_T& retval ) const
-        {
-          retval = mix.num_components();
-          return true;
-        }
-    };
+  //: The main function
+  bool operator() ( const mixture_& mix, return_T& retval ) const
+  {
+    retval = mix.num_components();
+    return true;
+  }
+};
 
 
 #endif // bsta_basic_functors_h_
--- contrib/mul/mbl/tests/test_text_file.cxx
+++ contrib/mul/mbl/tests/test_text_file.cxx
@@ -1,13 +1,6 @@
 #include <vcl_iostream.h>
-#include <vcl_fstream.h>
-#include <vcl_utility.h>
-#include <vcl_cmath.h>
 
 #include <testlib/testlib_test.h>
-
-#include <vil/vil_print.h>
-
-#include <vnl/vnl_math.h>
 
 #include <mbl/mbl_save_text_file.h>
 #include <mbl/mbl_load_text_file.h>
@@ -15,51 +8,45 @@
 
 void test_text_file()
 {
-  vcl_cout << "****************************" << vcl_endl;
-  vcl_cout << " Testing mbl_test_text_file " << vcl_endl;
-  vcl_cout << "****************************" << vcl_endl;
-	
-  
+  vcl_cout << "****************************\n"
+           << " Testing mbl_test_text_file\n"
+           << "****************************" << vcl_endl;
+
   //create double vector
   vcl_vector<double> double_vec(3);
   double_vec[0]= -1.1;
   double_vec[1]= 4.0;
   double_vec[2]= -5.3;
-   
+
   mbl_save_text_file( double_vec, "double_vec.txt");
   vcl_vector<double> double_vec_in;
   mbl_load_text_file( double_vec_in, "double_vec.txt");
-  
+
   TEST( "double_vec io", double_vec==double_vec_in, true );
-  
+
   //create int vector
   vcl_vector<int> int_vec(3);
   int_vec[0]= -1;
   int_vec[1]= 4;
   int_vec[2]= -5;
-   
+
   mbl_save_text_file( int_vec, "int_vec.txt");
   vcl_vector<int> int_vec_in;
   mbl_load_text_file( int_vec_in, "int_vec.txt");
-  
+
   TEST( "int_vec io", int_vec==int_vec_in, true );
- 
 
   //create bool vector
   vcl_vector<bool> bool_vec(3);
   int_vec[0]= true;
   int_vec[1]= false;
   int_vec[2]= true;
-   
+
   mbl_save_text_file( bool_vec, "bool_vec.txt");
   vcl_vector<bool> bool_vec_in;
   mbl_load_text_file( bool_vec_in, "bool_vec.txt");
-  
-  TEST( "bool_vec io", bool_vec==bool_vec_in, true );
- 
-  
- 
-}
 
+  TEST( "bool_vec io", bool_vec==bool_vec_in, true );
+}
 
 TESTMAIN(test_text_file);
