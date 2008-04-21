b0c9aad99c1f46d59968485c3e5beecc373e94d2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-04-21 14:09:52 -0400

    added missing #include <iostream> and replaced floor() by vcl_floor()

--- contrib/brl/bseg/bvxm/pro/bvxm_normalize_image_process.h
+++ contrib/brl/bseg/bvxm/pro/bvxm_normalize_image_process.h
@@ -9,38 +9,39 @@
 // \author Ozge Can Ozcanli
 // \date 02/13/2008
 // \verbatim
-//
-// Modifications 
+//  Modifications
 //   Ozge C Ozcanli - 03/25/08 - fixed a compiler warning as suggested by Daniel Lyddy
-//
 // \endverbatim
 
 #include <vcl_string.h>
 #include <vcl_algorithm.h>
+#include <vcl_cmath.h>
+#ifdef DEBUG
+#include <vcl_iostream.h>
+#endif
 #include <bprb/bprb_process.h>
 
 #include <vil/vil_image_view.h>
 
 class bvxm_normalize_image_process : public bprb_process
 {
-public:
+ public:
 
   bvxm_normalize_image_process();
 
   //: Copy Constructor (no local data)
-  bvxm_normalize_image_process(const bvxm_normalize_image_process& other): bprb_process(*static_cast<const bprb_process*>(&other)){};
+  bvxm_normalize_image_process(const bvxm_normalize_image_process& other): bprb_process(*static_cast<const bprb_process*>(&other)) {}
 
-  ~bvxm_normalize_image_process(){};
+  ~bvxm_normalize_image_process() {}
 
   //: Clone the process
-  virtual bvxm_normalize_image_process* clone() const {return new bvxm_normalize_image_process(*this);}
+  virtual bvxm_normalize_image_process* clone() const { return new bvxm_normalize_image_process(*this); }
 
-  vcl_string name(){return "bvxmNormalizeImageProcess";}
+  vcl_string name() { return "bvxmNormalizeImageProcess"; }
 
   bool init() { return true; }
   bool execute();
-  bool finish(){return true;}
-
+  bool finish() { return true; }
 };
 
 
@@ -63,7 +64,6 @@ inline bool normalize_image(const vil_image_view<float>& in_view, vil_image_view
         // Proposed fix
         float min_value = (float) 0;
         out_img(i, j, k) = vcl_min(vcl_max(min_value, p), max_value);
-
       }
 
       return true;
@@ -83,13 +83,14 @@ inline bool normalize_image(const vil_image_view<vxl_byte>& in_view, vil_image_v
     for (unsigned j=0;j<nj;++j)
       for (unsigned i=0;i<ni;++i)
       {
-        int p = (int)floor(a*in_view(i,j,k) + b);
-        if( !(p >= 0) ) out_img(i, j, k)  = 0;
-        else if( p > 255 ) out_img(i, j, k)  = 255;
+        int p = (int)vcl_floor(a*in_view(i,j,k) + b);
+        if ( !(p >= 0) ) out_img(i, j, k)  = 0;
+        else if ( p > 255 ) out_img(i, j, k)  = 255;
         else out_img(i, j, k)  = p;
-
       }
-      vcl_cerr<<"entered byte case.................."<< vcl_endl;
+#ifdef DEBUG
+      vcl_cerr << "entered byte case..................\n";
+#endif
       return true;
 }
 
