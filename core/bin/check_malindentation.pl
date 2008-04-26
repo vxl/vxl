67a0dad01d0829199436813f678b63ff27e0f327
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-04-26 17:56:12 -0400

    *** empty log message ***

---
+++ core/bin/check_malindentation.pl
@@ -0,0 +1,25 @@
+#! perl -n
+if (eof) { close ARGV; $in_paren=$in_cout=''; }
+next if (m/^#/ || m/^\s*$/);
+s+\\.+X+g;
+s+\".*?\"+X+g;
+s+\'.\'+X+g;
+s+//.*++;
+next if (m/^\s*$/);
+m/^( *)(.)?(.)?/; $indent=length($1); $in_cout = '' unless ($2.$3 eq '<<');
+++$indent if ($2 eq ')');
+print "vi +$. $ARGV\n" if (($in_paren && $paren_indent != $indent) || ($in_cout && $cout_indent != $indent));
+
+if ($in_paren)
+{
+  $in_paren = m/^([^()]*(\([^()]*\))*)*$/;
+}
+elsif (m/^([^()]*\( *)(([^()]*(\([^()]*\))*)*)$/ && length($2) > 1)
+{
+  $paren_indent=length($1); $in_paren=1;
+}
+
+if (m/^( *\w+ *)<</)
+{
+  $cout_indent=length($1); $in_cout=1;
+}
