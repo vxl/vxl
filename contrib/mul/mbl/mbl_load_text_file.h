186d0f63bc6b685906163bce918ce172874600f1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-04-26 17:50:59 -0400

    removed unused #include <vnl/vnl_vector.h>

--- contrib/mul/mbl/mbl_load_text_file.h
+++ contrib/mul/mbl/mbl_load_text_file.h
@@ -1,23 +1,15 @@
 #ifndef mbl_load_text_file_h_
 #define mbl_load_text_file_h_
-
 //:
 // \file
-// \brief Functions to load objects from text file 
+// \brief Functions to load objects from text file
 // \author dac
 
 #include <vcl_string.h>
 #include <vcl_vector.h>
 
-#include <vnl/vnl_vector.h>
-
-
-
 //: Load vector from file with format "v1 v2 .. vn"
 template <class T>
 void mbl_load_text_file(vcl_vector<T>& v, const vcl_string& path);
 
-
-
-
-#endif
+#endif // mbl_load_text_file_h_
