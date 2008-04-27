3fab20012f01933e58b25c47ab0450f08f0a67c3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-04-27 07:56:39 -0400

    avoid compiler warning (implicit conversion from "double" to "float")

--- contrib/brl/bbas/bsta/tests/test_gaussian_full.cxx
+++ contrib/brl/bbas/bsta/tests/test_gaussian_full.cxx
@@ -12,7 +12,7 @@ void test_gaussian_full_type(T epsilon, const vcl_string& type_name)
   bsta_gaussian_full<T,3> df_gauss;
 
   TEST(("dimension <"+type_name+">").c_str(),
-        (bsta_gaussian_full<T,3>::dimension), 3);
+       bsta_gaussian_full<T,3>::dimension, 3);
   TEST(("det(null covar) <"+type_name+">").c_str(),
        df_gauss.det_covar(), T(0));
 
@@ -29,32 +29,32 @@ void test_gaussian_full_type(T epsilon, const vcl_string& type_name)
   TEST(("covar <"+type_name+">").c_str(), gauss.covar(), covar);
 
   TEST_NEAR(("det(covar) <"+type_name+">").c_str(),
-       gauss.det_covar(), T(0.04),epsilon);
+            gauss.det_covar(), T(0.04), epsilon);
 
   vnl_vector_fixed<T,3> test_pt(T(1.5), T(3.0), T(3.0));
   vnl_vector_fixed<T,3> d = mean - test_pt;
 
   T sqr_mah_dist = dot_product(d,(vnl_inverse(covar)*d));
 
-  TEST_NEAR(("mahalanobis dist <"+type_name+">").c_str(),
-       gauss.sqr_mahalanobis_dist(test_pt), sqr_mah_dist, epsilon);
+  TEST_NEAR(("Mahalanobis dist <"+type_name+">").c_str(),
+            gauss.sqr_mahalanobis_dist(test_pt), sqr_mah_dist, epsilon);
 
-  T two_pi = (T)2.0*vnl_math::pi;
-  T prob = (T)1.0/vcl_sqrt(two_pi*two_pi*two_pi*gauss.det_covar()) * vcl_exp(-sqr_mah_dist/2);
+  T two_pi = T(2*vnl_math::pi);
+  T prob = T(1.0/vcl_sqrt(two_pi*two_pi*two_pi*gauss.det_covar()) * vcl_exp(-sqr_mah_dist/2));
   TEST_NEAR(("probability density <"+type_name+">").c_str(),
-       gauss.prob_density(test_pt), prob, epsilon);
+            gauss.prob_density(test_pt), prob, epsilon);
 
   bsta_gaussian_full<T,3> zero_var_gauss;
-  TEST(("zero var mahalanobis dist <"+type_name+">").c_str(),
-        zero_var_gauss.sqr_mahalanobis_dist(test_pt),
-        vcl_numeric_limits<T>::infinity());
+  TEST(("zero var Mahalanobis dist <"+type_name+">").c_str(),
+       zero_var_gauss.sqr_mahalanobis_dist(test_pt),
+       vcl_numeric_limits<T>::infinity());
 
   TEST(("zero var probability density <"+type_name+">").c_str(),
-        zero_var_gauss.prob_density(test_pt), T(0));
+       zero_var_gauss.prob_density(test_pt), T(0));
 
   // test stream
-  vcl_cout << "testing stream operator\n";
-  vcl_cout << gauss << '\n';
+  vcl_cout << "testing stream operator\n"
+           << gauss << '\n';
 }
 
 MAIN( test_gaussian_full )
