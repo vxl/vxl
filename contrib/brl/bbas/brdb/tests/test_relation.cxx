92b9b1fc37bf9f321cceb74b2d77570d1d78f0e1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-04-27 08:54:57 -0400

    avoid compiler warning (variable "it" declared inside for loop, and reused outside)

--- contrib/brl/bbas/brdb/tests/test_relation.cxx
+++ contrib/brl/bbas/brdb/tests/test_relation.cxx
@@ -4,7 +4,6 @@
 #include <brdb/brdb_relation.h>
 #include <vcl_iostream.h>
 
-
 MAIN( test_relation )
 {
   START ("DB Relation");
@@ -40,19 +39,19 @@ MAIN( test_relation )
   vcl_cout << vcl_endl;
 
   vsl_b_ofstream out_stream("test_relation_bio.vsl");
-  if (!out_stream){
-    vcl_cerr<<"Failed to open " << "test_relation_bio.vsl" << " for output." << vcl_endl;
-  }
-  vcl_cout << "Opened file successfully " << vcl_endl;
+  if (!out_stream)
+    vcl_cerr<<"Failed to open test_relation_bio.vsl for output.\n";
+  else
+    vcl_cout << "Opened file test_relation_bio.vsl successfully" << vcl_endl;
 
   my_relation2.b_write(out_stream);
   out_stream.close();
 
   vsl_b_ifstream in_stream("test_relation_bio.vsl");
-  if (!in_stream){
-    vcl_cerr<<"Failed to open " << "test_relation_bio.vsl" << " for input." << vcl_endl;
-  }
-  vcl_cout << "Opened file successfully " << vcl_endl;
+  if (!in_stream)
+    vcl_cerr<<"Failed to open test_relation_bio.vsl for input.\n";
+  else
+    vcl_cout << "Opened file test_relation_bio.vsl successfully" << vcl_endl;
 
   my_relation1.b_read(in_stream);
   in_stream.close();
@@ -61,76 +60,62 @@ MAIN( test_relation )
 
   TEST("Binary IO read and write", true, true);
 
-  
-
-  TEST("size()", (my_relation2.size()==4), true);
-  TEST("arity()", (my_relation2.arity()==3), true);
-  TEST("name()", (my_relation2.name(0) == vcl_string("frames") &&
-          my_relation2.name(1) == vcl_string("time") &&
-          my_relation2.name(2) == vcl_string("nickname")),
-          true);
-  TEST("index()", (my_relation2.index(vcl_string("frames")) == 0 &&
-        my_relation2.index(vcl_string("time")) == 1 &&
-        my_relation2.index(vcl_string("nickname")) == 2),
-        true);
-
+  TEST("size()", my_relation2.size(), 4);
+  TEST("arity()", my_relation2.arity(), 3);
+  TEST("name(0)", my_relation2.name(0), vcl_string("frames"));
+  TEST("name(1)", my_relation2.name(1), vcl_string("time"));
+  TEST("name(2)", my_relation2.name(2), vcl_string("nickname"));
+  TEST("index=0", my_relation2.index(vcl_string("frames")), 0);
+  TEST("index=1", my_relation2.index(vcl_string("time")), 1);
+  TEST("index=2", my_relation2.index(vcl_string("nickname")), 2);
 
   vcl_vector<brdb_value_t<int> > my_int(4);
   my_relation2.order_by(vcl_string("frames"), false);
-  int i=0;
-  for(vcl_vector<brdb_tuple_sptr>::iterator it = my_relation2.begin(); it != my_relation2.end(); ++it, ++i)
+
+  vcl_vector<brdb_tuple_sptr>::iterator it = my_relation2.begin();
+
+  for (int i=0; it != my_relation2.end(); ++it, ++i)
     my_relation2.get_value(it, "frames", my_int[i]);
 
-  if(my_int[0].value() >= my_int[1].value() &&
-    my_int[1].value() >= my_int[2].value() &&
-    my_int[2].value() >= my_int[3].value())
-  {
-    TEST("sort_by_name", true, true);
-    TEST("iterator", true, true);
-    TEST("get_value()", true, true);  
-  }
+  TEST("sort_by_name", my_int[0].value() >= my_int[1].value() &&
+                       my_int[1].value() >= my_int[2].value() &&
+                       my_int[2].value() >= my_int[3].value(), true);
 
   bool set_value_test_pass = true;
-  i=0;
   brdb_value_t<int> int_val1, int_val2;
-  for(vcl_vector<brdb_tuple_sptr>::iterator it = my_relation2.begin(); it != my_relation2.end(); ++it, ++i)
+  it = my_relation2.begin();
+  for (int i=0; it != my_relation2.end(); ++it, ++i)
   {
     my_relation2.get_value(it, "frames", int_val1);
     int_val2 = 0 - int_val1;
     my_relation2.set_value(it, "frames", int_val2);
     my_relation2.get_value(it, "frames", int_val2);
 
-    if(int_val1 + int_val2 != 0)
+    if (int_val1 + int_val2 != 0)
     {
-    set_value_test_pass = false;
-    break;
+      set_value_test_pass = false;
+      break;
     }
   }
-
   TEST("set_value", set_value_test_pass, true);
 
-
-  vcl_vector<brdb_value_t<double> > my_double(4);  
+  vcl_vector<brdb_value_t<double> > my_double(4);
   my_relation2.order_by(1, true);
-  i=0;
-  for(vcl_vector<brdb_tuple_sptr>::iterator it = my_relation2.begin(); it != my_relation2.end(); ++it, ++i)
+  it = my_relation2.begin();
+  for (int i=0; it != my_relation2.end(); ++it, ++i)
     my_relation2.get_value(it, "time", my_double[i]);
 
-  if(my_double[0].value() <= my_double[1].value() &&
-    my_double[1].value() <= my_double[2].value() &&
-    my_double[2].value() <= my_double[3].value())
-  {
-    TEST("sort_by_index", true, true);
-  }
+  TEST("sort_by_index", my_double[0].value() <= my_double[1].value() &&
+                        my_double[1].value() <= my_double[2].value() &&
+                        my_double[2].value() <= my_double[3].value(), true);
 
-  
-  vcl_vector<brdb_tuple_sptr>::iterator it = my_relation2.begin();
+  it = my_relation2.begin();
   brdb_tuple_sptr new_tuple1 = new brdb_tuple(300, -20.1, vcl_string("stringX"));
   my_relation2.insert_tuple(new_tuple1, it);
   it = my_relation2.begin();
   brdb_value_t<int> int_val3;
   my_relation2.get_value(it, "frames", int_val3);
-  TEST("insert tuple", (int_val3 == 300), true);
+  TEST("insert tuple", int_val3, 300);
 
   it = my_relation2.begin();
   brdb_tuple_sptr new_tuple2 = new brdb_tuple(-300, 20.1, vcl_string("stringX2"));
@@ -140,17 +125,18 @@ MAIN( test_relation )
   it = my_relation2.end();
   --it;
   my_relation2.get_value(it, "frames", int_val5);
-  TEST("add tuple", (int_val5 == -300), true);
+  TEST("add tuple", int_val5, -300);
 
   int size1 = my_relation2.size();
   my_relation2.remove_tuple(it);
   it = my_relation2.end();
   --it;
   my_relation2.get_value(it, "frames", int_val5);
-  TEST("remove", (int_val5 != -300 && static_cast<int>(my_relation2.size()) == size1-1), true);
+  TEST("remove", int_val5 != -300 && my_relation2.size()+1 == size1, true);
 
   my_relation2.clear();
-  TEST("clear()", (my_relation2.arity() == 3 && my_relation2.size() == 0), true);
+  TEST("clear(): arity", my_relation2.arity(), 3);
+  TEST("clear(): size",  my_relation2.size(),  0);
 
   SUMMARY();
 }
