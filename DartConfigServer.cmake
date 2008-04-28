43485d6744bdc96671856c4228a0152ccf340852
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-04-28 02:53:34 -0400

    replaced by Vista_8.0

--- DartConfigServer.cmake
+++ DartConfigServer.cmake
@@ -60,7 +60,6 @@ SET (EXPECTED_BUILDS "\\
   {GE Win2k_bcc-5.5.1_Release} \\
   {GE Win2k_msvc-7.1_Debug} \\
   {GE WinXP_msvc-8.0_Release} \\
-  {imorphics.com WinXP_msvc-7.1_RelWithDebInfo} \\
   {imorphics.com Vista_msvc-8.0_RelWithDebInfo} \\
   {imorphics.com Linux-2.6.22_gcc-4.1.2_RelWithDebInfo} \\
   {lems.brown.edu Linux-2.6_gcc-4.1.3_-Wall} \\
