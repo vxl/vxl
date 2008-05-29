2c5b2ad18c60b5f561fcf45df7f12646f40ad768
Kevin de Souza <kdesouza@users.sourceforge.net>
2008-05-29 11:53:13 -0400

    Added new expected build.

--- DartConfigServer.cmake
+++ DartConfigServer.cmake
@@ -63,6 +63,7 @@ SET (EXPECTED_BUILDS "\\
   {GE WinXP_msvc-8.0_Release} \\
   {imorphics.com WinVista_msvc-8.0_RelWithDebInfo} \\
   {imorphics.com Linux-2.6.22_gcc-4.1.2_RelWithDebInfo} \\
+  {imorphics.com Linux-2.6.22_gcc-4.1.2_Debug} \\
   {lems.brown.edu Linux-2.6_gcc-4.2.3_-Wall} \\
   {lems.brown.edu Linux-2.6.18_gcc-4.1.2} \\
 ")
