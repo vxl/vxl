059c796c0530264ed0c3700fa10689730d26a9f0
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-05-31 16:29:17 -0400

    removed unused #include <vil/vil_image_view.h>

--- contrib/brl/bseg/bvxm/pro/bvxm_compare_rpc_process.h
+++ contrib/brl/bseg/bvxm/pro/bvxm_compare_rpc_process.h
@@ -27,7 +27,6 @@
 #include <bgeo/bgeo_lvcs_sptr.h>
 #include <bprb/bprb_process.h>
 #include <vpgl/vpgl_local_rational_camera.h>
-#include <vil/vil_image_view.h>
 
 class bvxm_compare_rpc_process : public bprb_process
 {
@@ -37,22 +36,19 @@ class bvxm_compare_rpc_process : public bprb_process
 
   //: Copy Constructor (no local data)
   bvxm_compare_rpc_process(const bvxm_compare_rpc_process& other)
-    : bprb_process(*static_cast<const bprb_process*>(&other)){};
+    : bprb_process(*static_cast<const bprb_process*>(&other)) {}
 
-  ~bvxm_compare_rpc_process(){};
+  ~bvxm_compare_rpc_process() {}
 
   //: Clone the process
   virtual bvxm_compare_rpc_process* clone() const
     { return new bvxm_compare_rpc_process(*this); }
 
-  vcl_string name(){return "bvxmCompareRpcProcess";}
+  vcl_string name() { return "bvxmCompareRpcProcess"; }
 
   bool init() { return true; }
   bool execute();
-  bool finish(){return true;}
-
- private:
-
+  bool finish() { return true; }
 };
 
 #endif // bvxm_compare_rpc_process_h_
