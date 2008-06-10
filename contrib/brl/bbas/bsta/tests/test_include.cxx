f47918be29f9d762ddbaaeeb26f29a1691590f60
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-06-09 23:32:54 -0400

    adding bsta/io header files to bsta/tests/test_include.cxx since there is no bsta/io/tests subdir

--- contrib/brl/bbas/bsta/tests/test_include.cxx
+++ contrib/brl/bbas/bsta/tests/test_include.cxx
@@ -37,4 +37,12 @@
 #include <bsta/algo/bsta_mixture_functors.h>
 #include <bsta/algo/bsta_truth_updater.h>
 
+#include <bsta/io/bsta_io_attributes.h>
+#include <bsta/io/bsta_io_gaussian_full.h>
+#include <bsta/io/bsta_io_gaussian_indep.h>
+#include <bsta/io/bsta_io_gaussian_sphere.h>
+#include <bsta/io/bsta_io_histogram.h>
+#include <bsta/io/bsta_io_mixture.h>
+#include <bsta/io/bsta_io_mixture_fixed.h>
+
 int main() { return 0; }
