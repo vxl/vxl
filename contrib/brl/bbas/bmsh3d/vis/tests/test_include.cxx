431c471faecc93fefc0f060ab1a9f3368db405ac
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-06-10 18:51:49 -0400

    added test_include for bmsh3d/vis

---
+++ contrib/brl/bbas/bmsh3d/vis/tests/test_include.cxx
@@ -0,0 +1,8 @@
+#include <bmsh3d/vis/bmsh3d_vis_backpt.h>
+#include <bmsh3d/vis/bmsh3d_vis_edge.h>
+#include <bmsh3d/vis/bmsh3d_vis_face.h>
+#include <bmsh3d/vis/bmsh3d_vis_mesh.h>
+#include <bmsh3d/vis/bmsh3d_vis_utils.h>
+#include <bmsh3d/vis/bmsh3d_vis_vertex.h>
+
+int main() { return 0; }
