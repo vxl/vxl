9b31ce44c28b6e5159f8a198a2ed15717383c18f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-06-17 17:28:22 -0400

    renamed from contrib/mul/mipa/Templates/mipa_orientation_histogram+float-+float-.cxx to contrib/mul/mipa/Templates/mipa_orientation_histogram+float.float-.cxx

--- contrib/mul/mipa/Templates/mipa_orientation_histogram+float-+float-.cxx
+++
@@ -1,3 +0,0 @@
-#include <mipa/mipa_orientation_histogram.txx>
-MIPA_ORIENTATION_HISTOGRAM_INSTANTIATE(float, float);
-
---
+++ contrib/mul/mipa/Templates/mipa_orientation_histogram+float.float-.cxx
@@ -0,0 +1,3 @@
+#include <mipa/mipa_orientation_histogram.txx>
+
+MIPA_ORIENTATION_HISTOGRAM_INSTANTIATE(float, float);
