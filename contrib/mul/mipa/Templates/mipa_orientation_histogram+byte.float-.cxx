6d7cfb864c905a28d0d7cd91bffd1c0d95bad3b4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-06-17 17:28:16 -0400

    renamed from contrib/mul/mipa/Templates/mipa_orientation_histogram+byte-+float-.cxx to contrib/mul/mipa/Templates/mipa_orientation_histogram+byte.float-.cxx

--- contrib/mul/mipa/Templates/mipa_orientation_histogram+byte.float-.cxx
+++ contrib/mul/mipa/Templates/mipa_orientation_histogram+byte.float-.cxx
@@ -1,4 +1,4 @@
 #include <mipa/mipa_orientation_histogram.txx>
 #include <vxl_config.h>
-MIPA_ORIENTATION_HISTOGRAM_INSTANTIATE(vxl_byte, float);
 
+MIPA_ORIENTATION_HISTOGRAM_INSTANTIATE(vxl_byte, float);
