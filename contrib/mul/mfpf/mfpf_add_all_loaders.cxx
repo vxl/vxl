24e8e3fbac68184663e9178691ca36b29fdcdc51
Tim Cootes <timcootes@users.sourceforge.net>
2008-06-17 11:26:22 -0400

    Added HOG finders to loaders.

--- contrib/mul/mfpf/mfpf_add_all_loaders.cxx
+++ contrib/mul/mfpf/mfpf_add_all_loaders.cxx
@@ -18,6 +18,8 @@
 #include <mfpf/mfpf_sad_vec_cost_builder.h>
 #include <mfpf/mfpf_ssd_vec_cost.h>
 #include <mfpf/mfpf_ssd_vec_cost_builder.h>
+#include <mfpf/mfpf_hog_box_finder.h>
+#include <mfpf/mfpf_hog_box_finder_builder.h>
 
 #include <vpdfl/vpdfl_add_all_binary_loaders.h>
 
@@ -32,12 +34,14 @@ void mfpf_add_all_loaders()
   mbl_cloneables_factory<mfpf_point_finder_builder>::add(mfpf_profile_pdf_builder());
   mbl_cloneables_factory<mfpf_point_finder_builder>::add(mfpf_region_pdf_builder());
   mbl_cloneables_factory<mfpf_point_finder_builder>::add(mfpf_region_finder_builder());
+  mbl_cloneables_factory<mfpf_point_finder_builder>::add(mfpf_hog_box_finder_builder());
   vsl_add_to_binary_loader(mfpf_edge_finder_builder());
   vsl_add_to_binary_loader(mfpf_norm_corr1d_builder());
   vsl_add_to_binary_loader(mfpf_norm_corr2d_builder());
   vsl_add_to_binary_loader(mfpf_profile_pdf_builder());
   vsl_add_to_binary_loader(mfpf_region_pdf_builder());
   vsl_add_to_binary_loader(mfpf_region_finder_builder());
+  vsl_add_to_binary_loader(mfpf_hog_box_finder_builder());
 
   mbl_cloneables_factory<mfpf_point_finder>::add(mfpf_edge_finder());
   mbl_cloneables_factory<mfpf_point_finder>::add(mfpf_norm_corr1d());
@@ -45,12 +49,14 @@ void mfpf_add_all_loaders()
   mbl_cloneables_factory<mfpf_point_finder>::add(mfpf_profile_pdf());
   mbl_cloneables_factory<mfpf_point_finder>::add(mfpf_region_pdf());
   mbl_cloneables_factory<mfpf_point_finder>::add(mfpf_region_finder());
+  mbl_cloneables_factory<mfpf_point_finder>::add(mfpf_hog_box_finder());
   vsl_add_to_binary_loader(mfpf_edge_finder());
   vsl_add_to_binary_loader(mfpf_norm_corr1d());
   vsl_add_to_binary_loader(mfpf_norm_corr2d());
   vsl_add_to_binary_loader(mfpf_profile_pdf());
   vsl_add_to_binary_loader(mfpf_region_pdf());
   vsl_add_to_binary_loader(mfpf_region_finder());
+  vsl_add_to_binary_loader(mfpf_hog_box_finder());
 
   mbl_cloneables_factory<mfpf_vec_cost_builder>::add(mfpf_sad_vec_cost_builder());
   mbl_cloneables_factory<mfpf_vec_cost_builder>::add(mfpf_ssd_vec_cost_builder());
