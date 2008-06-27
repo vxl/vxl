630eb9d536b2092cbad2c74b9baf172b330068f1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-06-27 03:39:29 -0400

    removed unused #include <utility>

--- contrib/brl/bseg/bvxm/pro/bvxm_roc_process.cxx
+++ contrib/brl/bseg/bvxm/pro/bvxm_roc_process.cxx
@@ -4,7 +4,6 @@
 #include <bprb/bprb_parameters.h>
 
 #include <vcl_fstream.h>
-#include <vcl_utility.h>
 #include <vil/vil_load.h>
 #include <vil/vil_save.h>
 #include <vil/vil_convert.h>
@@ -12,8 +11,6 @@
 #include <vul/vul_file.h>
 #include <vul/vul_file_iterator.h>
 #include <vcl_vector.h>
-#include <vcl_map.h>
-#include <vcl_utility.h>
 #include <vcl_sstream.h>
 
 bvxm_roc_process::bvxm_roc_process()
@@ -21,7 +18,7 @@ bvxm_roc_process::bvxm_roc_process()
   //process takes 3 inputs
   //input[0]: True changes directory
   //input[1]: Observed changes directory
-  //input[2]: The ROC ouput filename
+  //input[2]: The ROC output filename
 
   input_data_.resize(3,brdb_value_sptr(0));
   input_types_.resize(3);
@@ -37,21 +34,20 @@ bvxm_roc_process::bvxm_roc_process()
 
 bool bvxm_roc_process::execute()
 {
-
   // Sanity check
-  if(!this->verify_inputs())
+  if (!this->verify_inputs())
     return false;
 
   //get the inputs
-  brdb_value_t<vcl_string>* input0 = 
+  brdb_value_t<vcl_string>* input0 =
     static_cast<brdb_value_t<vcl_string>* >(input_data_[0].ptr());
   vcl_string true_changes_dir= input0->value();
 
-  brdb_value_t<vcl_string>* input1 = 
+  brdb_value_t<vcl_string>* input1 =
     static_cast<brdb_value_t<vcl_string>* >(input_data_[1].ptr());
   vcl_string detected_changes_dir = input1->value();
 
-  brdb_value_t<vcl_string>* input2 = 
+  brdb_value_t<vcl_string>* input2 =
     static_cast<brdb_value_t<vcl_string>* >(input_data_[2].ptr());
   vcl_string roc_filename = input2->value();
   vcl_ofstream roc_stream(roc_filename.c_str());
@@ -59,23 +55,22 @@ bool bvxm_roc_process::execute()
 
   //iterate through changes dir, get all image names that we need to process
   vcl_vector<vcl_string> names;
-  for( vul_file_iterator fit = (true_changes_dir + "/*.*"); fit; ++fit ){
-
-    if( vul_file::is_directory(fit()) )
+  for ( vul_file_iterator fit = (true_changes_dir + "/*.*"); fit; ++fit )
+  {
+    if ( vul_file::is_directory(fit()) )
       continue;
     vcl_string image_name = fit();
 
     vcl_string file_ext = vul_file::extension(fit());
 
     //get extension
-    if(( file_ext !=  ".png") & ( file_ext !=  ".jpg") & ( file_ext !=  ".tiff"))
+    if (( file_ext !=  ".png") & ( file_ext !=  ".jpg") & ( file_ext !=  ".tiff"))
     {
-      vcl_cerr << "error: file extension not supported" <<vcl_endl;
+      vcl_cerr << "error: file extension not supported\n";
       continue;
     }
 
     names.push_back(vul_file::strip_extension(vul_file::basename(fit())));
-
   }
 
   //define threshold interval
@@ -94,24 +89,24 @@ bool bvxm_roc_process::execute()
 
     for (vcl_vector<vcl_string>::iterator vit = names.begin(); vit !=names.end(); vit++)
     {
-      //Read image containing probabilistic changes       
-      vcl_string prob_image = detected_changes_dir + "\\" + *vit + ".tiff";   
-      vil_image_view<float> prob_change = vil_load( prob_image.c_str() ); 
+      //Read image containing probabilistic changes
+      vcl_string prob_image = detected_changes_dir + "\\" + *vit + ".tiff";
+      vil_image_view<float> prob_change = vil_load( prob_image.c_str() );
 
       unsigned image_height = prob_change.nj();
       unsigned image_width = prob_change.ni();
 
       //Read image containing true changes
       vcl_string true_change_file =  true_changes_dir + "\\" + *vit + ".png";
-      vil_image_view<vxl_byte> true_change = 
-        vil_convert_to_grey_using_average( vil_load(true_change_file.c_str())); 
+      vil_image_view<vxl_byte> true_change =
+        vil_convert_to_grey_using_average( vil_load(true_change_file.c_str()));
 
 
       //iterate through the image
       vil_image_view<vxl_byte> detected_change(image_width, image_height);
-      for( unsigned x = 0; x < image_width; x++ ){
-        for( unsigned y = 0; y < image_height; y++ ){
-          if( prob_change(x,y) < (thresh[n] * 20.0 )) 
+      for ( unsigned x = 0; x < image_width; x++ ){
+        for ( unsigned y = 0; y < image_height; y++ ){
+          if ( prob_change(x,y) < (thresh[n] * 20.0 ))
             detected_change(x,y) = 255;
           else
             detected_change(x,y) = 0;
@@ -119,8 +114,8 @@ bool bvxm_roc_process::execute()
       }
 
       //Compare thesholded images to the model
-      for( unsigned i = 0; i < true_change.ni(); i++ ){
-        for( unsigned j = 0; j < true_change.nj()-1; j++ ){
+      for ( unsigned i = 0; i < true_change.ni(); i++ ){
+        for ( unsigned j = 0; j < true_change.nj()-1; j++ ){
          //hiafa
           /* unsigned thresh_i = i - 164;
           unsigned thresh_j =j - 424;*/
@@ -130,25 +125,24 @@ bool bvxm_roc_process::execute()
           unsigned thresh_i = i;
           unsigned thresh_j =j;
 
-          //count changes 
+          //count changes
           if ( thresh_i < detected_change.ni() && thresh_j < detected_change.nj())
           {
-            if(( true_change(i,j) != 255 )&& ( true_change(i,j) != 0 ))continue;
+            if (( true_change(i,j) != 255 )&& ( true_change(i,j) != 0 ))continue;
 
-            ////for debugging
-            //if(true_change(i,j) ==255)
-            //  vcl_cout<<i<<' '<< j<<' ' << thresh_i<< ' ' <<thresh_j<< '\n';
-
-            if( true_change(i,j) > 200  && detected_change(thresh_i, thresh_j) == 255 )
+#ifdef DEBUG //for debugging
+            if (true_change(i,j) == 255)
+              vcl_cout<<i<<' '<< j<<' ' << thresh_i<< ' ' <<thresh_j<< '\n';
+#endif // DEBUG
+            if ( true_change(i,j) > 200  && detected_change(thresh_i, thresh_j) == 255 )
               change_marked_change++;
-            if( true_change(i,j) > 200 && detected_change(thresh_i, thresh_j) == 0 )
+            if ( true_change(i,j) > 200 && detected_change(thresh_i, thresh_j) == 0 )
               change_marked_nonchange++;
-            if( true_change(i,j) < 50 && detected_change(thresh_i, thresh_j) == 255 )
+            if ( true_change(i,j) < 50 && detected_change(thresh_i, thresh_j) == 255 )
               nonchange_marked_change++;
-            if( true_change(i,j) < 50 && detected_change(thresh_i, thresh_j) == 0 )
+            if ( true_change(i,j) < 50 && detected_change(thresh_i, thresh_j) == 0 )
               nonchange_marked_nonchange++;
           }
-
         }
       }
 
@@ -164,14 +158,11 @@ bool bvxm_roc_process::execute()
       (double)(change_marked_change+change_marked_nonchange);
     double percent_nonchange_marked_change = (double)nonchange_marked_change/
       (double)(nonchange_marked_change+nonchange_marked_nonchange);
-    roc_stream << percent_change_marked_change << "\t" <<
+    roc_stream << percent_change_marked_change << '\t' <<
       percent_nonchange_marked_change << '\n';
     vcl_cerr << '.';
   }
   vcl_cerr << '\n';
   return true;
 }
-
-
-
 
