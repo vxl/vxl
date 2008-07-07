b28f1efcf1f07c44c8b2eb365cb730e6ec810c45
Vishal Jain <jainv@users.sourceforge.net>
2008-07-07 09:50:18 -0400

    smart ptr intantiantion file for bvxm_multi_scale_world

---
+++ contrib/brl/bseg/bvxm/Templates/vbl_io_smart_ptr+bvxm_multi_scale_voxel_world-.cxx
@@ -0,0 +1,4 @@
+#include <multiscale/bvxm_multi_scale_voxel_world.h>
+#include <vbl/io/vbl_io_smart_ptr.txx>
+
+VBL_IO_SMART_PTR_INSTANTIATE(bvxm_multi_scale_voxel_world);
---
+++ contrib/brl/bseg/bvxm/Templates/vbl_smart_ptr+bvxm_multi_scale_voxel_world-.cxx
@@ -0,0 +1,5 @@
+#include <multiscale/bvxm_multi_scale_voxel_world.h>
+#include <vbl/vbl_smart_ptr.txx>
+
+VBL_SMART_PTR_INSTANTIATE(bvxm_multi_scale_voxel_world);
+
