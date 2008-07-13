7f58446c2d8f9bd3bff56a5527fe88d5441fc8dd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-07-13 07:16:40 -0400

    removed unused "#include <fstream>"

--- contrib/brl/bbas/bwm/video/exe/bwm_extract_snippets.cxx
+++ contrib/brl/bbas/bwm/video/exe/bwm_extract_snippets.cxx
@@ -4,8 +4,6 @@
 #include <vcl_string.h>
 #include <vul/vul_file.h>
 #include <vul/vul_arg.h>
-#include <vcl_fstream.h>
-#include <vcl_string.h>
 #include <vcl_vector.h>
 #include <vsl/vsl_binary_io.h>
 #include <vsl/vsl_vector_io.h>
@@ -14,7 +12,7 @@
 #include <vil/vil_load.h>
 #include <vil/vil_save.h>
 #include <vsol/vsol_polygon_2d.h>
-#include <vsol/vsol_polygon_2d_sptr.h>
+#include <vsol/vsol_spatial_object_2d_sptr.h>
 #include <vsol/vsol_box_2d.h>
 #include <vsol/vsol_box_2d_sptr.h>
 
@@ -40,81 +38,81 @@ static bool extract_snippets(vcl_string const& spatial_object_path,
     return false;
 
   vsl_b_ifstream istr(spatial_object_path);
-  if(!istr){
+  if (!istr) {
     vcl_cerr << "Failed to open spatial object input stream "
              << spatial_object_path << vcl_endl;
     return false;
   }
   vcl_vector<vsol_spatial_object_2d_sptr> sos;
-  
+
   vsl_b_read(istr, sos);
-  
-  if(!sos.size())
-   {
+
+  if (!sos.size())
+  {
     vcl_cerr << "no spatial objects in file "
              << spatial_object_path << vcl_endl;
     return false;
-  } 
+  }
 
   vil_image_resource_sptr image = vil_load_image_resource(image_path.c_str());
-  if(!image)
-   {
+  if (!image)
+  {
     vcl_cerr << "image failed to load "
              << image_path << vcl_endl;
     return false;
-  } 
-    
+  }
+
   if (!vul_file::exists(snippet_output_dir))
     if (!vul_file::make_directory_path(snippet_output_dir))
       return false;
 
   unsigned ni = 0, nj = 0;
-  for(vcl_vector<vsol_spatial_object_2d_sptr>::iterator soit = sos.begin();
-      soit != sos.end(); ++soit)
-    {
-      vsol_spatial_object_2d_sptr so = (*soit);
-      if(!so)
-        continue;
-      vsol_box_2d_sptr bb = so->get_bounding_box();
-      if(!bb)
-        continue;
-      double w = bb->width(), h = bb->height();
-      unsigned iw = static_cast<unsigned>(w), ih = static_cast<unsigned>(h);
-      if(iw>ni) ni = iw;
-      if(ih>nj) nj = ih;
-    }
-  if(!ni || !nj)
-   {
-    vcl_cerr << "degenerate snippet " << vcl_endl;
+  for (vcl_vector<vsol_spatial_object_2d_sptr>::iterator soit = sos.begin();
+       soit != sos.end(); ++soit)
+  {
+    vsol_spatial_object_2d_sptr so = (*soit);
+    if (!so)
+      continue;
+    vsol_box_2d_sptr bb = so->get_bounding_box();
+    if (!bb)
+      continue;
+    double w = bb->width(), h = bb->height();
+    unsigned iw = static_cast<unsigned>(w), ih = static_cast<unsigned>(h);
+    if (iw>ni) ni = iw;
+    if (ih>nj) nj = ih;
+  }
+  if (!ni || !nj)
+  {
+    vcl_cerr << "degenerate snippet\n";
     return false;
-   } 
+  }
 
-   unsigned index = 0;
-  for(vcl_vector<vsol_spatial_object_2d_sptr>::iterator soit = sos.begin();
-      soit != sos.end(); ++soit, ++index)
+  unsigned index = 0;
+  for (vcl_vector<vsol_spatial_object_2d_sptr>::iterator soit = sos.begin();
+       soit != sos.end(); ++soit, ++index)
+  {
+    vsol_spatial_object_2d_sptr so = (*soit);
+    if (!so)
+      continue;
+    vsol_box_2d_sptr bb = so->get_bounding_box();
+    if (!bb)
+      continue;
+    double xmin = bb->get_min_x(), xmax = bb->get_max_x();
+    double ymin = bb->get_min_y(), ymax = bb->get_max_y();
+    double cx = (xmax+xmin)/2, cy = (ymax+ymin)/2;
+    unsigned i0 = static_cast<unsigned>(cx-ni/2);
+    unsigned j0 = static_cast<unsigned>(cy-nj/2);
+    vil_image_view_base_sptr view = image->get_view(i0, ni, j0, nj);
+    // create the output snippet file path
+    vcl_string path = image_sequence_path(snippet_output_dir,
+                                          "snippet", index,
+                                          "tiff");
+    if (!vil_save(*view, path.c_str()))
     {
-      vsol_spatial_object_2d_sptr so = (*soit);
-      if(!so)
-        continue;
-      vsol_box_2d_sptr bb = so->get_bounding_box();
-      if(!bb)
-        continue;
-      double xmin = bb->get_min_x(), xmax = bb->get_max_x();
-      double ymin = bb->get_min_y(), ymax = bb->get_max_y();
-      double cx = (xmax+xmin)/2, cy = (ymax+ymin)/2;
-      unsigned i0 = static_cast<unsigned>(cx-ni/2);
-      unsigned j0 = static_cast<unsigned>(cy-nj/2);
-      vil_image_view_base_sptr view = image->get_view(i0, ni, j0, nj);
-      // create the output snippet file path
-      vcl_string path = image_sequence_path(snippet_output_dir,
-                                            "snippet", index,
-                                            "tiff");      
-      if(!vil_save(*view, path.c_str()))
-        {
-          vcl_cerr << "snippet not saved to " << path << vcl_endl;
-          return false;
-        }
+      vcl_cerr << "snippet not saved to " << path << vcl_endl;
+      return false;
     }
+  }
   return true;
 }
 
@@ -124,13 +122,12 @@ int main(int argc, char** argv)
   vul_arg_info_list arglist;
   vul_arg<vcl_string> spatial_object_path(arglist, "-spatial_object_path",
                                           "spatial object path", "");
-  vul_arg<vcl_string> image_path(arglist, "-image_path",
-                                          "image path", "");
+  vul_arg<vcl_string> image_path(arglist, "-image_path", "image path", "");
   vul_arg<vcl_string> snippet_output_dir(arglist, "-snippet_output_dir",
-                                       "snippet output file directory", "");
+                                         "snippet output file directory", "");
   arglist.parse(argc, argv, true);
 
-  if (!extract_snippets(spatial_object_path(), image_path(), 
+  if (!extract_snippets(spatial_object_path(), image_path(),
                         snippet_output_dir()))
     return -1;
   return 0;
