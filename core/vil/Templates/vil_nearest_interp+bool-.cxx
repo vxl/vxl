15fef294265c95f328d4cd38eb53275e7c4b9763
Ian Scott <scottim@users.sourceforge.net>
2008-07-14 07:36:50 -0400

    Remove unused files

--- core/vil/Templates/vil_nearest_interp+bool-.cxx
+++
@@ -1,2 +0,0 @@
-#include <vil/vil_nearest_interp.txx>
-VIL_NEAREST_INTERP_INSTANTIATE(bool);
--- core/vil/Templates/vil_nearest_interp+byte-.cxx
+++
@@ -1,3 +0,0 @@
-#include <vil/vil_nearest_interp.txx>
-#include <vxl_config.h>
-VIL_NEAREST_INTERP_INSTANTIATE(vxl_byte);
