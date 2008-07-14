146aeb76c946de7bab376f9adfce8579c21847fe
Vishal Jain <jainv@users.sourceforge.net>
2008-07-14 18:35:41 -0400

    removed header file

--- contrib/brl/bseg/bvxm/pro/Templates/brdb_value_t+bvxm_multi_scale_voxel_world-.cxx
+++ contrib/brl/bseg/bvxm/pro/Templates/brdb_value_t+bvxm_multi_scale_voxel_world-.cxx
@@ -1,6 +1,6 @@
 #include <brdb/brdb_value.txx>
 
 #include <vbl/io/vbl_io_smart_ptr.h>
-#include <multiscale/bvxm_multi_scale_voxel_world.h>
+//#include <multiscale/bvxm_multi_scale_voxel_world.h>
 
 //BRDB_VALUE_INSTANTIATE(bvxm_multi_scale_voxel_world_sptr, "bvxm_multi_scale_voxel_world_sptr");
