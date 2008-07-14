5b8758da813a6e8217ccf6115588619ee2e800b5
Vishal Jain <jainv@users.sourceforge.net>
2008-07-14 18:36:40 -0400

    removed the template instantiation of brdb_value_t<bvxm_multi_scale_world>

--- contrib/brl/bseg/bvxm/pro/Templates/brdb_value_t+bvxm_multi_scale_voxel_world-.cxx
+++
@@ -1,6 +0,0 @@
-#include <brdb/brdb_value.txx>
-
-#include <vbl/io/vbl_io_smart_ptr.h>
-//#include <multiscale/bvxm_multi_scale_voxel_world.h>
-
-//BRDB_VALUE_INSTANTIATE(bvxm_multi_scale_voxel_world_sptr, "bvxm_multi_scale_voxel_world_sptr");
