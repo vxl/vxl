3eab0ba0f16e0780fd22e0051f35e17c1f5c7384
Vishal Jain <jainv@users.sourceforge.net>
2008-07-14 15:47:13 -0400

    *** empty log message ***

--- contrib/brl/bseg/bvxm/pro/Templates/brdb_value_t+bvxm_multi_scale_voxel_world-.cxx
+++ contrib/brl/bseg/bvxm/pro/Templates/brdb_value_t+bvxm_multi_scale_voxel_world-.cxx
@@ -3,4 +3,4 @@
 #include <vbl/io/vbl_io_smart_ptr.h>
 #include <multiscale/bvxm_multi_scale_voxel_world.h>
 
-BRDB_VALUE_INSTANTIATE(bvxm_multi_scale_voxel_world_sptr, "bvxm_multi_scale_voxel_world_sptr");
+//BRDB_VALUE_INSTANTIATE(bvxm_multi_scale_voxel_world_sptr, "bvxm_multi_scale_voxel_world_sptr");
