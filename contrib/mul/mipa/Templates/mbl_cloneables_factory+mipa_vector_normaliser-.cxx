8b7ba9a7ba2462fdff985952645b3d971d296bc9
Martin G. Roberts <mgroberts@users.sourceforge.net>
2008-07-14 11:46:22 -0400

    Added templates for binary loaders of mipa_vector_normalisers

---
+++ contrib/mul/mipa/Templates/mbl_cloneables_factory+mipa_vector_normaliser-.cxx
@@ -0,0 +1,3 @@
+#include <mbl/mbl_cloneables_factory.h>
+#include <mipa/mipa_vector_normaliser.h>
+MBL_CLONEABLES_FACTORY_INSTANTIATE(mipa_vector_normaliser);
---
+++ contrib/mul/mipa/Templates/vsl_binary_loader+mipa_vector_normaliser-.cxx
@@ -0,0 +1,3 @@
+#include <vsl/vsl_binary_loader.txx>
+#include <mipa/mipa_vector_normaliser.h>
+VSL_BINARY_LOADER_INSTANTIATE(mipa_vector_normaliser);
