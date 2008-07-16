c05338f8a0c274069b31f0528964ded34acb551a
Matthew J. Leotta <mleotta@users.sourceforge.net>
2008-07-16 10:14:48 -0400

    corrected a TRY_RUN statement that failed on OS X

--- config/cmake/config/vxl_config_macros.cmake
+++ config/cmake/config/vxl_config_macros.cmake
@@ -295,12 +295,13 @@ MACRO( DETERMINE_TYPE VAR INTEGRAL_TYPE SIZE TYPE_LIST )
       # line to avoid issues with spaces. (In "long double", for
       # example)
       WRITE_FILE( ${CMAKE_BINARY_DIR}/CMakeTmp/config.h "#define THE_TYPE ${TYPE}\n#define THE_SIZE ${SIZE}\n#define INTEGRAL_TYPE ${INTEGRAL_TYPE}" )
-      SET( MACRO_DETERMINE_TYPE_FLAGS "-DVXL_HAS_TYPE_OF_SIZE -I\"${CMAKE_BINARY_DIR}/CMakeTmp\"" )
+      SET( MACRO_DETERMINE_TYPE_FLAGS "-DVXL_HAS_TYPE_OF_SIZE" )
       MESSAGE( STATUS "${MSG} [Checking ${TYPE}...]" )
       TRY_RUN( RUN_RESULT COMPILE_RESULT
             ${CMAKE_BINARY_DIR}
             ${vxl_config_SOURCE_DIR}/vxl_platform_tests.cxx
             CMAKE_FLAGS -DCOMPILE_DEFINITIONS:STRING=${MACRO_DETERMINE_TYPE_FLAGS}
+                        -DINCLUDE_DIRECTORIES:STRING=${CMAKE_BINARY_DIR}/CMakeTmp
                         -DLINK_LIBRARIES:STRING=${CMAKE_REQUIRED_LIBRARIES}
             OUTPUT_VARIABLE OUTPUT )
       IF( COMPILE_RESULT )
