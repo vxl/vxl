b71ea278f8c11e7e0a1204983f17df32b5ec4099
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2008-07-29 11:13:31 -0400

    added a random mode for snippet extraction

--- contrib/brl/bbas/bwm/video/exe/bwm_extract_snippets.cxx
+++ contrib/brl/bbas/bwm/video/exe/bwm_extract_snippets.cxx
@@ -1,6 +1,7 @@
 #include <vcl_iostream.h>
 #include <vcl_iomanip.h>
 #include <vcl_sstream.h>
+#include <vnl/vnl_random.h>
 #include <vcl_string.h>
 #include <vul/vul_file.h>
 #include <vul/vul_arg.h>
@@ -12,9 +13,10 @@
 #include <vil/vil_load.h>
 #include <vil/vil_save.h>
 #include <vsol/vsol_polygon_2d.h>
+#include <vsol/vsol_point_2d.h>
 #include <vsol/vsol_spatial_object_2d_sptr.h>
 #include <vsol/vsol_box_2d.h>
-#include <vsol/vsol_box_2d_sptr.h>
+
 
 static vcl_string image_sequence_path(vcl_string const& dir_path,
                                       vcl_string const& sname,
@@ -30,37 +32,79 @@ static vcl_string image_sequence_path(vcl_string const& dir_path,
 
 static bool extract_snippets(vcl_string const& spatial_object_path,
                              vcl_string const& image_path,
-                             vcl_string const& snippet_output_dir)
+                             vcl_string const& snippet_output_dir,
+                             vcl_string const& mode,
+                             unsigned num_objects)
 {
-  if (spatial_object_path == ""||
-      image_path == ""||
-      snippet_output_dir == "")
-    return false;
-
-  vsl_b_ifstream istr(spatial_object_path);
-  if (!istr) {
-    vcl_cerr << "Failed to open spatial object input stream "
-             << spatial_object_path << vcl_endl;
-    return false;
-  }
-  vcl_vector<vsol_spatial_object_2d_sptr> sos;
-
-  vsl_b_read(istr, sos);
-
-  if (!sos.size())
+  if (image_path == ""||
+    snippet_output_dir == "")
   {
-    vcl_cerr << "no spatial objects in file "
-             << spatial_object_path << vcl_endl;
+    vcl_cerr<< " image path or output path arguments are empty" << vcl_endl;
     return false;
   }
 
+  //get the image
   vil_image_resource_sptr image = vil_load_image_resource(image_path.c_str());
   if (!image)
   {
     vcl_cerr << "image failed to load "
-             << image_path << vcl_endl;
+      << image_path << vcl_endl;
     return false;
   }
+
+
+  //get the 2d spatial objects
+  vcl_vector<vsol_spatial_object_2d_sptr> sos;
+
+  if (mode == "binary")
+  {
+    if (spatial_object_path == "")
+    {
+      vcl_cerr << "Spatial object path is empty" << vcl_endl;
+      return false;
+    }
+    vsl_b_ifstream istr(spatial_object_path);
+    if (!istr) {
+      vcl_cerr << "Failed to open spatial object input stream "
+        << spatial_object_path << vcl_endl;
+      return false;
+    }
+
+    vsl_b_read(istr, sos);
+
+    if (!sos.size())
+    {
+      vcl_cerr << "no spatial objects in file "
+        << spatial_object_path << vcl_endl;
+      return false;
+    }
+
+  }
+  else if(mode == "random") 
+  {
+    vnl_random rand;
+    for(unsigned i=0; i<num_objects; i++)
+    {
+      double v1 = rand.drand32(image->ni());
+      double v2 = rand.drand32(image->nj());
+
+      // This case supports only 2d squares
+      vsol_point_2d_sptr p0 = new vsol_point_2d(v1, v2);
+      vsol_point_2d_sptr p1 = new vsol_point_2d(v1+10, v2);
+      vsol_point_2d_sptr p2 = new vsol_point_2d(v1+10, v2+10);
+      vsol_point_2d_sptr p3 = new vsol_point_2d(v1, v2+10);
+      vcl_vector<vsol_point_2d_sptr> vertices;
+      vertices.push_back(p0);
+      vertices.push_back(p1);
+      vertices.push_back(p2);
+      vertices.push_back(p3);
+      vsol_spatial_object_2d_sptr so = new vsol_polygon_2d(vertices);
+      sos.push_back(so);
+    }
+  }
+  else
+    vcl_cout << "unsupported spatialobject generation type" << vcl_endl;
+
 
   if (!vul_file::exists(snippet_output_dir))
     if (!vul_file::make_directory_path(snippet_output_dir))
@@ -68,7 +112,7 @@ static bool extract_snippets(vcl_string const& spatial_object_path,
 
   unsigned ni = 0, nj = 0;
   for (vcl_vector<vsol_spatial_object_2d_sptr>::iterator soit = sos.begin();
-       soit != sos.end(); ++soit)
+    soit != sos.end(); ++soit)
   {
     vsol_spatial_object_2d_sptr so = (*soit);
     if (!so)
@@ -89,7 +133,7 @@ static bool extract_snippets(vcl_string const& spatial_object_path,
 
   unsigned index = 0;
   for (vcl_vector<vsol_spatial_object_2d_sptr>::iterator soit = sos.begin();
-       soit != sos.end(); ++soit, ++index)
+    soit != sos.end(); ++soit, ++index)
   {
     vsol_spatial_object_2d_sptr so = (*soit);
     if (!so)
@@ -105,8 +149,8 @@ static bool extract_snippets(vcl_string const& spatial_object_path,
     vil_image_view_base_sptr view = image->get_view(i0, ni, j0, nj);
     // create the output snippet file path
     vcl_string path = image_sequence_path(snippet_output_dir,
-                                          "snippet", index,
-                                          "tiff");
+      "snippet", index,
+      "tiff");
     if (!vil_save(*view, path.c_str()))
     {
       vcl_cerr << "snippet not saved to " << path << vcl_endl;
@@ -120,15 +164,32 @@ int main(int argc, char** argv)
 {
   vsl_add_to_binary_loader(vsol_polygon_2d());
   vul_arg_info_list arglist;
+
+  //Input arguments
+
+  //1. -spatial_object_mode : binary (loads spatial objects from binary file)
+  //          random (generate a given number of random patches)
+  //2. -spatial_object_path(Optinal) : must be provided if the spatial objects
+  //                                  are loaded from file
+  //3. -snippet_output_dir: Output filepath
+  //4. -number_sos(Optional): Number of spatial objects. Must be specified in
+  //                          random mode   
+
+  vul_arg<vcl_string> spatial_object_mode(arglist, "-spatial_object_mode",
+    "spatial object mode", "");
   vul_arg<vcl_string> spatial_object_path(arglist, "-spatial_object_path",
-                                          "spatial object path", "");
+    "spatial object path", "");
   vul_arg<vcl_string> image_path(arglist, "-image_path", "image path", "");
   vul_arg<vcl_string> snippet_output_dir(arglist, "-snippet_output_dir",
-                                         "snippet output file directory", "");
+    "snippet output file directory", "");
+  vul_arg<unsigned> number_objects(arglist, "-number_objects",
+    "number objects", 0);
   arglist.parse(argc, argv, true);
 
+  //isa parse the inputs correctly....
   if (!extract_snippets(spatial_object_path(), image_path(),
-                        snippet_output_dir()))
+    snippet_output_dir(), spatial_object_mode(),
+    number_objects()))
     return -1;
   return 0;
 }
