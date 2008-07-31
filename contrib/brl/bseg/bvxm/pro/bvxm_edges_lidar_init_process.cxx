e97666860a0c4a19929280f6dfcb23b47b3d5537
Ibrahim Eden <ibrahimeden@users.sourceforge.net>
2008-07-31 14:57:55 -0400

    *** empty log message ***

--- contrib/brl/bseg/bvxm/pro/bvxm_edges_lidar_init_process.cxx
+++ contrib/brl/bseg/bvxm/pro/bvxm_edges_lidar_init_process.cxx
@@ -54,12 +54,28 @@ bool bvxm_edges_lidar_init_process::execute()
   // image
   brdb_value_t<vil_image_view_base_sptr>* input0 = static_cast<brdb_value_t<vil_image_view_base_sptr>* >(input_data_[0].ptr());
   vil_image_view_base_sptr image_first_return_base = input0->value();
-  vil_image_view<vxl_byte> image_first_return(image_first_return_base);
+  vil_pixel_format input_pixel_format = image_first_return_base->pixel_format();
 
   brdb_value_t<vil_image_view_base_sptr>* input1 = static_cast<brdb_value_t<vil_image_view_base_sptr>* >(input_data_[1].ptr());
   vil_image_view_base_sptr image_second_return_base = input1->value();
-  vil_image_view<vxl_byte> image_second_return(image_second_return_base);
 
+  bool is_input_float = true;
+  if(input_pixel_format == VIL_PIXEL_FORMAT_BYTE){
+    is_input_float = false;
+  }
+  else if(input_pixel_format == VIL_PIXEL_FORMAT_FLOAT){
+    is_input_float = true;
+  }
+  else{
+    vcl_cout << "this process only accepts vxl_float or vxl_byte pixel formats\n";
+    return false;
+  }
+
+
+  vil_image_view<float> image_first_return = *(vil_convert_cast(float(), image_first_return_base));
+  vil_image_view<float> image_second_return = *(vil_convert_cast(float(), image_second_return_base));
+
+  // todo : check if this is given in meters
   double threshold_edge_difference;
   if (!parameters()->get_value("threshold_edge_difference", threshold_edge_difference)) {
       vcl_cout << "problems in retrieving parameters\n";
@@ -69,26 +85,26 @@ bool bvxm_edges_lidar_init_process::execute()
   unsigned ni = image_first_return.ni();
   unsigned nj = image_first_return.nj();
 
-  vil_image_view<vxl_byte> edges_lidar(ni,nj);
-  edges_lidar.fill(0);
+  vil_image_view<float> edges_lidar(ni,nj);
+  edges_lidar.fill(0.0f);
   vil_image_view<vxl_byte> edges_mask(ni,nj);
   edges_mask.fill(0);
-  vil_image_view<vxl_byte> edges_difference(ni,nj);
-  edges_difference.fill(0);
+  vil_image_view<float> edges_prob(ni,nj);
+  edges_prob.fill(0.0f);
 
   for(unsigned i=0; i<ni; i++){
     for(unsigned j=0; j<nj; j++){
-      double curr_difference = image_first_return(i,j)-image_second_return(i,j);
+      float curr_difference = image_first_return(i,j)-image_second_return(i,j);
       if(curr_difference>threshold_edge_difference){
         edges_lidar(i,j) = image_first_return(i,j);
         edges_mask(i,j) = 255;
-        edges_difference(i,j) = (vxl_byte)curr_difference;
+        edges_prob(i,j) = curr_difference;
       }
     }
   }
 
   // store image output
-  brdb_value_sptr output0 = new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<vxl_byte>(edges_lidar));
+  brdb_value_sptr output0 = new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<float>(edges_lidar));
   output_data_[0] = output0;
   
   // store image output
@@ -96,7 +112,7 @@ bool bvxm_edges_lidar_init_process::execute()
   output_data_[1] = output1;
   
   // store image output
-  brdb_value_sptr output2 = new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<vxl_byte>(edges_difference));
+  brdb_value_sptr output2 = new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<float>(edges_prob));
   output_data_[2] = output2;
 
   return true;
