930e947d1f2128bb6f0a70853745cba5e67f2500
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-08-05 11:42:23 -0400

    added EOLN at EOF -- this avoids a compiler warning

--- contrib/mul/vimt3d/vimt3d_save.cxx
+++ contrib/mul/vimt3d/vimt3d_save.cxx
@@ -1,15 +1,15 @@
 // This is mul/vimt3d/vimt3d_save.cxx
+#include "vimt3d_save.h"
 //:
 // \file
 // \author Ian Scott
 
-#include "vimt3d_save.h"
 #include <mbl/mbl_log.h>
 #include <vil3d/vil3d_image_resource.h>
 #include <vil3d/vil3d_new.h>
 #include <vil3d/vil3d_save.h>
 #include <vimt3d/vimt3d_transform_3d.h>
-#include <vimt3d/vimt3d_image_3d_of.h>
+#include <vimt3d/vimt3d_image_3d.h>
 #include <vimt3d/vimt3d_vil3d_v3i.h>
 
 
@@ -22,7 +22,7 @@ static mbl_logger& logger()
 //: Create a transform from the properties of image resource.
 // The transform will be from world co-ordinates in metres to image co-ordinates (or mm if requested).
 void vimt3d_save_transform(vil3d_image_resource_sptr &ir,
-  const vimt3d_transform_3d& trans, bool use_millimetres)
+                           const vimt3d_transform_3d& trans, bool use_millimetres)
 {
   if (dynamic_cast<vimt3d_vil3d_v3i_image *>(ir.ptr()))
   {
@@ -38,7 +38,7 @@ void vimt3d_save_transform(vil3d_image_resource_sptr &ir,
     tr.set_zoom_only (1000.0*vox_per_mm.x(),
                       1000.0*vox_per_mm.y(),
                       1000.0*vox_per_mm.z(), tx,ty,tz );
-    
+
     static_cast<vimt3d_vil3d_v3i_image &>(*ir).set_world2im(tr);
   }
   else
@@ -48,10 +48,8 @@ void vimt3d_save_transform(vil3d_image_resource_sptr &ir,
                                          vgl_vector_3d<double> (1.0, 1.0, 1.0));
     if (!ir->set_voxel_size(float(dp.x()),float(dp.y()),float(dp.z())))
       MBL_LOG(WARN, logger(), "sm3d_image_io_3d::saveImage(): Unable to include voxel sizes:"
-        <<dp.x()<<","<<dp.y()<<","<<dp.z() );
+              <<dp.x()<<','<<dp.y()<<','<<dp.z() );
   }
-
-
 }
 
 
