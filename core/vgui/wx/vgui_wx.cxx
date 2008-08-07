298cf8f571a1cc88c70a67e928082ac80baf4fe1
Matthew J. Leotta <mleotta@users.sourceforge.net>
2008-08-07 14:56:07 -0400

    Added a class containing static function calls

---
+++ core/vgui/wx/vgui_wx.cxx
@@ -0,0 +1,18 @@
+// This is core/vgui/wx/vgui_wx.cxx
+
+#include "vgui_wx.h"
+#include <wx/xrc/xmlres.h>
+
+// Includes for XML Resource Handlers
+#include "vgui_wx_adaptorXmlHandler.h"
+#include "wxSliderPanelXmlHandler.h"
+
+
+//: Initialize all vgui_wx XML Resource Handlers
+// Call this right after you call wxXmlResource::Get()->InitAllHandlers();
+void vgui_wx::InitVguiHandlers()
+{
+  wxXmlResource::Get()->AddHandler(new vgui_wx_adaptorXmlHandler);
+  wxXmlResource::Get()->AddHandler(new wxSliderPanelXmlHandler);
+  // Add new resource handlers here
+}
---
+++ core/vgui/wx/vgui_wx.h
@@ -0,0 +1,28 @@
+// This is core/vgui/wx/vgui_wx.h
+#ifndef vgui_wx_h_
+#define vgui_wx_h_
+//:
+// \file
+// \brief  vgui_wx static methods
+// \author Matt Leotta
+// \date   August 7, 2008
+//
+
+
+// include impl stuff so the user doesn't have to
+#include <vgui/impl/wx/vgui_wx_adaptor.h>
+#include <vgui/impl/wx/vgui_wx_statusbar.h>
+
+
+//: vgui_wx static methods
+class vgui_wx
+{
+public:
+  //: Initialize all vgui_wx XML Resource Handlers
+  // Call this right after you call wxXmlResource::Get()->InitAllHandlers();
+  static void InitVguiHandlers();
+};
+
+
+#endif  //vgui_wx_h_
+
