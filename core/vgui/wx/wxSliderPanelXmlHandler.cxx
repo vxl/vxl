7d0c660902630ee114881df774c371a5772d73e8
Matthew J. Leotta <mleotta@users.sourceforge.net>
2008-08-08 11:49:41 -0400

    removed example slider initialization

--- core/vgui/wx/wxSliderPanelXmlHandler.cxx
+++ core/vgui/wx/wxSliderPanelXmlHandler.cxx
@@ -15,23 +15,18 @@ wxSliderPanelXmlHandler::wxSliderPanelXmlHandler()
 wxObject *wxSliderPanelXmlHandler::DoCreateResource()
 {
   long base_id = GetLong(wxT("base_id"),10100);
-  
+
   wxSliderPanel* panel =
       new wxSliderPanel(m_parentAsWindow,
                         GetID(),
-			                  base_id,
+                        base_id,
                         GetPosition(), GetSize(),
                         GetStyle(),
                         GetName());
-  
-  vcl_vector<double> min_vals, max_vals, init_vals;
-  min_vals.push_back(1.0);
-  min_vals.push_back(0.5);
-  max_vals.push_back(2.5);
-  max_vals.push_back(1.7);
-  init_vals.push_back(1.5);
-  init_vals.push_back(1.0);
-  panel->CreateSliders(init_vals,min_vals,max_vals);
+
+  // TODO: Load initial sliders from the XML File
+  //vcl_vector<double> min_vals, max_vals, init_vals;
+  //panel->CreateSliders(init_vals,min_vals,max_vals);
 
   return panel;
 }
