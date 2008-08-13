bf86d8967af1b0543793bf08527a207bf8312525
Ian Scott <scottim@users.sourceforge.net>
2008-08-13 10:03:06 -0400

    Add clamp_above a mirror to the existing clamp_below

--- contrib/mul/vil3d/vil3d_clamp.h
+++ contrib/mul/vil3d/vil3d_clamp.h
@@ -66,4 +66,38 @@ inline void vil3d_clamp_below(vil3d_image_view<T>& src, T t)
   vil3d_clamp_below(src, t, t);
 }
 
+//: Clamp an image view below a given value t, setting it to v if above or on t
+// \relates vil3d_image_view
+template <class T>
+inline void vil3d_clamp_above(vil3d_image_view<T>& src, T t, T v)
+{
+  vcl_ptrdiff_t istepA=src.istep(), jstepA=src.jstep(),
+    kstepA=src.kstep(), pstepA=src.planestep();
+  T* planeA = src.origin_ptr();
+  for (unsigned int p=0; p<src.nplanes(); ++p,planeA+=pstepA)
+  {
+    T* sliceA = planeA;
+    for (unsigned int k=0; k<src.nk(); ++k,sliceA+=kstepA)
+    {
+      T* rowA = sliceA;
+      for (unsigned int j=0; j<src.nj(); ++j,rowA+=jstepA)
+      {
+        T* voxelA = rowA;
+        for (unsigned int i=0; i<src.ni(); ++i,voxelA+=istepA)
+          if (*voxelA >= t)
+            *voxelA = v;
+      }
+    }
+  }
+}
+
+//: Clamp an image view below a given value t, setting it to this t if above t
+// \relates vil3d_image_view
+template <class T>
+inline void vil3d_clamp_above(vil3d_image_view<T>& src, T t)
+{
+  vil3d_clamp_above(src, t, t);
+}
+
+
 #endif // vil3d_clamp_h_
