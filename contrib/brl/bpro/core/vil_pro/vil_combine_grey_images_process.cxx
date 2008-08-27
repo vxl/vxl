5104ba4186a004dd8a48d30fa4465917e81ea1f2
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2008-08-27 14:25:24 -0400

    Added porcess to combine grey-scale image into one

---
+++ contrib/brl/bpro/core/vil_pro/vil_combine_grey_images_process.cxx
@@ -0,0 +1,135 @@
+// This is brl/bpro/core/vil_pro/vil_combine_grey_images_process.cxx
+#include "vil_combine_grey_images_process.h"
+//:
+// \file
+
+#include <bprb/bprb_parameters.h>
+#include <vil/vil_load.h>
+#include <vil/vil_image_view.h>
+
+
+//: Constructor
+vil_combine_grey_images_process::vil_combine_grey_images_process()
+{
+  //this process takes 1 input:
+  // input(0): Filename containing the list of images to combine
+  input_data_.resize(1,brdb_value_sptr(0));
+  input_types_.resize(1);
+  input_types_[0]="vcl_string";
+
+  //this process has 1 output
+  // output(0): the multi-schannel image
+  output_data_.resize(1,brdb_value_sptr(0));
+  output_types_.resize(1);
+  output_types_[0]="vil_image_view_base_sptr";
+}
+
+//: Destructor
+vil_combine_grey_images_process::~vil_combine_grey_images_process()
+{
+}
+
+// Get images from a file.
+bool
+vil_combine_grey_images_process::get_images(vcl_string file,
+                 vcl_vector<vil_image_view_base_sptr>  &grey_imgs)
+{
+  grey_imgs.clear();
+  vcl_ifstream ifs( file.c_str() );
+  //vector to store image sizes
+  vcl_vector< vcl_vector<unsigned> > sizes;
+
+  //Caution: Your file should not have an empty line at the end
+  while(!ifs.eof())
+  {
+      vcl_string image_filename;
+      ifs >> image_filename;
+      if(image_filename.empty())
+        continue;
+      vil_image_view_base_sptr loaded_image = vil_load(image_filename.c_str() );
+      if ( !loaded_image ) {
+        vcl_cerr << "Failed to load image file" << image_filename << vcl_endl;
+        return false;
+      }
+      if ( loaded_image->nplanes() != 1)
+      {
+        vcl_cerr << "Image" << image_filename << "is not gray scale "<< vcl_endl;
+        return false;
+      }
+      grey_imgs.push_back( loaded_image );
+      vcl_vector<unsigned> this_size;
+      this_size.push_back(loaded_image->ni());
+      this_size.push_back(loaded_image->nj());
+      sizes.push_back(this_size);
+   }
+
+  //check that all images have the same size
+  for (unsigned i = 1; i < sizes.size(); i++)
+  {
+    if ( sizes[i] != sizes[i-1]){
+      vcl_cerr << "Grey-scale images have different sizes" << vcl_endl;
+      return false;
+    }
+
+  }
+  width_ = sizes[0][0];
+  height_ = sizes[0][1];
+  
+  
+  return true;
+}
+
+
+// Combine the grey-scale images.
+bool
+vil_combine_grey_images_process::combine(vcl_vector<vil_image_view_base_sptr>const  &grey_imgs,
+                                         vil_image_view_base_sptr &mul_img)
+{
+  
+  vil_image_view<vxl_byte> byte_img(width_,height_,grey_imgs.size());
+    
+  for (unsigned int p = 0; p < grey_imgs.size(); p++)
+  {
+    vil_image_view<vxl_byte> grey_img = static_cast<vil_image_view<vxl_byte> >(*grey_imgs[p]);
+
+    for (unsigned i = 0; i < width_; i++)
+      for (unsigned j = 0; j < height_; j++)
+        byte_img(i,j,p) = grey_img(i,j);
+
+  }
+
+  mul_img = new vil_image_view<vxl_byte>(byte_img);
+ 
+  return true;
+}
+
+//: Execute the process
+bool
+vil_combine_grey_images_process::execute()
+{
+  // Sanity check
+  if (!this->verify_inputs())
+    return false;
+
+  //Retrieve image from input
+  brdb_value_t<vcl_string>* input0 =
+    static_cast<brdb_value_t<vcl_string>* >(input_data_[0].ptr());
+
+  vcl_string imgs_file = input0->value();
+
+  vcl_vector<vil_image_view_base_sptr> grey_imgs;
+
+  if (!this->get_images(imgs_file, grey_imgs))
+    return false;
+
+  vil_image_view_base_sptr out_img_ptr;
+
+  if (!this->combine(grey_imgs,out_img_ptr))
+    return false;
+
+  brdb_value_sptr output0 = new brdb_value_t<vil_image_view_base_sptr>(out_img_ptr);
+  output_data_[0] = output0;
+
+  return true;
+}
+
---
+++ contrib/brl/bpro/core/vil_pro/vil_combine_grey_images_process.h
@@ -0,0 +1,54 @@
+// This is brl/bpro/core/vil_pro/vil_combine_grey_images_process.h
+#ifndef vil_combine_grey_images_process_h_
+#define vil_combine_grey_images_process_h_
+//:
+// \file
+// \brief A process for combining grey scale images to multichannel byte images.
+// \author Isabel Restrepo (mir@lems.brown.edu)
+// \date 8/20/2008
+//
+// \verbatim
+//  Modifications
+//   <none yet>
+// \endverbatim
+
+#include <vcl_string.h>
+#include <bprb/bprb_process.h>
+#include <vil/vil_image_view_base.h>
+#include <vcl_vector.h>
+
+
+//: This procces combines grey scale images into a single multi-channel
+// image of the same format as input images. All input images must be of 
+// the same type. Only byte images are supported for now. Further support 
+// should be implemented as needed
+
+class vil_combine_grey_images_process : public bprb_process
+{
+ public:
+
+  vil_combine_grey_images_process();
+
+  //: Copy Constructor (no local data)
+  vil_combine_grey_images_process(const vil_combine_grey_images_process& other): bprb_process(*static_cast<const bprb_process*>(&other)){}
+
+ ~vil_combine_grey_images_process();
+
+  //: Clone the process
+  virtual vil_combine_grey_images_process* clone() const {return new vil_combine_grey_images_process(*this);}
+
+  vcl_string name(){return "VilCombineGreyImagesProcess";}
+ 
+  bool init() { return true; }
+  bool execute();
+  bool finish(){return true;}
+
+protected:
+  bool get_images(vcl_string file, vcl_vector<vil_image_view_base_sptr>  &grey_imgs);
+  bool combine(vcl_vector<vil_image_view_base_sptr>const  &grey_imgs,
+                                         vil_image_view_base_sptr &multi_band_imgs);
+  unsigned width_;
+  unsigned height_;
+};
+
+#endif //vil_combine_grey_images_process_h_
