f77d329b9e2f61d0ac61fecae90b7b320a54d912
Matthew J. Leotta <mleotta@users.sourceforge.net>
2008-08-28 12:48:37 -0400

    added a synthetic bundle adjustment example to use for speed tests

---
+++ contrib/gel/mrc/vpgl/algo/examples/vpgl_synth_bundle_adjust.cxx
@@ -0,0 +1,122 @@
+#include <vul/vul_arg.h>
+#include <vgl/vgl_point_2d.h>
+#include <vgl/vgl_vector_2d.h>
+#include <vgl/vgl_point_3d.h>
+#include <vgl/vgl_homg_point_2d.h>
+#include <vgl/vgl_homg_point_3d.h>
+#include <vgl/algo/vgl_rotation_3d.h>
+#include <vpgl/algo/vpgl_bundle_adjust.h>
+#include <vnl/vnl_crs_index.h>
+#include <vnl/vnl_random.h>
+#include <vnl/vnl_double_3.h>
+
+
+
+// The Main Function
+int main(int argc, char** argv)
+{
+  vul_arg<int>    a_num_cameras("-ncam", "number of cameras", 30);
+  vul_arg<int>    a_num_points("-npt", "number of points", 30);
+  vul_arg_parse(argc, argv);
+
+  const double max_p_err = 1.0; // maximum image error to introduce (pixels)
+
+  // deterministic random number generator -- random but repeatable
+  long int seed = 0;
+  vnl_random rnd(seed);
+
+  vcl_vector<vgl_point_3d<double> > world;
+  for(unsigned int i=0; i<a_num_cameras(); ++i)
+    world.push_back(vgl_point_3d<double>(rnd.drand32(-1,1), rnd.drand32(-1,1), rnd.drand32(-1,1)));
+
+  // our known internal calibration
+  vpgl_calibration_matrix<double> K(2000.0,vgl_homg_point_2d<double>(500,500));
+  vgl_rotation_3d<double> I; // no rotation initially
+
+  vcl_vector<vpgl_perspective_camera<double> > cameras;
+  for(unsigned int i=0; i<a_num_cameras(); ++i)
+  {
+    vnl_double_3 p;
+    do{
+      p = vnl_double_3(rnd.drand32(-1,1),rnd.drand32(-1,1),rnd.drand32(-1,1));
+    } while( p.magnitude() <= 1.0 );
+    p.normalize();
+    p *= 10.0;
+    vgl_homg_point_3d<double> c(p[0], p[1], p[2]);
+    cameras.push_back(vpgl_perspective_camera<double>(K,c,I));
+    cameras.back().look_at(vgl_homg_point_3d<double>(0.0, 0.0, 0.0));
+  }
+
+
+  // project all points in all images
+  vcl_vector<vgl_point_2d<double> > image_points;
+  for (unsigned int i=0; i<cameras.size(); ++i){
+    for (unsigned int j=0; j<world.size(); ++j){
+      image_points.push_back(cameras[i](vgl_homg_point_3d<double>(world[j])));
+    }
+  }
+
+
+  // project each point adding uniform noise in a [-max_p_err/2, max_p_err/2] pixel window
+  vcl_vector<vgl_point_2d<double> > noisy_image_points(image_points);
+  for (unsigned int i=0; i<noisy_image_points.size(); ++i){
+    vgl_vector_2d<double> noise(rnd.drand32()-0.5, rnd.drand32()-0.5);
+    noisy_image_points[i] += max_p_err * noise;
+  }
+
+
+  // make default cameras
+  vpgl_perspective_camera<double> init_cam(K,vgl_homg_point_3d<double>(0.0, 0.0, -10.0),I);
+  init_cam.look_at(vgl_homg_point_3d<double>(0.0, 0.0, 0.0));
+  vcl_vector<vpgl_perspective_camera<double> >
+    unknown_cameras(cameras.size(),init_cam);
+
+  // make the unknown world points
+  vcl_vector<vgl_point_3d<double> > unknown_world(world.size(),vgl_point_3d<double>(0.0, 0.0, 0.0));
+
+
+  // make the mask (using all the points)
+  vcl_vector<vcl_vector<bool> > mask(cameras.size(), vcl_vector<bool>(world.size(),true) );
+
+#if 0
+
+  // remove several correspondences
+  mask[0][1] = false;
+  mask[0][2] = false;
+  mask[1][0] = false;
+  mask[1][1] = false;
+  mask[1][3] = false;
+  mask[2][3] = false;
+  mask[2][5] = false;
+  mask[2][6] = false;
+  mask[3][7] = false;
+  mask[3][8] = false;
+  mask[4][0] = false;
+  mask[4][1] = false;
+  mask[4][6] = false;
+  mask[4][8] = false;
+#endif
+
+
+  // create a subset of projections based on the mask
+  vnl_crs_index crs(mask);
+  vcl_vector<vgl_point_2d<double> > subset_image_points(crs.num_non_zero());
+  for (int i=0; i<crs.num_rows(); ++i){
+    for (int j=0; j<crs.num_cols(); ++j){
+      int k = crs(i,j);
+      if (k >= 0)
+        subset_image_points[k] = noisy_image_points[i*crs.num_cols() + j];
+    }
+  }
+
+  // make some correspondences incorrect
+  //subset_image_points[crs(0,3)] = subset_image_points[crs(0,4)];
+
+
+  vpgl_bundle_adjust::optimize(unknown_cameras, unknown_world, subset_image_points, mask);
+
+
+
+  //vpgl_bundle_adjust::write_vrml("test_bundle2.wrl",unknown_cameras,unknown_world);
+}
+
