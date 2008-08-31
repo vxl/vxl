5011d7fd97edeedd4e8a174eda8f67eef0ce7d56
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-08-31 10:30:36 -0400

    corrected a typo

--- contrib/brl/bpro/core/vil_pro/vil_combine_grey_images_process.h
+++ contrib/brl/bpro/core/vil_pro/vil_combine_grey_images_process.h
@@ -17,11 +17,10 @@
 #include <vil/vil_image_view_base.h>
 #include <vcl_vector.h>
 
-
-//: This procces combines grey scale images into a single multi-channel
-// image of the same format as input images. All input images must be of 
-// the same type. Only flaot images are supported for now. Further support 
-// should be implemented as needed
+//: This process combines grey scale images into a single multi-channel image of the same format as input images.
+// All input images must be of the same type.
+// Only float images are supported for now.
+// Further support should be implemented as needed.
 
 class vil_combine_grey_images_process : public bprb_process
 {
@@ -38,12 +37,12 @@ class vil_combine_grey_images_process : public bprb_process
   virtual vil_combine_grey_images_process* clone() const {return new vil_combine_grey_images_process(*this);}
 
   vcl_string name(){return "VilCombineGreyImagesProcess";}
- 
+
   bool init() { return true; }
   bool execute();
   bool finish(){return true;}
 
-protected:
+ protected:
   bool get_images(vcl_string file, vcl_vector<vil_image_view_base_sptr>  &grey_imgs);
   bool combine(vcl_vector<vil_image_view_base_sptr>const  &grey_imgs,
                                          vil_image_view_base_sptr &multi_band_imgs);
