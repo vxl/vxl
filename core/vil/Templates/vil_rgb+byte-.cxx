4255888ef02ce9d84b73eeea15c6721db157803e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-09-11 01:56:24 -0400

    Previous change cannot work: since operator<< is inline defined in vil_rgb.h, it must be specialised there, too.

--- core/vil/Templates/vil_rgb+byte-.cxx
+++ core/vil/Templates/vil_rgb+byte-.cxx
@@ -1,10 +1,5 @@
 #include <vil/vil_rgb.txx>
 #include <vxl_config.h>
 
-VCL_DEFINE_SPECIALIZATION
-vcl_ostream& operator<<(vcl_ostream& s, vil_rgb<vxl_byte> const& rgb)
-{
- return s << '[' << (int)rgb.r << ' ' << (int)rgb.g << ' ' << (int)rgb.b << ']';
-}
-
 VIL_RGB_INSTANTIATE( vxl_byte );
+VIL_RGB_INSTANTIATE_LS( vxl_byte );
