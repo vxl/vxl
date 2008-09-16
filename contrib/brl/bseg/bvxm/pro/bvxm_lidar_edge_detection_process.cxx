d3cd9fd0da6093649e6dac2843533be30634dca9
Ibrahim Eden <ibrahimeden@users.sourceforge.net>
2008-09-16 11:22:12 -0400

    submitted changed for the lidar edge probabilities

--- contrib/brl/bseg/bvxm/pro/bvxm_lidar_edge_detection_process.cxx
+++ contrib/brl/bseg/bvxm/pro/bvxm_lidar_edge_detection_process.cxx
@@ -94,13 +94,14 @@ bool bvxm_lidar_edge_detection_process::execute()
   for (unsigned i=0; i<ni; i++){
     for (unsigned j=0; j<nj; j++){
       float curr_difference = image_first_return(i,j)-image_second_return(i,j);
-      lidar_height(i,j) = image_first_return(i,j);
-      lidar_edges_prob(i,j) = 1.0f - 0.5f*(1.0f/(1.0f+curr_difference-threshold_edge_difference));
-      if (curr_difference>threshold_edge_difference){
+      lidar_edges_prob(i,j) = curr_difference/(threshold_edge_difference+curr_difference);
+      if (curr_difference>0.0f){
         lidar_edges(i,j) = 1.0f;
+        lidar_height(i,j) = image_first_return(i,j);
       }
       else{
-        lidar_edges(i,j) = 0.1f;
+        lidar_edges(i,j) = 0.0f;
+        lidar_height(i,j) = 0.0f;
       }
     }
   }
