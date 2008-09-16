291170ec33bbd59cf6a197ed8f0edf22fb164458
Ibrahim Eden <ibrahimeden@users.sourceforge.net>
2008-09-16 15:28:48 -0400

    *** empty log message ***

--- contrib/brl/bseg/bvxm/pro/bvxm_lidar_edge_detection_process.cxx
+++ contrib/brl/bseg/bvxm/pro/bvxm_lidar_edge_detection_process.cxx
@@ -95,12 +95,13 @@ bool bvxm_lidar_edge_detection_process::execute()
     for (unsigned j=0; j<nj; j++){
       float curr_difference = image_first_return(i,j)-image_second_return(i,j);
       lidar_edges_prob(i,j) = curr_difference/(1.0f+curr_difference);
-      lidar_height(i,j) = image_first_return(i,j);
       if (curr_difference>0.0f){
         lidar_edges(i,j) = 1.0f;
+        lidar_height(i,j) = image_first_return(i,j);
       }
       else{
         lidar_edges(i,j) = 0.0f;
+        lidar_height(i,j) = 0.0f;
       }
     }
   }
