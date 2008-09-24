04a69086641ef800c3a47eefe87c9814d57d0a97
Kevin de Souza <kdesouza@users.sourceforge.net>
2008-09-24 09:16:30 -0400

    Amended doxygen comments slightly.

--- core/vgl/vgl_triangle_3d.h
+++ core/vgl/vgl_triangle_3d.h
@@ -2,9 +2,9 @@
 #define VGL_TRIANGLE_3D_H_
 
 //! \file
-//  \brief Some helpful functions when working with triangles
-//  \author Kieran O'Mahony
-//  \date 21/6/2007
+//! \brief Some helpful functions when working with triangles
+//! \author Kieran O'Mahony
+//! \date 21/6/2007
 
 #include <vcl_algorithm.h>
 #include <vcl_utility.h>
@@ -21,7 +21,7 @@ enum vgl_triangle_3d_intersection_t
 };
 
 //! Check for coincident edges of triangles a and b
-//  \return a vector of the coincident edges
+//! \return a vector of the coincident edges
 vcl_vector<vcl_pair<unsigned,unsigned> > vgl_triangle_3d_coincident_edges(
   const vgl_point_3d<double>& a_p1,
   const vgl_point_3d<double>& a_p2,
@@ -31,8 +31,8 @@ vcl_vector<vcl_pair<unsigned,unsigned> > vgl_triangle_3d_coincident_edges(
   const vgl_point_3d<double>& b_p3);
 
 //! Check if the given point is inside the triangle
-//  The triangle is represented by its vertices \a p1, \a p2, \a p3
-//  \return true if point is inside
+//! The triangle is represented by its vertices \a p1, \a p2, \a p3
+//! \return true if point is inside
 bool vgl_triangle_3d_test_inside(
   const vgl_point_3d<double>& i_pnt,
   const vgl_point_3d<double>& p1,
@@ -40,10 +40,9 @@ bool vgl_triangle_3d_test_inside(
   const vgl_point_3d<double>& p3);
 
 //! Check if point \a i_pnt is inside the triangle
-//  The triangle is represented by its vertices \a p1, \a p2, \a p3
-//  \return true if point is inside
-//
-//  \note this method uses the less efficient 'angles' method which requires 3 calls to acos()
+//! The triangle is represented by its vertices \a p1, \a p2, \a p3
+//! \return true if point is inside
+//! \note this method uses the less efficient 'angles' method which requires 3 calls to acos()
 bool vgl_triangle_3d_test_inside_simple(
   const vgl_point_3d<double>& i_pnt,
   const vgl_point_3d<double>& p1,
@@ -52,8 +51,8 @@ bool vgl_triangle_3d_test_inside_simple(
 
 
 //! Compute the intersection point between the line segment and triangle
-//  The triangle is represented by its vertices \a p1, \a p2, \a p3
-//  \return true if line intersects triangle
+//! The triangle is represented by its vertices \a p1, \a p2, \a p3
+//! \return true if line intersects triangle
 vgl_triangle_3d_intersection_t vgl_triangle_3d_line_intersection(
   const vgl_line_segment_3d<double>& line,
   const vgl_point_3d<double>& p1,
@@ -63,9 +62,9 @@ vgl_triangle_3d_intersection_t vgl_triangle_3d_line_intersection(
   bool ignore_coplanar = false);
 
 //! Compute if the given triangles a and b intersect
-//  The triangles are represented by their respective vertices \a a_p1, \a a_p2, \a a_p3
-//  and \a b_p1, \a b_p2, \a b_p3
-//  \return intersection type
+//! The triangles are represented by their respective vertices \a a_p1, \a a_p2, \a a_p3
+//! and \a b_p1, \a b_p2, \a b_p3
+//! \return intersection type
 vgl_triangle_3d_intersection_t vgl_triangle_3d_triangle_intersection(
   const vgl_point_3d<double>& a_p1,
   const vgl_point_3d<double>& a_p2,
@@ -74,9 +73,9 @@ vgl_triangle_3d_intersection_t vgl_triangle_3d_triangle_intersection(
   const vgl_point_3d<double>& b_p2,
   const vgl_point_3d<double>& b_p3);
 
-//! compute the intersection line of the given triangles
-//  \see vgl_triangle_3d_triangle_intersection()
-//  \note an intesection line is not computed for a coplanar intersection
+//! Compute the intersection line of the given triangles
+//! \see vgl_triangle_3d_triangle_intersection()
+//! \note an intesection line is not computed for a coplanar intersection
 vgl_triangle_3d_intersection_t vgl_triangle_3d_triangle_intersection(
   const vgl_point_3d<double>& a_p1,
   const vgl_point_3d<double>& a_p2,
@@ -87,9 +86,9 @@ vgl_triangle_3d_intersection_t vgl_triangle_3d_triangle_intersection(
   vgl_line_segment_3d<double>& i_line);
 
 //! Compute the line of intersection of the given triangle and plane
-//  The triangle is represented by its vertices \a p1, \a p2, \a p3
-//  \return intersection type
-//  \note an intersection line is not defined (NaN) for a coplanar intersection
+//! The triangle is represented by its vertices \a p1, \a p2, \a p3
+//! \return intersection type
+//! \note an intersection line is not defined (NaN) for a coplanar intersection
 vgl_triangle_3d_intersection_t vgl_triangle_3d_plane_intersection(
   const vgl_point_3d<double>& p1,
   const vgl_point_3d<double>& p2,
@@ -98,8 +97,8 @@ vgl_triangle_3d_intersection_t vgl_triangle_3d_plane_intersection(
   vgl_line_segment_3d<double>& i_line);
 
 //! Compute the longest side of the given triangle
-//  The triangle is represented by its vertices \a p1, \a p2, \a p3
-//  \return length of the longest side
+//! The triangle is represented by its vertices \a p1, \a p2, \a p3
+//! \return length of the longest side
 inline double vgl_triangle_3d_longest_side(
   const vgl_point_3d<double>& p1,
   const vgl_point_3d<double>& p2,
@@ -111,8 +110,8 @@ inline double vgl_triangle_3d_longest_side(
 }
 
 //! Compute the shortest side of the given triangle
-//  The triangle is represented by its vertices \a p1, \a p2, \a p3
-//  \return length of the longest side
+//! The triangle is represented by its vertices \a p1, \a p2, \a p3
+//! \return length of the longest side
 inline double vgl_triangle_3d_shortest_side(
   const vgl_point_3d<double>& p1,
   const vgl_point_3d<double>& p2,
@@ -144,8 +143,8 @@ double vgl_triangle_3d_distance(
   const vgl_point_3d<double>& p3);
 
 //! Check if the two triangles are coplanar
-//  The triangles are represented by their respective vertices \a a_p1, \a a_p2, \a a_p3
-//  and \a b_p1, \a b_p2, \a b_p3
+//! The triangles are represented by their respective vertices \a a_p1, \a a_p2, \a a_p3
+//! and \a b_p1, \a b_p2, \a b_p3
 bool vgl_triangle_3d_triangle_coplanar(
   const vgl_point_3d<double>& a_p1,
   const vgl_point_3d<double>& a_p2,
@@ -157,7 +156,7 @@ bool vgl_triangle_3d_triangle_coplanar(
 
 //=======================================================================
 //! Compute the area of a triangle
-//  The triangle is represented by its vertices \a p1, \a p2, \a p3
+//! The triangle is represented by its vertices \a p1, \a p2, \a p3
 double vgl_triangle_3d_area(
   const vgl_point_3d<double> &p0,
   const vgl_point_3d<double> &p1,
@@ -165,7 +164,7 @@ double vgl_triangle_3d_area(
 
 //=======================================================================
 //! Compute the aspect ratio of a triangle
-//  The triangle is represented by its vertices \a p1, \a p2, \a p3
+//! The triangle is represented by its vertices \a p1, \a p2, \a p3
 double vgl_triangle_3d_aspect_ratio(
   const vgl_point_3d<double> &p0,
   const vgl_point_3d<double> &p1,
