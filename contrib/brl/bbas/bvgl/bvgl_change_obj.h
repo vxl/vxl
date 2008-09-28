3f31a625487fc81d5ad3a59207922ecadb5580d5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-09-28 18:18:47 -0400

    TABs replaced by spaces

--- contrib/brl/bbas/bvgl/bvgl_change_obj.h
+++ contrib/brl/bbas/bvgl/bvgl_change_obj.h
@@ -2,13 +2,12 @@
 // \file
 // \brief Helper classes to annotate polygonal regions in images
 // \author Ozge C. Ozcanli ozge@lems.brown.edu
-// \date   09/26/08
+// \date   09/26/2008
 //
 // \verbatim
 //  Modifications
-//
+//   <None yet>
 // \endverbatim
-//
 
 #ifndef bvgl_change_obj_h_
 #define bvgl_change_obj_h_
@@ -20,27 +19,26 @@
 
 class bvgl_change_obj: public vbl_ref_count
 {
-public:
+ public:
   bvgl_change_obj(vgl_polygon<double>& poly) : poly_(poly), class_("unknown") {}
   bvgl_change_obj(vgl_polygon<double>& poly, vcl_string cls) : poly_(poly), class_(cls) {}
   bvgl_change_obj(const bvgl_change_obj& other);
   bvgl_change_obj() {}
-  
+
   //: binary IO write
   void b_write(vsl_b_ostream& os);
 
   //: binary IO read
   void b_read(vsl_b_istream& is);
-  
+
   //: Return IO version number;
   unsigned char version() const;
 
-	//void xml_read();
-	//void xml_write();
-	
+  //void xml_read();
+  //void xml_write();
+
   vcl_string class_;
-	vgl_polygon<double> poly_;
-	
+  vgl_polygon<double> poly_;
 };
 
 #endif  //bvgl_change_obj_h_
--- contrib/brl/bbas/bvgl/bvgl_changes.h
+++ contrib/brl/bbas/bvgl/bvgl_changes.h
@@ -1,15 +1,13 @@
 //:
 // \file
-// \brief Helper classes to annotate polygonal regions in images
-//        a set of bvgl_change_obj
+// \brief Helper classes to annotate polygonal regions in images a set of bvgl_change_obj
 // \author Ozge C. Ozcanli ozge@lems.brown.edu
-// \date   09/26/08
+// \date   09/26/2008
 //
 // \verbatim
 //  Modifications
-//
+//   <None yet>
 // \endverbatim
-//
 
 #ifndef bvgl_changes_h_
 #define bvgl_changes_h_
@@ -22,28 +20,27 @@
 
 class bvgl_changes : public vbl_ref_count
 {
-public:
+ public:
   bvgl_changes() {}
 
   unsigned size() { return objs_.size(); }
 
-	//: binary IO write
+  //: binary IO write
   void b_write(vsl_b_ostream& os);
 
   //: binary IO read
   void b_read(vsl_b_istream& is);
-  
+
   //: Return IO version number;
   unsigned char version() const;
-	//void xml_read();
-	//void xml_write();
-	
+  //void xml_read();
+  //void xml_write();
+
   vil_image_view<vxl_byte> create_mask_from_objs();
-	void add_obj(bvgl_change_obj_sptr obj);
+  void add_obj(bvgl_change_obj_sptr obj);
 
   vcl_vector<bvgl_change_obj_sptr> objs_;
-	vcl_string img_name_;
-	
+  vcl_string img_name_;
 };
 
 #endif  //bvgl_changes_h_
