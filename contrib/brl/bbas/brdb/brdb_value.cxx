9c8cac537254a076f5d300a74ae30d66f3221157
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-09-28 18:19:14 -0400

    *** empty log message ***

--- contrib/brl/bbas/brdb/brdb_value.cxx
+++ contrib/brl/bbas/brdb/brdb_value.cxx
@@ -1,15 +1,11 @@
 // This is brl/bbas/brdb/brdb_value.cxx
-
+#include <brdb/brdb_value.h>
 //:
 // \file
-
-// updated by Yong Zhao
-// Apr 4th, 2007
+// \author updated by Yong Zhao
+// \date Apr 4th, 2007
+//
 // make it work with the database initially based on Matt's sketch
-
-
-#include <brdb/brdb_value.h>
-
 
 //: Return a reference to the global registry of database value classes
 vcl_map<vcl_string, const brdb_value*> &
@@ -18,7 +14,6 @@ brdb_value::mut_registry()
   static vcl_map<vcl_string, const brdb_value*> reg;
   return reg;
 }
-
 
 //: The Constructor for this struct registers an exemplar database value object with the base class
 brdb_value::registrar::registrar(const brdb_value* exemplar)
