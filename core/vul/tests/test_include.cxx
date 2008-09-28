3107ef3e816c9dc17f68b93e006e0d94a5a691ae
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-09-28 13:18:06 -0400

    removed duplicates

--- core/vul/tests/test_include.cxx
+++ core/vul/tests/test_include.cxx
@@ -1,52 +1,27 @@
-/* */
-#include <vul/vul_fwd.h>
 #include <vul/vul_fwd.h>
 #include <vul/vul_whereami.h>
-#include <vul/vul_whereami.h>
-#include <vul/vul_arg.h>
 #include <vul/vul_arg.h>
 #include <vul/vul_awk.h>
-#include <vul/vul_awk.h>
-#include <vul/vul_debug.h>
 #include <vul/vul_debug.h>
 #include <vul/vul_expand_path.h>
-#include <vul/vul_expand_path.h>
-#include <vul/vul_file.h>
 #include <vul/vul_file.h>
 #include <vul/vul_file_iterator.h>
-#include <vul/vul_file_iterator.h>
-#include <vul/vul_get_timestamp.h>
 #include <vul/vul_get_timestamp.h>
 #include <vul/vul_ios_state.h>
-#include <vul/vul_ios_state.h>
-#include <vul/vul_printf.h>
 #include <vul/vul_printf.h>
 #include <vul/vul_psfile.h>
-#include <vul/vul_psfile.h>
-#include <vul/vul_redirector.h>
 #include <vul/vul_redirector.h>
 #include <vul/vul_reg_exp.h>
-#include <vul/vul_reg_exp.h>
-#include <vul/vul_sequence_filename_map.h>
 #include <vul/vul_sequence_filename_map.h>
 #include <vul/vul_sprintf.h>
-#include <vul/vul_sprintf.h>
-#include <vul/vul_string.h>
 #include <vul/vul_string.h>
 #include <vul/vul_temp_filename.h>
-#include <vul/vul_temp_filename.h>
-#include <vul/vul_timer.h>
 #include <vul/vul_timer.h>
 #include <vul/vul_timestamp.h>
-#include <vul/vul_timestamp.h>
-#include <vul/vul_trace.h>
 #include <vul/vul_trace.h>
 #include <vul/vul_user_info.h>
-#include <vul/vul_user_info.h>
-#include <vul/vul_url.h>
 #include <vul/vul_url.h>
 
-
-int main(){return 0;}
+int main() { return 0; }
 
 
