f390a4547b142df650ca35b1d2e6ef996c656769
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-10-11 10:22:10 -0400

    replaced vcl_ios::fixed by vcl_ios_fixed

--- contrib/mul/mbl/mbl_progress_time.cxx
+++ contrib/mul/mbl/mbl_progress_time.cxx
@@ -1,16 +1,14 @@
 #ifdef VCL_NEEDS_PRAGMA_INTERFACE
 #pragma implementation
 #endif
-
-
 //:
 // \file
 // \brief Progress class that outputs elapsed time reporting on progress
 // \author Kevin de Souza
 // \date 26 June 2008
 
-
 #include "mbl_progress_time.h"
+#include <vcl_ios.h>
 
 
 //========================================================================
@@ -20,7 +18,7 @@ mbl_progress_time::mbl_progress_time(vcl_ostream& os/*=vcl_cout*/)
 : os_(os), ios_state_(os_)
 {
   os_.precision(3);
-  os_.setf(vcl_ios::fixed);
+  os_.setf(vcl_ios_fixed);
 }
 
 
@@ -36,18 +34,18 @@ mbl_progress_time::~mbl_progress_time()
 // Name of the class
 //========================================================================
 vcl_string mbl_progress_time::is_a() const
-{ 
-  return "mbl_progress_time"; 
+{
+  return "mbl_progress_time";
 }
 
-  
+
 //========================================================================
 // Called when set_estimate_iterations() is called for a given identifier.
 //========================================================================
 void mbl_progress_time::on_set_estimated_iterations(const vcl_string& identifier,
                                                     const int /*total_iterations*/)
-{ 
-  os_ << "Starting mbl_progress_time \"" << identifier << "\"" << vcl_endl; 
+{
+  os_ << "Starting mbl_progress_time \"" << identifier << "\"" << vcl_endl;
   timer_.mark();
 }
 
@@ -67,7 +65,7 @@ void mbl_progress_time::on_set_progress(const vcl_string& identifier,
 // Called when end_progress() is called for a given identifier.
 //========================================================================
 void mbl_progress_time::on_end_progress(const vcl_string &identifier)
-{ 
+{
   double tsec = timer_.real()/1000.0;
-  os_ << "Finishing mbl_progress_time \"" << identifier << "\" (total elapsed time: " << tsec << " s)" << vcl_endl; 
+  os_ << "Finishing mbl_progress_time \"" << identifier << "\" (total elapsed time: " << tsec << " s)" << vcl_endl;
 }
