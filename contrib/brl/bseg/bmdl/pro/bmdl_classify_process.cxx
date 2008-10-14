8e1d43f628e78ee128181eee3f9d4951a14683ea
Gamze Tunali <gamze@users.sourceforge.net>
2008-10-14 16:39:56 -0400

    added a process lib

---
+++ contrib/brl/bseg/bmdl/pro/bmdl_classify_process.cxx
@@ -0,0 +1,123 @@
+#include "bmdl_classify_process.h"
+//:
+// \file
+
+#include <vcl_cassert.h>
+#include <vcl_cstring.h>
+
+#include <vgl/vgl_box_2d.h>
+#include <vgl/vgl_box_3d.h>
+#include <vgl/vgl_point_2d.h>
+#include <vgl/vgl_point_3d.h>
+#include <vsol/vsol_box_2d.h>
+
+#include <vil/vil_load.h>
+#include <vil/vil_image_resource.h>
+#include <vil/vil_image_view.h>
+#include <vil/vil_pixel_format.h>
+
+#include <bprb/bprb_parameters.h>
+
+#include <bmdl/bmdl_classify.h>
+
+bmdl_classify_process::bmdl_classify_process()
+{
+  //this process takes 2 input:
+  //the file paths of the first return and second return
+  input_data_.resize(2, brdb_value_sptr(0));
+  input_types_.resize(2);
+
+  int i=0;
+  input_types_[i++] = "vcl_string";             // first ret. image path (geotiff)
+  input_types_[i++] = "vcl_string";             // last ret. image path (geotiff)
+
+  //output
+  output_data_.resize(2,brdb_value_sptr(0));
+  output_types_.resize(2);
+  int j=0;
+  output_types_[j++]= "vil_image_view_base_sptr";  // label image
+  output_types_[j++]= "vil_image_view_base_sptr";  // height image
+
+  //if (!parameters()->add( "Upper bound for Lidar differences", "mask_thresh", 10.0f ))
+   // vcl_cout << "ERROR: Adding parameters in bmdl_classify_process\n";
+}
+
+bool bmdl_classify_process::execute()
+{
+  // Sanity check
+  if (!this->verify_inputs())
+    return false;
+
+  // get the inputs:
+  // image
+  brdb_value_t<vcl_string>* input0 =
+    static_cast<brdb_value_t< vcl_string>* >(input_data_[0].ptr());
+  vcl_string first = input0->value();
+
+  brdb_value_t<vcl_string>* input1 =
+    static_cast<brdb_value_t< vcl_string>* >(input_data_[1].ptr());
+  vcl_string last = input1->value();
+
+  // check first return's validity
+  vil_image_resource_sptr first_ret = vil_load_image_resource(first.c_str());
+  if (!first_ret) {
+    vcl_cout << "bmdl_classify_process -- First return image path is not valid!\n";
+    return false;
+  }
+
+  // check last return's validity
+  vil_image_resource_sptr last_ret = vil_load_image_resource(last.c_str());
+  if (!last_ret) {
+    vcl_cout << "bmdl_classify_process -- Last return image path is not valid!\n";
+    return false;
+  }
+
+  vil_image_view_base_sptr label_img=0, height_img=0;
+  if (!classify(first_ret, last_ret, label_img, height_img)) {
+    vcl_cout << "bmdl_classify_process -- The process has failed!\n";
+    return false;
+  }
+
+  // store image output (labels)
+  brdb_value_sptr output1 =
+    new brdb_value_t<vil_image_view_base_sptr>(label_img);
+  output_data_[1] = output1;
+
+  // store image output (height)
+  brdb_value_sptr output2 =
+    new brdb_value_t<vil_image_view_base_sptr>(height_img);
+  output_data_[2] = output2;
+
+}
+
+bool bmdl_classify_process::classify(vil_image_resource_sptr lidar_first,
+                                         vil_image_resource_sptr lidar_last,
+                                         vil_image_view_base_sptr& label_img,
+                                         vil_image_view_base_sptr& height_img)
+{
+  // the file should be a at least a tiff (better, geotiff)
+  vcl_cout << "File FORMAT=" << lidar_first->file_format();
+
+  if (vcl_strcmp(lidar_first->file_format(), "tiff") != 0 && 
+    vcl_strcmp(lidar_last->file_format(),"tiff") != 0) {
+    vcl_cout << "bmdl_classify_process::classify -- The lidar images should be a TIFF!\n";
+    return false;
+  }
+  
+  
+  if ((lidar_first->pixel_format() == VIL_PIXEL_FORMAT_DOUBLE) && 
+    (lidar_last->pixel_format() == VIL_PIXEL_FORMAT_DOUBLE)) {
+    vil_image_view<double> first_return = lidar_first->get_view();
+    vil_image_view<double> last_return = lidar_last->get_view();
+    label_img = new vil_image_view<unsigned int>();
+    height_img = new vil_image_view<double>();
+    bmdl_classify::label_lidar(first_return, last_return, (vil_image_view<unsigned int>)label_img, height_img);
+
+  } else {
+    vcl_cout << "bmdl_classify_process::classify -- The Image Pixel Type is not DOUBLE!\n";
+    return false;
+  }
+
+  return true;
+}
+
---
+++ contrib/brl/bseg/bmdl/pro/bmdl_classify_process.h
@@ -0,0 +1,62 @@
+// This is brl/bseg/bmdl/pro/bmdl_classify_process.h
+#ifndef bmdl_classify_process_h_
+#define bmdl_classify_process_h_
+//:
+// \file
+// \brief A class for clipping and image based on a 3D bounding box.
+//        -  Input:
+//             - First return path (string)
+//             - Last return path (string)
+//
+//        -  Output:
+//             - label image "vil_image_view_base_sptr"
+//             - height image "vil_image_view_base_sptr"
+//
+//        -  Params:
+//             
+//
+// \author  Gamze D. Tunali
+// \date    10/14/2008
+// \verbatim
+//  Modifications
+//   <none yet>
+// \endverbatim
+
+#include <vcl_string.h>
+#include <vcl_vector.h>
+
+#include <bprb/bprb_process.h>
+#include <vil/vil_image_view_base.h>
+#include <vil/vil_image_resource_sptr.h>
+
+class bmdl_classify_process : public bprb_process
+{
+ public:
+
+   bmdl_classify_process();
+
+  //: Copy Constructor (no local data)
+  bmdl_classify_process(const bmdl_classify_process& other)
+    : bprb_process(*static_cast<const bprb_process*>(&other)){}
+
+  ~bmdl_classify_process(){}
+
+  //: Clone the process
+  virtual bmdl_classify_process* clone() const
+    { return new bmdl_classify_process(*this); }
+
+  vcl_string name(){return "bmdlClassifyProcess";}
+
+  bool init() { return true; }
+  bool execute();
+  bool finish(){return true;}
+
+ private:
+
+   bool classify(vil_image_resource_sptr lidar_first,
+                 vil_image_resource_sptr lidar_last,
+                 vil_image_view_base_sptr& label_img,
+                 vil_image_view_base_sptr& height_img);
+};
+
+#endif // bmdl_classify_process_h_
