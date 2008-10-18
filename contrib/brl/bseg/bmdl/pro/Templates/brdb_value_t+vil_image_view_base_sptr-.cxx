18b3b1059e09a3631bc4a32bb5cbf81247af91ba
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-10-18 09:14:53 -0400

    added missing #include <vil/io/vil_io_image_view.h>

--- contrib/brl/bseg/bmdl/pro/Templates/brdb_value_t+vil_image_view_base_sptr-.cxx
+++ contrib/brl/bseg/bmdl/pro/Templates/brdb_value_t+vil_image_view_base_sptr-.cxx
@@ -1,4 +1,5 @@
 #include <vil/vil_image_view_base.h>
+#include <vil/io/vil_io_image_view.h>
 #include <brdb/brdb_value.txx>
 
 BRDB_VALUE_INSTANTIATE(vil_image_view_base_sptr,"vil_image_view_base_sptr");
