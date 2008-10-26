acd618d2f4e92ca9cae9f64bf5a0161fc46f563c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-10-26 06:26:20 -0400

    added new header file

--- contrib/brl/bseg/bmdl/pro/tests/test_include.cxx
+++ contrib/brl/bseg/bmdl/pro/tests/test_include.cxx
@@ -1,5 +1,6 @@
 #include <bmdl/pro/bmdl_classify_process.h>
 #include <bmdl/pro/bmdl_generate_mesh_process.h>
+#include <bmdl/pro/bmdl_lidar_roi_process.h>
 #include <bmdl/pro/bmdl_trace_boundaries_process.h>
 
 int main() { return 0; }
