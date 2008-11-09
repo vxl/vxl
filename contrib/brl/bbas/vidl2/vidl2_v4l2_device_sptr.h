0c25b447de334a3f0d1a51a50bd2eae6e8e2ce50
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-11-08 20:06:45 -0500

    corrected the "This is ..." line

--- contrib/brl/bbas/vidl2/vidl2_v4l2_device_sptr.h
+++ contrib/brl/bbas/vidl2/vidl2_v4l2_device_sptr.h
@@ -1,4 +1,4 @@
-// This is brl/bbas/vidl2/vidl2_device_sptr.h
+// This is brl/bbas/vidl2/vidl2_v4l2_device_sptr.h
 #ifndef vidl2_v4l2_device_sptr_h_
 #define vidl2_v4l2_device_sptr_h_
 //:
@@ -8,15 +8,12 @@
 // \author Antonio Garrido
 // \verbatim
 //  Modifications
-//     15 Apr 2009 Created (A. Garrido)
+//   15 Apr 2008 Created (A. Garrido)
 //\endverbatim
-
-
 
 #include <vil/vil_smart_ptr.h>
 
 class vidl2_v4l2_device;
-
 
 //: A smart pointer to a device
 typedef vil_smart_ptr<vidl2_v4l2_device> vidl2_v4l2_device_sptr;
