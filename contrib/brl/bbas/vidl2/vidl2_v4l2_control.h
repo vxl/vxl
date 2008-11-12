358f8f6f53780bf5bc432cc4725f0ea15e66d7e7
Antonio Garrido <antonio1-2-3@users.sourceforge.net>
2008-11-12 11:37:29 -0500

    Cleaning up and adding doc

--- contrib/brl/bbas/vidl2/vidl2_v4l2_control.h
+++ contrib/brl/bbas/vidl2/vidl2_v4l2_control.h
@@ -13,7 +13,6 @@
 
 #include <vcl_string.h>
 #include <vcl_vector.h>
-//#include "vidl2_v4l2_control_sptr.h"
 
 extern "C" {
 #include <asm/types.h>          /* for videodev2.h */
@@ -31,58 +30,41 @@ class vidl2_v4l2_control
     int get_value() const;
   public:
     virtual ~vidl2_v4l2_control() {}
-
     //: Factory method to create new controls
     // \param ctr details from VIDIOC_QUERYCTRL
     // \param f associated file descriptor
+    // \return pointer to base class of new control
     static vidl2_v4l2_control * new_control(const v4l2_queryctrl& ctr, int f);
-
+    //: Type of control
+    // \return type as indicated in v4l2 specification
     v4l2_ctrl_type type() const { return ctrl_.type; }
-
+    //: Name of control
+    // \return name the driver assign to the control
     vcl_string name() const { return  (const char *) ctrl_.name; }
-
-    //: Print a 1-line brief description
+    //: A 1-line brief description
     virtual vcl_string description() const= 0;
-
+    //: Id of control
+    // \return ID (for example, V4L2_CID_BRIGHTNESS corresponds to brightness)
     int id() const { return ctrl_.id; }
-
     //: Control is read only
     bool read_only() const { return ctrl_.flags & V4L2_CTRL_FLAG_READ_ONLY; }
     //: Control can change value of other controls
     bool affect_other_controls() const { return ctrl_.flags & V4L2_CTRL_FLAG_UPDATE; }
-
-
-    //-------------------------------------------------------
-    // reference counting if used through sptr
-//     public:
-// 
-//       //: Increment reference count
-//       void ref() { ref_count_++; }
-// 
-//       //: Decrement reference count
-//       void unref(){
-//         assert (ref_count_ >0);
-//         ref_count_--;
-//         if (ref_count_==0)
-//         {
-//           delete this;
-//         }
-//       }
-// 
-//       //: Number of objects referring to this data
-//       int ref_count() const { return ref_count_; }
-// 
-//     private:
-//       int ref_count_;
 };
 
+//: A class for handle a control of type integer
 class vidl2_v4l2_control_integer: public vidl2_v4l2_control
 {
   public:
     vidl2_v4l2_control_integer(const v4l2_queryctrl& ctr, int f): vidl2_v4l2_control(ctr,f) {}
+    //: Minimum value of the control
     int minimum() const { return ctrl_.minimum; }
+    //: Maximum value of the control
     int maximum() const { return ctrl_.maximum; }
+    //: Step size
+    // Indicates the increment between values which are actually different on the hardware
     int step() const { return ctrl_.step; }
+    //: Default value of this control
     int default_value() const { return ctrl_.default_value; }
     //: Set the value of the control 
     // \param value to be set in range determined by driver
@@ -96,47 +78,59 @@ class vidl2_v4l2_control_integer: public vidl2_v4l2_control
     //: Get the value of the control 
     // \return value in range 0-100
     int get_100() const { return (get_value()-ctrl_.minimum)*100/(ctrl_.maximum-ctrl_.minimum); }
+    //: A 1-line brief description
     virtual vcl_string description() const;
 
 };
 
+//: A class for handle a control of type menu
 class vidl2_v4l2_control_menu: public vidl2_v4l2_control
 {
     vcl_vector<vcl_string> items;
   public:
     vidl2_v4l2_control_menu(const v4l2_queryctrl& ctr, int f);
-    //int minimum() const { return 0; }
-    //int maximum() const { return ctrl_.maximum; }
-    //int step() const { return 1; }
+    //: Number of items
+    // \return number of entries in the menu
     int n_items() const { return items.size();}
+    //: Item i
+    // \return string assigned to item i
     vcl_string get(unsigned int i) const { return items[i]; }
+    //: Select item i
     void set(unsigned int i) const { if (i>=0 && i<n_items()) set_value((int) i); }
     //: Get the value of the control 
     // \return index in the menu
     unsigned int get() const { return (unsigned int) get_value(); }
+    //: Default value of this control
     int default_value() const { return ctrl_.default_value; }
+    //: A 1-line brief description
     virtual vcl_string description() const;
 };
 
+//: A class for handle a control of type boolean
 class vidl2_v4l2_control_boolean: public vidl2_v4l2_control
 {
   public:
     vidl2_v4l2_control_boolean(const v4l2_queryctrl& ctr, int f): vidl2_v4l2_control(ctr,f) {}
-    bool default_value() const { return ctrl_.default_value; }
+    //: Set the value of the control 
     void set(bool v) const { set_value(v?1:0); }
+    //: Get the value of the control 
     bool get() const { return get_value(); }
+    //: Default value of this control
+    bool default_value() const { return ctrl_.default_value; }
+    //: A 1-line brief description
     virtual vcl_string description() const 
        { return "Control \""+name()+"\": boolean (default: "+(default_value()?"true":"false")+")"; }
 };
 
+//: A class for handle a control of type button
 class vidl2_v4l2_control_button: public vidl2_v4l2_control
 {
   public:
     vidl2_v4l2_control_button(const v4l2_queryctrl& ctr, int f): vidl2_v4l2_control(ctr,f) {}
+    //: Push button
     void push() const { set_value(1); }
+    //: A 1-line brief description
     virtual vcl_string description() const { return "Control \""+name()+"\": button"; }
 };
-
-
 
 #endif // vidl2_v4l2_control_h_
