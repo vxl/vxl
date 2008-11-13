3ac330fe3a5c36ebdb973606de366bd9e9486f12
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-11-13 14:09:34 -0500

    "dummy" instantiation, for testing the vgl_rtree implementation on potential compile errors or warnings

---
+++ core/vgl/algo/Templates/vgl_rtree+vgl_point_2d+double-.vgl_box_2d+double-.dummy-.cxx
@@ -0,0 +1,21 @@
+// Instantiation of vgl_rtree<vgl_point_2d<double>, vgl_box_2d<double>, dummy>
+// Treat this template instantiation as a "dummy" instantiation, for purposes of
+// testing the vgl_rtree implementation on potential compile errors or warnings.
+#include <vgl/vgl_point_2d.h>
+#include <vgl/vgl_box_2d.h>
+#include <vgl/algo/vgl_rtree.txx>
+
+typedef vgl_point_2d<double> V;
+typedef vgl_box_2d<double>   B;
+
+struct dummy // dummy "namespace", used by some of the rtree methods
+{
+  static void  init  (B &, V const &) {}
+  static void  update(B &, V const &) {}
+  static void  update(B &, B const &) {}
+  static bool  meet  (B const &, V const &) { return false; }
+  static bool  meet  (B const &, B const &) { return false; }
+  static float volume(B const &) { return 0.0f; }
+};
+
+VGL_RTREE_INSTANTIATE(V, B, dummy);
