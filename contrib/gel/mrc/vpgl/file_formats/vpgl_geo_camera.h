53c3c64e626c0418ddbdcbd4edfba395f04ba93d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-11-14 18:35:46 -0500

    avoid compiler warning (initialisation order in constructor)

--- contrib/gel/mrc/vpgl/file_formats/vpgl_geo_camera.h
+++ contrib/gel/mrc/vpgl/file_formats/vpgl_geo_camera.h
@@ -32,8 +32,8 @@ class vpgl_geo_camera : public vpgl_camera<double>
   vpgl_geo_camera(vnl_matrix<double> trans_matrix,
                   bgeo_lvcs_sptr lvcs,
                   vcl_vector<vcl_vector<double> > tiepoints)
-    : trans_matrix_(trans_matrix), lvcs_(lvcs),
-    tiepoints_(tiepoints), is_utm(false), tx_(0), ty_(0) {}
+    : trans_matrix_(trans_matrix), tx_(0), ty_(0), lvcs_(lvcs),
+      tiepoints_(tiepoints), is_utm(false) {}
 
   // copy constructor
   vpgl_geo_camera(vpgl_geo_camera const& rhs);
