f080e738edbc8e1b35610ad776111c7caea187d1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-11-16 16:55:15 -0500

    commenting out unimplemented methods (with #if 0 ... #endif)

--- contrib/brl/bbas/bgrl2/bgrl2_hg_hypergraph.cxx
+++ contrib/brl/bbas/bgrl2/bgrl2_hg_hypergraph.cxx
@@ -1,171 +1,173 @@
-// This is lemsvxlsrc/brcv/shp/dbhg/bgrl2_hg_hypergraph.cxx
+// This is brl/bbas/bgrl2/bgrl2_hg_hypergraph.cxx
+#include <bgrl2/bgrl2_hg_hypergraph.h>
 //:
 // \file
 // author: Ming-Ching Chang
 // date:   Apr 04, 2005
 
-#include <bgrl2/bgrl2_hg_hypergraph.h>
-
+#include <vcl_cassert.h>
 
 ////////////////////////////////////////////////////////////////////
 //: ===== Internal Low-level Graph operation (without handling connectivity) =====
-bool bgrl2_hg_hypergraph::_internal_del_vertex (bgrl2_hg_vertex* vertex)
+bool bgrl2_hg_hypergraph::_internal_del_vertex(bgrl2_hg_vertex* vertex)
 {
   assert (vertex->connecting_hyperedges().size() == 0);
-  return bgrl2_hg_graph::_internal_del_vertex (vertex);
+  return bgrl2_hg_graph::_internal_del_vertex(vertex);
 }
 
-bool bgrl2_hg_hypergraph::_internal_del_edge (bgrl2_hg_edge* edge)
+bool bgrl2_hg_hypergraph::_internal_del_edge(bgrl2_hg_edge* edge)
 {
   assert (edge->connecting_hyperedges().size() == 0);
-  return bgrl2_hg_graph::_internal_del_edge (edge);
+  return bgrl2_hg_graph::_internal_del_edge(edge);
 }
 
-void bgrl2_hg_hypergraph::_internal_add_hyperedge (bgrl2_hg_hyperedge* hyperedge) 
+void bgrl2_hg_hypergraph::_internal_add_hyperedge(bgrl2_hg_hyperedge* hyperedge)
 {
   ///assert (hyperedge->id() > 0);
-  ///hyperedges_.insert (vcl_pair<int, bgrl2_hg_hyperedge*> (hyperedge->id(), hyperedge));
+  ///hyperedges_.insert(vcl_pair<int, bgrl2_hg_hyperedge*>(hyperedge->id(), hyperedge));
 }
 
-bool bgrl2_hg_hypergraph::_internal_del_hyperedge (bgrl2_hg_hyperedge* hyperedge) 
+bool bgrl2_hg_hypergraph::_internal_del_hyperedge(bgrl2_hg_hyperedge* hyperedge)
 {
   assert (hyperedge->connecting_edges().size() == 0);
   assert (hyperedge->connecting_vertices().size() == 0);
 
-  ///int n_removed = hyperedges_.erase (hyperedge->id());
-  ///return (n_removed == 1);
+  ///int n_removed = hyperedges_.erase(hyperedge->id());
+  ///return n_removed == 1;
   return true;
 }
 
 ////////////////////////////////////////////////////////////////////
 //: ===== High-level Graph operation (handling connectivity) =====
-bool bgrl2_hg_hypergraph::remove_vertex (bgrl2_hg_vertex* vertex)
+bool bgrl2_hg_hypergraph::remove_vertex(bgrl2_hg_vertex* vertex)
 {
   vcl_vector<bgrl2_hg_edge*>::iterator E_it = vertex->connecting_edges().begin();
   while (E_it != vertex->connecting_edges().end()) {
     bgrl2_hg_edge* edge = (*E_it);
-    _internal_disconnect_edge_vertex (edge, vertex);
+    _internal_disconnect_edge_vertex(edge, vertex);
   }
 
   vcl_vector<bgrl2_hg_hyperedge*>::iterator H_it = vertex->connecting_hyperedges().begin();
   while (H_it != vertex->connecting_hyperedges().end()) {
     bgrl2_hg_hyperedge* hyperedge = (*H_it);
-    _internal_disconnect_hyperedge_vertex (hyperedge, vertex);
+    _internal_disconnect_hyperedge_vertex(hyperedge, vertex);
   }
 
-  return _internal_del_vertex (vertex);
+  return _internal_del_vertex(vertex);
 }
 
-/*bool bgrl2_hg_hypergraph::remove_vertex (int id)
-{
-  bgrl2_hg_vertex* vertex = vertices (id);
-  if (vertex)
-    return remove_vertex (vertex);
-  else
-    return false;
-}*/
-
-bool bgrl2_hg_hypergraph::remove_edge (bgrl2_hg_edge* edge)
+bool bgrl2_hg_hypergraph::remove_edge(bgrl2_hg_edge* edge)
 {
   bgrl2_hg_vertex* sv = edge->connecting_s_vertex();
   bgrl2_hg_vertex* ev = edge->connecting_e_vertex();
 
   if (sv)
-    _internal_disconnect_edge_vertex (edge, sv);
+    _internal_disconnect_edge_vertex(edge, sv);
   if (ev)
-    _internal_disconnect_edge_vertex (edge, ev);
+    _internal_disconnect_edge_vertex(edge, ev);
 
   for (unsigned int i=0; i<edge->connecting_hyperedges().size(); i++) {
     bgrl2_hg_hyperedge* hyperedge = edge->connecting_hyperedges(i);
-    _internal_disconnect_hyperedge_edge (hyperedge, edge);
+    _internal_disconnect_hyperedge_edge(hyperedge, edge);
   }
 
-  return _internal_del_edge (edge);
+  return _internal_del_edge(edge);
 }
 
-/*bool bgrl2_hg_hypergraph::remove_edge (int id)
-{
-  bgrl2_hg_edge* edge = edges (id);
-  if (edge)
-    return remove_edge (edge);
-  else
-    return false;
-}*/
-
-bool bgrl2_hg_hypergraph::remove_hyperedge (bgrl2_hg_hyperedge* hyperedge)
+bool bgrl2_hg_hypergraph::remove_hyperedge(bgrl2_hg_hyperedge* hyperedge)
 {
   vcl_vector<bgrl2_hg_edge*>::iterator E_it = hyperedge->connecting_edges().begin();
   while (E_it != hyperedge->connecting_edges().end()) {
     bgrl2_hg_edge* edge = (*E_it);
-    _internal_disconnect_hyperedge_edge (hyperedge, edge);
+    _internal_disconnect_hyperedge_edge(hyperedge, edge);
   }
 
   vcl_vector<bgrl2_hg_vertex*>::iterator V_it = hyperedge->connecting_vertices().begin();
   while (V_it != hyperedge->connecting_vertices().end()) {
     bgrl2_hg_vertex* vertex = (*V_it);
-    _internal_disconnect_hyperedge_vertex (hyperedge, vertex);
+    _internal_disconnect_hyperedge_vertex(hyperedge, vertex);
   }
 
-  return _internal_del_hyperedge (hyperedge);
+  return _internal_del_hyperedge(hyperedge);
 }
 
-/*bool bgrl2_hg_hypergraph::remove_hyperedge (int id)
+#if 0
+bool bgrl2_hg_hypergraph::remove_vertex(int id)
 {
-  bgrl2_hg_hyperedge* hyperedge = hyperedges (id);
-  if (hyperedge)
-    return remove_hyperedge (hyperedge);
+  bgrl2_hg_vertex* vertex = vertices(id);
+  if (vertex)
+    return remove_vertex(vertex);
   else
     return false;
-}*/
+}
+
+bool bgrl2_hg_hypergraph::remove_edge(int id)
+{
+  bgrl2_hg_edge* edge = edges(id);
+  if (edge)
+    return remove_edge(edge);
+  else
+    return false;
+}
+
+bool bgrl2_hg_hypergraph::remove_hyperedge(int id)
+{
+  bgrl2_hg_hyperedge* hyperedge = hyperedges(id);
+  if (hyperedge)
+    return remove_hyperedge(hyperedge);
+  else
+    return false;
+}
+
+bool bgrl2_hg_hypergraph::topo_remove_vertex(int id)
+{
+  bgrl2_hg_vertex* vertex = vertices(id);
+  if (vertex)
+    return topo_remove_vertex(vertex);
+  else
+    return false;
+}
+
+bool bgrl2_hg_hypergraph::topo_remove_edge(int id)
+{
+  bgrl2_hg_edge* edge = edges(id);
+  if (edge)
+    return topo_remove_edge(edge);
+  else
+    return false;
+}
+
+bool bgrl2_hg_hypergraph::topo_remove_hyperedge(int id)
+{
+  //: for bgrl2_hg_hypergraph, can remove the hyperedge directly.
+  return remove_hyperedge(id);
+}
+#endif // 0
 
 
 ////////////////////////////////////////////////////////////////////
 //: ===== High-level Graph operation that the user should use =====
-bool bgrl2_hg_hypergraph::topo_remove_vertex (bgrl2_hg_vertex* vertex)
+bool bgrl2_hg_hypergraph::topo_remove_vertex(bgrl2_hg_vertex* vertex)
 {
   // ! Should check both edges and hyperedges!!
   if (vertex->connecting_edges().size() == 0) {
     assert (vertex->connecting_hyperedges().size() == 0);
-    return remove_vertex (vertex);
+    return remove_vertex(vertex);
   }
   else
     return false;
 }
 
-/*bool bgrl2_hg_hypergraph::topo_remove_vertex (int id)
-{
-  bgrl2_hg_vertex* vertex = vertices (id);
-  if (vertex)
-    return topo_remove_vertex (vertex);
-  else
-    return false;
-}*/
-
-bool bgrl2_hg_hypergraph::topo_remove_edge (bgrl2_hg_edge* edge)
+bool bgrl2_hg_hypergraph::topo_remove_edge(bgrl2_hg_edge* edge)
 {
   if (edge->connecting_hyperedges().size() == 0)
-    return remove_edge (edge);
+    return remove_edge(edge);
   else
     return false;
 }
 
-/*bool bgrl2_hg_hypergraph::topo_remove_edge (int id)
-{
-  bgrl2_hg_edge* edge = edges (id);
-  if (edge)
-    return topo_remove_edge (edge);
-  else
-    return false;
-}*/
-
-bool bgrl2_hg_hypergraph::topo_remove_hyperedge (bgrl2_hg_hyperedge* hyperedge)
+bool bgrl2_hg_hypergraph::topo_remove_hyperedge(bgrl2_hg_hyperedge* hyperedge)
 {
   //: for bgrl2_hg_hypergraph, can remove the hyperedge directly.
-  return remove_hyperedge (hyperedge);
+  return remove_hyperedge(hyperedge);
 }
-
-/*bool bgrl2_hg_hypergraph::topo_remove_hyperedge (int id)
-{
-  //: for bgrl2_hg_hypergraph, can remove the hyperedge directly.
-  return remove_hyperedge (id);
-}*/
--- contrib/brl/bbas/bgrl2/bgrl2_hg_hypergraph.h
+++ contrib/brl/bbas/bgrl2/bgrl2_hg_hypergraph.h
@@ -1,4 +1,6 @@
-// This is contrib/brl/bbas/bgrl2/bgrl2_hg_hypergraph.h
+// This is brl/bbas/bgrl2/bgrl2_hg_hypergraph.h
+#ifndef dbhg_hypergraph_h_
+#define dbhg_hypergraph_h_
 //:
 // \file
 // \author Ming-Ching Chang
@@ -9,88 +11,81 @@
 //   Ozge C. Ozcanli 11/15/08  Moved up to vxl
 // \endverbatim
 
-#ifndef dbhg_hypergraph_h_
-#define dbhg_hypergraph_h_
-
 #include <vcl_map.h>
-#include <vcl_utility.h>
 
 #include <bgrl2/bgrl2_hg_graph.h>
 #include <bgrl2/bgrl2_hg_hyperedge.h>
 
 class bgrl2_hg_hypergraph : public bgrl2_hg_graph
 {
-protected:
+ protected:
   vcl_map<int, bgrl2_hg_hyperedge*> hyperedges_;
 
-public:
+ public:
   //: ====== Hypergraph query functions ======
   vcl_map<int, bgrl2_hg_hyperedge*>& hyperedges() {
     return hyperedges_;
   }
-  bgrl2_hg_hyperedge* hyperedges (const int i) {
-    vcl_map<int, bgrl2_hg_hyperedge*>::iterator H_it = hyperedges_.find (i);
+  bgrl2_hg_hyperedge* hyperedges(const int i) {
+    vcl_map<int, bgrl2_hg_hyperedge*>::iterator H_it = hyperedges_.find(i);
     if (H_it == hyperedges_.end())
       return NULL;
-    
+
     bgrl2_hg_hyperedge* hyperedge = (*H_it).second;
     return hyperedge;
   }
 
   //: ===== Internal Low-level Graph operation (without handling connectivity) =====
-  virtual bool _internal_del_vertex (bgrl2_hg_vertex* vertex);
-  virtual bool _internal_del_edge (bgrl2_hg_edge* edge);
+  virtual bool _internal_del_vertex(bgrl2_hg_vertex* vertex);
+  virtual bool _internal_del_edge(bgrl2_hg_edge* edge);
 
-  void _internal_add_hyperedge (bgrl2_hg_hyperedge* hyperedge);
-  bool _internal_del_hyperedge (bgrl2_hg_hyperedge* hyperedge);
+  void _internal_add_hyperedge(bgrl2_hg_hyperedge* hyperedge);
+  bool _internal_del_hyperedge(bgrl2_hg_hyperedge* hyperedge);
 
   //: ===== Internal Mid-level Graph operation (handling connectivity) =====
-  void _internal_connect_hyperedge_edge (bgrl2_hg_hyperedge* hyperedge, bgrl2_hg_edge* edge) {
+  void _internal_connect_hyperedge_edge(bgrl2_hg_hyperedge* hyperedge, bgrl2_hg_edge* edge) {
     //: unordered edge list
-    hyperedge->connect_edge (edge);
-    edge->connect_hyperedge (hyperedge);
+    hyperedge->connect_edge(edge);
+    edge->connect_hyperedge(hyperedge);
   }
-  void _internal_disconnect_hyperedge_edge (bgrl2_hg_hyperedge* hyperedge, bgrl2_hg_edge* edge) {
+  void _internal_disconnect_hyperedge_edge(bgrl2_hg_hyperedge* hyperedge, bgrl2_hg_edge* edge) {
     //: unordered edge list
-    hyperedge->disconnect_edge (edge);
-    edge->disconnect_hyperedge (hyperedge);
+    hyperedge->disconnect_edge(edge);
+    edge->disconnect_hyperedge(hyperedge);
   }
 
-  void _internal_connect_hyperedge_vertex (bgrl2_hg_hyperedge* hyperedge, bgrl2_hg_vertex* vertex) {
-    hyperedge->connect_vertex (vertex);
-    vertex->connect_hyperedge (hyperedge);
+  void _internal_connect_hyperedge_vertex(bgrl2_hg_hyperedge* hyperedge, bgrl2_hg_vertex* vertex) {
+    hyperedge->connect_vertex(vertex);
+    vertex->connect_hyperedge(hyperedge);
   }
-  void _internal_disconnect_hyperedge_vertex (bgrl2_hg_hyperedge* hyperedge, bgrl2_hg_vertex* vertex) {
-    hyperedge->disconnect_vertex (vertex);
-    vertex->disconnect_hyperedge (hyperedge);
+  void _internal_disconnect_hyperedge_vertex(bgrl2_hg_hyperedge* hyperedge, bgrl2_hg_vertex* vertex) {
+    hyperedge->disconnect_vertex(vertex);
+    vertex->disconnect_hyperedge(hyperedge);
   }
 
   //: ===== High-level Graph operation for insert/remove/replace element =====
-  virtual bool remove_vertex (bgrl2_hg_vertex* vertex);
-  ///virtual bool remove_vertex (int id);
-  virtual bool remove_edge (bgrl2_hg_edge* edge);
-  //virtual bool remove_edge (int id);
-  virtual bool remove_hyperedge (bgrl2_hg_hyperedge* hyperedge);
-  ///virtual bool remove_hyperedge (int id);
+  virtual bool remove_vertex(bgrl2_hg_vertex* vertex);
+  virtual bool remove_edge(bgrl2_hg_edge* edge);
+  virtual bool remove_hyperedge(bgrl2_hg_hyperedge* hyperedge);
+#if 0
+  virtual bool remove_vertex(int id);
+  virtual bool remove_edge(int id);
+  virtual bool remove_hyperedge(int id);
+  virtual bool topo_remove_vertex(int id);
+  virtual bool topo_remove_edge(int id);
+  virtual bool topo_remove_hyperedge(int id);
+#endif // 0
 
   //: ===== High-level Graph operation that user should use =====
   //  These operations are always topologically consistent,
   //  i.e. the resulting graph is always a geometric graph.
-  //  (You will not have an edge that has no end nodes.)  
-  virtual bool topo_remove_vertex (bgrl2_hg_vertex* vertex);
-  ///virtual bool topo_remove_vertex (int id);
-  virtual bool topo_remove_edge (bgrl2_hg_edge* edge);
-  ///virtual bool topo_remove_edge (int id);
-  virtual bool topo_remove_hyperedge (bgrl2_hg_hyperedge* hyperedge);
-  ///virtual bool topo_remove_hyperedge (int id);
+  //  (You will not have an edge that has no end nodes.)
+  virtual bool topo_remove_vertex(bgrl2_hg_vertex* vertex);
+  virtual bool topo_remove_edge(bgrl2_hg_edge* edge);
+  virtual bool topo_remove_hyperedge(bgrl2_hg_hyperedge* hyperedge);
 
-  bgrl2_hg_hypergraph () :
-    bgrl2_hg_graph ()
-  {
-  }
-  virtual ~bgrl2_hg_hypergraph ()
-  {
-  }
+  bgrl2_hg_hypergraph() : bgrl2_hg_graph() {}
+  virtual ~bgrl2_hg_hypergraph() {}
 };
 
 #endif
