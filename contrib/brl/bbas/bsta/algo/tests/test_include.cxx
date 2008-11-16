3cb5602eef6cd481dd96ea8d5f2f11e3b4b55b8e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-11-16 17:49:37 -0500

    added new header files

---
+++ contrib/brl/bbas/bsta/algo/tests/test_include.cxx
@@ -0,0 +1,9 @@
+#include <bsta/algo/bsta_adaptive_updater.h>
+#include <bsta/algo/bsta_bayes_functor.h>
+#include <bsta/algo/bsta_fit_weibull.h>
+#include <bsta/algo/bsta_gaussian_stats.h>
+#include <bsta/algo/bsta_gaussian_updater.h>
+#include <bsta/algo/bsta_mixture_functors.h>
+#include <bsta/algo/bsta_truth_updater.h>
+
+int main() { return 0; }
