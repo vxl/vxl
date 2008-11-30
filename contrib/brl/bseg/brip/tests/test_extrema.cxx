cac5c50a7747a8ff5e0a1fafc3557a7dab697497
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-11-30 07:56:06 -0500

    avoid compiler warning (removed unused variables "cc", "rc"; added test for variables "m45b", "m45d")

--- contrib/brl/bseg/brip/tests/test_extrema.cxx
+++ contrib/brl/bseg/brip/tests/test_extrema.cxx
@@ -1,4 +1,5 @@
 // This is brl/bseg/brip/tests/test_extrema.cxx
+
 #include <vil/vil_image_view.h>
 #include <brip/brip_vil_float_ops.h>
 #include <testlib/testlib_test.h>
@@ -12,10 +13,10 @@ static void test_extrema()
   brip_vil_float_ops::extrema_kernel_mask(lambda0, lambda1, 45.0f,
                                           kernel45, mask45);
   unsigned nck45 = kernel45.cols(), nrk45 = kernel45.rows();
-  unsigned cc = (nck45-1)/2, rc= (nrk45-1)/2;
 #if 0
-  for(int k = -3; k<=3; ++k){
-    for(int m = -3; m<=3; ++m)
+  unsigned cc = (nck45-1)/2, rc= (nrk45-1)/2;
+  for (int k = -3; k<=3; ++k){
+    for (int m = -3; m<=3; ++m)
       vcl_cout << kernel45[rc+k][cc+m] << ' ';
     vcl_cout << '\n';
   }
@@ -27,9 +28,9 @@ static void test_extrema()
   unsigned nck135 = kernel135.cols(), nrk135 = kernel135.rows();
   bool good = nck135==nck45 && nrk45 == nrk135;
   float sum = 0.0f;
-  if(good)
-    for(unsigned r = 0; r<nrk45; ++r)
-      for(unsigned c = 0; c<nck45; ++c)
+  if (good)
+    for (unsigned r = 0; r<nrk45; ++r)
+      for (unsigned c = 0; c<nck45; ++c)
         sum += kernel45[r][c]-kernel135[c][r];
   good = good && vcl_fabs(sum)<1e-5;
   TEST("mask kernel ", good, true);
@@ -47,6 +48,7 @@ static void test_extrema()
     brip_vil_float_ops::extrema(im,lambda0, lambda1, 45.0f, true, true);
   float r45b = resd(16,16,0), m45b = resd(13,16, 1);
   TEST_NEAR("bright response", r45b, 0.46213278, 1e-06);
+  TEST_NEAR("bright response", m45b, 0.46213278, 1e-06);
   im.fill(1.0f);
   im(13,14) = 0.0f;  im(14,14) = 0.0f;   im(15,14) = 0.0f;
   im(14,15) = 0.0f;  im(15,15) = 0.0f;   im(16,15) = 0.0f;
@@ -57,7 +59,7 @@ static void test_extrema()
   resd = brip_vil_float_ops::extrema(im,lambda0, lambda1, 45.0f, false, true);
   float r45d = resd(16,16,0), m45d = resd(13,16, 1);
   TEST_NEAR("dark response", r45d, 0.42070714, 1e-06);
-
+  TEST_NEAR("dark response", m45d, 0.42070714, 1e-06);
 }
 
 TESTMAIN(test_extrema);
