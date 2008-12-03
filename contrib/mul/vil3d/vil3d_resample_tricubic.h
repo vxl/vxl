99bb10241e03fb3dd25279ef270b8e4df2919ce6
Gwenael Guillard <gwenaelguillard@users.sourceforge.net>
2008-12-03 11:09:23 -0500

    Added tricubic interpolation

---
+++ contrib/mul/vil3d/vil3d_resample_tricubic.h
@@ -0,0 +1,99 @@
+#ifndef vil3d_resample_tricubic_h_
+#define vil3d_resample_tricubic_h_
+
+//:
+// \file mul/vil3d/vil3d_resample_tricubic.h
+// \brief Resample a 3D image by a tricubic method
+// \author Gwenael Guillard
+
+#include <vil3d/vil3d_image_view.h>
+
+
+//: Sample grid of points in one image and place in another, using
+//  tricubic interpolation.
+//  dest_image(i,j,k,p) is sampled from the src_image at
+//  (x0+i.dx1+j.dx2+k.dx3, y0+i.dy1+j.dy2+k.dy3, z0+i.dz1+j.dz2+k.dz3),
+//  where i=[0..n1-1], j=[0..n2-1], k=[0..n3-1].
+//  dest_image resized to (n1,n2,n3,src_image.nplanes())
+//  Points outside interpolatable region return zero.
+template <class S, class T>
+void vil3d_resample_tricubic(const vil3d_image_view<S>& src_image,
+                             vil3d_image_view<T>& dest_image,
+                             double x0, double y0, double z0,
+                             double dx1, double dy1, double dz1,
+                             double dx2, double dy2, double dz2,
+                             double dx3, double dy3, double dz3,
+                             int n1, int n2, int n3);
+
+
+//: Sample grid of points in one image and place in another, using tricubic interpolation and edge extension.
+//  dest_image(i,j,k,p) is sampled from the src_image at
+//  (x0+i.dx1+j.dx2+k.dx3, y0+i.dy1+j.dy2+k.dy3, z0+i.dz1+j.dz2+k.dz3),
+//  where i=[0..n1-1], j=[0..n2-1], k=[0..n3-1].
+//  dest_image resized to (n1,n2,n3,src_image.nplanes())
+//  Points outside interpolatable return the value of the nearest valid pixel.
+template <class S, class T>
+void vil3d_resample_tricubic_edge_extend(const vil3d_image_view<S>& src_image,
+                                         vil3d_image_view<T>& dest_image,
+                                         double x0, double y0, double z0,
+                                         double dx1, double dy1, double dz1,
+                                         double dx2, double dy2, double dz2,
+                                         double dx3, double dy3, double dz3,
+                                         int n1, int n2, int n3);
+
+//: Sample grid of points in one image and place in another, using tricubic interpolation and edge extension.
+//  dest_image(i,j,k,p) is sampled from the src_image at
+//  (x0+i.dx1+j.dx2+k.dx3, y0+i.dy1+j.dy2+k.dy3, z0+i.dz1+j.dz2+k.dz3),
+//  where i=[0..n1-1], j=[0..n2-1], k=[0..n3-1].
+//  dest_image resized to (n1,n2,n3,src_image.nplanes())
+//  Points outside interpolatable return the value of the nearest valid pixel.
+template <class S, class T>
+void vil3d_resample_tricubic_edge_extend(const vil3d_image_view<S>& src_image,
+                                         vil3d_image_view<T>& dest_image,
+                                         int n1, int n2, int n3);
+
+template <class S, class T>
+void vil3d_resample_tricubic_edge_trilin_extend(const vil3d_image_view<S>& src_image,
+                                                vil3d_image_view<T>& dest_image,
+                                                double x0, double y0, double z0,
+                                                double dx1, double dy1, double dz1,
+                                                double dx2, double dy2, double dz2,
+                                                double dx3, double dy3, double dz3,
+                                                int n1, int n2, int n3);
+
+template <class S, class T>
+void vil3d_resample_tricubic_edge_trilin_extend(const vil3d_image_view<S>& src_image,
+                                                vil3d_image_view<T>& dest_image,
+                                                int n1, int n2, int n3);
+
+//: Resample image to a specified dimensions (n1 * n2 * n3)
+template <class S, class T>
+void vil3d_resample_tricubic(const vil3d_image_view<S>& src_image,
+                             vil3d_image_view<T>& dest_image,
+                             int n1, int n2, int n3);
+
+
+// //: Resample a 3D image by a different factor in each dimension.
+// //  \p dst_image resized by factors \p dx, \p dy, \p dz.
+// // \note The upper image boundaries are extended.
+// // \param dx Scaling factor >1
+// // \param dy Scaling factor >1
+// // \param dz Scaling factor >1
+// //  dst_image(i, j, k, p) is sampled from src_image(i/dx, j/dy, k/dz, p).
+// // Interpolated values are rounded when the type T is smaller than double.
+// template <class T>
+// void vil3d_resample_trilinear(const vil3d_image_view<T>& src_image,
+//                               vil3d_image_view<T>& dst_image,
+//                               const double dx,
+//                               const double dy,
+//                               const double dz);
+
+// //: Resample a 3D image by a factor of 2 in each dimension.
+// // \p dst_image is resized to 2*src_image.n?()-1 in each direction.
+// // Interpolated values are truncated when the type T is smaller than double.
+// template <class T>
+// void vil3d_resample_trilinear_scale_2(
+//   const vil3d_image_view<T>& src_image,
+//   vil3d_image_view<T>& dst_image);
+
+#endif // vil3d_resample_tricubic_h_
---
+++ contrib/mul/vil3d/vil3d_tricub_interp.h
@@ -0,0 +1,101 @@
+#ifndef vil3d_tricub_interp_h_
+#define vil3d_tricub_interp_h_
+//:
+// \file
+// \brief Tricubic interpolation functions for 3D images
+//
+#include <vcl_cassert.h>
+#include <vcl_cstddef.h>
+#include <vil3d/vil3d_image_view.h>
+#include <vil3d/vil3d_trilin_interp.h>
+
+//: Compute tricubic interpolation at (x,y,z), no bound checks
+//  Image is nx * ny * nz array of T. x,y,z element is data[z*zstep+ystep*y+x*xstep]
+//  No bound checks are done.
+template<class T>
+double vil3d_tricub_interp_raw(double x, double y, double z, const T* data,
+                               vcl_ptrdiff_t xstep, vcl_ptrdiff_t ystep, vcl_ptrdiff_t zstep);
+
+//: Compute tricubic interpolation at (x,y,z), with bound checks
+//  Image is nx * ny * nz array of T. x,y,z element is data[z*zstep+ystep*y+x*xstep]
+//  If (x,y,z) is outside interpolatable image region, zero is returned.
+//  The safe interpolatable region is [1,nx-3]*[1,ny-3]*[1,nz-3].
+template<class T>
+inline double vil3d_tricub_interp_safe(double x, double y, double z, const T* data,
+                                     int nx, int ny, int nz,
+                                     vcl_ptrdiff_t xstep, vcl_ptrdiff_t ystep, vcl_ptrdiff_t zstep)
+{
+    if (x<1) return 0.0;
+    if (y<1) return 0.0;
+    if (z<1) return 0.0;
+    if (x>nx-3) return 0.0;
+    if (y>ny-3) return 0.0;
+    if (z>nz-3) return 0.0;
+    return vil3d_tricub_interp_raw(x,y,z,data,xstep,ystep,zstep);
+}
+
+//: Compute tricubic interpolation at (x,y,z), with minimal bound checks
+//  Image is nx * ny * nz array of Ts. x,y,z element is data[ystep*y+xstep*x]
+//  If (x,y) is outside interpolatable image region and NDEBUG is not defined
+//  the code will fail an ASSERT.
+//  The safe interpolatable region is [1,nx-2]*[1,ny-2]*[1,nz-2].
+template<class T>
+inline double vil3d_tricub_interp(double x, double y, double z, const T* data,
+                                  int nx, int ny, int nz,
+                                  vcl_ptrdiff_t xstep, vcl_ptrdiff_t ystep, vcl_ptrdiff_t zstep)
+{
+    assert (x>=1);
+    assert (y>=1);
+    assert (z>=1);
+    assert (x<=nx-3);
+    assert (y<=ny-3);
+    assert (z<=nz-3);
+    return vil3d_tricub_interp_raw(x,y,z,data,xstep,ystep,zstep);
+}
+
+//: Compute tricubic interpolation at (x,y,z), with bound checks
+//  Image is nx * ny array of Ts. x,y element is data[nx*y+x]
+//  If (x,y) is outside safe interpolatable image region, nearest pixel value is returned.
+//  The safe interpolatable region is [1,nx-2]*[1,ny-2].
+template<class T>
+inline double vil3d_tricub_interp_safe_extend(double x, double y, double z,
+                                              const T* data,
+                                              int nx, int ny, int nz,
+                                              vcl_ptrdiff_t xstep, vcl_ptrdiff_t ystep, vcl_ptrdiff_t zstep)
+{
+  if (x<0.9999999) x= 0.0;
+  else if (x>nx-3.0000001 && x<nx-1.9999999) x=nx-2.0;
+  else if (x>nx-2.0000001) x=nx-1.0;
+
+  if (y<0.9999999) y= 0.0;
+  else if (y>ny-3.0000001 && y<ny-1.9999999) y=ny-2.0;
+  else if (y>ny-2.0000001) y=ny-1.0;
+
+  if (z<0.9999999) z= 0.0;
+  else if (z>nz-3.0000001 && z<nz-1.9999999) z=nz-2.0;
+  else if (z>nz-2.0000001) z=nz-1.0;
+
+  return vil3d_tricub_interp_raw(x,y,z,data,xstep,ystep,zstep);
+}
+
+template<class T>
+double vil3d_tricub_interp_safe_trilinear_extend(double x, double y, double z,
+                                                 const T* data,
+                                                 int nx, int ny, int nz,
+                                                 vcl_ptrdiff_t xstep, vcl_ptrdiff_t ystep, vcl_ptrdiff_t zstep);
+
+
+// //: Compute tricubic interpolation at (x,y,z), with bound checks
+// //  If (x,y,z) is outside safe interpolatable image region, nearest pixel value is returned.
+// //  The safe interpolatable region is [1,view.ni()-2]*[1,view.nj()-2]*[1,view.nk()-2].
+// // \relates vil3d_image_view
+// template<class T>
+// inline double vil3d_tricub_interp_safe_extend(const vil3d_image_view<T> &view,
+//                                               double x, double y, double z, unsigned p=0)
+// {
+//   return vil3d_tricub_interp_safe_extend(x, y, z, &view(0,0,p),
+//                                          view.ni(), view.nj(), view.nk(),
+//                                          view.istep(), view.jstep(), view.kstep() );
+// }
+
+#endif // vil3d_tricub_interp_h_
