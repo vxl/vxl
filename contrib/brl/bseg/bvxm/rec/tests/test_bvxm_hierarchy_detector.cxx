959142d65e85f0d8c4d2a4fd8386d74df4d2b035
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-12-05 18:39:12 -0500

    removed unused #includes

--- contrib/brl/bseg/bvxm/rec/tests/test_bvxm_hierarchy_detector.cxx
+++ contrib/brl/bseg/bvxm/rec/tests/test_bvxm_hierarchy_detector.cxx
@@ -1,8 +1,6 @@
 #include <testlib/testlib_test.h>
 #include <vcl_iostream.h>
 #include <vcl_vector.h>
-#include <vcl_cmath.h>
-#include <vul/vul_file.h>
 
 #include <rec/bvxm_part_base.h>
 #include <rec/bvxm_part_base_sptr.h>
@@ -34,13 +32,13 @@ static void test_bvxm_hierarchy_detector()
   // insert  points
   //vgl_point_2d<float> p0(0.0f, 0.1f), p1(1.0f,0.0f),
     //p2(1.0f, 1.0f), p3(0.5f, 0.5f), p4(0.65f, 0.65f);
-  bvxm_part_instance_sptr p0i = new bvxm_part_instance(0, 0, 1, 0.0f, 0.0f, 0.1f); 
-  bvxm_part_instance_sptr p1i = new bvxm_part_instance(0, 0, 1, 1.0f, 0.0f, 0.1f); 
-  bvxm_part_instance_sptr p2i = new bvxm_part_instance(0, 0, 1, 1.0f, 1.0f, 0.1f); 
-  bvxm_part_instance_sptr p3i = new bvxm_part_instance(0, 0, 1, 0.5f, 0.5f, 0.1f); 
-  bvxm_part_instance_sptr p4i = new bvxm_part_instance(0, 0, 1, 0.65f, 0.65f, 0.1f); 
+  bvxm_part_instance_sptr p0i = new bvxm_part_instance(0, 0, 1, 0.0f, 0.0f, 0.1f);
+  bvxm_part_instance_sptr p1i = new bvxm_part_instance(0, 0, 1, 1.0f, 0.0f, 0.1f);
+  bvxm_part_instance_sptr p2i = new bvxm_part_instance(0, 0, 1, 1.0f, 1.0f, 0.1f);
+  bvxm_part_instance_sptr p3i = new bvxm_part_instance(0, 0, 1, 0.5f, 0.5f, 0.1f);
+  bvxm_part_instance_sptr p4i = new bvxm_part_instance(0, 0, 1, 0.65f, 0.65f, 0.1f);
   trc.add(p0i);   trc.add(p1i);   trc.add(p2i);   trc.add(p3i); trc.add(p4i);
-  
+
   // test contains method
   bool p1_in = trc.contains(p1i);
   TEST("test contains", p1_in, true);
@@ -51,7 +49,7 @@ static void test_bvxm_hierarchy_detector()
   bbc.add(pb0c); bbc.add(pb1c);
   vcl_vector<bvxm_part_instance_sptr> foundc;
   trc.get(bbc, foundc);
-  
+
   TEST("test region search",foundc[0] , p3i);
   //test iterator
   vcl_cout << "Traversing point_box rtree, num nodes = "<< trc.nodes()<< '\n';
@@ -69,7 +67,7 @@ static void test_bvxm_hierarchy_detector()
     return;
 
   unsigned ni = img->ni(); unsigned nj = img->nj();
-  
+
   vcl_cout << "image ni: " << ni << " nj: " << nj << vcl_endl;
 
   //bvxm_part_hierarchy_sptr h = bvxm_part_hierarchy_builder::construct_detector_roi1_2();
@@ -96,7 +94,7 @@ static void test_bvxm_hierarchy_detector()
   vcl_vector<bvxm_part_instance_sptr> parts_prims = hd.get_parts(0);
   vcl_cout << "\t extracted " << parts_prims.size() << " primitives\n";
 
-  //: test vgl_rtree_point_box_2d on parts_prims
+  // test vgl_rtree_point_box_2d on parts_prims
   typedef vgl_rtree_point_box_2d<float> C_; // the helper class
   typedef C_::v_type V_; // the contained object type
   typedef C_::b_type B_; // the bounding object type
@@ -108,7 +106,7 @@ static void test_bvxm_hierarchy_detector()
     vgl_point_2d<float> p1(parts_prims[i]->x_, parts_prims[i]->y_);
     tr_vxl.add(p1);
   }
-  
+
   // test contains method
   bool p0_in = tr_vxl.contains(p0);
   TEST("test contains", p0_in, true);
@@ -128,10 +126,10 @@ static void test_bvxm_hierarchy_detector()
   vcl_vector<bvxm_part_instance_sptr> found;
   tr->get(bb, found);
   unsigned n = found.size();
-  //for(unsigned i = 0; i<n; ++i)
+  //for (unsigned i = 0; i<n; ++i)
   //  vcl_cout << "point(s) in region " << found[i] << '\n';;
   //TEST("test region search", n, 2);
-  if(n>=2) {
+  if (n>=2) {
     TEST("test region search",found[0] , parts_prims[0]);
     TEST("test region search",found[1] , parts_prims[1]);
   }
@@ -151,15 +149,15 @@ static void test_bvxm_hierarchy_detector()
   vgl_box_2d<float> bb2;
   pb0.set(383, 189); pb1.set(389, 194);
   vgl_point_2d<float> pb2(393, 198);//, pb3(370, 150), pb4(400,200);
-  bb2.add(pb0); 
-  bb2.add(pb1); 
+  bb2.add(pb0);
+  bb2.add(pb1);
   bb2.add(pb2); //bb2.add(pb3);
   //bb2.add(pb3);
   //bb2.add(pb4);
   bool dummy = bb2.contains(pb1);
   bool dummy1 = bb2.contains(pb0);
   bool dummy2 = bb2.contains(pb2);
-  //pb0.set(393, 198); 
+  //pb0.set(393, 198);
   //bb2.add(pb0);
   found.clear();
   //tr->get(bb2, found);
@@ -172,7 +170,7 @@ static void test_bvxm_hierarchy_detector()
   unsigned highest = h->highest_layer_id();
   vcl_vector<bvxm_part_instance_sptr> parts_upper_most = hd.get_parts(highest);
   vcl_cout << "\t extracted " << parts_upper_most.size() << " parts from highest layer: " << highest << vcl_endl;
-  
+
   vil_image_view<float> output_map_float(ni, nj);
   bvxm_part_hierarchy::generate_output_map(parts_upper_most, output_map_float);
   float min, max;
@@ -191,23 +189,17 @@ static void test_bvxm_hierarchy_detector()
   //test rotation with a quaternion
   //vnl_vector_fixed<float, 2> v = pp->direction_vector();  // get orientation vector of central part: pi
   vnl_vector_fixed<float,2> v = parts_prims[0]->direction_vector();
-  vcl_cout << " parts prims theta: " << parts_prims[0]->cast_to_gaussian()->theta_ << vcl_endl;
-  vcl_cout << " direction vector 0: " << v[0] << " 1: " << v[1] << vcl_endl;
-  
-  //: define a rotation about z axis (in the image plane)
-  vnl_quaternion<float> q(0.0f, 0.0f, float(vnl_math::pi/2.0f));
-  
+  vcl_cout << " parts prims theta: " << parts_prims[0]->cast_to_gaussian()->theta_ << vcl_endl
+           << " direction vector 0: " << v[0] << " 1: " << v[1] << vcl_endl;
+
+  // define a rotation about z axis (in the image plane)
+  vnl_quaternion<float> q(0.0f, 0.0f, float(vnl_math::pi/2.0));
+
   vnl_vector_fixed<float,3> v3d(v[0], v[1], 0.0f);
   vnl_vector_fixed<float,3> out = q.rotate(v3d);
   vcl_cout << " direction vector after rotation by 90 degrees 0: " << out[0] << " 1: " << out[1] << " 2: " << out[2] << vcl_endl;
   vnl_vector_fixed<float,3> out_dist = out*5.0f;
   vcl_cout << " direction vector after scaling by 5: " << out_dist[0] << " 1: " << out_dist[1] << vcl_endl;
-
 }
 
 TESTMAIN( test_bvxm_hierarchy_detector );
-
-
-
-
-
