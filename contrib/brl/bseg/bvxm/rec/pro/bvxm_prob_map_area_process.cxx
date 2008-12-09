97bc6086fe6516ecc5ddfe12bd6bd616a97480ba
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2008-12-09 15:02:36 -0500

    new processes to detect foreground glitches on background

--- contrib/brl/bseg/bvxm/rec/pro/bvxm_prob_map_area_process.cxx
+++ contrib/brl/bseg/bvxm/rec/pro/bvxm_prob_map_area_process.cxx
@@ -9,28 +9,26 @@
 #include <vil/vil_image_view.h>
 #include <vil/vil_convert.h>
 #include <vil/vil_math.h>
+#include <vil/vil_save.h>
 #include <brip/brip_vil_float_ops.h>
 #include <core/vidl2_pro/vidl2_pro_utils.h>
-
+#include <rec/bvxm_glitch.h>
 
 //: Constructor
 bvxm_prob_map_area_process::bvxm_prob_map_area_process()
 {
   //input
-  input_data_.resize(2,brdb_value_sptr(0));
-  input_types_.resize(2);
+  input_data_.resize(3,brdb_value_sptr(0));
+  input_types_.resize(3);
   input_types_[0]= "vil_image_view_base_sptr"; //input probability frame
   input_types_[1]= "vil_image_view_base_sptr"; //input probability frame's mask
+  input_types_[2]= "unsigned"; // size of the inner-square to measure area (e.g. 5 means 5x5 area mask)
 
   //output
-  output_data_.resize(6, brdb_value_sptr(0));
-  output_types_.resize(6);
+  output_data_.resize(2, brdb_value_sptr(0));
+  output_types_.resize(2);
   output_types_[0] = "vil_image_view_base_sptr";
   output_types_[1] = "vil_image_view_base_sptr";
-  output_types_[2] = "vil_image_view_base_sptr";  // output the glitch map as well
-  output_types_[3] = "vil_image_view_base_sptr";  // output the glitch map as well
-  output_types_[4] = "vil_image_view_base_sptr";  // output the glitch map as well
-  output_types_[5] = "vil_image_view_base_sptr";  // output the glitch map as well
 }
 
 //: Execute the process
@@ -57,28 +55,28 @@ bvxm_prob_map_area_process::execute()
   temp = input1->value();
   vil_image_view<bool> input_mask = *vil_convert_cast(bool(), temp);
 
+  brdb_value_t<unsigned>* input2 = static_cast<brdb_value_t<unsigned>* >(input_data_[2].ptr());
+  unsigned c_size = input2->value();  // center size
+
   vil_image_view<float> out(ni, nj, 1);
   out.fill(0);
 
   //: g_b will contain the normalized glitch probability and will be returned
   vil_image_view<float> g_b(ni, nj, 1), g_bb(ni, nj, 1), g_ff(ni, nj, 1), g_fb(ni, nj, 1);
-  g_b.fill(0); g_bb.fill(0); g_ff.fill(0); g_fb.fill(0);
+  g_b.fill(-1.0f); g_bb.fill(-1.0f); g_ff.fill(-1.0f); g_fb.fill(-1.0f);
 
   vcl_vector<vcl_pair<int, int> > neighborhood;
-  int ns = 2; // for 5x5 neighborhood
-  //int ns = 1; // for 3x3 neighborhood
-  for (int l = -ns; l < ns+1; l++)
-    for (int m = -ns; m < ns+1; m++)
-      neighborhood.push_back(vcl_pair<int, int>(l,m));
-  neighborhood.pop_back(); // pop the last one to have equal number of pixels in the center and in the surround
+  vcl_vector<vcl_pair<int, int> > neighborhood_outer;
+  //: use square glitch to find a center of c_size x c_size
+  bvxm_glitch::square_glitch(c_size, neighborhood, neighborhood_outer);
 
-  for (unsigned j = 3; j<nj-3; ++j)
-    for (unsigned i = 3; i<ni-3; ++i)
+  for (unsigned j = 0; j<nj; ++j)
+    for (unsigned i = 0; i<ni; ++i)
     {
       if (input_mask(i,j)) {
         float sum = 0.0f;
         bool all_inside = true;
-        float prod_back = 1.0f; float prod_fore = 1.0f;
+      
         for (unsigned t = 0; t < neighborhood.size(); t++) {
           int ii = i+neighborhood[t].first;
           int jj = j+neighborhood[t].second;
@@ -87,17 +85,12 @@ bvxm_prob_map_area_process::execute()
               all_inside = false;
               break;
             }
+            
             sum += map(ii,jj);
-            prod_back *= (1.0f-map(ii, jj));
-            prod_fore *= map(ii, jj);
           }
         }
         if (all_inside) {
           out(i,j) = sum;
-          g_b(i,j) = prod_fore;
-          g_bb(i,j) = prod_back;
-          g_ff(i,j) = prod_fore;
-          g_fb(i,j) = prod_back;
         }
       }
     }
@@ -120,78 +113,6 @@ bvxm_prob_map_area_process::execute()
   brdb_value_sptr output1 =
     new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<vxl_byte>(out_byte));
   output_data_[1] = output1;
-
-  vil_image_view<float> out2(ni, nj, 1);
-  out2.fill(0);
-  vcl_vector<vcl_pair<int, int> > neighborhood_outer;
-  for (int l = -(ns+1); l < (ns+2); l++)
-    neighborhood_outer.push_back(vcl_pair<int, int>(l,-(ns+1)));
-  for (int l = -(ns+1); l < (ns+2); l++)
-    neighborhood_outer.push_back(vcl_pair<int, int>(l,(ns+1)));
-  for (int m = -(ns+1); m < (ns+2); m++)
-    neighborhood_outer.push_back(vcl_pair<int, int>(-(ns+1),m));
-  for (int m = -(ns+1); m < (ns+2); m++)
-    neighborhood_outer.push_back(vcl_pair<int, int>((ns+1),m));
-
-  for (unsigned j = 3; j<nj-3; ++j)
-    for (unsigned i = 3; i<ni-3; ++i)
-    {
-      if (input_mask(i,j)) {
-        float sum = 0.0f;
-        bool all_inside = true;
-        float prod_back = 1.0f; float prod_fore = 1.0f;
-        for (unsigned t = 0; t < neighborhood_outer.size(); t++) {
-          int ii = i+neighborhood_outer[t].first;
-          int jj = j+neighborhood_outer[t].second;
-          if (ii >= 0 && jj >= 0 && ii < (int)ni && jj < (int)nj) {
-            if (!input_mask(ii, jj)) {
-              all_inside = false;
-              break;
-            }
-            sum += map(ii,jj);
-            prod_back *= (1.0f-map(ii, jj));
-            prod_fore *= map(ii, jj);
-          }
-        }
-        if (all_inside) {
-          out2(i,j) = (float)vcl_abs(sum-out(i,j));
-          g_b(i,j) = g_b(i,j)*prod_back;
-          g_bb(i,j) = g_bb(i,j)*prod_back;
-          g_ff(i,j) = g_ff(i,j)*prod_fore;
-          g_fb(i,j) = g_fb(i,j)*prod_fore;
-        }
-      }
-    }
-
-  brdb_value_sptr output2 = new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<float>(out2));
-  output_data_[2] = output2;
-
-  vil_math_value_range(out2, min, max);
-  vcl_cout << "\t glitch map min: " << min << " max: " << max << vcl_endl;
-  vil_image_view<vxl_byte> out_byte2(ni, nj, 1);
-  vil_convert_stretch_range_limited(out2, out_byte2, 0.0f, max);
-
-  brdb_value_sptr output3 = new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<vxl_byte>(out_byte2));
-  output_data_[3] = output3;
-
-  //: normalize the g_b map
-  for (unsigned j = 3; j<nj-3; ++j) {
-    for (unsigned i = 3; i<ni-3; ++i) {
-      if (g_bb(i,j) <= 0)  // if not on a valid pixel continue;
-        continue;
-      g_b(i,j) = g_b(i,j)/(g_bb(i,j) + g_ff(i,j) + g_fb(i,j) + g_b(i,j));
-    }
-  }
-
-  brdb_value_sptr output4 = new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<float>(g_b));
-  output_data_[4] = output4;
-
-  vil_math_value_range(g_b, min, max);
-  vcl_cout << "\t glitch normalized prob map min: " << min << " max: " << max << vcl_endl;
-  vil_image_view<vxl_byte> out_byte3(ni, nj, 1);
-  vil_convert_stretch_range_limited(g_b, out_byte3, 0.0f, max);
-  brdb_value_sptr output5 = new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<vxl_byte>(out_byte3));
-  output_data_[5] = output5;
 
   return true;
 }
