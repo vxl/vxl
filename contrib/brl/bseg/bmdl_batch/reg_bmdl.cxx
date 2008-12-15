ef3cf879968db94ab26683ff1ff2c0b59c4438e8
Gamze Tunali <gamze@users.sourceforge.net>
2008-12-15 14:13:00 -0500

    using bpro_batch library

--- contrib/brl/bseg/bmdl_batch/reg_bmdl.cxx
+++ contrib/brl/bseg/bmdl_batch/reg_bmdl.cxx
@@ -1,4 +1,4 @@
-#include "batch_bmdl.h"
+#include "reg_bmdl.h"
 #include <bprb/bprb_macros.h>
 #include <bprb/bprb_batch_process_manager.h>
 // processes
@@ -63,3 +63,30 @@ register_datatypes(PyObject *self, PyObject *args)
   Py_INCREF(Py_None);
   return Py_None;
 }
+
+PyMODINIT_FUNC
+initbmdl_batch(void)
+{
+  PyMethodDef reg_pro;
+  reg_pro.ml_name = "register_processes";
+  reg_pro.ml_meth = register_processes;
+  reg_pro.ml_doc = "register_processes() create instances of each defined process";
+  reg_pro.ml_flags = METH_VARARGS;
+  
+
+  PyMethodDef reg_data;
+  reg_data.ml_name = "register_datatypes";
+  reg_data.ml_meth = register_datatypes;
+  reg_data.ml_doc = "register_datatypes() insert tables in the database for each type";
+  reg_data.ml_flags = METH_VARARGS;
+  
+  bmdl_batch_methods[0]=reg_pro;
+  bmdl_batch_methods[1]=reg_data;
+  
+  for (unsigned i=0; i<METHOD_NUM; i++) {
+    bmdl_batch_methods[i+2]=batch_methods[i];
+  }
+
+  Py_InitModule("bmdl_batch", bmdl_batch_methods);
+}
+
