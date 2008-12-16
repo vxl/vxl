aab91e23a5e7f165109eead6b2c047f81d65b2da
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-12-16 16:09:36 -0500

    added new header file

--- contrib/brl/bseg/brec/pro/tests/test_include.cxx
+++ contrib/brl/bseg/brec/pro/tests/test_include.cxx
@@ -1,13 +1,14 @@
+#include <brec/pro/brec_bayesian_update_process.h>
 #include <brec/pro/brec_change_area_process.h>
+#include <brec/pro/brec_density_to_prob_map_process.h>
+#include <brec/pro/brec_glitch_process.h>
+#include <brec/pro/brec_glitch_overlay_process.h>
 #include <brec/pro/brec_prob_map_area_process.h>
 #include <brec/pro/brec_prob_map_roc_compute_process.h>
 #include <brec/pro/brec_prob_map_supress_process.h>
 #include <brec/pro/brec_prob_map_threshold_process.h>
-#include <brec/pro/brec_density_to_prob_map_process.h>
 #include <brec/pro/brec_recognize_structure_process.h>
+#include <brec/pro/brec_save_mog_image_process.h>
 #include <brec/pro/brec_update_changes_process.h>
-#include <brec/pro/brec_glitch_process.h>
-#include <brec/pro/brec_glitch_overlay_process.h>
-#include <brec/pro/brec_bayesian_update_process.h>
 
 int main() { return 0; }
