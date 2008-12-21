2448b891465d34c0d364783c0b9c56dcd5ca12b5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-12-21 12:36:20 -0500

    *** empty log message ***

---
+++ config/config-win32-gcc.mk
@@ -0,0 +1,38 @@
+OBJBASE:=
+no_x := 1
+LIB_EXT := dll
+AR_EXT := lib
+MKDIR_P := mkdir -p
+optimize :=
+optimize_for_speed :=
+profile := 
+RANLIB=:
+LORDER=:
+LIB_PREFIX :=
+C++ := c++
+CC  := gcc
+
+wall := -Wall
+
+STRIP := :
+RM := rm -f
+CP := cp
+MAKE_SHLIB = c++
+
+MAKE_EXE := c++
+
+MAKE_ARLIB = c++
+CCDRIVER := $(C++)
+
+HAS_SH_LINK := 1
+OBJ_EXT := obj
+EXESUFFIX := .exe
+PROGSUFFIX :=
+
+lib_m :=
+MAKE = make
+
+TJWD = pwd
+
+no_implicit_templates :=
+implicit_templates := -DINSTANTIATE_TEMPLATES
