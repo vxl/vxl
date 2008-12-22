266bc7e736568986c5eb9b4489209472203f3d6e
Joseph L. Mundy <mundyj@users.sourceforge.net>
2008-12-22 14:03:51 -0500

    added standard dev

--- contrib/brl/bseg/bbgm/pro/bbgm_display_dist_image_process.cxx
+++ contrib/brl/bseg/bbgm/pro/bbgm_display_dist_image_process.cxx
@@ -17,6 +17,7 @@
 #include <vbl/io/vbl_io_smart_ptr.h>
 #include <vil/vil_image_view.h>
 #include <vil/vil_convert.h>
+#include <vil/vil_math.h>
 #include <bsta/bsta_histogram.h>
 #include <bbgm/bbgm_viewer.h>
 #include <bbgm/bbgm_viewer_sptr.h>
@@ -25,11 +26,12 @@
 bbgm_display_dist_image_process::bbgm_display_dist_image_process()
 {
   //input
-  input_data_.resize(3,brdb_value_sptr(0));
-  input_types_.resize(3);
+  input_data_.resize(4,brdb_value_sptr(0));
+  input_types_.resize(4);
   input_types_[0]= "bbgm_image_sptr"; //background image
   input_types_[1]= "vcl_string"; //what to display, e.g. mean, variance etc.
   input_types_[2]= "int"; //the component to display
+  input_types_[3]= "bool"; //scale the output to byte range
 
   //output
   output_data_.resize(1, brdb_value_sptr(0));
@@ -75,10 +77,15 @@ bbgm_display_dist_image_process::execute()
     static_cast<brdb_value_t<int>* >(input_data_[2].ptr());
   int comp_index = input2->value();
 
+  //Retrieve scale switch
+  brdb_value_t<bool>* input3 =
+    static_cast<brdb_value_t<bool>* >(input_data_[3].ptr());
+  bool scale = input3->value();
+
   bbgm_viewer_sptr viewer;
   if (attr=="mean")
     viewer = new bbgm_mean_viewer();
-  else if (attr == "variance")
+  else if (attr == "variance"||attr == "std_dev")
     viewer = new bbgm_variance_viewer();
   else if (attr == "weight")
     viewer = new bbgm_weight_viewer();
@@ -100,16 +107,34 @@ bbgm_display_dist_image_process::execute()
              << " extract view (apply) failed\n";
     return false;
   }
-
+  brdb_value_sptr output0;
   //convert to a byte image for display
   vil_image_view<vxl_byte> byte_image;
-    vil_convert_stretch_range_limited(d_image, byte_image,
-                                      0.0, 1.0);
-
-  brdb_value_sptr output0 =
+  if(attr == "variance"||attr == "std_dev"){
+    if(attr == "std_dev") vil_math_sqrt(d_image);
+    if(scale){
+      double dmin, dmax;
+      vil_math_value_range(d_image, dmin, dmax);
+      vil_convert_stretch_range_limited(d_image, byte_image, dmin, dmax);
+      output0 =
+        new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<vxl_byte>(byte_image));
+    }else{
+    vil_image_view<float> fimg;
+    vil_convert_cast(d_image, fimg);
+    output0 =
+    new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<float>(fimg));
+    }
+  }else if(scale){
+    vil_convert_stretch_range_limited(d_image, byte_image, 0.0, 1.0);
+    output0 =
     new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<vxl_byte>(byte_image));
+  }else{
+    vil_image_view<float> fimg;
+    vil_convert_cast(d_image, fimg);
+    output0 =
+    new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<float>(fimg));
+  }
   output_data_[0] = output0;
-
   return true;
 }
 
