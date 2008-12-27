affadc375d2df6d1c02e91c05443cc9bb7a2b867
Joseph L. Mundy <mundyj@users.sourceforge.net>
2008-12-27 15:07:50 -0500

    Added command line image read

--- contrib/brl/bseg/sbin/seg_vil.cxx
+++ contrib/brl/bseg/sbin/seg_vil.cxx
@@ -1,3 +1,4 @@
+#include <vcl_string.h>
 #include <vgui/vgui.h>
 #include <vgui/vgui_adaptor.h>
 #include <vgui/vgui_window.h>
@@ -10,6 +11,7 @@ int main(int argc, char* argv[])
 #if 1
   char** my_argv = new char*[argc+1];
   my_argv[1] = new char[13];
+  char* temp = my_argv[1];
   vcl_strcpy(my_argv[1], "--mfc-use-gl");
   my_argv[0] = argv[0];
   for (int i=1; i<argc; ++i)
@@ -17,7 +19,7 @@ int main(int argc, char* argv[])
 
    // Initialize the toolkit.
   vgui::init(++argc, my_argv);
-  delete [] my_argv[1];
+  delete [] temp;//init shifts argv positions
   delete [] my_argv;
 #else
   vgui::init(argc, argv);
@@ -29,6 +31,14 @@ int main(int argc, char* argv[])
   segv_vil_segmentation_manager* segm=segv_vil_segmentation_manager::instance();
   segm->set_window(win);
   win->get_adaptor()->set_tableau(segm);
+  // Read a set of image file names with blank space separation 
+  // to load before startup
+  if(argc>=2)
+    for(int i = 1; i<argc; ++i){
+      char* file = argv[i];
+      vcl_string path(file);
+      segm->load_image_nomenu(path);
+    }
   win->set_statusbar(true);
   win->enable_vscrollbar(true);
   win->enable_hscrollbar(true);
