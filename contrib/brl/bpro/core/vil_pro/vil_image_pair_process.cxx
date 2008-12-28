ecc5ad337836b31b05283071f032568afcbeb80c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-12-28 07:43:09 -0500

    TABs replaced by spaces

--- contrib/brl/bpro/core/vil_pro/vil_image_pair_process.cxx
+++ contrib/brl/bpro/core/vil_pro/vil_image_pair_process.cxx
@@ -50,66 +50,63 @@ vil_image_pair_process::execute()
   unsigned ni0 = image0->ni(), nj0 = image0->nj();
   unsigned ni1 = image1->ni(), nj1 = image1->nj();
   unsigned ni = ni0, nj = nj0;
-  if(ni>ni1) ni = ni1;
-  if(nj>nj1) nj = nj1;
+  if (ni>ni1) ni = ni1;
+  if (nj>nj1) nj = nj1;
   unsigned separating_bar = 5;
   unsigned nip = 2*ni + separating_bar;
   //only implement for byte grey scale and byte color
 
-  vil_pixel_format fmt0 = image0->pixel_format(); 
-  vil_pixel_format fmt1 = image1->pixel_format(); 
-  if(fmt0!=fmt1)
+  vil_pixel_format fmt0 = image0->pixel_format();
+  vil_pixel_format fmt1 = image1->pixel_format();
+  if (fmt0!=fmt1)
     return false;
   vil_image_view_base_sptr out_image;
-  switch(fmt0)
+  switch (fmt0)
+  {
+    case VIL_PIXEL_FORMAT_BYTE:
     {
-    case  VIL_PIXEL_FORMAT_BYTE:
-      {
-        vil_image_view<vxl_byte> bimage0 = 
-          vil_convert_cast(vxl_byte(), image0);
+      vil_image_view<vxl_byte> bimage0 = vil_convert_cast(vxl_byte(), image0);
 
-        vil_image_view<vxl_byte> bimage1 = 
-          vil_convert_cast(vxl_byte(), image1);
-        
-        vil_image_view<vxl_byte>* comb = 
-          new vil_image_view<vxl_byte>(nip, nj);
+      vil_image_view<vxl_byte> bimage1 = vil_convert_cast(vxl_byte(), image1);
 
-        for(unsigned j=0; j<nj; ++j){
-          for(unsigned i=0; i<ni; ++i){
-            (*comb)(i,j) = bimage0(i, j);
-            (*comb)(ni+i+separating_bar,j) = bimage1(i, j);
-          }
-          for(unsigned i=ni; i<ni+separating_bar; ++i)
-            (*comb)(i,j) = static_cast<vxl_byte>(255);
+      vil_image_view<vxl_byte>* comb = new vil_image_view<vxl_byte>(nip, nj);
+
+      for (unsigned j=0; j<nj; ++j) {
+        for (unsigned i=0; i<ni; ++i) {
+          (*comb)(i,j) = bimage0(i, j);
+          (*comb)(ni+i+separating_bar,j) = bimage1(i, j);
         }
-        out_image = comb;
-		break;
+        for (unsigned i=ni; i<ni+separating_bar; ++i)
+          (*comb)(i,j) = static_cast<vxl_byte>(255);
       }
+      out_image = comb;
+      break;
+    }
     case VIL_PIXEL_FORMAT_RGB_BYTE:
-      {
-        vil_image_view<vil_rgb<vxl_byte> > cimage0 = 
-          vil_convert_cast(vil_rgb<vxl_byte>(), image0);
+    {
+      vil_image_view<vil_rgb<vxl_byte> > cimage0 =
+        vil_convert_cast(vil_rgb<vxl_byte>(), image0);
 
-        vil_image_view<vil_rgb<vxl_byte> > cimage1 = 
-          vil_convert_cast(vil_rgb<vxl_byte>(), image1);
+      vil_image_view<vil_rgb<vxl_byte> > cimage1 =
+        vil_convert_cast(vil_rgb<vxl_byte>(), image1);
 
-        vil_image_view<vil_rgb<vxl_byte> >* comb = 
-          new vil_image_view<vil_rgb<vxl_byte> >(nip, nj);
+      vil_image_view<vil_rgb<vxl_byte> >* comb =
+        new vil_image_view<vil_rgb<vxl_byte> >(nip, nj);
 
-        for(unsigned j=0; j<nj; ++j){
-          for(unsigned i=0; i<ni; ++i){
-            (*comb)(i,j) = cimage0(i, j);
-            (*comb)(ni+i+separating_bar,j) = cimage1(i, j);
-          }
-          for(unsigned i=ni; i<ni+separating_bar; ++i)
-            (*comb)(i,j) = vil_rgb<vxl_byte>(0, 255,0);
+      for (unsigned j=0; j<nj; ++j) {
+        for (unsigned i=0; i<ni; ++i) {
+          (*comb)(i,j) = cimage0(i, j);
+          (*comb)(ni+i+separating_bar,j) = cimage1(i, j);
         }
-        out_image = comb;
-		break;
+        for (unsigned i=ni; i<ni+separating_bar; ++i)
+          (*comb)(i,j) = vil_rgb<vxl_byte>(0, 255,0);
       }
+      out_image = comb;
+        break;
+    }
     default:
       return false;
-    }
+  }
 
   brdb_value_sptr output0 = new brdb_value_t<vil_image_view_base_sptr>(out_image);
   output_data_[0] = output0;
