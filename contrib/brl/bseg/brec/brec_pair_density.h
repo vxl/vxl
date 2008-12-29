f0ba5db5f4a3740d154dab17a104a1d7a0a1bd3a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-12-29 08:03:42 -0500

    made destructor virtual, since class has virtual methods

--- contrib/brl/bseg/brec/brec_pair_density.h
+++ contrib/brl/bseg/brec/brec_pair_density.h
@@ -1,11 +1,11 @@
-#ifndef brec_PAIR_DENSITY_H_
-#define brec_PAIR_DENSITY_H_
+#ifndef brec_pair_density_h_
+#define brec_pair_density_h_
 //:
 // \file
 // \brief Base class for prob density of an intensity pair
 //
-// \author Ozge C Ozcanli (ozge@lems.brown.edu)
-// \date 10/01/08
+// \author Ozge C Ozcanli (ozge at lems dot brown dot edu)
+// \date Oct. 01, 2008
 //
 // \verbatim
 //  Modifications
@@ -19,6 +19,7 @@ class brec_pair_density
  public:
 
   brec_pair_density() : sigma_(0.1) {}
+  virtual ~brec_pair_density() {}
 
   void set_sigma(double s) { sigma_ = s; }
   double sigma() { return sigma_; }
@@ -30,4 +31,4 @@ class brec_pair_density
   double sigma_;
 };
 
-#endif // brec_PAIR_DENSITY_H_
+#endif // brec_pair_density_h_
