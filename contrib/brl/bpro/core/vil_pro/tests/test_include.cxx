b4008f39e54407507d2d89cca8b38678d6da8bfb
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2008-12-31 12:22:19 -0500

    added new header files

--- contrib/brl/bpro/core/vil_pro/tests/test_include.cxx
+++ contrib/brl/bpro/core/vil_pro/tests/test_include.cxx
@@ -1,8 +1,10 @@
 #include <vil_pro/vil_binary_image_op_process.h>
 #include <vil_pro/vil_combine_grey_images_process.h>
 #include <vil_pro/vil_convert_to_n_planes_process.h>
+#include <vil_pro/vil_crop_image_process.h>
 #include <vil_pro/vil_gaussian_process.h>
 #include <vil_pro/vil_gradient_process.h>
+#include <vil_pro/vil_image_pair_process.h>
 #include <vil_pro/vil_io_image_view_base.h>
 #include <vil_pro/vil_load_from_ascii_process.h>
 #include <vil_pro/vil_load_image_view_binary_process.h>
