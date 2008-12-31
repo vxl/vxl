6da19f12f5849fcc933be6028284ecf6773c0938
Joseph L. Mundy <mundyj@users.sourceforge.net>
2008-12-31 08:01:42 -0500

    Removed obsolete tests

--- contrib/brl/bbas/bvgl/tests/test_driver.cxx
+++ contrib/brl/bbas/bvgl/tests/test_driver.cxx
@@ -1,14 +1,10 @@
 #include <testlib/testlib_register.h>
 
-DECLARE(test_articulated_poly);
-DECLARE(test_misc);
 DECLARE(test_bvgl_changes);
 
 void
 register_tests()
 {
-  //REGISTER(test_articulated_poly);
-  //REGISTER(test_misc);
   REGISTER(test_bvgl_changes);
 }
 
