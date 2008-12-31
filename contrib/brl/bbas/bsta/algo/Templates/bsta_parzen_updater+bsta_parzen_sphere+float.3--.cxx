528d0875922567d065c846f4faf637ef23088c43
Joseph L. Mundy <mundyj@users.sourceforge.net>
2008-12-31 12:09:37 -0500

    removed adaptive updater for parzen sphere

--- contrib/brl/bbas/bsta/algo/Templates/bsta_parzen_updater+bsta_parzen_sphere+float.3--.cxx
+++ contrib/brl/bbas/bsta/algo/Templates/bsta_parzen_updater+bsta_parzen_sphere+float.3--.cxx
@@ -1,6 +1,5 @@
 #include <bsta/algo/bsta_parzen_updater.txx>
-#include <bsta/bsta_parzen_sphere.h>
+#include <bsta/bsta_parzen_sf3.h>
 
-typedef bsta_parzen_sphere<float, 3> parzen;
 
-BSTA_PARZEN_UPDATER_INSTANTIATE(parzen);
+BSTA_PARZEN_UPDATER_INSTANTIATE(bsta_parzen_sf3);
