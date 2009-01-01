383f6cfb59a8ddb22f567ac245d56b95c8b9600a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-01-01 08:22:41 -0500

    with recent versions of MSVC, fileno() became _fileno() and fdopen() became _fdopen() (according to the compile warning)

--- core/vpl/vpl_fdopen.cxx
+++ core/vpl/vpl_fdopen.cxx
@@ -12,6 +12,8 @@ vcl_FILE *vpl_fdopen(int fd, char const *mode)
   return fdopen(fd, const_cast<char*>(mode));
 #elif defined(VCL_BORLAND_56)
   return std::fdopen(fd, const_cast<char*>(mode));
+#elif defined(VCL_WIN32) && !defined(__CYGWIN__) && _MSC_VER >= 1400
+  return _fdopen(fd, mode);
 #else
   return fdopen(fd, mode);
 #endif
--- core/vpl/vpl_fileno.cxx
+++ core/vpl/vpl_fileno.cxx
@@ -7,5 +7,9 @@ extern "C" int fileno(FILE *);
 
 int vpl_fileno(vcl_FILE *fp)
 {
+#if defined(VCL_WIN32) && !defined(__CYGWIN__) && _MSC_VER >= 1400
+  return _fileno(fp);
+#else
   return fileno(fp);
+#endif
 }
