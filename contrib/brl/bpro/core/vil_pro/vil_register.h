e9bbeea88b7c88ac6c4af3c1ebe14df6289283e5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-01-19 03:15:19 -0500

    added EOLN at EOF to avoid compile warning

--- contrib/brl/bpro/core/vil_pro/vil_register.h
+++ contrib/brl/bpro/core/vil_pro/vil_register.h
@@ -1,10 +1,11 @@
 #ifndef vil_register_h_
 #define vil_register_h_
 
-class vil_register {
-public:
+class vil_register
+{
+ public:
   static void register_datatype();
   static void register_process();
 };
 
-#endif
+#endif // vil_register_h_
