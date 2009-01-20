cbc098eccb58ce9184ae67d945ab564a67c0c9e8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-01-20 02:47:43 -0500

    avoid compiler warning by adding EOLN at EOF

--- contrib/brl/bpro/core/vidl2_pro/vidl2_register.h
+++ contrib/brl/bpro/core/vidl2_pro/vidl2_register.h
@@ -1,9 +1,11 @@
 #ifndef vidl2_register_h_
 #define vidl2_register_h_
 
-class vidl2_register {
-public:
+class vidl2_register
+{
+ public:
   static void register_datatype();
   static void register_process();
 };
+
 #endif
