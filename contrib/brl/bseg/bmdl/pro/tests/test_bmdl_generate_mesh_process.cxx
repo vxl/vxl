1184741c57ae62b5b45a7192ae86444355425927
Gamze Tunali <gamze@users.sourceforge.net>
2009-01-21 14:16:53 -0500

    added a camera input

--- contrib/brl/bseg/bmdl/pro/tests/test_bmdl_generate_mesh_process.cxx
+++ contrib/brl/bseg/bmdl/pro/tests/test_bmdl_generate_mesh_process.cxx
@@ -14,6 +14,9 @@
 #include <vil/vil_load.h>
 #include <vil/vil_image_view_base.h>
 
+#include <vpgl/vpgl_camera.h>
+#include <vpgl/file_formats/vpgl_geo_camera.h>
+
 MAIN( test_bmdl_generate_mesh_process )
 {
   REG_PROCESS_FUNC(bprb_func_process, bprb_batch_process_manager, bmdl_generate_mesh_process, "bmdlGenerateMeshProcess");
@@ -32,12 +35,14 @@ MAIN( test_bmdl_generate_mesh_process )
 
   vcl_string polygons_path = "polygons.bin";
   vcl_string meshes_path = "meshes.obj";
+  vpgl_camera_double_sptr camera = new vpgl_geo_camera();
 
   brdb_value_sptr v0 = new brdb_value_t<vcl_string>(polygons_path);
   brdb_value_sptr v1 = new brdb_value_t<vil_image_view_base_sptr>(label);
   brdb_value_sptr v2 = new brdb_value_t<vil_image_view_base_sptr>(height);
   brdb_value_sptr v3 = new brdb_value_t<vil_image_view_base_sptr>(ground);
   brdb_value_sptr v4 = new brdb_value_t<vcl_string>(meshes_path);
+  brdb_value_sptr v5 = new brdb_value_t<vpgl_camera_double_sptr>(camera);
 
   bool good = bprb_batch_process_manager::instance()->init_process("bmdlGenerateMeshProcess");
   good = good && bprb_batch_process_manager::instance()->set_input(0, v0);
@@ -45,6 +50,7 @@ MAIN( test_bmdl_generate_mesh_process )
   good = good && bprb_batch_process_manager::instance()->set_input(2, v2);
   good = good && bprb_batch_process_manager::instance()->set_input(3, v3);
   good = good && bprb_batch_process_manager::instance()->set_input(4, v4);
+  good = good && bprb_batch_process_manager::instance()->set_input(5, v5);
   good = good && bprb_batch_process_manager::instance()->run_process();
 
   TEST("run generate mesh process", good ,true);
