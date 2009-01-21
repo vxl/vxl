5787a293f297a8fdd1de3ece021ae39ebd00349e
Matthew J. Leotta <mleotta@users.sourceforge.net>
2009-01-21 14:55:52 -0500

    removed old files

--- contrib/brl/bseg/bmdl/pro/tests/test_include.cxx
+++ contrib/brl/bseg/bmdl/pro/tests/test_include.cxx
@@ -1,9 +1,5 @@
-#include <bmdl/pro/bmdl_classify_process.h>
-#include <bmdl/pro/bmdl_generate_mesh_process.h>
-#include <bmdl/pro/bmdl_lidar_roi_process.h>
-#include <bmdl/pro/bmdl_modeling_process.h>
+
 #include <bmdl/pro/bmdl_processes.h>
 #include <bmdl/pro/bmdl_register.h>
-#include <bmdl/pro/bmdl_trace_boundaries_process.h>
 
 int main() { return 0; }
