c6c9726a05a7ba070b566fdb6a64c92c8dfbfae1
Gamze Tunali <gamze@users.sourceforge.net>
2009-01-21 14:56:09 -0500

    removed registartion to specific libraries

--- contrib/brl/bseg/bmdl_batch/reg_bmdl.cxx
+++ contrib/brl/bseg/bmdl_batch/reg_bmdl.cxx
@@ -1,47 +1,18 @@
 #include "reg_bmdl.h"
 #include <bprb/bprb_macros.h>
 #include <bprb/bprb_batch_process_manager.h>
+#include <bprb/bprb_func_process.h>
 // processes
-#include <vil_pro/vil_load_image_view_process.h>
-#include <vil_pro/vil_save_image_view_process.h>
-#include <vil_pro/vil_rgbi_to_grey_process.h>
-#include <vpgl_pro/vpgl_load_proj_camera_process.h>
-#include <vpgl_pro/vpgl_load_perspective_camera_process.h>
-#include <vpgl_pro/vpgl_save_perspective_camera_process.h>
-#include <vpgl_pro/vpgl_load_rational_camera_process.h>
-#include <vpgl_pro/vpgl_save_rational_camera_process.h>
-#include <vpgl_pro/vpgl_load_rational_camera_nitf_process.h>
+#include <vil_pro/vil_register.h>
+#include <vpgl_pro/vpgl_register.h>
 
-// datatypes
-#include <vcl_string.h>
-#include <vil/vil_image_view_base.h>
-#include <vpgl/vpgl_camera.h>
-
-// bmdl files
-#include <bmdl/pro/bmdl_classify_process.h>
-#include <bmdl/pro/bmdl_trace_boundaries_process.h>
-#include <bmdl/pro/bmdl_generate_mesh_process.h>
-#include <bmdl/pro/bmdl_lidar_roi_process.h>
-#include <bmdl/pro/bmdl_modeling_process.h>
+#include <bmdl/pro/bmdl_processes.h>
+#include <bmdl/pro/bmdl_register.h>
 
 PyObject *
 register_processes(PyObject *self, PyObject *args)
 {
-  REG_PROCESS(vil_load_image_view_process, bprb_batch_process_manager);
-  REG_PROCESS(vil_save_image_view_process, bprb_batch_process_manager);
-  REG_PROCESS(vil_rgbi_to_grey_process, bprb_batch_process_manager);
-  REG_PROCESS(vpgl_load_rational_camera_process, bprb_batch_process_manager);
-  REG_PROCESS(vpgl_save_rational_camera_process, bprb_batch_process_manager);
-  REG_PROCESS(vpgl_load_rational_camera_nitf_process, bprb_batch_process_manager);
-  REG_PROCESS(vpgl_load_proj_camera_process, bprb_batch_process_manager);
-  REG_PROCESS(vpgl_load_perspective_camera_process, bprb_batch_process_manager);
-  REG_PROCESS(vpgl_save_perspective_camera_process, bprb_batch_process_manager);
-
-  REG_PROCESS(bmdl_classify_process, bprb_batch_process_manager);
-  REG_PROCESS(bmdl_trace_boundaries_process, bprb_batch_process_manager);
-  REG_PROCESS(bmdl_generate_mesh_process, bprb_batch_process_manager);
-  REG_PROCESS(bmdl_lidar_roi_process, bprb_batch_process_manager);
-  REG_PROCESS(bmdl_modeling_process, bprb_batch_process_manager);
+  bmdl_register::register_process();
   Py_INCREF(Py_None);
   return Py_None;
 }
@@ -49,16 +20,9 @@ register_processes(PyObject *self, PyObject *args)
 PyObject *
 register_datatypes(PyObject *self, PyObject *args)
 {
-  REGISTER_DATATYPE(bool);
-  REGISTER_DATATYPE(vcl_string);
-  REGISTER_DATATYPE(int);
-  REGISTER_DATATYPE(unsigned);
-  REGISTER_DATATYPE(long);
-  REGISTER_DATATYPE(float);
-  REGISTER_DATATYPE(double);
-  REGISTER_DATATYPE(vil_image_view_base_sptr);
-  typedef vbl_smart_ptr<vpgl_camera<double> > vpgl_camera_double_sptr;
-  REGISTER_DATATYPE( vpgl_camera_double_sptr );
+  register_basic_datatypes();
+  vil_register::register_datatype();
+  vpgl_register::register_datatype();
 
   Py_INCREF(Py_None);
   return Py_None;
