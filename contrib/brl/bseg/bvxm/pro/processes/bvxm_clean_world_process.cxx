986978583498286160dc5c95edeebca3894a02fe
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-01-22 15:28:21 -0500

    bvxm_clean_voxel_world_process is added to bvxm/pro/processes

---
+++ contrib/brl/bseg/bvxm/pro/processes/bvxm_clean_world_process.cxx
@@ -0,0 +1,41 @@
+//This is brl/bseg/bvxm/pro/processes/bvxm_clean_world_process.cxx
+
+//:
+// \file
+#include<<bprb/bprb_func_process.h>
+#include <bprb/bprb_parameters.h>
+
+#include <brdb/brdb_value.h>
+
+#include <bvxm/bvxm_voxel_world.h>
+
+
+bool bvxm_clean_world_process(bprb_func_process& pro)
+{
+  // process takes 1 input: 
+  //input[0]: The voxel world
+  unsigned n_inputs_ = 1;
+  if(pro.n_inputs<n_inputs_)
+  {
+    vcl_cout << pro.name() <<" : The input number should be "<< n_inputs_ << vcl_endl;
+    return false; 
+  }
+
+  //get the inputs:
+  unsigned i=0;
+  bvxm_voxel_world_sptr world = pro.get_input<bvxm_voxel_world_sptr>(i++);
+
+  //check inputs validity
+  i = 0;
+  if (!world){
+    vcl_cout << pro.name() <<" :--  Input " << i++ << " is not valid!\n";
+    return false;
+  }
+  
+  return world->clean_grids();
+   
+}
+
+
+
+
