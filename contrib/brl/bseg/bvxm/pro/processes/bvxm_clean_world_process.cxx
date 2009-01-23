88f402b05867f64737549beccee3f33e60d955d0
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-01-23 02:37:16 -0500

    typo

--- contrib/brl/bseg/bvxm/pro/processes/bvxm_clean_world_process.cxx
+++ contrib/brl/bseg/bvxm/pro/processes/bvxm_clean_world_process.cxx
@@ -2,7 +2,7 @@
 
 //:
 // \file
-#include<<bprb/bprb_func_process.h>
+#include <bprb/bprb_func_process.h>
 #include <bprb/bprb_parameters.h>
 
 #include <brdb/brdb_value.h>
@@ -12,13 +12,13 @@
 
 bool bvxm_clean_world_process(bprb_func_process& pro)
 {
-  // process takes 1 input: 
+  // process takes 1 input:
   //input[0]: The voxel world
   unsigned n_inputs_ = 1;
-  if(pro.n_inputs<n_inputs_)
+  if (pro.n_inputs<n_inputs_)
   {
     vcl_cout << pro.name() <<" : The input number should be "<< n_inputs_ << vcl_endl;
-    return false; 
+    return false;
   }
 
   //get the inputs:
@@ -31,11 +31,6 @@ bool bvxm_clean_world_process(bprb_func_process& pro)
     vcl_cout << pro.name() <<" :--  Input " << i++ << " is not valid!\n";
     return false;
   }
-  
+
   return world->clean_grids();
-   
 }
-
-
-
-
