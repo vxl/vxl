243aee7f2c07ebd8ae100b7b8f31cdad818374e1
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-01-26 17:22:41 -0500

    processes functors

---
+++ contrib/brl/bseg/bvxm/pro/processes/bvxm_create_voxel_world_process.cxx
@@ -0,0 +1,121 @@
+//This is brl/bseg/bvxm/pro/processes/bvxm_create_voxel_world_process.cxx
+//:
+// \file
+
+#include <bprb/bprb_func_process.h>
+#include <bprb/bprb_parameters.h>
+
+#include <bvxm/bvxm_voxel_world.h>
+#include <bvxm/bvxm_mog_grey_processor.h>
+#include <bvxm/bvxm_world_params.h>
+
+#include <brdb/brdb_value.h>
+
+#include <vcl_string.h>
+#include <vcl_iostream.h>
+#include <vcl_fstream.h>
+
+#include <vgl/vgl_point_3d.h>
+#include <vgl/vgl_vector_3d.h>
+
+#include <vul/vul_file.h>
+
+//Define parameters here
+#define PARAM_INPUT_DIRECTORY "input_diectory"
+#define PARAM_CORNER_X "corner_x"
+#define PARAM_CORNER_Y "corner_y"
+#define PARAM_CORNER_Z "corner_z"
+#define PARAM_VOXEL_DIM_X "voxel_dim_x"
+#define PARAM_VOXEL_DIM_Y "voxel_dim_y"
+#define PARAM_VOXEL_DIM_Z "voxel_dim_z"
+#define PARAM_VOXEL_LENGTH "voxel_length"
+#define PARAM_LVCS "lvcs"
+#define PARAM_MIN_OCP_PROB "min_ocp_prob"
+#define PARAM_MAX_OCP_PROB "max_ocp_prob"
+#define PARAM_MAX_SCALE "max_scale"
+
+//: Creates a voxel world
+bool bvxm_create_voxel_world_process(bprb_func_process& pro)
+{
+  //this process takes no inputs
+  
+  //define and read in the parameters
+  vcl_string vox_dir;
+  pro.parameters()->get_value(PARAM_INPUT_DIRECTORY, vox_dir);
+  if (!vul_file::is_directory(vox_dir) || !vul_file::exists(vox_dir)) {
+    vcl_cerr << "In bvxm_create_voxel_world_process::execute() -- input directory is not valid!\n";
+    return false;
+  }
+  vcl_cout << "In bvxm_create_voxel_world_process::execute() -- input directory is: " << vox_dir << vcl_endl;
+
+  
+  float corner_x = 0.0f;
+  pro.parameters()->get_value(PARAM_CORNER_X, corner_x);
+  float corner_y = 0.0f;
+  pro.parameters()->get_value(PARAM_CORNER_Y, corner_y);
+  float corner_z = 0.0f;
+  pro.parameters()->get_value(PARAM_CORNER_Z, corner_z);
+  vgl_point_3d<float> corner(corner_x, corner_y, corner_z);
+
+  unsigned int dimx = 10;
+  pro.parameters()->get_value(PARAM_VOXEL_DIM_X, dimx);
+  unsigned int dimy = 10;
+  pro.parameters()->get_value(PARAM_VOXEL_DIM_Y, dimy);
+  unsigned int dimz = 10;
+  pro.parameters()->get_value(PARAM_VOXEL_DIM_Z, dimz);
+  vgl_vector_3d<unsigned int> voxel_dims(dimx, dimy, dimz);
+
+  float vox_len = 1.0f;
+  pro.parameters()->get_value(PARAM_VOXEL_LENGTH, vox_len);
+
+#if 0
+  bvxm_world_params::appearance_model_type apm_type;
+  unsigned int type = parameters()->value<unsigned int>("apm_type");
+  if (type < 3) // there are 3 types and values are 0,1 and 2
+    apm_type = (bvxm_world_params::appearance_model_type) type;
+  else
+    apm_type = bvxm_world_params::mog_grey;
+#endif // 0
+
+  vcl_string lvcs_path;
+  pro.parameters()->get_value(PARAM_LVCS, lvcs_path);
+
+  bgeo_lvcs_sptr lvcs = new bgeo_lvcs();
+  if (lvcs_path != "") {
+    vcl_ifstream is(lvcs_path.c_str());
+    if (!is)
+    {
+      vcl_cerr << " Error opening file  " << lvcs_path << vcl_endl;
+      return false;
+    }
+    lvcs->read(is);
+  }
+
+  float min_ocp_prob = 1e-5f;
+  pro.parameters()->get_value(PARAM_MIN_OCP_PROB, min_ocp_prob);
+  float max_ocp_prob = 1- 1e-5f;
+  pro.parameters()->get_value(PARAM_MAX_OCP_PROB, max_ocp_prob);
+
+  unsigned int max_scale = 1;
+  pro.parameters()->get_value<unsigned int>(PARAM_MAX_SCALE, max_scale);
+
+  bvxm_world_params_sptr params = new bvxm_world_params();
+  params->set_params(vox_dir, corner, voxel_dims, vox_len, lvcs, min_ocp_prob, max_ocp_prob, max_scale);
+
+  bvxm_voxel_world_sptr vox_world = new bvxm_voxel_world;
+  vox_world->set_params(params);
+
+  //Set and store outputs
+  int j = 0;
+  vcl_vector<vcl_string> output_types_(2);
+  output_types_[j++] = "bvxm_voxel_world_sptr";
+  pro.set_output_types(output_types_);
+  
+  j=0;
+  //: output voxel world
+  brdb_value_sptr output0 = new brdb_value_t<bvxm_voxel_world_sptr>(vox_world);
+  
+  pro.set_output(j++, output0);
+
+  return true;
+}
---
+++ contrib/brl/bseg/bvxm/pro/processes/bvxm_detect_changes_process.cxx
@@ -0,0 +1,144 @@
+//This is brl/bseg/bvxm/pro/processes/bvxm_create_voxel_world_process.cxx
+//:
+// \file
+
+#include <bprb/bprb_func_process.h>
+#include <bprb/bprb_parameters.h>
+
+#include <brdb/brdb_value.h>
+
+#include <vil/vil_image_view_base.h>
+#include <vil/vil_math.h>
+#include <vil/algo/vil_threshold.h>
+
+#include <vpgl/vpgl_camera.h>
+
+#include <bvxm/bvxm_voxel_world.h>
+#include <bvxm/bvxm_image_metadata.h>
+#include <bvxm/bvxm_mog_grey_processor.h>
+
+bool bvxm_detect_changes_process(bprb_func_process& pro)
+{
+  //inputs
+  //input[0]: The observation to detect changes
+  //input[1]: The camera of the observation
+  //input[2]: The voxel world
+  //input[3]: The apperance model type, the supported strings are:
+  //          -apm_mog_grey
+  //          -apm_mog_rgb
+  //          -apm_mog_mc_2_3
+  //          -apm_mog_mc_3_3
+  //          -apm_mog_mc_4_3
+  //input[4]: The bin index to be updated
+  //input[5]: The image scale index  detected
+  unsigned n_inputs_ = 6;
+  if(pro.n_inputs()<n_inputs_)
+  {
+    vcl_cout << pro.name() << " The input number should be " << n_inputs_<< vcl_endl;
+    return false; 
+  }
+  
+  //get the inputs
+  unsigned i = 0;
+  vil_image_view_base_sptr img = pro.get_input<vil_image_view_base_sptr>(i++);
+  vpgl_camera_double_sptr camera = pro.get_input<vpgl_camera_double_sptr>(i++);
+  bvxm_voxel_world_sptr world = pro.get_input<bvxm_voxel_world_sptr>(i++);
+  vcl_string voxel_type = pro.get_input<vcl_string>(i++);
+  unsigned bin_index = pro.get_input<unsigned>(i++);
+  unsigned scale_index = pro.get_input<unsigned>(i++);
+  
+  //check inputs validity
+  if (!img) {
+    vcl_cout << pro.name() <<" :--  Input 0  is not valid!\n";
+    return false;
+  }
+  if (!camera) {
+    vcl_cout << pro.name() <<" :--  Input 1 is not valid!\n";
+    return false;
+  }
+  if (!world) {
+    vcl_cout << pro.name() <<" :--  Input 2 is not valid!\n";
+    return false;
+  }
+  
+  //create metadata:
+  bvxm_image_metadata observation(img,camera);
+
+  //get probability density of all pixels in image
+  vil_image_view<float> prob_map(img->ni(),img->nj(),1);
+  vil_image_view<bool> mask(img->ni(),img->nj(),1);
+
+  bool result = true;
+
+  if (voxel_type == "apm_mog_grey")
+    result = world->pixel_probability_density<APM_MOG_GREY>(observation,prob_map, mask, bin_index,scale_index);
+  else if (voxel_type == "apm_mog_rgb")
+    result = world->pixel_probability_density<APM_MOG_RGB>(observation,prob_map, mask, bin_index,scale_index);
+  else if (voxel_type == "apm_mog_mc_2_3")
+  {
+    if (observation.img->nplanes()!= 2)
+    {
+      vcl_cerr << "Error bvxm_detect_changes_process: appereance model type" << voxel_type << "does not support images with "
+               << observation.img->nplanes()<< " planes\n";
+      return false;
+    }
+    result = world->pixel_probability_density<APM_MOG_MC_2_3>(observation,prob_map, mask, bin_index,scale_index);
+  }
+  else if (voxel_type == "apm_mog_mc_3_3")
+    result = world->pixel_probability_density<APM_MOG_MC_3_3>(observation,prob_map, mask, bin_index,scale_index);
+  else if (voxel_type == "apm_mog_mc_4_3")
+  {
+    if (observation.img->nplanes()!= 4)
+    {
+      vcl_cerr << "Error bvxm_detect_changes_process: appereance model type" << voxel_type << "does not support images with "
+               << observation.img->nplanes()<< " planes\n";
+      return false;
+    }
+    result = world->pixel_probability_density<APM_MOG_MC_4_3>(observation,prob_map, mask, bin_index,scale_index);
+  }
+  else
+    vcl_cerr << "Error in: bvxm_detect_changes_processor: Unsuppported appereance model\n";
+
+  if (!result) {
+    vcl_cerr << "Error bvxm_detect_changes_process: failed to detect changes\n";
+    return false;
+  }
+  // TODO: filtering / thresholding if necessary (Thom?)
+
+  // the following thresholding is added temporarily in order to produce an
+  // image that can be saved threshold the image to make sure that it is binary
+  float threshold_value = 0.5;
+  float min_value = 0, max_value = 0;
+  vil_math_value_range(prob_map, min_value, max_value);
+
+  threshold_value = (min_value+max_value)/2;
+  vil_image_view<bool> binary_img;
+  vil_threshold_above<float>(prob_map, binary_img, threshold_value);
+
+  //set and store output
+  unsigned j=0;
+  vcl_vector<vcl_string> output_types_(3);
+  //the probability map
+  output_types_[j++] = "vil_image_view_base_sptr";
+  //the mask
+  output_types_[j++] = "vil_image_view_base_sptr";
+  //the binary image
+  output_types_[j++] = "vil_image_view_base_sptr";
+  pro.set_output_types(output_types_);
+  
+  j = 0;  
+  brdb_value_sptr output0 =
+    new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<float>(prob_map));
+  pro.set_output(j++, output0);
+
+  brdb_value_sptr output1 =
+    new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<bool>(mask));
+  pro.set_output(j++,output1);
+
+  brdb_value_sptr output2 =
+    new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<bool>(binary_img));
+  pro.set_output(j++,output2);
+
+  return true;
+}
+
