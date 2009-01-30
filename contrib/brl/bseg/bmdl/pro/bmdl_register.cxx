1e75adaca3c3f9602cd24545f78b320e35649be0
Gamze Tunali <gamze@users.sourceforge.net>
2009-01-30 14:35:45 -0500

    added init() func

--- contrib/brl/bseg/bmdl/pro/bmdl_register.cxx
+++ contrib/brl/bseg/bmdl/pro/bmdl_register.cxx
@@ -12,8 +12,12 @@ void bmdl_register::register_datatype()
 
 void bmdl_register::register_process()
 {
-  REG_PROCESS_FUNC(bprb_func_process, bprb_batch_process_manager, bmdl_classify_process, "bmdlClassifyProcess");
-  REG_PROCESS_FUNC(bprb_func_process, bprb_batch_process_manager, bmdl_trace_boundaries_process, "bmdlTraceBoundariesProcess");
-  REG_PROCESS_FUNC(bprb_func_process, bprb_batch_process_manager, bmdl_generate_mesh_process, "bmdlGenerateMeshProcess");
-  REG_PROCESS_FUNC(bprb_func_process, bprb_batch_process_manager, bmdl_lidar_roi_process, "bmdlLidarRoiProcess");
+  REG_PROCESS_FUNC_INIT_FINALIZE(bprb_func_process, bprb_batch_process_manager, bmdl_classify_process, 
+    "bmdlClassifyProcess", bmdl_classify_process_init, 0);
+  REG_PROCESS_FUNC_INIT_FINALIZE(bprb_func_process, bprb_batch_process_manager, bmdl_trace_boundaries_process, 
+    "bmdlTraceBoundariesProcess", bmdl_trace_boundaries_process_init, 0);
+  REG_PROCESS_FUNC_INIT_FINALIZE(bprb_func_process, bprb_batch_process_manager, bmdl_generate_mesh_process, 
+    "bmdlGenerateMeshProcess", bmdl_generate_mesh_process_init, 0);
+  REG_PROCESS_FUNC_INIT_FINALIZE(bprb_func_process, bprb_batch_process_manager, bmdl_lidar_roi_process, 
+    "bmdlLidarRoiProcess", bmdl_lidar_roi_process_init, 0);
 }
