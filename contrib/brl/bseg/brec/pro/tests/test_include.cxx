76ee46cccf812768216041f107f944ff1dedf3e4
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2009-02-04 14:03:13 -0500

    updates

--- contrib/brl/bseg/brec/pro/tests/test_include.cxx
+++ contrib/brl/bseg/brec/pro/tests/test_include.cxx
@@ -1,16 +1,4 @@
-#include <brec/pro/brec_bayesian_update_process.h>
-#include <brec/pro/brec_change_area_process.h>
-#include <brec/pro/brec_create_mog_image_process.h>
-#include <brec/pro/brec_density_to_prob_map_process.h>
-#include <brec/pro/brec_glitch_process.h>
-#include <brec/pro/brec_glitch_overlay_process.h>
-#include <brec/pro/brec_prob_map_area_process.h>
-#include <brec/pro/brec_prob_map_roc_compute_process.h>
-#include <brec/pro/brec_prob_map_supress_process.h>
-#include <brec/pro/brec_prob_map_threshold_process.h>
-#include <brec/pro/brec_recognize_structure_process.h>
+#include <brec/pro/brec_processes.h>
 #include <brec/pro/brec_register.h>
-#include <brec/pro/brec_save_mog_image_process.h>
-#include <brec/pro/brec_update_changes_process.h>
 
 int main() { return 0; }
