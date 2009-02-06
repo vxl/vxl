b2aa6d2bcf4d900731d5416a4e62846e08c4a6e1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-02-06 02:58:16 -0500

    *** empty log message ***

--- contrib/brl/bseg/bvxm/pro/processes/bvxm_normalization_util.h
+++ contrib/brl/bseg/bvxm/pro/processes/bvxm_normalization_util.h
@@ -1,6 +1,5 @@
 #ifndef bvxm_normalization_util_h_
 #define bvxm_normalization_util_h_
-
 //:
 // \file
 // \brief Utility functions for normalization processes.
@@ -9,7 +8,7 @@
 //
 // \verbatim
 //  Modifications
-//      <none>
+//   <none>
 // \endverbatim
 
 #include <vil/vil_image_view_base.h>
@@ -17,20 +16,17 @@
 
 class bvxm_normalization_util
 {
-  public:
-    
-     //: Float specialized function to normalize and image given a,b where new_I = a*I +b;
-    static bool normalize_image(const vil_image_view<float>& in_view,
-                                vil_image_view<float>& out_img,
-                                float a, float b, float max_value);
-  
-  
+ public:
+
+  //: Float specialized function to normalize and image given a,b where new_I = a*I +b;
+  static bool normalize_image(const vil_image_view<float>& in_view,
+                              vil_image_view<float>& out_img,
+                              float a, float b, float max_value);
+
   //: Byte specialized function to normalize and image given a,b where new_I = a*I +b;
-    static bool normalize_image(const vil_image_view<vxl_byte>& in_view,
-                                vil_image_view<vxl_byte>& out_img,
-                                float a, float b, vxl_byte max_value = 255);
-  
- 
+  static bool normalize_image(const vil_image_view<vxl_byte>& in_view,
+                              vil_image_view<vxl_byte>& out_img,
+                              float a, float b, vxl_byte max_value = 255);
 };
 
-#endif // bvxm_normalization_process_util_h_
+#endif // bvxm_normalization_util_h_
