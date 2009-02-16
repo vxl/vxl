3a8018ec5ebf70b739f367e0afa493360b3ef677
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-02-16 16:29:48 -0500

    added missing #include <cassert>

--- core/vpdl/vpdl_base_traits.h
+++ core/vpdl/vpdl_base_traits.h
@@ -12,7 +12,7 @@
 // a single distribution base class without specializations.
 //
 // \verbatim
-// Modifications
+//  Modifications
 //   None
 // \endverbatim
 
@@ -20,66 +20,66 @@
 #include <vnl/vnl_matrix.h>
 #include <vnl/vnl_vector_fixed.h>
 #include <vnl/vnl_matrix_fixed.h>
-
+#include <vcl_cassert.h>
 
 //: The traits for different distributions template arguments.
 // The vector and matrix data types vary with both T and n.
 // - For n > 1 the data types are vnl_vector_fixed<T,n> and vnl_matrix_fixed<T,n,n>
 // - For n == 1 the data types are T and T
 // - For n == 0 the data types are vnl_vector<T> and vnl_matrix<T>
-// 
+//
 // The n == 0 indicates that the dimension is dynamic and set at run time.
 template<class T, unsigned int n=0>
 class vpdl_base_traits
 {
-public:
+ public:
   //: The compile time dimension of the distribution
   enum { fixed_dim = n };
-  //: the data type used for vectors (e.g. the mean) 
+  //: the data type used for vectors (e.g. the mean)
   typedef vnl_vector_fixed<T,n> vector;
   //: the data type used for matrices (e.g. covariance)
   typedef vnl_matrix_fixed<T,n,n> matrix;
-  
+
   //: Constructor - does nothing
   vpdl_base_traits(unsigned int dim=n) {}
-  
+
   //: return the dimension of the space
-  unsigned int dimension() const { return n; }  
+  unsigned int dimension() const { return n; }
   //: set the variable dimension
   // the dimension is not variable, so do nothing and hope to be optimized away
   void set_dimension(unsigned int) {}
-  
+
   // functions for uniform array access to all vector and matrix types
   //==================================================================
-  
+
   //: Access vector size
   static inline unsigned int v_size(const vector& v) { return n; }
   //: Access matrix size
   static inline unsigned int m_size(const matrix& m) { return n; }
-  
+
   //: resize a vector - do nothing, fixed size
   static inline void set_size(vector& v, unsigned int s) {}
   //: resize a matrix - do nothing, fixed size
   static inline void set_size(matrix& v, unsigned int s) {}
-  
+
   //: initialize a vector by filling it with val
   static inline void v_init(vector& v, unsigned int s, const T& val)
   { v.fill(val); }
-  
+
   //: index into a vector
-  static inline T& index(vector& v, unsigned int i) 
+  static inline T& index(vector& v, unsigned int i)
   { return v[i]; }
   //: index into a vector
-  static inline const T& index(const vector& v, unsigned int i) 
+  static inline const T& index(const vector& v, unsigned int i)
   { return v[i]; }
-  
+
   //: index into a matrix
-  static inline T& index(matrix& m, unsigned int i, unsigned int j) 
+  static inline T& index(matrix& m, unsigned int i, unsigned int j)
   { return m(i,j); }
   //: index into a matrix
-  static inline const T& index(const matrix& m, unsigned int i, unsigned int j) 
+  static inline const T& index(const matrix& m, unsigned int i, unsigned int j)
   { return m(i,j); }
-}; 
+};
 
 
 //: The traits specialization for scalars
@@ -89,117 +89,117 @@ public:
 template<class T>
 class vpdl_base_traits<T,1>
 {
-public:
+ public:
   //: The compile time dimension of the distribution
   enum { fixed_dim = 1 };
-  //: the data type used for vectors (e.g. the mean) 
+  //: the data type used for vectors (e.g. the mean)
   typedef T vector;
   //: the data type used for matrices (e.g. covariance)
   typedef T matrix;
-  
+
   //: Constructor - does nothing
   vpdl_base_traits(unsigned int dim=1) {}
-  
+
   //: return the dimension of the space
-  unsigned int dimension() const { return 1; }  
+  unsigned int dimension() const { return 1; }
   //: set the variable dimension
   // the dimension is not variable, so do nothing and hope to be optimized away
   void set_dimension(unsigned int) {}
-  
+
   // functions for uniform array access to all vector and matrix types
   //==================================================================
-  
+
   //: Access vector size
   static inline unsigned int v_size(const vector& v) { return 1; }
   //: Access matrix size
   static inline unsigned int m_size(const matrix& m) { return 1; }
-  
+
   //: resize a vector or matrix - do nothing, fixed size
   static inline void set_size(vector& v, unsigned int s) {}
-  
+
   //: initialize a vector by filling it with val
   static inline void v_init(vector& v, unsigned int s, const T& val)
   { v = val; }
-  
+
   //: index into a vector
-  static inline T& index(vector& v, unsigned int i) 
+  static inline T& index(vector& v, unsigned int i)
   { return v; }
   //: index into a vector
-  static inline const T& index(const vector& v, unsigned int i) 
+  static inline const T& index(const vector& v, unsigned int i)
   { return v; }
-  
+
   //: index into a matrix
-  static inline T& index(matrix& m, unsigned int i, unsigned int j) 
+  static inline T& index(matrix& m, unsigned int i, unsigned int j)
   { return m; }
   //: index into a matrix
-  static inline const T& index(const matrix& m, unsigned int i, unsigned int j) 
+  static inline const T& index(const matrix& m, unsigned int i, unsigned int j)
   { return m; }
-}; 
+};
 
 
 //: The traits specialization for dynamic size vectors
 template<class T>
 class vpdl_base_traits<T,0>
 {
-public:
+ public:
   //: The compile time dimension of the distribution
   enum { fixed_dim = 0 };
-  //: the data type used for vectors (e.g. the mean) 
+  //: the data type used for vectors (e.g. the mean)
   typedef vnl_vector<T> vector;
   //: the data type used for matrices (e.g. covariance)
   typedef vnl_matrix<T> matrix;
-  
+
   //: Constructor - optionally sets the dimension
   vpdl_base_traits(unsigned int dim=0) : var_dim_(dim) {}
-  
+
   //: return the dimension of the space
   unsigned int dimension() const { return var_dim_; }
   //: set the variable dimension
   void set_dimension(unsigned int dim) { var_dim_ = dim; }
-  
+
   // functions for uniform array access to all vector and matrix types
   //==================================================================
-  
+
   //: Access vector size
   static inline unsigned int v_size(const vector& v) { return v.size(); }
   //: Access matrix size
-  static inline unsigned int m_size(const matrix& m) 
-  { 
-    assert(m.cols() == m.rows()); 
-    return m.cols(); 
+  static inline unsigned int m_size(const matrix& m)
+  {
+    assert(m.cols() == m.rows());
+    return m.cols();
   }
-  
+
   //: resize a vector
-  static inline void set_size(vector& v, unsigned int s) 
+  static inline void set_size(vector& v, unsigned int s)
   { v.set_size(s); }
   //: resize a matrix
-  static inline void set_size(matrix& m, unsigned int s) 
+  static inline void set_size(matrix& m, unsigned int s)
   { m.set_size(s,s); }
-  
+
   //: initialize a vector by filling it with val
   static inline void v_init(vector& v, unsigned int s, const T& val)
-  { 
-    v.set_size(s); 
-    v.fill(val); 
+  {
+    v.set_size(s);
+    v.fill(val);
   }
-   
+
   //: index into a vector
-  static inline T& index(vector& v, unsigned int i) 
+  static inline T& index(vector& v, unsigned int i)
   { return v[i]; }
   //: index into a vector
-  static inline const T& index(const vector& v, unsigned int i) 
+  static inline const T& index(const vector& v, unsigned int i)
   { return v[i]; }
-  
+
   //: index into a matrix
-  static inline T& index(matrix& m, unsigned int i, unsigned int j) 
+  static inline T& index(matrix& m, unsigned int i, unsigned int j)
   { return m(i,j); }
   //: index into a matrix
-  static inline const T& index(const matrix& m, unsigned int i, unsigned int j) 
+  static inline const T& index(const matrix& m, unsigned int i, unsigned int j)
   { return m(i,j); }
-  
-private:
+
+ private:
   //: the variable dimension of the space, set at run-time
   unsigned int var_dim_;
-}; 
+};
 
 #endif // vpdl_base_traits_h_
--- core/vpdl/vpdl_distribution.txx
+++ core/vpdl/vpdl_distribution.txx
@@ -6,7 +6,7 @@
 
 #include "vpdl_distribution.h"
 #include <vcl_limits.h>
-
+#include <vcl_cassert.h>
 
 //: Default implementation of numerical CDF inverse computation.
 // This function is called by the virtual function inverse_cdf() by default
@@ -26,9 +26,9 @@ namespace{
 template <class T, unsigned int n>
 class inverse_cdf_helper
 {
-public:
+ public:
   typedef typename vpdl_distribution<T,n>::vector vector;
-  
+
   //: Do the actual inversion
   static inline vector invert(const vpdl_distribution<T,n>& dist, const T& p)
   {
@@ -39,9 +39,9 @@ public:
 template <class T>
 class inverse_cdf_helper<T,1>
 {
-public:
+ public:
   typedef typename vpdl_distribution<T,1>::vector vector;
-  
+
   //: Do the actual inversion
   static inline vector invert(const vpdl_distribution<T,1>& dist, const T& p)
   {
@@ -52,9 +52,9 @@ public:
 template <class T>
 class inverse_cdf_helper<T,0>
 {
-public:
+ public:
   typedef typename vpdl_distribution<T,0>::vector vector;
-  
+
   //: Do the actual inversion
   static inline vector invert(const vpdl_distribution<T,0>& dist, const T& p)
   {
@@ -70,7 +70,7 @@ public:
 // This is only valid for univariate distributions
 // multivariate distributions will return -infinity
 template <class T, unsigned int n>
-typename vpdl_distribution<T,n>::vector 
+typename vpdl_distribution<T,n>::vector
 vpdl_distribution<T,n>::inverse_cdf(const T& p) const
 {
   // use a static helper class to do partial specialization
@@ -82,54 +82,53 @@ vpdl_distribution<T,n>::inverse_cdf(const T& p) const
 // The box is defined by two points, the minimum and maximum.
 // Implemented in terms of \c cumulative_prob() by default.
 template <class T, unsigned int n>
-T vpdl_distribution<T,n>::box_prob(const vector& min_pt, 
+T vpdl_distribution<T,n>::box_prob(const vector& min_pt,
                                    const vector& max_pt) const
 {
   const unsigned int dim = this->dimension();
 
   // return zero for ill-defined box
-  for(unsigned int j=0; j<dim; ++j){
-    if(index(max_pt,j)<=index(min_pt,j))
+  for (unsigned int j=0; j<dim; ++j){
+    if (index(max_pt,j)<=index(min_pt,j))
       return T(0);
   }
-  
+
   // this method is not tractable for large dimensions
   assert(sizeof(unsigned long)*8 > dim);
   // compute the number of corners of the box (2^dim)
   const unsigned long num_corners = 1 << dim;
-  
+
   T prob = T(0);
   vector corner(min_pt);
-  for(unsigned long i=0; i<num_corners; ++i)
+  for (unsigned long i=0; i<num_corners; ++i)
   {
-    // In odd dimensions, corners with an odd number of maximal axis 
+    // In odd dimensions, corners with an odd number of maximal axis
     // are added to the sum.  In even dimensions, corners with an even
     // number of maximal axis are added. The other corners are subtracted.
     bool plus = (dim%2 != 1);
     // create the corner position by selecting elements from max_pt and min_pt
-    for(unsigned int j=0; j<dim; ++j){
+    for (unsigned int j=0; j<dim; ++j){
       bool is_max = (i>>j) & 1;
       plus ^= is_max; // toggle plus if is_max
       index(corner,j) = is_max?index(max_pt,j):index(min_pt,j);
     }
-    if(plus)
+    if (plus)
       prob += cumulative_prob(corner);
     else
       prob -= cumulative_prob(corner);
   }
-  
+
   return prob;
 }
 
 
 #undef VPDL_DISTRIBUTION_INSTANTIATE
 #define VPDL_DISTRIBUTION_INSTANTIATE(T,n) \
-template class vpdl_distribution<T, n >; 
+template class vpdl_distribution<T,n >
 
 // instantiate this function only for n == 1
 #undef VPDL_INVERSE_CDF_INSTANTIATE
 #define VPDL_INVERSE_CDF_INSTANTIATE(T) \
-template T vpdl_compute_inverse_cdf(const vpdl_distribution<T ,1>& dist, double p); 
-
+template T vpdl_compute_inverse_cdf(const vpdl_distribution<T,1>& dist, double p)
 
 #endif // vpdl_distribution_txx_
