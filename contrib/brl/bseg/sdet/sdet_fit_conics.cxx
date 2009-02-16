e46acb8116d06d9cf3215db91783611dd6610268
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-02-16 18:23:56 -0500

    *** empty log message ***

--- contrib/brl/bseg/sdet/sdet_fit_conics.cxx
+++ contrib/brl/bseg/sdet/sdet_fit_conics.cxx
@@ -82,7 +82,7 @@ bool sdet_fit_conics::fit_conics()
       vsol_conic_2d_sptr conic = new vsol_conic_2d(*sit);
       vcl_cout << "Fitted a conic of type " << conic->real_conic_type() << '\n';
       //adding a condition on aspect ratio
-      if (conic->real_type() != real_ellipse)
+      if (conic->real_type() != vsol_conic_2d::real_ellipse)
       {
           conic_segs_.push_back(conic);
       }
