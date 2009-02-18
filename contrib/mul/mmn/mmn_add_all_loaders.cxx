ab9a54657e6cc2d3ea06e6219a0379ece260116f
Tim Cootes <timcootes@users.sourceforge.net>
2009-02-18 10:14:38 -0500

    Added function to add all valid loaders to relevent singleton objects.

---
+++ contrib/mul/mmn/mmn_add_all_loaders.cxx
@@ -0,0 +1,16 @@
+#include "mmn_add_all_loaders.h"
+//:
+// \file
+// \brief Add all binary loaders and factory objects for mmn library
+// \author Tim Cootes
+
+#include <mbl/mbl_cloneables_factory.h>
+#include "mmn_dp_solver.h"
+
+//: Add all binary loaders and factory objects for mmn library
+void mmn_add_all_loaders()
+{
+
+  mbl_cloneables_factory<mmn_solver>::add(mmn_dp_solver());
+  vsl_add_to_binary_loader(mmn_dp_solver());
+}
