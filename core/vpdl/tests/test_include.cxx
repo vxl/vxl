5964673fc12a89507789c521d80dbbb5b6f245d9
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-03-07 11:08:03 -0500

    added test_include.cxx

---
+++ core/vpdl/tests/test_include.cxx
@@ -0,0 +1,21 @@
+#include <vpdl/vpdl_distribution.h>
+#include <vpdl/vpdl_gaussian.h>
+#include <vpdl/vpdl_gaussian_base.h>
+#include <vpdl/vpdl_gaussian_indep.h>
+#include <vpdl/vpdl_gaussian_sphere.h>
+#include <vpdl/vpdl_kernel_base.h>
+#include <vpdl/vpdl_kernel_gaussian_sfbw.h>
+#include <vpdl/vpdl_mixture.h>
+#include <vpdl/vpdl_mixture_of.h>
+#include <vpdl/vpdl_multi_cmp_dist.h>
+#include <vpdl/vpdl_update_gaussian.h>
+
+#include <vpdl/vpdt/vpdt_access.h>
+#include <vpdl/vpdt/vpdt_dist_traits.h>
+#include <vpdl/vpdt/vpdt_eigen_sym_matrix.h>
+#include <vpdl/vpdt/vpdt_field_default.h>
+#include <vpdl/vpdt/vpdt_field_traits.h>
+#include <vpdl/vpdt/vpdt_gaussian.h>
+#include <vpdl/vpdt/vpdt_norm_metric.h>
+
+int main() { return 0; }
