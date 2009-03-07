75fa3893340e4cc0bf13f4d4c10e98de1c269ddc
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-03-07 18:51:02 -0500

    avoid unnecessary compiler warning about unused parameters

--- core/vil/vil_file_format.h
+++ core/vil/vil_file_format.h
@@ -34,7 +34,7 @@ class vil_file_format
   //: Read a pyramid resource from a list of image files in a directory
   //  ... or from an image file_format that supports multiple images per file.
   virtual vil_pyramid_image_resource_sptr
-    make_input_pyramid_image(char const* directory_or_file)
+    make_input_pyramid_image(char const* /*directory_or_file*/)
     {return 0;}
 
   //: Construct a pyramid image resource from a base image.
@@ -46,21 +46,21 @@ class vil_file_format
   //  be removed from the directory after completion.  If temp_dir is 0
   //  then the intermediate resources are created in memory.
   virtual vil_pyramid_image_resource_sptr
-    make_pyramid_image_from_base(char const* filename,
-                                 vil_image_resource_sptr const& base_image,
-                                 unsigned nlevels,
-                                 char const* temp_dir)
-  {return 0;}
+    make_pyramid_image_from_base(char const* /*filename*/,
+                                 vil_image_resource_sptr const& /*base_image*/,
+                                 unsigned /*nlevels*/,
+                                 char const* /*temp_dir*/)
+    {return 0;}
 
   //: Make a "generic_image" on which put_section may be applied.
   // The stream vs is assumed to be open for writing, as an image header may be
   // written to it immediately.
   // The width/height etc are explicitly specified, so that file_format implementors
   // know what they need to do...
-  virtual vil_image_resource_sptr make_output_image(vil_stream* vs,
-                                                    unsigned nx,
-                                                    unsigned ny,
-                                                    unsigned nplanes,
+  virtual vil_image_resource_sptr make_output_image(vil_stream* /*vs*/,
+                                                    unsigned /*nx*/,
+                                                    unsigned /*ny*/,
+                                                    unsigned /*nplanes*/,
                                                     enum vil_pixel_format) = 0;
   //: Construct a blocked output image resource
   // Returns a null resource unless the format supports blocking
@@ -71,11 +71,11 @@ class vil_file_format
                               unsigned /*nplanes*/,
                               unsigned /*size_block_i*/,
                               unsigned /* size_block_j*/,
-                              enum vil_pixel_format /*fmt*/)
+                              enum vil_pixel_format)
     {return 0;}
 
   virtual vil_pyramid_image_resource_sptr
-    make_pyramid_output_image(char const* file)
+    make_pyramid_output_image(char const* /*file*/)
     {return 0;}
 
  public:
