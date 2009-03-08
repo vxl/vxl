e4da094933175188d132c4c84eafb596e1f0fff2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-03-08 11:55:39 -0400

    added lots of tests, to increase the coverage (was only 12% ...)

--- core/vgl/tests/test_p_matrix.cxx
+++ core/vgl/tests/test_p_matrix.cxx
@@ -2,16 +2,158 @@
 // \file
 // \author Joseph Mundy
 // \date  June 4, 2003
+//
+// \verbatim
+//  Modifications
+//   2009-03-08 Peter Vanroose - Increased the test coverage by adding tests for
+//                               constructors, read/write, and accessors.
+// \endverbatim
 
 #include <testlib/testlib_test.h>
+#include <vgl/algo/vgl_p_matrix.h>
+#include <vgl/algo/vgl_h_matrix_3d.h>
 #include <vcl_cmath.h>
 #include <vcl_iostream.h>
+#include <vcl_sstream.h>
+#include <vgl/vgl_homg_point_2d.h>
 #include <vgl/vgl_homg_point_3d.h>
-#include <vnl/vnl_matrix_fixed.h>
-#include <vgl/algo/vgl_p_matrix.h>
+#include <vgl/vgl_distance.h>
+#include <vnl/vnl_double_3.h>
+#include <vnl/vnl_double_4.h>
+#include <vnl/vnl_double_3x3.h>
+#include <vnl/vnl_double_3x4.h>
+#include <vnl/vnl_matrix.h>
+
 static double d(double x0, double x1, double y0, double y1)
 {
   return vcl_sqrt((x0-x1)*(x0-x1) + (y0-y1)*(y0-y1));
+}
+
+static bool equals(double x[12], double y[12])
+{
+  for (int i=0; i<12; ++i) if (x[i] != y[i]) return false;
+  return true;
+}
+
+static void test_constructors()
+{
+  double data[12]; // the projective c-matrix
+  {
+    double gold[] = {1,0,0,0, 0,1,0,0, 0,0,1,0}; // the "ground truth"
+    vgl_p_matrix<double> P; P.get(data);
+    TEST( "Default constructor", equals(data, gold), true );
+  }
+  {
+    double gold[] = {1,2,3,4, 5,6,7,8, 9,10,11,12}; // the "ground truth"
+    vgl_p_matrix<double> P0(gold);
+    vgl_p_matrix<double> P(P0); P.get(data);
+    TEST( "Copy constructor", equals(data, gold), true );
+  }
+  {
+    vcl_stringstream ss; ss << "1 2 3 4 5 6 7 8 9 10 11 12";
+    double gold[] = {1,2,3,4, 5,6,7,8, 9,10,11,12}; // the "ground truth"
+    vgl_p_matrix<double> P(ss); P.get(data);
+    TEST( "Constructor from istream", equals(data, gold), true );
+  }
+  {
+    vcl_stringstream ss; ss << "1 2 3 4 5 6 7 8 9 10 11 12";
+    double gold[] = {1,2,3,4, 5,6,7,8, 9,10,11,12}; // the "ground truth"
+    vgl_p_matrix<double> P; ss >> P; P.get(data);
+    TEST( "operator>> from istream", equals(data, gold), true );
+  }
+  {
+    vcl_stringstream ss; ss << "1 2 3 4 5 6 7 8 9 10 11 12";
+    double gold[] = {1,2,3,4, 5,6,7,8, 9,10,11,12}; // the "ground truth"
+    vgl_p_matrix<double> P; P.read_ascii(ss); P.get(data);
+    TEST( "read_ascii() method", equals(data, gold), true );
+  }
+  {
+    vcl_stringstream ss; ss << "1 2 3 4 5 6 7 8 9 10 11 12";
+    double gold[] = {1,2,3,4, 5,6,7,8, 9,10,11,12}; // the "ground truth"
+    vgl_p_matrix<double> P = vgl_p_matrix<double>::read(ss); P.get(data);
+    TEST( "read() static method", equals(data, gold), true );
+  }
+  {
+    double gold[] = {1,2,3,4, 5,6,7,8, 9,10,11,12}; // the "ground truth"
+    vnl_double_3x4 M(gold);
+    vgl_p_matrix<double> P(M); P.get(data);
+    TEST( "Constructor from vnl_double_3x4", equals(data, gold), true );
+  }
+  {
+    double gold[] = {1,2,3,4, 5,6,7,8, 9,10,11,12}; // the "ground truth"
+    vgl_p_matrix<double> P(gold); P.get(data);
+    TEST( "Constructor from raw 3x4 data array", equals(data, gold), true );
+  }
+  {
+    double gold[] = {7,7,7,30, 7,7,7,40, 7,7,7,50}; // the "ground truth"
+    vnl_matrix<double> M(3,3,7.0);
+    vnl_double_3 V(30,40,50);
+    vgl_p_matrix<double> P(M,V); P.get(data);
+    TEST( "Constructor from vnl_matrix and vnl_vector", equals(data, gold), true );
+    vnl_vector<double> v = V;
+    P.get(&M,&v);
+    vgl_p_matrix<double> P2(M,v); P2.get(data);
+    TEST( "get(vnl_matrix*,vnl_vector*)", equals(data, gold), true );
+    vnl_double_3x3 m = M;
+    P.get(&m,&V);
+    vgl_p_matrix<double> P3(m,V); P3.get(data);
+    TEST( "get(vnl_matrix_fixed*,vnl_vector_fixed*)", equals(data, gold), true );
+  }
+  {
+    double gold[] = {1,2,3,4, 5,6,7,8, 9,10,11,12}; // the "ground truth"
+    vgl_p_matrix<double> P0(gold);
+    vgl_p_matrix<double> P; P = P0; P.get(data);
+    TEST( "Assignment operator", equals(data, gold), true );
+  }
+  {
+    double gold[] = {1,2,3,4, 5,6,7,8, 9,10,11,12}; // the "ground truth"
+    vgl_p_matrix<double> P; P.set(gold); P.get(data);
+    TEST( "set(array)", equals(data, gold), true );
+  }
+  {
+    double gold[] = {1,2,3,4, 5,6,7,8, 9,10,11,12}; // the "ground truth"
+    double matrix[][4] = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}}; // the "ground truth"
+    vgl_p_matrix<double> P; P.set(matrix); P.get(data);
+    TEST( "set(matrix)", equals(data, gold), true );
+  }
+  {
+    double gold[] = {7,7,7,30, 7,7,7,40, 7,7,7,50}; // the "ground truth"
+    vnl_matrix<double> M(3,3,7.0);
+    vnl_double_3 V(30,40,50);
+    vgl_p_matrix<double> P; P.set(M,V); P.get(data);
+    TEST( "set(matrix,vector)", equals(data, gold), true );
+  }
+}
+
+static void test_accessors()
+{
+  double rotation[] = {1,0,0,0, 0,0.6,0.8,0, 0,-0.8,0.6,0, 0,0,0,1}; // Euclidean rotation around (0,0,0,1)
+  vgl_p_matrix<double> P(rotation);
+  vnl_double_3x4 M(rotation);
+
+  TEST( "get()", P.get(1,2), 0.8 );
+  TEST( "get_matrix()", P.get_matrix(), M );
+  vnl_vector<double> v1, v2, v3; P.get_rows(&v1,&v2,&v3);
+  TEST( "get_rows(vnl_vector x3)", v1==vnl_double_4(1,0,0,0) && v2==vnl_double_4(0,0.6,0.8,0) && v3==vnl_double_4(0,-0.8,0.6,0), true );
+  vnl_double_4 d1, d2, d3; P.get_rows(&d1,&d2,&d3);
+  TEST( "get_rows(vnl_vector_fixed x3)", d1==v1 && d2==v2 && d3==v3, true );
+  vcl_stringstream ss; ss << P;
+  TEST( "operator<< to ostream", P, vgl_p_matrix<double>(ss) );
+  TEST_NEAR( "operator()(point projection)", vgl_distance(P(vgl_homg_point_3d<double>(1.0,2.0,0.0,0.0)),vgl_homg_point_2d<double>(1.0,1.2,-1.6)), 0, 1e-10 );
+  TEST( "get_focal()", P.get_focal(), vgl_homg_point_3d<double>(0.0,0.0,0.0,1.0) );
+  rotation[6] *= -1; rotation[9] *= -1; vgl_h_matrix_3d<double> H(rotation);
+  TEST( "get_canonical_H()", P.get_canonical_H(), H.get_matrix() );
+  TEST( "is_canonical() (small tolerance)", P.is_canonical(0.75), false );
+  TEST( "is_canonical() (larger tolerance)", P.is_canonical(0.8), true );
+}
+
+static void test_cheirality()
+{
+  double rotation[] = {1,0,0,0, 0,-0.6,0.8,0, 0,0.8,0.6,0}; // Euclidean rotation around (0,0,0,1)
+  vgl_p_matrix<double> P(rotation);
+  P.fix_cheirality(); // this should flip P's sign since rotation[] has det = -1
+  P.flip_sign();      // flip back
+  TEST( "fix_cheirality()", P.get_matrix(), vnl_double_3x4(rotation) );
 }
 
 static void test_homog_product()
@@ -58,7 +200,7 @@ static void test_general_projection()
 {
   vcl_cout << "Testing general projection for points\n";
   // Testing an actual camera projection matrix
-  vnl_matrix_fixed<double, 3, 4> M;
+  vnl_double_3x4 M;
   M.put(0,0,2.081017567642e-003);
   M.put(0,1,1.873683094206e-003);
   M.put(0,2,-3.780462177066e-004);
@@ -91,9 +233,18 @@ static void test_general_projection()
 
 static void test_p_matrix()
 {
+  vcl_cout << "\n==================== test_constructors ====================\n\n";
+  test_constructors();
+  vcl_cout << "\n===================== test_accessors ======================\n\n";
+  test_accessors();
+  vcl_cout << "\n================ test_identity_projection =================\n\n";
   test_identity_projection();
+  vcl_cout << "\n================= test_general_projection =================\n\n";
   test_general_projection();
+  vcl_cout << "\n=================== test_homog_product ====================\n\n";
   test_homog_product();
+  vcl_cout << "\n===================== test_cheirality =====================\n\n";
+  test_cheirality();
 }
 
 TESTMAIN(test_p_matrix);
