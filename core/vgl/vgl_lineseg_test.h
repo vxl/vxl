ecba5a45f46763c89e6327205ebe3aa96f0efdd9
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-03-10 17:03:45 -0400

    bug fix (missing sqrt()) in vgl_lineseg_test_point, with thanks to Dirk Steckhan

--- core/vgl/vgl_lineseg_test.h
+++ core/vgl/vgl_lineseg_test.h
@@ -9,12 +9,15 @@
 //   Nov.2003 - Peter Vanroose - made all functions templated
 //   Sep.2005 - Peter Vanroose - bug fix: collinear line segments always "true"
 //   Mar.2008 - Ibrahim Eden - bug fix: bool vgl_lineseg_test_line(vgl_line_2d<T> const& l1,vgl_line_segment_2d<T> const& l2)
+//   Mar.2009 - Dirk Steckhan - bug fix in vgl_lineseg_test_point (missing sqrt)
 // \endverbatim
 
 #include <vgl/vgl_line_segment_2d.h>
 #include <vgl/vgl_line_2d.h>
 #include <vgl/vgl_point_2d.h>
 #include <vgl/vgl_tolerance.h>
+#include <vcl_cmath.h> // for std::sqrt()
+
 // The old signature vgl_lineseg_test() was incorrectly documented. Its
 // meaning was the same as the new vgl_lineseg_test_line(). Only you can
 // decide which one you want.
@@ -50,24 +53,17 @@ template <class T>
 inline bool vgl_lineseg_test_point(vgl_point_2d<T> const& p,
                                    vgl_line_segment_2d<T> const& lseg)
 {
-  vgl_point_2d<T> p1 = lseg.point1();
-  vgl_point_2d<T> p2 = lseg.point2();
-  T x1 = p1.x(), y1 = p1.y();
-  T x2 = p2.x(), y2 = p2.y();
-  T x3 = p.x(), y3 = p.y();
-  // compute distances
-  T d1p = (x3-x1)*(x3-x1) + (y3-y1)*(y3-y1);
-  T d2p = (x3-x2)*(x3-x2) + (y3-y2)*(y3-y2);
+  vgl_point_2d<T> p1 = lseg.point1(), p2 = lseg.point2();
+  T x1 = p1.x(), y1 = p1.y(),
+    x2 = p2.x(), y2 = p2.y(),
+    xp = p.x(),  yp = p.y();
+  // compute squared distances
+  T d1p = (xp-x1)*(xp-x1) + (yp-y1)*(yp-y1);
+  T d2p = (xp-x2)*(xp-x2) + (yp-y2)*(yp-y2);
   T d12 = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
-  T sum = d1p + d2p;
-  //diff should be positive
-  T diff;
-  if(sum<=d12) diff = d12-sum;
-  else diff = sum-d12;
-  T tol = vgl_tolerance<T>::position;
-  if(diff>tol*tol)
-    return false;
-  return true;
+  double diff = vcl_sqrt(d1p) + vcl_sqrt(d2p) - vcl_sqrt(d12);
+  // diff is always >= 0 (triangle inequality)
+  return diff <= vgl_tolerance<double>::position;
 }
 
 //: return true if the two linesegments meet.
