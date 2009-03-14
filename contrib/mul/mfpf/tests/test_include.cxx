dfa60cb1ac8d83f0c4b789c5428dcf987818228e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-03-13 20:31:34 -0400

    added new header files

--- contrib/mul/mfpf/tests/test_include.cxx
+++ contrib/mul/mfpf/tests/test_include.cxx
@@ -6,6 +6,8 @@
 #include <mfpf/mfpf_edge_finder.h>
 #include <mfpf/mfpf_edge_finder_builder.h>
 #include <mfpf/mfpf_feature_vec.h>
+#include <mfpf/mfpf_grad_corr2d_builder.h>
+#include <mfpf/mfpf_grad_corr2d.h>
 #include <mfpf/mfpf_hog_box_finder.h>
 #include <mfpf/mfpf_hog_box_finder_builder.h>
 #include <mfpf/mfpf_max_finder.h>
