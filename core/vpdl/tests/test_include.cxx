1f348e122ee29c36974484fbba519b9203f5710a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-03-14 17:45:01 -0400

    added new header files

--- core/vpdl/tests/test_include.cxx
+++ core/vpdl/tests/test_include.cxx
@@ -14,13 +14,15 @@
 #include <vpdl/vpdt/vpdt_eigen_sym_matrix.h>
 #include <vpdl/vpdt/vpdt_field_default.h>
 #include <vpdl/vpdt/vpdt_field_traits.h>
-#include <vpdl/vpdt/vpdt_probability.h>
-#include <vpdl/vpdt/vpdt_log_probability.h>
 #include <vpdl/vpdt/vpdt_gaussian.h>
-#include <vpdl/vpdt/vpdt_update_gaussian.h>
-#include <vpdl/vpdt/vpdt_update_mog.h>
+#include <vpdl/vpdt/vpdt_gaussian_detector.h>
+#include <vpdl/vpdt/vpdt_log_probability.h>
+#include <vpdl/vpdt/vpdt_mixture_detector.h>
+#include <vpdl/vpdt/vpdt_mixture_of.h>
 #include <vpdl/vpdt/vpdt_mog_fitness.h>
 #include <vpdl/vpdt/vpdt_norm_metric.h>
-#include <vpdl/vpdt/vpdt_mixture_of.h>
+#include <vpdl/vpdt/vpdt_probability.h>
+#include <vpdl/vpdt/vpdt_update_gaussian.h>
+#include <vpdl/vpdt/vpdt_update_mog.h>
 
 int main() { return 0; }
