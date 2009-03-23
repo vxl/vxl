f9adfaaa4e1336b31c8438a8f1763bb0cca7862f
Gamze Tunali <gamze@users.sourceforge.net>
2009-03-23 13:45:59 -0400

    added print and split methods
    SVN-Revision: 24507

--- contrib/brl/bseg/boct/tests/test_create_tree.cxx
+++ contrib/brl/bseg/boct/tests/test_create_tree.cxx
@@ -8,9 +8,13 @@
 MAIN( test_create_tree )
 {
   START ("CREATE TREE");
-  short nlevels=10;
+  short nlevels=5;
   boct_tree * block=new boct_tree(nlevels);
-  TEST("No of Max levels of tree",nlevels, block->numlevels());
+  TEST("No of Max levels of tree",nlevels, block->num_levels());
+  
+  block->split();
+  block->print();
+
   SUMMARY();
 
   
