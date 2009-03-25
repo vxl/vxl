acc2ed8888673624d148a2cb06c32f4f561009ab
Vishal Jain <jainv@users.sourceforge.net>
2009-03-25 09:02:49 -0400

    SVN-Revision: 24534

--- contrib/brl/bseg/boct/tests/test_create_tree.cxx
+++ contrib/brl/bseg/boct/tests/test_create_tree.cxx
@@ -2,8 +2,22 @@
 #include <testlib/testlib_test.h>
 
 #include <boct/boct_tree.h>
+#include <vnl/vnl_random.h>
 
+void create_random_configuration_tree(boct_tree* tree)
+{
+    tree=new boct_tree(10);
+    
+    for(unsigned int i=0;i<200;i++)
+    {
+        vnl_random rand;
+        vgl_point_3d<double> p(rand.drand32(),rand.drand32(),rand.drand32());
+        boct_tree_cell * curr_cell=tree->locate_point(p);
+        if(curr_cell)
+            curr_cell->split();
+    }
 
+}
 
 MAIN( test_create_tree )
 {
@@ -14,6 +28,12 @@ MAIN( test_create_tree )
   
   block->split();
   block->print();
+
+
+  boct_tree * randomtree;
+  create_random_configuration_tree(randomtree);
+
+  randomtree->print();
 
   SUMMARY();
 
