f94d3edefcf1ca67734ad90f3fc19826b58aa7b6
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-03-28 16:22:46 -0400

    contrib/brl/bbas/vidl2 was moved to core/vidl
    SVN-Revision: 24565

--- contrib/brl/bbas/vidl2/config.mk
+++
@@ -1,3 +0,0 @@
-IULIBS += -lvidl2
-include ${VXLROOT}/core/vil/config.mk
-include ${VXLROOT}/core/vul/config.mk
--- contrib/brl/bbas/vidl2/gui/config.mk
+++
@@ -1,3 +0,0 @@
-IULIBS += -lvidl2_gui
-include ${VXLROOT}/contrib/brl/bbas/vidl2/config.mk
-include ${VXLROOT}/core/vgui/config.mk
