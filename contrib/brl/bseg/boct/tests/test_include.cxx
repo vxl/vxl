5bd0518dca17d44776e71783b64083cafd4f781f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-04-01 03:04:32 -0400

    removed nonexisting #includes
    SVN-Revision: 24584

--- contrib/brl/bseg/boct/tests/test_include.cxx
+++ contrib/brl/bseg/boct/tests/test_include.cxx
@@ -1,9 +1,6 @@
-#include <boct/boct_tree_cell<T>_sptr.h>
-
 #include <boct/boct_loc_code.h>
 #include <boct/boct_test_util.h>
 #include <boct/boct_tree.h>
 #include <boct/boct_tree_cell.h>
-#include <boct/boct_tree_cell<T>.h>
 
 int main() { return 0; }
