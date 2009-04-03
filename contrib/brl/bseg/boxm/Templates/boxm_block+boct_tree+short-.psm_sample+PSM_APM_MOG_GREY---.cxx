effe9b5644799a3c53d01a5b39171b8497030e6c
Gamze Tunali <gamze@users.sourceforge.net>
2009-04-03 16:22:54 -0400

    added template instantiations
    SVN-Revision: 24617

---
+++ contrib/brl/bseg/boxm/Templates/boxm_block+boct_tree+short-.psm_sample+PSM_APM_MOG_GREY---.cxx
@@ -0,0 +1,8 @@
+#include <boxm/boxm_block.txx>
+#include <boxm/boxm_apm_traits.h>
+#include <boxm/boxm_sample.h>
+#include <boct/boct_tree.txx>
+#include <vgl/io/vgl_io_point_3d.h>
+
+typedef boct_tree<short, boxm_sample<BOXM_APM_MOG_GREY> >  tree_type;
+BOXM_BLOCK_INSTANTIATE(tree_type);
