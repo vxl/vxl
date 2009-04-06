03b98a31722d01a3aadac3a9a08e07634a6b3607
Vishal Jain <jainv@users.sourceforge.net>
2009-04-06 13:33:30 -0400

    added instantiations for BOXM_APM_MOG_GREY
    SVN-Revision: 24626

--- contrib/brl/bseg/boxm/Templates/boct_tree+short-.psm_sample+BOXM_APM_MOG_GREY-.cxx
+++ contrib/brl/bseg/boxm/Templates/boct_tree+short-.psm_sample+BOXM_APM_MOG_GREY-.cxx
@@ -2,5 +2,5 @@
 #include "boxm/boxm_apm_traits.h"
 #include "boxm/boxm_sample.h"
 
-BOCT_TREE_INSTANTIATE(short, boxm_sample<BOXM_APM_MOG_GREY>);
+BOCT_TREE_INSTANTIATE(short, boxm_sample<BOXM_APM_MOG_GREY> );
 
--- contrib/brl/bseg/boxm/Templates/boxm_scene+boct_tree+short-.psm_sample+BOXM_APM_MOG_GREY---.cxx
+++ contrib/brl/bseg/boxm/Templates/boxm_scene+boct_tree+short-.psm_sample+BOXM_APM_MOG_GREY---.cxx
@@ -1,8 +1,6 @@
 #include <boxm/boxm_scene.txx>
 #include <boct/boct_tree.txx>
 #include <boxm/boxm_sample.h>
-#include <boct/boct_tree.txx>
-#include <vgl/io/vgl_io_point_3d.h>
 
 typedef boct_tree<short, boxm_sample<BOXM_APM_MOG_GREY> >  tree_type;
 BOXM_SCENE_INSTANTIATE(tree_type);
