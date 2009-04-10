a312440ba58fc63f3665951ef013637a3bae0a6f
Gamze Tunali <gamze@users.sourceforge.net>
2009-04-09 21:45:06 -0400

    added new methods
    SVN-Revision: 24653

--- contrib/brl/bseg/boxm/boxm_utils.cxx
+++ contrib/brl/bseg/boxm/boxm_utils.cxx
@@ -2,10 +2,14 @@
 #include <boct/boct_tree_cell.h>
 #include <vgl/vgl_box_2d.h>
 #include <vgl/vgl_point_2d.h>
+#include <vgl/vgl_vector_2d.h>
+#include <vsol/vsol_point_3d.h>
+#include <vsol/vsol_point_3d_sptr.h>
+#include <vsol/vsol_polygon_3d.h>
 #include <vpgl/vpgl_perspective_camera.h>
 
 
-bool is_visible(vgl_box_3d<double> const& bbox, 
+bool boxm_utils::is_visible(vgl_box_3d<double> const& bbox, 
                 vpgl_camera_double_sptr const& camera, 
                 unsigned int img_ni, unsigned int img_nj, 
                 bool do_front_test)
@@ -65,7 +69,70 @@ bool is_visible(vgl_box_3d<double> const& bbox,
   return true;
 }
 
-boct_face_idx visible_faces(vgl_box_3d<double> &bbox, vpgl_camera_double_sptr camera)
+//                                 MaxPosition
+//                                      
+//                       7-----------6 
+//                      /           /|  
+//                     /           / |   
+//                    4-----------5  | 
+//                    |           |  |  
+//                    |           |  |
+//                    |    3      |  2
+//     Y              |           | /   
+//     |   Z          |           |/  
+//     |  /           0-----------1  
+//     | /         MinPosition
+//     O-----X
+
+vcl_vector<vgl_point_3d<double> >
+boxm_utils::corners_of_box_3d(vgl_box_3d<double> box)
+{
+  vcl_vector<vgl_point_3d<double> > corners;
+
+  corners.push_back(box.min_point());
+  corners.push_back(vgl_point_3d<double> (box.min_x()+box.width(), box.min_y(), box.min_z()));
+  corners.push_back(vgl_point_3d<double> (box.min_x()+box.width(), box.min_y()+box.height(), box.min_z()));
+  corners.push_back(vgl_point_3d<double> (box.min_x(), box.min_y()+box.height(), box.min_z()));
+  corners.push_back(vgl_point_3d<double> (box.min_x(), box.min_y(), box.max_z()));
+  corners.push_back(vgl_point_3d<double> (box.min_x()+box.width(), box.min_y(), box.max_z()));
+  corners.push_back(box.max_point());
+  corners.push_back(vgl_point_3d<double> (box.min_x(), box.min_y()+box.height(), box.max_z()));
+  return corners;
+}
+
+bool boxm_utils::is_face_visible(vcl_vector<vgl_point_3d<double> > corners,
+                                vpgl_camera_double_sptr const& camera)
+{
+  double u,v;
+  vgl_box_2d<double> face;
+  vcl_vector<vsol_point_3d_sptr> vs;
+
+  assert(corners.size() >= 3);
+
+  for (unsigned i=0; i<corners.size(); i++) {
+    camera->project(corners[i].x(), corners[i].y(), corners[i].z(), u, v);
+    vs.push_back(new vsol_point_3d(u,v,0));
+  }
+  
+  vsol_polygon_3d poly3d(vs);
+  vgl_vector_3d<double> normal = poly3d.normal();
+  /*vsol_point_2d_sptr c = poly2d.centroid();
+  vgl_vector_3d<double> normal(0.0, 0.0, 0.0);
+  for (unsigned i=0; i<vs.size(); i++) {
+    vgl_vector_2d<double> v0 = vs[i]->get_p() - c->get_p();
+    unsigned next = i+1;
+    if (next == vs.size())
+      next = 0;
+    vgl_vector_2d<double> v1 = vs[next]->get_p() - c->get_p();
+    normal += cross_product<double>(v0,v1);
+  }*/
+  if (normal.z() < 0)
+    return true;
+  return false;
+}
+
+boct_face_idx
+boxm_utils::visible_faces(vgl_box_3d<double> &bbox, vpgl_camera_double_sptr camera)
 {
   boct_face_idx faces = boct_cell_face::NONE;
   if (camera->type_name().compare("vpgl_perspective_camera")) {
@@ -90,7 +157,62 @@ boct_face_idx visible_faces(vgl_box_3d<double> &bbox, vpgl_camera_double_sptr ca
     else if (cam_center.z() < bbox.min_z()) {
       faces |= boct_cell_face::Z_LOW;
     }
-  }
+  } 
+  // for other cameras, use projection and normals
+  else {
+    // fix the face normals so that the vertices are the counter clokwise order
+    vcl_vector<vgl_point_3d<double> > corners=corners_of_box_3d(bbox);
+  
+    // face bottom [1,0,3,2]
+    vcl_vector<vgl_point_3d<double> > face_corners;
+    face_corners.push_back(corners[1]);
+    face_corners.push_back(corners[0]);
+    face_corners.push_back(corners[3]);
+    face_corners.push_back(corners[2]);
+    if (is_face_visible(face_corners, camera))
+      faces |= boct_cell_face::Z_LOW;
+  
+    // face top [4,5,6,7]
+    face_corners.clear();
+    face_corners.push_back(corners[4]);
+    face_corners.push_back(corners[5]);
+    face_corners.push_back(corners[6]);
+    face_corners.push_back(corners[7]);
+    if (is_face_visible(face_corners, camera))
+      faces |= boct_cell_face::Z_HIGH;  
 
+    face_corners.clear();
+    face_corners.push_back(corners[1]);
+    face_corners.push_back(corners[2]);
+    face_corners.push_back(corners[6]);
+    face_corners.push_back(corners[5]);
+    if (is_face_visible(face_corners, camera))
+      faces |= boct_cell_face::X_HIGH;  
+
+    face_corners.clear();
+    face_corners.push_back(corners[3]);
+    face_corners.push_back(corners[0]);
+    face_corners.push_back(corners[4]);
+    face_corners.push_back(corners[7]);
+    if (is_face_visible(face_corners, camera))
+      faces |= boct_cell_face::X_LOW;  
+
+    face_corners.clear();
+    face_corners.push_back(corners[2]);
+    face_corners.push_back(corners[3]);
+    face_corners.push_back(corners[7]);
+    face_corners.push_back(corners[6]);
+    if (is_face_visible(face_corners, camera))
+      faces |= boct_cell_face::Y_HIGH;  
+    face_corners.clear();
+
+    face_corners.clear();
+    face_corners.push_back(corners[0]);
+    face_corners.push_back(corners[1]);
+    face_corners.push_back(corners[5]);
+    face_corners.push_back(corners[4]);
+    if (is_face_visible(face_corners, camera))
+      faces |= boct_cell_face::Y_LOW;  
+  }
   return faces;
 }
--- contrib/brl/bseg/boxm/boxm_utils.h
+++ contrib/brl/bseg/boxm/boxm_utils.h
@@ -13,20 +13,31 @@
 // \endverbatim
 
 #include <vgl/vgl_box_3d.h>
+#include <vgl/vgl_point_3d.h>
 #include <vpgl/vpgl_camera.h>
 #include <boct/boct_tree_cell.h>
 
-//: returns true if a given box is visible from the camera
-// and the image plane
-bool is_visible(vgl_box_3d<double> const& bbox, 
-                vpgl_camera_double_sptr const& camera, 
-                unsigned int img_ni, unsigned int img_nj, 
-                bool do_front_test = true);
+class boxm_utils {
+
+public:
+
+    //: returns true if a given box is visible from the camera
+    // and the image plane
+  static bool is_visible(vgl_box_3d<double> const& bbox, 
+                    vpgl_camera_double_sptr const& camera, 
+                    unsigned int img_ni, unsigned int img_nj, 
+                    bool do_front_test = true);
 
 
-//: returns the visible faces of a box given a camera. It puts
-// a bit 1 for each face visible based on the boct_cell_face values.
-boct_face_idx visible_faces(vgl_box_3d<double> &bbox, 
-                            vpgl_camera_double_sptr camera);
+  static vcl_vector<vgl_point_3d<double> > corners_of_box_3d(vgl_box_3d<double> box);
+
+  static bool is_face_visible(vcl_vector<vgl_point_3d<double> > corners,
+                              vpgl_camera_double_sptr const& camera);
+
+    //: returns the visible faces of a box given a camera. It puts
+    // a bit 1 for each face visible based on the boct_cell_face values.
+  static boct_face_idx visible_faces(vgl_box_3d<double> &bbox, 
+                                vpgl_camera_double_sptr camera);
+};
 
 #endif
