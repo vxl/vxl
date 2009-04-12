650b2d42e9f2f69a631d21ff5d5bc776ff3dd278
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-04-12 11:41:49 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 24676

---
+++ contrib/brl/bseg/boxm/config.mk
@@ -0,0 +1,11 @@
+IULIBS += -lboxm
+include ${VXLROOT}/contrib/brl/bseg/boxm/io/config.mk
+include ${VXLROOT}/contrib/brl/bseg/boct/config.mk
+include ${VXLROOT}/contrib/brl/bbas/bsta/config.mk
+include ${VXLROOT}/contrib/gel/mrc/vpgl/config.mk
+include ${VXLROOT}/contrib/gel/vsol/config.mk
+include ${VXLROOT}/core/vgl/config.mk
+include ${VXLROOT}/core/vgl/io/config.mk
+include ${VXLROOT}/core/vgl/xio/config.mk
+include ${VXLROOT}/core/vbl/config.mk
+include ${VXLROOT}/core/vsl/config.mk
