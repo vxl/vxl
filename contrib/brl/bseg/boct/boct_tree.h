8fb7a1e8629435e82bb6860823799f7830b3be9f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-04-13 07:40:51 -0400

    functions can only be instantiated when they are defined ;-)
    SVN-Revision: 24701

--- contrib/brl/bseg/boct/boct_tree.h
+++ contrib/brl/bseg/boct/boct_tree.h
@@ -1,6 +1,7 @@
 #ifndef boct_tree_h_
 #define boct_tree_h_
-
+//:
+// \file
 #include "boct_tree_cell.h"
 #include "boct_loc_code.h"
 
@@ -9,11 +10,12 @@
 #include <vgl/vgl_box_3d.h>
 
 template <class T_loc, class T_data, class T_aux>
-class boct_tree {
-public:
+class boct_tree
+{
+ public:
   boct_tree(): root_(0),global_bbox_(){}
   boct_tree(short max_level, short init_levels=1);
-  ~boct_tree(); 
+  ~boct_tree();
   boct_tree_cell<T_loc,T_data,T_aux>* locate_point(const vgl_point_3d<double>& p);
   boct_tree_cell<T_loc,T_data,T_aux>* locate_point_at_level(const vgl_point_3d<double>& p, short level);
   boct_tree_cell<T_loc,T_data,T_aux>* locate_region(const vgl_box_3d<double>& r);
@@ -30,20 +32,19 @@ public:
   void b_write(vsl_b_ostream & os);
   void b_read(vsl_b_istream & is);
   short version_no() { return 1; }
-private:
+ private:
   short max_level_;
   boct_tree_cell<T_loc,T_data,T_aux>* root_;
 
   //: the local coordinates are from 0 to 1
-  //: thats why need the global coordinates
+  // that's why we need the global coordinates
   vgl_box_3d<double> global_bbox_;
-
 };
 
-template <class T_loc,class T_data,class T_aux>
-void vsl_b_write(vsl_b_ostream & os, boct_tree<T_loc,T_data,T_aux>& tree);
+template <class T_loc,class T_data, class T_aux>
+void vsl_b_write(vsl_b_ostream & os, boct_tree<T_loc,T_data,T_aux>& tree) { tree.b_write(os); }
 
-template <class T_loc,class T_data,class T_aux>
-void vsl_b_read(vsl_b_istream & is, boct_tree<T_loc,T_data,T_aux>& tree);
+template <class T_loc,class T_data, class T_aux>
+void vsl_b_read(vsl_b_istream & is, boct_tree<T_loc,T_data,T_aux>& tree) { tree.b_read(is); }
 
 #endif
