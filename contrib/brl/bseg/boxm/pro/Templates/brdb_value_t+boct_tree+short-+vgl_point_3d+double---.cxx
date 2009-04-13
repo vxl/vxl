6f8e46f01e5f51a605db15bd183b5c7576566ad2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-04-13 04:36:30 -0400

    renamed template instantiation files (canonic file name)
    SVN-Revision: 24683

--- contrib/brl/bseg/boxm/pro/Templates/brdb_value_t+boct_tree+short-+vgl_point_3d+double---.cxx
+++
@@ -1,6 +0,0 @@
-#include <brdb/brdb_value.txx>
-
-#include <vbl/io/vbl_io_smart_ptr.h>
-#include <boxm/boxm_scene.h>
-
-BRDB_VALUE_INSTANTIATE(boxm_scene_short_point_double_sptr, "boxm_scene_short_point_double_sptr");
