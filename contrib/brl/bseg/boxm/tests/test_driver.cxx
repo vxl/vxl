70463136738c2e23d8e6860142ec9498d2e74e88
Vishal Jain <jainv@users.sourceforge.net>
2009-04-13 14:25:17 -0400

    added test for checking visibility graph for cells
    SVN-Revision: 24718

--- contrib/brl/bseg/boxm/tests/test_driver.cxx
+++ contrib/brl/bseg/boxm/tests/test_driver.cxx
@@ -3,12 +3,15 @@
 DECLARE( test_binary_io );
 DECLARE( test_block_iter );
 DECLARE( test_block_vis_graph );
+DECLARE( test_cell_vis_graph );
 
 void register_tests()
 {
   REGISTER( test_binary_io );
   REGISTER( test_block_iter );
   REGISTER( test_block_vis_graph );
+    REGISTER( test_cell_vis_graph );
+
 }
 
 
