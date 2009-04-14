71d1a1184952850e3aaf51b6d40d82936c152ace
Vishal Jain <jainv@users.sourceforge.net>
2009-04-14 15:09:05 -0400

    corrected the errors in the template file
    SVN-Revision: 24747

--- contrib/brl/bseg/boxm/Templates/boct_tree_cell+short.boxm_sample+BOXM_APM_MOG_GREY--.cxx
+++ contrib/brl/bseg/boxm/Templates/boct_tree_cell+short.boxm_sample+BOXM_APM_MOG_GREY--.cxx
@@ -2,4 +2,5 @@
 #include "boxm/boxm_apm_traits.h"
 #include "boxm/boxm_sample.h"
 
-BOCT_TREE_CELL_INSTANTIATE(short, boxm_sample<BOXM_APM_MOG_GREY>);
+typedef boxm_sample<BOXM_APM_MOG_GREY> boxm_sample_mog_grey;
+BOCT_TREE_CELL_INSTANTIATE(short,boxm_sample_mog_grey );
--- contrib/brl/bseg/boxm/boxm_render_image.h
+++ contrib/brl/bseg/boxm/boxm_render_image.h
@@ -18,23 +18,35 @@
 
 
 template <class T_loc, boxm_apm_type APM>
-void boxm_render_image_splatting(boxm_scene<T_loc, boxm_sample<APM> > &scene, vpgl_camera_double_sptr cam, 
+void boxm_render_image_splatting(boxm_scene<boct_tree<T_loc, boxm_sample<APM>> > &scene, vpgl_camera_double_sptr cam, 
 					   vil_image_view<typename boxm_apm_traits<APM>::obs_datatype> &expected, 
 					   vil_image_view<float> &mask, 
 					   bool use_black_background = false)
 {
-  //: code to iterate over the blocks in order of visibility 
-  boxm_block_vis_graph_iterator<tree_type> block_vis_iter(cam, &scene, expected.ni(), expected.nj());
+	unsigned ni=expected.ni();
+	unsigned nj=expected.nj();
+	//: code to iterate over the blocks in order of visibility 
+	boxm_block_vis_graph_iterator<tree_type> block_vis_iter(cam, &scene, expected.ni(), expected.nj());
+	while (block_vis_iter.next()) {
+		vcl_vector<vgl_point_3d<int> > block_indices = block_vis_iter.frontier_indices();
+		for(unsigned i=0; i<block_indices.size(); i++) {
+			//: code for each block
+			scene.load_block(block_indices[i]);
+			boxm_block<boct_tree<T_loc,boxm_sample<APM> > > * curr_block=scene.get_active_block();
+			boxm_cell_vis_graph_iterator<T_loc, boxm_sample<APM> > frontier_it(cam,curr_block->get_tree(),ni,nj);	
+			//: for each frontier layer of each block
+			while (frontier_it.next()){
+				vcl_vector<boct_tree_cell<T_loc, boxm_sample<APM> > *> vis_cells=cell_iterator.frontier();
+				for(unsigned i=0;i<vis_cells.size();i++)
+				{
+					//: for each cell
 
-  vcl_vector<boxm_block<tree_type>*> blocks;
-  while (block_vis_iter.next()) {
-    vcl_cout << "Frontier\n";
-    blocks = block_vis_iter.frontier();
-    for(unsigned i=0; i<blocks.size(); i++) {
-      vcl_cout << i << "- " << blocks[i]->bounding_box() << vcl_endl; 
-    }
-  }
-  return;
+				}
+
+			}
+		}
+	}
+	return;
 }
 
 
