6a1dde7b0f6664b7708680ab8973ad601de8ff5a
Vishal Jain <jainv@users.sourceforge.net>
2009-04-15 08:34:54 -0400

    corrected the errors in the template file
    SVN-Revision: 24756

--- contrib/brl/bseg/boxm/Templates/boxm_block+boct_tree+short.boxm_sample+BOXM_APM_MOG_GREY---.cxx
+++ contrib/brl/bseg/boxm/Templates/boxm_block+boct_tree+short.boxm_sample+BOXM_APM_MOG_GREY---.cxx
@@ -1,8 +1,7 @@
 #include <boxm/boxm_block.txx>
 #include <boxm/boxm_apm_traits.h>
 #include <boxm/boxm_sample.h>
-#include <boct/boct_tree.txx>
-#include <vgl/io/vgl_io_point_3d.h>
+#include <boct/boct_tree.h>
 
-typedef boct_tree<short, boxm_sample<BOXM_APM_MOG_GREY> >  tree_type;
+typedef boct_tree<short,boxm_sample<BOXM_APM_MOG_GREY> >  tree_type;
 BOXM_BLOCK_INSTANTIATE(tree_type);
