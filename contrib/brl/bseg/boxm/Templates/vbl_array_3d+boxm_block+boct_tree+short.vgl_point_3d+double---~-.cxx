aacc427f8c3608ccca7520a3507c8fe0b6d2ac48
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-04-15 02:36:22 -0400

    ">>" should be "> >" within a nested template argument list
    SVN-Revision: 24751

--- contrib/brl/bseg/boxm/Templates/vbl_array_3d+boxm_block+boct_tree+short.vgl_point_3d+double---~-.cxx
+++ contrib/brl/bseg/boxm/Templates/vbl_array_3d+boxm_block+boct_tree+short.vgl_point_3d+double---~-.cxx
@@ -4,5 +4,5 @@
 #include <boxm/boxm_block.txx>
 #include <boct/boct_tree.txx>
 
-typedef boct_tree<short, vgl_point_3d<double>> tree_type;
+typedef boct_tree<short, vgl_point_3d<double> > tree_type;
 VBL_ARRAY_3D_INSTANTIATE(boxm_block<tree_type>*);
