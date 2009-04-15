cee14d5480ee1d2cb7b1035ba3f83dc48ce58189
Gamze Tunali <gamze@users.sourceforge.net>
2009-04-15 10:48:42 -0400

    unused template intsantiations removed
    SVN-Revision: 24760

--- contrib/brl/bseg/boxm/Templates/vbl_smart_ptr+boxm_scene+boct_tree+short.boxm_sample+BOXM_APM_MOG_GREY----.cxx
+++
@@ -1,6 +0,0 @@
-#include <boxm/boxm_scene.h>
-#include <boxm/boxm_sample.h>
-#include <vbl/vbl_smart_ptr.txx>
-
-typedef boxm_scene<boct_tree<short,boxm_sample<BOXM_APM_MOG_GREY> > > boxm_scene_short_sample_mog_grey;
-VBL_SMART_PTR_INSTANTIATE(boxm_scene_short_sample_mog_grey);
