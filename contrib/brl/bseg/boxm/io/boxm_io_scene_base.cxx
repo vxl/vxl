ad8126ec3a892239e28c975ee6ce8e86245ac882
Gamze Tunali <gamze@users.sourceforge.net>
2009-04-15 10:49:41 -0400

    io added fro boxm_scene_base
    SVN-Revision: 24761

---
+++ contrib/brl/bseg/boxm/io/boxm_io_scene_base.cxx
@@ -0,0 +1,24 @@
+#include "boxm_io_scene_base.h"
+
+//: Binary write boxm scene to stream
+void vsl_b_write(vsl_b_ostream & os, boxm_scene_base_sptr const& scene_base)
+{
+}
+
+
+//: Binary load boxm scene from stream.
+void vsl_b_read(vsl_b_istream & is, boxm_scene_base_sptr &scene_base)
+{
+}
+
+
+//: Binary write boxm scene to stream
+void vsl_b_write(vsl_b_ostream & os, boxm_scene_base const& scene_base)
+{
+}
+
+
+//: Binary load boxm scene from stream.
+void vsl_b_read(vsl_b_istream & is, boxm_scene_base &scene_base)
+{
+}
