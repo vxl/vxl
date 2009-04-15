fdc20015e0e9f8699d7b681c0bf5b2d793fa60b3
Gamze Tunali <gamze@users.sourceforge.net>
2009-04-15 15:22:21 -0400

    implementing class boxm_apm_types
    SVN-Revision: 24771

---
+++ contrib/brl/bseg/boxm/boxm_apm_traits.cxx
@@ -0,0 +1,12 @@
+#include "boxm_apm_traits.h"
+
+const char* boxm_apm_types::app_model_strings[]  = { "apm_mog_grey", "apm_mog_rgb", "apm_simple_grey", "apm_simple_rgb"};
+
+boxm_apm_type boxm_apm_types::str_to_enum(const char* s)
+{
+  for (int i=0; i < boxm_apm_type::BOXM_APM_UNKNOWN; i++)
+    if (vcl_strcmp(s, boxm_apm_types::app_model_strings[i]) == 0)
+      return (boxm_apm_type) i;
+  return boxm_apm_type::BOXM_APM_UNKNOWN;
+}
+
