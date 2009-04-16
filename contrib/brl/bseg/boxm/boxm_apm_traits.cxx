c378988826501ae7293bf5687875eb96ce1eff3b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-04-16 17:47:08 -0400

    SVN-Revision: 24788

--- contrib/brl/bseg/boxm/boxm_apm_traits.cxx
+++ contrib/brl/bseg/boxm/boxm_apm_traits.cxx
@@ -4,9 +4,9 @@ const char* boxm_apm_types::app_model_strings[]  = { "apm_mog_grey", "apm_mog_rg
 
 boxm_apm_type boxm_apm_types::str_to_enum(const char* s)
 {
-  for (int i=0; i < boxm_apm_types::BOXM_APM_UNKNOWN; i++)
+  for (int i=0; i < int(boxm_apm_type::BOXM_APM_UNKNOWN); i++)
     if (vcl_strcmp(s, boxm_apm_types::app_model_strings[i]) == 0)
       return (boxm_apm_type) i;
-  return boxm_apm_types::BOXM_APM_UNKNOWN;
+  return boxm_apm_type::BOXM_APM_UNKNOWN;
 }
 
