4adf1b2937cf20aea699b0fb18675dfda8a97570
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-04-16 18:01:02 -0400

    Placed Doxygen brief comment on a single line
    SVN-Revision: 24793

--- contrib/brl/bseg/boxm/boxm_block.h
+++ contrib/brl/bseg/boxm/boxm_block.h
@@ -1,41 +1,36 @@
 #ifndef boxm_block_h_
 #define boxm_block_h_
-
 //:
 // \file
-// \brief  a block is a smallest area that is going to be processed
-//         with an octree
+// \brief  a block is a smallest area that is going to be processed with an octree
 //
 // \author Gamze Tunali
-// \date 04/01/2009
+// \date Apr 01, 2009
 // \verbatim
 //  Modifications
-//  
+//   <none yet>
 // \endverbatim
-
 
 #include <vpgl/vpgl_camera.h>
 #include <vgl/vgl_box_3d.h>
 #include <boct/boct_tree.h>
 
-
 template <class T>
 class boxm_block
 {
-public:
+ public:
   boxm_block(): octree_(0) {}
-  ~boxm_block(){}
+  ~boxm_block() {}
   boxm_block(vgl_box_3d<double> bbox): bbox_(bbox), octree_(0) {}
   boxm_block(vgl_box_3d<double> bbox, T* tree) : bbox_(bbox), octree_(tree) {}
   void init_tree(T * octree);
-  //void set_tree(T * octree){ octree_=octree;}
   vgl_box_3d<double> bounding_box() { return bbox_; }
   T* get_tree(){return octree_;}
   void b_read(vsl_b_istream &s);
   void b_write(vsl_b_ostream &s);
   short version_no() { return 1; }
 
-private:
+ private:
   vgl_box_3d<double> bbox_;
   T* octree_;
 };
--- contrib/brl/bseg/boxm/boxm_utils.h
+++ contrib/brl/bseg/boxm/boxm_utils.h
@@ -1,15 +1,14 @@
 #ifndef boxm_utils_h_
 #define boxm_utils_h_
-
 //:
 // \file
 // \brief  The utility methods for the boxm project
 //
 // \author Gamze Tunali
-// \date 04/07/2009
+// \date Apr 07, 2009
 // \verbatim
 //  Modifications
-//  
+//   <none yet>
 // \endverbatim
 
 #include <vgl/vgl_box_3d.h>
@@ -20,61 +19,56 @@
 #include <vgl/vgl_polygon_scan_iterator.h>
 #include <vil/vil_image_view.h>
 
-class boxm_utils {
+class boxm_utils
+{
+ public:
 
-public:
-
-    //: returns true if a given box is visible from the camera
-    // and the image plane
-  static bool is_visible(vgl_box_3d<double> const& bbox, 
-                    vpgl_camera_double_sptr const& camera, 
-                    unsigned int img_ni, unsigned int img_nj, 
-                    bool do_front_test = true);
-
+  //: returns true if a given box is visible from the camera and the image plane
+  static bool is_visible(vgl_box_3d<double> const& bbox,
+                         vpgl_camera_double_sptr const& camera,
+                         unsigned int img_ni, unsigned int img_nj,
+                         bool do_front_test = true);
 
   static vcl_vector<vgl_point_3d<double> > corners_of_box_3d(vgl_box_3d<double> box);
 
   static bool is_face_visible(vcl_vector<vgl_point_3d<double> > &face,
                               vpgl_camera_double_sptr const& camera);
 
-    //: returns the visible faces of a box given a camera. It puts
-    // a bit 1 for each face visible based on the boct_cell_face values.
-  static boct_face_idx visible_faces(vgl_box_3d<double> &bbox, 
-                                vpgl_camera_double_sptr camera);
+  //: returns the visible faces of a box given a camera.
+  // It puts a bit 1 for each face visible based on the boct_cell_face values.
+  static boct_face_idx visible_faces(vgl_box_3d<double> &bbox,
+                                     vpgl_camera_double_sptr camera);
 
-  static void faces_of_box_3d(vgl_box_3d<double> &bbox, 
-                            vcl_map<boct_face_idx, vcl_vector<vgl_point_3d<double> > > & faces);
+  static void faces_of_box_3d(vgl_box_3d<double> &bbox,
+                              vcl_map<boct_face_idx, vcl_vector<vgl_point_3d<double> > > & faces);
 
-  static void project_cube(vgl_box_3d<double> &bbox, 
-                              vpgl_camera_double_sptr camera,
-                              vcl_map<boct_face_idx, vcl_vector< vgl_point_3d<double> > > & faces,
-							  boct_face_idx & vis_face_ids);
+  static void project_cube(vgl_box_3d<double> &bbox,
+                           vpgl_camera_double_sptr camera,
+                           vcl_map<boct_face_idx, vcl_vector< vgl_point_3d<double> > > & faces,
+                           boct_face_idx & vis_face_ids);
   static vcl_vector<vgl_point_2d<double> >  project_face(vcl_vector<vgl_point_3d<double> > &face,
-														 vpgl_camera_double_sptr const& camera);
+                                                         vpgl_camera_double_sptr const& camera);
 
-  
-  static void quad_interpolate(vgl_polygon_scan_iterator<double> &poly_it, 
-								 double* xvals, double* yvals, double* vals, 
-								 vil_image_view<float> &img,unsigned img_plane_num=0,
-								 unsigned int v0=0, unsigned int v1=1, 
-								 unsigned int v2=2,unsigned int v3=3);
-  static void quad_fill(vgl_polygon_scan_iterator<double> &poly_it, 
-						 vil_image_view<float> &img, float val,
-						 unsigned img_plane_num=0);
+  static void quad_interpolate(vgl_polygon_scan_iterator<double> &poly_it,
+                               double* xvals, double* yvals, double* vals,
+                               vil_image_view<float> &img,unsigned img_plane_num=0,
+                               unsigned int v0=0, unsigned int v1=1,
+                               unsigned int v2=2,unsigned int v3=3);
+  static void quad_fill(vgl_polygon_scan_iterator<double> &poly_it,
+                        vil_image_view<float> &img, float val,
+                        unsigned img_plane_num=0);
 
   static bool project_cube_xyz(vcl_map<boct_face_idx,
-							   vcl_vector< vgl_point_3d<double> > > & faces,
-							   boct_face_idx & vis_face_ids,
-							   vil_image_view<float> &front_xyz,
-							   vil_image_view<float> &back_xyz,
-							   vpgl_camera_double_sptr camera);
+                               vcl_vector< vgl_point_3d<double> > > & faces,
+                               boct_face_idx & vis_face_ids,
+                               vil_image_view<float> &front_xyz,
+                               vil_image_view<float> &back_xyz,
+                               vpgl_camera_double_sptr camera);
 
   static bool project_cube_fill_val( vcl_map<boct_face_idx,vcl_vector< vgl_point_3d<double> > > & faces,
-								   boct_face_idx & vis_face_ids,
-								   vil_image_view<float> &fill_img,
-								   float val, vpgl_camera_double_sptr cam);
-
-
+                                     boct_face_idx & vis_face_ids,
+                                     vil_image_view<float> &fill_img,
+                                     float val, vpgl_camera_double_sptr cam);
 };
 
 #endif
