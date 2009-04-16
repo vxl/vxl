be5cce6c79cd271a6c7f916036bdf47ca704a762
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-04-16 18:03:17 -0400

    added EOLN at EOF
    SVN-Revision: 24795

--- contrib/brl/bseg/boxm/boxm_mog_grey_processor.cxx
+++ contrib/brl/bseg/boxm/boxm_mog_grey_processor.cxx
@@ -8,13 +8,12 @@
 #include <bsta/bsta_mixture_fixed.h>
 #include <bsta/bsta_gaussian_indep.h>
 
-
 #include <vcl_cassert.h>
+
 //: Expected value
-boxm_apm_traits<BOXM_APM_MOG_GREY>::obs_datatype 
+boxm_apm_traits<BOXM_APM_MOG_GREY>::obs_datatype
 boxm_mog_grey_processor::expected_color(boxm_apm_traits<BOXM_APM_MOG_GREY>::apm_datatype const& appear)
 {
-
   float total_weight= 0;
   obs_datatype c = 0;
   obs_datatype expected = 0;
