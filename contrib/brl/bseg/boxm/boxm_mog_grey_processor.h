7816096a2ca4558b852729e668a81d933346b6db
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-04-24 08:40:16 -0400

    "This is" line corrected
    SVN-Revision: 24887

--- contrib/brl/bseg/boxm/boxm_mog_grey_processor.h
+++ contrib/brl/bseg/boxm/boxm_mog_grey_processor.h
@@ -1,4 +1,4 @@
-// This is vxl/contrib/brl/bbas/boxm/boxm_mog_grey_processor.h
+// This is brl/bseg/boxm/boxm_mog_grey_processor.h
 #ifndef boxm_mog_grey_processor_h_
 #define boxm_mog_grey_processor_h_
 //:
@@ -6,12 +6,11 @@
 // \brief A class for a grey-scale-mixture-of-gaussian processor
 //
 // \author Gamze Tunali
-// \date   04/03/2009
+// \date   Apr 03, 2009
 // \verbatim
 //  Modifications
-//  
+//   <none yet>
 // \endverbatim
-
 
 #include <bsta/algo/bsta_adaptive_updater.h>
 #include <bsta/bsta_distribution.h>
@@ -20,28 +19,23 @@
 #include <bsta/bsta_mixture_fixed.h>
 #include <bsta/bsta_gaussian_indep.h>
 
-
 #include "boxm_apm_traits.h"
 
 
 class  boxm_mog_grey_processor
 {
-protected:
+ protected:
   // convienance typedefs
   typedef boxm_apm_traits<BOXM_APM_MOG_GREY>::apm_datatype apm_datatype;
   typedef boxm_apm_traits<BOXM_APM_MOG_GREY>::obs_datatype obs_datatype;
   typedef boxm_apm_traits<BOXM_APM_MOG_GREY>::obs_mathtype obs_mathtype;
 
-public:
+ public:
   static obs_datatype expected_color(apm_datatype const& appear);
 
   static float prob_density(apm_datatype const& appear, obs_datatype const& obs);
 
   static bool update( apm_datatype &appear, obs_datatype const& obs, float const& weight);
-
-private:
-
-
 };
 
 #endif // boxm_mog_grey_processor_h_
