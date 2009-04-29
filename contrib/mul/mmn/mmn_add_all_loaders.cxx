4f7621146bfb161349fe0cba0f5bdfc8c4f9604a
Martin G. Roberts <mgroberts@users.sourceforge.net>
2009-04-29 06:35:56 -0400

    Added mmn_lbp_solver (now derived from mmn_solver)
    SVN-Revision: 24935

--- contrib/mul/mmn/mmn_add_all_loaders.cxx
+++ contrib/mul/mmn/mmn_add_all_loaders.cxx
@@ -6,6 +6,7 @@
 
 #include <mbl/mbl_cloneables_factory.h>
 #include "mmn_dp_solver.h"
+#include "mmn_lbp_solver.h"
 
 //: Add all binary loaders and factory objects for mmn library
 void mmn_add_all_loaders()
@@ -13,4 +14,7 @@ void mmn_add_all_loaders()
 
   mbl_cloneables_factory<mmn_solver>::add(mmn_dp_solver());
   vsl_add_to_binary_loader(mmn_dp_solver());
+
+  mbl_cloneables_factory<mmn_solver>::add(mmn_lbp_solver());
+  vsl_add_to_binary_loader(mmn_lbp_solver());
 }
