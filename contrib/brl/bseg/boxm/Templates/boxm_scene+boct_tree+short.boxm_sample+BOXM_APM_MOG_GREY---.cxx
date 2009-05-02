10fe89fac040f983ce4c145e4a110970419cdb49
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-05-02 10:10:08 -0400

    avoid linker error (duplicate symbol)
    SVN-Revision: 25005

--- contrib/brl/bseg/boxm/Templates/boxm_scene+boct_tree+short.boxm_sample+BOXM_APM_MOG_GREY---.cxx
+++ contrib/brl/bseg/boxm/Templates/boxm_scene+boct_tree+short.boxm_sample+BOXM_APM_MOG_GREY---.cxx
@@ -1,5 +1,5 @@
 #include <boxm/boxm_scene.txx>
-#include <boct/boct_tree.txx>
+#include <boct/boct_tree.h>
 #include <boxm/boxm_sample.h>
 
 typedef boct_tree<short, boxm_sample<BOXM_APM_MOG_GREY> >  tree_type;
