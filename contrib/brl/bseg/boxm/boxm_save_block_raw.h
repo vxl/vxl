3eeb7a6e3ab5c84bea4cc603ed941d595c13cf74
Gamze Tunali <gamze@users.sourceforge.net>
2009-05-06 14:50:31 -0400

    made some update for efficiency
    SVN-Revision: 25038

--- contrib/brl/bseg/boxm/boxm_save_block_raw.h
+++ contrib/brl/bseg/boxm/boxm_save_block_raw.h
@@ -29,16 +29,14 @@ void boxm_save_block_raw(boxm_scene<boct_tree<T_loc, boxm_sample<APM> > > &scene
 {
   typedef boct_tree<T_loc, boxm_sample<APM> > tree_type;
 
-  vcl_ofstream os(filename.c_str(),vcl_ios::binary);
-  if (!os.good()) {
-    vcl_cerr << "error opening " << filename << " for write!\n";
-    return;
-  }
-
   scene.load_block(block_idx);
   boxm_block<tree_type>* block = scene.get_block(block_idx);
   vgl_box_3d<double> block_bb = block->bounding_box();
   tree_type* tree = block->get_tree();
+
+  // query the finest level of the tree and do not make the resolution smaller than that
+  if (tree->finest_level() > resolution_level)
+    resolution_level = tree->finest_level();
 
   // get origin of block
   vgl_point_3d<double> min = block_bb.min_point();
@@ -50,8 +48,10 @@ void boxm_save_block_raw(boxm_scene<boct_tree<T_loc, boxm_sample<APM> > > &scene
   // origin should specify center of first cell
   vgl_point_3d<double> data_og(min.x() + (step_len/2.0), min.y() + (step_len/2.0), min.z() + (step_len/2.0));
 
+  int data_size = ncells*ncells*ncells;
+  int y_size = ncells*ncells;
   float *data = 0;
-  data = new (std::nothrow)float[ncells*ncells*ncells];
+  data = new (std::nothrow)float[data_size];
 
   if (data == 0) {
     vcl_cout << "boxm_save_block_raw: Could not allocate data!" << vcl_endl;
@@ -76,8 +76,11 @@ void boxm_save_block_raw(boxm_scene<boct_tree<T_loc, boxm_sample<APM> > > &scene
 
     if (level == resolution_level) {
       // just copy value to output array
-      unsigned int out_index = static_cast<unsigned int>(node.x()*ncells*ncells + node.y()*ncells + node.z());
-      data[out_index] = cell_val;
+      int out_index = static_cast<int>((node.x()/step_len)*ncells*ncells + (node.y()/step_len)*ncells + (node.z()/step_len));
+      if (out_index >= data_size)
+        vcl_cout << "ERROR 1 index=" << out_index << " -- " << data_size << vcl_endl;
+      else 
+        data[out_index] = cell_val;
     }
     else if (level > resolution_level) {
       // cell is bigger than output cells.  copy value to all contained output cells.
@@ -88,8 +91,11 @@ void boxm_save_block_raw(boxm_scene<boct_tree<T_loc, boxm_sample<APM> > > &scene
       for (unsigned int z=node_z_start; z<node_z_start+us_factor; ++z) {
         for (unsigned int y=node_y_start; y<node_y_start+us_factor; ++y) {
           for (unsigned int x=node_x_start; x<node_x_start+us_factor; ++x) {
-            unsigned int out_index = z*ncells*ncells + y*ncells + x;
-            data[out_index] = cell_val;
+            int out_index = z*y_size + y*ncells + x;
+            if (out_index >= data_size)
+              vcl_cout << "ERROR 2 index=" << out_index << " -- " << data_size << vcl_endl;
+            else 
+              data[out_index] = cell_val;
           }
         }
       }
@@ -98,21 +104,21 @@ void boxm_save_block_raw(boxm_scene<boct_tree<T_loc, boxm_sample<APM> > > &scene
       // cell is smaller than output cells. increment output cell value
       unsigned int ds_factor = 1 << (level - resolution_level);
       double update_weight = 1.0 / double(ds_factor*ds_factor*ds_factor);
-      unsigned int out_index = static_cast<unsigned int>((node.z()/ds_factor)*ncells*ncells + (node.y()/ds_factor)*ncells + (node.x()/ds_factor));
+      unsigned int out_index = static_cast<unsigned int>((node.z()/ds_factor)*y_size + (node.y()/ds_factor)*ncells + (node.x()/ds_factor));
       data[out_index] += float(cell_val*update_weight);
     }
   }
   // convert float values to char
 
   unsigned char *byte_data = 0;
-  byte_data = new (std::nothrow) unsigned char[ncells*ncells*ncells];
+  byte_data = new (std::nothrow) unsigned char[data_size];
   if (byte_data == 0) {
     vcl_cout << "boxm_save_block_raw: Could not allocate byte data!" << vcl_endl;
     return;
   }
 
   float* dp = data;
-  for (unsigned char* bdp = byte_data; dp < data + ncells*ncells*ncells; ++dp, ++bdp) {
+  for (unsigned char* bdp = byte_data; dp < data + data_size; ++dp, ++bdp) {
     double P = 1.0 - vcl_exp(-*dp*step_len);
     *bdp = (unsigned char)(vcl_floor((255.0 * P) + 0.5)); // always positive so this is an ok way to round
   }
@@ -124,13 +130,19 @@ void boxm_save_block_raw(boxm_scene<boct_tree<T_loc, boxm_sample<APM> > > &scene
   vxl_uint_32 nx = ncells;
   vxl_uint_32 ny = ncells;
   vxl_uint_32 nz = ncells;
+  
+  vcl_ofstream os(filename.c_str(),vcl_ios::binary);
+  if (!os.good()) {
+    vcl_cerr << "error opening " << filename << " for write!\n";
+    return;
+  }
 
   os.write(reinterpret_cast<char*>(&data_type),sizeof(data_type));
   os.write(reinterpret_cast<char*>(&nx),sizeof(nx));
   os.write(reinterpret_cast<char*>(&ny),sizeof(ny));
   os.write(reinterpret_cast<char*>(&nz),sizeof(nz));
 
-  os.write((char*)byte_data,ncells*ncells*ncells);
+  os.write((char*)byte_data,data_size);
   delete[] byte_data;
 
   return;
