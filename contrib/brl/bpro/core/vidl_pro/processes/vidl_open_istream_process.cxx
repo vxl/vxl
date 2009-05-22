e2e0ef83ff646bddbe5288614d1f7fca37f79cb1
Joseph L. Mundy <mundyj@users.sourceforge.net>
2009-05-22 16:37:43 -0400

    fixed problem with istream open
    SVN-Revision: 25141

--- contrib/brl/bpro/core/vidl_pro/processes/vidl_open_istream_process.cxx
+++ contrib/brl/bpro/core/vidl_pro/processes/vidl_open_istream_process.cxx
@@ -2,8 +2,12 @@
 #include <bprb/bprb_func_process.h>
 //:
 // \file
-
+#include <vul/vul_file.h>
 #include <vidl/vidl_image_list_istream.h>
+#include <vidl/vidl_config.h>
+#if VIDL_HAS_DSHOW
+#include <vidl/vidl_dshow_file_istream.h>
+#endif
 #include <vidl/vidl_istream_sptr.h>
 #include <bprb/bprb_parameters.h>
 #include <vcl_iostream.h>
@@ -26,20 +30,30 @@ bool vidl_open_istream_process_cons(bprb_func_process& pro)
 }
 
 //: Execute the process
-// NOTE! currently only implemented for image list istream
 bool vidl_open_istream_process(bprb_func_process& pro)
 {
-  if (pro.n_inputs()< 2) {
-    vcl_cout << "vidl_open_ostream_process: The input number should be 2" << vcl_endl;
+  if (pro.n_inputs()!=1) {
+    vcl_cout << "vidl_open_ostream_process: The input number should be 1" << vcl_endl;
     return false;
   }
- 
-  unsigned i=0;
   //Retrieve filename from input
-  vcl_string istream_filename = pro.get_input<vcl_string>(i++);
-
-  vidl_istream_sptr vistr = new vidl_image_list_istream(istream_filename);
-  if (!vistr->is_open()){
+  vcl_string istream_filename = pro.get_input<vcl_string>(0);
+  bool glob = false;
+  if(vul_file::is_directory(istream_filename)) glob = true;
+  else
+    for(vcl_string::const_iterator cit = istream_filename.begin(); 
+      cit != istream_filename.end()&&!glob; ++cit)
+      if(*cit=='*')
+        glob = true;
+  vidl_istream_sptr vistr;
+  //if glob open an image list istream
+  if(glob)
+    vistr = new vidl_image_list_istream(istream_filename);
+  //otherwise if DSHOW is present, open a dshow stream
+#if VIDL_HAS_DSHOW
+  else{  vistr = new vidl_dshow_file_istream(istream_filename);}
+#endif
+  if (!vistr || !vistr->is_open()){
     vcl_cerr << "In vidl_open_istream_process::execute()"
              << " - could not open" << istream_filename << vcl_endl;
     return false;
