75b8a083f022b4926d4734c7bbdeb9cbdbeea22e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-05-29 03:01:10 -0400

    avoid compiler warning (unused variable "max_scale")
    SVN-Revision: 25162

--- contrib/brl/bseg/bvxm/pro/processes/bvxm_locate_region_process.cxx
+++ contrib/brl/bseg/bvxm/pro/processes/bvxm_locate_region_process.cxx
@@ -53,22 +53,18 @@ bool bvxm_locate_region_process_cons(bprb_func_process& pro)
   input_types_[4] = "vcl_string";
   input_types_[5] = "vcl_string";
 
-  if(!pro.set_input_types(input_types_))
+  if (!pro.set_input_types(input_types_))
     return false;
 
   //output has 0 output
 
-  
   return true;
-
-
 }
 
 bool bvxm_locate_region_process(bprb_func_process& pro)
 {
-
   using namespace bvxm_locate_region_process_globals;
-  
+
   if (pro.n_inputs() < n_inputs_)
   {
     vcl_cout << pro.name() << " The input number should be " << n_inputs_<< vcl_endl;
@@ -78,54 +74,46 @@ bool bvxm_locate_region_process(bprb_func_process& pro)
   //get inputs:
   unsigned i = 0;
   vil_image_view_base_sptr img = pro.get_input<vil_image_view_base_sptr>(i++);
+  if ( !img ){
+      vcl_cout << pro.name() << " :-- Input " << (i-1) << " is not valid!\n";
+      return false;
+  }
   vil_image_view_base_sptr mask = pro.get_input<vil_image_view_base_sptr>(i++);
 
   //vil_image_view<float> mask = *(vil_convert_cast(float(),mask_base));
 
   //vil_save(mask,"./binary_mask");
   vpgl_camera_double_sptr camera = pro.get_input<vpgl_camera_double_sptr>(i++);
+  if ( !camera ){
+      vcl_cout << pro.name() << " :-- Input " << (i-1) << " is not valid!\n";
+      return false;
+  }
   bvxm_voxel_world_sptr world = pro.get_input<bvxm_voxel_world_sptr>(i++);
+  if ( !world ){
+      vcl_cout << pro.name() << " :-- Input " << (i-1) << " is not valid!\n";
+      return false;
+  }
   vcl_string output_path = pro.get_input<vcl_string>(i++);
   vcl_string voxel_type = pro.get_input<vcl_string>(i++);
   unsigned bin_index = 0;
   unsigned scale = 0;
 
-  //check input's validity
-  i = 0;
-  if ( !img ){
-      vcl_cout << pro.name() << " :-- Input " << i++ << " is not valid!\n";
-      return false;
-  }
-
-  if ( !camera ){
-      vcl_cout << pro.name() << " :-- Input " << i++ << " is not valid!\n";
-      return false;
-  }
-
-  if ( !world ){
-      vcl_cout << pro.name() << " :-- Input " << i++ << " is not valid!\n";
-      return false;
-  }
-
-
   //create metadata:
   bvxm_image_metadata observation(img,camera);
-  unsigned max_scale = world->get_params()->max_scale();
 
   //create output_grid:
   vgl_vector_3d<unsigned int> grid_size = world->get_params()->num_voxels(scale);
   bvxm_voxel_grid_base_sptr grid_out = new bvxm_voxel_grid<float>(output_path, grid_size);
-  
+
 
   bool result = true;
 
   if (voxel_type == "apm_mog_grey")
     result = world->region_probability_density<APM_MOG_GREY>(observation,mask,grid_out, bin_index,scale);
-  else 
+  else
     vcl_cerr << "Error in: bvxm_locate_region_processor: Unsuppported appereance model" << vcl_endl;
 
   vcl_cout.flush();
   return true;
-
 }
 
