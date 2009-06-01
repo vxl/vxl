efa44991b2a0ce28d05ffc58bafa40895ed742a5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-06-01 06:14:18 -0400

    avoid compiler warning: added EOLN at EOF
    SVN-Revision: 25173

--- contrib/brl/bseg/bvpl/tests/test_subgrid.cxx
+++ contrib/brl/bseg/bvpl/tests/test_subgrid.cxx
@@ -10,7 +10,7 @@
 static void test_subgrid()
 {
   // create the grid
-// we need temporary disk storage for this test.
+  // we need temporary disk storage for this test.
   vcl_string storage_fname("bvxm_voxel_grid_test_temp.vox");
   vcl_string storage_cached_fname("bvxm_voxel_grid_cached_test_temp.vox");
   // remove file if exists from previous test.
@@ -28,37 +28,34 @@ static void test_subgrid()
   bvxm_voxel_grid<float>* grid = new bvxm_voxel_grid<float>(storage_fname,grid_size); // disk storage;
   vcl_string test_name;
 
-  
-    // check num_observations
-    unsigned nobs = grid->num_observations();
+  // check num_observations
+  unsigned nobs = grid->num_observations();
 
-    // fill with test data
-    float init_val = 0.5f;
-    grid->initialize_data(init_val);
-    bool init_check = true;
-    bool write_read_check = true;
+  // fill with test data
+  float init_val = 0.5f;
+  grid->initialize_data(init_val);
+  bool init_check = true;
+  bool write_read_check = true;
 
+  grid->increment_observations();
 
-    grid->increment_observations();
-  
-    // read in each slice, check that init_val was set, and fill with new value
-    unsigned count = 0;
-    vcl_cout << "read/write: ";
-    bvxm_voxel_grid<float>::iterator slab_it;
-    for (slab_it = grid->begin(); slab_it != grid->end(); ++slab_it) {
-      vcl_cout << '.';
-      bvxm_voxel_slab<float>::iterator vit;
-      for (vit = slab_it->begin(); vit != slab_it->end(); vit++, count++) {
-        if (*vit != init_val) {
-          vcl_cerr << "error: read in value does not match init value! count = " << count << vcl_endl;
-          init_check = false;
-        }
-        // write new value
-        *vit = static_cast<float>(count);
+  // read in each slice, check that init_val was set, and fill with new value
+  unsigned count = 0;
+  vcl_cout << "read/write: ";
+  bvxm_voxel_grid<float>::iterator slab_it;
+  for (slab_it = grid->begin(); slab_it != grid->end(); ++slab_it) {
+    vcl_cout << '.';
+    bvxm_voxel_slab<float>::iterator vit;
+    for (vit = slab_it->begin(); vit != slab_it->end(); vit++, count++) {
+      if (*vit != init_val) {
+        vcl_cerr << "error: read in value does not match init value! count = " << count << '\n';
+        init_check = false;
       }
+      // write new value
+      *vit = static_cast<float>(count);
     }
-    vcl_cout << "done." << vcl_endl;
-
+  }
+  vcl_cout << "done." << vcl_endl;
 
   // get the subgrid iterator
   int kernel_x=11, kernel_y=11, kernel_z=11;
