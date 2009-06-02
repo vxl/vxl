af0edfb75690dc38243c2827de26b757b97f67c3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-06-02 14:25:49 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 25181

--- contrib/brl/bseg/bvpl/tests/test_include.cxx
+++ contrib/brl/bseg/bvpl/tests/test_include.cxx
@@ -1,5 +1,7 @@
+#include <bvpl/bvpl_edge2d_kernel.h>
+#include <bvpl/bvpl_kernel_base.h>
+#include <bvpl/bvpl_kernel_iterator.h>
 #include <bvpl/bvpl_subgrid_iterator.h>
 #include <bvpl/bvpl_voxel_subgrid.h>
 
 int main() { return 0; }
-#include <bvpl/bvpl_kernel_iterator.h>
