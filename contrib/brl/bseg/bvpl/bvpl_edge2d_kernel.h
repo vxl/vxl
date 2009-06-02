82ed64ddd8a28db1f9468f81f8b7259a7e96ab51
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-06-02 14:34:08 -0400

    SVN-Revision: 25182

--- contrib/brl/bseg/bvpl/bvpl_edge2d_kernel.h
+++ contrib/brl/bseg/bvpl/bvpl_edge2d_kernel.h
@@ -1,14 +1,11 @@
-// This is//Projects/vxl/src/contrib/brl/bseg/bvpl/bvpl_edge2d_kernel.h
+// This is brl/bseg/bvpl/bvpl_edge2d_kernel.h
 #ifndef bvpl_edge2d_kernel_h_
 #define bvpl_edge2d_kernel_h_
-//
 //:
 // \file
-// \brief 
-//
+// \brief
 // \author Isabel Restrepo mir@lems.brown.edu
-//
-// \date  5/29/09
+// \date  May 29, 2009
 //
 // \verbatim
 //  Modifications
@@ -17,24 +14,21 @@
 
 #include "bvpl_kernel_base.h"
 
-class bvpl_edge2d_kernel : public bvpl_kernel_base 
-{ 
-  
-public: 
+class bvpl_edge2d_kernel : public bvpl_kernel_base
+{
+ public:
   //:Default Constructors
   bvpl_edge2d_kernel();
   //:Constructor
   bvpl_edge2d_kernel(unsigned height, unsigned width, vnl_vector_fixed<double,3> const& rotations);
   unsigned height() {return height_;}
   unsigned width() {return width_;}
-  virtual vcl_string name() {return "edge2d";};
-  
-private:
- 
+  virtual vcl_string name() {return "edge2d";}
+
+ private:
   unsigned height_;
   unsigned width_;
   static const unsigned max_size_ = 15;
-  
 };
+
 #endif
-  
