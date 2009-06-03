387f209937bf957eba5a9530251f8f94211e62fc
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-06-03 11:46:23 -0400

    templated edge2d added save_raw
    SVN-Revision: 25195

--- contrib/brl/bseg/bvpl/bvpl_edge2d_kernel.h
+++ contrib/brl/bseg/bvpl/bvpl_edge2d_kernel.h
@@ -1,11 +1,14 @@
-// This is brl/bseg/bvpl/bvpl_edge2d_kernel.h
+// This is//Projects/vxl/src/contrib/brl/bseg/bvpl/bvpl_edge2d_kernel.h
 #ifndef bvpl_edge2d_kernel_h_
 #define bvpl_edge2d_kernel_h_
+//
 //:
 // \file
-// \brief
+// \brief 
+//
 // \author Isabel Restrepo mir@lems.brown.edu
-// \date  May 29, 2009
+//
+// \date  5/29/09
 //
 // \verbatim
 //  Modifications
@@ -13,22 +16,123 @@
 // \endverbatim
 
 #include "bvpl_kernel_base.h"
+#include <vnl/vnl_matrix_fixed.h>
+#include <vnl/vnl_rotation_matrix.h>
+#include <vnl/vnl_vector_fixed.h>
+#include <vcl_iostream.h>
 
-class bvpl_edge2d_kernel : public bvpl_kernel_base
+
+enum edge2d_type {
+  SYMMETRIC =0,
+  UNKNOWN
+};
+
+
+
+template <edge2d_type> 
+class bvpl_edge2d_kernel;
+
+//Template specialization of 2d_edge kernels
+template <>
+class bvpl_edge2d_kernel<SYMMETRIC> : public bvpl_kernel_base 
 {
- public:
+
+public: 
   //:Default Constructors
   bvpl_edge2d_kernel();
-  //:Constructor
-  bvpl_edge2d_kernel(unsigned height, unsigned width, vnl_vector_fixed<double,3> const& rotations);
+  //:Creates a 2d edge kernel of specified type
+  bool create(unsigned height, unsigned width, vnl_vector_fixed<double,3> const& rotations);
   unsigned height() {return height_;}
   unsigned width() {return width_;}
-  virtual vcl_string name() {return "edge2d";}
-
- private:
+  virtual vcl_string name() {return "edge2d";};
+  
+private:
+ 
   unsigned height_;
   unsigned width_;
   static const unsigned max_size_ = 15;
+  
 };
 
+//:Default Constructor
+bvpl_edge2d_kernel<SYMMETRIC>::bvpl_edge2d_kernel()
+{
+  height_=0;
+  width_=0;
+  R_.fill(0.0);
+  angular_resolution_x_=0;
+  angular_resolution_y_=0;
+  angular_resolution_z_=0;
+}
+
+bool
+bvpl_edge2d_kernel<SYMMETRIC>::create(unsigned height, unsigned width,vnl_vector_fixed<double,3> const& rotations)
+{
+  
+  if( width % 2)
+  {
+    vcl_cerr<< "Warning, height of kernel is even. It has been increased by one"<< vcl_endl;
+    height = height++;
+  }
+  if(height % 2)
+  {
+    vcl_cerr<< "Warning, width of kernel is even. It has been increased by one"<< vcl_endl;
+    width =width++;
+  }
+  //The size of the kernel is limited. If widht or height of the kernel is too large,
+  //the user should subsample the image/grid
+  if(height > max_size_)
+  {
+    vcl_cerr<< "Warning, height of kernel is too large. It has been set to is max value of" << max_size_ << vcl_endl;
+    height = max_size_;
+  }
+  if(width > max_size_)
+  {
+    vcl_cerr<< "Warning, weight of kernel is too large. It  has been set to is max value of" << max_size_ << vcl_endl;
+    width = max_size_;
+  }
+  
+  
+  height_=height;
+  width_ = width;
+  
+  //create a basic centered edge with 1, 0, -1, if width is odd
+  // 1 1 0 -1 -1
+  // 1 1 0 -1 -1
+  // 1 1 0 -1 -1
+  // 1 1 0 -1 -1
+  
+  typedef vgl_point_3d<int> point_3d;
+  typedef bvpl_kernel_dispatch dispatch;
+  
+
+  
+    for(int r= -(height/2); r< (height/2); ++r)
+    {
+      for (unsigned c=-(width/2); r <(width/2); ++c)
+      {
+        if(c < 0)
+          kernel_.push_back(vcl_pair<point_3d,dispatch>(point_3d(c,r,0), dispatch('-')));
+        if(c == 0)
+          kernel_.push_back(vcl_pair<point_3d,dispatch>(point_3d(c,r,0), dispatch('0')));
+        if(c >  0)
+          kernel_.push_back(vcl_pair<point_3d,dispatch>(point_3d(c,r,0), dispatch('+')));
+      }
+    }
+   
+     //TO DO: determine angular resolition based on size of kernel
+      
+      //Rotate the basic kernel according to user specifications
+      vnl_rotation_matrix(rotations, R_);
+      
+      if(rotations != vnl_vector_fixed<double,3>(0.0, 0.0, 0.0));
+      {
+        this->warp_nearest_neighbor();
+      }
+  return true;
+      
+}
+
+
 #endif
+  
