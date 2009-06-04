c19dc87017a6ed66a2360bac2c3240207b6716ff
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-06-04 09:33:12 -0400

    SVN-Revision: 25199

---
+++ contrib/brl/bseg/bvpl/tests/test_bvpl_kernels.cxx
@@ -0,0 +1,34 @@
+//:
+// \file
+#include <testlib/testlib_test.h>
+#include <bvpl/bvpl_edge2d_kernel.h>
+#include <vnl/vnl_math.h>
+
+#include <vul/vul_file.h>
+
+//: Test changes
+bool test_edge2d(vcl_string grid_path,unsigned height, unsigned width,vnl_vector_fixed<double,3> const& rotations )
+{
+ //create a kernel and save it to a grid
+  bvpl_edge2d_kernel<SYMMETRIC> kernel; 
+  vcl_cout<< "alive \n";
+  kernel.create(height, width, rotations);
+  kernel.save_raw(grid_path);
+  return true;
+  
+}
+
+MAIN(test_bvpl_kernels)
+{
+  //define variaty of heights, widths, and rotations
+  unsigned h1=3, h2=7, h3 = 12;
+  double a1=2.0*vnl_math::pi/3.0, a2=vnl_math::pi/4.0, a3=vnl_math::pi/2.0, a4=vnl_math::pi;
+  bool result = test_edge2d("grid1.raw", h1,h1,vnl_vector_fixed<double,3>(a4, 0.0, 0.0));
+  TEST("grid 1", result, true);
+//  test_edge2d("grid2.vox", h1,h2,vnl_vector_fixed<double,3>(a4, a2, a3));
+//  test_edge2d("grid3.vox", h3,h2,vnl_vector_fixed<double,3>(a1, a4, 0));
+  SUMMARY();
+}
+
+
+
