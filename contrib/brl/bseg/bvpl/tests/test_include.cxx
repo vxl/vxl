3b9a8ac83db4246fda76f16070692e88a77e8652
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-06-04 12:30:00 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 25208

--- contrib/brl/bseg/bvpl/tests/test_include.cxx
+++ contrib/brl/bseg/bvpl/tests/test_include.cxx
@@ -2,6 +2,7 @@
 #include <bvpl/bvpl_edge2d_kernel.h>
 #include <bvpl/bvpl_kernel_base.h>
 #include <bvpl/bvpl_kernel_iterator.h>
+#include <bvpl/bvpl_neighb_operator.h>
 #include <bvpl/bvpl_subgrid_iterator.h>
 #include <bvpl/bvpl_voxel_subgrid.h>
 
