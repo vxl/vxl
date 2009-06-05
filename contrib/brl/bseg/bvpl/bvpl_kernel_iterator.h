dd0cd7ef51c84b3a1f197fe2e2060f0779a8c261
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-06-05 15:05:00 -0400

    avoid compiler warning (no EOLN at EOF)
    SVN-Revision: 25231

--- contrib/brl/bseg/bvpl/bvpl_kernel_iterator.h
+++ contrib/brl/bseg/bvpl/bvpl_kernel_iterator.h
@@ -1,15 +1,14 @@
 #ifndef bvpl_kernel_iterator_h_
 #define bvpl_kernel_iterator_h_
-
 //:
 // \file
-// \brief A class for iterating over the kernel values, paired with the position and value 
+// \brief A class for iterating over the kernel values, paired with the position and value
 //
 // \author Gamze Tunali
 // \date May 28, 2009
 // \verbatim
 //  Modifications
-//   
+//   <None yet>
 // \endverbatim
 
 #include <vbl/vbl_ref_count.h>
@@ -18,8 +17,9 @@
 #include <vcl_vector.h>
 #include <vcl_utility.h>
 
-class bvpl_kernel_dispatch{
-public:
+class bvpl_kernel_dispatch
+{
+ public:
   bvpl_kernel_dispatch(char c) { c_=c; }
   ~bvpl_kernel_dispatch() {}
   char c_;
@@ -27,7 +27,7 @@ public:
 
 class bvpl_kernel_iterator: public vbl_ref_count
 {
-public:
+ public:
   typedef vcl_pair<vgl_point_3d<int>, bvpl_kernel_dispatch> kernel_pair;
 
   bvpl_kernel_iterator() : cur_index_(0) {}
@@ -47,10 +47,10 @@ public:
   bvpl_kernel_iterator& operator--() { if (cur_index_ != 0) --cur_index_; return *this;}
 
   bvpl_kernel_dispatch operator*() { return kernel_vals_[cur_index_].second; }
- 
+
   bool isDone() { if (cur_index_ == kernel_vals_.size()) return true; return false; }
 
-private:
+ private:
   vcl_vector<vcl_pair<vgl_point_3d<int>, bvpl_kernel_dispatch> > kernel_vals_;
   unsigned cur_index_;
 };
