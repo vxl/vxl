06bec8ced46c69eef575cec9752253b6e88b9a06
Ian Scott <scottim@users.sourceforge.net>
2009-06-18 13:24:20 -0400

    remove empty template instantiations
    SVN-Revision: 25370

--- contrib/mul/pdf1d/Templates/mbl_cloneable_ptr+pdf1d_pdf-.cxx
+++
@@ -1,3 +0,0 @@
-#include <mbl/mbl_cloneable_ptr.h>
-#include <pdf1d/pdf1d_pdf.h>
-MBL_CLONEABLE_PTR_INSTANTIATE(pdf1d_pdf);
--- contrib/mul/vpdfl/Templates/mbl_cloneable_ptr+vpdfl_pdf_base-.cxx
+++
@@ -1,3 +0,0 @@
-#include <mbl/mbl_cloneable_ptr.h>
-#include <vpdfl/vpdfl_pdf_base.h>
-MBL_CLONEABLE_PTR_INSTANTIATE(vpdfl_pdf_base);
