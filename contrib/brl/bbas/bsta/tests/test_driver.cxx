5084cccbf18fd9a4eda932141a0f5319147c1c7f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-06-20 16:03:09 -0400

    brought tests out of comment, so running them (in CMakeLists.txt) might succeed...
    SVN-Revision: 25396

--- contrib/brl/bbas/bsta/tests/test_driver.cxx
+++ contrib/brl/bbas/bsta/tests/test_driver.cxx
@@ -14,16 +14,16 @@ DECLARE( test_sampler );
 void
 register_tests()
 {
-  //REGISTER( test_gaussian_full );
-  //REGISTER( test_gaussian_indep );
-  //REGISTER( test_gaussian_sphere );
-  //REGISTER( test_parzen_sphere );
-  //REGISTER( test_weibull );
-  //REGISTER( test_mixture );
+  REGISTER( test_gaussian_full );
+  REGISTER( test_gaussian_indep );
+  REGISTER( test_gaussian_sphere );
+  REGISTER( test_parzen_sphere );
+  REGISTER( test_weibull );
+  REGISTER( test_mixture );
   REGISTER( test_bsta_histogram );
-  //REGISTER( test_k_medoid );
-  //REGISTER( test_otsu_threshold );
-  //REGISTER( test_sampler );
+  REGISTER( test_k_medoid );
+  REGISTER( test_otsu_threshold );
+  REGISTER( test_sampler );
 }
 
 DEFINE_MAIN;
