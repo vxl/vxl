a8ef519e387b8893876f9c231f7f54c4cc148b29
Gamze Tunali <gamze@users.sourceforge.net>
2009-06-22 15:34:30 -0400

    instantiation of opinion for grid
    SVN-Revision: 25426

---
+++ contrib/brl/bseg/bvpl/Templates/bvpl_subgrid_iterator+bvxm_opinion-.cxx
@@ -0,0 +1,4 @@
+#include <bvpl/bvpl_subgrid_iterator.txx>
+#include <bvxm/bvxm_opinion.h>
+
+BVPL_SUBGRID_ITERATOR_INSTANTIATE(bvxm_opinion);
---
+++ contrib/brl/bseg/bvpl/Templates/bvpl_voxel_subgrid+bvxm_opinion-.cxx
@@ -0,0 +1,4 @@
+#include <bvpl/bvpl_voxel_subgrid.txx>
+#include <bvxm/bvxm_opinion.h>
+
+BVPL_VOXEL_SUBGRID_INSTANTIATE(bvxm_opinion);
