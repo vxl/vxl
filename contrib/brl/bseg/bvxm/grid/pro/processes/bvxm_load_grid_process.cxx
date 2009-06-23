bb6b6a42e6856b7c517866b89cc7ddeee0544986
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-06-23 10:20:59 -0400

    load_grid_process added
    SVN-Revision: 25448

---
+++ contrib/brl/bseg/bvxm/grid/pro/processes/bvxm_load_grid_process.cxx
@@ -0,0 +1,94 @@
+//This is brl/bseg/bvxm/grid/pro/processes/bvxm_load_grid_process.cxx
+//:
+// \file
+// \brief A process for  creating  an instance of a grid as specified by a 
+//        path and outputs a bvxm_voxel_grid_base_sptr to be store on the database
+// \author Isabel Restrepo
+// \date 6/15/2009
+//
+// \verbatim
+//  Modifications
+//    
+// \endverbatim
+
+#include <vcl_string.h>
+#include <bprb/bprb_func_process.h>
+#include <bprb/bprb_parameters.h>
+#include <bvxm/grid/bvxm_voxel_grid_base.h>
+#include <bvxm/grid/bvxm_voxel_grid.h>
+#include <vul/vul_file.h>
+
+#include <bsta/bsta_distribution.h>
+#include <bsta/bsta_gauss_f1.h>
+#include <bsta/bsta_attributes.h>
+#include <bsta/bsta_mixture_fixed.h>
+#include <bsta/bsta_mixture.h>
+#include <bsta/bsta_gaussian_indep.h>
+
+
+
+//: set input and output types
+bool bvxm_load_grid_process_cons(bprb_func_process& pro)
+{
+  //This process has no inputs nor outputs only parameters
+  vcl_vector<vcl_string> input_types_(2);
+  input_types_.push_back("vcl_string"); //the input path
+  input_types_.push_back("vcl_string"); //the type e.g. "float","double"...
+  
+  
+  vcl_vector<vcl_string> output_types_(1);
+  output_types_.push_back("bvxm_voxel_grid_base_sptr");  // The resulting grid
+  
+  if (!pro.set_input_types(input_types_))
+    return false;
+  
+  if (!pro.set_output_types(output_types_))
+    return false;
+  
+  return true;
+}
+
+
+//: Execute the process
+bool bvxm_load_grid_process(bprb_func_process& pro)
+{
+  // check number of inputs
+  if (pro.n_inputs() != 1)
+  {
+    vcl_cout << pro.name() << "The number of inputs should be " << 1 << vcl_endl;
+    return false;
+  }
+  
+  vcl_string input_path = pro.get_input<vcl_string>(0);
+  vcl_string datatype =  pro.get_input<vcl_string>(1);
+  
+  if (vul_file::is_directory(input_path) || !vul_file::exists(input_path)) {
+    vcl_cerr << "In bvxm_load_grid_process -- input path " << input_path<< "is not valid!\n";
+    return false;
+  }
+  vcl_cout << "In bvxm_load_grid_process( -- input file is: " <<  input_path << vcl_endl;
+  
+  //This is temporary. What should happen is that we can read the type from the file header. 
+  //Also the header should be such that we can check if the file is not currupt
+  if (datatype == "float"){
+    bvxm_voxel_grid_base_sptr grid = new bvxm_voxel_grid<float>(input_path);
+    pro.set_output_val<bvxm_voxel_grid_base_sptr>(0, grid);
+    return true;
+  }else if(datatype == "bool"){
+    bvxm_voxel_grid_base_sptr grid = new bvxm_voxel_grid<bool>(input_path);
+    pro.set_output_val<bvxm_voxel_grid_base_sptr>(0, grid);
+    return true;
+  }else if(datatype == "bsta_gaussian_mixture_float_3"){
+    //Only floating point are supported for now and mixtures with 3 components.
+    typedef bsta_num_obs<bsta_gauss_f1> gauss_type;
+    typedef bsta_mixture_fixed<gauss_type, 3> mix_gauss;
+    typedef bsta_num_obs<mix_gauss> mix_gauss_type;    
+    bvxm_voxel_grid_base_sptr grid = new bvxm_voxel_grid<mix_gauss_type>(input_path);
+    pro.set_output_val<bvxm_voxel_grid_base_sptr>(0, grid);
+    return true;
+  }else{
+    vcl_cerr << "datatype not supported\n";
+  }
+    
+  return false;
+}
