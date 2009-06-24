01dd83402ebe9cfe6f1ca718ee37635b6e39d49a
Vishal Jain <jainv@users.sourceforge.net>
2009-06-24 09:56:42 -0400

    added bvxm_io_voxel_grid_base.h
    SVN-Revision: 25484

---
+++ contrib/brl/bseg/bvxm/grid/io/bvxm_io_voxel_grid_base.cxx
@@ -0,0 +1,30 @@
+#include <bvxm/grid/io/bvxm_io_voxel_grid_base.h>
+#include <bvxm/grid/bvxm_voxel_slab.h>
+#include <bvxm/grid/bvxm_voxel_grid.h>
+//:
+// \file
+#include <vsl/vsl_binary_io.h>
+
+//: Binary io, NOT IMPLEMENTED, signatures defined to use brec_part_hierarchy as a brdb_value
+void vsl_b_write(vsl_b_ostream & /*os*/, bvxm_voxel_grid_base const & /*ph*/)
+{
+  vcl_cerr << "vsl_b_write() -- Binary io, NOT IMPLEMENTED, signatures defined to use bvxm_voxel_grid_base as a brdb_value\n";
+  return;
+}
+
+void vsl_b_read(vsl_b_istream & /*is*/, bvxm_voxel_grid_base & /*ph*/)
+{
+  vcl_cerr << "vsl_b_read() -- Binary io, NOT IMPLEMENTED, signatures defined to use bvxm_voxel_grid_base as a brdb_value\n";
+  return;
+}
+
+void vsl_b_read(vsl_b_istream& is, bvxm_voxel_grid_base* ph)
+{
+
+}
+
+void vsl_b_write(vsl_b_ostream& os, const bvxm_voxel_grid_base* &ph)
+{
+
+}
+
---
+++ contrib/brl/bseg/bvxm/grid/io/bvxm_io_voxel_grid_base.h
@@ -0,0 +1,28 @@
+#ifndef bvxm_io_voxel_grid_base_h_
+#define bvxm_io_voxel_grid_base_h_
+//:
+// \file
+
+#include <vsl/vsl_binary_io.h>
+#include <vcl_iostream.h>
+#include <bvxm/grid/bvxm_voxel_grid_base.h>
+//#include <bvxm/bvxm_world_params.h>
+
+// Binary io, NOT IMPLEMENTED, signatures defined to use bvxm_voxel_grid_base as a brdb_value
+
+//: Binary save parameters to stream.
+void vsl_b_write(vsl_b_ostream & os, bvxm_voxel_grid_base const &world);
+
+//: Binary load parameters from stream.
+void vsl_b_read(vsl_b_istream & is, bvxm_voxel_grid_base &sb);
+
+void vsl_print_summary(vcl_ostream &os, const bvxm_voxel_grid_base &sb);
+
+void vsl_b_read(vsl_b_istream& is, bvxm_voxel_grid_base* p);
+
+void vsl_b_write(vsl_b_ostream& os, const bvxm_voxel_grid_base* &p);
+
+
+void vsl_print_summary(vcl_ostream& os, const bvxm_voxel_grid_base* &p);
+
+#endif
