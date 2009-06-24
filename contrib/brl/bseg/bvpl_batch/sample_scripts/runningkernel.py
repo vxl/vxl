66d00dffb594e72dc65c41e3d5e3b00c4a1f5f09
Vishal Jain <jainv@users.sourceforge.net>
2009-06-24 16:35:50 -0400

    added some sample python scripts.
    SVN-Revision: 25500

---
+++ contrib/brl/bseg/bvpl_batch/sample_scripts/runningkernel.py
@@ -0,0 +1,47 @@
+import bvpl_batch
+bvpl_batch.register_processes();
+bvpl_batch.register_datatypes();
+
+
+class dbvalue:
+  def __init__(self, index, type):
+    self.id = index    # unsigned integer
+    self.type = type   # string
+
+#first creat an empty world.
+print("Load Voxel World");
+bvpl_batch.init_process("bvxmLoadGridProcess");
+bvpl_batch.set_input_string(0,"d:/vj/scripts/Lidar_edges/ocp_200_100_0_250_200_100.vox");
+bvpl_batch.set_input_string(1,"float");
+bvpl_batch.run_process();
+(world_id,world_type)= bvpl_batch.commit_output(0);
+world = dbvalue(world_id,world_type);
+
+print("Creating 2D edge kernel");
+bvpl_batch.init_process("bvplCreateEdge2dKernelProcess");
+bvpl_batch.set_params_process("./edge2d_kernel_params.xml");
+bvpl_batch.run_process();
+(kernel_id,kernel_type)= bvpl_batch.commit_output(0);
+kernel = dbvalue(kernel_id,kernel_type);
+
+print("Running Operator");
+bvpl_batch.init_process("bvplNeighborhoodOperatorProcess");
+bvpl_batch.set_input_from_db(0,world );
+bvpl_batch.set_input_from_db(1,kernel);
+bvpl_batch.set_input_string(2,"float");
+bvpl_batch.set_input_string(3,"edge2d");
+bvpl_batch.set_input_string(4,"d:/vj/scripts/Lidar_edges/x_conv.vox");
+bvpl_batch.run_process();
+(result_x_world_id,result_x_world_type)= bvpl_batch.commit_output(0);
+result_x_world = dbvalue(result_x_world_id,result_x_world_type);
+
+
+print("Writing World");
+bvpl_batch.init_process("bvxmSaveGridRawProcess");
+bvpl_batch.set_input_from_db(0,result_x_world);
+bvpl_batch.set_input_string(1,"d:/vj/scripts/Lidar_edges/x_crop_world.raw");
+bvpl_batch.set_input_string(2,"float");
+bvpl_batch.run_process();
+
+
+
---
+++ contrib/brl/bseg/bvpl_batch/sample_scripts/save_raw.py
@@ -0,0 +1,36 @@
+import bvpl_batch
+bvpl_batch.register_processes();
+bvpl_batch.register_datatypes();
+
+
+class dbvalue:
+  def __init__(self, index, type):
+    self.id = index    # unsigned integer
+    self.type = type   # string
+
+#first creat an empty world.
+print("Load Voxel World");
+bvpl_batch.init_process("bvxmLoadGridProcess");
+bvpl_batch.set_input_string(0,"d:/vj/scripts/Lidar_edges/ocp_bin_0_scale_0.vox");
+bvpl_batch.set_input_string(1,"float");
+bvpl_batch.run_process();
+(world_id,world_type)= bvpl_batch.commit_output(0);
+world = dbvalue(world_id,world_type);
+
+print("Crop Voxel World");
+bvpl_batch.init_process("bvxmCropGridProcess");
+bvpl_batch.set_params_process("d:/vj/scripts/Lidar_edges/bvxmCropGridProcess.xml");
+bvpl_batch.set_input_from_db(0,world);
+bvpl_batch.run_process();
+(crop_world_id,crop_world_type)= bvpl_batch.commit_output(0);
+crop_world = dbvalue(crop_world_id,crop_world_type);
+
+print("Writing World");
+bvpl_batch.init_process("bvxmSaveGridRawProcess");
+bvpl_batch.set_input_from_db(0,crop_world);
+bvpl_batch.set_input_string(1,"d:/vj/scripts/Lidar_edges/crop_world.raw");
+bvpl_batch.set_input_string(2,"float");
+bvpl_batch.run_process();
+
+
+
