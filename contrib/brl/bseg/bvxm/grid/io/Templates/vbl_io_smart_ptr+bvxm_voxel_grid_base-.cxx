a29a3304841b38091b6d6bbc30e1a407c762a929
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-06-24 17:59:57 -0400

    added io
    SVN-Revision: 25506

---
+++ contrib/brl/bseg/bvxm/grid/io/Templates/vbl_io_smart_ptr+bvxm_voxel_grid_base-.cxx
@@ -0,0 +1,5 @@
+#include <bvxm/grid/bvxm_voxel_grid_base.h>
+#include <bvxm/grid/io/bvxm_io_voxel_grid_base.h>
+#include <vbl/io/vbl_io_smart_ptr.txx>
+
+VBL_IO_SMART_PTR_INSTANTIATE(bvxm_voxel_grid_base);
