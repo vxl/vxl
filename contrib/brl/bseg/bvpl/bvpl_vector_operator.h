55910aff2248126f4c1b7c5477fd691847883e6f
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-06-25 11:14:09 -0400

    fixed grid type
    SVN-Revision: 25521

--- contrib/brl/bseg/bvpl/bvpl_vector_operator.h
+++ contrib/brl/bseg/bvpl/bvpl_vector_operator.h
@@ -27,25 +27,25 @@ public:
   
   //: Applies a vector of kernel and functor to a grid. The result is  a grid of maxima response and  a grid 
   //  containing a vnl_vectors in the orientation of maxima response e.i. the direction is rotation axis, the magnitude is the angle
-  void max_orientation(bvxm_voxel_grid<T>* grid, bvpl_kernel_vector_sptr kernel_vector,
+  void apply_and_suppress(bvxm_voxel_grid<T>* grid, bvpl_kernel_vector_sptr kernel_vector,
                        bvpl_neighb_operator<T,F>* oper, bvxm_voxel_grid<T>* out_grid,  
-                       bvxm_voxel_grid<T>* orientation_grid);
+                       bvxm_voxel_grid<vnl_vector_fixed<float,3> >* orientation_grid);
 
+  //
   void get_max_orientation_grid(bvxm_voxel_grid<T>* out_grid, bvxm_voxel_grid<T>* temp_grid,
-                                bvxm_voxel_grid<T>* orientation_grid, 
+                                bvxm_voxel_grid<vnl_vector_fixed<float,3> >* orientation_grid, 
                                 vnl_vector_fixed<float,3> temp_orientation);
 private:
 
 };
 
 template<class T, class F>
-void bvpl_vector_operator<T,F>::max_orientation(bvxm_voxel_grid<T>* grid, bvpl_kernel_vector_sptr kernel_vector,
+void bvpl_vector_operator<T,F>::apply_and_suppress(bvxm_voxel_grid<T>* grid, bvpl_kernel_vector_sptr kernel_vector,
                                                bvpl_neighb_operator<T,F>* oper, bvxm_voxel_grid<T>* out_grid,  
-                                               bvxm_voxel_grid<T>* orientation_grid)
+                                               bvxm_voxel_grid<vnl_vector_fixed<float, 3> >* orientation_grid)
 {
   bvxm_voxel_grid<T> temp_grid("temp_grid.vox",out_grid->grid_size());
-  bvxm_voxel_grid<T> temp_orientation_grid("temp_grid.vox",out_grid->grid_size());
-  out_grid->initialize_data(vcl_numeric_limits<T>::min);
+  out_grid->initialize_data(T(0));
   temp_grid.initialize_data(T(0.0));
   orientation_grid->initialize_data(vnl_vector_fixed<float,3>(0.0f,0.0f,0.0f));
   bvpl_kernel_vector::iterator vit = kernel_vector->kernels_.begin();
@@ -63,7 +63,7 @@ void bvpl_vector_operator<T,F>::max_orientation(bvxm_voxel_grid<T>* grid, bvpl_k
 
 template<class T, class F>
 void bvpl_vector_operator<T,F>::get_max_orientation_grid(bvxm_voxel_grid<T>* out_grid, bvxm_voxel_grid<T>* temp_grid,
-                                                    bvxm_voxel_grid<T>* orientation_grid, vnl_vector_fixed<float,3> temp_orientation)
+                                                    bvxm_voxel_grid<vnl_vector_fixed<float, 3> >* orientation_grid, vnl_vector_fixed<float,3> temp_orientation)
 {
   typename bvxm_voxel_grid<T>::iterator out_grid_it = out_grid->begin();
   typename bvxm_voxel_grid<T>::iterator temp_grid_it = temp_grid->begin();
@@ -73,9 +73,9 @@ void bvpl_vector_operator<T,F>::get_max_orientation_grid(bvxm_voxel_grid<T>* out
   for (; out_grid_it!=out_grid->end(); ++out_grid_it, ++temp_grid_it, ++or_grid_it) 
   {
     
-    typename bvxm_voxel_slab<T>::iterator out_slab_it = out_grid_it->begin();
-    typename bvxm_voxel_grid<T>::iterator temp_slab_it= temp_grid_it->begin();
-    bvxm_voxel_grid<vnl_vector_fixed<float,3> >::iterator or_slab_it = or_grid_it->begin();
+    typename bvxm_voxel_slab<T>::iterator out_slab_it = (*out_grid_it).begin();
+    typename bvxm_voxel_slab<T>::iterator temp_slab_it= (*temp_grid_it).begin();
+    bvxm_voxel_slab<vnl_vector_fixed<float,3> >::iterator or_slab_it = or_grid_it->begin();
 
     
     for (; out_slab_it!=out_grid_it->end(); ++out_slab_it, ++temp_slab_it, ++or_slab_it) 
