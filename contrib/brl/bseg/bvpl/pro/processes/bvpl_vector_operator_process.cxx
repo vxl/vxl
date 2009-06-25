b181feda20c6825395a83caddd4a00084d28b3e6
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-06-25 11:13:50 -0400

    completed process
    SVN-Revision: 25520

--- contrib/brl/bseg/bvpl/pro/processes/bvpl_vector_operator_process.cxx
+++ contrib/brl/bseg/bvpl/pro/processes/bvpl_vector_operator_process.cxx
@@ -28,8 +28,8 @@
 
 namespace bvpl_vector_operator_process_globals
 {
-  const unsigned n_inputs_ = 5;
-  const unsigned n_outputs_ = 1;
+  const unsigned n_inputs_ = 6;
+  const unsigned n_outputs_ = 2;
 }
 
 
@@ -44,19 +44,22 @@ bool bvpl_vector_operator_process_cons(bprb_func_process& pro)
   //          -opinion
   //          ....
   //input[4]: The functor type
-  //input[5]: Output grid path
+  //input[5]: Output grid path to hold response
+  //input[6]: Output grid path to hold orientations
   vcl_vector<vcl_string> input_types_(n_inputs_);
-  input_types_[0] = "bvxm_voxel_grid_sptr";
+  input_types_[0] = "bvxm_voxel_grid_base_sptr";
   input_types_[1] = "bvpl_kernel_vector_sptr";
   input_types_[2] = "vcl_string";
   input_types_[3] = "vcl_string";
   input_types_[4] = "vcl_string";
+  input_types_[5] = "vcl_string";
   if (!pro.set_input_types(input_types_))
     return false;
   
   //output
   vcl_vector<vcl_string> output_types_(n_outputs_);
-  output_types_[0] = "bvxm_voxel_grid_sptr";
+  output_types_[0] = "bvxm_voxel_grid_base_sptr";
+  output_types_[1] = "bvxm_voxel_grid_base_sptr";
   if (!pro.set_output_types(output_types_))
     return false;
   return true;
@@ -79,6 +82,7 @@ bool bvpl_vector_operator_process(bprb_func_process& pro)
   vcl_string datatype = pro.get_input<vcl_string>(i++);
   vcl_string functor_name = pro.get_input<vcl_string>(i++);
   vcl_string out_grid_path = pro.get_input<vcl_string>(i++);
+  vcl_string orientation_grid_path = pro.get_input<vcl_string>(i++);
   
   //check input's validity
   i=0;
@@ -94,18 +98,24 @@ bool bvpl_vector_operator_process(bprb_func_process& pro)
   
   if (datatype == "float") {
     bvxm_voxel_grid<float> *grid = dynamic_cast<bvxm_voxel_grid<float>* > (grid_base.ptr());
-    //bvxm_voxel_grid<float> grid_out(out_grid_path, grid->grid_size());
+    bvxm_voxel_grid<float> grid_out(out_grid_path, grid->grid_size());
+    bvxm_voxel_grid<vnl_vector_fixed<float,3> > orientation_grid(orientation_grid_path, grid->grid_size());
     if (functor_name == "edge2d") {
       bvpl_edge2d_functor<float> func;
       bvpl_neighb_operator<float, bvpl_edge2d_functor<float> > oper(func);
-     // oper.operate(grid, kernel);
+      bvpl_vector_operator<float, bvpl_edge2d_functor<float> > vector_oper;
+      vector_oper.apply_and_suppress(grid,kernel,&oper,&grid_out, &orientation_grid);
+      pro.set_output_val<bvxm_voxel_grid_base_sptr>(0, &grid_out);
+      pro.set_output_val<bvxm_voxel_grid_base_sptr>(1, &orientation_grid);
     }
   } else if (datatype == "opinion") {
    bvxm_voxel_grid<bvxm_opinion> *grid = dynamic_cast<bvxm_voxel_grid<bvxm_opinion>* > (grid_base.ptr());;
    bvxm_voxel_grid<bvxm_opinion> grid_out(out_grid_path, grid->grid_size());
    bvpl_opinion_functor func;
    bvpl_neighb_operator<bvxm_opinion, bvpl_opinion_functor> oper(func);
-    //oper.operate(grid, kernel);
+  // bvpl_vector_operator operator;
+    
+    
   }
   
   return true;
