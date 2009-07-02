d08439b974879adc2b8b60cf96eaa1e3f80d34e5
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-07-02 12:52:11 -0400

    added template
    SVN-Revision: 25590

---
+++ contrib/brl/bseg/bvxm/grid/Templates/bvxm_voxel_grid+vnl_vector_fixed+float.4--.cxx
@@ -0,0 +1,13 @@
+#include <bvxm/grid/bvxm_voxel_grid.txx>
+#include <bvxm/grid/bvxm_voxel_storage_disk.txx>
+#include <bvxm/grid/bvxm_voxel_storage_mem.txx>
+#include <bvxm/grid/bvxm_voxel_storage_disk_cached.txx>
+#include <vnl/vnl_vector_fixed.h>
+
+typedef vnl_vector_fixed<float,4> vnl_vector_fixed_f4;
+
+BVXM_VOXEL_GRID_INSTANTIATE(vnl_vector_fixed_f4);
+BVXM_VOXEL_STORAGE_DISK_INSTANTIATE(vnl_vector_fixed_f4);
+BVXM_VOXEL_STORAGE_MEM_INSTANTIATE(vnl_vector_fixed_f4);
+BVXM_VOXEL_STORAGE_DISK_CACHED_INSTANTIATE(vnl_vector_fixed_f4);
+
--- contrib/brl/bseg/bvxm/grid/Templates/bvxm_voxel_slab+vnl_vector_fixed+float.4--.cxx
+++ contrib/brl/bseg/bvxm/grid/Templates/bvxm_voxel_slab+vnl_vector_fixed+float.4--.cxx
@@ -1,6 +1,8 @@
 #include <bvxm/grid/bvxm_voxel_slab.txx>
+#include <bvxm/grid/bvxm_voxel_slab_iterator.txx>
 #include <vnl/vnl_vector_fixed.h>
 
 typedef vnl_vector_fixed<float,4> vnl_vector_fixed_f4;
 
 BVXM_VOXEL_SLAB_INSTANTIATE(vnl_vector_fixed_f4);
+BVXM_VOXEL_SLAB_ITERATOR_INSTANTIATE(vnl_vector_fixed_f4);
