f2fab56a62300fdb059f1020ace28dfc7ace0db2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-07-08 12:38:25 -0400

    avoid compiler warning about unused static functions by moving them from .h to .cxx
    SVN-Revision: 25703

--- contrib/brl/bpro/bpro_batch/bpro_batch.cxx
+++ contrib/brl/bpro/bpro_batch/bpro_batch.cxx
@@ -10,6 +10,26 @@
 #include <brdb/brdb_selection.h>
 #include <brdb/brdb_database_manager.h>
 
+static PyObject *init_process(PyObject *self, PyObject *args);
+static PyObject *set_input_bool(PyObject *self, PyObject *args);
+static PyObject *set_input_string(PyObject *self, PyObject *args);
+static PyObject *set_input_int(PyObject *self, PyObject *args);
+static PyObject *set_input_unsigned(PyObject *self, PyObject *args);
+static PyObject *set_input_long(PyObject *self, PyObject *args);
+static PyObject *set_input_float(PyObject *self, PyObject *args);
+static PyObject *set_input_double(PyObject *self, PyObject *args);
+static PyObject *get_input_float(PyObject *self, PyObject *args);
+static PyObject *get_input_unsigned(PyObject *self, PyObject *args);
+static PyObject *process_print_default_params(PyObject *self, PyObject *args);
+static PyObject *process_init(PyObject *self, PyObject *args);
+static PyObject *set_params_process(PyObject *self, PyObject *args);
+static PyObject *run_process(PyObject *self, PyObject *args);
+static PyObject *commit_output(PyObject *self, PyObject *args);
+static PyObject *set_input_from_db(PyObject* self, PyObject *args);
+static PyObject *remove_data(PyObject *self, PyObject *args);
+static PyObject *remove_data_obj(PyObject *self, PyObject *args);
+static PyObject *print_db(PyObject *self, PyObject *args);
+static PyObject *clear(PyObject *self, PyObject *args);
 
 PyMethodDef batch_methods[] =
 {
--- contrib/brl/bpro/bpro_batch/bpro_batch.h
+++ contrib/brl/bpro/bpro_batch/bpro_batch.h
@@ -13,33 +13,14 @@
 // \endverbatim
 #include "Python.h"
 
-typedef struct {
+typedef struct
+{
   PyObject_HEAD
   unsigned int id;
   char* type;
 } dbvalue;
 
 const int METHOD_NUM = 24;
-static PyObject *init_process(PyObject *self, PyObject *args);
-static PyObject *set_input_bool(PyObject *self, PyObject *args);
-static PyObject *set_input_string(PyObject *self, PyObject *args);
-static PyObject *set_input_int(PyObject *self, PyObject *args);
-static PyObject *set_input_unsigned(PyObject *self, PyObject *args);
-static PyObject *set_input_long(PyObject *self, PyObject *args);
-static PyObject *set_input_float(PyObject *self, PyObject *args);
-static PyObject *set_input_double(PyObject *self, PyObject *args);
-static PyObject *get_input_float(PyObject *self, PyObject *args);
-static PyObject *get_input_unsigned(PyObject *self, PyObject *args);
-static PyObject *process_print_default_params(PyObject *self, PyObject *args);
-static PyObject *process_init(PyObject *self, PyObject *args);
-static PyObject *set_params_process(PyObject *self, PyObject *args);
-static PyObject *run_process(PyObject *self, PyObject *args);
-static PyObject *commit_output(PyObject *self, PyObject *args);
-static PyObject *set_input_from_db(PyObject* self, PyObject *args);
-static PyObject *remove_data(PyObject *self, PyObject *args);
-static PyObject *remove_data_obj(PyObject *self, PyObject *args);
-static PyObject *print_db(PyObject *self, PyObject *args);
-static PyObject *clear(PyObject *self, PyObject *args);
 void register_basic_datatypes();
 extern PyMethodDef batch_methods[];
 
