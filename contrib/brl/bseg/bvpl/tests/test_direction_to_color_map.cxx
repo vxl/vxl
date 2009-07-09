6d2bcfe5f5f7d27c1fae8b21171f654b41e8f16f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-07-09 06:32:00 -0400

    return type of find_closest_points_from_cube_to_peano_curve() is std::map, not std::vector
    SVN-Revision: 25716

--- contrib/brl/bseg/bvpl/tests/test_direction_to_color_map.cxx
+++ contrib/brl/bseg/bvpl/tests/test_direction_to_color_map.cxx
@@ -31,10 +31,10 @@ static void test_direction_to_color_map()
   vcl_vector<vgl_point_3d<double> >  proj_on_cube;
   project_sphereical_samples_to_cubes(samples,proj_on_cube);
   vcl_vector<vgl_point_3d<double> > p3d=peano_curve_on_cube(2);
-  vcl_vector<float> indices=find_closest_points_from_cube_to_peano_curve(samples,p3d,proj_on_cube);
+  vcl_map<vgl_point_3d<double>,float,point_3d_cmp> indices=find_closest_points_from_cube_to_peano_curve(samples,p3d,proj_on_cube);
 #if 0
   vcl_ofstream ofile("d:/vj/test_proj_samples.txt");
-  for (unsigned i=0;i<proj_on_cube.size();i++)
+  for (unsigned i=0;i<proj_on_cube.size();++i)
   {
     ofile<<proj_on_cube[i].x()<<' '<<proj_on_cube[i].y()<<' '<<proj_on_cube[i].z()<<'\n';
   }
@@ -42,12 +42,12 @@ static void test_direction_to_color_map()
 
   vcl_ofstream ofile1("d:/vj/test_peano_curve.txt");
 
-  for (unsigned i=0;i<p3d.size();i++)
+  for (unsigned i=0;i<p3d.size();++i)
     ofile1<<p3d[i].x()<<' '<<p3d[i].y()<<' '<<p3d[i].z()<<'\n';
   ofile1.close();
 
   vcl_ofstream ofile2("d:/vj/test_indices.txt");
-  for (unsigned i=0;i<indices.size();i++)
+  for (unsigned i=0;i<indices.size();++i)
     ofile2<<indices[i]<<'\n';
   ofile2.close();
 #endif
