05c9a117c8b1f7056d7d12f900bfa96fe097bd1e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-07-09 16:59:10 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 25731

--- contrib/brl/bbas/bxml/bsvg/tests/test_include.cxx
+++ contrib/brl/bbas/bxml/bsvg/tests/test_include.cxx
@@ -1,4 +1,8 @@
 #include <bxml/bsvg/bsvg_document.h>
 #include <bxml/bsvg/bsvg_element.h>
+#include <bxml/bsvg/bsvg_plot.h>
+
+#include <bxml/bsvg/pro/bsvg_processes.h>
+#include <bxml/bsvg/pro/bsvg_register.h>
 
 int main() { return 0; }
