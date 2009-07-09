b3cc22e222cb517d20c9df70f2160887d716f381
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-07-09 17:26:38 -0400

    brief Doxygen comment should be on a single line
    SVN-Revision: 25734

--- contrib/brl/bbas/bxml/bsvg/bsvg_document.h
+++ contrib/brl/bbas/bxml/bsvg/bsvg_document.h
@@ -1,6 +1,9 @@
+#ifndef bsvg_document_h_
+#define bsvg_document_h_
 //:
 // \file
 // \brief classes based on bxml to create SVG documents
+//
 //        create an XML document with SVG primitives (text, lines, ellipses, polygons, etc.) and display them using any browser
 //
 // \author Ozge C. Ozcanli (Brown)
@@ -11,26 +14,20 @@
 //   Ozge C. Ozcanli - July 08, 09 - ported to vxl from local repository - minor fixes
 // \endverbatim
 
-#if !defined(_bsvg_document_h)
-#define _bsvg_document_h
-
 #include <bxml/bxml_document.h>
 #include <vcl_string.h>
 
-class bsvg_element;
-
 class bsvg_document : public bxml_document
 {
-public:
-    bsvg_document(float w, float h);  // if viewBox params are not specified, then each pixel corresponds to one unit square while drawing
-    bsvg_document(float w, float h, float viewBox_x, float viewBox_y, float viewBox_w, float viewBox_h);
-    bool add_description(const vcl_string& d);
-    bool add_element(const bxml_data_sptr& element);
+ public:
+  bsvg_document(float w, float h);  // if viewBox params are not specified, then each pixel corresponds to one unit square while drawing
+  bsvg_document(float w, float h, float viewBox_x, float viewBox_y, float viewBox_w, float viewBox_h);
+  bool add_description(const vcl_string& d);
+  bool add_element(const bxml_data_sptr& element);
 
-public:
-    float w_;
-    float h_;
-
+ public:
+  float w_;
+  float h_;
 };
 
-#endif  //_bsvg_document_h
+#endif  // bsvg_document_h_
