b0d19c05c248bac2bf998d81911a1a3c0f3c3b2d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-07-20 12:03:30 -0400

    avoid compiler warning ("bool" function must return a value)
    SVN-Revision: 25791

--- contrib/brl/bseg/bvxm/grid/pro/processes/bvxm_combine_grids_process.cxx
+++ contrib/brl/bseg/bvxm/grid/pro/processes/bvxm_combine_grids_process.cxx
@@ -23,23 +23,23 @@ bool bvxm_combine_grids_process_cons(bprb_func_process& pro)
   // 0. grid 1
   // 1. grid2
   // 2. Path to output grid
-  
+
   vcl_vector<vcl_string> input_types_(3);
   input_types_[0] = "bvxm_voxel_grid_base_sptr";
   input_types_[1] = "bvxm_voxel_grid_base_sptr";
   input_types_[2] = "vcl_string";
-  
-  
+
+
   // Outputs
   // 0. grid
   vcl_vector<vcl_string> output_types_(1);
   output_types_[0] = "bvxm_voxel_grid_base_sptr";
   if (!pro.set_input_types(input_types_))
     return false;
-  
+
   if (!pro.set_output_types(output_types_))
     return false;
-  
+
   return true;
 }
 
@@ -53,7 +53,7 @@ bool bvxm_combine_grids_process(bprb_func_process& pro)
     vcl_cout << pro.name() << "The number of inputs should be " << 3<< vcl_endl;
     return false;
   }
-  
+
   bvxm_voxel_grid_base_sptr grid1_base = pro.get_input< bvxm_voxel_grid_base_sptr>(0);
   bvxm_voxel_grid_base_sptr grid2_base = pro.get_input< bvxm_voxel_grid_base_sptr>(1);
   vcl_string output_path = pro.get_input<vcl_string>(2);
@@ -71,7 +71,7 @@ bool bvxm_combine_grids_process(bprb_func_process& pro)
 
   //combine response grid and orientation grid. the result is a vnl_vector<4> where the first 3-numbers correspond to orientation
   // and the last number corresponds to kernel response
- bvxm_voxel_grid<float>::iterator grid2_it = grid2->begin();
+  bvxm_voxel_grid<float>::iterator grid2_it = grid2->begin();
   bvxm_voxel_grid<vnl_vector_fixed<float, 3> >::iterator grid1_it = grid1->begin();
   bvxm_voxel_grid<vnl_vector_fixed<float,4> >::iterator grid_out_it = grid_out->begin();
   for (; grid_out_it!=grid_out->end(); ++grid_out_it, ++grid2_it, ++grid1_it)
@@ -86,11 +86,8 @@ bool bvxm_combine_grids_process(bprb_func_process& pro)
       *out_slab_it = this_feature;
     }
   }
-  
+
   pro.set_output_val<bvxm_voxel_grid_base_sptr>(0, grid_out);
+  return true;
 }
-
-
-
-
 
