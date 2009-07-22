3c47bf48d13534c747235315ff73136d86af1dc3
Gamze Tunali <gamze@users.sourceforge.net>
2009-07-22 16:51:45 -0400

    did mesh transformation into block boundaries
    SVN-Revision: 25838

--- contrib/brl/bseg/boxm/boxm_upload_mesh.h
+++ contrib/brl/bseg/boxm/boxm_upload_mesh.h
@@ -5,18 +5,22 @@
 #include <boct/boct_tree.h>
 #include <boct/boct_tree_cell.h>
 #include <boxm/boxm_block.h>
-#include <vgl/vgl_intersection.h>
+#include <boxm/boxm_scene.h>
+#include <boxm/boxm_utils.h>
 
 #include <imesh/imesh_mesh.h>
-
+#include <vpgl/bgeo/bgeo_lvcs.h>
+#include <vgl/vgl_box_3d.h>
+#include <vgl/vgl_intersection.h>
 #include <vcl_iostream.h>
 
 template <class T_loc, class T_data>
 void boxm_upload_mesh_into_block(boxm_block<boct_tree<T_loc, T_data> > *block,
-                                 imesh_mesh& mesh, bgeo_lvcs const& lvcs, T_data val)
+                                 imesh_mesh& mesh, bgeo_lvcs& lvcs, bool use_lvcs, T_data val)
 {
   typedef boct_tree<T_loc, T_data> tree_type;
   tree_type* tree = block->get_tree();
+  vgl_box_3d<double> block_bb = block->bounding_box();
 
   // initialize grid with big values
   imesh_face_array_base& fs = mesh.faces();
@@ -28,34 +32,43 @@ void boxm_upload_mesh_into_block(boxm_block<boct_tree<T_loc, T_data> > *block,
     vgl_box_3d<double> bb;
     for (unsigned j=0; j<fs.num_verts(i); ++j) {
       unsigned int v_id = fs(i,j);
-      double lx, ly, lz;
-      lvcs.global_to_local(vertices(v_id,0), vertices(v_id,1), vertices(v_id,2),
-                           bgeo_lvcs::wgs84,lx,ly,lz);
-      vgl_point_3d<double> v(lx,ly,lz);
+      vgl_point_3d<double> v;
+      if (use_lvcs) {
+        double lx, ly, lz;
+        lvcs.global_to_local(vertices(v_id,0), vertices(v_id,1), vertices(v_id,2),
+                             bgeo_lvcs::wgs84,lx,ly,lz);
+        v=vgl_point_3d<double>(lx,ly,lz);
+      } else
+        v=vgl_point_3d<double>(vertices(v_id,0), vertices(v_id,1), vertices(v_id,2));
+
+      vgl_vector_3d<double> diff = v-block_bb.min_point();   // get the value according to the block origin
+      //convert the values into [0,1] range
+      v.set(diff.x()/block_bb.width(),diff.y()/ block_bb.height(),diff.y()/ block_bb.depth());
       bb.add(v);
       v_list.push_back(v);
     }
-  }
-
-  // check if polygon's bounding box intersects with the block
-  vgl_box_3d<double> block_bb = block->bounding_box();
-  vgl_box_3d<double> inters = vgl_intersection(block_bb, bb);
-  if (inters.is_empty())
-    return false;
   
-  //tree->initialize();
 
-  // locate the polygon bounding box in tree
-  boct_tree_cell<T_loc,T_data>* region=tree->locate_region(bb);
-  if (region) {
-    // test all the children for intersection
-    vcl_vector<boct_tree_cell<T_loc,T_data>*> children;
-    region->leaf_children(children);
-    for (unsigned i=0; i<children.size(); i++) {
-      boct_tree_cell<T_loc,T_data>* cell=children[i];
-      vgl_box_3d<double> cell_bb=tree->cell_bounding_box(cell);
-      if (vgl_intersection(cell_bb, v_list).size() > 0) {
-        cell->set_data(val);
+    // check if polygon's bounding box intersects with the block
+    vgl_box_3d<double> inters = vgl_intersection(block_bb, bb);
+    if (inters.is_empty())
+      return;
+
+    // locate the polygon bounding box in tree
+    boct_tree_cell<T_loc,T_data>* region=tree->locate_region(bb);
+    if (region) {
+      // test all the children for intersection
+      vcl_vector<boct_tree_cell<T_loc,T_data>*> children;
+      if (!region->is_leaf())
+        region->leaf_children(children);
+      else // insert the node itself, if no children
+        children.push_back(region);
+      for (unsigned i=0; i<children.size(); i++) {
+        boct_tree_cell<T_loc,T_data>* cell=children[i];
+        vgl_box_3d<double> cell_bb=tree->cell_bounding_box(cell);
+        if (boxm_utils::intersection(cell_bb, v_list)) {
+          cell->set_data(val);
+        }
       }
     }
   }
@@ -63,7 +76,7 @@ void boxm_upload_mesh_into_block(boxm_block<boct_tree<T_loc, T_data> > *block,
 
 template <class T_loc, class T_data>
 void boxm_upload_mesh_into_scene(boxm_scene<boct_tree<T_loc, T_data > > &scene,
-                                 imesh_mesh& mesh, T_data val)
+                                 imesh_mesh& mesh, bool use_lvcs, T_data val)
 {
   typedef boct_tree<T_loc, T_data > tree_type;
   bgeo_lvcs lvcs=scene.lvcs();
@@ -71,7 +84,7 @@ void boxm_upload_mesh_into_scene(boxm_scene<boct_tree<T_loc, T_data > > &scene,
   for (; !iter.end(); iter++) {
     scene.load_block(iter.index());
     boxm_block<tree_type>* block = *iter;
-    boxm_upload_mesh_into_block(block, mesh, lvcs, val);
+    boxm_upload_mesh_into_block(block, mesh, lvcs, use_lvcs, val);
     scene.write_active_block();
   }
 }
