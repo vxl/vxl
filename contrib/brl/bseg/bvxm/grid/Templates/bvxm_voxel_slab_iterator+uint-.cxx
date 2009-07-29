a7d7646138ac5b19c77544ebf2077a1de4a0b27c
Vishal Jain <jainv@users.sourceforge.net>
2009-07-29 13:10:44 -0400

    added template instantiation for uint
    SVN-Revision: 25907

---
+++ contrib/brl/bseg/bvxm/grid/Templates/bvxm_voxel_slab_iterator+uint-.cxx
@@ -0,0 +1,6 @@
+#include <bvxm/grid/bvxm_voxel_slab_iterator.txx>
+
+BVXM_VOXEL_SLAB_ITERATOR_INSTANTIATE(unsigned int);
+
+
+
---
+++ contrib/brl/bseg/bvxm/grid/Templates/bvxm_voxel_storage_disk_cached+uint-.cxx
@@ -0,0 +1,3 @@
+#include <bvxm/grid/bvxm_voxel_storage_disk_cached.txx>
+
+BVXM_VOXEL_STORAGE_DISK_CACHED_INSTANTIATE(unsigned int);
