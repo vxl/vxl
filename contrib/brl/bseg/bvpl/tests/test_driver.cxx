882275ed3b0acf5053d317167a51cd2c6bb39675
Vishal Jain <jainv@users.sourceforge.net>
2009-07-31 11:14:51 -0400

    uncommented out all other tests
    SVN-Revision: 25936

--- contrib/brl/bseg/bvpl/tests/test_driver.cxx
+++ contrib/brl/bseg/bvpl/tests/test_driver.cxx
@@ -9,11 +9,11 @@ DECLARE( test_bvpl_vector_operator );
 
 void register_tests()
 {
-  //REGISTER( test_subgrid );
-  //REGISTER( test_bvpl_kernels );
-  //REGISTER( test_neighb_oper );
-  //REGISTER( test_bvpl_kernel_functors);
-  //REGISTER( test_direction_to_color_map);
+  REGISTER( test_subgrid );
+  REGISTER( test_bvpl_kernels );
+  REGISTER( test_neighb_oper );
+  REGISTER( test_bvpl_kernel_functors);
+  REGISTER( test_direction_to_color_map);
   REGISTER( test_bvpl_vector_operator);
 
 }
