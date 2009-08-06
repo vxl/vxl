15eec761ede39009dd3d12f04879ff99d4c6b0cf
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-08-06 18:05:02 -0400

    added EOLN at EOF
    SVN-Revision: 26001

--- contrib/brl/bseg/boct/boct_tree_cell_reader.h
+++ contrib/brl/bseg/boct/boct_tree_cell_reader.h
@@ -1,11 +1,9 @@
 #ifndef boct_tree_cell_reader_h_
 #define boct_tree_cell_reader_h_
-
 //:
 // \file
-// \brief  Sequential binary octree reader. next() methods returns the 
-//         leaf cells one by one until end of file is found.
-// 
+// \brief  Sequential binary octree reader. next() methods returns the leaf cells one by one until end of file is found.
+//
 // \author Gamze Tunali
 // \date   July 31, 2009
 // \verbatim
@@ -18,27 +16,27 @@
 #include "boct_tree_cell.h"
 
 template <class T_loc,class T_data>
-class boct_tree_cell_reader {
-public:
+class boct_tree_cell_reader
+{
+ public:
   boct_tree_cell_reader(vsl_b_istream* s) : is_(*s) {}
 
   ~boct_tree_cell_reader() {}
- 
+
   //boct_tree_cell_reader(boct_tree_cell_reader const &r) { is_= r.is_; }
 
   //: reads the tree header info and gets ready to read the cells
   void begin();
 
-  // returns the next leaf cell read from the binary file
+  //: returns the next leaf cell read from the binary file
   bool next(boct_tree_cell<T_loc,T_data>& c);
 
   void close() {/*is_.close(); */}
 
-private:
-  // input binary stream of octree
+ private:
+  //: input binary stream of octree
   vsl_b_istream is_;
 };
 
 
-
-#endif
+#endif // boct_tree_cell_reader_h_
