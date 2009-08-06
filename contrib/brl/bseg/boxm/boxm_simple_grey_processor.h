657831158b78f4c067a1704e3013a48ab5aa5063
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-08-06 18:07:03 -0400

    corrected the "This is" line
    SVN-Revision: 26004

--- contrib/brl/bseg/boxm/boxm_simple_grey_processor.h
+++ contrib/brl/bseg/boxm/boxm_simple_grey_processor.h
@@ -1,34 +1,31 @@
-// This is vxl/contrib/brl/bseg/boxm/boxm_simple_grey_processor.h
+// This is brl/bseg/boxm/boxm_simple_grey_processor.h
 #ifndef boxm_simple_grey_processor_h_
 #define boxm_simple_grey_processor_h_
 //:
 // \file
 // \brief A class for processing simple, single valued grey-scale appearance models.
-//  The represented distribution can be thought of as a delta function centered on the value. 
+//  The represented distribution can be thought of as a delta function centered on the value.
 //
 // \author Daniel Crispell
-// \date 09/29/2008
+// \date Sept 29, 2008
 // \verbatim
 //  Modifications
-//  
+//  <none yet>
 // \endverbatim
-
-
 
 #include "boxm_apm_traits.h"
 
-
 class  boxm_simple_grey_processor
 {
-protected:
+ protected:
   // convienance typedefs
   typedef boxm_apm_traits<BOXM_APM_SIMPLE_GREY>::apm_datatype apm_datatype;
   typedef boxm_apm_traits<BOXM_APM_SIMPLE_GREY>::obs_datatype obs_datatype;
   typedef boxm_apm_traits<BOXM_APM_SIMPLE_GREY>::obs_mathtype obs_mathtype;
-  
-  // one_over_sigma now a member of apm_datatype 
+
+  // one_over_sigma now a member of apm_datatype
   //static const float one_over_sigma_;
-public:
+ public:
 
   // all methods are static - no constructor needed
   //boxm_simple_grey_processor(){};
@@ -51,16 +48,14 @@ public:
 
   static void finalize_appearance(vcl_vector<boxm_apm_traits<BOXM_APM_SIMPLE_GREY>::obs_datatype> const& obs, vcl_vector<float> const& weights, boxm_apm_traits<BOXM_APM_SIMPLE_GREY>::apm_datatype &model);
 
-private:
+ private:
   static void compute_gaussian_params(vcl_vector<boxm_apm_traits<BOXM_APM_SIMPLE_GREY>::obs_datatype> obs, vcl_vector<float> weights, boxm_apm_traits<BOXM_APM_SIMPLE_GREY>::obs_datatype &mean, float &sigma);
 
   static float sigma_norm_factor(unsigned int nobs);
 
   static float sigma_norm_factor(float nobs);
-
 };
 
 //vcl_ostream& operator<<(vcl_ostream &os, boxm_simple_grey const& apm);
-
 
 #endif // boxm_simple_grey_processor_h_
