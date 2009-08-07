95f091460e6c7e4d46404425ee48fa715408958f
Joseph L. Mundy <mundyj@users.sourceforge.net>
2009-08-07 18:29:46 -0400

    removed main edge processes from voxel world
    SVN-Revision: 26012

---
+++ contrib/brl/bseg/bvxm/bvxm_voxel_world_sptr.h
@@ -0,0 +1,9 @@
+#ifndef bvxm_voxel_world_sptr_h
+#define bvxm_voxel_world_sptr_h
+
+
+
+#include <vbl/vbl_smart_ptr.h>
+class bvxm_voxel_world;
+typedef vbl_smart_ptr<bvxm_voxel_world> bvxm_voxel_world_sptr;
+#endif // bvxm_voxel_world_sptr_h
