2f6f10034a397d219b125c04287a8125d188517e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-08-11 10:55:51 -0400

    bug fix: non-void function must return a value
    SVN-Revision: 26039

--- contrib/brl/bseg/bvpl/bvpl_gauss3d_xx_kernel_factory.cxx
+++ contrib/brl/bseg/bvpl/bvpl_gauss3d_xx_kernel_factory.cxx
@@ -26,16 +26,16 @@ bvpl_gauss3d_xx_kernel_factory::bvpl_gauss3d_xx_kernel_factory(float s1, float s
   sigma1_ = s1;
   sigma2_ = s2;
   sigma3_ = s2;
-  
+
   //this skernel is symmetric around main axis
   angular_resolution_=0;
-  
+
   //set canonical axis to x-axis
   canonical_rotation_axis_[0] = 1.0f; canonical_rotation_axis_[1] = 0.0f; canonical_rotation_axis_[2] = 0.0f;
-  
+
   //since rotation around x-axis is symmetric, the parallel axis is of non importance
   canonical_parallel_axis_[0] = 0.0f; canonical_parallel_axis_[1] = 0.0f; canonical_parallel_axis_[2] = 0.0f;
-  
+
   //initialize variables
   angle_ = 0.0f;
   rotation_axis_ = canonical_rotation_axis_;
@@ -51,20 +51,20 @@ bvpl_gauss3d_xx_kernel_factory::bvpl_gauss3d_xx_kernel_factory(float s1, float s
   sigma1_ = s1;
   sigma2_ = s2;
   sigma3_ = s3;
-  
+
   //this value is a meant as a limit there is not theoretical meaning to it
   angular_resolution_= vnl_math::pi/16.0 ;
-  
+
   //set canonical axis to x-axis
   canonical_rotation_axis_[0] = 1.0f; canonical_rotation_axis_[1] = 0.0f; canonical_rotation_axis_[2] = 0.0f;
-  
+
   //parallel axis is the y-axis
   canonical_parallel_axis_[0] = 0.0f; canonical_parallel_axis_[1] = 1.0f; canonical_parallel_axis_[2] = 0.0f;
-  
+
   //initialize variables
   angle_ = 0.0f;
   rotation_axis_ = canonical_rotation_axis_;
-  
+
   //create the default kernel
   create_canonical();
 }
@@ -72,34 +72,29 @@ bvpl_gauss3d_xx_kernel_factory::bvpl_gauss3d_xx_kernel_factory(float s1, float s
 
 void bvpl_gauss3d_xx_kernel_factory::create_canonical()
 {
-
   float var1 = vcl_pow(sigma1_, 2);
   float var2 = vcl_pow(sigma2_, 2);
   float var3 = vcl_pow(sigma3_, 2);
-  
 
-  
   //The size of the kernel is limited. If widht or height of the kernel is too large,
   //the user should subsample the image/grid
   if ( (sigma1_ > max_size_) || (sigma2_ > max_size_) || (sigma3_ > max_size_) )
-   {
-     vcl_cerr<< "Warning, kernel is too large. You should subsample image. Processing may take a long time \n";
-   }
+  {
+    vcl_cerr<< "Warning, kernel is too large. You should subsample image. Processing may take a long time.\n";
+  }
 
-  
- 
   typedef vgl_point_3d<float> point_3d;
   typedef bvpl_kernel_dispatch dispatch;
-  
+
   int min_x= -1*(4.0*sigma1_);
   int max_x =(4.0*sigma1_);
   int min_y= -1*(4.0*sigma2_);
   int max_y =(4.0*sigma2_);
   int min_z=  -1*(4.0*sigma3_);
   int max_z =(4.0*sigma3_);
-  
+
   //Compute the first entry on the Hessian of a independent gaussian with variances (v1, v, v)
-  
+
   //constant expressions
   int dimension = 3;
   float w = vcl_sqrt(vcl_pow(2.0f*float(vnl_math::pi),dimension)* (1.0f/var1*var2*var3) );
@@ -108,26 +103,25 @@ void bvpl_gauss3d_xx_kernel_factory::create_canonical()
   for (int x=min_x; x<= max_x; x++)
   {
     float x_2_over_v1 = vcl_pow(float(x),2)/var1;
-    float diff_term =  one_over_v1 * (x_2_over_v1 - 1.0f) ; 
-    
-     for (int y= min_y; y<= max_y; y++)
+    float diff_term =  one_over_v1 * (x_2_over_v1 - 1.0f) ;
+
+    for (int y= min_y; y<= max_y; y++)
+    {
+      for (int z= min_z; z<= max_z; z++)
       {
-        for (int z= min_z; z<= max_z; z++)
-         {
-           float exponential = vcl_exp( -0.5 * (x_2_over_v1 + vcl_pow(float(y),2)/var2 + vcl_pow(float(z),2)/var3));
-           canonical_kernel_.push_back(vcl_pair<point_3d,dispatch>(point_3d(float(x),float(y),float(z)), dispatch((exponential/w)*diff_term)));        
-         }
+        float exponential = vcl_exp( -0.5 * (x_2_over_v1 + vcl_pow(float(y),2)/var2 + vcl_pow(float(z),2)/var3));
+        canonical_kernel_.push_back(vcl_pair<point_3d,dispatch>(point_3d(float(x),float(y),float(z)), dispatch((exponential/w)*diff_term)));
       }
-   }
-           
-                                       
+    }
+  }
+
   //set the dimension of the 3-d grid
   max3d_.set(max_x,max_y,max_z);
   min3d_.set(min_x,min_y,min_z);
-  
+
   //set the current kernel
   kernel_ = canonical_kernel_;
-  
+
   return;
 }
 
@@ -138,31 +132,35 @@ void bvpl_gauss3d_xx_kernel_factory::create_canonical()
 //  This uses spherical coordinates where \theta \in  [0,\pi) and \phi \in [0,2\pi)
 bvpl_kernel_vector_sptr bvpl_gauss3d_xx_kernel_factory::create_kernel_vector()
 {
-//  bvpl_kernel_vector_sptr kernels = new bvpl_kernel_vector();
-//  vnl_float_3 axis(0.0f, 0.0f, 1.0f);
-//  //polar phi=0,pi
-//  this->set_rotation_axis(axis);
-//  kernels->kernels_.push_back(vcl_make_pair(axis, new bvpl_kernel(this->create())));
-//  axis = -axis;
-//  this->set_rotation_axis(axis);
-//  kernels->kernels_.push_back(vcl_make_pair(axis, new bvpl_kernel(this->create())));
-//  
-//  double theta_res = vnl_math::pi_over_4;
-//  double phi_res   = vnl_math::pi_over_4;
-//  // phi=pi/4,pi/2,3pi/4
-//  for (double phi=vnl_math::pi_over_4; phi<vnl_math::pi-1e-5; phi+=phi_res)
-//   {
-//     // theta=0,pi/4,pi/2,3pi/4,pi,5pi/4,3pi/2,7pi/4
-//     for (double theta=0.0; theta<2.0*vnl_math::pi-1e-5; theta+=theta_res)
-//      {
-//        axis[0] = float(vcl_cos(theta) * vcl_sin(phi));
-//        axis[1] = float(vcl_sin(theta) * vcl_sin(phi));
-//        axis[2] = float(vcl_cos(phi));
-//        this->set_rotation_axis(axis);
-//        kernels->kernels_.push_back(vcl_make_pair(axis, new bvpl_kernel(this->create())));
-//      }
-//   }
-//  return kernels;
+#if 0
+  bvpl_kernel_vector_sptr kernels = new bvpl_kernel_vector();
+  vnl_float_3 axis(0.0f, 0.0f, 1.0f);
+  //polar phi=0,pi
+  this->set_rotation_axis(axis);
+  kernels->kernels_.push_back(vcl_make_pair(axis, new bvpl_kernel(this->create())));
+  axis = -axis;
+  this->set_rotation_axis(axis);
+  kernels->kernels_.push_back(vcl_make_pair(axis, new bvpl_kernel(this->create())));
+
+  double theta_res = vnl_math::pi_over_4;
+  double phi_res   = vnl_math::pi_over_4;
+  // phi=pi/4,pi/2,3pi/4
+  for (double phi=vnl_math::pi_over_4; phi<vnl_math::pi-1e-5; phi+=phi_res)
+  {
+    // theta=0,pi/4,pi/2,3pi/4,pi,5pi/4,3pi/2,7pi/4
+    for (double theta=0.0; theta<2.0*vnl_math::pi-1e-5; theta+=theta_res)
+    {
+      axis[0] = float(vcl_cos(theta) * vcl_sin(phi));
+      axis[1] = float(vcl_sin(theta) * vcl_sin(phi));
+      axis[2] = float(vcl_cos(phi));
+      this->set_rotation_axis(axis);
+      kernels->kernels_.push_back(vcl_make_pair(axis, new bvpl_kernel(this->create())));
+    }
+  }
+  return kernels;
+#else
+  return 0; // to be implemented
+#endif
 }
 
 //: Creates a vector of kernels according to given  azimuthal and elevation resolutio, and angle of rotation= angular_resolution_
