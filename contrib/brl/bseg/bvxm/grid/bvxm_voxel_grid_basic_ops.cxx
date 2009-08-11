c7b0db584728aa5bf6754a780a9f834912925f12
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-08-11 16:21:42 -0400

    added EOLN at EOF to avoid compiler warning
    SVN-Revision: 26049

--- contrib/brl/bseg/bvxm/grid/bvxm_voxel_grid_basic_ops.cxx
+++ contrib/brl/bseg/bvxm/grid/bvxm_voxel_grid_basic_ops.cxx
@@ -1,16 +1,23 @@
-#include <bvxm/grid/bvxm_voxel_grid_basic_ops.h>
+#include "bvxm_voxel_grid_basic_ops.h"
+//:
+// \file
+
+#include <imesh/imesh_vertex.h>
+#include <vgl/vgl_box_3d.h>
+#include <vgl/vgl_vector_3d.h>
+#include <vcl_list.h>
 
 //: function to convert a mesh in global coordinates to a mesh in local coordinates
 void covert_global_mesh_to_local(imesh_mesh & inmesh,bgeo_lvcs& lvcs)
 {
-    imesh_vertex_array<3>& in_verts=inmesh.vertices<3>() ;
-    for (unsigned i=0; i < in_verts.size(); ++i)
-    {
-        double lx, ly, lz;
-        lvcs.global_to_local(in_verts(i,0),in_verts(i,1),in_verts(i,2),bgeo_lvcs::wgs84,lx,ly,lz);
-        imesh_vertex<3> v(lx,ly,lz);
-        in_verts[i]=v;
-    }
+  imesh_vertex_array<3>& in_verts=inmesh.vertices<3>() ;
+  for (unsigned i=0; i < in_verts.size(); ++i)
+  {
+    double lx, ly, lz;
+    lvcs.global_to_local(in_verts(i,0),in_verts(i,1),in_verts(i,2),bgeo_lvcs::wgs84,lx,ly,lz);
+    imesh_vertex<3> v(lx,ly,lz);
+    in_verts[i]=v;
+  }
 }
 
 bool bvxm_load_mesh_normals_into_grid(bvxm_voxel_grid<vnl_vector_fixed<float,3> >* grid,imesh_mesh& mesh)
@@ -25,9 +32,9 @@ bool bvxm_load_mesh_normals_into_grid(bvxm_voxel_grid<vnl_vector_fixed<float,3> 
     vgl_box_3d<double> bb;
     for (unsigned j=0; j<fs.num_verts(i); ++j) {
       unsigned int v_id = fs(i,j);
-      vgl_point_3d<double> v(vertices(v_id,0), 
-                              vertices(v_id,1), 
-                              vertices(v_id,2));
+      vgl_point_3d<double> v(vertices(v_id,0),
+                             vertices(v_id,1),
+                             vertices(v_id,2));
       bb.add(v);
       v_list.push_back(v);
     }
