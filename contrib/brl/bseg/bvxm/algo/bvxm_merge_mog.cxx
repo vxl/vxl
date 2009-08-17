f8b213ea56312ed0ea2a27962c12e56760c9fb88
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-08-17 14:46:15 -0400

    SVN-Revision: 26087

--- contrib/brl/bseg/bvxm/algo/bvxm_merge_mog.cxx
+++ contrib/brl/bseg/bvxm/algo/bvxm_merge_mog.cxx
@@ -6,18 +6,26 @@
 #include <vcl_vector.h>
 #include <bvxm/grid/bvxm_voxel_grid.h>
 
-void bvxm_merge_mog::kl_merge(mix_gauss_type const& mixture, bsta_gauss_f1 &gaussian)
+void bvxm_merge_mog::kl_merge(bvxm_merge_mog::mix_gauss_type const& mixture, bsta_gauss_f1 &gaussian)
 {
   //When merging components of gaussian mixtures, we need to perform the followign calculations
   //1. find the normalizing weight of the new component. In this case this value is 1 because
   //   we are merging all the componets
   //2. find the mean
   //3. find the variance
-
+  
+  
+  //Find mean and variance
   float mean = 0.0f;
   float var = 0.0f;
-  //Find mean and variance
-  for (unsigned i = 0; i<mixture.num_components(); i++)
+  
+  //CAUTION: only mixtures of 3 modes are supported for now. The value is hardcoded, because when reading and writing the world
+  //mixture.num_components becomes curropted
+  
+  //unsigned num_components = mixture.num_components();
+  unsigned num_components = 3; 
+  
+  for (unsigned i = 0; i<num_components; i++) 
   {
     //note these equations ommit the weight of the final distribution
     // only becausee in this case it is 1
@@ -25,7 +33,8 @@ void bvxm_merge_mog::kl_merge(mix_gauss_type const& mixture, bsta_gauss_f1 &gaus
     var = var +  mixture.weight(i)*mixture.distribution(i).var() +
       mixture.weight(i)*vcl_pow(mixture.distribution(i).mean(),2);
   }
-
+  if(var <  vcl_pow(mean,2))
+    vcl_cout<< mixture <<vcl_endl;
   var = var - vcl_pow(mean,2);
 
   gaussian.set_mean(mean);
@@ -53,8 +62,36 @@ bool bvxm_merge_mog::kl_merge_grid(bvxm_voxel_grid_base_sptr apm_base,
 
     for (; apm_slab_it!=(*apm_it).end(); apm_slab_it++, gauss_slab_it++)
     {
+      mix_gauss_type temp = *apm_slab_it;
       kl_merge(*apm_slab_it, *gauss_slab_it);
     }
   }
   return true;
 }
+
+//: Converts a mog grid into a most probable mode grid
+bool bvxm_merge_mog::mpm_grid(bvxm_voxel_grid_base_sptr apm_base,
+                                   bvxm_voxel_grid_base_sptr gauss_base)
+{
+  //cast grids
+  bvxm_voxel_grid< mix_gauss_type >* apm_grid = static_cast<bvxm_voxel_grid< mix_gauss_type>* >(apm_base.ptr());
+  bvxm_voxel_grid< gauss_type >* gauss_grid = static_cast<bvxm_voxel_grid< gauss_type>* >(gauss_base.ptr());
+  
+  //iterate through the grid merging mixtures
+  bvxm_voxel_grid< mix_gauss_type >::iterator apm_it = apm_grid->begin();
+  bvxm_voxel_grid< gauss_type >::iterator gauss_it = gauss_grid->begin();
+  
+  for (unsigned z=0; z<apm_grid->grid_size().z(); ++z, ++apm_it, ++gauss_it)
+  {
+    bvxm_voxel_slab< mix_gauss_type >::iterator apm_slab_it = (*apm_it).begin();
+    bvxm_voxel_slab< gauss_type >::iterator gauss_slab_it = (*gauss_it).begin();
+    
+    for (; apm_slab_it!=(*apm_it).end(); apm_slab_it++, gauss_slab_it++)
+    {
+      //most probable mode
+      *gauss_slab_it = (*apm_slab_it).distribution(0);
+    }
+  }
+  return true;
+}
+
