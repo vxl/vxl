6e6bf88ffeccfc63f81ab67b1f79b59f5de78662
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-08-18 19:20:55 -0400

    added bsta_num_obs<bsta_gauss_f1>
    SVN-Revision: 26108

--- contrib/brl/bseg/bvpl/Templates/bvpl_voxel_subgrid+bsta_gauss_f1-.cxx
+++ contrib/brl/bseg/bvpl/Templates/bvpl_voxel_subgrid+bsta_gauss_f1-.cxx
@@ -1,5 +1,7 @@
 #include <bvpl/bvpl_voxel_subgrid.txx>
 #include <bsta/bsta_gauss_f1.h>
+#include <bsta/bsta_attributes.h>
 
 BVPL_VOXEL_SUBGRID_INSTANTIATE(bsta_gauss_f1);
-
+typedef bsta_num_obs<bsta_gauss_f1> gauss_type;
+BVPL_VOXEL_SUBGRID_INSTANTIATE(gauss_type);
