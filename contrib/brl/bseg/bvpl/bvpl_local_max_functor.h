7e698c0e4635a6ba1a99a4ba459b3313505891f6
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-08-18 19:19:11 -0400

    added more functions
    SVN-Revision: 26106

--- contrib/brl/bseg/bvpl/bvpl_local_max_functor.h
+++ contrib/brl/bseg/bvpl/bvpl_local_max_functor.h
@@ -14,6 +14,8 @@
 
 #include "bvpl_kernel_iterator.h"
 #include <bvxm/grid/bvxm_opinion.h>
+#include <bsta/bsta_gauss_f1.h>
+#include <bsta/bsta_attributes.h>
 
 template <class T>
 class bvpl_local_max_functor
@@ -30,8 +32,15 @@ class bvpl_local_max_functor
 
   //: Returns the final operation of this functor
   T result();
+ 
+  // Some additional functionalities 
+
+  bool greater_than(T& val1, T& val2);
+ 
+  static T min_response();
 
  private:
+  
   T max_;  // max_value over neighborhood
   T cur_val_;  // max_value over neighborhood
   //: Initializes class variables
@@ -75,11 +84,31 @@ T bvpl_local_max_functor<T>::result()
   return result;
 }
 
+template <class T>
+bool bvpl_local_max_functor<T>::greater_than(T& val1, T& val2)
+{
+  return val1 > val2;
+}
+
+template <class T>
+T bvpl_local_max_functor<T>::min_response()
+{
+  return T(0);
+}
+
+
 template <>
 void bvpl_local_max_functor<bvxm_opinion>::init()
 {
   max_=bvxm_opinion(1.0,0.0);
 }
+
+template <>
+void bvpl_local_max_functor<bsta_num_obs<bsta_gauss_f1> >::init()
+{
+  max_= bsta_gauss_f1(0.0f, 1.0f);
+}
+
 
 template <>
 bvxm_opinion bvpl_local_max_functor<bvxm_opinion>::result()
@@ -100,5 +129,18 @@ bvxm_opinion bvpl_local_max_functor<bvxm_opinion>::result()
   }
   //reset all variables
 }
+
+template <>
+bool bvpl_local_max_functor<bsta_num_obs<bsta_gauss_f1> >::greater_than(bsta_num_obs<bsta_gauss_f1>& g1, bsta_num_obs<bsta_gauss_f1>& g2)
+{
+  return g1.mean() > g2.mean();
+}
+
+template <>
+bsta_num_obs<bsta_gauss_f1> bvpl_local_max_functor<bsta_num_obs<bsta_gauss_f1> >::min_response()
+{
+  return bsta_gauss_f1(0.0f, 1.0f);
+}
+
 
 #endif
