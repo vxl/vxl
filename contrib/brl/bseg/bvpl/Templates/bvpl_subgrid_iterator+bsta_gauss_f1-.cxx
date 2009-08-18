2f2674a18e6db627ed339de66f53d799ffcf15eb
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-08-18 11:48:50 -0400

    template instantiations for bsta_gauss_f1
    SVN-Revision: 26095

---
+++ contrib/brl/bseg/bvpl/Templates/bvpl_subgrid_iterator+bsta_gauss_f1-.cxx
@@ -0,0 +1,6 @@
+#include <bvpl/bvpl_subgrid_iterator.txx>
+#include <bsta/bsta_gauss_f1.h>
+
+BVPL_SUBGRID_ITERATOR_INSTANTIATE(bsta_gauss_f1);
+
+
---
+++ contrib/brl/bseg/bvpl/Templates/bvpl_voxel_subgrid+bsta_gauss_f1-.cxx
@@ -0,0 +1,5 @@
+#include <bvpl/bvpl_voxel_subgrid.txx>
+#include <bsta/bsta_gauss_f1.h>
+
+BVPL_VOXEL_SUBGRID_INSTANTIATE(bsta_gauss_f1);
+
