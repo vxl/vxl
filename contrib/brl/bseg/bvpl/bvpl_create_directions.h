7c02d471e9dca2dd0782a4dbbb67d11d0e17d8f8
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-09-04 09:34:38 -0400

    moved common interface to base class
    SVN-Revision: 26252

--- contrib/brl/bseg/bvpl/bvpl_create_directions.h
+++ contrib/brl/bseg/bvpl/bvpl_create_directions.h
@@ -17,49 +17,49 @@
 #include <vnl/vnl_float_3.h>
 #include <vcl_iostream.h>
 #include <vcl_vector.h>
+#include <vcl_string.h>
 
+class bvpl_create_directions_base
+{
+ public:
+ bvpl_create_directions_base(){}
+ //:Destructor
+//  virtual ~bvpl_create_directions_base(){};
+
+ //:Accessor methods
+ vcl_vector<vnl_float_3> get_axes(){ return axes_;}
+ vcl_vector<float> get_angles(){ return angles_;}
+
+ //:Print to file
+ void save_directions_map(vcl_string outfile);
+  
+ protected:
+  vcl_vector<vnl_float_3> axes_;
+  vcl_vector<float> angles_;
+};
 //:
 // this class samples the
 // phi=0, theta=0;
 // phi=pi/4 theta = [0, 2\pi)
 // phi=pi/2 theta = [0, 2\pi)
-class bvpl_create_directions_a
+class bvpl_create_directions_a: public bvpl_create_directions_base
 {
  public:
   //: Default constructor
   bvpl_create_directions_a();
 
-  //: Destructor
-  ~bvpl_create_directions_a() {}
-
-  vcl_vector<vnl_float_3> get_axes(){ return axes_;}
-  vcl_vector<float> get_angles(){ return angles_;}
-
-  
- private:
-  vcl_vector<vnl_float_3> axes_;
-  vcl_vector<float> angles_;
 };
 
 //: This class samples
 // phi = 0, theta= 0;
 // phi = pi/4 theta =[0, 2*pi)
 // phi = pi/2 theta = [0, 3*pi/2)
-class bvpl_create_directions_b
+class bvpl_create_directions_b: public bvpl_create_directions_base
 {
  public:
   //: Default constructor
   bvpl_create_directions_b();
 
-  //: Destructor
-  ~bvpl_create_directions_b() {}
-
-  vcl_vector<vnl_float_3> get_axes(){ return axes_;}
-  vcl_vector<float> get_angles(){ return angles_;}
-
- private:
-  vcl_vector<vnl_float_3> axes_;
-  vcl_vector<float> angles_;
 };
 
 
@@ -68,20 +68,12 @@ class bvpl_create_directions_b
 // phi = pi/4 theta =[0, 2*pi)
 // phi = pi/2 theta = [0, 2*pi)
 // rotation around the axis with resolution pi/4
-class bvpl_create_directions_c
-  {
+class bvpl_create_directions_c: public bvpl_create_directions_base
+{
   public:
-    //: Default constructor
-    bvpl_create_directions_c();
-    
-    //: Destructor
-    ~bvpl_create_directions_c() {}
-    
-    vcl_vector<vnl_float_3> get_axes(){ return axes_;}
-    vcl_vector<float> get_angles(){ return angles_;}
-    
-  private:
-    vcl_vector<vnl_float_3> axes_;
-    vcl_vector<float> angles_;
-  };
+  //: Default constructor
+  bvpl_create_directions_c();
+
+};
+ 
 #endif //bvpl_create_directions_h_
