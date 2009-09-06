07620ca83c8a98d6d05d27c8952903312ce80025
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-09-06 11:54:29 -0400

    added explicit template instantiations
    SVN-Revision: 26290

---
+++ contrib/brl/bbas/imesh/algo/Templates/imesh_imls_surface+imesh_imls_surface+-integral_data.vgl_point_3d+double--.cxx
@@ -0,0 +1,3 @@
+#include <vgl/vgl_point_3d.h>
+#include <imesh/algo/imesh_imls_surface.txx>
+IMESH_IMLS_SURFACE_INSTANTATE(imesh_imls_surface::integral_data,vgl_point_3d<double>);
---
+++ contrib/brl/bbas/imesh/algo/Templates/imesh_imls_surface+vgl_vector_2d+double-.vgl_point_3d+double--.cxx
@@ -0,0 +1,4 @@
+#include <vgl/vgl_point_3d.h>
+#include <vgl/vgl_vector_2d.h>
+#include <imesh/algo/imesh_imls_surface.txx>
+IMESH_IMLS_SURFACE_INSTANTATE(vgl_vector_2d<double>,vgl_point_3d<double>);
