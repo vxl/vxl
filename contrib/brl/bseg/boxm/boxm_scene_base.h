e125c7f14263a37b617a3f40ce72f33dc9bb8ac8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-09-08 16:01:20 -0400

    bug fix: 3rd parameter was being ignored
    SVN-Revision: 26311

--- contrib/brl/bseg/boxm/boxm_scene_base.h
+++ contrib/brl/bseg/boxm/boxm_scene_base.h
@@ -23,8 +23,8 @@ class boxm_scene_base : public vbl_ref_count
 {
  public:
   boxm_scene_base()
-    : app_model_(BOXM_APM_UNKNOWN), multi_bin_(false), tree_level_set_(false), 
-	scene_path_(""), block_pref_(""), max_tree_level_(0), init_tree_level_(0) {}
+  : app_model_(BOXM_APM_UNKNOWN), multi_bin_(false), tree_level_set_(false),
+    scene_path_(""), block_pref_(""), max_tree_level_(0), init_tree_level_(0) {}
 
   virtual ~boxm_scene_base(){}
 
@@ -35,8 +35,8 @@ class boxm_scene_base : public vbl_ref_count
   void set_appearence_model(boxm_apm_type model) { app_model_ = model; }
 
   void set_bin_option(bool multi_bin) { multi_bin_ = multi_bin; }
-  void set_octree_levels(unsigned max, unsigned init, bool tree_level_set_=true)
-  { max_tree_level_=max; init_tree_level_=init; tree_level_set_=true; }
+  void set_octree_levels(unsigned max, unsigned init, bool tree_level_set=true)
+  { max_tree_level_=max; init_tree_level_=init; tree_level_set_=tree_level_set; }
 
   void set_paths(vcl_string scene_path, vcl_string block_prefix)
   { scene_path_ = scene_path;  block_pref_=block_prefix; }
@@ -66,4 +66,4 @@ class boxm_scene_base : public vbl_ref_count
 
 typedef vbl_smart_ptr<boxm_scene_base> boxm_scene_base_sptr;
 
-#endif
+#endif // boxm_scene_base_h_
