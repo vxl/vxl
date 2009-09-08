d96f7444c5067a388361ae236d5ccd1416625311
Gamze Tunali <gamze@users.sourceforge.net>
2009-09-08 10:49:10 -0400

    boxm_opt_rt_sample is moved up to boxm and renamed boxm_rt_sample
    SVN-Revision: 26305

--- contrib/brl/bseg/boxm/opt/Templates/boct_tree_cell+short.boxm_opt_rt_sample+float--.cxx
+++
@@ -1,4 +0,0 @@
-#include <boct/boct_tree_cell.txx>
-#include <boxm/boxm_rt_sample.h>
-
-BOCT_TREE_CELL_INSTANTIATE(short,boxm_rt_sample<float>);
--- contrib/brl/bseg/boxm/opt/Templates/boxm_scene+boct_tree+short.boxm_opt_rt_sample+float---.cxx
+++
@@ -1,6 +0,0 @@
-#include <boxm/boxm_scene.txx>
-#include <boct/boct_tree.h>
-#include "boxm/boxm_rt_sample.h"
-
-typedef boct_tree<short,boxm_rt_sample<float> > tree;
-BOXM_SCENE_INSTANTIATE(tree);
