47ed63798cd66b01fb72fedbb670eb98870b73e7
VXL Robots <vxl_robots@users.sourceforge.net>
2009-09-10 07:00:14 -0400

    daily version number
    SVN-Revision: 26335

--- core/vxl_version.h
+++ core/vxl_version.h
@@ -28,7 +28,7 @@
 // Formats are year=CCYY, month=MM, day=DD
 #define VXL_VERSION_DATE_YEAR 2009
 #define VXL_VERSION_DATE_MONTH 09
-#define VXL_VERSION_DATE_DAY 09
+#define VXL_VERSION_DATE_DAY 10
 
 //: Helper macros to create strings with the preprocessor.
 #define VXL_VERSION_TO_STRING(s) VXL_VERSION_TO_STRING0(s)
