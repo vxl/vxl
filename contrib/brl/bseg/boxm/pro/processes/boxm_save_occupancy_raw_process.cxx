aa5884fe6158812aa54801fecdb40dacd7981078
Gamze Tunali <gamze@users.sourceforge.net>
2009-09-11 15:31:41 -0400

    added the option writing the scene into one raw file
    SVN-Revision: 26395

--- contrib/brl/bseg/boxm/pro/processes/boxm_save_occupancy_raw_process.cxx
+++ contrib/brl/bseg/boxm/pro/processes/boxm_save_occupancy_raw_process.cxx
@@ -8,19 +8,23 @@
 // \date Apr 14, 2009
 // \verbatim
 //  Modifications
-//   <none yet>
+//    Sep 11, 2009   Gamze Tunali 
+//         Added scene writing option, input number is increased to 4 and if 4th
+//         input is 0, blocks saved as raw files seperately, if 1, then blocks 
+//         combined to generate 1 raw file for the whole scene
 // \endverbatim
 
 #include <vcl_fstream.h>
 #include <boxm/boxm_scene_base.h>
 #include <boxm/boxm_scene.h>
 #include <boxm/boxm_save_block_raw.h>
+#include <boxm/boxm_save_scene_raw.h>
 #include <boxm/boxm_apm_traits.h>
 #include <boxm/boxm_sample_multi_bin.h>
 
 namespace boxm_save_occupancy_raw_process_globals
 {
-  const unsigned n_inputs_ = 3;
+  const unsigned n_inputs_ = 4;
   const unsigned n_outputs_ = 0;
 }
 
@@ -31,10 +35,12 @@ bool boxm_save_occupancy_raw_process_cons(bprb_func_process& pro)
   //input[0]: scene binary file
   //input[1]: output file (raw) path
   //input[2]: the resolution level wanted.. 0 is the most detailed (lowest possible level)
+  //input[3]: 0 for writing blocks individually, 1 for writing the scene into one file
   vcl_vector<vcl_string> input_types_(n_inputs_);
   input_types_[0] = "boxm_scene_base_sptr";
   input_types_[1] = "vcl_string";
   input_types_[2] = "unsigned";
+  input_types_[3] = "unsigned";
   if (!pro.set_input_types(input_types_))
     return false;
 
@@ -55,7 +61,10 @@ bool boxm_save_occupancy_raw_process(bprb_func_process& pro)
   unsigned i = 0;
   boxm_scene_base_sptr scene_ptr = pro.get_input<boxm_scene_base_sptr>(i++);
   vcl_string filepath = pro.get_input<vcl_string>(i++);
-   unsigned resolution =  pro.get_input<unsigned>(i++);
+  unsigned resolution =  pro.get_input<unsigned>(i++);
+  unsigned whole = pro.get_input<unsigned>(i++);
+
+  
 
   // check the scene's app model
   if (scene_ptr->appearence_model() == BOXM_APM_MOG_GREY)
@@ -64,32 +73,41 @@ bool boxm_save_occupancy_raw_process(bprb_func_process& pro)
     {
       typedef boct_tree<short, boxm_sample_multi_bin<BOXM_APM_MOG_GREY> > type;
       boxm_scene<type>* scene = static_cast<boxm_scene<type>*>(scene_ptr.as_pointer());
-      boxm_block_iterator<type> it(scene);
-      it.begin();
-      while (!it.end()) {
-        vcl_stringstream strm;
-        vgl_point_3d<int> index = it.index();
-        strm << index.x() << '_' << index.y() << '_' << index.z();
-        vcl_string str(strm.str());
-        vcl_string s = filepath + str + ".raw";
-        boxm_save_block_raw<short, boxm_sample_multi_bin<BOXM_APM_MOG_GREY> >(*scene, it.index(), s, resolution);
-        it++;
+      if (!whole) {
+        boxm_block_iterator<type> it(scene);
+        it.begin();
+        while (!it.end()) {
+          vcl_stringstream strm;
+          vgl_point_3d<int> index = it.index();
+          strm << index.x() << '_' << index.y() << '_' << index.z();
+          vcl_string str(strm.str());
+          vcl_string s = filepath + str + ".raw";
+          boxm_save_block_raw<short, boxm_sample_multi_bin<BOXM_APM_MOG_GREY> >(*scene, it.index(), s, resolution);
+          it++;
+        }
+      } else { // write the whole scene
+        boxm_save_scene_raw<short, boxm_sample_multi_bin<BOXM_APM_MOG_GREY> >(*scene, filepath + ".raw", resolution);
       }
+
     }
     else
     {
       typedef boct_tree<short, boxm_sample<BOXM_APM_MOG_GREY> > type;
       boxm_scene<type>* scene = static_cast<boxm_scene<type>*>(scene_ptr.as_pointer());
-      boxm_block_iterator<type> it(scene);
-      it.begin();
-      while (!it.end()) {
-        vcl_stringstream strm;
-        vgl_point_3d<int> index = it.index();
-        strm << index.x() << '_' << index.y() << '_' << index.z();
-        vcl_string str(strm.str());
-        vcl_string s = filepath + str + ".raw";
-        boxm_save_block_raw<short,boxm_sample<BOXM_APM_MOG_GREY> >(*scene, it.index(), s, resolution);
-        it++;
+      if (!whole) {
+        boxm_block_iterator<type> it(scene);
+        it.begin();
+        while (!it.end()) {
+          vcl_stringstream strm;
+          vgl_point_3d<int> index = it.index();
+          strm << index.x() << '_' << index.y() << '_' << index.z();
+          vcl_string str(strm.str());
+          vcl_string s = filepath + str + ".raw";
+          boxm_save_block_raw<short,boxm_sample<BOXM_APM_MOG_GREY> >(*scene, it.index(), s, resolution);
+          it++;
+        }
+      } else { // write the whole scene
+        boxm_save_scene_raw<short,boxm_sample<BOXM_APM_MOG_GREY> >(*scene, filepath + ".raw", resolution);
       }
     }
   }
