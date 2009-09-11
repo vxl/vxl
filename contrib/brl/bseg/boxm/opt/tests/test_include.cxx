428bc2eea78724869cdc21f85fbee8a64b876462
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-09-11 12:57:13 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 26388

--- contrib/brl/bseg/boxm/opt/tests/test_include.cxx
+++ contrib/brl/bseg/boxm/opt/tests/test_include.cxx
@@ -1,9 +1,14 @@
 #include <boxm/opt/boxm_aux_scene.h>
 #include <boxm/opt/boxm_aux_traits.h>
 #include <boxm/opt/boxm_opt_appearance_estimator.h>
+#include <boxm/opt/boxm_opt_rt_bayesian_optimizer.h>
+#include <boxm/opt/boxm_raytrace_function.h>
+#include <boxm/opt/boxm_render_expected_depth_functor.h>
+#include <boxm/opt/boxm_render_expected_image_functor.h>
+#include <boxm/opt/boxm_rt_sample.h>
+#include <boxm/opt/boxm_update_image_functor.h>
+
 #include <boxm/opt/pro/boxm_opt_processes.h>
 #include <boxm/opt/pro/boxm_opt_register.h>
-#include <boxm/opt/boxm_opt_rt_bayesian_optimizer.h>
-#include <boxm/opt/boxm_opt_sample.h>
 
 int main() { return 0; }
