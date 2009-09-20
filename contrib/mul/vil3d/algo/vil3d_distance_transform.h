c76fbaf009f35c4c276a9309935cf992ad2e28c4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-09-20 07:05:08 -0400

    removed unused #include <vgl/vgl_vector_3d.h>
    SVN-Revision: 26518

--- contrib/mul/vil3d/algo/vil3d_distance_transform.h
+++ contrib/mul/vil3d/algo/vil3d_distance_transform.h
@@ -6,7 +6,6 @@
 // \author Kola Babalola
 
 #include <vil3d/vil3d_image_view.h>
-#include <vgl/vgl_vector_3d.h>
 #include <vil/vil_rgb.h>
 
 //: Compute 3d distance transform from zeros in original image.
@@ -34,8 +33,8 @@ void vil3d_distance_transform_one_way(vil3d_image_view<float>& image, const floa
 //  On exit, the values are the 26-connected distance to the
 //  nearest original zero region (or max_dist, if that is smaller).
 void vil_distance_transform(const vil3d_image_view<bool>& mask,
-              vil3d_image_view<float>& distance_image,
-              float max_dist);
+                            vil3d_image_view<float>& distance_image,
+                            float max_dist);
 
 //: Compute signed distance transform in 3d from zeros in original image.
 //  Image is assumed to be filled with max_dist
@@ -47,23 +46,23 @@ void vil_distance_transform(const vil3d_image_view<bool>& mask,
 // \param distance_link_i use when calculating connected distance between i and i+1
 // \param distance_link_j use when calculating connected distance between j and j+1
 // \param distance_link_k use when calculating connected distance between k and k+1
-void vil3d_signed_distance_transform(vil3d_image_view<float>& image, 
-                                     const float distance_link_i=1, 
-                                     const float distance_link_j=1, 
+void vil3d_signed_distance_transform(vil3d_image_view<float>& image,
+                                     const float distance_link_i=1,
+                                     const float distance_link_j=1,
                                      const float distance_link_k=1);
 
 float vil3d_min_equal(float const& a, float const& b, bool& eq);
 
-void vil3d_distance_transform_one_way_with_dir(vil3d_image_view<float>& image, 
-                                               vil3d_image_view<vil_rgb<float> >& orient, 
-                                               const float distance_link_i=1, 
-                                               const float distance_link_j=1, 
+void vil3d_distance_transform_one_way_with_dir(vil3d_image_view<float>& image,
+                                               vil3d_image_view<vil_rgb<float> >& orient,
+                                               const float distance_link_i=1,
+                                               const float distance_link_j=1,
                                                const float distance_link_k=1);
 
 void vil3d_distance_transform_with_dir(vil3d_image_view<float>& image,
                                        vil3d_image_view<vil_rgb<float> >& orient,
-                                       const float distance_link_i, 
-                                       const float distance_link_j, 
+                                       const float distance_link_i,
+                                       const float distance_link_j,
                                        const float distance_link_k);
 
 //: Compute 3d signed distance transform from true elements in mask.
@@ -75,10 +74,10 @@ void vil3d_distance_transform_with_dir(vil3d_image_view<float>& image,
 // \param distance_link_j use when calculating connected distance between j and j+1
 // \param distance_link_k use when calculating connected distance between k and k+1
 void vil3d_signed_distance_transform(const vil3d_image_view<bool>& mask,
-                   vil3d_image_view<float>& image,
-                   float max_dist,
-                   const float distance_link_i=1,
-                   const float distance_link_j=1,
-                   const float distance_link_k=1);
+                                     vil3d_image_view<float>& image,
+                                     float max_dist,
+                                     const float distance_link_i=1,
+                                     const float distance_link_j=1,
+                                     const float distance_link_k=1);
 
-#endif
+#endif // vil3d_distance_transform_h_
