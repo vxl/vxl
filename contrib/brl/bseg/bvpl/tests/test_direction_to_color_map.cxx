56047683b5a5d263ffdfcc93d70c495ba5f372d9
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-09-20 07:02:43 -0400

    removed unused #include <vul/vul_timer.h>
    SVN-Revision: 26503

--- contrib/brl/bseg/bvpl/tests/test_direction_to_color_map.cxx
+++ contrib/brl/bseg/bvpl/tests/test_direction_to_color_map.cxx
@@ -1,12 +1,10 @@
-//:
-// \file
 #include <testlib/testlib_test.h>
+
 #include <bvpl/bvpl_direction_to_color_map.h>
 #include <vgl/vgl_point_3d.h>
 #include <vcl_iostream.h>
-#include <vul/vul_timer.h>
 
-//: Test changes
+// Test changes
 static void test_direction_to_color_map()
 {
   double theta_res = vnl_math::pi_over_4;
@@ -26,7 +24,6 @@ static void test_direction_to_color_map()
   }
   bvpl_direction_to_color_map color_map(samples,"random");
   color_map.make_svg_color_map("cmap.svg");
-
 }
 
 TESTMAIN( test_direction_to_color_map );
