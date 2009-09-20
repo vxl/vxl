877f381d1200acb5f0902bd51effeaaab90ec54b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-09-20 07:00:10 -0400

    removed unused #include <vil/vil_image_view_base.h>, <vil/vil_image_view.h>, and <vil/vil_convert.h>
    SVN-Revision: 26485

--- contrib/brl/bseg/boxm/opt/pro/processes/boxm_opt_bayesian_update_process.cxx
+++ contrib/brl/bseg/boxm/opt/pro/processes/boxm_opt_bayesian_update_process.cxx
@@ -17,10 +17,6 @@
 #include <brdb/brdb_value.h>
 #include <bprb/bprb_parameters.h>
 
-#include <vil/vil_image_view_base.h>
-#include <vil/vil_image_view.h>
-#include <vil/vil_convert.h>
-
 #include <vpgl/vpgl_camera.h>
 #include <vpgl/vpgl_perspective_camera.h>
 
