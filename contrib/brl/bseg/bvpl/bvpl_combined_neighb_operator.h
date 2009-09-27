d3ab4807df06509d836b3420da38a10a02c5fdda
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-09-27 16:53:11 -0400

    added missing #include "bvpl_kernel.h"
    SVN-Revision: 26577

--- contrib/brl/bseg/bvpl/bvpl_combined_neighb_operator.h
+++ contrib/brl/bseg/bvpl/bvpl_combined_neighb_operator.h
@@ -1,25 +1,24 @@
 // This is//projects/vxl/src/contrib/brl/bseg/bvpl/bvpl_combined_neighb_operator.h
 #ifndef bvpl_combined_neighb_operator_h
 #define bvpl_combined_neighb_operator_h
-
 //:
 // \file
 // \brief A class for performing simultaneous neighborhood operations on a appearance and occupancy grids.
 // \author Isabel Restrepo mir@lems.brown.edu
-// \date  9/20/09
+// \date  September 20, 2009
+//
+// This class assumes occupancy=float and apperearance=unimodal gaussian. The class can be made more general
+// in the future but at moment of creation it is not clear if it will be necessary.
 //
 // \verbatim
 //  Modifications
 //   <none yet>
 // \endverbatim
 
-// This class assumes occupancy=float and apperearance=unimodal gaussian. The class can be made more general 
-// in the future but at moment of creation it is not clear if it will be necessary.
-
 #include "bvpl_subgrid_iterator.h"
 #include "bvpl_kernel_iterator.h"
 #include "bvpl_voxel_subgrid.h"
-
+#include "bvpl_kernel.h"
 
 #include <vgl/vgl_point_3d.h>
 
@@ -30,26 +29,26 @@
 template <class OCP_FUNC, class APP_FUNC>
 class bvpl_combined_neighb_operator
 {
-public:
+ public:
   bvpl_combined_neighb_operator(const OCP_FUNC ocp_functor, const APP_FUNC app_functor): ocp_func_(ocp_functor),app_func_(app_functor){}
   ~bvpl_combined_neighb_operator() {}
-  
+
   // makes the neighborhood operation and stores the result in the output subgrid, which may be equal to the input
   void operate(bvxm_voxel_grid<float>* ocp_grid,
                bvxm_voxel_grid<bsta_num_obs<bsta_gauss_f1> >* app_grid,
-               bvpl_kernel_sptr kernel, 
+               bvpl_kernel_sptr kernel,
                bvxm_voxel_grid<float>* out_grid)
   {
     bvpl_subgrid_iterator<float> ocp_iter(ocp_grid, kernel->dim());
     bvpl_subgrid_iterator<bsta_num_obs<bsta_gauss_f1> > app_iter(app_grid, kernel->dim());
     bvpl_subgrid_iterator<float> output_iter(out_grid, kernel->dim());
-    
+
     //kernel->print();
     while (!ocp_iter.isDone() && !app_iter.isDone()) {
       bvpl_kernel_iterator kernel_iter = kernel->iterator();
       bvpl_voxel_subgrid<float> ocp_subgrid = *ocp_iter;
       bvpl_voxel_subgrid<bsta_num_obs<bsta_gauss_f1> > app_subgrid = *app_iter;
-      
+
       //reset the kernel iterator
       kernel_iter.begin();
       while (!kernel_iter.isDone()) {
@@ -71,8 +70,8 @@ public:
       ++output_iter;
     }
   }
-  
-private:
+
+ private:
   OCP_FUNC ocp_func_;
   APP_FUNC app_func_;
 };
