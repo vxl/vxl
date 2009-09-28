e0bb87071a85d55c88318fe5fc9a66943e625bff
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-09-28 14:17:51 -0400

    implementation changes
    SVN-Revision: 26599

--- contrib/brl/bseg/bvpl/bvpl_neighb_operator.h
+++ contrib/brl/bseg/bvpl/bvpl_neighb_operator.h
@@ -30,8 +30,8 @@ class bvpl_neighb_operator
   // makes the neighborhood operation and stores the result in the output subgrid, which may be equal to the input
   void operate(bvxm_voxel_grid<T>* grid, bvpl_kernel_sptr kernel, bvxm_voxel_grid<T>* out_grid)
   {
-    bvpl_subgrid_iterator<T> subgrid_iter(grid, kernel->dim());
-    bvpl_subgrid_iterator<T> output_iter(out_grid, kernel->dim());
+    bvpl_subgrid_iterator<T> subgrid_iter(grid, kernel->max(), kernel->min());
+    bvpl_subgrid_iterator<T> output_iter(out_grid, kernel->max(), kernel->min());
     //kernel->print();
     while (!subgrid_iter.isDone()) {
        bvpl_kernel_iterator kernel_iter = kernel->iterator();
