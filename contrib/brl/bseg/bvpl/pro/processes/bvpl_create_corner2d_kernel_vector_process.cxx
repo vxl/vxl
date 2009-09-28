ebf302ffbfe62382ecbeb47c5a134111211d427c
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-09-28 14:01:11 -0400

    added one input
    SVN-Revision: 26581

--- contrib/brl/bseg/bvpl/pro/processes/bvpl_create_corner2d_kernel_vector_process.cxx
+++ contrib/brl/bseg/bvpl/pro/processes/bvpl_create_corner2d_kernel_vector_process.cxx
@@ -22,7 +22,7 @@
 
 namespace bvpl_create_corner2d_kernel_vector_process_globals
 {
-  const unsigned n_inputs_ = 3;
+  const unsigned n_inputs_ = 4;
   const unsigned n_outputs_ = 1;
 }
 
@@ -34,11 +34,13 @@ bool bvpl_create_corner2d_kernel_vector_process_cons(bprb_func_process& pro)
   //input[0]: Kernel length
   //input[1]: Kernel width
   //input[2]: Kernel thickness
+  //input[3]: String : type of kernel directions
   
   vcl_vector<vcl_string> input_types_(n_inputs_);
   input_types_[0] = "unsigned";
   input_types_[1] = "unsigned";
   input_types_[2] = "unsigned";
+  input_types_[3] = "vcl_string";
   
   if (!pro.set_input_types(input_types_))
     return false;
@@ -68,15 +70,26 @@ bool bvpl_create_corner2d_kernel_vector_process(bprb_func_process& pro)
   width = pro.get_input<unsigned int>(1);
   unsigned int thickness = 5;
   thickness = pro.get_input<unsigned int>(2);
-  
+  vcl_string dir_type =pro.get_input<vcl_string>(3);
   
   //Create the factory and get the vector of kernels
   bvpl_corner2d_kernel_factory factory(length,width,thickness);
   
-  //Note: This set o fdirections assume that length and width are equal
-  bvpl_create_directions_c dir;
-  bvpl_kernel_vector_sptr kernels = factory.create_kernel_vector(dir);
-  pro.set_output_val<bvpl_kernel_vector_sptr>(0, kernels);
+  if(dir_type == "main_corners"){
+    bvpl_main_corner_dirs dir;
+    bvpl_kernel_vector_sptr kernels = factory.create_kernel_vector(dir);
+    pro.set_output_val<bvpl_kernel_vector_sptr>(0, kernels);
+  }
+  if(dir_type == "main_plane"){
+    bvpl_main_plane_corner_dirs dir;
+    bvpl_kernel_vector_sptr kernels = factory.create_kernel_vector(dir);
+    pro.set_output_val<bvpl_kernel_vector_sptr>(0, kernels);
+  }
+  if(dir_type == "all_corners"){
+    bvpl_all_corner_dirs dir;
+    bvpl_kernel_vector_sptr kernels = factory.create_kernel_vector(dir);
+    pro.set_output_val<bvpl_kernel_vector_sptr>(0, kernels);
+  }
   
   
   return true;
