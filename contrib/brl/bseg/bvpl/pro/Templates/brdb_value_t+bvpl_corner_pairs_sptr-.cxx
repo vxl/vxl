739fd216581fdda751862c9cc0d50ccbfac6d933
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-09-28 17:20:29 -0400

    added missing template instantiations
    SVN-Revision: 26608

---
+++ contrib/brl/bseg/bvpl/pro/Templates/brdb_value_t+bvpl_corner_pairs_sptr-.cxx
@@ -0,0 +1,7 @@
+#include <brdb/brdb_value.txx>
+
+#include <vbl/io/vbl_io_smart_ptr.h>
+#include <bvpl/util/bvpl_corner_pair_finder.h>
+
+BRDB_VALUE_INSTANTIATE(bvpl_corner_pairs_sptr, "bvpl_corner_pairs_sptr");
+
---
+++ contrib/brl/bseg/bvpl/util/io/Templates/vbl_io_smart_ptr+bvpl_corner_pairs-.cxx
@@ -0,0 +1,5 @@
+#include <bvpl/util/io/bvpl_corner_pairs_io.h>
+#include <vbl/io/vbl_io_smart_ptr.txx>
+
+VBL_IO_SMART_PTR_INSTANTIATE(bvpl_corner_pairs);
+
