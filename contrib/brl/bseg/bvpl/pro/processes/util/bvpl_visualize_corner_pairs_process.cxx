00bc995070dc3ae1ce8491fb392bded92e72bdf7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-09-29 06:06:43 -0400

    avoid compiler warning (no EOLN at EOF)
    SVN-Revision: 26628

--- contrib/brl/bseg/bvpl/pro/processes/util/bvpl_visualize_corner_pairs_process.cxx
+++ contrib/brl/bseg/bvpl/pro/processes/util/bvpl_visualize_corner_pairs_process.cxx
@@ -28,15 +28,15 @@ namespace bvpl_find_corner_pairs_globals
   const unsigned n_outputs_ = 0;
 }
 
-//:Inputs
-// input[0]: The vector of corner pairs
-// input[1]: The index of lines to be displayed
-// input[2]: The path for output vrml file
-// input[3]: A flag to write vrml header and delete previous file or add info
-// input[4]: Hue value for the lines -a float in [0 1]
-
-//Outputs:
-// output[0]: A vector containing the lines connecting corners found
+//:
+// Inputs:
+// * input[0]: The vector of corner pairs
+// * input[1]: The index of lines to be displayed
+// * input[2]: The path for output vrml file
+// * input[3]: A flag to write vrml header and delete previous file or add info
+// * input[4]: Hue value for the lines -a float in [0 1]
+// Outputs:
+// * output[0]: A vector containing the lines connecting corners found
 
 bool bvpl_visualize_corner_pairs_process_cons(bprb_func_process& pro)
 {
@@ -47,23 +47,22 @@ bool bvpl_visualize_corner_pairs_process_cons(bprb_func_process& pro)
   input_types_[2] = "vcl_string";
   input_types_[3] = "bool";
   input_types_[4] = "float";
-  
-  
+
   vcl_vector<vcl_string> output_types_(n_outputs_);
-  
+
   return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
 }
 
 bool bvpl_visualize_corner_pairs_process(bprb_func_process& pro)
 {
   using namespace bvpl_find_corner_pairs_globals;
-  
+
   if (pro.n_inputs() != n_inputs_)
   {
     vcl_cout << pro.name() << " The input number should be " << n_inputs_<< vcl_endl;
     return false;
   }
-  
+
   //get inputs:
   unsigned i = 0;
   bvpl_corner_pairs_sptr pairs =pro.get_input<bvpl_corner_pairs_sptr>(i++);
@@ -71,11 +70,11 @@ bool bvpl_visualize_corner_pairs_process(bprb_func_process& pro)
   vcl_string vrml_path =pro.get_input<vcl_string>(i++);
   bool write_header = pro.get_input<bool>(i++);
   float hue = pro.get_input<float>(i++);
-  
+
   vcl_ofstream os;
-  
-  if(write_header){
-    if(vul_file::exists(vrml_path.c_str()))
+
+  if (write_header) {
+    if (vul_file::exists(vrml_path.c_str()))
       vpl_unlink(vrml_path.c_str());
     os.open(vrml_path.c_str(), vcl_ios::out);
     bvxm_vrml_voxel_grid::write_vrml_header(os);
@@ -83,10 +82,9 @@ bool bvpl_visualize_corner_pairs_process(bprb_func_process& pro)
   else {
     os.open(vrml_path.c_str(), vcl_ios::app);
   }
-  
-   for(unsigned j=0; j<pairs->pairs_[lines_id].size(); ++j)
-      bvxm_vrml_voxel_grid::write_vrml_line_segment(os, pairs->pairs_[lines_id][j]);
-  
-  
-  
+
+   for (unsigned j=0; j<pairs->pairs_[lines_id].size(); ++j)
+     bvxm_vrml_voxel_grid::write_vrml_line_segment(os, pairs->pairs_[lines_id][j]);
+
+  return true;
 }
--- contrib/brl/bseg/bvxm/grid/io/bvxm_vrml_voxel_grid.cxx
+++ contrib/brl/bseg/bvxm/grid/io/bvxm_vrml_voxel_grid.cxx
@@ -1,60 +1,56 @@
-//
-//\:File
-
 #include "bvxm_vrml_voxel_grid.h"
 
 
 void bvxm_vrml_voxel_grid::write_vrml_header(vcl_ofstream& str)
 {
   str << "#VRML V2.0 utf8\n"
-  << "Background {\n"
-  << "  skyColor [ 0 0 0 ]\n"
-  << "  groundColor [ 0 0 0 ]\n"
-  << "}\n"
-  << "PointLight {\n"
-  << "  on FALSE\n"
-  << "  intensity 1\n"
-  << "ambientIntensity 0\n"
-  << "color 1 1 1\n"
-  << "location 0 0 0\n"
-  << "attenuation 1 0 0\n"
-  << "radius 100\n"
-  << "}\n";
+      << "Background {\n"
+      << "  skyColor [ 0 0 0 ]\n"
+      << "  groundColor [ 0 0 0 ]\n"
+      << "}\n"
+      << "PointLight {\n"
+      << "  on FALSE\n"
+      << "  intensity 1\n"
+      << "ambientIntensity 0\n"
+      << "color 1 1 1\n"
+      << "location 0 0 0\n"
+      << "attenuation 1 0 0\n"
+      << "radius 100\n"
+      << "}\n";
 }
 
 
 void bvxm_vrml_voxel_grid::write_vrml_grid(vcl_ofstream& str, bvxm_voxel_grid<float> *grid, float threshold)
 {
   bvxm_voxel_grid<float>::iterator grid_it = grid->begin();
-  
+
   str << "Shape {\n"
-  << "  appearance NULL\n"
-  << "    geometry PointSet {\n"
-  << "      color Color{\n"
-  << "        color[\n";
+      << "  appearance NULL\n"
+      << "    geometry PointSet {\n"
+      << "      color Color{\n"
+      << "        color[\n";
   //write the colors
   for (unsigned k=0; grid_it != grid->end(); ++grid_it, ++k) {
     for (unsigned i=0; i<(*grid_it).nx(); ++i){
       for (unsigned j=0; j < (*grid_it).ny(); ++j) {
-        if(((*grid_it)(i,j) > threshold))
-           str <<"        " << (*grid_it)(i,j) << ' '<< (*grid_it)(i,j) << ' '<< (*grid_it)(i,j)
-          << "\n";
-      }           
+        if (((*grid_it)(i,j) > threshold))
+          str <<"        " << (*grid_it)(i,j) << ' '<< (*grid_it)(i,j) << ' '<< (*grid_it)(i,j)
+              << '\n';
+      }
     }
   }
   str << "        ]\n     }\n"
-  << "      coord Coordinate{\n"
-  << "        point[\n";
-  
+      << "      coord Coordinate{\n"
+      << "        point[\n";
+
   //write the coordinates
   grid_it = grid->begin();
   for (unsigned k=0; grid_it != grid->end(); ++grid_it, ++k) {
     vcl_cout << '.';
     for (unsigned i=0; i<(*grid_it).nx(); ++i) {
       for (unsigned j=0; j < (*grid_it).ny(); ++j) {
-        if((*grid_it)(i,j) > threshold)
-         str<<"        " << i << ' ' <<j << ' ' << k <<  "\n";
-        
+        if ((*grid_it)(i,j) > threshold)
+         str<<"        " << i << ' ' <<j << ' ' << k <<  '\n';
       }
     }
   }
@@ -65,18 +61,16 @@ void bvxm_vrml_voxel_grid::write_vrml_grid(vcl_ofstream& str, bvxm_voxel_grid<fl
 void bvxm_vrml_voxel_grid::write_vrml_grid_as_spheres(vcl_ofstream& str, bvxm_voxel_grid<float> *grid, float threshold)
 {
   bvxm_voxel_grid<float>::iterator grid_it = grid->begin();
-  
 
   //write the colors
   for (unsigned k=0; grid_it != grid->end(); ++grid_it, ++k) {
     for (unsigned i=0; i<(*grid_it).nx(); ++i){
       for (unsigned j=0; j < (*grid_it).ny(); ++j) {
-        if(((*grid_it)(i,j) > threshold)){
+        if (((*grid_it)(i,j) > threshold)){
           vgl_sphere_3d<float> sphere((float)i,(float)j,(float)k,0.1f);
           write_vrml_sphere(str, sphere, (*grid_it)(i,j),(*grid_it)(i,j),(*grid_it)(i,j),0);
         }
-           
-      }           
+      }
     }
   }
 }
@@ -84,20 +78,18 @@ void bvxm_vrml_voxel_grid::write_vrml_grid_as_spheres(vcl_ofstream& str, bvxm_vo
 void bvxm_vrml_voxel_grid::write_vrml_grid_as_spheres(vcl_ofstream& str, bvxm_voxel_grid<vnl_float_4> *grid, float threshold)
 {
   bvxm_voxel_grid<vnl_float_4>::iterator grid_it = grid->begin();
-  
-  
+
   //write the colors
   for (unsigned k=0; grid_it != grid->end(); ++grid_it, ++k) {
     for (unsigned i=0; i<(*grid_it).nx(); ++i){
       for (unsigned j=0; j < (*grid_it).ny(); ++j) {
         float temp = (*grid_it)(i,j)[3]/255.0;
-        if((*grid_it)(i,j)[3]/255.0 > threshold){
-          vcl_cout<< (*grid_it)(i,j)[3]<< ' '<< temp << vcl_endl;
+        if ((*grid_it)(i,j)[3]/255.0 > threshold){
+          vcl_cout << (*grid_it)(i,j)[3]<< ' '<< temp << vcl_endl;
           vgl_sphere_3d<float> sphere((float)i,(float)j,(float)k,0.1f);
           write_vrml_sphere(str, sphere, (*grid_it)(i,j)[0]/255.0f,(*grid_it)(i,j)[1]/255.0f,(*grid_it)(i,j)[2]/255.0f,1.0 - (*grid_it)(i,j)[3]/255.0f);
         }
-        
-      }           
+      }
     }
   }
 }
@@ -110,44 +102,42 @@ void bvxm_vrml_voxel_grid::write_vrml_sphere(vcl_ofstream& str, vgl_sphere_3d<fl
   double x0 = sphere.centre().x(), y0 = sphere.centre().y(), z0 = sphere.centre().z();
   double rad = sphere.radius();
   str << "Transform {\n"
-  << "translation " << x0 << ' ' << y0 << ' '
-  << ' ' << z0 << '\n'
-  << "children [\n"
-  << "Shape {\n"
-  << " appearance Appearance{\n"
-  << "   material Material\n"
-  << "    {\n"
-  << "      diffuseColor " << r << ' ' << g << ' ' << b << '\n'
-  << "      transparency " << transparency << '\n'
-  << "    }\n"
-  << "  }\n"
-  << " geometry Sphere\n"
-  <<   "{\n"
-  << "  radius " << rad << '\n'
-  <<  "   }\n"
-  <<  "  }\n"
-  <<  " ]\n"
-  << "}\n";
+      << "translation " << x0 << ' ' << y0 << ' '
+      << ' ' << z0 << '\n'
+      << "children [\n"
+      << "Shape {\n"
+      << " appearance Appearance{\n"
+      << "   material Material\n"
+      << "    {\n"
+      << "      diffuseColor " << r << ' ' << g << ' ' << b << '\n'
+      << "      transparency " << transparency << '\n'
+      << "    }\n"
+      << "  }\n"
+      << " geometry Sphere\n"
+      <<   "{\n"
+      << "  radius " << rad << '\n'
+      <<  "   }\n"
+      <<  "  }\n"
+      <<  " ]\n"
+      << "}\n";
 }
 
 void bvxm_vrml_voxel_grid::write_vrml_line_segment(vcl_ofstream& str, const vgl_line_segment_3d<int> &line)
 {
   str << "Shape {\n"
-  << " appearance Appearance{\n"
-  << "   material Material\n"
-  << "    {\n"
-  << "      emissiveColor " << 1 << ' ' << 0 << ' ' << 0 << '\n'
-  << "    }\n"
-  << "  }\n"
-  << " geometry IndexedLineSet\n"
-  <<   "{\n"
-  << "      coord Coordinate{\n"
-  << "       point[\n";
-  str <<line.point1().x() << ' ' << line.point1().y() << ' ' <<line.point1().z() << '\n';
-  str <<line.point2().x() << ' ' << line.point2().y() << ' ' <<line.point2().z() << '\n';
-  str << "   ]\n\n }"
-  << "   coordIndex [\n";
-  str << 0 << ',';
-  str << 1 << "   ]\n  }\n}";
-
+      << " appearance Appearance{\n"
+      << "   material Material\n"
+      << "    {\n"
+      << "      emissiveColor " << 1 << ' ' << 0 << ' ' << 0 << '\n'
+      << "    }\n"
+      << "  }\n"
+      << " geometry IndexedLineSet\n"
+      <<   "{\n"
+      << "      coord Coordinate{\n"
+      << "       point[\n"
+      << line.point1().x() << ' ' << line.point1().y() << ' ' <<line.point1().z() << '\n'
+      << line.point2().x() << ' ' << line.point2().y() << ' ' <<line.point2().z() << '\n'
+      << "   ]\n\n }"
+      << "   coordIndex [\n"
+      << "0,1   ]\n  }\n}";
 }
