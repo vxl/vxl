777f0fae711d306a8c4e818db67371b4e717e54d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-09-29 06:09:39 -0400

    Doxygen needs "\file"
    SVN-Revision: 26630

--- contrib/brl/bseg/bvpl/bvpl_find_surface_functor.cxx
+++ contrib/brl/bseg/bvpl/bvpl_find_surface_functor.cxx
@@ -1,11 +1,9 @@
-//
-//\:file
+//:
+// \file
 #include "bvpl_find_surface_functor.h"
 
 #include <vcl_cmath.h>
 #include <vcl_iostream.h>
-
-
 
 // Default constructor
 bvpl_find_surface_functor::bvpl_find_surface_functor()
@@ -16,36 +14,29 @@ bvpl_find_surface_functor::bvpl_find_surface_functor()
 //:Initializes all local variables
 void bvpl_find_surface_functor::init()
 {
-  
   P1_ = float(0);
   n1_=0;
 }
 
-
 void bvpl_find_surface_functor::apply(float& val, bvpl_kernel_dispatch& d)
 {
-  
   if (d.c_ > 0) {
     P1_ += vcl_log(val);
     n1_++;
   }
-  
 }
 
 float bvpl_find_surface_functor::result()
 {
-  
   P1_/=(float)n1_;
-  
+
   float result =0.0f;
-  
+
   if (vcl_exp(P1_) > 0.05f)
     result = 1.0f;
-  
+
   //reset all variables
   init();
-  
+
   return result;
 }
-
-
