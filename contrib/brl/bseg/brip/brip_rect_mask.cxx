f2ccd97d5aea7752ebc401acf2b3c3ea4d6a08dc
Joseph L. Mundy <mundyj@users.sourceforge.net>
2009-09-30 18:01:23 -0400

    Added mask types
    SVN-Revision: 26650

--- contrib/brl/bseg/brip/brip_rect_mask.cxx
+++ contrib/brl/bseg/brip/brip_rect_mask.cxx
@@ -2,6 +2,10 @@
 
 #include <vcl_iostream.h>
 
+vcl_map<brip_rect_mask::ang_id, vcl_string > brip_rect_mask::names_=vcl_map<brip_rect_mask::ang_id, vcl_string >();
+vcl_map<brip_rect_mask::ang_id, float > brip_rect_mask::angles_=vcl_map<brip_rect_mask::ang_id, float >();
+bool brip_rect_mask::is_init_ = false;
+
 //        0 0 - 0 0
 //        0 - + - 0
 //        - + + + -
@@ -497,8 +501,90 @@ static int c4_45_45_5x7[] =
     0,-1,1,1,1,-1,0,
     -1,1,1,1,1,1,-1
   };
+//====================== extrema ==========================
+//   0 - 0 
+//   - + -
+//   0 - 0
+static int m1a0_3x3[] =
+  { 0,-1, 0,
+    -1, 1, -1,
+    0, -1, 0 };
+
+//   - + - 
+//   + + +
+//   - + -
+static int m4a0_3x3[] =
+  { -1, 1,-1,
+     1, 1, 1,
+    -1, 1,-1 };
+
+//  0 0 - 0 0
+//  0 - + - 0
+//  - + + + -
+//  0 - + - 0
+//  0 0 - 0 0
+static int m4sa0_5x5[] =
+  {  0, 0,-1, 0, 0,
+     0,-1, 1,-1, 0,
+    -1, 1, 1, 1,-1,
+     0,-1, 1,-1, 0,
+     0, 0,-1, 0, 0};
+
+// 0 0 0 - 0 0 0
+// 0 0 - + - 0 0
+// 0 - + + + - 0
+// - + + + + + -
+// 0 - + + + - 0
+// 0 0 - + - 0 0
+// 0 0 0 - 0 0 0
+static int m7a0_7x7[] =
+  {  0, 0, 0,-1, 0, 0, 0,
+     0, 0,-1, 1,-1, 0, 0,
+     0,-1, 1, 1, 1,-1, 0,
+    -1, 1, 1, 1, 1, 1,-1,
+     0,-1, 1, 1, 1,-1, 0,
+     0, 0,-1, 1,-1, 0, 0,
+     0, 0, 0,-1, 0, 0, 0};
 
 //=======================   edges =========================
+
+//   0 - 0 
+//   0 + 0
+//   0 0 0
+static int e2a0_3x3[] =
+  { 0,-1, 0,
+    0, 1, 0,
+    0, 0, 0 };
+
+//    - 0 0
+//    0 + 0
+//    0 0 0
+static int e2a45_3x3[] =
+  { -1, 0, 0,
+     0, 1, 0,
+     0, 0, 0 };
+
+//        - - - 
+//        + + + 
+//        + + + 
+
+//
+static int e3a0_3x3[] =
+  {
+    -1,-1,-1,
+     1, 1, 1,
+     1, 1, 1
+  };
+
+//  0 0 - + +
+//  0 - + + 0 
+//  - + + 0 0
+static int e3a45_3x5[] =
+  {
+     0, 0,-1, 1, 1,
+     0,-1, 1, 1, 0,
+    -1, 1, 1, 0, 0
+  };
 
 //       - - - - -
 //       - - - - -
@@ -561,7 +647,75 @@ static int e11a0_5x11[] =
     1,1,1,1,1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,1,1,1,1
   };
+//======================== region types ======================
+//
+//  all foreground
+//   + + + + +
+//   + + + + +
+//   + + + + +
+//   + + + + +
+//   + + + + +
+static int f5a0_5x5[] =
+  { 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1
+ };
 
+//    0 + 0 
+//    + + + 
+//    0 + 0 
+static int f3a0_3x3[] =
+  {0,1,0,
+   1,1,1,
+   0,1,0 };
+
+//  all background
+//   - - - - -
+//   - - - - - 
+//   - - - - -
+//   - - - - -
+//   - - - - -
+static int b5a0_5x5[] =
+  { -1,-1,-1,-1,-1,
+    -1,-1,-1,-1,-1,
+    -1,-1,-1,-1,-1,
+    -1,-1,-1,-1,-1,
+    -1,-1,-1,-1,-1
+ };
+
+//  all background
+//  0 0 0 - 0 0 0
+//  0 0 - - - 0 0
+//  0 - - - - - 0
+//  - - - - - - -
+//  0 - - - - - 0
+//  0 0 - - - 0 0
+//  0 0 0 - 0 0 0
+static int b7a0_7x7[] =
+  {  0, 0, 0,-1, 0, 0, 0,
+     0, 0,-1,-1,-1, 0, 0,
+     0,-1,-1,-1,-1,-1, 0,
+    -1,-1,-1,-1,-1,-1,-1,
+     0,-1,-1,-1,-1,-1, 0,
+     0, 0,-1,-1,-1, 0, 0,
+     0, 0, 0,-1, 0, 0, 0,
+ };
+// 0 0 0 - 0 0 0
+// 0 0 - + - 0 0
+// 0 - + + + - 0
+// - + + + + + -
+// 0 - + + + - 0
+// 0 0 - + - 0 0
+// 0 0 0 - 0 0 0
+//    0 - 0  
+//    - - - 
+//    0 - 0 
+static int b3a0_3x3[] =
+  { 0,-1, 0,
+   -1,-1,-1,
+    0,-1, 0 };
 vnl_matrix<int> reflect_about_vert(vnl_matrix<int> const& m)
 {
   unsigned nc = m.cols(), nr = m.rows();
@@ -746,6 +900,30 @@ brip_rect_mask::brip_rect_mask(mask_id mid)
       masks_[a270] =  reflect_about_horz(c4_45_45a0).transpose();
       break;
     }
+    case e2:{
+      vnl_matrix<int> e2a0(e2a0_3x3, 3, 3), e2a45(e2a45_3x3, 3, 3);
+      masks_[a0] =    e2a0;
+      masks_[a45] =   e2a45;
+      masks_[a90]  =  e2a0.transpose();
+      masks_[a135]  =  reflect_about_horz(e2a45);
+      masks_[a180]  =  reflect_about_horz(e2a0);
+      masks_[a225] =  reflect_about_vert(reflect_about_horz(e2a45));
+      masks_[a270]  =  reflect_about_vert(e2a0.transpose());
+      masks_[a315]  =  reflect_about_vert(e2a45.transpose());
+      break;
+    }
+    case e3:{
+      vnl_matrix<int> e3a0(e3a0_3x3, 3, 3), e3a45(e3a45_3x5, 3, 5);
+      masks_[a0] =    e3a0;
+      masks_[a45] =   e3a45;
+      masks_[a90]  =  e3a0.transpose();
+      masks_[a135]  =  reflect_about_horz(e3a45);
+      masks_[a180]  =  reflect_about_horz(e3a0);
+      masks_[a225] =  reflect_about_vert(reflect_about_horz(e3a45));
+      masks_[a270]  =  reflect_about_vert(e3a0.transpose());
+      masks_[a315]  =  reflect_about_vert(e3a45.transpose());
+      break;
+    }
     case e5:{
       vnl_matrix<int> e5a0(e5a0_5x5, 5, 5), e5a45(e5a45_5x5, 5, 5);
       masks_[a0] =    e5a0;
@@ -764,6 +942,59 @@ brip_rect_mask::brip_rect_mask(mask_id mid)
       masks_[a90]  =  e11a0.transpose();
       masks_[a180]  =  reflect_about_horz(e11a0);
       masks_[a270]  =  reflect_about_vert(e11a0.transpose());
+      break;
+    }
+    case m1:{
+      vnl_matrix<int> m1a0(m1a0_3x3, 3, 3);
+      masks_[a0] =    m1a0;
+      break;
+    }
+
+	case m4:{
+      vnl_matrix<int> m4a0(m4a0_3x3, 3, 3);
+      masks_[a0] =    m4a0;
+      break;
+    }
+
+	case m4s:{
+      vnl_matrix<int> m4sa0(m4sa0_5x5, 5, 5);
+      masks_[a0] =    m4sa0;
+      break;
+    }
+	
+	case m7:{
+      vnl_matrix<int> m7a0(m7a0_7x7, 7, 7);
+      masks_[a0] =    m7a0;
+      break;
+    }
+
+	case f3:{
+      vnl_matrix<int> f3a0(f3a0_3x3, 3, 3);
+      masks_[a0] =    f3a0;
+      break;
+    }
+
+	case b3:{
+      vnl_matrix<int> b3a0(b3a0_3x3, 3, 3);
+      masks_[a0] =    b3a0;
+      break;
+    }
+
+	case f5:{
+      vnl_matrix<int> f5a0(f5a0_5x5, 5, 5);
+      masks_[a0] =    f5a0;
+      break;
+    }
+
+	case b5:{
+      vnl_matrix<int> b5a0(b5a0_5x5, 5, 5);
+      masks_[a0] =    b5a0;
+      break;
+    }
+
+	case b7:{
+      vnl_matrix<int> b7a0(b7a0_7x7, 7, 7);
+      masks_[a0] =    b7a0;
       break;
     }
 
@@ -802,6 +1033,145 @@ bool brip_rect_mask::set_angle_id(ang_id aid)
   ru_ = (current_mask_.cols()-1)/2;
   rv_ = (current_mask_.rows()-1)/2;
   return true;
+}
+
+void brip_rect_mask::init(){
+    if(!is_init_){
+      names_[a0]="0 deg";       angles_[a0]=0.0f;
+      names_[a22]="22.5 deg";   angles_[a22]=22.5f;
+      names_[a45]="45 deg";     angles_[a45]=45.0f;
+      names_[a67]="67.5 deg";   angles_[a67]=67.5f;
+      names_[a90]="90 deg";     angles_[a90]=90.0f;
+      names_[a112]="112.5 deg"; angles_[a112]=112.5f;
+      names_[a135]="135 deg";   angles_[a135]=135.0f;
+      names_[a157]="157.5 deg"; angles_[a157]=157.5f;
+      names_[a180]="180 deg";   angles_[a180]=180.0f;
+      names_[a202]="202.5 deg"; angles_[a202]=202.5f;
+      names_[a225]="225 deg";   angles_[a225]=225.0f;
+      names_[a247]="247.5 deg"; angles_[a247]=247.5f;
+      names_[a270]="270 deg";   angles_[a270]=270.0f;
+      names_[a292]="292.5 deg"; angles_[a292]=292.5f;
+      names_[a315]="315 deg";   angles_[a315]=315.0f;
+      names_[a337]="337.5 deg"; angles_[a337]=337.5f;
+	  is_init_ = true;
+    }
+  }
+
+vcl_string brip_rect_mask::name(ang_id aid)
+{
+  init();
+  vcl_map<ang_id, vcl_string >::const_iterator ait;
+  ait = names_.find(aid);
+  if (ait == names_.end())
+    return "null";
+  return (*ait).second;
+ }
+
+float brip_rect_mask::angle(ang_id aid)
+{
+  init();
+  vcl_map<ang_id, float >::const_iterator ait;
+  ait = angles_.find(aid);
+  if (ait == angles_.end())
+    return 0.0f;
+  return (*ait).second;
+ }
+
+unsigned brip_rect_mask::nplus() const
+{
+  unsigned np = 0;
+  vnl_matrix<int> const& weights = current_mask_;
+  for(vnl_matrix<int>::const_iterator wit = weights.begin();
+      wit != weights.end(); ++wit)
+    if((*wit)>0) ++np;
+  return np;
+}
+
+unsigned brip_rect_mask::nminus() const
+{
+  unsigned np = 0;
+  vnl_matrix<int> const& weights = current_mask_;
+  for(vnl_matrix<int>::const_iterator wit = weights.begin();
+      wit != weights.end(); ++wit)
+    if((*wit)<0) ++np;
+  return np;
+}
+
+float brip_rect_mask::ang_diff(ang_id aid0,ang_id aid1)
+{
+  float diff = brip_rect_mask::angle(aid1)-brip_rect_mask::angle(aid0);
+  if(diff>180.0f) diff-=360.0f;
+  if(diff<-180.0f) diff+=360.0f;
+  return diff;
+}
+
+bool brip_rect_mask::intersect(mask_id mid, 
+                               ang_id aid0, 
+                               unsigned short i0, unsigned short j0,
+                               ang_id aid1,
+                               unsigned short i1, unsigned short j1)
+{
+  brip_rect_mask m0(mid), m1(mid);
+  m0.set_angle_id(aid0);
+  m1.set_angle_id(aid1);
+  unsigned ni0 = m0.ni(), nj0 = m0.nj(), ni1 = m1.ni(), nj1 = m1.nj();
+  int  ru0 = (ni0-1)/2, rv0 = (nj0-1)/2;
+  int ru1 = (ni1-1)/2, rv1 = (nj1-1)/2;
+  for(int jr0 = -rv0; jr0<=rv0; ++jr0)
+    for(int ir0 = -ru0; ir0<=ru0; ++ir0)
+      if(m0(ir0,jr0)>0)
+          for(int jr1 = -rv1; jr1<=rv1; ++jr1)
+            for(int ir1 = -ru1; ir1<=ru1; ++ir1)
+              if(m1(ir1,jr1)>0)
+                if((i0+ir0)==(i1+ir1)&&(j0+jr0)==(j1+jr1))
+                  return true;
+  return false;
+}
+
+bool brip_rect_mask::intersect_domain(mask_id mid, 
+                      ang_id aid0, 
+                      unsigned short i0, unsigned short j0,
+                      ang_id aid1,
+                      unsigned short i1, unsigned short j1)
+{
+  brip_rect_mask m0(mid), m1(mid);
+  m0.set_angle_id(aid0);
+  m1.set_angle_id(aid1);
+  unsigned ni0 = m0.ni(), nj0 = m0.nj(), ni1 = m1.ni(), nj1 = m1.nj();
+  int  ru0 = (ni0-1)/2, rv0 = (nj0-1)/2;
+  int ru1 = (ni1-1)/2, rv1 = (nj1-1)/2;
+  for(int jr0 = -rv0; jr0<=rv0; ++jr0)
+    for(int ir0 = -ru0; ir0<=ru0; ++ir0)
+      for(int jr1 = -rv1; jr1<=rv1; ++jr1)
+        for(int ir1 = -ru1; ir1<=ru1; ++ir1)
+          if((i0+ir0)==(i1+ir1)&&(j0+jr0)==(j1+jr1))
+            return true;
+  return false;
+}
+void brip_rect_mask::print(ang_id aid)
+{
+  vcl_map<ang_id, vnl_matrix<int> >::const_iterator ait;
+  bool found = find_ait(aid, ait);
+  if(!found){
+    vcl_cout<< "no such angle\n";
+    return;
+  }
+  vnl_matrix<int> msk = (*ait).second;
+  int ri = (msk.cols()-1)/2, rj = (msk.rows()-1)/2;
+  for (int jr = -rj; jr<=rj ; ++jr){
+    for (int ir = -ri; ir<= ri; ++ir)
+      {
+        int m = msk(jr+rj, ir+ri);
+        if (m >0)
+          vcl_cout << '+' << ' ';
+        else if (m<0)
+          vcl_cout << '-' << ' ';
+        else
+          vcl_cout << '0' << ' ';
+      }
+      vcl_cout << '\n';
+    }
+  vcl_cout<< '\n';
 }
 
 vcl_ostream& operator<<(vcl_ostream& s, brip_rect_mask const& msk)
--- contrib/brl/bseg/brip/brip_rect_mask.h
+++ contrib/brl/bseg/brip/brip_rect_mask.h
@@ -22,6 +22,7 @@
 //       0 - + - 0 0 0
 //       0 - - 0 0 0 0
 //
+#include <vcl_string.h>
 #include <vcl_iosfwd.h>
 #include <vnl/vnl_matrix.h>
 #include <vcl_map.h>
@@ -31,12 +32,37 @@ class brip_rect_mask
  public:
   //: construct from radius, aspect ratio and orientation (degrees)
   enum mask_id { cr1 = 0, s1, b0, b1, b0s, b1s, r10, r30, r31, r32, r51, r52,
-                 c4_90_0, c4_45_0, c4_45_45, e5, e11, ntypes};
-  enum ang_id {a0 = 0, a22, a45, a67, a90, a112, a135, a157, a180, a225,
-               a270, a315, nangles};
+                 c4_90_0, c4_45_0, c4_45_45, e2, e3, e5, e11, m1, m4, m4s,m7,
+                 f3, b3, f5, b5, b7, ntypes};
+  enum ang_id {a0 = 0, a22, a45, a67, a90, a112, a135, a157, a180,a202, a225,
+               a247, a270, a292, a315, a337, nangles};
   brip_rect_mask(mask_id mid);
   ~brip_rect_mask() {}
 
+  //: initialize static quantities
+  static void init();
+
+  //: string name of angle id
+  static vcl_string name(ang_id aid);
+
+  //: floating point angle from id
+  static float angle(ang_id aid);
+
+  //: angle difference angle(aid1)-angle(aid0)
+  static float ang_diff(ang_id aid0,ang_id aid1);
+
+  //: Intersect '+' symbols of two masks of the same type
+  static bool intersect(mask_id mid, 
+                        ang_id aid0, 
+                        unsigned short i0, unsigned short j0,
+                        ang_id aid1,
+                        unsigned short i1, unsigned short j1);
+  //: Intersect full domain of two masks of the same type (0, ni, 0-nj)
+  static bool intersect_domain(mask_id mid, 
+                               ang_id aid0, 
+                               unsigned short i0, unsigned short j0,
+                               ang_id aid1,
+                               unsigned short i1, unsigned short j1);
   //: the number of columns needed to span the mask
   unsigned ni() const {return current_mask_.cols();}
 
@@ -49,6 +75,11 @@ class brip_rect_mask
   //: the orientation code
   ang_id angle_id(unsigned angle_index) const;
 
+  //: the number of plus symbols for the current mask
+  unsigned nplus() const;
+  //: the number of minus symbols for the current mask
+  unsigned nminus() const;
+
   //: set current angle id
   bool set_angle_id(ang_id aid);
 
@@ -59,7 +90,8 @@ class brip_rect_mask
     if (dv>rv_||dv<-rv_) return 0;
     return current_mask_[dv+rv_][du+ru_];
   }
-
+  //: print the mask with angle id, aid
+  void print(ang_id aid);
  protected:
   bool find_ait(ang_id aid,
                 vcl_map<ang_id, vnl_matrix<int> >::const_iterator& ait) const;
@@ -67,6 +99,9 @@ class brip_rect_mask
   vnl_matrix<int> current_mask_;
   int ru_, rv_;
   vcl_map<ang_id, vnl_matrix<int> > masks_;
+  static vcl_map<ang_id, vcl_string > names_;
+  static vcl_map<ang_id, float > angles_;
+  static bool is_init_;
 };
 
 vcl_ostream& operator<<(vcl_ostream& s, brip_rect_mask const& m);
