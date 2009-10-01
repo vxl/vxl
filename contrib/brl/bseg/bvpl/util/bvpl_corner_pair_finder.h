cc00f226ed8b7a81081ac4f6b5905922ee84e7d7
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-10-01 15:01:08 -0400

    minor changes
    SVN-Revision: 26674

--- contrib/brl/bseg/bvpl/util/bvpl_corner_pair_finder.h
+++ contrib/brl/bseg/bvpl/util/bvpl_corner_pair_finder.h
@@ -17,6 +17,7 @@
 #include <bvpl/bvpl_kernel.h>
 #include <bvxm/grid/bvxm_voxel_grid.h>
 #include <vgl/vgl_line_segment_3d.h>
+#include <vgl/vgl_box_3d.h>
 
 
 class bvpl_corner_pairs: public vbl_ref_count
@@ -29,10 +30,21 @@ public:
   //: Constructor from a vector
   bvpl_corner_pairs(vcl_vector<vcl_vector<vgl_line_segment_3d<int> > >& all_lines) { pairs_ = all_lines;}
   
+  //: Constructor from  2 vectors
+  bvpl_corner_pairs(vcl_vector<vcl_vector<vgl_line_segment_3d<int> > >& all_lines,
+                    vcl_vector<vcl_vector<vgl_box_3d<int> > > all_boxes)
+  { 
+    pairs_ = all_lines;
+    boxes_ =all_boxes;
+  }
+  
   int size() {return pairs_.size(); }
   
-  //: vector of kernel
+  //: vector of pairs
   vcl_vector<vcl_vector<vgl_line_segment_3d<int> > > pairs_;
+  
+  // a vector of kernel coverage at certain points
+  vcl_vector<vcl_vector<vgl_box_3d<int> > > boxes_;
   
 };
 
@@ -41,13 +53,20 @@ typedef vbl_smart_ptr<bvpl_corner_pairs> bvpl_corner_pairs_sptr;
 class bvpl_corner_pair_finder
 {
 public:
-  //:Construnctor 
-  bvpl_corner_pair_finder(){}
-  
-  //:Find pairs that are 90 degrees appart e.g |_ with _|
-  static bvpl_corner_pairs_sptr find_pairs(bvxm_voxel_grid<int>* id_grid, bvpl_kernel_vector_sptr search_kernels,
-                  bvpl_kernel_vector_sptr corner_kernels);
-  
+	//:Construnctor 
+	bvpl_corner_pair_finder(){}
+	
+	//:Find pairs that are 90 degrees appart e.g |_ with _|
+	static bvpl_corner_pairs_sptr find_pairs(bvxm_voxel_grid<int>* id_grid, 
+                                           bvpl_kernel_vector_sptr search_kernels,
+											                     bvpl_kernel_vector_sptr corner_kernels);
+	
+	//:Find pairs that are 90 degrees appart e.g |_ with _|
+	static bvpl_corner_pairs_sptr find_pairs(bvxm_voxel_grid<int>* id_grid, 
+											                     bvxm_voxel_grid<float>* response_grid,
+											                     bvpl_kernel_vector_sptr search_kernels,
+											                     bvpl_kernel_vector_sptr corner_kernels);
+	
 };
 
 
