4455e6def1ac21b04f0e4208d8b0ccfe70539d6d
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-11-04 15:38:22 -0500

    impl changes
    SVN-Revision: 26841

---
+++ contrib/brl/bseg/bvpl/util/Templates/bvxm_voxel_x+bvpl_pair-.cxx
@@ -0,0 +1,20 @@
+//These instantiations are all together because, if one class is created, the other ones are needed
+//Please leave them together.
+#include <bvxm/grid/bvxm_voxel_grid.txx>
+#include <bvxm/grid/bvxm_voxel_slab.txx>
+#include <bvxm/grid/bvxm_voxel_slab_iterator.txx>
+#include <bvxm/grid/bvxm_voxel_storage_disk.txx>
+#include <bvxm/grid/bvxm_voxel_storage_disk_cached.txx>
+#include <bvxm/grid/bvxm_voxel_storage_mem.txx>
+#include <bvpl/util/bvpl_corner_pair_finder.h>
+#include <bvpl/bvpl_subgrid_iterator.txx>
+#include <bvpl/bvpl_voxel_subgrid.txx>
+
+BVXM_VOXEL_GRID_INSTANTIATE(bvpl_pair);
+BVXM_VOXEL_SLAB_INSTANTIATE(bvpl_pair);
+BVXM_VOXEL_SLAB_ITERATOR_INSTANTIATE(bvpl_pair);
+BVXM_VOXEL_STORAGE_DISK_INSTANTIATE(bvpl_pair);
+BVXM_VOXEL_STORAGE_DISK_CACHED_INSTANTIATE(bvpl_pair);
+BVXM_VOXEL_STORAGE_MEM_INSTANTIATE(bvpl_pair);
+BVPL_SUBGRID_ITERATOR_INSTANTIATE(bvpl_pair);
+BVPL_VOXEL_SUBGRID_INSTANTIATE(bvpl_pair);
--- contrib/brl/bseg/bvpl/util/Templates/vbl_smart_ptr+bvpl_corner_pairs-.cxx
+++ contrib/brl/bseg/bvpl/util/Templates/vbl_smart_ptr+bvpl_corner_pairs-.cxx
@@ -1,4 +1,6 @@
 #include <bvpl/util/bvpl_corner_pair_finder.h>
+
 #include <vbl/vbl_smart_ptr.txx>
+
 
 VBL_SMART_PTR_INSTANTIATE(bvpl_corner_pairs);
