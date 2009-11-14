35caba367e9fac8ae6e0d6743abb36c9a6d6758d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-11-14 05:54:06 -0500

    renamed template instantiation file, in correspondance with majority of similar instantiations
    SVN-Revision: 26899

---
+++ contrib/gel/gkll/Templates/vbl_smart_ptr+gkll_multi_view_data+vtol_vertex_2d_sptr--.cxx
@@ -0,0 +1,4 @@
+#include <gkll/gkll_multi_view_data.h>
+#include <vtol/vtol_vertex_2d_sptr.h>
+#include <vbl/vbl_smart_ptr.txx>
+VBL_SMART_PTR_INSTANTIATE(gkll_multi_view_data<vtol_vertex_2d_sptr>);
