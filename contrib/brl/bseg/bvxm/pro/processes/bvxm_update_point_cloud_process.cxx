7ebaad4e9c8f4a4fa1b1b6f2c881b570c57adc51
Vishal Jain <jainv@users.sourceforge.net>
2009-11-16 12:09:31 -0500

    added a process to ingest point clouds in bvxm_voxel_grid.
    SVN-Revision: 26932

---
+++ contrib/brl/bseg/bvxm/pro/processes/bvxm_update_point_cloud_process.cxx
@@ -0,0 +1,96 @@
+//This is brl/bseg/bvxm/pro/processes/bvxm_update_point_cloud_process.cxx
+
+//:
+// \file
+#include <brdb/brdb_value.h>
+#include <bprb/bprb_parameters.h>
+
+#include <vil/vil_image_view_base.h>
+#include <vpgl/vpgl_camera.h>
+#include <vgl/vgl_point_3d.h>
+
+#include <bvxm/bvxm_voxel_world.h>
+#include <bvxm/bvxm_image_metadata.h>
+#include <bvxm/bvxm_mog_grey_processor.h>
+#include "bvxm_update_point_cloud_process.h"
+
+struct compare_point_3d
+{
+bool operator()(const vgl_point_3d<float> &a, vgl_point_3d<float>  &b) const
+{
+return a.z() > b.z();
+}
+};
+bool bvxm_update_point_cloud_process_cons(bprb_func_process& pro)
+{
+  using namespace bvxm_update_point_cloud_process_globals;
+
+  //process takes 4inputs
+  //input[0]: filename for input point cloud (x y z)
+  //input[1]: filename for the voxel grid
+  //input[2]: spacing in x-y
+  //input[3]: spacing in z
+  //input[4]: use opinion ?
+  vcl_vector<vcl_string> input_types_(n_inputs_);
+  input_types_[0] = "vcl_string";
+  input_types_[1] = "bvxm_voxel_world_sptr";
+  input_types_[2] = "float";
+  input_types_[3] = "float";
+  input_types_[4] = "bool";
+  if (!pro.set_input_types(input_types_))
+    return false;
+
+
+  return true;
+}
+
+bool bvxm_update_point_cloud_process(bprb_func_process& pro)
+{
+  using namespace bvxm_update_point_cloud_process_globals;
+
+  //check number of inputs
+  if (pro.n_inputs()<n_inputs_)
+  {
+    vcl_cout << pro.name() << " The input number should be " << n_inputs_<< vcl_endl;
+    return false;
+  }
+
+  //get inputs
+  unsigned i = 0;
+  vcl_string point_filename = pro.get_input<vcl_string>(i++);
+  bvxm_voxel_world_sptr world = pro.get_input<bvxm_voxel_world_sptr>(i++);
+  float xy_spacing = pro.get_input<float>(i++);
+  float z_spacing = pro.get_input<float>(i++);
+  bool use_opinion = pro.get_input<bool>(i++);
+
+  vcl_ifstream ifile(point_filename.c_str());
+  if(!ifile)
+  {
+    vcl_cout<<"Failed to open "<<point_filename<<vcl_endl;
+    return false;
+  }
+
+  vcl_vector<vgl_point_3d<float> > point_cloud;
+  float x,y,z;
+  
+  //: read the file to obtain min, max
+  while(ifile)
+  {
+      
+      ifile>>x>>y>>z;
+      vgl_point_3d<float> p3d(x,y,z);
+      point_cloud.push_back(p3d);
+  }
+  vcl_sort(point_cloud.begin(), point_cloud.end(), compare_point_3d());
+  bool result=true;
+  if (!use_opinion)
+  {
+      result =result && world->update_point_cloud<OCCUPANCY>(point_cloud);
+  }
+  else {
+      vcl_cout << "Working with opinion!" << vcl_endl;
+      result =result && world->update_point_cloud<OCCUPANCY_OPINION>(point_cloud);
+  } 
+
+  return result;
+}
---
+++ contrib/brl/bseg/bvxm/pro/processes/bvxm_update_point_cloud_process.h
@@ -0,0 +1,26 @@
+// This is brl/bseg/bvxm/pro/processes/bvxm_update_point_cloud_process.h
+#ifndef bvxm_update_point_cloud_process_h_
+#define bvxm_update_point_cloud_process_h_
+//:
+// \file
+// \brief A class for update process of a voxel world.
+//
+// \author Vishal JAin
+// \date Nov 11, 2009
+// \verbatim
+// \endverbatim
+
+#include <bprb/bprb_func_process.h>
+
+//: global variables
+namespace bvxm_update_point_cloud_process_globals
+{
+  const unsigned n_inputs_ = 5;
+  const unsigned n_outputs_ = 0;
+}
+
+bool bvxm_update_point_cloud_process_cons(bprb_func_process& pro);
+
+bool bvxm_update_point_cloud_process(bprb_func_process& pro);
+
+#endif // bvxm_update_point_cloud_process_h_
