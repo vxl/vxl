ce018c11daf1483f0d8355f76f527332736720bc
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-11-19 18:30:21 -0500

    added support for apple platform
    SVN-Revision: 26965

--- contrib/brl/bmods/find_open_cl.cmake
+++ contrib/brl/bmods/find_open_cl.cmake
@@ -3,21 +3,30 @@
 # http://developer.amd.com/gpu/ATIStreamSDKBetaProgram/Pages/default.aspx
 # Undoubtedly a different setup is required for the Nvidia SDK
 #
-SET(OPENCL_FOUND "NO")
-FIND_PATH( OPENCL_INCLUDE_PATH calcl.h )
+IF(WIN_32)
+	SET(OPENCL_FOUND "NO")
+	FIND_PATH( OPENCL_INCLUDE_PATH calcl.h )
+	
+	IF(OPENCL_INCLUDE_PATH)
+	
+	 FIND_PATH( OPENCL_LIBRARY_PATH aticalcl.lib )
+	
+	  IF(OPENCL_LIBRARY_PATH)
+		 SET(OPENCL_FOUND "YES")
+		 SET( OPENCL_LIBRARIES 
+			  ${OPENCL_LIBRARY_PATH}/aticalcl.lib
+			  ${OPENCL_LIBRARY_PATH}/amuabi.lib
+			  ${OPENCL_LIBRARY_PATH}/aticalrt.lib 
+			  ${OPENCL_LIBRARY_PATH}/x86/OpenCL.lib)
+	  ENDIF(OPENCL_LIBRARY_PATH)
+	ENDIF(OPENCL_INCLUDE_PATH)
+ENDIF (WIN_32)
 
-IF(OPENCL_INCLUDE_PATH)
 
- FIND_PATH( OPENCL_LIBRARY_PATH aticalcl.lib )
+IF (APPLE)
+  SET (OPENCL_FOUND "YES")
+  SET (OPENCL_LIBRARIES "-framework OpenCL" CACHE STRING "OpenCL lib for OSX")
+ENDIF (APPLE)
 
-  IF(OPENCL_LIBRARY_PATH)
-     SET(OPENCL_FOUND "YES")
-     SET( OPENCL_LIBRARIES 
-          ${OPENCL_LIBRARY_PATH}/aticalcl.lib
-          ${OPENCL_LIBRARY_PATH}/amuabi.lib
-          ${OPENCL_LIBRARY_PATH}/aticalrt.lib 
-          ${OPENCL_LIBRARY_PATH}/x86/OpenCL.lib)
-  ENDIF(OPENCL_LIBRARY_PATH)
-ENDIF(OPENCL_INCLUDE_PATH)
 MESSAGE("Found Open Cl?")
 MESSAGE(${OPENCL_FOUND})
