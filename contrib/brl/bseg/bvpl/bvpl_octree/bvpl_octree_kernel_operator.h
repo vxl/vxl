4cda1125b1f7fb2534e9710655689cd1d14e76c0
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-11-21 10:31:13 -0500

    Ambiguous \date
    SVN-Revision: 26998

--- contrib/brl/bseg/bvpl/bvpl_octree/bvpl_octree_kernel_operator.h
+++ contrib/brl/bseg/bvpl/bvpl_octree/bvpl_octree_kernel_operator.h
@@ -1,12 +1,11 @@
-// This is bvpl_octree_kernel_operator.h
+// This is brl/bseg/bvpl/bvpl_octree/bvpl_octree_kernel_operator.h
 #ifndef bvpl_octree_kernel_operator_h
 #define bvpl_octree_kernel_operator_h
-
 //:
 // \file
-// \brief A class that operates a bvpl_kernel on a octree 
+// \brief A class that operates a bvpl_kernel on a octree
 // \author Isabel Restrepo mir@lems.brown.edu
-// \date  11/13/09
+// \date  November 13, 2009
 //
 // \verbatim
 //  Modifications
@@ -20,63 +19,60 @@
 template<class T, class F>
 class bvpl_octree_kernel_operator
 {
-public:
-  
+ public:
+
   //Constructor from a bvpl functor
-  bvpl_octree_kernel_operator(const F functor): func_(functor){}
-    
+  bvpl_octree_kernel_operator(const F functor): func_(functor) {}
+
   // "Convolves" kernel with and input octree, storing the output in an output octree.
   void operate(boct_tree<short ,T>* tree_in, bvpl_kernel_sptr kernel,  boct_tree<short,T>* out_tree, short level, double cell_length)
   {
     //get all cells at given scale
     vcl_vector<boct_tree_cell<short,T>* > cells = tree_in->cells_at_level(level);
     vcl_vector<boct_tree_cell<short,T>* > out_cells = out_tree->cells_at_level(level);
-    
+
     bvpl_kernel_iterator kernel_iter = kernel->iterator();
-    
+
     //iterate, through all cells
-    for(unsigned i = 0; i <  cells.size();i++)
+    for (unsigned i = 0; i <  cells.size();i++)
     {
-      
       //iterate through vector an compute result
       boct_tree_cell<short,T>* center_cell = cells[i];
       vgl_point_3d<double> this_cell_origin = tree_in->local_origin(center_cell);
-     
 
-      //:TO DO: check bounds. Is it more efficient to check bounds for every kernel or ro let is run and  store a dummy value in it?
-      
+      // -- TO DO: check bounds.
+      // Is it more efficient to check bounds for every kernel
+      // or to let it run and store a dummy value in it?
+
       //reset the kernel iterator
       kernel_iter.begin();
-      while (!kernel_iter.isDone()) {
-
+      while (!kernel_iter.isDone())
+      {
         vgl_point_3d<int> kernel_idx = kernel_iter.index();
 
 
         vgl_point_3d<double> this_cell_idx(this_cell_origin.x() + (double)kernel_idx.x()*cell_length,
-                                             this_cell_origin.y() + (double)kernel_idx.y()*cell_length,
-                                             this_cell_origin.z() + (double)kernel_idx.z()*cell_length); 
-       
+                                           this_cell_origin.y() + (double)kernel_idx.y()*cell_length,
+                                           this_cell_origin.z() + (double)kernel_idx.z()*cell_length);
+
         boct_tree_cell<short,T>* this_cell = tree_in->locate_point_at_level(this_cell_idx, level, true);
-        
+
         if (this_cell) {
-         
           bvpl_kernel_dispatch d = *kernel_iter;
           T val = this_cell->data();
           func_.apply(val, d);
         }
-        else 
+        else
           break;
         ++kernel_iter;
       }
-      
+
       out_cells[i]->set_data(func_.result());
-      
     }
   }
-  
-private:
-  F func_;
 
+ private:
+  F func_;
 };
 
 #endif
--- contrib/brl/bseg/bvpl/bvpl_octree/tests/test_octree_kernel_operator.cxx
+++ contrib/brl/bseg/bvpl/bvpl_octree/tests/test_octree_kernel_operator.cxx
@@ -1,10 +1,9 @@
 //
+#include <testlib/testlib_test.h>
 //:
 //\file
 //\author Isabel Restrepo
-//\date 11/16/09.
-
-#include <testlib/testlib_test.h>
+//\date November 16, 2009.
 
 #include "../bvpl_octree_kernel_operator.h"
 #include "../bvpl_octree_vector_operator.h"
@@ -19,9 +18,9 @@ bool debug =0;
 void fill_sample_octree(boct_tree<short,float>* tree)
 {
   vcl_cout << "Creating Octree" << vcl_endl;
- 
+
   //Create tree
-    tree->split();
+  tree->split();
 
   {
     vcl_vector<boct_tree_cell<short,float>*> leaves = tree->leaf_cells();
@@ -29,30 +28,29 @@ void fill_sample_octree(boct_tree<short,float>* tree)
     leaves[1]->set_data(0.5);
 #if debug
     for (unsigned i=0; i<leaves.size(); i++) {
-          vcl_cout<< leaves[i]->get_code().x_loc_ << ",";
-          vcl_cout<< leaves[i]->get_code().y_loc_ << ",";
-          vcl_cout<< leaves[i]->get_code().z_loc_ << ",";
-          vcl_cout<< leaves[i]->data() << vcl_endl;
+          vcl_cout << leaves[i]->get_code().x_loc_ << ','
+                   << leaves[i]->get_code().y_loc_ << ','
+                   << leaves[i]->get_code().z_loc_ << ','
+                   << leaves[i]->data() << vcl_endl;
         }
 #endif
-    
+
     leaves[1]->split();
     //leaves[1]->set_data(0.6);
-
   }
-  
-  
+
+
 #if 0
   {
     vcl_vector<boct_tree_cell<short,float>*> leaves = tree->cells_at_level((short)0);
     for (unsigned i=0; i<leaves.size(); i++) {
-      vcl_cout<< leaves[i]->get_code().x_loc_ << ",";
-      vcl_cout<< leaves[i]->get_code().y_loc_ << ",";
-      vcl_cout<< leaves[i]->get_code().z_loc_ << ",";
-      vcl_cout<< tree->local_origin(leaves[i]) << ",";
-      vcl_cout<< leaves[i]->get_code().level << ",";
-      vcl_cout<< tree->cell_size(leaves[i]) << ",";
-      vcl_cout<< leaves[i]->data() << vcl_endl;
+      vcl_cout << leaves[i]->get_code().x_loc_ << ','
+               << leaves[i]->get_code().y_loc_ << ','
+               << leaves[i]->get_code().z_loc_ << ','
+               << tree->local_origin(leaves[i]) << ','
+               << leaves[i]->get_code().level << ','
+               << tree->cell_size(leaves[i]) << ','
+               << leaves[i]->data() << vcl_endl;
     }
   }
 #endif
@@ -63,56 +61,54 @@ void fill_edge3d_tree(boct_tree<short,float>* tree)
 {
   tree-> split();
   vcl_vector<boct_tree_cell<short,float>*> leaves = tree->leaf_cells();
-  for(unsigned i= 0; i<4; i++)
+  for (unsigned i= 0; i<4; i++)
   {
     leaves[i]->set_data(0.2);
     leaves[i]->split();
     {
     boct_tree_cell<short,float>* cc =leaves[i]->children();
-    for(unsigned j1=0; j1<8; j1++)
+    for (unsigned j1=0; j1<8; j1++)
       cc[j1].split();
     }
     leaves[i+4]->set_data(0.9);
     leaves[i+4]->split();
     {
       boct_tree_cell<short,float>* cc =leaves[i+4]->children();
-      for(unsigned j1=0; j1<8; j1++)
+      for (unsigned j1=0; j1<8; j1++)
         cc[j1].split();
     }
     //split children
     vcl_vector<boct_tree_cell<short,float>*> leaves = tree->leaf_cells();
-
   }
- 
 }
+
 bool octree_kernel()
 {
   //Create kernel
   bvpl_edge3d_kernel_factory edge_factory(2,1,1);
   bvpl_kernel_sptr kernel = new bvpl_kernel(edge_factory.create());
-  
+
   kernel->print();
-  
+
   //Create functor
   bvpl_edge_geometric_mean_functor<float> functor;
-  
+
   //Create the octree
   boct_tree<short,float> *tree = new boct_tree<short,float>(3);
   fill_sample_octree(tree);
-  
+
   //operate kernel on octree
   bvpl_octree_kernel_operator<float, bvpl_edge_geometric_mean_functor<float> > oper(functor);
-  
+
   boct_tree<short,float> *tree_out = tree->clone();
-  
+
   //tree_out -> print();
-  oper.operate(tree, kernel, tree_out, 1, 0.5); 
+  oper.operate(tree, kernel, tree_out, 1, 0.5);
   // tree_out ->print();
 
   delete tree;
   delete tree_out;
   return true;
-    
 }
 
 bool octree_vector_operator()
@@ -121,46 +117,44 @@ bool octree_vector_operator()
   bvpl_edge3d_kernel_factory kernels_3d(5,5,5);
   bvpl_create_directions_a dir;
   bvpl_kernel_vector_sptr kernel_vec = kernels_3d.create_kernel_vector(dir);
-  
-  
+
+
   //Create functor
   bvpl_edge_geometric_mean_functor<float> functor;
-  
+
   //Create tree. this tree is initialized with 4 leves as a regular grid
   boct_tree<short,float> *tree = new boct_tree<short,float>(4);
-  
+
   fill_edge3d_tree(tree);
-  
+
   bvpl_octree_kernel_operator<float,bvpl_edge_geometric_mean_functor<float> >  kernel_op(functor);
   bvpl_octree_vector_operator vector_op;
-  
+
   boct_tree<short,float> *tree_out = tree->clone();
   tree_out->init_cells(0.0f);
   short level = 0;
   double cell_length = 1.0/(double)(1<<(3 -level));
   boct_tree<short,int> *id_tree = tree->clone_to_type<int>();
-  
+
   id_tree->init_cells(-1);
-  
+
   vector_op.max_response(tree, kernel_vec, &kernel_op, tree_out, id_tree, level, cell_length);
-     
 
   //test response at the center is correct
   boct_loc_code<short> loc_code;
-  
+
   bool result = true;
-  for(short x=3; x<5; x++)
-    for(short y=3; y<5; y++)
-      for(short z=3; z<5; z++)
+  for (short x=3; x<5; x++)
+    for (short y=3; y<5; y++)
+      for (short z=3; z<5; z++)
       {
-        loc_code.set_code(x,y,z); 
+        loc_code.set_code(x,y,z);
         loc_code.set_level(0);
         result = result && id_tree->get_cell(loc_code)->data() == 0;
         vcl_cout << id_tree->get_cell(loc_code)->data();
       }
-  
+
   return result;
-  
 }
 
 
