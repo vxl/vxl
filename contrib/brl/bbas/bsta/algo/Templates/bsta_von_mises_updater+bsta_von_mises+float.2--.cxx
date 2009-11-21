09e533a800767a4d4fc03ec53402eb5611fa8269
Joseph L. Mundy <mundyj@users.sourceforge.net>
2009-11-21 08:50:07 -0500

    updater for bsta_von_mises distribution
    SVN-Revision: 26988

---
+++ contrib/brl/bbas/bsta/algo/Templates/bsta_von_mises_updater+bsta_von_mises+float.2--.cxx
@@ -0,0 +1,4 @@
+#include <bsta/algo/bsta_von_mises_updater.txx>
+#include <bsta/bsta_von_mises.h>
+typedef bsta_von_mises<float, 2> vm2;
+BSTA_VON_MISES_UPDATER_INSTANTIATE(vm2);
---
+++ contrib/brl/bbas/bsta/algo/Templates/bsta_von_mises_updater+bsta_von_mises+float.3--.cxx
@@ -0,0 +1,4 @@
+#include <bsta/algo/bsta_von_mises_updater.txx>
+#include <bsta/bsta_von_mises.h>
+typedef bsta_von_mises<float, 3> vm3;
+BSTA_VON_MISES_UPDATER_INSTANTIATE(vm3);
