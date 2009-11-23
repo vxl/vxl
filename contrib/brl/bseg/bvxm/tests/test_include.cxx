cff79c3671832eb59efc80e82a1b2bb97608bd36
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-11-23 17:22:56 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 27040

--- contrib/brl/bseg/bvxm/tests/test_include.cxx
+++ contrib/brl/bseg/bvxm/tests/test_include.cxx
@@ -9,6 +9,7 @@
 #include <bvxm/bvxm_mog_mc_processor.h>
 #include <bvxm/bvxm_mog_rgb_processor.h>
 #include <bvxm/bvxm_util.h>
+#include <bvxm/bvxm_von_mises_tangent_processor.h>
 #include <bvxm/bvxm_voxel_traits.h>
 #include <bvxm/bvxm_voxel_world.h>
 #include <bvxm/bvxm_world_params.h>
