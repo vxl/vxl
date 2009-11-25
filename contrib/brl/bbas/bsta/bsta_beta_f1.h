c67fa279007f0df3f82c0c9c5181dc62e063ea38
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-11-25 02:26:57 -0500

    added missing file
    SVN-Revision: 27067

---
+++ contrib/brl/bbas/bsta/bsta_beta_f1.h
@@ -0,0 +1,14 @@
+// This is brl/bbas/bsta/bsta_beta_f1.h
+#ifndef bsta_beta_f1_h_
+#define bsta_beta_f1_h_
+//:
+// \file
+// \brief beta distribution with float type
+// \author Peter Vanroose
+// \date September 25, 2009
+
+#include "bsta_beta.h"
+
+typedef bsta_beta<float> bsta_beta_f1;
+
+#endif // bsta_beta_f1_h_
