f3f27f82a505fe11c85e5f8ea8adaef49fa62509
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-11-26 17:00:27 -0500

    renamed (standard template instantiation fome naming)
    SVN-Revision: 27091

--- contrib/brl/bseg/boxm/Templates/vbl_array_3d+boxm_block+boct_tree+short.float--~-.cxx
+++ contrib/brl/bseg/boxm/Templates/vbl_array_3d+boxm_block+boct_tree+short.float--~-.cxx
@@ -3,5 +3,5 @@
 #include <boxm/boxm_block.txx>
 #include <boct/boct_tree.txx>
 
-typedef boct_tree<short, float > tree_type;
+typedef boct_tree<short,float> tree_type;
 VBL_ARRAY_3D_INSTANTIATE(boxm_block<tree_type>*);
--- contrib/brl/bseg/bvxm/grid/Templates/bvxm_voxel_slab_iterator+bsta_num_obs+bsta_gaussian_sphere+double.2---.cxx
+++ contrib/brl/bseg/bvxm/grid/Templates/bvxm_voxel_slab_iterator+bsta_num_obs+bsta_gaussian_sphere+double.2---.cxx
@@ -4,5 +4,5 @@
 #include <bsta/io/bsta_io_attributes.h>
 #include <bsta/io/bsta_io_gaussian_sphere.h>
 
-typedef bsta_num_obs<bsta_gaussian_sphere<double, 2> > gauss_type;
+typedef bsta_num_obs<bsta_gaussian_sphere<double,2> > gauss_type;
 BVXM_VOXEL_SLAB_ITERATOR_INSTANTIATE(gauss_type);
