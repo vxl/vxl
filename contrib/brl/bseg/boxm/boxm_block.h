7757d0b0a1f8cbe0cf1bc2411902ebeae7b4fa47
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-12-03 16:00:18 -0500

    no need to test on null when issuing "delete"
    SVN-Revision: 27160

--- contrib/brl/bseg/boxm/boxm_block.h
+++ contrib/brl/bseg/boxm/boxm_block.h
@@ -20,14 +20,14 @@ class boxm_block
 {
  public:
   boxm_block(): octree_(0) {}
-  ~boxm_block() {if (octree_) delete octree_; }
+  ~boxm_block() { if (octree_) delete octree_; }
   boxm_block(vgl_box_3d<double> bbox): bbox_(bbox), octree_(0) {}
   boxm_block(vgl_box_3d<double> bbox, T* tree) : bbox_(bbox), octree_(tree) {}
   void init_tree(T * octree);
-  void set_tree(T * octree){octree_=octree;}
+  void set_tree(T * octree) { octree_=octree; }
   vgl_box_3d<double> bounding_box() const { return bbox_; }
-  T* get_tree(){return octree_;}
-  void delete_tree() { if (octree_) delete octree_; octree_ = 0; }
+  T* get_tree() { return octree_; }
+  void delete_tree() { delete octree_; octree_ = 0; }
   void b_read(vsl_b_istream &s);
   void b_write(vsl_b_ostream &s);
   static short version_no() { return 1; }
