d7dd70dbe91651ee8574f4d390af9f173308db2e
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-12-04 18:44:43 -0500

    added some instantiations
    SVN-Revision: 27189

--- contrib/brl/bseg/boxm/Templates/boxm_block_iterator+boct_tree+short.boxm_sample+BOXM_APM_MOG_GREY---.cxx
+++ contrib/brl/bseg/boxm/Templates/boxm_block_iterator+boct_tree+short.boxm_sample+BOXM_APM_MOG_GREY---.cxx
@@ -5,3 +5,7 @@
 
 typedef boct_tree<short,boxm_sample<BOXM_APM_MOG_GREY> >  tree_type;
 BOXM_BLOCK_ITERATOR_INSTANTIATE(tree_type);
+
+typedef boxm_apm_traits<BOXM_APM_MOG_GREY>::apm_datatype apm_datatype;
+typedef boct_tree<short,apm_datatype >  apm_tree_type;
+BOXM_BLOCK_ITERATOR_INSTANTIATE(apm_tree_type);
--- contrib/brl/bseg/boxm/Templates/boxm_scene+boct_tree+short.boxm_sample+BOXM_APM_MOG_GREY---.cxx
+++ contrib/brl/bseg/boxm/Templates/boxm_scene+boct_tree+short.boxm_sample+BOXM_APM_MOG_GREY---.cxx
@@ -4,3 +4,7 @@
 
 typedef boct_tree<short, boxm_sample<BOXM_APM_MOG_GREY> >  tree_type;
 BOXM_SCENE_INSTANTIATE(tree_type);
+
+typedef boxm_apm_traits<BOXM_APM_MOG_GREY>::apm_datatype apm_datatype;
+typedef boct_tree<short,apm_datatype >  apm_tree_type;
+BOXM_SCENE_INSTANTIATE(apm_tree_type);
