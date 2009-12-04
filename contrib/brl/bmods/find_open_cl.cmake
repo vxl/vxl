acf37ed9bd3a86376a1e40ab543fe67cfd9a88ea
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-12-04 11:54:32 -0500

    fixed APPLE set up for OpenCL libraries
    SVN-Revision: 27176

--- contrib/brl/bmods/find_open_cl.cmake
+++ contrib/brl/bmods/find_open_cl.cmake
@@ -3,7 +3,9 @@
 # http://developer.amd.com/gpu/ATIStreamSDKBetaProgram/Pages/default.aspx
 # Undoubtedly a different setup is required for the Nvidia SDK
 #
+
 SET(OPENCL_FOUND "NO")	
+
 IF(WIN32)
 	FIND_PATH( OPENCL_INCLUDE_PATH calcl.h)
 
@@ -22,10 +24,15 @@ IF(WIN32)
 	ENDIF(OPENCL_INCLUDE_PATH)
 ENDIF (WIN32)
 
-
 IF (APPLE)
-  SET (OPENCL_FOUND "YES")
-  SET (OPENCL_LIBRARIES "-framework OpenCL" CACHE STRING "OpenCL lib for OSX")
+  FIND_PATH(OPENCL_INCLUDE_PATH cl.h "Include for OpenCL on OSX")
+  IF(OPENCL_INCLUDE_PATH)
+    FIND_LIBRARY(OPENCL_LIBRARY OpenCL "OpenCL lib for OSX")
+    IF(OPENCL_LIBRARY)
+      SET(OPENCL_FOUND "YES")
+      SET(OPENCL_LIBRARIES ${OPENCL_LIBRARY})
+    ENDIF(OPENCL_LIBRARY)
+  ENDIF(OPENCL_INCLUDE_PATH)
 ENDIF (APPLE)
 
 MESSAGE(STATUS "Found Open Cl? ${OPENCL_FOUND}")
