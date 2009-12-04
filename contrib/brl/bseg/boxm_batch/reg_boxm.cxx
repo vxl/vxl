37604aa9b01bee835c67e82cb233eeec3c9baf9e
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-12-04 18:46:18 -0500

    boxm_algo_pro is added
    SVN-Revision: 27190

--- contrib/brl/bseg/boxm_batch/reg_boxm.cxx
+++ contrib/brl/bseg/boxm_batch/reg_boxm.cxx
@@ -7,6 +7,7 @@
 #include <vpgl_pro/vpgl_register.h>
 #include <boxm/pro/boxm_register.h>
 #include <boxm/opt/pro/boxm_opt_register.h>
+#include <boxm/algo/pro/boxm_algo_register.h>
 
 PyObject *
 register_processes(PyObject *self, PyObject *args)
@@ -15,6 +16,7 @@ register_processes(PyObject *self, PyObject *args)
   vpgl_register::register_process();
   boxm_register::register_process();
   boxm_opt_register::register_process();
+  boxm_algo_register::register_process();
 
   Py_INCREF(Py_None);
   return Py_None;
@@ -28,6 +30,7 @@ register_datatypes(PyObject *self, PyObject *args)
   vpgl_register::register_datatype();
   boxm_register::register_datatype();
   boxm_opt_register::register_datatype();
+  boxm_algo_register::register_datatype();
   Py_INCREF(Py_None);
   return Py_None;
 }
