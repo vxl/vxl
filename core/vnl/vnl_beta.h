51816b0a4d3feb3da7a5667905c8959f3a3a01e3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-12-05 08:32:05 -0500

    added vnl_log_beta(), and added a little bit more documentation
    SVN-Revision: 27201

--- core/vnl/vnl_beta.h
+++ core/vnl/vnl_beta.h
@@ -8,10 +8,21 @@
 
 #include "vnl_gamma.h"
 
-//: Computation of beta function in terms of gamma
+#if 1 // implementation via vnl_log_gamma
+//: Computation of beta function in terms of gamma function.
+//  Actually, this implementation refers to vnl_log_gamma,
+//  since this involves just a single call to std::exp instead of three.
 template <class T>
 inline double vnl_beta(T x, T y) {return vcl_exp(vnl_log_gamma(x)+vnl_log_gamma(y)-vnl_log_gamma(x+y)); }
+#else // implementation via vnl_gamma; less efficient since it needs 3x vcl_exp
+//: Computation of beta function in terms of gamma function.
+template <class T>
+inline double vnl_beta(T x, T y) {return (vnl_gamma(x)*vnl_gamma(y))/vnl_gamma(x+y); }
+#endif
 
-//inline double vnl_beta(T x, T y) {return (vnl_gamma(x)*vnl_gamma(y))/vnl_gamma(x+y); }
+//: Computation of the log beta function in terms of the log gamma function.
+//  vnl_log_beta is just the std::log (natural logarithm) of the beta function.
+template <class T>
+inline double vnl_log_beta(T x, T y) {return vnl_log_gamma(x)+vnl_log_gamma(y)-vnl_log_gamma(x+y); }
 
 #endif
