dc3404b8e29dc5fa490a289d94657d476f677b31
Vishal Jain <jainv@users.sourceforge.net>
2009-12-07 12:23:15 -0500

    added a process to convert a projective camera to a rational camera.
    SVN-Revision: 27221

---
+++ contrib/brl/bpro/core/vpgl_pro/processes/vpgl_projective_to_rational_camera_process.cxx
@@ -0,0 +1,95 @@
+// This is brl/bpro/core/vpgl_pro/processes/vpgl_projective_to_rational_camera_process.cxx
+#include <bprb/bprb_func_process.h>
+//:A process to convert perspective camera to rational camera.
+// \file
+
+#include <vcl_iostream.h>
+#include <vcl_fstream.h>
+#include <vpgl/vpgl_camera.h>
+#include <vpgl/vpgl_perspective_camera.h>
+#include <vpgl/vpgl_rational_camera.h>
+#include <vpgl/vpgl_calibration_matrix.h>
+vpgl_rational_camera<double>
+projective_to_rational(vpgl_proj_camera<double>& cam_pers);
+
+//: Init function
+bool vpgl_projective_to_rational_camera_process_cons(bprb_func_process& pro)
+{
+  //this process takes one input: the filename
+  bool ok=false;
+  vcl_vector<vcl_string> input_types;
+  input_types.push_back("vpgl_camera_double_sptr");
+  ok = pro.set_input_types(input_types);
+  if (!ok) return ok;
+
+  vcl_vector<vcl_string> output_types;
+  output_types.push_back("vpgl_camera_double_sptr");  // label image
+  ok = pro.set_output_types(output_types);
+  if (!ok) return ok;
+
+  return true;
+}
+
+//: Execute the process
+bool vpgl_projective_to_rational_camera_process(bprb_func_process& pro)
+{
+  if (pro.n_inputs()< 1) {
+    vcl_cout << "vpgl_projective_to_rational_camera_process: The input number should be 1" << vcl_endl;
+    return false;
+  }
+
+  // get the inputs
+  vpgl_camera_double_sptr camera = pro.get_input<vpgl_camera_double_sptr>(0);
+  if(vpgl_proj_camera<double>* cam = dynamic_cast<vpgl_proj_camera<double>*>(camera.ptr()))
+  {     
+      vpgl_rational_camera<double> rat_cam=projective_to_rational(*cam);
+
+      vpgl_camera_double_sptr rat_cam_sptr=new vpgl_rational_camera<double>(rat_cam);
+      pro.set_output_val<vpgl_camera_double_sptr>(0, rat_cam_sptr);
+  }
+  else{
+      vcl_cout<<"Wrong type of camera input"<<vcl_endl;
+      return false;
+  }
+
+
+  return true;
+}
+
+vpgl_rational_camera<double>
+projective_to_rational(vpgl_proj_camera<double>& cam_proj)
+{
+  vnl_matrix_fixed<double,3,4> cam_pers_matrix = cam_proj.get_matrix();
+  vcl_vector<double> neu_u,den_u,neu_v,den_v;
+  double x_scale = 1.0,
+         x_off = 0.0,
+         y_scale = 1.0,
+         y_off = 0.0,
+         z_scale = 1.0,
+         z_off = 0.0,
+         u_scale = 1.0,
+         u_off = 0.0,
+         v_scale = 1.0,
+         v_off = 0.0;
+
+  for (int i=0; i<20; i++){
+    neu_u.push_back(0.0);
+    neu_v.push_back(0.0);
+    den_u.push_back(0.0);
+    den_v.push_back(0.0);
+  }
+
+  int vector_map[] = {9,15,18,19};
+
+  for (int i=0; i<4; i++){
+    neu_u[vector_map[i]] = cam_pers_matrix(0,i);
+    neu_v[vector_map[i]] = cam_pers_matrix(1,i);
+    den_u[vector_map[i]] = cam_pers_matrix(2,i);
+    den_v[vector_map[i]] = cam_pers_matrix(2,i);
+  }
+
+  vpgl_rational_camera<double> cam_rat(neu_u,den_u,neu_v,den_v,
+                                       x_scale,x_off,y_scale,y_off,z_scale,z_off,
+                                       u_scale,u_off,v_scale,v_off);
+  return cam_rat;
+}
