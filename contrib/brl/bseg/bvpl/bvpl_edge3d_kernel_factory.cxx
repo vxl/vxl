4573d52cafb847319885e8bec682fad14f66b566
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-12-08 14:50:52 -0500

    avoid compiler warning (assignment of float to int)
    SVN-Revision: 27246

--- contrib/brl/bseg/bvpl/bvpl_edge3d_kernel_factory.cxx
+++ contrib/brl/bseg/bvpl/bvpl_edge3d_kernel_factory.cxx
@@ -3,8 +3,6 @@
 // \file
 
 #include <vcl_algorithm.h>
-#include <vnl/vnl_math.h>
-#include <vnl/vnl_float_3.h>
 
 // Default Constructor
 bvpl_edge3d_kernel_factory::bvpl_edge3d_kernel_factory()
@@ -25,30 +23,23 @@ bvpl_edge3d_kernel_factory::bvpl_edge3d_kernel_factory()
 // + + 0 - -
 // + + 0 - -
 // + + 0 - -
-bvpl_edge3d_kernel_factory::bvpl_edge3d_kernel_factory(unsigned length, unsigned width, unsigned height)
+bvpl_edge3d_kernel_factory::bvpl_edge3d_kernel_factory(unsigned len, unsigned wid, unsigned hgt)
+: height_(hgt), width_(wid), length_(len) //set dimensions
 {
-  //set dimensions
-  length_=length;
-  height_=height;
-  width_=width;
-
-
-  //Determine angular resolition based on size of kernel
+  //Determine angular resolution based on size of kernel
   //If this was 2D, then the angular resolution would be 180/(2l -2) (Recusive Binary Dilation... Desikachari Nadadur)
   angular_resolution_=0;
   //set canonical axis to x-axis
   canonical_rotation_axis_[0] = 1.0f; canonical_rotation_axis_[1] = 0.0f; canonical_rotation_axis_[2] = 0.0f;
-
   canonical_parallel_axis_[0] = 0.0f; canonical_parallel_axis_[1] = 1.0f; canonical_parallel_axis_[2] = 0.0f;
   angle_ = 0.0f;
   rotation_axis_ = canonical_rotation_axis_;
-  //parallel_axis_ = canonical_paralell_axis_;
+  //parallel_axis_ = canonical_parallel_axis_;
   create_canonical();
 }
 
 void bvpl_edge3d_kernel_factory::create_canonical()
 {
-
   //The size of the kernel is limited. If widht or height of the kernel is too large,
   //the user should subsample the image/grid
   if (height_ > max_size_)
@@ -71,12 +62,12 @@ void bvpl_edge3d_kernel_factory::create_canonical()
   typedef vgl_point_3d<float> point_3d;
   typedef bvpl_kernel_dispatch dispatch;
 
-  int min_x= -1*(length_/2);
-  int max_x =vcl_ceil((float)length_/2);
-  int min_y= -1*(width_/2);
-  int max_y =vcl_ceil((float)width_/2);
-  int min_z= -1*(height_/2);
-  int max_z =vcl_ceil((float)height_/2);
+  int min_x= -(length_/2);
+  int max_x = (length_+1)/2;
+  int min_y= -(width_/2);
+  int max_y = (width_+1)/2;
+  int min_z= -(height_/2);
+  int max_z = (height_+1)/2;
 
   for (int x=min_x; x< max_x; x++)
   {
@@ -88,7 +79,6 @@ void bvpl_edge3d_kernel_factory::create_canonical()
           canonical_kernel_.push_back(vcl_pair<point_3d,dispatch>(point_3d(float(x),float(y),float(z)), dispatch(-1)));
         else if (x >=  0)
           canonical_kernel_.push_back(vcl_pair<point_3d,dispatch>(point_3d(float(x),float(y),float(z)), dispatch(1)));
-  
       }
     }
   }
