55e6a56044acb319f528c68be6222288cd7c43f2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-12-17 17:23:48 -0500

    TABs replaced by spaces
    SVN-Revision: 27315

--- contrib/brl/bseg/bvxm/bvxm_illum_util.cxx
+++ contrib/brl/bseg/bvxm/bvxm_illum_util.cxx
@@ -9,78 +9,78 @@ load_surface_nhbds(vcl_string const& path,
                    vcl_vector<vcl_vector<vnl_matrix<float> > >& nhds)
 {
   vcl_ifstream is(path.c_str());
-  if(!is.is_open())
-    {
-      vcl_cerr << "In bvxm_illum_util::load_surface_nhbds(.) - "
-               <<"neigborhood path is not valid\n";
-      return false;
-    }
-  
+  if (!is.is_open())
+  {
+    vcl_cerr << "In bvxm_illum_util::load_surface_nhbds(.) - "
+             <<"neigborhood path is not valid\n";
+    return false;
+  }
+
   unsigned dim, ntracks;
   vcl_string temp;
   is >> temp;
-  if(temp!="dim:"){
-      vcl_cerr << "In bvxm_illum_util::load_surface_nhbds(.) - "
-               <<"file parse error\n";
-      return false;
-    }
+  if (temp!="dim:"){
+    vcl_cerr << "In bvxm_illum_util::load_surface_nhbds(.) - "
+             <<"file parse error\n";
+    return false;
+  }
   is >> dim;
   is >> temp;
-  if(temp!="n_tracks:"){
-      vcl_cerr << "In bvxm_illum_util::load_surface_nhbds(.) - "
-               <<"file parse error\n";
-      return false;
-    }
+  if (temp!="n_tracks:"){
+    vcl_cerr << "In bvxm_illum_util::load_surface_nhbds(.) - "
+             <<"file parse error\n";
+    return false;
+  }
   is >> ntracks;
-  for(unsigned i = 0; i<ntracks; ++i){
+  for (unsigned i = 0; i<ntracks; ++i){
     vcl_vector<vnl_matrix<float> > nbs;
     is >> temp;
-    if(temp!="n_i:"){
+    if (temp!="n_i:") {
       vcl_cerr << "In bvxm_illum_util::load_surface_nhbds(.) - "
                <<"file parse error\n";
       return false;
     }
-	unsigned ni;
+    unsigned ni;
     is >> ni;
-    for(unsigned i = 0; i<ni; ++i)
-      {
-        vnl_matrix<float> m(dim, dim);
-        is >> m;
-        nbs.push_back(m);
-      }
+    for (unsigned i = 0; i<ni; ++i)
+    {
+      vnl_matrix<float> m(dim, dim);
+      is >> m;
+      nbs.push_back(m);
+    }
     nhds.push_back(nbs);
   }
   return true;
 }
 
-bool 
+bool
 bvxm_illum_util::load_illumination_dirs(vcl_string const& path,
                                         vcl_vector<vnl_double_3>& ill_dirs)
 {
   vcl_ifstream is(path.c_str());
-  if(!is.is_open())
-    {
-      vcl_cerr << "In bvxm_illum_util::load_illumination_dirs(.) - "
-               <<"illumination dir path is not valid\n";
-      return false;
-    }
+  if (!is.is_open())
+  {
+    vcl_cerr << "In bvxm_illum_util::load_illumination_dirs(.) - "
+             <<"illumination dir path is not valid\n";
+    return false;
+  }
   ill_dirs.clear();
   vcl_string temp;
   is >> temp;
-  if(temp != "n_dirs:")
-    {
-      vcl_cerr << "In bvxm_illum_util::load_illumination_dirs(.) - "
-               <<"invalid file syntax\n";
-      return false;
-    }
+  if (temp != "n_dirs:")
+  {
+    vcl_cerr << "In bvxm_illum_util::load_illumination_dirs(.) - "
+             <<"invalid file syntax\n";
+    return false;
+  }
   unsigned n_dirs = 0;
   is >> n_dirs;
-  for(unsigned i = 0; i<n_dirs; ++i)
-    {
-      vnl_double_3 v;
-      is >> v;
-      ill_dirs.push_back(v);
-    }
+  for (unsigned i = 0; i<n_dirs; ++i)
+  {
+    vnl_double_3 v;
+    is >> v;
+    ill_dirs.push_back(v);
+  }
   return true;
 }
 
@@ -92,19 +92,19 @@ solve_lambertian_model(vcl_vector<vnl_double_3> const& ill_dirs,
 {
   // form the "A" and "b" matrices
   unsigned m = ill_dirs.size(), n = 3;
-  if(m<4){
-      vcl_cerr << "In bvxm_illum_util::solve_lambertian_model(.) - "
-               <<"insufficient number of illumination dirs\n";
-      return false;
+  if (m<4) {
+    vcl_cerr << "In bvxm_illum_util::solve_lambertian_model(.) - "
+             <<"insufficient number of illumination dirs\n";
+    return false;
   }
   vnl_matrix<double> S(m,n), A3x3(3,3), A(4,4);
-  for(unsigned j = 0; j<m; ++j)
-    for(unsigned i = 0; i<3; ++i)
+  for (unsigned j = 0; j<m; ++j)
+    for (unsigned i = 0; i<3; ++i)
       S[j][i]=ill_dirs[j][i];
   A3x3 = S.transpose()*S;
   double s0=0.0, s1=0.0, s2=0.0;
   double b0=0.0, b1=0.0, b2=0.0, b3=0.0;
-  for(unsigned j=0; j<m; ++j){
+  for (unsigned j=0; j<m; ++j){
     s0 += S[j][0]; s1 += S[j][1]; s2 += S[j][2];
     b0 += S[j][0]*intensities[j];
     b1 += S[j][1]*intensities[j];
@@ -114,8 +114,8 @@ solve_lambertian_model(vcl_vector<vnl_double_3> const& ill_dirs,
   vnl_vector<double> b(4);
   b[0]=b0;   b[1]=b1;   b[2]=b2;   b[3]=b3;
   b /= m;
-  for(unsigned j =0; j<3; ++j)
-    for(unsigned i =0; i<3; ++i)
+  for (unsigned j =0; j<3; ++j)
+    for (unsigned i =0; i<3; ++i)
       A[j][i]=A3x3[j][i];
   A[3][0]=s0; A[0][3]=s0;
   A[3][1]=s1; A[1][3]=s1;
@@ -131,25 +131,25 @@ solve_lambertian_model(vcl_vector<vnl_double_3> const& ill_dirs,
 
   model_params = Ainv*b;
 
-  // compute model fitting error  
+  // compute model fitting error
   double var = 0.0;
-  for(unsigned j = 0; j<m; ++j)
-    {
-      double Im = S[j][0]*model_params[0];
-      Im += S[j][1]*model_params[1]; Im += S[j][2]*model_params[2];
-      Im += model_params[3];
-      double Io = intensities[j];
-      var += (Im-Io)*(Im-Io);
-    }
+  for (unsigned j = 0; j<m; ++j)
+  {
+    double Im = S[j][0]*model_params[0];
+    Im += S[j][1]*model_params[1]; Im += S[j][2]*model_params[2];
+    Im += model_params[3];
+    double Io = intensities[j];
+    var += (Im-Io)*(Im-Io);
+  }
   fitting_error = vcl_sqrt(var/m);
-  return true;  
+  return true;
 }
 
 double bvxm_illum_util::expected_intensity(vnl_double_3 const& illum_dir,
                                            vnl_double_4 const& model_params)
 {
   double Im = 0.0;
-  for(unsigned i = 0; i<3; ++i)
+  for (unsigned i = 0; i<3; ++i)
     Im += illum_dir[i]*model_params[i];
   Im += model_params[3];
   return Im;
