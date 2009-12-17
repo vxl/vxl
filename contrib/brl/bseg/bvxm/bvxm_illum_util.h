a276f23a38071d3a1a605ec0fd83d0406e9a8957
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-12-17 17:26:25 -0500

    brief Doxygen comment should be on a single line
    SVN-Revision: 27317

--- contrib/brl/bseg/bvxm/bvxm_illum_util.h
+++ contrib/brl/bseg/bvxm/bvxm_illum_util.h
@@ -7,6 +7,8 @@
 // \date November 29, 2009
 //
 // \verbatim
+//  Modifications
+//   <none yet>
 // \endverbatim
 
 #include <vnl/vnl_matrix.h>
@@ -19,15 +21,15 @@
 class bvxm_illum_util
 {
  public:
-  static bool 
+  static bool
     load_surface_nhbds(vcl_string const& path,
                        vcl_vector<vcl_vector<vnl_matrix<float> > >& nhds);
-static bool 
-  load_illumination_dirs(vcl_string const& path,
-                         vcl_vector<vnl_double_3>& ill_dirs);
+  static bool
+    load_illumination_dirs(vcl_string const& path,
+                           vcl_vector<vnl_double_3>& ill_dirs);
 
-
-  //: given a set of illumination directions and image intensities,
+  //:
+  // Given a set of illumination directions and image intensities,
   // find the surface normal and ambient illumination factor, where
   // both are weighted by the surface albedo. A minium of 4 directions
   // are required.
@@ -35,12 +37,9 @@ static bool
                                      vcl_vector<double> const& intensities,
                                      vnl_double_4& model_params,
                                      double& fitting_error);
-  //:given an illumination direction and Lambertian model, predict the 
-  // resulting image intensity
+  //:given an illumination direction and Lambertian model, predict the resulting image intensity
   static double expected_intensity(vnl_double_3 const& illum_dir,
                                    vnl_double_4 const& model_params);
-
-  
 };
 
 #endif // bvxm_illum_util_h_
