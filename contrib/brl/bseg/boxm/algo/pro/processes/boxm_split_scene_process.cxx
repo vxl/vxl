ab2ae404b73a852a3cbee368c8a26ae1231f723c
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2009-12-18 11:37:45 -0500

    output scenes are now saved to the database
    SVN-Revision: 27332

--- contrib/brl/bseg/boxm/algo/pro/processes/boxm_split_scene_process.cxx
+++ contrib/brl/bseg/boxm/algo/pro/processes/boxm_split_scene_process.cxx
@@ -20,15 +20,14 @@
 namespace boxm_split_scene_process_globals
 {
   const unsigned n_inputs_ = 1;
-  const unsigned n_outputs_ = 0;
+  const unsigned n_outputs_ = 2;
 }
 
 //: process takes 1 inputs and has no outputs.
 // input[0]: The sample scene
-//
-// Note: The resulting scenes are stored in the same directory as the input scene.
-//       The appearance scenes are not returned as outputs, because it is assumed that this
-//       process won't be called regularly.
+// output[0]: The appeaance scene
+// outout[1]: The alpha scene
+
 
 bool boxm_split_scene_process_cons(bprb_func_process& pro)
 {
@@ -37,6 +36,9 @@ bool boxm_split_scene_process_cons(bprb_func_process& pro)
   input_types_[0] = "boxm_scene_base_sptr";
 
   vcl_vector<vcl_string> output_types_(n_outputs_);
+
+  output_types_[0] = "boxm_scene_base_sptr";  
+  output_types_[1] = "boxm_scene_base_sptr";
 
   return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
 }
@@ -76,15 +78,20 @@ bool boxm_split_scene_process(bprb_func_process& pro)
   // parameters of the output scene are the same as those of the input scene
   boxm_scene<apm_tree_type> apm_scene(scene_in->lvcs(), scene_in->origin(), scene_in->block_dim(), scene_in->world_dim());
   apm_scene.set_paths(scene_in->path(), "apm_mog_grey");
+  apm_scene.set_appearance_model(BSTA_MOG_F1);
   boxm_scene<alpha_tree_type > alpha_scene(scene_in->lvcs(), scene_in->origin(), scene_in->block_dim(), scene_in->world_dim());
   alpha_scene.set_paths(scene_in->path(), "alpha");
-
+  alpha_scene.set_appearance_model(FLOAT);
   boxm_split_sample<BOXM_APM_MOG_GREY> splitter;
 
   splitter.split_scene( *scene_in, apm_scene, alpha_scene);
 
   apm_scene.write_scene("apm_scene.xml");
-  alpha_scene.write_scene("alpha_scene.xml");
+  alpha_scene.write_scene("/alpha_scene.xml");
+  
+  pro.set_output_val<boxm_scene_base_sptr>(0, &apm_scene);
+  pro.set_output_val<boxm_scene_base_sptr>(1, &alpha_scene);
+
 
   return true;
 }
