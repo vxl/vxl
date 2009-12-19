3d2ffdd8e3a9d065c33443808bb9339901466eb8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-12-19 11:57:30 -0500

    added missing write_vrml_header(os)
    SVN-Revision: 27350

--- contrib/brl/bseg/bvxm/grid/io/bvxm_vrml_voxel_grid.h
+++ contrib/brl/bseg/bvxm/grid/io/bvxm_vrml_voxel_grid.h
@@ -1,16 +1,15 @@
 // This is//projects/vxl/src/contrib/brl/bseg/bvxm/grid/io/bvxm_vrml_voxel_grid.h
 #ifndef bvxm_vrml_voxel_grid_h
 #define bvxm_vrml_voxel_grid_h
-
 //:
 // \file
 // \brief A class with vrml utilities for a voxel grid
 // \author Isabel Restrepo mir@lems.brown.edu
-// \date  9/21/09
+// \date  September 21, 2009
 //
 // \verbatim
 //  Modifications
-//   <none yet>
+//   2009-12-19 Peter Vanroose - added write_vrml_header()
 // \endverbatim
 
 #include "../bvxm_voxel_grid.h"
@@ -21,23 +20,18 @@
 #include <vnl/vnl_float_4.h>
 #include <bvrml/bvrml_write.h>
 
-
 #include <bsta/bsta_attributes.h>
 #include <bsta/bsta_gauss_f1.h>
 
 class bvxm_vrml_voxel_grid
 {
-public:
-
+ public:
+  static void write_vrml_header(vcl_ofstream& str) { str << "#VRML V2.0 utf8\n"; }
   static void write_vrml_grid(vcl_ofstream& str, bvxm_voxel_grid<float> *grid, float threshold);
-  
   static void write_vrml_grid(vcl_ofstream& str, bvxm_voxel_grid<bsta_num_obs<bsta_gauss_f1> > *grid, float threshold);
-  
   static void write_vrml_grid_as_spheres(vcl_ofstream& str, bvxm_voxel_grid<float> *grid, float threshold);
-  
   static void write_vrml_grid_as_spheres(vcl_ofstream& str, bvxm_voxel_grid<vnl_float_4> *grid, float threshold, int s=1);
-
   static void write_vrml_grid_as_pointers(vcl_ofstream& str, bvxm_voxel_grid<vnl_float_4> *grid, float threshold, int s=1);
-
 };
-#endif
+
+#endif // bvxm_vrml_voxel_grid_h
