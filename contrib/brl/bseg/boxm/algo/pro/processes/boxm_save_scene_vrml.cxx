457869dbbe70026d9d5db2af437a262bf240c7bd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-12-20 08:10:09 -0500

    corrected the comment on # inputs
    SVN-Revision: 27360

--- contrib/brl/bseg/boxm/algo/pro/processes/boxm_save_scene_vrml.cxx
+++ contrib/brl/bseg/boxm/algo/pro/processes/boxm_save_scene_vrml.cxx
@@ -1,4 +1,4 @@
-// This is brl/bseg/bvpl/pro/processes/boxm_save_scene_vrml_process.cxx
+// This is brl/bseg/boxm/algo/pro/processes/boxm_save_scene_vrml.cxx
 #include <bprb/bprb_func_process.h>
 //:
 // \file
@@ -23,10 +23,9 @@ namespace boxm_save_scene_vrml_process_globals
   const unsigned n_outputs_ = 0;
 }
 
-//: process takes 1 inputs and has no outputs.
+//: process takes 2 inputs and has no outputs.
 // input[0]: The scene
-// input[2]: Path to output vrml file
-
+// input[1]: Path to output vrml file
 // No outputs
 
 bool boxm_save_scene_vrml_process_cons(bprb_func_process& pro)
@@ -35,44 +34,40 @@ bool boxm_save_scene_vrml_process_cons(bprb_func_process& pro)
   vcl_vector<vcl_string> input_types_(n_inputs_);
   input_types_[0] = "boxm_scene_base_sptr";
   input_types_[1] = "vcl_string";
-  
+
   vcl_vector<vcl_string> output_types_(n_outputs_);
-  
+
   return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
 }
 
 bool boxm_save_scene_vrml_process(bprb_func_process& pro)
 {
   using namespace boxm_save_scene_vrml_process_globals;
-  
+
   if (pro.n_inputs() != n_inputs_)
   {
     vcl_cout << pro.name() << ": the input number should be " << n_inputs_
-    << " but instead it is " << pro.n_inputs() << vcl_endl;
+             << " but instead it is " << pro.n_inputs() << vcl_endl;
     return false;
   }
-  
-  //get inputs:
+
+  // get inputs:
   boxm_scene_base_sptr scene_base = pro.get_input<boxm_scene_base_sptr>(0);
   vcl_string vrml_path = pro.get_input<vcl_string>(1);
-  
-  //check input's validity
+
+  // check input's validity
   if (!scene_base.ptr()) {
     vcl_cout <<  " :-- Grid is not valid!\n";
     return false;
   }
-  
-  //:Note initial implementation is for fixed types, but this can be changed if more cases are needed
-  
-  typedef boct_tree<short, float > tree_type;
 
-  
+  // Note initial implementation is for fixed types, but this can be changed if more cases are needed
+
+  typedef boct_tree<short, float > tree_type;
   boxm_scene<tree_type> *scene_in = static_cast<boxm_scene<tree_type>* > (scene_base.as_pointer());
-  
   vcl_ofstream ofs(vrml_path.c_str());
-  
   boxm_vrml_util::write_vrml_scene(ofs, scene_in);
- 
+
   return true;
 }
 
