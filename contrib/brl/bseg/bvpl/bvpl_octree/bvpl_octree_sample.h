bc4fa3101c72864d856dd7e9ef002f4e072f88f5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-12-26 10:49:00 -0500

    removed unused #include <vnl/vnl_float_3.h>
    SVN-Revision: 27393

--- contrib/brl/bseg/bvpl/bvpl_octree/bvpl_octree_sample.h
+++ contrib/brl/bseg/bvpl/bvpl_octree/bvpl_octree_sample.h
@@ -1,36 +1,34 @@
-// This is bvpl_octree_sample.h
+// This is brl/bseg/bvpl/bvpl_octree/bvpl_octree_sample.h
 #ifndef bvpl_octree_sample_h
 #define bvpl_octree_sample_h
-
 //:
 // \file
 // \brief  A data structture to contain- bvpl_kernel response, axis of rotations and rotation around axis
 // \author Isabel Restrepo mir@lems.brown.edu
-// \date  12/9/09
+// \date  December 9, 2009
 //
 // \verbatim
 //  Modifications
 //   <none yet>
 // \endverbatim
 
-#include <vnl/vnl_float_3.h>
 #include <vcl_iostream.h>
 #include <vsl/vsl_binary_io.h>
 
 template<class T_data>
 class bvpl_octree_sample
 {
-public:
+ public:
   //: Default Constructor. Becareful when using default constructor, the value of response may be nonsense
   //  This is indicated by the negative id.
   bvpl_octree_sample():response_(),id_(-1){}
-  
+
   //: Constructor from response and id
-  bvpl_octree_sample(const T_data& response, const int& id): 
+  bvpl_octree_sample(const T_data& response, const int& id):
   response_(response),id_(id){}
-  
+
   short version_no() const{ return 1; }
-  
+
   void  print(vcl_ostream& os) const;
 
   //Member variables
