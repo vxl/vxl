ee5c519947efde885c9cfa1bb0b8effb12a0ed49
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2009-12-26 11:58:20 -0500

    corrected the "This is" line
    SVN-Revision: 27404

--- contrib/brl/bseg/bvpl/bvpl_octree/pro/processes/bvpl_create_scene_process.cxx
+++ contrib/brl/bseg/bvpl/bvpl_octree/pro/processes/bvpl_create_scene_process.cxx
@@ -1,10 +1,8 @@
-//This is brl/bseg/boxm/pro/processes/bvpl_create_scene_process.cxx
+//This is brl/bseg/bvpl/bvpl_octree/pro/processes/bvpl_create_scene_process.cxx
 #include <boxm/boxm_scene_base.h>
-#include <boxm/boxm_scene.h>
 //:
 // \file
 // \brief A process loading xml parameters of a boxm_scene of types specific to bvpl_octree
-//      
 //
 // \author Isabel Restrepo
 // \date Nov 15, 2009
@@ -19,10 +17,7 @@
 
 #include <brdb/brdb_value.h>
 
-
 #include <vcl_string.h>
-//#include <vcl_iostream.h>
-//#include <vcl_fstream.h>
 
 #include <boxm/boxm_scene.h>
 #include <boxm/boxm_scene_parser.h>
@@ -41,12 +36,12 @@ bool bvpl_create_scene_process_cons(bprb_func_process& pro)
 {
   //set output types
   using namespace bvpl_create_scene_process_globals;
-  
+
   vcl_vector<vcl_string> input_types_(n_inputs_);
   input_types_[0] = "vcl_string";
   if (!pro.set_input_types(input_types_))
     return false;
-  
+
   vcl_vector<vcl_string> output_types_(n_outputs_);
   output_types_[0] = "boxm_scene_base_sptr";
   if (!pro.set_output_types(output_types_))
@@ -59,9 +54,9 @@ bool bvpl_create_scene_process(bprb_func_process& pro)
 {
   using namespace bvpl_create_scene_process_globals;
   vcl_string fname = pro.get_input<vcl_string>(0);
-  
+
   boxm_scene_parser parser;
-  
+
   boxm_scene_base_sptr scene_ptr=new boxm_scene_base();
   scene_ptr->load_scene(fname, parser);
   if (scene_ptr->appearence_model() == BVPL_SAMPLE_FLOAT) {
@@ -83,12 +78,12 @@ bool bvpl_create_scene_process(bprb_func_process& pro)
     }
   }
   else {
-    vcl_cout << "bvpl_create_scene_process: undefined datatype" << vcl_endl;
+    vcl_cerr << "bvpl_create_scene_process: undefined datatype\n";
     return false;
   }
-  
+
   //store output
   pro.set_output_val<boxm_scene_base_sptr>(0, scene_ptr);
-  
+
   return true;
 }
--- contrib/brl/bseg/bvpl/bvpl_octree/pro/processes/bvpl_save_vrml_process.cxx
+++ contrib/brl/bseg/bvpl/bvpl_octree/pro/processes/bvpl_save_vrml_process.cxx
@@ -1,4 +1,4 @@
-// This is brl/bseg/bvpl/pro/processes/bvpl_save_vrml_process.cxx
+// This is brl/bseg/bvpl/bvpl_octree/pro/processes/bvpl_save_vrml_process.cxx
 #include <bprb/bprb_func_process.h>
 //:
 // \file
@@ -25,11 +25,10 @@ namespace bvpl_save_vrml_process_globals
   const unsigned n_outputs_ = 0;
 }
 
-//: process takes 1 inputs and has no outputs.
+//: process takes 3 inputs and has no outputs.
 // input[0]: The scene
 // input[1]: The vetor of kernels
 // input[2]: Path to output vrml file
-
 // No outputs
 
 bool bvpl_save_vrml_process_cons(bprb_func_process& pro)
@@ -39,57 +38,52 @@ bool bvpl_save_vrml_process_cons(bprb_func_process& pro)
   input_types_[0] = "boxm_scene_base_sptr";
   input_types_[1] = "bvpl_kernel_vector_sptr";
   input_types_[2] = "vcl_string";
-  
+
   vcl_vector<vcl_string> output_types_(n_outputs_);
-  
+
   return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
 }
 
 bool bvpl_save_vrml_process(bprb_func_process& pro)
 {
   using namespace bvpl_save_vrml_process_globals;
-  
+
   if (pro.n_inputs() != n_inputs_)
   {
     vcl_cout << pro.name() << ": the input number should be " << n_inputs_
-    << " but instead it is " << pro.n_inputs() << vcl_endl;
+             << " but instead it is " << pro.n_inputs() << vcl_endl;
     return false;
   }
-  
+
   //get inputs:
   boxm_scene_base_sptr scene_base = pro.get_input<boxm_scene_base_sptr>(0);
   bvpl_kernel_vector_sptr kernel_vector = pro.get_input<bvpl_kernel_vector_sptr>(1);
   vcl_string vrml_path = pro.get_input<vcl_string>(2);
-  
+
   //check input's validity
   if (!scene_base.ptr()) {
     vcl_cout <<  " :-- Grid is not valid!\n";
     return false;
   }
-  
+
   //:Note initial implementation is for fixed types, but this can be changed if more cases are needed
-  
+
   vcl_ofstream ofs(vrml_path.c_str());
 
-  if (boxm_scene<boct_tree<short, bvpl_octree_sample<float> > > *scene_in = 
+  if (boxm_scene<boct_tree<short, bvpl_octree_sample<float> > > *scene_in =
       dynamic_cast<boxm_scene<boct_tree<short, bvpl_octree_sample<float> > >* > (scene_base.as_pointer()))
   {
     bvpl_octree_vrml_util::write_scene_as_disks(ofs, scene_in, kernel_vector);
   }
-  else if (boxm_scene<boct_tree<short, bvpl_octree_sample<bsta_num_obs<bsta_gauss_f1> > > > *scene_in = 
+  else if (boxm_scene<boct_tree<short, bvpl_octree_sample<bsta_num_obs<bsta_gauss_f1> > > > *scene_in =
            dynamic_cast<boxm_scene<boct_tree<short, bvpl_octree_sample<bsta_num_obs<bsta_gauss_f1> > > >* >(scene_base.as_pointer()))
   {
     bvpl_octree_vrml_util::write_scene_as_disks(ofs, scene_in, kernel_vector);
   }
   else {
-    vcl_cerr << "In bvpl_save_vrml_process: Unsupported scene type" << vcl_endl;
+    vcl_cerr << "In bvpl_save_vrml_process: Unsupported scene type\n";
   }
 
-    
-  
-  
-  
   return true;
 }
-
 
