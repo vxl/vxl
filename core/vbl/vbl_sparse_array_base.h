a6800f2a133a33380930d292c2e52230a79ad3cf
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-01-01 10:56:54 -0500

    removed the since long deprecated operator[]
    SVN-Revision: 27415

--- core/vbl/vbl_sparse_array_base.h
+++ core/vbl/vbl_sparse_array_base.h
@@ -47,7 +47,7 @@ class vbl_sparse_array_base
   //: Return contents at (i)
   T      & operator () (Index i) { return storage_[i]; }
 
-  //: Return contents at (i). Asserts if (i) empty.
+  //: Return contents at (i). Asserts that (i) is non-empty.
   T const& operator () (Index i) const;
 
   //: Erase element at location (i). Assertion failure if not yet filled.
@@ -87,24 +87,6 @@ class vbl_sparse_array_base
 
   //: A bidirectional iterator pointing just beyond last non-empty element.
   const_iterator end() const { return storage_.end(); }
-
-#if 0
-  //: DEPRECATED Return contents at (i)
-  // use operator () instead
-  //
-  // Deprecated 10 April 2001,
-  // because the interface does not generalise to more than one
-  // dimension. e.g. array[i,j] will not compile.
-  T      & operator [] (Index i) { return storage_[i]; }
-
-  //: DEPRECATED Return contents at (i).
-  // use operator () instead;
-  //
-  // Deprecated 10 April 2001,
-  // because the interface does not generalise to more than one
-  // dimension. e.g. array[i,j] will not compile.
-  T const& operator [] (Index i) const;
-#endif
 };
 
 #endif // vbl_sparse_array_base_h_
