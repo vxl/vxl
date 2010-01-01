166e99f20ceb7d0d442b0a01d944d71b2a7d380a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-01-01 11:17:15 -0500

    no longer using the (de facto deprecated) method get_addr() in the tests, since it may return a pointer to volatile memory, and hence produce compiler warnings
    SVN-Revision: 27417

--- core/vbl/tests/vbl_test_sparse_array_2d.cxx
+++ core/vbl/tests/vbl_test_sparse_array_2d.cxx
@@ -5,24 +5,19 @@ static void vbl_test_sparse_array_2d()
 {
   vbl_sparse_array_2d<double> x;
   double d = 1.23;
-
+  testlib_test_begin("Putting something in (1,2)");
   x(1,2) = d;
+  testlib_test_perform(true);
+  testlib_test_begin("Putting something in (100,200)");
   x(100,200) = 100.2003;
-
+  testlib_test_perform(true);
   TEST("Something in (1,2)", x.fullp(1,2), true);
-  TEST("get_addr in (1,2)", x.get_addr(1,2) != 0, true);
-
-  TEST("x(1,2) == 1.23", x(1,2), d);
-
+  TEST("Content of x(1,2) is correct", x(1,2), d);
   TEST("Something in (100,200)", x.fullp(100,200), true);
-  TEST("get_addr in (100,200)", x.get_addr(100,200) != 0, true);
-
   TEST("Nothing in (2,3) yet", x.fullp(2,3), false);
-  TEST("Still nothing in (2,3)", x.get_addr(2,3), 0);
   x.put(2,3, 7);
   TEST("Something in (2,3) now", x.fullp(2,3), true);
-
-  TEST("Thing in (2,3) == 7", x(2,3), 7);
+  TEST("Content of x(2,3) is correct", x(2,3), 7);
 }
 
 TESTMAIN(vbl_test_sparse_array_2d);
--- core/vbl/tests/vbl_test_sparse_array_3d.cxx
+++ core/vbl/tests/vbl_test_sparse_array_3d.cxx
@@ -5,24 +5,19 @@ static void vbl_test_sparse_array_3d()
 {
   vbl_sparse_array_3d<double> x;
   double d = 1.23;
-
+  testlib_test_begin("Putting something in (1,2,3)");
   x(1,2,3) = d;
+  testlib_test_perform(true);
+  testlib_test_begin("Putting something in (100,200,300)");
   x(100,200,300) = 100.2003;
-
+  testlib_test_perform(true);
   TEST("Something in (1,2,3)", x.fullp(1,2,3), true);
-  TEST("get_addr in (1,2,3)", x.get_addr(1,2,3) != 0, true);
-
-  TEST("x(1,2,3) == 1.23", x(1,2,3), d);
-
+  TEST("Content of x(1,2,3) is correct", x(1,2,3), d);
   TEST("Something in (100,200,300)", x.fullp(100,200,300), true);
-  TEST("get_addr in (100,200,300)", x.get_addr(100,200,300) != 0, true);
-
   TEST("Nothing in (2,3,4) yet", x.fullp(2,3,4), false);
-  TEST("Still nothing in (2,3,4)", x.get_addr(2,3,4), 0);
   x.put(2,3,4, 7);
   TEST("Something in (2,3,4) now", x.fullp(2,3,4), true);
-
-  TEST("Thing in (2,3,4) == 7", x(2,3,4), 7);
+  TEST("Content of x(2,3,4) is correct", x(2,3,4), 7);
 }
 
 TESTMAIN(vbl_test_sparse_array_3d);
