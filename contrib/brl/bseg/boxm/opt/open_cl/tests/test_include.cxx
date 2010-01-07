791c3136f3708129e3b6905863f6420409c9ffae
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-01-07 07:24:55 -0500

    added new header file
    SVN-Revision: 27428

--- contrib/brl/bseg/boxm/opt/open_cl/tests/test_include.cxx
+++ contrib/brl/bseg/boxm/opt/open_cl/tests/test_include.cxx
@@ -1,4 +1,5 @@
 #include <boxm/opt/open_cl/boxm_ray_trace_manager.h>
 #include <boxm/opt/open_cl/boxm_opencl_manager.h>
+#include <boxm/opt/open_cl/boxm_opt_opencl_utils.h>
 
 int main() { return 0; }
