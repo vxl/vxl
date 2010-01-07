98cb8ea00ab7e30a54bcb7c0585677b7b8ea2252
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-01-07 07:24:47 -0500

    added temp.cxx to the build
    SVN-Revision: 27427

---
+++ contrib/brl/bseg/bvpl/bvpl_octree/temp.cxx
@@ -0,0 +1,4 @@
+// This is a dummy source file, to avoid linking errors when building the library
+// Please remove this file when other source files get added to the library
+#include "bvpl_octree_kernel_operator.h"
+#include "bvpl_octree_vector_operator.h"
