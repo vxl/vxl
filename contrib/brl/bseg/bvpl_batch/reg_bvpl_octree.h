93e73a0d565a69fa5a8d3507ca0bcce791316be4
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2010-01-07 15:14:07 -0500

    SVN-Revision: 27438

---
+++ contrib/brl/bseg/bvpl_batch/reg_bvpl_octree.h
@@ -0,0 +1,10 @@
+#ifndef reg_bvpl_h_
+#define reg_bvpl_h_
+
+#include <bpro/bpro_batch/bpro_batch.h>
+
+static PyObject *register_processes(PyObject *self, PyObject *args);
+static PyObject *register_datatypes(PyObject *self, PyObject *args);
+static PyMethodDef bvpl_octree_batch_methods[METHOD_NUM+2];
+
+#endif
