1d18f000843dc5ffdbaa71034a384cfe479c982a
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-01-12 18:01:21 -0500

    added new header files
    SVN-Revision: 27486

--- contrib/brl/bseg/boxm/algo/tests/test_include.cxx
+++ contrib/brl/bseg/boxm/algo/tests/test_include.cxx
@@ -1,3 +1,10 @@
+#include <boxm/algo/boxm_crop_scene.h>
+#include <boxm/algo/boxm_merge_mog.h>
+#include <boxm/algo/boxm_merge_mog_functor.h>
+#include <boxm/algo/boxm_split_sample.h>
+#include <boxm/algo/boxm_vrml_util.h>
 
+#include <boxm/algo/pro/boxm_algo_processes.h>
+#include <boxm/algo/pro/boxm_algo_register.h>
 
 int main() { return 0; }
