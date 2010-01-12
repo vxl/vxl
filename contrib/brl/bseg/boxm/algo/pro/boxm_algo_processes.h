5f67b220b118fe3ef67fd8e1809df2f714d33a4c
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2010-01-12 17:42:46 -0500

    Added boxm_crop_scene_process
    SVN-Revision: 27484

--- contrib/brl/bseg/boxm/algo/pro/boxm_algo_processes.h
+++ contrib/brl/bseg/boxm/algo/pro/boxm_algo_processes.h
@@ -8,5 +8,6 @@
 DECLARE_FUNC_CONS(boxm_split_scene_process);
 DECLARE_FUNC_CONS(boxm_merge_mixtures_process);
 DECLARE_FUNC_CONS(boxm_save_scene_vrml_process);
+DECLARE_FUNC_CONS(boxm_crop_scene_process);
 
 #endif
