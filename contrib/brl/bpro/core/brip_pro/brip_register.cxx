4bf17a215d2f08fc276a00d54617c1ce176c40d5
Gamze Tunali <gamze@users.sourceforge.net>
2010-01-21 11:33:37 -0500

    brip_detect_edges_process is added
    SVN-Revision: 27531

--- contrib/brl/bpro/core/brip_pro/brip_register.cxx
+++ contrib/brl/bpro/core/brip_pro/brip_register.cxx
@@ -1,5 +1,6 @@
 #include "brip_register.h"
 #include "brip_processes.h"
+#include "processes/brip_detect_edges_process.h"
 
 #include <bprb/bprb_macros.h>
 #include <bprb/bprb_func_process.h>
@@ -12,4 +13,5 @@ void brip_register::register_datatype()
 void brip_register::register_process()
 {
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, brip_extrema_process, "bripExtremaProcess");
+  REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, brip_detect_edges_process, "bripDetectEdgesProcess");
 }
