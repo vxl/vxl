6be5e35c42605a27ef8bce4cf41cc832f6293b09
Joseph L. Mundy <mundyj@users.sourceforge.net>
2010-01-21 14:04:46 -0500

    Split up raytrace functors
    SVN-Revision: 27532

---
+++ contrib/brl/bseg/boxm/opt/boxm_pre_infinity_functor.h
@@ -0,0 +1,65 @@
+#ifndef boxm_pre_infinity_functor_h
+#define boxm_pre_infinity_functor_h
+//:
+// \file
+#include <boxm/boxm_apm_traits.h>
+#include <boxm/opt/boxm_raytrace_function.h>
+#include <boxm/opt/boxm_rt_sample.h>
+#include <boxm/boxm_mog_grey_processor.h>
+#include <boxm/boxm_simple_grey_processor.h>
+#include <vil/vil_math.h>
+#include <vil/vil_save.h>
+#include <vcl_iostream.h>
+
+template <boxm_apm_type APM, class T_aux>
+class boxm_pre_infinity_functor
+{
+ public:
+  //: "default" constructor
+  boxm_pre_infinity_functor(vil_image_view<typename boxm_apm_traits<APM>::obs_datatype> const& image,
+                                          vil_image_view<float> &pre_inf, vil_image_view<float> &vis_inf)
+    : alpha_integral_(image.ni(),image.nj(),1), obs_(image), vis_img_(vis_inf), pre_img_(pre_inf)
+  {
+    alpha_integral_.fill(0.0f);
+    pre_img_.fill(0.0f);
+    vis_img_.fill(1.0f);
+    //only reads info from the scene
+    scene_read_only_=true;
+    //needs to write aux
+    is_aux_=true;
+  }
+
+  inline bool step_cell(unsigned int i, unsigned int j,
+                        vgl_point_3d<double> s0, vgl_point_3d<double> s1,
+                        boxm_sample<APM> &cell_value, T_aux & aux_val)
+  {
+    // compute segment length
+    const float seg_len = (float)(s1 - s0).length();
+    // compute average intensity for the cell
+    const float mean_obs_ =aux_val.obs_/aux_val.seg_len_;
+    // compute appearance probability of observation
+    const float PI  = boxm_apm_traits<APM>::apm_processor::prob_density(cell_value.appearance(),mean_obs_); // aux_val.PI_;
+    // update alpha integral
+    alpha_integral_(i,j) += cell_value.alpha * seg_len;
+    // compute new visibility probability with updated alpha_integral
+    const float vis_prob_end = vcl_exp(-alpha_integral_(i,j));
+    // compute weight for this cell
+    const float Omega = vis_img_(i,j) - vis_prob_end;
+    // and update pre
+    pre_img_(i,j) +=  PI * Omega;
+    // update visibility probabilty
+    vis_img_(i,j) = vis_prob_end;
+    return true;
+  }
+
+ public:
+  bool scene_read_only_;
+  bool is_aux_;
+  vil_image_view<float> alpha_integral_;
+ private:
+  vil_image_view<typename boxm_apm_traits<APM>::obs_datatype> const& obs_;
+  vil_image_view<float> &vis_img_;
+  vil_image_view<float> &pre_img_;
+};
+
+#endif // boxm_pre_infinity_functor_h
---
+++ contrib/brl/bseg/boxm/opt/boxm_seg_length_functor.h
@@ -0,0 +1,43 @@
+#ifndef boxm_seg_length_functor_h
+#define boxm_seg_length_functor_h
+//:
+// \file
+#include <boxm/boxm_apm_traits.h>
+#include <boxm/opt/boxm_raytrace_function.h>
+#include <boxm/opt/boxm_rt_sample.h>
+#include <boxm/boxm_mog_grey_processor.h>
+#include <boxm/boxm_simple_grey_processor.h>
+#include <vil/vil_math.h>
+#include <vil/vil_save.h>
+#include <vcl_iostream.h>
+
+template <boxm_apm_type APM, class T_aux>
+class boxm_seg_length_functor
+{
+ public:
+  boxm_seg_length_functor(vil_image_view<typename boxm_apm_traits<APM>::obs_datatype> &observation,
+                                          unsigned int ni,unsigned nj)
+    : obs_(observation)
+  {
+    scene_read_only_=true;
+    is_aux_=true;
+  }
+
+  inline bool step_cell(unsigned int i, unsigned int j, vgl_point_3d<double> s0, vgl_point_3d<double> s1,
+                        boxm_sample<APM> &cell_value,T_aux &aux_val)
+  {
+    const float seg_len = (float)(s1 - s0).length();
+    aux_val.obs_ += obs_(i,j) * seg_len;
+    aux_val.seg_len_ += seg_len;
+    return true;
+  }
+
+ public:
+  bool scene_read_only_;
+  bool is_aux_;
+
+ private:
+  vil_image_view<typename boxm_apm_traits<APM>::obs_datatype> &obs_;
+};
+
+#endif // boxm_seg_length_functor_h
