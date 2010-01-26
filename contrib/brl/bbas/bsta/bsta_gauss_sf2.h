94f179b28ca0ead7c72333a5504490f46229b9ee
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-01-26 14:33:00 -0500

    make sure Doxygen sees the full comment header
    SVN-Revision: 27574

--- contrib/brl/bbas/bsta/bsta_gauss_sf2.h
+++ contrib/brl/bbas/bsta/bsta_gauss_sf2.h
@@ -6,7 +6,7 @@
 // \brief A Gaussian with uniform variance in 2-d with type float
 // \author Joseph L. Mundy
 // \date August 8, 2009
-
+//
 // do not remove the following text
 // Approved for public release, distribution unlimited (DISTAR Case 14389)
 //
@@ -14,6 +14,5 @@
 #include "bsta_gaussian_sphere.h"
 
 typedef bsta_gaussian_sphere<float,2> bsta_gauss_sf2;
-
 
 #endif // bsta_gauss_sf2_h_
