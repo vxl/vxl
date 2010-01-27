6b7d4e6a1d0caf76220bd63efdfdc0a71e558c33
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2010-01-27 17:20:40 -0500

    boxm_scene_to_grid is added
    SVN-Revision: 27598

--- contrib/brl/bseg/boxm/algo/pro/boxm_algo_processes.h
+++ contrib/brl/bseg/boxm/algo/pro/boxm_algo_processes.h
@@ -9,5 +9,6 @@ DECLARE_FUNC_CONS(boxm_split_scene_process);
 DECLARE_FUNC_CONS(boxm_merge_mixtures_process);
 DECLARE_FUNC_CONS(boxm_save_scene_vrml_process);
 DECLARE_FUNC_CONS(boxm_crop_scene_process);
+DECLARE_FUNC_CONS(boxm_scene_to_bvxm_grid_process);
 
 #endif
