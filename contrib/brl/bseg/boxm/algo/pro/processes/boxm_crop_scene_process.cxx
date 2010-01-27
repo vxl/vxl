b0697c55420473b94fe3069c12481d48eb0e26fa
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2010-01-27 17:25:35 -0500

    added support for float case
    SVN-Revision: 27600

--- contrib/brl/bseg/boxm/algo/pro/processes/boxm_crop_scene_process.cxx
+++ contrib/brl/bseg/boxm/algo/pro/processes/boxm_crop_scene_process.cxx
@@ -27,17 +27,17 @@ namespace boxm_crop_scene_process_globals
 bool boxm_crop_scene_process_cons(bprb_func_process& pro)
 {
   using namespace boxm_crop_scene_process_globals;
-
+  
   vcl_vector<vcl_string> input_types_(n_inputs_);
   input_types_[0]="boxm_scene_base_sptr"; //the input scene
-                                           
+  
   //The output grid
   vcl_vector<vcl_string> output_types_(n_outputs_);
   output_types_[0]="boxm_scene_base_sptr";
-
+  
   if (!(pro.set_input_types(input_types_) && pro.set_output_types(output_types_))) 
     return false;
- 
+  
   return true;
 }
 
@@ -46,7 +46,7 @@ bool boxm_crop_scene_process_cons(bprb_func_process& pro)
 bool boxm_crop_scene_process(bprb_func_process& pro)
 {
   using namespace boxm_crop_scene_process_globals;
-
+  
   
   // check number of inputs
   if (pro.n_inputs() != n_inputs_)
@@ -82,15 +82,32 @@ bool boxm_crop_scene_process(bprb_func_process& pro)
                           vgl_point_3d<double>((corner_x + dimx), (corner_y + dimy), (corner_z + dimz)));
   
   //figure out the type
-  if (scene_ptr->appearence_model() == BOXM_APM_MOG_GREY) {
+  if (scene_ptr->appearence_model() == BOXM_APM_MOG_GREY)
+  {
     if (!scene_ptr->multi_bin())
     {
       boxm_scene<boct_tree<short,boxm_sample<BOXM_APM_MOG_GREY> > > *scene_in = 
       dynamic_cast<boxm_scene<boct_tree<short,boxm_sample<BOXM_APM_MOG_GREY> > >* > (scene_ptr.ptr());
       pro.set_output_val<boxm_scene_base_sptr>(0, boxm_crop_scene<boxm_sample<BOXM_APM_MOG_GREY> >(scene_in, bbox));
+      return true;
       
     }
+    
   }
-  return true;
+  if (scene_ptr->appearence_model() == FLOAT)
+  {
+    if (!scene_ptr->multi_bin())
+    {
+      boxm_scene<boct_tree<short,float > > *scene_in = 
+      dynamic_cast<boxm_scene<boct_tree<short,float > >* > (scene_ptr.ptr());
+      pro.set_output_val<boxm_scene_base_sptr>(0, boxm_crop_scene<float >(scene_in, bbox));
+      return true;
+    }
+  } 
   
- }
+}
+
+vcl_cerr<< "Scene type not supported in boxm_crop_scene_process" << vcl_endl;
+return false;
+
+}
