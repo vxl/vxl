9e91aad1bbc49901c9133c8f910597eaea116010
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-02-01 02:11:28 -0500

    bug fix: added "typename" in front of "vnl_vector<T>::size_type"
    SVN-Revision: 27636

--- contrib/conversions/math/vbl_array_to_vnl_vector.h
+++ contrib/conversions/math/vbl_array_to_vnl_vector.h
@@ -9,8 +9,8 @@
 template <class T>
 inline vnl_vector<T> vbl_array_to_vnl_vector(vbl_array_1d<T> a)
 {
-  assert(a.end()-a.begin() < vcl_numeric_limits<vnl_vector<T>::size_type>::max());
-  return vnl_vector<T>(a.begin(), static_cast<vnl_vector<T>::size_type>(a.end()-a.begin()));
+  assert(a.end() < a.begin() + vcl_numeric_limits<typename vnl_vector<T>::size_type>::max());
+  return vnl_vector<T>(a.begin(), static_cast<typename vnl_vector<T>::size_type>(a.end()-a.begin()));
 }
 
 #endif // vbl_array_to_vnl_vector_h_
