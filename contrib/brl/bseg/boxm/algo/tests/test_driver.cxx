8116d118b3900bdc09e1ab4478ce9a269ca85ec8
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2010-02-04 18:29:52 -0500

    added test for boxm_fill_internal_cells
    SVN-Revision: 27662

--- contrib/brl/bseg/boxm/algo/tests/test_driver.cxx
+++ contrib/brl/bseg/boxm/algo/tests/test_driver.cxx
@@ -2,12 +2,14 @@
 
 DECLARE( test_boxm_scene_crop );
 DECLARE( test_boxm_scene_to_bvxm_grid );
+DECLARE( test_boxm_fill_internal_cells );
 
 
 void register_tests()
 {
   REGISTER( test_boxm_scene_crop );
   REGISTER( test_boxm_scene_to_bvxm_grid );
+  REGISTER( test_boxm_fill_internal_cells );
 }
 
 
