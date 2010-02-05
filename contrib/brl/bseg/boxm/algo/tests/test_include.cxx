b48d05a697c3b1129ae96c51bd6a84d403ca875d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-02-05 02:39:07 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 27674

--- contrib/brl/bseg/boxm/algo/tests/test_include.cxx
+++ contrib/brl/bseg/boxm/algo/tests/test_include.cxx
@@ -1,8 +1,9 @@
 #include <boxm/algo/boxm_crop_scene.h>
+#include <boxm/algo/boxm_fill_internal_cells.h>
 #include <boxm/algo/boxm_merge_mog.h>
+#include <boxm/algo/boxm_scene_to_bvxm_grid.h>
 #include <boxm/algo/boxm_split_sample.h>
 #include <boxm/algo/boxm_vrml_util.h>
-#include <boxm/algo/boxm_scene_to_bvxm_grid.h>
 
 #include <boxm/algo/pro/boxm_algo_processes.h>
 #include <boxm/algo/pro/boxm_algo_register.h>
