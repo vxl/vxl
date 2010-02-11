95f88ac213443282fd953fabafc038c758bc97c6
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-02-11 18:18:46 -0500

    separated the two instantiations into two files
    SVN-Revision: 27764

--- contrib/brl/bseg/boxm/opt/Templates/boxm_cell_vis_graph_iterator+short.boxm_inf_line_sample+float--.cxx
+++ contrib/brl/bseg/boxm/opt/Templates/boxm_cell_vis_graph_iterator+short.boxm_inf_line_sample+float--.cxx
@@ -3,4 +3,4 @@
 #include <boxm/boxm_edge_tangent_sample.txx>
 #include <vgl/io/vgl_io_point_3d.h>
 
-BOXM_CELL_VIS_GRAPH_ITERATOR_INSTANTIATE(short, boxm_edge_tangent_sample<float>);
+BOXM_CELL_VIS_GRAPH_ITERATOR_INSTANTIATE(short, boxm_inf_line_sample<float>);
