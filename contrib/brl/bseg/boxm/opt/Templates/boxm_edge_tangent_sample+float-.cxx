223ff9305eb3424a8c964a28400cdfa91404617d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-02-13 08:51:49 -0500

    split up instantiation files for differnet classes
    SVN-Revision: 27789

--- contrib/brl/bseg/boxm/opt/Templates/boxm_edge_tangent_sample+float-.cxx
+++ contrib/brl/bseg/boxm/opt/Templates/boxm_edge_tangent_sample+float-.cxx
@@ -1,5 +1,3 @@
 #include <boxm/boxm_edge_tangent_sample.txx>
 
 BOXM_EDGE_TANGENT_SAMPLE_INSTANTIATE(float);
-BOXM_PLANE_OBS_INSTANTIATE(float);
-BOXM_INF_LINE_SAMPLE_INSTANTIATE(float);
---
+++ contrib/brl/bseg/boxm/opt/Templates/boxm_inf_line_sample+float-.cxx
@@ -0,0 +1,3 @@
+#include <boxm/boxm_inf_line_sample.txx>
+
+BOXM_INF_LINE_SAMPLE_INSTANTIATE(float);
