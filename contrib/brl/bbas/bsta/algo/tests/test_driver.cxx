c3498d53fd191d7662f6d51dfb88cbbacc57a4e0
Daniel Crispell <dcrispell@users.sourceforge.net>
2010-02-15 20:53:35 -0500

    Added methods for fitting gaussian distributions to sets of weighted samples (bsta_fit_gaussian.h) and tests of those functions (test_fit_gaussian.cxx)
    SVN-Revision: 27797

--- contrib/brl/bbas/bsta/algo/tests/test_driver.cxx
+++ contrib/brl/bbas/bsta/algo/tests/test_driver.cxx
@@ -3,9 +3,9 @@
 DECLARE( test_fit_weibull );
 DECLARE( test_gaussian_model );
 DECLARE( test_mean_shift );
+DECLARE( test_fit_gaussian );
 DECLARE( test_von_mises_update );
 DECLARE( test_beta_updater );
-
 
 void
 register_tests()
@@ -13,6 +13,7 @@ register_tests()
   REGISTER( test_fit_weibull );
   REGISTER( test_gaussian_model );
   REGISTER( test_mean_shift );
+  REGISTER( test_fit_gaussian );
   REGISTER( test_von_mises_update );
   REGISTER( test_beta_updater );
 
