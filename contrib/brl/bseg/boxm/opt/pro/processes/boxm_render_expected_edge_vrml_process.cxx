c5e2045d8dfad926c4bdfffd928ed92f2688a21a
Gamze Tunali <gamze@users.sourceforge.net>
2010-02-19 16:09:41 -0500

    cleaned up the code
    SVN-Revision: 27809

--- contrib/brl/bseg/boxm/opt/pro/processes/boxm_render_expected_edge_vrml_process.cxx
+++ contrib/brl/bseg/boxm/opt/pro/processes/boxm_render_expected_edge_vrml_process.cxx
@@ -33,9 +33,12 @@ namespace boxm_render_expected_edge_vrml_process_globals
 bool boxm_render_expected_edge_vrml_process_cons(bprb_func_process& pro)
 {
   using namespace boxm_render_expected_edge_vrml_process_globals;
-  //process takes 4 inputs
+  //process takes 5 inputs
   //input[0]: scene binary file
   //input[1]: camera
+  //input[2]: the path for vrml file
+  //input[3]: threshold
+  //input[4]: s (to write every s cell)
   vcl_vector<vcl_string> input_types_(n_inputs_);
   input_types_[0] = "boxm_scene_base_sptr";
   input_types_[1] = "vpgl_camera_double_sptr";
@@ -45,14 +48,8 @@ bool boxm_render_expected_edge_vrml_process_cons(bprb_func_process& pro)
   if (!pro.set_input_types(input_types_))
     return false;
 
-  // process has 1 output:
-  // output[0]: rendered image
-  // output[0]: mask
+  // process has no output:
   vcl_vector<vcl_string>  output_types_(n_outputs_);
-  //output_types_[0] = "vil_image_view_base_sptr";
-  //output_types_[1] = "vil_image_view_base_sptr";
-  //if (!pro.set_output_types(output_types_))
-  //  return false;
 
   return true;
 }
@@ -75,14 +72,8 @@ bool boxm_render_expected_edge_vrml_process(bprb_func_process& pro)
   int s = pro.get_input<int>(i++);
 
   vcl_ofstream stream(path.c_str());
-  // number of updates
-  //unsigned num_updates = pro.get_input<unsigned>(i++);
-  //vil_image_view_base_sptr img;
-  //vil_image_view_base_sptr img_mask;
 
   if (scene_ptr->appearence_model() == BOXM_EDGE_LINE) {
-//    vil_image_view<boxm_apm_traits<BOXM_EDGE_LINE>::obs_datatype> expected(ni,nj,3);
-//    vil_image_view<float> mask(ni,nj);
     if (!scene_ptr->multi_bin())
     {
       typedef boct_tree<short, boxm_inf_line_sample<float> > type;
@@ -91,7 +82,6 @@ bool boxm_render_expected_edge_vrml_process(bprb_func_process& pro)
         vcl_cout << "boxm_render_expected_edge_process: the scene is not of expected type" << vcl_endl;
         return false;
       }
-      //////////////////////////////
       bvrml_write::write_vrml_header(stream);
       vgl_point_3d<double> origin(450.0,450.0,0.0);
       vgl_vector_3d<double> dirx(1,0,0);
@@ -101,8 +91,6 @@ bool boxm_render_expected_edge_vrml_process(bprb_func_process& pro)
       bvrml_write::write_vrml_line(stream,origin,diry,3,0,1,0);
       bvrml_write::write_vrml_line(stream,origin,dirz,3,0,0,1);
 
-      //vgl_vector_3d<unsigned> dim=grid->grid_size();
-      //write the colors
       // for each block
       boxm_block_iterator<type> iter(scene);
       iter.begin();
@@ -133,42 +121,21 @@ bool boxm_render_expected_edge_vrml_process(bprb_func_process& pro)
               dir/=length;
               bvrml_write::write_vrml_line(stream, p0,dir,length,1.f,0.f,0.f);
             }
-#if 0
-            vgl_point_3d<double> point = tree->global_origin(cell);
-            // find the center of the cell
-            double l = tree->cell_length(cell);
-            point.set(point.x()+l/2.0, point.y()+l/2.0, point.z()+l/2.0);
-            bvrml_write::write_vrml_line(stream, point,dir,4.0f*(1.0f-.5f),1.f,0.f,0.f);
-           // bvrml_write::write_vrml_disk(stream, point,dir,2*(1.0-.5f),0.f,.5f,0.f);
-#endif
           }
         }
         iter++;
       }
-      //boxm_render_edge_tangent_image_rt<short, boxm_inf_line_sample<float> >(*scene, camera, expected, mask,n_normal,num_updates);
-      //////////////////////////////
     }
     else
     {
       vcl_cerr << "Ray tracing version not yet implemented\n";
       return false;
     }
- /*   img_mask = new vil_image_view<float>(mask);
-
-    vil_image_view<float> *ex = new vil_image_view<float>(ni,nj,expected.nplanes());
-    for (unsigned i=0; i<ni; i++) {
-      for (unsigned j=0;j<nj; j++) {
-        (*ex)(i,j) = expected(i,j);//static_cast<unsigned char>(255.0*(expected(i,j)));
-      }
-    }
-    img = ex;*/
   } else {
     vcl_cerr << "boxm_render_expected_edge_vrml_process: undefined APM type\n";
     return false;
   }
 
- // unsigned j = 0;
- // pro.set_output_val<vil_image_view_base_sptr>(j++, img);
- // pro.set_output_val<vil_image_view_base_sptr>(j++, img_mask);
+  // no output
   return true;
 }
