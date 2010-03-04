6e7a7540238aa0f031e564333257e354ca363b8d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-03-04 03:15:13 -0500

    never pass a read-only argument as refererence, always as const ref
    SVN-Revision: 27875

--- contrib/brl/bseg/boct/boct_tree.h
+++ contrib/brl/bseg/boct/boct_tree.h
@@ -44,16 +44,16 @@ class boct_tree
     num_levels_ = num_levels;
     max_val_ = (double)(1 << root_level_);
   }
-  
+
   //: Destructor
   ~boct_tree();
 
   //: Clones with the same data
   boct_tree<T_loc,T_data>* clone();
-  
+
   //: Clone a subtree determined by the root
   boct_tree<T_loc, T_data>* clone_subtree(boct_tree_cell<T_loc, T_data>* root, short num_levels);
-  
+
   //: Clones(from a root) the part of the subtree that intesects the bounding region
   boct_tree<T_loc, T_data>* clone_and_intersect(boct_tree_cell<T_loc, T_data>* subtree_root,
                                                 short parent_tree_root_level,
@@ -61,7 +61,7 @@ class boct_tree
 
   template <class T_data_to>
   boct_tree<T_loc,T_data_to>* clone_to_type() {
-    
+
     vcl_vector<boct_tree_cell<T_loc, T_data>*> cells = leaf_cells();
     vcl_vector<boct_tree_cell<T_loc, T_data_to> > cloned_cells;
     for (unsigned i=0; i<cells.size(); i++) {
@@ -88,11 +88,11 @@ class boct_tree
 
   //: Returns the smallest cell that entirely contains a 3d region in global coordinates
   boct_tree_cell<T_loc,T_data>* locate_region_global(const vgl_box_3d<double>& r, bool check_out_of_bounds = false);
-  
+
   //: Returns the smallest cell that entirely contains a 3d region in octree coordinates [0,1)x[0,1)x[0,1)
   boct_tree_cell<T_loc,T_data>* locate_region(const vgl_box_3d<double>& r, bool check_out_of_bounds = false);
-  
-  //: Returns all leaf cells entirely contained in 3d region (in octree coordinates [0,1)x[0,1)x[0,1)) 
+
+  //: Returns all leaf cells entirely contained in 3d region (in octree coordinates [0,1)x[0,1)x[0,1))
   vcl_vector<boct_tree_cell<T_loc,T_data>*> locate_region_leaves(const vgl_box_3d<double>& r)
   {
     boct_tree_cell<T_loc,T_data>* root = locate_region(r);
@@ -100,7 +100,7 @@ class boct_tree
     root->leaf_children(leaves);
     return leaves;
   }
-  
+
   //: Return cell with a partcular locational code
   boct_tree_cell<T_loc,T_data>* get_cell(  boct_loc_code<T_loc>& code) { return root_->traverse(code); }
 
@@ -121,10 +121,10 @@ class boct_tree
 
   //: Returns all cells at a specified level of the tree (wheather or not they are leafs)
   vcl_vector<boct_tree_cell<T_loc,T_data>*> cells_at_level(short l);
-  
+
   //: Returns all cells on the tree
   vcl_vector<boct_tree_cell<T_loc,T_data>*> all_cells();
-  
+
   //: Fills cells with the average of the children
   void fill_with_average();
 
@@ -141,7 +141,7 @@ class boct_tree
   short finest_level();
 
   //: Set the global-coordinates bounding box for the tree
-  void set_bbox(vgl_box_3d<double> & bbox) { global_bbox_=bbox; }
+  void set_bbox(vgl_box_3d<double> const& bbox) { global_bbox_=bbox; }
 
   //: Return the bounding box of a cell
   vgl_box_3d<double> cell_bounding_box(boct_tree_cell<T_loc,T_data>* const cell);
@@ -153,7 +153,7 @@ class boct_tree
   vgl_box_3d<double> cell_bounding_box_canonical(boct_tree_cell<T_loc,T_data>* const cell);
   //: Returns bounding box of the tree
   vgl_box_3d<double> bounding_box() const { return global_bbox_; }
-  
+
   //: Returns the length of a cell, assuming cell is cubical
   double cell_length(boct_tree_cell<T_loc,T_data>* const cell) const
   {
@@ -163,21 +163,21 @@ class boct_tree
   //: Return cell's local origin
   vgl_point_3d<double> local_origin(boct_tree_cell<T_loc,T_data>* const cell) const
   {
-    return vgl_point_3d<double> (cell->code_.x_loc_/max_val_,cell->code_.y_loc_/max_val_,cell->code_.z_loc_/max_val_);
+    return vgl_point_3d<double>(cell->code_.x_loc_/max_val_,cell->code_.y_loc_/max_val_,cell->code_.z_loc_/max_val_);
   }
-  
+
   //: Return cell's global origin
   vgl_point_3d<double> global_origin(boct_tree_cell<T_loc,T_data>* const cell) const
   {
-    return vgl_point_3d<double> (global_bbox_.min_x()+ (cell->code_.x_loc_/max_val_)*global_bbox_.width(),
-                                 global_bbox_.min_y()+ (cell->code_.y_loc_/max_val_)*global_bbox_.height(),
-                                 global_bbox_.min_z()+ (cell->code_.z_loc_/max_val_)*global_bbox_.depth());
+    return vgl_point_3d<double>(global_bbox_.min_x()+ (cell->code_.x_loc_/max_val_)*global_bbox_.width(),
+                                global_bbox_.min_y()+ (cell->code_.y_loc_/max_val_)*global_bbox_.height(),
+                                global_bbox_.min_z()+ (cell->code_.z_loc_/max_val_)*global_bbox_.depth());
   }
-  
+
 #if 0
   //: prunes the tree by deleting the nodes outside of the bounding box
   void prune_tree(vgl_box_3d<double> bb) { root_->prune(bb, max_val_); }
-#endif 
+#endif
 
   //: Print tree
   void print();
@@ -185,14 +185,13 @@ class boct_tree
   // Binary I/O
   void b_write(vsl_b_ostream & os, const bool save_internal_nodes);
   void b_read(vsl_b_istream & is);
-  static short version_no(bool save_internal_nodes) 
-  { 
-    if(save_internal_nodes)
-     return 1;
-    else 
+  static short version_no(bool save_internal_nodes)
+  {
+    if (save_internal_nodes)
+      return 1;
+    else
       return 2;
   }
-
 
  private:
   //: Maximum number of levels in the octree
