7d05c50e9b80af137b3b5a298f32187928c0d876
Vishal Jain <jainv@users.sourceforge.net>
2010-03-09 09:29:33 -0500

    added line_clipped_
    SVN-Revision: 27902

--- contrib/brl/bseg/boxm/boxm_inf_line_sample.h
+++ contrib/brl/bseg/boxm/boxm_inf_line_sample.h
@@ -6,6 +6,8 @@
 #include <vcl_iostream.h>
 #include <vsl/vsl_binary_io.h>
 #include <vgl/vgl_infinite_line_3d.h>
+#include <vgl/vgl_line_3d_2_points.h>
+
 #include <boxm/opt/boxm_aux_traits.h>
 
 template<class T>
@@ -13,14 +15,17 @@ class boxm_inf_line_sample
 {
  public:
   boxm_inf_line_sample() : 
-       line_(vgl_infinite_line_3d<T>(vgl_vector_2d<T>(0,0),vgl_vector_3d<T>(10,10,10))) {}
-  boxm_inf_line_sample(vgl_infinite_line_3d<T> line) : line_(line) {}
+       line_(vgl_infinite_line_3d<T>(vgl_vector_2d<T>(0,0),vgl_vector_3d<T>(10,10,10))),line_clipped_(vgl_point_3d<T>(0,0,0),vgl_point_3d<T>(0,0,0)), num_obs_(0){}
+  boxm_inf_line_sample(vgl_infinite_line_3d<T> line,unsigned num_obs=0) : line_(line),num_obs_(num_obs),line_clipped_(vgl_point_3d<T>(0,0,0),vgl_point_3d<T>(0,0,0)) {}
   ~boxm_inf_line_sample() {}
   static short version_no() { return 1; }
   void print(vcl_ostream& os) { os << "(line=" << line_ << ')';  }
 
   // public members
+   
   vgl_infinite_line_3d<T> line_;
+  vgl_line_3d_2_points<T> line_clipped_;
+  unsigned num_obs_;
 };
 
 template <class T>
--- contrib/brl/bseg/boxm/boxm_inf_line_sample.txx
+++ contrib/brl/bseg/boxm/boxm_inf_line_sample.txx
@@ -3,12 +3,15 @@
 
 #include <boxm/boxm_inf_line_sample.h>
 #include <vgl/io/vgl_io_infinite_line_3d.h> // for vsl_b_write(os, sample.line_);
+#include <vgl/io/vgl_io_line_3d_2_points.h> // for vsl_b_write(os, sample.line_clipped_);
 
 template <class T>
 void vsl_b_write(vsl_b_ostream & os, boxm_inf_line_sample<T> const &sample)
 {
   vsl_b_write(os, sample.version_no());
+  vsl_b_write(os, sample.num_obs_);
   vsl_b_write(os, sample.line_);
+  vsl_b_write(os, sample.line_clipped_);
 }
 
 template <class T>
@@ -29,7 +32,9 @@ void vsl_b_read(vsl_b_istream & is, boxm_inf_line_sample<T> &sample)
   switch (version)
   {
     case 1:
+      vsl_b_read(is, sample.num_obs_);
       vsl_b_read(is, sample.line_);
+      vsl_b_read(is, sample.line_clipped_);
       break;
     default:
       vcl_cerr << "I/O ERROR: vsl_b_read(vsl_b_istream&, boxm_inf_line_sample<T>&)\n"
