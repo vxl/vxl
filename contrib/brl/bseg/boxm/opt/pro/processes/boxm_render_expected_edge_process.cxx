075789771f2fae4b23ae1651298373b7a14b7ebd
Vishal Jain <jainv@users.sourceforge.net>
2010-03-09 11:16:03 -0500

    modified rendering of the image
    SVN-Revision: 27905

--- contrib/brl/bseg/boxm/opt/pro/processes/boxm_render_expected_edge_process.cxx
+++ contrib/brl/bseg/boxm/opt/pro/processes/boxm_render_expected_edge_process.cxx
@@ -110,9 +110,23 @@ bool boxm_render_expected_edge_process(bprb_func_process& pro)
       }
     }
     img = expected_byte;
+        img_mask = new vil_image_view<float>(mask);
+
+    vil_image_view<float> *ex = new vil_image_view<float>(ni,nj,expected.nplanes());
+    for (unsigned i=0; i<ni; i++) {
+      for (unsigned j=0; j<nj; j++) {
+        (*ex)(i,j) = expected(i,j);//static_cast<unsigned char>(255.0*(expected(i,j)));
+      }
+    }
+    img = ex;
+      unsigned j = 0;
+  pro.set_output_val<vil_image_view_base_sptr>(j++, img);
+  pro.set_output_val<vil_image_view_base_sptr>(j++, img_mask);
+  return true;
+
   }
   if (scene_ptr->appearence_model() == BOXM_EDGE_LINE) {
-    vil_image_view<boxm_apm_traits<BOXM_EDGE_LINE>::obs_datatype> expected(ni,nj,1);
+    vil_image_view<boxm_apm_traits<BOXM_EDGE_LINE>::apm_datatype> expected(ni,nj,1);
     vil_image_view<float> mask(ni,nj);
     if (!scene_ptr->multi_bin())
     {
@@ -122,29 +136,44 @@ bool boxm_render_expected_edge_process(bprb_func_process& pro)
         vcl_cout << "boxm_render_expected_edge_process: the scene is not of expected type" << vcl_endl;
         return false;
       }
-      boxm_render_edge_tangent_image_rt<short, boxm_inf_line_sample<float> >(*scene, camera, expected, mask,n_normal,num_updates);
+      boxm_render_edge_tangent_image_rt<short, boxm_inf_line_sample<float> >(*scene, camera, expected, n_normal,num_updates);
+
+    vil_image_view<float> *edge_image=new  vil_image_view<float>(ni,nj,3);
+    for (unsigned i=0; i<ni; i++) {
+      for (unsigned j=0; j<nj; j++) {
+        double u1,v1,u2,v2;
+        vgl_point_3d<float>  p1=expected(i,j).line_clipped_.point1();
+        vgl_point_3d<float>  p2=expected(i,j).line_clipped_.point2();
+        camera->project(p1.x(),p1.y(),p1.z(),u1,v1);
+        camera->project(p2.x(),p2.y(),p2.z(),u2,v2);
+        if((u1-u2)==0 && v1-v2==0)
+        {
+            (*edge_image)(i,j,0)=-1;
+            (*edge_image)(i,j,1)=-1;
+            (*edge_image)(i,j,2)=-1;
+        }
+        else
+        {
+        (*edge_image)(i,j,0)=(u1+u2)/2;
+        (*edge_image)(i,j,1)=(v1+v2)/2;
+        (*edge_image)(i,j,2)=vcl_atan2(v2-v1,u2-u1);
+        }
+      }
+    }
+    unsigned j = 0;
+    pro.set_output_val<vil_image_view_base_sptr>(j++,edge_image);
+    pro.set_output_val<vil_image_view_base_sptr>(j++,edge_image);
+  return true;
     }
     else
     {
       vcl_cerr << "Ray tracing version not yet implemented\n";
       return false;
     }
-    img_mask = new vil_image_view<float>(mask);
 
-    vil_image_view<float> *ex = new vil_image_view<float>(ni,nj,expected.nplanes());
-    for (unsigned i=0; i<ni; i++) {
-      for (unsigned j=0; j<nj; j++) {
-        (*ex)(i,j) = expected(i,j);//static_cast<unsigned char>(255.0*(expected(i,j)));
-      }
-    }
-    img = ex;
   } else {
     vcl_cerr << "boxm_render_expected_edge_process: undefined APM type\n";
     return false;
   }
 
-  unsigned j = 0;
-  pro.set_output_val<vil_image_view_base_sptr>(j++, img);
-  pro.set_output_val<vil_image_view_base_sptr>(j++, img_mask);
-  return true;
 }
