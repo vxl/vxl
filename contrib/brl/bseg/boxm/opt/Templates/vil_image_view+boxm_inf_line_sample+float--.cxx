1dfc6a039e0124814abac9eb450399052f7acec4
Gamze Tunali <gamze@users.sourceforge.net>
2010-03-09 10:53:48 -0500

    added a typedef
    SVN-Revision: 27904

--- contrib/brl/bseg/boxm/opt/Templates/vil_image_view+boxm_inf_line_sample+float--.cxx
+++ contrib/brl/bseg/boxm/opt/Templates/vil_image_view+boxm_inf_line_sample+float--.cxx
@@ -1,5 +1,5 @@
 #include <vil/vil_image_view.txx>
 #include <boxm/boxm_inf_line_sample.h>
 
-
-VIL_IMAGE_VIEW_INSTANTIATE(sample)
+typedef boxm_inf_line_sample<float> sample;
+VIL_IMAGE_VIEW_INSTANTIATE(sample);
