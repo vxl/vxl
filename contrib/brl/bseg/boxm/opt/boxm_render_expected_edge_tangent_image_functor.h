6320d2979f39b02c78870fc651a77c3b6958b6a7
Vishal Jain <jainv@users.sourceforge.net>
2010-03-12 14:10:46 -0500

    Modified toinclude subpixel
    SVN-Revision: 27933

--- contrib/brl/bseg/boxm/opt/boxm_render_expected_edge_tangent_image_functor.h
+++ contrib/brl/bseg/boxm/opt/boxm_render_expected_edge_tangent_image_functor.h
@@ -9,6 +9,7 @@
 #include <boxm/boxm_mob_grey_processor.h>
 #include <boxm/boxm_edge_tangent_sample.h>
 #include <boxm/boxm_inf_line_sample.h>
+#include <boxm/boxm_line_samples.h>
 #include <vnl/vnl_math.h>
 #include <vil/vil_transform.h>
 #include <vbl/vbl_array_2d.h>
@@ -22,14 +23,16 @@ class boxm_render_expected_edge_tangent_image_functor
 {
  public:
   //: "default" constructor
-  boxm_render_expected_edge_tangent_image_functor(vil_image_view<typename boxm_apm_traits<APM>::apm_datatype> &expected,
+  boxm_render_expected_edge_tangent_image_functor(vil_image_view<float > &expected,
+                                                  vpgl_camera_double_sptr cam,
+                                                  float threshold,
                                                   unsigned int /*ni*/=0, unsigned /*nj*/=0,
                                                   bool scene_read_only=false,
                                                   bool is_aux=true)
-                                                  : expected_(expected), num_obs_(expected.ni(),expected.nj())
+                                                  : cam_(cam),residual(threshold),expected_(expected), num_obs_(expected.ni(),expected.nj())
   {
-    typename boxm_apm_traits<APM>::apm_datatype nil;
-    expected_.fill(nil);
+    //boxm_line_samples<float>  nil;
+    expected_.fill(-1);
     num_obs_.fill(0);
     scene_read_only_=scene_read_only;
     is_aux_=is_aux;
@@ -41,12 +44,28 @@ class boxm_render_expected_edge_tangent_image_functor
                         boxm_inf_line_sample<typename boxm_apm_traits<APM>::obs_datatype> &cell_value,
                         T_aux aux_val)
   {
-    typedef typename boxm_apm_traits<APM>::obs_datatype t;
 
-    if(cell_value.num_obs_>num_obs_(i,j))
+    //vcl_cout<< cell_value.residual_<<" ";
+    if(cell_value.num_obs_>2 && cell_value.residual_<residual && cell_value.num_obs_>num_obs_(i,j))
     {
         num_obs_(i,j)=cell_value.num_obs_;
-        expected_(i,j)=cell_value;
+        double u1,v1,u2,v2;
+        vgl_point_3d<float>  p1=cell_value.line_clipped_.point1();
+        vgl_point_3d<float>  p2=cell_value.line_clipped_.point2();
+        cam_->project(p1.x(),p1.y(),p1.z(),u1,v1);
+        cam_->project(p2.x(),p2.y(),p2.z(),u2,v2);
+        if((u1-u2)==0 && v1-v2==0)
+        {
+            expected_(i,j,0)=-1;
+            expected_(i,j,1)=-1;
+            expected_(i,j,2)=-1;
+        }
+        else
+        {
+        expected_(i,j,0)=(u1+u2)/2;
+        expected_(i,j,1)=(v1+v2)/2;
+        expected_(i,j,2)=angle_0_360(vcl_atan2(v2-v1,u2-u1));
+        }
     }
     return true;
   }
@@ -55,17 +74,20 @@ class boxm_render_expected_edge_tangent_image_functor
   bool is_aux_;
 
  private:
-  vil_image_view<typename boxm_apm_traits<APM>::apm_datatype> &expected_;
+  vil_image_view<float > &expected_;
   vil_image_view<unsigned> num_obs_;
+  vpgl_camera_double_sptr cam_;
+  float residual;
 
 };
 
 template <class T_loc, class T_data>
 void boxm_render_edge_tangent_image_rt(boxm_scene<boct_tree<T_loc, T_data > > &scene,
                                        vpgl_camera_double_sptr cam,
-                                      vil_image_view<typename boxm_apm_traits<BOXM_EDGE_LINE>::apm_datatype> &expected,
+                                       vil_image_view<float> &expected,
                                        float n_normal,
-                                       unsigned int num_samples) //degree of freedom
+                                       unsigned int num_samples,
+                                       float threshold) //degree of freedom
 {
   
   typedef boxm_aux_traits<BOXM_AUX_NULL>::sample_datatype sample_datatype;
@@ -73,7 +95,7 @@ void boxm_render_edge_tangent_image_rt(boxm_scene<boct_tree<T_loc, T_data > > &s
   typedef boxm_render_expected_edge_tangent_image_functor<BOXM_EDGE_LINE,boxm_edge_tangent_sample<sample_datatype> > expfunctor;
   boxm_raytrace_function<expfunctor,T_loc, T_data, boxm_edge_tangent_sample<sample_datatype> > raytracer
     (scene,aux_scene, cam.ptr(),expected.ni(),expected.nj());
-  expfunctor exp_functor(expected,expected.ni(),expected.nj(),true,false);
+  expfunctor exp_functor(expected,cam,threshold,expected.ni(),expected.nj(),true,false);
   raytracer.run(exp_functor);
 
 }
