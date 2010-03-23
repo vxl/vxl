c7f4e4b9cc6c64d83860a26f430b309bb01f68e5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-03-23 09:26:43 -0400

    added a "float" instantation for all classes that had only a "double" instantiation
    SVN-Revision: 28008

---
+++ core/vgl/Templates/vgl_cylinder+float-.cxx
@@ -0,0 +1,3 @@
+// Instantiation of vgl_cylinder<float>
+#include <vgl/vgl_cylinder.txx>
+VGL_CYLINDER_INSTANTIATE(float);
---
+++ core/vgl/Templates/vgl_sphere_3d+float-.cxx
@@ -0,0 +1,3 @@
+// Instantiation of vgl_sphere_3d<float>
+#include <vgl/vgl_sphere_3d.txx>
+VGL_SPHERE_3D_INSTANTIATE(float);
