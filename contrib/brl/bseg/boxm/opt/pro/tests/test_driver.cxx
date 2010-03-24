8ad23ed881ba88182757b87f5feb6b217f3cf00a
Gamze Tunali <gamze@users.sourceforge.net>
2010-03-24 16:01:16 -0400

    a new process boxm_propagate_plane_process and related test is added
    SVN-Revision: 28021

---
+++ contrib/brl/bseg/boxm/opt/pro/tests/test_driver.cxx
@@ -0,0 +1,10 @@
+#include <testlib/testlib_register.h>
+
+DECLARE( test_boxm_plane_propagation_process );
+
+void register_tests()
+{
+  REGISTER( test_boxm_plane_propagation_process );
+}
+
+DEFINE_MAIN;
