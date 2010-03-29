fda73dc14490fd80a3254e20091c0d3195cfb0a0
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-03-29 16:59:43 -0400

    added missing implementation of angle_0_360()
    SVN-Revision: 28057

--- contrib/brl/bseg/boxm/opt/boxm_render_expected_edge_tangent_image_functor.h
+++ contrib/brl/bseg/boxm/opt/boxm_render_expected_edge_tangent_image_functor.h
@@ -17,6 +17,13 @@
 #include <vcl_iostream.h>
 #endif
 
+inline static double angle_0_360(double ang)
+{
+  while (ang<0) ang += 2*vnl_math::pi;
+  while (ang > 2.0*vnl_math::pi) ang -= 2*vnl_math::pi;
+  return ang;
+}
+
 template <boxm_apm_type APM, class T_aux>
 class boxm_render_expected_edge_tangent_image_functor
 {
@@ -42,28 +49,27 @@ class boxm_render_expected_edge_tangent_image_functor
                         boxm_inf_line_sample<typename boxm_apm_traits<APM>::obs_datatype> &cell_value,
                         T_aux aux_val)
   {
-
-    //vcl_cout<< cell_value.residual_<<" ";
-    if(cell_value.num_obs_>2 && cell_value.residual_<residual && cell_value.num_obs_>num_obs_(i,j))
+    //vcl_cout<< cell_value.residual_<<' ';
+    if (cell_value.num_obs_>2 && cell_value.residual_<residual && cell_value.num_obs_>num_obs_(i,j))
     {
-        num_obs_(i,j)=cell_value.num_obs_;
-        double u1,v1,u2,v2;
-        vgl_point_3d<float>  p1=cell_value.line_clipped_.point1();
-        vgl_point_3d<float>  p2=cell_value.line_clipped_.point2();
-        cam_->project(p1.x(),p1.y(),p1.z(),u1,v1);
-        cam_->project(p2.x(),p2.y(),p2.z(),u2,v2);
-        if((u1-u2)==0 && v1-v2==0)
-        {
-            expected_(i,j,0)=-1;
-            expected_(i,j,1)=-1;
-            expected_(i,j,2)=-1;
-        }
-        else
-        {
+      num_obs_(i,j)=cell_value.num_obs_;
+      double u1,v1,u2,v2;
+      vgl_point_3d<float>  p1=cell_value.line_clipped_.point1();
+      vgl_point_3d<float>  p2=cell_value.line_clipped_.point2();
+      cam_->project(p1.x(),p1.y(),p1.z(),u1,v1);
+      cam_->project(p2.x(),p2.y(),p2.z(),u2,v2);
+      if ((u1-u2)==0 && v1-v2==0)
+      {
+        expected_(i,j,0)=-1;
+        expected_(i,j,1)=-1;
+        expected_(i,j,2)=-1;
+      }
+      else
+      {
         expected_(i,j,0)=(u1+u2)/2;
         expected_(i,j,1)=(v1+v2)/2;
         expected_(i,j,2)=angle_0_360(vcl_atan2(v2-v1,u2-u1));
-        }
+      }
     }
     return true;
   }
@@ -76,7 +82,6 @@ class boxm_render_expected_edge_tangent_image_functor
   vil_image_view<unsigned> num_obs_;
   vpgl_camera_double_sptr cam_;
   float residual;
-
 };
 
 template <class T_loc, class T_data>
@@ -87,14 +92,12 @@ void boxm_render_edge_tangent_image_rt(boxm_scene<boct_tree<T_loc, T_data > > &s
                                        unsigned int num_samples,
                                        float threshold) //degree of freedom
 {
-  
   typedef boxm_aux_traits<BOXM_AUX_NULL>::sample_datatype sample_datatype;
   boxm_aux_scene<T_loc, T_data,boxm_edge_tangent_sample<sample_datatype> > aux_scene(&scene,boxm_aux_traits<BOXM_AUX_NULL>::storage_subdir(), boxm_aux_scene<T_loc, T_data,boxm_edge_tangent_sample<sample_datatype> >::LOAD);
   typedef boxm_render_expected_edge_tangent_image_functor<BOXM_EDGE_LINE,sample_datatype > expfunctor;
   boxm_raytrace_function<expfunctor,T_loc, T_data> raytracer(scene, cam.ptr(),expected.ni(),expected.nj());
   expfunctor exp_functor(expected,cam,threshold,expected.ni(),expected.nj(),true,false);
   raytracer.run(exp_functor);
-
 }
 
 #endif
