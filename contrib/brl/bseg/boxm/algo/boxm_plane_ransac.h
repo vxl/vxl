e1c88e17f3952d38b788157de27b95afc4773ab5
Gamze Tunali <gamze@users.sourceforge.net>
2010-03-29 09:23:25 -0400

    added code for plane ransac and associated test
    SVN-Revision: 28046

---
+++ contrib/brl/bseg/boxm/algo/boxm_plane_ransac.h
@@ -0,0 +1,54 @@
+#ifndef boxm_plane_ransac_h_
+#define boxm_plane_ransac_h_
+
+#include <vgl/vgl_infinite_line_3d.h>
+#include <vgl/vgl_intersection.h>
+#include <vgl/vgl_plane_3d.h>
+#include <vgl/vgl_vector_3d.h>
+
+#include <vcl_cstdlib.h> // for rand()
+#include <vcl_iostream.h>
+
+template <class T>
+void boxm_plane_ransac(vcl_vector<vgl_plane_3d<T> >& planes, 
+                       //vcl_vector<vgl_plane_3d<T> >& fit_planes,
+                       vcl_vector<unsigned>& indices,
+                       int threshold)
+{
+  int num_planes = planes.size();
+
+  while (indices.size() < threshold) {
+    //fit_planes.resize(0);
+    indices.resize(0);
+    // select two planes randomly
+    int index1 = vcl_rand() % num_planes;
+    int index2 = vcl_rand() % num_planes;
+    vgl_plane_3d<T> plane1 = planes[index1];
+    vgl_plane_3d<T> plane2 = planes[index2];
+
+    // intersect them to get a line
+    vgl_infinite_line_3d<T> line;
+    if (vgl_intersection(plane1, plane2, line)) {
+      vgl_vector_3d<T> line_dir = line.direction();
+
+      // test the line on each plane
+      for (unsigned i=0; i<num_planes; i++) {
+        vgl_plane_3d<T> plane = planes[i];
+        vgl_vector_3d<T> normal = plane.normal();
+        // see if the line direction and plane normal is perpendicular
+        T res = dot_product(normal,line_dir);
+        if (res < 0.0001)  {
+          vgl_point_3d<T> p=line.point();
+          if (plane.contains(p,0.0001)) {
+            vcl_cout << "Adding " << i << "th plane!" << vcl_endl;
+            //fit_planes.push_back(plane);
+            indices.push_back(i);
+          }
+        }
+      }
+    }
+    vcl_cout << "the number of fit planes=" << indices.size() << vcl_endl;
+  }
+}
+
+#endif
--- contrib/brl/bseg/boxm/algo/tests/test_driver.cxx
+++ contrib/brl/bseg/boxm/algo/tests/test_driver.cxx
@@ -3,13 +3,14 @@
 DECLARE( test_boxm_scene_crop );
 DECLARE( test_boxm_scene_to_bvxm_grid );
 DECLARE( test_boxm_fill_internal_cells );
-
+DECLARE( test_boxm_plane_ransac );
 
 void register_tests()
 {
   REGISTER( test_boxm_scene_crop );
   REGISTER( test_boxm_scene_to_bvxm_grid );
   REGISTER( test_boxm_fill_internal_cells );
+  REGISTER( test_boxm_plane_ransac );
 }
 
 
