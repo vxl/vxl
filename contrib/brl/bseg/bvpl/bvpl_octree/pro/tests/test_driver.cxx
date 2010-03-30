3012db4110458fa8bdac575521c80f129174784c
Gamze Tunali <gamze@users.sourceforge.net>
2010-03-30 16:33:54 -0400

    tests lib added for bvpl_octree/pro
    SVN-Revision: 28069

---
+++ contrib/brl/bseg/bvpl/bvpl_octree/pro/tests/test_driver.cxx
@@ -0,0 +1,10 @@
+#include <testlib/testlib_register.h>
+
+DECLARE( test_bvpl_plane_propagation_process );
+
+void register_tests()
+{
+  REGISTER( test_bvpl_plane_propagation_process );
+}
+
+DEFINE_MAIN;
---
+++ contrib/brl/bseg/bvpl/bvpl_octree/pro/tests/test_include.cxx
@@ -0,0 +1,4 @@
+#include <bvpl/bvpl_octree/pro/bvpl_octree_processes.h>
+#include <bvpl/bvpl_octree/pro/bvpl_octree_register.h>
+
+int main() { return 0; }
