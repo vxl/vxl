a4e5f703e0bb01fb1d82a4713c8e862b274947df
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-04-13 13:45:21 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 28151

--- contrib/brl/bseg/boxm/opt/tests/test_include.cxx
+++ contrib/brl/bseg/boxm/opt/tests/test_include.cxx
@@ -1,5 +1,7 @@
 #include <boxm/opt/boxm_aux_scene.h>
 #include <boxm/opt/boxm_aux_traits.h>
+#include <boxm/opt/boxm_change_3d_functor.h>
+#include <boxm/opt/boxm_change_probability_functor.h>
 #include <boxm/opt/boxm_edge_tangent_updater.h>
 #include <boxm/opt/boxm_edge_updater.h>
 #include <boxm/opt/boxm_generate_edge_sample_functor.h>
@@ -19,9 +21,11 @@
 #include <boxm/opt/boxm_render_expected_edge_image_functor.h>
 #include <boxm/opt/boxm_render_expected_edge_tangent_image_functor.h>
 #include <boxm/opt/boxm_render_expected_image_functor.h>
+#include <boxm/opt/boxm_render_expected_shadow_image_functor.h>
 #include <boxm/opt/boxm_rpc_registration.h>
 #include <boxm/opt/boxm_rt_sample.h>
 #include <boxm/opt/boxm_seg_length_functor.h>
+#include <boxm/opt/boxm_sun_vis_prob_functor.h>
 #include <boxm/opt/boxm_update_image_functor.h>
 
 #include <boxm/opt/pro/boxm_opt_processes.h>
