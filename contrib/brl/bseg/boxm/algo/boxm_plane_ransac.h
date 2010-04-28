28bc8eefd214abf420754bd6bb3a76b794105cff
Vishal Jain <jainv@users.sourceforge.net>
2010-04-28 16:05:01 -0400

    moved the code fragment to check intersection of line with the box to the plane_ransac.
    SVN-Revision: 28193

--- contrib/brl/bseg/boxm/algo/boxm_plane_ransac.h
+++ contrib/brl/bseg/boxm/algo/boxm_plane_ransac.h
@@ -63,7 +63,7 @@ bool boxm_plane_ransac(vcl_vector<boxm_edge_tangent_sample<T> > aux_samples,
                   T res = dot_product(normal,line_dir);
                   if (vcl_fabs(res) < 0.05)  {
                       vgl_point_3d<T> p=vgl_closest_point<T>(inters_line,local_origin);
-                      if (plane.contains(p,cell_global_box.width()/8)) 
+                      if (plane.contains(p,cell_global_box.width()/4)) 
                       {
                           fit_planes.push_back(plane);
                           ws.push_back(weights[i]);
@@ -88,17 +88,19 @@ bool boxm_plane_ransac(vcl_vector<boxm_edge_tangent_sample<T> > aux_samples,
               T res=0;
               vgl_infinite_line_3d<T> l;
               bool good = vgl_intersection(fit_planes, ws, l, res);
-              if (good) {
+              vgl_point_3d<double> p0,p1;
+              vgl_vector_2d<double> x0(l.x0().x(), l.x0().y());
+              vgl_vector_3d<double> dir(l.direction().x(), l.direction().y(), l.direction().z());
+
+              if (good&&vgl_intersection<double>(cell_global_box, vgl_infinite_line_3d<double>(x0,dir), p0, p1)) {
                   if (res<min_res) {
                       min_res=res;
                       line=l;
-                      //vcl_cout<<" "<<line.direction();
                       set=true;
                   }
               }
           
           }
-       //vcl_cout<<"\n";
       }
   }
   residual=min_res;
