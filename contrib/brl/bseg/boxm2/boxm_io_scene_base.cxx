3ada3210bc1d694f0c801ee429649a96565a133d
Gamze Tunali <gamze@users.sourceforge.net>
2010-05-05 11:04:45 -0400

    boxm lib is restructured into boxm2
    SVN-Revision: 28212

---
+++ contrib/brl/bseg/boxm2/boxm_io_scene_base.cxx
@@ -0,0 +1,25 @@
+#include "boxm_io_scene_base.h"
+//:
+// \file
+
+
+//: Binary write boxm scene to stream
+void vsl_b_write(vsl_b_ostream& /*os*/, boxm_scene_base const& /*scene_base*/)
+{
+}
+
+
+//: Binary load boxm scene from stream.
+void vsl_b_read(vsl_b_istream& /*is*/, boxm_scene_base& /*scene_base*/)
+{
+}
+
+//: Binary write boxm scene pointer to stream
+void vsl_b_read(vsl_b_istream& /*is*/, boxm_scene_base* /*ph*/)
+{
+}
+
+//: Binary write boxm scene pointer to stream
+void vsl_b_write(vsl_b_ostream& /*os*/, boxm_scene_base* const& /*ph*/)
+{
+}
