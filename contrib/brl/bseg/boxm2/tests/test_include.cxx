767314fb64279fe91939af4db608f58aa92d7c8b
Gamze Tunali <gamze@users.sourceforge.net>
2010-05-05 15:43:04 -0400

    boxm lib is restructured into boxm2
    SVN-Revision: 28214

---
+++ contrib/brl/bseg/boxm2/tests/test_include.cxx
@@ -0,0 +1,13 @@
+#include <boxm2/boxm_apm_traits.h>
+#include <boxm2/boxm_aux_scene.h>
+#include <boxm2/boxm_aux_traits.h>
+#include <boxm2/boxm_block.h>
+#include <boxm2/boxm_io_scene_base.h>
+#include <boxm/boxm_scene.h>
+#include <boxm2/boxm_scene_base.h>
+#include <boxm2/boxm_scene_parser.h>
+
+#include <boxm2/pro/boxm_processes.h>
+#include <boxm2/pro/boxm_register.h>
+
+int main() { return 0; }
