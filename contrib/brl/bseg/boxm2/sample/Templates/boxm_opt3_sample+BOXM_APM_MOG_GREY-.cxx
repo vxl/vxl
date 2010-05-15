9009504003f7adecb13c6d9f1be170be1e6ccebb
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-05-15 13:56:10 -0400

    added dummy implementation for a "missing symbol" which should not be in library boxm2_sample but in boxm_sample_algo
    SVN-Revision: 28324

--- contrib/brl/bseg/boxm2/sample/Templates/boxm_opt3_sample+BOXM_APM_MOG_GREY-.cxx
+++ contrib/brl/bseg/boxm2/sample/Templates/boxm_opt3_sample+BOXM_APM_MOG_GREY-.cxx
@@ -1,3 +1,6 @@
 #include <boxm2/sample/boxm_opt3_sample.txx>
 
+// DUMMY IMPLEMENTIATION of a static method of library boxm2_sample_algo, to avoid "undefined symbol" error in library boxm2_sample:
+bool boxm_mog_grey_processor::update(bsta_num_obs<bsta_mixture_fixed<bsta_num_obs<bsta_gaussian_sphere<float,1> >,3> >&, float const&, float const&) { return false; }
+
 BOXM2_OPT3_SAMPLE_INSTANTIATE(BOXM_APM_MOG_GREY);
