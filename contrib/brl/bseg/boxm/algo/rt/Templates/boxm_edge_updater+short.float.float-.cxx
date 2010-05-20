ce39262bcaf999038cd6d796dd475dc866e2c81b
Vishal Jain <jainv@users.sourceforge.net>
2010-05-20 10:16:17 -0400

    Added missing templates.
    SVN-Revision: 28364

---
+++ contrib/brl/bseg/boxm/algo/rt/Templates/boxm_edge_updater+short.float.float-.cxx
@@ -0,0 +1,3 @@
+#include <boxm/algo/rt/boxm_edge_updater.txx>
+
+BOXM_EDGE_UPDATER_INSTANTIATE(short,float,float);
---
+++ contrib/brl/bseg/boxm/algo/rt/Templates/boxm_opt2_optimizer+short.BOXM_APM_MOG_GREY.BOXM_AUX_OPT2_GREY-.cxx
@@ -0,0 +1,4 @@
+#include <boxm/algo/rt/boxm_opt2_optimizer.txx>
+#include <boxm/boxm_apm_traits.h>
+
+BOXM_OPT2_OPTIMIZER_INSTANTIATE(short,BOXM_APM_MOG_GREY,BOXM_AUX_OPT2_GREY);
