94030515401aaef02a2a78ad5bb01c71a9b8d226
Vishal Jain <jainv@users.sourceforge.net>
2010-05-21 15:42:10 -0400

    adding a functor to compute no of rays.
    SVN-Revision: 28375

--- contrib/brl/bseg/boxm/sample/Templates/boxm_aux_scene+short.boxm_sample+BOXM_APM_SIMPLE_GREY-.boxm_scalar_sample+float--.cxx
+++ contrib/brl/bseg/boxm/sample/Templates/boxm_aux_scene+short.boxm_sample+BOXM_APM_SIMPLE_GREY-.boxm_scalar_sample+float--.cxx
@@ -5,3 +5,4 @@
 #include <boxm/boxm_aux_scene.txx>
 
 BOXM_AUX_SCENE_INSTANTIATE(short,boxm_sample<BOXM_APM_SIMPLE_GREY>,boxm_scalar_sample<float>);
+BOXM_AUX_SCENE_INSTANTIATE(short,boxm_sample<BOXM_APM_MOG_GREY>,boxm_scalar_sample<float>);
