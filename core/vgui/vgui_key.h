9370c22f2a9dccb07036708fc3a4edb449d1c0f4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-05-29 14:03:36 -0400

    avoid compiler warnings: explicitly added all used keys to enum vgui_key
    SVN-Revision: 28454

--- core/vgui/vgui_key.h
+++ core/vgui/vgui_key.h
@@ -13,10 +13,23 @@
 //: Enumeration of possible key strokes.
 enum vgui_key {
   vgui_KEY_NULL = 0,
-  vgui_ESC=27, /* */ vgui_ESCAPE=27,
   vgui_TAB='\t',
-  vgui_RETURN='\r',
   vgui_NEWLINE='\n',
+  vgui_RETURN='\r',
+  vgui_ESC=27, /* */ vgui_ESCAPE=27,
+  vgui_STAR='*', // 42
+  vgui_MINUS='-', // 45
+  vgui_SLASH='/', // 47
+  vgui_EQUALS='=', // 61
+  vgui_LEFTBRACKET='[', // 91
+  vgui_RIGHTBRACKET=']', // 93
+  vgui_CHAR_c='c', // 99
+  vgui_CHAR_d='d', // 100
+  vgui_CHAR_n='n', // 110
+  vgui_CHAR_x='x', // 120
+  vgui_CHAR_z='z', // 122
+  vgui_LEFTBRACE='{', // 123
+  vgui_RIGHTBRACE='{', // 125
   vgui_KEY_XXX=0x100, // non-ascii encoded keys below :
   vgui_F1, vgui_F2 , vgui_F3 , vgui_F4 , // function keys
   vgui_F5, vgui_F6 , vgui_F7 , vgui_F8 ,
