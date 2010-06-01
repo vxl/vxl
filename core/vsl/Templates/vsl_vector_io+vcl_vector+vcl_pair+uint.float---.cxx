3a4d09c1e8198926dcbda799acfa792d38e194b0
Graham Vincent <grahamrvincent@users.sourceforge.net>
2010-06-01 10:28:45 -0400

    template instantiation
    SVN-Revision: 28466

---
+++ core/vsl/Templates/vsl_vector_io+vcl_vector+vcl_pair+uint.float---.cxx
@@ -0,0 +1,5 @@
+#include <vcl_utility.h>
+#include <vsl/vsl_pair_io.h>
+#include <vsl/vsl_vector_io.txx>
+typedef vcl_vector<vcl_pair<unsigned int,float> > vector_pair_uf;
+VSL_VECTOR_IO_INSTANTIATE(vector_pair_uf);
