4bb55ce79207314d39c0bfa63813fc29c0831f6b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-06-04 02:26:21 -0400

    removed duplicate #include <testlib/testlib_test.h>
    SVN-Revision: 28491

--- contrib/mul/vimt3d/tests/test_run_length_encoding.cxx
+++ contrib/mul/vimt3d/tests/test_run_length_encoding.cxx
@@ -3,13 +3,11 @@
 #include <vcl_iostream.h>
 #include <vcl_fstream.h>
 #include <vcl_utility.h>
-#include <testlib/testlib_test.h>
 #include <vimt3d/vimt3d_run_length_encoding.h>
 
 
 void test_run_length_encoding1()
 {
-
   {
     vimt3d_image_3d_of<float> image(9,10,11);
     image.image().fill(0);
@@ -28,13 +26,11 @@ void test_run_length_encoding1()
   vimt3d_run_length_decode(in,image2);
   in.close();
  
-  TEST("Run length encoded image relaoded correcttly: ", image2.image()(1,2,3)==4 , true );
-  TEST("Run length encoded image relaoded correcttly: ", image2.image()(3,6,4)==5, true );
-  TEST("Run length encoded image relaoded correcttly: ", image2.image()(8,7,4)==6 , true );
-  TEST("Run length encoded image relaoded correcttly: ", image2.image()(8,9,10)==14 , true );
-  TEST("Run length encoded image relaoded correcttly: ", image2.image()(1,2,2)==0 , true );
-
-
+  TEST("Run length encoded image relaoded correctly: ", image2.image()(1,2,3)==4 , true );
+  TEST("Run length encoded image relaoded correctly: ", image2.image()(3,6,4)==5, true );
+  TEST("Run length encoded image relaoded correctly: ", image2.image()(8,7,4)==6 , true );
+  TEST("Run length encoded image relaoded correctly: ", image2.image()(8,9,10)==14 , true );
+  TEST("Run length encoded image relaoded correctly: ", image2.image()(1,2,2)==0 , true );
 }
 
 MAIN( test_run_length_encoding )
@@ -44,5 +40,4 @@ MAIN( test_run_length_encoding )
 
   SUMMARY();
 }
-
 
