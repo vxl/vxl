4e12a453bcaa9862958aa0c5d9ab262ec37e92f2
Andrew Miller <andrew-miller@users.sourceforge.net>
2010-06-07 12:36:49 -0400

    bocl_manager+ commented out for linux compilation
    SVN-Revision: 28525

--- contrib/brl/bseg/boxm/ocl/Templates/bocl_manager+boxm_ray_trace_manager+boxm_sample+BOXM_APM_SIMPLE_GREY---.cxx
+++ contrib/brl/bseg/boxm/ocl/Templates/bocl_manager+boxm_ray_trace_manager+boxm_sample+BOXM_APM_SIMPLE_GREY---.cxx
@@ -1,9 +1,9 @@
-#include <bocl/bocl_manager.txx>
-#include <boxm/ocl/boxm_ray_trace_manager.h>
-#include <boxm/boxm_apm_traits.h>
+//#include <bocl/bocl_manager.txx>
+//#include <boxm/ocl/boxm_ray_trace_manager.h>
+//#include <boxm/boxm_apm_traits.h>
 
 
 
-typedef boxm_ray_trace_manager<boxm_sample<BOXM_APM_SIMPLE_GREY> > boxm_ray_trace_manager_simple_grey;
+//typedef boxm_ray_trace_manager<boxm_sample<BOXM_APM_SIMPLE_GREY> > boxm_ray_trace_manager_simple_grey;
 
-BOCL_MANAGER_INSTANTIATE(boxm_ray_trace_manager_simple_grey );
+//BOCL_MANAGER_INSTANTIATE(boxm_ray_trace_manager_simple_grey );
--- contrib/brl/bseg/boxm/ocl/Templates/bocl_manager+boxm_ray_trace_manager+float--.cxx
+++ contrib/brl/bseg/boxm/ocl/Templates/bocl_manager+boxm_ray_trace_manager+float--.cxx
@@ -1,7 +1,7 @@
-#include <bocl/bocl_manager.txx>
-#include <boxm/ocl/boxm_ray_trace_manager.h>
-#include <boxm/boxm_apm_traits.h>
+//#include <bocl/bocl_manager.txx>
+//#include <boxm/ocl/boxm_ray_trace_manager.h>
+//#include <boxm/boxm_apm_traits.h>
 
-typedef boxm_ray_trace_manager<float > boxm_ray_trace_manager_float;
+//typedef boxm_ray_trace_manager<float > boxm_ray_trace_manager_float;
 
-BOCL_MANAGER_INSTANTIATE(boxm_ray_trace_manager_float );
+//BOCL_MANAGER_INSTANTIATE(boxm_ray_trace_manager_float );
