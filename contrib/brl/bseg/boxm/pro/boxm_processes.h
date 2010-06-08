d31ba9f99d6c6c4b755c1c6271cfb1cf2d3861ab
Vishal Jain <jainv@users.sourceforge.net>
2010-06-08 10:58:30 -0400

    Added a process to force write blocks in case of load_all_blocks
    SVN-Revision: 28541

--- contrib/brl/bseg/boxm/pro/boxm_processes.h
+++ contrib/brl/bseg/boxm/pro/boxm_processes.h
@@ -12,4 +12,5 @@ DECLARE_FUNC_CONS(boxm_proj_local_cam_process);
 DECLARE_FUNC_CONS(boxm_camera_viewing_scene_process);
 DECLARE_FUNC_CONS(boxm_upload_mesh_process);
 DECLARE_FUNC_CONS(boxm_clean_scene_process);
+DECLARE_FUNC_CONS(boxm_force_write_blocks_process);
 #endif
