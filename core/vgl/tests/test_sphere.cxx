e61644a9093d96a25345e5092cdd2f0d8e91c2e1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-06-12 13:00:54 -0400

    added some comments
    SVN-Revision: 28568

--- core/vgl/tests/test_sphere.cxx
+++ core/vgl/tests/test_sphere.cxx
@@ -1,4 +1,4 @@
-// Some tests for vgl_sphere
+// Some tests for vgl_sphere_3d
 // Ian Scott, Aug 2005.
 #include <testlib/testlib_test.h>
 #include <vgl/vgl_sphere_3d.h>
@@ -9,36 +9,32 @@ static void test_sphere()
 {
   vcl_cout << "Simple sphere\n";
 
-  // Simple triangle
+  // Default sphere
   vgl_sphere_3d<double> s;
-
-  
-  TEST("default sphere is empty", s.is_empty(), true);
-  
+  // Unit sphere, centered at the origin
   vgl_sphere_3d<double> u(0, 0, 0, 1.0);
+  
+  TEST("default sphere is empty",  s.is_empty(), true);
   TEST("unit sphere is not empty", u.is_empty(), false);
 
-  TEST("O not inside empty sphere",s.contains( vgl_point_3d<double>(0,0,0) ), false);
-  TEST("O inside unit sphere",u.contains( vgl_point_3d<double>(0,0,0) ), true);
-  TEST("(1,0,0) inside unit sphere",u.contains( vgl_point_3d<double>(1,0,0) ), true);
-  TEST("(1,1,0) outside unit sphere",u.contains( vgl_point_3d<double>(1,1,0) ), false);
+  TEST("origin is not inside empty sphere",s.contains( vgl_point_3d<double>(0,0,0) ), false);
+  TEST("origin is inside unit sphere",     u.contains( vgl_point_3d<double>(0,0,0) ), true);
+  TEST("(1,0,0) is inside unit sphere",    u.contains( vgl_point_3d<double>(1,0,0) ), true);
+  TEST("(1,1,0) is outside unit sphere",   u.contains( vgl_point_3d<double>(1,1,0) ), false);
 
-
+  // l1 is the X-axis
   vgl_line_3d_2_points<double> l1(vgl_point_3d<double>(-2,0,0),vgl_point_3d<double>(2,0,0));
   vgl_point_3d<double> p1, p2;
   TEST("clip x-axis to empty sphere", s.clip(l1, p1, p2), false);
-  TEST("clip x-axis to unit sphere", u.clip(l1, p1, p2), true);
-  TEST("clip x-axis to unit sphere 1", p1, vgl_point_3d<double>(-1,0,0));
-  TEST("clip x-axis to unit sphere 2", p2, vgl_point_3d<double>(1,0,0));
+  TEST("clip x-axis to unit sphere",  u.clip(l1, p1, p2), true);
+  TEST("Intersection point 1", p1, vgl_point_3d<double>(-1,0,0));
+  TEST("Intersection point 2", p2, vgl_point_3d<double>(1,0,0));
 
+  // l2 is the line (y=1,z=0) parallel to the X axis, touching the unit sphere in (0,1,0)
   vgl_line_3d_2_points<double> l2(vgl_point_3d<double>(-2,1,0),vgl_point_3d<double>(2,1,0));
-  TEST("clip y,z=1,0 to unit sphere", u.clip(l2, p1, p2), true);
-  TEST("clip y,z=1,0 to unit sphere 1", p1, vgl_point_3d<double>(0,1,0));
-  TEST("clip y,z=1,0 to unit sphere 2", p2, vgl_point_3d<double>(0,1,0));
-
+  TEST("clip (y=1,z=0) to unit sphere", u.clip(l2, p1, p2), true);
+  TEST("Intersection point 1", p1, vgl_point_3d<double>(0,1,0));
+  TEST("Intersection point 2", p2, vgl_point_3d<double>(0,1,0));
 }
-
-
-
 
 TESTMAIN(test_sphere);
