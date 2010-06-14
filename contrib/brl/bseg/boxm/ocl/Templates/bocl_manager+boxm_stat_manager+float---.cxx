97a89e996d42e615b8d58c2b690386aae276dbba
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-06-14 16:23:51 -0400

    renamed template instantiation file (standardised file naming)
    SVN-Revision: 28584

--- contrib/brl/bseg/boxm/ocl/Templates/bocl_manager+boxm_stat_manager+float---.cxx
+++
@@ -1,7 +0,0 @@
-#include <bocl/bocl_manager.txx>
-#include <boxm/ocl/boxm_stat_manager.h>
-#include <boxm/boxm_apm_traits.h>
-
-typedef boxm_stat_manager<float > boxm_stat_manager_float;
-
-BOCL_MANAGER_INSTANTIATE(boxm_stat_manager_float );
---
+++ contrib/brl/bseg/boxm/ocl/Templates/bocl_manager+boxm_stat_manager+float--.cxx
@@ -0,0 +1,6 @@
+#include <bocl/bocl_manager.txx>
+#include <boxm/ocl/boxm_stat_manager.h>
+#include <boxm/boxm_apm_traits.h>
+
+typedef boxm_stat_manager<float> boxm_stat_manager_float;
+BOCL_MANAGER_INSTANTIATE(boxm_stat_manager_float);
