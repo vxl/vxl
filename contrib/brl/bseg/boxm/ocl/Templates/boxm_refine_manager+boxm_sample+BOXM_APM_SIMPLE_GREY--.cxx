ba47d22b411abd3887a7aacf3b987ba06bffefd9
Andrew Miller <andrew-miller@users.sourceforge.net>
2010-06-16 09:26:50 -0400

    opencl refine templates added
    SVN-Revision: 28597

---
+++ contrib/brl/bseg/boxm/ocl/Templates/boxm_refine_manager+boxm_sample+BOXM_APM_SIMPLE_GREY--.cxx
@@ -0,0 +1,4 @@
+#include <boxm/boxm_apm_traits.h>
+#include <boxm/ocl/boxm_refine_manager.txx>
+
+BOXM_REFINE_MANAGER_INSTANTIATE(boxm_sample<BOXM_APM_SIMPLE_GREY> );
---
+++ contrib/brl/bseg/boxm/ocl/Templates/boxm_refine_manager+float-.cxx
@@ -0,0 +1,2 @@
+#include <boxm/ocl/boxm_refine_manager.txx>
+BOXM_REFINE_MANAGER_INSTANTIATE(float );
