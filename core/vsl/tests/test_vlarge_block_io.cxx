debf9173a40a207ed137fa636428fedd0732f94f
Ian Scott <scottim@users.sourceforge.net>
2010-06-16 11:25:57 -0400

    Fix error introduced into manual test. record additional manual tests.
    SVN-Revision: 28603

--- core/vsl/tests/test_vlarge_block_io.cxx
+++ core/vsl/tests/test_vlarge_block_io.cxx
@@ -56,7 +56,7 @@ void test_vlarge_block(void * block, vcl_size_t s, T scale)
 
   vcl_size_t errors=0;
   for (vcl_size_t i=0; i<n; ++i)
-    if (numbers[i] != static_cast<T>(vcl_numeric_limits<T>::max() - T(i)) )
+    if (numbers[i] != static_cast<T>(vcl_numeric_limits<T>::max() - T(i) * scale) )
       errors++;
 
   TEST_NEAR("No errors in stored numbers", static_cast<double>(errors), 0, 0);
