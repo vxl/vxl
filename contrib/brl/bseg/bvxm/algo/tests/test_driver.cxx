ae8f9e43a567d65d8ace71cc95031554c21f90ed
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2010-06-25 12:33:34 -0400

    added missing test
    SVN-Revision: 28702

--- contrib/brl/bseg/bvxm/algo/tests/test_driver.cxx
+++ contrib/brl/bseg/bvxm/algo/tests/test_driver.cxx
@@ -2,12 +2,14 @@
 
 DECLARE( test_merge_mog );
 DECLARE( test_mog_norm );
+DECLARE( test_boxm_scene_to_bvxm_grid);
 
 
 void register_tests()
 {
   REGISTER( test_merge_mog );
   REGISTER( test_mog_norm );
+  REGISTER( test_boxm_scene_to_bvxm_grid );
 
 }
 
