2b0808af9e3d0875126d4ed73151b38919b35fab
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-06-28 14:19:32 -0400

    avoid compiler warning (no EOLN at EOF)
    SVN-Revision: 28733

--- contrib/brl/bseg/boxm/ocl/Templates/boxm_render_image_manager+boxm_sample+BOXM_APM_SIMPLE_GREY--.cxx
+++ contrib/brl/bseg/boxm/ocl/Templates/boxm_render_image_manager+boxm_sample+BOXM_APM_SIMPLE_GREY--.cxx
@@ -1,5 +1,4 @@
 #include <boxm/ocl/boxm_render_image_manager.txx>
 #include <boxm/boxm_apm_traits.h>
 
-
-BOXM_RENDER_IMAGE_MANAGER_INSTANTIATE(boxm_sample<BOXM_APM_SIMPLE_GREY> );
+BOXM_RENDER_IMAGE_MANAGER_INSTANTIATE(boxm_sample<BOXM_APM_SIMPLE_GREY>);
