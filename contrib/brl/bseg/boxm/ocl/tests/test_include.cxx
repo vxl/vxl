52b78ba7644c7fb04d58d97e91ec0382af4f1819
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-07-06 08:24:06 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 28809

--- contrib/brl/bseg/boxm/ocl/tests/test_include.cxx
+++ contrib/brl/bseg/boxm/ocl/tests/test_include.cxx
@@ -1,10 +1,12 @@
 #include <boxm/ocl/boxm_ocl_refine_scene.h>
 #include <boxm/ocl/boxm_ocl_render_expected.h>
+#include <boxm/ocl/boxm_ocl_scene.h>
 #include <boxm/ocl/boxm_ocl_utils.h>
 #include <boxm/ocl/boxm_online_update_manager.h>
 #include <boxm/ocl/boxm_ray_bundle_trace_manager.h>
 #include <boxm/ocl/boxm_ray_trace_manager.h>
 #include <boxm/ocl/boxm_refine_manager.h>
+#include <boxm/ocl/boxm_refine_scene_manager.h>
 #include <boxm/ocl/boxm_render_image_manager.h>
 #include <boxm/ocl/boxm_render_single_block_manager.h>
 #include <boxm/ocl/boxm_stack_ray_trace_manager.h>
