458c500e768b120bec36711b39a79f5a28a61684
Ian Scott <scottim@users.sourceforge.net>
2010-07-20 11:03:53 -0400

    Better error reporting
    SVN-Revision: 28922

--- contrib/mul/mbl/mbl_exception.h
+++ contrib/mul/mbl/mbl_exception.h
@@ -145,7 +145,7 @@ void mbl_exception_warning(T exception)
     vcl_string filename_;
    public:
      mbl_exception_parse_file_error(const vcl_string &msg, const vcl_string& filename)
-      : mbl_exception_parse_error(msg+" "+filename), filename_(filename) {}
+      : mbl_exception_parse_error(msg+" in "+filename), filename_(filename) {}
     const char * filename() const {return filename_.c_str();}
     virtual ~mbl_exception_parse_file_error() throw() {}
   };
