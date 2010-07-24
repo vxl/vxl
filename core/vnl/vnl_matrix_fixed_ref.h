13f9c26378d1c8961bb6864d888db931a6f5cce7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-07-24 15:57:18 -0400

    disambiguating use of method of templated parent class (needed by some compilers...)
    SVN-Revision: 28976

--- core/vnl/vnl_matrix_fixed_ref.h
+++ core/vnl/vnl_matrix_fixed_ref.h
@@ -653,7 +653,7 @@ class vnl_matrix_fixed_ref : public vnl_matrix_fixed_ref_const<T,num_rows,num_co
   //: Return true if *this == rhs
   bool operator_eq (vnl_matrix_fixed_ref_const<T,num_rows,num_cols> const & rhs) const
   {
-    return equal( this->data_block(), rhs.data_block() );
+    return vnl_matrix_fixed_ref<T,num_rows,num_cols>::equal( this->data_block(), rhs.data_block() );
   }
 
   //: Equality operator
