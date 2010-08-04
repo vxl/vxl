3ff957799f7db0d64d8fa1cccaf2c41da0eb7dfb
Vishal Jain <jainv@users.sourceforge.net>
2010-08-04 09:14:52 -0400

    cleaning up the library
    SVN-Revision: 29101

--- contrib/brl/bseg/boxm/ocl/Templates/bocl_manager+boxm_stack_ray_trace_manager+boxm_sample+BOXM_APM_MOG_GREY---.cxx
+++
@@ -1,9 +0,0 @@
-#include <bocl/bocl_manager.txx>
-#include <boxm/ocl/boxm_stack_ray_trace_manager.h>
-#include <boxm/boxm_apm_traits.h>
-
-
-
-typedef boxm_stack_ray_trace_manager<boxm_sample<BOXM_APM_MOG_GREY> > boxm_stack_ray_trace_manager_mog_grey;
-
-BOCL_MANAGER_INSTANTIATE(boxm_stack_ray_trace_manager_mog_grey );
