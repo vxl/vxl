469608e2dbd54ed2552e656612b0bc8d3c1e9022
Vishal Jain <jainv@users.sourceforge.net>
2010-08-04 09:59:54 -0400

    removed stack based implementation
    SVN-Revision: 29105

--- contrib/brl/bseg/boxm/ocl/pro/boxm_ocl_register.cxx
+++ contrib/brl/bseg/boxm/ocl/pro/boxm_ocl_register.cxx
@@ -13,7 +13,6 @@ void boxm_ocl_register::register_datatype()
 void boxm_ocl_register::register_process()
 {
     REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm_ocl_render_expected_process,"boxmOclRenderExpectedProcess");
-    REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm_ocl_stack_render_expected_process,"boxmOclStackRenderExpectedProcess");
     REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm_ocl_ray_bundle_expected_process,"boxmOclRayBundleExpectedProcess");
     REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm_ocl_refine_process,"boxmOclRefineProcess");
     REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm_ocl_render_all_blocks_expected_process,"boxmOclRenderAllBlocksExpectedProcess");
