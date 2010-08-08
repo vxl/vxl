349d36cd4fbf8ef70f75f8a0b430e606c5617ce8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-08-08 16:00:37 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 29160

--- contrib/gel/mrc/vpgl/ihog/tests/test_include.cxx
+++ contrib/gel/mrc/vpgl/ihog/tests/test_include.cxx
@@ -1,5 +1,13 @@
-#include <ihog/ihog_world_roi.h>
-#include <ihog/ihog_lsqr_cost_func.h>
+#include <vpgl/ihog/ihog_cost_func.h>
+#include <vpgl/ihog/ihog_lsqr_cost_func.h>
+#include <vpgl/ihog/ihog_minimizer.h>
+#include <vpgl/ihog/ihog_region.h>
+#include <vpgl/ihog/ihog_super_res.h>
+#include <vpgl/ihog/ihog_transform_2d.h>
+#include <vpgl/ihog/ihog_world_roi.h>
 
+#include <vpgl/ihog/ihog_region_sptr.h>
+#include <vpgl/ihog/ihog_transform_2d_sptr.h>
+#include <vpgl/ihog/ihog_world_roi_sptr.h>
 
 int main() { return 0; }
