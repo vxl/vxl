6ad282f20ffab33304a1b492d9e31ac1df4cfcd6
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-08-17 17:40:48 -0400

    renamed from bocl_manager+bit_tree_test_manager.cxx to bocl_manager+bit_tree_test_manager-.cxx (standard file naming for template instantiation files)
    SVN-Revision: 29273

--- contrib/brl/bseg/boxm/ocl/tests/Templates/bocl_manager+bit_tree_test_manager-.cxx
+++ contrib/brl/bseg/boxm/ocl/tests/Templates/bocl_manager+bit_tree_test_manager-.cxx
@@ -1,4 +1,4 @@
 #include <bocl/bocl_manager.txx>
 #include <boxm/ocl/tests/bit_tree_test_manager.h>
 
-BOCL_MANAGER_INSTANTIATE(bit_tree_test_manager );
+BOCL_MANAGER_INSTANTIATE(bit_tree_test_manager);
