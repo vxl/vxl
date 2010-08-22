8d76a8e0957c6fd12c18f4cf13a00674c3ad889f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-08-22 10:28:51 -0400

    renamed locally defined "vcl_pow" to "int_pow", to avoid name clash with std::pow
    SVN-Revision: 29331

--- core/vnl/tests/test_pow_log.cxx
+++ core/vnl/tests/test_pow_log.cxx
@@ -7,22 +7,22 @@
 // certainly in those cases where b is relatively small.
 // Negative exponents make of course no sense since the result must be int.
 // Beware of overflow!
-inline static int vcl_pow(int a, unsigned int b)
+inline static int int_pow(int a, unsigned int b)
 {
   if (b==0) return 1;
   else if (b==1) return a;
-  else return vcl_pow(a*a,b/2) * vcl_pow(a, b%2);
+  else return int_pow(a*a,b/2) * int_pow(a, b%2);
 }
 
 // A recursive implementation for a^b with a double and b integer;
 // this is a more accurate alternative for std::pow(double a,double b),
 // certainly in those cases where b is relatively small.
-inline static double vcl_pow(double a, int b)
+inline static double int_pow(double a, int b)
 {
   if (b==0) return 1;
   else if (b==1) return a;
-  else if (b<0) return vcl_pow(1.0/a, -b);
-  else return vcl_pow(a*a,b/2) * vcl_pow(a, b%2);
+  else if (b<0) return int_pow(1.0/a, -b);
+  else return int_pow(a*a,b/2) * int_pow(a, b%2);
 }
 
 // An implementation for floor(log(a)/log(8)) with integer argument a;
@@ -40,29 +40,29 @@ inline static int log8(unsigned int a)
 
 static void test_int_pow()
 {
-  TEST("Exponent 0", vcl_pow(-33, 0U), 1);
-  TEST("Exponent 1", vcl_pow(-33, 1U), -33);
-  TEST("Exponent of 1", vcl_pow(1, 12345U), 1);
-  TEST("Even exponent of -1", vcl_pow(-1, 12346U), 1);
-  TEST("Odd exponent of -1", vcl_pow(-1, 12345U), -1);
-  TEST("Large power of 2", vcl_pow(2, 30U), 0x40000000);
-  TEST("Power of 2 with overflow", vcl_pow(2, 31U), -0x80000000);
-  TEST("Power of 2 with even more overflow", vcl_pow(2, 32U), 0);
-  TEST("Just a \"random\" case...", vcl_pow(-19, 7U), -893871739);
+  TEST("Exponent 0", int_pow(-33, 0U), 1);
+  TEST("Exponent 1", int_pow(-33, 1U), -33);
+  TEST("Exponent of 1", int_pow(1, 12345U), 1);
+  TEST("Even exponent of -1", int_pow(-1, 12346U), 1);
+  TEST("Odd exponent of -1", int_pow(-1, 12345U), -1);
+  TEST("Large power of 2", int_pow(2, 30U), 0x40000000);
+  TEST("Power of 2 with overflow", int_pow(2, 31U), -0x80000000);
+  TEST("Power of 2 with even more overflow", int_pow(2, 32U), 0);
+  TEST("Just a \"random\" case...", int_pow(-19, 7U), -893871739);
 }
 
 static void test_dbl_pow()
 {
-  TEST("Exponent 0", vcl_pow(-33.3, 0), 1.0);
-  TEST("Exponent 1", vcl_pow(-33.3, 1), -33.3);
-  TEST("Exponent -1", vcl_pow(0.4, -1), 2.5);
-  TEST("Even exponent of -1", vcl_pow(-1.0, 12346), 1.0);
-  TEST("Odd exponent of -1", vcl_pow(-1.0, 12345), -1.0);
-  TEST("Just a small \"random\" case...", vcl_pow(-23.0, 7), -3404825447.0);
+  TEST("Exponent 0", int_pow(-33.3, 0), 1.0);
+  TEST("Exponent 1", int_pow(-33.3, 1), -33.3);
+  TEST("Exponent -1", int_pow(0.4, -1), 2.5);
+  TEST("Even exponent of -1", int_pow(-1.0, 12346), 1.0);
+  TEST("Odd exponent of -1", int_pow(-1.0, 12345), -1.0);
+  TEST("Just a small \"random\" case...", int_pow(-23.0, 7), -3404825447.0);
   // for small x: (1+x)^a = 1 + ax + a(a-1)/2 x^2 + ...
-  TEST_NEAR("And a larger example...", vcl_pow(1.00000001, 900), 1.000009000040455, 1e-13);
-  TEST_NEAR("And a large example...", vcl_pow(-10.0, 300), 1e300, 1e285);
-  TEST_NEAR("Negative exponent", vcl_pow(-10.0, -300), 1e-300, 1e-313);
+  TEST_NEAR("And a larger example...", int_pow(1.00000001, 900), 1.000009000040455, 1e-13);
+  TEST_NEAR("And a large example...", int_pow(-10.0, 300), 1e300, 1e285);
+  TEST_NEAR("Negative exponent", int_pow(-10.0, -300), 1e-300, 1e-313);
 }
 
 static void test_log()
