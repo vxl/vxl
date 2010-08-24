1b72ade7e67ff0e8ffa3832ff5ea419ffbeba80c
Gamze Tunali <gamze@users.sourceforge.net>
2010-08-24 14:35:49 -0400

    a new class vil_pyramid_image_view is added
    SVN-Revision: 29347

---
+++ core/vil/vil_pyramid_image_view.h
@@ -0,0 +1,109 @@
+#ifndef vil_pyramid_image_view_h_
+#define vil_pyramid_image_view_h_
+#ifdef VCL_NEEDS_PRAGMA_INTERFACE
+#pragma interface
+#endif
+//:
+// \file
+// \brief Representation of a pyramid hierarchy of image views. 
+//        The images can be passed to the view (in this case they are any 
+//        size or scale but sorted based on the scale in the descending order, 
+//        or can be generated in here (default case).
+//        By default: the levels of views are half the size of the previous 
+//        level and image scales are in the descending order. The biggest image 
+//        is at level 0 and with scale 1.0 (1,1.0), and the next levels goes like: 
+//        (1,0.5), (2,0.25) etc..
+//
+// \author Gamze D. Tunali
+// \date   Aug 16, 2010
+
+#include <vcl_vector.h>
+#include <vil/vil_image_view.h>
+#include <vil/vil_image_view_base.h>
+#include <vil/vil_smart_ptr.h>
+#include <vil/vil_pyramid_image_resource.h>
+
+template <class T>
+class vil_pyramid_image_view 
+{
+ public:
+
+    //: Default constructor, creates and empty list of pyramid
+    vil_pyramid_image_view(): nlevels_(0), max_levels_(256) {}
+
+    //: Creates a pyramid with one image only and it's scale is set to 1.0 (biggest)
+    vil_pyramid_image_view(vil_image_view_base_sptr image): nlevels_(1), max_levels_(256)
+      {images_.push_back(image); scales_.push_back(1.0); }
+
+    //: Creates a pyramid with one image only and it's scale is set to 1.0 (biggest)
+    vil_pyramid_image_view(const vil_image_view<T>& image): nlevels_(1), max_levels_(256)
+      {images_.push_back(new vil_image_view<T>(image)); scales_.push_back(1.0); }
+
+    //: Creates a pyramid of nlevels and sets the image at the scale 1.0. 
+    // It creates smaller images for the smalles scales. Each image is 
+    // half the the size of the previous image
+    vil_pyramid_image_view(vil_image_view_base_sptr image, unsigned nlevels);
+
+    //: creates a pyramid of empty image views
+    vil_pyramid_image_view(unsigned levels, unsigned ni, unsigned nj, unsigned n_planes=1);
+
+    //: Creates an pyramid of given image views with associates scales, 
+    // post: sorted in the descending order of the scales
+    vil_pyramid_image_view(vcl_vector<vil_image_view_base_sptr> const& images, 
+                           vcl_vector<double> const& scales);
+
+    //: Copy construct.
+    // The new object will point to the same underlying image as the rhs.
+    vil_pyramid_image_view(const vil_pyramid_image_view<T>& rhs);
+
+    virtual ~vil_pyramid_image_view() {}
+
+    //: adds a view to the list of view sorted in the right place based on the 
+    // scale in tde descending order
+    void add_view(vil_image_view_base_sptr &image, double scale);
+
+    vil_image_view_base_sptr get_view(unsigned level, double& scale) { scale=scales_[level]; return images_[level];  }
+
+    //: The pixel type of the images
+    typedef T pixel_type;
+
+    void set_max_level(unsigned l) { max_levels_=l; }
+
+    unsigned max_levels() const { return max_levels_; }
+
+    //: Number of pyramid levels
+    unsigned nlevels() const { return nlevels_; }
+
+    const vil_pyramid_image_view<T>& operator=(const vil_pyramid_image_view<T>& rhs);
+
+    vil_image_view<T>& operator()(unsigned l) { return static_cast<vil_image_view<T>&>(*images_[l]); }
+
+    // iterators
+    typedef vil_image_view_base_sptr iterator;
+    inline iterator begin() { return images_[0]; }
+    inline iterator end  () { return images_[images_.size()]; }
+
+ protected:
+    //: the list of image vieas
+    vcl_vector<vil_image_view_base_sptr> images_;
+
+    //: the associated scales of images, scales_.size() is always equals to images_.size()
+    vcl_vector<double> scales_;
+
+    // the number of images in the view, 0 if it is empty
+    unsigned nlevels_;
+
+    // this is the number of levels that cannot be exceeded, by default it is 256
+    unsigned max_levels_;
+
+
+    //: returns true if the image size is < 4x4 or the max_level is reached
+    inline bool limit_reached(unsigned i, unsigned j) 
+      {if (i<2 || j<2 || nlevels_==max_levels_) return true; return false; }
+
+    //: generates an image half the size of the given image and takes the averages 
+    // of pixel values in 4x4 neighborhoods
+    void scale_down(const vil_image_view<T>&, vil_image_view_base_sptr& image_out);
+};
+
+#endif
---
+++ core/vil/vil_pyramid_image_view.txx
@@ -0,0 +1,135 @@
+#ifndef vil_pyramid_image_view_txx_
+#define vil_pyramid_image_view_txx_
+
+#include "vil_pyramid_image_view.h"
+#include <vil/vil_image_view.h>
+
+template <class T>
+vil_pyramid_image_view<T>::vil_pyramid_image_view(unsigned levels, unsigned ni, 
+                                                  unsigned nj, unsigned n_planes)
+                                                  : nlevels_(levels), max_levels_(256)
+{
+  images_.resize(levels);
+  scales_.resize(levels);
+  unsigned i=ni, j=nj;
+  double scale=1.0;
+  for (unsigned l=0; l<levels && !limit_reached(i,j); l++) {
+    images_[l] = new vil_image_view<T>(i, j, n_planes);
+    scales_[l] = scale;
+    i/=2;j/=2;
+    scale/=2.0;
+  }
+}
+
+template <class T>
+vil_pyramid_image_view<T>::vil_pyramid_image_view(vil_image_view_base_sptr image, unsigned levels)
+: nlevels_(levels), max_levels_(256)
+{
+ 
+  vil_image_view<T>* img=dynamic_cast<vil_image_view<T>*>(image.ptr());
+  if (!img)
+    return;
+  
+  // it works with grey scale images
+  if (img->nplanes() != 1)
+    return;
+    
+  images_.resize(levels);
+  scales_.resize(levels);
+  unsigned i=image->ni()/2, j=image->nj()/2;
+  double scale=1.0;
+  images_[0] = image;
+  scales_[0] = 1.0;  
+    
+  for (unsigned l=1; l<levels && !limit_reached(i,j); l++) {
+    // scale down the image
+    scale/=2.0;
+    img = static_cast<vil_image_view<T>*>(images_[l-1].ptr());
+    vil_image_view_base_sptr new_img=0;
+    scale_down(*img, new_img);
+    images_[l] = new_img;
+    scales_[l] = scale;
+    i/=2;j/=2;
+  }
+}
+
+template <class T>
+vil_pyramid_image_view<T>::vil_pyramid_image_view(vcl_vector<vil_image_view_base_sptr> const& images, 
+                                                  vcl_vector<double> const& scales)
+{
+  nlevels_=images.size();
+  images_.resize(nlevels_);
+  scales_.resize(nlevels_);
+  for (unsigned l=0; l<nlevels_; l++) {
+    images_[l] = images[l];
+    scales_[l] = scales[l];
+  }
+}
+
+template <class T>
+vil_pyramid_image_view<T>::vil_pyramid_image_view(const vil_pyramid_image_view<T>& rhs)
+{
+  this->images_.resize(rhs.nlevels());
+  this->nlevels_ = rhs.nlevels();
+  this->images_ = rhs.images_;
+  this->scales_ = rhs.scales_;
+}
+
+template <class T>
+void vil_pyramid_image_view<T>::add_view(vil_image_view_base_sptr &image, double scale)
+{
+  // find the place of the image based on the ordered scales
+  unsigned i=0;
+  vcl_vector<vil_image_view_base_sptr>::iterator image_iter=images_.begin();
+  vcl_vector<double>::iterator scale_iter= scales_.begin();
+  while (i<nlevels_ && scale<scales_[i]) {
+    i++;
+    image_iter++;
+    scale_iter++;
+  }
+
+  // insert into the vector
+  images_.insert(image_iter,image);
+  scales_.insert(scale_iter,scale);
+  nlevels_++;
+}
+
+template <class T>
+const vil_pyramid_image_view<T>& 
+vil_pyramid_image_view<T>::operator=(const vil_pyramid_image_view<T>& rhs)
+{
+  this->images_.resize(rhs.nlevels());
+  this->max_levels_=rhs.max_levels();
+  this->nlevels_ = rhs.nlevels();
+  this->images_ = rhs.images_;
+  this->scales_ = rhs.scales_;
+  return *this;
+}
+
+template <class T>
+void vil_pyramid_image_view<T>::scale_down(const vil_image_view<T>& image_in, 
+                                           vil_image_view_base_sptr& image_out)
+{
+  unsigned ni=image_in.ni();
+  unsigned nj=image_in.nj();
+  unsigned ii=ni/2;
+  unsigned jj=nj/2;
+  
+ // image_out = new vil_image_view<T>(ii, jj);
+  vil_image_view<T>* img = new vil_image_view<T>(ii, jj);
+  for (unsigned i=0; i<ii; i++) 
+    for (unsigned j=0; j<jj; j++) {
+      // get the 2x2 neighborhood
+      unsigned pos_i=i/2;
+      unsigned pos_j=j/2;
+      float v = 0.25f*(image_in(pos_i,pos_j)+image_in(pos_i+1,pos_j)+image_in(pos_i,pos_j+1)+image_in(pos_i+1,pos_j+1));
+      (*img)(i,j)=v;
+     }
+   image_out=img;
+}
+
+
+#define VIL_PYRAMID_IMAGE_VIEW_INSTANTIATE(T) \
+template class vil_pyramid_image_view<T >
+
+#endif
