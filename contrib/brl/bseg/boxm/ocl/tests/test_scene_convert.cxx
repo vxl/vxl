748d57881c689b66c918f33465a2fb1b93403217
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-08-28 05:25:38 -0400

    avoid compiler warning (unused variable "num_buffers")
    SVN-Revision: 29403

--- contrib/brl/bseg/boxm/ocl/tests/test_scene_convert.cxx
+++ contrib/brl/bseg/boxm/ocl/tests/test_scene_convert.cxx
@@ -15,14 +15,13 @@ boxm_scene<boct_tree<short, boxm_sample<BOXM_APM_MOG_GREY> > > create_scene()
   typedef boct_tree<short, data_type> tree_type;
   typedef boxm_scene<tree_type> scene_type;
 
-
   bgeo_lvcs lvcs(33.33,44.44,10.0, bgeo_lvcs::wgs84, bgeo_lvcs::DEG, bgeo_lvcs::METERS);
   vgl_point_3d<double> origin(0,0,0);
   vgl_vector_3d<double> block_dim(10,10,10);
   vgl_vector_3d<unsigned> world_dim(3,2,1);
   scene_type scene(lvcs, origin, block_dim, world_dim);
   scene.set_appearance_model(BOXM_APM_MOG_GREY);
-  
+
   vcl_string scene_dir = vcl_string(VCL_SOURCE_ROOT_DIR)+"/contrib/brl/bseg/boxm/ocl/tests/boxm_scene1";
   vcl_string xml_path = scene_dir + "/scene1.xml";
   scene.set_paths(scene_dir, "block");
@@ -33,7 +32,7 @@ boxm_scene<boct_tree<short, boxm_sample<BOXM_APM_MOG_GREY> > > create_scene()
 
   unsigned max_level=10, init_level=8;
   scene.set_octree_levels(max_level, init_level);
-    
+
   // default model (alpha = .001)
   bsta_gauss_f1 simple_gauss_f1(0.0f,0.1f);
   bsta_num_obs<bsta_gauss_f1> simple_obs_gauss_val_f1(simple_gauss_f1,1);
@@ -85,10 +84,11 @@ boxm_scene<boct_tree<short, boxm_sample<BOXM_APM_MOG_GREY> > > create_scene()
     scene.load_block(iter.index().x(),iter.index().y(),iter.index().z());
     boxm_block<tree_type>* block=scene.get_active_block();
     tree_type* tree;
-    //make first block one level further initialized than others 
+    //make first block one level further initialized than others
     if (iter.index().x()==0 && iter.index().y()==0 && iter.index().z()==0) {
       tree = new tree_type(max_level,init_level);
-    } else {
+    }
+    else {
       tree = new tree_type(max_level,init_level-1);
     }
     boct_tree_cell<short,data_type>* cel11 = tree->locate_point(vgl_point_3d<double>(0.01,0.01,0.9));
@@ -112,13 +112,13 @@ static void test_scene_convert()
   typedef boxm_sample<BOXM_APM_MOG_GREY> data_type;
   typedef boct_tree<short, data_type> tree_type;
   typedef boxm_scene<tree_type> scene_type;
-  
+
   scene_type scene = create_scene();
-  
-  //for each block in the boxm_scene, hide a random point and determine it's 
+
+  //for each block in the boxm_scene, hide a random point and determine it's
   int sum = 0, leaves = 0;
   boxm_block_iterator<tree_type> iter(&scene);
-  for(iter.begin(); !iter.end(); iter++) {
+  for (iter.begin(); !iter.end(); iter++) {
     vgl_point_3d<int> blk_ind = iter.index();
     scene.load_block(blk_ind.x(), blk_ind.y(), blk_ind.z());
     boxm_block<tree_type>* block = scene.get_active_block();
@@ -126,18 +126,16 @@ static void test_scene_convert()
     sum+=tree->all_cells().size();
     leaves+=tree->leaf_cells().size();
   }
-    
-  vcl_cout<<"Tree cells = "<<sum<<vcl_endl;  
-  vcl_cout<<"leaf cells = "<<leaves<<vcl_endl;
-  vcl_cout<<scene.get_world_bbox()<<vcl_endl;
-  
-  
-  int num_buffers = 1;
+
+  vcl_cout<<"Tree cells = "<<sum<<vcl_endl
+          <<"leaf cells = "<<leaves<<vcl_endl
+          <<scene.get_world_bbox()<<vcl_endl;
+
   int max_mb = 100;
   boxm_ocl_scene ocl_scene;
   boxm_ocl_convert<boxm_sample<BOXM_APM_MOG_GREY> >::convert_scene(&scene, ocl_scene, max_mb);
   vcl_cout<<ocl_scene<<vcl_endl;
-
 }
+
 
 TESTMAIN(test_scene_convert);
