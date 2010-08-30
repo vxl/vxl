4a5da08deed28537db545ba2172862868876fd13
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-08-30 05:29:55 -0400

    avoid compiler warning (passing "double" arg where "float" expected)
    SVN-Revision: 29413

--- contrib/brl/bseg/boxm/tests/test_utils.cxx
+++ contrib/brl/bseg/boxm/tests/test_utils.cxx
@@ -9,33 +9,32 @@ void init_tree(boct_tree<short,float> *tree, unsigned i)
 {
   tree-> split(); //now we have 8 cells
   vcl_vector<boct_tree_cell<short,float>*> leaves = tree->leaf_cells();
-  leaves[i]->set_data(0.8);
+  leaves[i]->set_data(0.8f);
   leaves[i]->split();
 }
 
 boxm_scene<boct_tree<short, float> >* create_scene(unsigned world_dimx,unsigned world_dimy,unsigned world_dimz)
 {
-  
   //crete the input scene
   bgeo_lvcs lvcs(33.33,44.44,10.0, bgeo_lvcs::wgs84, bgeo_lvcs::DEG, bgeo_lvcs::METERS);
   vgl_point_3d<double> origin(0,0,0);
-  
+
   vgl_vector_3d<double> block_dim(10, 10, 10); //world coordinate dimensions of a block
-  
+
   //number of blocks in a scene
   //  unsigned world_dimx = 2;
   //  unsigned world_dimy = 2;
   //  unsigned world_dimz = 2;
-  vgl_vector_3d<unsigned> world_dim(world_dimx,world_dimy,world_dimz); 
-  
+  vgl_vector_3d<unsigned> world_dim(world_dimx,world_dimy,world_dimz);
+
   boxm_scene<boct_tree<short, float> > *scene = new boxm_scene<boct_tree<short, float> >(lvcs, origin, block_dim, world_dim);
   vcl_string scene_path("./");
   scene->set_paths(scene_path, "test_scene");
-  
+
   unsigned cell_index = 7;
   boxm_block_iterator<boct_tree<short, float> > iter=scene->iterator();
   iter.begin();
-  while(!iter.end())
+  while (!iter.end())
   {
     scene->load_block(iter.index());
     boxm_block<boct_tree<short, float> > *block = scene->get_active_block();
@@ -46,7 +45,7 @@ boxm_scene<boct_tree<short, float> >* create_scene(unsigned world_dimx,unsigned 
     //tree->print();
     block->init_tree(tree);
     scene->write_active_block();
-    if(cell_index == 0) cell_index = 7;
+    if (cell_index == 0) cell_index = 7;
     cell_index--;
     ++iter;
   }
@@ -62,5 +61,4 @@ void clean_up()
     vpl_unlink(file_it());
     vul_file::delete_file_glob(file_it());
   }
-  
 }
