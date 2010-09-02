1f2627d048362161d925bfca0388852c0228e64d
Andrew Miller <andrew-miller@users.sourceforge.net>
2010-09-02 14:09:45 -0400

    added a boct_bit_tree traverse_opt test
    SVN-Revision: 29452

--- contrib/brl/bseg/boct/tests/test_bit_tree.cxx
+++ contrib/brl/bseg/boct/tests/test_bit_tree.cxx
@@ -24,14 +24,18 @@ static void test_bit_tree()
   
   boct_bit_tree tree(test_tree); 
   
+  //---------------------------------------------------------------------------
   //test set and bit_at bits
+  //---------------------------------------------------------------------------
   bool good = true;
   for(int i=0; i<73; i++) {
     good = good && (tree.bit_at(i) == test_tree[i]);
   }
   TEST("Set bits and get bits works ", true, good);
 
+  //---------------------------------------------------------------------------
   //test traverse
+  //---------------------------------------------------------------------------
   vgl_point_3d<double> o(0.0, 0.0, 0.0);
   int leaf_bit_index = tree.traverse(o);
   TEST("Traverse to origin works ", leaf_bit_index, 73);
@@ -39,15 +43,35 @@ static void test_bit_tree()
   vgl_point_3d<double> m(0.25, 0.25, 0.0);
   int mid_bit_index = tree.traverse(m);
   TEST("Traverse to mid point works ", mid_bit_index, 12);
+  
+  vgl_point_3d<double> h(.99, .99, .99);
+  int high_bit_index = tree.traverse(h);
+  TEST("Traverse to far edge works ", high_bit_index, 584);
+  
+  //---------------------------------------------------------------------------
+  //test traverse opt
+  //---------------------------------------------------------------------------
+  int leaf_opt = tree.traverse_opt(o);
+  int mid_opt = tree.traverse_opt(m);
+  int high_opt = tree.traverse_opt(h);
+  vcl_cout<<"origin: "<<leaf_opt<<"  mid: "<<mid_opt<<"  high: "<<high_opt<<vcl_endl;
+  good = (leaf_opt == leaf_bit_index) && (mid_opt == mid_bit_index);
+  TEST("Traverse opt works ", good, true);
 
+  //---------------------------------------------------------------------------
   //test data_index lookup
+  //---------------------------------------------------------------------------
   int origin_data = tree.get_data_index(leaf_bit_index);
+  vcl_cout<<origin_data<<vcl_endl;
   TEST("Origin data at 73", origin_data, 73);
 
   int mid_data = tree.get_data_index(mid_bit_index);
+  vcl_cout<<mid_data<<vcl_endl;
   TEST("Mid data @ 12", mid_data, 12);
-  
+
+  //---------------------------------------------------------------------------  
   //Test size of tree
+  //---------------------------------------------------------------------------
   int size = tree.size();
   TEST("Size of tree = 89", size, 89);
 
