d0d69f945e6217317fab07b32102f366da40e6c8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-09-07 15:11:05 -0400

    avoid compiler warning (unused variable)
    SVN-Revision: 29498

--- contrib/gel/mrc/vpgl/ihog/ihog_lsqr_cost_func.cxx
+++ contrib/gel/mrc/vpgl/ihog/ihog_lsqr_cost_func.cxx
@@ -8,12 +8,11 @@
 #include <vil/algo/vil_gauss_filter.h>
 
 
-
 //: Constructor
 ihog_lsqr_cost_func::ihog_lsqr_cost_func( const ihog_image<float>& image1,
-                                            const ihog_image<float>& image2,
-                                            const ihog_world_roi& roi,
-                                            const ihog_transform_2d& init_xform )
+                                          const ihog_image<float>& image2,
+                                          const ihog_world_roi& roi,
+                                          const ihog_transform_2d& init_xform )
  : vnl_least_squares_function(1,1),
    from_image_(image1),
    to_image_(image2),
@@ -25,16 +24,15 @@ ihog_lsqr_cost_func::ihog_lsqr_cost_func( const ihog_image<float>& image1,
   vnl_vector<double> params;
   init_xform.params(params);
   from_samples_ = roi_.sample(from_image_);
-  int number_of_residuals = from_samples_.size(); 
   use_gradient_ = false;
 }
 
 
 ihog_lsqr_cost_func::ihog_lsqr_cost_func( const ihog_image<float>& image1,
-                                            const ihog_image<float>& image2,
-                                            const ihog_image<float>& mask,
-                                            const ihog_world_roi& roi,
-                                            const ihog_transform_2d& init_xform, bool image1_mask)
+                                          const ihog_image<float>& image2,
+                                          const ihog_image<float>& mask,
+                                          const ihog_world_roi& roi,
+                                          const ihog_transform_2d& init_xform, bool image1_mask)
  : vnl_least_squares_function(1,1),
    from_image_(image1),
    to_image_(image2),
@@ -45,24 +43,25 @@ ihog_lsqr_cost_func::ihog_lsqr_cost_func( const ihog_image<float>& image1,
 {
   if (from_mask_) {
     from_mask_image_ = mask;
-  } else {
+  }
+  else {
     to_mask_image_ = mask;
   }
   vnl_vector<double> params;
   init_xform.params(params);
   from_samples_ = roi_.sample(from_image_);
-  int number_of_residuals = from_samples_.size(); 
+  int number_of_residuals = from_samples_.size();
   use_gradient_ = false;
   vnl_least_squares_function::init(params.size(), number_of_residuals);
   vnl_least_squares_function::init(params.size(), number_of_residuals);
 }
 
 ihog_lsqr_cost_func::ihog_lsqr_cost_func(const ihog_image<float>& image1,
-                                           const ihog_image<float>& image2,
-                                           const ihog_image<float>& mask1,
-                                           const ihog_image<float>& mask2,
-                                           const ihog_world_roi& roi,
-                                           const ihog_transform_2d& init_xform)
+                                         const ihog_image<float>& image2,
+                                         const ihog_image<float>& mask1,
+                                         const ihog_image<float>& mask2,
+                                         const ihog_world_roi& roi,
+                                         const ihog_transform_2d& init_xform)
  : vnl_least_squares_function(1,1),
    from_image_(image1),
    to_image_(image2),
@@ -76,17 +75,16 @@ ihog_lsqr_cost_func::ihog_lsqr_cost_func(const ihog_image<float>& image1,
   vnl_vector<double> params;
   init_xform.params(params);
   from_samples_ = roi_.sample(from_image_);
-  int number_of_residuals = from_samples_.size(); 
+  int number_of_residuals = from_samples_.size();
   use_gradient_ = false;
   vnl_least_squares_function::init(params.size(), number_of_residuals);
-  
 }
 
 
 //: The main function.
 //  Given the parameter vector x, compute the vector of residuals fx.
 //  Fx has been sized appropriately before the call.
-void 
+void
 ihog_lsqr_cost_func::f(vnl_vector<double> const& x, vnl_vector<double>& fx)
 {
   ihog_transform_2d new_xform;
@@ -97,7 +95,7 @@ ihog_lsqr_cost_func::f(vnl_vector<double> const& x, vnl_vector<double>& fx)
 
   if (from_mask_ || to_mask_) {
     vnl_vector<double> mask_samples;
-    if(from_mask_){
+    if (from_mask_) {
       mask_samples = roi_.sample(from_mask_image_);
     }
     if (to_mask_) {
@@ -108,31 +106,31 @@ ihog_lsqr_cost_func::f(vnl_vector<double> const& x, vnl_vector<double>& fx)
       //double nsamples = to_mask_samples.sum();
       //if (nsamples)
       //  to_mask_samples /= nsamples;
-      //else 
+      //else
       //  vcl_cerr << "error: mask sums to 0\n";
       if (from_mask_) {
         // mask_samples already filled in - multiply by second mask to get AND
         mask_samples = element_product<double>(mask_samples,to_mask_samples);
-      } else {
-        // no prevous mask - mask is to_mask
+      }
+      else {
+        // no previous mask - mask is to_mask
         mask_samples = to_mask_samples;
       }
     }
-    fx = element_product<double>(mask_samples,(from_samples_ - to_samples));    
+    fx = element_product<double>(mask_samples,(from_samples_ - to_samples));
   }
   else
   {
     fx = from_samples_;
-    unsigned i = 0;
-    for(vnl_vector<double>::iterator sit = fx.begin();
-      sit != fx.end(); ++sit, ++i)
+    vnl_vector<double>::iterator sit = fx.begin();
+    for (unsigned i = 0; sit != fx.end(); ++sit, ++i)
       (*sit)-=to_samples[i];
   }
 }
 
 
 //: Returns the transformed second image
-vil_image_view<float> 
+vil_image_view<float>
 ihog_lsqr_cost_func::last_xformed_image()
 {
   return roi_.resample(to_image_);
--- contrib/gel/mrc/vpgl/ihog/ihog_utils.cxx
+++ contrib/gel/mrc/vpgl/ihog/ihog_utils.cxx
@@ -2,7 +2,7 @@
 #include <vgl/vgl_polygon.h>
 #include <vgl/vgl_polygon_scan_iterator.h>
 #include <ihog/ihog_utils.h>
-void ihog_utils::image_bounds(unsigned source_ni, unsigned source_nj, 
+void ihog_utils::image_bounds(unsigned source_ni, unsigned source_nj,
                               ihog_transform_2d const& t, unsigned& dest_ni,
                               unsigned& dest_nj, ihog_transform_2d& mod_trans)
 {
@@ -11,20 +11,20 @@ void ihog_utils::image_bounds(unsigned source_ni, unsigned source_nj,
   //compute inverse to determine bounds on destination image
   ihog_transform_2d tinv = t.inverse();
   //initial bounds of destination image
-  vgl_point_2d<double> ul=tinv(0,0), 
+  vgl_point_2d<double> ul=tinv(0,0),
     ur = tinv(ni_d,0), lr = tinv(ni_d, nj_d), ll=tinv(0, nj_d);
   vgl_box_2d<double> bb;
-  bb.add(ul);   bb.add(ur);   bb.add(lr);   bb.add(ll);  
+  bb.add(ul);   bb.add(ur);   bb.add(lr);   bb.add(ll);
   double minu = bb.min_x(), minv = bb.min_y();
   double maxu = bb.max_x(), maxv = bb.max_y();
-  
-  // define shift transform so that destination image is 
+
+  // define shift transform so that destination image is
   // mapped for all positive image pixel coordinates
   ihog_transform_2d t_shift;
 
-  t_shift.set_affine(vgl_point_2d<double>(minu, minv), 
-                    vgl_vector_2d<double>(1,0),
-                    vgl_vector_2d<double>(0,1));
+  t_shift.set_affine(vgl_point_2d<double>(minu, minv),
+                     vgl_vector_2d<double>(1,0),
+                     vgl_vector_2d<double>(0,1));
   //compose the two transformations
   mod_trans = t*t_shift;//note shift then map
   ihog_transform_2d dinv = mod_trans.inverse();
@@ -32,32 +32,32 @@ void ihog_utils::image_bounds(unsigned source_ni, unsigned source_nj,
   ul=dinv(0,0);  ur = dinv(ni_d,0); lr = dinv(ni_d, nj_d); ll = dinv(0, nj_d);
   bb.empty();//clear the box
   bb.add(ul); bb.add(ur); bb.add(lr); bb.add(ll);
-  dest_ni = static_cast<int>(bb.max_x())+1; 
-  dest_nj = static_cast<int>(bb.max_y())+1;
+  dest_ni = static_cast<int>(maxu)+1;
+  dest_nj = static_cast<int>(maxv)+1;
 }
 
 ihog_image<float> ihog_utils::destination_mask(unsigned source_ni,
-                                                   unsigned source_nj,
-                                                   ihog_transform_2d const& t)
+                                               unsigned source_nj,
+                                               ihog_transform_2d const& t)
 {
   unsigned ni, nj;
   ihog_transform_2d m, m_inv;
-  ihog_utils::image_bounds(source_ni, source_nj, t, ni, nj, m); 
+  ihog_utils::image_bounds(source_ni, source_nj, t, ni, nj, m);
   m_inv = m.inverse();
   double ni_d = source_ni-1, nj_d = source_nj-1;
-  vgl_point_2d<double> ul=m_inv(0,0), 
+  vgl_point_2d<double> ul=m_inv(0,0),
     ur = m_inv(ni_d,0), lr = m_inv(ni_d, nj_d), ll=m_inv(0, nj_d);
   vil_image_view<float> mask(ni, nj, 1);
   mask.fill(0.0f);
- //create scan converter
+  //create scan converter
   vgl_polygon<double> poly(1);
-  
+
   poly.push_back(ul);poly.push_back(ur);poly.push_back(lr);poly.push_back(ll);
   vgl_polygon_scan_iterator<double> psc(poly, false);
   //set active mask pixels
-  for(psc.reset(); psc.next();){
+  for (psc.reset(); psc.next();){
     int v = psc.scany();
-    for(int u = psc.startx(); u<=psc.endx(); ++u)
+    for (int u = psc.startx(); u<=psc.endx(); ++u)
       mask(u,v) = 1.0f;
   }
   ihog_image<float>imask(mask, m_inv);
