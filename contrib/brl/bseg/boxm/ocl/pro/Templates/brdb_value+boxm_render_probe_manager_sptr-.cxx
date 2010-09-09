862c29c8cbdf8a5a53049b9e366add89ff1d045f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-09-09 18:44:12 -0400

    renamed (standard file naming)
    SVN-Revision: 29529

--- contrib/brl/bseg/boxm/ocl/pro/Templates/brdb_value+boxm_render_probe_manager_sptr-.cxx
+++
@@ -1,4 +0,0 @@
-#include <brdb/brdb_value.txx>
-#include <boxm/ocl/boxm_render_probe_manager_sptr.h>
-#include <vbl/io/vbl_io_smart_ptr.h>
-BRDB_VALUE_INSTANTIATE(boxm_render_probe_manager_sptr, "boxm_render_probe_manager_sptr");
