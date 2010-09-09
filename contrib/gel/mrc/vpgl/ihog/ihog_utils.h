89fcfe42e38be2ef9e28670ea1cd2b488c944178
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-09-09 18:45:25 -0400

    SVN-Revision: 29530

--- contrib/gel/mrc/vpgl/ihog/ihog_utils.h
+++ contrib/gel/mrc/vpgl/ihog/ihog_utils.h
@@ -22,29 +22,28 @@
 class ihog_utils
 {
  public:
-  //: destination image bounds and modified transform
+  //: destination image bounds and modified transform.
   //  under a homography the resampled image bounds map from
-  //  a rectangle (the source image) to a quadrilateral in the 
-  //  destination. This function finds the destination image 
+  //  a rectangle (the source image) to a quadrilateral in the
+  //  destination. This function finds the destination image
   //  bounds and the modified transform to insure all source
   //  points are mapped to the destination with positive coordinates
   //  lying inside the destination bounds.
-  static void image_bounds(unsigned source_ni, unsigned source_nj, 
+  static void image_bounds(unsigned source_ni, unsigned source_nj,
                            ihog_transform_2d const& t, unsigned& dest_ni,
                            unsigned& dest_nj, ihog_transform_2d& mod_trans);
 
   //: When resampling a source image to a destination,
   //  some pixels in the destination image may not be valid.
   //  The returned mask is 0 where the destination image does not contain
-  //  valid pixels.  Note that transform t is from the destination 
+  //  valid pixels.  Note that transform t is from the destination
   //  to the source. This transform sense is used in ihog_sample_grid_bilin
   static ihog_image<float> destination_mask(unsigned source_ni,
                                             unsigned source_nj,
                                             ihog_transform_2d const& t);
- private:  
+ private:
   ihog_utils();
   ~ihog_utils();
 };
 
 #endif // ihog_utils_h_
-
