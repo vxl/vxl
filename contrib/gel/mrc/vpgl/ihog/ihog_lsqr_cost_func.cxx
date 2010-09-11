20bd90d148e1bb3ccbfe93daca9c41f3744d19bc
Joseph L. Mundy <mundyj@users.sourceforge.net>
2010-09-11 13:41:10 -0400

    missing least squares function init
    SVN-Revision: 29550

--- contrib/gel/mrc/vpgl/ihog/ihog_lsqr_cost_func.cxx
+++ contrib/gel/mrc/vpgl/ihog/ihog_lsqr_cost_func.cxx
@@ -25,6 +25,7 @@ ihog_lsqr_cost_func::ihog_lsqr_cost_func( const ihog_image<float>& image1,
   init_xform.params(params);
   from_samples_ = roi_.sample(from_image_);
   use_gradient_ = false;
+  vnl_least_squares_function::init(params.size(), number_of_residuals);
 }
 
 
@@ -52,7 +53,6 @@ ihog_lsqr_cost_func::ihog_lsqr_cost_func( const ihog_image<float>& image1,
   from_samples_ = roi_.sample(from_image_);
   int number_of_residuals = from_samples_.size();
   use_gradient_ = false;
-  vnl_least_squares_function::init(params.size(), number_of_residuals);
   vnl_least_squares_function::init(params.size(), number_of_residuals);
 }
 
