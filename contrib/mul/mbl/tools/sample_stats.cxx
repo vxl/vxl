019b115d58d846662132d0f96131f80925d3531e
Kevin de Souza <kdesouza@users.sourceforge.net>
2010-09-24 04:32:32 -0400

    Added interface to other statistics
    SVN-Revision: 29674

--- contrib/mul/mbl/tools/sample_stats.cxx
+++ contrib/mul/mbl/tools/sample_stats.cxx
@@ -46,17 +46,24 @@ int main2(int argc, char *argv[])
   // Parse the program arguments
   vul_arg<vcl_string> in_file("-i", "input file containing scalar values (whitespace-separated); otherwise uses stdin", "");
   vul_arg<vcl_string> out_file("-o", "output file to append statistics; logger will be used otherwise", "");
-  vul_arg<bool> n("-n", "Specify this to record number of samples", false);
-  vul_arg<bool> min("-min", "Specify this to record minimum", false);
-  vul_arg<bool> max("-max", "Specify this to record maximum", false);
-  vul_arg<bool> mean("-mean", "Specify this to record mean", false);
-  vul_arg<bool> sd("-sd", "Specify this to record standard deviation", false);
-  vul_arg<bool> se("-se", "Specify this to record standard error", false);
-  vul_arg<bool> med("-med", "Specify this to record median", false);
-  vul_arg<vcl_vector<int> > pc("-pc", "Specify this switch with 1 or more comma-separated integers (e.g. 5,50,95) to record percentile(s)");
-  vul_arg<vcl_vector<double> > quant("-q", "Specify this switch with 1 or more comma-separated floats (e.g. 0.05,0.50,0.95) to record quantile(s)");
   vul_arg<vcl_string> sep("-sep", "String to use as a separator between output values, e.g. \", \" or \"  \" (default=TAB)", "\t");
   vul_arg<bool> nohead("-h","Specify this to SUPPRESS column headers", false);
+  vul_arg<bool> n("-n", "Specify this to record the number of samples", false);
+  vul_arg<bool> mean("-mean", "Specify this to record the mean", false);
+  vul_arg<bool> variance("-var", "Specify this to record the variance", false);
+  vul_arg<bool> sd("-sd", "Specify this to record the standard deviation", false);
+  vul_arg<bool> se("-se", "Specify this to record the standard error", false);
+  vul_arg<bool> med("-med", "Specify this to record the median", false);
+  vul_arg<bool> min("-min", "Specify this to record the minimum", false);
+  vul_arg<bool> max("-max", "Specify this to record the maximum", false);
+  vul_arg<vcl_vector<int> > pc("-pc", "Specify this switch with 1 or more comma-separated integers (e.g. 5,50,95) to record percentile(s)");
+  vul_arg<vcl_vector<double> > quant("-q", "Specify this switch with 1 or more comma-separated floats (e.g. 0.05,0.50,0.95) to record quantile(s)");
+  vul_arg<bool> sum("-sum", "Specify this to record the sum", false);
+  vul_arg<bool> sum_squares("-ssq", "Specify this to record the sum of squares", false);
+  vul_arg<bool> rms("-rms", "Specify this to record the rms", false);
+  vul_arg<bool> mean_of_absolutes("-moa", "Specify this to record the mean_of_absolutes", false);
+  vul_arg<bool> skewness("-skew", "Specify this to record the skewness", false);
+  vul_arg<bool> kurtosis("-kurt", "Specify this to record the kurtosis", false);
   vul_arg_parse(argc, argv);
 
   // Try to open the input file if specified or use stdin
@@ -87,13 +94,20 @@ int main2(int argc, char *argv[])
   // Calculate the requested statistics
   mbl_sample_stats_1d data(data_vec);
   vcl_map<vcl_string, double> stats;
-  if (n.set())    stats["n"]=data.n_samples();
-  if (min.set())  stats["min"]=data.min();
-  if (max.set())  stats["max"]=data.max();
-  if (mean.set()) stats["mean"]=data.mean();
-  if (sd.set())   stats["sd"]=data.sd();
-  if (se.set())   stats["se"]=data.stdError();
-  if (med.set())  stats["med"]=data.median();
+  if (n.set())                 stats["n"]=data.n_samples();
+  if (mean.set())              stats["mean"]=data.mean();
+  if (variance.set())          stats["var"]=data.variance();
+  if (sd.set())                stats["sd"]=data.sd();
+  if (se.set())                stats["se"]=data.stdError();
+  if (med.set())               stats["med"]=data.median();
+  if (min.set())               stats["min"]=data.min();
+  if (max.set())               stats["max"]=data.max();
+  if (sum.set())               stats["sum"]=data.sum();
+  if (sum_squares.set())       stats["ssq"]=data.sum_squares();
+  if (rms.set())               stats["rms"]=data.rms();
+  if (mean_of_absolutes.set()) stats["moa"]=data.mean_of_absolutes();
+  if (skewness.set())          stats["skew"]=data.skewness();
+  if (kurtosis.set())          stats["kurt"]=data.kurtosis();
   if (pc.set())
   {
     for (vcl_vector<int>::const_iterator it=pc().begin(); it!=pc().end(); ++it)
