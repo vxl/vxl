0a60d40f39d7c6075359a94da704830fd3d2cfb2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-09-26 02:34:29 -0400

    avoid compiler warning (redefinition of macro)
    SVN-Revision: 29693

--- contrib/tbl/vipl/accessors/vipl_accessors_vcl_vector.h
+++ contrib/tbl/vipl/accessors/vipl_accessors_vcl_vector.h
@@ -4,7 +4,9 @@
 
 #undef FILTER_IMPTR_DEC_REFCOUNT
 #define FILTER_IMPTR_DEC_REFCOUNT(v) {delete (v); (v)=0;}
+#undef FILTER_IMPTR_INC_REFCOUNT
 #define FILTER_IMPTR_INC_REFCOUNT(v)
+#undef FILTER_KERNPTR_DEC_REFCOUNT
 #define FILTER_KERNPTR_DEC_REFCOUNT(v)
 
 template <class DataType> DataType fgetpixel(vcl_vector<DataType> const& i, int x, int y, DataType);
