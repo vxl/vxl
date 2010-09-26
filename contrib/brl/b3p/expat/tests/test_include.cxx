67114b20c4281df696445503287ca18bddacdc7f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-09-26 11:21:57 -0400

    removed the generated include files "expatDllConfig.h" and "expatConfig.h"
    SVN-Revision: 29697

--- contrib/brl/b3p/expat/tests/test_include.cxx
+++ contrib/brl/b3p/expat/tests/test_include.cxx
@@ -1,11 +1,9 @@
-#include <b3p/expat/cm_expat_mangle.h>
 #include <b3p/expat/ascii.h>
-#include <b3p/expat/expatDllConfig.h>
+#include <b3p/expat/cm_expat_mangle.h>
 #include <b3p/expat/expat.h>
+#include <b3p/expat/nametab.h>
+#include <b3p/expat/xmlrole.h>
 #include <b3p/expat/xmltok.h>
 #include <b3p/expat/xmltok_impl.h>
-#include <b3p/expat/expatConfig.h>
-#include <b3p/expat/xmlrole.h>
-#include <b3p/expat/nametab.h>
 
 int main() { return 0; }
