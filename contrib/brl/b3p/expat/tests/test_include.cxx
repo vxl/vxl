4b46fbb7135ef0ee28727b11a4cadbd2ce16cab7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-09-26 07:53:13 -0400

    these four files are not meant to be #included
    SVN-Revision: 29696

--- contrib/brl/b3p/expat/tests/test_include.cxx
+++ contrib/brl/b3p/expat/tests/test_include.cxx
@@ -1,15 +1,11 @@
 #include <b3p/expat/cm_expat_mangle.h>
 #include <b3p/expat/ascii.h>
-#include <b3p/expat/utf8tab.h>
 #include <b3p/expat/expatDllConfig.h>
 #include <b3p/expat/expat.h>
 #include <b3p/expat/xmltok.h>
-#include <b3p/expat/asciitab.h>
-#include <b3p/expat/latin1tab.h>
 #include <b3p/expat/xmltok_impl.h>
 #include <b3p/expat/expatConfig.h>
 #include <b3p/expat/xmlrole.h>
 #include <b3p/expat/nametab.h>
-#include <b3p/expat/iasciitab.h>
 
 int main() { return 0; }
