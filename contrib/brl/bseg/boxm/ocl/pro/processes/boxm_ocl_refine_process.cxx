ca6dc74f0a56f885b5595664049b9e8c80ab2df6
Andrew Miller <andrew-miller@users.sourceforge.net>
2010-09-29 10:30:48 -0400

    updated host code to reflect deleted refine manager
    SVN-Revision: 29739

--- contrib/brl/bseg/boxm/ocl/pro/processes/boxm_ocl_refine_process.cxx
+++ contrib/brl/bseg/boxm/ocl/pro/processes/boxm_ocl_refine_process.cxx
@@ -13,7 +13,6 @@
 
 #include <vcl_fstream.h>
 #include <boxm/ocl/boxm_ocl_scene.h>
-#include <boxm/ocl/boxm_ocl_refine_scene.h>
 #include <boxm/ocl/boxm_refine_scene_manager.h>
 
 namespace boxm_ocl_refine_process_globals
@@ -52,52 +51,17 @@ bool boxm_ocl_refine_process(bprb_func_process& pro)
   float prob_thresh = pro.get_input<float>(i++);
   vcl_string out_path = pro.get_input<vcl_string>(i++);
 
+  //refine scene
   vcl_cout<<"Refining OCL scene: "<<scene_path<<" with prob "<<prob_thresh<<vcl_endl;
-
   boxm_ocl_scene scene = boxm_ocl_scene(scene_path);
   vcl_cout<<scene<<vcl_endl;
-
   boxm_refine_scene_manager* mgr = boxm_refine_scene_manager::instance();
   mgr->init_refine(&scene, prob_thresh);
   mgr->run_refine();
 
+  //save scene
   vcl_cout<<scene<<vcl_endl
           <<"Saving scene to "<<out_path<<vcl_endl;
   scene.save_scene(out_path);
-
-#if 0 // commented out
-  // check the scene's appearance model
-  switch (scene_ptr->appearence_model())
-  {
-    case BOXM_APM_MOG_GREY:
-      if (!scene_ptr->multi_bin())
-      {
-        typedef boct_tree<short, boxm_sample<BOXM_APM_MOG_GREY> > type;
-        boxm_scene<type>* scene = dynamic_cast<boxm_scene<type>*> (scene_ptr.as_pointer());
-        boxm_ocl_refine_scene<BOXM_APM_MOG_GREY>(scene, prob_thresh);
-      }
-      else
-      {
-        vcl_cerr<<"OpenCL rendering of multi-bin scenes not implemented\n";
-        return false;
-      }
-      break;
-    case BOXM_APM_SIMPLE_GREY:
-      if (!scene_ptr->multi_bin()) {
-        typedef boct_tree<short, boxm_sample<BOXM_APM_SIMPLE_GREY> > type;
-        boxm_scene<type>* scene = dynamic_cast<boxm_scene<type>*> (scene_ptr.as_pointer());
-        boxm_ocl_refine_scene<BOXM_APM_SIMPLE_GREY>(scene, prob_thresh);
-      }
-      else
-      {
-        vcl_cerr<<"OpenCL rendering of multi-bin scenes not implemented\n";
-        return false;
-      }
-      break;
-    default:
-      vcl_cerr << "boxm_ocl_render_expected_process: unsupported APM type\n";
-      return false;
-  }
-#endif // 0
   return true;
 }
