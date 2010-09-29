134d2c76db89b892ceb497963f3a8978c350d58e
Andrew Miller <andrew-miller@users.sourceforge.net>
2010-09-29 10:28:59 -0400

    deleted old refine manager templates
    SVN-Revision: 29737

--- contrib/brl/bseg/boxm/ocl/Templates/bocl_manager+boxm_refine_manager+boxm_sample+BOXM_APM_MOG_GREY---.cxx
+++
@@ -1,9 +0,0 @@
-#include <bocl/bocl_manager.txx>
-#include <boxm/ocl/boxm_refine_manager.h>
-#include <boxm/boxm_apm_traits.h>
-
-
-
-typedef boxm_refine_manager<boxm_sample<BOXM_APM_MOG_GREY> > boxm_refine_manager_mog_grey;
-
-BOCL_MANAGER_INSTANTIATE(boxm_refine_manager_mog_grey );
--- contrib/brl/bseg/boxm/ocl/Templates/boxm_refine_manager+float-.cxx
+++
@@ -1,2 +0,0 @@
-#include <boxm/ocl/boxm_refine_manager.txx>
-BOXM_REFINE_MANAGER_INSTANTIATE(float );
