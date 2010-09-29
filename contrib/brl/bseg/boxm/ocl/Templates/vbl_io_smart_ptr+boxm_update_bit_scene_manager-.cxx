d7a99e7b00842270b39fbf3f729491987d84164a
Vishal Jain <jainv@users.sourceforge.net>
2010-09-29 16:05:10 -0400

    Added instantiations for python interface.
    SVN-Revision: 29743

---
+++ contrib/brl/bseg/boxm/ocl/Templates/vbl_io_smart_ptr+boxm_update_bit_scene_manager-.cxx
@@ -0,0 +1,5 @@
+#include <boxm/ocl/boxm_update_bit_scene_manager.h>
+#include <bocl/bocl_manager.h>
+#include <vbl/io/vbl_io_smart_ptr.txx>
+ 
+VBL_IO_SMART_PTR_INSTANTIATE(boxm_update_bit_scene_manager);
