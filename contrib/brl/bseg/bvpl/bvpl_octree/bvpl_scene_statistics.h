894fbe4388b3ec58f77f0db818ec3bec21308dce
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2010-09-30 15:44:19 -0400

    a file to compute statistics of bvpl scenes
    SVN-Revision: 29766

---
+++ contrib/brl/bseg/bvpl/bvpl_octree/bvpl_scene_statistics.h
@@ -0,0 +1,48 @@
+// This is bvpl_octree_scene_statistics.h
+#ifndef bvpl_scene_statistics_h
+#define bvpl_scene_statistics_h
+
+//:
+// \file
+// \brief A set of functions to provide statistics about response scenes
+// \author Isabel Restrepo mir@lems.brown.edu
+// \date  22-Sep-2010.
+//
+// \verbatim
+//  Modifications
+//   <none yet>
+// \endverbatim
+
+
+template <class T_loc, class T_data>
+bool compute_scene_statistics(boxm_scene<boct_tree<T_loc, T_data > >& scene, bsta_histogram<float>& response_hist )//, bsta_histogram<float>& level_hist, unsigned& n_leaves)
+{
+  typedef boct_tree<T_loc, T_data> tree_type;
+  typedef boct_tree_cell<T_loc,T_data> cell_type;
+  
+ 
+  //(1) Traverse the leaves of the scene
+  boxm_cell_iterator<boct_tree<short, T_data > > iterator = 
+  scene_in.cell_iterator(&boxm_scene<boct_tree<short, T_data> >::load_block);
+
+  iterator.begin();
+  float cell_count = 0;
+  while (!iterator.end()) {
+    cell_count++;
+  }
+  
+  float nbins = vcl_sqrt(cell_count);
+  response_hist = bsta_histogram<float>(0.0f, 1.0f, nbins);
+  //level_hist = bsta_histogram<float>(0.0f, 10.0f, 10);
+  
+  iterator.begin();
+  
+  while (!iterator.end()) {
+    boct_tree_cell<short,T_data> *cell = *iterator;
+    //level_hist.upcount(static_cast<float>(level));
+    response_hist.upcount(static_cast<float>(cell->data().mean()));
+  }
+                         
+                    
+  return true;
+}
