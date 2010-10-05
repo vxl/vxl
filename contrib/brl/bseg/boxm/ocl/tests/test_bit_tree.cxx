b392b24c354d58f0e4e2a30070275e587d7fed01
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-10-05 17:18:36 -0400

    avoid compiler warning (unused static function)
    SVN-Revision: 29813

--- contrib/brl/bseg/boxm/ocl/tests/test_bit_tree.cxx
+++ contrib/brl/bseg/boxm/ocl/tests/test_bit_tree.cxx
@@ -1,5 +1,5 @@
 //Tests bit tree OpenCL traverse, traverse_force and others
-//will run ocl scene comparison as well. 
+//will run ocl scene comparison as well.
 #include <testlib/testlib_test.h>
 #include "open_cl_test_data.h"
 #include <boct/boct_bit_tree.h>
@@ -29,7 +29,7 @@ static void test_loc_code(bit_tree_test_manager* test_mgr)
                 0,2,0,0,
                 3,2,3,0};
     bool good = true;
-    for (vcl_size_t i= 0; i<size; i++)
+    for (vcl_size_t i= 0; i<size; ++i)
       good = good && results[i]==test[i];
     TEST("test_loc_code_data", good, true);
     if (!good)
@@ -50,9 +50,9 @@ static void test_loc_code(bit_tree_test_manager* test_mgr)
 static void test_bit_at(bit_tree_test_manager* test_mgr)
 {
   bool good = false;
-  TEST("Run Kernel test_bit_at", good=test_mgr->run_test(1), true); 
-  if(!good) return;
-  
+  TEST("Run Kernel test_bit_at", good=test_mgr->run_test(1), true);
+  if (!good) return;
+
   cl_int* results = test_mgr->get_output();
   vcl_size_t size = 16*4;
   if (results)
@@ -65,7 +65,7 @@ static void test_bit_at(bit_tree_test_manager* test_mgr)
                   1,0,0,0,0,0,0,0,  1,0,0,0,0,0,0,0
                   };
     bool good = true;
-    for (vcl_size_t i= 0; i<size; i++)
+    for (vcl_size_t i= 0; i<size; ++i)
       good = good && results[i]==test[i];
     TEST("test_bit_at_results", good, true);
     if (!good) {
@@ -88,35 +88,35 @@ static void test_bit_at(bit_tree_test_manager* test_mgr)
 static void test_traverse(bit_tree_test_manager* test_mgr, boct_tree<short, float>* tree)
 {
   bool good = false;
-  TEST("Run Kernel test_traverse", good=test_mgr->run_test(2), true); 
-  if(!good) return;
-  
+  TEST("Run Kernel test_traverse", good=test_mgr->run_test(2), true);
+  if (!good) return;
+
   cl_int* results = test_mgr->get_output();
   cl_float* bbox_results = test_mgr->get_bbox_output();
- 
+
   //make sure results match boct_tree
-  boct_loc_code<short> codes[8]; 
+  boct_loc_code<short> codes[8];
   codes[0].set_code(0,0,0);
   codes[1].set_code(4,0,0);
-  codes[2].set_code(5,1,1);   
-  codes[3].set_code(6,6,2);  
-  codes[4].set_code(1,1,6);   
-  codes[5].set_code(4,0,4);   
-  codes[6].set_code(4,4,5);   
+  codes[2].set_code(5,1,1);
+  codes[3].set_code(6,6,2);
+  codes[4].set_code(1,1,6);
+  codes[5].set_code(4,0,4);
+  codes[6].set_code(4,4,5);
   codes[7].set_code(6,6,6);
-  
-  //check codes 
+
+  //check codes
   good = true;
-  for(int i=0; i< 8; i++) {
+  for (int i=0; i< 8; ++i) {
     boct_tree_cell<short,float>* cell = tree->get_cell(codes[i]);
     boct_loc_code<short> code = cell->get_code();
-    
-    bool same = (code.x_loc_ == results[8*i]) && 
+
+    bool same = (code.x_loc_ == results[8*i]) &&
                 (code.y_loc_ == results[8*i+1]) &&
                 (code.z_loc_ == results[8*i+2]) &&
                 (code.level  == results[8*i+3]);
-    
-    if(!same) {
+
+    if (!same) {
       vcl_cout << "result: ["
                << results[8*i] << ' '
                << results[8*i+1] << ' '
@@ -125,86 +125,84 @@ static void test_traverse(bit_tree_test_manager* test_mgr, boct_tree<short, floa
                << code << " level: " << code.level << vcl_endl;
     }
     good = good && same;
-    
+
     //check index
     int index = boct_bit_tree::loc_code_to_index(code, 3);
     same = (index == results[8*i+4]);
-    if(!same) {
-      vcl_cout << "result index [" 
-               << results[8*i] << "," << results[8*i+1] << ","
-               << results[8*i+2] << "," << results[8*i+3] << "] "
-               << "  index: " << results[8*i+4] 
+    if (!same) {
+      vcl_cout << "result index ["
+               << results[8*i] << ',' << results[8*i+1] << ','
+               << results[8*i+2] << ',' << results[8*i+3] << "] "
+               << "  index: " << results[8*i+4]
                << " should be: "<<index<< vcl_endl;
     }
     good = good && same;
-    
   }
   TEST("Test_traverse code results", good, true);
-  
+
   vcl_cout<<"Time spent test_traverse "<<test_mgr->gpu_time_/(10000.0*8)<<" ms"<<vcl_endl;
 
 
   //check BOUNDING BOX results
   vcl_cout<<"BBOX Results--------------------------------------"<<vcl_endl;
-  for(int i=0; i<8; i++) {
+  for (int i=0; i<8; ++i) {
     vnl_vector_fixed<float,4> cell_min, cell_max, cell_min_opt, cell_max_opt;
-    for(int j=0; j<4; j++) {
+    for (int j=0; j<4; ++j) {
       cell_min[j] = bbox_results[16*i + j];
       cell_max[j] = bbox_results[16*i + 4 + j];
       cell_min_opt[j] = bbox_results[16*i + 8 + j];
       cell_max_opt[j] = bbox_results[16*i + 12 + j];
     }
-    vcl_cout<<"point "<<i<<vcl_endl;
-    vcl_cout<<"old bbox: min="<<cell_min<<"  max="<<cell_max<<vcl_endl
-            <<" box size: "<<cell_max[0]-cell_min[0]<<vcl_endl;
-    vcl_cout<<"new bbox: min="<<cell_min_opt<<"  max="<<cell_max_opt<<vcl_endl
+    vcl_cout<<"point "<<i<<'\n'
+            <<"old bbox: min="<<cell_min<<"  max="<<cell_max<<'\n'
+            <<" box size: "<<cell_max[0]-cell_min[0]<<'\n'
+            <<"new bbox: min="<<cell_min_opt<<"  max="<<cell_max_opt<<'\n'
             <<" box size: "<<cell_max_opt[0]-cell_min_opt[0]<<vcl_endl;
   }
-  
+
   //debug print just to see all resutls
   int index = 0;
-  for(int i=0; i<16; i++) {
-    for(int j=0; j<4; j++) {
+  for (int i=0; i<16; ++i) {
+    for (int j=0; j<4; ++j) {
       vcl_cout<<results[index++]<<' ';
     }
     vcl_cout<<'\n';
   }
-  
 }
 #endif // 0
 
 #if 0 // most tests are commented out for the time being ...
 static void test_traverse_force(bit_tree_test_manager* test_mgr, boct_tree<short, float>* tree)
 {
-  //test the execution 
+  //test the execution
   bool good = false;
-  TEST("Run Kernel test_traverse_force", good=test_mgr->run_test(3), true); 
-  if(!good) return;
-  
-  //get results 
+  TEST("Run Kernel test_traverse_force", good=test_mgr->run_test(3), true);
+  if (!good) return;
+
+  //get results
   cl_int* results = test_mgr->get_output();
-  
+
   //make sure results match boct_tree
   boct_loc_code<short> start_code; start_code.level = 1;
-  boct_loc_code<short> target_codes[4]; 
+  boct_loc_code<short> target_codes[4];
   target_codes[0].set_code(2,0,0); target_codes[0].level = 0;
   target_codes[1].set_code(0,2,0); target_codes[1].level = 0;
   target_codes[2].set_code(0,0,2);  target_codes[2].level = 0;
   target_codes[3].set_code(2,2,2);  target_codes[3].level = 0;
 
-  for(int i=0; i<4; i++) {
+  for (int i=0; i<4; ++i) {
     //get start cell; run traverse force on boct_tree
     boct_tree_cell<short,float>* start = tree->get_cell(start_code);
     boct_tree_cell<short,float>* result = start->traverse_force(target_codes[i]);
     boct_loc_code<short> result_code = result->get_code();
-    
-    //compare 
-    bool same = (result_code.x_loc_ == results[8*i]) && 
+
+    //compare
+    bool same = (result_code.x_loc_ == results[8*i]) &&
                 (result_code.y_loc_ == results[8*i+1]) &&
                 (result_code.z_loc_ == results[8*i+2]) &&
                 (result_code.level  == results[8*i+3]);
-    
-    if(!same) {
+
+    if (!same) {
       vcl_cout << "result: ["
                << results[8*i] << ' '
                << results[8*i+1] << ' '
@@ -214,8 +212,8 @@ static void test_traverse_force(bit_tree_test_manager* test_mgr, boct_tree<short
     }
     good = good && same;
   }
-  
-  for(int i=0; i<8; i++) {
+
+  for (int i=0; i<8; ++i) {
       vcl_cout << "result: ["
                << results[4*i] << ' '
                << results[4*i+1] << ' '
@@ -227,75 +225,76 @@ static void test_traverse_force(bit_tree_test_manager* test_mgr, boct_tree<short
 }
 #endif // 0
 
+#if 0 // most tests are commented out for the time being ...
 static void compare_traverse()
-{  
+{
   boct_tree<short, float>* btree = open_cl_test_data::four_tree<float>();
   bool good = false;
   cl_int* results;
-  
+
   /////////////////////////////////////////////////////////////////////////
   //////// 1. test bit_tree traverse
   /////////////////////////////////////////////////////////////////////////
   bit_tree_test_manager* test_mgr = bit_tree_test_manager::instance();
-  if(!test_mgr){
+  if (!test_mgr) {
     vcl_cout<<"bit_tree_test instance failed"<<vcl_endl;
     return;
   }
   test_mgr->init_arrays();
-  
+
   //set tree in manager
   boct_bit_tree test_tree = boct_bit_tree(btree);
   test_mgr->set_tree(test_tree.get_bits());
   test_mgr->init_manager();
-  
+
   //run traverse;
-  good=test_mgr->run_test(2); 
+  good=test_mgr->run_test(2);
   TEST("Bit tree traverse runs ", good, true);
   results = test_mgr->get_output();
-  for(int i=0; i<16; i++)
+  for (int i=0; i<16; ++i)
     vcl_cout<<results[i]<<vcl_endl;
 
-  
-  /////////////////////////////////////////////////////////////////////
-  //// 2. test ocl scene tree
-  /////////////////////////////////////////////////////////////////////
-  //ocl_scene_test_manager* ocl_mgr = ocl_scene_test_manager::instance();
-  //if(!ocl_mgr) {
-    //vcl_cout<<"OCL Manager instance failed"<<vcl_endl;
-    //return;
-  //}
-  //ocl_mgr->init_arrays();
-  
-  ////set up tree
-  //typedef vnl_vector_fixed<int,4> int4;
-  //typedef vnl_vector_fixed<float,16> float16;
-  //int4 arr_root(0);
-  //arr_root[0] = -1; //no parent for root
-  //arr_root[1] = -1; // no children yet
-  //vcl_vector<int4> cell_array;
-  //cell_array.push_back(arr_root);
-  //vcl_vector<float16> data_array;
-  //float16 dat_init(0.0);
-  //data_array.push_back(dat_init);
-  //boxm_ocl_convert<float>::copy_to_arrays(btree->root(), cell_array, data_array, 0);
-  //ocl_mgr->set_tree(cell_array);
-  
-  ////run traverse;
-  //ocl_mgr->init_manager();
-  //good = ocl_mgr->run_test(0);
-  //TEST("Ocl tree traverse runs ", good, true);
-  //results = ocl_mgr->get_output();
-  //for(int i=0; i<16; i++)
-    //vcl_cout<<results[i]<<vcl_endl;
-  
+#if 0
+  ///////////////////////////////////////////////////////////////////
+  // 2. test ocl scene tree
+  ///////////////////////////////////////////////////////////////////
+  ocl_scene_test_manager* ocl_mgr = ocl_scene_test_manager::instance();
+  if (!ocl_mgr) {
+    vcl_cout<<"OCL Manager instance failed"<<vcl_endl;
+    return;
+  }
+  ocl_mgr->init_arrays();
+
+  //set up tree
+  typedef vnl_vector_fixed<int,4> int4;
+  typedef vnl_vector_fixed<float,16> float16;
+  int4 arr_root(0);
+  arr_root[0] = -1; //no parent for root
+  arr_root[1] = -1; // no children yet
+  vcl_vector<int4> cell_array;
+  cell_array.push_back(arr_root);
+  vcl_vector<float16> data_array;
+  float16 dat_init(0.0);
+  data_array.push_back(dat_init);
+  boxm_ocl_convert<float>::copy_to_arrays(btree->root(), cell_array, data_array, 0);
+  ocl_mgr->set_tree(cell_array);
+
+  //run traverse;
+  ocl_mgr->init_manager();
+  good = ocl_mgr->run_test(0);
+  TEST("Ocl tree traverse runs ", good, true);
+  results = ocl_mgr->get_output();
+  for (int i=0; i<16; ++i)
+    vcl_cout<<results[i]<<vcl_endl;
+#endif // 0
 }
+#endif // static test not being used
 
-
+#if 0 // static test not being used
 static void compare_float3()
 {
-  
   bit_tree_test_manager* test_mgr = bit_tree_test_manager::instance();
-  if(!test_mgr){
+  if (!test_mgr) {
     vcl_cout<<"bit_tree_test instance failed"<<vcl_endl;
     return;
   }
@@ -308,9 +307,9 @@ static void compare_float3()
 
   cl_float* bbox_results = test_mgr->get_bbox_output();
   int index = 0;
-  for(int i=0; i<2; i++) {
-    for(int j=0; j<4; j++) {
-      vcl_cout<<bbox_results[index++]<<" ";
+  for (int i=0; i<2; ++i) {
+    for (int j=0; j<4; ++j) {
+      vcl_cout<<bbox_results[index++]<<' ';
     }
     vcl_cout<<'\n';
   }
@@ -320,20 +319,19 @@ static void compare_float3()
 
   bbox_results = test_mgr->get_bbox_output();
   index = 0;
-  for(int i=0; i<2; i++) {
-    for(int j=0; j<4; j++) {
-      vcl_cout<<bbox_results[index++]<<" ";
+  for (int i=0; i<2; ++i) {
+    for (int j=0; j<4; ++j) {
+      vcl_cout<<bbox_results[index++]<<' ';
     }
     vcl_cout<<'\n';
   }
-
-  
 }
+#endif //  // static test not being used
 
 static void test_scene_info()
 {
   bit_tree_test_manager* test_mgr = bit_tree_test_manager::instance();
-  if(!test_mgr){
+  if (!test_mgr) {
     vcl_cout<<"bit_tree_test instance failed"<<vcl_endl;
     return;
   }
@@ -342,9 +340,9 @@ static void test_scene_info()
   test_mgr->run_test(6); //test float 4
   cl_float* bbox_results = test_mgr->get_bbox_output();
   int index = 0;
-  for(int i=0; i<7; i++) {
-    for(int j=0; j<4; j++) {
-      vcl_cout<<bbox_results[index++]<<" ";
+  for (int i=0; i<7; ++i) {
+    for (int j=0; j<4; ++j) {
+      vcl_cout<<bbox_results[index++]<<' ';
     }
     vcl_cout<<'\n';
   }
@@ -361,33 +359,35 @@ static void test_bit_tree()
 
   ////prepare an octree for testing
   boct_tree<short, float>* boct_tree = open_cl_test_data::four_tree<float>();
-  
+
   ////prepare boct_bit_tree (test_tree)
   boct_bit_tree test_tree = boct_bit_tree(boct_tree);
-  
+
   ////set test tree and initialize manager
   bool init_arrays = test_mgr->init_arrays();
+  TEST("bit_tree_test_manager::init_arrays()", init_arrays, true);
   unsigned char* bits = test_tree.get_bits();
   test_mgr->set_tree(bits);
   bool init_mgr = test_mgr->init_manager();
-  //TEST("bit_tree_test_manager::init_manager()", init_mgr, true);
+  TEST("bit_tree_test_manager::init_manager()", init_mgr, true);
 
+#if 0
   //Run first test
-  //if (init_mgr) test_loc_code(test_mgr);
-  
+  if (init_mgr) test_loc_code(test_mgr);
+
   //run bit_at test
-  //if (init_mgr) test_bit_at(test_mgr);
-  
+  if (init_mgr) test_bit_at(test_mgr);
+
   //traverse test...
-  //if(init_mgr) test_traverse(test_mgr, boct_tree);
-  
-  //traverse force test ... 
-  //if (init_mgr) test_traverse_force(test_mgr, boct_tree);
-  
+  if (init_mgr) test_traverse(test_mgr, boct_tree);
+
+  //traverse force test ...
+  if (init_mgr) test_traverse_force(test_mgr, boct_tree);
+
   //comparison test:
-  //compare_traverse();
-  
-  //compare_float3();
+  compare_traverse();
+  compare_float3();
+#endif
   test_scene_info();
 }
 
