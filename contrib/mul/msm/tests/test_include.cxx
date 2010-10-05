d0e0d14aaa9da1ccc7d18592a61e9fc8b6af8aaf
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-10-05 02:30:15 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 29804

--- contrib/mul/msm/tests/test_include.cxx
+++ contrib/mul/msm/tests/test_include.cxx
@@ -1,3 +1,18 @@
+#include <msm/msm_add_all_loaders.h>
+#include <msm/msm_aligner.h>
+#include <msm/msm_box_limiter.h>
+#include <msm/msm_curve.h>
+#include <msm/msm_ellipsoid_limiter.h>
+#include <msm/msm_no_limiter.h>
+#include <msm/msm_param_limiter.h>
 #include <msm/msm_points.h>
+#include <msm/msm_ref_shape_instance.h>
+#include <msm/msm_shape_instance.h>
+#include <msm/msm_shape_model_builder.h>
+#include <msm/msm_shape_model.h>
+#include <msm/msm_similarity_aligner.h>
+#include <msm/msm_translation_aligner.h>
+#include <msm/msm_wt_mat_2d.h>
+#include <msm/msm_zoom_aligner.h>
 
 int main() { return 0; }
