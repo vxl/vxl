3457753ed7eadb5533982d46d4a48afc577a277b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-10-09 20:45:43 -0400

    corrected the "This is" line
    SVN-Revision: 29879

--- contrib/gel/mrc/vpgl/vsph/vsph_sph_point_3d.cxx
+++ contrib/gel/mrc/vpgl/vsph/vsph_sph_point_3d.cxx
@@ -1,4 +1,4 @@
-// This is gel/mrc/vpgl/vsph/vsph_spherical_coord.cxx
+// This is gel/mrc/vpgl/vsph/vsph_sph_point_3d.cxx
 #include "vsph_spherical_coord.h"
 #include <vcl_complex.h>
 #include <vcl_cmath.h>
@@ -13,28 +13,28 @@ void vsph_sph_point_3d::print(vcl_ostream& os) const
   os << " vsph_sph_point_3d:[radius=" <<radius_ << ",theta=" << theta_ << ",phi=" << phi_ << "] ";
 }
 
-void vsph_sph_point_3d::b_read(vsl_b_istream& is) 
+void vsph_sph_point_3d::b_read(vsl_b_istream& is)
 {
   short version;
   vsl_b_read(is, version);
   switch (version) {
     case 1:
       vsl_b_read(is,radius_);
-      vsl_b_read(is,theta_); 
+      vsl_b_read(is,theta_);
       vsl_b_read(is,phi_);
   }
 }
 
-void vsph_sph_point_3d::b_write(vsl_b_ostream& os) 
+void vsph_sph_point_3d::b_write(vsl_b_ostream& os)
 {
   vsl_b_write(os, version());
-  vsl_b_write(os, radius_); 
+  vsl_b_write(os, radius_);
   vsl_b_write(os, theta_);
   vsl_b_write(os,phi_);
 }
 
 vcl_ostream& operator<<(vcl_ostream& os, vsph_sph_point_3d const& p)
-{ 
+{
   p.print(os);
   return os;
 }
