d59ecf81d6f26caa3d4a52708e41caeaee5004c3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-10-10 04:23:31 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 29889

--- contrib/gel/mrc/vpgl/icam/tests/test_include.cxx
+++ contrib/gel/mrc/vpgl/icam/tests/test_include.cxx
@@ -1,8 +1,10 @@
 #include <vpgl/icam/icam_cost_func.h>
 #include <vpgl/icam/icam_depth_trans_pyramid.h>
 #include <vpgl/icam/icam_depth_transform.h>
+#include <vpgl/icam/icam_image.h>
 #include <vpgl/icam/icam_minimizer.h>
 #include <vpgl/icam/icam_sample.h>
+#include <vpgl/icam/icam_transform_2d.h>
 #include <vpgl/icam/icam_utils.h>
 #include <vpgl/icam/icam_view_metadata.h>
 #include <vpgl/icam/icam_view_sphere.h>
