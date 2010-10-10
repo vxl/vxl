004db288e4404c18f34dace0f6599d5c087499e3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-10-09 20:49:07 -0400

    TABs replaced by spaces
    SVN-Revision: 29886

--- contrib/gel/mrc/vpgl/icam/icam_depth_transform.h
+++ contrib/gel/mrc/vpgl/icam/icam_depth_transform.h
@@ -4,6 +4,9 @@
 //:
 // \file
 // \brief An image to image transformation with respect to a depth map
+// The transformation between an image with a depth map
+// and an image with a camera specified by K, R and t.
+// Both images have the same calibration matrix
 // \author J.L. Mundy
 // \date Sept 04, 2010
 //
@@ -11,9 +14,7 @@
 //  Modifications
 //   None
 // \endverbatim
-//: the transformation between an image with a depth map
-// and an image with a camera specified by K, R and t.
-// Both images have the same calibration matrix
+
 #include <vcl_vector.h>
 #include <vsl/vsl_binary_io.h>
 #include <vbl/vbl_ref_count.h>
@@ -27,60 +28,59 @@
 class icam_depth_transform
 {
  public:
-	 icam_depth_transform(): scale_factors_(vnl_vector<double>()){}
+  icam_depth_transform(): scale_factors_(vnl_vector<double>()){}
+
   //: Construct with the same calibration matrix
-  icam_depth_transform(vnl_matrix_fixed<double, 3, 3> const& K, 
+  icam_depth_transform(vnl_matrix_fixed<double, 3, 3> const& K,
                        vil_image_view<double> const& depth,
                        vgl_rotation_3d<double> const& rot,
                        vgl_vector_3d<double> const& trans,
-                       bool adjust_to_fl = false
-                        );
+                       bool adjust_to_fl = false);
   //: Construct with separate from and to calibration matrices
-  icam_depth_transform(vnl_matrix_fixed<double, 3, 3> const& K_from, 
-                       vnl_matrix_fixed<double, 3, 3> const& K_to, 
+  icam_depth_transform(vnl_matrix_fixed<double, 3, 3> const& K_from,
+                       vnl_matrix_fixed<double, 3, 3> const& K_to,
                        vil_image_view<double> const& depth,
                        vgl_rotation_3d<double> const& rot,
                        vgl_vector_3d<double> const& trans,
-                       bool adjust_to_fl = false
-                       );
+                       bool adjust_to_fl = false);
   //: Construct with separate from and to calibration matrices
-  icam_depth_transform(vnl_matrix_fixed<double, 3, 3> const& K_from, 
+  icam_depth_transform(vnl_matrix_fixed<double, 3, 3> const& K_from,
                        double to_fl, double to_pu, double to_pv,
                        vil_image_view<double> const& depth,
                        vgl_rotation_3d<double> const& rot,
                        vgl_vector_3d<double> const& trans,
-                       bool adjust_to_fl = false
-                       );
-  ~icam_depth_transform(){}
+                       bool adjust_to_fl = false);
+  ~icam_depth_transform() {}
   void set_scale_factors(vnl_vector<double> const& scales)
-    { scale_factors_ = scales;}
+  { scale_factors_ = scales; }
   vnl_vector<double>& scale_factors(){return scale_factors_;}
   void set_adjust_to_fl(bool adjust){adjust_to_fl_=adjust;}
   bool adjust_to_fl(){return adjust_to_fl_;}
   void set_calibration_matrix(vnl_matrix_fixed<double, 3, 3> const& K)
-    {this->set_k(K);}
+  { this->set_k(K); }
   void set_calibration_matrix(vnl_matrix_fixed<double, 3, 3> const& K_from,
                               vnl_matrix_fixed<double, 3, 3> const& K_to)
-    {this->set_k(K_from, K_to);}
+  { this->set_k(K_from, K_to); }
   void set_depth_map(vil_image_view<double> const& depth){depth_ = depth;}
   void set_rotation(vgl_rotation_3d<double> const& rot){rot_=rot;}
   void set_translation(vgl_vector_3d<double> const& trans){trans_ = trans;}
   vnl_matrix_fixed<double, 3, 3> calibration_matrix(){return K_to();}
   vnl_matrix_fixed<double, 3, 3>& from_calibration_matrix_inv()
-    {return K_from_inv_;}
+  { return K_from_inv_; }
   vnl_matrix_fixed<double, 3, 3> to_calibration_matrix()
-    {return K_to();}
+  { return K_to(); }
   vil_image_view<double>& depth_map(){return depth_ ;}
   vgl_rotation_3d<double> rotation(){return vgl_rotation_3d<double>(rot_);}
   vgl_vector_3d<double>& translation(){return trans_;}
 
   bool transform(double from_u, double from_v, double& to_u, double& to_v) const;
-  bool transform(vgl_point_2d<double> const& from_p, 
+  bool transform(vgl_point_2d<double> const& from_p,
                  vgl_point_2d<double>& to_p ) const
-    {return false;}
+  { return false; }
   //: support for cost function
-  unsigned n_params() {if(adjust_to_fl_)return 7; return 6;}
-  //: params are [a.vx, a.vy, a.vz, tx, ty, tz]
+  unsigned n_params() {if (adjust_to_fl_)return 7; return 6;}
+  //:
+  // params are [a.vx, a.vy, a.vz, tx, ty, tz]
   // where a.v is a Rodriques vector (a angle of rotation, v rotation axis)
   // the projection for the "from" camera is K[R, t]
   void set_params(vnl_vector<double> const& params);
@@ -114,4 +114,3 @@ class icam_depth_transform
 };
 
 #endif // icam_depth_transform_h_
-
