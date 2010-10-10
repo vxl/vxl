7da388bb2ed4b684e966d9091bdd26289a644b2c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-10-09 20:46:11 -0400

    added missing #include <icam/icam_image.h> and #include <icam/icam_transform_2d.h>
    SVN-Revision: 29880

--- contrib/gel/mrc/vpgl/icam/icam_utils.h
+++ contrib/gel/mrc/vpgl/icam/icam_utils.h
@@ -16,34 +16,35 @@
 #include <vsl/vsl_binary_io.h>
 #include <vbl/vbl_ref_count.h>
 #include <vgl/vgl_point_2d.h>
+#include <icam/icam_image.h>
+#include <icam/icam_transform_2d.h>
 
-#if 0
 class icam_utils
 {
  public:
-  //: destination image bounds and modified transform
-  //  under a homography the resampled image bounds map from
-  //  a rectangle (the source image) to a quadrilateral in the 
-  //  destination. This function finds the destination image 
+  //: destination image bounds and modified transform under a homography
+  //  The resampled image bounds map from a rectangle (the source image)
+  //  to a quadrilateral in the destination.
+  //  This function finds the destination image
   //  bounds and the modified transform to insure all source
   //  points are mapped to the destination with positive coordinates
   //  lying inside the destination bounds.
-  static void image_bounds(unsigned source_ni, unsigned source_nj, 
+  static void image_bounds(unsigned source_ni, unsigned source_nj,
                            icam_transform_2d const& t, unsigned& dest_ni,
                            unsigned& dest_nj, icam_transform_2d& mod_trans);
-
-  //: When resampling a source image to a destination,
+  //:
+  //  When resampling a source image to a destination,
   //  some pixels in the destination image may not be valid.
   //  The returned mask is 0 where the destination image does not contain
-  //  valid pixels.  Note that transform t is from the destination 
+  //  valid pixels.  Note that transform t is from the destination
   //  to the source. This transform sense is used in icam_sample_grid_bilin
   static icam_image<float> destination_mask(unsigned source_ni,
                                             unsigned source_nj,
                                             icam_transform_2d const& t);
- private:  
+ private:
+  // Private constructor/destructor: only static methods in this class.
   icam_utils();
   ~icam_utils();
 };
-#endif
-#endif // icam_utils_h_
 
+#endif // icam_utils_h_
