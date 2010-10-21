022264166b518be828a6fe577ea580226bdc3596
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-10-21 17:25:23 -0400

    added test_include.cxx
    SVN-Revision: 29990

---
+++ contrib/gel/mrc/vpgl/icam/icam_ocl/tests/test_include.cxx
@@ -0,0 +1,8 @@
+#include <vpgl/icam/icam_ocl/icam_ocl_utils.h>
+#include <vpgl/icam/icam_ocl/icam_ocl_cl.h>
+#include <vpgl/icam/icam_ocl/icam_ocl_cl_gl.h>
+#include <vpgl/icam/icam_ocl/icam_ocl_search_manager.h>
+#include <vpgl/icam/icam_ocl/icam_ocl_manager.h>
+#include <vpgl/icam/icam_ocl/icam_ocl_mem.h>
+
+int main() { return 0; }
