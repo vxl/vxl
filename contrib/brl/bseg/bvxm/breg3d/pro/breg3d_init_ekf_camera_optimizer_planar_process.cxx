0cd3451860fbf3311eed37fc59c2eecc0295c462
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-10-24 12:26:44 -0400

    minor typo
    SVN-Revision: 30004

--- contrib/brl/bseg/bvxm/breg3d/pro/breg3d_init_ekf_camera_optimizer_planar_process.cxx
+++ contrib/brl/bseg/bvxm/breg3d/pro/breg3d_init_ekf_camera_optimizer_planar_process.cxx
@@ -23,7 +23,7 @@
 
 breg3d_init_ekf_camera_optimizer_planar_process::breg3d_init_ekf_camera_optimizer_planar_process()
 {
-  // process takes 4 inputs: 
+  // process takes 4 inputs:
   //input[0]: The first estimated camera (should be of type vgpl_perspective_camera)
   //input[1]: The first frame
   //input[2]: The second frame
@@ -35,9 +35,9 @@ breg3d_init_ekf_camera_optimizer_planar_process::breg3d_init_ekf_camera_optimize
   input_types_[2] = "vil_image_view_base_sptr";
   input_types_[3] = "bvxm_voxel_world_sptr";
 
-  // process has 6 output:
+  // process has 6 outputs:
   // output[0]: The initial state estimate for the kalman filter
-  // output[1]: The second camera's estimated postion
+  // output[1]: The second camera's estimated position
   // outputs[2-5]: The plane parameters a,b,c,d  (ax + by + cz + d = 0)
   output_data_.resize(6,brdb_value_sptr(0));
   output_types_.resize(6);
@@ -51,44 +51,40 @@ breg3d_init_ekf_camera_optimizer_planar_process::breg3d_init_ekf_camera_optimize
   // parameters
   // default corresponds to roughly 1 degree std deviation
   if (!parameters()->add("Translation Scale Factor", "translation_scale", 0.005))
-    vcl_cerr << "ERROR: Adding parameters in " << __FILE__ << vcl_endl;
+    vcl_cerr << "ERROR: Adding parameters in " << __FILE__ << '\n';
 
   // default corresponds to roughly 1 degree std deviation
   if (!parameters()->add("Rotation Measurement Variance", "rotation_measurement_variance", 3e-4))
-    vcl_cerr << "ERROR: Adding parameters in " << __FILE__ << vcl_endl;
+    vcl_cerr << "ERROR: Adding parameters in " << __FILE__ << '\n';
 
   // default corresponds to roughly 0.5 meter std deviation
   if (!parameters()->add("Position Measurement Variance", "position_measurement_variance", 0.25))
-    vcl_cerr << "ERROR: Adding parameters in " << __FILE__ << vcl_endl;
-
+    vcl_cerr << "ERROR: Adding parameters in " << __FILE__ << '\n';
 }
 
 
 bool breg3d_init_ekf_camera_optimizer_planar_process::execute()
 {
-
   // Sanity check
-  if(!this->verify_inputs())
+  if (!this->verify_inputs())
     return false;
 
-  brdb_value_t<vpgl_camera_double_sptr>* input0 = 
+  brdb_value_t<vpgl_camera_double_sptr>* input0 =
     static_cast<brdb_value_t<vpgl_camera_double_sptr>* >(input_data_[0].ptr());
-  
-  brdb_value_t<vil_image_view_base_sptr>* input1 = 
+
+  brdb_value_t<vil_image_view_base_sptr>* input1 =
     static_cast<brdb_value_t<vil_image_view_base_sptr>* >(input_data_[1].ptr());
 
-  
-  brdb_value_t<vil_image_view_base_sptr>* input2 = 
+  brdb_value_t<vil_image_view_base_sptr>* input2 =
     static_cast<brdb_value_t<vil_image_view_base_sptr>* >(input_data_[2].ptr());
 
-  
-  brdb_value_t<bvxm_voxel_world_sptr>* input3 = 
+  brdb_value_t<bvxm_voxel_world_sptr>* input3 =
     static_cast<brdb_value_t<bvxm_voxel_world_sptr>* >(input_data_[3].ptr());
 
   // get first camera
   vpgl_perspective_camera<double> *cam0;
   if (!(cam0 = dynamic_cast<vpgl_perspective_camera<double>*>(input0->value().ptr()))) {
-       vcl_cerr << "error: process expects camera to be a vpgl_perspective_camera." << vcl_endl;
+       vcl_cerr << "error: process expects camera to be a vpgl_perspective_camera.\n";
       return false;
   }
 
@@ -108,7 +104,7 @@ bool breg3d_init_ekf_camera_optimizer_planar_process::execute()
     vcl_cout << "breg3d_init_ekf_camera_optimizer_planar_process::execute() -- problem in retrieving parameter position_variance\n";
     return false;
   }
-  
+
   if (!parameters()->get_value(vcl_string("translation_scale"), t_scale)) {
     vcl_cout << "breg3d_init_ekf_camera_optimizer_planar_process::execute() -- problem in retrieving parameter translation_scale\n";
     return false;
@@ -126,7 +122,7 @@ bool breg3d_init_ekf_camera_optimizer_planar_process::execute()
   h_gen->set_image1(img1_float);
   ihog_transform_2d H = h_gen->compute_homography();
 
-  vcl_cout << "H = " << vcl_endl << H.matrix() << vcl_endl;
+  vcl_cout << "H =\n" << H.get_matrix() << vcl_endl;
 
   // TEMP
   vpgl_perspective_camera<double> *cam1 = new vpgl_perspective_camera<double>(cam0->get_calibration(),cam0->get_camera_center(),cam0->get_rotation());
@@ -140,32 +136,29 @@ bool breg3d_init_ekf_camera_optimizer_planar_process::execute()
 
   //store output
   // initial state
-  brdb_value_sptr output0 = 
+  brdb_value_sptr output0 =
     new brdb_value_t<breg3d_ekf_camera_optimizer_state>(init_state);
   output_data_[0] = output0;
 
   // camera1 estimate
-  brdb_value_sptr output1 = 
+  brdb_value_sptr output1 =
     new brdb_value_t<vpgl_camera_double_sptr>(cam1);
   output_data_[1] = output1;
 
   // plane estimate
-  brdb_value_sptr output2 = 
+  brdb_value_sptr output2 =
     new brdb_value_t<double>(plane_est.a());
   output_data_[2] = output2;
-  brdb_value_sptr output3 = 
+  brdb_value_sptr output3 =
     new brdb_value_t<double>(plane_est.b());
   output_data_[3] = output3;
-  brdb_value_sptr output4 = 
+  brdb_value_sptr output4 =
     new brdb_value_t<double>(plane_est.c());
   output_data_[4] = output4;
-  brdb_value_sptr output5 = 
+  brdb_value_sptr output5 =
     new brdb_value_t<double>(plane_est.d());
   output_data_[5] = output5;
 
-
   return true;
 }
-
-
 
