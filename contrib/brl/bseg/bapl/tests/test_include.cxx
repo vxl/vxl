75325bfa7ce77e901da84e3708484fd68e74de05
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-10-29 07:05:44 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 30068

--- contrib/brl/bseg/bapl/tests/test_include.cxx
+++ contrib/brl/bseg/bapl/tests/test_include.cxx
@@ -2,16 +2,24 @@
 #include <bapl/bapl_affine_roi.h>
 #include <bapl/bapl_affine_transform.h>
 #include <bapl/bapl_bbf_tree.h>
+#include <bapl/bapl_connectivity.h>
 #include <bapl/bapl_keypoint.h>
 #include <bapl/bapl_keypoint_extractor.h>
-#include <bapl/bapl_keypoint_sptr.h>
+#include <bapl/bapl_keypoint_set.h>
 #include <bapl/bapl_lowe_cluster.h>
 #include <bapl/bapl_lowe_keypoint.h>
 #include <bapl/bapl_lowe_pyramid.h>
 #include <bapl/bapl_lowe_pyramid_set.h>
-#include <bapl/bapl_lowe_pyramid_set_sptr.h>
 #include <bapl/bapl_mi_matcher.h>
 #include <bapl/bapl_mi_matcher_params.h>
 
+#include <bapl/bapl_connectivity_sptr.h>
+#include <bapl/bapl_keypoint_sptr.h>
+#include <bapl/bapl_keypoint_set_sptr.h>
+#include <bapl/bapl_lowe_keypoint_sptr.h>
+#include <bapl/bapl_lowe_pyramid_set_sptr.h>
+
+#include <bapl/pro/bapl_processes.h>
+#include <bapl/pro/bapl_register.h>
 
 int main() { return 0; }
--- contrib/gel/mrc/vpgl/icam/icam_ocl/tests/test_include.cxx
+++ contrib/gel/mrc/vpgl/icam/icam_ocl/tests/test_include.cxx
@@ -1,8 +1,9 @@
-#include <vpgl/icam/icam_ocl/icam_ocl_utils.h>
 #include <vpgl/icam/icam_ocl/icam_ocl_cl.h>
 #include <vpgl/icam/icam_ocl/icam_ocl_cl_gl.h>
-#include <vpgl/icam/icam_ocl/icam_ocl_search_manager.h>
+#include <vpgl/icam/icam_ocl/icam_ocl_kernel.h>
 #include <vpgl/icam/icam_ocl/icam_ocl_manager.h>
 #include <vpgl/icam/icam_ocl/icam_ocl_mem.h>
+#include <vpgl/icam/icam_ocl/icam_ocl_search_manager.h>
+#include <vpgl/icam/icam_ocl/icam_ocl_utils.h>
 
 int main() { return 0; }
