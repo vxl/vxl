53a9c57e847e0b26f3114ba16283e4bc55fc310b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-10-29 16:51:21 -0400

    avoid compiler warning (implicit cast from int to float), by actually not using "int" at all!
    SVN-Revision: 30073

--- contrib/brl/bseg/bvpl/bvpl_gauss3d_x_kernel_factory.cxx
+++ contrib/brl/bseg/bvpl/bvpl_gauss3d_x_kernel_factory.cxx
@@ -26,15 +26,15 @@ bvpl_gauss3d_x_kernel_factory::bvpl_gauss3d_x_kernel_factory(float s1, float s2)
   sigma1_ = s1;
   sigma2_ = s2;
   sigma3_ = s2;
-  
+
   //this skernel is symmetric around main axis
   angular_resolution_=0;
-  
+
   //initialize variables
   angle_ = 0.0f;
   rotation_axis_ = canonical_rotation_axis_;
   parallel_axis_ = canonical_parallel_axis_;
-  
+
   //create the default kernel
   create_canonical();
 }
@@ -46,16 +46,16 @@ bvpl_gauss3d_x_kernel_factory::bvpl_gauss3d_x_kernel_factory(float s1, float s2,
   sigma1_ = s1;
   sigma2_ = s2;
   sigma3_ = s3;
-  
+
   //this value is a meant as a limit there is not theoretical meaning to it
   angular_resolution_= float(vnl_math::pi/16.0);
-  
+
   //initialize variables
   angle_ = 0.0f;
   rotation_axis_ = canonical_rotation_axis_;
   parallel_axis_ = canonical_parallel_axis_;
 
-  
+
   //create the default kernel
   create_canonical();
 }
@@ -66,39 +66,37 @@ static inline float third_power(float x) { return x*x*x; }
 void bvpl_gauss3d_x_kernel_factory::create_canonical()
 {
   bsta_gauss_if3 gauss_kernel(vnl_float_3(0,0,0), vnl_float_3(sigma1_*sigma1_, sigma2_*sigma2_, sigma3_*sigma3_));
-  
+
   typedef vgl_point_3d<float> point_3d;
   typedef bvpl_kernel_dispatch dispatch;
-  
+
   //This is the support of the kernel
-  int min_x = -int(3.0f*sigma1_+0.01f);
-  int max_x =  int(3.0f*sigma1_+0.01f);
-  int min_y = -int(2.0f*sigma2_+0.01f);
-  int max_y =  int(2.0f*sigma2_+0.01f);
-  int min_z = -int(2.0f*sigma3_+0.01f);
-  int max_z =  int(2.0f*sigma3_+0.01f);
-  
-  for (int x=min_x; x<= max_x; x++)
+  float min_x = -3.0f*sigma1_;
+  float max_x =  3.0f*sigma1_;
+  float min_y = -2.0f*sigma2_;
+  float max_y =  2.0f*sigma2_;
+  float min_z = -2.0f*sigma3_;
+  float max_z =  2.0f*sigma3_;
+
+  for (float x=min_x; x<= max_x; x+=1.f)
   {
-    for (int y= min_y; y<= max_y; y++)
+    for (float y= min_y; y<= max_y; y+=1.f)
     {
-      for (int z= min_z; z<= max_z; z++)
+      for (float z= min_z; z<= max_z; z+=1.f)
       {
         vnl_float_3 pt(x,y,z);
-        canonical_kernel_.push_back(vcl_pair<point_3d,dispatch>(point_3d(float(x),float(y),float(z)), dispatch(gauss_kernel.gradient(pt)[0])));
+        canonical_kernel_.push_back(vcl_pair<point_3d,dispatch>(point_3d(x,y,z), dispatch(gauss_kernel.gradient(pt)[0])));
       }
     }
   }
-  
+
   //set the dimension of the 3-d grid
   max_point_.set(max_x,max_y,max_z);
   min_point_.set(min_x,min_y,min_z);
-  
+
   //set the current kernel
   kernel_ = canonical_kernel_;
   factory_name_ = name();
-  
-  return;
 }
 
 #if 0 // commented out
@@ -117,14 +115,14 @@ bvpl_kernel_vector_sptr bvpl_gauss3d_x_kernel_factory::create_kernel_vector()
   vnl_float_3 axis;
   float theta = 0.0f;
   float phi = 0.0f;
-  
+
   //when zenith angle is 0
   axis[0] =0.0f;
   axis[1] =0.0f;
   axis[2] =1.0f;
   this->set_rotation_axis(axis);
   kernels->kernels_.push_back(vcl_make_pair(axis , new bvpl_kernel(this->create())));
-  
+
   //when zenith is pi/4 traverse all hemisphere
   phi = float(vnl_math::pi_over_4);
   for (;theta < 2.0f*float(vnl_math::pi)-1e-5; theta +=theta_res)
@@ -135,7 +133,7 @@ bvpl_kernel_vector_sptr bvpl_gauss3d_x_kernel_factory::create_kernel_vector()
     this->set_rotation_axis(axis);
     kernels->kernels_.push_back(vcl_make_pair(axis , new bvpl_kernel(this->create())));
   }
-  
+
   //when zenith is pi/2 we only traverse half a hemisphere
   phi = float(vnl_math::pi_over_2);
   theta =0.0f;
@@ -147,7 +145,7 @@ bvpl_kernel_vector_sptr bvpl_gauss3d_x_kernel_factory::create_kernel_vector()
     this->set_rotation_axis(axis);
     kernels->kernels_.push_back(vcl_make_pair(axis , new bvpl_kernel(this->create())));
   }
-  
+
   return kernels;
 }
 
@@ -161,7 +159,7 @@ bvpl_kernel_vector_sptr bvpl_gauss3d_x_kernel_factory::create_kernel_vector(floa
 //: Creates a vector of kernels  according to given azimuthal, levation, and angular resolution
 bvpl_kernel_vector_sptr bvpl_gauss3d_x_kernel_factory::create_kernel_vector(float pi, float phi, float angular_res)
 {
-  //to be impemented
+  //to be implemented
   return 0;
 }
 
