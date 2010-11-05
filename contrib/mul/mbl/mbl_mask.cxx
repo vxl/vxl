8ec2229f4f76371628a7a3f22338377d7d8117cc
Gwenael Guillard <gwenaelguillard@users.sourceforge.net>
2010-11-05 11:40:37 -0400

    More explicit thrown exception
    SVN-Revision: 30116

--- contrib/mul/mbl/mbl_mask.cxx
+++ contrib/mul/mbl/mbl_mask.cxx
@@ -99,18 +99,18 @@ void mbl_mask_logic_nand(const mbl_mask & A, mbl_mask & B)
 void mbl_mask_logic(const mbl_mask & A, mbl_mask & B, const vcl_string & operation)
 {
   if (A.size() != B.size())
-    throw vcl_runtime_error("Mask lengths differ");
+    throw vcl_runtime_error("mbl_mask_logic: Mask lengths differ");
 
   // Validate the operation to perform and parse into vector
 
   if (operation.length() != 4)
-    throw vcl_runtime_error("Operation must be of length 4");
+    throw vcl_runtime_error("mbl_mask_logic: Operation must be of length 4");
   vcl_vector<bool> op_rule(4);
   for (unsigned i = 0 ; i < 4 ; ++i)
   {
     if (operation[i] == '0') op_rule[i] = false;
     else if (operation[i] == '1') op_rule[i] = true;
-    else throw vcl_runtime_error("Invalid character in operation string - must contain only '0' or '1'");
+    else throw vcl_runtime_error("mbl_mask_logic: Invalid character in operation string - must contain only '0' or '1'");
   }
 
   // Apply the operation in place
