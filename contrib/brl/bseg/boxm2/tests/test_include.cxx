dc4e1544ada92dccfc795365f626426feb3992e3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-11-13 06:13:33 -0500

    added test_include
    SVN-Revision: 30215

---
+++ contrib/brl/bseg/boxm2/tests/test_include.cxx
@@ -0,0 +1,7 @@
+#include <boxm2/boxm2_block.h>
+#include <boxm2/boxm2_disk_io_mgr.h>
+#include <boxm2/boxm2_scene.h>
+#include <boxm2/boxm2_scene_parser.h>
+#include <boxm2/boxm2_storage_mgr.h>
+
+int main() { return 0; }
