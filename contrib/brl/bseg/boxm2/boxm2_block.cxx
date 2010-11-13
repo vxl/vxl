6c6470a8fb7f7249acb646cbba6afc4b98c39326
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-11-13 06:11:03 -0500

    added missing file (just a "dummy" implementation for now)
    SVN-Revision: 30210

---
+++ contrib/brl/bseg/boxm2/boxm2_block.cxx
@@ -0,0 +1,5 @@
+#include <boxm2/boxm2_block.h>
+
+boxm2_block::boxm2_block(char*) {}                 // NOT YET IMPLEMENTED
+bool boxm2_block::b_read(char*) { return false; }  // NOT YET IMPLEMENTED
+bool boxm2_block::b_write(char*) { return false; } // NOT YET IMPLEMENTED
---
+++ contrib/brl/bseg/boxm2/boxm2_storage_mgr.cxx
@@ -0,0 +1,3 @@
+#include <boxm2/boxm2_storage_mgr.h>
+
+int boxm2_storage_mgr::get_dummy() const { return dummy_; }
