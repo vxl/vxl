fe5ae3cb16a3931d6f56b0a986cf079ac561f0bc
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-11-13 05:13:01 -0500

    added missing #include <iosfwd>
    SVN-Revision: 30207

--- contrib/brl/bseg/boxm2/boxm2_scene.h
+++ contrib/brl/bseg/boxm2/boxm2_scene.h
@@ -9,12 +9,13 @@
 #include <vpgl/bgeo/bgeo_lvcs.h>
 #include <vgl/vgl_point_3d.h>
 #include <vgl/vgl_vector_3d.h>
+#include <vcl_iosfwd.h>
 
 //smart pointer stuff
 #include <vbl/vbl_ref_count.h>
 #include <vbl/vbl_smart_ptr.h>
- 
-//: boxm2_scene: simple scene model that maintains (in world coordinates) 
+
+//: boxm2_scene: simple scene model that maintains (in world coordinates)
 //      - scene origin
 //      - number of blocks in each dimension
 //      - size of each block in each dimension
@@ -27,47 +28,46 @@ class boxm2_scene : public vbl_ref_count
 
     //: initializes scene from xmlFile
     boxm2_scene(vcl_string filename);
-    
+
     //: destructor
     ~boxm2_scene() { }
-    
+
     //: scene dimensions accessors
-    vgl_vector_3d<double>   block_dim()   { return block_dim_; }
-    vgl_vector_3d<unsigned> block_num()   { return block_num_; }
-    vgl_point_3d<double>    local_origin(){ return local_origin_; }
-    vgl_point_3d<double>    rpc_origin()  { return rpc_origin_; }
-    bgeo_lvcs               lvcs()        { return lvcs_; }
-    
-    //: scene path accessors  
-    vcl_string              xml_path()    { return xml_path_; }
-    vcl_string              data_path()   { return data_path_; }
+    vgl_vector_3d<double>   block_dim()   const { return block_dim_; }
+    vgl_vector_3d<unsigned> block_num()   const { return block_num_; }
+    vgl_point_3d<double>    local_origin()const { return local_origin_; }
+    vgl_point_3d<double>    rpc_origin()  const { return rpc_origin_; }
+    bgeo_lvcs               lvcs()        const { return lvcs_; }
+
+    //: scene path accessors
+    vcl_string              xml_path()    const { return xml_path_; }
+    vcl_string              data_path()   const { return data_path_; }
 
     //: scene version number
     static short version_no() { return 1; }
 
   private:
 
-    //: world scene information 
+    //: world scene information
     bgeo_lvcs               lvcs_;
     vgl_point_3d<double>    local_origin_;
     vgl_point_3d<double>    rpc_origin_;
-    
+
     //: World dimensions of a block .e.g 1 meter x 1 meter x 1 meter
     vgl_vector_3d<double>   block_dim_;
-    
+
     //: number of blocks in each dimension
     vgl_vector_3d<unsigned> block_num_;
-    
+
     //: location on disk of xml file and data/block files
     vcl_string data_path_, xml_path_;
-
 };
 
 //Smart_Pointer typedef for boxm2_scene
 typedef vbl_smart_ptr<boxm2_scene> boxm2_scene_sptr;
 
-//: scene output stream operator 
-vcl_ostream& operator <<(vcl_ostream &s, boxm2_scene& scene);
+//: scene output stream operator
+vcl_ostream& operator<<(vcl_ostream &s, boxm2_scene& scene);
 
 //: scene xml write function
 void x_write(vcl_ostream &os, boxm2_scene scene, vcl_string name);
