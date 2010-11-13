7a6c618314a8257e255b7cd3dab513fb0bf4bc84
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-11-13 05:13:26 -0500

    added missing (and required) include guard
    SVN-Revision: 30208

--- contrib/brl/bseg/boxm2/boxm2_disk_io_mgr.h
+++ contrib/brl/bseg/boxm2/boxm2_disk_io_mgr.h
@@ -1,4 +1,7 @@
-
+#ifndef boxm2_disk_io_mgr_h_
+#define boxm2_disk_io_mgr_h_
+//:
+// \file
 #include <boxm2/boxm2_block.h>
 #include <vcl_iostream.h>
 #include <vcl_sstream.h>
@@ -6,31 +9,34 @@
 #include <bbas/baio/baio.h>
 #include <vgl/vgl_point_3d.h>
 
-//: boxm2_disk_io_mgr: disk level storage class
+//: disk level storage class.
 //  handles all of the asynchronous IO read and write requests
 class boxm2_disk_io_mgr
 {
-  
-  public: 
-    
-    //: creates a BAIO object that loads/saves block data from disk
-    void load_block(vcl_string dir, vgl_point_3d<int> block_id); 
+  public:
+
+    //: creates a BAIO object that loads block data from disk
+    void load_block(vcl_string dir, vgl_point_3d<int> block_id);
+    //: creates a BAIO object that saves block data to disk
     void save_block(vcl_string dir, boxm2_block* block );
-    
-    //: creates a BAIO object that laods/saves data from disk
-    void load_data(vcl_string dir, vgl_point_3d<int> block_id, int data_type); 
+
+    //: creates a BAIO object that loads data from disk
+    void load_data(vcl_string dir, vgl_point_3d<int> block_id, int data_type);
+    //: creates a BAIO object that saves data to disk
     void save_data(vcl_string dir, vgl_point_3d<int> block_id, int data_type);
-  
-    //: returns load and save lists (for updating cache)
+
+    //: returns load list (for updating cache)
     vcl_vector<baio> load_list() { return load_list_; }
+    //: returns save list (for updating cache)
     vcl_vector<baio> save_list() { return save_list_; }
-  
-  private: 
-    
+
+  private:
+
     //: list of asynchronous io loads
     vcl_vector<baio> load_list_;
-    
+
     //: list of asynchronous io saves
-    vcl_vector<baio> save_list_; 
-  
+    vcl_vector<baio> save_list_;
 };
+
+#endif // boxm2_disk_io_mgr_h_
