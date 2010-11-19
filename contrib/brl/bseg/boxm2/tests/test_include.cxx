80c705efbc0f1472756c1be342b756ee2d84a56b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-11-19 01:49:35 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 30278

--- contrib/brl/bseg/boxm2/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/tests/test_include.cxx
@@ -5,6 +5,8 @@
 #include <boxm2/boxm2_block.h>
 #include <boxm2/boxm2_block_id.h>
 #include <boxm2/boxm2_cache.h>
+#include <boxm2/boxm2_data.h>
+#include <boxm2/boxm2_data_traits.h>
 #include <boxm2/boxm2_disk_io_mgr.h>
 #include <boxm2/boxm2_dumb_cache.h>
 #include <boxm2/boxm2_scene.h>
