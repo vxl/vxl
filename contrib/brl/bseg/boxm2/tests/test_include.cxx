3d0d088f8266c30f243a1a610aa3d156fa01cadc
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-11-25 18:13:26 -0500

    moved test_include.cxx entries for subdir "basic" into that subdir
    SVN-Revision: 30341

--- contrib/brl/bseg/boxm2/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/tests/test_include.cxx
@@ -1,6 +1,3 @@
-#include <boxm2/basic/boxm2_array_1d.h>
-#include <boxm2/basic/boxm2_array_2d.h>
-#include <boxm2/basic/boxm2_array_3d.h>
 #include <boxm2/boxm2_asio_mgr.h>
 #include <boxm2/boxm2_block.h>
 #include <boxm2/boxm2_block_id.h>
@@ -8,6 +5,7 @@
 #include <boxm2/boxm2_data.h>
 #include <boxm2/boxm2_data_traits.h>
 #include <boxm2/boxm2_dumb_cache.h>
+#include <boxm2/boxm2_nn_cache.h>
 #include <boxm2/boxm2_scene.h>
 #include <boxm2/boxm2_scene_parser.h>
 #include <boxm2/boxm2_sio_mgr.h>
