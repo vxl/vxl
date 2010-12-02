2b7bef9603fbcd5607791ce507d73ba4f0a3e02b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-12-02 16:53:42 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 30377

--- contrib/brl/bseg/boxm2/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/tests/test_include.cxx
@@ -1,13 +1,21 @@
-#include <boxm2/io/boxm2_asio_mgr.h>
 #include <boxm2/boxm2_block.h>
 #include <boxm2/boxm2_block_id.h>
-#include <boxm2/io/boxm2_cache.h>
+#include <boxm2/boxm2_cpp_process_base.h>
+#include <boxm2/boxm2_cpp_processor.h>
 #include <boxm2/boxm2_data.h>
 #include <boxm2/boxm2_data_traits.h>
-#include <boxm2/io/boxm2_dumb_cache.h>
-#include <boxm2/io/boxm2_nn_cache.h>
+#include <boxm2/boxm2_opencl_process_base.h>
+#include <boxm2/boxm2_opencl_processor.h>
+#include <boxm2/boxm2_opencl_render_process.h>
+#include <boxm2/boxm2_process.h>
+#include <boxm2/boxm2_processor.h>
 #include <boxm2/boxm2_scene.h>
 #include <boxm2/boxm2_scene_parser.h>
+
+#include <boxm2/io/boxm2_asio_mgr.h>
+#include <boxm2/io/boxm2_cache.h>
+#include <boxm2/io/boxm2_dumb_cache.h>
+#include <boxm2/io/boxm2_nn_cache.h>
 #include <boxm2/io/boxm2_sio_mgr.h>
 
 int main() { return 0; }
