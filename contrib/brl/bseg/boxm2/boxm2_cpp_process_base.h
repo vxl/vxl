e76a9a0f13897f9e59d2883e4a1dad455f4c4de5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-12-02 16:11:55 -0500

    added missing #include
    SVN-Revision: 30373

--- contrib/brl/bseg/boxm2/boxm2_cpp_process_base.h
+++ contrib/brl/bseg/boxm2/boxm2_cpp_process_base.h
@@ -1,7 +1,9 @@
 #ifndef boxm2_cpp_process_base_h
 #define boxm2_cpp_process_base_h
 
-class boxm2_cpp_process_base:public boxm2_process
+#include <boxm2/boxm2_process.h>
+
+class boxm2_cpp_process_base : public boxm2_process
 {
 };
 
--- contrib/brl/bseg/boxm2/boxm2_opencl_render_process.h
+++ contrib/brl/bseg/boxm2/boxm2_opencl_render_process.h
@@ -1,7 +1,9 @@
 #ifndef boxm2_opencl_render_process_h
 #define boxm2_opencl_render_process_h
 
-class boxm2_opencl_render_process:: public boxm2_opencl_process_base
+#include <boxm2/boxm2_opencl_process_base.h>
+
+class boxm2_opencl_render_process : public boxm2_opencl_process_base
 {
 };
 
