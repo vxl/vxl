d6357cea36769b1671e62644e3286ed7b4d5f14d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-12-09 12:26:44 -0500

    corrected the Docygen comment
    SVN-Revision: 30452

--- contrib/brl/bseg/boxm2/boxm2_block.cxx
+++ contrib/brl/bseg/boxm2/boxm2_block.cxx
@@ -117,20 +117,21 @@ bool boxm2_block::b_write(char* buff)
 }
 
 
-
-
-
 //------------ I/O -------------------------------------------------------------
-//: Binary write boxm_update_bit_scene_manager scene to stream
-//void vsl_b_write(vsl_b_ostream& os, boxm2_block const& scene) {}
-//void vsl_b_write(vsl_b_ostream& os, const boxm2_block* &p) {}
-//void vsl_b_write(vsl_b_ostream& os, boxm2_block_sptr& sptr) {}
+#if 0
+void vsl_b_write(vsl_b_ostream& os, boxm2_block const& scene) {}
+void vsl_b_write(vsl_b_ostream& os, const boxm2_block* &p) {}
+void vsl_b_write(vsl_b_ostream& os, boxm2_block_sptr& sptr) {}
+#endif
+//: Binary write boxm2_block to stream.
 void vsl_b_write(vsl_b_ostream& os, boxm2_block_sptr const& sptr) {}
 
-
-//: Binary load boxm_update_bit_scene_manager scene from stream.
-//void vsl_b_read(vsl_b_istream& is, boxm2_block &scene) {}
-//void vsl_b_read(vsl_b_istream& is, boxm2_block* p) {}
+#if 0
+void vsl_b_read(vsl_b_istream& is, boxm2_block &scene) {}
+void vsl_b_read(vsl_b_istream& is, boxm2_block* p) {}
+#endif
+//: Binary load boxm2_block from stream.
 void vsl_b_read(vsl_b_istream& is, boxm2_block_sptr& sptr) {}
+//: Binary load boxm2_block from stream.
 void vsl_b_read(vsl_b_istream& is, boxm2_block_sptr const& sptr) {}
 
