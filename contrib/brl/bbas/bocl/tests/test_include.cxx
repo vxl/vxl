139386d4b5590b915ea066ffede59b4ae61d346c
Gamze Tunali <gamze@users.sourceforge.net>
2010-12-09 10:26:24 -0500

    added the paths for new files
    SVN-Revision: 30448

--- contrib/brl/bbas/bocl/tests/test_include.cxx
+++ contrib/brl/bbas/bocl/tests/test_include.cxx
@@ -1,7 +1,11 @@
+#include <bocl/bocl_buffer.h>
+#include <bocl/bocl_buffer_mgr.h>
 #include <bocl/bocl_cl.h>
 #include <bocl/bocl_cl_gl.h>
 #include <bocl/bocl_global_memory_bandwidth_manager.h>
+#include <bocl/bocl_kernel.h>
 #include <bocl/bocl_manager.h>
+#include <bocl/bocl_mem.h>
 #include <bocl/bocl_utils.h>
 
 int main() { return 0; }
