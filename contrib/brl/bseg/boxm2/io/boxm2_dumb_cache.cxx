a70bd4424e33e93007df2dd90e948839c5e5e25e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-12-13 05:49:52 -0500

    avoid compiler warning (unused variable)
    SVN-Revision: 30492

--- contrib/brl/bseg/boxm2/io/boxm2_dumb_cache.cxx
+++ contrib/brl/bseg/boxm2/io/boxm2_dumb_cache.cxx
@@ -2,40 +2,39 @@
 //:
 // \file
 
-//: destructor 
+//: destructor
 boxm2_dumb_cache::~boxm2_dumb_cache()
 {
+#if 0
   // clean up block
-  //if(cached_block_) delete cached_block_; 
-  
+  delete cached_block_;
+
   // clean up loaded data
-  vcl_map<vcl_string, boxm2_data_base* >::iterator iter; 
-  for(iter=cached_data_.begin(); iter!=cached_data_.end(); ++iter) {
-    boxm2_data_base* dat = (*iter).second; 
-    //if(dat) delete dat; 
-  } 
+  vcl_map<vcl_string, boxm2_data_base* >::iterator iter;
+  for (iter=cached_data_.begin(); iter!=cached_data_.end(); ++iter) {
+    delete (*iter).second;
+  }
+#endif // 0
 }
 
 //: realization of abstract "get_block(block_id)"
 boxm2_block* boxm2_dumb_cache::get_block(boxm2_block_id id)
 {
   vcl_cout<<"Dumb Cache Get Block"<<vcl_endl;
-  if(cached_block_) {
-    if(cached_block_->block_id() == id)
-      return cached_block_; 
-  }
+  if (cached_block_ && cached_block_->block_id() == id)
+    return cached_block_;
 
   //otherwise load it from disk with blocking
   boxm2_block* loaded = boxm2_sio_mgr::load_block(scene_dir_, id);
-  
+
   //update cache
-  this->update_block_cache(loaded); 
+  this->update_block_cache(loaded);
   return loaded;
 }
 
-void boxm2_dumb_cache::update_block_cache(boxm2_block* blk) 
+void boxm2_dumb_cache::update_block_cache(boxm2_block* blk)
 {
-  if(cached_block_) delete cached_block_; 
-  cached_block_ = blk; 
+  delete cached_block_;
+  cached_block_ = blk;
 }
 
