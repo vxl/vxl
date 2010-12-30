606865e5a503e1fe8af23cc4893d37d2dcbe9398
Brandon Mayer <bmayer1@users.sourceforge.net>
2010-12-30 12:15:21 -0500

    SVN-Revision: 30574

--- contrib/brl/bseg/bapl/tests/test_driver.cxx
+++ contrib/brl/bseg/bapl/tests/test_driver.cxx
@@ -7,6 +7,7 @@ DECLARE( test_matcher );
 DECLARE( test_lowe_pyramid_set );
 DECLARE( test_compute_tracks );
 DECLARE( test_match_keypoints );
+DECLARE( test_dense_sift );
 
 void
 register_tests()
@@ -17,6 +18,7 @@ register_tests()
   REGISTER( test_lowe_pyramid_set );
   REGISTER( test_compute_tracks );
   REGISTER( test_match_keypoints );
+  REGISTER( test_dense_sift );
 }
 
 DEFINE_MAIN;
