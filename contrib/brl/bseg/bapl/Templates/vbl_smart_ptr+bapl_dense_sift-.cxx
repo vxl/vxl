b2a133e64770d6a2c609a022c7b9267c92f0bf2f
Brandon Mayer <bmayer1@users.sourceforge.net>
2010-12-30 18:01:04 -0500

    SVN-Revision: 30583

---
+++ contrib/brl/bseg/bapl/Templates/vbl_smart_ptr+bapl_dense_sift-.cxx
@@ -0,0 +1,6 @@
+//this is bseg/bapl/Templates/vbl_smart_ptr+bapl_dense_sift-.cxx
+
+#include<vbl/vbl_smart_ptr.txx>
+#include"bapl/bapl_dense_sift.h"
+
+VBL_SMART_PTR_INSTANTIATE( bapl_dense_sift );
