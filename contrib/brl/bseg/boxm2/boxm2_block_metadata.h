6d03bd6d44b6a200e2f7b26ce9dc67f30ee5ecf1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2010-12-31 12:42:04 -0500

    avoid compiler warning (need explicit init of vbl_ref_count() in copy constructor)
    SVN-Revision: 30601

--- contrib/brl/bseg/boxm2/boxm2_block_metadata.h
+++ contrib/brl/bseg/boxm2/boxm2_block_metadata.h
@@ -4,8 +4,8 @@
 // \file
 // \brief  boxm2_block_meta data keeps track of non structure, non visual data
 //
-//  Block meta data includes: 
-//    id, 
+//  Block meta data includes:
+//    id,
 //    block origin
 //    num_sub_blocks (x,y,z)
 //    sub_block_dim (x,y,z)
@@ -27,16 +27,16 @@
 
 class boxm2_block_metadata: public vbl_ref_count
 {
-  public: 
+  public:
     boxm2_block_metadata() {}
-    boxm2_block_metadata( boxm2_block_id id, 
+    boxm2_block_metadata( boxm2_block_id id,
                           vgl_point_3d<double> local_origin,
                           vgl_vector_3d<double> sub_block_dim,
                           vgl_vector_3d<unsigned> sub_block_num,
                           int init_level,
                           int max_level,
                           double max_mb,
-                          double p_init ) : id_(id), 
+                          double p_init ) : id_(id),
                                             local_origin_(local_origin),
                                             sub_block_dim_(sub_block_dim),
                                             sub_block_num_(sub_block_num),
@@ -44,18 +44,19 @@ class boxm2_block_metadata: public vbl_ref_count
                                             max_level_(max_level),
                                             max_mb_(max_mb),
                                             p_init_(p_init) {}
-                                            
-    boxm2_block_metadata( const boxm2_block_metadata& that) {
-      id_ = that.id_; 
-      local_origin_  = that.local_origin_;
-      sub_block_dim_ = that.sub_block_dim_;
-      sub_block_num_ = that.sub_block_num_;
-      init_level_    = that.init_level_; 
-      max_level_     = that.max_level_;
-      max_mb_        = that.max_mb_;
-      p_init_        = that.p_init_;
-    }
-  
+
+    boxm2_block_metadata( const boxm2_block_metadata& that)
+    : vbl_ref_count ()
+    , id_           (that.id_)
+    , local_origin_ (that.local_origin_)
+    , sub_block_dim_(that.sub_block_dim_)
+    , sub_block_num_(that.sub_block_num_)
+    , init_level_   (that.init_level_)
+    , max_level_    (that.max_level_)
+    , max_mb_       (that.max_mb_)
+    , p_init_       (that.p_init_)
+    {}
+
     boxm2_block_id          id_;
     vgl_point_3d<double>    local_origin_;
 
@@ -70,20 +71,20 @@ class boxm2_block_metadata: public vbl_ref_count
     int                     max_level_;    //each sub_blocks's max_level (default 4)
     double                  max_mb_;       //each total block mb
     double                  p_init_;       //initialize occupancy probs with this
-}; 
+};
 //: Smart_Pointer typedef for boxm2_block
 typedef vbl_smart_ptr<boxm2_block_metadata> boxm2_block_metadata_sptr;
 
 //: Binary write boxm_update_bit_scene_manager scene to stream
 void vsl_b_write(vsl_b_ostream& os, boxm2_block_metadata const& scene);
 void vsl_b_write(vsl_b_ostream& os, const boxm2_block_metadata* &p);
-void vsl_b_write(vsl_b_ostream& os, boxm2_block_metadata_sptr& sptr); 
+void vsl_b_write(vsl_b_ostream& os, boxm2_block_metadata_sptr& sptr);
 void vsl_b_write(vsl_b_ostream& os, boxm2_block_metadata_sptr const& sptr);
 
 //: Binary load boxm_update_bit_scene_manager scene from stream.
 void vsl_b_read(vsl_b_istream& is, boxm2_block_metadata &scene);
 void vsl_b_read(vsl_b_istream& is, boxm2_block_metadata* p);
-void vsl_b_read(vsl_b_istream& is, boxm2_block_metadata_sptr& sptr); 
+void vsl_b_read(vsl_b_istream& is, boxm2_block_metadata_sptr& sptr);
 void vsl_b_read(vsl_b_istream& is, boxm2_block_metadata_sptr const& sptr);
 
 #endif
