54599396d51566256515d5dcd49ad4c00aadf299
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-01-09 14:44:19 -0500

    avoid compiler warning (comparison signed/unsigned)
    SVN-Revision: 30673

--- contrib/brl/bseg/boxm2/exe/boxm2_render_expected_exe.cxx
+++ contrib/brl/bseg/boxm2/exe/boxm2_render_expected_exe.cxx
@@ -44,96 +44,93 @@ int main(int argc,  char** argv)
   vcl_ifstream ifs(camfile().c_str());
   vpgl_perspective_camera<double>* pcam =new vpgl_perspective_camera<double>;
   if (!ifs.is_open()) {
-      vcl_cerr << "Failed to open file " << camfile() << vcl_endl;
-      return -1;
+    vcl_cerr << "Failed to open file " << camfile() << '\n';
+    return -1;
   }
   else  {
-      ifs >> *pcam;
+    ifs >> *pcam;
   }
-  vpgl_camera_double_sptr cam = pcam; 
-  brdb_value_sptr brdb_cam = new brdb_value_t<vpgl_camera_double_sptr>(cam); 
-  
+  vpgl_camera_double_sptr cam = pcam;
+  brdb_value_sptr brdb_cam = new brdb_value_t<vpgl_camera_double_sptr>(cam);
+
   //create output image buffer
-  vil_image_view<unsigned int>* expimg = new vil_image_view<unsigned int>(ni(), nj()); 
+  vil_image_view<unsigned int>* expimg = new vil_image_view<unsigned int>(ni(), nj());
   expimg->fill(0);
-  vil_image_view_base_sptr expimg_sptr(expimg);// = new vil_image_view<unsigned int>(ni(), nj()); 
-  brdb_value_sptr brdb_expimg = new brdb_value_t<vil_image_view_base_sptr>(expimg_sptr); 
-  
+  vil_image_view_base_sptr expimg_sptr(expimg);// = new vil_image_view<unsigned int>(ni(), nj());
+  brdb_value_sptr brdb_expimg = new brdb_value_t<vil_image_view_base_sptr>(expimg_sptr);
+
   //create vis image buffer
-  vil_image_view<float>* vis_img = new vil_image_view<float>(ni(), nj()); 
-  vis_img->fill(1.0f); 
-  brdb_value_sptr brdb_vis = new brdb_value_t<vil_image_view_base_sptr>(vis_img); 
-  
+  vil_image_view<float>* vis_img = new vil_image_view<float>(ni(), nj());
+  vis_img->fill(1.0f);
+  brdb_value_sptr brdb_vis = new brdb_value_t<vil_image_view_base_sptr>(vis_img);
+
   //----------------------------------------------------------------------------
   //--- BEGIN BOXM2 API EXAMPLE ------------------------------------------------
   //----------------------------------------------------------------------------
   //start out rendering with the CPU
-  boxm2_scene_sptr scene = new boxm2_scene(scene_file()); 
-  brdb_value_sptr brdb_scene = new brdb_value_t<boxm2_scene_sptr>(scene); 
-  
+  boxm2_scene_sptr scene = new boxm2_scene(scene_file());
+  brdb_value_sptr brdb_scene = new brdb_value_t<boxm2_scene_sptr>(scene);
+
   //get relevant blocks
   boxm2_nn_cache cache( scene->data_path(), vgl_vector_3d<int>(2,2,1) );
-  
+
   //initialize gpu pro / manager
   boxm2_opencl_processor* gpu_pro = boxm2_opencl_processor::instance();
-  gpu_pro->set_scene(scene.ptr()); 
-  gpu_pro->set_cpu_cache(&cache); 
+  gpu_pro->set_scene(scene.ptr());
+  gpu_pro->set_cpu_cache(&cache);
   gpu_pro->init();
-  
+
   //set inputs
   vcl_vector<brdb_value_sptr> input;
-  input.push_back(brdb_scene);  
+  input.push_back(brdb_scene);
   input.push_back(brdb_cam);
-  input.push_back(brdb_expimg); 
-  input.push_back(brdb_vis); 
-  
+  input.push_back(brdb_expimg);
+  input.push_back(brdb_vis);
+
   //initoutput vector
-  vcl_vector<brdb_value_sptr> output; 
-  
+  vcl_vector<brdb_value_sptr> output;
+
   //initialize the GPU render process
-  boxm2_opencl_render_process gpu_render; 
-  gpu_render.init_kernel(&gpu_pro->context(), &gpu_pro->devices()[0]); 
+  boxm2_opencl_render_process gpu_render;
+  gpu_render.init_kernel(&gpu_pro->context(), &gpu_pro->devices()[0]);
 
   //run expected image like 10 times and get average
   int numTrials = 10;
-  vul_timer t; 
-  for(int i=0; i<numTrials; i++) {
-    
+  vul_timer t;
+  for (int i=0; i<numTrials; i++) {
     //execute process ////////////////////////////////////////////////////
     expimg->fill(0);
-    vis_img->fill(1.0f); 
+    vis_img->fill(1.0f);
     gpu_pro->run(&gpu_render, input, output);
-    gpu_pro->finish(); 
+    gpu_pro->finish();
     //////////////////////////////////////////////////////////////////////
-    
   }
-  float time = t.all() / (float) numTrials;  
+  float time = t.all() / (float) numTrials;
   vcl_cout<<"average render time: "<<time<<" ms"<<vcl_endl;
 
   //clean up
-  gpu_render.clean(); 
-  gpu_pro->finish(); 
+  gpu_render.clean();
+  gpu_pro->finish();
 
   //----------------------------------------------------------------------------
   //------- END API EXAMPLE ----------------------------------------------------
   //----------------------------------------------------------------------------
   //save to disk
-  vil_image_view<unsigned int>* expimg_view = static_cast<vil_image_view<unsigned int>* >(expimg_sptr.ptr()); 
+  vil_image_view<unsigned int>* expimg_view = static_cast<vil_image_view<unsigned int>* >(expimg_sptr.ptr());
   //unsigned int min_val, max_val;
-  //vil_math_value_range( *expimg_view, min_val, max_val); 
-  
-  vil_image_view<vxl_byte> byte_img(ni(), nj()); 
-  for(int i=0; i<ni(); i++) 
-    for(int j=0; j<nj(); j++) 
+  //vil_math_value_range( *expimg_view, min_val, max_val);
+
+  vil_image_view<vxl_byte> byte_img(ni(), nj());
+  for (unsigned int i=0; i<ni(); ++i)
+    for (unsigned int j=0; j<nj(); ++j)
       byte_img(i,j) =  static_cast<vxl_byte>( (*expimg_view)(i,j) );   //just grab the first byte (all foura r the same)
   vil_save( byte_img, img().c_str());
-  
-  vil_image_view<vxl_byte> vis_byte(ni(), nj()); 
-  for(int i=0; i<ni(); i++) 
-    for(int j=0; j<nj(); j++) 
+
+  vil_image_view<vxl_byte> vis_byte(ni(), nj());
+  for (unsigned int i=0; i<ni(); ++i)
+    for (unsigned int j=0; j<nj(); ++j)
       vis_byte(i,j) = (*vis_img)(i,j)*255;   //just grab the first byte (all foura r the same)
   vil_save( vis_byte, "vis_img.png");
-  
+
   return 0;
 }
-    
--- contrib/brl/bseg/boxm2/exe/boxm2_rep_scene.cxx
+++ contrib/brl/bseg/boxm2/exe/boxm2_rep_scene.cxx
@@ -19,7 +19,6 @@
 //brdb stuff
 #include <brdb/brdb_value.h>
 
-
 //  Converts a bit_scene to a boxm2 scene.
 //  NOTE: currently will only create one block with id 0,0,0
 
@@ -39,40 +38,40 @@ int main(int argc,  char** argv)
   typedef vnl_vector_fixed<uchar, 8>  uchar8;
   typedef vnl_vector_fixed<uchar, 16> uchar16;
   typedef vnl_vector_fixed<float, 16> float16;
-  
+
   //1. Create boxm2_scene from XML file
-  boxm2_scene_sptr scene = new boxm2_scene(scene_file()); 
-  if(scene->num_blocks() > 1) 
+  boxm2_scene_sptr scene = new boxm2_scene(scene_file());
+  if (scene->num_blocks() > 1)
     vcl_cout<<"Scene has more than one block, just repeating first one"<<vcl_endl;
 
   //2. create a block meta data table
-  boxm2_block_metadata data = scene->blocks()[boxm2_block_id(0,0,0)]; 
+  boxm2_block_metadata data = scene->blocks()[boxm2_block_id(0,0,0)];
   vcl_cout<<"Original Origin: "<<data.local_origin_<<vcl_endl;
-  
+
   //3.  rep the initial block, changing ID and origin
   vgl_vector_3d<double> block_side( data.sub_block_dim_.x() * data.sub_block_num_.x(),
                                     data.sub_block_dim_.y() * data.sub_block_num_.y(),
-                                    data.sub_block_dim_.z() * data.sub_block_num_.z()); 
+                                    data.sub_block_dim_.z() * data.sub_block_num_.z());
   vcl_cout<<"Block Side: "<<block_side<<vcl_endl;
-  vcl_vector<boxm2_block_metadata> blocks; 
-  for(int i=0; i<2; i++) {
-    for(int j=0; j<2; j++) {
-      boxm2_block_metadata data_copy(data); 
+  vcl_vector<boxm2_block_metadata> blocks;
+  for (int i=0; i<2; ++i) {
+    for (int j=0; j<2; ++j) {
+      boxm2_block_metadata data_copy(data);
       boxm2_block_id id(i,j,0);
-      data_copy.id_ = id; 
-      data_copy.local_origin_ = vgl_point_3d<double>(data_copy.local_origin_.x() + i*block_side.x(), 
-                                                     data_copy.local_origin_.y() + j*block_side.y(), 
-                                                     data_copy.local_origin_.z()); 
+      data_copy.id_ = id;
+      data_copy.local_origin_ = vgl_point_3d<double>(data_copy.local_origin_.x() + i*block_side.x(),
+                                                     data_copy.local_origin_.y() + j*block_side.y(),
+                                                     data_copy.local_origin_.z());
       vcl_cout<<"id: "<<id<<"  origin: "<<data_copy.local_origin_<<vcl_endl;
-      blocks.push_back(data_copy); 
+      blocks.push_back(data_copy);
     }
   }
-  
+
   //2. create empty boxm2_scene
   boxm2_scene new_scene;
-  
+
   //3. set scene metadata
-  for(int i=0; i<blocks.size(); i++)
+  for (unsigned int i=0; i<blocks.size(); ++i)
   new_scene.add_block_metadata(blocks[i]);
   new_scene.set_local_origin(scene->local_origin());
   new_scene.set_rpc_origin(scene->local_origin());
@@ -80,38 +79,38 @@ int main(int argc,  char** argv)
   new_scene.set_xml_path(new_dir() + "/scene.xml");
   new_scene.set_data_path(new_dir());
   new_scene.save_scene();
-  
-  //4. GET SINGLE BLOCK 
-  boxm2_block_id id(0,0,0); 
+
+  //4. GET SINGLE BLOCK
+  boxm2_block_id id(0,0,0);
   boxm2_dumb_cache dcache(scene->data_path());
-  boxm2_block_sptr blk      = dcache.get_block(id); 
-  boxm2_data_base_sptr alph = dcache.get_data<BOXM2_ALPHA>(id); 
-  boxm2_data_base_sptr mog  = dcache.get_data<BOXM2_MOG3_GREY>(id); 
-  
+  boxm2_block_sptr blk      = dcache.get_block(id);
+  boxm2_data_base_sptr alph = dcache.get_data<BOXM2_ALPHA>(id);
+  boxm2_data_base_sptr mog  = dcache.get_data<BOXM2_MOG3_GREY>(id);
+
   //5. copy and save it
-  for(int i=0; i<2; i++) {
-    for(int j=0; j<2; j++) {
-      char* blkBuffer = new char[blk->byte_count()]; 
-      vcl_memcpy(blkBuffer, blk->buffer(), blk->byte_count()); 
-      boxm2_block_id id(i,j,0); 
-      
+  for (int i=0; i<2; ++i) {
+    for (int j=0; j<2; ++j) {
+      char* blkBuffer = new char[blk->byte_count()];
+      vcl_memcpy(blkBuffer, blk->buffer(), blk->byte_count());
+      boxm2_block_id id(i,j,0);
+
       //save voxel block
-      boxm2_block block(id, blkBuffer); 
+      boxm2_block block(id, blkBuffer);
       boxm2_sio_mgr::save_block(new_scene.data_path(), &block);
-      
+
       //save alpha buffer
-      char* alphaBuffer = new char[alph->buffer_length()]; 
-      vcl_memcpy(alphaBuffer, alph->data_buffer(), alph->buffer_length()); 
+      char* alphaBuffer = new char[alph->buffer_length()];
+      vcl_memcpy(alphaBuffer, alph->data_buffer(), alph->buffer_length());
       boxm2_data<BOXM2_ALPHA> alpha_data(alphaBuffer, alph->buffer_length(), id);
       boxm2_sio_mgr::save_block_data<BOXM2_ALPHA>(new_scene.data_path(), id, &alpha_data);
-      
+
       //save mog buffer
-      char* mogBuffer = new char[mog->buffer_length()]; 
-      vcl_memcpy(mogBuffer, mog->data_buffer(), mog->buffer_length()); 
+      char* mogBuffer = new char[mog->buffer_length()];
+      vcl_memcpy(mogBuffer, mog->data_buffer(), mog->buffer_length());
       boxm2_data<BOXM2_MOG3_GREY> mog_data(mogBuffer, mog->buffer_length(), id);
       boxm2_sio_mgr::save_block_data<BOXM2_MOG3_GREY>(new_scene.data_path(), id, &mog_data);
     }
   }
-  
+
   return 0;
 }
