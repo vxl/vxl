fa1174f0d46ba38edb2ae1ff335b26a9d2ed0a37
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-01-09 11:44:49 -0500

    no need for Doxygen comments here
    SVN-Revision: 30666

--- contrib/brl/bseg/boxm2/ocl/boxm2_opencl_processor.cxx
+++ contrib/brl/bseg/boxm2/ocl/boxm2_opencl_processor.cxx
@@ -3,7 +3,6 @@
 
 //boxm2 data structures
 
-
 //cameras/images
 #include <vpgl/vpgl_perspective_camera.h>
 #include <vil/vil_image_view_base.h>
@@ -17,7 +16,7 @@
 bool boxm2_opencl_processor::init()
 {
   vcl_cout<<"PROCESSOR INIT CALLED?"<<vcl_endl;
-  
+
   // set up 2 command queues
   int status;
   for (int i=0; i<NUM_QUEUES; ++i) {
@@ -28,31 +27,30 @@ bool boxm2_opencl_processor::init()
     if (!check_val(status,CL_SUCCESS,"Failed in command queue creation" + error_to_string(status)))
       return false;
   }
-  
+
   //set up our cache
-  gpu_cache_ = new boxm2_opencl_cache(cpu_cache_, 
-                                      &(this->context()), 
-                                      &queues_[0], 
-                                      scene_); 
+  gpu_cache_ = new boxm2_opencl_cache(cpu_cache_,
+                                      &(this->context()),
+                                      &queues_[0],
+                                      scene_);
   return true;
 }
 
 bool boxm2_opencl_processor::run(boxm2_process * process, vcl_vector<brdb_value_sptr> & input, vcl_vector<brdb_value_sptr> & output)
 {
   boxm2_opencl_process_base* pro = (boxm2_opencl_process_base*) process;
-  pro->set_gpu_cache(gpu_cache_); 
+  pro->set_gpu_cache(gpu_cache_);
   pro->set_command_queue(&queues_[0]);
   pro->execute(input, output);
-  
-  vcl_cout<<" Time: total ("<<pro->total_time()<<"ms), "
-          <<" transfer("<<pro->transfer_time()<<"ms), "
-          <<" gpu("<<pro->gpu_time()<<"ms)"<<vcl_endl;
+
+  vcl_cout<<" Time: total ("<<pro->total_time()<<" ms), "
+          <<" transfer("<<pro->transfer_time()<<" ms), "
+          <<" gpu("<<pro->gpu_time()<<" ms)"<<vcl_endl;
 
   return true;
 }
 
-//: clean up allocated buffers
 bool boxm2_opencl_processor::finish()
 {
-   return true;
+  return true;
 }
