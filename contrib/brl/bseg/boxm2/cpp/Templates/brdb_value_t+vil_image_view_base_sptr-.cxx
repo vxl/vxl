dee0a0026735c9610994a2a14fed1aee3261f547
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-01-10 02:46:12 -0500

    SVN-Revision: 30687

---
+++ contrib/brl/bseg/boxm2/cpp/Templates/brdb_value_t+vil_image_view_base_sptr-.cxx
@@ -0,0 +1,9 @@
+#include <vil/vil_image_view_base.h>
+#include <vil/io/vil_io_image_view.h>
+#include <brdb/brdb_value.txx>
+
+//stubs for binary IO, since we don't need to use it
+void vsl_b_read(vsl_b_istream&, vil_image_view_base_sptr&) {}
+void vsl_b_write(vsl_b_ostream&, vil_image_view_base_sptr const&) {}
+
+BRDB_VALUE_INSTANTIATE(vil_image_view_base_sptr,"vil_image_view_base_sptr");
