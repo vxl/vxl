3cdebda6b9e4f3d2fa586fe69ba84af750388048
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-01-10 16:21:07 -0500

    brief Doxygen comment should be on a single line
    SVN-Revision: 30703

--- contrib/brl/bseg/boxm2/boxm2_block.cxx
+++ contrib/brl/bseg/boxm2/boxm2_block.cxx
@@ -1,7 +1,7 @@
-#include <boxm2/boxm2_block.h>
-#include <boxm2/boxm2_util.h>
+#include "boxm2_block.h"
 //:
 // \file
+#include <boxm2/boxm2_util.h>
 
 boxm2_block::boxm2_block(boxm2_block_id id, char* buff)
 {
@@ -13,7 +13,7 @@ boxm2_block::boxm2_block(boxm2_block_id id, char* buff)
 boxm2_block::boxm2_block(boxm2_block_metadata data)
 {
   block_id_ = data.id_;
-  this->init_empty_block(data); 
+  this->init_empty_block(data);
 }
 
 
@@ -125,7 +125,7 @@ bool boxm2_block::b_write(char* buff)
 
 
 //: initializes empty scene given
-// This method uses the max_mb parameter to determine how many data cells to 
+// This method uses the max_mb parameter to determine how many data cells to
 // allocate.  MAX_MB is assumed to include blocks, alpha, mog3, num_obs and 16 byte aux data
 bool boxm2_block::init_empty_block(boxm2_block_metadata data)
 {
@@ -159,20 +159,20 @@ bool boxm2_block::init_empty_block(boxm2_block_metadata data)
             <<"**************************************************\n";
     return false;
   }
-  
+
   vcl_cout<<"OCL Scene buffer shape: ["
           <<num_buffers<<" buffers by "
           <<BUFF_LENGTH<<" cells ("
           <<blocks_per_buffer<<" trees per buffer)]. "
           <<"[total tree:"<<num_buffers*BUFF_LENGTH<<']'<<vcl_endl;
-  
+
   //now construct a byte stream, and read in with b_read
-  byte_count_ = calc_byte_count(num_buffers, blocks_per_buffer, total_blocks);  
-  init_level_ = data.init_level_; 
-  max_level_  = data.max_level_; 
+  byte_count_ = calc_byte_count(num_buffers, blocks_per_buffer, total_blocks);
+  init_level_ = data.init_level_;
+  max_level_  = data.max_level_;
   max_mb_     = data.max_mb_;
-  buffer_ = new char[byte_count_]; 
-  
+  buffer_ = new char[byte_count_];
+
   //get member variable metadata straight, then write to the buffer
   long bytes_read = 0;
   bytes_read += sizeof(byte_count_);   //0. first 8 bytes denote size
@@ -182,11 +182,11 @@ bool boxm2_block::init_empty_block(boxm2_block_metadata data)
   double dims[4]; int nums[4];
   bytes_read += sizeof(dims);          //2. read in sub block dimension, sub block num
   bytes_read += sizeof(nums);
-  sub_block_dim_ = data.sub_block_dim_; 
+  sub_block_dim_ = data.sub_block_dim_;
   sub_block_num_ = data.sub_block_num_;
   bytes_read += sizeof(num_buffers);        //3.  read number of buffers
   bytes_read += sizeof(blocks_per_buffer);  //3.a read length of tree buffers
-  
+
   //4. setup big arrays (3d block of trees)
   uchar16* treesBuff = (uchar16*) (buffer_+bytes_read);
   trees_     = boxm2_array_3d<uchar16>( sub_block_num_.x(),
@@ -203,58 +203,60 @@ bool boxm2_block::init_empty_block(boxm2_block_metadata data)
   //6. 1d aray of trees_in_buffers
   ushort* treesCountBuff = (ushort*) (buffer_+bytes_read);
   trees_in_buffers_ = boxm2_array_1d<ushort>(num_buffers, treesCountBuff);
-  for(int i=0; i<trees_in_buffers_.size(); ++i) trees_in_buffers_[i] = 0; 
+  for (int i=0; i<trees_in_buffers_.size(); ++i)
+    trees_in_buffers_[i] = 0;
   bytes_read += sizeof(ushort) * num_buffers;
 
   //7. 1d array of mem pointers
   ushort2* memPtrsBuff = (ushort2*) (buffer_+bytes_read);
   mem_ptrs_ = boxm2_array_1d<ushort2>(num_buffers, memPtrsBuff);
-  for(int i=0; i<mem_ptrs_.size(); ++i) {
-    mem_ptrs_[i][0] = 0; 
-    mem_ptrs_[i][1] = 0; 
+  for (int i=0; i<mem_ptrs_.size(); ++i) {
+    mem_ptrs_[i][0] = 0;
+    mem_ptrs_[i][1] = 0;
   }
   bytes_read += sizeof(ushort2) * num_buffers;
-  
+
   //--- Now initialize blocks and their pointers randomly ---------------------
   //6.a create a random 'iterator'
   int* randIndex = new int[trees_.size()];
   for (unsigned int i=0; i<trees_.size(); ++i) randIndex[i] = i;
   boxm2_util::random_permutation(randIndex, trees_.size());
-  int tree_index = 0; 
-  boxm2_array_3d<uchar16>::iterator iter; 
+  int tree_index = 0;
+  boxm2_array_3d<uchar16>::iterator iter;
   for (iter = trees_.begin(); iter != trees_.end(); ++iter, ++tree_index)
   {
     //get a random index, convert it to Buffer Index/Offset pair
     int r_index  = randIndex[tree_index];
-    int b_index  = r_index / blocks_per_buffer; 
-    int b_offset = r_index % blocks_per_buffer; 
-    
+    int b_index  = r_index / blocks_per_buffer;
+    int b_offset = r_index % blocks_per_buffer;
+
     //store buffer offset in bits [10,11]
     uchar16 treeBlk( (unsigned char) 0 );
-    unsigned char off_hi = (unsigned char) (b_offset >> 8); 
-    unsigned char off_lo = (unsigned char) (b_offset & 255); 
+    unsigned char off_hi = (unsigned char) (b_offset >> 8);
+    unsigned char off_lo = (unsigned char) (b_offset & 255);
     treeBlk[10] = off_hi; treeBlk[11] = off_lo;
-    
+
     //store buffer index in bits [12,13]
-    unsigned char b_hi = (unsigned char) (b_index >> 8); 
-    unsigned char b_lo = (unsigned char) (b_index & 255); 
-    treeBlk[12] = b_hi; treeBlk[13] = b_lo; 
-    for(int i=0; i<16; i++) (*iter)[i] = treeBlk[i]; 
+    unsigned char b_hi = (unsigned char) (b_index >> 8);
+    unsigned char b_lo = (unsigned char) (b_index & 255);
+    treeBlk[12] = b_hi; treeBlk[13] = b_lo;
+    for (int i=0; i<16; i++)
+      (*iter)[i] = treeBlk[i];
 
     //make sure mem_ptrs_ and blocks in buffers add up
-    mem_ptrs_[b_index][1]++; 
-    trees_in_buffers_[b_index]++; 
-    
+    mem_ptrs_[b_index][1]++;
+    trees_in_buffers_[b_index]++;
+
     //make sure block pointers points back to the right index
-    tree_ptrs_[b_index][b_offset] = tree_index; 
+    tree_ptrs_[b_index][b_offset] = tree_index;
   }
   delete [] randIndex;
   return true;
 }
 
 
-//: Given number of buffers, number of trees in each buffer, and number of
-// total trees (x*y*z number), returns size of byte stream
+//: Given number of buffers, number of trees in each buffer, and number of total trees (x*y*z number)
+// \return size of byte stream
 long boxm2_block::calc_byte_count(int num_buffers, int trees_per_buffer, int num_trees)
 {
   long toReturn = num_trees * sizeof(uchar16) +                     //3d block pointers
--- contrib/brl/bseg/boxm2/io/boxm2_sio_mgr.h
+++ contrib/brl/bseg/boxm2/io/boxm2_sio_mgr.h
@@ -2,6 +2,8 @@
 #define boxm2_sio_mgr_h_
 //:
 // \file
+// \brief Loads blocks and data from ID's and data_types with blocking.
+//  If file is not available, will return null.
 #include <boxm2/boxm2_block.h>
 #include <boxm2/basic/boxm2_block_id.h>
 #include <boxm2/boxm2_data_traits.h>
@@ -9,41 +11,36 @@
 #include <vul/vul_file.h>
 #include <vcl_iostream.h>
 
-//: Loads blocks and data from ID's and data_types with blocking.  If file is 
-//  not available, will return null. 
-
 //: disk level storage class.
 //  handles all of the synchronous IO read and write requests
 class boxm2_sio_mgr
 {
   public:
-
     //: loads block from disk
-    static boxm2_block*   load_block(vcl_string dir, boxm2_block_id block_id); 
-    
+    static boxm2_block*   load_block(vcl_string dir, boxm2_block_id block_id);
+
     //: saves block to disk
-    static void           save_block(vcl_string dir, boxm2_block* block );
+    static void           save_block(vcl_string dir, boxm2_block* block);
 
     //: load data from disk
-    template <boxm2_data_type data_type> 
-    static boxm2_data<data_type> *  load_block_data(vcl_string dir, boxm2_block_id block_id); 
-    
-    //: load data generically
-    static boxm2_data_base*  load_block_data_generic(vcl_string dir, boxm2_block_id id, vcl_string data_type); 
-    
-    //: saves data to disk
-    template <boxm2_data_type data_type> 
-    static void save_block_data(vcl_string dir, boxm2_block_id block_id, boxm2_data<data_type> * block_data );
+    template <boxm2_data_type data_type>
+    static boxm2_data<data_type> *  load_block_data(vcl_string dir, boxm2_block_id block_id);
 
+    //: load data generically
+    static boxm2_data_base*  load_block_data_generic(vcl_string dir, boxm2_block_id id, vcl_string data_type);
+
+    //: saves data to disk
+    template <boxm2_data_type data_type>
+    static void save_block_data(vcl_string dir, boxm2_block_id block_id, boxm2_data<data_type> * block_data );
 };
 
-template <boxm2_data_type data_type> 
+template <boxm2_data_type data_type>
 boxm2_data<data_type> *  boxm2_sio_mgr::load_block_data(vcl_string dir, boxm2_block_id block_id)
 {
-  return (boxm2_data<data_type>*) 
-    boxm2_sio_mgr::load_block_data_generic(dir, block_id, boxm2_data_traits<data_type>::prefix()); 
-  
-/*
+  return (boxm2_data<data_type>*)
+    boxm2_sio_mgr::load_block_data_generic(dir, block_id, boxm2_data_traits<data_type>::prefix());
+
+#if 0
     // file name
     vcl_string filename = dir + boxm2_data_traits<data_type>::prefix() + "_" + block_id.to_string() + ".bin";
 
@@ -51,7 +48,7 @@ boxm2_data<data_type> *  boxm2_sio_mgr::load_block_data(vcl_string dir, boxm2_bl
     unsigned long numBytes=vul_file::size(filename);
 
     //Read bytes into stream
-    char * bytes = new char[numBytes]; 
+    char * bytes = new char[numBytes];
     vcl_ifstream myFile (filename.c_str(), vcl_ios::in | vcl_ios::binary);
     myFile.read(bytes, numBytes);
     if (!myFile) {
@@ -60,19 +57,19 @@ boxm2_data<data_type> *  boxm2_sio_mgr::load_block_data(vcl_string dir, boxm2_bl
     }
 
     //instantiate new block
-    return new boxm2_data<data_type>(bytes,numBytes,block_id); 
-*/
+    return new boxm2_data<data_type>(bytes,numBytes,block_id);
+#endif // 0
 }
 
 //: saves block to disk
-template <boxm2_data_type data_type> 
+template <boxm2_data_type data_type>
 void boxm2_sio_mgr::save_block_data(vcl_string dir, boxm2_block_id block_id, boxm2_data<data_type> * block_data )
 {
     vcl_string filename = dir + boxm2_data_traits<data_type>::prefix() + "_" + block_id.to_string() + ".bin";
-  
-    char * bytes = block_data->data_buffer(); 
+
+    char * bytes = block_data->data_buffer();
     vcl_ofstream myFile (filename.c_str(), vcl_ios::out | vcl_ios::binary);
-    myFile.write(bytes, block_data->buffer_length());  
+    myFile.write(bytes, block_data->buffer_length());
     myFile.close();
     return;
 }
