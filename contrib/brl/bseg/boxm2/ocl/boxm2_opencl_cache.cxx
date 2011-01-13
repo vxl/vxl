d23f967201c64a4f316ca7e744021663eb175eb8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-01-13 02:50:35 -0500

    cannot delete a "void*"
    SVN-Revision: 30740

--- contrib/brl/bseg/boxm2/ocl/boxm2_opencl_cache.cxx
+++ contrib/brl/bseg/boxm2/ocl/boxm2_opencl_cache.cxx
@@ -4,15 +4,15 @@
 
 
 //: constructor
-boxm2_opencl_cache::boxm2_opencl_cache(boxm2_cache* cpu_cache, 
-                                       cl_context* context, 
+boxm2_opencl_cache::boxm2_opencl_cache(boxm2_cache* cpu_cache,
+                                       cl_context* context,
                                        cl_command_queue* queue,
                                        boxm2_scene* scene)
 {
   cpu_cache_ = cpu_cache;
-  context_ = context; 
-  queue_ = queue; 
-  loaded_ = boxm2_block_id(-1, -1, -1); 
+  context_ = context;
+  queue_ = queue;
+  loaded_ = boxm2_block_id(-1, -1, -1);
   cached_block_ = 0;
   block_info_ = 0;
   scene_ = scene;
@@ -41,22 +41,22 @@ bocl_mem* boxm2_opencl_cache::get_block(boxm2_block_id id)
     return cached_block_;
 
   //clean up...
-  if(cached_block_) {
-    delete cached_block_; 
-    cached_block_ = 0;
-  }
+  delete cached_block_;
+  cached_block_ = 0;
 
   //otherwise load it from disk with blocking
-  loaded_ = id; 
+  loaded_ = id;
   boxm2_block* loaded = cpu_cache_->get_block(id);
   typedef vnl_vector_fixed<unsigned char, 16> uchar16;
-  boxm2_array_3d<uchar16>& trees = loaded->trees(); 
+  boxm2_array_3d<uchar16>& trees = loaded->trees();
   cached_block_ = new bocl_mem(*context_, trees.data_block(), trees.size()*sizeof(uchar16), "3d trees buffer");
   cached_block_->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR );
 
   //store the metadata
-  if(block_info_) {
-     if( block_info_->cpu_buffer() ) delete block_info_->cpu_buffer(); 
+  if (block_info_) {
+#if 0 // cannot delete a void* !!!
+     if ( block_info_->cpu_buffer() ) delete block_info_->cpu_buffer();
+#endif
      delete block_info_;
   }
   boxm2_scene_info* info_buffer = scene_->get_blk_metadata(loaded_);
