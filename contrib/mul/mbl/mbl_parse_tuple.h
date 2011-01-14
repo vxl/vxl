e6489da76ebd08230caddaaaaa2323ba3a263aa6
Ian Scott <scottim@users.sourceforge.net>
2011-01-14 04:58:38 -0500

    added support for 5- and 6-tuples
    SVN-Revision: 30754

--- contrib/mul/mbl/mbl_parse_tuple.h
+++ contrib/mul/mbl/mbl_parse_tuple.h
@@ -115,7 +115,7 @@ inline void mbl_parse_tuple(vcl_istream &afs, T& a, U& b, V& c)
 // int b;
 // unsigned c;
 // char d;
-// mbl_parse_tuple(ss, a, b, c)
+// mbl_parse_tuple(ss, a, b, c, d)
 // \endverbatim
 template <class T, class U, class V, class W>
 inline void mbl_parse_tuple(vcl_istream &afs, T& a, U& b, V& c, W& d)
@@ -147,4 +147,101 @@ inline void mbl_parse_tuple(vcl_istream &afs, T& a, U& b, V& c, W& d)
   }
 }
 
+
+//: Read a 5-tuple of PODs from a config file.
+// This function will read through a stream, and store the text found to a string.
+// The function reads 5 elements. If there was an openning brace it will also consume the closing brace.
+// Other conditions will cause an exception to be thrown, and the stream's fail bit to be set
+//
+// \throws mbl_exception_parse_error if unrecoverable parse error.
+//
+// \verbatim
+// Example:
+// vcl_istringstream ss("{ 1.0 -5 4 k l }");
+// float a;
+// int b;
+// unsigned c;
+// char d, e;
+// mbl_parse_tuple(ss, a, b, c, d, e)
+// \endverbatim
+template <class T, class U, class V, class W, class X>
+inline void mbl_parse_tuple(vcl_istream &afs, T& a, U& b, V& c, W& d, X& e)
+{
+  if (!afs) return;
+  char brace1, brace2;
+  afs >> vcl_ws >> brace1;
+  if (afs.eof())
+    throw mbl_exception_parse_error("mbl_parse_tuple failed unexpected eof");
+  if ( brace1 == '{')
+  {
+    afs >> vcl_ws >> a >> vcl_ws >> b >> vcl_ws >> c >> vcl_ws >> d >> vcl_ws >> e >> vcl_ws >> brace2;
+    if (!afs)
+      throw mbl_exception_parse_error("mbl_parse_tuple failed with stream error");
+    if (brace2 != '}')
+    {
+      afs.putback(brace2);
+      afs.clear(vcl_ios::failbit); // Set a recoverable IO error on stream
+
+      throw mbl_exception_parse_error("mbl_parse_tuple failed to find closing brace");
+    }
+  }
+  else
+  {
+    afs.putback(brace1);
+    afs >> vcl_ws >> a >> vcl_ws >> b >> vcl_ws >> c >> vcl_ws >> d >> vcl_ws >> e;
+    if (!afs)
+      throw mbl_exception_parse_error("mbl_parse_tuple failed with stream error");
+  }
+}
+
+
+
+//: Read a 6-tuple of PODs from a config file.
+// This function will read through a stream, and store the text found to a string.
+// The function reads 6 elements. If there was an openning brace it will also consume the closing brace.
+// Other conditions will cause an exception to be thrown, and the stream's fail bit to be set
+//
+// \throws mbl_exception_parse_error if unrecoverable parse error.
+//
+// \verbatim
+// Example:
+// vcl_istringstream ss("{ 1.0 -3.4 -5 4 k l }");
+// float a;
+// double b;
+// int c;
+// unsigned d;
+// char e, f;
+// mbl_parse_tuple(ss, a, b, c, d, e, f)
+// \endverbatim
+template <class T, class U, class V, class W, class X, class Y>
+inline void mbl_parse_tuple(vcl_istream &afs, T& a, U& b, V& c, W& d, X& e, Y& f)
+{
+  if (!afs) return;
+  char brace1, brace2;
+  afs >> vcl_ws >> brace1;
+  if (afs.eof())
+    throw mbl_exception_parse_error("mbl_parse_tuple failed unexpected eof");
+  if ( brace1 == '{')
+  {
+    afs >> vcl_ws >> a >> vcl_ws >> b >> vcl_ws >> c >> vcl_ws >> d >> vcl_ws >>
+      e >> vcl_ws >> f >> vcl_ws >> brace2;
+    if (!afs)
+      throw mbl_exception_parse_error("mbl_parse_tuple failed with stream error");
+    if (brace2 != '}')
+    {
+      afs.putback(brace2);
+      afs.clear(vcl_ios::failbit); // Set a recoverable IO error on stream
+
+      throw mbl_exception_parse_error("mbl_parse_tuple failed to find closing brace");
+    }
+  }
+  else
+  {
+    afs.putback(brace1);
+    afs >> vcl_ws >> a >> vcl_ws >> b >> vcl_ws >> c >> vcl_ws >> d >>
+      vcl_ws >> e >> vcl_ws >> f;
+    if (!afs)
+      throw mbl_exception_parse_error("mbl_parse_tuple failed with stream error");
+  }
+}
 #endif // mbl_parse_tuple_h_
--- contrib/mul/mbl/tests/test_parse_tuple.cxx
+++ contrib/mul/mbl/tests/test_parse_tuple.cxx
@@ -20,7 +20,7 @@ void test_parse_tuple()
     mbl_parse_tuple(ss, a, b);
     vcl_cout << "Parsed: " << a << ' ' << b << vcl_endl;
 
-    TEST("Case 1: \"{  1 1.0 }\"", a==1 && b==1.0, true);
+    TEST("\"{  1 1.0 }\"", a==1 && b==1.0, true);
   }
 
   {
@@ -31,7 +31,7 @@ void test_parse_tuple()
     mbl_parse_tuple(ss, a, b);
     vcl_cout << "Parsed: " << a << ' ' << b << vcl_endl;
 
-    TEST("Case 2: \"  1 1.0\"", a==1 && b==1.0, true);
+    TEST("\"  1 1.0\"", a==1 && b==1.0, true);
   }
 
   {
@@ -54,7 +54,7 @@ void test_parse_tuple()
     mbl_parse_tuple(ss, a, b, c );
     vcl_cout << "Parsed: " << a << ' ' << b << ' ' << c << vcl_endl;
 
-    TEST("Case 4: \"{ 1 1.0 a }\"", a==1 && b==1.0 && c=='a', true);
+    TEST("\"{ 1 1.0 a }\"", a==1 && b==1.0 && c=='a', true);
   }
 
   {
@@ -78,7 +78,7 @@ void test_parse_tuple()
     mbl_parse_tuple(ss, a, b, c );
     vcl_cout << "Parsed: " << a << ' ' << b << ' ' << c << vcl_endl;
 
-    TEST("Case 6: \"1 1.0 a\"", a==1 && b==1.0 && c=='a', true);
+    TEST("\"1 1.0 a\"", a==1 && b==1.0 && c=='a', true);
   }
 
   {
@@ -104,7 +104,7 @@ void test_parse_tuple()
     mbl_parse_tuple(ss, a, b, c, d );
     vcl_cout << "Parsed: " << a << ' ' << b << ' ' << c << ' ' << d << vcl_endl;
 
-    TEST("Case 8: \"{ 1 1.0 a -4 }\"", a==1 && b==1.0 && c=='a' && d==-4, true);
+    TEST("\"{ 1 1.0 a -4}\"", a==1 && b==1.0 && c=='a' && d==-4, true);
   }
 
   {
@@ -121,7 +121,7 @@ void test_parse_tuple()
     {
       caught = true;
     }
-    TEST("Case 9: \"{}\"", ss.fail() && caught, true);
+    TEST("\"{}\"", ss.fail() && caught, true);
   }
 
   {
@@ -138,7 +138,7 @@ void test_parse_tuple()
     {
       caught = true;
     }
-    TEST("Case 10: \"\"", ss.fail() && caught, true);
+    TEST("\"\"", ss.fail() && caught, true);
   }
 
   {
@@ -172,7 +172,7 @@ void test_parse_tuple()
     {
       caught = true;
     }
-    TEST("Case 12: \"{ 1 }\"", ss.fail() && caught, true);
+    TEST("\"{ 1 }\"", ss.fail() && caught, true);
   }
   {
     vcl_cout << "\nCase 13\n";
@@ -188,7 +188,35 @@ void test_parse_tuple()
     {
       caught = true;
     }
-    TEST("Case 13: \"1\"", ss.fail() && caught, true);
+    TEST("\"1\"", ss.fail() && caught, true);
+  }
+  {
+    vcl_cout << "\nCase 14\n";
+    vcl_istringstream ss("{ 1 1.0 a -4 -2e20 }");
+    unsigned a;
+    double b;
+    char c;
+    short d;
+    float e;
+    mbl_parse_tuple(ss, a, b, c, d, e );
+    vcl_cout << "Parsed: " << a << ' ' << b << ' ' << c << ' ' << d << ' ' << e << vcl_endl;
+
+    TEST("\"{ 1 1.0 a -4 -2e20 }\"", a==1 && b==1.0 && c=='a' && d==-4 && e==-2e20f, true);
+  }
+  {
+    vcl_cout << "\nCase 15\n";
+    vcl_istringstream ss("{ 1 1.0 a -4 -2e20 $ }");
+    unsigned a;
+    double b;
+    char c;
+    short d;
+    float e;
+    char f;
+    mbl_parse_tuple(ss, a, b, c, d, e, f );
+    vcl_cout << "Parsed: " << a << ' ' << b << ' ' << c << ' ' << d << ' ' << e << ' ' << f << vcl_endl;
+
+    TEST("\"{ 1 1.0 a -4 -2e20 $ }\"", a==1 && b==1.0 && c=='a' && d==-4
+      && e==-2e20f && f=='$', true);
   }
 
   vcl_cout << "\n\n";
