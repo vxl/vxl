59e4d2426627e973e17f05c7cec778072ed9f7d1
Andrew Miller <andrew-miller@users.sourceforge.net>
2011-01-17 12:23:34 -0500

    fixed paths in render expected and update exe
    SVN-Revision: 30826

--- contrib/brl/bseg/boxm2/ocl/exe/boxm2_render_expected_exe.cxx
+++ contrib/brl/bseg/boxm2/ocl/exe/boxm2_render_expected_exe.cxx
@@ -19,8 +19,8 @@
 #include <boxm2/boxm2_scene.h>
 #include <boxm2/basic/boxm2_block_id.h>
 #include <boxm2/ocl/boxm2_opencl_processor.h>
-#include <boxm2/ocl/boxm2_opencl_render_process.h>
-#include <boxm2/ocl/boxm2_opencl_render_depth_process.h>
+#include <boxm2/ocl/pro/boxm2_opencl_render_process.h>
+#include <boxm2/ocl/pro/boxm2_opencl_render_depth_process.h>
 #include <boxm2/cpp/boxm2_cpp_processor.h>
 #include <boxm2/cpp/pro/boxm2_cpp_render_process.h>
 
