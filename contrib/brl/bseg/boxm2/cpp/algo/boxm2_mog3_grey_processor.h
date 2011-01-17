9efb285f1f937056bd1cb82b4893e7ed77fbf4c8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-01-17 12:15:06 -0500

    corrected the "This is" line
    SVN-Revision: 30822

--- contrib/brl/bseg/boxm2/cpp/algo/boxm2_mog3_grey_processor.h
+++ contrib/brl/bseg/boxm2/cpp/algo/boxm2_mog3_grey_processor.h
@@ -1,4 +1,4 @@
-// This is brl/bseg/boxm2/cpp/boxm2_mog3_grey_processor.h
+// This is brl/bseg/boxm2/cpp/algo/boxm2_mog3_grey_processor.h
 #ifndef boxm2_mog3_grey_processor_h_
 #define boxm2_mog3_grey_processor_h_
 //:
@@ -21,19 +21,17 @@ class  boxm2_mog3_grey_processor
      static float prob_density( const vnl_vector_fixed<unsigned char, 8> & mog3, float x);
      static float gauss_prob_density(float x, float mu, float sigma);
      static void update_gauss_mixture_3(vnl_vector_fixed<unsigned char, 8> & mog3,
-                                        vnl_vector_fixed<float, 4> & nobs, 
+                                        vnl_vector_fixed<float, 4> & nobs,
                                         float x, float w, float init_sigma,float min_sigma);
      static void sort_mix_3(float & mu0, float& sigma0, float& w0, float& Nobs0,
                             float & mu1, float& sigma1, float& w1, float& Nobs1,
                             float & mu2, float& sigma2, float& w2, float& Nobs2);
      static void update_gauss(float & x, float & rho, float & mu, float &  sigma,float min_sigma);
-     static void insert_gauss_3(float x, float init_weight, float init_sigma, int & match, 
+     static void insert_gauss_3(float x, float init_weight, float init_sigma, int & match,
                                 float& mu0, float& sigma0, float& w0, float& Nobs0,
                                 float& mu1, float& sigma1, float& w1, float& Nobs1,
                                 float& mu2, float& sigma2, float& w2, float& Nobs2);
-         static float clamp(float x, float a, float b){    return x < a ? a : (x > b ? b : x);}
-
-
+     static float clamp(float x, float a, float b) { return x < a ? a : (x > b ? b : x); }
 };
 
 #endif // boxm2_mog3_grey_processor_h_
