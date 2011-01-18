9f08f1d09f51798a1ac43856ec3db7e01cc83e00
Octavian Biris <octibiris@users.sourceforge.net>
2011-01-17 20:48:10 -0500

    SVN-Revision: 30830

--- config/cmake/Modules/NewCMake/FindECW.cmake
+++ config/cmake/Modules/NewCMake/FindECW.cmake
@@ -8,7 +8,8 @@
 # The original sponsorring website of this library appears to have vanished,
 # but there are still traces at http://www.gdal.org/frmt_ecw.html and a
 # distribution at https://svn.zib.de/lenne3d/lib/libecw/current - IMS  7-Dec-2009.
-
+IF( VXL_FORCE_V3P_J2K )
+ELSE( VXL_FORCE_V3P_J2K )
 SET( ECW_FOUND "NO" )
 
 FIND_PATH( ECW_INCLUDE_DIR NCSEcw.h
@@ -36,10 +37,24 @@ IF( ECW_INCLUDE_DIR )
   IF( ECW_ncsecw_LIBRARY )
 
     SET( ECW_FOUND "YES" )
-    SET( ECW_LIBRARIES ${ECW_ncsutil_LIBRARY} ${ECW_ncsecw_LIBRARY} )
+	SET( ECW_LIBRARIES ${ECW_ncsutil_LIBRARY} ${ECW_ncsecw_LIBRARY} )
 
   ENDIF( ECW_ncsecw_LIBRARY )
   ENDIF( ECW_ncsutil_LIBRARY )
 
 
 ENDIF( ECW_INCLUDE_DIR )
+ENDIF( VXL_FORCE_V3P_J2K )
+
+IF (ECW_FOUND)
+	SET(VXL_USING_NATIVE_J2K "YES")
+ELSE (ECW_FOUND)
+ IF(EXISTS ${vxl_SOURCE_DIR}/v3p/j2k/Source/include/NCSEcw.h)
+
+    SET( ECW_FOUND "YES" )
+    SET( ECW_INCLUDE_DIR ${vxl_SOURCE_DIR}/v3p/j2k/Source/include)  
+    SET( ECW_INSTALL_INCLUDE_DIR ${CMAKE_INSTALL_DIR}/include/vxl/v3p/j2k)
+    SET( ECW_LIBRARIES NCSEcw NCSUtil )
+  
+  ENDIF(EXISTS ${vxl_SOURCE_DIR}/v3p/j2k/Source/include/NCSEcw.h)
+ ENDIF(ECW_FOUND)
