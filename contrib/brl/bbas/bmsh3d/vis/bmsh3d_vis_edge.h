7a764f93865aee67b019afa967ce36ed9227f24d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-01-22 09:35:37 -0500

    added missing class declaration
    SVN-Revision: 30886

--- contrib/brl/bbas/bmsh3d/vis/bmsh3d_vis_edge.h
+++ contrib/brl/bbas/bmsh3d/vis/bmsh3d_vis_edge.h
@@ -1,12 +1,14 @@
-//: This is lemsvxlsrc/brcv/shp/bmsh3d/vis/bmsh3d_vis_mesh.h
-//  MingChing Chang
-//  May 03, 2005.
-
-#ifndef _bmsh3d_vis_edge_h_
-#define _bmsh3d_vis_edge_h_
+// This is brl/bbas/bmsh3d/vis/bmsh3d_vis_edge.h
+#ifndef bmsh3d_vis_edge_h_
+#define bmsh3d_vis_edge_h_
+//:
+// \file
+// \author MingChing Chang
+// \date May 03, 2005.
 
 #include <Inventor/SbColor.h>
 class SoSeparator;
+class bmsh3d_edge;
 
 void draw_edge_geom (SoSeparator* root, const bmsh3d_edge *E,
                      const bool user_defined_class = true);
@@ -14,7 +16,4 @@ void draw_edge_geom (SoSeparator* root, const bmsh3d_edge *E,
 SoSeparator* draw_edge (bmsh3d_edge* E, const SbColor& color, const float width = 1.0f,
                         const bool user_defined_class = true);
 
-
 #endif
-
-
