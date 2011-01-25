829163974af3657ebc806d9e3bb4bb6e8e12ba86
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-01-25 15:42:08 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 30912

--- contrib/brl/bseg/boxm2/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/tests/test_include.cxx
@@ -12,6 +12,7 @@
 #include <boxm2/io/boxm2_asio_mgr.h>
 #include <boxm2/io/boxm2_cache.h>
 #include <boxm2/io/boxm2_dumb_cache.h>
+#include <boxm2/io/boxm2_lru_cache.h>
 #include <boxm2/io/boxm2_nn_cache.h>
 #include <boxm2/io/boxm2_sio_mgr.h>
 
