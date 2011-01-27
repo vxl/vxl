14c59d6388768799d6261db126403740a422c9c8
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-01-27 12:30:09 -0500

    restructuring library
    SVN-Revision: 30954

--- contrib/brl/bseg/bvpl/bvpl_octree/bvpl_discover_pca_kernels.cxx
+++ contrib/brl/bseg/bvpl/bvpl_octree/bvpl_discover_pca_kernels.cxx
@@ -9,7 +9,6 @@
 #include <bxml/bxml_read.h>
 #include <bxml/bxml_find.h>
 #include <boxm/boxm_scene_parser.h>
-#include <vcl_assert.h>
 
 bvpl_discover_pca_kernels::bvpl_discover_pca_kernels(const vcl_string &path)
 {
