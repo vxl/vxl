2142f0209cadffd776510dedc88c4da467afc52c
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-01-27 12:10:00 -0500

    restructuring library
    SVN-Revision: 30942

--- contrib/brl/bseg/bvpl/pro/Templates/brdb_value_t+bvpl_kernel_sptr-.cxx
+++
@@ -1,7 +0,0 @@
-#include <brdb/brdb_value.txx>
-
-#include <vbl/io/vbl_io_smart_ptr.h>
-#include <bvpl/bvpl_kernel_factory.h>
-
-BRDB_VALUE_INSTANTIATE(bvpl_kernel_sptr, "bvpl_kernel_sptr");
-BRDB_VALUE_INSTANTIATE(bvpl_kernel_vector_sptr, "bvpl_kernel_vector_sptr");
