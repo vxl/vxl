c667f4cb51ad0959d1f550a273ee7ddfa47524be
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-01-27 12:25:29 -0500

    restructuring library
    SVN-Revision: 30953

--- contrib/brl/bseg/bvpl/io/Templates/vbl_io_smart_ptr+bvpl_kernel-.cxx
+++
@@ -1,6 +0,0 @@
-#include <bvpl/bvpl_kernel_factory.h>
-#include <bvpl/io/bvpl_io_kernel.h>
-#include <vbl/io/vbl_io_smart_ptr.txx>
-
-VBL_IO_SMART_PTR_INSTANTIATE(bvpl_kernel);
-VBL_IO_SMART_PTR_INSTANTIATE(bvpl_kernel_vector);
