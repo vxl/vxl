61cf9dc58598cfe0ae50367567d18f294c441ee3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-02-05 12:08:59 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 31037

--- contrib/brl/bseg/boxm/algo/tests/test_include.cxx
+++ contrib/brl/bseg/boxm/algo/tests/test_include.cxx
@@ -4,13 +4,13 @@
 #include <boxm/algo/boxm_fill_in_mesh.h>
 #include <boxm/algo/boxm_fill_internal_cells.h>
 #include <boxm/algo/boxm_init_scene.h>
+#include <boxm/algo/boxm_linear_operations.h>
 #include <boxm/algo/boxm_refine.h>
 #include <boxm/algo/boxm_save_block_raw.h>
 #include <boxm/algo/boxm_save_scene_raw.h>
 #include <boxm/algo/boxm_split_sample.h>
 #include <boxm/algo/boxm_upload_mesh.h>
 #include <boxm/algo/boxm_merge_mog.h>
-
 
 #include <boxm/algo/pro/boxm_algo_processes.h>
 #include <boxm/algo/pro/boxm_algo_register.h>
