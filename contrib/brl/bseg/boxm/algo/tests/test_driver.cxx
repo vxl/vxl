172478637ae888d3c9a9026c9cd6506a29a743f3
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-02-07 13:50:17 -0500

    added test_utils and test_linear_operations
    SVN-Revision: 31063

--- contrib/brl/bseg/boxm/algo/tests/test_driver.cxx
+++ contrib/brl/bseg/boxm/algo/tests/test_driver.cxx
@@ -5,6 +5,7 @@ DECLARE( test_save_scene_raw );
 DECLARE( test_upload_mesh );
 DECLARE( test_boxm_fill_internal_cells );
 DECLARE( test_boxm_scene_crop );
+DECLARE( test_linear_operations );
 
 void register_tests()
 {
@@ -13,6 +14,7 @@ void register_tests()
   REGISTER( test_upload_mesh );
   REGISTER( test_boxm_fill_internal_cells );  
   REGISTER( test_boxm_scene_crop );
+  REGISTER( test_linear_operations );
 }
 
 
