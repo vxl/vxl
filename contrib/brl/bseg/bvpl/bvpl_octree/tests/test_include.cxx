1a1e88d48517ddc290bd86b306091f5544cb3534
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-02-08 17:51:01 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 31107

--- contrib/brl/bseg/bvpl/bvpl_octree/tests/test_include.cxx
+++ contrib/brl/bseg/bvpl/bvpl_octree/tests/test_include.cxx
@@ -1,3 +1,4 @@
+#include <bvpl/bvpl_octree/bvpl_block_kernel_operator.h>
 #include <bvpl/bvpl_octree/bvpl_cell_data_traits.h>
 #include <bvpl/bvpl_octree/bvpl_discover_pca_kernels.h>
 #include <bvpl/bvpl_octree/bvpl_octree_sample.h>
@@ -10,5 +11,6 @@
 #include <bvpl/bvpl_octree/bvpl_scene_kernel_operator.h>
 #include <bvpl/bvpl_octree/bvpl_scene_neighborhood_operator.h>
 #include <bvpl/bvpl_octree/bvpl_scene_statistics.h>
+#include <bvpl/bvpl_octree/bvpl_taylor_basis.h>
 
 int main() { return 0; }
