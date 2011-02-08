c86b4462952a2cfffeb72ca27041ec1042815bde
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-02-08 10:37:35 -0500

    added missing #include <cstdio> (for std::sprintf)
    SVN-Revision: 31088

--- contrib/brl/bseg/boxm2/boxm2_util.cxx
+++ contrib/brl/bseg/boxm2/boxm2_util.cxx
@@ -18,41 +18,41 @@
 //vcl io stuff
 #include <vcl_iostream.h>
 #include <vcl_fstream.h>
+#include <vcl_cstdio.h>
 
 void boxm2_util::random_permutation(int* buffer, int size)
 {
   vnl_random random(9667566);
-  
+
   //shuffle the buffer
-  for(int i=0; i<size; i++) 
+  for (int i=0; i<size; i++)
   {
     //swap current value with random one after it
-    int curr = buffer[i]; 
-    int rInd = random.lrand32(i, size-1); 
+    int curr = buffer[i];
+    int rInd = random.lrand32(i, size-1);
     buffer[i] = buffer[rInd];
-    buffer[rInd] = curr; 
+    buffer[rInd] = curr;
   }
-
 }
+
 float boxm2_util::clamp(float x, float a, float b)
 {
     return x < a ? a : (x > b ? b : x);
 }
 
 
-
-//Constructs a camera given elevation, azimuth (degrees), radius, and bounding box.  
-vpgl_perspective_camera<double>* 
-boxm2_util::construct_camera( double elevation, 
-                              double azimuth, 
-                              double radius, 
+//Constructs a camera given elevation, azimuth (degrees), radius, and bounding box.
+vpgl_perspective_camera<double>*
+boxm2_util::construct_camera( double elevation,
+                              double azimuth,
+                              double radius,
                               unsigned ni,
                               unsigned nj,
                               vgl_box_3d<double> bb)
 {
   double dni = static_cast<double>(ni);
   double dnj = static_cast<double>(nj);
-  
+
   //
   //find a camera that will project the scene bounding box
   //entirely inside the image
@@ -99,7 +99,7 @@ boxm2_util::construct_camera( double elevation,
   cam->set_calibration(K);
 
   //vcl_cout<<"Camera  :" <<*cam<<vcl_endl;
-  return cam; 
+  return cam;
 }
 
 
@@ -113,7 +113,7 @@ bool boxm2_util::copy_file(vcl_string file, vcl_string dest)
     while ( myfile.good() )
     {
       getline (myfile,line);
-      outfile << line << '\n'; 
+      outfile << line << '\n';
     }
     myfile.close();
     outfile.close();
@@ -122,98 +122,93 @@ bool boxm2_util::copy_file(vcl_string file, vcl_string dest)
     vcl_cout<<"Couldn't open " << file << " or " << dest << vcl_endl;
     return false;
   }
-  return true; 
+  return true;
 }
 
 bool boxm2_util::generate_html(int nrows, int ncols, vcl_string dest)
 {
-  char html[4*1024]; 
-  sprintf(html, 
-         "<!DOCTYPE html>  \n \
-          <html lang='en'>  \n \
-          <head>            \n \
-            <meta charset='utf-8' content='text/html' http-equiv='Content-type' /> \n \
-            <title>Object on iPhone Example - jQuery Reel 360°</title> \n \
-            <!-- Reel/Jquery Script Includes -->  \n \
-            <script src='js/jquery.min.js' type='text/javascript'></script> \n \
-            <script src='js/jquery.reel-min.js' type='text/javascript'></script> \n \
-            <script src='js/jquery.disabletextselect-min.js' type='text/javascript'></script> \n \
-            <script src='js/jquery.mousewheel-min.js' type='text/javascript'></script> \n \
-            <script src='js/js.js' type='text/javascript'></script> \n \
-            <meta name='viewport' content='width = 480' /> \n \
-            <style> \n \
-              html, body{ margin: 0; background: #000 url(iphone.instructions.gif) no-repeat 0 480px; } \n \
-            </style> \n \
-          </head> \n \
-          <body> \n \
-            <div id='wrapper'> \n \
-              <img id='image' src='img/scene_0_0.jpg' width='640px' height='480px' /> \n \
-            </div> \n \
-            <script type='text/javascript'> \n \
-              $(document).ready(function(){  \n \
-                $('#image').reel({  \n \
-                  frame: 1,         \n \
-                  footage: %d,      \n \
-                  frames: %d,       \n \
-                  rows: %d,          \n \
-                  row: 1,           \n \
-                  path: 'img/',     \n \
-                  image: 'img/scene_0_0.jpg', \n \
-                  images: scene_frames(), \n \
-                  horizontal: true,  \n \
-                });  \n \
-              }); \n \
-            </script> \n \
-          </body> \n \
-          </html>", 
-          ncols, 
-          ncols, 
-          nrows); 
+  char html[4*1024];
+  vcl_sprintf(html,
+              "<!DOCTYPE html>\n \
+<html lang='en'>\n \
+<head>\n \
+  <meta charset='utf-8' content='text/html' http-equiv='Content-type' />\n \
+  <title>Object on iPhone Example - jQuery Reel 360°</title>\n \
+  <!-- Reel/Jquery Script Includes -->\n \
+  <script src='js/jquery.min.js' type='text/javascript'></script>\n \
+  <script src='js/jquery.reel-min.js' type='text/javascript'></script>\n \
+  <script src='js/jquery.disabletextselect-min.js' type='text/javascript'></script>\n \
+  <script src='js/jquery.mousewheel-min.js' type='text/javascript'></script>\n \
+  <script src='js/js.js' type='text/javascript'></script>\n \
+  <meta name='viewport' content='width = 480' />\n \
+  <style>\n \
+    html, body{ margin: 0; background: #000 url(iphone.instructions.gif) no-repeat 0 480px; }\n \
+  </style>\n \
+</head>\n \
+<body>\n \
+  <div id='wrapper'>\n \
+    <img id='image' src='img/scene_0_0.jpg' width='640px' height='480px' />\n \
+  </div>\n \
+  <script type='text/javascript'>\n \
+    $(document).ready(function(){\n \
+      $('#image').reel({\n \
+        frame: 1,\n \
+        footage: %d,\n \
+        frames: %d,\n \
+        rows: %d,\n \
+        row: 1,\n \
+        path: 'img/',\n \
+        image: 'img/scene_0_0.jpg',\n \
+        images: scene_frames(),\n \
+        horizontal: true,\n \
+      });\n \
+    });\n \
+  </script>\n \
+</body>\n \
+</html>",
+              ncols,
+              ncols,
+              nrows);
 
   //write to destination file
   vcl_ofstream outfile(dest.c_str());
   if (outfile.is_open())
   {
-    outfile << html; 
+    outfile << html;
     outfile.close();
   }
   else {
     vcl_cout<<"Couldn't open " << dest << vcl_endl;
     return false;
   }
-  return true; 
+  return true;
 }
 
 
 bool boxm2_util::generate_jsfunc(vbl_array_2d<vcl_string> img_files, vcl_string dest)
 {
   vcl_string js = "function scene_frames(frames){\n var stack = [ ";
-     
+
   //go through the array in img_files
-  int totalCells = img_files.rows() * img_files.cols(); 
-  for(int row=0; row<img_files.rows(); ++row) {
-    for(int col=0; col<img_files.cols(); ++col) {
-      js += "'" + img_files(row, col) + "'"; 
-     
-      //don't put a comma at the end...
-      if(row + col * img_files.rows() < totalCells-1) js += ", ";
+  for (unsigned int row=0; row<img_files.rows(); ++row) {
+    for (unsigned int col=0; col<img_files.cols(); ++col) {
+      if (row != 0 && col != 0) js += ", "; //don't put a comma at the end...
+      js += "'" + img_files(row, col) + "'";
     }
   }
-  js += "]\n"; 
-  js += "return stack\n";
-  js += "}"; 
-  
+  js += "]\nreturn stack\n}";
+
   //write to destination file
   vcl_ofstream outfile(dest.c_str());
   if (outfile.is_open())
   {
-    outfile << js; 
+    outfile << js;
     outfile.close();
+    return true;
   }
   else {
     vcl_cout<<"Couldn't open " << dest << vcl_endl;
     return false;
   }
-  return true; 
 }
-  
+
