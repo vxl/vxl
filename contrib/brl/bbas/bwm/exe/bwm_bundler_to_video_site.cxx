131ae7bb793139a99b1e67e13dde11b592a45a32
Vishal Jain <jainv@users.sourceforge.net>
2011-03-01 10:46:48 -0500

    Added few more features.
    SVN-Revision: 31226

--- contrib/brl/bbas/bwm/exe/bwm_bundler_to_video_site.cxx
+++ contrib/brl/bbas/bwm/exe/bwm_bundler_to_video_site.cxx
@@ -19,10 +19,18 @@
 #include <vgl/vgl_polygon_scan_iterator.h>
 #include <vgl/algo/vgl_fit_plane_3d.h>
 #include <vgl/vgl_distance.h>
+#include <vgl/vgl_point_3d.h>
+#include <vgl/vgl_vector_3d.h>
+#include <vgl/vgl_box_3d.h>
+#include <vgl/algo/vgl_rotation_3d.h>
+#include <vnl/vnl_double_3.h>
+#include <vnl/vnl_quaternion.h>
 #include <vpgl/vpgl_camera.h>
 #include <vpgl/vpgl_rational_camera.h>
 #include <vpgl/vpgl_local_rational_camera.h>
 #include <vpgl/vpgl_perspective_camera.h>
+#include <vpgl/algo/vpgl_camera_bounds.h>
+
 #include <vidl/vidl_image_list_istream.h>
 #include <vnl/vnl_transpose.h>
 
@@ -31,7 +39,97 @@
 #include <vil/vil_image_view.h>
 #include <vil/vil_save.h>
 #include <vcl_cstdlib.h> // for rand()
+#include <bwm/bwm_site_mgr.h>
+static void write_vrml_header(vcl_ofstream& str)
+{
+  str << "#VRML V2.0 utf8\n"
+      << "Background {\n"
+      << "  skyColor [ 0 0 0 ]\n"
+      << "  groundColor [ 0 0 0 ]\n"
+      << "}\n";
+}
+static void
+wrtie_vrml_cameras(vcl_ofstream& str,vcl_vector<vpgl_perspective_camera<double> > & cams, double rad)
+{
+  str << "#VRML V2.0 utf8\n"
+      << "Background {\n"
+      << "  skyColor [ 0 0 0 ]\n"
+      << "  groundColor [ 0 0 0 ]\n"
+      << "}\n";
+  int n = cams.size();
+  for (int i =0; i<n; i++) {
+    vgl_point_3d<double> cent =  cams[i].get_camera_center();
+    str << "Transform {\n"
+        << "translation " << cent.x() << ' ' << cent.y() << ' '
+        << ' ' << cent.z() << '\n'
+        << "children [\n"
+        << "Shape {\n"
+        << " appearance Appearance{\n"
+        << "   material Material\n"
+        << "    {\n"
+        << "      diffuseColor " << 1 << ' ' << 1.0 << ' ' << 0.0 << '\n'
+        << "      transparency " << 0.0 << '\n'
+        << "    }\n"
+        << "  }\n"
+        << " geometry Sphere\n"
+        <<   "{\n"
+        << "  radius " << rad << '\n'
+        <<  "   }\n"
+        <<  "  }\n"
+        <<  " ]\n"
+        << "}\n";
+    vgl_vector_3d<double> r = cams[i].principal_axis();
+    vnl_double_3 yaxis(0.0, 1.0, 0.0), pvec(r.x(), r.y(), r.z());
+    vgl_rotation_3d<double> rot(yaxis, pvec);
+    vnl_quaternion<double> q = rot.as_quaternion();
 
+    vnl_double_3 axis = q.axis();
+    double ang = q.angle();
+    str <<  "Transform {\n"
+        << " translation " << cent.x()+6*rad*r.x() << ' ' << cent.y()+6*rad*r.y()
+        << ' ' << cent.z()+6*rad*r.z() << '\n'
+        << " rotation " << axis[0] << ' ' << axis[1] << ' ' << axis[2] << ' ' <<  ang << '\n'
+        << "children [\n"
+        << " Shape {\n"
+        << " appearance Appearance{\n"
+        << "  material Material\n"
+        << "   {\n"
+        << "     diffuseColor 1 0 0\n"
+        << "     transparency 0\n"
+        << "    }\n"
+        << "  }\n"
+        << " geometry Cylinder\n"
+        << "{\n"
+        << " radius "<<rad/3<<'\n'
+        << " height " << 12*rad << '\n'
+        << " }\n"
+        << " }\n"
+        << "]\n"
+        << "}\n";
+  }
+}
+static void write_vrml_points(vcl_ofstream& str,
+                              vcl_vector<vgl_point_3d<double> > const& pts3d, double rad=2.0)
+{
+  int n = pts3d.size();
+  str<<"Shape {\n";
+  str<<"geometry PointSet {\n";
+  str<<"coord Coordinate {\n";
+  str<<"point [\n";
+  for (int i =0; i<n; i++)
+    str<<pts3d[i].x()<<' '<<pts3d[i].y()<<' '<< pts3d[i].z() <<','<<"\n";
+
+  str<<"]\n";
+  str<<"}\n";
+  str<<"color Color {\n";
+  str<<"color [\n";
+  for (int i =0; i<n; i++)
+    str<<0<<' '<<1.0<<' '<< 0.0 <<','<<"\n";
+  str<<"]\n";
+  str<<"}\n";
+  str<<"}\n";
+  str<<"}\n";
+}
 bool fit_plane_ransac(vcl_vector<vgl_homg_point_3d<double> > & points, vgl_homg_plane_3d<double>  & plane)
 {
   unsigned int nchoose=3;
@@ -96,7 +194,6 @@ bool axis_align_scene(vcl_vector<bwm_video_corr_sptr> & corrs,
     return false;
 
   vgl_homg_plane_3d<double> plane=fit_plane.get_plane();
-  vcl_cout<<"Normal "<<plane.normal();
   vgl_rotation_3d<double> rot_scene(plane.normal(),vgl_vector_3d<double>(0,0,1));
 
   double sumx=0,sumy=0,sumz=0;
@@ -138,7 +235,6 @@ bool axis_align_scene(vcl_vector<bwm_video_corr_sptr> & corrs,
 
   if (2*up<cams.size())// flip the world
   {
-    vcl_cout<<"Flipping the world"<<vcl_endl;
     vnl_quaternion<double> q(vnl_math::pi,0,0);
     vgl_rotation_3d<double> rotx_pi(q);
     for (unsigned i=0;i<corrs.size();++i)
@@ -166,7 +262,6 @@ bool axis_align_scene(vcl_vector<bwm_video_corr_sptr> & corrs,
       return false;
 
   vgl_homg_plane_3d<double> plane1=fit_plane1.get_plane();
-  vcl_cout<<"Plane "<<plane1;
 
   return true;
 }
@@ -176,12 +271,13 @@ int main(int argc, char** argv)
 {
   //Get Inputs
 
-  vul_arg<vcl_string> bundlerfile("-bundler", "Output file of bundler",  "");
-  vul_arg<vcl_string> cam_dir    ("-cam_dir",      "directory to store cams", "");
-  vul_arg<vcl_string> img_dir   ("-img_dir",     "list of images filenames", "");
-  vul_arg<vcl_string> site_name  ("-site_name",    "Name of the site", "");
+  vul_arg<vcl_string> bundlerfile   ("-bundler", "Output file of bundler",  "");
+  vul_arg<vcl_string> cam_dir       ("-cam_dir",      "directory to store cams", "");
+  vul_arg<vcl_string> img_dir       ("-img_dir",     "list of images filenames", "");
+  vul_arg<vcl_string> site_name     ("-site_name",    "Name of the site", "");
   vul_arg<vcl_string> site_directory("-site_dir", "Directory for the site", "");
-  vul_arg<vcl_string> cam_txt_dir    ("-cam_txt_dir",      "directory to store txt cams", "");
+  vul_arg<vcl_string> cam_txt_dir   ("-cam_txt_dir",      "directory to store txt cams", "");
+  vul_arg<vcl_string> vrml_file     ("-vrml_file",      "vrml file", "");
 
   vul_arg_parse(argc, argv);
 
@@ -261,10 +357,13 @@ int main(int argc, char** argv)
       bad_cams.insert(i);
     }
   }
+
+
   vcl_vector<bwm_video_corr_sptr> corrs;
   //  Projection error
-  double err=0;
-  double cnt=0;
+  double err=0;double cnt=0;
+  double maxerr=0;
+  int maxerrcam=-1;
   // read the correspondence and 3-d points
   for (unsigned i=0;i<num_pts;++i)
   {
@@ -275,6 +374,7 @@ int main(int argc, char** argv)
     bfile>>x>>y>>z;
 
     corr->set_world_pt(vgl_point_3d<double>(x,y,z));
+
     vgl_homg_point_3d<double> homg_world_pt(corr->world_pt());
 
     int r,g,b;
@@ -288,30 +388,30 @@ int main(int argc, char** argv)
     {
       unsigned view_number, key_number;
       double img_x,img_y;
+
+      
       bfile>>view_number>>key_number>>img_x>>img_y;
+      
       img_x=img_x+principal_point.x();
       img_y=nj-(img_y+principal_point.y());
+      double maxerrframe=0;
       double u=0,v=0;
       if (cams[view_number].is_behind_camera(homg_world_pt))
-      {
         bad_cams.insert(view_number);
-      }
       else
       {
         cams[view_number].project(x,y,z,u,v);
-        err+=vcl_sqrt((u-img_x)*(u-img_x)+(v-img_y)*(v-img_y));
+        double rms=vcl_sqrt((u-img_x)*(u-img_x)+(v-img_y)*(v-img_y));
+        err+=rms;
         ++cnt;
       }
       corr->add(view_number,vgl_point_2d<double>(img_x,img_y));
     }
     corrs.push_back(corr);
   }
+  vcl_cout<<"Avg Error per projection "<<err/cnt<<vcl_endl;
+  vcl_cout<<"Max Error"<<maxerr<<" is in camera # "<<maxerrcam<<vcl_endl;
   // save it to a site
-  vcl_cout<<"Bad cameras "<<bad_cams.size()<<" :";
-  vcl_set<int>::iterator iter=bad_cams.begin();
-  for (;iter!=bad_cams.end();++iter)
-    vcl_cout<<*iter<<' ';
-  vcl_cout<<"\nError per projection "<<err/cnt<<vcl_endl;
   if (!axis_align_scene(corrs,cams))
     return -1;
   bwm_video_cam_ostream camstream(cam_dir());
@@ -336,10 +436,41 @@ int main(int argc, char** argv)
       }
     }
   }
+
+  vcl_vector<vgl_point_3d<double> > pts_3d;
   vgl_box_3d<double> bounding_box;
   for (unsigned i=0;i<corrs.size();++i)
+  {
     bounding_box.add(corrs[i]->world_pt());
+    pts_3d.push_back(corrs[i]->world_pt());
+  }
+  //: Dimesnions of the World
   vcl_cout<<"Bounding Box "<<bounding_box<<vcl_endl;
+  vcl_cout<<"Center       "<<centre(pts_3d)<<vcl_endl;
+  vcl_cout<<"Stddev       "<<stddev(pts_3d)<<vcl_endl;
+
+
+  //: Determing the resolution of the cells
+  vgl_ray_3d<double> cone_axis;
+  double cone_half_angle;
+  double solid_angle;
+
+  vpgl_camera_bounds::pixel_solid_angle(cams[0],ni/4,nj/4,cone_axis,cone_half_angle,solid_angle);
+
+  vgl_point_3d<double> cc=cams[0].camera_center();
+  double res=2*(cc-centre(pts_3d)).length()*cone_half_angle;
+
+  vcl_cout<<"Resolution     "<<res<<vcl_endl;
+  vcl_ofstream os(vrml_file().c_str());
+  if (os)
+  {
+      write_vrml_header(os);
+      write_vrml_points(os,pts_3d,res);
+      wrtie_vrml_cameras(os,cams,res*5);
+  }
+  else
+      vcl_cout<<"ERROR OPENING  "<< vrml_file() <<vcl_endl;
+
   bwm_video_site_io site;
   site.set_name(site_name());
   site.set_corrs(corrs);
@@ -349,4 +480,11 @@ int main(int argc, char** argv)
 
   vcl_string xml_filename=site_directory()+"/"+site_name()+".xml";
   site.x_write(xml_filename.c_str());
+
+  vcl_cout<<"Bad cameras "<<bad_cams.size()<<" :";
+  vcl_set<int>::iterator iter=bad_cams.begin();
+  for (;iter!=bad_cams.end();++iter)
+    vcl_cout<<*iter<<' ';
+
+
 }
