6646af9018333f367a8a567b915f4cd331e767e8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-03-02 03:02:19 -0500

    Typo corrected
    SVN-Revision: 31235

--- contrib/brl/bseg/boxm2/ocl/boxm2_opencl_cache.cxx
+++ contrib/brl/bseg/boxm2/ocl/boxm2_opencl_cache.cxx
@@ -17,29 +17,30 @@ boxm2_opencl_cache::boxm2_opencl_cache(boxm2_cache* cpu_cache,
   block_info_ = 0;
   tree_ptrs_ = 0;
   trees_per_buffer_ = 0;
-  mem_ptrs_ = 0; 
+  mem_ptrs_ = 0;
   scene_ = scene;
 }
 
 bool boxm2_opencl_cache::clear_cache()
 {
-  if(cached_block_) delete cached_block_, cached_block_=0;
-  if(block_info_) delete block_info_, block_info_=0;
-  if(tree_ptrs_) delete tree_ptrs_, tree_ptrs_=0;
-  if(trees_per_buffer_) delete trees_per_buffer_, trees_per_buffer_=0;
-  if(mem_ptrs_) delete mem_ptrs_, mem_ptrs_=0;
+  delete cached_block_;     cached_block_=0;
+  delete block_info_;       block_info_=0;
+  delete tree_ptrs_;        tree_ptrs_=0;
+  delete trees_per_buffer_; trees_per_buffer_=0;
+  delete mem_ptrs_;         mem_ptrs_=0;
 
   // clean up loaded data
   vcl_map<vcl_string, bocl_mem*>::iterator iter;
   for (iter=cached_data_.begin(); iter!=cached_data_.end(); ++iter) {
-    bocl_mem* dat = iter->second; 
-    if(dat) delete dat; 
+    bocl_mem* dat = iter->second;
+    delete dat;
   }
-  cached_data_.clear(); 
-  loaded_data_.clear(); 
+  cached_data_.clear();
+  loaded_data_.clear();
 
   return true;
 }
+
 //: realization of abstract "get_block(block_id)"
 bocl_mem* boxm2_opencl_cache::get_block(boxm2_block_id id)
 {
@@ -47,10 +48,8 @@ bocl_mem* boxm2_opencl_cache::get_block(boxm2_block_id id)
     return cached_block_;
 
   //clean up...
-  if (cached_block_)
-    delete cached_block_;
-    //this->clear_cache();
-  cached_block_ = 0;
+  delete cached_block_; cached_block_ = 0;
+  //this->clear_cache();
 
   //otherwise load it from disk with blocking
   loaded_ = id;
@@ -63,7 +62,7 @@ bocl_mem* boxm2_opencl_cache::get_block(boxm2_block_id id)
   //store the metadata
   if (block_info_) {
      boxm2_scene_info* buff = (boxm2_scene_info*) block_info_->cpu_buffer();
-     if ( buff ) delete buff;
+     delete buff;
      delete block_info_;
   }
   boxm2_scene_info* info_buffer = scene_->get_blk_metadata(loaded_);
@@ -77,54 +76,60 @@ bocl_mem* boxm2_opencl_cache::get_block(boxm2_block_id id)
 
 //: get tree_ptrs returns the tree pointers for the block currently in the cache
 bocl_mem* boxm2_opencl_cache::get_loaded_tree_ptrs()
-{  
-  //if( tree_ptrs_ ) delete tree_ptrs_; 
-  //tree_ptrs_ = 0;
- 
-  boxm2_block* loaded = cpu_cache_->get_block(loaded_); 
-  boxm2_array_2d<int>& tree_ptrs = loaded->tree_ptrs(); 
+{
+#if 0
+  delete tree_ptrs_;
+  tree_ptrs_ = 0;
+#endif
+
+  boxm2_block* loaded = cpu_cache_->get_block(loaded_);
+  boxm2_array_2d<int>& tree_ptrs = loaded->tree_ptrs();
   tree_ptrs_ = new bocl_mem(*context_, tree_ptrs.begin(), tree_ptrs.size()*sizeof(int), "2d tree_ptrs buffer");
   tree_ptrs_->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR );
-  return tree_ptrs_; 
+  return tree_ptrs_;
 }
 
 bocl_mem* boxm2_opencl_cache::get_loaded_trees_per_buffer()
 {
-  //if( trees_per_buffer_ ) delete trees_per_buffer_; 
-  //trees_per_buffer_ = 0;
- 
-  boxm2_block* loaded = cpu_cache_->get_block(loaded_); 
-  boxm2_array_1d<unsigned short>& trees_per_buffer = loaded->trees_in_buffers(); 
+#if 0
+  delete trees_per_buffer_;
+  trees_per_buffer_ = 0;
+#endif
+
+  boxm2_block* loaded = cpu_cache_->get_block(loaded_);
+  boxm2_array_1d<unsigned short>& trees_per_buffer = loaded->trees_in_buffers();
   trees_per_buffer_ = new bocl_mem(*context_, trees_per_buffer.begin(), trees_per_buffer.size()*sizeof(int), "1d trees_per_buffer buffer");
   trees_per_buffer_->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR );
-  return trees_per_buffer_; 
+  return trees_per_buffer_;
 }
+
 bocl_mem* boxm2_opencl_cache::get_loaded_mem_ptrs()
 {
-  //if( mem_ptrs_ ) delete mem_ptrs_; 
-  //mem_ptrs_ = 0;
-  boxm2_block* loaded = cpu_cache_->get_block(loaded_); 
-  
-  typedef vnl_vector_fixed<unsigned short, 2> ushort2; 
-  boxm2_array_1d<ushort2>& mem_ptrs = loaded->mem_ptrs(); 
+#if 0
+  delete mem_ptrs_;
+  mem_ptrs_ = 0;
+#endif
+  boxm2_block* loaded = cpu_cache_->get_block(loaded_);
+
+  typedef vnl_vector_fixed<unsigned short, 2> ushort2;
+  boxm2_array_1d<ushort2>& mem_ptrs = loaded->mem_ptrs();
   mem_ptrs_ = new bocl_mem(*context_, mem_ptrs.begin(), mem_ptrs.size()*sizeof(int), "1d mem_ptrs buffer");
   mem_ptrs_->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR );
-  return mem_ptrs_; 
+  return mem_ptrs_;
 }
 
 
 //: Get data generic
-// Possible issue: if num_bytes is greater than 0, then should it always intiialize a new data object?
+// Possible issue: if num_bytes is greater than 0, should it then always initialize a new data object?
 bocl_mem* boxm2_opencl_cache::get_data(boxm2_block_id id, vcl_string type, vcl_size_t num_bytes)
 {
-  
   //make sure that the data is in the
-  if (loaded_data_[type] == id) 
+  if (loaded_data_[type] == id)
     return cached_data_[type];
 
   //otherwise get the data block from cpu cache
   //this statment is: does cached_data_ contain(type)?
-  if ( cached_data_.find(type) != cached_data_.end()) 
+  if ( cached_data_.find(type) != cached_data_.end())
   {
 #ifdef DEBUG
     vcl_cout<<"ocl_cache release memory for :"<<type<<vcl_endl;
@@ -137,7 +142,7 @@ bocl_mem* boxm2_opencl_cache::get_data(boxm2_block_id id, vcl_string type, vcl_s
 
   //create new memory
   boxm2_data_base* data_base = cpu_cache_->get_data_base(id,type,num_bytes);
-  if(num_bytes > 0 && data_base->buffer_length() != num_bytes )
+  if (num_bytes > 0 && data_base->buffer_length() != num_bytes )
   {
     vcl_cout<<"GRABBING "<<type<<" that doesn't match input size of "<<num_bytes<<vcl_endl;
   }
@@ -147,20 +152,20 @@ bocl_mem* boxm2_opencl_cache::get_data(boxm2_block_id id, vcl_string type, vcl_s
   cached_data_[type] = data;
 
   return data;
-} 
+}
 
 //: Does a soft delete of data - removes it from the cache but it stays allocated
-//  this will have to NOT decrement the total bytes allocated counter or just 
+//  This will have to NOT decrement the total bytes allocated counter or just
 //  simply move the data to have a different type
 void boxm2_opencl_cache::remove_data(boxm2_block_id id, vcl_string type)
 {
   //make sure that the data is in the
   if (loaded_data_[type] == id) {
-    //loaded_data_[type] = 0; 
+    //loaded_data_[type] = 0;
     //cached_data_[type] = 0;
     vcl_map<vcl_string, boxm2_block_id>::iterator liter = loaded_data_.find(type);
-    loaded_data_.erase(liter); 
+    loaded_data_.erase(liter);
     vcl_map<vcl_string, bocl_mem* >::iterator citer = cached_data_.find(type);
-    cached_data_.erase(citer); 
+    cached_data_.erase(citer);
   }
 }
