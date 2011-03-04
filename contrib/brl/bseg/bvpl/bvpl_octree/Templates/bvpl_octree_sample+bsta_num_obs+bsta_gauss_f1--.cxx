23804046b7e440a0f225446e51b07aef95ea4cb5
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-03-04 10:36:03 -0500

    files moved so bvpl_octree/sample
    SVN-Revision: 31268

--- contrib/brl/bseg/bvpl/bvpl_octree/Templates/bvpl_octree_sample+bsta_num_obs+bsta_gauss_f1--.cxx
+++
@@ -1,7 +0,0 @@
-#include <bvpl/bvpl_octree/bvpl_octree_sample.txx>
-#include <bsta/bsta_gauss_f1.h>
-#include <bsta/io/bsta_io_attributes.h>
-#include <bsta/io/bsta_io_gaussian_sphere.h>
-
-typedef bsta_num_obs<bsta_gauss_f1> gauss_type;
-BVPL_OCTREE_SAMPLE_INSTANTIATE(gauss_type);
--- contrib/brl/bseg/bvpl/bvpl_octree/Templates/bvpl_octree_sample+float-.cxx
+++
@@ -1,3 +0,0 @@
-#include <bvpl/bvpl_octree/bvpl_octree_sample.txx>
-
-BVPL_OCTREE_SAMPLE_INSTANTIATE(float);
