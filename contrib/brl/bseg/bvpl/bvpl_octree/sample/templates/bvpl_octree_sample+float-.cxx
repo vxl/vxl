cb974fad99784c43060e67e9db38b4867b1018b9
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-03-04 14:19:31 -0500

    typo in folder name
    SVN-Revision: 31293

--- contrib/brl/bseg/bvpl/bvpl_octree/sample/templates/bvpl_octree_sample+float-.cxx
+++
@@ -1,3 +0,0 @@
-#include <bvpl/bvpl_octree/sample/bvpl_octree_sample.txx>
-
-BVPL_OCTREE_SAMPLE_INSTANTIATE(float);
--- contrib/brl/bseg/bvpl/bvpl_octree/sample/templates/bvpl_pca_basis_sample+10-.cxx
+++
@@ -1,3 +0,0 @@
-#include <bvpl/bvpl_octree/sample/bvpl_pca_basis_sample.txx>
-
-BVPL_PCA_BASIS_SAMPLE_INSTANTIATE(10);
