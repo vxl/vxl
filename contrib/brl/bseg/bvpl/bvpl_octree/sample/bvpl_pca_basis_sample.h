3b03724ced4c9b7fbc1c0c6388e2eb4a0131ca6f
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-03-04 10:35:08 -0500

    files created
    SVN-Revision: 31266

---
+++ contrib/brl/bseg/bvpl/bvpl_octree/sample/bvpl_pca_basis_sample.h
@@ -0,0 +1,46 @@
+// This is bvpl_pca_basis_sample.h
+#ifndef bvpl_pca_basis_sample_h
+#define bvpl_pca_basis_sample_h
+
+//:
+// \file
+// \brief A data structure that contains the vector of projections on pca space
+// \author Isabel Restrepo mir@lems.brown.edu
+// \date  28-Feb-2011.
+//
+// \verbatim
+//  Modifications
+//   <none yet>
+// \endverbatim
+
+#include <vnl/vnl_vector_fixed.h>
+#include <vcl_iostream.h>
+#include <vsl/vsl_binary_io.h>
+
+template <unsigned dim>
+class bvpl_pca_basis_sample{
+  
+public:
+  bvpl_pca_basis_sample(){}
+  short version_no() const{ return 1; }
+  //: Vector of projections on pca space
+  vnl_vector_fixed<double,dim> pca_projections_;
+};
+
+
+template <unsigned dim>
+vcl_ostream& operator << (vcl_ostream& os, const bvpl_pca_basis_sample<dim>& sample);
+
+template <unsigned dim>
+void vsl_b_write(vsl_b_ostream & os, bvpl_pca_basis_sample<dim> const &sample);
+
+template <unsigned dim>
+void vsl_b_write(vsl_b_ostream & os, bvpl_pca_basis_sample<dim> const * &sample);
+
+template <unsigned dim>
+void vsl_b_read(vsl_b_istream & is, bvpl_pca_basis_sample<dim> &sample);
+
+template <unsigned dim>
+void vsl_b_read(vsl_b_istream & is, bvpl_pca_basis_sample<dim> *&sample);
+
+#endif
---
+++ contrib/brl/bseg/bvpl/bvpl_octree/sample/templates/bvpl_pca_basis_sample+10-.cxx
@@ -0,0 +1,3 @@
+#include <bvpl/bvpl_octree/sample/bvpl_pca_basis_sample.txx>
+
+BVPL_PCA_BASIS_SAMPLE_INSTANTIATE(10);
