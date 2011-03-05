53a8f8796f006b91358efcd0b39967fcf80c3af3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-03-05 16:54:55 -0500

    renamed (standard template instantiation file naming)
    SVN-Revision: 31302

--- contrib/brl/bseg/bvpl/bvpl_octree/Templates/boct_tree+short.bvpl_taylor_basis2_sample-.cxx
+++ contrib/brl/bseg/bvpl/bvpl_octree/Templates/boct_tree+short.bvpl_taylor_basis2_sample-.cxx
@@ -2,7 +2,5 @@
 #include <boct/boct_tree_cell.txx>
 #include <bvpl/bvpl_octree/sample/bvpl_taylor_basis_sample.h>
 
-BOCT_TREE_INSTANTIATE(short, bvpl_taylor_basis2_sample );
+BOCT_TREE_INSTANTIATE(short, bvpl_taylor_basis2_sample);
 BOCT_TREE_CELL_INSTANTIATE(short, bvpl_taylor_basis2_sample);
-
-
--- contrib/brl/bseg/bvpl/bvpl_octree/Templates/boxm_block+boct_tree+short.bvpl_taylor_basis2_sample--.cxx
+++ contrib/brl/bseg/bvpl/bvpl_octree/Templates/boxm_block+boct_tree+short.bvpl_taylor_basis2_sample--.cxx
@@ -4,7 +4,7 @@
 #include <bvpl/bvpl_octree/sample/bvpl_taylor_basis_sample.h>
 #include <vbl/vbl_array_3d.txx>
 
-typedef boct_tree<short, bvpl_taylor_basis2_sample > tree_type;
+typedef boct_tree<short, bvpl_taylor_basis2_sample> tree_type;
 
 BOXM_BLOCK_INSTANTIATE(tree_type);
 BOXM_BLOCK_ITERATOR_INSTANTIATE(tree_type);
