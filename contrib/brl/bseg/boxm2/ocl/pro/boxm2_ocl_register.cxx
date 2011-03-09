b5fb72d333086c0990530c59e6ffce763020d998
Vishal Jain <jainv@users.sourceforge.net>
2011-03-09 15:21:45 -0500

    added infrastructure to add processes.
    SVN-Revision: 31359

---
+++ contrib/brl/bseg/boxm2/ocl/pro/boxm2_ocl_register.cxx
@@ -0,0 +1,18 @@
+#include "boxm2_ocl_register.h"
+
+#include <bprb/bprb_macros.h>
+#include <bprb/bprb_batch_process_manager.h>
+#include <bprb/bprb_func_process.h>
+
+
+#include "boxm2_ocl_processes.h"
+
+void boxm2_ocl_register::register_datatype()
+{
+  //REGISTER_DATATYPE( boxm2_scene_sptr );
+}
+
+void boxm2_ocl_register::register_process()
+{
+  //REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_load_scene_process, "boxm2LoadSceneProcess");
+}
