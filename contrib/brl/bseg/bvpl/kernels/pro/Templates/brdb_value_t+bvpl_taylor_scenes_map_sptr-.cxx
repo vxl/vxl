5a9c6992d3ff016ff74c315ab705cef3f21ec2a3
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-03-15 10:58:35 -0400

    added missing template instantiations
    SVN-Revision: 31420

---
+++ contrib/brl/bseg/bvpl/kernels/pro/Templates/brdb_value_t+bvpl_taylor_scenes_map_sptr-.cxx
@@ -0,0 +1,7 @@
+#include <brdb/brdb_value.txx>
+
+#include <vbl/io/vbl_io_smart_ptr.h>
+#include <bvpl/bvpl_octree/bvpl_taylor_basis.h>
+
+BRDB_VALUE_INSTANTIATE(bvpl_taylor_scenes_map_sptr, "bvpl_taylor_scenes_map_sptr");
+
---
+++ contrib/brl/bseg/bvpl/pro/Templates/brdb_value_t+bvxm_voxel_grid_base_sptr-.cxx
@@ -0,0 +1,7 @@
+#include <brdb/brdb_value.txx>
+
+#include <vbl/io/vbl_io_smart_ptr.h>
+#include <bvxm/grid/bvxm_voxel_grid_base.h>
+#include <bvxm/grid/io/bvxm_io_voxel_grid_base.h>
+
+BRDB_VALUE_INSTANTIATE(bvxm_voxel_grid_base_sptr, "bvxm_voxel_grid_base_sptr");
