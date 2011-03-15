cd9d522ca6b695b774a7b2da8f94c1a6f636d23d
Gamze Tunali <gamze@users.sourceforge.net>
2011-03-15 11:52:03 -0400

    added a new test on inserting a subtree to a leaf node
    SVN-Revision: 31426

--- contrib/brl/bseg/boct/tests/test_create_tree.cxx
+++ contrib/brl/bseg/boct/tests/test_create_tree.cxx
@@ -10,19 +10,16 @@ static void test_create_tree()
   TEST("No of Max levels of tree",nlevels, tree->number_levels());
 
   tree->split();
-
+  tree->init_cells(vgl_point_3d<double>(0,0,0));
   vcl_vector<boct_tree_cell<short,vgl_point_3d<double> >*> leaves = tree->leaf_cells();
   TEST("No of Leaf Cells", 8, leaves.size());
-  //for (unsigned i=0; i<leaves.size(); i++)
-  //  leaves[i]->print();
-
 
   boct_tree<short,vgl_point_3d<double> > *init_tree = new boct_tree<short,vgl_point_3d<double> >(5, 3);
+  tree->init_cells(vgl_point_3d<double>(1,2,3));
   vcl_vector<boct_tree_cell<short,vgl_point_3d<double> >*> leaves2 = init_tree->leaf_cells();
   TEST("No of Leaf Cells after 3 levels", 8*8, leaves2.size());
-  //init_tree->print();
 
-  // TEST THE TREE CREATION FROM THE LEAF NODES
+  // Test the tree creation from the fleaf nodes
   vcl_vector<boct_tree_cell<short,vgl_point_3d<double> > > leaf_nodes;
   for (unsigned i=0; i<leaves2.size(); i++) {
     leaf_nodes.push_back(*leaves2[i]);
@@ -40,10 +37,33 @@ static void test_create_tree()
       good=false;
   }
   TEST("Constructiong a tree from leaf nodes", true, good);
-  //new_tree.print();
-
   delete tree;
   delete init_tree;
+
+  // Test the tree insertion
+  // create the main tree
+  tree=new boct_tree<short,vgl_point_3d<double> >(nlevels);
+  tree->split();
+  tree->init_cells(vgl_point_3d<double>(0,0,0));
+
+  //create the tree to be inserted 
+  boct_tree<short,vgl_point_3d<double> > * subtree=new boct_tree<short,vgl_point_3d<double> >(3, 3);
+  subtree->init_cells(vgl_point_3d<double> (10,10,10) );
+  boct_tree_cell<short,vgl_point_3d<double> > *node = tree->leaf_cells()[3];
+  boct_tree_cell<short,vgl_point_3d<double> > *sub_root = subtree->root();
+  int before = tree->leaf_cells().size();
+  int sub_count=subtree->leaf_cells().size();
+  node->insert_subtree(sub_root);
+  int after = tree->leaf_cells().size();
+
+  leaves.clear();
+  leaves = tree->leaf_cells();
+  TEST_EQUAL("Inserting the subtree, leaf nodes count", before+sub_count-1, leaves.size());
+  TEST("Inserting the subtree, leaf nodes data at index 3", leaves[3]->data(), vgl_point_3d<double> (10,10,10));
+  TEST("Inserting the subtree, leaf nodes data at index 67", leaves[67]->data(), vgl_point_3d<double> (0,0,0));
+ 
+  delete tree;
+  delete subtree;
 }
 
 TESTMAIN(test_create_tree);
