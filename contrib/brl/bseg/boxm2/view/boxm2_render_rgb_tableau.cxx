a590dc180ca3cc4bd51ce8141de83a9c5901f4a1
Andrew Miller <andrew-miller@users.sourceforge.net>
2011-03-16 17:01:03 -0400

    added boxm2_render_rgb_tableau, now boxm2_render_view executable takes -rgb argument
    SVN-Revision: 31468

--- contrib/brl/bseg/boxm2/view/boxm2_render_rgb_tableau.cxx
+++ contrib/brl/bseg/boxm2/view/boxm2_render_rgb_tableau.cxx
@@ -1,17 +1,17 @@
-#include "boxm2_render_tableau.h"
+#include "boxm2_render_rgb_tableau.h"
 //:
 // \file
-#include <boxm/ocl/boxm_ocl_utils.h>
+#include <boxm2/ocl/boxm2_ocl_util.h>
 #include <vpgl/vpgl_perspective_camera.h>
 #include <vpgl/vpgl_calibration_matrix.h>
 #include <vgui/internals/trackball.h>
 #include <vgui/vgui_modifier.h>
 #include <vgl/vgl_distance.h>
 #include <vcl_sstream.h>
-#include <boxm2/ocl/boxm2_ocl_util.h>
+
 
 //: Constructor
-boxm2_render_tableau::boxm2_render_tableau()
+boxm2_render_rgb_tableau::boxm2_render_rgb_tableau()
 {
   curr_frame_ = 0;
   pbuffer_=0;
@@ -21,12 +21,12 @@ boxm2_render_tableau::boxm2_render_tableau()
 }
 
 //: initialize tableau properties
-bool boxm2_render_tableau::init(vcl_string scene_file,
-                                unsigned ni,
-                                unsigned nj,
-                                vpgl_perspective_camera<double> * cam,
-                                vcl_vector<vcl_string> cam_files,
-                                vcl_vector<vcl_string> img_files)
+bool boxm2_render_rgb_tableau::init(vcl_string scene_file,
+                                    unsigned ni,
+                                    unsigned nj,
+                                    vpgl_perspective_camera<double> * cam,
+                                    vcl_vector<vcl_string> cam_files,
+                                    vcl_vector<vcl_string> img_files)
 {
   //set image dimensions, camera and scene
   ni_ = ni;
@@ -38,26 +38,15 @@ bool boxm2_render_tableau::init(vcl_string scene_file,
   cam_files_ = cam_files;
   img_files_ = img_files;
 
-
-  tf_sptr=new bbas_1d_array_float(256);
-
-  if (img_files_.size()>0)
-      boxm2_ocl_util::get_render_transfer_function(img_files_,mini_,maxi_,tf_sptr->data_array);
-  else  //: else use the default values and a linear transfer function.
-  {
-        mini_=0.0f;
-        maxi_=1.0f;
-        for (int i=0; i<256; ++i) tf_sptr->data_array[i] =(float)i/255.0f;
-  }
-
   //: default image type is uchar;
   img_type_ ="uchar";
-  if (img_files_.size()>0)
-  {
-      vil_image_view_base_sptr loaded_image = vil_load(img_files_[0].c_str());
-      if (vil_image_view<unsigned short> *img_byte = dynamic_cast<vil_image_view<unsigned short>*>(loaded_image.ptr()))
-          img_type_="ushort";
-  }
+
+  //initialize transfer function...
+  tf_sptr=new bbas_1d_array_float(256);
+  mini_=0.0f;
+  maxi_=1.0f;
+  for (int i=0; i<256; ++i) tf_sptr->data_array[i] =(float)i/255.0f;
+  
   //create the scene
   scene_ = new boxm2_scene(scene_file);
 
@@ -66,7 +55,7 @@ bool boxm2_render_tableau::init(vcl_string scene_file,
 
 
 //: Handles tableau events (drawing and keys)
-bool boxm2_render_tableau::handle(vgui_event const &e)
+bool boxm2_render_rgb_tableau::handle(vgui_event const &e)
 {
   //draw handler - called on post_draw()
   if (e.type == vgui_DRAW)
@@ -89,7 +78,8 @@ bool boxm2_render_tableau::handle(vgui_event const &e)
 
     //calculate and write fps to status
     vcl_stringstream str;
-    str<<"num updates: "<<update_count_<<".  rendering at ~ "<< (1000.0f / gpu_time) <<" fps ";
+    str<<"num updates: "<<update_count_
+       <<".  rendering at ~ "<< (1000.0f / gpu_time) <<" fps ";
     status_->write(str.str().c_str());
     }
     return true;
@@ -112,7 +102,6 @@ bool boxm2_render_tableau::handle(vgui_event const &e)
     this->refine_model();
     return true;
   }
-
   //HANDLE idle events - do model updating
   else if (e.type == vgui_IDLE)
   {
@@ -137,9 +126,9 @@ bool boxm2_render_tableau::handle(vgui_event const &e)
   return false;
 }
 
-bool boxm2_render_tableau::save_model()
+bool boxm2_render_rgb_tableau::save_model()
 {
-  vcl_cout<<"SAVING MODEL to: "<< scene_->data_path() <<vcl_endl;
+  vcl_cout<<"SAVING MODEL!!!"<<vcl_endl;
 
   //save blocks and data to disk for debugging
   vcl_map<boxm2_block_id, boxm2_block_metadata> blocks = scene_->blocks();
@@ -149,10 +138,10 @@ bool boxm2_render_tableau::save_model()
     boxm2_block_id id = iter->first;
     boxm2_sio_mgr::save_block(scene_->data_path(), cache_->get_block(id));
     boxm2_sio_mgr::save_block_data(scene_->data_path(), id, cache_->get_data<BOXM2_ALPHA>(id) );
-    if (img_type_=="uchar")
-      boxm2_sio_mgr::save_block_data(scene_->data_path(), id, cache_->get_data<BOXM2_MOG3_GREY>(id) );
-    if (img_type_=="ushort")
-      boxm2_sio_mgr::save_block_data(scene_->data_path(), id, cache_->get_data<BOXM2_MOG3_GREY_16>(id) );
+    if(img_type_=="uchar")
+    boxm2_sio_mgr::save_block_data(scene_->data_path(), id, cache_->get_data<BOXM2_MOG3_GREY>(id) );
+    if(img_type_=="ushort")
+    boxm2_sio_mgr::save_block_data(scene_->data_path(), id, cache_->get_data<BOXM2_MOG3_GREY_16>(id) );
 
     boxm2_sio_mgr::save_block_data(scene_->data_path(), id, cache_->get_data<BOXM2_NUM_OBS>(id) );
   }
@@ -160,14 +149,12 @@ bool boxm2_render_tableau::save_model()
 }
 
 //: refines model
-float boxm2_render_tableau::refine_model()
+float boxm2_render_rgb_tableau::refine_model()
 {
     //create generic scene
     brdb_value_sptr brdb_scene = new brdb_value_t<boxm2_scene_sptr>(scene_);
-    brdb_value_sptr brdb_thresh = new brdb_value_t<float>(0.2f);
-
     brdb_value_sptr brdb_data_type;
-    if (img_type_=="uchar")
+    if(img_type_=="uchar")
         brdb_data_type= new brdb_value_t<vcl_string>(vcl_string("8bit"));
     else if (img_type_=="ushort")
         brdb_data_type= new brdb_value_t<vcl_string>(vcl_string("16bit"));
@@ -175,7 +162,6 @@ float boxm2_render_tableau::refine_model()
     //set inputs
     vcl_vector<brdb_value_sptr> input;
     input.push_back(brdb_scene);
-    //input.push_back(brdb_thresh);
     input.push_back(brdb_data_type);
 
     //initoutput vector
@@ -187,7 +173,7 @@ float boxm2_render_tableau::refine_model()
 }
 
 //: calls on ray manager to render frame into the pbuffer_
-float boxm2_render_tableau::render_frame()
+float boxm2_render_rgb_tableau::render_frame()
 {
   cl_int status = clEnqueueAcquireGLObjects( *gpu_pro_->get_queue(), 1,
                                              &render_.gl_image()->buffer() , 0, 0, 0);
@@ -200,8 +186,8 @@ float boxm2_render_tableau::render_frame()
   vpgl_camera_double_sptr cam = new vpgl_perspective_camera<double>(cam_);
   brdb_value_sptr brdb_cam = new brdb_value_t<vpgl_camera_double_sptr>(cam);
 
-  //create output image buffer
-  vil_image_view_base_sptr expimg = new vil_image_view<float>(ni_, nj_);
+  //create output image buffer (rgba float image)
+  vil_image_view_base_sptr expimg = new vil_image_view<vil_rgba<float> >(ni_, nj_);
   brdb_value_sptr brdb_expimg = new brdb_value_t<vil_image_view_base_sptr>(expimg);
 
   //create vis image buffer
@@ -211,17 +197,10 @@ float boxm2_render_tableau::render_frame()
 
   //create scene brdbvalue pointer
   brdb_value_sptr brdb_scene = new brdb_value_t<boxm2_scene_sptr>(scene_);
-  brdb_value_sptr brdb_data_type;
-  if (img_type_=="uchar")
-      brdb_data_type= new brdb_value_t<vcl_string>(vcl_string("8bit"));
-  else if (img_type_=="ushort")
-      brdb_data_type= new brdb_value_t<vcl_string>(vcl_string("16bit"));
-
-
+  brdb_value_sptr brdb_data_type = new brdb_value_t<vcl_string>(vcl_string("8bit"));
   brdb_value_sptr brdb_mini = new brdb_value_t<float>(mini_);
   brdb_value_sptr brdb_maxi = new brdb_value_t<float>(maxi_);
   brdb_value_sptr brdb_float_array = new brdb_value_t<bbas_1d_array_float_sptr>(tf_sptr);
-
 
   vcl_vector<brdb_value_sptr> input;
   input.push_back(brdb_scene);
@@ -232,34 +211,32 @@ float boxm2_render_tableau::render_frame()
   input.push_back(brdb_mini);
   input.push_back(brdb_maxi);
   input.push_back(brdb_float_array);
-
+  
   //initoutput vector
   vcl_vector<brdb_value_sptr> output;
 
   //initialize the GPU render process
   gpu_pro_->run(&render_, input, output);
+  //gpu_pro_->finish();
 
   status = clEnqueueReleaseGLObjects( *gpu_pro_->get_queue(), 1, &render_.gl_image()->buffer(), 0, 0, 0);
-  clFinish( *gpu_pro_->get_queue() );
+  clFinish( *render_.command_queue() );
   return gpu_pro_->exec_time();
 }
 
 //: updates given a random frame
-float boxm2_render_tableau::update_frame()
+float boxm2_render_rgb_tableau::update_frame()
 {
     update_count_++;
-
-    if (update_count_%1==0)
-    {
-        //this->compute_convergence();
-    }
     if (update_count_%5==0)
     {
-        //vcl_cout<<"Refining"<<vcl_endl;
+        //vcl_cout<<"Refining  "<<vcl_endl;
         //float time=this->refine_model();
     }
     //pickup a random frame
     int curr_frame = rand.lrand32(0,cam_files_.size()-1);
+    //curr_frame_ = (curr_frame_+1 >= cam_files_.size()) ? 0 : curr_frame_+1;
+    //int curr_frame = curr_frame_;
     vcl_cout<<"Cam "<<cam_files_[curr_frame]<<'\n'
             <<"Image "<<img_files_[curr_frame]<<vcl_endl;
 
@@ -272,22 +249,15 @@ float boxm2_render_tableau::update_frame()
     }
     ifs >> *pcam;
     vpgl_camera_double_sptr cam_sptr(pcam);
-
-
     brdb_value_sptr brdb_cam = new brdb_value_t<vpgl_camera_double_sptr>(cam_sptr);
 
-    //load image from file, create input image buffer
-    vil_image_view_base_sptr loaded_image = boxm2_ocl_util::prepare_input_image(img_files_[curr_frame]);
-    brdb_value_sptr brdb_inimg = new brdb_value_t<vil_image_view_base_sptr>(loaded_image);
+    //load image from file
+    vil_image_view_base_sptr floatimg_sptr =  boxm2_ocl_util::prepare_input_image(img_files_[curr_frame]); 
+    brdb_value_sptr brdb_inimg = new brdb_value_t<vil_image_view_base_sptr>(floatimg_sptr);
 
     //create generic scene
     brdb_value_sptr brdb_scene = new brdb_value_t<boxm2_scene_sptr>(scene_);
-
-    brdb_value_sptr brdb_data_type;
-    if (img_type_=="uchar")
-     brdb_data_type= new brdb_value_t<vcl_string>(vcl_string("8bit"));
-    else if (img_type_=="ushort")
-     brdb_data_type= new brdb_value_t<vcl_string>(vcl_string("16bit"));
+    brdb_value_sptr brdb_data_type = new brdb_value_t<vcl_string>(vcl_string("8bit"));
 
     //set inputs
     vcl_vector<brdb_value_sptr> input;
@@ -300,108 +270,16 @@ float boxm2_render_tableau::update_frame()
     vcl_vector<brdb_value_sptr> output;
 
     //execute gpu_update
-    //choose which process to run (depends on RGBA or not)
-    if (loaded_image->pixel_format()==VIL_PIXEL_FORMAT_RGBA_BYTE)
-      gpu_pro_->run(&update_rgb_, input, output);
-    else
-      gpu_pro_->run(&update_, input, output);
+    gpu_pro_->run(&update_, input, output);
     return gpu_pro_->exec_time();
 }
 
-bool boxm2_render_tableau::compute_convergence()
-{
- unsigned int num_images=6;
- int intervals=(img_files_.size()-1)/num_images;
- float sum=0.0f;
-
- for (unsigned i=0;i<num_images;i++)
- {
-   int index=intervals*i;
-   vcl_stringstream ss;
-   ss << index;
-   vil_image_view<vxl_byte> byte_img(ni_,nj_);
-   vcl_string filename="f:/test"+ss.str()+".png";
-//   render_and_save_image(index,byte_img);
-   if (vul_file::exists(filename.c_str()))
-   {
-     vil_image_view_base_sptr read_img_ptr=vil_load(filename.c_str());
-     if (vil_image_view<vxl_byte> * prev_img=dynamic_cast<vil_image_view<vxl_byte> * >(read_img_ptr.ptr()))
-        sum+=vil_math_ssd<unsigned char, float>(byte_img,*prev_img,sum);
-   }
-   vil_save(byte_img,filename.c_str());
- }
- vcl_ofstream ofile("f:/errlog.txt",vcl_ios::app);
- ofile<<vcl_sqrt(sum/(ni_*nj_*6))<<vcl_endl;
- ofile.close();
- vcl_cout<<"Error -------------> "<<vcl_sqrt(sum/(ni_*nj_*6))<<vcl_endl;
-
- return true;
-}
-
-#if 0 // method "prepare_input_image()" commented out
-
-//: private helper method prepares an input image to be processed by update
-vil_image_view_base_sptr boxm2_render_tableau::prepare_input_image(vcl_string filename)
-{
-  //load from file
-  vil_image_view_base_sptr loaded_image = vil_load(filename.c_str());
-
-  //then it's an RGB image (assumes byte image...)
-  if (loaded_image->nplanes() == 3 || loaded_image->nplanes() == 4)
-  {
-    //load image from file and format it into RGBA
-    vil_image_view_base_sptr n_planes = vil_convert_to_n_planes(4, loaded_image);
-    vil_image_view_base_sptr comp_image = vil_convert_to_component_order(n_planes);
-    vil_image_view<vil_rgba<vxl_byte> >* rgba_view = new vil_image_view<vil_rgba<vxl_byte> >(comp_image);
-
-    //make sure all alpha values are set to 255 (1)
-    vil_image_view<vil_rgba<vxl_byte> >::iterator iter;
-    for (iter = rgba_view->begin(); iter != rgba_view->end(); ++iter) {
-      (*iter) = vil_rgba<vxl_byte>(iter->R(), iter->G(), iter->B(), 255);
-      // was: = vil_rgba<vxl_byte>(iter->grey(), 0, 0, 255);
-    }
-    vil_image_view_base_sptr toReturn(rgba_view);
-    return toReturn;
-    vil_image_view<float>* floatimg = new vil_image_view<float>(loaded_image->ni(), loaded_image->nj(), 1);
-    vil_image_view<float>::iterator fiter;
-    for (iter = rgba_view->begin(), fiter=floatimg->begin(); iter != rgba_view->end(); ++iter, ++fiter)
-      (*fiter) = (float) (iter->R()/255.0f);
-    vil_image_view_base_sptr toReturn(floatimg);
-    return toReturn;
-  }
-
-  //else if loaded planes is just one...
-  if (loaded_image->nplanes() == 1)
-  {
-    vil_image_view<float>* floatimg = new vil_image_view<float>(loaded_image->ni(), loaded_image->nj(), 1);
-    if (vil_image_view<vxl_byte> *img_byte = dynamic_cast<vil_image_view<vxl_byte>*>(loaded_image.ptr()))
-    {
-      vil_convert_stretch_range_limited(*img_byte, *floatimg, vxl_byte(0), vxl_byte(255), 0.0f, 1.0f);
-    }
-    else if (vil_image_view<unsigned short> *img_byte = dynamic_cast<vil_image_view<unsigned short>*>(loaded_image.ptr()))
-    {
-      vil_convert_stretch_range_limited(*img_byte, *floatimg,(unsigned short)30500,(unsigned short)32500,  0.0f, 1.0f); //: hardcoded to be fixed.
-    }
-    else {
-      vcl_cerr << "Failed to load image " << filename << '\n';
-      return 0;
-    }
-    vil_image_view_base_sptr toReturn(floatimg);
-    return toReturn;
-  }
-
-  //otherwise it's messed up, return a null pointer
-  vcl_cerr<<"Failed to recognize input image type "<< filename << '\n';
-  return 0;
-}
-#endif // 0
-
 //: private helper method to init_clgl stuff (gpu processor)
-bool boxm2_render_tableau::init_clgl()
+bool boxm2_render_rgb_tableau::init_clgl()
 {
   //get relevant blocks
   vcl_cout<<"Data Path: "<<scene_->data_path()<<vcl_endl;
-
+  
   //create cache, grab singleton instance
   boxm2_lru_cache::create(scene_.ptr());
   cache_ = boxm2_cache::instance();
@@ -431,37 +309,27 @@ bool boxm2_render_tableau::init_clgl()
                                       CL_MEM_WRITE_ONLY,
                                       pbuffer_,
                                       &status);
-  exp_img_ = new bocl_mem(gpu_pro_->context(), /*(void*) pbuffer_*/ NULL, RoundUp(ni_,8)*RoundUp(nj_,8)*sizeof(GLubyte)*4, "exp image (gl) buffer");
+  exp_img_ = new bocl_mem(gpu_pro_->context(), /*(void*) pbuffer_*/ NULL, ni_*nj_*sizeof(GLubyte)*4, "exp image (gl) buffer");
   exp_img_->set_gl_buffer(clgl_buffer_);
-  vcl_string render_opts;
-  if (img_type_=="uchar")
-    render_opts += " -D MOG_TYPE_8";
-  else if (img_type_=="ushort")
-    render_opts += " -D MOG_TYPE_16";
-
-
+  
   //initialize the GPU render process
-  render_.init_kernel(&gpu_pro_->context(), &gpu_pro_->devices()[0],render_opts);
+  vcl_string render_opts = "";
+  render_.init_kernel(&gpu_pro_->context(), &gpu_pro_->devices()[0], render_opts);
+  //render_.set_image(exp_img_);
   render_.set_gl_image(exp_img_);
-  render_rgb_.init_kernel(&gpu_pro_->context(), &gpu_pro_->devices()[0], render_opts);
-  //render_rgb_.set_image(exp_img_);
 
   //initlaize gpu update process
-  vcl_string update_opts=" -D MOG_TYPE_8 ";
-  if (img_type_=="ushort")
-    update_opts = " -D MOG_TYPE_16 ";
-
-  update_.init_kernel(&gpu_pro_->context(), &gpu_pro_->devices()[0],update_opts);
-  update_rgb_.init_kernel(&gpu_pro_->context(), &gpu_pro_->devices()[0], update_opts);
+  vcl_string update_opts = "";
+  update_.init_kernel(&gpu_pro_->context(), &gpu_pro_->devices()[0], update_opts);
 
   //initialize refine process
-  refine_.init_kernel(&gpu_pro_->context(), &gpu_pro_->devices()[0],update_opts);
+  refine_.init_kernel(&gpu_pro_->context(), &gpu_pro_->devices()[0], "");
 
   return true;
 }
 
 //: private helper method to create clgl context using cl_context properties
-cl_context boxm2_render_tableau::create_clgl_context()
+cl_context boxm2_render_rgb_tableau::create_clgl_context()
 {
   //init glew
   GLenum err = glewInit();
---
+++ contrib/brl/bseg/boxm2/view/boxm2_render_rgb_tableau.h
@@ -0,0 +1,128 @@
+// This is brl/bseg/boxm2/view/boxm2_render_rgb_tableau.h
+#ifndef boxm2_render_rgb_tableau_h
+#define boxm2_render_rgb_tableau_h
+//:
+// \file
+// \brief A tableau to view octree models in real time
+// \author Vishal Jain
+// \date Aug 11, 2010
+
+#include "boxm2_include_glew.h"
+#include "boxm2_cam_tableau.h"
+
+//vgui includes
+#include <vgui/vgui_gl.h>
+#include <vgui/vgui_event_condition.h>
+#include <vgui/vgui_statusbar.h>
+#include <vgui/vgui_tableau_sptr.h>
+
+//utilities
+#include <vpgl/vpgl_perspective_camera.h>
+#include <bocl/bocl_utils.h>
+#include <vil/vil_convert.h>
+#include <vil/vil_image_view_base.h>
+#include <vil/vil_image_view.h>
+#include <vil/vil_math.h>
+#include <vil/vil_load.h>
+#include <vil/vil_save.h>
+#include <vnl/vnl_random.h>
+
+//boxm2 includes
+#include <boxm2/boxm2_scene.h>
+#include <boxm2/boxm2_data.h>
+#include <boxm2/ocl/boxm2_opencl_processor.h>
+#include <boxm2/ocl/pro/boxm2_opencl_render_rgb_process.h>
+#include <boxm2/ocl/pro/boxm2_opencl_update_rgb_process.h>
+#include <boxm2/ocl/pro/boxm2_opencl_refine_process.h>
+#include <boxm2/io/boxm2_lru_cache.h>
+#include <bpro/core/bbas_pro/bbas_1d_array_float.h>
+
+//brdb stuff
+#include <brdb/brdb_value.h>
+
+class boxm2_render_rgb_tableau : public boxm2_cam_tableau
+{
+ public:
+  boxm2_render_rgb_tableau();
+  virtual ~boxm2_render_rgb_tableau() {}
+  
+  //: initialize tableau with scene_file, viewport size, initial cam,  
+  bool init(vcl_string scene_file,
+            unsigned ni,
+            unsigned nj,
+            vpgl_perspective_camera<double>* cam,
+            vcl_vector<vcl_string> cam_files,
+            vcl_vector<vcl_string> img_files);
+  
+  //: virtual function handles mouse and keyboard actions
+  virtual bool handle( vgui_event const& e );
+
+  //;  set the GL buffer which needs to be displayed.
+  void set_glbuffer(GLuint  pbuffer) { pbuffer_ = pbuffer; }
+  void set_statusbar(vgui_statusbar* status) { status_ = status; }
+
+ protected:
+
+  //vector of image files, vector of 
+  vcl_vector<vcl_string> cam_files_;
+  vcl_vector<vcl_string> img_files_;
+  vcl_string img_type_;
+
+  //: gpu processor and render_process, update_process
+  boxm2_opencl_processor* gpu_pro_;
+  boxm2_opencl_render_rgb_process render_;
+  boxm2_opencl_update_rgb_process update_; 
+  boxm2_opencl_refine_process refine_;
+  
+  //: Boxm2 Scene
+  boxm2_scene_sptr scene_;
+  boxm2_cache*     cache_; 
+  unsigned ni_;
+  unsigned nj_;
+  vgui_statusbar* status_;
+  
+  //: shared GL_CL image buffer
+  GLuint pbuffer_;
+  cl_mem clgl_buffer_;
+  bocl_mem* exp_img_;
+
+  //: update count, 
+  bool do_update_;
+  int update_count_; 
+  vnl_random rand;
+
+  //--Render, update, refine, save helper methods ------------------------------
+  //func to render frame on GPU (returns gpu time)
+  float render_frame();
+  float update_frame(); 
+  float refine_model();
+  bool save_model(); 
+
+  //-- HELPER INIT Methods and vars---------------------------------------------
+  //: create cl_gl_context
+  vil_image_view_base_sptr prepare_input_image(vcl_string filename);
+  cl_context create_clgl_context();
+  bool init_clgl();
+  bool do_init_ocl;
+  
+  int curr_frame_; 
+  
+  //-- transfer function for render
+  float mini_;
+  float maxi_;
+  float tf[256];
+  bbas_1d_array_float_sptr tf_sptr;
+};
+
+//: declare smart pointer
+typedef vgui_tableau_sptr_t<boxm2_render_rgb_tableau> boxm2_render_rgb_tableau_sptr;
+
+//: Create a smart-pointer to a boxm2_render_rgb_tableau tableau.
+struct boxm2_render_rgb_tableau_new : public boxm2_render_rgb_tableau_sptr
+{
+  //: Constructor - create an empty vgui_easy3D_tableau.
+  typedef boxm2_render_rgb_tableau_sptr base;
+  boxm2_render_rgb_tableau_new() : base( new boxm2_render_rgb_tableau ) { }
+};
+
+#endif // boxm2_render_rgb_tableau_h
