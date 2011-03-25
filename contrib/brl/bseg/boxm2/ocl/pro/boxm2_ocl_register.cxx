2263415c5fe42cd97a218ccf42a11704b684ea21
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-03-25 12:21:50 -0400

    "//:" is for Doxygen structured comments
    SVN-Revision: 31545

--- contrib/brl/bseg/boxm2/ocl/pro/boxm2_ocl_register.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/boxm2_ocl_register.cxx
@@ -4,7 +4,6 @@
 #include <bprb/bprb_batch_process_manager.h>
 #include <bprb/bprb_func_process.h>
 
-
 #include "boxm2_ocl_processes.h"
 #include <boxm2/ocl/boxm2_opencl_cache.h>
 
@@ -15,21 +14,20 @@ void boxm2_ocl_register::register_datatype()
 
 void boxm2_ocl_register::register_process()
 {
-  //: utilites
+  // utilities
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_ocl_render_expected_image_process, "boxm2OclRenderExpectedImageProcess");
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_ocl_render_expected_depth_process, "boxm2OclRenderExpectedDepthProcess");
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_ocl_update_process, "boxm2OclUpdateProcess");
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_ocl_refine_process, "boxm2OclRefineProcess");
 
-
-  //: research
+  // research
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_ocl_update_histogram_process, "boxm2OclUpdateHistogramProcess");
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_ocl_batch_probability_process, "boxm2OclBatchProbabilityProcess");
-  
-  //: tools 
+
+  // tools
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_ocl_query_hist_data_process, "boxm2OclQueryHistDataProcess");
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_ocl_query_cell_data_process, "boxm2OclQueryCellDataProcess");
 
-  //: admin 
+  // admin
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_create_opencl_cache_process, "boxm2CreateOpenclCacheProcess");
 }
