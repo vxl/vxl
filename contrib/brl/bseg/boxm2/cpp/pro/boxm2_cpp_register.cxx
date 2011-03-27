6f412369aa5d394eae6079f17c677255a03099c7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-03-27 15:42:31 -0400

    minor typo corrected
    SVN-Revision: 31565

--- contrib/brl/bseg/boxm2/cpp/pro/boxm2_cpp_register.cxx
+++ contrib/brl/bseg/boxm2/cpp/pro/boxm2_cpp_register.cxx
@@ -4,7 +4,6 @@
 #include <bprb/bprb_batch_process_manager.h>
 #include <bprb/bprb_func_process.h>
 
-
 #include "boxm2_cpp_processes.h"
 
 void boxm2_cpp_register::register_datatype()
@@ -13,7 +12,7 @@ void boxm2_cpp_register::register_datatype()
 
 void boxm2_cpp_register::register_process()
 {
-  //: utilites
+  // utilities
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_cpp_render_expected_image_process, "boxm2CppRenderExpectedImageProcess");
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_cpp_update_image_process, "boxm2CppUpdateImageProcess");
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, boxm2_cpp_refine_process2, "boxm2CppRefineProcess");
