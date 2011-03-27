026dedd0337f6115381e076726bb62df4af66d75
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-03-27 15:26:14 -0400

    SVN-Revision: 31562

--- contrib/brl/bbas/imesh/algo/imesh_generate_mesh.h
+++ contrib/brl/bbas/imesh/algo/imesh_generate_mesh.h
@@ -3,7 +3,7 @@
 #define imesh_generate_mesh_h_
 //:
 // \file
-// \brief mesh generation functions 
+// \brief mesh generation functions
 // \author J.L. Mundy
 // \date March 16, 2011
 //
@@ -15,10 +15,11 @@
 #include <imesh/imesh_mesh.h>
 #include <vcl_vector.h>
 #include <vgl/vgl_fwd.h>
+
 //: Fill the convex hull with triangles, constrained by the line segments
-void 
+void
 imesh_generate_mesh_2d(vcl_vector<vgl_point_2d<double> > const& convex_hull,
                        vcl_vector< vgl_line_segment_2d<double> > const & segs,
                        imesh_mesh& mesh);
 
-#endif // imesh_algo_generate_mesh_h_
+#endif // imesh_generate_mesh_h_
