3f3b1f71d0636d9e8d0aa371ef1cec7869df05f9
Andrew Miller <andrew-miller@users.sourceforge.net>
2011-03-28 18:13:58 -0400

    texturing mesh from multiple sources
    SVN-Revision: 31575

--- contrib/brl/bseg/boxm2/pro/processes/boxm2_export_textured_mesh_process.cxx
+++ contrib/brl/bseg/boxm2/pro/processes/boxm2_export_textured_mesh_process.cxx
@@ -9,6 +9,7 @@
 
 #include <vcl_fstream.h>
 #include <vul/vul_file.h>
+#include <vul/vul_timer.h>
 #include <boxm2/boxm2_scene.h>
 #include <boxm2/boxm2_util.h>
 
@@ -97,7 +98,7 @@ bool boxm2_export_textured_mesh_process(bprb_func_process& pro)
     for(int j=0; j<nj; ++j) {
         
         //grab the ray from the camera
-        vgl_ray_3d<double> ray = pcam->backproject_ray(i,j);
+        vgl_ray_3d<double> ray = pcam->backproject_ray( (double) i+.5, (double) j+.5);
         
         //ray o
         float ray_ox = ray.origin().x();
@@ -120,8 +121,6 @@ bool boxm2_export_textured_mesh_process(bprb_func_process& pro)
         (*z_img)(i,j) = ray_oz - ray_t*dray_z; 
     }
   }
-  //vcl_string xyzname = out_dir + "/xyz.tiff";
-  //vil_save(*z_img, xyzname.c_str()); 
   
   //////////////////////////////////////////////////////////////////////////////
   // Generate 3d mesh from depth image
@@ -158,6 +157,7 @@ bool boxm2_export_textured_mesh_process(bprb_func_process& pro)
   ////////////////////////////////////////////////////////////////////////////////
   //// normalize mesh world points to fit in the image_bb from above
   ////////////////////////////////////////////////////////////////////////////////
+  vul_timer t; 
   //Get XY bounding box for image
   //vgl_box_2d<double> image_bb = vpgl_project::project_bounding_box(*pcam, scene->bounding_box());
   //vcl_cout<<"Image bounding box (scene xy box): "<<image_bb<<vcl_endl;
@@ -194,9 +194,11 @@ bool boxm2_export_textured_mesh_process(bprb_func_process& pro)
     verts[iv][1] = scene_y; 
     verts[iv][2] = scene_z; 
   }
-  
+  vcl_cout<<mesh.has_tex_coords()<<vcl_endl;
+    
   ////////////////////////////////////////////////////////////////////////////////
-  //// Gather cameras and iamges that will contribute to the texture
+  // BEGIN TEXTURE MAPPING
+  // Gather cameras and iamges that will contribute to the texture
   ////////////////////////////////////////////////////////////////////////////////
   vcl_vector<vcl_string> camfiles = boxm2_util::camfiles_from_directory(cam_dir); 
   vcl_vector<vcl_string> imfiles  = boxm2_util::images_from_directory(cam_dir); 
@@ -216,11 +218,93 @@ bool boxm2_export_textured_mesh_process(bprb_func_process& pro)
   
   
   ////////////////////////////////////////////////////////////////////////////////
+  // For each Face: 
+  //   determine which image is closest, store face index in a map[image_str, vector<int>face]
+  ////////////////////////////////////////////////////////////////////////////////
+  mesh.compute_vertex_normals_from_faces(); 
+  imesh_face_array_base& faces = mesh.faces(); 
+  unsigned nfaces = mesh.num_faces();
+  vcl_map<vcl_string, vcl_vector<unsigned> > app_faces; 
+  for(unsigned iface = 0; iface<nfaces; ++iface) 
+  {
+    vgl_vector_3d<double>& normal = faces.normal(iface); 
+    
+    //find camera with the closest look vector to this normal
+    unsigned closeIdx = boxm2_util::find_nearest_cam(normal, cameras); 
+    vpgl_perspective_camera<double>* closest = cameras[closeIdx]; 
+    vcl_string im_name = imfiles[closeIdx]; 
+    
+    //grab appropriate face list (create it if it's not there)
+    vcl_map<vcl_string, vcl_vector<unsigned> >::iterator iter = app_faces.find(im_name); 
+    if( iter == app_faces.end() ) {
+      vcl_vector<unsigned> faceList; 
+      faceList.push_back(iface); 
+      app_faces[im_name] = faceList; 
+    }
+    else {
+      app_faces[im_name].push_back(iface); 
+    }
+  }
+  
+  ////////////////////////////////////////////////////////////////////////////////
+  // For each image/appearance: 
+  //   - create a vert list 
+  //   - create a face list  
+  //   - create a sub mesh that is textured
+  ////////////////////////////////////////////////////////////////////////////////
+  vcl_map<vcl_string, vcl_vector<imesh_mesh&> > meshes;  
+  vcl_map<vcl_string, vcl_vector<unsigned> >::iterator apps; 
+  for(apps = app_faces.begin(); apps != app_faces.end(); ++apps) 
+  {
+    
+    
+    
+    //instantiate vertex array
+    imesh_vertex_array<3>* vs = new imesh_vertex_array<3>();
+    vcl_auto_ptr<imesh_vertex_array_base> v3(verts3);
+    
+    //instantiate face array
+    imesh_face_array_base* fs = new imesh_face_array_base();
+    vcl_auto_ptr<imesh_face_array_base> f3(verts3);
+    
+    //create the mesh object
+    imesh_mesh subMesh(v3, f3); 
+    
+    //store mesh object 
+  }
+  
+  
+  ////////////////////////////////////////////////////////////////////////////////
   //// for each triangle, 
   ////   if surface normal is less than X degrees off horizontal
   ////      project triangle onto image, store triangle points (see if there are functions for this in imesh)
   ////////////////////////////////////////////////////////////////////////////////
   
+  vcl_vector<vcl_string> tex_sources(nfaces, "");  //each face gets a 
+  vcl_vector<vgl_point_2d<double> > tex_coords(nverts, vgl_point_2d<double>(0.0,0.0)); 
+  for(unsigned iv = 0; iv<nverts; ++iv)
+  {
+    //get face normal
+    vgl_vector_3d<double>& normal = verts.normal(iv); 
+    
+    //find camera with the closest look vector to this normal
+    unsigned closeIdx = boxm2_util::find_nearest_cam(normal, cameras); 
+    vpgl_perspective_camera<double>* closest = cameras[closeIdx]; 
+    //tex_sources[iv] = imfiles[closeIdx]; 
+
+    //project the vertex onto the camera, store the texture coordinate
+    double x = verts[iv][0];
+    double y = verts[iv][1];
+    double z = verts[iv][2]; 
+    double u,v; 
+    closest->project(x, y, z, u, v);
+      
+    //store the tex_coordinate
+    vgl_point_2d<double> uv(u/nj,v/ni); 
+    tex_coords[iv] = uv; 
+  }
+  mesh.set_tex_coords(tex_coords); 
+  vcl_cout<<"Mesh has tex_coordinates "<<mesh.has_tex_coords()<<vcl_endl;
   
   ////////////////////////////////////////////////////////////////////////////////
   //// Write out in VRML format
@@ -230,6 +314,7 @@ bool boxm2_export_textured_mesh_process(bprb_func_process& pro)
   imesh_write_vrml(os, mesh);
   os.close();
   
+  vcl_cout<<"Texture Mapping Mesh Time: "<<t.all()<<"ms"<<vcl_endl;
   
   return true;
 }
