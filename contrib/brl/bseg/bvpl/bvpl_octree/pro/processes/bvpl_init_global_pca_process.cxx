b2231b6a999f02ae29ea73c4003e772830206020
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-03-29 16:55:07 -0400

    added processes related to global_pca
    SVN-Revision: 31608

---
+++ contrib/brl/bseg/bvpl/bvpl_octree/pro/processes/bvpl_init_global_pca_process.cxx
@@ -0,0 +1,48 @@
+//:
+// \brief A process to init auxiliary scenes and other variables 
+// \file
+// \author Isabel Restrepo
+// \date 23-Mar-2011
+
+#include <bprb/bprb_func_process.h>
+#include <bprb/bprb_parameters.h>
+
+#include <brdb/brdb_value.h>
+
+#include <bvpl/bvpl_octree/bvpl_global_pca.h>
+
+//:global variables
+namespace bvpl_init_global_pca_process_globals 
+{
+  const unsigned n_inputs_ = 1;
+  const unsigned n_outputs_ = 0;
+}
+
+
+//:sets input and output types
+bool bvpl_init_global_pca_process_cons(bprb_func_process& pro)
+{
+  using namespace bvpl_init_global_pca_process_globals ;
+  
+  vcl_vector<vcl_string> input_types_(n_inputs_);
+  input_types_[0] = "vcl_string"; // path to pca_global_info file
+  
+  vcl_vector<vcl_string> output_types_(n_outputs_);
+  
+  return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
+}
+
+
+//:the process
+bool bvpl_init_global_pca_process(bprb_func_process& pro)
+{
+  using namespace bvpl_init_global_pca_process_globals;
+  
+  //get inputs
+  vcl_string pca_dir = pro.get_input<vcl_string>(0);
+  
+  bvpl_global_pca<125> global_pca(pca_dir);
+  global_pca.init();
+  
+  return true;
+} 
