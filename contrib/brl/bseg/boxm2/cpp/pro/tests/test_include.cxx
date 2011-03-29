f3cdd5abf6fd33edc8c407d376c6aa1246e35e69
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-03-29 11:41:22 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 31590

--- contrib/brl/bseg/boxm2/cpp/pro/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/cpp/pro/tests/test_include.cxx
@@ -1,7 +1,9 @@
 #include <boxm2/cpp/pro/boxm2_cpp_change_detection_process.h>
 #include <boxm2/cpp/pro/boxm2_cpp_change_detection_uncertainity_process.h>
 #include <boxm2/cpp/pro/boxm2_cpp_process_base.h>
+#include <boxm2/cpp/pro/boxm2_cpp_processes.h>
 #include <boxm2/cpp/pro/boxm2_cpp_refine_process.h>
+#include <boxm2/cpp/pro/boxm2_cpp_register.h>
 #include <boxm2/cpp/pro/boxm2_cpp_render_process.h>
 #include <boxm2/cpp/pro/boxm2_cpp_update_process.h>
 
