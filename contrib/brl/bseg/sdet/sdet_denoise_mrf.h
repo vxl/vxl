339301fed21134903532fe5523bc601883038a63
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-04-02 04:31:42 -0400

    renamed accessor "ouput_valid()" to "output_valid()"
    SVN-Revision: 31651

--- contrib/brl/bseg/sdet/sdet_denoise_mrf.h
+++ contrib/brl/bseg/sdet/sdet_denoise_mrf.h
@@ -6,19 +6,19 @@
 // \file
 // \brief a processor applying a binary mrf denoise algorithm
 //
-//  
-//  This algorithm selectively smooths the image based on 
+//
+//  This algorithm selectively smooths the image based on
 //  a variance value at each pixel. The smoothing is carried out
 //  by a MRF with binary cliques, where the clique energy weights
-//  are controlled in part by the variance image. 
-//  
-// 
+//  are controlled in part by the variance image.
+//
+//
 // \author
 //  J.L. Mundy - March 22, 2011
 //
 //
 // The mrf energy minimization is carried out with a closed form algorithm
-// i.e. non-iterative as compared with belief propagation, 
+// i.e. non-iterative as compared with belief propagation,
 // which requires iterations of message passing. The algorithm is based in
 // part on the paper:
 // "Continuous MRF-based image denoising with a closed form solution,"
@@ -41,10 +41,13 @@
 class sdet_denoise_mrf : public sdet_denoise_mrf_params
 {
  public:
-  //:Constructors/destructor
+  // === Constructors/destructor ===
+
   sdet_denoise_mrf(sdet_denoise_mrf_params& imp);
   ~sdet_denoise_mrf();
-  //: Process methods
+
+  // === Process methods ===
+
   void set_image(vil_image_resource_sptr const& resource){in_resc_ = resource;}
   void set_variance(vil_image_resource_sptr const& var_resc)
     {var_resc_ = var_resc;}
@@ -56,17 +59,20 @@ class sdet_denoise_mrf : public sdet_denoise_mrf_params
   //: the full denoising process
   bool denoise();
 
-  //: Accessors
+  // === Accessors ===
+
   vil_image_resource_sptr output(){return out_resc_;}
-  bool ouput_valid() {return output_valid_;}
+  bool output_valid() {return output_valid_;}
   vnl_sparse_matrix<double>& incidence_matrix()  {return W_mat_;}
   vnl_sparse_matrix<double>& diag_matrix()  {return D_mat_;}
   vil_image_resource_sptr Dimgr();
   vnl_sparse_matrix<double>& diag_inv_sqrt()  {return D_inv_sqrt_;}
   vnl_sparse_matrix<double>& L() {return L_mat_;}
   vnl_vector<double>& F()  {return F_;}
+
  protected:
-  //:protected methods
+  // === protected methods ===
+
   //: compute the clique energy weight between two image locations
   double weight(unsigned i0, unsigned j0, unsigned i1, unsigned j1,
                 vil_image_view<float> const& depth,
@@ -75,7 +81,9 @@ class sdet_denoise_mrf : public sdet_denoise_mrf_params
   void compute_laplacian_matrix();
   //:the solution vector
   void compute_F();
-  //:members
+
+  // === members ===
+
   bool output_valid_;      //process state flag
   double sigma_sq_inv_;
   vil_image_resource_sptr in_resc_;
