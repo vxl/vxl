2745dc749e6791614ec8666a87ecaa9bc7b09802
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-04-03 06:13:40 -0400

    no need to include vsol_point_2d.h since the smart pointer definition is in vsol_point_2d_sptr.h
    SVN-Revision: 31667

--- contrib/gel/vsol/Templates/vcl_map+uint.vbl_smart_ptr+vsol_point_2d--.cxx
+++ contrib/gel/vsol/Templates/vcl_map+uint.vbl_smart_ptr+vsol_point_2d--.cxx
@@ -1,3 +1,4 @@
+#include <vsol/vsol_point_2d_sptr.h>
 #include <vcl_map.txx>
-#include <vsol/vsol_point_2d.h>
-VCL_MAP_INSTANTIATE(unsigned int, vbl_smart_ptr<vsol_point_2d>, vcl_less<unsigned int>)
+
+VCL_MAP_INSTANTIATE(unsigned int, vsol_point_2d_sptr, vcl_less<unsigned int>)
