c28ef111b8f9bd7acc9b24899cde2ecdc5629e41
Andrew Miller <andrew-miller@users.sourceforge.net>
2011-04-10 19:09:16 -0400

    moved blk_info call to immediately after blk call
    SVN-Revision: 31771

--- contrib/brl/bseg/boxm2/ocl/algo/boxm2_ocl_render_expected_image_function.cxx
+++ contrib/brl/bseg/boxm2/ocl/algo/boxm2_ocl_render_expected_image_function.cxx
@@ -52,9 +52,9 @@ void render_expected_image( boxm2_scene_sptr & scene,
         //write the image values to the buffer
         vul_timer transfer;
         bocl_mem* blk       = opencl_cache->get_block(*id);
+        bocl_mem* blk_info  = opencl_cache->loaded_block_info();
         bocl_mem* alpha     = opencl_cache->get_data<BOXM2_ALPHA>(*id);
         bocl_mem* mog       = opencl_cache->get_data(*id,data_type);
-        bocl_mem * blk_info  = opencl_cache->loaded_block_info();
         transfer_time += (float) transfer.all();
 
         ////3. SET args
