5a5478ef204dd912fbd8a1af523fad0b9977cca7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-04-20 10:26:47 -0400

    fixed two dashboard test failures
    SVN-Revision: 31857

--- core/vil/algo/tests/test_driver.cxx
+++ core/vil/algo/tests/test_driver.cxx
@@ -36,6 +36,8 @@ DECLARE( test_algo_suppress_non_max_edges );
 DECLARE( test_algo_checker_board );
 DECLARE( test_algo_quad_distance_function );
 DECLARE( test_algo_flood_fill );
+DECLARE( test_algo_trace_4con_boundary );
+DECLARE( test_algo_trace_8con_boundary );
 
 void
 register_tests()
@@ -76,6 +78,8 @@ register_tests()
   REGISTER( test_algo_checker_board );
   REGISTER( test_algo_quad_distance_function );
   REGISTER( test_algo_flood_fill );
+  REGISTER( test_algo_trace_4con_boundary );
+  REGISTER( test_algo_trace_8con_boundary );
 }
 
 DEFINE_MAIN;
