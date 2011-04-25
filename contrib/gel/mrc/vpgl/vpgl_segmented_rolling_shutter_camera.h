e4a76739befd29c72aa2d9a8230745516c65f4a6
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-04-25 17:23:09 -0400

    indentation
    SVN-Revision: 31923

--- contrib/gel/mrc/vpgl/vpgl_segmented_rolling_shutter_camera.h
+++ contrib/gel/mrc/vpgl/vpgl_segmented_rolling_shutter_camera.h
@@ -17,22 +17,21 @@
 template <class T>
 class vpgl_segmented_rolling_shutter_camera : public vpgl_rolling_shutter_camera<T>
 {
-    
  public:
   typedef typename vcl_map<unsigned int ,vpgl_perspective_camera<T> >  maptype;
   typedef typename maptype::iterator  iterator;
   typedef typename maptype::const_iterator const_iterator;
 
-  //: Iterators
+  // === Iterators ===
+
   iterator begin() { return cam_map_.begin(); }
   iterator end  () { return cam_map_.end(); }
 
   const_iterator begin() const { return cam_map_.begin();}
   const_iterator end  () const { return cam_map_.end(); }
 
-     //: default constructor
+  //: default constructor
   vpgl_segmented_rolling_shutter_camera();
-
 
   //: Constructor from a rational camera and a geographic origin
   vpgl_segmented_rolling_shutter_camera(maptype camera_segments);
@@ -45,41 +44,42 @@ class vpgl_segmented_rolling_shutter_camera : public vpgl_rolling_shutter_camera
   //  See Prototype pattern
   virtual vpgl_segmented_rolling_shutter_camera<T>* clone(void) const;
 
-        // Mutators/Accessors
+  // === Mutators/Accessors ===
+
  //: project function for a point
   void project(const T x, const T y, const T z, T& u, T& v);
 
   //: The generic camera interface. u represents image column, v image row.
   virtual void project(const T x, const T y, const T z, T& u, T& v) const;
 
-
- // Interface for vnl
+  // === Interface for vnl ===
 
   //: Project a world point onto the image
   virtual vnl_vector_fixed<T, 2> project(vnl_vector_fixed<T, 3> const& world_point) const;
 
-        // Interface for vgl
+  // === Interface for vgl ===
 
   //: Project a world point onto the image
   virtual vgl_point_2d<T> project(vgl_point_3d<T> world_point) const;
 
- //: project function for a point
+  //: project function for a point
   bool cam_index(T v, unsigned int & index);
 
-  unsigned int num_segments(){return this->cam_map_.size();}
+  unsigned int num_segments() const {return this->cam_map_.size();}
 
   //: function to add a new camera
   bool add_camera(unsigned int v, vpgl_perspective_camera<double> cam);
 
   //: function to update an existing camera
   bool update_camera(unsigned int v, vpgl_perspective_camera<double> cam);
+
   //: print the camera parameters
   virtual void print(vcl_ostream& s = vcl_cout) const;
 
   //: save to file (the lvcs is after the global rational camera parameters)
   virtual bool save(vcl_string cam_path);
 
-  // binary IO
+  // === binary IO ===
 
   //: Binary save self to stream.
   virtual void b_write(vsl_b_ostream &os) const;
@@ -92,13 +92,13 @@ class vpgl_segmented_rolling_shutter_camera : public vpgl_rolling_shutter_camera
 };
 
 //: Creates a local rational camera from a file
-  // \relatesalso vpgl_segmented_rolling_shutter_camera
+// \relatesalso vpgl_segmented_rolling_shutter_camera
 template <class T>
-  vpgl_segmented_rolling_shutter_camera<T>* read_segmented_rolling_shutter_camera(vcl_string cam_path);
+vpgl_segmented_rolling_shutter_camera<T>* read_segmented_rolling_shutter_camera(vcl_string cam_path);
 
 //: Creates a local rational camera from a file
-  // \relatesalso vpgl_segmented_rolling_shutter_camera
- template <class T>
+// \relatesalso vpgl_segmented_rolling_shutter_camera
+template <class T>
 vpgl_segmented_rolling_shutter_camera<T>* read_segmented_rolling_shutter_camera(vcl_istream& istr);
 
 //: Write to stream
@@ -113,6 +113,4 @@ vcl_istream& operator>>(vcl_istream& is, vpgl_segmented_rolling_shutter_camera<T
 
 #define VPGL_SEGMENTED_ROLLING_SHUTTER_CAMERA_INSTANTIATE(T) extern "please include vgl/vpgl_segmented_rolling_shutter_camera.txx first"
 
-
 #endif // vpgl_segmented_rolling_shutter_camera_h_
-
