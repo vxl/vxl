363786f8ffe33b80d2bb4e2d79defece43951907
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-04-28 12:34:17 -0400

    added missing #include
    SVN-Revision: 31945

--- contrib/mul/msm/msm_shape_perturber.h
+++ contrib/mul/msm/msm_shape_perturber.h
@@ -1,5 +1,9 @@
-#ifndef __msm_shape_perturber_h__
-#define __msm_shape_perturber_h__
+#ifndef msm_shape_perturber_h__
+#define msm_shape_perturber_h__
+//:
+// \file
+// \brief Class for perturbing shapes from ground truth
+// \author Patrick Sauer
 
 #include <vnl/vnl_random.h>
 #include <vnl/vnl_vector.h>
@@ -8,10 +12,7 @@
 #include <msm/msm_shape_instance.h>
 #include <msm/msm_aligner.h>
 
-//:
-// \file
-// \brief Class for perturbing shapes from ground truth
-// \author Patrick Sauer
+#include <vcl_cstddef.h> // for std::size_t
 
 class msm_shape_perturber
 {
@@ -24,8 +25,8 @@ class msm_shape_perturber
 
     msm_shape_instance gt_inst_;
 
-    vnl_random rand_;  
-  
+    vnl_random rand_;
+
     vnl_vector<double> max_dpose_;
     vnl_vector<double> max_dparams_;
 
@@ -33,28 +34,28 @@ class msm_shape_perturber
     vnl_vector<double> dparams_,  inv_dparams_;
     vnl_vector<double> all_, dall_, inv_dall_;
 
-    size_t n_pose_;
-    size_t n_params_;
+    vcl_size_t n_pose_;
+    vcl_size_t n_params_;
 
     double trunc_normal_sample( vnl_random& rand1, double sd, double max_d );
-    double random_value( vnl_random& rand, double max_v, double rel_gauss_sd );    
-  
+    double random_value( vnl_random& rand, double max_v, double rel_gauss_sd );
+
   public:
-    
+
     msm_shape_perturber();
-  
+
     void perturb( const msm_points& );
 
     void set_model( const msm_shape_model& );
     void set_max_dp( const vnl_vector<double>&, const vnl_vector<double>& = vnl_vector<double>() );
     void set_rel_gauss( double );
-    void set_seed( size_t );
+    void set_seed( vcl_size_t );
 
-    size_t seed() const;
+    vcl_size_t seed() const;
     double rel_gauss() const;
 
     const vnl_vector<double>& max_d_pose() const;
-    const vnl_vector<double>& max_d_params() const;    
+    const vnl_vector<double>& max_d_params() const;
 
     const vnl_vector<double>& gt_params() const;
     const vnl_vector<double>& gt_pose() const;
@@ -73,7 +74,7 @@ class msm_shape_perturber
 
     //: return pose displacement needed to go grom perturbed points back to ground truth
     const vnl_vector<double>& inv_d_pose() const;
-   
+
     //: return pose+params displacement needed to go grom perturbed points back to ground truth
     const vnl_vector<double>& inv_d_all() const;
 
@@ -81,5 +82,4 @@ class msm_shape_perturber
     const msm_points& points() const;
 };
 
-#endif
-
+#endif // msm_shape_perturber_h__
