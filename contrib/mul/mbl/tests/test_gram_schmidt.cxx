1c92d99d4f418815d52fa698fd8f5426438a5cca
Patrick Sauer <psauer55@users.sourceforge.net>
2011-04-28 09:24:53 -0400

    added test for overload
    SVN-Revision: 31942

--- contrib/mul/mbl/tests/test_gram_schmidt.cxx
+++ contrib/mul/mbl/tests/test_gram_schmidt.cxx
@@ -20,7 +20,7 @@ void test_gram_schmidt()
     basis.set_column(2,v2);
     vnl_matrix<double > norm_basis(4,3);
 
-    mbl_mod_gram_schmidt(basis,norm_basis);
+    mbl_mod_gram_schmidt(basis, norm_basis);
     vnl_vector<double >  e0,e1,e2;
     e0 = norm_basis.get_column(0);
     e1 = norm_basis.get_column(1);
@@ -32,6 +32,27 @@ void test_gram_schmidt()
     TEST_NEAR("mbl_gram_schmidt e0 and e1 orthogonal", dot_product(e0,e1), 0.0, 1e-8);
     TEST_NEAR("mbl_gram_schmidt e0 and e2 orthogonal", dot_product(e0,e2), 0.0, 1e-8);
     TEST_NEAR("mbl_gram_schmidt e1 and e2 orthogonal", dot_product(e1,e2), 0.0, 1e-8);
+
+
+    //** test version returning normalisation weights
+    vnl_vector<double> normalisers;
+    norm_basis.fill(0);
+    mbl_mod_gram_schmidt(basis, norm_basis, normalisers);
+    vnl_vector<double> f0, f1, f2;
+    f0 = norm_basis.get_column(0);
+    f1 = norm_basis.get_column(1);
+    f2 = norm_basis.get_column(2);
+
+    TEST_NEAR("mbl_gram_schmidt e0 norm", f0.magnitude(), 1.0, 1e-8);
+    TEST_NEAR("mbl_gram_schmidt e1 norm", f1.magnitude(), 1.0, 1e-8);
+    TEST_NEAR("mbl_gram_schmidt e2 norm", f2.magnitude(), 1.0, 1e-8);
+    TEST_NEAR("mbl_gram_schmidt e0 and e1 orthogonal", dot_product(f0,f1), 0.0, 1e-8);
+    TEST_NEAR("mbl_gram_schmidt e0 and e2 orthogonal", dot_product(f0,f2), 0.0, 1e-8);
+    TEST_NEAR("mbl_gram_schmidt e1 and e2 orthogonal", dot_product(f1,f2), 0.0, 1e-8);
+    
+    TEST_EQUAL("basis variants e0, f0", e0==f0, true);
+    TEST_EQUAL("basis variants e1, f1", e1==f1, true);
+    TEST_EQUAL("basis variants e2, f2", e2==f2, true);    
 }
 
 TESTMAIN(test_gram_schmidt);
