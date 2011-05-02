2ad6e20f802f2ff7247f96311a8c9ad7d597d8b6
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-05-02 02:10:46 -0400

    renamed (standard naming for template instantiation files)
    SVN-Revision: 31987

--- contrib/brl/bseg/boxm/algo/rt/Templates/boxm_shadow_bayes_optimizer+short.BOXM_APM_SIMPLE_GREY.BOXM_AUX_OPT_RT_GREY-.cxx
+++ contrib/brl/bseg/boxm/algo/rt/Templates/boxm_shadow_bayes_optimizer+short.BOXM_APM_SIMPLE_GREY.BOXM_AUX_OPT_RT_GREY-.cxx
@@ -1,4 +1,3 @@
 #include <boxm/algo/rt/boxm_shadow_bayes_optimizer.txx>
 #include <boxm/boxm_apm_traits.h>
-
 BOXM_SHADOW_BAYES_OPTIMIZER_INSTANTIATE(short,BOXM_APM_SIMPLE_GREY,BOXM_AUX_OPT_RT_GREY);
