855e32c21a5a85ffe39bc2ae1dabddf68401ee52
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2011-05-03 16:38:33 -0400

    added depth rendering functionality
    SVN-Revision: 32014

--- contrib/brl/bseg/boxm2/cpp/algo/boxm2_render_functions.cxx
+++ contrib/brl/bseg/boxm2/cpp/algo/boxm2_render_functions.cxx
@@ -38,3 +38,21 @@ void boxm2_render_cone_exp_image(boxm2_scene_info * linfo,
       (render_functor,linfo,blk_sptr,cam,roi_ni,roi_nj,roi_ni0,roi_nj0);
   vcl_cout<<"Cone Render Time: "<<t.all()<<"ms"<<vcl_endl;
 }
+
+void boxm2_render_expected_depth( boxm2_scene_info * linfo,
+                                  boxm2_block * blk_sptr,
+                                  boxm2_data_base * data,
+                                  vpgl_camera_double_sptr cam ,
+                                  vil_image_view<float> *expected,
+                                  vil_image_view<float> * vis,
+                                  vil_image_view<float> * len_img,
+                                  unsigned int roi_ni,
+                                  unsigned int roi_nj,
+                                  unsigned int roi_ni0,
+                                  unsigned int roi_nj0)
+{
+  boxm2_render_exp_depth_functor render_functor;
+  render_functor.init_data(data,expected,vis,len_img);
+  cast_ray_per_block<boxm2_render_exp_depth_functor>
+      (render_functor,linfo,blk_sptr,cam,roi_ni,roi_nj,roi_ni0,roi_nj0);
+}
--- contrib/brl/bseg/boxm2/cpp/algo/boxm2_render_functions.h
+++ contrib/brl/bseg/boxm2/cpp/algo/boxm2_render_functions.h
@@ -4,6 +4,7 @@
 // Render block functions (make use of the render functor classes)
 //
 #include "boxm2_render_exp_image_functor.h"
+#include "boxm2_render_exp_depth_functor.h"
 
 void boxm2_render_expected_image( boxm2_scene_info * linfo,
                                   boxm2_block * blk_sptr,
@@ -27,6 +28,18 @@ void boxm2_render_cone_exp_image(boxm2_scene_info * linfo,
                                 unsigned int roi_nj,
                                 unsigned int roi_ni0=0,
                                 unsigned int roi_nj0=0); 
+
+void boxm2_render_expected_depth( boxm2_scene_info * linfo,
+                                  boxm2_block * blk_sptr,
+                                  boxm2_data_base * data,
+                                  vpgl_camera_double_sptr cam,
+                                  vil_image_view<float> *expected,
+                                  vil_image_view<float> * vis,
+                                  vil_image_view<float> * len_img,
+                                  unsigned int roi_ni,
+                                  unsigned int roi_nj,
+                                  unsigned int roi_ni0=0,
+                                  unsigned int roi_nj0=0); 
 
 
 #endif  //boxm2_render_functions_h_
