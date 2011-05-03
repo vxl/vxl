abfd8b9919e40af3e34a7f28befcf7c843546fb8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-05-03 08:26:01 -0400

    boxm2_include_glew.h must be included first
    SVN-Revision: 32006

--- contrib/brl/bseg/boxm2/view/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/view/tests/test_include.cxx
@@ -1,5 +1,7 @@
-#include <boxm2/view/boxm2_cam_tableau.h>
+// Careful: boxm2_include_glew.h must be included first!
 #include <boxm2/view/boxm2_include_glew.h>
+
+#include <boxm2/view/boxm2_cam_tableau.h>
 #include <boxm2/view/boxm2_ocl_render_tableau.h>
 #include <boxm2/view/boxm2_ocl_update_tableau.h>
 #include <boxm2/view/boxm2_trajectory.h>
