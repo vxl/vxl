67fa9bed4ff50d3814a0a2c87e4170b52e6dfa18
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-05-13 12:19:03 -0400

    templates for char type
    SVN-Revision: 32086

---
+++ contrib/brl/bseg/boxm/Templates/boxm_block+boct_tree+short.char--.cxx
@@ -0,0 +1,6 @@
+#include <boxm/boxm_block.txx>
+#include <boct/boct_tree.h>
+
+typedef boct_tree<short,char>  tree_type;
+BOXM_BLOCK_INSTANTIATE(tree_type);
+
---
+++ contrib/brl/bseg/boxm/Templates/boxm_scene+boct_tree+short.char--.cxx
@@ -0,0 +1,5 @@
+#include <boxm/boxm_scene.txx>
+#include <boct/boct_tree.txx>
+
+typedef boct_tree<short,char > tree;
+BOXM_SCENE_INSTANTIATE(tree);
