a855effc9d23fc472270ec6841d16d45b67d0030
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2011-05-13 16:23:47 -0400

    update
    SVN-Revision: 32094

--- contrib/brl/bseg/boxm2/cpp/algo/boxm2_cast_intensities_functor.h
+++ contrib/brl/bseg/boxm2/cpp/algo/boxm2_cast_intensities_functor.h
@@ -13,20 +13,20 @@ class boxm2_cast_intensities_functor
 
   bool init_data(boxm2_data_base* data, vil_image_view<float> * input_img)
   {
-    alpha_data_=new boxm2_data<BOXM2_ALPHA>(data->data_buffer(),data->buffer_length(),data->block_id());
+    alpha_data_=new boxm2_data<BOXM2_AUX0>(data->data_buffer(),data->buffer_length(),data->block_id());
     input_img_=input_img;
     return true;
   }
 
   inline bool step_cell(float seg_len,int index,unsigned i,unsigned j)
   {
-    boxm2_data<BOXM2_ALPHA>::datatype & alpha=alpha_data_->data()[index];
+    boxm2_data<BOXM2_AUX0>::datatype & alpha=alpha_data_->data()[index];
     alpha = (*input_img_)(i,j);
     return true;
   }
  private:
-  boxm2_data<BOXM2_ALPHA> * alpha_data_;
-  vil_image_view<float> * input_img_;
+   boxm2_data<BOXM2_AUX0> * alpha_data_;
+   vil_image_view<float> * input_img_;
 };
 
 #endif
