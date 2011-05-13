6df6475d88275bc315bc5e23078f3a8fc186b24d
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2011-05-13 10:17:32 -0400

    update
    SVN-Revision: 32076

---
+++ contrib/brl/bseg/boxm2/cpp/pro/processes/boxm2_cpp_cast_intensities_process.cxx
@@ -0,0 +1,108 @@
+// This is brl/bseg/boxm2/cpp/pro/processes/boxm2_cpp_cast_intensities_process.cxx
+#include <bprb/bprb_func_process.h>
+//:
+// \file
+// \brief  A process for casting intensities of an image along the rays of its camera into the world.
+//
+// \author Ozge C. Ozcanli
+// \date May 04, 2011
+
+#include <vcl_fstream.h>
+#include <boxm2/io/boxm2_cache.h>
+#include <boxm2/boxm2_scene.h>
+#include <boxm2/boxm2_block.h>
+#include <boxm2/boxm2_data_base.h>
+#include <vil/vil_save.h>
+#include <vil/vil_image_view.h>
+#include <vil/vil_transform.h>
+#include <vil/vil_math.h>
+//brdb stuff
+#include <brdb/brdb_value.h>
+#include <boxm2/boxm2_util.h>
+#include <boxm2/cpp/algo/boxm2_cast_intensities_functor.h>
+
+//directory utility
+#include <vul/vul_timer.h>
+#include <vcl_where_root_dir.h>
+
+namespace boxm2_cpp_cast_intensities_process_globals
+{
+  const unsigned n_inputs_ = 5;
+  const unsigned n_outputs_ = 0;
+}
+
+bool boxm2_cpp_cast_intensities_process_cons(bprb_func_process& pro)
+{
+  using namespace boxm2_cpp_cast_intensities_process_globals;
+
+  //process takes 5 inputs
+  // 0) scene
+  // 1) cache
+  // 2) camera
+  // 3) image
+  // 4) image identifier
+  vcl_vector<vcl_string> input_types_(n_inputs_);
+  input_types_[0] = "boxm2_scene_sptr";
+  input_types_[1] = "boxm2_cache_sptr";
+  input_types_[2] = "vpgl_camera_double_sptr";
+  input_types_[3] = "vil_image_view_base_sptr";
+  input_types_[4] = "vcl_string";  //image identifier
+  // process has 1 output:
+  // output[0]: scene sptr
+  vcl_vector<vcl_string>  output_types_(n_outputs_);
+
+  return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
+}
+
+bool boxm2_cpp_cast_intensities_process(bprb_func_process& pro)
+{
+    using namespace boxm2_cpp_cast_intensities_process_globals;
+
+    if ( pro.n_inputs() < n_inputs_ ){
+        vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
+        return false;
+    }
+    //get the inputs
+    unsigned i = 0;
+    boxm2_scene_sptr scene =pro.get_input<boxm2_scene_sptr>(i++);
+    boxm2_cache_sptr cache= pro.get_input<boxm2_cache_sptr>(i++);
+    vpgl_camera_double_sptr cam= pro.get_input<vpgl_camera_double_sptr>(i++);
+    vil_image_view_base_sptr in_img=pro.get_input<vil_image_view_base_sptr>(i++);
+    vil_image_view_base_sptr float_image=boxm2_util::prepare_input_image(in_img);
+    vcl_string identifier = pro.get_input<vcl_string>(i++);
+
+    if (vil_image_view<float> * input_image=dynamic_cast<vil_image_view<float> * > (float_image.ptr()))
+    {
+        
+        vcl_vector<boxm2_block_id> vis_order=scene->get_vis_blocks(reinterpret_cast<vpgl_generic_camera<double>*>(cam.ptr()));
+        if (vis_order.empty())
+        {
+            vcl_cout<<" None of the blocks are visible from this viewpoint"<<vcl_endl;
+            return true;
+        }
+
+        bool success=true;
+        boxm2_cast_intensities_functor pass;
+
+        vcl_vector<boxm2_block_id>::iterator id;
+        for (id = vis_order.begin(); id != vis_order.end(); ++id)
+        {
+          vcl_cout<<"Block id "<<(*id)<<' ';
+          boxm2_block *     blk   = cache->get_block(*id);
+          // identifier of the image causes a whole new data base to be generated for this image under the scene
+          boxm2_data_base *  alph = cache->get_data_base(*id,boxm2_data_traits<BOXM2_ALPHA>::prefix(identifier));
+          //int alphaTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_ALPHA>::prefix());
+          boxm2_scene_info_wrapper *scene_info_wrapper=new boxm2_scene_info_wrapper();
+          scene_info_wrapper->info=scene->get_blk_metadata(*id);
+          pass.init_data(alph,input_image);
+          success=success && cast_ray_per_block<boxm2_cast_intensities_functor>(pass,
+                                                                                scene_info_wrapper->info,
+                                                                                blk,
+                                                                                cam,
+                                                                                input_image->ni(),
+                                                                                input_image->nj());
+        }
+        return true;
+    }
+    return false;
+}
