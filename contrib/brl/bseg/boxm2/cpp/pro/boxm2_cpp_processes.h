63b1f600221d83eaa2c19cb89abc1731b972752a
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2011-05-13 10:18:30 -0400

    update
    SVN-Revision: 32078

--- contrib/brl/bseg/boxm2/cpp/pro/boxm2_cpp_processes.h
+++ contrib/brl/bseg/boxm2/cpp/pro/boxm2_cpp_processes.h
@@ -12,4 +12,7 @@ DECLARE_FUNC_CONS(boxm2_cpp_refine_process2);
 DECLARE_FUNC_CONS(boxm2_cpp_change_detection_process2);
 DECLARE_FUNC_CONS(boxm2_cpp_render_expected_image_process);
 DECLARE_FUNC_CONS(boxm2_cpp_render_expected_depth_process);
+DECLARE_FUNC_CONS(boxm2_cpp_cast_intensities_process);
+DECLARE_FUNC_CONS(boxm2_cpp_mean_intensities_batch_process);
+DECLARE_FUNC_CONS(boxm2_cpp_mean_intensities_print_process);
 #endif
