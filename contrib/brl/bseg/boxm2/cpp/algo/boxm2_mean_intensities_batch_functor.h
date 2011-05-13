6143512f8cfcf8d599262af349f059634155fd93
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2011-05-13 16:24:15 -0400

    update
    SVN-Revision: 32095

--- contrib/brl/bseg/boxm2/cpp/algo/boxm2_mean_intensities_batch_functor.h
+++ contrib/brl/bseg/boxm2/cpp/algo/boxm2_mean_intensities_batch_functor.h
@@ -12,23 +12,23 @@
 class boxm2_mean_intensities_batch_functor
 {
  public:
-  typedef boxm2_data_traits<BOXM2_ALPHA>::datatype datatype;
+  typedef boxm2_data_traits<BOXM2_AUX0>::datatype datatype;
 
   //: "default" constructor
   boxm2_mean_intensities_batch_functor() {}
 
   bool init_data(boxm2_data_base *output_alph, boxm2_stream_cache_sptr str_cache)
   {
-    alpha_data_=new boxm2_data<BOXM2_ALPHA>(output_alph->data_buffer(),output_alph->buffer_length(),output_alph->block_id());
+    alpha_data_=new boxm2_data<BOXM2_AUX0>(output_alph->data_buffer(),output_alph->buffer_length(),output_alph->block_id());
     str_cache_ = str_cache;
-    data_type_ = boxm2_data_traits<BOXM2_ALPHA>::prefix();
+    data_type_ = boxm2_data_traits<BOXM2_AUX0>::prefix();
     id_ = output_alph->block_id();
     return true;
   }
   inline bool process_cell(int index)
   {
     datatype & alpha=alpha_data_->data()[index];
-    vcl_vector<datatype> out = str_cache_->get_next<BOXM2_ALPHA,datatype>(id_, index);
+    vcl_vector<datatype> out = str_cache_->get_next<BOXM2_AUX0,datatype>(id_, index);
 
     alpha = datatype(0.0);
     for (unsigned j = 0; j < out.size(); j++) {
@@ -38,7 +38,7 @@ class boxm2_mean_intensities_batch_functor
     return true;
   }
  private:
-  boxm2_data<BOXM2_ALPHA>     * alpha_data_;
+  boxm2_data<BOXM2_AUX0>     * alpha_data_;
   boxm2_stream_cache_sptr str_cache_;
   vcl_string data_type_;
   boxm2_block_id id_;
@@ -47,14 +47,14 @@ class boxm2_mean_intensities_batch_functor
 class boxm2_mean_intensities_print_functor
 {
  public:
-  typedef boxm2_data_traits<BOXM2_ALPHA>::datatype datatype;
+  typedef boxm2_data_traits<BOXM2_AUX0>::datatype datatype;
 
   //: "default" constructor
   boxm2_mean_intensities_print_functor() {}
 
   bool init_data(boxm2_data_base *output_alph, boxm2_stream_cache_sptr str_cache)
   {
-    alpha_data_=new boxm2_data<BOXM2_ALPHA>(output_alph->data_buffer(),output_alph->buffer_length(),output_alph->block_id());
+    alpha_data_=new boxm2_data<BOXM2_AUX0>(output_alph->data_buffer(),output_alph->buffer_length(),output_alph->block_id());
     str_cache_ = str_cache;
     id_ = output_alph->block_id();
     return true;
@@ -62,7 +62,7 @@ class boxm2_mean_intensities_print_functor
   inline bool process_cell(int index)
   {
     datatype & alpha=alpha_data_->data()[index];
-    vcl_vector<datatype> out = str_cache_->get_next<BOXM2_ALPHA,datatype>(id_, index);
+    vcl_vector<datatype> out = str_cache_->get_next<BOXM2_AUX0,datatype>(id_, index);
     
     if (index%1000000 == 0) {
       vcl_cout << alpha << " ";
@@ -75,7 +75,7 @@ class boxm2_mean_intensities_print_functor
     return true;
   }
  private:
-  boxm2_data<BOXM2_ALPHA>     * alpha_data_;
+  boxm2_data<BOXM2_AUX0>     * alpha_data_;
   boxm2_stream_cache_sptr str_cache_;
   boxm2_block_id id_;
 };
