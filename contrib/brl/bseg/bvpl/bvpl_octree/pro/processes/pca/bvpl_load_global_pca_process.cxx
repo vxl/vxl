4a45b0d704e23752fcff341f3639a0404362d816
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-05-16 17:22:19 -0400

    avoid compiler warning (missing EOLN at EOF
    SVN-Revision: 32131

--- contrib/brl/bseg/bvpl/bvpl_octree/pro/processes/pca/bvpl_load_global_pca_process.cxx
+++ contrib/brl/bseg/bvpl/bvpl_octree/pro/processes/pca/bvpl_load_global_pca_process.cxx
@@ -1,6 +1,6 @@
 //:
-// \brief
 // \file
+// \brief
 // \author Isabel Restrepo
 // \date 15-May-2011
 
@@ -11,40 +11,40 @@
 
 #include <bvpl_octree/bvpl_global_pca.h>
 
-//:global variables
-namespace bvpl_load_global_pca_process_globals 
+//: global variables
+namespace bvpl_load_global_pca_process_globals
 {
   const unsigned n_inputs_ = 1;
   const unsigned n_outputs_ = 1;
 }
 
 
-//:sets input and output types
+//: sets input and output types
 bool bvpl_load_global_pca_125_process_cons(bprb_func_process& pro)
 {
   using namespace bvpl_load_global_pca_process_globals ;
-  
+
   vcl_vector<vcl_string> input_types_(n_inputs_);
   input_types_[0] = "vcl_string";
-  
+
   vcl_vector<vcl_string> output_types_(n_outputs_);
   output_types_[0] = "bvpl_global_pca_125_sptr";
-  
+
   return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
 }
 
 
-//:the process
+//: the process
 bool bvpl_load_global_pca_125_process(bprb_func_process& pro)
 {
   using namespace bvpl_load_global_pca_process_globals;
-  
-  //get inputs
+
+  // get inputs
   vcl_string pca_file = pro.get_input<vcl_string>(0);
   bvpl_global_pca<125> *global_pca = new bvpl_global_pca<125>(pca_file);
- 
-  //store output
+
+  // store output
   pro.set_output_val<bvpl_global_pca_125_sptr>(0, global_pca);
-  
+
   return true;
 }
