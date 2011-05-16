f1bf077aa3c380f69323ea0cb030d21c88cd8677
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-05-16 02:30:49 -0400

    "vpgl_bundler_routines" was first declared as "class", later defined as "struct"
    SVN-Revision: 32119

--- contrib/gel/mrc/vpgl/algo/vpgl_bundler.h
+++ contrib/gel/mrc/vpgl/algo/vpgl_bundler.h
@@ -1,43 +1,43 @@
 #ifndef VPGL_BUNDLER_H
 #define VPGL_BUNDLER_H
 
-/*Generic includes.*/
+// Generic includes.
 #include <vcl_vector.h>
 #include <vnl/vnl_sparse_matrix.h>
 
-/*We use vil_image_resources since they are not in memory, and we might
- * have a very large collection of images.*/
-#include <vil/vil_image_resource.h> 
+// We use vil_image_resources since they are not in memory, and we might
+// have a very large collection of images.
+#include <vil/vil_image_resource.h>
 
-/*Include the classes used to represent cameras and points.*/
+// Include the classes used to represent cameras and points.
 #include <vpgl/vpgl_perspective_camera.h>
-#include <vgl/vgl_point_3d.h> 
+#include <vgl/vgl_point_3d.h>
 
-/*Include the classes for the stages.*/
+// Include the classes for the stages.
 #include <vpgl/algo/vpgl_bundler_tracks.h>
 #include <vpgl/algo/vpgl_bundler_sfm.h>
 
-/*Include the default implementations.*/
+// Include the default implementations.
 #include <vpgl/algo/vpgl_bundler_tracks_impl.h>
 #include <vpgl/algo/vpgl_bundler_sfm_impl.h>
 
-/*Include the settings and intermediate types*/
+// Include the settings and intermediate types
 #include <vpgl/algo/vpgl_bundler_settings.h>
 #include <vpgl/algo/vpgl_bundler_inters.h>
 
-class vpgl_bundler_routines;
+struct vpgl_bundler_routines;
 
-/*The default driver for the bundler routines. Uses the functions packaged
- * in routines to calculate the pose of each of the cameras and a set
- * of 3D points from the images included in imageset. These values
- * are placed in cameras and points, respectively. Also includes a matrix 
- * which holds which points are available from which cameras.
- * The exif_tags should hold information extracted from the EXIF tags for 
- * each image. It either holds the focal length in pixels, or 
- * VPGL_BUNDLER_NO_FOCAL_LEN for images that either don't have an exif tag
- * or whose exif tag is missing the focal length.*/
+// The default driver for the bundler routines. Uses the functions packaged
+// in routines to calculate the pose of each of the cameras and a set
+// of 3D points from the images included in imageset. These values
+// are placed in cameras and points, respectively. Also includes a matrix
+// which holds which points are available from which cameras.
+// The exif_tags should hold information extracted from the EXIF tags for
+// each image. It either holds the focal length in pixels, or
+// VPGL_BUNDLER_NO_FOCAL_LEN for images that either don't have an exif tag
+// or whose exif tag is missing the focal length.
 void vpgl_bundler_driver(
-    const vpgl_bundler_routines &routines, 
+    const vpgl_bundler_routines &routines,
     const vcl_vector<vil_image_resource_sptr> &imageset,
     const vcl_vector<double> exif_tags,
 
@@ -46,15 +46,15 @@ void vpgl_bundler_driver(
     vnl_sparse_matrix<bool> visibility_graph);
 
 
-
-/*A collection of routines that provide the feature phase of
- * the bundler pipeline. There are three constructors, one for the default,
- * which will use the routines for the standard feature matching as well as
- * default settings, another which allows the user to specify thresholds, 
- * and the last which takes in pointers to the stages used. If the default
- * stages are used (the first or second constructor), the stages are freed 
- * in the deconstructor, otherwise, the stages are left well enough alone.*/
-class vpgl_bundler_tracks{
+// A collection of routines that provide the feature phase of
+// the bundler pipeline. There are three constructors, one for the default,
+// which will use the routines for the standard feature matching as well as
+// default settings, another which allows the user to specify thresholds,
+// and the last which takes in pointers to the stages used. If the default
+// stages are used (the first or second constructor), the stages are freed
+// in the destructor, otherwise, the stages are left well enough alone.
+class vpgl_bundler_tracks
+{
     private:
         bool manage_pointers;
 
@@ -65,36 +65,35 @@ class vpgl_bundler_tracks{
         vpgl_bundler_tracks_refine *refine;
         vpgl_bundler_tracks_chain_matches *chain_matches;
 
-        /*A constructor that uses the default pipeline stages as well
-         * as default settings for each stage.*/
+        // A constructor that uses the default pipeline stages as well
+        // as default settings for each stage.
         vpgl_bundler_tracks();
 
-
-        /*A constructor that uses the default pipeline stages, but 
-         * allows the user to override the settings for each stage.
-         * The settings include thresholds, number of iterations, and 
-         * similar details.*/
+        // A constructor that uses the default pipeline stages, but
+        // allows the user to override the settings for each stage.
+        // The settings include thresholds, number of iterations, and
+        // similar details.
         vpgl_bundler_tracks(vpgl_bundler_settings_tracks s);
 
-        /*Constructor that allows an override of each of the stages
-         * of the feature pipeline. These pointers are managed 
-         * externally! As in, if you allocate them, you must 
-         * deallocate them.*/
+        // Constructor that allows an override of each of the stages
+        // of the feature pipeline. These pointers are managed
+        // externally! As in, if you allocate them, you must
+        // deallocate them.
         vpgl_bundler_tracks(
             vpgl_bundler_tracks_detect *d,
             vpgl_bundler_tracks_propose_matches *ml,
             vpgl_bundler_tracks_match *ma,
             vpgl_bundler_tracks_refine *re,
             vpgl_bundler_tracks_chain_matches *cm):
-            
+
             manage_pointers(false),
             detect_features(d), propose_matches(ml),
             match(ma), refine(re), chain_matches(cm) { }
 
-        /*The destructor that frees the allocations made in the 
-         * constructor, if there are any.*/
-        ~vpgl_bundler_tracks(){
-            if(manage_pointers){
+        // The destructor that frees the allocations made in the
+        // constructor, if there are any.
+        ~vpgl_bundler_tracks() {
+            if (manage_pointers) {
                 delete detect_features;
                 delete propose_matches;
                 delete match;
@@ -103,9 +102,9 @@ class vpgl_bundler_tracks{
             }
         }
 
-        /*A convienence method that runs the entire feature matching
-         * pipeline. Alternatively, you can directly call functors 
-         * yourself.*/
+        // A convienence method that runs the entire feature matching
+        // pipeline. Alternatively, you can directly call functors
+        // yourself.
         virtual void run_feature_stage(
             const vcl_vector<vil_image_resource_sptr> &imageset,
             const vcl_vector<double> exif_tags,
@@ -114,9 +113,10 @@ class vpgl_bundler_tracks{
 };
 
 
-/*A collections of routines that provides the structure from motion (sfm)
- * phase of the bundler pipeline. The constructors are similar to the above.*/
-class vpgl_bundler_sfm{
+// A collections of routines that provides the structure from motion (sfm)
+// phase of the bundler pipeline. The constructors are similar to the above.
+class vpgl_bundler_sfm
+{
     private:
         bool manage_pointers;
 
@@ -131,36 +131,34 @@ class vpgl_bundler_sfm{
 
         vpgl_bundler_sfm_bundle_adjust *bundle_adjust;
 
-        /*A constructor that uses the default pipeline stages as well as 
-         * default settings for each stage*/
+        // A constructor that uses the default pipeline stages as well as
+        // default settings for each stage
         vpgl_bundler_sfm();
 
+        // A constructor that uses the default pipeline stages, but
+        // allows the user to override the settings for each stage.
+        // The settings include thresholds, number of iterations, and
+        // similar details.
+        vpgl_bundler_sfm(vpgl_bundler_settings_sfm s);
 
-        /*A constructor that uses the default pipeline stages, but 
-         * allows the user to override the settings for each stage.
-         * The settings include thresholds, number of iterations, and 
-         * similar details.*/
-         vpgl_bundler_sfm(vpgl_bundler_settings_sfm s);
-
-        /*Constructor that allows an override of each of the stages
-         * of the sfm pipeline. These pointers are managed 
-         * externally! As in, if you allocate them, you must 
-         * deallocate them.*/
-         vpgl_bundler_sfm(
+        // Constructor that allows an override of each of the stages
+        // of the sfm pipeline. These pointers are managed
+        // externally! As in, if you allocate them, you must
+        // deallocate them.
+        vpgl_bundler_sfm(
             vpgl_bundler_sfm_create_initial_recon *cir,
             vpgl_bundler_sfm_select_next_images *sni,
             vpgl_bundler_sfm_add_next_images *ani,
             vpgl_bundler_sfm_add_new_points *anp,
-            vpgl_bundler_sfm_bundle_adjust *ba) : 
-
+            vpgl_bundler_sfm_bundle_adjust *ba) :
                 create_initial_recon(cir), select_next_images(sni),
-                add_next_images(ani), add_new_points(anp), 
-                bundle_adjust(ba){ }
+                add_next_images(ani), add_new_points(anp),
+                bundle_adjust(ba) {}
 
-        /*The destructor that frees the allocations made in the 
-         * constructor, if there are any.*/
-        ~vpgl_bundler_sfm(){
-            if(manage_pointers){
+        // The destructor that frees the allocations made in the
+        // constructor, if there are any.
+        ~vpgl_bundler_sfm() {
+            if (manage_pointers) {
                 delete create_initial_recon;
                 delete select_next_images;
                 delete add_next_images;
@@ -169,40 +167,39 @@ class vpgl_bundler_sfm{
             }
         }
 
-        /*A convienence method that runs the entire sfm matching
-         * pipeline. Alternatively, you can directly call functors 
-         * yourself.*/
+        // A convenience method that runs the entire sfm matching
+        // pipeline. Alternatively, you can directly call functors
+        // yourself.
         virtual void run_sfm_stage(vpgl_bundler_inters_track_set &track_set,
-                vcl_vector<vpgl_perspective_camera<double> > &cameras,
-                vcl_vector<vgl_point_3d<double> > &points,
-                vnl_sparse_matrix<bool> visiblity_graph);
+                                   vcl_vector<vpgl_perspective_camera<double> > &cameras,
+                                   vcl_vector<vgl_point_3d<double> > &points,
+                                   vnl_sparse_matrix<bool> visibility_graph);
 };
 
 
-
-/*A wrapper struct for the functions vpgl_bundler_driver will use.*/
-struct vpgl_bundler_routines{
+// A wrapper struct for the functions vpgl_bundler_driver will use.
+struct vpgl_bundler_routines
+{
     vpgl_bundler_tracks features_phase;
     vpgl_bundler_sfm sfm_phase;
 
-    /*Uses default everything, stages and settings*/
-    vpgl_bundler_routines(){};
+    // Uses default everything, stages and settings
+    vpgl_bundler_routines() {}
 
-    /*Uses the default stages, but allows the user to specify settings
-     * for the thresholds.*/
-     vpgl_bundler_routines(vpgl_bundler_settings s):
-        features_phase(s.feature_settings), 
-        sfm_phase(s.sfm_settings){ }
+    // Uses the default stages, but allows the user to specify settings
+    // for the thresholds.
+    vpgl_bundler_routines(vpgl_bundler_settings s):
+        features_phase(s.feature_settings),
+        sfm_phase(s.sfm_settings) {}
 
-    /*Allows the user to specify a set of feature and sfm routines
-     * to use.*/
-    vpgl_bundler_routines(vpgl_bundler_tracks f, 
-        vpgl_bundler_sfm s){
-
+    // Allows the user to specify a set of feature and sfm routines
+    // to use.
+    vpgl_bundler_routines(vpgl_bundler_tracks f,
+                          vpgl_bundler_sfm s) {
         features_phase = f;
         sfm_phase = s;
     }
 };
 
 
-#endif /*VPGL_BUNDLER_H*/
+#endif // VPGL_BUNDLER_H
