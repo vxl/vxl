fb609ee3c4953343bfa0f2c09f34e4d61de7ab62
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-05-16 14:07:00 -0400

    added bvpl_global_sptr
    SVN-Revision: 32129

---
+++ contrib/brl/bseg/bvpl/bvpl_octree/pro/Templates/brdb_value_t+bvpl_global_pca_125_sptr-.cxx
@@ -0,0 +1,9 @@
+#include <brdb/brdb_value.txx>
+
+#include <vbl/io/vbl_io_smart_ptr.h>
+#include <bvpl/bvpl_octree/bvpl_global_pca.h>
+
+BRDB_VALUE_INSTANTIATE(bvpl_global_pca_125_sptr, "bvpl_global_pca_125_sptr");
+
+
+
