95ae196a16385546251deb31bec4e0904e759e7d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-05-17 04:34:53 -0400

    converted the documentation to Doxygen style
    SVN-Revision: 32138

--- contrib/gel/mrc/vpgl/algo/vpgl_bundler_sfm.h
+++ contrib/gel/mrc/vpgl/algo/vpgl_bundler_sfm.h
@@ -1,56 +1,67 @@
 #ifndef VPGL_BUNDLER_SFM_H
 #define VPGL_BUNDLER_SFM_H
+//:
+// \file
 
 #include <vpgl/algo/vpgl_bundler_inters.h>
-
 #include <vcl_vector.h>
 
-/*An abstract functor. Takes in the track set, and returns the initial
- * reconstruction.*/
-class vpgl_bundler_sfm_create_initial_recon {
-public:
-    virtual void operator()(
-        vpgl_bundler_inters_track_set &track_set,
-        vpgl_bundler_inters_reconstruction &reconstruction) = 0;
+//: An abstract functor. Takes in the track set, and returns the initial reconstruction.
+class vpgl_bundler_sfm_create_initial_recon
+{
+ public:
+  virtual void operator() (
+      vpgl_bundler_inters_track_set &track_set,
+      vpgl_bundler_inters_reconstruction &reconstruction) = 0;
 };
 
 
-/*Takes in the current reconstruction and the image set, and selects the 
- * next images to be added to the reconstruction.*/
-class vpgl_bundler_sfm_select_next_images {
-public:
-    /*Takes in reconstruction and track_set, fills to_add as a return val.*/
-    virtual void operator()(
-        vpgl_bundler_inters_reconstruction &reconstruction,
-        vpgl_bundler_inters_track_set &track_set,
+//:
+// Takes in the current reconstruction and the image set, and selects the
+// next images to be added to the reconstruction.
+class vpgl_bundler_sfm_select_next_images
+{
+ public:
+  //: Takes in reconstruction and track_set, fills to_add as a return val.
+  virtual void operator() (
+      vpgl_bundler_inters_reconstruction &reconstruction,
+      vpgl_bundler_inters_track_set &track_set,
 
-        vcl_vector<vpgl_bundler_inters_feature_set_sptr> &to_add) = 0;
+      vcl_vector<vpgl_bundler_inters_feature_set_sptr> &to_add) = 0;
 };
 
 
-/*Takes in the current reconstruction and an image, and adds the image to
- * the reconstruction. This calculates the pose of the images' camera.*/
-class vpgl_bundler_sfm_add_next_images {
-public:
-    virtual void operator()(
-        vpgl_bundler_inters_reconstruction reconstruction,
-        const vcl_vector<vpgl_bundler_inters_feature_set> &to_add) = 0;
+//:
+// Takes in the current reconstruction and an image, and adds the image to
+// the reconstruction. This calculates the pose of the images' camera.
+class vpgl_bundler_sfm_add_next_images
+{
+ public:
+  virtual void operator() (
+      vpgl_bundler_inters_reconstruction reconstruction,
+      const vcl_vector<vpgl_bundler_inters_feature_set> &to_add) = 0;
 };
 
-/*Adds points to the reconstruction using the new cameras calculated in the 
- * add_next_images stage.*/
-class vpgl_bundler_sfm_add_new_points {
-public:
-    virtual void operator()(
-        vpgl_bundler_inters_reconstruction &reconstruction,
-        vpgl_bundler_inters_track_set &track_set) = 0;
+
+//:
+// Adds points to the reconstruction using the new cameras calculated in the
+// add_next_images stage.
+class vpgl_bundler_sfm_add_new_points
+{
+ public:
+  virtual void operator() (
+      vpgl_bundler_inters_reconstruction &reconstruction,
+      vpgl_bundler_inters_track_set &track_set) = 0;
 };
 
-/*Performs bundle adjustment on the reconstruction.*/
-class vpgl_bundler_sfm_bundle_adjust {
-public:
-    virtual void operator()(
-        vpgl_bundler_inters_reconstruction reconstruction) = 0;
+
+//:
+// Performs bundle adjustment on the reconstruction.
+class vpgl_bundler_sfm_bundle_adjust
+{
+ public:
+  virtual void operator() (
+      vpgl_bundler_inters_reconstruction reconstruction) = 0;
 };
 
-#endif /*VPGL_BUNDLER_SFM_H*/
+#endif // VPGL_BUNDLER_SFM_H
--- contrib/gel/mrc/vpgl/algo/vpgl_bundler_tracks_impl.h
+++ contrib/gel/mrc/vpgl/algo/vpgl_bundler_tracks_impl.h
@@ -1,127 +1,129 @@
 #ifndef VPGL_BUNDLER_TRACKS_IMPL
 #define VPGL_BUNDLER_TRACKS_IMPL
+//:
+// \file
+// Provides a default implementation of the feature detect routines. These
+// can all be overridden by creating a new class that inherits from the
+// relevant stage. These defaults are the most general case.
 
 #include <vcl_vector.h>
 #include <vil/vil_image_resource.h>
 #include <vpgl/algo/vpgl_bundler_tracks.h>
 #include <vpgl/algo/vpgl_bundler_settings.h>
 
-/*Provides a default implementation of the feature detect routines. These
- * can all be overrided by creating a new class that inherits from the 
- * relevant stage. These defaults are the most general case.*/
+//:
+// This is the default of the features detect stage.
+// It uses SIFT to find all of the features in the image set.
+class vpgl_bundler_tracks_impl_detect_sift
+  : public vpgl_bundler_tracks_detect
+{
+ protected:
+  vpgl_bundler_settings_sift settings;
 
-/*This is the default of the features detect stage. It uses SIFT to find
- * all of the features in the image set.*/
-class vpgl_bundler_tracks_impl_detect_sift: 
-    public vpgl_bundler_tracks_detect{
+ public:
+  //: A default constructor that uses default settings
+  vpgl_bundler_tracks_impl_detect_sift()
+    : settings(vpgl_bundler_settings_sift()) {}
 
-protected:
-    vpgl_bundler_settings_sift settings;
+  //: A constructor that allows the user to override thresholds and such
+  vpgl_bundler_tracks_impl_detect_sift(
+      vpgl_bundler_settings_sift s) : settings(s) {}
 
-public:
-    /*A default constructor that uses default settings*/
-    vpgl_bundler_tracks_impl_detect_sift() : 
-        settings(vpgl_bundler_settings_sift()){ }
-
-    /*A constructor that allows the user to override thresholds and
-     * such.*/
-    vpgl_bundler_tracks_impl_detect_sift(
-        vpgl_bundler_settings_sift s) : settings(s){ }
-
-    /*Performs the feature matching.*/
-    vpgl_bundler_inters_feature_set_sptr operator ()(
-        const vil_image_resource_sptr &image,
-        const double exif_focal_len);
+  //: Performs the feature matching
+  vpgl_bundler_inters_feature_set_sptr operator() (
+      const vil_image_resource_sptr &image,
+      const double exif_focal_len);
 };
 
 
+//: A functor that provides the default matching algorithm.
+// It outputs a  set of pairs of images that should be matched.
+// Constructors are the  same as above.
+class vpgl_bundler_tracks_impl_propose_matches_all
+  : public vpgl_bundler_tracks_propose_matches
+{
+ protected:
+  vpgl_bundler_settings_propose_matches_all settings;
 
-/*An functor that provides the default matching algorithm. It outputs a 
- * set of pairs of images that should be matched. Constructors are the 
- * same as above.*/
-class vpgl_bundler_tracks_impl_propose_matches_all: 
-    public vpgl_bundler_tracks_propose_matches{
+ public:
+  vpgl_bundler_tracks_impl_propose_matches_all()
+    : settings(vpgl_bundler_settings_propose_matches_all()) {}
 
-protected:
-    vpgl_bundler_settings_propose_matches_all settings;
+  vpgl_bundler_tracks_impl_propose_matches_all(
+      vpgl_bundler_settings_propose_matches_all s) : settings(s) {}
 
-public:
-    vpgl_bundler_tracks_impl_propose_matches_all() : 
-        settings(vpgl_bundler_settings_propose_matches_all()){ }
-
-    vpgl_bundler_tracks_impl_propose_matches_all(
-        vpgl_bundler_settings_propose_matches_all s) : settings(s){ }
-
-    /*Performs the match list creation.*/
-    void operator ()(
-        const vcl_vector<vpgl_bundler_inters_feature_set_sptr> &features,
-        vcl_vector<vpgl_bundler_inters_feature_set_pair> &proposed_matches);
+    //: Performs the match list creation
+  void operator() (
+      const vcl_vector<vpgl_bundler_inters_feature_set_sptr> &features,
+      vcl_vector<vpgl_bundler_inters_feature_set_pair> &proposed_matches);
 };
 
 
+//:
+// A functor that takes in a pair of images to be matched,
+// finds all corresponding features between the two images.
+class vpgl_bundler_tracks_impl_match_ann
+  : public vpgl_bundler_tracks_match
+{
+ protected:
+  vpgl_bundler_settings_match_ann settings;
 
-/*A functor that takes in a pair of images to be matched,
- * finds all corresponding features between the two images.*/
-class vpgl_bundler_tracks_impl_match_ann : 
-    public vpgl_bundler_tracks_match {
+ public:
+  vpgl_bundler_tracks_impl_match_ann()
+    : settings(vpgl_bundler_settings_match_ann()) {}
 
-protected:
-    vpgl_bundler_settings_match_ann settings;
+  vpgl_bundler_tracks_impl_match_ann(
+      vpgl_bundler_settings_match_ann s) : settings(s) {}
 
-public:
-    vpgl_bundler_tracks_impl_match_ann() : 
-        settings(vpgl_bundler_settings_match_ann()) { }
-
-    vpgl_bundler_tracks_impl_match_ann(
-            vpgl_bundler_settings_match_ann s) : settings(s){ }
-
-    /*Performs the feature matching.*/
-    void operator ()(
-        const vpgl_bundler_inters_feature_set_pair &to_match,
-        vpgl_bundler_inters_match_set &matches);
-}; 
-
-
-/*A functor that takes refines a set of matches features
- * according to some thresholds.*/
-class vpgl_bundler_tracks_impl_refine_epipolar : 
-    public vpgl_bundler_tracks_refine {
-
-protected:
-    vpgl_bundler_settings_refine_epipolar settings;
-
-public:
-    vpgl_bundler_tracks_impl_refine_epipolar() : 
-        settings(vpgl_bundler_settings_refine_epipolar()) { }
-
-    vpgl_bundler_tracks_impl_refine_epipolar(
-            vpgl_bundler_settings_refine_epipolar s) : settings(s){ }
-
-    /*Performs the feature matching.*/
-    void operator ()(vpgl_bundler_inters_match_set &matches);
-}; 
-
-
-/*An functor that takes in all matched features between pairs 
- * of images, and chains them together into tracks*/
-class vpgl_bundler_tracks_default_chain_matches : 
-    public vpgl_bundler_tracks_chain_matches { 
-
-protected:
-    vpgl_bundler_settings_chain_matches settings;
-
-public:
-    vpgl_bundler_tracks_default_chain_matches() : 
-        settings(vpgl_bundler_settings_chain_matches()){ }
-
-    vpgl_bundler_tracks_default_chain_matches(
-        vpgl_bundler_settings_chain_matches s) : settings(s){ }
-
-    /*Performs the track chaining.*/
-    void operator ()(
-        const vcl_vector<vpgl_bundler_inters_match_set> &match_set,
-        vcl_vector<vpgl_bundler_inters_track> &tracks);
+  //: Performs the feature matching
+  void operator() (
+    const vpgl_bundler_inters_feature_set_pair &to_match,
+    vpgl_bundler_inters_match_set &matches);
 };
 
 
-#endif /*VPGL_BUNDLER_TRACKS_IMPL*/
+//:
+// A functor that takes refines a set of matches features
+// according to some thresholds.
+class vpgl_bundler_tracks_impl_refine_epipolar
+  : public vpgl_bundler_tracks_refine
+{
+ protected:
+  vpgl_bundler_settings_refine_epipolar settings;
+
+ public:
+  vpgl_bundler_tracks_impl_refine_epipolar()
+    : settings(vpgl_bundler_settings_refine_epipolar()) {}
+
+  vpgl_bundler_tracks_impl_refine_epipolar(
+      vpgl_bundler_settings_refine_epipolar s) : settings(s) {}
+
+  // Performs the feature matching.
+  void operator() (vpgl_bundler_inters_match_set &matches);
+};
+
+
+//:
+// An functor that takes in all matched features between pairs
+// of images, and chains them together into tracks
+class vpgl_bundler_tracks_default_chain_matches
+  : public vpgl_bundler_tracks_chain_matches
+{
+ protected:
+  vpgl_bundler_settings_chain_matches settings;
+
+ public:
+  vpgl_bundler_tracks_default_chain_matches()
+    : settings(vpgl_bundler_settings_chain_matches()) {}
+
+  vpgl_bundler_tracks_default_chain_matches(
+      vpgl_bundler_settings_chain_matches s) : settings(s) {}
+
+  //: Performs the track chaining
+  void operator() (
+      const vcl_vector<vpgl_bundler_inters_match_set> &match_set,
+      vcl_vector<vpgl_bundler_inters_track> &tracks);
+};
+
+
+#endif // VPGL_BUNDLER_TRACKS_IMPL
