c44925e33821033b90c74217a59a1e0f615c67ad
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-05-17 09:37:02 -0400

    added missing #include <vcl_iosfwd.h>
    SVN-Revision: 32146

--- contrib/brl/bbas/brad/brad_sun_dir_index.h
+++ contrib/brl/bbas/brad/brad_sun_dir_index.h
@@ -1,3 +1,5 @@
+#ifndef __BRAD_SUN_DIR_INDEX_H
+#define __BRAD_SUN_DIR_INDEX_H
 //-----------------------------------------------------------------------------
 //:
 // \file
@@ -6,20 +8,20 @@
 // \date May 10, 2011
 //
 //----------------------------------------------------------------------------
-#ifndef __BRAD_SUN_DIR_INDEX_H
-#define __BRAD_SUN_DIR_INDEX_H
 #include <vcl_map.h>
 #include <bbas/bsta/bsta_spherical_histogram.h>
 #include <vnl/vnl_double_3.h>
 #include <vcl_vector.h>
+#include <vcl_iosfwd.h>
+
 class brad_sun_dir_index
 {
  public:
   brad_sun_dir_index(double longitude_deg, double latitude_deg,
-                int obs_year_start, int obs_hour, int obs_min,
-                int obs_time_range_min, int sampling_interval_years,
-                int bin_radius);
-  ~brad_sun_dir_index(){}
+                     int obs_year_start, int obs_hour, int obs_min,
+                     int obs_time_range_min, int sampling_interval_years,
+                     int bin_radius);
+  ~brad_sun_dir_index() {}
 
   //: uses standard definion of sun azimuth and elevation, i.e. elevation zero tangent plane, increasing to nadir and azimuth zero at North and increasing to East, angles in degrees.
   int index(double geo_sun_azimuth, double geo_sun_elevation) const;
@@ -31,24 +33,24 @@ class brad_sun_dir_index
 
   //: cone that defines the range of illuminations in a given bin
   double cone_half_angle() const {return cone_half_angle_;}
-  vnl_double_3 cone_axis(unsigned i) const {return cone_axes_[i];}  
+  vnl_double_3 cone_axis(unsigned i) const {return cone_axes_[i];}
 
   //: location on the Earth being modeled
   double longitude_deg() const {return longitude_deg_;}
   double latitude_deg() const {return latitude_deg_;}
 
-  // useful for debugging
   //: path along the major axis of the illumination distribution, +- 2std_dev.
+  // useful for debugging
   vcl_vector<vnl_double_3> major_path();
-  
-  //: output a vrml display of the major path and the bin axis positions on sun direction sphere 
+
+  //: output a vrml display of the major path and the bin axis positions on sun direction sphere
   void print_to_vrml(vcl_ostream& os);
  private:
   brad_sun_dir_index();//no default constructor
   //: the histogram of sun directions for the specified location and collection time
   double longitude_deg_;
   double latitude_deg_;
-  //the total number of bins is 2*bin_radius_ + 1, i.e. a centered neighborhood
+  //: the total number of bins is 2*bin_radius_ + 1, i.e. a centered neighborhood
   int bin_radius_;
   bsta_spherical_histogram<double> hist_;
   //: cone axes associated with each illumination bin
