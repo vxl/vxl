95d91332a2e2ebaa3579abe0f3d6f55b83fc5027
Brandon Mayer <bmayer1@users.sourceforge.net>
2011-05-17 17:08:16 -0400

    Added binary io signatures so bapl_dsift_sptr can be used with the python interface.
    SVN-Revision: 32148

---
+++ contrib/brl/bseg/bapl/io/Templates/vbl_io_smart_ptr+bapl_dsift-.cxx
@@ -0,0 +1,5 @@
+// This is brl/bseg/bapl/io/Templates/vbl_io_smart_ptr+bapl_dift-.cxx
+#include<bapl/io/bapl_io_dsift.h>
+#include<vbl/io/vbl_io_smart_ptr.txx>
+
+VBL_IO_SMART_PTR_INSTANTIATE(bapl_dsift);
---
+++ contrib/brl/bseg/bapl/io/bapl_io_dsift.h
@@ -0,0 +1,29 @@
+// This is brl/bseg/bapl/io/bapl_io_dsift.h
+#ifndef BAPL_IO_DSIFT_H_
+#define BAPL_IO_DSIFT_H_
+//:
+// \file
+// \brief Binary IO for bapl_dsift class
+//
+// \author Brandon A. Mayer b.mayer1@gmail.com
+//
+// \date  May 16, 2011
+//
+// \verbatim
+//  Modifications
+//   <none yet>
+// \endverbatim
+#include<bapl/bapl_dsift.h>
+
+#include<vcl_iostream.h>
+
+#include<vsl/vsl_binary_io.h>
+
+//: Binary save parameters to stream.
+void vsl_b_read(vsl_b_istream &is, bapl_dsift &dsift);
+void vsl_b_write(vsl_b_ostream &os, bapl_dsift const& dsift);
+void vsl_b_read(vsl_b_istream &is, bapl_dsift* &p);
+void vsl_b_write(vsl_b_ostream &os, const bapl_dsift* p);
+void vsl_print_summary(vcl_ostream& os, const bapl_dsift *p);
+
+#endif //BAPL_IO_DSIFT_H_
