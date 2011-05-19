a70028d9ce62c590bede8bee65cb97c0607fe8c4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-05-19 14:17:04 -0400

    Doxygen comments
    SVN-Revision: 32175

--- contrib/brl/bseg/boct/boct_bit_tree2.h
+++ contrib/brl/bseg/boct/boct_bit_tree2.h
@@ -37,21 +37,21 @@ class boct_bit_tree2
 
   //: Returns index of data for given bit
   int  get_data_index(int bit_index, bool is_random=false) const;
-  
+
   //: returns bit index assuming root data is located at 0
-  int  get_relative_index(int bit_index) const; 
+  int  get_relative_index(int bit_index) const;
 
   //: traverse tree to get leaf index that contains point
   int traverse(const vgl_point_3d<double> p, int deepest=4);
 
   //: gets the cell center (octree is assumed to be [0,1]x[0,1]x[0,1]
-  vgl_point_3d<double> cell_center(int bit_index); 
-  
+  vgl_point_3d<double> cell_center(int bit_index);
+
   //: tells you if a valid cell is at bit_index (if and only if its parent is 1)
-  bool valid_cell(int bit_index); 
-  
+  bool valid_cell(int bit_index);
+
   //: tells you if the bit_index is a leaf
-  bool is_leaf(int bit_index); 
+  bool is_leaf(int bit_index);
 
   //: Return the maximum number of levels, which is root_level+1
   short number_levels() const { return num_levels_; }
@@ -69,29 +69,32 @@ class boct_bit_tree2
 
   //: returns value (0 or 1) of bit at given index (0,73);
   unsigned char bit_at(int index) const;
-  
+
   //: sets bit at given index with bool value
   void set_bit_at(int index, bool val);
 
   //get bits and data
   unsigned char* get_bits() {return bits_; }
 
-  //: sets and gets pointers stored in bits 10, 11, 12, 13
-  //int get_buffer_ptr(); 
-  //int set_buffer_ptr(int ptr); 
-  int get_data_ptr(bool is_random=false); 
-  int set_data_ptr(int ptr, bool is_random=false); 
-  
-  //returns bit indices of leaf nodes under rootBit
-  vcl_vector<int> get_leaf_bits(int rootBit=0); 
-  
-  //cached arrays are public - make em const too
-  static unsigned char bit_lookup[256]; 
-  static float         centerX[585]; 
-  static float         centerY[585]; 
-  static float         centerZ[585]; 
+  //: gets pointers stored in bits 10, 11, 12, 13
+  int get_data_ptr(bool is_random=false);
+  //: sets pointers stored in bits 10, 11, 12, 13
+  int set_data_ptr(int ptr, bool is_random=false);
+#if 0
+  int get_buffer_ptr();
+  int set_buffer_ptr(int ptr);
+#endif
 
-private:
+  //: returns bit indices of leaf nodes under rootBit
+  vcl_vector<int> get_leaf_bits(int rootBit=0);
+
+  //cached arrays are public - make em const too
+  static unsigned char bit_lookup[256];
+  static float         centerX[585];
+  static float         centerY[585];
+  static float         centerZ[585];
+
+ private:
 
   //: Tree structure stored as "bits" = really a char array
   unsigned char* bits_;
