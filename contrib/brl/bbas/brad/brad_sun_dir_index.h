67b484be23e395b87b59061ca4b3b058d6263b8c
Joseph L. Mundy <mundyj@users.sourceforge.net>
2011-05-19 11:17:56 -0400

    additional constructor for brad_sun_dir_index
    SVN-Revision: 32164

--- contrib/brl/bbas/brad/brad_sun_dir_index.h
+++ contrib/brl/bbas/brad/brad_sun_dir_index.h
@@ -4,6 +4,15 @@
 //:
 // \file
 // \brief An index for sun direction vectors.
+//
+// A sun direction vector is assigned to a bin which is closest in 
+// terms of the angle between the direction vector and the vector corresponding
+// to the center of a given bin. This relationship defines a set of 
+// vectors forming a cone around the bin center vector.
+// Currently there is no limit on the cone angle, but a member,cone_half_angle,
+// is provided if future development requires a limit to the extent 
+// of a bin cone neighborhood.
+//
 // \author J. L. Mundy
 // \date May 10, 2011
 //
@@ -17,11 +26,21 @@
 class brad_sun_dir_index
 {
  public:
+
+  //:constructor to provide the sun illumination direction histogram from which the bin structure is automatically derived
   brad_sun_dir_index(double longitude_deg, double latitude_deg,
-                     int obs_year_start, int obs_hour, int obs_min,
-                     int obs_time_range_min, int sampling_interval_years,
-                     int bin_radius);
-  ~brad_sun_dir_index() {}
+                int obs_year_start, int obs_hour, int obs_min,
+                int obs_time_range_min, int sampling_interval_years,
+                int bin_radius);
+
+  //:constructor not enabling the direction histogram, bin centers are previously defined
+  brad_sun_dir_index(double longitude_deg, double latitude_deg,
+                     vcl_vector<vnl_double_3> sun_dir_bin_centers)
+    : longitude_deg_(longitude_deg), latitude_deg_(latitude_deg),
+    cone_axes_(sun_dir_bin_centers), cone_half_angle_(0.0){
+    bin_radius_ = (sun_dir_bin_centers.size()-1)/2;}
+
+  ~brad_sun_dir_index(){}
 
   //: uses standard definion of sun azimuth and elevation, i.e. elevation zero tangent plane, increasing to nadir and azimuth zero at North and increasing to East, angles in degrees.
   int index(double geo_sun_azimuth, double geo_sun_elevation) const;
@@ -55,7 +74,7 @@ class brad_sun_dir_index
   bsta_spherical_histogram<double> hist_;
   //: cone axes associated with each illumination bin
   vcl_vector<vnl_double_3> cone_axes_;
-  //: cone half angle
+  //: cone half angle, to truncate bin membership (not currently used)
   double cone_half_angle_;
 };
 
