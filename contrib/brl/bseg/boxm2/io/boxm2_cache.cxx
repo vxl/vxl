05198daeb92d2d3f8e61e866a1185f2945c41f80
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2011-05-25 10:05:56 -0400

    removed destroyer class
    SVN-Revision: 32219

--- contrib/brl/bseg/boxm2/io/boxm2_cache.cxx
+++ contrib/brl/bseg/boxm2/io/boxm2_cache.cxx
@@ -5,9 +5,6 @@
 //: global initialization for singleton instance_
 boxm2_cache* boxm2_cache::instance_ = 0;
 
-//: global initialization for singleton destroyer instance
-boxm2_cache_destroyer boxm2_cache::destroyer_; 
-
 //: Only one instance should be created (by a derived class)
 boxm2_cache* boxm2_cache::instance()
 {
@@ -28,18 +25,4 @@ void vsl_b_read(vsl_b_istream& is, boxm2_cache* p){}
 void vsl_b_read(vsl_b_istream& is, boxm2_cache_sptr& sptr){}
 void vsl_b_read(vsl_b_istream& is, boxm2_cache_sptr const& sptr){}
 
-boxm2_cache_destroyer::boxm2_cache_destroyer(boxm2_cache* s) 
-{
-  s_ = s;
-}
-//: the destructor deletes the instance
-boxm2_cache_destroyer::~boxm2_cache_destroyer()
-{
-  if (s_ != 0)
-    delete s_;
-}
 
-void boxm2_cache_destroyer::set_singleton(boxm2_cache *s) 
-{
-  s_ = s;
-}
