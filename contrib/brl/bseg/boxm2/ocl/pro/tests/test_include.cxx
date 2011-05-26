b6d7ce9b0665c54dc5d92e1d716913e4446f9171
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-05-26 16:25:34 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 32272

--- contrib/brl/bseg/boxm2/ocl/pro/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/tests/test_include.cxx
@@ -1,4 +1,5 @@
 #include <boxm2/ocl/pro/boxm2_ocl_processes.h>
 #include <boxm2/ocl/pro/boxm2_ocl_register.h>
+#include <boxm2/ocl/pro/processes/boxm2_ocl_filter_process.h>
 
 int main() { return 0; }
