6eef160eedbd15b0ebf19afa3c6838f966e141ed
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-06-06 12:48:43 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 32345

--- contrib/brl/bseg/boxm2/cpp/algo/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/cpp/algo/tests/test_include.cxx
@@ -3,15 +3,18 @@
 #include <boxm2/cpp/algo/boxm2_cast_intensities_functor.h>
 #include <boxm2/cpp/algo/boxm2_cast_ray_function.h>
 #include <boxm2/cpp/algo/boxm2_change_detection_functor.h>
+#include <boxm2/cpp/algo/boxm2_cone_update_image_functor.h>
 #include <boxm2/cpp/algo/boxm2_data_serial_iterator.h>
 #include <boxm2/cpp/algo/boxm2_filter_block_function.h>
 #include <boxm2/cpp/algo/boxm2_mean_intensities_batch_functor.h>
 #include <boxm2/cpp/algo/boxm2_mog3_grey_processor.h>
+#include <boxm2/cpp/algo/boxm2_ray_probe_functor.h>
 #include <boxm2/cpp/algo/boxm2_refine_block_function.h>
 #include <boxm2/cpp/algo/boxm2_render_cone_functor.h>
 #include <boxm2/cpp/algo/boxm2_render_exp_depth_functor.h>
 #include <boxm2/cpp/algo/boxm2_render_exp_image_functor.h>
 #include <boxm2/cpp/algo/boxm2_render_functions.h>
+#include <boxm2/cpp/algo/boxm2_update_functions.h>
 #include <boxm2/cpp/algo/boxm2_update_image_functor.h>
 
 int main() { return 0; }
--- contrib/brl/bseg/boxm2/ocl/pro/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/tests/test_include.cxx
@@ -1,5 +1,4 @@
 #include <boxm2/ocl/pro/boxm2_ocl_processes.h>
 #include <boxm2/ocl/pro/boxm2_ocl_register.h>
-#include <boxm2/ocl/pro/processes/boxm2_ocl_filter_process.h>
 
 int main() { return 0; }
