d3937c697ef08a7ee69571ac562a2d1f03eca078
Vishal Jain <jainv@users.sourceforge.net>
2011-06-07 11:54:44 -0400

    returns float image itself.
    SVN-Revision: 32357

--- contrib/brl/bseg/boxm2/ocl/boxm2_ocl_util.cxx
+++ contrib/brl/bseg/boxm2/ocl/boxm2_ocl_util.cxx
@@ -173,8 +173,12 @@ vil_image_view_base_sptr boxm2_ocl_util::prepare_input_image(vil_image_view_base
     if (vil_image_view<vxl_byte> *img_byte = dynamic_cast<vil_image_view<vxl_byte>*>(loaded_image.ptr()))
         vil_convert_stretch_range_limited(*img_byte, *floatimg, vxl_byte(0), vxl_byte(255), 0.0f, 1.0f);
     else if (vil_image_view<unsigned short> *img_byte = dynamic_cast<vil_image_view<unsigned short>*>(loaded_image.ptr()))
-        vil_convert_stretch_range_limited(*img_byte, *floatimg,(unsigned short)28000,(unsigned short)33000,  0.0f, 1.0f); // hardcoded to be fixed.
+        vil_convert_stretch_range_limited(*img_byte, *floatimg,(unsigned short)30500,(unsigned short)32500,  0.0f, 1.0f); // hardcoded to be fixed.
         //vil_convert_stretch_range(*img_byte, *floatimg,  0.0f, 1.0f); // hardcoded to be fixed.
+    else if (vil_image_view<float> *img_float = dynamic_cast<vil_image_view<float>*>(loaded_image.ptr()))
+    {
+        return vil_image_view_base_sptr(img_float);
+    }
     else {
         vcl_cerr << "Failed to load image\n";
         return 0;
