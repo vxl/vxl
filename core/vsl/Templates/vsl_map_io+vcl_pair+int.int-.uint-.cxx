13fa566c60110c8dd139903a0d69954d1c483d1e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-06-08 11:29:50 -0400

    renamed (standard template instantiation file naming convention)
    SVN-Revision: 32364

--- core/vsl/Templates/vsl_map_io+vcl_pair+int.int-.uint-.cxx
+++ core/vsl/Templates/vsl_map_io+vcl_pair+int.int-.uint-.cxx
@@ -1,6 +1,5 @@
-// Instantiation of IO for vcl_map<vcl_pair<int,int>,unsigned int>
 #include <vsl/vsl_map_io.txx>
-#include <vsl/vsl_pair_io.txx>
+#include <vcl_utility.h>
 typedef vcl_pair<int,int> pii;
 typedef vcl_less<vcl_pair<int,int> > comp;
 VSL_MAP_IO_INSTANTIATE(pii, unsigned, comp);
