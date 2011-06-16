9dd4521a7196bc64a30523b74d8dd8effe908d7e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-06-16 12:48:02 -0400

    added missing template instantiation
    SVN-Revision: 32443

---
+++ contrib/brl/bseg/boxm2/pro/Templates/brdb_value_t+vil_image_view_base_sptr-.cxx
@@ -0,0 +1,8 @@
+#include <brdb/brdb_value.txx>
+#include <vil/vil_image_view_base.h>
+#include <vil/io/vil_io_smart_ptr.h>
+#include <vil/io/vil_io_image_view_base.h>
+
+BRDB_VALUE_INSTANTIATE(vil_image_view_base_sptr, "vil_image_view_base_sptr");
+void vsl_b_read(vsl_b_istream&, vil_image_view_base_sptr&) {}
+void vsl_b_write(vsl_b_ostream&, vil_image_view_base_sptr const&) {}
