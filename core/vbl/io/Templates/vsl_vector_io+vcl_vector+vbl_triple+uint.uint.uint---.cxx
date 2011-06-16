b7241c019b563bd5f0e658f1cd792d1f34d1257d
Graham Vincent <grahamrvincent@users.sourceforge.net>
2011-06-16 08:51:15 -0400

    added instantiation
    SVN-Revision: 32437

---
+++ core/vbl/io/Templates/vsl_vector_io+vcl_vector+vbl_triple+uint.uint.uint---.cxx
@@ -0,0 +1,5 @@
+#include <vsl/vsl_vector_io.txx>
+#include <vbl/vbl_triple.txx>
+#include <vbl/io/vbl_io_triple.txx>
+typedef vcl_vector<vbl_triple<unsigned,unsigned,unsigned> > vector_triple_uuu;
+VSL_VECTOR_IO_INSTANTIATE(vector_triple_uuu);
