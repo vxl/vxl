05bf4e6a5b5636728772811e5c9621d5bfb0f9a6
Tim Cootes <timcootes@users.sourceforge.net>
2011-06-27 04:52:55 -0400

    Added function to reflect shape.
    SVN-Revision: 32500

---
+++ contrib/mul/msm/msm_reflect_shape.cxx
@@ -0,0 +1,30 @@
+//:
+// \file
+// \brief Functions to reflect shapes in various ways.
+//  Renumbering necessary for symmetric shapes.  For instance,
+//  if reflecting a face, the left eye becomes the right eye.
+//  This would mangle a model, so we renumber.
+// \author Tim Cootes
+
+#include "msm_reflect_shape.h"
+
+
+//: Reflect points in the line x=ax, then re-number
+//  On exit, new_points[i] = points[relabel[i]] reflected in x=ax
+void msm_reflect_shape_along_x(const msm_points& points,
+                               const vcl_vector<unsigned>& sym_pts,
+                               msm_points& new_points,
+                               double ax)
+{
+  unsigned n= points.size();
+  assert(sym_pts.size()==n);
+  new_points.set_size(n);
+  for (unsigned i=0;i<n;++i)
+  {
+    vgl_point_2d<double> p=points[i];
+    double x = ax - (p.x()-ax);
+    assert(sym_pts[i]<n);
+    new_points.set_point(sym_pts[i],x,p.y());
+  }
+}
+
