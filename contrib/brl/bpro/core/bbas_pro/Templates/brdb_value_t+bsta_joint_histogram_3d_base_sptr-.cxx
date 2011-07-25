dbb1f93c323afa4d3fabfa5ed76661409b684c2b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-07-25 16:15:42 -0400

    added dummy instantiation for vsl_b_write<bsta_joint_histogram_3d_base>(vsl_b_ostream&, vbl_smart_ptr<bsta_joint_histogram_3d_base> const&) and vsl_b_read<bsta_joint_histogram_3d_base>(vsl_b_istream&, vbl_smart_ptr<bsta_joint_histogram_3d_base>&)
    SVN-Revision: 32620

--- contrib/brl/bpro/core/bbas_pro/Templates/brdb_value_t+bsta_joint_histogram_3d_base_sptr-.cxx
+++ contrib/brl/bpro/core/bbas_pro/Templates/brdb_value_t+bsta_joint_histogram_3d_base_sptr-.cxx
@@ -2,3 +2,6 @@
 #include <bsta/bsta_joint_histogram_3d_sptr.h>
 
 BRDB_VALUE_INSTANTIATE(bsta_joint_histogram_3d_base_sptr,"bsta_joint_histogram_3d_base_sptr");
+// dummy instantiation for the following two:
+void vsl_b_read(vsl_b_istream&, bsta_joint_histogram_3d_base_sptr&) {}
+void vsl_b_write(vsl_b_ostream&, bsta_joint_histogram_3d_base_sptr const&) {}
