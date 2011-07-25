59f28140c01f942864b0d50be3153b2dc71485df
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-07-25 15:23:11 -0400

    moved tests for brad/pro into brad/pro/tests
    SVN-Revision: 32617

---
+++ contrib/brl/bbas/brad/pro/tests/test_include.cxx
@@ -0,0 +1,4 @@
+#include <brad/pro/brad_processes.h>
+#include <brad/pro/brad_register.h>
+
+int main() { return 0; }
--- contrib/brl/bbas/brad/tests/test_include.cxx
+++ contrib/brl/bbas/brad/tests/test_include.cxx
@@ -1,5 +1,10 @@
+#include <brad/brad_eigenspace.h>
+#include <brad/brad_eigenspace_base.h>
+#include <brad/brad_eigenspace_sptr.h>
+#include <brad/brad_hist_prob_feature_vector.h>
 #include <brad/brad_illum_cost_function.h>
 #include <brad/brad_illum_util.h>
+#include <brad/brad_phongs_model_est.h>
 #include <brad/brad_sun_dir_index.h>
 #include <brad/brad_sun_pos.h>
 
