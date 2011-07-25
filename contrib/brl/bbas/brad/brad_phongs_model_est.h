d834f487579d5bd03de439efbc13854bc3a11dc7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-07-25 14:36:37 -0400

    added missing inclusion guard
    SVN-Revision: 32607

--- contrib/brl/bbas/brad/brad_phongs_model_est.h
+++ contrib/brl/bbas/brad/brad_phongs_model_est.h
@@ -1,28 +1,28 @@
+#ifndef brad_phongs_model_est_h_
+#define brad_phongs_model_est_h_
+
 #include <vnl/vnl_least_squares_function.h>
 #include <vnl/vnl_double_3.h>
 
-
 class brad_phongs_model
 {
+ public:
+  brad_phongs_model(float kd, float ks, float gamma, float normal_elev, float normal_azim);
+  ~brad_phongs_model(){}
 
-public:
-    brad_phongs_model(float kd, float ks, float gamma, float normal_elev, float normal_azim);
-    ~brad_phongs_model(){}
+  float val(float view_elev, float view_azim, float sun_elev, float sun_azim);
+ protected:
 
-    float val(float view_elev, float view_azim, float sun_elev, float sun_azim);
-protected:
-
-    float kd_;
-    float ks_;
-    float gamma_;
-    vnl_double_3 normal_;
-
-
+  float kd_;
+  float ks_;
+  float gamma_;
+  vnl_double_3 normal_;
 };
+
 class brad_phongs_model_est : public vnl_least_squares_function
 {
-public:
-  brad_phongs_model_est(double sun_elev, 
+ public:
+  brad_phongs_model_est(double sun_elev,
                         double sun_azim,
                         vnl_vector<double> & camera_elev,
                         vnl_vector<double> & camera_azim,
@@ -34,12 +34,13 @@ public:
 
   void gradf(vnl_vector<double> const& x, vnl_matrix<double> &J);
 
-protected:
-    double sun_elev_;
-    double sun_azim_;
-    vnl_vector<double>  camera_elev_;
-    vnl_vector<double>  camera_azim_;
-    vnl_vector<double>  obs_;
-    vnl_vector<double>  obs_weights_;
-
+ protected:
+  double sun_elev_;
+  double sun_azim_;
+  vnl_vector<double>  camera_elev_;
+  vnl_vector<double>  camera_azim_;
+  vnl_vector<double>  obs_;
+  vnl_vector<double>  obs_weights_;
 };
+
+#endif // brad_phongs_model_est_h_
