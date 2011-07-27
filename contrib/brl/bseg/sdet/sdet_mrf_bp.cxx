998b0a20e4108f46834f082b13d24ca9798e91e9
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-07-27 04:50:45 -0400

    avoid compiler warning (unused variable)
    SVN-Revision: 32647

--- contrib/brl/bseg/sdet/sdet_mrf_bp.cxx
+++ contrib/brl/bseg/sdet/sdet_mrf_bp.cxx
@@ -18,7 +18,7 @@ void lower_envelope_linear(float w, vcl_vector<float>& msg)
 {
   unsigned nlabels = msg.size();
   // pass 1
-  for (unsigned fq=1; fq<nlabels; ++fq){
+  for (unsigned fq=1; fq<nlabels; ++fq) {
     float mfq = msg[fq];
     float mfqm1 = msg[fq-1] + w;
     if (mfq<mfqm1)
@@ -27,7 +27,7 @@ void lower_envelope_linear(float w, vcl_vector<float>& msg)
       msg[fq]=mfqm1;
   }
   // pass 2
-  for (int fq=nlabels-2; fq>=0; --fq){
+  for (int fq=nlabels-2; fq>=0; --fq) {
     float mfq = msg[fq];
     float mfqp1 = msg[fq+1]+w;
     if (mfq<mfqp1)
@@ -49,21 +49,21 @@ vcl_vector<float> lower_envelope_quadratic(float w,
   z[0] = -vnl_numeric_traits<float>::maxval;
   z[1] = -z[0];
 
-  for (int fq = 1; fq <= nlabels-1; fq++) {
+  for (int fq = 1; fq <= nlabels-1; ++fq) {
     float s  = ((h[fq]+w*fq*fq)-(h[v[k]]+w*v[k]*v[k])) / (2*w*(fq-v[k]));
     while (s <= z[k]) {
       k--;
       s  = ((h[fq]+fq*fq)-(h[v[k]]+v[k]*v[k])) / (2*(fq-v[k]));
     }
-    k++;
+    ++k;
     v[k] = fq;
     z[k] = s;
     z[k+1] = vnl_numeric_traits<float>::maxval;
   }
   k = 0;
-  for (int fq = 0; fq <= nlabels-1; fq++) {
+  for (int fq = 0; fq <= nlabels-1; ++fq) {
     while (z[k+1] < fq)
-      k++;
+      ++k;
     env_out[fq] = w*(fq-v[k])*(fq-v[k]) + h[v[k]];
   }
   return env_out;
@@ -97,7 +97,7 @@ sdet_mrf_bp::sdet_mrf_bp(vil_image_resource_sptr obs_labels, unsigned n_labels,
   sites_.resize(nj_, ni_);
   float scale = (n_labels-1)/(max_-min_);
   for (unsigned j = 0; j<nj_; ++j)
-    for (unsigned i = 0; i<ni_; ++i){
+    for (unsigned i = 0; i<ni_; ++i) {
       sdet_mrf_site_bp_sptr s =
         new sdet_mrf_site_bp(n_labels_, lambda_, truncation_cost_);
       s->set_label(scale*(view(i,j)-min_));
@@ -119,7 +119,7 @@ sdet_mrf_bp::sdet_mrf_bp(vil_image_view<float> const& obs_labels,
   sites_.resize(nj_, ni_);
   float scale = (n_labels-1)/(max_-min_);
   for (unsigned j = 0; j<nj_; ++j)
-    for (unsigned i = 0; i<ni_; ++i){
+    for (unsigned i = 0; i<ni_; ++i) {
       sdet_mrf_site_bp_sptr s =
         new sdet_mrf_site_bp(n_labels_, lambda_, truncation_cost_);
       s->set_label(scale*(obs_labels(i,j)-min_));
@@ -143,7 +143,7 @@ sdet_mrf_bp::sdet_mrf_bp(vil_image_view<float> const& obs_labels,
   float scale = (n_labels-1)/(max_-min_);
   int ni = static_cast<int>(ni_), nj = static_cast<int>(nj_);
   for (int j = 0; j<nj; ++j)
-    for (int i = 0; i<ni; ++i){
+    for (int i = 0; i<ni; ++i) {
       float vlamb = lambda_;
       if (var(i,j)>0.0f)
         vlamb = lambda_/var(i,j);
@@ -159,7 +159,7 @@ sdet_mrf_bp::sdet_mrf_bp(vil_image_resource_sptr  obs_labels,
                          unsigned n_labels, float discontinuity_cost,
                          float truncation_cost, float kappa, float lambda)
   :  n_labels_(n_labels),discontinuity_cost_(discontinuity_cost),
-     truncation_cost_(truncation_cost), kappa_(kappa), lambda_(lambda), 
+     truncation_cost_(truncation_cost), kappa_(kappa), lambda_(lambda),
      min_(0.0f), max_(0.0f)
 {
   if (!obs_labels) return;
@@ -174,7 +174,7 @@ sdet_mrf_bp::sdet_mrf_bp(vil_image_resource_sptr  obs_labels,
   vil_image_view<float> var_view = *vil_convert_cast(float(), temp);
   int ni = static_cast<int>(ni_), nj = static_cast<int>(nj_);
   for (int j = 0; j<nj; ++j)
-    for (int i = 0; i<ni; ++i){
+    for (int i = 0; i<ni; ++i) {
       float vlamb = lambda_/var_view(i,j);
       sdet_mrf_site_bp_sptr s =
         new sdet_mrf_site_bp(n_labels_,vlamb, truncation_cost_);
@@ -187,12 +187,11 @@ void sdet_mrf_bp::send_messages_optimized()
 {
   int ni = static_cast<int>(ni_), nj = static_cast<int>(nj_);
   float maxb = static_cast<float>(vnl_numeric_traits<short>::maxval);
-  float minb = -maxb;
   for (int j = 0; j<nj; ++j)
-    for (int i = 0; i<ni; ++i){
+    for (int i = 0; i<ni; ++i) {
       //site sending the messages
       sdet_mrf_site_bp_sptr sp = sites_[j][i];
-      for (int n = 0; n<4; ++n){
+      for (int n = 0; n<4; ++n) {
         int ki = i+di[n], kj = j+dj[n];
         if (ki<0||ki>=ni||kj<0||kj>=nj)
           continue;
@@ -203,7 +202,7 @@ void sdet_mrf_bp::send_messages_optimized()
         vcl_vector<float> temp(n_labels_), msg;
         // minr is the smallest value of h
         float minh = vnl_numeric_traits<float>::maxval;
-        for (unsigned fq = 0; fq<n_labels_; ++fq){
+        for (unsigned fq = 0; fq<n_labels_; ++fq) {
           temp[fq]=sp->h(n, fq);
           if (temp[fq]<minh)
             minh = temp[fq];
@@ -225,18 +224,18 @@ void sdet_mrf_bp::send_messages_optimized()
         summ /= n_labels_;
 
         //subtract the average
-        for (unsigned fq=0; fq<n_labels_; ++fq){
+        for (unsigned fq=0; fq<n_labels_; ++fq) {
           float ms = msg[fq]-summ;
           //if this assert fails, the number of labels is too large
           //compared to the dynamic range of the message elements
-          assert(ms<maxb&&ms>minb);
+          assert(ms<maxb&&ms>-maxb);
           sq->set_cur_message(3-n, fq, ms);
         }
       }
     }
   //all done sending messages so swap buffers
   for (int j = 0; j<nj; ++j)
-    for (int i = 0; i<ni; ++i){
+    for (int i = 0; i<ni; ++i) {
       sdet_mrf_site_bp_sptr sp = sites_[j][i];
       sp->switch_buffers();
     }
@@ -256,7 +255,7 @@ void sdet_mrf_bp::print_prior_messages()
            << "     3\n\n";
 
   for (unsigned j = 0; j<nj_; ++j)
-    for (unsigned i = 0; i<ni_; ++i){
+    for (unsigned i = 0; i<ni_; ++i) {
       sdet_mrf_site_bp_sptr sp = sites_[j][i];
       vcl_cout << " site(" << i << ' ' << j << ")==>\n";
       sp->print_prior_messages();
@@ -266,7 +265,7 @@ void sdet_mrf_bp::print_prior_messages()
 void sdet_mrf_bp::print_belief_vectors()
 {
   for (unsigned j = 0; j<nj_; ++j)
-    for (unsigned i = 0; i<ni_; ++i){
+    for (unsigned i = 0; i<ni_; ++i) {
       sdet_mrf_site_bp_sptr sp = sites_[j][i];
       vcl_cout << " site(" << i << ' ' << j << ")==>\n";
       sp->print_belief_vector();
@@ -279,11 +278,11 @@ vil_image_resource_sptr sdet_mrf_bp::belief_image()
   if (nj_==0||ni_==0)
     return ret;
   vil_image_view<float> view(ni_, nj_);
-  if(min_==max_)
+  if (min_==max_)
     return ret;
   float scale = (max_-min_)/static_cast<float>(n_labels_-1);
   for (unsigned j = 0; j<nj_; ++j)
-    for (unsigned i = 0; i<ni_; ++i){
+    for (unsigned i = 0; i<ni_; ++i) {
       sdet_mrf_site_bp_sptr sp = sites_[j][i];
       if (!sp) continue;
       float label = static_cast<float>(sp->believed_label());
@@ -302,7 +301,7 @@ vcl_vector<float> sdet_mrf_bp::prior_message(unsigned i, unsigned j, unsigned n)
 void sdet_mrf_bp::clear()
 {
   for (unsigned j = 0; j<nj_; ++j)
-    for (unsigned i = 0; i<ni_; ++i){
+    for (unsigned i = 0; i<ni_; ++i) {
       sdet_mrf_site_bp_sptr sp = sites_[j][i];
       if (!sp) continue;
       sp->clear();
