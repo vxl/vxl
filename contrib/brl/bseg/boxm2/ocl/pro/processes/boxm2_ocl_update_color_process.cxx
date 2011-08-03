eccba5afa56a20486ef164b558c7eef521b52de8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-08-03 11:10:23 -0400

    "bool" function should return a value
    SVN-Revision: 32701

--- contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_update_color_process.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_update_color_process.cxx
@@ -121,11 +121,11 @@ bool boxm2_ocl_update_color_process_cons(bprb_func_process& pro)
   vcl_vector<vcl_string>  output_types_(n_outputs_);
 
   bool good = pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
-  // in case the 6th input is not set
+  // in case the 5th and/or the 6th input would not be set
   brdb_value_sptr idx = new brdb_value_t<vcl_string>("");
   pro.set_input(5, idx);
-
   pro.set_input(6, idx);
+  return good;
 }
 
 bool boxm2_ocl_update_color_process(bprb_func_process& pro)
@@ -301,217 +301,216 @@ bool boxm2_ocl_update_color_process(bprb_func_process& pro)
       continue;
     }
     vis_image->read_to_buffer(queue);
-    if(mask_filename!="")
+    if (mask_filename!="")
     {
-        vcl_cout<<"MASK FOUND "<<vcl_endl;
-        vil_image_view_base_sptr mask_img=vil_load(mask_filename.c_str());
-        if(vil_image_view<unsigned char> * mask_char
-            =dynamic_cast<vil_image_view<unsigned char> * >(mask_img.ptr()))
-        {
-            int count = 0;
-            for (unsigned int j=0;j<cl_nj;++j)
-                for (unsigned int i=0;i<cl_ni;++i)
-                {
-                    if (i<mask_char->ni() && j<mask_char->nj()  ) 
-                    {
-                        if( (*mask_char)(i,j)>0 )
-                        {
-                            input_buff[numFloats*count+0]=-1.0f;
-                            vis_buff[count] =-1.0f;
-                        }
-                    }
-                    ++count;
-                }
-        }
+      vcl_cout<<"MASK FOUND"<<vcl_endl;
+      vil_image_view_base_sptr mask_img=vil_load(mask_filename.c_str());
+      if (vil_image_view<unsigned char> * mask_char
+          =dynamic_cast<vil_image_view<unsigned char> * >(mask_img.ptr()))
+      {
+        int count = 0;
+        for (unsigned int j=0;j<cl_nj;++j)
+          for (unsigned int i=0;i<cl_ni;++i)
+          {
+            if (i<mask_char->ni() && j<mask_char->nj()  )
+            {
+              if ( (*mask_char)(i,j)>0 )
+              {
+                input_buff[numFloats*count+0]=-1.0f;
+                vis_buff[count] =-1.0f;
+              }
+            }
+            ++count;
+          }
+      }
     }
     in_image->write_to_buffer(queue);
     vis_image->write_to_buffer(queue);
     clFinish(queue);
     for (id = vis_order.begin(); id != vis_order.end(); ++id)
     {
-        //choose correct render kernel
-        boxm2_block_metadata mdata = scene->get_block_metadata(*id);
-        bocl_kernel* kern =  kernels[identifier][i];
+      //choose correct render kernel
+      boxm2_block_metadata mdata = scene->get_block_metadata(*id);
+      bocl_kernel* kern =  kernels[identifier][i];
 
-        //write the image values to the buffer
-        vul_timer transfer;
-        bocl_mem* blk       = opencl_cache->get_block(*id);
-        bocl_mem* alpha     = opencl_cache->get_data<BOXM2_ALPHA>(*id, 0, false);
-        bocl_mem * blk_info  = opencl_cache->loaded_block_info();
+      //write the image values to the buffer
+      vul_timer transfer;
+      bocl_mem* blk       = opencl_cache->get_block(*id);
+      bocl_mem* alpha     = opencl_cache->get_data<BOXM2_ALPHA>(*id, 0, false);
+      bocl_mem * blk_info  = opencl_cache->loaded_block_info();
 
-        //make sure the scene info data size reflects the real data size
-        boxm2_scene_info* info_buffer = (boxm2_scene_info*) blk_info->cpu_buffer();
-        int alphaTypeSize = boxm2_data_info::datasize(boxm2_data_traits<BOXM2_ALPHA>::prefix());
-        info_buffer->data_buffer_length = (int) (alpha->num_bytes()/alphaTypeSize);
-        blk_info->write_to_buffer((queue));
+      //make sure the scene info data size reflects the real data size
+      boxm2_scene_info* info_buffer = (boxm2_scene_info*) blk_info->cpu_buffer();
+      int alphaTypeSize = boxm2_data_info::datasize(boxm2_data_traits<BOXM2_ALPHA>::prefix());
+      info_buffer->data_buffer_length = (int) (alpha->num_bytes()/alphaTypeSize);
+      blk_info->write_to_buffer((queue));
 
-        // data type string may contain an identifier so determine the buffer size
-        int nobsTypeSize = (int)boxm2_data_info::datasize(num_obs_type);
-        int appTypeSize = (int) boxm2_data_info::datasize(data_type); 
-        bocl_mem* mog       = opencl_cache->get_data(*id, data_type, info_buffer->data_buffer_length*appTypeSize, false);    
-        bocl_mem* num_obs   = opencl_cache->get_data(*id, num_obs_type, info_buffer->data_buffer_length*nobsTypeSize, false);
+      // data type string may contain an identifier so determine the buffer size
+      int nobsTypeSize = (int)boxm2_data_info::datasize(num_obs_type);
+      int appTypeSize = (int) boxm2_data_info::datasize(data_type);
+      bocl_mem* mog       = opencl_cache->get_data(*id, data_type, info_buffer->data_buffer_length*appTypeSize, false);
+      bocl_mem* num_obs   = opencl_cache->get_data(*id, num_obs_type, info_buffer->data_buffer_length*nobsTypeSize, false);
 
-        //grab an appropriately sized AUX data buffer
-        int auxTypeSize = boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX0>::prefix());
-        bocl_mem *aux0   = opencl_cache->get_data<BOXM2_AUX0>(*id, info_buffer->data_buffer_length*auxTypeSize);
-        auxTypeSize = boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX1>::prefix());
-        bocl_mem *aux1   = opencl_cache->get_data<BOXM2_AUX1>(*id, info_buffer->data_buffer_length*auxTypeSize);
-        auxTypeSize = boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX2>::prefix());
-        bocl_mem *aux2   = opencl_cache->get_data<BOXM2_AUX2>(*id, info_buffer->data_buffer_length*auxTypeSize);
-        auxTypeSize = boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX3>::prefix());
-        bocl_mem *aux3   = opencl_cache->get_data<BOXM2_AUX3>(*id, info_buffer->data_buffer_length*auxTypeSize);
+      //grab an appropriately sized AUX data buffer
+      int auxTypeSize = boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX0>::prefix());
+      bocl_mem *aux0   = opencl_cache->get_data<BOXM2_AUX0>(*id, info_buffer->data_buffer_length*auxTypeSize);
+      auxTypeSize = boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX1>::prefix());
+      bocl_mem *aux1   = opencl_cache->get_data<BOXM2_AUX1>(*id, info_buffer->data_buffer_length*auxTypeSize);
+      auxTypeSize = boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX2>::prefix());
+      bocl_mem *aux2   = opencl_cache->get_data<BOXM2_AUX2>(*id, info_buffer->data_buffer_length*auxTypeSize);
+      auxTypeSize = boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX3>::prefix());
+      bocl_mem *aux3   = opencl_cache->get_data<BOXM2_AUX3>(*id, info_buffer->data_buffer_length*auxTypeSize);
 
-        transfer_time += (float) transfer.all();
-        if (i==UPDATE_SEGLEN)
-        {
-            local_threads[0] = 8; local_threads[1] = 8;
-            global_threads[0]=cl_ni; global_threads[1]=cl_nj;
-            kern->set_arg( blk_info );
-            kern->set_arg( blk );
-            kern->set_arg( alpha );
-            kern->set_arg( aux0 );
-            kern->set_arg( aux1 );
-            kern->set_arg( aux2 );
-            kern->set_arg( aux3 );
-            kern->set_arg( lookup.ptr() );
-            // kern->set_arg( persp_cam.ptr() );
-            kern->set_arg( ray_o_buff.ptr() );
-            kern->set_arg( ray_d_buff.ptr() );
+      transfer_time += (float) transfer.all();
+      if (i==UPDATE_SEGLEN)
+      {
+        local_threads[0] = 8; local_threads[1] = 8;
+        global_threads[0]=cl_ni; global_threads[1]=cl_nj;
+        kern->set_arg( blk_info );
+        kern->set_arg( blk );
+        kern->set_arg( alpha );
+        kern->set_arg( aux0 );
+        kern->set_arg( aux1 );
+        kern->set_arg( aux2 );
+        kern->set_arg( aux3 );
+        kern->set_arg( lookup.ptr() );
+        // kern->set_arg( persp_cam.ptr() );
+        kern->set_arg( ray_o_buff.ptr() );
+        kern->set_arg( ray_d_buff.ptr() );
 
-            kern->set_arg( img_dim.ptr() );
-            kern->set_arg( in_image.ptr() );
-            kern->set_arg( cl_output.ptr() );
-            kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_uchar16) );//local tree,
-            kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_uchar4) ); //ray bundle,
-            kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_int) );    //cell pointers,
-            kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_float4) ); //cached aux,
-            kern->set_local_arg( local_threads[0]*local_threads[1]*10*sizeof(cl_uchar) ); //cumsum buffer, imindex buffer
-        }
-        else if (i==UPDATE_COMPRESS)
-        {
-            local_threads[0] = 64;
-            local_threads[1] = 1 ;
-            global_threads[0]=RoundUp(info_buffer->data_buffer_length,local_threads[0]);
-            global_threads[1]=1;
-            kern->set_arg( blk_info );
-            kern->set_arg( aux0 );
-            kern->set_arg( aux1 );
-            kern->set_arg( aux2 );
-            kern->set_arg( aux3 );
-        }
-        else if (i==UPDATE_PREINF)
-        {
-            local_threads[0] = 8; local_threads[1] = 8;
-            global_threads[0]=cl_ni; global_threads[1]=cl_nj;
-            kern->set_arg( blk_info );
-            kern->set_arg( blk );
-            kern->set_arg( alpha );
-            kern->set_arg( mog );
-            kern->set_arg( num_obs );
-            kern->set_arg( aux0 );
-            kern->set_arg( aux1 );
-            kern->set_arg( lookup.ptr() );
+        kern->set_arg( img_dim.ptr() );
+        kern->set_arg( in_image.ptr() );
+        kern->set_arg( cl_output.ptr() );
+        kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_uchar16) );//local tree,
+        kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_uchar4) ); //ray bundle,
+        kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_int) );    //cell pointers,
+        kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_float4) ); //cached aux,
+        kern->set_local_arg( local_threads[0]*local_threads[1]*10*sizeof(cl_uchar) ); //cumsum buffer, imindex buffer
+      }
+      else if (i==UPDATE_COMPRESS)
+      {
+        local_threads[0] = 64;
+        local_threads[1] = 1 ;
+        global_threads[0]=RoundUp(info_buffer->data_buffer_length,local_threads[0]);
+        global_threads[1]=1;
+        kern->set_arg( blk_info );
+        kern->set_arg( aux0 );
+        kern->set_arg( aux1 );
+        kern->set_arg( aux2 );
+        kern->set_arg( aux3 );
+      }
+      else if (i==UPDATE_PREINF)
+      {
+        local_threads[0] = 8; local_threads[1] = 8;
+        global_threads[0]=cl_ni; global_threads[1]=cl_nj;
+        kern->set_arg( blk_info );
+        kern->set_arg( blk );
+        kern->set_arg( alpha );
+        kern->set_arg( mog );
+        kern->set_arg( num_obs );
+        kern->set_arg( aux0 );
+        kern->set_arg( aux1 );
+        kern->set_arg( lookup.ptr() );
 
-            // kern->set_arg( persp_cam.ptr() );
-            kern->set_arg( ray_o_buff.ptr() );
-            kern->set_arg( ray_d_buff.ptr() );
+        // kern->set_arg( persp_cam.ptr() );
+        kern->set_arg( ray_o_buff.ptr() );
+        kern->set_arg( ray_d_buff.ptr() );
 
-            kern->set_arg( img_dim.ptr() );
-            kern->set_arg( vis_image.ptr() );
-            kern->set_arg( pre_image.ptr() );
-            kern->set_arg( cl_output.ptr() );
-            kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_uchar16) );//local tree,
-            kern->set_local_arg( local_threads[0]*local_threads[1]*10*sizeof(cl_uchar) ); //cumsum buffer, imindex buffer
-        }
-        else if (i==UPDATE_BAYES)
-        {
-            local_threads[0] = 8; local_threads[1] = 8;
-            global_threads[0]=cl_ni; global_threads[1]=cl_nj;
-            kern->set_arg( blk_info );
-            kern->set_arg( blk );
-            kern->set_arg( alpha );
-            kern->set_arg( mog );
-            kern->set_arg( num_obs );
-            kern->set_arg( aux0 );
-            kern->set_arg( aux1 );
-            kern->set_arg( aux2 );
-            kern->set_arg( aux3 );
-            kern->set_arg( lookup.ptr() );
+        kern->set_arg( img_dim.ptr() );
+        kern->set_arg( vis_image.ptr() );
+        kern->set_arg( pre_image.ptr() );
+        kern->set_arg( cl_output.ptr() );
+        kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_uchar16) );//local tree,
+        kern->set_local_arg( local_threads[0]*local_threads[1]*10*sizeof(cl_uchar) ); //cumsum buffer, imindex buffer
+      }
+      else if (i==UPDATE_BAYES)
+      {
+        local_threads[0] = 8; local_threads[1] = 8;
+        global_threads[0]=cl_ni; global_threads[1]=cl_nj;
+        kern->set_arg( blk_info );
+        kern->set_arg( blk );
+        kern->set_arg( alpha );
+        kern->set_arg( mog );
+        kern->set_arg( num_obs );
+        kern->set_arg( aux0 );
+        kern->set_arg( aux1 );
+        kern->set_arg( aux2 );
+        kern->set_arg( aux3 );
+        kern->set_arg( lookup.ptr() );
 
-            // kern->set_arg( persp_cam.ptr() );
-            kern->set_arg( ray_o_buff.ptr() );
-            kern->set_arg( ray_d_buff.ptr() );
+        // kern->set_arg( persp_cam.ptr() );
+        kern->set_arg( ray_o_buff.ptr() );
+        kern->set_arg( ray_d_buff.ptr() );
 
-            kern->set_arg( img_dim.ptr() );
-            kern->set_arg( vis_image.ptr() );
-            kern->set_arg( pre_image.ptr() );
-            kern->set_arg( norm_image.ptr() );
-            kern->set_arg( cl_output.ptr() );
-            kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_uchar16) );//local tree,
-            kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_short2) ); //ray bundle,
-            kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_int) );    //cell pointers,
-            kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_float) ); //cached aux,
-            kern->set_local_arg( local_threads[0]*local_threads[1]*10*sizeof(cl_uchar) ); //cumsum buffer, imindex buffer
-        }
-        else if (i==UPDATE_CELL)
-        {
-            local_threads[0] = 64;
-            local_threads[1] = 1 ;
-            global_threads[0]=RoundUp(info_buffer->data_buffer_length,local_threads[0]);
-            global_threads[1]=1;
+        kern->set_arg( img_dim.ptr() );
+        kern->set_arg( vis_image.ptr() );
+        kern->set_arg( pre_image.ptr() );
+        kern->set_arg( norm_image.ptr() );
+        kern->set_arg( cl_output.ptr() );
+        kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_uchar16) );//local tree,
+        kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_short2) ); //ray bundle,
+        kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_int) );    //cell pointers,
+        kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_float) ); //cached aux,
+        kern->set_local_arg( local_threads[0]*local_threads[1]*10*sizeof(cl_uchar) ); //cumsum buffer, imindex buffer
+      }
+      else if (i==UPDATE_CELL)
+      {
+        local_threads[0] = 64;
+        local_threads[1] = 1 ;
+        global_threads[0]=RoundUp(info_buffer->data_buffer_length,local_threads[0]);
+        global_threads[1]=1;
 
-            kern->set_arg( blk_info );
-            kern->set_arg( alpha );
-            kern->set_arg( mog );
-            kern->set_arg( num_obs );
-            kern->set_arg( aux0 );
-            kern->set_arg( aux1 );
-            kern->set_arg( aux2 );
-            kern->set_arg( aux3 );
-            kern->set_arg( cl_output.ptr() );
-        }
-        //execute kernel
-        kern->execute(queue, 2, local_threads, global_threads);
-        int status = clFinish(queue);
-        if (!check_val(status, MEM_FAILURE, "UPDATE EXECUTE FAILED: " + error_to_string(status)) )
-          return false;
-        gpu_time += kern->exec_time();
+        kern->set_arg( blk_info );
+        kern->set_arg( alpha );
+        kern->set_arg( mog );
+        kern->set_arg( num_obs );
+        kern->set_arg( aux0 );
+        kern->set_arg( aux1 );
+        kern->set_arg( aux2 );
+        kern->set_arg( aux3 );
+        kern->set_arg( cl_output.ptr() );
+      }
+      //execute kernel
+      kern->execute(queue, 2, local_threads, global_threads);
+      int status = clFinish(queue);
+      if (!check_val(status, MEM_FAILURE, "UPDATE EXECUTE FAILED: " + error_to_string(status)) )
+        return false;
+      gpu_time += kern->exec_time();
 
-        //clear render kernel args so it can reset em on next execution
-        kern->clear_args();
+      //clear render kernel args so it can reset em on next execution
+      kern->clear_args();
 
-        //read info back to host memory
-        blk->read_to_buffer(queue);
-        alpha->read_to_buffer(queue);
-        mog->read_to_buffer(queue);
-        num_obs->read_to_buffer(queue);
-        aux0->read_to_buffer(queue);
-        aux1->read_to_buffer(queue);
-        aux2->read_to_buffer(queue);
-        aux3->read_to_buffer(queue);
+      //read info back to host memory
+      blk->read_to_buffer(queue);
+      alpha->read_to_buffer(queue);
+      mog->read_to_buffer(queue);
+      num_obs->read_to_buffer(queue);
+      aux0->read_to_buffer(queue);
+      aux1->read_to_buffer(queue);
+      aux2->read_to_buffer(queue);
+      aux3->read_to_buffer(queue);
 
-        //read image out to buffer (from gpu)
-        in_image->read_to_buffer(queue);
-        vis_image->read_to_buffer(queue);
-        pre_image->read_to_buffer(queue);
-        cl_output->read_to_buffer(queue);
-        clFinish(queue);
+      //read image out to buffer (from gpu)
+      in_image->read_to_buffer(queue);
+      vis_image->read_to_buffer(queue);
+      pre_image->read_to_buffer(queue);
+      cl_output->read_to_buffer(queue);
+      clFinish(queue);
     }
   }
 
+  delete [] vis_buff;
+  delete [] pre_buff;
+  delete [] norm_buff;
+  delete [] input_buff;
+  delete [] ray_origins;
+  delete [] ray_directions;
 
-   delete [] vis_buff;
-   delete [] pre_buff;
-   delete [] norm_buff;
-   delete [] input_buff;
-   delete [] ray_origins;
-   delete [] ray_directions;
-
-   //reset local threads to 8/8 (default);
-   local_threads[0] = 8;
-   local_threads[1] = 8;
-   global_threads[0]=cl_ni;
-   global_threads[1]=cl_nj;
+  //reset local threads to 8/8 (default);
+  local_threads[0] = 8;
+  local_threads[1] = 8;
+  global_threads[0]=cl_ni;
+  global_threads[1]=cl_nj;
 
   vcl_cout<<"Gpu time "<<gpu_time<<" transfer time "<<transfer_time<<vcl_endl;
   clReleaseCommandQueue(queue);
