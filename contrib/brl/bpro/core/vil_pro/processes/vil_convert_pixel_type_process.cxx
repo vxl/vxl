30b206922b866738178e97ae68d9559487c84ad2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-08-04 17:26:24 -0400

    brief Doxygen comment should be on a single line
    SVN-Revision: 32710

--- contrib/brl/bpro/core/vil_pro/processes/vil_convert_pixel_type_process.cxx
+++ contrib/brl/bpro/core/vil_pro/processes/vil_convert_pixel_type_process.cxx
@@ -2,6 +2,7 @@
 #include <bprb/bprb_func_process.h>
 //:
 // \file
+// \brief Converts floating point image (0,1.0) to byte image (0,255) for compression/saveability reasons
 
 #include <bprb/bprb_parameters.h>
 #include <vcl_iostream.h>
@@ -10,9 +11,6 @@
 #include <vil/vil_math.h>
 #include <vil/vil_convert.h>
 
-//: Converts floating point image (0,1.0) to byte image (0,255) for
-//  compression/saveability reasons
-
 //: Constructor
 bool vil_convert_pixel_type_process_cons(bprb_func_process& pro)
 {
@@ -20,7 +18,7 @@ bool vil_convert_pixel_type_process_cons(bprb_func_process& pro)
   // input(0): the vil_image_view_base_sptr
   vcl_vector<vcl_string> input_types;
   input_types.push_back("vil_image_view_base_sptr");
-  input_types.push_back("vcl_string"); 
+  input_types.push_back("vcl_string");
   bool ok = pro.set_input_types(input_types);
 
   //this process has 1 outputs
@@ -41,67 +39,67 @@ bool vil_convert_pixel_type_process(bprb_func_process& pro)
     return false;
   }
   unsigned i=0;
-  
+
   //Retrieve image from input
   vil_image_view_base_sptr img = pro.get_input<vil_image_view_base_sptr>(i++);
 
   //retrieve output type
   vcl_string out_type = pro.get_input<vcl_string>(i++);
-  
+
   ////////////////////////////////////////////////////////////////////
   //Convert to float image
   ////////////////////////////////////////////////////////////////////
-  vil_image_view<float> fimage; 
-  if(img->pixel_format() == VIL_PIXEL_FORMAT_RGBA_BYTE ) {
-    vil_image_view_base_sptr plane_image = vil_convert_to_n_planes(4, img); 
+  vil_image_view<float> fimage;
+  if (img->pixel_format() == VIL_PIXEL_FORMAT_RGBA_BYTE ) {
+    vil_image_view_base_sptr plane_image = vil_convert_to_n_planes(4, img);
     fimage = *vil_convert_cast(float(), plane_image);
-    vil_math_scale_values(fimage, 1.0/255.0); 
+    vil_math_scale_values(fimage, 1.0/255.0);
   }
   else {
     //can use convert cast
     fimage = *vil_convert_cast(float(), img);
-    
+
     // convert input image to float in range from 0 to 1
     if (img->pixel_format() == VIL_PIXEL_FORMAT_BYTE)
       vil_math_scale_values(fimage,1.0/255.0);
     if (img->pixel_format() == VIL_PIXEL_FORMAT_UINT_16)
       vil_math_scale_values(fimage,1.0/65536);
   }
-  unsigned ni = fimage.ni(); 
-  unsigned nj = fimage.nj(); 
-  unsigned nplanes = fimage.nplanes(); 
+  unsigned ni = fimage.ni();
+  unsigned nj = fimage.nj();
+  unsigned nplanes = fimage.nplanes();
 
   ////////////////////////////////////////////////////////////////////
   //return image of same type (convert back)
   ////////////////////////////////////////////////////////////////////
-  if(out_type=="float") {
-      vil_image_view<float>* bimage = new vil_image_view<float>(ni, nj, nplanes); 
-      vil_convert_cast(fimage, *bimage); 
-      pro.set_output_val<vil_image_view_base_sptr>(0, bimage);
-      return true; 
+  if (out_type=="float") {
+    vil_image_view<float>* bimage = new vil_image_view<float>(ni, nj, nplanes);
+    vil_convert_cast(fimage, *bimage);
+    pro.set_output_val<vil_image_view_base_sptr>(0, bimage);
+    return true;
   }
-  else if(out_type=="byte") {    
-      vil_math_scale_values( fimage, 255.0 ); 
-      vil_image_view<vxl_byte>* bimage = new vil_image_view<vxl_byte>(ni, nj, nplanes); 
-      vil_convert_cast(fimage, *bimage); 
-      pro.set_output_val<vil_image_view_base_sptr>(0, bimage);
-      return true; 
+  else if (out_type=="byte") {
+    vil_math_scale_values( fimage, 255.0 );
+    vil_image_view<vxl_byte>* bimage = new vil_image_view<vxl_byte>(ni, nj, nplanes);
+    vil_convert_cast(fimage, *bimage);
+    pro.set_output_val<vil_image_view_base_sptr>(0, bimage);
+    return true;
   }
-  else if(out_type=="uint16") {
-      vil_math_scale_values( fimage, 65535.0 ); 
-      vil_image_view<vxl_uint_16>* bimage = new vil_image_view<vxl_uint_16>(ni, nj, nplanes); 
-      vil_convert_cast(fimage, *bimage); 
-      pro.set_output_val<vil_image_view_base_sptr>(0, bimage);    
-      return true; 
+  else if (out_type=="uint16") {
+    vil_math_scale_values( fimage, 65535.0 );
+    vil_image_view<vxl_uint_16>* bimage = new vil_image_view<vxl_uint_16>(ni, nj, nplanes);
+    vil_convert_cast(fimage, *bimage);
+    pro.set_output_val<vil_image_view_base_sptr>(0, bimage);
+    return true;
   }
-  else if(out_type=="rgba")    {
-      vil_math_scale_values( fimage, 255.0); 
-      vil_image_view<vxl_byte>* bimage = new vil_image_view<vxl_byte>(ni, nj, nplanes); 
-      vil_convert_cast(fimage, *bimage); 
-      pro.set_output_val<vil_image_view_base_sptr>(0, vil_convert_to_component_order(bimage));    
-      return true; 
+  else if (out_type=="rgba")    {
+    vil_math_scale_values( fimage, 255.0);
+    vil_image_view<vxl_byte>* bimage = new vil_image_view<vxl_byte>(ni, nj, nplanes);
+    vil_convert_cast(fimage, *bimage);
+    pro.set_output_val<vil_image_view_base_sptr>(0, vil_convert_to_component_order(bimage));
+    return true;
   }
-  
-  return true;
+  else
+    return false;
 }
 
