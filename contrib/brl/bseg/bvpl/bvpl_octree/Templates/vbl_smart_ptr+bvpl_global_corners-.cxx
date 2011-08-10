836bcee323146693f0725a67752677dd7a2226d4
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-08-10 11:20:07 -0400

    added corner related files
    SVN-Revision: 32777

---
+++ contrib/brl/bseg/bvpl/bvpl_octree/Templates/vbl_smart_ptr+bvpl_global_corners-.cxx
@@ -0,0 +1,4 @@
+#include <bvpl/bvpl_octree/bvpl_global_corners.h>
+#include <vbl/vbl_smart_ptr.txx>
+
+VBL_SMART_PTR_INSTANTIATE(bvpl_global_corners);
---
+++ contrib/brl/bseg/bvpl/bvpl_octree/Templates/vbl_smart_ptr+bvpl_global_taylor-.cxx
@@ -0,0 +1,6 @@
+#include <bvpl/bvpl_octree/bvpl_global_taylor.h>
+#include <vbl/vbl_smart_ptr.txx>
+
+VBL_SMART_PTR_INSTANTIATE(bvpl_global_taylor);
+
+
