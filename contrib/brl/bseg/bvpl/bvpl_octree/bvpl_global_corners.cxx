1147224fee0287e60e916bf80a8e7a93ab1b2dbd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-08-13 18:04:57 -0400

    replaced UTF-8 character by ASCII to avoid potential misinterpretation (Doxygen text fragment)
    SVN-Revision: 32808

--- contrib/brl/bseg/bvpl/bvpl_octree/bvpl_global_corners.cxx
+++ contrib/brl/bseg/bvpl/bvpl_octree/bvpl_global_corners.cxx
@@ -38,20 +38,20 @@ bvpl_global_corners::bvpl_global_corners(const vcl_string &path)
     vcl_cerr << "Error: bvpl_discover_pca_kernels - could not parse xml root\n";
     throw;
   }
-  
+
   //Parse scenes
   bxml_element scenes_query("scene");
   vcl_vector<bxml_data_sptr> scenes_data = bxml_find_all_with_name(root, scenes_query);
-  
+
   unsigned nscenes=scenes_data.size();
   vcl_cout << "Number of scenes: " << nscenes <<vcl_endl;
-    
+
   aux_dirs_.clear();
   aux_dirs_.resize(nscenes);
-  
+
   finest_cell_length_.clear();
   finest_cell_length_.resize(nscenes);
-  
+
   for (unsigned si = 0; si < nscenes; si++)
   {
     bxml_element* scenes_elm = dynamic_cast<bxml_element*>(scenes_data[si].ptr());
@@ -59,23 +59,21 @@ bvpl_global_corners::bvpl_global_corners(const vcl_string &path)
     scenes_elm->get_attribute("id", id);
     scenes_elm->get_attribute("aux_dir", aux_dirs_[id]);
     scenes_elm->get_attribute("cell_length" , finest_cell_length_[id]);
-    
+
     if (!(vul_file::exists(aux_dirs_[id]) && vul_file::is_directory(aux_dirs_[id]))){
       vul_file::make_directory(aux_dirs_[id]);
     }
-    
   }
-  
+
   //Parse kernels
   bxml_element params_query("parameters");
   bxml_data_sptr params_data = bxml_find_by_name(root, params_query);
   if (params_data)
   {
     bxml_element* params_elm = dynamic_cast<bxml_element*>(params_data.ptr());
-    
+
     params_elm->get_attribute("harris_k", harris_k_);
     vcl_cout << "Harris_k is " << harris_k_ << '\n';
-    
   }
   else {
     vcl_cerr << " In Gobal Corners: no parameters element\n";
@@ -84,46 +82,45 @@ bvpl_global_corners::bvpl_global_corners(const vcl_string &path)
 
 
 //: Compute Harris' measure extension to 3-d as proposed by:
-//  I. Laptev. On space-time interest points. Int. J. Computer Vision, 64(2):107–123, 2005
+//  I. Laptev. On space-time interest points. Int. J. Computer Vision, 64(2):107--123, 2005
 void bvpl_global_corners::compute_laptev_corners(bvpl_global_taylor_sptr global_taylor,int scene_id, int block_i, int block_j, int block_k)
 {
-  
   typedef boct_tree<short,vnl_vector_fixed<double,10> > taylor_tree_type;
   typedef boct_tree_cell<short,vnl_vector_fixed<double,10> > taylor_cell_type;
-  
+
   boxm_scene_base_sptr proj_scene_base =global_taylor->load_projection_scene(scene_id);
   boxm_scene_base_sptr valid_scene_base = global_taylor->load_valid_scene(scene_id);
-  
+
   boxm_scene<taylor_tree_type>* proj_scene = dynamic_cast<boxm_scene<taylor_tree_type>*>(proj_scene_base.as_pointer());
   boxm_scene<boct_tree<short, bool> >* valid_scene = dynamic_cast<boxm_scene<boct_tree<short, bool> >*> (valid_scene_base.as_pointer());
-  boxm_scene<boct_tree<short, float> >* corner_scene = 
+  boxm_scene<boct_tree<short, float> >* corner_scene =
   new boxm_scene<boct_tree<short, float> >(valid_scene->lvcs(), valid_scene->origin(), valid_scene->block_dim(), valid_scene->world_dim(), valid_scene->max_level(), valid_scene->init_level());
   corner_scene->set_appearance_model(BOXM_FLOAT);
   corner_scene->set_paths(aux_dirs_[scene_id], "corner_scene");
   corner_scene->write_scene("harris_scene.xml");
-    
+
   if (!( proj_scene && valid_scene && corner_scene ))
   {
     vcl_cerr << "Error in bvpl_global_corners::threshold_corners: Could not cast input scenes\n";
     return;
   }
-  
+
   //init variables
   proj_scene->unload_active_blocks();
   valid_scene->unload_active_blocks();
   corner_scene->unload_active_blocks();
-  
+
   //operate on scene
   bvpl_corner_detector corner_detector;
   bvpl_harris_laptev_functor harris_functor(harris_k_);
-  
-  vgl_point_3d<int> max_neigborhood_idx = global_taylor->kernel_vector_->max();
-  vgl_point_3d<int> min_neigborhood_idx = global_taylor->kernel_vector_->min();
-  
-  vcl_cout << "Neigborhood for harris threshhold: " << min_neigborhood_idx << " , " << max_neigborhood_idx << vcl_endl;
-  
-  corner_detector.compute_C(proj_scene, harris_functor, min_neigborhood_idx, max_neigborhood_idx, block_i, block_j, block_k, valid_scene, corner_scene, finest_cell_length_[scene_id]);
-  
+
+  vgl_point_3d<int> max_neighborhood_idx = global_taylor->kernel_vector_->max();
+  vgl_point_3d<int> min_neighborhood_idx = global_taylor->kernel_vector_->min();
+
+  vcl_cout << "Neighborhood for Harris threshold: " << min_neighborhood_idx << " , " << max_neighborhood_idx << vcl_endl;
+
+  corner_detector.compute_C(proj_scene, harris_functor, min_neighborhood_idx, max_neighborhood_idx, block_i, block_j, block_k, valid_scene, corner_scene, finest_cell_length_[scene_id]);
+
   //clean memory
   proj_scene->unload_active_blocks();
   valid_scene->unload_active_blocks();
@@ -135,37 +132,36 @@ void bvpl_global_corners::compute_laptev_corners(bvpl_global_taylor_sptr global_
 // P. Beaudet, Rotationally invariant image operators, in Proc. 4th Int. Joint Conf. Patt. Recog. 1978.
 void bvpl_global_corners::compute_beaudet_corners(bvpl_global_taylor_sptr global_taylor,int scene_id, int block_i, int block_j, int block_k)
 {
-  
   typedef boct_tree<short,vnl_vector_fixed<double,10> > taylor_tree_type;
   typedef boct_tree_cell<short,vnl_vector_fixed<double,10> > taylor_cell_type;
-  
+
   boxm_scene_base_sptr proj_scene_base =global_taylor->load_projection_scene(scene_id);
   boxm_scene_base_sptr valid_scene_base = global_taylor->load_valid_scene(scene_id);
-  
+
   boxm_scene<taylor_tree_type>* proj_scene = dynamic_cast<boxm_scene<taylor_tree_type>*>(proj_scene_base.as_pointer());
   boxm_scene<boct_tree<short, bool> >* valid_scene = dynamic_cast<boxm_scene<boct_tree<short, bool> >*> (valid_scene_base.as_pointer());
-  boxm_scene<boct_tree<short, float> >* corner_scene = 
+  boxm_scene<boct_tree<short, float> >* corner_scene =
   new boxm_scene<boct_tree<short, float> >(valid_scene->lvcs(), valid_scene->origin(), valid_scene->block_dim(), valid_scene->world_dim(), valid_scene->max_level(), valid_scene->init_level());
   corner_scene->set_appearance_model(BOXM_FLOAT);
   corner_scene->set_paths(aux_dirs_[scene_id], "corner_scene");
   corner_scene->write_scene("harris_scene.xml");
-  
+
   if (!( proj_scene && valid_scene && corner_scene ))
   {
     vcl_cerr << "Error in bvpl_global_corners::threshold_corners: Could not cast input scenes\n";
     return;
   }
-  
+
   //init variables
   proj_scene->unload_active_blocks();
   valid_scene->unload_active_blocks();
   corner_scene->unload_active_blocks();
-  
+
   //operate on scene
   bvpl_corner_detector corner_detector;
 
   corner_detector.compute_det_H(proj_scene, block_i, block_j, block_k, valid_scene, corner_scene);
-  
+
   //clean memory
   proj_scene->unload_active_blocks();
   valid_scene->unload_active_blocks();
@@ -174,67 +170,63 @@ void bvpl_global_corners::compute_beaudet_corners(bvpl_global_taylor_sptr global
 
 
 //: Threshold a percentage of corners, based of the Harris' measure extension to 3-d proposed by:
-//  I. Laptev. On space-time interest points. Int. J. Computer Vision, 64(2):107–123, 2005
+//  I. Laptev. On space-time interest points. Int. J. Computer Vision, 64(2):107--123, 2005
 void bvpl_global_corners::threshold_laptev_corners(bvpl_global_taylor_sptr global_taylor,int scene_id, float harris_thresh, vcl_string output_path)
 {
-  
   typedef boct_tree<short,vnl_vector_fixed<double,10> > taylor_tree_type;
   typedef boct_tree_cell<short,vnl_vector_fixed<double,10> > taylor_cell_type;
-  
+
   boxm_scene_base_sptr valid_scene_base = global_taylor->load_valid_scene(scene_id);
   boxm_scene_base_sptr corner_scene_base = this->load_corner_scene(scene_id);
   boxm_scene_base_sptr proj_scene_base =global_taylor->load_projection_scene(scene_id);
-  
+
   boxm_scene<taylor_tree_type>* proj_scene = dynamic_cast<boxm_scene<taylor_tree_type>*>(proj_scene_base.as_pointer());
-  
+
   boxm_scene<boct_tree<short, bool> >* valid_scene = dynamic_cast<boxm_scene<boct_tree<short, bool> >*> (valid_scene_base.as_pointer());
-  
-  boxm_scene<boct_tree<short, float> >* corner_scene = dynamic_cast<boxm_scene<boct_tree<short, float> >*> (corner_scene_base.as_pointer());  
-   
+
+  boxm_scene<boct_tree<short, float> >* corner_scene = dynamic_cast<boxm_scene<boct_tree<short, float> >*> (corner_scene_base.as_pointer());
+
   if (!(vul_file::exists(output_path) && vul_file::is_directory(output_path)))
     vul_file::make_directory(output_path);
-  
-  boxm_scene<boct_tree<short, bool> >* valid_corner_scene = 
+
+  boxm_scene<boct_tree<short, bool> >* valid_corner_scene =
   new boxm_scene<boct_tree<short, bool> >(valid_scene->lvcs(), valid_scene->origin(), valid_scene->block_dim(), valid_scene->world_dim(), valid_scene->max_level(), valid_scene->init_level());
   valid_corner_scene->set_appearance_model(BOXM_BOOL);
-  
+
   vcl_stringstream valid_ss;
   valid_ss << "valid_scene_" << scene_id ;
-  
+
   valid_corner_scene->set_paths(output_path, valid_ss.str());
   valid_corner_scene->write_scene(valid_ss.str() + ".xml");
   valid_scene->clone_blocks(*valid_corner_scene);
-  
+
   vcl_stringstream proj_ss;
   proj_ss << "proj_taylor_scene_" << scene_id ;
-  
-  boxm_scene<taylor_tree_type >* copy_proj_scene = 
+
+  boxm_scene<taylor_tree_type >* copy_proj_scene =
   new boxm_scene<taylor_tree_type >(proj_scene->lvcs(), proj_scene->origin(), proj_scene->block_dim(), proj_scene->world_dim(), proj_scene->max_level(), proj_scene->init_level());
   copy_proj_scene->set_appearance_model(VNL_DOUBLE_10);
   copy_proj_scene->set_paths(proj_scene->path(), proj_scene->block_prefix());
   copy_proj_scene->write_scene(output_path, proj_ss.str() + ".xml");
-  
+
   delete copy_proj_scene;
-  
-  
+
   if (!( valid_scene && corner_scene && valid_corner_scene))
   {
     vcl_cerr << "Error in bvpl_global_corners::threshold_corners: Could not cast input scenes\n";
     return;
   }
-  
- 
-  
+
   //(1) Traverse the scene - is there an easy way to modify the cell iterator so to only use leaf cells at level 0;
   boxm_cell_iterator<boct_tree<short, bool > > valid_it = valid_scene->cell_iterator(&boxm_scene<boct_tree<short, bool > >::load_block, true);
   valid_it.begin();
-  
+
   boxm_cell_iterator<boct_tree<short, float > > corners_it = corner_scene->cell_iterator(&boxm_scene<boct_tree<short, float > >::load_block, true);
   corners_it.begin();
-  
+
   boxm_cell_iterator<boct_tree<short, bool > > valid_corners_it = valid_corner_scene->cell_iterator(&boxm_scene<boct_tree<short, bool > >::load_block);
   valid_corners_it.begin();
-  
+
   while ( !(valid_it.end() || corners_it.end() || valid_corners_it.end()) )
   {
     boct_tree_cell<short,bool> *valid_cell = *valid_it;
@@ -247,72 +239,69 @@ void bvpl_global_corners::threshold_laptev_corners(bvpl_global_taylor_sptr globa
 
     //if level and location code of cells isn't the same then continue
     if ((valid_cell->level() != corner_cell->level()) || (valid_cell->level() != valid_corner_cell->level()) || !(valid_code.isequal(&corner_code)) || !(valid_code.isequal(&valid_corner_code))){
-      vcl_cerr << " Error in threshold_laptev_corners: Cells don't have the same structure " << vcl_endl;
+      vcl_cerr << " Error in threshold_laptev_corners: Cells don't have the same structure\n";
       return;
     }
-    
+
     /*****CAUTION: Only looking at cells at level 0*/
-    if(valid_cell->level()!=0){
+    if (valid_cell->level()!=0) {
       ++valid_it;
       ++corners_it;
       valid_corner_cell->set_data(false);
       ++valid_corners_it;
       continue;
     }
-    
-    if(corner_cell->data() < harris_thresh)
+
+    if (corner_cell->data() < harris_thresh)
       valid_corner_cell->set_data(false);
     else
       valid_corner_cell->set_data(true);
-    
-    
+
     ++valid_it;
     ++corners_it;
     ++valid_corners_it;
   }
-  
+
   //clean memory
   valid_scene->unload_active_blocks();
   corner_scene->unload_active_blocks();
   valid_corner_scene->unload_active_blocks();
-  
-  delete valid_corner_scene;
 
+  delete valid_corner_scene;
 }
 
 //: Take a histogram of corners and get different values for percentage of thresholds
 void bvpl_global_corners::explore_corner_statistics(bvpl_global_taylor_sptr global_taylor,int scene_id)
 {
-  
   typedef boct_tree<short,vnl_vector_fixed<double,10> > taylor_tree_type;
   typedef boct_tree_cell<short,vnl_vector_fixed<double,10> > taylor_cell_type;
-  
+
   boxm_scene_base_sptr valid_scene_base = global_taylor->load_valid_scene(scene_id);
   boxm_scene_base_sptr corner_scene_base = this->load_corner_scene(scene_id);
- 
+
   boxm_scene<boct_tree<short, bool> >* valid_scene = dynamic_cast<boxm_scene<boct_tree<short, bool> >*> (valid_scene_base.as_pointer());
-  
-  boxm_scene<boct_tree<short, float> >* corner_scene = dynamic_cast<boxm_scene<boct_tree<short, float> >*> (corner_scene_base.as_pointer());  
-   
+
+  boxm_scene<boct_tree<short, float> >* corner_scene = dynamic_cast<boxm_scene<boct_tree<short, float> >*> (corner_scene_base.as_pointer());
+
   if (!( valid_scene && corner_scene))
   {
     vcl_cerr << "Error in bvpl_global_corners::explore_corner_statistics: Could not cast input scenes\n";
     return;
   }
-  
+
   //init variables
   valid_scene->unload_active_blocks();
   corner_scene->unload_active_blocks();
-  
+
   //compute scene statistics at valid cells only
-  
+
   //(1) Traverse the scene - is there an easy way to modify the cell iterator so to only use leaf cells at level 0;
   boxm_cell_iterator<boct_tree<short, bool > > valid_it = valid_scene->cell_iterator(&boxm_scene<boct_tree<short, bool > >::load_block, true);
   valid_it.begin();
-  
+
   boxm_cell_iterator<boct_tree<short, float > > corners_it = corner_scene->cell_iterator(&boxm_scene<boct_tree<short, float > >::load_block, true);
   corners_it.begin();
-  
+
   float cell_count = 0;
   float max = (float)(*corners_it)->data();
   float min = max;
@@ -321,33 +310,32 @@ void bvpl_global_corners::explore_corner_statistics(bvpl_global_taylor_sptr glob
   {
     boct_tree_cell<short,bool> *valid_cell = *valid_it;
     boct_tree_cell<short,float> *corner_cell = *corners_it;
-    
+
     boct_loc_code<short> valid_code = valid_cell->get_code();
     boct_loc_code<short> corner_code = corner_cell->get_code();
-    
+
     //if level and location code of cells isn't the same then continue
     if ((valid_cell->level() != corner_cell->level()) || !(valid_code.isequal(&corner_code))){
-      vcl_cerr << " Error in threshold_laptev_corners: Cells don't have the same structure " << vcl_endl;
+      vcl_cerr << " Error in threshold_laptev_corners: Cells don't have the same structure\n";
       return;
     }
-    
+
     /*****CAUTION: Only looking at cells at level 0*/
-    if(valid_cell->level()!=0){
+    if (valid_cell->level()!=0) {
       ++valid_it;
       ++corners_it;
       continue;
     }
-    
+
     cell_count++;
     this_val = (float)(*corners_it)->data();
     if ( this_val > max)  max = this_val;
     if ( this_val < min)  min = this_val;
-    
-    
+
     ++valid_it;
     ++corners_it;
   }
-  
+
   unsigned nbins =  vcl_floor(vcl_sqrt(cell_count));
   bsta_histogram<float>  corner_hist(min, max, nbins);
   valid_it.begin();
@@ -356,25 +344,25 @@ void bvpl_global_corners::explore_corner_statistics(bvpl_global_taylor_sptr glob
   {
     boct_tree_cell<short,bool> *valid_cell = *valid_it;
     boct_tree_cell<short,float> *corner_cell = *corners_it;
-    
+
     boct_loc_code<short> valid_code = valid_cell->get_code();
     boct_loc_code<short> corner_code = corner_cell->get_code();
-    
+
     //if level and location code of cells isn't the same then continue
     if ((valid_cell->level() != corner_cell->level()) || !(valid_code.isequal(&corner_code))){
-      vcl_cerr << " Error in threshold_laptev_corners: Cells don't have the same structure " << vcl_endl;
+      vcl_cerr << " Error in threshold_laptev_corners: Cells don't have the same structure\n";
       return;
     }
-    
+
     /*****CAUTION: Only looking at cells at level 0*/
-    if(valid_cell->level()!=0){
+    if (valid_cell->level()!=0) {
       ++valid_it;
       ++corners_it;
       continue;
     }
-    
-    corner_hist.upcount((float)(*corners_it)->data(), 1.0f);   
-    
+
+    corner_hist.upcount((float)(*corners_it)->data(), 1.0f);
+
     ++valid_it;
     ++corners_it;
   }
@@ -384,14 +372,13 @@ void bvpl_global_corners::explore_corner_statistics(bvpl_global_taylor_sptr glob
   vcl_ofstream ofs(file.c_str());
   ofs.precision(7);
   for (unsigned i = 0; i < 7; i++) {
-    ofs << threshold[i] << " " << corner_hist.value_with_area_above(threshold[i]) << "\n";
+    ofs << threshold[i] << ' ' << corner_hist.value_with_area_above(threshold[i]) << '\n';
   }
-  
+
   //clean memory
   ofs.close();
   valid_scene->unload_active_blocks();
   corner_scene->unload_active_blocks();
-  
 }
 
 //: Load scene info
@@ -408,7 +395,7 @@ boxm_scene_base_sptr bvpl_global_corners::load_corner_scene(int scene_id)
   vcl_stringstream aux_scene_ss;
   aux_scene_ss << aux_dirs_[scene_id] << "/harris_scene.xml";
   scene_base->load_scene(aux_scene_ss.str(), scene_parser);
-  
+
   //cast scene
   boxm_scene<boct_tree<short, float > > *scene= new boxm_scene<boct_tree<short, float > >();
   if (scene_base->appearence_model() == BOXM_FLOAT){
@@ -419,7 +406,7 @@ boxm_scene_base_sptr bvpl_global_corners::load_corner_scene(int scene_id)
     vcl_cerr << "Error in bvpl_global_corners::load_corner_scene: Invalid appearance model\n";
     return NULL;
   }
-  
+
   return scene_base;
 }
 
@@ -437,7 +424,7 @@ boxm_scene_base_sptr bvpl_global_corners::load_valid_scene (int scene_id)
   vcl_stringstream aux_scene_ss;
   aux_scene_ss << aux_dirs_[scene_id] << "/valid_scene_" << scene_id << ".xml";
   aux_scene_base->load_scene(aux_scene_ss.str(), aux_parser);
-  
+
   //cast scene
   boxm_scene<boct_tree<short, bool > > *aux_scene= new boxm_scene<boct_tree<short, bool > >();
   if (aux_scene_base->appearence_model() == BOXM_BOOL){
@@ -448,7 +435,7 @@ boxm_scene_base_sptr bvpl_global_corners::load_valid_scene (int scene_id)
     vcl_cerr << "Error in bvpl_global_corners::load_aux_scene: Invalid appearance model\n";
     return NULL;
   }
-  
+
   return aux_scene_base;
 }
 
@@ -459,8 +446,7 @@ void bvpl_global_corners::xml_write()
   bxml_element *root = new bxml_element("global_corners_info");
   doc.set_root_element(root);
   root->append_text("\n");
-  
-  
+
   //write the scene elements
   for (unsigned i =0; i<aux_dirs_.size(); i++)
   {
@@ -472,14 +458,14 @@ void bvpl_global_corners::xml_write()
     root->append_data(scenes_elm);
     root->append_text("\n");
   }
-  
+
   //write parameters for corner detection
   bxml_element* param_elm = new bxml_element("parameters");
   param_elm->append_text("\n");
   param_elm->set_attribute("harris_k", harris_k_);
   root->append_data(param_elm);
   root->append_text("\n");
-  
+
   //write to disk
   vcl_ofstream os(xml_path().c_str());
   bxml_write(os, doc);
