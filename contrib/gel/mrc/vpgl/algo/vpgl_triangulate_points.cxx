d679ec55faa04f13fe396cdc0aac2ac3ec24efbe
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-08-29 02:12:05 -0400

    avoid compiler warning (comparison signed/unsigned)
    SVN-Revision: 32886

--- contrib/gel/mrc/vpgl/algo/vpgl_triangulate_points.cxx
+++ contrib/gel/mrc/vpgl/algo/vpgl_triangulate_points.cxx
@@ -24,7 +24,6 @@ double vpgl_triangulate_points::triangulate(
         const vcl_vector<vpgl_perspective_camera<double> > &cameras,
         vgl_point_3d<double> &point_3d)
 {
-
     const int num_vars = 3;// One var for x, y, z of output 3d point
     const int num_eqs = 2 * points.size();
 
@@ -32,7 +31,7 @@ double vpgl_triangulate_points::triangulate(
     vnl_matrix<double> A(num_eqs, num_vars, 0.0);
     vnl_vector<double> b(num_eqs, 0.0);
 
-    for (int i = 0; i < points.size(); i++) {
+    for (unsigned int i = 0; i < points.size(); ++i) {
         const vgl_vector_3d<double> &trans =
             cameras[i].get_translation();
 
@@ -40,24 +39,14 @@ double vpgl_triangulate_points::triangulate(
             cameras[i].get_rotation().as_matrix();
 
         // Set the row for x for this point
-        A.put(2 * i, 0,
-            rot.get(0, 0) - points[i].x() * rot.get(2, 0) );
-
-        A.put(2 * i, 1,
-            rot.get(0, 1) - points[i].x() * rot.get(2, 1) );
-
-        A.put(2 * i, 2,
-            rot.get(0, 2) - points[i].x() * rot.get(2, 2) );
+        A.put(2 * i, 0, rot.get(0, 0) - points[i].x() * rot.get(2, 0) );
+        A.put(2 * i, 1, rot.get(0, 1) - points[i].x() * rot.get(2, 1) );
+        A.put(2 * i, 2, rot.get(0, 2) - points[i].x() * rot.get(2, 2) );
 
         // Set the row for y for this point
-        A.put(2*i+1, 0,
-            rot.get(1, 0) - points[i].y() * rot.get(2, 0) );
-
-        A.put(2*i+1, 1,
-            rot.get(1, 1) - points[i].y() * rot.get(2, 1) );
-
-        A.put(2*i+1, 2,
-            rot.get(1, 2) - points[i].y() * rot.get(2, 2) );
+        A.put(2*i+1, 0, rot.get(1, 0) - points[i].y() * rot.get(2, 0) );
+        A.put(2*i+1, 1, rot.get(1, 1) - points[i].y() * rot.get(2, 1) );
+        A.put(2*i+1, 2, rot.get(1, 2) - points[i].y() * rot.get(2, 2) );
 
         // Set the RHS row.
         b[2*i + 0] = trans.z() * points[i].x() - trans.x();
@@ -72,7 +61,7 @@ double vpgl_triangulate_points::triangulate(
 
     // Find the error
     double error = 0.0;
-    for (int i = 0; i < points.size(); i++) {
+    for (unsigned int i = 0; i < points.size(); ++i) {
         // Compute projection error
         vnl_double_3 pp = cameras[i].get_rotation().as_matrix() * x;
 
