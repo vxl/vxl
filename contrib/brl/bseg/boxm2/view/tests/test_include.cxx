d5d32dbdd9aab36143ee565b2bd6a17a878b9b79
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-09-07 01:48:38 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 32959

--- contrib/brl/bseg/boxm2/view/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/view/tests/test_include.cxx
@@ -2,6 +2,7 @@
 #include <boxm2/view/boxm2_include_glew.h>
 
 #include <boxm2/view/boxm2_cam_tableau.h>
+#include <boxm2/view/boxm2_ocl_change_tableau.h>
 #include <boxm2/view/boxm2_ocl_render_tableau.h>
 #include <boxm2/view/boxm2_ocl_update_tableau.h>
 #include <boxm2/view/boxm2_trajectory.h>
