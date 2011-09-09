d95c4e5de227bde21633efeb3ad2d386dd60b558
Brandon Mayer <bmayer1@users.sourceforge.net>
2011-09-08 22:58:58 -0400

    smart point instantiation for bvpl_bundler_features_2d
    SVN-Revision: 32975

---
+++ contrib/brl/bseg/bvpl/util/Templates/vbl_smart_ptr+bvpl_bundler_features_2d-.cxx
@@ -0,0 +1,4 @@
+//this is /contrib/brl/bseg/bvpl/util/Templates/vbl_smart_ptr+bvpl_bundler_features_2d.cxx
+#include<bvpl/util/bvpl_bundler_features_2d.h>
+#include<vbl/vbl_smart_ptr.txx>
+VBL_SMART_PTR_INSTANTIATE(bvpl_bundler_features_2d);
