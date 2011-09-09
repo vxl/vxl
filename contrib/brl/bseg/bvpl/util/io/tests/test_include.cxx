85185d33c9b89f728b678a06f2e8ee99709363af
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-09-09 02:46:40 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 32981

--- contrib/brl/bseg/bvpl/util/io/tests/test_include.cxx
+++ contrib/brl/bseg/bvpl/util/io/tests/test_include.cxx
@@ -1,3 +1,4 @@
+#include <bvpl/util/io/bvpl_bundler_features_2d_io.h>
 #include <bvpl/util/io/bvpl_corner_pairs_io.h>
 
 int main() { return 0; }
--- contrib/brl/bseg/bvpl/util/tests/test_include.cxx
+++ contrib/brl/bseg/bvpl/util/tests/test_include.cxx
@@ -1,3 +1,4 @@
+#include <bvpl/util/bvpl_bundler_features_2d.h>
 #include <bvpl/util/bvpl_corner_pair_finder.h>
 #include <bvpl/util/bvpl_corner_search_functor.h>
 
