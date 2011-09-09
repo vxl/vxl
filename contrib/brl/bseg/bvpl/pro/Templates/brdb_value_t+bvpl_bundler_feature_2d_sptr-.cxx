5dc1d976477a1f2503fd14d76040d25a56fd5fca
Brandon Mayer <bmayer1@users.sourceforge.net>
2011-09-09 19:05:33 -0400

    brdb_value for bvpl_bundler_feature_2d_sptr
    SVN-Revision: 32994

---
+++ contrib/brl/bseg/bvpl/pro/Templates/brdb_value_t+bvpl_bundler_feature_2d_sptr-.cxx
@@ -0,0 +1,4 @@
+#include <brdb/brdb_value.txx>
+#include <vbl/io/vbl_io_smart_ptr.h>
+#include <bvpl/util/bvpl_bundler_features_2d.h>
+BRDB_VALUE_INSTANTIATE(bvpl_bundler_features_2d_sptr, "bvpl_bundler_features_2d_sptr");
