f0e9d8a8a91bf6a9cd76a8971c2854fabd2633a2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-09-13 17:07:03 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 33041

--- contrib/brl/bseg/boxm2/cpp/algo/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/cpp/algo/tests/test_include.cxx
@@ -1,9 +1,12 @@
 #include <boxm2/cpp/algo/boxm2_batch_functors.h>
 #include <boxm2/cpp/algo/boxm2_batch_opt2_functors.h>
+#include <boxm2/cpp/algo/boxm2_batch_opt2_phongs_functors.h>
 #include <boxm2/cpp/algo/boxm2_cast_cone_ray_function.h>
 #include <boxm2/cpp/algo/boxm2_cast_intensities_functor.h>
 #include <boxm2/cpp/algo/boxm2_cast_ray_function.h>
 #include <boxm2/cpp/algo/boxm2_change_detection_functor.h>
+#include <boxm2/cpp/algo/boxm2_compute_nonsurface_histogram_functor.h>
+#include <boxm2/cpp/algo/boxm2_compute_phongs_model_functor.h>
 #include <boxm2/cpp/algo/boxm2_cone_update_image_functor.h>
 #include <boxm2/cpp/algo/boxm2_data_serial_iterator.h>
 #include <boxm2/cpp/algo/boxm2_filter_block_function.h>
@@ -12,12 +15,14 @@
 #include <boxm2/cpp/algo/boxm2_mean_intensities_batch_functor.h>
 #include <boxm2/cpp/algo/boxm2_merge_block_function.h>
 #include <boxm2/cpp/algo/boxm2_mog3_grey_processor.h>
+#include <boxm2/cpp/algo/boxm2_phongs_model_processor.h>
 #include <boxm2/cpp/algo/boxm2_ray_probe_functor.h>
 #include <boxm2/cpp/algo/boxm2_refine_block_function.h>
 #include <boxm2/cpp/algo/boxm2_render_cone_functor.h>
 #include <boxm2/cpp/algo/boxm2_render_exp_depth_functor.h>
 #include <boxm2/cpp/algo/boxm2_render_exp_image_functor.h>
 #include <boxm2/cpp/algo/boxm2_render_functions.h>
+#include <boxm2/cpp/algo/boxm2_shadow_model_functor.h>
 #include <boxm2/cpp/algo/boxm2_update_functions.h>
 #include <boxm2/cpp/algo/boxm2_update_image_functor.h>
 #include <boxm2/cpp/algo/boxm2_update_with_shadow_functor.h>
