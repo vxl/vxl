b559f6f1ac2d61db6d25fb2e979f9cda7a4b6903
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-09-20 02:49:57 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 33127

---
+++ contrib/brl/bseg/boxm2/util/tests/test_include.cxx
@@ -0,0 +1,4 @@
+#include <boxm2/util/boxm2_point_util.h>
+#include <boxm2/util/boxm2_convert_nvm.h>
+#include <boxm2/util/boxm2_cams_and_box_to_scene.h>
+#include <boxm2/util/boxm2_convert_bundle.h>
