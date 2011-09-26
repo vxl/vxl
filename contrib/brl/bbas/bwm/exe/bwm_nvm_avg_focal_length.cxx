7f82dd4f23661811f38823d5222e09d6d0e389fb
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-09-26 18:15:54 -0400

    replaced strstr() by vcl_strstr()
    SVN-Revision: 33153

--- contrib/brl/bbas/bwm/exe/bwm_nvm_avg_focal_length.cxx
+++ contrib/brl/bbas/bwm/exe/bwm_nvm_avg_focal_length.cxx
@@ -44,98 +44,91 @@
 bool NVMreplaceByAvgFocalLength(vcl_ifstream& in1,
                                 vcl_ifstream& in2,
                                 vcl_ofstream& out)
- 
 {
-    
-    int rotation_parameter_num = 4; 
+    int rotation_parameter_num = 4;
     vcl_string token;
     bool format_r9t = false;
-    if(in1.peek() == 'N') 
+    if (in1.peek() == 'N')
     {
         in1 >> token; //file header
-        if(strstr(token.c_str(), "R9T"))
+        if (vcl_strstr(token.c_str(), "R9T"))
         {
             rotation_parameter_num = 9;    //rotation as 3x3 matrix
             format_r9t = true;
         }
     }
-    int ncam = 0, npoint = 0, nproj = 0;   
+    int ncam = 0, npoint = 0, nproj = 0;
     // read # of cameras
-    in1 >> ncam;  if(ncam <= 1) return false; 
+    in1 >> ncam;  if (ncam <= 1) return false;
 
     float count = 0;
     float sum_focals = 0.0;
-    for(int i = 0; i < ncam; ++i)
+    for (int i = 0; i < ncam; ++i)
     {
         double f, q[9], c[3], d[2];
         in1 >> token >> f ;
 
-
-        if(f> 0.0)
+        if (f> 0.0)
         {
             sum_focals += f;
             count = count + 1.0f;
-
         }
-        for(int j = 0; j < rotation_parameter_num; ++j) in1 >> q[j]; 
+        for (int j = 0; j < rotation_parameter_num; ++j) in1 >> q[j];
         in1 >> c[0] >> c[1] >> c[2] >> d[0] >> d[1];
-
-
     }
 
     float avg_focal_length = 0.0;
-    if( count > 0.0 )
+    if (count > 0.0 )
         avg_focal_length = sum_focals/count ;
 
     in1.close();
     //======================================================
-    if(in2.peek() == 'N') 
+    if (in2.peek() == 'N')
     {
         in2 >> token; //file header
-        if(strstr(token.c_str(), "R9T"))
+        if (vcl_strstr(token.c_str(), "R9T"))
         {
             rotation_parameter_num = 9;    //rotation as 3x3 matrix
             format_r9t = true;
         }
     }
-    ncam = 0, npoint = 0, nproj = 0;   
+    ncam = 0, npoint = 0, nproj = 0;
     // read # of cameras
-    in2 >> ncam;  if(ncam <= 1) return false; 
+    in2 >> ncam;  if (ncam <= 1) return false;
 
-    out<<token<<'\n';
-    out<<ncam<<'\n';
+    out<<token<<'\n'
+       <<ncam<<'\n';
 
     //read the camera parameters
 
-    for(int i = 0; i < ncam; ++i)
+    for (int i = 0; i < ncam; ++i)
     {
         double f, q[9], c[3], d[2];
         in2 >> token >> f ;
 
-
-        for(int j = 0; j < rotation_parameter_num; ++j) in2 >> q[j]; 
+        for (int j = 0; j < rotation_parameter_num; ++j) in2 >> q[j];
         in2 >> c[0] >> c[1] >> c[2] >> d[0] >> d[1];
 
         out<<token<<'\t'<<avg_focal_length<<'\t';
-        for(int j = 0; j < rotation_parameter_num; ++j) out << q[j]<<'\t'; 
+        for (int j = 0; j < rotation_parameter_num; ++j) out << q[j]<<'\t';
         out << c[0] <<'\t'<< c[1] <<'\t'<< c[2] <<'\t'<< d[0] <<'\t'<< d[1]<<'\n';
     }
 
     //////////////////////////////////////
-    in2 >> npoint;   if(npoint <= 0) return false; 
+    in2 >> npoint;   if (npoint <= 0) return false;
 
     out << npoint << '\n';
 
     //read image projections and 3D points.
-    for(int i = 0; i < npoint; ++i)
+    for (int i = 0; i < npoint; ++i)
     {
         float pt[3]; int cc[3], npj;
-        in2  >> pt[0] >> pt[1] >> pt[2] 
+        in2  >> pt[0] >> pt[1] >> pt[2]
             >> cc[0] >> cc[1] >> cc[2] >> npj;
-        out << pt[0] <<'\t'<< pt[1] <<'\t'<< pt[2] 
+        out << pt[0] <<'\t'<< pt[1] <<'\t'<< pt[2]
             <<'\t'<< cc[0] <<'\t'<< cc[1] <<'\t'<< cc[2] <<'\t'<< npj<<'\t';
-        
-        for(int j = 0; j < npj; ++j)
+
+        for (int j = 0; j < npj; ++j)
         {
             int cidx, fidx; float imx, imy;
             in2 >> cidx >> fidx >> imx >> imy;
@@ -143,9 +136,9 @@ bool NVMreplaceByAvgFocalLength(vcl_ifstream& in1,
         }
         out<<'\n';
     }
-    ///////////////////////////////////////////////////////////////////////////////
-
+    ////////////////////////////////////////////////////////////////////////////
 }
+
 // An executable that read bundler file and convert it into video site.
 int main(int argc, char** argv)
 {
@@ -155,7 +148,6 @@ int main(int argc, char** argv)
   vul_arg<vcl_string> outfile   ("-osfm", "Output file of bundler",  "");
 
   vul_arg_parse(argc, argv);
-
 
   // open the bundler file
   vcl_ifstream ifile1( infile().c_str() );
@@ -177,10 +169,7 @@ int main(int argc, char** argv)
     return -1;
   }
 
-
-
   NVMreplaceByAvgFocalLength(ifile1,ifile2,ofile);
-
 
   return 0;
 }
