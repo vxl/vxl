b586a98194fbbe87eb08f39b4e1e3371d5389ccf
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-09-29 14:53:22 -0400

    new macro for registering processes
    SVN-Revision: 33179

--- contrib/brl/bpro/bpro_batch/bpro_batch.h
+++ contrib/brl/bpro/bpro_batch/bpro_batch.h
@@ -21,7 +21,7 @@ typedef struct
 } dbvalue;
 
 // METHOD_NUM defined as one more than the actual number of processes to include trailing NULL process 
-const int METHOD_NUM = 27; 
+const int METHOD_NUM = 30; 
 void register_basic_datatypes();
 extern PyMethodDef batch_methods[];
 
