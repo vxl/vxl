bf3f7960a03db7f052641ffd34f8af2e54e60abf
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-10-02 15:38:34 -0400

    tests are built conditionally
    SVN-Revision: 33191

--- contrib/brl/bseg/boxm2/ocl/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/ocl/tests/test_include.cxx
@@ -1,5 +1,6 @@
+#include <boxm2_ocl_where_root_dir.h> // in ${vxl_BINARY_DIR}/contrib/brl/bseg/boxm2/ocl/
+
 #include <boxm2/ocl/boxm2_ocl_util.h>
-#include <boxm2/ocl/boxm2_ocl_where_root_dir.h>
 #include <boxm2/ocl/boxm2_opencl_cache.h>
 
 int main() { return 0; }
