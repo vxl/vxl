f725c1bba1d54263740e885bfec53cc729094706
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-10-03 17:58:43 -0400

    TAB replaced by spaces
    SVN-Revision: 33202

--- contrib/brl/bbas/brad/tests/test_synoptic_function.cxx
+++ contrib/brl/bbas/brad/tests/test_synoptic_function.cxx
@@ -2,19 +2,20 @@
 #include <brad/brad_synoptic_function_1d.h>
 #include <vcl_cstdlib.h> // for rand()
 #include <vcl_fstream.h>
+
 static void test_synoptic_function()
 {
   START("synoptic function test");
   brad_synoptic_function_1d sf;
   sf.load_samples("E:/mundy/CVGroup/Nibbler/Notes/Experiments/intensities_and_viewing_direction.txt");
   unsigned n = sf.size();
-  for(unsigned i = 0; i<n; ++i){
-	  double s = sf.arc_length(i);
+  for (unsigned i = 0; i<n; ++i){
+    double s = sf.arc_length(i);
 #if 0
     vcl_cout << s << ' ' << sf.intensity(i) 
              << ' ' << sf.cubic_interp_inten(s)<< ' ' << sf.vis(i) <<  '\n';
 #endif
-vcl_cout << s << ' ' << sf.elev(i) 
+    vcl_cout << s << ' ' << sf.elev(i) 
              << ' ' << sf.cubic_interp_elev(s)<<  '\n';
   }
 }
