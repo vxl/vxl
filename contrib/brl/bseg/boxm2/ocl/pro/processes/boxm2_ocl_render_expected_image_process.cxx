f149227f1ea3003b9ba2aba10dd9a75c34554fdf
Vishal Jain <jainv@users.sourceforge.net>
2011-10-11 14:45:37 -0400

    facilitated rendering of only occupancy.
    SVN-Revision: 33271

--- contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_render_expected_image_process.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_render_expected_image_process.cxx
@@ -131,18 +131,22 @@ bool boxm2_ocl_render_expected_image_process(bprb_func_process& pro)
   bool foundDataType = false;
   vcl_string data_type,options;
   vcl_vector<vcl_string> apps = scene->appearances();
+
+  int apptypesize = 0;
   for (unsigned int i=0; i<apps.size(); ++i) {
     if ( apps[i] == boxm2_data_traits<BOXM2_MOG3_GREY>::prefix() )
     {
       data_type = apps[i];
       foundDataType = true;
       options=" -D MOG_TYPE_8 ";
+      apptypesize = boxm2_data_traits<BOXM2_MOG3_GREY>::datasize();
     }
     else if ( apps[i] == boxm2_data_traits<BOXM2_MOG3_GREY_16>::prefix() )
     {
       data_type = apps[i];
       foundDataType = true;
       options=" -D MOG_TYPE_16 ";
+      apptypesize = boxm2_data_traits<BOXM2_MOG3_GREY_16>::datasize();
     }
   }
   if (!foundDataType) {
@@ -192,7 +196,7 @@ bool boxm2_ocl_render_expected_image_process(bprb_func_process& pro)
   // run expected image function
   render_expected_image(scene, device, opencl_cache, queue,
                         cam, exp_image, vis_image, exp_img_dim,
-                        data_type, kernels[identifier][0], lthreads, cl_ni, cl_nj);
+                        data_type, kernels[identifier][0], lthreads, cl_ni, cl_nj,apptypesize);
 
   // normalize
   {
