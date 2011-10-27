11530ef0d6b2e677f21d3a92a7b31be694604db2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-10-27 16:16:41 -0400

    TABs replaced by spaces
    SVN-Revision: 33345

--- contrib/brl/bseg/boxm2/pyscripts/vpgl_adaptor.py
+++ contrib/brl/bseg/boxm2/pyscripts/vpgl_adaptor.py
@@ -12,29 +12,29 @@ def load_perspective_camera(file_path) :
   return cam;
 
 def load_rational_camera(file_path) :
-  boxm2_batch.init_process("vpglLoadRationalCameraProcess")
-  boxm2_batch.set_input_string(0, file_path)
-  boxm2_batch.run_process()
-  (id,type) = boxm2_batch.commit_output(0)
-  cam = dbvalue(id,type)
-  return cam
+  boxm2_batch.init_process("vpglLoadRationalCameraProcess");
+  boxm2_batch.set_input_string(0, file_path);
+  boxm2_batch.run_process();
+  (id,type) = boxm2_batch.commit_output(0);
+  cam = dbvalue(id,type);
+  return cam;
 
 def load_rational_camera_nitf(file_path) :
-  boxm2_batch.init_process("vpglLoadRationalCameraNITFProcess")
-  boxm2_batch.set_input_string(0, file_path)
-  boxm2_batch.run_process()
-  (id,type) = boxm2_batch.commit_output(0)
-  cam = dbvalue(id,type)
-  return cam
+  boxm2_batch.init_process("vpglLoadRationalCameraNITFProcess");
+  boxm2_batch.set_input_string(0, file_path);
+  boxm2_batch.run_process();
+  (id,type) = boxm2_batch.commit_output(0);
+  cam = dbvalue(id,type);
+  return cam;
 
 ###################
 #camera saving
 ###################
 def save_rational_camera(camera,path) :
-  boxm2_batch.init_process("vpglSaveRationalCameraProcess")
-  boxm2_batch.set_input_from_db(0,camera)
-  boxm2_batch.set_input_string(1,path)
-  boxm2_batch.run_process()
+  boxm2_batch.init_process("vpglSaveRationalCameraProcess");
+  boxm2_batch.set_input_from_db(0,camera);
+  boxm2_batch.set_input_string(1,path);
+  boxm2_batch.run_process();
 
 #################################################
 # perspective go generic conversion
@@ -57,17 +57,17 @@ def camera_dir_planar_bbox(dir_name) :
 
 # gets view direction at a point for a perspective camera
 def get_view_at_point(persp_cam,x,y,z):
-	boxm2_batch.init_process("vpglGetViewDirectionAtPointProcess");
-	boxm2_batch.set_input_from_db(0,persp_cam);
-	boxm2_batch.set_input_float(1,x);
-	boxm2_batch.set_input_float(2,y);
-	boxm2_batch.set_input_float(3,z);
-	boxm2_batch.run_process();
-	(id,type) = boxm2_batch.commit_output(0);
-	theta=boxm2_batch.get_output_float(id);
-	(id,type) = boxm2_batch.commit_output(1);
-	phi=boxm2_batch.get_output_float(id);
-	return theta, phi;
+  boxm2_batch.init_process("vpglGetViewDirectionAtPointProcess");
+  boxm2_batch.set_input_from_db(0,persp_cam);
+  boxm2_batch.set_input_float(1,x);
+  boxm2_batch.set_input_float(2,y);
+  boxm2_batch.set_input_float(3,z);
+  boxm2_batch.run_process();
+  (id,type) = boxm2_batch.commit_output(0);
+  theta=boxm2_batch.get_output_float(id);
+  (id,type) = boxm2_batch.commit_output(1);
+  phi=boxm2_batch.get_output_float(id);
+  return theta, phi;
 
 def get_3d_from_depth(persp_cam,u,v,t) :
   boxm2_batch.init_process("vpglGenerate3dPointFromDepthProcess");
