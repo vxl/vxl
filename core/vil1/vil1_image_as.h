404e42a1246f709961d307843daf72d1e20839f2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-10-30 16:12:52 -0400

    Doxygen "\sa" special command should be on a separate line
    SVN-Revision: 33361

--- core/vil1/vil1_image_as.h
+++ core/vil1/vil1_image_as.h
@@ -28,28 +28,36 @@
 template<class T>
 vil1_image vil1_image_as(vil1_image const & i, T* dummy /* = 0 */);
 
-//: \brief Converts to a greyscale image with byte (8bit unsigned) pixels. \sa{vil1_image_as}
+//: \brief Converts to a greyscale image with byte (8bit unsigned) pixels.
+// \sa vil1_image_as
 vil1_image vil1_image_as_byte(vil1_image const & i);
 
-//: \brief Converts to a greyscale image with 16bit unsigned pixels. \sa{vil1_image_as}
+//: \brief Converts to a greyscale image with 16bit unsigned pixels.
+// \sa vil1_image_as
 vil1_image vil1_image_as_uint16(vil1_image const &image);
 
-//: \brief Converts to a greyscale image with integer (32bit) pixels. \sa{vil1_image_as}
+//: \brief Converts to a greyscale image with integer (32bit) pixels.
+// \sa vil1_image_as
 vil1_image vil1_image_as_int(vil1_image const & i);
 
-//: \brief Converts to a grayscale image with float pixels. \sa{vil1_image_as}
+//: \brief Converts to a grayscale image with float pixels.
+// \sa vil1_image_as
 vil1_image vil1_image_as_float(vil1_image const & i);
 
-//: \brief Converts to a greyscale image with integer double pixels. \sa{vil1_image_as}
+//: \brief Converts to a greyscale image with integer double pixels.
+// \sa vil1_image_as
 vil1_image vil1_image_as_double(vil1_image const & i);
 
-//: \brief Converts to a rgb image with 8 bits per component. \sa{vil1_image_as}
+//: \brief Converts to a rgb image with 8 bits per component.
+// \sa vil1_image_as
 vil1_image vil1_image_as_rgb_byte(vil1_image const & i);
 
-//: \brief Converts to a rgb image with 16 bits per component. \sa{vil1_image_as}
+//: \brief Converts to a rgb image with 16 bits per component.
+// \sa vil1_image_as
 vil1_image vil1_image_as_rgb_uint16(vil1_image const & i);
 
-//: \brief Converts to a rgb image with float components. \sa{vil1_image_as}
+//: \brief Converts to a rgb image with float components.
+// \sa vil1_image_as
 vil1_image vil1_image_as_rgb_float(vil1_image const & i);
 
 #endif // vil1_image_as_h_
