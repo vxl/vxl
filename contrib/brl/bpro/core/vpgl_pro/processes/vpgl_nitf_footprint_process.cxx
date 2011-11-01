c55a03508ecc2a45c09666510afaae9f0f92a71b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-11-01 10:33:09 -0400

    std::[oi]fstream constructor expects const char* argument, not std::string
    SVN-Revision: 33387

--- contrib/brl/bpro/core/vpgl_pro/processes/vpgl_nitf_footprint_process.cxx
+++ contrib/brl/bpro/core/vpgl_pro/processes/vpgl_nitf_footprint_process.cxx
@@ -17,7 +17,7 @@ bool vpgl_nitf_footprint_process_cons(bprb_func_process& pro)
   //this process takes 2 inputs:
   // 1: (vcl_string) the filename containing a list of images to evaluate
   // 2: (vcl_string) the filename of the kml file to write footprints to
- 
+
   vcl_vector<vcl_string> input_types;
   input_types.push_back("vcl_string");
   input_types.push_back("vcl_string");
@@ -29,54 +29,53 @@ bool vpgl_nitf_footprint_process_cons(bprb_func_process& pro)
 // write kml header
 void kml_init(vcl_ofstream &ofs)
 {
-    ofs << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << vcl_endl;
-    ofs << "<kml xmlns=\"http://www.opengis.net/kml/2.2\"> <Document>" << vcl_endl;
-    ofs << vcl_endl;
-    return;
+  ofs << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+      << "<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document>\n"
+      << vcl_endl;
+  return;
 }
-
 
 
 void kml_poly(vcl_ofstream &ofs, vcl_string name, vcl_string description, vnl_double_2 ul, vnl_double_2 ur, vnl_double_2 ll, vnl_double_2 lr)
 {
-    ofs << "<Placemark>" << vcl_endl;
-    ofs << "  <name>" << name << "</name>" << vcl_endl;
-    ofs << "  <description>" << description << "</description>" << vcl_endl;
-    ofs << "  <Style> " << vcl_endl;
-    ofs << "    <PolyStyle>  " << vcl_endl;
-    ofs << "      <colorMode>random</colorMode>" << vcl_endl;
-    ofs << "      <color>ffffffff</color>" << vcl_endl;
-    ofs << "      <fill>0</fill>" << vcl_endl;
-    ofs << "    </PolyStyle> " << vcl_endl;
-    ofs << "    <LineStyle> " << vcl_endl;
-    ofs << "      <colorMode>random</colorMode>" << vcl_endl;
-    ofs << "      <color>ffffffff</color>" << vcl_endl;
-    ofs << "      <width>3</width>" << vcl_endl;
-    ofs << "    </LineStyle>" << vcl_endl;
-    ofs << "  </Style>" << vcl_endl;
-    ofs << "  <Polygon>" << vcl_endl;
-    ofs << "   <tessellate>1</tessellate>" << vcl_endl;
-    ofs << "   <outerBoundaryIs>" << vcl_endl;
-    ofs << "     <LinearRing>" << vcl_endl;
-    ofs << "       <coordinates>" << vcl_endl;
-    ofs << "          " << ul[1] << "," << ul[0] << ",0  ";
-    ofs << ur[1] << "," << ur[0] << ",0  ";
-    ofs << lr[1] << "," << lr[0] << ",0  ";
-    ofs << ll[1] << "," << ll[0] << ",0  ";
-    ofs << ul[1] << "," << ul[0] << ",0" << vcl_endl;
+  ofs << "<Placemark>\n"
+      << "  <name>" << name << "</name>\n"
+      << "  <description>" << description << "</description>\n"
+      << "  <Style>\n"
+      << "    <PolyStyle>\n"
+      << "      <colorMode>random</colorMode>\n"
+      << "      <color>ffffffff</color>\n"
+      << "      <fill>0</fill>\n"
+      << "    </PolyStyle>\n"
+      << "    <LineStyle>\n"
+      << "      <colorMode>random</colorMode>\n"
+      << "      <color>ffffffff</color>\n"
+      << "      <width>3</width>\n"
+      << "    </LineStyle>\n"
+      << "  </Style>\n"
+      << "  <Polygon>\n"
+      << "   <tessellate>1</tessellate>\n"
+      << "   <outerBoundaryIs>\n"
+      << "     <LinearRing>\n"
+      << "       <coordinates>\n"
+      << "          "
+      << ul[1] << ',' << ul[0] << ",0  "
+      << ur[1] << ',' << ur[0] << ",0  "
+      << lr[1] << ',' << lr[0] << ",0  "
+      << ll[1] << ',' << ll[0] << ",0  "
+      << ul[1] << ',' << ul[0] << ",0\n"
+      << "       </coordinates>\n"
+      << "     </LinearRing>\n"
+      << "   </outerBoundaryIs>\n"
+      << "  </Polygon>\n"
+      << "</Placemark>\n" << vcl_endl;
 
-    ofs << "	    </coordinates>" << vcl_endl;
-    ofs << "     </LinearRing>" << vcl_endl;
-    ofs << "   </outerBoundaryIs>" << vcl_endl;
-    ofs << "  </Polygon>" << vcl_endl;
-    ofs << "</Placemark>" << vcl_endl << vcl_endl;
-
-    return;
+  return;
 }
 
 void kml_finish(vcl_ofstream &ofs)
 {
-    ofs << "</Document></kml>" << vcl_endl;
+  ofs << "</Document></kml>" << vcl_endl;
 }
 
 //: Execute the process
@@ -91,50 +90,50 @@ bool vpgl_nitf_footprint_process(bprb_func_process& pro)
   vcl_string in_img_list = pro.get_input<vcl_string>(0);
   vcl_string footprint_filename = pro.get_input<vcl_string>(1);
 
-  vcl_ofstream ofs(footprint_filename);
-  if(!ofs.good()) {
-      vcl_cerr << "ERROR: vpgl_nitf_footprint_process : Error opening " << footprint_filename << " for write." << vcl_endl;
-      return false;
+  vcl_ofstream ofs(footprint_filename.c_str());
+  if (!ofs.good()) {
+    vcl_cerr << "ERROR: vpgl_nitf_footprint_process : Error opening " << footprint_filename << " for write.\n";
+    return false;
   }
 
   kml_init(ofs);
 
-  vcl_ifstream ifs(in_img_list);
+  vcl_ifstream ifs(in_img_list.c_str());
   vcl_string nitf_path;
 
-  while(ifs.good()) {
-      ifs >> nitf_path;
+  while (ifs.good()) {
+    ifs >> nitf_path;
 
-      vil_image_resource_sptr image =
-          vil_load_image_resource(nitf_path.c_str());
-      if (!image)
-      {
-          vcl_cout << "NITF image load failed in vpgl_nitf_footprint_process\n";
-          return false;
-      }
+    vil_image_resource_sptr image =
+        vil_load_image_resource(nitf_path.c_str());
+    if (!image)
+    {
+      vcl_cout << "NITF image load failed in vpgl_nitf_footprint_process\n";
+      return false;
+    }
 
-      vcl_string format = image->file_format();
-      vcl_string prefix = format.substr(0,4);
+    vcl_string format = image->file_format();
+    vcl_string prefix = format.substr(0,4);
 
-      if (prefix != "nitf")
-      {
-          vcl_cout << "source image is not NITF in vpgl_nitf_footprint_process\n";
-          return false;
-      }
+    if (prefix != "nitf")
+    {
+      vcl_cout << "source image is not NITF in vpgl_nitf_footprint_process\n";
+      return false;
+    }
 
-      //cast to an nitf2_image
-      vil_nitf2_image *nitf_image = static_cast<vil_nitf2_image*>(image.ptr());
-      vpgl_nitf_rational_camera *nitf_cam=new vpgl_nitf_rational_camera(nitf_image, true);
+    //cast to an nitf2_image
+    vil_nitf2_image *nitf_image = static_cast<vil_nitf2_image*>(image.ptr());
+    vpgl_nitf_rational_camera *nitf_cam=new vpgl_nitf_rational_camera(nitf_image, true);
 
-      vnl_double_2 ul = nitf_cam->upper_left();
-      vnl_double_2 ur = nitf_cam->upper_right();
-      vnl_double_2 ll = nitf_cam->lower_left();
-      vnl_double_2 lr = nitf_cam->lower_right();
+    vnl_double_2 ul = nitf_cam->upper_left();
+    vnl_double_2 ur = nitf_cam->upper_right();
+    vnl_double_2 ll = nitf_cam->lower_left();
+    vnl_double_2 lr = nitf_cam->lower_right();
 
-      vcl_string nitf_id = vul_file::strip_directory(nitf_path);
-      vcl_string desc = nitf_path + " footprint";
+    vcl_string nitf_id = vul_file::strip_directory(nitf_path);
+    vcl_string desc = nitf_path + " footprint";
 
-      kml_poly(ofs, nitf_id, desc, ul, ur, ll, lr);
+    kml_poly(ofs, nitf_id, desc, ul, ur, ll, lr);
   }
   kml_finish(ofs);
 
