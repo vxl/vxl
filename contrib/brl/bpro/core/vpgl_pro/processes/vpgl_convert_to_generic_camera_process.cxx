81682cde04507c82181c0862de02d4ecfd496334
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2011-11-02 12:37:46 -0400

    update
    SVN-Revision: 33417

--- contrib/brl/bpro/core/vpgl_pro/processes/vpgl_convert_to_generic_camera_process.cxx
+++ contrib/brl/bpro/core/vpgl_pro/processes/vpgl_convert_to_generic_camera_process.cxx
@@ -22,11 +22,19 @@ bool vpgl_convert_to_generic_camera_process_cons(bprb_func_process& pro)
   input_types.push_back("vpgl_camera_double_sptr");
   input_types.push_back("unsigned");// ni
   input_types.push_back("unsigned");// nj
+  input_types.push_back("unsigned"); // level, e.g. camera needs to be scaled or not, pass the ni-nj of the original image 
   ok = pro.set_input_types(input_types);
+
+  // in case the 5th input is not set
+  brdb_value_sptr idx = new brdb_value_t<unsigned>(0);
+  pro.set_input(3, idx);
+
   if (!ok) return ok;
 
   vcl_vector<vcl_string> output_types;
   output_types.push_back("vpgl_camera_double_sptr");  // label image
+  output_types.push_back("unsigned");
+  output_types.push_back("unsigned");
   ok = pro.set_output_types(output_types);
   if (!ok) return ok;
 
@@ -36,8 +44,8 @@ bool vpgl_convert_to_generic_camera_process_cons(bprb_func_process& pro)
 //: Execute the process
 bool vpgl_convert_to_generic_camera_process(bprb_func_process& pro)
 {
-  if (pro.n_inputs()!= 3) {
-    vcl_cout << "vpgl_convert_to_generic_camera_process: The input number should be 3" << vcl_endl;
+  if (pro.n_inputs()!= 4) {
+    vcl_cout << "vpgl_convert_to_generic_camera_process: The input number should be 4" << vcl_endl;
     return false;
   }
   // get the inputs
@@ -47,13 +55,16 @@ bool vpgl_convert_to_generic_camera_process(bprb_func_process& pro)
     return false;
   }
   unsigned ni = pro.get_input<unsigned>(1), nj = pro.get_input<unsigned>(2);
+  unsigned level = pro.get_input<unsigned>(3);
   vpgl_generic_camera<double> gcam;
-  if(!vpgl_generic_camera_compute::compute(camera, ni, nj, gcam)){
+  if(!vpgl_generic_camera_compute::compute(camera, (int)ni, (int)nj, gcam, level)){
    vcl_cout<<"camera conversion failed\n"<<vcl_endl;
     return false;
   } 
   vpgl_camera_double_sptr out = new vpgl_generic_camera<double>(gcam);
   pro.set_output_val<vpgl_camera_double_sptr>(0, out);
+  pro.set_output_val<unsigned>(1,gcam.cols());
+  pro.set_output_val<unsigned>(2,gcam.rows());
   return true;
 }
 
