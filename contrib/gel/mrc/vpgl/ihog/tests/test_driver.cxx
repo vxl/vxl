5c53d123f7a74f5167ea2b58a575975d64b01810
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2011-11-02 12:18:59 -0400

    update
    SVN-Revision: 33409

--- contrib/gel/mrc/vpgl/ihog/tests/test_driver.cxx
+++ contrib/gel/mrc/vpgl/ihog/tests/test_driver.cxx
@@ -2,11 +2,13 @@
 
 
 DECLARE( test_minimizer );
+DECLARE( test_matcher_translation );
 
 void
 register_tests()
 {
   REGISTER( test_minimizer );
+  REGISTER( test_matcher_translation );
 }
 
 DEFINE_MAIN;
