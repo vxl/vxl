b0553682c8e3be3d4e55e67c5d1316dbc603f6c8
Vishal Jain <jainv@users.sourceforge.net>
2011-11-03 09:17:05 -0400

    modified parameters.
    SVN-Revision: 33436

--- contrib/brl/bseg/sdet/tests/test_denoise_mrf_bp.cxx
+++ contrib/brl/bseg/sdet/tests/test_denoise_mrf_bp.cxx
@@ -12,35 +12,26 @@
 static void test_denoise_mrf_bp()
 {
   vil_image_resource_sptr depth =
-    vil_load_image_resource("C:/images/MeshStudies/Quantico/depth_med.tif");
+      vil_load_image_resource("f:/visdt/z_image.tiff");
   vil_image_resource_sptr var =
-    vil_load_image_resource("C:/images/MeshStudies/Quantico/var_med.tif");
+    vil_load_image_resource("f:/visdt/var_image.tiff");
   sdet_denoise_mrf_bp_params dmbp;
   dmbp.n_iter_ = 3;
   dmbp.pyramid_levels_ = 5;
   dmbp.discontinuity_cost_ = 5.0;
   dmbp.truncation_cost_=400;
   dmbp.n_labels_ = 64;
-  //dmbp.lambda_=0.000001f;
-  dmbp.lambda_=0.0000003f;
+
+  dmbp.lambda_=0.01f;
   sdet_denoise_mrf_bp mbp(dmbp);
   mbp.set_image(depth);   mbp.set_variance(var);
   bool success = mbp.denoise();
   if (success) {
     vil_image_resource_sptr outr = mbp.output();
     vil_save_image_resource(outr,
-                            "C:/images/MeshStudies/Quantico/belief_med.tif",
+                            "f:/visdt/z_image_out.tiff",
                             "tiff");
-#if 0
-    unsigned pi = 214, pj = 88;
-    sdet_mrf_bp_sptr mrf = mbp.mrf();
-    sdet_mrf_site_bp_sptr s = mrf->site(pi, pj);
-    for (unsigned lab = 0; lab < dmbp.n_labels_; ++ lab)
-      vcl_cout << s->prior_message(0,lab) << ' '
-               << s->prior_message(1,lab) << ' '
-               << s->prior_message(2,lab) << ' '
-               << s->prior_message(3,lab) << '\n';
-#endif
+
   }
 }
 
