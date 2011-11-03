e579bfbb6c356b996424c68226dffa13dbf2e52e
Joseph L. Mundy <mundyj@users.sourceforge.net>
2011-11-03 06:59:17 -0400

    removed bgeo_sun_pos
    SVN-Revision: 33434

--- contrib/gel/mrc/vpgl/bgeo/tests/test_driver.cxx
+++ contrib/gel/mrc/vpgl/bgeo/tests/test_driver.cxx
@@ -1,13 +1,12 @@
 #include <testlib/testlib_register.h>
 
 DECLARE( test_lvcs );
-DECLARE( test_sun_pos );
+
 
 void
 register_tests()
 {
   REGISTER ( test_lvcs );
-  REGISTER ( test_sun_pos );
 }
 
 DEFINE_MAIN;
