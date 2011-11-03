6e983e4384eae9bbbe3c83ca9060aef84082e7c1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-11-03 11:29:15 -0400

    corrected the "This is" line
    SVN-Revision: 33437

--- contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_paint_batch_process.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_paint_batch_process.cxx
@@ -1,4 +1,5 @@
-// This is brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_paint_process.cxx
+// This is brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_paint_batch_process.cxx
+#include <boxm2/ocl/algo/boxm2_ocl_paint_batch.h>
 //:
 // \file
 // \brief  A process for updating the scene.
@@ -6,9 +7,6 @@
 // \author Vishal Jain
 // \date Mar 25, 2011
 
-#include <bprb/bprb_func_process.h>
-#include <vcl_fstream.h>
-#include <vcl_algorithm.h>
 #include <boxm2/ocl/boxm2_opencl_cache.h>
 #include <boxm2/boxm2_scene.h>
 #include <boxm2/boxm2_block.h>
@@ -16,7 +14,9 @@
 #include <boxm2/io/boxm2_cache.h>
 #include <boxm2/io/boxm2_stream_cache.h>
 #include <boxm2/boxm2_util.h>
-#include <boxm2/ocl/algo/boxm2_ocl_paint_batch.h>
+#include <bprb/bprb_func_process.h>
+#include <vcl_fstream.h>
+#include <vcl_algorithm.h>
 
 //bsta sigma normalizer...
 #include <bsta/algo/bsta_sigma_normalizer.h>
@@ -59,7 +59,7 @@ bool boxm2_ocl_paint_batch_process(bprb_func_process& pro)
   }
   //get the inputs
   unsigned i = 0;
-  bocl_device_sptr           device    = pro.get_input<bocl_device_sptr>(i++); 
+  bocl_device_sptr           device    = pro.get_input<bocl_device_sptr>(i++);
   boxm2_scene_sptr           scene     = pro.get_input<boxm2_scene_sptr>(i++);
   boxm2_opencl_cache_sptr    ocl_cache = pro.get_input<boxm2_opencl_cache_sptr>(i++);
   boxm2_stream_cache_sptr    str_cache = pro.get_input<boxm2_stream_cache_sptr>(i++);
@@ -67,32 +67,32 @@ bool boxm2_ocl_paint_batch_process(bprb_func_process& pro)
 
   //get scene data type and appTypeSize
   vcl_string data_type;
-  int appTypeSize; 
-  vcl_vector<vcl_string> valid_types; 
-  valid_types.push_back(boxm2_data_traits<BOXM2_MOG3_GREY>::prefix()); 
-  valid_types.push_back(boxm2_data_traits<BOXM2_GAUSS_GREY>::prefix()); 
-  if( !boxm2_util::verify_appearance(*scene, valid_types, data_type, appTypeSize) ) {
+  int appTypeSize;
+  vcl_vector<vcl_string> valid_types;
+  valid_types.push_back(boxm2_data_traits<BOXM2_MOG3_GREY>::prefix());
+  valid_types.push_back(boxm2_data_traits<BOXM2_GAUSS_GREY>::prefix());
+  if ( !boxm2_util::verify_appearance(*scene, valid_types, data_type, appTypeSize) ) {
     vcl_cout<<"boxm2_ocl_paint_batch ERROR: scene doesn't have BOXM2_MOG3_GREY or BOXM2_MOG3_GREY_16 data type"<<vcl_endl;
     return false;
   }
-   
+
   //command queue
-  int status; 
+  int status;
   cl_command_queue queue = clCreateCommandQueue(device->context(),
                                                 *(device->device_id()),
-                                                CL_QUEUE_PROFILING_ENABLE,&status); 
+                                                CL_QUEUE_PROFILING_ENABLE,&status);
   if (status!=0) {
     vcl_cout<<" ERROR in initializing a queue"<<vcl_endl;
     return false;
   }
-   
+
   // iterate the scene block by block and write to output
   vcl_vector<boxm2_block_id> blk_ids = scene->get_block_ids();
-  vcl_vector<boxm2_block_id>::iterator id = blk_ids.begin(); 
+  vcl_vector<boxm2_block_id>::iterator id = blk_ids.begin();
   for (; id != blk_ids.end(); ++id) {
-    boxm2_block_id bid = *id; 
+    boxm2_block_id bid = *id;
     vcl_cout<<" block "<<bid<<vcl_endl;
-    boxm2_ocl_paint_batch::paint_block(scene,device,str_cache,ocl_cache,queue,data_type,bid,n_table); 
+    boxm2_ocl_paint_batch::paint_block(scene,device,str_cache,ocl_cache,queue,data_type,bid,n_table);
   }
 
   return true;
