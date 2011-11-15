4358cea583d4782753efcf47038b954998672871
Andrew Miller <andrew-miller@users.sourceforge.net>
2011-11-15 15:37:05 -0500

    added test weighted em kernel
    SVN-Revision: 33477

--- contrib/brl/bseg/boxm2/ocl/tests/test_driver.cxx
+++ contrib/brl/bseg/boxm2/ocl/tests/test_driver.cxx
@@ -2,11 +2,13 @@
 
 DECLARE( test_image_pyramid );
 DECLARE( test_process_mains ); 
+DECLARE( test_weighted_em   ); 
 
 void register_tests()
 {
   REGISTER( test_image_pyramid );
   REGISTER( test_process_mains ); 
+  REGISTER( test_weighted_em   );
 }
 
 DEFINE_MAIN;
