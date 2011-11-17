1a6e45c1e8242b1ed58bf61aafc7571eb8cae2d2
Joseph L. Mundy <mundyj@users.sourceforge.net>
2011-11-17 07:29:22 -0500

    Removed radial distortion test
    SVN-Revision: 33494

--- core/vpgl/tests/test_driver.cxx
+++ core/vpgl/tests/test_driver.cxx
@@ -6,7 +6,6 @@ DECLARE( test_affine_camera );
 DECLARE( test_fundamental_matrix );
 DECLARE( test_essential_matrix );
 DECLARE( test_calibration_matrix );
-DECLARE( test_poly_radial_distortion );
 DECLARE( test_rational_camera );
 DECLARE( test_local_rational_camera );
 DECLARE( test_generic_camera );
@@ -19,7 +18,6 @@ void register_tests()
   REGISTER( test_fundamental_matrix );
   REGISTER( test_essential_matrix );
   REGISTER( test_calibration_matrix );
-  REGISTER( test_poly_radial_distortion );
   REGISTER( test_rational_camera );
   REGISTER( test_local_rational_camera );
   REGISTER( test_generic_camera );
