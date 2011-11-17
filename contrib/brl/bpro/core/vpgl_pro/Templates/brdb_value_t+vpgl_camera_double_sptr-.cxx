2eb94278ee012239065f8df7765bdde57d0407b6
Andrew Miller <andrew-miller@users.sourceforge.net>
2011-11-17 11:12:57 -0500

    added vpgl_io include to brdb_value_t instantiation
    SVN-Revision: 33509

--- contrib/brl/bpro/core/vpgl_pro/Templates/brdb_value_t+vpgl_camera_double_sptr-.cxx
+++ contrib/brl/bpro/core/vpgl_pro/Templates/brdb_value_t+vpgl_camera_double_sptr-.cxx
@@ -1,4 +1,5 @@
 #include <vpgl/vpgl_camera_double_sptr.h>
+#include <vpgl/io/vpgl_io_camera.h>
 #include <brdb/brdb_value.txx>
 
 BRDB_VALUE_INSTANTIATE(vpgl_camera_double_sptr, "vpgl_camera_double_sptr");
