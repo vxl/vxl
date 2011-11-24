526b3dff0985b1b95f15c19fadfc03cdfdb490f9
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-11-24 08:47:27 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 33652

---
+++ contrib/brl/bseg/boxm2_multi/algo/tests/test_include.cxx
@@ -0,0 +1,7 @@
+#include <boxm2_multi/algo/boxm2_multi_pre_vis_inf.h>
+#include <boxm2_multi/algo/boxm2_multi_render.h>
+#include <boxm2_multi/algo/boxm2_multi_store_aux.h>
+#include <boxm2_multi/algo/boxm2_multi_update.h>
+#include <boxm2_multi/algo/boxm2_multi_update_cell.h>
+
+int main() {}
---
+++ contrib/brl/bseg/boxm2_multi/tests/test_include.cxx
@@ -0,0 +1,4 @@
+#include <boxm2_multi/boxm2_multi_cache.h>
+#include <boxm2_multi/boxm2_multi_util.h>
+
+int main() {}
