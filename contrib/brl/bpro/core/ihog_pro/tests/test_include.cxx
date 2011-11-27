820ce731fc887a89477a4462fa3133e55169e769
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-11-27 04:50:40 -0500

    added test_include.cxx -- this makes sure that all .h files belonging to a library are included at least once in a test
    SVN-Revision: 33659

---
+++ contrib/brl/bpro/core/ihog_pro/tests/test_include.cxx
@@ -0,0 +1,4 @@
+#include <ihog_pro/ihog_processes.h>
+#include <ihog_pro/ihog_register.h>
+
+int main() { return 0; }
---
+++ core/vgui/wx/tests/test_include.cxx
@@ -0,0 +1,8 @@
+#include <vgui/wx/vgui_wx.h>
+#include <vgui/wx/vgui_wx_adaptorXmlHandler.h>
+#include <vgui/wx/wxSliderPanel.h>
+#include <vgui/wx/wxSliderPanelXmlHandler.h>
+#include <vgui/wx/wxVideoControl.h>
+#include <vgui/wx/wxVideoControlXmlHandler.h>
+
+int main() { return 0; }
