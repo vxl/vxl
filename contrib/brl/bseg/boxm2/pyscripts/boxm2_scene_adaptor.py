a2258ee4bc599815f895b36bb9fd00cbba0a562c
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-11-29 16:53:03 -0500

    using TABs in a Python script is not a good idea...
    SVN-Revision: 33690

--- contrib/brl/bseg/boxm2/pyscripts/boxm2_scene_adaptor.py
+++ contrib/brl/bseg/boxm2/pyscripts/boxm2_scene_adaptor.py
@@ -22,8 +22,8 @@ class boxm2_scene_adaptor:
     self.device = None;
     self.opencl_cache = None;
     self.ocl_mgr = None;
-    self.str_cache = None; 
-    self.model_dir = None; 
+    self.str_cache = None;
+    self.model_dir = None;
 
     #if device_string is gpu, load up opencl
     if device_string[0:3]=="gpu" :
@@ -32,14 +32,14 @@ class boxm2_scene_adaptor:
     elif device_string[0:3]=="cpp" :
       self.scene, self.cpu_cache = load_cpp(scene_str);
       self.active_cache = self.cpu_cache;
-    
+
     #store model directory for later use
-    self.model_dir = self.describe(); 
+    self.model_dir = self.describe();
 
   #describe scene
   def describe(self) :
     return describe_scene(self.scene);
-  
+
   #update wrapper, can pass in a Null device to use
   def update(self, cam, img, update_alpha=True, mask=None, device_string="") :
     cache = self.active_cache;
@@ -65,8 +65,8 @@ class boxm2_scene_adaptor:
     if device_string=="gpu" :
       cache = self.opencl_cache;
     elif device_string=="cpp" :
-	  print " Not  implemented in C++ yet ";
-	  return;
+      print " Not  implemented in C++ yet ";
+      return;
     update_app_grey(self.scene, cache, cam, img, dev);
 
   #render wrapper, same as above
@@ -86,35 +86,35 @@ class boxm2_scene_adaptor:
     return expimg;
 
   #render wrapper, same as above
-  def render_vis(self, cam, ni=1280, nj=720, device_string="") : 
-    cache = self.active_cache; 
-    dev = self.device; 
+  def render_vis(self, cam, ni=1280, nj=720, device_string="") :
+    cache = self.active_cache;
+    dev = self.device;
     #check if force gpu or cpu
-    if device_string=="gpu" : 
-      cache = self.opencl_cache; 
-    elif device_string=="cpp" : 
-      cache = self.cpu_cache; 
-      dev = None; 
-    if self.rgb : 
-      print "Render visibility map not implemented for color scenes"; 
-      return; 
-    else : 
-      expimg, vis_image = render_grey_and_vis(self.scene, cache, cam, ni, nj, dev); 
-    return expimg, vis_image; 
-  
+    if device_string=="gpu" :
+      cache = self.opencl_cache;
+    elif device_string=="cpp" :
+      cache = self.cpu_cache;
+      dev = None;
+    if self.rgb :
+      print "Render visibility map not implemented for color scenes";
+      return;
+    else :
+      expimg, vis_image = render_grey_and_vis(self.scene, cache, cam, ni, nj, dev);
+    return expimg, vis_image;
+
   #render depth image wrapper
   def render_depth(self, cam, ni=1280, nj=720, device_string="") :
     cache = self.active_cache;
     dev = self.device;
     #check if force gpu or cpu
-    if device_string=="gpu" : 
-      cache = self.opencl_cache; 
-    elif device_string=="cpp" : 
-      cache = self.cpu_cache; 
-      dev = None; 
-    expimg,varimg = render_depth(self.scene, cache, cam, ni, nj, dev); 
-    return expimg,varimg; 
-  
+    if device_string=="gpu" :
+      cache = self.opencl_cache;
+    elif device_string=="cpp" :
+      cache = self.cpu_cache;
+      dev = None;
+    expimg,varimg = render_depth(self.scene, cache, cam, ni, nj, dev);
+    return expimg,varimg;
+
   #render heigh map render
   def render_height_map(self, device_string="") :
     cache = self.active_cache;
@@ -182,7 +182,7 @@ class boxm2_scene_adaptor:
   #clear cache (both caches if OPENCL scene)
   def clear_cache(self) :
     clear_cache(self.cpu_cache);
-    if self.opencl_cache: 
+    if self.opencl_cache:
       clear_cache(self.opencl_cache);
 
 
@@ -190,19 +190,19 @@ class boxm2_scene_adaptor:
   ######### BATCH UPDATE METHODS ######################################
   #####################################################################
   def create_stream_cache(self, imgs, interval=1, types=""):
-    
+
     # write image identifiers to file
-    #imgRange = range(0, len(imgs), interval); 
-    #num_imgs = len(imgRange); 
+    #imgRange = range(0, len(imgs), interval);
+    #num_imgs = len(imgRange);
     image_id_fname = self.model_dir + "/image_list.txt";
     fd = open(image_id_fname,"w");
     print >> fd, len(imgs)
     #for i in imgRange:
-    #  print >>fd, "img_%05d"%i 
+    #  print >>fd, "img_%05d"%i
     for img in imgs:
-      fname, fextension = splitext(img); 
+      fname, fextension = splitext(img);
       bname = basename(fname)
-      print >> fd, bname; 
+      print >> fd, bname;
     fd.close();
 
     #write type identifiers into file
@@ -230,32 +230,32 @@ class boxm2_scene_adaptor:
     for idx in range( len(imgs) ) :
       print '--------------------------';
       print "processing image " + imgs[idx];
-      
+
       #load cam/img
       img, ni, nj = load_image (imgs[idx]);
-      pcam        = load_perspective_camera(cams[idx]); 
-      gcam        = persp2gen(pcam,ni,nj); 
-     
+      pcam        = load_perspective_camera(cams[idx]);
+      gcam        = persp2gen(pcam,ni,nj);
+
       # update aux per view call
-      fname, fextension = splitext(imgs[idx]); 
+      fname, fextension = splitext(imgs[idx]);
       imageID = basename(fname)
-      self.update_aux(img, gcam, imageID); 
-  
+      self.update_aux(img, gcam, imageID);
+
   #create an imagewise aux buffer for cam/img
-  def update_aux(self, img, cam, imgId, device_string="") : 
+  def update_aux(self, img, cam, imgId, device_string="") :
     if device_string=="":
       update_aux_per_view(self.scene, self.active_cache, img, cam, imgId, self.device)
     elif device_string=="gpu" :
-      update_aux_per_view(self.scene, self.opencl_cache, img, cam, imgId, self.device) 
+      update_aux_per_view(self.scene, self.opencl_cache, img, cam, imgId, self.device)
     elif device_string=="cpp" :
       update_aux_per_view(self.scene, self.cpu_cache, img, cam, imgId, None)
 
-  #takes already created aux buffers (for each image) and fits a Mixture of 3 
-  #Gaussians to each cell, saves the apperaance
+  #takes already created aux buffers (for each image) and fits a Mixture of 3
+  #Gaussians to each cell, saves the appearance
   def batch_paint(self, imgs, cams, device_string="") :
     #verify stream cache
-    if(self.str_cache==None): 
-      self.create_stream_cache(imgs); 
+    if (self.str_cache==None):
+      self.create_stream_cache(imgs);
 
     #sigma norm table?
     under_estimation_probability = 0.2;
@@ -286,13 +286,13 @@ class boxm2_scene_adaptor:
     boxm2_batch.init_process("boxm2StreamCacheCloseFilesProcess");
     boxm2_batch.set_input_from_db(0,self.str_cache);
     boxm2_batch.run_process();
-    
+
     #write out afterwards
-    self.write_cache(); 
-    
+    self.write_cache();
+
   def cpu_batch_paint(self, imgs, cams) :
-    if(self.str_cache==None): 
-      self.create_stream_cache(imgs); 
+    if (self.str_cache==None):
+      self.create_stream_cache(imgs);
 
     #sigma norm table?
     under_estimation_probability = 0.2;
@@ -304,11 +304,11 @@ class boxm2_scene_adaptor:
 
     #loop over images creating aux data
     for idx in range(0, len(imgs)):
-    
+
       #load cam/img
       img, ni, nj = load_image (imgs[idx]);
-      pcam        = load_perspective_camera(cams[idx]); 
-      gcam        = persp2gen(pcam,ni,nj); 
+      pcam        = load_perspective_camera(cams[idx]);
+      gcam        = persp2gen(pcam,ni,nj);
 
       #create norm intensity (num rays...)
       boxm2_batch.init_process("boxm2CppCreateNormIntensitiesProcess");
@@ -327,7 +327,7 @@ class boxm2_scene_adaptor:
       boxm2_batch.set_input_from_db(3, img);
       boxm2_batch.set_input_string(4, "img_"+"%05d"%idx);
       boxm2_batch.run_process();
-      self.write_cache(True); 
+      self.write_cache(True);
 
     boxm2_batch.init_process("boxm2CppBatchUpdateOPT2Process");
     boxm2_batch.set_input_from_db(0, self.scene);
@@ -340,5 +340,5 @@ class boxm2_scene_adaptor:
     boxm2_batch.init_process("boxm2StreamCacheCloseFilesProcess");
     boxm2_batch.set_input_from_db(0, self.str_cache);
     boxm2_batch.run_process();
-    
-    self.write_cache(); 
+
+    self.write_cache();
