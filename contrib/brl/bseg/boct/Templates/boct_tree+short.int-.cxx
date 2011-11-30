7b40688d1668c6721e550d60fcb25a50201381d2
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-11-30 11:22:07 -0500

    added short int
    SVN-Revision: 33708

--- contrib/brl/bseg/boct/Templates/boct_tree+short.int-.cxx
+++ contrib/brl/bseg/boct/Templates/boct_tree+short.int-.cxx
@@ -1,3 +1,4 @@
 #include <boct/boct_tree.txx>
 
 BOCT_TREE_INSTANTIATE(short,int);
+BOCT_TREE_INSTANTIATE(short,short int);
--- contrib/brl/bseg/boct/Templates/boct_tree_cell+short.int-.cxx
+++ contrib/brl/bseg/boct/Templates/boct_tree_cell+short.int-.cxx
@@ -1,3 +1,4 @@
 #include <boct/boct_tree_cell.txx>
 
 BOCT_TREE_CELL_INSTANTIATE(short,int);
+BOCT_TREE_CELL_INSTANTIATE(short,short int);
