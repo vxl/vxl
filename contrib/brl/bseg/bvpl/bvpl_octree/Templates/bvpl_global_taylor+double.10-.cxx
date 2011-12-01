0f5321ff752ece570d4b0b0f949f2a3040443e8d
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-12-01 09:29:50 -0500

    adding template instantiations
    SVN-Revision: 33731

---
+++ contrib/brl/bseg/bvpl/bvpl_octree/Templates/bvpl_global_taylor+double.10-.cxx
@@ -0,0 +1,3 @@
+#include <bvpl_octree/bvpl_global_taylor.txx>
+
+BVPL_GLOBAL_TAYLOR_INSTANTIATE(double, 10);
---
+++ contrib/brl/bseg/bvpl/bvpl_octree/Templates/vbl_smart_ptr+bvpl_global_taylor_double.10--.cxx
@@ -0,0 +1,8 @@
+#include <bvpl/bvpl_octree/bvpl_global_taylor.h>
+#include <vbl/vbl_smart_ptr.txx>
+
+typedef bvpl_global_taylor<double, 10> bvpl_global_taylor_double_10;
+
+VBL_SMART_PTR_INSTANTIATE(bvpl_global_taylor_double_10);
+
+
