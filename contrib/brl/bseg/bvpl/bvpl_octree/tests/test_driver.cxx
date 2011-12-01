0aa42af4d25877b9f2a1039b9bc3315c9193c581
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2011-12-01 09:50:01 -0500

    added tests for steerable filters
    SVN-Revision: 33751

--- contrib/brl/bseg/bvpl/bvpl_octree/tests/test_driver.cxx
+++ contrib/brl/bseg/bvpl/bvpl_octree/tests/test_driver.cxx
@@ -4,12 +4,15 @@ DECLARE( test_octree_kernel_operator );
 DECLARE( test_octree_neighbors );
 DECLARE( test_pca_kernels );
 DECLARE( test_taylor_basis );
+DECLARE( test_gauss3D_steerable_filters );
+
 void register_tests()
 {
   REGISTER( test_octree_kernel_operator );
   REGISTER( test_octree_neighbors );
   REGISTER( test_pca_kernels );
   REGISTER( test_taylor_basis );
+  REGISTER( test_gauss3D_steerable_filters );
 }
 
 
