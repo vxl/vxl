65c4a2511f33d4b9c0954aea7a2f0b09f9461781
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-12-01 15:46:31 -0500

    Made brief Doxygen comment an argument of \brief
    SVN-Revision: 33786

--- contrib/brl/bpro/core/vil_pro/processes/vil_image_mean_process.cxx
+++ contrib/brl/bpro/core/vil_pro/processes/vil_image_mean_process.cxx
@@ -1,7 +1,8 @@
 // This is brl/bpro/core/vil_pro/processes/vil_image_mean_process.cxx
 #include <bprb/bprb_func_process.h>
 //:
-// \file Compute mean value in an image
+// \file
+// \brief Compute mean value in an image
 
 #include <vil/vil_math.h>
 #include <vil/vil_image_view.h>
@@ -12,19 +13,13 @@
 //: Constructor
 bool vil_image_mean_process_cons(bprb_func_process& pro)
 {
-  //this process takes one input: the filename
-  bool ok=false;
+  //this process takes one input: the image
   vcl_vector<vcl_string> input_types;
-  input_types.push_back("vil_image_view_base_sptr"); 
-  ok = pro.set_input_types(input_types);
-  if (!ok) return ok;
-  
+  input_types.push_back("vil_image_view_base_sptr");
   vcl_vector<vcl_string> output_types;
   output_types.push_back("float");  // mean
-  ok = pro.set_output_types(output_types);
-  if (!ok) return ok;
-  
-  return true;
+  return pro.set_input_types(input_types)
+     &&  pro.set_output_types(output_types);
 }
 
 //: Execute the process
@@ -35,23 +30,18 @@ bool vil_image_mean_process(bprb_func_process& pro)
     vcl_cout << "vil_image_mean_process: The input number should be 1" << vcl_endl;
     return false;
   }
-  
-  // get the inputs
+
+  // get the input
   unsigned i=0;
   vil_image_view_base_sptr img_ptr_a = pro.get_input<vil_image_view_base_sptr>(i++);
-  
-  // for now assume input is a float images
+
+  // for now assume input is a float image
   vil_image_view<float> view_a = *(vil_convert_cast(float(), img_ptr_a));
-  
-  float mean= 0.0f; 
-  
+
+  float mean= 0.0f;
   vil_math_mean(mean, view_a, 0);
-  
   pro.set_output_val<float>(0, mean);
-  
+
   return true;
 }
-
-
-
 
--- contrib/brl/bpro/core/vil_pro/processes/vil_image_ssd_process.cxx
+++ contrib/brl/bpro/core/vil_pro/processes/vil_image_ssd_process.cxx
@@ -1,7 +1,8 @@
 // This is brl/bpro/core/vil_pro/processes/vil_image_ssd_process.cxx
 #include <bprb/bprb_func_process.h>
 //:
-// \file Compute sum of square images between two images
+// \file
+// \brief Compute sum of square differences between two images
 
 #include <vil/vil_math.h>
 #include <vil/vil_image_view.h>
@@ -12,20 +13,14 @@
 //: Constructor
 bool vil_image_ssd_process_cons(bprb_func_process& pro)
 {
-  //this process takes one input: the filename
-  bool ok=false;
+  //this process takes two input images and one output value
   vcl_vector<vcl_string> input_types;
-  input_types.push_back("vil_image_view_base_sptr"); 
-  input_types.push_back("vil_image_view_base_sptr"); 
-  ok = pro.set_input_types(input_types);
-  if (!ok) return ok;
-  
+  input_types.push_back("vil_image_view_base_sptr");
+  input_types.push_back("vil_image_view_base_sptr");
   vcl_vector<vcl_string> output_types;
   output_types.push_back("float");  // ssd
-  ok = pro.set_output_types(output_types);
-  if (!ok) return ok;
-  
-  return true;
+  return pro.set_input_types(input_types)
+     &&  pro.set_output_types(output_types);
 }
 
 //: Execute the process
@@ -33,27 +28,24 @@ bool vil_image_ssd_process(bprb_func_process& pro)
 {
   // Sanity check
   if (pro.n_inputs()< 2) {
-    vcl_cout << "vil_image_ssd_process: The input number should be 2" << vcl_endl;
+    vcl_cout << "vil_image_ssd_process: The number of inputs should be 2" << vcl_endl;
     return false;
   }
-  
+
   // get the inputs
   unsigned i=0;
   vil_image_view_base_sptr img_ptr_a = pro.get_input<vil_image_view_base_sptr>(i++);
   vil_image_view_base_sptr img_ptr_b = pro.get_input<vil_image_view_base_sptr>(i++);
-  
+
   // for now assume inputs are byte images
   vil_image_view<vxl_byte> view_a = *(vil_convert_cast(vxl_byte(), img_ptr_a));
   vil_image_view<vxl_byte> view_b = *(vil_convert_cast(vxl_byte(), img_ptr_b));
-  
-  float dummy;
-  float result = vil_math_ssd(view_a, view_b, dummy );
-  
+
+  float dummy = 0.0f;
+  float result = vil_math_ssd(view_a, view_b, dummy);
+
   pro.set_output_val<float>(0, result);
-  
+
   return true;
 }
-
-
-
 
