8d0a33181b77f2bb265e4813aec0d1939c507ee0
Andrew Miller <andrew-miller@users.sourceforge.net>
2011-12-02 17:00:45 -0500

    added blobwise KL div to blobwise_mutual_info
    SVN-Revision: 33809

--- contrib/brl/bseg/brip/brip_blobwise_mutual_info.h
+++ contrib/brl/bseg/brip/brip_blobwise_mutual_info.h
@@ -16,8 +16,31 @@
 template<class T>
 void brip_blobwise_mutual_info ( const vil_image_view<T>& img1,
                                  const vil_image_view<T>& img2,
+                                 const vil_image_view<T>& weights,
                                  const vil_image_view<bool>& mask,
                                        vil_image_view<T>& mi_img ); 
 
+
+template<class T>
+void brip_blobwise_kl_div( const vil_image_view<T>& img1,
+                           const vil_image_view<T>& img2,
+                           const vil_image_view<bool>& mask,
+                                 vil_image_view<T>& mi_img ); 
+                                 
+//: calculates the kl divergeance D_kl ( P || Q ); 
+inline
+double brip_hist_kl_div(const vcl_vector<double>& P, double magP, 
+                        const vcl_vector<double>& Q, double magQ)
+{
+  double kl = 0.0;
+  for(int i=0; i<P.size(); ++i) {
+    double probP = P[i]/magP; 
+    double probQ = Q[i]/magQ; 
+    if(probP==0 || probQ==0) continue; 
+    
+    kl += probP * vcl_log( probP / probQ ); 
+  }
+  return  kl/vnl_math::ln2; // divide by ln(2) to convert this measure to base 2
+}
 
 #endif // brip_blobwise_mutual_info_h_
