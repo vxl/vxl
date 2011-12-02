2d63ac8f61c39a6c66cda7956dae35e6630710b1
Andrew Miller <andrew-miller@users.sourceforge.net>
2011-12-02 17:01:48 -0500

    added kl div
    SVN-Revision: 33810

--- contrib/brl/bseg/boxm2/pyscripts/brip_adaptor.py
+++ contrib/brl/bseg/boxm2/pyscripts/brip_adaptor.py
@@ -3,13 +3,29 @@ from boxm2_register import boxm2_batch, dbvalue;
 ###########################################################
 # Calculate mutual information per blob between two images
 ###########################################################
-def blobwise_mutual_info(in_img, exp_img, blob_img) :
+def blobwise_mutual_info(in_img, exp_img, gradImg, blob_img) :
   boxm2_batch.init_process("bripBlobwiseMutualInfoProcess")
   boxm2_batch.set_input_from_db(0, in_img);
-  boxm2_batch.set_input_from_db(1, exp_img);
-  boxm2_batch.set_input_from_db(2, blob_img);
+  boxm2_batch.set_input_from_db(1, exp_img);  
+  boxm2_batch.set_input_from_db(2, gradImg);
+  boxm2_batch.set_input_from_db(3, blob_img);
   boxm2_batch.run_process();
   (id,type) = boxm2_batch.commit_output(0);
   vis_img = dbvalue(id,type);
   return vis_img;
   
+###########################################################
+# Calculate KL Divergence per blob between two images
+###########################################################
+def blobwise_kl_div(p_img, q_img, blob_img, thresh=.1) :
+  boxm2_batch.init_process("bripBlobwiseKLDivProcess")
+  boxm2_batch.set_input_from_db(0, p_img);
+  boxm2_batch.set_input_from_db(1, q_img);  
+  boxm2_batch.set_input_from_db(2, blob_img);
+  boxm2_batch.set_input_float(3, thresh); 
+  boxm2_batch.run_process();
+  (id,type) = boxm2_batch.commit_output(0);
+  kl_img = dbvalue(id,type);
+  (id,type) = boxm2_batch.commit_output(1);
+  thresh_img = dbvalue(id,type);
+  return kl_img, thresh_img;
