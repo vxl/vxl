1770672a448977548d04ba37dc01525f35d92cfd
Joseph L. Mundy <mundyj@users.sourceforge.net>
2011-12-08 16:51:01 -0500

    Remove vidl1 dependency
    SVN-Revision: 33849

--- contrib/brl/bseg/vpro/Templates/vbl_smart_ptr+vpro_video_process-.cxx
+++
@@ -1,5 +0,0 @@
-#include <vpro/vpro_video_process.h>
-#include <vsol/vsol_box_2d.h>
-#include <vbl/vbl_smart_ptr.txx>
-
-VBL_SMART_PTR_INSTANTIATE(vpro_video_process);
--- contrib/brl/bseg/vpro/Templates/vbl_smart_ptr+vpro_vil_video_process-.cxx
+++
@@ -1,5 +0,0 @@
-#include <vpro/vpro_vil_video_process.h>
-#include <vsol/vsol_box_2d.h>
-#include <vbl/vbl_smart_ptr.txx>
-
-VBL_SMART_PTR_INSTANTIATE(vpro_vil_video_process);
