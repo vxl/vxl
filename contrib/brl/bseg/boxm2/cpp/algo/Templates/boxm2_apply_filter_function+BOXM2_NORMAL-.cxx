dc94937660d6969716583f7bbf8251017cba1265
Ali Osman Ulusoy <aulusoy@users.sourceforge.net>
2011-12-08 17:26:16 -0500

    Explicit instantiations of boxm2_apply_filter_function.
    SVN-Revision: 33851

---
+++ contrib/brl/bseg/boxm2/cpp/algo/Templates/boxm2_apply_filter_function+BOXM2_NORMAL-.cxx
@@ -0,0 +1,5 @@
+#include <boxm2/boxm2_data.txx>
+#include <boxm2/boxm2_data_traits.h>
+#include <boxm2/cpp/algo/boxm2_apply_filter_function.txx>
+
+template class boxm2_apply_filter_function<BOXM2_NORMAL>;
