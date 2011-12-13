89283b0dede0edecb215589716997e7c848a0de9
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-12-13 11:09:11 -0500

    corrected a comment
    SVN-Revision: 33884

--- contrib/brl/bpro/core/brip_pro/processes/brip_blob_intersection_process.cxx
+++ contrib/brl/bpro/core/brip_pro/processes/brip_blob_intersection_process.cxx
@@ -1,4 +1,4 @@
-// This is brl/bseg/boxm2/pro/processes/brip_blob_intersection_process.cxx
+// This is brl/bpro/core/brip_pro/processes/brip_blob_intersection_process.cxx
 #include <bprb/bprb_func_process.h>
 //:
 // \file
@@ -7,8 +7,7 @@
 #include <bil/algo/bil_blob_finder.h>
 #include <bprb/bprb_parameters.h>
 #include <vil/vil_image_view.h>
-#include <vil/vil_convert.h>
-#include <vil/vil_save.h>
+#include <vil/vil_chord.h>
 #include <bbas_pro/bbas_1d_array_float.h>
 #include <vcl_algorithm.h>
 #include <vcl_sstream.h>
@@ -24,23 +23,18 @@ bool brip_blob_intersection_process_cons(bprb_func_process& pro)
 {
   using namespace brip_blob_intersection_process_globals;
 
-  // this process takes 3 inputs:
+  // this process takes 2 inputs:
   vcl_vector<vcl_string> input_types;
   input_types.push_back("vil_image_view_base_sptr");  // modeled blob map
   input_types.push_back("vil_image_view_base_sptr");  // ground truth map
-  if (! pro.set_input_types(input_types))
-    return false;
-
-  //// default arguments
-  //brdb_value_sptr empty_mask = new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<unsigned char>(1,1));
-  //pro.set_input(2, empty_mask);
-
   // this process takes 3 outputs:
   vcl_vector<vcl_string> output_types;
   output_types.push_back("int");  // num true positives
   output_types.push_back("int");  // num false positives
   output_types.push_back("int");  // num blobs in GT
-  return pro.set_output_types(output_types);
+
+  return pro.set_input_types(input_types)
+      && pro.set_output_types(output_types);
 }
 
 //: Execute the process
@@ -76,82 +70,81 @@ bool brip_blob_intersection_process(bprb_func_process& pro)
     return false;
   }
 
-  //create boolean gtmap
-  vil_image_view<bool>  gt_map(gt_uchar->ni(), gt_uchar->nj()), 
+  // create boolean gtmap
+  vil_image_view<bool>  gt_map(gt_uchar->ni(), gt_uchar->nj()),
                         blob_map(gt_uchar->ni(), gt_uchar->nj());
   for (unsigned int i=0; i<gt_uchar->ni(); ++i)
     for (unsigned int j=0; j<gt_uchar->nj(); ++j) {
       gt_map(i,j)   = (*gt_uchar)(i,j) == 0 ? false : true;
-      blob_map(i,j) = (*blob_uchar)(i,j) == 0 ? false : true; 
+      blob_map(i,j) = (*blob_uchar)(i,j) == 0 ? false : true;
     }
 
-  //blob detect each image
+  // blob detect each image
   bil_blob_finder gt_finder(gt_map);
   bil_blob_finder mp_finder(blob_map);
-  typedef vcl_vector<vil_chord> blob_t; 
+  typedef vcl_vector<vil_chord> blob_t;
   vcl_vector<blob_t> gt_blobs, mp_blobs;
 
-  //blob region is just a vector of vil_chords (rows in image)
+  // blob region is just a vector of vil_chords (rows in image)
   vcl_vector<vil_chord> region;
-  while (gt_finder.next_4con_region(region)) 
-    gt_blobs.push_back(region); 
-  while (mp_finder.next_4con_region(region)) 
-    mp_blobs.push_back(region); 
-  
-  //---- cross check blobs ---------
+  while (gt_finder.next_4con_region(region))
+    gt_blobs.push_back(region);
+  while (mp_finder.next_4con_region(region))
+    mp_blobs.push_back(region);
+
+  // ---- cross check blobs ---------
   int numTP=0;
-  bool* trueBlobs = new bool[mp_blobs.size()]; 
+  bool* trueBlobs = new bool[mp_blobs.size()];
   vcl_fill(trueBlobs, trueBlobs+mp_blobs.size(), false);
-  for(int i=0; i<gt_blobs.size(); ++i) {
-    bool gt_blob_found = false; 
-    for(int j=0; j<mp_blobs.size(); ++j) {
-      blob_t gt_blob = gt_blobs[i]; 
-      blob_t mp_blob = mp_blobs[j]; 
-    
-      //determine if gt_blob[i] intersects with mp_blob[j]
-      bool intersects = false; 
-      blob_t::iterator gt_iter; 
-      for(gt_iter=gt_blob.begin(); gt_iter!=gt_blob.end(); ++gt_iter) {
-        for(unsigned gt_i=gt_iter->ilo; gt_i<gt_iter->ihi; ++gt_i) {
-          
-          //gt pixel = (gt_i, gt_iter->j)
-          blob_t::iterator mp_iter; 
-          for(mp_iter=mp_blob.begin(); mp_iter!=mp_blob.end(); ++mp_iter) {
-            for(unsigned mp_i=mp_iter->ilo; mp_i<mp_iter->ihi; ++mp_i) {
-              //mp pixel = (mp_i, mp_iter->j)
-              //if pixels match, they intersect
-              if(mp_i==gt_i && mp_iter->j==gt_iter->j) {
-                intersects = true; 
-                break; 
+  for (int i=0; i<gt_blobs.size(); ++i) {
+    bool gt_blob_found = false;
+    for (int j=0; j<mp_blobs.size(); ++j) {
+      blob_t gt_blob = gt_blobs[i];
+      blob_t mp_blob = mp_blobs[j];
+
+      // determine if gt_blob[i] intersects with mp_blob[j]
+      bool intersects = false;
+      blob_t::iterator gt_iter;
+      for (gt_iter=gt_blob.begin(); gt_iter!=gt_blob.end(); ++gt_iter) {
+        for (unsigned gt_i=gt_iter->ilo; gt_i<gt_iter->ihi; ++gt_i) {
+
+          // gt pixel = (gt_i, gt_iter->j)
+          blob_t::iterator mp_iter;
+          for (mp_iter=mp_blob.begin(); mp_iter!=mp_blob.end(); ++mp_iter) {
+            for (unsigned mp_i=mp_iter->ilo; mp_i<mp_iter->ihi; ++mp_i) {
+              // mp pixel = (mp_i, mp_iter->j)
+              // if pixels match, they intersect
+              if (mp_i==gt_i && mp_iter->j==gt_iter->j) {
+                intersects = true;
+                break;
               }
             }
           }
-          if(intersects) 
-            break;    
+          if (intersects)
+            break;
         }
-        if(intersects) 
-          break; 
+        if (intersects)
+          break;
       }
-      
-      //count total number of model blobs that intersect
-      if(intersects)
-        trueBlobs[j] = true; 
 
-      //determine true positive, false positive
-      if(intersects && !gt_blob_found) {
+      // count total number of model blobs that intersect
+      if (intersects)
+        trueBlobs[j] = true;
+
+      // determine true positive, false positive
+      if (intersects && !gt_blob_found) {
         numTP++;
-        gt_blob_found = true; 
+        gt_blob_found = true;
         break;
       }
-
     }
   }
-  
-  //count non intersecting blobs
+
+  // count non intersecting blobs
   int falseBlobs = 0;
-  for(int i=0; i<mp_blobs.size(); ++i)
-    if(!trueBlobs[i]) falseBlobs++;
-  
+  for (int i=0; i<mp_blobs.size(); ++i)
+    if (!trueBlobs[i]) falseBlobs++;
+
   // set outputs
   if (pro.n_outputs() < n_outputs_) {
     vcl_cerr << "brip_blob_intersection_process: The number of outputs should be "<<n_outputs_<<'\n';
