dad111612320b5e344af1fc75ac5129598b1239b
Gwenael Guillard <gwenaelguillard@users.sourceforge.net>
2011-12-15 05:34:32 -0500

    new tests
    SVN-Revision: 33895

--- contrib/mul/mbl/tests/test_text_file.cxx
+++ contrib/mul/mbl/tests/test_text_file.cxx
@@ -1,6 +1,7 @@
-#include <vcl_iostream.h>
-
 #include <testlib/testlib_test.h>
+
+#include <vcl_iostream.h>
+#include <vcl_fstream.h>
 
 #include <mbl/mbl_save_text_file.h>
 #include <mbl/mbl_load_text_file.h>
@@ -21,8 +22,21 @@ void test_text_file()
   mbl_save_text_file( double_vec, "double_vec.txt");
   vcl_vector<double> double_vec_in;
   mbl_load_text_file( double_vec_in, "double_vec.txt");
+  TEST( "double_vec io - file", double_vec==double_vec_in, true );
 
-  TEST( "double_vec io", double_vec==double_vec_in, true );
+  {
+    vcl_ofstream os("double_vec2.txt");
+    mbl_save_text_file( double_vec, os);
+    os.close();
+    vcl_ifstream is;
+    is.open("double_vec2.txt", vcl_ifstream::in);
+    vcl_vector<double> double_vec_in2;
+    mbl_load_text_file( double_vec_in2, is);
+    is.close();
+
+    TEST( "double_vec io - stream", double_vec==double_vec_in2, true );
+  }
+
 
   //create int vector
   vcl_vector<int> int_vec(3);
@@ -34,7 +48,20 @@ void test_text_file()
   vcl_vector<int> int_vec_in;
   mbl_load_text_file( int_vec_in, "int_vec.txt");
 
-  TEST( "int_vec io", int_vec==int_vec_in, true );
+  TEST( "int_vec io - file", int_vec==int_vec_in, true );
+
+  {
+    vcl_ofstream os("int_vec2.txt");
+    mbl_save_text_file( int_vec, os);
+    os.close();
+    vcl_ifstream is;
+    is.open("int_vec2.txt", vcl_ifstream::in);
+    vcl_vector<int> int_vec_in2;
+    mbl_load_text_file( int_vec_in2, is);
+    is.close();
+
+    TEST( "int_vec io - stream", int_vec==int_vec_in2, true );
+  }
 
   //create bool vector
   vcl_vector<bool> bool_vec(3);
@@ -46,7 +73,20 @@ void test_text_file()
   vcl_vector<bool> bool_vec_in;
   mbl_load_text_file( bool_vec_in, "bool_vec.txt");
 
-  TEST( "bool_vec io", bool_vec==bool_vec_in, true );
+  TEST( "bool_vec io - file", bool_vec==bool_vec_in, true );
+
+  {
+    vcl_ofstream os("bool_vec2.txt");
+    mbl_save_text_file( bool_vec, os);
+    os.close();
+    vcl_ifstream is;
+    is.open("bool_vec2.txt", vcl_ifstream::in);
+    vcl_vector<bool> bool_vec_in2;
+    mbl_load_text_file( bool_vec_in2, is);
+    is.close();
+
+    TEST( "bool_vec io - stream", bool_vec==bool_vec_in2, true );
+  }
 }
 
 TESTMAIN(test_text_file);
