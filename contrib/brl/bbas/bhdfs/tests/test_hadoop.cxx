7ad7c880f3e5c9f8011d5a0d005a5c587c5bbac2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-12-21 13:47:39 -0500

    moved "\file" to the top, and added "\brief", for Doxygen style comments
    SVN-Revision: 33937

--- contrib/brl/bbas/bhdfs/tests/test_hadoop.cxx
+++ contrib/brl/bbas/bhdfs/tests/test_hadoop.cxx
@@ -1,17 +1,18 @@
-//: test to open and write a test file to the current user's home directory : ${USER_HOME_ON_HADOOP}  e.g. /user/hduser
+//:
+// \file
+// \brief test to open and write a test file to the current user's home directory : ${USER_HOME_ON_HADOOP}  e.g. /user/hduser
 //
 //  start hadoop ${HADOOP_HOME}/bin/start-all.sh
 //
 //  run the executable for this test
 //  ./bhdfs_test_all test_hadoop
 //
-//  check if any error messages come up 
+//  check if any error messages come up
 //
 //  check if the file is successfully created on hadoop
 //  ${HADOOP_HOME}/bin/hadoop dfs -ls ${USER_HOME_ON_HADOOP}
 //  ${HADOOP_HOME}/bin/hadoop dfs -cat ${USER_HOME_ON_HADOOP}/testfile.txt  --> should see Hello, World!
-//
-// \file
+
 #include <testlib/testlib_test.h>
 #include <vcl_iostream.h>
 
@@ -20,20 +21,20 @@
 //:
 void test_hadoop()
 {
-  hdfsFS fs = hdfsConnect("default", 0); 
+  hdfsFS fs = hdfsConnect("default", 0);
   char buffer[256];
   hdfsGetWorkingDirectory(fs, buffer, sizeof(buffer));
   vcl_cout << "working directory is: " << buffer << vcl_endl;
-  vcl_string writePath = vcl_string(buffer) + "/testfile.txt"; 
-  hdfsFile writeFile = hdfsOpenFile(fs, writePath.c_str(), O_WRONLY|O_CREAT, 0, 0, 0); 
+  vcl_string writePath = vcl_string(buffer) + "/testfile.txt";
+  hdfsFile writeFile = hdfsOpenFile(fs, writePath.c_str(), O_WRONLY|O_CREAT, 0, 0, 0);
 
   TEST("open file ", !writeFile, false);
 
-  vcl_string buffer2 = "Hello, World!"; 
-  tSize num_written_bytes = hdfsWrite(fs, writeFile, (void*)buffer2.c_str(), buffer2.length()); 
+  vcl_string buffer2 = "Hello, World!";
+  tSize num_written_bytes = hdfsWrite(fs, writeFile, (void*)buffer2.c_str(), buffer2.length());
 
   TEST("flush buffer into the file ", !hdfsFlush(fs, writeFile), true);
-  hdfsCloseFile(fs, writeFile); 
+  hdfsCloseFile(fs, writeFile);
 
   int val = hdfsExists(fs, writePath.c_str());
   TEST("check existence of test file ", val==0, true);
