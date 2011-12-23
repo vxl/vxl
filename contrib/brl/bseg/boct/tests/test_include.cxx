623ffb674688f9d97e5b0626362dfab2fd49e99b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-12-23 15:52:36 -0500

    added boct/util
    SVN-Revision: 33980

--- contrib/brl/bseg/boct/tests/test_include.cxx
+++ contrib/brl/bseg/boct/tests/test_include.cxx
@@ -5,4 +5,7 @@
 #include <boct/boct_tree_cell.h>
 #include <boct/boct_tree_cell_reader.h>
 
+#include <boct/util/boct_clone_tree.h>
+#include <boct/util/boct_construct_tree.h>
+
 int main() { return 0; }
