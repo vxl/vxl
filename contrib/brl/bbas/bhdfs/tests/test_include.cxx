9101a9ac463515892cc5e0d0f1dd3da133d76481
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-12-23 15:17:35 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 33977

--- contrib/brl/bbas/bhdfs/tests/test_include.cxx
+++ contrib/brl/bbas/bhdfs/tests/test_include.cxx
@@ -3,7 +3,5 @@
 #include <bhdfs/bhdfs_vil_load.h>
 #include <bhdfs/bhdfs_vil_save.h>
 #include <bhdfs/bhdfs_vil_stream.h>
-#include <bhdfs/pro/bhdfs_processes.h>
-#include <bhdfs/pro/bhdfs_register.h>
 
 int main() { return 0; }
