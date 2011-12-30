7a5e4e06b3abb13fb99746e704c62b9bc31b2405
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2011-12-30 05:44:28 -0500

    avoid compiler warning (comparison signed/unsigned)
    SVN-Revision: 34025

--- contrib/brl/bpro/core/vpgl_pro/processes/vpgl_generate_3d_point_from_cams_process.cxx
+++ contrib/brl/bpro/core/vpgl_pro/processes/vpgl_generate_3d_point_from_cams_process.cxx
@@ -1,18 +1,17 @@
-// This is brl/bpro/core/vpgl_pro/processes/vpgl_generate_3d_point_process_from_depth_process.cxx
+// This is brl/bpro/core/vpgl_pro/processes/vpgl_generate_3d_point_from_cams_process.cxx
 #include <bprb/bprb_func_process.h>
+//:
+// \file
 #include <brdb/brdb_value.h>
 #include <brdb/brdb_selection.h>
 #include <bprb/bprb_batch_process_manager.h>
 #include <bprb/bprb_parameters.h>
 #include <bprb/bprb_macros.h>
-//:
-// \file
 
 #include <vcl_iostream.h>
 #include <vcl_fstream.h>
 #include <vpgl/vpgl_camera.h>
 #include <vpgl/vpgl_perspective_camera.h>
-#include <vpgl/vpgl_calibration_matrix.h>
 #include <vpgl/algo/vpgl_triangulate_points.h>
 #include <vsl/vsl_binary_io.h>
 #include <bpro/core/bbas_pro/bbas_1d_array_int.h>
@@ -23,57 +22,57 @@ namespace vpgl_generate_3d_point_from_cams_process_globals
     const unsigned n_inputs_ = 2;
     const unsigned n_outputs_ = 3;
 }
+
 //: Init function
 bool vpgl_generate_3d_point_from_cams_process_cons(bprb_func_process& pro)
 {
-    using namespace vpgl_generate_3d_point_from_cams_process_globals;
+  using namespace vpgl_generate_3d_point_from_cams_process_globals;
 
-    //process takes 1 input
-    vcl_vector<vcl_string> input_types_(n_inputs_);
-    input_types_[0] = "bbas_1d_array_unsigned_sptr";
-    input_types_[1] = "bbas_1d_array_int_sptr"; // points
+  //process takes 2 inputs
+  vcl_vector<vcl_string> input_types_(n_inputs_);
+  input_types_[0] = "bbas_1d_array_unsigned_sptr";
+  input_types_[1] = "bbas_1d_array_int_sptr"; // points
 
-    //input_types_[1] = "float";
-    //input_types_[2] = "float";
-    //input_types_[3] = "vpgl_camera_double_sptr";
-    //input_types_[4] = "float";
-    //input_types_[5] = "float";
-    
-    // process has 1 output:
-    // output[0]: scene sptr
-    vcl_vector<vcl_string>  output_types_(n_outputs_);
-    output_types_[0] = "float"; //X
-    output_types_[1] = "float"; //Y
-    output_types_[2] = "float"; //Z
+  //input_types_[1] = "float";
+  //input_types_[2] = "float";
+  //input_types_[3] = "vpgl_camera_double_sptr";
+  //input_types_[4] = "float";
+  //input_types_[5] = "float";
 
-    bool good = pro.set_input_types(input_types_) &&
-                pro.set_output_types(output_types_);
-    return good;
+  // process has 3 outputs
+  vcl_vector<vcl_string>  output_types_(n_outputs_);
+  output_types_[0] = "float"; //X
+  output_types_[1] = "float"; //Y
+  output_types_[2] = "float"; //Z
+
+  bool good = pro.set_input_types(input_types_) &&
+              pro.set_output_types(output_types_);
+  return good;
 }
 
 //: Execute the process
 bool vpgl_generate_3d_point_from_cams_process(bprb_func_process& pro)
 {
-   // Sanity check
+  // Sanity check
   if (!pro.verify_inputs()) {
     vcl_cerr << "vpgl_generate_3d_point_from_cams_process: Invalid inputs\n";
     return false;
   }
-  
+
   // get the inputs
   unsigned i=0;
   bbas_1d_array_unsigned_sptr cam_ids_ptr = pro.get_input<bbas_1d_array_unsigned_sptr>(i++);
   bbas_1d_array_int_sptr      imgPoints   = pro.get_input<bbas_1d_array_int_sptr>(i++);
 
   //vector of cams, vector of points
-  vcl_vector<vpgl_perspective_camera<double> > cams; 
-  vbl_array_1d<unsigned>& cam_ids = cam_ids_ptr->data_array; 
-  for(int i=0; i<cam_ids.size(); ++i) {
-    unsigned cam_id = cam_ids[i]; 
+  vcl_vector<vpgl_perspective_camera<double> > cams;
+  vbl_array_1d<unsigned>& cam_ids = cam_ids_ptr->data_array;
+  for (unsigned int i=0; i<cam_ids.size(); ++i) {
+    unsigned cam_id = cam_ids[i];
     brdb_query_aptr Q = brdb_query_comp_new("id", brdb_query::EQ, cam_id);
     brdb_selection_sptr S = DATABASE->select("vpgl_camera_double_sptr_data", Q);
     if (S->size()!=1) {
-      vcl_cout << "in vpgl_generate_3d_from_cams- bad input value\n";
+      vcl_cout << "in vpgl_generate_3d_point_from_cams_process - bad input value\n";
       return false;
     }
     brdb_value_sptr value;
@@ -88,10 +87,10 @@ bool vpgl_generate_3d_point_from_cams_process(bprb_func_process& pro)
 
   //gather image points
   vcl_vector<vgl_point_2d<double> > points;
-  vbl_array_1d<int>& pts = imgPoints->data_array; 
-  for(int i=0; i<pts.size(); i+=2)
+  vbl_array_1d<int>& pts = imgPoints->data_array;
+  for (unsigned int i=0; i<pts.size(); i+=2)
     points.push_back( vgl_point_2d<double>(pts[i], pts[i+1]) );
-  
+
   //: Calculates the best 3D point corresponding to a set of 2D camera points.
   // Takes in a list of points and cameras, and finds the
   // least-squared solution to the intersection of the rays
@@ -101,7 +100,7 @@ bool vpgl_generate_3d_point_from_cams_process(bprb_func_process& pro)
   vgl_point_3d<double> point_3d;
   double error = vpgl_triangulate_points::triangulate( points, cams, point_3d );
   vcl_cout<<"RAYS INTERSECT AT POINT: "<<point_3d<< " with error "<<error<<vcl_endl;
-  
+
   pro.set_output_val<float>(0, (float)point_3d.x());
   pro.set_output_val<float>(1, (float)point_3d.y());
   pro.set_output_val<float>(2, (float)point_3d.z());
