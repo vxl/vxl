c30735c5c93b6f827b22fc32d6d00a49f2ffeada
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-01-08 04:59:34 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 34066

--- contrib/brl/bseg/boxm2/ocl/algo/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/ocl/algo/tests/test_include.cxx
@@ -3,6 +3,7 @@
 #include <boxm2/ocl/algo/boxm2_ocl_cone_update_function.h>
 #include <boxm2/ocl/algo/boxm2_ocl_paint_batch.h>
 #include <boxm2/ocl/algo/boxm2_ocl_paint_online.h>
+#include <boxm2/ocl/algo/boxm2_ocl_refine.h>
 #include <boxm2/ocl/algo/boxm2_ocl_render_expected_image_function.h>
 #include <boxm2/ocl/algo/boxm2_ocl_update_color.h>
 
