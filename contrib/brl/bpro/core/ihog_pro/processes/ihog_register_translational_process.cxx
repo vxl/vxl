035a0089925cbecfcffea8185794c81bc8af0858
Daniel Crispell <dcrispell@users.sourceforge.net>
2012-01-23 13:55:05 -0500

    added second mask to registration process
    SVN-Revision: 34123

--- contrib/brl/bpro/core/ihog_pro/processes/ihog_register_translational_process.cxx
+++ contrib/brl/bpro/core/ihog_pro/processes/ihog_register_translational_process.cxx
@@ -6,6 +6,7 @@
 
 #include <vcl_iostream.h>
 #include <ihog/ihog_minimizer.h>
+#include <ihog/ihog_world_roi.h>
 #include <vil/vil_convert.h>
 
 
@@ -18,6 +19,7 @@ bool ihog_register_translational_process_cons(bprb_func_process& pro)
   //  2) int        radius for an exhaustive search at the beginning, no exhaustive search if passed as 0
   bool ok=false;
   vcl_vector<vcl_string> input_types;
+  input_types.push_back("vil_image_view_base_sptr");
   input_types.push_back("vil_image_view_base_sptr");
   input_types.push_back("vil_image_view_base_sptr");
   input_types.push_back("int");   
@@ -37,14 +39,15 @@ bool ihog_register_translational_process_cons(bprb_func_process& pro)
 //: Execute the process
 bool ihog_register_translational_process(bprb_func_process& pro)
 {
-  if (pro.n_inputs()!= 3) {
-    vcl_cout << "ihog_register_translational_process: The input number should be 3" << vcl_endl;
+  if (pro.n_inputs()!= pro.input_types().size()) {
+    vcl_cout << "ihog_register_translational_process: The input number should be " << pro.input_types().size() << vcl_endl;
     return false;
   }
   // get the inputs
   vil_image_view<vxl_byte> img_0(pro.get_input<vil_image_view_base_sptr>(0));
-  vil_image_view<vxl_byte> img_1(pro.get_input<vil_image_view_base_sptr>(1));
-  int radius = pro.get_input<int>(2);
+  vil_image_view<vxl_byte> img_0_mask(pro.get_input<vil_image_view_base_sptr>(1));
+  vil_image_view<vxl_byte> img_1(pro.get_input<vil_image_view_base_sptr>(2));
+  int radius = pro.get_input<int>(3);
 
   unsigned ni = img_0.ni(), nj = img_0.nj();
   vil_image_view<float> img0(ni,nj);
@@ -52,6 +55,10 @@ bool ihog_register_translational_process(bprb_func_process& pro)
   
   vil_image_view<float> img1(img_1.ni(),img_1.nj());
   vil_convert_cast(img_1, img1);
+
+  vil_image_view<float> mask0(img_0_mask.ni(), img_0_mask.nj());
+  vil_convert_stretch_range_limited(img_0_mask, mask0, vxl_byte(0), vxl_byte(255), 0.0f, 1.0f);
+
   vil_image_view<float> mask1(img_1.ni(),img_1.nj());
   mask1.fill(1.0f);
 
@@ -64,8 +71,9 @@ bool ihog_register_translational_process(bprb_func_process& pro)
   init_xform.set_translation_only(0,0);
   ihog_image<float> from_img(img0, init_xform);
   ihog_image<float> to_img(img1, ihog_transform_2d());
-  ihog_image<float> mask_img(mask1, ihog_transform_2d());
-  ihog_minimizer minimizer(from_img, to_img, mask_img, roi, false);
+  ihog_image<float> mask1_img(mask1, ihog_transform_2d());
+  ihog_image<float> mask0_img(mask0, ihog_transform_2d());
+  ihog_minimizer minimizer(from_img, to_img, mask0_img, mask1_img, roi);
   
   minimizer.minimize_exhaustive_minfo(radius, init_xform);
   double error = minimizer.get_end_error();
@@ -77,6 +85,7 @@ bool ihog_register_translational_process(bprb_func_process& pro)
   
   //: sanity check
   if (vcl_abs(p0.x()) > radius || vcl_abs(p0.y()) > radius) {
+    vcl_cout << "FAILED SANITY CHECK: TX = " << p0.x() << " TY = " << p0.y() << vcl_endl;
     pro.set_output_val<double>(0, 0.0);
     pro.set_output_val<double>(1, 0.0);
   } else {
