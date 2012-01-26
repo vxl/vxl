5718cbf0cf88a4a357c1d1355c2dea09c746808c
Daniel Crispell <dcrispell@users.sourceforge.net>
2012-01-26 14:30:48 -0500

    removed deleted processes, added some new ones to adaptors
    SVN-Revision: 34163

--- contrib/brl/bseg/boxm2/pyscripts/bbas_adaptor.py
+++ contrib/brl/bseg/boxm2/pyscripts/bbas_adaptor.py
@@ -38,6 +38,21 @@ def sun_angles(image_path):
   boxm2_batch.remove_data(id)
   return(sun_az, sun_el)
 
+def sun_position(image_path):
+  boxm2_batch.init_process('bbasSunAnglesProcess')
+  boxm2_batch.set_input_string(0,image_path)
+  boxm2_batch.run_process()
+  (id,type) = boxm2_batch.commit_output(0)
+  sun_el = boxm2_batch.get_output_float(id)
+  boxm2_batch.remove_data(id)
+  (id,type) = boxm2_batch.commit_output(1)
+  sun_az = boxm2_batch.get_output_float(id)
+  boxm2_batch.remove_data(id)
+  (id,type) = boxm2_batch.commit_output(2)
+  sun_dist = boxm2_batch.get_output_float(id)
+  boxm2_batch.remove_data(id)  
+  return(sun_az, sun_el, sun_dist)
+
 def camera_angles(camera, x,y,z):
   boxm2_batch.init_process('bbasCameraAnglesProcess')
   boxm2_batch.set_input_from_db(0,camera)
@@ -51,47 +66,40 @@ def camera_angles(camera, x,y,z):
   cam_el = boxm2_batch.get_output_float(id)
   return (cam_az, cam_el)
 
-def batch_normalize(img_list, cam_list, nitf_list, x_range, y_range, DEM_fname, geoid_height, output_fname):
-    boxm2_batch.init_process('bbasBatchNormalizeProcess')
-    boxm2_batch.set_input_string(0,img_list)
-    boxm2_batch.set_input_string(1,cam_list)
-    boxm2_batch.set_input_string(2,nitf_list)
-    boxm2_batch.set_input_float(3,x_range[0])
-    boxm2_batch.set_input_float(4,y_range[0])
-    boxm2_batch.set_input_float(5,x_range[1])
-    boxm2_batch.set_input_float(6,y_range[1])
-    boxm2_batch.set_input_string(7,DEM_fname)
-    boxm2_batch.set_input_float(8,geoid_height)
-    boxm2_batch.set_input_string(9,output_fname)
-    boxm2_batch.run_process()
+# radiometrically normalize a Quickbird image (cropped) based on AbsCalFactor in IMD file
+def radiometrically_calibrate(cropped_image, imd_filename):
+  boxm2_batch.init_process("bradNITFAbsRadiometricCalibrationProcess")
+  boxm2_batch.set_input_from_db(0, cropped_image)
+  boxm2_batch.set_input_string(1, imd_filename)
+  boxm2_batch.run_process()
+  (id, type) = boxm2_batch.commit_output(0)
+  cropped_img_cal = dbvalue(id, type)
+  return cropped_img_cal
 
-def batch_estimate(img_list, cam_list, nitf_list, x_range, y_range, DEM_fname, geoid_height, output_fname):
-    boxm2_batch.init_process('bbasBatchEstimateProcess')
-    boxm2_batch.set_input_string(0,img_list)
-    boxm2_batch.set_input_string(1,cam_list)
-    boxm2_batch.set_input_string(2,nitf_list)
-    boxm2_batch.set_input_float(3,x_range[0])
-    boxm2_batch.set_input_float(4,y_range[0])
-    boxm2_batch.set_input_float(5,x_range[1])
-    boxm2_batch.set_input_float(6,y_range[1])
-    boxm2_batch.set_input_string(7,DEM_fname)
-    boxm2_batch.set_input_float(8,geoid_height)
-    boxm2_batch.set_input_string(9,output_fname)
-    boxm2_batch.run_process()
+def estimate_radiance_values(image, sun_el, sun_dist, sensor_el, solar_irrad=None, downwelled_irrad=None, optical_depth=None):
+  boxm2_batch.init_process("bradEstimateRadianceValuesProcess")
+  boxm2_batch.set_input_from_db(0,image)
+  boxm2_batch.set_input_float(1,sun_el)
+  boxm2_batch.set_input_float(2,sun_dist)
+  boxm2_batch.set_input_float(3,sensor_el)
+  if solar_irrad != None:
+     boxm2_batch.set_input_float(4,solar_irrad)
+  if downwelled_irrad != None:
+     boxm2_batch.set_input_float(5,downwelled_irrad)
+  if optical_depth != None:
+     boxm2_batch.set_input_float(6, optical_depth)
+  boxm2_batch.run_process()
+  (id,type) = boxm2_batch.commit_output(0)
+  airlight = boxm2_batch.get_output_float(id)
+  boxm2_batch.remove_data(id)
+  (id,type) = boxm2_batch.commit_output(1)
+  ref_horizontal = boxm2_batch.get_output_float(id)
+  boxm2_batch.remove_data(id)
+  (id,type) = boxm2_batch.commit_output(2)
+  ref_sun_facing = boxm2_batch.get_output_float(id)
+  boxm2_batch.remove_data(id)
+  return (airlight, ref_horizontal, ref_sun_facing)
 
-def batch_estimate_probs(img_list, cam_list, nitf_list, x_range, y_range, DEM_fname, geoid_height, output_fname):
-    boxm2_batch.init_process('bbasBatchEstimateProbsProcess')
-    boxm2_batch.set_input_string(0,img_list)
-    boxm2_batch.set_input_string(1,cam_list)
-    boxm2_batch.set_input_string(2,nitf_list)
-    boxm2_batch.set_input_float(3,x_range[0])
-    boxm2_batch.set_input_float(4,y_range[0])
-    boxm2_batch.set_input_float(5,x_range[1])
-    boxm2_batch.set_input_float(6,y_range[1])
-    boxm2_batch.set_input_string(7,DEM_fname)
-    boxm2_batch.set_input_float(8,geoid_height)
-    boxm2_batch.set_input_string(9,output_fname)
-    boxm2_batch.run_process()
 
 #Removes elements from brdb (list of elements, or just one)
 def remove_from_db(dbvals) :
--- contrib/brl/bseg/boxm2/pyscripts/vil_adaptor.py
+++ contrib/brl/bseg/boxm2/pyscripts/vil_adaptor.py
@@ -177,3 +177,15 @@ def threshold_image(img, value, threshold_above=True):
     mask = dbvalue(id,type)
     return mask
 
+def stretch_image(img, min_value, max_value, output_type_str='float'):
+    boxm2_batch.init_process("vilStretchImageProcess")
+    boxm2_batch.set_input_from_db(0,img)
+    boxm2_batch.set_input_float(1,min_value)
+    boxm2_batch.set_input_float(2,max_value)
+    boxm2_batch.set_input_string(3,output_type_str)
+    boxm2_batch.run_process()
+    (id,type) = boxm2_batch.commit_output(0)
+    img_out = dbvalue(id,type)
+    return img_out
+
+
