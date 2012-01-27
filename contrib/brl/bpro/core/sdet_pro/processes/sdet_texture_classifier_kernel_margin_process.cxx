fc4c1f6b5c2c06ea1a007101f6986207ae38fdc4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-01-27 01:54:54 -0500

    corrected the "This is" line
    SVN-Revision: 34168

--- contrib/brl/bpro/core/sdet_pro/processes/sdet_texture_classifier_kernel_margin_process.cxx
+++ contrib/brl/bpro/core/sdet_pro/processes/sdet_texture_classifier_kernel_margin_process.cxx
@@ -1,27 +1,25 @@
-// This is brl/bpro/core/sdet_pro/kernel_margin_processes/sdet_texture_classifier_kernel_margin_process.cxx
-//:
-// \file
-
+// This is brl/bpro/core/sdet_pro/processes/sdet_texture_classifier_kernel_margin_process.cxx
 #include <bprb/bprb_func_process.h>
 #include <bprb/bprb_parameters.h>
 #include <brdb/brdb_value.h>
+//:
+// \file
+
 #include <sdet/sdet_texture_classifier.h>
 #include <sdet/sdet_texture_classifier_params.h>
 #include <vil/vil_image_view.h>
+
 //: initialize input and output types
 bool sdet_texture_classifier_kernel_margin_process_cons(bprb_func_process& pro)
 {
-  // kernel_margin_process takes 3 inputs:
+  // kernel_margin_process takes 1 input:
   vcl_vector<vcl_string> input_types;
   input_types.push_back("vcl_string"); //texton dictionary path
-  if (!pro.set_input_types(input_types))
-    return false;
-
   // kernel_margin_process has 1 output:
-  // output[0]: kernel margin
   vcl_vector<vcl_string> output_types;
-  output_types.push_back("unsigned");
-  return pro.set_output_types(output_types);
+  output_types.push_back("unsigned"); // output[0]: kernel margin
+  return pro.set_input_types(input_types)
+      && pro.set_output_types(output_types);
 }
 
 //: generates the edge map
@@ -29,12 +27,11 @@ bool sdet_texture_classifier_kernel_margin_process(bprb_func_process& pro)
 {
   if (!pro.verify_inputs())
   {
-    vcl_cout << pro.name() << "texture classifier kernel_margin_process inputs are not valid "<< vcl_endl;
+    vcl_cout << pro.name() << "texture classifier kernel_margin_process inputs are not valid"<< vcl_endl;
     return false;
   }
   // get inputs
   vcl_string texton_dict_path = pro.get_input<vcl_string>(0);
-
 
   sdet_texture_classifier_params tcp;
   sdet_texture_classifier tc(tcp);
