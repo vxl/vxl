fde7d19ae846e6786582a60e4054efc556a04395
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2012-01-28 15:03:27 -0500

    adding metadata reader and abs radiometric calibration processes
    SVN-Revision: 34193

--- contrib/brl/bseg/boxm2/pyscripts/brad_adaptor.py
+++ contrib/brl/bseg/boxm2/pyscripts/brad_adaptor.py
@@ -16,3 +16,22 @@ def estimate_atmospheric_parameters(image, metadata):
   atm_params = dbvalue(id,type)
   return atm_params
 
+# radiometrically normalize a sat image (cropped) based on its metadata
+def radiometrically_calibrate(cropped_image, meta):
+  boxm2_batch.init_process("bradNITFAbsRadiometricCalibrationProcess")
+  boxm2_batch.set_input_from_db(0, cropped_image)
+  boxm2_batch.set_input_from_db(1, meta)
+  boxm2_batch.run_process()
+  (id, type) = boxm2_batch.commit_output(0)
+  cropped_img_cal = dbvalue(id, type)
+  return cropped_img_cal
+
+# parse metadata of a sat image, reads sat image, nitf2 header and a metadata file in the same folder as the image if found
+def read_nitf_metadata(nitf_filename, imd_folder=""):
+  boxm2_batch.init_process("bradNITFReadMetadataProcess")
+  boxm2_batch.set_input_string(0, nitf_filename)  # requires full path and name
+  boxm2_batch.set_input_string(1, imd_folder)  # pass empty if meta is in img folder
+  boxm2_batch.run_process()
+  (id, type) = boxm2_batch.commit_output(0)
+  meta = dbvalue(id, type)
+  return meta
