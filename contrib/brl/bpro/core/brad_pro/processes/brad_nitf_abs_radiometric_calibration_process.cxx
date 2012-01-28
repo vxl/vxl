267a0528415f6105a849f99ff4838016c1fda7a7
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2012-01-28 14:25:14 -0500

    adding metadata reader and abs radiometric calibration processes
    SVN-Revision: 34183

--- contrib/brl/bpro/core/brad_pro/processes/brad_nitf_abs_radiometric_calibration_process.cxx
+++ contrib/brl/bpro/core/brad_pro/processes/brad_nitf_abs_radiometric_calibration_process.cxx
@@ -1,10 +1,10 @@
-//This is brl/bpro/core/bbas_pro/processes/brad_nitf_abs_radiometric_calibration_process.cxx
+//This is brl/bpro/core/brad_pro/processes/brad_nitf_abs_radiometric_calibration_process.cxx
 //:
 // \file
-//   DigitalGlobe products require an absolute radiometric calibration:
+//   Satellite images usually require an absolute radiometric calibration:
 //      http://www.digitalglobe.com/downloads/QuickBird_technote_raduse_v1.pdf
 //   In the output image, the pixel values are top-of-atmosphere  band-integrated  radiance values 
-//     with unit W/sr per m^2
+//     with unit W m^-2 sr^-1 um^-1
 //
 //
 
@@ -14,16 +14,18 @@
 #include <vcl_fstream.h>
 #include <bprb/bprb_func_process.h>
 
-#include <vul/vul_awk.h>
-#include <vul/vul_file.h>
+//#include <vul/vul_awk.h>
+//#include <vul/vul_file.h>
 #include <vil/vil_convert.h>
+
+#include <brad/brad_image_metadata.h>
 
 //: set input and output types
 bool brad_nitf_abs_radiometric_calibration_process_cons(bprb_func_process& pro)
 {
   vcl_vector<vcl_string> input_types;
   input_types.push_back("vil_image_view_base_sptr"); // cropped satellite image, 
-  input_types.push_back("vcl_string"); // This is the path to .imd file that accompanies nitf files
+  input_types.push_back("brad_image_metadata_sptr"); 
   if (!pro.set_input_types(input_types))
     return false;
 
@@ -43,43 +45,10 @@ bool brad_nitf_abs_radiometric_calibration_process(bprb_func_process& pro)
 
   //get the inputs
   vil_image_view_base_sptr img_sptr = pro.get_input<vil_image_view_base_sptr>(0);
-  vcl_string imd_file_path = pro.get_input<vcl_string>(1);
+  brad_image_metadata_sptr md = pro.get_input<brad_image_metadata_sptr>(1);
 
   vil_image_view<float> img = *vil_convert_cast(float(), img_sptr);
-
-  vcl_string prefix = vul_file::extension(imd_file_path);
-  if (prefix != ".IMD")
-  {
-    vcl_cout << "source image is not IMD file that accompany an NITF file\n";
-    return false;
-  }
-  vcl_ifstream ifs( imd_file_path.c_str() );
-  //: now parse the IMD file
-  vul_awk awk(ifs);
-  float absCalfact = 1.0f;
-  float effectiveBand = 1.0f;
-  for (; awk; ++awk)
-  {
-    vcl_stringstream linestr(awk.line());
-    vcl_string tag; 
-    linestr >> tag;
-    
-    if (tag.compare("absCalFactor") == 0) {
-      linestr >> tag;  // read =
-      linestr >> absCalfact;
-      continue;
-    }
-    if (tag.compare("effectiveBandwidth") == 0) {
-      linestr >> tag;  // read =
-      linestr >> effectiveBand;
-      continue;
-    }
-  }
-  vcl_cout << "abs: " << absCalfact << " eff: " << effectiveBand << vcl_endl;
-  //: now just scale the image with this factor
-  vil_math_scale_values(img, absCalfact);
-  float scale = 1.0f/effectiveBand;
-  vil_math_scale_values(img, scale);
+  vil_math_scale_and_offset_values(img, md->gain_, md->offset_);
 
   //output date time info
   pro.set_output_val<vil_image_view_base_sptr>(0, new vil_image_view<float>(img));
