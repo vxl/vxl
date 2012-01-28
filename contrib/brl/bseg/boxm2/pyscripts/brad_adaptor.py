22a8491d5ccb2d67d2a01c9acdf8b7cd169be637
Daniel Crispell <dcrispell@users.sourceforge.net>
2012-01-28 15:30:43 -0500

    added adaptor for new process
    SVN-Revision: 34198

--- contrib/brl/bseg/boxm2/pyscripts/brad_adaptor.py
+++ contrib/brl/bseg/boxm2/pyscripts/brad_adaptor.py
@@ -6,15 +6,15 @@ from boxm2_register import boxm2_batch, dbvalue
 #############################################################################
 
 
-# estimate atmospheric parameters
-def estimate_atmospheric_parameters(image, metadata):
-  boxm2_batch.init_process("bradEstimateAtmosphericParametersProcess")
-  boxm2_batch.set_input_from_db(0,image)
-  boxm2_batch.set_input_from_db(1,metadata)
+# parse metadata of a sat image, reads sat image, nitf2 header and a metadata file in the same folder as the image if found
+def read_nitf_metadata(nitf_filename, imd_folder=""):
+  boxm2_batch.init_process("bradNITFReadMetadataProcess")
+  boxm2_batch.set_input_string(0, nitf_filename)  # requires full path and name
+  boxm2_batch.set_input_string(1, imd_folder)  # pass empty if meta is in img folder
   boxm2_batch.run_process()
-  (id,type) = boxm2_batch.commit_output(0)
-  atm_params = dbvalue(id,type)
-  return atm_params
+  (id, type) = boxm2_batch.commit_output(0)
+  meta = dbvalue(id, type)
+  return meta
 
 # radiometrically normalize a sat image (cropped) based on its metadata
 def radiometrically_calibrate(cropped_image, meta):
@@ -26,12 +26,25 @@ def radiometrically_calibrate(cropped_image, meta):
   cropped_img_cal = dbvalue(id, type)
   return cropped_img_cal
 
-# parse metadata of a sat image, reads sat image, nitf2 header and a metadata file in the same folder as the image if found
-def read_nitf_metadata(nitf_filename, imd_folder=""):
-  boxm2_batch.init_process("bradNITFReadMetadataProcess")
-  boxm2_batch.set_input_string(0, nitf_filename)  # requires full path and name
-  boxm2_batch.set_input_string(1, imd_folder)  # pass empty if meta is in img folder
+# estimate atmospheric parameters
+def estimate_atmospheric_parameters(image, metadata):
+  boxm2_batch.init_process("bradEstimateAtmosphericParametersProcess")
+  boxm2_batch.set_input_from_db(0,image)
+  boxm2_batch.set_input_from_db(1,metadata)
   boxm2_batch.run_process()
-  (id, type) = boxm2_batch.commit_output(0)
-  meta = dbvalue(id, type)
-  return meta
+  (id,type) = boxm2_batch.commit_output(0)
+  atm_params = dbvalue(id,type)
+  return atm_params
+
+# convert radiance values to estimated reflectances
+def estimate_reflectance(image, metadata, atmospheric_params):
+  boxm2_batch.init_process("bradEstimateReflectanceProcess")
+  boxm2_batch.set_input_from_db(0,image)
+  boxm2_batch.set_input_from_db(1,metadata)
+  boxm2_batch.set_input_from_db(2,atmospheric_params)
+  boxm2_batch.run_process()
+  (id,type) = boxm2_batch.commit_output(0)
+  reflectance_img = dbvalue(id,type)
+  return reflectance_img
+
+
