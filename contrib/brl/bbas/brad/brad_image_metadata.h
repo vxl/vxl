0cbf1cf67a3eba056c04438afd83a8fbfc95efdf
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-01-31 01:24:06 -0500

    parse() is used in contrib/brl/bpro/core/brad_pro/processes/brad_nitf_read_metadata_process.cxx; so made it public
    SVN-Revision: 34224

--- contrib/brl/bbas/brad/brad_image_metadata.h
+++ contrib/brl/bbas/brad/brad_image_metadata.h
@@ -2,7 +2,7 @@
 //:
 // \file
 // \brief Collection of image metadata values needed for radiometric calculations
-// \author D. E. Crispell 
+// \author D. E. Crispell
 // \date January 27, 2012
 //
 //----------------------------------------------------------------------------
@@ -21,38 +21,37 @@ struct image_time {
 
 class brad_image_metadata : public vbl_ref_count
 {
-public:
-   //: Constructor extracts information from NITF header and vendor-specific metadata file
-   brad_image_metadata(vcl_string const& nitf_filename, vcl_string const& meta_folder = "");
-   //: Default constructor
-   brad_image_metadata() : gain_(1.0), offset_(0.0) {} 
+ public:
+  //: Constructor extracts information from NITF header and vendor-specific metadata file
+  brad_image_metadata(vcl_string const& nitf_filename, vcl_string const& meta_folder = "");
+  //: Default constructor
+  brad_image_metadata() : gain_(1.0), offset_(0.0) {}
 
-   // position of sun relative to imaged location 
-   double sun_elevation_; // degrees above horizon
-   double sun_azimuth_;   // degrees east of north
+  // position of sun relative to imaged location
+  double sun_elevation_; // degrees above horizon
+  double sun_azimuth_;   // degrees east of north
 
-   // position of image sensor relative to imaged location
-   double view_elevation_; // degrees above horizon
-   double view_azimuth_;   // degrees east of north
+  // position of image sensor relative to imaged location
+  double view_elevation_; // degrees above horizon
+  double view_azimuth_;   // degrees east of north
 
-   // gain and offset required to convert digital number (DN) to band-averaged radiance value
-   double gain_;           // units W m^-2 sr^-1 um^-1 DN^-1
-   double offset_;         // units W m^-2 sr^-1 um^-1
+  // gain and offset required to convert digital number (DN) to band-averaged radiance value
+  double gain_;           // units W m^-2 sr^-1 um^-1 DN^-1
+  double offset_;         // units W m^-2 sr^-1 um^-1
 
-   // band-averaged sun irradiance (includes term accounting for Earth-Sun distance)
-   double sun_irradiance_; // units W m^-2 
+  // band-averaged sun irradiance (includes term accounting for Earth-Sun distance)
+  double sun_irradiance_; // units W m^-2
 
-   image_time t_;
-   unsigned number_of_bits_;
-protected:
-   //: parse header in nitf image, assumes that metadata files are in the same folder with the image
-   //   if meta_folder is not empty, they are searched in that folder as well
-   bool parse(vcl_string const& nitf_filename, vcl_string const& meta_folder = "");
-   //: Parse Quickbird IMD file
-   bool parse_from_imd(vcl_string const& filename);
-   //: Parse GeoEye PVL file
-   bool parse_from_pvl(vcl_string const& filename);
-
+  image_time t_;
+  unsigned number_of_bits_;
+  //: parse header in nitf image, assumes that metadata files are in the same folder with the image
+  //  If meta_folder is not empty, they are searched in that folder as well
+  bool parse(vcl_string const& nitf_filename, vcl_string const& meta_folder = "");
+ protected:
+  //: Parse Quickbird IMD file
+  bool parse_from_imd(vcl_string const& filename);
+  //: Parse GeoEye PVL file
+  bool parse_from_pvl(vcl_string const& filename);
 };
 
 typedef vbl_smart_ptr<brad_image_metadata> brad_image_metadata_sptr;
@@ -64,4 +63,3 @@ vcl_ostream&  operator<<(vcl_ostream& s, brad_image_metadata const& md);
 vcl_istream&  operator>>(vcl_istream& s, brad_image_metadata& md);
 
 #endif
-
