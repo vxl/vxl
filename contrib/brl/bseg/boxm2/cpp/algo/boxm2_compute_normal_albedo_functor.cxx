e44c74e1abdfab82603703145e15664fdb44afe4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-02-04 06:37:27 -0500

    added missing file (with "dummy" implementation)
    SVN-Revision: 34261

---
+++ contrib/brl/bseg/boxm2/cpp/algo/boxm2_compute_normal_albedo_functor.cxx
@@ -0,0 +1,18 @@
+// This is brl/bseg/boxm2/cpp/algo/boxm2_compute_normal_albedo_functor.cxx
+#include "boxm2_compute_normal_albedo_functor.h"
+
+bool boxm2_compute_normal_albedo_functor::init_data(vcl_vector<brad_image_metadata> const& metadata,
+                                                    vcl_vector<brad_atmospheric_parameters> const& atm_params,
+                                                    boxm2_stream_cache_sptr str_cache,
+                                                    boxm2_data_base * alpha_data,
+                                                    boxm2_data_base * normal_albedo_model)
+{
+  // TODO - not yet implemented
+  return false;
+}
+
+bool boxm2_compute_normal_albedo_functor::process_cell(unsigned int index, bool is_leaf, float side_len)
+{
+  // TODO - not yet implemented
+  return false;
+}
