ceda7f4b81a337c5409fde621793e32dec984930
Daniel Crispell <dcrispell@users.sourceforge.net>
2012-02-06 12:24:28 -0500

    fixed vbl_io_smart_ptr linker error
    SVN-Revision: 34309

--- contrib/brl/bseg/sdet/Templates/vbl_smart_ptr+sdet_texture_classifier-.cxx
+++ contrib/brl/bseg/sdet/Templates/vbl_smart_ptr+sdet_texture_classifier-.cxx
@@ -1,4 +1,7 @@
 #include <sdet/sdet_texture_classifier.h>
 #include <vbl/vbl_smart_ptr.txx>
+#include <vbl/io/vbl_io_smart_ptr.txx>
 
 VBL_SMART_PTR_INSTANTIATE(sdet_texture_classifier);
+VBL_IO_SMART_PTR_INSTANTIATE(sdet_texture_classifier);
+
