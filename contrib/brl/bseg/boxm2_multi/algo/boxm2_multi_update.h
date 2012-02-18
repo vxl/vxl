423df9e3be5d1e736af94365e49d401a2fa0d7e9
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-02-18 03:08:25 -0500

    removed duplicate #include
    SVN-Revision: 34354

--- contrib/brl/bseg/boxm2_multi/algo/boxm2_multi_update.h
+++ contrib/brl/bseg/boxm2_multi/algo/boxm2_multi_update.h
@@ -5,13 +5,12 @@
 #include <boxm2_multi_cache.h>
 #include <boxm2/boxm2_scene.h>
 #include <boxm2/ocl/boxm2_opencl_cache.h>
-#include <vil/vil_image_view.h>
+#include <boxm2/ocl/boxm2_ocl_util.h>
 #include <vpgl/vpgl_perspective_camera.h>
 #include <bocl/bocl_device.h>
 #include <bocl/bocl_kernel.h>
 #include <bocl/bocl_manager.h>
-#include <boxm2/ocl/boxm2_ocl_util.h>
-#include <boxm2/ocl/boxm2_opencl_cache.h>
+#include <vil/vil_image_view.h>
 
 //: boxm2_multi_cache - example realization of abstract cache class
 class boxm2_multi_update
@@ -21,15 +20,12 @@ class boxm2_multi_update
 
     //multi render header
     static float update(boxm2_multi_cache& cache, vil_image_view<float>& img, vpgl_camera_double_sptr cam );
-
 };
-
 
 
 //: Helper class that stores factored out update objects - rays, queues, bit lookup
 class boxm2_multi_update_helper
 {
-
   public:
     boxm2_multi_update_helper(vcl_vector<cl_command_queue>& queues,
                               vcl_vector<bocl_mem_sptr>& ray_os,
@@ -39,7 +35,7 @@ class boxm2_multi_update_helper
                               vcl_vector<bocl_mem_sptr>& outputs,
                               vcl_vector<vcl_vector<boxm2_block_id> >& vis_orders,
                               vcl_vector<boxm2_opencl_cache*>& vis_caches,
-                              vcl_size_t maxBlocks) : 
+                              vcl_size_t maxBlocks) :
         queues_(queues),
         ray_os_(ray_os),
         ray_ds_(ray_ds),
@@ -48,16 +44,17 @@ class boxm2_multi_update_helper
         outputs_(outputs),
         vis_orders_(vis_orders),
         vis_caches_(vis_caches),
-        maxBlocks_(maxBlocks){} ;
+        maxBlocks_(maxBlocks)
+    {}
 
     //update command queues
     vcl_vector<cl_command_queue>& queues_;
-    
-    //ray trace vars 
-    vcl_vector<bocl_mem_sptr>&  img_dims_, outputs_, ray_ds_, ray_os_, lookups_; 
-    
+
+    //ray trace vars
+    vcl_vector<bocl_mem_sptr>&  img_dims_, outputs_, ray_ds_, ray_os_, lookups_;
+
     //visibility order for each dev
-    vcl_vector<vcl_vector<boxm2_block_id> >& vis_orders_; 
+    vcl_vector<vcl_vector<boxm2_block_id> >& vis_orders_;
     vcl_vector<boxm2_opencl_cache*>& vis_caches_;
     vcl_size_t maxBlocks_;
 };
