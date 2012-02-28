ce7d500b9dc5b9054e4c298d9c8647034bfacf7e
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2012-02-28 13:25:40 -0500

    adding rng to be shared by python processes
    SVN-Revision: 34405

--- contrib/brl/bbas/bsta/pro/bsta_register.cxx
+++ contrib/brl/bbas/bsta/pro/bsta_register.cxx
@@ -6,6 +6,7 @@
 
 #include <bsta/bsta_histogram_sptr.h>
 #include <bsta/algo/bsta_sigma_normalizer.h>
+#include <bsta/bsta_random_wrapper.h>
 
 #include "bsta_processes.h"
 
@@ -13,9 +14,11 @@ void bsta_register::register_datatype()
 {
   REGISTER_DATATYPE( bsta_histogram_sptr );
   REGISTER_DATATYPE( bsta_sigma_normalizer_sptr );
+  REGISTER_DATATYPE( bsta_random_wrapper_sptr );
 }
 
 void bsta_register::register_process()
 {
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, bsta_sigma_normalization_table_process, "bstaSigmaNormTableProcess");
+  REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, bsta_initialize_random_seed_process, "bstaInitializeRandomSeedProcess");
 }
