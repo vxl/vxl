9a49a697463a14763f023b76a2b0ca7287568588
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2012-02-28 13:23:15 -0500

    adding rng to be shared by python processes
    SVN-Revision: 34403

---
+++ contrib/brl/bbas/bsta/pro/Templates/brdb_value_t+bsta_random_wrapper_sptr-.cxx
@@ -0,0 +1,5 @@
+#include <brdb/brdb_value.txx>
+#include <bsta/bsta_random_wrapper.h>
+#include <vbl/io/vbl_io_smart_ptr.h>
+
+BRDB_VALUE_INSTANTIATE(bsta_random_wrapper_sptr, "bsta_random_wrapper_sptr");
