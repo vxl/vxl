695e63335879462c09dbfab0139a872a60b5b841
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-03-03 04:53:11 -0500

    removed unused #include
    SVN-Revision: 34442

--- contrib/brl/bpro/core/sdet_pro/processes/sdet_print_texton_dict_process.cxx
+++ contrib/brl/bpro/core/sdet_pro/processes/sdet_print_texton_dict_process.cxx
@@ -7,22 +7,19 @@
 #include <brdb/brdb_value.h>
 #include <sdet/sdet_texture_classifier.h>
 #include <sdet/sdet_texture_classifier_params.h>
-#include <vil/vil_image_view.h>
+
 //: initialize input and output types
 bool sdet_print_texton_dict_process_cons(bprb_func_process& pro)
 {
-  // process takes 3 inputs:
+  // process takes 3 inputs, no outputs:
   vcl_vector<vcl_string> input_types;
   input_types.push_back("sdet_texture_classifier_sptr"); //classifier
   input_types.push_back("vcl_string"); //texton dictionary
   input_types.push_back("vcl_string");  // print mode
   // "histograms" , "textons", "inter_probs", "inter_dist"
-  if (!pro.set_input_types(input_types))
-    return false;
-
-  // process has 0 outputs:
   vcl_vector<vcl_string> output_types;
-  return pro.set_output_types(output_types);
+  return pro.set_input_types(input_types)
+     &&  pro.set_output_types(output_types);
 }
 
 //: generates the edge map
@@ -30,36 +27,38 @@ bool sdet_print_texton_dict_process(bprb_func_process& pro)
 {
   if (!pro.verify_inputs())
   {
-    vcl_cout << pro.name() << "print texton dictionary process inputs are not valid "<< vcl_endl;
+    vcl_cout << pro.name() << "print texton dictionary process inputs are not valid"<< vcl_endl;
     return false;
   }
   // get inputs
-  sdet_texture_classifier_sptr tc_ptr = 
+  sdet_texture_classifier_sptr tc_ptr =
     pro.get_input<sdet_texture_classifier_sptr>(0);
-  sdet_texture_classifier_params* tcp = 
-	  static_cast<sdet_texture_classifier_params*>(tc_ptr.ptr());
+  sdet_texture_classifier_params* tcp =
+    static_cast<sdet_texture_classifier_params*>(tc_ptr.ptr());
   sdet_texture_classifier tc(*tcp);
-  
+
   vcl_string texton_dict_path = pro.get_input<vcl_string>(1);
   tc.load_dictionary(texton_dict_path);
 
   vcl_string print_mode = pro.get_input<vcl_string>(2);
-  if(print_mode == "histograms"){
+  if (print_mode == "histograms") {
     tc.print_category_histograms();
     return true;
   }
-  if(print_mode == "textons"){
+  else if (print_mode == "textons") {
     tc.print_dictionary();
     return true;
   }
-  if(print_mode == "inter_probs"){
+  else if (print_mode == "inter_probs") {
     tc.print_interclass_probs();
     return true;
   }
-  if(print_mode == "inter_dist"){
+  else if (print_mode == "inter_dist") {
     tc.print_distances();
     return true;
   }
-  vcl_cout << "unrecognized print mode\n";
-  return false;
+  else {
+    vcl_cout << "unrecognized print mode\n";
+    return false;
+  }
 }
