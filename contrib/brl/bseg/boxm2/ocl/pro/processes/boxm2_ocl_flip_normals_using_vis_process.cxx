d2ae317953a02e48e2cd5aa434eb3823927765e0
Ali Osman Ulusoy <aulusoy@users.sourceforge.net>
2012-03-07 17:44:56 -0500

    update.
    SVN-Revision: 34483

--- contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_flip_normals_using_vis_process.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_flip_normals_using_vis_process.cxx
@@ -48,6 +48,7 @@ namespace boxm2_ocl_flip_normals_using_vis_process_globals
     src_paths.push_back(source_dir + "scene_info.cl");
     src_paths.push_back(source_dir + "cell_utils.cl");
     src_paths.push_back(source_dir + "bit/bit_tree_library_functions.cl");
+    src_paths.push_back(source_dir + "basic/sort_vector.cl");
     src_paths.push_back(source_dir + "backproject.cl");
     src_paths.push_back(source_dir + "statistics_library_functions.cl");
     src_paths.push_back(source_dir + "ray_bundle_library_opt.cl");
