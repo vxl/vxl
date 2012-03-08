763c4b4e6f01aa4fdc1a5318f224ca133448f6bd
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-03-08 12:33:07 -0500

    removed duplicate #include <boxm2/boxm2_util.h>
    SVN-Revision: 34499

--- contrib/brl/bseg/boxm2_multi/exe/boxm2_multi_update.cxx
+++ contrib/brl/bseg/boxm2_multi/exe/boxm2_multi_update.cxx
@@ -1,13 +1,13 @@
 //:
-// \file 
+// \file
 // \brief  First attempt at multi gpu update
 // \author Andy Miller
 // \date 13-Nov-2011
+
 #include <boxm2_multi_cache.h>
 #include <algo/boxm2_multi_render.h>
 #include <algo/boxm2_multi_update.h>
 #include <algo/boxm2_multi_refine.h>
-#include <boxm2/boxm2_util.h>
 
 #include <vcl_where_root_dir.h>
 #include <vcl_algorithm.h>
@@ -39,16 +39,16 @@ int main(int argc,  char** argv)
 {
   //init vgui (should choose/determine toolkit)
   vul_arg<vcl_string> scene_file("-scene", "scene filename", "");
-  vul_arg<vcl_string> img_dir("-imgdir", "update image directory", "nvm_out/imgs/"); 
-  vul_arg<vcl_string> cam_dir("-camdir", "update camera directory", "nvm_out/cams_krt/"); 
-  vul_arg<unsigned>   num_updates("-num", "Number of updates", 10); 
+  vul_arg<vcl_string> img_dir("-imgdir", "update image directory", "nvm_out/imgs/");
+  vul_arg<vcl_string> cam_dir("-camdir", "update camera directory", "nvm_out/cams_krt/");
+  vul_arg<unsigned>   num_updates("-num", "Number of updates", 10);
   vul_arg<int>        inFrame("-frame", "Single frame to use", -1);
   vul_arg<int>        renderInt("-renderInt", "Interval to render progress", -1);
   vul_arg_parse(argc, argv);
-  
+
   //create scene
   boxm2_scene_sptr scene = new boxm2_scene(scene_file());
-  
+
   //make bocl manager (handles a lot of OpenCL stuff)
   bocl_manager_child_sptr mgr = bocl_manager_child::instance();
 
@@ -57,69 +57,69 @@ int main(int argc,  char** argv)
 
   //DEBUG - using just one gpu at first
   //make a multicache
-  for(int i=0; i<mgr->gpus_.size(); ++i)
+  for (int i=0; i<mgr->gpus_.size(); ++i)
     vcl_cout<<" GPU "<<i<<": "<<mgr->gpus_[i]<<vcl_endl;
-  //vcl_vector<bocl_device*> gpus; 
-  //gpus.push_back(mgr->gpus_[1]); 
-  boxm2_multi_cache mcache(scene, mgr->gpus_); 
-  vcl_cout<<"Multi Cache: \n"<<mcache.to_string()<<vcl_endl;
+  //vcl_vector<bocl_device*> gpus;
+  //gpus.push_back(mgr->gpus_[1]);
+  boxm2_multi_cache mcache(scene, mgr->gpus_);
+  vcl_cout<<"Multi Cache:\n"<<mcache.to_string()<<vcl_endl;
 
   //-- GET UPDATE IMG/CAMS ---
   vcl_vector<vcl_string> imgs = boxm2_util::images_from_directory( img_dir() );
   vcl_vector<vcl_string> cams = boxm2_util::camfiles_from_directory( cam_dir() );
-  
+
   //-----------------------------------------------------------
   //render setup
   //-----------------------------------------------------------
-  float mean_time=0.0f; 
-  int num_renders = 5; 
-  boxm2_multi_render renderer; 
-  int ni=1280, nj=720; 
+  float mean_time=0.0f;
+  int num_renders = 5;
+  boxm2_multi_render renderer;
+  int ni=1280, nj=720;
 
   //-----------------------------------------------------------
   //run update/renders
   //-----------------------------------------------------------
-  int numUpdates = vcl_min( (int) num_updates(), (int) imgs.size()); 
+  int numUpdates = vcl_min( (int) num_updates(), (int) imgs.size());
 
   //shuffle frames
   int* frames = new int[cams.size()];
-  for(int i=0; i<cams.size(); ++i) frames[i] = i;
+  for (int i=0; i<cams.size(); ++i) frames[i] = i;
   boxm2_util::random_permutation(frames, cams.size());
-  vul_timer t; 
+  vul_timer t;
   //update with each frame
-  for(int i=0; i<numUpdates; ++i) 
+  for (int i=0; i<numUpdates; ++i)
   {
     //update with random frame (or user chosen frame)
     int frame = (inFrame() >= 0) ? inFrame() : frames[i];
     vcl_cout<<"Update with frame: "<<frame<<", num "<<i<<" of "<<numUpdates<<vcl_endl;
-    vil_image_view_base_sptr inImg = boxm2_util::prepare_input_image(imgs[frame], true); 
+    vil_image_view_base_sptr inImg = boxm2_util::prepare_input_image(imgs[frame], true);
     vpgl_camera_double_sptr  inCam = boxm2_util::camera_from_file( cams[frame] );
-    vil_image_view<float>* inImgPtr = dynamic_cast<vil_image_view<float>* >(inImg.ptr()); 
-    boxm2_multi_update::update(mcache, *inImgPtr, inCam); 
+    vil_image_view<float>* inImgPtr = dynamic_cast<vil_image_view<float>* >(inImg.ptr());
+    boxm2_multi_update::update(mcache, *inImgPtr, inCam);
 
-    if(renderInt() > 0 && i%renderInt()==(renderInt()-1)) 
+    if (renderInt() > 0 && i%renderInt()==(renderInt()-1))
     {
       //create cam
       double currInc    = 45.0;
-      double currRadius = scene->bounding_box().height(); 
-      double currAz     = i*30.0f; 
+      double currRadius = scene->bounding_box().height();
+      double currAz     = i*30.0f;
       vpgl_perspective_camera<double>* pcam;
       pcam = boxm2_util::construct_camera(currInc, currAz, currRadius, ni, nj, scene->bounding_box(), false);
       vpgl_camera_double_sptr cam = new vpgl_perspective_camera<double>(*pcam);
-      
+
       //render/save image
-      vil_image_view<float> out(ni,nj); 
-      float rtime = renderer.render(mcache, out, cam); 
+      vil_image_view<float> out(ni,nj);
+      float rtime = renderer.render(mcache, out, cam);
       vcl_cout<<"Render "<<i<<" time: "<<rtime<<vcl_endl;
-      vcl_stringstream s; s<<"out_"<<i<<".tiff"; 
-      vil_save(out, s.str().c_str()); 
+      vcl_stringstream s; s<<"out_"<<i<<".tiff";
+      vil_save(out, s.str().c_str());
     }
   }
-  
+
   float totalTime = t.all();
   vcl_cout<<"-----------------------------\n"
-          <<" total update time: "<<totalTime/1000.0f<<" seconds \n"
-          <<" ave   update time: "<<totalTime/numUpdates/1000.0f<<" seconds \n"
+          <<" total update time: "<<totalTime/1000.0f<<" seconds\n"
+          <<" ave   update time: "<<totalTime/numUpdates/1000.0f<<" seconds\n"
           <<"-------------------------------"<<vcl_endl;
   //write cache
   boxm2_lru_cache::instance()->write_to_disk();
