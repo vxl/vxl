478f5bbe3665a440c4951b5e77f5e1c3cb54237d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-03-10 05:20:26 -0500

    removed unused #includes
    SVN-Revision: 34512

--- core/vpgl/algo/vpgl_camera_compute.h
+++ core/vpgl/algo/vpgl_camera_compute.h
@@ -9,28 +9,26 @@
 //
 // Should template this class.
 
-#include <vcl_vector.h>
-#include <vgl/vgl_fwd.h>
-#include <vnl/vnl_fwd.h>
-#include <vpgl/vpgl_calibration_matrix.h>
 #include <vpgl/vpgl_perspective_camera.h>
 #include <vpgl/vpgl_proj_camera.h>
 #include <vpgl/vpgl_affine_camera.h>
-#include <vpgl/vpgl_rational_camera.h>
-#include <vpgl/vpgl_local_rational_camera.h>
-#include <vpgl/vpgl_generic_camera.h>
-#include <vgl/algo/vgl_h_matrix_3d.h>
+#include <vcl_vector.h>
+#include <vgl/vgl_fwd.h>
+#include <vnl/vnl_fwd.h>
 
 //: Basic least squares solution for a general projective camera given corresponding world and image points.
 class vpgl_proj_camera_compute
 {
  public:
   //: Compute from two sets of corresponding points.
-  // Put the resulting camera into camera, return true if successful.
-  static
-    bool compute( const vcl_vector< vgl_homg_point_2d<double> >& image_pts,
-                  const vcl_vector< vgl_homg_point_3d<double> >& world_pts,
-                  vpgl_proj_camera<double>& camera );
+  // Put the resulting camera into \p camera
+  // \return true if successful.
+  static bool compute( const vcl_vector< vgl_homg_point_2d<double> >& image_pts,
+                       const vcl_vector< vgl_homg_point_3d<double> >& world_pts,
+                       vpgl_proj_camera<double>& camera );
+  //: Compute from two sets of corresponding points.
+  // Put the resulting camera into \p camera
+  // \return true if successful.
   static bool compute( const vcl_vector< vgl_point_2d<double> >& image_pts,
                        const vcl_vector< vgl_point_3d<double> >& world_pts,
                        vpgl_proj_camera<double>& camera );
@@ -45,7 +43,8 @@ class vpgl_affine_camera_compute
 {
  public:
   //: Compute from two sets of corresponding points.
-  // Put the resulting camera into camera, return true if successful.
+  // Put the resulting camera into \p camera
+  // \return true if successful.
   static bool compute( const vcl_vector< vgl_point_2d<double> >& image_pts,
                        const vcl_vector< vgl_point_3d<double> >& world_pts,
                        vpgl_affine_camera<double>& camera );
@@ -59,7 +58,8 @@ class vpgl_perspective_camera_compute
 {
  public:
   //: Compute from two sets of corresponding points.
-  // Put the resulting camera into \p camera, return true if successful.
+  // Put the resulting camera into \p camera
+  // \return true if successful.
   static bool compute( const vcl_vector< vgl_point_2d<double> >& image_pts,
                        const vcl_vector< vgl_point_3d<double> >& world_pts,
                        const vpgl_calibration_matrix<double>& K,
@@ -69,23 +69,23 @@ class vpgl_perspective_camera_compute
   //: Uses the direct linear transform algorithm described in "Multiple
   // View Geometry in Computer Vision" to find the projection matrix,
   // and extracts the parameters of the camera from this projection matrix.
-  // Requires: image_pts and world_pts are correspondences. image_pts is
-  //  the projected form, and world_pts is the unprojected form. There
-  //  need to be at least 6 points.
-  // Returns: true if successful. err is filled with the two-norm of the
-  //  projection error vector. camera is filled with the perspective
-  //  decomposition of the projection matrix
-  static bool compute_dlt (
-                       const vcl_vector< vgl_point_2d<double> >& image_pts,
-                       const vcl_vector< vgl_point_3d<double> >& world_pts,
-                       vpgl_perspective_camera<double> &camera,
-                       double &err);
+  // Requires: \p image_pts and \p world_pts are correspondences.
+  //  \p image_pts is the projected form, and \p world_pts is the unprojected form.
+  //  There need to be at least 6 points.
+  // \returns true if successful.
+  // \p err is filled with the two-norm of the projection error vector.
+  // \p camera is filled with the perspective decomposition of the projection matrix.
+  static bool compute_dlt ( const vcl_vector< vgl_point_2d<double> >& image_pts,
+                            const vcl_vector< vgl_point_3d<double> >& world_pts,
+                            vpgl_perspective_camera<double> &camera,
+                            double &err);
 
   //: Compute from two sets of corresponding 2D points (image and ground plane).
   // \param ground_pts are 2D points representing world points with Z=0
-  // The calibration matrix of \a camera is enforced
+  // The calibration matrix of \p camera is enforced
   // This computation is simpler than the general case above and only requires 4 points
-  // Put the resulting camera into \p camera, return true if successful.
+  // Put the resulting camera into \p camera
+  // \return true if successful.
   static bool compute( const vcl_vector< vgl_point_2d<double> >& image_pts,
                        const vcl_vector< vgl_point_2d<double> >& ground_pts,
                        vpgl_perspective_camera<double>& camera );
