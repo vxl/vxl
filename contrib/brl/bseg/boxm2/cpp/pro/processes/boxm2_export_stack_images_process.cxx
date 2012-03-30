1ba1c2ba5a105186599e5704ce130187ae4d791a
Vishal Jain <jainv@users.sourceforge.net>
2012-03-30 11:49:33 -0400

    added process to export stack of images.
    SVN-Revision: 34669

---
+++ contrib/brl/bseg/boxm2/cpp/pro/processes/boxm2_export_stack_images_process.cxx
@@ -0,0 +1,94 @@
+// This is brl/bseg/boxm2/pro/processes/boxm2_export_stack_images_process.cxx
+#include <bprb/bprb_func_process.h>
+//:
+// \file
+// \brief  A process for exporting a scene as a stack of  images
+//
+// \author Vishal Jain
+// \date Mar 28, 2012
+
+#include <vcl_fstream.h>
+#include <vcl_iomanip.h>
+#include <vul/vul_file.h>
+#include <boxm2/boxm2_scene.h>
+#include <boxm2/io/boxm2_cache.h>
+
+//vil includes
+#include <vil/vil_image_view.h>
+#include <vil/vil_new.h>
+#include <vil/vil_math.h>
+#include <vil/vil_save.h>
+#include <vil3d/vil3d_image_view.h>
+#include <vil3d/vil3d_slice.h>
+#include <vil3d/vil3d_save.h>
+
+#include <boxm2/cpp/algo/boxm2_export_stack_images_function.h>
+
+
+namespace boxm2_export_stack_images_process_globals
+{
+	const unsigned n_inputs_ = 3;
+	const unsigned n_outputs_ = 0;
+
+}
+
+bool boxm2_export_stack_images_process_cons(bprb_func_process& pro)
+{
+	using namespace boxm2_export_stack_images_process_globals;
+
+	//process takes 2 inputs
+	int i=0;
+	vcl_vector<vcl_string> input_types_(n_inputs_);
+	input_types_[i++] = "boxm2_scene_sptr";
+	input_types_[i++] = "boxm2_cache_sptr";
+	input_types_[i++] = "vcl_string";                //output dir of saved DICOM images
+
+	vcl_vector<vcl_string> output_types_(n_outputs_);
+	return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
+}
+
+bool boxm2_export_stack_images_process(bprb_func_process& pro)
+{
+	using namespace boxm2_export_stack_images_process_globals;
+	if ( pro.n_inputs() < n_inputs_ ) {
+		vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
+		return false;
+	}
+
+	//get the inputs
+	unsigned argIdx = 0;
+	boxm2_scene_sptr scene = pro.get_input<boxm2_scene_sptr>(argIdx++);
+	boxm2_cache_sptr cache = pro.get_input<boxm2_cache_sptr>(argIdx++);
+	vcl_string outdir = pro.get_input<vcl_string>(argIdx++);
+
+	//create the mesh directory
+	if (outdir != "") {
+		if (!vul_file::make_directory_path(outdir.c_str())) {
+			vcl_cout<<"Couldn't make directory path "<<outdir<<vcl_endl;
+			return false;
+		}
+	}
+
+	bool iscolor = false; 
+
+	if(scene->has_data_type("boxm2_gauss_rgb"))
+		iscolor = true ; 
+
+	vil3d_image_view<unsigned char> img3d;
+
+	if(!iscolor)
+		boxm2_export_stack_images_function::export_greyscale_stack_images(scene,cache,img3d);
+	else
+		boxm2_export_stack_images_function::export_color_stack_images(scene,cache,img3d);
+
+	for(unsigned k = 0 ;  k < img3d.nk() ;  k ++)
+	{
+		vil_image_view<unsigned char> img = vil3d_slice_ji(img3d,k);
+		vcl_stringstream ss;
+		ss<<outdir<<"/img_"<<vcl_setw(5) << vcl_setfill('0') << k<<".png";
+		vcl_cout<<"Filename : "<<ss.str()<<vcl_endl;
+		vil_save(img,ss.str().c_str());
+	}
+
+	return true;
+}
