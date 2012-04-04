5d88291da096b89594df95ad3ffc64bf93a1eeb5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-04-04 11:48:03 -0400

    avoid compiler warnings (unused variables, comparison signed/unsigned)
    SVN-Revision: 34704

--- contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_update_color_process.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_update_color_process.cxx
@@ -1,4 +1,5 @@
 // This is brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_update_color_process.cxx
+#include <bprb/bprb_func_process.h>
 //:
 // \file
 // \brief  A process for updating a color model
@@ -6,7 +7,6 @@
 // \author Vishal Jain
 // \date Mar 25, 2011
 
-#include <bprb/bprb_func_process.h>
 #include <boxm2/ocl/boxm2_opencl_cache.h>
 #include <boxm2/boxm2_scene.h>
 #include <boxm2/ocl/algo/boxm2_ocl_update_color.h>
@@ -18,27 +18,26 @@
 
 namespace boxm2_ocl_update_color_process_globals
 {
-  const unsigned n_inputs_  = 8;
-  const unsigned n_outputs_ = 0;
+  const unsigned int n_inputs_  = 8;
+  const unsigned int n_outputs_ = 0;
 }
 
 bool boxm2_ocl_update_color_process_cons(bprb_func_process& pro)
 {
   using namespace boxm2_ocl_update_color_process_globals;
 
-  //process takes 1 input
+  //process takes 8 inputs (of which the three last ones are optional):
   vcl_vector<vcl_string> input_types_(n_inputs_);
   input_types_[0] = "bocl_device_sptr";
   input_types_[1] = "boxm2_scene_sptr";
   input_types_[2] = "boxm2_opencl_cache_sptr";
-  input_types_[3] = "vpgl_camera_double_sptr";
-  input_types_[4] = "vil_image_view_base_sptr";
+  input_types_[3] = "vpgl_camera_double_sptr";      //input camera
+  input_types_[4] = "vil_image_view_base_sptr";     //input image
   input_types_[5] = "vcl_string";
   input_types_[6] = "vcl_string";
-  input_types_[7] = "bool";
+  input_types_[7] = "bool";                         //do_update_alpha/don't update alpha
 
-  // process has 1 output:
-  // output[0]: scene sptr
+  // process has no outputs
   vcl_vector<vcl_string>  output_types_(n_outputs_);
   bool good = pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
 
@@ -60,7 +59,7 @@ bool boxm2_ocl_update_color_process(bprb_func_process& pro)
   }
 
   //get the inputs: device, scene, opencl_cache, cam, img
-  unsigned argIdx = 0;
+  unsigned int argIdx = 0;
   bocl_device_sptr          device = pro.get_input<bocl_device_sptr>(argIdx++);
   boxm2_scene_sptr          scene = pro.get_input<boxm2_scene_sptr>(argIdx++);
   boxm2_opencl_cache_sptr   opencl_cache = pro.get_input<boxm2_opencl_cache_sptr>(argIdx++);
@@ -72,49 +71,45 @@ bool boxm2_ocl_update_color_process(bprb_func_process& pro)
 
   //make sure this image small enough (or else carve it into image pieces)
   const vcl_size_t MAX_PIXELS = 16777216;
-  if(img->ni()*img->nj() > MAX_PIXELS) {
-    int sni = RoundUp(img->ni(), 16);  
-    int snj = RoundUp(img->nj(), 16);
-    int numSegI = 1;
-    int numSegJ = 1;
-    while( sni*snj > MAX_PIXELS/4 ) {
-      sni /= 2; 
+  if (img->ni()*img->nj() > MAX_PIXELS) {
+    vcl_size_t sni = RoundUp(img->ni(), 16);
+    vcl_size_t snj = RoundUp(img->nj(), 16);
+    unsigned int numSegI = 1;
+    unsigned int numSegJ = 1;
+    while ( sni*snj*4 > MAX_PIXELS ) {
+      sni /= 2;
       snj /= 2;
-      numSegI++;
-      numSegJ++;
+      ++numSegI;
+      ++numSegJ;
     }
     sni = RoundUp(sni, 16);
     snj = RoundUp(snj, 16);
-    vil_image_resource_sptr ir = vil_new_image_resource_of_view(*img); 
+    vil_image_resource_sptr ir = vil_new_image_resource_of_view(*img);
 
     //run update for each image make sure to input i/j
-    for(int i=0; i<numSegI+1; ++i) {
-      for(int j=0; j<numSegJ+1; ++j) {
+    for (unsigned int i=0; i<=numSegI; ++i) {
+      for (unsigned int j=0; j<=numSegJ; ++j) {
         //make sure the view doesn't extend past the original image
-        vcl_size_t startI = (vcl_size_t) i * (vcl_size_t) sni; 
-        vcl_size_t startJ = (vcl_size_t) j * (vcl_size_t) snj;
+        vcl_size_t startI = (vcl_size_t) i * sni;
+        vcl_size_t startJ = (vcl_size_t) j * snj;
         vcl_size_t endI = vcl_min(startI + sni, (vcl_size_t) img->ni());
         vcl_size_t endJ = vcl_min(startJ + snj, (vcl_size_t) img->nj());
-        if(endI <= startI || endJ <= startJ)
+        if (endI <= startI || endJ <= startJ)
           break;
-        vcl_cout<<"Gettin patch: ("<<startI<<','<<startJ<<") -> ("<<endI<<','<<endJ<<")"<<vcl_endl;
+        vcl_cout<<"Getting patch: ("<<startI<<','<<startJ<<") -> ("<<endI<<','<<endJ<<')'<<vcl_endl;
         vil_image_view_base_sptr view = ir->get_copy_view(startI, endI-startI, startJ, endJ-startJ);
-        
+
         //run update
-        boxm2_ocl_update_color::update_color(scene, device, opencl_cache, 
-                                             cam, view, in_identifier, 
+        boxm2_ocl_update_color::update_color(scene, device, opencl_cache,
+                                             cam, view, in_identifier,
                                              mask_filename, updateAlpha,
                                              startI, startJ);
       }
     }
     return true;
   }
-
-  //call ocl update
-  boxm2_ocl_update_color::update_color(scene,device,opencl_cache,
-                                       cam,img,in_identifier,
-                                       mask_filename,updateAlpha);
-
-  //no outputs
-  return true;
+  else // otherwise just call ocl update
+    return boxm2_ocl_update_color::update_color(scene,device,opencl_cache,
+                                                cam,img,in_identifier,
+                                                mask_filename,updateAlpha);
 }
--- contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_update_process.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_update_process.cxx
@@ -16,13 +16,10 @@
 #include <boxm2/ocl/boxm2_ocl_util.h>
 #include <boxm2/boxm2_util.h>
 #include <vil/vil_image_view.h>
+#if 0
 #include <vil/vil_save.h>
+#endif
 #include <vil/vil_new.h>
-#include <vil/vil_load.h>
-#include <vil/vil_property.h>
-#include <vil/vil_blocked_image_resource.h>
-#include <vil/vil_block_cache.h>
-#include <vil/vil_crop.h>
 #include <vpl/vpl.h> // vpl_unlink()
 
 #include <boxm2/ocl/algo/boxm2_ocl_camera_converter.h>
@@ -32,22 +29,21 @@
 #include <brdb/brdb_value.h>
 
 //directory utility
-#include <vul/vul_timer.h>
 #include <vcl_where_root_dir.h>
 #include <bocl/bocl_device.h>
 #include <bocl/bocl_kernel.h>
 
 namespace boxm2_ocl_update_process_globals
 {
-  const unsigned n_inputs_  = 9;
-  const unsigned n_outputs_ = 0;
+  const unsigned int n_inputs_  = 9;
+  const unsigned int n_outputs_ = 0;
 }
 
 bool boxm2_ocl_update_process_cons(bprb_func_process& pro)
 {
   using namespace boxm2_ocl_update_process_globals;
 
-  //process takes 1 input
+  //process takes 9 inputs (of which the four last ones are optional):
   vcl_vector<vcl_string> input_types_(n_inputs_);
   input_types_[0] = "bocl_device_sptr";
   input_types_[1] = "boxm2_scene_sptr";
@@ -59,12 +55,11 @@ bool boxm2_ocl_update_process_cons(bprb_func_process& pro)
   input_types_[7] = "bool";                         //do_update_alpha/don't update alpha
   input_types_[8] = "float";                        //variance value? if 0.0 or less, then use variable variance
 
-  // process has 1 output:
-  // output[0]: scene sptr
+  // process has no outputs
   vcl_vector<vcl_string>  output_types_(n_outputs_);
   bool good = pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
 
-  // default 6 and 7 and 8 inputs
+  // default 5, 6 and 7 and 8 inputs
   brdb_value_sptr idx        = new brdb_value_t<vcl_string>("");
   brdb_value_sptr empty_mask = new brdb_value_t<vil_image_view_base_sptr>(new vil_image_view<unsigned char>(1,1));
   brdb_value_sptr up_alpha   = new brdb_value_t<bool>(true);  //by default update alpha
@@ -79,19 +74,17 @@ bool boxm2_ocl_update_process_cons(bprb_func_process& pro)
 bool boxm2_ocl_update_process(bprb_func_process& pro)
 {
   using namespace boxm2_ocl_update_process_globals;
+#if 0 // unused!
   vcl_size_t local_threads[2]={8,8};
   vcl_size_t global_threads[2]={8,8};
-
+#endif
   //sanity check inputs
   if ( pro.n_inputs() < n_inputs_ ) {
     vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
     return false;
   }
-  float transfer_time=0.0f;
-  float gpu_time=0.0f;
-
   //get the inputs
-  unsigned i = 0;
+  unsigned int i = 0;
   bocl_device_sptr         device       = pro.get_input<bocl_device_sptr>(i++);
   boxm2_scene_sptr         scene        = pro.get_input<boxm2_scene_sptr>(i++);
   boxm2_opencl_cache_sptr  opencl_cache = pro.get_input<boxm2_opencl_cache_sptr>(i++);
@@ -105,49 +98,47 @@ bool boxm2_ocl_update_process(bprb_func_process& pro)
   //TODO Factor this out to a utility function
   //make sure this image small enough (or else carve it into image pieces)
   const vcl_size_t MAX_PIXELS = 16777216;
-  if(img->ni()*img->nj() > MAX_PIXELS) {
-    int sni = RoundUp(img->ni(), 16);  
-    int snj = RoundUp(img->nj(), 16);
-    int numSegI = 1;
-    int numSegJ = 1;
-    while( sni*snj > MAX_PIXELS/2 ) {
-      sni /= 2; 
+  if (img->ni()*img->nj() > MAX_PIXELS) {
+    vcl_size_t sni = RoundUp(img->ni(), 16);
+    vcl_size_t snj = RoundUp(img->nj(), 16);
+    unsigned int numSegI = 1;
+    unsigned int numSegJ = 1;
+    while ( sni*snj*2 > MAX_PIXELS ) {
+      sni /= 2;
       snj /= 2;
-      numSegI++;
-      numSegJ++;
+      ++numSegI;
+      ++numSegJ;
     }
     sni = RoundUp(sni, 16);
     snj = RoundUp(snj, 16);
-    vil_image_resource_sptr ir = vil_new_image_resource_of_view(*img); 
+    vil_image_resource_sptr ir = vil_new_image_resource_of_view(*img);
 
     //run update for each image make sure to input i/j
-    for(int i=0; i<numSegI+1; ++i) {
-      for(int j=0; j<numSegJ+1; ++j) {
+    for (unsigned int i=0; i<=numSegI; ++i) {
+      for (unsigned int j=0; j<=numSegJ; ++j) {
         //make sure the view doesn't extend past the original image
-        vcl_size_t startI = (vcl_size_t) i * (vcl_size_t) sni; 
-        vcl_size_t startJ = (vcl_size_t) j * (vcl_size_t) snj;
+        vcl_size_t startI = (vcl_size_t) i * sni;
+        vcl_size_t startJ = (vcl_size_t) j * snj;
         vcl_size_t endI = vcl_min(startI + sni, (vcl_size_t) img->ni());
         vcl_size_t endJ = vcl_min(startJ + snj, (vcl_size_t) img->nj());
-        if(endI <= startI || endJ <= startJ)
+        if (endI <= startI || endJ <= startJ)
           break;
-        vcl_cout<<"Gettin patch: ("<<startI<<','<<startJ<<") -> ("<<endI<<','<<endJ<<")"<<vcl_endl;
+        vcl_cout<<"Getting patch: ("<<startI<<','<<startJ<<") -> ("<<endI<<','<<endJ<<')'<<vcl_endl;
         vil_image_view_base_sptr view = ir->get_copy_view(startI, endI-startI, startJ, endJ-startJ);
 #if 0
         //test saving
-        vcl_stringstream s; 
-        s<<"block_"<<startI<<"_"<<startJ<<".png";
-        vil_save(*view, s.str().c_str());        
+        vcl_stringstream s;
+        s<<"block_"<<startI<<'_'<<startJ<<".png";
+        vil_save(*view, s.str().c_str());
 #endif
         //run update
-        boxm2_ocl_update::update(scene, device, opencl_cache, cam, view, 
-                                 ident, mask_sptr, update_alpha, mog_var, 
+        boxm2_ocl_update::update(scene, device, opencl_cache, cam, view,
+                                 ident, mask_sptr, update_alpha, mog_var,
                                  startI, startJ);
       }
     }
     return true;
   }
-
-  //otherwise just run a normal update with one image
-  return boxm2_ocl_update::update(scene, device, opencl_cache, cam, img, ident, mask_sptr, update_alpha, mog_var);
-
+  else //otherwise just run a normal update with one image
+    return boxm2_ocl_update::update(scene, device, opencl_cache, cam, img, ident, mask_sptr, update_alpha, mog_var);
 }
