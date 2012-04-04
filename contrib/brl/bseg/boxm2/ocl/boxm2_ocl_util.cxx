3d8c2654bc018cedee991933f4e17e85bbd7942e
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2012-04-04 11:13:15 -0400

    minor editing of options to work on Mac OS X
    SVN-Revision: 34701

--- contrib/brl/bseg/boxm2/ocl/boxm2_ocl_util.cxx
+++ contrib/brl/bseg/boxm2/ocl/boxm2_ocl_util.cxx
@@ -349,11 +349,11 @@ void boxm2_ocl_util::get_render_transfer_function(vcl_vector<vcl_string> imgfile
 vcl_string boxm2_ocl_util::mog_options(vcl_string data_type)
 {
   if ( data_type == boxm2_data_traits<BOXM2_MOG3_GREY>::prefix() )
-    return " -D MOG_TYPE_8 ";
+    return "-D MOG_TYPE_8 ";
   else if ( data_type == boxm2_data_traits<BOXM2_MOG3_GREY_16>::prefix() )
-    return " -D MOG_TYPE_16 ";
+    return "-D MOG_TYPE_16 ";
   else if ( data_type == boxm2_data_traits<BOXM2_GAUSS_GREY>::prefix() )
-    return " -D GAUSS_TYPE_2 ";
+    return "-D GAUSS_TYPE_2 ";
   else
     return "";
 }
