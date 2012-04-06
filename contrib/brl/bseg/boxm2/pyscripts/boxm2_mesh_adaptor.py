192fee3af1c524f24470c7ec31d98a6de864b748
Vishal Jain <jainv@users.sourceforge.net>
2012-04-06 11:13:54 -0400

    added functions to paint mesh, get point cloud.
    SVN-Revision: 34723

--- contrib/brl/bseg/boxm2/pyscripts/boxm2_mesh_adaptor.py
+++ contrib/brl/bseg/boxm2/pyscripts/boxm2_mesh_adaptor.py
@@ -18,3 +18,43 @@ def gen_tex_mesh(mesh,in_img_dir, in_cam_dir,out_dir):
   boxm2_batch.set_input_string(2,in_cam_dir);
   boxm2_batch.set_input_string(3,out_dir);
   boxm2_batch.run_process();
+  
+def gen_point_cloud(scene,cache):
+	boxm2_batch.init_process("boxm2ExtractPointCloudProcess");
+	boxm2_batch.set_input_from_db(0,scene);
+	boxm2_batch.set_input_from_db(1,cache);
+	boxm2_batch.set_input_float(2,0); #prob threshold
+	boxm2_batch.run_process();
+	
+def gen_oriented_point_cloud(scene,cache,prob_t, norm_mag_t, filename):
+	boxm2_batch.init_process("boxm2CppComputeDerivativeProcesses");
+	boxm2_batch.set_input_from_db(0,scene);
+	boxm2_batch.set_input_from_db(1,cache);
+	boxm2_batch.set_input_float(2,prob_t);
+	boxm2_batch.set_input_float(3,norm_mag_t);
+
+	boxm2_batch.init_process("boxm2ExportOrientedPointCloudProcess");
+	boxm2_batch.set_input_from_db(0,scene);
+	boxm2_batch.set_input_from_db(1,cache);
+	boxm2_batch.set_input_string(2,filename);
+	boxm2_batch.run_process();
+
+def export_stack(scene,cache,outdir):
+	boxm2_batch.init_process("boxm2ExportStackImagesProcess");
+	boxm2_batch.set_input_from_db(0,scene);
+	boxm2_batch.set_input_from_db(1,cache);
+	boxm2_batch.set_input_string(2,outdir);
+	boxm2_batch.run_process();
+
+	boxm2_batch.init_process("boxm2ExportStackImagesProcess");
+	boxm2_batch.set_input_from_db(0,scene);
+	boxm2_batch.set_input_from_db(1,cache);
+	boxm2_batch.set_input_string(2,outdir);
+	boxm2_batch.run_process();
+def paint_mesh(scene,cache,in_file,out_file):
+	boxm2_batch.init_process("boxm2PaintMeshProcess");
+	boxm2_batch.set_input_from_db(0,scene);
+	boxm2_batch.set_input_from_db(1,cache);
+	boxm2_batch.set_input_string(2,in_file);
+	boxm2_batch.set_input_string(3,out_file);
+	boxm2_batch.run_process();
