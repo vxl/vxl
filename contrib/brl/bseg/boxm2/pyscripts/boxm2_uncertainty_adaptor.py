4c310cb0831482e8611dfe964a266b9f1a95924a
Vishal Jain <jainv@users.sourceforge.net>
2012-04-06 11:14:29 -0400

    added batch update alpha process.
    SVN-Revision: 34724

--- contrib/brl/bseg/boxm2/pyscripts/boxm2_uncertainty_adaptor.py
+++ contrib/brl/bseg/boxm2/pyscripts/boxm2_uncertainty_adaptor.py
@@ -172,3 +172,24 @@ class UncertainScene(boxm2_scene_adaptor):
     boxm2_batch.set_input_float(5,0.09);
     boxm2_batch.run_process();
     self.write_cache(True)
+	
+	
+  def batch_synoptic_alpha_update(self):
+    """ Create synoptic function (cubic function) at each voxel """
+    image_id_fname = self.model_dir + "/image_list.txt"
+    for i,img in enumerate(self.imgList):
+		if i > 0:
+			fd = open(image_id_fname,"w")
+			print >> fd, 1
+			print >> fd, "img_%05d"%i
+			fd.close()
+		
+			#open the stream cache, this is a read-only cache
+			boxm2_batch.init_process("boxm2OclSynopticUpdateAlphaProcess");
+			boxm2_batch.set_input_from_db(0,self.device);
+			boxm2_batch.set_input_from_db(1,self.scene);
+			boxm2_batch.set_input_from_db(2,self.opencl_cache);
+			boxm2_batch.set_input_unsigned(3,1);
+			boxm2_batch.set_input_string(4,image_id_fname);
+			boxm2_batch.run_process();
+    self.write_cache(True)
