c0ff61016d5ec883b797e60e540cffbe9beaea65
Isabel Restrepo <isabel_restrepo@users.sourceforge.net>
2012-04-19 12:33:59 -0400

    made support of kernel an input
    SVN-Revision: 34788

--- contrib/brl/bseg/bvpl/kernels/pro/processes/bvpl_create_generic_kernel_vector_process.cxx
+++ contrib/brl/bseg/bvpl/kernels/pro/processes/bvpl_create_generic_kernel_vector_process.cxx
@@ -18,7 +18,7 @@
 
 namespace bvpl_create_generic_kernel_vector_process_globals
 {
-  const unsigned n_inputs_ = 5;
+  const unsigned n_inputs_ = 8;
   const unsigned n_outputs_ = 1;
 }
 
@@ -31,14 +31,20 @@ bool bvpl_create_generic_kernel_vector_process_cons(bprb_func_process& pro)
   //input[0]: Kernel length
   //input[1]: Kernel width
   //input[2]: Kernel thickness
-  //input[3]: String : type of kernel 
+  //input[3]: Kernel support_x 
+  //input[4]: Kernel support_y
+  //input[5]: Kernel support_z
+  //input[6]: String : type of kernel 
   //input[4]: String : type of directions
   vcl_vector<vcl_string> input_types_(n_inputs_);
-  input_types_[0] = "unsigned";
-  input_types_[1] = "unsigned";
-  input_types_[2] = "unsigned";
-  input_types_[3] = "vcl_string";
-  input_types_[4] = "vcl_string";
+  input_types_[0] = "float";
+  input_types_[1] = "float";
+  input_types_[2] = "float";
+  input_types_[3] = "float";
+  input_types_[4] = "float";
+  input_types_[5] = "float";
+  input_types_[6] = "vcl_string";
+  input_types_[7] = "vcl_string";
 
 
   vcl_vector<vcl_string> output_types_(n_outputs_);
@@ -58,17 +64,20 @@ bool bvpl_create_generic_kernel_vector_process(bprb_func_process& pro)
   }
 
   //get inputs:
-  unsigned int dim_x = 5; dim_x = pro.get_input<unsigned int>(0);
-  unsigned int dim_y = 5; dim_y = pro.get_input<unsigned int>(1);
-  unsigned int dim_z = 5; dim_z = pro.get_input<unsigned int>(2);
-  vcl_string factory_name =pro.get_input<vcl_string>(3);
-  vcl_string dir_type =pro.get_input<vcl_string>(4);
+  float dim_x = 1.0f; dim_x = pro.get_input<float>(0);
+  float dim_y = 1.0f; dim_y = pro.get_input<float>(1);
+  float dim_z = 1.0f; dim_z = pro.get_input<float>(2);
+  float supp_x = 1.0f; supp_x = pro.get_input<float>(3);
+  float supp_y = 1.0f; supp_y = pro.get_input<float>(4);
+  float supp_z = 1.0f; supp_z = pro.get_input<float>(5);
+  vcl_string factory_name =pro.get_input<vcl_string>(6);
+  vcl_string dir_type =pro.get_input<vcl_string>(7);
 
 
   if (factory_name == "gauss_x") //first order derivative of gaussian
   {
     //Create the factory and get the vector of kernels
-    bvpl_gauss3d_x_kernel_factory factory(dim_x, dim_y, dim_z);  //in this case the values of dim_* correspond of sigma
+    bvpl_gauss3d_x_kernel_factory factory(dim_x, dim_y, dim_z, supp_x, supp_y, supp_z);  //in this case the values of dim_* correspond of sigma
     if (dir_type == "XYZ") {
       bvpl_create_directions_xyz dir;
       bvpl_kernel_vector_sptr kernels = factory.create_kernel_vector(dir);
