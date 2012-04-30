2b4c660c33bc7118fc54378b171bcb378220613e
Brandon Mayer <bmayer1@users.sourceforge.net>
2012-04-30 11:05:08 -0400

    Added another input to specify number of planes.
    SVN-Revision: 34835

--- contrib/brl/bpro/core/vil_pro/processes/vil_init_float_image_process.cxx
+++ contrib/brl/bpro/core/vil_pro/processes/vil_init_float_image_process.cxx
@@ -15,11 +15,18 @@ bool vil_init_float_image_process_cons(bprb_func_process& pro)
   //input
   bool ok=false;
   vcl_vector<vcl_string> input_types;
-  input_types.push_back("unsigned"); 
-  input_types.push_back("unsigned");
+  input_types.push_back("unsigned");//ni 
+  input_types.push_back("unsigned");//nj
+  input_types.push_back("unsigned");//np
   input_types.push_back("float");
   ok = pro.set_input_types(input_types);
   if (!ok) return ok;
+
+  //default values
+  pro.set_input(0, brdb_value_sptr(new brdb_value_t<unsigned>(1)));
+  pro.set_input(1, brdb_value_sptr(new brdb_value_t<unsigned>(1)));
+  pro.set_input(2, brdb_value_sptr(new brdb_value_t<unsigned>(1)));
+  pro.set_input(3, brdb_value_sptr(new brdb_value_t<float>(0.0)));
 
   //output
   vcl_vector<vcl_string> output_types;
@@ -40,11 +47,13 @@ bool vil_init_float_image_process(bprb_func_process& pro)
   }
 
   // get the inputs
-  unsigned int ni = pro.get_input<unsigned>(0);
-  unsigned int nj = pro.get_input<unsigned>(1);
-  float init_val = pro.get_input<float>(2);
+  unsigned i = 0;
+  unsigned int ni = pro.get_input<unsigned>(i++);
+  unsigned int nj = pro.get_input<unsigned>(i++);
+  unsigned int np = pro.get_input<unsigned>(i++);
+  float init_val = pro.get_input<float>(i++);
  
-  vil_image_view<float> *img = new vil_image_view<float>(ni,nj);
+  vil_image_view<float> *img = new vil_image_view<float>(ni,nj,np);
   img->fill(init_val);
   
   pro.set_output_val<vil_image_view_base_sptr>(0, vil_image_view_base_sptr(img));
