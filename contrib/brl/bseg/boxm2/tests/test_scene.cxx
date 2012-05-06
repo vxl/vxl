ff2ff5da6c47c78b950d78a14321298d01665769
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-05-06 06:28:20 -0400

    avoid compiler warning (uninitialized data.version_
    SVN-Revision: 34869

--- contrib/brl/bseg/boxm2/tests/test_scene.cxx
+++ contrib/brl/bseg/boxm2/tests/test_scene.cxx
@@ -10,46 +10,47 @@
 
 void test_scene()
 {
-  //test xml file 
+  //test xml file
   vcl_string test_dir  = testlib_root_dir()+ "/contrib/brl/bseg/boxm2/tests/";
-  vcl_string test_file = test_dir + "test.xml"; 
-  
+  vcl_string test_file = test_dir + "test.xml";
+
   //create block metadata
-  vcl_map<boxm2_block_id, boxm2_block_metadata> blocks; 
-  for(int i=0; i<2; i++) {
-    for(int j=0; j<2; j++) {
-      double big_block_side = 2.0; 
-      boxm2_block_id id(i,j,0); 
-      boxm2_block_metadata data; 
-      data.id_ = id; 
-      data.local_origin_ = vgl_point_3d<double>(big_block_side*i, big_block_side*j, 0.0); 
-      data.sub_block_dim_ = vgl_vector_3d<double>(.2, .2, .2); 
-      data.sub_block_num_ = vgl_vector_3d<unsigned>(10, 10, 10); 
-      data.init_level_ = 1; 
-      data.max_level_ = 4; 
-      data.max_mb_ = 400; 
-      data.p_init_ = .001;  
-      
+  vcl_map<boxm2_block_id, boxm2_block_metadata> blocks;
+  for (int i=0; i<2; i++) {
+    for (int j=0; j<2; j++) {
+      double big_block_side = 2.0;
+      boxm2_block_id id(i,j,0);
+      boxm2_block_metadata data;
+      data.id_ = id;
+      data.local_origin_ = vgl_point_3d<double>(big_block_side*i, big_block_side*j, 0.0);
+      data.sub_block_dim_ = vgl_vector_3d<double>(.2, .2, .2);
+      data.sub_block_num_ = vgl_vector_3d<unsigned>(10, 10, 10);
+      data.init_level_ = 1;
+      data.max_level_ = 4;
+      data.max_mb_ = 400;
+      data.p_init_ = .001;
+      data.version_ = 1;
+
       //push it into the map
-      blocks[id] = data; 
+      blocks[id] = data;
     }
   }
-  
+
   //create scene
-  boxm2_scene scene; 
-  scene.set_local_origin(vgl_point_3d<double>(0.0, 0.0, 0.0)); 
-  scene.set_rpc_origin(vgl_point_3d<double>(0.0, 0.0, 0.0));  
-  vpgl_lvcs lvcs; 
-  scene.set_lvcs(lvcs); 
-  scene.set_xml_path(test_file); 
-  scene.set_data_path(test_dir); 
-  scene.set_blocks(blocks); 
-  scene.save_scene(); 
-  
+  boxm2_scene scene;
+  scene.set_local_origin(vgl_point_3d<double>(0.0, 0.0, 0.0));
+  scene.set_rpc_origin(vgl_point_3d<double>(0.0, 0.0, 0.0));
+  vpgl_lvcs lvcs;
+  scene.set_lvcs(lvcs);
+  scene.set_xml_path(test_file);
+  scene.set_data_path(test_dir);
+  scene.set_blocks(blocks);
+  scene.save_scene();
+
   //create test scene
-  boxm2_scene test_scene_data(test_file); 
+  boxm2_scene test_scene_data(test_file);
   vcl_cout<<test_scene_data<<vcl_endl;
-  
+
   //delete file created
   vpl_unlink(test_file.c_str());
 }
