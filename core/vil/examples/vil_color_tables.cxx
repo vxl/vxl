b2c972f7f9103b45bfb2a99af7610ba7d1277d95
Glen Brooksby <brooksby@users.sourceforge.net>
2012-05-23 10:41:23 -0400

    Minor changes.
    SVN-Revision: 34939

--- core/vil/examples/vil_color_tables.cxx
+++ core/vil/examples/vil_color_tables.cxx
@@ -12,19 +12,17 @@ int main()
   vil_image_view<vil_rgb<vxl_byte> > cimg(2550,100,1);
 
   // Write an image for each of the 38 color maps
-  for (unsigned int c=0; c<38; c++) {
+  for (unsigned int c=0; c<39; c++) {
     for (unsigned int j=0; j<cimg.nj(); j++) {
       for (unsigned int i=0; i<cimg.ni(); i++) {
         int color_index = int(i*0.1+0.5);
         cimg(i,j) = color_value(c, color_index);
       }
     }
-    vcl_string fname;
-    vcl_stringstream s;
-    s << "color_table_" << c << ".tif";
-    fname = s.str();
+    char fname[25];
+    sprintf(fname,"color_table_%2.2d.png",c);
     vcl_cout << "Saving " << fname << vcl_endl;
-    vil_save(cimg, fname.c_str());
+    vil_save(cimg, fname);
   }
 
   return 0;
