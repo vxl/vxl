e3c5537bbd10044cf2135fe1128b14f363e3b5a2
Daniel Crispell <dcrispell@users.sourceforge.net>
2012-05-31 13:21:08 -0400

    added missing smart_ptr io template instantiations
    SVN-Revision: 34971

---
+++ core/vpgl/io/Templates/vbl_io_smart_ptr+vpgl_camera+float--.cxx
@@ -0,0 +1,6 @@
+#include <vpgl/vpgl_camera.h>
+#include <vpgl/io/vpgl_io_camera.h>
+#include <vbl/io/vbl_io_smart_ptr.txx>
+
+typedef vpgl_camera<float> vpgl_camera_float;
+VBL_IO_SMART_PTR_INSTANTIATE(vpgl_camera_float);
