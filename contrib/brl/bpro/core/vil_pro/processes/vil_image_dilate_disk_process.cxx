89b3960f60c965ab38e51b4a7d38b514e7453e39
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-06-04 12:38:30 -0400

    TABs replaced by spaces
    SVN-Revision: 34985

--- contrib/brl/bpro/core/vil_pro/processes/vil_image_dilate_disk_process.cxx
+++ contrib/brl/bpro/core/vil_pro/processes/vil_image_dilate_disk_process.cxx
@@ -2,11 +2,10 @@
 #include <bprb/bprb_func_process.h>
 //:
 // \file
-// \brief Dialate a binary image using a disk structural element
+// \brief Dilate a binary image using a disk structural element
 
 #include<vil/algo/vil_structuring_element.h>
 #include<vil/algo/vil_binary_dilate.h>
-#include<vil/vil_convert.h>
 #include<vil/vil_image_view.h>
 
 namespace vil_image_dilate_disk_process_globals
@@ -28,10 +27,10 @@ bool vil_image_dilate_disk_process_cons(bprb_func_process& pro)
 
     output_types_[0] = "vil_image_view_base_sptr";//output image
 
-    if( !pro.set_input_types(input_types_) )
-		return false;
+    if ( !pro.set_input_types(input_types_) )
+        return false;
 
-    if( !pro.set_output_types(output_types_) )
+    if ( !pro.set_output_types(output_types_) )
         return false;
 
     //default value
@@ -44,35 +43,35 @@ bool vil_image_dilate_disk_process(bprb_func_process& pro)
 {
     using namespace vil_image_dilate_disk_process_globals;
 
-    if( pro.n_inputs() < n_inputs_)
+    if ( pro.n_inputs() < n_inputs_)
     {
-        vcl_cerr << "In vil_image_dilate_disk_process(.): "
-                 << "Must provide at least 1 input" << vcl_endl;
+        vcl_cerr << "In vil_image_dilate_disk_process(.)\n"
+                 << "\tMust provide at least 1 input\n";
         return false;
     }
 
     unsigned i = 0;
-    vil_image_view_base_sptr imgPtr 
+    vil_image_view_base_sptr imgPtr
                  = pro.get_input<vil_image_view_base_sptr>(i++);
     float radius = pro.get_input<float>(i++);
 
-    if( imgPtr->pixel_format() != VIL_PIXEL_FORMAT_BOOL )
+    if ( imgPtr->pixel_format() != VIL_PIXEL_FORMAT_BOOL )
     {
-        vcl_cerr << "In vil_image_dilate_disk_process(.)" << vcl_endl
-                 << "\tMust provide boolean image." << vcl_endl;
+        vcl_cerr << "In vil_image_dilate_disk_process(.)\n"
+                 << "\tMust provide boolean image.\n";
         return false;
     }
 
-    if( imgPtr->nplanes() > 1 )
+    if ( imgPtr->nplanes() > 1 )
     {
-        vcl_cerr << "In vil_image_dilate_disk_process(.)" << vcl_endl
-                 << "\tInput image must be a single plane." << vcl_endl;
+        vcl_cerr << "In vil_image_dilate_disk_process(.)\n"
+                 << "\tInput image must be a single plane.\n";
         return false;
     }
     vil_image_view<bool> inView(imgPtr);
 
     vil_image_view<bool>* oView = new vil_image_view<bool>(imgPtr->ni(), imgPtr->nj(), 1);
-    
+
     vil_structuring_element disk;
     disk.set_to_disk(radius);
 
