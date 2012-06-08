4685eefd4599425f77f9b9e99f39323ed600fbd6
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2012-06-08 12:58:48 -0400

    adding binary save lvcs sptr
    SVN-Revision: 35002

--- core/vpgl/io/vpgl_io_lvcs.h
+++ core/vpgl/io/vpgl_io_lvcs.h
@@ -4,6 +4,7 @@
 // \file
 #include <vsl/vsl_binary_io.h>
 #include <vpgl/vpgl_lvcs.h>
+#include <vpgl/vpgl_lvcs_sptr.h>
 
 //: Binary save lvcs to stream
 void vsl_b_write(vsl_b_ostream & os, vpgl_lvcs const& lvcs);
@@ -13,6 +14,12 @@ void vsl_b_read(vsl_b_istream & is, vpgl_lvcs &lvcs);
 
 //: Print human readable summary of object to a stream
 void vsl_print_summary(vcl_ostream& os,const vpgl_lvcs & l);
+
+//: Binary save lvcs sptr to stream
+void vsl_b_write(vsl_b_ostream & os, vpgl_lvcs_sptr const& lvcs_sptr);
+
+//: Binary load lvcs from stream.
+void vsl_b_read(vsl_b_istream & is, vpgl_lvcs_sptr &lvcs);
 
 #endif
 
