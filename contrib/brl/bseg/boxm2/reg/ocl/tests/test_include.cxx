cb89796cfe78b597b78cd8ee6af0c99bbb0bc3c5
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-07-20 09:34:58 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 35239

--- contrib/brl/bseg/boxm2/reg/ocl/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/reg/ocl/tests/test_include.cxx
@@ -1,3 +1,4 @@
+#include <boxm2/reg/ocl/boxm2_ocl_reg_depth_map_to_vol.h>
 #include <boxm2/reg/ocl/boxm2_ocl_reg_minfo_func.h>
 #include <boxm2/reg/ocl/boxm2_ocl_reg_mutual_info.h>
 
--- contrib/brl/bseg/boxm2/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/tests/test_include.cxx
@@ -3,6 +3,7 @@
 #include <boxm2/boxm2_data.h>
 #include <boxm2/boxm2_data_base.h>
 #include <boxm2/boxm2_data_traits.h>
+#include <boxm2/boxm2_feature_vector.h>
 #include <boxm2/boxm2_normal_albedo_array.h>
 #include <boxm2/boxm2_scene.h>
 #include <boxm2/boxm2_scene_parser.h>
