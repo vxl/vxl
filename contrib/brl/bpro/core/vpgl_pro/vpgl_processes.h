c8dc26e9812164af8472ddc51313459352b10c14
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2012-07-26 09:06:48 -0400

    adding geo cam processes
    SVN-Revision: 35265

--- contrib/brl/bpro/core/vpgl_pro/vpgl_processes.h
+++ contrib/brl/bpro/core/vpgl_pro/vpgl_processes.h
@@ -46,5 +46,8 @@ DECLARE_FUNC_CONS(vpgl_create_perspective_camera_process3);
 DECLARE_FUNC_CONS(vpgl_create_perspective_camera_process4);
 DECLARE_FUNC_CONS(vpgl_perturb_uniform_persp_cam_orient_process);
 DECLARE_FUNC_CONS(bpgl_transform_perspective_cameras_process);
+DECLARE_FUNC_CONS(vpgl_geo_footprint_process);
+DECLARE_FUNC_CONS(vpgl_translate_geo_camera_process);
+DECLARE_FUNC_CONS(vpgl_convert_geo_camera_to_generic_process);
 
 #endif
