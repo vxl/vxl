3ece9c10c7b02865e5c6ec5f75dedd6b1bffe53e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-08-07 18:29:37 -0400

    non-void function should return a value
    SVN-Revision: 35368

--- contrib/brl/bbas/bwm/bwm_observer_geo_cam.cxx
+++ contrib/brl/bbas/bwm/bwm_observer_geo_cam.cxx
@@ -5,56 +5,51 @@
 #include <vsl/vsl_binary_io.h>
 #include <vgl/vgl_point_2d.h>
 #include <vgl/vgl_point_3d.h>
-#include <vgl/vgl_vector_3d.h>
 #include <vgl/vgl_plane_3d.h>
 #include <vgl/vgl_homg_point_2d.h>
 #include <vgl/vgl_homg_point_3d.h>
 #include <vgl/vgl_homg_line_3d_2_points.h>
 #include <vgl/algo/vgl_homg_operators_3d.h>
-#include <vpgl/vpgl_perspective_camera.h>
 #include <vpgl/io/vpgl_io_proj_camera.h>
 #include <vpgl/vpgl_affine_camera.h>
 #include <vpgl/io/vpgl_io_perspective_camera.h>
-#include <vpgl/vpgl_lvcs.h>
 #include <vnl/vnl_inverse.h>
-
-#define DEBUG
 
 vpgl_camera<double>* bwm_observer_geo_cam::
 read_camera(vcl_string cam_path)
 {
-	vcl_string ext = vul_file_extension(cam_path);
-	if (ext == ".tfw")
-	{
+  vcl_string ext = vul_file_extension(cam_path);
+  if (ext == ".tfw")
+  {
+    vcl_ifstream cam_stream(cam_path.c_str());
+    if (!cam_stream) {
+      vcl_cerr << "In bwm_observer_geo_cam::read_camera(.) -\n"
+               << " invalid binary camera file " << cam_path.data() << '\n';
+      return 0;
+    }
+    vnl_matrix<double> trans(2,2);
+    cam_stream >> trans;
 
-		vcl_ifstream cam_stream(cam_path.c_str());
-		if (!cam_stream) {
-			vcl_cerr << "In bwm_observer_geo_cam::read_camera(.) -\n"
-				<< " invalid binary camera file " << cam_path.data() << '\n';
-			return 0;
-		}
-		vnl_matrix<double> trans(2,2);
-		cam_stream >> trans;
+    vnl_matrix<double> transinv=vnl_inverse(trans);
 
-		vnl_matrix<double> transinv=vnl_inverse(trans);
-		
-		vnl_vector_fixed<double,4> r1(transinv(0,0),transinv(0,1),0,0);
-		vnl_vector_fixed<double,4> r2(transinv(1,0),transinv(1,1),0,0);
-		vpgl_affine_camera<double> * affine_cam = new vpgl_affine_camera<double>(r1,r2);
+    vnl_vector_fixed<double,4> r1(transinv(0,0),transinv(0,1),0,0);
+    vnl_vector_fixed<double,4> r2(transinv(1,0),transinv(1,1),0,0);
+    vpgl_affine_camera<double> * affine_cam = new vpgl_affine_camera<double>(r1,r2);
 
-		vcl_cout<<*affine_cam;
-		return affine_cam;
-	}
+    vcl_cout<<*affine_cam;
+    return affine_cam;
+  }
+  else
+    return static_cast<vpgl_camera<double>*>(0);
 }
 
 bwm_observer_geo_cam::bwm_observer_geo_cam(bgui_image_tableau_sptr img,
-										   vcl_string name,
-										   vcl_string& image_path,
-										   vcl_string& cam_path,
-										   bool display_image_path)
+                                           vcl_string name,
+                                           vcl_string& image_path,
+                                           vcl_string& cam_path,
+                                           bool display_image_path)
   : bwm_observer_cam(img)
 {
-
   img->show_image_path(display_image_path);
 
   // LOAD IMAGE
@@ -87,14 +82,14 @@ bwm_observer_geo_cam::bwm_observer_geo_cam(bgui_image_tableau_sptr img,
 }
 
 bool bwm_observer_geo_cam::intersect_ray_and_plane(vgl_point_2d<double> img_point,
-                                                    vgl_plane_3d<double> plane,
-                                                    vgl_point_3d<double> &world_point)
+                                                   vgl_plane_3d<double> plane,
+                                                   vgl_point_3d<double> &world_point)
 {
   vpgl_affine_camera<double> * geo_cam = static_cast<vpgl_affine_camera<double> *> (camera_);
   vgl_homg_line_3d_2_points<double> ray = geo_cam->backproject(vgl_homg_point_2d<double>(img_point));
   vgl_homg_operators_3d<double> oper;
   vgl_homg_point_3d<double> p = oper.intersect_line_and_plane(ray,plane);
-  vcl_cout<<"World Point "<<world_point<<"\n";
+  vcl_cout<<"World Point "<<world_point<<'\n';
   world_point = p;
   return true;
 }
