7ca8b3e5e6aa9554a81a8f11bb203cfee0367609
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-08-07 03:47:48 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 35354

--- contrib/brl/bbas/bwm/io/tests/test_include.cxx
+++ contrib/brl/bbas/bwm/io/tests/test_include.cxx
@@ -1,4 +1,6 @@
 #include <bwm/io/bwm_io_config_parser.h>
+#include <bwm/io/bwm_io_kml_camera.h>
+#include <bwm/io/bwm_io_kml_parser.h>
 #include <bwm/io/bwm_io_structs.h>
 #include <bwm/io/bwm_site.h>
 #include <bwm/io/bwm_site_sptr.h>
