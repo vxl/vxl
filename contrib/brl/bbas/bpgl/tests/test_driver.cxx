3356d68282faa619322cf86dd39d308c2465fa44
Joseph L. Mundy <mundyj@users.sourceforge.net>
2012-08-13 07:09:05 -0400

    Added test for kml camera
    SVN-Revision: 35380

--- contrib/brl/bbas/bpgl/tests/test_driver.cxx
+++ contrib/brl/bbas/bpgl/tests/test_driver.cxx
@@ -3,12 +3,14 @@
 
 DECLARE(test_segmented_rolling_shutter_camera );
 DECLARE(test_poly_radial_distortion );
+DECLARE(test_kml_camera );
 
 void
 register_tests()
 {
   REGISTER(test_segmented_rolling_shutter_camera  );
   REGISTER(test_poly_radial_distortion  );
+  REGISTER(test_kml_camera  );
 }
 
 DEFINE_MAIN;
---
+++ contrib/brl/bbas/bpgl/tests/test_kml_camera.cxx
@@ -0,0 +1,29 @@
+#include <testlib/testlib_test.h>
+#include <vcl_iostream.h>
+
+#include <bpgl/bpgl_camera_utils.h>
+#include <vgl/vgl_vector_3d.h>
+static void test_kml_camera()
+{
+  unsigned ni = 1280, nj = 720;
+  double nid = static_cast<double>(ni), njd = static_cast<double>(nj);
+  double right_fov = 29.605;
+  double top_fov = 17.725;
+  double altitude = 1.6;
+  double heading = 170.65;
+  double tilt = 78.62;
+  double roll = -2.76;
+  vpgl_perspective_camera<double> cam =
+    bpgl_camera_utils::camera_from_kml(nid, njd, right_fov, top_fov,
+                                       altitude, heading, tilt, roll);
+  vgl_vector_3d<double> pray = cam.principal_axis();
+  //s,c == sin,cos(head), st == sin(tilt)
+  double s = 0.1625, c = -0.9867, st = 0.9803;
+  double dif = vcl_fabs(pray.x()-(s*st));
+  dif += vcl_fabs(pray.y()-(c*st));
+  TEST_NEAR("principal ray (KML)", dif, 0.0, 0.0001);
+
+}
+
+
+TESTMAIN(test_kml_camera);
