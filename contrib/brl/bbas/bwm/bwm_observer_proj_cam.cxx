db034d0a444efcf4ece5268c2d290a799f6f3509
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-08-14 16:13:24 -0400

    indentation
    SVN-Revision: 35396

--- contrib/brl/bbas/bwm/bwm_observer_proj_cam.cxx
+++ contrib/brl/bbas/bwm/bwm_observer_proj_cam.cxx
@@ -39,18 +39,19 @@ read_camera(vcl_string cam_path, vcl_string subtype,
       bp_in.close();
       vpgl_proj_camera<double> cam(pcam.get_matrix());
       return cam.clone();
-    }else if(ext == ".kml"){
+    }
+    else if (ext == ".kml") {
       double right_fov, top_fov;
       double altitude, heading;
       double tilt, roll;
-      bool success = 
+      bool success =
         bwm_io_kml_camera::read_camera(cam_path, right_fov, top_fov,
                                        altitude, heading, tilt, roll);
-      if(!success){
+      if (!success) {
         vcl_cerr << "In bwm_observer_proj_cam::read_kml_camera(.) -\n"
                  << " invalid binary camera file " << cam_path.data() << '\n';
         return 0;
-      } 
+      }
       vpgl_perspective_camera<double> cam =
         bpgl_camera_utils::camera_from_kml(ni, nj, right_fov, top_fov,
                                            altitude, heading, tilt, roll);
@@ -59,9 +60,6 @@ read_camera(vcl_string cam_path, vcl_string subtype,
     //An ASCII stream for perspective camera
     vpgl_perspective_camera<double> pcam;
     vcl_ifstream cam_stream(cam_path.data());
-
-
-
 
     if (!cam_stream.is_open())
     {
@@ -78,7 +76,7 @@ read_camera(vcl_string cam_path, vcl_string subtype,
     vpgl_proj_camera<double> cam(pcam.get_matrix());
     return cam.clone();
 #endif
-	// now returns a perspective cam
+    // now returns a perspective cam
     return new vpgl_perspective_camera<double>(pcam);
   }
   //must be an ASCII stream for projective camera
@@ -125,10 +123,10 @@ bwm_observer_proj_cam::bwm_observer_proj_cam(bgui_image_tableau_sptr img,
   if (subtype=="identity")
     camera_ = new vpgl_perspective_camera<double>();
   else
-  camera_ = bwm_observer_proj_cam::read_camera(cam_path,subtype,
-                                               img_res->ni(),
-                                               img_res->nj());
-  if (camera_&& subtype == "perspective"){
+    camera_ = bwm_observer_proj_cam::read_camera(cam_path,subtype,
+                                                 img_res->ni(),
+                                                 img_res->nj());
+  if (camera_&& subtype == "perspective") {
     vpgl_perspective_camera<double>* cam =
       static_cast<vpgl_perspective_camera<double>*>(camera_);
     scene_.set_camera(*cam);
