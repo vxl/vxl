6fd819c66a98d229e210e074c5fbb498ddc66b0c
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2012-08-15 09:49:29 -0400

    adding a version using lvcs
    SVN-Revision: 35408

--- contrib/brl/bpro/core/vpgl_pro/processes/vpgl_convert_to_local_coordinates_process.cxx
+++ contrib/brl/bpro/core/vpgl_pro/processes/vpgl_convert_to_local_coordinates_process.cxx
@@ -73,3 +73,59 @@ bool vpgl_convert_to_local_coordinates_process(bprb_func_process& pro)
   return true;
 }
 
+//: initialization
+bool vpgl_convert_to_local_coordinates_process2_cons(bprb_func_process& pro)
+{
+  //this process takes four inputs: 
+  // 0: lvcs 
+  // 1: (float)  latitude
+  // 2: (float)  longitude
+  // 3: (float)  elevation
+  bool ok=false;
+  vcl_vector<vcl_string> input_types;
+  input_types.push_back("vpgl_lvcs_sptr"); 
+  input_types.push_back("float");
+  input_types.push_back("float");
+  input_types.push_back("float");
+  ok = pro.set_input_types(input_types);
+  if (!ok) return ok;
+
+  // outputs x,y,z in local coordinates
+  // 0: (float) x
+  // 1: (float) y
+  // 2: (float) z
+  vcl_vector<vcl_string> output_types;
+  output_types.push_back("float");  
+  output_types.push_back("float");  
+  output_types.push_back("float");  
+  ok = pro.set_output_types(output_types);
+  if (!ok) return ok;
+  
+  return true;
+
+}
+
+//: Execute the process
+bool vpgl_convert_to_local_coordinates_process2(bprb_func_process& pro)
+{
+  if (pro.n_inputs() != pro.input_types().size()) {
+    vcl_cout << "vpgl_convert_to_local_coordinates_process: The number of inputs should be " << pro.input_types().size() << vcl_endl;
+    return false;
+  }
+
+  // get the inputs
+  vpgl_lvcs_sptr lvcs = pro.get_input<vpgl_lvcs_sptr>(0);
+  float lat = pro.get_input<float>(1);
+  float lon = pro.get_input<float>(2);
+  float el = pro.get_input<float>(3);
+
+  double x,y,z;
+  lvcs->global_to_local(lon,lat,el, vpgl_lvcs::wgs84, x, y, z, vpgl_lvcs::DEG, vpgl_lvcs::METERS);
+
+  pro.set_output_val<float>(0, (float)x);
+  pro.set_output_val<float>(1, (float)y);
+  pro.set_output_val<float>(2, (float)z);
+
+  return true;
+}
+
