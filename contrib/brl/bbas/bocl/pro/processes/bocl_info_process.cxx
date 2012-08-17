0e1bd9904a00bcb0fd51b6fd9cb040d37b661df1
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-08-17 10:40:01 -0400

    Doxyghen: placed "\file" just after "//:"
    SVN-Revision: 35454

--- contrib/brl/bbas/bocl/pro/processes/bocl_info_process.cxx
+++ contrib/brl/bbas/bocl/pro/processes/bocl_info_process.cxx
@@ -1,6 +1,6 @@
 //:
-// \brief A Process to print all devices info
 // \file
+// \brief A Process to print all devices info
 // \author Isabel Restrepo
 // \date 9-Mar-2012
 
@@ -15,7 +15,7 @@
 
 
 //:global variables
-namespace bocl_info_process_globals 
+namespace bocl_info_process_globals
 {
   const unsigned n_inputs_ = 1;
   const unsigned n_outputs_ = 0;
@@ -28,9 +28,9 @@ bool bocl_info_process_cons(bprb_func_process& pro)
   using namespace bocl_info_process_globals;
   vcl_vector<vcl_string>  input_types_(n_inputs_);
   input_types_[0] = "bocl_manager_child_sptr";
-  
+
   vcl_vector<vcl_string>  output_types_(n_outputs_);
-  
+
   return pro.set_input_types(input_types_)
   && pro.set_output_types(output_types_);
 }
@@ -40,27 +40,22 @@ bool bocl_info_process_cons(bprb_func_process& pro)
 bool bocl_info_process(bprb_func_process& pro)
 {
   using namespace bocl_info_process_globals ;
-  
+
   bocl_manager_child_sptr mgr = pro.get_input<bocl_manager_child_sptr>(0);
-  
+
   //list GPU info
-  vcl_cout<<"  GPUs: \n";
-  for (unsigned int i=0; i<mgr->gpus_.size(); ++i){
-    vcl_cout<<"gpu"<<i<<", ";
-    vcl_cout<<*(mgr->gpus_[i]);
-    
-  }
-  
-  //list CPU info
-  vcl_cout<<"  CPUs: \n";
-  for (unsigned int i=0; i<mgr->cpus_.size(); ++i){
-    vcl_cout<<"cpu"<<i <<", ";
-    vcl_cout<<*(mgr->cpus_[i]);
-    
-  }
-  
-  vcl_cout<<vcl_endl;
-  return true;
+  vcl_cout<<"  GPUs:\n";
+  for (unsigned int i=0; i<mgr->gpus_.size(); ++i) {
+    vcl_cout<<"gpu"<<i<<", "<<*(mgr->gpus_[i]);
   }
 
+  //list CPU info
+  vcl_cout<<"  CPUs:\n";
+  for (unsigned int i=0; i<mgr->cpus_.size(); ++i) {
+    vcl_cout<<"cpu"<<i<<", "<<*(mgr->cpus_[i]);
+  }
+
+  vcl_cout<<vcl_endl;
+  return true;
+}
 
