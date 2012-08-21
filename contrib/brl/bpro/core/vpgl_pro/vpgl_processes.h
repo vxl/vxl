7ee1fe92862b18a86eec9afd7ba03e7689d418d3
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2012-08-21 12:22:17 -0400

    load camera from kml
    SVN-Revision: 35467

--- contrib/brl/bpro/core/vpgl_pro/vpgl_processes.h
+++ contrib/brl/bpro/core/vpgl_pro/vpgl_processes.h
@@ -52,5 +52,6 @@ DECLARE_FUNC_CONS(vpgl_translate_geo_camera_process);
 DECLARE_FUNC_CONS(vpgl_convert_geo_camera_to_generic_process);
 DECLARE_FUNC_CONS(vpgl_create_perspective_camera_from_kml_process);
 DECLARE_FUNC_CONS(vpgl_convert_to_local_coordinates_process2);
+DECLARE_FUNC_CONS(vpgl_load_perspective_camera_from_kml_file_process);
 
 #endif
