b34a0855f347a38eb64be8e9de3c37ef3d38b9ff
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2012-08-21 12:21:41 -0400

    load camera from kml
    SVN-Revision: 35466

--- contrib/brl/bpro/core/vpgl_pro/processes/vpgl_load_perspective_camera_process.cxx
+++ contrib/brl/bpro/core/vpgl_pro/processes/vpgl_load_perspective_camera_process.cxx
@@ -63,3 +63,67 @@ bool vpgl_load_perspective_camera_process(bprb_func_process& pro)
   return true;
 }
 
+// create camera from kml parameters
+bool vpgl_load_perspective_camera_from_kml_file_process_cons(bprb_func_process& pro)
+{
+  bool ok=false;
+  vcl_vector<vcl_string> input_types(3);
+  input_types[0] = "unsigned";// ni
+  input_types[1] = "unsigned";// nj
+  input_types[2] = "vcl_string";  // file name
+  ok = pro.set_input_types(input_types);
+  if (!ok) return ok;
+
+  vcl_vector<vcl_string> output_types(4);
+  output_types[0] = "vpgl_camera_double_sptr";  // output camera
+  output_types[1] = "double";  // longitude, the returned camera is at 0,0,altitude however return the read lat, lon in case it is used by other processes
+  output_types[2] = "double";  // lattitude
+  output_types[3] = "double";  // altitude 
+  ok = pro.set_output_types(output_types);
+  if (!ok) return ok;
+
+  return true;
+}
+
+#include <bkml/bkml_parser.h>
+#include <bpgl/bpgl_camera_utils.h>
+
+//: Execute the process
+bool vpgl_load_perspective_camera_from_kml_file_process(bprb_func_process& pro)
+{
+   // Sanity check
+  if (!pro.verify_inputs()) {
+    vcl_cerr << "vpgl_create_perspective_camera_process4: Invalid inputs\n";
+    return false;
+  }
+  // get the inputs
+  unsigned ni = pro.get_input<unsigned>(0);
+  unsigned nj = pro.get_input<unsigned>(1);
+  vcl_string name = pro.get_input<vcl_string>(2);
+  
+  bkml_parser* parser = new bkml_parser();
+  vcl_FILE* xmlFile = vcl_fopen(name.c_str(), "r");
+  if (!xmlFile) {
+    vcl_cerr << name.c_str() << " error on opening\n";
+    delete parser;
+    return false;
+  }
+  if (!parser->parseFile(xmlFile)) {
+    vcl_cerr << XML_ErrorString(parser->XML_GetErrorCode()) << " at line "
+             << parser->XML_GetCurrentLineNumber() << '\n';
+
+    delete parser;
+    return false;
+  }
+  vpgl_perspective_camera<double> out_cam = 
+    bpgl_camera_utils::camera_from_kml((double)ni, (double)nj, parser->right_fov_, parser->top_fov_, parser->altitude_, parser->heading_, parser->tilt_, parser->roll_);  
+  
+  vpgl_perspective_camera<double>* ncam = new vpgl_perspective_camera<double>(out_cam);
+  pro.set_output_val<vpgl_camera_double_sptr>(0, ncam);
+  pro.set_output_val<double>(1, parser->longitude_);
+  pro.set_output_val<double>(2, parser->latitude_);
+  pro.set_output_val<double>(3, parser->altitude_);
+  return true;
+}
+
+
