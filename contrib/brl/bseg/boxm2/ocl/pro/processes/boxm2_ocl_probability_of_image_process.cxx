eacd952a514f92248124517e38ef94f1a5d7fdf5
Vishal Jain <jainv@users.sourceforge.net>
2012-08-24 07:56:05 -0400

    changed the way mog3 buffer was being read.
    SVN-Revision: 35486

--- contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_probability_of_image_process.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_probability_of_image_process.cxx
@@ -250,8 +250,14 @@ bool boxm2_ocl_probability_of_image_process(bprb_func_process& pro)
       bocl_mem* blk       = opencl_cache->get_block(*id);
       bocl_mem* blk_info  = opencl_cache->loaded_block_info();
       bocl_mem* alpha     = opencl_cache->get_data<BOXM2_ALPHA>(*id);
-      bocl_mem* mog       = opencl_cache->get_data(*id,data_type);
-      transfer_time += (float) transfer.all();
+	  int mogTypeSize    = (int) boxm2_data_info::datasize(data_type);
+	        boxm2_scene_info* info_buffer = (boxm2_scene_info*) blk_info->cpu_buffer();
+      int alphaTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_ALPHA>::prefix());
+      info_buffer->data_buffer_length = (int) (alpha->num_bytes()/alphaTypeSize);
+      blk_info->write_to_buffer((queue));
+
+     bocl_mem* mog       = opencl_cache->get_data(*id,data_type,alpha->num_bytes()/alphaTypeSize*mogTypeSize,false);    //info_buffer->data_buffer_length*boxm2_data_info::datasize(data_type));
+       transfer_time += (float) transfer.all();
 
       ////3. SET args
       kern->set_arg( blk_info );
