ddfaec35832421e0e770e8648cec3e13b6239829
Vishal Jain <jainv@users.sourceforge.net>
2012-09-13 16:25:26 -0400

    adding a parser to read xml bounding boxes for ingesting in the volumes.
    SVN-Revision: 35594

---
+++ contrib/brl/bseg/boxm2/boxm2_bounding_box_parser.cxx
@@ -0,0 +1,82 @@
+//:
+// \file
+// \brief Parses the configuration file for bounding box tool.
+//
+#include "boxm2_bounding_box_parser.h"
+
+#include <vcl_sstream.h>
+#include <vcl_cstring.h>
+#include <vcl_iostream.h>
+#include <vcl_cstdio.h>
+
+// --------------
+// --- PARSER ---
+// --------------
+template <typename T>
+void convert(const char* t, T& d)
+{
+  vcl_stringstream strm(t);
+  strm >> d;
+}
+
+boxm2_bounding_box_parser::boxm2_bounding_box_parser()
+{
+  init_params();
+}
+
+
+void boxm2_bounding_box_parser::init_params()
+{
+  dataset_ = "" ;
+  entity_id_= -1;
+  volume_id_=-1;
+  version_ = 1;
+}
+
+
+//-----------------------------------------------------------------------------
+//: Start Element needs to parse the following tags
+
+void
+	boxm2_bounding_box_parser::startElement(const char* name, const char** atts)
+{
+	if (vcl_strcmp(name,DATASET_TAG) == 0) {
+		for (int i=0; atts[i]; i+=2) {
+			if (vcl_strcmp(atts[i], "name") == 0)
+				convert(atts[i+1], dataset_);
+		}
+	}
+	else if (vcl_strcmp(name,ENTITY_TAG)== 0) {
+		for (int i=0; atts[i]; i+=2) {
+			if (vcl_strcmp(atts[i], "id") == 0)
+			{
+				convert(atts[i+1], entity_id_);
+			}
+		}
+	}
+	else if (vcl_strcmp(name,VOLUME_TAG)== 0) {
+		for (int i=0; atts[i]; i+=2) {
+			if (vcl_strcmp(atts[i], "height") == 0)
+				convert(atts[i+1], height_);
+			else if (vcl_strcmp(atts[i], "id") == 0)
+				convert(atts[i+1], volume_id_);
+		}
+		heights_[entity_id_][volume_id_] = height_;
+	}
+	else if (vcl_strcmp(name,VERT_TAG)== 0) {
+		double x,y,z;
+		int vertid;
+		for (int i=0; atts[i]; i+=2) {
+			if (vcl_strcmp(atts[i], "id") == 0)
+				convert(atts[i+1], vertid);
+			else if (vcl_strcmp(atts[i], "x") == 0)
+				convert(atts[i+1], x);
+			else if (vcl_strcmp(atts[i], "y") == 0)
+				convert(atts[i+1], y);
+			else if (vcl_strcmp(atts[i], "z") == 0)
+				convert(atts[i+1], z);
+		}
+		verts_[entity_id_][volume_id_].push_back(vgl_point_3d<double>(x,y,z));
+	}
+}
+
---
+++ contrib/brl/bseg/boxm2/boxm2_bounding_box_parser.h
@@ -0,0 +1,53 @@
+#ifndef boxm2_bounding_box_parser_h_
+#define boxm2_bounding_box_parser_h_
+
+#include <expatpplib.h>
+#include <vcl_string.h>
+
+#include <boxm2/basic/boxm2_block_id.h>
+#include <boxm2/boxm2_block_metadata.h>
+#include <vpgl/vpgl_lvcs.h>
+#include <vgl/vgl_point_3d.h>
+#include <vcl_map.h>
+
+//block level metadata
+#define DATASET_TAG "dataSet"
+#define ENTITY_TAG  "entity"
+#define VOLUME_TAG  "volume"
+#define VERT_TAG    "vert"
+
+class boxm2_bounding_box_parser : public expatpp
+{
+ public:
+  boxm2_bounding_box_parser();
+  ~boxm2_bounding_box_parser(void) {}
+
+   // ACCESSORS for parser info
+   vcl_string dataset() const { return dataset_; }
+   int entity_id() const { return entity_id_; }
+   int volume_id() const { return volume_id_; }
+   vcl_vector< vgl_point_3d<double> >  verts(int entity_id, int volume_id)  {return verts_[entity_id][volume_id];}
+
+
+   int version(){return version_;}
+  vcl_map<int, vcl_map<int, vcl_vector< vgl_point_3d<double> > > > verts_;
+  vcl_map<int, vcl_map<int,double> > heights_;
+ private:
+  virtual void startElement(const XML_Char* name, const XML_Char** atts);
+  virtual void endElement(const XML_Char* /*name*/) {}
+  virtual void charData(const XML_Char* /*s*/, int /*len*/) {}
+
+  void init_params();
+
+	
+  vcl_string dataset_;
+  int entity_id_;
+  int volume_id_;
+  double height_;
+
+
+
+  int version_;
+};
+
+#endif
