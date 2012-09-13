9585cf15d7e00b5fa687ff9563d3581b288f3839
Vishal Jain <jainv@users.sourceforge.net>
2012-09-13 15:50:36 -0400

    Added ARF image stream.
    SVN-Revision: 35591

---
+++ contrib/brl/bbas/bil/Templates/vbl_smart_ptr+bil_arf_image_istream-.cxx
@@ -0,0 +1,4 @@
+#include <bil/bil_arf_image_istream.h>
+#include <vbl/vbl_smart_ptr.txx>
+
+VBL_SMART_PTR_INSTANTIATE(bil_arf_image_istream);
--- contrib/brl/bbas/bil/bil_arf_image_istream.cxx
+++ contrib/brl/bbas/bil/bil_arf_image_istream.cxx
@@ -1,15 +1,15 @@
-// This is brl/bbas/bil/bil_raw_image_istream.cxx
+// This is brl/bbas/bil/bil_arf_image_istream.cxx
 #ifdef VCL_NEEDS_PRAGMA_INTERFACE
 #pragma implementation
 #endif
 //:
 // \file
-// \author Andrew Miller
-// \date   19 Dec 2011
+// \author Vishal Jain
+// \date   Aug 31, 2012
 //
 //-----------------------------------------------------------------------------
 
-#include "bil_raw_image_istream.h"
+#include "bil_arf_image_istream.h"
 #include <vcl_algorithm.h>
 #include <vcl_sstream.h>
 #include <vul/vul_file_iterator.h>
@@ -18,15 +18,29 @@
 #include <vil/vil_memory_chunk.h>
 
 //--------------------------------------------------------------------------------
-
+void endian_swap(unsigned int& x) {
+ 
+        // Int is 4 bytes on this machine
+ 
+        x = (x >> 24) |
+        ((x<<8) & 0x00FF0000) |
+        ((x>>8) & 0x0000FF00) |
+        (x<<24);
+}
+void endian_swap_short(unsigned short& x) {
+ 
+        // Int is 4 bytes on this machine
+        x = (x >> 8) |((x<<8) & 0x0000FF00) ;
+        
+}
 //: The initial frame index
 // \note the initial frame index is invalid until advance() is called
 static const unsigned int INIT_INDEX = unsigned(-1);
 
 
 //: Constructor
-bil_raw_image_istream::
-bil_raw_image_istream()
+bil_arf_image_istream::
+bil_arf_image_istream()
   : index_(INIT_INDEX),
     ni_(0), nj_(0),
     format_(VIDL_PIXEL_FORMAT_UNKNOWN),
@@ -35,8 +49,8 @@ bil_raw_image_istream()
 
 
 //: Constructor
-bil_raw_image_istream::
-bil_raw_image_istream(const vcl_string& glob)
+bil_arf_image_istream::
+bil_arf_image_istream(const vcl_string& glob)
   : index_(INIT_INDEX),
     ni_(0), nj_(0),
     format_(VIDL_PIXEL_FORMAT_UNKNOWN),
@@ -50,40 +64,54 @@ bil_raw_image_istream(const vcl_string& glob)
 //: Open a new stream using a file glob (see vul_file_iterator)
 // \note files are loaded in alphanumeric order by path name
 bool
-bil_raw_image_istream::
+bil_arf_image_istream::
 open(const vcl_string& rawFile)
 {
   //open up raw file, and read 20 byte header
   this->raw_file_ = rawFile;
   raw_.open(raw_file_.c_str(), vcl_ios::in | vcl_ios::binary);
 
-  int ni, nj, pixelSize;
-  int numFrames;
-  raw_.read( (char*) &ni, sizeof(ni) );
-  raw_.read( (char*) &nj, sizeof(nj) );
-  raw_.read( (char*) &pixelSize, sizeof(pixelSize) );
-  raw_.read( (char*) &numFrames, sizeof(numFrames) );
-  //vj: modification 
-  vxl_int_64 timestamp;
-  for(unsigned i = 0 ; i < numFrames; i++)
-	  raw_.read( (char*) &timestamp, sizeof(timestamp) );
+  unsigned int magic_num, version, arf_image_type, ni, nj, num_images, image_offset, subheader_flags;
 
+  raw_.read( (char*) &magic_num, sizeof(magic_num) );
+  raw_.read( (char*) &version, sizeof(version) );
+  raw_.read( (char*) &nj, sizeof(nj) ); 
+  raw_.read( (char*) &ni, sizeof(ni) );
+  raw_.read( (char*) &arf_image_type, sizeof(arf_image_type) );
+  raw_.read( (char*) &num_images, sizeof(num_images) );
+  raw_.read( (char*) &image_offset, sizeof(image_offset) );
+  raw_.read( (char*) &subheader_flags, sizeof(subheader_flags) );
+
+  endian_swap(magic_num);
+  endian_swap(version);
+  endian_swap(ni);
+  endian_swap(nj);
+  endian_swap(arf_image_type);
+  endian_swap(num_images);
+  endian_swap(image_offset);
+  endian_swap(subheader_flags);
+
+  //vj: modification 
   vcl_cout<<"Raw file header:\n"
-          <<"  size: "<<ni<<','<<nj<<'\n'
-          <<"  pixel:"<<pixelSize<<'\n'
-          <<"  #fram:"<<numFrames<<vcl_endl;
+		  <<"Magic Num "<<magic_num<<"\n"
+		  <<"version "<<version<<"\n"
+          <<"size: "<<ni<<','<<nj<<'\n'
+          <<"pixel:"<<arf_image_type<<'\n'
+          <<"#fram:"<<num_images<<'\n'
+		  <<"Image Offset "<<image_offset<<'\n'
+		  <<"Subheader "<<subheader_flags<<vcl_endl;
 
   //store in member vars
   ni_ = ni;
   nj_ = nj;
-  num_frames_ = (unsigned int)numFrames; // possible overflow...
-  pixel_size_ = pixelSize;
-  if (pixelSize==24)
-    format_ = VIDL_PIXEL_FORMAT_RGB_24;
-  else if (pixelSize==8)
-    format_ = VIDL_PIXEL_FORMAT_MONO_8;
-  else if (pixelSize==16)
+  num_frames_ = (unsigned int)num_images; // possible overflow...
+  
+  if (arf_image_type==5)
+  {
     format_ = VIDL_PIXEL_FORMAT_MONO_16;
+	pixel_size_ = 16;
+  }
+  image_offset_ = image_offset;
 
   //index is invalid until advance is called
   index_ = INIT_INDEX;
@@ -93,7 +121,7 @@ open(const vcl_string& rawFile)
 
 //: Close the stream
 void
-bil_raw_image_istream::
+bil_arf_image_istream::
 close()
 {
   //image_paths_.clear();
@@ -103,13 +131,13 @@ close()
   nj_ = 0;
   format_ = VIDL_PIXEL_FORMAT_UNKNOWN;
   raw_.close();
-  vcl_cout<<"bil_raw_image_istream closed"<<vcl_endl;
+  vcl_cout<<"bil_arf_image_istream closed"<<vcl_endl;
 }
 
 
 //: Advance to the next frame (but do not load the next image)
 bool
-bil_raw_image_istream::
+bil_arf_image_istream::
 advance()
 {
   current_frame_ = NULL;
@@ -122,7 +150,7 @@ advance()
 
 //: Read the next frame from the stream
 vil_image_view_base_sptr
-bil_raw_image_istream::read_frame()
+bil_arf_image_istream::read_frame()
 {
   advance();
   return current_frame();
@@ -131,7 +159,7 @@ bil_raw_image_istream::read_frame()
 
 //: Return the current frame in the stream
 vil_image_view_base_sptr
-bil_raw_image_istream::current_frame()
+bil_arf_image_istream::current_frame()
 {
   //hack way to clean up current memory
   if (is_valid()) {
@@ -140,7 +168,9 @@ bil_raw_image_istream::current_frame()
       //calc image size, seek to offset
       unsigned int imgSize = ni_*nj_*pixel_size_/8;
 	  	  //:  vj Modification
-      long long loc = 20+8*num_frames_ + (long long) index_* ( (long long)imgSize );
+      long long loc = image_offset_+((long long) index_ -1)* ( (long long)imgSize +40 );
+
+	  vcl_cout<<"location "<<loc <<" "<<index_<<vcl_endl;
       raw_.seekg(loc, vcl_ios::beg);
 
       //allocate vil memory chunk
@@ -157,14 +187,22 @@ bil_raw_image_istream::current_frame()
       }
       else if (pixel_size_==16) {
         raw_.read( (char*) mem_chunk->data(), imgSize );
-        current_frame_ = new vil_image_view<unsigned short>(mem_chunk, (unsigned short*) mem_chunk->data(), ni_, nj_, 1, 1, ni_, ni_*nj_);
-      }
-	  //:  vj Modification
-      //read timestamp
-      //vxl_int_64 timeStamp;
-      //raw_.read( (char*) &timeStamp, sizeof(timeStamp) );
-      //time_stamp_ = timeStamp;
+        vil_image_view<unsigned short> * current_frame = new vil_image_view<unsigned short>(mem_chunk, (unsigned short*) mem_chunk->data(), ni_, nj_, 1, 1, ni_, ni_*nj_);
+
+		for(unsigned i = 0 ; i < current_frame->ni(); i++)
+		{
+			for(unsigned j = 0 ; j < current_frame->nj(); j++)
+			{
+				unsigned short x = (*current_frame)(i,j) ;
+				endian_swap_short(x);
+				(*current_frame)(i,j) = x;
+			}
+		}
+		current_frame_ = current_frame ; 
+	  }
+
     }
+
     return current_frame_;
   }
   return NULL;
@@ -173,7 +211,7 @@ bil_raw_image_istream::current_frame()
 
 //: Return the path to the current image in the stream
 vcl_string
-bil_raw_image_istream::current_path() const
+bil_arf_image_istream::current_path() const
 {
   return raw_file_;
 }
@@ -182,7 +220,7 @@ bil_raw_image_istream::current_path() const
 //: Seek to the given frame number (but do not load the image)
 // \returns true if successful
 bool
-bil_raw_image_istream::
+bil_arf_image_istream::
 seek_frame(unsigned int frame_nr)
 {
   if (is_open() && frame_nr < num_frames_) {
@@ -195,21 +233,21 @@ seek_frame(unsigned int frame_nr)
 }
 
 //: Binary write boxm2_data_base to stream
-void vsl_b_write(vsl_b_ostream& os, bil_raw_image_istream const& scene) {}
+void vsl_b_write(vsl_b_ostream& os, bil_arf_image_istream const& scene) {}
 //: Binary write boxm2_data_base to stream
-void vsl_b_write(vsl_b_ostream& os, const bil_raw_image_istream* &p) {}
+void vsl_b_write(vsl_b_ostream& os, const bil_arf_image_istream* &p) {}
 //: Binary write boxm2_data_base_sptr to stream
-void vsl_b_write(vsl_b_ostream& os, bil_raw_image_istream_sptr& sptr) {}
+void vsl_b_write(vsl_b_ostream& os, bil_arf_image_istream_sptr& sptr) {}
 //: Binary write boxm2_data_base_sptr to stream
-void vsl_b_write(vsl_b_ostream& os, bil_raw_image_istream_sptr const& sptr) {}
+void vsl_b_write(vsl_b_ostream& os, bil_arf_image_istream_sptr const& sptr) {}
 
 //: Binary load boxm2_data_base from stream.
-void vsl_b_read(vsl_b_istream& is, bil_raw_image_istream &scene) {}
+void vsl_b_read(vsl_b_istream& is, bil_arf_image_istream &scene) {}
 //: Binary load boxm2_data_base from stream.
-void vsl_b_read(vsl_b_istream& is, bil_raw_image_istream* p) {}
+void vsl_b_read(vsl_b_istream& is, bil_arf_image_istream* p) {}
 //: Binary load boxm2_data_base_sptr from stream.
-void vsl_b_read(vsl_b_istream& is, bil_raw_image_istream_sptr& sptr) {}
+void vsl_b_read(vsl_b_istream& is, bil_arf_image_istream_sptr& sptr) {}
 //: Binary load boxm2_data_base_sptr from stream.
-void vsl_b_read(vsl_b_istream& is, bil_raw_image_istream_sptr const& sptr) {}
+void vsl_b_read(vsl_b_istream& is, bil_arf_image_istream_sptr const& sptr) {}
 
 
