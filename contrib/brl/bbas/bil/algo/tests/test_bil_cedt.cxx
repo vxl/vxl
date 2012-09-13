24c592e8fb5973d69724c51c7c47e204ddaef49e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-09-13 02:51:57 -0400

    removed unused parameter "print"
    SVN-Revision: 35584

--- contrib/brl/bbas/bil/algo/tests/test_bil_cedt.cxx
+++ contrib/brl/bbas/bil/algo/tests/test_bil_cedt.cxx
@@ -40,125 +40,112 @@ static void test_bil_cedt()
   bil_cedt_test(image,true);
 }
 
-TESTMAIN(test_bil_cedt);
-
 void
-bil_cedt_test(vil_image_view<unsigned char> &im, bool print)
+bil_cedt_test(vil_image_view<unsigned char> &im, bool /*print*/)
 {
-   print = false; //:< not used
+  bil_cedt cedt(im);
+  cedt.compute_cedt();
 
+  vil_image_view<float> cedtim=cedt.cedtimg();
+  vil_image_view<float> bruteforcedtim=bil_cedt_brute_force(im);
+  vcl_cout << "CEDT:\n";
+  vil_print_all(vcl_cout,cedtim);
 
-   bil_cedt cedt(im);
-   cedt.compute_cedt();
+  vcl_cout << "Brute Force:\n";
+  vil_print_all(vcl_cout,bruteforcedtim);
 
+  bool cedt_error=false;
 
-   vil_image_view<float> cedtim=cedt.cedtimg();
-   vil_image_view<float> bruteforcedtim=bil_cedt_brute_force(im);
-   vcl_cout << "CEDT:\n";
-   vil_print_all(vcl_cout,cedtim);
+  for (unsigned i=0; i<im.size(); ++i) {
+    float dst,cedtdst;
+    dst = DATA(bruteforcedtim)[i];
+    cedtdst=DATA(cedtim)[i];
+    if (vcl_fabs(dst -cedtdst)>0.1) {
+      vcl_printf("Error. CEDT: %g BF: %g\n",DATA(cedtim)[i],dst);
+      cedt_error = true;
+      break;
+    }
+  }
 
-   vcl_cout << "Brute Force:\n";
-   vil_print_all(vcl_cout,bruteforcedtim);
-
-
-   bool cedt_error=false;
-
-   for (unsigned i=0; i<im.size(); ++i) {
-      float dst,cedtdst;
-      dst = DATA(bruteforcedtim)[i];
-      cedtdst=DATA(cedtim)[i];
-      if (vcl_fabs(dst -cedtdst)>0.1) {
-         vcl_printf("Error. CEDT: %g BF: %g\n",DATA(cedtim)[i],dst);
-         cedt_error = true;
-         break;
-      }
-   }
-
-   TEST("CEDT", cedt_error, false);
+  TEST("CEDT", cedt_error, false);
 }
 
 vil_image_view<float> bil_cedt_brute_force(vil_image_view<unsigned char> &im)
 {
-    vil_image_view<float> dist(im.ni(),im.nj());
-    dist.fill(0.0);
+  vil_image_view<float> dist(im.ni(),im.nj());
+  dist.fill(0.0);
 
-#if 0
-//this variable is not used in the code.  PLEASE FIX!  -MM
-    float infty_=1e6;
-#endif
-
-    for (unsigned int arrayj=0;arrayj<dist.nj();arrayj++)
+  for (unsigned int arrayj=0;arrayj<dist.nj();arrayj++)
+  {
+    for (unsigned int arrayi=0;arrayi<dist.ni();arrayi++)
     {
-     for (unsigned int arrayi=0;arrayi<dist.ni();arrayi++)
-     {
-         float dst=9999.0;
-         for (unsigned int i=0;i<im.ni();i++)
-         {
-             for (unsigned int j=0;j< im.nj();j++)
-             {
-                 if (im(i,j)==0)
-                 {
-                     float temp=vcl_sqrt((float)((i-arrayi)*(i-arrayi)+(j-arrayj)*(j-arrayj)));
-                     if (temp<dst)
-                         dst=temp;
-                 }
-             }
-         }
-         dist(arrayi,arrayj)=dst;
-     }
+      float dst=1e34f; // will become min dist, so initialise with high value
+      for (unsigned int i=0;i<im.ni();i++)
+      {
+        for (unsigned int j=0;j< im.nj();j++)
+        {
+          if (im(i,j)==0)
+          {
+            float temp=vcl_sqrt((float)((i-arrayi)*(i-arrayi)+(j-arrayj)*(j-arrayj)));
+            if (temp<dst)
+              dst=temp;
+          }
+        }
+      }
+      dist(arrayi,arrayj)=dst;
     }
+  }
 
-    return dist;
+  return dist;
 }
 
 
 bool generate_random_line(vil_image_view<unsigned char> &im, int seed)
 {
-    vnl_random rnd(seed);
-    int x0 = rnd.lrand32(0, im.ni()-1);
-    int y0 = rnd.lrand32(0, im.nj()-1);
-    double theta=rnd.drand32(0, vnl_math::pi/2);
-    for (unsigned i=0;i<im.ni();i++)
-    {
-      int j=(int)((i-x0)*vcl_tan(theta)+y0);
+  vnl_random rnd(seed);
+  int x0 = rnd.lrand32(0, im.ni()-1);
+  int y0 = rnd.lrand32(0, im.nj()-1);
+  double theta=rnd.drand32(0, vnl_math::pi/2);
+  for (unsigned i=0; i<im.ni(); ++i)
+  {
+    int j=(int)((i-x0)*vcl_tan(theta)+y0);
 
-      int imnjunsigned = static_cast<int>(im.nj());
-      assert(imnjunsigned >= 0);
+    int imnjunsigned = static_cast<int>(im.nj());
+    assert(imnjunsigned >= 0);
 
-      if (j>=0 && j<imnjunsigned)
-          im(i,j)=0;
-    }
-    return true;
+    if (j>=0 && j<imnjunsigned)
+      im(i,j)=0;
+  }
+  return true;
 }
 
 bool generate_random_circle(vil_image_view<unsigned char> &im,int seed)
 {
-    vnl_random rnd(seed);
-    int x0 = rnd.lrand32(0, im.ni()-1);
-#if 0
-//this variable is not used in the code.  PLEASE FIX!  -MM
-    int y0 = rnd.lrand32(0, im.nj()-1);
-#endif
-    int radius=rnd.lrand32(0, (int)vcl_min(im.nj(),im.ni())/2);
+  vnl_random rnd(seed);
+  int x0 = rnd.lrand32(0, im.ni()-1);
+  int radius=rnd.lrand32(0, (int)vcl_min(im.nj(),im.ni())/2);
 
-      int imniunsigned = static_cast<int>(im.ni());
-      assert(imniunsigned>=0);
+  int imniunsigned = static_cast<int>(im.ni());
+  assert(imniunsigned>=0);
 
-
-    for (int i=0;i<imniunsigned;i++)
+  for (int i=0;i<imniunsigned;i++)
+  {
+    float discriminant= (float)(radius*radius-(i-x0)*(i-x0));
+    if (discriminant>0)
     {
-      float discriminant= (float)(radius*radius-(i-x0)*(i-x0));
-      if (discriminant>0)
-      {
-        int j1=x0+(int)vcl_sqrt(discriminant);
-        int j2=x0-(int)vcl_sqrt(discriminant);
+      int j1=x0+(int)vcl_sqrt(discriminant);
+      int j2=x0-(int)vcl_sqrt(discriminant);
 
-        if (j1>=0 && j1< static_cast<int>(im.nj()) )
-            im(i,j1)=0;
+      int imnjunsigned = static_cast<int>(im.nj());
+      assert(imnjunsigned>=0);
+      if (j1>=0 && j1<imnjunsigned)
+        im(i,j1)=0;
 
-        if (j2>=0 && j2<static_cast<int>(im.nj()))
-            im(i,j2)=0;
-      }
+      if (j2>=0 && j2<imnjunsigned)
+        im(i,j2)=0;
     }
-    return true;
+  }
+  return true;
 }
+
+TESTMAIN(test_bil_cedt);
