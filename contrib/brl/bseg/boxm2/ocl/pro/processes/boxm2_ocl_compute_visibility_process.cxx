ffe5921c806a73bf0132cd468ee225dac83ba5dc
Vishal Jain <jainv@users.sourceforge.net>
2012-09-19 11:44:24 -0400

    SVN-Revision: 35650

---
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_compute_visibility_process.cxx
@@ -0,0 +1,285 @@
+// This is brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_compute_visibility_process.cxx
+#include <bprb/bprb_func_process.h>
+//:
+// \file
+// \brief  A process for rendering visibilty of the scene from a given point.
+//
+// \author Vishal Jain
+// \date Sept 15, 2012
+
+#include <vcl_fstream.h>
+#include <boxm2/ocl/boxm2_opencl_cache.h>
+#include <boxm2/boxm2_scene.h>
+#include <boxm2/boxm2_block.h>
+#include <boxm2/boxm2_data_base.h>
+#include <boxm2/ocl/boxm2_ocl_util.h>
+#include <vil/vil_image_view.h>
+//brdb stuff
+#include <brdb/brdb_value.h>
+
+//directory utility
+#include <vcl_where_root_dir.h>
+#include <bocl/bocl_device.h>
+#include <bocl/bocl_kernel.h>
+#include <vul/vul_timer.h>
+#include <vul/vul_file.h>
+#include <boxm2/ocl/algo/boxm2_ocl_camera_converter.h>
+#include <boxm2/boxm2_util.h>
+#include <vcl_cstdio.h>
+#include <vil/vil_save.h>
+
+namespace boxm2_ocl_compute_visibility_process_globals
+{
+    const unsigned n_inputs_ = 9;
+    const unsigned n_outputs_ = 0;
+    vcl_size_t lthreads[2]={8,8};
+    void compile_kernel(bocl_device_sptr device,vcl_vector<bocl_kernel*> & vec_kernels)
+    {
+        //gather all render sources... seems like a lot for rendering...
+        vcl_vector<vcl_string> src_paths;
+        vcl_string source_dir = boxm2_ocl_util::ocl_src_root();
+        src_paths.push_back(source_dir + "scene_info.cl");
+        src_paths.push_back(source_dir + "pixel_conversion.cl");
+        src_paths.push_back(source_dir + "bit/bit_tree_library_functions.cl");
+        src_paths.push_back(source_dir + "backproject.cl");
+        src_paths.push_back(source_dir + "statistics_library_functions.cl");
+        src_paths.push_back(source_dir + "expected_functor.cl");
+        src_paths.push_back(source_dir + "ray_bundle_library_opt.cl");
+        src_paths.push_back(source_dir + "bit/render_bit_scene.cl");
+        src_paths.push_back(source_dir + "bit/cast_ray_bit.cl");
+
+        //set kernel options
+        vcl_string options = " -D RENDER_VISIBILITY ";
+        options += " -D STEP_CELL=step_cell_vis(aux_args.alpha,data_ptr,d*linfo->block_len,aux_args.vis)";
+
+        //have kernel construct itself using the context and device
+        bocl_kernel * ray_trace_kernel=new bocl_kernel();
+
+        ray_trace_kernel->create_kernel( &device->context(),
+            device->device_id(),
+            src_paths,
+            "render_visibiltiy",   //kernel name
+            options,              //options
+            "boxm2 opencl render visibility"); //kernel identifier (for error checking)
+        vec_kernels.push_back(ray_trace_kernel);
+    }
+    static vcl_map<vcl_string,vcl_vector<bocl_kernel*> > kernels;
+}
+
+bool boxm2_ocl_compute_visibility_process_cons(bprb_func_process& pro)
+{
+    using namespace boxm2_ocl_compute_visibility_process_globals;
+
+    //process takes 1 input
+    vcl_vector<vcl_string> input_types_(n_inputs_);
+    input_types_[0] = "bocl_device_sptr";
+    input_types_[1] = "boxm2_scene_sptr";
+    input_types_[2] = "boxm2_opencl_cache_sptr";
+    input_types_[3] = "vcl_string"; // cams file
+    input_types_[4] = "vcl_string"; // directory for depth images
+    input_types_[5] = "float";  
+    input_types_[6] = "float";  
+    input_types_[7] = "float";  
+    input_types_[8] = "vcl_string"; // output directory for visibility images
+    // process has 1 output:
+    // output[0]: scene sptr
+    vcl_vector<vcl_string>  output_types_(n_outputs_);
+    return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
+}
+
+bool boxm2_ocl_compute_visibility_process(bprb_func_process& pro)
+{
+    using namespace boxm2_ocl_compute_visibility_process_globals;
+    vul_timer t;
+    t.mark();
+    if ( pro.n_inputs() < n_inputs_ ) {
+        vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
+        return false;
+    }
+    float transfer_time=0.0f;
+    float gpu_time=0.0f;
+    //get the inputs
+    unsigned i = 0;
+    bocl_device_sptr device= pro.get_input<bocl_device_sptr>(i++);
+    boxm2_scene_sptr scene =pro.get_input<boxm2_scene_sptr>(i++);
+    boxm2_opencl_cache_sptr opencl_cache= pro.get_input<boxm2_opencl_cache_sptr>(i++);
+    vcl_string camsfile= pro.get_input<vcl_string>(i++);
+    vcl_string indir= pro.get_input<vcl_string>(i++);
+    float x = pro.get_input<float>(i++);
+    float y = pro.get_input<float>(i++);
+    float z = pro.get_input<float>(i++);
+    vcl_string outdir= pro.get_input<vcl_string>(i++);
+    vcl_string identifier=device->device_identifier();
+
+    // create a command queue.
+    int status=0;
+    cl_command_queue queue = clCreateCommandQueue(device->context(),
+        *(device->device_id()),
+        CL_QUEUE_PROFILING_ENABLE,
+        &status);
+    if (status!=0)
+        return false;
+    // compile the kernel
+    if (kernels.find(identifier)==kernels.end())
+    {
+        vcl_cout<<"===========Compiling kernels==========="<<vcl_endl;
+        vcl_vector<bocl_kernel*> ks;
+        compile_kernel(device,ks);
+        kernels[identifier]=ks;
+    }
+    vcl_vector<vil_image_view<float>*>  raydirs;
+    vcl_vector<vil_image_view<float>*>  tfinals;
+    float rayo_buff[4];
+    rayo_buff[0] = x;
+    rayo_buff[1] = y;
+    rayo_buff[2] = z;
+    rayo_buff[3] = 0;
+    bocl_mem_sptr ray_o_buff=opencl_cache->alloc_mem(sizeof(float)*4, rayo_buff,  "image dims");
+    ray_o_buff->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+
+    boxm2_util::get_raydirs_tfinal(indir,camsfile,vgl_point_3d<double>(x,y,z),raydirs,tfinals);
+    unsigned cl_ni =0;
+    unsigned cl_nj =0;
+    vcl_vector<bocl_mem_sptr> bocl_raydirs;
+    vcl_vector<bocl_mem_sptr> bocl_vis;
+
+    vcl_vector<float*> rayd_buffs;
+    vcl_vector<float*> vis_buffs;
+    int img_dim_buff[4];
+    vcl_size_t gThreads[]={8,8};
+    for (unsigned int count = 0 ; count < tfinals.size(); count ++)
+    {
+        char filename[1000];
+        vcl_sprintf(filename,"tfinal_%d.tiff",count);
+        vcl_string outfile = outdir +"/" + filename;
+        vil_save(*(tfinals[count]),outfile.c_str());
+
+        cl_ni=RoundUp(tfinals[count]->ni(),lthreads[0]);
+        cl_nj=RoundUp(tfinals[count]->nj(),lthreads[1]);
+
+        float* rayd_buff = new float[cl_ni*cl_nj*4];
+        float* vis_buff = new float[cl_ni*cl_nj];
+        int num=0;
+        for (unsigned int j=0;j<cl_nj;++j)
+            for (unsigned int i=0;i<cl_ni;++i)
+            {
+                if (i<tfinals[count]->ni() && j< tfinals[count]->nj()) 
+                { 
+                    rayd_buff[num*4+0]=(* raydirs[count])(i,j,0);
+                    rayd_buff[num*4+1]=(* raydirs[count])(i,j,1);
+                    rayd_buff[num*4+2]=(* raydirs[count])(i,j,2);
+                    rayd_buff[num*4+3]=(* tfinals[count])(i,j);
+                }
+                vis_buff[num] = 1.0f;
+                ++num;
+            }
+            rayd_buffs.push_back(rayd_buff);
+            vis_buffs.push_back(vis_buff);
+            if(count ==0)
+            {
+                img_dim_buff[0] = 0;
+                img_dim_buff[1] = 0;
+                img_dim_buff[2] = tfinals[count]->ni();
+                img_dim_buff[3] = tfinals[count]->nj();
+
+                gThreads[0] = cl_ni;
+                gThreads[1] = cl_nj;
+            }
+    }
+
+    // Image Dimensions
+    bocl_mem_sptr exp_img_dim=opencl_cache->alloc_mem(sizeof(int)*4, img_dim_buff,  "image dims");
+    exp_img_dim->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+
+    // Output Array
+    float output_arr[100];
+    for (int i=0; i<100; ++i) output_arr[i] = 0.0f;
+    bocl_mem_sptr  cl_output=opencl_cache->alloc_mem(sizeof(float)*100, output_arr, "output buffer");
+    cl_output->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+
+    // bit lookup buffer
+    cl_uchar lookup_arr[256];
+    boxm2_ocl_util::set_bit_lookup(lookup_arr);
+    bocl_mem_sptr lookup=opencl_cache->alloc_mem( sizeof(cl_uchar)*256, lookup_arr,  "bit lookup buffer");
+    lookup->create_buffer(CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR);
+
+    float subblk_dim = 0.0;
+    bocl_kernel* kern =  kernels[identifier][0];
+    // set arguments
+    boxm2_block_id pt_id;
+    vgl_point_3d<double> local_coords;
+    scene->contains(vgl_point_3d<double>(x,y,z),pt_id,local_coords);
+
+    vcl_vector<boxm2_block_id> vis_order = boxm2_util::order_about_a_block(scene,pt_id);
+    vcl_cout<<"GOT TH BLOCKS ORDERED"<<vcl_endl;
+    vcl_vector<boxm2_block_id>::iterator id;
+    for (id = vis_order.begin(); id != vis_order.end(); ++id)
+    {
+        boxm2_block_metadata mdata = scene->get_block_metadata(*id);
+        vul_timer transfer;
+        bocl_mem* blk           = opencl_cache->get_block(*id);
+        bocl_mem* alpha         = opencl_cache->get_data<BOXM2_ALPHA>(*id);
+        bocl_mem * blk_info     = opencl_cache->loaded_block_info();
+        transfer_time          += (float) transfer.all();
+        vcl_cout<<(*id)<<vcl_endl;
+        for(unsigned int frame = 0 ; frame < rayd_buffs.size(); frame ++)
+        {
+            
+            //choose correct render kernel
+            bocl_mem * ray_image = opencl_cache->alloc_mem(4*cl_ni*cl_nj*sizeof(float), rayd_buffs[frame],"ray direction buffer");
+            ray_image->create_buffer(CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR);
+            
+            bocl_mem * vis_image = opencl_cache->alloc_mem(cl_ni*cl_nj*sizeof(float), vis_buffs[frame],"ray direction buffer");
+            vis_image->create_buffer(CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR);
+ 
+            ////3. SET args
+            kern->set_arg( blk_info );
+            kern->set_arg( blk );
+            kern->set_arg( alpha );
+            kern->set_arg( ray_o_buff.ptr() );
+            kern->set_arg( ray_image );
+            kern->set_arg( exp_img_dim.ptr());
+            kern->set_arg( cl_output.ptr() );
+            kern->set_arg( lookup.ptr() );
+            kern->set_arg( vis_image );
+            //local tree , cumsum buffer, imindex buffer
+            kern->set_local_arg( lthreads[0]*lthreads[1]*sizeof(cl_uchar16) );
+            kern->set_local_arg( lthreads[0]*lthreads[1]*10*sizeof(cl_uchar) );
+            kern->set_local_arg( lthreads[0]*lthreads[1]*sizeof(cl_int) );
+            //execute kernel
+            kern->execute(queue, 2, lthreads, gThreads);
+            clFinish(queue);
+            gpu_time += kern->exec_time();
+            
+            cl_output->read_to_buffer(queue);
+            vis_image->read_to_buffer(queue);
+            opencl_cache->free_mem(ray_image);
+            opencl_cache->free_mem(vis_image);
+            // clear render kernel args so it can reset em on next execution
+            kern->clear_args();
+        }
+    }
+
+    clReleaseCommandQueue(queue);
+    for (unsigned int count = 0 ; count < tfinals.size(); count ++)
+    {
+        float * vis_buf = (float*)vis_buffs[count];
+        vil_image_view<unsigned char> visout(tfinals[count]->ni(),tfinals[count]->nj());
+
+        for (unsigned c=0;c<tfinals[count]->nj();c++)
+            for (unsigned r=0;r<tfinals[count]->ni();r++)
+            {
+                if(vis_buf[c*cl_ni+r] > 0.5)
+                visout(r,c)=255;
+            }
+
+            char filename[1000];
+            vcl_sprintf(filename,"vis_%d.png",count);
+            vcl_string outfile = outdir +"/" + filename;
+            vil_save(visout,outfile.c_str());
+
+    }
+
+    vcl_cout<<"Time taken is "<<t.all()<<vcl_endl;
+    return true;
+}
---
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_probability_of_image_wcubic_process.cxx
@@ -0,0 +1,311 @@
+// This is brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_probability_of_image_wcubic_process.cxx
+//:
+// \file
+// \brief  A process for change detection
+//
+// \author Vishal Jain
+// \date Mar 10, 2011
+
+#include <bprb/bprb_func_process.h>
+
+#include <vcl_fstream.h>
+#include <vcl_algorithm.h>
+#include <boxm2/ocl/boxm2_opencl_cache.h>
+#include <boxm2/boxm2_scene.h>
+#include <boxm2/boxm2_block.h>
+#include <boxm2/boxm2_data_base.h>
+#include <boxm2/ocl/boxm2_ocl_util.h>
+#include <boxm2/boxm2_util.h>
+#include <vil/vil_image_view.h>
+//brdb stuff
+#include <brdb/brdb_value.h>
+
+//directory utility
+#include <vul/vul_timer.h>
+#include <vcl_where_root_dir.h>
+#include <bocl/bocl_device.h>
+#include <bocl/bocl_kernel.h>
+
+#include <boxm2/ocl/algo/boxm2_ocl_camera_converter.h>
+
+namespace boxm2_ocl_probability_of_image_wcubic_process_globals
+{
+  const unsigned n_inputs_ = 7;
+  const unsigned n_outputs_ = 1;
+  vcl_size_t lthreads[2]={8,8};
+
+  static vcl_map<vcl_string,vcl_vector<bocl_kernel*> > kernels;
+
+  void compile_kernel(bocl_device_sptr device,vcl_vector<bocl_kernel*> & vec_kernels, vcl_string opts)
+  {
+    //gather all render sources... seems like a lot for rendering...
+  //gather all render sources... seems like a lot for rendering...
+  vcl_vector<vcl_string> src_paths;
+  vcl_string source_dir = boxm2_ocl_util::ocl_src_root();
+  src_paths.push_back(source_dir + "scene_info.cl");
+  src_paths.push_back(source_dir + "cell_utils.cl");
+  src_paths.push_back(source_dir + "bit/bit_tree_library_functions.cl");
+  src_paths.push_back(source_dir + "backproject.cl");
+  src_paths.push_back(source_dir + "statistics_library_functions.cl");
+  src_paths.push_back(source_dir + "ray_bundle_library_opt.cl");
+  src_paths.push_back(source_dir + "bit/compute_probability_of_image.cl");
+  src_paths.push_back(source_dir + "update_cubic_functors.cl");
+  src_paths.push_back(source_dir + "bit/cast_ray_bit.cl");
+
+  //set kernel options
+  opts += " -D PROB_CUBIC_IMAGE  ";
+  vcl_string options=opts;
+
+  opts += " -D STEP_CELL=step_cell_cubic_compute_probability_of_intensity(aux_args,data_ptr,d*linfo->block_len,vis,aux_args.prob_image) ";
+
+    //have kernel construct itself using the context and device
+    bocl_kernel * ray_trace_kernel=new bocl_kernel();
+
+    ray_trace_kernel->create_kernel( &device->context(),
+                                     device->device_id(),
+                                     src_paths,
+                                     "cubic_compute_probability_of_image",   //kernel name
+                                     opts,              //options
+                                     "boxm2 ocl probability computation"); //kernel identifier (for error checking)
+    vec_kernels.push_back(ray_trace_kernel);
+    //create normalize image kernel
+    vcl_vector<vcl_string> norm_src_paths;
+    norm_src_paths.push_back(source_dir + "pixel_conversion.cl");
+    norm_src_paths.push_back(source_dir + "bit/normalize_kernels.cl");
+    bocl_kernel * normalize_render_kernel=new bocl_kernel();
+
+    normalize_render_kernel->create_kernel( &device->context(),
+                                            device->device_id(),
+                                            norm_src_paths,
+                                            "normalize_probability_image_kernel",   //kernel name
+                                            "-D PROB_IMAGE",              //options
+                                            "normalize probability image kernel"); //kernel identifier (for error checking)
+
+    vec_kernels.push_back(normalize_render_kernel);
+  }
+}
+
+bool boxm2_ocl_probability_of_image_wcubic_process_cons(bprb_func_process& pro)
+{
+  using namespace boxm2_ocl_probability_of_image_wcubic_process_globals;
+
+  //process takes 1 input
+  vcl_vector<vcl_string> input_types_(n_inputs_);
+  input_types_[0] = "bocl_device_sptr";
+  input_types_[1] = "boxm2_scene_sptr";
+  input_types_[2] = "boxm2_opencl_cache_sptr";
+  input_types_[3] = "vpgl_camera_double_sptr";
+  input_types_[4] = "vil_image_view_base_sptr";
+  input_types_[5] = "vcl_string";  // model identifier
+  input_types_[6] = "vcl_string";  // aux identifier
+
+  // process has 1 output:
+  // output[0]: scene sptr
+  vcl_vector<vcl_string>  output_types_(n_outputs_);
+  output_types_[0] = "vil_image_view_base_sptr";
+  
+  brdb_value_sptr idx        = new brdb_value_t<vcl_string>("");
+  pro.set_input(5, idx);
+  return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
+}
+
+bool boxm2_ocl_probability_of_image_wcubic_process(bprb_func_process& pro)
+{
+  using namespace boxm2_ocl_probability_of_image_wcubic_process_globals;
+  vcl_size_t local_threads[2]={8,8};
+  vcl_size_t global_threads[2]={8,8};
+
+  if ( pro.n_inputs() < n_inputs_ ) {
+    vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
+    return false;
+  }
+  float transfer_time=0.0f;
+  float gpu_time=0.0f;
+  //get the inputs
+  unsigned i = 0;
+  bocl_device_sptr device= pro.get_input<bocl_device_sptr>(i++);
+  boxm2_scene_sptr scene =pro.get_input<boxm2_scene_sptr>(i++);
+  boxm2_opencl_cache_sptr opencl_cache= pro.get_input<boxm2_opencl_cache_sptr>(i++);
+  vpgl_camera_double_sptr cam= pro.get_input<vpgl_camera_double_sptr>(i++);
+  vil_image_view_base_sptr img =pro.get_input<vil_image_view_base_sptr>(i++);
+  vcl_string data_identifier =pro.get_input<vcl_string>(i++);
+  vcl_string image_identifier =pro.get_input<vcl_string>(i++);
+  unsigned ni=img->ni();
+  unsigned nj=img->nj();
+  bool foundDataType = false, foundNumObsType = false;
+  vcl_string data_type,options;
+  vcl_vector<vcl_string> apps = scene->appearances();
+  for (unsigned int i=0; i<apps.size(); ++i) {
+    if ( apps[i] == boxm2_data_traits<BOXM2_FLOAT8>::prefix() )
+    {
+      data_type = apps[i];
+      foundDataType = true;
+      options=" -D FLOAT8 ";
+    }
+  }
+  if (!foundDataType) {
+    vcl_cout<<"boxm2_ocl_probability_of_image_wcubic_process ERROR: scene doesn't have BOXM2_MOG3_GREY or BOXM2_MOG3_GREY_16 data type"<<vcl_endl;
+    return false;
+  }
+
+  if(data_identifier!="")
+      data_type = data_type+"_"+data_identifier;
+
+//: create a command queue.
+  int status=0;
+  cl_command_queue queue = clCreateCommandQueue(device->context(),*(device->device_id()),
+                                                CL_QUEUE_PROFILING_ENABLE,&status);
+  if (status!=0) return false;
+
+  vcl_string identifier=device->device_identifier()+options;
+  // compile the kernel
+  if (kernels.find(identifier)==kernels.end())
+  {
+    vcl_cout<<"===========Compiling kernels==========="<<vcl_endl;
+    vcl_vector<bocl_kernel*> ks;
+    compile_kernel(device,ks,options);
+    kernels[identifier]=ks;
+  }
+
+  //set generic cam
+
+  vil_image_view_base_sptr float_img=boxm2_util::prepare_input_image(img);
+  vil_image_view<float>* img_view = static_cast<vil_image_view<float>* >(float_img.ptr());
+
+  unsigned cl_ni=RoundUp(img_view->ni(),local_threads[0]);
+  unsigned cl_nj=RoundUp(img_view->nj(),local_threads[1]);
+
+  cl_float* ray_origins = new cl_float[4*cl_ni*cl_nj];
+  cl_float* ray_directions = new cl_float[4*cl_ni*cl_nj];
+  bocl_mem_sptr ray_o_buff = new bocl_mem(device->context(), ray_origins, cl_ni*cl_nj * sizeof(cl_float4) , "ray_origins buffer");
+  bocl_mem_sptr ray_d_buff = new bocl_mem(device->context(), ray_directions,  cl_ni*cl_nj * sizeof(cl_float4), "ray_directions buffer");
+  boxm2_ocl_camera_converter::compute_ray_image( device, queue, cam, cl_ni, cl_nj, ray_o_buff, ray_d_buff);
+
+  global_threads[0]=cl_ni;
+  global_threads[1]=cl_nj;
+  float* vis_buff = new float[cl_ni*cl_nj];
+  float* prob_image_buff = new float[cl_ni*cl_nj];
+  for (unsigned i=0;i<cl_ni*cl_nj;i++)
+  {
+      vis_buff[i]=1.0f;
+      prob_image_buff[i]=0.0f;
+  }
+  int count=0;
+
+  bocl_mem_sptr prob_image=new bocl_mem(device->context(),prob_image_buff,cl_ni*cl_nj*sizeof(float),"expected image buffer");
+  prob_image->create_buffer(CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR);
+
+  bocl_mem_sptr vis_image=new bocl_mem(device->context(),vis_buff,cl_ni*cl_nj*sizeof(float),"vis image buffer");
+  vis_image->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+
+  // Image Dimensions
+  int img_dim_buff[4];
+  img_dim_buff[0] = 0;
+  img_dim_buff[1] = 0;
+  img_dim_buff[2] = img_view->ni();
+  img_dim_buff[3] = img_view->nj();
+  bocl_mem_sptr img_dim=new bocl_mem(device->context(), img_dim_buff, sizeof(int)*4, "image dims");
+  img_dim->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+
+  // Output Array
+  float output_arr[100];
+  for (int i=0; i<100; ++i) output_arr[i] = 0.0f;
+  bocl_mem_sptr  cl_output=new bocl_mem(device->context(), output_arr, sizeof(float)*100, "output buffer");
+  cl_output->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+
+  // bit lookup buffer
+  cl_uchar lookup_arr[256];
+  boxm2_ocl_util::set_bit_lookup(lookup_arr);
+  bocl_mem_sptr lookup=new bocl_mem(device->context(), lookup_arr, sizeof(cl_uchar)*256, "bit lookup buffer");
+  lookup->create_buffer(CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR);
+
+  //For each ID in the visibility order, grab that block
+  vcl_vector<boxm2_block_id> vis_order = scene->get_vis_blocks(  (vpgl_perspective_camera<double>*) cam.ptr());
+  vcl_vector<boxm2_block_id>::iterator id;
+  for (id = vis_order.begin(); id != vis_order.end(); ++id)
+  {
+    //choose correct render kernel
+    boxm2_block_metadata mdata = scene->get_block_metadata(*id);
+    bocl_kernel* kern =  kernels[identifier][0];
+    //write the image values to the buffer
+    vul_timer transfer;
+    bocl_mem* blk       = opencl_cache->get_block(*id);
+    bocl_mem* blk_info  = opencl_cache->loaded_block_info();
+    bocl_mem* alpha     = opencl_cache->get_data<BOXM2_ALPHA>(*id);
+    int mogTypeSize    = (int) boxm2_data_info::datasize(data_type);
+    boxm2_scene_info* info_buffer = (boxm2_scene_info*) blk_info->cpu_buffer();
+    int alphaTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_ALPHA>::prefix());
+    info_buffer->data_buffer_length = (int) (alpha->num_bytes()/alphaTypeSize);
+    blk_info->write_to_buffer((queue));
+    
+    bocl_mem* mog       = opencl_cache->get_data(*id,data_type,alpha->num_bytes()/alphaTypeSize*mogTypeSize,false);    //info_buffer->data_buffer_length*boxm2_data_info::datasize(data_type));
+    vcl_string aux0_datatype = boxm2_data_traits<BOXM2_AUX0>::prefix(image_identifier);
+    bocl_mem* aux0       = opencl_cache->get_data(*id,aux0_datatype,alpha->num_bytes(),false);    
+    vcl_string aux1_datatype = boxm2_data_traits<BOXM2_AUX1>::prefix(image_identifier);
+    bocl_mem* aux1       = opencl_cache->get_data(*id,aux1_datatype,alpha->num_bytes(),false);    
+
+    transfer_time += (float) transfer.all();
+
+    ////3. SET args
+    kern->set_arg( blk_info );
+    kern->set_arg( blk );
+    kern->set_arg( alpha );
+    kern->set_arg( mog );
+    kern->set_arg( aux0 );
+    kern->set_arg( aux1 );
+    kern->set_arg( ray_o_buff.ptr() );
+    kern->set_arg( ray_d_buff.ptr() );
+    kern->set_arg( prob_image.ptr() );
+    kern->set_arg( vis_image.ptr() );
+    kern->set_arg( img_dim.ptr());
+    kern->set_arg( cl_output.ptr() );
+    kern->set_arg( lookup.ptr() );
+
+    //local tree , cumsum buffer, imindex buffer
+    kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_uchar16) );
+    kern->set_local_arg( local_threads[0]*local_threads[1]*10*sizeof(cl_uchar) );
+    kern->set_local_arg( local_threads[0]*local_threads[1]*sizeof(cl_int) );
+
+    //execute kernel
+    kern->execute(queue, 2, local_threads, global_threads);
+    clFinish(queue);
+    gpu_time += kern->exec_time();
+
+    //clear render kernel args so it can reset em on next execution
+    kern->clear_args();
+  }
+
+#if 1 
+  // normalize
+  bocl_kernel* normalize_prob_image_kernel =  kernels[identifier][1];
+  {
+    normalize_prob_image_kernel->set_arg( prob_image.ptr() );
+    normalize_prob_image_kernel->set_arg( vis_image.ptr() );
+    normalize_prob_image_kernel->set_arg( img_dim.ptr());
+    normalize_prob_image_kernel->execute( queue, 2, local_threads, global_threads);
+    clFinish(queue);
+    gpu_time += normalize_prob_image_kernel->exec_time();
+
+    //clear render kernel args so it can reset em on next execution
+    normalize_prob_image_kernel->clear_args();
+  }
+#endif
+  // read out expected image
+  prob_image->read_to_buffer(queue);
+  vis_image->read_to_buffer(queue);
+   clFinish(queue);
+  vil_image_view<float>* prob_img_out=new vil_image_view<float>(ni,nj);
+
+  for (unsigned c=0;c<nj;c++)
+    for (unsigned r=0;r<ni;r++)
+      (*prob_img_out)(r,c)=prob_image_buff[c*cl_ni+r];
+
+  delete [] prob_image_buff;
+  delete [] vis_buff;
+
+  clReleaseCommandQueue(queue);
+  i=0;
+  // store scene smaprt pointer
+  pro.set_output_val<vil_image_view_base_sptr>(i++, prob_img_out);
+  return true;
+}
