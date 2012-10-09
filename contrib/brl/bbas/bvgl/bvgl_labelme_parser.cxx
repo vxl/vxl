869c513419c36836c92ac9c1be028c5814a80807
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-10-09 18:46:23 -0400

    avoid compiler warning (comparison signed/unsigned)
    SVN-Revision: 35728

--- contrib/brl/bbas/bvgl/bvgl_labelme_parser.cxx
+++ contrib/brl/bbas/bvgl/bvgl_labelme_parser.cxx
@@ -10,7 +10,6 @@
 #include <vcl_cstdio.h>
 
 
-
 //Constructor from file
 bvgl_labelme_parser::bvgl_labelme_parser(vcl_string& filename)
 {
@@ -41,12 +40,12 @@ void
 bvgl_labelme_parser::startElement(const char* name, const char** atts)
 {
   //set active tag for charData
-  active_tag_ = vcl_string(name); 
+  active_tag_ = vcl_string(name);
 
   //parse object/polygon, start with a fresh set of points
-  if(vcl_strcmp(name, POLYGON_TAG)==0) 
+  if (vcl_strcmp(name, POLYGON_TAG)==0)
     pts_.clear();
-    
+
   if (vcl_strcmp(name, OBJECT_TAG)==0) {
     dist_ = -1;
     order_ = -1;
@@ -57,19 +56,19 @@ bvgl_labelme_parser::startElement(const char* name, const char** atts)
 void bvgl_labelme_parser::endElement(const XML_Char* name)
 {
   //Finish up polygon
-  if(vcl_strcmp(name, POLYGON_TAG)==0) {
-    vgl_polygon<double> poly(pts_); 
+  if (vcl_strcmp(name, POLYGON_TAG)==0) {
+    vgl_polygon<double> poly(pts_);
     polygons_.push_back(poly);
   }
 
   //finish up a point
-  if(vcl_strcmp(name, Y_TAG)==0) {
+  if (vcl_strcmp(name, Y_TAG)==0) {
     vgl_point_2d<double> pt(x_, y_);
     pts_.push_back(pt);
   }
-  
+
   //finish up an object
-  if(vcl_strcmp(name, OBJECT_TAG)==0) {
+  if (vcl_strcmp(name, OBJECT_TAG)==0) {
     obj_dists_.push_back(dist_);
     obj_depth_orders_.push_back(order_);
   }
@@ -79,51 +78,53 @@ void bvgl_labelme_parser::endElement(const XML_Char* name)
 //Grabs data from points
 void bvgl_labelme_parser::charData(const XML_Char* s, int len)
 {
-  if(active_tag_ == X_TAG || active_tag_ == Y_TAG) {
-    int val; 
+  if (active_tag_ == X_TAG || active_tag_ == Y_TAG) {
+    int val;
     convert(vcl_string(s,len), val);
-    if(active_tag_ == X_TAG)
+    if (active_tag_ == X_TAG)
       x_ = (double) val;
-    if(active_tag_ == Y_TAG) 
+    if (active_tag_ == Y_TAG)
       y_ = (double) val;
   }
 
-  if(active_tag_ == FILENAME_TAG)
+  if (active_tag_ == FILENAME_TAG)
     image_name_ = vcl_string(s, len);
 
-  if(active_tag_ == NAME_TAG) {
+  if (active_tag_ == NAME_TAG) {
     vcl_string name = vcl_string(s,len);
     this->trim_string(name);
     if (name.length() != 0)
       obj_names_.push_back(name);
   }
-  
-  if (active_tag_ == OBJECT_MINDIST_TAG) 
+
+  if (active_tag_ == OBJECT_MINDIST_TAG)
     convert(vcl_string(s,len), dist_);
-    
-  if (active_tag_ == OBJECT_ORDER_TAG) 
+
+  if (active_tag_ == OBJECT_ORDER_TAG)
     convert(vcl_string(s,len), order_);
-    
 }
 
 void bvgl_labelme_parser::trim_string(vcl_string& s)
 {
   char trims[4];
   trims[0] = ' '; trims[1] = '\0'; trims[2] = '\n'; trims[3] = '\t';
-  //int i = (int)s.find_first_not_of(' ');
-  //int j = (int)s.find_last_not_of(' ');
-  //vcl_string t = s.substr(i,j-i+1);
+#if 0
+  int i = (int)s.find_first_not_of(' ');
+  int j = (int)s.find_last_not_of(' ');
+  vcl_string t = s.substr(i,j-i+1);
+#endif
   vcl_string t = s;
   bool trimmed = true;
   while (trimmed) {
     trimmed = false;
     for (unsigned kk = 0; kk < 4; kk++) {
       vcl_string current = t;
-      int i = (int)current.find_first_not_of(trims[kk]);
-      int j = (int)current.find_last_not_of(trims[kk]);
-      if (i < 0 || j < 0 || j >= current.size()) {
+      unsigned int i = (unsigned int)current.find_first_not_of(trims[kk]);
+      unsigned int j = (unsigned int)current.find_last_not_of(trims[kk]);
+      if (i > j || j >= current.size()) {
         t = ""; break;
-      } else {
+      }
+      else {
         t = current.substr(i,j-i+1);
         if (t.size() != current.size())
           trimmed = true;
