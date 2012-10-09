c5e5cf5fe7788a9b5f94c730ebe7d1f02615530e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-10-09 18:51:05 -0400

    added test_include.cxx
    SVN-Revision: 35729

---
+++ contrib/brl/bbas/bpgl/depth_map/tests/test_include.cxx
@@ -0,0 +1,6 @@
+#include <bpgl/depth_map/depth_map_region.h>
+#include <bpgl/depth_map/depth_map_region_sptr.h>
+#include <bpgl/depth_map/depth_map_scene.h>
+#include <bpgl/depth_map/depth_map_scene_sptr.h>
+
+int main() { return 0; }
