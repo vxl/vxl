260695637b3328830c07b4be91b36c5351ec13e2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-10-16 17:57:27 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 35799

--- contrib/brl/bseg/boxm2/volm/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/volm/tests/test_include.cxx
@@ -1,3 +1,6 @@
-#include <boxm2/reg/boxm2_reg_mutual_info.h>
+#include <boxm2/volm/boxm2_volm_locations.h>
+#include <boxm2/volm/boxm2_volm_locations_sptr.h>
+#include <boxm2/volm/boxm2_volm_matcher.h>
+#include <boxm2/volm/boxm2_volm_wr3db_index.h>
 
 int main() { return 0; }
