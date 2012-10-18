4f9c1645d45a1bcd5c77c38b0f11b846b535fae7
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-10-18 12:07:14 -0400

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 35815

--- contrib/brl/bbas/volm/tests/test_include.cxx
+++ contrib/brl/bbas/volm/tests/test_include.cxx
@@ -1,6 +1,8 @@
+#include <volm/volm_query_sptr.h>
+#include <volm/volm_spherical_container_sptr.h>
+//
 #include <volm/volm_io.h>
 #include <volm/volm_query.h>
-#include <volm/volm_query_sptr.h>
 #include <volm/volm_spherical_container.h>
 #include <volm/volm_tile.h>
 
--- contrib/brl/bseg/boxm2/volm/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/volm/tests/test_include.cxx
@@ -1,5 +1,7 @@
-#include <boxm2/volm/boxm2_volm_locations.h>
 #include <boxm2/volm/boxm2_volm_locations_sptr.h>
+#include <boxm2/volm/boxm2_volm_wr3db_index_sptr.h>
+//
+#include <boxm2/volm/boxm2_volm_locations.h>
 #include <boxm2/volm/boxm2_volm_matcher.h>
 #include <boxm2/volm/boxm2_volm_wr3db_index.h>
 
