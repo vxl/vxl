c0ffc7022e2572673957e89f6ddb5d8a37f1586b
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-10-18 12:01:53 -0400

    minor correction in Doxygen documentation for get_uncertainty_region()
    SVN-Revision: 35813

--- contrib/brl/bbas/volm/volm_tile.h
+++ contrib/brl/bbas/volm/volm_tile.h
@@ -4,7 +4,7 @@
 //:
 // \file
 // \brief A class to hold volumetric matchers
-// units are in meters
+// Units are in meters
 //
 // \author Ozge C. Ozcanli
 // \date September 18, 2012
@@ -26,23 +26,23 @@ class volm_tile
    unsigned ni_;
    unsigned nj_;
    vpgl_geo_camera cam_; // the output of this camera cannot be directly used, if 'W' use -lon
-   
+
    volm_tile(float lat, float lon, char hemisphere, char direction, float scale_i, float scale_j, unsigned ni, unsigned nj);
    vcl_string get_string();
-   
+
    void img_to_global(unsigned i, unsigned j, double& lon, double& lat);
-   
+
    //: create a kml file of the tile as a box and with circular marks throughout at every n pixels in each direction
    void write_kml(vcl_string name, int n);
-   
+
    //: mark the uncertainty region around a given location using a gaussian mask, the center has the designated score, the rest diminishes with respect to a cutoff degree
-   //  lambda is the radius of the gaussian mask in pixels
-   //  cut_off is the percentage that shows how quickly the kernel dies off, typically 50
+   //  \p lambda_i and \p lambda_j are the radii of the gaussian mask in pixels
+   //  \p cut_off is the percentage that shows how quickly the kernel dies off, typically 50
    static void get_uncertainty_region(float lambda_i, float lambda_j, float cut_off, vbl_array_2d<bool>& mask, vbl_array_2d<float>& kernel);
    static void mark_uncertainty_region(int i, int j, float score, vbl_array_2d<bool>& mask, vbl_array_2d<float>& kernel, vil_image_view<unsigned int>& img);
-   
-   //static volm_tile parse_string(vcl_string& filename);
-   
+#if 0
+   static volm_tile parse_string(vcl_string& filename);
+#endif
    static vcl_vector<volm_tile> generate_p1_tiles();
    static vcl_vector<volm_tile> generate_p1_wr1_tiles();
 };
