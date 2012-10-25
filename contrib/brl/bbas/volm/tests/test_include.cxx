c8f634678cf819bdafe8ad28cbbcca8a806acd0d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2012-10-25 11:35:56 -0400

    updated w.r.t. CMakelists.txt
    SVN-Revision: 35850

--- contrib/brl/bbas/volm/tests/test_include.cxx
+++ contrib/brl/bbas/volm/tests/test_include.cxx
@@ -1,9 +1,11 @@
 #include <volm/volm_query_sptr.h>
 #include <volm/volm_spherical_container_sptr.h>
+#include <volm/volm_spherical_shell_container_sptr.h>
 //
 #include <volm/volm_io.h>
 #include <volm/volm_query.h>
 #include <volm/volm_spherical_container.h>
+#include <volm/volm_spherical_shell_container.h>
 #include <volm/volm_tile.h>
 
 int main() { return 0; }
