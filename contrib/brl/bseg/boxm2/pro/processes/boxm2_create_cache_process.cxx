d60af52d83759f972d079bf079557dbdaf31e1f4
Vishal Jain <jainv@users.sourceforge.net>
2012-10-31 15:28:47 -0400

    added bool variable to mention hdfs or local file system
    SVN-Revision: 35866

--- contrib/brl/bseg/boxm2/pro/processes/boxm2_create_cache_process.cxx
+++ contrib/brl/bseg/boxm2/pro/processes/boxm2_create_cache_process.cxx
@@ -18,7 +18,7 @@
 
 namespace boxm2_create_cache_process_globals
 {
-  const unsigned n_inputs_ = 2;
+  const unsigned n_inputs_ = 3;
   const unsigned n_outputs_ = 1;
 }
 bool boxm2_create_cache_process_cons(bprb_func_process& pro)
@@ -29,12 +29,13 @@ bool boxm2_create_cache_process_cons(bprb_func_process& pro)
   vcl_vector<vcl_string> input_types_(n_inputs_);
   input_types_[0] = "boxm2_scene_sptr";
   input_types_[1] = "vcl_string";
-
+  input_types_[2] = "bool";
   // process has 1 output:
   // output[0]: scene sptr
   vcl_vector<vcl_string>  output_types_(n_outputs_);
   output_types_[0] = "boxm2_cache_sptr";
-
+  brdb_value_sptr idx = new brdb_value_t<bool>(true);
+  pro.set_input(2, idx);
   return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
 }
 
@@ -50,10 +51,15 @@ bool boxm2_create_cache_process(bprb_func_process& pro)
   unsigned i = 0;
   boxm2_scene_sptr scene= pro.get_input<boxm2_scene_sptr>(i++);
   vcl_string cache_type= pro.get_input<vcl_string>(i++);
+  bool islocal= pro.get_input<bool>(i++);
   if(cache_type=="lru")
   {
       vcl_cout<<"Create Cache"<<vcl_endl;
-      boxm2_lru_cache::create(scene);
+      if(!islocal)
+          boxm2_lru_cache::create(scene,HDFS);
+      else
+          boxm2_lru_cache::create(scene,LOCAL);
+
   }
   else if (cache_type=="nn")
   {
