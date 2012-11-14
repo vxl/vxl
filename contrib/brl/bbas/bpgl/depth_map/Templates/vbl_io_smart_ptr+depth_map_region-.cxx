85fe31228e18326b712686f913da879e1d3dce5c
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2012-11-14 12:11:21 -0500

    adding match and depth image generation methods
    SVN-Revision: 35967

--- contrib/brl/bbas/bpgl/depth_map/Templates/vbl_io_smart_ptr+depth_map_region-.cxx
+++ contrib/brl/bbas/bpgl/depth_map/Templates/vbl_io_smart_ptr+depth_map_region-.cxx
@@ -5,7 +5,9 @@
 #include <vsol/vsol_box_2d.h>
 #include <vsol/vsol_polygon_2d.h>
 #include <depth_map/depth_map_region.h>
+#include <depth_map/depth_map_scene.h>
 
 #include <vbl/io/vbl_io_smart_ptr.txx>
 
 VBL_IO_SMART_PTR_INSTANTIATE(depth_map_region);
+VBL_IO_SMART_PTR_INSTANTIATE(depth_map_scene);
