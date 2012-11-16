ed2ef8cf88cc6592ebe5e25217f72a6f791a8ec5
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2012-11-16 10:24:18 -0500

    adding lidar to xyz process
    SVN-Revision: 36009

--- contrib/brl/bseg/boxm2/pro/boxm2_processes.h
+++ contrib/brl/bseg/boxm2/pro/boxm2_processes.h
@@ -47,4 +47,9 @@ DECLARE_FUNC_CONS(boxm2_add_aux_info_to_ply_process);
 DECLARE_FUNC_CONS(boxm2_scene_kml_process);
 DECLARE_FUNC_CONS(boxm2_distribute_scene_blocks_process);
 DECLARE_FUNC_CONS(boxm2_ingest_convex_mesh_process);
+DECLARE_FUNC_CONS(boxm2_lidar_to_xyz_process);
+DECLARE_FUNC_CONS(boxm2_dem_to_xyz_process2);
+
+//DECLARE_FUNC_CONS(boxm2_scene_fetch_alpha_process);
+//DECLARE_FUNC_CONS(boxm2_create_crop_img_process);
 #endif
