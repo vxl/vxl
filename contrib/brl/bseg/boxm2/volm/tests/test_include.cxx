a245d6ccfeacb9fd541618518c2622cecb8293c8
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-01-07 08:21:13 -0500

    volm_pro depends on volm, but does not require OPENCL; but volm required OPENCL, which caused build errors when no OPENCL. Therefore, made sure the volm library builds anyhow (also without OPENCL), possibly with less source files
    SVN-Revision: 36184

--- contrib/brl/bseg/boxm2/volm/tests/test_include.cxx
+++ contrib/brl/bseg/boxm2/volm/tests/test_include.cxx
@@ -3,8 +3,10 @@
 //
 #include <boxm2/volm/boxm2_volm_io.h>
 #include <boxm2/volm/boxm2_volm_locations.h>
+#ifdef HAS_OPENCL
 #include <boxm2/volm/boxm2_volm_matcher.h>
 #include <boxm2/volm/boxm2_volm_matcher_order.h>
+#endif
 #include <boxm2/volm/boxm2_volm_wr3db_index.h>
 
 int main() { return 0; }
