9232308d00c864036014093d56e76c29498196a4
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-01-11 04:55:56 -0500

    removed unused "#include <vil/vil_convert.h>"
    SVN-Revision: 36217

--- contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_compute_expectation_per_view_process.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_compute_expectation_per_view_process.cxx
@@ -25,7 +25,6 @@
 #include <boxm2/boxm2_util.h>
 #include <vil/vil_image_view.h>
 #include <boxm2/ocl/algo/boxm2_ocl_camera_converter.h>
-#include <vil/vil_convert.h>
 
 //brdb stuff
 #include <brdb/brdb_value.h>
