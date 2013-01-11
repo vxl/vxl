06b7f797794faf5c2bf744916f131bc74485913f
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-01-11 08:34:59 -0500

    removed unused "#include <vpgl/vpgl_perspective_camera.h>"
    SVN-Revision: 36262

--- contrib/brl/bbas/volm/volm_io.h
+++ contrib/brl/bbas/volm/volm_io.h
@@ -14,7 +14,6 @@
 // \endverbatim
 
 #include <vcl_string.h>
-#include <vpgl/vpgl_perspective_camera.h>
 #include <bpgl/depth_map/depth_map_scene_sptr.h>
 #include <bpgl/depth_map/depth_map_scene.h>
 #include <vcl_set.h>
@@ -37,15 +36,16 @@ class volm_io
   //: return true if MATCHER_EXE_FINISHED, otherwise return false
   static bool check_matcher_status(vcl_string out_folder);
 
-  static bool read_camera(vcl_string kml_file, unsigned const& ni, unsigned const& nj,
-                                               double& heading,   double& heading_dev,
-                                               double& tilt,      double& tilt_dev,
-                                               double& roll,      double& roll_dev,
-                                               double& top_fov,   double& top_fov_dev,
-                                               double& altitude, double& lat, double& lon);
+  static bool read_camera(vcl_string kml_file,
+                          unsigned const& ni, unsigned const& nj,
+                          double& heading,   double& heading_dev,
+                          double& tilt,      double& tilt_dev,
+                          double& roll,      double& roll_dev,
+                          double& top_fov,   double& top_fov_dev,
+                          double& altitude, double& lat, double& lon);
 
   static bool read_labelme(vcl_string xml_file, depth_map_scene_sptr& depth_scene, vcl_string& img_category);
-  
+
   //: piecewise linear s.t. [1,127) -> [0,t), [127,255] -> [t,1]
   static float scale_score_to_0_1(unsigned char pix_value, float threshold);
   //: piecewise linear s.t. [0,t) -> [1,127), [t,1] -> [127,255]"
@@ -55,8 +55,9 @@ class volm_io
 class volm_rationale;
 bool operator>(const vcl_pair<float, volm_rationale>& a, const vcl_pair<float, volm_rationale>& b);
 
-class volm_rationale {
-public:
+class volm_rationale
+{
+ public:
   float lat;
   float lon;
   float elev;
@@ -64,11 +65,9 @@ public:
   unsigned cam_id;   // index for camera in volm_query
   vcl_string index_file;
   vcl_string score_file;
-  
+
   static bool write_top_matches(vcl_multiset<vcl_pair<float, volm_rationale>, std::greater<vcl_pair<float, volm_rationale> > >& top_matches, vcl_string& filename);
   static bool read_top_matches(vcl_multiset<vcl_pair<float, volm_rationale>, std::greater<vcl_pair<float, volm_rationale> > >& top_matches, vcl_string& filename);
-  
-
 };
 
 
