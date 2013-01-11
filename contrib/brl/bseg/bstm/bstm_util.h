05ea912e2b31f49c260f7e419d928a25076dc14e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-01-11 06:36:04 -0500

    replaced "#include <vgl/vgl_box_3d.h>" (unused) by "#include <vgl/vgl_point_3d.h>"
    SVN-Revision: 36253

--- contrib/brl/bseg/bstm/bstm_util.h
+++ contrib/brl/bseg/bstm/bstm_util.h
@@ -3,7 +3,7 @@
 //:
 // \file
 #include <vcl_iostream.h>
-#include <vgl/vgl_box_3d.h>
+#include <vgl/vgl_point_3d.h>
 #include <vil/vil_image_view.h>
 #include <vil/vil_load.h>
 #include <bstm/bstm_scene.h>
@@ -19,8 +19,8 @@ class bstm_util
     static bool query_point(bstm_scene_sptr& scene, bstm_cache_sptr& cache, const vgl_point_3d<double>& point, unsigned time, float& prob, float& intensity);
 
     static bool query_point_color(bstm_scene_sptr& scene, bstm_cache_sptr& cache,
-                                    const vgl_point_3d<double>& point, unsigned time,
-                                    float& prob, vnl_vector_fixed< unsigned char, 3>& rgb);
+                                  const vgl_point_3d<double>& point, unsigned time,
+                                  float& prob, vnl_vector_fixed< unsigned char, 3>& rgb);
 
     //verifies that a scene has a valid appearance, spits out data type and appearance type size
     static bool verify_appearance(bstm_scene& scene, const vcl_vector<vcl_string>&valid_types, vcl_string& data_type, int& appTypeSize );
@@ -30,7 +30,6 @@ class bstm_util
       vil_image_view_base_sptr loaded_image = vil_load(filename.c_str());
       return bstm_util::prepare_input_image(loaded_image,force_grey);
     }
-
 };
 
 #endif // bstm_util_h
