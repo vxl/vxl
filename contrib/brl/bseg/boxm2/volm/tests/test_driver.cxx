cbc08d12705a7ccaf7a112e6be459d2fd650c966
Yi Dong <yidong80@users.sourceforge.net>
2013-01-15 14:14:12 -0500

    update adding test for obj_based_matcher
    SVN-Revision: 36270

--- contrib/brl/bseg/boxm2/volm/tests/test_driver.cxx
+++ contrib/brl/bseg/boxm2/volm/tests/test_driver.cxx
@@ -2,11 +2,13 @@
 
 DECLARE( test_volm_wr3db_index );
 DECLARE( test_volm_locations );
+DECLARE( test_volm_obj_based_matcher );
 
 void register_tests()
 {
   REGISTER( test_volm_locations );
   REGISTER( test_volm_wr3db_index );
+  REGISTER( test_volm_obj_based_matcher );
 }
 
 
