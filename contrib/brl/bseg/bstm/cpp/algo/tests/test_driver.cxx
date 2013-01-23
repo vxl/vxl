6f3559042ee18db515d30ca4c63bab783e919813
Ali Osman Ulusoy <aulusoy@users.sourceforge.net>
2013-01-22 23:28:43 -0500

    removed invalid tests.
    SVN-Revision: 36315

--- contrib/brl/bseg/bstm/cpp/algo/tests/test_driver.cxx
+++ contrib/brl/bseg/bstm/cpp/algo/tests/test_driver.cxx
@@ -2,12 +2,11 @@
 
 
 DECLARE( test_time_tree_ingestion);
-DECLARE( test_boxm2_ingestion);
 
 void register_tests()
 {
   REGISTER( test_time_tree_ingestion );
-  REGISTER( test_boxm2_ingestion);
+
 
 }
 
