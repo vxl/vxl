7ba61f2938617e5549a1725cf30134386d7cac5e
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-01-27 16:24:18 -0500

    avoid compiler warning (comparison signed/unsigned)
    SVN-Revision: 36382

--- contrib/brl/bseg/bstm/pro/processes/bstm_scene_statistics_process.cxx
+++ contrib/brl/bseg/bstm/pro/processes/bstm_scene_statistics_process.cxx
@@ -29,7 +29,7 @@ bool bstm_scene_statistics_process_cons(bprb_func_process& pro)
 {
   using namespace bstm_scene_statistics_process_globals;
 
-  //process takes 1 input
+  //process takes 1 inputs
   vcl_vector<vcl_string> input_types_(n_inputs_);
   input_types_[0] = "bstm_scene_sptr";
   input_types_[1] = "bstm_cache_sptr";
@@ -67,7 +67,7 @@ bool bstm_scene_statistics_process(bprb_func_process& pro)
   //get blocks
   vcl_map<bstm_block_id, bstm_block_metadata> blocks = scene->blocks();
   vcl_map<bstm_block_id, bstm_block_metadata>::const_iterator bstm_iter = blocks.begin();
-  for(; bstm_iter != blocks.end(); bstm_iter++)
+  for (; bstm_iter != blocks.end(); ++bstm_iter)
   {
     bstm_block_id bstm_id = bstm_iter->first;
     bstm_block_metadata bstm_metadata = bstm_iter->second;
@@ -93,7 +93,7 @@ bool bstm_scene_statistics_process(bprb_func_process& pro)
         if ( ((i==0) || curr_tree.bit_at(pi)) && !curr_tree.is_leaf(i)) //check if non-leaf cell
         {
           boxm2_array_1d<vnl_vector_fixed<unsigned char, 8> > time_trees = blk_t->get_cell_all_tt(curr_tree.get_data_index( i));
-          for(int t_idx = 0; t_idx < bstm_metadata.sub_block_num_t_;t_idx++, innercell_time_tree_cell_count++) {
+          for (unsigned int t_idx = 0; t_idx < bstm_metadata.sub_block_num_t_;++t_idx, ++innercell_time_tree_cell_count) {
             bstm_time_tree tree(time_trees[t_idx].data_block() );
             total_innercell_time_tree_cells += tree.num_cells();
           }
@@ -105,21 +105,18 @@ bool bstm_scene_statistics_process(bprb_func_process& pro)
     boxm2_array_1d<uchar8>::iterator time_trees_iter;
     for (time_trees_iter = time_trees.begin(); time_trees_iter != time_trees.end(); ++time_trees_iter)
     {
-        bstm_time_tree new_time_tree((unsigned char*) (*time_trees_iter).data_block(), bstm_metadata.max_level_t_);
-        average_time_tree_depths += new_time_tree.num_leaves();
-        num_time_trees++;
+      bstm_time_tree new_time_tree((unsigned char*) (*time_trees_iter).data_block(), bstm_metadata.max_level_t_);
+      average_time_tree_depths += new_time_tree.num_leaves();
+      ++num_time_trees;
     }
   }
 
-  vcl_cout << "Total number of leaf cells: " << total_leaf_cells << vcl_endl;
-  vcl_cout << "Total number of inner cells: " << total_inner_cells << vcl_endl;
-  vcl_cout << "Total number of cells: " << total_num_cells << vcl_endl;
-  //vcl_cout << "Average number of time tree cells of non-leaf octree cells: " << total_innercell_time_tree_cells / innercell_time_tree_cell_count << vcl_endl;
-  vcl_cout << "Total time tree number of cells: " << (average_time_tree_depths) << vcl_endl;
-  vcl_cout << "Average time tree number of cells: " << ((float)average_time_tree_depths)/num_time_trees << vcl_endl;
-
-
-
+  vcl_cout << "Total number of leaf cells: " << total_leaf_cells << '\n'
+           << "Total number of inner cells: " << total_inner_cells << '\n'
+           << "Total number of cells: " << total_num_cells << '\n'
+//         << "Average number of time tree cells of non-leaf octree cells: " << total_innercell_time_tree_cells / innercell_time_tree_cell_count << '\n'
+           << "Total time tree number of cells: " << (average_time_tree_depths) << '\n'
+           << "Average time tree number of cells: " << ((float)average_time_tree_depths)/num_time_trees << vcl_endl;
 
   return true;
 }
