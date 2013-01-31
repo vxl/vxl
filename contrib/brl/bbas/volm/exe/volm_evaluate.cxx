2b5df13d8abe7d3e6249bfe527229e6799d7e442
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-01-31 17:34:38 -0500

    avoid compiler warning (comparison signed/unsigned, and comparison of unsigned with 0)
    SVN-Revision: 36437

--- contrib/brl/bbas/volm/exe/volm_evaluate.cxx
+++ contrib/brl/bbas/volm/exe/volm_evaluate.cxx
@@ -1,10 +1,10 @@
 //:
 // \file
 // \brief executable to evaluate output tiles using the ground truth locations
-//        given a test image id, for all the available output prob maps, 
+//        given a test image id, for all the available output prob maps,
 //                                  find the number of pixels within/intersect the candidate list
 //                                                            below the given threshold, and report the value at the gt loc pixel
-//        in addition: 
+//        in addition:
 //               read the geo index, count the number of total hyps
 //                                   count the number of hyps in the leaves that are within or intersect the candidate list
 //               read the intermediate output binary files, count the number of total hyps which are below the given score threshold
@@ -20,7 +20,6 @@
 #include <volm/volm_camera_space.h>
 #include <vul/vul_arg.h>
 #include <vul/vul_file.h>
-#include <vul/vul_file_iterator.h>
 #include <vil/vil_image_view.h>
 #include <vil/vil_load.h>
 #include <bkml/bkml_parser.h>
@@ -51,17 +50,18 @@ int main(int argc,  char** argv)
   vcl_cout << " READ camera space, number of cams: " << camera_space->valid_indices().size() << vcl_endl;
 
   vcl_vector<vcl_pair<vgl_point_3d<double>, vcl_pair<vcl_string, vcl_string> > > samples;
-  int cnt = volm_io::read_gt_file(gt_file(), samples);
+  unsigned int cnt = volm_io::read_gt_file(gt_file(), samples);
   if (id() >= cnt) {
     vcl_cerr << "the file: " << gt_file() << " does not contain test id: " << id() << "!\n";
     return volm_io::EXE_ARGUMENT_ERROR;
-  } else 
+  }
+  else
     vcl_cout << "evaluating using gt id: " << id() << " with name: " << samples[id()].second.first << " type: " << samples[id()].second.second << " lat: " << samples[id()].first.y() << " lon: " << samples[id()].first.x() << " elev: " << samples[id()].first.z() << '\n';
-  
-  vcl_cout << "\n==================================================================================================\n";
-  vcl_cout << "\t\t  Evaluate output prob maps using pixel value threshold: " << threshold << '\n';
-  vcl_cout << "==================================================================================================\n " << vcl_endl;
- 
+
+  vcl_cout << "\n==================================================================================================\n"
+           << "\t\t  Evaluate output prob maps using pixel value threshold: " << threshold << '\n'
+           << "==================================================================================================\n" << vcl_endl;
+
 
   // read the output tiles
   unsigned cnt_below = 0;
@@ -70,51 +70,51 @@ int main(int argc,  char** argv)
   vcl_vector<volm_tile> tiles;
   if (samples[id()].second.second == "desert")
     tiles = volm_tile::generate_p1_wr1_tiles();
-  else 
+  else
     tiles = volm_tile::generate_p1_wr2_tiles();
   vcl_cout << " generated " << tiles.size() << " tiles for type: " << samples[id()].second.second << '\n';
 
   for (unsigned i = 0; i < tiles.size(); i++)
   {
-    vcl_string name = out() + "/" + "ProbMap_" + tiles[i].get_string() + ".tif"; 
-    if (!vul_file::exists(name)) 
+    vcl_string name = out() + "/" + "ProbMap_" + tiles[i].get_string() + ".tif";
+    if (!vul_file::exists(name))
     {
       vcl_cerr << " missing:\t " << name << "!\n";
       continue;
-    } else
+    }
+    else
       vcl_cout << " adding:\t " << name << " to evaluation!\n";
-      
+
     vil_image_view<vxl_byte> tile = vil_load(name.c_str());
     for (unsigned ii = 0; ii < tile.ni(); ii++)
-      for (unsigned jj = 0; jj < tile.nj(); jj++) 
+      for (unsigned jj = 0; jj < tile.nj(); jj++)
       {
         double tlat, tlon;
         tiles[i].img_to_global(ii, jj, tlon, tlat);
-        if (tile(ii, jj) == 0) 
+        if (tile(ii, jj) == 0)
           tot_pix_unevaluated++;
-        else 
+        else
         {
           tot_pix_count++;
           if (tile(ii, jj) < threshold)
             cnt_below++;
-        }  
+        }
       }
 
     unsigned u, v;
     tiles[i].global_to_img(samples[id()].first.x(),  samples[id()].first.y(), u, v);
-    if (u >= 0 && v >= 0 && u < tile.ni() && v < tile.nj()) 
+    if (u < tile.ni() && v < tile.nj())
       vcl_cout << "\t GT location: " << samples[id()].first.x() << ", " << samples[id()].first.y() << " is at pixel: " << u << ", " << v << " and has value: " << (int)tile(u, v) << vcl_endl;
   }
-  vcl_cout << "tot pixels unevaluated: " << tot_pix_unevaluated << '\n';
-  vcl_cout << "tot pixels evaluated: " << tot_pix_count << '\n';
-  vcl_cout << "tot pixels below threshold " << threshold << ": " << cnt_below << '\n';
-  vcl_cout << "so knocked out " << (float)cnt_below/tot_pix_count*100 << " percent of the evaluated ROI!\n";
- 
+  vcl_cout << "tot pixels unevaluated: " << tot_pix_unevaluated << '\n'
+           << "tot pixels evaluated: " << tot_pix_count << '\n'
+           << "tot pixels below threshold " << threshold << ": " << cnt_below << '\n'
+           << "so knocked out " << (float)cnt_below/tot_pix_count*100 << " percent of the evaluated ROI!\n\n"
 
-  vcl_cout << "\n==================================================================================================\n";
-  vcl_cout << "\t\t  Evaluate output score binaries using camera score threshold: " << cam_threshold() << '\n';
-  vcl_cout << "==================================================================================================\n " << vcl_endl;
- 
+           << "\n==================================================================================================\n"
+           << "\t\t  Evaluate output score binaries using camera score threshold: " << cam_threshold() << '\n'
+           << "==================================================================================================\n" << vcl_endl;
+
   unsigned cnt_hyp_below = 0;
   unsigned tot_hyp_count = 0;
   int cnt_cam_below = 0;
@@ -123,13 +123,14 @@ int main(int argc,  char** argv)
   // read the scores, assumes that score binaries for a given tile fits into memory
   for (unsigned i = 0; i < tiles.size(); i++) {
     vcl_stringstream score_file_name; score_file_name << local_folder() << "pass" << pass_id() << "_scores_tile_" << i << ".bin";
-    if (!vul_file::exists(score_file_name.str())) 
+    if (!vul_file::exists(score_file_name.str()))
     {
       vcl_cerr << " missing:\t " << score_file_name.str() << "!\n";
       continue;
-    } else
+    }
+    else
       vcl_cout << " adding:\t " << score_file_name.str() << " to evaluation!\n";
-    
+
     vcl_cout << " \t\t size of the file is: " << (float)(vul_file::size(score_file_name.str()))/(1024.0*1024.0*1024.0) << " Gb, assuming it fits memory!!\n";
     vcl_vector<volm_score_sptr> scores;
     volm_score::read_scores_tile(scores, score_file_name.str());  // this file may be too large, make sure it fits to memory!!
@@ -140,22 +141,22 @@ int main(int argc,  char** argv)
       if (scores[j]->max_score_ < cam_threshold()) {
         cnt_hyp_below++;
         cnt_cam_below += total_cams;  // all the cams are eliminated for this position then
-      } else
+      }
+      else
         cnt_cam_below += (total_cams - scores[j]->cam_id_.size());
     }
   }
 
-  vcl_cout << "total # of hypotheses in the output score file: " << tot_hyp_count << vcl_endl;
-  vcl_cout << "total # of hypotheses below threshold (" << cam_threshold() << "): " << cnt_hyp_below << vcl_endl;
-  vcl_cout << "  so previous pass knocked out: " << ((float)cnt_hyp_below/tot_hyp_count)*100.0 << " percent of locations!\n";
-  vcl_cout << "total # of cameras (total hyp*cam space size): " << tot_hyp_count*total_cams << vcl_endl;
-  vcl_cout << "total # of cameras saved in score files: " << total_cam_in_file << vcl_endl;
-  vcl_cout << "so total # of cameras knocked out: " << cnt_cam_below << vcl_endl;
-  vcl_cout << "  so previous pass knocked out: " << ((float)cnt_cam_below/(tot_hyp_count*total_cams))*100.0 << " percent of cameras!\n";
+  vcl_cout << "total # of hypotheses in the output score file: " << tot_hyp_count << vcl_endl
+           << "total # of hypotheses below threshold (" << cam_threshold() << "): " << cnt_hyp_below << vcl_endl
+           << "  so previous pass knocked out: " << ((float)cnt_hyp_below/tot_hyp_count)*100.0 << " percent of locations!\n"
+           << "total # of cameras (total hyp*cam space size): " << tot_hyp_count*total_cams << vcl_endl
+           << "total # of cameras saved in score files: " << total_cam_in_file << vcl_endl
+           << "so total # of cameras knocked out: " << cnt_cam_below << vcl_endl
+           << "  so previous pass knocked out: " << ((float)cnt_cam_below/(tot_hyp_count*total_cams))*100.0 << " percent of cameras!\n"
+           << "\n==================================================================================================\n"
+           << "==================================================================================================\n" << vcl_endl;
 
-  vcl_cout << "\n==================================================================================================\n";
-  vcl_cout << "==================================================================================================\n " << vcl_endl;
-  
   return volm_io::SUCCESS;
 }
 
