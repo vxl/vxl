d2818a9c67a99f80a89731edaf6bfc6a84d08a57
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-02-01 12:36:55 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 36444

--- contrib/brl/bbas/vsph/tests/test_include.cxx
+++ contrib/brl/bbas/vsph/tests/test_include.cxx
@@ -5,6 +5,7 @@
 #include <vsph/vsph_sph_point_2d.h>
 #include <vsph/vsph_sph_point_3d.h>
 #include <vsph/vsph_spherical_coord.h>
+#include <vsph/vsph_unit_sphere.h>
 #include <vsph/vsph_utils.h>
 #include <vsph/vsph_view_point.h>
 #include <vsph/vsph_view_sphere.h>
