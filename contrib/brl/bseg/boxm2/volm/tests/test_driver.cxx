44002d93d02e6ebece81c3d8f0cf267919cc9a25
Yi Dong <yidong80@users.sourceforge.net>
2013-02-01 19:12:07 -0500

    update clean unnecessary matchers
    SVN-Revision: 36449

--- contrib/brl/bseg/boxm2/volm/tests/test_driver.cxx
+++ contrib/brl/bseg/boxm2/volm/tests/test_driver.cxx
@@ -2,14 +2,12 @@
 
 DECLARE( test_volm_wr3db_index );
 DECLARE( test_volm_locations );
-DECLARE( test_volm_obj_based_matcher );
 DECLARE( test_volm_matcher_p1 );
 
 void register_tests()
 {
   REGISTER( test_volm_locations );
   REGISTER( test_volm_wr3db_index );
-  REGISTER( test_volm_obj_based_matcher );
   REGISTER( test_volm_matcher_p1 );
 }
 
