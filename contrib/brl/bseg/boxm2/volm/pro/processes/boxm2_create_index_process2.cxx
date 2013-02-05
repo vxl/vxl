e9f6216398ff9d2c36d87ec906c7ff2595ddea23
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2013-02-05 18:19:20 -0500

    updates
    SVN-Revision: 36499

--- contrib/brl/bseg/boxm2/volm/pro/processes/boxm2_create_index_process2.cxx
+++ contrib/brl/bseg/boxm2/volm/pro/processes/boxm2_create_index_process2.cxx
@@ -405,9 +405,6 @@ bool boxm2_create_index_process2(bprb_func_process& pro)
         //remove from device memory unnecessary items
         //opencl_cache->shallow_remove_data(id_inner,boxm2_data_traits<BOXM2_ALPHA>::prefix());
         //opencl_cache->clear_cache();
-
-        status = clFinish(queue);
-        check_val(status, MEM_FAILURE, "opencl clear cache FAILED: " + error_to_string(status));
       }
       if (vis_blocks.size() != 0)  // normalize
       {
@@ -483,6 +480,7 @@ bool boxm2_create_index_process2(bprb_func_process& pro)
     boxm2_volm_wr3db_index_params::write_size_file(index_file, indexed_cnt);
 
     opencl_cache->clear_cache();
+    
   }
 
   delete ray_dir_buffer;
