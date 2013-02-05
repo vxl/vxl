ec7d7763b8ab71c33bd848901e50744651c2d279
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-02-05 18:24:54 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 36500

--- contrib/brl/bbas/bpgl/depth_map/tests/test_include.cxx
+++ contrib/brl/bbas/bpgl/depth_map/tests/test_include.cxx
@@ -1,3 +1,4 @@
+#include <bpgl/depth_map/depth_map_defs.h>
 #include <bpgl/depth_map/depth_map_region.h>
 #include <bpgl/depth_map/depth_map_region_sptr.h>
 #include <bpgl/depth_map/depth_map_scene.h>
