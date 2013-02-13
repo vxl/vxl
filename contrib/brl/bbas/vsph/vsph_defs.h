a4107b3b3eaadee760a43a58a357cd043361b473
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-02-13 12:43:04 -0500

    Doxygen comments are only shown when the "\file" structured comment is within a "//:" block
    SVN-Revision: 36553

--- contrib/brl/bbas/vsph/vsph_defs.h
+++ contrib/brl/bbas/vsph/vsph_defs.h
@@ -1,11 +1,12 @@
 #ifndef vsph_defs_h_
 #define vsph_defs_h_
+//:
 // \file
 // \brief  Definition of constants used in spherical geometry calculations
 // \author J. L. Mundy
 // \date February 11, 2013
 
-//: the tolerance on distance comparison between two unit vectors
+//: the tolerance on distance comparison between two unit vectors.
 //  i.e. Cartesian points on the unit sphere
 #define DIST_TOL 0.0001
 
