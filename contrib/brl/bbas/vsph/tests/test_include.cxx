e5de4b2e6315d559b796dd1de033b18bb9226360
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-02-19 18:22:14 -0500

    updated w.r.t. CMakeLists.txt
    SVN-Revision: 36572

--- contrib/brl/bbas/vsph/tests/test_include.cxx
+++ contrib/brl/bbas/vsph/tests/test_include.cxx
@@ -4,6 +4,7 @@
 #include <vsph/vsph_camera_bounds.h>
 #include <vsph/vsph_defs.h>
 #include <vsph/vsph_grid_index_2d.h>
+#include <vsph/vsph_segment_sphere.h>
 #include <vsph/vsph_sph_box_2d.h>
 #include <vsph/vsph_sph_point_2d.h>
 #include <vsph/vsph_sph_point_3d.h>
