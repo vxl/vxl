bc2536c6562d2a2d42e323a7348bb633d540cba2
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-02-19 12:13:24 -0500

    added missing "\file" for doxygen
    SVN-Revision: 36568

--- contrib/brl/bbas/vsph/vsph_segment_sphere.h
+++ contrib/brl/bbas/vsph/vsph_segment_sphere.h
@@ -1,25 +1,27 @@
 #ifndef vsph_segment_sphere_h_
 #define vsph_segment_sphere_h_
 #include "vsph_unit_sphere.h"
+//:
+// \file
 #include <vcl_map.h>
 #include <vbl/vbl_disjoint_sets.h>
+
 class vsph_segment_sphere
 {
  public:
- vsph_segment_sphere(vsph_unit_sphere const& usph, double sigma, double c, int min_size) : seg_valid_(false),usph_(usph),sigma_(sigma), c_(c), min_size_(min_size), num_ccs_(0){}
-  //: accessors
-  double sigma() const {return sigma_;} 
-  double c() const {return c_;} 
+  vsph_segment_sphere(vsph_unit_sphere const& usph, double sigma, double c, int min_size) : seg_valid_(false),usph_(usph),sigma_(sigma), c_(c), min_size_(min_size), num_ccs_(0){}
+  // === accessors ==-
+  double sigma() const {return sigma_;}
+  double c() const {return c_;}
   int min_size() const {return min_size_;}
   int num_ccs() const {return num_ccs_;}
   const vcl_map<int,  vcl_vector<int> >& regions() const {return regions_;}
-  //: process methods
+  // === process methods ===
   void set_data(vcl_vector<double> data){data_ = data;}
   void smooth_data();
   void segment();
   bool extract_region_bounding_boxes();
-  const vcl_map<int, vsph_sph_box_2d>& region_boxes() const{
-    return bboxes_;}
+  const vcl_map<int, vsph_sph_box_2d>& region_boxes() const {return bboxes_;}
   vcl_vector<double> region_data() const;
   vcl_vector<vcl_vector<float> > region_color() const;
  private:
@@ -32,11 +34,11 @@ class vsph_segment_sphere
   vcl_vector<double> data_;
   vcl_vector<double> smooth_data_;
   int num_ccs_;
-  //:segmented regions
+  //: segmented regions
   vbl_disjoint_sets ds_;
-  //:   region id  sph vert ids
+  //: region id  sph vert ids
   vcl_map<int,  vcl_vector<int> > regions_;
-  //:   region id    box
+  //: region id  box
   vcl_map<int, vsph_sph_box_2d> bboxes_;
 };
 
