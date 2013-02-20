f56165cf8a18d30e25c526384cbb744c7553cabe
Yi Dong <yidong80@users.sourceforge.net>
2013-02-20 10:32:55 -0500

    update
    SVN-Revision: 36579

--- contrib/brl/bbas/volm/tests/test_query.cxx
+++ contrib/brl/bbas/volm/tests/test_query.cxx
@@ -3,6 +3,8 @@
 #include <volm/volm_spherical_container_sptr.h>
 #include <volm/volm_spherical_shell_container.h>
 #include <volm/volm_spherical_shell_container_sptr.h>
+#include <volm/volm_camera_space.h>
+#include <volm/volm_camera_space_sptr.h>
 #include <volm/volm_io.h>
 #include <volm/volm_query.h>
 #include <volm/volm_query_sptr.h>
@@ -12,56 +14,91 @@
 
 static void test_query()
 {
-  // create the depth interval using volm_spherical_container
-
-  // parameter for coast
+  // input files
+  vcl_string cam_bin_file = "D:\\work\\find\\volm_matcher\\test1\\local_output\\p1a_test1_p1a_test1_83\\camera_space.bin";
+  vcl_string dms_bin_file = "Z:\\projects\\find\\test1\\p1a_test1_83\\p1a_test1_83.vsl";
+  vcl_string sph_shell_file = "Z:\\projects\\find\\index\\sph_shell_vsph_ca_180_pa_5_ta_75_ba_75.bin";
+  
+  // parameter for depth_map_interval
   float vmin = 2.0f;         // min voxel resolution
   float dmax = 3000.0f;      // maximum depth
   float solid_angle = 5.0f;
-#if 0
-  // parameters for desert
-  float vmin = 5.0f;
-  float dmax = 30000.0f;
-  float solid_angle = 2.0f;
-#endif
-
   volm_spherical_container_sptr sph = new volm_spherical_container(solid_angle,vmin,dmax);
 
-  // create spherical shell for query rays
-  // parameter for coast
-  float cap_angle = 180.0f;
-  float point_angle = 5.0f;
-  double radius = 1;
-  float top_angle = 70.0f;
-  float bottom_angle = 60.0f;
-#if 0
-  // parameter for desert
-  float cap_angle = 180.0f;
-  float point_angle = 2.0f;
-  double radius = 1;
-  float top_angle = 70.0f;
-  float bottom_angle = 70.0f;
-#endif
-  volm_spherical_shell_container_sptr sph_shell = new volm_spherical_shell_container(radius, cap_angle, point_angle, top_angle, bottom_angle);
-#if 1 //files not available
-  // labelme file and camera files
-  vcl_string out_folder = "D:/work/find/volumetric_algo/queries/coast/job_160/viewing_volume/";
-  vcl_string label_file = "D:/work/find/volm_matcher/finderuploads/coast/job_160/Result_USC_calibration_noFurther.xml";
-  vcl_string cam_file =   "D:/work/find/volm_matcher/finderuploads/coast/job_160/Camera_test.kml";
-  // check the labelme file and camera file
-  depth_map_scene_sptr dm = new depth_map_scene;
-  vcl_string img_category;
-  if (!volm_io::read_labelme(label_file, dm, img_category)) {
-    volm_io::write_status(out_folder, volm_io::LABELME_FILE_IO_ERROR);
+  // load the spherical_shell_container
+  volm_spherical_shell_container_sptr sph_shell = new volm_spherical_shell_container;
+  vsl_b_ifstream sph_ifs(sph_shell_file);
+  sph_shell->b_read(sph_ifs);
+  sph_ifs.close();
+
+  // load cam_space 
+  vsl_b_ifstream cam_ifs(cam_bin_file);
+  volm_camera_space_sptr csp_in = new volm_camera_space;
+  csp_in->b_read(cam_ifs);
+
+  // create volm_query
+  volm_query_sptr query = new volm_query(csp_in, dms_bin_file, sph_shell, sph);
+
+  // screen output for query information
+  unsigned total_size = query->obj_based_query_size_byte();
+  vcl_cout << " For spherical surface, point angle = " << sph_shell->point_angle() << " degree, "
+           << ", top_angle = " << sph_shell->top_angle() << " degree, "
+           << ", bottom_angle = " << sph_shell->bottom_angle() << " degree, "
+           << ", generated query has " << query->get_query_size() << " rays, "
+           << query->get_cam_num() << " cameras:" << '\n'
+           << " The query with " << query->get_cam_num() << " has " << (float)total_size/1024 << " Kbyte in total "
+           << vcl_endl;
+
+  // query check
+  depth_map_scene_sptr dm = query->depth_scene();
+  vcl_cout << " The " << dm->ni() << " x " << dm->nj() << " query image has following defined depth region" << vcl_endl;
+
+  if (dm->sky().size()) {
+    vcl_cout << " -------------- SKYs -------------- " << vcl_endl;
+    for (unsigned i = 0; i < dm->sky().size(); i++)
+      vcl_cout << "\t name = " << (dm->sky()[i]->name())
+               << ", depth = " << 254
+               << ", orient = " << (int)query->sky_orient()
+               << ", land_id = " << dm->sky()[i]->land_id() 
+               << ", land_name = " << volm_label_table::land_string(dm->sky()[i]->land_id())
+               << vcl_endl;
   }
-  double heading, heading_dev, tilt, tilt_dev, roll, roll_dev, altitude, lat, lon;
-  double top_fov, top_fov_dev;
-  if (!volm_io::read_camera(cam_file, dm->ni(), dm->nj(), heading, heading_dev, tilt, tilt_dev, roll, roll_dev, top_fov, top_fov_dev, altitude, lat, lon)) {
-    volm_io::write_status(out_folder, volm_io::CAM_FILE_IO_ERROR);
+  if (dm->ground_plane().size()) {
+    vcl_cout << " -------------- GROUND PLANE -------------- " << vcl_endl;
+    for (unsigned i = 0; i < dm->ground_plane().size(); i++)
+      vcl_cout << "\t name = " << dm->ground_plane()[i]->name()
+               << ", depth = " << dm->ground_plane()[i]->min_depth()
+               << ", orient = " << dm->ground_plane()[i]->orient_type()
+               << ", land_id = " << dm->ground_plane()[i]->land_id() 
+               << ", land_name = " << volm_label_table::land_string(dm->ground_plane()[i]->land_id())
+               << vcl_endl;
   }
-  volm_query_sptr query = new volm_query(cam_file, label_file, sph, sph_shell, false);
-  
-#endif
+  if (dm->scene_regions().size()) {
+    vcl_cout << " -------------- DEPTH REGIONS -------------- " << vcl_endl;
+    for (unsigned i = 0; i < dm->scene_regions().size(); i++) {
+      vcl_cout << "\t " <<  (dm->scene_regions())[i]->name()  << " region "
+               << ",\t min_depth = " << (dm->scene_regions())[i]->min_depth()
+               << ",\t max_depth = " << (dm->scene_regions())[i]->max_depth()
+               << ",\t order = " << (dm->scene_regions())[i]->order()
+               << vcl_endl;
+    }
+  }
+
+  vcl_vector<depth_map_region_sptr> drs = query->depth_regions();
+  vcl_cout << " The depth regions map inside query follows on order" << vcl_endl;
+  if (drs.size()) {
+    for (unsigned i = 0; i < drs.size(); i++) {
+      vcl_cout << "\t " <<  (dm->scene_regions())[i]->name()  << " region "
+               << ",\t min_depth = " << (dm->scene_regions())[i]->min_depth()
+               << ",\t max_depth = " << (dm->scene_regions())[i]->max_depth()
+               << ",\t order = " << (dm->scene_regions())[i]->order()
+               << ",\t orient = " << (dm->scene_regions())[i]->orient_type()
+               << ",\t land_id = " << (dm->scene_regions())[i]->land_id()
+               << ",\t land_name = " << volm_label_table::land_string( (dm->scene_regions())[i]->land_id() )
+               << vcl_endl;
+    }
+  }
+
 #if 0
   vcl_string depth_scene_path = "c:/Users/mundy/VisionSystems/Finder/VolumetricQuery/Queries/p1a_res06_dirtroad_depthscene_v2.vsl";
   // create the query
@@ -143,6 +180,7 @@ static void test_query()
 
   //TEST("number of rays for current query", query->get_query_size(), 29440); // for 2 degree resolution
 #endif//NEED FILES TO RUN!! JLM
+
 }
 
 
