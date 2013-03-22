5bb1c5af5b15911dc5d2302580e65b30ef7e8cb1
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2013-03-21 23:03:29 -0400

    fixed
    SVN-Revision: 36762

--- contrib/brl/bbas/volm/volm_tile.cxx
+++ contrib/brl/bbas/volm/volm_tile.cxx
@@ -16,7 +16,8 @@ volm_tile::volm_tile(float lat, float lon, char hemisphere, char direction, floa
         lat_(lat), lon_(lon), hemisphere_(hemisphere), direction_(direction), scale_i_(scale_i), scale_j_(scale_j), ni_(ni), nj_(nj)
 {
   vnl_matrix<double> trans_matrix(4,4,0.0);
-  trans_matrix[0][0] = -scale_i_/ni; trans_matrix[1][1] = -scale_j_/nj;
+  //trans_matrix[0][0] = -scale_i_/ni; trans_matrix[1][1] = -scale_j_/nj;
+  trans_matrix[0][0] = -scale_i_/3600.0; trans_matrix[1][1] = -scale_j_/3600.0;
   trans_matrix[0][3] = lon_; trans_matrix[1][3] = lat_+scale_j_;
   // just pass an empty lvcs, this geo cam will only be used to compute image pixels to global coords mappings
   vpgl_lvcs_sptr lv = new vpgl_lvcs;
@@ -54,7 +55,8 @@ volm_tile::volm_tile(vcl_string file_name, unsigned ni, unsigned nj) : ni_(ni), 
            << "lower right corner in the image is: " << lat_ << " N " << lon_-scale_i_ << " W" << vcl_endl;
 #endif
   vnl_matrix<double> trans_matrix(4,4,0.0);
-  trans_matrix[0][0] = -scale_i_/ni; trans_matrix[1][1] = -scale_j_/nj;
+  //trans_matrix[0][0] = -scale_i_/ni; trans_matrix[1][1] = -scale_j_/nj;
+  trans_matrix[0][0] = -scale_i_/3600.0; trans_matrix[1][1] = -scale_j_/3600.0;
   trans_matrix[0][3] = lon_; trans_matrix[1][3] = lat_+scale_j_;
   vpgl_lvcs_sptr dummy_lvcs = new vpgl_lvcs;
   cam_ = vpgl_geo_camera(trans_matrix, dummy_lvcs);
@@ -70,7 +72,8 @@ volm_tile::volm_tile(float lat, float lon, float scale_i, float scale_j, unsigne
   if (lon < 0) { lon_ = -lon; direction_ = 'W'; } else { lon_ = lon; direction_ = 'E'; }
 
   vnl_matrix<double> trans_matrix(4,4,0.0);
-  trans_matrix[0][0] = -scale_i_/ni; trans_matrix[1][1] = -scale_j_/nj;
+  //trans_matrix[0][0] = -scale_i_/ni; trans_matrix[1][1] = -scale_j_/nj;
+  trans_matrix[0][0] = -scale_i_/3600.0; trans_matrix[1][1] = -scale_j_/3600.0;
   trans_matrix[0][3] = lon_; trans_matrix[1][3] = lat_+scale_j_;
   // just pass an empty lvcs, this geo cam will only be used to compute image pixels to global coords mappings
   vpgl_lvcs_sptr lv = new vpgl_lvcs;
@@ -406,5 +409,13 @@ vgl_box_2d<float> volm_tile::bbox()
   vgl_point_2d<float> p1((float)this->lower_left_lon(), (float)this->lower_left_lat());
   vgl_point_2d<float> p2(p1.x()+this->scale_i_, p1.y()+this->scale_j_);
   vgl_box_2d<float> box(p1, p2);
+  return box;
+}
+
+vgl_box_2d<double> volm_tile::bbox_double()
+{
+  vgl_point_2d<double> p1(this->lower_left_lon(), this->lower_left_lat());
+  vgl_point_2d<double> p2(p1.x()+this->scale_i_, p1.y()+this->scale_j_);
+  vgl_box_2d<double> box(p1, p2);
   return box;
 }
