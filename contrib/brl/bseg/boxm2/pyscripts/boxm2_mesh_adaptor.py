bceb9aca7eba850da37d6824e5668a4a1cf18302
Vishal Jain <jainv@users.sourceforge.net>
2013-03-28 09:10:45 -0400

    added a function to load a mesh.
    SVN-Revision: 36808

--- contrib/brl/bseg/boxm2/pyscripts/boxm2_mesh_adaptor.py
+++ contrib/brl/bseg/boxm2/pyscripts/boxm2_mesh_adaptor.py
@@ -1,5 +1,13 @@
 from boxm2_register import boxm2_batch, dbvalue;
 
+def load_mesh(filename):
+  boxm2_batch.init_process("boxm2LoadMeshProcess");
+  boxm2_batch.set_input_string(0,filename);
+  boxm2_batch.run_process();
+  (id, type) = boxm2_batch.commit_output(0);
+  mesh = dbvalue(id, type);
+  return mesh;
+  
 def gen_mesh(z_img,x_img,y_img,out_dir):
   boxm2_batch.init_process("boxm2ExportMeshProcess");
   boxm2_batch.set_input_from_db(0,z_img);
@@ -19,13 +27,18 @@ def gen_tex_mesh(mesh,in_img_dir, in_cam_dir,out_dir):
   boxm2_batch.set_input_string(3,out_dir);
   boxm2_batch.run_process();
 
-def gen_point_cloud(scene,cache):
+def gen_point_cloud(scene,cache,filename):
   boxm2_batch.init_process("boxm2ExtractPointCloudProcess");
   boxm2_batch.set_input_from_db(0,scene);
   boxm2_batch.set_input_from_db(1,cache);
-  boxm2_batch.set_input_float(2,0); #prob threshold
+  boxm2_batch.set_input_float(2,0.3); #prob threshold
   boxm2_batch.run_process();
 
+  boxm2_batch.init_process("boxm2ExportOrientedPointCloudProcess");
+  boxm2_batch.set_input_from_db(0,scene);
+  boxm2_batch.set_input_from_db(1,cache);
+  boxm2_batch.set_input_string(2,filename);
+  boxm2_batch.run_process();
 def gen_oriented_point_cloud(scene,cache,prob_t, norm_mag_t, filename):
   boxm2_batch.init_process("boxm2CppComputeDerivativeProcesses");
   boxm2_batch.set_input_from_db(0,scene);
@@ -33,11 +46,7 @@ def gen_oriented_point_cloud(scene,cache,prob_t, norm_mag_t, filename):
   boxm2_batch.set_input_float(2,prob_t);
   boxm2_batch.set_input_float(3,norm_mag_t);
 
-  boxm2_batch.init_process("boxm2ExportOrientedPointCloudProcess");
-  boxm2_batch.set_input_from_db(0,scene);
-  boxm2_batch.set_input_from_db(1,cache);
-  boxm2_batch.set_input_string(2,filename);
-  boxm2_batch.run_process();
+
 
 def export_stack(scene,cache,outdir):
   boxm2_batch.init_process("boxm2ExportStackImagesProcess");
