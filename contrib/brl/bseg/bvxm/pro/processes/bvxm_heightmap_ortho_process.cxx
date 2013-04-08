48aa4658a47258ef78eae81c48cd1dd814021142
Ozge Can Ozcanli <ozge@users.sourceforge.net>
2013-04-08 15:00:53 -0400

    adding ortho height map rendering
    SVN-Revision: 36845

---
+++ contrib/brl/bseg/bvxm/pro/processes/bvxm_heightmap_ortho_process.cxx
@@ -0,0 +1,80 @@
+// This is brl/bseg/bvxm/pro/processes/bvxm_heightmap_ortho_process.cxx
+#include "bvxm_heightmap_ortho_process.h"
+//:
+// \file
+#include <bprb/bprb_func_process.h>
+#include <bvxm/bvxm_voxel_world.h>
+#include <vil/vil_image_view.h>
+
+bool bvxm_heightmap_ortho_process_cons(bprb_func_process& pro)
+{
+  using namespace bvxm_heightmap_ortho_process_globals;
+  // This process has 4 inputs:
+  //input[0]: The voxel world
+  vcl_vector<vcl_string> input_types_(n_inputs_);
+  int i=0;
+  input_types_[i++] = "bvxm_voxel_world_sptr";    // voxel_world
+  if (!pro.set_input_types(input_types_))
+    return false;
+
+  //output
+  vcl_vector<vcl_string> output_types_(n_outputs_);
+  output_types_[0] = "vil_image_view_base_sptr";
+  return pro.set_output_types(output_types_);
+}
+
+// generates a height map from a given camera viewpoint
+bool bvxm_heightmap_ortho_process(bprb_func_process& pro)
+{
+  using namespace bvxm_heightmap_ortho_process_globals;
+
+  if (pro.n_inputs()<n_inputs_)
+  {
+    vcl_cout << pro.name() << " The input number should be " << n_inputs_<< vcl_endl;
+    return false;
+  }
+
+  unsigned i = 0;
+  //voxel_world
+  bvxm_voxel_world_sptr world =  pro.get_input<bvxm_voxel_world_sptr>(i++);
+  if (!world) {
+    vcl_cout << pro.name() <<" :--  Input 3  is not valid!\n";
+    return false;
+  }
+
+  // generate vpgl_geo_camera for the scene
+  bvxm_world_params_sptr params = world->get_params();
+  vgl_box_3d<double> box = params->world_box_local();
+  vgl_point_3d<float> corner = params->corner();
+  vgl_point_3d<float> upper_left(corner.x(), (float)(corner.y() + box.height()), corner.z());
+  vgl_point_3d<float> lower_right((float)(corner.x()+box.width()), corner.y(), corner.z());
+  vpgl_lvcs_sptr lvcs = params->lvcs();
+  double lat, lon, elev;
+  lvcs->get_origin(lat, lon, elev);
+  vcl_cout << " lvcs origin: " << lat << " " << lon << " " << elev << vcl_endl;
+
+  // determine the upper left corner to use a vpgl_geo_cam, WARNING: assumes that the world is compass-alinged
+  double upper_left_lon, upper_left_lat, upper_left_elev;
+  lvcs->local_to_global(upper_left.x(), upper_left.y(), upper_left.z(), vpgl_lvcs::wgs84, upper_left_lon, upper_left_lat, upper_left_elev);
+  vcl_cout << "upper left corner in the image is: " << upper_left_lon << " lat: " << upper_left_lat << vcl_endl;
+
+  double lower_right_lon, lower_right_lat, lower_right_elev;
+  lvcs->local_to_global(lower_right.x(), lower_right.y(), lower_right.z(), vpgl_lvcs::wgs84, lower_right_lon, lower_right_lat, lower_right_elev);
+  vcl_cout << "lower right corner in the image is: " << lower_right_lon << " lat: " << lower_right_lat << vcl_endl;
+  
+  int ni = box.width();
+  int nj = box.height();
+  vnl_matrix<double> trans_matrix(4,4,0.0);
+  trans_matrix[0][0] = (lower_right_lon-lon)/ni; trans_matrix[1][1] = -(upper_left_lat-lat)/nj;
+  trans_matrix[0][3] = upper_left_lon; trans_matrix[1][3] = upper_left_lat;
+  vpgl_geo_camera* cam = new vpgl_geo_camera(trans_matrix, lvcs); 
+  cam->set_scale_format(true);
+  vpgl_camera_double_sptr camera = new vpgl_geo_camera(*cam);  
+  
+  vil_image_view<unsigned> *hmap = new vil_image_view<unsigned>(ni, nj, 1);
+  world->heightmap(camera,*hmap);
+  //store output
+  pro.set_output_val<vil_image_view_base_sptr>(0, hmap);
+  
+  return true;
+}
---
+++ contrib/brl/bseg/bvxm/pro/processes/bvxm_heightmap_ortho_process.h
@@ -0,0 +1,38 @@
+// This is brl/bseg/bvxm/pro/processes/bvxm_heightmap_ortho_process.h
+#ifndef bvxm_heightmap_ortho_process_h_
+#define bvxm_heightmap_ortho_process_h_
+//:
+// \file
+// \brief A class for generating an orthographic height map
+//        creates an orthographic vpgl_geo_camera first, then renders the height map using this camera
+//        WARNING: this process assumes that the world is compass-alinged 
+//
+//        -  Inputs:
+//             * bvxm_voxel_world_sptr
+//
+//        -  Output:
+//             * vil_image_view_base_sptr   generated image
+//
+// \author  Ozge C. Ozcanli 
+// \date    Apr 03, 2013
+// \verbatim
+//  Modifications
+// \endverbatim
+
+#include <bprb/bprb_func_process.h>
+
+// generate a heightmap with an ortho camera - generate a vpgl_geo_camera for the scene 
+//: global variables
+namespace bvxm_heightmap_ortho_process_globals
+{
+  const unsigned n_inputs_ = 1;
+  const unsigned n_outputs_ = 1;
+}
+
+//: set input and output types
+bool bvxm_heightmap_ortho_process_cons(bprb_func_process& pro);
+
+// generates a height map from a given camera viewpoint
+bool bvxm_heightmap_ortho_process(bprb_func_process& pro);
+
+#endif // bvxm_heightmap_ortho_process_h_
