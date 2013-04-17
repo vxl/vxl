b012f64de1b74d0c055c5c9f35772cc60af387a6
Ali Osman Ulusoy <aulusoy@users.sourceforge.net>
2013-04-17 13:26:21 -0400

    Added BSTM_GAUSS_RGB_VIEW_COMPACT datatype.
    SVN-Revision: 36873

--- contrib/brl/bseg/bstm/bstm_data_traits.h
+++ contrib/brl/bseg/bstm/bstm_data_traits.h
@@ -20,6 +20,7 @@ enum bstm_data_type
   BSTM_MOG6_VIEW,
   BSTM_MOG6_VIEW_COMPACT,
   BSTM_GAUSS_RGB,
+  BSTM_GAUSS_RGB_VIEW_COMPACT,
   BSTM_NUM_OBS,
   BSTM_LABEL,
   BSTM_UNKNOWN
@@ -57,6 +58,16 @@ class bstm_data_traits<BSTM_GAUSS_RGB>
   static vcl_size_t datasize() { return sizeof(datatype); }
   static vcl_string prefix(const vcl_string& identifier = "")
   { if (!identifier.size()) return "bstm_gauss_rgb"; else return "bstm_gauss_rgb_"+identifier; }
+};
+
+template<>
+class bstm_data_traits<BSTM_GAUSS_RGB_VIEW_COMPACT>
+{
+ public:
+  typedef vnl_vector_fixed<int, 8> datatype;
+  static vcl_size_t datasize() { return sizeof(datatype); }
+  static vcl_string prefix(const vcl_string& identifier = "")
+  { if (!identifier.size()) return "bstm_gauss_rgb_view_compact"; else return "bstm_gauss_rgb_view_compact_"+identifier; }
 };
 
 
@@ -123,11 +134,13 @@ class bstm_data_info
 
     if (prefix.find(bstm_data_traits<BSTM_NUM_OBS>::prefix()) != vcl_string::npos)
       return bstm_data_traits<BSTM_NUM_OBS>::datasize();
-
+    if (prefix.find(bstm_data_traits<BSTM_GAUSS_RGB_VIEW_COMPACT>::prefix()) != vcl_string::npos)
+      return bstm_data_traits<BSTM_GAUSS_RGB_VIEW_COMPACT>::datasize();
     if (prefix.find(bstm_data_traits<BSTM_GAUSS_RGB>::prefix()) != vcl_string::npos)
       return bstm_data_traits<BSTM_GAUSS_RGB>::datasize();
     if (prefix.find(bstm_data_traits<BSTM_MOG6_VIEW_COMPACT>::prefix()) != vcl_string::npos)
       return bstm_data_traits<BSTM_MOG6_VIEW_COMPACT>::datasize();
+
     if (prefix.find(bstm_data_traits<BSTM_MOG6_VIEW>::prefix()) != vcl_string::npos)
       return bstm_data_traits<BSTM_MOG6_VIEW>::datasize();
     if (prefix.find(bstm_data_traits<BSTM_LABEL>::prefix()) != vcl_string::npos)
@@ -148,6 +161,12 @@ class bstm_data_info
       return  BSTM_GAUSS_RGB ;
     else if (prefix.find(bstm_data_traits<BSTM_LABEL>::prefix()) != vcl_string::npos)
       return  BSTM_LABEL ;
+    else if (prefix.find(bstm_data_traits<BSTM_GAUSS_RGB_VIEW_COMPACT>::prefix()) != vcl_string::npos)
+      return  BSTM_GAUSS_RGB_VIEW_COMPACT ;
+    else if (prefix.find(bstm_data_traits<BSTM_MOG6_VIEW_COMPACT>::prefix()) != vcl_string::npos)
+      return  BSTM_MOG6_VIEW_COMPACT ;
+    else if (prefix.find(bstm_data_traits<BSTM_MOG6_VIEW>::prefix()) != vcl_string::npos)
+      return  BSTM_MOG6_VIEW ;
     else
       return BSTM_UNKNOWN;
   }
