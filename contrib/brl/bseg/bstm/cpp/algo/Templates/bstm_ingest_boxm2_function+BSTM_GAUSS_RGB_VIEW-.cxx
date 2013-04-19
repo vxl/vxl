293c61486d5d83c1f30b8ca70857c6bc6a505ba6
Ali Osman Ulusoy <aulusoy@users.sourceforge.net>
2013-04-19 17:39:05 -0400

    Added functionality for more appearance types.
    SVN-Revision: 36884

---
+++ contrib/brl/bseg/bstm/cpp/algo/Templates/bstm_ingest_boxm2_function+BSTM_GAUSS_RGB_VIEW-.cxx
@@ -0,0 +1,5 @@
+#include <boxm2/boxm2_data_traits.h>
+#include <bstm/bstm_data_traits.h>
+#include <bstm/cpp/algo/bstm_ingest_boxm2_scene_function.txx>
+
+template class bstm_ingest_boxm2_scene_function<BSTM_GAUSS_RGB_VIEW_COMPACT, BOXM2_GAUSS_RGB_VIEW_COMPACT>;
