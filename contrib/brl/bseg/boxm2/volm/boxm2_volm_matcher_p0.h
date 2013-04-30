b3d2e1f079ed666d7590fe4b6a9ca3f7590f493d
Peter Vanroose <peter_vanroose@users.sourceforge.net>
2013-04-30 16:05:39 -0400

    removed unused #includes
    SVN-Revision: 36938

--- contrib/brl/bseg/boxm2/volm/boxm2_volm_matcher_p0.h
+++ contrib/brl/bseg/boxm2/volm/boxm2_volm_matcher_p0.h
@@ -14,10 +14,6 @@
 
 #include "boxm2_volm_wr3db_index_sptr.h"
 #include "boxm2_volm_wr3db_index.h"
-#include <bocl/bocl_manager.h>
-#include <bocl/bocl_device.h>
-#include <bocl/bocl_kernel.h>
-#include <bocl/bocl_mem.h>
 
 class boxm2_volm_matcher_p0
 {
