fe930abac2266743ab2b8808e5ad65f89348193c
Ali Osman Ulusoy <aliosman.ulusoy@gmail.com>
2013-07-31 13:54:37 -0400

    Updates to view based appearance modeling.

--- contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_render_gl_view_dep_app_expected_color_process.cxx
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_render_gl_view_dep_app_expected_color_process.cxx
@@ -1,11 +1,12 @@
-// This is brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_render_view_dep_expected_color_process.cxx
-#include <bprb/bprb_func_process.h>
+// This is brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_render_gl_view_dep_app_expected_color_process .cxx
 //:
 // \file
 // \brief  A process for rendering the scene.
 //
 // \author Vishal Jain
 // \date Mar 10, 2011
+
+#include <bprb/bprb_func_process.h>
 
 #include <vcl_fstream.h>
 #include <vcl_algorithm.h>
@@ -14,27 +15,23 @@
 #include <boxm2/boxm2_block.h>
 #include <boxm2/boxm2_data_base.h>
 #include <boxm2/ocl/boxm2_ocl_util.h>
-#include <vil/vil_image_view.h>
 //brdb stuff
 #include <brdb/brdb_value.h>
-
 #include <boxm2/boxm2_util.h>
-
 //directory utility
 #include <vcl_where_root_dir.h>
 #include <bocl/bocl_device.h>
 #include <bocl/bocl_kernel.h>
 #include <boxm2/ocl/algo/boxm2_ocl_render_expected_image_function.h>
-#include <vul/vul_timer.h>
 
-
-namespace boxm2_ocl_render_view_dep_expected_color_process_globals
+namespace boxm2_ocl_render_gl_view_dep_app_expected_color_process_globals
 {
-  const unsigned n_inputs_ = 7;
-  const unsigned n_outputs_ = 2;
-  vcl_size_t lthreads[2]={8,8};
+  const unsigned n_inputs_   = 10;
+  const unsigned n_outputs_  = 1;
+  vcl_size_t     lthreads[2] = {8,8};
 
   static vcl_map<vcl_string,vcl_vector<bocl_kernel*> > kernels;
+
   void compile_kernel(bocl_device_sptr device,vcl_vector<bocl_kernel*> & vec_kernels, vcl_string opts)
   {
     //gather all render sources... seems like a lot for rendering...
@@ -48,47 +45,43 @@ namespace boxm2_ocl_render_view_dep_expected_color_process_globals
     src_paths.push_back(source_dir + "ray_bundle_library_opt.cl");
     src_paths.push_back(source_dir + "view_dep_app_color_helper_functions.cl");
     src_paths.push_back(source_dir + "bit/render_view_dep_rgb.cl");
-    //src_paths.push_back(source_dir + "expected_functor.cl");
     src_paths.push_back(source_dir + "bit/cast_ray_bit.cl");
 
-    {
-      vcl_string options = opts;
-      options += " -D RENDER_VIEW_DEP ";
-      options += " -D STEP_CELL=step_cell_render(aux_args,data_ptr,d*linfo->block_len)";
-      // options += "-D STEP_CELL=step_cell_render(aux_args.mog,aux_args.alpha,data_ptr,aux_args.app_model_weights,d*linfo->block_len,vis,aux_args.expint)";
-      //have kernel construct itself using the context and device
-      bocl_kernel * ray_trace_kernel=new bocl_kernel();
-      ray_trace_kernel->create_kernel( &device->context(),
-                                       device->device_id(),
-                                       src_paths,
-                                       "render_bit_scene",   //kernel name
-                                       options,              //options
-                                       "boxm2 opencl render_view_dep_color_bit_scene"); //kernel identifier (for error checking)
-      vec_kernels.push_back(ray_trace_kernel);
+    //set kernel options
+    //#define STEP_CELL step_cell_render(mixture_array, alpha_array, data_ptr, d, &vis, &expected_int);
+    vcl_string options = opts + " -D RENDER_VIEW_DEP ";
+    options += " -D DETERMINISTIC ";
+    options += " -D STEP_CELL=step_cell_render(aux_args,data_ptr,d*linfo->block_len)";
 
-      //create normalize image kernel
-      vcl_vector<vcl_string> norm_src_paths;
-      norm_src_paths.push_back(source_dir + "pixel_conversion.cl");
-      norm_src_paths.push_back(source_dir + "bit/normalize_kernels.cl");
-      bocl_kernel * normalize_render_kernel=new bocl_kernel();
+    //have kernel construct itself using the context and device
+    bocl_kernel * ray_trace_kernel=new bocl_kernel();
+    ray_trace_kernel->create_kernel( &device->context(),
+                                     device->device_id(),
+                                     src_paths,
+                                     "render_bit_scene",   //kernel name
+                                     options,              //options
+                                     "boxm2 opencl render gl color"); //kernel identifier (for error checking)
+    vec_kernels.push_back(ray_trace_kernel);
 
-      vcl_string normalize_options = opts;
-      normalize_options += "-D RENDER ";
-      normalize_render_kernel->create_kernel( &device->context(),
-                                              device->device_id(),
-                                              norm_src_paths,
-                                              "normalize_render_rgb_kernel",   //kernel name
-                                              normalize_options,              //options
-                                              "normalize render color kernel"); //kernel identifier (for error checking)
+    //create normalize image kernel
+    vcl_vector<vcl_string> norm_src_paths;
+    norm_src_paths.push_back(source_dir + "pixel_conversion.cl");
+    norm_src_paths.push_back(source_dir + "bit/normalize_kernels.cl");
+    bocl_kernel * normalize_render_kernel=new bocl_kernel();
+    normalize_render_kernel->create_kernel( &device->context(),
+                                            device->device_id(),
+                                            norm_src_paths,
+                                            "normalize_render_kernel_rgb_gl",   //kernel name
+                                            " -D NORMALIZE_RENDER_GL ",              //options
+                                            "normalize render kernel"); //kernel identifier (for error checking)
 
-      vec_kernels.push_back(normalize_render_kernel);
-    }
+    vec_kernels.push_back(normalize_render_kernel);
   }
 }
 
-bool boxm2_ocl_render_view_dep_expected_color_process_cons(bprb_func_process& pro)
+bool boxm2_ocl_render_gl_view_dep_app_expected_color_process_cons(bprb_func_process& pro)
 {
-  using namespace boxm2_ocl_render_view_dep_expected_color_process_globals;
+  using namespace boxm2_ocl_render_gl_view_dep_app_expected_color_process_globals;
 
   //process takes 1 input
   vcl_vector<vcl_string> input_types_(n_inputs_);
@@ -98,57 +91,58 @@ bool boxm2_ocl_render_view_dep_expected_color_process_cons(bprb_func_process& pr
   input_types_[3] = "vpgl_camera_double_sptr";
   input_types_[4] = "unsigned";
   input_types_[5] = "unsigned";
-  input_types_[6] = "vcl_string";
+  input_types_[6] = "bocl_mem_sptr"; // exp image buffer;
+  input_types_[7] = "bocl_mem_sptr"; // exp image dimensions buffer;
+  input_types_[8] = "vcl_string";  //identifier
+  input_types_[9] = "bool"; // exp image dimensions buffer;
 
-  // process has 1 output:
-  // output[0]: scene sptr
-  vcl_vector<vcl_string>  output_types_(n_outputs_);
-  output_types_[0] = "vil_image_view_base_sptr";
-  output_types_[1] = "vil_image_view_base_sptr";
-
-  bool good = pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
-  // in case the 7th input is not set
+  //default last two args
   brdb_value_sptr idx = new brdb_value_t<vcl_string>("");
-  pro.set_input(6, idx);
-  return good;
+  pro.set_input(8, idx);
+  brdb_value_sptr brdb_is_bw = new brdb_value_t<bool>(false);
+  pro.set_input(9, brdb_is_bw);
+
+  //set output types
+  vcl_vector<vcl_string> output_types_(n_outputs_);
+  output_types_[0] = "float";
+
+  return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
 }
 
-bool boxm2_ocl_render_view_dep_expected_color_process(bprb_func_process& pro)
+bool boxm2_ocl_render_gl_view_dep_app_expected_color_process (bprb_func_process& pro)
 {
-  using namespace boxm2_ocl_render_view_dep_expected_color_process_globals;
-
-  vul_timer rtime;
+  using namespace boxm2_ocl_render_gl_view_dep_app_expected_color_process_globals;
   if ( pro.n_inputs() < n_inputs_ ) {
     vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
     return false;
   }
   //get the inputs
   unsigned i = 0;
-  bocl_device_sptr device= pro.get_input<bocl_device_sptr>(i++);
-  boxm2_scene_sptr scene =pro.get_input<boxm2_scene_sptr>(i++);
+  bocl_device_sptr        device       = pro.get_input<bocl_device_sptr>(i++);
+  boxm2_scene_sptr        scene        = pro.get_input<boxm2_scene_sptr>(i++);
+  boxm2_opencl_cache_sptr opencl_cache = pro.get_input<boxm2_opencl_cache_sptr>(i++);
+  vpgl_camera_double_sptr cam          = pro.get_input<vpgl_camera_double_sptr>(i++);
+  unsigned                ni           = pro.get_input<unsigned>(i++);
+  unsigned                nj           = pro.get_input<unsigned>(i++);
+  bocl_mem_sptr           exp_image    = pro.get_input<bocl_mem_sptr>(i++);
+  bocl_mem_sptr           exp_img_dim  = pro.get_input<bocl_mem_sptr>(i++);
+  vcl_string              ident        = pro.get_input<vcl_string>(i++);
+  bool                    is_bw        = pro.get_input<bool>(i++);
 
-  boxm2_opencl_cache_sptr opencl_cache= pro.get_input<boxm2_opencl_cache_sptr>(i++);
-  vpgl_camera_double_sptr cam= pro.get_input<vpgl_camera_double_sptr>(i++);
-  unsigned ni=pro.get_input<unsigned>(i++);
-  unsigned nj=pro.get_input<unsigned>(i++);
-  vcl_string ident = pro.get_input<vcl_string>(i++);
 
   vcl_string data_type;
   int apptypesize;
   vcl_vector<vcl_string> valid_types;
   valid_types.push_back(boxm2_data_traits<BOXM2_GAUSS_RGB_VIEW>::prefix());
+  valid_types.push_back(boxm2_data_traits<BOXM2_GAUSS_RGB_VIEW_COMPACT>::prefix());
   if ( !boxm2_util::verify_appearance( *scene, valid_types, data_type, apptypesize ) ) {
-    vcl_cout<<"boxm2_ocl_render_gl_view_dep_color_app_expected_image_process ERROR: scene doesn't have BOXM2_MOG6_VIEW data type"<<vcl_endl;
+    vcl_cout<<"boxm2_ocl_render_gl_view_dep_app_expected_image_process ERROR: scene doesn't have BOXM2_MOG6_VIEW data type"<<vcl_endl;
     return false;
   }
-
   vcl_string options = boxm2_ocl_util::mog_options(data_type);
 
-  if (ident.size() > 0) {
-    data_type += "_" + ident;
-  }
-
-//: create a command queue.
+  vcl_cout << options << " " << data_type << " " << apptypesize << vcl_endl;
+  // create a command queue.
   int status=0;
   cl_command_queue queue = clCreateCommandQueue(device->context(),*(device->device_id()),
                                                 CL_QUEUE_PROFILING_ENABLE,&status);
@@ -167,97 +161,57 @@ bool boxm2_ocl_render_view_dep_expected_color_process(bprb_func_process& pro)
   unsigned cl_ni=RoundUp(ni,lthreads[0]);
   unsigned cl_nj=RoundUp(nj,lthreads[1]);
 
+  //create float4 image here
   float* buff = new float[4*cl_ni*cl_nj];
   vcl_fill(buff, buff + 4*cl_ni*cl_nj, 0.0f);
-  bocl_mem_sptr exp_image = opencl_cache->alloc_mem(cl_ni*cl_nj*sizeof(cl_float4),buff,  "exp color image (float4) buffer");
-
-
-  //float* buff = new float[cl_ni*cl_nj];
-  //for (unsigned i=0;i<cl_ni*cl_nj;i++) buff[i]=0.0f;
- //bocl_mem_sptr exp_image=opencl_cache->alloc_mem(cl_ni*cl_nj*sizeof(float), buff,"exp image buffer");
-
-
-  exp_image->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
-
-
-  int img_dim_buff[4];
-  img_dim_buff[0] = 0;   img_dim_buff[2] = cl_ni;
-  img_dim_buff[1] = 0;   img_dim_buff[3] = cl_nj;
-  bocl_mem_sptr exp_img_dim=new bocl_mem(device->context(), img_dim_buff, sizeof(int)*4, "image dims");
-  exp_img_dim->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+  bocl_mem_sptr exp_color = new bocl_mem(device->context(), buff, 4*cl_ni*cl_nj*sizeof(float), "color im buffer (float4) buffer");
+  exp_color->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
 
   // visibility image
   float* vis_buff = new float[cl_ni*cl_nj];
   vcl_fill(vis_buff, vis_buff + cl_ni*cl_nj, 1.0f);
-  bocl_mem_sptr vis_image = opencl_cache->alloc_mem(cl_ni*cl_nj*sizeof(float), vis_buff,"vis image buffer");
+  bocl_mem_sptr vis_image = new bocl_mem(device->context(), vis_buff, cl_ni*cl_nj*sizeof(float), "vis image (single float) buffer");
   vis_image->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
-   float* max_omega_buff = new float[cl_ni*cl_nj];
-  vcl_fill(max_omega_buff, max_omega_buff + cl_ni*cl_nj, 0.0f);
-  bocl_mem_sptr max_omega_image = opencl_cache->alloc_mem(cl_ni*cl_nj*sizeof(float), max_omega_buff,"vis image buffer");
-  max_omega_image->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
-  float transfer_t=0;
-  float gpu_t=0;
-  // run expected image function
-  render_expected_image(scene, device, opencl_cache, queue,
-                        cam, exp_image, vis_image, max_omega_image,exp_img_dim,
-                        data_type, kernels[identifier][0], lthreads, cl_ni, cl_nj,apptypesize);
-  // normalize
-  if (kernels[identifier].size()>1)
-  {
-    vcl_size_t gThreads[] = {cl_ni,cl_nj};
-    bocl_kernel* normalize_kern = kernels[identifier][1];
-    normalize_kern->set_arg( exp_image.ptr() );
-    normalize_kern->set_arg( vis_image.ptr() );
-    normalize_kern->set_arg( exp_img_dim.ptr());
-    normalize_kern->execute( queue, 2, lthreads, gThreads);
-    clFinish(queue);
 
-    //clear render kernel args so it can reset em on next execution
-    normalize_kern->clear_args();
+  float* max_omega_buff = new float[cl_ni*cl_nj];
+  vcl_fill(max_omega_buff, max_omega_buff + cl_ni*cl_nj, 0.0f);
+  bocl_mem_sptr max_omega_image = new bocl_mem(device->context(), max_omega_buff, cl_ni*cl_nj*sizeof(float), "max_omega_image image (single float) buffer");
+  max_omega_image->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+
+  // run expected image function
+
+  float time = render_expected_image(scene, device, opencl_cache, queue,
+                                     cam, exp_color, vis_image, max_omega_image, exp_img_dim,
+                                     data_type, kernels[identifier][0], lthreads, cl_ni, cl_nj,apptypesize);
+
+  // normalize and write image to GL buffer
+  {
+    cl_bool isbw[1] = { is_bw };
+    bocl_mem_sptr is_bw_sptr = new bocl_mem(device->context(), isbw, sizeof(cl_bool), "is bw hack buffer");
+    is_bw_sptr->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+
+    vcl_size_t gThreads[] = {cl_ni,cl_nj};
+    bocl_kernel* norm_rgb_gl = kernels[identifier][1];
+    norm_rgb_gl->set_arg( exp_color.ptr() );
+    norm_rgb_gl->set_arg( vis_image.ptr() );
+    norm_rgb_gl->set_arg( exp_img_dim.ptr());
+    norm_rgb_gl->set_arg( exp_image.ptr() );
+    norm_rgb_gl->set_arg( is_bw_sptr.ptr() ) ;
+    norm_rgb_gl->execute( queue, 2, lthreads, gThreads);
+    clFinish(queue);
+    norm_rgb_gl->clear_args();
+    time += norm_rgb_gl->exec_time();
   }
+
+  //delete visibilty image
+  delete[] vis_buff;
+  delete[] buff;
 
   // read out expected image
-  exp_image->read_to_buffer(queue);
-  vis_image->read_to_buffer(queue);
-
-
-
-
-
- //vil_image_view<float>* exp_img_out=new vil_image_view<float>(ni,nj);
- // for (unsigned c=0;c<nj;c++)
- //   for (unsigned r=0;r<ni;r++)
- //     (*exp_img_out)(r,c)=buff[c*cl_ni+r];
-
- vil_image_view<vil_rgba<vxl_byte> >* exp_img_out = new vil_image_view<vil_rgba<vxl_byte> >(ni,nj);
-  int numFloats = 4;
-  int count = 0;
-  for (unsigned c=0;c<nj;++c) {
-    for (unsigned r=0;r<ni;++r,count+=numFloats) {
-      (*exp_img_out)(r,c) =
-      vil_rgba<vxl_byte> ( (vxl_byte) (buff[count]  * 255.0f),
-                           (vxl_byte) (buff[count+1]*255.0f),
-                           (vxl_byte) (buff[count+2]*255.0f),
-                           (vxl_byte) 255 );
-    }
-  }
-  vil_image_view<float>* vis_img_out=new vil_image_view<float>(ni,nj);
-  for (unsigned c=0;c<nj;c++)
-    for (unsigned r=0;r<ni;r++)
-      (*vis_img_out)(r,c)=vis_buff[c*cl_ni+r];
-
-
-  vcl_cout<<"Total Render time: "<<rtime.all()<<" ms"<<vcl_endl;
-  delete [] vis_buff;
-  delete [] buff;
-  delete [] max_omega_buff;
-  opencl_cache->unref_mem(vis_image.ptr());
-  opencl_cache->unref_mem(exp_image.ptr());
-  opencl_cache->unref_mem(max_omega_image.ptr());
   clReleaseCommandQueue(queue);
-  i=0;
-  // store scene smaprt pointer
-  pro.set_output_val<vil_image_view_base_sptr>(i++, exp_img_out);
-  pro.set_output_val<vil_image_view_base_sptr>(i++, vis_img_out);
+
+  // store scene smart pointer
+  int argIdx = 0;
+  pro.set_output_val<float>(argIdx, time);
   return true;
 }
