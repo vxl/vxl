9c0f461cde2fce194c8ae77f8cb12552d86174e4
Vishal Jain <vishal@visionsystemsinc.com>
2013-09-08 13:35:06 -0400

    FAB: was storing greyscale for color check

--- contrib/brl/bseg/boxm2/cpp/pro/processes/boxm2_export_stack_images_process.cxx
+++ contrib/brl/bseg/boxm2/cpp/pro/processes/boxm2_export_stack_images_process.cxx
@@ -25,71 +25,71 @@
 
 namespace boxm2_export_stack_images_process_globals
 {
-  const unsigned n_inputs_ = 4;
-  const unsigned n_outputs_ = 0;
+    const unsigned n_inputs_ = 4;
+    const unsigned n_outputs_ = 0;
 }
 
 bool boxm2_export_stack_images_process_cons(bprb_func_process& pro)
 {
-  using namespace boxm2_export_stack_images_process_globals;
+    using namespace boxm2_export_stack_images_process_globals;
 
-  //process takes 3 inputs, no outputs
-  int i=0;
-  vcl_vector<vcl_string> input_types_(n_inputs_);
-  input_types_[i++] = "boxm2_scene_sptr";
-  input_types_[i++] = "boxm2_cache_sptr";
-  input_types_[i++] = "vcl_string";                //output dir of saved DICOM images
-  input_types_[i++] = "bool";
+    //process takes 3 inputs, no outputs
+    int i=0;
+    vcl_vector<vcl_string> input_types_(n_inputs_);
+    input_types_[i++] = "boxm2_scene_sptr";
+    input_types_[i++] = "boxm2_cache_sptr";
+    input_types_[i++] = "vcl_string";                //output dir of saved DICOM images
+    input_types_[i++] = "bool";
 
-  brdb_value_sptr out_app = new brdb_value_t<bool>(true);
-  pro.set_input(3, out_app);
+    brdb_value_sptr out_app = new brdb_value_t<bool>(true);
+    pro.set_input(3, out_app);
 
-  vcl_vector<vcl_string> output_types_(n_outputs_);
-  return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
+    vcl_vector<vcl_string> output_types_(n_outputs_);
+    return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
 }
 
 bool boxm2_export_stack_images_process(bprb_func_process& pro)
 {
-  using namespace boxm2_export_stack_images_process_globals;
-  if ( pro.n_inputs() < n_inputs_ ) {
-    vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
-    return false;
-  }
-
-  //get the inputs
-  unsigned argIdx = 0;
-  boxm2_scene_sptr scene = pro.get_input<boxm2_scene_sptr>(argIdx++);
-  boxm2_cache_sptr cache = pro.get_input<boxm2_cache_sptr>(argIdx++);
-  vcl_string outdir = pro.get_input<vcl_string>(argIdx++);
-  bool opacity_only = pro.get_input<bool>(argIdx++);
-  //create the mesh directory
-  if (outdir != "") {
-    if (!vul_file::make_directory_path(outdir.c_str())) {
-      vcl_cout<<"Couldn't make directory path "<<outdir<<vcl_endl;
-      return false;
+    using namespace boxm2_export_stack_images_process_globals;
+    if ( pro.n_inputs() < n_inputs_ ) {
+        vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
+        return false;
     }
-  }
 
-  if (opacity_only)
-    boxm2_export_stack_images_function::export_opacity_stack_images(scene,cache,outdir);
-#if 0
-  else
-  {
-    vil3d_image_view<unsigned char> img3d;
-    if (scene->has_data_type("boxm2_gauss_rgb"))
-      boxm2_export_stack_images_function::export_greyscale_stack_images(scene,cache,img3d);
+    //get the inputs
+    unsigned argIdx = 0;
+    boxm2_scene_sptr scene = pro.get_input<boxm2_scene_sptr>(argIdx++);
+    boxm2_cache_sptr cache = pro.get_input<boxm2_cache_sptr>(argIdx++);
+    vcl_string outdir = pro.get_input<vcl_string>(argIdx++);
+    bool opacity_only = pro.get_input<bool>(argIdx++);
+    //create the mesh directory
+    if (outdir != "") {
+        if (!vul_file::make_directory_path(outdir.c_str())) {
+            vcl_cout<<"Couldn't make directory path "<<outdir<<vcl_endl;
+            return false;
+        }
+    }
+
+    if (opacity_only)
+        boxm2_export_stack_images_function::export_opacity_stack_images(scene,cache,outdir);
     else
-      boxm2_export_stack_images_function::export_color_stack_images(scene,cache,img3d);
-  }
-  for (unsigned k = 0 ;  k < img3d.nk() ;  k ++)
-  {
-    vil_image_view<unsigned char> img = vil3d_slice_ji(img3d,k);
-    vcl_stringstream ss;
-    ss<<outdir<<"/img_"<<vcl_setw(5) << vcl_setfill('0') << k<<".png";
-    vcl_cout<<"Filename : "<<ss.str()<<vcl_endl;
-    vil_save(img,ss.str().c_str());
-  }
-#endif
+    {
+        vil3d_image_view<unsigned char> img3d;
+        if (scene->has_data_type("boxm2_gauss_rgb"))
+            boxm2_export_stack_images_function::export_color_stack_images(scene,cache,img3d);
+        else
+            boxm2_export_stack_images_function::export_greyscale_stack_images(scene,cache,img3d);
 
-  return true;
+
+
+        for (unsigned k = 0 ;  k < img3d.nk() ;  k ++)
+        {
+            vil_image_view<unsigned char> img = vil3d_slice_ji(img3d,k);
+            vcl_stringstream ss;
+            ss<<outdir<<"/img_"<<vcl_setw(5) << vcl_setfill('0') << k<<".dcm";
+            vcl_cout<<"Filename : "<<ss.str()<<vcl_endl;
+            vil_save(img,ss.str().c_str());
+        }
+    }
+    return true;
 }
