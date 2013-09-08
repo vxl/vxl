b527755184c45bcd3654f2d8977d414c9ed13d81
Vishal Jain <vishal@visionsystemsinc.com>
2013-09-08 13:22:50 -0400

    Added a process to threshold imae at max

--- contrib/brl/bpro/core/vil_pro/vil_processes.h
+++ contrib/brl/bpro/core/vil_pro/vil_processes.h
@@ -59,6 +59,6 @@ DECLARE_FUNC_CONS(vil_fill_holes_in_regions_process);
 DECLARE_FUNC_CONS(vil_EO_IR_combine_process);
 DECLARE_FUNC_CONS(vil_truncate_image_process);
 DECLARE_FUNC_CONS(vil_grey_to_rgb_process);
-
+DECLARE_FUNC_CONS(vil_threshold_max_image_process);
 
 #endif
