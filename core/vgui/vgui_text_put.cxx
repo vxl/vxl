c5b96fa4c4d36c9125bb89559a7a0e16da22f8de
Joseph L. Mundy <mundyj@users.sourceforge.net>
2014-02-05 12:40:56 -0500

    added fonts from glut and char/string width functions

--- core/vgui/vgui_text_put.cxx
+++ core/vgui/vgui_text_put.cxx
@@ -20,7 +20,7 @@
 // multiple times if glut is used as the toolkit too.
 bool glut_was_initialized = false;
 
-void vgui_text_put(char const *str, unsigned size)
+void vgui_text_put(const char* str, unsigned size)
 {
   if( ! glut_was_initialized ) {
     glut_was_initialized = true;
@@ -28,14 +28,78 @@ void vgui_text_put(char const *str, unsigned size)
     char* argv[1] = { 0 };
     glutInit( &argc, argv );
   }
-
-  if (size > 17) {
-    for (unsigned k=0; str[k]; ++k)
-      glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, str[k]);
-  } else {
-    for (unsigned k=0; str[k]; ++k)
-      glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10, str[k]);
+  if (size > 20) {
+    for (unsigned k=0; str[k]; ++k){
+      int ic = static_cast<int>(str[k]);
+      glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ic);
+    }
   }
+  else if(size > 17){
+    for (unsigned k=0; str[k]; ++k){
+      int ic = static_cast<int>(str[k]);
+      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ic);
+    }
+  } else if (size > 10){
+    for (unsigned k=0; str[k]; ++k){
+      int ic = static_cast<int>(str[k]);
+      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ic);
+    }   
+  }else{
+    for (unsigned k=0; str[k]; ++k){
+      int ic = static_cast<int>(str[k]);
+      glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10, ic);
+    }
+  }
+}
+int vgui_text_length(const char* str, unsigned size){
+  if( ! glut_was_initialized ) {
+    glut_was_initialized = true;
+    int argc = 0;
+    char* argv[1] = { 0 };
+    glutInit( &argc, argv );
+  }
+  int w = 0;
+  if (size > 20) {
+    for (unsigned k=0; str[k]; ++k){
+      int ic = static_cast<int>(str[k]);
+      w+= glutBitmapWidth(GLUT_BITMAP_TIMES_ROMAN_24, ic);
+    }
+  } else if(size > 17){
+    for (unsigned k=0; str[k]; ++k){
+      int ic = static_cast<int>(str[k]);
+      w+= glutBitmapWidth(GLUT_BITMAP_HELVETICA_18, ic);
+    }
+  } else if (size > 10){
+    for (unsigned k=0; str[k]; ++k){
+      int ic = static_cast<int>(str[k]);
+      w+=  glutBitmapWidth(GLUT_BITMAP_HELVETICA_12, ic);
+    }
+  }else{
+    for (unsigned k=0; str[k]; ++k){
+      int ic = static_cast<int>(str[k]);
+      w+=  glutBitmapWidth(GLUT_BITMAP_TIMES_ROMAN_10, ic);
+    }
+  }
+  return w;
+}
+int vgui_char_width(unsigned char c, unsigned size){
+  if( ! glut_was_initialized ) {
+    glut_was_initialized = true;
+    int argc = 0;
+    char* argv[1] = { 0 };
+    glutInit( &argc, argv );
+  }
+  int ic = static_cast<int>(c);
+  if (size > 20) {
+    return glutBitmapWidth(GLUT_BITMAP_TIMES_ROMAN_24, ic);
+  } else if(size > 17){
+    return glutBitmapWidth(GLUT_BITMAP_HELVETICA_18, ic);
+  } else if (size > 10){
+    return  glutBitmapWidth(GLUT_BITMAP_HELVETICA_12, ic);
+  }else{
+    return  glutBitmapWidth(GLUT_BITMAP_TIMES_ROMAN_10, ic);
+  }
+  return 0;
 }
 #else
 #include <vgui/vgui_font_bitmap.h>
@@ -43,5 +107,16 @@ void vgui_text_put(char const *str, unsigned int size)
 {
   vgui_font_bitmap font(vgui_font_bitmap::BITMAP_FONT_8_13);
   font.draw(str);
+  return w;
+}
+int vgui_text_length(const unsigned char *s, unsigned size){
+  int w = 0;
+  int i = 0;
+  while(str[i]!=0){
+    i++; w+=8;
+  }
+}
+int vgui_char_width(unsigned char c, unsigned size){
+  return 8;
 }
 #endif
--- core/vgui/vgui_text_put.h
+++ core/vgui/vgui_text_put.h
@@ -11,8 +11,15 @@
 // \brief  Function for rendering text at the current raster position.
 
 //: Put the given text at the current raster position.
-// \param size Approximate point size of the text. Current
-// implementation only has 10pt and 24pt Times New Roman.
-void vgui_text_put(char const *s, unsigned size=24);
+// \param size Approximate point size of the text.
+// with glut, the choices are:
+//  times roman 24, 10
+//  helvectia 18, 12
+// without glut, the only choice is:
+// bitmap font 8x13
+void vgui_text_put(const char* s, unsigned size=24);
+// the return value is the total pixel width spanned by the char string
+int vgui_text_length(const char* s, unsigned size=24);
+int vgui_char_width(unsigned char s, unsigned size=24);
 
 #endif // vgui_text_put_h_
