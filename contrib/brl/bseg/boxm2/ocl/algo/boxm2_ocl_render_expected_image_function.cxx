6524e3bb59fcc677e6269720c50d50982942c074
Vishal Jain <vishal@visionsystemsinc.com>
2014-04-22 11:35:45 -0400

    added a rendering funciton with opencl_cache2

--- contrib/brl/bseg/boxm2/ocl/algo/boxm2_ocl_render_expected_image_function.cxx
+++ contrib/brl/bseg/boxm2/ocl/algo/boxm2_ocl_render_expected_image_function.cxx
@@ -10,7 +10,6 @@
 #include <brad/brad_image_metadata.h>
 #include <brad/brad_atmospheric_parameters.h>
 #include <brad/brad_illum_util.h>
-
 float render_expected_image(  boxm2_scene_sptr & scene,
                               bocl_device_sptr & device,
                               boxm2_opencl_cache_sptr & opencl_cache,
@@ -82,6 +81,114 @@ float render_expected_image(  boxm2_scene_sptr & scene,
         int alphaTypeSize   = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_ALPHA>::prefix());
         // data type string may contain an identifier so determine the buffer size
         bocl_mem* mog       = opencl_cache->get_data(*id,data_type,alpha->num_bytes()/alphaTypeSize*apptypesize,true);
+        transfer_time += (float) transfer.all();
+
+        ////3. SET args
+        kern->set_arg( blk_info );
+        kern->set_arg( blk );
+        kern->set_arg( alpha );
+        kern->set_arg( mog );
+        kern->set_arg( ray_o_buff.ptr() );
+        kern->set_arg( ray_d_buff.ptr() );
+        kern->set_arg( tnearfar_mem_ptr.ptr() );
+        kern->set_arg( exp_image.ptr() );
+        kern->set_arg( exp_img_dim.ptr());
+        kern->set_arg( cl_output.ptr() );
+        kern->set_arg( lookup.ptr() );
+        kern->set_arg( vis_image.ptr() );
+        kern->set_arg( max_omega_image.ptr() );
+
+        //local tree , cumsum buffer, imindex buffer
+        kern->set_local_arg( lthreads[0]*lthreads[1]*sizeof(cl_uchar16) );
+        kern->set_local_arg( lthreads[0]*lthreads[1]*10*sizeof(cl_uchar) );
+        kern->set_local_arg( lthreads[0]*lthreads[1]*sizeof(cl_int) );
+
+        //execute kernel
+        kern->execute(queue, 2, lthreads, gThreads);
+        clFinish(queue);
+        gpu_time += kern->exec_time();
+
+        //clear render kernel args so it can reset em on next execution
+        kern->clear_args();
+    }
+
+    //clean up cam
+    delete[] ray_origins;
+    delete[] ray_directions;
+    opencl_cache->unref_mem(ray_o_buff.ptr());
+    opencl_cache->unref_mem(ray_d_buff.ptr());
+    opencl_cache->unref_mem(cl_output.ptr());
+    opencl_cache->unref_mem(lookup.ptr());
+    vcl_cout<<"Gpu time "<<gpu_time<<" transfer time "<<transfer_time<<vcl_endl;
+    return gpu_time + transfer_time;
+}
+
+float render_expected_image(  boxm2_scene_sptr & scene,
+                              bocl_device_sptr & device,
+                              boxm2_opencl_cache2_sptr & opencl_cache,
+                              cl_command_queue & queue,
+                              vpgl_camera_double_sptr & cam,
+                              bocl_mem_sptr & exp_image,
+                              bocl_mem_sptr & vis_image,
+                              bocl_mem_sptr & max_omega_image,
+                              bocl_mem_sptr & exp_img_dim,
+                              vcl_string data_type,
+                              bocl_kernel* kernel,
+                              vcl_size_t * lthreads,
+                              unsigned cl_ni,
+                              unsigned cl_nj,
+                              int apptypesize,
+                            bocl_mem_sptr & tnearfar_mem_ptr )
+{
+    float transfer_time=0.0f;
+    float gpu_time=0.0f;
+
+    //camera check
+    if (cam->type_name()!= "vpgl_perspective_camera" &&
+        cam->type_name()!= "vpgl_generic_camera" ) {
+      vcl_cout<<"Cannot render with camera of type "<<cam->type_name()<<vcl_endl;
+      return 0.0f;
+    }
+
+    // create all buffers
+    cl_float* ray_origins = new cl_float[4*cl_ni*cl_nj];
+    cl_float* ray_directions = new cl_float[4*cl_ni*cl_nj];
+    bocl_mem_sptr ray_o_buff = opencl_cache->alloc_mem(cl_ni*cl_nj*sizeof(cl_float4), ray_origins, "ray_origins buffer");
+    bocl_mem_sptr ray_d_buff = opencl_cache->alloc_mem(cl_ni*cl_nj*sizeof(cl_float4), ray_directions, "ray_directions buffer");
+    boxm2_ocl_camera_converter::compute_ray_image( device, queue, cam, cl_ni, cl_nj, ray_o_buff, ray_d_buff);
+
+    // Output Array
+    float output_arr[100];
+    for (int i=0; i<100; ++i) output_arr[i] = 0.0f;
+    bocl_mem_sptr  cl_output=opencl_cache->alloc_mem(sizeof(float)*100,output_arr,  "output buffer");
+    cl_output->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+
+    // bit lookup buffer
+    cl_uchar lookup_arr[256];
+    boxm2_ocl_util::set_bit_lookup(lookup_arr);
+    bocl_mem_sptr lookup=opencl_cache->alloc_mem(  sizeof(cl_uchar)*256,lookup_arr, "bit lookup buffer");
+    lookup->create_buffer(CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR);
+
+    //2. set global thread size
+    vcl_size_t gThreads[] = {cl_ni,cl_nj};
+
+    // set arguments
+    vcl_vector<boxm2_block_id> vis_order = scene->get_vis_blocks_opt((vpgl_perspective_camera<double>*)cam.ptr(),cl_ni,cl_nj);
+    vcl_cout<<"Scene : "<<scene->data_path()<<' '<<vis_order.size()<<" cache size "<<opencl_cache->bytes_in_cache()<< vcl_endl;
+    vcl_vector<boxm2_block_id>::iterator id;
+    for (id = vis_order.begin(); id !=  vis_order.end(); ++id)
+    {
+        //choose correct render kernel
+        boxm2_block_metadata mdata = scene->get_block_metadata(*id);
+        bocl_kernel* kern =  kernel;
+        //write the image values to the buffer
+        vul_timer transfer;
+        bocl_mem* blk       = opencl_cache->get_block(scene,*id);
+        bocl_mem* blk_info  = opencl_cache->loaded_block_info();
+        bocl_mem* alpha     = opencl_cache->get_data<BOXM2_ALPHA>(scene,*id);
+        int alphaTypeSize   = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_ALPHA>::prefix());
+        // data type string may contain an identifier so determine the buffer size
+        bocl_mem* mog       = opencl_cache->get_data(scene,*id,data_type,alpha->num_bytes()/alphaTypeSize*apptypesize,true);
         transfer_time += (float) transfer.all();
 
         ////3. SET args
--- contrib/brl/bseg/boxm2/ocl/algo/boxm2_ocl_render_expected_image_function.h
+++ contrib/brl/bseg/boxm2/ocl/algo/boxm2_ocl_render_expected_image_function.h
@@ -6,9 +6,26 @@
 #include <boxm2/boxm2_scene.h>
 #include <boxm2/boxm2_block.h>
 #include <boxm2/ocl/boxm2_opencl_cache.h>
+#include <boxm2/ocl/boxm2_opencl_cache2.h>
 #include <brad/brad_image_metadata.h>
 #include <brad/brad_atmospheric_parameters.h>
 
+float render_expected_image( boxm2_scene_sptr & scene,
+                            bocl_device_sptr & device,
+                            boxm2_opencl_cache2_sptr & opencl_cache,
+                            cl_command_queue & queue,
+                            vpgl_camera_double_sptr & cam,
+                            bocl_mem_sptr & exp_image,
+                            bocl_mem_sptr & vis_image,
+                            bocl_mem_sptr & max_omega_image,
+                            bocl_mem_sptr & exp_img_dim,
+                            vcl_string data_type,
+                            bocl_kernel* kernel,
+                            vcl_size_t * lthreads,
+                            unsigned cl_ni,
+                            unsigned cl_nj,
+                            int apptypesize,
+                            bocl_mem_sptr & tnearfar_mem_ptr);
 float render_expected_image( boxm2_scene_sptr & scene,
                             bocl_device_sptr & device,
                             boxm2_opencl_cache_sptr & opencl_cache,
