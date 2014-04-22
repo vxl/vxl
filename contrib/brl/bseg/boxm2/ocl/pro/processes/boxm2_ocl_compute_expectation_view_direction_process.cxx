1eacb90059f13fd3e39a7ca302a842a6694578f8
Vishal Jain <vishal@visionsystemsinc.com>
2014-04-22 11:44:19 -0400

    added new processes for synoptic function

---
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_compute_expectation_view_direction_process.cxx
@@ -0,0 +1,265 @@
+// This is brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_compute_expectation_view_direction_process.cxx
+//:
+// \file
+// \brief  A process for updating the aux with viewing directions.
+//
+// \author Vishal Jain
+// \date Aug 08, 2011
+
+#include <bprb/bprb_func_process.h>
+
+#include <vcl_fstream.h>
+#include <vcl_algorithm.h>
+#include <boxm2/ocl/boxm2_opencl_cache.h>
+#include <boxm2/boxm2_scene.h>
+#include <boxm2/boxm2_block.h>
+#include <boxm2/boxm2_data_base.h>
+#include <boxm2/ocl/boxm2_ocl_util.h>
+#include <boxm2/boxm2_util.h>
+#include <vil/vil_image_view.h>
+#include <boxm2/ocl/algo/boxm2_ocl_camera_converter.h>
+#include <vpgl/vpgl_lvcs_sptr.h>
+#include <vpgl/file_formats/vpgl_geo_camera.h>
+
+//brdb stuff
+#include <brdb/brdb_value.h>
+
+//directory utility
+#include <vul/vul_timer.h>
+#include <vcl_where_root_dir.h>
+#include <bocl/bocl_device.h>
+#include <bocl/bocl_kernel.h>
+
+namespace boxm2_ocl_compute_expectation_view_direction_process_globals
+{
+    const unsigned n_inputs_  = 5;
+    const unsigned n_outputs_ = 0;
+
+    void compile_kernel(bocl_device_sptr device,vcl_vector<bocl_kernel*> & vec_kernels,vcl_string opts)
+    {
+        //gather all render sources... seems like a lot for rendering...
+        vcl_vector<vcl_string> src_paths;
+        vcl_string source_dir = boxm2_ocl_util::ocl_src_root();
+        src_paths.push_back(source_dir + "scene_info.cl");
+        src_paths.push_back(source_dir + "stat/directional_statistics.cl");
+
+        //compilation options
+        vcl_string options = opts;
+        //create all passes
+        vcl_string accumulate_opts = options + " -D ACCUMULATE_DIRECTION_VECTORS";
+        bocl_kernel* accumulate_direction_kernel = new bocl_kernel();
+        accumulate_direction_kernel->create_kernel(&device->context(),device->device_id(), src_paths, "accumulate_direction_vectors", accumulate_opts, "accumulate_direction_vectors");
+        vec_kernels.push_back(accumulate_direction_kernel);
+        
+        vcl_string dispersion_opts = options + " -D COMPUTE_DISPERSION";
+        bocl_kernel* compute_dispersion = new bocl_kernel();
+        compute_dispersion->create_kernel(&device->context(),device->device_id(), src_paths, "compute_dispersion",dispersion_opts, "compute_dispersion");
+        vec_kernels.push_back(compute_dispersion);
+        return ;
+    }
+
+
+    static vcl_map<vcl_string,vcl_vector<bocl_kernel*> > kernels;
+}
+
+bool boxm2_ocl_compute_expectation_view_direction_process_cons(bprb_func_process& pro)
+{
+    using namespace boxm2_ocl_compute_expectation_view_direction_process_globals;
+
+    //process takes 1 input
+    vcl_vector<vcl_string> input_types_(n_inputs_);
+    input_types_[0] = "bocl_device_sptr";
+    input_types_[1] = "boxm2_scene_sptr";
+    input_types_[2] = "boxm2_opencl_cache_sptr";
+    input_types_[3] = "vcl_string";   // file with list of identifiers
+    input_types_[4] = "vcl_string";   // coordinate type  
+
+    // process has 1 output:
+    // output[0]: scene sptr
+    vcl_vector<vcl_string>  output_types_(n_outputs_);
+
+    bool good = pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
+    brdb_value_sptr idx = new brdb_value_t<vcl_string>("");
+    pro.set_input(3, idx);
+    brdb_value_sptr idx_coordinate_type = new brdb_value_t<vcl_string>("Cartesian");
+    pro.set_input(4, idx_coordinate_type);
+    return good;
+}
+
+bool boxm2_ocl_compute_expectation_view_direction_process(bprb_func_process& pro)
+{
+    using namespace boxm2_ocl_compute_expectation_view_direction_process_globals;
+    vcl_size_t local_threads[1]={64};
+    vcl_size_t global_threads[1]={64};
+
+    if ( pro.n_inputs() < n_inputs_ ) {
+        vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
+        return false;
+    }
+    float transfer_time=0.0f;
+    float gpu_time=0.0f;
+    //get the inputs
+    unsigned i = 0;
+    bocl_device_sptr device               = pro.get_input<bocl_device_sptr>(i++);
+    boxm2_scene_sptr scene                = pro.get_input<boxm2_scene_sptr>(i++);
+    boxm2_opencl_cache_sptr opencl_cache  = pro.get_input<boxm2_opencl_cache_sptr>(i++);
+    vcl_string identifier_file            = pro.get_input<vcl_string>(i++);
+    vcl_string coordinate_type            = pro.get_input<vcl_string>(i++);
+    long binCache = opencl_cache.ptr()->bytes_in_cache();
+    vcl_cout<<"Update MBs in cache: "<<binCache/(1024.0*1024.0)<<vcl_endl;
+
+    // create a command queue.
+    int status=0;
+    cl_command_queue queue = clCreateCommandQueue(device->context(),*(device->device_id()),
+        CL_QUEUE_PROFILING_ENABLE,&status);
+    if (status!=0) 
+        return false;
+
+    vcl_string identifier=device->device_identifier();
+    // compile the kernel if not already compiled
+    if (kernels.find(identifier)==kernels.end())
+    {
+        vcl_cout<<"===========Compiling kernels==========="<<vcl_endl;
+        vcl_vector<bocl_kernel*> ks;
+        compile_kernel(device,ks,"");
+        kernels[identifier]=ks;
+    }
+    vcl_ifstream ifile( identifier_file.c_str() );
+    if(!ifile ) 
+    {
+        vcl_cout<<"Cannot open the files for suffixes "<<vcl_endl;
+        return false;
+    }
+    vcl_vector<vcl_string> suffixes;
+    while(!ifile.eof())
+    {
+        vcl_string suffix;
+        ifile>>suffix;
+        suffixes.push_back(suffix);
+    }
+    ifile.close();
+    // Output Array
+    float output_arr[100];
+    for (int i=0; i<100; ++i) output_arr[i] = 0.0f;
+    bocl_mem_sptr  cl_output=new bocl_mem(device->context(), output_arr, sizeof(float)*100, "output buffer");
+    cl_output->create_buffer(CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+
+    // bit lookup buffer
+    cl_uchar lookup_arr[256];
+    boxm2_ocl_util::set_bit_lookup(lookup_arr);
+    bocl_mem_sptr lookup=new bocl_mem(device->context(), lookup_arr, sizeof(cl_uchar)*256, "bit lookup buffer");
+    lookup->create_buffer(CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR);
+
+    vcl_vector<boxm2_block_id> vis_order = scene->get_block_ids();
+    for (vcl_vector<boxm2_block_id>::iterator id = vis_order.begin(); id != vis_order.end(); ++id)
+    {
+        boxm2_block_metadata mdata = scene->get_block_metadata(*id);
+        //write the image values to the buffer
+        vul_timer transfer;
+        bocl_mem* blk       = opencl_cache->get_block(*id);
+        bocl_mem* blk_info  = opencl_cache->loaded_block_info();
+        bocl_mem* alpha     = opencl_cache->get_data<BOXM2_ALPHA>(*id,0,false);
+        boxm2_scene_info* info_buffer = (boxm2_scene_info*) blk_info->cpu_buffer();
+        int alphaTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_ALPHA>::prefix());
+        info_buffer->data_buffer_length = (int) (alpha->num_bytes()/alphaTypeSize);
+        blk_info->write_to_buffer((queue));
+        int auxTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX0>::prefix());
+        bocl_mem *aux_direction_expectation_x  = opencl_cache->get_data(*id, boxm2_data_traits<BOXM2_AUX0>::prefix("direction_expectation_x"),info_buffer->data_buffer_length*auxTypeSize,false);
+        auxTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX0>::prefix());
+        bocl_mem *aux_direction_expectation_y  = opencl_cache->get_data(*id, boxm2_data_traits<BOXM2_AUX0>::prefix("direction_expectation_y"),info_buffer->data_buffer_length*auxTypeSize,false);
+        auxTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX0>::prefix());
+        bocl_mem *aux_direction_expectation_z  = opencl_cache->get_data(*id, boxm2_data_traits<BOXM2_AUX0>::prefix("direction_expectation_z"),info_buffer->data_buffer_length*auxTypeSize,false);
+        auxTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX0>::prefix());
+        bocl_mem *aux_vis_expectation  = opencl_cache->get_data(*id, boxm2_data_traits<BOXM2_AUX0>::prefix("visibility_expectation"),info_buffer->data_buffer_length*auxTypeSize,false);
+
+        bocl_kernel* kern =  kernels[identifier][0];
+        //choose correct render kernel
+        global_threads[0]=RoundUp(info_buffer->data_buffer_length,local_threads[0]);
+        /*for(unsigned int k = 0 ; k < suffixes.size() ; k++)
+        {
+            vcl_string suffix = suffixes[k];
+            //grab an appropriately sized AUX data buffer
+            auxTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX0>::prefix());
+            bocl_mem *aux0  = opencl_cache->get_data(*id, boxm2_data_traits<BOXM2_AUX0>::prefix("viewdir_"+suffix),info_buffer->data_buffer_length*auxTypeSize,false);
+            auxTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX1>::prefix());
+            bocl_mem *aux1  = opencl_cache->get_data(*id, boxm2_data_traits<BOXM2_AUX1>::prefix("viewdir_"+suffix),info_buffer->data_buffer_length*auxTypeSize,false);
+            auxTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX2>::prefix());
+            bocl_mem *aux2  = opencl_cache->get_data(*id, boxm2_data_traits<BOXM2_AUX2>::prefix("viewdir_"+suffix),info_buffer->data_buffer_length*auxTypeSize,false);
+            auxTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX2>::prefix());
+            bocl_mem *aux3  = opencl_cache->get_data(*id, boxm2_data_traits<BOXM2_AUX3>::prefix("viewdir_"+suffix),info_buffer->data_buffer_length*auxTypeSize,false);
+            auxTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX0>::prefix());
+            bocl_mem *aux0_img  = opencl_cache->get_data(*id, boxm2_data_traits<BOXM2_AUX0>::prefix("img_"+suffix),info_buffer->data_buffer_length*auxTypeSize,false);
+            auxTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX2>::prefix());
+            bocl_mem *aux2_img  = opencl_cache->get_data(*id, boxm2_data_traits<BOXM2_AUX2>::prefix("img_"+suffix),info_buffer->data_buffer_length*auxTypeSize,false);
+            kern->set_arg( blk_info );
+            kern->set_arg( aux0 );
+            kern->set_arg( aux1 );
+            kern->set_arg( aux2 );
+            kern->set_arg( aux3 );
+            kern->set_arg( aux0_img );
+            kern->set_arg( aux2_img );
+            kern->set_arg( aux_direction_expectation_x );
+            kern->set_arg( aux_direction_expectation_y );
+            kern->set_arg( aux_direction_expectation_z );
+            kern->set_arg( aux_vis_expectation );
+            kern->set_arg( cl_output.ptr() );
+            //execute kernel
+            kern->execute(queue, 1, local_threads, global_threads);
+            int status = clFinish(queue);
+            check_val(status, MEM_FAILURE, "UPDATE EXECUTE FAILED: " + error_to_string(status));
+            gpu_time += kern->exec_time();
+            //clear render kernel args so it can reset em on next execution
+            kern->clear_args();
+            //read image out to buffer (from gpu)
+            clFinish(queue);
+            opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_AUX0>::prefix("viewdir_"+suffix),false);
+            opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_AUX1>::prefix("viewdir_"+suffix),false);
+            opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_AUX2>::prefix("viewdir_"+suffix),false);
+            opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_AUX3>::prefix("viewdir_"+suffix),false);
+            opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_AUX0>::prefix("img_"+suffix),false);
+            opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_AUX2>::prefix("img_"+suffix),false);
+        }*/
+
+
+        //: Compute dispersion and mean direction
+        kern =  kernels[identifier][1];
+        {
+            auxTypeSize = (int)boxm2_data_info::datasize(boxm2_data_traits<BOXM2_AUX0>::prefix());
+            bocl_mem *aux_dispersion  = opencl_cache->get_data(*id, boxm2_data_traits<BOXM2_AUX0>::prefix("dispersion"),info_buffer->data_buffer_length*auxTypeSize,false);
+            kern->set_arg( blk_info );
+            kern->set_arg( alpha );
+            kern->set_arg( aux_direction_expectation_x );
+            kern->set_arg( aux_direction_expectation_y );
+            kern->set_arg( aux_direction_expectation_z );
+            kern->set_arg( aux_vis_expectation );
+            kern->set_arg( aux_dispersion );
+            kern->set_arg( cl_output.ptr() );
+            //execute kernel
+            kern->execute(queue, 1, local_threads, global_threads);
+            int status = clFinish(queue);
+            check_val(status, MEM_FAILURE, "UPDATE EXECUTE FAILED: " + error_to_string(status));
+            gpu_time += kern->exec_time();
+            aux_dispersion->read_to_buffer(queue);
+            aux_direction_expectation_x->read_to_buffer(queue);
+            aux_direction_expectation_y->read_to_buffer(queue);
+            aux_direction_expectation_z->read_to_buffer(queue);
+            aux_vis_expectation->read_to_buffer(queue);
+            alpha->read_to_buffer(queue);
+            opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_AUX0>::prefix("direction_expectation_x"),true);
+            opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_AUX0>::prefix("direction_expectation_y"),true);
+            opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_AUX0>::prefix("direction_expectation_z"),true);
+            opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_AUX0>::prefix("visibility_expectation"),true);
+            opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_AUX0>::prefix("dispersion"),true);
+            //opencl_cache->deep_remove_data(*id,boxm2_data_traits<BOXM2_ALPHA>::prefix(),true);
+            //clear render kernel args so it can reset em on next execution
+            kern->clear_args();
+            //read image out to buffer (from gpu)
+            clFinish(queue);
+        }
+    }
+
+    vcl_cout<<"Gpu time "<<gpu_time<<" transfer time "<<transfer_time<<vcl_endl;
+    opencl_cache->get_cpu_cache()->write_to_disk();
+    clReleaseCommandQueue(queue);
+    return true;
+}
---
+++ contrib/brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_fuse_based_visibility_process.cxx
@@ -0,0 +1,160 @@
+// This is brl/bseg/boxm2/ocl/pro/processes/boxm2_ocl_fuse_based_visibility_process.cxx
+#include <bprb/bprb_func_process.h>
+//:
+// \file
+// \brief  A process for fusing the scene based on visibility
+//
+// \author Vishal Jain
+// \date Nov 13, 2013
+
+#include <vcl_fstream.h>
+#include <vcl_algorithm.h>
+#include <boxm2/ocl/boxm2_opencl_cache2.h>
+#include <boxm2/boxm2_scene.h>
+#include <boxm2/boxm2_block.h>
+#include <boxm2/boxm2_data_base.h>
+#include <boxm2/ocl/boxm2_ocl_util.h>
+#include <boxm2/boxm2_util.h>
+#include <vil/vil_image_view.h>
+
+
+#include <boxm2/ocl/algo/boxm2_ocl_fuse_based_visibility.h>
+
+//brdb stuff
+#include <brdb/brdb_value.h>
+
+//directory utility
+#include <vcl_where_root_dir.h>
+#include <bocl/bocl_device.h>
+#include <bocl/bocl_kernel.h>
+#include <vul/vul_timer.h>
+
+namespace boxm2_ocl_fuse_based_visibility_process_globals
+{
+  const unsigned int n_inputs_  = 4;
+  const unsigned int n_outputs_ = 0;
+}
+
+bool boxm2_ocl_fuse_based_visibility_process_cons(bprb_func_process& pro)
+{
+  using namespace boxm2_ocl_fuse_based_visibility_process_globals;
+
+  //process takes 9 inputs (of which the four last ones are optional):
+  vcl_vector<vcl_string> input_types_(n_inputs_);
+  input_types_[0] = "bocl_device_sptr";
+  input_types_[1] = "boxm2_scene_sptr"; // scene A
+  input_types_[2] = "boxm2_scene_sptr"; // scene B
+  input_types_[3] = "boxm2_opencl_cache2_sptr";
+  // process has no outputs
+  vcl_vector<vcl_string>  output_types_(n_outputs_);
+  bool good = pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
+
+  return good;
+}
+
+bool boxm2_ocl_fuse_based_visibility_process(bprb_func_process& pro)
+{
+  using namespace boxm2_ocl_fuse_based_visibility_process_globals;
+
+  //sanity check inputs
+  if ( pro.n_inputs() < n_inputs_ ) {
+    vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
+    return false;
+  }
+  //get the inputs
+  unsigned int i = 0;
+  bocl_device_sptr         device       = pro.get_input<bocl_device_sptr>(i++);
+  boxm2_scene_sptr         sceneA        = pro.get_input<boxm2_scene_sptr>(i++);
+  boxm2_scene_sptr         sceneB        = pro.get_input<boxm2_scene_sptr>(i++);
+  boxm2_opencl_cache2_sptr  opencl_cache = pro.get_input<boxm2_opencl_cache2_sptr>(i++);
+  vul_timer t;
+  t.mark();
+  boxm2_ocl_fuse_based_visibility::fuse_based_visibility(sceneA,sceneB, device, opencl_cache);
+  vcl_cout<<"Total time taken is "<<t.all()<<vcl_endl;
+}
+namespace boxm2_ocl_fuse_based_orientation_process_globals
+{
+  const unsigned int n_inputs_  = 4;
+  const unsigned int n_outputs_ = 0;
+}
+
+bool boxm2_ocl_fuse_based_orientation_process_cons(bprb_func_process& pro)
+{
+  using namespace boxm2_ocl_fuse_based_orientation_process_globals;
+
+  //process takes 9 inputs (of which the four last ones are optional):
+  vcl_vector<vcl_string> input_types_(n_inputs_);
+  input_types_[0] = "bocl_device_sptr";
+  input_types_[1] = "boxm2_scene_sptr"; // scene A
+  input_types_[2] = "boxm2_scene_sptr"; // scene B
+  input_types_[3] = "boxm2_opencl_cache2_sptr";
+  // process has no outputs
+  vcl_vector<vcl_string>  output_types_(n_outputs_);
+  bool good = pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
+
+  return good;
+}
+
+bool boxm2_ocl_fuse_based_orientation_process(bprb_func_process& pro)
+{
+  using namespace boxm2_ocl_fuse_based_orientation_process_globals;
+
+  //sanity check inputs
+  if ( pro.n_inputs() < n_inputs_ ) {
+    vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
+    return false;
+  }
+  //get the inputs
+  unsigned int i = 0;
+  bocl_device_sptr         device       = pro.get_input<bocl_device_sptr>(i++);
+  boxm2_scene_sptr         sceneA        = pro.get_input<boxm2_scene_sptr>(i++);
+  boxm2_scene_sptr         sceneB        = pro.get_input<boxm2_scene_sptr>(i++);
+  boxm2_opencl_cache2_sptr  opencl_cache = pro.get_input<boxm2_opencl_cache2_sptr>(i++);
+  vul_timer t;
+  t.mark();
+  boxm2_ocl_fuse_based_orientation::fuse_based_orientation(sceneA,sceneB, device, opencl_cache);
+  vcl_cout<<"Total time taken is "<<t.all()<<vcl_endl;
+}
+namespace boxm2_ocl_fuse_surface_density_process_globals
+{
+  const unsigned int n_inputs_  = 4;
+  const unsigned int n_outputs_ = 0;
+}
+
+bool boxm2_ocl_fuse_surface_density_process_cons(bprb_func_process& pro)
+{
+  using namespace boxm2_ocl_fuse_surface_density_process_globals;
+
+  //process takes 9 inputs (of which the four last ones are optional):
+  vcl_vector<vcl_string> input_types_(n_inputs_);
+  input_types_[0] = "bocl_device_sptr";
+  input_types_[1] = "boxm2_scene_sptr"; // scene A
+  input_types_[2] = "boxm2_scene_sptr"; // scene B
+  input_types_[3] = "boxm2_opencl_cache2_sptr";
+  // process has no outputs
+  vcl_vector<vcl_string>  output_types_(n_outputs_);
+  bool good = pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
+
+  return good;
+}
+
+bool boxm2_ocl_fuse_surface_density_process(bprb_func_process& pro)
+{
+  using namespace boxm2_ocl_fuse_surface_density_process_globals;
+
+  //sanity check inputs
+  if ( pro.n_inputs() < n_inputs_ ) {
+    vcl_cout << pro.name() << ": The input number should be " << n_inputs_<< vcl_endl;
+    return false;
+  }
+  //get the inputs
+  unsigned int i = 0;
+  bocl_device_sptr         device       = pro.get_input<bocl_device_sptr>(i++);
+  boxm2_scene_sptr         sceneA        = pro.get_input<boxm2_scene_sptr>(i++);
+  boxm2_scene_sptr         sceneB        = pro.get_input<boxm2_scene_sptr>(i++);
+  boxm2_opencl_cache2_sptr  opencl_cache = pro.get_input<boxm2_opencl_cache2_sptr>(i++);
+  vul_timer t;
+  t.mark();
+  boxm2_ocl_fuse_surface_density::fuse_surface_density(sceneA,sceneB, device, opencl_cache);
+  vcl_cout<<"Total time taken is "<<t.all()<<vcl_endl;
+}
