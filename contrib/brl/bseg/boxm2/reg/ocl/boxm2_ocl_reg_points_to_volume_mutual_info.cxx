321d89eba8aade6f28305101b4c78b89320d38bd
Daniel Crispell <dancrispell@gmail.com>
2014-05-05 14:35:23 -0400

    fixed compile error

--- contrib/brl/bseg/boxm2/reg/ocl/boxm2_ocl_reg_points_to_volume_mutual_info.cxx
+++ contrib/brl/bseg/boxm2/reg/ocl/boxm2_ocl_reg_points_to_volume_mutual_info.cxx
@@ -132,7 +132,7 @@ bool boxm2_ocl_reg_points_to_volume_mutual_info::boxm2_ocl_register_world(vgl_ro
     bocl_mem_sptr ocl_depth = new bocl_mem(device_->context(), &(depth), sizeof(int), "  depth of octree " );
     ocl_depth->create_buffer(CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR );
 
-    vcl_vector<boxm2_block_id>& blocks_B = sceneB_->get_block_ids();
+    vcl_vector<boxm2_block_id> blocks_B = sceneB_->get_block_ids();
     vcl_vector<boxm2_block_id>::iterator iter_blks_B = blocks_B.begin();
     vcl_size_t local_threads[1]={64};
     vcl_size_t global_threads[1]={1};
