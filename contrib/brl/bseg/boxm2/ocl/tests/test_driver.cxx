0dbc261ea25b073dc4a73a1c3da3ef1bdf293e2d
Vishal Jain <vishal@visionsystemsinc.com>
2014-05-15 11:40:45 -0400

    fixed the test_refine

--- contrib/brl/bseg/boxm2/ocl/tests/test_driver.cxx
+++ contrib/brl/bseg/boxm2/ocl/tests/test_driver.cxx
@@ -1,5 +1,6 @@
 #include <testlib/testlib_register.h>
 
+DECLARE( test_refine );
 DECLARE( test_image_pyramid );
 DECLARE( test_process_mains ); 
 DECLARE( test_weighted_em   ); 
@@ -12,10 +13,11 @@ DECLARE( test_opencl_cache2 );
 
 void register_tests()
 {
+  REGISTER(test_refine);
   REGISTER( test_image_pyramid );
   REGISTER( test_process_mains ); 
   REGISTER( test_weighted_em   );
-  REGISTER( test_kernel_filter );
+  REGISTER( test_kernel_filter );   
   REGISTER( test_kernel_vector_filter );
   REGISTER( test_ocl_cholesky );
   REGISTER( test_ocl_levenberg_marquardt );
