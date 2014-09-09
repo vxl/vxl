vxl/vxl

commit 9a27bf34d0315f73a624846a9bbba524a92575e1

Simplify vxl_config_macros conditions with if(DEFINED)
Replace old hacks of the form 'if("${VAR}" MATCHES "^${VAR}$")'
with the much simpler 'if(NOT DEFINED ${VAR})'.

config/cmake/config/vxl_config_macros.cmake
@@ -12,7 +12,7 @@ MACRO(PERFORM_CMAKE_TEST FILE TEST)
   IF( VXL_UPDATE_CONFIGURATION )
     SET( ${TEST} "${TEST}" )
   ENDIF( VXL_UPDATE_CONFIGURATION )
-  IF("${TEST}" MATCHES "^${TEST}$")
+  IF(NOT DEFINED "${TEST}")
     # Perform test
     SET(MACRO_CHECK_FUNCTION_DEFINITIONS
         "-D${TEST} ${CMAKE_REQUIRED_FLAGS}")
@@ -38,12 +38,12 @@ MACRO(PERFORM_CMAKE_TEST FILE TEST)
                  "Performing Test ${TEST} failed with the following output:\n"
                  "${OUTPUT}\n" APPEND)
     ENDIF(${TEST})
-  ELSE("${TEST}" MATCHES "^${TEST}$")
+  ELSE()
     # Have result
     #FOREACH(tst ${TEST})
     #  MESSAGE("Test ${TEST} resulted in ${${tst}}")
     #ENDFOREACH(tst ${TEST})
-  ENDIF("${TEST}" MATCHES "^${TEST}$")
+  ENDIF()
 ENDMACRO(PERFORM_CMAKE_TEST FILE TEST)
 
 #
@@ -59,7 +59,7 @@ MACRO(PERFORM_CMAKE_TEST_CUSTOM DIR TEST)
   IF( VXL_UPDATE_CONFIGURATION )
     SET( ${TEST} "${TEST}" )
   ENDIF( VXL_UPDATE_CONFIGURATION )
-  IF("${TEST}" MATCHES "^${TEST}$")
+  IF(NOT DEFINED "${TEST}")
     # Perform test
     SET(MACRO_CHECK_FUNCTION_DEFINITIONS
         "-D${TEST} ${CMAKE_REQUIRED_FLAGS}")
@@ -88,7 +88,7 @@ MACRO(PERFORM_CMAKE_TEST_CUSTOM DIR TEST)
                  "Performing Test ${TEST} failed with the following output:\n"
                  "${OUTPUT}\n" APPEND)
     ENDIF(${TEST})
-  ENDIF("${TEST}" MATCHES "^${TEST}$")
+  ENDIF()
   FILE(REMOVE_RECURSE ${CMAKE_BINARY_DIR}/config/${DIR})
 ENDMACRO(PERFORM_CMAKE_TEST_CUSTOM DIR TEST)
 
@@ -103,7 +103,7 @@ MACRO(PERFORM_CMAKE_TEST_RUN FILE TEST)
   IF( VXL_UPDATE_CONFIGURATION )
     SET( ${TEST} "${TEST}" )
   ENDIF( VXL_UPDATE_CONFIGURATION )
-  IF("${TEST}" MATCHES "^${TEST}$")
+  IF(NOT DEFINED "${TEST}")
     # Perform test
     SET(MACRO_CHECK_FUNCTION_DEFINITIONS
         "-D${TEST} ${CMAKE_REQUIRED_FLAGS}")
@@ -140,12 +140,12 @@ MACRO(PERFORM_CMAKE_TEST_RUN FILE TEST)
                  "Performing Try-Run Test ${TEST} failed to compile with the following output:\n"
                  "${OUTPUT}\n" APPEND)
     ENDIF(${TEST}_COMPILED)
-  ELSE("${TEST}" MATCHES "^${TEST}$")
+  ELSE()
     # Have result
     #FOREACH(tst ${TEST})
     #  MESSAGE("Test ${TEST} resulted in ${${tst}}")
     #ENDFOREACH(tst ${TEST})
-  ENDIF("${TEST}" MATCHES "^${TEST}$")
+  ENDIF()
 ENDMACRO(PERFORM_CMAKE_TEST_RUN FILE TEST)
 
 #
@@ -157,9 +157,9 @@ MACRO(PERFORM_CHECK_HEADER FILE VARIABLE)
     UNSET(${VARIABLE})
   ENDIF( VXL_UPDATE_CONFIGURATION )
   CHECK_INCLUDE_FILE_CXX(${FILE} ${VARIABLE})
-  IF("${VARIABLE}" MATCHES "^$")
+  IF("x${VARIABLE}" STREQUAL "x")
     SET(${VARIABLE} 0)
-  ENDIF("${VARIABLE}" MATCHES "^$")
+  ENDIF()
 ENDMACRO(PERFORM_CHECK_HEADER FILE VARIABLE)
 
 #
@@ -197,7 +197,7 @@ MACRO(CHECK_TYPE_EXISTS TYPE FILES VARIABLE)
   IF( VXL_UPDATE_CONFIGURATION )
     SET( ${VARIABLE} "${VARIABLE}" )
   ENDIF( VXL_UPDATE_CONFIGURATION )
-  IF("${VARIABLE}" MATCHES "^${VARIABLE}$")
+  IF(NOT DEFINED "${VARIABLE}")
     SET(CHECK_TYPE_EXISTS_CONTENT "/* */\n")
     SET(MACRO_CHECK_TYPE_EXISTS_FLAGS ${CMAKE_REQUIRED_FLAGS})
     IF(CMAKE_REQUIRED_LIBRARIES)
@@ -239,7 +239,7 @@ MACRO(CHECK_TYPE_EXISTS TYPE FILES VARIABLE)
            "${OUTPUT}\nFile ${CMAKE_BINARY_DIR}/CMakeTmp/CheckTypeExists.c:\n"
            "${CHECK_TYPE_EXISTS_CONTENT}\n")
     ENDIF(${VARIABLE})
-  ENDIF("${VARIABLE}" MATCHES "^${VARIABLE}$")
+  ENDIF()
 ENDMACRO(CHECK_TYPE_EXISTS TYPE FILES VARIABLE)
 
 #

commit e19a8afbf0147677169a0427dd9f7f5dee0c3641

Fix one more vxl_config_macros check broken by refactoring
In commit 9a27bf34 (Simplify vxl_config_macros conditions with
if(DEFINED), 2014-09-08) we removed sevaral uses of implicit variable
evaluation.  However, CHECK_TYPE_EXISTS was abusing details of the prior
implementation and depended on setting a check result variable to its
own name to reset the check.  Use unset() instead.

config/cmake/config/vxl_config_macros.cmake
@@ -195,7 +195,7 @@ ENDMACRO(SET_INVERT VAR VALUE)
 
 MACRO(CHECK_TYPE_EXISTS TYPE FILES VARIABLE)
   IF( VXL_UPDATE_CONFIGURATION )
-    SET( ${VARIABLE} "${VARIABLE}" )
+    UNSET( ${VARIABLE} )
   ENDIF( VXL_UPDATE_CONFIGURATION )
   IF(NOT DEFINED "${VARIABLE}")
     SET(CHECK_TYPE_EXISTS_CONTENT "/* */\n")