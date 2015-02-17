c8863deea44f2d6bc8b1f7bbe95ab3d4cf9d3db3
Daniel Crispell <dancrispell@gmail.com>
2015-02-17 12:54:10 -0500

    rewrite of graph visibility ordering computation.
    new ordering algorithm fixes some image artifacts in satellite image
    model building and rendering.  Should produce correct orderings for all
    cameras with non-intersecting rays.

--- contrib/brl/bseg/boxm2/boxm2_blocks_vis_graph.cxx
+++ contrib/brl/bseg/boxm2/boxm2_blocks_vis_graph.cxx
@@ -1,101 +1,218 @@
 #include <boxm2/boxm2_blocks_vis_graph.h>
+#include <vgl/vgl_intersection.h>
 #include <vcl_queue.h>
 
-boxm2_block_vis_graph::boxm2_block_vis_graph(vcl_map<boxm2_block_id,boxm2_block_metadata> & blkmetadata, vpgl_generic_camera<double> & cam ): nodes_()
+boxm2_block_vis_graph::boxm2_block_vis_graph(vcl_map<boxm2_block_id,boxm2_block_metadata> & blkmetadata, vpgl_generic_camera<double> const& cam ): nodes_()
 {
+    // bounding box representation of image bounds
+    vgl_box_2d<double> image_bounds(vgl_point_2d<double>(0,0),
+                                    vgl_point_2d<double>(cam.cols(), cam.rows()));
 
-    vcl_map<boxm2_block_id,boxm2_block_metadata>::iterator iter = blkmetadata.begin();
     vcl_list<boxm2_block_vis_graph_node_sptr> temp_nodes;
-    for(;iter!=blkmetadata.end();iter++)
+    // create a node for every block in the model
+    for(vcl_map<boxm2_block_id,boxm2_block_metadata>::iterator iter = blkmetadata.begin();
+        iter!=blkmetadata.end();iter++)
     {
-        boxm2_block_vis_graph_node * node = new boxm2_block_vis_graph_node();
-        node->id_ = iter->first;
-        temp_nodes.push_back(node);
-    }
-    vcl_list<boxm2_block_vis_graph_node_sptr>::iterator set_iter = temp_nodes.begin();
-    for(;set_iter != temp_nodes.end();set_iter++)
-    {
-        boxm2_block_metadata md = blkmetadata[(*set_iter)->id_];
-        vgl_box_3d<double> bbox = md.bbox();
-        vcl_vector<vgl_point_3d<double> > vertices =  bbox.vertices() ;
-        double u,v ;
-        for( int x = -1 ; x <=1; x+=2)
-            for( int y = -1 ; y <=1; y+=2)
-                for( int z = -1 ; z <=1; z+=2)
-                {
-                    cam.project(bbox.centroid_x()+ x * bbox.width()/2,
-                        bbox.centroid_y()+ y * bbox.height()/2,
-                        bbox.centroid_z()+ z * bbox.depth()/2,
-                        u,v);
-                    if(u <0.0 || v< 0.0 || u >= cam.cols()-1 || v >= cam.rows()-1 )
-                        continue;
-                    vgl_ray_3d<double> ray = cam.ray(u,v);
-                    //cam.project(
-                    if(ray.direction().x()*x > 0)
-                    {
-                        vcl_list<boxm2_block_vis_graph_node_sptr>::iterator find_iter
-                            = this->find(boxm2_block_id((*set_iter)->id_.i()+x,(*set_iter)->id_.j(),(*set_iter)->id_.k()), temp_nodes) ;
-                        if ( find_iter != temp_nodes.end() )
-                        {
-                            (*set_iter)->out_edges.insert((*find_iter)->id_);
-                            (*find_iter)->in_edges.insert((*set_iter)->id_);
-                        }
-                    }
-                    if(ray.direction().y()*y > 0)
-                    {
-                        vcl_list<boxm2_block_vis_graph_node_sptr>::iterator find_iter
-                            = this->find(boxm2_block_id((*set_iter)->id_.i(),(*set_iter)->id_.j()+y,(*set_iter)->id_.k()), temp_nodes) ;
-                        if ( find_iter != temp_nodes.end() )
-                        {
-                            (*set_iter)->out_edges.insert((*find_iter)->id_);
-                            (*find_iter)->in_edges.insert((*set_iter)->id_);
-                        }
-                    }
-                    if(ray.direction().z()*z > 0)
-                    {
-                        vcl_list<boxm2_block_vis_graph_node_sptr>::iterator find_iter
-                            = this->find(boxm2_block_id((*set_iter)->id_.i(),(*set_iter)->id_.j(),(*set_iter)->id_.k()+z), temp_nodes) ;
-                        if ( find_iter != temp_nodes.end() )
-                        {
-                            (*set_iter)->out_edges.insert((*find_iter)->id_);
-                            (*find_iter)->in_edges.insert((*set_iter)->id_);
-                        }
-                    }
-                }
-    }
-    //: find the block with minmum or 0 in edges
-    vcl_queue<boxm2_block_vis_graph_node_sptr> visitednodes ;
-    for(set_iter = temp_nodes.begin();set_iter != temp_nodes.end();set_iter++)
-    {
-        if((*set_iter)->in_edges.size() == 0)
-        {
-            (*set_iter)->visited = true;
-            visitednodes.push(*set_iter);
-            break;
+        vgl_box_3d<double> bbox = iter->second.bbox();
+
+        // project bounding box and check for any overlap with the image bounds
+        vgl_box_2d<double> projected_bbox;
+        const vcl_vector<vgl_point_3d<double> > block_corners = bbox.vertices();
+        for (vcl_vector<vgl_point_3d<double> >::const_iterator corner_it = block_corners.begin();
+             corner_it != block_corners.end(); ++corner_it) {
+          double u,v;
+          cam.project( corner_it->x(), corner_it->y(), corner_it->z(), u, v);
+          projected_bbox.add( vgl_point_2d<double>(u,v) );
+        }
+        vgl_box_2d<double> cropped_projection = vgl_intersection(projected_bbox, image_bounds);
+          // no need to consider blocks whose projections have no overlap with the image
+        if (!cropped_projection.is_empty() ) {
+          boxm2_block_vis_graph_node_sptr node(new boxm2_block_vis_graph_node(iter->first));
+          temp_nodes.push_back(node);
         }
     }
-    while(!visitednodes.empty())
-    {
-         boxm2_block_vis_graph_node_sptr node = visitednodes.front();
 
-         ordered_nodes_.push_back(node->id_);
-         vcl_cout<<node->id_<<": ";
-         for(vcl_set<boxm2_block_id>::iterator outiter = node->out_edges.begin(); outiter !=node->out_edges.end();outiter++)
-         {
- 
-             vcl_list<boxm2_block_vis_graph_node_sptr>::iterator find_iter = this->find( boxm2_block_id(*outiter), temp_nodes );
-             if(find_iter !=  temp_nodes.end()   )
-                 if(! (*find_iter)->visited)
-                 {
-                    visitednodes.push(*find_iter);
-                    (*find_iter)->visited = true;
-                    vcl_cout<<*outiter<<"  ";
-                 }
-         }
-         vcl_cout<<vcl_endl;
-         visitednodes.pop();
+    
+    // loop over each vis graph node and create necessary edges
+    // each edge A->B represents the constraint that block A must be processed before
+    // block B, i.e. block A is "in front of" block B from the perspective of cam
+    for(vcl_list<boxm2_block_vis_graph_node_sptr>::iterator node_iter = temp_nodes.begin();
+        node_iter != temp_nodes.end();node_iter++)
+    {
+      boxm2_block_id this_block_id = (*node_iter)->id_;
+      boxm2_block_metadata md = blkmetadata[this_block_id];
+      vgl_box_3d<double> block_bbox = md.bbox();
+
+      // project each face of the block into the image and get the nominal ray direction for that face.
+      // lots of code duplication between the X,Y,and Z cases; maybe somebody more clever can figure out how to eliminate it.
+      // -X and +X faces
+      for (unsigned xi =0; xi<2; ++xi) {
+        vgl_box_3d<double> block_face;
+        double x = xi == 0? block_bbox.min_x() : block_bbox.max_x();
+        double u,v;
+        vgl_box_2d<double> face_projection;
+        cam.project(x, block_bbox.min_y(), block_bbox.min_z(), u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+        cam.project(x, block_bbox.min_y(), block_bbox.max_z(), u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+        cam.project(x, block_bbox.max_y(), block_bbox.min_z(), u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+        cam.project(x, block_bbox.max_y(), block_bbox.max_z(), u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+
+        vgl_box_2d<double> cropped_face_projection = vgl_intersection(face_projection, image_bounds);
+
+        if (!cropped_face_projection.is_empty()) {
+          vgl_point_2d<double> face_proj_center = cropped_face_projection.centroid();
+          vgl_ray_3d<double> ray = cam.ray(face_proj_center.x(), face_proj_center.y());
+          bool this_block_in_front = (xi == 0)? ray.direction().x() < 0.0 : ray.direction().x() > 0.0;
+          if(this_block_in_front) {
+            // this block is in front of block on other side of face
+            boxm2_block_id other_block_id = this_block_id;
+            if (xi == 0) {
+              --other_block_id.i_;
+            }
+            else {
+              ++other_block_id.i_;
+            }
+            // check for other block in list of nodes
+            vcl_list<boxm2_block_vis_graph_node_sptr>::iterator other =
+              this->find(other_block_id, temp_nodes) ;
+            if ( other != temp_nodes.end() )
+            {
+              // found the other block, so add the edge
+              (*node_iter)->out_edges.insert((*other));
+              ++(*other)->num_incoming_edges_;
+            }
+          }
+        }
+      }
+      // -Y and +Y faces
+      for (unsigned yi =0; yi<2; ++yi) {
+        vgl_box_3d<double> block_face;
+        double y = yi == 0? block_bbox.min_y() : block_bbox.max_y();
+        double u,v;
+        vgl_box_2d<double> face_projection;
+        cam.project(block_bbox.min_x(), y, block_bbox.min_z(), u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+        cam.project(block_bbox.min_x(), y, block_bbox.max_z(), u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+        cam.project(block_bbox.max_x(), y, block_bbox.min_z(), u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+        cam.project(block_bbox.max_x(), y, block_bbox.max_z(), u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+
+        vgl_box_2d<double> cropped_face_projection = vgl_intersection(face_projection, image_bounds);
+
+        if (!cropped_face_projection.is_empty()) {
+          vgl_point_2d<double> face_proj_center = cropped_face_projection.centroid();
+          vgl_ray_3d<double> ray = cam.ray(face_proj_center.x(), face_proj_center.y());
+          bool this_block_in_front = (yi == 0)? ray.direction().y() < 0.0 : ray.direction().y() > 0.0;
+          if(this_block_in_front) {
+            // this block is in front of block on other side of face
+            boxm2_block_id other_block_id = this_block_id;
+            if (yi == 0) {
+              --other_block_id.j_;
+            }
+            else {
+              ++other_block_id.j_;
+            }
+            // check for other block in list of nodes
+            vcl_list<boxm2_block_vis_graph_node_sptr>::iterator other =
+              this->find(other_block_id, temp_nodes) ;
+            if ( other != temp_nodes.end() )
+            {
+              // found the other block, so add the edge
+              (*node_iter)->out_edges.insert((*other));
+              ++(*other)->num_incoming_edges_;
+            }
+          }
+        }
+      }
+      // -Z and +Z faces
+      for (unsigned zi =0; zi<2; ++zi) {
+        vgl_box_3d<double> block_face;
+        double z = zi == 0? block_bbox.min_z() : block_bbox.max_z();
+        double u,v;
+        vgl_box_2d<double> face_projection;
+        cam.project(block_bbox.min_x(), block_bbox.min_y(), z, u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+        cam.project(block_bbox.min_x(), block_bbox.max_y(), z, u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+        cam.project(block_bbox.max_x(), block_bbox.min_y(), z, u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+        cam.project(block_bbox.max_x(), block_bbox.max_y(), z, u, v);
+        face_projection.add(vgl_point_2d<double>(u,v));
+
+        vgl_box_2d<double> cropped_face_projection = vgl_intersection(face_projection, image_bounds);
+
+        if (!cropped_face_projection.is_empty()) {
+          vgl_point_2d<double> face_proj_center = cropped_face_projection.centroid();
+          vgl_ray_3d<double> ray = cam.ray(face_proj_center.x(), face_proj_center.y());
+          bool this_block_in_front = (zi == 0)? ray.direction().z() < 0.0 : ray.direction().z() > 0.0;
+          if(this_block_in_front) {
+            // this block is in front of block on other side of face
+            boxm2_block_id other_block_id = this_block_id;
+            if (zi == 0) {
+              --other_block_id.k_;
+            }
+            else {
+              ++other_block_id.k_;
+            }
+            // check for other block in list of nodes
+            vcl_list<boxm2_block_vis_graph_node_sptr>::iterator other =
+              this->find(other_block_id, temp_nodes) ;
+            if ( other != temp_nodes.end() )
+            {
+              // found the other block, so add the edge
+              (*node_iter)->out_edges.insert((*other));
+              ++(*other)->num_incoming_edges_;
+            }
+          }
+        }
+      }
+    }
+
+    // all edges added. now traverse the graph picking off the node with minimum number of incoming edges;
+    while(!temp_nodes.empty()) {
+      int min_incoming = 7; // maximum possible number of incoming edges + 1
+      boxm2_block_vis_graph_node_sptr next_node;
+
+      for(vcl_list<boxm2_block_vis_graph_node_sptr>::iterator node_iter = temp_nodes.begin();
+          node_iter != temp_nodes.end();node_iter++)
+      {
+        int num_incoming = (*node_iter)->num_incoming_edges_;
+        if (num_incoming < min_incoming) {
+          min_incoming = num_incoming;
+          next_node = *node_iter;
+        }
+        if (min_incoming == 0) {
+          // dont bother searching for other 0's
+          break;
+        }
+      }
+      
+      if (min_incoming > 0) {
+        vcl_cerr << "Warning: unable to find strict visibilty ordering of blocks." << vcl_endl;
+      }
+
+      assert(next_node);
+
+      // add the best node to the list
+      ordered_nodes_.push_back(next_node->id_);
+
+      // decrement incoming edge count of all blocks behind this one
+      for (vcl_set<boxm2_block_vis_graph_node_sptr>::iterator sink_node_it = 
+           next_node->out_edges.begin(); sink_node_it != next_node->out_edges.end(); ++sink_node_it) {
+        --(*sink_node_it)->num_incoming_edges_;
+      }
+      // remove the links to avoid cyclical smart pointers
+      next_node->out_edges.clear();
+
+      temp_nodes.remove( next_node );
     }
 }
+
 vcl_list<boxm2_block_vis_graph_node_sptr>::iterator 
     boxm2_block_vis_graph::find(const boxm2_block_id & id, vcl_list<boxm2_block_vis_graph_node_sptr> & list_nodes )
 {
--- contrib/brl/bseg/boxm2/boxm2_blocks_vis_graph.h
+++ contrib/brl/bseg/boxm2/boxm2_blocks_vis_graph.h
@@ -15,18 +15,17 @@ typedef vbl_smart_ptr<boxm2_block_vis_graph_node> boxm2_block_vis_graph_node_spt
 class boxm2_block_vis_graph_node : public vbl_ref_count
 {
  public:
-    boxm2_block_vis_graph_node():visited(false){}
-     boxm2_block_id id_;
-     vcl_set<boxm2_block_id> out_edges;
-     vcl_set<boxm2_block_id> in_edges;
-     bool visited;
+    boxm2_block_vis_graph_node(boxm2_block_id id): id_(id), num_incoming_edges_(0){}
+    boxm2_block_id id_;
+    vcl_set<boxm2_block_vis_graph_node_sptr> out_edges;
+    int num_incoming_edges_;
 };
 
 class boxm2_block_vis_graph
 {
  public:
   boxm2_block_vis_graph(){}
-  boxm2_block_vis_graph(vcl_map<boxm2_block_id,boxm2_block_metadata> & blkmetadata,  vpgl_generic_camera<double> & cam );
+  boxm2_block_vis_graph(vcl_map<boxm2_block_id,boxm2_block_metadata> & blkmetadata,  vpgl_generic_camera<double> const& cam );
   vcl_list<boxm2_block_vis_graph_node_sptr>::iterator find(const boxm2_block_id & id, vcl_list<boxm2_block_vis_graph_node_sptr> & list_nodes );
   vcl_vector<boxm2_block_id>  get_ordered_ids(){return ordered_nodes_;}
 
