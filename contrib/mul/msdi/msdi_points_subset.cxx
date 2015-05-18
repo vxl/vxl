cc8f6b9f0f207d48040315cc0f82752e3115c4ce
Tim Cootes <t.cootes@manchester.ac.uk>
2015-05-18 08:10:40 +0100

    Added new class to compute subsets of points.

---
+++ contrib/mul/msdi/msdi_points_subset.cxx
@@ -0,0 +1,99 @@
+
+//:
+// \file
+// \author Tim Cootes
+// \brief Adaptor which steps through images and returns a subset of original points
+
+#include <vcl_cassert.h>
+#include <vcl_string.h>
+
+#include "msdi_points_subset.h"
+#include <vimt/vimt_image_2d_of.h>
+#include <vil/vil_flip.h>
+#include <msm/msm_reflect_shape.h>
+
+//: Construct with external vectors of images and points
+msdi_points_subset::msdi_points_subset(
+                        msdi_marked_images& raw_data,
+                        const vcl_vector<unsigned>& index)
+  : marked_images_(raw_data), index_(index)
+{
+  points_.set_size(index.size());
+  reset();
+}
+
+//=======================================================================
+// Destructor
+//=======================================================================
+
+msdi_points_subset::~msdi_points_subset()
+{
+}
+
+unsigned msdi_points_subset::size() const
+{
+  return marked_images_.size();
+}
+
+//: Return current image
+const vimt_image_2d& msdi_points_subset::image()
+{
+  return marked_images_.image();
+}
+
+
+  //: Return current image pyramid
+const vimt_image_pyramid& msdi_points_subset::image_pyr()
+{
+  return marked_images_.image_pyr();
+}
+
+const msm_points& msdi_points_subset::points()
+{
+  if (!points_ok_)
+  {
+    // Get subset
+    for (size_t i=0;i<index_.size();++i) 
+      points_.set_point(i,marked_images_.points()[index_[i]]);
+    points_ok_=true;
+  }
+  return points_;
+}
+
+//=======================================================================
+//: Set to first example
+//=======================================================================
+void msdi_points_subset::reset()
+{
+  marked_images_.reset();
+  points_ok_=false;
+}
+
+//=======================================================================
+//: Move to next example
+//=======================================================================
+bool msdi_points_subset::next()
+{
+  if (!marked_images_.next())
+  {
+    return false;
+  }
+  points_ok_=false;
+
+  return true;
+}
+
+//: Return current image file name
+vcl_string msdi_points_subset::image_name() const
+{
+  return marked_images_.image_name();
+}
+
+//: Return current points file name
+vcl_string msdi_points_subset::points_name() const
+{
+  return marked_images_.points_name();
+}
+
+
+
