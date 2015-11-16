0a67966905330b2b6550b3b9e9607db56e116fc8
Hans Johnson <hans-johnson@uiowa.edu>
2015-11-15 20:17:07 -0600

    ENH: Remove ICC8 VCL_ICC_8

--- vcl/vcl_vector.h
+++ vcl/vcl_vector.h
@@ -19,18 +19,6 @@
 // Intel compiler's std::swap can't handle the special bit-iterator of
 // vector<bool>. But, on systems with newer GCCs, the Intel compiler
 // can use the GNU library, which doesn't have this problem.
-#if defined(VCL_ICC_8) && defined(__INTEL_CXXLIB_ICC)
-# include "vcl_utility.h"
-namespace std {
-  inline void swap(std::vector<bool, std::allocator<bool> >::iterator::reference a,
-                   std::vector<bool, std::allocator<bool> >::iterator::reference b)
-  {
-    bool tmp = a;
-    a = b;
-    b = tmp;
-  }
-} // end namespace std
-#endif
 
 #define VCL_VECTOR_INSTANTIATE extern "you must include vcl_vector.txx first"
 
