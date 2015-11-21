cbc9978bbcd84f890e84a9e4d518a4253b7e0256
Hans Johnson <hans-johnson@uiowa.edu>
2015-11-21 08:19:36 -0600

    COMP: Remove code that could never be accessed
    The Templates directory was not referenced and could
    the included files could never be used.  These files
    depended on the deprecated vepl1 library that has been
    removed.

--- contrib/tbl/vipl/Templates/vipl_accessors_vil1_image+vil1_rgb+uchar--.cxx
+++
@@ -1,6 +0,0 @@
-#ifndef INSTANTIATE_TEMPLATES
-#include <vipl/accessors/vipl_accessors_vil1_image.txx>
-#include <vil1/vil1_rgb.h>
-
-VIPL_INSTANTIATE_ACCESSORS(vil1_rgb<unsigned char>);
-#endif
--- contrib/tbl/vipl/Templates/vipl_section_container+float-.cxx
+++
@@ -1,4 +0,0 @@
-#ifndef INSTANTIATE_TEMPLATES
-#include <vipl/section/vipl_section_container.txx>
-template class vipl_section_container<float>;
-#endif
