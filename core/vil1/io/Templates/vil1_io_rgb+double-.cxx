11fe69ac2e2dd2b9090881a818127b142eceb9d6
Hans Johnson <hans-johnson@uiowa.edu>
2015-11-21 08:25:14 -0600

    COMP: Remove deprecated vil1/io library
    The vil1/io library was listed as deprecated.
    Removing to minimize maintenance burden in
    the future.

--- core/vil1/io/Templates/vil1_io_rgb+double-.cxx
+++
@@ -1,4 +0,0 @@
-// Instantiation of IO for vil1_rgb<double>
-#include <vil1/io/vil1_io_rgb.txx>
-VIL1_IO_RGB_INSTANTIATE(double);
-
--- core/vil1/io/tests/test_template_include.cxx
+++
@@ -1,5 +0,0 @@
-#include <vil1/io/vil1_io_memory_image_of.txx>
-#include <vil1/io/vil1_io_rgba.txx>
-#include <vil1/io/vil1_io_rgb.txx>
-
-int main() { return 0; }
