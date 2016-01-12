801a49ed42385ec48cef296604f7d53f98fd27f7
Gehua Yang <gehua@dualaligncom>
2016-01-12 10:12:35 -0500

    To use API of FFMpeg 2.8.4: 1) enum values now have AV_ prefix 2) Use vidl_ffmpeg_ostream_v56.txx and vidl_ffmpeg_istream_v56.txx  for the actual implementation.

--- core/vidl/vidl_ffmpeg_convert.cxx
+++ core/vidl/vidl_ffmpeg_convert.cxx
@@ -12,6 +12,7 @@
 #include "vidl_ffmpeg_convert.h"
 #include "vidl_frame.h"
 #include <vcl_cstring.h>
+#include <vcl_iostream.h>
 
 
 #ifdef LIBAVFORMAT_BUILD
@@ -27,9 +28,9 @@ extern "C" {
 }
 #endif
 
-
 //--------------------------------------------------------------------------------
 
+#if LIBAVCODEC_VERSION_MAJOR < 56
 //: Convert the pixel format of a frame using FFMPEG
 //
 // The \p in_frame->data() is converted from \p in_frame->pixel_format()
@@ -147,3 +148,125 @@ vidl_pixel_format_to_ffmpeg(vidl_pixel_format vidl_pix_fmt)
   }
   return PIX_FMT_NONE;
 }
+
+#else
+//: Convert the pixel format of a frame using FFMPEG
+//
+// The \p in_frame->data() is converted from \p in_frame->pixel_format()
+// to \p out_frame->pixel_format() and stored in \p out_frame->data()
+// If the output frame data is not the correct size new memory
+// will be allocated
+bool vidl_ffmpeg_convert(const vidl_frame_sptr& in_frame,
+                         vidl_frame_sptr& out_frame)
+{
+  if (!in_frame || !out_frame)
+    return false;
+
+  AVPixelFormat in_fmt =
+    vidl_pixel_format_to_ffmpeg(in_frame->pixel_format());
+
+  AVPixelFormat out_fmt =
+    vidl_pixel_format_to_ffmpeg(out_frame->pixel_format());
+
+  if (in_fmt == AV_PIX_FMT_NONE || out_fmt == AV_PIX_FMT_NONE)
+    return false;
+
+  unsigned ni = in_frame->ni();
+  unsigned nj = in_frame->nj();
+  unsigned out_size = (unsigned)avpicture_get_size(out_fmt, ni, nj);
+
+  if (out_frame->size() != out_size ||
+      out_frame->ni() != ni ||
+      out_frame->nj() != nj ||
+      !out_frame->data())
+    out_frame = new vidl_memory_chunk_frame(ni, nj, out_frame->pixel_format(),
+                                            new vil_memory_chunk(out_size, VIL_PIXEL_FORMAT_BYTE));
+
+  AVPicture in_pic;
+  vcl_memset(&in_pic, 0, sizeof(in_pic));
+  avpicture_fill(&in_pic, (uint8_t*)in_frame->data(), in_fmt, ni, nj);
+
+  AVPicture out_pic;
+  vcl_memset(&out_pic, 0, sizeof(out_pic));
+  avpicture_fill(&out_pic, (uint8_t*)out_frame->data(), out_fmt, ni, nj);
+
+#if LIBAVCODEC_BUILD < ((52<<16)+(10<<8)+0)  // before ver 52.10.0
+  if (img_convert(&out_pic, out_fmt, &in_pic, in_fmt, ni, nj) < 0)
+    return false;
+#else
+  SwsContext* ctx = sws_getContext(ni, nj, in_fmt,
+                                   ni, nj, out_fmt,
+                                   SWS_BILINEAR,
+                                   NULL, NULL, NULL);
+  sws_scale(ctx,
+            in_pic.data, in_pic.linesize,
+            0, nj,
+            out_pic.data, out_pic.linesize);
+  sws_freeContext(ctx);
+#endif
+
+  return true;
+}
+
+
+//: Find the vidl pixel format that matches a FFMPEG one
+vidl_pixel_format
+vidl_pixel_format_from_ffmpeg(AVPixelFormat ffmpeg_pix_fmt)
+{
+  switch (ffmpeg_pix_fmt)
+  {
+  case AV_PIX_FMT_YUV420P:   return VIDL_PIXEL_FORMAT_YUV_420P;
+  case AV_PIX_FMT_YUYV422:   return VIDL_PIXEL_FORMAT_YUYV_422;
+  case AV_PIX_FMT_RGB24:     return VIDL_PIXEL_FORMAT_RGB_24;
+  case AV_PIX_FMT_BGR24:     return VIDL_PIXEL_FORMAT_BGR_24;
+  case AV_PIX_FMT_YUV422P:   return VIDL_PIXEL_FORMAT_YUV_422P;
+  case AV_PIX_FMT_YUV444P:   return VIDL_PIXEL_FORMAT_YUV_444P;
+#ifdef AV_PIX_FMT_RGBA
+  case AV_PIX_FMT_RGBA:      return VIDL_PIXEL_FORMAT_RGBA_32;
+#endif
+  case AV_PIX_FMT_YUV410P:   return VIDL_PIXEL_FORMAT_YUV_410P;
+  case AV_PIX_FMT_YUV411P:   return VIDL_PIXEL_FORMAT_YUV_411P;
+  case AV_PIX_FMT_RGB565:    return VIDL_PIXEL_FORMAT_RGB_565;
+  case AV_PIX_FMT_RGB555:    return VIDL_PIXEL_FORMAT_RGB_555;
+  case AV_PIX_FMT_GRAY8:     return VIDL_PIXEL_FORMAT_MONO_8;
+  case AV_PIX_FMT_PAL8:      return VIDL_PIXEL_FORMAT_MONO_8;   //HACK: Treating 8-bit palette as greyscale image
+  case AV_PIX_FMT_MONOWHITE: return VIDL_PIXEL_FORMAT_MONO_1;
+  case AV_PIX_FMT_MONOBLACK: return VIDL_PIXEL_FORMAT_MONO_1;
+  case AV_PIX_FMT_UYVY422:   return VIDL_PIXEL_FORMAT_UYVY_422;
+  case AV_PIX_FMT_UYYVYY411: return VIDL_PIXEL_FORMAT_UYVY_411;
+  default: break;
+  }
+  return VIDL_PIXEL_FORMAT_UNKNOWN;
+}
+
+
+//: Find the FFMPEG pixel format that matches a vidl one
+AVPixelFormat
+vidl_pixel_format_to_ffmpeg(vidl_pixel_format vidl_pix_fmt)
+{
+  switch (vidl_pix_fmt)
+  {
+  case VIDL_PIXEL_FORMAT_RGB_24:   return AV_PIX_FMT_RGB24;
+  case VIDL_PIXEL_FORMAT_BGR_24:   return AV_PIX_FMT_BGR24;
+#ifdef AV_PIX_FMT_RGBA
+  case VIDL_PIXEL_FORMAT_RGBA_32:  return AV_PIX_FMT_RGBA;
+#endif
+  case VIDL_PIXEL_FORMAT_RGB_565:  return AV_PIX_FMT_RGB565;
+  case VIDL_PIXEL_FORMAT_RGB_555:  return AV_PIX_FMT_RGB555;
+  case VIDL_PIXEL_FORMAT_YUV_444P: return AV_PIX_FMT_YUV444P;
+  case VIDL_PIXEL_FORMAT_YUYV_422: return AV_PIX_FMT_YUYV422;
+  case VIDL_PIXEL_FORMAT_YUV_422P: return AV_PIX_FMT_YUV422P;
+  case VIDL_PIXEL_FORMAT_YUV_420P: return AV_PIX_FMT_YUV420P;
+  case VIDL_PIXEL_FORMAT_YUV_411P: return AV_PIX_FMT_YUV411P;
+  case VIDL_PIXEL_FORMAT_YUV_410P: return AV_PIX_FMT_YUV410P;
+  case VIDL_PIXEL_FORMAT_UYVY_422: return AV_PIX_FMT_UYVY422;
+  case VIDL_PIXEL_FORMAT_UYVY_411: return AV_PIX_FMT_UYYVYY411;
+  case VIDL_PIXEL_FORMAT_MONO_1:   return AV_PIX_FMT_MONOBLACK;
+  case VIDL_PIXEL_FORMAT_MONO_8:   return AV_PIX_FMT_GRAY8;
+  default: break;
+  }
+  vcl_cerr << "Warning: unknown FFMPeg pixel format type : " << vidl_pix_fmt << '\n';
+  return AV_PIX_FMT_NONE;
+}
+
+#endif
--- core/vidl/vidl_ffmpeg_convert.h
+++ core/vidl/vidl_ffmpeg_convert.h
@@ -27,6 +27,7 @@ extern "C" {
 #endif
 }
 
+#if LIBAVCODEC_VERSION_MAJOR < 56
 //: Convert the pixel format of a frame using FFMPEG
 //
 // The \p in_frame->data() is converted from \p in_frame->pixel_format()
@@ -45,6 +46,27 @@ vidl_pixel_format_from_ffmpeg(PixelFormat ffmpeg_pix_fmt);
 //: Find the FFMPEG pixel format that matches a vidl one
 PixelFormat
 vidl_pixel_format_to_ffmpeg(vidl_pixel_format vidl_pix_fmt);
+
+#else
+//: Convert the pixel format of a frame using FFMPEG
+//
+// The \p in_frame->data() is converted from \p in_frame->pixel_format()
+// to \p out_frame->pixel_format() and stored in \p out_frame->data()
+// If the output frame data is not the correct size new memory
+// will be allocated
+bool vidl_ffmpeg_convert(const vidl_frame_sptr& in_frame,
+                               vidl_frame_sptr& out_frame);
+
+
+//: Find the vidl pixel format that matches a FFMPEG one
+vidl_pixel_format
+vidl_pixel_format_from_ffmpeg(AVPixelFormat ffmpeg_pix_fmt);
+
+
+//: Find the FFMPEG pixel format that matches a vidl one
+AVPixelFormat
+vidl_pixel_format_to_ffmpeg(vidl_pixel_format vidl_pix_fmt);
+#endif
 
 #endif // vidl_ffmpeg_convert_h_
 
