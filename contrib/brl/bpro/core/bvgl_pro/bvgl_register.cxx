57db3f73cd866796a1118080b4ad0801277ace83
Yi Dong <yidong80@gmail.com>
2016-01-22 10:46:37 -0500

    add a process to retrieve leaf ids that intersect with kml polygon

--- contrib/brl/bpro/core/bvgl_pro/bvgl_register.cxx
+++ contrib/brl/bpro/core/bvgl_pro/bvgl_register.cxx
@@ -16,4 +16,5 @@ void bvgl_register::register_process()
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, bvgl_set_change_type_process, "bvglSetChangeTypeProcess");
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, bvgl_2d_box_intersection_process, "bvgl2DBoxIntersectionProcess");
   REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, bvgl_geo_index_region_resource_process, "bvglGeoIndexRegionResourceProcess");
+  REG_PROCESS_FUNC_CONS(bprb_func_process, bprb_batch_process_manager, bvgl_geo_index_region_poly_resource_process, "bvglGeoIndexRegionPolyResourceProcess");
 }
