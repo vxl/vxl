cb877721b3bad6eb57c6be480e6e7d39f942e9bc
Hans Johnson <hans-johnson@uiowa.edu>
2016-02-03 14:58:40 -0600

    BUG: Value required for variable used in header define
    check_cxx_expression_compiles macro was returning "" instead of 0
    for failure case.
    
    This was an artifact of CHECK_CXX_SOURCE_COMPILES return 1 on success,
    "" on failure.
    
    The resultant macro definitions in vxl_config.h becomes un-valued, as
    shown below.  This leads to compile error c1017
    (https://msdn.microsoft.com/en-us/library/h5sh3k99.aspx) in VS11.
    
    The solution was to ensure that the variable had a value of 0
    on failure.

--- config/cmake/config/CheckCXXExpressionCompiles.cmake
+++ config/cmake/config/CheckCXXExpressionCompiles.cmake
@@ -12,6 +12,8 @@
 #  CMAKE_REQUIRED_DEFINITIONS = list of macros to define (-DFOO=bar)
 #  CMAKE_REQUIRED_INCLUDES = list of include directories
 #  CMAKE_REQUIRED_LIBRARIES = list of libraries to link
+#
+# ${variable} is set to 1 on success 0 on failure
 
 #=============================================================================
 # Copyright 2003-2011 Kitware, Inc.
@@ -28,7 +30,7 @@
 
 include(CheckCXXSourceCompiles)
 
-macro(CHECK_CXX_EXPRESSION_COMPILES EXPRESSION FILES VARIABLE)
+macro(check_cxx_expression_compiles EXPRESSION FILES VARIABLE)
   set(SOURCE "/* CHECK_CXX_EXPRESSION_COMPILES */\n")
   foreach(FILE ${FILES})
     set(SOURCE "${SOURCE}#include <${FILE}>\n")
@@ -36,5 +38,8 @@ macro(CHECK_CXX_EXPRESSION_COMPILES EXPRESSION FILES VARIABLE)
   set(SOURCE "${SOURCE}\nint main()\n{\n")
   set(SOURCE "${SOURCE}  static_cast<void>(${EXPRESSION});\n\n")
   set(SOURCE "${SOURCE}  return 0;\n}\n")
-  CHECK_CXX_SOURCE_COMPILES("${SOURCE}" "${VARIABLE}")
+  CHECK_CXX_SOURCE_COMPILES("${SOURCE}" "${VARIABLE}") # Returns 1 - Success "" Failure
+  if( NOT ${VARIABLE} ) # Variable is set to zero
+    set(${VARIABLE} 0 CACHE INTERNAL "Test ${VARIABLE}") #Assume failure, only set to non-zero if CHECK_CXX_SOURCE_COMPILES
+  endif()
 endmacro()
