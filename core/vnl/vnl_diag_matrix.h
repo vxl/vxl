vxl/vxl

commit 0dd5124a823be67f7779613fbc5d06a1af5d4508

ENH: Unifying out-of-bounds behavior for vnl containers.
Currently, the behavior when accessing elements in vnl containers
using the get() and set() functions is inconsistent.  This patch
attempts to unify the behavior somewhat, such that exceptions
are raised when VNL_CONFIG_CHECK_BOUNDS is enabled.

core/vnl/vnl_diag_matrix.h
@@ -89,18 +89,6 @@ class vnl_diag_matrix
   inline T& operator[] (unsigned i) { return diagonal_[i]; }
   inline T const& operator[] (unsigned i) const { return diagonal_[i]; }
 
-  //: set element with boundary checks.
-  inline void put (unsigned r, unsigned c, T const& v) {
-    assert(r == c); (void)c;
-    assert (r<size()); diagonal_[r] = v;
-  }
-
-  //: get element with boundary checks.
-  inline T get (unsigned r, unsigned c) const {
-    assert(r == c); (void)c;
-    assert (r<size()); return diagonal_[r];
-  }
-
   //: Return a vector (copy) with the content of the (main) diagonal
   inline vnl_vector<T> get_diagonal() const { return diagonal_; }
 
@@ -122,10 +110,43 @@ class vnl_diag_matrix
   inline const_iterator begin() const { return diagonal_.begin(); }
   inline const_iterator end() const { return diagonal_.end(); }
 
-  inline unsigned size() const { return diagonal_.size(); }
-  inline unsigned rows() const { return diagonal_.size(); }
-  inline unsigned cols() const { return diagonal_.size(); }
-  inline unsigned columns() const { return diagonal_.size(); }
+  //: Return the total number of elements stored by the matrix.
+  // Since vnl_diag_matrix only stores values on the diagonal
+  // and must be square, size() == rows() == cols().
+  inline unsigned int size() const { return diagonal_.size(); }
+
+  //: Return the number of rows.
+  inline unsigned int rows() const { return diagonal_.size(); }
+
+  //: Return the number of columns.
+  // A synonym for columns().
+  inline unsigned int cols() const { return diagonal_.size(); }
+
+  //: Return the number of columns.
+  // A synonym for cols().
+  inline unsigned int columns() const { return diagonal_.size(); }
+
+  //: set element with boundary checks.
+  inline void put (unsigned r, unsigned c, T const& v) {
+    assert(r == c);
+    (void)c;
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+    if (r >= this->size())                  // If invalid size specified
+      vnl_error_matrix_row_index("get", r); // Raise exception
+#endif
+    diagonal_[r] = v;
+  }
+
+  //: get element with boundary checks.
+  inline T get (unsigned r, unsigned c) const {
+    assert(r == c);
+    (void)c;
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+  if (r >= this->size())                  // If invalid size specified
+    vnl_error_matrix_row_index("get", r); // Raise exception
+#endif
+    return diagonal_[r];
+  }
 
   // Need this until we add a vnl_diag_matrix ctor to vnl_matrix;
   inline vnl_matrix<T> asMatrix() const;

core/vnl/vnl_diag_matrix_fixed.h
@@ -88,18 +88,6 @@ class vnl_diag_matrix_fixed
   inline T& operator[] (unsigned i) { return diagonal_[i]; }
   inline T const& operator[] (unsigned i) const { return diagonal_[i]; }
 
-  //: set element with boundary checks.
-  inline void put (unsigned r, unsigned c, T const& v) {
-    assert(r == c); (void)c;
-    assert (r<size()); diagonal_[r] = v;
-  }
-
-  //: get element with boundary checks.
-  inline T get (unsigned r, unsigned c) const {
-    assert(r == c); (void)c;
-    assert (r<size()); return diagonal_[r];
-  }
-
   //: Return a vector (copy) with the content of the (main) diagonal
   inline vnl_vector_fixed<T,N> get_diagonal() const { return diagonal_; }
 
@@ -121,10 +109,45 @@ class vnl_diag_matrix_fixed
   inline const_iterator begin() const { return diagonal_.begin(); }
   inline const_iterator end() const { return diagonal_.end(); }
 
-  inline unsigned size() const { return diagonal_.size(); }
-  inline unsigned rows() const { return diagonal_.size(); }
-  inline unsigned cols() const { return diagonal_.size(); }
-  inline unsigned columns() const { return diagonal_.size(); }
+  //: Return the total number of elements stored by the matrix.
+  // Since vnl_diag_matrix_fixed only stores values on the diagonal
+  // and must be square, size() == rows() == cols().
+  inline unsigned int size() const { return diagonal_.size(); }
+
+  //: Return the number of rows.
+  inline unsigned int rows() const { return diagonal_.size(); }
+
+  //: Return the number of columns.
+  // A synonym for columns().
+  inline unsigned int cols() const { return diagonal_.size(); }
+
+  //: Return the number of columns.
+  // A synonym for cols().
+  inline unsigned int columns() const { return diagonal_.size(); }
+
+  //: set element with boundary checks.
+  inline void put (unsigned r, unsigned c, T const& v)
+  {
+    assert(r == c);
+    (void)c;
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+    if (r >= this->size())                  // If invalid size specified
+      vnl_error_matrix_row_index("put", r); // Raise exception
+#endif
+    diagonal_[r] = v;
+  }
+
+  //: get element with boundary checks.
+  inline T get (unsigned r, unsigned c) const
+  {
+    assert(r == c);
+    (void)c;
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+    if (r >= this->size())                  // If invalid size specified
+      vnl_error_matrix_row_index("get", r); // Raise exception
+#endif
+    return diagonal_[r];
+  }
 
   // Need this until we add a vnl_diag_matrix_fixed ctor to vnl_matrix;
   inline vnl_matrix_fixed<T,N,N> as_matrix_fixed() const;

core/vnl/vnl_matrix.h
@@ -169,26 +169,26 @@ class vnl_matrix
 
 // Basic 2D-Array functionality-------------------------------------------
 
-  //: Return number of rows
-  unsigned rows()    const { return num_rows; }
-
-  //: Return number of columns
-  // A synonym for cols()
-  unsigned columns()  const { return num_cols; }
-
-  //: Return number of columns
-  // A synonym for columns()
-  unsigned cols()    const { return num_cols; }
-
-  //: Return number of elements
+  //: Return the total number of elements stored by the matrix.
   // This equals rows() * cols()
-  unsigned size()    const { return rows()*cols(); }
+  inline unsigned int size() const { return this->num_rows*this->num_cols; }
+
+  //: Return the number of rows.
+  inline unsigned int rows() const { return this->num_rows; }
+
+  //: Return the number of columns.
+  // A synonym for columns().
+  inline unsigned int cols() const { return this->num_cols; }
+
+  //: Return the number of columns.
+  // A synonym for cols().
+  inline unsigned int columns() const { return this->num_cols; }
 
   //: set element with boundary checks if error checking is on.
-  void put(unsigned r, unsigned c, T const&);
+  inline void put(unsigned r, unsigned c, T const&);
 
   //: get element with boundary checks if error checking is on.
-  T    get(unsigned r, unsigned c) const;
+  inline T get(unsigned r, unsigned c) const;
 
   //: return pointer to given row
   // No boundary checking here.
@@ -712,30 +712,32 @@ class vnl_matrix
 // Checks for valid range of indices.
 
 template<class T>
-inline T vnl_matrix<T>::get(unsigned row, unsigned column) const
+inline T vnl_matrix<T>
+::get(unsigned r, unsigned c) const
 {
-#ifdef ERROR_CHECKING
-  if (row >= this->num_rows)                   // If invalid size specified
-    vnl_error_matrix_row_index("get", row);    // Raise exception
-  if (column >= this->num_cols)                // If invalid size specified
-    vnl_error_matrix_col_index("get", column); // Raise exception
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+  if (r >= this->num_rows)                // If invalid size specified
+    vnl_error_matrix_row_index("get", r); // Raise exception
+  if (c >= this->num_cols)                // If invalid size specified
+    vnl_error_matrix_col_index("get", c); // Raise exception
 #endif
-  return this->data[row][column];
+  return this->data[r][c];
 }
 
 //: Puts value into element at specified row and column. O(1).
 // Checks for valid range of indices.
 
 template<class T>
-inline void vnl_matrix<T>::put(unsigned row, unsigned column, T const& value)
+inline void vnl_matrix<T>
+::put(unsigned r, unsigned c, T const& v)
 {
-#ifdef ERROR_CHECKING
-  if (row >= this->num_rows)                   // If invalid size specified
-    vnl_error_matrix_row_index("put", row);    // Raise exception
-  if (column >= this->num_cols)                // If invalid size specified
-    vnl_error_matrix_col_index("put", column); // Raise exception
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+  if (r >= this->num_rows)                // If invalid size specified
+    vnl_error_matrix_row_index("put", r); // Raise exception
+  if (c >= this->num_cols)                // If invalid size specified
+    vnl_error_matrix_col_index("put", c); // Raise exception
 #endif
-  this->data[row][column] = value;             // Assign data value
+  this->data[r][c] = v;             // Assign data value
 }
 
 

core/vnl/vnl_matrix_fixed.h
@@ -179,29 +179,47 @@ class vnl_matrix_fixed
 
 // Basic 2D-Array functionality-------------------------------------------
 
-  //: Return number of rows
-  unsigned rows()    const { return num_rows; }
-
-  //: Return number of columns
-  // A synonym for cols()
-  unsigned columns()  const { return num_cols; }
-
-  //: Return number of columns
-  // A synonym for columns()
-  unsigned cols()    const { return num_cols; }
-
-  //: Return number of elements
+  //: Return the total number of elements stored by the matrix.
   // This equals rows() * cols()
-  unsigned size()    const { return num_rows*num_cols; }
+  inline unsigned int size() const { return num_rows*num_cols; }
+
+  //: Return the number of rows.
+  inline unsigned int rows() const { return num_rows; }
+
+  //: Return the number of columns.
+  // A synonym for columns().
+  inline unsigned int cols() const { return num_cols; }
+
+  //: Return the number of columns.
+  // A synonym for cols().
+  inline unsigned int columns() const { return num_cols; }
 
   //: set element
-  void put (unsigned r, unsigned c, T const& v) { (*this)(r,c) = v; }
+  inline void put (unsigned r, unsigned c, T const& v)
+  {
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+    if (r >= num_rows)                // If invalid size specified
+      vnl_error_matrix_row_index("put", r); // Raise exception
+    if (c >= num_cols)                // If invalid size specified
+      vnl_error_matrix_col_index("put", c); // Raise exception
+#endif
+    this->data_[r][c] = v;
+  }
+
+  //: get element
+  inline T get (unsigned r, unsigned c) const
+  {
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+    if (r >= num_rows)                // If invalid size specified
+      vnl_error_matrix_row_index("get", r); // Raise exception
+    if (c >= num_cols)                // If invalid size specified
+      vnl_error_matrix_col_index("get", c); // Raise exception
+#endif
+    return this->data_[r][c];
+  }
 
   //: set element, and return *this
   vnl_matrix_fixed& set (unsigned r, unsigned c, T const& v) { (*this)(r,c) = v; return *this; }
-
-  //: get element
-  T    get (unsigned r, unsigned c) const { return (*this)(r,c); }
 
   //: return pointer to given row
   // No boundary checking here.

core/vnl/vnl_sym_matrix.h
@@ -101,10 +101,46 @@ class vnl_sym_matrix
   inline const_iterator begin() const { return data_; }
   inline const_iterator end() const { return data_ + size(); }
 
-  unsigned long size() const { return nn_ * (nn_ + 1) / 2; }
-  unsigned rows() const { return nn_; }
-  unsigned cols() const { return nn_; }
-  unsigned columns() const { return nn_; }
+  //: Return the total number of elements stored by the matrix.
+  // Since vnl_sym_matrix only stores the diagonal and lower
+  // triangular elements and must be square, this returns
+  // n*(n+1)/2, where n == rows() == cols().
+  inline unsigned int size() const { return nn_ * (nn_ + 1) / 2; }
+
+  //: Return the number of rows.
+  inline unsigned int rows() const { return nn_; }
+
+  //: Return the number of columns.
+  // A synonym for columns().
+  inline unsigned int cols() const { return nn_; }
+
+  //: Return the number of columns.
+  // A synonym for cols().
+  inline unsigned int columns() const { return nn_; }
+
+  //: set element
+  inline void put (unsigned r, unsigned c, T const& v)
+  {
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+    if (r >= this->nn_)                // If invalid size specified
+      vnl_error_matrix_row_index("put", r); // Raise exception
+    if (c >= this->nn_)                // If invalid size specified
+      vnl_error_matrix_col_index("put", c); // Raise exception
+#endif
+    (r > c) ? index_[r][c] = v : index_[c][r] = v;
+  }
+
+  //: get element
+  inline T get (unsigned r, unsigned c) const
+  {
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+    if (r >= this->nn_)                // If invalid size specified
+      vnl_error_matrix_row_index("get", r); // Raise exception
+    if (c >= this->nn_)                // If invalid size specified
+      vnl_error_matrix_col_index("get", c); // Raise exception
+#endif
+    return (r > c) ? index_[r][c] : index_[c][r];
+  }
 
   // Need this until we add a vnl_sym_matrix ctor to vnl_matrix;
   inline vnl_matrix<T> as_matrix() const;

core/vnl/vnl_vector.h
@@ -80,15 +80,22 @@ class vnl_vector
   //: Creates an empty vector. O(1).
   vnl_vector() : num_elmts(0) , data(VXL_NULLPTR) {}
 
-  //: Creates vector containing n elements.
-  // Elements are not initialized.
-  explicit vnl_vector(unsigned len);
+  //: Creates a vector containing n uninitialized elements.
+  explicit vnl_vector(unsigned int len);
 
-  //: Creates vector of len elements, all set to v0
-  vnl_vector(unsigned len, T const& v0);
+  //: Creates a vector containing n elements, all set to v0.
+  vnl_vector(unsigned int len, T const& v0);
 
-  //: Creates a vector of specified length and initialize first n elements with values. O(n).
-  vnl_vector(unsigned len, int n, T const values[]);
+  //: Creates a vector containing len elements, with the first n
+  // elements taken from the array values[]. O(n).
+  vnl_vector(unsigned int len, int n, T const values[]);
+
+  //: Creates a vector containing len elements, initialized with values from
+  // a data block.
+  vnl_vector(T const* data_block,unsigned int n);
+
+  //: Copy constructor.
+  vnl_vector(vnl_vector<T> const&);
 
 #if VNL_CONFIG_LEGACY_METHODS // these constructors are deprecated and should not be used
   //: Creates a vector of length 2 and initializes with the arguments, px,py.
@@ -109,12 +116,6 @@ class vnl_vector
   // \deprecated
   vnl_vector(unsigned len, T const& px, T const& py, T const& pz, T const& pw);
 #endif
-
-  //: Create n element vector and copy data from data_block
-  vnl_vector(T const* data_block,unsigned int n);
-
-  //: Copy constructor
-  vnl_vector(vnl_vector<T> const&);
 
 #ifndef VXL_DOXYGEN_SHOULD_SKIP_THIS
 // <internal>
@@ -143,10 +144,10 @@ class vnl_vector
   ~vnl_vector();
 
   //: Return the length, number of elements, dimension of this vector.
-  unsigned size() const { return num_elmts; }
+  unsigned int size() const { return this->num_elmts; }
 
   //: Put value at given position in vector.
-  inline void put(unsigned int i, T const&);
+  inline void put(unsigned int i, T const& v);
 
   //: Get value at element i
   inline T get(unsigned int i) const;
@@ -488,26 +489,28 @@ class vnl_vector
 // Range check is performed.
 
 template <class T>
-inline T vnl_vector<T>::get(unsigned int index) const
+inline T vnl_vector<T>
+::get(unsigned int i) const
 {
-#ifdef ERROR_CHECKING
-  if (index >= this->num_elmts)     // If invalid index specified
-    vnl_error_vector_index("get", index);  // Raise exception
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+  if (i >= this->size())     // If invalid index specified
+    vnl_error_vector_index("get", i);  // Raise exception
 #endif
-  return this->data[index];
+  return this->data[i];
 }
 
 //: Puts the value at specified index. O(1).
 // Range check is performed.
 
 template <class T>
-inline void vnl_vector<T>::put(unsigned int index, T const& value)
+inline void vnl_vector<T>
+::put(unsigned int i, T const& v)
 {
-#ifdef ERROR_CHECKING
-  if (index >= this->num_elmts)     // If invalid index specified
-    vnl_error_vector_index("put", index); // Raise exception
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+  if (i >= this->size())     // If invalid index specified
+    vnl_error_vector_index("put", i); // Raise exception
 #endif
-  this->data[index] = value;    // Assign data value
+  this->data[i] = v;    // Assign data value
 }
 
 //: multiply matrix and (column) vector. O(m*n).

core/vnl/vnl_vector_fixed.h
@@ -186,19 +186,33 @@ class vnl_vector_fixed
 
   //: Length of the vector.
   // This is always \a n.
-  unsigned size() const { return n; }
+  unsigned int size() const { return n; }
 
   //: Put value at given position in vector.
-  void put (unsigned int i, T const& v) { data_[i] = v; }
+  inline void put (unsigned int i, T const& v)
+  {
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+    if (i >= this->size())           // If invalid index specified
+      vnl_error_vector_index("put", i); // Raise exception
+#endif
+    this->data_[i] = v;
+  }
 
   //: Get value at element i
-  T get (unsigned int i) const { return data_[i]; }
+  inline T get (unsigned int i) const
+  {
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+    if (i >= this->size())            // If invalid index specified
+      vnl_error_vector_index("get", i);  // Raise exception
+#endif
+    return this->data_[i];
+  }
 
   //: Set all values to v
   vnl_vector_fixed& fill( T const& v )
   {
     for ( size_type i = 0; i < n; ++i )
-      data_[i] = v;
+      this->data_[i] = v;
     return *this;
   }
 

core/vnl/tests/test_diag_matrix.cxx
@@ -9,6 +9,7 @@
 #include <vnl/vnl_diag_matrix.h>
 #include <vnl/vnl_matrix_fixed.h>
 #include <vnl/vnl_vector_fixed.h>
+#include <vcl_exception.h>
 
 void test_diag_matrix()
 {
@@ -99,6 +100,29 @@ void test_diag_matrix()
   TEST("matrix product", p2(0,0) ==  0.0  && p2(0,1) == 0.0 && p2(0,2) == -0.75
                       && p2(1,0) == -1.5  && p2(1,1) == 0.0 && p2(1,2) == -0.5
                       && p2(2,0) == -0.75 && p2(2,1) == 0.0 && p2(2,2) ==  0.0, true);
+
+  ///////////////
+  // ACCESSORS //
+  ///////////////
+
+#ifdef VNL_CONFIG_CHECK_BOUNDS
+
+  {
+  // Get
+  bool exceptionThrownAndCaught = false;
+  vcl_try { m1.get(25,25); }  // Raise out of bounds exception.
+  vcl_catch_all { exceptionThrownAndCaught = true; }
+  TEST("Out of bounds get(25,25)", exceptionThrownAndCaught, true);
+
+  // Put
+  exceptionThrownAndCaught = false;
+  vcl_try { m1.put(25,25,0); }  // Raise out of bounds exception.
+  vcl_catch_all { exceptionThrownAndCaught = true; }
+  TEST("Out of bounds put(25,25,0)", exceptionThrownAndCaught, true);
+  }
+
+#endif
+
 }
 
 TESTMAIN(test_diag_matrix);

commit bfbd17712971bcc91406c10fc030d10cf3d9cdf3

BUG: Fixed VNL_CONFIG_CHECK_BOUNDS bug
A bunch of #ifdefs were added when it should have been #ifs. This caused
building in Release to fail, because code that should not have been active, was

Bugs were introduced in 0dd5124a823be67f7779613fbc5d06a1af5d4508 and PR #207

core/vnl/vnl_diag_matrix.h
@@ -130,7 +130,7 @@ class vnl_diag_matrix
   inline void put (unsigned r, unsigned c, T const& v) {
     assert(r == c);
     (void)c;
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
     if (r >= this->size())                  // If invalid size specified
       vnl_error_matrix_row_index("get", r); // Raise exception
 #endif
@@ -141,7 +141,7 @@ class vnl_diag_matrix
   inline T get (unsigned r, unsigned c) const {
     assert(r == c);
     (void)c;
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
   if (r >= this->size())                  // If invalid size specified
     vnl_error_matrix_row_index("get", r); // Raise exception
 #endif

core/vnl/vnl_diag_matrix_fixed.h
@@ -130,7 +130,7 @@ class vnl_diag_matrix_fixed
   {
     assert(r == c);
     (void)c;
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
     if (r >= this->size())                  // If invalid size specified
       vnl_error_matrix_row_index("put", r); // Raise exception
 #endif
@@ -142,7 +142,7 @@ class vnl_diag_matrix_fixed
   {
     assert(r == c);
     (void)c;
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
     if (r >= this->size())                  // If invalid size specified
       vnl_error_matrix_row_index("get", r); // Raise exception
 #endif

core/vnl/vnl_matrix.h
@@ -715,7 +715,7 @@ template<class T>
 inline T vnl_matrix<T>
 ::get(unsigned r, unsigned c) const
 {
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
   if (r >= this->num_rows)                // If invalid size specified
     vnl_error_matrix_row_index("get", r); // Raise exception
   if (c >= this->num_cols)                // If invalid size specified
@@ -731,7 +731,7 @@ template<class T>
 inline void vnl_matrix<T>
 ::put(unsigned r, unsigned c, T const& v)
 {
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
   if (r >= this->num_rows)                // If invalid size specified
     vnl_error_matrix_row_index("put", r); // Raise exception
   if (c >= this->num_cols)                // If invalid size specified

core/vnl/vnl_matrix_fixed.h
@@ -197,7 +197,7 @@ class vnl_matrix_fixed
   //: set element
   inline void put (unsigned r, unsigned c, T const& v)
   {
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
     if (r >= num_rows)                // If invalid size specified
       vnl_error_matrix_row_index("put", r); // Raise exception
     if (c >= num_cols)                // If invalid size specified
@@ -209,7 +209,7 @@ class vnl_matrix_fixed
   //: get element
   inline T get (unsigned r, unsigned c) const
   {
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
     if (r >= num_rows)                // If invalid size specified
       vnl_error_matrix_row_index("get", r); // Raise exception
     if (c >= num_cols)                // If invalid size specified

core/vnl/vnl_sym_matrix.h
@@ -121,7 +121,7 @@ class vnl_sym_matrix
   //: set element
   inline void put (unsigned r, unsigned c, T const& v)
   {
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
     if (r >= this->nn_)                // If invalid size specified
       vnl_error_matrix_row_index("put", r); // Raise exception
     if (c >= this->nn_)                // If invalid size specified
@@ -133,7 +133,7 @@ class vnl_sym_matrix
   //: get element
   inline T get (unsigned r, unsigned c) const
   {
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
     if (r >= this->nn_)                // If invalid size specified
       vnl_error_matrix_row_index("get", r); // Raise exception
     if (c >= this->nn_)                // If invalid size specified

core/vnl/vnl_vector.h
@@ -16,6 +16,7 @@
 //   Mar.2009 - Peter Vanroose - added arg_min() and arg_max()
 //   Oct.2010 - Peter Vanroose - mutators and setters now return *this
 // \endverbatim
+# include <vnl/vnl_error.h>
 
 #include <vcl_iosfwd.h>
 #include <vnl/vnl_tag.h>
@@ -492,7 +493,7 @@ template <class T>
 inline T vnl_vector<T>
 ::get(unsigned int i) const
 {
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
   if (i >= this->size())     // If invalid index specified
     vnl_error_vector_index("get", i);  // Raise exception
 #endif
@@ -506,7 +507,7 @@ template <class T>
 inline void vnl_vector<T>
 ::put(unsigned int i, T const& v)
 {
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
   if (i >= this->size())     // If invalid index specified
     vnl_error_vector_index("put", i); // Raise exception
 #endif

core/vnl/vnl_vector_fixed.h
@@ -191,7 +191,7 @@ class vnl_vector_fixed
   //: Put value at given position in vector.
   inline void put (unsigned int i, T const& v)
   {
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
     if (i >= this->size())           // If invalid index specified
       vnl_error_vector_index("put", i); // Raise exception
 #endif
@@ -201,7 +201,7 @@ class vnl_vector_fixed
   //: Get value at element i
   inline T get (unsigned int i) const
   {
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
     if (i >= this->size())            // If invalid index specified
       vnl_error_vector_index("get", i);  // Raise exception
 #endif

core/vnl/tests/test_diag_matrix.cxx
@@ -105,7 +105,7 @@ void test_diag_matrix()
   // ACCESSORS //
   ///////////////
 
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
 
   {
   // Get

core/vnl/tests/test_diag_matrix_fixed.cxx
@@ -105,7 +105,7 @@ void test_diag_matrix_fixed()
   // ACCESSORS //
   ///////////////
 
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
 
   {
   // Get

core/vnl/tests/test_matrix.cxx
@@ -53,7 +53,7 @@ void test_int()
   // ACCESSORS //
   ///////////////
 
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
 
   {
   // Get

core/vnl/tests/test_matrix_fixed.cxx
@@ -115,7 +115,7 @@ void test_int()
   // ACCESSORS //
   ///////////////
 
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
 
   {
   // Get

core/vnl/tests/test_sym_matrix.cxx
@@ -74,7 +74,7 @@ void test_int()
   // ACCESSORS //
   ///////////////
 
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
 
   {
   // Get

core/vnl/tests/test_vector.cxx
@@ -51,7 +51,7 @@ void vnl_vector_test_int()
   // ACCESSORS //
   ///////////////
 
-#ifdef VNL_CONFIG_CHECK_BOUNDS
+#if VNL_CONFIG_CHECK_BOUNDS
 
   {
   bool exceptionThrownAndCaught = false;