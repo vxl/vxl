513bea76e55b0c54ce96e2bc4da7c70291494c2d
Hans Johnson <hans-johnson@uiowa.edu>
2016-03-05 17:45:13 -0600

    ENH: Simplify std:: math function definitions
    In the early to mid 2000's C++ compilers had
    many variations and shortcommings.  A set of
    vcl_* functions were made to provide a consistent
    interface to the C++98 standard functions and
    features.  Currently all VXL supported compilers
    are now compliant and consistently provide
    interfaces to these functions, so there
    is no need for the complicated include files
    or the complicated optional code paths.
    
    This patch set identified the only valid
    code path and greatly simplifies the conditional
    logic so that maintenance is easier.
    
    Additionally, it demonstrates that packages
    that depend on this version of VXL could simply
    use the C++ provided version of these functions
    with out using the aliases.

--- vcl/vcl_complex.hxx
+++
@@ -1,9 +0,0 @@
-#ifndef vcl_complex_hxx_
-#define vcl_complex_hxx_
-// -*- c++ -*-
-
-#include "vcl_complex.h"
-
-#include "iso/vcl_complex.hxx"
-
-#endif
--- vcl/vcl_cstdlib.h
+++ vcl/vcl_cstdlib.h
@@ -3,15 +3,42 @@
 #define vcl_cstdlib_h_
 //:
 // \file
-//
-// [26.5.4] In addition to the signatures from <stdlib.h> the C++
-// header <cstdlib> adds the overloaded signatures :
-//   long   abs(long);        // labs()
-//   ldiv_t div(long, long);  // ldiv()
-//
+// Early compilers required work arounds for
+// these functions that are not longer needed
+// for the current set of supported compilers.
+// greatly simplifying the aliasing of fuctions
+// that are never changed.
 // NB: size_t is declared in <cstddef>, not <cstdlib>
 
 #include "vcl_compiler.h"
-#include "iso/vcl_cstdlib.h"
+#include "vcl_cmath.h"
+#include <cstdlib>
+
+#define vcl_div std::div
+#define vcl_labs std::labs
+#define vcl_ldiv std::ldiv
+#define vcl_srand std::srand
+#define vcl_rand std::rand
+#define vcl_atexit std::atexit
+#define vcl_getenv std::getenv
+#define vcl_system std::system
+#define vcl_exit std::exit
+#define vcl_abort std::abort
+#define vcl_qsort std::qsort
+#define vcl_calloc std::calloc
+#define vcl_malloc std::malloc
+#define vcl_free std::free
+#define vcl_realloc std::realloc
+#define vcl_atol std::atol
+#define vcl_atof std::atof
+#define vcl_atoi std::atoi
+#define vcl_mblen std::mblen
+#define vcl_mbstowcs std::mbstowcs
+#define vcl_mbtowc std::mbtowc
+#define vcl_strtod std::strtod
+#define vcl_strtol std::strtol
+#define vcl_strtoul std::strtoul
+#define vcl_wctomb std::wctomb
+#define vcl_wcstombs std::wcstombs
 
 #endif // vcl_cstdlib_h_
