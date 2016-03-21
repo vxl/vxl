5e0dbbcf7902352351815663b7602d60cc35c6fe
Joseph L. Mundy <mundyj@users.sourceforge.net>
2016-03-21 19:21:45 -0400

    remove unused template instantiations

--- contrib/brl/bbas/bvgl/Templates/vbl_array_3d+vgl_point_3d+double--.cxx
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <vgl/vgl_point_3d.h>
-#include <vcl_compiler.h>
-#include <vbl/vbl_array_3d.hxx>
-typedef std::vector<vgl_point_3d<double> > vpt;
-VBL_ARRAY_3D_INSTANTIATE(vpt );
