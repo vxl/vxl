e02c5cf8461805f7224ff5c8ec66f69c3b6b744e
Ozge Ozcanli <ozge@visionsystemsinc.com>
2016-07-14 09:36:38 -0400

    adding phase correlation process

--- contrib/brl/bseg/bvxm/pyscripts/bvxm_brip_adaptor.py
+++ contrib/brl/bseg/bvxm/pyscripts/bvxm_brip_adaptor.py
@@ -31,3 +31,24 @@ def image_mutual_info(image1, image2, min_val, max_val, n_bins):
         return mutual_info
     else:
         return -1.0
+
+def phase_correlation(image1, image2, gauss_sigma = 1.0, peak_radius = 2.0, alpha = 0.5):
+    bvxm_batch.init_process("bripPhaseCorrelationProcess")
+    bvxm_batch.set_input_from_db(0, image1)
+    bvxm_batch.set_input_from_db(1, image2)
+    bvxm_batch.set_input_float(2, gauss_sigma)
+    bvxm_batch.set_input_float(3, peak_radius)
+    bvxm_batch.set_input_float(4, alpha)
+    status = bvxm_batch.run_process()
+    if status:
+        (id, type) = bvxm_batch.commit_output(0)
+        tu = bvxm_batch.get_output_float(id)
+        (id, type) = bvxm_batch.commit_output(1)
+        tv = bvxm_batch.get_output_float(id)
+        (id, type) = bvxm_batch.commit_output(2)
+        conf = bvxm_batch.get_output_float(id)
+        (id, type) = bvxm_batch.commit_output(3)
+        out_img = dbvalue(id, type)
+        return status, tu, tv, conf, out_img
+    else:
+        return status, 0.0, 0.0, 0.0, 0
