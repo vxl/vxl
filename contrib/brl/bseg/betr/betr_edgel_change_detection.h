f3f370e6bd7c8865080e2ef576c31ac5f6162a32
mundyj <jlm@visionsystemsinc.com>
2016-10-16 17:20:15 -0400

    Added parameter classes

--- contrib/brl/bseg/betr/betr_edgel_change_detection.h
+++ contrib/brl/bseg/betr/betr_edgel_change_detection.h
@@ -8,17 +8,19 @@
 //
 #include <string>
 #include "betr_algorithm.h"
+#include "betr_edgel_change_detection_params.h"
 class betr_edgel_change_detection : public betr_algorithm
 {
  public:
- betr_edgel_change_detection(): betr_algorithm("edgel_change_detection"), sigma_(1.0), noise_mul_(0.75), js_div_(0.0), i_offset_(0), j_offset_(0), change_img_(VXL_NULLPTR){}
+ betr_edgel_change_detection(): betr_algorithm("edgel_change_detection"),js_div_(0.0), i_offset_(0), j_offset_(0), change_img_(VXL_NULLPTR){
+	 params_ =  new betr_edgel_change_detection_params();}
 
- betr_edgel_change_detection(double sigma, double noise_mul): betr_algorithm("edgel_change_detection"), sigma_(sigma),noise_mul_(noise_mul),
-    js_div_(0.0), i_offset_(0), j_offset_(0), change_img_(VXL_NULLPTR){}
+ betr_edgel_change_detection(betr_edgel_change_detection_params const& params): betr_algorithm("edgel_change_detection"), 
+    js_div_(0.0), i_offset_(0), j_offset_(0), change_img_(VXL_NULLPTR){params_ =  new betr_edgel_change_detection_params();}
 
   // offset and alpha are  sigmoid parameters to convert js_divergence to a change probability
- betr_edgel_change_detection(double sigma, double noise_mul, double offset, double alpha): betr_algorithm("edgel_change_detection", offset, alpha),
-    sigma_(sigma),noise_mul_(noise_mul), js_div_(0.0), i_offset_(0), j_offset_(0), change_img_(VXL_NULLPTR){}
+ betr_edgel_change_detection(betr_edgel_change_detection_params const& params, double offset, double alpha): betr_algorithm("edgel_change_detection", offset, alpha), js_div_(0.0),
+	 i_offset_(0), j_offset_(0), change_img_(VXL_NULLPTR){params_ =  new betr_edgel_change_detection_params();}
   //: process change
   virtual bool process();
 
@@ -33,8 +35,6 @@ class betr_edgel_change_detection : public betr_algorithm
   //:results
   double js_div_;
   //: parameters
-  double sigma_;
-  double noise_mul_;
   unsigned i_offset_;
   unsigned j_offset_;
   vil_image_resource_sptr change_img_;
---
+++ contrib/brl/bseg/betr/betr_edgel_factory_params.cxx
@@ -0,0 +1,4 @@
+#include "betr_edgel_factory_params.h"
+bool betr_edgel_factory_params::sanity_check(std::string& errors) const{
+  return true;
+}
