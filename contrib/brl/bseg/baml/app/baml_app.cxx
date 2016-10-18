1778a042d664358bb778d14c52513ac7b58d2650
Tom Pollard <tom.pollard@stresearch.com>
2016-10-18 13:37:47 -0400

    First attempt at baml integration in betr

--- contrib/brl/bseg/baml/app/baml_app.cxx
+++ contrib/brl/bseg/baml/app/baml_app.cxx
@@ -29,19 +29,36 @@ int main(int argc,char * argv[])
   // Input params
   //
 
-  vpgl_lvcs lvcs( 35.1996380169, 48.6745313514, 1670.27321333 );
-  vgl_box_2d<int> region( 2900, 3600, 900, 1900 );
-  //vgl_box_2d<int> region( 2000, 3000, 3500, 4375 );
-  double z_ground = 25.0;
-
+  //vpgl_lvcs lvcs( 35.1996380169, 48.6745313514, 1670.27321333 );
+  //vgl_box_2d<int> region( 2900, 3600, 900, 1900 );
+  
+  /*vpgl_lvcs lvcs( 35.2080385626545, 48.70485305698821, 1675 );
   std::string target_img_file("D:/data/sattel/hamadan/20160820_073052_0c76.tif");
   std::string target_cam_file("D:/data/sattel/hamadan/20160820_073052_0c76.tif_RPC.txt");
-  //std::string target_img_file("D:/data/sattel/hamadan/20160701_091758_0c64.tif");
-  //std::string target_cam_file("D:/data/sattel/hamadan/20160701_091758_0c64.tif_RPC.txt");
+  std::string ref_img_file("D:/data/sattel/hamadan/20160817_135113_0c68.tif");
+  std::string ref_cam_file("D:/data/sattel/hamadan/20160817_135113_0c68.tif_RPC.txt");
+  std::string output_namebase("D:/results/20160820_073052_0c76_20160817_135113_0c68");
+  vgl_box_2d<int> region( 1700, 3800, 0, 1900 );
+  double z_ground = 0.0;*/
+
+  vpgl_lvcs lvcs( 35.2080385626545, 48.70485305698821, 1675 );
+  std::string target_img_file("D:/data/sattel/hamadan/20160820_073052_0c76.tif");
+  std::string target_cam_file("D:/data/sattel/hamadan/20160820_073052_0c76.tif_RPC.txt");
   std::string ref_img_file("D:/data/sattel/hamadan/20160831_063745_0e0d.tif");
   std::string ref_cam_file("D:/data/sattel/hamadan/20160831_063745_0e0d.tif_RPC.txt");
-  std::string output_namebase("D:/results/a");
+  std::string output_namebase("D:/results/20160820_073052_0c76_20160831_063745_0e0d");
+  //vgl_box_2d<int> region( 1900, 3800, 0, 1900 );
+  vgl_box_2d<int> region( 3000, 3800, 0, 800 );
+  double z_ground = 0.0;//*/
 
+  /*vpgl_lvcs lvcs( 35.2080385626545, 48.70485305698821, 1675 );
+  std::string target_img_file("D:/data/sattel/hamadan/20160902_094643_0c19.tif");
+  std::string target_cam_file("D:/data/sattel/hamadan/20160902_094643_0c19.tif_RPC.txt");
+  std::string ref_img_file("D:/data/sattel/hamadan/20160911_063924_0e20.tif");
+  std::string ref_cam_file("D:/data/sattel/hamadan/20160911_063924_0e20.tif_RPC.txt");
+  std::string output_namebase("D:/results/20160902_094643_0c19_20160911_063924_0e20");
+  vgl_box_2d<int> region( 1900, 3800, 0, 1900 );
+  double z_ground = 0.0;*/
 
 
 
@@ -135,7 +152,7 @@ int main(int argc,char * argv[])
 
   // Detect changes
   vil_image_view<float> tar_lh, tar_prob;
-  baml_detect_change_bt( tar_blur, ref_warped, ref_valid, tar_lh );
+  baml_detect_change_bt( tar_blur, ref_cor, ref_valid, tar_lh, 50.0f );
   //baml_detect_change_census( tar_blur, ref_warped, ref_valid, tar_lh, 0.3f, 10 );
   //baml_detect_change_nonparam( tar_blur, ref_warped, ref_valid, tar_lh );
 
