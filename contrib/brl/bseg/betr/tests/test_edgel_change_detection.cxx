a11efb121be3480f759183db3fb81016f1b480c6
mundyj <jlm@visionsystemsinc.com>
2016-10-21 15:28:20 -0400

    test for single mesh face

--- contrib/brl/bseg/betr/tests/test_edgel_change_detection.cxx
+++ contrib/brl/bseg/betr/tests/test_edgel_change_detection.cxx
@@ -320,6 +320,20 @@ void test_edgel_change_detection()
  double lat = 35.1964842393;
  double elev =1678.81629561;
  vpgl_lvcs lvcs = vpgl_lvcs(lat, lon, elev, vpgl_lvcs::wgs84, vpgl_lvcs::DEG, vpgl_lvcs::METERS);
+ vil_image_resource_sptr ref_imgr = vil_load_image_resource(ref_img_path.c_str());
+ //vpgl_local_rational_camera<double>* ref_lcam = read_local_rational_camera_from_txt<double>(ref_cam_path);
+ vpgl_rational_camera<double>* ref_rpccam = read_rational_camera_from_txt<double>(ref_cam_path);
+ if(!ref_rpccam)
+	 return;
+ vpgl_local_rational_camera<double>* ref_lcam = new vpgl_local_rational_camera<double>( lvcs, *ref_rpccam );
+ vpgl_camera_double_sptr ref_camera( ref_lcam );
+ if(!ref_camera)
+	 return;
+ vil_image_resource_sptr imgr = vil_load_image_resource(evt_img_path.c_str());
+ //vpgl_local_rational_camera<double>* lcam = read_local_rational_camera_from_txt<double>(evt_cam_path);
+ vpgl_rational_camera<double>* rpccam = read_rational_camera_from_txt<double>(evt_cam_path);
+ vpgl_local_rational_camera<double>* lcam = new vpgl_local_rational_camera<double>( lvcs, *rpccam );
+ vpgl_camera_double_sptr camera(lcam);
  betr_event_trigger etr("hamadan", lvcs);
  etr.set_verbose(true);
  etr.add_geo_object("tarmac_ref", lon, lat, elev, ref_obj_path, true);
