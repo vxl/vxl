5355f271cc385fa5fc83a50c1d558e0ee283a77a
Joseph Mundy <jlm@visionsystemsinc.com>
2017-09-18 11:34:51 -0400

    removed ineffective bwm tests

--- contrib/brl/bbas/bwm/tests/test_driver.cxx
+++ contrib/brl/bbas/bwm/tests/test_driver.cxx
@@ -1,13 +1,9 @@
 #include <testlib/testlib_register.h>
 
-DECLARE(test_move_proj_plane);
-DECLARE(test_delaunay);
 
 void
 register_tests()
 {
-  REGISTER(test_move_proj_plane);
-  REGISTER(test_delaunay);
 }
 
 DEFINE_MAIN;
