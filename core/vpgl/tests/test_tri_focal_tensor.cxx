376d4024674fdb877dceaf27d33e7b02f544f7fb
Joseph Mundy <jlm@visionsystemsinc.com>
2018-04-02 07:38:27 -0400

    Trifocal tensor, expanding contrib/oxl/mvl/TriTensor.h - The trifocal tensor is a generalization of the fundamental matrix in three views. - Upgrade and expand contrib/oxl/mvl/TriTensor.h - New VPGL code offers both projective and affine trifocal tensors. - Unit tests included

---
+++ core/vpgl/tests/test_tri_focal_tensor.cxx
@@ -0,0 +1,169 @@
+#include <iostream>
+#include <testlib/testlib_test.h>
+
+#ifdef _MSC_VER
+#  include <vcl_msvc_warnings.h>
+#endif
+
+#include <vnl/vnl_fwd.h>
+#include <vnl/vnl_matrix_fixed.h>
+#include <vnl/vnl_vector_fixed.h>
+#include <vgl/vgl_point_2d.h>
+#include <vgl/vgl_point_3d.h>
+#include <vgl/vgl_distance.h>
+#include <vgl/vgl_homg_point_2d.h>
+#include <vgl/vgl_homg_point_3d.h>
+#include <vgl/vgl_homg_line_2d.h>
+#include <vgl/vgl_line_segment_2d.h>
+#include <vgl/vgl_line_segment_3d.h>
+#include <vgl/vgl_homg_line_3d_2_points.h>
+#include <vgl/vgl_homg_plane_3d.h>
+#include <vgl/vgl_ray_3d.h>
+#include <vgl/algo/vgl_h_matrix_3d.h>
+#include <vpgl/vpgl_proj_camera.h>
+#include <vpgl/vpgl_tri_focal_tensor.h>
+
+static void test_tri_focal_tensor()
+{
+  vpgl_tri_focal_tensor<double> T0;
+
+  vgl_homg_point_3d<double> X(0.5, 0.5, 2.0, 1.0), X0(1.0, 1.0, 2.0, 1.0);
+
+  // canonical camera
+  vpgl_proj_camera<double> c1;
+  vgl_homg_point_2d<double> x0(0, 0, 1), x1 = c1.project(X), x10 = c1.project(X0);
+  vgl_homg_line_2d<double> l1(x1, x10);
+  // move one unit along x
+  vnl_matrix_fixed<double,3,4> M( 0.0 );
+  M[0][0]=1.0;  M[1][1]=1.0;   M[2][2]=1.0;
+  M[0][3] = -1.0;
+  vpgl_proj_camera<double> c2(M);
+  vgl_homg_point_2d<double> x2 = c2.project(X), x20 = c2.project(X0);
+
+  // move one unit along y
+  M[0][3] = 0.0;   M[1][3] = -1.0; 
+  vpgl_proj_camera<double> c3(M);
+  vgl_homg_point_2d<double> x3 = c3.project(X), x30 = c3.project(X0);
+
+  vpgl_tri_focal_tensor<double> T(c1, c2, c3);
+  std::cout << T << std::endl;
+
+  vgl_homg_line_2d<double> l2(x2, x20), l3(x3, x30);//(l2 0.5, -0.5, 0.25) (l3 -0.5, 0.5, 0.25)
+  vgl_homg_line_2d<double> tl1= T.image1_transfer(l2,l3);
+  vgl_homg_line_2d<double> tl2= T.image2_transfer(l1,l3);
+  vgl_homg_line_2d<double> tl3 = T.image3_transfer(l1, l2);
+  std::cout << "tl1 " << tl1 << std::endl;
+  std::cout << "tl2 " << tl2 << std::endl;
+  std::cout << "tl3 " << tl2 << std::endl;
+  double er_lines = fabs(tl1.a() / tl1.b() +1.0);
+  er_lines += fabs(tl2.a() / tl2.c() - 2.0);
+  er_lines +=fabs(tl2.b() / tl2.c() + 2.0);
+  er_lines += fabs(tl3.a() / tl3.c() + 2.0);
+  er_lines +=fabs(tl3.b() / tl3.c() - 2.0);
+  TEST_NEAR("line transfer", er_lines, 0.0, 0.0001);
+  vgl_homg_point_2d<double> tx1 = T.image1_transfer(x2, x3);
+  std::cout << "tx1 " << tx1.x()/tx1.w()<< ' '  << tx1.y()/tx1.w()<< std::endl;
+  double er_points = fabs(tx1.x() / tx1.w() - 0.25);
+  er_points += fabs(tx1.y() / tx1.w() - 0.25);
+  vgl_homg_point_2d<double> tx2 = T.image2_transfer(x1, x3);
+  std::cout << "tx2 " << tx2.x()/tx2.w()<< ' '  << tx2.y()/tx2.w()<< std::endl;
+  er_points += fabs(tx2.x() / tx2.w() + 0.25);
+  er_points += fabs(tx2.y() / tx2.w() - 0.25);
+  vgl_homg_point_2d<double> tx3 = T.image3_transfer(x1, x2);
+  std::cout << "tx3 " << tx3.x()/tx3.w()<< ' '  << tx3.y()/tx3.w()<< std::endl;
+  er_points += fabs(tx3.x() / tx2.w() - 0.25);
+  er_points += fabs(tx3.y() / tx3.w() + 0.25);
+  TEST_NEAR("point transfer", er_points, 0.0, 0.0001);
+  bool good_epipoles = T.compute_epipoles();
+  bool good_f_matrices = T.compute_f_matrices();
+  bool good_f23 = T.compute_f_matrix_23();
+  bool good_p_cams = T.compute_proj_cameras();
+  TEST("Good Computations", good_epipoles&&good_f_matrices&&good_f23&&good_p_cams, true);
+  vgl_homg_point_2d<double> e12, e13;
+  T.get_epipoles(e12, e13);
+  std::cout << "e12 " << e12 << " e13 " << e13 << std::endl;
+  double er_e12 = fabs(e12.x() / e12.x() - 1.0) + fabs(e12.y()) + fabs(e12.w());
+  double er_e13 = fabs(e13.y() / e13.y() - 1.0) + fabs(e13.x()) + fabs(e12.w());
+  TEST_NEAR("Epipoles", er_e12 + er_e13, 0.0, 0.0001);
+  vpgl_fundamental_matrix<double> f12 = T.fmatrix_12();
+  vpgl_fundamental_matrix<double> f13 = T.fmatrix_13();
+  vpgl_fundamental_matrix<double> f23 = T.fmatrix_23();
+  std::cout << "F12\n" << f12 << std::endl;
+  std::cout << "F13\n" << f13 << std::endl;
+  std::cout << "F23\n" << f23 << std::endl;
+  vgl_homg_line_2d<double> epl2 = f12.r_epipolar_line(x1);
+  double alg_dist_2 = x2.x()*epl2.a() + x2.y()*epl2.b() + x2.w()*epl2.c();
+  TEST_NEAR("Fundamental Matrix F12", fabs(alg_dist_2), 0.0, 0.0001);
+  vgl_homg_line_2d<double> epl3 = f13.r_epipolar_line(x1);
+  double alg_dist_3 = x3.x()*epl3.a() + x3.y()*epl3.b() + x3.w()*epl3.c();
+  TEST_NEAR("Fundamental Matrix F13", fabs(alg_dist_3), 0.0, 0.0001);
+  vgl_homg_line_2d<double> epl23 = f23.r_epipolar_line(x2);
+  double alg_dist_23 = x3.x()*epl23.a() + x3.y()*epl23.b() + x3.w()*epl23.c();
+  TEST_NEAR("Fundamental Matrix F23", fabs(alg_dist_23), 0.0, 0.0001);
+
+  vgl_h_matrix_2d<double> H13 = T.hmatrix_13(epl3);
+  vgl_h_matrix_2d<double> H12 = T.hmatrix_12(epl2);
+  std::cout << "H13\n" << H13 << std::endl;
+  std::cout << "H12\n" << H12 << std::endl;
+  std::cout << "x13 " << H13*x3 << std::endl;
+  std::cout << "x12 " << H12*x2 << std::endl;
+
+  // matrix three point constraint
+  vnl_matrix_fixed<double,3,3> zero_3x3 = T.point_constraint_3x3(x1,x2,x3);
+  std::cout << "zero 3x3\n" << zero_3x3 << std::endl;
+  double fbz = zero_3x3.frobenius_norm();
+  TEST_NEAR("perfect tri point match", fbz, 0.0, 0.000001);
+
+  //scalar three point constraint
+  double scalar_zero = T.point_constraint(x1, x2, x3);
+  std::cout << "scalar zero = " << scalar_zero << std::endl;
+  TEST_NEAR("perfect scalar tri point match", scalar_zero, 0.0, 0.000001);
+
+  // test with perturbed points
+  double d = 1.5;
+  vgl_homg_point_2d<double> x2p((x2.x()/x2.w())+d, (x2.y()/x2.w())+d);  
+  vnl_matrix_fixed<double,3,3> zero_3x3_p = T.point_constraint_3x3(x1,x2p,x3);
+  std::cout << "zero 3x3 perturbed\n" << zero_3x3_p << std::endl;
+  double fbp = zero_3x3_p.frobenius_norm();
+  TEST("perturbed tri point match", fbp>0.0, true);
+  // scalar point constraint
+  double scalar_zero_p = T.point_constraint(x1, x2p, x3);
+  std::cout << "perturbed scalar zero = " << scalar_zero_p << std::endl;
+  TEST("perturbed scalar tri point match", scalar_zero_p>0.0, true);
+  // test homographies
+
+  // from a line in image 3
+  vgl_homg_line_2d<double> line_3(x30, x3);
+  line_3.normalize();
+
+  vgl_h_matrix_2d<double> HL12 = T.hmatrix_12(line_3);
+  vgl_homg_point_2d<double> p2_HL12 = HL12*x1;
+  vgl_point_2d<double> p2(p2_HL12), p2_gt(x2);
+  std::cout << "p2 " << p2 << " vs. p2_gt " << p2_gt<< std::endl;
+  double er_p2 = (p2 - p2_gt).length();
+  TEST_NEAR("H12 from line", er_p2, 0.0, 0.001);
+
+  // from a line in image 2
+  vgl_homg_line_2d<double> line_2(x20, x2);
+  line_2.normalize();
+
+  vgl_h_matrix_2d<double> HL13 = T.hmatrix_13(line_2);
+  vgl_homg_point_2d<double> p3_HL13 = HL13*x1;
+  vgl_point_2d<double> p3(p3_HL13), p3_gt(x3);
+  std::cout << "p3 " << p3 << " vs. p3_gt " << p3_gt<< std::endl;
+  double er_p3 = (p3 - p3_gt).length();
+  TEST_NEAR("H13 from line", er_p3, 0.0, 0.001);
+  //  test line constraint
+  double one_sixth = 1.0 / 6.0;
+  vgl_homg_point_2d<double> hlc10(0.25, 0.25), hlc11(one_sixth, one_sixth);
+  vgl_homg_point_2d<double> hlc20(-0.25, 0.25), hlc21(-0.5, one_sixth);
+  vgl_homg_point_2d<double> hlc30(0.25, -0.25), hlc31(one_sixth, -0.5);
+  vgl_homg_line_2d<double> lc1(hlc10, hlc11),lc2(hlc20, hlc21),lc3(hlc30, hlc31);
+  vnl_vector_fixed<double, 3> lc_3 = T.line_constraint_3(lc1, lc2, lc3);
+  std::cout << "line constraint 3 " << lc_3 << std::endl;
+  double er_lc = lc_3.magnitude();
+  TEST_NEAR("three line constraint", er_lc, 0.0, 1e-10);
+}
+
+
+TESTMAIN(test_tri_focal_tensor);
---
+++ core/vpgl/vpgl_tri_focal_tensor.h
@@ -0,0 +1,286 @@
+// This is vpgl/vpgl_tri_focal_tensor.h
+#ifndef vpgl_tri_focal_tensor_h_
+#define vpgl_tri_focal_tensor_h_
+#ifdef VCL_NEEDS_PRAGMA_INTERFACE
+#pragma interface
+#endif
+//:
+// \file
+// \brief The trifocal tensor
+//
+// A class to hold a Trifocal Tensor and perform common operations, such as
+// point and line transfer, coordinate-frame transformation and I/O.
+//
+// \author
+//             Paul Beardsley, 29.03.96
+//             Oxford University, UK
+//
+// \verbatim
+//  Modifications:
+//   AWF - Added composition, transformation, homography generation.
+//   Peter Vanroose - 11 Mar 97 - added operator==
+//   Peter Vanroose - 22 Jun 03 - added vgl interface
+//   JL Mundy       - 30 Mar 18 - moved to vpgl, templated
+// \endverbatim
+//
+//------------------------------------------------------------------------------
+
+#include <vector>
+#include <iostream>
+#include <iosfwd>
+#include <memory>
+#include <stdexcept>
+#include <vbl/vbl_array_3d.h>
+#include <vnl/vnl_matrix.h>
+#include <vnl/vnl_matrix_fixed.h>
+#include <vgl/vgl_fwd.h>
+#include <vgl/algo/vgl_algo_fwd.h>
+#include <vpgl/vpgl_proj_camera.h>
+#include <vpgl/vpgl_fundamental_matrix.h>
+
+template <class Type>
+class vpgl_tri_focal_tensor
+{
+  // Data Members------------------------------------------------------------
+protected:
+  vbl_array_3d<Type> T_;
+
+  // Epipoles
+  // e12 is the epipole corresponding to the center of camera 1 projected into image 2
+  // e13 is the epipole corresponding to the center of camera 1 projected into image 3
+  bool epipoles_valid_;
+  vgl_homg_point_2d<Type> e12_;
+  vgl_homg_point_2d<Type> e13_;
+
+  // cameras
+  bool cameras_valid_;
+  vpgl_proj_camera<Type> c1_;
+  vpgl_proj_camera<Type> c2_;
+  vpgl_proj_camera<Type> c3_;
+
+  // fundamental matrices
+  // f12 is the fundamental matrix mapping a point in image 1 into a line in image 2
+  // f13 is the fundamental matrix mapping a point in image 1 into a line in image 3
+  // (note this order is opposite to H&Z but seems more intuitive)
+  //
+  bool f_matrices_1213_valid_;
+  vpgl_fundamental_matrix<Type> f12_;
+  vpgl_fundamental_matrix<Type> f13_;
+  bool f_matrix_23_valid_;
+  vpgl_fundamental_matrix<Type> f23_;
+
+  // set flags false and results invalid
+  void init();
+  // make the Frobenius norm == 1
+  void normalize();
+ public:
+
+  // Constructors/Initializers/Destructors-----------------------------------
+
+ vpgl_tri_focal_tensor(): T_(vbl_array_3d<Type>(3, 3, 3, Type(0))),cameras_valid_(false)
+    {
+      for(size_t i = 0; i<3; ++i) T_[i][i][i]=Type(1);
+      this->init();
+    }
+ vpgl_tri_focal_tensor(const vbl_array_3d<Type>& T): T_(T),cameras_valid_(false)
+    {
+      this->init();
+    }
+  //: Construct from 27-element vector
+ vpgl_tri_focal_tensor(const Type *tri_focal_tensor_array): T_(vbl_array_3d<Type>(3, 3, 3, tri_focal_tensor_array)),cameras_valid_(false){
+    this->init();
+  }
+
+  //: Construct from three cameras
+ vpgl_tri_focal_tensor(const vpgl_proj_camera<Type>& c1, const vpgl_proj_camera<Type>& c2, const vpgl_proj_camera<Type>& c3):
+  T_(vbl_array_3d<Type>(3, 3, 3, Type(0))){
+    set(c1, c2, c3);
+    this->init();// init must be second to avoid writing over cameras
+  }
+
+  //: Construct from two remaining cameras, the first camera is already canonical, i.e. [I | 0]
+ vpgl_tri_focal_tensor(const vpgl_proj_camera<Type>& c2, const vpgl_proj_camera<Type>& c3):
+  T_(vbl_array_3d<Type>(3, 3, 3, Type(0))){
+    set( vpgl_proj_camera<Type>(), c2, c3);
+    this->init();// init must be second to avoid writing over cameras
+  }
+
+  //: Construct from three camera matrices
+ vpgl_tri_focal_tensor(const vnl_matrix_fixed<Type,3,4>& m1, const vnl_matrix_fixed<Type,3,4>& m2, const vnl_matrix_fixed<Type,3,4>& m3)
+   {
+    set(vpgl_proj_camera<Type>(m1), vpgl_proj_camera<Type>(m2), vpgl_proj_camera<Type>(m3));
+    this->init();// init must be second to avoid writing over cameras
+   }
+
+  //: Construct from two camera matrices
+ vpgl_tri_focal_tensor(const vnl_matrix_fixed<Type,3,4>& m2, const vnl_matrix_fixed<Type,3,4>& m3){
+    set(vpgl_proj_camera<Type>(),  vpgl_proj_camera<Type>(m2), vpgl_proj_camera<Type>(m3));
+    this->init();// init must be second to avoid writing over cameras
+ }
+
+  virtual ~vpgl_tri_focal_tensor() {}
+
+  //: compute all derivative quantities
+  virtual bool compute() {
+    return (
+      this->compute_epipoles() &&
+      this->compute_f_matrices() &&
+      this->compute_proj_cameras() &&
+      this->compute_f_matrix_23());
+  }
+  // Data Access-------------------------------------------------------------
+
+  // vpgl_tri_focal_tensor<Type>& operator=(const vpgl_tri_focal_tensor<Type>& T);
+  virtual bool operator==(vpgl_tri_focal_tensor<Type> const& T) const {
+    for (size_t i=0;i<3;++i) for (size_t j=0;j<3;++j) for (size_t k=0;k<3;++k) if (T_(i,j,k)!=T(i,j,k)) return false;
+    return true; }
+  Type& operator() (size_t i1, size_t i2, size_t i3) { return T_(i1,i2,i3); }
+  Type operator() (size_t i1, size_t i2, size_t i3) const { return T_(i1,i2,i3); }
+
+  void set(size_t i1, size_t i2, size_t i3, Type value){T_(i1, i2, i3) = value;}
+
+  void set(vbl_array_3d<Type>& array){
+    *this = vpgl_tri_focal_tensor<Type>(array);
+  }
+  void set(const vpgl_proj_camera<Type>& c1, const vpgl_proj_camera<Type>& c2, const vpgl_proj_camera<Type>& c3);
+  void set(const vpgl_proj_camera<Type>& c2, const vpgl_proj_camera<Type>& c3){
+    vpgl_proj_camera<Type> canon; this->set(canon, c2, c3);
+  }
+  void set(const vnl_matrix_fixed<Type,3,4>& M1, const vnl_matrix_fixed<Type,3,4>& M2, const vnl_matrix_fixed<Type,3,4>& M3){
+    this->set(vpgl_proj_camera<Type>(M1), vpgl_proj_camera<Type>(M2), vpgl_proj_camera<Type>(M3));
+  }
+
+  // Data Control------------------------------------------------------------
+  //: tri focal tensor point constraint (should be a 3x3 array of all zeros if points correspond)
+  vnl_matrix_fixed<Type, 3, 3> point_constraint_3x3(vgl_homg_point_2d<Type> const& point1,
+                                                    vgl_homg_point_2d<Type> const& point2,
+                                                    vgl_homg_point_2d<Type> const& point3);
+
+  
+  //:tri focal tensor scalar point constraint (should == 0 if points correspond)
+  Type point_constraint(vgl_homg_point_2d<Type> const& point1,
+                        vgl_homg_point_2d<Type> const& point2,
+                        vgl_homg_point_2d<Type> const& point3);
+
+  //: tri focal tensor line constraint (should be a 3 vector all zeros if lines correspond)
+  vnl_vector_fixed<Type, 3> line_constraint_3(vgl_homg_line_2d<Type> const& line1,
+                                              vgl_homg_line_2d<Type> const& line2,
+                                              vgl_homg_line_2d<Type> const& line3);
+  //: point transfer 
+  //  point in image 1 corresponding to points in images 2 and 3 and etc.
+  virtual vgl_homg_point_2d<Type> image1_transfer(vgl_homg_point_2d<Type> const& point2,
+                                          vgl_homg_point_2d<Type> const& point3) const;
+  virtual vgl_homg_point_2d<Type> image2_transfer(vgl_homg_point_2d<Type> const& point1,
+                                          vgl_homg_point_2d<Type> const& point3) const;
+  virtual vgl_homg_point_2d<Type> image3_transfer(vgl_homg_point_2d<Type> const& point1,
+                                          vgl_homg_point_2d<Type> const& point2) const;
+  //: line transfer
+  //  line in image 1 corresponding to lines in images 2 and 3 and etc.
+  virtual vgl_homg_line_2d<Type> image1_transfer(vgl_homg_line_2d<Type> const& line2,
+                                         vgl_homg_line_2d<Type> const& line3) const;
+  virtual vgl_homg_line_2d<Type> image2_transfer(vgl_homg_line_2d<Type> const& line1,
+                                         vgl_homg_line_2d<Type> const& line3) const;
+  virtual vgl_homg_line_2d<Type> image3_transfer(vgl_homg_line_2d<Type> const& line1,
+                                         vgl_homg_line_2d<Type> const& line2) const;
+  //: homographies induced by a line
+  // homography between images 3 and 1 given a line in image 2 and etc.
+  virtual vgl_h_matrix_2d<Type> hmatrix_13(vgl_homg_line_2d<Type> const& line2) const;
+  virtual vgl_h_matrix_2d<Type> hmatrix_12(vgl_homg_line_2d<Type> const& line3) const;
+
+
+  virtual bool get_epipoles(vgl_homg_point_2d<Type>& e12, vgl_homg_point_2d<Type>& e13)  {
+    if (!epipoles_valid_) compute_epipoles(); e12 = e12_; e13 = e13_; return epipoles_valid_;
+  }
+ 
+  bool compute_epipoles();
+
+  virtual vgl_homg_point_2d<Type> epipole_12() {if(!epipoles_valid_) compute_epipoles(); return e12_;}
+  virtual vgl_homg_point_2d<Type> epipole_13() {if(!epipoles_valid_) compute_epipoles(); return e13_;}
+
+  // The fundamental matrix between image 1 and image 2 is given by:
+  // ${\tt F12}_{jk} = \left [e12 \right ]_\times T_{ijk} e13_k$.  //note the use of Einstein notation for summation
+  // and between image 1 and image 3 is given by:
+  // ${\tt F13}_{jk} = \left [e13 \right]_\times T_{ijk} e12_j$.
+  // Note that $\left [ e13 \right ]_\times$ is just the skew-symmetric matrix corresponding to e13.
+  //
+  bool compute_f_matrices();
+  bool compute_f_matrix_23();
+  vpgl_fundamental_matrix<Type> fmatrix_12(){if(!f_matrices_1213_valid_) compute_f_matrices(); return f12_;}
+  vpgl_fundamental_matrix<Type> fmatrix_13(){if(!f_matrices_1213_valid_) compute_f_matrices(); return f13_;}
+  vpgl_fundamental_matrix<Type> fmatrix_23(){if(!f_matrix_23_valid_) compute_f_matrix_23(); return f23_;}
+
+  bool compute_proj_cameras();
+
+  vpgl_proj_camera<Type> proj_camera_1(){if(!cameras_valid_) compute_proj_cameras(); return c1_;}
+  vpgl_proj_camera<Type> proj_camera_2(){if(!cameras_valid_) compute_proj_cameras(); return c2_;}
+  vpgl_proj_camera<Type> proj_camera_3(){if(!cameras_valid_) compute_proj_cameras(); return c3_;}
+
+
+  // Utility Methods---------------------------------------------------------
+
+  void get_constraint_lines_image1(vgl_homg_point_2d<Type> const& p2,
+                                   vgl_homg_point_2d<Type> const& p3,
+                                   std::vector<vgl_homg_line_2d<Type> >& lines) const;
+
+  void get_constraint_lines_image2(vgl_homg_point_2d<Type> const& p1,
+                                   vgl_homg_point_2d<Type> const& p3,
+                                   std::vector<vgl_homg_line_2d<Type> >& lines) const;
+
+  void get_constraint_lines_image3(vgl_homg_point_2d<Type> const& p1,
+                                   vgl_homg_point_2d<Type> const& p2,
+                                   std::vector<vgl_homg_line_2d<Type> >& lines) const;
+
+
+  //: Contract Tensor axis tensor_axis with first component of Matrix M.
+  // That is:
+  // For tensor_axis = 1,  Compute T_ijk = T_pjk M_pi
+  // For tensor_axis = 2,  Compute T_ijk = T_ipk M_pj
+  // For tensor_axis = 3,  Compute T_ijk = T_ijp M_pk
+  vpgl_tri_focal_tensor<Type> postmultiply(size_t tensor_axis, const vnl_matrix<Type>& M) const;
+
+  //: Contract Tensor axis tensor_axis with second component of Matrix M.
+  // That is:
+  // For tensor_axis = 1,  Compute T_ijk = M_ip T_pjk
+  // For tensor_axis = 2,  Compute T_ijk = M_jp T_ipk
+  // For tensor_axis = 3,  Compute T_ijk = M_kp T_ijp
+  vpgl_tri_focal_tensor<Type> premultiply(size_t tensor_axis, const vnl_matrix<Type>& M) const;
+
+  //: implementations for individual axes
+  vpgl_tri_focal_tensor<Type> postmultiply1(const vnl_matrix<Type>& M) const;
+  vpgl_tri_focal_tensor<Type> postmultiply2(const vnl_matrix<Type>& M) const;
+  vpgl_tri_focal_tensor<Type> postmultiply3(const vnl_matrix<Type>& M) const;
+
+  vpgl_tri_focal_tensor<Type> premultiply1(const vnl_matrix<Type>& M) const;
+  vpgl_tri_focal_tensor<Type> premultiply2(const vnl_matrix<Type>& M) const;
+  vpgl_tri_focal_tensor<Type> premultiply3(const vnl_matrix<Type>& M) const;
+    
+  // contractions involving vectors
+  //: ${\tt M}_{jk} = T_{ijk} v_i$
+  vnl_matrix_fixed<Type,3,3> dot1(const vnl_vector_fixed<Type,3>& v) const;
+  //: ${\tt M}_{ik} = T_{ijk} v_j$
+  vnl_matrix_fixed<Type,3,3> dot2(const vnl_vector_fixed<Type,3>& v) const;
+  //: ${\tt M}_{ij} = T_{ijk} v_k$
+  vnl_matrix_fixed<Type,3,3> dot3(const vnl_vector_fixed<Type, 3>& v) const;
+  //: ${\tt M}_{kj} = T_{ijk} v_i$ (The transpose of dot1)
+  vnl_matrix_fixed<Type,3,3> dot1t(const vnl_vector_fixed<Type,3>& v) const;
+  //: ${\tt M}_{ki} = T_{ijk} v_j$ (The transpose of dot2).
+  vnl_matrix_fixed<Type,3,3> dot2t(const vnl_vector_fixed<Type,3>& v) const;
+  //: ${\tt M}_{ji} = T_{ijk} v_k$ (The transpose of dot3)
+  vnl_matrix_fixed<Type,3,3> dot3t(const vnl_vector_fixed<Type,3>& v) const;
+
+  // INTERNALS---------------------------------------------------------------
+
+ private:
+
+};
+//: stream operators
+template<class Type>
+std::ostream& operator << (std::ostream&, const vpgl_tri_focal_tensor<Type>& T);
+template<class Type>
+std::istream& operator >> (std::istream&, vpgl_tri_focal_tensor<Type>& T);
+template<class Type>
+
+//: are two tensors within a scale factor of each other, i.e. T1 ~ T2.
+bool within_scale(const vpgl_tri_focal_tensor<Type>& T1, const vpgl_tri_focal_tensor<Type>& T2);
+
+#endif // vpgl_tri_focal_tensor_h_
