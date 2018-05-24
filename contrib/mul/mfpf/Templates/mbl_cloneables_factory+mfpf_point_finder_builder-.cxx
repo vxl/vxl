a31e2eac7f1a20beb8859b3503815cb2fa6b89c9
T.J. Corona <tj.corona@kitware.com>
2018-05-24 11:23:27 -0400

    mbl_cloneables_factory: split into def and implementaiton
    The use of the MBL_CLONEABLES_FACTORY_INSTANTIATE() macro after the
    implementation of mbl_cloneables_factory was causing the compilation error
    "error: explicit specialization of 'objects' after instantiation". Breaking
    the definition and implementation into two files fixes this issue.

--- contrib/mul/mfpf/Templates/mbl_cloneables_factory+mfpf_point_finder_builder-.cxx
+++ contrib/mul/mfpf/Templates/mbl_cloneables_factory+mfpf_point_finder_builder-.cxx
@@ -1,4 +1,4 @@
-#include <mbl/mbl_cloneables_factory.h>
+#include <mbl/mbl_cloneables_factory_def.h>
 #include <mfpf/mfpf_point_finder_builder.h>
 MBL_CLONEABLES_FACTORY_INSTANTIATE(mfpf_point_finder_builder);
-
+#include <mbl/mbl_cloneables_factory.h>
--- contrib/mul/msm/Templates/mbl_cloneables_factory+msm_aligner-.cxx
+++ contrib/mul/msm/Templates/mbl_cloneables_factory+msm_aligner-.cxx
@@ -1,3 +1,4 @@
-#include <mbl/mbl_cloneables_factory.h>
+#include <mbl/mbl_cloneables_factory_def.h>
 #include <msm/msm_aligner.h>
 MBL_CLONEABLES_FACTORY_INSTANTIATE(msm_aligner);
+#include <mbl/mbl_cloneables_factory.h>
