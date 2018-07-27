bb428304cebf9e7d3362b84fd5c63ea44cc33c72
thomas.pollard <thomas.pollard@str.corp>
2018-07-27 14:56:42 -0400

    Added optional directional bias argument to SGM primarily for shadow mitigation

--- contrib/brl/bseg/bsgm/bsgm_disparity_estimator.h
+++ contrib/brl/bseg/bsgm/bsgm_disparity_estimator.h
@@ -12,6 +12,7 @@
 #include <vul/vul_timer.h>
 #include <vnl/vnl_math.h>
 #include <vil/vil_image_view.h>
+#include <vgl/vgl_vector_2d.h>
 
 //:
 // \file
@@ -52,6 +53,12 @@ struct bsgm_disparity_estimator_params
   // when error_check_mode > 0
   vxl_byte shadow_thresh;
 
+  //: Set between 0.0 and 1.0 to bias the SGM directional average against the
+  // the "bias_dir" argument to "compute".  Use this if smoothing from certain
+  // directions (i.e. sun angle for satellite imagery) is unreliable.  Set to
+  // 0 to disable biasing.
+  float directional_bias;
+
   //: Appearance costs computed by different algorithms are statically fused
   // using these weights. Set any to <= 0 to prevent computation.
   float census_weight;
@@ -79,6 +86,7 @@ struct bsgm_disparity_estimator_params
     perform_quadratic_interp(true),
     error_check_mode(1),
     shadow_thresh(0),
+    directional_bias(0.0f),
     census_weight(0.3f),
     xgrad_weight(0.7f),
     census_tol(2),
@@ -114,6 +122,7 @@ class bsgm_disparity_estimator
     const vil_image_view<int>& min_disparity,
     float invalid_disparity,
     vil_image_view<float>& disp_target,
+    vgl_vector_2d<float> bias_dir = vgl_vector_2d<float>(0.0f, 0.0f),
     bool skip_error_check = false);
 
   //: Write out the appearance or total cost volume as a set of images for
@@ -193,7 +202,8 @@ class bsgm_disparity_estimator
     const vil_image_view<bool>& invalid_target,
     const vil_image_view<float>& grad_x,
     const vil_image_view<float>& grad_y,
-    const vil_image_view<int>& min_disparity);
+    const vil_image_view<int>& min_disparity,
+    vgl_vector_2d<float> bias_dir);
 
   //: Pixel-wise directional cost
   inline void compute_dir_cost(
