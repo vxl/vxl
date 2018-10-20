5b06c6dadc2f980d459b7e4cf84fda16f9aa4fba
Hans Johnson <hans.j.johnson@gmail.com>
2018-10-20 13:23:13 -0500

    STYLE: Remove end of line spaces
    Provide conformance with the style guidelines.
    Remove empty end-of-line spaces.

--- contrib/mul/msm/tools/msm_make_shapes.cxx
+++ contrib/mul/msm/tools/msm_make_shapes.cxx
@@ -25,41 +25,41 @@ int main(int argc, char** argv)
     print_usage();
     return 0;
   }
-  
+
   unsigned n_pts=5;
   std::vector<vgl_point_2d<double> > pts(n_pts);
-  
+
   // First four are corners of a square
   pts[0].set(0,0);
   pts[1].set(10,0);
   pts[2].set(10,10);
   pts[3].set(0,10);
-  
+
   for (unsigned i=0;i<n_shapes();++i)
   {
     double A=double(i)/(n_shapes()-1)*0.5*3.1415;
     double r=10;
     pts[4].set(10+r*std::cos(A),r*std::sin(A));
-    
+
     msm_points points;
     points.set_points(pts);
-    
+
     std::stringstream ss;
     ss<<"box_hinge_";
     if (i<10) ss<<"0";
     ss<<i<<".pts";
-    
+
     vcl_string pts_path=output_dir()+"/"+ss.str();
-    
+
     if (!points.write_text_file(pts_path))
     {
       vcl_cerr<<"Failed to write points to "<<pts_path<<std::endl;
       return 1;
     }
   }
-  
+
   std::cout<<"Saved points to "<<output_dir()<<"/box_hinge_XX.pts"<<vcl_endl;
-  
+
 
   return 0;
 }
--- contrib/mul/msm/utils/msm_make_grid.h
+++ contrib/mul/msm/utils/msm_make_grid.h
@@ -13,17 +13,17 @@
 //  Creates (nx+1)*(ny+1) points and 2+nx+ny curves (describing the horizontal
 //  and vertical lines)
 void msm_make_grid(msm_points& points, msm_curves& curves,
-                    unsigned nx, double dx, 
+                    unsigned nx, double dx,
                     unsigned ny, double dy);
 
 //: Generate points and curves to display a grid
 //  Grid contains nx x ny squares, each of size dx x dy.
 //  Draw in n_per_seg points along each line
-//  Creates (nx+1)*(ny+1)*n_per_seg^2 points and 2+nx+ny curves 
+//  Creates (nx+1)*(ny+1)*n_per_seg^2 points and 2+nx+ny curves
 //  (describing the horizontal and vertical lines)
 void msm_make_grid(msm_points& points, msm_curves& curves,
-                   unsigned nx, double dx, 
-                   unsigned ny, double dy, 
+                   unsigned nx, double dx,
+                   unsigned ny, double dy,
                    unsigned n_per_seg);
 
 #endif // msm_make_grid_h_
