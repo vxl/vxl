90fd16dc1443937bcb7cf2af5ae07f9b4c26a453
Hans Johnson <hans.j.johnson@gmail.com>
2018-10-20 09:14:06 -0500

    STYLE: Remove duplicate header includes
    It is redundant to include both the .h and the .hxx file.

--- core/vnl/Templates/vnl_c_vector+ulonglong-.cxx
+++ core/vnl/Templates/vnl_c_vector+ulonglong-.cxx
@@ -5,10 +5,7 @@
 // 4146: unary minus operator applied to unsigned type, result still unsigned
 # pragma warning(disable:4146)
 #endif //VCL_VC
-
-#include <vnl/vnl_c_vector.h>
 #include <vnl/vnl_c_vector.hxx>
-
 VNL_C_VECTOR_INSTANTIATE_ordered(unsigned long long);
 #else
 void vnl_c_vector_ulonglong_dummy(void) {}
