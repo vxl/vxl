5310905be488597b7d1c1faeb1fe8c03cd84f769
Hans Johnson <hans-johnson@uiowa.edu>
2018-10-24 14:20:08 -0500

    COMP: Remove unused typedef warning.

--- contrib/rpl/rgrl/rgrl_util.cxx
+++ contrib/rpl/rgrl/rgrl_util.cxx
@@ -816,8 +816,6 @@ rgrl_util_irls( rgrl_set_of<rgrl_match_set_sptr> const& match_sets,
 {
   DebugFuncMacro( debug_flag, 1, " In irls for model "<<estimator->transformation_type().name()<<'\n' );
 
-  typedef rgrl_match_set::from_iterator  from_iter;
-
   unsigned int iteration = 0;
   unsigned int max_iterations = 25;
   bool failed = false;
