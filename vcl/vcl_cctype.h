7f36e7ebd84bf6858e33ad1900738ede740af055
Hans Johnson <hans-johnson@uiowa.edu>
2018-10-31 15:44:29 -0500

    COMP: Provide feedback for deprecated header files
    Provide feedback to users of deprecated header files.
    These header files will be removed in favor of standard
    C+++ headers in the near future.

--- vcl/vcl_cctype.h
+++ vcl/vcl_cctype.h
@@ -3,5 +3,9 @@
 
 #include <cctype>
 #include "vcl_compiler.h"
-
+#ifdef VXL_LEGACY_FUTURE_REMOVE
+  # error  "Deprecated.  Use equivalant C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
+#else
+  # warning "This header will be removed in future versions of VXL.  Use equivalant C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
+#endif
 #endif // vcl_cctype_h_
--- vcl/vcl_stdexcept.h
+++ vcl/vcl_stdexcept.h
@@ -3,5 +3,9 @@
 
 #include <stdexcept>
 #include "vcl_compiler.h"
-
+#ifdef VXL_LEGACY_FUTURE_REMOVE
+  # error  "Deprecated.  Use equivalant C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
+#else
+  # warning "This header will be removed in future versions of VXL.  Use equivalant C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
+#endif
 #endif // vcl_stdexcept_h_
