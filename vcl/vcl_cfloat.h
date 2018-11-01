780a43898fef76f3f3db01021963858f261ef5a8
Hans Johnson <hans.j.johnson@gmail.com>
2018-11-01 11:28:48 -0500

    STYLE: Fix spelling error in comment.

--- vcl/vcl_cfloat.h
+++ vcl/vcl_cfloat.h
@@ -4,8 +4,8 @@
 #include <cfloat>
 #include "vcl_compiler.h"
 #if VXL_LEGACY_FUTURE_REMOVE
-  # error  "Deprecated.  Use equivalant C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
+  # error  "Deprecated.  Use equivalent C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
 #else
-  # warning "This header will be removed in future versions of VXL.  Use equivalant C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
+  # warning "This header will be removed in future versions of VXL.  Use equivalent C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
 #endif
 #endif // vcl_cfloat_h_
--- vcl/vcl_functional.h
+++ vcl/vcl_functional.h
@@ -4,8 +4,8 @@
 #include <functional>
 #include "vcl_compiler.h"
 #if VXL_LEGACY_FUTURE_REMOVE
-  # error  "Deprecated.  Use equivalant C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
+  # error  "Deprecated.  Use equivalent C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
 #else
-  # warning "This header will be removed in future versions of VXL.  Use equivalant C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
+  # warning "This header will be removed in future versions of VXL.  Use equivalent C++11 header instead. see: vxl/scripts/UseStandardHeaders.py"
 #endif
 #endif // vcl_functional_h_
