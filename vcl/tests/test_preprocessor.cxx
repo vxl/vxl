237ca93f6f08c049a8ee40fcff5e964952c58471
Hans Johnson <hans.j.johnson@gmail.com>
2018-11-02 08:04:01 -0500

    ENH: Update compiler platform test
    With the use of CMake compiler platform identification,
    much of the testing for specific supported VXL compilers has become obsolete.

--- vcl/tests/test_preprocessor.cxx
+++ vcl/tests/test_preprocessor.cxx
@@ -2,12 +2,37 @@
 
 #include <iostream>
 #include <vcl_compiler.h>
+#include <vcl_compiler_detection.h>
 
 int test_preprocessor_main(int /*argc*/,char* /*argv*/[])
 {
-  unsigned compiler_count = 0;
-  unsigned major_count = 0;
-  unsigned minor_count = 0;
+  constexpr unsigned compiler_count
+      = VXL_COMPILER_IS_Comeau
+        + VXL_COMPILER_IS_Intel
+        + VXL_COMPILER_IS_PathScale
+        + VXL_COMPILER_IS_Embarcadero
+        + VXL_COMPILER_IS_Borland
+        + VXL_COMPILER_IS_Watcom
+        + VXL_COMPILER_IS_OpenWatcom
+        + VXL_COMPILER_IS_SunPro
+        + VXL_COMPILER_IS_HP
+        + VXL_COMPILER_IS_Compaq
+        + VXL_COMPILER_IS_zOS
+        + VXL_COMPILER_IS_XL
+        + VXL_COMPILER_IS_VisualAge
+        + VXL_COMPILER_IS_PGI
+        + VXL_COMPILER_IS_Cray
+        + VXL_COMPILER_IS_TI
+        + VXL_COMPILER_IS_Fujitsu
+        + VXL_COMPILER_IS_SCO
+        + VXL_COMPILER_IS_AppleClang
+        + VXL_COMPILER_IS_Clang
+        + VXL_COMPILER_IS_GNU
+        + VXL_COMPILER_IS_MSVC
+        + VXL_COMPILER_IS_ADSP
+        + VXL_COMPILER_IS_IAR
+        + VXL_COMPILER_IS_ARMCC
+        + VXL_COMPILER_IS_MIPSpro;
 
   int result = 0;
 
@@ -17,56 +42,24 @@ int test_preprocessor_main(int /*argc*/,char* /*argv*/[])
   } else if ( compiler_count < 1 ) {
     result = 1;
     std::cout << "FAILED\n"
-             << "This compiler is not recognized by vcl_compiler.h.\n"
+             << "This compiler is not recognized by vcl_compiler_detection.h.\n"
              << "Please contact the VXL maintainers and ask them\n"
              << "to fix it. (vxl-maintainers@lists.sourceforge.net)\n";
   } else if ( compiler_count > 1 ) {
     result = 1;
     std::cout << "FAILED\n"
              << "This compiler is recognized as multiple compilers\n"
-             << "by vcl_compiler.h.\n"
+             << "by vcl_compiler_detection.h.\n"
              << "Please contact the VXL maintainers and ask them\n"
              << "to fix it. (vxl-maintainers@lists.sourceforge.net)\n";
   }
 
-  std::cout << "Compiler release identified: ";
-  if ( major_count == 1 ) {
-    std::cout << "PASSED\n";
-  } else if ( major_count > 1 ) {
-    result = 1;
-    std::cout << "FAILED\n"
-             << "This release is recognized as multiple releases\n"
-             << "by vcl_compiler.h.\n"
-             << "Please contact the VXL maintainers and ask them\n"
-             << "to fix it. (vxl-maintainers@lists.sourceforge.net)\n";
-  } else {
-    std::cout << "(not identified) PASSED\n";
-  }
-
-  std::cout << "Compiler version identified: ";
-  if ( minor_count == 1 ) {
-    std::cout << "PASSED\n";
-  } else if ( minor_count > 1 ) {
-    result = 1;
-    std::cout << "FAILED\n"
-             << "This version is recognized as multiple versions\n"
-             << "by vcl_compiler.h.\n"
-             << "Please contact the VXL maintainers and ask them\n"
-             << "to fix it. (vxl-maintainers@lists.sourceforge.net)\n";
-  } else {
-    std::cout << "(not identified) PASSED\n";
-  }
+  std::cout << "Compiler release identified: "
+            <<  VXL_COMPILER_VERSION_MAJOR << "."
+            <<  VXL_COMPILER_VERSION_MINOR << "."
+            <<  VXL_COMPILER_VERSION_PATCH ;
 
   std::cout <<  "Version identified implies release identified: ";
-  if ( minor_count>0 && major_count==0 ) {
-    result = 1;
-    std::cout << "FAILED\n"
-             << "This compiler defines a flag for the compiler version\n"
-             << "(minor version) without defining a flag for the\n"
-             << "corresponding release (major version)\n";
-  } else {
-    std::cout << "PASSED\n";
-  }
-
+  std::cout << "PASSED\n";
   return result;
 }
