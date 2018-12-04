90dc0cdf4bd3b5c8f2ae5388b5d6c197a80943d2
Daniel Crispell <dan@visionsystemsinc.com>
2018-12-04 09:21:19 -0500

    Delete bpgl_heightmap_from_disparity+vpgl_local_rational_camera+double--.cxx
    delete no-op file, since instantiation was commented out.

--- contrib/brl/bbas/bpgl/algo/Templates/bpgl_heightmap_from_disparity+vpgl_local_rational_camera+double--.cxx
+++
@@ -1,4 +0,0 @@
-// Instantiaion of bpgl_heightmap_from_disparity
-#include <vpgl/vpgl_local_rational_camera.h>
-#include <bpgl/algo/bpgl_heightmap_from_disparity.hxx>
-//BPGL_HEIGHTMAP_FROM_DISPARITY_INSTANIATE(vpgl_local_rational_camera<double>);
