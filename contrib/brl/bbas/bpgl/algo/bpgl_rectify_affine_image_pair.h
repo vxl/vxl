1a430ea6fba743f5026ad0c165dcfc1ed7c42893
Joseph Mundy <jlm@visionsystemsinc.com>
2019-02-08 15:40:24 -0500

    brl/bpgl/algo: rectify affine image pair using vpgl_equi_rectification

---
+++ contrib/brl/bbas/bpgl/algo/bpgl_rectify_affine_image_pair.h
@@ -0,0 +1,106 @@
+// This is brl/bbas/bpgl/algo/bpgl_rectify_affine_image_pair.h
+#ifndef bpgl_rectify_affine_image_pair_h_
+#define bpgl_rectify_affine_image_pair_h_
+//:
+// \file/
+// \brief rectify two images into a stero pair given affine cameras for each
+// \author J. L. Mundy
+// \date February 6, 2019
+#include <iostream>
+#include <vpgl/vpgl_affine_camera.h>
+#include <vpgl/vpgl_affine_fundamental_matrix.h>
+#include <vil/vil_image_resource.h>
+#include <vil/vil_image_view.h>
+#include <vnl/vnl_matrix_fixed.h>
+
+//
+// requires two images and associated affine cameras. Class can load the data from files if needed.
+// the output is a pair of images that have common epiplolar lines along image rows. The difference in
+// column coordinates of corresponding points is minimized.
+//
+class bpgl_rectify_affine_image_pair
+{
+ public:
+  bpgl_rectify_affine_image_pair(){}
+
+  bpgl_rectify_affine_image_pair(vil_image_view_base_sptr const& view0, vpgl_affine_camera<double> const& acam0,
+                                 vil_image_view_base_sptr const& view1, vpgl_affine_camera<double> const& acam1)
+  {
+    bool good = this->set_images_and_cams(view0,acam0,view1,acam1);
+  }
+
+  bpgl_rectify_affine_image_pair(vil_image_resource_sptr const& resc0, vpgl_affine_camera<double> const& acam0,
+                                 vil_image_resource_sptr const& resc1, vpgl_affine_camera<double> const& acam1)
+  {
+    bool good = this->set_images_and_cams(resc0,acam0,resc1,acam1);
+  }
+
+  //: set images & cameras
+  bool set_images_and_cams(vil_image_view_base_sptr const& view0, vpgl_affine_camera<double> const& acam0,
+                           vil_image_view_base_sptr const& view1, vpgl_affine_camera<double> const& acam1);
+
+  bool set_images_and_cams(vil_image_resource_sptr const& resc0, vpgl_affine_camera<double> const& acam0,
+                           vil_image_resource_sptr const& resc1, vpgl_affine_camera<double> const& acam1);
+
+  //: accessors
+  const vpgl_affine_camera<double>& acam0()const{return acam0_;}
+  const vpgl_affine_camera<double>& acam1()const{return acam1_;}
+  const vpgl_affine_camera<double>& rect_acam0()const{return rect_acam0_;}
+  const vpgl_affine_camera<double>& rect_acam1()const{return rect_acam1_;}
+  const vil_image_view<float>& input_float_view0()const{return fview0_;}
+  const vil_image_view<float>& input_float_view1()const{return fview1_;}
+  const vil_image_view<float>& rectified_fview0() const{return rect_fview0_;}
+  const vil_image_view<float>& rectified_fview1() const{return rect_fview1_;}
+  vnl_matrix_fixed<double, 3, 3> H0() const {return H0_;}
+  vnl_matrix_fixed<double, 3, 3> H1() const {return H1_;}
+
+  //: utility methods
+  static bool load_affine_camera(std::string const& cam_path, vpgl_affine_camera<double>& acam);
+
+  bool process(vgl_box_3d<double>const& scene_box, size_t n_points = 1000)
+  {
+    if(!compute_rectification(scene_box,n_points))
+      return false;
+    warp_pair();
+    return true;
+  }
+
+  // for debug purposes
+  bool load_images_and_cams(std::string const& image0_path, std::string const& cam0_path,
+                            std::string const& image1_path, std::string const& cam1_path);
+
+ protected:
+
+  // protected utility methods
+  bool compute_rectification(vgl_box_3d<double> const& scene_box, size_t n_points = 1000);
+  void compute_warp_dimensions_offsets();
+  static void warp_image(vil_image_view<float> fview,
+                         vnl_matrix_fixed<double, 3, 3> const& H,
+                         vil_image_view<float>& fwarp,
+                         size_t out_ni, size_t out_nj);
+  void warp_pair();
+
+ private:
+  vil_image_view<float> fview0_;
+  vil_image_view<float> fview1_;
+  vil_image_view<float> rect_fview0_;
+  vil_image_view<float> rect_fview1_;
+
+  vpgl_affine_camera<double> acam0_;
+  vpgl_affine_camera<double> acam1_;
+  vpgl_affine_camera<double> rect_acam0_;
+  vpgl_affine_camera<double> rect_acam1_;
+
+  vpgl_affine_fundamental_matrix<double> aF_;
+  // H0, H1 include a translation to keep
+  // image coordinates positive definite
+  vnl_matrix_fixed<double, 3, 3> H0_;
+  vnl_matrix_fixed<double, 3, 3> H1_;
+
+  double du_off_;
+  double dv_off_;
+  size_t out_ni_;
+  size_t out_nj_;
+};
+
+#endif // bpgl_rectify_affine_image_pair_h_
