33f25fe99ce80aa1e039b55573e74949f103f47d
Drew Gilliam <drew@visionsystemsinc.com>
2019-03-29 10:45:41 -0400

    brl/bvgl adaptor - return region list directly

--- contrib/brl/bpro/core/bvgl_pro/processes/bvgl_geo_index_region_resource_process.cxx
+++ contrib/brl/bpro/core/bvgl_pro/processes/bvgl_geo_index_region_resource_process.cxx
@@ -14,6 +14,7 @@
 #  include <vcl_msvc_warnings.h>
 #endif
 #include <bkml/bkml_parser.h>
+#include <bpro/core/bbas_pro/bbas_1d_array_unsigned.h>
 
 
 //: process to get list of leaves that intersect with given 2-d bbox region
@@ -26,21 +27,25 @@ namespace bvgl_geo_index_region_resource_process_globals
 bool bvgl_geo_index_region_resource_process_cons(bprb_func_process& pro)
 {
   using namespace bvgl_geo_index_region_resource_process_globals;
+
   std::vector<std::string> input_types_(n_inputs_);
   input_types_[0] = "vcl_string";    // input geo index text file
   input_types_[1] = "double";        // lower left lon
   input_types_[2] = "double";        // lower left lat
   input_types_[3] = "double";        // upper right lon
   input_types_[4] = "double";        // upper right lat
-  input_types_[5] = "vcl_string";    // output file to store the ids of leaves that intersect with the given region
+  input_types_[5] = "vcl_string";    // output file (optional)
+
   std::vector<std::string> output_types_(n_outputs_);
-  output_types_[0] = "unsigned";     // number of leaves that intersect with given region
+  output_types_[0] = "bbas_1d_array_unsigned_sptr";  // list of leaves that intersect with given region
+
   return pro.set_input_types(input_types_) && pro.set_output_types(output_types_);
 }
 
 bool bvgl_geo_index_region_resource_process(bprb_func_process& pro)
 {
   using namespace bvgl_geo_index_region_resource_process_globals;
+
   // sanity check
   if (!pro.verify_inputs()) {
     std::cerr << pro.name() << ": Wrong Inputs!!!\n";
@@ -54,6 +59,7 @@ bool bvgl_geo_index_region_resource_process(bprb_func_process& pro)
   auto ur_lon = pro.get_input<double>(i++);
   auto ur_lat = pro.get_input<double>(i++);
   std::string out_txt = pro.get_input<std::string>(i++);
+
   // read the tree
   // since we are interested only for the structure, type of the tree doesn't matter here
   if (!vul_file::exists(geo_index_txt)) {
@@ -64,29 +70,44 @@ bool bvgl_geo_index_region_resource_process(bprb_func_process& pro)
   bvgl_2d_geo_index_node_sptr root = bvgl_2d_geo_index::read_and_construct<float>(geo_index_txt, min_size);
   std::vector<bvgl_2d_geo_index_node_sptr> leaves;
   bvgl_2d_geo_index::get_leaves(root, leaves);
+
   // get the ids of intersected leaves
   std::vector<unsigned> leaf_ids;
   vgl_box_2d<double> bbox(ll_lon, ur_lon, ll_lat, ur_lat);
   std::cout << "bounding box: " << bbox << std::endl;
 
   for (unsigned i = 0; i < leaves.size(); i++) {
-    if (vgl_area(vgl_intersection(bbox, leaves[i]->extent_)) > 0)
+    if (vgl_area(vgl_intersection(bbox, leaves[i]->extent_)) > 0) {
       leaf_ids.push_back(i);
+    }
   }
 
-  // write it to file
-  std::ofstream ofs(out_txt.c_str());
-  if (!ofs.is_open()) {
-    std::cerr << pro.name() << ": can not write into file " << out_txt << "!\n";
-    return false;
+  unsigned n_leaves = leaf_ids.size();
+
+  // write leaves to a file (optional)
+  if (!out_txt.empty()) {
+    std::ofstream ofs(out_txt.c_str());
+    if (!ofs.is_open()) {
+      std::cerr << pro.name() << ": can not write into file " << out_txt << "!\n";
+      return false;
+    }
+    ofs << n_leaves << " leaves intersect with region [" << ll_lon << ',' << ll_lat << "] , [" << ur_lon << ',' << ur_lat << "]\n";
+    for (unsigned int leaf_id : leaf_ids)
+      ofs << leaf_id << '\n';
+    ofs.close();
   }
-  ofs << leaf_ids.size() << " leaves intersect with region [" << ll_lon << ',' << ll_lat << "] , [" << ur_lon << ',' << ur_lat << "]\n";
-  for (unsigned int leaf_id : leaf_ids)
-    ofs << leaf_id << '\n';
-  ofs.close();
-  pro.set_output_val<unsigned>(0, leaf_ids.size());
+
+  // fill out bbas_1d array
+  bbas_1d_array_unsigned_sptr leaf_ids_bbas = new bbas_1d_array_unsigned(n_leaves);
+  for (unsigned i = 0; i<n_leaves; i++) {
+    leaf_ids_bbas->data_array[i] = leaf_ids[i];
+  }
+
+  // output
+  pro.set_output_val<bbas_1d_array_unsigned_sptr>(0, leaf_ids_bbas);
   return true;
 }
+
 
 //: process to get leaves that intersect with given polygon (input as kml)
 namespace bvgl_geo_index_region_poly_resource_process_globals
