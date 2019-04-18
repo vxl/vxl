86fe50d447287706f34650fb74c2446aebd1ed17
Cynthia Lucock <cynthia.lucock@visionsystemsinc.com>
2019-04-18 15:33:44 -0400

    fixed final core LNK4221 warning

--- core/vil/io/Templates/vil_io_smart_ptr+vil_image_view_base-.cxx
+++
@@ -1,6 +0,0 @@
-#include <vil/io/vil_io_smart_ptr.hxx>
-#include <vil/io/vil_io_image_view_base.h>
-#ifdef __GNUC__
-//VIL_IO_SMART_PTR_INSTANTIATE(vil_image_view_base);
-//VIL_IO_SMART_PTR_INSTANTIATE(vil_image_resource);
-#endif
