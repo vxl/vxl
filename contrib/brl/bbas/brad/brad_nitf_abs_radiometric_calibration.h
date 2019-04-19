689cddac6b7e6b3dca1d3765a534804ca4842687
Noah Johnson <noah.johnson@visionsystemsinc.com>
2019-04-19 11:13:49 -0400

    renamed

--- contrib/brl/bbas/brad/brad_nitf_abs_radiometric_calibration.h
+++
@@ -1,8 +0,0 @@
-#ifndef brad_nitf_abs_radiometric_calibration_h_
-#define brad_nitf_abs_radiometric_calibration_h_
-
-#include <brad/brad_image_metadata.h>
-#include <vil/vil_convert.h>
-
-vil_image_view_base_sptr brad_nitf_abs_radiometric_calibrate(vil_image_view_base_sptr img_sptr, brad_image_metadata_sptr md);
-#endif // brad_nitf_abs_radiometric_calibration_h_
