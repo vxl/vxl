90e3e4bde0c497fdbb80c357c4399cef07926218
Hans Johnson <hans.j.johnson@gmail.com>
2019-12-11 13:12:17 -0600

    BUG: Isolate timing of function.
    When testing the timing of a function, only the function
    should be included in the timing mechanism.
    
    This type of testing will be susceptible to optimization
    behaviors.

--- core/vnl/algo/tests/test_symmetric_eigensystem.cxx
+++ core/vnl/algo/tests/test_symmetric_eigensystem.cxx
@@ -89,16 +89,14 @@ test_symmetric_eigensystem()
 
   { // compare speed and values of specialised 3x3 version with nxn version
     constexpr unsigned n = 20000;
-    double fixed_data[n][3];
-    double netlib_data[n][3];
 
-    int fixed_time;
+    double fixed_data[n][3];
+    int fixed_time=0;
     {
       double M11, M12, M13, M22, M23, M33;
       // Generate a random system
       vnl_random rng(5);
 
-      const std::clock_t timer_01 = std::clock();
       for (auto & c : fixed_data)
       {
         M11 = rng.drand64() * 10.0 - 5.0;
@@ -107,20 +105,21 @@ test_symmetric_eigensystem()
         M22 = rng.drand64() * 10.0 - 5.0;
         M23 = rng.drand64() * 10.0 - 5.0;
         M33 = rng.drand64() * 10.0 - 5.0;
+        const std::clock_t timer_01 = std::clock();
         vnl_symmetric_eigensystem_compute_eigenvals(M11, M12, M13, M22, M23, M33, c[0], c[1], c[2]);
+        const std::clock_t timer_02 = std::clock();
+        fixed_time += (timer_02 - timer_01) / (CLOCKS_PER_SEC / 1000);
       }
-      const std::clock_t timer_02 = std::clock();
-      fixed_time = (timer_02 - timer_01) / (CLOCKS_PER_SEC / 1000);
     }
 
-    int netlib_time;
+    double netlib_data[n][3];
+    int netlib_time=0;
     {
       // Generate same random system
       vnl_random rng(5);
       vnl_double_3x3 M, evecs;
       vnl_double_3 evals;
 
-      const std::clock_t timer_03 = std::clock();
       for (auto & c : netlib_data)
       {
         M(0, 0) = rng.drand64() * 10.0 - 5.0;
@@ -130,13 +129,14 @@ test_symmetric_eigensystem()
         M(2, 1) = M(1, 2) = rng.drand64() * 10.0 - 5.0;
         M(2, 2) = rng.drand64() * 10.0 - 5.0;
 
+        const std::clock_t timer_03 = std::clock();
         vnl_symmetric_eigensystem_compute(M.as_ref(), evecs.as_ref().non_const(), evals.as_ref().non_const());
+        const std::clock_t timer_04 = std::clock();
+        netlib_time += (timer_04 - timer_03) / (CLOCKS_PER_SEC / 1000);
         c[0] = evals[0];
         c[1] = evals[1];
         c[2] = evals[2];
       }
-      const std::clock_t timer_04 = std::clock();
-      netlib_time = (timer_04 - timer_03) / (CLOCKS_PER_SEC / 1000);
     }
 
     std::cout << "Fixed Time: " << fixed_time << "   netlib time: " << netlib_time << std::endl;
