310cb72d568e6948858cdeb1b3ba44e030e11eb3
Drew Gilliam <drew@visionsystemsinc.com>
2020-06-08 14:15:02 -0400

    brad remove unnecessary #include

--- contrib/brl/bbas/brad/brad_multispectral_functions.cxx
+++ contrib/brl/bbas/brad/brad_multispectral_functions.cxx
@@ -1,24 +1,8 @@
-#include <string>
 #include <vector>
-#include <iostream>
-#include <sstream>
-#include <fstream>
+#include <vil/vil_image_view.h>
+#include <vgl/vgl_polygon.h>
 
-#include "vul/vul_file.h"
-#include "vul/vul_file_iterator.h"
-#include "vil/vil_convert.h"
-#include "vil/vil_image_view.h"
-#include "vil/vil_load.h"
-#include "vil/vil_crop.h"
-#include "vil/vil_save.h"
-#include "vnl/vnl_math.h"
-#include "vul/vul_awk.h"
-#include <brad/brad_image_metadata.h>
-#include <brad/brad_atmospheric_parameters.h>
-#include <brad/brad_image_atmospherics_est.h>
-#include <brad_multispectral_functions.h>
-#include <brad_spectral_angle_mapper.h>
-
+#include "brad_multispectral_functions.h"
 
 
 //----------------------------------------------------
