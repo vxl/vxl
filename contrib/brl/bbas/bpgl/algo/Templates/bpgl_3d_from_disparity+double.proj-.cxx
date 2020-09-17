fedba41282ed249df208579a83d948725a89117b
Joseph Mundy <jlm@visionsystemsinc.com>
2020-09-17 07:10:42 -0400

    add proj_camera as type for 3d reconstruction

---
+++ contrib/brl/bbas/bpgl/algo/Templates/bpgl_3d_from_disparity+double.proj-.cxx
@@ -0,0 +1,4 @@
+// Instantiaion of bpgl_3d_from_disparity
+#include <vpgl/vpgl_proj_camera.h>
+#include <bpgl/algo/bpgl_3d_from_disparity.hxx>
+BPGL_3D_FROM_DISPARITY_INSTANIATE(double, vpgl_proj_camera<double>);
