e2e7d88a13447b6e29a32ab102e93afb9cca67dc
Joseph Mundy <jlm@visionsystemsinc.com>
2020-11-02 14:03:18 -0500

    add grid index for float data

---
+++ contrib/brl/bbas/bvgl/Templates/vbl_array_3d+vcl_vector+vgl_vector_3d+float---.cxx
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include "vgl/vgl_vector_3d.h"
+#ifdef _MSC_VER
+#  include "vcl_msvc_warnings.h"
+#endif
+#include "vbl/vbl_array_3d.hxx"
+typedef std::vector<vgl_vector_3d<float> > vvect;
+VBL_ARRAY_3D_INSTANTIATE(vvect);
