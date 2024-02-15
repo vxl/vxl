f2dd4e1b4c1fd70704cb20dc0d95e90dcfd7c403
Joseph Mundy <jlm@visionsystemsinc.com>
2024-02-15 07:29:31 -0500

    add pyramid image byte

--- core/vil/vil_pyramid_image_view.hxx
+++ core/vil/vil_pyramid_image_view.hxx
@@ -31,8 +31,8 @@ vil_pyramid_image_view<T>::vil_pyramid_image_view(vil_image_view_base_sptr image
     return;
 
   // it works with grey scale images
-  if (img->nplanes() != 1)
-    return;
+  //if (img->nplanes() != 1)
+   // return;
 
   images_.resize(levels);
   scales_.resize(levels);
