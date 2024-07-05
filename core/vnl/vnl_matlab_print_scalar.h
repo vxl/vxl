d77f5af0a0f0041bcf55519ed760a76842c52da3
Andrei Sandor <andrei.sandor@mail.mcgill.ca>
2024-07-05 17:23:02 -0400

    COMP: Replace sprintf to snprintf
    Replacement of sprintf to snprintf.
    
    Here the snprintf requires the size of buf. Unfortunately, buf, without it’s length, is passed to the function.
    
    We can also see that each function in the file has the same name and a argument list very similar for each of them.
    
    I propose to create a function that has the same name as the others, but with the length of buf added and snprintf used. Then, use that function for the other functions accordingly. Use VXL_DEPRECATED for the function without buf_len. Updated the header file to introduce the newly deprecated functions

--- core/vnl/vnl_matlab_print_scalar.h
+++ core/vnl/vnl_matlab_print_scalar.h
@@ -14,11 +14,13 @@
 #endif
 #include "vnl_matlab_print_format.h"
 #include "vnl/vnl_export.h"
+#include "vcl_compiler.h"
 
 //: print real or complex scalar into character buffer.
 #define vnl_matlab_print_scalar_declare(T) \
 VNL_EXPORT void vnl_matlab_print_scalar(T v, \
                              char *buf, \
+                             size_t buf_len, \
                              vnl_matlab_print_format =vnl_matlab_print_format_default)
 
 // Even with a function template we would have to
@@ -31,6 +33,27 @@ vnl_matlab_print_scalar_declare(long double);
 vnl_matlab_print_scalar_declare(std::complex<float>);
 vnl_matlab_print_scalar_declare(std::complex<double>);
 vnl_matlab_print_scalar_declare(std::complex<long double>);
+
+
+//: print real or complex scalar into character buffer.
+// These variants are deprecated because the buffer size is not provided
+#define vnl_matlab_print_scalar_declare_old(T) \
+VNL_EXPORT VXL_DEPRECATED void vnl_matlab_print_scalar(T v, \
+                             char *buf, \
+                             vnl_matlab_print_format =vnl_matlab_print_format_default)
+
+// Even with a function template we would have to
+// forward declare all the specializations anyway.
+vnl_matlab_print_scalar_declare_old(int);
+vnl_matlab_print_scalar_declare_old(unsigned int);
+vnl_matlab_print_scalar_declare_old(float);
+vnl_matlab_print_scalar_declare_old(double);
+vnl_matlab_print_scalar_declare_old(long double);
+vnl_matlab_print_scalar_declare_old(std::complex<float>);
+vnl_matlab_print_scalar_declare_old(std::complex<double>);
+vnl_matlab_print_scalar_declare_old(std::complex<long double>);
+
+
 
 //: print scalar to std::ostream.
 template <class T> VNL_EXPORT
