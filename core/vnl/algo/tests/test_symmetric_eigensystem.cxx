b417d51f4709f02f084277f3bc345c0415917354
Hans Johnson <hans-johnson@uiowa.edu>
2024-12-23 10:03:27 -0600

    BUG: Make speed test more robust to failure
    The speed test can fail due when the hardware context switches
    during the running of the test.
    
    Put a guard on the speed tests to make this test more stable.  Running
    this test on as part of continuous integration environment indicated
    that it was not stable.  The CI environment is not well controlled, and
    it was possible for the tests to give poor results due to overloaded CI
    hardware (or virtualised hardware) making the process of timing very
    unstable.  This was observed when the CI systems are under heavy load
    
    This solution adds a guard that at lest 95% of the 20000 tests need
    to be faster in the custom 3D computation vs the generic sized version.

--- core/vnl/algo/tests/test_symmetric_eigensystem.cxx
+++ core/vnl/algo/tests/test_symmetric_eigensystem.cxx
@@ -92,63 +92,63 @@ test_symmetric_eigensystem()
   { // compare speed and values of specialised 3x3 version with nxn version
     constexpr unsigned n = 20000;
 
+    // Generate a random system with seeded value to known value
+    vnl_random rng(5);
     double fixed_data[n][3];
     unsigned int fixed_time = 0;
-    {
-      double M11;
-      double M12;
-      double M13;
-      double M22;
-      double M23;
-      double M33;
-      // Generate a random system
-      vnl_random rng(5);
-
-      for (auto & c : fixed_data)
-      {
-        M11 = rng.drand64() * 10.0 - 5.0;
-        M12 = rng.drand64() * 10.0 - 5.0;
-        M13 = rng.drand64() * 10.0 - 5.0;
-        M22 = rng.drand64() * 10.0 - 5.0;
-        M23 = rng.drand64() * 10.0 - 5.0;
-        M33 = rng.drand64() * 10.0 - 5.0;
-        const auto timer_01 = std::chrono::high_resolution_clock::now();
-        vnl_symmetric_eigensystem_compute_eigenvals(M11, M12, M13, M22, M23, M33, c[0], c[1], c[2]);
-        const auto timer_02 = std::chrono::high_resolution_clock::now();
-        fixed_time += (timer_02 - timer_01).count();
-      }
-    }
 
     double netlib_data[n][3];
     unsigned int netlib_time = 0;
+
+    unsigned int count_fixed_is_faster = 0;
+
+    // Ensure that the matrix values are the same for both tests
+    for (int mi = 0; mi < n; ++mi)
     {
-      // Generate same random system
-      vnl_random rng(5);
       vnl_double_3x3 M;
-      vnl_double_3x3 evecs;
-      vnl_double_3 evals;
+      const double M11 = M(0, 0) = rng.drand64() * 10.0 - 5.0;
+      const double M12 = M(1, 0) = M(0, 1) = rng.drand64() * 10.0 - 5.0;
+      const double M13 = M(2, 0) = M(0, 2) = rng.drand64() * 10.0 - 5.0;
+      const double M22 = M(1, 1) = rng.drand64() * 10.0 - 5.0;
+      const double M23 = M(2, 1) = M(1, 2) = rng.drand64() * 10.0 - 5.0;
+      const double M33 = M(2, 2) = rng.drand64() * 10.0 - 5.0;
 
-      for (auto & c : netlib_data)
-      {
-        M(0, 0) = rng.drand64() * 10.0 - 5.0;
-        M(1, 0) = M(0, 1) = rng.drand64() * 10.0 - 5.0;
-        M(2, 0) = M(0, 2) = rng.drand64() * 10.0 - 5.0;
-        M(1, 1) = rng.drand64() * 10.0 - 5.0;
-        M(2, 1) = M(1, 2) = rng.drand64() * 10.0 - 5.0;
-        M(2, 2) = rng.drand64() * 10.0 - 5.0;
-
+      unsigned int current_iter_fixed_time = 0;
+      { // Compute time for custom 3D
+        auto c = fixed_data[mi];
+        const auto timer_01 = std::chrono::high_resolution_clock::now();
+        vnl_symmetric_eigensystem_compute_eigenvals(M11, M12, M13, M22, M23, M33, c[0], c[1], c[2]);
+        const auto timer_02 = std::chrono::high_resolution_clock::now();
+        current_iter_fixed_time = (timer_02 - timer_01).count();
+        fixed_time += current_iter_fixed_time;
+      }
+      unsigned int current_iter_netlib_time = 0;
+      { // Compute time for any size array
+        vnl_double_3x3 evecs;
+        vnl_double_3 evals;
         const auto timer_03 = std::chrono::high_resolution_clock::now();
         vnl_symmetric_eigensystem_compute(M.as_ref(), evecs.as_ref().non_const(), evals.as_ref().non_const());
         const auto timer_04 = std::chrono::high_resolution_clock::now();
-        netlib_time += (timer_04 - timer_03).count();
+        current_iter_netlib_time = (timer_04 - timer_03).count();
+        netlib_time += current_iter_netlib_time;
+        auto c = netlib_data[mi];
         c[0] = evals[0];
         c[1] = evals[1];
         c[2] = evals[2];
       }
+      count_fixed_is_faster += (current_iter_fixed_time < current_iter_netlib_time) ? 1 : 0;
     }
 
-    std::cout << "Fixed Time: " << fixed_time << "   netlib time: " << netlib_time << std::endl;
-    TEST("Specialised version is faster", fixed_time < netlib_time, true);
+    std::cout << "Cummulative fixed Time: " << fixed_time << "  cummulative netlib time: " << netlib_time << std::endl;
+    const auto percent_faster = static_cast<double>(count_fixed_is_faster) * 100.0 / static_cast<double>(n);
+    std::cout << "Fixed test was faster than netlib test " << percent_faster << "% of the " << n << " tests\n";
+    const auto speedup = static_cast<double>(netlib_time) / static_cast<double>(fixed_time);
+    std::cout << "Speedup for fixed tests was (on average)  " << speedup << " times faster than " << " netlib tests\n";
+    // Put a guard on the speed tests to make this test more stable.  Running this test on as part of continuous
+    // integration environment indicated that it was not stable.  The CI environment is not well controlled, and
+    // it was possible for the tests to give poor results due to overloaded CI hardware (or virutalized hardware)
+    // making the process of timing very unstable.  This was observed when the CI systems are under heavy load
+    TEST("Specialised version is faster", fixed_time < netlib_time || percent_faster > 95.0, true);
 
     double sum_dsq = 0.0;
     double max_dsq = 0.0;
